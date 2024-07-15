015f35be992963125f1652546d3ca179f66293bd
beep boop <githubbot@vadim.me>
2024-07-15 17:53:20 +0200

    Solution 5 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ewn88e2l Prompt:       |                              ^~~~~~ adc41afb-3902-484a-8700-779d5d40c71e.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ adc41afb-3902-484a-8700-779d5d40c71e.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ adc41afb-3902-484a-8700-779d5d40c71e.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from adc41afb-3902-484a-8700-779d5d40c71e.cpp:13: adc41afb-3902-484a-8700-779d5d40c71e.cpp: In function ‘int main()’: adc41afb-3902-484a-8700-779d5d40c71e.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ adc41afb-3902-484a-8700-779d5d40c71e.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -4,16 +4,16 @@
 #include <cassert>
 
 std::vector<std::string> reverse_delete(std::string s, std::string c) {
-        std::string result;
-        for (char ch : s) {
-            if (c.find(ch) == std::string::npos) {
-                result += ch;
-            }
+    std::string result;
+    for (char ch : s) {
+        if (c.find(ch) == std::string::npos) {
+            result += ch;
         }
-        std::string result_reverse = result;
-        std::reverse(result_reverse.begin(), result_reverse.end());
-        return {result, (result == result_reverse) ? "True" : "False"};
     }
+    std::string result_reverse = result;
+    std::reverse(result_reverse.begin(), result_reverse.end());
+    return {result, (result == result_reverse) ? "True" : "False"};
+}
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
