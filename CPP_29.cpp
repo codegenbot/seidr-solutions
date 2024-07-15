a1d015b54bf7ec01e465cda813b3d5ad45b78e27
beep boop <githubbot@vadim.me>
2024-07-15 13:32:03 +0200

    Solution 28 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/meqxroy8 Prompt: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d12aac09-5044-4c6c-9b8c-2c60b464343b.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d12aac09-5044-4c6c-9b8c-2c60b464343b.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d12aac09-5044-4c6c-9b8c-2c60b464343b.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d12aac09-5044-4c6c-9b8c-2c60b464343b.cpp:21: d12aac09-5044-4c6c-9b8c-2c60b464343b.cpp: In function ‘int main()’: d12aac09-5044-4c6c-9b8c-2c60b464343b.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -20,7 +20,6 @@ std::vector<std::string> filter_by_prefix(const std::vector<std::string>& vec, c
 }
 
 int main() {
-    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"),
-                  std::vector<std::string>{"xxx", "xxxAAA", "xxx"}));
+    assert(std::issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
     return 0;
 }
