0fe75a8ab9abbe74d3c6a115651cd619008b2373
beep boop <githubbot@vadim.me>
2024-07-15 16:07:35 +0200

    Solution 36 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ep2ro3qu Prompt: 4be04e24-3504-4dd8-81e5-d40fc2223bdb.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 4be04e24-3504-4dd8-81e5-d40fc2223bdb.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -20,7 +20,4 @@ std::string match_parens(std::vector<std::string> lst) {
     return cnt == 0 ? "Yes" : "No";
 }
 
-int main() {
-    assert(match_parens({"(", ")"}) == "Yes");
-    return 0;
-}
+assert(match_parens({"(", ")"}) == "Yes");
