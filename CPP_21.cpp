33cfe363a8f87473c35af3000214c1cebe75a746
beep boop <githubbot@vadim.me>
2024-07-15 16:31:22 +0200

    Solution 38 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ano08tdj Prompt:    15 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ ae3b04c5-4271-4fcf-9389-faa4e092fc1e.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ ae3b04c5-4271-4fcf-9389-faa4e092fc1e.cpp:15:43: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from ae3b04c5-4271-4fcf-9389-faa4e092fc1e.cpp:14: ae3b04c5-4271-4fcf-9389-faa4e092fc1e.cpp: In function ‘int main()’: ae3b04c5-4271-4fcf-9389-faa4e092fc1e.cpp:24:20: error: ‘rescale_to_unit’ was not  declared in this scope    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ ae3b04c5-4271-4fcf-9389-faa4e092fc1e.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,5 +1,18 @@
-vector<float> rescale_to_unit(const vector<float>& numbers);
-
 bool issame(const vector<float>& a, const vector<float>& b) {
     return a == b;
+}
+
+vector<float> rescale_to_unit(const vector<float>& numbers);
+
+vector<float> rescale_to_unit(const vector<float>& numbers) {
+    float min_num = *min_element(numbers.begin(), numbers.end());
+    float max_num = *max_element(numbers.begin(), numbers.end());
+    
+    vector<float> rescaled_numbers;
+    for (float num : numbers) {
+        float rescaled_num = (num - min_num) / (max_num - min_num);
+        rescaled_numbers.push_back(rescaled_num);
+    }
+    
+    return rescaled_numbers;
 }
