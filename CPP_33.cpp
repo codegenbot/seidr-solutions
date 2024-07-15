78d3dc844678a5e161a68229e41f2031cee73cb6
beep boop <githubbot@vadim.me>
2024-07-15 16:50:56 +0200

    Solution 20 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/qrbhv912 Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 33ac9713-99fd-47cc-a060-fdb30a348ac2.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 33ac9713-99fd-47cc-a060-fdb30a348ac2.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 33ac9713-99fd-47cc-a060-fdb30a348ac2.cpp:16: 33ac9713-99fd-47cc-a060-fdb30a348ac2.cpp: In function ‘int main()’: 33ac9713-99fd-47cc-a060-fdb30a348ac2.cpp:26:20: error: ‘sort_third’ was not decl ared in this scope    26 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 33ac9713-99fd-47cc-a060-fdb30a348ac2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,26 +1,27 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
-
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); ++i) {
-        if ((i + 1) % 3 == 0) {
-            std::sort(l_copy.begin() + i - 2, l_copy.begin() + i + 1);
-        }
-    }
-    return l_copy;
-}
 
 bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
+std::vector<int> sort_third(std::vector<int> l){
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            std::vector<int> temp;
+            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+                temp.push_back(l[j]);
+            }
+            std::sort(temp.begin(), temp.end());
+            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+                l[j] = temp[j - i];
+            }
+        }
+    }
+    return l;
+}
+
 int main() {
     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    // Add more test cases here if needed
-    std::cout << "All tests pass!" << std::endl;
-
     return 0;
 }
