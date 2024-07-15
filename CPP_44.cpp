5ec51f742bc299ffbfc52f45a2b8b9a5551756dc
beep boop <githubbot@vadim.me>
2024-07-15 22:47:58 +0200

    Solution 65 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/twiyy66a Prompt:    29 | int main(){       |     ^~~~ 4b18456a-de38-424d-8fdc-f8f8a9a6d5c9.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 4b18456a-de38-424d-8fdc-f8f8a9a6d5c9.cpp:28: 4b18456a-de38-424d-8fdc-f8f8a9a6d5c9.cpp: In function ‘int main()’: 4b18456a-de38-424d-8fdc-f8f8a9a6d5c9.cpp:32:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    32 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 4b18456a-de38-424d-8fdc-f8f8a9a6d5c9.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -15,7 +15,7 @@ std::string change_base(int num, int base) {
 
 int main() {
     int x = 2;
-    for(int i = 2; i <= 10; ++i){
+    for (int i = 2; i <= 10; ++i) {
         std::cout << "Base " << i << ": " << change_base(x, i) << std::endl;
     }
 
