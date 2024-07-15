cae3bdb1b1b7a1003e05775757e1c041c56dc39b
beep boop <githubbot@vadim.me>
2024-07-15 13:31:49 +0200

    Solution 50 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/56a0g59v Prompt:       |             ^~~~~~ 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp:22: 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp: In function ‘int main()’: 1945e8a2-d433-4b6a-adb4-eb7ab122ac57.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -6,16 +6,10 @@ bool is_same(std::vector<int> a, std::vector<int> b) {
 }
 
 std::vector<int> sum_product(std::vector<int> a) {
-    int sum = 0;
-    int product = 1;
+    int sum = 0, product = 1;
     for (int num : a) {
         sum += num;
         product *= num;
     }
     return {sum, product};
-}
-
-int main() {
-    assert(is_same(sum_product({10}), {10, 10}));
-    return 0;
 }
