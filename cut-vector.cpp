cc46baaa6f5638e5971b0b127d310ee2a3dbd898
beep boop <githubbot@vadim.me>
2024-07-15 19:50:17 +0200

    Solution 49 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,44 +1,47 @@
 ```
 #include <vector>
+#include <cmath>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
-
-    if(n <= 1)
-        return {{}, {}};
-
-    int min_diff = INT_MAX;
-    int cut_idx = 0;
-
+    
+    int minDiff = INT_MAX;
+    int pos = -1;
+    
     for(int i = 0; i < n; i++) {
-        int left_sum = 0, right_sum = 0;
+        int leftSum = 0, rightSum = 0;
         
-        for(int j = 0; j <= i; j++)
-            left_sum += v[j];
-        for(int j = i+1; j < n; j++)
-            right_sum += v[j];
-
-        if(abs(left_sum - right_sum) < min_diff) {
-            min_diff = abs(left_sum - right_sum);
-            cut_idx = i;
+        if(i > 0)
+            leftSum = accumulate(v.begin(), v.begin() + i, 0);
+        else
+            leftSum = v[0];
+        
+        for(int j = i+1; j <= n-1; j++)
+            rightSum += v[j];
+        
+        int diff = abs(leftSum - rightSum);
+        
+        if(diff < minDiff) {
+            minDiff = diff;
+            pos = i;
         }
     }
-
-    res[0].assign(v.begin(), v.begin() + cut_idx+1);
-    res[1].assign(v.begin()+cut_idx, v.end());
-
+    
+    res[0] = vector<int>(v.begin(), v.begin() + pos);
+    res[1] = vector<int>(v.begin() + pos, v.end());
+    
     return res;
 }
 
-int main() {
-    vector<int> v = {2, 3, 5, 10};
+int main() { 
+    vector<int> v = {5, 8, 9, 10};
     vector<vector<int>> result = cutVector(v);
-
-    for(auto sub : result) {
-        for(int x : sub)
-            cout << x << " ";
-        cout << endl;
+    for(auto &sub : result) {
+        cout << "[";
+        for(int i : sub)
+            cout << i << " ";
+        cout << "] ";
     }
 }
