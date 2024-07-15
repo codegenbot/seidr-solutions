1fca730ba97c8325e1e5612bd08c7b53cea4a4dc
beep boop <githubbot@vadim.me>
2024-07-15 16:40:00 +0200

    Solution 55 of square-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/kvslozs3 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from b40f5052-a310-48ca-8649-4cb0d1d56e74.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ b40f5052-a310-48ca-8649-4cb0d1d56e74.cpp:5:25:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- square-digits.cpp
+++ square-digits.cpp
@@ -11,9 +11,8 @@ std::string squareDigits(std::string input) {
     for(int i = 0; i < input.length(); i++) {
         int digit = input[i] - '0';
         std::string squared = std::to_string(digit * digit);
-        if(i + squared.length() > input.length()) break;
         input = input.substr(0, i) + squared + input.substr(i+1);
-        i += squared.length();
+        i += (int)squared.length();
     }
     return input;
 }
