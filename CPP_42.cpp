700b1ee329c648380966b4d66f4f0cbf912fb7a4
beep boop <githubbot@vadim.me>
2024-07-15 14:16:16 +0200

    Solution 53 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ryrc7ga5 Prompt:       |             ^~~~~~ 672eeaa0-1440-43a9-a9ab-d792e1c346b7.cpp:22:20: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 672eeaa0-1440-43a9-a9ab-d792e1c346b7.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 672eeaa0-1440-43a9-a9ab-d792e1c346b7.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 672eeaa0-1440-43a9-a9ab-d792e1c346b7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 672eeaa0-1440-43a9-a9ab-d792e1c346b7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 672eeaa0-1440-43a9-a9ab-d792e1c346b7.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -13,7 +13,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 std::vector<int> incr_list(std::vector<int> l) {
     for (int i = 0; i < l.size(); i++) {
-        l[i]++;
+       l[i]++;
     }
     return l;
 }
