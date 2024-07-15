2713b3499bca65020a243fbb16e25617f9066c7f
beep boop <githubbot@vadim.me>
2024-07-15 22:26:12 +0200

    Solution 11 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/9of3l22s Prompt:       |                                  ^~~ 2b167d9d-11b6-445a-9bfa-857cad23a9f4.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 2b167d9d-11b6-445a-9bfa-857cad23a9f4.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 2b167d9d-11b6-445a-9bfa-857cad23a9f4.cpp:24: 2b167d9d-11b6-445a-9bfa-857cad23a9f4.cpp: In function ‘int main()’: 2b167d9d-11b6-445a-9bfa-857cad23a9f4.cpp:34:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    34 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 2b167d9d-11b6-445a-9bfa-857cad23a9f4.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -13,16 +13,3 @@ std::vector<int> solve(const std::vector<int>& l) {
     result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
 }
-
-int main() {
-    std::vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    std::vector<int> expected_output = {0, 2, 3, 5, 9, 123};
-    
-    if (issame(solve(input), expected_output)) {
-        std::cout << "Test Case Passed" << std::endl;
-    } else {
-        std::cout << "Test Case Failed" << std::endl;
-    }
-
-    return 0;
-}
