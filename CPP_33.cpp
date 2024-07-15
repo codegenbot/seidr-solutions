fa4c39b52fe02606983cd89cc5c009b8a4d4fb04
beep boop <githubbot@vadim.me>
2024-07-15 13:33:45 +0200

    Solution 4 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/z9ihtea4 Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f88683bc-a9da-4384-8619-2efd744afcdd.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f88683bc-a9da-4384-8619-2efd744afcdd.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f88683bc-a9da-4384-8619-2efd744afcdd.cpp:10: f88683bc-a9da-4384-8619-2efd744afcdd.cpp: In function ‘int main()’: f88683bc-a9da-4384-8619-2efd744afcdd.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ f88683bc-a9da-4384-8619-2efd744afcdd.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,22 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
-
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 3 == 0) {
-            std::sort(l_copy.begin() + i, l_copy.begin() + i + 3);
+vector<int> sort_third(vector<int> l) {
+    vector<int> l_copy = l;
+    for (int i = 0; i < l.size(); i += 3) {
+        if (i + 2 < l.size()) {
+            sort(l_copy.begin() + i, l_copy.begin() + i + 3);
         }
     }
     return l_copy;
-}
-
-int main() {
-    assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , std::vector<int>{2, 6, 3, 4, 8, 9, 5, 1}));
 }
