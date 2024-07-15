6ee31a09f6e9cbb7f27677e67e4e0e8b61be7ae6
beep boop <githubbot@vadim.me>
2024-07-15 21:01:44 +0200

    Solution 42 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yxhj599w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/994bf557-7e93-4428-a1bb-efa588a0e 88d.py", line 18, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/994bf557-7e93-4428-a1bb-efa588a0e 88d.py", line 14, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,8 +2,8 @@ def encode(message):
     return ''.join([
         chr(((ord(char.upper()) - ord('A') + 1) % 26) + ord('A'))
         if char.isalpha() and char.lower() not in set('aeiou')
-        else char.swapcase() 
-        if char.isalpha() 
+        else char.swapcase()
+        if char.isalpha()
         else char
         for char in message.replace('\n', '').replace('\r', '')
     ])
