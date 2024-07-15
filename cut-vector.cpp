ebb2ed489e9749f3eac3f96cde9e2ca0448cf851
beep boop <githubbot@vadim.me>
2024-07-15 18:02:04 +0200

    Solution 37 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/r9npe7x9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,31 +1,38 @@
+```c++
 #include <vector>
-#include <cmath>
 using namespace std;
 
 int main() {
     vector<int> v = {1, 2, 3, 4, 5};
     vector<vector<int>> res = cutVector(v);
-    return 0;
+    for (auto& x : res) {
+        for (auto y : x) {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
 }
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
 
+    if(n == 1)
+        return {{v[0]}};
+
     for(int i = 0; i < n; i++) {
         if(i == 0 || i == n-1)
             res[0].push_back(v[i]);
         else {
-            if(abs(v[0] - v[i]) <= abs(v[n-1] - v[i])) {
+            int left_sum = 0, right_sum = 0;
+            for(int j = 0; j <= i; j++)
+                left_sum += v[j];
+            for(int j = i; j < n; j++)
+                right_sum += v[j];
+
+            if(left_sum == right_sum) {
                 res[0].clear();
-                for(int j = 0; j < i; j++)
-                    res[0].push_back(v[j]);
-                res[0].push_back(v[i]);
-                break;
-            }
-            else {
-                res[0].clear();
-                for(int j = i; j < n; j++)
+                for(int j = 0; j <= i; j++)
                     res[0].push_back(v[j]);
                 break;
             }
@@ -33,19 +40,17 @@ vector<vector<int>> cutVector(vector<int> v) {
     }
 
     if(res[0].size() != n) {
+        int left_sum = 0, right_sum = 0;
         for(int i = 0; i < n; i++) {
             if(i == 0 || i == n-1)
                 res[1].push_back(v[i]);
             else {
-                if(abs(v[0] - v[i]) <= abs(v[n-1] - v[i])) {
+                left_sum += v[i];
+                right_sum += v[n-i-1];
+
+                if(left_sum == right_sum) {
                     res[1].clear();
                     for(int j = i; j < n; j++)
-                        res[1].push_back(v[j]);
-                    break;
-                }
-                else {
-                    res[1].clear();
-                    for(int j = 0; j < i; j++)
                         res[1].push_back(v[j]);
                     break;
                 }
