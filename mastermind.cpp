8555e318ce4ee73d54dce4c07310995ac7dbd95a
beep boop <githubbot@vadim.me>
2024-07-15 16:35:13 +0200

    Solution 36 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/ak149vls Prompt: Program failed

--- mastermind.cpp
+++ mastermind.cpp
@@ -14,33 +14,20 @@ int whitePegs(std::string code, std::string guess) {
 
 int blackPegs(std::string code, std::string guess) {
     int blackCount = 0;
-    int whiteCount = whitePegs(code, guess);
-    
     std::unordered_map<char,int> codeMap;
+    
     for(int i=0; i<4; i++) {
-        codeMap[code[i]]++;
+        if(code[i] != ' ') {
+            codeMap[code[i]]++;
+        }
     }
     
     for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
-            continue;
-        }
-        
-        codeMap[guess[i]]--;
-        
-        bool isBlack = false;
-        for(int j=0; j<4; j++) {
-            if(i != j && code[j] == guess[i]) {
-                isBlack = true;
-                break;
-            } else if (i != j && codeMap[code[j]]-- > 0) {
-                blackCount++;
-                break;
-            }
-        }
-        
-        if(!isBlack) {
             blackCount++;
+        } else if(codeMap.find(guess[i]) != codeMap.end() && codeMap[guess[i]] > 0) {
+            blackCount++;
+            codeMap[guess[i]]--;
         }
     }
     
@@ -50,8 +37,7 @@ int blackPegs(std::string code, std::string guess) {
 int main() {
     std::string code, guess;
     std::cin >> code >> guess;
-    int white = whitePegs(code, guess);
-    std::cout << white << std::endl;
+    std::cout << whitePegs(code, guess) << std::endl;
     std::cout << blackPegs(code, guess) << std::endl;
     return 0;
 }
