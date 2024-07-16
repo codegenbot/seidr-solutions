e00c95fa68f6bded1c1ef3b3d2d82c5e91260add
beep boop <githubbot@vadim.me>
2024-07-16 15:54:51 +0200

    Solution 74 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6e2io3hy Prompt: 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:26:14: error: ‘string’ was not declared  in this scope    26 | int getBonus(string s) {       |              ^~~~~~ 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:26:14: note: ‘std::string’ is defined i n header ‘<string>’; did you forget to ‘#include <string>’?

--- bowling.cpp
+++ bowling.cpp
@@ -1,3 +1,4 @@
+```c++
 #include <string>
 using namespace std;
 
@@ -5,16 +6,19 @@ int bowlingScore(string s) {
     int score = 0;
     for (int i = 0; i < 10; i++) {
         if (s[i] == 'X') { // Strike
-            score += 10 + getBonus(s.substr(i+1));
+            score += 10;
+            if (i < 9)
+                score += getBonus(s.substr(i+1));
         } else if (s[i] == '/') { // Spare
-            score += 5 + getBonus(s.substr(i+1,2));
+            score += 5;
+            score += getRoll(s.substr(i+1));
         } else { // Normal roll
-            int roll1 = s[i] - '0';
+            int roll = s[i] - '0';
             if (i < 8 && s[i+1] != '/') {
-                int roll2 = s[i+1] - '0';
-                score += roll1 + roll2;
+                int next = s[i+1] - '0';
+                score += roll + next;
             } else {
-                score += roll1;
+                score += roll;
             }
         }
     }
@@ -22,16 +26,23 @@ int bowlingScore(string s) {
     return score;
 }
 
-int getBonus(string s) {
-    int bonus = 0;
-    for (int j = 0; j < s.size(); j++) {
-        if (s[j] == 'X') { // Strike
-            bonus += 10;
-        } else if (s[j] == '/') { // Spare
-            bonus += 5;
+int getRoll(string s) {
+    int score = 0;
+    bool bonus = false;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'X') { // Strike
+            score += 10;
+            bonus = true;
+        } else if (s[i] == '/') { // Spare
+            score += 5;
+            break;
         } else { // Normal roll
-            bonus += s[j] - '0';
+            score += s[i] - '0';
         }
     }
 
-    return bonus;
+    if (bonus)
+        score += getRoll(s.substr(1));
+
+    return score;
+}
