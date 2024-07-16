6d6766986b7ec9c5b6458c54e8d150bd2c93ff26
beep boop <githubbot@vadim.me>
2024-07-16 09:08:29 +0200

    Solution 52 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gskb9992 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 373053fe-c5be-4cec-958b-4d578b24e012.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 373053fe-c5be-4cec-958b-4d578b24e012.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 373053fe-c5be-4cec-958b-4d578b24e012.cpp:5: 373053fe-c5be-4cec-958b-4d578b24e012.cpp: In function ‘int main()’: 373053fe-c5be-4cec-958b-4d578b24e012.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 373053fe-c5be-4cec-958b-4d578b24e012.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,9 +1,9 @@
 vector<int> minPath(vector<vector<int>> grid, int k){
     vector<int> result;
-    for(int i = 0; i < k; ++i){
-        for(auto row : grid){
-            for(int val : row){
-                result.push_back(val);
+    for (int i = 0; i < k / grid.size(); ++i) {
+        for (const auto& row : grid) {
+            for (int num : row) {
+                result.push_back(num);
             }
         }
     }
@@ -14,7 +14,8 @@ bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
+// Test case in main function
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
+    assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
     return 0;
 }
