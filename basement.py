61cd45e4e5602f93467daaa054a921154f42db46
beep boop <githubbot@vadim.me>
2024-07-16 17:01:12 +0200

    Solution 75 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/d8eawbwe Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92e76506-b254-43ea-bd55-4ef787fe9 33b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- basement.py
+++ basement.py
@@ -1,8 +1,11 @@
 def basement(nums):
-    cum_sum = 0
-    i = 0
+    cum_sum = nums[0]
+    i = 1
     while i < len(nums):
+        if cum_sum <= 0:
+            cum_sum += nums[i]
+        else:
+            cum_sum += -nums[i]  
         if cum_sum < 0:
-            return i + 1
-        cum_sum += nums[i]
+            return i + 1        
         i += 1
