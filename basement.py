43cbf08f5147d76182355eb4cc50ecdc14eea12e
beep boop <githubbot@vadim.me>
2024-07-16 16:06:24 +0200

    Solution 24 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/d8eawbwe Prompt: it must return 8 for input 10 45 50 19 7 21 -67 -63 18 -69 -41, but it returns .

--- basement.py
+++ basement.py
@@ -4,4 +4,6 @@ def basement(nums):
         cum_sum += nums[i]
         if cum_sum < 0:
             return i + 1
+        elif cum_sum > 0 and cum_sum == 1:
+            cum_sum = 0
     return len(nums) + 1
