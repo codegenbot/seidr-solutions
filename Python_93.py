7900e01db785cee85d0fca0d81d9cbd4608c15b5
beep boop <githubbot@vadim.me>
2024-07-16 07:58:49 +0200

    Solution 75 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yuz53mar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3855912b-caf6-4cc8-bb44-339e12571 5ed.py", line 3, in <module>     (ord(char) - ord("A") + 2) % 26 + ord("A") if "A" <= char <= "Z" else ord(ch ar)                                                          ^^^^ NameError: name 'char' is not defined. Did you mean: 'chr'?

--- Python_93.py
+++ Python_93.py
@@ -1,2 +1,6 @@
-encoded_message = ""
-encoded_message += chr((ord(char) - ord("A") + 2) % 26 + ord("A") if "A" <= char <= "Z" else ord(char)
+for char in input_string:
+    encoded_message = ''
+    encoded_message += chr(
+        (ord(char) - ord("A") + 2) % 26 + ord("A") if "A" <= char <= "Z" else ord(char)
+    )
+    print(encoded_message)
