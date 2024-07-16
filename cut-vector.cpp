ec01ce227f8e2a30225c904a0fb87c95f87aa05d
beep boop <githubbot@vadim.me>
2024-07-16 08:51:44 +0200

    Solution 60 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/7ssq1x3t Prompt: ared in this scope    14 |         int leftSum = accumulate(nums.begin(), nums.begin() + i, 0);       |                       ^~~~~~~~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:17:27: error: ‘abs’ was not declared in  this scope    17 |         int currentDiff = abs(leftSum - rightSum);       |                           ^~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:25:9: error: ‘cout’ was not declared in  this scope    25 |         cout << nums[i] << endl;       |         ^~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:25:28: error: ‘endl’ was not declared i n this scope    25 |         cout << nums[i] << endl;       |                            ^~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:28:5: error: ‘cout’ was not declared in  this scope    28 |     cout << 0 << endl;       |     ^~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:28:18: error: ‘endl’ was not declared i n this scope    28 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
-#include <cmath>
+#include <numeric> // for std::accumulate
+#include <cstdlib> // for abs
+
+using namespace std;
 
 int main() {
-    std::vector<int> nums;
+    vector<int> nums;
     int num;
     
-    while (std::cin >> num) {
+    while (cin >> num) {
         nums.push_back(num);
     }
     
@@ -16,10 +18,10 @@ int main() {
     int cutIdx = 0;
     
     for (int i = 1; i < n; ++i) {
-        int leftSum = std::accumulate(nums.begin(), nums.begin() + i, 0);
-        int rightSum = std::accumulate(nums.begin() + i, nums.end(), 0);
+        int leftSum = accumulate(nums.begin(), nums.begin() + i, 0);
+        int rightSum = accumulate(nums.begin() + i, nums.end(), 0);
         
-        int currentDiff = std::abs(leftSum - rightSum);
+        int currentDiff = abs(leftSum - rightSum);
         if (currentDiff < diff) {
             diff = currentDiff;
             cutIdx = i;
@@ -27,13 +29,13 @@ int main() {
     }
     
     for (int i = 0; i < cutIdx; ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << endl;
     }
     
-    std::cout << 0 << std::endl;
+    cout << 0 << endl;
     
     for (int i = cutIdx; i < n; ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << endl;
     }
     
     return 0;
