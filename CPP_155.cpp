f6f012bd8de1f3bf1661bde269f541ac5bd42be2
beep boop <githubbot@vadim.me>
2024-07-16 09:24:20 +0200

    Solution 18 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/szzx4ujr Prompt:       |                    ^~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 59c21e69-dee1-437d-86ba-461b55998a18.cpp:13: 59c21e69-dee1-437d-86ba-461b55998a18.cpp: In function ‘int main()’: 59c21e69-dee1-437d-86ba-461b55998a18.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -4,7 +4,7 @@
 
 std::vector<int> even_odd_count(int num) {
     std::vector<int> result(2, 0);
-    std::string numStr = std::to_string(abs(num));
+    std::string numStr = std::to_string(std::abs(num));
     for (char c : numStr) {
         if ((c - '0') % 2 == 0) {
             result[0]++;
@@ -16,11 +16,5 @@ std::vector<int> even_odd_count(int num) {
 }
 
 bool issame(std::vector<int> v1, std::vector<int> v2) {
-    assert(v1.size() == v2.size());
-    for (size_t i = 0; i < v1.size(); ++i) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
+    return v1[0] == v2[0] && v1[1] == v2[1];
 }
