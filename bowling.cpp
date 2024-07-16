f82ea5a334e40243cc260c8bc09422f1f16cdc97
beep boop <githubbot@vadim.me>
2024-07-16 23:19:05 +0200

    Solution 83 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/1x5r1kks Prompt: dc74c369-6919-4f72-b03b-537c1823f5c3.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,24 +2,51 @@
 
 int bowlingScore(string s) {
     int score = 0;
-    int roll = 0;
-    bool spare = false;
-
-    for (int i = 0; i < 10; ++i) {
+    int frame = 1;
+    for (int i = 0; i <= s.size(); ++i) {
         if (s[i] == 'X') {
             score += 10;
-            spare = true;
+            if (frame < 10) {
+                score += bowlingScoreHelper(&s[i+1]);
+            }
         } else if (s[i] == '/') {
             int firstRoll = s[i-1] - '0';
-            roll = (spare ? 10 : max(firstRoll, roll)) + (roll != 0);
-            score += roll;
-            spare = false;
+            int secondRoll = s[i+1] - '0';
+            score += max(firstRoll, secondRoll);
+            frame++;
         } else {
-            int currentRoll = s[i] - '0';
-            if (!spare) {
-                roll = currentRoll;
+            int roll = s[i] - '0';
+            if (frame < 10) {
+                score += roll;
+                if (s[i+1] == '/') {
+                    i++;
+                } else if (i < s.size() && s[i+1] != 'X') {
+                    score += bowlingScoreHelper(&s[i+1]);
+                    frame++;
+                }
+            } else {
+                score += roll;
             }
-            score += currentRoll;
+        }
+    }
+    return score;
+}
+
+int bowlingScoreHelper(string *s) {
+    int score = 0;
+    for (int i = 0; i < s->size(); ++i) {
+        if ((*s)[i] == 'X') {
+            score += 10;
+            if (i < s->size() - 1 && (*s)[i+1] == '/') {
+                score += max((*s)[i-1] - '0', (*s)[i+2] - '0');
+            }
+        } else if ((*s)[i] == '/') {
+            int firstRoll = (*s)[i-1] - '0';
+            int secondRoll = (*s)[i+1] - '0';
+            score += max(firstRoll, secondRoll);
+        } else {
+            int roll = (*s)[i] - '0';
+            score += roll;
         }
     }
     return score;
