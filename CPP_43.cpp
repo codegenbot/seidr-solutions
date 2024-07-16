4f78f743d9ebd8fb849fafca56d22ad21d963f22
beep boop <githubbot@vadim.me>
2024-07-16 05:08:47 +0200

    Solution 20 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/q9xverfm Prompt:     1 | for (size_t i = 0; i < l.size(); ++i) {       | ^~~ 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp:1:20: error: ‘i’ does not name a type     1 | for (size_t i = 0; i < l.size(); ++i) {       |                    ^ 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp:1:34: error: expected unqualified-id be fore ‘++’ token     1 | for (size_t i = 0; i < l.size(); ++i) {       |                                  ^~ 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp:8:5: error: expected unqualified-id bef ore ‘return’     8 |     return false;       |     ^~~~~~ 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp:9:1: error: expected declaration before  ‘}’ token     9 | }       | ^ In file included from 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp:11: 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp: In function ‘int main()’: 0969fc22-3448-4c54-b68c-d6290145dc7b.cpp:13:13: error: ‘pairs_sum_to_zero’ was n ot declared in this scope    13 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ^~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,12 +1,10 @@
-bool pairs_sum_to_zero(vector<int>& l) {
-    unordered_set<int> set;
-
-    for (int num : l) {
-        if (set.count(-num) > 0) {
-            return true;
+bool pairs_sum_to_zero(const std::vector<int>& l) {
+    for (size_t i = 0; i < l.size(); ++i) {
+        for (size_t j = i + 1; j < l.size(); ++j) {
+            if (l[i] + l[j] == 0) {
+                return true;
+            }
         }
-        set.insert(num);
     }
-
     return false;
 }
