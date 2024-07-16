21f43c655de4099e353f954d2bcb5f69b26321f4
beep boop <githubbot@vadim.me>
2024-07-16 03:54:45 +0200

    Solution 38 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/08mwha3c Prompt: bf638df4-96de-4dda-a23c-f9e94eae5c23.cpp:20:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    20 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff bf638df4-96de-4dda-a23c-f9e94eae5c23.cpp:23:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    23 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff bf638df4-96de-4dda-a23c-f9e94eae5c23.cpp:23:36: error: ‘find_zero’ cannot be use d as a function    23 |         solution = find_zero(coeffs);       |                                    ^ In file included from bf638df4-96de-4dda-a23c-f9e94eae5c23.cpp:7: bf638df4-96de-4dda-a23c-f9e94eae5c23.cpp:24:21: error: ‘poly’ was not declared i n this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ bf638df4-96de-4dda-a23c-f9e94eae5c23.cpp:24:17: error: ‘abs’ was not declared in  this scope    24 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -10,16 +10,8 @@ double abs(double x){
     return x >= 0 ? x : -x;
 }
 
-double find_zero(vector<double> xs){
-    double a = xs[0];
-    double b = xs[1];
-    
-    double epsilon = 1e-6;
-    double x = 0.0;
-    
-    while(abs(poly(xs, x)) > epsilon){
-        x -= poly(xs, x) / poly({a}, x);
-    }
-    
-    return x;
+double find_zero(vector<double> coeffs){
+    double a = poly(coeffs, 1.0);
+    double b = poly(coeffs, -1.0);
+    return -b/a;
 }
