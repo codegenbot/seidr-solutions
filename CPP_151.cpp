7b1aacc7f1ee3fee179b8cdbe9a51f05c06932f2
beep boop <githubbot@vadim.me>
2024-07-16 10:34:30 +0200

    Solution 70 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/0zc4o58x Prompt: e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst){       |                                 ^~~~~~ e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst){       |                                        ^~~~~ In file included from e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp:11: e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp: In function ‘int main()’: e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ e21b9598-c51c-42b5-ac4c-bd2afe2713fc.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,19 +1,12 @@
 #include <vector>
-#include <cmath>
+#include <cmath> 
 
-double double_the_difference(std::vector<float> lst){
-    double sum = 0;
-    for(float num : lst){
-        if(num > 0 && std::fmod(num, 1) == 0 && (int)num % 2 != 0){
-            sum += std::pow(num, 2);
+long long double_the_difference(std::vector<float> lst) {
+    long long sum = 0;
+    for (float num : lst) {
+        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
+            sum += (long long)pow(num, 2);
         }
     }
     return sum;
-}
-
-int main(){
-    std::vector<float> lst = {1.0, 2.0, 3.0};
-    double odd_sum = 10.0;
-    assert(double_the_difference(lst) == odd_sum);
-    return 0;
 }
