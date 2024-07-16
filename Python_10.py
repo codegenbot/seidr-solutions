b2fd643b191a870645c99dee13929ca852014818
beep boop <githubbot@vadim.me>
2024-07-16 04:43:13 +0200

    Solution 20 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qg75bv7u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5324eef1-7929-4ba6-ab08-3a866aa0b 819.py", line 11, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5324eef1-7929-4ba6-ab08-3a866aa0b 819.py", line 8, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5324eef1-7929-4ba6-ab08-3a866aa0b 819.py", line 2, in make_palindrome     if is_palindrome(string):        ^^^^^^^^^^^^^ NameError: name 'is_palindrome' is not defined. Did you mean: 'make_palindrome'?

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,7 @@
-def is_palindrome(s: str) -> bool:
-    return s == s[::-1]
-
 def make_palindrome(string: str) -> str:
+    def is_palindrome(s: str) -> bool:
+        return s == s[::-1]
+
     if is_palindrome(string):
         return string
     for i in range(len(string), 0, -1):
