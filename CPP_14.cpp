283edbb2649b4229c6923649e4ac290857aadef3
beep boop <githubbot@vadim.me>
2024-07-16 04:13:30 +0200

    Solution 52 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/cr3bh6us Prompt:       |                              ^~~~~~ 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp:11: 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp: In function ‘int main()’: 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp:21:20: error: ‘all_prefixes’ was not de clared in this scope    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 4e852976-37b2-40ac-a3bc-eafcd9036f4c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,8 +1,6 @@
 #include <vector>
 #include <string>
 
-bool issame(const vector<string>& a, const vector<string>& b);
-
 vector<string> all_prefixes(string str){
     vector<string> prefixes;
     string prefix = "";
@@ -14,13 +12,5 @@ vector<string> all_prefixes(string str){
 }
 
 bool issame(const vector<string>& a, const vector<string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
