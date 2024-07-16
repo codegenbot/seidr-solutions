053361c5e1bc7d5f5fec52d631c9881999df8edd
beep boop <githubbot@vadim.me>
2024-07-16 17:33:32 +0200

    Solution 57 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/z88i02r3 Prompt: Complete the following code given the task description and function signature.

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,4 +1,4 @@
-if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
+if (a * a + b * b == c * c || b * b + c * c == a * a || c * c + a * a == b * b) {
         return true;
     }
     return false;
