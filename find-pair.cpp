5efabc8f3da4f98e8a0df0e0e43adc5dd7ac0864
beep boop <githubbot@vadim.me>
2024-07-16 16:45:05 +0200

    Solution 4 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/aut863mg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from ee40b450-0c91-4724-a48e-13f3058f74ba.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,8 +1,8 @@
 ```cpp
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
 #include <vector>
 #include <unordered_map>
 #include <iostream>
-
 using namespace std;
 
 pair<int, int> findPair(vector<int>& nums, int target) {
