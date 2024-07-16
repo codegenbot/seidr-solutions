c73550ae1d2d3faa973322ff92a524e1b6b106ba
beep boop <githubbot@vadim.me>
2024-07-16 09:00:44 +0200

    Solution 34 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/doitzeus Prompt:       |                    ^~~ e01e0009-9736-48d4-b627-14369c8b7e6c.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e01e0009-9736-48d4-b627-14369c8b7e6c.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e01e0009-9736-48d4-b627-14369c8b7e6c.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e01e0009-9736-48d4-b627-14369c8b7e6c.cpp:11: e01e0009-9736-48d4-b627-14369c8b7e6c.cpp: In function ‘int main()’: e01e0009-9736-48d4-b627-14369c8b7e6c.cpp:21:20: error: ‘generate_integers’ was n ot declared in this scope    21 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ e01e0009-9736-48d4-b627-14369c8b7e6c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <cassert>
 
+vector<int> generate_integers(int a, int b);
+
 bool issame(vector<int> a, vector<int> b);
 
 vector<int> generate_integers(int a, int b){
@@ -14,20 +16,10 @@ vector<int> generate_integers(int a, int b){
 }
 
 bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    
-    for(size_t i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    
-    return true;
+    return a == b;
 }
 
 int main() {
-    // Test the functions if needed
+    // Test code here
     return 0;
 }
