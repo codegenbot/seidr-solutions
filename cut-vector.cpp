d2e7f884710e560bb3c0de3bbe530043f38c2761
beep boop <githubbot@vadim.me>
2024-07-16 09:45:57 +0200

    Solution 36 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/0gzzutdb Prompt: a6ad332a-55a1-44e0-a41e-b65119f48d81.cpp: In function ‘std::pair<std::vector<int >, std::vector<int> > cutVector(std::vector<int>)’: a6ad332a-55a1-44e0-a41e-b65119f48d81.cpp:7:19: error: ‘INT_MAX’ was not declared  in this scope     7 |     int minDiff = INT_MAX;       |                   ^~~~~~~ a6ad332a-55a1-44e0-a41e-b65119f48d81.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 |

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,53 +1,44 @@
-#include <vector>
+```
 #include <iostream>
+#include <vector>
+#include <climits>
+
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
-    int cut_index = -1;
-
-    for (int i = 0; i < v.size(); ++i) {
-        int left_sum = 0, right_sum = 0;
-        for (int j = 0; j < i; ++j) {
-            left_sum += v[j];
-        }
-        for (int j = i; j < v.size(); ++j) {
-            right_sum += v[j];
-        }
-
-        int diff = abs(left_sum - right_sum);
-        if (diff <= min_diff) {
-            min_diff = diff;
-            cut_index = i;
+    int minDiff = INT_MAX;
+    int splitIndex = 0;
+    
+    for (int i = 1; i < v.size(); i++) {
+        int diff = abs(v[i] - v[0]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            splitIndex = i;
         }
     }
-
-    vector<int> left(v.begin(), v.begin() + cut_index);
-    vector<int> right(v.begin() + cut_index, v.end());
+    
+    vector<int> left(v.begin(), v.begin() + splitIndex);
+    vector<int> right(v.begin() + splitIndex, v.end());
+    
     return {left, right};
 }
 
 int main() {
     int n;
     cin >> n;
-
     vector<int> v(n);
-
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-
     pair<vector<int>, vector<int>> result = cutVector(v);
-    
-    cout << "Left: ";
-    for (auto x : result.first) {
-        cout << x << " ";
+    cout << "[";
+    for (int num : result.first) {
+        cout << num << " ";
     }
-    cout << endl;
-
-    cout << "Right: ";
-    for (auto x : result.second) {
-        cout << x << " ";
+    cout << "] [";
+    for (int num : result.second) {
+        cout << num << " ";
     }
-    cout << endl;
+    cout << "]" << endl;
+    return 0;
 }
