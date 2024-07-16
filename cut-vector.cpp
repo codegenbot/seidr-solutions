a55a36283170462488eea45483c453037424e26a
beep boop <githubbot@vadim.me>
2024-07-16 09:37:09 +0200

    Solution 70 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/7ssq1x3t Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { 69cd1212-976f-42b8-b308-77379558bfd0.cpp:15:20: error: ‘abs’ was not declared in  this scope    15 |         int diff = abs(leftSum - rightSum);       |                    ^~~ 69cd1212-976f-42b8-b308-77379558bfd0.cpp:22:9: error: ‘cout’ was not declared in  this scope    22 |         cout << nums[i] << endl;       |         ^~~~ 69cd1212-976f-42b8-b308-77379558bfd0.cpp:22:28: error: ‘endl’ was not declared i n this scope    22 |         cout << nums[i] << endl;       |                            ^~~~ 69cd1212-976f-42b8-b308-77379558bfd0.cpp:24:5: error: ‘cout’ was not declared in  this scope    24 |     cout << 0 << endl;       |     ^~~~ 69cd1212-976f-42b8-b308-77379558bfd0.cpp:24:18: error: ‘endl’ was not declared i n this scope    24 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,42 +1,33 @@
 #include <iostream>
+#include <climits>
 #include <vector>
-#include <numeric> // for std::accumulate
-#include <cstdlib> // for abs
+#include <numeric>
 
 using namespace std;
 
 int main() {
     vector<int> nums;
-    int num;
-    
-    while (cin >> num) {
-        nums.push_back(num);
+    int n;
+    while (cin >> n) {
+        nums.push_back(n);
     }
-    
-    int n = nums.size();
-    int diff = INT_MAX;
-    int cutIdx = 0;
-    
-    for (int i = 1; i < n; ++i) {
-        int leftSum = accumulate(nums.begin(), nums.begin() + i, 0);
-        int rightSum = accumulate(nums.begin() + i, nums.end(), 0);
-        
-        int currentDiff = abs(leftSum - rightSum);
-        if (currentDiff < diff) {
-            diff = currentDiff;
-            cutIdx = i;
+    int totalSum = accumulate(nums.begin(), nums.end(), 0);
+    int leftSum = 0;
+    int rightSum = totalSum;
+    int minDiff = INT_MAX;
+    int cutIndex = -1;
+    for (int i = 0; i < nums.size(); ++i) {
+        leftSum += nums[i];
+        rightSum -= nums[i];
+        int diff = std::abs(leftSum - rightSum);
+        if (diff < minDiff) {
+            minDiff = diff;
+            cutIndex = i;
         }
     }
-    
-    for (int i = 0; i < cutIdx; ++i) {
+    for (int i = 0; i <= cutIndex; ++i) {
         cout << nums[i] << endl;
     }
-    
     cout << 0 << endl;
-    
-    for (int i = cutIdx; i < n; ++i) {
-        cout << nums[i] << endl;
-    }
-    
     return 0;
 }
