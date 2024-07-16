67798a830b394e0db90be01fdadc395484b6dbad
beep boop <githubbot@vadim.me>
2024-07-16 15:42:41 +0200

    Solution 71 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/j2sjknbs Prompt: Complete the following code given the task description and function signature.

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -5,13 +5,11 @@ vector<int> result;
     if(n == 1) return result;
     result.push_back(2);
     if(n == 2) return result;
-    
-    for(int i = 3; i <= n; ++i){
-        if(i % 2 == 0){
+    for(int i = 3; i <= n; i++){
+        if(i % 2 == 0)
             result.push_back(1 + i / 2);
-        } else {
+        else
             result.push_back(result[i - 1] + result[i - 2] + result[i - 3]);
-        }
     }
     return result;
 }
