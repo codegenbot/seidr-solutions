97755657bc28c629ac14f34d0df3521566cfbf4c
beep boop <githubbot@vadim.me>
2024-07-16 06:05:18 +0200

    Solution 78 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vxc8xfvh Prompt:       |                    ^~~ 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp:11: 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp: In function ‘int main()’: 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp:21:20: error: ‘common’ was not declared  in this scope    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 3bd00cb8-fd39-47f7-9a2f-c1b1c733ad78.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,10 +1,19 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> common(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
 
+    std::vector<int> result;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
+
+    result.erase(std::unique(result.begin(), result.end()), result.end());
+
+    return !result.empty();
+}
+
+std::vector<int> common(std::vector<int> a, std::vector<int> b) {
     std::vector<int> result;
     std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
 
