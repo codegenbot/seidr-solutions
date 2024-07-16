02f7ae4727322e0649e0875b5668bfe1a28e20d1
beep boop <githubbot@vadim.me>
2024-07-16 17:54:55 +0200

    Solution 40 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3v8snoyk Prompt: 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp:34:10: error: ‘int main()’ redeclared a s different kind of entity    34 | int main(){       |          ^ 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp:18:5: note: previous declaration ‘int m ain’    18 | int main {       |     ^~~~ In file included from 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp:25: 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp: In function ‘int main()’: 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp:35:35: error: too few arguments to func tion ‘bool order_by_points(std::vector<int>, std::vector<int>)’    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 9d394a32-780f-49d6-8e97-d2c1bf67a84a.cpp:14:6: note: declared here    14 | bool order_by_points(std::vector<int> a, std::vector<int> b) {       |      ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -18,6 +18,6 @@ bool order_by_points(std::vector<int> a, std::vector<int> b) {
 int main() {
     assert(issame(sort_vector({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
     assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}, std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
-
+    
     return 0;
 }
