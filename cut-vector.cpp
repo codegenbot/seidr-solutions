8358b56fa8390e8239a3106e9d83093fc5b6ac4b
beep boop <githubbot@vadim.me>
2024-07-16 07:16:44 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/zk87tlmx Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,42 @@
 int main() {
     vector<int> nums;
     int num;
+    
     while (cin >> num) {
         nums.push_back(num);
     }
-
+    
+    int n = nums.size();
+    int sum = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        sum += nums[i];
+    }
+    
+    int prefix_sum = 0;
     int min_diff = INT_MAX;
     int cut_index = -1;
-
-    for (int i = 1; i < nums.size(); ++i) {
-        int left_sum = 0;
-        int right_sum = 0;
-
-        for (int j = 0; j < i; ++j) {
-            left_sum += nums[j];
-        }
-
-        for (int j = i; j < nums.size(); ++j) {
-            right_sum += nums[j];
-        }
-
-        int diff = abs(left_sum - right_sum);
-        if (diff < min_diff || (diff == min_diff && i < cut_index)) {
+    
+    for (int i = 0; i < n; ++i) {
+        prefix_sum += nums[i];
+        int diff = abs(prefix_sum - (sum - prefix_sum));
+        
+        if (diff < min_diff) {
             min_diff = diff;
             cut_index = i;
         }
     }
-
-    for (int i = 0; i < cut_index; ++i) {
-        cout << nums[i] << endl;
+    
+    vector<int> subvector1(nums.begin(), nums.begin() + cut_index + 1);
+    vector<int> subvector2(nums.begin() + cut_index + 1, nums.end());
+    
+    for (int num : subvector1) {
+        cout << num << endl;
     }
-    cout << endl;
-    for (int i = cut_index; i < nums.size(); ++i) {
-        cout << nums[i] << endl;
+    
+    for (int num : subvector2) {
+        cout << num << endl;
     }
-    cout << endl;
-
+    
     return 0;
 }
