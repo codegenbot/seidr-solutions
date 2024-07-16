45e542b3e82d05a91955d0dc98597dc13da706c2
beep boop <githubbot@vadim.me>
2024-07-16 14:23:11 +0200

    Solution 39 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/eumscrgm Prompt: fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp: In function ‘int main()’: fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp:2:5: error: ‘string’ was not declared i n this scope     2 |     string spinWords(string str) {       |     ^~~~~~ fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << spinWords("This is a test");       |     ^~~~ fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp:21:13: error: ‘spinWords’ was not decla red in this scope    21 |     cout << spinWords("This is a test");       |             ^~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -7,17 +7,17 @@ int main() {
         int i = 0;
         while (i < str.length()) {
             if (str.find(" ", i) == -1 || i + 1 >= str.length()) {
-                if (str.substr(i).length() >= 5) {
-                    for (int k = str.substr(i).length() - 1; k >= 0; --k)
+                if(str.substr(i).length() >= 5)
+                    for(int k = str.substr(i).length() - 1; k >= 0; --k)
                         result += str.substr(i)[k];
-                    } else
-                        result += str.substr(i);
+                else
+                    result += str.substr(i);
                 break;
             }
             int j = str.find(" ", i);
             string word = str.substr(i, j - i);
             if (word.length() >= 5) {
-                for (int k = word.length() - 1; k >= 0; --k)
+                for(int k = word.length() - 1; k >= 0; --k)
                     result += word[k];
             } else
                 result += word;
