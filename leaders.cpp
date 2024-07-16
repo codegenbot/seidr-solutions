af0c4c2525d247f09b97e519abb297e141a3fde7
beep boop <githubbot@vadim.me>
2024-07-16 11:30:56 +0200

    Solution 2 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/j96pttdu Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- leaders.cpp
+++ leaders.cpp
@@ -1,22 +1,29 @@
 #include <vector>
 using namespace std;
 
-vector<int> leaders(vector<int>& arr) {
+vector<int> leaders(vector<int> arr) {
     int n = arr.size();
-    vector<int> leaders;
+    vector<int> res;
     
-    for (int i = n - 1; i >= 0; i--) {
-        bool leader = true;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] >= arr[i]) {
-                leader = false;
-                break;
-            }
-        }
-        if (leader) {
-            leaders.push_back(arr[i]);
+    if(n == 0)
+        return res;
+    
+    res.push_back(arr[n-1]);
+    
+    for(int i=n-2; i>=0; i--){
+        if(arr[i] > res.back()){
+            res.push_back(arr[i]);
         }
     }
     
-    return leaders;
+    return res;
+}
+
+int main() {
+    vector<int> arr = {16, 17, 4, 3, 5, 2};
+    vector<int> leadersVec = leaders(arr);
+    for(int i:leadersVec){
+        cout << i << " ";
+    }
+    return 0;
 }
