f82144421ce895d3b673c6ab77c5fdbbc47e892a
beep boop <githubbot@vadim.me>
2024-07-16 05:30:45 +0200

    Solution 19 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/j45q1oes Prompt:       |                    ^~~ 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp:9: 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp: In function ‘int main()’: 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 6336c4e1-1a9e-4ada-a2ba-40694a958f36.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,18 +7,10 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 vector<int> common(const vector<int>& l1, const vector<int>& l2) {
+    vector<int> result;
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
-    vector<int> result;
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
-}
-
-int main() {
-    vector<int> l1 = {1, 2, 3, 4, 5};
-    vector<int> l2 = {3, 4, 5, 6, 7};
-    vector<int> expectedResult = {3, 4, 5};
-    vector<int> actualResult = common(l1, l2);
-    assert(issame(expectedResult, actualResult));
 }
