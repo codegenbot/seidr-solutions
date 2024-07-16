a13005d15a8e43c4dd6fab1d3d2e6771707c6250
beep boop <githubbot@vadim.me>
2024-07-16 11:44:35 +0200

    Solution 61 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uj26qelu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9041989c-4dcf-436d-83fc-245ed9d1d 331.py", line 10, in <module>     check(prime_fib)           ^^^^^^^^^ NameError: name 'prime_fib' is not defined

--- Python_39.py
+++ Python_39.py
@@ -1,1 +1,13 @@
+def prime_fib(n):
+    if n <= 1:
+        return "Not Prime"
+    elif n == 2:
+        return "Prime"
+    else:
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return "Not Prime"
+        return "Prime"
+
 n = int(input())
+print(prime_fib(n))
