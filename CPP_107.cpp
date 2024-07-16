10d38ae1a78e6fbc7d61b984a226b4f2f3a95b42
beep boop <githubbot@vadim.me>
2024-07-16 07:01:42 +0200

    Solution 46 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/z65p3l1k Prompt:       |                    ^~~ 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp:18: 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp: In function ‘int main()’: 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 53a29fd4-50f0-4e64-a9b1-81d943640527.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    return a[0] == b[0] && a[1] == b[1];
+    return (a[0] == b[0] && a[1] == b[1]);
 }
 
 vector<int> count_palindromes(int n) {
@@ -24,7 +24,14 @@ vector<int> count_palindromes(int n) {
 }
 
 int main() {
-    vector<int> even_odd_palindrome(int n);
-    // Code to use the functions
+    vector<int> input = count_palindromes(100);
+    vector<int> expected_output = {9, 10};
+
+    if (issame(input, expected_output)) {
+        cout << "Test Passed" << endl;
+    } else {
+        cout << "Test Failed" << endl;
+    }
+
     return 0;
 }
