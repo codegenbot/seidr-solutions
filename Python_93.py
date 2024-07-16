8c08f6cb3dca36117f7efa1c9c55d27ad2f101a3
beep boop <githubbot@vadim.me>
2024-07-16 03:37:00 +0200

    Solution 54 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c587c6ea-512f-46d7-abd3-96cc75d00 e37.py", line 19, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c587c6ea-512f-46d7-abd3-96cc75d00 e37.py", line 15, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,10 @@
 def encode(message):
     result = ""
     for char in message:
-        if char.isspace():
-            result += char  
-        elif char.isalnum():  
+        if char.isalnum():
             ascii_offset = 65 if not char.islower() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
             result += chr(shifted_char_code)
-        else: 
-            result += char  # handle non-alphanumeric characters correctly
+        else:
+            result += char
     return result
