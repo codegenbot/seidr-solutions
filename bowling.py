06f3cd25b966cdd51c433e5d97e147e99fd56bd1
beep boop <githubbot@vadim.me>
2024-07-16 11:36:53 +0200

    Solution 46 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/21dc2h6k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3aeb999-3e8f-4142-a4e2-3010241e8 06c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -3,37 +3,37 @@ def bowling_game(score):
     score = score.replace('/', '')
     total_score = 0
     frame = 1
-    i = 0
     while len(score) > 0:
-        if score[i].isdigit():
-            first_roll = int(score[i])
-            i += 1
-            if score[i] == 'X' or str(int(score[i])) + score[i+1] in ['10', '20']:
-                if frame < 9:
-                    total_score += first_roll + 10
-                else:
-                    total_score += first_roll + int(score[i+1]) + int(score[i+2])
-                frame += 1
-                i += 1
-            elif len(score) > 0 and score[i].isdigit():
-                if frame < 9:
-                    total_score += first_roll + int(score[i])
-                else:
-                    total_score += first_roll + int(score[i]) + int(score[i+1])
-                frame += 1
-                i += 2
-            else:
-                total_score += first_roll
-                frame += 1
-        elif score[i] == 'X':
-            total_score += 10 + 10
-            frame += 2
-            i += 1
-        else:
-            if len(score) > 0 and score[i].isdigit():
-                total_score += 10 + int(score[i])
+        if score[0] == 'X':
+            total_score += 30
+            if len(score) >= 2 and score[:2] in ['XX', 'SX']:
+                total_score += 10
+            frame += 1
+            score = score[1:]
+        elif score[0].isdigit():
+            first_roll = int(score[0])
+            score = score[1:]
+            if len(score) > 0 and (score[0] == '/' or score[0] == 'X'):
+                total_score += first_roll + 10
                 frame += 2
-                i += 2
+                score = score[1:]
+            elif len(score) > 0 and int(score[0]) == 10:
+                total_score += 10 + first_roll
+                frame += 2
+                score = score[1:]
+            else:
+                if len(score) >= 2 and str(int(score[0]) + int(score[1])) in ['11', '12', '13', '14', '15', '16']:
+                    total_score += 10 + first_roll
+                    frame += 2
+                    score = score[2:]
+                else:
+                    total_score += first_roll + int(score[0])
+                    frame += 2
+        else:
+            if len(score) >= 2 and str(int(score[0]) + int(score[1])) == '10':
+                total_score += 10
+                frame += 2
+                score = score[2:]
             else:
                 total_score += 10
                 frame += 1
