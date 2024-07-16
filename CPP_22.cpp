67d0ca026437b73feb7a7b117f88352828d9bdd2
beep boop <githubbot@vadim.me>
2024-07-16 04:04:47 +0200

    Solution 43 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niur2qrc Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bf879b00-f812-43f2-9901-ce1464fc7630.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf879b00-f812-43f2-9901-ce1464fc7630.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bf879b00-f812-43f2-9901-ce1464fc7630.cpp:11: bf879b00-f812-43f2-9901-ce1464fc7630.cpp: In function ‘int main()’: bf879b00-f812-43f2-9901-ce1464fc7630.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ bf879b00-f812-43f2-9901-ce1464fc7630.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,30 +1,15 @@
-#include <vector>
-#include <list>
-#include <boost/any.hpp>
-#include <typeinfo>
-
-bool issame(vector<int> a, vector<int> b);
-
-std::vector<int> filter_integers(std::list<boost::any> values){
-    std::vector<int> result;
+vector<int> filter_integers(list<any> values){
+    vector<int> result;
     for(auto& val : values){
         if(typeid(int) == val.type()){
-            result.push_back(boost::any_cast<int>(val));
+            result.push_back(any_cast<int>(val));
         }
     }
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(size_t i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<int> a, vector<int> b){
+    return (a == b);
 }
 
 int main(){
