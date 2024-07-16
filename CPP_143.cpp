0ed0e9fb1f0369cc8cdc5b8a5587cc0684c6e2de
beep boop <githubbot@vadim.me>
2024-07-16 09:52:36 +0200

    Solution 54 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/obh9hq0y Prompt: 1c039896-5b69-4dc0-b10f-daee55a804b1.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence){       | ^~~~~~ In file included from 1c039896-5b69-4dc0-b10f-daee55a804b1.cpp:20: 1c039896-5b69-4dc0-b10f-daee55a804b1.cpp: In function ‘int main()’: 1c039896-5b69-4dc0-b10f-daee55a804b1.cpp:22:13: error: ‘words_in_sentence’ was n ot declared in this scope    22 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -5,7 +5,7 @@ bool is_prime(int n){
     if(n <= 1){
         return false;
     }
-    for(int i = 2; i * i <= n; i++){
+    for(int i = 2; i*i <= n; i++){
         if(n % i == 0){
             return false;
         }
