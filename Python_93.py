ef981e7e83e7625cd2aee14ebf95eba950887aec
beep boop <githubbot@vadim.me>
2024-07-16 07:56:16 +0200

    Solution 73 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yuz53mar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4398e7d-43ab-4aa6-b676-94ae45e9d bb7.py", line 1, in <module>     encoded_message += chr(     ^^^^^^^^^^^^^^^ NameError: name 'encoded_message' is not defined

--- Python_93.py
+++ Python_93.py
@@ -1,17 +1,4 @@
-def encode(message):
-    encoded_message = ""
-    for char in message:
-        if char.isalpha():
-            if char.islower():
-                encoded_message += chr((ord(char) - ord("a") + 2) % 26 + ord("a"))
-            else:
-                encoded_message += chr(
-                    (ord(char) - ord("A") + 2) % 26 + ord("A")
-                    if ord(char) >= ord("A") and ord(char) <= ord("Z")
-                    else ord(char)
-                )
-        elif char.isspace():
-            encoded_message += " "
-        else:
-            encoded_message += char
-    return encoded_message.lower()
+encoded_message = ''
+encoded_message += chr(
+    (ord(char) - ord("A") + 2) % 26 + ord("A") if "A" <= char <= "Z" else ord(char)
+)
