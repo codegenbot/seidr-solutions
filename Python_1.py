5c24fd0cd9bae4df1259d2eef660445477728f20
beep boop <githubbot@vadim.me>
2024-07-16 18:27:18 +0200

    Solution 89 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/qwgu36s6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae7632ce-0a12-4f87-b1ae-feb86d0a0 619.py", line 27, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae7632ce-0a12-4f87-b1ae-feb86d0a0 619.py", line 24, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -6,12 +6,14 @@ def separate_paren_groups(paren_string):
     for char in paren_string:
         if char == '(':
             stack.append('(')
+            if len(stack) > 1:
+                current_group.append(char)
         elif char == ')':
             stack.pop()
             if len(stack) == 0 and current_group:
                 groups.append(''.join(current_group) + char)
                 current_group = []
-        elif len(stack) > 0:
+        elif stack != []:
             current_group.append(char)
 
     if current_group:
