1cc3638600a68b8e877c1db580d60af5699baa6d
beep boop <githubbot@vadim.me>
2024-07-16 18:30:38 +0200

    Solution 59 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/r6nd93dw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b7317eb-8d42-49df-b1c1-0186951e1 692.py", line 1     ```     ^ SyntaxError: invalid syntax

--- indices-of-substring.py
+++ indices-of-substring.py
@@ -1,8 +1,10 @@
+```
 def indices_of_substring(text, target):
     result = []
     for i in range(len(text)):
         pos = i
-        while pos <= len(text) - len(target) and text[pos:pos+len(target)].lower() == target.lower():
-            result.append(pos)
+        while pos <= len(text) - len(target):
+            if text[pos:pos+len(target)].lower() == target.lower():
+                result.append(pos)
             pos += 1
     return result
