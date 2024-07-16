6f677568ba2c43a0c904dc59c4736c0c61fb8587
beep boop <githubbot@vadim.me>
2024-07-16 09:46:19 +0200

    Solution 12 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/eumscrgm Prompt: bc920538-7662-4ab2-8a7c-77b297b385d9.cpp: In function ‘std::string spinWords(std ::string)’: bc920538-7662-4ab2-8a7c-77b297b385d9.cpp:16:17: error: ‘reverse’ was not declare d in this scope    16 |                 reverse(word.begin(), word.end());       |                 ^~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,29 +1,30 @@
-```c++
-#include <string>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int i = 0;
-    while (i < str.length()) {
-        if (str.find(" ", i) == -1 || i + 1 >= str.length()) {
-            result += str.substr(i);
-            break;
+    int wordStart = 0;
+    
+    for(int i=0; i<=str.length(); i++) {
+        if(i == str.length() || str[i] == ' ') {
+            string word = str.substr(wordStart, i-wordStart);
+            
+            if(word.length() >= 5) {
+                reverse(word.begin(), word.end());
+            }
+            
+            result += word + " ";
+            wordStart = i+1;
         }
-        int j = str.find(" ", i);
-        string word = str.substr(i, j - i);
-        if (word.length() >= 5) {
-            for (int k = word.length() - 1; k >= 0; --k)
-                result += word[k];
-        } else
-            result += word;
-        i = j + 1;
     }
+    
     return result;
 }
 
 int main() {
-    return spinWords("This is a test string");
-}
+    cout << spinWords("a") << endl; 
+    cout << spinWords("this is a test") << endl; 
+    cout << spinWords("this is another test") << endl; 
+    cout << spinWords("hi") << endl; 
+    return 0;
