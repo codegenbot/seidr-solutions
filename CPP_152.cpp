93db3b0fcca7cb1ab38b1b137d21b728b9ccb2b7
beep boop <githubbot@vadim.me>
2024-07-16 03:34:38 +0200

    Solution 53 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8hc54835 Prompt: bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp:35: bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp: In function ‘int main()’: bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp:45:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    45 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp:16:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    16 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ bc6cbdfb-28bb-4b74-885d-19d9627a0f8d.cpp:36:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    36 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 vector<int> compare(const vector<int>& a, const vector<int>& b) {
     vector<int> result;
-    for(size_t i = 0; i < a.size(); ++i) {
+    for(size_t i = 0; i < min(a.size(), b.size()); ++i) {
         result.push_back(abs(a[i] - b[i]));
     }
     return result;
@@ -17,17 +17,8 @@ bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
-int contest_main() {
-    vector<int> result = compare({1, 2, 3, 5}, {-1, 2, 3, 4});
-    for(int num : result) {
-        cout << num << " ";
-    }
-    
-    return 0;
-}
-
 int main() {
-    assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), {2, 0, 0, 1}));
+    assert(issame(compare({1,2,3,5},{-1,2,3,4}), {2,0,0,1}));
     
     return 0;
 }
