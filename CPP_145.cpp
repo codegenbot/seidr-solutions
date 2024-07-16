d2501412c4146fd6a83d2da19a30119913358ea5
beep boop <githubbot@vadim.me>
2024-07-16 09:49:16 +0200

    Solution 62 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/osarpqwd Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c7e0e413-16e5-4ed5-b1cd-1f57ac609677.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c7e0e413-16e5-4ed5-b1cd-1f57ac609677.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c7e0e413-16e5-4ed5-b1cd-1f57ac609677.cpp:19: c7e0e413-16e5-4ed5-b1cd-1f57ac609677.cpp: In function ‘int main()’: c7e0e413-16e5-4ed5-b1cd-1f57ac609677.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ c7e0e413-16e5-4ed5-b1cd-1f57ac609677.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,4 +1,4 @@
-vector<int> issame(vector<int> a) {
+vector<int> issame(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end(), [](int a, int b) {
         int sumA = 0, sumB = 0;
         int tempA = abs(a), tempB = abs(b);
@@ -16,4 +16,12 @@ vector<int> issame(vector<int> a) {
         return sumA < sumB;
     });
     return a;
+}
+
+int main() {
+    vector<int> result = issame({0, 6, 6, -76, -21, 23, 4}, {-76, -21, 0, 4, 23, 6, 6});
+    for (int num : result) {
+        cout << num << " ";
+    }
+    return 0;
 }
