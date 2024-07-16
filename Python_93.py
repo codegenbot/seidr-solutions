158cc1448a107e585903595518d9b17c33263da7
beep boop <githubbot@vadim.me>
2024-07-16 03:54:20 +0200

    Solution 71 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3d88784-3803-4cfa-ac73-5194dc50f 26f.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3d88784-3803-4cfa-ac73-5194dc50f 26f.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,4 +1,3 @@
-```
 def encode(message):
     result = ""
     for char in message:
@@ -10,4 +9,8 @@ def encode(message):
             ascii_offset = 65 if char.isupper() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
             result += chr(shifted_char_code)
+            if ord('A') <= ascii_offset and char.isupper():
+                result += 'r'
+            elif ord('a') <= ascii_offset and char.islower():
+                result += 'r'
     return result
