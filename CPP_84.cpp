0d8fd1e020b90fa69bf510c6541f28dbb678612d
beep boop <githubbot@vadim.me>
2024-07-16 06:25:00 +0200

    Solution 70 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/p2rfpoj0 Prompt: ce7ea20f-e6e1-41ea-b917-94c39d0c5012.cpp:2:5: error: expected unqualified-id bef ore ‘while’     2 |     while (N > 0) {       |     ^~~~~ ce7ea20f-e6e1-41ea-b917-94c39d0c5012.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return bitset<8>(sum).to_string().substr(8 - __builtin_clz(sum));       |     ^~~~~~ In file included from ce7ea20f-e6e1-41ea-b917-94c39d0c5012.cpp:8: ce7ea20f-e6e1-41ea-b917-94c39d0c5012.cpp: In function ‘int main()’: ce7ea20f-e6e1-41ea-b917-94c39d0c5012.cpp:10:13: error: ‘solve’ was not declared in this scope    10 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <bitset>
-#include <cassert>
 
 std::string solve(int N) {
     int sum = 0;
-    while(N > 0){
+    while (N > 0) {
         sum += N % 2;
         N /= 2;
     }
-    return bitset<32>(sum).to_string().substr(32 - __builtin_clz(sum));
+    return bitset<8>(sum).to_string().substr(8 - __builtin_clz(sum));
 }
 
 int main() {
     assert(solve(963) == "10010");
-
     return 0;
 }
