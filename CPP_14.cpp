36b5440400a0fed4932cbca2f5d5820b45b5c3eb
beep boop <githubbot@vadim.me>
2024-07-16 03:36:07 +0200

    Solution 21 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/cr3bh6us Prompt:       |                              ^~~~~~ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:9: e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp: In function ‘int main()’: e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -5,7 +5,7 @@ bool issame(const vector<string>& vec1, const vector<string>& vec2) {
     if (vec1.size() != vec2.size()) {
         return false;
     }
-    for (size_t i = 0; i < vec1.size(); i++) {
+    for (int i = 0; i < vec1.size(); i++) {
         if (vec1[i] != vec2[i]) {
             return false;
         }
@@ -16,15 +16,17 @@ bool issame(const vector<string>& vec1, const vector<string>& vec2) {
 vector<string> all_prefixes(string str);
 
 int main() {
-    string input;
-    cin >> input;
-    vector<string> prefixes = all_prefixes(input);
-    vector<string> expected = {"a", "ab", "abc", "abcd"};
-    if (issame(prefixes, expected)) {
-        cout << "Prefixes match expected values";
-    } else {
-        cout << "Prefixes do not match expected values";
-    }
+    string input_str;
+    cin >> input_str;
+    
+    vector<string> prefixes = all_prefixes(input_str);
+    
+    // Test issame function
+    vector<string> test_vec = {"abc", "def", "ghi"};
+    bool result = issame(test_vec, prefixes);
+    
+    cout << "Are the vectors the same? " << (result ? "Yes" : "No") << endl;
+    
     return 0;
 }
 
