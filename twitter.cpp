0414a73facf0085a43bf74d360c0bfdf34058eda
beep boop <githubbot@vadim.me>
2024-07-17 00:59:15 +0200

    Solution 53 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8x9ggrs8 Prompt:  does not name a type     1 | std::string tweet;       |      ^~~~~~ c667d53e-31be-47fc-ab48-22fb85c87c42.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string tweet; c667d53e-31be-47fc-ab48-22fb85c87c42.cpp:2:6: error: ‘cin’ in namespace ‘std’ do es not name a type     2 | std::cin >> tweet;       |      ^~~ c667d53e-31be-47fc-ab48-22fb85c87c42.cpp:1:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?   +++ |+#include <iostream>     1 | std::string tweet; c667d53e-31be-47fc-ab48-22fb85c87c42.cpp:4:1: error: expected unqualified-id bef ore ‘if’     4 | if (!tweet.empty()) {       | ^~ c667d53e-31be-47fc-ab48-22fb85c87c42.cpp:11:3: error: expected unqualified-id be fore ‘else’    11 | } else {       |   ^~~~

--- twitter.cpp
+++ twitter.cpp
@@ -2,22 +2,18 @@
 #include <iostream>
 #include <string>
 
-std::string validateTweet(const std::string& tweet) {
-    if (tweet.empty()) {
-        return "You didn't type anything";
-    } 
-    if (tweet.length() > 140) {
-        return "Too many characters";
-    } else {
-        std::string result = "Your tweet has " + std::to_string(tweet.length()) + " characters";
-        return result;
-    }
-}
-
 int main() {
     std::string tweet;
-    std::cout << "Enter your tweet: ";
-    std::getline(std::cin, tweet);
-    std::cout << validateTweet(tweet) << std::endl;
-    return 0;
+    std::cin >> tweet;
+
+    if (!tweet.empty()) {
+        if (tweet.length() > 140) {
+            return "Too many characters";
+        } else {
+            std::string result = "Your tweet has " + std::to_string(tweet.length()) + " characters";
+            return result;
+        }
+    } else {
+        return "You didn't type anything";
+    }
 }
