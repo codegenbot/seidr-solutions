99fc038a69e6f2db16c9cb2090e99e114376e5ca
beep boop <githubbot@vadim.me>
2024-07-16 07:57:41 +0200

    Solution 80 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/gyyvleu4 Prompt:       |                              ^~~~~~ 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp:15:37: error: ‘string’ was not declared  in this scope    15 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp:14: 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp: In function ‘int main()’: 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp:24:20: error: ‘reverse_delete’ was not declared in this scope    24 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 07a4ef2e-ce76-4d9a-b097-f00cdae8a336.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
-
-using namespace std;
 
 vector<string> reverse_delete(string s, string c){
     string result = "";
@@ -16,13 +11,4 @@ vector<string> reverse_delete(string s, string c){
     reverse(reversed.begin(), reversed.end());
     bool is_palindrome = (result == reversed);
     return {result, is_palindrome ? "True" : "False"};
-}
-
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
-    return 0;
 }
