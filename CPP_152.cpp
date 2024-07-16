0ae55589742da43c4243922486902415984efed4
beep boop <githubbot@vadim.me>
2024-07-16 18:16:32 +0200

    Solution 96 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/eqwxg643 Prompt:       |             ^~~~~~ 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp:24: 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp: In function ‘int main()’: 7899f55a-fc84-43ab-ad0e-63183fd9f831.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -8,10 +8,8 @@ bool compare(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 std::vector<int> calculate_difference(const std::vector<int>& game, const std::vector<int>& guess) {
-    std::vector<int> result;
-    for (size_t i = 0; i < game.size(); ++i) {
-        result.push_back(std::abs(game[i] - guess[i]));
-    }
+    std::vector<int> result(game.size());
+    transform(game.begin(), game.end(), guess.begin(), result.begin(), [](int a, int b) { return std::abs(a - b); });
     return result;
 }
 
