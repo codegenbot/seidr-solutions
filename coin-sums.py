f0489cde0381f92fcbdb29fb819380199b3f0cc2
beep boop <githubbot@vadim.me>
2024-07-16 12:48:27 +0200

    Solution 93 of coin-sums, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/r45ce1y5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/766cd15f-e712-41de-9665-1bc69073d 82e.py", line 25, in <module>     print(*coin_sums(int(input())))            ^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/766cd15f-e712-41de-9665-1bc69073d 82e.py", line 4, in coin_sums     [   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/766cd15f-e712-41de-9665-1bc69073d 82e.py", line 14, in <listcomp>     for d in coins[coins.index(c) :]                                ^ NameError: name 'c' is not defined

--- coin-sums.py
+++ coin-sums.py
@@ -12,6 +12,7 @@ def coin_sums(cents):
                             else [(coin % c) // d, (coin % c) % d]
                         )
                         for i, c in enumerate(coins)
+                        for d in coins[i:]
                     ]
                 )
             )
