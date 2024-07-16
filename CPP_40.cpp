f48f52c9144ab8457b5985cb6e4fd83da8b7854b
beep boop <githubbot@vadim.me>
2024-07-16 07:04:45 +0200

    Solution 91 of CPP/40, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/x5qtte4r Prompt: ore ‘for’     2 |     for (int i = 0; i < l.size() - 2; i++) {       |     ^~~ 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 0; i < l.size() - 2; i++) {       |                     ^ 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp:2:39: error: ‘i’ does not name a type     2 |     for (int i = 0; i < l.size() - 2; i++) {       |                                       ^ 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp:16:5: error: expected unqualified-id be fore ‘return’    16 |     return false;       |     ^~~~~~ 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp:17:1: error: expected declaration befor e ‘}’ token    17 | }       | ^ In file included from 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp:19: 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp: In function ‘int main()’: 34d719c3-62f1-434c-bd58-8afc028a55ab.cpp:21:13: error: ‘triples_sum_to_zero’ was  not declared in this scope    21 |     assert (triples_sum_to_zero({100, 3, 5, -100}) == false);       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_40.cpp
+++ CPP_40.cpp
@@ -23,8 +23,7 @@ bool triples_sum_to_zero(const vector<int>& l) {
 }
 
 int main() {
-    vector<int> input = {0, -1, 2, -3, 1};
+    vector<int> input = {1, -2, 1, 0, 5, -1};
     assert(triples_sum_to_zero(input) == true);
-
     return 0;
 }
