330f5580f1869b973e900ba4e9d135a25c0f9668
beep boop <githubbot@vadim.me>
2024-07-16 17:21:42 +0200

    Solution 80 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jn24s87c Prompt: Program did not receive expected input

--- Python_12.py
+++ Python_12.py
@@ -1,6 +1,4 @@
 n = int(input())
-def longest(strings):
-    return max(strings, key=len)
 for _ in range(n):
     strings = input().split()
-    print(longest(strings))
+    print(max(strings, key=len))
