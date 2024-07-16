80473af78724c2c25039a0e171797ba2b058b65c
beep boop <githubbot@vadim.me>
2024-07-16 03:44:05 +0200

    Solution 29 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/cr3bh6us Prompt:       |                              ^~~~~~ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:9: e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp: In function ‘int main()’: e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ e6a83e75-daf0-4aa3-80e2-d9ec21aa0ad4.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>& vec2) {
+bool issame(const vector<string>& vec1, const vector<string>& vec2) {
     if (vec1.size() != vec2.size()) {
         return false;
     }
-    for (size_t i = 0; i < vec1.size(); i++) {
+    for (int i = 0; i < vec1.size(); i++) {
         if (vec1[i] != vec2[i]) {
             return false;
         }
@@ -13,17 +13,25 @@ bool issame(const std::vector<std::string>& vec1, const std::vector<std::string>
     return true;
 }
 
-std::vector<std::string> all_prefixes(std::string str);
+vector<string> all_prefixes(string str);
 
-std::vector<std::string> all_prefixes(std::string str){
-    std::vector<std::string> result;
+int main() {
+    string input;
+    cin >> input;
+    vector<string> prefixes = all_prefixes(input);
+    vector<string> prefixes2 = all_prefixes(input);
+    if (issame(prefixes, prefixes2)) {
+        cout << "Prefixes are the same" << endl;
+    } else {
+        cout << "Prefixes are different" << endl;
+    }
+    return 0;
+}
+
+vector<string> all_prefixes(string str){
+    vector<string> result;
     for (int i = 1; i <= str.size(); i++) {
         result.push_back(str.substr(0, i));
     }
     return result;
-}
-
-int main() {
-    // Your main function code here
-    return 0;
 }
