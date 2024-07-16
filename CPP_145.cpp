cab30de7aa8eff23c48beedcc5dd25ea2e2f4f71
beep boop <githubbot@vadim.me>
2024-07-16 17:53:58 +0200

    Solution 37 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3v8snoyk Prompt:     4 | 9accabbe-2e6a-4f74-9d14-87b275c1ea73.cpp:34:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    34 | int main(){       |         ^~ 9accabbe-2e6a-4f74-9d14-87b275c1ea73.cpp:34:9: note: remove parentheses to defau lt-initialize a variable    34 | int main(){       |         ^~       |         -- 9accabbe-2e6a-4f74-9d14-87b275c1ea73.cpp:34:9: note: or replace parentheses with  braces to value-initialize a variable 9accabbe-2e6a-4f74-9d14-87b275c1ea73.cpp:34:11: error: a function-definition is not allowed here before ‘{’ token    34 | int main(){       |           ^ 9accabbe-2e6a-4f74-9d14-87b275c1ea73.cpp:37:2: error: expected ‘}’ at end of inp ut    37 | }       |  ^ 9accabbe-2e6a-4f74-9d14-87b275c1ea73.cpp:18:12: note: to match this ‘{’    18 | int main() {       |            ^

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -15,9 +15,9 @@ bool order_by_points(std::vector<int> a, std::vector<int> b) {
     return a.size() < b.size();
 }
 
-int main() {
+int main {
     assert(issame(sort_vector({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
-    assert(issame(sort_vector({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}, std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
     
     return 0;
 }
