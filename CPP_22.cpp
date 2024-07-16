01a6fb0962f2d61dde47bfc90396d439c9479356
beep boop <githubbot@vadim.me>
2024-07-16 04:05:50 +0200

    Solution 44 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niur2qrc Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bf879b00-f812-43f2-9901-ce1464fc7630.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bf879b00-f812-43f2-9901-ce1464fc7630.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bf879b00-f812-43f2-9901-ce1464fc7630.cpp:11: bf879b00-f812-43f2-9901-ce1464fc7630.cpp: In function ‘int main()’: bf879b00-f812-43f2-9901-ce1464fc7630.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ bf879b00-f812-43f2-9901-ce1464fc7630.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,18 +1,16 @@
-vector<int> filter_integers(list<any> values){
-    vector<int> result;
-    for(auto& val : values){
-        if(typeid(int) == val.type()){
-            result.push_back(any_cast<int>(val));
-        }
-    }
-    return result;
-}
-
 bool issame(vector<int> a, vector<int> b){
-    return (a == b);
+    vector<int> filter_integers(list<any> values){
+        vector<int> result;
+        for(auto& val : values){
+            if(typeid(int) == val.type()){
+                result.push_back(boost::any_cast<int>(val));
+            }
+        }
+        return result;
+    }
 }
 
-int main(){
+int main() {
     // Main function code here
     return 0;
 }
