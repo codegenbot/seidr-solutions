507ec7080642c4770d897838f0b3df2d99912acc
beep boop <githubbot@vadim.me>
2024-07-16 03:39:13 +0200

    Solution 59 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/8hc54835 Prompt: In file included from 0b3ef3ba-5ba7-4be3-bd1d-2eb23c38470c.cpp:20: 0b3ef3ba-5ba7-4be3-bd1d-2eb23c38470c.cpp: In function ‘int main()’: 0b3ef3ba-5ba7-4be3-bd1d-2eb23c38470c.cpp:30:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    30 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 0b3ef3ba-5ba7-4be3-bd1d-2eb23c38470c.cpp:16:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    16 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 0b3ef3ba-5ba7-4be3-bd1d-2eb23c38470c.cpp:21:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    21 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -16,9 +16,3 @@ vector<int> compare(const vector<int>& a, const vector<int>& b) {
 bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
-
-int main() {
-    assert(issame(compare({1,2,3,5},{-1,2,3,4}), vector<int>{2,0,0,1}));
-    
-    return 0;
-}
