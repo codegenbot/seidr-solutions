8dbec9282d1a31d0c02b8375cddca8ed5c4f0271
beep boop <githubbot@vadim.me>
2024-07-16 16:39:37 +0200

    Solution 10 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/p0919w1h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8dd8aee-0730-4a00-80f2-18c7ae171 5d7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- dice-game.py
+++ dice-game.py
@@ -1,3 +1,2 @@
 def dice_game(n, m):
-    peter_win_count = sum(1 for i in range(1, n+1) for j in range(1, m+1) if i > j)
-    return peter_win_count / (n * m)
+    return sum(1 for i in range(1, n+1) for j in range(1, m+1) if i > j) / (n * m)
