46751e91e48e36f5b449339dad06c59f28ec5ed1
beep boop <githubbot@vadim.me>
2024-07-16 08:41:36 +0200

    Solution 10 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/osarpqwd Prompt:    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:26: 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp: In function ‘int main()’: 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:36:20: error: ‘order_by_points’ was not  declared in this scope    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,17 +1,31 @@
-sort(nums.begin(), nums.end(), [](int a, int b) {
-    int sumA = 0, sumB = 0;
-    int tempA = abs(a), tempB = abs(b);
-    while (tempA) {
-        sumA += tempA % 10;
-        tempA /= 10;
-    }
-    while (tempB) {
-        sumB += tempB % 10;
-        tempB /= 10;
-    }
-    if (sumA == sumB) {
-        return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
-    }
-    return sumA < sumB;
-});
-return nums;
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+vector<int> sortNumbers(const vector<int>& nums) {
+    vector<int> sorted_nums = nums;
+
+    sort(sorted_nums.begin(), sorted_nums.end(), [](int a, int b) {
+        int sum_a = 0, sum_b = 0;
+        a = abs(a);
+        b = abs(b);
+
+        while (a > 0) {
+            sum_a += a % 10;
+            a /= 10;
+        }
+
+        while (b > 0) {
+            sum_b += b % 10;
+            b /= 10;
+        }
+
+        if (sum_a == sum_b) {
+            return a < b;
+        }
+
+        return sum_a < sum_b;
+    });
+
+    return sorted_nums;
+}
