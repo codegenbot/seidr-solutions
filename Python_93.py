1190ba254af0401af4296d008bc0dbbed9b2d40a
beep boop <githubbot@vadim.me>
2024-07-16 03:40:24 +0200

    Solution 57 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3677752a-a800-425d-bfad-c5cd14ea7 422.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3677752a-a800-425d-bfad-c5cd14ea7 422.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,10 +3,10 @@ def encode(message):
     for char in message:
         if char.isspace():
             result += char  
-        elif char.isalnum():  
+        elif char.isalpha():  
             ascii_offset = 65 if char.isupper() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
             result += chr(shifted_char_code)
         else: 
-            result += char  # handle non-alphanumeric characters correctly
+            result += char  # keep non-alphanumeric characters unchanged
     return result
