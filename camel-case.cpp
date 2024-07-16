7e51a3c2053c2c14447633757f8c223d1dca65c9
beep boop <githubbot@vadim.me>
2024-07-16 21:45:53 +0200

    Solution 10 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/jheuxvuw Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 9f1527ea-7d4a-4823-be94-eecfed9013d5.cpp:29:45:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
-#include <string>
+using namespace std;
 
-std::string camelCase(std::string str) {
-    std::string result = "";
-    bool firstWord = true;
+string camelCase(string s) {
+    string result = "";
+    int start = 0;
     
-    for (char c : str) {
-        if (c == '-') {
-            if (!firstWord) {
-                result += char(toupper(c));
+    for (int i = 0; i <= s.size(); i++) {
+        if (i == s.size() || s[i] == ' ') {
+            string word = s.substr(start, i - start);
+            if (!result.empty()) {
+                result += toupper(word[0]);
             } else {
-                firstWord = false;
+                result += word;
             }
-        } else if (c == ' ') {
-            if (!firstWord) {
-                result += char(toupper(c));
-            } else {
-                firstWord = false;
-            }
-        } else {
-            if (!firstWord) {
-                result += c;
-            } else {
-                result += tolower(c);
-                firstWord = false;
-            }
+            start = i + 1;
         }
     }
     
@@ -32,9 +21,9 @@ std::string camelCase(std::string str) {
 }
 
 int main() {
-    std::string str;
-    std::cout << "Enter a string in kebab-case: ";
-    std::cin >> str;
-    std::cout << "CamelCase: " << camelCase(str) << std::endl;
+    string input;
+    cout << "Enter a kebab-case string: ";
+    cin >> input;
+    cout << "camelCase: " << camelCase(input) << endl;
     return 0;
 }
