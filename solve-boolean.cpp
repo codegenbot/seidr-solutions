712985c452ab2183d02520dad73617d7b267a6d0
beep boop <githubbot@vadim.me>
2024-07-16 17:30:09 +0200

    Solution 95 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/0vygyqol Prompt:   +++ |+#include <iostream>     2 | using namespace std; 4bd2a443-84f0-4dea-9e90-f578692b4aa7.cpp:18:5: error: ‘cin’ was not declared in this scope    18 |     cin >> expression;       |     ^~~ 4bd2a443-84f0-4dea-9e90-f578692b4aa7.cpp:18:5: note: ‘std::cin’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’? 4bd2a443-84f0-4dea-9e90-f578692b4aa7.cpp:21:27: error: ‘endl’ was not declared i n this scope    21 |         cout << "True" << endl;       |                           ^~~~ 4bd2a443-84f0-4dea-9e90-f578692b4aa7.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std; 4bd2a443-84f0-4dea-9e90-f578692b4aa7.cpp:23:28: error: ‘endl’ was not declared i n this scope    23 |         cout << "False" << endl;       |                            ^~~~ 4bd2a443-84f0-4dea-9e90-f578692b4aa7.cpp:23:28: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’?

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,57 +1,22 @@
-```cpp
 #include <iostream>
 #include <string>
 using namespace std;
 
 bool solveBoolean(string expression) {
-    if(expression.empty())return false;
     bool result = true;
     int i = 0;
-    while(i<expression.length())
-    {
-        if(expression[i]=='f'||expression[i]=='F')
-            return false;
-        else if(expression[i]=='t'||expression[i]=='T')
-            i++;
-        else
-        {
-            int j=i+1;
-            while(j<expression.length()&&((expression[j]>' '&&expression[j]<'a')||(expression[j]>'z'&&expression[j]<'|')))
-                j++;
-            if(expression[j-1]== '&')
-            {
-                bool temp=true;
-                i=j;
-                while(i<expression.length())
-                {
-                    if(expression[i]=='f'||expression[i]=='F')
-                        return false;
-                    else if(expression[i]=='t'||expression[i]=='T')
-                        i++;
-                    else
-                        return false;
-                }
-            }
-            else if(expression[j-1] == '|')
-            {
-                bool temp1=false,temp2=true;
-                i=j;
-                while(i<expression.length())
-                {
-                    if(expression[i]=='f'&&temp1==false)
-                        return false;
-                    else if(expression[i]=='t'&&temp1==true)
-                        {result=temp2;break;}
-                    else if((expression[i]>' '&&expression[i]<'a')||(expression[i]>'z'&&expression[i]<'|'))
-                    {
-                        temp1=(temp1)?false:true;
-                        i++;
-                    }
-                    else
-                        return false;
-                }
-            }
+    while(i < expression.length()) {
+        if(expression[i] == 'F' || expression[i] == 'f') {
+            result = false;
+            break;
         }
+        else if(expression[i] == '|') {
+            result = !result;
+        }
+        else if(expression[i] == '&') {
+            return result;
+        }
+        i++;
     }
     return result;
 }
@@ -67,4 +32,3 @@ int main() {
         cout << "False" << endl;
     }
     return 0;
-}
