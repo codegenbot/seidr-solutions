65cf3536ed666d60bb42c9aea79ae38a6cb777a3
beep boop <githubbot@vadim.me>
2024-07-16 04:23:05 +0200

    Solution 90 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf4d313-583f-4b77-8d39-4f7c6ef2d 85e.py", line 27, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf4d313-583f-4b77-8d39-4f7c6ef2d 85e.py", line 23, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -8,5 +8,7 @@ def encode(message):
         else:
             ascii_offset = 65 if char.isupper() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
-            result += chr(shifted_char_code)
-    return result
+            if ord('A') <= ascii_offset and char.isupper():
+                result += chr(shifted_char_code).upper()
+            elif ord('a') <= ascii_offset and char.islower():
+                result += chr(shifted_char_code).lower()
