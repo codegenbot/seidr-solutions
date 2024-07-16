87d9117fcbf15da94725d47b9715e6a57224fb0d
beep boop <githubbot@vadim.me>
2024-07-16 10:01:00 +0200

    Solution 2 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/o2oujxn5 Prompt: Given a string representing a tweet, validate whether the tweet meets Twitter’s original character requirements. If the tweet has more than 140 characters, return the string "Too many characters". If the tweet is empty, return the string "You didn't type anything". Otherwise, return "Your tweet has X characters", where the X is the number of characters in the tweet.

--- twitter.cpp
+++ twitter.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <string>
 
-std::string validateTweet(const std::string& tweet) {
+std::string validateTweet(std::string tweet) {
     if (tweet.empty()) {
         return "You didn't type anything";
     }
-    
-    if (tweet.length() > 140) {
+    else if (tweet.length() > 140) {
         return "Too many characters";
     }
-    
-    return "Your tweet has " + std::to_string(tweet.length()) + " characters";
+    else {
+        return "Your tweet has " + std::to_string(tweet.length()) + " characters";
+    }
 }
 
 int main() {
-    // Test cases
-    std::cout << validateTweet("") << std::endl;
-    std::cout << validateTweet("1") << std::endl;
-    std::cout << validateTweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it") << std::endl;
-    std::cout << validateTweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)") << std::endl;
-    std::cout << validateTweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1") << std::endl;
-    
+    std::string input;
+    while (true) {
+        std::cout << "Enter a tweet: ";
+        std::getline(std::cin, input);
+        std::cout << validateTweet(input) << "\n";
+    }
     return 0;
 }
