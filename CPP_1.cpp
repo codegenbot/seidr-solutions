2ab0e5ad68441c9e795f4ae45ca9dc89243f70b5
beep boop <githubbot@vadim.me>
2024-07-16 03:47:00 +0200

    Solution 51 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1vr8zy9e Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:28: 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp: In function ‘int main()’: 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:38:20: error: ‘separate_paren_groups’ w as not declared in this scope    38 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -2,27 +2,32 @@ bool issame(vector<string> v1, vector<string> v2) {
     if (v1.size() != v2.size()) {
         return false;
     }
-
+    
     for (int i = 0; i < v1.size(); i++) {
         if (v1[i] != v2[i]) {
             return false;
         }
     }
-
+    
     return true;
 }
 
 vector<string> separate_paren_groups(string paren_string);
 
 int main() {
-    vector<string> result = separate_paren_groups("((group1)(group2)(group3))");
-    vector<string> expected_result = {"(group1)", "(group2)", "(group3)"};
-
+    string input;
+    cin >> input;
+    
+    vector<string> result = separate_paren_groups(input);
+    
+    vector<string> expected_result;
+    // Populate expected_result with the expected output
+    
     if (issame(result, expected_result)) {
-        cout << "Test Passed" << endl;
+        cout << "Output is correct" << endl;
     } else {
-        cout << "Test Failed" << endl;
+        cout << "Output is incorrect" << endl;
     }
-
+    
     return 0;
 }
