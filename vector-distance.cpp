0fe2c9c66f59a7c3a26f20ef115b8a133aa5d9d1
beep boop <githubbot@vadim.me>
2024-07-16 15:53:33 +0200

    Solution 84 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/bv2ogq57 Prompt:   +++ |+#include <cstddef>     1 | double calculateDistance(const std::vector<double>& vec1, const std::vec tor<double>& vec2) { 42ffd16e-953a-40cf-ba50-8f76677b39d9.cpp:3:24: error: ‘i’ was not declared in th is scope     3 |     for (size_t i = 0; i < vec1.size(); ++i) {       |                        ^ 42ffd16e-953a-40cf-ba50-8f76677b39d9.cpp:3:28: error: ‘vec1’ was not declared in  this scope     3 |     for (size_t i = 0; i < vec1.size(); ++i) {       |                            ^~~~ 42ffd16e-953a-40cf-ba50-8f76677b39d9.cpp:4:23: error: ‘pow’ is not a member of ‘ std’     4 |         sumSq += std::pow(vec1[i] - vec2[i], 2);       |                       ^~~ 42ffd16e-953a-40cf-ba50-8f76677b39d9.cpp:4:37: error: ‘vec2’ was not declared in  this scope     4 |         sumSq += std::pow(vec1[i] - vec2[i], 2);       |                                     ^~~~ 42ffd16e-953a-40cf-ba50-8f76677b39d9.cpp:6:12: error: ‘sqrt’ was not declared in  this scope     6 |     return sqrt(sumSq);       |            ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -3,8 +3,9 @@
 
 double calculateDistance(const std::vector<double>& vec1, const std::vector<double>& vec2) {
     double sumSq = 0.0;
-    for (size_t i = 0; i < vec1.size(); ++i) {
-        sumSq += std::pow(vec1[i] - vec2[i], 2.0);
+    size_t i;
+    for (i = 0; i < vec1.size(); ++i) {
+        sumSq += std::pow(vec1[i] - vec2[i], 2);
     }
     return std::sqrt(sumSq);
 }
