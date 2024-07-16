508234799c03da02b5af689f3966863e6fb6017d
beep boop <githubbot@vadim.me>
2024-07-16 07:00:04 +0200

    Solution 57 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3aqsxwbq Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from cc53f7a3-ca0c-4d49-ae45-b3beaadc7082.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <initializer_list>
 
 using namespace std;
 
@@ -12,8 +11,8 @@ bool is_palindrome(string s) {
     return (temp == s);
 }
 
-std::pair<std::string, std::string> reverse_delete(std::string s, std::string c) {
-    std::string temp = "";
+std::pair<string, string> reverse_delete(string s, string c) {
+    string temp = "";
     for (char ch : s) {
         bool found = false;
         for (char cc : c) {
@@ -28,10 +27,10 @@ std::pair<std::string, std::string> reverse_delete(std::string s, std::string c)
     }
     string rev = s;
     reverse(rev.begin(), rev.end());
-    return {temp, is_palindrome(temp) ? "True" : "False"};
+    return make_pair(temp, (is_palindrome(temp)) ? "True" : "False");
 }
 
 int main() {
-    assert (std::make_pair("", reverse_delete("mamma", "mia").second) == std::pair<std::string, std::string>("", "True"));
+    assert(make_pair("", reverse_delete("mamma", "mia").second) == pair<string, string>("", "True"));
     return 0;
 }
