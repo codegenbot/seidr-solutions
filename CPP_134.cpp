b59f50c6a3bd4b59459ec9062d49a758b4bc9802
beep boop <githubbot@vadim.me>
2024-07-16 17:24:37 +0200

    Solution 43 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/zhsyzucv Prompt: Complete the following code given the task description and function signature.

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,7 +1,8 @@
-if(txt.empty()) return false;
+if (txt.empty()) return false;
     char lastChar = txt.back();
-    if(isalpha(lastChar)){
-        if(txt.find_last_of(" ") == txt.length()-1) return true;
+    if (isalpha(lastChar)) {
+        if (txt.size() == 1) return true;
+        if (txt[txt.size() - 2] == ' ') return true;
     }
     return false;
 }
