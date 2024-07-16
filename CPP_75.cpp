2c205a9049e104289ee02afe294055499cdcde8c
beep boop <githubbot@vadim.me>
2024-07-16 13:11:53 +0200

    Solution 11 of CPP/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/n3w5ojmo Prompt: Complete the following code given the task description and function signature.

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-bool is_prime(int num) {
-    if (num < 2) {
+bool is_prime(int n) {
+    if (n <= 1) {
         return false;
     }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
             return false;
         }
     }
@@ -16,14 +16,14 @@ bool is_prime(int num) {
 }
 
 bool is_multiply_prime(int a) {
+    if (a < 6) {
+        return false;
+    }
     vector<int> primes;
-    for (int i = 2; i <= a; i++) {
+    for (int i = 2; i <= 100; i++) {
         if (is_prime(i)) {
             primes.push_back(i);
         }
-    }
-    if (primes.size() < 3) {
-        return false;
     }
     for (int i = 0; i < primes.size(); i++) {
         for (int j = i + 1; j < primes.size(); j++) {
@@ -41,6 +41,10 @@ int main() {
     int num;
     cout << "Enter a number less than 100: ";
     cin >> num;
-    cout << (is_multiply_prime(num) ? "true" : "false") << endl;
+    if (is_multiply_prime(num)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
     return 0;
 }
