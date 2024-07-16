acb54d547ea52c94e6ce63d33a1098014693c102
beep boop <githubbot@vadim.me>
2024-07-16 04:21:52 +0200

    Solution 89 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf4d313-583f-4b77-8d39-4f7c6ef2d 85e.py", line 27, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2bf4d313-583f-4b77-8d39-4f7c6ef2d 85e.py", line 23, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,4 +1,3 @@
-```
 def encode(message):
     result = ""
     for char in message:
@@ -10,8 +9,4 @@ def encode(message):
             ascii_offset = 65 if char.isupper() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
             result += chr(shifted_char_code)
-            if char.isupper():
-                result += 'R'
-            elif char.islower():
-                result += 'r'
     return result
