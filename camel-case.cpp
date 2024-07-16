052a8d4f0e22dc620adb1576583713c8a6a6ed25
beep boop <githubbot@vadim.me>
2024-07-16 16:48:25 +0200

    Solution 24 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8q722hdb Prompt:     7 |     for (const auto& c : split(str, '-')) {       |                          ^~~~~ af4cb2a1-9054-4284-acbf-341f143f773e.cpp: In function ‘std::string split(const s tring&, char)’: af4cb2a1-9054-4284-acbf-341f143f773e.cpp:28:18: error: passing ‘const string’ {a ka ‘const std::__cxx11::basic_string<char>’} as ‘this’ argument discards qualifi ers [-fpermissive]    28 |         str.erase(0, pos + 1);       |         ~~~~~~~~~^~~~~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from af4cb2a1-9054-4284-acbf-341f143f773e.cpp:1: /usr/include/c++/11/bits/basic_string.h:1827:7: note:   in call to ‘std::__cxx11 ::basic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Tra its, _Alloc>::erase(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_ty pe, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type) [with _CharT  = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>; std::_ _cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1827 |       erase(size_type __pos = 0, size_type __n = npos)       |       ^~~~~ af4cb2a1-9054-4284-acbf-341f143f773e.cpp:31:12: error: ‘join’ was not declared i n this scope    31 |     return join(tokens, ' ');       |            ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,44 +1,43 @@
 #include <string>
 #include <vector>
 
-std::string join(const std::vector<std::string>& tokens) {
-    std::string result;
-    for (const auto& token : tokens) {
-        if (!result.empty()) {
-            result += ' ';
-        }
-        result += token;
-    }
-    return result;
-}
-
-std::string split(const std::string& str, char delimiter) {
-    std::vector<std::string> tokens;
-    size_t pos = 0;
-    while ((pos = str.find(delimiter)) != std::string::npos) {
-        tokens.push_back(str.substr(0, pos));
-        str = str.substr(pos + 1);
-    }
-    tokens.push_back(str);
-    return join(tokens);
-}
-
 std::string camelCase(const std::string& str) {
     std::string result;
     bool capitalizeNext = true;
     for (const auto& c : split(str, '-')) {
         if (!result.empty()) {
             if (capitalizeNext) {
-                result += toupper(c[0]);
+                result += toupper(c);
                 capitalizeNext = false;
             } else {
-                result += tolower(c[0]);
+                result += tolower(c);
             }
-            result += c.substr(1);
         } else {
             result = tolower(c);
             capitalizeNext = true;
         }
     }
     return result;
+}
+
+std::string join(const std::vector<std::string>& tokens, char delimiter) {
+    std::string result;
+    for (const auto& token : tokens) {
+        if (!result.empty()) {
+            result += delimiter;
+        }
+        result += token;
+    }
+    return result;
+}
+
+std::string split(std::string str, char delimiter) {
+    std::vector<std::string> tokens;
+    size_t pos = 0;
+    while ((pos = str.find(delimiter)) != std::string::npos) {
+        tokens.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
+    }
+    tokens.push_back(str);
+    return join(tokens, ' ');
 }
