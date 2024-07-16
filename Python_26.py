82ebbc3914b77d3770addd5f03ccc195e9b6c921
beep boop <githubbot@vadim.me>
2024-07-16 02:57:33 +0200

    Solution 16 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/05ynjqkz Prompt: [2, 3, 4] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/293156c5-4396-4a7e-8709-36b5bf5c8 5d0.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/293156c5-4396-4a7e-8709-36b5bf5c8 5d0.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'list' object is not callable

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,2 @@
 def check(numbers):
-    return list(set(numbers))
-
-remove_duplicates = [1, 2, 3, 2, 4, 3, 5]
-result = check(remove_duplicates)
-print(result)
+    return list(dict.fromkeys(numbers))
