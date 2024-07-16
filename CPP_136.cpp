4a7798d31a78cfa3d7971e1d612e6370f0fb2a3d
beep boop <githubbot@vadim.me>
2024-07-16 10:16:57 +0200

    Solution 70 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/byjhxe6e Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bb39d185-0a4b-42a6-85e2-87f13248bc33.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bb39d185-0a4b-42a6-85e2-87f13248bc33.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bb39d185-0a4b-42a6-85e2-87f13248bc33.cpp:17: bb39d185-0a4b-42a6-85e2-87f13248bc33.cpp: In function ‘int main()’: bb39d185-0a4b-42a6-85e2-87f13248bc33.cpp:27:20: error: ‘largest_smallest_integer s’ was not declared in this scope    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ bb39d185-0a4b-42a6-85e2-87f13248bc33.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,30 +1,27 @@
+#include <vector>
+
+vector<int> largest_smallest_integers(vector<int> lst){
+    int max_neg = 0;
+    int min_pos = 0;
+    
+    for(int num : lst){
+        if(num < 0 && num < max_neg){
+            max_neg = num;
+        }
+        if(num > 0 && (num < min_pos || min_pos == 0)){
+            min_pos = num;
+        }
+    }
+    
+    return {max_neg, min_pos};
+}
+
 bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-vector<int> largest_smallest_integers(vector<int> lst){
-    int largest_negative = INT_MIN, smallest_positive = INT_MAX;
-    for(int num : lst){
-        if(num < 0 && num > largest_negative){
-            largest_negative = num;
-        }
-        if(num > 0 && num < smallest_positive){
-            smallest_positive = num;
-        }
-    }
-    return {largest_negative, smallest_positive};
-}
-
-int main() {
-    vector<int> input = {3, -2, 5, -8, 10};
-    vector<int> result = largest_smallest_integers(input);
-    vector<int> expected = {-2, 3};
+int main(){
+    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
     
-    if(issame(result, expected)){
-        cout << "Test passed!" << endl;
-    } else {
-        cout << "Test failed!" << endl;
-    }
-
     return 0;
 }
