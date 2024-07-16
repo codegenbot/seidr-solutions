72407adb52ba42192b023c07cdcebb41f276f22a
beep boop <githubbot@vadim.me>
2024-07-16 13:00:57 +0200

    Solution 34 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/j96pttdu Prompt: 80e61502-0fc0-457c-8060-aeedd29f1937.cpp: In function ‘std::vector<int> calculat eLeaders(std::vector<int>)’: 80e61502-0fc0-457c-8060-aeedd29f1937.cpp:17:13: error: ‘leaders’ was not declare d in this scope    17 |             leaders.push_back(maxSoFar);       |             ^~~~~~~ 80e61502-0fc0-457c-8060-aeedd29f1937.cpp:21:23: error: ‘leaders’ was not declare d in this scope    21 |     res = vector<int>(leaders.rbegin(), leaders.rend());       |                       ^~~~~~~

--- leaders.cpp
+++ leaders.cpp
@@ -1,14 +1,33 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
 vector<int> calculateLeaders(vector<int> arr) {
     int n = arr.size();
-    vector<int> leaders;
+    vector<int> res;
     
-    for(int i=n-1; i>=0; i--){
-        while(i > 0 && arr[i] <= arr[i-1]){
-            i--;
+    if(n == 0)
+        return res;
+    
+    res.push_back(arr[n-1]);
+    
+    int maxSoFar = arr.back();
+    for(int i=n-2; i>=0; i--){
+        if(arr[i] >= maxSoFar){
+            res.push_back(maxSoFar);
+            maxSoFar = arr[i];
         }
-        if(i == 0 || arr[i] >= arr[i-1])
-            leaders.push_back(arr[i]);
     }
+    res = vector<int>(res.rbegin(), res.rend());
     
-    return leaders;
+    return res;
+}
+
+int main() {
+    vector<int> arr = {16, 17, 4, 3, 5, 2};
+    vector<int> leadersVec = calculateLeaders(arr);
+    for(int i:leadersVec){
+        cout << i << " ";
+    }
+    return 0;
 }
