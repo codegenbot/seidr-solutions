e8f4c3780e99f167698171f22979ad1829af76a7
beep boop <githubbot@vadim.me>
2024-07-17 00:34:45 +0200

    Solution 48 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8x9ggrs8 Prompt: d285d82e-0a37-42b5-9913-c38617cace10.cpp:1:1: error: expected unqualified-id bef ore ‘else’     1 | else {       | ^~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,8 +1,3 @@
-else if (tweet.length() > 140) {
-    return "Too many characters";
-} else if (!tweet.empty()) {
-    std::string result = "Your tweet has " + std::to_string(tweet.length()) + " characters";
-    return result;
-} else {
-    return "You didn't type anything";
+else {
+    return "Your tweet has " + std::to_string(tweet.length()) + " characters";
 }
