7648a2feb694a2d4f55e696dd9a2877ae1e71af6
beep boop <githubbot@vadim.me>
2024-07-16 10:16:51 +0200

    Solution 58 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/0zc4o58x Prompt: 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp:1:30: error: ‘vector’ was not declared in this scope     1 | double double_the_difference(vector<float> lst){       |                              ^~~~~~ 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp:1:37: error: expected primary-expressio n before ‘float’     1 | double double_the_difference(vector<float> lst){       |                                     ^~~~~ In file included from 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp:11: 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp: In function ‘int main()’: 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 9a014393-362c-407b-bcea-9e2f7d5a9078.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -5,8 +5,8 @@
 double double_the_difference(std::vector<float> lst){
     double sum = 0;
     for(float num : lst){
-        if(num > 0 && std::fmod(num, 1) == 0 && (int)num % 2 != 0){
-            sum += std::pow(num, 2);
+        if(num > 0 && fmod(num, 1) == 0 && (int)num % 2 != 0){
+            sum += pow(num, 2);
         }
     }
     return sum;
