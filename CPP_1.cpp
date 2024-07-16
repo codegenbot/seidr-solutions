36436c80408c3d35cf83fa6a2074dc04bfdef413
beep boop <githubbot@vadim.me>
2024-07-16 04:01:28 +0200

    Solution 70 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1vr8zy9e Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cfe5cd16-44ac-4777-80ad-56c5de0d8bf5.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cfe5cd16-44ac-4777-80ad-56c5de0d8bf5.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cfe5cd16-44ac-4777-80ad-56c5de0d8bf5.cpp:26: cfe5cd16-44ac-4777-80ad-56c5de0d8bf5.cpp: In function ‘int main()’: cfe5cd16-44ac-4777-80ad-56c5de0d8bf5.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ cfe5cd16-44ac-4777-80ad-56c5de0d8bf5.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,28 +1,29 @@
+#include <vector>
+#include <string>
+
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
-    string group;
-    int count = 0;
+    string current_group;
+    int open_braces = 0;
 
     for (char c : paren_string) {
         if (c == '(') {
-            count++;
-            if (count > 1) {
-                group += c;
+            if (open_braces > 0) {
+                current_group += c;
             }
+            open_braces++;
         } else if (c == ')') {
-            count--;
-            if (count > 0) {
-                group += c;
-            } else if (count == 0) {
-                result.push_back(group);
-                group = "";
+            open_braces--;
+            if (open_braces > 0) {
+                current_group += c;
+            } else if (open_braces == 0) {
+                result.push_back(current_group);
+                current_group = "";
             }
-        } else if (count > 0) {
-            group += c;
         }
     }
 
@@ -31,5 +32,4 @@ vector<string> separate_paren_groups(string paren_string) {
 
 int main() {
     assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
-    return 0;
 }
