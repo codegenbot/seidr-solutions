989511b84f5eedd0dddb235f0269a892994568b5
beep boop <githubbot@vadim.me>
2024-07-16 03:59:36 +0200

    Solution 65 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/hbrl2ml1 Prompt:       |                    ^~~ bb289087-5bfc-4d65-900e-9b09808c83a7.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bb289087-5bfc-4d65-900e-9b09808c83a7.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bb289087-5bfc-4d65-900e-9b09808c83a7.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bb289087-5bfc-4d65-900e-9b09808c83a7.cpp:12: bb289087-5bfc-4d65-900e-9b09808c83a7.cpp: In function ‘int main()’: bb289087-5bfc-4d65-900e-9b09808c83a7.cpp:22:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    22 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ bb289087-5bfc-4d65-900e-9b09808c83a7.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -15,3 +15,19 @@ std::vector<int> sum_product(const std::vector<int>& numbers) {
 bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
+
+int main() {
+    std::vector<int> numbers = {2, 3, 4};
+    std::vector<int> result = sum_product(numbers);
+    
+    std::vector<int> test1 = {5, 6};
+    std::vector<int> test2 = {5, 6};
+    
+    if (issame(test1, test2)) {
+        std::cout << "Test1 and Test2 are the same" << std::endl;
+    } else {
+        std::cout << "Test1 and Test2 are different" << std::endl;
+    }
+    
+    return 0;
+}
