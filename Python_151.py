99caad43b9144968ec60c7bc3abc3bd9931868af
beep boop <githubbot@vadim.me>
2024-07-16 04:41:50 +0200

    Solution 18 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ce5asl8u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14049db2-524a-48bb-9b90-5eecb6503 518.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14049db2-524a-48bb-9b90-5eecb6503 518.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,5 @@
 def double_the_difference(lst):
-    return sum(x**2 for x in lst if x > 0 and x % 2 != 0)
-
+    return sum(x ** 2 for x in lst if x > 0 and x % 2 != 0)
 
 input_list = [3, -2, 5, 7]
 result = double_the_difference(input_list)
