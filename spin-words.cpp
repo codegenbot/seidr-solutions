1a5c1cac185b0722c7df3b843633b636d3e60aa2
beep boop <githubbot@vadim.me>
2024-07-16 14:27:04 +0200

    Solution 42 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/eumscrgm Prompt: fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp: In function ‘int main()’: fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp:2:5: error: ‘string’ was not declared i n this scope     2 |     string spinWords(string str) {       |     ^~~~~~ fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << spinWords("This is a test");       |     ^~~~ fe78f71f-e03f-43e9-ac26-efd6080b998a.cpp:21:13: error: ‘spinWords’ was not decla red in this scope    21 |     cout << spinWords("This is a test");       |             ^~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -4,24 +4,25 @@
 using namespace std;
 
 int main() {
-    string spinWords(string str) {
-        string result = "";
-        int i = 0;
-        while (i < str.length()) {
-            if (str.find(" ", i) == -1 || i + 1 >= str.length()) {
-                result += str.substr(i);
-                break;
-            }
-            int j = str.find(" ", i);
-            string word = str.substr(i, j - i);
-            if (word.length() >= 5) {
-                for (int k = word.length() - 1; k >= 0; --k)
-                    result += word[k];
-            } else
-                result += word;
-            i = j + 1;
-        }
-        return result;
-    }
     cout << spinWords("This is a test"); 
+}
+
+string spinWords(string str) {
+    string result = "";
+    int i = 0;
+    while (i < str.length()) {
+        if (str.find(" ", i) == -1 || i + 1 >= str.length()) {
+            result += str.substr(i);
+            break;
+        }
+        int j = str.find(" ", i);
+        string word = str.substr(i, j - i);
+        if (word.length() >= 5) {
+            for (int k = word.length() - 1; k >= 0; --k)
+                result += word[k];
+        } else
+            result += word;
+        i = j + 1;
+    }
+    return result;
 }
