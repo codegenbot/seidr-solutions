29cd61f920b72c368eaac2cbe7b3e6291197fc54
beep boop <githubbot@vadim.me>
2024-07-16 03:11:10 +0200

    Solution 21 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dx1ic3yv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c24028fa-0c47-43e8-bbbe-6f680d1d3 0b4.py", line 10, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c24028fa-0c47-43e8-bbbe-6f680d1d3 0b4.py", line 7, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,6 +1,9 @@
+```
 def solve(N):
-    if N == 0:
-        return "0"
-    s = str(bin(N)[2:])
-    odd_sum = sum(int(i) for i in s if (i != '0' and int(i)) % 2)
-    return bin(odd_sum)[2:]
+    if N < 0:
+        return "-" + solve(-N)
+    elif type(N) != int:
+        return "Error: Input must be an integer."
+    else:
+        s = format(abs(N), 'b').zfill(len(str(bin(abs(N))[2:]))).lstrip("0")
+        return "0" if s == "" else s
