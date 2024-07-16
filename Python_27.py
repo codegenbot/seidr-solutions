ee45d8802ad7c3a5e21a1293a1848550a33f96b1
beep boop <githubbot@vadim.me>
2024-07-16 07:22:12 +0200

    Solution 36 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/19h53pnh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/674db675-065b-41a6-85ee-ac01977b9 c4f.py", line 1     return "".join([char.lower() if char.isupper() else char.upper() for char in  string])     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_27.py
+++ Python_27.py
@@ -1,4 +1,2 @@
 def invert_case(string):
-    return "".join(
-        [char.lower() if char.isupper() else char.upper() for char in string]
-    )
+    return "".join([char.lower() if char.isupper() else char.upper() for char in string])
