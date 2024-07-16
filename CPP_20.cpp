c37d649032f2fbf9eda45fe421157bba2100c9a1
beep boop <githubbot@vadim.me>
2024-07-16 04:30:16 +0200

    Solution 60 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/juygrexr Prompt:    16 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0bedf283-7449-4860-9fba-6a720045866f.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0bedf283-7449-4860-9fba-6a720045866f.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 0bedf283-7449-4860-9fba-6a720045866f.cpp:15: 0bedf283-7449-4860-9fba-6a720045866f.cpp: In function ‘int main()’: 0bedf283-7449-4860-9fba-6a720045866f.cpp:25:20: error: ‘find_closest_elements’ w as not declared in this scope    25 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0bedf283-7449-4860-9fba-6a720045866f.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -3,8 +3,6 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
 pair<float, float> find_closest_elements(vector<float> numbers) {
     sort(numbers.begin(), numbers.end());
     float min_diff = numbers[1] - numbers[0];
@@ -20,14 +18,8 @@ pair<float, float> find_closest_elements(vector<float> numbers) {
     return closest_pair;
 }
 
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1});
-
-    cout << "Test passed!" << endl;
+    assert(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) == make_pair(2.2, 3.1));
     
     return 0;
 }
