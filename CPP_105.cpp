1f9bee537a1d89c02f6f8e271750a60402dfa8e7
beep boop <githubbot@vadim.me>
2024-07-16 06:50:42 +0200

    Solution 10 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/t4hbw3du Prompt:       |                              ^~~~~~ 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp:23: 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp: In function ‘int main()’: 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp:33:20: error: ‘by_length’ was not decla red in this scope    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 56a6042f-3f9c-4023-85ee-fcdd2a116db3.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,23 +1,25 @@
+#include <string>
+
+bool issame(vector<string> a, vector<string> b);
+
 vector<string> by_length(vector<int> arr){
+    vector<string> names = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<int> sorted_arr;
     vector<string> result;
-    map<int, string> numToString = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
-    };
-    
-    vector<int> filtered_arr;
+
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            filtered_arr.push_back(num);
+            sorted_arr.push_back(num);
         }
     }
-    
-    sort(filtered_arr.begin(), filtered_arr.end());
-    reverse(filtered_arr.begin(), filtered_arr.end());
-    
-    for (int num : filtered_arr) {
-        result.push_back(numToString[num]);
+
+    sort(sorted_arr.begin(), sorted_arr.end());
+
+    reverse(sorted_arr.begin(), sorted_arr.end());
+
+    for (int num : sorted_arr) {
+        result.push_back(names[num - 1]);
     }
-    
+
     return result;
 }
