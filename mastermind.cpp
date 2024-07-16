b90625f526c2c72dab9e380fb6f975ad74daf2dc
beep boop <githubbot@vadim.me>
2024-07-16 19:18:50 +0200

    Solution 51 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xy4vhrzw Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -4,30 +4,33 @@
 
 int main() {
     std::string codeStr, guessStr;
-    int whitePegs = 0, blackPegs = 0;
 
+    // Read user input
     std::cout << "Enter the Mastermind code: ";
     std::cin >> codeStr;
 
     std::cout << "Enter your guess: ";
     std::cin >> guessStr;
 
+    int whitePegs = 0, blackPegs = 0;
     for (int i = 0; i < 4; i++) {
         if (codeStr[i] == guessStr[i]) {
             blackPegs++;
-        } else {
-            int count = 0;
-            for (int j = 0; j < 4; j++) {
-                if (codeStr[j] == guessStr[i]) {
-                    count++;
-                }
-            }
-            if (count > 0) {
-                whitePegs++;
-            }
         }
     }
 
+    for (char c : guessStr) {
+        int count = 0;
+        for (int j = 0; j < 4; j++) {
+            if (c == codeStr[j] && guessStr[j] != codeStr[j]) {
+                whitePegs++;
+                count++;
+            }
+        }
+        blackPegs -= count;
+    }
+
+    // Print the result
     std::cout << "Black pegs: " << blackPegs << std::endl;
     std::cout << "White pegs: " << whitePegs << std::endl;
 
