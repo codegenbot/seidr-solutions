13c406163a09270b4e2390f24fb18d9f5a2113fa
beep boop <githubbot@vadim.me>
2024-07-16 16:53:37 +0200

    Solution 37 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/pmrvrh9v Prompt: nt]’ for array subscript     6 |         char c = s[i];       |                   ^ 82fc8df4-e0a5-44f9-8d94-b6405cdc9a49.cpp:15:22: error: ‘isdigit’ is not a member  of ‘std’    15 |             if (std::isdigit(c)) {       |                      ^~~~~~~ 82fc8df4-e0a5-44f9-8d94-b6405cdc9a49.cpp:18:23: error: request for member ‘find’  in ‘s’, which is of non-class type ‘const int’    18 |                 if (s.find('/') != std::string::npos) {       |                       ^~~~ 82fc8df4-e0a5-44f9-8d94-b6405cdc9a49.cpp:18:41: error: ‘std::string’ has not bee n declared    18 |                 if (s.find('/') != std::string::npos) {       |                                         ^~~~~~ 82fc8df4-e0a5-44f9-8d94-b6405cdc9a49.cpp:20:34: error: request for member ‘size’  in ‘s’, which is of non-class type ‘const int’    20 |                 } else if (i < s.size() - 1) {       |                                  ^~~~ 82fc8df4-e0a5-44f9-8d94-b6405cdc9a49.cpp:21:39: error: invalid types ‘const int[ int]’ for array subscript    21 |                     int secondRoll = s[i + 1] - '0';       |                                       ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
 int bowlingScore(const std::string& s) {
     int score = 0;
     int currentRoll = 0;
@@ -15,12 +19,21 @@ int bowlingScore(const std::string& s) {
             if (std::isdigit(c)) {
                 int firstRoll = c - '0';
                 score += firstRoll;
-                if (s.find('/') != std::string::npos) {
-                    currentRoll = 10 - firstRoll;
-                } else if (i < s.size() - 1) {
-                    int secondRoll = s[i + 1] - '0';
-                    score += secondRoll;
-                    currentRoll = 10 - firstRoll - secondRoll;
+                if (i < s.size() - 1) {
+                    char nextC = s[i + 1];
+                    if (std::isdigit(nextC)) {
+                        int secondRoll = nextC - '0';
+                        if (firstRoll + secondRoll == 10) {
+                            score += 10;
+                            currentRoll = 0;
+                            i++;
+                        } else {
+                            score += secondRoll;
+                            currentRoll = 10 - firstRoll - secondRoll;
+                        }
+                    } else {
+                        currentRoll = 10 - firstRoll;
+                    }
                 } else {
                     currentRoll = 20 - firstRoll;
                 }
