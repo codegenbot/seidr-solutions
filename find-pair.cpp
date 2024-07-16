3849830a8111e69716b6e5c12937675f08b5f41a
beep boop <githubbot@vadim.me>
2024-07-16 18:43:05 +0200

    Solution 38 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/aut863mg Prompt: 8291d1e5-a8b7-4742-b6b7-e1ec95efbfe1.cpp:1:6: error: ‘vector’ in namespace ‘std’  does not name a template type     1 | std::vector<std::pair<int, int>> findPairs(std::vector<int>& nums, int t arget) {       |      ^~~~~~ 8291d1e5-a8b7-4742-b6b7-e1ec95efbfe1.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | std::vector<std::pair<int, int>> findPairs(std::vector<int>& nums, int t arget) {

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,32 +1,46 @@
 #include <vector>
-#include <algorithm>
 
 std::vector<std::pair<int, int>> findPairs(std::vector<int>& nums, int target) {
     std::sort(nums.begin(), nums.end());
     
     std::vector<std::pair<int, int>> result;
     
-    for (int i = 0; i < nums.size(); ++i) {
-        int j = nums.size() - 1;
-        while (j > i) {
-            if ((nums[i] + nums[j]) == target) {
-                result.push_back({nums[i], nums[j]});
-                --j;
-                while (j > i && nums[j] == nums[j-1]) {
-                    --j;
-                }
-                break;
-            } else if ((nums[i] + nums[j]) < target) {
-                --j;
-                while (j > i && nums[j] == nums[j-1]) {
-                    --j;
-                }
-            } else {
-                ++i;
-                while (i < j && nums[i] == nums[i+1]) {
-                    ++i;
+    int i = 0;
+    int j = nums.size() - 1;
+    
+    while (i < j) {
+        int sum = nums[i] + nums[j];
+        
+        if (sum == target) {
+            bool found = false;
+            for (auto& pair : result) {
+                if (pair.first == nums[i] && pair.second == nums[j]) {
+                    found = true;
+                    break;
                 }
             }
+            
+            if (!found) {
+                int firstIndex = i;
+                while (i < j && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+                result.push_back({nums[firstIndex], nums[j]});
+                int lastIndex = j;
+                while (i < j && nums[j] == nums[j - 1]) {
+                    j--;
+                }
+            }
+            
+            i++;
+            j--;
+        } else if (sum < target) {
+            i++;
+        } else {
+            while (i < j && nums[i] == nums[i + 1]) {
+                i++;
+            }
+            j--;
         }
     }
     
