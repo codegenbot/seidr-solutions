525a25bd94b6daeee6d1900c7f8dd7c5533b064b
beep boop <githubbot@vadim.me>
2024-07-16 16:33:11 +0200

    Solution 88 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/59z7jf6s Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bfbead02-1110-4f49-9eb9-9117afdd2 2ee.py", line 23, in <module>     check(find_zero)           ^^^^^^^^^ NameError: name 'find_zero' is not defined

--- Python_32.py
+++ Python_32.py
@@ -1,3 +1,5 @@
 def sort_integers(input_list):
-    input_list.sort(key=lambda x: x != 0)
-    return input_list
+    return sorted(input_list)
+
+def find_zero(input_list):
+    return [num for num in input_list if num == 0]
