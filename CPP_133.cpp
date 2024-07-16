e5e73a112fcbc6598f1982ce48f78c7ea115d01b
beep boop <githubbot@vadim.me>
2024-07-16 03:18:28 +0200

    Solution 16 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zjzfh4vm Prompt: 8952306b-912a-4e50-a574-c05a360ba12f.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 8952306b-912a-4e50-a574-c05a360ba12f.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,4 +1,4 @@
-```c++
+```
 #include <vector>
 #include <cmath>
 #include <initializer_list>
@@ -12,9 +12,4 @@ int sum_squares(std::initializer_list<float> lst){
     }
     return total;
 
-}
-
-int main() {
-    assert(sum_squares({-1,1,0})==2);
-    return 0;
 }
