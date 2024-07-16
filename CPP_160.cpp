81563511e3e6cb8d8285d18ffa3d86b11e30edd3
beep boop <githubbot@vadim.me>
2024-07-16 16:27:38 +0200

    Solution 79 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9ukk1lth Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,16 +1,17 @@
-int result = operand[0];
-        for (int i = 0; i < operator.size(); ++i) {
-            if (operator[i] == "+") {
-                result += operand[i + 1];
-            } else if (operator[i] == "-") {
-                result -= operand[i + 1];
-            } else if (operator[i] == "*") {
-                result *= operand[i + 1];
-            } else if (operator[i] == "//") {
-                result /= operand[i + 1];
-            } else if (operator[i] == "**") {
-                result = pow(result, operand[i + 1]);
-            }
+{
+    int result = operand[0];
+    for (int i = 0; i < operato.size(); ++i) {
+        if (operato[i] == "+") {
+            result += operand[i + 1];
+        } else if (operato[i] == "-") {
+            result -= operand[i + 1];
+        } else if (operato[i] == "*") {
+            result *= operand[i + 1];
+        } else if (operato[i] == "//") {
+            result /= operand[i + 1];
+        } else if (operato[i] == "**") {
+            result = pow(result, operand[i + 1]);
         }
-        return result;
     }
+    return result;
+}
