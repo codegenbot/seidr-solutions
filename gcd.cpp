44ea85394383f291eecfc84369636bb720e259b8
beep boop <githubbot@vadim.me>
2024-07-16 11:38:15 +0200

    Solution 52 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/ajre3ytd Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from c020ce2f-e95e-441e-9ca7-bc33e6711ab8.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- gcd.cpp
+++ gcd.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-#include <string>
-using namespace std;
-
 int findGCD(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -9,12 +5,4 @@ int findGCD(int a, int b) {
         a = temp;
     }
     return a;
-}
-
-int main() {
-    int num1, num2;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    cout << "GCD of " << num1 << " and " << num2 << " is: " << findGCD(num1, num2) << endl;
-    return 0;
 }
