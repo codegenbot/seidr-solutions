0b896d970439164a9b25d65c25dfaf398e0ec299
beep boop <githubbot@vadim.me>
2024-07-16 05:20:11 +0200

    Solution 47 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/n2puv9bj Prompt:       |                    ^~~~~ 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp:9: 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp: In function ‘int main()’: 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 6d3ede6c-0ffe-4408-bde8-f862b6eed766.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -2,13 +2,7 @@
 
 bool issame(vector<float> a, vector<float> b);
 
-vector<float> derivative(vector<float> xs){
-    vector<float> result;
-    for(int i = 1; i < xs.size(); i++){
-        result.push_back(xs[i] * i);
-    }
-    return result;
-}
+vector<float> derivative(vector<float> xs);
 
 bool issame(vector<float> a, vector<float> b){
     if(a.size() != b.size()){
@@ -22,8 +16,21 @@ bool issame(vector<float> a, vector<float> b){
     return true;
 }
 
+vector<float> derivative(vector<float> xs){
+    vector<float> result;
+    for(int i = 1; i < xs.size(); i++){
+        result.push_back(xs[i] * i);
+    }
+    return result;
+}
+
 int main(){
     vector<float> input = {1.0, 2.0, 3.0, 4.0};
-    vector<float> output = derivative(input);
-    return 0;
+    vector<float> result = derivative(input);
+    vector<float> expected = {2.0, 6.0, 12.0};
+    if(issame(result, expected)){
+        return 0;
+    } else {
+        return 1;
+    }
 }
