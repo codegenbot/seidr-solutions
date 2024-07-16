3fc11d14d83ef96c769e8725fcbfe6d1e864789f
beep boop <githubbot@vadim.me>
2024-07-16 03:15:50 +0200

    Solution 26 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dx1ic3yv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dec0d02-cd7f-4829-bbfa-b2201f0c1 720.py", line 15, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dec0d02-cd7f-4829-bbfa-b2201f0c1 720.py", line 12, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,10 +1,10 @@
 def solve(N):
     if N == 0:
         return "0"
-    if N < 0:
+    elif N < 0:
         return "-" + solve(-N)
     elif type(N) != int:
-        return 
+        return "Error: Input must be an integer."
     else:
         s = format(abs(N), 'b').zfill(len(str(bin(abs(N))[2:]))).lstrip("0")
         return "0" if s == "" else s
