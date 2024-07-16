62feb3e30364473c413270fd28d5c1e218eae1a0
beep boop <githubbot@vadim.me>
2024-07-16 13:57:39 +0200

    Solution 73 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jnq23dc3 Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,3 @@
 def find_zero(xs: list):
-    for i in range(len(xs) - 1):
-        if xs[i] != 0:
-            leading_coefficient = xs[i]
-            break
-    return -xs[-1] / leading_coefficient
+    n = len(xs) - 1
+    return -xs[-2] / xs[-1]
