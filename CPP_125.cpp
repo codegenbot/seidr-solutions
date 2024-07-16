c5a83796ea3c6c1b57bf50a17e1b229a3d5b99af
beep boop <githubbot@vadim.me>
2024-07-16 09:01:06 +0200

    Solution 43 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1nqs6oqj Prompt:       |                              ^~~~~~ 672a3a81-0950-4535-91ae-2230c4f913b9.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 672a3a81-0950-4535-91ae-2230c4f913b9.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 672a3a81-0950-4535-91ae-2230c4f913b9.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 672a3a81-0950-4535-91ae-2230c4f913b9.cpp:30: 672a3a81-0950-4535-91ae-2230c4f913b9.cpp: In function ‘int main()’: 672a3a81-0950-4535-91ae-2230c4f913b9.cpp:40:20: error: ‘split_words’ was not dec lared in this scope    40 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 672a3a81-0950-4535-91ae-2230c4f913b9.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -7,7 +7,7 @@ bool issame(vector<string> a, vector<string> b){
     if(a.size() != b.size()){
         return false;
     }
-    for(int i=0; i<a.size(); i++){
+    for(int i = 0; i < a.size(); i++){
         if(a[i] != b[i]){
             return false;
         }
@@ -45,6 +45,6 @@ vector<string> split_words(string txt){
 }
 
 int main() {
-    // Your main function code here
+    // Main function
     return 0;
 }
