4ce89af8d320205a3b173f4dd1aed896309015b2
beep boop <githubbot@vadim.me>
2024-07-16 13:17:23 +0200

    Solution 8 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/cdgxr9oh Prompt: Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -3,37 +3,14 @@ using namespace std;
 
 class Solution {
 public:
-    string numberToWords(int num) {
-        if (num == 0) return "Zero";
-        map<int, string> ones = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"}, 
-                                  {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-        map<int, string> teens = {{10, "Ten"}, {11, "Eleven"}, {12, "Twelve"}, {13, "Thirteen"}, 
-                                   {14, "Fourteen"}, {15, "Fifteen"}, {16, "Sixteen"}, {17, "Seventeen"}, 
-                                   {18, "Eighteen"}, {19, "Nineteen"}};
-        map<int, string> tens = {{20, "Twenty"}, {30, "Thirty"}, {40, "Forty"}, {50, "Fifty"}, 
-                                  {60, "Sixty"}, {70, "Seventy"}, {80, "Eighty"}, {90, "Ninety"}};
-        if (num >= 100) {
-            return hundreds(num) + " Hundred" + (num % 100 ? " and " : "") + numberToWords(num % 100);
-        } else if (num >= 20) {
-            return tens[num / 10] + (num % 10 ? " " + ones[num % 10] : "");
-        } else if (num >= 10) {
-            return teens[num];
-        } else {
-            return ones[num];
-        }
-    }
-
-    string hundreds(int num) {
-        if (num == 0) return "";
-        if (num / 100 > 1) return ones[num / 100] + " Hundred";
-        return ones[num / 100] + " Hundred";
+    string fizzBuzz(int x) {
+        if (x % 3 == 0 && x % 5 == 0)
+            return "FizzBuzz";
+        else if (x % 3 == 0)
+            return "Fizz";
+        else if (x % 5 == 0)
+            return "Buzz";
+        else
+            return to_string(x);
     }
 };
-
-string fizzBuzz(int x) {
-    Solution s;
-    if(x % 3 == 0 && x % 5 == 0) return "FizzBuzz";
-    else if (x % 3 == 0) return "Fizz";
-    else if (x % 5 == 0) return "Buzz";
-    return to_string(x);
-}
