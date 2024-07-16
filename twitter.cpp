b4ed767dd932e341722b01c19d3688b6fc4b03fd
beep boop <githubbot@vadim.me>
2024-07-16 11:12:25 +0200

    Solution 18 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/o2oujxn5 Prompt: it must return Your tweet has 140 characters for input 2)Ipf>Q+x+~oomtxGJDB7SXj\#y<ydzy4)D9*B_%;F6RM(hlZh'oZ&VLaoL"$GbtE9$wy)qyZs>o$0}OlI4cJ,pJ=!<RuG>8|76scKr-_wyP'|y}1VOBi,?;z#kJNlawi;=LzJp^}'i1, but it returns You didn't type anything Your tweet has 1 characters Your tweet has 140 characters Your tweet has 90 characters Too many characters.

--- twitter.cpp
+++ twitter.cpp
@@ -1,6 +1,7 @@
-```cpp
 #include <iostream>
-#include <wstring>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 std::wstring validateTweet(const std::wstring& tweet) {
     if (tweet.empty()) {
@@ -16,11 +17,11 @@ std::wstring validateTweet(const std::wstring& tweet) {
 
 int main() {
     // Test cases
-    std::cout << validateTweet(L"") << std::wendl;
-    std::cout << validateTweet(L"1") << std::wendl;
-    std::cout << validateTweet(L"max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it") << std::wendl;
-    std::cout << validateTweet(L"40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)") << std::wendl;
-    std::cout << validateTweet(L"Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1") << std::wendl;
+    std::cout << validateTweet(L"") << std::wcout << std::endl;
+    std::cout << validateTweet(L"1") << std::wcout << std::endl;
+    std::cout << validateTweet(L"max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it") << std::wcout << std::endl;
+    std::cout << validateTweet(L"40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)") << std::wcout << std::endl;
+    std::cout << validateTweet(L"Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1") << std::wcout << std::endl;
 
     return 0;
 }
