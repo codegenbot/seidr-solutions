0178874b35a7a439ff2c3872f314a575f4aa323d
beep boop <githubbot@vadim.me>
2024-07-16 14:23:00 +0200

    Solution 7 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/j7ex0j47 Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
 #include <string>
+using namespace std;
 
-std::string spinWords(std::string sentence) {
-    std::string result = "";
-    std::string word;
+string spinWords(string sentence) {
+    string result = "";
+    string word;
     
     for (char c : sentence) {
         if (c == ' ') {
-            if (word.length() >= 5) {
-                std::reverse(word.begin(), word.end());
-            }
             result += word + " ";
             word = "";
         } else {
@@ -17,23 +14,51 @@ std::string spinWords(std::string sentence) {
         }
     }
     
-    if (word.length() >= 5) {
-        std::reverse(word.begin(), word.end());
-    }
     result += word;
+    
+    int len;
+    string revWord;
+    
+    for (string s : split(result, " ")) {
+        len = s.length();
+        
+        if (len >= 5) {
+            revWord = s;
+            reverse(revWord.begin(), revWord.end());
+            result = replace(result, s, revWord);
+        }
+    }
     
     return result;
 }
 
-int main() {
-    std::string sentence;
-    while (true) {
-        std::cout << "Enter a sentence (or 'q' to quit): ";
-        std::cin >> sentence;
-        if (sentence == "q") {
-            break;
-        }
-        std::cout << spinWords(sentence) << std::endl;
+string replace(string &str, string oldVal, string newVal) {
+    size_t pos = 0;
+    while((pos = str.find(oldVal)) != string::npos) {
+        str.replace(pos, oldVal.length(), newVal);
+        pos += newVal.length();
     }
+    return str;
+}
+
+vector<string> split(const string &s, char delim) {
+    vector<string> result;
+    size_t pos = 0;
+    size_t lastPos = s.find(delim);
+
+    while (string::npos != lastPos) {
+        result.push_back(s.substr(pos, lastPos - pos));
+        pos = lastPos + 1;
+        lastPos = s.find(delim, pos);
+    }
+    result.push_back(s.substr(pos));
+    return result;
+}
+
+int main() {
+    cout << spinWords("a") << endl; // a
+    cout << spinWords("this is a test") << endl; // this is a test
+    cout << spinWords("this is another test") << endl; // this is rehtona test
+    cout << spinWords("hi") << endl; // hi
     return 0;
 }
