a37d6d24ccb7ba1a3e159e090939be3606c88c2e
beep boop <githubbot@vadim.me>
2024-07-16 06:20:42 +0200

    Solution 30 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/mj58376v Prompt: 7ff980ad-6439-402c-8147-e2aa6fe0e98c.cpp:4:16: error: ‘string’ was not declared in this scope     4 | int mastermind(string code, string guess) {       |                ^~~~~~ 7ff980ad-6439-402c-8147-e2aa6fe0e98c.cpp:2:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     1 | #include <vector>   +++ |+#include <string>     2 | using namespace std; 7ff980ad-6439-402c-8147-e2aa6fe0e98c.cpp:4:29: error: ‘string’ was not declared in this scope     4 | int mastermind(string code, string guess) {       |                             ^~~~~~ 7ff980ad-6439-402c-8147-e2aa6fe0e98c.cpp:4:29: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 7ff980ad-6439-402c-8147-e2aa6fe0e98c.cpp:4:41: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
-
+    
     for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
             black++;
         }
     }
-
+    
     for(int i=0; i<6 && black<4; i++) {
         int count = 0;
         for(int j=0; j<4; j++) {
@@ -25,6 +25,5 @@ int mastermind(string code, string guess) {
             black += count;
         }
     }
-
+    
     return black;
-}
