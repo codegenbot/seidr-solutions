927668fd64b1b125e1d9bfbfd176a821ea04255b
beep boop <githubbot@vadim.me>
2024-07-16 08:58:10 +0200

    Solution 38 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gskb9992 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8bc6c9c5-82f9-4acf-9299-df708f4721a4.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8bc6c9c5-82f9-4acf-9299-df708f4721a4.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8bc6c9c5-82f9-4acf-9299-df708f4721a4.cpp:5: 8bc6c9c5-82f9-4acf-9299-df708f4721a4.cpp: In function ‘int main()’: 8bc6c9c5-82f9-4acf-9299-df708f4721a4.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 8bc6c9c5-82f9-4acf-9299-df708f4721a4.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -3,12 +3,14 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
+    vector<int> result;
+
     int n = grid.size();
     int m = grid[0].size();
-    
+
     vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
     dp[0][0] = grid[0][0];
-    
+
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             if (i > 0) {
@@ -19,19 +21,22 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
             }
         }
     }
-    
-    vector<int> result;
+
+    int cost = k;
     int x = n - 1, y = m - 1;
-    
     while (x >= 0 && y >= 0) {
         result.push_back(grid[x][y]);
-        if (x > 0 && dp[x][y] - grid[x][y] == dp[x - 1][y]) {
+        if (x > 0 && cost - grid[x][y] >= 0 && dp[x - 1][y] == cost - grid[x][y]) {
             x--;
-        } else {
+            cost -= grid[x][y];
+        } else if (y > 0 && cost - grid[x][y] >= 0 && dp[x][y - 1] == cost - grid[x][y]) {
             y--;
+            cost -= grid[x][y];
+        } else {
+            break;
         }
     }
-    
+
     reverse(result.begin(), result.end());
     return result;
 }
