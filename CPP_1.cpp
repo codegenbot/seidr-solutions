aea2ad6477311db9568c66ae355935758ab897e9
beep boop <githubbot@vadim.me>
2024-07-16 03:49:26 +0200

    Solution 56 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1vr8zy9e Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:28: 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp: In function ‘int main()’: 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:38:20: error: ‘separate_paren_groups’ w as not declared in this scope    38 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 6933d0cc-09ee-4de4-a269-2eda36eee398.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -2,13 +2,11 @@ bool issame(vector<string> vec1, vector<string> vec2) {
     if (vec1.size() != vec2.size()) {
         return false;
     }
-
-    for (size_t i = 0; i < vec1.size(); ++i) {
+    for (int i = 0; i < vec1.size(); i++) {
         if (vec1[i] != vec2[i]) {
             return false;
         }
     }
-
     return true;
 }
 
@@ -18,12 +16,12 @@ int main() {
     string input;
     cin >> input;
     vector<string> result = separate_paren_groups(input);
-    vector<string> expected_result = {"(ab)(cd)", "(ef)"};
-
+    vector<string> expected_result = {"(abc)", "(def)"};
+    
     if (issame(result, expected_result)) {
-        cout << "The results match the expected output." << endl;
+        cout << "Result is correct" << endl;
     } else {
-        cout << "The results do not match the expected output." << endl;
+        cout << "Result is incorrect" << endl;
     }
 
     return 0;
