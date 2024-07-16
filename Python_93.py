4fa3a62d066f63f70464a8b772c1142a96b024a8
beep boop <githubbot@vadim.me>
2024-07-16 03:58:32 +0200

    Solution 74 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3242383e-e844-4dcd-92be-7eddf5a93 a11.py", line 25, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3242383e-e844-4dcd-92be-7eddf5a93 a11.py", line 21, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -5,13 +5,9 @@ def encode(message):
         if char.isspace():
             result += char
         elif not char.isalpha():
-            result += char
+            result += char  
         else:
             ascii_offset = 65 if char.isupper() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
             result += chr(shifted_char_code)
-            if ord('A') <= ascii_offset and char.isupper():
-                result += 'R'
-            elif ord('a') <= ascii_offset and char.islower():
-                result += 'r'
     return result
