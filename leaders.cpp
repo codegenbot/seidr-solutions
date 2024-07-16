78b03895a48d41080b9766fc919c997e878e13d8
beep boop <githubbot@vadim.me>
2024-07-17 01:50:12 +0200

    Solution 40 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/db3hs28h Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- leaders.cpp
+++ leaders.cpp
@@ -4,20 +4,22 @@ using namespace std;
 vector<int> leaders(vector<int>& v) {
     int n = v.size();
     vector<int> res;
+    int maxR = v[n-1];
     for(int i=n-1; i>=0; i--) {
-        bool leader = true;
-        for(int j=i+1; j<n; j++) {
-            if(v[j] >= v[i]) {
-                leader = false;
-                break;
-            }
+        if(v[i] >= maxR) {
+            res.push_back(v[i]);
+            maxR = v[i];
         }
-        if(leader) res.push_back(v[i]);
     }
+    reverse(res.begin(), res.end());
     return res;
-
 }
 
 int main() {
+    vector<int> v = {1, 3, 4, 2};
+    vector<int> result = leaders(v);
+    for(int i: result) {
+        cout << i << " ";
+    }
     return 0;
 }
