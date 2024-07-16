4eb010cbcae33b8ec4f01552218260c79bbf078d
beep boop <githubbot@vadim.me>
2024-07-16 08:32:31 +0200

    Solution 50 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/7ssq1x3t Prompt: ared in this scope    14 |         int leftSum = accumulate(nums.begin(), nums.begin() + i, 0);       |                       ^~~~~~~~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:17:27: error: ‘abs’ was not declared in  this scope    17 |         int currentDiff = abs(leftSum - rightSum);       |                           ^~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:25:9: error: ‘cout’ was not declared in  this scope    25 |         cout << nums[i] << endl;       |         ^~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:25:28: error: ‘endl’ was not declared i n this scope    25 |         cout << nums[i] << endl;       |                            ^~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:28:5: error: ‘cout’ was not declared in  this scope    28 |     cout << 0 << endl;       |     ^~~~ 436a9811-31ed-4f8e-997f-37718f683ad9.cpp:28:18: error: ‘endl’ was not declared i n this scope    28 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <numeric>
+#include <cmath>
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+    std::vector<int> nums;
+    int num;
+    
+    while (std::cin >> num) {
+        nums.push_back(num);
     }
     
-    int sum = 0;
-    for (int num : nums) {
-        sum += num;
-    }
+    int n = nums.size();
+    int diff = INT_MAX;
+    int cutIdx = 0;
     
-    int halfSum = sum / 2;
-    int currSum = 0;
-    int idx = 0;
-    for (int i = 0; i < n; ++i) {
-        currSum += nums[i];
-        if (currSum >= halfSum) {
-            idx = i;
-            break;
+    for (int i = 1; i < n; ++i) {
+        int leftSum = std::accumulate(nums.begin(), nums.begin() + i, 0);
+        int rightSum = std::accumulate(nums.begin() + i, nums.end(), 0);
+        
+        int currentDiff = std::abs(leftSum - rightSum);
+        if (currentDiff < diff) {
+            diff = currentDiff;
+            cutIdx = i;
         }
     }
     
-    vector<int> subVec1(nums.begin(), nums.begin() + idx + 1);
-    vector<int> subVec2(nums.begin() + idx + 1, nums.end());
-    
-    for (int num : subVec1) {
-        cout << num << endl;
+    for (int i = 0; i < cutIdx; ++i) {
+        std::cout << nums[i] << std::endl;
     }
     
-    for (int num : subVec2) {
-        cout << num << endl;
+    std::cout << 0 << std::endl;
+    
+    for (int i = cutIdx; i < n; ++i) {
+        std::cout << nums[i] << std::endl;
     }
     
     return 0;
