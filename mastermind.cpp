fefc9db271f399e900dcf28d5810ba68d78248ce
beep boop <githubbot@vadim.me>
2024-07-16 15:19:30 +0200

    Solution 73 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p9s49ghj Prompt: 1605d4b6-6297-4301-bda0-83203d3f3561.cpp: In function ‘int main()’: 1605d4b6-6297-4301-bda0-83203d3f3561.cpp:31:14: error: ‘white’ was not declared in this scope; did you mean ‘fwrite’?    31 |     std::tie(white, black) = mastermind(code, guess);       |              ^~~~~       |              fwrite 1605d4b6-6297-4301-bda0-83203d3f3561.cpp:31:21: error: ‘black’ was not declared in this scope    31 |     std::tie(white, black) = mastermind(code, guess);       |                     ^~~~~ 1605d4b6-6297-4301-bda0-83203d3f3561.cpp:33:14: error: expected ‘}’ at end of in put    33 |     return 0;       |              ^ 1605d4b6-6297-4301-bda0-83203d3f3561.cpp:28:12: note: to match this ‘{’    28 | int main() {       |            ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,31 +1,3 @@
-```cpp
-#include <tuple>
-#include <vector>
-#include <iostream>
-#include <string>
-
-std::tuple<int, int> mastermind(std::string code, std::string guess) {
-    int white = 0;
-    int black = 0;
-
-    for (int i = 0; i < 4; i++) {
-        bool inCode = false;
-        for (int j = 0; j < 4; j++) {
-            if (guess[i] == code[j]) { 
-                inCode = true;
-                if (j == i) { 
-                    black++;
-                    break;
-                } else {
-                    white++;
-                }
-            }
-        }
-    }
-
-    return std::make_tuple(white, black);
-}
-
 int main() {
     std::string code = "RGOR";
     std::string guess = "BRBB";
