58209cafaa41a30c2bc53d2e6548bbd8b7a4b32c
beep boop <githubbot@vadim.me>
2024-07-16 15:35:48 +0200

    Solution 81 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p9s49ghj Prompt:       |                                               ^~~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:5:30: error: ‘mastermind’ was not decla red in this scope     5 |     std::tie(white, black) = mastermind(code, guess);       |                              ^~~~~~~~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:6:10: error: ‘cout’ is not a member of ‘std’     6 |     std::cout << "White pegs: " << white << ", Black pegs: " << black <<  std::endl;       |          ^~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:1:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?   +++ |+#include <iostream>     1 | int main() { 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:6:79: error: ‘endl’ is not a member of ‘std’     6 | t << "White pegs: " << white << ", Black pegs: " << black << std::endl;       |                                                                   ^~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | int main() {

--- mastermind.cpp
+++ mastermind.cpp
@@ -5,11 +5,16 @@ int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    for (int i = 0; i < 4; i++) {
-        if (code[i] == guess[i]) {
+    for (int i = 0; i < 4; ++i) {
+        if (guess[i] == code[i]) {
             black++;
-        } else if (count(guess.begin(), guess.end(), code[i]) > 0) {
-            white++;
+        } else {
+            for (int j = 0; j < 4; ++j) {
+                if (guess[i] == code[j] && i != j) {
+                    white++;
+                    break;
+                }
+            }
         }
     }
 
