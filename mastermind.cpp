2b813b91bb2bd2b36830de8e410b1b9a8d9d6e56
beep boop <githubbot@vadim.me>
2024-07-16 12:15:59 +0200

    Solution 0 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p9s49ghj Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,28 @@
+#include <vector>
+#include <iostream>
 #include <string>
-#include <unordered_map>
 
-int blackPegs(std::string code, std::string guess) {
-    int blackCount = 0;
-    int whiteCount = 0;
-    
-    std::unordered_map<char,int> codeMap;
-    for(int i=0; i<4; i++) {
-        if(code[i] == guess[i]) {
-            blackCount++;
+int mastermind(string code, string guess) {
+    int white = 0;
+    int black = 0;
+
+    vector<int> codeCount(6, 0);
+    vector<int> guessCount(6, 0);
+
+    for (int i = 0; i < 4; i++) {
+        if (code[i] == guess[i]) {
+            black++;
+            codeCount[code[i] - 'A']++;
+            guessCount[guess[i] - 'A']++;
         } else {
-            if(codeMap.find(guess[i]) != codeMap.end()) {
-                codeMap[guess[i]]++;
-            }
+            codeCount[code[i] - 'A']++;
+            guessCount[guess[i] - 'A']++;
         }
     }
-    
-    for(auto it = codeMap.begin(); it != codeMap.end(); it++) {
-        whiteCount += std::min(it->second, 1);
+
+    for (int i = 0; i < 6; i++) {
+        white += min(codeCount[i], guessCount[i]) - black;
     }
-    
-    return blackCount + whiteCount;
+
+    return make_tuple(white, black);
 }
