7f4edc411babbae5872390d80d17f316140b1138
beep boop <githubbot@vadim.me>
2024-07-16 09:26:12 +0200

    Solution 20 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/szzx4ujr Prompt:       |                    ^~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 59c21e69-dee1-437d-86ba-461b55998a18.cpp:13: 59c21e69-dee1-437d-86ba-461b55998a18.cpp: In function ‘int main()’: 59c21e69-dee1-437d-86ba-461b55998a18.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 59c21e69-dee1-437d-86ba-461b55998a18.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,10 +1,8 @@
 #include <vector>
-#include <string>
-#include <cassert>
 
-std::vector<int> even_odd_count(int num) {
-    std::vector<int> result(2, 0);
-    std::string numStr = std::to_string(std::abs(num));
+vector<int> even_odd_count(int num) {
+    vector<int> result(2, 0);
+    string numStr = to_string(abs(num));
     for (char c : numStr) {
         if ((c - '0') % 2 == 0) {
             result[0]++;
@@ -13,14 +11,4 @@ std::vector<int> even_odd_count(int num) {
         }
     }
     return result;
-}
-
-bool issame(std::vector<int> v1, std::vector<int> v2) {
-    assert(v1.size() == v2.size());
-    for (size_t i = 0; i < v1.size(); ++i) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
 }
