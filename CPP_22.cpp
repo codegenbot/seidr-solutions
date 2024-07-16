4fe8cdd00156d3916bdea119d5acc44bfdc797c1
beep boop <githubbot@vadim.me>
2024-07-16 03:44:54 +0200

    Solution 18 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niur2qrc Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ aa5418d6-1d95-4f0f-980a-9012ea14bee5.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ aa5418d6-1d95-4f0f-980a-9012ea14bee5.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from aa5418d6-1d95-4f0f-980a-9012ea14bee5.cpp:11: aa5418d6-1d95-4f0f-980a-9012ea14bee5.cpp: In function ‘int main()’: aa5418d6-1d95-4f0f-980a-9012ea14bee5.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ aa5418d6-1d95-4f0f-980a-9012ea14bee5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,12 +1,20 @@
 #include <vector>
 #include <boost/any.hpp>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-std::vector<int> filter_integers(std::list<boost::any> values){
-    std::vector<int> result;
+vector<int> filter_integers(list_any values){
+    vector<int> result;
     for (auto val : values) {
         if (val.type() == typeid(int)) {
             result.push_back(boost::any_cast<int>(val));
