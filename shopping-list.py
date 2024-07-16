ebff44812da7636a5d354b2eafbf4bac2b6c62f8
beep boop <githubbot@vadim.me>
2024-07-16 22:59:47 +0200

    Solution 24 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/cix2qqmx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1900fa5b-cd46-46d6-873c-6f90eab59 b83.py", line 1     ```     ^ SyntaxError: invalid syntax

--- shopping-list.py
+++ shopping-list.py
@@ -1,10 +1,11 @@
-```
 n = int(input())
 prices = list(map(float, input().strip().split()))
 discounts = [list(map(float, input().strip().split())) for _ in range(n)]
 
+
 def calculate_total_price():
-    total_price = sum((price * (100 - d) / 100) for price, *d in zip(prices, *discounts))
+    total_price = sum((price * (100 - d) / 100) for price, d in zip(prices, *discounts))
     print(format(total_price, ".1f"))
+
 
 calculate_total_price()
