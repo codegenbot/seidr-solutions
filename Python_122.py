e5dc0207630a833a2ccf9125cfc81fca326646cc
beep boop <githubbot@vadim.me>
2024-07-16 05:45:22 +0200

    Solution 55 of Python/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xwap3zk0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0978893c-059d-46e7-823e-61b3fa8f4 c50.py", line 14, in <module>     check(add_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0978893c-059d-46e7-823e-61b3fa8f4 c50.py", line 10, in check     add_elements([1], 1) == 1 AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_122.py
+++ Python_122.py
@@ -1,9 +1,5 @@
 def add_elements(arr, k=0):
     filtered_nums = [num for num in arr if 10 <= num <= 100]
-    
-    if not filtered_nums:
-        return 0
-    
-    filtered_nums.sort(reverse=True)
-   
-    return sum(filtered_nums[:k] if k > 0 and k <= len(filtered_nums) else filtered_nums)
+    if k == len(filtered_nums):
+        return sum(filtered_nums)
+    return sum(sorted(filtered_nums, reverse=True)[:k]) if k >= 1 else filtered_nums[0] if filtered_nums else 0
