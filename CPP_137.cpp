d974a5d33d7171c4bac1662cf9a873319e88b7d1
beep boop <githubbot@vadim.me>
2024-07-16 17:50:09 +0200

    Solution 10 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/crve8xko Prompt:       |                                              ^ 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp:35:46: error: expected primary-expressi on before ‘>’ token    35 |         num2 = std::stof(any_cast<std::string>(b));       |                                              ^ In file included from 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp:48: 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp: In function ‘int main()’: 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp:50:13: error: ‘boost’ has not been decl ared    50 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp:50:35: error: expected primary-expressi on before ‘>’ token    50 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp: In function ‘std::any compare_one(std: :any, std::any)’: 8bffc390-6cfe-4d01-8330-f55c92a03067.cpp:46:1: warning: control reaches end of n on-void function [-Wreturn-type]    46 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,32 +1,46 @@
-#include <any>
+#include <boost/any.hpp>
 #include <string>
 #include <algorithm>
 #include <cassert>
-using std::any;
+
+using boost::any;
 using std::string;
 
 any compare_one(any a, any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return any_cast<int>(a) > any_cast<int>(b) ? a : b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return any_cast<float>(a) > any_cast<float>(b) ? a : b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = any_cast<string>(a);
-        string str_b = any_cast<string>(b);
-        
-        if (str_a.find(",") != string::npos) {
-            std::replace(str_a.begin(), str_a.end(), ',', '.');
+    if(a.type() == typeid(int) && b.type() == typeid(int)){
+        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+            return a;
+        } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+            return b;
+        } else {
+            return any();
         }
-        if (str_b.find(",") != string::npos) {
-            std::replace(str_b.begin(), str_b.end(), ',', '.');
+    } else if(a.type() == typeid(float) && b.type() == typeid(float)){
+        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+            return a;
+        } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+            return b;
+        } else {
+            return any();
         }
-        
-        float num1 = std::stof(str_a);
-        float num2 = std::stof(str_b);
-        
-        return num1 > num2 ? a : (num1 < num2 ? b : any());
+    } else if(a.type() == typeid(string) && b.type() == typeid(string)){
+        float num1, num2;
+        if(boost::any_cast<std::string>(a).find(",") != string::npos){
+            std::replace(boost::any_cast<std::string&>(boost::any_cast<string&>(a)).begin(), boost::any_cast<std::string&>(boost::any_cast<string&>(a)).end(), ',', '.');
+        }
+        if(boost::any_cast<std::string>(b).find(",") != string::npos){
+            std::replace(boost::any_cast<std::string&>(boost::any_cast<string&>(b)).begin(), boost::any_cast<std::string&>(boost::any_cast<string&>(b)).end(), ',', '.');
+        }
+        num1 = std::stof(boost::any_cast<std::string>(a));
+        num2 = std::stof(boost::any_cast<std::string>(b));
+        if(num1 > num2){
+            return a;
+        } else if(num1 < num2){
+            return b;
+        } else {
+            return any();
+        }
+    } else {
+        return any();
     }
-    return any();
 }
