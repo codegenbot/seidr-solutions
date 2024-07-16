bdfb0d385f85535b9abe0fb7a48ff17d2a15eefd
beep boop <githubbot@vadim.me>
2024-07-16 10:47:08 +0200

    Solution 70 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jb54hx4z Prompt: 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions) {       | ^~~~~~ In file included from 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp:22: 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp: In function ‘int main()’: 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp:24:13: error: ‘Strongest_Extension’ was  not declared in this scope    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <cassert>
 
-using namespace std;
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions);
 
-string Strongest_Extension(string class_name, vector<string> extensions){
-    string strongest_extension = "";
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
+    std::string strongest_extension;
     int max_strength = INT_MIN;
-
-    for (string extension : extensions) {
-        int CAP = 0, SM = 0;
-        for (char c : extension) {
-            if (isupper(c)) CAP++;
-            else if (islower(c)) SM++;
+    
+    for(const auto& ext : extensions){
+        int cap_count = 0, sm_count = 0;
+        for(char c : ext){
+            if(isupper(c)) cap_count++;
+            else if(islower(c)) sm_count++;
         }
-        int strength = CAP - SM;
-        if (strength > max_strength || (strength == max_strength && strongest_extension.empty())) {
+        
+        int strength = cap_count - sm_count;
+        if(strength > max_strength){
             max_strength = strength;
-            strongest_extension = extension;
+            strongest_extension = ext;
         }
     }
-
+    
     return class_name + "." + strongest_extension;
 }
 
-int main() {
+int main(){
     assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    
     return 0;
 }
