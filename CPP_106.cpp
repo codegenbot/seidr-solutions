8ec22f4c050a04edfcd95bd313e64b39418652be
beep boop <githubbot@vadim.me>
2024-07-16 08:09:17 +0200

    Solution 65 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/s2acrrwr Prompt:       |                    ^~~ 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp:17: 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp: In function ‘int main()’: 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp:27:20: error: ‘f’ was not declared in t his scope    27 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 73c0c68a-e4c0-4f47-bd99-e2e698f11c3d.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -3,8 +3,20 @@
 
 bool issame(std::vector<int> a, std::vector<int> b);
 
-std::vector<int> f(int n){
-    std::vector<int> result;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> f(int n){
+    vector<int> result;
     int sum = 0;
     int factorial = 1;
     for (int i = 1; i <= n; ++i) {
@@ -17,10 +29,6 @@ std::vector<int> f(int n){
         }
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
 }
 
 assert(issame(f(3), {1, 2, 6}));
