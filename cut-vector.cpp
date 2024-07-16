c7cde58f885000bb9be4b5f8b954403a9fbc7d27
beep boop <githubbot@vadim.me>
2024-07-16 03:08:31 +0200

    Solution 17 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/zk87tlmx Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,21 +1,19 @@
 int main() {
     vector<int> nums;
     int num;
-    
     while (cin >> num) {
         nums.push_back(num);
     }
-    
+
     int n = nums.size();
     int sum = 0;
     for (int i = 0; i < n; i++) {
         sum += nums[i];
     }
-    
+
     int half_sum = sum / 2;
     int prefix_sum = 0;
     int cut_index = -1;
-    
     for (int i = 0; i < n; i++) {
         prefix_sum += nums[i];
         if (prefix_sum >= half_sum) {
@@ -23,20 +21,16 @@ int main() {
             break;
         }
     }
-    
-    if (abs(sum - 2 * prefix_sum) < abs(sum - 2 * (prefix_sum - nums[cut_index]))) {
-        cout << "1" << endl;
-        for (int i = 0; i <= cut_index; i++) {
-            cout << nums[i] << endl;
-        }
-        cout << "0" << endl;
-    } else {
-        cout << "1" << endl;
-        for (int i = 0; i < cut_index; i++) {
-            cout << nums[i] << endl;
-        }
-        cout << "0" << endl;
+
+    vector<int> subvector1(nums.begin(), nums.begin() + cut_index + 1);
+    vector<int> subvector2(nums.begin() + cut_index + 1, nums.end());
+
+    for (int num : subvector1) {
+        cout << num << endl;
     }
-    
+    for (int num : subvector2) {
+        cout << num << endl;
+    }
+
     return 0;
 }
