df3b5c9723375525577108469259de57cb9917ef
beep boop <githubbot@vadim.me>
2024-07-16 16:23:37 +0200

    Solution 66 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/bq2p4pp5 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ ce0fdb00-6f93-4638-b7cd-8c7927454477.cpp:22:21:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -6,7 +6,7 @@
 double vectorDistance(int n, const std::vector<float>& v1, const std::vector<float>& v2) {
     double sum = 0;
     for (int i = 0; i < n; i++) {
-        sum += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+        sum += pow(v1[i] - v2[i], 2);
     }
     return sqrt(sum);
 }
@@ -14,12 +14,12 @@ double vectorDistance(int n, const std::vector<float>& v1, const std::vector<flo
 int main() {
     int n;
     std::cin >> n;
-    std::vector<float> v1(n), v2(n);
+    std::vector<float> v1, v2;
     for (int i = 0; i < n; i++) {
-        std::cin >> v1[i];
-    }
-    for (int i = 0; i < n; i++) {
-        std::cin >> v2[i];
+        float val1, val2;
+        std::cin >> val1 >> val2;
+        v1.push_back(val1);
+        v2.push_back(val2);
     }
     std::cout << std::fixed << std::setprecision(10) << vectorDistance(n, v1, v2) << std::endl;
     return 0;
