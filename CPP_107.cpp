1545ae1bff7bd68eda53270012d9e7d2f20c80ce
beep boop <githubbot@vadim.me>
2024-07-16 09:10:11 +0200

    Solution 79 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/685treyk Prompt:       |                    ^~~ 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp:17: 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp: In function ‘int main()’: 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 2abacb8a-8d46-4bd3-a595-eb58771274ea.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,12 +1,11 @@
 #include <vector>
-#include <algorithm>
 
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> result(2, 0);
+vector<int> even_odd_palindrome(int n) {
+    vector<int> result(2, 0);
     for (int i = 1; i <= n; ++i) {
-        std::string str = std::to_string(i);
-        std::string rev_str = str;
-        std::reverse(rev_str.begin(), rev_str.end());
+        string str = to_string(i);
+        string rev_str = str;
+        reverse(rev_str.begin(), rev_str.end());
         if (str == rev_str) {
             if (i % 2 == 0) {
                 ++result[0];
