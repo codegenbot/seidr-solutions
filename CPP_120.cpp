fefd04ec4c2b110d2062766252a7767ef8982cec
beep boop <githubbot@vadim.me>
2024-07-16 07:35:11 +0200

    Solution 34 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/okypfm2k Prompt:       |                    ^~~ 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp:9: 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp: In function ‘int main()’: 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 9518d1bb-dcd5-4188-8ed9-b02d2d4d68a2.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,15 +2,7 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<int> maximum(std::vector<int> arr, int k){
@@ -24,4 +16,5 @@ std::vector<int> maximum(std::vector<int> arr, int k){
 
 int main(){
     assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
+    return 0;
 }
