734d1358d3cfc0c27d453bb9e76e231ac97fcb6c
beep boop <githubbot@vadim.me>
2024-07-16 08:19:25 +0200

    Solution 70 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/utfchmtc Prompt:       |                    ^~~ 242309b0-32e2-4341-b38a-61db7c747002.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 242309b0-32e2-4341-b38a-61db7c747002.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 242309b0-32e2-4341-b38a-61db7c747002.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 242309b0-32e2-4341-b38a-61db7c747002.cpp:21: 242309b0-32e2-4341-b38a-61db7c747002.cpp: In function ‘int main()’: 242309b0-32e2-4341-b38a-61db7c747002.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 242309b0-32e2-4341-b38a-61db7c747002.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-vector<int> unique_digits(vector<int> x) {
+vector<int> unique_digits(vector<int> x){
     vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
