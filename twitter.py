ef72eaa4da6bbd319bee61b34d682bf6edb74f1d
beep boop <githubbot@vadim.me>
2024-07-16 23:56:20 +0200

    Solution 49 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8tr8sey0 Prompt: it must return Your tweet has 135 characters for input r]REf[NNh0:E(2+-d0Q1!aff^GHM&d@'pl`NUPKL=QEjt)w5pHZ@x;bh5s4v3JY27t^z\Inzl(0M.*Yz'7*S>7-6;3Q[p!rt'APZ^*f60%JMe-dwhiOb51,v)eq'0@A9|7\U$ao, but it returns .

--- twitter.py
+++ twitter.py
@@ -1,8 +1,7 @@
 def validate_tweet(tweet):
-    tweet = ''.join(e for e in tweet if e.isalnum() or e.isspace())
-    if len(tweet) > 140:
+    if len(tweet.encode('utf-8')) > 140:
         return "Too many characters"
-    elif not tweet.strip():
+    elif not tweet:
         return "You didn't type anything"
     else:
         return f"Your tweet has {len(tweet)} characters"
