49eb604f58004de01e227a6440c3e3959b6d1434
beep boop <githubbot@vadim.me>
2024-07-16 03:35:14 +0200

    Solution 52 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb039143-e2eb-4734-a46d-c7aef003e afa.py", line 19, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb039143-e2eb-4734-a46d-c7aef003e afa.py", line 15, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,9 +2,11 @@ def encode(message):
     result = ""
     for char in message:
         if char.isspace():
-            result += char  
-        elif char.isalnum() or char.isspace():  
-            ascii_offset = 65 if not char.islower() else 97
+            result += " "
+        elif char.isalpha():
+            ascii_offset = 65 if char.isupper() else 97
             shifted_char_code = (ord(char) - ascii_offset + 3) % 26 + ascii_offset
             result += chr(shifted_char_code)
+        else:
+            result += char  
     return result
