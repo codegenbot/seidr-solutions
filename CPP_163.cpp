972e334dc2ecedaf45bcb193a67bf7501ee26ec7
beep boop <githubbot@vadim.me>
2024-07-16 03:55:08 +0200

    Solution 34 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rbzodm96 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp:16: 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp: In function ‘int main()’: 0dcc41f7-a34d-4414-98e6-abb610cd4adc.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
+bool is_same(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
@@ -11,9 +11,4 @@ std::vector<int> generate_integers(int start, int end){
         result.push_back(i);
     }
     return result;
-}
-
-int main() {
-    assert(issame(generate_integers(17, 89), {}));
-    return 0;
 }
