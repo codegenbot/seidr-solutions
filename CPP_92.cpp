b158f62e64e7888ef47849790634d0dded1a2422
beep boop <githubbot@vadim.me>
2024-07-16 14:44:16 +0200

    Solution 83 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/hqy64owr Prompt: Complete the following code given the task description and function signature.

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,5 +1,5 @@
-if((a == b + c) || (b == a + c) || (c == a + b))
-        return true;
+if (a == b + c || b == a + c || c == a + b)
+        return (a == (int)a && b == (int)b && c == (int)c);
     else
         return false;
 }
