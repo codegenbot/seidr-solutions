c2b4f92f192b5e6f479b2c36283ad3c4bb25b360
beep boop <githubbot@vadim.me>
2024-07-16 04:13:34 +0200

    Solution 80 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/s7x355nl Prompt:       |                    ^~~ 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp:12: 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp: In function ‘int main()’: 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 5449ff5a-3b40-4a18-9ed0-93f4aded7330.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,13 +1,28 @@
+#include <iostream>
 #include <vector>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
+using namespace std;
+
+vector<int> intersperse(vector<int> numbers, int delimeter){
+    vector<int> result;
+    for(int i = 0; i < numbers.size(); ++i){
+        result.push_back(numbers[i]);
+        if(i < numbers.size() - 1){
+            result.push_back(delimeter);
         }
     }
-    return true;
+    return result;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
+int main(){
+    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    
+    cout << "Test Passed" << endl;
+    
+    return 0;
 }
