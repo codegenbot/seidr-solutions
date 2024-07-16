7154d4c7293a52c61925ebf438166d05e6acff66
beep boop <githubbot@vadim.me>
2024-07-16 06:00:54 +0200

    Solution 29 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/g5at8u4d Prompt: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:16: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp: In function ‘int main()’: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp: In function ‘int total_chars(int)’: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,7 +2,12 @@ bool issame(const vector<string>& a, const vector<string>& b) {
     return a == b;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2);
+vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
+    if (total_chars(lst1) < total_chars(lst2)) {
+        return lst1;
+    }
+    return total_chars(lst1) == total_chars(lst2) ? lst1 : lst2;
+}
 
 int total_chars(const vector<string>& lst) {
     int total = 0;
