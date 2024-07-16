03f809ab8d077b9f8d4ac2e31bbdfa916b16663a
beep boop <githubbot@vadim.me>
2024-07-16 17:28:42 +0200

    Solution 11 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/4xntkmrb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/81236c3d-c1e8-4acb-923a-f2b82f50b 825.py", line 1     ```     ^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -3,7 +3,7 @@ def luhn(card_number):
     sum = 0
     for i in range(len(card_number)):
         if i % 2 == 1:  
-            doubled = card_number[i] * 2 
+            doubled = card_number[i] * 2
             if doubled > 9:
                 doubled -= 9
             sum += doubled
