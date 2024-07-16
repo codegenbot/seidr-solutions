774f5810ea2b609655a76bb7a85cc90b87b2d7f9
beep boop <githubbot@vadim.me>
2024-07-16 18:15:41 +0200

    Solution 45 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/crve8xko Prompt: b120684b-690a-4ebf-8ad3-60669ad92f74.cpp:44:29: error: ‘string’ was not declared  in this scope    44 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ b120684b-690a-4ebf-8ad3-60669ad92f74.cpp:44:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from b120684b-690a-4ebf-8ad3-60669ad92f74.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from b120684b-690a-4ebf-8ad3-60669ad92f74.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,9 +2,7 @@
 #include <any>
 #include <algorithm>
 #include <cassert>
-#include <string>
-
-using std::string;
+#include <string> // Add this line
 
 std::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
@@ -13,14 +11,14 @@ std::any compare_one(std::any a, std::any b) {
     else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         return std::any_cast<float>(a) > std::any_cast<float>(b) ? a : b;
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = std::any_cast<string>(a);
-        string str_b = std::any_cast<string>(b);
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        std::string str_a = std::any_cast<std::string>(a);
+        std::string str_b = std::any_cast<std::string>(b);
 
-        if (str_a.find(",") != string::npos) {
+        if (str_a.find(",") != std::string::npos) {
             std::replace(str_a.begin(), str_a.end(), ',', '.');
         }
-        if (str_b.find(",") != string::npos) {
+        if (str_b.find(",") != std::string::npos) {
             std::replace(str_b.begin(), str_b.end(), ',', '.');
         }
 
@@ -33,7 +31,7 @@ std::any compare_one(std::any a, std::any b) {
 }
 
 int main() {
-    assert(std::any_cast<string>(compare_one(string("1.2"), string("3.4"))) == "1.2");
+    assert(std::any_cast<std::string>(compare_one(std::string("1.2"), std::string("3.4"))) == "1.2");
     assert(std::any_cast<float>(compare_one(1, 3.4f)) == 3.4f);
 
     std::cout << "All test cases passed!" << std::endl;
