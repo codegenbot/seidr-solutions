c729a96757e9367a7e8888def5a7c74bad7458a0
beep boop <githubbot@vadim.me>
2024-07-16 03:40:10 +0200

    Solution 4 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/oovhb9h7 Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,29 +1,6 @@
-vector<int> l_divisible_by_three;
-    vector<int> l_not_divisible_by_three;
-    
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            l_divisible_by_three.push_back(l[i]);
-        } else {
-            l_not_divisible_by_three.push_back(l[i]);
-        }
+vector<int> res = l;
+    for (int i = 0; i < l.size(); i += 3) {
+        sort(res.begin() + i, res.begin() + i + 3);
     }
-    
-    sort(l_divisible_by_three.begin(), l_divisible_by_three.end());
-    
-    vector<int> result;
-    int idx_divisible = 0;
-    int idx_not_divisible = 0;
-    
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            result.push_back(l_divisible_by_three[idx_divisible]);
-            idx_divisible++;
-        } else {
-            result.push_back(l_not_divisible_by_three[idx_not_divisible]);
-            idx_not_divisible++;
-        }
-    }
-    
-    return result;
+    return res;
 }
