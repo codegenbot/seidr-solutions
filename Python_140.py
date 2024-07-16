dda2a419da0bae74480b71be44fd4538da3fd6a7
beep boop <githubbot@vadim.me>
2024-07-16 03:44:53 +0200

    Solution 70 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/kc5eoo8o Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1033359-f579-4802-b0ed-9bb8c43f0 e3e.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1033359-f579-4802-b0ed-9bb8c43f0 e3e.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,11 +1,13 @@
-```
 def fix_spaces(text):
-    new_text = ''
+    new_text = ""
+    previous_space = False
     for char in text:
-        if char == ' ' and new_text[-1] == ' ':
-            new_text += '-'
-        elif char == ' ':
-            new_text += '_'
+        if char == " ":
+            if not previous_space:
+                new_text += "_"
+            else:
+                new_text += "-"
         else:
             new_text += char
+        previous_space = char == " "
     return new_text
