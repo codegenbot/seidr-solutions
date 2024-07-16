d7f7ffa45549bbd4aa1bf3b4dc5d79076795959c
beep boop <githubbot@vadim.me>
2024-07-16 05:36:18 +0200

    Solution 36 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/4n2lnkcg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92c0bffc-051f-43df-8407-0ae5c29ea c78.py", line 19, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92c0bffc-051f-43df-8407-0ae5c29ea c78.py", line 16, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,12 +1,12 @@
 def match_parens(lst):
-    if lst[0] == ')':
-        return 'No'
+    if not lst or lst[0] == ")":
+        return "No"
     stack = []
-    for s in lst[1:]:
-        if s == '(':
+    for s in lst:
+        if s == "(":
             stack.append(s)
-        elif s == ')':
-            if not stack or stack[-1] != '(':
-                return 'No'
+        elif s == ")":
+            if not stack or stack[-1] != "(":
+                return "No"
             stack.pop()
-    return 'Yes' if not stack else 'No'
+    return "Yes" if not stack else "No"
