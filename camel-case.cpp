9bdf746f734b384e75cb0cb92de29233605df43e
beep boop <githubbot@vadim.me>
2024-07-16 17:29:30 +0200

    Solution 71 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/8knc5rk7 Prompt: Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,35 +1,37 @@
-#include <vector>
 #include <iostream>
 #include <string>
+using namespace std;
 
-std::string kebabToCamelCase(const std::string& str) {
-    std::vector<std::string> words;
-    size_t start = 0, end;
+string kebabToCamel(string str) {
+    string result = "";
+    string temp = "";
 
-    while ((end = str.find(' ')) != std::string::npos) {
-        words.push_back(str.substr(start, end - start));
-        start = end + 1;
+    for (char c : str) {
+        if (c == '-') {
+            result += char(toupper(temp[0]));
+            temp = "";
+        } else if (c == ' ') {
+            if (!temp.empty()) {
+                result += char(toupper(temp[0]));
+                temp = "";
+            }
+        } else {
+            temp += c;
+        }
     }
-    words.push_back(str.substr(start));
 
-    std::string result;
-    for (size_t i = 0; i < words.size(); ++i) {
-        if (i > 0)
-            result += char(std::toupper(words[i][0]));
-        else
-            result += words[i];
-        for (size_t j = 1; j < words[i].size(); ++j)
-            result += std::tolower(words[i][j]);
-        if (i < words.size() - 1)
-            result += " ";
+    if (!temp.empty()) {
+        result += char(toupper(temp[0]));
     }
+    result += temp;
+
     return result;
 }
 
 int main() {
-    std::string str;
-    while (std::cout << "input: "), std::getline(std::cin, str) {
-        std::cout << "output: " << kebabToCamelCase(str) << "\n";
-    }
+    string str;
+    cout << "Enter a string in kebab-case: ";
+    cin >> str;
+    cout << "CamelCase representation: " << kebabToCamel(str) << endl;
     return 0;
 }
