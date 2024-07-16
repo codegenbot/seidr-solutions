c83901f88565d68870c8819f5b02411610e0a96a
beep boop <githubbot@vadim.me>
2024-07-16 15:26:03 +0200

    Solution 30 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fv1zann3 Prompt: 4216 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ef2204ab-fc7c-40a8-b94d-4ea92bde0 24a.py", line 16, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ef2204ab-fc7c-40a8-b94d-4ea92bde0 24a.py", line 13, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,8 +1,17 @@
-def fizz_buzz(n: int) -> int:
+def fizz_buzz(n: int) -> str:
     return sum(
-        1
+        int(i)
         for i in range(1, n + 1)
-        if ((i % 11 == 0 or i % 13 == 0) and "7" in str(i) and "8" not in str(i))
+        if (i % 11 == 0 or i % 13 == 0) and "7" in str(i) and "8" not in str(i)
     )
 
-print(fizz_buzz(100000))  # Output: 8026
+
+print(
+    sum(
+        int(i)
+        for i in range(1, 100000 + 1)
+        if (int(i) % 11 == 0 or int(i) % 13 == 0)
+        and "7" in str(i)
+        and "8" not in str(i)
+    )
+)  # Output should be 8026
