d18eeed5ffeeac1bf1edc10d4326980e46431bb0
beep boop <githubbot@vadim.me>
2024-07-16 11:18:55 +0200

    Solution 89 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jb54hx4z Prompt: 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions) {       | ^~~~~~ In file included from 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp:22: 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp: In function ‘int main()’: 38f9157b-2a67-4509-922b-6d7d2497cd99.cpp:24:13: error: ‘Strongest_Extension’ was  not declared in this scope    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -10,8 +10,8 @@ std::string Strongest_Extension(std::string class_name, std::vector<std::string>
     for(const auto& ext : extensions){
         int cap_count = 0, sm_count = 0;
         for(char c : ext){
-            if(isupper(c)) cap_count++;
-            else if(islower(c)) sm_count++;
+            if(std::isupper(c)) cap_count++;
+            else if(std::islower(c)) sm_count++;
         }
         
         int strength = cap_count - sm_count;
