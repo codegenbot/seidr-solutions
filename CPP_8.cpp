616cae95ddc4aafa2ae32bb454c67e25b43ce233
beep boop <githubbot@vadim.me>
2024-07-16 17:58:00 +0200

    Solution 12 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/k8gswtgn Prompt:       |                    ^~~ 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp:9: 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp: In function ‘int main()’: 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp:19:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 8aac3c5e-715a-44ad-a48e-01ea770f4d49.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -3,13 +3,20 @@
 
 bool issame(std::vector<int> a, std::vector<int> b){
     std::vector<int> sum_product = {0, 1};
+    for (int num : a) {
+        sum_product[0] += num;
+        sum_product[1] *= num;
+    }
+    return a == b;
+}
+
+int main() {
+    std::vector<int> numbers = {2, 5};
+    std::vector<int> sum_product = {0, 1};
     for (int num : numbers) {
         sum_product[0] += num;
         sum_product[1] *= num;
     }
-    return a == sum_product;
-}
-
-int main() {
     assert(issame(sum_product, {10, 10}));
+    return 0;
 }
