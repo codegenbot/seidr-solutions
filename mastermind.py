957d3981a04eb4e4515598e8d8620d6ecee2de86
beep boop <githubbot@vadim.me>
2024-07-17 01:55:58 +0200

    Solution 98 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/rk4q8xde Prompt: it must return 0 4 for input WYGY WYGY, but it returns .

--- mastermind.py
+++ mastermind.py
@@ -5,6 +5,12 @@ def mastermind(code, guess):
         if code[i] == guess[i]:
             black += 1
         elif str(guess[i]) in code:
-            white -= 1  
-    white += str(guess).count(str(code[0])) + str(guess).count(str(code[1])) + str(guess).count(str(code[2])) + str(guess).count(str(code[3])) - black  
+            white -= 1
+    white += (
+        str(guess).count(str(code[0]))
+        + str(guess).count(str(code[1]))
+        + str(guess).count(str(code[2]))
+        + str(guess).count(str(code[3]))
+        - black
+    )
     return str(black) + "\n" + str(white)
