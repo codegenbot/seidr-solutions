6da96d916e4ac99ba4235b6c11b90e5cca67d5ea
beep boop <githubbot@vadim.me>
2024-07-16 16:47:22 +0200

    Solution 31 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/pmrvrh9v Prompt: 8c3f0158-7147-4dc4-9946-c0efcc253609.cpp: In function ‘int bowlingScore(const st ring&)’: 8c3f0158-7147-4dc4-9946-c0efcc253609.cpp:33:18: error: expected ‘}’ at end of in put    33 |     return score;       |                  ^ 8c3f0158-7147-4dc4-9946-c0efcc253609.cpp:4:40: note: to match this ‘{’     4 | int bowlingScore(const std::string& s) {       |                                        ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,11 +1,12 @@
-#include <cctype>
+```cpp
+#include <algorithm>
 #include <string>
 
 int bowlingScore(const std::string& s) {
     int score = 0;
     int currentRoll = 0;
 
-    for (int i = 0; i < s.length(); ++i) {
+    for (int i = 0; i < s.size(); ++i) {
         char c = s[i];
         
         if (c == 'X') {
@@ -15,29 +16,24 @@ int bowlingScore(const std::string& s) {
             score += currentRoll + 5;
             currentRoll = 0;
         } else {
-            if (currentRoll > 9) { 
-                if (std::isdigit(c)) {
-                    int firstRoll = c - '0';
-                    score += firstRoll;
-                    if ((s.substr(i)).find('/') < ((s.substr(i)).size())-1) {
-                        currentRoll = 10 - c;
-                    } else {
-                        currentRoll = 20 - c;
-                    }
+            if (std::isdigit(c)) {
+                int firstRoll = c - '0';
+                score += firstRoll;
+                if (s.find('/') != s.npos) {
+                    currentRoll = 10 - firstRoll;
+                } else if (i < s.size() - 1) {
+                    int secondRoll = s[i + 1] - '0';
+                    score += secondRoll;
+                    currentRoll = 10 - firstRoll - secondRoll;
                 } else {
-                    currentRoll *= 10;
-                    currentRoll += c - '0';
+                    currentRoll = 20 - firstRoll;
                 }
             } else {
-                if (std::isdigit(c)) {
-                    currentRoll *= 10;
-                    currentRoll += c - '0';
-                } else {
-                    currentRoll *= 10;
-                    currentRoll += c - '0';
-                }
+                currentRoll *= 10;
+                currentRoll += c - '0';
             }
         }
     }
 
     return score;
+}
