c23e06e57ee0139a35055fb160b6cb3f9f8d50d4
beep boop <githubbot@vadim.me>
2024-07-16 09:25:26 +0200

    Solution 90 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rrma4mfw Prompt:       |                              ^~~~~~ d49ff279-36b8-4e25-802f-b420a925be38.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d49ff279-36b8-4e25-802f-b420a925be38.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d49ff279-36b8-4e25-802f-b420a925be38.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d49ff279-36b8-4e25-802f-b420a925be38.cpp:29: d49ff279-36b8-4e25-802f-b420a925be38.cpp: In function ‘int main()’: d49ff279-36b8-4e25-802f-b420a925be38.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ d49ff279-36b8-4e25-802f-b420a925be38.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
+#include <vector>
 #include <string>
+#include <cassert>
 
-bool issame(vector<string> a, vector<string> b);
+using namespace std;
 
 vector<string> split_words(string txt){
     vector<string> result;
@@ -21,11 +24,21 @@ vector<string> split_words(string txt){
     if(result.empty()){
         int count = 0;
         for(char c : txt){
-            if(islower(c) && (c - 'a') % 2 == 1){
+            if(islower(c) && (c - 'a') % 2 != 0){
                 count++;
             }
         }
         result.push_back(to_string(count));
     }
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+int main(){
+    assert(issame(split_words(""), {"0}));
+    // Add more test cases here
+    return 0;
 }
