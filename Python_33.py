a2bbc409383435872480baf53e9b30da53b57746
beep boop <githubbot@vadim.me>
2024-07-16 16:27:35 +0200

    Solution 11 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vfpgwfnv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf4d98f9-85e5-4260-bb35-1e5bbf7d1 0e1.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf4d98f9-85e5-4260-bb35-1e5bbf7d1 0e1.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 def sort_third(l: list):
     padding = 3 - len(l) % 3
     l += [None] * padding
-    return [sorted(filter(lambda x: x is not None, l[i : i + 3])) for i in range(0, len(l), 3) if all(x is not None for x in l[i : i + 3])]
+    return [sorted([x for x in l[i : i + 3] if x is not None]) for i in range(0, len(l), 3) if all(x is not None for x in l[i : i + 3])]
