f9bd7f130139c6f5ca87a7eeeb263b5903927d11
beep boop <githubbot@vadim.me>
2024-07-16 16:58:39 +0200

    Solution 30 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8q722hdb Prompt: /usr/include/c++/11/type_traits: In instantiation of ‘struct std::is_destructibl e<std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/stl_construct.h:188:51:   required from ‘void std::_Des troy(_ForwardIterator, _ForwardIterator) [with _ForwardIterator = std::__cxx11:: basic_string<char>*]’ /usr/include/c++/11/bits/alloc_traits.h:848:15:   required from ‘void std::_Dest roy(_ForwardIterator, _ForwardIterator, std::allocator<_T2>&) [with _ForwardIter ator = std::__cxx11::basic_string<char>*; _Tp = std::__cxx11::basic_string<char> ]’ /usr/include/c++/11/bits/stl_vector.h:680:15:   required from ‘std::vector<_Tp, _Alloc>::~vector() [with _Tp = std::__cxx11::basic_string<char>; _Alloc = std::a llocator<std::__cxx11::basic_string<char> >]’ df787c3d-ded1-4fe8-bc7c-1275bfd55dd9.cpp:17:30:   required from here /usr/include/c++/11/type_traits:885:52: error: static assertion failed: template  argument must be a complete class or an unbounded array   885 |       static_assert(std::__is_complete_or_unbounded(__type_identity<_Tp> {}),       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~ ~~~ /usr/include/c++/11/type_traits:885:52: note: ‘std::__is_complete_or_unbounded<s td::__type_identity<std::__cxx11::basic_string<char> > >((std::__type_identity<s td::__cxx11::basic_string<char> >{}, std::__type_identity<std::__cxx11::basic_st ring<char> >()))’ evaluates to false

--- camel-case.cpp
+++ camel-case.cpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <vector>
 
-std::string join(const std::vector<std::string>& tokens) {
-    std::string result;
+::std::string join(const ::std::vector< ::std::string>& tokens) {
+    ::std::string result;
     for (const auto& token : tokens) {
         if (!result.empty()) {
             result += ' ';
@@ -13,10 +13,10 @@ std::string join(const std::vector<std::string>& tokens) {
     return result;
 }
 
-std::string split(const std::string& str, char delimiter) {
-    std::vector<std::string> tokens;
+::std::string split(const ::std::string& str, char delimiter) {
+    ::std::vector< ::std::string > tokens;
     size_t pos = 0;
-    while ((pos = str.find(delimiter)) != std::string::npos) {
+    while ((pos = str.find(delimiter)) != ::std::string::npos) {
         tokens.push_back(str.substr(0, pos));
         str = str.substr(pos + 1);
     }
@@ -24,17 +24,17 @@ std::string split(const std::string& str, char delimiter) {
     return join(tokens);
 }
 
-std::string camelCase(const std::string& str) {
-    std::string result;
+::std::string camelCase(const ::std::string& str) {
+    ::std::string result;
     size_t pos = 0;
     bool capitalizeNext = (str[0] >= 'a' && str[0] <= 'z');
     for (const auto& c : split(str, '-')) {
         if (!result.empty()) {
             if (capitalizeNext) {
-                result += std::string(1, toupper(c[0])) + c.substr(1);
+                result += ::std::string(1, ::std::toupper(c[0])) + c.substr(1);
                 capitalizeNext = false;
             } else {
-                result += std::string(1, tolower(c[0])) + c.substr(1);
+                result += ::std::string(1, ::std::tolower(c[0])) + c.substr(1);
             }
         } else {
             if (c[0] >= 'A' && c[0] <= 'Z') {
