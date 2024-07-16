499f626eef65baa01804e22e0613b39779ce554a
beep boop <githubbot@vadim.me>
2024-07-16 08:30:57 +0200

    Solution 30 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ifxks7p6 Prompt:       |                    ^~~ c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp:12: c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp: In function ‘int main()’: c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ c2c8930f-9d78-4a54-8eb4-cc76147e737b.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -3,6 +3,10 @@
 #include <algorithm>
 #include <cassert>
 
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
+
 std::vector<int> sort_array(std::vector<int> arr) {
     std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int count_a = __builtin_popcount(a);
@@ -12,16 +16,11 @@ std::vector<int> sort_array(std::vector<int> arr) {
         }
         return count_a < count_b;
     });
-    
     return arr;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
 }
 
 int main() {
     assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-    
+    std::cout << "Test Passed" << std::endl;
     return 0;
 }
