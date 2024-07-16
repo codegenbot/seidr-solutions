b3d083ab6c4e3dd27a6600c6c0d66548306ce890
beep boop <githubbot@vadim.me>
2024-07-16 05:55:31 +0200

    Solution 50 of CPP/94, ratio 0.6666666666666666 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/oh9j0j3h Prompt: 3136066b-140b-4e53-83fb-49398f44707a.cpp: In function ‘int skjkasdkd(std::vector <int>)’: 3136066b-140b-4e53-83fb-49398f44707a.cpp:9:13: error: ‘isPrime’ was not declared  in this scope     9 |         if (isPrime(num)) {       |             ^~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,29 +1,35 @@
-Here is the completed code:
+#include <vector>
+#include <string>
 
-int skjkasdkd(vector<int> lst){
-    int maxPrime = 0;
-    for(int i : lst){
-        if(i > 1 && isPrime(i)){
-            maxPrime = i;
-            break;
-        }
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
     }
-    return sumOfDigits(maxPrime);
-}
-
-bool isPrime(int num){
-    if(num <= 1) return false;
-    for(int i = 2; i*i <= num; i++){
-        if(num % i == 0) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
     }
     return true;
 }
 
-int sumOfDigits(int num){
-    int sum = 0;
-    while(num > 0){
-        sum += num % 10;
-        num /= 10;
+int skjkasdkd(vector<int> lst) {
+    int maxPrime = 0;
+    for (int num : lst) {
+        if (isPrime(num)) {
+            if (num > maxPrime) {
+                maxPrime = num;
+            }
+        }
     }
-    return sum;
+
+    int sumOfDigits = 0;
+    while (maxPrime > 0) {
+        sumOfDigits += maxPrime % 10;
+        maxPrime /= 10;
+    }
+
+    return sumOfDigits;
 }
