0b6434bd6e695c639349a2bb306bb65ee81240a4
beep boop <githubbot@vadim.me>
2024-07-16 16:33:10 +0200

    Solution 19 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/pmrvrh9v Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 26ef740f-e64e-48ca-ad4c-872232a67639.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 26ef740f-e64e-48ca-ad4c-872232a67639.cpp:38:25:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -5,7 +5,9 @@ int bowlingScore(const std::string& s) {
     int score = 0;
     int currentRoll = 0;
 
-    for (char c : s) { 
+    for (int i = 0; i < s.size(); ++i) {
+        char c = s[i];
+        
         if (c == 'X') {
             score += 10 + 30; // strike, add the bonus
             currentRoll = 0;
@@ -14,12 +16,26 @@ int bowlingScore(const std::string& s) {
             currentRoll = 0;
         } else {
             if (currentRoll > 9) { 
-                int firstRoll = c - '0';
-                score += firstRoll + 10; 
-                currentRoll = 10 - c;
+                if (isdigit(c)) {
+                    int firstRoll = c - '0';
+                    score += firstRoll + 10; 
+                    if (s.find('/', i) < s.size()-1) {
+                        currentRoll = 10 - c;
+                    } else {
+                        currentRoll = 20 - c;
+                    }
+                } else {
+                    currentRoll *= 10;
+                    currentRoll += c - '0';
+                }
             } else {
-                currentRoll *= 10;
-                currentRoll += c - '0';
+                if (isdigit(c)) {
+                    currentRoll *= 10;
+                    currentRoll += c - '0';
+                } else {
+                    currentRoll *= 10;
+                    currentRoll += c - '0';
+                }
             }
         }
     }
