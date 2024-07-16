30619add66fe835882a04b24a88670f68bc379d5
beep boop <githubbot@vadim.me>
2024-07-16 16:44:03 +0200

    Solution 4 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/6scc09f8 Prompt: 8bd98385-1e96-47cc-8864-908217c00d19.cpp:5:16: error: invalid conversion from ‘c onst char*’ to ‘int’ [-fpermissive]     5 |         return "Fizz";       |                ^~~~~~       |                |       |                const char* 8bd98385-1e96-47cc-8864-908217c00d19.cpp:7:16: error: invalid conversion from ‘c onst char*’ to ‘int’ [-fpermissive]     7 |         return "Buzz";       |                ^~~~~~       |                |       |                const char* 8bd98385-1e96-47cc-8864-908217c00d19.cpp:9:16: error: ‘to_string’ was not declar ed in this scope     9 |         return to_string(x);       |                ^~~~~~~~~ 8bd98385-1e96-47cc-8864-908217c00d19.cpp:9:29: error: expected ‘}’ at end of inp ut     9 |         return to_string(x);       |                             ^ 8bd98385-1e96-47cc-8864-908217c00d19.cpp:1:21: note: to match this ‘{’     1 | int fizzBuzz(int x) {       |                     ^

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -2,10 +2,10 @@
 
 int fizzBuzz(int x) {
     if (x % 3 == 0 && x % 5 == 0)
-        return "FizzBuzz";
+        return std::string("FizzBuzz");
     else if (x % 3 == 0)
-        return "Fizz"; 
+        return std::string("Fizz");
     else if (x % 5 == 0)
-        return "Buzz"; 
+        return std::string("Buzz");
     else
         return std::to_string(x);
