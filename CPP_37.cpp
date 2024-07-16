77bba870e489324bd16a4f43c12187819efcf87e
beep boop <githubbot@vadim.me>
2024-07-16 05:04:05 +0200

    Solution 40 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/wv5xgwlc Prompt:    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 11ff907c-a60c-45fb-a705-468ae1006099.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 11ff907c-a60c-45fb-a705-468ae1006099.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 11ff907c-a60c-45fb-a705-468ae1006099.cpp:22: 11ff907c-a60c-45fb-a705-468ae1006099.cpp: In function ‘int main()’: 11ff907c-a60c-45fb-a705-468ae1006099.cpp:32:20: error: ‘sort_even’ was not decla red in this scope    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 11ff907c-a60c-45fb-a705-468ae1006099.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,32 +1,25 @@
-vector<float> sort_even(vector<float> a) {
+vector<float> sort_even(const vector<float>& l) {
     vector<float> even_values;
-    for (int i = 0; i < a.size(); i++) {
+    for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            even_values.push_back(a[i]);
+            even_values.push_back(l[i]);
         }
     }
     sort(even_values.begin(), even_values.end());
+
     vector<float> result;
     int even_index = 0;
-    for (int i = 0; i < a.size(); i++) {
+    for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             result.push_back(even_values[even_index]);
             even_index++;
         } else {
-            result.push_back(a[i]);
+            result.push_back(l[i]);
         }
     }
     return result;
 }
 
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(const vector<float>& v1, const vector<float>& v2) {
+    return v1 == v2;
 }
