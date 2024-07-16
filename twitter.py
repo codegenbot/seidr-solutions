31c0b71a86f08bb30cc0f306681f902782b34336
beep boop <githubbot@vadim.me>
2024-07-16 22:14:57 +0200

    Solution 19 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8tr8sey0 Prompt: Program did not receive expected input

--- twitter.py
+++ twitter.py
@@ -1,8 +1,6 @@
 ```
-import re
-
 def validate_tweet(tweet):
-    if len(re.sub(r'\W+', '', tweet)) > 140:
+    if len(tweet) > 140:
         return "Too many characters"
     elif tweet == "":
         return "You didn't type anything"
