cd84fef8a01227ea1b7680b786d9d4a44aea090e
beep boop <githubbot@vadim.me>
2024-07-16 15:58:04 +0200

    Solution 47 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9ukk1lth Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,4 +1,4 @@
-int do_algebra(vector<string> operato, vector<int> operand) {
+int do_algebra(vector<string> operato, vector<int> operand){
     int result = operand[0];
     for (int i = 0; i < operato.size(); i++) {
         if (operato[i] == "+") {
