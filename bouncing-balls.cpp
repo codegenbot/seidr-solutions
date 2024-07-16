76db6b54b5c9aa9a0b466f86502544e04df7327d
beep boop <githubbot@vadim.me>
2024-07-16 18:38:58 +0200

    Solution 75 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xaq9j0ra Prompt: /usr/include/c++/11/iomanip:210:12: error: ‘class std::basic_ostream<char>’ has no member named ‘precision’   210 |       __os.precision(__f._M_n);       |       ~~~~~^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 39527235-6f1f-448d-8e47-d938bdb84f65.cpp:2: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,6 +1,7 @@
+```cpp
 #include <iostream>
 #include <cmath>
-#include <iomanip>
+#include <ios>
 using namespace std;
 
 int main() {
@@ -10,11 +11,11 @@ int main() {
 
     cin >> startHeight >> firstBounce >> numBounces;  
 
-    double bouncinessIndex = firstBounce / startHeight;
+    double bouncinessIndex = (startHeight / firstBounce);
 
-    double totalDistance = 1 - pow(1 - bouncinessIndex, numBounces);
+    double totalDistance = 1 - pow((1 - bouncinessIndex), numBounces);
     
-    cout << fixed << std::setprecision(10);
+    cout << fixed << setprecision(10);
     cout << totalDistance + 0.00001 << endl;
 
     return 0;
