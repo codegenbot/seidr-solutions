304e58a9ee4044087bced76552f1454d9badee4d
beep boop <githubbot@vadim.me>
2024-07-16 04:19:29 +0200

    Solution 73 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/6t4hxh2e Prompt:       |                    ^~~ d5bf8a14-da69-440b-afda-e797ecbc410e.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d5bf8a14-da69-440b-afda-e797ecbc410e.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d5bf8a14-da69-440b-afda-e797ecbc410e.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d5bf8a14-da69-440b-afda-e797ecbc410e.cpp:15: d5bf8a14-da69-440b-afda-e797ecbc410e.cpp: In function ‘int main()’: d5bf8a14-da69-440b-afda-e797ecbc410e.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ d5bf8a14-da69-440b-afda-e797ecbc410e.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,19 +1,7 @@
 #include <vector>
 #include <cassert>
 
-vector<int> factorize(int n);
-
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
+bool issame(vector<int> a, vector<int> b);
 
 vector<int> factorize(int n){
     vector<int> factors;
@@ -27,8 +15,4 @@ vector<int> factorize(int n){
         factors.push_back(n);
     }
     return factors;
-}
-
-int main() {
-    // Your main function code here
 }
