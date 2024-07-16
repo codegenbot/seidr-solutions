610eec2e776814cc3f86fe92d4cc5e236cc0214d
beep boop <githubbot@vadim.me>
2024-07-16 09:43:55 +0200

    Solution 50 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/byjhxe6e Prompt:    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c882f9ef-5f87-4b14-ab56-6ebea4027013.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c882f9ef-5f87-4b14-ab56-6ebea4027013.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c882f9ef-5f87-4b14-ab56-6ebea4027013.cpp:14: c882f9ef-5f87-4b14-ab56-6ebea4027013.cpp: In function ‘int main()’: c882f9ef-5f87-4b14-ab56-6ebea4027013.cpp:24:20: error: ‘largest_smallest_integer s’ was not declared in this scope    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ c882f9ef-5f87-4b14-ab56-6ebea4027013.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <cassert>
 
@@ -8,7 +9,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
 std::vector<int> largest_smallest_integers(std::vector<int> lst){
     int largest_negative = INT_MIN, smallest_positive = INT_MAX;
     for(int num : lst){
-        if(num < 0 && num < largest_negative){
+        if(num < 0 && num > largest_negative){
             largest_negative = num;
         }
         if(num > 0 && (num < smallest_positive)){
@@ -18,6 +19,7 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst){
     return {largest_negative, smallest_positive};
 }
 
-int main() {
-    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-100, 1}));
+int main(){
+    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    return 0;
 }
