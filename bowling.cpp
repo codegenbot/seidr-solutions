13dc7b292125c3ae0c045d553c8e1b3e260ae36a
beep boop <githubbot@vadim.me>
2024-07-16 12:57:09 +0200

    Solution 64 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vou69ibb Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,36 +1,23 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
+```cpp
 int bowlingScore(string s) {
     int score = 0;
-    int roll = 0;
+    int nextRoll = 0;
 
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == '/') {
-            if (roll > 9)
+            if (nextRoll > 9)
                 score += 10;
             else
-                score += roll;
-            roll = 0;
+                score += nextRoll;
+            nextRoll = 0;
         } else if (isdigit(s[i])) {
-            std::string str = std::to_string(s[i]) - '0';
-            roll *= 10;
-            roll += stoi(str);
+            nextRoll *= 10;
+            nextRoll += s[i] - '0';
         }
     }
 
-    if (roll > 9)
-        score += 10;
-    else
-        score += roll;
+    if (nextRoll > 0)
+        score += nextRoll;
 
     return score;
-}
-
-int main() {
-    string s = "1-2/3 4-5/6-7-8/9";
-    int score = bowlingScore(s);
-    cout << "The score is: " << score << endl;
-    return 0;
 }
