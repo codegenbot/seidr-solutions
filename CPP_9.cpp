07365256dacb955fdfdefe7d8fd75aca532fd154
beep boop <githubbot@vadim.me>
2024-07-16 03:57:44 +0200

    Solution 60 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1mkz12zl Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 62257962-69b6-4dfb-8ccb-b9ead45d542c.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 62257962-69b6-4dfb-8ccb-b9ead45d542c.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 62257962-69b6-4dfb-8ccb-b9ead45d542c.cpp:11: 62257962-69b6-4dfb-8ccb-b9ead45d542c.cpp: In function ‘int main()’: 62257962-69b6-4dfb-8ccb-b9ead45d542c.cpp:21:20: error: ‘rolling_max’ was not dec lared in this scope    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 62257962-69b6-4dfb-8ccb-b9ead45d542c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -7,12 +7,13 @@ using namespace std;
 
 vector<int> rolling_max(vector<int> numbers);
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
 vector<int> rolling_max(vector<int> numbers){
     vector<int> result;
     int max_val = INT_MIN;
-
     for (int i = 0; i < numbers.size(); ++i) {
         max_val = max(max_val, numbers[i]);
         result.push_back(max_val);
@@ -20,6 +21,8 @@ vector<int> rolling_max(vector<int> numbers){
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+int main() {
+    assert (issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
+    
+    return 0;
 }
