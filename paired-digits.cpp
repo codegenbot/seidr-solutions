15e846aed1285f680bbd338e53490bd3fb9a51b5
beep boop <githubbot@vadim.me>
2024-07-16 10:37:44 +0200

    Solution 54 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/lv177yxy Prompt:       |                             ^~~~~~ 6d409e9b-36b2-4a7a-99c7-c25fc5d632dc.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int pairedDigits(const std::string& str) { 6d409e9b-36b2-4a7a-99c7-c25fc5d632dc.cpp: In function ‘int pairedDigits(const in t&)’: 6d409e9b-36b2-4a7a-99c7-c25fc5d632dc.cpp:3:10: error: ‘size_t’ was not declared in this scope     3 |     for (size_t i = 0; i < str.length() - 1; ++i) {       |          ^~~~~~ 6d409e9b-36b2-4a7a-99c7-c25fc5d632dc.cpp:1:1: note: ‘size_t’ is defined in heade r ‘<cstddef>’; did you forget to ‘#include <cstddef>’?   +++ |+#include <cstddef>     1 | int pairedDigits(const std::string& str) { 6d409e9b-36b2-4a7a-99c7-c25fc5d632dc.cpp:3:24: error: ‘i’ was not declared in th is scope     3 |     for (size_t i = 0; i < str.length() - 1; ++i) {       |                        ^ 6d409e9b-36b2-4a7a-99c7-c25fc5d632dc.cpp:3:32: error: request for member ‘length ’ in ‘str’, which is of non-class type ‘const int’     3 |     for (size_t i = 0; i < str.length() - 1; ++i) {       |                                ^~~~~~

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -3,7 +3,8 @@
 
 int pairedDigits(const std::string& str) {
     int sum = 0;
-    for (size_t i = 0; i < str.length() - 1; ++i) {
+    size_t i;
+    for (i = 0; i < str.length() - 1; ++i) {
         if ((str[i] - '0') == (str[i + 1] - '0')) {
             sum += (str[i] - '0') + (str[i + 1] - '0');
         }
