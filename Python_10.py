ff3958e0a89b6fa81697011c072178a893d724f4
beep boop <githubbot@vadim.me>
2024-07-16 03:32:03 +0200

    Solution 22 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/a5r6cmt8 Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -5,8 +5,10 @@ def is_palindrome(string: str) -> bool:
 
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
-        return string + string
-    for i in range(len(string), 0, -1):
-        if string[:i] == string[:i][::-1]:
-            return string[:i] + string[i:][::-1]
-    return None
+        return string + string[::-1]
+    for i in range(len(string)):
+        postfix = string[i:]
+        if postfix == postfix[::-1]:
+            prefix = string[:i][::-1]
+            return prefix + postfix
+    return string + string[::-1]
