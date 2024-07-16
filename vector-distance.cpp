fa7e5a1be8f43c2715d83a227094e73ffdb46909
beep boop <githubbot@vadim.me>
2024-07-16 14:26:57 +0200

    Solution 57 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/bv2ogq57 Prompt: b4fb9b4b-493d-4780-ae44-36fac88a9176.cpp: In function ‘double calculateDistance( const std::vector<double>&, const std::vector<double>&)’: b4fb9b4b-493d-4780-ae44-36fac88a9176.cpp:5:10: error: ‘size_t’ was not declared in this scope; did you mean ‘std::size_t’?     5 |     for (size_t i = 0; i < vec1.size(); ++i) {       |          ^~~~~~       |          std::size_t In file included from /usr/include/c++/11/bits/stl_algobase.h:59,                  from /usr/include/c++/11/vector:60,                  from b4fb9b4b-493d-4780-ae44-36fac88a9176.cpp:1: /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h:280:33: note: ‘std::size_t ’ declared here   280 |   typedef __SIZE_TYPE__         size_t;       |                                 ^~~~~~ b4fb9b4b-493d-4780-ae44-36fac88a9176.cpp:5:24: error: ‘i’ was not declared in th is scope     5 |     for (size_t i = 0; i < vec1.size(); ++i) {       |                        ^ b4fb9b4b-493d-4780-ae44-36fac88a9176.cpp:8:17: error: ‘sqrt’ is not a member of ‘std’     8 |     return std::sqrt(sum);       |                 ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,15 +1,10 @@
-#include <cmath>
 #include <vector>
+#include <cmath>
 
 double calculateDistance(const std::vector<double>& vec1, const std::vector<double>& vec2) {
-    if (vec1.size() != vec2.size()) {
-        return -1.0;
-    }
-
     double sum = 0.0;
-    for (size_t i = 0; i < vec1.size(); ++i) {
+    for (std::size_t i = 0; i < vec1.size(); ++i) {
         sum += (vec1[i] - vec2[i]) * (vec1[i] - vec2[i]);
     }
-    
     return std::sqrt(sum);
 }
