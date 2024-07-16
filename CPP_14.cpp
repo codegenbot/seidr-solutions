1218309d94c152e98075393c147e67e87bbf605d
beep boop <githubbot@vadim.me>
2024-07-16 04:47:31 +0200

    Solution 80 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/cr3bh6us Prompt:       |                              ^~~~~~ cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp:9: cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp: In function ‘int main()’: cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ cc3f17a3-e5bf-4bf1-8644-9f70f875a8d8.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -5,19 +5,20 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
 vector<string> all_prefixes(string str) {
     vector<string> result;
-    for (int i = 1; i <= str.size(); i++) {
+    for(int i = 1; i <= str.size(); i++) {
         result.push_back(str.substr(0, i));
     }
     return result;
 }
 
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
 int main() {
     assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    
     return 0;
 }
