3f2fe185b5f1a836856243f0b6727521b55697e1
beep boop <githubbot@vadim.me>
2024-07-16 07:52:30 +0200

    Solution 89 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9gh01s36 Prompt: ec4003dc-041d-46b3-a566-f3c3992e97af.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ec4003dc-041d-46b3-a566-f3c3992e97af.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ec4003dc-041d-46b3-a566-f3c3992e97af.cpp:17: ec4003dc-041d-46b3-a566-f3c3992e97af.cpp: In function ‘int main()’: ec4003dc-041d-46b3-a566-f3c3992e97af.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ ec4003dc-041d-46b3-a566-f3c3992e97af.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ec4003dc-041d-46b3-a566-f3c3992e97af.cpp: In function ‘int totalChars(int)’: ec4003dc-041d-46b3-a566-f3c3992e97af.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,6 +1,20 @@
 #include <vector>
 #include <string>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+using namespace std;
+
+int totalChars(int val) {
+    return val;
+}
+
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
+}
+
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    if (totalChars(lst1.size()) < totalChars(lst2.size())) {
+        return lst1;
+    } else {
+        return issame(lst1, lst2) ? lst1 : lst2;
+    }
 }
