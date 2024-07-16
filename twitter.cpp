60a3008bee55ca5dfb28bb40ef1647fc4ed4f12e
beep boop <githubbot@vadim.me>
2024-07-16 23:18:38 +0200

    Solution 37 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8x9ggrs8 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -5,13 +5,13 @@
 int main() {
     std::string tweet;
     std::cout << "Enter your tweet: ";
-    std::getline(std::cin, tweet);
+    getline(std::cin, tweet);
 
     if (tweet.empty()) {
         std::cout << "You didn't type anything" << std::endl;
-    } else if (tweet.length() > 140) {
+    } else if (tweet.length() > 0 && tweet.length() > 140) {
         std::cout << "Too many characters" << std::endl;
     } else {
-        std::cout << "Your tweet has " << static_cast<int>(tweet.length()) << " characters" << std::endl;
+        std::cout << "Your tweet has " << tweet.length() << " characters" << std::endl;
     }
 }
