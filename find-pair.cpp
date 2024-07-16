3585941e78d8e9a52f7d6160b2c8580cfe7c04cf
beep boop <githubbot@vadim.me>
2024-07-16 17:41:38 +0200

    Solution 23 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/aut863mg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 041c1019-478d-4801-b220-92ff82f78386.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
@@ -8,27 +7,26 @@ std::vector<std::pair<int, int>> findPairs(std::vector<int>& nums, int target) {
     std::vector<std::pair<int, int>> result;
     
     for (int i = 0; i < nums.size(); i++) {
-        int j = i + 1;
-        
-        while (j < nums.size()) {
-            if (nums[i] + nums[j] == target) {
+        auto it = nums.begin() + i;
+        while (it != nums.end()) {
+            if (*it + *(it - 1) == target) {
                 bool isPairUnique = true;
 
                 for (auto& existingPair : result) {
-                    if ((existingPair.first == nums[i] && existingPair.second == nums[j]) ||
-                        (existingPair.first == nums[j] && existingPair.second == nums[i])) {
+                    if ((existingPair.first == *it && existingPair.second == *(it - 1)) ||
+                        (existingPair.first == *(it - 1) && existingPair.second == *it)) {
                         isPairUnique = false;
                         break;
                     }
                 }
 
                 if (isPairUnique) {
-                    result.push_back({std::min(nums[i], nums[j]), std::max(nums[i], nums[j])});
+                    result.push_back({std::min(*it, *(it - 1)), std::max(*it, *(it - 1))});
                 }
-            } else if (nums[i] + nums[j] > target) {
-                j = i; 
+            } else if (*it + *(it - 1) > target) {
+                it = nums.begin() + i; 
             } else {
-                j++;
+                it++;
             }
         }
     }
@@ -40,8 +38,6 @@ int main() {
     std::vector<int> nums = {1, 2, 3, 4, 5};
     int target = 7;
     std::vector<std::pair<int, int>> result = findPairs(nums, target);
-    for (auto& pair : result) {
-        std::cout << "(" << pair.first << ", " << pair.second << ")" << std::endl;
-    }
+    // print or use result as needed
     return 0;
 }
