efbd09bd8157457cb14b1312b2925f5e24cba6cf
beep boop <githubbot@vadim.me>
2024-07-16 06:55:28 +0200

    Solution 26 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/z65p3l1k Prompt:       |                    ^~~ c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp:17: c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp: In function ‘int main()’: c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ c541a5ee-9369-4e96-bafa-2ada340f6a9c.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,6 +1,19 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+std::pair<int, int> countPalindrome(int n) {
+    int even = 0, odd = 0;
+    for (int i = 1; i <= n; ++i) {
+        std::string s = std::to_string(i);
+        std::string rev = s;
+        std::reverse(rev.begin(), rev.end());
+        if (s == rev) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+    }
+    return {even, odd};
 }
