967959493e3ba641d9d3ad78210d473fbc432cba
beep boop <githubbot@vadim.me>
2024-07-16 04:21:49 +0200

    Solution 90 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1vr8zy9e Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b58df107-4812-4f0b-a61d-f5310a3331aa.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b58df107-4812-4f0b-a61d-f5310a3331aa.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b58df107-4812-4f0b-a61d-f5310a3331aa.cpp:25: b58df107-4812-4f0b-a61d-f5310a3331aa.cpp: In function ‘int main()’: b58df107-4812-4f0b-a61d-f5310a3331aa.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ b58df107-4812-4f0b-a61d-f5310a3331aa.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,3 +1,28 @@
-vector<string> separate_paren_groups(string paren_string);
+bool issame(const string &s1, const string &s2) {
+    return s1 == s2;
+}
 
-bool issame(vector<string> a, vector<string> b)
+vector<string> separate_paren_groups(const string &paren_string) {
+    vector<string> result;
+    string current_group;
+    int balance = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (balance > 0) {
+                current_group += c;
+            }
+            balance++;
+        } else if (c == ')') {
+            balance--;
+            if (balance > 0) {
+                current_group += c;
+            } else if (balance == 0) {
+                result.push_back(current_group);
+                current_group = "";
+            }
+        }
+    }
+
+    return result;
+}
