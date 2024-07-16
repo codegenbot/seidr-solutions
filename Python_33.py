b8dafde980b44f989868f63c4531af5e4d7a74af
beep boop <githubbot@vadim.me>
2024-07-16 16:34:00 +0200

    Solution 22 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vfpgwfnv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f07eeb41-b1ff-4004-acb4-a700d2146 447.py", line 4     sorted_triplets = [sorted([x for x in l[i : i + 3] if x is not None]) for i in range(0, len(l), 3]                      ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,9 @@
 def sort_third(l: list):
     padding = 3 - len(l) % 3
     l += [None] * padding
-    sorted_triplets = [sorted([x for x in l[i : i + 3] if x is not None]) for i in range(0, len(l), 3)]
-    return [triplet for triplet in sorted_triplets if all(x is not None for x in triplet)]
+    sorted_triplets = [
+        sorted([x for x in l[i : i + 3] if x is not None]) for i in range(0, len(l), 3)
+    ]
+    return [
+        triplet for triplet in sorted_triplets if all(x is not None for x in triplet)
+    ]
