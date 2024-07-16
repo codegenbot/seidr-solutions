ab03db89ddb879b0cc3885eaee89b48e7ebc8a6d
beep boop <githubbot@vadim.me>
2024-07-16 12:08:13 +0200

    Solution 80 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/ccj3n8nn Prompt: d6446534-b4ed-4bd2-b532-46b68c7e2142.cpp: In function ‘int main()’: d6446534-b4ed-4bd2-b532-46b68c7e2142.cpp:5:5: error: ‘cin’ was not declared in t his scope     5 |     cin >> start_height >> bounce_height >> num_bounces;       |     ^~~ d6446534-b4ed-4bd2-b532-46b68c7e2142.cpp:11:60: error: ‘pow’ was not declared in  this scope    11 |         total_distance += (start_height + bounce_height) * pow(bouncines s_index, i);       |                                                            ^~~ d6446534-b4ed-4bd2-b532-46b68c7e2142.cpp:14:5: error: ‘cout’ was not declared in  this scope    14 |     cout << total_distance << endl;       |     ^~~~ d6446534-b4ed-4bd2-b532-46b68c7e2142.cpp:14:31: error: ‘endl’ was not declared i n this scope    14 |     cout << total_distance << endl;       |                               ^~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 int main() {
     double start_height, bounce_height, bounciness_index, total_distance;
     int num_bounces;
 
-    std::cin >> start_height >> bounce_height >> num_bounces;
+    cin >> start_height >> bounce_height >> num_bounces;
 
     bounciness_index = bounce_height / start_height;
     total_distance = start_height + (start_height + bounce_height) * (1 / (1 - bounciness_index));
 
     for (int i = 1; i < num_bounces; ++i) {
-        total_distance += (start_height + bounce_height) * std::pow(bounciness_index, i);
+        total_distance += (start_height + bounce_height) * pow(bounciness_index, i);
     }
 
-    std::cout << total_distance << std::endl;
+    cout << total_distance << endl;
 
     return 0;
 }
