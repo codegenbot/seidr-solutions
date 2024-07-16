449d02fabb9bd8b9e9ebe13e7232a7e61cb1fb74
beep boop <githubbot@vadim.me>
2024-07-16 04:49:17 +0200

    Solution 55 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nzqa9sxi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcd7a6e4-991b-429f-903c-cb567788c dec.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,8 +1,5 @@
-```Python
 def solution(lst):
-    odd_sum = sum(i for i in lst[::2] if i % 2 != 0)
-    even_sum = sum(i for i in lst[1::2] if i % 2 == 0)
-    return odd_sum, even_sum
+    return sum(i for i in lst[1::2] if i % 2 == 0)
 
 while True:
     try:
@@ -13,7 +10,4 @@ while True:
         break
     except ValueError:
         print("Invalid input. Please enter numbers separated by spaces.")
-
-odd_sum, even_sum = solution(lst)
-print(f"Sum of odd numbers: {odd_sum}")
-print(f"Sum of even numbers: {even_sum}")
+print(solution(lst))
