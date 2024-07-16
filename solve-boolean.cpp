3be7c0104964c17853136da57e276fea688391b0
beep boop <githubbot@vadim.me>
2024-07-16 18:47:23 +0200

    Solution 18 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/15ciwpbp Prompt: ’ does not name a type     2 | bool solveBoolean(const std::string& s) {       |                              ^~~~~~ 2d70ffc9-f04e-4210-86c3-e0c6b331d048.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | ``` 2d70ffc9-f04e-4210-86c3-e0c6b331d048.cpp: In function ‘bool solveBoolean(const i nt&)’: 2d70ffc9-f04e-4210-86c3-e0c6b331d048.cpp:4:26: error: ‘begin’ was not declared i n this scope     4 |     for (const auto& c : s) {       |                          ^ 2d70ffc9-f04e-4210-86c3-e0c6b331d048.cpp:4:26: error: ‘end’ was not declared in this scope 2d70ffc9-f04e-4210-86c3-e0c6b331d048.cpp:12:27: error: request for member ‘find’  in ‘s’, which is of non-class type ‘const int’    12 |             auto temp = s.find(c);       |                           ^~~~ 2d70ffc9-f04e-4210-86c3-e0c6b331d048.cpp:13:30: error: ‘std::string’ has not bee n declared    13 |             if (temp != std::string::npos) c = s[temp + 1];       |                              ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,7 +1,8 @@
 #include <string>
+
 bool solveBoolean(const std::string& s) {
     bool result = true;
-    for (auto c : s) {  
+    for (const auto c : s) {  
         if (c == 'T') {
             result = true;
         } else if (c == 'F') {
@@ -10,7 +11,9 @@ bool solveBoolean(const std::string& s) {
             result = !result;
         } else if (c == '&') {
             auto temp = s.find(c);
-            if (temp != std::string::npos) c = s[temp + 1];
+            if (temp != std::string::npos) 
+                result = (s[temp + 1] == 'T');
         }
     }
     return result;
+}
