68ad18f72b08df1badc02d42b301d94344e52a6b
beep boop <githubbot@vadim.me>
2024-07-16 12:10:33 +0200

    Solution 86 of CPP/32, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/sltonii7 Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,5 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+using namespace std;
+
+double poly(vector<double> xs, double x){
+    double sum=0;
+    int i;
+    for (i=0;i<xs.size();i++)
+    {
+        sum+=xs[i]*pow(x,i);
+    }
+    return sum;
+}
+
 double find_zero(vector<double> xs){
-    double a = xs[0];
-    double b = xs[1];
-    return -b/a;
+    double a = xs[xs.size()-1];
+    double b = xs[0];
+    double x = -b/a;
+    return x;
 }
