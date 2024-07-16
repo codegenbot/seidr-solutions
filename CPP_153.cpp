202ba6962af6d3587c91ca613b4f4c9ee8edd988
beep boop <githubbot@vadim.me>
2024-07-16 03:04:11 +0200

    Solution 9 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/m7kwn51t Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 5b256a54-77bd-4c52-a2e1-5ae26ac6067b.cpp:4: 5b256a54-77bd-4c52-a2e1-5ae26ac6067b.cpp:33:1: error: expected unqualified-id be fore ‘static_cast’    33 | assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       | ^~~~~~ 5b256a54-77bd-4c52-a2e1-5ae26ac6067b.cpp:33:1: error: expected ‘)’ before ‘stati c_cast’    33 | assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       | ^~~~~~ 5b256a54-77bd-4c52-a2e1-5ae26ac6067b.cpp:33:50: error: expected unqualified-id b efore ‘)’ token    33 | assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |                                                  ^ 5b256a54-77bd-4c52-a2e1-5ae26ac6067b.cpp:34:1: error: expected unqualified-id be fore ‘return’    34 | return 0;       | ^~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,12 +1,11 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cassert>
 #include <climits>
 
 using namespace std;
 
-void Strongest_Extension(string class_name, vector<string> extensions) {
+string Strongest_Extension(string class_name, vector<string> extensions) {
     string strongest_extension = "";
     int max_strength = INT_MIN;
 
@@ -27,7 +26,10 @@ void Strongest_Extension(string class_name, vector<string> extensions) {
         }
     }
 
-    for (const string& ext : extensions) {
-        assert(class_name + "." + strongest_extension == Strongest_Extension(class_name, extensions));
-    }
+    return class_name + "." + strongest_extension;
+}
+
+int main() {
+    cout << Strongest_Extension("Sp", {"671235", "Bb"}) << endl;
+    return 0;
 }
