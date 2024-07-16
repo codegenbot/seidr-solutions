44fbd691fe9684e582365f9f7174098ca7c1b5f5
beep boop <githubbot@vadim.me>
2024-07-16 03:23:12 +0200

    Solution 36 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p5nabqe9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3a1b4c5-819a-4f77-b18d-f0957bba5 e87.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3a1b4c5-819a-4f77-b18d-f0957bba5 e87.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,4 +1,6 @@
+```
 def encode(message):
+    assert message == "k dQnT kNqW wHcT Tq wRkTg", f"Expected '{message}' but got '{encode("I DoNt KnOw WhAt tO WrItE")}'"
     result = ""
     for char in message:
         if char == " ": 
@@ -8,3 +10,4 @@ def encode(message):
             result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
         else: 
             result += char
+    return result
