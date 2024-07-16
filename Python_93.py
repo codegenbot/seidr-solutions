ce9f8d71ad49a3ba969a6f0cbd0e8f319084cab1
beep boop <githubbot@vadim.me>
2024-07-16 04:52:35 +0200

    Solution 7 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yuz53mar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4d05338-ef07-4df1-85ce-45a5c4cbf c0a.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4d05338-ef07-4df1-85ce-45a5c4cbf c0a.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,9 +4,11 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.upper() in vowels:
-                encoded_message += chr(ord(char) + 2)
-            else:
                 encoded_message += chr(ord(char) + 1)
+            else:
+                encoded_message += char.swapcase()
         else:
             encoded_message += char
-    return encoded_message
+    return encoded_message.lower()
+
+encode("I DoNt KnOw WhAt tO WrItE")
