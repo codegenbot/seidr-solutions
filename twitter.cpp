4a100f72ec9d9303feecfebe1ba86327657ccc53
beep boop <githubbot@vadim.me>
2024-07-16 17:14:37 +0200

    Solution 86 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/o2oujxn5 Prompt: it must return Your tweet has 140 characters for input 2)Ipf>Q+x+~oomtxGJDB7SXj\#y<ydzy4)D9*B_%;F6RM(hlZh'oZ&VLaoL"$GbtE9$wy)qyZs>o$0}OlI4cJ,pJ=!<RuG>8|76scKr-_wyP'|y}1VOBi,?;z#kJNlawi;=LzJp^}'i1, but it returns Enter a tweet: Your tweet has 140 characters.

--- twitter.cpp
+++ twitter.cpp
@@ -1,8 +1,19 @@
-size_t len = tweet.length();
-if(len > 140) {
-    cout << "Too many characters" << endl;
-} else if(len == 0) {
-    cout << "You didn't type anything" << endl;
-} else {
-    cout << "Your tweet has " << len << " characters" << endl;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string tweet;
+    cout << "Enter a tweet: ";
+    getline(cin, tweet);
+    
+    size_t len = tweet.length();
+    if(len > 140) {
+        cout << "Too many characters" << endl;
+    } else if(len == 0) {
+        cout << "You didn't type anything" << endl;
+    } else {
+        cout << "Your tweet has " << len << " characters" << endl;
+    }
 }
