a2d62287bce69b676d230e961eb111e953c28f24
beep boop <githubbot@vadim.me>
2024-07-16 15:38:33 +0200

    Solution 82 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p9s49ghj Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,31 +1,16 @@
-#include <iostream>
-#include <string>
+std::pair<int, int> mastermind(const std::string& code, const std::string& guess) {
+    std::string all_chars = "RGOR"; // assuming the 6 possible characters are R, G, O, and so on
 
-int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
     for (int i = 0; i < 4; ++i) {
         if (guess[i] == code[i]) {
             black++;
-        } else {
-            for (int j = 0; j < 4; ++j) {
-                if (guess[i] == code[j] && i != j) {
-                    white++;
-                    break;
-                }
-            }
+        } else if (std::count(all_chars.begin(), all_chars.end(), guess[i]) > 0) {
+            white++;
         }
     }
 
     return {white, black};
-}
-
-int main() {
-    int white, black;
-    std::string code = "RGOR";
-    std::string guess = "BRBB";
-    std::tie(white, black) = mastermind(code, guess);
-    std::cout << "White pegs: " << white << ", Black pegs: " << black << std::endl;
-    return 0;
 }
