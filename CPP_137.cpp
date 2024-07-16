2a88a72448f0579cf4c8055fed6b4b0bce7c52b9
beep boop <githubbot@vadim.me>
2024-07-16 17:46:29 +0200

    Solution 6 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/crve8xko Prompt:       |                        ^~~~~~~~ /usr/include/c++/11/any:562:24: note:   template argument deduction/substitution  failed: In file included from 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp:45: 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp: In function ‘int main()’: 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp:47:13: error: ‘boost’ has not been decl ared    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp:47:29: error: ‘string’ was not declared  in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp:47:29: note: ‘std::string’ is defined i n header ‘<string>’; did you forget to ‘#include <string>’? 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp: In function ‘std::any compare_one(std: :any, std::any)’: 8537de6d-f3ac-4f8d-9549-74f733dbaa69.cpp:43:1: warning: control reaches end of n on-void function [-Wreturn-type]    43 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,9 +1,8 @@
 #include <any>
-#include <string>
 #include <algorithm>
-#include <cassert>
+#include <string>
 
-using std::any;
+using namespace std;
 
 any compare_one(any a, any b) {
     if(a.type() == typeid(int) && b.type() == typeid(int)){
@@ -12,7 +11,7 @@ any compare_one(any a, any b) {
         } else if(any_cast<int>(a) < any_cast<int>(b)){
             return b;
         } else {
-            return any();
+            return "None";
         }
     } else if(a.type() == typeid(float) && b.type() == typeid(float)){
         if(any_cast<float>(a) > any_cast<float>(b)){
@@ -20,26 +19,28 @@ any compare_one(any a, any b) {
         } else if(any_cast<float>(a) < any_cast<float>(b)){
             return b;
         } else {
-            return any();
+            return "None";
         }
     } else if(a.type() == typeid(string) && b.type() == typeid(string)){
         float num1, num2;
-        if(any_cast<std::string>(a).find(",") != std::string::npos){
-            std::replace(any_cast<std::string&>(a).begin(), any_cast<std::string&>(a).end(), ',', '.');
+        if(any_cast<string>(a).find(",") != string::npos){
+            replace(any_cast<string&>(a).begin(), any_cast<string&>(a).end(), ',', '.');
         }
-        if(any_cast<std::string>(b).find(",") != std::string::npos){
-            std::replace(any_cast<std::string&>(b).begin(), any_cast<std::string&>(b).end(), ',', '.');
+        if(any_cast<string>(b).find(",") != string::npos){
+            replace(any_cast<string&>(b).begin(), any_cast<string&>(b).end(), ',', '.');
         }
-        num1 = std::stof(any_cast<std::string>(a));
-        num2 = std::stof(any_cast<std::string>(b));
+        num1 = stof(any_cast<string>(a));
+        num2 = stof(any_cast<string>(b));
         if(num1 > num2){
             return a;
         } else if(num1 < num2){
             return b;
         } else {
-            return any();
+            return "None";
         }
     } else {
-        return any();
+        return "None";
     }
+    
+    return "None"; // Added to handle control reaching the end of non-void function
 }
