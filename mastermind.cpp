6f69d2c1238aae8dda7ba2cb7c9792fcc97eec7d
beep boop <githubbot@vadim.me>
2024-07-16 15:31:44 +0200

    Solution 78 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p9s49ghj Prompt:       |                                               ^~~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:5:30: error: ‘mastermind’ was not decla red in this scope     5 |     std::tie(white, black) = mastermind(code, guess);       |                              ^~~~~~~~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:6:10: error: ‘cout’ is not a member of ‘std’     6 |     std::cout << "White pegs: " << white << ", Black pegs: " << black <<  std::endl;       |          ^~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:1:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?   +++ |+#include <iostream>     1 | int main() { 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:6:79: error: ‘endl’ is not a member of ‘std’     6 | t << "White pegs: " << white << ", Black pegs: " << black << std::endl;       |                                                                   ^~~~ 0f9b9d9b-fe3e-422d-956c-42095ccfb999.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | int main() {

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,28 +1,27 @@
-```cpp
 #include <iostream>
 #include <string>
 
-std::pair<int, int> mastermind(const std::string& code, const std::string& guess) {
-    int white = 0;
-    int black = 0;
-    
-    for(int i = 0; i < 4; i++) {
-        if(code[i] == guess[i]) {
+int mastermind(std::string code, std::string guess) {
+    int white = 0, black = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (guess[i] == code[i]) {
             black++;
-        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {
-            white++;
         }
     }
-    
-    return std::make_pair(white, black);
+    for (char c : guess) {
+        if (code.find(c) != std::string::npos) {
+            white++;
+            code.erase(code.find(c), 1);
+        }
+    }
+    return white - black;
 }
 
 int main() {
     int white, black;
     std::string code = "RGOR";
     std::string guess = "BRBB";
-    auto res = mastermind(code, guess);
-    std::tie(white, black) = res;
+    std::tie(white, black) = mastermind(code, guess);
     std::cout << "White pegs: " << white << ", Black pegs: " << black << std::endl;
     return 0;
 }
