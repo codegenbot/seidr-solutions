57b603fc77b06c5b6a0b9ad0f9bcfdb374e72f7a
beep boop <githubbot@vadim.me>
2024-07-16 09:42:44 +0200

    Solution 20 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rqcq4htz Prompt:       |                    ^~~ e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp:10: e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp: In function ‘int main()’: e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp:20:20: error: ‘generate_integers’ was n ot declared in this scope    20 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ e1a23da7-cac7-439f-be9c-002c8144ec5c.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,21 +1,23 @@
 #include <vector>
-#include <cassert>
 
-std::vector<int> generate_integers(int a, int b) {
-    std::vector<int> result;
+bool issame(const vector<int>& a, const vector<int>& b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> generate_integers(int a, int b) {
+    vector<int> result;
     for (int i = a; i <= b; ++i) {
         if (i % 2 == 0) {
             result.push_back(i);
         }
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(generate_integers(17, 89), {}));
-    return 0;
 }
