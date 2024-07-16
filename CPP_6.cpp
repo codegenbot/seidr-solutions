016bab11117e6c6ff713c1fb72aa98ef9c5a05ee
beep boop <githubbot@vadim.me>
2024-07-16 18:06:16 +0200

    Solution 96 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/9jfgiys1 Prompt:       |                    ^~~ 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp:24: 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp: In function ‘int main()’: 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 30b533ac-3c85-467d-ac5b-14ecfdb27310.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,7 +1,19 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b);
+std::vector<int> parse_nested_parens(std::string paren_string);
+
+bool issame(std::vector<int> a, std::vector<int> b){
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 std::vector<int> parse_nested_parens(std::string paren_string){
     std::vector<int> result;
@@ -26,21 +38,7 @@ std::vector<int> parse_nested_parens(std::string paren_string){
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
 int main(){
-    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
+    assert (issame(parse_nested_parens("(()(())((())))"), {4}));
     // Other code
 }
