37884471d570d8a1f0a736147cfd487f5b836dfc
beep boop <githubbot@vadim.me>
2024-07-16 19:47:40 +0200

    Solution 71 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/aut863mg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from fee9105c-49ad-4c71-a328-01afdd25a725.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- find-pair.cpp
+++ find-pair.cpp
@@ -9,16 +9,15 @@ std::vector<std::pair<int, int>> findPairs(std::vector<int>& nums, int target) {
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return {{complement, nums[i]}};
+            return {std::make_pair(complement, nums[i])};
         }
         numMap[nums[i]] = i;
     }
     
     std::vector<std::pair<int, int>> result;
-    result.reserve(nums.size());
     for (const auto& pair : numMap) {
         if (numMap.find(target - pair.first) != numMap.end()) {
-            result.push_back({target - pair.first, pair.first});
+            result.push_back({std::make_pair(target - pair.first, pair.first)});
         }
     }
     
