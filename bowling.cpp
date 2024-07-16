8da64f6046a452f9a9b3a794c3a130468e735514
beep boop <githubbot@vadim.me>
2024-07-16 16:37:10 +0200

    Solution 23 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/pmrvrh9v Prompt: in ‘s’, which is of non-class type ‘const int’     5 |     for (int i = 0; i < s.size(); ++i) {       |                           ^~~~ 92e2a5b8-57e6-4cb1-80b7-30400c2cbdd5.cpp:6:19: error: invalid types ‘const int[i nt]’ for array subscript     6 |         char c = s[i];       |                   ^ 92e2a5b8-57e6-4cb1-80b7-30400c2cbdd5.cpp:16:21: error: ‘isdigit’ was not declare d in this scope    16 |                 if (isdigit(c)) {       |                     ^~~~~~~ 92e2a5b8-57e6-4cb1-80b7-30400c2cbdd5.cpp:19:27: error: request for member ‘find’  in ‘s’, which is of non-class type ‘const int’    19 |                     if (s.find('/', i) < s.size()-1) {       |                           ^~~~ 92e2a5b8-57e6-4cb1-80b7-30400c2cbdd5.cpp:19:44: error: request for member ‘size’  in ‘s’, which is of non-class type ‘const int’    19 |                     if (s.find('/', i) < s.size()-1) {       |                                            ^~~~ 92e2a5b8-57e6-4cb1-80b7-30400c2cbdd5.cpp:29:21: error: ‘isdigit’ was not declare d in this scope    29 |                 if (isdigit(c)) {       |                     ^~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,47 +1,43 @@
-int bowlingScore(const std::string& s) {
+```cpp
+#include <cctype>
+int bowlingScore(const std::string s) {
     int score = 0;
     int currentRoll = 0;
 
     for (int i = 0; i < s.size(); ++i) {
         char c = s[i];
-
+        
         if (c == 'X') {
             score += 10;
-            // handle strike
-            if (i + 3 < s.size()) {
-                int nextRoll1 = s[i+1] - '0';
-                int nextRoll2 = s[i+2] - '0';
-                score += nextRoll1 + nextRoll2;
-            }
+            currentRoll = 0;
         } else if (c == '/') {
-            // handle two-roll frame
-            score += currentRoll;
-            if (i + 3 < s.size()) {
-                int nextRoll = s[i+1] - '0';
-                currentRoll = 10 - c;
-                if (nextRoll > 0) {
-                    currentRoll -= nextRoll;
-                }
-            } else {
-                currentRoll = 0;
-            }
+            score += currentRoll + 5;
+            currentRoll = 0;
         } else {
-            if (isdigit(c)) {
-                if (currentRoll > 9) {
-                    score += currentRoll + c - '0';
-                    currentRoll = 0;
+            if (currentRoll > 9) { 
+                if (std::isdigit(c)) {
+                    int firstRoll = c - '0';
+                    score += firstRoll;
+                    if (s.find('/', i) < s.size()-1) {
+                        currentRoll = 10 - c;
+                    } else {
+                        currentRoll = 20 - c;
+                    }
                 } else {
                     currentRoll *= 10;
                     currentRoll += c - '0';
                 }
             } else {
-                // handle invalid characters
-                return -1; 
+                if (std::isdigit(c)) {
+                    currentRoll *= 10;
+                    currentRoll += c - '0';
+                } else {
+                    currentRoll *= 10;
+                    currentRoll += c - '0';
+                }
             }
         }
     }
-
-    score += currentRoll;
 
     return score;
 }
