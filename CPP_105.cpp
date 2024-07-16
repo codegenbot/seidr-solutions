3d3fcd7204e4ffb9e48033604f2820dcf01c3001
beep boop <githubbot@vadim.me>
2024-07-16 06:58:54 +0200

    Solution 38 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/t4hbw3du Prompt:       |                              ^~~~~~ c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp:25: c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp: In function ‘int main()’: c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ c1f6372e-edf6-489c-9d58-7919ae5f6f6c.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -9,24 +9,24 @@ using namespace std;
 vector<string> by_length(vector<int> arr) {
     vector<string> result;
     map<int, string> num_to_string = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, 
         {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
-
+    
     vector<int> valid_nums;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             valid_nums.push_back(num);
         }
     }
-
+    
     sort(valid_nums.begin(), valid_nums.end());
     reverse(valid_nums.begin(), valid_nums.end());
-
+    
     for (int num : valid_nums) {
         result.push_back(num_to_string[num]);
     }
-
+    
     return result;
 }
 
