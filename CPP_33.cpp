9f64215335bc940a79e56bba87096315c3bc3ce4
beep boop <githubbot@vadim.me>
2024-07-16 04:13:28 +0200

    Solution 48 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/oovhb9h7 Prompt:    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b4581cdd-5df7-487a-af55-f68bf2578287.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b4581cdd-5df7-487a-af55-f68bf2578287.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b4581cdd-5df7-487a-af55-f68bf2578287.cpp:18: b4581cdd-5df7-487a-af55-f68bf2578287.cpp: In function ‘int main()’: b4581cdd-5df7-487a-af55-f68bf2578287.cpp:28:20: error: ‘sort_third’ was not decl ared in this scope    28 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ b4581cdd-5df7-487a-af55-f68bf2578287.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -6,28 +6,32 @@ bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
+
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
-void sort_third(std::vector<int>& l) {
-    std::vector<int> sorted_indices;
+bool sort_third(const vector<int>& l) {
+    vector<int> sorted_indices;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
             sorted_indices.push_back(i);
         }
     }
-    std::sort(sorted_indices.begin(), sorted_indices.end(), [&](int a, int b) {
+    sort(sorted_indices.begin(), sorted_indices.end(), [&](int a, int b) {
         return l[a] < l[b];
     });
 
     for (int i = 0; i < sorted_indices.size(); ++i) {
         l[sorted_indices[i]] = l[i];
     }
+
+    return l;
 }
 
 int main() {
