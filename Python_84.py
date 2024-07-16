8aaba6b097185afb4442eb233e2d78426032ba80
beep boop <githubbot@vadim.me>
2024-07-16 03:05:36 +0200

    Solution 16 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dx1ic3yv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e2663204-651d-43cb-b36a-b9ff9a3d2 a51.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,9 +1,6 @@
-````
 def solve(N):
-    """"""
     if N == 0:
         return "0"
     s = str(bin(N)[2:])
     odd_sum = sum(int(i) for i in s if (i != '0' and int(i)) % 2)
     return format(odd_sum, 'b')[2:]
-"""```
