82157fe694b8aa04598d92f80d75940e938096bf
beep boop <githubbot@vadim.me>
2024-07-16 17:42:36 +0200

    Solution 1 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/crve8xko Prompt:    17 | } else if(a.type() == typeid(string) && b.type() == typeid(string)){       |   ^~~~ 75d16f8b-88cb-490a-b07a-0bd694c25182.cpp:34:3: error: expected unqualified-id be fore ‘else’    34 | } else {       |   ^~~~ In file included from 75d16f8b-88cb-490a-b07a-0bd694c25182.cpp:38: 75d16f8b-88cb-490a-b07a-0bd694c25182.cpp: In function ‘int main()’: 75d16f8b-88cb-490a-b07a-0bd694c25182.cpp:40:13: error: ‘boost’ has not been decl ared    40 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 75d16f8b-88cb-490a-b07a-0bd694c25182.cpp:40:29: error: ‘string’ was not declared  in this scope    40 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 75d16f8b-88cb-490a-b07a-0bd694c25182.cpp:40:37: error: ‘compare_one’ was not dec lared in this scope    40 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,36 +1,43 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
-        return a;
-    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
-        return b;
+#include <boost/any.hpp>
+#include <algorithm>
+
+using namespace std;
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if(a.type() == typeid(int) && b.type() == typeid(int)){
+        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+            return a;
+        } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+            return b;
+        } else {
+            return "None";
+        }
+    } else if(a.type() == typeid(float) && b.type() == typeid(float)){
+        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+            return a;
+        } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+            return b;
+        } else {
+            return "None";
+        }
+    } else if(a.type() == typeid(string) && b.type() == typeid(string)){
+        float num1, num2;
+        if(boost::any_cast<string>(a).find(",") != string::npos){
+            replace(boost::any_cast<string&>(a).begin(), boost::any_cast<string&>(a).end(), ',', '.');
+        }
+        if(boost::any_cast<string>(b).find(",") != string::npos){
+            replace(boost::any_cast<string&>(b).begin(), boost::any_cast<string&>(b).end(), ',', '.');
+        }
+        num1 = stof(boost::any_cast<string>(a));
+        num2 = stof(boost::any_cast<string>(b));
+        if(num1 > num2){
+            return a;
+        } else if(num1 < num2){
+            return b;
+        } else {
+            return "None";
+        }
     } else {
         return "None";
     }
-} else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
-        return a;
-    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
-        return b;
-    } else {
-        return "None";
-    }
-} else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    float num1, num2;
-    if(((string)boost::any_cast<string>(a)).find(",") != string::npos){
-        replace(((string&)boost::any_cast<string>(a)).begin(), ((string&)boost::any_cast<string>(a)).end(), ',', '.');
-    }
-    if(((string)boost::any_cast<string>(b)).find(",") != string::npos){
-        replace(((string&)boost::any_cast<string>(b)).begin(), ((string&)boost::any_cast<string>(b)).end(), ',', '.');
-    }
-    num1 = stof(boost::any_cast<string>(a));
-    num2 = stof(boost::any_cast<string>(b));
-    if(num1 > num2){
-        return a;
-    } else if(num1 < num2){
-        return b;
-    } else {
-        return "None";
-    }
-} else {
-    return "None";
 }
