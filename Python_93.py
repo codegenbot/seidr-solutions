c5c192040371cb43031844d400c5aa8bd9104578
beep boop <githubbot@vadim.me>
2024-07-16 04:57:03 +0200

    Solution 12 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yuz53mar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/44da294f-034a-4a35-9673-60d5d15c7 b0e.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/44da294f-034a-4a35-9673-60d5d15c7 b0e.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,9 +1,12 @@
 def encode(message):
     vowels = "aeiouAEIOU"
     return "".join(
-        chr(ord(char) + 1) if char.isalpha() and char.upper() in vowels
-        else char.swapcase() if char.isalpha() else char
+        (
+            chr(ord(char) + 1)
+            if char.isalpha() and char.upper() in vowels
+            else char.swapcase()
+            if char.isalpha() and char.lower() != "a"
+            else char
+        )
         for char in message
-    ).lower()
-
-print(encode(input()))
+    )
