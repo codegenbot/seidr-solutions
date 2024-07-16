cf39532548d03857f8c1e1a28624e41abc8b20a3
beep boop <githubbot@vadim.me>
2024-07-16 16:56:41 +0200

    Solution 6 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ivkn9r35 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,36 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
         return a;
-    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
         return b;
+    } else {
+        return "None";
     }
-} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+} else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
         return a;
-    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
         return b;
+    } else {
+        return "None";
     }
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
+} else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    float num1, num2;
+    if(boost::any_cast<string>(a).find(',') != string::npos){
+        replace(boost::any_cast<string>(a).begin(), boost::any_cast<string>(a).end(), ',', '.');
+    }
+    if(boost::any_cast<string>(b).find(',') != string::npos){
+        replace(boost::any_cast<string>(b).begin(), boost::any_cast<string>(b).end(), ',', '.');
+    }
+    num1 = stof(boost::any_cast<string>(a));
+    num2 = stof(boost::any_cast<string>(b));
+    if(num1 > num2){
         return a;
-    } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
+    } else if(num1 < num2){
         return b;
+    } else {
+        return "None";
     }
-}
-return "None";
+} else {
+    return "None";
 }
