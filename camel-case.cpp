043c62aed927878ef1745d236ee2429ac2c29140
beep boop <githubbot@vadim.me>
2024-07-16 16:15:58 +0200

    Solution 58 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/8knc5rk7 Prompt: Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 #include <string>
 
-std::string camelCase(std::string s) {
-    std::string result = "";
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '-') {
-            i++;
-            while (i < s.size() && s[i] == ' ') {
-                i++;
-            }
-            result += toupper(s[i]);
-        } else if (s[i] == ' ') {
-            continue;
-        } else {
-            if (!result.empty()) {
-                result += tolower(s[i]);
-            } else {
-                result += toupper(s[i]);
-            }
+std::string camelCase(std::string str) {
+    std::vector<std::string> words;
+    size_t pos = 0;
+    while ((pos = str.find(' ')) != std::string::npos) {
+        words.push_back(str.substr(0, pos));
+        str.erase(0, pos + 1);
+    }
+    if (!str.empty()) {
+        words.push_back(str);
+    }
+
+    std::string result;
+    for (size_t i = 0; i < words.size(); ++i) {
+        if (i > 0)
+            result += std::toupper(words[i][0]);
+        else
+            result += words[i];
+        for (size_t j = 1; j < words[i].size(); ++j) {
+            result += std::tolower(words[i][j]);
         }
     }
+
     return result;
 }
 
 int main() {
-    std::string s;
-    while (std::cin >> s) {
-        std::cout << camelCase(s) << std::endl;
+    int t;
+    cin >> t;
+    while(t--) {
+        string str;
+        cin >> str;
+        cout << camelCase(str) << endl;
     }
     return 0;
 }
