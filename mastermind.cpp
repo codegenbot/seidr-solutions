53bb0d66118f775811f1304920088f10820d78f7
beep boop <githubbot@vadim.me>
2024-07-16 12:44:49 +0200

    Solution 14 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p9s49ghj Prompt:       |             ^~~~~~~~~ a9d25a29-7ad8-4758-b68a-93883a04ba99.cpp:20:13: error: ‘guessCount’ was not decl ared in this scope    20 |             guessCount[guess[i] - 'A']++;       |             ^~~~~~~~~~ a9d25a29-7ad8-4758-b68a-93883a04ba99.cpp:25:22: error: ‘codeCount’ was not decla red in this scope    25 |         white += min(codeCount[i], guessCount[i]) - black;       |                      ^~~~~~~~~ a9d25a29-7ad8-4758-b68a-93883a04ba99.cpp:25:36: error: ‘guessCount’ was not decl ared in this scope    25 |         white += min(codeCount[i], guessCount[i]) - black;       |                                    ^~~~~~~~~~ a9d25a29-7ad8-4758-b68a-93883a04ba99.cpp:25:18: error: ‘min’ was not declared in  this scope; did you mean ‘std::min’?    25 |         white += min(codeCount[i], guessCount[i]) - black;       |                  ^~~       |                  std::min In file included from /usr/include/c++/11/vector:60,                  from a9d25a29-7ad8-4758-b68a-93883a04ba99.cpp:1: /usr/include/c++/11/bits/stl_algobase.h:278:5: note: ‘std::min’ declared here   278 |     min(const _Tp& __a, const _Tp& __b, _Compare __comp)       |     ^~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,14 +1,9 @@
-```cpp
-#include <vector>
-#include <iostream>
-#include <string>
-
-int mastermind(std::string code, std::string guess) {
+std::tuple<int, int> mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    std::vector<int> codeCount(6, 0);
-    std::vector<int> guessCount(6, 0);
+    vector<int> codeCount(6, 0);
+    vector<int> guessCount(6, 0);
 
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
@@ -21,10 +16,9 @@ int mastermind(std::string code, std::string guess) {
         }
     }
 
-    white = 0;
     for (int i = 0; i < 6; i++) {
         white += std::min(codeCount[i], guessCount[i]) - black;
     }
 
-    return white;
+    return std::make_tuple(white, black);
 }
