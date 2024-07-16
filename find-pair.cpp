d651711644dccbb2c136df47866a34c5a191318c
beep boop <githubbot@vadim.me>
2024-07-16 20:26:02 +0200

    Solution 82 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/aut863mg Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = const std::pair<int, int>*; _F orwardIterator = std::pair<int, int>*; _Tp = std::pair<int, int>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::pair<int, int>*; _Tp = std ::pair<int, int>; _Alloc = std::allocator<std::pair<int, int> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::pair<int, int>; _Alloc = std::allocator<std::pair<int, int> >; std::vector<_ Tp, _Alloc>::allocator_type = std::allocator<std::pair<int, int> >]’ 27f44f34-06c3-40b2-96d9-0ecd4a0ea306.cpp:12:56:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,14 +1,15 @@
-```cpp
 std::vector<std::pair<int, int>> findPairs(std::vector<int>& nums, int target) {
     std::unordered_map<int, int> numMap;
+    
+    std::vector<std::pair<int, int>> result;
     
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
-            return {{complement, nums[i]}};
+            result.push_back({complement, nums[i]});
         }
         numMap[nums[i]] = i;
     }
     
-    return {};
+    return result;
 }
