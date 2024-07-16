023b399a61095476f299c53fb0f90b9dd80af86c
beep boop <githubbot@vadim.me>
2024-07-16 05:39:43 +0200

    Solution 32 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rwjg9ra8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97aa56f3-cd89-4c8a-bb8e-18cbf233b 403.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97aa56f3-cd89-4c8a-bb8e-18cbf233b 403.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,5 @@
-```
-def sort_third(l):
-    result = []
-    i = 0
-    while i < len(l):
-        if i % 3 == 0:
-            temp = [x for j in range(i + 1) if (j % 3) != 0]
-            result.append(sorted(temp)[0])
-        else:
-            result.append(l[i])
-        i += 1
-    return result
+def sort_third(l: list):
+    return [
+        x if i % 3 != 0 else sorted([x for x in l[:i + 1] if i % 3 == 0])[0]
+        for i, x in enumerate(reversed(l))
+    ]
