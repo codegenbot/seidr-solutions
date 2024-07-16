d5fcddc7eb3efdf469e9f590b427579d39e19e61
beep boop <githubbot@vadim.me>
2024-07-16 09:56:14 +0200

    Solution 30 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rqcq4htz Prompt:       |                    ^~~ ff2284a4-b908-4053-8712-8501ecc21f67.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ff2284a4-b908-4053-8712-8501ecc21f67.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ff2284a4-b908-4053-8712-8501ecc21f67.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ff2284a4-b908-4053-8712-8501ecc21f67.cpp:9: ff2284a4-b908-4053-8712-8501ecc21f67.cpp: In function ‘int main()’: ff2284a4-b908-4053-8712-8501ecc21f67.cpp:19:20: error: ‘generate_integers’ was n ot declared in this scope    19 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ ff2284a4-b908-4053-8712-8501ecc21f67.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,23 +1,13 @@
 #include <vector>
 
-bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<int> generate_integers(int a, int b) {
     vector<int> result;
     for (int i = a; i <= b; ++i) {
-        if (i % 2 == 0) {
-            result.push_back(i);
-        }
+        result.push_back(i);
     }
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
