5950a232cbadf8e0ff27156db748ac9093b8b5c2
beep boop <githubbot@vadim.me>
2024-07-16 06:14:46 +0200

    Solution 60 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/6vzj13p0 Prompt:       |                    ^~~ 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp:18: 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp: In function ‘int main()’: 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp:28:20: error: ‘strange_sort_list’ was n ot declared in this scope    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 1032ab5f-370f-45ea-9383-a2034b6e188d.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,20 +1,13 @@
 #include <vector>
+#include <algorithm>
 
-bool is_same(vector<int> a, vector<int> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
 }
 
-vector<int> strange_sort_list(vector<int> lst){
-    sort(lst.begin(), lst.end());
-    vector<int> result;
+std::vector<int> strange_sort_list(std::vector<int> lst){
+    std::sort(lst.begin(), lst.end());
+    std::vector<int> result;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
         if (left == right) {
