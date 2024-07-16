1945e088783e199ad4102b51b396d9e0a2df9718
beep boop <githubbot@vadim.me>
2024-07-16 05:48:00 +0200

    Solution 38 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/p4orujsn Prompt: 9645832a-5fa0-459f-9cb8-9958d62b7a7d.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 9645832a-5fa0-459f-9cb8-9958d62b7a7d.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 9645832a-5fa0-459f-9cb8-9958d62b7a7d.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 9645832a-5fa0-459f-9cb8-9958d62b7a7d.cpp:2:10: fatal error: boost/variant2/varia nt.hpp: No such file or directory     2 | #include <boost/variant2/variant.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,37 +5,37 @@
 
 boost::variant<int, float, std::string> compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::get<int>(a.convert_to<boost::any>()) > boost::get<float>(b.convert_to<boost::any>()) ? boost::variant<int, float, std::string>(boost::get<int>(a.convert_to<boost::any>())) : boost::variant<int, float, std::string>(boost::get<float>(b.convert_to<boost::any>()));
+        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
     }
     else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
-        float f = boost::get<float>(a.convert_to<boost::any>());
-        std::string str = boost::get<std::string>(b.convert_to<boost::any>());
-        return f > std::stod(str) ? boost::variant<int, float, std::string>(f) : boost::variant<int, float, std::string>(str);
+        std::string str = (std::string)a.convert_to<std::string>();
+        float f = (float)b.convert_to<float>();
+        return (f > std::stod(str)) ? boost::variant<int, float, std::string>(f) : boost::variant<int, float, std::string>(str);
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string str1 = boost::get<std::string>(a.convert_to<boost::any>());
-        std::string str2 = boost::get<std::string>(b.convert_to<boost::any>());
-        return std::stod(str2) > std::stod(str1) ? boost::variant<int, float, std::string>(str2) : (std::stod(str1) == std::stod(str2)) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(str1);
+        std::string str1 = (std::string)a.convert_to<std::string>();
+        std::string str2 = (std::string)b.convert_to<std::string>();
+        return (std::stod(str2) > std::stod(str1)) ? boost::variant<int, float, std::string>(str2) : (std::stod(str1) == std::stod(str2)) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(str1);
     }
     else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        int i = boost::get<int>(a.convert_to<boost::any>());
-        std::string str = boost::get<std::string>(b.convert_to<boost::any>());
-        return std::stod(str) > i ? boost::variant<int, float, std::string>(str) : (i == std::stod(str)) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i);
+        int i = (int)a.convert_to<int>();
+        std::string str = (std::string)b.convert_to<std::string>();
+        return (std::stod(str) > i) ? boost::variant<int, float, std::string>(str) : (i == std::stod(str)) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i);
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
-        std::string str = boost::get<std::string>(a.convert_to<boost::any>());
-        int i = boost::get<int>(b.convert_to<boost::any>());
-        return std::stod(str) > i ? boost::variant<int, float, std::string>(str) : (i == std::stod(str)) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i);
+        std::string str = (std::string)a.convert_to<std::string>();
+        int i = (int)b.convert_to<int>();
+        return (std::stod(str) > i) ? boost::variant<int, float, std::string>(str) : (i == std::stod(str)) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i);
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float f = boost::get<float>(a.convert_to<boost::any>());
-        int i = boost::get<int>(b.convert_to<boost::any>());
-        return f > i ? boost::variant<int, float, std::string>(f) : (i == f) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i);
+        float f = (float)a.convert_to<float>();
+        int i = (int)b.convert_to<int>();
+        return (f > i) ? boost::variant<int, float, std::string>(f) : (i == f) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i);
     }
     else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int i1 = boost::get<int>(a.convert_to<boost::any>());
-        int i2 = boost::get<int>(b.convert_to<boost::any>());
-        return i2 > i1 ? boost::variant<int, float, std::string>(i2) : (i1 == i2) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i1);
+        int i1 = (int)a.convert_to<int>();
+        int i2 = (int)b.convert_to<int>();
+        return (i2 > i1) ? boost::variant<int, float, std::string>(i2) : (i1 == i2) ? boost::variant<int, float, std::string>("None") : boost::variant<int, float, std::string>(i1);
     }
     else {
         return "None";
@@ -52,10 +52,7 @@ int main() {
         std::cout << "The result is: " << boost::get<std::string>(result) << std::endl;
     }
     else {
-        if (boost::holds_alternative<int>(result))
-            std::cout << "The result is: " << boost::get<int>(result) << std::endl;
-        else
-            std::cout << "The result is: " << boost::get<float>(result) << std::endl;
+        std::cout << "The result is: " << boost::get<int>(result) << " or " << boost::get<float>(result) << std::endl;
     }
 
     return 0;
