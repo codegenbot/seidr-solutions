6233ba190a3fb47d7f09ca947c81609b06ba5ebf
beep boop <githubbot@vadim.me>
2024-07-16 14:02:25 +0200

    Solution 87 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vrvge3d4 Prompt: Program did not receive expected input

--- spin-words.cpp
+++ spin-words.cpp
@@ -5,20 +5,25 @@ string spinWords(string str) {
     string result = "";
     int i = 0;
     while (i < str.length()) {
-        int j = 0;
-        while (i < str.length() && str[i] != ' ') {
+        if (str[i] == ' ') {
+            result += " ";
             i++;
-            j++;
-        }
-        string word = str.substr(i - j, j);
-        if (word.length() >= 5) {
-            for (int k = word.length() - 1; k >= 0; k--) {
-                result += word[k];
-            }
         } else {
-            result += word;
+            int j = i;
+            while (j < str.length() && str[j] != ' ') {
+                j++;
+            }
+            string word = str.substr(i, j - i);
+            i = j;
+
+            if (word.size() >= 5) {
+                for (int k = word.size() - 1; k >= 0; k--) {
+                    result += word[k];
+                }
+            } else {
+                result += word;
+            }
         }
-        i++;
     }
     return result;
 }
