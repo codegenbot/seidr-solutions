7ccec4923ca91e21e3b5628db9ef2ef186ff9498
beep boop <githubbot@vadim.me>
2024-07-16 06:27:50 +0200

    Solution 96 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vxc8xfvh Prompt:       |                    ^~~ 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp:9: 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp: In function ‘int main()’: 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8c286fbe-e4a3-4a55-82e5-e002e7f4a13a.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,14 +1,15 @@
 #include <vector>
 
-vector<int> common(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+vector<int> common(vector<int> l1, vector<int> l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
     vector<int> result;
-    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
 }
 
 int main() {
     assert(issame(common({4, 3, 2, 8}, {}), {}));
+    return 0;
 }
