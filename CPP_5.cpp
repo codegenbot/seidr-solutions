56d217821aa4de8d3548e4cfdf3165a783cfeeee
beep boop <githubbot@vadim.me>
2024-07-16 18:10:33 +0200

    Solution 55 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/56b48a7o Prompt:       |                    ^~~ 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp:12: 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp: In function ‘int main()’: 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 2dc84f6f-7e16-4933-9f4c-951a72a20f3c.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-bool issame(const vector<int>& v1, const vector<int>& v2){
+bool issame(vector<int> v1, vector<int> v2){
     if(v1.size() != v2.size()){
         return false;
     }
@@ -12,4 +12,4 @@ bool issame(const vector<int>& v1, const vector<int>& v2){
     return true;
 }
 
-vector<int> intersperse(const vector<int>& numbers, int delimeter);
+vector<int> intersperse(vector<int> numbers, int delimeter);
