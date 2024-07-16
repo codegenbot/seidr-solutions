5eae8be8b32ec6b7660dfa27f92e39e1a6bdf1df
beep boop <githubbot@vadim.me>
2024-07-16 03:43:32 +0200

    Solution 10 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fq3haqk5 Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 54cb30a8-c0ec-4850-a58e-16e7ebf9e8c9.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 54cb30a8-c0ec-4850-a58e-16e7ebf9e8c9.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 54cb30a8-c0ec-4850-a58e-16e7ebf9e8c9.cpp:10: 54cb30a8-c0ec-4850-a58e-16e7ebf9e8c9.cpp: In function ‘int main()’: 54cb30a8-c0ec-4850-a58e-16e7ebf9e8c9.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 54cb30a8-c0ec-4850-a58e-16e7ebf9e8c9.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,14 +1,13 @@
-for (int i = 0; i < l.size(); ++i) {
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> sort_third(vector<int> l) {
+    vector<int> l_copy = l;
+    for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
-            vector<int> temp;
-            for (int j = i; j < l.size() && j % 3 == 0; ++j) {
-                temp.push_back(l[j]);
-            }
-            sort(temp.begin(), temp.end());
-            for (int j = i; j < l.size() && j % 3 == 0; ++j) {
-                l[j] = temp[j - i];
-            }
+            sort(l_copy.begin() + i, l_copy.begin() + i + 3);
         }
     }
-    return l;
+    return l_copy;
 }
