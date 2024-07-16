1f4cae962ddd6ae4e229260a1c3caa9565ef116b
beep boop <githubbot@vadim.me>
2024-07-16 17:58:26 +0200

    Solution 40 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/0eiv3xjh Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bbb82798-ea1c-4c90-b5d5-71718f99a472.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bbb82798-ea1c-4c90-b5d5-71718f99a472.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bbb82798-ea1c-4c90-b5d5-71718f99a472.cpp:25: bbb82798-ea1c-4c90-b5d5-71718f99a472.cpp: In function ‘int main()’: bbb82798-ea1c-4c90-b5d5-71718f99a472.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ bbb82798-ea1c-4c90-b5d5-71718f99a472.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,13 +1,5 @@
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
 }
 
 vector<string> separate_paren_groups(string paren_string) {
@@ -33,15 +25,4 @@ vector<string> separate_paren_groups(string paren_string) {
     }
 
     return result;
-}
-
-int main() {
-    vector<string> input = {"(abc)", "((def))", "((ghi(jkl))mno)"};
-    vector<string> expected_output = {"abc", "def", "ghi(jkl)"};
-    
-    vector<string> actual_output = separate_paren_groups(input);
-    
-    assert(issame(actual_output, expected_output));
-    
-    return 0;
 }
