88b7ff753de0db68957674c69e7a10cd79802bcf
beep boop <githubbot@vadim.me>
2024-07-16 15:40:15 +0200

    Solution 2 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yqh3qu1h Prompt:    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 69ab0d9b-54a9-4dfc-be51-d75c963981ab.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 69ab0d9b-54a9-4dfc-be51-d75c963981ab.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 69ab0d9b-54a9-4dfc-be51-d75c963981ab.cpp:18: 69ab0d9b-54a9-4dfc-be51-d75c963981ab.cpp: In function ‘int main()’: 69ab0d9b-54a9-4dfc-be51-d75c963981ab.cpp:28:20: error: ‘parse_music’ was not dec lared in this scope    28 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 69ab0d9b-54a9-4dfc-be51-d75c963981ab.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,19 +1,36 @@
-vector<int> parse_music(string music_string){
-    vector<int> beats;
-    int duration = 0;
-    for (char note : music_string) {
-        if (note == 'o') {
-            beats.push_back(duration + 1);
-            duration = 0;
-        } else if (note == '|') {
-            beats.push_back(duration);
-            duration = 0;
-        } else if (note == '.') {
-            duration++;
+vector<int> parse_music(string music_string);
+
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
         }
     }
-    if (duration > 0) {
-        beats.push_back(duration);
+    return true;
+}
+
+int main() {
+    string music_string = "o|o||oo";
+    vector<int> beats = parse_music(music_string);
+    return 0;
+}
+
+vector<int> parse_music(string music_string){
+    vector<int> beats;
+    int length = music_string.length();
+    for (int i = 0; i < length; i++) {
+        if (music_string[i] == 'o') {
+            beats.push_back(4);
+        } else if (music_string[i] == '|') {
+            if (music_string[i - 1] == 'o') {
+                beats.back() = 2;
+            } else {
+                beats.back() = 1;
+            }
+        }
     }
     return beats;
 }
