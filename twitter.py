c5e7b082e28604e1ee476b0aa74410948c829342
beep boop <githubbot@vadim.me>
2024-07-16 23:15:04 +0200

    Solution 31 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8tr8sey0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7cdce3a6-0eaa-40b4-8fad-2249d41e7 9db.py", line 9, in <module>     print(validate_tweet(""))           ^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7cdce3a6-0eaa-40b4-8fad-2249d41e7 9db.py", line 2, in validate_tweet     if len(tweet.replace(" ", "").replace("!@#$%^&*()_+-={}:;<>?,.<\/|\\[]{}").l ower()) > 140:            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: replace expected at least 2 arguments, got 1

--- twitter.py
+++ twitter.py
@@ -1,14 +1,7 @@
-```
 def validate_tweet(tweet):
-    if len(tweet.lower().translate(str.maketrans("", "", "!@#$%^&*()_+-={}:;<>?,.<\/|\\[]{}")).replace(" ", "")) > 140:
+    if len(tweet.replace(" ", "").replace("!@#$%^&*()_+-={}:;<>?,.<\/|\\[]{}", "")) > 140:
         return "Too many characters"
     elif tweet == "":
         return "You didn't type anything"
     else:
         return f"Your tweet has {len(tweet)} characters"
-
-print(validate_tweet(""))
-print(validate_tweet("1"))
-print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeat it"))
-print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)"))
-print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
