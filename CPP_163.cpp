48719b3cb026ce1daf3c45a498f2ef9204ed2508
beep boop <githubbot@vadim.me>
2024-07-16 10:53:36 +0200

    Solution 81 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rqcq4htz Prompt:       |                    ^~~ 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp:9: 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp: In function ‘int main()’: 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp:19:20: error: ‘generate_integers’ was n ot declared in this scope    19 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 1ed7caba-aea7-4560-ba35-50d3e85238d6.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,16 +1,14 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b){
+bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
 
@@ -25,14 +23,6 @@ vector<int> generate_integers(int a, int b) {
 }
 
 int main() {
-    vector<int> vec1 = generate_integers(1, 10);
-    vector<int> vec2 = generate_integers(2, 11);
-    
-    if (issame(vec1, vec2)) {
-        cout << "Vectors are the same" << endl;
-    } else {
-        cout << "Vectors are different" << endl;
-    }
-    
+    // Call generate_integers function here
     return 0;
 }
