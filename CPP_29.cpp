fac8095aa47acce95170f3f2f7552301b6d1c0aa
beep boop <githubbot@vadim.me>
2024-07-16 04:28:27 +0200

    Solution 62 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/sa8bqh14 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cbb8dafe-c0d6-40f5-87f5-6c8b8c5dee23.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cbb8dafe-c0d6-40f5-87f5-6c8b8c5dee23.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cbb8dafe-c0d6-40f5-87f5-6c8b8c5dee23.cpp:11: cbb8dafe-c0d6-40f5-87f5-6c8b8c5dee23.cpp: In function ‘int main()’: cbb8dafe-c0d6-40f5-87f5-6c8b8c5dee23.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ cbb8dafe-c0d6-40f5-87f5-6c8b8c5dee23.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -7,19 +7,11 @@ using namespace std;
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix);
 
-bool issame(vector<string> a, vector<string> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<string> a, vector<string> b) {
+    // Implementation of issame function
 }
 
-vector<string> filter_by_prefix(vector<string> strings, string prefix){
+vector<string> filter_by_prefix(vector<string> strings, string prefix) {
     vector<string> filtered_strings;
     for (const string& str : strings) {
         if (str.find(prefix) == 0) {
@@ -30,7 +22,7 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
 }
 
 int main() {
-    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
-    
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"});
+
     return 0;
 }
