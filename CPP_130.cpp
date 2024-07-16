0163e04805b4521d31c28880078ea4a7176efc8b
beep boop <githubbot@vadim.me>
2024-07-16 17:26:14 +0200

    Solution 55 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bdyu2gir Prompt: Complete the following code given the task description and function signature.

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,14 +1,13 @@
-vector<int> tri(int n){
-    vector<int> result;
+vector<int> res;
     if(n >= 0){
-        result.push_back(3);
-        for(int i = 1; i <= n; i++){
+        res.push_back(3);
+        for(int i = 1; i <= n; ++i){
             if(i % 2 == 0){
-                result.push_back(1 + i / 2);
+                res.push_back(1 + i / 2);
             } else {
-                result.push_back(result[i - 1] + result[i - 2] + result[i + 1]);
+                res.push_back(res[i - 1] + res[i - 2] + res[i + 1]);
             }
         }
     }
-    return result;
+    return res;
 }
