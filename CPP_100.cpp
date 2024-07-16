4d9e76c9ea782e7f4b3265adb5cf696e6b7fd0de
beep boop <githubbot@vadim.me>
2024-07-16 04:29:04 +0200

    Solution 2 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5lrudh85 Prompt:       |                    ^~~ 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp:17: 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp: In function ‘int main()’: 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 4bfafa4d-c8a2-461d-89dc-0bf4f2a42a70.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,8 +1,13 @@
+```cpp
 #include <vector>
-using namespace std;
+#include <cassert>
 
-bool issame(vector<int> a,vector<int>b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<int> make_a_pile(int n) {
@@ -10,11 +15,11 @@ vector<int> make_a_pile(int n) {
     int stones = n;
     while (stones > 0) {
         pile.push_back(stones);
-        if (n % 2 == 1) {
+        if(n % 2 == 1) {
             n++;
             stones = n;
         } else {
-            n++;
+            n += 2;
             stones = n;
         }
     }
@@ -22,6 +27,5 @@ vector<int> make_a_pile(int n) {
 }
 
 int main() {
-    assert(make_a_pile(8) == vector<int>({8, 10, 12, 14, 16, 18, 20, 22}));
-    return 0;
+    assert(std::equal(make_a_pile(8).begin(), make_a_pile(8).end(), {8, 10, 12, 14, 16, 18, 20, 22}.begin()));
 }
