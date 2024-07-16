dd5038b7faaf73829767802eb04c46b4c5d8a303
beep boop <githubbot@vadim.me>
2024-07-16 04:53:43 +0200

    Solution 70 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/3685c2rc Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 01e06505-d0f5-4df3-b6de-623edb4490f5.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 01e06505-d0f5-4df3-b6de-623edb4490f5.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 01e06505-d0f5-4df3-b6de-623edb4490f5.cpp:20: 01e06505-d0f5-4df3-b6de-623edb4490f5.cpp: In function ‘int main()’: 01e06505-d0f5-4df3-b6de-623edb4490f5.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 01e06505-d0f5-4df3-b6de-623edb4490f5.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,20 +1,33 @@
-vector<float> sort_even(vector<float> l) {
-    vector<float> sorted_even_indices;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
 
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a == b;
+}
+
+std::vector<float> sort_even(std::vector<float> l) {
+    std::vector<float> even_values;
+    std::vector<float> sorted_even_values;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            sorted_even_indices.push_back(l[i]);
+            even_values.push_back(l[i]);
+            sorted_even_values.push_back(l[i]);
         }
     }
-
-    sort(sorted_even_indices.begin(), sorted_even_indices.end());
-
-    for (int i = 0, j = 0; i < l.size(); i++) {
+    sort(sorted_even_values.begin(), sorted_even_values.end());
+    int j = 0;
+    for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            l[i] = sorted_even_indices[j];
+            l[i] = sorted_even_values[j];
             j++;
         }
     }
-
     return l;
+}
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
 }
