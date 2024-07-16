046294e6aa610a6eda09b1aea29cc269bd7aa27b
beep boop <githubbot@vadim.me>
2024-07-16 17:50:08 +0200

    Solution 25 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3v8snoyk Prompt:       |      ^~~~~~ 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(std::vector<int> a, std::vector<int> b) {       |      ^~~~~~ 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp:25: 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp: In function ‘int main()’: 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp:35:35: error: too few arguments to func tion ‘bool order_by_points(std::vector<int>, std::vector<int>)’    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 0fb80945-34db-4562-b2d9-8e2fca190b2e.cpp:16:6: note: declared here    16 | bool order_by_points(std::vector<int> a, std::vector<int> b) {       |      ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -4,17 +4,22 @@
 
 using namespace std;
 
-std::vector<int> sort_vector(std::vector<int> nums){
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> sort_vector(std::vector<int> nums) {
     std::sort(nums.begin(), nums.end());
     return nums;
 }
 
-bool order_by_points(std::vector<int> a, std::vector<int> b){
-    return sort_vector(a) == sort_vector(b);
+bool order_by_points(std::vector<int> a, std::vector<int> b) {
+    return a.size() < b.size();
 }
 
-int main(){
+int main() {
     assert(issame(sort_vector({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
     assert(order_by_points({0, 6, 6, -76, -21, 23, 4}, {-76, -21, 0, 4, 23, 6, 6}));
+    
     return 0;
 }
