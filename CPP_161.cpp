5547192d2599bd992386c4dd6e50e4fbf612abb5
beep boop <githubbot@vadim.me>
2024-07-16 17:34:15 +0200

    Solution 30 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/8s2rpfuf Prompt: Complete the following code given the task description and function signature.

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -3,7 +3,7 @@ for (char &c : s) {
             c = islower(c) ? toupper(c) : tolower(c);
         }
     }
-    if (count_if(s.begin(), s.end(), isalpha) == 0) {
+    if (none_of(s.begin(), s.end(), ::isalpha)) {
         reverse(s.begin(), s.end());
     }
     return s;
