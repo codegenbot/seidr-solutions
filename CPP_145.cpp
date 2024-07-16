a751a453f51266a06b44fcb172a9ef8b4667855e
beep boop <githubbot@vadim.me>
2024-07-16 03:01:16 +0200

    Solution 5 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zvhhpgja Prompt:       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from e3a5a16b-3b8a-45ca-940b-182aa3678957.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ e3a5a16b-3b8a-45ca-940b-182aa3678957.cpp:5:40: note:   initializing argument 1 o f ‘void order_by_points(std::vector<int>&)’     5 | void order_by_points(std::vector<int>& nums){       |                      ~~~~~~~~~~~~~~~~~~^~~~ In file included from e3a5a16b-3b8a-45ca-940b-182aa3678957.cpp:32: e3a5a16b-3b8a-45ca-940b-182aa3678957.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,9 +1,13 @@
 #include <algorithm>
 #include <vector>
 #include <cassert>
+#include <cmath>
+#include <numeric>
+
+#define issame(a, b) (std::equal(a.begin(), a.end(), b.begin(), b.end()))
 
 void order_by_points(std::vector<int>& nums){
-    sort(nums.begin(), nums.end(), [&nums](int a, int b) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         int temp_a = abs(a), temp_b = abs(b);
         while (temp_a > 0) {
@@ -14,18 +18,16 @@ void order_by_points(std::vector<int>& nums){
             sum_b += temp_b % 10;
             temp_b /= 10;
         }
-        if (sum_a == sum_b) {
-            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
-        }
-        return sum_a < sum_b;
+        int index_a = find(nums.begin(), nums.end(), a) - nums.begin();
+        int index_b = find(nums.begin(), nums.end(), b) - nums.begin();
+        return index_a < index_b;
     });
 }
 
 int main() {
-    std::vector<int> nums = {0, 6, 6, -76, -21, 23, 4};
-    order_by_points(nums);
-    
-    assert(nums == std::vector<int>{-76, -21, 0, 4, 23, 6, 6});
+    std::vector<int> input = {0, 6, 6, -76, -21, 23, 4};
+    order_by_points(input);
+    assert(issame(input, std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
     
     return 0;
 }
