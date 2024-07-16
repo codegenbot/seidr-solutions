85a7d23bbc3dd102718371d4d5c60fb038ede359
beep boop <githubbot@vadim.me>
2024-07-16 15:03:42 +0200

    Solution 45 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6e2io3hy Prompt: dfbe2888-7cc9-450d-9c8e-2b2f863561c0.cpp: In function ‘int bowlingScore(std::str ing)’: dfbe2888-7cc9-450d-9c8e-2b2f863561c0.cpp:9:27: error: ‘getBonus’ was not declare d in this scope     9 |             score += 10 + getBonus(s, i);       |                           ^~~~~~~~ dfbe2888-7cc9-450d-9c8e-2b2f863561c0.cpp:11:26: error: ‘getBonus’ was not declar ed in this scope    11 |             score += 5 + getBonus(s.substr(i+1), 0);       |                          ^~~~~~~~ dfbe2888-7cc9-450d-9c8e-2b2f863561c0.cpp:13:13: error: ‘roll1’ was not declared in this scope    13 |             roll1 = s[i] - '0';       |             ^~~~~ dfbe2888-7cc9-450d-9c8e-2b2f863561c0.cpp:15:17: error: ‘roll2’ was not declared in this scope    15 |                 roll2 = s[i+1] - '0';       |                 ^~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,6 +2,31 @@
 #include <iostream>
 #include <string>
 
+int bowlingScore(std::string s) {
+    int score = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == 'X') { 
+            int roll1 = 10;
+            score += roll1 + getBonus(s, i);
+        } else if (s[i] == '/') { 
+            int roll1 = s[i-1] - '0';
+            int roll2 = s[i+1] - '0';
+            score += roll1 + roll2;
+        } else { 
+            int roll1 = s[i] - '0';
+            int roll2 = s[i+1] - '0';
+            if (i < 8 && s[i+2] != '/') {
+                score += roll1 + roll2;
+            } else {
+                score += roll1;
+            }
+        }
+    }
+
+    return score;
+}
+
 int getBonus(std::string s, int i) {
     int bonus = 0;
 
@@ -16,26 +41,4 @@ int getBonus(std::string s, int i) {
     }
 
     return bonus;
-}
-
-int bowlingScore(std::string s) {
-    int score = 0;
-
-    for (int i = 0; i < 10; i++) {
-        if (s[i] == 'X') { 
-            score += 10 + getBonus(s, i);
-        } else if (s[i] == '/') { 
-            score += 5 + getBonus(s.substr(i+1), 0);
-        } else { 
-            int roll1 = s[i] - '0';
-            if (i < 8 && s[i+1] != '/') {
-                int roll2 = s[i+1] - '0';
-                score += roll1 + roll2;
-            } else {
-                score += roll1;
-            }
-        }
-    }
-
-    return score;
 }
