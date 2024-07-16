26a7ee1a4a07b1d924aae44f6866d1373a9e171d
beep boop <githubbot@vadim.me>
2024-07-16 04:12:20 +0200

    Solution 50 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niur2qrc Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9fd05a28-f0c4-4158-99f5-5793ccedd297.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9fd05a28-f0c4-4158-99f5-5793ccedd297.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9fd05a28-f0c4-4158-99f5-5793ccedd297.cpp:11: 9fd05a28-f0c4-4158-99f5-5793ccedd297.cpp: In function ‘int main()’: 9fd05a28-f0c4-4158-99f5-5793ccedd297.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 9fd05a28-f0c4-4158-99f5-5793ccedd297.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,20 +1,29 @@
 #include <vector>
 #include <list>
 #include <boost/any.hpp>
+#include <cassert>
+
+using namespace std;
+
+vector<int> filter_integers(list<boost::any> values);
 
 bool issame(vector<int> a, vector<int> b);
 
-vector<int> filter_integers(list_any values){
+int main() {
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+    return 0;
+}
+
+vector<int> filter_integers(list<boost::any> values) {
     vector<int> result;
-    for(auto& val : values){
-        if(typeid(int) == val.type()){
-            result.push_back(boost::any_cast<int>(val));
+    for (auto val : values) {
+        if (auto ptr = boost::any_cast<int>(&val)) {
+            result.push_back(*ptr);
         }
     }
     return result;
 }
 
-int main() {
-    // Your main function code here
-    return 0;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
