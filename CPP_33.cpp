9d5875f3c5b0281c9a7d33b12b774249c0739d85
beep boop <githubbot@vadim.me>
2024-07-16 04:15:25 +0200

    Solution 58 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fq3haqk5 Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 83d7a39d-a527-465e-8b6d-9446c94fa752.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 83d7a39d-a527-465e-8b6d-9446c94fa752.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 83d7a39d-a527-465e-8b6d-9446c94fa752.cpp:15: 83d7a39d-a527-465e-8b6d-9446c94fa752.cpp: In function ‘int main()’: 83d7a39d-a527-465e-8b6d-9446c94fa752.cpp:25:20: error: ‘sort_third’ was not decl ared in this scope    25 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 83d7a39d-a527-465e-8b6d-9446c94fa752.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,8 +1,5 @@
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a[2] == b[2];
-}
-
-vector<int> sort_third(vector<int> l) {
+bool issame(const vector<int>& a, const vector<int>& b);
+void sort_third(vector<int>& l) {
     vector<int> sorted_indices;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
@@ -13,6 +10,8 @@ vector<int> sort_third(vector<int> l) {
     for (int index : sorted_indices) {
         sort(l.begin() + index, l.begin() + index + 3, issame);
     }
-    
-    return l;
+}
+
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a[2] == b[2];
 }
