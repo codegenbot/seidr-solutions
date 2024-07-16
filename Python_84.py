eb698294ae5a06672f5c4cea933d2006676e5956
beep boop <githubbot@vadim.me>
2024-07-16 03:21:19 +0200

    Solution 31 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dx1ic3yv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c3e49f0e-151a-4257-9870-8fcaa9c5f 352.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,8 +1,17 @@
+```
 def solve(N):
     if N == 0:
         return "0"
-    elif N < 0:
-        return "-" + bin(abs(N))[2:]
+    if N < 0:
+        return "-" + solve(-N)
+    elif type(N) != int:
+        pass
     else:
-        s = bin(N)[2:]
-        return "1" if len(s) == 1 else s
+        s = bin(abs(N))[2:]
+        if len(s) % 4 != 0:
+            s = '0' * (4 - len(s) % 4) + s
+        result = ''
+        for i in range(0, len(s), 4):
+            d4 = s[i:i+4]
+            result += chr(int(d4, 2))
+        return result
