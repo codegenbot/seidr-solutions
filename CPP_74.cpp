763453b5635a53c404389cec86190f9c57dedcd7
beep boop <githubbot@vadim.me>
2024-07-16 05:57:41 +0200

    Solution 26 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/g5at8u4d Prompt: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:16: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp: In function ‘int main()’: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp: In function ‘int total_chars(int)’: c865892b-516b-4cdd-914f-a4c33e2e59f9.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,13 +1,10 @@
-bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
-}
+bool issame(const vector<string>& a, const vector<string>& b);
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2);
+int total_chars(const vector<string>& lst);
 
-int total_chars(const vector<string>& lst) {
-    int total = 0;
-    for (const string& str : lst) {
-        total += str.size();
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    if (total_chars(lst1) < total_chars(lst2)) {
+        return lst1;
     }
-    return total;
+    return total_chars(lst1) == total_chars(lst2) ? lst1 : lst2;
 }
