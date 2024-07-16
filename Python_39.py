fe6c667ec65ab68f5e47f00b2e953b1fec000f6e
beep boop <githubbot@vadim.me>
2024-07-16 11:47:55 +0200

    Solution 60 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/pd101rs5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/16bc958e-b9f7-4dc5-bb85-929f68e36 1f9.py", line 26, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/16bc958e-b9f7-4dc5-bb85-929f68e36 1f9.py", line 23, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,3 +1,12 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
 def prime_fib(n: int):
     if n == 1:
         return 2
@@ -5,9 +14,9 @@ def prime_fib(n: int):
         return 3
     else:
         fib_list = [1, 1]
-        prime_fib_num = 0  # Initialize prime_fib_num
         while len(fib_list) < n:
             fib_list.append(fib_list[-1] + fib_list[-2])
+        prime_fib_num = None
         for num in fib_list:
             if is_prime(num):
                 prime_fib_num = num
