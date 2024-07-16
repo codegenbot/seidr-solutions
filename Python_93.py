f9429e234679067ee819c008abb9d123e48fb7ca
beep boop <githubbot@vadim.me>
2024-07-16 08:04:41 +0200

    Solution 77 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yuz53mar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d26f1b9d-3a1b-4159-8b1b-eada09d49 7b8.py", line 1, in <module>     for char in input_string:                 ^^^^^^^^^^^^ NameError: name 'input_string' is not defined

--- Python_93.py
+++ Python_93.py
@@ -1,6 +1,7 @@
-input_message = input("Enter the message: ")
-encoded_message = ''
-for char in input_message:
-    encoded_message += chr((ord(char) - ord("A") + 2) % 26 + ord("A") if "A" <= char <= "Z" else ord(char)
-
-print(encoded_message)
+input_string = input("Enter the input string: ")
+for char in input_string:
+    encoded_message = ''
+    encoded_message += chr(
+        (ord(char) - ord("A") + 2) % 26 + ord("A") if "A" <= char <= "Z" else ord(char)
+    )
+    print(encoded_message)
