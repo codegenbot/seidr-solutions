7124e79edbd517be2443f0ffdeb0bf3ebc1274bb
beep boop <githubbot@vadim.me>
2024-07-16 08:42:24 +0200

    Solution 69 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/k9c1wnuj Prompt:       |             s ff81c489-8d84-40b5-942d-f26105cda08a.cpp:22:13: error: ‘st’ was not declared in this scope; did you mean ‘s’?    22 |             st.push(s[i]);       |             ^~       |             s ff81c489-8d84-40b5-942d-f26105cda08a.cpp:25:13: error: ‘st’ was not declared in this scope; did you mean ‘s’?    25 |     while (!st.empty() && st.top() == '&') {       |             ^~       |             s ff81c489-8d84-40b5-942d-f26105cda08a.cpp:28:13: error: ‘st’ was not declared in this scope; did you mean ‘s’?    28 |     return !st.empty();       |             ^~       |             s ff81c489-8d84-40b5-942d-f26105cda08a.cpp:28:24: error: expected ‘}’ at end of in put    28 |     return !st.empty();       |                        ^ ff81c489-8d84-40b5-942d-f26105cda08a.cpp:4:34: note: to match this ‘{’     4 | bool solveBoolean(std::string s) {       |                                  ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,28 +1,28 @@
+```
 #include <stack>
 #include <string>
 
 bool solveBoolean(std::string s) {
     stack<char> st;
+    stack<bool> val; // Store the actual value of T/F
     for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '|') {
-            while (!st.empty() && st.top() == '|') {
-                st.pop();
+        if (s[i] == '&') {
+            while (!val.empty() && !val.top()) { // Evaluate the & operator
+                val.pop();
             }
-            if (st.empty()) return true;
-            st.push('|');
-        } else if (s[i] == '&') {
-            while (!st.empty() && st.top() == '&') {
-                st.pop();
-            }
-            if (st.empty()) return false;
             st.push('&');
-        } else if (s[i] != 'T' && s[i] != 'F') {
-            return false;
+        } else if (s[i] == '|') {
+            while (!val.empty() && val.top()) { // Evaluate the | operator
+                val.pop();
+            }
+            st.push('|');
         } else {
+            val.push(s[i] == 'T'); // Store the value of T/F as bool
             st.push(s[i]);
         }
     }
-    while (!st.empty() && st.top() == '&') {
+    while (!st.empty() && st.top() == '&') { // Evaluate the remaining operators
         st.pop();
     }
-    return !st.empty();
+    return val.empty(); // Return true if all values were F, false otherwise
+}
