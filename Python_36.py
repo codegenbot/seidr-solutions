d8ce1fb9af6db05bb5f99d0e9ed26182197d56ac
beep boop <githubbot@vadim.me>
2024-07-16 15:20:48 +0200

    Solution 25 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/fv1zann3 Prompt: 10815 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6d5a07c-8871-4656-9aae-c9f4d55a3 b94.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6d5a07c-8871-4656-9aae-c9f4d55a3 b94.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,4 +1,11 @@
 def fizz_buzz(n: int) -> int:
-    return sum(1 for i in range(1, n + 1) if (('7' in str(i) or '8' in str(i)) and (i % 11 == 0 or i % 13 == 0))
+    return sum(
+        1
+        for i in range(1, n + 1)
+        if (i % 11 == 0 or i % 13 == 0)
+        and "7" in str(i)
+        and "8" not in str(i)
+    )
 
-print(fizz_buzz(100000))  # Output will be 8026
+
+print(fizz_buzz(100000))  # Output should be 8026
