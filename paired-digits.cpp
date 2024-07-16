9be32885400b61f7ed4c0a58926adcb625c78a8a
beep boop <githubbot@vadim.me>
2024-07-16 21:28:14 +0200

    Solution 88 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/z5qz6kmq Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,22 +1,18 @@
 ```c++
 #include <iostream>
-#include <string>
-
-int pairedDigits(const std::string& s) {
-    int sum = 0;
-    for (int i = 0; i < s.length() - 1; i++) {
-        if (s[i] == s[i+1]) {
-            sum += s[i] - '0';
-        }
-    }
-    return sum;
-}
 
 int main() {
-    std::string input;
+    char c;
+    int sum = 0;
     std::cout << "Enter a string of digits: ";
-    getline(std::cin, input);  
-    int output = pairedDigits(input);
-    std::cout << "Sum of paired digits is: " << output << std::endl;
+    
+    while((c = getchar()) != '\n') {
+        if (c >= '0' && c <= '9') {
+            if (c == getchar()) { 
+                sum += c - '0'; 
+            }
+        }
+    }
+    std::cout << "Sum of paired digits is: " << sum << std::endl;
     std::cout << "Program has finished." << std::endl;
 }
