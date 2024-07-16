379ce113372d28c00fcff5b66aa79097ca73b904
beep boop <githubbot@vadim.me>
2024-07-16 08:37:46 +0200

    Solution 65 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/3xxz6ioc Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,36 +1,32 @@
 ```c++
 #include <vector>
+#include <cmath>
 using namespace std;
-
-int main() {
-    vector<int> v = {1, 3, 4, 5, 6, 7, 8};
-    vector<vector<int>> res = cutVector(v);
-    
-    for (int i = 0; i < res.size(); ++i) {
-        cout << "Subvector " << i + 1 << ": ";
-        for (int j = 0; j < res[i].size(); ++j) {
-            cout << res[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
-}
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
-    
     for (int i = 0; i < n - 1; ++i) {
         if (abs(v[i] - v[i + 1]) <= abs(v[0] - v[n - 1])) {
             res[0].insert(res[0].end(), v.begin(), v.end());
             return {{}, res[0]};
         }
     }
-    
     int mid = n / 2;
     res[0] = vector<int>(v.begin(), v.begin() + mid);
     res[1] = vector<int>(v.begin() + mid, v.end());
-    
     return res;
+}
+
+int main() {
+    vector<int> v = {5, 6, 4, 7};
+    vector<vector<int>> res = cutVector(v);
+    for (int i = 0; i < res.size(); i++) {
+        cout << "Subvector " << i + 1 << ": ";
+        for (int j = 0; j < res[i].size(); j++) {
+            cout << res[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
