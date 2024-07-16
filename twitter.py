d86d8940a8d6ffb2af3f365ee8160d4c9fe1a406
beep boop <githubbot@vadim.me>
2024-07-17 00:07:44 +0200

    Solution 61 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8tr8sey0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1ea322d-8eda-4147-a719-416080d9c a28.py", line 1     ```     ^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -1,7 +1,10 @@
+```
+import re
+
 def validate_tweet(tweet):
-    if len(tweet) > 140:
+    if len(re.sub(r'\s+', '', tweet)) > 140:
         return "Too many characters"
     elif not tweet.strip():
         return "You didn't type anything"
     else:
-        return f"Your tweet has {len(tweet)} characters"
+        return f"Your tweet has {len(re.sub(r'\s+', '', tweet))} characters"
