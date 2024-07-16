135f570a7a2e5071bc664b20064cc38ab3053df6
beep boop <githubbot@vadim.me>
2024-07-16 11:16:58 +0200

    Solution 34 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/21dc2h6k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3aeb999-3e8f-4142-a4e2-3010241e8 06c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -3,48 +3,47 @@ def bowling_game(score):
     score = score.replace('/', '')
     total_score = 0
     frame = 1
+    i = 0
     while len(score) > 0:
-        if score[0] == 'X':
-            total_score += 10
-            if len(score) >= 2 and score[:2] in ['XX', 'X/']:
-                score = score[2:]
+        if score[i].isdigit():
+            first_roll = int(score[i])
+            i += 1
+            if i < len(score) and score[i].isdigit():
+                second_roll = int(score[i])
+                i += 1
+                total_score += first_roll + second_roll
+                frame += 1
+            elif score[i] == 'X':
+                total_score += 30
+                frame += 1
+                i += 1
             else:
-                score = score[1:]
-            frame += 1
-        elif score[0] != 'X' and score[0].isdigit():
-            first_roll = int(score[0])
-            if len(score) >= 2 and score[:2] in ['--', '-/']:
-                total_score += 10
-                score = score[2:]
-            else:
-                score = score[1:]
-                if len(score) > 0 and score[0].isdigit():
-                    second_roll = int(score[0])
-                    score = score[1:]
-                    if first_roll + second_roll == 10:
-                        total_score += first_roll + second_roll
-                        frame += 1
-                    else:
-                        total_score += first_roll + second_roll
-                        frame += 2
-                else:
-                    total_score += first_roll + (10 - int(score[0]))
-                    score = score[1:]
+                if len(score) > i and score[i].isdigit():
+                    total_score += 10 + int(score[i])
                     frame += 2
-        else:
-            if len(score) > 0 and score[0].isdigit():
-                first_roll = int(score[0])
-                score = score[1:]
-                if first_roll == 10:
-                    total_score += 10 + (10 - int(score[0]))
-                    score = score[1:]
+                    i += 1
+                elif score[i] == 'X':
+                    total_score += 20
                     frame += 2
+                    i += 1
                 else:
                     total_score += 10
                     frame += 1
-            else:
-                total_score += 10
+        else:
+            if score[i] == 'X':
+                total_score += 30
                 frame += 1
+                i += 1
+            elif score[i] == '/':
+                first_roll = int(score[:i].split(' ')[0])
+                second_roll = int(score[i+1].split(' ')[0])
+                if first_roll + second_roll == 10:
+                    total_score += 10
+                    frame += 1
+                else:
+                    total_score += 10 + second_roll
+                    frame += 2
+                i += 2
     return total_score
 
 print(bowling_game("1234567890"))
