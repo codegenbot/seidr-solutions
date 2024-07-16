3226439f81b07799f4b6ec15c8fe4cd1094c458e
beep boop <githubbot@vadim.me>
2024-07-16 06:42:21 +0200

    Solution 81 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/97a3wy3p Prompt:       |                    ^~~ e52f42dc-1531-4aae-b520-efd446430804.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e52f42dc-1531-4aae-b520-efd446430804.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e52f42dc-1531-4aae-b520-efd446430804.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e52f42dc-1531-4aae-b520-efd446430804.cpp:18: e52f42dc-1531-4aae-b520-efd446430804.cpp: In function ‘int main()’: e52f42dc-1531-4aae-b520-efd446430804.cpp:28:20: error: ‘pluck’ was not declared in this scope    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ e52f42dc-1531-4aae-b520-efd446430804.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -3,18 +3,6 @@
 
 vector<int> pluck(vector<int> arr);
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<int> pluck(vector<int> arr){
     vector<int> result;
     int smallest_even = -1;
@@ -30,14 +18,4 @@ vector<int> pluck(vector<int> arr){
         result.push_back(smallest_index);
     }
     return result;
-}
-
-int main() {
-    // Test the pluck function
-    vector<int> test_input = {3, 5, 2, 8, 6, 4};
-    vector<int> expected_output = {2, 2};
-    vector<int> output = pluck(test_input);
-    assert(issame(output, expected_output));
-    
-    return 0;
 }
