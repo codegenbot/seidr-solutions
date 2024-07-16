a76b155b4d979336f9a57333669fe60fde5e212b
beep boop <githubbot@vadim.me>
2024-07-16 02:57:01 +0200

    Solution 8 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/dx1ic3yv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78f1614e-5355-4628-b88d-c773045a1 533.py", line 13, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/78f1614e-5355-4628-b88d-c773045a1 533.py", line 10, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -3,4 +3,4 @@ def solve(N):
         return "0"
     s = str(N)
     odd_sum = sum(int(i) for i in s if (i != '0' and int(i)) % 2)
-    return bin(odd_sum)[2:]
+    return bin(odd_sum)[2:] if odd_sum > 1 else "1" if odd_sum == 1 else "0"
