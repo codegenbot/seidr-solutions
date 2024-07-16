a4be1cdf03e2e18756cb0c778b404165bc852938
beep boop <githubbot@vadim.me>
2024-07-16 16:49:29 +0200

    Solution 25 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8q722hdb Prompt: , _Alloc>::operator=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’   785 |       operator=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:785:42: note:   no known conversion for argument 1 from ‘std::__cxx11::basic_string<char>’ to ‘std::initializer_list<cha r>’   785 |       operator=(initializer_list<_CharT> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~^~~ 6f1b6158-91fa-4c74-9340-10d982cb8cb6.cpp: In function ‘std::string camelCase(con st string&)’: 6f1b6158-91fa-4c74-9340-10d982cb8cb6.cpp:32:36: error: invalid types ‘const char [int]’ for array subscript    32 |                 result += toupper(c[0]);       |                                    ^ 6f1b6158-91fa-4c74-9340-10d982cb8cb6.cpp:35:36: error: invalid types ‘const char [int]’ for array subscript    35 |                 result += tolower(c[0]);       |                                    ^ 6f1b6158-91fa-4c74-9340-10d982cb8cb6.cpp:37:25: error: request for member ‘subst r’ in ‘c’, which is of non-class type ‘const char’    37 |             result += c.substr(1);       |                         ^~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,43 +1,42 @@
 #include <string>
 #include <vector>
 
-std::string camelCase(const std::string& str) {
-    std::string result;
-    bool capitalizeNext = true;
-    for (const auto& c : split(str, '-')) {
-        if (!result.empty()) {
-            if (capitalizeNext) {
-                result += toupper(c);
-                capitalizeNext = false;
-            } else {
-                result += tolower(c);
-            }
-        } else {
-            result = tolower(c);
-            capitalizeNext = true;
-        }
-    }
-    return result;
-}
-
-std::string join(const std::vector<std::string>& tokens, char delimiter) {
+std::string join(const std::vector<std::string>& tokens) {
     std::string result;
     for (const auto& token : tokens) {
         if (!result.empty()) {
-            result += delimiter;
+            result += ' ';
         }
         result += token;
     }
     return result;
 }
 
-std::string split(std::string str, char delimiter) {
+std::string split(const std::string& str, char delimiter) {
     std::vector<std::string> tokens;
     size_t pos = 0;
     while ((pos = str.find(delimiter)) != std::string::npos) {
         tokens.push_back(str.substr(0, pos));
-        str.erase(0, pos + 1);
+        str = str.substr(pos + 1);
     }
     tokens.push_back(str);
-    return join(tokens, ' ');
+    return join(tokens);
+}
+
+std::string camelCase(const std::string& str) {
+    std::string result;
+    size_t pos = 0;
+    bool capitalizeNext = (str[0] >= 'a' && str[0] <= 'z');
+    for (const auto& c : split(str, '-')) {
+        if (!result.empty()) {
+            if (capitalizeNext) {
+                if (!capitalizeNext) { result += c; } else { result += std::string(1, toupper(c[0])); result += c.substr(1); capitalizeNext = false; }
+            } else {
+                else { result += std::string(1, tolower(c[0])); result += c.substr(1); }
+            }
+        } else {
+            if (!capitalizeNext) { result += c; } else { result += std::string(1, toupper(c[0])); result += c.substr(1); capitalizeNext = false; }
+        }
+    }
+    return result;
 }
