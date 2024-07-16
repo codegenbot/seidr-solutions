4cd54f27e08008ee3a4cd14830bb49dbde819e5c
beep boop <githubbot@vadim.me>
2024-07-16 14:37:54 +0200

    Solution 14 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/j7ex0j47 Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -2,25 +2,54 @@
 #include <iostream>
 #include <string>
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    size_t start = 0;
-    for (size_t i = 0; i <= str.size(); ++i) {
-        if (i == str.size() || str[i] == ' ') {
-            std::string word = str.substr(start, i - start);
-            if (word.length() >= 5)
-                std::reverse(word.begin(), word.end());
-            result += word + (i == str.size() ? "" : " ");
-            start = i + 1;
+using namespace std;
+
+string spinWords(string s) {
+    string result = "";
+    string temp = "";
+    
+    for(int i=0; i<s.length(); i++) {
+        if(s[i] == ' ') {
+            if(temp.length() >= 5)
+                result += reverse(temp) + " ";
+            else
+                result += temp + " ";
+            temp = "";
+        } 
+        else {
+            temp += s[i];
         }
     }
+
+    if(temp.length() >= 5)
+        result += reverse(temp);
+    else
+        result += temp;
+
     return result;
 }
 
+string reverse(string str) {
+    int start = 0;
+    int end = str.length() - 1;
+    
+    while(start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+    
+    return str;
+}
+
 int main() {
-    std::cout << spinWords("a") << std::endl; // a
-    std::cout << spinWords("this is a test") << std::endl; // this is a test
-    std::cout << spinWords("this is another test") << std::endl; // this is rehtona test
-    std::cout << spinWords("hi") << std::endl; // hi
+    // Your code here
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    cout << spinWords(s) << endl;
+
     return 0;
 }
