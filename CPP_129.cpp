e936102ed4fea12a1cf5a48e7c3eef1b6378da15
beep boop <githubbot@vadim.me>
2024-07-16 17:53:30 +0200

    Solution 4 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/iyzh3nnh Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 20af8d2c-493c-4d17-a3ba-1c3b18f04d80.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 20af8d2c-493c-4d17-a3ba-1c3b18f04d80.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 20af8d2c-493c-4d17-a3ba-1c3b18f04d80.cpp:17: 20af8d2c-493c-4d17-a3ba-1c3b18f04d80.cpp: In function ‘int main()’: 20af8d2c-493c-4d17-a3ba-1c3b18f04d80.cpp:27:20: error: ‘minPath’ was not declare d in this scope    27 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 20af8d2c-493c-4d17-a3ba-1c3b18f04d80.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,5 +1,9 @@
 #include <vector>
 
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
+
 std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
     std::vector<int> result;
     for(int i = 0; i < k; ++i){
@@ -10,14 +14,4 @@ std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
         }
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
-    
-    return 0;
 }
