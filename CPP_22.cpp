80de3e8daacfd2d1d9227e9c1ee4a8b2e1790b59
beep boop <githubbot@vadim.me>
2024-07-16 04:07:58 +0200

    Solution 70 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/o83a1slg Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 207cf579-d3be-4ff6-94a2-91bba81e08fa.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 207cf579-d3be-4ff6-94a2-91bba81e08fa.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 207cf579-d3be-4ff6-94a2-91bba81e08fa.cpp:11: 207cf579-d3be-4ff6-94a2-91bba81e08fa.cpp: In function ‘int main()’: 207cf579-d3be-4ff6-94a2-91bba81e08fa.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 207cf579-d3be-4ff6-94a2-91bba81e08fa.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,25 +1,24 @@
-#include <boost/any.hpp>
 #include <vector>
 #include <list>
-#include <algorithm>
+#include <boost/any.hpp>
+#include <cassert>
 
-using namespace std;
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
 
-vector<int> filter_integers(const list<boost::any>& values){
+vector<int> filter_integers(std::list<boost::any> values){
     vector<int> result;
-    for(const auto &value : values){
-        if(auto ptr = boost::any_cast<int>(&value)){
+    for (const auto &value : values) {
+        if (auto ptr = boost::any_cast<int>(&value)) {
             result.push_back(*ptr);
         }
     }
     return result;
 }
 
-bool issame(const vector<int>& a, const vector<int>& b){
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
-int main(){
+int main() {
     assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+
     return 0;
 }
