fdc9d53e8fd31f6ed165291eeb03d722e7314ac6
beep boop <githubbot@vadim.me>
2024-07-16 03:09:07 +0200

    Solution 20 of CPP/121, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rin5wabv Prompt: cc33a3f6-701e-4dea-a4c0-999fced18937.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ cc33a3f6-701e-4dea-a4c0-999fced18937.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 #include <vector>
 
@@ -10,8 +9,4 @@ int solutions(std::vector<int> lst) {
         }
     }
     return sum;
-}
-
-int main() {
-    std::cout << solutions({3, 13, 2, 9}) << std::endl;
 }
