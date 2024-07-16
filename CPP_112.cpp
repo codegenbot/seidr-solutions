7ca6cb43b746594447a32e1d926cdb079be56469
beep boop <githubbot@vadim.me>
2024-07-16 07:13:31 +0200

    Solution 37 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/gyyvleu4 Prompt:       |                              ^~~~~~ a5f32c43-582a-4294-bfa0-9108838ea772.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a5f32c43-582a-4294-bfa0-9108838ea772.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a5f32c43-582a-4294-bfa0-9108838ea772.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a5f32c43-582a-4294-bfa0-9108838ea772.cpp:13: a5f32c43-582a-4294-bfa0-9108838ea772.cpp: In function ‘int main()’: a5f32c43-582a-4294-bfa0-9108838ea772.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ a5f32c43-582a-4294-bfa0-9108838ea772.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -16,5 +16,5 @@ std::vector<std::string> reverse_delete(std::string s, std::string c){
     }
     std::string reversed = result;
     std::reverse(reversed.begin(), reversed.end());
-    return {result, result == reversed ? "True" : "False"};
+    return {result, issame({result}, {reversed}) ? "True" : "False"};
 }
