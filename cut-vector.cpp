383827010533b584a7ce3192126a76a2accf9c69
beep boop <githubbot@vadim.me>
2024-07-16 21:45:42 +0200

    Solution 10 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7u3fwbt5 Prompt: this scope    33 |     cin >> n;       |     ^~~ fa50c713-678d-4f73-8b05-fe308e9ce4d6.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; fa50c713-678d-4f73-8b05-fe308e9ce4d6.cpp:39:5: error: ‘cout’ was not declared in  this scope    39 |     cout << "First subvector: ";       |     ^~~~ fa50c713-678d-4f73-8b05-fe308e9ce4d6.cpp:39:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? fa50c713-678d-4f73-8b05-fe308e9ce4d6.cpp:43:13: error: ‘endl’ was not declared i n this scope    43 |     cout << endl;       |             ^~~~ fa50c713-678d-4f73-8b05-fe308e9ce4d6.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,14 +1,51 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
     int min_diff = INT_MAX;
-    int split_index = 0;
-    for (int i = 1; i < v.size(); ++i) {
-        int diff = abs(v[i] - v[0]);
-        if (diff < min_diff) {
+    pair<vector<int>, vector<int>> result;
+    
+    for(int i = 1; i <= vec.size(); i++) {
+        int left_sum = 0, right_sum = 0;
+        
+        for(int j = 0; j < i; j++) {
+            left_sum += vec[j];
+        }
+        
+        for(int j = i; j < vec.size(); j++) {
+            right_sum += vec[j];
+        }
+        
+        int diff = abs(left_sum - right_sum);
+        
+        if(diff <= min_diff) {
             min_diff = diff;
-            split_index = i;
+            result.first = vector<int>(vec.begin(), vec.begin() + i);
+            result.second = vector<int>(vec.begin() + i, vec.end());
         }
     }
-    return {{v.begin(), v.begin() + split_index}, {v.begin() + split_index, v.end()}};
+    
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n+1);
+    for(int i = 0; i <= n; i++) {
+        cin >> vec[i];
+    }
+    pair<vector<int>, vector<int>> res = cutVector(vec);
+    cout << "First subvector: ";
+    for(auto x : res.first) {
+        cout << x << " ";
+    }
+    cout << endl;
+    cout << "Second subvector: ";
+    for(auto x : res.second) {
+        cout << x << " ";
+    }
+    cout << endl;
+    
+    return 0;
