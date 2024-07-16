edb2467df9ed075a447b0937273138848889e019
beep boop <githubbot@vadim.me>
2024-07-16 03:37:48 +0200

    Solution 53 of Python/138, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rtj380pn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ad9b5213-8c9f-44b6-ae2b-eca882ff4 49a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_138.py
+++ Python_138.py
@@ -1,10 +1,10 @@
 def is_equal_to_sum_even(n):
-    for i in range(2, n + 1, 2):
-        sum_even = 0
-        for j in range(2, i // 2 + 1, 2):
-            if sum_even + j > n:
-                break
-            sum_even += j
-        if sum_even == n:
-            return True
+    even_sum = 0
+    for i in range(2, n+1, 2):
+        even_sum = i
+        while even_sum <= n:
+            if even_sum == n:
+                return True
+            else:
+                even_sum += i
     return False
