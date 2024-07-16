c1f5662d9a67786ca9448f525a54f1decd129f85
beep boop <githubbot@vadim.me>
2024-07-16 14:22:02 +0200

    Solution 15 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/0vygyqol Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,32 +1,51 @@
-#include <string>
-using namespace std;
+bool solveBoolean(string expression) {
+    stack<char> ops;
+    stack<bool> vals;
 
-bool evaluateBooleanExpression(string expression) {
-    bool result = false;
-    for (int i = 0; i < expression.length(); i++) {
-        if (expression[i] == 't') {
-            return true;
-        } else if (expression[i] == 'f') {
-            return false;
-        } else if (expression[i] == '&') {
-            result &= evaluateBooleanExpression(expression.substr(i + 1));
-            i = expression.length() - 1;
-        } else if (expression[i] == '|') {
-            bool temp = evaluateBooleanExpression(expression.substr(i + 1));
-            result |= temp;
-            i = expression.length() - 1;
+    for (int i = 0; i < expression.size(); i++) {
+        if (expression[i] == 'T')
+            vals.push(true);
+        else if (expression[i] == 'F')
+            vals.push(false);
+
+        if (i > 0 && (isymbol(expression[i]) || isop(expression[i]))) {
+            bool right = vals.top();
+            vals.pop();
+
+            if (expression[i] == '|') {
+                while (!ops.empty() && ops.top() == '&') {
+                    right = vals.top();
+                    vals.pop();
+                    ops.pop();
+                }
+            } else if (expression[i] == '&') {
+                while (!ops.empty() && isop(ops.top())) {
+                    right = vals.top();
+                    vals.pop();
+                    ops.pop();
+                }
+            }
+
+            bool left;
+            if (!vals.empty()) {
+                left = vals.top();
+                vals.pop();
+            } else
+                left = right;
+
+            if (expression[i] == '|')
+                vals.push(left || right);
+            else
+                vals.push(left && right);
+
+            if (isop(expression[i]))
+                ops.push(expression[i]);
         }
     }
-    return result;
+
+    return vals.top();
 }
 
-int main() {
-    string expression;
-    cout << "Enter a Boolean expression: ";
-    cin >> expression;
-    if (expression == "t" || expression == "true")
-        cout << "True" << endl;
-    else
-        cout << "False" << endl;
-    return 0;
+bool isop(char c) {
+    return c == '|' || c == '&';
 }
