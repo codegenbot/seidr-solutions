43abacafb39454665cb6afb68d1fe81db2afb69d
beep boop <githubbot@vadim.me>
2024-07-16 09:49:12 +0200

    Solution 82 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/m6crd1q4 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f940bffc-8f82-4d4f-84cb-a885a353807c.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f940bffc-8f82-4d4f-84cb-a885a353807c.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f940bffc-8f82-4d4f-84cb-a885a353807c.cpp:11: f940bffc-8f82-4d4f-84cb-a885a353807c.cpp: In function ‘int main()’: f940bffc-8f82-4d4f-84cb-a885a353807c.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ f940bffc-8f82-4d4f-84cb-a885a353807c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -9,4 +9,15 @@ bool issame(const vector<string>& a, const vector<string>& b) {
     return lst;
 }
 
+vector<string> sorted_list_sum(const vector<string>& lst) {
+    vector<string> sorted_list = lst;
+    sort(sorted_list.begin(), sorted_list.end(), [](const string& a, const string& b) {
+        if (a.length() == b.length()) {
+            return a < b;
+        }
+        return a.length() < b.length();
+    });
+    return sorted_list;
+}
+
 assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
