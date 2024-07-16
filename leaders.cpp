e0f44b55aa32bc629a44176b02dce8687ec78dbc
beep boop <githubbot@vadim.me>
2024-07-16 17:15:29 +0200

    Solution 15 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/ync6dtbk Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 176449b4-f3a0-4bf2-ad25-0cc987c216be.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- leaders.cpp
+++ leaders.cpp
@@ -1,18 +1,9 @@
-```cpp
 #include <vector>
 #include <iostream>
-using namespace std;
 
-int main() {
-    vector<int> arr = {1, 3, 4, 2, 3}; 
-    vector<int> leaders = leaders(arr);
-    for (int i=0; i<leaders.size(); i++) cout << leaders[i] << " "; 
-    return 0;
-}
-
-vector<int> leaders(const vector<int>& arr) {
+std::vector<int> leaders(const std::vector<int>& arr) {
     int n = arr.size();
-    vector<int> res;
+    std::vector<int> res;
 
     for(int i=n-1; i>=0; i--) {
         bool leader = true;
@@ -26,4 +17,11 @@ vector<int> leaders(const vector<int>& arr) {
     }
 
     return res;
+}
+
+int main() {
+    std::vector<int> arr = {1, 3, 4, 2, 3}; 
+    std::vector<int> leaders = leaders(arr);
+    for (int i=0; i<leaders.size(); i++) cout << leaders[i] << " "; 
+    return 0;
 }
