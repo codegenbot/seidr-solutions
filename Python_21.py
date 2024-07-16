11c6dabdcebec7005b941fc8bdae7901b5146324
beep boop <githubbot@vadim.me>
2024-07-16 06:13:09 +0200

    Solution 69 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qfa09exs Prompt: Complete the following code given the task description and function signature.

--- Python_21.py
+++ Python_21.py
@@ -1,8 +1,2 @@
-from typing import List
-import math
-
-
 def rescale_to_unit(numbers: List[float]) -> List[float]:
-    return [
-        round((x - min(numbers)) / (max(numbers) - min(numbers)), 6) for x in numbers
-    ]
+    return [(x - min(numbers)) / (max(numbers) - min(numbers)) for x in numbers]
