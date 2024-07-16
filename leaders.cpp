7193e1c5fd1af75151c06394af15abceb3454493
beep boop <githubbot@vadim.me>
2024-07-16 07:45:04 +0200

    Solution 44 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/hrsc9sp9 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 06382239-ed90-4e88-8614-2e585c329a7d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- leaders.cpp
+++ leaders.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 using namespace std;
 
@@ -5,13 +6,8 @@ vector<int> leaders(vector<int>& arr) {
     int n = arr.size();
     vector<int> result;
     
-    if (n == 0)
-        return result;
-    
-    result.push_back(arr[n-1]);
-    
-    for (int i = n - 2; i >= 0; i--) {
-        if (arr[i] > result.back()) {
+    for (int i = n - 1; i >= 0; i--) {
+        if (i == n - 1 || arr[i] >= arr[i + 1]) {
             result.push_back(arr[i]);
         }
     }
@@ -23,7 +19,7 @@ int main() {
     vector<int> arr = {1, 3, 4, 1, 5};
     vector<int> leadersResult = leaders(arr);
     for (int leader : leadersResult) {
-        std::cout << leader << " ";
+        cout << leader << " ";
     }
     return 0;
 }
