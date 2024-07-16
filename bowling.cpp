104b336a07c29d2b918dcc5be1537e3fabebd7aa
beep boop <githubbot@vadim.me>
2024-07-16 10:15:56 +0200

    Solution 25 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vou69ibb Prompt: a417e1fa-c925-4a9d-ba19-8b3aab1ec023.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ a417e1fa-c925-4a9d-ba19-8b3aab1ec023.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -4,14 +4,16 @@
 int bowlingScore(std::string s) {
     int score = 0;
     int roll = 0;
-    int frame = 0;
+    int frame = 1;
 
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == '/') {
-            if (roll > 9)
-                score += 10;
-            else
+            if (frame < 10 && (roll == 10 || ((roll + (s[i+1] - '0')) <= 10))) {
+                score += roll + s[i+1] - '0';
+                i++;
+            } else {
                 score += roll;
+            }
             roll = 0;
             frame++;
         } else if (isdigit(s[i])) {
@@ -19,12 +21,6 @@ int bowlingScore(std::string s) {
             roll += s[i] - '0';
         }
     }
-
-    // Last two rolls
-    if (roll > 9)
-        score += 10;
-    else
-        score += roll;
 
     return score;
 }
