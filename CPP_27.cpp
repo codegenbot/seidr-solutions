14bef2386b9ad0bfd1e9a1b0cd12fc026c9fa87d
beep boop <githubbot@vadim.me>
2024-07-16 04:45:06 +0200

    Solution 95 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/tsz2hudl Prompt: 1ab86e11-8984-4c87-ba8d-e7e95eff5efa.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 1ab86e11-8984-4c87-ba8d-e7e95eff5efa.cpp:12: 1ab86e11-8984-4c87-ba8d-e7e95eff5efa.cpp: In function ‘int main()’: 1ab86e11-8984-4c87-ba8d-e7e95eff5efa.cpp:14:13: error: ‘filp_case’ was not decla red in this scope    14 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cassert>
 
 string flip_case(string str){
     for(char &c : str){
@@ -13,5 +14,8 @@ string flip_case(string str){
 
 int main() {
     assert(flip_case("Hello, World!") == "hELLO, wORLD!");
+    assert(flip_case("cPp") == "Cpp");
+    assert(flip_case("123") == "123");
+    
     return 0;
 }
