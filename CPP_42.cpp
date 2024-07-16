c30d251a64ebf98979772a96853700e5d06cffcf
beep boop <githubbot@vadim.me>
2024-07-16 04:59:12 +0200

    Solution 23 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/kdbjy62z Prompt:     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c5f51f5a-811e-4a91-837b-7babaf87b465.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c5f51f5a-811e-4a91-837b-7babaf87b465.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c5f51f5a-811e-4a91-837b-7babaf87b465.cpp:6: c5f51f5a-811e-4a91-837b-7babaf87b465.cpp: In function ‘int main()’: c5f51f5a-811e-4a91-837b-7babaf87b465.cpp:16:20: error: ‘incr_list’ was not decla red in this scope    16 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ c5f51f5a-811e-4a91-837b-7babaf87b465.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,23 +1,32 @@
 vector<int> incr_list(const vector<int>& l) {
     vector<int> result = l;
-    for(int i = 0; i < result.size(); i++){
+    for (int i = 0; i < result.size(); i++) {
         result[i]++;
     }
     return result;
 }
 
 bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-    vector<int> l = {1, 2, 3, 4, 5};
-    vector<int> incremented_list = incr_list(l);
+    vector<int> input = {1, 2, 3, 4, 5};
+    vector<int> incremented = incr_list(input);
+    vector<int> expected = {2, 3, 4, 5, 6};
     
-    if(issame(l, incremented_list)){
-        cout << "The lists are the same." << endl;
+    if (issame(incremented, expected)) {
+        cout << "Vectors are the same" << endl;
     } else {
-        cout << "The lists are different." << endl;
+        cout << "Vectors are different" << endl;
     }
     
     return 0;
