4253580b70cf55c4d1a0d6e32990ade29dd234ee
beep boop <githubbot@vadim.me>
2024-07-16 15:37:59 +0200

    Solution 61 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/v2upefae Prompt: Complete the following code given the task description and function signature.

--- Python_154.py
+++ Python_154.py
@@ -1,8 +1,2 @@
 def cycpattern_check(a, b):
-    if len(a) != len(b):
-        return False
-    for i in range(len(b)):
-        if b in a:
-            return True
-        b = b[1:] + b[0]
-    return False
+    return b in a + a
