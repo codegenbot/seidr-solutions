bbf7bcbe9d6edcc90778ea7cd04519c78eb959ab
beep boop <githubbot@vadim.me>
2024-07-16 18:10:16 +0200

    Solution 66 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/tmzcnw0i Prompt: 4ce1f76c-f22f-4a79-9ea4-35e5255890d9.cpp:1:19: error: ‘vector’ is not a member o f ‘std’     1 | int fuelCost(std::vector<int> &stations) {       |                   ^~~~~~ 4ce1f76c-f22f-4a79-9ea4-35e5255890d9.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | int fuelCost(std::vector<int> &stations) { 4ce1f76c-f22f-4a79-9ea4-35e5255890d9.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int fuelCost(std::vector<int> &stations) {       |                          ^~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,9 +1,11 @@
 #include <vector>
-int fuelCost(std::vector<int> &stations) {
+#include <cmath>
+
+int fuelCost(std::vector<int> stations) {
     int sum = 0;
     for (auto i : stations) { 
         if(i <= 3) break; 
-        int newFuel = static_cast<int>(i / 3.0) - 2;
+        int newFuel = std::floor((i * 1.0) / 3.0) - 2;
         sum += newFuel;
     }
     return sum;
