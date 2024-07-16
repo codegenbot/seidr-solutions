545bdec9cbfb36665ed322a52be6abf672815a0a
beep boop <githubbot@vadim.me>
2024-07-16 18:08:11 +0200

    Solution 77 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/zhns0p25 Prompt: it must return Too many characters for input Wph.v+|<Io_s"e"|R4]=+oL-!TqP/dqx75N~CdoT=wgafHR'uL;M7oT{?zEkez48&j~S7#}=7%?zpV8`h.ZW;dkB)v=>2XaYpOY1NvRwn^K7Ba0*yB%{k"39AnWra32=_F"+xg1?"kk_%vteOdl{1s~9"x;=_lz*p*7AE.Ve70TOW838q{z#xs4J'GX4)84R:Fdo, but it returns Enter a tweet: Too many characters.

--- twitter.cpp
+++ twitter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -10,10 +11,10 @@ int main() {
     
     if (tweet.empty()) {
         cout << "You didn't type anything" << endl;
-    } else if (std::ctype<char>::make_lower(tweet).length() > 140) {
+    } else if (std::ctype<char>::make_lowercase(tweet).length() > 140) {
         cout << "Too many characters" << endl;
     } else {
-        cout << "Your tweet has " << std::ctype<char>::make_lower(tweet).length() << " characters" << endl;
+        cout << "Your tweet has " << std::ctype<char>::make_lowercase(tweet).length() << " characters" << endl;
     }
     
     return 0;
