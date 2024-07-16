857c58d01385a6c2a7716ec7af96d70281580a0d
beep boop <githubbot@vadim.me>
2024-07-16 08:34:10 +0200

    Solution 20 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/uufdjf4w Prompt: 1e22a5b7-c1f6-4ec0-aa34-334823703e8c.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word){       | ^~~~~~ In file included from 1e22a5b7-c1f6-4ec0-aa34-334823703e8c.cpp:11: 1e22a5b7-c1f6-4ec0-aa34-334823703e8c.cpp: In function ‘int main()’: 1e22a5b7-c1f6-4ec0-aa34-334823703e8c.cpp:13:13: error: ‘get_closest_vowel’ was n ot declared in this scope    13 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cassert>
+
 using namespace std;
 
 string get_closest_vowel(string word){
@@ -14,5 +16,6 @@ string get_closest_vowel(string word){
 
 int main() {
     assert(get_closest_vowel("Above") == "o");
+    
     return 0;
 }
