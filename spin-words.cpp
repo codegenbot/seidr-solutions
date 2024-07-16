d3ef43ee13139fa8951663b405ffeeec59d506a8
beep boop <githubbot@vadim.me>
2024-07-16 15:37:27 +0200

    Solution 72 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/eumscrgm Prompt:       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 623639fb-f7b0-4475-b0da-e75b9703ac40.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 623639fb-f7b0-4475-b0da-e75b9703ac40.cpp:29:5: error: ‘cout’ was not declared in  this scope; did you mean ‘std::cout’?    29 |     cout << spinWords("This is a test");       |     ^~~~       |     std::cout In file included from 623639fb-f7b0-4475-b0da-e75b9703ac40.cpp:1: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ 623639fb-f7b0-4475-b0da-e75b9703ac40.cpp:29:13: error: ‘spinWords’ was not decla red in this scope    29 |     cout << spinWords("This is a test");       |             ^~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,33 +1,31 @@
-```c++
+```cpp
 #include <iostream>
 #include <string>
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    int i = 0;
-    while (i < str.length()) {
-        if (str.find(" ", i) == -1 || i + 1 >= str.length()) {
-            std::string word = str.substr(i);
+int main() {
+    std::string spinWords(std::string str) {
+        std::string result = "";
+        int i = 0;
+        while (i < str.length()) {
+            if (str.find(" ", i) == -1 || i + 1 >= str.length()) {
+                std::string word = str.substr(i);
+                if (word.length() >= 5) {
+                    for (int k = word.length() - 1; k >= 0; --k)
+                        result += word[k];
+                } else
+                    result += word;
+                break;
+            }
+            int j = str.find(" ", i);
+            std::string word = str.substr(i, j - i);
             if (word.length() >= 5) {
                 for (int k = word.length() - 1; k >= 0; --k)
                     result += word[k];
             } else
                 result += word;
-            break;
+            i = j + 1;
         }
-        int j = str.find(" ", i);
-        std::string word = str.substr(i, j - i);
-        if (word.length() >= 5) {
-            for (int k = word.length() - 1; k >= 0; --k)
-                result += word[k];
-        } else
-            result += word;
-        i = j + 1;
+        return result;
     }
-    return result;
-}
-
-int main() {
     std::cout << spinWords("This is a test");
-    return 0;
 }
