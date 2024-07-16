c3f87f89e7ec5d4b8112090ee8212e0c148144ed
beep boop <githubbot@vadim.me>
2024-07-16 14:15:09 +0200

    Solution 82 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/sofexba7 Prompt: Complete the following code given the task description and function signature.

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,14 +1,3 @@
-if (x == 1)
-        return true;
-    
-    for (int i = 2; i <= sqrt(x); i++) {
-        int p = i;
-        while (p <= x) {
-            if (p == x)
-                return true;
-            p *= i;
-        }
-    }
-    
-    return false;
+double res = log(x)/log(n);
+    return (res == (int)res);
 }
