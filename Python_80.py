9e6fa6b66fa2656762f3a62c9729138e308702dd
beep boop <githubbot@vadim.me>
2024-07-16 05:38:49 +0200

    Solution 8 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ueftrsin Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39041e53-545c-493f-8956-4811441ec bce.py", line 15, in <module>     check(is_happy)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39041e53-545c-493f-8956-4811441ec bce.py", line 12, in check     assert is_happy("iopaxioi") == False, "iopaxioi"            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/39041e53-545c-493f-8956-4811441ec bce.py", line 6, in is_happy     if set(chunk)[0] == set(chunk)[1] or set(chunk)[0] == set(chunk)[2] or set(c hunk)[1] == set(chunk)[2]:        ~~~~~~~~~~^^^ TypeError: 'set' object is not subscriptable

--- Python_80.py
+++ Python_80.py
@@ -3,6 +3,6 @@ def is_happy(s):
         return "Invalid input"
     for i in range(0, len(s), 3):
         chunk = s[i:i+3]
-        if len(set(chunk)) == 1 or (list(set(chunk))[0] in list(set(chunk))[1:]):
+        if len(set(chunk)) <= 1 or any(c1 == c2 for c1, c2 in zip(set(chunk), set(chunk)[1:])):
             return "Not happy"
     return "Happy"
