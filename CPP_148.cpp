8a16616b486422a63bc9da953149ccbdfb2581d6
beep boop <githubbot@vadim.me>
2024-07-16 09:06:27 +0200

    Solution 40 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/u0rxyrgh Prompt:       |                              ^~~~~~ 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp:29: 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp: In function ‘int main()’: 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 4e084c44-2b42-49ce-b47c-e0c0f1f392eb.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -6,14 +6,16 @@
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
     return a == b;
 }
 
-vector<string> getPlanetsBetween(const string& planet1, const string& planet2) {
+vector<string> findPlanetsBetween(const string& planet1, const string& planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     vector<string> result;
     int start = -1, end = -1;
-
+    
     for (int i = 0; i < planets.size(); ++i) {
         if (planets[i] == planet1) {
             start = i;
@@ -22,24 +24,24 @@ vector<string> getPlanetsBetween(const string& planet1, const string& planet2) {
             end = i;
         }
     }
-
+    
     if (start == -1 || end == -1) {
         return result;
     }
-
+    
     if (start > end) {
         swap(start, end);
     }
-
+    
     for (int i = start + 1; i < end; ++i) {
         result.push_back(planets[i]);
     }
-
+    
     return result;
 }
 
 int main() {
-    assert(issame(getPlanetsBetween("Jupiter", "Makemake"), {}));
-
+    assert(issame(findPlanetsBetween("Jupiter", "Makemake"), {}));
+    
     return 0;
 }
