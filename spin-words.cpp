824cf4bea024918e4ccfc312a7d315625c5f4e24
beep boop <githubbot@vadim.me>
2024-07-16 14:18:15 +0200

    Solution 4 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/j7ex0j47 Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,34 +1,32 @@
-#include <iostream>
 #include <string>
+using namespace std;
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    std::size_t start = 0;
-    
-    for (std::size_t i = 0; i <= str.size(); ++i) {
-        if (i == str.size() || str[i] == ' ') {
-            std::size_t len = i - start;
-            
-            if (len >= 5) {
-                std::string word = str.substr(start, len);
-                std::reverse(word.begin(), word.end());
-                result += word + " ";
+string spinWords(string str) {
+    string result = "";
+    string temp = "";
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] != ' ') {
+            temp += str[i];
+        } else {
+            if (temp.length() >= 5) {
+                for (int j = temp.length() - 1; j >= 0; j--) {
+                    result += temp[j];
+                }
             } else {
-                result += str.substr(start, len) + " ";
+                result += temp;
             }
-            
-            start = i + 1;
+            temp = "";
         }
     }
-    
-    return result.substr(0, result.size() - 1);
-}
 
-int main() {
-    std::cout << spinWords("a") << std::endl; // a
-    std::cout << spinWords("this is a test") << std::endl; // this is a test
-    std::cout << spinWords("this is another test") << std::endl; // this is rehtona test
-    std::cout << spinWords("hi") << std::endl; // hi
-    
-    return 0;
+    if (temp.length() >= 5) {
+        for (int i = temp.length() - 1; i >= 0; i--) {
+            result += temp[i];
+        }
+    } else {
+        result += temp;
+    }
+
+    return result;
 }
