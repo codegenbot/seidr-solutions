ef10a70529a1b9ad8111d7a0a90a7ac76b149594
beep boop <githubbot@vadim.me>
2024-07-16 04:14:12 +0200

    Solution 46 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/juygrexr Prompt:    13 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ c7b03f6f-cb20-4d23-9bfa-4a5ced5b444d.cpp:13:36: error: expected primary-expressi on before ‘float’    13 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ c7b03f6f-cb20-4d23-9bfa-4a5ced5b444d.cpp:13:43: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from c7b03f6f-cb20-4d23-9bfa-4a5ced5b444d.cpp:12: c7b03f6f-cb20-4d23-9bfa-4a5ced5b444d.cpp: In function ‘int main()’: c7b03f6f-cb20-4d23-9bfa-4a5ced5b444d.cpp:22:20: error: ‘find_closest_elements’ w as not declared in this scope    22 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ c7b03f6f-cb20-4d23-9bfa-4a5ced5b444d.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -13,8 +13,3 @@ std::pair<float, float> find_closest_elements(std::vector<float>& numbers) {
     }
     return closest_elements;
 }
-
-int main() {
-    assert((find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) == std::make_pair(2.2, 3.1)));
-    return 0;
-}
