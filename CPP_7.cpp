55d78753601782ffb2c026367da77bd85079540b
beep boop <githubbot@vadim.me>
2024-07-16 03:48:08 +0200

    Solution 49 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/auc2rckb Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0a38e8a1-4bc7-4cfc-9d65-53b42ccc4a78.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0a38e8a1-4bc7-4cfc-9d65-53b42ccc4a78.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0a38e8a1-4bc7-4cfc-9d65-53b42ccc4a78.cpp:11: 0a38e8a1-4bc7-4cfc-9d65-53b42ccc4a78.cpp: In function ‘int main()’: 0a38e8a1-4bc7-4cfc-9d65-53b42ccc4a78.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 0a38e8a1-4bc7-4cfc-9d65-53b42ccc4a78.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -3,21 +3,21 @@
 #include <string>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(a.size() != b.size()) {
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
 }
 
-std::vector<std::string> filter_by_substring(std::vector<std::string> strings, std::string substring){
+std::vector<std::string> filter_by_substring(std::vector<std::string> strings, std::string substring) {
     std::vector<std::string> filtered_strings;
-    for(const std::string& str : strings){
-        if(str.find(substring) != std::string::npos){
+    for (const std::string& str : strings) {
+        if (str.find(substring) != std::string::npos) {
             filtered_strings.push_back(str);
         }
     }
@@ -25,14 +25,6 @@ std::vector<std::string> filter_by_substring(std::vector<std::string> strings, s
 }
 
 int main() {
-    std::vector<std::string> strings = {"apple", "banana", "orange", "grape"};
-    std::string substring = "an";
-
-    std::vector<std::string> filtered_strings = filter_by_substring(strings, substring);
-
-    for(const std::string& str : filtered_strings){
-        std::cout << str << std::endl;
-    }
-
+    // Test your functions here
     return 0;
 }
