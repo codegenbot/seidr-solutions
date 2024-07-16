e81139b9cf8a027781bf0cf25baa05b0880ae03b
beep boop <githubbot@vadim.me>
2024-07-16 10:34:32 +0200

    Solution 5 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/1xeay9jp Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,43 +1,47 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<int> cutVector(vector<int>& nums) {
     int min_diff = INT_MAX;
-    int index = 0;
+    int cut_index = 0;
     
-    for(int i = 1; i < v.size(); i++) {
-        int diff = abs(v[i] - v[i-1]);
+    for(int i = 1; i < nums.size(); i++) {
+        int diff = abs(nums[i] - nums[0]);
         if(diff <= min_diff) {
             min_diff = diff;
-            index = i;
+            cut_index = i;
         }
     }
-
-    return {vector<int>(v.begin(), v.begin() + index), vector<int>(v.begin() + index, v.end())};
+    
+    return {vector<int>(nums.begin(), nums.begin() + cut_index), vector<int>(nums.begin() + cut_index, nums.end())};
 }
 
 int main() {
-    int n;
-    cin >> n;
-
-    vector<int> v(n);
-    for(int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-
-    pair<vector<int>, vector<int>> result = cutVector(v);
-
-    cout << "First subvector: ";
-    for(int x : result.first) {
-        cout << x << " ";
-    }
+    // test the function
+    vector<int> nums1 = {1};
+    vector<int> result1 = cutVector(nums1);
+    for(int num : result1) cout << num << " ";
     cout << endl;
-
-    cout << "Second subvector: ";
-    for(int y : result.second) {
-        cout << y << " ";
-    }
+    
+    vector<int> nums2 = {1, 10};
+    vector<int> result2 = cutVector(nums2);
+    for(int num : result2) cout << num << " ";
     cout << endl;
-
+    
+    vector<int> nums3 = {1, 100};
+    vector<int> result3 = cutVector(nums3);
+    for(int num : result3) cout << num << " ";
+    cout << endl;
+    
+    vector<int> nums4 = {1, 1000};
+    vector<int> result4 = cutVector(nums4);
+    for(int num : result4) cout << num << " ";
+    cout << endl;
+    
+    vector<int> nums5 = {1, 10000};
+    vector<int> result5 = cutVector(nums5);
+    for(int num : result5) cout << num << " ";
+    cout << endl;
+    
     return 0;
 }
