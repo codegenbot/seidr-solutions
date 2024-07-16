f619ed3f1468b7cf9e58d111576b0cf20c1c76c5
beep boop <githubbot@vadim.me>
2024-07-16 18:33:57 +0200

    Solution 65 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/iyzh3nnh Prompt: ined here    34 | int main() {       |     ^~~~ In file included from 7875edda-1006-4458-9f74-490287fe5303.cpp:40: 7875edda-1006-4458-9f74-490287fe5303.cpp: In function ‘int main()’: 7875edda-1006-4458-9f74-490287fe5303.cpp:50:27: error: too many arguments to fun ction ‘int minPath(const std::vector<std::vector<int> >&)’    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 7875edda-1006-4458-9f74-490287fe5303.cpp:9:5: note: declared here     9 | int minPath(const std::vector<std::vector<int>>& grid) {       |     ^~~~~~~ In file included from 7875edda-1006-4458-9f74-490287fe5303.cpp:40: 7875edda-1006-4458-9f74-490287fe5303.cpp:50:27: error: invalid initialization of  reference of type ‘const std::vector<int>&’ from expression of type ‘int’    50 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 7875edda-1006-4458-9f74-490287fe5303.cpp:5:37: note: in passing argument 1 of ‘b ool issame(const std::vector<int>&, const std::vector<int>&)’     5 | bool issame(const std::vector<int>& a, const std::vector<int>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,38 +1,5 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return std::equal(a.begin(), a.end(), b.begin(), b.end());
-}
-
-int minPath(const std::vector<std::vector<int>>& grid) {
-    int rows = grid.size();
-    int cols = grid[0].size();
-    
-    std::vector<std::vector<int>> dp(rows, std::vector<int>(cols, 0));
-    
-    dp[0][0] = grid[0][0];
-    
-    for (int i = 1; i < rows; ++i) {
-        dp[i][0] = dp[i - 1][0] + grid[i][0];
-    }
-    
-    for (int j = 1; j < cols; ++j) {
-        dp[0][j] = dp[0][j - 1] + grid[0][j];
-    }
-    
-    for (int i = 1; i < rows; ++i) {
-        for (int j = 1; j < cols; ++j) {
-            dp[i][j] = std::min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
-        }
-    }
-    
-    return dp[rows - 1][cols - 1];
-}
-
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}), {1, 3, 6, 8}));
+    assert(issame(minPath({{1, 3}, {3, 2}}), {{1, 4}, {4, 6}}));
     
     return 0;
 }
