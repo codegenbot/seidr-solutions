6634a3988dc8e87f6583a7eb0df25b701100aeac
beep boop <githubbot@vadim.me>
2024-07-16 11:06:14 +0200

    Solution 48 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vw4klvf6 Prompt: Complete the following code given the task description and function signature.

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,9 +1,9 @@
 string make_palindrome(string str){
     string s(str.rbegin(),str.rend());
-    for(int i=0; i<str.length(); i++){
-        if(str.substr(i)==s.substr(0,str.length()-i)){
-            return str+s.substr(str.length()-i);
+    for(int i=0; i<str.size(); i++){
+        if(str.substr(i)==s.substr(0, str.size()-i)){
+            return str+s.substr(str.size()-i);
         }
     }
-    return str+s.substr(1);
+    return str+s;
 }
