b59aadfed69abcbb8423b4069cd4ae47001943b1
beep boop <githubbot@vadim.me>
2024-07-16 08:09:48 +0200

    Solution 40 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/wkbazhxj Prompt:       |                    ^~~ f5ca63eb-f560-4f89-8503-841755da604c.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f5ca63eb-f560-4f89-8503-841755da604c.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f5ca63eb-f560-4f89-8503-841755da604c.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f5ca63eb-f560-4f89-8503-841755da604c.cpp:12: f5ca63eb-f560-4f89-8503-841755da604c.cpp: In function ‘int main()’: f5ca63eb-f560-4f89-8503-841755da604c.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ f5ca63eb-f560-4f89-8503-841755da604c.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-bool isSame(std::vector<int> a, std::vector<int> b) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> sortArray(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end(), [](int a, int b) {
+vector<int> sort_array(vector<int> arr) {
+    sort(arr.begin(), arr.end(), [](int a, int b) {
         int countA = __builtin_popcount(a);
         int countB = __builtin_popcount(b);
         if (countA == countB) {
@@ -16,11 +17,10 @@ std::vector<int> sortArray(std::vector<int> arr) {
         }
         return countA < countB;
     });
-
     return arr;
 }
 
 int main() {
-    assert(isSame(sortArray({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
+    assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
     return 0;
 }
