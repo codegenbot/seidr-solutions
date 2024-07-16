6e6f5674da80ddeb401e5a7a6067c7af1e8440ee
beep boop <githubbot@vadim.me>
2024-07-16 15:44:05 +0200

    Solution 68 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6e2io3hy Prompt: 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:26:14: error: ‘string’ was not declared  in this scope    26 | int getBonus(string s) {       |              ^~~~~~ 9e4dab24-016e-455a-82c1-9a13dbd898fc.cpp:26:14: note: ‘std::string’ is defined i n header ‘<string>’; did you forget to ‘#include <string>’?

--- bowling.cpp
+++ bowling.cpp
@@ -1,16 +1,20 @@
-```cpp
+#include <string>
+
+using namespace std;
+
 int bowlingScore(string s) {
     int score = 0;
+    int roll1, roll2;
+
     for (int i = 0; i < 10; i++) {
         if (s[i] == 'X') { 
-            score += 10 + getBonus(s, i);
+            score += 10 + getBonus(s.substr(i+1));
         } else if (s[i] == '/') { 
-            score += 5 + getBonus(s, i);
+            score += 5 + getBonus(s.substr(i+1));
         } else { 
-            int roll1 = s[i] - '0';
+            roll1 = s[i] - '0';
             if (i < 8 && s[i+1] != '/') {
-                string temp = s.substr(i+1, 1);
-                int roll2 = temp == "/" ? 0 : (temp[0] - '0');
+                roll2 = s[i+1] - '0';
                 score += roll1 + roll2;
             } else {
                 score += roll1;
@@ -21,9 +25,10 @@ int bowlingScore(string s) {
     return score;
 }
 
-int getBonus(string s, int i) {
+int getBonus(string s) {
     int bonus = 0;
-    for (int j = i + 1; j < 11; j++) {
+
+    for (int j = 0; j < s.size(); j++) {
         if (s[j] == 'X') { 
             bonus += 10;
         } else if (s[j] == '/') { 
@@ -34,4 +39,3 @@ int getBonus(string s, int i) {
     }
 
     return bonus;
-}
