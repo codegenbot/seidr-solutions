eb36455949e2797981e9987ab7379daf06867883
beep boop <githubbot@vadim.me>
2024-07-16 03:02:54 +0200

    Solution 7 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fzweyjty Prompt:                  from /usr/include/c++/11/complex:45,                  from /usr/include/c++/11/ccomplex:39,                  from /usr/include/x86_64-linux-gnu/c++/11/bits/stdc++.h:54,                  from 9d1e01a4-453d-4e64-9aae-7c3392654abf.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/istream:38,                  from /usr/include/c++/11/sstream:38,                  from /usr/include/c++/11/complex:45,                  from /usr/include/c++/11/ccomplex:39,                  from /usr/include/x86_64-linux-gnu/c++/11/bits/stdc++.h:54,                  from 9d1e01a4-453d-4e64-9aae-7c3392654abf.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9d1e01a4-453d-4e64-9aae-7c3392654abf.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,24 +2,20 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
-    lst.erase(std::remove_if(lst.begin(), lst.end(), [](const std::string& s) { return s.length() % 2 != 0; }), lst.end());
-    std::sort(lst.begin(), lst.end(), [](const std::string& a, const std::string& b) {
+vector<string> sorted_list_sum(vector<string> lst) {
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
+    sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
         if (a.length() == b.length()) {
             return a < b;
         }
         return a.length() < b.length();
     });
     return lst;
-}
-
-void main() {
-    std::vector<std::string> b = {"cc", "dd", "aaaa", "bbbb"};
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), b);
 }
