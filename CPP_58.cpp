3c0edb9a21886abbbcba07f5f6c676c5f04476b8
beep boop <githubbot@vadim.me>
2024-07-16 05:01:17 +0200

    Solution 14 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vxc8xfvh Prompt:       |                    ^~~ 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp:9: 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp: In function ‘int main()’: 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 37943793-68af-43d9-9bd2-4973e59dfeb8.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -5,11 +6,21 @@ bool issame(int a, int b) {
     return a == b;
 }
 
-vector<int> common(vector<int>& l1, vector<int>& l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
-    vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-    result.erase(unique(result.begin(), result.end()), result.end());
+std::vector<int> common(std::vector<int>& l1, std::vector<int>& l2) {
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
+    std::vector<int> result;
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
+    result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
+}
+
+int main() {
+    std::vector<int> l1 = {1, 2, 3, 4, 5};
+    std::vector<int> l2 = {3, 4, 5, 6, 7};
+    std::vector<int> result = common(l1, l2);
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+    return 0;
 }
