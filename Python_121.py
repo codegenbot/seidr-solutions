e9f91bf23cafd6348f2c61b0f1f660befd81a022
beep boop <githubbot@vadim.me>
2024-07-16 03:13:38 +0200

    Solution 15 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nzqa9sxi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10bbb887-ec84-4a49-8079-f00d561d2 b42.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,12 +1,5 @@
-```
 def solution(lst):
     return sum(i for i in lst[1::2] if i % 2 == 1)
 
-while True:
-    try:
-        lst = list(map(int, input("Enter numbers separated by space: ").split()))
-        break
-    except ValueError:
-        print("Invalid input. Please enter valid numbers.")
-
+lst = list(map(int, input("Enter numbers separated by space: ").split()))
 print(solution(lst))
