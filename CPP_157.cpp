41a3161652693b3b77c0057df281906c5305d402
beep boop <githubbot@vadim.me>
2024-07-16 17:37:55 +0200

    Solution 89 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/z88i02r3 Prompt: Complete the following code given the task description and function signature.

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,5 +1,5 @@
-if(a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
-        return true;
-    else
-        return false;
+float sides[3] = {a, b, c};
+    sort(sides, sides + 3);
+
+    return sides[0]*sides[0] + sides[1]*sides[1] == sides[2]*sides[2];
 }
