38b6799a69f414577f3a0233a21e3ce5bbba8338
beep boop <githubbot@vadim.me>
2024-07-16 06:49:03 +0200

    Solution 17 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vs66q0w0 Prompt:     3 |     for(int i = 0; i < s.length() - 2; ++i){       |     ^~~ 52190300-206a-4e64-8775-8054d9724864.cpp:3:20: error: ‘i’ does not name a type     3 |     for(int i = 0; i < s.length() - 2; ++i){       |                    ^ 52190300-206a-4e64-8775-8054d9724864.cpp:3:40: error: expected unqualified-id be fore ‘++’ token     3 |     for(int i = 0; i < s.length() - 2; ++i){       |                                        ^~ 52190300-206a-4e64-8775-8054d9724864.cpp:9:5: error: expected unqualified-id bef ore ‘return’     9 |     return true;       |     ^~~~~~ 52190300-206a-4e64-8775-8054d9724864.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from 52190300-206a-4e64-8775-8054d9724864.cpp:12: 52190300-206a-4e64-8775-8054d9724864.cpp: In function ‘int main()’: 52190300-206a-4e64-8775-8054d9724864.cpp:14:13: error: ‘is_happy’ was not declar ed in this scope    14 |     assert (is_happy("iopaxioi") == false );       |             ^~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -17,6 +17,7 @@ bool is_happy(std::string s) {
 int main() {
     assert(is_happy("iopaxioi") == false );
     assert(is_happy("abcde") == true );
+    assert(is_happy("abca") == false );
     
     return 0;
 }
