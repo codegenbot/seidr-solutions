485b7672581e4c356f6bff678b32b1e2bd07fa42
beep boop <githubbot@vadim.me>
2024-07-16 04:49:23 +0200

    Solution 11 of CPP/100, ratio 0.2 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/5lrudh85 Prompt: Program did not receive expected input

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,15 +1,22 @@
+#include <vector>
+#include <cassert>
+using namespace std;
+
+bool vectorEqual(vector<int> a,vector<int>b){
+    return a == b;
+}
+
 vector<int> make_a_pile(int n) {
     vector<int> pile;
     int stones = n;
     while (stones > 0) {
         pile.push_back(stones);
-        if (n % 2 == 1) {
-            n++;
-            stones = n;
-        } else {
-            n++;
-            stones = n;
-        }
+        stones += 2;
     }
     return pile;
+}
+
+int test_make_a_pile() {
+    assert(vectorEqual(make_a_pile(8), vector<int>({8, 10, 12, 14, 16, 18, 20, 22})) == true);
+    return 0;
 }
