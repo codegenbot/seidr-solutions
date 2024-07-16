6f1e3d6f74cfd33146a24cdfcfc0e3edb2d7573b
beep boop <githubbot@vadim.me>
2024-07-16 11:50:19 +0200

    Solution 63 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/lim0045y Prompt: Complete the following code given the task description and function signature.

--- Python_6.py
+++ Python_6.py
@@ -1,2 +1,4 @@
 def parse_nested_parens(paren_string: str) -> List[int]:
-    return [max([paren_string.count("(")] + [-1 * paren_string[::-1].find(")")])]
+    return [
+        max([s.count("(") for s in group.split(" ")]) for group in paren_string.split()
+    ]
