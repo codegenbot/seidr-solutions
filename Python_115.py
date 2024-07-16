7c973dd19c6b6f246046ea1baa2ce9e83dfe02bd
beep boop <githubbot@vadim.me>
2024-07-16 05:41:06 +0200

    Solution 45 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/91ksqau1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0431fa96-28d6-494f-9b61-485a9d7be 794.py", line 11, in <module>     check(max_fill)           ^^^^^^^^ NameError: name 'max_fill' is not defined

--- Python_115.py
+++ Python_115.py
@@ -1,4 +1,5 @@
 import math
 
-def calculate_water_needed(total_water, grid):
-    return math.ceil(total_water / (len(grid) * len(grid[0])))
+def calculate_water(grid, total_water):
+    max_fill = math.ceil(total_water / (len(grid) * len(grid[0]))
+    return max_fill
