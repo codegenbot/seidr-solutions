da349b969563e26f896d7ead576296bb440aadaa
beep boop <githubbot@vadim.me>
2024-07-16 15:15:43 +0200

    Solution 61 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/kkjpevxo Prompt: Complete the following code given the task description and function signature.

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,7 +1,4 @@
 double num = stod(value);
-    int closestInt = round(num);
-    if (num - closestInt == 0.5) {
-        closestInt = (num > 0) ? ceil(num) : floor(num);
-    }
-    return closestInt;
+    int res = round(num);
+    return res;
 }
