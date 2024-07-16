aaf8765cb1848ad9cb5fe3e8bbbc978d8a3a7548
beep boop <githubbot@vadim.me>
2024-07-16 03:28:12 +0200

    Solution 19 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1vr8zy9e Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 945e86ca-29d0-4717-8da0-1804728c22c5.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 945e86ca-29d0-4717-8da0-1804728c22c5.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 945e86ca-29d0-4717-8da0-1804728c22c5.cpp:26: 945e86ca-29d0-4717-8da0-1804728c22c5.cpp: In function ‘int main()’: 945e86ca-29d0-4717-8da0-1804728c22c5.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 945e86ca-29d0-4717-8da0-1804728c22c5.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -4,9 +4,8 @@ bool issame(vector<string> a, vector<string> b);
 
 int main() {
     vector<string> expected_result;
-
+    
     assert(issame(separate_paren_groups("((ab)(cd))"), expected_result));
-    assert(issame(separate_paren_groups("((ab)(cd))(ef)"), expected_result));
-
+    
     return 0;
 }
