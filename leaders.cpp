75831af70482f340b52f3f14af38aa34cd9d2316
beep boop <githubbot@vadim.me>
2024-07-16 11:15:38 +0200

    Solution 94 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/hrsc9sp9 Prompt: ’    27 | vector<int> leadersResult = leaders({1, 3, 4, 1, 5});       |                             ~~~~~~~^~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 45ec1c4f-96be-40cd-8ebe-3ba37c20e61a.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 45ec1c4f-96be-40cd-8ebe-3ba37c20e61a.cpp:5:34: note:   initializing argument 1 o f ‘std::vector<int> leaders(std::vector<int>&)’     5 | vector<int> leaders(vector<int>& arr) {       |                     ~~~~~~~~~~~~~^~~ 45ec1c4f-96be-40cd-8ebe-3ba37c20e61a.cpp:28:1: error: expected unqualified-id be fore ‘for’    28 | for (int leader : leadersResult) {       | ^~~ 45ec1c4f-96be-40cd-8ebe-3ba37c20e61a.cpp:31:1: error: expected unqualified-id be fore ‘return’    31 | return 0;       | ^~~~~~

--- leaders.cpp
+++ leaders.cpp
@@ -3,29 +3,32 @@
 #include <vector>
 using namespace std;
 
+vector<int> leaders(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> result;
+    
+    for (int i = 0; i < n; i++) {
+        bool isLeader = true;
+        
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] >= arr[i]) {
+                isLeader = false;
+                break;
+            }
+        }
+        
+        if (isLeader) {
+            result.push_back(arr[i]);
+        }
+    }
+    
+    return result;
+}
+
 int main() {
     vector<int> leadersResult = leaders({1, 3, 4, 1, 5});
     for (int leader : leadersResult) {
         cout << leader << " ";
     }
     return 0;
-}
-
-vector<int> leaders(vector<int>& arr) {
-    int n = arr.size();
-    vector<int> result;
-    
-    int leader = arr[n - 1];
-    result.push_back(leader);
-    
-    for (int i = n - 2; i >= 0; i--) {
-        if (arr[i] >= leader) {
-            leader = arr[i];
-            result.push_back(leader);
-        }
-    }
-    
-    reverse(result.begin(), result.end());
-    
-    return result;
 }
