199417cd2682d65f025d8ca6f8d174eeee5bc5d7
beep boop <githubbot@vadim.me>
2024-07-16 17:13:21 +0200

    Solution 30 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/dwdiqpux Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ba7188bd-ff6d-4b58-9c78-7b6be0148 697.py", line 11, in <module>     print(sum(fuel_cost(vector)))           ^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'int' object is not iterable

--- fuel-cost.py
+++ fuel-cost.py
@@ -8,5 +8,4 @@ def calculate_fuel(amount):
 def fuel_cost(vector):
     return sum(calculate_fuel(i) for i in vector)
 
-vector = [int(x) for x in input().split()]
-print(fuel_cost(vector))
+print(sum(fuel_cost([int(x) for x in input().split()])))
