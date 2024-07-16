cb0f49cd8d42cd62d2dcef221acd2d70f9f73c00
beep boop <githubbot@vadim.me>
2024-07-16 18:27:17 +0200

    Solution 56 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/68yz2635 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 8af38ddb-57f9-4a42-a83c-0b0fcecc18e7.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- luhn.cpp
+++ luhn.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int luhn(vector<int> digits) {
     int sum = 0;
-    for (auto i = 0; i < digits.size(); ++i) {
+    for (int i = 0; i < digits.size(); i++) {
         if (i % 2 == 1) {
             int temp = digits[i] * 2;
             if (temp > 9) {
@@ -19,8 +19,7 @@ int luhn(vector<int> digits) {
 }
 
 int main() {
-    int cardNumber[] = {4, 5, 3, 9, 2, 6, 5, 3, 8, 7, 3, 1, 6, 4, 3, 5};
-    vector<int> digits(cardNumber, cardNumber + 16);
+    vector<int> digits({4, 5, 3, 9, 2, 6, 5, 3, 8, 7, 3, 1, 6, 4, 3, 5});
     int result = luhn(digits);
     return 0;
 }
