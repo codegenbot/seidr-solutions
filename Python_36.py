34b682ae3a62ac8ed415f813aeece7421602aa3e
beep boop <githubbot@vadim.me>
2024-07-16 03:24:06 +0200

    Solution 20 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8l48dzsg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4337b911-54f4-4577-babf-3752903b1 97d.py", line 12, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4337b911-54f4-4577-babf-3752903b1 97d.py", line 9, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,7 +1,8 @@
 def has_property(num: int) -> bool:
-    return num % 3 == 0 or num % 5 == 0
+    return num % 11 == 0 and "7" in str(num) and num % 13 != 0
+
 
 def fizz_buzz(n: int) -> int:
-    count = sum(1 for i in range(1, n + 1) if has_property(i))
-  
+    count = (n // 11) * 9 + ((n // 11) + (n % 11 >= 7)) * 1
+
     return count
