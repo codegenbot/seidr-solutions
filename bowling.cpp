2a69835656c6b7980afdc00a8b04c1302cab7fe5
beep boop <githubbot@vadim.me>
2024-07-16 16:16:05 +0200

    Solution 87 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6e2io3hy Prompt: b67ac283-2e88-4598-be91-08cdc992a049.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ b67ac283-2e88-4598-be91-08cdc992a049.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; b67ac283-2e88-4598-be91-08cdc992a049.cpp:26:14: error: ‘string’ was not declared  in this scope    26 | int getBonus(string s) {       |              ^~~~~~ b67ac283-2e88-4598-be91-08cdc992a049.cpp:26:14: note: ‘std::string’ is defined i n header ‘<string>’; did you forget to ‘#include <string>’?

--- bowling.cpp
+++ bowling.cpp
@@ -1,6 +1,3 @@
-Here is the modified code:
-
-```cpp
 #include <string>
 using namespace std;
 
@@ -9,11 +6,11 @@ int bowlingScore(string s) {
     int roll1, roll2;
 
     for (int i = 0; i < 10; i++) {
-        if (s[i] == 'X') { // Strike
+        if (s[i] == 'X') { 
             score += 10 + getBonus(s.substr(i+1));
-        } else if (s[i] == '/') { // Spare
+        } else if (s[i] == '/') { 
             score += 5 + getBonus(s.substr(i+1));
-        } else { // Normal roll
+        } else { 
             roll1 = s[i] - '0';
             if (i < 8 && s[i+1] != '/') {
                 roll2 = s[i+1] - '0';
@@ -31,11 +28,11 @@ int getBonus(string s) {
     int bonus = 0;
 
     for (int j = 0; j < s.size(); j++) {
-        if (s[j] == 'X') { // Strike
+        if (s[j] == 'X') { 
             bonus += 10;
-        } else if (s[j] == '/') { // Spare
+        } else if (s[j] == '/') { 
             bonus += 5;
-        } else { // Normal roll
+        } else { 
             bonus += s[j] - '0';
         }
     }
