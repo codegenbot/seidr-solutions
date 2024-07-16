3384f8a8a5098fe4871eb7382ddce5bee3165213
beep boop <githubbot@vadim.me>
2024-07-16 08:49:07 +0200

    Solution 62 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rrma4mfw Prompt:       |                              ^~~~~~ 03e377e5-4081-489d-88f2-aa997dcc58de.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 03e377e5-4081-489d-88f2-aa997dcc58de.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 03e377e5-4081-489d-88f2-aa997dcc58de.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 03e377e5-4081-489d-88f2-aa997dcc58de.cpp:28: 03e377e5-4081-489d-88f2-aa997dcc58de.cpp: In function ‘int main()’: 03e377e5-4081-489d-88f2-aa997dcc58de.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 03e377e5-4081-489d-88f2-aa997dcc58de.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
@@ -13,9 +13,9 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> split_words(std::string txt) {
-    std::vector<std::string> result;
-    std::string word = "";
+vector<string> split_words(string txt) {
+    vector<string> result;
+    string word = "";
     for (char c : txt) {
         if (c == ' ' || c == ',') {
             if (!word.empty()) {
@@ -36,7 +36,7 @@ std::vector<std::string> split_words(std::string txt) {
                 count++;
             }
         }
-        result.push_back(std::to_string(count));
+        result.push_back(to_string(count));
     }
     return result;
 }
