84da84f2df4efd99e732da05bef05edb75f37be5
beep boop <githubbot@vadim.me>
2024-07-16 17:50:49 +0200

    Solution 27 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3v8snoyk Prompt:       |      ^~~~~~ be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(std::vector<int> a, std::vector<int> b) {       |      ^~~~~~ be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp:27: be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp: In function ‘int main()’: be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp:37:35: error: too few arguments to func tion ‘bool order_by_points(std::vector<int>, std::vector<int>)’    37 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ be5aa393-c05b-4a6a-b4f7-d306266a4834.cpp:16:6: note: declared here    16 | bool order_by_points(std::vector<int> a, std::vector<int> b) {       |      ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -20,5 +20,6 @@ bool order_by_points(std::vector<int> a, std::vector<int> b) {
 int main() {
     assert(issame(sort_vector({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
     assert(order_by_points({0, 6, 6, -76, -21, 23, 4}, {-76, -21, 0, 4, 23, 6, 6}));
+    
     return 0;
 }
