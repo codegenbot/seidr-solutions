197cafed2da4b2eae1f2eba7b1b6936699f97aee
beep boop <githubbot@vadim.me>
2024-07-16 06:33:08 +0200

    Solution 58 of CPP/120, ratio 0.9090909090909091 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/hq312q7f Prompt: Complete the following code given the task description and function signature.

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,12 +1,13 @@
+#include <vector>
 #include <algorithm>
 using namespace std;
 
 vector<int> maximum(vector<int> arr, int k) {
-    vector<int> res;
-    for (int i = 0; i < k; i++) {
+    vector<int> result;
+    for (int i = 0; i < k; ++i) {
         auto it = max_element(arr.begin(), arr.end());
-        res.push_back(*it);
+        result.push_back(*it);
         arr.erase(it);
     }
-    return res;
+    return result;
 }
