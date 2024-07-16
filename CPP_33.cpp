a79568869a584f1025858193654d34b306be15ea
beep boop <githubbot@vadim.me>
2024-07-16 04:26:55 +0200

    Solution 70 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fq3haqk5 Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 57969e21-f1c9-4726-8a60-dd2de77a8a6b.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 57969e21-f1c9-4726-8a60-dd2de77a8a6b.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 57969e21-f1c9-4726-8a60-dd2de77a8a6b.cpp:10: 57969e21-f1c9-4726-8a60-dd2de77a8a6b.cpp: In function ‘int main()’: 57969e21-f1c9-4726-8a60-dd2de77a8a6b.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 57969e21-f1c9-4726-8a60-dd2de77a8a6b.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,22 +3,11 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 vector<int> sort_third(vector<int> l) {
-    vector<int> sorted_indices;
-    for (int i = 0; i < l.size(); ++i) {
+    vector<int> l_copy = l;
+    for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
-            sorted_indices.push_back(i);
+            sort(l_copy.begin() + i, l_copy.begin() + i + 3);
         }
     }
-    
-    for (int index : sorted_indices) {
-        sort(l.begin() + index, l.begin() + index + 3);
-    }
-    
-    return l;
-}
-
-int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-
-    return 0;
+    return l_copy;
 }
