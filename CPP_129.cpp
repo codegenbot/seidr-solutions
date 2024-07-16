dc54930db1680a058988f123f214e1f8d0f89977
beep boop <githubbot@vadim.me>
2024-07-16 08:37:06 +0200

    Solution 17 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gskb9992 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:5: 8023913f-de88-43c9-b629-affaa2eadfb5.cpp: In function ‘int main()’: 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -6,40 +6,32 @@ bool issame(vector<int> a, vector<int> b) {
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     vector<int> result;
-    if (grid.empty() || grid[0].empty()) {
-        return result;
-    }
+    int n = grid.size();
+    if (n == 0) return result;
     
-    int rows = grid.size();
-    int cols = grid[0].size();
+    int m = grid[0].size();
+    if (m == 0) return result;
     
-    vector<vector<int>> dp(rows, vector<int>(cols, INT_MAX));
+    vector<vector<int>> dp(n, vector<int>(m, k + 1));
     dp[0][0] = grid[0][0];
     
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            if (i > 0) {
-                dp[i][j] = min(dp[i][j], dp[i - 1][j] + grid[i][j]);
-            }
-            if (j > 0) {
-                dp[i][j] = min(dp[i][j], dp[i][j - 1] + grid[i][j]);
-            }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (i > 0) dp[i][j] = min(dp[i][j], dp[i - 1][j] + grid[i][j]);
+            if (j > 0) dp[i][j] = min(dp[i][j], dp[i][j - 1] + grid[i][j]);
         }
     }
     
-    int remaining = k;
-    int r = rows - 1, c = cols - 1;
-    result.push_back(grid[r][c]);
+    int x = n - 1, y = m - 1;
+    result.push_back(grid[x][y]);
     
-    while (r > 0 || c > 0) {
-        if (r > 0 && dp[r - 1][c] <= remaining) {
-            result.push_back(grid[r - 1][c]);
-            remaining -= grid[r - 1][c];
-            --r;
+    while (x > 0 || y > 0) {
+        if (x > 0 && dp[x][y] - grid[x][y] == dp[x - 1][y]) {
+            result.push_back(grid[x - 1][y]);
+            --x;
         } else {
-            result.push_back(grid[r][c - 1]);
-            remaining -= grid[r][c - 1];
-            --c;
+            result.push_back(grid[x][y - 1]);
+            --y;
         }
     }
     
