7d254c53696afd5f7cf2eafbd9c09c18d8323b2b
beep boop <githubbot@vadim.me>
2024-07-16 17:43:44 +0200

    Solution 2 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3v8snoyk Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9e982302-058b-40c3-92e3-04abe3f5d428.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9e982302-058b-40c3-92e3-04abe3f5d428.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9e982302-058b-40c3-92e3-04abe3f5d428.cpp:19: 9e982302-058b-40c3-92e3-04abe3f5d428.cpp: In function ‘int main()’: 9e982302-058b-40c3-92e3-04abe3f5d428.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 9e982302-058b-40c3-92e3-04abe3f5d428.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,14 +1,25 @@
 #include <algorithm>
 #include <vector>
-#include <cassert>
 
-bool issame(vector<int> a, vector<int> b) {
-    // Your comparison logic here
-    // Return true if a is same as b, false otherwise
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
 
-sort(nums.begin(), nums.end(), [](int a, int b) {
-    // Your comparison logic here
-});
-
-assert(issame(sort({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
+void order_by_points(std::vector<int>& nums) {
+    std::sort(nums.begin(), nums.end(), [](int a, int b) {
+        int sum_a = 0, sum_b = 0;
+        int temp_a = abs(a), temp_b = abs(b);
+        while (temp_a > 0) {
+            sum_a += temp_a % 10;
+            temp_a /= 10;
+        }
+        while (temp_b > 0) {
+            sum_b += temp_b % 10;
+            temp_b /= 10;
+        }
+        if (sum_a == sum_b) {
+            return std::find(nums.begin(), nums.end(), a) < std::find(nums.begin(), nums.end(), b);
+        }
+        return sum_a < sum_b;
+    });
+}
