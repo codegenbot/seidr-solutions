fc9c365d94944730e9b16c23f980375a1c5b7666
beep boop <githubbot@vadim.me>
2024-07-16 04:48:44 +0200

    Solution 11 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/p4orujsn Prompt: 79d743fd-4df8-4e68-9247-5182e091bb67.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ 79d743fd-4df8-4e68-9247-5182e091bb67.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ 79d743fd-4df8-4e68-9247-5182e091bb67.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ 79d743fd-4df8-4e68-9247-5182e091bb67.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,5 +1,7 @@
+```cpp
 #include <boost/any.hpp>
 #include <boost/variant.hpp>
+#include <string>
 
 using namespace std;
 
@@ -10,22 +12,22 @@ boost::variant<int, float, string> compare_one(boost::any a, boost::any b) {
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         string str = (string)a.convert_to<string>();
         float f = (float)b.convert_to<float>();
-        return (f > stof(str)) ? boost::variant<int, float, string>(f) : boost::variant<int, float, string>(str);
+        return (f > stod(str)) ? boost::variant<int, float, string>(f) : boost::variant<int, float, string>(str);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string str1 = (string)a.convert_to<string>();
         string str2 = (string)b.convert_to<string>();
-        return (stof(str2) > stof(str1)) ? boost::variant<int, float, string>(str2) : (stof(str1) == stof(str2)) ? boost::variant<int, float, string>("None") : boost::variant<int, float, string>(str1);
+        return (stod(str2) > stod(str1)) ? boost::variant<int, float, string>(str2) : (stod(str1) == stod(str2)) ? boost::variant<int, float, string>("None") : boost::variant<int, float, string>(str1);
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         int i = (int)a.convert_to<int>();
         string str = (string)b.convert_to<string>();
-        return (stof(str) > i) ? boost::variant<int, float, string>(str) : (i == stof(str)) ? boost::variant<int, float, string>("None") : boost::variant<int, float, string>(i);
+        return (stod(str) > i) ? boost::variant<int, float, string>(str) : (i == stod(str)) ? boost::variant<int, float, string>("None") : boost::variant<int, float, string>(i);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         string str = (string)a.convert_to<string>();
         int i = (int)b.convert_to<int>();
-        return (stof(str) > i) ? boost::variant<int, float, string>(str) : (i == stof(str)) ? boost::variant<int, float, string>("None") : boost::variant<int, float, string>(i);
+        return (stod(str) > i) ? boost::variant<int, float, string>(str) : (i == stod(str)) ? boost::variant<int, float, string>("None") : boost::variant<int, float, string>(i);
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
         float f = (float)a.convert_to<float>();
