a8104248f5bf8054687497dfea1a4df8c6ed840b
beep boop <githubbot@vadim.me>
2024-07-16 02:55:40 +0200

    Solution 4 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/ni89a9ek Prompt: this scope    16 |     cin >> n;       |     ^~~ e47ddee0-fb6a-44f6-ba06-0d32468b7e92.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; e47ddee0-fb6a-44f6-ba06-0d32468b7e92.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << fuelCost(vec) << endl;       |     ^~~~ e47ddee0-fb6a-44f6-ba06-0d32468b7e92.cpp:21:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? e47ddee0-fb6a-44f6-ba06-0d32468b7e92.cpp:21:30: error: ‘endl’ was not declared i n this scope    21 |     cout << fuelCost(vec) << endl;       |                              ^~~~ e47ddee0-fb6a-44f6-ba06-0d32468b7e92.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,12 +1,24 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 
-int fuelCost(vector<int> nums) {
+int fuelCost(vector<int>& vec) {
     int sum = 0;
-    for (int num : nums) {
-        int res = (num / 3) - 2;
-        if (res < 0) res = 0;
-        sum += res;
+    for (int i : vec) {
+        int x = i / 3;
+        x = floor(x);
+        sum += x - 2;
     }
     return sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
+    cout << fuelCost(vec) << endl;
+    return 0;
 }
