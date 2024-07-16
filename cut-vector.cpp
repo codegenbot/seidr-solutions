99fe21d9bb52c25d1cb02d823d14736a9d530644
beep boop <githubbot@vadim.me>
2024-07-16 09:51:08 +0200

    Solution 77 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/3xxz6ioc Prompt: a1f6fc5a-0546-481e-93d8-3082891d7027.cpp: In function ‘int main()’: a1f6fc5a-0546-481e-93d8-3082891d7027.cpp:30:9: error: ‘cout’ was not declared in  this scope    30 |         cout << "[";       |         ^~~~ a1f6fc5a-0546-481e-93d8-3082891d7027.cpp:3:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     2 | #include <cmath>   +++ |+#include <iostream>     3 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,18 +1,24 @@
+```c++
 #include <vector>
 #include <cmath>
-#include <iostream>
+#include <iostream> 
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     
+    if (n == 1) {
+        return {{v}, {}};
+    }
+    
     for (int i = 0; i < n - 1; ++i) {
         if (abs(v[i] - v[i + 1]) <= abs(v[0] - v[n - 1])) {
-            for (int j = 0; j <= i; ++j) {
+            int midIndex = i;
+            for (int j = 0; j < i; ++j) {
                 res[0].push_back(v[j]);
             }
-            for (int j = i + 1; j < n; ++j) {
+            for (int j = i; j < n; ++j) {
                 res[1].push_back(v[j]);
             }
             return {{}, res[0], {}, res[1]};
@@ -22,7 +28,6 @@ vector<vector<int>> cutVector(vector<int> v) {
     int mid = n / 2;
     res[0] = vector<int>(v.begin(), v.begin() + mid);
     res[1] = vector<int>(v.begin() + mid, v.end());
-    
     return res;
 }
 
@@ -36,6 +41,5 @@ int main() {
         }
         cout << "]\n";
     }
-    
     return 0;
 }
