6d95a7326bd6878ddf73bffa653130d8878d450c
beep boop <githubbot@vadim.me>
2024-07-16 13:49:06 +0200

    Solution 8 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6e2io3hy Prompt: ea77a845-e3c0-4a55-a4ad-9d43045ffa64.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ ea77a845-e3c0-4a55-a4ad-9d43045ffa64.cpp:24:14: error: ‘string’ was not declared  in this scope    24 | int getBonus(string s, int i) {       |              ^~~~~~ ea77a845-e3c0-4a55-a4ad-9d43045ffa64.cpp:24:24: error: expected primary-expressi on before ‘int’    24 | int getBonus(string s, int i) {       |                        ^~~ ea77a845-e3c0-4a55-a4ad-9d43045ffa64.cpp:24:29: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | int getBonus(string s, int i) {       |                             ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,18 +1,16 @@
-```cpp
-#include <iostream>
-#include <string>
-
-int bowlingScore(string s) {
+int bowlingScore(std::string s) {
     int score = 0;
+    int roll1, roll2;
+
     for (int i = 0; i < 10; i++) {
-        if (s[i] == 'X') { 
+        if (s[i] == 'X') { // Strike
             score += 10 + getBonus(s, i);
-        } else if (s[i] == '/') { 
+        } else if (s[i] == '/') { // Spare
             score += 5 + getBonus(s, i);
-        } else { 
-            int roll1 = s[i] - '0';
+        } else { // Normal roll
+            roll1 = s[i] - '0';
             if (i < 8 && s[i+1] != '/') {
-                int roll2 = s[i+1] - '0';
+                roll2 = s[i+1] - '0';
                 score += roll1 + roll2;
             } else {
                 score += roll1;
@@ -23,17 +21,17 @@ int bowlingScore(string s) {
     return score;
 }
 
-int getBonus(string s, int i) {
+int getBonus(std::string s, int i) {
     int bonus = 0;
+
     for (int j = i + 1; j < 11; j++) {
-        if (s[j] == 'X') { 
+        if (s[j] == 'X') { // Strike
             bonus += 10;
-        } else if (s[j] == '/') { 
+        } else if (s[j] == '/') { // Spare
             bonus += 5;
-        } else { 
+        } else { // Normal roll
             bonus += s[j] - '0';
         }
     }
 
     return bonus;
-}
