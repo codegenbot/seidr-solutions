f452abb3d1b8024f40f9afb411334bc8b7c31239
beep boop <githubbot@vadim.me>
2024-07-16 05:50:38 +0200

    Solution 30 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/97a3wy3p Prompt:       |                    ^~~ cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp:21: cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp: In function ‘int main()’: cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ cbf64649-87a9-4efc-a78e-8ed0a723eb14.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,30 +1,15 @@
 #include <vector>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
+    if (a.size() != b.size()) {
+        return false;
+    }
 
-std::vector<int> pluck(std::vector<int> arr) {
-    std::vector<int> result;
-    int smallest_even = -1;
-    int smallest_index = -1;
-
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] % 2 == 0 && (smallest_even == -1 || arr[i] < smallest_even)) {
-            smallest_even = arr[i];
-            smallest_index = i;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
 
-    if (smallest_even != -1) {
-        result.push_back(smallest_even);
-        result.push_back(smallest_index);
-    }
-
-    return result;
-}
-
-int main() {
-    assert(issame(pluck({7, 9, 7, 1}), {}));
-    return 0;
+    return true;
 }
