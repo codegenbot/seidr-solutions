a7bd0f26c40bb67823a2a2af9e352fe8d0a4f563
beep boop <githubbot@vadim.me>
2024-07-16 05:41:38 +0200

    Solution 47 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/91ksqau1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1c2d7390-9f2d-4950-82be-3cd5c7448 106.py", line 4     max_fill = math.ceil(total_water / (len(grid) * len(grid[0]))                         ^ SyntaxError: '(' was never closed

--- Python_115.py
+++ Python_115.py
@@ -1,7 +1,5 @@
 import math
 
 def calculate_water(grid, total_water):
-    return math.ceil(total_water / (len(grid) * len(grid[0]))
-
-max_fill = 100
-check(max_fill)
+    max_fill = math.ceil(total_water / (len(grid) * len(grid[0])))
+    return max_fill
