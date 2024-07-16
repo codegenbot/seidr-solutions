be11884bcc3cde6c8b7e6f9baec924aa227e2206
beep boop <githubbot@vadim.me>
2024-07-16 16:33:35 +0200

    Solution 94 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9ukk1lth Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,5 +1,5 @@
 int result = operand[0];
-    for (size_t i = 0; i < operato.size(); i++) {
+    for (size_t i = 0; i < operato.size(); ++i) {
         if (operato[i] == "+") {
             result += operand[i + 1];
         } else if (operato[i] == "-") {
