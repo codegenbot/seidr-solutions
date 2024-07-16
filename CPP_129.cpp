9d668cac757525e8907fff365b4085a8cc043413
beep boop <githubbot@vadim.me>
2024-07-16 08:42:29 +0200

    Solution 23 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gskb9992 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:5: 8023913f-de88-43c9-b629-affaa2eadfb5.cpp: In function ‘int main()’: 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 8023913f-de88-43c9-b629-affaa2eadfb5.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -2,23 +2,48 @@ bool issame(vector<int> a, vector<int> b) {
     return a[0] == b[0] && a[1] == b[1];
 }
 
+vector<int> minPath(vector<vector<int>> grid, int k);
+
 vector<int> minPath(vector<vector<int>> grid, int k) {
-    vector<int> result;
-    // Implementation of minPath function
-    return result;
+    int n = grid.size();
+    int m = grid[0].size();
+    
+    vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+    dp[0][0] = 0;
+    
+    queue<vector<int>> q;
+    q.push({0, 0});
+    
+    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+        
+        for (auto dir : dirs) {
+            int x = cur[0] + dir[0];
+            int y = cur[1] + dir[1];
+            
+            if (x >= 0 && x < n && y >= 0 && y < m) {
+                int cost = issame(grid[cur[0]][cur[1]], grid[x][y]) ? 0 : 1;
+                
+                if (dp[x][y] > dp[cur[0]][cur[1]] + cost) {
+                    dp[x][y] = dp[cur[0]][cur[1]] + cost;
+                    q.push({x, y});
+                }
+            }
+        }
+    }
+    
+    return dp[n - 1][m - 1] <= k ? vector<int>{dp[n - 1][m - 1], k - dp[n - 1][m - 1]} : vector<int>{-1};
 }
 
 int main() {
-    // Forward declaration of minPath function
-    vector<int> minPath(vector<vector<int>> grid, int k);
-
-    // Test minPath function
-    vector<vector<int>> testGrid = {{1, 2}, {3, 4}, {5, 6}};
-    int k = 3;
-    vector<int> expectedOutput = {1, 2};
-
-    vector<int> output = minPath(testGrid, k);
-    assert(issame(output, expectedOutput));
-
+    vector<vector<int>> grid = {{0, 0, 1}, {0, 0, 1}, {1, 1, 0}};
+    int k = 1;
+    
+    vector<int> result = minPath(grid, k);
+    assert(result == vector<int>{2, -1});
+    
     return 0;
 }
