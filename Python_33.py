18c18a8bb64e54f10c1e38f0631a2189f53e289b
beep boop <githubbot@vadim.me>
2024-07-16 16:43:30 +0200

    Solution 38 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vfpgwfnv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92068f65-657b-41d6-8c5f-458441794 856.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/92068f65-657b-41d6-8c5f-458441794 856.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,11 +1,3 @@
 def sort_third(l: list):
-    sorted_triplets = sorted(
-        [
-            sorted(triplet)
-            for triplet in zip(
-                *[iter([x for x in l if x % 2 != 0 and x is not None])] * 3
-            )
-        ],
-        key=lambda x: max(x),
-    )
+    sorted_triplets = sorted([sorted(triplet) for triplet in zip(*[iter(sorted([x for x in l if x % 2 == 1 and x is not None]))]*3)], key=lambda x: max(x))
     return sorted_triplets
