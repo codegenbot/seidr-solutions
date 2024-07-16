2a52bfda8c88b89d07e5fea84688832491ef1f66
beep boop <githubbot@vadim.me>
2024-07-16 17:55:26 +0200

    Solution 23 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/is6b1rt8 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1e44a317-ab54-458c-b3fb-cd9f52dc946a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1e44a317-ab54-458c-b3fb-cd9f52dc946a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1e44a317-ab54-458c-b3fb-cd9f52dc946a.cpp:11: 1e44a317-ab54-458c-b3fb-cd9f52dc946a.cpp: In function ‘int main()’: 1e44a317-ab54-458c-b3fb-cd9f52dc946a.cpp:21:20: error: ‘rolling_max’ was not dec lared in this scope    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 1e44a317-ab54-458c-b3fb-cd9f52dc946a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,4 +1,8 @@
-bool issame(vector<int> a, vector<int> b);
+vector<int> rolling_max(vector<int> numbers);
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
