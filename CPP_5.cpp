9c5273d3805b743482563e5749629a7504b981a8
beep boop <githubbot@vadim.me>
2024-07-16 18:09:03 +0200

    Solution 47 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/56b48a7o Prompt:       |                    ^~~ a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp:12: a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp: In function ‘int main()’: a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ a5a6eaa4-3cce-4800-a640-16ebb3c4480c.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -4,6 +4,17 @@ bool issame(vector<int> a, vector<int> b);
 
 vector<int> intersperse(vector<int> numbers, int delimeter);
 
+vector<int> intersperse(vector<int> numbers, int delimeter){
+    vector<int> result;
+    for(int i = 0; i < numbers.size(); ++i){
+        result.push_back(numbers[i]);
+        if(i < numbers.size()-1){
+            result.push_back(delimeter);
+        }
+    }
+    return result;
+}
+
 bool issame(vector<int> a, vector<int> b){
     if(a.size() != b.size()){
         return false;
@@ -14,15 +25,4 @@ bool issame(vector<int> a, vector<int> b){
         }
     }
     return true;
-}
-
-vector<int> intersperse(vector<int> numbers, int delimeter){
-    vector<int> result;
-    for(int i = 0; i < numbers.size(); ++i){
-        result.push_back(numbers[i]);
-        if(i < numbers.size()-1){
-            result.push_back(delimeter);
-        }
-    }
-    return result;
 }
