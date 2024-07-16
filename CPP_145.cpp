715654d7b135238600654a1adf333c8d45c4b80d
beep boop <githubbot@vadim.me>
2024-07-16 08:54:50 +0200

    Solution 20 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/osarpqwd Prompt:    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:26: 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp: In function ‘int main()’: 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:36:20: error: ‘order_by_points’ was not  declared in this scope    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 95c13e10-c1f1-4ddd-8796-c741e67e6a97.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,13 +2,11 @@ bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
-vector<int> sortNumbers(const vector<int>& nums) {
-    vector<int> sorted_nums = nums;
-
-    sort(sorted_nums.begin(), sorted_nums.end(), [](int a, int b) {
+vector<int> order_by_points(const vector<int>& nums) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
-        a = abs(a);
-        b = abs(b);
+        if (a < 0) a *= -1;
+        if (b < 0) b *= -1;
 
         while (a > 0) {
             sum_a += a % 10;
@@ -27,5 +25,5 @@ vector<int> sortNumbers(const vector<int>& nums) {
         return sum_a < sum_b;
     });
 
-    return sorted_nums;
+    return nums;
 }
