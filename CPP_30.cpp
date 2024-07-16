3dead95f4b71c07f4a213c595790f8ee9f52ae8c
beep boop <githubbot@vadim.me>
2024-07-16 03:55:20 +0200

    Solution 50 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/pwh2fa66 Prompt:       |                    ^~~~~ 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp:11: 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp: In function ‘int main()’: 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 1a9d4c3a-5a42-47ad-aa31-2617f34d0813.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,21 +1,23 @@
 #include <vector>
 
-bool areSame(vector<float> a, vector<float> b){
-    return a == b;
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<float> get_positive(vector<float> l){
     vector<float> result;
-    for(float num : l){
-        if(num > 0){
+    for (float num : l) {
+        if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
-}
-
-int main() {
-    assert(areSame(get_positive({}), {}));
-  
-    return 0;
 }
