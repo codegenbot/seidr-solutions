b40d6e117f169adb552d7fe248d3c3de67312a8b
beep boop <githubbot@vadim.me>
2024-07-17 06:01:06 +0200

    Solution 48 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/k4744atr Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,46 +1,31 @@
-Here's the solution:
+Here is the solution:
 
 #include <iostream>
 #include <string>
-using namespace std;
 
-string spinWords(string str) {
-    string result = "";
-    int wordLength;
-    for (int i = 0; i <= str.length(); i++) {
-        if (str[i] == ' ') {
-            result += str.substr(i - wordLength, wordLength) + " ";
-            wordLength = 0;
-        } else if (i == str.length()) {
-            result += str.substr(i - wordLength);
-        } else {
-            wordLength++;
+std::string spinWords(std::string str) {
+    std::string result = "";
+    std::size_t prevPos = 0;
+    for (std::size_t pos = 0; pos <= str.size(); ++pos) {
+        if (pos == str.size() || str[pos] == ' ') {
+            std::size_t len = pos - prevPos;
+            if (len >= 5) {
+                for (int i = len - 1; i >= 0; --i)
+                    result += str[prevPos + i];
+            } else
+                result += str.substr(prevPos, len);
+            if (pos < str.size())
+                result += ' ';
+            prevPos = pos + 1;
         }
     }
-
-    for (int i = 0; i < result.length(); i++) {
-        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
-            int j = i;
-            while ((j + 1 < result.length()) && ((result[j + 1] >= 'a' && result[j + 1] <= 'z') || (result[j + 1] >= 'A' && result[j + 1] <= 'Z'))) {
-                j++;
-            }
-            if (j - i >= 5) {
-                string word = result.substr(i, j - i + 1);
-                reverse(word.begin(), word.end());
-                result.replace(i, j - i + 1, word);
-                i += j - i;
-            }
-        }
-    }
-
     return result;
 }
 
 int main() {
-    cout << spinWords("a") << endl; // a
-    cout << spinWords("this is a test") << endl; // this is a test
-    cout << spinWords("this is another test") << endl; // this is rehtona test
-    cout << spinWords("hi") << endl; // hi
-
+    std::string str;
+    while (std::cin >> str) {
+        std::cout << spinWords(str) << std::endl;
+    }
     return 0;
 }
