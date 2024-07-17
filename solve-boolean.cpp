fd53685e0ab1811ac7c617d92f3d4b7eb6db94d8
beep boop <githubbot@vadim.me>
2024-07-17 02:53:31 +0200

    Solution 50 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/a3mny2dw Prompt: it must return True for input f|f&t|t|t&t|f, but it returns Enter a Boolean expression: terminate called after throwing an instance of 'std: :out_of_range'   what():  basic_string::substr: __pos (which is 14) > this->size() (which is 13 ).

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,34 +1,59 @@
-std::string solveBoolean(std::string s) {
-    std::stack<char> st;
-    std::string res = "";
-    
-    for(int i=0; i<s.length(); i++){
-        if(s[i] == '&'){
-            while(!st.empty() && st.top() == '&') {
-                st.pop();
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool evaluateBooleanExpression(string expression) {
+    bool result = true;
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '&') {
+            int j = i + 1;
+            while (j < expression.length() && expression[j] != '&') {
+                j++;
             }
-            if(st.empty()) 
-                res += "T";
-            else
-                res += "F";
-            st.push('&');
-        }else if(s[i] == '|'){
-            while(!st.empty() && st.top() == '|') {
-                st.pop();
+            if (j <= expression.length()) { 
+                string subExpression = expression.substr(i + 1, j - i - 1);
+                bool left = (subExpression == "t") ? true : false;
+                int k = j + 1;
+            while (k < expression.length() && expression[k] != '&') {
+                k++;
             }
-            if(st.empty()) 
-                res += "T";
-            else
-                res += "F";
-            st.push('|');
-        }else{
-            st.push(s[i]);
+            if (k <= expression.length()) { 
+                string subRight = expression.substr(j + 1, k - j - 1);
+                bool right = (subRight == "t") ? true : false;
+                result &= (left && right);
+            } else {
+                result &= left;
+            }
+        } else if (expression[i] == '|') {
+            int j = i + 1;
+            while (j < expression.length() && expression[j] != '|') {
+                j++;
+            }
+            if (j <= expression.length()) { 
+                string subExpression = expression.substr(i + 1, j - i - 1);
+                bool left = (subExpression == "t") ? true : false;
+                int k = j + 1;
+            while (k < expression.length() && expression[k] != '|') {
+                k++;
+            }
+            if (k <= expression.length()) { 
+                string subRight = expression.substr(j + 1, k - j - 1);
+                bool right = (subRight == "t") ? true : false;
+                result |= (left || right);
+            } else {
+                result |= left;
+            }
         }
     }
-    
-    while(!st.empty()){
-        st.pop();
-    }
-    
-    return (res == "T") ? "True" : "False";
+    return result;
+}
+
+int main() {
+    string expression;
+    cout << "Enter a Boolean expression: ";
+    cin >> expression;
+    cout << "Result: " << (evaluateBooleanExpression(expression) ? "True" : "False") << endl;
+    return 0;
 }
