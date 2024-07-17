ee431478dd0594afb20717949026e6ef1dad8b39
beep boop <githubbot@vadim.me>
2024-07-17 03:40:46 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,56 +1,16 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
     int minDiff = INT_MAX;
-    int cutIndex = -1;
-    
-    for (int i = 0; i < v.size(); i++) {
-        int leftSum = 0, rightSum = 0;
-        
-        for (int j = 0; j <= i; j++) {
-            leftSum += v[j];
-        }
-        
-        for (int j = i + 1; j < v.size(); j++) {
-            rightSum += v[j];
-        }
-        
-        int diff = abs(leftSum - rightSum);
-        
-        if (diff < minDiff) {
+    pair<int, int> result;
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs((int)nums[i-1] - (int)nums[i]);
+        if (diff <= minDiff) {
             minDiff = diff;
-            cutIndex = i;
+            result.first = vector<int>(nums.begin(), nums.begin() + i);
+            result.second = vector<int>(nums.begin() + i, nums.end());
         }
     }
-    
-    vector<int> left = vector<int>(v.begin(), v.begin() + cutIndex);
-    vector<int> right = vector<int>(v.begin() + cutIndex, v.end());
-    
-    return make_pair(left, right);
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    
-    pair<vector<int>, vector<int>> result = cutVector(v);
-    
-    cout << "[";
-    for (int num : result.first) {
-        cout << num << " ";
-    }
-    cout << "]" << endl;
-    
-    cout << "[";
-    for (int num : result.second) {
-        cout << num << " ";
-    }
-    cout << "0]" << endl;
-    
-    return 0;
+    return result;
 }
