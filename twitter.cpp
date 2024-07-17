031f8eca00a02a37584cb49e9c8f02ea7258cc91
beep boop <githubbot@vadim.me>
2024-07-17 09:06:49 +0200

    Solution 58 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/04pxk33s Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,16 +1,16 @@
-```cpp
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
     string tweet;
-    
     cout << "Enter a tweet: ";
-    if (!getline(cin, tweet)) {
-        cout << "You didn't type anything" << endl;
-    } else if (tweet.length() > 140) {
+    cin >> tweet;
+    
+    if (tweet.length() > 140) {
         cout << "Too many characters" << endl;
+    } else if (tweet.empty()) {
+        cout << "You didn't type anything" << endl;
     } else {
         cout << "Your tweet has " << tweet.length() << " characters" << endl;
     }
