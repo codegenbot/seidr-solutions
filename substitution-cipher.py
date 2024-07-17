9330b28f36e51ff447a24640d6ec55dbf755a84a
beep boop <githubbot@vadim.me>
2024-07-17 06:31:21 +0200

    Solution 45 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/02hdq8ub Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a09f1016-af0e-4c7f-82e7-311dca5c6 7a7.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -3,4 +3,4 @@ def substitution_cipher(key, message):
     
     rev_key_map = {v: k for k, v in key_map.items()}
     
-    return "".join([rev_key_map.get(char, char) for char in message])
+    return "".join([rev_key_map.get(char, char) for char in key] + [rev_key_map.get(char, char) for char in message])
