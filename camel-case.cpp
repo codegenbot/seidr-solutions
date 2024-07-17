a11d67fa770f19c5735635b8e94f01c69abfbf8b
beep boop <githubbot@vadim.me>
2024-07-17 16:47:08 +0200

    Solution 75 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/lx281pc9 Prompt: Take a string in kebab-case and convert all of the words to camelCase. Each group of words to convert is delimited by "-", and each grouping is separated by a space. For example: "camel-case example-test-string"-> "camelCase exampleTestString".

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,40 +1,46 @@
+#include <vector>
 #include <iostream>
 #include <string>
 
-std::string camelCase(std::string str) {
-    std::vector<std::string> words;
-    size_t pos = 0;
+using namespace std;
 
-    while ((pos = str.find(" ")) != std::string::npos) {
-        words.push_back(str.substr(0, pos));
-        str.erase(0, pos + 1);
-    }
-
-    if (!str.empty()) {
-        words.push_back(str);
-    }
-
-    for (size_t i = 0; i < words.size(); ++i) {
-        if (i > 0)
-            words[i][0] = toupper(words[i][0]);
-    }
-
-    std::string result;
-    for (const auto& word : words) {
-        result += word;
-        if (i + 1 < words.size()) {
-            result += " ";
+string camelCase(string str) {
+    vector<string> words;
+    string temp = "";
+    for (char c : str) {
+        if (c == '-') {
+            words.push_back(temp);
+            temp = "";
+        } else if (c == ' ') {
+            if (!temp.empty()) {
+                words.push_back(temp);
+                temp = "";
+            }
+        } else {
+            temp += tolower(c);
         }
-        ++i;
+    }
+    if (!temp.empty())
+        words.push_back(temp);
+
+    string result;
+    for (int i = 0; i < words.size(); ++i) {
+        if (i > 0)
+            result += char(toupper(words[i][0]));
+        else
+            result += words[i];
     }
 
     return result;
 }
 
 int main() {
-    std::string input;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, input);
-    std::cout << camelCase(input) << std::endl;
+    int numCases;
+    cin >> numCases;
+    for (int caseNum = 1; caseNum <= numCases; ++caseNum) {
+        string str;
+        cin >> str;
+        cout << "Case " << caseNum << ": " << camelCase(str) << endl;
+    }
     return 0;
 }
