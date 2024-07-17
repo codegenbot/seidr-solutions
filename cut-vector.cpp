4ffaaba5011a7a30f538c720c1c45d0397f86fcb
beep boop <githubbot@vadim.me>
2024-07-17 03:48:53 +0200

    Solution 86 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,18 +2,50 @@
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
-    int pos = 0;
-    
-    for (int i = 1; i < v.size(); i++) {
-        if (abs(v[i] - v[0]) <= min_diff) {
-            min_diff = abs(v[i] - v[0]);
-            pos = i;
+    int n = v.size();
+    for(int i = 0; i < n-1; i++) {
+        int leftSum = 0, rightSum = 0;
+        for(int j = 0; j <= i; j++) {
+            leftSum += v[j];
+        }
+        for(int j = i+1; j < n; j++) {
+            rightSum += v[j];
+        }
+        if(abs(leftSum-rightSum) == 0 || abs(leftSum-rightSum) < abs((n-1)/2-(n-1)/2)) {
+            return {{v[0],v[0]}, v.substr(1)};
         }
     }
-    
-    vector<int> left(v.begin(), v.begin() + pos);
-    vector<int> right(v.begin() + pos, v.end());
-    
-    return make_pair(left, right);
+    // If no cut is found, the entire vector is one subvector
+    return {{}, v};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    pair<vector<int>, vector<int>> res = cutVector(v);
+    cout << "[";
+
+    if(res.first.size() > 0) {
+        for(int i = 0; i < res.first.size(); i++) {
+            cout << res.first[i] << (i == res.first.size()-1 ? "" : ",");
+        }
+        cout << "] [";  
+    } else {
+        cout << "] [";
+    }
+
+    if(res.second.size() > 0) {
+        for(int i = 0; i < res.second.size(); i++) {
+            cout << res.second[i] << (i == res.second.size()-1 ? "" : ",");
+        }
+        cout << "]\n";
+    } else {
+        cout << "]\n";
+    }
+
+    return 0;
 }
