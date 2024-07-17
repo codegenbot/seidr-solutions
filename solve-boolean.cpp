c2b9c107c1750fa8088a2f55c124aa1667393b0d
beep boop <githubbot@vadim.me>
2024-07-17 05:16:09 +0200

    Solution 99 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xwifqaul Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,34 +1,74 @@
+#include <vector>
+#include <iostream>
 #include <string>
+
 using namespace std;
 
-bool evaluateBooleanExpression(string expression) {
-    if (expression == "T" || expression == "t")
-        return true;
-    else if (expression == "F" || expression == "f")
-        return false;
-    else if (expression.find('&') != string::npos) {
-        size_t pos = expression.find('&');
-        bool left = evaluateBooleanExpression(expression.substr(0, pos));
-        bool right = evaluateBooleanExpression(expression.substr(pos + 1));
-        return left && right;
+bool solveBoolean(string s) {
+    if(s == "T") return true;
+    if(s == "F") return false;
+    
+    int i = 0, j = s.size() - 1;
+    while(i <= j){
+        if(s[i] == '&' || s[j] == '&'){
+            return false;
+        }
+        if(s[i] == '|' || s[j] == '|'){
+            return true;
+        }
+        i++;
+        j--;
     }
-    else if (expression.find('|') != string::npos) {
-        size_t pos = expression.find('|');
-        bool left = evaluateBooleanExpression(expression.substr(0, pos));
-        bool right = evaluateBooleanExpression(expression.substr(pos + 1));
-        return left || right;
+    
+    for(int k = 0; k < s.size(); k++){
+        if(s[k] == '&') {
+            int left = 0, right = 0;
+            for(int l = 0; l <= k; l++) {
+                if(s[l] == '&') left++;
+                else break;
+            }
+            for(int r = k+1; r < s.size(); r++){
+                if(s[r] == '&') right++;
+                else break;
+            }
+            while(left > 0 || right > 0) {
+                if(left > 0 && right > 0) return false;
+                if(left > 0) left--;
+                else if(right > 0) right--;
+                k++;
+            }
+        }
+        else if(s[k] == '|') {
+            int left = 0, right = 0;
+            for(int l = 0; l <= k; l++) {
+                if(s[l] == '|') left++;
+                else break;
+            }
+            for(int r = k+1; r < s.size(); r++){
+                if(s[r] == '|') right++;
+                else break;
+            }
+            while(left > 0 || right > 0) {
+                if(left > 0 && right > 0) return true;
+                if(left > 0) left--;
+                else if(right > 0) right--;
+                k++;
+            }
+        }
     }
-    return false; // Should not reach here
+    
+    return false;
 }
 
 int main() {
-    string input;
+    string s;
     cout << "Enter a Boolean expression: ";
-    cin >> input;
-    bool result = evaluateBooleanExpression(input);
-    if (result)
+    cin >> s;
+    bool result = solveBoolean(s);
+    if(result)
         cout << "True" << endl;
     else
         cout << "False" << endl;
+    
     return 0;
 }
