3b3b98a52d85146b27f420e0b948688fd0574d09
beep boop <githubbot@vadim.me>
2024-07-17 06:42:15 +0200

    Solution 22 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/04pxk33s Prompt: n this scope     2 |     string tweet;       |     ^~~~~~ 2c7c6ff1-5b5e-43cd-aef4-b98434377ae9.cpp:3:5: error: ‘cout’ was not declared in this scope     3 |     cout << "Enter a tweet: ";       |     ^~~~ 2c7c6ff1-5b5e-43cd-aef4-b98434377ae9.cpp:4:13: error: ‘cin’ was not declared in this scope     4 |     getline(cin, tweet);       |             ^~~ 2c7c6ff1-5b5e-43cd-aef4-b98434377ae9.cpp:4:18: error: ‘tweet’ was not declared i n this scope     4 |     getline(cin, tweet);       |                  ^~~~~ 2c7c6ff1-5b5e-43cd-aef4-b98434377ae9.cpp:4:5: error: ‘getline’ was not declared in this scope     4 |     getline(cin, tweet);       |     ^~~~~~~ 2c7c6ff1-5b5e-43cd-aef4-b98434377ae9.cpp:6:5: error: ‘validateTweet’ was not dec lared in this scope     6 |     validateTweet(tweet);       |     ^~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -2,18 +2,28 @@
 #include <iostream>
 #include <string>
 
+int validateTweet(const std::string& tweet) {
+    if (tweet.empty()) return 1; 
+    else if (tweet.length() > 140) return 2; 
+    else return 3; 
+}
+
 int main() {
     std::string tweet;
     std::cout << "Enter a tweet: ";
     std::getline(std::cin, tweet);
-
-    if (tweet.length() > 140) {
-        std::cout << "Too many characters";
-    } else if (tweet.empty()) {
-        std::cout << "You didn't type anything";
-    } else {
-        std::cout << "Your tweet has " << tweet.length() << " characters";
+    
+    int result = validateTweet(tweet);
+    switch (result) {
+        case 1:
+            std::cout << "You didn't type anything";
+            break;
+        case 2:
+            std::cout << "Too many characters";
+            break;
+        default:
+            std::cout << "Your tweet has " << tweet.length() << " characters";
     }
-
+    
     return 0;
 }
