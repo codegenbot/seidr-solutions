1455ee7b1a68d4d282c798896fd4b21ae26c5f4b
beep boop <githubbot@vadim.me>
2024-07-17 05:20:17 +0200

    Solution 26 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/d5tswv8n Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from cf9d0d55-52c0-439b-8d91-b322bf81dbec.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,6 +1,6 @@
 #include <climits>
 #include <vector>
-#include <cmath>
+#include <initializer_list>
 
 using namespace std;
 
@@ -10,9 +10,9 @@ vector<vector<int>> cutVector(vector<int> v) {
     
     for(int i = 1; i <= v.size(); i++) {
         if(i == v.size()) {
-            vector<int> left(v.begin(), v.begin());
-            vector<int> right(v.begin() + i, v.end());
-            int diff = std::abs(left[0] - right[0]);
+            vector<int> left(v.begin(), v.end());
+            vector<int> right({});
+            int diff = abs((int)left[0] - (int)right[0]);
             if(diff < min_diff) {
                 min_diff = diff;
                 pos = i;
@@ -20,7 +20,7 @@ vector<vector<int>> cutVector(vector<int> v) {
         } else {
             vector<int> left(v.begin(), v.begin() + i);
             vector<int> right(v.begin() + i, v.end());
-            int diff = std::abs(left[0] - right[0]);
+            int diff = abs((int)left[0] - (int)right[0]);
             if(diff < min_diff) {
                 min_diff = diff;
                 pos = i;
