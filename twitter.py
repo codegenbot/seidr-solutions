2196e781c471db7b799b1c56e8909ef04c210cf9
beep boop <githubbot@vadim.me>
2024-07-17 06:58:45 +0200

    Solution 96 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/eqhcjrai Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db76a9b2-f4fe-4fdb-9082-9b017e305 30a.py", line 8     return f"Your tweet has {len(tweet.encode('utf-8','ignore').decode().replace (\" \", \"\""))} characters"              ^ SyntaxError: unmatched ')'

--- twitter.py
+++ twitter.py
@@ -1,7 +1,1 @@
-def validate_tweet(tweet):
-    if len(tweet.encode('utf-8','ignore').decode().replace(" ", "")) > 140:
-        return "Too many characters"
-    elif tweet == "":
-        return "You didn't type anything"
-    else:
-        return f"Your tweet has {len(tweet.encode('utf-8', 'ignore').decode().replace(" ", ""))} characters"
+return f"Your tweet has {len(tweet.replace(" ", ""))} characters"
