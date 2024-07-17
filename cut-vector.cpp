736a46d16d9d97ba9864d57b75675376bb5c4533
beep boop <githubbot@vadim.me>
2024-07-17 03:24:59 +0200

    Solution 33 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,52 +1,53 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> &v) {
-    int n = v.size();
-    pair<vector<int>, vector<int>> res;
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
+    int n = vec.size();
+    int minDiff = INT_MAX;
+    int index = -1;
     
     for (int i = 0; i < n; i++) {
-        if (i == n - 1 || v[i] != v[i + 1]) {
-            if (i > 0 && v[i] == v[0])
-                res.first = vector<int>(v.begin(), v.begin() + i);
-            else
-                res.first = vector<int>(v.begin(), v.begin() + i);
-
-            res.second = vector<int>(v.begin() + i, v.end());
-            return res;
+        int sumLeft = 0, sumRight = 0;
+        
+        for (int j = 0; j < i; j++)
+            sumLeft += vec[j];
+        for (int j = i + 1; j < n; j++)
+            sumRight += vec[j];
+        
+        if (sumLeft == sumRight) {
+            return {{vec[0], vec[0], vec.begin() + 1, vec.end()}};
+        }
+        
+        int diff = abs(sumLeft - sumRight);
+        if (diff < minDiff) {
+            minDiff = diff;
+            index = i;
         }
     }
-
-    // If the input is a single element, just return it as both subvectors
-    if (n > 0) {
-        res.first = vector<int>(v.begin(), v.begin());
-        res.second = vector<int>(v.begin(), v.end());
-    } else {
-        res.first = vector<int>();
-        res.second = vector<int>();
-    }
     
-    return res;
+    vector<int> left(vec.begin(), vec.begin() + index);
+    vector<int> right(vec.begin() + index, vec.end());
+    return {left, right};
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> v(n);
+    vector<int> vec(n);
     for (int i = 0; i < n; i++) {
-        cin >> v[i];
+        cin >> vec[i];
     }
-
-    pair<vector<int>, vector<int>> res = cutVector(v);
-
-    cout << "First subvector: ";
-    for (int x : res.first) {
+    
+    pair<vector<int>, vector<int>> result = cutVector(vec);
+    
+    cout << "Left: ";
+    for (int x : result.first) {
         cout << x << " ";
     }
     cout << endl;
-
-    cout << "Second subvector: ";
-    for (int x : res.second) {
+    
+    cout << "Right: ";
+    for (int x : result.second) {
         cout << x << " ";
     }
     cout << endl;
