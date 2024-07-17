e7a0dbf1f8d24de5a04c149e6c41b645ecf7e44a
beep boop <githubbot@vadim.me>
2024-07-17 07:09:22 +0200

    Solution 29 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/82zahkh4 Prompt: 363dc4ce-2648-410a-86d7-f7f63282a1ab.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ 363dc4ce-2648-410a-86d7-f7f63282a1ab.cpp:26:19: error: ‘string’ was not declared  in this scope    26 | int getFrameValue(string s, int i) {       |                   ^~~~~~ 363dc4ce-2648-410a-86d7-f7f63282a1ab.cpp:26:29: error: expected primary-expressi on before ‘int’    26 | int getFrameValue(string s, int i) {       |                             ^~~ 363dc4ce-2648-410a-86d7-f7f63282a1ab.cpp:26:34: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | int getFrameValue(string s, int i) {       |                                  ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,33 +1,31 @@
-```cpp
 #include <string>
 
-int bowlingScore(std::string s) {
+int bowlingScore(string s) {
     int score = 0;
+    bool spare = false;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == 'X') {
             score += 10 + (i < 9 ? getFrameValue(s, i+1) : 0);
+            spare = false;
         } else if (s[i] == '/') {
             int a = s[i-1] - '0';
             int b = s[i+1] - '0';
-            score += a + b;
-            if (i < 9) {
-                score += getFrameValue(s, i+2);
-            }
+            score += a + b + (i < 9 ? getFrameValue(s, i+2) : 0);
+            spare = true;
         } else {
             int a = s[i] - '0';
-            if (s[i+1] == 'X') {
-                score += 10 + getFrameValue(s, i+2);
-            } else if (s[i+1] == '/') {
-                score += a + getFrameValue(s, i+2);
+            if (!spare) {
+                score += a;
             } else {
                 score += a;
+                spare = false;
             }
         }
     }
     return score;
 }
 
-int getFrameValue(std::string s, int i) {
+int getFrameValue(string s, int i) {
     while (s[i] != 'X' && s[i] != '/') {
         i++;
     }
