cbab8ba4be3e1476c9c1332462af079b935dc5b7
beep boop <githubbot@vadim.me>
2024-07-17 07:20:50 +0200

    Solution 66 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/y6992is1 Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = float*; _Size = long unsigned int; _Tp = float]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::size_type  = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::size_typ e = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator <float>]’ 4261e049-a3fc-460f-ab9d-cd1180749f11.cpp:22:25:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,9 +1,11 @@
-```cpp
-#include <initializer_list>
-#include <vector>
+```
+#include <iostream>
 #include <cmath>
+#include <vector>
 
-double calculateEuclideanDistance(const std::vector<float>& vector1, const std::vector<float>& vector2) {
+using namespace std;
+
+double calculateEuclideanDistance(const vector<float>& vector1, const vector<float>& vector2) {
     double sum = 0;
     if (vector1.size() != vector2.size()) {
         return -1; 
@@ -12,4 +14,31 @@ double calculateEuclideanDistance(const std::vector<float>& vector1, const std::
         sum += pow(vector2[i] - vector1[i], 2); 
     }
     return sqrt(sum);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<float> vec1; 
+    vec1.reserve(n); 
+
+    vector<float> vec2; 
+    vec2.reserve(n);
+
+    for(int i = 0; i < n; ++i){
+        float val;
+        cin >> val;
+        vec1.push_back(val);
+    }
+
+    for(int i = 0; i < n; ++i){
+        float val;
+        cin >> val;
+        vec2.push_back(val);
+    }
+
+    double distance = calculateEuclideanDistance(vec1, vec2);
+    cout << "The Euclidean Distance between two vectors is: " << distance << endl;
+
+    return 0;
 }
