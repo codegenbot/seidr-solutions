40b3996e7246c6ede54e0286999ccb66f7048c16
beep boop <githubbot@vadim.me>
2024-07-17 08:01:25 +0200

    Solution 84 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/qt95kmuo Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- gcd.cpp
+++ gcd.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+using namespace std;
 
 int gcd(int a, int b) {
     while (b != 0) {
@@ -10,33 +13,9 @@ int gcd(int a, int b) {
     return a;
 }
 
-std::vector<int> indices(std::string text, std::string target) {
-    std::vector<int> result;
-    for (int i = 0; i <= text.size() - target.size(); i++) {
-        bool found = true;
-        for (int j = 0; j < target.size(); j++) {
-            if (text[i + j] != target[j]) {
-                found = false;
-                break;
-            }
-        }
-        if (found) {
-            result.push_back(i);
-        }
-    }
-    return result;
-}
-
 int main() {
     int a = 12; 
     int b = 15;
     std::cout << "GCD: " << gcd(a, b) << std::endl;
-
-    std::string text = "HelloWorld";
-    std::string target = "Wor";
-    for (int i : indices(text, target)) {
-        std::cout << "Index " << i << ": " << text.substr(i, target.size()) << std::endl;
-    }
-
     return 0;
 }
