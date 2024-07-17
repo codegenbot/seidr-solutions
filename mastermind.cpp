c9e4b8ee75e278dde69bd8101633f170346cb8ef
beep boop <githubbot@vadim.me>
2024-07-17 05:47:23 +0200

    Solution 31 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8416du5e Prompt: d9aaa433-d8f1-4c0c-9cae-fbd8e14a4b05.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ d9aaa433-d8f1-4c0c-9cae-fbd8e14a4b05.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { d9aaa433-d8f1-4c0c-9cae-fbd8e14a4b05.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ d9aaa433-d8f1-4c0c-9cae-fbd8e14a4b05.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? d9aaa433-d8f1-4c0c-9cae-fbd8e14a4b05.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -12,8 +12,17 @@ int mastermind(std::string code, std::string guess) {
     }
 
     for (char c : guess) {
-        if (std::count(code.begin(), code.end(), c) > 0) {
+        bool foundInCode = false;
+        for (int i = 0; i < 4; i++) {
+            if (code[i] == c) {
+                foundInCode = true;
+                break;
+            }
+        }
+        if (!foundInCode) {
             white++;
+        } else {
+            black++;
         }
     }
 
