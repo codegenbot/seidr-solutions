ebf1176a98b206683c52899d82d3b977640f8e01
beep boop <githubbot@vadim.me>
2024-07-17 07:32:06 +0200

    Solution 44 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/e0lg0k4d Prompt: /usr/include/c++/11/iomanip:210:12: error: ‘class std::basic_ostream<char>’ has no member named ‘precision’   210 |       __os.precision(__f._M_n);       |       ~~~~~^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from d67c59a0-c50f-41b4-9c6a-6ad2133b5c83.cpp:2: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,28 +1,25 @@
-```
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <cmath>  
+#include <iomanip>  
 
-using namespace std;
+using namespace std;  
 
-int main() {
-    double startHeight, firstBounceHeight;
-    int numBounces;
-
-    cin >> startHeight >> firstBounceHeight >> numBounces;
-
-    double bouncinessIndex = firstBounceHeight / startHeight;
-
-    double totalDistance = 0.0;
-    double prevHeight = startHeight;
-
-    for (int i = 1; i <= numBounces; ++i) {
-        totalDistance += (2 * prevHeight * (1 - pow(bouncinessIndex, i)));
-        prevHeight *= bouncinessIndex;
-    }
-
-    cout << fixed << setprecision(5);
-    cout << totalDistance << endl;
-
-    return 0;
+int main() {  
+    double startHeight, firstBounceHeight;  
+    int numBounces;  
+  
+    cin >> startHeight >> firstBounceHeight >> numBounces;  
+  
+    double bouncinessIndex = firstBounceHeight / startHeight;  
+  
+    double totalDistance = 0.0;  
+  
+    for (int i = 1; i <= numBounces; ++i) {  
+        totalDistance += (2 * startHeight * (1 - pow(bouncinessIndex, i)));  
+    }  
+  
+    cout << std::fixed << std::setprecision(5);  
+    cout << totalDistance << endl;  
+  
+    return 0;  
 }
