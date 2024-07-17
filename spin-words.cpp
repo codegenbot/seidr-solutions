ed2dc93a9a5c32dae5d8e5001d64caed8ad126ab
beep boop <githubbot@vadim.me>
2024-07-17 06:17:50 +0200

    Solution 39 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xfeec785 Prompt: 9fa331a9-cdf2-46d3-b8dc-f456de7ea3a3.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string spinWords(string str) {       | ^~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-string spinWords(string str) {
+std::string spinWords(std::string str) {
     string result = "";
     int i = 0;
     while (i < str.length()) {
@@ -14,7 +14,7 @@ string spinWords(string str) {
             while (j < str.length() && str[j] != ' ') {
                 j++;
             }
-            string word = str.substr(i, j - i);
+            std::string word = str.substr(i, j - i);
             if (word.length() >= 5) {
                 string revWord = "";
                 for (int k = word.length() - 1; k >= 0; k--) {
