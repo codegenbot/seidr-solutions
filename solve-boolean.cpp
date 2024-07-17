c6f4208331d06ee783c634d381c1ca4fd9dd9213
beep boop <githubbot@vadim.me>
2024-07-17 05:13:56 +0200

    Solution 95 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xwifqaul Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,46 +1,26 @@
-#include <iostream>
-using namespace std;
+Here is the solution:
 
-bool evaluateBooleanExpression(string expression) {
-    bool res = true;
-    int i = 0, j = 0;
-    while (i < expression.size()) {
-        if (expression[i] == 't') {
-            res &= true;
-            i++;
-        } else if (expression[i] == 'f') {
-            res &= false;
-            i++;
-        } else if (expression[i] == '|') {
-            j = i + 1;
-            while (j < expression.size() && expression[j] != '&') {
-                j++;
-            }
-            string subexpr = expression.substr(i, j - i);
-            res &= (subexpr == "t" || subexpr == "f");
-            i = j;
-        } else if (expression[i] == '&') {
-            j = i + 1;
-            while (j < expression.size() && expression[j] != '|') {
-                j++;
-            }
-            string subexpr = expression.substr(i, j - i);
-            res &= (subexpr == "t" && subexpr != "f");
-            i = j;
+string solveBoolean(string expression) {
+    stack<char> operators;
+    stack<bool> values;
+
+    for (char c : expression) {
+        if (c == 't' || c == 'T') {
+            values.push(true);
+        } else if (c == 'f' || c == 'F') {
+            values.push(false);
+        } else if (c == '&') {
+            bool right = values.top(); values.pop();
+            bool left = values.top(); values.pop();
+
+            values.push(left && right);
+        } else if (c == '|') {
+            bool right = values.top(); values.pop();
+            bool left = values.top(); values.pop();
+
+            values.push(left || right);
         }
     }
-    return res;
-}
 
-int main() {
-    string expression;
-    cout << "Enter a Boolean expression: ";
-    cin >> expression;
-    bool result = evaluateBooleanExpression(expression);
-    if (result) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
-    return 0;
+    return values.top() ? "True" : "False";
 }
