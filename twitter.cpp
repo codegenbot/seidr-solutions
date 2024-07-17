3261f2802b4135e0b6c205bb077179444e5ea9d2
beep boop <githubbot@vadim.me>
2024-07-17 17:37:10 +0200

    Solution 92 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/04pxk33s Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <string>
 #include <limits>
+using namespace std;
 
 int main() {
-    std::string tweet;
-    std::cout << "Enter a tweet: ";
+    string tweet;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+    cout << "Enter a tweet: ";
     
-    std::getline(std::cin, tweet);
+    getline(cin, tweet);
     
     if (tweet.length() > 140) {
-        std::cout << "Too many characters" << std::endl;
+        cout << "Too many characters" << endl;
     } else if (tweet.empty()) {
-        std::cout << "You didn't type anything" << std::endl;
+        cout << "You didn't type anything" << endl;
     } else {
-        std::cout << "Your tweet has " << tweet.length() << " characters" << std::endl;
+        cout << "Your tweet has " << tweet.length() << " characters" << endl;
     }
     
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
     return 0;
 }
