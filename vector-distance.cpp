ba9306cd0a99e13504d38683a491cd0dcd847626
beep boop <githubbot@vadim.me>
2024-07-17 07:37:43 +0200

    Solution 80 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/y6992is1 Prompt: it must return 403.2133951219185 for input 16 57.44301368491679 16.742719062832492 -74.61965857106088 -22.258861274437976 47.79647406590382 -32.20428381983302 -65.08934683973966 27.316919328500774 91.2185994843515 87.14178519030159 -23.538327699114745 80.67579056885924 39.8707884917965 -98.04752972780524 43.97043003043066 -89.87342736723099 16 52.535781710654504 -29.20229114209529 -9.48178364940793 61.72037068052089 -87.60109184420004 8.21330069296313 38.13007064261569 -91.61814782853776 -40.66391286840037 69.68116290329556 87.65531266355663 -80.92843694483736 -54.98459934451658 34.87027160528609 93.383716191845 48.33113430292258, but it returns The Euclidean Distance between two vectors is: 350.132.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,44 +1,10 @@
-```c++
-#include <iostream>
-#include <cmath>
-#include <vector>
-
-using namespace std;
-
 double calculateEuclideanDistance(const vector<float>& vector1, const vector<float>& vector2) {
     double sum = 0;
     if (vector1.size() != vector2.size()) {
         return -1; 
     }
     for (int i = 0; i < vector1.size(); ++i) {
-        sum += pow((vector2[i] - vector1[i]), 2); 
+        sum += pow(vector2[i] - vector1[i], 2); 
     }
     return sqrt(sum);
-}
-
-int main(){
-    int n;
-    cin >> n;
-    vector<float> vec1; 
-    vec1.reserve(n); 
-
-    vector<float> vec2; 
-    vec2.reserve(n);
-
-    for(int i = 0; i < n; ++i){
-        float temp;
-        cin >> temp;
-        vec1.push_back(temp);
-    }
-
-    for(int i = 0; i < n; ++i){
-        float temp;
-        cin >> temp;
-        vec2.push_back(temp);
-    }
-
-    double distance = calculateEuclideanDistance(vec1, vec2);
-    cout << "The Euclidean Distance between two vectors is: " << distance << endl;
-
-    return 0;
 }
