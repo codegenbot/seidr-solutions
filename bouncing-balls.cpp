5963ab1c1284fc76893ee40a311997d8aaee1d8e
beep boop <githubbot@vadim.me>
2024-07-17 05:12:00 +0200

    Solution 10 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/tha9k574 Prompt: it must return 265.11172549905456 for input 82.24846341633294 43.30574160094839 13, but it returns -7756.49346.

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,25 +1,5 @@
-#include <iostream>
-#include <cmath>
-#include <iomanip>
-
-using namespace std;
-
-int main() {
-    double startHeight, firstBounce;
-    int numBounces;
-
-    cin >> startHeight >> firstBounce >> numBounces;
-
-    double bouncinessIndex = firstBounce / startHeight;
-
-    double totalDistance = 0.0;
-    for (int i = 1; i <= numBounces; ++i) {
-        totalDistance += pow(2, -bouncinessIndex) * startHeight;
-        startHeight *= 2 * (1 - bouncinessIndex);
-    }
-
-    cout << fixed << setprecision(4);
-    cout << totalDistance << endl;
-
-    return 0;
+```
+double totalDistance = 0.0;
+for (int i = 1; i <= numBounces; ++i) {
+    totalDistance += (pow(2, i/2) + pow(2, (i-1)/2)) * (bouncinessIndex - 1);
 }
