1dae76a5d82c1de33cb2ae187c2798512808c75a
beep boop <githubbot@vadim.me>
2024-07-17 05:43:43 +0200

    Solution 26 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8416du5e Prompt: b9f5a5b1-4c98-40a2-b390-1ee2c085c7a8.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ b9f5a5b1-4c98-40a2-b390-1ee2c085c7a8.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { b9f5a5b1-4c98-40a2-b390-1ee2c085c7a8.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ b9f5a5b1-4c98-40a2-b390-1ee2c085c7a8.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? b9f5a5b1-4c98-40a2-b390-1ee2c085c7a8.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,16 +1,21 @@
-```cpp
-int mastermind(const std::string& code, const std::string& guess) {
+#include <string>
+
+int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
             black++;
+        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {
+            white++;
         }
     }
 
     for (char c : guess) {
-        if (std::count(code.begin(), code.end(), c) > 0) {
+        if (std::count(code.begin(), code.end(), c) > 0 && guess.find(std::string(1, c)) != std::string::npos) {
+            black++;
+        } else if (std::count(code.begin(), code.end(), c) > 0) {
             white++;
         }
     }
