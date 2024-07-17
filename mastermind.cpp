e191b25bf994c01eacc11c5e63d48969edda99d6
beep boop <githubbot@vadim.me>
2024-07-17 05:30:16 +0200

    Solution 10 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8416du5e Prompt:       |                                 ^~~~~~ In file included from ab239d4a-a1aa-4de1-95c0-9cac9aad76e3.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ab239d4a-a1aa-4de1-95c0-9cac9aad76e3.cpp:3:29: error: ‘string’ was not declared in this scope     3 | int mastermind(string code, string guess) {       |                             ^~~~~~ ab239d4a-a1aa-4de1-95c0-9cac9aad76e3.cpp:3:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from ab239d4a-a1aa-4de1-95c0-9cac9aad76e3.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ab239d4a-a1aa-4de1-95c0-9cac9aad76e3.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ab239d4a-a1aa-4de1-95c0-9cac9aad76e3.cpp:3:41: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,18 @@
+```cpp
 #include <string>
+#include <algorithm>
 
-int mastermind(string code, string guess) {
+int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
             black++;
+        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {
+            white++;
         }
     }
 
-    for (char c : code) {
-        int count = 0;
-        for (char d : guess) {
-            if (c == d && c != ' ') {
-                count++;
-            }
-        }
-        white += min(count, 1);
-    }
-
-    return black;
+    return white;
 }
