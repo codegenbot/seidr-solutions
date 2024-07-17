ad7dcb0b41c02839433ea47ffade826e98ef8186
beep boop <githubbot@vadim.me>
2024-07-17 06:57:49 +0200

    Solution 1 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/zib6hjbk Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,20 +1,24 @@
-Here is the Python solution:
-
-def bowling(score):
-    score = score.replace('/', '')
-    total_score = 0
+```
+def bowling_score(game):
+    score = 0
     frame = 1
-    for i in range(0, len(score), 2):
-        if score[i] == 'X':
-            total_score += 10 + (10 - int(score[i+1])) if i < len(score) - 1 and score[i+1] != 'X' else 10
+    while frame <= 10:
+        if game[2*frame-2:2*frame] == 'X':
+            score += 30
             frame += 1
-        elif score[i] == '-':
-            first_roll = 10 - int(score[i-1])
-            total_score += first_roll + int(score[i+1]) if i < len(score) - 2 and score[i+2] != 'X' else first_roll
+        elif game[2*frame-2] == '/':
+            first_roll = int(game[2*frame-3])
+            second_roll = 10 - first_roll
+            score += first_roll + second_roll
             frame += 1
         else:
-            first_roll = int(score[i])
-            second_roll = int(score[i+1])
-            total_score += first_roll + second_roll
+            first_roll = int(game[2*frame-2:2*frame])
+            second_roll = int(game[2*frame-1])
+            if first_roll + second_roll == 10:
+                score += 10
+            elif first_roll == 10:
+                score += first_roll + second_roll
+            else:
+                score += first_roll + second_roll
             frame += 1
-    return total_score
+    return score
