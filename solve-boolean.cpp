03421bd83bf94db85be81a8f5da5f1af89c50d48
beep boop <githubbot@vadim.me>
2024-07-17 05:13:39 +0200

    Solution 94 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xwifqaul Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,21 +1,46 @@
-#include <string>
+#include <iostream>
+using namespace std;
 
-bool evaluateBooleanExpression(std::string expression) {
-    bool result = true;
-    int i = 0;
-    
-    while (i < expression.length()) {
-        if (expression[i] == '&') {
-            result &= (expression[i + 1] == 'T');
-            i += 2;
-        } else if (expression[i] == '|') {
-            result |= (expression[i + 1] == 'T');
-            i += 2;
-        } else {
-            result = expression[i] == 'T';
+bool evaluateBooleanExpression(string expression) {
+    bool res = true;
+    int i = 0, j = 0;
+    while (i < expression.size()) {
+        if (expression[i] == 't') {
+            res &= true;
             i++;
+        } else if (expression[i] == 'f') {
+            res &= false;
+            i++;
+        } else if (expression[i] == '|') {
+            j = i + 1;
+            while (j < expression.size() && expression[j] != '&') {
+                j++;
+            }
+            string subexpr = expression.substr(i, j - i);
+            res &= (subexpr == "t" || subexpr == "f");
+            i = j;
+        } else if (expression[i] == '&') {
+            j = i + 1;
+            while (j < expression.size() && expression[j] != '|') {
+                j++;
+            }
+            string subexpr = expression.substr(i, j - i);
+            res &= (subexpr == "t" && subexpr != "f");
+            i = j;
         }
     }
-    
-    return result;
+    return res;
+}
+
+int main() {
+    string expression;
+    cout << "Enter a Boolean expression: ";
+    cin >> expression;
+    bool result = evaluateBooleanExpression(expression);
+    if (result) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+    return 0;
 }
