5325fe8a31285c267077c46843040f2661bf0209
beep boop <githubbot@vadim.me>
2024-07-17 07:25:19 +0200

    Solution 57 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/82zahkh4 Prompt: f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp: In function ‘int bowlingScore(const in t&)’: f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:8:19: error: ‘begin’ was not declared i n this scope; did you mean ‘std::begin’?     8 |     for (char c : frames) {       |                   ^~~~~~       |                   std::begin In file included from /usr/include/c++/11/string:54,                  from f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:1: /usr/include/c++/11/bits/range_access.h:108:37: note: ‘std::begin’ declared here   108 |   template<typename _Tp> const _Tp* begin(const valarray<_Tp>&) noexcept ;       |                                     ^~~~~ f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:8:19: error: ‘end’ was not declared in this scope; did you mean ‘std::end’?     8 |     for (char c : frames) {       |                   ^~~~~~       |                   std::end In file included from /usr/include/c++/11/string:54,                  from f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:1: /usr/include/c++/11/bits/range_access.h:110:37: note: ‘std::end’ declared here   110 |   template<typename _Tp> const _Tp* end(const valarray<_Tp>&) noexcept;       |                                     ^~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,3 +1,5 @@
+#include <string>
+#include <cctype>
 using namespace std;
 
 int bowlingScore(const string& frames) {
@@ -11,7 +13,7 @@ int bowlingScore(const string& frames) {
         } else if (frames[i] == '/') {
             score += (10 - currentRolls);
             currentRolls = 0;
-        } else if (isdigit(frames[i])) {
+        } else if (std::isdigit(frames[i])) {
             int roll = frames[i] - '0';
             currentRolls++;
             if (currentRolls == 2) {
