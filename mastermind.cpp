debd84a9732a0c0edfeafce2c6e848d1aa3efea2
beep boop <githubbot@vadim.me>
2024-07-17 06:43:37 +0200

    Solution 96 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8416du5e Prompt:     8 |     std::map<char, int> placementMap;       |              ^~~~ 34c8d28f-9a9a-4a22-b7bd-171951974672.cpp:13:25: error: ‘count’ is not a member o f ‘std’    13 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                         ^~~~~ 34c8d28f-9a9a-4a22-b7bd-171951974672.cpp:15:13: error: ‘colorMap’ was not declar ed in this scope    15 |             colorMap[code[i]]++;       |             ^~~~~~~~ 34c8d28f-9a9a-4a22-b7bd-171951974672.cpp:19:23: error: ‘colorMap’ was not declar ed in this scope    19 |     for (auto& pair : colorMap) {       |                       ^~~~~~~~ 34c8d28f-9a9a-4a22-b7bd-171951974672.cpp:20:9: error: ‘placementMap’ was not dec lared in this scope    20 |         placementMap[pair.first] = pair.second;       |         ^~~~~~~~~~~~ 34c8d28f-9a9a-4a22-b7bd-171951974672.cpp:24:29: error: ‘placementMap’ was not de clared in this scope    24 |     for (const auto& pair : placementMap) {       |                             ^~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,24 +1,25 @@
+#include <string>
+#include <map>
+#include <algorithm>
+
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    std::map<char, int> colorMap; 
-    std::map<char, int> placementMap; 
+    std::map<char, int> colorMap = {};
+    std::map<char, int> placementMap = {};
 
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) { 
             black++;
-        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) { 
+        } else if (std::count(std::begin(guess), std::end(guess), code[i]) > 0) { 
             white++;
             colorMap[code[i]]++; 
         }
     }
 
-    for (int i = 0; i < 6; i++) {
-        if(colorMap.count(i+'A') > 0) {
-            placementMap[i+'A'] = colorMap[i+'A'];
-            colorMap.erase(i+'A');
-        }
+    for (auto& pair : colorMap) {
+        placementMap[pair.first] = pair.second; 
     }
 
     int blackCount = 0;
