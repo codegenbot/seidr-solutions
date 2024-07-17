997ef2a321a15c51415addea810c89c5539bed08
beep boop <githubbot@vadim.me>
2024-07-17 07:14:29 +0200

    Solution 66 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/cix2qqmx Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8f7c9ad-c911-4b9e-a9ec-040f959e8 6e5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- shopping-list.py
+++ shopping-list.py
@@ -1,10 +1,11 @@
+```
 prices_and_discounts = []
 while True:
     try:
         temp = list(map(float, input().strip().split()))
         prices = [temp[0]]
         discounts = [(100 - temp[1]) / 100.0]
-        prices_and_discounts.extend(list(zip(prices, discounts)))
+        prices_and_discounts.extend(zip(prices, discounts))
         if len(temp) == 2: break
     except ValueError:
         print("Invalid input. Please provide price and discount as two separate float values.")
