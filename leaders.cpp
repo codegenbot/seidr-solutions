de0af113de9c3ac379a96da74bb5c363b0564ee9
beep boop <githubbot@vadim.me>
2024-07-17 05:54:39 +0200

    Solution 47 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/wcd6zq0c Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from b5e5b3b8-199c-458d-9168-29f7612250e0.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- leaders.cpp
+++ leaders.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <algorithm>
 #include <iostream>
 using namespace std;
@@ -17,18 +16,6 @@ vector<int> leaders(vector<int> v) {
 
     reverse(res.begin(), res.end());
     return res;
-}
-
-vector<int> leaders(vector<int> a) {
-    int n = a.size();
-    vector<int> ans;
-    ans.push_back(a[n-1]);
-    for(int i=n-2; i>=0; i--){
-        if(a[i]>=ans.back())
-            ans.push_back(a[i]);
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
 }
 
 int main() {
