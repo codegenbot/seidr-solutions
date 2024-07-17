70436ac2cee6afb68f26f251df114af2c8213153
beep boop <githubbot@vadim.me>
2024-07-17 04:57:53 +0200

    Solution 65 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xwifqaul Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,39 +1,17 @@
-#include <string>
-using namespace std;
-
-bool solveBoolean(string expr) {
-    bool result = true;
-    int i = 0;
-    while (i < expr.length()) {
-        if (expr[i] == 't') {
-            result &= true;
-        } else if (expr[i] == 'f') {
-            result &= false;
-        } else if (expr[i] == '&') {
-            i++; // skip &
-            bool temp = true;
-            while (i < expr.length() && expr[i] != '|') {
-                if (expr[i] == 't') {
-                    temp &= true;
-                } else if (expr[i] == 'f') {
-                    temp &= false;
-                }
-                i++;
-            }
-            result &= temp;
-        } else if (expr[i] == '|') {
-            i++; // skip |
-            bool temp = false;
-            while (i < expr.length() && expr[i] != '&') {
-                if (expr[i] == 't') {
-                    temp |= true;
-                } else if (expr[i] == 'f') {
-                    temp |= false;
-                }
-                i++;
-            }
-            result |= temp;
+string solveBoolean(string input) {
+    stack<char> s;
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] == '&') {
+            char a = s.top(); s.pop();
+            char b = s.top(); s.pop();
+            s.push((a == 'T' && b == 'T') ? 'T' : 'F');
+        } else if (input[i] == '|') {
+            char a = s.top(); s.pop();
+            char b = s.top(); s.pop();
+            s.push((a == 'T' || b == 'T') ? 'T' : 'F');
+        } else {
+            s.push(input[i]);
         }
     }
-    return result;
+    return (s.top() == 'T') ? "True" : "False";
 }
