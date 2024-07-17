26cd3d643cd20c9124c4341bc883acd9f199b8f8
beep boop <githubbot@vadim.me>
2024-07-17 02:59:38 +0200

    Solution 55 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/a3mny2dw Prompt: it must return True for input f|f&t|t|t&t|f, but it returns Enter a Boolean expression: terminate called after throwing an instance of 'std: :out_of_range'   what():  basic_string::substr: __pos (which is 14) > this->size() (which is 13 ).

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -20,7 +20,7 @@ bool evaluateBooleanExpression(string expression) {
             }
             string rightSubExpression = expression.substr(j + 1, k - j - 1);
             bool right = (rightSubExpression == "t") ? true : false;
-            result &= left && right;
+            result &= (left && right);
         } else if (expression[i] == '|') {
             int j = i + 1;
             while (j <= expression.length() && expression[j] != '|') {
@@ -34,9 +34,13 @@ bool evaluateBooleanExpression(string expression) {
             }
             string rightSubExpression = expression.substr(j + 1, k - j - 1);
             bool right = (rightSubExpression == "t") ? true : false;
-            result |= left || right;
+            result |= (left || right);
         } else if (expression[i] == 'T' || expression[i] == 't') {
-            result = (expression[i] == 'T');
+            result = true;
+            return result;
+        } else if (expression[i] == 'F' || expression[i] == 'f') {
+            result = false;
+            return result;
         }
     }
     return result;
