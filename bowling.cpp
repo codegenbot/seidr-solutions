6fbfe19e1d1decc650798c31f495d63a24092a35
beep boop <githubbot@vadim.me>
2024-07-17 07:27:36 +0200

    Solution 60 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/82zahkh4 Prompt: f021f575-4763-4df2-9d8a-8a9fa5a644b4.cpp: In function ‘int bowlingScore(const in t&)’: f021f575-4763-4df2-9d8a-8a9fa5a644b4.cpp:8:19: error: ‘begin’ was not declared i n this scope; did you mean ‘std::begin’?     8 |     for (char c : frames) {       |                   ^~~~~~       |                   std::begin In file included from /usr/include/c++/11/string:54,                  from f021f575-4763-4df2-9d8a-8a9fa5a644b4.cpp:1: /usr/include/c++/11/bits/range_access.h:108:37: note: ‘std::begin’ declared here   108 |   template<typename _Tp> const _Tp* begin(const valarray<_Tp>&) noexcept ;       |                                     ^~~~~ f021f575-4763-4df2-9d8a-8a9fa5a644b4.cpp:8:19: error: ‘end’ was not declared in this scope; did you mean ‘std::end’?     8 |     for (char c : frames) {       |                   ^~~~~~       |                   std::end In file included from /usr/include/c++/11/string:54,                  from f021f575-4763-4df2-9d8a-8a9fa5a644b4.cpp:1: /usr/include/c++/11/bits/range_access.h:110:37: note: ‘std::end’ declared here   110 |   template<typename _Tp> const _Tp* end(const valarray<_Tp>&) noexcept;       |                                     ^~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,22 +1,22 @@
-using namespace std;
+#include <string>
+#include <cctype>
 
-int bowlingScore(const string& frames) {
+int bowlingScore(const std::string& frames) {
     int score = 0;
     int currentRolls = 0;
 
-    for (unsigned int i = 0; i < frames.length(); i++) {
-        if (frames[i] == 'X') {
+    for (char c : frames) { 
+        if (c == 'X') {
             score += 30;
             currentRolls = 2;
-        } else if (frames[i] == '/') {
+        } else if (c == '/') {
             score += (10 - currentRolls);
             currentRolls = 0;
-        } else if (isdigit(frames[i])) {
-            int roll = frames[i] - '0';
+        } else if (std::isdigit(c)) { 
+            int roll = c - '0';
             currentRolls++;
             if (currentRolls == 2) {
-                score += 1 * roll + 1 * (10 - roll);
-                currentRolls = 0;
+                score += roll * 2;
             }
         }
     }
