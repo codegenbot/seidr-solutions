a353f33b3219592fb468e3731541b4d0136ff1d8
beep boop <githubbot@vadim.me>
2024-07-17 05:52:58 +0200

    Solution 38 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/lo60cofz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90ac9319-97a5-4b9d-8e4c-e59bf2ad8 7ed.py", line 6     print("{:.1f}".format(sum([float("".join(map(str, map(lambda x: round(x * (1  - d/100))), p)))) for p, d in zip(prices, discounts)])))                  ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- shopping-list.py
+++ shopping-list.py
@@ -1,5 +1,1 @@
-def shopping_list():
-    n = int(input())
-    prices = list(map(float, input().split()))
-    discounts = list(map(float, input().split()))
-    print("{:.1f}".format(sum([round(x * (1 - d/100)) for p, d in zip(prices, discounts)])))
+print("{:.1f}".format(sum((float("".join(map(str, map(lambda x: round(x * (1 - d/100))), p))))) for p, d in zip(prices, discounts))))
