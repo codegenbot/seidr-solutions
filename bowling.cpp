122b0ee285afd4a8c6d0db62c224a2a755323714
beep boop <githubbot@vadim.me>
2024-07-17 07:22:59 +0200

    Solution 52 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/82zahkh4 Prompt: f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp: In function ‘int bowlingScore(const in t&)’: f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:8:19: error: ‘begin’ was not declared i n this scope; did you mean ‘std::begin’?     8 |     for (char c : frames) {       |                   ^~~~~~       |                   std::begin In file included from /usr/include/c++/11/string:54,                  from f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:1: /usr/include/c++/11/bits/range_access.h:108:37: note: ‘std::begin’ declared here   108 |   template<typename _Tp> const _Tp* begin(const valarray<_Tp>&) noexcept ;       |                                     ^~~~~ f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:8:19: error: ‘end’ was not declared in this scope; did you mean ‘std::end’?     8 |     for (char c : frames) {       |                   ^~~~~~       |                   std::end In file included from /usr/include/c++/11/string:54,                  from f06320fa-1f01-4742-b9b9-1b2ad8d5534f.cpp:1: /usr/include/c++/11/bits/range_access.h:110:37: note: ‘std::end’ declared here   110 |   template<typename _Tp> const _Tp* end(const valarray<_Tp>&) noexcept;       |                                     ^~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,25 +1,24 @@
-```cpp
-#include <string>
+using namespace std;
 
-int bowlingScore(const std::string& frames) {
+int bowlingScore(const string& frames) {
     int score = 0;
     int currentRolls = 0;
 
-    for (char c : frames) {
+    for (auto c : frames) {
         if (c == 'X') {
             score += 30;
             currentRolls = 2;
         } else if (c == '/') {
             score += (10 - currentRolls);
             currentRolls = 0;
-        } else if (std::isdigit(c)) {
+        } else if (isdigit(c)) {
             int roll = c - '0';
             currentRolls++;
             if (currentRolls == 2) {
                 score += 1 * roll + 1 * (10 - roll);
+                currentRolls = 0;
             }
         }
     }
 
     return score;
-}
