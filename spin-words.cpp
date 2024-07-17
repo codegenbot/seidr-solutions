898ba1fbffa73d22c5225122c080ca34ce92e6e4
beep boop <githubbot@vadim.me>
2024-07-17 06:47:53 +0200

    Solution 75 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/k4744atr Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -2,39 +2,35 @@
 #include <iostream>
 #include <string>
 
-std::string spinWords(const std::string& input) {
-    std::string output = "";
-    for (const auto& word : input.split(" ")) {
-        if (word.length() >= 5) {
-            for (int i = word.length() - 1; i >= 0; --i) {
-                output += word[i];
-            }
+using namespace std;
+
+string spinWords(string str) {
+    string result = "";
+    string word = "";
+
+    for (char c : str) {
+        if (c == ' ') {
+            if (word.length() >= 5)
+                result += newReverse(word) + " ";
+            else
+                result += word + " ";
+            word = "";
         } else {
-            output += word + " ";
+            word += c;
         }
     }
-    return output;
+
+    if (word.length() >= 5)
+        result += newReverse(word);
+    else
+        result += word;
+
+    return result;
 }
 
-std::string std::string::split(const char* delimiter) const {
-    std::vector<std::string> words;
-    size_t pos = 0, pos2 = this->find(delimiter);
-    while (pos2 != std::string::npos) {
-        words.push_back(std::string(this->substr(pos, pos2 - pos)));
-        pos = pos2 + delimiter->length();
-        pos2 = this->find(delimiter, pos);
-    }
-    words.push_back(std::string(this->substr(pos)));
-    return join(words, " ");
-}
-
-std::string std::string::join(const std::vector<std::string>& words, const char* delimiter) {
-    std::string output;
-    for (const auto& word : words) {
-        output += word + delimiter;
-    }
-    if (!output.empty()) {
-        output.pop_back();
-    }
-    return output;
+string newReverse(string str) {
+    string reversedStr = "";
+    for (int i = str.length() - 1; i >= 0; --i)
+        reversedStr += str[i];
+    return reversedStr;
 }
