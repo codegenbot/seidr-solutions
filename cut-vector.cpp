65990540f3da9239c766cf36d0a06ca7e1af06d9
beep boop <githubbot@vadim.me>
2024-07-17 03:45:51 +0200

    Solution 79 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,49 +1,25 @@
 #include <vector>
-#include <iostream>
-
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int minDiff = INT_MAX;
-    int cutIndex = 0;
-    
-    for (int i = 1; i <= v.size() - 1; i++) {
-        int leftSum = 0, rightSum = 0;
-        for (int j = 0; j < i; j++) {
-            leftSum += v[j];
-        }
-        for (int j = i; j < v.size(); j++) {
-            rightSum += v[j];
-        }
-        
-        int diff = abs(leftSum - rightSum);
-        if (diff <= minDiff) {
-            minDiff = diff;
-            cutIndex = i;
-        }
-    }
-    
-    vector<int> leftVec(v.begin(), v.begin() + cutIndex);
-    vector<int> rightVec(v.begin() + cutIndex, v.end());
-    
-    return {leftVec, rightVec};
-}
+vector<int> cutVector(vector<int> v) {
+    int n = v.size();
+    int min_diff = INT_MAX;
+    int split_idx = 0;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (auto &x : v) {
-        cin >> x;
+    for (int i = 1; i < n; ++i) {
+        int left_sum = 0, right_sum = 0;
+        for (int j = 0; j < i; ++j)
+            left_sum += v[j];
+        for (int j = i; j < n; ++j)
+            right_sum += v[j];
+
+        if (abs(left_sum - right_sum) < min_diff) {
+            min_diff = abs(left_sum - right_sum);
+            split_idx = i;
+        }
     }
-    pair<vector<int>, vector<int>> res = cutVector(v);
-    cout << "0\n";
-    for (const auto &x : res.first) {
-        cout << x << " ";
-    }
-    cout << "\n0\n";
-    for (const auto &x : res.second) {
-        cout << x << " ";
-    }
-    return 0;
+
+    vector<int> left(v.begin(), v.begin() + split_idx);
+    vector<int> right(v.begin() + split_idx, v.end());
+    return {left, right};
 }
