34d34c807f83d0f01f35619cd2cde3cf29dc3dac
beep boop <githubbot@vadim.me>
2024-07-17 02:18:42 +0200

    Solution 50 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/1lxuu31t Prompt: cca10240-e55c-4280-8f9e-57ea8c67cf06.cpp: In function ‘std::pair<int, int> findP air(std::vector<int>&, int)’: cca10240-e55c-4280-8f9e-57ea8c67cf06.cpp:5:5: error: ‘unordered_map’ was not dec lared in this scope     5 |     unordered_map<int, int> numMap;       |     ^~~~~~~~~~~~~ cca10240-e55c-4280-8f9e-57ea8c67cf06.cpp:2:1: note: ‘std::unordered_map’ is defi ned in header ‘<unordered_map>’; did you forget to ‘#include <unordered_map>’?     1 | #include <vector>   +++ |+#include <unordered_map>     2 | using namespace std; cca10240-e55c-4280-8f9e-57ea8c67cf06.cpp:5:19: error: expected primary-expressio n before ‘int’     5 |     unordered_map<int, int> numMap;       |                   ^~~ cca10240-e55c-4280-8f9e-57ea8c67cf06.cpp:8:13: error: ‘numMap’ was not declared in this scope     8 |         if (numMap.find(complement) != numMap.end()) {       |             ^~~~~~ cca10240-e55c-4280-8f9e-57ea8c67cf06.cpp:11:9: error: ‘numMap’ was not declared in this scope    11 |         numMap[nums[i]] = i;       |         ^~~~~~

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
 
 pair<int, int> findPair(vector<int>& nums, int target) {
-    unordered_map<int, int> numMap;
+    std::unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
         if (numMap.find(complement) != numMap.end()) {
@@ -14,18 +13,4 @@ pair<int, int> findPair(vector<int>& nums, int target) {
         numMap[nums[i]] = i;
     }
     return {-1, -1};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> numbers(n);
-    for (int i = 0; i < n; i++) {
-        cin >> numbers[i];
-    }
-    int target;
-    cin >> target;
-    pair<int, int> result = findPair(numbers, target);
-    cout << result.first << " " << result.second << endl;
-    return 0;
 }
