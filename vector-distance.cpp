a8aec54ff427cf018446c779d9a99e3ed6f5ae3a
beep boop <githubbot@vadim.me>
2024-07-17 07:08:55 +0200

    Solution 50 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/y6992is1 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d254654e-430b-465a-8588-efbdf5d42ecd.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,5 +1,6 @@
-#include <cmath>
+```cpp
 #include <vector>
+#include <cmath>
 
 double calculateEuclideanDistance(const std::vector<float>& vector1, const std::vector<float>& vector2) {
     double sum = 0;
@@ -7,14 +8,7 @@ double calculateEuclideanDistance(const std::vector<float>& vector1, const std::
         return -1; 
     }
     for (int i = 0; i < vector1.size(); ++i) {
-        sum += pow(vector1[i] - vector2[i], 2);
+        sum += pow(vector2[i] - vector1[i], 2); 
     }
     return sqrt(sum);
-}
-
-int main() {
-    std::vector<float> vec1 = {1.0, 2.0, 3.0};
-    std::vector<float> vec2 = {4.0, 5.0, 6.0};
-    double dist = calculateEuclideanDistance(vec1, vec2);
-    return 0;
 }
