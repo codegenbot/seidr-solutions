ea35dc7e489d0f78c358641035af3b8cf34e7517
beep boop <githubbot@vadim.me>
2024-07-17 05:18:42 +0200

    Solution 24 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/d5tswv8n Prompt: c17261ce-9b0d-4390-b7de-753d5ca59a1f.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: c17261ce-9b0d-4390-b7de-753d5ca59a1f.cpp:14:24: error: ‘abs’ was not declared in  this scope    14 |             int diff = abs((int)left[0] - (int)right[0]);       |                        ^~~ c17261ce-9b0d-4390-b7de-753d5ca59a1f.cpp:22:24: error: ‘abs’ was not declared in  this scope    22 |             int diff = abs((int)left[0] - (int)right[0]);       |                        ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -12,7 +12,7 @@ vector<vector<int>> cutVector(vector<int> v) {
         if(i == v.size()) {
             vector<int> left(v.begin(), v.begin());
             vector<int> right(v.begin() + i, v.end());
-            int diff = abs((int)left[0] - (int)right[0]);
+            int diff = std::abs(left[0] - right[0]);
             if(diff < min_diff) {
                 min_diff = diff;
                 pos = i;
@@ -20,7 +20,7 @@ vector<vector<int>> cutVector(vector<int> v) {
         } else {
             vector<int> left(v.begin(), v.begin() + i);
             vector<int> right(v.begin() + i, v.end());
-            int diff = abs((int)left[0] - (int)right[0]);
+            int diff = std::abs(left[0] - right[0]);
             if(diff < min_diff) {
                 min_diff = diff;
                 pos = i;
