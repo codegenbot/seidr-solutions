f7d91b7f3c73a956fcc36c7df7be26bd9969575d
beep boop <githubbot@vadim.me>
2024-07-17 05:11:08 +0200

    Solution 14 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/0c08dgzw Prompt: 2920b46f-948c-4001-92c5-212caa28dcaa.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ 2920b46f-948c-4001-92c5-212caa28dcaa.cpp:3:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 2920b46f-948c-4001-92c5-212caa28dcaa.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 2920b46f-948c-4001-92c5-212caa28dcaa.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,10 +1,16 @@
+#include <string>
+
 int bowlingScore(std::string s) {
     int score = 0;
     int roll = 0;
     for (char c : s) {
         if (c == '/') {
             if (roll < 2) {
-                score += 10 - (10 - roll);
+                if (roll == 1) {
+                    score += 10 - (10 - roll);
+                } else {
+                    score += 10;
+                }
             }
             roll = 0;
         } else if (c >= '1' && c <= '9') {
@@ -14,10 +20,9 @@ int bowlingScore(std::string s) {
             roll = 0;
         }
     }
-    if (roll < 2) {
-        if (roll == 1)
-            score += roll * 10;
-        else
-            score += 10 + (roll - 2) * 10 / 3;
+    if (roll > 1) {
+        score += roll * 10;
+    } else if (roll == 1) {
+        score += 10 - (10 - roll);
     }
     return score;
