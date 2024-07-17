cfbe048d5b2ee896fb268458e47ed21909d9fff6
beep boop <githubbot@vadim.me>
2024-07-17 05:49:58 +0200

    Solution 42 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/wcd6zq0c Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 0517825e-b491-4fd5-851f-1c0912760f0a.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- leaders.cpp
+++ leaders.cpp
@@ -1,23 +1,26 @@
+```cpp
 #include <algorithm>
-#include <iostream> 
+#include <iostream>
 using namespace std;
 
 vector<int> leaders(vector<int> v) {
     vector<int> res;
-    for (int i = 0; i < v.size(); ++i) {
-        res.push_back(v[i]);
-        for (int j = i + 1; j < v.size(); ++j) {
-            if (v[j] >= res.back()) {
-                res.pop_back();
-                break;
-            }
+    int rightmost = v.back();
+    res.push_back(rightmost);
+
+    for (int i = v.size() - 2; i >= 0; --i) {
+        if (v[i] >= rightmost) {
+            rightmost = v[i];
+            res.push_back(rightmost);
         }
     }
+
+    reverse(res.begin(), res.end());
     return res;
 }
 
 int main() {
-    vector<int> v = {12, 993, 557, 890, 695, 295, 484, 866, 801, 909, 397, 667, 431};
+    vector<int> v = {1, 3, 4, 1, 5};
     vector<int> result = leaders(v);
     for (int x : result) cout << x << " ";
     return 0;
