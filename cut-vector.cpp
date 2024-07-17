f912c1c66a62ebb122fea3ab07c8a66bc22f8725
beep boop <githubbot@vadim.me>
2024-07-17 03:25:21 +0200

    Solution 34 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,56 +1,15 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
-    int n = vec.size();
+vector<vector<int>> cutVector(vector<int>& nums) {
     int minDiff = INT_MAX;
-    int index = -1;
-    
-    for (int i = 0; i < n; i++) {
-        int sumLeft = 0, sumRight = 0;
-        
-        for (int j = 0; j < i; j++)
-            sumLeft += vec[j];
-        for (int j = i + 1; j < n; j++)
-            sumRight += vec[j];
-        
-        if (sumLeft == sumRight) {
-            return {{vec[0], vec[0], vec.begin() + 1, vec.end()}};
-        }
-        
-        int diff = abs(sumLeft - sumRight);
-        if (diff < minDiff) {
+    int cutIndex = 0;
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs(nums[i] - nums[i-1]);
+        if (diff <= minDiff) {
             minDiff = diff;
-            index = i;
+            cutIndex = i;
         }
     }
-    
-    vector<int> left(vec.begin(), vec.begin() + index);
-    vector<int> right(vec.begin() + index, vec.end());
-    return {left, right};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
-    }
-    
-    pair<vector<int>, vector<int>> result = cutVector(vec);
-    
-    cout << "Left: ";
-    for (int x : result.first) {
-        cout << x << " ";
-    }
-    cout << endl;
-    
-    cout << "Right: ";
-    for (int x : result.second) {
-        cout << x << " ";
-    }
-    cout << endl;
-    
-    return 0;
+    return {{nums.begin(), nums.begin() + cutIndex}, {nums.begin() + cutIndex, nums.end()}};
 }
