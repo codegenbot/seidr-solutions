a1f7dc756d8eeca018a0e283ff3cb06447a75d4c
beep boop <githubbot@vadim.me>
2024-07-17 05:59:38 +0200

    Solution 23 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/6f340va8 Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -7,31 +7,17 @@ bool solveBoolean(std::string expression) {
         if (expression[i] == '&') {
             int j = i + 1;
             while (j < expression.size() && expression[j] != '|') j++;
-            std::string subexpr = expression.substr(i + 1, j - i - 1);
+            std::string subexpr = expression.substr(i, j - i);
             result &= solveBoolean(subexpr);
             i = j;
         }
     }
-
-    if (expression.find('|') != std::string::npos) {
-        size_t pos1 = expression.find('&');
-        size_t pos2 = expression.find('|', pos1 + 1);
-
-        std::string subexpr1 = expression.substr(0, pos1);
-        bool result1 = solveBoolean(subexpr1);
-        std::string subexpr2 = expression.substr(pos2 + 1);
-        bool result2 = solveBoolean(subexpr2);
-
-        return !result1 && !result2;
+    if (expression[i] == '|') {
+        int j = i + 1;
+        while (j < expression.size() && expression[j] != ' ') j++;
+        std::string subexpr2 = expression.substr(i, j - i);
+        result |= solveBoolean(subexpr2);
     }
 
-    if (expression.find('&') != std::string::npos) {
-        size_t pos1 = expression.find('&');
-        std::string subexpr1 = expression.substr(0, pos1 + 1);
-        bool result1 = solveBoolean(subexpr1);
-
-        return !result1;
-    }
-    
-    return true;
+    return !result;
 }
