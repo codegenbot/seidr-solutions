54849224cce3043c592067d4dd64be7086376650
beep boop <githubbot@vadim.me>
2024-07-17 02:33:29 +0200

    Solution 87 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/72dpnhqy Prompt: 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp:2:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     1 | #include <vector>   +++ |+#include <string>     2 | using namespace std; 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp:4:45: error: ‘string’ was not declared in this scope     4 | vector<int> indicesOfSubstring(string text, string target) {       |                                             ^~~~~~ 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp:4:45: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp:4:60: error: expected ‘,’ or ‘;’ before  ‘{’ token     4 | vector<int> indicesOfSubstring(string text, string target) {       |                                                            ^ 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp: In function ‘int gcd(int, int)’: 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp:28:14: error: expected ‘}’ at end of in put    28 |     return a;       |              ^ 9329edc5-051e-4c2e-9668-c914d997f1fc.cpp:22:23: note: to match this ‘{’    22 | int gcd(int a, int b) {       |                       ^

--- gcd.cpp
+++ gcd.cpp
@@ -1,3 +1,4 @@
+```
 #include <vector>
 #include <string>
 
@@ -11,14 +12,13 @@ vector<int> indicesOfSubstring(string text, string target) {
     for (int i = 0; ; i++) {
         int j = 0;
         while (i + j < n && i + j < m) {
-            if (text.substr(i, m).find(target) != string::npos)
+            if (text.substr(i, j).find(target) != string::npos)
                 result.push_back(i);
             j++;
         }
         i += m;
     }
-
-};
+}
 
 int gcd(int a, int b) {
     while (b != 0) {
