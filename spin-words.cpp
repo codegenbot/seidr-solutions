ca0163938904a1628aaa1310f8bfd4ec07b1ae2e
beep boop <githubbot@vadim.me>
2024-07-17 06:34:36 +0200

    Solution 64 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/xfeec785 Prompt: 5fca6858-f4c9-4756-b52b-955a06d4cda6.cpp: In function ‘int main()’: 5fca6858-f4c9-4756-b52b-955a06d4cda6.cpp:5:34: error: a function-definition is n ot allowed here before ‘{’ token     5 |     string spinWords(string str) {       |                                  ^

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,35 +1,37 @@
-int main() {
-    string spinWords(string str) {
-        std::string result = "";
-        int i = 0;
-        while (i < str.length()) {
-            if (str[i] == ' ') {
-                result += " ";
-                i++;
-            } else {
-                int j = i;
-                while (j < str.length() && str[j] != ' ') {
-                    j++;
-                }
-                string word = str.substr(i, j - i);
-                if (word.length() >= 5) {
-                    string revWord = "";
-                    for (int k = word.length() - 1; k >= 0; k--) {
-                        revWord += word[k];
-                    }
-                    result += revWord + " ";
-                } else {
-                    result += word + " ";
-                }
-                i = j;
+#include <string>
+using namespace std;
+
+string spinWords(string str) {
+    std::string result = "";
+    int i = 0;
+    while (i < str.length()) {
+        if (str[i] == ' ') {
+            result += " ";
+            i++;
+        } else {
+            int j = i;
+            while (j < str.length() && str[j] != ' ') {
+                j++;
             }
+            string word = str.substr(i, j - i);
+            if (word.length() >= 5) {
+                string revWord = "";
+                for (int k = word.length() - 1; k >= 0; k--) {
+                    revWord += word[k];
+                }
+                result += revWord + " ";
+            } else {
+                result += word + " ";
+            }
+            i = j;
         }
-        return result;
     }
-    string input;
-    cout << "Enter a string: ";
-    cin >> input;
-    string result = spinWords(input);
-    cout << "Result: " << result << endl;
-    return 0;
+    return result;
 }
+
+int main() {
+    string input;
+    cout << "Enter a sentence: ";
+    cin >> input;
+    cout << spinWords(input);
+    return 0;
