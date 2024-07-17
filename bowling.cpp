d4b6948d6b17dfed1020cc61a8a18e85e89bb079
beep boop <githubbot@vadim.me>
2024-07-17 03:32:48 +0200

    Solution 86 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/npoxbzm5 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,39 +1,24 @@
 int bowlingScore(string s) {
     int score = 0;
-    int frame = 0;
-    
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == 'X') {
-            score += 30;
-            frame++;
-        } else if (isdigit(s[i])) {
-            int pins = 0;
-            if (i + 1 < s.size() && isdigit(s[i+1]))
-                pins = 10 - (s[i] - '0');
-            else
-                pins = s[i] - '0';
-            
-            if (frame < 9) {
-                score += pins;
-                frame++;
-            } else {
-                if (i + 2 < s.size() && s[i+1] == '/') {
-                    i++;
-                    continue;
-                }
-                score += pins;
-            }
-        } else if (s[i] == '/') {
-            int first = s[i-1] - '0';
-            int second = 10 - first;
-            
-            if (i + 2 < s.size() && s[i+1] == 'X') {
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == '/') {
+            string firstPart = s.substr(0, i);
+            string secondPart = s.substr(i + 1);
+            int strike = count(firstPart.begin(), firstPart.end(), 'X') - 1;
+            int spare = 10 - strike;
+            score += strike * 10 + spare;
+        } else {
+            if (s[i] == 'X') {
                 score += 30;
-                frame++;
-            } else
-                score += first * 10 + second;
+            } else if (s[i] == '/') {
+                string part = s.substr(i);
+                int firstRoll = part[0] - '0';
+                int secondRoll = part[1] - '0';
+                score += 10 + firstRoll + secondRoll;
+            } else {
+                score += s[i] - '0' * 2;
+            }
         }
     }
-    
     return score;
 }
