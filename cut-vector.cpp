1c78a4367b144ae37f628969213a7a2f83f06134
beep boop <githubbot@vadim.me>
2024-07-17 03:47:47 +0200

    Solution 83 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,44 +1,24 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
-    int n = nums.size();
-    pair<vector<int>, vector<int>> res;
-    
-    for (int i = 1; i < n; i++) {
-        if (abs(nums[i] - nums[0]) <= abs((nums[i] + nums[0]) / 2)) {
-            res.first = vector<int>(nums.begin(), nums.begin() + i);
-            res.second = vector<int>(nums.begin() + i, nums.end());
-            return res;
+vector<int> cutVector(vector<int>& v) {
+    int n = v.size();
+    int leftSum = 0;
+    for (int i = 0; i < n; i++) {
+        leftSum += v[i];
+    }
+
+    int minDiff = INT_MAX;
+    int cutIndex = -1;
+    for (int i = 0; i < n; i++) {
+        int rightSum = leftSum - v[i];
+        if (abs(leftSum - rightSum) < minDiff) {
+            minDiff = abs(leftSum - rightSum);
+            cutIndex = i;
         }
     }
-    
-    res.first = vector<int>(nums.begin(), nums.end());
-    res.second.clear();
-    return res;
-}
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-    
-    pair<vector<int>, vector<int>> result = cutVector(nums);
-    
-    cout << "First subvector: ";
-    for (int num : result.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    
-    cout << "Second subvector: ";
-    for (int num : result.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-    
-    return 0;
+    vector<int> leftVec(v.begin(), v.begin() + cutIndex);
+    vector<int> rightVec(v.begin() + cutIndex, v.end());
+    return {leftVec, rightVec};
 }
