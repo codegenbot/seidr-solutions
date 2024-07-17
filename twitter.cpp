5f0e6dbf72ca31daeb66221bc6291c09bedab1c0
beep boop <githubbot@vadim.me>
2024-07-17 09:10:59 +0200

    Solution 62 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/04pxk33s Prompt: Program did not receive expected input

--- twitter.cpp
+++ twitter.cpp
@@ -1,21 +1,21 @@
-```cpp
+```
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
     string tweet;
-    cin >> ws; // Consume whitespace
     cout << "Enter a tweet: ";
-    if (!(cin >> tweet) || tweet.length() > 140) {
-        if (tweet.empty()) {
-            cout << "You didn't type anything" << endl;
-        } else {
-            cout << "Too many characters" << endl;
-        }
+    getline(cin, tweet);
+    
+    if (tweet.length() > 140) {
+        cout << "Too many characters" << endl;
+    } else if (tweet.empty()) {
+        cout << "You didn't type anything" << endl;
     } else {
         cout << "Your tweet has " << tweet.length() << " characters" << endl;
     }
-    cin.get(); 
+    
+    cin.get();
     return 0;
 }
