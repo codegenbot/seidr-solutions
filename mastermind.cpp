21ac480fae693934d7706ea8e85eda3371fc55e3
beep boop <githubbot@vadim.me>
2024-07-17 06:36:06 +0200

    Solution 90 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/8416du5e Prompt:     8 |     std::map<char, int> placementMap;       |              ^~~~ a82d9b27-edf6-4578-a0ba-7fccdab55aff.cpp:13:25: error: ‘count’ is not a member o f ‘std’    13 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                         ^~~~~ a82d9b27-edf6-4578-a0ba-7fccdab55aff.cpp:15:13: error: ‘colorMap’ was not declar ed in this scope    15 |             colorMap[code[i]]++;       |             ^~~~~~~~ a82d9b27-edf6-4578-a0ba-7fccdab55aff.cpp:19:23: error: ‘colorMap’ was not declar ed in this scope    19 |     for (auto& pair : colorMap) {       |                       ^~~~~~~~ a82d9b27-edf6-4578-a0ba-7fccdab55aff.cpp:20:9: error: ‘placementMap’ was not dec lared in this scope    20 |         placementMap[pair.first] = pair.second;       |         ^~~~~~~~~~~~ a82d9b27-edf6-4578-a0ba-7fccdab55aff.cpp:24:29: error: ‘placementMap’ was not de clared in this scope    24 |     for (const auto& pair : placementMap) {       |                             ^~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,21 +1,33 @@
-int mastermind(std::string code, std::string guess) {
-    int black = 0;
-    int white = 0;
+#include <string>
+#include <algorithm>
+#include <map>
 
+int mastermind(std::string code, std::string guess) {
+    int white = 0;
+    int black = 0;
+
+    std::map<char, int> colorMap; 
     std::map<char, int> placementMap; 
 
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) { 
             black++;
-        }
-    }
-
-    for (char c : code) {
-        if (std::count(guess.begin(), guess.end(), c) > 0 && placementMap[c] < 1) {
+        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) { 
             white++;
-            placementMap[c]++;
+            colorMap[code[i]]++; 
         }
     }
 
-    return {black, white};
-}
+    for (auto& pair : colorMap) {
+        placementMap[pair.first] = pair.second; 
+    }
+
+    int blackCount = 0;
+    for (const auto& pair : placementMap) {
+        if (pair.second == 4) {
+            blackCount++;
+        }
+    }
+    black -= blackCount;
+
+    return black;
