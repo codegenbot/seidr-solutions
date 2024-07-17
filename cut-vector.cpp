c4da88191fc80709fe04f0eb13036202848a5424
beep boop <githubbot@vadim.me>
2024-07-17 03:35:18 +0200

    Solution 55 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/2pvmdwg4 Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,36 +1,50 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> nums) {
-    int n = nums.size();
-    int minDiff = INT_MAX;
-    int splitIndex = -1;
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int min_diff = INT_MAX;
+    pair<int, int> res;
     
-    for (int i = 0; i < n; i++) {
-        int leftSum = 0, rightSum = 0;
+    for (int i = 1; i <= v.size(); i++) {
+        int left_sum = 0;
+        int right_sum = 0;
         
         for (int j = 0; j < i; j++) {
-            leftSum += nums[j];
+            left_sum += v[j];
         }
         
-        for (int j = i; j < n; j++) {
-            rightSum += nums[j];
+        for (int j = i; j < v.size(); j++) {
+            right_sum += v[j];
         }
         
-        int diff = abs(leftSum - rightSum);
-        if (diff < minDiff) {
-            minDiff = diff;
-            splitIndex = i;
+        if (left_sum == right_sum) {
+            return {{v.begin(), v.begin() + i}, {v.begin() + i, v.end()}};
+        } else if (abs(left_sum - right_sum) < min_diff) {
+            min_diff = abs(left_sum - right_sum);
+            res = {make_pair(v.begin(), v.begin() + i), make_pair(v.begin() + i, v.end())};
         }
     }
     
-    vector<vector<int>> result(2);
-    for (int i = 0; i < splitIndex; i++) {
-        result[0].push_back(nums[i]);
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    for (int i = 0; i <= n; i++) {
+        cin >> v[i];
     }
-    for (int i = splitIndex; i < nums.size(); i++) {
-        result[1].push_back(nums[i]);
+    pair<vector<int>, vector<int>> result = cutVector(v);
+    cout << "[";
+    for(int i=0; i < result.first.size(); i++){
+        if(i==result.first.size()-1) cout<<result.first[i]<<"]\n";
+        else cout<<result.first[i]<<", ";
     }
-    
-    return result;
+    cout << "[";
+    for(int i=0; i < result.second.size(); i++){
+        if(i==result.second.size()-1) cout<<result.second[i]<<"]\n";
+        else cout<<result.second[i]<<", ";
+    }
+    return 0;
 }
