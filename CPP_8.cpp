bb3acb90d772d1c3d2968ea0c655f700cc666deb
beep boop <githubbot@vadim.me>
2024-07-19 01:58:21 +0200

    Solution 56 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8a7nvcid Prompt:       |                           ^~~~~~ 6cd91e9a-b8f5-4b8a-8ec6-77b350bcedc9.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 6cd91e9a-b8f5-4b8a-8ec6-77b350bcedc9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6cd91e9a-b8f5-4b8a-8ec6-77b350bcedc9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6cd91e9a-b8f5-4b8a-8ec6-77b350bcedc9.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6cd91e9a-b8f5-4b8a-8ec6-77b350bcedc9.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 6cd91e9a-b8f5-4b8a-8ec6-77b350bcedc9.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -15,9 +15,3 @@ std::vector<int> sum_product(std::vector<int> a) {
     }
     return {sum, product};
 }
-
-int main() {
-    assert(issame(std::vector<int>{10}, std::vector<int>{10, 10}));
-    
-    return 0;
-}
