505d31a3adc51ee32b71b298c15d170610d61e78
beep boop <githubbot@vadim.me>
2024-07-18 19:57:09 +0200

    Solution 3 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0tiggni6 Prompt:       |                    ^~~ 6c8c033a-7980-40f7-9018-a813979b0441.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6c8c033a-7980-40f7-9018-a813979b0441.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6c8c033a-7980-40f7-9018-a813979b0441.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6c8c033a-7980-40f7-9018-a813979b0441.cpp:13: 6c8c033a-7980-40f7-9018-a813979b0441.cpp: In function ‘int main()’: 6c8c033a-7980-40f7-9018-a813979b0441.cpp:23:20: error: ‘make_a_pile’ was not dec lared in this scope    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 6c8c033a-7980-40f7-9018-a813979b0441.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -24,6 +24,6 @@ std::vector<int> make_a_pile(int n){
 }
 
 int main(){
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22});
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
     return 0;
 }
