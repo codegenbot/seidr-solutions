c3e684b7781f4c808b73314d2ac0a18d7108b561
beep boop <githubbot@vadim.me>
2024-07-18 18:03:40 +0200

    Solution 24 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/qddz4bbx Prompt:       |                    ^~~ edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp:11: edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp: In function ‘int main()’: edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp:21:20: error: ‘generate_integers’ was n ot declared in this scope    21 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ edcc90a1-e182-4d69-8d00-02108ca5f78e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,23 +1,13 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
-
-vector<int> generate_integers(int a, int b){
-    vector<int> result;
-    for(int i = a; i <= b; ++i){
-        if(i % 2 == 0){
-            result.push_back(i);
-        }
-    }
-    return result;
-}
-
-bool issame(vector<int> a, vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
+std::vector<int> generate_integers(int a, int b);
+
 int main(){
-    assert(issame(generate_integers(17,89) , {}));
+    assert(issame(generate_integers(17, 89), {}));
     return 0;
 }
