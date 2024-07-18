af14337ee20066094b1b15aaae756233fef27cda
beep boop <githubbot@vadim.me>
2024-07-18 23:17:45 +0200

    Solution 28 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7r7w2c5w Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2b269343-8cd4-4aed-8e84-30c275678439.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2b269343-8cd4-4aed-8e84-30c275678439.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2b269343-8cd4-4aed-8e84-30c275678439.cpp:21: 2b269343-8cd4-4aed-8e84-30c275678439.cpp: In function ‘int main()’: 2b269343-8cd4-4aed-8e84-30c275678439.cpp:31:20: error: ‘parse_music’ was not dec lared in this scope    31 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 2b269343-8cd4-4aed-8e84-30c275678439.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
+using namespace std;
 
-std::vector<int> parse_music(std::string music_string) {
-    std::vector<int> beats;
-    int note_duration = 0;
-    for (char c : music_string) {
-        if (c == 'o' || c == '|') {
-            beats.push_back(note_duration + 1);
-            note_duration = 0;
-        } else if (c == '.') {
-            note_duration++;
+vector<int> parse_music(string music_string) {
+    vector<int> beats;
+    size_t pos = 0;
+    while ((pos = music_string.find("o", pos)) != string::npos) {
+        if (music_string[pos + 1] == '|') {
+            beats.push_back(2);
+            pos += 2;
+        } else {
+            beats.push_back(4);
+            pos++;
         }
     }
-    beats.push_back(note_duration + 1);
+    pos = 0;
+    while ((pos = music_string.find(".|", pos)) != string::npos) {
+        beats.push_back(1);
+        pos += 2;
+    }
     return beats;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 int main() {
     assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
+    cout << "Test passed successfully!" << endl;
     return 0;
 }
