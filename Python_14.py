c9ced61fd6a6ee6eab42d08293f30990a9a473b5
beep boop <githubbot@vadim.me>
2024-07-18 20:02:30 +0200

    Solution 33 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8ozje07y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7bfe362a-b790-44d6-a959-a0ad740b8 c23.py", line 1, in <module>     def all_prefixes(string: str) -> List[str]:                                      ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_14.py
+++ Python_14.py
@@ -1,7 +1,7 @@
 import sys
 
 def all_prefixes(string: str):
-    return [string[: i + 1] for i in range(len(string)]
+    return [string[:i + 1] for i in range(len(string)]
 
 string = sys.stdin.readline().strip()
 print(all_prefixes(string))
