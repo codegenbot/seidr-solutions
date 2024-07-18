a25b1f80a899bdc56541af8b7bf3c01d0fd9ee53
beep boop <githubbot@vadim.me>
2024-07-18 18:23:51 +0200

    Solution 22 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/gcs5wfsa Prompt:  this scope    21 |         cout << numbers[i] << endl;       |         ^~~~ 1ac753ed-ae3f-4b39-881a-3ced9ed5919d.cpp:21:17: error: ‘numbers’ was not declare d in this scope    21 |         cout << numbers[i] << endl;       |                 ^~~~~~~ 1ac753ed-ae3f-4b39-881a-3ced9ed5919d.cpp:21:31: error: ‘endl’ was not declared i n this scope    21 |         cout << numbers[i] << endl;       |                               ^~~~ 1ac753ed-ae3f-4b39-881a-3ced9ed5919d.cpp:24:5: error: ‘cout’ was not declared in  this scope    24 |     cout << 0 << endl;       |     ^~~~ 1ac753ed-ae3f-4b39-881a-3ced9ed5919d.cpp:24:18: error: ‘endl’ was not declared i n this scope    24 |     cout << 0 << endl;       |                  ^~~~ 1ac753ed-ae3f-4b39-881a-3ced9ed5919d.cpp:26:33: error: ‘numbers’ was not declare d in this scope    26 |     for (int i = cut_index; i < numbers.size(); ++i) {       |                                 ^~~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -8,14 +8,14 @@ vector<int> numbers;
 
 int main() {
     int num;
-
+    
     while (cin >> num) {
         numbers.push_back(num);
     }
-
+    
     int min_diff = INT_MAX;
     int cut_index = 0;
-
+    
     for (int i = 1; i < numbers.size(); ++i) {
         int diff = abs(accumulate(numbers.begin(), numbers.begin() + i, 0) - accumulate(numbers.begin() + i, numbers.end(), 0));
         if (diff < min_diff) {
@@ -23,18 +23,18 @@ int main() {
             cut_index = i;
         }
     }
-
+    
     for (int i = 0; i < cut_index; ++i) {
         cout << numbers[i] << "\n";
     }
-
+    
     cout << 0 << "\n";
-
+    
     for (int i = cut_index; i < numbers.size(); ++i) {
         cout << numbers[i] << "\n";
     }
-
+    
     cout << 0 << "\n";
-
+    
     return 0;
 }
