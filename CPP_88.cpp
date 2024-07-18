7f78b031b2068df3f68bf9f91f78e61e041e1559
beep boop <githubbot@vadim.me>
2024-07-19 01:06:54 +0200

    Solution 17 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/tewd0zhi Prompt:       |                    ^~~ de7c99b3-221c-463c-9dff-9dd86645b19c.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ de7c99b3-221c-463c-9dff-9dd86645b19c.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ de7c99b3-221c-463c-9dff-9dd86645b19c.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from de7c99b3-221c-463c-9dff-9dd86645b19c.cpp:10: de7c99b3-221c-463c-9dff-9dd86645b19c.cpp: In function ‘int main()’: de7c99b3-221c-463c-9dff-9dd86645b19c.cpp:20:20: error: ‘sort_array’ was not decl ared in this scope    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ de7c99b3-221c-463c-9dff-9dd86645b19c.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -11,16 +11,3 @@ std::vector<int> sort_array(const std::vector<int>& array){
         std::sort(sorted_array.begin(), sorted_array.end());
     return sorted_array;
 }
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-int main(){
-    std::vector<int> array = {21, 14, 23, 11};
-    assert(issame(sort_array(array), {23, 21, 14, 11}));
-}
