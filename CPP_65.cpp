8ae7688c038858f5411a1528f8b72546cff541ed
beep boop <githubbot@vadim.me>
2024-07-18 22:48:54 +0200

    Solution 4 of CPP/65, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i074105k Prompt:       |             std b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp:3:5: error: ‘shift’ does not name a typ e     3 |     shift %= n;       |     ^~~~~ b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp:4:5: error: expected unqualified-id bef ore ‘if’     4 |     if (shift == 0) {       |     ^~ b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp:6:7: error: expected unqualified-id bef ore ‘else’     6 |     } else {       |       ^~~~ b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp:12: b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp: In function ‘int main()’: b67fc38d-6c3c-4809-b540-3ca5ff3ccf85.cpp:14:13: error: ‘circular_shift’ was not declared in this scope    14 |     assert (circular_shift(11, 101) == "11");       |             ^~~~~~~~~~~~~~

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,13 +1,18 @@
-string num = to_string(x);
-    int n = num.size();
-    shift %= n;
+#include <iostream>
+#include <string>
+#include <cassert>
+
+std::string circular_shift(int x, int shift) {
+    std::string str = std::to_string(x);
+    int n = str.size();
+    shift = shift % n;
     if (shift == 0) {
-        return num;
+        return str;
     }
-    if (shift > n) {
-        reverse(num.begin(), num.end());
-        return num;
-    }
-    string result = num.substr(n - shift) + num.substr(0, n - shift);
-    return result;
+    return str.substr(n - shift) + str.substr(0, n - shift);
+}
+
+int main() {
+    assert(circular_shift(11, 101) == "11");
+    return 0;
 }
