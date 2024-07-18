3451076309d6efa51c3fab7292f25e89514f51ff
beep boop <githubbot@vadim.me>
2024-07-18 22:55:49 +0200

    Solution 42 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/n7z7xgvg Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘int’ and ‘const int’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -13,15 +13,15 @@ vector<int> common(const vector<int>& l1, const vector<int>& l2){
     vector<int> result;
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
-    
+
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
-    
+
     return result;
 }
 
 int main() {
     assert(issame(common({4, 3, 2, 8}, {}), {}));
-    
+
     return 0;
 }
