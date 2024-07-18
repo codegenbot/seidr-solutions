8e42762bb92992717a146e1b803b7f94f5642be1
beep boop <githubbot@vadim.me>
2024-07-18 23:20:32 +0200

    Solution 12 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nqdhikkh Prompt:       |                    ^~~ 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp:18: 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp: In function ‘int main()’: 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 5bde6ee6-336e-4c00-a5c3-fca7b18c2130.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,29 +1,19 @@
 #include <vector>
-#include <string>
-#include <algorithm>
+#include <cassert>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    return a == b;
-}
-
-std::vector<int> even_odd_palindrome(int n){
-    std::vector<int> result(2, 0);
-    for(int i = 1; i <= n; ++i){
-        std::string str = std::to_string(i);
-        std::string rev = str;
-        std::reverse(rev.begin(), rev.end());
-        if(str == rev){
-            if(i % 2 == 0){
-                result[0]++;
+vector<int> even_odd_palindrome(int n){
+    vector<int> res(2, 0);
+    for (int i = 1; i <= n; ++i) {
+        string num = to_string(i);
+        string rev = num;
+        reverse(rev.begin(), rev.end());
+        if (num == rev) {
+            if (i % 2 == 0) {
+                res[0]++;
             } else {
-                result[1]++;
+                res[1]++;
             }
         }
     }
-    return result;
-}
-
-int main(){
-    assert(issame(even_odd_palindrome(1), {0, 1}));
-    return 0;
+    return res;
 }
