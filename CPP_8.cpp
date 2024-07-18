fb0e006af842cf58f78d9fedf67365d68c3fc2ed
beep boop <githubbot@vadim.me>
2024-07-19 01:52:34 +0200

    Solution 27 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8a7nvcid Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bceb135d-75ca-4bb1-acc5-24c30740effa.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bceb135d-75ca-4bb1-acc5-24c30740effa.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bceb135d-75ca-4bb1-acc5-24c30740effa.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bceb135d-75ca-4bb1-acc5-24c30740effa.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from bceb135d-75ca-4bb1-acc5-24c30740effa.cpp:7: bceb135d-75ca-4bb1-acc5-24c30740effa.cpp: In function ‘int main()’: bceb135d-75ca-4bb1-acc5-24c30740effa.cpp:17:20: error: ‘sum_product’ was not dec lared in this scope    17 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,25 +1,15 @@
 #include <vector>
-#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> sum_product(std::vector<int> vec) {
-    std::vector<int> result;
-    if (vec.empty()) {
-        return result;
+    if (a.size() != b.size()) {
+        return false;
     }
-    int product = 1;
-    for (int num : vec) {
-        product *= num;
+    
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
-    result.push_back(product);
-    result.push_back(vec.front());
-    return result;
-}
-
-int main() {
-    assert(issame(sum_product({10}), {10, 10}));
-    return 0;
+    
+    return true;
 }
