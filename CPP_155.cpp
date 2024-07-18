08b7f0937903994832f7e0ecd040a47d33f96d7d
beep boop <githubbot@vadim.me>
2024-07-19 00:15:28 +0200

    Solution 37 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zlj3vahe Prompt: In file included from /usr/include/c++/11/vector:67,                  from c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp:17: c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp: In function ‘int main()’: c0de2ebf-6f44-433a-bde3-a2e8b7ddab67.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -6,19 +6,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> even_odd_count(int num) {
-    std::vector<int> counts(2, 0);
-    std::string num_str = std::to_string(std::abs(num));
-    for (char c : num_str) {
-        if ((c - '0') % 2 == 0) {
-            counts[0]++;
-        } else {
-            counts[1]++;
-        }
-    }
-    return counts;
-}
-
 int main() {
     assert(issame(even_odd_count(0), {1, 0}));
     
