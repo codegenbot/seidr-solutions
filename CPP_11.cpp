395e74ad07b2761ce7f9044a0a5ce012dee25315
beep boop <githubbot@vadim.me>
2024-07-19 01:51:12 +0200

    Solution 3 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/m7r5bypp Prompt: f71fde99-12c1-48d6-a8d8-76bad7cee4a7.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string string_xor(const string &a, const string &b) {       | ^~~~~~ In file included from f71fde99-12c1-48d6-a8d8-76bad7cee4a7.cpp:13: f71fde99-12c1-48d6-a8d8-76bad7cee4a7.cpp: In function ‘int main()’: f71fde99-12c1-48d6-a8d8-76bad7cee4a7.cpp:15:13: error: ‘string_xor’ was not decl ared in this scope    15 |     assert (string_xor("0101", "0000") == "0101");       |             ^~~~~~~~~~

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include <string>
 #include <cassert>
 
-std::string string_xor(const std::string& a, const std::string& b) {
+std::string string_xor(const std::string &a, const std::string &b) {
     std::string result = "";
     for(size_t i=0; i<a.size(); i++){
         if(a[i] != b[i]){
@@ -15,7 +14,6 @@ std::string string_xor(const std::string& a, const std::string& b) {
 }
 
 int main() {
-    assert (string_xor("0101", "0000") == "0101");
-    
+    assert(string_xor("0101", "0000") == "0101");
     return 0;
 }
