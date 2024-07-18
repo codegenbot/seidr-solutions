fa40bfc5647c3a0ece5db8b2787b8ddd684a2950
beep boop <githubbot@vadim.me>
2024-07-18 20:57:19 +0200

    Solution 91 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/wvhpg5or Prompt:       |                              ^~~~~~ 65be1932-b8fa-45c1-baed-73278a05722b.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 65be1932-b8fa-45c1-baed-73278a05722b.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 65be1932-b8fa-45c1-baed-73278a05722b.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 65be1932-b8fa-45c1-baed-73278a05722b.cpp:13: 65be1932-b8fa-45c1-baed-73278a05722b.cpp: In function ‘int main()’: 65be1932-b8fa-45c1-baed-73278a05722b.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 65be1932-b8fa-45c1-baed-73278a05722b.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b);
 
 vector<string> reverse_delete(string s, string c);
 
-bool issame(vector<string> a, vector<string> b) {
-    return a[0] == b[0];
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
 }
 
 vector<string> reverse_delete(string s, string c){
@@ -15,7 +20,9 @@ vector<string> reverse_delete(string s, string c){
             result += ch;
         }
     }
+
     string reversed = result;
     reverse(reversed.begin(), reversed.end());
-    return {result, result == reversed ? "True" : "False"};
+
+    return {result, issame({result}, {reversed}) ? "True" : "False"};
 }
