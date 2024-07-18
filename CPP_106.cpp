47c09caeaf3a1b9923faf98d23cb20ab5d8b74e9
beep boop <githubbot@vadim.me>
2024-07-18 20:37:17 +0200

    Solution 8 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/yin433ue Prompt:       |                    ^~~ c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp:18: c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp: In function ‘int main()’: c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp:28:20: error: ‘f’ was not declared in t his scope    28 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ c755290b-2d4b-4ba1-9c70-565df4a58d7e.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,33 +1,16 @@
 #include <vector>
+#include <cassert>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
 
-    for (int i = 0; i < a.size(); ++i) {
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
 
     return true;
-}
-
-vector<int> f(int n) {
-    vector<int> result;
-    int factorial = 1;
-    int sum = 0;
-    
-    for (int i = 1; i <= n; ++i) {
-        if (i % 2 == 0) {
-            factorial *= i;
-            result.push_back(factorial);
-        } else {
-            sum += i;
-            result.push_back(sum);
-        }
-    }
-    
-    return result;
 }
