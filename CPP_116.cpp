4b25853c561126050b1a3b5e414a1bb758df5f82
beep boop <githubbot@vadim.me>
2024-07-18 20:36:45 +0200

    Solution 4 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1wiuvt4w Prompt:       |                    ^~~ f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp:12: f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp: In function ‘int main()’: f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ f17fe1d8-b3e1-429b-bb4c-78c8134d30b1.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,9 +1,12 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-vector<int> sort_array(vector<int>& arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
+
+std::vector<int> sort_array(std::vector<int> arr){
+    std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int count_a = __builtin_popcount(a);
         int count_b = __builtin_popcount(b);
         if (count_a == count_b) {
@@ -12,9 +15,4 @@ vector<int> sort_array(vector<int>& arr) {
         return count_a < count_b;
     });
     return arr;
-}
-
-int main() {
-    assert((sort_array({2, 4, 8, 16, 32}) == vector<int>{2, 4, 8, 16, 32}));
-    return 0;
 }
