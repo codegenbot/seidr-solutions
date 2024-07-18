fbe357e89bc1e4724d09128dff7d5e4f0062db6d
beep boop <githubbot@vadim.me>
2024-07-18 16:23:32 +0200

    Solution 34 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4l9354hn Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 50fd5aca-9d16-4eb2-a857-6084b96a20c4.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 50fd5aca-9d16-4eb2-a857-6084b96a20c4.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 50fd5aca-9d16-4eb2-a857-6084b96a20c4.cpp:7: 50fd5aca-9d16-4eb2-a857-6084b96a20c4.cpp: In function ‘int main()’: 50fd5aca-9d16-4eb2-a857-6084b96a20c4.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 50fd5aca-9d16-4eb2-a857-6084b96a20c4.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 vector<int> incr_list(vector<int> l) {
     for(int i = 0; i < l.size(); i++){
         l[i]++;
@@ -7,4 +12,21 @@ vector<int> incr_list(vector<int> l) {
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> input(n);
+    for(int i = 0; i < n; i++){
+        cin >> input[i];
+    }
+
+    vector<int> result = incr_list(input);
+
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i] << " ";
+    }
+
+    return 0;
 }
