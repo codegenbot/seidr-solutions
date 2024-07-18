a5554a7b003dd1f26b25c5c963ddeb516c34dbd8
beep boop <githubbot@vadim.me>
2024-07-18 23:16:01 +0200

    Solution 4 of Python/106, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/x4kqsp6t Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2378b5f1-2d0a-4fde-8c65-4e510ce16 034.py", line 7, in <module>     check(f)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2378b5f1-2d0a-4fde-8c65-4e510ce16 034.py", line 4, in check     assert f(3) == [1, 2, 6]            ^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2378b5f1-2d0a-4fde-8c65-4e510ce16 034.py", line 2, in f     return [math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1)) for i in r ange(1, n + 1)]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2378b5f1-2d0a-4fde-8c65-4e510ce16 034.py", line 2, in <listcomp>     return [math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1)) for i in r ange(1, n + 1)]             ^^^^ NameError: name 'math' is not defined

--- Python_106.py
+++ Python_106.py
@@ -1,9 +1,4 @@
+import math
+
 def f(n):
-    return [
-        1 if i == 0 else factorial(i) if i % 2 == 0 else sum(range(1, i + 1))
-        for i in range(n)
-    ]
-
-
-def factorial(num):
-    return 1 if num == 0 else num * factorial(num - 1)
+    return [math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1)) for i in range(1, n + 1)]
