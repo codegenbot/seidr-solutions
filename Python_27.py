8c233ee3330173de9edf9364425f395d32e73c69
beep boop <githubbot@vadim.me>
2024-07-18 16:49:13 +0200

    Solution 2 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ksrhb03v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55a34802-462d-406e-a734-ebdd2875a 901.py", line 1     return "".join([char.lower() if char.isupper() else char.upper() for char in  string])     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_27.py
+++ Python_27.py
@@ -1,1 +1,2 @@
-return "".join(char.lower() if char.isupper() else char.upper() for char in string)
+def modify_string(string):
+    return "".join([char.lower() if char.isupper() else char.upper() for char in string])
