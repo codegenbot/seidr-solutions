864c99d501ccd7192a0f02feecab83adbccf7832
beep boop <githubbot@vadim.me>
2024-07-18 17:50:38 +0200

    Solution 6 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/w1q2b7x8 Prompt:       |                    ^~~ 7796fcfa-dab2-4557-966a-c91290eb456b.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7796fcfa-dab2-4557-966a-c91290eb456b.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7796fcfa-dab2-4557-966a-c91290eb456b.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7796fcfa-dab2-4557-966a-c91290eb456b.cpp:24: 7796fcfa-dab2-4557-966a-c91290eb456b.cpp: In function ‘int main()’: 7796fcfa-dab2-4557-966a-c91290eb456b.cpp:34:20: error: ‘get_odd_collatz’ was not  declared in this scope    34 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 7796fcfa-dab2-4557-966a-c91290eb456b.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,24 +1,25 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> result;
     while (n != 1) {
-        if (n % 2 == 1) {
-            result.push_back(n);
+        result.push_back(n);
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
         }
-        n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
-    sort(result.begin(), result.end());
-    return result;
+
+    std::vector<int> odd_numbers;
+    for (int num : result) {
+        if (num % 2 != 0) {
+            odd_numbers.push_back(num);
+        }
+    }
+
+    std::sort(odd_numbers.begin(), odd_numbers.end());
+
+    return odd_numbers;
 }
