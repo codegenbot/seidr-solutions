70370b5f20695ea42690774d9bcc4e56636df349
beep boop <githubbot@vadim.me>
2024-07-19 01:31:28 +0200

    Solution 14 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uq575vp2 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,18 @@
 if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int int_a = boost::any_cast<int>(a);
-        int int_b = boost::any_cast<int>(b);
-        if (int_a > int_b) {
-            return int_a;
-        } else if (int_b > int_a) {
-            return int_b;
-        } else {
-            return "None";
-        }
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float float_a = boost::any_cast<float>(a);
-        float float_b = boost::any_cast<float>(b);
-        if (float_a > float_b) {
-            return float_a;
-        } else if (float_b > float_a) {
-            return float_b;
-        } else {
-            return "None";
-        }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = boost::any_cast<string>(a);
-        string str_b = boost::any_cast<string>(b);
-        float val_a = stof(str_a);
-        float val_b = stof(str_b);
-        
-        if (val_a > val_b) {
-            return str_a;
-        } else if (val_b > val_a) {
-            return str_b;
-        } else {
-            return "None";
-        }
-    } else {
-        return "None";
-    }
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b))
+        return a;
+    else if (boost::any_cast<int>(a) < boost::any_cast<int>(b))
+        return b;
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b))
+        return a;
+    else if (boost::any_cast<float>(a) < boost::any_cast<float>(b))
+        return b;
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) 
+        return a;
+    else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b)))
+        return b;
+} else {
+    return "None";
 }
