9125adec425d2bbfaea83dc22bb130c19afd4b37
beep boop <githubbot@vadim.me>
2024-07-18 17:59:29 +0200

    Solution 32 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ajuz77c4 Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3e2aa090-9ddc-4e55-8bc0-44e13b8cc2e7.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3e2aa090-9ddc-4e55-8bc0-44e13b8cc2e7.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3e2aa090-9ddc-4e55-8bc0-44e13b8cc2e7.cpp:21: 3e2aa090-9ddc-4e55-8bc0-44e13b8cc2e7.cpp: In function ‘int main()’: 3e2aa090-9ddc-4e55-8bc0-44e13b8cc2e7.cpp:31:20: error: ‘count_up_to’ was not dec lared in this scope    31 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 3e2aa090-9ddc-4e55-8bc0-44e13b8cc2e7.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
 #include <vector>
-#include <cassert>
 
-std::vector<int> count_up_to(int n) {
-    std::vector<int> res;
+bool issame(int a, int b) {
+    return a == b;
+}
+
+int count_up_to(int n) {
+    vector<int> res;
     if (n <= 1) {
         return res;
     }
-    std::vector<bool> isPrime(n, true);
+    vector<bool> isPrime(n, true);
     for (int p = 2; p * p < n; p++) {
         if (isPrime[p]) {
             for (int i = p * p; i < n; i += p) {
@@ -21,22 +23,4 @@ std::vector<int> count_up_to(int n) {
         }
     }
     return res;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-  
-    return 0;
 }
