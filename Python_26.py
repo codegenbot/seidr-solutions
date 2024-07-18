335b3e3127c9ac4a91c17306762500e2248dcba9
beep boop <githubbot@vadim.me>
2024-07-19 01:34:30 +0200

    Solution 81 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/msus78oo Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/27bb96e2-f27e-4f2f-aa5c-5f2a6c40a c3d.py", line 1, in <module>     def remove_duplicates(numbers: List[int]) -> List[int]:                                    ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_26.py
+++ Python_26.py
@@ -1,3 +1,5 @@
 from typing import List
+
+
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    return list(set([num for num in numbers if numbers.count(num) == 1]))
+    return [num for num in numbers if numbers.count(num) == 1]
