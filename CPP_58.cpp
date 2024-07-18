8dc0d3445ee4ceab36284560ba88c0301c42ec83
beep boop <githubbot@vadim.me>
2024-07-19 01:03:22 +0200

    Solution 79 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8mcjvope Prompt:       |                    ^~~ f8b38616-44b5-4555-a158-7f1869c97c16.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f8b38616-44b5-4555-a158-7f1869c97c16.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f8b38616-44b5-4555-a158-7f1869c97c16.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f8b38616-44b5-4555-a158-7f1869c97c16.cpp:12: f8b38616-44b5-4555-a158-7f1869c97c16.cpp: In function ‘int main()’: f8b38616-44b5-4555-a158-7f1869c97c16.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f8b38616-44b5-4555-a158-7f1869c97c16.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,18 +3,14 @@
 #include <set>
 #include <cassert>
 
+bool issame(std::vector<int> a, std::vector<int> b);
+
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> main(std::vector<int> l1, std::vector<int> l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
+int main() {
+    assert(issame(std::vector<int>({4, 3, 2, 8}), std::vector<int>{}));
 
-    std::vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-
-    result.erase(unique(result.begin(), result.end()), result.end());
-
-    return result;
+    return 0;
 }
