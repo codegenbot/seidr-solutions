e9507c7be3b1ba452be427c535305ba5df75681f
beep boop <githubbot@vadim.me>
2024-07-19 01:13:32 +0200

    Solution 65 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8ek5c5c8 Prompt:       |                                   ^~~~~~ f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp:19: f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp: In function ‘int main()’: f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ f13babfc-cf5b-41a1-9a4a-e1623fd0396f.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -2,12 +2,10 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    return a[0] == b[0] && a[1] == b[1];
 }
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x);
-
-vector<vector<int>> get_row(vector<vector<int>> lst, int x){
+vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     vector<vector<int>> result;
     for (int i = 0; i < lst.size(); i++) {
         for (int j = 0; j < lst[i].size(); j++) {
