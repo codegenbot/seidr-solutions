23768c241ea2a8ee15e7394c430e97a9598d969c
beep boop <githubbot@vadim.me>
2024-07-18 23:24:28 +0200

    Solution 36 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8ri42qob Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp:13: 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp: In function ‘int main()’: 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp:23:20: error: ‘by_length’ was not decla red in this scope    23 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 22b082a5-7b77-46b0-b2f8-8a6694d8ee46.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,39 +1,12 @@
-#include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
 #include <string>
-#include <cassert>
 
-vector<string> by_length(vector<int> arr){
-    vector<string> result;
-    map<int, string> num_to_str = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"},
-        {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
-    };
+std::vector<std::string> by_length(std::vector<int> arr);
 
-    vector<int> sorted_arr;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            sorted_arr.push_back(num);
-        }
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
     }
-    sort(sorted_arr.begin(), sorted_arr.end());
-    reverse(sorted_arr.begin(), sorted_arr.end());
-
-    for (int num : sorted_arr) {
-        result.push_back(num_to_str[num]);
-    }
-
-    return result;
-}
-
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-int main(){
-    assert (issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
-    
-    return 0;
+    return true;
 }
