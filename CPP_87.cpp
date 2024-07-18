5aad85785b6b0867faf38891d4d533d2f334f612
beep boop <githubbot@vadim.me>
2024-07-18 17:09:17 +0200

    Solution 3 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/adfdtsfz Prompt:       |                                   ^~~~~~ 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp:19: 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp: In function ‘int main()’: 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 3d616d4b-b4c8-4a6a-8b2c-d483b02821a1.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,7 +1,6 @@
 #include <vector>
-#include <algorithm>
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
+vector<vector<int>> get_row(vector<vector<int>> lst, int x){
     vector<vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
