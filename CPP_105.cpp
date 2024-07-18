65c99e28c205b94f0a5665713dc2383b9a9165a8
beep boop <githubbot@vadim.me>
2024-07-18 23:30:06 +0200

    Solution 60 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8ri42qob Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp:29: 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp: In function ‘int main()’: 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp:39:20: error: ‘by_length’ was not decla red in this scope    39 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 61c0d12d-03e4-4d04-b15b-136d14f3220c.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,24 +1,14 @@
-#include <algorithm>
 #include <vector>
-#include <string>
 #include <map>
-
-using namespace std;
-
-vector<string> by_length(vector<int> arr);
+#include <string>
+#include <cassert>
 
 bool issame(vector<string> a, vector<string> b);
 
+vector<string> by_length(vector<int> arr);
+
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 vector<string> by_length(vector<int> arr){
@@ -47,5 +37,6 @@ vector<string> by_length(vector<int> arr){
 
 int main() {
     assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    
     return 0;
 }
