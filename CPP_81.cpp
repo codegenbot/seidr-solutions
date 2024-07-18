b85d2a109d2d29437923e617ec7667c94cae10d2
beep boop <githubbot@vadim.me>
2024-07-19 01:12:42 +0200

    Solution 89 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6wk0gwp7 Prompt:       |                              ^~~~~~ cf70a07f-8382-4bec-b04c-8c825b65769e.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cf70a07f-8382-4bec-b04c-8c825b65769e.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cf70a07f-8382-4bec-b04c-8c825b65769e.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cf70a07f-8382-4bec-b04c-8c825b65769e.cpp:21: cf70a07f-8382-4bec-b04c-8c825b65769e.cpp: In function ‘int main()’: cf70a07f-8382-4bec-b04c-8c825b65769e.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ cf70a07f-8382-4bec-b04c-8c825b65769e.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b){
+bool issame(std::vector<std::string> a,std::vector<std::string> b){
     return a == b;
 }
 
@@ -27,6 +27,6 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
+    assert (issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
     return 0;
 }
