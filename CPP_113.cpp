c0af0befe3bd70e7d56a1f090ef15a155400f2d2
beep boop <githubbot@vadim.me>
2024-07-18 20:56:28 +0200

    Solution 48 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/5nmcqdef Prompt: In file included from 0d995f57-23ea-4ef9-8049-472db8880756.cpp:36: 0d995f57-23ea-4ef9-8049-472db8880756.cpp: In function ‘int main()’: 0d995f57-23ea-4ef9-8049-472db8880756.cpp:46:20: error: ‘odd_count’ was not decla red in this scope; did you mean ‘solution_name::odd_count’?    46 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ 0d995f57-23ea-4ef9-8049-472db8880756.cpp:6:9: note: ‘solution_name::odd_count’ d eclared here     6 |     int odd_count(const std::vector<std::string>& strings) {       |         ^~~~~~~~~ In file included from 0d995f57-23ea-4ef9-8049-472db8880756.cpp:36: 0d995f57-23ea-4ef9-8049-472db8880756.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    47 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    48 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    49 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    50 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -28,7 +28,7 @@ namespace solution_name {
 int main() {
     assert(solution_name::issame(solution_name::odd_count({"271", "137", "314"}),
                                   { "the number of odd elements 3\nthe string 271 of the input.",
-                                    "the number of odd elements 2\nthe string 137 of the input.",
-                                    "the number of odd elements 2\nthe string 314 of the input."}));
+                                    "the number of odd elements 3\nthe string 137 of the input.",
+                                    "the number of odd elements 3\nthe string 314 of the input."}));
     return 0;
 }
