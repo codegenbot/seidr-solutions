76c9a3bd48ee9dff483a2be3ae4eb7dd7989f684
beep boop <githubbot@vadim.me>
2024-07-18 16:55:20 +0200

    Solution 60 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5z59ourz Prompt:    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:37: ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp: In function ‘int main()’: ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:47:20: error: ‘sort_third’ was not decl ared in this scope    47 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -4,25 +4,19 @@
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> sorted_indices;
+vector<int> sort_third(vector<int> l);
+
+vector<int> sort_third(vector<int> l){
+    vector<int> sorted_indices;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
             sorted_indices.push_back(l[i]);
         }
     }
-    std::sort(sorted_indices.begin(), sorted_indices.end());
+    sort(sorted_indices.begin(), sorted_indices.end());
 
     int sorted_idx = 0;
     for (int i = 0; i < l.size(); ++i) {
@@ -30,17 +24,15 @@ std::vector<int> sort_third(std::vector<int> l) {
             l[i] = sorted_indices[sorted_idx++];
         }
     }
-
+    
     return l;
 }
 
 int main() {
-    std::vector<int> input = {5, 2, 9, 3, 7, 6, 1, 4, 8};
-    std::vector<int> expected_output = {1, 2, 9, 4, 7, 6, 5, 3, 8};
-    std::vector<int> output = sort_third(input);
-    assert(issame(output, expected_output));
+    vector<int> input = {7, 3, 2, 4, 9, 1, 5, 8, 6};
+    vector<int> expected_output = {2, 3, 1, 4, 9, 5, 6, 8, 7};
+
+    vector<int> result = sort_third(input);
 
     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-
-    return 0;
 }
