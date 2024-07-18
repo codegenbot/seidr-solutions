f2dc97245cb3f2c45af791a60690c3ca74132cc2
beep boop <githubbot@vadim.me>
2024-07-18 23:32:21 +0200

    Solution 52 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/blp13r65 Prompt: in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ae71c487-b5da-4273-a33c-6006c781f450.cpp:9:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ae71c487-b5da-4273-a33c-6006c781f450.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ae71c487-b5da-4273-a33c-6006c781f450.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ae71c487-b5da-4273-a33c-6006c781f450.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from ae71c487-b5da-4273-a33c-6006c781f450.cpp:8: ae71c487-b5da-4273-a33c-6006c781f450.cpp: In function ‘int main()’: ae71c487-b5da-4273-a33c-6006c781f450.cpp:18:20: error: ‘sort_array’ was not decl ared in this scope    18 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,18 +1,9 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
     return a == b;
-}
-
-std::vector<int> sortVectorBasedOnBitCount(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end(), [](int a, int b) {
-        int countA = __builtin_popcount(a);
-        int countB = __builtin_popcount(b);
-        if (countA == countB) {
-            return a < b;
-        }
-        return countA < countB;
-    });
-    return arr;
 }
