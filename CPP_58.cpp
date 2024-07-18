5c7e0012044150c53f4f390036964ddc4871c9d7
beep boop <githubbot@vadim.me>
2024-07-18 17:45:49 +0200

    Solution 8 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eiay1ole Prompt:       |                    ^~~ 717987b9-b043-44d9-b4e9-759a13f5447f.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 717987b9-b043-44d9-b4e9-759a13f5447f.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 717987b9-b043-44d9-b4e9-759a13f5447f.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 717987b9-b043-44d9-b4e9-759a13f5447f.cpp:9: 717987b9-b043-44d9-b4e9-759a13f5447f.cpp: In function ‘int main()’: 717987b9-b043-44d9-b4e9-759a13f5447f.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 717987b9-b043-44d9-b4e9-759a13f5447f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,25 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iterator>
-#include <cassert>
-
-using namespace std;
-
-vector<int> common(vector<int> l1, vector<int> l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
-    vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-    result.erase(unique(result.begin(), result.end()), result.end());
-    return result;
-}
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-    return 0;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    vector<int> result;
+    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
+    result.erase(unique(result.begin(), result.end()), result.end());
+    return result;
 }
