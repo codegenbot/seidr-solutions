4b97101d29bec88e728715d33f005910adecffe6
beep boop <githubbot@vadim.me>
2024-07-19 00:16:27 +0200

    Solution 48 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8p4axrrj Prompt:    37 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 9012e396-0df5-402c-a5f0-7d345a9f47ab.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 9012e396-0df5-402c-a5f0-7d345a9f47ab.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 9012e396-0df5-402c-a5f0-7d345a9f47ab.cpp:36: 9012e396-0df5-402c-a5f0-7d345a9f47ab.cpp: In function ‘int main()’: 9012e396-0df5-402c-a5f0-7d345a9f47ab.cpp:46:20: error: ‘order_by_points’ was not  declared in this scope    46 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 9012e396-0df5-402c-a5f0-7d345a9f47ab.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -5,39 +5,40 @@
 
 using namespace std;
 
-bool comparison(int a, int b) {
-    int sumA = 0, sumB = 0;
-    if (a < 0) a = -a;
-    if (b < 0) b = -b;
-    while (a > 0) {
-        sumA += a % 10;
-        a /= 10;
-    }
-    while (b > 0) {
-        sumB += b % 10;
-        b /= 10;
-    }
-    if (sumA == sumB) {
-        return a < b;
-    }
-    return sumA < sumB;
+bool issame(vector<int> a, vector<int> b) {
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
 vector<int> order_by_points(vector<int> nums) {
-    sort(nums.begin(), nums.end(), comparison);
+    sort(nums.begin(), nums.end(), [](int a, int b) {
+        int sumA = 0, sumB = 0;
+        a = (a < 0) ? -a : a;
+        b = (b < 0) ? -b : b;
+        while (a > 0) {
+            sumA += a % 10;
+            a /= 10;
+        }
+        while (b > 0) {
+            sumB += b % 10;
+            b /= 10;
+        }
+        if (sumA == sumB) {
+            return a < b;
+        }
+        return sumA < sumB;
+    });
     return nums;
 }
 
 int main() {
     vector<int> nums = {123, 456, 789};
-
     nums = order_by_points(nums);
-
+    
     for (int num : nums) {
         cout << num << " ";
     }
 
-    assert(is_same(order_by_points({0, 6, 6, -76, -21, 23, 4}), vector<int>{-76, -21, 0, 4, 23, 6, 6});
-    
+    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+
     return 0;
 }
