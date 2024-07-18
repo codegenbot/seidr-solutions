e814a22702e8491a4b909eadf4e956d6c3ac8f77
beep boop <githubbot@vadim.me>
2024-07-18 23:26:43 +0200

    Solution 36 of CPP/113, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qkdnrvhf Prompt: e9ea7e5b-a532-4774-be15-593e5948d735.cpp:35:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    35 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ e9ea7e5b-a532-4774-be15-593e5948d735.cpp:22:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    22 | bool issame(vector<string> a, vector<string> b){       |      ^~~~~~ e9ea7e5b-a532-4774-be15-593e5948d735.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ e9ea7e5b-a532-4774-be15-593e5948d735.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main(){       |     ^~~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -5,30 +5,20 @@
 
 using namespace std;
 
-vector<string> odd_count(vector<string> lst) {
+vector<string> odd_count(vector<string> lst){
     vector<string> result;
-    for (string s : lst) {
-        int odd_count = 0;
-        for (char c : s) {
-            if ((c - '0') % 2 != 0) {
-                odd_count++;
+    for(string s : lst){
+        int count = 0;
+        for(char c : s){
+            if((c - '0') % 2 != 0){
+                count++;
             }
         }
-        result.push_back("the number of odd elements " + to_string(odd_count) + " in the string " + s + " of the input.");
+        result.push_back("the number of odd elements " + to_string(count) + " in the string " + s + " of the input.");
     }
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b) {
+bool are_equal(vector<string> a, vector<string> b){
     return a == b;
-}
-
-int main() {
-    assert(issame(odd_count({"271", "137", "314"}), {
-        "the number of odd elements 1 in the string 271 of the input.",
-        "the number of odd elements 2 in the string 137 of the input.",
-        "the number of odd elements 2 in the string 314 of the input."
-    }));
-    cout << "Test Passed!" << endl;
-    return 0;
 }
