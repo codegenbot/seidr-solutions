99871497d7f9a2f0bf72cec6dff37f9e86e9e09f
beep boop <githubbot@vadim.me>
2024-07-18 23:38:23 +0200

    Solution 12 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7tensaer Prompt:    28 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ cf9c733d-72df-4e0e-bd57-614b82a42344.cpp:28:36: error: expected primary-expressi on before ‘float’    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ cf9c733d-72df-4e0e-bd57-614b82a42344.cpp:28:43: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from cf9c733d-72df-4e0e-bd57-614b82a42344.cpp:27: cf9c733d-72df-4e0e-bd57-614b82a42344.cpp: In function ‘int main()’: cf9c733d-72df-4e0e-bd57-614b82a42344.cpp:37:20: error: ‘sort_even’ was not decla red in this scope    37 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ cf9c733d-72df-4e0e-bd57-614b82a42344.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,6 +1,29 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
+vector<float> sort_even(vector<float> l) {
+    vector<float> even_values;
+    vector<float> sorted_even_values;
+    vector<float> result;
+
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 2 == 0) {
+            even_values.push_back(l[i]);
+            sorted_even_values.push_back(l[i]);
+        }
+    }
+
+    sort(sorted_even_values.begin(), sorted_even_values.end());
+
+    int sorted_even_index = 0;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 2 == 0) {
+            result.push_back(sorted_even_values[sorted_even_index]);
+            sorted_even_index++;
+        } else {
+            result.push_back(l[i]);
+        }
+    }
+
+    return result;
 }
