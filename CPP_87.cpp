faf217f4a71e4d68af8084c272b0f68231d7f8ab
beep boop <githubbot@vadim.me>
2024-07-18 18:29:16 +0200

    Solution 52 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ix1epbny Prompt:       |                                   ^~~~~~ 992211a6-d94e-407c-bb3c-81a004eab010.cpp:21:42: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 992211a6-d94e-407c-bb3c-81a004eab010.cpp:21:49: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 992211a6-d94e-407c-bb3c-81a004eab010.cpp:21:56: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 992211a6-d94e-407c-bb3c-81a004eab010.cpp:20: 992211a6-d94e-407c-bb3c-81a004eab010.cpp: In function ‘int main()’: 992211a6-d94e-407c-bb3c-81a004eab010.cpp:33:20: error: ‘get_row’ was not declare d in this scope    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 992211a6-d94e-407c-bb3c-81a004eab010.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -6,8 +6,8 @@ bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
     return (a == b);
 }
 
-std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
-    std::vector<std::vector<int>> result;
+vector<vector<int>> get_row(vector<vector<int>> lst, int x){
+    vector<vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
             if (lst[i][j] == x) {
@@ -15,7 +15,7 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
             }
         }
     }
-    std::sort(result.begin(), result.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
+    sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b) {
         if (a[0] != b[0]) {
             return a[0] < b[0];
         } else {
