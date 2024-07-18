42729419be60d76aa426ad2f84fe6de8d57bf110
beep boop <githubbot@vadim.me>
2024-07-18 16:25:23 +0200

    Solution 16 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1189bgqw Prompt:       |                    ^~~ 77a640da-066e-4baf-aefa-62b552631c73.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 77a640da-066e-4baf-aefa-62b552631c73.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77a640da-066e-4baf-aefa-62b552631c73.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77a640da-066e-4baf-aefa-62b552631c73.cpp:15: 77a640da-066e-4baf-aefa-62b552631c73.cpp: In function ‘int main()’: 77a640da-066e-4baf-aefa-62b552631c73.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 77a640da-066e-4baf-aefa-62b552631c73.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
-using namespace std;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
 
-vector<int> factorize(int n){
-    vector<int> factors;
+std::vector<int> factorize(int n){
+    std::vector<int> factors;
     for(int i = 2; i * i <= n; ++i){
         while(n % i == 0){
             factors.push_back(i);
@@ -16,13 +17,4 @@ vector<int> factorize(int n){
         factors.push_back(n);
     }
     return factors;
-}
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
-    return 0;
 }
