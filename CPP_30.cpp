b038059092addddbf3859c8d357dafb8a7ba0fa5
beep boop <githubbot@vadim.me>
2024-07-18 23:38:41 +0200

    Solution 24 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/f7zc41ee Prompt:       |                    ^~~~~ a0e51f7a-3662-4530-b84c-68c374b4689d.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a0e51f7a-3662-4530-b84c-68c374b4689d.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a0e51f7a-3662-4530-b84c-68c374b4689d.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a0e51f7a-3662-4530-b84c-68c374b4689d.cpp:11: a0e51f7a-3662-4530-b84c-68c374b4689d.cpp: In function ‘int main()’: a0e51f7a-3662-4530-b84c-68c374b4689d.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ a0e51f7a-3662-4530-b84c-68c374b4689d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,11 +1,7 @@
 #include <vector>
 
-bool issame(std::vector<float> a, std::vector<float> b){
-    return a == b;
-}
-
-std::vector<float> get_positive(std::vector<float> l){
-    std::vector<float> result;
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
@@ -13,3 +9,11 @@ std::vector<float> get_positive(std::vector<float> l){
     }
     return result;
 }
+
+bool issame(vector<float> a, vector<float> b){
+    return a == b;
+}
+
+int main(){
+    assert(get_positive({}) == {});
+}  
