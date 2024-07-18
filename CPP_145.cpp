7b660738a5b134127b0f7d4bdee3d691c9f12d6f
beep boop <githubbot@vadim.me>
2024-07-19 00:10:07 +0200

    Solution 8 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8p4axrrj Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 853c231c-6096-47d3-8198-218d5ad359d0.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 853c231c-6096-47d3-8198-218d5ad359d0.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 853c231c-6096-47d3-8198-218d5ad359d0.cpp:20: 853c231c-6096-47d3-8198-218d5ad359d0.cpp: In function ‘int main()’: 853c231c-6096-47d3-8198-218d5ad359d0.cpp:30:20: error: ‘order_by_points’ was not  declared in this scope    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 853c231c-6096-47d3-8198-218d5ad359d0.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,8 +1,12 @@
-vector<int> order_by_points(vector<int> nums) {
+bool issame(const vector<int>& a, const vector<int>& b){
+    return a == b;
+}
+
+vector<int> order_by_points(const vector<int>& nums){
     sort(nums.begin(), nums.end(), [](int a, int b) {
         int sumA = 0, sumB = 0;
-        a = abs(a);
-        b = abs(b);
+        if (a < 0) a = -a;
+        if (b < 0) b = -b;
         while (a > 0) {
             sumA += a % 10;
             a /= 10;
@@ -12,9 +16,14 @@ vector<int> order_by_points(vector<int> nums) {
             b /= 10;
         }
         if (sumA == sumB) {
-            return a < b;
+            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
         }
         return sumA < sumB;
     });
     return nums;
+}
+
+int main() {
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
+    return 0;
 }
