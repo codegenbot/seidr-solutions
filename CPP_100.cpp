64e2d04efc2961e5b1b585bea939d17c871be612
beep boop <githubbot@vadim.me>
2024-07-18 23:17:34 +0200

    Solution 12 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6hklkxak Prompt:       |                    ^~~ 56ab4dff-a767-47ec-894e-87356af8b171.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 56ab4dff-a767-47ec-894e-87356af8b171.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 56ab4dff-a767-47ec-894e-87356af8b171.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 56ab4dff-a767-47ec-894e-87356af8b171.cpp:15: 56ab4dff-a767-47ec-894e-87356af8b171.cpp: In function ‘int main()’: 56ab4dff-a767-47ec-894e-87356af8b171.cpp:25:20: error: ‘make_a_pile’ was not dec lared in this scope    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 56ab4dff-a767-47ec-894e-87356af8b171.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,14 +1,12 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
+vector<int> make_a_pile(int n);
 
-std::vector<int> make_a_pile(int n){
-    std::vector<int> stones;
+vector<int> make_a_pile(int n){
+    vector<int> stones;
     stones.push_back(n);
-    for(int i = 1; i < n; ++i){
+    for(int i=1; i<n; i++){
         if(n % 2 == 0){
             n += 2;
         } else {
@@ -19,7 +17,14 @@ std::vector<int> make_a_pile(int n){
     return stones;
 }
 
-int main(){
-    assert (issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    return 0;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
