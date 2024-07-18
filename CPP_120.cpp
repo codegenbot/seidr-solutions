bb06e1ac1c5084f2d21334e9f8bbc35c523d5871
beep boop <githubbot@vadim.me>
2024-07-18 23:28:58 +0200

    Solution 12 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/lfpcqq2x Prompt:       |                    ^~~ b4200043-4b4b-4643-bebe-90761dfaa01d.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b4200043-4b4b-4643-bebe-90761dfaa01d.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b4200043-4b4b-4643-bebe-90761dfaa01d.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b4200043-4b4b-4643-bebe-90761dfaa01d.cpp:6: b4200043-4b4b-4643-bebe-90761dfaa01d.cpp: In function ‘int main()’: b4200043-4b4b-4643-bebe-90761dfaa01d.cpp:16:20: error: ‘maximum’ was not declare d in this scope    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ b4200043-4b4b-4643-bebe-90761dfaa01d.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,21 +1,8 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cassert>
+#include <vector>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-vector<int> maximum(vector<int> arr, int k){
-    sort(arr.begin(), arr.end(), greater<int>());
+std::vector<int> solve(std::vector<int> arr, int k) {
+    std::sort(arr.begin(), arr.end(), std::greater<int>());
     arr.resize(k);
     return arr;
 }
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
-}
-
