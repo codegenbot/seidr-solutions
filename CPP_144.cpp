0a0dcb72aeb1d3d9d82cef9260d562e9bc636e24
beep boop <githubbot@vadim.me>
2024-07-18 20:39:26 +0200

    Solution 3 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/n9gb3r4d Prompt:       |                     ^ 1d004c8e-1634-45b2-b5ae-80e421672732.cpp:4:30: error: ‘n’ was not declared in th is scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                              ^ 1d004c8e-1634-45b2-b5ae-80e421672732.cpp:4:16: error: ‘stoi’ was not declared in  this scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                ^~~~ 1d004c8e-1634-45b2-b5ae-80e421672732.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (num1 * num2) % (den1 * den2) == 0;       |     ^~~~~~ 1d004c8e-1634-45b2-b5ae-80e421672732.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 1d004c8e-1634-45b2-b5ae-80e421672732.cpp:9: 1d004c8e-1634-45b2-b5ae-80e421672732.cpp: In function ‘int main()’: 1d004c8e-1634-45b2-b5ae-80e421672732.cpp:11:13: error: ‘simplify’ was not declar ed in this scope    11 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -6,16 +6,20 @@ bool simplify(const std::string& x, const std::string& n) {
     int den1 = std::stoi(x.substr(x.find('/') + 1));
     int num2 = std::stoi(n.substr(0, n.find('/')));
     int den2 = std::stoi(n.substr(n.find('/') + 1));
-
+    
     return (num1 * num2) % (den1 * den2) == 0;
 }
 
 int main() {
-    std::string x, n;
-    std::cin >> x >> n;
+    std::string input1, input2;
+    std::cout << "Enter two fractions (in the form 'numerator/denominator'): ";
+    std::cin >> input1 >> input2;
 
-    bool result = simplify(x, n);
-    std::cout << std::boolalpha << result << std::endl;
+    if (simplify(input1, input2)) {
+        std::cout << "The fractions can be simplified.\n";
+    } else {
+        std::cout << "The fractions cannot be simplified.\n";
+    }
 
     return 0;
 }
