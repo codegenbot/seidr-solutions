1cad7804efebf293a53f123025de43b6d1200f5a
beep boop <githubbot@vadim.me>
2024-07-18 20:45:34 +0200

    Solution 25 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/wvhpg5or Prompt:       |                              ^~~~~~ 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp:13:37: error: ‘string’ was not declared  in this scope    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp:12: 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp: In function ‘int main()’: 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp:22:20: error: ‘reverse_delete’ was not declared in this scope    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 480a9cbf-da9a-4f7a-8070-c275d9a3e690.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,13 +3,15 @@
 #include <string>
 #include <vector>
 
-pair<string, string> removeChars(string s, const string& c) {
+vector<string> solve(const string& s, const string& c) {
+    string temp = s;
+
     for (char ch : c) {
-        s.erase(remove(s.begin(), s.end(), ch), s.end());
+        temp.erase(remove(temp.begin(), temp.end(), ch), temp.end());
     }
 
-    string result = s;
-    string reversed = s;
+    string result = temp;
+    string reversed = temp;
     reverse(reversed.begin(), reversed.end());
 
     return {result, (result == reversed) ? "True" : "False"};
