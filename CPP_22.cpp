3d8f716f07c0e17493ed74fa42ecd1af4a0476ed
beep boop <githubbot@vadim.me>
2024-07-18 23:19:28 +0200

    Solution 6 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/axy73ih4 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ca5d79f5-a0df-4e9d-bfa8-e9b139b0fe21.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ca5d79f5-a0df-4e9d-bfa8-e9b139b0fe21.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ca5d79f5-a0df-4e9d-bfa8-e9b139b0fe21.cpp:11: ca5d79f5-a0df-4e9d-bfa8-e9b139b0fe21.cpp: In function ‘int main()’: ca5d79f5-a0df-4e9d-bfa8-e9b139b0fe21.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ca5d79f5-a0df-4e9d-bfa8-e9b139b0fe21.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,5 +1,9 @@
 vector<int> filter_integers(list_any values);
-
 bool issame(vector<int> a, vector<int> b);
 
-using list_any = std::list<boost::any>;
+class list_any : public std::list<boost::any> {};
+
+template<typename ValueType>
+ValueType any_cast(const boost::any& operand) {
+    return boost::any_cast<ValueType>(operand);
+}
