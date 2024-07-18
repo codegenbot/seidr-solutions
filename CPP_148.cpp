5e502ea814cf321e75c7ebb0273a95ddd5f361b7
beep boop <githubbot@vadim.me>
2024-07-19 00:15:52 +0200

    Solution 34 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/xe16pmgz Prompt:       |                              ^~~~~~ 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp:30: 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp: In function ‘int main()’: 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp:40:20: error: ‘bf’ was not declared in this scope    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 22691804-64e6-4ee9-96c3-6a8e4232988e.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -5,13 +5,23 @@ bool issame(string planet1, string planet2) {
 vector<string> bf(string planet1, string planet2);
 
 int main() {
-    string planet1, planet2;
-    cin >> planet1 >> planet2;
+    string start, end;
+    cin >> start >> end;
+    
+    if (issame(start, end)) {
+        cout << "Cannot select the same planet." << endl;
+        return 0;
+    }
 
-    vector<string> result = bf(planet1, planet2);
-
-    for (string planet : result) {
-        cout << planet << " ";
+    vector<string> result = bf(start, end);
+    
+    if (result.empty()) {
+        cout << "Invalid planets selected." << endl;
+    } else {
+        cout << "Planets between " << start << " and " << end << ":" << endl;
+        for (const string& planet : result) {
+            cout << planet << endl;
+        }
     }
 
     return 0;
@@ -23,10 +33,10 @@ vector<string> bf(string planet1, string planet2) {
 
     int start = -1, end = -1;
     for (int i = 0; i < planets.size(); ++i) {
-        if (issame(planets[i], planet1)) {
+        if (planets[i] == planet1) {
             start = i;
         }
-        if (issame(planets[i], planet2)) {
+        if (planets[i] == planet2) {
             end = i;
         }
     }
