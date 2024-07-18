67d1b2efd8f6a4e7f1bcc2468256b27c6b7eb00c
beep boop <githubbot@vadim.me>
2024-07-19 00:40:26 +0200

    Solution 84 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vio88gbt Prompt: 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp:4:13: error: ‘vector’ was not declared in this scope     4 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp:4:20: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp:4:27: error: ‘vector’ was not declared in this scope     4 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp:3: 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp: In function ‘int main()’: 9edfdba6-71c7-4ca1-b501-cdb80cc9b756.cpp:13:20: error: ‘incr_list’ was not decla red in this scope    13 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,20 +1,15 @@
 #include <vector>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     if (a.size() != b.size()) {
         return false;
     }
 
-    for (int i = 0; i < a.size(); ++i) {
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
 
     return true;
-}
-
-int main() {
-    // Test cases can be added here
-    return 0;
 }
