c5137515b4b506cbbd428054ad9572b8fa1e3c78
beep boop <githubbot@vadim.me>
2024-07-18 22:24:55 +0200

    Solution 43 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1dkqprw2 Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f7e3346e-9436-4b91-adc9-3b8883531381.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f7e3346e-9436-4b91-adc9-3b8883531381.cpp:16:12: error: expected primary-expressi on before ‘float’    16 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from f7e3346e-9436-4b91-adc9-3b8883531381.cpp:14: f7e3346e-9436-4b91-adc9-3b8883531381.cpp:17:31: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    17 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                               |       |                               <brace-enclosed initializer list> f7e3346e-9436-4b91-adc9-3b8883531381.cpp:3:24: note:   initializing argument 1 o f ‘int has_close_elements(int)’     3 | int has_close_elements(const vector<float>& numbers, float threshold) {       |                        ^~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -11,9 +11,3 @@ int has_close_elements(const vector<float>& numbers, float threshold) {
     }
     return false;
 }
-
-int main() {
-    vector<float> numbers = {1.1, 2.2, 3.1, 4.1, 5.1};
-    has_close_elements(numbers, 0.5);
-    return 0;
-}
