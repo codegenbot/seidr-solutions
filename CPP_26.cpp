d50628891cc7dc25e1db9a84b6f76d220266135a
beep boop <githubbot@vadim.me>
2024-07-19 00:37:30 +0200

    Solution 16 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6roebfrf Prompt: in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 99891d52-81d5-4d72-8d3e-b540d8ff32c3.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 99891d52-81d5-4d72-8d3e-b540d8ff32c3.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 99891d52-81d5-4d72-8d3e-b540d8ff32c3.cpp:5: 99891d52-81d5-4d72-8d3e-b540d8ff32c3.cpp: In function ‘int main()’: 99891d52-81d5-4d72-8d3e-b540d8ff32c3.cpp:15:20: error: ‘remove_duplicates’ was n ot declared in this scope    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 99891d52-81d5-4d72-8d3e-b540d8ff32c3.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,8 +1,20 @@
-vector<int> result;
-    for (int num : numbers) {
-        if (count(numbers.begin(), numbers.end(), num) == 1) {
-            result.push_back(num);
-        }
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
+}
+
+vector<int> remove_duplicates(vector<int> numbers) {
+    sort(numbers.begin(), numbers.end());
+    numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
+    return numbers;
+}
+
+int main() {
+    vector<int> numbers = {1, 2, 2, 3, 4, 4, 4, 5};
+    vector<int> unique_numbers = remove_duplicates(numbers);
+
+    for (int num : unique_numbers) {
+        cout << num << " ";
     }
-    return result;
+
+    return 0;
 }
