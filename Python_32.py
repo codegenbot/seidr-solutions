646efb5323eb7265d89ca8bb0552d013ffe6cdff
beep boop <githubbot@vadim.me>
2024-07-19 01:16:25 +0200

    Solution 11 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/x4xeuz6b Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,6 @@
-from scipy.optimize import fsolve
-
-
 def find_zero(xs: list):
-    def poly(x):
-        return sum([coeff * x**i for i, coeff in enumerate(xs)])
-
-    return fsolve(poly, 0)[0]
+    n = len(xs) // 2
+    ys = [xs[i] / xs[-1] for i in range(n)]  # rescale coefficients by largest one
+    if n == 1:
+        return -ys[0]
+    return -sum(ys[:-1])
