c14534ae1b76afed751fd9f4aff532325fee9d2e
beep boop <githubbot@vadim.me>
2024-07-18 23:06:32 +0200

    Solution 5 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dobtw2jd Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 87fac407-de41-446f-b02c-b706af5174a2.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 87fac407-de41-446f-b02c-b706af5174a2.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 87fac407-de41-446f-b02c-b706af5174a2.cpp:22: 87fac407-de41-446f-b02c-b706af5174a2.cpp: In function ‘int main()’: 87fac407-de41-446f-b02c-b706af5174a2.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 87fac407-de41-446f-b02c-b706af5174a2.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -15,10 +15,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 vector<int> count_up_to(int n);
 
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-}
-
 vector<int> count_up_to(int n){
     vector<int> primes;
     if(n < 2) {
@@ -39,3 +35,5 @@ vector<int> count_up_to(int n){
     }
     return primes;
 }
+
+assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
