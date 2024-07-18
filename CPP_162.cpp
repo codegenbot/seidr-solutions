9eeeda5332ea2807b69ad1f48701bcc8cd06c922
beep boop <githubbot@vadim.me>
2024-07-18 17:45:55 +0200

    Solution 50 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rp2h1ogq Prompt: /usr/bin/ld: /tmp/cc5Cokwd.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0x8e): undefined reference to `O PENSSL_init_crypto' /usr/bin/ld: 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0x93): undefined re ference to `EVP_md5' /usr/bin/ld: 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0x9f): undefined re ference to `EVP_MD_CTX_new' /usr/bin/ld: 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0xc4): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0xfd): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0x11a): undefined r eference to `EVP_DigestFinal_ex' /usr/bin/ld: 52050488-0cd0-480a-b604-182f3fd5ae16.cpp:(.text+0x129): undefined r eference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,15 +1,29 @@
-#include <openssl/md5.h>
-#include <openssl/sha.h>
-#include <string>
-#include <iostream>
+#pragma comment(lib, "libssl.lib")
+#pragma comment(lib, "libcrypto.lib")
 
-std::string string_to_md5(const std::string& input) {
-    unsigned char digest[MD5_DIGEST_LENGTH];
-    MD5((const unsigned char*)input.c_str(), input.length(), digest);
-
+std::string string_to_md5(const std::string& text) {
+    if (text.empty()) {
+        return "None";
+    }
+    
+    EVP_MD_CTX* mdctx;
+    const EVP_MD* md;
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+    
+    OpenSSL_add_all_digests();
+    
+    md = EVP_md5();
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+    EVP_MD_CTX_free(mdctx);
+    
     char mdString[33];
-    for(int i = 0; i < 16; i++)
-         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+    for (int i = 0; i < md_len; i++) {
+        sprintf(&mdString[i * 2], "%02x", md_value[i]);
+    }
 
     return std::string(mdString);
 }
