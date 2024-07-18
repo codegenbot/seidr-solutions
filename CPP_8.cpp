92ca9bc030b57fc475a9c4167ac60a1f705b4d3f
beep boop <githubbot@vadim.me>
2024-07-18 22:33:42 +0200

    Solution 9 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/f8b9ftcs Prompt:       |                    ^~~ 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp:9: 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp: In function ‘int main()’: 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp:19:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 6ecff136-b3d4-40c8-ac0b-2f0063285789.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,15 +1,14 @@
 #include <vector>
-
 bool issame(std::vector<int> a, std::vector<int> b) {
-    auto sum_product = [](const std::vector<int>& numbers) {
-        int sum = 0;
-        int product = 1;
-        for (int num : numbers) {
-            sum += num;
-            product *= num;
-        }
-        return std::make_pair(sum, product);
-    };
-
     return sum_product(a) == sum_product(b);
+}
+
+std::pair<int, int> sum_product(std::vector<int> numbers) {
+    int sum = 0;
+    int product = 1;
+    for (int num : numbers) {
+        sum += num;
+        product *= num;
+    }
+    return std::make_pair(sum, product);
 }
