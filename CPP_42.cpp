e5ff2c826775a2a5c0f7055a886c53a31a4ef502
beep boop <githubbot@vadim.me>
2024-07-18 17:27:01 +0200

    Solution 97 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zroziyya Prompt:       |                           ^~~~~~ 86a96e48-927d-405d-aef0-707591d5b4e4.cpp:21:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 86a96e48-927d-405d-aef0-707591d5b4e4.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 86a96e48-927d-405d-aef0-707591d5b4e4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 86a96e48-927d-405d-aef0-707591d5b4e4.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 86a96e48-927d-405d-aef0-707591d5b4e4.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 86a96e48-927d-405d-aef0-707591d5b4e4.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,11 +1,18 @@
-bool issame(std::vector<int> a, std::vector<int> b) {
-        if (a.size() != b.size())
-            return false;
-        
-        for (size_t i = 0; i < a.size(); ++i) {
-            if (a[i] != b[i])
-                return false;
-        }
-        
-        return true;
+#include <cassert>
+#include <vector>
+
+std::vector<int> incr_list(std::vector<int> l) {
+    for (int &num : l) {
+        num++;
     }
+    return l;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    return 0;
+}
