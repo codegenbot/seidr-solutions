b43a6d3325bec061f8ef56020d57a1f222e6bf59
beep boop <githubbot@vadim.me>
2024-07-19 01:44:11 +0200

    Solution 14 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/po6k3bng Prompt: Complete the following code given the task description and function signature.

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,7 +1,9 @@
-vector<int> generate_integers(int a, int b) {
+vector<int> generate_integers(int a, int b){
     vector<int> result;
-    for (int i = a%2 == 0 ? a : a+1; i <= b; i += 2) {
-        result.push_back(i);
+    for(int i=a; i<=b; i++){
+        if(i % 2 == 0){
+            result.push_back(i);
+        }
     }
     return result;
 }
