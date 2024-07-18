524848ba07ea537c2e857c265c0ef37d1ba1318d
beep boop <githubbot@vadim.me>
2024-07-18 23:21:45 +0200

    Solution 26 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/naxhnw29 Prompt:       |                    ^~~ c4873fe9-50a2-410a-a796-c52232787216.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c4873fe9-50a2-410a-a796-c52232787216.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c4873fe9-50a2-410a-a796-c52232787216.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c4873fe9-50a2-410a-a796-c52232787216.cpp:21: c4873fe9-50a2-410a-a796-c52232787216.cpp: In function ‘int main()’: c4873fe9-50a2-410a-a796-c52232787216.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ c4873fe9-50a2-410a-a796-c52232787216.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -5,8 +5,8 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-vector<int> unique_digits(std::vector<int> x){
-    std::vector<int> result;
+vector<int> unique_digits(vector<int> x){
+    vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
@@ -21,7 +21,7 @@ vector<int> unique_digits(std::vector<int> x){
             result.push_back(num);
         }
     }
-    std::sort(result.begin(), result.end());
+    sort(result.begin(), result.end());
     return result;
 }
 
