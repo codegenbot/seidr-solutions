3496863e2e275cd70829ded77feb63bb39d27ef9
beep boop <githubbot@vadim.me>
2024-07-19 01:03:34 +0200

    Solution 80 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8mcjvope Prompt:       |                    ^~~ 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp:9: 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp: In function ‘int main()’: 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 7c0dedcc-0ed4-4255-a602-9e11aa35a34a.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,16 +1,23 @@
-#include <algorithm>
 #include <vector>
-#include <set>
-#include <cassert>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
 
-bool issame(std::vector<int> a, std::vector<int> b);
-
-bool issame(std::vector<int> a, std::vector<int> b) {
+template <typename T>
+bool issame(std::vector<T> a, std::vector<T> b) {
     return a == b;
 }
 
-int main() {
-    assert(issame(std::vector<int>({4, 3, 2, 8}), std::vector<int>{}));
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
+    std::vector<int> result;
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
+    result.erase(std::unique(result.begin(), result.end()), result.end());
+    return result;
+}
 
+int main() {
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
