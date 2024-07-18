c8de4eba532095b0ecdd2284d63cb570a5d1a9d2
beep boop <githubbot@vadim.me>
2024-07-18 18:38:57 +0200

    Solution 55 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/nys0ah8z Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -12,10 +12,10 @@ def separate_paren_groups(paren_string: str) -> List[str]:
             level += 1
         elif char == ")":
             level -= 1
-            if level < 0:
+            if level >= 0:
+                group += char
+            if level == -1:
                 result.append(group)
                 group = ""
-            else:
-                group += char
 
     return result
