a40285ba3de1820333d3910b37fb563067845fdd
beep boop <githubbot@vadim.me>
2024-07-19 01:59:26 +0200

    Solution 2 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qieuxi48 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 118033cf-fd83-4397-ba43-2a3d937b6a15.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 118033cf-fd83-4397-ba43-2a3d937b6a15.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 118033cf-fd83-4397-ba43-2a3d937b6a15.cpp:11: 118033cf-fd83-4397-ba43-2a3d937b6a15.cpp: In function ‘int main()’: 118033cf-fd83-4397-ba43-2a3d937b6a15.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 118033cf-fd83-4397-ba43-2a3d937b6a15.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,27 +1,17 @@
-#include <boost/any.hpp>
-#include <vector>
+bool issame(vector<int> a, vector<int> b);
 
-std::vector<int> filter_integers(std::vector<boost::any> values);
+vector<int> filter_integers(list_any values);
 
-bool issame(std::vector<int> a, std::vector<int> b);
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
-std::vector<int> filter_integers(std::vector<boost::any> values){
-    std::vector<int> result;
+vector<int> filter_integers(list_any values){
+    vector<int> result;
     for (const auto &val : values) {
         if (val.type() == typeid(int)) {
             result.push_back(boost::any_cast<int>(val));
         }
     }
     return result;
-}
-
-int main() {
-    std::vector<boost::any> values = {1, 2, "hello", 3.14, 5};
-    std::vector<int> filtered_values = filter_integers(values);
-    assert(issame(filtered_values, std::vector<int>{1, 2, 5}));
-    return 0;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
 }
