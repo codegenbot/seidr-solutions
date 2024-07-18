669303240dfa3b4a8b86bf5adeae4e35e1c88202
beep boop <githubbot@vadim.me>
2024-07-18 17:55:55 +0200

    Solution 20 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ajuz77c4 Prompt:    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0eeb092f-0883-4598-9c0e-d6d23f38217e.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0eeb092f-0883-4598-9c0e-d6d23f38217e.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0eeb092f-0883-4598-9c0e-d6d23f38217e.cpp:24: 0eeb092f-0883-4598-9c0e-d6d23f38217e.cpp: In function ‘int main()’: 0eeb092f-0883-4598-9c0e-d6d23f38217e.cpp:34:20: error: ‘count_up_to’ was not dec lared in this scope    34 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 0eeb092f-0883-4598-9c0e-d6d23f38217e.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,10 +1,19 @@
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+#include <vector>
+
+vector<int> count_up_to(int n){
+    vector<int> numbers;
+    for(int i = 2; i <= n; ++i){
+        numbers.push_back(i);
+    }
+    return numbers;
 }
 
-vector<int> count_up_to(int n);
-
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    return 0;
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); ++i){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
