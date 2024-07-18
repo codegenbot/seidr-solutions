422575ef6718299f8c18c329eaeceaaf955a75da
beep boop <githubbot@vadim.me>
2024-07-18 19:17:32 +0200

    Solution 16 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kjxyryni Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c943055-d697-41aa-a8da-b6b485656 473.py", line 6     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,13 +1,7 @@
-def generate_list_with_delimeter(numbers, delimeter):
+def fill_with_delimeter(numbers, delimeter):
     result = []
     for num in numbers[:-1]:
         result.extend([num, delimeter])
     if numbers:
         result.append(numbers[-1])
     return result
-
-# Example usage
-input_numbers = [1, 2, 3, 4, 5]
-input_delimeter = '-'
-output_list = generate_list_with_delimeter(input_numbers, input_delimeter)
-print(output_list)
