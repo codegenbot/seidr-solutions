fe2b68332b3b5ea0f0222d5d321d306009466bb2
beep boop <githubbot@vadim.me>
2024-07-19 01:16:32 +0200

    Solution 12 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/x4xeuz6b Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,6 +1,1 @@
-def find_zero(xs: list):
-    n = len(xs) // 2
-    ys = [xs[i] / xs[-1] for i in range(n)]  # rescale coefficients by largest one
-    if n == 1:
-        return -ys[0]
-    return -sum(ys[:-1])
+return -xs[0] / xs[1]
