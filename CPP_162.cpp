39c40fc329bbacb4490b72108317ae12e5d7d2da
beep boop <githubbot@vadim.me>
2024-07-18 21:39:58 +0200

    Solution 18 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pv213i5a Prompt: 143e2427-bd33-4d7f-bec4-293396aa6f9c.cpp: In function ‘std::string string_to_md5 (const string&)’: 143e2427-bd33-4d7f-bec4-293396aa6f9c.cpp:11:26: error: ‘MD5_DIGEST_LENGTH’ was n ot declared in this scope    11 |     unsigned char digest[MD5_DIGEST_LENGTH];       |                          ^~~~~~~~~~~~~~~~~ 143e2427-bd33-4d7f-bec4-293396aa6f9c.cpp:12:61: error: ‘digest’ was not declared  in this scope    12 |     EVP_Digest((unsigned char*)text.c_str(), text.length(), digest, NULL , EVP_md5(), NULL);       |                                                             ^~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,5 +1,9 @@
 #include <string>
+#include <openssl/md5.h>
 #include <openssl/evp.h>
+
+#define MD5_DIGEST_LENGTH 16
+
 using namespace std;
 
 string string_to_md5(const string& text) {
@@ -7,17 +11,11 @@ string string_to_md5(const string& text) {
         return "None";
     }
 
-    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit(mdctx, EVP_md5());
-    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-
     unsigned char digest[MD5_DIGEST_LENGTH];
-    EVP_DigestFinal(mdctx, digest, nullptr);
-
-    EVP_MD_CTX_free(mdctx);
+    EVP_Digest((unsigned char*)text.c_str(), text.length(), digest, NULL, EVP_md5(), NULL);
 
     char mdString[33];
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    for (int i = 0; i < 16; i++) {
         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
 
