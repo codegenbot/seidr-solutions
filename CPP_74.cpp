ceb12b61c664d906cd5b3640cf8068dcc6d4bc61
beep boop <githubbot@vadim.me>
2024-07-18 18:25:00 +0200

    Solution 56 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/k3k7lvut Prompt:       |                              ^~~~~~ 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp:17: 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp: In function ‘int main()’: 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 425f5e4c-4123-479a-9ee9-eade14c8d065.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,19 +1,19 @@
 #include <string>
 
-bool issame(vector<string> lst1, vector<string> lst2) {
+bool issame(vector<string> a, vector<string> b) {
     int total_chars1 = 0;
-    for (string str : lst1) {
+    for (string str : a) {
         total_chars1 += str.length();
     }
 
     int total_chars2 = 0;
-    for (string str : lst2) {
+    for (string str : b) {
         total_chars2 += str.length();
     }
 
     if (total_chars1 < total_chars2) {
-        return lst1;
+        return true;
     } else {
-        return lst2;
+        return false;
     }
 }
