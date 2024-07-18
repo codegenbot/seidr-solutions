b149d8928e9aca055d05b872c039bc2cceea9224
beep boop <githubbot@vadim.me>
2024-07-18 17:24:23 +0200

    Solution 13 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6dtqijt9 Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,4 +1,4 @@
 def decode_shift(s: str):
     if not s.islower():
         return "Input should contain only lowercase alphabetic characters"
-    return "".join([chr(((ord(ch) - 7 - ord("a")) % 26) + ord("a") for ch in s])
+    return "".join([chr(((ord(ch) - 7 - ord("a")) % 26) + ord("a")) for ch in s])
