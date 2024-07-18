db55f7df95f64f611821732ca3904a43cf8e1932
beep boop <githubbot@vadim.me>
2024-07-18 21:02:30 +0200

    Solution 14 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/xooa38es Prompt:       |                                     ^~~~~~ 05a58848-2b74-44fc-a023-8db10c535f5c.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 05a58848-2b74-44fc-a023-8db10c535f5c.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 05a58848-2b74-44fc-a023-8db10c535f5c.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 05a58848-2b74-44fc-a023-8db10c535f5c.cpp:22: 05a58848-2b74-44fc-a023-8db10c535f5c.cpp: In function ‘int main()’: 05a58848-2b74-44fc-a023-8db10c535f5c.cpp:32:20: error: ‘bf’ was not declared in this scope    32 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 05a58848-2b74-44fc-a023-8db10c535f5c.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -2,24 +2,12 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
-
 bool issame(const vector<string>& a, const vector<string>& b) {
-    if(a.size() != b.size()){
-        return false;
-    }
-    
-    for(size_t i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    
-    return true;
+    return a == b;
 }
 
 int main() {
-    assert(issame(vector<string>{"Jupiter", "Makemake"}, vector<string>{}));
+    assert (issame({"Jupiter", "Makemake"}, {}));
     
     return 0;
 }
