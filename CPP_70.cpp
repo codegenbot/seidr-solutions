b99db52a13499365a952d616f0c5ef4a7361aeeb
beep boop <githubbot@vadim.me>
2024-07-18 18:10:42 +0200

    Solution 30 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/j5df363o Prompt:       |                    ^~~ 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp:14: 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp: In function ‘int main()’: 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp:24:20: error: ‘strange_sort_list’ was n ot declared in this scope    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 0a660357-ec2b-4452-b44c-fd130e3a5011.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -8,7 +8,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
-    sort(lst.begin(), lst.end());
+    std::sort(lst.begin(), lst.end());
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
         result.push_back(lst[left++]);
