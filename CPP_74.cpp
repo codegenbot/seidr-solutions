e606697f0ffd54c249dd854d56b21be116ab0822
beep boop <githubbot@vadim.me>
2024-07-19 01:05:16 +0200

    Solution 54 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/d9f1hhh0 Prompt: 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp:17: 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp: In function ‘int main()’: 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp: In function ‘int total_chars(int)’: 2e5354a9-6406-4445-8af5-3d9ae7e28e4c.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,16 +1,6 @@
-bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
-}
+bool issame(const vector<string>& a, const vector<string>& b);
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2);
-
-int total_chars(const vector<string>& lst) {
-    int total = 0;
-    for (const string& str : lst) {
-        total += str.size();
-    }
-    return total;
-}
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     if (total_chars(lst1) < total_chars(lst2)) {
@@ -20,7 +10,4 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     }
 }
 
-int main() {
-    assert(total_match({"this"}, {}).empty());
-    return 0;
-}
+assert(total_match({"this"}, {}).empty());
