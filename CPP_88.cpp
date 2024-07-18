d46b01cb89fab29c26d10daa20dfab9e5e0bb34c
beep boop <githubbot@vadim.me>
2024-07-18 18:26:59 +0200

    Solution 27 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ulq9g9g7 Prompt:       |                    ^~~ a029bee0-64ba-4387-866f-ee6378e636c6.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a029bee0-64ba-4387-866f-ee6378e636c6.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a029bee0-64ba-4387-866f-ee6378e636c6.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a029bee0-64ba-4387-866f-ee6378e636c6.cpp:10: a029bee0-64ba-4387-866f-ee6378e636c6.cpp: In function ‘int main()’: a029bee0-64ba-4387-866f-ee6378e636c6.cpp:20:20: error: ‘sort_array’ was not decl ared in this scope    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ a029bee0-64ba-4387-866f-ee6378e636c6.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -3,15 +3,14 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b);
-vector<int> sort_array(vector<int> array);
+bool issame(std::vector<int> a, std::vector<int> b);
 
-vector<int> sort_array(vector<int> array) {
+std::vector<int> sort_array(std::vector<int> array) {
     if(array.empty()) return array;
     if((array.front() + array.back()) % 2 == 0) {
-        sort(array.rbegin(), array.rend());
+        std::sort(array.rbegin(), array.rend());
     } else {
-        sort(array.begin(), array.end());
+        std::sort(array.begin(), array.end());
     }
     return array;
 }
