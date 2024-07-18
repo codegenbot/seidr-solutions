26340cdcab734f26b652cce76ce91c788d575c00
beep boop <githubbot@vadim.me>
2024-07-19 00:28:56 +0200

    Solution 81 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/g9solkns Prompt: -54 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3d6f561-57fd-4733-aef7-3a1eedb80 c61.py", line 17, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3d6f561-57fd-4733-aef7-3a1eedb80 c61.py", line 13, in check     double_the_difference(lst) == odd_sum                                   ^^^^^^^ NameError: name 'odd_sum' is not defined

--- Python_151.py
+++ Python_151.py
@@ -3,7 +3,6 @@ def double_the_difference(lst):
     even_sum = sum(x for x in lst if x > 0 and x % 2 == 0)
     return 2 * (odd_sum - even_sum**2)
 
-
 lst = [1, 2, 3, 4, 5]
 odd_sum = sum(x for x in lst if x > 0 and x % 2 != 0)
 even_sum = sum(x for x in lst if x > 0 and x % 2 == 0)
