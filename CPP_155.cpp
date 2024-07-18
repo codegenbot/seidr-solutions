f0cc335cab63d362869c5b0520ed70685b452c60
beep boop <githubbot@vadim.me>
2024-07-19 00:12:05 +0200

    Solution 16 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zlj3vahe Prompt:       |                    ^~~ 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp:13: 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp: In function ‘int main()’: 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 3c50c9a5-7f92-40ed-9c93-1804ac32a870.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,8 +1,10 @@
 #include <vector>
+#include <string>
+#include <cassert>
 
-vector<int> even_odd_count(int num) {
-    vector<int> result(2, 0);
-    string numStr = to_string(abs(num));
+std::vector<int> even_odd_count(int num) {
+    std::vector<int> result(2, 0);
+    std::string numStr = std::to_string(abs(num));
     for (char c : numStr) {
         if ((c - '0') % 2 == 0) {
             result[0]++;
@@ -11,4 +13,13 @@ vector<int> even_odd_count(int num) {
         }
     }
     return result;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(even_odd_count(0), {1, 0}));
+    return 0;
 }
