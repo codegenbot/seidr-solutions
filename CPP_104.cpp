440c4e9445a1476d0fbaf6652fc8b07e44c73e05
beep boop <githubbot@vadim.me>
2024-07-18 20:37:58 +0200

    Solution 18 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vb9dagx5 Prompt:       |           ^~~~~~ In file included from a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main(){       |     ^~~~ In file included from a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp:34: a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp: In function ‘int main()’: a9c1ff6d-95fb-4d8c-92da-edd5530c3c3d.cpp:44:20: error: ‘unique_digits’ was not d eclared in this scope    44 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -2,19 +2,17 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(int a, int b) {
+bool issame(int a, int b){
     return a < b;
 }
 
-vector<int> unique_digits(vector<int> x);
-
-vector<int> unique_digits(vector<int> x){
-    vector<int> result;
+std::vector<int> unique_digits(std::vector<int> x){
+    std::vector<int> result;
     for(int num : x){
         bool hasEvenDigit = false;
         int temp = num;
         while(temp > 0){
-            if(temp % 2 == 0){
+            if(temp % 10 % 2 == 0){
                 hasEvenDigit = true;
                 break;
             }
@@ -29,6 +27,6 @@ vector<int> unique_digits(vector<int> x){
 }
 
 int main(){
-    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
+    assert(issame(unique_digits({135, 103, 31}) , {31, 135}));
     return 0;
 }
