6471e815c2b7b25905db1b4e3323bccd055c57a4
beep boop <githubbot@vadim.me>
2024-07-18 23:32:37 +0200

    Solution 5 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/f7zc41ee Prompt:       |                    ^~~~~ d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp:11: d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp: In function ‘int main()’: d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ d2b5d1d7-9150-486f-a4ca-2203721d13ba.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,15 @@
 #include <vector>
 
-bool issame(float a, float b){
-    return a == b;
+bool issame vector<float> (vector<float> l1, vector<float> l2){
+    if (l1.size() != l2.size()) {
+        return false;
+    }
+    for (int i = 0; i < l1.size(); i++) {
+        if (l1[i] != l2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<float> get_positive(vector<float> l){
@@ -15,8 +23,11 @@ vector<float> get_positive(vector<float> l){
 }
 
 int main(){
-    vector<float> v = {1.5, -2.3, 4.0, -5.2};
-    assert(issame(3.0, 3.0));
-    assert(get_positive(v).size() == 2);
+    vector<float> l1 = {1.2, -3.4, 5.6, -7.8};
+    vector<float> l2 = {-1.2, 3.4, -5.6, 7.8};
+
+    assert(issame vector<float>(l1, l2));
+    assert(get_positive(l1).size() == 2);
+
     return 0;
 }
