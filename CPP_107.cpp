e2e2bbd596dfae8b3384fa461b649781e8d64664
beep boop <githubbot@vadim.me>
2024-07-18 20:39:22 +0200

    Solution 17 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3maj1lfp Prompt:       |                           ^~~~~~ 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp:23: 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp: In function ‘int main()’: 271320d6-216a-4764-92d2-d71e0ddf08b9.cpp:33:39: error: could not convert ‘even_o dd_palindrome(1)’ from ‘std::pair<int, int>’ to ‘std::vector<int>’    33 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ~~~~~~~~~~~~~~~~~~~^~~       |                                       |       |                                       std::pair<int, int>

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,23 +1,3 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::pair<int, int> even_odd_palindrome(int n) {
-    int even = 0, odd = 0;
-    for (int i = 1; i <= n; ++i) {
-        std::string num = std::to_string(i);
-        std::string rev = num;
-        std::reverse(rev.begin(), rev.end());
-        if (num == rev) {
-            if (i % 2 == 0)
-                even++;
-            else
-                odd++;
-        }
-    }
-    return {even, odd};
-}
