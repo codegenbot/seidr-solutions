d4df3bb13946908c9bd3a72727ce475990a05344
beep boop <githubbot@vadim.me>
2024-07-19 00:45:25 +0200

    Solution 87 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6roebfrf Prompt: in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 14bc68e3-b8aa-4af8-9196-13d629b39310.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 14bc68e3-b8aa-4af8-9196-13d629b39310.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 14bc68e3-b8aa-4af8-9196-13d629b39310.cpp:5: 14bc68e3-b8aa-4af8-9196-13d629b39310.cpp: In function ‘int main()’: 14bc68e3-b8aa-4af8-9196-13d629b39310.cpp:15:20: error: ‘remove_duplicates’ was n ot declared in this scope    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 14bc68e3-b8aa-4af8-9196-13d629b39310.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -7,17 +7,17 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 vector<int> remove_duplicates(vector<int> numbers) {
-    numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
+    numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
 }
 
 int main() {
     vector<int> input = {1, 2, 2, 3, 4, 4, 5};
     vector<int> result = remove_duplicates(input);
-
+    
     for (int num : result) {
-        cout << num << " ";
+        std::cout << num << " ";
     }
-
+    
     return 0;
 }
