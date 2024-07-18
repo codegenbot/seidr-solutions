53e1566fa96600df9556dc0960425ff27c1d8a8e
beep boop <githubbot@vadim.me>
2024-07-18 16:18:03 +0200

    Solution 2 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qyqj0ek0 Prompt:       |                    ^~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:21: ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp: In function ‘int main()’: ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,19 +1,3 @@
-vector<int> unique_digits(vector<int> x){
-    vector<int> result;
-    for(int num : x){
-        bool hasEvenDigit = false;
-        int temp = num;
-        while(temp > 0){
-            if((temp % 10) % 2 == 0){
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        if(!hasEvenDigit){
-            result.push_back(num);
-        }
-    }
-    sort(result.begin(), result.end());
-    return result;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
