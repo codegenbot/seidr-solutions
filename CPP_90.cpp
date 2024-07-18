7e7c0a9f7ef578aaafd5ce1bf187b3f109c587d1
beep boop <githubbot@vadim.me>
2024-07-19 01:07:28 +0200

    Solution 16 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1f9kff0z Prompt:     3 |     for (int i = 1; i < lst.size(); ++i) {       |     ^~~ d0192a2d-8e40-4bac-822e-993d99eebb80.cpp:3:21: error: ‘i’ does not name a type     3 |     for (int i = 1; i < lst.size(); ++i) {       |                     ^ d0192a2d-8e40-4bac-822e-993d99eebb80.cpp:3:37: error: expected unqualified-id be fore ‘++’ token     3 |     for (int i = 1; i < lst.size(); ++i) {       |                                     ^~ d0192a2d-8e40-4bac-822e-993d99eebb80.cpp:11:5: error: expected unqualified-id be fore ‘return’    11 |     return -1;       |     ^~~~~~ d0192a2d-8e40-4bac-822e-993d99eebb80.cpp:12:1: error: expected declaration befor e ‘}’ token    12 | }       | ^ In file included from d0192a2d-8e40-4bac-822e-993d99eebb80.cpp:14: d0192a2d-8e40-4bac-822e-993d99eebb80.cpp: In function ‘int main()’: d0192a2d-8e40-4bac-822e-993d99eebb80.cpp:16:13: error: ‘next_smallest’ was not d eclared in this scope    16 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ^~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,13 +1,13 @@
-sort(lst.begin(), lst.end());
-int count = 1;
-int prev = lst[0];
-for (int i = 1; i < lst.size(); ++i) {
-    if (lst[i] != prev) {
-        count++;
-        prev = lst[i];
+int next_smallest(vector<int> lst) {
+    sort(lst.begin(), lst.end());
+    int count = 1;
+    for (int i = 1; i < lst.size(); ++i) {
+        if (lst[i] != lst[i - 1]) {
+            count++;
+        }
         if (count == 2) {
             return lst[i];
         }
     }
+    return -1;
 }
-return None;
