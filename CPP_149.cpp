86a9e31d8e8bcc322d0d6d2fbe73646e2c330b11
beep boop <githubbot@vadim.me>
2024-07-19 01:51:10 +0200

    Solution 87 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ehl7gpnv Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2f59f1f4-2020-4863-ab14-76211238959a.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2f59f1f4-2020-4863-ab14-76211238959a.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2f59f1f4-2020-4863-ab14-76211238959a.cpp:11: 2f59f1f4-2020-4863-ab14-76211238959a.cpp: In function ‘int main()’: 2f59f1f4-2020-4863-ab14-76211238959a.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 2f59f1f4-2020-4863-ab14-76211238959a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,8 @@
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+    return accumulate(a.begin(), a.end(), 0) == accumulate(b.begin(), b.end(), 0);
 }
 
-vector<string> sorted_list_sum(vector<string> lst) {
+vector<string> sorted_list_sum(const vector<string>& lst) {
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
         if (a.length() == b.length()) {
@@ -14,6 +14,6 @@ vector<string> sorted_list_sum(vector<string> lst) {
 }
 
 int main() {
-    // Main function implementation
+    // Main function logic here
     return 0;
 }
