77face0b5e9c7455c90a786d97d9742de55aeb7b
beep boop <githubbot@vadim.me>
2024-07-18 17:48:19 +0200

    Solution 58 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rp2h1ogq Prompt: 55fe7d7c-3e6d-45b5-871f-c112d5dc59b7.cpp:4:6: error: ‘string’ in namespace ‘std’  does not name a type     4 | std::string string_to_md5(const std::string& text) {       |      ^~~~~~ 55fe7d7c-3e6d-45b5-871f-c112d5dc59b7.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | #pragma comment(lib, "libssl.lib") In file included from 55fe7d7c-3e6d-45b5-871f-c112d5dc59b7.cpp:31: 55fe7d7c-3e6d-45b5-871f-c112d5dc59b7.cpp: In function ‘int main()’: 55fe7d7c-3e6d-45b5-871f-c112d5dc59b7.cpp:33:13: error: ‘string_to_md5’ was not d eclared in this scope    33 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+
 #include <string>
-#include <openssl/md5.h>
 #include <openssl/evp.h>
 
 std::string string_to_md5(const std::string& text) {
@@ -13,7 +12,7 @@ std::string string_to_md5(const std::string& text) {
     unsigned char md_value[EVP_MAX_MD_SIZE];
     unsigned int md_len;
     
-    OpenSSL_add_all_algorithms();
+    OpenSSL_add_all_digests();
     
     md = EVP_md5();
     mdctx = EVP_MD_CTX_new();
@@ -21,7 +20,7 @@ std::string string_to_md5(const std::string& text) {
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
     EVP_DigestFinal_ex(mdctx, md_value, &md_len);
     EVP_MD_CTX_free(mdctx);
-
+    
     char mdString[33];
     for (int i = 0; i < md_len; i++) {
         sprintf(&mdString[i * 2], "%02x", md_value[i]);
