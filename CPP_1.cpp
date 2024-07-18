37ba0b686345894020f3ec8964bbe4e48a509a7e
beep boop <githubbot@vadim.me>
2024-07-18 22:30:04 +0200

    Solution 58 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r5k3h4ja Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ c89d614a-59d4-4dff-a2c8-5a2d6151705b.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c89d614a-59d4-4dff-a2c8-5a2d6151705b.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from c89d614a-59d4-4dff-a2c8-5a2d6151705b.cpp:28: c89d614a-59d4-4dff-a2c8-5a2d6151705b.cpp: In function ‘int main()’: c89d614a-59d4-4dff-a2c8-5a2d6151705b.cpp:38:20: error: ‘separate_paren_groups’ w as not declared in this scope    38 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ c89d614a-59d4-4dff-a2c8-5a2d6151705b.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -2,16 +2,18 @@
 #include <string>
 #include <cassert>
 
-bool issame(vector<string> a, vector<string> b);
-
-vector<string> separate_paren_groups(string paren_string);
-
-int main() {
-    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
-}
-
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 vector<string> separate_paren_groups(string paren_string) {
