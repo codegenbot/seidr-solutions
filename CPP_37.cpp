8af68f345f1b8a329586804ab507e1f1884fb4d9
beep boop <githubbot@vadim.me>
2024-07-18 23:43:03 +0200

    Solution 37 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7tensaer Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 20ef60d5-5e5d-42dd-8231-c516f21370b3.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 20ef60d5-5e5d-42dd-8231-c516f21370b3.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 20ef60d5-5e5d-42dd-8231-c516f21370b3.cpp:23: 20ef60d5-5e5d-42dd-8231-c516f21370b3.cpp: In function ‘int main()’: 20ef60d5-5e5d-42dd-8231-c516f21370b3.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 20ef60d5-5e5d-42dd-8231-c516f21370b3.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -28,7 +28,7 @@ vector<float> sort_even(vector<float> l) {
 
 int main() {
     // Test the sort_even function
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
     // Define the issame function to compare two vectors
     auto issame = [](vector<float> a, vector<float> b) { return a == b; };
 
