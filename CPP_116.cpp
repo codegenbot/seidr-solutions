51e55f863f5fabff9363f24f8bb2a0ec6002bb9f
beep boop <githubbot@vadim.me>
2024-07-18 23:23:53 +0200

    Solution 12 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/blp13r65 Prompt:       |                    ^~~ 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp:12: 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp: In function ‘int main()’: 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 545e9c40-e54e-4cea-99f3-8e946e71d6af.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,20 +1,9 @@
-#include <algorithm>
 #include <vector>
-#include <cassert>
 
-std::vector<int> sort_array(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end(), [](int a, int b) {
-        int countA = __builtin_popcount(a);
-        int countB = __builtin_popcount(b);
-        if (countA == countB) {
-            return a < b;
-        }
-        return countA < countB;
-    });
-    return arr;
+bool issame(std::vector<int> a std::vector<int> b){
+    return a == b; 
 }
 
-int main() {
-    assert(sort_array({2, 4, 8, 16, 32}) == std::vector<int>{2, 4, 8, 16, 32});
-    return 0;
+int main(){
+    assert(issame(std::sort({2,4,8,16,32}), {2, 4, 8, 16, 32}));
 }
