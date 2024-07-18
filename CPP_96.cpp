68a586010d517d1ef522dd1b12e3e9a984c05046
beep boop <githubbot@vadim.me>
2024-07-18 18:38:50 +0200

    Solution 47 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9ohodpjd Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 43dd0d92-6a7d-4878-af00-e7223b13adba.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 43dd0d92-6a7d-4878-af00-e7223b13adba.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 43dd0d92-6a7d-4878-af00-e7223b13adba.cpp:16: 43dd0d92-6a7d-4878-af00-e7223b13adba.cpp: In function ‘int main()’: 43dd0d92-6a7d-4878-af00-e7223b13adba.cpp:26:20: error: ‘count_up_to’ was not dec lared in this scope    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 43dd0d92-6a7d-4878-af00-e7223b13adba.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,8 +1,8 @@
 #include <vector>
 #include <cassert>
 
-vector<int> count_up_to(int n) {
-    vector<int> result;
+std::vector<int> count_up_to(int n) {
+    std::vector<int> result;
     for (int i = 2; i < n; ++i) {
         bool is_prime = true;
         for (int j = 2; j * j <= i; ++j) {
@@ -18,20 +18,21 @@ vector<int> count_up_to(int n) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
+    
     for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
 int main() {
-    vector<int> expected = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    std::vector<int> expected = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
     assert(issame(count_up_to(101), expected));
-    return 0;
 }
