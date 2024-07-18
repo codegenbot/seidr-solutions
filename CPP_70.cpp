9ea2ba03eb7742a298bd6582f1aadaa4deca9b1d
beep boop <githubbot@vadim.me>
2024-07-19 01:54:54 +0200

    Solution 12 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7jpulwa4 Prompt:       |                    ^~~ 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp:15: 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp: In function ‘int main()’: 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp:25:20: error: ‘strange_sort_list’ was n ot declared in this scope    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 0782b89e-7599-4cff-90b7-f7c15ddcacf1.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,26 +1,27 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
 
-std::vector<int> strange_sort_list(std::vector<int> lst){
-    std::vector<int> result;
+std::vector<int> strange_sort_list(const std::vector<int>& lst) {
     std::sort(lst.begin(), lst.end());
-    int i = 0, j = lst.size() - 1;
-    while (i <= j) {
-        result.push_back(lst[i]);
-        if (i != j) {
-            result.push_back(lst[j]);
+    std::vector<int> res;
+    int left = 0, right = lst.size() - 1;
+    while (left <= right) {
+        res.push_back(lst[left]);
+        if (left != right) {
+            res.push_back(lst[right]);
         }
-        i++;
-        j--;
+        left++;
+        right--;
     }
-    return result;
+    return res;
 }
 
-int main(){
+int main() {
     assert(issame(strange_sort_list({111111}), {111111}));
     return 0;
 }
