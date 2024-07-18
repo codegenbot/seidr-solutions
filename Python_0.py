95580cee36fdab54ab1308810857d296a0cfee3f
beep boop <githubbot@vadim.me>
2024-07-18 16:44:39 +0200

    Solution 4 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/06f6r86g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/42937215-61e5-4818-8d1b-1fa10e080 71b.py", line 4     return True     ^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_0.py
+++ Python_0.py
@@ -3,4 +3,4 @@ def check_numbers(numbers, threshold):
     for i in range(len(numbers) - 1):
         if abs(numbers[i] - numbers[i + 1]) < threshold:
             return True
-    return False  
+    return False
