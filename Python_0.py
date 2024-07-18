5a1b72260f36b13a6d9be4054aba1236991b4126
beep boop <githubbot@vadim.me>
2024-07-18 16:44:37 +0200

    Solution 2 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/inirkppn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f880f6c0-e751-4ba8-bccb-f0b37dadd 5e8.py", line 2     for i in range(len(numbers) - 1): IndentationError: unexpected indent

--- Python_0.py
+++ Python_0.py
@@ -1,6 +1,5 @@
-def within_threshold(numbers, threshold):
-    numbers.sort()
-    for i in range(len(numbers) - 1):
-        if abs(numbers[i] - numbers[i + 1]) < threshold:
-            return True
-    return False
+numbers.sort()
+for i in range(len(numbers) - 1):
+    if abs(numbers[i] - numbers[i + 1]) < threshold:
+        return True
+return False
