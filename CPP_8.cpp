849efc63e4a3346d9e6a9ab86a443d7c0775b848
beep boop <githubbot@vadim.me>
2024-07-18 17:40:05 +0200

    Solution 4 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zxly4lgs Prompt:       |                    ^~~ 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp:9: 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp: In function ‘int main()’: 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp:19:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 00b3d917-3047-43a2-9e9d-c2f6b3294c9e.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,6 +1,24 @@
-int sum = 0, product = 1;
-for (int num : numbers) {
-    sum += num;
-    product *= num;
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+vector<int> sum_product(const vector<int>& numbers) {
+    int sum = 0;
+    int product = 1;
+    for (int num : numbers) {
+        sum += num;
+        product *= num;
+    }
+    return {sum, product};
 }
-return {sum, product};
+
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(sum_product({10}), {10, 10}));
+    return 0;
+}
