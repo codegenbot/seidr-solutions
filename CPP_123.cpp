529d6210d528a8bf2a87a8530b341e3c72ce49ad
beep boop <githubbot@vadim.me>
2024-07-18 18:04:21 +0200

    Solution 7 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/qgjlx1sr Prompt:       |                    ^~~ 623a0cef-c596-4bed-9313-529181b7d455.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 623a0cef-c596-4bed-9313-529181b7d455.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 623a0cef-c596-4bed-9313-529181b7d455.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 623a0cef-c596-4bed-9313-529181b7d455.cpp:17: 623a0cef-c596-4bed-9313-529181b7d455.cpp: In function ‘int main()’: 623a0cef-c596-4bed-9313-529181b7d455.cpp:27:20: error: ‘get_odd_collatz’ was not  declared in this scope    27 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 623a0cef-c596-4bed-9313-529181b7d455.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,32 +1,30 @@
 #include <vector>
+#include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
+    if (a.size() != b.size()) {
         return false;
-        
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
     }
-    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
     return true;
 }
 
 vector<int> get_odd_collatz(int n) {
     vector<int> result;
-    
     while (n != 1) {
         if (n % 2 != 0) {
             result.push_back(n);
         }
-        
         if (n % 2 == 0) {
             n /= 2;
         } else {
             n = 3 * n + 1;
         }
     }
-    
     result.push_back(1);
     sort(result.begin(), result.end());
     return result;
