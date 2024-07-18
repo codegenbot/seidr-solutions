58a0bd93d89a0ddbc93bd600f8d26252800651df
beep boop <githubbot@vadim.me>
2024-07-19 01:45:01 +0200

    Solution 4 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7jpulwa4 Prompt:       |                    ^~~ 2979c928-05cc-4347-89ea-180dec769c7e.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2979c928-05cc-4347-89ea-180dec769c7e.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2979c928-05cc-4347-89ea-180dec769c7e.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2979c928-05cc-4347-89ea-180dec769c7e.cpp:16: 2979c928-05cc-4347-89ea-180dec769c7e.cpp: In function ‘int main()’: 2979c928-05cc-4347-89ea-180dec769c7e.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 2979c928-05cc-4347-89ea-180dec769c7e.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,14 +1,34 @@
-vector<int> strange_sort_list(vector<int> lst){
-    vector<int> result;
-    sort(lst.begin(), lst.end());
-    int left = 0, right = lst.size() - 1;
-    while(left <= right){
-        result.push_back(lst[left]);
-        if(left != right){
-            result.push_back(lst[right]);
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> strange_sort_list(std::vector<int> lst);
+
+int main() {
+    std::vector<int> input = {4, 2, 7, 1, 8};
+    std::vector<int> expected = {1, 8, 2, 7, 4};
+    
+    std::vector<int> result = strange_sort_list(input);
+    
+    assert(issame(result, expected));
+    return 0;
+}
+
+std::vector<int> strange_sort_list(std::vector<int> lst){
+    std::vector<int> result;
+    std::sort(lst.begin(), lst.end());
+    int i = 0, j = lst.size() - 1;
+    while (i <= j) {
+        result.push_back(lst[i]);
+        if (i != j) {
+            result.push_back(lst[j]);
         }
-        left++;
-        right--;
+        i++;
+        j--;
     }
     return result;
 }
