ea7e12a3f5364825c3826e044dbff70a56eec7a8
beep boop <githubbot@vadim.me>
2024-07-19 00:13:45 +0200

    Solution 22 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4032b5pp Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ adc83016-2324-4c66-9797-8a5aa5480417.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ adc83016-2324-4c66-9797-8a5aa5480417.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from adc83016-2324-4c66-9797-8a5aa5480417.cpp:13: adc83016-2324-4c66-9797-8a5aa5480417.cpp: In function ‘int main()’: adc83016-2324-4c66-9797-8a5aa5480417.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ adc83016-2324-4c66-9797-8a5aa5480417.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
-#include <vector>
-
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
+    if(a.size() != b.size()) {
         return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i])
+    }
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
@@ -24,16 +23,5 @@ vector<int> largest_smallest_integers(vector<int> lst) {
 }
 
 int main() {
-    vector<int> test_input = {3, -7, 9, 2, -5, 6};
-    vector<int> expected_output = {-5, 2};
-
-    vector<int> result = largest_smallest_integers(test_input);
-
-    if (issame(result, expected_output)) {
-        std::cout << "Test Passed\n";
-    } else {
-        std::cout << "Test Failed\n";
-    }
-
     return 0;
 }
