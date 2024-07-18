d62856a1b4442b0d9ea617e21861154a022695f0
beep boop <githubbot@vadim.me>
2024-07-19 00:10:45 +0200

    Solution 5 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ladb6pks Prompt:    20 | return "None";       | ^~~~~~ 50476527-3c53-4c0f-9c38-5881d325b848.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from 50476527-3c53-4c0f-9c38-5881d325b848.cpp:23: 50476527-3c53-4c0f-9c38-5881d325b848.cpp: In function ‘int main()’: 50476527-3c53-4c0f-9c38-5881d325b848.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 50476527-3c53-4c0f-9c38-5881d325b848.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 50476527-3c53-4c0f-9c38-5881d325b848.cpp:25:37: error: ‘compare_one’ was not dec lared in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -25,3 +25,7 @@ boost::any compare_one(boost::any a, boost::any b) {
     }
     return boost::any();
 }
+
+int main() {
+    assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "None");
+}
