11e0fa2400b5dcb5f5ae7f5b0fb9e9d52fa2c162
beep boop <githubbot@vadim.me>
2024-07-18 18:43:48 +0200

    Solution 80 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xt1mp1yb Prompt:       |                    ^~~ de7e8995-c848-4d43-b54c-122c461753ee.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ de7e8995-c848-4d43-b54c-122c461753ee.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ de7e8995-c848-4d43-b54c-122c461753ee.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from de7e8995-c848-4d43-b54c-122c461753ee.cpp:20: de7e8995-c848-4d43-b54c-122c461753ee.cpp: In function ‘int main()’: de7e8995-c848-4d43-b54c-122c461753ee.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ de7e8995-c848-4d43-b54c-122c461753ee.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,23 +1,13 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
-int main() {
-    std::vector<int> x = {123, 456, 789};
-    std::vector<int> result;
-
+vector<int> unique_digits(vector<int> x) {
+    vector<int> result;
     for (int num : x) {
         int temp = num;
         bool hasEvenDigit = false;
         while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
+            if (temp % 2 == 0) {
                 hasEvenDigit = true;
                 break;
             }
@@ -27,7 +17,6 @@ int main() {
             result.push_back(num);
         }
     }
-
     sort(result.begin(), result.end());
     return result;
 }
