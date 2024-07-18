f188230e27ed7ff6ae55742185f8e3313b9cc502
beep boop <githubbot@vadim.me>
2024-07-18 20:44:43 +0200

    Solution 32 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3maj1lfp Prompt:       |                           ^~~~~~ a26df627-4048-40c7-bd27-034b395e7646.cpp:35:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a26df627-4048-40c7-bd27-034b395e7646.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a26df627-4048-40c7-bd27-034b395e7646.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a26df627-4048-40c7-bd27-034b395e7646.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a26df627-4048-40c7-bd27-034b395e7646.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ a26df627-4048-40c7-bd27-034b395e7646.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -5,12 +5,12 @@
 
 std::vector<int> even_odd_palindrome(int n) {
     int even = 0, odd = 0;
-
+    
     for (int i = 1; i <= n; ++i) {
         std::string num = std::to_string(i);
         std::string rev = num;
         std::reverse(rev.begin(), rev.end());
-
+        
         if (num == rev) {
             if (i % 2 == 0)
                 even++;
@@ -18,10 +18,15 @@ std::vector<int> even_odd_palindrome(int n) {
                 odd++;
         }
     }
-
+    
     return {even, odd};
 }
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
+}
+
+int main() {
+    assert(issame(even_odd_palindrome(1), {0, 1}));
+    return 0;
 }
