fd1d2d8915635ddba0844b3e2293361b4b1e4bc7
beep boop <githubbot@vadim.me>
2024-07-18 17:52:54 +0200

    Solution 15 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9m2kqutf Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 564c201e-2558-4d2d-b958-8697e30ab35f.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 564c201e-2558-4d2d-b958-8697e30ab35f.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 564c201e-2558-4d2d-b958-8697e30ab35f.cpp:19: 564c201e-2558-4d2d-b958-8697e30ab35f.cpp: In function ‘int main()’: 564c201e-2558-4d2d-b958-8697e30ab35f.cpp:29:20: error: ‘largest_smallest_integer s’ was not declared in this scope    29 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 564c201e-2558-4d2d-b958-8697e30ab35f.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -23,7 +23,7 @@ bool issame(std::vector<int>& a, std::vector<int>& b) {
 }
 
 int main() {
-    assert(issame(largest_smallest_integer({-6, -4, -4, -3, -100, 1}), {-3, 1});
+    assert(issame(largest_smallest_integer({-6, -4, -4, -3, -100, 1}), {-3, 1}));
     
     return 0;
 }
