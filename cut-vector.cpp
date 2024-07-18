83b55fd64b7e442b3df6ad344e8868d412b1c567
beep boop <githubbot@vadim.me>
2024-07-18 18:20:45 +0200

    Solution 19 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/gcs5wfsa Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { 61fb24d5-3289-4f3d-8e33-0e24fbddff67.cpp:23:20: error: ‘abs’ was not declared in  this scope    23 |         int diff = abs(leftSum - rightSum);       |                    ^~~ 61fb24d5-3289-4f3d-8e33-0e24fbddff67.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << nums[i] << endl;       |         ^~~~ 61fb24d5-3289-4f3d-8e33-0e24fbddff67.cpp:31:28: error: ‘endl’ was not declared i n this scope    31 |         cout << nums[i] << endl;       |                            ^~~~ 61fb24d5-3289-4f3d-8e33-0e24fbddff67.cpp:33:5: error: ‘cout’ was not declared in  this scope    33 |     cout << 0 << endl;       |     ^~~~ 61fb24d5-3289-4f3d-8e33-0e24fbddff67.cpp:33:18: error: ‘endl’ was not declared i n this scope    33 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,26 +1,25 @@
 #include <climits>
 #include <iostream>
-#include <vector>
 
 int main() {
     std::vector<int> nums;
     int num;
-
+    
     while (std::cin >> num) {
         nums.push_back(num);
     }
-
+    
     int n = nums.size();
     int totalSum = 0;
     for (int i = 0; i < n; i++) {
         totalSum += nums[i];
     }
-
+    
     int leftSum = 0;
     int rightSum = totalSum;
     int minDiff = INT_MAX;
     int cutIndex = -1;
-
+    
     for (int i = 0; i < n; i++) {
         leftSum += nums[i];
         rightSum -= nums[i];
@@ -30,11 +29,11 @@ int main() {
             cutIndex = i;
         }
     }
-
+    
     for (int i = 0; i <= cutIndex; i++) {
         std::cout << nums[i] << std::endl;
     }
     std::cout << 0 << std::endl;
-
+    
     return 0;
 }
