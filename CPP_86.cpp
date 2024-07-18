1d9030948242d3afc85ac7ecab69785c34610a69
beep boop <githubbot@vadim.me>
2024-07-18 17:11:36 +0200

    Solution 9 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/c0hqnsbl Prompt: 43800968-b355-41a8-8a91-687a94a95317.cpp:12:9: error: expected constructor, dest ructor, or type conversion before ‘(’ token    12 |     sort(word.begin(), word.end());       |         ^ 43800968-b355-41a8-8a91-687a94a95317.cpp:13:5: error: ‘result’ does not name a t ype    13 |     result += word;       |     ^~~~~~ 43800968-b355-41a8-8a91-687a94a95317.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return result;       |     ^~~~~~ 43800968-b355-41a8-8a91-687a94a95317.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from 43800968-b355-41a8-8a91-687a94a95317.cpp:17: 43800968-b355-41a8-8a91-687a94a95317.cpp: In function ‘int main()’: 43800968-b355-41a8-8a91-687a94a95317.cpp:19:13: error: ‘anti_shuffle’ was not de clared in this scope    19 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -19,7 +19,7 @@ string anti_shuffle(string s) {
 }
 
 int main() {
-    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ". Hi My aemn is Meirst . Rboot How aer ? ouy");
+    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
     
     return 0;
 }
