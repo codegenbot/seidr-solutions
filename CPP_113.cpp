2ec430f2d8d88bd73c193d47e2f75a71ac8bbfb0
beep boop <githubbot@vadim.me>
2024-07-19 01:20:02 +0200

    Solution 25 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/b8px59zt Prompt:       |                                            ^ 1bb6a3cd-aea3-4470-b497-d3864cda7065.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1bb6a3cd-aea3-4470-b497-d3864cda7065.cpp:16: 1bb6a3cd-aea3-4470-b497-d3864cda7065.cpp: In function ‘int main()’: 1bb6a3cd-aea3-4470-b497-d3864cda7065.cpp:26:20: error: ‘odd_count’ was not decla red in this scope    26 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ 1bb6a3cd-aea3-4470-b497-d3864cda7065.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    30 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -2,13 +2,7 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
-vector<string> odd_count(vector<string> lst);
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
+bool issame(vector<string> a, vector<string> b);
 
 vector<string> odd_count(vector<string> lst){
     vector<string> result;
@@ -19,7 +13,7 @@ vector<string> odd_count(vector<string> lst){
                 count++;
             }
         }
-        string res = "The number of odd elements " + to_string(count) + "\nIn the string " + str + " of the input.\n";
+        string res = "The number of odd elements " + to_string(count) + "\nIn the string " + str + " of the input.";
         result.push_back(res);
     }
     return result;
