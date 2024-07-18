0d94c2c52364a35ca95d70c0c1e4a5315ec95335
beep boop <githubbot@vadim.me>
2024-07-18 17:14:30 +0200

    Solution 17 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/adfdtsfz Prompt:       |           ^~~~~~ In file included from 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp:22:49: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp:22:56: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp:21: 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp: In function ‘int main()’: 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp:34:20: error: ‘get_row’ was not declare d in this scope    34 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 5bc1e89d-46de-408a-95c6-f46914cc84e1.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
@@ -27,7 +26,7 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
 }
 
 int main(){
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
     
     return 0;
 }
