866497971debd7156f82bffb5adc0ec68d045039
beep boop <githubbot@vadim.me>
2024-07-18 17:46:02 +0200

    Solution 47 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yrznummp Prompt: n header ‘<string>’; did you forget to ‘#include <string>’? 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp:42:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    42 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp:41: 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp: In function ‘int main()’: 69d51c40-b4cf-46ba-8a0f-64b4324d882c.cpp:51:39: error: no match for call to ‘(st d::vector<int>) (const char [15])’    51 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-vector<int> parse_nested_parens(string paren_string);
-
-int main() {
-    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
-}
-
 vector<int> parse_nested_parens(string paren_string) {
     vector<int> result;
     int max_depth = 0;
@@ -33,4 +27,8 @@ vector<int> parse_nested_parens(string paren_string) {
     result.push_back(max_depth);
 
     return result;
+}
+
+int main() {
+    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
 }
