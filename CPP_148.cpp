5ddd9071a17f21bf764299bfe6ec5d06ae0415c0
beep boop <githubbot@vadim.me>
2024-07-19 01:39:09 +0200

    Solution 16 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/tycdwt7y Prompt:       |                              ^~~~~~ dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp:29: dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp: In function ‘int main()’: dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ dc771404-c7f9-45ac-a408-696ae5c44e2c.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,21 +1,36 @@
+#include <string>
+#include <vector>
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     vector<string> result;
     
-    if (find(planets.begin(), planets.end(), planet1) == planets.end() || find(planets.begin(), planets.end(), planet2) == planets.end()) {
-        return {};
+    int index1 = -1, index2 = -1;
+    for (int i = 0; i < planets.size(); ++i) {
+        if (planets[i] == planet1) {
+            index1 = i;
+        }
+        if (planets[i] == planet2) {
+            index2 = i;
+        }
     }
     
-    int startIdx = find(planets.begin(), planets.end(), planet1) - planets.begin();
-    int endIdx = find(planets.begin(), planets.end(), planet2) - planets.begin();
-    
-    if (startIdx > endIdx) {
-        swap(startIdx, endIdx);
+    if (index1 == -1 || index2 == -1) {
+        return result;
     }
     
-    for (int i = startIdx + 1; i < endIdx; ++i) {
+    int start = min(index1, index2) + 1;
+    int end = max(index1, index2);
+    
+    for (int i = start; i < end; ++i) {
         result.push_back(planets[i]);
     }
     
     return result;
 }
+
+assert(issame(bf("Jupiter", "Makemake"), vector<string>{}));
