7d9f7b1ee4199e1a809975bdf7f5be70ae204d10
beep boop <githubbot@vadim.me>
2024-07-19 01:12:35 +0200

    Solution 16 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/j62f7omk Prompt:       |                  ^~~~~ 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp:1:13: error: ‘stof’ was not declared in  this scope     1 | float num = stof(value);       |             ^~~~ 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp:2:15: error: ‘round’ was not declared i n this scope     2 |     int res = round(num);       |               ^~~~~ 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp:3:5: error: expected unqualified-id bef ore ‘return’     3 |     return res;       |     ^~~~~~ 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp:4:1: error: expected declaration before  ‘}’ token     4 | }       | ^ In file included from 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp:6: 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp: In function ‘int main()’: 5dd0a00b-c53a-4777-b179-bb476abeda59.cpp:8:13: error: ‘closest_integer’ was not declared in this scope     8 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,4 +1,10 @@
-float num = stof(value);
-    int res = round(num);
+#include <string>
+#include <cmath>
+
+int closest_integer(std::string value);
+
+int closest_integer(std::string value) {
+    float num = std::stof(value);
+    int res = std::round(num);
     return res;
 }
