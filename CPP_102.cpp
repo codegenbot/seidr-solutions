0dec22b89962421a51c0c69fdd8444b0735d46bf
beep boop <githubbot@vadim.me>
2024-07-18 18:32:20 +0200

    Solution 2 of CPP/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/niigu171 Prompt: Complete the following code given the task description and function signature.

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,9 +1,8 @@
 int max_even = -1;
-    for (int i = y; i >= x; i--) {
-        if (i % 2 == 0) {
+    for(int i = y; i >= x; i--){
+        if(i % 2 == 0){
             max_even = i;
             break;
         }
     }
     return max_even;
-}
