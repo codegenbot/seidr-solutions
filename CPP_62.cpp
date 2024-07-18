b7cce1810f3c354bcc03a7c72f5498fec58f6969
beep boop <githubbot@vadim.me>
2024-07-18 17:42:13 +0200

    Solution 8 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/o1iikb0z Prompt:       |                    ^~~~~ 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp:9: 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp: In function ‘int main()’: 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 9aaf71c4-5ca9-495f-9867-44c62710627f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,18 +1,20 @@
 #include <vector>
+#include <cassert>
+
+using namespace std;
+
+vector<float> derivative(vector<float> xs){
+    vector<float> result;
+    for(int i = 1; i < xs.size(); i++){
+        result.push_back(xs[i] - xs[i-1]);
+    }
+    return result;
+}
 
 bool issame(vector<float> a, vector<float> b){
     return a == b;
 }
 
-vector<float> derivative(vector<float> xs){
-    vector<float> result;
-    for(int i = 1; i < xs.size(); i++){
-        result.push_back(xs[i] * i);
-    }
-    return result;
-}
-
-int main(){
+int main() {
     assert(issame(derivative({1}), {}));
-    return 0;
 }
