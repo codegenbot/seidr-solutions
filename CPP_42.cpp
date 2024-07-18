796cb0d1fa5f23d011d457b0eedefb71ffea1672
beep boop <githubbot@vadim.me>
2024-07-18 16:47:58 +0200

    Solution 82 of CPP/42, ratio 0.3333333333333333 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4l9354hn Prompt:    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 07387414-a881-4908-9680-26ad5fa7c195.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 07387414-a881-4908-9680-26ad5fa7c195.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 07387414-a881-4908-9680-26ad5fa7c195.cpp:29: 07387414-a881-4908-9680-26ad5fa7c195.cpp: In function ‘int main()’: 07387414-a881-4908-9680-26ad5fa7c195.cpp:39:20: error: ‘incr_list’ was not decla red in this scope    39 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 07387414-a881-4908-9680-26ad5fa7c195.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,5 +1,29 @@
-for (int i = 0; i < l.size(); i++) {
-        l[i]++;
+#include <vector>
+#include <assert.h>
+
+vector<int> incr_list(vector<int> l);
+
+bool issame(const vector<int>& a, const vector<int>& b);
+
+vector<int> incr_list(vector<int> l) {
+    for (int &num : l) {
+        num++;
     }
     return l;
+}
+
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
+int main() {
+    vector<int> l = {1, 2, 3};
+    vector<int> l_inc = incr_list(l);
+    vector<int> l_copy = {2, 3, 4};
+
+    assert(issame(l_inc, l_copy));
+
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+
+    return 0;
 }
