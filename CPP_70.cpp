6fb26b801017d10738c5031a871956c7412ead91
beep boop <githubbot@vadim.me>
2024-07-18 16:55:17 +0200

    Solution 22 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jykh6l4w Prompt: c6faf63a-696e-4403-a103-0ebf9ed7152d.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    30 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ c6faf63a-696e-4403-a103-0ebf9ed7152d.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ c6faf63a-696e-4403-a103-0ebf9ed7152d.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ c6faf63a-696e-4403-a103-0ebf9ed7152d.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -5,17 +5,7 @@
 using namespace std;
 
 bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    
-    for(int i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+    return a == b;
 }
 
 vector<int> strange_sort_list(vector<int> lst){
