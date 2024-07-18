c3f640ec9bf02ac7a5a3f45f1959dc1be53e5e7f
beep boop <githubbot@vadim.me>
2024-07-18 20:40:19 +0200

    Solution 2 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3kkt6zyt Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c352ddb8-c12a-4b1c-b62c-70bbfd2eba53.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c352ddb8-c12a-4b1c-b62c-70bbfd2eba53.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c352ddb8-c12a-4b1c-b62c-70bbfd2eba53.cpp:20: c352ddb8-c12a-4b1c-b62c-70bbfd2eba53.cpp: In function ‘int main()’: c352ddb8-c12a-4b1c-b62c-70bbfd2eba53.cpp:30:20: error: ‘order_by_points’ was not  declared in this scope    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ c352ddb8-c12a-4b1c-b62c-70bbfd2eba53.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,17 +1,30 @@
-sort(nums.begin(), nums.end(), [](int a, int b) {
-    int sum_a = 0, sum_b = 0;
-    int temp_a = abs(a), temp_b = abs(b);
-    while (temp_a) {
-        sum_a += temp_a % 10;
-        temp_a /= 10;
-    }
-    while (temp_b) {
-        sum_b += temp_b % 10;
-        temp_b /= 10;
-    }
-    if (sum_a == sum_b) {
-        return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
-    }
-    return sum_a < sum_b;
-});
-return nums;
+#include <vector>
+#include <algorithm>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> order_by_points(std::vector<int> nums) {
+    std::sort(nums.begin(), nums.end(), [](int a, int b) {
+        int sum_a = 0, sum_b = 0;
+        int temp_a = a, temp_b = b;
+
+        while (temp_a != 0) {
+            sum_a += std::abs(temp_a % 10);
+            temp_a /= 10;
+        }
+
+        while (temp_b != 0) {
+            sum_b += std::abs(temp_b % 10);
+            temp_b /= 10;
+        }
+
+        if (sum_a == sum_b) {
+            return a < b;
+        }
+        return sum_a < sum_b;
+    });
+
+    return nums;
+}
