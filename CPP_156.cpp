13dfaa2c8a8f965a5f9d9406531c9a383eea272b
beep boop <githubbot@vadim.me>
2024-07-19 00:28:25 +0200

    Solution 92 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/z1ikt787 Prompt: 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp:4:46: error: ‘roman_map’ was not declar ed in this scope     4 |     std::vector<std::pair<int, std::string>> roman_map = {       |                                              ^~~~~~~~~ 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp: In function ‘int main()’: 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp:22:5: error: ‘assert’ was not declared in this scope    22 |     assert (int_to_mini_romank(1000) == "m");       |     ^~~~~~ 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <string>   +++ |+#include <cassert>     2 | 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp: At global scope: 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 56989da5-a46c-4fde-b4dd-c7e9de489cb6.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main(){       |     ^~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,7 +1,6 @@
-#include <vector>
-#include <cassert>
 #include <string>
 #include <vector>
+#include <cassert>
 
 std::string int_to_mini_romank(int number){
     std::vector<std::pair<int, std::string>> roman_map = {
@@ -22,6 +21,6 @@ std::string int_to_mini_romank(int number){
 }
 
 int main(){
-    assert(int_to_mini_romank(1000) == "m");
+    assert (int_to_mini_romank(1000) == "m");
     return 0;
 }
