5b93167860aa1761cbc839e8f64a2fd5549de67f
beep boop <githubbot@vadim.me>
2024-07-18 16:54:37 +0200

    Solution 8 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/menynbfl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe8b270a-d056-48a2-83ed-d2c286794 87a.py", line 3     return [(num - min_num) / (max_num - min_num) for num in numbers]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_21.py
+++ Python_21.py
@@ -1,4 +1,4 @@
-def normalize_numbers(numbers):
+def calculate_normalized_values(numbers):
     min_num = min(numbers)
     max_num = max(numbers)
     return [(num - min_num) / (max_num - min_num) for num in numbers]
