7439a734e7485a2fb4c0488b80f14d8a5b4899f9
beep boop <githubbot@vadim.me>
2024-07-18 20:36:44 +0200

    Solution 10 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vb9dagx5 Prompt:       |           ^~~~~~ In file included from 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp:34: 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp: In function ‘int main()’: 32413d6c-5bf6-4bc8-b71d-48179cace334.cpp:44:20: error: ‘unique_digits’ was not d eclared in this scope    44 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,39 +1,32 @@
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
+bool issame(int a, int b){
+    return a < b;
 }
 
 vector<int> unique_digits(vector<int> x){
     vector<int> result;
-    for (int num : x) {
+    for(int num : x){
         bool hasEvenDigit = false;
         int temp = num;
-        while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
+        while(temp > 0){
+            if(temp % 10 % 2 == 0){
                 hasEvenDigit = true;
                 break;
             }
             temp /= 10;
         }
-        if (!hasEvenDigit) {
+        if(!hasEvenDigit){
             result.push_back(num);
         }
     }
-    sort(result.begin(), result.end());
+    sort(result.begin(), result.end(), issame);
     return result;
 }
 
-int main() {
-    // Main function implementation here
+int main(){
+    assert(issame(unique_digits({135, 103, 31}) , {31, 135}));
     return 0;
 }
