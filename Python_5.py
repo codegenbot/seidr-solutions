ce2711371371ef03029cc24ab366232880e66d72
beep boop <githubbot@vadim.me>
2024-07-19 00:26:40 +0200

    Solution 96 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ebvt8ojk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d65ab69-1670-48f2-a3a8-4ab6da154 5ae.py", line 6     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,7 @@
-def join_with_delimeter(numbers, delimeter):
+def join_with_delimiter(numbers, delimiter):
     result = []
     for i in range(len(numbers)):
         result.append(numbers[i])
         if i < len(numbers) - 1:
-            result.append(delimeter)
+            result.append(delimiter)
     return result
