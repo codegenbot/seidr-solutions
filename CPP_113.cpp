8fbb74ba7b7c9ef53eb4c32e34a8e8682f4ae2a5
beep boop <githubbot@vadim.me>
2024-07-18 20:52:10 +0200

    Solution 24 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/5nmcqdef Prompt: In file included from d80a3c22-33c1-42ea-a420-b250246d1cba.cpp:53: d80a3c22-33c1-42ea-a420-b250246d1cba.cpp: In function ‘int main()’: d80a3c22-33c1-42ea-a420-b250246d1cba.cpp:63:20: error: ‘odd_count’ was not decla red in this scope; did you mean ‘solution_name::odd_count’?    63 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ d80a3c22-33c1-42ea-a420-b250246d1cba.cpp:9:20: note: ‘solution_name::odd_count’ declared here     9 |     vector<string> odd_count(vector<string> lst) {       |                    ^~~~~~~~~ In file included from d80a3c22-33c1-42ea-a420-b250246d1cba.cpp:53: d80a3c22-33c1-42ea-a420-b250246d1cba.cpp:63:19: error: ‘issame’ cannot be used a s a function    63 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    64 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    65 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    66 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    67 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -3,49 +3,47 @@
 #include <string>
 #include <cassert>
 
-namespace solution_name {
-    using namespace std;
+using namespace std;
 
-    vector<string> odd_count(vector<string> lst) {
-        vector<string> result;
-        for (string s : lst) {
-            int count = 0;
-            for (char c : s) {
-                if ((c - '0') % 2 != 0) {
-                    count++;
-                }
+vector<string> odd_count(vector<string> lst) {
+    vector<string> result;
+    for (string s : lst) {
+        int count = 0;
+        for (char c : s) {
+            if ((c - '0') % 2 != 0) {
+                count++;
             }
-            result.push_back("the number of odd elements " + to_string(count) + "\nthe string " + s + " of the input.");
         }
-        return result;
+        result.push_back("the number of odd elements " + to_string(count) + "\n the string " + s + " of the input.");
     }
+    return result;
+}
 
-    bool issame(vector<string> a, vector<string> b) {
-        return a == b;
-    }
-
-    int main() {
-        int n;
-        cin >> n; // Reading the number of strings
-        vector<string> input(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> input[i]; // Reading the strings
-        }
-
-        vector<string> output = solution_name::odd_count(input);
-
-        for (string res : output) {
-            cout << res << endl; // Output the result
-        }
-
-        return 0;
-    }
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
 
 int main() {
-    assert(solution_name::issame(solution_name::odd_count({"271", "137", "314"}),
-                                  {"the number of odd elements 2\nthe string 271 of the input.",
-                                   "the number of odd elements 2\nthe string 137 of the input.",
-                                   "the number of odd elements 2\nthe string 314 of the input."}));
+    int n;
+    cin >> n; // Reading the number of strings
+    vector<string> input(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> input[i]; // Reading the strings
+    }
+
+    vector<string> output = odd_count(input);
+
+    for (string res : output) {
+        cout << res << endl; // Output the result
+    }
+
+    return 0;
+}
+
+int main() {
+    assert(issame(odd_count({"271", "137", "314"}),
+                  {"the number of odd elements 2\n the string 271 of the input.",
+                   "the number of odd elements 2\n the string 137 of the input.",
+                   "the number of odd elements 2\n the string 314 of the input."}));
     return 0;
 }
