1334ff10327e25286c62620f375a538d4009a655
beep boop <githubbot@vadim.me>
2024-07-18 23:31:32 +0200

    Solution 12 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zvczyy3e Prompt:       |                    ^~~ 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp:21: 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp: In function ‘int main()’: 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp:31:20: error: ‘get_odd_collatz’ was not  declared in this scope    31 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 4054c9e4-ca17-45d9-aa2d-08a3897e30ea.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,40 +1,40 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-} 
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
+vector<int> get_odd_collatz(int n) {
+    vector<int> result;
     while (n != 1) {
         result.push_back(n);
         if (n % 2 == 0) {
-            n = n / 2;
+            n /= 2;
         } else {
             n = 3 * n + 1;
         }
     }
     result.push_back(1);
-
-    std::vector<int> odd_result;
+    vector<int> odd_result;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_result.push_back(num);
         }
     }
-
-    std::sort(odd_result.begin(), odd_result.end());
-
+    sort(odd_result.begin(), odd_result.end());
     return odd_result;
-} 
+}
 
 int main() {
-    int n;
-    std::cin >> n;
-
-    std::vector<int> result = get_odd_collatz(n);
-    // Do something with the result vector
-
-    return 0;
+    assert(issame(get_odd_collatz(1), {1}));
 }
