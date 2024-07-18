7dfef3d9273af06286abffa063707bfafd387cda
beep boop <githubbot@vadim.me>
2024-07-18 23:25:16 +0200

    Solution 23 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/irvk3kjm Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 7752270f-d148-4a96-98bf-e3000d9bf685.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 7752270f-d148-4a96-98bf-e3000d9bf685.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 7752270f-d148-4a96-98bf-e3000d9bf685.cpp:13: 7752270f-d148-4a96-98bf-e3000d9bf685.cpp: In function ‘int main()’: 7752270f-d148-4a96-98bf-e3000d9bf685.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 7752270f-d148-4a96-98bf-e3000d9bf685.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -5,13 +5,13 @@ bool issame(vector<float> a, vector<float> b) {
 vector<float> rescale_to_unit(vector<float> numbers) {
     float min_val = *min_element(numbers.begin(), numbers.end());
     float max_val = *max_element(numbers.begin(), numbers.end());
-
+    
     vector<float> rescaled_numbers;
     for (float num : numbers) {
         float rescaled_num = (num - min_val) / (max_val - min_val);
         rescaled_numbers.push_back(rescaled_num);
     }
-
+    
     return rescaled_numbers;
 }
 
