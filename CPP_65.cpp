1f6c7704593921d0b11270f68be01a695e000cf0
beep boop <githubbot@vadim.me>
2024-07-18 18:03:13 +0200

    Solution 13 of CPP/65, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jpajjbof Prompt: Complete the following code given the task description and function signature.

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,14 +1,16 @@
 string circular_shift(int x, int shift){
-    string s = to_string(x);
-    int n = s.size();
-    shift %= n;
-    if(shift == 0) return s;
-    
-    if(shift > n){
-        reverse(s.begin(), s.end());
-        return s;
+    string res = to_string(x);
+    int n = res.size();
+    shift = shift % n;
+    if(shift == 0){
+        return res;
     }
     
-    rotate(s.rbegin(), s.rbegin() + shift, s.rend());
-    return s;
+    if(shift > n){
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    
+    rotate(res.rbegin(), res.rbegin() + shift, res.rend());
+    return res;
 }
