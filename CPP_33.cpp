5d8795def8d439c4fb62638c1333478b90b75178
beep boop <githubbot@vadim.me>
2024-07-18 16:26:01 +0200

    Solution 16 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5z59ourz Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ea60eb9a-e2d4-410b-a812-813f47c781ee.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ea60eb9a-e2d4-410b-a812-813f47c781ee.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ea60eb9a-e2d4-410b-a812-813f47c781ee.cpp:16: ea60eb9a-e2d4-410b-a812-813f47c781ee.cpp: In function ‘int main()’: ea60eb9a-e2d4-410b-a812-813f47c781ee.cpp:26:20: error: ‘sort_third’ was not decl ared in this scope    26 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ ea60eb9a-e2d4-410b-a812-813f47c781ee.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -18,7 +18,7 @@ std::vector<int> sort_third(std::vector<int> l) {
     return l;
 }
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
