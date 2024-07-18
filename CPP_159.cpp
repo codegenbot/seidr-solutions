56c842b1503fb3ccd4df16f0e4ca0704194a5772
beep boop <githubbot@vadim.me>
2024-07-19 01:47:52 +0200

    Solution 63 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/kaciey1x Prompt:       |                    ^~~ 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp:7: 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp: In function ‘int main()’: 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp:17:20: error: ‘eat’ was not declared in  this scope    17 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 24eafbf3-dfd5-4d5a-a953-366e972840f7.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -8,11 +8,11 @@ std::pair<int, int> eat(int number, int need, int remaining) {
 }
 
 bool issame(std::pair<int, int> a, std::pair<int, int> b) {
-    return a == b;
+    return a == b; 
 }
 
 int main() {
     assert(issame(eat(4, 5, 1), {5, 0}));
-
+    
     return 0;
 }
