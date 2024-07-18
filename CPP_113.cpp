43ad44bfe86ac820cc36edef29ec14a5f3f9461f
beep boop <githubbot@vadim.me>
2024-07-18 17:57:49 +0200

    Solution 24 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xs1dg4af Prompt:       |                                            ^ 1e17f8b4-e7a1-4ae0-a7c1-827c10c022eb.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1e17f8b4-e7a1-4ae0-a7c1-827c10c022eb.cpp:16: 1e17f8b4-e7a1-4ae0-a7c1-827c10c022eb.cpp: In function ‘int main()’: 1e17f8b4-e7a1-4ae0-a7c1-827c10c022eb.cpp:26:20: error: ‘odd_count’ was not decla red in this scope    26 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ 1e17f8b4-e7a1-4ae0-a7c1-827c10c022eb.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    30 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
 
 vector<string> odd_count(vector<string> lst){
     vector<string> result;
@@ -17,12 +17,10 @@ vector<string> odd_count(vector<string> lst){
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
 int main(){
-    assert(issame(odd_count({"271", "137", "314"}), {"the number of odd elements 2 in the string 271 of the input.",
-                                                     "the number of odd elements 2 in the string 137 of the input.",
-                                                     "the number of odd elements 2 in the string 314 of the input."}));
+    assert (issame(odd_count({"271", "137", "314"}) , {
+        "the number of odd elements 2 in the string 271 of the input.",
+        "the number of odd elements 2 in the string 137 of the input.",
+        "the number of odd elements 3 in the string 314 of the input."
+    }));
 }
