55c80ee27cadfe559c6f41eca1ebd58ca49470b4
beep boop <githubbot@vadim.me>
2024-07-18 17:01:16 +0200

    Solution 5 of CPP/75, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7udq82da Prompt:     2 |     for (int i = 2; i <= a / 3; ++i) {       |     ^~~ 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 2; i <= a / 3; ++i) {       |                     ^ 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp:2:33: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 2; i <= a / 3; ++i) {       |                                 ^~ 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp:15:5: error: expected unqualified-id be fore ‘return’    15 |     return false;       |     ^~~~~~ 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp:16:1: error: expected declaration befor e ‘}’ token    16 | }       | ^ In file included from 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp:18: 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp: In function ‘int main()’: 7c2d8539-0fe3-49eb-aecf-b8c6ec456328.cpp:20:13: error: ‘is_multiply_prime’ was n ot declared in this scope    20 |     assert (is_multiply_prime(11 * 13 * 7) == true);       |             ^~~~~~~~~~~~~~~~~

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -1,9 +1,7 @@
-#include <iostream>
-
 bool is_multiply_prime(int a) {
+    int i = 2;
     if (a < 30) return false;
-    int i;
-    for (i = 2; i <= a / 3; ++i) {
+    for (i; i <= a / 3; ++i) {
         if (a % i == 0) {
             int b = a / i;
             for (int j = 2; j <= b / 2; ++j) {
@@ -17,11 +15,4 @@ bool is_multiply_prime(int a) {
         }
     }
     return false;
-}
-
-int main() {
-    int x;
-    std::cin >> x;
-    std::cout << std::boolalpha << is_multiply_prime(x) << std::endl;
-    return 0;
 }
