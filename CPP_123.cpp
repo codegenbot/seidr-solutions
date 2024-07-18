48c54b38366c21a444fd7605eb25c30715d9857d
beep boop <githubbot@vadim.me>
2024-07-18 18:01:17 +0200

    Solution 30 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ff58ep78 Prompt:       |                    ^~~ f163474f-3481-48b4-9062-f6b336fed417.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f163474f-3481-48b4-9062-f6b336fed417.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f163474f-3481-48b4-9062-f6b336fed417.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f163474f-3481-48b4-9062-f6b336fed417.cpp:12: f163474f-3481-48b4-9062-f6b336fed417.cpp: In function ‘int main()’: f163474f-3481-48b4-9062-f6b336fed417.cpp:22:20: error: ‘get_odd_collatz’ was not  declared in this scope    22 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ f163474f-3481-48b4-9062-f6b336fed417.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -6,7 +6,7 @@ bool issame(int a, int b) {
 
 vector<int> result;
 while (n != 1) {
-    if (n % 2 == 1) {
+    if (issame(n % 2, 1)) {
         result.push_back(n);
     }
     n = n % 2 == 0 ? n / 2 : 3 * n + 1;
