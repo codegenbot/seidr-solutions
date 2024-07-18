b41629ca488aa501f4743507dd5540eed21b2ddb
beep boop <githubbot@vadim.me>
2024-07-18 23:35:03 +0200

    Solution 33 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/02i9lig7 Prompt: in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e3a134d4-c0e7-4eaf-9697-53329920339b.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e3a134d4-c0e7-4eaf-9697-53329920339b.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e3a134d4-c0e7-4eaf-9697-53329920339b.cpp:5: e3a134d4-c0e7-4eaf-9697-53329920339b.cpp: In function ‘int main()’: e3a134d4-c0e7-4eaf-9697-53329920339b.cpp:15:20: error: ‘remove_duplicates’ was n ot declared in this scope    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ e3a134d4-c0e7-4eaf-9697-53329920339b.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-#include <algorithm>
-
 std::vector<int> remove_duplicates(std::vector<int> numbers) {
     sort(numbers.begin(), numbers.end());
     numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
@@ -8,15 +5,13 @@ std::vector<int> remove_duplicates(std::vector<int> numbers) {
 }
 
 bool issame(std::vector<int> vec1, std::vector<int> vec2) {
-    if (vec1.size() != vec2.size()) {
+    if(vec1.size() != vec2.size()) {
         return false;
     }
-
-    for (size_t i = 0; i < vec1.size(); ++i) {
-        if (vec1[i] != vec2[i]) {
+    for(size_t i = 0; i < vec1.size(); ++i) {
+        if(vec1[i] != vec2[i]) {
             return false;
         }
     }
-
     return true;
 }
