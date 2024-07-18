d9a71278b3539908798ad4951ee2188e19747f9a
beep boop <githubbot@vadim.me>
2024-07-19 01:48:29 +0200

    Solution 12 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ook0pqt9 Prompt:       |             ^~~~~~ e97c163c-4bec-48e4-ab50-be8e12844e64.cpp:35:20: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ e97c163c-4bec-48e4-ab50-be8e12844e64.cpp:35:27: error: ‘vector’ was not declared  in this scope    35 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e97c163c-4bec-48e4-ab50-be8e12844e64.cpp:35:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from e97c163c-4bec-48e4-ab50-be8e12844e64.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e97c163c-4bec-48e4-ab50-be8e12844e64.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e97c163c-4bec-48e4-ab50-be8e12844e64.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -29,4 +29,4 @@ std::vector<int> parse_nested_parens(std::string paren_string){
     return depths;
 }
 
-assert(issame(parse_nested_parens("(()(())((())))"), std::vector<int>{4}));
+std::assert(issame(parse_nested_parens("(()(())((())))"), std::vector<int>{4});
