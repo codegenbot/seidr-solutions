2be33ba3513b2e926b2063ed390e8d49349fda6c
beep boop <githubbot@vadim.me>
2024-07-18 20:52:52 +0200

    Solution 25 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/pgupldjj Prompt:       |                                 ^~~~ 248669d3-7ea4-4555-bb93-332a845e40c9.cpp:26:38: error: expected primary-expressi on before ‘int’    26 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 248669d3-7ea4-4555-bb93-332a845e40c9.cpp:26:44: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ 248669d3-7ea4-4555-bb93-332a845e40c9.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 248669d3-7ea4-4555-bb93-332a845e40c9.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 248669d3-7ea4-4555-bb93-332a845e40c9.cpp:25: 248669d3-7ea4-4555-bb93-332a845e40c9.cpp: In function ‘int main()’: 248669d3-7ea4-4555-bb93-332a845e40c9.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -3,8 +3,6 @@
 #include <string>
 #include <cassert>
 
-std::map<char, int> histogram(std::string test);
-
 std::map<char, int> histogram(std::string test) {
     std::map<char, int> result;
     for (char c : test) {
@@ -13,11 +11,11 @@ std::map<char, int> histogram(std::string test) {
     return result;
 }
 
-bool issame(std::map<char, int>& a, std::map<char, int>& b) {
+bool is_same(std::map<char, int>& a, std::map<char, int>& b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(histogram("a"), std::map<char, int>{{'a', 1}}));
+    assert(is_same(histogram("a"), std::map<char, int>{{'a', 1}}));
     return 0;
 }
