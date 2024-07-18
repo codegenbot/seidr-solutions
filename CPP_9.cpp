9cb2d95ea40b1ad525e8bb7602cb39a7bd9292c6
beep boop <githubbot@vadim.me>
2024-07-19 00:29:38 +0200

    Solution 42 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pqphr2io Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:16: e5aa2de5-20c8-47ae-a584-f7460012a100.cpp: In function ‘int main()’: e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:26:20: error: ‘rolling_max’ was not dec lared in this scope    26 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -2,6 +2,8 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
+vector<int> rolling_max(vector<int> numbers);
+
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
     int n = numbers.size();
@@ -18,11 +20,16 @@ vector<int> rolling_max(vector<int> numbers) {
 }
 
 int main() {
-    vector<int> numbers = {1, 3, 5, 2, 6, 4};
-    vector<int> expected_result = {5, 5, 6, 6, 6, 4};
-    
-    vector<int> result = rolling_max(numbers);
-    bool same = issame(result, expected_result);
-    
+    vector<int> test_numbers = {1, 3, 5, 2, 6, 3, 9, 7};
+    vector<int> expected_result = {5, 5, 6, 6, 9, 9, 9, 7};
+
+    vector<int> result = rolling_max(test_numbers);
+
+    if (issame(result, expected_result)) {
+        cout << "Test Passed!" << endl;
+    } else {
+        cout << "Test Failed!" << endl;
+    }
+
     return 0;
 }
