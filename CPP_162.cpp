01019b3dd6be923133a737652380a05f912095f6
beep boop <githubbot@vadim.me>
2024-07-18 21:44:52 +0200

    Solution 26 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pv213i5a Prompt: /usr/bin/ld: /tmp/ccT730pN.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': aa422ac5-dc38-439f-93ed-939d38625835.cpp:(.text+0x6e): undefined reference to `E VP_md5' /usr/bin/ld: aa422ac5-dc38-439f-93ed-939d38625835.cpp:(.text+0xac): undefined re ference to `EVP_Digest' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,6 +2,8 @@
 #include <openssl/md5.h>
 #include <openssl/evp.h>
 
+#define MD5_DIGEST_LENGTH 16
+
 using namespace std;
 
 string string_to_md5(const string& text) {
@@ -9,11 +11,16 @@ string string_to_md5(const string& text) {
         return "None";
     }
 
-    unsigned char digest[EVP_MAX_MD_SIZE];
-    EVP_Digest((unsigned char*)text.c_str(), text.length(), digest, NULL, EVP_md5(), NULL);
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    EVP_MD_CTX* ctx;
+    ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, EVP_md5(), NULL);
+    EVP_DigestUpdate(ctx, text.c_str(), text.size());
+    EVP_DigestFinal_ex(ctx, digest, NULL);
+    EVP_MD_CTX_free(ctx);
 
     char mdString[33];
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
 
