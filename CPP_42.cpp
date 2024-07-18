3e25523e1b3af082561ea3baa51597e7842fc7fa
beep boop <githubbot@vadim.me>
2024-07-18 17:45:02 +0200

    Solution 14 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/wmhp41ef Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ add3cb41-23c1-4044-992b-3fc53562723c.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ add3cb41-23c1-4044-992b-3fc53562723c.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from add3cb41-23c1-4044-992b-3fc53562723c.cpp:7: add3cb41-23c1-4044-992b-3fc53562723c.cpp: In function ‘int main()’: add3cb41-23c1-4044-992b-3fc53562723c.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ add3cb41-23c1-4044-992b-3fc53562723c.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
-std::vector<int> incr_list(const std::vector<int>& l) {
-    std::vector<int> incremented = l;
-    for (int i = 0; i < incremented.size(); i++) {
-        incremented[i]++;
+std::vector<int> incr_list(std::vector<int> l) {
+    for (int i = 0; i < l.size(); i++) {
+        l[i]++;
     }
-    return incremented;
+    return l;
 }
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
@@ -16,5 +14,4 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 
 int main() {
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
-    return 0;
 }
