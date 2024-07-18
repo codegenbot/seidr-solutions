2d55b785fbba8afc26825573992c42ed46ab4323
beep boop <githubbot@vadim.me>
2024-07-18 18:16:55 +0200

    Solution 27 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/k3k7lvut Prompt:       |                              ^~~~~~ 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp:20:37: error: ‘string’ was not declared  in this scope    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp:19: 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp: In function ‘int main()’: 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp:29:20: error: ‘total_match’ was not dec lared in this scope    29 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 545351e8-bbba-4f4f-8e32-be8f9a92ffb4.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,21 +2,21 @@
 #include <string>
 #include <cassert>
 
-std::vector<std::string> issame(std::vector<std::string> a, std::vector<std::string> b) {
+std::vector<std::string> issame(std::vector<std::string> lst1, std::vector<std::string> lst2) {
     int total_chars1 = 0;
     int total_chars2 = 0;
     
-    for(const std::string& s : a){
+    for(const std::string& s : lst1){
         total_chars1 += s.size();
     }
     
-    for(const std::string& s : b){
+    for(const std::string& s : lst2){
         total_chars2 += s.size();
     }
     
     if(total_chars1 < total_chars2){
-        return a;
+        return lst1;
     }
     
-    return b;
+    return lst2;
 }
