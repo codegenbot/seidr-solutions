f676e835f042e82dd56874daed1fffeb4a1edd90
beep boop <githubbot@vadim.me>
2024-07-18 18:06:38 +0200

    Solution 43 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/7mhmu5zd Prompt:    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 6acaaa64-8631-4eff-be11-ae0aae7a4248.cpp: In function ‘int main()’: 6acaaa64-8631-4eff-be11-ae0aae7a4248.cpp:23:5: error: ‘assert’ was not declared in this scope    23 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |     ^~~~~~ 6acaaa64-8631-4eff-be11-ae0aae7a4248.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <openssl/md5.h>   +++ |+#include <cassert>     4 | using namespace std; 6acaaa64-8631-4eff-be11-ae0aae7a4248.cpp: At global scope: 6acaaa64-8631-4eff-be11-ae0aae7a4248.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 6acaaa64-8631-4eff-be11-ae0aae7a4248.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <openssl/md5>
 #include <cassert>
-
 using namespace std;
 
 string string_to_md5(const string& text) {
@@ -22,6 +21,6 @@ string string_to_md5(const string& text) {
 }
 
 int main() {
-    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+    assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
     return 0;
 }
