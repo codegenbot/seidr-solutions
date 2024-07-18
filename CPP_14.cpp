be0458b833cce7d778e369d5891ee347d4755142
beep boop <githubbot@vadim.me>
2024-07-19 01:55:20 +0200

    Solution 19 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4cz2lphy Prompt: /usr/include/assert.h:92: note: macro "assert" defined here    92 | #  define assert(expr) \       | a292c39f-8347-4c40-a6c8-8594fa204506.cpp: In function ‘int main()’: a292c39f-8347-4c40-a6c8-8594fa204506.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(all_prefixes("WWW") == vector<string>{"W", "WW", "WWW"});       |     ^~~~~~ a292c39f-8347-4c40-a6c8-8594fa204506.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <vector>   +++ |+#include <cassert>     4 | #include <string> a292c39f-8347-4c40-a6c8-8594fa204506.cpp: At global scope: a292c39f-8347-4c40-a6c8-8594fa204506.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ a292c39f-8347-4c40-a6c8-8594fa204506.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-vector<string> all_prefixes(string str){
+vector<string> all_prefixes(string str) {
     vector<string> result;
-    for (int i = 1; i <= str.size(); ++i) {
+    for (size_t i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
