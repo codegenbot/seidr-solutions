d219823ad58b78a2fd2252fc88e167dcddf2b83d
beep boop <githubbot@vadim.me>
2024-07-18 23:26:26 +0200

    Solution 20 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/k5eiep43 Prompt: 1b531782-beea-4054-88a3-fb3756861355.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return str.length();       | ^~~~~~ In file included from 1b531782-beea-4054-88a3-fb3756861355.cpp:3: 1b531782-beea-4054-88a3-fb3756861355.cpp: In function ‘int main()’: 1b531782-beea-4054-88a3-fb3756861355.cpp:5:13: error: ‘strlen’ was not declared in this scope     5 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ 1b531782-beea-4054-88a3-fb3756861355.cpp:4:1: note: ‘strlen’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     3 | #include<assert.h>   +++ |+#include <cstring>     4 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,9 +1,2 @@
-#include <iostream>
-#include <cstring>
-
-int main(){
-    std::string str;
-    std::cin >> str;
-    std::cout << str.length();
-    return 0;
-}
+#include <string>
+return str.length();
