842f123ae6ade26d75d04a17c1ef0d2a02c1b4e6
beep boop <githubbot@vadim.me>
2024-07-18 17:01:44 +0200

    Solution 76 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/mrc034hd Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a675f457-dff8-4994-afad-416a2979bff4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a675f457-dff8-4994-afad-416a2979bff4.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from a675f457-dff8-4994-afad-416a2979bff4.cpp:14: a675f457-dff8-4994-afad-416a2979bff4.cpp: In function ‘int main()’: a675f457-dff8-4994-afad-416a2979bff4.cpp:24:20: error: ‘remove_duplicates’ was n ot declared in this scope    24 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ a675f457-dff8-4994-afad-416a2979bff4.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 bool issame(std::vector<int>& a, std::vector<int>& b) {
     return a == b;
@@ -9,4 +10,9 @@ std::vector<int> remove_duplicates(std::vector<int> numbers) {
     std::sort(numbers.begin(), numbers.end());
     numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
+}
+
+int main() {
+    assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+    return 0;
 }
