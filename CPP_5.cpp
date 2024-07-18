9aed0fed603690f68caf831dc14b5a897fa4e46e
beep boop <githubbot@vadim.me>
2024-07-19 01:47:01 +0200

    Solution 7 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/j18db22i Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 6a71d73c-8567-425d-b305-e6ca8e2fbf65.cpp:2: 6a71d73c-8567-425d-b305-e6ca8e2fbf65.cpp: In function ‘int main()’: 6a71d73c-8567-425d-b305-e6ca8e2fbf65.cpp:19:12: error: ‘issame’ was not declared  in this scope    19 |     assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));       |            ^~~~~~ 6a71d73c-8567-425d-b305-e6ca8e2fbf65.cpp: At global scope: 6a71d73c-8567-425d-b305-e6ca8e2fbf65.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 6a71d73c-8567-425d-b305-e6ca8e2fbf65.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 vector<int> intersperse(vector<int> numbers, int delimiter);
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> intersperse(vector<int> numbers, int delimiter) {
     vector<int> result;
     for (int i = 0; i < numbers.size(); ++i) {
@@ -20,6 +16,6 @@ vector<int> intersperse(vector<int> numbers, int delimiter) {
 }
 
 int main() {
-    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    assert(intersperse({2, 2, 2}, 2) == vector<int>{2, 2, 2, 2, 2});
     return 0;
 }
