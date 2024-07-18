cae4be8dcab733b8b18918c42d7540c92f19d360
beep boop <githubbot@vadim.me>
2024-07-18 23:40:49 +0200

    Solution 88 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7r7w2c5w Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 7bfc3002-7cf3-4c22-9606-059768674766.cpp:3: 7bfc3002-7cf3-4c22-9606-059768674766.cpp: In function ‘int main()’: 7bfc3002-7cf3-4c22-9606-059768674766.cpp:21:12: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    21 |     assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 2, 2, 2, 2}));       |            ^~~~~~ 7bfc3002-7cf3-4c22-9606-059768674766.cpp: At global scope: 7bfc3002-7cf3-4c22-9606-059768674766.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 7bfc3002-7cf3-4c22-9606-059768674766.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main(){       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <cassert>
 
 using namespace std;
+
+bool issame(vector<int> v1, vector<int> v2){
+    if(v1.size() != v2.size()){
+        return false;
+    }
+    for(int i=0; i<v1.size(); i++){
+        if(v1[i] != v2[i]){
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<int> parse_music(string music_string){
     vector<int> beats;
@@ -19,7 +30,7 @@ vector<int> parse_music(string music_string){
 }
 
 int main(){
-    assert(std::equal(parse_music("o| .| o| .| o o| o o|").begin(), parse_music("o| .| o| .| o o| o o|").end(), vector<int>{2, 1, 2, 1, 2, 2, 2, 2}.begin()));
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 2, 2, 2, 2}));
     
     return 0;
 }
