02ef0a3e10aa1afee959452bc47496d771576eb6
beep boop <githubbot@vadim.me>
2024-07-18 18:04:09 +0200

    Solution 43 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ff58ep78 Prompt:       |                           ^~~~~~ 1a2c5d0f-8b9c-43ac-beca-bdd57e0c18e3.cpp:45:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1a2c5d0f-8b9c-43ac-beca-bdd57e0c18e3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1a2c5d0f-8b9c-43ac-beca-bdd57e0c18e3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1a2c5d0f-8b9c-43ac-beca-bdd57e0c18e3.cpp:45:34: error: expected primary-expressi on before ‘int’    45 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1a2c5d0f-8b9c-43ac-beca-bdd57e0c18e3.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ 1a2c5d0f-8b9c-43ac-beca-bdd57e0c18e3.cpp:38:5: note: ‘int main()’ previously def ined here    38 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -14,27 +14,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> get_odd_collatz(int n) {
-    std::vector<int> result;
-    while (n != 1) {
-        result.push_back(n);
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
-    }
-    result.push_back(1);
-    std::vector<int> odd_nums;
-    for (int num : result) {
-        if (num % 2 != 0) {
-            odd_nums.push_back(num);
-        }
-    }
-    std::sort(odd_nums.begin(), odd_nums.end());
-    return odd_nums;
-}
-
 int main() {
     assert(issame(get_odd_collatz(1), {1}));
 
