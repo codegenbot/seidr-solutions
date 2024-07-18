9a37f3cec00337a377a60487508eed6780f0126d
beep boop <githubbot@vadim.me>
2024-07-19 00:21:53 +0200

    Solution 79 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9blsf2ld Prompt: 3077168b-d2d1-4e8c-a226-6f9edb646d04.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 3077168b-d2d1-4e8c-a226-6f9edb646d04.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 3077168b-d2d1-4e8c-a226-6f9edb646d04.cpp:21: 3077168b-d2d1-4e8c-a226-6f9edb646d04.cpp: In function ‘int main()’: 3077168b-d2d1-4e8c-a226-6f9edb646d04.cpp:23:35: error: ‘lst’ was not declared in  this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 3077168b-d2d1-4e8c-a226-6f9edb646d04.cpp:23:43: error: ‘odd_sum’ was not declare d in this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -15,5 +15,5 @@ long long double_the_difference(std::vector<float> lst) {
 
 std::vector<float> lst = {2.0, 3.0, 4.0, 5.0, 6.0};
 long long odd_sum = double_the_difference(lst);
-assert(double_the_difference(lst) == odd_sum);
+assert(double_the_difference(lst) == odd_sum); 
 return 0;
