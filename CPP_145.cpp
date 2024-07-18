0886e60a3db313db892121d8d32ebfaf85a6fcf6
beep boop <githubbot@vadim.me>
2024-07-19 00:22:18 +0200

    Solution 82 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8p4axrrj Prompt:    40 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0d2f7f71-0d71-44e4-8ad6-836cff18ffa3.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 0d2f7f71-0d71-44e4-8ad6-836cff18ffa3.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 0d2f7f71-0d71-44e4-8ad6-836cff18ffa3.cpp:39: 0d2f7f71-0d71-44e4-8ad6-836cff18ffa3.cpp: In function ‘int main()’: 0d2f7f71-0d71-44e4-8ad6-836cff18ffa3.cpp:49:20: error: ‘order_by_points’ was not  declared in this scope    49 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 0d2f7f71-0d71-44e4-8ad6-836cff18ffa3.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -36,6 +36,6 @@ int main() {
     for (int num : nums) {
         cout << num << " ";
     }
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), vector<int>{-76, -21, 0, 4, 23, 6, 6});
     return 0;
 }
