c8266148f18fd4be86a075bbda361787e3ba3df8
beep boop <githubbot@vadim.me>
2024-07-19 01:43:31 +0200

    Solution 41 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kk1vwmeu Prompt:       |     ^~~~ In file included from ceaae860-f8c8-4709-91e7-c05a7410d7ef.cpp:23: ceaae860-f8c8-4709-91e7-c05a7410d7ef.cpp: In function ‘int main()’: ceaae860-f8c8-4709-91e7-c05a7410d7ef.cpp:33:26: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    33 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from ceaae860-f8c8-4709-91e7-c05a7410d7ef.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ ceaae860-f8c8-4709-91e7-c05a7410d7ef.cpp:5:43: note:   initializing argument 1 o f ‘std::vector<int> common(std::vector<int>&, std::vector<int>&)’     5 | std::vector<int> common(std::vector<int>& l1, std::vector<int>& l2) {       |                         ~~~~~~~~~~~~~~~~~~^~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,17 +3,9 @@
 #include <cassert>
 
 std::vector<int> common(const std::vector<int>& l1, const std::vector<int>& l2) {
-    std::vector<int> l1_copy = l1;
-    std::vector<int> l2_copy = l2;
-    
-    std::sort(l1_copy.begin(), l1_copy.end());
-    std::sort(l2_copy.begin(), l2_copy.end());
-    
     std::vector<int> result;
-    std::set_intersection(l1_copy.begin(), l1_copy.end(), l2_copy.begin(), l2_copy.end(), std::back_inserter(result));
-    
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
     result.erase(std::unique(result.begin(), result.end()), result.end());
-    
     return result;
 }
 
