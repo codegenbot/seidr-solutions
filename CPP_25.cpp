6bdf623182b2ad435230b51dd76ead129ceb007a
beep boop <githubbot@vadim.me>
2024-07-18 16:38:44 +0200

    Solution 44 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1189bgqw Prompt:       |           ^~~~~~ In file included from 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp:17: 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp: In function ‘int main()’: 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp:27:20: error: ‘factorize’ was not decla red in this scope    27 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 9da3851f-b4b9-44b4-82d9-58fe265fe378.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
-using namespace std;
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
+    return a == b;
+}
 
-vector<int> factorize(int n){
-    vector<int> factors;
+std::vector<int> factorize(int n){
+    std::vector<int> factors;
     for(int i=2; i*i<=n; i++){
         while(n%i == 0){
             factors.push_back(i);
@@ -18,19 +19,7 @@ vector<int> factorize(int n){
     return factors;
 }
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
 int main(){
-    assert(issame(factorize(3 * 2 * 3) , {2, 3, 3}));
+    assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));
     return 0;
 }
