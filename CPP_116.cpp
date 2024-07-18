55503cceafa1543884afe4189cda38e7cdc16bce
beep boop <githubbot@vadim.me>
2024-07-18 21:18:38 +0200

    Solution 84 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1wiuvt4w Prompt: e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp:26: e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp: In function ‘int main()’: e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp:36:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    36 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp:9:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     9 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ e2f59552-f0b4-4b6e-b822-9c0e9ccd3649.cpp:27:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    27 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -11,13 +11,8 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 vector<int> sort_array(vector<int> arr) {
-    sort(arr.begin(), arr.end(), [](auto a, auto b) mutable {
-        return __builtin_popcount(a) == __builtin_popcount(b) ? a < b : __builtin_popcount(a) < __builtin_popcount(b);
-    });
-    return arr;
-}
-
-int main() {
-    assert(issame(sort_array({2, 4, 8, 16, 32}), vector<int>{2, 4, 8, 16, 32}));
-    return 0;
+	sort(arr.begin(), arr.end(), [](auto a, auto b) mutable {
+		return __builtin_popcount(a) == __builtin_popcount(b) ? a < b : __builtin_popcount(a) < __builtin_popcount(b);
+	});
+	return arr;
 }
