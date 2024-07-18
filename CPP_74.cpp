95cb17ae8d65e9b822b7a2d70b5e6d383a2f6b21
beep boop <githubbot@vadim.me>
2024-07-18 16:19:42 +0200

    Solution 4 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/mla5w1xo Prompt: 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp:16: 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp: In function ‘int main()’: 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp: In function ‘int totalChars(int)’: 10653842-23f8-450e-b16f-bd9eeb4d074a.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,30 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-int totalChars(const vector<string>& lst) {
-    int total = 0;
-    for (const string& str : lst) {
-        total += str.size();
-    }
-    return total;
-}
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(const vector<string>& a, const vector<string>& b) {
     return a == b;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    if (totalChars(lst1) < totalChars(lst2)) {
-        return lst1;
-    }
-    return totalChars(lst1) == totalChars(lst2) ? lst1 : lst2;
-}
-
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
-    return 0;
+vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
+    return totalChars(lst1) < totalChars(lst2) ? lst1 : lst2;
 }
