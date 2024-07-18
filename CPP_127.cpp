c8aa330acadcf7389076c8a6209c787c78bb847c
beep boop <githubbot@vadim.me>
2024-07-19 01:25:21 +0200

    Solution 16 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/hr2f1mza Prompt: fore ‘for’    19 |     for (int i = 2; i * i <= length; i++) {       |     ^~~ 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp:19:21: error: ‘i’ does not name a type    19 |     for (int i = 2; i * i <= length; i++) {       |                     ^ 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp:19:38: error: ‘i’ does not name a type    19 |     for (int i = 2; i * i <= length; i++) {       |                                      ^ 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp:25:5: error: expected unqualified-id be fore ‘return’    25 |     return "YES";       |     ^~~~~~ 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp:26:1: error: expected declaration befor e ‘}’ token    26 | }       | ^ In file included from 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp:28: 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp: In function ‘int main()’: 12cf749a-3670-43ec-99f6-b6c3a1d09728.cpp:30:13: error: ‘intersection’ was not de clared in this scope; did you mean ‘intersectionEnd’?    30 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,33 +1,35 @@
-int isPrime(int n) {
-    if (n <= 1) {
-        return 0;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
+#include <string>
 
-string intersection(vector<int> interval1, vector<int> interval2) {
+string intersection(const vector<int>& interval1, const vector<int>& interval2) {
     int start1 = interval1[0];
     int end1 = interval1[1];
     int start2 = interval2[0];
     int end2 = interval2[1];
 
-    int intersection_start = max(start1, start2);
-    int intersection_end = min(end1, end2);
-    
-    if (intersection_start > intersection_end) {
-        return "NO";
+    int intersectionStart = max(start1, start2);
+    int intersectionEnd = min(end1, end2);
+
+    if (intersectionStart > intersectionEnd) {
+        return "NO"s;
     }
-    
-    int intersection_length = intersection_end - intersection_start + 1;
-    
-    if (isPrime(intersection_length)) {
-        return "YES";
-    } else {
-        return "NO";
+
+    int length = intersectionEnd - intersectionStart;
+
+    if (length <= 1) {
+        return "NO"s;
     }
+
+    for (int i = 2; i * i <= length; i++) {
+        if (length % i == 0) {
+            return "NO"s;
+        }
+    }
+
+    return "YES"s;
+}
+
+int main() {
+    assert(intersection({-2, -2}, {-3, -2}) == "NO"s);
+
+    return 0;
 }
