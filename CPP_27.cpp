14fa9716b6f51c4275852db923fc89eb129a19e1
beep boop <githubbot@vadim.me>
2024-07-18 23:37:15 +0200

    Solution 35 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pdupkn03 Prompt:     1 | for(int i=0; i<str.length(); i++){       | ^~~ c36e6486-3cae-46cf-82f4-58660cdcca08.cpp:1:14: error: ‘i’ does not name a type     1 | for(int i=0; i<str.length(); i++){       |              ^ c36e6486-3cae-46cf-82f4-58660cdcca08.cpp:1:30: error: ‘i’ does not name a type     1 | for(int i=0; i<str.length(); i++){       |                              ^ c36e6486-3cae-46cf-82f4-58660cdcca08.cpp:9:5: error: expected unqualified-id bef ore ‘return’     9 |     return str;       |     ^~~~~~ c36e6486-3cae-46cf-82f4-58660cdcca08.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from c36e6486-3cae-46cf-82f4-58660cdcca08.cpp:12: c36e6486-3cae-46cf-82f4-58660cdcca08.cpp: In function ‘int main()’: c36e6486-3cae-46cf-82f4-58660cdcca08.cpp:14:13: error: ‘filp_case’ was not decla red in this scope    14 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <string>
 
-std::string flip_case(std::string str){
-    for(size_t i = 0; i < str.length(); i++){
-        if(islower(str[i])){
+std::string flip_case(std::string str) {
+    for (size_t i = 0; i < str.length(); i++) {
+        if (islower(str[i])) {
             str[i] = toupper(str[i]);
-        }
-        else if(isupper(str[i])){
+        } else if (isupper(str[i])) {
             str[i] = tolower(str[i]);
         }
     }
     return str;
 }
 
-int main(int argc, char *argv[]){
-    std::string input;
-    std::cout << "Enter a string: ";
-    std::cin >> input;
-    std::cout << "String after flipping case: " << flip_case(input) << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: ./program_name <string_to_flip_case>\n";
+        return 1;
+    }
+    
+    std::string str(argv[1]);
+    std::cout << flip_case(str) << std::endl;
+
     return 0;
 }
