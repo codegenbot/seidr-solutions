45d0d5e2b78d7d39e77494b5e58c41d7af7dad88
beep boop <githubbot@vadim.me>
2024-07-18 18:04:59 +0200

    Solution 32 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/qddz4bbx Prompt:       |                    ^~~ c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp:11: c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp: In function ‘int main()’: c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp:21:20: error: ‘generate_integers’ was n ot declared in this scope    21 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ c0a373f6-ae9a-4817-ab59-d9a0dce87f85.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
+bool is_same(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
 
-vector<int> generate_integers(int a, int b){
-    vector<int> result;
+std::vector<int> generate_integers(int a, int b){
+    std::vector<int> result;
     for(int i = a; i <= b; ++i){
         if(i % 2 == 0){
             result.push_back(i);
@@ -13,11 +15,7 @@ vector<int> generate_integers(int a, int b){
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-int main() {
-    assert(issame(generate_integers(17,89), {}));
+int main(){
+    assert(is_same(generate_integers(17,89) , {}));
     return 0;
 }
