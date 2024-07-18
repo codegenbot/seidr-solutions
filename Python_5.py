b87b45274b14bec59385a38c8c4805e4ec7491ee
beep boop <githubbot@vadim.me>
2024-07-19 00:15:07 +0200

    Solution 30 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ebvt8ojk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6fd66f25-abee-43aa-877a-e8f73f2e6 d3c.py", line 5     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,2 +1,4 @@
 def interleave_numbers(numbers, delimiter):
-    return [e for sublist in [[num, delimiter] for num in numbers] for e in sublist][:-1]
+    return [e for sublist in [[num, delimiter] for num in numbers] for e in sublist][
+        :-1
+    ]
