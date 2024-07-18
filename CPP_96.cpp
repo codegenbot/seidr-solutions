e30321581f39900daffafefabd9cb2bd75cc8e30
beep boop <githubbot@vadim.me>
2024-07-18 20:36:26 +0200

    Solution 7 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/m1095shm Prompt:    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d282eaac-20ff-4ff1-b72f-1d9d0c359ea6.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d282eaac-20ff-4ff1-b72f-1d9d0c359ea6.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d282eaac-20ff-4ff1-b72f-1d9d0c359ea6.cpp:25: d282eaac-20ff-4ff1-b72f-1d9d0c359ea6.cpp: In function ‘int main()’: d282eaac-20ff-4ff1-b72f-1d9d0c359ea6.cpp:35:20: error: ‘count_up_to’ was not dec lared in this scope    35 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ d282eaac-20ff-4ff1-b72f-1d9d0c359ea6.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,40 +1,44 @@
-int count_up_to(vector<int> primes, int n) {
-    int count = 0;
-    for (int prime : primes) {
-        if (prime <= n) {
-            ++count;
-        } else {
-            break;
-        }
-    }
-    return count;
-}
-
 bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); ++i) {
+    
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+    
     return true;
 }
 
-int main() {
-    int n;
-    cin >> n;
-    
-    vector<int> result = sieve_of_eratosthenes(n);
-    
-    vector<int> expected{2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-    
-    if (count_up_to(result, n) == count_up_to(expected, n) && issame(result, expected)) {
-        cout << "Correct";
-    } else {
-        cout << "Incorrect";
+vector<int> count_up_to(int n) {
+    vector<int> result;
+    if (n < 2) {
+        return result;
     }
     
+    vector<bool> isPrime(n, true);
+    isPrime[0] = isPrime[1] = false;
+    
+    for (int i = 2; i * i < n; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    
+    for (int i = 2; i < n; ++i) {
+        if (isPrime[i]) {
+            result.push_back(i);
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    // Main function
     return 0;
 }
