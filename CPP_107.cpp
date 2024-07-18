dd737c13b9a680b333b2f670e9c1dc4a5cc53c40
beep boop <githubbot@vadim.me>
2024-07-18 20:36:35 +0200

    Solution 3 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3maj1lfp Prompt:       |                    ^~~ 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp:16: 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp: In function ‘int main()’: 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp:26:20: error: ‘even_odd_palindrome’ was  not declared in this scope    26 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 7711f3dc-2756-4e5b-aa2c-220fe1397ff9.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 
-vector<int> even_odd_palindrome(int n) {
-    
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::pair<int, int> even_odd_palindrome(int n) {
     int even = 0, odd = 0;
-    
     for (int i = 1; i <= n; ++i) {
-        string num = to_string(i);
-        string rev = num;
-        reverse(rev.begin(), rev.end());
+        std::string num = std::to_string(i);
+        std::string rev = num;
+        std::reverse(rev.begin(), rev.end());
         if (num == rev) {
             if (i % 2 == 0)
                 even++;
@@ -15,10 +17,5 @@ vector<int> even_odd_palindrome(int n) {
                 odd++;
         }
     }
-    
     return {even, odd};
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
 }
