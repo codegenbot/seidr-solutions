0f71f68bcec388856ad9db59f10c1e583cac17bd
beep boop <githubbot@vadim.me>
2024-07-19 00:16:30 +0200

    Solution 52 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/g9solkns Prompt: 35 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5792fb80-cb89-458c-b8e7-c78bfb30d 7c4.py", line 15, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5792fb80-cb89-458c-b8e7-c78bfb30d 7c4.py", line 11, in check     double_the_difference(lst) == odd_sum                                   ^^^^^^^ NameError: name 'odd_sum' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,9 @@
 def double_the_difference(lst):
-    odd_sum = sum(x**2 for x in lst if x > 0 and x % 2 != 0)
-    return 2 * odd_sum
+    return sum(x**2 for x in lst if x > 0 and x % 2 != 0)
+
+def check(func, odd_sum):
+    return func(odd_sum)
 
 lst = [1, 2, 3, 4, 5]
-print(double_the_difference(lst))
+odd_sum = sum(x for x in lst if x % 2 != 0)
+print(check(double_the_difference, odd_sum) )
