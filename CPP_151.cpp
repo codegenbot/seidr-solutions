a2bf312cc7239ae018a9164d0c220873413e20e7
beep boop <githubbot@vadim.me>
2024-07-19 01:39:09 +0200

    Solution 5 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7l7lezz6 Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,7 +1,7 @@
 long long double_the_difference(vector<float> lst){
     long long sum = 0;
-    for (float num : lst) {
-        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
+    for(float num : lst){
+        if(num > 0 && (int)num == num && (int)num % 2 != 0){
             sum += (long long)pow(num, 2);
         }
     }
