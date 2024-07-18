e65589a550a250e5c653a7e8676e11b834e302ad
beep boop <githubbot@vadim.me>
2024-07-18 18:07:12 +0200

    Solution 16 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/o1iikb0z Prompt: In file included from /usr/include/c++/11/vector:67,                  from 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp:25: 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp: In function ‘int main()’: 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp:35:20: error: ‘derivative’ was not decl ared in this scope    35 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 70dfabc3-52d9-4573-8f59-23298f5da7c0.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,9 +1,15 @@
-#include <cassert>
 #include <vector>
-#include <cmath>
+#include <cassert>
+
 using namespace std;
 
-vector<float> derivative(vector<float> xs);
+vector<float> derivative(vector<float> xs) {
+    vector<float> result;
+    for (int i = 1; i < xs.size(); i++) {
+        result.push_back(xs[i] * i);
+    }
+    return result;
+}
 
 bool issame(vector<float> a, vector<float> b){
     if (a.size() != b.size()) {
@@ -17,10 +23,7 @@ bool issame(vector<float> a, vector<float> b){
     return true;
 }
 
-vector<float> derivative(vector<float> xs){
-    vector<float> result;
-    for (int i = 1; i < xs.size(); i++) {
-        result.push_back(xs[i] * i);
-    }
-    return result;
+int main() {
+    assert(issame(derivative({1}), {}));
+    return 0;
 }
