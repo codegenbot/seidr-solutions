1c437c6ab7b657144d1d524a73ffbf03a142381c
beep boop <githubbot@vadim.me>
2024-07-18 20:54:11 +0200

    Solution 88 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/67v79vzq Prompt:       |                         ^ bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp:5:35: error: expected unqualified-id be fore ‘++’ token     5 |         for (int j = 0; j < cols; ++j) {       |                                   ^~ bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp:14:9: error: expected unqualified-id be fore ‘while’    14 |         while (total_fill > 0) {       |         ^~~~~ bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp:19:9: error: expected unqualified-id be fore ‘return’    19 |         return num_operations;       |         ^~~~~~ bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp:20:5: error: expected declaration befor e ‘}’ token    20 |     }       |     ^ In file included from bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp:22: bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp: In function ‘int main()’: bdd80b68-1db4-4871-bb1b-e77fb9aade7c.cpp:24:13: error: ‘max_fill’ was not declar ed in this scope; did you mean ‘total_fill’?    24 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ^~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -1,26 +1,26 @@
 int max_fill(vector<vector<int>>& grid, int capacity) {
-    int rows = grid.size();
-    int cols = grid[0].size();
-    int total_fill = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int total_fill = 0;
 
-    for (int j = 0; j < cols; ++j) {
-        int well_fill = 0;
-        for (int i = 0; i < rows; ++i) {
-            well_fill += grid[i][j];
+        for (int j = 0; j < cols; ++j) {
+            int well_fill = 0;
+            for (int i = 0; i < rows; ++i) {
+                well_fill += grid[i][j];
+            }
+            total_fill += well_fill;
         }
-        total_fill += well_fill;
+
+        int num_operations = 0;
+        while (total_fill > 0) {
+            total_fill -= min(total_fill, capacity * rows);
+            num_operations++;
+        }
+
+        return num_operations;
     }
 
-    int num_operations = 0;
-    while (total_fill > 0) {
-        total_fill -= min(total_fill, capacity * rows);
-        num_operations++;
+    int main() {
+        assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);
+        return 0;
     }
-
-    return num_operations;
-}
-
-int main() {
-    assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);
-    return 0;
-}
