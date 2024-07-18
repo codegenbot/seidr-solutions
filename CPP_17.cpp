3717d71fab76f6d2004288e73e3af575c5bd0ce6
beep boop <githubbot@vadim.me>
2024-07-18 23:22:23 +0200

    Solution 37 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7r7w2c5w Prompt: c9724867-8343-496d-beea-8ba578063561.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ c9724867-8343-496d-beea-8ba578063561.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ c9724867-8343-496d-beea-8ba578063561.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ c9724867-8343-496d-beea-8ba578063561.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main(){       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -8,17 +8,17 @@ vector<int> parse_music(string music_string){
     vector<int> beats;
     int length = music_string.length();
     for(int i=0; i<length; i+=2){
-	if(music_string[i] == 'o'){
-	    beats.push_back(2);
-	} else if(music_string[i] == '.'){
-	    beats.push_back(1);
-	}
+        if(music_string[i] == 'o'){
+            beats.push_back(2);
+        } else if(music_string[i] == '.'){
+            beats.push_back(1);
+        }
     }
     return beats;
 }
 
 int main(){
-    assert(parse_music("o| .| o| .| o o| o o|") == vector<int>{2, 1, 2, 1, 2, 2, 2, 2});
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 2, 2, 2, 2}));
     
     return 0;
 }
