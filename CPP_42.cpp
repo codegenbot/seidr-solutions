b419d64e2f76f62c15b8925af443ad233df00dab
beep boop <githubbot@vadim.me>
2024-07-18 16:25:33 +0200

    Solution 38 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4l9354hn Prompt:    13 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ fab19fe6-896f-490d-a123-1eb913b7282a.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fab19fe6-896f-490d-a123-1eb913b7282a.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from fab19fe6-896f-490d-a123-1eb913b7282a.cpp:12: fab19fe6-896f-490d-a123-1eb913b7282a.cpp: In function ‘int main()’: fab19fe6-896f-490d-a123-1eb913b7282a.cpp:22:20: error: ‘incr_list’ was not decla red in this scope    22 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ fab19fe6-896f-490d-a123-1eb913b7282a.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -2,18 +2,7 @@
 #include <assert.h>
 
 vector<int> incr_list(vector<int> l);
-
 bool issame(vector<int> a, vector<int> b);
-
-int main() {
-    vector<int> l = {1, 2, 3};
-    vector<int> l_inc = incr_list(l);
-    vector<int> l_copy = {2, 3, 4};
-  
-    assert(issame(l_inc, l_copy));
-
-    return 0;
-}
 
 vector<int> incr_list(vector<int> l) {
     for (int &num : l) {
@@ -24,4 +13,15 @@ vector<int> incr_list(vector<int> l) {
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
+}
+
+int main() {
+    vector<int> list1 = {1, 2, 3};
+    vector<int> list2 = {1, 2, 4};
+
+    vector<int> incremented_list1 = incr_list(list1);
+    assert(!issame(list1, list2));
+    assert(issame(list1, incremented_list1));
+
+    return 0;
 }
