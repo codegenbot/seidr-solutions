e113fea2e7e19ae441efb669ff3acfdf63f92587
beep boop <githubbot@vadim.me>
2024-07-19 01:08:57 +0200

    Solution 33 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8ek5c5c8 Prompt:       |                                   ^~~~~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:19: 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp: In function ‘int main()’: 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -2,9 +2,9 @@
 
 bool issame(const vector<int>& a, const vector<int>& b){
     if(a[0] == b[0]){
-        return a[1] == b[1];
+        return a[1] > b[1];
     }
-    return false;
+    return a[0] < b[0];
 }
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
@@ -16,11 +16,6 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
             }
         }
     }
-    sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b){
-        if(a[0] == b[0]){
-            return a[1] > b[1];
-        }
-        return a[0] < b[0];
-    });
+    sort(result.begin(), result.end(), issame);
     return result;
 }
