f88b4722bcb83a8d7d87d36d59771d69ec6e0bb1
beep boop <githubbot@vadim.me>
2024-07-19 00:24:53 +0200

    Solution 48 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2h9ycuvd Prompt:       |                    ^~~ 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp:12: 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp: In function ‘int main()’: 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 9ac7dd13-ef5e-45dc-9e4f-952c41cc0124.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,21 +1,5 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-vector<int> intersperse(vector<int> numbers, int delimiter){ 
-    vector<int> result;
-    for (int i = 0; i < numbers.size(); ++i) {
-        result.push_back(numbers[i]);
-        if (i < numbers.size() - 1) {
-            result.push_back(delimiter);
-        }
-    }
-    return result;
-}
-
-int main() {
-    assert (issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
-    return 0;
-}
+bool issame(std::vector<int> a, std::vector<int> b){
+    std::vector<int> intersperse(std::vector<int> numbers, int delimeter);
+    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
