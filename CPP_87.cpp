51ea2d00575e3e133764938fb0d039853f82d21c
beep boop <githubbot@vadim.me>
2024-07-19 01:08:40 +0200

    Solution 31 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8ek5c5c8 Prompt:       |                                   ^~~~~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:19: 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp: In function ‘int main()’: 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 2a25f1e4-4eaa-44b0-92cf-efa9a568dd7e.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -4,12 +4,6 @@
 
 bool issame(vector<vector<int>> a, vector<vector<int>> b);
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x);
-
-bool issame(vector<vector<int>> a, vector<vector<int>> b){
-    return a == b;
-}
-
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
     vector<vector<int>> result;
     for(int i = 0; i < lst.size(); i++){
@@ -26,4 +20,8 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
         return a[0] < b[0];
     });
     return result;
+}
+
+bool issame(vector<vector<int>> a, vector<vector<int>> b){
+    return a == b;
 }
