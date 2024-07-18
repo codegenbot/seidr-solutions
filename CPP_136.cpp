e66f7822eab8f893ecfc58b1e91d430063d1fd79
beep boop <githubbot@vadim.me>
2024-07-19 01:31:45 +0200

    Solution 16 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/61xlll96 Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c0c52d6b-1c26-4cff-9e76-48f44130ac4f.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c0c52d6b-1c26-4cff-9e76-48f44130ac4f.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c0c52d6b-1c26-4cff-9e76-48f44130ac4f.cpp:13: c0c52d6b-1c26-4cff-9e76-48f44130ac4f.cpp: In function ‘int main()’: c0c52d6b-1c26-4cff-9e76-48f44130ac4f.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ c0c52d6b-1c26-4cff-9e76-48f44130ac4f.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,14 +1,17 @@
-#include <vector>
-#include <algorithm>
-
-vector<int> largest_smallest_integers(vector<int> lst){
-    int max_negative = 0, min_positive = 0;
+pair<int, int> largest_smallest_integers(vector<int> lst){
+    int max_neg = 0, min_pos = 0;
     for (int num : lst) {
-        if (num < 0 && num < max_negative) {
-            max_negative = num;
-        } else if (num > 0 && (num < min_positive || min_positive == 0)) {
-            min_positive = num;
+        if (num < 0 && num < max_neg) {
+            max_neg = num;
+        } else if (num > 0 && (num < min_pos || min_pos == 0)) {
+            min_pos = num;
         }
     }
-    return {max_negative, min_positive};
+    return {max_neg, min_pos};
+}
+
+int main(){
+    auto result = largest_smallest_integers({-6, -4, -4, -3, -100, 1});
+    assert(result.first == -3 && result.second == 1);
+    return 0;
 }
