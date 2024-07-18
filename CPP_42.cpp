a4ec020ab75f176c676692d7b2316f9613e6cc50
beep boop <githubbot@vadim.me>
2024-07-18 16:39:17 +0200

    Solution 69 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4l9354hn Prompt:    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 20173a7f-fb02-4aa0-90cb-b95fd8e817ea.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 20173a7f-fb02-4aa0-90cb-b95fd8e817ea.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 20173a7f-fb02-4aa0-90cb-b95fd8e817ea.cpp:28: 20173a7f-fb02-4aa0-90cb-b95fd8e817ea.cpp: In function ‘int main()’: 20173a7f-fb02-4aa0-90cb-b95fd8e817ea.cpp:38:20: error: ‘incr_list’ was not decla red in this scope    38 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 20173a7f-fb02-4aa0-90cb-b95fd8e817ea.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -4,6 +4,15 @@
 std::vector<int> incr_list(std::vector<int> l);
 bool issame(std::vector<int> a, std::vector<int> b);
 
+int main() {
+    std::vector<int> l1 = {1, 2, 3};
+    std::vector<int> l2 = incr_list(l1);
+
+    assert(issame(l1, l2));
+
+    return 0;
+}
+
 std::vector<int> incr_list(std::vector<int> l) {
     for (int &num : l) {
         num++;
@@ -13,13 +22,4 @@ std::vector<int> incr_list(std::vector<int> l) {
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
-}
-
-int main() {
-    std::vector<int> l1 = {1, 2, 3};
-    std::vector<int> l2 = incr_list(l1);
-
-    assert(issame(l1, l2));
-
-    return 0;
 }
