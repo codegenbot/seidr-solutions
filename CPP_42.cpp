1445408092fa06d4d9c4807eedf784a9d2266daa
beep boop <githubbot@vadim.me>
2024-07-18 16:50:34 +0200

    Solution 18 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zroziyya Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a8d7c789-fab9-40b3-aa07-055c174522b8.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ a8d7c789-fab9-40b3-aa07-055c174522b8.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from a8d7c789-fab9-40b3-aa07-055c174522b8.cpp:21: a8d7c789-fab9-40b3-aa07-055c174522b8.cpp: In function ‘int main()’: a8d7c789-fab9-40b3-aa07-055c174522b8.cpp:31:20: error: ‘incr_list’ was not decla red in this scope    31 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ a8d7c789-fab9-40b3-aa07-055c174522b8.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,4 +1,7 @@
+#include <cassert>
 #include <vector>
+
+vector<int> incr_list(vector<int> l);
 
 vector<int> incr_list(vector<int> l) {
     for (int &num : l) {
@@ -9,4 +12,9 @@ vector<int> incr_list(vector<int> l) {
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
+}
+
+int main() {
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    return 0;
 }
