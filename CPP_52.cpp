486eec20e5142b8b989c31d58dde75769951ead5
beep boop <githubbot@vadim.me>
2024-07-18 19:08:11 +0200

    Solution 29 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ipsi8949 Prompt: 7bca1880-11f8-4257-bc93-494fe21a4100.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 7bca1880-11f8-4257-bc93-494fe21a4100.cpp:17: 7bca1880-11f8-4257-bc93-494fe21a4100.cpp: In function ‘int main()’: 7bca1880-11f8-4257-bc93-494fe21a4100.cpp:19:32: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    19 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 7bca1880-11f8-4257-bc93-494fe21a4100.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 7bca1880-11f8-4257-bc93-494fe21a4100.cpp:4:40: note:   initializing argument 1 o f ‘bool below_threshold(std::vector<int>&, int)’     4 | bool below_threshold(std::vector<int>& l, int t) {       |                      ~~~~~~~~~~~~~~~~~~^

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,15 +1,3 @@
-#include <vector>
-#include <cassert>
-
-bool below_threshold(std::vector<int>& l, int t) {
-    for(int num : l) {
-        if(num >= t) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
     std::vector<int> nums = {1, 8, 4, 10};
     assert(!below_threshold(nums, 10));
