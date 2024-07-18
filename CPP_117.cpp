a7f83c6c9959df1a5aa139dea9624a4f12bc9f8b
beep boop <githubbot@vadim.me>
2024-07-19 01:27:56 +0200

    Solution 80 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/licr75sj Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d598e308-62c6-4638-9d28-d3e5cfbf3644.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d598e308-62c6-4638-9d28-d3e5cfbf3644.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d598e308-62c6-4638-9d28-d3e5cfbf3644.cpp:26: d598e308-62c6-4638-9d28-d3e5cfbf3644.cpp: In function ‘int main()’: d598e308-62c6-4638-9d28-d3e5cfbf3644.cpp:36:20: error: ‘select_words’ was not de clared in this scope    36 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ d598e308-62c6-4638-9d28-d3e5cfbf3644.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -2,34 +2,33 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> select_words(string s, int n) {
-    vector<string> result;
-    string word = "";
-    int consonant_count = 0;
-
-    for (char c : s) {
-        if (c == ' ') {
-            if (consonant_count == n) {
-                result.push_back(word);
-            }
-            word = "";
-            consonant_count = 0;
-        } else if (isalpha(c)) {
-            if (tolower(c) != 'a' && tolower(c) != 'e' && tolower(c) != 'i' && tolower(c) != 'o' && tolower(c) != 'u') {
-                consonant_count++;
-            }
-            word += c;
-        }
-    }
-
-    if (!word.empty() && consonant_count == n) {
-        result.push_back(word);
-    }
-
-    return result;
-}
+vector<string> select_words(string s, int n);
 
 int main() {
     assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
-    return 0;
+}
+
+vector<string> select_words(string s, int n) {
+    vector<string> result;
+    string word = "";
+    for (char c : s) {
+        if (c == ' ') {
+            int consonants = count_if(word.begin(), word.end(), [](char ch) {
+                return !strchr("aeiouAEIOU", ch) && isalpha(ch);
+            });
+            if (consonants == n) {
+                result.push_back(word);
+            }
+            word = "";
+        } else {
+            word += c;
+        }
+    }
+    int consonants = count_if(word.begin(), word.end(), [](char ch) {
+        return !strchr("aeiouAEIOU", ch) && isalpha(ch);
+    });
+    if (consonants == n) {
+        result.push_back(word);
+    }
+    return result;
 }
