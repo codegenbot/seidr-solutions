de9e54b04c9a15bb6d2c214c088216ea01394691
beep boop <githubbot@vadim.me>
2024-07-18 16:15:56 +0200

    Solution 48 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/6f3s3uth Prompt:       |                           ^~~~~~ 1f596347-caf9-44f5-abde-8cafd32b9af6.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1f596347-caf9-44f5-abde-8cafd32b9af6.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1f596347-caf9-44f5-abde-8cafd32b9af6.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1f596347-caf9-44f5-abde-8cafd32b9af6.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1f596347-caf9-44f5-abde-8cafd32b9af6.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 1f596347-caf9-44f5-abde-8cafd32b9af6.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <cassert>
 
@@ -17,4 +18,9 @@ std::vector<int> factorize(int n) {
 
 bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
+}
+
+int main() {
+    assert(issame(factorize(3 * 2 * 3) , {2, 3, 3}));
+    return 0;
 }
