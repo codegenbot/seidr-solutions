31cc75ec9506ad2ac8f3e30a2ebbd646d451018e
beep boop <githubbot@vadim.me>
2024-07-18 23:30:13 +0200

    Solution 22 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/a6d98qpt Prompt:       |                    ^~~ e405234a-8f27-48d3-821d-91ab216a9265.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e405234a-8f27-48d3-821d-91ab216a9265.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e405234a-8f27-48d3-821d-91ab216a9265.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e405234a-8f27-48d3-821d-91ab216a9265.cpp:12: e405234a-8f27-48d3-821d-91ab216a9265.cpp: In function ‘int main()’: e405234a-8f27-48d3-821d-91ab216a9265.cpp:22:20: error: ‘factorize’ was not decla red in this scope    22 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ e405234a-8f27-48d3-821d-91ab216a9265.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -7,3 +7,11 @@ using namespace std;
 bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
+
+int main(){
+    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
+    
+    cout << "Test passed!" << endl;
+    
+    return 0;
+}
