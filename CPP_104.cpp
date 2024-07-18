1b0cb0516abc34f4ca9be8bac9333ec2b35ebe74
beep boop <githubbot@vadim.me>
2024-07-18 16:19:31 +0200

    Solution 5 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qyqj0ek0 Prompt:       |                    ^~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:21: ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp: In function ‘int main()’: ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ ffbd1bb1-1464-4c6a-b58e-4315268a04aa.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,21 +1,20 @@
 #include <vector>
+#include <algorithm>
 
-bool is_same(const vector<int> &a, const vector<int> &b){
+bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
+    
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
-int main(){
-    vector<int> a = {1, 2, 3};
-    vector<int> b = {1, 2, 3};
-    assert(is_same(a, b));
-
-    return 0;
+int main() {
+    assert(issame(...));
 }
