51ac1cae15a595b9041d1ba1ca02e3a45e444b74
beep boop <githubbot@vadim.me>
2024-07-18 19:20:52 +0200

    Solution 44 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/fq3be9ks Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c51b2f6f-5868-4ca1-a1fc-47ee66325 420.py", line 4     return True     ^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_0.py
+++ Python_0.py
@@ -1,4 +1,4 @@
-def check_numbers(numbers, threshold):
+def has_close_numbers(numbers, threshold):
     numbers.sort()
     for i in range(len(numbers) - 1):
         if abs(numbers[i] - numbers[i + 1]) < threshold:
