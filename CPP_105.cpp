5f22fd20fbac385a116f61956c1fa0689d3667fd
beep boop <githubbot@vadim.me>
2024-07-19 01:15:36 +0200

    Solution 13 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/aubcb8hr Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,6 +1,5 @@
 vector<string> by_length(vector<int> arr){
-    vector<string> res;
-    map<int, string> numToStr = {
+    map<int, string> digit_map = {
         {1, "One"},
         {2, "Two"},
         {3, "Three"},
@@ -12,19 +11,20 @@ vector<string> by_length(vector<int> arr){
         {9, "Nine"}
     };
 
-    vector<int> valid_nums;
+    vector<int> sorted_digits;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            valid_nums.push_back(num);
+            sorted_digits.push_back(num);
         }
     }
 
-    sort(valid_nums.begin(), valid_nums.end());
-    reverse(valid_nums.begin(), valid_nums.end());
+    sort(sorted_digits.begin(), sorted_digits.end());
+    reverse(sorted_digits.begin(), sorted_digits.end());
 
-    for (int num : valid_nums) {
-        res.push_back(numToStr[num]);
+    vector<string> result;
+    for (int sorted_num : sorted_digits) {
+        result.push_back(digit_map[sorted_num]);
     }
 
-    return res;
+    return result;
 }
