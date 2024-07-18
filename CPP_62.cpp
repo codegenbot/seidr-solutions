50e2a971a6c4650a92e7190943b18928b7b5608b
beep boop <githubbot@vadim.me>
2024-07-19 01:33:23 +0200

    Solution 8 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/dyysjjmy Prompt:       |                    ^~~~~ 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp:9: 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp: In function ‘int main()’: 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 2b6fda3e-c135-4928-8682-c57845e7a4cf.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,23 +1,19 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
-
-vector<float> derivative(vector<float> xs);
-bool issame(vector<float> a, vector<float> b);
-
-int main() {
-    assert(issame(derivative({1}), {}));
-}
-
-vector<float> derivative(vector<float> xs) {
-    vector<float> result;
-    for (int i = 1; i < xs.size(); i++) {
+std::vector<float> derivative(std::vector<float> xs){
+    std::vector<float> result;
+    for(int i = 1; i < xs.size(); i++){
         result.push_back(xs[i] * i);
     }
     return result;
 }
 
-bool issame(vector<float> a, vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b){
     return a == b;
+}
+
+int main(){
+    assert(issame(derivative({1}), {}));
+    return 0;
 }
