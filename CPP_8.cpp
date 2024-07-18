7aee2603590da5356c5a7f6abed1b7f7fdda9a8b
beep boop <githubbot@vadim.me>
2024-07-18 22:39:51 +0200

    Solution 20 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/f8b9ftcs Prompt:       |                    ^~~ c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp:12: c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp: In function ‘int main()’: c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp:22:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    22 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ c42e8b93-8fbc-4ad9-942f-a48fcad5afb6.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,17 +1,23 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> sum_product(std::vector<int> numbers) {
+std::vector<int> sum_product(const std::vector<int>& numbers) {
     int sum = 0;
     int product = 1;
+    
     for (int num : numbers) {
         sum += num;
         product *= num;
     }
+    
     return {sum, product};
 }
+  
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
 
-assert(issame(sum_product({10}), {10, 10}));
+int main() {
+    assert(issame(sum_product({10}), {10, 10}));
+    
+    return 0;
+}
