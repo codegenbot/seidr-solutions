0e6af9bfe07ba0fd7b2c6deba45b7f650dbfaab9
beep boop <githubbot@vadim.me>
2024-07-18 17:14:41 +0200

    Solution 32 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sjf9pq1z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/630584b6-2979-4e17-8527-d2407acf4 4d1.py", line 16, in <module>     check(decode_cyclic)           ^^^^^^^^^^^^^ NameError: name 'decode_cyclic' is not defined

--- Python_38.py
+++ Python_38.py
@@ -1,3 +1,5 @@
-def decode_cyclic(groups):
+def rearrange_groups(groups):
     groups = [group[-1] + group[:-1] if len(group) == 3 else group for group in groups]
     return "".join(groups)
+
+check(rearrange_groups)
