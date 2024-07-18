2f9cd88c0131585643deb8692245e310d896a971
beep boop <githubbot@vadim.me>
2024-07-18 22:51:49 +0200

    Solution 20 of CPP/65, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i074105k Prompt: c98a902a-1027-4b9f-ac6b-09d3c09541ef.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string circular_shift(int x, int shift){       | ^~~~~~ In file included from c98a902a-1027-4b9f-ac6b-09d3c09541ef.cpp:18: c98a902a-1027-4b9f-ac6b-09d3c09541ef.cpp: In function ‘int main()’: c98a902a-1027-4b9f-ac6b-09d3c09541ef.cpp:20:13: error: ‘circular_shift’ was not declared in this scope    20 |     assert (circular_shift(11, 101) == "11");       |             ^~~~~~~~~~~~~~

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,10 +1,20 @@
-string str = to_string(x);
-    int n = str.size();
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string circular_shift(int x, int shift){
+    string numStr = to_string(x);
+    int n = numStr.size();
     shift %= n;
-    if (shift == 0) {
-        return str;
-    } else {
-        string shifted_str = str.substr(n - shift) + str.substr(0, n - shift);
-        return shifted_str;
+    if(shift == 0){
+        return numStr;
     }
+    if(shift > n){
+        reverse(numStr.begin(), numStr.end());
+        return numStr;
+    }
+    reverse(numStr.begin(), numStr.begin() + n - shift);
+    reverse(numStr.begin() + n - shift, numStr.end());
+    reverse(numStr.begin(), numStr.end());
+    return numStr;
 }
