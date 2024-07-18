c6423c75efd00b376fb72f8ba0e178f9de34eb92
beep boop <githubbot@vadim.me>
2024-07-18 17:44:01 +0200

    Solution 13 of CPP/55, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/45t4t2n2 Prompt: b310261d-0e71-44ec-bff9-f2703a5f199f.cpp: In function ‘int main()’: b310261d-0e71-44ec-bff9-f2703a5f199f.cpp:8:5: error: ‘assert’ was not declared i n this scope     8 |     assert(fib(12) == 144);       |     ^~~~~~ b310261d-0e71-44ec-bff9-f2703a5f199f.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | int fib(int n) { b310261d-0e71-44ec-bff9-f2703a5f199f.cpp: At global scope: b310261d-0e71-44ec-bff9-f2703a5f199f.cpp:13:5: error: redefinition of ‘int main( )’    13 | int main(){       |     ^~~~ b310261d-0e71-44ec-bff9-f2703a5f199f.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main() {       |     ^~~~

--- CPP_55.cpp
+++ CPP_55.cpp
@@ -1,9 +1,14 @@
 #include <cassert>
 
 int fib(int n) {
-    if (n <= 1)
-        return n;
-    return fib(n - 1) + fib(n - 2);
+    int fib[n + 2];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
+
+    return fib[n];
 }
 
 int main() {
