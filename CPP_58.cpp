d5687a9803c9dc366a8344ce0d62d58676386a52
beep boop <githubbot@vadim.me>
2024-07-19 01:49:44 +0200

    Solution 47 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kk1vwmeu Prompt:       |     ^~~~ In file included from 4a69456c-afc7-40b3-ad30-eafdbc4d1df0.cpp:23: 4a69456c-afc7-40b3-ad30-eafdbc4d1df0.cpp: In function ‘int main()’: 4a69456c-afc7-40b3-ad30-eafdbc4d1df0.cpp:33:26: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    33 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 4a69456c-afc7-40b3-ad30-eafdbc4d1df0.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 4a69456c-afc7-40b3-ad30-eafdbc4d1df0.cpp:5:43: note:   initializing argument 1 o f ‘std::vector<int> common(std::vector<int>&, std::vector<int>&)’     5 | std::vector<int> common(std::vector<int>& l1, std::vector<int>& l2) {       |                         ~~~~~~~~~~~~~~~~~~^~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,11 +5,14 @@
 std::vector<int> common(const std::vector<int>& l1, const std::vector<int>& l2) {
     std::vector<int> l1_copy(l1.begin(), l1.end());
     std::vector<int> l2_copy(l2.begin(), l2.end());
+    
     std::sort(l1_copy.begin(), l1_copy.end());
     std::sort(l2_copy.begin(), l2_copy.end());
+    
     std::vector<int> result;
     std::set_intersection(l1_copy.begin(), l1_copy.end(), l2_copy.begin(), l2_copy.end(), std::back_inserter(result));
     result.erase(std::unique(result.begin(), result.end()), result.end());
+    
     return result;
 }
 
