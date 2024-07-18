fb3555b55856e8778c931f5a01ffacc1092373ea
beep boop <githubbot@vadim.me>
2024-07-18 16:21:51 +0200

    Solution 9 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1189bgqw Prompt:       |                    ^~~ 124611cf-2c54-43b0-a811-870ccc3293cd.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 124611cf-2c54-43b0-a811-870ccc3293cd.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 124611cf-2c54-43b0-a811-870ccc3293cd.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 124611cf-2c54-43b0-a811-870ccc3293cd.cpp:15: 124611cf-2c54-43b0-a811-870ccc3293cd.cpp: In function ‘int main()’: 124611cf-2c54-43b0-a811-870ccc3293cd.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 124611cf-2c54-43b0-a811-870ccc3293cd.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -4,15 +4,15 @@
 
 using namespace std;
 
-vector<int> factorize(int n) {
+vector<int> factorize(int n){
     vector<int> factors;
-    for (int i = 2; i * i <= n; i++) {
-        while (n % i == 0) {
+    for(int i=2; i*i<=n; i++){
+        while(n%i == 0){
             factors.push_back(i);
             n /= i;
         }
     }
-    if (n > 1) {
+    if(n > 1){
         factors.push_back(n);
     }
     return factors;
@@ -22,7 +22,7 @@ bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-int main() {
+int main(){
     assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
 
     return 0;
