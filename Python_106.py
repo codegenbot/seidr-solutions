e7f4ddebda5d3eaf6762c5f1376ff04b6200a550
beep boop <githubbot@vadim.me>
2024-07-18 18:41:36 +0200

    Solution 68 of Python/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/h3k0fii7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac2f9949-1340-408d-aebd-6a39dfe0e c94.py", line 9, in <module>     check(f)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac2f9949-1340-408d-aebd-6a39dfe0e c94.py", line 6, in check     assert f(3) == [1, 2, 6]            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_106.py
+++ Python_106.py
@@ -1,4 +1,6 @@
-def f(n, memo={0: 1, 1: 1}):
+def f(n, memo={0: 1}):
+    if n == 0:
+        return [1]
     if n not in memo:
-        memo[n] = n * f(n - 1, memo)
+        memo[n] = n * f(n - 1, memo)[-1]
     return [memo[i] for i in range(n+1)]
