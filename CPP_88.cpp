63d9f6895b56c5cc428b54a4e5daca7e90a75467
beep boop <githubbot@vadim.me>
2024-07-18 17:13:57 +0200

    Solution 4 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fvg5q4xo Prompt:       |                    ^~~ d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp:9: d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp: In function ‘int main()’: d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp:19:20: error: ‘sort_array’ was not decl ared in this scope    19 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ d0acb3ee-0c18-4b27-ad14-ce282b2cc227.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <functional>
-#include <cassert>
 
-using namespace std;
+bool issame(vector<int>& a, vector<int>& b) {
+    return a == b;
+}
 
 vector<int> sort_array(vector<int> array) {
     if(array.empty()) return array;
@@ -13,13 +12,4 @@ vector<int> sort_array(vector<int> array) {
     else
         sort(array.begin(), array.end());
     return array;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
-    return 0;
 }
