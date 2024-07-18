17aa76893a3123c324170261d7e865233a2fc07e
beep boop <githubbot@vadim.me>
2024-07-18 17:35:51 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eiay1ole Prompt:       |                    ^~~ 09a4780a-d138-4d39-9d91-90cb9b379212.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 09a4780a-d138-4d39-9d91-90cb9b379212.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 09a4780a-d138-4d39-9d91-90cb9b379212.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 09a4780a-d138-4d39-9d91-90cb9b379212.cpp:9: 09a4780a-d138-4d39-9d91-90cb9b379212.cpp: In function ‘int main()’: 09a4780a-d138-4d39-9d91-90cb9b379212.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 09a4780a-d138-4d39-9d91-90cb9b379212.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,11 +1,16 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
 bool issame(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
     return a == b;
+}
+
+vector<int> common_elements(vector<int>& l1, vector<int>& l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
+    vector<int> common_elements;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(common_elements));
+    common_elements.erase(unique(common_elements.begin(), common_elements.end()), common_elements.end());
+    return common_elements;
 }
 
 int main() {
