e0a838f39b62c42b27067f799a71709a2bb1b86b
beep boop <githubbot@vadim.me>
2024-07-18 16:48:48 +0200

    Solution 14 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zroziyya Prompt:    13 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 23e655bd-4e21-48ba-8957-9f7b14bf5d71.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 23e655bd-4e21-48ba-8957-9f7b14bf5d71.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 23e655bd-4e21-48ba-8957-9f7b14bf5d71.cpp:12: 23e655bd-4e21-48ba-8957-9f7b14bf5d71.cpp: In function ‘int main()’: 23e655bd-4e21-48ba-8957-9f7b14bf5d71.cpp:22:20: error: ‘incr_list’ was not decla red in this scope    22 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 23e655bd-4e21-48ba-8957-9f7b14bf5d71.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,17 +1,13 @@
 #include <vector>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> incr_list(std::vector<int> l);
+vector<int> incr_list(vector<int> l);
 
-int main() {
-    assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
-    return 0;
-}
-
-std::vector<int> incr_list(std::vector<int> l) {
+vector<int> incr_list(vector<int> l) {
     for (int &num : l) {
         num++;
     }
