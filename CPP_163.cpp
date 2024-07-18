16dcb742bc0e326a268d783c05806d091791ea46
beep boop <githubbot@vadim.me>
2024-07-18 18:01:43 +0200

    Solution 14 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/qddz4bbx Prompt:       |                    ^~~ 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp:11: 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp: In function ‘int main()’: 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp:21:20: error: ‘generate_integers’ was n ot declared in this scope    21 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 0791b8fa-fa82-45fe-ac55-73263b4928fb.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -4,7 +4,15 @@
 vector<int> generate_integers(int a, int b);
 
 bool issame(vector<int> a, vector<int> b){
-    return (a == b);
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(size_t i = 0; i < a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<int> generate_integers(int a, int b){
@@ -18,10 +26,7 @@ vector<int> generate_integers(int a, int b){
 }
 
 int main() {
-    vector<int> first = generate_integers(1, 10);
-    vector<int> second = generate_integers(1, 10);
-    
-    assert(issame(first, second));
-    
+    assert(issame({2, 4, 6}, generate_integers(2, 6)));
+    assert(issame({-10, -8, -6, -4, -2, 0, 2}, generate_integers(-10, 2)));
     return 0;
 }
