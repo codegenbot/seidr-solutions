e99cbb02fe9c0146beb4e45324e5bea4d268c793
beep boop <githubbot@vadim.me>
2024-07-18 16:35:27 +0200

    Solution 38 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/mrc034hd Prompt: on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ b1040475-1281-4141-b771-aeae58e23a41.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b1040475-1281-4141-b771-aeae58e23a41.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from b1040475-1281-4141-b771-aeae58e23a41.cpp:11: b1040475-1281-4141-b771-aeae58e23a41.cpp: In function ‘int main()’: b1040475-1281-4141-b771-aeae58e23a41.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ b1040475-1281-4141-b771-aeae58e23a41.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -9,4 +9,4 @@ vector<int> remove_duplicates(vector<int> numbers) {
     sort(numbers.begin(), numbers.end());
     numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
-} 
+}
