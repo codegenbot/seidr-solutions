44584f73a024e675140ac8b2c8cab9301d4ead4b
beep boop <githubbot@vadim.me>
2024-07-18 18:49:48 +0200

    Solution 93 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/perj33wq Prompt:       |                              ^~~~~~ 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp:25: 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp: In function ‘int main()’: 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 85c32d14-1016-465d-a6c9-9affcbd80bbb.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,19 +1,7 @@
 #include <string>
 #include <cassert>
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<string> by_length(vector<int> arr);
+bool issame(vector<string> a, vector<string> b);
 
 vector<string> by_length(vector<int> arr){
     vector<string> result;
