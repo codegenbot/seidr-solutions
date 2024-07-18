ab29842183d017fcef1b3985c9dc33c8cd6eb43f
beep boop <githubbot@vadim.me>
2024-07-18 16:54:19 +0200

    Solution 19 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jykh6l4w Prompt: 5a3f23e5-2a72-4fab-a85f-aca94d1c510b.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    30 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 5a3f23e5-2a72-4fab-a85f-aca94d1c510b.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 5a3f23e5-2a72-4fab-a85f-aca94d1c510b.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 5a3f23e5-2a72-4fab-a85f-aca94d1c510b.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -17,7 +17,4 @@ vector<int> strange_sort_list(vector<int> lst){
     return result;
 }
 
-int main() {
-    assert(strange_sort_list({111111}) == vector<int>({111111}));
-    return 0;
-}
+assert(issame(strange_sort_list({111111}), {111111}));
