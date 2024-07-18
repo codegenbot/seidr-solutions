72d27834d05b852236732d18038577992e2bbfeb
beep boop <githubbot@vadim.me>
2024-07-18 16:26:14 +0200

    Solution 17 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/wq7oje32 Prompt:    26 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 6ae1a44b-4d06-40c3-86a2-9a9b61e49f68.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 6ae1a44b-4d06-40c3-86a2-9a9b61e49f68.cpp:26:43: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 6ae1a44b-4d06-40c3-86a2-9a9b61e49f68.cpp:25: 6ae1a44b-4d06-40c3-86a2-9a9b61e49f68.cpp: In function ‘int main()’: 6ae1a44b-4d06-40c3-86a2-9a9b61e49f68.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 6ae1a44b-4d06-40c3-86a2-9a9b61e49f68.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,8 +1,7 @@
 bool issame(vector<float> a, vector<float> b);
 
-bool sort_even(vector<float>& l) {
+void sort_even(vector<float>& l) {
     vector<float> even_vals;
-    vector<float> result;
 
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -15,12 +14,8 @@ bool sort_even(vector<float>& l) {
     int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            result.push_back(even_vals[even_index]);
+            l[i] = even_vals[even_index];
             even_index++;
-        } else {
-            result.push_back(l[i]);
         }
     }
-
-    return result;
-}  
+}
