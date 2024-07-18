067167dd4405fcf2984100145712398b9ac7c10a
beep boop <githubbot@vadim.me>
2024-07-19 00:25:28 +0200

    Solution 80 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/illce29z Prompt:       |                     ^~~~~~ f28276bd-b6d1-4716-8035-e96163b34180.cpp:11:13: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ f28276bd-b6d1-4716-8035-e96163b34180.cpp:11:20: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ f28276bd-b6d1-4716-8035-e96163b34180.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f28276bd-b6d1-4716-8035-e96163b34180.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from f28276bd-b6d1-4716-8035-e96163b34180.cpp:10: f28276bd-b6d1-4716-8035-e96163b34180.cpp: In function ‘int main()’: f28276bd-b6d1-4716-8035-e96163b34180.cpp:20:20: error: ‘eat’ was not declared in  this scope    20 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,15 +1,11 @@
-#include <utility>
 #include <vector>
 #include <cassert>
 
-std::vector<int> issame(std::vector<int> a, std::vector<int> b) {
-    if(a[0] == a[1] + b[1]) {
-        return {a[1], 0};
-    } else {
-        return {a[1] + b[1], 0};
-    }
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
 }
 
-int main() {
-    assert(issame({4, 5}, {5, 1}) == std::vector<int>({5, 0}));
+int main(){
+    assert (issame({4, 5, 1}, {4, 5, 1}));
+    return 0;
 }
