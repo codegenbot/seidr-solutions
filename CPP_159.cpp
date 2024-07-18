9e96fc70521cc1c396cd88e90736774545202eee
beep boop <githubbot@vadim.me>
2024-07-18 21:26:19 +0200

    Solution 18 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4c08o64d Prompt: )’    30 | int main(){       |     ^~~~ 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp:21: 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp: In function ‘int main()’: 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp:31:23: error: too many arguments to fun ction ‘std::pair<int, int> eat(int, int)’    31 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ~~~^~~~~~~~~ 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp:8:21: note: declared here     8 | std::pair<int, int> eat(int number, int remaining) {       |                     ^~~ In file included from 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp:21: 8a719005-2fb2-4149-a0ec-4d4929b63cc6.cpp:31:23: error: could not convert ‘eat(4,  5)’ from ‘std::pair<int, int>’ to ‘std::vector<int>’    31 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ~~~^~~~~~~~~       |                       |       |                       std::pair<int, int>

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -5,15 +5,14 @@ bool issame(std::pair<int, int> a, std::pair<int, int> b){
     return a == b;
 }
 
-std::pair<int, int> eat(int number, int need, int remaining) {
-    int total = number + need;
+std::pair<int, int> eat(int number, int remaining) {
+    int total = number + remaining;
     int eaten = total > remaining ? remaining : total;
     int left = remaining - eaten;
     return {eaten, left};
 }
 
 int main() {
-    assert (issame(eat(4, 5, 1), {5, 0}));
-    
+    assert(issame(eat(4, 5), {5, 0}));
     return 0;
 }
