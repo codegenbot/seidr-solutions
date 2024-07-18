b3ce021a10325b21c83ee6d716ed5a435f10c3a3
beep boop <githubbot@vadim.me>
2024-07-18 16:46:14 +0200

    Solution 3 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3cefkrqs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d25e6e9-d361-46ea-a35a-786f8b1df f52.py", line 1, in <module>     def parse_nested_parens(paren_string: str) -> List[int]:                                                   ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_6.py
+++ Python_6.py
@@ -7,7 +7,7 @@ def parse_nested_parens(paren_string: str) -> List[int]:
     for char in paren_string:
         if char == "(":
             current_depth += 1
-            depths.append(current_depth)
         elif char == ")":
+            depths.append(current_depth)
             current_depth -= 1
     return depths
