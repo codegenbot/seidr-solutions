b396c28fb94e7a92e93837b21d1da63b3f9fd3d3
beep boop <githubbot@vadim.me>
2024-07-18 23:44:40 +0200

    Solution 74 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7yb0hidi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a9fbbef-0e8b-44ee-80b9-c626e8871 6c0.py", line 13, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a9fbbef-0e8b-44ee-80b9-c626e8871 6c0.py", line 10, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,10 +1,7 @@
 import math
 
-
 def max_fill(grid, capacity):
     max_water = max(max(row) for row in grid)
     buckets_needed = math.ceil(max_water / capacity)
     total_water_needed = buckets_needed * capacity
-    return max(
-        buckets_needed * len(grid[0]) * len(grid) - sum(sum(row) for row in grid), 0
-    )
+    return max(buckets_needed * len(grid[0]) * len(grid) - sum(sum(row) for row in grid), 0)
