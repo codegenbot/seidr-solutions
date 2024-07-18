617d2418ae89428540f7dcad272651bbd3d1e057
beep boop <githubbot@vadim.me>
2024-07-19 01:52:09 +0200

    Solution 1 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4cz2lphy Prompt:       |                              ^~~~~~ ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp:9: ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp: In function ‘int main()’: ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ ebd5691d-ba50-42c4-a476-b341e8c11e10.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 vector<string> all_prefixes(string str){
     vector<string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+int main() {
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    cout << "Test Passed\n";
+    return 0;
 }
