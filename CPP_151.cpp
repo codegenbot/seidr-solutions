80eb326cd603f41ff426ba92702a4ebf8fa45d54
beep boop <githubbot@vadim.me>
2024-07-18 21:17:14 +0200

    Solution 7 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/uoumztts Prompt: 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst){       |                                 ^~~~~~ 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst){       |                                        ^~~~~ In file included from 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp:11: 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp: In function ‘int main()’: 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 67d272d8-b9cf-40e7-a814-7e981f0a7f8d.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,17 +1,18 @@
 #include <vector>
 
 long long double_the_difference(std::vector<float> lst){
-    long long odd_sum = 0;
+    long long sum = 0;
     for (float num : lst) {
         if (num > 0 && num == (int)num && (int)num % 2 != 0) {
-            odd_sum += (long long)num * num;
+            sum += (long long)num * num;
         }
     }
-    return odd_sum;
+    return sum;
 }
 
 int main() {
-    std::vector<float> lst = {1.5, 2, 3, 4.5, 5};
-    std::cout << double_the_difference(lst);
+    std::vector<float> lst = {2.5, 3, -4, 1.0, 5.5};
+    long long odd_sum = double_the_difference(lst);
+    
     return 0;
 }
