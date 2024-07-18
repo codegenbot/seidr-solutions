02bddd2dcba2b75a7a653692bf905440cb029905
beep boop <githubbot@vadim.me>
2024-07-18 23:25:55 +0200

    Solution 32 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/guq6v58r Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41d85bec-00b6-4544-8c43-24dc11e50 efa.py", line 28, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41d85bec-00b6-4544-8c43-24dc11e50 efa.py", line 25, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41d85bec-00b6-4544-8c43-24dc11e50 efa.py", line 19, in prime_fib     num = next(fib_gen)           ^^^^^^^^^^^^^ StopIteration

--- Python_39.py
+++ Python_39.py
@@ -8,11 +8,11 @@ def prime_fib(n: int):
     fib_gen = fibonacci()
     result = None
     try:
-        for _ in range(n):
+        while n > 0:
             num = next(fib_gen)
             if is_prime(num):
                 result = num
+                n -= 1
     except StopIteration:
         pass
-
     return result
