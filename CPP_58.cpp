220a7cdb9d6a10ab1544b014bf51bcb9d04ffa04
beep boop <githubbot@vadim.me>
2024-07-18 19:12:07 +0200

    Solution 36 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/eiay1ole Prompt:       |                           ^~~~~~ 333cc390-34da-4244-b604-5aca99a3590b.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 333cc390-34da-4244-b604-5aca99a3590b.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 333cc390-34da-4244-b604-5aca99a3590b.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 333cc390-34da-4244-b604-5aca99a3590b.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 333cc390-34da-4244-b604-5aca99a3590b.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 333cc390-34da-4244-b604-5aca99a3590b.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
+#include <assert.h>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
@@ -17,6 +17,6 @@ std::vector<int> common(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), std::vector<int>{}));
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
