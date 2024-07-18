7100f2c8ad3ede05dfafa84b2b9dde33375335d3
beep boop <githubbot@vadim.me>
2024-07-18 16:57:28 +0200

    Solution 62 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5z59ourz Prompt:    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:37: ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp: In function ‘int main()’: ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:47:20: error: ‘sort_third’ was not decl ared in this scope    47 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ ffc5f6df-d4d2-46f3-a2aa-1474a87a8371.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -9,25 +9,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 vector<int> sort_third(vector<int> l);
 
-vector<int> sort_third(vector<int> l){
-    vector<int> sorted_indices;
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            sorted_indices.push_back(l[i]);
-        }
-    }
-    sort(sorted_indices.begin(), sorted_indices.end());
-
-    int sorted_idx = 0;
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            l[i] = sorted_indices[sorted_idx++];
-        }
-    }
-    
-    return l;
-}
-
 int main() {
     vector<int> input = {7, 3, 2, 4, 9, 1, 5, 8, 6};
     vector<int> expected_output = {2, 3, 1, 4, 9, 5, 6, 8, 7};
