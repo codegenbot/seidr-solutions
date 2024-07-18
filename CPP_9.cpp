3ed1b713b77bddb9e7f37e8f1aaac1647736cced
beep boop <githubbot@vadim.me>
2024-07-19 00:28:15 +0200

    Solution 29 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pqphr2io Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:16: e5aa2de5-20c8-47ae-a584-f7460012a100.cpp: In function ‘int main()’: e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:26:20: error: ‘rolling_max’ was not dec lared in this scope    26 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ e5aa2de5-20c8-47ae-a584-f7460012a100.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,18 +1,16 @@
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
     int n = numbers.size();
+    deque<int> q;
     for (int i = 0; i < n; ++i) {
-        int max_val = numbers[i];
-        for (int j = i + 1; j < n; ++j) {
-            if (numbers[j] > max_val) {
-                max_val = numbers[j];
-            }
+        while (!q.empty() && q.front() < i) {
+            q.pop_front();
         }
-        result.push_back(max_val);
+        while (!q.empty() && numbers[q.back()] < numbers[i]) {
+            q.pop_back();
+        }
+        q.push_back(i);
+        result.push_back(numbers[q.front()]);
     }
     return result;
 }
