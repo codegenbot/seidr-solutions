62484c4edea4958ef4427f0c9349be84a29d1188
beep boop <githubbot@vadim.me>
2024-07-18 23:40:07 +0200

    Solution 64 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/k5eiep43 Prompt: 1d84430e-b43a-4f58-8c07-752320c88760.cpp: In function ‘int main()’: 1d84430e-b43a-4f58-8c07-752320c88760.cpp:6:5: error: ‘assert’ was not declared i n this scope     6 |     assert(str.length() == 9);       |     ^~~~~~ 1d84430e-b43a-4f58-8c07-752320c88760.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cstring>   +++ |+#include <cassert>     3 | 1d84430e-b43a-4f58-8c07-752320c88760.cpp: At global scope: 1d84430e-b43a-4f58-8c07-752320c88760.cpp:10:5: error: redefinition of ‘int main( )’    10 | int main(){       |     ^~~~ 1d84430e-b43a-4f58-8c07-752320c88760.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <cassert> // Include needed header
+#include <cassert>
 
 int main() {
     std::string str = "asdasnakj";
-    assert(str.length() == 9); // Assert the condition
+    assert(str.length() == 9);
 }
