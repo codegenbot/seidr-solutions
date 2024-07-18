f9d75d595ebb2e04d8b2c3e77dae7a9876226238
beep boop <githubbot@vadim.me>
2024-07-18 17:54:50 +0200

    Solution 8 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xs1dg4af Prompt:       |                                            ^ 31d26635-b659-41e3-a3ba-59c65819df56.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 31d26635-b659-41e3-a3ba-59c65819df56.cpp:15: 31d26635-b659-41e3-a3ba-59c65819df56.cpp: In function ‘int main()’: 31d26635-b659-41e3-a3ba-59c65819df56.cpp:25:20: error: ‘odd_count’ was not decla red in this scope    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ 31d26635-b659-41e3-a3ba-59c65819df56.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    26 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,4 +1,4 @@
-bool issame(vector<string> a, vector<string> b) {
+bool odd_count(vector<string> a, vector<string> b) {
     return a == b;
 }
 
@@ -14,13 +14,4 @@ vector<string> odd_count(vector<string> lst) {
         result.push_back("the number of odd elements " + to_string(odd_count) + " in the string " + s + " of the input.");
     }
     return result;
-}
-
-int main() {
-    assert(issame(odd_count({"271", "137", "314"}), {
-        "the number of odd elements 2 in the string 271 of the input.",
-        "the number of odd elements 3 in the string 137 of the input.",
-        "the number of odd elements 2 in the string 314 of the input."
-    }));
-    return 0;
 }
