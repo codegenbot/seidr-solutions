b0061672149b82fe3249ac54a0531015eccc56ad
beep boop <githubbot@vadim.me>
2024-07-18 23:33:32 +0200

    Solution 69 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7r7w2c5w Prompt:    92 | #  define assert(expr) \       | 214b52ce-ff2f-4413-9000-1de50e30a3a7.cpp: In function ‘int main()’: 214b52ce-ff2f-4413-9000-1de50e30a3a7.cpp:21:5: error: ‘assert’ was not declared in this scope    21 |     assert(parse_music("o| .| o| .| o o| o o|") == vector<int>{2, 1, 2, 1, 2, 2, 2, 2});       |     ^~~~~~ 214b52ce-ff2f-4413-9000-1de50e30a3a7.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | 214b52ce-ff2f-4413-9000-1de50e30a3a7.cpp: At global scope: 214b52ce-ff2f-4413-9000-1de50e30a3a7.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 214b52ce-ff2f-4413-9000-1de50e30a3a7.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main(){       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <cassert>
@@ -8,11 +9,11 @@ vector<int> parse_music(string music_string){
     vector<int> beats;
     int length = music_string.length();
     for(int i=0; i<length; i+=2){
-	if(music_string[i] == 'o'){
-	    beats.push_back(2);
-	} else if(music_string[i] == '.'){
-	    beats.push_back(1);
-	}
+        if(music_string[i] == 'o'){
+            beats.push_back(2);
+        } else if(music_string[i] == '.'){
+            beats.push_back(1);
+        }
     }
     return beats;
 }
