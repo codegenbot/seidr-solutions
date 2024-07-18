a969449b88ac78b3d7ca91ddc5f339b79736cf8e
beep boop <githubbot@vadim.me>
2024-07-18 17:11:22 +0200

    Solution 84 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qyqj0ek0 Prompt:       |                                  ^~~ a41a96b6-e964-4590-94a4-da3747c1f46a.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a41a96b6-e964-4590-94a4-da3747c1f46a.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ a41a96b6-e964-4590-94a4-da3747c1f46a.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from a41a96b6-e964-4590-94a4-da3747c1f46a.cpp:6: a41a96b6-e964-4590-94a4-da3747c1f46a.cpp: In function ‘int main()’: a41a96b6-e964-4590-94a4-da3747c1f46a.cpp:16:20: error: ‘unique_digits’ was not d eclared in this scope    16 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ a41a96b6-e964-4590-94a4-da3747c1f46a.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -3,38 +3,19 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
+std::vector<int> unique_digits(std::vector<int> nums) {
+    // Implement unique_digits function
+    std::sort(nums.begin(), nums.end());
+    nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    return nums;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    // Implement issame function
     return a == b;
 }
 
-std::vector<int> unique_digits(std::vector<int> nums){
-    std::vector<int> result;
-    
-    for(int num : nums){
-        std::vector<int> digits;
-        while(num > 0){
-            digits.push_back(num%10);
-            num /= 10;
-        }
-        
-        std::sort(digits.begin(), digits.end());
-        int unique_num = 0;
-        
-        for(int digit : digits){
-            unique_num = unique_num * 10 + digit;
-        }
-        
-        if(std::find(result.begin(), result.end(), unique_num) == result.end()){
-            result.push_back(unique_num);
-        }
-    }
-    
-    return result;
-}
-
-int main(){
+int main() {
     assert(issame(unique_digits({135, 103, 31}), {31, 135}));
     return 0;
 }
