dd659bf0c0bd0170cb23fe11774a0d332139364c
beep boop <githubbot@vadim.me>
2024-07-18 21:26:17 +0200

    Solution 66 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/xooa38es Prompt:       |                                 ^~~~~~ In file included from 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main(){       |     ^~~~ In file included from 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp:13: 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp: In function ‘int main()’: 4c960997-60ce-4ea6-961c-6a34496f30e4.cpp:23:20: error: ‘bf’ was not declared in this scope    23 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,9 +3,10 @@
 #include <string>
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
-    return a.empty() && b.empty();
+    return a == b;
 }
 
 int main(){
-    assert(issame(std::vector<std::string>{"Jupiter", "Makemake"}, std::vector<std::string>{}));
+    std::vector<std::string> bf{"Jupiter", "Makemake"};
+    assert(issame(bf, {}));
 }
