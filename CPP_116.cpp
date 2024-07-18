5828c361341b6dbbabb02493bd30aa3363d29127
beep boop <githubbot@vadim.me>
2024-07-18 20:54:46 +0200

    Solution 47 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1wiuvt4w Prompt:   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:71,                  from /usr/include/c++/11/vector:60,                  from abeda01e-e260-4cd4-aa39-52149a1dfef1.cpp:1: /usr/include/c++/11/bits/predefined_ops.h: At global scope: /usr/include/c++/11/bits/predefined_ops.h:187:7: warning: ‘__gnu_cxx::__ops::_It er_comp_val<_Compare>::_Iter_comp_val(__gnu_cxx::__ops::_Iter_comp_iter<_Compare >&&) [with _Compare = sort_array(const std::vector<int>&)::<lambda(int, int)>]’ used but never defined   187 |       _Iter_comp_val(_Iter_comp_iter<_Compare>&& __comp)       |       ^~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -16,10 +16,6 @@ vector<int> sort_array(vector<int>& arr) {
     return arr;
 }
 
-bool issame(const vector<int>& a, const vector<int>& b){
-    return a == b;
-}
-
 int main() {
     assert(issame(sort_array({2, 4, 8, 16, 32}), vector<int>{2, 4, 8, 16, 32}));
     return 0;
