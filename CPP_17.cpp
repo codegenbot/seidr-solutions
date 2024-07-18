db01ab2091bf7eba93600f74a45b3ed5d47624b0
beep boop <githubbot@vadim.me>
2024-07-18 23:37:45 +0200

    Solution 76 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7r7w2c5w Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 0c31ce73-604c-4949-816d-062adfe6d9e1.cpp:3: 0c31ce73-604c-4949-816d-062adfe6d9e1.cpp: In function ‘int main()’: 0c31ce73-604c-4949-816d-062adfe6d9e1.cpp:21:12: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    21 |     assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 2, 2, 2, 2}));       |            ^~~~~~ 0c31ce73-604c-4949-816d-062adfe6d9e1.cpp: At global scope: 0c31ce73-604c-4949-816d-062adfe6d9e1.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 0c31ce73-604c-4949-816d-062adfe6d9e1.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main(){       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -7,7 +7,7 @@ using namespace std;
 vector<int> parse_music(string music_string){
     vector<int> beats;
     int length = music_string.length();
-    for(int i=0; i<length; i+=3){
+    for(int i=0; i<length; i+=2){
         if(music_string[i] == 'o'){
             beats.push_back(2);
         } else if(music_string[i] == '.'){
