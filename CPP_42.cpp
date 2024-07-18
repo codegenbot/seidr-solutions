33fc23f384363cf1402e298d0d43ac4803e242ec
beep boop <githubbot@vadim.me>
2024-07-18 23:49:27 +0200

    Solution 32 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vio88gbt Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8c4c4c2a-90d7-4d75-8af7-397abb765937.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8c4c4c2a-90d7-4d75-8af7-397abb765937.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8c4c4c2a-90d7-4d75-8af7-397abb765937.cpp:7: 8c4c4c2a-90d7-4d75-8af7-397abb765937.cpp: In function ‘int main()’: 8c4c4c2a-90d7-4d75-8af7-397abb765937.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 8c4c4c2a-90d7-4d75-8af7-397abb765937.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,16 +1,11 @@
-
-bool issame(vector<int> a, vector<int> b) { 
-    return a == b; 
+bool issame(const vector<int>& a, const vector<int>& b){
+    if(a.size() != b.size()) return false;
+    return equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> incr_list(vector<int> l) { 
-    for(int i = 0; i < l.size(); i++) {
-        l[i]++;
-    } 
-    return l; 
-}
-
-int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
-    return 0;
+vector<int> incr_list(vector<int> l){
+    for(int& num : l){
+        num++;
+    }
+    return l;
 }
