d6ebd34eae91d41c4c9252ce40d67d507e79bee3
beep boop <githubbot@vadim.me>
2024-07-18 19:04:04 +0200

    Solution 1 of Python/161, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sl95k2ez Prompt: Complete the following code given the task description and function signature.

--- Python_161.py
+++ Python_161.py
@@ -1,8 +1,2 @@
 def solve(s):
-    return (
-        "".join(
-            c.upper() if c.islower() else c.lower() if c.isupper() else c for c in s
-        )[::-1]
-        if any(char.isalpha() for char in s)
-        else s[::-1]
-    )
+    return s.swapcase() if any(char.isalpha() for char in s) else s[::-1]
