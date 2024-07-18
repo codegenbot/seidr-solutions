1088ef858adae4d810451952bf3706b71573c5a6
beep boop <githubbot@vadim.me>
2024-07-18 16:33:29 +0200

    Solution 34 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/mrc034hd Prompt: in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f53d5edd-2472-4e64-9166-55b2c058ad6d.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f53d5edd-2472-4e64-9166-55b2c058ad6d.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f53d5edd-2472-4e64-9166-55b2c058ad6d.cpp:5: f53d5edd-2472-4e64-9166-55b2c058ad6d.cpp: In function ‘int main()’: f53d5edd-2472-4e64-9166-55b2c058ad6d.cpp:15:20: error: ‘remove_duplicates’ was n ot declared in this scope    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ f53d5edd-2472-4e64-9166-55b2c058ad6d.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,30 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 vector<int> remove_duplicates(vector<int> numbers){
     sort(numbers.begin(), numbers.end());
     numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
 }
 
-int main() {
-    vector<int> numbers;
-    int n, num;
-    
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-        numbers.push_back(num);
-    }
-    
-    vector<int> uniqueNumbers = remove_duplicates(numbers);
-    
-    for (int i = 0; i < uniqueNumbers.size(); i++) {
-        cout << uniqueNumbers[i] << " ";
-    }
-    
+int main(){
+    // Main function implementation
     return 0;
 }
