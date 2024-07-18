83ec587811bfaaf356529b6e5f6e647992b20e29
beep boop <githubbot@vadim.me>
2024-07-18 16:56:25 +0200

    Solution 33 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/lg2a35fs Prompt: 765995c0-1014-483f-beba-eddef9aeb6aa.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 765995c0-1014-483f-beba-eddef9aeb6aa.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~ In file included from 765995c0-1014-483f-beba-eddef9aeb6aa.cpp:22: 765995c0-1014-483f-beba-eddef9aeb6aa.cpp: In function ‘int main()’: 765995c0-1014-483f-beba-eddef9aeb6aa.cpp:24:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    24 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -12,7 +12,7 @@ std::string flip_case(std::string str){
 }
 
 int main(){
-    assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
+    assert (flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
 
     return 0;
 }
