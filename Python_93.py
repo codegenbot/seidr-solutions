5794c623d720b2995a01706ec10b9fdb63db74cb
beep boop <githubbot@vadim.me>
2024-07-18 18:48:35 +0200

    Solution 99 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9urf17rl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9411cda-98dc-41ce-8bfe-2b2292894 96b.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9411cda-98dc-41ce-8bfe-2b2292894 96b.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,7 +2,7 @@ def encode(message):
     encoded_message = ""
     for char in message:
         if char.isalpha():
-            shift = 2  # Shift value changed to 2
+            shift = 2
             encoded_char = chr((ord(char.lower()) - ord("a") + shift) % 26 + ord("a"))
             encoded_message += encoded_char.upper() if char.isupper() else encoded_char
         else:
