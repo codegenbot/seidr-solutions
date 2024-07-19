b5c85c9e7ff6d01916946363166f2ccee9763ff3
beep boop <githubbot@vadim.me>
2024-07-19 03:10:16 +0200

    Solution 12 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1hc4poj8 Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 93af9800-720d-45d5-89f1-8b08aef10b0d.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 93af9800-720d-45d5-89f1-8b08aef10b0d.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 93af9800-720d-45d5-89f1-8b08aef10b0d.cpp:16: 93af9800-720d-45d5-89f1-8b08aef10b0d.cpp: In function ‘int main()’: 93af9800-720d-45d5-89f1-8b08aef10b0d.cpp:26:20: error: ‘count_up_to’ was not dec lared in this scope    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 93af9800-720d-45d5-89f1-8b08aef10b0d.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,1 +1,31 @@
-bool issame(vector<int> a, vector<int> b) {
+#include <vector>
+#include <cassert>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(size_t i=0; i<a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<int> count_up_to(int n) {
+    std::vector<int> result;
+    for(int i=2; i<n; i++){
+        bool isPrime = true;
+        for(int j=2; j*j<=i; j++){
+            if(i % j == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+            result.push_back(i);
+        }
+    }
+    return result;
+}
