033dbc7e22cc4d114a43e7d0f854c5a149ab2abe
beep boop <githubbot@vadim.me>
2024-07-19 04:40:45 +0200

    Solution 20 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9smdgoum Prompt:       |                    ^~~ 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp:21: 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp: In function ‘int main()’: 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 734d8de5-1ae3-4fda-92fb-3325cf09c568.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,5 +1,34 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+
+using namespace std;
+
+bool issame(int a, int b) {
+    vector<int> digits_a, digits_b;
+    while (a > 0) {
+        digits_a.push_back(a % 10);
+        a /= 10;
+    }
+    while (b > 0) {
+        digits_b.push_back(b % 10);
+        b /= 10;
+    }
+    sort(digits_a.begin(), digits_a.end());
+    sort(digits_b.begin(), digits_b.end());
+    return digits_a == digits_b;
+}
+
+vector<int> unique_digits(vector<int> x);
+
+int main() {
+    vector<int> numbers = {123, 456, 789, 321, 654};
+    vector<int> result = unique_digits(numbers);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    return 0;
+}
 
 vector<int> unique_digits(vector<int> x){
     vector<int> result;
