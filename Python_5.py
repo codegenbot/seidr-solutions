1d2b3fee77f0d1c2cd1d814368a209fc3bbe809a
beep boop <githubbot@vadim.me>
2024-07-19 04:15:58 +0200

    Solution 3 of Python/5, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/cvejl6ap Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d70bd435-44e2-4d25-a543-bb884b79e e97.py", line 11, in <module>     check(intersperse)           ^^^^^^^^^^^ NameError: name 'intersperse' is not defined

--- Python_5.py
+++ Python_5.py
@@ -1,5 +1,6 @@
-result = []
-for num in numbers:
-    result.extend([num, delimeter])
-result.pop()
-return result
+def intersperse(numbers, delimeter):
+    result = []
+    for num in numbers:
+        result.extend([num, delimeter])
+    result.pop()
+    return result
