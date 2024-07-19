0bb74ca909ab5052a01c7c9fd1a3b8b79548c558
beep boop <githubbot@vadim.me>
2024-07-20 00:48:33 +0200

    Solution 60 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lp46jxd4 Prompt: 5dbb91dc-2725-4385-9694-22f213b2bf81.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 5dbb91dc-2725-4385-9694-22f213b2bf81.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 5dbb91dc-2725-4385-9694-22f213b2bf81.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 5dbb91dc-2725-4385-9694-22f213b2bf81.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,13 +3,6 @@
 #include <string>
 #include <boost/any.hpp>
 
-int max(int a, float b) {
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-
 int main() {
     boost::any a = "123";
     boost::any b = "456";
@@ -21,64 +14,22 @@ int main() {
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+        int num1 = boost::any_cast<int>(a);
+        float num2 = boost::any_cast<float>(b);
+
+        return num1 > num2 ? a : b;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
+        float num1 = boost::any_cast<float>(a);
+        int num2 = boost::any_cast<int>(b);
+
+        return num1 > num2 ? a : b;
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string str1 = a.convert_to<std::string>();
-        std::string str2 = b.convert_to<std::string>();
-
-        int num1 = 0, num2 = 0;
-        bool dot1 = false, dot2 = false;
-
-        for (int i = 0; i < str1.length(); i++) {
-            if (!dot1 && !isdigit(str1[i])) {
-                dot1 = true;
-                continue;
-            }
-            if (!dot2 && !isdigit(str2[i])) {
-                dot2 = true;
-                continue;
-            }
-            num1 = num1 * 10 + (str1[i] - '0');
-            num2 = num2 * 10 + (str2[i] - '0');
-        }
+        std::string str1 = boost::any_cast<std::string>(a);
+        std::string str2 = boost::any_cast<std::string>(b);
 
         return str1 > str2 ? a : b;
-    }
-    else if (a.type() == typeid(std::string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
-        std::string str = a.convert_to<std::string>();
-
-        int num = 0;
-        bool dot = false;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (!dot && !isdigit(str[i])) {
-                dot = true;
-                continue;
-            }
-            num = num * 10 + (str[i] - '0');
-        }
-
-        return b.convert_to<boost::any>() > boost::any(num) ? b : a;
-    }
-    else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(std::string)) {
-        std::string str = b.convert_to<std::string>();
-
-        int num = 0;
-        bool dot = false;
-
-        for (int i = 0; i < str.length(); i++) {
-            if (!dot && !isdigit(str[i])) {
-                dot = true;
-                continue;
-            }
-            num = num * 10 + (str[i] - '0');
-        }
-
-        return a.convert_to<boost::any>() > boost::any(num) ? a : b;
     }
     else {
         return boost::any(typeid(int));
