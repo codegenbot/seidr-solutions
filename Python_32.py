3345554223b9317a6d54b58a694971692e4b2606
beep boop <githubbot@vadim.me>
2024-07-19 13:51:28 +0200

    Solution 85 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pvucsq64 Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,5 +1,9 @@
 def find_zero(xs: list):
     if len(xs) % 2 != 0:
-        return None
-    largest_nonzero_coeff = max(filter(lambda x: x != 0, xs))
-    return -1 * (largest_nonzero_coeff / xs[0])
+        raise ValueError("Number of coefficients must be even")
+    if max(xs) == 0:
+        raise ValueError("No non-zero coefficients found")
+
+    for i in range(len(xs) - 1, -1, -1):
+        if xs[i] != 0:
+            return round(-xs[i - 1] / xs[i], 2)
