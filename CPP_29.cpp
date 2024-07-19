fe349a6e79c7ddbb280eb85c2af62ad48e1eb444
beep boop <githubbot@vadim.me>
2024-07-19 21:38:25 +0200

    Solution 10 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n2ai249y Prompt:       |                                            ^ ef10e441-b4ed-4cf4-a292-2bcf8779b895.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ ef10e441-b4ed-4cf4-a292-2bcf8779b895.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from ef10e441-b4ed-4cf4-a292-2bcf8779b895.cpp:31: ef10e441-b4ed-4cf4-a292-2bcf8779b895.cpp: In function ‘int main()’: ef10e441-b4ed-4cf4-a292-2bcf8779b895.cpp:41:36: error: could not convert ‘filter _by_prefix(std::vector<std::__cxx11::basic_string<char> >, std::string)(std::__c xx11::basic_string<char>(((const char*)"xxx"), std::allocator<char>()))’ from ‘v ector<std::vector<std::__cxx11::basic_string<char> >>’ to ‘vector<std::__cxx11:: basic_string<char>>’    41 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~       |                                    |       |                                    vector<std::vector<std::__cxx11::basi c_string<char> >>

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -10,13 +10,13 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::vector<std::string>> filter_by_prefix(std::vector<std::vector<std::string>> strings, std::string prefix) {
+std::vector<std::vector<std::string>> filter_by_prefix(std::vector<std::string> strings, std::string prefix) {
     std::vector<std::vector<std::string>> result;
     for (const auto& s : strings) {
-        if (s[0].find(prefix) == 0) {
+        if (s.find(prefix) == 0) {
             std::vector<std::string> temp = {prefix};
-            for(int i = prefix.size(); i < s[0].size(); i++) {
-                temp.push_back(s[0].substr(i, 1));
+            for(int i = prefix.size(); i < s.size(); i++) {
+                temp.push_back(s.substr(i, 1));
             }
             result.push_back(temp);
         }
@@ -24,7 +24,11 @@ std::vector<std::vector<std::string>> filter_by_prefix(std::vector<std::vector<s
     return result;
 }
 
+void testMain() {
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
+}
+
 int main() {
-    assert(issame(filter_by_prefix({{"xxx"}, {"asd"}, {"xxy"}, {"john doe"}, {"xxxxAAA"}, {"xxx"}}, "xxx") , {{"xxx"}, {"xxxAAA"}, {"xxx"}}));
+    testMain();
     return 0;
 }
