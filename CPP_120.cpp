0432f650f55e683916c09df1231e5ecda83ddc23
beep boop <githubbot@vadim.me>
2024-07-20 00:18:33 +0200

    Solution 63 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hcv8ocbq Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ e8d462a0-d174-402b-bcf5-64c07492b70d.cpp:17:39:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <vector>
 
-bool sameVector(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-std::vector<int> maximum(std::vector<int> a, int k) {
+std::vector<int> maximum(std::vector<int> a) {
     std::vector<int> result;
+    int max = *max_element(a.begin(), a.end());
     for (int i = 0; i < a.size(); ++i) {
-        if (a[i] > k) result.push_back(a[i]);
+        if (a[i] > max) result.push_back(a[i]);
     }
     return result;
 }
 
 int main() {
-    int n, k;
+    int n, k = 0;
     cin >> n;
     vector<int> a(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
-    cin >> k;
-    vector<int> result = maximum(a, k);
-    if (!sameVector(maximum(a, k), result)) return 1;
+    std::vector<int> result = maximum(a);
+    if (!isSame(result, maximum(a))) return 1;
     return 0;
 }
