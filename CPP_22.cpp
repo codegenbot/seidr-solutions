821eb83ab110e48f599eaff22d26e621a413fa8b
beep boop <githubbot@vadim.me>
2024-07-19 21:02:33 +0200

    Solution 60 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/2l76vjzy Prompt: on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 2bfd9daa-83eb-42cb-b188-278968e62d0a.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2bfd9daa-83eb-42cb-b188-278968e62d0a.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 2bfd9daa-83eb-42cb-b188-278968e62d0a.cpp:17: 2bfd9daa-83eb-42cb-b188-278968e62d0a.cpp: In function ‘int main()’: 2bfd9daa-83eb-42cb-b188-278968e62d0a.cpp:27:20: error: ‘filter_integers’ was not  declared in this scope    27 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 2bfd9daa-83eb-42cb-b188-278968e62d0a.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,25 +1,17 @@
 #include <vector>
 #include <list>
-#include <any>
-#include <cassert>
+#include <boost/any.hpp>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-vector<int> filter_integers(list<any> values){
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values){
+    std::vector<int> result;
     for(auto val : values){
-        if(auto ptr = any_cast<int>(&val)){
+        if(auto ptr = boost::any_cast<int>(&val)){
             result.push_back(*ptr);
         }
     }
     return result;
-}
-
-int main(){
-    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
-    return 0;
 }
