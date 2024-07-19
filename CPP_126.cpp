28821a487dfa58ba2c9ce6bd7c6d1ba4fa8e1e4d
beep boop <githubbot@vadim.me>
2024-07-19 20:23:18 +0200

    Solution 13 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/h0fiaar5 Prompt: Complete the following code given the task description and function signature.

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,10 +1,13 @@
-bool is_sorted(vector<int> lst) {
-    for(int i = 1; i < lst.size(); i++) {
-        if(lst[i-1] >= lst[i]) {
-            vector<int>::iterator it = unique(lst.begin(), lst.end());
-            lst.erase(it, lst.end());
+bool is_sorted(vector<int> lst){
+    if (lst.size() <= 1)
+        return true;
+
+    for(int i = 1; i < lst.size(); i++){
+        if(lst[i-1] >= lst[i]){
+            if(count(lst.begin(), lst.end(), lst[i]) > 1)
+                return false;
+        } else
             return false;
-        }
     }
     return true;
 }
