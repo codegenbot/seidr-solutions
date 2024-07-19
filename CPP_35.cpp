ae22b3b14c4aa56f9a68e0e8ba05cf386713f1a6
beep boop <githubbot@vadim.me>
2024-07-19 17:37:05 +0200

    Solution 16 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/l7740gcc Prompt: f5876884-acbe-41b8-a518-4ab02b67f54a.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ f5876884-acbe-41b8-a518-4ab02b67f54a.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -13,6 +13,8 @@ float max_element(const std::vector<float>& l) {
     return max;
 }
 
-void main() {
+int main() {
     assert(std::abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}) - 124) < 1e-4);
+    
+    return 0;
 }
