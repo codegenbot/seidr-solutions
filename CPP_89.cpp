3402366143427e768f41e3344afd8ffb637626ab
beep boop <githubbot@vadim.me>
2024-07-19 20:01:39 +0200

    Solution 37 of CPP/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/x37fw5cl Prompt: d03f213c-df39-432d-b9df-e9f9111fd3aa.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string encrypt(string s) {       | ^~~~~~ In file included from d03f213c-df39-432d-b9df-e9f9111fd3aa.cpp:21: d03f213c-df39-432d-b9df-e9f9111fd3aa.cpp: In function ‘int main()’: d03f213c-df39-432d-b9df-e9f9111fd3aa.cpp:23:13: error: ‘encrypt’ was not declare d in this scope    23 |     assert (encrypt("a")=="e");       |             ^~~~~~~

--- CPP_89.cpp
+++ CPP_89.cpp
@@ -24,6 +24,6 @@ int main() {
     std::string str;
     std::cout << "Enter a string: ";
     std::getline(std::cin, str);
-    std::cout << "Encrypted String: " << encrypt(str) << std::endl;
+    std::cout << "Encrypted string: " << encrypt(str) << std::endl;
     return 0;
 }
