885c5e6ba30611bf744c9a61b92e54dff2e96106
beep boop <githubbot@vadim.me>
2024-07-19 18:38:04 +0200

    Solution 20 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3p991rkm Prompt: ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp:16:20: error: ‘common’ was not declared  in this scope    16 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp:16:13: error: ‘issame’ was not declared  in this scope    16 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ^~~~~~ ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp: At global scope: ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp:20: ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp: In function ‘int main()’: ac027af7-0ea2-4172-82d7-814ce9df4f6b.cpp:30:20: error: ‘common’ was not declared  in this scope    30 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
+
 using namespace std;
 
 vector<int> solve(vector<int>& l1, vector<int>& l2) {
@@ -17,7 +18,11 @@ bool issame(const vector<int>& v1, const vector<int>& v2) {
     return v1 == v2;
 }
 
+vector<int> common(const vector<int>& l1, const vector<int>& l2) {
+    return solve(l1, l2);
+}
+
 int main() {
-    assert (issame(solve({4, 3, 2, 8}, {}) , {}));
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
