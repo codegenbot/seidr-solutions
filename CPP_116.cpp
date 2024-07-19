30ca74396d5293397dbc2e0e1db8127a7c0bf2e6
beep boop <githubbot@vadim.me>
2024-07-19 19:50:47 +0200

    Solution 30 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r9jq48yp Prompt:       |             ^~~~~~ 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp:6:20: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp:5: 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp: In function ‘int main()’: 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp:15:20: error: ‘sort_array’ was not decl ared in this scope    15 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 243b4f97-271a-4947-a944-2ef044ea2f2a.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,23 +1,13 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
-
-std::vector<int> sort_array(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end());
-    return arr;
-}
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    a = sort_array(a);
-    b = sort_array(b);
-    
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+
     return a == b;
-}
-
-using namespace std;
-
-int main() {
-    assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-    
-    return 0;
 }
