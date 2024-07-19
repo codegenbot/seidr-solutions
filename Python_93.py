0e99a75f690d4e575736504825b26db68f541e26
beep boop <githubbot@vadim.me>
2024-07-19 17:11:40 +0200

    Solution 40 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/189ox018 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80ae5f71-d330-42da-974c-91b8d2396 321.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80ae5f71-d330-42da-974c-91b8d2396 321.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,8 @@
 def encode(message):
     result = ""
     for char in message:
-        if char.isalnum():  
-            if char.lower() in 'aeiou':
-                base = ord('a' if char.islower() else 'A')
-            else:
-                base = ord('a' if char.islower() else 'A')
-
+        if char.isalpha():
+            base = ord('a' if char.islower() else 'A')
             result += chr((ord(char) - base + 3) % 26 + base)
         else:
             result += char
