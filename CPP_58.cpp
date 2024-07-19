f2ace28125ce187b4144e330f1e8c80f1c953ce3
beep boop <githubbot@vadim.me>
2024-07-19 22:55:44 +0200

    Solution 82 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt:                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 0647b609-28c6-49c1-89b5-7f829d603e27.cpp:1: /usr/include/c++/11/bits/allocator.h:204:7: note: candidate: ‘bool std::operator ==(const std::allocator<int>&, const std::allocator<int>&)’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |       ^~~~~~~~ /usr/include/c++/11/bits/allocator.h:204:18: note:   no known conversion for arg ument 1 from ‘std::set<int>’ to ‘const std::allocator<int>&’   204 |       operator==(const allocator&, const allocator&) _GLIBCXX_NOTHROW       |                  ^~~~~~~~~~~~~~~~ In file included from 0647b609-28c6-49c1-89b5-7f829d603e27.cpp:29: 0647b609-28c6-49c1-89b5-7f829d603e27.cpp: In function ‘int main()’: 0647b609-28c6-49c1-89b5-7f829d603e27.cpp:39:26: error: could not convert ‘common (std::vector<int>(std::initializer_list<int>{((const int*)(& const int [4]{4, 3,  2, 8})), 4}, std::allocator<int>()), std::vector<int>())’ from ‘bool’ to ‘std:: vector<int>’    39 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~       |                          |       |                          bool

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,1 @@
-bool common(vector<int> a, vector<int> b) {
-    set<int> result;
-    for (int i = 0; i < min(a.size(), b.size()); i++) {
-        result.insert(b[i]);
-    }
-    return result == set<int>(a.begin(), a.end()) || result == set<int>(b.begin(), b.end());
-}
+cout << (user_input == user_input ? "Yes" : "No") << endl;
