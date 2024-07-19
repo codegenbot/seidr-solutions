59d579e5766c67d9866d25d7db05f4dbc6fd2562
beep boop <githubbot@vadim.me>
2024-07-19 03:55:01 +0200

    Solution 32 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9xcpuj31 Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5498e272-975c-47ed-8628-339d36996940.cpp:51:34: error: expected primary-expressi on before ‘int’    51 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5498e272-975c-47ed-8628-339d36996940.cpp:51:39: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5498e272-975c-47ed-8628-339d36996940.cpp:50: 5498e272-975c-47ed-8628-339d36996940.cpp: In function ‘int main()’: 5498e272-975c-47ed-8628-339d36996940.cpp:60:20: error: ‘minPath’ was not declare d in this scope    60 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 5498e272-975c-47ed-8628-339d36996940.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,21 +1,62 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {}
-
 vector<int> minPath(vector<vector<int>> grid, int k);
 
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 int main() {
-    vector<vector<int>> grid = {{1, 3, 5}, {2, 4, 10}, {6, 8, 9}};
-    int k = 4;
+    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
+    return 0;
+}
 
-    vector<int> result = minPath(grid, k);
+vector<int> minPath(vector<vector<int>> grid, int k) {
+    int n = grid.size();
+    vector<int> path;
 
-    for (int val : result) {
-        cout << val << " ";
+    for (int i = 0; i < k; ++i) {
+        int min_val = n * n + 1;
+        pair<int, int> min_pos;
+
+        for (int r = 0; r < n; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] != -1) {
+                    if (grid[r][c] < min_val) {
+                        min_val = grid[r][c];
+                        min_pos = {r, c};
+                    }
+                }
+            }
+        }
+
+        path.push_back(min_val);
+        grid[min_pos.first][min_pos.second] = -1;
+
+        if (i == k - 1) {
+            break;
+        }
+
+        int r = min_pos.first;
+        int c = min_pos.second;
+
+        if (r > 0 && grid[r - 1][c] != -1) {
+            grid[r - 1][c] = -1;
+        }
+        if (r < n - 1 && grid[r + 1][c] != -1) {
+            grid[r + 1][c] = -1;
+        }
+        if (c > 0 && grid[r][c - 1] != -1) {
+            grid[r][c - 1] = -1;
+        }
+        if (c < n - 1 && grid[r][c + 1] != -1) {
+            grid[r][c + 1] = -1;
+        }
     }
 
-    return 0;
+    return path;
 }
