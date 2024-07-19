8232e04d61587d6f2e907dfce6e83cbcaee74382
beep boop <githubbot@vadim.me>
2024-07-19 04:01:03 +0200

    Solution 14 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pkm4vxj8 Prompt:    20 | return "None";       | ^~~~~~ 98ad682e-59c3-44df-b707-1b1297f36f2f.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from 98ad682e-59c3-44df-b707-1b1297f36f2f.cpp:23: 98ad682e-59c3-44df-b707-1b1297f36f2f.cpp: In function ‘int main()’: 98ad682e-59c3-44df-b707-1b1297f36f2f.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 98ad682e-59c3-44df-b707-1b1297f36f2f.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 98ad682e-59c3-44df-b707-1b1297f36f2f.cpp:25:37: error: ‘compare_one’ was not dec lared in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,23 +3,23 @@
 using namespace boost;
 using namespace std;
 
-boost::any compare_one(const boost::any& a, const boost::any& b) {
+boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+        if (any_cast<int>(a) > any_cast<int>(b)) {
             return a;
-        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+        } else if (any_cast<int>(a) < any_cast<int>(b)) {
             return b;
         }
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+        if (any_cast<float>(a) > any_cast<float>(b)) {
             return a;
-        } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+        } else if (any_cast<float>(a) < any_cast<float>(b)) {
             return b;
         }
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
+        if (stof(any_cast<string>(a)) > stof(any_cast<string>(b))) {
             return a;
-        } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
+        } else if (stof(any_cast<string>(a)) < stof(any_cast<string>(b))) {
             return b;
         }
     }
