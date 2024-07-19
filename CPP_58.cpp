88b5447c2c62befbef6cbd9e27711a87eebace63
beep boop <githubbot@vadim.me>
2024-07-19 18:27:29 +0200

    Solution 4 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3p991rkm Prompt:       |                    ^~~ c5c97c14-a5ba-4104-bc86-467070a405b4.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c5c97c14-a5ba-4104-bc86-467070a405b4.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c5c97c14-a5ba-4104-bc86-467070a405b4.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c5c97c14-a5ba-4104-bc86-467070a405b4.cpp:9: c5c97c14-a5ba-4104-bc86-467070a405b4.cpp: In function ‘int main()’: c5c97c14-a5ba-4104-bc86-467070a405b4.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ c5c97c14-a5ba-4104-bc86-467070a405b4.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,10 +1,20 @@
-#include <vector>
-using namespace std;
-vector<int> solve(vector<int>& l1, vector<int>& l2) {
+vector<int> intersect(vector<int>& l1, vector<int>& l2){
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
     vector<int> result;
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
