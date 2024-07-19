0bb63ebf64a8a2368cf7bf9ed777f9dcc5e02123
beep boop <githubbot@vadim.me>
2024-07-19 23:01:43 +0200

    Solution 35 of CPP/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dbz71o2c Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: e1d54145-baa9-4e5b-bce1-d94d9ebba29b.cpp:24:51:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_89.cpp
+++ CPP_89.cpp
@@ -20,10 +20,7 @@ std::string encrypt(std::string s) {
 int main() {
     std::string s;
     std::cout << "Enter a string: ";
-    if (!(std::getline(std::cin, s))) {
-        std::cerr << "Failed to read input.\n";
-        return 1; 
-    } 
+    std::getline(std::cin, s); 
     std::cout << "Encrypted string: " << encrypt(s) << std::endl;
     return 0;
 }
