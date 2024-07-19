42d19227e0924247be2f623d70660d1bbbf395f7
beep boop <githubbot@vadim.me>
2024-07-19 06:00:48 +0200

    Solution 15 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/skao3hy9 Prompt: Complete the following code given the task description and function signature.

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,6 +1,6 @@
 if (a + b > c && a + c > b && b + c > a) {
         float s = (a + b + c) / 2;
-        return roundf(sqrt(s * (s - a) * (s - b) * (s - c)) * 100) / 100;
+        return sqrt(s * (s - a) * (s - b) * (s - c));
     } else {
         return -1;
     }
