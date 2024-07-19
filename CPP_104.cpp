90d21a64ce9c734f2ecada0d2ed21abb34308c99
beep boop <githubbot@vadim.me>
2024-07-20 00:55:18 +0200

    Solution 85 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/whi9clwf Prompt:       |                    ^~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:21: fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp: In function ‘int main()’: fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -2,15 +2,11 @@
 #include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
+    if (a.size() != b.size())
         return false;
-    }
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
     for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+        if (a[i] != b[i])
             return false;
-        }
     }
     return true;
 }
@@ -37,7 +33,12 @@ vector<int> unique_digits(vector<int> x) {
 }
 
 int main() {
-    vector<int> input = {123, 456, 789, 135};
+    vector<int> input = {123, 456, 789, 135, 246};
     vector<int> output = unique_digits(input);
-    return 0;
+    vector<int> expected_output = {135, 789};
+    if (issame(output, expected_output)) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
