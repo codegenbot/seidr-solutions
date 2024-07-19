a7ede6b0b236c243c9b92f273ec745c32a7c4f88
beep boop <githubbot@vadim.me>
2024-07-19 03:07:36 +0200

    Solution 4 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/p8luf1tu Prompt: b553cdc2-5eeb-4a8f-85c7-ed12981055c6.cpp: In function ‘long long int double_the_ difference(std::vector<int>)’: b553cdc2-5eeb-4a8f-85c7-ed12981055c6.cpp:7:31: error: ‘pow’ was not declared in this scope     7 |             sum += (long long)pow(num, 2);       |                               ^~~ In file included from b553cdc2-5eeb-4a8f-85c7-ed12981055c6.cpp:13: b553cdc2-5eeb-4a8f-85c7-ed12981055c6.cpp: In function ‘int main()’: b553cdc2-5eeb-4a8f-85c7-ed12981055c6.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ b553cdc2-5eeb-4a8f-85c7-ed12981055c6.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -10,15 +10,3 @@ long long double_the_difference(std::vector<int> lst){
     }
     return sum;
 }
-
-int main() {
-    std::vector<int> lst = {1, 3, 5, 2, 4};
-    long long odd_sum = 0;
-    for (int num : lst) {
-        if (num > 0 && num % 2 != 0) {
-            odd_sum += static_cast<long long>(pow(num, 2));
-        }
-    }
-    assert(double_the_difference(lst) == odd_sum);
-    return 0;
-}
