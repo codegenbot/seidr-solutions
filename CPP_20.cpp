b16e669f6985000b40448abd439c2b053c4f33ca
beep boop <githubbot@vadim.me>
2024-07-20 01:35:14 +0200

    Solution 12 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xoe84td3 Prompt:    20 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 10237576-9ec1-47cb-8b9b-7630e1f55555.cpp:20:36: error: expected primary-expressi on before ‘float’    20 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 10237576-9ec1-47cb-8b9b-7630e1f55555.cpp:20:43: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 10237576-9ec1-47cb-8b9b-7630e1f55555.cpp:19: 10237576-9ec1-47cb-8b9b-7630e1f55555.cpp: In function ‘int main()’: 10237576-9ec1-47cb-8b9b-7630e1f55555.cpp:29:20: error: ‘find_closest_elements’ w as not declared in this scope    29 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 10237576-9ec1-47cb-8b9b-7630e1f55555.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,13 +1,9 @@
-#include <algorithm>
 #include <vector>
 #include <numeric>
-
-bool issame(const std::vector<float>& a, const std::vector<float>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-}
+#include <limits>
 
 std::pair<float, float> find_closest_elements(const std::vector<float>& numbers) {
-    vector<pair<float, float>> pairs = {};
+    std::vector<std::pair<float, float>> pairs = {};
     for (int i = 0; i < numbers.size() - 1; i++) {
         for (int j = i + 1; j <= numbers.size() - 1; j++) {
             pairs.push_back({numbers[i], numbers[j]});
@@ -15,13 +11,13 @@ std::pair<float, float> find_closest_elements(const std::vector<float>& numbers)
     }
 
     float min_diff = std::numeric_limits<float>::max();
-    pair<float, float> closest_pair;
+    std::pair<float, float> closest_pair;
     for (auto &p : pairs) {
         float diff = p.second - p.first;
-        if (std::abs(diff) < std::abs(min_diff)) {
+        if (abs(diff) < abs(min_diff)) {
             min_diff = diff;
             closest_pair = p;
         }
     }
-    return {closest_pair.first, closest_pair.second};
+    return closest_pair;
 }
