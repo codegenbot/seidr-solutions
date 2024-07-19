7d8e7d0af4227746b0bdc110c667f33f8ffc7dad
beep boop <githubbot@vadim.me>
2024-07-19 19:52:10 +0200

    Solution 72 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bawfox83 Prompt: Complete the following code given the task description and function signature.

--- Python_93.py
+++ Python_93.py
@@ -2,16 +2,20 @@ def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if char.isupper():
-                if char == "Z":
-                    result += "A"
-                else:
-                    result += chr(ord(char) + 1)
+            if (
+                char.lower() == "a"
+                or char.lower() == "e"
+                or char.lower() == "i"
+                or char.lower() == "o"
+                or char.lower() == "u"
+            ):
+                new_char = chr((ord(char) - ord("a") + 2) % 26 + ord("a"))
             else:
-                if char == "z":
-                    result += "a"
+                if char.isupper():
+                    new_char = chr((ord(char) - ord("A") + 2) % 26 + ord("A"))
                 else:
-                    result += chr(ord(char) + 1)
+                    new_char = chr((ord(char) - ord("a") + 2) % 26 + ord("a"))
+            result += new_char
         else:
             result += char
-    return result.lower().swapcase()
+    return result
