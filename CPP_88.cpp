80c02e7679fc0873c29791067fc3ca076f1e66bb
beep boop <githubbot@vadim.me>
2024-07-20 00:41:32 +0200

    Solution 80 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hr0d31hs Prompt:       |                    ^~~ a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp:14: a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp: In function ‘int main()’: a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ a8e1e246-3d3c-4d50-bc32-7c97de8673c9.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,18 +1,25 @@
-bool issame(vector<int> a, vector<int> b);
+#include <vector>
+
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
+    
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    
+    return true;
+}
 
 vector<int> sort_array(vector<int> array) {
-    if (array.empty()) return array;
+    if(array.empty()) return array;
     
     int sum = array.front() + array.back();
-    if (sum % 2 == 0) {
+    
+    if(sum % 2 == 0){
         sort(array.rbegin(), array.rend());
     } else {
         sort(array.begin(), array.end());
     }
     
     return array;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
 }
