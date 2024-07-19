2ce7bc8273c602fd32c3099af114c7cd0b432224
beep boop <githubbot@vadim.me>
2024-07-20 00:17:19 +0200

    Solution 27 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/oisngjxm Prompt:       |                    ^~~ d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp:21: d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp: In function ‘int main()’: d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ d15e6ead-c4cd-43d0-8b89-1da6028e2687.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,45 +1,31 @@
 #include <vector>
-
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
 
 vector<int> pluck(vector<int> arr){
-        vector<int> result;
-        int smallestEven = INT_MAX;
-        int smallestIndex = -1;
-
-        for(int i = 0; i < arr.size(); i++){
-            if(arr[i] % 2 == 0 && arr[i] < smallestEven){
-                smallestEven = arr[i];
-                smallestIndex = i;
-            }
-        }
-
-        if(smallestIndex != -1){
-            result.push_back(smallestEven);
-            result.push_back(smallestIndex);
-        }
-
-        return result;
-} 
-
-bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
+    vector<int> result;
+    int smallestEven = INT_MAX;
+    int smallestIndex = -1;
+        
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] % 2 == 0 && arr[i] < smallestEven){
+            smallestEven = arr[i];
+            smallestIndex = i;
         }
     }
-    
-    return true;
+        
+    if(smallestIndex != -1){
+        result.push_back(smallestEven);
+        result.push_back(smallestIndex);
+    }
+        
+    return result;
 }
 
 int main(){
     assert(issame(pluck({7, 9, 7, 1}), vector<int>{}));
-    
     return 0;
 }
