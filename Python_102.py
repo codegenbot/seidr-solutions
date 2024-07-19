dd1a0565ce0b8b08759ee7e3ba36d466a430e243
beep boop <githubbot@vadim.me>
2024-07-19 19:40:26 +0200

    Solution 1 of Python/102, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/glckvktn Prompt: Complete the following code given the task description and function signature.

--- Python_102.py
+++ Python_102.py
@@ -1,8 +1,5 @@
-```
 def choose_num(x, y):
-    if x > y:
-        return -1
-    for i in range(x, y+1):
+    for i in range(max(x, y), min(x, y), -1):
         if i % 2 == 0:
             return i
     return -1
