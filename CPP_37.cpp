b3da48a4ba31938f5fcafaedd1a994f47454d442
beep boop <githubbot@vadim.me>
2024-07-19 18:53:42 +0200

    Solution 84 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xrtgbsd6 Prompt:    33 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 6dd56b27-0da3-4edd-80b1-36b4af566931.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 6dd56b27-0da3-4edd-80b1-36b4af566931.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main(){       |     ^~~~ In file included from 6dd56b27-0da3-4edd-80b1-36b4af566931.cpp:32: 6dd56b27-0da3-4edd-80b1-36b4af566931.cpp: In function ‘int main()’: 6dd56b27-0da3-4edd-80b1-36b4af566931.cpp:42:20: error: ‘sort_even’ was not decla red in this scope; did you mean ‘sortEven’?    42 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 6dd56b27-0da3-4edd-80b1-36b4af566931.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,26 +2,29 @@
 #include <vector>
 
 bool areSame(const std::vector<float>& a, const std::vector<float>& b) {
-    return a == b;
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
 
 std::vector<float> sortEven(std::vector<float> l){
     std::vector<float> l_prime;
     for(int i = 0; i < l.size(); i++){
         if(i % 2 == 0){
-            auto even_elements = std::partition(l.begin(), l.end(), [](float x){return std::abs(x) % 2 == 0});
-            while(even_elements != l.end() && *even_elements >= 0) {
+            auto even_elements = std::partition(l.begin(), l.end(), [](float x){return x % 2 == 0});
+            auto end_even = even_elements;
+            while(even_elements != end_even && *even_elements >= 0) {
                 ++even_elements;
             }
-            std::sort(even_elements, l.end());
-            l_prime.push_back(*std::min_element(even_elements, l.end()));
+            end_even = even_elements;
+            std::sort(even_elements, end_even);
+            l_prime.push_back(*std::min_element(even_elements, end_even));
         } else {
             l_prime.push_back(l[i]);
         }
     }
+    return l_prime;
 }
 
-int test() {
+int main(){
     assert(areSame(sortEven({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), sortEven({-12, 8, 3, 4, 5, 2, 12, 11, 23, -10})));
     return 0;
 }
