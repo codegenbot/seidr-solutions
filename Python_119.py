0ab5909c68f2b2c030cf7f0fe79e90e2e25c0c68
beep boop <githubbot@vadim.me>
2024-07-19 07:37:15 +0200

    Solution 19 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/quq3p7gy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87ce317e-121b-43a6-9765-31cc334ae e7c.py", line 21, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87ce317e-121b-43a6-9765-31cc334ae e7c.py", line 18, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,10 +1,11 @@
 def match_parens(lst):
     count = 0
     for s in lst:
-        if s == '(':
+        if s == "(":
             count += 1
-        elif s == ')':
-            if count == 0:
-                return 'No'
+        elif s == ")":
+            if count <= 0:
+                return "No"
             count -= 1
-    return 'Yes' if count == 0 else 'No'
+
+    return "Yes" if count == 0 else "No"
