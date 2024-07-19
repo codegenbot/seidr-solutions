47a507156431a36b72a87fec7a71dc075db5083f
beep boop <githubbot@vadim.me>
2024-07-19 02:36:47 +0200

    Solution 19 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/cj6ib63j Prompt:       |                           ^~~~~~ 7b1e584b-70e7-4ab4-ba8c-aa7cc90f2f49.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 7b1e584b-70e7-4ab4-ba8c-aa7cc90f2f49.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 7b1e584b-70e7-4ab4-ba8c-aa7cc90f2f49.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7b1e584b-70e7-4ab4-ba8c-aa7cc90f2f49.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7b1e584b-70e7-4ab4-ba8c-aa7cc90f2f49.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 7b1e584b-70e7-4ab4-ba8c-aa7cc90f2f49.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main(){       |     ^~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -2,11 +2,15 @@
 #include <algorithm>
 #include <cassert>
 
+namespace std {
+    using ::std::vector;
+}
+
 bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
 
-std::vector<int> sort_array(std::vector<int>& array){
+std::vector<int> sort_array(std::vector<int> array){
     if(array.empty()) return array;
     if((array.front() + array.back()) % 2 == 0)
         std::sort(array.begin(), array.end(), std::greater<int>());
@@ -17,7 +21,7 @@ std::vector<int> sort_array(std::vector<int>& array){
 
 #define ASSERT_VECTOR_EQ(a, b) assert(std::equal(a.begin(), a.end(), b.begin(), b.end()))
 
-int main(){
+int main() { 
     ASSERT_VECTOR_EQ(sort_array({21, 14, 23, 11}), {23, 21, 14, 11});
     return 0;
 }
