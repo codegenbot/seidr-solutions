b3df3b036d464204501be9bd835fdd8c0789b234
beep boop <githubbot@vadim.me>
2024-07-19 18:01:40 +0200

    Solution 6 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4j6mhwyx Prompt:  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c4748474-cd83-41a5-a2e9-f894306b784c.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c4748474-cd83-41a5-a2e9-f894306b784c.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c4748474-cd83-41a5-a2e9-f894306b784c.cpp:15: c4748474-cd83-41a5-a2e9-f894306b784c.cpp: In function ‘int main()’: c4748474-cd83-41a5-a2e9-f894306b784c.cpp:25:20: error: ‘remove_duplicates’ was n ot declared in this scope    25 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ c4748474-cd83-41a5-a2e9-f894306b784c.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,15 +1,11 @@
-bool issame(vector<int> a,vector<int>b){
-    return a==b;
-}
-
-vector<int> remove_duplicates(vector<int> numbers) {
-    unordered_map<int, bool> seen;
+vector<int> remove_duplicates(vector<int> numbers){
+    unordered_map<int,int> mp;
     vector<int> result;
 
-    for (int num : numbers) {
-        if (!seen.count(num)) { 
-            seen[num] = true;
+    for(auto num : numbers) {
+        if(mp[num] == 0) {
             result.push_back(num);
+            mp[num]++;
         }
     }
 
