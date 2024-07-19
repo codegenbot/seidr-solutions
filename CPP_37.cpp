c20fbaf9dec00b8355e8fb6fc54a2c450d40b4e9
beep boop <githubbot@vadim.me>
2024-07-19 04:05:13 +0200

    Solution 80 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/l2tmypae Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 06455133-602f-4d29-a90b-13240f15aeea.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 06455133-602f-4d29-a90b-13240f15aeea.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 06455133-602f-4d29-a90b-13240f15aeea.cpp:20: 06455133-602f-4d29-a90b-13240f15aeea.cpp: In function ‘int main()’: 06455133-602f-4d29-a90b-13240f15aeea.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 06455133-602f-4d29-a90b-13240f15aeea.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,11 +1,8 @@
-#include <vector>
-#include <algorithm>
-
 bool issame(vector<float> a, vector<float> b) {
     return a == b;
 }
 
-vector<float> sort_even(vector<float> l) {
+void sort_even(vector<float>& l) {
     vector<float> even_vals;
     vector<float> sorted_even_vals;
     for (int i = 0; i < l.size(); i++) {
@@ -22,10 +19,9 @@ vector<float> sort_even(vector<float> l) {
             j++;
         }
     }
-    return l;
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    // Your main function code here
     return 0;
 }
