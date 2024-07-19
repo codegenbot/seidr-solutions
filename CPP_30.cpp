326a21712fc57f0005c60e91bfb890f94d9870aa
beep boop <githubbot@vadim.me>
2024-07-19 21:00:34 +0200

    Solution 10 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/43e8v26u Prompt:       |                    ^~~~~ 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp:11: 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp: In function ‘int main()’: 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 92a7fc2c-1c73-4efa-beab-8e7c00286951.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,11 @@
 #include <vector>
 
-bool issame(std::vector<float> a, std::vector<float> b){
-    return a == b;
+std::vector<float> get_positive(std::vector<float> l){
+    std::vector<float> result;
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
+        }
+    }
+    return result;
 }
-
-// Update the main function to use the correct function name
