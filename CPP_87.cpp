626bea0aafa03d07335ae78db2c6c3f02afe55ee
beep boop <githubbot@vadim.me>
2024-07-19 03:08:34 +0200

    Solution 28 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r1dqhr1m Prompt:       |                                   ^~~~~~ 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp:19: 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp: In function ‘int main()’: 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 2b6a5168-2a69-4efa-a2b2-973ec4480904.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,35 +1,4 @@
 #include <vector>
-
-bool issame(vector<int> a, vector<int> b) {
-    return a[0] == b[0] && a[1] == b[1];
-}
-
-vector<vector<int>> get_row(vector<vector<int>> lst, int x){
-    vector<vector<int>> result;
-    for(int i = 0; i < lst.size(); ++i){
-        for(int j = 0; j < lst[i].size(); ++j){
-            if(lst[i][j] == x){
-                result.push_back({i, j});
-            }
-        }
-    }
-    sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b){
-        if(a[0] == b[0]){
-            return a[1] > b[1];
-        }
-        return a[0] < b[0];
-    });
-    return result;
-}
-
-int main() {
-    vector<vector<int>> lst = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    vector<vector<int>> result = get_row(lst, 5);
-    for(const auto& vec : result){
-        for(int num : vec){
-            cout << num << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+bool issame(vector<vector<int>> a, vector<vector<int>> b) {
+    return a == b;
 }
