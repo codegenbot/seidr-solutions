2110a3a8096bfedcf21c4a1368725cc13e30f378
beep boop <githubbot@vadim.me>
2024-07-19 23:51:22 +0200

    Solution 86 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
-#include <cassert>
 #include <string>
 
 using namespace std;
 
 int mainFunction() {
-    int a, b, c;
+    string str1, str2, str3;
     cout << "Enter three numbers: ";
-    if (!(cin >> a >> b >> c)) {
+    cin >> ws >> str1 >> ws >> str2 >> ws >> str3;
+    
+    if (str1.empty() || str2.empty() || str3.empty()) {
         cout << "Error: Please enter valid integer values.\n";
     } else {
+        int a = stoi(str1), b = stoi(str2), c = stoi(str3);
         cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
     }
 
