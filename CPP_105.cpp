5207225c3c88d9a1f492a0506a8b15c6a56cafa9
beep boop <githubbot@vadim.me>
2024-07-19 04:45:49 +0200

    Solution 39 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pmyfkyx7 Prompt:       |                              ^~~~~~ 460b21e4-f47b-410a-9a07-9852ec042b25.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 460b21e4-f47b-410a-9a07-9852ec042b25.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 460b21e4-f47b-410a-9a07-9852ec042b25.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 460b21e4-f47b-410a-9a07-9852ec042b25.cpp:26: 460b21e4-f47b-410a-9a07-9852ec042b25.cpp: In function ‘int main()’: 460b21e4-f47b-410a-9a07-9852ec042b25.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 460b21e4-f47b-410a-9a07-9852ec042b25.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -15,8 +15,8 @@ vector<string> by_length(vector<int> arr) {
     vector<int> sorted_arr;
     vector<string> result;
     map<int, string> num_to_name = {
-            {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-            {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
 
     for (int num : arr) {
