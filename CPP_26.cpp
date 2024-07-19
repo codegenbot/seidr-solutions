53a463bcfbefc3d4d913f4bd7e5a4a55a9e9056c
beep boop <githubbot@vadim.me>
2024-07-19 21:30:54 +0200

    Solution 9 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2rg56foq Prompt: s compound expression in initializer [-fpermissive]    31 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a96d103f-7109-4d92-afd1-29c1e7292867.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ a96d103f-7109-4d92-afd1-29c1e7292867.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from a96d103f-7109-4d92-afd1-29c1e7292867.cpp:30: a96d103f-7109-4d92-afd1-29c1e7292867.cpp: In function ‘int main()’: a96d103f-7109-4d92-afd1-29c1e7292867.cpp:40:20: error: ‘remove_duplicates’ was n ot declared in this scope    40 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ a96d103f-7109-4d92-afd1-29c1e7292867.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,11 +1,11 @@
 ```cpp
-bool checkEqual(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     if(a.size() != b.size()) {
-        return false;
+        throw runtime_error("Vectors are not of the same size");
     }
     for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i]) {
-            return false;
+            throw runtime_error("Vectors are not equal");
         }
     }
     return true;
@@ -19,4 +19,11 @@ vector<int> uniqueNumbers(vector<int> numbers) {
         }
     }
     return result;
+
+}
+
+int main() {
+    vector<int> test = {1, 2, 3, 2, 4, 3, 5};
+    assert (issame(uniqueNumbers(test), {1, 2, 3, 4, 5}) );
+    return 0;
 }
