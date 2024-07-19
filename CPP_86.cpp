ca4a143f411a5a3fa55d3174798bc541d21daf96
beep boop <githubbot@vadim.me>
2024-07-19 06:21:15 +0200

    Solution 91 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7v6ds9sl Prompt: Complete the following code given the task description and function signature.

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,19 +1,2 @@
-string anti_shuffle(string s){
-    string newString = "";
-    string word = "";
-    
-    for (char c : s) {
-        if (c == ' ') {
-            sort(word.begin(), word.end());
-            newString += word + ' ';
-            word = "";
-        } else {
-            word += c;
-        }
-    }
-    
-    sort(word.begin(), word.end());
-    newString += word;
-    
-    return newString;
-}
+sort(s.begin(), s.end());
+return s;
