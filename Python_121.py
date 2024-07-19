271747c36b152c8b1e52c6bc2c30665dfbb38b89
beep boop <githubbot@vadim.me>
2024-07-19 17:23:55 +0200

    Solution 91 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f14b8274-b296-41fb-a3b7-2feb6a2ba 04e.py", line 16, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f14b8274-b296-41fb-a3b7-2feb6a2ba 04e.py", line 11, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f14b8274-b296-41fb-a3b7-2feb6a2ba 04e.py", line 4, in solution     elif all(x.isdigit() for x in input_list):          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f14b8274-b296-41fb-a3b7-2feb6a2ba 04e.py", line 4, in <genexpr>     elif all(x.isdigit() for x in input_list):              ^^^^^^^^^ AttributeError: 'int' object has no attribute 'isdigit'

--- Python_121.py
+++ Python_121.py
@@ -1,7 +1,7 @@
-def solution(input_list):
-    if not input_list:
-        return "The list is empty."
-    elif all(x.isdigit() for x in input_list):
-        return int(''.join(map(str, input_list)))
-    else:
-        return "The list contains non-integer values."
+def solution(input_list):    
+    if not input_list:        
+        return 
+    elif all(isinstance(x, int) and str(x).isdigit() for x in input_list):        
+        return int(''.join(map(str, input_list)))    
+    else:        
+        return
