bf0d2248dafd9e755fb37985d9f5473f3045cafa
beep boop <githubbot@vadim.me>
2024-07-19 18:55:40 +0200

    Solution 64 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dilniizg Prompt: e");       |             ^~~~~ 10a9735f-3162-4d29-8357-19f321db0464.cpp:15:29: error: ‘string’ was not declared  in this scope    15 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 10a9735f-3162-4d29-8357-19f321db0464.cpp:15:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 10a9735f-3162-4d29-8357-19f321db0464.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 10a9735f-3162-4d29-8357-19f321db0464.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 10a9735f-3162-4d29-8357-19f321db0464.cpp:13: 10a9735f-3162-4d29-8357-19f321db0464.cpp:15:37: error: ‘compare_one’ was not dec lared in this scope    15 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,25 +1,29 @@
+```cpp
 #include <string>
 #include <algorithm>
 
 int compare_strings(const std::string& a, const std::string& b) {
-    for (size_t i = 0; i < a.size() && i < b.size(); ++i) {
-        if (compare_one(a[i], b[i]) > 0)
-            return 1;
-        else if (compare_one(a[i], b[i]) < 0)
-            return -1;
-    }
-    if (a.size() > b.size())
-        return 1;
-    else if (a.size() < b.size())
-        return -1;
-    return 0;
-}
-
-int compare_one(char a, char b) {
     if (a > b)
         return 1;
     else if (b > a)
         return -1;
     else
         return 0;
+}
+
+std::string compare_one(const std::string& str, int num) {
+    if(num == 1) {
+        for(int i = 0; i < str.length(); i++) {
+            if(str[i] >= 'a' && str[i] <= 'z') {
+                str[i] = (str[i] - 'a' + 'A');
+            }
+        }
+    } else if(num == -1) {
+        for(int i = 0; i < str.length(); i++) {
+            if(str[i] >= 'A' && str[i] <= 'Z') {
+                str[i] = (str[i] - 'A' + 'a');
+            }
+        }
+    }
+    return str;
 }
