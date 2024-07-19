bc9fbc6424fa51f64026eb21b52f8471bcb679f9
beep boop <githubbot@vadim.me>
2024-07-19 18:20:06 +0200

    Solution 66 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l2x96g5c Prompt: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a058638b-2d44-404e-b891-dba971680fd8.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a058638b-2d44-404e-b891-dba971680fd8.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a058638b-2d44-404e-b891-dba971680fd8.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a058638b-2d44-404e-b891-dba971680fd8.cpp:34: a058638b-2d44-404e-b891-dba971680fd8.cpp: In function ‘int main()’: a058638b-2d44-404e-b891-dba971680fd8.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,7 @@
-```cpp
 #include <vector>
 #include <string>
 
-bool same(const std::vector<std::string>& v1, const std::vector<std::string>& v2) {
+bool areSame(const std::vector<std::string>& v1, const std::vector<std::string>& v2) {
     if (v1.size() != v2.size()) {
         return false;
     }
@@ -12,7 +11,6 @@ bool same(const std::vector<std::string>& v1, const std::vector<std::string>& v2
         }
     }
     return true;
-
 }
 
 std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix){
@@ -25,11 +23,10 @@ std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std:
     return result;
 
 }
-
-int main() {
+int main_function() {
     std::vector<std::string> a = {"xxx", "asd", "xxy", "john doe", "xxxA", "xxx"};
     std::vector<std::string> b = {"xxx", "xxxAAA", "xxx"};
 
-    assert (same(filter_by_prefix(a, "xxx") , b));
+    assert (areSame(filter_by_prefix(a, "xxx") , b));
     return 0;
 }
