8a4fedcfbe1c46daf41f071ee691249788b163a5
beep boop <githubbot@vadim.me>
2024-07-19 20:38:05 +0200

    Solution 4 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/cvyz6vq2 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4c1b4e06-819b-43cc-9da0-c96b94ea4df7.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4c1b4e06-819b-43cc-9da0-c96b94ea4df7.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4c1b4e06-819b-43cc-9da0-c96b94ea4df7.cpp:11: 4c1b4e06-819b-43cc-9da0-c96b94ea4df7.cpp: In function ‘int main()’: 4c1b4e06-819b-43cc-9da0-c96b94ea4df7.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 4c1b4e06-819b-43cc-9da0-c96b94ea4df7.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,9 +1,11 @@
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(const auto& str : strings){
-        if(str.find(substring) != string::npos){
-            result.push_back(str);
-        }
-    }
-    return result;
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+vector<string> filter_by_substring(vector<string> strings, string substring);
+
+int main(){
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
+    
+    return 0;
 }
