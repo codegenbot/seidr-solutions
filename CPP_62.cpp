357ec87c1b1f7c11022f6f4165304445bb560358
beep boop <githubbot@vadim.me>
2024-07-19 02:20:01 +0200

    Solution 17 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6u64fdcj Prompt:       |             ^~~~~~ e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp:21:20: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp:21:29: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp:20: e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp: In function ‘int main()’: e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp:30:20: error: ‘derivative’ was not decl ared in this scope    30 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ e9a7976c-018f-4503-a6d2-ab28080bc24f.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,23 +1,26 @@
 #include <vector>
-#include <cassert>
 #include <cmath>
 
-using namespace std;
-
-bool issame(vector<float> a, vector<float> b){
+bool issame(std::vector<float> a, std::vector<float> b);
+bool issame(std::vector<float> a, std::vector<float> b){
     if (a.size() != b.size()) return false;
     
-    for (int i = 0; i < a.size(); i++) {
-        if (abs(a[i] - b[i]) > 1e-4) return false;
+    for (int i = 0; i < a.size(); i++){
+        if (std::abs(a[i] - b[i]) > 1e-4) return false;
     }
     
     return true;
 }
 
-vector<float> derivative(vector<float> xs){
-    vector<float> result;
+std::vector<float> derivative(std::vector<float> xs){
+    std::vector<float> result;
     for(int i = 1; i < xs.size(); i++){
         result.push_back(xs[i] * i);
     }
     return result;
+}
+
+int main(){
+    assert(issame(derivative({1}), {}));
+    return 0;
 }
