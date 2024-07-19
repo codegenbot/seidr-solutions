ca92a761028a2c1f1978028ac2cac710f024f385
beep boop <githubbot@vadim.me>
2024-07-20 00:19:44 +0200

    Solution 12 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/npdxugun Prompt: Complete the following code given the task description and function signature.

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,4 +1,11 @@
-if (x < 1 || n < 2) return false;
-    double res = log(x) / log(n);
-    return (floor(res) == res);
+if(x == 0) return false;
+    if(x == 1) return true;
+    
+    int power = 0;
+    while(pow(n,power) <= x){
+        if(pow(n,power) == x)
+            return true;
+        power++;
+    }
+    return false;
 }
