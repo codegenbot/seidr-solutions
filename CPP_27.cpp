c463909faded069525e7670d8dac7ad839e1623f
beep boop <githubbot@vadim.me>
2024-07-19 21:42:41 +0200

    Solution 30 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ed3sgnfv Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: b144267a-9371-446d-ae97-14541737b44e.cpp:23:41:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -7,13 +7,12 @@ std::string flip_case(std::string str) {
     std::string result;
     for(int i=0; i<str.length(); i++){
         char c = str[i];
-        if(std::isprint(c))
-            if(c >= 'a' && c <= 'z')
-                result += toupper(c);
-            else if(c >= 'A' && c <= 'Z')
-                result += tolower(c);
-            else
-                result += c;
+        if(c >= 'a' && c <= 'z')
+            result += toupper(c);
+        else if(c >= 'A' && c <= 'Z')
+            result += tolower(c);
+        else
+            result += c;
     }
     return result;
 }
@@ -22,6 +21,6 @@ int main() {
     std::string input = "These violent delights have violent ends";
     std::string output = flip_case(input);
     std::cout << "Computed output: " << output << std::endl;
-    assert(flip_case(input) == "these violent delights have violent ends");
+    assert(flip_case(input) == "these Violent Delights Have Violent Ends");
     return 0;
 }
