792a5f4c5081f3c65cefa59eba639f82bd18d4c4
beep boop <githubbot@vadim.me>
2024-07-20 00:54:54 +0200

    Solution 81 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/whi9clwf Prompt:       |                    ^~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:21: fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp: In function ‘int main()’: fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ fd8ebbcf-1098-4d8f-8603-505ddb749c55.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -5,8 +5,6 @@ bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
@@ -37,10 +35,7 @@ vector<int> unique_digits(vector<int> x) {
 }
 
 int main() {
-    vector<int> input = {123, 456, 789, 246};
+    vector<int> input = {123, 456, 789, 246, 135};
     vector<int> output = unique_digits(input);
-    for (int num : output) {
-        cout << num << " ";
-    }
     return 0;
 }
