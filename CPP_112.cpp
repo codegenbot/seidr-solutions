f98376fb4b678ff084535a6ff6149ab9a49a3636
beep boop <githubbot@vadim.me>
2024-07-19 18:12:22 +0200

    Solution 4 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4nydrkb3 Prompt:       |                                 ^~~~~~ In file included from f4a8314e-db62-46b3-bdff-2aef30a30905.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ f4a8314e-db62-46b3-bdff-2aef30a30905.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f4a8314e-db62-46b3-bdff-2aef30a30905.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ f4a8314e-db62-46b3-bdff-2aef30a30905.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from f4a8314e-db62-46b3-bdff-2aef30a30905.cpp:17: f4a8314e-db62-46b3-bdff-2aef30a30905.cpp: In function ‘int main()’: f4a8314e-db62-46b3-bdff-2aef30a30905.cpp:27:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    27 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -2,31 +2,25 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i<a.size(); i++)
-        if(a[i] != b[i])
-            return false;
-    return true;
-}
-
-std::vector<std::string> reverse_delete(const std::string& s, const std::string& t) {
+std::vector<std::string> reverse_delete(const std::string& s1, const std::string& s2) {
     std::vector<std::string> result;
-    for(int i = 0; i < s.size(); i++) {
-        if(std::find(t.begin(), t.end(), s[i]) == t.end()) {
-            result.push_back(s.substr(i));
-            break;
-        }
-        else if(i == s.size() - 1) {
-            result.push_back(s);
+    for (int i = 0; i < s1.size(); i++) {
+        if (s1[i] != s2[i]) {
+            result.push_back(std::string(1, s1[i]));
         }
     }
     return result;
 }
 
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++)
+        if (a[i] != b[i])
+            return false;
+    return true;
+}
+
 int main() {
-    vector<string> a = reverse_delete("mamma", "mia");
-    vector<string> b = {"", "True"};
-    assert(issame(a, b));
+    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
 }
