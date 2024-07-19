fd30ce4eae349108ef4f93435f40a4eb1033146c
beep boop <githubbot@vadim.me>
2024-07-19 20:30:44 +0200

    Solution 47 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7hj0ixx6 Prompt:       |                    ^~~ bcee00b2-2ede-47f5-8687-08706cafc83a.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bcee00b2-2ede-47f5-8687-08706cafc83a.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bcee00b2-2ede-47f5-8687-08706cafc83a.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bcee00b2-2ede-47f5-8687-08706cafc83a.cpp:7: bcee00b2-2ede-47f5-8687-08706cafc83a.cpp: In function ‘int main()’: bcee00b2-2ede-47f5-8687-08706cafc83a.cpp:17:20: error: ‘compare’ was not declare d in this scope    17 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ bcee00b2-2ede-47f5-8687-08706cafc83a.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -12,6 +12,5 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 int main() {
     assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), {2, 0, 0, 1});
-    // Add any additional test cases here
     return 0;
 }
