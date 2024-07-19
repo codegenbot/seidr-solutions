aa3fd495a23277f685b42ad30123665599089396
beep boop <githubbot@vadim.me>
2024-07-19 14:46:22 +0200

    Solution 53 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wb8za879 Prompt:       |           ^~~~~~ In file included from 32193836-0097-4a6d-bbfb-a43d246597b6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 32193836-0097-4a6d-bbfb-a43d246597b6.cpp:8:36: error: expected primary-expressio n before ‘float’     8 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 32193836-0097-4a6d-bbfb-a43d246597b6.cpp:8:43: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 32193836-0097-4a6d-bbfb-a43d246597b6.cpp:7: 32193836-0097-4a6d-bbfb-a43d246597b6.cpp: In function ‘int main()’: 32193836-0097-4a6d-bbfb-a43d246597b6.cpp:17:20: error: ‘get_positive’ was not de clared in this scope    17 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 32193836-0097-4a6d-bbfb-a43d246597b6.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
 #include <vector>
 
-bool areVectorsEqual(std::vector<float> a, std::vector<float> b) {
-    return std::equal(a.begin(), a.end(), b.begin(), b.end());
+bool issame(std::vector<float> a, std::vector<float> b){
+    return a == b;
 }
