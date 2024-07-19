a6b118010ee6ed3cc42c4941767919436d456c23
beep boop <githubbot@vadim.me>
2024-07-19 19:33:40 +0200

    Solution 71 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wgpgl3tr Prompt: in this scope     3 | int smallest_change(vector<int> arr) {       |                     ^~~~~~ a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp:3:21: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp:3:28: error: expected primary-expressio n before ‘int’     3 | int smallest_change(vector<int> arr) {       |                            ^~~ In file included from a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp:22: a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp: In function ‘int main()’: a78383c3-9c20-4ddb-9cb3-1d58a4be5990.cpp:24:28: error: ‘smallest_change’ cannot be used as a function    24 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,1 +1,17 @@
 int smallest_change(std::vector<int> myArr) {
+    int n = myArr.size();
+    std::vector<std::vector<int>> dp(n, std::vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 0;
+    }
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i < n - len + 1; i++) {
+            int j = i + len - 1;
+            if (myArr[i] == myArr[j]) {
+                dp[i][j] = dp[i + 1][j - 1];
+            } else {
+                dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[0][n - 1];
