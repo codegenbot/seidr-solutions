ff49b830cf7d2e4afcf7ee850a7b28c18b8b3aeb
beep boop <githubbot@vadim.me>
2024-07-19 06:51:37 +0200

    Solution 92 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/iickrqpt Prompt: Program did not receive expected input

--- Python_0.py
+++ Python_0.py
@@ -1,12 +1,6 @@
-try:
-    while True:
-        try:
-            result = input()
-            if not result:
-                break
-            if result.isnumeric():
-                print(int(result))
-        except EOFError:
-            break
-except EOFError:
-    pass
+while True:
+    result = input()
+    if not result:
+        break
+    if result.isnumeric():
+        print(int(result))
