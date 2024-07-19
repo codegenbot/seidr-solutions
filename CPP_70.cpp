ab6d7bd4a481e6fc9bc230ca2c71097f6f342bb8
beep boop <githubbot@vadim.me>
2024-07-20 00:21:36 +0200

    Solution 56 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4bqexe7l Prompt:       |                    ^~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:13: f34ea320-8c02-4016-9918-70cca1e2f80c.cpp: In function ‘int main()’: f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:23:20: error: ‘strange_sort_list’ was n ot declared in this scope    23 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -7,6 +7,14 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    // Add your main function implementation here
+    std::vector<int> vec1 = {1, 2, 3, 4, 5};
+    std::vector<int> vec2 = {5, 4, 3, 2, 1};
+
+    if (issame(vec1, vec2)) {
+        std::cout << "Vectors are the same.\n";
+    } else {
+        std::cout << "Vectors are different.\n";
+    }
+
     return 0;
 }
