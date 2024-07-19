7bfcabf65ce28d6dec7833b1e53dd212d5c3f8b6
beep boop <githubbot@vadim.me>
2024-07-19 20:59:22 +0200

    Solution 0 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/664ehzue Prompt: Complete the following code given the task description and function signature.

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-
 bool has_close_elements(vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
@@ -10,10 +6,4 @@ bool has_close_elements(vector<float> numbers, float threshold){
         }
     }
     return false;
-}
-
-int main(){
-    std::vector<float> a={1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-    assert (has_close_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5f) == false);
-    return 0;
 }
