76127b3ddf0b75209cce5678f5377a3975b48b56
beep boop <githubbot@vadim.me>
2024-07-19 04:42:46 +0200

    Solution 68 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pkm4vxj8 Prompt:       |     ^~~~ In file included from 65cda791-9991-45dd-92a3-951ae8afc16c.cpp:31: 65cda791-9991-45dd-92a3-951ae8afc16c.cpp: In function ‘int main()’: 65cda791-9991-45dd-92a3-951ae8afc16c.cpp:33:13: error: ‘boost’ has not been decl ared    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 65cda791-9991-45dd-92a3-951ae8afc16c.cpp:33:29: error: ‘string’ was not declared  in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 65cda791-9991-45dd-92a3-951ae8afc16c.cpp:33:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 65cda791-9991-45dd-92a3-951ae8afc16c.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 65cda791-9991-45dd-92a3-951ae8afc16c.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,18 +3,27 @@
 #include <cassert>
 
 template <typename T>
-T compare_one(const T& a, const T& b) {
-    if (a > b) {
-        return a;
-    } else if (a < b) {
-        return b;
+T compare_one(std::any a, std::any b) {
+    if (std::any_cast<T>(a) > std::any_cast<T>(b)) {
+        return std::any_cast<T>(a);
+    } else if (std::any_cast<T>(a) < std::any_cast<T>(b)) {
+        return std::any_cast<T>(b);
     }
+    return std::any_cast<T>(a);
 }
 
 int main() {
-    assert(compare_one<int>(5, 10) == 10);
-    assert(compare_one<float>(5.5f, 3.3f) == 5.5f);
-    assert(compare_one<std::string>("hello", "world") == "world");
+    auto a = std::any(10);
+    auto b = std::any(5);
+    assert(compare_one<int>(a, b) == 10);
+
+    auto c = std::any(7.5f);
+    auto d = std::any(3.2f);
+    assert(compare_one<float>(c, d) == 7.5f);
+
+    auto e = std::any(std::string("20.3"));
+    auto f = std::any(std::string("12.8"));
+    assert(compare_one<std::string>(e, f) == "20.3");
 
     return 0;
 }
