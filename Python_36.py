e2874a42e2047825b6352dcfa8eedaa7439db688
beep boop <githubbot@vadim.me>
2024-07-19 05:30:57 +0200

    Solution 32 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r5iimeo4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09662df7-61bb-4605-87df-404ff43e5 3ed.py", line 9, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/09662df7-61bb-4605-87df-404ff43e5 3ed.py", line 6, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,4 +1,4 @@
 def fizz_buzz(n: int):
     digit_sequences = ['3', '5', '33', '55', '153', '335', '515', '35']
-    count = sum(1 for i in range(1, n + 1) if any(seq in str(i) or (i % int(seq) == 0 and int(seq) != 0) for seq in digit_sequences))
+    count = sum(1 for i in range(1, n + 1) if any(i % int(seq) == 0 for seq in digit_sequences))
     return count
