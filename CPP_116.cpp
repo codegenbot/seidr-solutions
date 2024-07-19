487d067f61bac1515c0ffb147e5d36c1b08a401b
beep boop <githubbot@vadim.me>
2024-07-19 11:24:26 +0200

    Solution 92 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/p70n7sfp Prompt:       |                    ^~~ 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp:12: 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp: In function ‘int main()’: 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 13b5aa17-40b2-41df-8f1a-707ccccda65a.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
-#include <algorithm>
 #include <vector>
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+    int count_a = __builtin_popcount(a);
+    int count_b = __builtin_popcount(b);
+    if(count_a == count_b){
+        return a < b;
+    }
+    return count_a < count_b;
 }
 
 std::vector<int> sort(std::vector<int> arr){
-    std::sort(arr.begin(), arr.end(), [](int a, int b){
+    sort(arr.begin(), arr.end(), [](int a, int b){
         int count_a = __builtin_popcount(a);
         int count_b = __builtin_popcount(b);
         if(count_a == count_b){
@@ -19,5 +22,5 @@ std::vector<int> sort(std::vector<int> arr){
 }
 
 int main(){
-    assert(issame(sort({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
+    assert (issame(sort({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
 }
