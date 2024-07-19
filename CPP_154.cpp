bdb8612570adc63f89fd7e61db78c2b5c987eca1
beep boop <githubbot@vadim.me>
2024-07-19 20:24:59 +0200

    Solution 4 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/u7h5bvsp Prompt: 6d38edf7-0ebe-41d7-9a8f-f472792957fd.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string temp = b + b;       | ^~~~~~ 6d38edf7-0ebe-41d7-9a8f-f472792957fd.cpp:2:5: error: expected unqualified-id bef ore ‘return’     2 |     return (temp.find(a) != string::npos);       |     ^~~~~~ 6d38edf7-0ebe-41d7-9a8f-f472792957fd.cpp:3:1: error: expected declaration before  ‘}’ token     3 | }       | ^ In file included from 6d38edf7-0ebe-41d7-9a8f-f472792957fd.cpp:5: 6d38edf7-0ebe-41d7-9a8f-f472792957fd.cpp: In function ‘int main()’: 6d38edf7-0ebe-41d7-9a8f-f472792957fd.cpp:7:14: error: ‘cycpattern_check’ was not  declared in this scope     7 |     assert  (cycpattern_check("winemtt","tinem") == true );       |              ^~~~~~~~~~~~~~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,15 +1,4 @@
-#include <string>
-
-bool cycpattern_check(string a, string b) {
+bool cycpattern_check(const string& a, const string& b) {
     string temp = b + b;
     return (temp.find(a) != string::npos);
-}
-
-int main() {
-    string a, b;
-    cin >> a >> b;
-    
-    cout << cycpattern_check(a, b) << endl;
-    
-    return 0;
 }
