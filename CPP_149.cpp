48b49e5f14b5446251c8e610877fd4289feb460f
beep boop <githubbot@vadim.me>
2024-07-20 01:20:48 +0200

    Solution 41 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9ufpz40d Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 79201e57-6de5-406a-9bd0-fc1b1cea360a.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 79201e57-6de5-406a-9bd0-fc1b1cea360a.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 79201e57-6de5-406a-9bd0-fc1b1cea360a.cpp:11: 79201e57-6de5-406a-9bd0-fc1b1cea360a.cpp: In function ‘int main()’: 79201e57-6de5-406a-9bd0-fc1b1cea360a.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 79201e57-6de5-406a-9bd0-fc1b1cea360a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -2,16 +2,15 @@ bool issame(vector<string> a, const vector<string>& b) {
     return a == b;
 }
 
-vector<string> sorted_list_sum(const vector<string>& lst) {
-    vector<string> lst_copy = lst;
-    lst_copy.erase(remove_if(lst_copy.begin(), lst_copy.end(), [](const string& s) { return s.length() % 2 != 0; }), lst_copy.end());
-    sort(lst_copy.begin(), lst_copy.end(), [](const string& a, const string& b) {
+vector<string> sorted_list_sum(vector<string> lst) {
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
+    sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
         if (a.length() == b.length()) {
             return a < b;
         }
         return a.length() < b.length();
     });
-    return lst_copy;
+    return lst;
 }
 
 int main() {
@@ -19,9 +18,9 @@ int main() {
     vector<string> vec2 = {"apple", "banana", "orange"};
 
     if (issame(vec1, vec2)) {
-        cout << "The vectors are the same" << endl;
+        cout << "Vectors are the same" << endl;
     } else {
-        cout << "The vectors are different" << endl;
+        cout << "Vectors are different" << endl;
     }
 
     return 0;
