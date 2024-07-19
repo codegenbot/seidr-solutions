50983a5e0f1e4b71fb6f069cc423cd9138d2269b
beep boop <githubbot@vadim.me>
2024-07-19 23:00:33 +0200

    Solution 53 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s07geol4 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -6,7 +6,7 @@ int solve(int N) {
     std::string str;
     while (N > 0) {
         int remainder = N % 2;
-        str.push_back(remainder + '0'); 
+        str += (remainder == 0) ? "0" : "1";
         N /= 2;
     }
     int sum = 0;
@@ -18,10 +18,6 @@ int solve(int N) {
 }
 
 void printResult(int N) {
-    if(N == 0) {
-        std::cout << "Invalid input. Please enter a non-zero integer." << std::endl;
-        return;
-    }
     int sum = solve(N);
     std::cout << "Sum of bits is: " << sum << std::endl;
 }
@@ -30,6 +26,10 @@ int main() {
     std::cout << "Enter a number: ";
     int N;
     std::cin >> N;
+    if (N == 0) {
+        std::cout << "Invalid input. Please enter a non-zero integer." << std::endl;
+        return 1;
+    }
     printResult(N);
     return 0;
 }
