8cc32806c81fcf42e9e480b343782e9fd620f48e
beep boop <githubbot@vadim.me>
2024-07-19 14:55:08 +0200

    Solution 69 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/qfp6dldx Prompt:    23 | int main(){       |     ^~~~ 953f16e9-7d2f-43a4-ba8d-c1aa4457bf43.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 953f16e9-7d2f-43a4-ba8d-c1aa4457bf43.cpp:22: 953f16e9-7d2f-43a4-ba8d-c1aa4457bf43.cpp: In function ‘int main()’: 953f16e9-7d2f-43a4-ba8d-c1aa4457bf43.cpp:26:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    26 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 953f16e9-7d2f-43a4-ba8d-c1aa4457bf43.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -15,6 +15,6 @@ int main() {
     int x, base;
     std::cin >> x >> base;
     assert(change_base(x, base) == std::to_string(x));
-    assert(change_base(x, base) == std::to_string(x));
+    assert(change_base(x, x + 1) == std::to_string(x));
     return 0;
 }
