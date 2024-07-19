1e0519b1fb446a40d37a2b573e28f07b2a83e76a
beep boop <githubbot@vadim.me>
2024-07-19 21:55:56 +0200

    Solution 32 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mj2jfwr8 Prompt:       |                    ^~~ 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp:22: 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp: In function ‘int main()’: 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp:32:20: error: ‘unique_digits’ was not d eclared in this scope    32 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 80c1a527-46d6-45a9-84c3-d71e88a56c9d.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,8 +1,25 @@
 #include <vector>
+#include <algorithm>
+
+bool issame(vector<int> a, vector<int> b);
+
+vector<int> unique_digits(vector<int> x);
 
 bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> unique_digits(vector<int> x){
     vector<int> result;
-    for (int num : a) {
+    for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
         while (temp > 0) {
@@ -18,5 +35,5 @@ bool issame(vector<int> a, vector<int> b) {
         }
     }
     sort(result.begin(), result.end());
-    return result == b;
+    return result;
 }
