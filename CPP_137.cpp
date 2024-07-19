3ff568ad820c1781ae4a25e48ca7fa0174493947
beep boop <githubbot@vadim.me>
2024-07-19 22:28:00 +0200

    Solution 90 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8hrrqsp9 Prompt: e");       |             ^~~~~ cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:23:29: error: ‘string’ was not declared  in this scope    23 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp: In function ‘std::any compare_one(std: :any, std::any)’: cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:19:1: warning: control reaches end of n on-void function [-Wreturn-type]    19 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -8,15 +8,14 @@ boost::any compare_one(boost::any a, boost::any b) {
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         return boost::any_cast<float>(a) > boost::any_cast<float>(b) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        float valA = stof(boost::any_cast<std::string>(a));
-        float valB = stof(boost::any_cast<std::string>(b));
+        float valA = std::stof(boost::any_cast<std::string>(a));
+        float valB = std::stof(boost::any_cast<std::string>(b));
         return valA > valB ? a : (valA < valB ? b : "None");
-    } else if ((a.type() == typeid(int) && b.type() == typeid(std::string)) || 
-               (a.type() == typeid(std::string) && b.type() == typeid(int))) {
-        float valA = a.type() == typeid(int) ? boost::any_cast<int>(a) : stof(boost::any_cast<std::string>(a));
-        float valB = b.type() == typeid(int) ? boost::any_cast<int>(b) : stof(boost::any_cast<std::string>(b));
+    } else if ((a.type() == typeid(int) && b.type() == typeid(std::string)) || (a.type() == typeid(std::string) && b.type() == typeid(int))) {
+        float valA = a.type() == typeid(int) ? boost::any_cast<int>(a) : std::stof(boost::any_cast<std::string>(a));
+        float valB = b.type() == typeid(int) ? boost::any_cast<int>(b) : std::stof(boost::any_cast<std::string>(b));
         return valA > valB ? a : (valA < valB ? b : "None");
     } else {
-        assert(false); // Handle default case appropriately based on requirements
+        throw std::runtime_error("Unsupported data types for comparison.");
     }
 }
