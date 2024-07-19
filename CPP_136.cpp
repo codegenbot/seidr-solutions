deb86f812ea7cd1f8ecb00efbb12e02b468a26bf
beep boop <githubbot@vadim.me>
2024-07-19 20:23:56 +0200

    Solution 62 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/a5fpriio Prompt: on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5ebec125-aff2-4c2f-b2e9-c252b7476a0d.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 5ebec125-aff2-4c2f-b2e9-c252b7476a0d.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 5ebec125-aff2-4c2f-b2e9-c252b7476a0d.cpp:27: 5ebec125-aff2-4c2f-b2e9-c252b7476a0d.cpp: In function ‘int main()’: 5ebec125-aff2-4c2f-b2e9-c252b7476a0d.cpp:37:45: error: too few arguments to func tion ‘std::vector<int> largest_smallest_integers(std::vector<int>, std::vector<i nt>)’    37 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~ 5ebec125-aff2-4c2f-b2e9-c252b7476a0d.cpp:9:18: note: declared here     9 | std::vector<int> largest_smallest_integers(std::vector<int> a, std::vect or<int> b) {       |                  ^~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -6,12 +6,12 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> largest_smallest_integers(std::vector<int> a) {
-    int largest_negative = INT_MIN, smallest_positive = INT_MAX;
+std::vector<int> largest_smallest_integers(std::vector<int> a, std::vector<int> b) {
+    int largest_negative = a[0], smallest_positive = a[0];
     for (int num : a) {
-        if (num < 0 && num > largest_negative) {
+        if (num < 0 && num < largest_negative) {
             largest_negative = num;
-        } else if (num > 0 && num < smallest_positive) {
+        } else if (num > 0 && (num < smallest_positive || smallest_positive < 0)) {
             smallest_positive = num;
         }
     }
@@ -19,7 +19,7 @@ std::vector<int> largest_smallest_integers(std::vector<int> a) {
 }
 
 int main() {
-    std::vector<int> result = largest_smallest_integers({-6, -4, -4, -3, -100, 1});
+    std::vector<int> result = largest_smallest_integers({-6, -4, -4, -3, -100, 1}, {});
     assert(issame(result, {-100, 1}));
     return 0;
 }
