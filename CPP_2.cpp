83ef993671a664c6a83f6c358291cd53a76f61a3
beep boop <githubbot@vadim.me>
2024-07-19 20:39:29 +0200

    Solution 49 of CPP/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8qswhnzt Prompt: b01473f8-658f-4147-b41b-d5ffcacab94b.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ b01473f8-658f-4147-b41b-d5ffcacab94b.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -10,6 +10,6 @@ int main() {
     assert(std::abs(truncate_number(123.456) - 0.456) < 1e-4);
     assert(std::abs(truncate_number(10.123) - 0.123) < 1e-4);
     assert(std::abs(truncate_number(3.1415) - 0.1415) < 1e-4);
-
+    
     return 0;
 }
