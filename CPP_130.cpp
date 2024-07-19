91c580e967413ef1a611a1f62784d009c5105835
beep boop <githubbot@vadim.me>
2024-07-19 04:18:12 +0200

    Solution 71 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sg453w14 Prompt:       |             ^~~~~~ 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp:30:39: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main(){       |     ^~~~ In file included from 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp:29: 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp: In function ‘int main()’: 1b3f8254-196a-4bfd-8c14-7501c227f123.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,18 +1,19 @@
 #include <vector>
 #include <cassert>
 
-bool is_same(std::vector<int> a, std::vector<int> b){
+bool is_same(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> tri(int n){
-    std::vector<int> tribonacci(n+1);
+std::vector<int> tri(int n) {
+    std::vector<int> tribonacci(n + 1);
     tribonacci[0] = 3;
-    if(n > 0) tribonacci[1] = 1;
-    if(n > 1) tribonacci[2] = 3;
     
-    for(int i = 3; i <= n; ++i){
-        if(i % 2 == 0){
+    if (n > 0) tribonacci[1] = 1;
+    if (n > 1) tribonacci[2] = 3;
+    
+    for (int i = 3; i <= n; ++i) {
+        if (i % 2 == 0) {
             tribonacci[i] = 1 + i / 2;
         } else {
             tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3];
@@ -22,6 +23,6 @@ std::vector<int> tri(int n){
     return tribonacci;
 }
 
-int main(){
+int main() {
     assert(is_same(tri(1), std::vector<int>{3, 1}));
 }
