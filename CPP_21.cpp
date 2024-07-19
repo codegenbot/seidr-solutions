024895f0388802ea58b6907300711787437dd7ec
beep boop <githubbot@vadim.me>
2024-07-19 17:46:44 +0200

    Solution 64 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3l8mlrwb Prompt:       |                             ^~~~~~ fe71e1f7-d71a-4f18-8a95-1078d501a11e.cpp:28:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from fe71e1f7-d71a-4f18-8a95-1078d501a11e.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fe71e1f7-d71a-4f18-8a95-1078d501a11e.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fe71e1f7-d71a-4f18-8a95-1078d501a11e.cpp:28:36: error: expected primary-expressi on before ‘float’    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fe71e1f7-d71a-4f18-8a95-1078d501a11e.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ fe71e1f7-d71a-4f18-8a95-1078d501a11e.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -19,7 +19,7 @@ std::vector<float> rescale_to_unit(const std::vector<float>& numbers) {
 }
 
 int main() {
-    assert(issame(rescale_to_unit({12.0f, 11.0f, 15.0f, 13.0f, 14.0f}), {0.25f, 0.0f, 1.0f, 0.5f, 0.75f}));
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25f, 0.0f, 1.0f, 0.5f, 0.75f}));
     
     return 0;
 }
