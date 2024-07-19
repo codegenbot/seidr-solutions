1c98cc29e35c11ca66cf64fef908ad7660425c18
beep boop <githubbot@vadim.me>
2024-07-19 16:59:22 +0200

    Solution 14 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/c96pp2vg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4386c26-ad23-490a-b0c7-10d98b026 372.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_80.py
+++ Python_80.py
@@ -1,9 +1,10 @@
 ```
 def is_happy(s):
+    s = s.lower()
     if len(s) < 3:
         return False
     for i in range(0, len(s), 3):
         chunk = s[i:i+3]
-        if set(chunk).count(set(chunk)[0]) == len(chunk):
+        if set(chunk) == {'h', 'a'} or set(chunk) == {'p'}:
             return True
     return False
