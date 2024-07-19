9dc9c48f727fb64cedb08a4c0285df14b766aed4
beep boop <githubbot@vadim.me>
2024-07-19 03:53:38 +0200

    Solution 40 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hbkd1rh0 Prompt: In file included from /usr/include/c++/11/string:39,                  from e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp:45:44: error: ‘b’ was not declared in t his scope    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp:44: e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp: In function ‘int main()’: e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp:54:20: error: ‘split_words’ was not dec lared in this scope    54 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ e59a87ad-2e92-4f2b-af74-c623e89fe268.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,12 +1,10 @@
 #include <vector>
 #include <string>
-#include <cctype>
+#include <cassert>
 
-using namespace std;
+std::vector<std::string> split_words(std::string txt);
 
-vector<string> split_words(string txt);
-
-bool issame(const vector<string>& a, const vector<string>& b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -18,31 +16,11 @@ bool issame(const vector<string>& a, const vector<string>& b) {
     return true;
 }
 
-vector<string> split_words(string txt) {
-    vector<string> result;
-    string word = "";
-    for (char c : txt) {
-        if (c == ' ' || c == ',') {
-            if (!word.empty()) {
-                result.push_back(word);
-                word = "";
-            }
-        } else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        result.push_back(word);
-    }
-  
-    if (result.empty()) {
-        int count = 0;
-        for (char c : txt) {
-            if (islower(c) && (c - 'a') % 2 == 1) {
-                count++;
-            }
-        }
-        result.push_back(to_string(count));
-    }
-    return result;
+int main() {
+    assert(issame(split_words(""), {"0"}));
+    return 0;
+}
+
+std::vector<std::string> split_words(std::string txt) {
+    // implementation of the split_words function
 }
