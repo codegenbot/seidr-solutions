cc724543543161eccc1708c08057be7c4cf00e11
beep boop <githubbot@vadim.me>
2024-07-19 20:47:32 +0200

    Solution 4 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fk9tk9i8 Prompt: Complete the following code given the task description and function signature.

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,10 +1,9 @@
 int Strongest_Extension(string class_name,vector<string> extensions){
     int strongest = -1;
-    string strongest_extension = "";
-    
-    for(auto extension : extensions) {
+    string strongest_extension;
+    for(auto extension:extensions){
         int cap = 0, sm = 0;
-        for(char c : extension) {
+        for(auto c : extension) {
             if(isupper(c)) cap++;
             else if(islower(c)) sm++;
         }
@@ -12,10 +11,7 @@ int Strongest_Extension(string class_name,vector<string> extensions){
         if(strength > strongest) {
             strongest = strength;
             strongest_extension = extension;
-        } else if(strength == strongest) {
-            strongest_extension = extension;
         }
     }
-    
     return class_name + "." + strongest_extension;
 }
