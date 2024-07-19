2f981f2f56c4478fbac6290807a0ef2be6758c80
beep boop <githubbot@vadim.me>
2024-07-20 01:07:21 +0200

    Solution 20 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kginkz7r Prompt: 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                ^~~~~ 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                               ^ 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp: In function ‘int main()’: 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp:14:5: error: ‘vector’ was not declared in this scope    14 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp:14:12: error: expected primary-expressi on before ‘float’    14 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp:12: 13af11cc-b6e5-4a49-9180-4e90198772fc.cpp:15:31: error: ‘has_close_elements’ cann ot be used as a function    15 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,8 +1,8 @@
+#include <iostream>
 #include <vector>
-#include <cmath> // for abs function
-using namespace std;
+#include <cmath>
 
-bool has_close_elements(vector<float> numbers, float threshold){
+bool has_close_elements(std::vector<float> numbers, float threshold) {
     for(int i = 0; i < numbers.size() - 1; i++) {
         for(int j = i + 1; j < numbers.size(); j++) {
             if(abs(numbers[i] - numbers[j]) <= threshold) {
@@ -13,8 +13,9 @@ bool has_close_elements(vector<float> numbers, float threshold){
     return false;
 }
 
-int main(){
-    vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false);
+int main() {
+    std::vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
+    std::vector<float> b={1.1, 2.2, 3.1, 4.1, 5.1};
+    assert (has_close_elements(b, 0.5) == false);
     return 0;
 }
