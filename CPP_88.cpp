bcceb594cdb0d1adef522b7f619f772c646b6c04
beep boop <githubbot@vadim.me>
2024-07-19 03:12:37 +0200

    Solution 30 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/p0z2phda Prompt:       |                    ^~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:14: 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp: In function ‘int main()’: 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -10,9 +10,9 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     int sum = a.front() + a.back();
     
     if(sum % 2 == 0) {
-        sort(a.begin(), a.end(), std::greater<int>());
+        std::sort(a.begin(), a.end(), std::greater<int>());
     } else {
-        sort(a.begin(), a.end());
+        std::sort(a.begin(), a.end());
     }
     
     return a;
