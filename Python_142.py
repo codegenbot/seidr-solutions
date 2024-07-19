dd3e1db45f61be4c5a08f12d28aa7364f6aa5586
beep boop <githubbot@vadim.me>
2024-07-19 17:24:20 +0200

    Solution 35 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xmpasg0z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04a278d0-a485-46be-a257-5c63fa3a9 867.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04a278d0-a485-46be-a257-5c63fa3a9 867.py", line 13, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,8 +1,8 @@
 def sum_squares(lst):
     total_sum = 0
     for num in lst:
-        if isinstance(num, (int, float)):
+        if isinstance(num, int) and (num % 3 == 0 and not num % 4 or not num % 3 and num % 4 == 0):
             total_sum += num ** 2
-        else:
-            print("Invalid input. Please enter numbers only.")
+        elif isinstance(num, float) and (num % 3 == 0 and not num % 4 or not num % 3 and num % 4 == 0):
+            total_sum += num ** 3
     return total_sum
