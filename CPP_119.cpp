60bd8daf44ecd0d666553e5b06cedd955d773ec3
beep boop <githubbot@vadim.me>
2024-07-19 23:21:55 +0200

    Solution 11 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/j1kc9ool Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = char]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = char; std::allocator_tra its<std::allocator<_CharT> >::pointer = char*; std::allocator_traits<std::alloca tor<_CharT> >::allocator_type = std::allocator<char>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = char; _Alloc = std::allocator<char>; std::_Vector_base<_Tp, _Alloc>::poin ter = char*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const char&}; _Tp = char; _Alloc = std::allocator<char>; std::vect or<_Tp, _Alloc>::iterator = std::vector<char>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = char; _Alloc = std::allo cator<char>; std::vector<_Tp, _Alloc>::value_type = char]’ 10629233-e5a5-4014-9f10-a815ef7a5796.cpp:19:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<char>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <vector>
 
-int balance_parens() {
-    std::vector<char> lst;
-    std::cout << "Enter the strings: ";
-    char c;
-    while((c = getchar()) != '\n') {
-        lst.push_back(c);
-    }
+bool match_parens(const std::vector<char>& lst) {
     int open = 0, close = 0;
     for (char x : lst) {
         if (x == '(') open++;
         else if (x == ')') close++;
     }
-    return (open == close) ? 0 : -1;
+    return open == close;
+}
+
+int main() {
+    std::vector<char> lst;
+    std::cout << "Enter the strings: ";
+    char c;
+    while((c = fgetc(stdin)) != '\n') {
+        lst.push_back(c);
+    }
+    if (match_parens(lst)) {
+        std::cout << "The parentheses are matched." << std::endl;
+    } else {
+        std::cout << "The parentheses are not matched." << std::endl;
+    }
+    return 0;
 }
