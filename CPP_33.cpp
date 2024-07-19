f2d89f44501466acebc5bb18a287074531624b02
beep boop <githubbot@vadim.me>
2024-07-19 18:02:24 +0200

    Solution 9 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/c1dicbex Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a5e60aae-bbd4-4c07-a63c-f01c0d28b8ca.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a5e60aae-bbd4-4c07-a63c-f01c0d28b8ca.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a5e60aae-bbd4-4c07-a63c-f01c0d28b8ca.cpp:20: a5e60aae-bbd4-4c07-a63c-f01c0d28b8ca.cpp: In function ‘int main()’: a5e60aae-bbd4-4c07-a63c-f01c0d28b8ca.cpp:30:20: error: ‘sort_third’ was not decl ared in this scope    30 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ a5e60aae-bbd4-4c07-a63c-f01c0d28b8ca.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,7 +1,15 @@
-bool issame(vector<int> a, vector<int> b) {
-    set<int> s1(a.begin(), a.end());
-    set<int> s2(b.begin(), b.end());
-    return s1 == s2;
+bool issame(vector<int> v1, vector<int> v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<int> sort_third(vector<int> l) {
@@ -13,22 +21,8 @@ vector<int> sort_third(vector<int> l) {
                 temp.push_back(l[j]);
             }
             sort(temp.begin(), temp.end());
-            bool flag = true;
-            for (int k = 1; k < temp.size(); k++) {
-                if (!issame(vector<int>(temp.begin(), temp.begin() + 1), vector<int>(temp.begin() + k, temp.begin() + k + 1))) {
-                    flag = false;
-                    break;
-                }
-            }
-            if (flag) {
-                for (int k = 0; k < temp.size(); k++) {
-                    result.push_back(temp[k]);
-                }
-            } else {
-                sort(temp.begin(), temp.end());
-                for (int k = 0; k < temp.size(); k++) {
-                    result.push_back(temp[k]);
-                }
+            for (int k = 0; k < temp.size(); k++) {
+                result.push_back(temp[k]);
             }
         } else {
             result.push_back(l[i]);
