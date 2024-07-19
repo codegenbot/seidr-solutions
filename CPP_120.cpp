b0d9e6db3841c508d1445fbb3fe90e07c3d54923
beep boop <githubbot@vadim.me>
2024-07-19 23:37:10 +0200

    Solution 29 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/hcv8ocbq Prompt:       |                                  ^~~ 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp:35:39: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp:34: 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp: In function ‘int main()’: 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp:44:20: error: ‘maximum’ was not declare d in this scope    44 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 98b68821-e108-4ef5-8453-37bf37b3c4e6.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool areSame(vector<int> v1, vector<int> v2) {
+bool issame(vector<int> v1, vector<int> v2) {
     if(v1.size() != v2.size()) return false;
     for(int i = 0; i < v1.size(); i++) {
         if(v1[i] != v2[i]) return false;
@@ -27,6 +27,6 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; ++i) cin >> a[i];
     cin >> k;
-    if (!areSame(maximum(a, k), vector<int>())) return 1;
+    if (!issame(maximum(a, k), vector<int>())) return 1;
     return 0;
 }
