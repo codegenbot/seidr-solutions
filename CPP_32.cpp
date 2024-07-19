157e08364fab79d2c80f85d3512f1ce067aa9fd4
beep boop <githubbot@vadim.me>
2024-07-19 02:03:24 +0200

    Solution 4 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/hxr8gn9v Prompt:       |           ^~~~~~ In file included from 07ed4d96-46e4-4e4f-866f-9456686b68df.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 07ed4d96-46e4-4e4f-866f-9456686b68df.cpp:26:16: error: expected primary-expressi on before ‘double’    26 |         vector<double> coeffs = {};       |                ^~~~~~ 07ed4d96-46e4-4e4f-866f-9456686b68df.cpp:31:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    31 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 07ed4d96-46e4-4e4f-866f-9456686b68df.cpp:34:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    34 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 07ed4d96-46e4-4e4f-866f-9456686b68df.cpp:34:36: error: ‘find_zero’ cannot be use d as a function    34 |         solution = find_zero(coeffs);       |                                    ^

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,17 +1,19 @@
+#include <iostream>
 #include <vector>
 #include <cmath>
+
 using namespace std;
 
-double poly(double x, vector<double> coeff){
+double poly(double x, std::vector<double> coeffs){
     double result = 0.0;
-    for(int i = 0; i < coeff.size(); i++){
-        result += coeff[i] * pow(x, i);
+    for(int i = 0; i < coeffs.size(); i++){
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
 
-double find_zero(vector<double> xs, vector<double> coeff){
-    double a = poly(xs[0], coeff);
-    double b = poly(xs[1], coeff);
+double find_zero(std::vector<double> xs, std::vector<double> coeffs){
+    double a = poly(xs[0], coeffs);
+    double b = poly(xs[1], coeffs);
     return -b/a;
 }
