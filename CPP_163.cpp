3176a3e6d6a632a34464895832cd807b8cf25fb4
beep boop <githubbot@vadim.me>
2024-07-19 06:00:59 +0200

    Solution 40 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/d15ibz0y Prompt:       |           ^~~~~~ In file included from 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp:13: 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp: In function ‘int main()’: 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp:23:20: error: ‘generate_integers’ was n ot declared in this scope    23 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 96fa4f0d-fe62-4af3-8c94-83f4fa24cb48.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,9 +1,10 @@
 #include <vector>
+using namespace std;
 
 std::vector<int> issame(int a, int b) {
     std::vector<int> result;
-    for (int i = a; i <= b; ++i) {
-        if (i % 2 == 0) {
+    for(int i = a; i <= b; ++i) {
+        if(i % 2 == 0) {
             result.push_back(i);
         }
     }
