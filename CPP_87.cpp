639c52b2a96f7a4e35a51673f7a11de3a63228f1
beep boop <githubbot@vadim.me>
2024-07-19 03:17:51 +0200

    Solution 44 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r1dqhr1m Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ae169f61-b4b3-4fdd-a136-b7d3a4bc46ca.cpp:35:49: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ ae169f61-b4b3-4fdd-a136-b7d3a4bc46ca.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ ae169f61-b4b3-4fdd-a136-b7d3a4bc46ca.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from ae169f61-b4b3-4fdd-a136-b7d3a4bc46ca.cpp:34: ae169f61-b4b3-4fdd-a136-b7d3a4bc46ca.cpp: In function ‘int main()’: ae169f61-b4b3-4fdd-a136-b7d3a4bc46ca.cpp:47:27: error: could not convert ‘get_ro w(std::vector<std::vector<int> >, int)(3)’ from ‘vector<std::vector<int>>’ to ‘v ector<int>’    47 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           vector<std::vector<int>>

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -3,9 +3,7 @@
 #include <algorithm>
 #include <cassert>
 
-using vector = std::vector<int>;
-
-bool issame(vector a, vector b){
+bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
     return a == b;
 }
 
@@ -30,6 +28,6 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
 }
 
 int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3)[0], {2, 2}); 
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}})); 
     return 0;
 }
