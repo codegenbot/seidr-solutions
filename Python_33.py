dab2d473ba972aa013bdec0220c28ebd472f92fb
beep boop <githubbot@vadim.me>
2024-07-19 17:43:26 +0200

    Solution 63 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tlt7txr2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14c1b987-1ec0-4541-8554-54dc436dc 6d6.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14c1b987-1ec0-4541-8554-54dc436dc 6d6.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,16 @@
 def sort_third(l: list):
-    thirds = sorted([i for i in l if i % 3 == 0])
-    n = len(thirds)
-    mid = n // 2
+    mult_of_three = [i for i in l if i % 3 == 0]
+    sorted_mult_of_three = sorted(mult_of_three)
+    middle_index = len(sorted_mult_of_three) // 2
     return [
-        i if i % 3 != 0 else (thirds[mid] if n % 2 == 0 else thirds[mid + 1]) for i in l
+        (
+            i
+            if i % 3 != 0
+            else (
+                sorted_mult_of_three[middle_index]
+                if len(sorted_mult_of_three) % 2 == 1
+                else sorted_mult_of_three[middle_index + 1]
+            )
+        )
+        for i in l
     ]
