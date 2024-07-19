f4841f724388fc463c0cd12d2001054b40eb1783
beep boop <githubbot@vadim.me>
2024-07-19 03:00:38 +0200

    Solution 18 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/p7han3r6 Prompt: eecab456-c752-42f4-ad57-4ef0b1981600.cpp:29:29: error: ‘string’ was not declared  in this scope    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ eecab456-c752-42f4-ad57-4ef0b1981600.cpp:29:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from eecab456-c752-42f4-ad57-4ef0b1981600.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from eecab456-c752-42f4-ad57-4ef0b1981600.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,1 +1,25 @@
-assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "Equal");
+#include <iostream>
+#include <any>
+#include <string>
+#include <cassert>
+
+template <typename T>
+std::string compare_one(const T& t1, const T& t2) {
+    return (t1 == t2) ? "Equal" : "Not Equal";
+}
+
+std::string compare_one(const std::any& t1, const std::any& t2) {
+    if (t1.type() == t2.type()) {
+        if (t1.type() == typeid(std::string)) {
+            return (std::any_cast<std::string>(t1) == std::any_cast<std::string>(t2)) ? "Equal" : "Not Equal";
+        }
+    }
+    return "Not Equal";
+}
+
+int main() {
+    assert(compare_one(1, 1) == "Equal");
+    assert(compare_one(std::string("abc"), std::string("def")) == "Not Equal");
+    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "Equal");
+    return 0;
+}
