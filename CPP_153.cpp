85691abaab8a92e8012d2bee3b44ddae3ee77ed4
beep boop <githubbot@vadim.me>
2024-07-19 20:58:57 +0200

    Solution 61 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fk9tk9i8 Prompt: 057114df-b343-4c1c-8701-0f3e7e8ccbff.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 057114df-b343-4c1c-8701-0f3e7e8ccbff.cpp:25: 057114df-b343-4c1c-8701-0f3e7e8ccbff.cpp: In function ‘int main()’: 057114df-b343-4c1c-8701-0f3e7e8ccbff.cpp:27:13: error: ‘Strongest_Extension’ was  not declared in this scope    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,24 +1,21 @@
-class Solution {
-public:
-    string Strongest_Extension(string class_name, vector<string> extensions) {
-        int max_strength = -1;
-        string strongest_extension;
+string Strongest_Extension(string class_name, vector<string> extensions) {
+    int max_strength = -1;
+    string strongest_extension;
 
-        for (const auto& extension : extensions) {
-            int cap = 0, sm = 0;
-            for (char c : extension) {
-                if (isupper(c)) cap++;
-                else if (islower(c)) sm++;
-            }
-            int strength = cap - sm;
-            if (strength > max_strength) {
-                max_strength = strength;
-                strongest_extension = extension;
-            } else if (strength == max_strength) {
-                strongest_extension = extension;
-            }
+    for (const auto& extension : extensions) {
+        int cap = 0, sm = 0;
+        for (char c : extension) {
+            if (isupper(c)) cap++;
+            else if (islower(c)) sm++;
         }
-
-        return class_name + "." + strongest_extension;
+        int strength = cap - sm;
+        if (strength > max_strength) {
+            max_strength = strength;
+            strongest_extension = extension;
+        } else if (strength == max_strength) {
+            strongest_extension = extension;
+        }
     }
-};
+
+    return class_name + "." + strongest_extension;
+}
