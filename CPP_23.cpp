90e051b5a89c340c90df3d0730011cfb10e72225
beep boop <githubbot@vadim.me>
2024-07-19 21:23:04 +0200

    Solution 2 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vxzr422a Prompt: c59fc791-d3b2-4cb1-9cac-823e08841d67.cpp:1:5: warning: built-in function ‘strlen ’ declared as non-function [-Wbuiltin-declaration-mismatch]     1 | int strlen(string str){       |     ^~~~~~ c59fc791-d3b2-4cb1-9cac-823e08841d67.cpp:1:12: error: ‘string’ was not declared in this scope     1 | int strlen(string str){       |            ^~~~~~ In file included from c59fc791-d3b2-4cb1-9cac-823e08841d67.cpp:5: c59fc791-d3b2-4cb1-9cac-823e08841d67.cpp: In function ‘int main()’: c59fc791-d3b2-4cb1-9cac-823e08841d67.cpp:7:19: error: ‘strlen’ cannot be used as  a function     7 |     assert (strlen("asdasnakj") == 9);       |             ~~~~~~^~~~~~~~~~~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,10 +1,9 @@
 #include <string>
 
-int my_strlen(std::string str){
+int strLength(std::string str){
     return str.length();
 }
 
 int main() {
-    assert (my_strlen("asdasnakj") == 12);
-    return 0;
+    assert (strLength("asdasnakj") == 13);
 }
