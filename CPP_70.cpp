bfd811fbcc23a2aa134039c1a3b4fdddd1abce89
beep boop <githubbot@vadim.me>
2024-07-20 00:20:34 +0200

    Solution 48 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4bqexe7l Prompt:       |                    ^~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:13: f34ea320-8c02-4016-9918-70cca1e2f80c.cpp: In function ‘int main()’: f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:23:20: error: ‘strange_sort_list’ was n ot declared in this scope    23 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ f34ea320-8c02-4016-9918-70cca1e2f80c.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,26 +1,12 @@
 #include <vector>
 
-vector<int> strange_sort_list(vector<int> lst) {
-    sort(lst.begin(), lst.end());
-    vector<int> result;
-    int left = 0, right = lst.size() - 1;
-    while (left <= right) {
-        result.push_back(lst[right]);
-        if (left != right) {
-            result.push_back(lst[left]);
-        }
-        left++;
-        right--;
-    }
-    return result;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
 }
-  
+
 int main() {
-    vector<int> input = {3, 1, 4, 1, 5, 9};
-    vector<int> output = strange_sort_list(input);
-    for (int val : output) {
-        cout << val << " ";
-    }
-  
+    // Add your main function implementation here
     return 0;
 }
