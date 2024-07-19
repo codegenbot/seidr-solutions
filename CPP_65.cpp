0c49f8bd8b7f2b592db3104e61e6db8372fbd6c4
beep boop <githubbot@vadim.me>
2024-07-19 21:33:19 +0200

    Solution 24 of CPP/65, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/bcfpo8tl Prompt: 1f4ed132-75c0-4f7d-bccf-78896034c545.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string circular_shift(int x, int shift){       | ^~~~~~ In file included from 1f4ed132-75c0-4f7d-bccf-78896034c545.cpp:14: 1f4ed132-75c0-4f7d-bccf-78896034c545.cpp: In function ‘int main()’: 1f4ed132-75c0-4f7d-bccf-78896034c545.cpp:16:13: error: ‘circular_shift’ was not declared in this scope    16 |     assert (circular_shift(11, 101) == "11");       |             ^~~~~~~~~~~~~~

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,15 +1,23 @@
 #include <string>
-using namespace std;
+#include <algorithm>
+#include <cassert>
 
-string circular_shift(int x, int shift){
-    string num = to_string(x);
+std::string circular_shift(int x, int shift);
+
+int main() {
+    assert(circular_shift(11, 101) == "11");
+    return 0;
+}
+
+std::string circular_shift(int x, int shift){
+    std::string num = std::to_string(x);
     int n = num.size();
     shift = shift % n;
     if(shift == 0) return num;
     if(shift > n) {
-        reverse(num.begin(), num.end());
+        std::reverse(num.begin(), num.end());
         return num;
     }
-    rotate(num.rbegin(), num.rbegin() + shift, num.rend());
+    std::rotate(num.rbegin(), num.rbegin() + shift, num.rend());
     return num;
 }
