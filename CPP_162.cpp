c75456f621c2aa392d7c3f6327ad7f961ea30278
beep boop <githubbot@vadim.me>
2024-07-19 06:09:30 +0200

    Solution 56 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/twbnhxvv Prompt: 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp: In function ‘std::string string_to_md5 (const string&)’: 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:12:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    12 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:3: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp: At global scope: 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,25 +2,31 @@
 #include <string>
 #include <openssl/evp.h>
 #include <cassert>
+#include <iomanip>
+#include <sstream>
 
 std::string string_to_md5(const std::string& text) {
     if (text.empty()) {
         return "None";
     }
 
-    unsigned char digest[EVP_MAX_MD_SIZE];
     EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
     EVP_DigestInit(mdctx, EVP_md5());
-    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal(mdctx, digest, nullptr);
-    EVP_MD_CTX_free(mdctx);
 
-    char mdString[33];
-    for (int i = 0; i < 16; i++) {
-        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
+    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
+
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int digest_len;
+    EVP_DigestFinal(mdctx, digest, &digest_len);
+
+    std::stringstream ss;
+    for (unsigned int i = 0; i < digest_len; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)digest[i];
     }
 
-    return std::string(mdString);
+    EVP_MD_CTX_free(mdctx);
+
+    return ss.str();
 }
 
 int main() {
