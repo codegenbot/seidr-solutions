cb7b1e8e8d0e02f24590af6e3d1a8dd6d62c3c6a
beep boop <githubbot@vadim.me>
2024-07-19 19:17:41 +0200

    Solution 74 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3p991rkm Prompt: a1f131bd-93c0-477a-a2a8-9682f01b46e6.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ a1f131bd-93c0-477a-a2a8-9682f01b46e6.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from a1f131bd-93c0-477a-a2a8-9682f01b46e6.cpp:32: a1f131bd-93c0-477a-a2a8-9682f01b46e6.cpp: In function ‘int main()’: a1f131bd-93c0-477a-a2a8-9682f01b46e6.cpp:42:20: error: ‘common’ was not declared  in this scope    42 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -8,7 +7,9 @@
 
 using namespace std;
 
-vector<int> solve(const vector<int>& l1, const vector<int>& l2) {
+vector<int> solve(vector<int> l1, vector<int> l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
     vector<int> result;
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
@@ -21,9 +22,4 @@ bool issame(const vector<int>& v1, const vector<int>& v2) {
 
 void solveProblem() {
     assert(issame(solve({4, 3, 2, 8}, {}), {}));
-}
-
-int main() {
-    solveProblem();
-    return 0;
 }
