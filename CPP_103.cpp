034821bdec2afe53995df3f651e9f8e92b47c733
beep boop <githubbot@vadim.me>
2024-07-20 00:45:20 +0200

    Solution 23 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/drb99h6s Prompt:       |     ^~~~~~ ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp:12:5: error: ‘size_t’ does not name a t ype    12 |     size_t pos = binaryAvg.find('1');       |     ^~~~~~ ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp:1:1: note: ‘size_t’ is defined in heade r ‘<cstddef>’; did you forget to ‘#include <cstddef>’?   +++ |+#include <cstddef>     1 | if (n > m) { ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return (pos != string::npos) ? binaryAvg.substr(pos) : "0";       |     ^~~~~~ ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp:16: ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp: In function ‘int main()’: ec5d8088-5f94-4e70-af13-318d5c8a69a6.cpp:18:13: error: ‘rounded_avg’ was not dec lared in this scope    18 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -19,7 +19,7 @@ string average_binary(int n, int m) {
     int avg = round((double)sum / (m - n + 1));
     string binaryAvg = bitset<32>(avg).to_string();
     size_t pos = binaryAvg.find('1');
-    return (pos != std::string::npos) ? binaryAvg.substr(pos) : "0";
+    return (pos != string::npos) ? binaryAvg.substr(pos) : "0";
 }
 
 int main() {
