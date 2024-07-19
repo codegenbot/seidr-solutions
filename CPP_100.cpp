7d3485ec443b6e9215a9b21689bc2f942207f313
beep boop <githubbot@vadim.me>
2024-07-19 02:58:12 +0200

    Solution 96 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yc5z6gyy Prompt: 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:23: 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp: In function ‘int main()’: 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:33:20: error: ‘make_a_pile’ was not dec lared in this scope; did you mean ‘contest::make_a_pile’?    33 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:9:22: note: ‘contest::make_a_pile’ decl ared here     9 |     std::vector<int> make_a_pile(int n) {       |                      ^~~~~~~~~~~ In file included from 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:23: 7a6d59f9-64d4-41df-8dbd-e93e28091754.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,21 +1,6 @@
-#include <vector>
-#include <cassert>
+#include <iostream>
 
-namespace contest {
-    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-        return a == b;
-    }
-
-    std::vector<int> make_a_pile(int n) {
-        std::vector<int> result;
-        for (int i = 0; i < n; ++i) {
-            result.push_back(8 + 2 * i);
-        }
-        return result;
-    }
-
-    int contest_main() {
-        assert(contest::issame(make_a_pile(8), std::vector<int>{8, 10, 12, 14, 16, 18, 20, 22}));
-        return 0;
-    }
+int main() {
+    contest::contest_main();
+    return 0;
 }
