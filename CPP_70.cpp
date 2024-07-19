efdd8d524d47b8e76e69c832453912d593fe9670
beep boop <githubbot@vadim.me>
2024-07-19 18:47:34 +0200

    Solution 24 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/j886bobi Prompt:       |             ^~~~~~ 782e061c-01b1-4c31-adc6-20086983fffa.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 782e061c-01b1-4c31-adc6-20086983fffa.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 782e061c-01b1-4c31-adc6-20086983fffa.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 782e061c-01b1-4c31-adc6-20086983fffa.cpp:33: 782e061c-01b1-4c31-adc6-20086983fffa.cpp: In function ‘int main()’: 782e061c-01b1-4c31-adc6-20086983fffa.cpp:43:20: error: ‘strange_sort_list’ was n ot declared in this scope    43 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 782e061c-01b1-4c31-adc6-20086983fffa.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,19 +2,12 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(const vector<int>& a, const vector<int>& b){
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-vector<int> strange_sort_list(vector<int> lst);
-
-int main() {
-    assert (issame(strange_sort_list({111111}), {111111}));
-    return 0;
-}
-
 vector<int> strange_sort_list(vector<int> lst){
-    sort(lst.begin(), lst.end(), [&](int a, int b) {
+    sort(lst.begin(), lst.end(), [](int a, int b){
         return issame({a%10}, {b%10}) ? a < b : a%10 < b%10;
     });
     vector<int> result;
@@ -30,4 +23,8 @@ vector<int> strange_sort_list(vector<int> lst){
         right--;
     }
     return result;
+}
+
+int main() {
+    assert(issame(strange_sort_list({111111}), {111111}));
 }
