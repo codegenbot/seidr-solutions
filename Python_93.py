640d1490ca0f2b0742fe0fc0907cd4f8c7648d03
beep boop <githubbot@vadim.me>
2024-07-19 17:43:10 +0200

    Solution 85 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/189ox018 Prompt: Program did not receive expected input

--- Python_93.py
+++ Python_93.py
@@ -1,8 +1,8 @@
 def encode(message):
     result = ""
     for char in message:
-        if char.isalpha():
-            base = ord('a' if char.islower() else 'A')
+        if char.isalnum():
+            base = ord("a" if char.islower() else "A")
             shift = 3
             result += chr((ord(char) - base + shift) % 26 + base)
         else:
