84e291a6b01a8801ae3d2e032b8045fb7c4b54ff
beep boop <githubbot@vadim.me>
2024-07-19 02:33:29 +0200

    Solution 40 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duyg3to1 Prompt:       |                                 ^~~~~~ In file included from 608eaab7-f848-49d7-8066-7f318e123ddb.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 608eaab7-f848-49d7-8066-7f318e123ddb.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 608eaab7-f848-49d7-8066-7f318e123ddb.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 608eaab7-f848-49d7-8066-7f318e123ddb.cpp:21: 608eaab7-f848-49d7-8066-7f318e123ddb.cpp: In function ‘int main()’: 608eaab7-f848-49d7-8066-7f318e123ddb.cpp:31:20: error: ‘total_match’ was not dec lared in this scope    31 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 608eaab7-f848-49d7-8066-7f318e123ddb.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,6 +1,16 @@
 #include <vector>
+#include <string>
+#include <cassert>
 
-using namespace std;
+vector<string> total_match(vector<string> lst1, vector<string> lst2);
+
+int total_chars(vector<string> lst) {
+    int total = 0;
+    for (string s : lst) {
+        total += s.length();
+    }
+    return total;
+}
 
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
@@ -12,14 +22,6 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     } else {
         return lst2;
     }
-}
-
-int total_chars(vector<string> lst) {
-    int total = 0;
-    for (string s : lst) {
-        total += s.length();
-    }
-    return total;
 }
 
 int main() {
