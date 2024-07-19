7d43f1df3e37d2b493c199d566de766bd1b61af3
beep boop <githubbot@vadim.me>
2024-07-19 23:55:51 +0200

    Solution 95 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/scsisb4x Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4c9f8d3d-942d-44b8-bfce-f87eb33777b5.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4c9f8d3d-942d-44b8-bfce-f87eb33777b5.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4c9f8d3d-942d-44b8-bfce-f87eb33777b5.cpp:11: 4c9f8d3d-942d-44b8-bfce-f87eb33777b5.cpp: In function ‘int main()’: 4c9f8d3d-942d-44b8-bfce-f87eb33777b5.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 4c9f8d3d-942d-44b8-bfce-f87eb33777b5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,22 +1,12 @@
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring);
 
 int main() {
-    vector<string> strings = {"apple", "banana", "grape", "orange"};
-    string substring = "an";
-    vector<string> filtered_strings = filter_by_substring(strings, substring);
-    
+    vector<string> strings = {"apple", "banana", "orange", "grape"};
+    vector<string> filtered_strings = filter_by_substring(strings, "an");
     assert(issame(filtered_strings, vector<string>{"banana", "orange"}));
     
     return 0;
