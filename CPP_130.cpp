976c212ae2b7ed27008827449831c0f0714a4905
beep boop <githubbot@vadim.me>
2024-07-19 03:50:21 +0200

    Solution 10 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sg453w14 Prompt:       |                    ^~~ 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp:18: 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp: In function ‘int main()’: 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 8bed3977-ff8b-4809-8a6b-7bfeeef58d98.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,28 +1,23 @@
 #include <vector>
-using namespace std;
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
+bool issame(std::vector<int> a, std::vector<int> b){
+    if(a.size() != b.size()) return false;
     
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+    for(int i=0; i<a.size(); ++i){
+        if(a[i] != b[i]) return false;
     }
     
     return true;
 }
 
-vector<int> tri(int n) {
-    vector<int> tribonacci(n + 1);
+std::vector<int> tri(int n){
+    std::vector<int> tribonacci(n+1);
     tribonacci[0] = 3;
-    if (n > 0) tribonacci[1] = 1;
-    if (n > 1) tribonacci[2] = 3;
+    if(n > 0) tribonacci[1] = 1;
+    if(n > 1) tribonacci[2] = 3;
     
-    for (int i = 3; i <= n; ++i) {
-        if (i % 2 == 0) {
+    for(int i = 3; i <= n; ++i){
+        if(i % 2 == 0){
             tribonacci[i] = 1 + i / 2;
         } else {
             tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3];
@@ -30,10 +25,4 @@ vector<int> tri(int n) {
     }
     
     return tribonacci;
-}
-
-int main() {
-    assert(issame(tri(1), {3, 1}));
-    
-    return 0;
 }
