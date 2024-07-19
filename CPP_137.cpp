5a5c93656a62269befdf194552121b77ea46018a
beep boop <githubbot@vadim.me>
2024-07-19 07:06:11 +0200

    Solution 93 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/riusp3s9 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,31 +1,32 @@
-if(typeid(int) == a.type() && typeid(int) == b.type()){
-    int int_a = boost::any_cast<int>(a);
-    int int_b = boost::any_cast<int>(b);
-    if(int_a > int_b){
-        return int_a;
-    } else if(int_a < int_b){
-        return int_b;
-    } else {
-        return "None";
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    int n1 = boost::any_cast<int>(a);
+    int n2 = boost::any_cast<int>(b);
+    if (n1 > n2) {
+        return n1;
+    } else if (n1 < n2) {
+        return n2;
     }
-} else if(typeid(float) == a.type() && typeid(float) == b.type()){
-    float float_a = boost::any_cast<float>(a);
-    float float_b = boost::any_cast<float>(b);
-    if(float_a > float_b){
-        return float_a;
-    } else if(float_a < float_b){
-        return float_b;
-    } else {
-        return "None";
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    float f1 = boost::any_cast<float>(a);
+    float f2 = boost::any_cast<float>(b);
+    if (f1 > f2) {
+        return f1;
+    } else if (f1 < f2) {
+        return f2;
     }
-} else if(typeid(string) == a.type() && typeid(string) == b.type()){
-    string str_a = boost::any_cast<string>(a);
-    string str_b = boost::any_cast<string>(b);
-    if(stof(str_a) > stof(str_b)){
-        return str_a;
-    } else if(stof(str_a) < stof(str_b)){
-        return str_b;
-    } else {
-        return "None";
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    string s1 = boost::any_cast<string>(a);
+    string s2 = boost::any_cast<string>(b);
+    if (s1.find(',') != string::npos) {
+        replace(s1.begin(), s1.end(), ',', '.');
+    }
+    if (s2.find(',') != string::npos) {
+        replace(s2.begin(), s2.end(), ',', '.');
+    }
+    if (stof(s1) > stof(s2)) {
+        return s1;
+    } else if (stof(s1) < stof(s2)) {
+        return s2;
     }
 }
+return "None";
