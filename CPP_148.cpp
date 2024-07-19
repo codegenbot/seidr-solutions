c89c8d2dc044ca529aea22bde1fad3fc831ffceb
beep boop <githubbot@vadim.me>
2024-07-19 20:42:59 +0200

    Solution 6 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5w0ff9r0 Prompt: Complete the following code given the task description and function signature.

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,29 +1,27 @@
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    int index1 = 0;
+    int index1 = -1;
+    int index2 = -1;
+
     for (int i = 0; i < planets.size(); i++) {
         if (planets[i] == planet1) {
             index1 = i;
-            break;
-        }
-    }
-    
-    int index2 = 0;
-    for (int i = 0; i < planets.size(); i++) {
-        if (planets[i] == planet2) {
+        } else if (planets[i] == planet2) {
             index2 = i;
-            break;
         }
     }
 
+    if (index1 == -1 || index2 == -1)
+        return {};
+
     vector<string> result;
-    if (index1 >= 0 && index2 >= 0) {
-        for (int i = index1 + 1; i < index2; i++) {
+
+    for (int i = 0; i < planets.size(); i++) {
+        if ((index1 > i && i > index2) || (index1 < i && i < index2))
+            continue;
+        else
             result.push_back(planets[i]);
-        }
-    } else {
-        result.clear();
     }
-    
+
     return result;
 }
