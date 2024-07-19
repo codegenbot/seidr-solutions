5b88d726f3b66aff50b30d5bd79f8bc520f6751e
beep boop <githubbot@vadim.me>
2024-07-20 00:26:16 +0200

    Solution 99 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bgze1p9g Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,12 +1,25 @@
-```
-```
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
 double rounded_avg(int n, int m) {
     if (n > m) return -1.0;
     double sum = 0.0;
     for (int i = n; i <= m; i++) {
-        sum += (double)i; 
+        ostringstream oss;
+        oss << i;
+        string s = oss.str();
+        sum += stod(s); 
     }
-    double avg = round((sum / (m - n + 1)) * 2.0) / 2.0;
+    double avg = round((sum / (m - n + 1)) * 2) / 2.0;
     return avg;
 }
-```
+
+int main() {
+    int n, m;
+    cout << "Enter the range: ";
+    cin >> n >> m;
+    cout << "The average is: " << rounded_avg(n, m);
+    return 0;
+}
