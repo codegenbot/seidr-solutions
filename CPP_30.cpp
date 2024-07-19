caf4c57c29b56f7fe2f43a19b09ccf09b1e032ca
beep boop <githubbot@vadim.me>
2024-07-19 14:32:09 +0200

    Solution 3 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wb8za879 Prompt:  in this scope    18 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d0184efa-ea93-4471-acc2-6dfcda41141d.cpp:18:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d0184efa-ea93-4471-acc2-6dfcda41141d.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d0184efa-ea93-4471-acc2-6dfcda41141d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d0184efa-ea93-4471-acc2-6dfcda41141d.cpp:18:36: error: expected primary-expressi on before ‘float’    18 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from d0184efa-ea93-4471-acc2-6dfcda41141d.cpp:17: d0184efa-ea93-4471-acc2-6dfcda41141d.cpp: In function ‘int main()’: d0184efa-ea93-4471-acc2-6dfcda41141d.cpp:27:20: error: ‘get_positive’ was not de clared in this scope    27 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,13 +1,22 @@
 #include <vector>
+#include <cassert> // Include necessary header for assert()
 
-bool issame(std::vector<float> a, std::vector<float> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
+bool issame(float a, float b) {
+    return a == b;
+}
+
+vector<float> get_positive(vector<float> l);
+
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
         }
     }
-    return true;
+    return result;
+}
+
+int main() {
+    assert(issame(get_positive({}), {}));
 }
