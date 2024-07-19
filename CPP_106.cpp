3e0bd64e7a3dd91797d79382b47121ca027e9273
beep boop <githubbot@vadim.me>
2024-07-19 03:24:12 +0200

    Solution 10 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lt4rk6x5 Prompt:       |                    ^~~ 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp:21: 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp: In function ‘int main()’: 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp:31:20: error: ‘f’ was not declared in t his scope    31 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 49ee55a1-bfd5-4771-9a6a-86f977c5fbc7.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -13,12 +13,12 @@ std::vector<int> f(int n){
         } else {
             int sum = 0;
             for(int j = 1; j <= i; ++j){
-               sum += j;
-           }
-           result[i-1] = sum;
-       }
-   }
-   return result;
+                sum += j;
+            }
+            result[i-1] = sum;
+        }
+    }
+    return result;
 }
 
 bool issame(std::vector<int> a, std::vector<int> b){
