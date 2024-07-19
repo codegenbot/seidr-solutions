387f683f3bdaa6339c90789cf9ccb0122fe71543
beep boop <githubbot@vadim.me>
2024-07-19 19:06:11 +0200

    Solution 48 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gahd0o9k Prompt:       |           ^~~~~~ In file included from cdffe908-97de-47f5-b70a-3cbd48c9baf4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cdffe908-97de-47f5-b70a-3cbd48c9baf4.cpp:5:22: error: expected primary-expressio n before ‘int’     5 | int max_value(vector<int> l) {       |                      ^~~ In file included from cdffe908-97de-47f5-b70a-3cbd48c9baf4.cpp:10: cdffe908-97de-47f5-b70a-3cbd48c9baf4.cpp: In function ‘int main()’: cdffe908-97de-47f5-b70a-3cbd48c9baf4.cpp:12:17: error: ‘max_element’ was not dec lared in this scope; did you mean ‘std::max_element’?    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from cdffe908-97de-47f5-b70a-3cbd48c9baf4.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: ‘std::max_element’ d eclared here   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,9 +1,12 @@
 #include <vector>
 #include <algorithm>
-#include <cmath>
-#include <iostream>
+
+using namespace std;
+
+int max_value(vector<int> l) {
+    return *max_element(l.begin(), l.end());
+}
 
 int main() {
-    auto v = std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
-    assert(abs(*v - 124) < 1e-4);
+    assert(abs(max_value({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12.4)<1e-4);
 }
