af963a13bb0cef4f998a29d1544278df04ff4c1e
beep boop <githubbot@vadim.me>
2024-07-19 09:17:29 +0200

    Solution 75 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/76xemsec Prompt:       |                    ^~~ 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp:21: 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp: In function ‘int main()’: 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 79aa8715-609c-46fc-a365-d1e33c6edc64.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,12 +1,12 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return (a == b);
 }
 
-std::vector<int> pluck(std::vector<int> arr) {
-    std::vector<int> result;
+vector<int> pluck(vector<int> arr) {
+    vector<int> result;
     int smallest_even = -1;
     int smallest_even_index = -1;
 
@@ -26,8 +26,8 @@ std::vector<int> pluck(std::vector<int> arr) {
 }
 
 int main() {
-    std::vector<int> input = {3, 5, 2, 8, 4};
-    std::vector<int> output = pluck(input);
-    assert(issame(output, std::vector<int>{2, 2}));
+    vector<int> input = {1, 3, 5, 2, 6, 4};
+    vector<int> output = pluck(input);
+    assert(issame(output, {2, 3}));
     return 0;
 }
