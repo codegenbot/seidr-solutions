75f32de28d2200c0f92dfda879cc70ab4130b543
beep boop <githubbot@vadim.me>
2024-07-19 18:32:58 +0200

    Solution 12 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3p991rkm Prompt: fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp: In function ‘std::vector<int> solve(st d::vector<int>&, std::vector<int>&)’: fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp:5:5: error: ‘sort’ was not declared in this scope; did you mean ‘short’?     5 |     sort(l1.begin(), l1.end());       |     ^~~~       |     short fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp:8:5: error: ‘set_intersection’ was not declared in this scope     8 |     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_in serter(result));       |     ^~~~~~~~~~~~~~~~ fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp:9:18: error: ‘unique’ was not declared in this scope     9 |     result.erase(unique(result.begin(), result.end()), result.end());       |                  ^~~~~~ In file included from fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp:13: fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp: In function ‘int main()’: fe857258-b913-48bf-a778-dd2ac6bd6ed8.cpp:23:20: error: ‘common’ was not declared  in this scope    23 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,7 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <iterator>
 #include <cassert>
-using namespace std;
 
 vector<int> solve(vector<int>& l1, vector<int>& l2) {
     sort(l1.begin(), l1.end());
@@ -12,7 +12,4 @@ vector<int> solve(vector<int>& l1, vector<int>& l2) {
     return result;
 }
 
-int main() {
-    assert (issame(common({4, 3, 2, 8}, {}) , {}));
-    return 0;
-}
+assert(issame(solve({4, 3, 2, 8}, {}), {}));
