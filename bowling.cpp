b3db0f46067283ddef20539e5a5b3a85b8195d6f
beep boop <githubbot@vadim.me>
2024-07-19 17:05:09 +0200

    Solution 20 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/rr9jqcik Prompt: 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:30:19: error: ‘string’ has not been dec lared    30 | char next(char c, string s) {       |                   ^~~~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp: In function ‘char next(char, int)’: 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:31:15: error: request for member ‘find’  in ‘s’, which is of non-class type ‘int’    31 |     int i = s.find(c);       |               ^~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:32:13: error: invalid types ‘int[int]’ for array subscript    32 |     return s[i + 1];       |             ^

--- bowling.cpp
+++ bowling.cpp
@@ -4,31 +4,32 @@ int bowlingScore(std::string s) {
     int score = 0;
     int currentFrame = 0;
     vector<int> frames(10);
-    
+
     for (char c : s) {
         if (c == '/') {
             currentFrame++;
             continue;
         }
-        
+
         if (c >= '1' && c <= '9') {
+            int strike = false;
             while (c != '/' && c >= '0' && c <= '9') {
-                frames[currentFrame] += c - '0';
+                frames[currentFrame] *= 10 + (c - '0');
                 c = next(c, s);
             }
-            
-            if (frames[currentFrame] == 10) {
-                score += 10;
-            } else {
+
+            if (!strike) {
                 score += frames[currentFrame];
+            } else {
+                score += 10;
             }
         }
     }
-    
+
     return score;
 }
 
-char next(char c, std::string s) {
-    int i = s.find(c);
+char next(char a, char b, std::string s) {
+    int i = s.find(std::string(1, a));
     return s[i + 1];
 }
