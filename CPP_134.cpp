0de230ebf9fce8f0f3329da24cf025a3dddb5df7
beep boop <githubbot@vadim.me>
2024-07-19 19:12:20 +0200

    Solution 6 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mn058wyc Prompt: 1a4871db-d6ed-45c3-889c-dc49b7c707f9.cpp:1:37: error: ‘string’ was not declared in this scope     1 | bool check_if_last_char_is_a_letter(string txt){       |                                     ^~~~~~ In file included from 1a4871db-d6ed-45c3-889c-dc49b7c707f9.cpp:12: 1a4871db-d6ed-45c3-889c-dc49b7c707f9.cpp: In function ‘int main()’: 1a4871db-d6ed-45c3-889c-dc49b7c707f9.cpp:14:43: error: ‘check_if_last_char_is_a_ letter’ cannot be used as a function    14 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,6 +1,6 @@
 using namespace std;
 
-bool check_if_last_char_is_a_letter(string txt){
+bool check_if_last_char_is_a_letter(string txt) {
     if(txt.empty()) return false;
     for(int i=0; i<txt.length(); i++){
         if(!isalpha(txt[i])) {
