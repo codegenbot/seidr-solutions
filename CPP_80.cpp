1e5d1ee7c31bef575667cb0315cd67e23338716d
beep boop <githubbot@vadim.me>
2024-07-19 22:13:00 +0200

    Solution 5 of CPP/80, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/unqcf6rv Prompt: f5b9a815-7b8a-4a1d-af30-fca861bfee23.cpp:3:15: error: ‘string’ was not declared in this scope     3 | bool is_happy(string s) {       |               ^~~~~~ f5b9a815-7b8a-4a1d-af30-fca861bfee23.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; In file included from f5b9a815-7b8a-4a1d-af30-fca861bfee23.cpp:26: f5b9a815-7b8a-4a1d-af30-fca861bfee23.cpp: In function ‘int main()’: f5b9a815-7b8a-4a1d-af30-fca861bfee23.cpp:28:21: error: ‘is_happy’ cannot be used  as a function    28 |     assert (is_happy("iopaxioi") == false );       |             ~~~~~~~~^~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,8 +1,12 @@
-bool is_happy(std::string s) {
+#include <string>
+
+using namespace std;
+
+bool is_happy(string s) {
     if (s.length() < 3)
         return false;
     for (int i = 0; i <= s.length() - 3; i++) {
-        std::string sub = s.substr(i, 3);
+        string sub = s.substr(i, 3);
         bool unique = true;
         for (char c : sub) {
             int count = 0;
