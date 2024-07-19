02dd9ac68beab522b55b6cd2a7ec4f45a749a3b6
beep boop <githubbot@vadim.me>
2024-07-20 00:36:17 +0200

    Solution 15 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4rvmz47y Prompt: Complete the following code given the task description and function signature.

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,6 +1,6 @@
 int sum = 0;
-    while (N != 0) {
+    while(N > 0) {
         sum += N % 2;
         N /= 2;
     }
-    return bitset<64>(sum).to_string().substr(64 - sum);
+    return bitset<8>(sum).to_string().substr(8 - sum, sum);
