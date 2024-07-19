01bace4360cd3c2f7f2945cba4c73548506a3c29
beep boop <githubbot@vadim.me>
2024-07-19 17:22:36 +0200

    Solution 88 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70d917e8-7478-483f-bef5-a8f0c8b2a 75a.py", line 1     ```'def solution(input_list):    if not input_list:        return "The list is empty."    elif all(isinstance(x, int) for x in input_list):        return su m(input_list)    else:        return "The list contains non-integer values."''                                                                              ^ SyntaxError: unterminated string literal (detected at line 1)

--- Python_121.py
+++ Python_121.py
@@ -1,8 +1,7 @@
-```python
-def solution(input_list):    
-    if not input_list:        
-        return "The list is empty."    
-    elif all(x.isdigit() for x in input_list):        
-        return int(''.join(map(str, input_list)))    
-    else:        
-        return "The list contains non-integer values."```
+def solution(input_list):
+    if not input_list:
+        return "The list is empty."
+    elif all(isinstance(x, int) for x in input_list):
+        return sum(input_list)
+    else:
+        return "The list contains non-integer values."
