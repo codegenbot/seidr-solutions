2be26f34792f9a9b9653db7e3f91119697162f5c
beep boop <githubbot@vadim.me>
2024-07-19 05:16:58 +0200

    Solution 65 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uoxaug98 Prompt:       |                    ^~~ 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp:9: 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp: In function ‘int main()’: 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp:19:20: error: ‘compare’ was not declare d in this scope    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 58b5efe7-90fe-4c50-9b76-def1e0dcab4c.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -5,7 +5,12 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-int main() {
-    assert(issame(compare({1,2,3,5},{-1,2,3,4}), {2,0,0,1}));
-    return 0;
+vector<int> compare(vector<int> game, vector<int> guess) {
+    vector<int> result;
+    for (int i = 0; i < game.size(); ++i) {
+        result.push_back(abs(game[i] - guess[i]));
+    }
+    return result;
 }
+
+assert(issame(compare({1,2,3,5},{-1,2,3,4}), {2,0,0,1}));
