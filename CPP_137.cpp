d070cc1e44838c51214efc1d765ce06d69cc2965
beep boop <githubbot@vadim.me>
2024-07-19 03:02:43 +0200

    Solution 27 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/p7han3r6 Prompt: 2ef1b078-b51f-465a-9dd4-034e2fc7a725.cpp:20:29: error: ‘string’ was not declared  in this scope    20 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 2ef1b078-b51f-465a-9dd4-034e2fc7a725.cpp:20:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 2ef1b078-b51f-465a-9dd4-034e2fc7a725.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 2ef1b078-b51f-465a-9dd4-034e2fc7a725.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
 #include <string>
-#include <boost/any.hpp>
+#include <iostream>
+#include <any>
+#include <cassert>
 
-boost::any compare_one(const std::string& s1, const std::string& s2) {
+std::any compare_one(const std::string& s1, const std::string& s2) {
     if (s1 == s2) {
-        return boost::lexical_cast<std::string>("Equal");
+        return std::string("Equal");
     } else {
-        return boost::lexical_cast<std::string>("Not equal");
+        return std::string("Not equal");
     }
 }
 
 int main() {
-    assert(boost::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "Equal");
+    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "Equal");
 }
