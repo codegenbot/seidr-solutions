b18264460aba44a39a6204ebba33bc8124198860
beep boop <githubbot@vadim.me>
2024-07-19 20:12:15 +0200

    Solution 2 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duz954ia Prompt: e363840a-3dc8-437e-a552-73ad00022619.cpp:14:5: error: ‘string’ does not name a t ype    14 |     string suffix = file_name.substr(dot_pos + 1);       |     ^~~~~~ e363840a-3dc8-437e-a552-73ad00022619.cpp:16:5: error: expected unqualified-id be fore ‘if’    16 |     if (!isalpha(prefix[0]) || suffix != "txt" && suffix != "exe" && suf fix != "dll") {       |     ^~ e363840a-3dc8-437e-a552-73ad00022619.cpp:20:5: error: expected unqualified-id be fore ‘return’    20 |     return "Yes";       |     ^~~~~~ e363840a-3dc8-437e-a552-73ad00022619.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from e363840a-3dc8-437e-a552-73ad00022619.cpp:23: e363840a-3dc8-437e-a552-73ad00022619.cpp: In function ‘int main()’: e363840a-3dc8-437e-a552-73ad00022619.cpp:25:13: error: ‘file_name_check’ was not  declared in this scope    25 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,23 +1,24 @@
-int digitCount = 0;
-    int dotCount = 0;
-    int dotIndex = -1;
-    for (int i = 0; i < file_name.length(); i++) {
-        if (file_name[i] >= '0' && file_name[i] <= '9') {
-            digitCount++;
-        } else if (file_name[i] == '.') {
-            dotCount++;
-            dotIndex = i;
+#include <string>
+
+std::string check_file_validity(std::string file_name) {
+    int digit_count = 0;
+    for (char c : file_name) {
+        if (isdigit(c)) {
+            digit_count++;
         }
     }
-    
-    if (digitCount > 3 || dotCount != 1 || dotIndex == 0 || dotIndex == file_name.length() - 1) {
+
+    size_t dot_pos = file_name.find(".");
+    if (digit_count > 3 || dot_pos == std::string::npos || dot_pos == 0 || dot_pos == file_name.size() - 1) {
         return "No";
     }
-    
-    string extension = file_name.substr(dotIndex + 1);
-    if (extension != "txt" && extension != "exe" && extension != "dll") {
+
+    std::string prefix = file_name.substr(0, dot_pos);
+    std::string suffix = file_name.substr(dot_pos + 1);
+
+    if (!isalpha(prefix[0]) || suffix != "txt" && suffix != "exe" && suffix != "dll") {
         return "No";
     }
-    
+
     return "Yes";
 }
