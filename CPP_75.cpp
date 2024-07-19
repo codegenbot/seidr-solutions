fab28ee0ec7d5c6d3eb7cd0f21028e1e62141f36
beep boop <githubbot@vadim.me>
2024-07-19 09:13:26 +0200

    Solution 25 of CPP/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/15izmf0e Prompt:     2 |     for (int i = 2; i <= a / 3; ++i) {       |     ^~~ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 2; i <= a / 3; ++i) {       |                     ^ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:2:33: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 2; i <= a / 3; ++i) {       |                                 ^~ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return false;       |     ^~~~~~ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:15: 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp: In function ‘int main()’: 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:17:13: error: ‘is_multiply_prime’ was n ot declared in this scope    17 |     assert (is_multiply_prime(11 * 13 * 7) == true);       |             ^~~~~~~~~~~~~~~~~

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -1,9 +1,7 @@
 bool is_prime(int num) {
     if (num < 2) return false;
     for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) {
-            return false;
-        }
+        if (num % i == 0) return false;
     }
     return true;
 }
@@ -11,9 +9,7 @@ bool is_prime(int num) {
 bool is_prime(int num) {
     if (num < 2) return false;
     for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) {
-            return false;
-        }
+        if (num % i == 0) return false;
     }
     return true;
 }
