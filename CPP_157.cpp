c520f7446727b8caa5b9b022f9be3db140e0e66e
beep boop <githubbot@vadim.me>
2024-07-20 01:21:28 +0200

    Solution 16 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/nxohli3d Prompt: 97b5b5d9-c5aa-4d25-a9c8-6ee2a75a8f22.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c ==  a * a) {       | ^~ 97b5b5d9-c5aa-4d25-a9c8-6ee2a75a8f22.cpp:3:7: error: expected unqualified-id bef ore ‘else’     3 |     } else {       |       ^~~~ 97b5b5d9-c5aa-4d25-a9c8-6ee2a75a8f22.cpp:6:1: error: expected declaration before  ‘}’ token     6 | }       | ^ In file included from 97b5b5d9-c5aa-4d25-a9c8-6ee2a75a8f22.cpp:8: 97b5b5d9-c5aa-4d25-a9c8-6ee2a75a8f22.cpp: In function ‘int main()’: 97b5b5d9-c5aa-4d25-a9c8-6ee2a75a8f22.cpp:10:13: error: ‘right_angle_triangle’ wa s not declared in this scope    10 |     assert (right_angle_triangle(2, 2, 10) == false);       |             ^~~~~~~~~~~~~~~~~~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,7 +1,12 @@
-if (pow(a, 2) + pow(b, 2) == pow(c, 2) ||
-        pow(b, 2) + pow(c, 2) == pow(a, 2) ||
-        pow(c, 2) + pow(a, 2) == pow(b, 2))
-        return true;
-    else
-        return false;
+#include <assert.h>
+
+bool right_angle_triangle(int a, int b, int c) {
+    return a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a;
+}
+
+int main() {
+    assert (right_angle_triangle(3, 4, 5) == true);
+    assert (right_angle_triangle(2, 2, 2) == false);
+
+    return 0;
 }
