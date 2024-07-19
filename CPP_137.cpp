c855421406c3b85c50fe5e6f0620df6c8a89de10
beep boop <githubbot@vadim.me>
2024-07-19 22:24:39 +0200

    Solution 72 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8hrrqsp9 Prompt: e");       |             ^~~~~ 8da8aef5-400a-4008-a88c-dd4afcc954e5.cpp:23:29: error: ‘string’ was not declared  in this scope    23 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 8da8aef5-400a-4008-a88c-dd4afcc954e5.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 8da8aef5-400a-4008-a88c-dd4afcc954e5.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 8da8aef5-400a-4008-a88c-dd4afcc954e5.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 8da8aef5-400a-4008-a88c-dd4afcc954e5.cpp: In function ‘std::any compare_one(std: :any, std::any)’: 8da8aef5-400a-4008-a88c-dd4afcc954e5.cpp:19:1: warning: control reaches end of n on-void function [-Wreturn-type]    19 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,18 +1,22 @@
 #include <any>
+#include <boost/any.hpp>
 #include <string>
-#include <sstream>
 #include <cassert>
 
-std::any compare_one(std::any a, std::any b) {
+boost::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         return std::any_cast<int>(a) > std::any_cast<int>(b) ? a : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         return std::any_cast<float>(a) > std::any_cast<float>(b) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        float valA = stof(std::any_cast<std::string>(std::any_cast<std::string>(a)));
-        float valB = stof(std::any_cast<std::string>(std::any_cast<std::string>(b)));
-        return valA > valB ? a : (valA < valB ? b : std::string("None"));
-    } else {
-        return std::string("None"); // Default case if types are not handled correctly
+        float valA = std::stof(std::any_cast<std::string>(a));
+        float valB = std::stof(std::any_cast<std::string>(b));
+        return valA > valB ? a : (valA < valB ? b : "None");
+    } else if ((a.type() == typeid(int) && b.type() == typeid(std::string)) || (a.type() == typeid(std::string) && b.type() == typeid(int))) {
+        float valA = a.type() == typeid(int) ? std::any_cast<int>(a) : std::stof(std::any_cast<std::string>(a));
+        float valB = b.type() == typeid(int) ? std::any_cast<int>(b) : std::stof(std::any_cast<std::string>(b));
+        return valA > valB ? a : (valA < valB ? b : "None");
     }
+    assert(false);
+    return {};
 }
