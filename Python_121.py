d63056d78d271e3fea82db9a9f2c4e04c2f6d0b6
beep boop <githubbot@vadim.me>
2024-07-19 17:18:32 +0200

    Solution 74 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ac794a4-b7d9-49b3-9936-f66f9f2d7 167.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,6 +1,2 @@
-def solution(input_list):
-    return all(i % 2 == 0 for i in input_list)
-
-
-input_list = [4, 2, 6]
-print(solution(input_list))
+    def solution(input_list):
+        return all(i % 2 == 0 for i in input_list)
