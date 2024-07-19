68444af629f5f3c47f324a6ab5c292dd1e9460f0
beep boop <githubbot@vadim.me>
2024-07-19 06:50:48 +0200

    Solution 96 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/em7phmrj Prompt:       |                    ^~~~~ 559ccaac-88b5-468c-91f1-80d7dc036642.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 559ccaac-88b5-468c-91f1-80d7dc036642.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 559ccaac-88b5-468c-91f1-80d7dc036642.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 559ccaac-88b5-468c-91f1-80d7dc036642.cpp:11: 559ccaac-88b5-468c-91f1-80d7dc036642.cpp: In function ‘int main()’: 559ccaac-88b5-468c-91f1-80d7dc036642.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 559ccaac-88b5-468c-91f1-80d7dc036642.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,8 +1,14 @@
 #include <vector>
 #include <cassert>
 
-std::vector<float> get_positive(std::vector<float> l){
-    std::vector<float> result;
+vector<float> get_positive(vector<float> l);
+
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
@@ -11,10 +17,7 @@ std::vector<float> get_positive(std::vector<float> l){
     return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b){
-    return a == b;
-}
-
-int main(){
+int main() {
     assert(issame(get_positive({}), {}));
+    return 0;
 }
