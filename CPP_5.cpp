83609b5529ad8ec2dca036f71b11ff3bcc9161a8
beep boop <githubbot@vadim.me>
2024-07-19 17:01:35 +0200

    Solution 6 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/rs7wf4nu Prompt:       |                    ^~~ 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp:12: 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp: In function ‘int main()’: 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 5e910ee1-f680-446e-9d0b-fccadea98a73.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,14 +1,25 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i = 0; i < a.size(); ++i){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
-int main() {
-    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
-    
-    return 0;
+std::vector<int> intersperse(std::vector<int> numbers, int delimeter){
+    std::vector<int> result;
+    for(int i = 0; i < numbers.size(); ++i){
+        if(i > 0){
+            result.push_back(delimeter);
+        }
+        result.push_back(numbers[i]);
+    }
+    return result;
 }
