7dad168a66bd4cbcb55bd5c113ee9c9b97aa435a
beep boop <githubbot@vadim.me>
2024-07-19 06:16:50 +0200

    Solution 68 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4ibd5jik Prompt: Program did not receive expected input

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,7 @@
-import sys
-
 def complete_code(string_input, integer_input):
     return string_input[:integer_input]
 
-string_input = sys.stdin.readline().rstrip()
-integer_input = int(sys.stdin.readline().rstrip())
+string_input = input().strip()
+integer_input = int(input().strip())
 output = complete_code(string_input, integer_input)
 print(output)
