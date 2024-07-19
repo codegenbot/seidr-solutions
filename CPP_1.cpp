dcabe4f52f25ff05f724d4c7cca3c2378eee8d4a
beep boop <githubbot@vadim.me>
2024-07-19 22:39:26 +0200

    Solution 89 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/imdtc467 Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 56047741-dc27-4a42-8a5b-bcc095419261.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 56047741-dc27-4a42-8a5b-bcc095419261.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 56047741-dc27-4a42-8a5b-bcc095419261.cpp:23: 56047741-dc27-4a42-8a5b-bcc095419261.cpp: In function ‘int main()’: 56047741-dc27-4a42-8a5b-bcc095419261.cpp:33:20: error: ‘separate_paren_groups’ w as not declared in this scope    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 56047741-dc27-4a42-8a5b-bcc095419261.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -10,7 +10,7 @@ bool issame(const vector<string> &a, const vector<string> &b) {
     return true;
 }
 
-vector<string> separate_paren_groups(const string &paren_string) {
+vector<string> separate_paren_groups(const string& paren_string) {
     vector<string> groups;
     int count = 0;
     string current_group;
@@ -34,8 +34,9 @@ vector<string> separate_paren_groups(const string &paren_string) {
 }
 
 int main() {
-    assert(issame(separate_paren_groups("((a)(b))"), {"(a)", "(b)"}));
-    assert(issame(separate_paren_groups("((x)(y)(z))"), {"(x)", "(y)", "(z)"}));
-    assert(issame(separate_paren_groups("(a)(b)"), {"(a)(b)"}));
+    string input = "((group1)(group2)(group3))";
+    vector<string> expected_output = {"group1", "group2", "group3"};
+    vector<string> output = separate_paren_groups(input);
+    assert(issame(output, expected_output));
     return 0;
 }
