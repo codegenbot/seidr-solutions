b010d947d8d0fb78d3c16032e95863b44362efaa
beep boop <githubbot@vadim.me>
2024-07-19 21:58:43 +0200

    Solution 52 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mj2jfwr8 Prompt:       |             ^~~~~~ f25ec07a-c133-418a-bc04-f677e5de29ec.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f25ec07a-c133-418a-bc04-f677e5de29ec.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ f25ec07a-c133-418a-bc04-f677e5de29ec.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from f25ec07a-c133-418a-bc04-f677e5de29ec.cpp:33: f25ec07a-c133-418a-bc04-f677e5de29ec.cpp: In function ‘int main()’: f25ec07a-c133-418a-bc04-f677e5de29ec.cpp:43:20: error: ‘unique_digits’ was not d eclared in this scope    43 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ f25ec07a-c133-418a-bc04-f677e5de29ec.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -2,15 +2,17 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b);
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
-std::vector<int> unique_digits(std::vector<int> x) {
-    std::vector<int> result;
+vector<int> unique_digits(vector<int> x){
+    vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
         while (temp > 0) {
-            if (temp % 10 % 2 == 0) {
+            if (temp % 2 == 0) {
                 hasEvenDigit = true;
                 break;
             }
@@ -20,12 +22,8 @@ std::vector<int> unique_digits(std::vector<int> x) {
             result.push_back(num);
         }
     }
-    std::sort(result.begin(), result.end());
+    sort(result.begin(), result.end());
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
 }
 
 int main() {
