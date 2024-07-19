db64c4fee0d46f8757e33cea5aec23a5190c1298
beep boop <githubbot@vadim.me>
2024-07-19 20:21:44 +0200

    Solution 13 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ptyt07ic Prompt:                  from e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main(){       |     ^~~~ In file included from e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp:27: e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp: In function ‘int main()’: e1495264-2db7-4d9b-8e16-4c5f6df69a00.cpp:37:20: error: ‘sorted_list_sum’ was not  declared in this scope    37 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -24,7 +24,7 @@ vector<string> sorted_list_sum(vector<string> lst){
     return lst;
 }
 
-int main() {
+int main(){
     assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     // Add more test cases here
     return 0;
