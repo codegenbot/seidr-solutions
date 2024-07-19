7c97565a1c61a101d117235b00b4a7b317e84ffe
beep boop <githubbot@vadim.me>
2024-07-19 21:22:32 +0200

    Solution 0 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vxzr422a Prompt: Complete the following code given the task description and function signature.

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,12 +1,3 @@
-#include <string>
-#include <cstring>
-#include <cassert>
-
-int strlenFunc(std::string str){
+int strlen(string str){
     return str.length();
-}
-
-int main(){
-    assert (strlenFunc("asdasnakj") == 12);
-    return 0;
 }
