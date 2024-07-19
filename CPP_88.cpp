1ea66bacaaaee3099135f8562a2ea6868f7bb488
beep boop <githubbot@vadim.me>
2024-07-19 03:19:48 +0200

    Solution 42 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/p0z2phda Prompt:       |             ^~~~~~ 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp:27: 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp: In function ‘int main()’: 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp:37:20: error: ‘sort_array’ was not decl ared in this scope    37 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 015a8ee9-e5c5-410b-942f-627799d6bea1.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 vector<int> sort_array(vector<int> array) {
-    if (array.empty()) return array;
-
+    if(array.empty()) return array;
+    
     int sum = array.front() + array.back();
-    if (sum % 2 == 0) {
+    if(sum % 2 == 0) {
         sort(array.begin(), array.end(), greater<int>());
     } else {
         sort(array.begin(), array.end());
     }
-
+    
     return array;
 }
 
@@ -21,6 +21,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
-
+    
     return 0;
 }
