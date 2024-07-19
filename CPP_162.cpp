577744e6e268b8e52ef61fb643734e4955c509d9
beep boop <githubbot@vadim.me>
2024-07-20 01:44:54 +0200

    Solution 60 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kywbzhfz Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp:2: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp:9:14: warning: ‘int MD5_Final(unsigned char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]     9 |     MD5_Final(result, &md5ctx);       |     ~~~~~~~~~^~~~~~~~~~~~~~~~~ In file included from 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccBLpqzm.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp:(.text+0x38): undefined reference to `M D5_Init' /usr/bin/ld: 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp:(.text+0x71): undefined re ference to `MD5_Update' /usr/bin/ld: 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp:(.text+0x87): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,16 +1,1 @@
-#include <string>
-#include <openssl/md5.h>
-
-std::string string_to_md5(const std::string& input) {
-    unsigned char result[16];
-    MD5_CTX md5ctx;
-    MD5_Init(&md5ctx);
-    MD5_Update(&md5ctx, input.c_str(), input.size());
-    MD5_Final(result, &md5ctx);
-
-    char output[33];
-    for (int i = 0; i < 16; i++) {
-        sprintf(output + i*2, "%02x", result[i]);
-    }
-    return std::string(output);
-}
+g++ -o 13e09792-bde9-4d3d-95d4-0f794de875e6.cpp -lcrypto -lcrypto-md5 -Wno-deprecated-declarations
