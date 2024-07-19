2efddefb3d77b0ed4bd30101ad19cb18cd18763f
beep boop <githubbot@vadim.me>
2024-07-19 18:53:35 +0200

    Solution 51 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/m87khfmf Prompt: 08a29231-cc63-4cfd-bc7e-7a898d667130.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 08a29231-cc63-4cfd-bc7e-7a898d667130.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 08a29231-cc63-4cfd-bc7e-7a898d667130.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 08a29231-cc63-4cfd-bc7e-7a898d667130.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 08a29231-cc63-4cfd-bc7e-7a898d667130.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -6,14 +6,14 @@
 float triangle_area(float a, float b, float c) {
     if (a + b > c && a + c > b && b + c > a) {
         float s = (a + b + c) / 2;
-        return sqrt(s * (s - a) * (s - b) * (s - c));
+        return std::round((std::sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
     } else {
         return -1.0f;
     }
 }
 
 int main() { 
-    assert(abs(triangle_area(2, 2, 10) - 2.23607) < 0.01);
+    assert(std::abs(triangle_area(2, 2, 10) +1)<0.01);
     float a, b, c;
     std::cout << "Enter side lengths a, b, and c of the triangle: ";
     std::cin >> a >> b >> c;
