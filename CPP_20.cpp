b0bb892257f529d5009eeb828587bc0e1079ec29
beep boop <githubbot@vadim.me>
2024-07-19 14:39:54 +0200

    Solution 48 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/una2gex6 Prompt:     4 | 3ded507e-cf3b-4af5-81ec-1f31c7803878.cpp:29:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    29 | int main(){       |         ^~ 3ded507e-cf3b-4af5-81ec-1f31c7803878.cpp:29:9: note: remove parentheses to defau lt-initialize a variable    29 | int main(){       |         ^~       |         -- 3ded507e-cf3b-4af5-81ec-1f31c7803878.cpp:29:9: note: or replace parentheses with  braces to value-initialize a variable 3ded507e-cf3b-4af5-81ec-1f31c7803878.cpp:29:11: error: a function-definition is not allowed here before ‘{’ token    29 | int main(){       |           ^ 3ded507e-cf3b-4af5-81ec-1f31c7803878.cpp:31:2: error: expected ‘}’ at end of inp ut    31 | }       |  ^ 3ded507e-cf3b-4af5-81ec-1f31c7803878.cpp:14:12: note: to match this ‘{’    14 | int main() {       |            ^

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -11,7 +11,7 @@ bool issame(const std::vector<double>& v1, const std::vector<double>& v2) {
     return v1 == v2;
 }
 
-int main {
+int main() {
     assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1});
     
     return 0;
