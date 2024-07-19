869212b8bc0b8d7ed20c5fc76b71c652b390a02e
beep boop <githubbot@vadim.me>
2024-07-19 22:20:55 +0200

    Solution 3 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s07geol4 Prompt:     1 | #include <string>   +++ |+#include <ostream>     2 | 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp: At global scope: 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp:10:1: error: ‘string’ does not name a t ype; did you mean ‘stdin’?    10 | string solve(int N) {       | ^~~~~~       | stdin 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp:3:5: note: ‘int main()’ previously defi ned here     3 | int main() {       |     ^~~~ In file included from 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp:20: 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp: In function ‘int main()’: 81fa0b5e-d9f9-4348-8afb-1d0bd678b65f.cpp:22:13: error: ‘solve’ was not declared in this scope    22 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,11 +1,19 @@
-using namespace std;
+#include <iostream>
+#include <string>
 
-string solve(int N) {
+int main() {
+    int N;
+    std::cout << "Enter a number: ";
+    std::cin >> N;
+    std::cout << "Sum of bits is: " << solve(N) << std::endl;
+}
+
+std::string solve(int N) {
     int sum = 0;
     while (N > 0) {
         int remainder = N % 2;
         sum += remainder;
         N /= 2;
     }
-    return to_string(sum);
+    return std::to_string(sum);
 }
