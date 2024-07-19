76246368657cc4919ff7c7fc3f818951a94308ec
beep boop <githubbot@vadim.me>
2024-07-19 18:13:00 +0200

    Solution 4 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xrtgbsd6 Prompt:    16 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ deec1b61-8dc2-405d-b7e8-45b3cd538891.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ deec1b61-8dc2-405d-b7e8-45b3cd538891.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from deec1b61-8dc2-405d-b7e8-45b3cd538891.cpp:15: deec1b61-8dc2-405d-b7e8-45b3cd538891.cpp: In function ‘int main()’: deec1b61-8dc2-405d-b7e8-45b3cd538891.cpp:25:20: error: ‘sort_even’ was not decla red in this scope    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ deec1b61-8dc2-405d-b7e8-45b3cd538891.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,8 +1,12 @@
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-bool operator==(const vector<float>& a, const vector<float>& b) {
-    return a == b;
+bool issame(vector<float> a, vector<float> b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(abs(a[i] - b[i]) > 1e-5) return false;
+    }
+    return true;
 }
 
 vector<float> sort_even(vector<float> l){
@@ -11,7 +15,7 @@ vector<float> sort_even(vector<float> l){
         if(i % 2 == 0){
             vector<float> even_elements(l.begin() + i, l.begin() + i+1);
             sort(even_elements.begin(), even_elements.end());
-            l_prime.push_back(*max_element(even_elements.begin(), even_evenlements.end()));
+            l_prime.push_back(even_elements[0]);
         } else {
             l_prime.push_back(l[i]);
         }
