de636e4ba3247515caa19446aa5e640de61abc91
beep boop <githubbot@vadim.me>
2024-07-19 18:17:47 +0200

    Solution 18 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2x7rb0pb Prompt:       |                      ^~~ 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp:1:26: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string,string> dict){       |                          ^~~~~~ 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp:1:33: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string,string> dict){       |                                 ^~~~~~ 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp:1:41: error: ‘dict’ was not declared in  this scope     1 | bool check_dict_case(map<string,string> dict){       |                                         ^~~~ 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp:1:45: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool check_dict_case(map<string,string> dict){       |                                             ^ In file included from 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp:19: 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp: In function ‘int main()’: 83ca5d7a-dc36-438d-8af6-37f31f700f55.cpp:21:28: error: ‘check_dict_case’ cannot be used as a function    21 |     assert (check_dict_case({}) == false);       |             ~~~~~~~~~~~~~~~^~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -5,7 +5,7 @@ bool check_dict_case(map<string,string> dict){
     bool all_lower = true;
 
     for(auto it = dict.begin(); it != dict.end(); ++it) {
-        if(!all_lower && !all_upper)
+        if(!all_lower && !allUpper)
             return false; // map contains both upper and lower case keys
 
         string key = it->first;
@@ -15,5 +15,5 @@ bool check_dict_case(map<string,string> dict){
             all_upper = false;
     }
 
-    return all_lower || all_upper;
+    return all_lower || allUpper;
 }
