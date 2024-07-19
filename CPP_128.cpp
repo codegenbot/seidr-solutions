037e11042f19248e186d302fee8b03363be15e5c
beep boop <githubbot@vadim.me>
2024-07-19 23:31:17 +0200

    Solution 4 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5l2jya3t Prompt: 71495d88-86dd-4098-b4be-be01a196aa4e.cpp: In function ‘int main()’: 71495d88-86dd-4098-b4be-be01a196aa4e.cpp:24:5: error: ‘assert’ was not declared in this scope    24 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |     ^~~~~~ 71495d88-86dd-4098-b4be-be01a196aa4e.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | 71495d88-86dd-4098-b4be-be01a196aa4e.cpp: At global scope: 71495d88-86dd-4098-b4be-be01a196aa4e.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 71495d88-86dd-4098-b4be-be01a196aa4e.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -19,9 +19,10 @@ int prod_signs(std::vector<int> arr) {
     }
 
     return product * sum;
+
 }
 
 int main() {
-    assert (prod_signs({-1, 1, 1, 0}) == 0);
+    assert(prod_signs({-1, 1, 1, 0}) == 0);
     return 0;
 }
