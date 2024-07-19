4ade19bfe53bb963dba478358adbb461a75a31d5
beep boop <githubbot@vadim.me>
2024-07-19 20:22:15 +0200

    Solution 6 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lx88rgdf Prompt: 26212070-f02b-4832-97d1-bdc209466077.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst){       |                                 ^~~~~~ 26212070-f02b-4832-97d1-bdc209466077.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst){       |                                        ^~~~~ In file included from 26212070-f02b-4832-97d1-bdc209466077.cpp:11: 26212070-f02b-4832-97d1-bdc209466077.cpp: In function ‘int main()’: 26212070-f02b-4832-97d1-bdc209466077.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 26212070-f02b-4832-97d1-bdc209466077.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 26212070-f02b-4832-97d1-bdc209466077.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,17 +1,12 @@
 #include <vector>
+#include <cmath>
 
 long long double_the_difference(std::vector<float> lst){
     long long sum = 0;
-    for (float num : lst) {
-        if (num > 0 && fmod(num, 1) == 0 && ((int)num) % 2 != 0) {
+    for(auto num : lst){
+        if(num > 0 && num == (int)num && (int)num % 2 != 0){
             sum += (long long)pow(num, 2);
         }
     }
     return sum;
-}
-
-int main() {
-    std::vector<float> lst = {1.5, 2.0, 3.3, -1.0, 5.0};
-    long long result = double_the_difference(lst);
-    return 0;
 }
