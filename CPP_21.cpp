dce18388ebf8d0ed088169819e842eee3dd5ff29
beep boop <githubbot@vadim.me>
2024-07-19 14:14:11 +0200

    Solution 2 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/81l0jz2g Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a3f972a4-2a13-49e4-b372-0f408f885ef1.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a3f972a4-2a13-49e4-b372-0f408f885ef1.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a3f972a4-2a13-49e4-b372-0f408f885ef1.cpp:13: a3f972a4-2a13-49e4-b372-0f408f885ef1.cpp: In function ‘int main()’: a3f972a4-2a13-49e4-b372-0f408f885ef1.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ a3f972a4-2a13-49e4-b372-0f408f885ef1.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,8 +1,12 @@
-vector<bool> rescale_numbers_to_unit(vector<float> numbers) {
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+vector<float> rescale_to_unit(vector<float> numbers) {
     float min_num = *min_element(numbers.begin(), numbers.end());
     float max_num = *max_element(numbers.begin(), numbers.end());
     
-    vector<bool> rescaled_numbers;
+    vector<float> rescaled_numbers;
     for (float num : numbers) {
         float rescaled_num = (num - min_num) / (max_num - min_num);
         rescaled_numbers.push_back(rescaled_num);
