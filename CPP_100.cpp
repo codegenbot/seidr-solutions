c5276d018ad64fb73c468941de5ac917c50c728d
beep boop <githubbot@vadim.me>
2024-07-20 00:44:18 +0200

    Solution 24 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/t375mxig Prompt:       |                    ^~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:14: 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp: In function ‘int main()’: 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:24:20: error: ‘make_a_pile’ was not dec lared in this scope    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -4,20 +4,17 @@ bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
 
 vector<int> make_a_pile(int n) {
     vector<int> stones;
     stones.push_back(n);
-    
     for (int i = 2; i <= n; ++i) {
         if (n % 2 == 0) {
             n += 1;
@@ -26,6 +23,5 @@ vector<int> make_a_pile(int n) {
         }
         stones.push_back(n);
     }
-    
     return stones;
 }
