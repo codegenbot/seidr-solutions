8c9e4e10d7bdfeae88ae7e4dabcf9349435f8bfc
beep boop <githubbot@vadim.me>
2024-07-19 17:25:47 +0200

    Solution 96 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10f28ba5-a50c-43e1-9a6a-7f4d98150 ec4.py", line 16, in <module>     check(solution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10f28ba5-a50c-43e1-9a6a-7f4d98150 ec4.py", line 11, in check     assert solution([3, 13, 2, 9]) == 3            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_121.py
+++ Python_121.py
@@ -2,6 +2,6 @@ def solution(input_list):
     if not input_list:        
         return None
     elif all(isinstance(x, int) and str(x).isdigit() for x in input_list):        
-        return int(''.join(map(lambda x: str(x) if isinstance(x, int) else '', input_list))))    
+        return sum(map(int, input_list))    
     else:        
         raise ValueError("Input list contains non-integer values")
