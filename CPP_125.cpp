3a14c6d5093dae4f0a9a7280e8e975d025e9c583
beep boop <githubbot@vadim.me>
2024-07-19 20:17:18 +0200

    Solution 80 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/svlp1004 Prompt:       |                                     ^~~~~~ eaacfad9-bbde-4a4b-b3be-ad45385a099c.cpp:28:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from eaacfad9-bbde-4a4b-b3be-ad45385a099c.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from eaacfad9-bbde-4a4b-b3be-ad45385a099c.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ eaacfad9-bbde-4a4b-b3be-ad45385a099c.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ eaacfad9-bbde-4a4b-b3be-ad45385a099c.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ eaacfad9-bbde-4a4b-b3be-ad45385a099c.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,25 +1,26 @@
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+bool issame(const std::vector<std::string> &a, const std::vector<std::string> &b) {
+    return a == b;
 }
 
 std::vector<std::string> split_words(const std::string& str) {
     std::vector<std::string> words;
-
+    
     size_t start = 0, end = 0;
     while ((start = str.find_first_not_of(' ', end)) != std::string::npos) {
         end = str.find(' ', start);
         words.push_back(str.substr(start, end - start));
     }
-
+    
     return words;
 }
 
 int main() {
-    assert(issame(split_words(""), {"0"}));
+    assert(issame(split_words(""), {"0"})); 
     return 0;
 }
