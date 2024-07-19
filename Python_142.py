1fd47db1189bd3cf4df637a03129b427e4863013
beep boop <githubbot@vadim.me>
2024-07-19 17:24:35 +0200

    Solution 36 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/xmpasg0z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04a278d0-a485-46be-a257-5c63fa3a9 867.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04a278d0-a485-46be-a257-5c63fa3a9 867.py", line 13, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,8 +1,8 @@
 def sum_squares(lst):
     total_sum = 0
-    for num in lst:
-        if isinstance(num, int) and (num % 3 == 0 and not num % 4 or not num % 3 and num % 4 == 0):
-            total_sum += num ** 2
-        elif isinstance(num, float) and (num % 3 == 0 and not num % 4 or not num % 3 and num % 4 == 0):
-            total_sum += num ** 3
+    for i in range(len(lst)):
+        if i % 3 == 0 and i % 4 != 0:
+            total_sum += lst[i] ** 2
+        elif i % 4 == 0 and i % 3 != 0:
+            total_sum += lst[i] ** 3
     return total_sum
