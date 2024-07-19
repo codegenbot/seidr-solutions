1fa48dea0f7c6cf55a2d5bdadf2b47418be499c5
beep boop <githubbot@vadim.me>
2024-07-19 21:44:25 +0200

    Solution 17 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n2ai249y Prompt: p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::vector<std::__cxx11::basic_string<char, std::char_trait s<char>, std::allocator<char> >, std::allocator<std::__cxx11::basic_string<char,  std::char_traits<char>, std::allocator<char> > > >&}; _Tp = std::vector<std::__ cxx11::basic_string<char> >; _Alloc = std::allocator<std::vector<std::__cxx11::b asic_string<char> > >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::vec tor<std::__cxx11::basic_string<char> > >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::vector<std::__cxx11 ::basic_string<char> >; _Alloc = std::allocator<std::vector<std::__cxx11::basic_ string<char> > >; std::vector<_Tp, _Alloc>::value_type = std::vector<std::__cxx1 1::basic_string<char> >]’ 3670b7ef-1c26-4a3f-9f5f-5580a8c41c28.cpp:20:29:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <bits/stdc++.h>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
@@ -14,20 +15,15 @@ std::vector<std::vector<std::string>> filter_by_prefix(std::vector<std::vector<s
         if (s[0].find(prefix) == 0) {
             std::vector<std::string> temp = {prefix};
             for(int i = prefix.size(); i < s[0].size(); i++) {
-                temp.push_back(s[0].substr(i, 1));
+                temp.push_back(std::string(1, s[0][i]));
             }
-            std::string finalString;
-            for(const auto& s : temp) {
-                finalString += s + " ";
-            }
-            result.push_back({finalString});
+            if(temp.size() > 1) result.push_back(temp); 
         }
     }
     return result;
 }
 
 int main() {
-    { 
     assert(issame(filter_by_prefix({{"xxx"}, {"asd"}, {"xxy"}, {"john doe"}, {"xxxxAAA"}, {"xxx"}}, "xxx") , {{"xxx"}, {"xxxAAA"}, {"xxx"}}));
     return 0;
 }
