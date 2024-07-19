64aca25a5953a527d9698b7ac883064bb5763add
beep boop <githubbot@vadim.me>
2024-07-19 21:56:14 +0200

    Solution 26 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dnk6zjdz Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 715e860a-ad1f-47c8-988e-3b5d00bf9741.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 715e860a-ad1f-47c8-988e-3b5d00bf9741.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 715e860a-ad1f-47c8-988e-3b5d00bf9741.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 715e860a-ad1f-47c8-988e-3b5d00bf9741.cpp:23: 715e860a-ad1f-47c8-988e-3b5d00bf9741.cpp: In function ‘int main()’: 715e860a-ad1f-47c8-988e-3b5d00bf9741.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,9 +1,17 @@
 #include <vector>
 #include <algorithm>
 
+bool issame(std::vector<float> a, std::vector<float> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 std::array<float, 10> sort_even(std::array<float, 10> l) {
     std::array<float, 10> result;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++) { 
         if (i % 2 == 0) {
             std::vector<float> evenVals;
             for (float val : l) {
@@ -18,4 +26,9 @@ std::array<float, 10> sort_even(std::array<float, 10> l) {
         }
     }
     return result;
+}
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 2, 4, 8, 11, 12, 5, 3, 23, -10}));
+    return 0;
 }
