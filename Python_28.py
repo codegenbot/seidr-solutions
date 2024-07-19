632945897cb27c3e83ed461bac84ca84780ad1a0
beep boop <githubbot@vadim.me>
2024-07-19 18:22:08 +0200

    Solution 43 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7a0267dg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a86da1a-43eb-463d-8e84-da069fcde 781.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,7 +1,10 @@
+def concatenate_strings():
+    strings = input("Enter strings separated by space: ").split()
+    print(concatenate(strings))
+
+
 from typing import List
 
+
 def concatenate(strings: List[str]) -> str:
-    output = ''
-    for string in strings:
-        output += string
-    return output
+    return "".join(strings)
