df4ef27590b9698c3699c65688c7bc05fa744ca5
beep boop <githubbot@vadim.me>
2024-07-19 03:52:45 +0200

    Solution 17 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sg453w14 Prompt:       |                    ^~~ eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp:18: eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp: In function ‘int main()’: eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ eeaf76ff-a535-407b-b6b6-5801ab328ab3.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -9,18 +9,27 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return true;
 }
 
-vector<int> res;
-if (n >= 0) {
-    res.push_back(3);
-    if (n > 0) {
-        res.push_back(1);
-        for (int i = 2; i <= n; ++i) {
-            if (i % 2 == 0) {
-                res.push_back(1 + i / 2);
-            } else {
-                res.push_back(res[i - 1] + res[i - 2] + res[i + 1]);
+std::vector<int> solve(int n) {
+    std::vector<int> res;
+    if (n >= 0) {
+        res.push_back(3);
+        if (n > 0) {
+            res.push_back(1);
+            for (int i = 2; i <= n; ++i) {
+                if (i % 2 == 0) {
+                    res.push_back(1 + i / 2);
+                } else {
+                    res.push_back(res[i - 1] + res[i - 2] + res[i - 1]);
+                }
             }
         }
     }
+    return res;
 }
-return res;
+
+int main(){
+    assert(issame(solve(0), {3}));
+    assert(issame(solve(1), {3, 1}));
+    assert(issame(solve(3), {3, 1, 2, 5}));
+    return 0;
+}
