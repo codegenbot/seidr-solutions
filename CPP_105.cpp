c98093cd50b5aff8903f7049f81be740d3558906
beep boop <githubbot@vadim.me>
2024-07-19 19:23:56 +0200

    Solution 6 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r4qp91p3 Prompt:       |                              ^~~~~~ 07d1c035-9073-456b-9257-193c0cc76af7.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 07d1c035-9073-456b-9257-193c0cc76af7.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 07d1c035-9073-456b-9257-193c0cc76af7.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 07d1c035-9073-456b-9257-193c0cc76af7.cpp:26: 07d1c035-9073-456b-9257-193c0cc76af7.cpp: In function ‘int main()’: 07d1c035-9073-456b-9257-193c0cc76af7.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 07d1c035-9073-456b-9257-193c0cc76af7.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,15 +1,40 @@
+#include <iostream>
 #include <vector>
-#include <string>
-#include <map>
 #include <algorithm>
-#include <cassert>
+#include <map>
+#include <string>
+
+using namespace std;
+
+vector<string> by_length(vector<int> arr){
+    vector<string> result;
+    map<int, string> numToString = {
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+    };
+
+    vector<int> sortedArr;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            sortedArr.push_back(num);
+        }
+    }
+
+    sort(sortedArr.begin(), sortedArr.end(), greater<int>());
+
+    for (int num : sortedArr) {
+        result.push_back(numToString[num]);
+    }
+
+    return result;
+}
 
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-vector<string> by_length(vector<int> arr);
-
-int main() {
+int main(){
     assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    
+    return 0;
 }
