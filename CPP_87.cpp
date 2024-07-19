63b06f6fe53fa3af300f707e803f9dca6780ebb4
beep boop <githubbot@vadim.me>
2024-07-19 03:08:04 +0200

    Solution 27 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r1dqhr1m Prompt:       |                                   ^~~~~~ e24ac594-6516-419e-9d81-7bcbb9423440.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ e24ac594-6516-419e-9d81-7bcbb9423440.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ e24ac594-6516-419e-9d81-7bcbb9423440.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from e24ac594-6516-419e-9d81-7bcbb9423440.cpp:19: e24ac594-6516-419e-9d81-7bcbb9423440.cpp: In function ‘int main()’: e24ac594-6516-419e-9d81-7bcbb9423440.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ e24ac594-6516-419e-9d81-7bcbb9423440.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    return a[0] == b[0] && a[1] == b[1];
 }
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
@@ -23,13 +23,13 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
 }
 
 int main() {
-    vector<vector<int>> test = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int target = 5;
-    vector<vector<int>> result = get_row(test, target);
-    
-    for(const auto& res : result){
-        cout << res[0] << " " << res[1] << endl;
+    vector<vector<int>> lst = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> result = get_row(lst, 5);
+    for(const auto& vec : result){
+        for(int num : vec){
+            cout << num << " ";
+        }
+        cout << endl;
     }
-
     return 0;
 }
