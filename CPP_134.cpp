8074b2d127ea48a2bf0c3527215f588fc0fa3c09
beep boop <githubbot@vadim.me>
2024-07-19 22:12:38 +0200

    Solution 12 of CPP/134, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/nqwc1ixu Prompt:       | ^~ 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp:5:21: error: ‘txt’ was not declared in this scope     5 |     char lastChar = txt.back();       |                     ^~~ 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp:6:5: error: expected unqualified-id bef ore ‘if’     6 |     if (isalpha(lastChar)) {       |     ^~ 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp:15:5: error: expected unqualified-id be fore ‘return’    15 |     return false;       |     ^~~~~~ 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp:16:1: error: expected declaration befor e ‘}’ token    16 | }       | ^ In file included from 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp:18: 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp: In function ‘int main()’: 35fb5bbe-7210-44fd-9f7e-0a51b5b5fdc7.cpp:20:13: error: ‘check_if_last_char_is_a_ letter’ was not declared in this scope    20 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,1 +1,19 @@
-return !txt.empty() && isalpha(txt.back()) && txt.find_last_of(' ') == txt.size() - 1;
+#include <string>
+
+bool check_if_last_char_is_a_letter(const std::string& txt) {
+    if (txt.empty()) {
+        return false;
+    }
+
+    char lastChar = txt.back();
+    if (std::isalpha(lastChar)) {
+        size_t lastSpacePos = txt.find_last_of(' ');
+        if (lastSpacePos == std::string::npos) {
+            return true;
+        } else {
+            return lastSpacePos > txt.find_last_not_of(' ');
+        }
+    }
+
+    return false;
+}
