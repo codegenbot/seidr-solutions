718dc53bf8931d70f4b5b6551b3bd01c0b92d42d
beep boop <githubbot@vadim.me>
2024-07-19 22:24:50 +0200

    Solution 73 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eoo9avp7 Prompt: 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    37 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp:8:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     8 | bool issame(vector<float> v1, vector<float> v2) {       |      ^~~~~~ 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp:36: 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp: In function ‘int main()’: 4b41af30-67cf-4dfa-acff-bb95b11da6cf.cpp:46:20: error: ‘get_positive’ was not de clared in this scope    46 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -5,23 +5,30 @@
 
 using namespace std;
 
-int main() {
+bool issame(vector<float> v1, vector<float> v2) {
+    return v1 == v2;
+}
+
+void mainFunction() {
     vector<float> a;
     float num;
 
     cout << "Enter the numbers: ";
     while (cin >> num) {
+        if(num < 0) {
+            break; 
+        }
         a.push_back(num);
     }
 
     vector<float> b(a);
     reverse(b.begin(), b.end());
 
-    if (a == b) {
+    if (issame(a, b)) {
         cout << "The vectors are same." << endl;
     } else {
         cout << "The vectors are not same." << endl;
     }
 
-    return 0;
+    return;
 }
