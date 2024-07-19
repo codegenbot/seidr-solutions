895fd7fc7cbd0f33ccff52594ea038f5b7d38401
beep boop <githubbot@vadim.me>
2024-07-19 12:35:20 +0200

    Solution 32 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/w0zufb7j Prompt:       |                    ^~~ 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp:9: 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp: In function ‘int main()’: 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp:19:20: error: ‘generate_integers’ was n ot declared in this scope    19 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 2c6abe94-d507-4bf3-800a-4892efe6a80a.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 
 std::vector<int> generate_integers(int a, int b) {
@@ -9,13 +8,4 @@ std::vector<int> generate_integers(int a, int b) {
         }
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(generate_integers(17, 89), {}));
-    return 0;
 }
