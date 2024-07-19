6261db951534d07328bad8a6846601623b428627
beep boop <githubbot@vadim.me>
2024-07-19 17:12:45 +0200

    Solution 44 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt: True Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24a23771-3fa9-4146-a9b9-7286c464e d1c.py", line 18, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,11 +1,4 @@
-```
-def sum_odd_digits(lst):
+def solution(lst):
     return sum(int(d) for d in "".join(map(str, lst)) if int(d) % 2 != 0)
-
-
-def check(lst):
-    solution = sum_odd_digits(lst)
-    return solution == 9
-
 
 print(check([1, 3, 5]))
