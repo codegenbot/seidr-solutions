b278260cfeca5316312bc4f01772b6b447c2c8a0
beep boop <githubbot@vadim.me>
2024-07-19 05:09:41 +0200

    Solution 80 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2elf4yq3 Prompt:    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d72e46c1-9881-4ccb-ad81-a692b91e87e7.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d72e46c1-9881-4ccb-ad81-a692b91e87e7.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d72e46c1-9881-4ccb-ad81-a692b91e87e7.cpp:14: d72e46c1-9881-4ccb-ad81-a692b91e87e7.cpp: In function ‘int main()’: d72e46c1-9881-4ccb-ad81-a692b91e87e7.cpp:24:20: error: ‘largest_smallest_integer s’ was not declared in this scope    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ d72e46c1-9881-4ccb-ad81-a692b91e87e7.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,4 +1,6 @@
-pair<int, int> largest_smallest_integer(vector<int> lst){
+#include <vector>
+
+vector<int> largest_smallest_integers(vector<int> lst){
     int max_neg = 0, min_pos = 0;
     for(int num : lst){
         if(num < 0 && num < max_neg){
@@ -8,5 +10,14 @@ pair<int, int> largest_smallest_integer(vector<int> lst){
             min_pos = num;
         }
     }
-    return make_pair(max_neg, min_pos);
+    return {max_neg, min_pos};
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
+int main(){
+    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    return 0;
 }
