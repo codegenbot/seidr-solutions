f19a2b3413964ee01d3af085f6549cdf5126efc9
beep boop <githubbot@vadim.me>
2024-07-19 14:52:44 +0200

    Solution 57 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9xs8yeft Prompt:     4 | b2178217-5577-4341-b884-c967d033d7a2.cpp:32:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    32 | int main(){       |         ^~ b2178217-5577-4341-b884-c967d033d7a2.cpp:32:9: note: remove parentheses to defau lt-initialize a variable    32 | int main(){       |         ^~       |         -- b2178217-5577-4341-b884-c967d033d7a2.cpp:32:9: note: or replace parentheses with  braces to value-initialize a variable b2178217-5577-4341-b884-c967d033d7a2.cpp:32:11: error: a function-definition is not allowed here before ‘{’ token    32 | int main(){       |           ^ b2178217-5577-4341-b884-c967d033d7a2.cpp:34:2: error: expected ‘}’ at end of inp ut    34 | }       |  ^ b2178217-5577-4341-b884-c967d033d7a2.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -17,4 +17,5 @@ int main() {
     std::vector<int> tempList2 = {5, 2, 5, 2, 3, 3, 9, 0, 123};
     tempList2 = incr_list(tempList2);
     assert(issame(tempList2, std::vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124});
+    return 0;
 }
