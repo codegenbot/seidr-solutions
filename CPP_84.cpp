4149e018d907565f1f356d374070b78d78662bef
beep boop <githubbot@vadim.me>
2024-07-19 18:51:49 +0200

    Solution 6 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ctaaepmz Prompt: 4bd10332-64f3-41d9-a873-a3e62f0cd9dd.cpp:2:5: error: expected unqualified-id bef ore ‘while’     2 |     while (N > 0) {       |     ^~~~~ 4bd10332-64f3-41d9-a873-a3e62f0cd9dd.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return bitset<8>(sum).to_string();       |     ^~~~~~ 4bd10332-64f3-41d9-a873-a3e62f0cd9dd.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 4bd10332-64f3-41d9-a873-a3e62f0cd9dd.cpp:9: 4bd10332-64f3-41d9-a873-a3e62f0cd9dd.cpp: In function ‘int main()’: 4bd10332-64f3-41d9-a873-a3e62f0cd9dd.cpp:11:13: error: ‘solve’ was not declared in this scope    11 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <string>
 #include <bitset>
+#include <cassert>
 
 std::string solve(int N) {
     int sum = 0;
-    while(N > 0){
+    while (N > 0) {
         sum += N % 2;
         N /= 2;
     }
-    return std::bitset<sizeof(int)*8>(sum).to_string();
+    return std::bitset<8>(sum).to_string();
 }
 
 int main() {
-    int N;
-    std::cin >> N;
-
-    std::cout << solve(N) << std::endl;
-
+    assert(solve(963) == "10010");
     return 0;
 }
