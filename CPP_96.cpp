46290277aea4bde286588a2f2036f1630c7e63f1
beep boop <githubbot@vadim.me>
2024-07-19 21:48:43 +0200

    Solution 16 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jamerrnh Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 38081e1f-6f32-42fe-8dad-e5735cf8520f.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 38081e1f-6f32-42fe-8dad-e5735cf8520f.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 38081e1f-6f32-42fe-8dad-e5735cf8520f.cpp:16: 38081e1f-6f32-42fe-8dad-e5735cf8520f.cpp: In function ‘int main()’: 38081e1f-6f32-42fe-8dad-e5735cf8520f.cpp:26:20: error: ‘count_up_to’ was not dec lared in this scope    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 38081e1f-6f32-42fe-8dad-e5735cf8520f.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,9 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-using namespace std;
-
 vector<int> count_up_to(int n) {
     vector<int> primes;
     for (int i = 2; i < n; ++i) {
@@ -21,11 +15,12 @@ vector<int> count_up_to(int n) {
     return primes;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    return 0;
+bool areSame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size())
+        return false;
+    for(int i=0; i<a.size(); ++i) {
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
 }
