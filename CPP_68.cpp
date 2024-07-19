7be9e19fa09c273f7012cd60e467099ba3cef89f
beep boop <githubbot@vadim.me>
2024-07-20 00:23:49 +0200

    Solution 67 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/oisngjxm Prompt:       |                    ^~~ b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp:21: b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp: In function ‘int main()’: b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ b812031b-aa48-4c58-8c39-7b1e8569fba8.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -2,13 +2,13 @@
 
 vector<int> pluck(vector<int> arr);
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
         return false;
     }
     
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
