0a5e4223c46c5d5dae8d2c6a020fc26ac51073c0
beep boop <githubbot@vadim.me>
2024-07-20 01:58:03 +0200

    Solution 53 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gtchbki1 Prompt: ac816809-774a-4ab3-ba22-8b473b601c87.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float> l;       | ^~~~~~ In file included from ac816809-774a-4ab3-ba22-8b473b601c87.cpp:3: ac816809-774a-4ab3-ba22-8b473b601c87.cpp: In function ‘int main()’: ac816809-774a-4ab3-ba22-8b473b601c87.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ ac816809-774a-4ab3-ba22-8b473b601c87.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -4,19 +4,7 @@
 
 int main() {
     vector<float> l;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    for(int i = 0; i < n; i++) {
-        float x;
-        cout << "Enter element " << (i+1) << ": ";
-        cin >> x;
-        l.push_back(x);
-    }
-
-    vector<float>::iterator it = max_element(l.begin(), l.end());
-    cout << "The maximum value is: " << *it << endl;
-
+    float max = *max_element(l.begin(),l.end());
+    cout << "Maximum value in the list is: " << max << endl;
     return 0;
 }
