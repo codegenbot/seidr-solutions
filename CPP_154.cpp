f4c36ea752e2f9a99d045c85874462eb725a761e
beep boop <githubbot@vadim.me>
2024-07-19 22:23:43 +0200

    Solution 8 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1pmlzr10 Prompt: 750cf701-1242-4bef-826f-793f6be15c15.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if (a.length() != b.length()) {       | ^~ 750cf701-1242-4bef-826f-793f6be15c15.cpp:5:5: error: ‘string’ does not name a ty pe     5 |     string temp = b + b;       |     ^~~~~~ 750cf701-1242-4bef-826f-793f6be15c15.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return (temp.find(a) != string::npos);       |     ^~~~~~ 750cf701-1242-4bef-826f-793f6be15c15.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from 750cf701-1242-4bef-826f-793f6be15c15.cpp:10: 750cf701-1242-4bef-826f-793f6be15c15.cpp: In function ‘int main()’: 750cf701-1242-4bef-826f-793f6be15c15.cpp:12:14: error: ‘cycpattern_check’ was no t declared in this scope    12 |     assert  (cycpattern_check("winemtt","tinem") == true );       |              ^~~~~~~~~~~~~~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -2,14 +2,16 @@
 #include <string>
 #include <cassert>
 
-bool cycpattern_check(const std::string& a, const std::string& b) {
+using namespace std;
+
+bool cycpattern_check(const string& a, const string& b) {
     if (a.length() != b.length()) {
         return false;
     }
-
-    std::string temp = b + b;
-
-    return (temp.find(a) != std::string::npos);
+    
+    string temp = b + b;
+    
+    return (temp.find(a) != string::npos);
 }
 
 int main() {
