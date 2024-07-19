bc03617edfd327d1df16061b47e5c4e168fc8324
beep boop <githubbot@vadim.me>
2024-07-19 18:40:15 +0200

    Solution 40 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dilniizg Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 35771dff-eb6b-4fad-974a-0dd6e72f4e02.cpp:26:42:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-int compare_strings(std::string a, std::string b) {
+int compare_strings(string a, string b) {
     if (a > b)
         return 1;
     else if (b > a)
@@ -11,19 +11,20 @@ int compare_strings(std::string a, std::string b) {
 }
 
 int main() {
-    std::string str1, str2;
-    std::cout << "Enter first string: ";
-    std::getline(std::cin, str1);
-    std::cout << "Enter second string: ";
-    std::getline(std::cin, str2);
+    string str1, str2;
+    cout << "Enter first string: ";
+    getline(cin, str1);
+    cout << "Enter second string: ";
+    getline(cin, str2);
 
     int result = compare_strings(str1, str2);
 
     if (result > 0)
-        std::cout << "Greater string is: " << str1 << std::endl;
+        cout << "Greater string is: " << str1 << endl;
     else if (result < 0)
-        std::cout << "Greater string is: " << str2 << std::endl;
+        cout << "Greater string is: " << str2 << endl;
     else
-        std::cout << "Both strings are equal" << std::endl;
+        cout << "Both strings are equal" << endl;
 
     return 0;
+}
