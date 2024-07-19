11fd76f2cbe22444367ed35ff1abe66cb668d32b
beep boop <githubbot@vadim.me>
2024-07-20 00:51:10 +0200

    Solution 12 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hxlqfw9w Prompt: Complete the following code given the task description and function signature.

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,13 +1,12 @@
-vector<string> odd_count(vector<string> lst){
-    vector<string> result;
-    for (string s : lst) {
+vector<string> result;
+    for (string num : lst) {
         int oddCount = 0;
-        for (char c : s) {
-            if ((c - '0') % 2 != 0) {
+        for (char digit : num) {
+            if ((digit - '0') % 2 != 0) {
                 oddCount++;
             }
         }
-        result.push_back("the number of odd elements " + to_string(oddCount) + "n the str" + to_string(oddCount) + "ng " + s + " of the " + to_string(oddCount) + "nput.");
+        result.push_back("the number of odd elements " + to_string(oddCount) + "n the str" + to_string(oddCount) + "ng " + num + " of the " + to_string(oddCount) + "nput.");
     }
     return result;
 }
