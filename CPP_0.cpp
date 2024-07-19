fa5d9b6589f3d5abc82f0bec282bf013444f1024
beep boop <githubbot@vadim.me>
2024-07-20 01:10:03 +0200

    Solution 30 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kginkz7r Prompt: 61435082-d553-4e36-acb8-66c3e931304e.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                ^~~~~ 61435082-d553-4e36-acb8-66c3e931304e.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                               ^ 61435082-d553-4e36-acb8-66c3e931304e.cpp: In function ‘int main()’: 61435082-d553-4e36-acb8-66c3e931304e.cpp:13:5: error: ‘vector’ was not declared in this scope    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 61435082-d553-4e36-acb8-66c3e931304e.cpp:13:12: error: expected primary-expressi on before ‘float’    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 61435082-d553-4e36-acb8-66c3e931304e.cpp:11: 61435082-d553-4e36-acb8-66c3e931304e.cpp:14:31: error: ‘has_close_elements’ cann ot be used as a function    14 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
 
-bool has_close_elements(std::vector<float> numbers, float threshold) {
-    for(int i = 0; i < numbers.size() - 1; i++) {
-        for(int j = i + 1; j < numbers.size(); j++) {
-            if(abs(numbers[i] - numbers[j]) <= threshold) {
+bool has_close_elements(std::vector<float> numbers, float threshold){
+    for(int i = 0; i < numbers.size(); i++){
+        for(int j = i+1; j < numbers.size(); j++){
+            if(abs(numbers[i] - numbers[j]) <= threshold)
                 return true;
-            }
         }
     }
     return false;
@@ -15,8 +13,6 @@ bool has_close_elements(std::vector<float> numbers, float threshold) {
 
 int main() {
     std::vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    std::vector<float> b={1.1, 2.2, 3.1, 4.1, 5.1};
-    cout << boolalpha << has_close_elements(a, 0.5) << endl;
-    cout << boolalpha << has_close_elements(b, 0.5) << endl;
+    assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false);
     return 0;
 }
