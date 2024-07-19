e6ca5aafc71baa1617d0bb726b7a8305ea81a542
beep boop <githubbot@vadim.me>
2024-07-19 03:28:24 +0200

    Solution 10 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/g6gz22nc Prompt: f6df1f6b-570b-4f84-a4c5-6dd02f756879.cpp:1:1: error: ‘map’ does not name a type     1 | map<string, int> number_map = {       | ^~~ f6df1f6b-570b-4f84-a4c5-6dd02f756879.cpp:14:1: error: ‘string’ does not name a t ype    14 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from f6df1f6b-570b-4f84-a4c5-6dd02f756879.cpp:37: f6df1f6b-570b-4f84-a4c5-6dd02f756879.cpp: In function ‘int main()’: f6df1f6b-570b-4f84-a4c5-6dd02f756879.cpp:39:13: error: ‘sort_numbers’ was not de clared in this scope    39 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,23 +1,45 @@
-map<string, int> num_map = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
-map<int, string> rev_num_map = {{0, "zero"}, {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}};
+#include <iostream>
+#include <map>
+#include <string>
+#include <cassert>
 
-vector<int> nums;
-string result;
+std::string sort_numbers(std::string numbers) {
+    std::map<std::string, int> number_map = {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9}
+    };
 
-size_t pos = 0, found;
-while ((found = numbers.find(" ", pos)) != string::npos) {
-    string num_str = numbers.substr(pos, found - pos);
-    nums.push_back(num_map[num_str]);
-    pos = found + 1;
+    std::map<int, std::string> reverse_map;
+    std::string result = "";
+    
+    std::string temp = "";
+    for (char c : numbers) {
+        if (c == ' ') {
+            reverse_map[number_map[temp]] = temp;
+            temp = "";
+        } else {
+            temp += c;
+        }
+    }
+    reverse_map[number_map[temp]] = temp;
+    
+    for (auto it = reverse_map.begin(); it != reverse_map.end(); ++it) {
+        result += it->second + " ";
+    }
+    
+    result.pop_back(); // Remove the extra space at the end
+    return result;
 }
-nums.push_back(num_map[numbers.substr(pos)]);
 
-sort(nums.begin(), nums.end());
-
-for (int num : nums) {
-    result += rev_num_map[num] + " ";
-}
-
-result.pop_back(); // Remove the extra space at the end
-return result;
+int main() {
+    assert(sort_numbers("six five four three two one zero") == "zero one two three four five six");
+    return 0;
 }
