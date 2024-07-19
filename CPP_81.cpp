34d0e0d81e53afbd317fb1b29b6195bdaff7835d
beep boop <githubbot@vadim.me>
2024-07-19 22:17:01 +0200

    Solution 5 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f6l5dx02 Prompt:       |           ^~~~~~ feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^ feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp:49:45: error: expression list treated a s compound expression in initializer [-fpermissive]    49 | bool issame(vector<string> a,vector<string>b){       |                                             ^ feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp:57:5: error: redefinition of ‘int main( )’    57 | int main(){       |     ^~~~ feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp:43:5: note: ‘int main()’ previously def ined here    43 | int main() {       |     ^~~~ In file included from feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp:48: feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp: In function ‘int main()’: feb7873c-420a-42b7-9148-fc40ffcf53d5.cpp:58:19: error: ‘issame’ cannot be used a s a function    58 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+bool issame(std::vector<std::string> vec1, std::vector<std::string> vec2) {
+    if (vec1.size() != vec2.size()) return false;
+    for (int i = 0; i < vec1.size(); i++) {
+        if (vec1[i] != vec2[i]) return false;
     }
     return true;
 }
 
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
-    std::vector<string> result;
+    std::vector<std::string> result;
     for (float grade : grades) {
         if (grade >= 4.0)
             result.push_back("A+");
@@ -42,10 +42,6 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    std::vector<float> grades = {0, 0.7};
-    for (std::string grade : numerical_letter_grade(grades)) {
-        std::cout << grade << " ";
-    }
-    std::cout << std::endl;
+    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
     return 0;
 }
