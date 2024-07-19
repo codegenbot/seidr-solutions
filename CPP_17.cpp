3b5b63c8c939c7f6fa298116e6b245347dc2fc5e
beep boop <githubbot@vadim.me>
2024-07-19 14:47:24 +0200

    Solution 94 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/e7hzo27n Prompt:    34 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 5772c3e9-8d7c-45de-a5a5-5420f7f35dc2.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 5772c3e9-8d7c-45de-a5a5-5420f7f35dc2.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 5772c3e9-8d7c-45de-a5a5-5420f7f35dc2.cpp:33: 5772c3e9-8d7c-45de-a5a5-5420f7f35dc2.cpp: In function ‘int main()’: 5772c3e9-8d7c-45de-a5a5-5420f7f35dc2.cpp:43:31: error: no match for call to ‘(st d::vector<int>) (const char [22])’    43 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~ 5772c3e9-8d7c-45de-a5a5-5420f7f35dc2.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <string>
 #include <cassert>
 
 bool is_same(std::vector<int> a, std::vector<int> b) {
@@ -27,6 +26,6 @@ std::vector<int> parse_music(std::string music_string) {
 }
 
 int main() {
-    assert(is_same(parse_music("o| .| o| .| o o| o o|"), std::vector<int>{2, 1, 2, 1, 4, 2, 4, 2}));
+    assert(is_same(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
     return 0;
 }
