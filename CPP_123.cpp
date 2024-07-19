0938f7c288c81edac8c68153af03c06edc4847b3
beep boop <githubbot@vadim.me>
2024-07-20 01:04:11 +0200

    Solution 52 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8msjkoba Prompt:       |                    ^~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:26:27: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:25: d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp: In function ‘int main()’: d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:35:20: error: ‘get_odd_collatz’ was not  declared in this scope    35 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -3,16 +3,14 @@
 #include <cassert>
 
 bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
+    if(a.size() != b.size()) {
         return false;
     }
-    
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
+    for(size_t i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
 
