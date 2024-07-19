f65fee461051f16600cd7feeac0daeb43c8ea44c
beep boop <githubbot@vadim.me>
2024-07-19 17:42:01 +0200

    Solution 60 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tlt7txr2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14c1b987-1ec0-4541-8554-54dc436dc 6d6.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/14c1b987-1ec0-4541-8554-54dc436dc 6d6.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,6 @@
 def sort_third(l: list):
-    multiples = sorted([i for i in l if i % 3 == 0])
-    n = len(multiples)
-    middle_idx = n // 2
+    multiples = [x for x in l if x % 3 == 0]
+    middle_index = len(multiples) // 2
     return [
-        (
-            i
-            if i % 3 != 0
-            else (multiples[middle_idx] if n % 2 == 0 else multiples[middle_idx])
-        )
-        for i in l
+        i if i % 3 != 0 else sorted([x for x in multiples])[middle_index] for i in l
     ]
