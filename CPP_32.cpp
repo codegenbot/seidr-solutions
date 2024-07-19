bb6f1431234c3b312ba56940afb43d496a3bd67c
beep boop <githubbot@vadim.me>
2024-07-19 17:23:58 +0200

    Solution 7 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0abw0hky Prompt: 421371c9-8d9a-4b2d-a88f-a2a8fdb25de1.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 421371c9-8d9a-4b2d-a88f-a2a8fdb25de1.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 421371c9-8d9a-4b2d-a88f-a2a8fdb25de1.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from 421371c9-8d9a-4b2d-a88f-a2a8fdb25de1.cpp:9: 421371c9-8d9a-4b2d-a88f-a2a8fdb25de1.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 421371c9-8d9a-4b2d-a88f-a2a8fdb25de1.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include <cmath>
 
-double poly(std::vector<double> xs){
+double poly(vector<double> xs){
     double a = xs[0];
     double b = xs[1];
-    double discriminant = b * b - 4 * a;
-    double x = (-b + std::sqrt(discriminant)) / (2 * a);
-    return x;
+    double c = xs[2];
+    double discriminant = b * b - 4 * a * c;
+    double x1 = (-b + sqrt(discriminant)) / (2 * a);
+    double x2 = (-b - sqrt(discriminant)) / (2 * a);
+    return x1;
 }
