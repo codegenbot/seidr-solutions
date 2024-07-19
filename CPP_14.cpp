7b89ecbf39627b62de6dcc2b8b6dc327f71ecccc
beep boop <githubbot@vadim.me>
2024-07-19 20:48:26 +0200

    Solution 20 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/s3c98xrb Prompt:       |                              ^~~~~~ 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp:11: 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp: In function ‘int main()’: 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp:21:20: error: ‘all_prefixes’ was not de clared in this scope    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 08fe103d-ffe8-47e1-87ef-bb45104f2d31.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,11 +1,16 @@
 #include <vector>
 #include <string>
-#include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(char a, char b) {
     return a == b;
 }
 
-vector<string> all_prefixes(string
+vector<string> all_prefixes(string str){
+    vector<string> result;
+    string prefix = "";
+    for (char c : str) {
+        prefix += c;
+        result.push_back(prefix);
+    }
+    return result;
+}
