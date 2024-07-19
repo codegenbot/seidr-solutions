feb0b5a2d6f645f89650e4b6e8321d4768951c53
beep boop <githubbot@vadim.me>
2024-07-19 20:53:46 +0200

    Solution 81 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pkupi6nj Prompt: 085f47e3-7a6a-4cc6-b1df-c313f50c8d52.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from 085f47e3-7a6a-4cc6-b1df-c313f50c8d52.cpp:22: 085f47e3-7a6a-4cc6-b1df-c313f50c8d52.cpp: In function ‘int main()’: 085f47e3-7a6a-4cc6-b1df-c313f50c8d52.cpp:24:13: error: ‘boost’ has not been decl ared    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 085f47e3-7a6a-4cc6-b1df-c313f50c8d52.cpp:24:29: error: ‘string’ was not declared  in this scope    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 085f47e3-7a6a-4cc6-b1df-c313f50c8d52.cpp:24:37: error: ‘compare_one’ was not dec lared in this scope    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,7 +3,7 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<int>(b) > boost::any_cast<int>(a) ? b : a;
+        return boost::any_cast<float>(b) > boost::any_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
         return boost::any_cast<float>(a) > boost::any_cast<int>(b) ? a : b;
@@ -18,6 +18,6 @@ boost::any compare_one(boost::any a, boost::any b) {
         return std::stoi(boost::any_cast<std::string>(a)) > boost::any_cast<int>(b) ? a : b;
     }
     else {
-        return boost::any("None");
+        return "None";
     }
 }
