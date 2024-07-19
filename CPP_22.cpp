f4dd6c678b67905e68ae4c3a19a7a852c3fa830a
beep boop <githubbot@vadim.me>
2024-07-19 20:54:12 +0200

    Solution 6 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/2l76vjzy Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5834545e-ca7d-45fd-a172-a51c1dc1bb67.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5834545e-ca7d-45fd-a172-a51c1dc1bb67.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5834545e-ca7d-45fd-a172-a51c1dc1bb67.cpp:11: 5834545e-ca7d-45fd-a172-a51c1dc1bb67.cpp: In function ‘int main()’: 5834545e-ca7d-45fd-a172-a51c1dc1bb67.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 5834545e-ca7d-45fd-a172-a51c1dc1bb67.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,31 +1,13 @@
-#include <iostream>
-#include <vector>
-#include <list>
-#include <boost/any.hpp>
-
-using namespace std;
-
 bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
 vector<int> filter_integers(list<any> values);
 
-int main() {
-    list<any> values = {10, "hello", 20, 30, "world", 40};
-    vector<int> result = filter_integers(values);
-    
-    for(auto val : result){
-        cout << val << " ";
-    }
-
-    return 0;
-}
-
 vector<int> filter_integers(list<any> values){
     vector<int> result;
     for(auto val : values){
-        if(auto ptr = boost::any_cast<int>(&val)){
+        if(auto ptr = any_cast<int>(&val)){
             result.push_back(*ptr);
         }
     }
