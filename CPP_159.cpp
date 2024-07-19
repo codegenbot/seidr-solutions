26e2d33209f2bf79cb429146c27f3019583eb117
beep boop <githubbot@vadim.me>
2024-07-19 03:30:02 +0200

    Solution 86 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/0s1qc97n Prompt:  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ 6817c8b4-d9cd-4c94-a1b5-c3be90e7113c.cpp:18:20: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 6817c8b4-d9cd-4c94-a1b5-c3be90e7113c.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6817c8b4-d9cd-4c94-a1b5-c3be90e7113c.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6817c8b4-d9cd-4c94-a1b5-c3be90e7113c.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 6817c8b4-d9cd-4c94-a1b5-c3be90e7113c.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,16 +1,15 @@
-#include <utility>
 #include <vector>
 #include <cassert>
 
-bool issame(std::pair<int, int> a, std::pair<int, int> b) {
-    return (a.first == b.first) && (a.second == b.second);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return (a[0] == b[0]) && (a[1] == b[1]);
 }
 
-std::pair<int, int> eat(int x, int y, int z) {
+std::vector<int> eat(int x, int y, int z) {
     return {x + y, y - z};
 }
 
 int main() {
-    assert(issame(eat(4, 5, 1), std::make_pair(4, 4)));
+    assert(issame(eat(4, 5, 1), {4, 5}));
     return 0;
 }
