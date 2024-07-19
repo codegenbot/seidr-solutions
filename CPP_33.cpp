39ae62630adbb04f08ad47b91449ce7358e91595
beep boop <githubbot@vadim.me>
2024-07-19 21:04:47 +0200

    Solution 28 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jr17mrsj Prompt:     5 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 05e31c76-7ec6-4cc1-863f-26de4d2da8bc.cpp:5:34: error: expected primary-expressio n before ‘int’     5 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 05e31c76-7ec6-4cc1-863f-26de4d2da8bc.cpp:5:39: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 05e31c76-7ec6-4cc1-863f-26de4d2da8bc.cpp:4: 05e31c76-7ec6-4cc1-863f-26de4d2da8bc.cpp: In function ‘int main()’: 05e31c76-7ec6-4cc1-863f-26de4d2da8bc.cpp:14:20: error: ‘sort_third’ was not decl ared in this scope    14 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 05e31c76-7ec6-4cc1-863f-26de4d2da8bc.cpp:14:19: error: ‘issame’ cannot be used a s a function    14 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,24 +3,16 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-vector<int> sort_third(const vector<int>& l) {
-    vector<int> res = l;
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            sort(res.begin() + i, res.begin() + i + 3);
-        }
-    }
-    return res;
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+vector<int> sort_third(vector<int> l) {
+    sort(l.begin(), l.end(), [](int a, int b) { return a % 3 == 0 && b % 3 == 0 ? a < b : false; });
+    return l;
 }
 
 int main() {
     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    cout << "Test Passed!" << endl;
     return 0;
 }
