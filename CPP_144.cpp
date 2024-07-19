deeedcb2ef46f756e4b82d8849b5b95217a93d75
beep boop <githubbot@vadim.me>
2024-07-19 20:44:23 +0200

    Solution 16 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oojp8dsm Prompt: _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ f24137cb-1339-456c-adfc-43849ff9a476.cpp:23:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ f24137cb-1339-456c-adfc-43849ff9a476.cpp:23:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,12 +1,17 @@
-#include<string>
+#include <string>
+#include <cmath>
+#include <cctype>
+
 using namespace std;
 
-bool simplify(string x,string n){
-    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
-    int b = stoi(strtok(NULL, "/"));
-    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
-    int d = stoi(strtok(NULL, "/"));
+bool simplify(string x, string n) {
+    int a = stoi(strtok((x.substr(1,x.length()-2)).c_str(),"/"));
+    int b = stoi(strtok(NULL,"/") );
+    
+    int c = stoi(strtok((n.substr(1,n.length()-2)).c_str(),"/"));
+    int d = stoi(strtok(NULL,"/") );
 
-    if (a*d == b*c) return true;
-    else return false;
+    double res = (double)a*d/(double)b*c;
+
+    return floor(res) == res;
 }
