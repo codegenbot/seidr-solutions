68016af60952fcc1b28f6a7b474c1e365b9590f8
beep boop <githubbot@vadim.me>
2024-07-19 13:01:27 +0200

    Solution 40 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6dh2sbiq Prompt:       |                                     ^~~~~~ 0e0cb6b1-747b-49aa-9b96-01fe877f8e7b.cpp:25:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 0e0cb6b1-747b-49aa-9b96-01fe877f8e7b.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 0e0cb6b1-747b-49aa-9b96-01fe877f8e7b.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 0e0cb6b1-747b-49aa-9b96-01fe877f8e7b.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0e0cb6b1-747b-49aa-9b96-01fe877f8e7b.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 0e0cb6b1-747b-49aa-9b96-01fe877f8e7b.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+bool issame(const std::vector<std::string>& a,const std::vector<std::string>& b) {
     return a == b;
 }
 
@@ -16,5 +16,7 @@ std::vector<std::string> filter_by_substring(const std::vector<std::string>& wor
     return result;
 }
 
-assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
-return 0;
+int main() {
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
+    return 0;
+}
