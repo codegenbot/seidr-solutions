edd8b7d673fefa25c89af0d65dc4cadaf1555873
beep boop <githubbot@vadim.me>
2024-07-19 20:33:35 +0200

    Solution 63 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/u0hz0tk3 Prompt:       |                    ^~~ 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp:9: 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp: In function ‘int main()’: 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 767fdf21-8b8b-4be6-81d5-195b82a72e18.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -3,14 +3,11 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
+    if(a.size() != b.size()) 
         return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
+    for(int i = 0; i < a.size(); i++) 
+        if(a[i] != b[i])
             return false;
-        }
-    }
     return true;
 }
 
@@ -22,5 +19,5 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
 
 int main() {
     assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));
-    return 0;
+    // ...
 }
