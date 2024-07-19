88854babfb6ee0ac2868e2922477268e4d8110cc
beep boop <githubbot@vadim.me>
2024-07-19 20:12:48 +0200

    Solution 4 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duz954ia Prompt: e363840a-3dc8-437e-a552-73ad00022619.cpp:14:5: error: ‘string’ does not name a t ype    14 |     string suffix = file_name.substr(dot_pos + 1);       |     ^~~~~~ e363840a-3dc8-437e-a552-73ad00022619.cpp:16:5: error: expected unqualified-id be fore ‘if’    16 |     if (!isalpha(prefix[0]) || suffix != "txt" && suffix != "exe" && suf fix != "dll") {       |     ^~ e363840a-3dc8-437e-a552-73ad00022619.cpp:20:5: error: expected unqualified-id be fore ‘return’    20 |     return "Yes";       |     ^~~~~~ e363840a-3dc8-437e-a552-73ad00022619.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from e363840a-3dc8-437e-a552-73ad00022619.cpp:23: e363840a-3dc8-437e-a552-73ad00022619.cpp: In function ‘int main()’: e363840a-3dc8-437e-a552-73ad00022619.cpp:25:13: error: ‘file_name_check’ was not  declared in this scope    25 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,25 +1,7 @@
-#include <iostream>
-#include <string>
-
-std::string validate_file_name(const std::string& file_name) {
-    int digit_count = 0;
-    for (char c : file_name) {
-        if (std::isdigit(c)) {
-            digit_count++;
-        }
-    }
-
-    size_t dot_pos = file_name.find(".");
-    if (digit_count > 3 || dot_pos == std::string::npos || dot_pos == 0 || dot_pos == file_name.size() - 1) {
-        return "No";
-    }
-
-    std::string prefix = file_name.substr(0, dot_pos);
-    std::string suffix = file_name.substr(dot_pos + 1);
-
-    if (!std::isalpha(prefix[0]) || (suffix != "txt" && suffix != "exe" && suffix != "dll")) {
-        return "No";
-    }
-
-    return "Yes";
-}
+assert(file_name_check("s.") == "No");
+assert(file_name_check("abc.txt") == "Yes");
+assert(file_name_check("CSE_141.dll") == "Yes");
+assert(file_name_check("file1.exe") == "Yes");
+assert(file_name_check("12345.") == "No");
+assert(file_name_check("1.txt") == "No");
+assert(file_name_check("test.") == "No");
