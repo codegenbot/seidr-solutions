ce6a78aed8f80708106a51a6e4f78fb01a6fb429
beep boop <githubbot@vadim.me>
2024-07-19 07:19:34 +0200

    Solution 91 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jyejj3th Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,12 +1,10 @@
-def decode_string():
-    string_to_decode = input().upper()
-
+def decode_string(input_string):
     decoded_string = ""
-    for char in string_to_decode:
+    for char in input_string:
         decoded_string += chr(ord(char) - 1)
 
     return decoded_string
 
-
-result = decode_string()
+input_string = str(input())
+result = decode_string(input_string)
 print(result)
