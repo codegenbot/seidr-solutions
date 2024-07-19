9b6deecd3e96f4bbf8fd58083cdcbafd70eeecd2
beep boop <githubbot@vadim.me>
2024-07-19 05:03:41 +0200

    Solution 49 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/vocsxz1q Prompt:    20 |     return "None";       |     ^~~~~~ ba87d3d8-7925-434c-8a44-b50406c79eea.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from ba87d3d8-7925-434c-8a44-b50406c79eea.cpp:23: ba87d3d8-7925-434c-8a44-b50406c79eea.cpp: In function ‘int main()’: ba87d3d8-7925-434c-8a44-b50406c79eea.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ ba87d3d8-7925-434c-8a44-b50406c79eea.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ ba87d3d8-7925-434c-8a44-b50406c79eea.cpp:25:37: error: ‘compare_one’ was not dec lared in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 template <typename T>
-T compare_one(const boost::any& a, const boost::any& b) {
+T compare_one(const boost::any &a, const boost::any &b) {
     if (a.type() == typeid(T) && b.type() == typeid(T)) {
         if (boost::any_cast<T>(a) > boost::any_cast<T>(b)) {
             return boost::any_cast<T>(a);
@@ -10,17 +10,5 @@ T compare_one(const boost::any& a, const boost::any& b) {
             return boost::any_cast<T>(b);
         }
     }
-    throw std::invalid_argument("Invalid types");
-}
-
-int main() {
-    boost::any a = 10;
-    boost::any b = 20;
-    
-    try {
-        int result = compare_one<int>(a, b);
-        return result;
-    } catch (const std::invalid_argument& e) {
-        return 0;
-    }
+    return T();
 }
