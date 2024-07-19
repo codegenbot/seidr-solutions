24ec5dae0a60f543b898904411f4bdd3f4fcc7d8
beep boop <githubbot@vadim.me>
2024-07-19 06:08:12 +0200

    Solution 52 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/twbnhxvv Prompt: 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp: In function ‘std::string string_to_md5 (const string&)’: 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:12:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    12 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:3: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp: At global scope: 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 58800ba2-7dc4-41ad-a924-3a7f03f9c9f6.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <openssl/md5.h>
 #include <openssl/evp.h>
 #include <cassert>
 
@@ -9,20 +8,15 @@ std::string string_to_md5(const std::string& text) {
         return "None";
     }
 
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
     unsigned char digest[EVP_MAX_MD_SIZE];
-    unsigned int md_len;
-
-    md = EVP_md5();
-    mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit(mdctx, EVP_md5());
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal_ex(mdctx, digest, &md_len);
+    EVP_DigestFinal(mdctx, digest, nullptr);
     EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
-    for (int i = 0; i < md_len; i++) {
+    for (int i = 0; i < 16; i++) {
         sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
     }
 
