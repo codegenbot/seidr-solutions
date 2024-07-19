d3fecc938cf07db17ae75f74980042a3626d28ea
beep boop <githubbot@vadim.me>
2024-07-19 23:20:24 +0200

    Solution 49 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
-#include <iomanip>
 #include <sstream>
 
 using namespace std;
 
-bool any_int(float a, float b, float c) {
-    return (a == (int)a) && (b == (int)b) && (c == (int)c);
-}
-
-int originalMain() {
+int main() {
     int a, b, c;
     cout << "Enter three numbers: ";
     cin >> a >> b >> c;
-    if(any_int(a, b, c))
-        cout << "The numbers are integers: " << to_string(a) << ", " << to_string(b) << ", " << to_string(c) << "\n";
+    
+    ostringstream ss;
+    ss << a << ", " << b << ", " << c;
+    
+    if(ss.str().size() == 0)
+        cout << "Error: Please enter valid integer values.\n";
     else
-        cout << "The numbers are not integers: " << to_string(a) << ".0, " << to_string(b) << ".0, " << to_string(c) << ".0\n";
-    return 0;
-}
-
-int main() {
-    originalMain();
+        cout << "The numbers are integers: " << ss.str() << "\n";
+    
     return 0;
 }
