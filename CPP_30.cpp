74e673e990fcb46e3165521812098f83885fa945
beep boop <githubbot@vadim.me>
2024-07-20 01:45:46 +0200

    Solution 10 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0vyyuw2q Prompt:       |                    ^~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:11: 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp: In function ‘int main()’: 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,10 +1,23 @@
-Here is the completed code:
+#include <vector>
+#include <algorithm>
 
-vector<float> get_positive(vector<float> l){
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
-    for (float x : l) {
-        if (x > 0) {
-            result.push_back(x);
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
         }
     }
     return result;
