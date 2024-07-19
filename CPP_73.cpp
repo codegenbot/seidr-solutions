bcded055e05d62fb52ccd6ace50a078e4ee42604
beep boop <githubbot@vadim.me>
2024-07-19 18:27:35 +0200

    Solution 2 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/l7mgyxar Prompt: 58324547-896a-4e4a-b69e-a6754d143634.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int smallest_change(vector<int> arr) {       |                     ^~~~~~ 58324547-896a-4e4a-b69e-a6754d143634.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int smallest_change(vector<int> arr) {       |                            ^~~ In file included from 58324547-896a-4e4a-b69e-a6754d143634.cpp:24: 58324547-896a-4e4a-b69e-a6754d143634.cpp: In function ‘int main()’: 58324547-896a-4e4a-b69e-a6754d143634.cpp:26:28: error: ‘smallest_change’ cannot be used as a function    26 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,26 +1,24 @@
 #include <vector>
-using namespace std;
 
 int smallest_change(vector<int> arr) {
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(n));
+    std::vector<std::vector<int>> dp(n, std::vector<int>(n));
     
     for (int i = 0; i < n; i++) {
         dp[i][i] = 0;
     }
     
-    for (int length = 2; length <= n; length++) {
-        for (int i = 0; i < n - length + 1; i++) {
-            int j = i + length - 1;
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i < n - len + 1; i++) {
+            int j = i + len - 1;
             
             if (arr[i] == arr[j]) {
-                dp[i][j][0] = 1;
+                dp[i][j][0] = dp[i+1][j-1][0];
             } else {
-                dp[i][j][0] = 1 + min(dp[i + 1][j - 1][0], dp[i + 1][j - 1][1]);
-                dp[i][j][1] = 1 + min(dp[i + 1][j - 1][0], dp[i + 1][j - 1][1]);
+                dp[i][j][0] = 1 + min(dp[i+1][j][0], dp[i][j-1][0]);
             }
         }
     }
     
-    return dp[0][n - 1][0];
+    return dp[0][n-1][0];
 }
