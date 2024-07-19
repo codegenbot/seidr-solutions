99913ff02d5faacbca1acc05f5b0c0e6a14847b3
beep boop <githubbot@vadim.me>
2024-07-19 15:38:09 +0200

    Solution 90 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/g2v0za89 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/50ed5e0f-168d-4dd6-b539-e980fac77 527.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -2,13 +2,24 @@
 def bowling_score(frames):
     score = 0
     frame = 1
-    for i in range(len(frames)):
+    i = 0
+    while i < len(frames):
         if frames[i].isdigit():
             if frame == 10:
                 break
-            score += int(frames[i])
-            frame += 1
+            if len(frames) - i > 2 and frames[i+1] == '/':
+                score += int(frames[i]) + 10
+                i += 3
+            else:
+                score += int(frames[i])
+                frame += 1
         elif frames[i] == '/':
-            score += 10 - (int(frames[i-2]) + int(frames[i-1]))
+            first_roll = int(frames[i-1].replace('/',''))
+            second_roll = int(frames[i+1].replace('/', ''))
+            if first_roll + second_roll < 10:
+                score += 10 - first_roll
+            else:
+                score += first_roll + second_roll
             frame += 1
+        i += 1
     return score
