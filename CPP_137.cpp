1f91d6116028462f82bd1938d35ff36d4f28c88c
beep boop <githubbot@vadim.me>
2024-07-19 22:15:32 +0200

    Solution 22 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8hrrqsp9 Prompt:    17 | } else if(a.type() == typeid(string) && b.type() == typeid(string)){       |   ^~~~ 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:29:3: error: expected unqualified-id be fore ‘else’    29 | } else {       |   ^~~~ In file included from 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:33: 4b349af5-749a-4950-9ec8-26e96296ac85.cpp: In function ‘int main()’: 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:35:13: error: ‘boost’ has not been decl ared    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:35:29: error: ‘string’ was not declared  in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:35:37: error: ‘compare_one’ was not dec lared in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,44 +1,42 @@
 #include <any>
 #include <string>
 #include <algorithm>
-#include <cassert>
 
 template <typename T>
 T compare_one(T a, T b) {
-    if (std::any(a).type() == typeid(int) && std::any(b).type() == typeid(int)) {
-        if (std::any_cast<int>(a) > std::any_cast<int>(b)) {
+    if constexpr(std::is_same_v<T, int>) {
+        if (a > b) {
             return a;
-        } else if (std::any_cast<int>(a) < std::any_cast<int>(b)) {
+        } else if (a < b) {
             return b;
         } else {
-            return T("None");
+            return "None";
         }
-    } else if (std::any(a).type() == typeid(float) && std::any(b).type() == typeid(float)) {
-        if (std::any_cast<float>(a) > std::any_cast<float>(b)) {
+    } else if constexpr(std::is_same_v<T, float>) {
+        if (a > b) {
             return a;
-        } else if (std::any_cast<float>(a) < std::any_cast<float>(b)) {
+        } else if (a < b) {
             return b;
         } else {
-            return T("None");
+            return "None";
         }
-    } else if (std::any(a).type() == typeid(std::string) && std::any(b).type() == typeid(std::string)) {
-        std::string strA = std::any_cast<std::string>(a);
-        std::string strB = std::any_cast<std::string>(b);
-        std::replace(strA.begin(), strA.end(), ',', '.');
-        std::replace(strB.begin(), strB.end(), ',', '.');
-        if (std::stof(strA) > std::stof(strB)) {
+    } else if constexpr(std::is_same_v<T, std::string>) {
+        std::replace(a.begin(), a.end(), ',', '.');
+        std::replace(b.begin(), b.end(), ',', '.');
+
+        if (std::stof(a) > std::stof(b)) {
             return a;
-        } else if (std::stof(strA) < std::stof(strB)) {
+        } else if (std::stof(a) < std::stof(b)) {
             return b;
         } else {
-            return T("None");
+            return "None";
         }
     } else {
-        return T("None");
+        return "None";
     }
 }
 
 int main() {
-    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("2"))) == "2");
+    assert(std::any_cast<std::string>(compare_one(std::string("1"), 1)) == "None");
     return 0;
 }
