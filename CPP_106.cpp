248d5787ef2a3d41017c504a56f7f16a07681097
beep boop <githubbot@vadim.me>
2024-07-20 00:46:18 +0200

    Solution 21 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/f5l3hydo Prompt:       |                    ^~~ f4a094e1-e532-42d6-8008-0a1c038aae84.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f4a094e1-e532-42d6-8008-0a1c038aae84.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f4a094e1-e532-42d6-8008-0a1c038aae84.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f4a094e1-e532-42d6-8008-0a1c038aae84.cpp:19: f4a094e1-e532-42d6-8008-0a1c038aae84.cpp: In function ‘int main()’: f4a094e1-e532-42d6-8008-0a1c038aae84.cpp:29:20: error: ‘f’ was not declared in t his scope    29 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ f4a094e1-e532-42d6-8008-0a1c038aae84.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -4,12 +4,16 @@
 std::vector<int> f(int n);
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size())
+    if(a.size() != b.size()) {
         return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i])
-            return false;
     }
+    
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    
     return true;
 }
 
@@ -31,11 +35,11 @@ std::vector<int> f(int n){
     return result;
 }
 
-int main(){
-    std::vector<int> test1 = f(5);
-    std::vector<int> test2 = {1, 2, 2, 6, 6};
+int main() {
+    std::vector<int> test = f(5);
+    std::vector<int> expected = {1, 3, 2, 10, 3};
     
-    assert(issame(test1, test2));
+    assert(issame(test, expected));
     
     return 0;
 }
