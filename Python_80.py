a1ee90e21f7d63734106ddab0bbddafaed463406
beep boop <githubbot@vadim.me>
2024-07-19 16:58:22 +0200

    Solution 10 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/c96pp2vg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d084e51-b9a1-4b48-b04a-be8d0fdbd ae4.py", line 15, in <module>     check(is_happy)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d084e51-b9a1-4b48-b04a-be8d0fdbd ae4.py", line 12, in check     assert is_happy("iopaxioi") == False, "iopaxioi"            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d084e51-b9a1-4b48-b04a-be8d0fdbd ae4.py", line 6, in is_happy     if set(chunk)[0] == set(chunk)[1] or set(chunk)[0] == set(chunk)[2] or set(c hunk)[1] == set(chunk)[2]:        ~~~~~~~~~~^^^ TypeError: 'set' object is not subscriptable

--- Python_80.py
+++ Python_80.py
@@ -1,9 +1,8 @@
-```
 def is_happy(s):
     if len(s) < 3:
         return False
     for i in range(0, len(s), 3):
         chunk = s[i:i+3]
-        if len(set(chunk)) == 1 or len(set(chunk)) == 2:
+        if list(set(chunk))[0] == list(set(chunk))[1] or list(set(chunk))[0] == list(set(chunk))[2] or list(set(chunk))[1] == list(set(chunk))[2]:
             return True
     return False
