4c1d2c7a0009d64abf30af5c4411a8fd898c280d
beep boop <githubbot@vadim.me>
2024-07-19 20:21:53 +0200

    Solution 74 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/j51oka9j Prompt:       |             ^~~~~~ d486a723-401f-4bb7-91dc-c432282faad1.cpp:37:20: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ d486a723-401f-4bb7-91dc-c432282faad1.cpp:37:27: error: ‘vector’ was not declared  in this scope    37 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d486a723-401f-4bb7-91dc-c432282faad1.cpp:37:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d486a723-401f-4bb7-91dc-c432282faad1.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d486a723-401f-4bb7-91dc-c432282faad1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d486a723-401f-4bb7-91dc-c432282faad1.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,23 +1,34 @@
 #include <vector>
 #include <cassert>
-#include <algorithm> 
+#include <algorithm>
 
-bool are_equal(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+bool issame(const std::vector<int> &a, const std::vector<int> &b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] % 2 == 0) {
+            return false;
+        }
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 std::vector<int> get_odd_collatz(int num) {
     std::vector<int> result;
     while (num != 1) {
-        result.push_back(num);
-        num = num % 2 == 0 ? num / 2 : 3 * num + 1;
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            if (num != 1) {
+                result.push_back(num);
+            }
+            num = 3 * num + 1;
+        }
     }
     result.push_back(1);
     return result;
-}
-
-int main() {
-    // Test the function
-    assert(are_equal(get_odd_collatz(1), {1}));
-    return 0;
 }
