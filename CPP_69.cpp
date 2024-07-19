2263f0582f858831784f667296eebcf7396eefd3
beep boop <githubbot@vadim.me>
2024-07-19 18:57:43 +0200

    Solution 46 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ulptsl46 Prompt: a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp:8:5: error: ‘cout’ was not declared in this scope     8 |     cout << "The largest number in the list is: " << result << endl;       |     ^~~~ a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp:8:64: error: ‘endl’ was not declared in  this scope     8 |     cout << "The largest number in the list is: " << result << endl;       |                                                                ^~~~ a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp: At global scope: a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp:13:5: error: redefinition of ‘int main( )’    13 | int main(){       |     ^~~~ a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp:2:5: note: ‘int main()’ previously defi ned here     2 | int main() {       |     ^~~~ In file included from a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp:12: a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp: In function ‘int main()’: a492b9d4-0694-4a52-9be6-ffbd7516a37e.cpp:14:13: error: ‘search’ was not declared  in this scope    14 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ^~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,27 +1,14 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
 
-int count(int num) {
-    int res = 0;
-    for (int i : {1, 2, 3, 4, 5, 6, 7, 8, 9}) {
-        if (i == num) res++;
-    }
-    return res;
-}
-
-int search(vector<int> lst) {
-    int max = 0;
-    for (int i : lst) {
-        if (i > 0 && i >= count(i)) {
-            max = i;
-        }
-    }
-    return max == 0 ? -1 : max;
+int search(vector<int>& lst) {
+    int max = *max_element(lst.begin(), lst.end());
+    return max;
 }
 
 int main() {
-    vector<int> lst = vector<int>(5, 0);
+    vector<int> lst(5); 
     for (int i = 0; i < 5; ++i) { 
         cin >> lst[i]; 
     }
