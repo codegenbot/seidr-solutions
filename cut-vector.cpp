6db6683fd75388bec7a825291c547d666e57a3df
beep boop <githubbot@vadim.me>
2024-07-19 17:40:22 +0200

    Solution 21 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt: efce01d3-4058-4894-ae26-ddfc4ba21027.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: efce01d3-4058-4894-ae26-ddfc4ba21027.cpp:28:13: error: ‘i’ was not declared in t his scope    28 |     int j = i;       |             ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -5,25 +5,37 @@ using namespace std;
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
-    long long sum1 = 0;
     
+    long long sum1 = 0, sum2 = 0;
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
             sum1 += v[i];
         } else {
-            long long leftSum = 0;
-            for (int j = 0; j <= i; j++) {
-                leftSum += v[j];
-            }
-            long long rightSum = sum1 - leftSum;
-            if (abs(leftSum - rightSum) <= abs(v[0] - rightSum)) {
-                res[0].insert(res[0].end(), v.begin(), v.begin() + i + 1);
-                for (int j = i; j < n; j++) {
-                    res[1].push_back(v[j]);
-                }
-                break;
-            }
+            sum2 += v[i];
         }
+    }
+    
+    int diff = abs(sum1 - sum2);
+    res[0].push_back(v[0]);
+    for (int i = 1; i < n; i++) {
+        if (abs(sum1 - sum2) == abs(v[i] - sum2)) {
+            res[0].push_back(v[i]);
+        } else {
+            break;
+        }
+    }
+    
+    int lastI = i;
+    while(lastI < n) { 
+        sum2 -= v[lastI];
+        sum1 += v[lastI];
+        if (abs(sum1 - sum2) <= diff) {
+            res[0].push_back(v[lastI]);
+        } else {
+            res[1] = vector<int>(v.begin() + lastI, v.end());
+            break;
+        }
+        lastI++;
     }
     
     return res;
