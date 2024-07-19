4c95b7672d0898ffa416913e51f5be298639238f
beep boop <githubbot@vadim.me>
2024-07-19 22:59:47 +0200

    Solution 35 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/scsisb4x Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 08f3ac46-4b75-4358-9561-45b44318cc19.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 08f3ac46-4b75-4358-9561-45b44318cc19.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 08f3ac46-4b75-4358-9561-45b44318cc19.cpp:11: 08f3ac46-4b75-4358-9561-45b44318cc19.cpp: In function ‘int main()’: 08f3ac46-4b75-4358-9561-45b44318cc19.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 08f3ac46-4b75-4358-9561-45b44318cc19.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,8 +1,16 @@
-bool are_same(vector<string> a, vector<string> b) {
+bool are_same(vector<string> a, vector<string> b) { 
     return a == b;
 }
 
-vector<string> filter_by_substring(vector<string> strings, string substring);
+vector<string> filter_by_substring(vector<string> strings, string substring) {
+    vector<string> result;
+    for (const auto &str : strings) {
+        if (str.find(substring) != string::npos) {
+            result.push_back(str);
+        }
+    }
+    return result;
+}
 
 int main() {
     assert(are_same(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
