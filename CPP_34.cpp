b3de60b3e22558854f83996b482aed1ab4ce10e0
beep boop <githubbot@vadim.me>
2024-07-19 19:09:34 +0200

    Solution 65 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xf1ve6zq Prompt:     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 67d6853b-d88d-48c2-b527-998cb62f15f8.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 67d6853b-d88d-48c2-b527-998cb62f15f8.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 67d6853b-d88d-48c2-b527-998cb62f15f8.cpp:6: 67d6853b-d88d-48c2-b527-998cb62f15f8.cpp: In function ‘int main()’: 67d6853b-d88d-48c2-b527-998cb62f15f8.cpp:16:20: error: ‘unique’ was not declared  in this scope    16 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 67d6853b-d88d-48c2-b527-998cb62f15f8.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,13 +1,13 @@
-```cpp
 auto unique_copy(vector<int>::iterator first, vector<int>::iterator last) {
     std::vector<int> result(std::unique_copy(first, last));
     return result;
 }
 
 bool isSame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); ++i){
-        if(std::find(b.begin(), b.end(), a[i]) == b.end())
+    if (a.size() != b.size()) 
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) 
             return false;
     }
     return true;
