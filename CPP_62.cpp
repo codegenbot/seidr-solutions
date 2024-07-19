ed6a9983735b6b444506a21a86763959f3b1f5c8
beep boop <githubbot@vadim.me>
2024-07-19 18:58:59 +0200

    Solution 72 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/361xm7c7 Prompt:       |           ^~~~~~ In file included from 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp:14: 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp: In function ‘int main()’: 7469bc75-3250-4a86-aec2-3dd55e8f8c4a.cpp:24:20: error: ‘derivative’ was not decl ared in this scope    24 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,13 +1,12 @@
 #include <vector>
 #include <cassert>
-#include <memory>
 
 bool issame(const std::vector<float>& a, const std::vector<float>& b){
-    return a.size() == b.size();
+    return a == b;
 }
 
-int main(){
-    assert(issame(std::vector<float>({1}), std::vector<float>{}));
+int main() {
+    assert(issame(std::vector<float>({1.0}), std::vector<float>{}));
     
     return 0;
 }
