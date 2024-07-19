12d9995769b68a7c4ba6c4583a33bdec9668192a
beep boop <githubbot@vadim.me>
2024-07-19 21:36:56 +0200

    Solution 7 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d91o4gsr Prompt: 7b864612-1d6f-4905-89a5-1d776d7d2dd0.cpp:24:16: error: expected primary-expressi on before ‘double’    24 |         vector<double> coeffs = {};       |                ^~~~~~ 7b864612-1d6f-4905-89a5-1d776d7d2dd0.cpp:29:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    29 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 7b864612-1d6f-4905-89a5-1d776d7d2dd0.cpp:32:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    32 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff In file included from 7b864612-1d6f-4905-89a5-1d776d7d2dd0.cpp:16: 7b864612-1d6f-4905-89a5-1d776d7d2dd0.cpp:33:21: error: ‘poly’ was not declared i n this scope    33 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 7b864612-1d6f-4905-89a5-1d776d7d2dd0.cpp:33:17: error: ‘abs’ was not declared in  this scope    33 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,28 +1,16 @@
-```cpp
-#include <vector>
-#include <cmath>
+```
+double find_zero(int n) {
+    double solution;
+    cin >> solution;
 
-double poly(vector<double> coeffs, double solution) {
-    double result = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        result += coeffs[i] * pow(solution, i);
+    vector<double> coeffs(n+1);
+    for (int i = 0; i <= n; i++) {
+        cin >> coeffs[i];
     }
-    return result;
-}
 
-double find_zero(vector<double> coeffs, double solution) {
     double sum = 0;
-    for (int i = 2; i < coeffs.size(); i += 2) {
+    for (int i = 2; i < n+1; i += 2) {
         sum -= coeffs[i/2] * pow(solution, i) / (pow(solution, i+1));
     }
     return -sum / pow(solution, 2);
-}
-
-int main() {
-    vector<double> coeffs = {1.0, -6.0, 11.0, -6.0, 1.0};
-    double solution = 1.0;
-
-    // Use the find_zero function with the declared variables
-    double result = find_zero(coeffs, solution);
-    cout << "The zero is: " << result << endl;
 }
