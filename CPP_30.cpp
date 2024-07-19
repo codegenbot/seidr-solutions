dc7148c2ad2f66b8d7522853576cb0ba7fc33c0c
beep boop <githubbot@vadim.me>
2024-07-19 21:52:20 +0200

    Solution 29 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eoo9avp7 Prompt: aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {float}; _Tp = float; _Alloc = std::allocator<float>; std::vector<_ Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {float}; _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc >::reference = float&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = flo at; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::value_type = float ]’ a5a89927-7748-47ca-a63f-518eb2fd4e3c.cpp:21:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <vector>
 
-bool checkVectorsEqual(std::vector<float> a, std::vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
-        }
     }
     return true;
 }
 
-std::vector<float> get_positive(const std::vector<int>& l) {
+std::vector<float> get_positive(const std::vector<float>& l) {
     std::vector<float> result;
-    for (int num : l) {
+    for (float num : l) {
         if (num > 0) {
-            result.push_back((float)num); 
+            result.push_back(num); 
         }
     }
     return result;
 }
 
 int main() {
-    assert(checkVectorsEqual(get_positive({}), std::vector<float>{}));
+    assert(issame(get_positive({}), {}) );
     return 0;
 }
