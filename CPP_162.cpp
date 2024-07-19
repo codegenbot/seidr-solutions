45d9433023adcb345b7c876b6bdf6779e010991d
beep boop <githubbot@vadim.me>
2024-07-19 05:57:45 +0200

    Solution 28 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/twbnhxvv Prompt:    12 | for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {       | ^~~ 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp:12:17: error: ‘i’ does not name a type    12 | for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {       |                 ^ 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp:12:40: error: ‘i’ does not name a type    12 | for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {       |                                        ^ 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp:16:1: error: expected unqualified-id be fore ‘return’    16 | return string(md5String);       | ^~~~~~ 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp:17:1: error: expected declaration befor e ‘}’ token    17 | }       | ^ In file included from 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp:19: 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp: In function ‘int main()’: 3db3da6f-76c0-437f-94a7-d720d6cbd099.cpp:21:13: error: ‘string_to_md5’ was not d eclared in this scope    21 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,25 +1,21 @@
-
-#include <iostream>
+#include <cassert>
 #include <string>
-#include <openssl/md5.h>
 
-std::string string_to_md5(const std::string& text) {
-    if(text.empty()) {
+string string_to_md5(const string& text) {
+    if (text.empty()) {
         return "None";
     }
 
-    unsigned char digest[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)text.c_str(), text.length(), digest);
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, text.c_str(), text.length());
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5_Final(result, &ctx);
 
-    char mdString[33];
-    for(int i = 0; i < 16; i++) {
-        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+    char md5String[2 * MD5_DIGEST_LENGTH + 1];
+    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        sprintf(&md5String[i * 2], "%02x", (unsigned int)result[i]);
     }
 
-    return std::string(mdString);
-}
-
-int main() {
-    assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-    return 0;
+    return string(md5String);
 }
