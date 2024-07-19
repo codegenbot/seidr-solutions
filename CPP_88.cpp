9d13e9de93a3c89a99632f61cd3744b1201b877d
beep boop <githubbot@vadim.me>
2024-07-19 02:47:44 +0200

    Solution 64 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/cj6ib63j Prompt:       |             ^~~~~~ a7e8ada6-2c3a-488c-bfb5-c1ee718c0680.cpp:25:20: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ a7e8ada6-2c3a-488c-bfb5-c1ee718c0680.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a7e8ada6-2c3a-488c-bfb5-c1ee718c0680.cpp:25:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a7e8ada6-2c3a-488c-bfb5-c1ee718c0680.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a7e8ada6-2c3a-488c-bfb5-c1ee718c0680.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a7e8ada6-2c3a-488c-bfb5-c1ee718c0680.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -4,21 +4,19 @@
 #include <functional>
 #include <iostream>
 
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b){
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
 
-vector<int> sort_array(vector<int> array){
+std::vector<int> sort_array(std::vector<int> array){
     if(array.empty()) return array;
     if((array.front() + array.back()) % 2 == 0)
-        sort(array.begin(), array.end(), greater<int>());
+        std::sort(array.begin(), array.end(), std::greater<int>());
     else
-        sort(array.begin(), array.end());
+        std::sort(array.begin(), array.end());
     return array;
 }
 
-#define ASSERT_VECTOR_EQ(a, b) assert(equal(a.begin(), a.end(), b.begin(), b.end()))
+#define ASSERT_VECTOR_EQ(a, b) assert(std::equal(a.begin(), a.end(), b.begin(), b.end()))
 
-ASSERT_VECTOR_EQ(sort_array(vector<int>{21, 14, 23, 11}), {23, 21, 14, 11});
+ASSERT_VECTOR_EQ(sort_array(std::vector<int>{21, 14, 23, 11}), {23, 21, 14, 11});
