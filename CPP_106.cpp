53e69e941beab4f4b357bd334ae88286fd26126f
beep boop <githubbot@vadim.me>
2024-07-19 19:39:17 +0200

    Solution 40 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/b3mz319z Prompt: 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp: At global scope: 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp:29:6: error: ambiguating new declaratio n of ‘bool issame(std::vector<int>, std::vector<int>)’    29 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp:6:18: note: old declaration ‘std::vecto r<int> issame(std::vector<int>, std::vector<int>)’     6 | std::vector<int> issame(std::vector<int> a, std::vector<int> b) {       |                  ^~~~~~ 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp:28: 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp: In function ‘int main()’: 41041b17-2e8f-4ef2-9d82-f7ef298cd718.cpp:38:20: error: ‘f’ was not declared in t his scope    38 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -3,24 +3,11 @@
 
 using namespace std;
 
-vector<int> issame(vector<int> a, vector<int> b) {
-    vector<int> result;
-    int factorial = 1;
-    int sum = 0;
-    int n = a.size();
-    for (int i = 1; i <= n; ++i) {
-        if (i % 2 == 0) {
-            factorial *= i;
-            result.push_back(factorial);
-        } else {
-            sum += i;
-            result.push_back(sum);
-        }
-    }
-    return result;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 int main() {
-    assert(issame({1, 2, 3}, {1, 2, 6}) == true);
+    assert(issame({1, 2, 3}, {1, 2, 6}));
     return 0;
 }
