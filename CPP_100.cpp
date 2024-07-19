81a313e40f31fc0f9c0310b2b69704afdb97c699
beep boop <githubbot@vadim.me>
2024-07-19 09:41:45 +0200

    Solution 21 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ktzb0o4v Prompt:       |                    ^~~ f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp:13: f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp: In function ‘int main()’: f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp:23:20: error: ‘make_a_pile’ was not dec lared in this scope    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ f79abdd6-0c6d-4c8a-8477-abd1472c10cf.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,22 +1,14 @@
 #include <vector>
 
-bool issame(const vector<int> a, const vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(const vector<int> &a, const vector<int> &b) {
+    return a == b;
 }
 
 vector<int> make_a_pile(int n) {
     vector<int> stones;
     stones.push_back(n);
-    for(int i = 2; i <= n; ++i) {
-        if(n % 2 == 0) {
+    for(int i = 2; i <= n; ++i){
+        if(n % 2 == 0){
             n += 2;
         } else {
             n += 1;
