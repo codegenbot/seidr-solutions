bc7d720f5b72a7ae8570676fb84e78375dc17597
beep boop <githubbot@vadim.me>
2024-07-19 23:08:11 +0200

    Solution 99 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt:       |             ^~~~~~ a5822213-99f6-4824-a20e-001e9a342524.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a5822213-99f6-4824-a20e-001e9a342524.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a5822213-99f6-4824-a20e-001e9a342524.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ a5822213-99f6-4824-a20e-001e9a342524.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from a5822213-99f6-4824-a20e-001e9a342524.cpp:24: a5822213-99f6-4824-a20e-001e9a342524.cpp: In function ‘int main()’: a5822213-99f6-4824-a20e-001e9a342524.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <set>
 #include <algorithm>
@@ -9,11 +10,14 @@ bool areSetsEqual(std::vector<int> a, std::vector<int> b) {
     return (s1 == s2 || s1.issubset(s2) || s2.issubset(s1));
 }
 
-bool areSame(std::vector<int> a, std::vector<int> b) {
-    return areSetsEqual(a, b);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    std::set<int> s1(a.begin(), a.end());
+    std::set<int> s2(b.begin(), b.end());
+
+    return (s1 == s2 || s1.issubset(s2) || s2.issubset(s1));
 }
 
 int main() {
-    assert(areSame({4, 3, 2, 8}, {}) == false);
+    assert(issame({4, 3, 2, 8}, {}) == false);
     return 0;
 }
