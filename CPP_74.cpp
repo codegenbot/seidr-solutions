49ee5a1a97e27622f6a855dc0099bde5f7fc839f
beep boop <githubbot@vadim.me>
2024-07-19 02:25:40 +0200

    Solution 32 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duyg3to1 Prompt: 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp:16: 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp: In function ‘int main()’: 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp: In function ‘int total_chars(int)’: 3e41f4a0-a0de-446a-988e-8c1e7e872d4c.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -12,19 +12,18 @@ int total_chars(const vector<string>& lst) {
     return total;
 }
 
-bool issame(vector<string> a, vector<string> b) {
+vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
+    if (total_chars(lst1) < total_chars(lst2)) {
+        return lst1;
+    }
+    return lst2;
+}
+
+bool issame(const vector<string>& a, const vector<string>& b){
     return a == b;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    if (total_chars(lst1) < total_chars(lst2)) {
-        return lst1;
-    } else {
-        return lst2;
-    }
-}
-
 int main() {
-    assert (issame(total_match({"this"}, {}) , {}));
+    assert(issame(total_match({"this"}, {}), {}));
     return 0;
 }
