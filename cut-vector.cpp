ce8bf2b73da5df44a9baea176e79954d8926ed37
beep boop <githubbot@vadim.me>
2024-07-19 17:17:46 +0200

    Solution 17 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt:   +++ |+#include <iostream>     2 | using namespace std; 44e1750d-18e9-41f9-958b-2c85ada00f35.cpp:45:13: error: ‘cout’ was not declared i n this scope    45 |             cout << x << " ";       |             ^~~~ 44e1750d-18e9-41f9-958b-2c85ada00f35.cpp:45:13: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 44e1750d-18e9-41f9-958b-2c85ada00f35.cpp:47:9: error: ‘cout’ was not declared in  this scope    47 |         cout << endl;       |         ^~~~ 44e1750d-18e9-41f9-958b-2c85ada00f35.cpp:47:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 44e1750d-18e9-41f9-958b-2c85ada00f35.cpp:47:17: error: ‘endl’ was not declared i n this scope    47 |         cout << endl;       |                 ^~~~ 44e1750d-18e9-41f9-958b-2c85ada00f35.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <algorithm>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,43 +1,33 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
-    long long sum1 = 0;
+    long long sum1 = 0, sum2 = 0;
     
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
             sum1 += v[i];
         } else {
-            long long sum2 = sum1 + v[i] - (i > 0 ? v[i-1] : 0);
-            res[0].push_back(v[i]);
-            while (abs(sum1 - sum2) >= abs(sum1 - (sum2 - v[i]))) {
-                if (i % 2 == 0) {
-                    sum1 -= v[i];
-                } else {
-                    sum2 -= v[i];
-                }
-                res[0].pop_back();
-                i--;
-            }
+            sum2 += v[i];
         }
     }
     
-    for (int i = n; i > 0; i--) {
-        if (i % 2 != 0) {
-            long long sum1 = sum2 + v[i-1] - (n > i ? v[n-1] : 0);
-            while (abs(sum1 - sum2) >= abs(sum1 - (sum2 - v[i-1]))) {
-                if (i % 2 == 0) {
-                    sum1 -= v[i-1];
-                } else {
-                    sum2 -= v[i-1];
-                }
-                res[1].push_back(v[i-1]);
-                i++;
+    if (abs(sum1 - sum2) <= abs((long long)v[0] - sum2)) {
+        res[0].push_back(v[0]);
+        for (int i = 1; i < n; i++) {
+            if (abs(sum1 - sum2) == abs(v[i] - sum2)) {
+                res[0].push_back(v[i]);
+            } else {
+                break;
             }
         }
+        res[1] = v;
+    } else {
+        res[0] = v;
+        res[1].clear();
     }
     
     return res;
