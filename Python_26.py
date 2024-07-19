a8cfc6be6479188e32f904c5ca13904b80c3a5d0
beep boop <githubbot@vadim.me>
2024-07-19 06:18:51 +0200

    Solution 64 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hj8ms4en Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ab75a418-879d-4138-8185-ba746fa6b 1cd.py", line 8, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ab75a418-879d-4138-8185-ba746fa6b 1cd.py", line 5, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -3,6 +3,6 @@ def remove_duplicates(numbers):
     seen = set()
     for x in numbers:
         if x not in seen:
-            unique_numbers.append(x)
             seen.add(x)
+            unique_numbers.append(x)
     return unique_numbers
