00586fcf1c6af4118651f44533dab8529ef8f6e8
beep boop <githubbot@vadim.me>
2024-07-19 22:19:05 +0200

    Solution 17 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/iho33lfc Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6823058c-bf02-472c-9a5b-0c032d5d51d2.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6823058c-bf02-472c-9a5b-0c032d5d51d2.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6823058c-bf02-472c-9a5b-0c032d5d51d2.cpp:19: 6823058c-bf02-472c-9a5b-0c032d5d51d2.cpp: In function ‘int main()’: 6823058c-bf02-472c-9a5b-0c032d5d51d2.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 6823058c-bf02-472c-9a5b-0c032d5d51d2.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-bool issame(int a, int b) {
+bool issame(vector<int> a, vector<int> b) {
     int sum_a = 0, sum_b = 0;
     int temp_a = abs(a), temp_b = abs(b);
     while (temp_a > 0) {
@@ -14,16 +10,10 @@ bool issame(int a, int b) {
         temp_b /= 10;
     }
     if (sum_a == sum_b) {
-        return a < b;
+        return find(a.begin(), a.end(), a) < find(b.begin(), b.end(), b);
     }
     return sum_a < sum_b;
 }
 
-int main() {
-    std::vector<int> nums = {123, 4352, 67, 56, 7890};
-    std::sort(nums.begin(), nums.end(), issame);
-    for (int num : nums) {
-        std::cout << num << " ";
-    }
-    return 0;
-}
+sort(nums.begin(), nums.end(), issame);
+return nums;
