c0d6b47184fe89e2e8643c63b7642f66d64b3b25
beep boop <githubbot@vadim.me>
2024-07-19 05:08:28 +0200

    Solution 24 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hq90sf52 Prompt:       |                              ^~~~~~ d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp:30: d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp: In function ‘int main()’: d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp:40:20: error: ‘bf’ was not declared in this scope    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ d9dc0311-af8e-400c-b94d-c05b3a4f468c.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
+bool issame(vector<string> a, vector<string> b);
 
 vector<string> bf(string planet1, string planet2){
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
@@ -36,10 +27,4 @@ vector<string> bf(string planet1, string planet2){
     }
     
     return result;
-}
-
-int main(){
-    assert(issame(bf("Jupiter", "Makemake"), {}));
-    
-    return 0;
 }
