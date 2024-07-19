1b032205620191b652f54fd9072461ef4e71e01a
beep boop <githubbot@vadim.me>
2024-07-19 17:23:37 +0200

    Solution 0 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ti2u6fw9 Prompt: Complete the following code given the task description and function signature.

--- Python_147.py
+++ Python_147.py
@@ -4,8 +4,7 @@ def get_max_triples(n):
     count = 0
     for i in range(len(a)):
         for j in range(i+1, len(a)):
-            if a[i] % 3 == 0 and a[j] % 3 == 0:
-                for k in range(j+1, len(a)):
-                    if (a[i] + a[j] + a[k]) % 3 == 0:
-                        count += 1
+            for k in range(j+1, len(a)):
+                if (a[i] + a[j] + a[k]) % 3 == 0:
+                    count += 1
     return count
