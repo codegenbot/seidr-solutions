423c0b8c3c8bf5962ca1d92f48f8d90e01b0530a
beep boop <githubbot@vadim.me>
2024-07-19 02:51:43 +0200

    Solution 2 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/349dp4rn Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5124653b-279a-41f6-98f7-3bddffc605f8.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5124653b-279a-41f6-98f7-3bddffc605f8.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5124653b-279a-41f6-98f7-3bddffc605f8.cpp:5: 5124653b-279a-41f6-98f7-3bddffc605f8.cpp: In function ‘int main()’: 5124653b-279a-41f6-98f7-3bddffc605f8.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 5124653b-279a-41f6-98f7-3bddffc605f8.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -4,32 +4,5 @@ bool issame(vector<int> a, vector<int> b){
 
 vector<int> minPath(vector<vector<int>> grid, int k){
     vector<int> result;
-    int n = grid.size();
-    if(n == 0) return result;
-    
-    vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-    dp[0][0] = grid[0][0];
-    
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < n; ++j){
-            if(i > 0) dp[i][j] = min(dp[i][j], dp[i-1][j] + grid[i][j]);
-            if(j > 0) dp[i][j] = min(dp[i][j], dp[i][j-1] + grid[i][j]);
-        }
-    }
-    
-    int x = n-1, y = n-1;
-    result.push_back(grid[x][y]);
-    
-    while(x > 0 || y > 0){
-        if(x > 0 && dp[x][y] - grid[x][y] == dp[x-1][y]){
-            x--;
-        } else {
-            y--;
-        }
-        result.push_back(grid[x][y]);
-    }
-    
-    reverse(result.begin(), result.end());
-    
     return result;
 }
