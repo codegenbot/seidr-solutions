e71f8688b936988c4fc2c2354c88f9cfaeb7a5b2
beep boop <githubbot@vadim.me>
2024-07-19 03:18:00 +0200

    Solution 87 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/p7han3r6 Prompt:       |     ^~~~ In file included from c9d57582-bccb-424d-af61-4b39b46acadd.cpp:18: c9d57582-bccb-424d-af61-4b39b46acadd.cpp: In function ‘int main()’: c9d57582-bccb-424d-af61-4b39b46acadd.cpp:20:13: error: ‘boost’ has not been decl ared    20 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ c9d57582-bccb-424d-af61-4b39b46acadd.cpp:20:29: error: ‘string’ was not declared  in this scope    20 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ c9d57582-bccb-424d-af61-4b39b46acadd.cpp:20:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from c9d57582-bccb-424d-af61-4b39b46acadd.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from c9d57582-bccb-424d-af61-4b39b46acadd.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,16 +2,15 @@
 #include <iostream>
 #include <cassert>
 #include <variant>
-#include <string_view>
 
-std::variant<std::string, std::string_view> compare_one(const std::string& s1, int number) {
+std::variant<std::string, bool> compare_one(const std::string& s1, int number) {
     if (std::stoi(s1) == number) {
-        return {"Equal"sv};
+        return {"Equal", true};
     } else {
-        return {"Not equal"sv};
+        return {"Not equal", false};
     }
 }
 
 int main() {
-    assert(std::get<std::string_view>(compare_one("1", 1)) == "Not equal");
+    assert(std::get<std::string>(compare_one("1", 1)) == "Equal");
 }
