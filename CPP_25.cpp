be145ebb26d385d417127159a88bb838ce2aa930
beep boop <githubbot@vadim.me>
2024-07-19 20:58:32 +0200

    Solution 25 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ydl7v3nq Prompt:       |                    ^~~ 40c0327f-4f00-440a-8233-12bc39f07314.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 40c0327f-4f00-440a-8233-12bc39f07314.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 40c0327f-4f00-440a-8233-12bc39f07314.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 40c0327f-4f00-440a-8233-12bc39f07314.cpp:15: 40c0327f-4f00-440a-8233-12bc39f07314.cpp: In function ‘int main()’: 40c0327f-4f00-440a-8233-12bc39f07314.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 40c0327f-4f00-440a-8233-12bc39f07314.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,19 +1,19 @@
 #include <vector>
 #include <assert.h>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) {
+bool issame(std::vector<int> a, std::vector<int> b){
+    if(a.size() != b.size()){
         return false;
     }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
             return false;
         }
     }
     return true;
 }
 
-int main() {
-    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
+int main(){
+    assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));
     return 0;
 }
