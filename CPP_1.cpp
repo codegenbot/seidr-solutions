fb06f87b8a0a6ef0e34bc194068b7c9b48c08b74
beep boop <githubbot@vadim.me>
2024-07-20 01:45:10 +0200

    Solution 86 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/l0ivx5b2 Prompt: his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1154dd50-1ed2-429a-be4a-4f2deb23285c.cpp:43:45: error: expression list treated a s compound expression in initializer [-fpermissive]    43 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 1154dd50-1ed2-429a-be4a-4f2deb23285c.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 1154dd50-1ed2-429a-be4a-4f2deb23285c.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from 1154dd50-1ed2-429a-be4a-4f2deb23285c.cpp:42: 1154dd50-1ed2-429a-be4a-4f2deb23285c.cpp: In function ‘int main()’: 1154dd50-1ed2-429a-be4a-4f2deb23285c.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,35 +1,7 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<assert.h>
-
 bool is_same(std::vector<std::string> a, std::vector<std::string> b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
     return a == b;
-}
-
-std::vector<std::string> separate_paren_groups(std::string s) {
-    std::vector<std::string> groups;
-    int count = 0;
-    std::string current_group;
-
-    for (char c : s) {
-        if (c == '(') {
-            count++;
-            current_group += c;
-        } else if (c == ')') {
-            count--;
-            current_group += c;
-            if (count == 0) {
-                groups.push_back(current_group);
-                current_group = "";
-            }
-        }
-    }
-
-    return groups;
 }
 
 int main() {
