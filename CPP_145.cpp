75263099fe08bdeb4421b15ad8bdffdb7431f8da
beep boop <githubbot@vadim.me>
2024-07-19 05:12:49 +0200

    Solution 56 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/l1xqhco9 Prompt:    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 78077252-81ca-45a1-943f-7046f6063cff.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 78077252-81ca-45a1-943f-7046f6063cff.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 78077252-81ca-45a1-943f-7046f6063cff.cpp:28: 78077252-81ca-45a1-943f-7046f6063cff.cpp: In function ‘int main()’: 78077252-81ca-45a1-943f-7046f6063cff.cpp:38:20: error: ‘order_by_points’ was not  declared in this scope    38 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 78077252-81ca-45a1-943f-7046f6063cff.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,8 +1,12 @@
 #include <algorithm>
 #include <cassert>
 
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+}
+
 vector<int> order_by_points(vector<int> nums) {
-    sort(nums.begin(), nums.end(), [](int a, int b) {
+    std::sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         int temp_a = abs(a), temp_b = abs(b);
         while (temp_a > 0) {
@@ -14,7 +18,7 @@ vector<int> order_by_points(vector<int> nums) {
             temp_b /= 10;
         }
         if (sum_a == sum_b) {
-            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+            return std::find(nums.begin(), nums.end(), a) < std::find(nums.begin(), nums.end(), b);
         }
         return sum_a < sum_b;
     });
@@ -22,5 +26,5 @@ vector<int> order_by_points(vector<int> nums) {
 }
 
 int main() {
-    assert(order_by_points({0, 6, 6, -76, -21, 23, 4}) == vector<int>{-76, -21, 0, 4, 23, 6, 6});
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), vector<int>{-76, -21, 0, 4, 23, 6, 6}));
 }
