ce30ef5cf378c22999d427f15296278ab0d11dba
beep boop <githubbot@vadim.me>
2024-07-20 01:47:57 +0200

    Solution 63 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/22o47gku Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d3693af2-a3dc-42eb-9037-43cb458825eb.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d3693af2-a3dc-42eb-9037-43cb458825eb.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d3693af2-a3dc-42eb-9037-43cb458825eb.cpp:11: d3693af2-a3dc-42eb-9037-43cb458825eb.cpp: In function ‘int main()’: d3693af2-a3dc-42eb-9037-43cb458825eb.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ d3693af2-a3dc-42eb-9037-43cb458825eb.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -2,15 +2,14 @@
 #include <vector>
 #include <list>
 
-bool issame(const boost::any& a1, const boost::any& a2) {
-    return a1.type() == a2.type();
-}
+using namespace std;
+using namespace boost;
 
-std::vector<int> filter_integers(std::list<boost::any> values) {
-    std::vector<int> result;
+vector<int> filter_integers(list<any> values) {
+    vector<int> result;
     for (const auto& value : values) {
-        if (issame(value, boost::any()) && value.type() == typeid(int)) {
-            result.push_back(boost::any_cast<int>(value));
+        if (any_cast<int>(value)) {
+            result.push_back(any_cast<int>(value));
         }
     }
     return result;
