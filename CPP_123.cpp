536e35d111c8f64b466d677fa227b87edafba0bb
beep boop <githubbot@vadim.me>
2024-07-19 23:27:18 +0200

    Solution 14 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v06y9f55 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ a7f50cef-04f1-4d52-b028-285fbae90f65.cpp:12:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <deque>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-deque<int> get_odd_collatz(int n) {
-    deque<int> result;
+vector<int> get_odd_collatz(int n) {
+    vector<int> result;
     while (n != 1) {
         if (n % 2 == 0) {
             result.push_back(n);
@@ -23,9 +23,20 @@ int main() {
     int n;
     cout << "Enter a positive integer: ";
     cin >> n;
-    deque<int> res = get_odd_collatz(n);
-    for (int i : res) {
-        cout << i << " ";
+    vector<int> res(1); // Initialize with capacity of 1
+    auto it = res.begin();
+    while (n != 1) {
+        if (n % 2 == 0) {
+            *it++ = n;
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+            *it++ = n;
+        }
+    }
+    *it++ = 1; // Add the final 1
+    for (int i = 0; i < res.size(); i++) {
+        cout << res[i] << " ";
     }
     cout << endl;
     return 0;
