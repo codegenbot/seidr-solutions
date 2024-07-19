705fbc6fba3a84e80a4a1685ea2abb01cdf4224d
beep boop <githubbot@vadim.me>
2024-07-20 00:17:13 +0200

    Solution 25 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4bqexe7l Prompt:       |                    ^~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:16: 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp: In function ‘int main()’: 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,30 +2,13 @@
 #include <cassert>
 
 bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
-} 
-
-bool issame(vector<int> a, vector<int> b);
-
-vector<int> strange_sort_list(vector<int> lst){
-    sort(lst.begin(), lst.end());
-    vector<int> result;
-    int left = 0, right = lst.size() - 1;
-    while (left <= right) {
-        result.push_back(lst[left]);
-        if (left != right) {
-            result.push_back(lst[right]);
-        }
-        left++;
-        right--;
-    }
-    return result;
 }
