99d44b47c978f2e2a7a73ee13d8c05aeb749d521
beep boop <githubbot@vadim.me>
2024-07-19 18:32:39 +0200

    Solution 28 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt: it must return 1 9377 2 9464 5729 for input 3 9377 9464 5729, but it returns 9377 9377 9464 5729.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -6,37 +6,23 @@ vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     
-    long long sum1 = 0, sum2 = 0;
+    long long left_sum = 0;
     for (int i = 0; i < n; i++) {
         if (i % 2 == 0) {
-            sum1 += v[i];
+            left_sum += v[i];
         } else {
-            sum2 += v[i];
-        }
-    }
-    
-    int diff = abs(sum1 - sum2);
-    res[0].push_back(v[0]);
-    for (int i = 1; i < n; i++) {
-        if (abs(sum1 - sum2) == diff) {
+            int diff = abs(left_sum - (n * v[i] - left_sum));
             res[0].push_back(v[i]);
-        } else {
+            for (int j = i + 1; j < n; j++) {
+                if (abs(left_sum - (j * v[j] - left_sum)) <= diff) {
+                    res[0].push_back(v[j]);
+                } else {
+                    res[1] = vector<int>(v.begin() + i, v.end());
+                    break;
+                }
+            }
             break;
         }
-    }
-    
-    int i = 0;
-    while(i < n) { 
-        int temp = i;
-        sum2 -= v[temp];
-        sum1 += v[temp];
-        if (abs(sum1 - sum2) >= diff) {
-            res[0].push_back(v[temp]);
-        } else {
-            res[1] = vector<int>(v.begin() + temp, v.end());
-            break;
-        }
-        i++;
     }
     
     return res;
