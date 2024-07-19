fcbae544ef1ca8fc535ca32d6280c1252e02659c
beep boop <githubbot@vadim.me>
2024-07-19 11:27:57 +0200

    Solution 94 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/qlb3aq2h Prompt:       |                              ^~~~~~ ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp:21: ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp: In function ‘int main()’: ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp:31:20: error: ‘split_words’ was not dec lared in this scope    31 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ ab203164-a07f-4ce3-b78c-ec1d58246c23.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,6 +1,31 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b){
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+}
+
+std::vector<std::string> split_words(std::string txt){
+    std::vector<std::string> result;
+    std::string word = "";
+    for(char c : txt){
+        if(c == ' ' || c == ','){
+            result.push_back(word);
+            word = "";
+        } else {
+            word += c;
+        }
+    }
+    if(word != ""){
+        result.push_back(word);
+    }
+    if(result.size() == 1 && result[0].find_first_not_of("abcdefghijklmnopqrstuvwxyz") == std::string::npos){
+        result = {std::to_string(count_if(result[0].begin(), result[0].end(), [](char c){ return c >= 'a' && c <= 'z' && (c - 'a') % 2 == 1; } ))};
+    }
+    return result;
+}
+
+int main(){
+    assert(issame(split_words(""), {"0"}));
 }
