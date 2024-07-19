63b529adb6a980c946609ba8d0983e68f75c383f
beep boop <githubbot@vadim.me>
2024-07-19 07:12:09 +0200

    Solution 81 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qzfx4whg Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,6 +1,6 @@
-double sum = 0;
+long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && fmod(num, 1) == 0 && ((int)num) % 2 != 0) {
+        if (num > 0 && fmod(num, 1) == 0 && ((int)num % 2 != 0)) {
             sum += num * num;
         }
     }
