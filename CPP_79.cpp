f50245bf3300e8f632120974b79336fffe37733a
beep boop <githubbot@vadim.me>
2024-07-19 04:25:16 +0200

    Solution 6 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/slsrb0vh Prompt: Complete the following code given the task description and function signature.

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,7 +1,13 @@
-string binary = "db";
-    while(decimal > 0){
-        binary = to_string(decimal % 2) + binary;
-        decimal /= 2;
+string decimal_to_binary(int decimal){
+    string binary = "db";
+    if(decimal == 0){
+        binary += "0";
+    } else{
+        while(decimal > 0){
+            binary = ((decimal & 1) ? '1' : '0') + binary;
+            decimal >>= 1;
+        }
     }
-    return binary + "db";
+    binary += "db";
+    return binary;
 }
