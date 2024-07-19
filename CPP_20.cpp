208c129e7e89034d387c6cd864b97cf23f980420
beep boop <githubbot@vadim.me>
2024-07-19 20:53:41 +0200

    Solution 21 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/uli69vac Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d07d5fa4-54cd-4550-89db-50daf03c661d.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d07d5fa4-54cd-4550-89db-50daf03c661d.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d07d5fa4-54cd-4550-89db-50daf03c661d.cpp:13: d07d5fa4-54cd-4550-89db-50daf03c661d.cpp: In function ‘int main()’: d07d5fa4-54cd-4550-89db-50daf03c661d.cpp:23:20: error: ‘find_closest_elements’ w as not declared in this scope    23 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ d07d5fa4-54cd-4550-89db-50daf03c661d.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -2,10 +2,6 @@ pair<float, float> find_closest_elements(vector<float> numbers);
 
 bool issame(vector<float> a, vector<float> b);
 
-int main() {
-    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1});
-}
-
 pair<float, float> find_closest_elements(vector<float> numbers) {
     sort(numbers.begin(), numbers.end());
     float min_diff = numbers[1] - numbers[0];
@@ -17,4 +13,9 @@ pair<float, float> find_closest_elements(vector<float> numbers) {
         }
     }
     return {closest_pair.first, closest_pair.second};
+}
+
+int main() {
+    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1});
+    return 0;
 }
