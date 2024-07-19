31ad6bc6e4aadcfbf55e8f35c8b00a1d301ba960
beep boop <githubbot@vadim.me>
2024-07-19 05:21:47 +0200

    Solution 90 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9xmcdlsf Prompt: 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst){       |                                 ^~~~~~ 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst){       |                                        ^~~~~ In file included from 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp:11: 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp: In function ‘int main()’: 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 30c3b486-4f7d-4e11-a1fc-685a97f0991c.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,7 +1,7 @@
 #include <vector>
-#include <cassert>
+using namespace std;
 
-long long double_the_difference(std::vector<float> lst){
+long long double_the_difference(vector<float>& lst){
     long long sum = 0;
     for (float num : lst) {
         if (num > 0 && num == (int)num && (int)num % 2 != 0) {
@@ -11,9 +11,8 @@ long long double_the_difference(std::vector<float> lst){
     return sum;
 }
 
-int main(){
-    std::vector<float> lst = {1.0, 2.0, 3.0};
+int main() {
+    vector<float> lst = {1.0, 2.0, 3.0, 4.0, 5.0};
     long long odd_sum = double_the_difference(lst);
-    assert(odd_sum == 1);
     return 0;
 }
