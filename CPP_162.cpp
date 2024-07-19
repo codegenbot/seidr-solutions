e66980be2408b55d333a77678fae342bf2c4b74c
beep boop <githubbot@vadim.me>
2024-07-20 01:40:06 +0200

    Solution 51 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kywbzhfz Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 5cedf340-9f10-43b5-951d-bd1daebfa4a9.cpp:3: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 5cedf340-9f10-43b5-951d-bd1daebfa4a9.cpp:12:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    12 |     MD5_Final(result, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~~~~ In file included from 5cedf340-9f10-43b5-951d-bd1daebfa4a9.cpp:3: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/cch3QIoT.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 5cedf340-9f10-43b5-951d-bd1daebfa4a9.cpp:(.text+0x38): undefined reference to `M D5_Init' /usr/bin/ld: 5cedf340-9f10-43b5-951d-bd1daebfa4a9.cpp:(.text+0x71): undefined re ference to `MD5_Update' /usr/bin/ld: 5cedf340-9f10-43b5-951d-bd1daebfa4a9.cpp:(.text+0x87): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,19 +1,22 @@
-#include <string>
-
+#include <openssl/ssl.h>
+#define OPENSSL_API_COMPAT 0x10100
 extern "C" {
-    #include <openssl/ssl.h>
     #include <openssl/md5.h>
 }
+#include <string>
 
 std::string string_to_md5(const std::string& input) {
     MD5_CTX ctx;
     unsigned char result[16];
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, input.c_str(), input.size());
-    MD5_Final(result, &ctx);
+    MD5_CTX md5ctx;
+    MD5_Update(&md5ctx, input.c_str(), input.size());
+    unsigned char* ptr = (unsigned char*)malloc(16);
+    MD5_Final(ptr, &md5ctx);
+    free(ptr);
 
     char output[33];
     for (int i = 0; i < 16; i++) {
-        sprintf(output + i*2, "%02x", result[i]);
+        sprintf(output + i*2, "%02x", ptr[i]);
     }
     return std::string(output);
+}
