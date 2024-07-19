2442b497f2942d84d3d4d0001daf6fe5f8f10cd4
beep boop <githubbot@vadim.me>
2024-07-19 02:34:36 +0200

    Solution 5 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dt19mrgq Prompt: 493457b0-99f0-4745-bbaa-a7f2375ab197.cpp: In function ‘int main()’: 493457b0-99f0-4745-bbaa-a7f2375ab197.cpp:24:5: error: ‘assert’ was not declared in this scope    24 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |     ^~~~~~ 493457b0-99f0-4745-bbaa-a7f2375ab197.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <climits>   +++ |+#include <cassert>     5 | 493457b0-99f0-4745-bbaa-a7f2375ab197.cpp: At global scope: 493457b0-99f0-4745-bbaa-a7f2375ab197.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 493457b0-99f0-4745-bbaa-a7f2375ab197.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 #include <cassert>
 
 using namespace std;
 
-int next_smallest(vector<int>& lst) {
+int next_smallest(vector<int> lst) {
     sort(lst.begin(), lst.end());
     int count = 0, prev = INT_MIN;
     for (auto num : lst) {
