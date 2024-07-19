ebc6122451839602e5d0504775a905a286149a11
beep boop <githubbot@vadim.me>
2024-07-19 23:54:05 +0200

    Solution 49 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v06y9f55 Prompt:       |                    ^~~ a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp:6: a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp: In function ‘int main()’: a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp:16:20: error: ‘get_odd_collatz’ was not  declared in this scope    16 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ a3773f7c-cdb5-44f9-a734-7ce3c6e10ec3.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,29 +1,25 @@
+#include <iostream>
 #include <vector>
-#include <assert.h>
-#include <initializer_list>
 
 using namespace std;
 
-vector<pair<int, int>> get_collatz(int n) {
-    vector<pair<int, int>> result;
-    while(n!=1){
-        if(n%2==0)n=n/2;
-        else n=3*n+1;
-        result.push_back({n, 1});
-    }
-    return result;
+bool same(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
-bool same(vector<pair<int, int>> a, vector<pair<int, int>> b) {
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i].first!=b[i].first || a[i].second!=b[i].second)return false;
+int get_odd_collatz(int n) {
+    vector<int> sequence = {n};
+    while (n != 1) {
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = 3 * n + 1;
+        sequence.push_back(n);
     }
-    return true;
-
+    return sequence.size();
 }
 
-int testMain() {
-    assert(same(get_collatz(1), {{1, 1}}));
+int main() {
+    assert(same({get_odd_collatz(1)}, {1}));
     return 0;
 }
