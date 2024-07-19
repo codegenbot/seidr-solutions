ca929bdba13afc035d8a22ee6ee6b1e32b22b523
beep boop <githubbot@vadim.me>
2024-07-19 13:42:22 +0200

    Solution 77 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6dh2sbiq Prompt: d1cec400-183a-45da-bcab-eb7596f2cae3.cpp: In function ‘int main()’: d1cec400-183a-45da-bcab-eb7596f2cae3.cpp:34:39: error: cannot bind non-const lva lue reference of type ‘std::vector<std::__cxx11::basic_string<char> >&’ to an rv alue of type ‘std::vector<std::__cxx11::basic_string<char> >’    34 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from d1cec400-183a-45da-bcab-eb7596f2cae3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<s td::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::allocator_type = st d::allocator<std::__cxx11::basic_string<char> >]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ d1cec400-183a-45da-bcab-eb7596f2cae3.cpp:9:52: note:   initializing argument 1 o f ‘void filter_by_substring(std::vector<std::__cxx11::basic_string<char> >&, con st string&)’     9 | void filter_by_substring(std::vector<std::string>& vec, const std::strin g& substr) {       |                          ~~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,7 +1,7 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return std::equal(a.begin(), a.end(), b.begin(), b.end());
+void filter_by_substring(const std::vector<std::string>& vec, const std::string& substr) {
+    std::vector<std::string> temp_vec = vec;
+    temp_vec.erase(std::remove_if(temp_vec.begin(), temp_vec.end(), [substr](const std::string& s) {
+        return s.find(substr) != std::string::npos;
+    }), temp_vec.end());
+    vec = temp_vec;
 }
