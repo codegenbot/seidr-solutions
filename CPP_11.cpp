3fc73e3b252836e9641cc59bbd55a345ecb65dc5
beep boop <githubbot@vadim.me>
2024-07-20 01:23:06 +0200

    Solution 0 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jpnk0www Prompt: Complete the following code given the task description and function signature.

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,7 +1,5 @@
-#include <string>
-
-std::string string_xor(std::string a, std::string b) {
-    std::string result = "";
+string string_xor(string a, string b) {
+    string result = "";
     for(int i=0; i<a.length(); i++) {
         if(a[i] == '1' && b[i] == '1' || a[i] == '0' && b[i] == '0')
             result += '0';
