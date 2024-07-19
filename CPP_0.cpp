702c67a8848b2e30b184f10f24f6806e0a6c2ca2
beep boop <githubbot@vadim.me>
2024-07-19 21:04:44 +0200

    Solution 12 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/664ehzue Prompt: 1a278622-df73-46a5-b603-49023ee05891.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(void){       |     ^~~~ 1a278622-df73-46a5-b603-49023ee05891.cpp: In function ‘int main()’: 1a278622-df73-46a5-b603-49023ee05891.cpp:27:5: error: ‘vector’ was not declared in this scope    27 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 1a278622-df73-46a5-b603-49023ee05891.cpp:27:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1a278622-df73-46a5-b603-49023ee05891.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1a278622-df73-46a5-b603-49023ee05891.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1a278622-df73-46a5-b603-49023ee05891.cpp:27:12: error: expected primary-expressi on before ‘float’    27 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <cmath>
 
-bool has_close_elements(vector<float> numbers, float threshold);
-
-int main(void) {
-    using namespace std;
-    vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-    if(has_close_elements(a, 0.1f)) {
-        cout << "The vector contains close elements." << endl;
-    } else {
-        cout << "The vector does not contain close elements." << endl;
-    }
-}
-
-bool has_close_elements(vector<float> numbers, float threshold) {
+bool has_close_elements(std::vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
-            if(abs(numbers[i] - numbers[j]) <= threshold)
+            if(std::abs(numbers[i] - numbers[j]) <= threshold)
                 return true;
         }
     }
     return false;
+
+}
+
+int main(void){
+    using namespace std;
+    vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
+    if(has_close_elements(a, 0.1f)){
+        cout << "The vector contains close elements." << endl;
+    } else {
+        cout << "The vector does not contain close elements." << endl;
+    }
 }
