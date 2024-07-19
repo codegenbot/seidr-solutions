f027fb3fa3b526ea3a66247b7eefecbf987ed715
beep boop <githubbot@vadim.me>
2024-07-19 07:05:07 +0200

    Solution 86 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/riusp3s9 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,22 +1,4 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    int num1 = boost::any_cast<int>(a);
-    int num2 = boost::any_cast<int>(b);
-    if(num1 > num2) return num1;
-    if(num2 > num1) return num2;
-}
-if(a.type() == typeid(float) && b.type() == typeid(float)){
-    float num1 = boost::any_cast<float>(a);
-    float num2 = boost::any_cast<float>(b);
-    if(num1 > num2) return num1;
-    if(num2 > num1) return num2;
-}
-if(a.type() == typeid(string) && b.type() == typeid(string)){
-    string str1 = boost::any_cast<string>(a);
-    string str2 = boost::any_cast<string>(b);
-    float num1 = stof(str1.replace(str1.find(','), 1, "."));
-    float num2 = stof(str2.replace(str2.find(','), 1, "."));
-    if(num1 > num2) return str1;
-    if(num2 > num1) return str2;
-}
-return "None";
+if(a < b) return b;
+else if(a > b) return a;
+else return "None";
 }
