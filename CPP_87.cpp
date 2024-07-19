9fe70c4a646eb5b93fabdc54affaad8f5b88ea7c
beep boop <githubbot@vadim.me>
2024-07-20 00:39:31 +0200

    Solution 76 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/72ots2es Prompt:       |                                   ^~~~~~ b4f02732-c777-4437-a87a-f30e9669a444.cpp:21:42: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ b4f02732-c777-4437-a87a-f30e9669a444.cpp:21:49: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ b4f02732-c777-4437-a87a-f30e9669a444.cpp:21:56: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from b4f02732-c777-4437-a87a-f30e9669a444.cpp:20: b4f02732-c777-4437-a87a-f30e9669a444.cpp: In function ‘int main()’: b4f02732-c777-4437-a87a-f30e9669a444.cpp:33:20: error: ‘get_row’ was not declare d in this scope    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ b4f02732-c777-4437-a87a-f30e9669a444.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -22,15 +22,9 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) 
 }
 
 int main() {
-    // Example usage of get_row function
     std::vector<std::vector<int>> lst = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int x = 5;
     std::vector<std::vector<int>> result = get_row(lst, x);
-
-    // Example usage of issame function
-    std::vector<std::vector<int>> a = {{1, 2}, {3, 4}};
-    std::vector<std::vector<int>> b = {{1, 2}, {3, 4}};
-    bool areSame = issame(a, b);
-
+    
     return 0;
 }
