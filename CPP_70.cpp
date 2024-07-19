c5cbe7de56294cf11d6a01ac10e03425f7eabcef
beep boop <githubbot@vadim.me>
2024-07-19 02:04:09 +0200

    Solution 20 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7jpulwa4 Prompt:       |                    ^~~ 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp:16: 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp: In function ‘int main()’: 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 4907e530-c5f6-4cba-93e6-4b819d2271c0.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,17 +1,28 @@
 #include <vector>
-#include <algorithm>
 
-vector<int> completeCode(vector<int>& lst) {
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> strange_sort_list(vector<int> lst){
+    vector<int> result;
     sort(lst.begin(), lst.end());
-    vector<int> res;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
-        res.push_back(lst[left]);
+        result.push_back(lst[left]);
         if (left != right) {
-            res.push_back(lst[right]);
+            result.push_back(lst[right]);
         }
         left++;
         right--;
     }
-    return res;
+    return result;
 }
