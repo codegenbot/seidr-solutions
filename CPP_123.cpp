63e6789bdc8d99d8117d60071a4384896b902969
beep boop <githubbot@vadim.me>
2024-07-19 23:52:11 +0200

    Solution 47 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v06y9f55 Prompt: In file included from 6d071abb-bc45-49c3-8632-6e6ead636897.cpp:31: 6d071abb-bc45-49c3-8632-6e6ead636897.cpp: In function ‘int main()’: 6d071abb-bc45-49c3-8632-6e6ead636897.cpp:41:20: error: ‘get_odd_collatz’ was not  declared in this scope; did you mean ‘get_collatz’?    41 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,28 +1,4 @@
-#include <vector>
-#include <assert.h>
-#include <initializer_list>
-
-using namespace std;
-
-vector<pair<int, int>> collatz(int n) {
-    vector<pair<int, int>> result;
-    while(n!=1){
-        if(n%2==0)n=n/2;
-        else n=3*n+1;
-        result.push_back({n, 1});
-    }
-    return result;
-}
-
-bool same(vector<pair<int, int>> a, vector<pair<int, int>> b) {
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i].first!=b[i].first || a[i].second!=b[i].second)return false;
-    }
-    return true;
-
-}
-
-int main() {
-    assert(same(collatz(1), {{1, 1}}));
+int testMain() {
+    assert(same(get_collatz(1), {{1, 1}}));
+    return 0;
 }
