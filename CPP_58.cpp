d7d0359c0ea13504b9c6b90f2f89de8a0f109d59
beep boop <githubbot@vadim.me>
2024-07-19 07:43:45 +0200

    Solution 38 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lyaescyd Prompt:       |                    ^~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:9: 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp: In function ‘int main()’: 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,7 +5,7 @@ bool issame(vector<int> a, vector<int> b) {
     // Add your logic here
 }
 
-vector<int> common(vector<int>& l1, vector<int>& l2) {
+vector<int> common(vector<int> l1, vector<int> l2) {
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
     vector<int> result;
