028bd217dfcaede82950783cfc8910b37d6ab2ac
beep boop <githubbot@vadim.me>
2024-07-19 11:14:04 +0200

    Solution 8 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/o50evrff Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,30 +1,23 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
         return a;
-    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
         return b;
     } else {
         return "None";
     }
-} else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
         return a;
-    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
         return b;
     } else {
         return "None";
     }
-} else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    float num1, num2;
-    string str_a = boost::any_cast<string>(a);
-    string str_b = boost::any_cast<string>(b);
-    replace(str_a.begin(), str_a.end(), ',', '.');
-    replace(str_b.begin(), str_b.end(), ',', '.');
-    num1 = stof(str_a);
-    num2 = stof(str_b);
-    if(num1 > num2){
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
         return a;
-    } else if(num1 < num2){
+    } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
         return b;
     } else {
         return "None";
