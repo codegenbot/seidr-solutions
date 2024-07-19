485059094b77059b7b532113d62a2d76d016c5b5
beep boop <githubbot@vadim.me>
2024-07-19 19:01:10 +0200

    Solution 50 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ulptsl46 Prompt: 91886208-efdd-4291-8479-39c18dcb4a21.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 91886208-efdd-4291-8479-39c18dcb4a21.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -2,19 +2,28 @@
 #include <vector>
 using namespace std;
 
-int search(const vector<int>& lst) {
-    int max = lst[0];
-    for (int i = 1; i < lst.size(); ++i) {
-        if (lst[i] > max)
-            max = lst[i];
+int count(int num) {
+    int res = 0;
+    for (int i : {1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+        if (i == num) res++;
     }
-    return max;
+    return res;
+}
+
+int search(vector<int> lst) {
+    int max = 0;
+    for (int i : lst) {
+        if (i > 0 && i >= count(i)) {
+            max = i;
+        }
+    }
+    return max == 0 ? -1 : max;
 }
 
 int main() {
-    vector<int> lst(5); 
+    vector<int> lst; 
     for (int i = 0; i < 5; ++i) { 
-        cin >> lst[i]; 
+        int temp; cin >> temp; lst.push_back(temp); 
     }
     int result = search(lst);
     cout << "The largest number in the list is: " << result << endl;
