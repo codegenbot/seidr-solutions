bfe3b6900bb208fd5eeb47a6017253df2c8bf506
beep boop <githubbot@vadim.me>
2024-07-20 01:45:11 +0200

    Solution 22 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ut1u9la1 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 69b37177-e6e3-4919-86ff-c265357f85c4.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 69b37177-e6e3-4919-86ff-c265357f85c4.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 69b37177-e6e3-4919-86ff-c265357f85c4.cpp:10: 69b37177-e6e3-4919-86ff-c265357f85c4.cpp: In function ‘int main()’: 69b37177-e6e3-4919-86ff-c265357f85c4.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 69b37177-e6e3-4919-86ff-c265357f85c4.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,13 +1,7 @@
-bool issame(vector<string> a, vector<string>b){
+bool issame(vector<string> a,vector<string>b){
     for(auto s : b){
-        if(find(a.begin(), a.end(), s) == a.end()){
+        if(find(a.begin(),a.end(),s) == a.end())
             return false;
-        }
     }
     return true;
-}
-
-int main() {
-    assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
-    return 0;
 }
