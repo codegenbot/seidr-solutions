47e1ee6e63cf330d89e59f5f613638b72c32785e
beep boop <githubbot@vadim.me>
2024-07-19 10:35:00 +0200

    Solution 26 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/a67dhc01 Prompt:       |               ^~~~~ a17840bc-19da-41f6-89be-7688f59d4b24.cpp:12:5: error: ‘string’ does not name a t ype    12 |     string binary_avg = "";       |     ^~~~~~ a17840bc-19da-41f6-89be-7688f59d4b24.cpp:13:5: error: expected unqualified-id be fore ‘while’    13 |     while (avg > 0) {       |     ^~~~~ a17840bc-19da-41f6-89be-7688f59d4b24.cpp:18:5: error: expected unqualified-id be fore ‘return’    18 |     return binary_avg;       |     ^~~~~~ a17840bc-19da-41f6-89be-7688f59d4b24.cpp:19:1: error: expected declaration befor e ‘}’ token    19 | }       | ^ In file included from a17840bc-19da-41f6-89be-7688f59d4b24.cpp:21: a17840bc-19da-41f6-89be-7688f59d4b24.cpp: In function ‘int main()’: a17840bc-19da-41f6-89be-7688f59d4b24.cpp:23:13: error: ‘rounded_avg’ was not dec lared in this scope    23 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-string averageBinaryRepresentation(int n, int m) {
+std::string averageBinaryRepresentation(int n, int m) {
     if (n > m) {
         return "-1";
     }
@@ -12,11 +12,22 @@ string averageBinaryRepresentation(int n, int m) {
     
     int avg = round((double)sum / (m - n + 1));
     
-    string binary_avg = "";
+    std::string binary_avg = "";
     while (avg > 0) {
         binary_avg = to_string(avg % 2) + binary_avg;
         avg /= 2;
     }
     
     return binary_avg;
+}
+
+int main() {
+    // Input n, m
+    int n, m;
+    std::cin >> n >> m;
+    
+    // Output result of the averageBinaryRepresentation function
+    std::cout << averageBinaryRepresentation(n, m);
+    
+    return 0;
 }
