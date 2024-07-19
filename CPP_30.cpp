dbac8326edf35d5cdbc1045c0a61c64807d87ae5
beep boop <githubbot@vadim.me>
2024-07-19 22:18:18 +0200

    Solution 63 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eoo9avp7 Prompt: me(std::vector<float>, std::vector<float>)’    48 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 0f1006d3-8783-4807-8f5a-a840bbdf75d3.cpp:35:6: note: ‘bool issame(std::vector<fl oat>, std::vector<float>)’ previously defined here    35 | bool issame(std::vector<float> a, std::vector<float> b) {       |      ^~~~~~ 0f1006d3-8783-4807-8f5a-a840bbdf75d3.cpp:56:5: error: redefinition of ‘int main( )’    56 | int main(){       |     ^~~~ 0f1006d3-8783-4807-8f5a-a840bbdf75d3.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 0f1006d3-8783-4807-8f5a-a840bbdf75d3.cpp:47: 0f1006d3-8783-4807-8f5a-a840bbdf75d3.cpp: In function ‘int main()’: 0f1006d3-8783-4807-8f5a-a840bbdf75d3.cpp:57:32: error: could not convert ‘get_po sitive(0.0f)’ from ‘bool’ to ‘std::vector<float>’    57 |     assert (issame(get_positive({}) , {}));       |                    ~~~~~~~~~~~~^~~~       |                                |       |                                bool

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool issame(std::vector<float> a, std::vector<float> b) {
+bool issame(vector<float> a, vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -15,6 +15,23 @@ bool issame(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-float get_positive(float x) {
-    return x > 0 ? x : 0.0f;
+int main() {
+    vector<float> a;
+    float num;
+
+    cout << "Enter the numbers: ";
+    while (cin >> num) {
+        a.push_back(num);
+    }
+
+    vector<float> b(a);
+    reverse(b.begin(), b.end());
+
+    if (issame(a, b)) {
+        cout << "The vectors are same." << endl;
+    } else {
+        cout << "The vectors are not same." << endl;
+    }
+
+    return 0;
 }
