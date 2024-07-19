47f0deca8791285bff10552576288410af0a72c7
beep boop <githubbot@vadim.me>
2024-07-19 20:33:13 +0200

    Solution 3 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pkupi6nj Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,20 +1,31 @@
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<boost/any.hpp>
+using namespace std;
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)b > (int)a ? b : a;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (float)a > (float)b ? a : b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return a.convert<string>().compare(b.convert<string>()) > 0 ? a : b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return (int)stoi(b.convert<string>()) > (int)a ? b : a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        return (int)stoi(a.convert<string>()) > (int)b ? a : b;
-    }
-    else {
-        return "None";
+        return (int)a > (float)b ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        string str = any_cast<string>(b);
+        int num = any_cast<int>(a);
+        return num > stoi(str) ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = any_cast<string>(b);
+        float num = any_cast<float>(a);
+        return num > stof(str) ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        int num = any_cast<int>(b);
+        string str = any_cast<string>(a);
+        return stoi(str) > num ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        float num = any_cast<float>(b);
+        string str = any_cast<string>(a);
+        return stof(str) > num ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int num1 = any_cast<int>(a), num2 = any_cast<int>(b);
+        return num1 > num2 ? a : b;
+    } else {
+        return boost::any("None");
     }
 }
