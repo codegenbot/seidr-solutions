710542ba3f239f0a0451722e03b3d196f41d7e59
beep boop <githubbot@vadim.me>
2024-07-19 02:23:19 +0200

    Solution 88 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kk1vwmeu Prompt:                  from 737b5770-6742-4fb6-9d10-9eea423b73b0.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 737b5770-6742-4fb6-9d10-9eea423b73b0.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 737b5770-6742-4fb6-9d10-9eea423b73b0.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 737b5770-6742-4fb6-9d10-9eea423b73b0.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 737b5770-6742-4fb6-9d10-9eea423b73b0.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <vector>
 #include <cassert>
-#include <set>
 
 std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
     std::sort(l1.begin(), l1.end());
