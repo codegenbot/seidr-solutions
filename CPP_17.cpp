358248d444b61d0573a03bdc3acb2e75b59d18c5
beep boop <githubbot@vadim.me>
2024-07-19 17:13:28 +0200

    Solution 20 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/n5npytfg Prompt:       |                           ^~~~~~ 56889754-cccb-44db-95ec-ebe286a3ce26.cpp:32:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 56889754-cccb-44db-95ec-ebe286a3ce26.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 56889754-cccb-44db-95ec-ebe286a3ce26.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 56889754-cccb-44db-95ec-ebe286a3ce26.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 56889754-cccb-44db-95ec-ebe286a3ce26.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 56889754-cccb-44db-95ec-ebe286a3ce26.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -19,6 +19,8 @@ std::vector<int> parse_music(std::string music_string) {
         } else if (music_string[i] == 'o') {
             beats.push_back(4);
             i++;
+        } else {
+            i++;
         }
     }
     return beats;
