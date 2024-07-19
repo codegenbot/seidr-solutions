fcff027429621180dad77ad1582fc6019228790f
beep boop <githubbot@vadim.me>
2024-07-19 21:40:24 +0200

    Solution 13 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/eh3abzqr Prompt: 6b3c8adc-66a3-499f-b461-e716d5c6aa0b.cpp:1:9: error: ‘vector’ was not declared i n this scope     1 | int add(vector<int> lst){       |         ^~~~~~ 6b3c8adc-66a3-499f-b461-e716d5c6aa0b.cpp:1:16: error: expected primary-expressio n before ‘int’     1 | int add(vector<int> lst){       |                ^~~ In file included from 6b3c8adc-66a3-499f-b461-e716d5c6aa0b.cpp:11: 6b3c8adc-66a3-499f-b461-e716d5c6aa0b.cpp: In function ‘int main()’: 6b3c8adc-66a3-499f-b461-e716d5c6aa0b.cpp:13:16: error: ‘add’ cannot be used as a  function    13 |     assert (add({4, 4, 6, 8}) == 12);       |             ~~~^~~~~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -2,7 +2,7 @@
 
 int add(vector<int> lst){
     int sum = 0;
-    for (int i = 0; i < lst.size(); ++i) {
+    for (int i = 1; i < lst.size(); i += 2) {
         if (lst[i] % 2 == 0) {
             sum += lst[i];
         }
