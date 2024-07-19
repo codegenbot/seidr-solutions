5c33a51942faab7c7ed0c0be6996be7892feaa53
beep boop <githubbot@vadim.me>
2024-07-19 02:04:11 +0200

    Solution 8 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/hxr8gn9v Prompt: 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp: In function ‘int main()’: 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp:44:29: error: too few arguments to func tion ‘double find_zero(std::vector<double>, std::vector<double>)’    44 |         solution = find_zero(coeffs);       |                    ~~~~~~~~~^~~~~~~~ 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp:14:8: note: declared here    14 | double find_zero(vector<double> xs, vector<double> coeff){       |        ^~~~~~~~~ In file included from 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp:28: 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp:45:26: error: cannot convert ‘std::vect or<double>’ to ‘double’    45 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                          ^~~~~~       |                          |       |                          std::vector<double> 83e5e4a9-3b46-47d3-a5b8-2e5b3da94cab.cpp:6:20: note:   initializing argument 1 o f ‘double poly(double, std::vector<double>)’     6 | double poly(double x, vector<double> coeff){       |             ~~~~~~~^

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -3,7 +3,7 @@
 #include <cassert>
 using namespace std;
 
-double poly(double x, const vector<double>& coeff){
+double poly(double x, vector<double>& coeff){
     double result = 0.0;
     for(int i = 0; i < coeff.size(); i++){
         result += coeff[i] * pow(x, i);
@@ -11,7 +11,7 @@ double poly(double x, const vector<double>& coeff){
     return result;
 }
 
-double find_zero(const vector<double>& xs, const vector<double>& coeff){
+double find_zero(vector<double>& xs, vector<double>& coeff){
     double a = poly(xs[0], coeff);
     double b = poly(xs[1], coeff);
     return -b/a;
