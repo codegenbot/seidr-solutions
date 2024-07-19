263424d1e0ae339dd98c78a602116785be71452a
beep boop <githubbot@vadim.me>
2024-07-20 00:17:05 +0200

    Solution 24 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4bqexe7l Prompt:       |                    ^~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:16: 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp: In function ‘int main()’: 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 24f90c22-e903-47ee-bfbb-b78939ee03a2.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -11,4 +11,21 @@ bool issame(vector<int> a, vector<int> b){
         }
     }
     return true;
+} 
+
+bool issame(vector<int> a, vector<int> b);
+
+vector<int> strange_sort_list(vector<int> lst){
+    sort(lst.begin(), lst.end());
+    vector<int> result;
+    int left = 0, right = lst.size() - 1;
+    while (left <= right) {
+        result.push_back(lst[left]);
+        if (left != right) {
+            result.push_back(lst[right]);
+        }
+        left++;
+        right--;
+    }
+    return result;
 }
