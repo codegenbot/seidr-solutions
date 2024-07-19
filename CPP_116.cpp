f4fbcb59f904f9d6ffbb423a81f64f09c77421d6
beep boop <githubbot@vadim.me>
2024-07-19 02:46:27 +0200

    Solution 9 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/cvur17y8 Prompt:       |      ^~~~~~ e128eadd-56c8-45ab-bc0d-bc485670a250.cpp:6:13: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ e128eadd-56c8-45ab-bc0d-bc485670a250.cpp:6:20: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ e128eadd-56c8-45ab-bc0d-bc485670a250.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e128eadd-56c8-45ab-bc0d-bc485670a250.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from e128eadd-56c8-45ab-bc0d-bc485670a250.cpp:5: e128eadd-56c8-45ab-bc0d-bc485670a250.cpp: In function ‘int main()’: e128eadd-56c8-45ab-bc0d-bc485670a250.cpp:15:20: error: ‘sort_array’ was not decl ared in this scope    15 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,24 +1,15 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
-
-std::vector<int> sort_array(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end(), [](int a, int b) {
-        int count_a = __builtin_popcount(a);
-        int count_b = __builtin_popcount(b);
-        if (count_a == count_b) {
-            return a < b;
-        }
-        return count_a < count_b;
-    });
-    return arr;
-}
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-    return 0;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
