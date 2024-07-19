e54ed00817732f623400f7ebc3da173efb716edf
beep boop <githubbot@vadim.me>
2024-07-19 20:19:12 +0200

    Solution 20 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tukd3xas Prompt:       |                           ^~~~~~ bd417ff8-67b3-43be-a459-30a665360ff2.cpp:28:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bd417ff8-67b3-43be-a459-30a665360ff2.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bd417ff8-67b3-43be-a459-30a665360ff2.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bd417ff8-67b3-43be-a459-30a665360ff2.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bd417ff8-67b3-43be-a459-30a665360ff2.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ bd417ff8-67b3-43be-a459-30a665360ff2.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -9,17 +9,12 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 std::vector<int> order_by_points(std::vector<int> nums) {
-    std::sort(nums.begin(), nums.end(), [](int num, int b) {
+    std::sort(nums.begin(), nums.end(), [](int a, int b) {
         auto sum = [](int num) {
             return std::accumulate(std::to_string(std::abs(num)).begin(), std::to_string(std::abs(num)).end(),
                                     0, [](int a, char b) { return a + (b - '0'); });
         };
-        return sum(num) == sum(b) ? num < b : sum(num) < sum(b);
+        return sum(a) == sum(b) ? a < b : sum(a) < sum(b);
     });
     return nums;
-}
-
-int main() {
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
 }
