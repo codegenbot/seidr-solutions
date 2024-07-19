9ce0abfc16c77ff7fdeba1ac2f533a0530aaa0e5
beep boop <githubbot@vadim.me>
2024-07-19 19:01:32 +0200

    Solution 40 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xf1ve6zq Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c7d45388-0dbf-4453-9b94-008ba9083e22.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c7d45388-0dbf-4453-9b94-008ba9083e22.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c7d45388-0dbf-4453-9b94-008ba9083e22.cpp:7: c7d45388-0dbf-4453-9b94-008ba9083e22.cpp: In function ‘int main()’: c7d45388-0dbf-4453-9b94-008ba9083e22.cpp:17:20: error: ‘unique’ was not declared  in this scope    17 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ c7d45388-0dbf-4453-9b94-008ba9083e22.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,17 +1,10 @@
-```cpp
-#include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+bool issame(vector<int> a,vector<int>b){
+    return (a.size() == b.size()) && (a == b);
 }
 
-auto unique(vector<int>::iterator first, vector<int>::iterator last) {
-    std::vector<int> result(std::unique(first, last), last);
+auto unique_copy(vector<int>::iterator first, vector<int>::iterator last) {
+    std::vector<int> result(std::unique_copy(first, last));
     return result;
-}
-
-int main() {
-    assert(std::issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));
-    return 0;
 }
