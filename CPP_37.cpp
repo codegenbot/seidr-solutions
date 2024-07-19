e655ac31ad4757afe770a3b926525ed46187f3fb
beep boop <githubbot@vadim.me>
2024-07-20 01:50:19 +0200

    Solution 1 of CPP/37, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/95ig7eel Prompt: Complete the following code given the task description and function signature.

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,25 +1,19 @@
+#include <algorithm>
+using namespace std;
+
 vector<float> sort_even(vector<float> l) {
     vector<float> result(l.size());
     for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 1) {
-            result[i] = l[i];
+        if (i % 2 == 0) {
+            vector<float> temp;
+            for (int j = 0; j < l.size(); j++) {
+                if (j % 2 == 0)
+                    temp.push_back(l[j]);
+            }
+            sort(temp.begin(), temp.end());
+            result[i] = temp[0];
         } else {
-            vector<float> evenVals;
-            for (int j = 0; j < l.size(); j++) {
-                if (j % 2 == 0) {
-                    evenVals.push_back(l[j]);
-                }
-            }
-            sort(evenVals.begin(), evenVals.end());
-            int k = 0;
-            for (int j = 0; j < l.size(); j++) {
-                if (j % 2 == 0) {
-                    result[j] = evenVals[k];
-                    k++;
-                } else {
-                    result[j] = l[j];
-                }
-            }
+            result[i] = l[i];
         }
     }
     return result;
