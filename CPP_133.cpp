c99389497fd03f11615994ea4b9521fabf49854e
beep boop <githubbot@vadim.me>
2024-07-20 01:16:14 +0200

    Solution 98 of CPP/133, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bw9nrda5 Prompt: 10b73082-bada-4a3a-b5e1-cb890acee028.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 10b73082-bada-4a3a-b5e1-cb890acee028.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,8 +1,13 @@
-int sum_squares(vector<float> lst){
+#include <iostream>
+#include <cmath>
+#include <initializer_list>
+
+int sum_squares(std::initializer_list<double> lst) {
     int total = 0;
-    for(float num : lst){
-        int ceil_num = ceil(num);
-        total += pow(ceil_num,2);
+    for (double num : lst) {
+        int ceil_num = std::ceil(num);
+        total += std::pow(ceil_num, 2);
     }
     return total;
+
 }
