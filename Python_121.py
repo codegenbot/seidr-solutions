795a1cd3232b255a148ec53129169a9e35c8985c
beep boop <githubbot@vadim.me>
2024-07-19 17:14:59 +0200

    Solution 55 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt: True Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b7b9f5a-c392-475a-9734-b32600a2f be3.py", line 14, in <module>     check(solution)           ^^^^^^^^ NameError: name 'solution' is not defined

--- Python_121.py
+++ Python_121.py
@@ -1,5 +1,5 @@
 def check(lst):
-    return sum(int(d) for d in "".join(map(str, lst)) if int(d) % 2 != 0) == 9
-
+    num_str = ''.join(map(str, lst))
+    return sum(int(d) for d in num_str if int(d) % 2 != 0) == 9
 
 print(check([1, 3, 5]))
