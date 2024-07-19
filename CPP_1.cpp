50cb49c770dc8f19a899759c05654fdf6da8da71
beep boop <githubbot@vadim.me>
2024-07-19 20:33:24 +0200

    Solution 27 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/unfgi8nj Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c4812191-ca0d-4574-93f2-0cb6369770fe.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c4812191-ca0d-4574-93f2-0cb6369770fe.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c4812191-ca0d-4574-93f2-0cb6369770fe.cpp:25: c4812191-ca0d-4574-93f2-0cb6369770fe.cpp: In function ‘int main()’: c4812191-ca0d-4574-93f2-0cb6369770fe.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ c4812191-ca0d-4574-93f2-0cb6369770fe.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,49 +1,23 @@
-#include <iostream>
-#include <vector>
-#include <string>
+vector<string> result;
+string current_group;
+int open_braces = 0;
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-vector<string> splitParenthesesGroups(string paren_string) {
-    vector<string> result;
-    string current_group;
-    int open_braces = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            if (open_braces > 0) {
-                current_group += c;
-            }
-            open_braces++;
-        } else if (c == ')') {
-            open_braces--;
-            if (open_braces > 0) {
-                current_group += c;
-            } else if (open_braces == 0) {
+for (char c : paren_string) {
+    if (c == '(') {
+        if (open_braces > 0) {
+            current_group += c;
+        }
+        open_braces++;
+    } else if (c == ')') {
+        open_braces--;
+        if (open_braces >= 0) {
+            current_group += c;
+            if (open_braces == 0) {
                 result.push_back(current_group);
                 current_group.clear();
             }
         }
     }
-
-    return result;
 }
 
-int main() {
-    string input;
-    cout << "Enter a string with parentheses: ";
-    cin >> input;
-
-    vector<string> output = splitParenthesesGroups(input);
-
-    cout << "Groups within parentheses are: ";
-    for (const auto& group : output) {
-        cout << group << " ";
-    }
-
-    return 0;
-}
+return result;
