08bf9edbc3ecddf3724f7c3d0825b663a57c1247
beep boop <githubbot@vadim.me>
2024-07-19 21:34:18 +0200

    Solution 23 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/yyarycmb Prompt: a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp:17: a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp: In function ‘int main()’: a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp: In function ‘int totalChars(int)’: a433cd7f-9cd6-463e-89f4-bad3b42dc776.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,3 +1,27 @@
-bool issame(vector<string> a, vector<string> b){
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+int totalChars(int numChars) {
+    return numChars;
+}
+
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
+}
+
+vector<string> total_match(vector<string> lst1, vector<string> lst2);
+
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    if (totalChars(lst1.size()) < totalChars(lst2.size())) {
+        return lst1;
+    } else {
+        return totalChars(lst1.size()) == totalChars(lst2.size()) ? lst1 : lst2;
+    }
+}
+
+int main() {
+    assert(issame(total_match({"this"}, {}), {}));
+    return 0;
 }
