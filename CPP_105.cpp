bab7c34cc2248f086f2cf6ce5f5035eed8f8c401
beep boop <githubbot@vadim.me>
2024-07-19 19:21:52 +0200

    Solution 2 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r4qp91p3 Prompt:       |                              ^~~~~~ 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp:25: 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp: In function ‘int main()’: 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 52b107c1-617b-4a5e-81ba-644276e3ad3a.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,24 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cassert>
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
 vector<string> by_length(vector<int> arr){
     vector<string> result;
     map<int, string> numToString = {
         {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
         {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
-
-    vector<int> sortedArr;
+    
+    vector<int> filtered;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            sortedArr.push_back(num);
+            filtered.push_back(num);
         }
     }
-
-    sort(sortedArr.begin(), sortedArr.end());
-
-    reverse(sortedArr.begin(), sortedArr.end());
-
-    for (int num : sortedArr) {
+    
+    sort(filtered.begin(), filtered.end());
+    reverse(filtered.begin(), filtered.end());
+    
+    for (int num : filtered) {
         result.push_back(numToString[num]);
     }
-
+    
     return result;
+}
+
+int main(){
+    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
+    
+    return 0;
 }
