772dc547b5845cf563712cdfae64409fa8d8471f
beep boop <githubbot@vadim.me>
2024-07-19 19:24:41 +0200

    Solution 35 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt: c1d20dcf-77ca-47b0-82a4-0e77ba043adf.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>)’: c1d20dcf-77ca-47b0-82a4-0e77ba043adf.cpp:10:23: error: ‘max_element’ was not dec lared in this scope    10 |     long long diff = *max_element(v.begin(), v.end());       |                       ^~~~~~~~~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,17 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     
     long long sum1 = 0, sum2 = 0;
     for (int i = 0; i < n; i++) { 
-        if (abs(sum1 - sum2) > abs(sum2 - (sum1 + v[i])) || i == n-1) {
-            res[0].insert(res[0].end(), v.begin() + i, v.end());
-            break;
+        if (abs(sum1 - sum2) > 0) {
+            res[0].push_back(v[i]);
+            sum1 += v[i];
+            sum2 = 0;
         } else {
             sum2 += v[i];
         }
     }
     
-    res[1] = vector<int>(v.begin(), res[0].begin());
+    res[1] = vector<int>(v.begin() + res[0].size(), v.end());
+    
     return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    vector<vector<int>> res = cutVector(v);
+    for (auto &v : res) {
+        for (int x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
