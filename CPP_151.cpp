f00775e376f265f83663f00186ab7d36e6627640
beep boop <githubbot@vadim.me>
2024-07-20 00:14:38 +0200

    Solution 3 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zmsfz8na Prompt: /usr/include/x86_64-linux-gnu/bits/mathcalls.h:110:1: note:   initializing argum ent 2 of ‘double modf(double, double*)’   110 | __MATHCALL (modf,, (_Mdouble_ __x, _Mdouble_ *__iptr)) __nonnull ((2));       | ^ 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp: At global scope: 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp:20: 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp: In function ‘int main()’: 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp:22:35: error: ‘lst’ was not declared in  this scope    22 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 497dd8f6-d5ef-4b4d-9102-945c8ebea79a.cpp:22:43: error: ‘odd_sum’ was not declare d in this scope    22 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,6 +1,18 @@
+```
+#include <vector>
+#include <cmath>
+
+long long double_the_difference(std::vector<float> lst) {
+    long long sum = 0;
+    for (float num : lst) {
+        if (num > 0 && modf(num, &num) == 0) {
+            sum += pow(num, 2);
+        }
+    }
+    return sum;
+}
+
 int main() {
-    vector<float> lst = {1.0f, 2.5f, 3.0f, 4.25f, 5.0f};
-    int odd_sum = 0;
-    long long result = double_the_difference(lst);
-    cout << "The sum of squares is: " << result << endl;
+    std::vector<float> lst = {1.0f, 2.0f, 3.0f};
+    long long odd_sum = double_the_difference(lst);
 }
