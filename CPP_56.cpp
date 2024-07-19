35fd1a3aa115471593ec3388f33f2a862e641a2e
beep boop <githubbot@vadim.me>
2024-07-19 21:18:39 +0200

    Solution 7 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/t5hmpgun Prompt: a9431ea7-1c00-4277-8ea9-5128cdb32c88.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for (char bracket : brackets) {       |     ^~~ a9431ea7-1c00-4277-8ea9-5128cdb32c88.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return count == 0;       |     ^~~~~~ a9431ea7-1c00-4277-8ea9-5128cdb32c88.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from a9431ea7-1c00-4277-8ea9-5128cdb32c88.cpp:15: a9431ea7-1c00-4277-8ea9-5128cdb32c88.cpp: In function ‘int main()’: a9431ea7-1c00-4277-8ea9-5128cdb32c88.cpp:17:18: error: ‘correct_bracketing’ was not declared in this scope    17 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,14 +1,3 @@
-bool correct_bracketing(const std::string& brackets) {
-    int count = 0;
-    for (char bracket : brackets) {
-        if (bracket == '<') {
-            count++;
-        } else if (bracket == '>') {
-            count--;
-        }
-        if (count < 0) {
-            return false;
-        }
-    }
-    return count == 0;
-}
+#include <string>
+
+bool correct_bracketing(const std::string& brackets);
