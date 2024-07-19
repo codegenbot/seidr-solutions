d6c3796daaa14150bdec366680165586fd46da79
beep boop <githubbot@vadim.me>
2024-07-19 20:13:38 +0200

    Solution 66 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/svlp1004 Prompt:       |                                     ^~~~~~ 7d54a0c9-1379-4172-b45c-a9ffbfb64f74.cpp:27:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 7d54a0c9-1379-4172-b45c-a9ffbfb64f74.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 7d54a0c9-1379-4172-b45c-a9ffbfb64f74.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 7d54a0c9-1379-4172-b45c-a9ffbfb64f74.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7d54a0c9-1379-4172-b45c-a9ffbfb64f74.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 7d54a0c9-1379-4172-b45c-a9ffbfb64f74.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -2,23 +2,24 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <sstream>
-
-std::vector<std::string> split_words(std::string str) {
-    std::vector<std::string> words;
-    std::istringstream iss(str);
-    std::string word;
-    while (iss >> word) {
-        words.push_back(word);
-    }
-    return words;
-}
 
 bool issame(const std::vector<std::string> &a, const std::vector<std::string> &b) {
     return a == b;
 }
 
+std::vector<std::string> split_words(const std::string& str) {
+    std::vector<std::string> words;
+    
+    size_t start = 0, end = 0;
+    while ((start = str.find_first_not_of(' ', end)) != std::string::npos) {
+        end = str.find(' ', start);
+        words.push_back(str.substr(start, end - start));
+    }
+    
+    return words;
+}
+
 int main() {
-    assert(issame(split_words(""), {"0"}));
+    assert(issame(split_words(""), {"0"})); 
     return 0;
 }
