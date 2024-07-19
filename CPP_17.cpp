6e4f2415224b84331cae9f273223e79924b336d6
beep boop <githubbot@vadim.me>
2024-07-20 01:54:03 +0200

    Solution 5 of CPP/17, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/z6sdz0tn Prompt:    92 | #  define assert(expr) \       | 05bd21e1-a14e-48dc-93ed-e85c2ebfe391.cpp: In function ‘int main()’: 05bd21e1-a14e-48dc-93ed-e85c2ebfe391.cpp:10:5: error: ‘assert’ was not declared in this scope    10 |     assert(parse_music("o| .| o| .| o o| o o|") == vector<int>{2, 1, 2, 1, 4, 2, 4, 2});       |     ^~~~~~ 05bd21e1-a14e-48dc-93ed-e85c2ebfe391.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | 05bd21e1-a14e-48dc-93ed-e85c2ebfe391.cpp: At global scope: 05bd21e1-a14e-48dc-93ed-e85c2ebfe391.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 05bd21e1-a14e-48dc-93ed-e85c2ebfe391.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main(){       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -6,15 +6,6 @@ using namespace std;
 
 vector<int> parse_music(string music_string);
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
-    return 0;
-}
-
 vector<int> parse_music(string music_string){
     vector<int> beats;
     int i = 0;
