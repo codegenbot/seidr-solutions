6d928134a64608d09e9505a213db448a5e56e1be
beep boop <githubbot@vadim.me>
2024-07-20 00:59:35 +0200

    Solution 13 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/u46z59hv Prompt:     6 |     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, " D"}, {400, "CD"},       |                            ^~ 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp:6:31: error: ‘roman’ was not declared i n this scope     6 |     vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, " D"}, {400, "CD"},       |                               ^~~~~ 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp: At global scope: 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp:24: 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp: In function ‘int main()’: 0d87473d-6342-4dad-a7a6-a7f36a5af92d.cpp:26:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    26 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,16 +1,11 @@
-#include <cassert>
-#include <string>
-using namespace std;
-
-vector<pair<int, string>> roman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-                                     {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-                                     {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-
-std::string intToMiniRoman(int number) {
+std::string intToMiniRoman(int num) {
+    vector<pair<int, string>> romanValues = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+                                              {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+                                              {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
     string result = "";
-    for (const auto& p : roman) {
-        while (number >= p.first) {
-            number -= p.first;
+    for (const auto& p : romanValues) {
+        while (num >= p.first) {
+            num -= p.first;
             result += p.second;
         }
     }
