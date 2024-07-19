f82e452b308a561293e565e3026225f651b10cf5
beep boop <githubbot@vadim.me>
2024-07-20 01:31:02 +0200

    Solution 95 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bg00m0r1 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from a4e9793d-4773-4774-88ba-a70648f3d74f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,6 +1,8 @@
+```cpp
+#include <initializer_list>
 #include <algorithm>
 #include <vector>
-#include <iostream>
+#include <utility>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
@@ -14,16 +16,16 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> order_by_points(std::vector<int> nums) {
+std::vector<int> order_by_points(std::initializer_list<int> nums) {
     std::vector<std::pair<int, int>> pairs;
-    for (int i = 0; i < nums.size(); i++) {
+    for (int num : nums) {
         int sumOfDigits = 0;
-        int num = abs(nums[i]);
-        while (num > 0) {
-            sumOfDigits += num % 10;
-            num /= 10;
+        int n = abs(num);
+        while (n > 0) {
+            sumOfDigits += n % 10;
+            n /= 10;
         }
-        pairs.push_back({nums[i], sumOfDigits});
+        pairs.push_back(std::make_pair(num, sumOfDigits));
     }
 
     std::sort(pairs.begin(), pairs.end());
@@ -37,7 +39,7 @@ std::vector<int> order_by_points(std::vector<int> nums) {
 }
 
 int main() {
-    std::vector<int> nums = {0,6,6,-76,-21,23,4};
-    assert(issame(order_by_points(nums), {-76, -21, 0, 4, 23, 6, 6}));
+    std::vector<int> result = order_by_points({0,6,6,-76,-21,23,4});
+    assert(issame(result, {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
