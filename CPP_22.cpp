fb43941ab9ceb9d10cf9906931873daed79928f4
beep boop <githubbot@vadim.me>
2024-07-19 22:00:52 +0200

    Solution 74 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/utcbni4v Prompt: In file included from 86cf3f33-c5bb-4e0e-b287-a97521be2e36.cpp:23: /usr/include/assert.h:361012:56: error: unterminated argument list invoking macr o "assert" 86cf3f33-c5bb-4e0e-b287-a97521be2e36.cpp:21:1: error: ‘assert’ does not name a t ype    21 | assert(issame({1}, filter_integers({{1}, {2}, {3}, {"hello"}, {4}}));       | ^~~~~~ 86cf3f33-c5bb-4e0e-b287-a97521be2e36.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <vector>   +++ |+#include <cassert>     5 | #include <initializer_list>

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,10 +1,5 @@
 #include <cassert>
 
-#include <any>
-#include <list>
-#include <vector>
-#include <initializer_list>
-
 std::vector<int> filter_integers(std::list<std::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
