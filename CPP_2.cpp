5fc56b700ee590c74bf443dfec41a5ec52c6325c
beep boop <githubbot@vadim.me>
2024-07-19 06:18:04 +0200

    Solution 96 of CPP/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9flzretj Prompt: c6076ee1-fd3d-4158-b2c8-86644f2b7e2a.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return number - floor(number);       | ^~~~~~ c6076ee1-fd3d-4158-b2c8-86644f2b7e2a.cpp:2:1: error: expected declaration before  ‘}’ token     2 | }       | ^ In file included from c6076ee1-fd3d-4158-b2c8-86644f2b7e2a.cpp:4: c6076ee1-fd3d-4158-b2c8-86644f2b7e2a.cpp: In function ‘int main()’: c6076ee1-fd3d-4158-b2c8-86644f2b7e2a.cpp:6:17: error: ‘truncate_number’ was not declared in this scope     6 |     assert (abs(truncate_number(123.456) - 0.456) < 1e-4);       |                 ^~~~~~~~~~~~~~~ c6076ee1-fd3d-4158-b2c8-86644f2b7e2a.cpp:6:13: error: ‘abs’ was not declared in this scope     6 |     assert (abs(truncate_number(123.456) - 0.456) < 1e-4);       |             ^~~

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,13 +1,3 @@
-#include <iostream>
-#include <cassert>
-#include <cmath>
-
 double truncate_number(double number) {
     return number - floor(number);
-}
-
-int main() {
-    assert (std::abs(truncate_number(123.456) - 0.456) < 1e-4);
-    
-    return 0;
 }
