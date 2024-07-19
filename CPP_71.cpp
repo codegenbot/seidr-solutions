af9dd56f4a35bffbde0d358822a9085d47b0ce65
beep boop <githubbot@vadim.me>
2024-07-19 18:31:56 +0200

    Solution 20 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/m87khfmf Prompt: eader ‘<iostream>’; did you forget to ‘#include <iostream>’? df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp:20:14: error: ‘cout’ is not a member of  ‘std’    20 |         std::cout << "The area of the triangle is " << triangle_area(a,b ,c);       |              ^~~~ df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp:20:14: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp:22:14: error: ‘cout’ is not a member of  ‘std’    22 |         std::cout << "The sides do not form a valid triangle";       |              ^~~~ df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp:22:14: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp: At global scope: df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ df35cfe0-4cc2-4aca-b92b-40e61b095266.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -12,7 +12,7 @@ float triangle_area(float a, float b, float c) {
 }
 
 int main() {
-    assert(abs(triangle_area(2, 2, 10) +1)<0.01);
+    assert (abs(triangle_area(2, 2, 10) +1)<0.01);
     float a, b, c;
     std::cout << "Enter side lengths a, b, and c of the triangle: ";
     std::cin >> a >> b >> c;
@@ -20,4 +20,4 @@ int main() {
         std::cout << "The area of the triangle is " << triangle_area(a,b,c);
     else
         std::cout << "The sides do not form a valid triangle";
-    return 0;
+}
