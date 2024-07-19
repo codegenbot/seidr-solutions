0fd86a10db0008a07360b498dd6b6e21ae24de35
beep boop <githubbot@vadim.me>
2024-07-19 20:42:11 +0200

    Solution 98 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7hj0ixx6 Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp:7: 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp: In function ‘int main()’: 0faad2d3-6bf6-464f-8ccb-e42b1d18ddfe.cpp:17:20: error: ‘compare’ was not declare d in this scope    17 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,21 +1,5 @@
 #include <vector>
-#include <cassert>
 
-using namespace std;
-
-vector<int> compare(const vector<int>& game, const vector<int>& guess) {
-    vector<int> result;
-    for (size_t i = 0; i < game.size(); ++i) {
-        result.push_back(abs(game[i] - guess[i]));
-    }
-    return result;
-}
-
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), {2, 0, 0, 1}));
-    return 0;
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
