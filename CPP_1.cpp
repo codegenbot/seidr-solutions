9e5ee36fe314c0b6c8ad2c36847126fb01b08dbd
beep boop <githubbot@vadim.me>
2024-07-19 17:09:39 +0200

    Solution 28 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8i5z75z8 Prompt: a5618b7e-8277-44c3-aaf9-9dc017dd75ba.cpp:50:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    50 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ a5618b7e-8277-44c3-aaf9-9dc017dd75ba.cpp:8:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     8 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ a5618b7e-8277-44c3-aaf9-9dc017dd75ba.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ a5618b7e-8277-44c3-aaf9-9dc017dd75ba.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -6,42 +6,5 @@
 using namespace std;
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-vector<string> separate_paren_groups(string paren_string);
-
-vector<string> separate_paren_groups(string paren_string) {
-    vector<string> result;
-    string group;
-    int balance = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            if (balance > 0) {
-                group += c;
-            }
-            balance++;
-        } else if (c == ')') {
-            balance--;
-            if (balance > 0) {
-                group += c;
-            } else if (balance == 0) {
-                result.push_back(group);
-                group = "";
-            }
-        }
-    }
-
-    return result;
+    return a == b;
 }
