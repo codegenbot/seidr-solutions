1627a5cda66970cc4d1e4a7c9f1da373a9c5d0bf
beep boop <githubbot@vadim.me>
2024-07-19 23:36:37 +0200

    Solution 0 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lp46jxd4 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,34 +1,71 @@
-#include <iostream>
+#include <boost/any.hpp>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-int compare_one(const string& s, int i) {
-    if (i == 1)
-        return stoi(s) - 10;
-    else if (i == 2)
-        return stoi(s);
-    else if (i == 3)
-        return stoi(s) + 10;
-    else 
-        return 0;
-}
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = a.convert_to<string>();
+        string str2 = b.convert_to<string>();
 
-int main() {
-    string a, b;
-    cout << "Enter the first number: ";
-    cin >> a;
-    cout << "Enter the second number: ";
-    cin >> b;
+        int num1 = 0, num2 = 0;
+        bool dot1 = false, dot2 = false;
 
-    int s1 = compare_one(a, 1);
-    int s2 = compare_one(b, 3);
+        for (int i = 0; i < str1.length(); i++) {
+            if (!dot1 && !isdigit(str1[i])) {
+                dot1 = true;
+                continue;
+            }
+            if (!dot2 && !isdigit(str2[i])) {
+                dot2 = true;
+                continue;
+            }
+            num1 = num1 * 10 + (str1[i] - '0');
+            num2 = num2 * 10 + (str2[i] - '0');
+        }
 
-    if (s1 < s2) {
-        return -1;
-    } else if (s2 < s1) {
-        return 1;
-    } else {
-        return 0;
+        return str1 > str2 ? a : b;
+    }
+    else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
+        string str = a.convert_to<string>();
+
+        int num = 0;
+        bool dot = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!dot && !isdigit(str[i])) {
+                dot = true;
+                continue;
+            }
+            num = num * 10 + (str[i] - '0');
+        }
+
+        return b.convert_to<boost::any>() > boost::any(num) ? b : a;
+    }
+    else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(string)) {
+        string str = b.convert_to<string>();
+
+        int num = 0;
+        bool dot = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!dot && !isdigit(str[i])) {
+                dot = true;
+                continue;
+            }
+            num = num * 10 + (str[i] - '0');
+        }
+
+        return a.convert_to<boost::any>() > boost::any(num) ? a : b;
+    }
+    else {
+        return boost::any("None");
     }
 }
