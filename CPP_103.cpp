f56b85db867c0b7e5426bc68ec2713b5e5c85957
beep boop <githubbot@vadim.me>
2024-07-19 23:42:27 +0200

    Solution 57 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bgze1p9g Prompt:       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: ccac3001-822d-4925-bc26-a44f73fcdf3c.cpp:25:29:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,26 +1,23 @@
-```cpp
 #include <iostream>
 #include <string>
 #include <cmath> 
 using namespace std;
 
-string rounded_avg(int n, int m) {
-    if (n > m) return "-" + rounded_avg(n, m);
+int rounded_avg(int n, int m) {
+    if (n > m) return -1;
     double sum = 0;
     for (int i = n; i <= m; i++) {
         sum += i;
     }
     double avg = round((double)sum / (m - n + 1)); 
-    string res = "";
     while(avg > 0.5) {
-        res = (int)avg & 1 ? res + "1" : res + "0";
         avg /= 2;
     }
-    return res;
+    return (int)avg;
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
-    cout << rounded_avg(n, m);
+    cout << to_string(rounded_avg(n, m));
 }
