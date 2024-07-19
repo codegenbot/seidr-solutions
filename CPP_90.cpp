c12be2e2d785c845be98ea1ae58dc0018c474e1c
beep boop <githubbot@vadim.me>
2024-07-19 18:42:20 +0200

    Solution 18 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mb66ijd7 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 80bec7d5-d866-43b9-97de-e9665f6ff831.cpp:22:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,28 +1,29 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
-int next_smallest(std::vector<int> lst) {
-    std::vector<int> sorted = lst;
-    std::sort(sorted.begin(), sorted.end());
+int next_smallest(vector<int> lst) {
+    vector<int> sorted = lst;
+    sort(sorted.begin(), sorted.end());
     if (sorted.size() < 2)
-        return -1; 
+        return -1; // or any other value that represents "None"
     for (int i = 0; i < sorted.size() - 1; i++) {
         if (sorted[i] < sorted[i + 1])
             return sorted[i + 1];
     }
-    return -1; 
+    return -1; // or any other value that represents "None"
 }
 
 int main() {
-    std::vector<int> lst;
+    std::vector<int> lst(std::vector<int>::size_type(0)); 
     int num;
-    while (true) {
-        std::cin >> num;
+    while(std::cin >> num) {
         lst.push_back(num);
-        if (!std::cin || num == -1)
-            break;
     }
-    std::cout << next_smallest(lst);
+    if(lst.size()>0) {
+        std::cout << next_smallest(lst) << std::endl;
+    } else {
+        std::cout << -1 << std::endl;
+    }
     return 0;
 }
