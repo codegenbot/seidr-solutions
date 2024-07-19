920740c0ecb87f5e78e8f04b56cb688c9a9d8684
beep boop <githubbot@vadim.me>
2024-07-19 20:49:32 +0200

    Solution 28 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/s3c98xrb Prompt:       |                              ^~~~~~ 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp:9: 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp: In function ‘int main()’: 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 82d2e37a-3686-4650-ba3f-184ccb5ae624.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,25 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <string>
-#include <cassert>
-
-using namespace std;
-
-vector<string> all_prefixes(string str){
-    vector<string> result;
-    string prefix = "";
-    for (char c : str) {
-        prefix += c;
-        result.push_back(prefix);
-    }
-    return result;
-}
 
 bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    return 0;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
