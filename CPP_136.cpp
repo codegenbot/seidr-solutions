118617c288e02fa85635d0985e30f3f29f87a405
beep boop <githubbot@vadim.me>
2024-07-19 05:03:28 +0200

    Solution 94 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mr70wchs Prompt:    35 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 5f37415d-4a44-4c51-80f4-7d278caeaf4c.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 5f37415d-4a44-4c51-80f4-7d278caeaf4c.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 5f37415d-4a44-4c51-80f4-7d278caeaf4c.cpp:34: 5f37415d-4a44-4c51-80f4-7d278caeaf4c.cpp: In function ‘int main()’: 5f37415d-4a44-4c51-80f4-7d278caeaf4c.cpp:44:20: error: ‘largest_smallest_integer s’ was not declared in this scope; did you mean ‘largest_smallest_integer’?    44 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 5f37415d-4a44-4c51-80f4-7d278caeaf4c.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -8,13 +8,13 @@ bool is_same(std::vector<int> a, std::vector<int> b) {
 
 std::vector<int> largest_smallest_integer(std::vector<int> lst) {
     std::vector<int> result = {0, 0};
-    int largest_negative = 0;
-    int smallest_positive = 0;
+    int largest_negative = INT_MIN;
+    int smallest_positive = INT_MAX;
     
     for (int num : lst) {
-        if (num < 0 && num < largest_negative) {
+        if (num < 0 && num > largest_negative) {
             largest_negative = num;
-        } else if (num > 0 && (num < smallest_positive || smallest_positive <= 0)) {
+        } else if (num > 0 && num < smallest_positive) {
             smallest_positive = num;
         }
     }
