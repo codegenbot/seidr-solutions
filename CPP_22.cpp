594b80c975dbd2de154332586f1559fd9276c1af
beep boop <githubbot@vadim.me>
2024-07-20 01:58:14 +0200

    Solution 6 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/xtzvyndu Prompt: 272e3b34-5812-493e-a21c-b9979de3806f.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 272e3b34-5812-493e-a21c-b9979de3806f.cpp:14: 272e3b34-5812-493e-a21c-b9979de3806f.cpp: In function ‘int main()’: 272e3b34-5812-493e-a21c-b9979de3806f.cpp:24:35: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    24 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   <brace-enclosed initializer list> 272e3b34-5812-493e-a21c-b9979de3806f.cpp:4:34: note:   initializing argument 1 o f ‘std::vector<int> filter_integers(int)’     4 | std::vector<int> filter_integers(const std::list<std::any>& values){       |                                  ^~~~~~~~~~~~~~~ In file included from 272e3b34-5812-493e-a21c-b9979de3806f.cpp:14: 272e3b34-5812-493e-a21c-b9979de3806f.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
 #include <vector>
+#include <list>
 #include <any>
 #include <cassert>
 
 std::vector<int> filter_integers(const std::vector<std::any>& values){
     std::vector<int> result;
-    for (auto val : values) {
-        if (val.type() == typeid(int).hash_code()) {
+    for (const auto &val : values) {
+        if (val.type() == typeid(int)) {
             result.push_back(std::any_cast<int>(val));
         }
     }
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-int main() {
+int main(){
     assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     
     return 0;
