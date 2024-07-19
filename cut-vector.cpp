3b04853a749862e8989600d62c74f5a79ef3de4a
beep boop <githubbot@vadim.me>
2024-07-19 20:55:47 +0200

    Solution 86 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt: it must return 1 9377 2 9464 5729 for input 3 9377 9464 5729, but it returns 9377 9464 5729 5729.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,63 +1,16 @@
-#include <climits>
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<vector<int>> res(2);
-    
-    long long sum1 = 0;
-    long long prev_sum = 0; 
-    long long min_diff = LLONG_MAX;
-    int index = -1;
-    
-    for (int i = 0; i < n; i++) { 
-        if(i > 0) {
-            long long diff = std::abs(v[i] - ((sum1 - prev_sum) / (i + 1)));
-            
-            if (diff < min_diff) {
-                min_diff = diff;
-                index = i; // update the cutting point
-            } else if (diff == 0) {
-                sum2 = std::accumulate(v.begin(), v.end(), 0);
-                int part1_sum = prev_sum + v[i];
-                if ((sum1 - part1_sum) == (sum2 - part1_sum)) {
-                    res[0].assign(v.begin(), v.begin() + i+1);
-                    res[1].assign(v.begin() + i, v.end());
-                    return res;
-                }
-            }
-        }
+for (int i = 0; i < n; i++) { 
+    if(i > 0) {
+        int diff = std::abs(v[i] - ((sum1 - v[i-1]) / i));
         
-        sum1 += v[i];
-        prev_sum = sum1; 
-    }
-    
-    if (min_diff == 0) {
-        res[0].assign(v.begin(), v.begin() + index+1);
-        res[1].assign(v.begin() + index, v.end());
-    } else {
-        res[0].assign(v.begin(), v.begin() + index+1);
-        res[1].assign(v.begin() + index, v.end());
-    }
-    
-    return res;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    vector<vector<int>> res = cutVector(v);
-    for (auto &v : res) {
-        for (int x : v) {
-            cout << x << " ";
+        if (diff < min_diff) {
+            min_diff = diff;
+            index = i; // update the cutting point
+        } else if (diff == 0) {
+            res[0].assign(v.begin(), v.begin() + i+1);
+            res[1].assign(v.begin() + i, v.end());
+            return res;
         }
-        cout << endl;
     }
-    return 0;
+    
+    sum1 += v[i];
 }
