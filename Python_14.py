e0a9ab345651ae02563f3b23ac943283835d5a00
beep boop <githubbot@vadim.me>
2024-07-19 04:32:07 +0200

    Solution 14 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/332i0iml Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25e8f47c-a6b0-41e2-8ea1-fc1d109f5 2d7.py", line 2     return [string[: i + 1] for i in range(len(string)]                                                       ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_14.py
+++ Python_14.py
@@ -6,6 +6,3 @@ def suffixes(string: str) -> List[str]:
 
 def substrings(string: str) -> List[str]:
     return [string[i:j] for i in range(len(string)) for j in range(i + 1, len(string) + 1)]
-
-def is_palindrome(string: str) -> bool:
-    return string == string[::-1]
