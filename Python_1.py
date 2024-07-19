8a93871e35d8d50a34b3b25c47f49429f97e326b
beep boop <githubbot@vadim.me>
2024-07-19 12:06:09 +0200

    Solution 39 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5w40kv03 Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -2,10 +2,11 @@ def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
     group = ""
     for char in paren_string:
-        if char == "(":
+        if char in "()":
             group += char
-        elif char == ")":
-            group += char
+        elif group:
             result.append(group)
             group = ""
+    if group:
+        result.append(group)
     return result
