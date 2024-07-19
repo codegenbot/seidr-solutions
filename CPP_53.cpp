e2b1222913474805af696c8cd9a32ea1f0930fa9
beep boop <githubbot@vadim.me>
2024-07-19 21:24:58 +0200

    Solution 11 of CPP/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mxur9ys5 Prompt: 5ca7c5de-9059-40b7-8bfa-8a785fb9fde7.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return x + y;       | ^~~~~~ 5ca7c5de-9059-40b7-8bfa-8a785fb9fde7.cpp:2:1: error: expected declaration before  ‘}’ token     2 | }       | ^ 5ca7c5de-9059-40b7-8bfa-8a785fb9fde7.cpp: In function ‘int main()’: 5ca7c5de-9059-40b7-8bfa-8a785fb9fde7.cpp:8:15: error: ‘rand’ was not declared in  this scope     8 |         int x=rand()%1000;       |               ^~~~ In file included from 5ca7c5de-9059-40b7-8bfa-8a785fb9fde7.cpp:4: 5ca7c5de-9059-40b7-8bfa-8a785fb9fde7.cpp:10:17: error: ‘add’ was not declared in  this scope    10 |         assert (add(x, y) == x + y);       |                 ^~~

--- CPP_53.cpp
+++ CPP_53.cpp
@@ -8,9 +8,9 @@ int add(int x, int y) {
 
 int main() {
     srand(time(NULL));
-    int x = rand()%1000;
-    int y = rand()%1000;
+    int x = rand() % 1000;
+    int y = rand() % 1000;
     assert(add(x, y) == x + y);
-    
+
     return 0;
 }
