b8abbad123e7a97e6faf22dc5b129eee288d9ca9
beep boop <githubbot@vadim.me>
2024-07-19 22:49:39 +0200

    Solution 88 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dnk6zjdz Prompt:    19 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5b136a9b-146f-487e-a9db-e87596270df6.cpp:19:36: error: expected primary-expressi on before ‘float’    19 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5b136a9b-146f-487e-a9db-e87596270df6.cpp:19:43: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5b136a9b-146f-487e-a9db-e87596270df6.cpp:18: 5b136a9b-146f-487e-a9db-e87596270df6.cpp: In function ‘int main()’: 5b136a9b-146f-487e-a9db-e87596270df6.cpp:28:20: error: ‘sort_even’ was not decla red in this scope    28 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 5b136a9b-146f-487e-a9db-e87596270df6.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,29 +1,25 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    if(a.size() != b.size())
-        return false;
-    for(int i = 0; i < a.size(); i++)
-        if(a[i] != b[i])
-            return false;
-    return true;
+std::vector<float> input;
+float val;
+
+bool issame(const std::vector<float>& a, const std::vector<float>& b) {
+    return a == b;
 }
 
 std::vector<float> sort_even(const std::vector<float>& input) {
     std::vector<float> output;
-    for(float val : input) {
-        if(val % 2 == 0)
+    for (float val : input) {
+        if (val % 2 == 0) { 
             output.push_back(val);
+        }
     }
     return output;
 }
 
 int main() {
-    std::vector<float> input;
-    float val;
     while(std::cin >> val) {
         input.push_back(val);
     }
