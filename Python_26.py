a91fe5699071da7b41fefb547175ba36eea9c58f
beep boop <githubbot@vadim.me>
2024-07-19 06:27:10 +0200

    Solution 97 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hj8ms4en Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2333f428-38d1-4a78-bc3a-a64bbe10e 88c.py", line 2     return sorted(list(set(numbers))                  ^ SyntaxError: '(' was never closed

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,2 @@
 def remove_duplicates(numbers):
-    unique_numbers = []
-    for num in numbers:
-        if num not in unique_numbers:
-            unique_numbers.append(num)
-    return unique_numbers
+    return sorted(list(set(numbers)))
