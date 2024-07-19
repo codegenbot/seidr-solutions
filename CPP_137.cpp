6c18ed615a167df45e68fcaf3075d4dcb5f8932a
beep boop <githubbot@vadim.me>
2024-07-19 18:56:00 +0200

    Solution 65 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dilniizg Prompt:       |             ^~~~~~~~~~~ In file included from b3a8931b-d881-40ab-b04f-3880026b39b6.cpp:27: b3a8931b-d881-40ab-b04f-3880026b39b6.cpp: In function ‘int main()’: b3a8931b-d881-40ab-b04f-3880026b39b6.cpp:29:13: error: ‘boost’ has not been decl ared    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ b3a8931b-d881-40ab-b04f-3880026b39b6.cpp:29:29: error: ‘string’ was not declared  in this scope    29 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ b3a8931b-d881-40ab-b04f-3880026b39b6.cpp:29:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from b3a8931b-d881-40ab-b04f-3880026b39b6.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b3a8931b-d881-40ab-b04f-3880026b39b6.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,29 +1,13 @@
-```cpp
-#include <string>
-#include <algorithm>
-
 int compare_strings(const std::string& a, const std::string& b) {
-    if (a > b)
-        return 1;
-    else if (b > a)
-        return -1;
-    else
-        return 0;
-}
-
-std::string compare_one(const std::string& str, int num) {
-    if(num == 1) {
-        for(int i = 0; i < str.length(); i++) {
-            if(str[i] >= 'a' && str[i] <= 'z') {
-                str[i] = (str[i] - 'a' + 'A');
-            }
-        }
-    } else if(num == -1) {
-        for(int i = 0; i < str.length(); i++) {
-            if(str[i] >= 'A' && str[i] <= 'Z') {
-                str[i] = (str[i] - 'A' + 'a');
-            }
-        }
+    for (size_t i = 0; i < a.size() && i < b.size(); ++i) {
+        if (a[i] > b[i])
+            return 1;
+        else if (a[i] < b[i])
+            return -1;
     }
-    return str;
+    if (a.size() > b.size())
+        return 1;
+    else if (a.size() < b.size())
+        return -1;
+    return 0;
 }
