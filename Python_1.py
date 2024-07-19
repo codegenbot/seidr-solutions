6f0bebf9e0e80ea8c113f9587983ec9252c0ad16
beep boop <githubbot@vadim.me>
2024-07-19 12:14:18 +0200

    Solution 96 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5w40kv03 Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -1,11 +1,14 @@
 def separate_paren_groups(paren_string: str) -> List[str]:
+    no_spaces = paren_string.replace(" ", "")
     result = []
     current_group = ""
-    for char in paren_string:
+
+    for char in no_spaces:
         if char == "(":
             current_group += char
         elif char == ")":
             current_group += char
             result.append(current_group)
             current_group = ""
+
     return result
