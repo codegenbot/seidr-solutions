7d67d88304e7dbd69ed8e46b8818c21ebc7c2ffb
beep boop <githubbot@vadim.me>
2024-07-19 23:13:17 +0200

    Solution 69 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s07geol4 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,6 +1,27 @@
-int solve() {
-    int n;
-    cin >> n;
-    cout << "A classic issue!" << endl;
+#include <iostream>
+#include <string>
+
+std::string solve(int num) {
+    std::string result = "";
+    while (num > 0) {
+        if (num % 2 == 0)
+            result += "0";
+        else
+            result += "1";
+        num /= 2;
+    }
+    // reverse the binary string to get correct output
+    std::string binStr = result;
+    for (int i = 0; i < binStr.length(); i++) {
+        if (i == 0) continue;
+        char c = binStr[i];
+        binStr[i] = binStr[0];
+        binStr[0] = c;
+    }
+    return binStr;
+}
+
+int main() {
+    assert(solve(963) == "10010");
     return 0;
 }
