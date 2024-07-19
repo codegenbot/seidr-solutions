d4fefac3360ecff2d978c686686e1244ed1ffff3
beep boop <githubbot@vadim.me>
2024-07-19 18:49:45 +0200

    Solution 37 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/f7vwk9l2 Prompt: cb0f4121-b7f9-47e5-a449-9d93b9a5901e.cpp:58:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    58 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ cb0f4121-b7f9-47e5-a449-9d93b9a5901e.cpp:7:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     7 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ cb0f4121-b7f9-47e5-a449-9d93b9a5901e.cpp:66:5: error: redefinition of ‘int main( )’    66 | int main(){       |     ^~~~ cb0f4121-b7f9-47e5-a449-9d93b9a5901e.cpp:48:5: note: ‘int main()’ previously def ined here    48 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// Remove this
 //bool issame(vector<string> a, vector<string> b) {
 //    if (a.size() != b.size()) return false;
 //    for (int i = 0; i < a.size(); ++i) {
