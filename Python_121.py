a3ed145f9ff2d2e7853016e414e53e52569faa40
beep boop <githubbot@vadim.me>
2024-07-19 17:26:08 +0200

    Solution 97 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ff35e9c-333b-4612-b6a4-ef5d675d4 b1d.py", line 5     return int(''.join(map(lambda x: str(x) if isinstance(x, int) else '', input _list))))         ^ SyntaxError: unmatched ')'

--- Python_121.py
+++ Python_121.py
@@ -2,6 +2,4 @@ def solution(input_list):
     if not input_list:        
         return None
     elif all(isinstance(x, int) and str(x).isdigit() for x in input_list):        
-        return sum(map(int, input_list))    
-    else:        
-        raise ValueError("Input list contains non-integer values")
+        return int(''.join(map(lambda x: str(x) if isinstance(x, int) else '', input_list))))
