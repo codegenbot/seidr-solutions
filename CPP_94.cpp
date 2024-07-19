6fdfade8215d535df8568d9c21906de38b028a94
beep boop <githubbot@vadim.me>
2024-07-19 23:21:24 +0200

    Solution 61 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r58ft6fs Prompt: In file included from 38c1e400-aa84-4ec0-961d-0fd9704a85ec.cpp:28: 38c1e400-aa84-4ec0-961d-0fd9704a85ec.cpp: In function ‘int main()’: 38c1e400-aa84-4ec0-961d-0fd9704a85ec.cpp:30:13: error: ‘skjkasdkd’ was not decla red in this scope    30 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,24 +1,3 @@
-#include <cassert>
-
-int customBitCount(void* ptr, int size) {
-    int count = 0;
-    unsigned char* p = (unsigned char*)ptr;
-    for(int i=0; i<size; i++) {
-        if((p[i] & 1))
-            count++;
-    }
-    return count;
-}
-
-int bitCount(int a) {
-    int count = 0;
-    while (a) {
-        a &= ~1;
-        count++;
-    }
-    return count;
-}
-
 int main() {
     int arr[] = {127, 97, 8192};
     assert(bitCount(*((int*)arr)) == customBitCount(&arr[0], sizeof(int)));
