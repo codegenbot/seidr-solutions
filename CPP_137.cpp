6163de14149dc6be0a33186f571226822ecc4f32
beep boop <githubbot@vadim.me>
2024-07-19 20:45:41 +0200

    Solution 88 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0w0ouwj6 Prompt:     8 | int main(){       |     ^~~~ d76a4421-4883-4fe9-b2c9-cda4e4280e23.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from d76a4421-4883-4fe9-b2c9-cda4e4280e23.cpp:7: d76a4421-4883-4fe9-b2c9-cda4e4280e23.cpp: In function ‘int main()’: d76a4421-4883-4fe9-b2c9-cda4e4280e23.cpp:9:13: error: ‘boost’ has not been decla red     9 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ d76a4421-4883-4fe9-b2c9-cda4e4280e23.cpp:9:29: error: ‘string’ was not declared in this scope     9 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ d76a4421-4883-4fe9-b2c9-cda4e4280e23.cpp:9:37: error: ‘compare_one’ was not decl ared in this scope     9 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,19 +1,13 @@
+#include <any>
 #include <cassert>
 #include <string>
-#include <boost/any.hpp>
 
-using namespace std;
-using boost::any_cast;
-
-boost::any compare_one(const std::string& str1, const std::string& str2) {
-    if (str1 == str2) {
-        return std::string("");
-    } else {
-        return std::string("None");
-    }
+std::any compare_one(const std::string& str1, const std::string& str2) {
+    // Implement the comparison logic here
 }
 
 int main() {
-    assert(any_cast<std::string>(compare_one(std::string("1"), std::string("1")))== "");
+    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "");
+    
     return 0;
 }
