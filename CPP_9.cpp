d818cd0ed15bff0d85149d9642803b8c1ffb9c98
beep boop <githubbot@vadim.me>
2024-07-19 17:06:08 +0200

    Solution 28 of CPP/9, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1uznveu6 Prompt: 7f8fe7fc-eb2c-4c1a-964b-2d5495b4d20f.cpp:32:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    32 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 7f8fe7fc-eb2c-4c1a-964b-2d5495b4d20f.cpp:21:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    21 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 7f8fe7fc-eb2c-4c1a-964b-2d5495b4d20f.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 7f8fe7fc-eb2c-4c1a-964b-2d5495b4d20f.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main(){       |     ^~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,7 +1,17 @@
+#include <vector>
+#include <cassert>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> rolling_max(vector<int> numbers);
+
+bool issame(vector<int> a, vector<int> b);
+
 vector<int> rolling_max(vector<int> numbers){
     vector<int> result;
     int max_num = numbers[0];
-    for(int i=0; i<numbers.size(); i++){
+    for(int i = 0; i < numbers.size(); i++){
         max_num = max(max_num, numbers[i]);
         result.push_back(max_num);
     }
