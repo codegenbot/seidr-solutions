146f52ecd023324c96319efedaebb518c164ba75
beep boop <githubbot@vadim.me>
2024-07-20 01:00:51 +0200

    Solution 2 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kywbzhfz Prompt:       |         ~~~~~~~~~~^~~~~~~~~~~~~~ In file included from c3a009f0-5b40-4b40-a34f-f2216ad348c9.cpp:2: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ c3a009f0-5b40-4b40-a34f-f2216ad348c9.cpp:17:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    17 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from c3a009f0-5b40-4b40-a34f-f2216ad348c9.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccjmKJHW.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': c3a009f0-5b40-4b40-a34f-f2216ad348c9.cpp:(.text+0x8e): undefined reference to `M D5_Init' /usr/bin/ld: c3a009f0-5b40-4b40-a34f-f2216ad348c9.cpp:(.text+0xc4): undefined re ference to `MD5_Update' /usr/bin/ld: c3a009f0-5b40-4b40-a34f-f2216ad348c9.cpp:(.text+0xf0): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -7,24 +7,21 @@ string string_to_md5(string text) {
     if (text.empty()) return "";
 
     unsigned char md5[MD5_DIGEST_LENGTH];
-    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(ctx, EVP_md_md5(), NULL);
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
     const char* ptr = text.c_str();
     while (*ptr) {
-        EVP_DigestUpdate(ctx, &(*ptr), 1);
+        MD5_Update(&ctx, ptr, 1);
         ptr++;
     }
-    unsigned char* md5_ptr = (unsigned char*)malloc(MD5_DIGEST_LENGTH);
-    EVP_DigestFinal_ex(ctx, md5_ptr, NULL);
-    EVP_MD_CTX_free(ctx);
+    MD5_Final(md5, &ctx);
 
     string result;
     for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
         char buf[3];
-        sprintf(buf, "%02x", md5_ptr[i]);
+        sprintf(buf, "%02x", md5[i]);
         result += buf;
     }
 
-    free(md5_ptr);
     return result;
 }
