64b2c20a99175359bbc7f6472b4066b887f14bb4
beep boop <githubbot@vadim.me>
2024-07-19 18:41:11 +0200

    Solution 30 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt: it must return 1 9377 2 9464 5729 for input 3 9377 9464 5729, but it returns 9377 9464 5729 9377 9464 5729.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,52 +1,39 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
-    vector<vector<int>> res(2);
-    
     long long sum1 = 0, sum2 = 0;
-    while(i < n) { 
-        sum2 += v[i];
-        sum1 -= v[i];
-        if (abs(sum1 - sum2) >= diff || sum2 > sum1 + v[i]) {
-            res[0].push_back(v[i]);
-        } else {
-            break;
-        }
-        i++;
-    }
-    
-    int i = 0;
-    while(i < n) { 
-        sum2 += v[i];
-        sum1 -= v[i];
-        if (abs(sum1 - sum2) >= diff) {
-            res[0].push_back(v[i]);
-        } else {
-            res[1] = vector<int>(v.begin() + i, v.end());
-            break;
-        }
-        i++;
-    }
-    
-    return res;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
     for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    vector<vector<int>> res = cutVector(v);
-    for (auto &v : res) {
-        for (int x : v) {
-            cout << x << " ";
+        if (i % 2 == 0) {
+            sum1 += v[i];
+        } else {
+            sum2 += v[i];
         }
-        cout << endl;
     }
-    return 0;
+
+    vector<vector<int>> res(2);
+    int diff = abs(sum1 - sum2);
+
+    res[0].push_back(v[0]);
+    for (int i = 1; i < n; i++) {
+        if (abs(sum1 - sum2) == diff) {
+            res[0].push_back(v[i]);
+        } else {
+            break;
+        }
+    }
+
+    int temp = 0;
+    while (temp < n) {
+        sum1 -= v[temp];
+        sum2 += v[temp];
+
+        if (abs(sum1 - sum2) <= diff) {
+            res[0].push_back(v[temp]);
+        } else {
+            res.push_back(vector<int>(v.begin() + temp + 1, v.end()));
+            break;
+        }
+        temp++;
+    }
+
+    return res;
 }
