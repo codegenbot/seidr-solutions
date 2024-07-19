00ed412cf6d8a88b250ab4aef2053a5a7d4c8d86
beep boop <githubbot@vadim.me>
2024-07-19 21:36:12 +0200

    Solution 4 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jlxecinn Prompt:     1 | ```c++       |    ^ 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp:3:1: error: expected unqualified-id bef ore ‘for’     3 | for (long i = 2; i * i <= n && !is_divisor; ++i) {       | ^~~ 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp:3:18: error: ‘i’ does not name a type     3 | for (long i = 2; i * i <= n && !is_divisor; ++i) {       |                  ^ 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp:3:45: error: expected unqualified-id be fore ‘++’ token     3 | for (long i = 2; i * i <= n && !is_divisor; ++i) {       |                                             ^~ 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp:8:1: error: expected unqualified-id bef ore ‘return’     8 | return !is_divisor;       | ^~~~~~ In file included from 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp:10: 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp: In function ‘int main()’: 438dd9f9-b79d-43e3-973c-6093cde1efbf.cpp:12:13: error: ‘is_prime’ was not declar ed in this scope    12 |     assert (is_prime(13441 * 19) == false);       |             ^~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
+#include <cassert>
+using namespace std;
 
-bool is_prime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    bool is_divisor = false;
-    for (int i = 2; i * i <= n && !is_divisor; ++i) {
+bool is_divisor = false;
+bool isPrime(long n) {
+    if (n <= 1) return false;
+    for (long i = 2; i * i <= n && !is_divisor; ++i) {
         if (n % i == 0) {
             is_divisor = true;
         }
@@ -14,8 +13,8 @@ bool is_prime(int n) {
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    assert(is_prime(n));
+    long n;
+    cin >> n;
+    cout << isPrime(n);
     return 0;
 }
