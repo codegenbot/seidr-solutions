82481ef414fc4870eb053a931a3108e4810dd853
beep boop <githubbot@vadim.me>
2024-07-19 20:55:18 +0200

    Solution 12 of CPP/161, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fk10948y Prompt: Complete the following code given the task description and function signature.

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,12 +1,14 @@
-string solve(string s) {
-    string res = "";
-    for (char c : s) {
-        if (isalpha(c)) {
-            res += toupper(islower(c)) ? tolower(c) : toupper(c);
-        } else {
-            res += c;
+#include<string>
+using namespace std;
+
+string solve(string s){
+    string result = "";
+    for(char c : s){
+        if(isalpha(c)){
+            result += (islower(c)) ? toupper(c) : tolower(c);
+        }else{
+            result += c;
         }
     }
-    reverse(res.begin(), res.end());
-    return res;
+    return ((result.size() > 1 || !ispunct(result[0])) && !isalnum(result[0])) ? string(1, result.back()) + result.substr(0, result.size()-1) : result;
 }
