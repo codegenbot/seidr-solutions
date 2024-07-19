206edfc3048b4e4fee96b525ba91314f53446500
beep boop <githubbot@vadim.me>
2024-07-19 06:03:55 +0200

    Solution 40 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/twbnhxvv Prompt: 2c43e14c-8a9d-4344-8874-9de6bc710a10.cpp: In function ‘std::string string_to_md5 (const string&)’: 2c43e14c-8a9d-4344-8874-9de6bc710a10.cpp:12:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    12 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 2c43e14c-8a9d-4344-8874-9de6bc710a10.cpp:3: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 2c43e14c-8a9d-4344-8874-9de6bc710a10.cpp: At global scope: 2c43e14c-8a9d-4344-8874-9de6bc710a10.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 2c43e14c-8a9d-4344-8874-9de6bc710a10.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,30 +8,31 @@ std::string string_to_md5(const std::string& text) {
         return "None";
     }
 
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
-    unsigned char md_value[EVP_MAX_MD_SIZE];
-    unsigned int md_len;
+    EVP_MD_CTX *md_ctx = EVP_MD_CTX_new();
+    EVP_DigestInit(md_ctx, EVP_md5());
 
-    OpenSSL_add_all_digests();
-    md = EVP_get_digestbyname("md5");
+    EVP_DigestUpdate(md_ctx, text.c_str(), text.length());
 
-    mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
-    EVP_MD_CTX_free(mdctx);
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int digest_len;
+    EVP_DigestFinal(md_ctx, digest, &digest_len);
+
+    EVP_MD_CTX_free(md_ctx);
 
     char mdString[33];
-    for (unsigned int i = 0; i < md_len; i++) {
-        sprintf(&mdString[i * 2], "%02x", md_value[i]);
+    for (int i = 0; i < 16; i++) {
+        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
     }
 
     return std::string(mdString);
 }
 
 int main() {
+    OpenSSL_add_all_digests();
+
     assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+
+    EVP_cleanup();
 
     return 0;
 }
