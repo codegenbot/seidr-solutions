7fc4952da1d0de3f4218f45d15e25d0ea34f5452
beep boop <githubbot@vadim.me>
2024-07-20 00:54:42 +0200

    Solution 57 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bg00m0r1 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from e6aa2780-3077-432d-ab77-b02f13705cba.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e6aa2780-3077-432d-ab77-b02f13705cba.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e6aa2780-3077-432d-ab77-b02f13705cba.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e6aa2780-3077-432d-ab77-b02f13705cba.cpp:40: e6aa2780-3077-432d-ab77-b02f13705cba.cpp: In function ‘int main()’: e6aa2780-3077-432d-ab77-b02f13705cba.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <iostream>
 
-bool areVectorsSame(std::vector<int> a, std::vector<int> b) {
+bool areVectorSame(std::vector<int> a, std::vector<int> b) {
     return (a.size() == b.size()) && (std::equal(a.begin(), a.end(), b.begin()));
 }
 
