45243c59532e1f7af96487dece69e67120cf82d3
beep boop <githubbot@vadim.me>
2024-07-19 23:58:29 +0200

    Solution 73 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bgze1p9g Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,22 +1,26 @@
+```
 #include <iostream>
-#include <string>
 #include <cmath> 
 using namespace std;
 
 int rounded_avg(int n, int m);
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    cout << rounded_avg(n, m) << endl;
-}
-
-double rounded_avg(int n, int m) {
-    if (n > m) return -1.0;
+rounded_avg(int n, int m) {
+    if (n > m) return -1;
     double sum = 0.0;
     for (int i = n; i <= m; i++) {
         sum += i;
     }
     double avg = round((sum / (m - n + 1)) * 2) / 2.0;
-    return avg;
+    return (int)avg;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << "Error: Invalid input. Please enter numbers in ascending order." << endl;
+    if (n <= m) {
+        cout << rounded_avg(n, m) << endl;
+    }
+    return 0;
 }
