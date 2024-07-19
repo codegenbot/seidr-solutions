5020d86e06691dda554bb05216d609a8cf1c2c4d
beep boop <githubbot@vadim.me>
2024-07-20 01:20:56 +0200

    Solution 74 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/beaykqr0 Prompt: ’ previously defined here     8 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ ff3620da-6d18-491e-9f07-2d120b9cc7a8.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ ff3620da-6d18-491e-9f07-2d120b9cc7a8.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from ff3620da-6d18-491e-9f07-2d120b9cc7a8.cpp:34: ff3620da-6d18-491e-9f07-2d120b9cc7a8.cpp: In function ‘int main()’: ff3620da-6d18-491e-9f07-2d120b9cc7a8.cpp:44:35: error: could not convert ‘sorted _list_sum(std::vector<std::__cxx11::basic_string<char> >)()’ from ‘vector<std::v ector<std::__cxx11::basic_string<char> >>’ to ‘vector<std::__cxx11::basic_string <char>>’    44 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   vector<std::vector<std::__cxx11::basic _string<char> >>

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,34 +5,40 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
+bool issame(vector<string> a,vector<string>b){
     return a == b;
 }
 
 vector<vector<string>> sorted_list_sum(vector<string> lst) {
-    map<string, int> mp;
-    for (auto &s : lst)
+    map<string,int> mp;
+    for(auto &s:lst)
         ++mp[s];
     vector<vector<string>> res;
-    for (const auto &pair : mp)
-        if (pair.second > 1)
-            res.push_back({{pair.first, string(pair.second - 1, 'c')}});
+    for(const auto& pair:mp)
+        if(pair.second > 1)
+            res.push_back({{pair.first, string(pair.second-1,'c')}});
     return res;
 }
 
-int main() {
+vector<string> get_input() {
     int n;
-    cin >> n; 
+    cout << "Enter the number of strings: ";
+    cin >> n;
     vector<string> lst(n);
-    for (auto &s : lst) {
-        cin >> s; 
+    for(int i=0; i<n; ++i) {
+        cin >> lst[i];
     }
+    return lst;
+}
+
+int main() {
+    vector<string> lst = get_input();
     
     vector<vector<string>> result = sorted_list_sum(lst);
-
-    for (auto &v : result) {
+    
+    for(auto &v : result) {
         cout << v[0] << ": " << v[1] << endl;
     }
-
+    
     return 0;
 }
