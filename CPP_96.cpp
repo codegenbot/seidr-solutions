0710e12a3091c8fa1179b0dd684e8ef702ca08e9
beep boop <githubbot@vadim.me>
2024-07-19 03:37:42 +0200

    Solution 49 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1hc4poj8 Prompt: 5336f0b2-c343-4787-83d5-9cd1faad071a.cpp:35:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    35 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 5336f0b2-c343-4787-83d5-9cd1faad071a.cpp:24:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    24 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 5336f0b2-c343-4787-83d5-9cd1faad071a.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 5336f0b2-c343-4787-83d5-9cd1faad071a.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -24,9 +24,3 @@ vector<int> count_up_to(int n){
 bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
-
-int main() {
-    assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-
-    return 0;
-}
