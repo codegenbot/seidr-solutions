0ccf98b9efe75b968eb2873c13f32d75fab09374
beep boop <githubbot@vadim.me>
2024-07-19 17:15:41 +0200

    Solution 59 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/67e77321-404e-4e8e-aca1-a7bcc13b4 8ad.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,5 +1,2 @@
 def check(lst):
-    num_str = ''.join(map(str, lst))
-    return sum(int(d) for d in num_str if int(d) % 2 != 0) == 9
-
-print(check([1, 3, 5]))
+    return all(i % 2 == 0 for i in lst)
