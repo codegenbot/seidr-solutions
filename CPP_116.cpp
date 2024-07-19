cf282150a81375c0778ed20068140c105e4a27ed
beep boop <githubbot@vadim.me>
2024-07-19 20:13:29 +0200

    Solution 87 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r9jq48yp Prompt: 3d67fa9a-ef15-4475-9d31-fa684b4ce77f.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 3d67fa9a-ef15-4475-9d31-fa684b4ce77f.cpp:25: 3d67fa9a-ef15-4475-9d31-fa684b4ce77f.cpp: In function ‘int main()’: 3d67fa9a-ef15-4475-9d31-fa684b4ce77f.cpp:35:30: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    35 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ~~~~~~~~~~^~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 3d67fa9a-ef15-4475-9d31-fa684b4ce77f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 3d67fa9a-ef15-4475-9d31-fa684b4ce77f.cpp:5:35: note:   initializing argument 1 o f ‘void sort_array(std::vector<int>&)’     5 | void sort_array(std::vector<int>& arr) {       |                 ~~~~~~~~~~~~~~~~~~^~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -14,7 +14,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(issame({2,4,8,16,32}, {2, 4, 8, 16, 32}));
+    assert(issame({2, 4, 8, 16, 32}, {2, 4, 8, 16, 32}));
 
     return 0;
 }
