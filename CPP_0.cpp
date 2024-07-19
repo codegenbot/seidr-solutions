fd6a5bfdb37993e15622e7fdc0cc7af2344d85f5
beep boop <githubbot@vadim.me>
2024-07-19 21:00:29 +0200

    Solution 1 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/664ehzue Prompt: 44dde082-1243-4edc-b0ef-994f59e321ed.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                ^~~~~ 44dde082-1243-4edc-b0ef-994f59e321ed.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold){       |                                                               ^ 44dde082-1243-4edc-b0ef-994f59e321ed.cpp: In function ‘int main()’: 44dde082-1243-4edc-b0ef-994f59e321ed.cpp:13:5: error: ‘vector’ was not declared in this scope    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 44dde082-1243-4edc-b0ef-994f59e321ed.cpp:13:12: error: expected primary-expressi on before ‘float’    13 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 44dde082-1243-4edc-b0ef-994f59e321ed.cpp:11: 44dde082-1243-4edc-b0ef-994f59e321ed.cpp:14:31: error: ‘has_close_elements’ cann ot be used as a function    14 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,9 +1,17 @@
-bool has_close_elements(vector<float> numbers, float threshold){
+#include <vector>
+#include <cmath>
+
+bool has_close_elements(std::vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
-            if(abs(numbers[i] - numbers[j]) <= threshold)
+            if(std::abs(numbers[i] - numbers[j]) <= threshold)
                 return true;
         }
     }
     return false;
+
+}
+
+int main(void){
+    // Code
 }
