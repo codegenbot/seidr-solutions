1de78c652c5a9d2d50e3c474c341b66dc59d2335
beep boop <githubbot@vadim.me>
2024-07-19 07:03:21 +0200

    Solution 72 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/riusp3s9 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,25 +1,35 @@
-if(a.index() == 0 && b.index() == 0){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) return b;
-    else return "None";
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+        return a;
+    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+        return b;
+    } else {
+        return "None";
+    }
+} else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+        return a;
+    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+        return b;
+    } else {
+        return "None";
+    }
+} else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))){
+        return a;
+    } else if(stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))){
+        return b;
+    } else {
+        return "None";
+    }
+} else if((a.type() == typeid(int) && b.type() == typeid(string)) || (a.type() == typeid(string) && b.type() == typeid(int))){
+    if(stof(boost::any_cast<string>(a)) > boost::any_cast<int>(b)){
+        return a;
+    } else if(stof(boost::any_cast<string>(a)) < boost::any_cast<int>(b)){
+        return b;
+    } else {
+        return "None";
+    }
 }
-else if(a.index() == 1 && b.index() == 1){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) return b;
-    else return "None";
-}
-else if((a.index() == 0 && b.index() == 1) || (a.index() == 1 && b.index() == 0)){
-    return "None";
-}
-else{
-    string str_a = boost::any_cast<string>(a);
-    string str_b = boost::any_cast<string>(b);
-
-    float num_a = stof(str_a);
-    float num_b = stof(str_b);
-
-    if(num_a > num_b) return a;
-    else if(num_a < num_b) return b;
-    else return "None";
-}
+return "None";
 }
