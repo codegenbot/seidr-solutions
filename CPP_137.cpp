dd1601918fa819610a0531ee17bfbc01a66ae3dc
beep boop <githubbot@vadim.me>
2024-07-19 18:33:43 +0200

    Solution 31 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dilniizg Prompt:    35 | int main(){       |     ^~~~ 5cd766fe-2bb9-46d8-bbe7-f9cef272e6d1.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 5cd766fe-2bb9-46d8-bbe7-f9cef272e6d1.cpp:34: 5cd766fe-2bb9-46d8-bbe7-f9cef272e6d1.cpp: In function ‘int main()’: 5cd766fe-2bb9-46d8-bbe7-f9cef272e6d1.cpp:36:13: error: ‘boost’ has not been decl ared    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 5cd766fe-2bb9-46d8-bbe7-f9cef272e6d1.cpp:36:35: error: expected primary-expressi on before ‘>’ token    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 5cd766fe-2bb9-46d8-bbe7-f9cef272e6d1.cpp:36:37: error: ‘compare_one’ was not dec lared in this scope    36 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,27 +3,30 @@
 
 using namespace std;
 
-int compare_one(int a, int b) {
+int compare_strings(string a, string b) {
     if (a > b)
-        return a;
+        return 1; 
+    else if (b > a)
+        return -1; 
     else
-        return b;
+        return 0; 
 }
 
 int main() {
     string str1, str2;
-    cout << "Enter first number: ";
+    cout << "Enter first string: ";
     cin >> str1;
-    cout << "Enter second number: ";
+    cout << "Enter second string: ";
     cin >> str2;
 
-    int num1 = stoi(str1);
-    int num2 = stoi(str2);
+    int result = compare_strings(str1, str2);
 
-    if (num1 > num2)
-        cout << "Greater number is: " << num1 << endl;
+    if (result > 0)
+        cout << "Greater string is: " << str1 << endl;
+    else if (result < 0)
+        cout << "Greater string is: " << str2 << endl;
     else
-        cout << "Greater number is: " << num2 << endl;
+        cout << "Both strings are equal" << endl;
 
     return 0;
 }
