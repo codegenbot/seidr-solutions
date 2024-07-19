f8b65e1b1d54a0e6cbee44fe96edb2d081bb9e5f
beep boop <githubbot@vadim.me>
2024-07-19 19:16:22 +0200

    Solution 84 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xf1ve6zq Prompt:       |                           ^~~~~~ 70659d49-3ffd-4aaa-9490-ab5d8563b51c.cpp:19:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 70659d49-3ffd-4aaa-9490-ab5d8563b51c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 70659d49-3ffd-4aaa-9490-ab5d8563b51c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 70659d49-3ffd-4aaa-9490-ab5d8563b51c.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 70659d49-3ffd-4aaa-9490-ab5d8563b51c.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 70659d49-3ffd-4aaa-9490-ab5d8563b51c.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,17 +1,21 @@
 #include <vector>
 #include <algorithm>
-#include <initializer_list>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
-auto unique(vector<int>::iterator first, vector<int>::iterator last) {
-    std::vector<int> result(std::unique(first, last), last);
+std::vector<int> unique(std::vector<int>::iterator first, std::vector<int>::iterator last) {
+    std::vector<int> result;
+    for(; first != last; ++first) {
+        if(std::find_if(result.rbegin(), result.rend(),
+            [&first](int x){ return x == *first;}) == result.rend())
+            result.push_back(*first);
+    }
     return result;
 }
 
 int main() {
-    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
+    assert(std::issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}.begin(), {5, 3, 5, 2, 3, 3, 9, 0, 123}.end()) , {0, 2, 3, 5, 9, 123});
     return 0;
 }
