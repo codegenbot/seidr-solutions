fc162b8307bd8deb6c3584f3d7f506785cdf182b
beep boop <githubbot@vadim.me>
2024-07-19 19:57:55 +0200

    Solution 8 of CPP/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jcukbfje Prompt:       |     ^~ 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp:20:7: error: expected unqualified-id be fore ‘else’    20 |     } else if(month == 4 || month == 6 || month == 9 || month == 11){       |       ^~~~ 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp:23:7: error: expected unqualified-id be fore ‘else’    23 |     } else {       |       ^~~~ 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp:28:5: error: expected unqualified-id be fore ‘return’    28 |     return true;       |     ^~~~~~ 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp:29:1: error: expected declaration befor e ‘}’ token    29 | }       | ^ In file included from 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp:31: 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp: In function ‘int main()’: 5ed0cb4a-5b14-4a90-853b-ff16a3660c94.cpp:33:13: error: ‘valid_date’ was not decl ared in this scope    33 |     assert (valid_date("04-2003") == false);       |             ^~~~~~~~~~

--- CPP_124.cpp
+++ CPP_124.cpp
@@ -1,31 +1,17 @@
-bool valid_date(string date) {
-    
-    if(date.empty())
-        return false;
-    
-    if(date.size() != 10)
-        return false;
-    
-    if(date[2] != '-' || date[5] != '-')
+bool valid_date(const string& date) {
+    if(date.empty() || date.size() != 10 || date[2] != '-' || date[5] != '-')
         return false;
     
     int day = stoi(date.substr(3, 2));
     int month = stoi(date.substr(0, 2));
-    int year = stoi(date.substr(6, 4));
     
     if(month < 1 || month > 12)
         return false;
     
-    if(month == 2){
-        if(day < 1 || day > 29)
-            return false;
-    } else if(month == 4 || month == 6 || month == 9 || month == 11){
-        if(day < 1 || day > 30)
-            return false;
-    } else {
-        if(day < 1 || day > 31)
-            return false;
-    }
+    if ((month == 2 && (day < 1 || day > 29)) ||
+        ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30)) ||
+        (day < 1 || day > 31))
+        return false;
     
     return true;
 }
