b0e3b005a41be09fc6e9c7455daffc351faa5146
beep boop <githubbot@vadim.me>
2024-07-19 03:15:15 +0200

    Solution 35 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/p0z2phda Prompt:       |                    ^~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:14: 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp: In function ‘int main()’: 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 7f0dca5b-dd68-4cd0-99b0-c48f301de8d0.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -4,15 +4,15 @@ bool issame(vector<int> a, vector<int> b);
 
 vector<int> sort_array(vector<int> array) {
     if(array.empty()) return array;
-    
+
     int sum = array.front() + array.back();
-    
+
     if(sum % 2 == 0) {
         sort(array.begin(), array.end(), greater<int>());
     } else {
         sort(array.begin(), array.end());
     }
-    
+
     return array;
 }
 
@@ -22,6 +22,6 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert (issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
-    
+
     return 0;
 }
