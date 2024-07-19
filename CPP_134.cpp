c58ea680f29de29f0bd4a78902a0acc0b02ea7b7
beep boop <githubbot@vadim.me>
2024-07-19 19:10:28 +0200

    Solution 0 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mn058wyc Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_134.cpp
@@ -0,0 +1,10 @@
+bool check_if_last_char_is_a_letter(string txt){
+    if(txt.empty()) return false;
+    for(int i=0; i<txt.length(); i++){
+        if(!isalpha(txt[i])) {
+            if(i==txt.length()-1) return false;
+            else return isalpha(txt[i+1]);
+        }
+    }
+    return true;
+}
