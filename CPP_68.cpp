5740d094a370cae3c0fec19ed2e47b237ba95ab3
beep boop <githubbot@vadim.me>
2024-07-19 18:59:22 +0200

    Solution 80 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fg8qcub3 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 5d64502a-e6e2-430f-9942-081e5ae0e9d6.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,19 +1,12 @@
+```
 #include <vector>
 #include <algorithm>
-
-std::vector<int> pluck(const std::vector<std::vector<int>>& v) {
-    std::vector<int> result;
-    for (const auto& i : v[0]) {
-        result.push_back(i);
-    }
-    return result;
-}
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
 int main(int argc, char *argv[]) {
-    assert(issame(pluck({{7, 9}, {7, 1}}), {}));
+    assert(issame({}, {7, 9, 7, 1}));
     return 0;
 }
