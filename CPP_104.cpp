c1daa37a0c57c0ff0bba04a5064e1f1775cc0fe6
beep boop <githubbot@vadim.me>
2024-07-20 00:54:30 +0200

    Solution 79 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/whi9clwf Prompt:       |                    ^~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:21: ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp: In function ‘int main()’: ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,13 +1,22 @@
+bool issame(vector<int> a, vector<int> b);
+
+int main() {
+    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
+}
+
 bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
+    
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    for (int i = 0; i < a.size(); ++i) {
+    
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+    
     return true;
 }
