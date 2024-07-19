cbeb2ad4a26d38a19614da7e091728d00d38917c
beep boop <githubbot@vadim.me>
2024-07-19 03:41:42 +0200

    Solution 73 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/rja8ut3m Prompt:    11 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 67f6cbea-fa48-40bc-87bc-11947123e1a4.cpp:11:36: error: expected primary-expressi on before ‘float’    11 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 67f6cbea-fa48-40bc-87bc-11947123e1a4.cpp:11:43: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 67f6cbea-fa48-40bc-87bc-11947123e1a4.cpp:10: 67f6cbea-fa48-40bc-87bc-11947123e1a4.cpp: In function ‘int main()’: 67f6cbea-fa48-40bc-87bc-11947123e1a4.cpp:20:20: error: ‘rescale_to_unit’ was not  declared in this scope    20 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 67f6cbea-fa48-40bc-87bc-11947123e1a4.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -13,3 +13,7 @@ vector<float> rescale_to_unit(vector<float> numbers) {
     
     return result;
 }
+
+int main() {
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+}
