f2c54ed66c1c4846bac45db1ccea0e60c40d1330
beep boop <githubbot@vadim.me>
2024-07-19 23:46:00 +0200

    Solution 39 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v06y9f55 Prompt:       |                           ^~~~~~ 5ed951cf-8561-4562-b4fd-5d7a6439068a.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 5ed951cf-8561-4562-b4fd-5d7a6439068a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 5ed951cf-8561-4562-b4fd-5d7a6439068a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5ed951cf-8561-4562-b4fd-5d7a6439068a.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5ed951cf-8561-4562-b4fd-5d7a6439068a.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 5ed951cf-8561-4562-b4fd-5d7a6439068a.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,26 +1,27 @@
-```cpp
 #include <vector>
 #include <assert.h>
-#include <initializer_list>
 
-int main() {
-    assert(issame(get_odd_collatz(1), vector<int>{1}));
-}
+using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i])return false;
-    }
-    return true;
-}
-
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+vector<pair<int, int>> get_collatz(int n) {
+    vector<pair<int, int>> result;
     while(n!=1){
         if(n%2==0)n=n/2;
         else n=3*n+1;
-        result.push_back(n);
+        result.push_back({n, 1});
     }
     return result;
+}
+
+bool same(vector<pair<int, int>> a, vector<pair<int, int>> b) {
+    if(a.size()!=b.size())return false;
+    for(int i=0;i<a.size();i++){
+        if(a[i].first!=b[i].first || a[i].second!=b[i].second)return false;
+    }
+    return true;
+
+}
+
+int main() {
+    assert(same(get_collatz(1), {{1, 1}}));
 }
