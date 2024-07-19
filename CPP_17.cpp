a0357e273049134bc954eae5f0494545b345583c
beep boop <githubbot@vadim.me>
2024-07-19 21:02:48 +0200

    Solution 64 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tam7yvui Prompt:    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bfd2a01e-91c0-4e18-b52f-440e5f2c0815.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ bfd2a01e-91c0-4e18-b52f-440e5f2c0815.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main(){       |     ^~~~ In file included from bfd2a01e-91c0-4e18-b52f-440e5f2c0815.cpp:32: bfd2a01e-91c0-4e18-b52f-440e5f2c0815.cpp: In function ‘int main()’: bfd2a01e-91c0-4e18-b52f-440e5f2c0815.cpp:42:20: error: ‘parse_music’ was not dec lared in this scope    42 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ bfd2a01e-91c0-4e18-b52f-440e5f2c0815.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,19 +1,19 @@
 #include <vector>
 #include <string>
-#include <assert.h>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
+using namespace std;
 
-std::vector<int> parse_music(std::string music_string){
-    std::vector<int> beats;
+vector<int> parse_music(string music_string);
+
+vector<int> parse_music(string music_string){
+    vector<int> beats;
     int length = music_string.length();
     for(int i = 0; i < length; ++i){
         if(music_string[i] == 'o'){
             beats.push_back(4);
         } else if(music_string[i] == '|'){
-            if(music_string[i - 1] == 'o'){
+            if(music_string[i-1] == 'o'){
                 beats.back() = 2;
             } else {
                 beats.back() = 1;
@@ -21,6 +21,10 @@ std::vector<int> parse_music(std::string music_string){
         }
     }
     return beats;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
 }
 
 int main(){
