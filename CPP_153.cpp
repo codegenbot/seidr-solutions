ec6b1a3f4daa392d3ca16b9e8f1d8073abfae38b
beep boop <githubbot@vadim.me>
2024-07-19 20:51:19 +0200

    Solution 16 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fk9tk9i8 Prompt: 0fa2ad9e-f358-4ee8-b848-31e89f3d13df.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 0fa2ad9e-f358-4ee8-b848-31e89f3d13df.cpp:23: 0fa2ad9e-f358-4ee8-b848-31e89f3d13df.cpp: In function ‘int main()’: 0fa2ad9e-f358-4ee8-b848-31e89f3d13df.cpp:25:13: error: ‘Strongest_Extension’ was  not declared in this scope    25 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,17 +1,1 @@
-int Strongest_Extension(string class_name,vector<string> extensions){
-    int max_strength = 0;
-    string strongest_extension;
-    for (string extension : extensions) {
-        int cap = 0, sm = 0;
-        for (char c : extension) {
-            if (isupper(c)) cap++;
-            else if (islower(c)) sm++;
-        }
-        int strength = cap - sm;
-        if (strength > max_strength) {
-            max_strength = strength;
-            strongest_extension = extension;
-        } 
-    }
-    return class_name + "." + strongest_extension;
-}
+string Strongest_Extension(string class_name, vector<string> extensions) {
