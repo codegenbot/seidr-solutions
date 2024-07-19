f80487cbce79dc05b4bd745d625064f6a11be8c1
beep boop <githubbot@vadim.me>
2024-07-19 21:54:08 +0200

    Solution 46 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jamerrnh Prompt: d63b29d7-3618-4b67-9dea-497c265931aa.cpp:32:27: error: ‘vector’ was not declared  in this scope    32 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d63b29d7-3618-4b67-9dea-497c265931aa.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d63b29d7-3618-4b67-9dea-497c265931aa.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ d63b29d7-3618-4b67-9dea-497c265931aa.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from d63b29d7-3618-4b67-9dea-497c265931aa.cpp:31: d63b29d7-3618-4b67-9dea-497c265931aa.cpp: In function ‘int main()’: d63b29d7-3618-4b67-9dea-497c265931aa.cpp:41:20: error: ‘count_up_to’ was not dec lared in this scope    41 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 #include <cassert>
 
+using namespace std;
+
 vector<int> count_up_to(int n) {
     vector<int> result;
     if (n < 2) {
@@ -25,8 +27,6 @@ vector<int> count_up_to(int n) {
 bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
-
-using namespace std;
 
 int main() {
     assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
