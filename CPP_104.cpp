ea4f0c6ded8f86588037c33048e812891677674d
beep boop <githubbot@vadim.me>
2024-07-19 21:58:12 +0200

    Solution 48 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mj2jfwr8 Prompt:       |             ^~~~~~ cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp:35: cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp: In function ‘int main()’: cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp:45:20: error: ‘unique_digits’ was not d eclared in this scope    45 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ cf41e8f5-dc79-43c3-aec7-df3dc2e4acb2.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -4,13 +4,13 @@
 
 bool issame(std::vector<int> a, std::vector<int> b);
 
-std::vector<int> unique_digits(std::vector<int> x){
+std::vector<int> unique_digits(std::vector<int> x) {
     std::vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
         while (temp > 0) {
-            if (temp % 2 == 0) {
+            if (temp % 10 % 2 == 0) {
                 hasEvenDigit = true;
                 break;
             }
