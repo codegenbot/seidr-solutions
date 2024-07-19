5656b43a6ea8f88eb3a17a8da0f4b822efb8b1e0
beep boop <githubbot@vadim.me>
2024-07-19 05:23:47 +0200

    Solution 17 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r5iimeo4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3bb75fc8-cc29-4636-998a-91665a691 8f1.py", line 8, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3bb75fc8-cc29-4636-998a-91665a691 8f1.py", line 5, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,3 +1,5 @@
 def fizz_buzz(n: int):
-    count = sum(1 for i in range(1, n + 1) if '17' in str(i) or ('7' in str(i) and ('1' in str(i) or '7' in str(i))))
+    count = sum(1 for i in range(1, n+1) if any(
+        digit_sequence in str(i) for digit_sequence in ['117', '137', '177', '777', '711', '171', '1177', '1777', '7777']
+    ))
     return count
