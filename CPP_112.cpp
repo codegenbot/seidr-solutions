6f203164054dbadc788085968f1261becd946a29
beep boop <githubbot@vadim.me>
2024-07-19 18:42:31 +0200

    Solution 68 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/fhctker5 Prompt: 5313f4bd-b818-4c21-985a-d191576eafc1.cpp:4:1: error: ‘vector’ does not name a ty pe     4 | vector<string> reverse_delete(string s, string c) {       | ^~~~~~ 5313f4bd-b818-4c21-985a-d191576eafc1.cpp:30:1: error: ‘string’ does not name a t ype; did you mean ‘stdin’?    30 | string reverse(string s) {       | ^~~~~~       | stdin

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+using namespace std;
 
 vector<string> reverse_delete(string s, string c) {
     vector<string> result;
@@ -15,12 +16,10 @@ vector<string> reverse_delete(string s, string c) {
             result.push_back(string(1, ch));
         }
     }
-    
     string temp = "";
     for (int i = 0; i < result.size(); i++) {
         temp += result[i];
     }
-    
     if (temp == reverse(temp)) {
         result.push_back("True");
     } else {
@@ -35,17 +34,4 @@ string reverse(string s) {
         rev += s[i];
     }
     return rev;
-}
-
-string issame(string str1, string str2) {
-    if (str1.length() != str2.length()) {
-        return "False";
-    } else {
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1[i] != str2[i]) {
-                return "False";
-            }
-        }
-        return "True";
-    }
 }
