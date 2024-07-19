f5da22b048a3ca77103f02c06355cb409553eea5
beep boop <githubbot@vadim.me>
2024-07-19 18:03:01 +0200

    Solution 10 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/c1dicbex Prompt:    24 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 5eb6a663-d38b-4db6-9ccf-9b03e4c0c8b9.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5eb6a663-d38b-4db6-9ccf-9b03e4c0c8b9.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 5eb6a663-d38b-4db6-9ccf-9b03e4c0c8b9.cpp:23: 5eb6a663-d38b-4db6-9ccf-9b03e4c0c8b9.cpp: In function ‘int main()’: 5eb6a663-d38b-4db6-9ccf-9b03e4c0c8b9.cpp:33:20: error: ‘sort_third’ was not decl ared in this scope    33 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 5eb6a663-d38b-4db6-9ccf-9b03e4c0c8b9.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,29 +1,22 @@
-bool issame(vector<int> v1, vector<int> v2) {
-    if (v1.size() != v2.size()) {
-        return false;
-    }
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
+#include <vector>
+#include <algorithm>
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
-vector<int> sort_third(vector<int> l) {
+vector<bool> sort_third(vector<int> l) {
     vector<int> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
             vector<int> temp;
-            for (int j = i; j < l.size() && j % 3 == 0; j++) {
-                temp.push_back(l[j]);
+            for (int j = 0; j < l.size(); j++) {
+                if (j % 3 == 0 && j <= i) {
+                    temp.push_back(l[j]);
+                }
             }
             sort(temp.begin(), temp.end());
-            for (int k = 0; k < temp.size(); k++) {
-                result.push_back(temp[k]);
-            }
+            result.insert(result.end(), temp.begin(), temp.end());
         } else {
             result.push_back(l[i]);
         }
