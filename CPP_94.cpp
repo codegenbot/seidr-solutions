0fc487cef296914d63324d673439312e7c91ae5d
beep boop <githubbot@vadim.me>
2024-07-19 18:39:52 +0200

    Solution 43 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/6rejqgz4 Prompt: 879fd639-cf77-4fe4-ad95-cd62e0c4e0d0.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 879fd639-cf77-4fe4-ad95-cd62e0c4e0d0.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 879fd639-cf77-4fe4-ad95-cd62e0c4e0d0.cpp:20: 879fd639-cf77-4fe4-ad95-cd62e0c4e0d0.cpp: In function ‘int main()’: 879fd639-cf77-4fe4-ad95-cd62e0c4e0d0.cpp:22:13: error: ‘skjkasdkd’ was not decla red in this scope    22 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -6,8 +6,13 @@ int setSize(std::set<int> s) {
     return s.size();
 }
 
-int originalMain() {
-    int len = setSize({127, 97, 8192});
+int getSetSize(int i, int j, int k) {
+    std::set<int> s = {i, j, k};
+    return setSize(s);
+}
+
+int main() {
+    int len = getSetSize(127, 97, 8192);
     assert(len == 3);
     return 0;
 }
