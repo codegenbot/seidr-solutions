b941c6d2da2d47f22049c2d810a91494faf12af3
beep boop <githubbot@vadim.me>
2024-07-19 17:18:13 +0200

    Solution 12 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/97ranvjo Prompt:       |                           ^~~~~~ 4fc58d40-4ed0-4f94-84f4-306785da4290.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4fc58d40-4ed0-4f94-84f4-306785da4290.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4fc58d40-4ed0-4f94-84f4-306785da4290.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4fc58d40-4ed0-4f94-84f4-306785da4290.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4fc58d40-4ed0-4f94-84f4-306785da4290.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 4fc58d40-4ed0-4f94-84f4-306785da4290.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -14,6 +14,6 @@ std::vector<int> remove_duplicates(std::vector<int> numbers){
 }
 
 int main(){
-    assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), std::vector<int>({1, 4, 5})));
+    assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
     return 0;
 }
