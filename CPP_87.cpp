72cb1bf2649d1e2fa271461a920c0cda417086c5
beep boop <githubbot@vadim.me>
2024-07-19 03:44:52 +0200

    Solution 85 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r1dqhr1m Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a9256c55-d46f-4140-878f-23a024fab290.cpp:36:49: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ a9256c55-d46f-4140-878f-23a024fab290.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ a9256c55-d46f-4140-878f-23a024fab290.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from a9256c55-d46f-4140-878f-23a024fab290.cpp:35: a9256c55-d46f-4140-878f-23a024fab290.cpp: In function ‘int main()’: a9256c55-d46f-4140-878f-23a024fab290.cpp:48:27: error: could not convert ‘get_ro w(std::vector<std::vector<int> >, int)(3)’ from ‘vector<std::vector<int>>’ to ‘v ector<int>’    48 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~       |                           |       |                           vector<std::vector<int>>

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -18,14 +18,14 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
             }
         }
     }
-    
+
     sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b){
         if(a[0] == b[0]){
             return a[1] > b[1];
         }
         return a[0] < b[0];
     });
-    
+
     return result;
 }
 
