1b7cf74851db8fe16c2f9f142fc5db3f5222e102
beep boop <githubbot@vadim.me>
2024-07-19 05:52:38 +0200

    Solution 52 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tyvegyas Prompt:       |             ^~~~~~ f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp:28: f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp: In function ‘int main()’: f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp:38:20: error: ‘even_odd_count’ was not declared in this scope    38 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ f5ce07c0-447b-4cf0-b925-9696d29dbd6a.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+vector<int> even_odd_count(int num);
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
+int main() {
+    assert(issame(even_odd_count(0), {1, 0}));
+    
+    return 0;
+}
+
 vector<int> even_odd_count(int num){
     vector<int> result = {0, 0};
     string numStr = to_string(abs(num));
@@ -15,13 +27,4 @@ vector<int> even_odd_count(int num){
         }
     }
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(even_odd_count(0), {1, 0}));
-    return 0;
 }
