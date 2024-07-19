8a70631565ab63e1ede4f239dd1856d607e1256c
beep boop <githubbot@vadim.me>
2024-07-19 02:03:36 +0200

    Solution 5 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/hxr8gn9v Prompt: e097d759-8cc4-433b-9631-e2a19bf2ee4f.cpp: In function ‘int main()’: e097d759-8cc4-433b-9631-e2a19bf2ee4f.cpp:35:29: error: too few arguments to func tion ‘double find_zero(std::vector<double>, std::vector<double>)’    35 |         solution = find_zero(coeffs);       |                    ~~~~~~~~~^~~~~~~~ e097d759-8cc4-433b-9631-e2a19bf2ee4f.cpp:13:8: note: declared here    13 | double find_zero(vector<double> xs, vector<double> coeff){       |        ^~~~~~~~~ In file included from e097d759-8cc4-433b-9631-e2a19bf2ee4f.cpp:19: e097d759-8cc4-433b-9631-e2a19bf2ee4f.cpp:36:26: error: cannot convert ‘std::vect or<double>’ to ‘double’    36 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                          ^~~~~~       |                          |       |                          std::vector<double> e097d759-8cc4-433b-9631-e2a19bf2ee4f.cpp:5:20: note:   initializing argument 1 o f ‘double poly(double, std::vector<double>)’     5 | double poly(double x, vector<double> coeff){       |             ~~~~~~~^

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,19 +1,26 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
-
+#include <cassert>
 using namespace std;
 
-double poly(double x, std::vector<double> coeffs){
+double poly(double x, vector<double> coeff){
     double result = 0.0;
-    for(int i = 0; i < coeffs.size(); i++){
-        result += coeffs[i] * pow(x, i);
+    for(int i = 0; i < coeff.size(); i++){
+        result += coeff[i] * pow(x, i);
     }
     return result;
 }
 
-double find_zero(std::vector<double> xs, std::vector<double> coeffs){
-    double a = poly(xs[0], coeffs);
-    double b = poly(xs[1], coeffs);
+double find_zero(vector<double> xs, vector<double> coeff){
+    double a = poly(xs[0], coeff);
+    double b = poly(xs[1], coeff);
     return -b/a;
+}
+
+int main() {
+    vector<double> xs = {1.0, 2.0};
+    vector<double> coeffs = {1.0, 2.0, 1.0};
+    double solution = find_zero(xs, coeffs);
+    assert(abs(poly(solution, coeffs)) < 1e-3);
+    return 0;
 }
