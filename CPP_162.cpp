cdd6c756afd46d62fbb6d94bf2b8f3eda02859e0
beep boop <githubbot@vadim.me>
2024-07-20 01:46:19 +0200

    Solution 62 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kywbzhfz Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from a0cd181e-0b3d-4799-9dc2-6c62f03c9ee0.cpp:2: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ a0cd181e-0b3d-4799-9dc2-6c62f03c9ee0.cpp:9:14: warning: ‘int MD5_Final(unsigned char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]     9 |     MD5_Final(result, &md5ctx);       |     ~~~~~~~~~^~~~~~~~~~~~~~~~~ In file included from a0cd181e-0b3d-4799-9dc2-6c62f03c9ee0.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccHqOGKs.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': a0cd181e-0b3d-4799-9dc2-6c62f03c9ee0.cpp:(.text+0x38): undefined reference to `M D5_Init' /usr/bin/ld: a0cd181e-0b3d-4799-9dc2-6c62f03c9ee0.cpp:(.text+0x71): undefined re ference to `MD5_Update' /usr/bin/ld: a0cd181e-0b3d-4799-9dc2-6c62f03c9ee0.cpp:(.text+0x87): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,13 +1,12 @@
-extern "C" {
-#include <openssl/md5.h>
-}
+#include <string>
+#include <openssl/evp.h>
 
 std::string string_to_md5(const std::string& input) {
     unsigned char result[16];
-    MD5_CTX md5ctx;
-    MD5_Init(&md5ctx);
-    MD5_Update(&md5ctx, input.c_str(), input.size());
-    MD5_Final(result, &md5ctx);
+    EVP_MD_CTX mdctx;
+    EVP_MD_CTX_init(&mdctx);
+    EVP_Update(&mdctx, input.c_str(), input.size());
+    EVP_Finalize(&mdctx, result);
 
     char output[33];
     for (int i = 0; i < 16; i++) {
