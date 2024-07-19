7e4b287604e8aec09b5a75f0a363670005f3e160
beep boop <githubbot@vadim.me>
2024-07-19 19:06:35 +0200

    Solution 73 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/m87khfmf Prompt: 97ac861c-2865-461b-a8c1-6ad4f969fc89.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 97ac861c-2865-461b-a8c1-6ad4f969fc89.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -14,7 +14,6 @@ float triangle_area(float a, float b, float c) {
 }
 
 int main() { 
-    assert(abs(triangle_area(2, 2, 10) - 2.23607) < 0.01);
     std::cout << "Enter side lengths a, b, and c of the triangle: ";
     std::string a_str, b_str, c_str;
     std::cin >> a_str >> b_str >> c_str;
