29969c07a686c948c229694c05f5e1c0f2ac4770
beep boop <githubbot@vadim.me>
2024-07-19 05:46:26 +0200

    Solution 88 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/14cfrwja Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/31ae5972-7c75-4390-93ca-cda78af99 065.py", line 8     ]     ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '(' on l ine 4

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,8 @@
 def sort_third(l: list) -> list:
     l += [0] * (3 - len(l) % 3)
     return [
-        sorted(sublist, key=lambda x: (x, -float('inf')) if isinstance(x, int) else (float('inf'), x) if isinstance(x, str) else (float('inf'), float('inf'))
+        sorted(sublist, key=lambda x: (x, -float('inf')) if isinstance(x, int) else \
+                                    (float('inf'), x) if isinstance(x, str) else (float('inf'), float('inf'))
         for sublist in [
             l[i:i + 3] for i in range(0, len(l), 3)
         ]
