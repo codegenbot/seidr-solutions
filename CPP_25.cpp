547ecf81c28ee5d811f6f295a2f188882c707010
beep boop <githubbot@vadim.me>
2024-07-19 06:22:01 +0200

    Solution 16 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/v44w4b3z Prompt:       |                    ^~~ 301b59ec-4ef7-4e03-a56b-852542abda41.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 301b59ec-4ef7-4e03-a56b-852542abda41.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 301b59ec-4ef7-4e03-a56b-852542abda41.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 301b59ec-4ef7-4e03-a56b-852542abda41.cpp:10: 301b59ec-4ef7-4e03-a56b-852542abda41.cpp: In function ‘int main()’: 301b59ec-4ef7-4e03-a56b-852542abda41.cpp:20:20: error: ‘factorize’ was not decla red in this scope    20 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 301b59ec-4ef7-4e03-a56b-852542abda41.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,13 +1,30 @@
-vector<int> factorize(int n){
+#include <vector> 
+
+vector<int> factorize(int n); 
+
+bool issame(vector<int> a, vector<int> b) { 
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+vector<int> factorize(int n) {
     vector<int> factors;
-    for(int i = 2; i*i <= n; i++){
-        while(n % i == 0){
+    for (int i = 2; i <= n; ++i) {
+        while (n % i == 0) {
             factors.push_back(i);
             n /= i;
         }
     }
-    if(n > 1){
-        factors.push_back(n);
-    }
     return factors;
+}
+
+int main() {
+    assert (issame(factorize(3 * 2 * 3), {2, 3, 3})); 
+
+    return 0;
 }
