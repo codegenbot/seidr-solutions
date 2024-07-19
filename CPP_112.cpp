b3c300d486fdcac0296976d74bb853cc074220d0
beep boop <githubbot@vadim.me>
2024-07-19 21:59:10 +0200

    Solution 26 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sruslc5g Prompt:       |                              ^~~~~~ 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp:15:37: error: ‘string’ was not declared  in this scope    15 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp:14: 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp: In function ‘int main()’: 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp:24:20: error: ‘reverse_delete’ was not declared in this scope    24 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 3246e0e6-c810-4c96-8f6f-5a18a0826bb0.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,16 +1,15 @@
 #include <string>
+
 bool issame(vector<string> a, vector<string> b) {
-    string reverse_delete(string s, string c) {
-        string result = "";
-        for (char ch : s) {
-            if (c.find(ch) == string::npos) {
-                result += ch;
-            }
+    string result = "";
+    for (char ch : s) {
+        if (c.find(ch) == string::npos) {
+            result += ch;
         }
-        string resultStr = result;
-        string revResult = result;
-        reverse(revResult.begin(), revResult.end());
-        bool isPalindrome = (resultStr == revResult);
-        return {result, isPalindrome ? "True" : "False"};
     }
+    string resultStr = result;
+    string revResult = result;
+    reverse(revResult.begin(), revResult.end());
+    bool isPalindrome = (resultStr == revResult);
+    return {result, isPalindrome ? "True" : "False"};
 }
