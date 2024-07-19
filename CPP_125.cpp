f133fb3a76c0136d87cf7f7e42a796cf3d724914
beep boop <githubbot@vadim.me>
2024-07-19 20:12:04 +0200

    Solution 60 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/svlp1004 Prompt:  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp:10:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp:9: 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp: In function ‘int main()’: 5c701d0a-12e6-4f0f-ac4e-eddad5127d3d.cpp:19:20: error: ‘split_words’ was not dec lared in this scope    19 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,25 +1,6 @@
+#include <vector> 
 #include <string>
-#include <vector>
-#include <algorithm>
 
 bool issame(const std::vector<std::string> &a, const std::vector<std::string> &b) {
     return a == b;
-}
-
-std::vector<std::string> split_words(const std::string& str) {
-    std::vector<std::string> words;
-    int start = 0;
-    for (int i = 0; i < str.size(); ++i) {
-        if (str[i] == ' ') {
-            words.push_back(str.substr(start, i - start));
-            start = i + 1;
-        }
-    }
-    words.push_back(str.substr(start)); // Add the last word
-    return words;
-}
-
-int main() {
-    assert(issame(split_words(""), {"0"}));
-    return 0;
 }
