a6de0ffd34fb938f8a4621b3f2276189469b7164
beep boop <githubbot@vadim.me>
2024-07-19 21:03:57 +0200

    Solution 24 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jr17mrsj Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ac8205b8-8886-4a37-abfc-e4edca7472f6.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ac8205b8-8886-4a37-abfc-e4edca7472f6.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ac8205b8-8886-4a37-abfc-e4edca7472f6.cpp:9: ac8205b8-8886-4a37-abfc-e4edca7472f6.cpp: In function ‘int main()’: ac8205b8-8886-4a37-abfc-e4edca7472f6.cpp:19:20: error: ‘sort_third’ was not decl ared in this scope    19 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ ac8205b8-8886-4a37-abfc-e4edca7472f6.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,11 +1,26 @@
-vector<int> sort_third(vector<int> l) {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+vector<int> sort_third(const vector<int>& l) {
     vector<int> res = l;
-    for (int i = 0; i < l.size(); i += 3) {
-        sort(res.begin() + i, res.begin() + i + 3);
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            sort(res.begin() + i, res.begin() + i + 3);
+        }
     }
     return res;
 }
 
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
+}
+
+int main() {
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    cout << "Test Passed!" << endl;
+    return 0;
 }
