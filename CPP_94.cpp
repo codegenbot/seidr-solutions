bde72359bfdaf39450758db8d76e991be5981261
beep boop <githubbot@vadim.me>
2024-07-19 23:46:43 +0200

    Solution 96 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r58ft6fs Prompt:     1 | ```cpp       | ^ ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp:22: ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp: In function ‘int main()’: ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp:24:22: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int*’    24 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~       |                      |       |                      <brace-enclosed initializer list> ab128e4b-17b1-4fd8-8ae9-6ef8cdea69d4.cpp:4:20: note:   initializing argument 1 o f ‘int skjkasdkd(int*, int)’     4 | int skjkasdkd(int* arr, int size) {       |               ~~~~~^~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -15,6 +15,6 @@ int skjkasdkd(int* arr, int size) {
 int testMain() {
     int arr[] = {127, 97, 8192};
     int size = sizeof(arr)/sizeof(int);
-    assert(skjkasdkd(&arr[0], size) == 10); 
+    assert(testMain() == 10); 
     return 0;
 }
