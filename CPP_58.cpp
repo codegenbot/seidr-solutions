7c2fbe5bab7c39016cd4a07da51689bafbe12e4c
beep boop <githubbot@vadim.me>
2024-07-19 22:29:39 +0200

    Solution 50 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt: ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:361:33:   required from ‘void std::_Vector _base<_Tp, _Alloc>::_M_create_storage(size_t) [with _Tp = int; _Alloc = std::all ocator<int>; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:305:9:   required from ‘std::_Vector_base< _Tp, _Alloc>::_Vector_base(size_t, const allocator_type&) [with _Tp = int; _Allo c = std::allocator<int>; size_t = long unsigned int; std::_Vector_base<_Tp, _All oc>::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:524:47:   required from ‘std::vector<_Tp, _Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const value_type&, const al locator_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _ Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::value_type = in t; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int>]’ 5cc59227-ce14-431b-8ce8-0ca2e03a81b1.cpp:21:32:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,7 @@
 ```cpp
 #include <iostream>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
 bool isSame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
@@ -17,17 +17,17 @@ bool isSame(vector<int> a, vector<int> b) {
 
 int main_test() {
     int n;
-    std::cin >> n;
-    std::vector<int> user_input(n, 0); 
+    cin >> n;
+    vector<int> user_input; 
     for (int i = 0; i < n; i++) {
         int x;
-        std::cin >> x;
-        user_input[i] = x; 
+        cin >> x;
+        user_input.push_back(x); 
     }
     if (isSame(user_input, user_input)) {
-        std::cout << "Yes" << std::endl;
+        cout << "Yes" << endl;
     } else {
-        std::cout << "No" << std::endl;
+        cout << "No" << endl;
     }
     return 0;
 }
