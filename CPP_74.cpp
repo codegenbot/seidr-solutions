0a13c3d78a8ca922144b6557276448ff06a441e3
beep boop <githubbot@vadim.me>
2024-07-19 21:31:44 +0200

    Solution 10 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/yyarycmb Prompt:       |                              ^~~~~~ 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp:16: 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp: In function ‘int main()’: 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 1bddea0e-db6f-4bcb-9557-57e9fd4d9927.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,5 +1,13 @@
 #include <string>
 
+int count_chars(vector<string> lst){
+    int count = 0;
+    for(auto str : lst){
+        count += str.size();
+    }
+    return count;
+}
+
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
@@ -9,4 +17,9 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2){
         return lst1;
     }
     return lst2;
+}
+
+int main() {
+    assert(issame(total_match({"this"}, {}), {}));
+    return 0;
 }
