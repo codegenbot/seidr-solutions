b3c60693d4dee35a67695f0a2d4af2d2241c42c9
beep boop <githubbot@vadim.me>
2024-07-19 18:37:40 +0200

    Solution 0 of CPP/106, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vxzt3v3w Prompt: Complete the following code given the task description and function signature.

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,19 +1,20 @@
 #include <vector>
-
 using namespace std;
 
 vector<int> f(int n) {
     vector<int> result(n);
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 0) { // i is even
+    for (int i = 1; i <= n; ++i) {
+        if (i % 2 == 0) {
             int fact = 1;
-            for (int j = 1; j <= i; j++)
+            for (int j = 1; j <= i; ++j) {
                 fact *= j;
+            }
             result[i - 1] = fact;
-        } else { // i is odd
+        } else {
             int sum = 0;
-            for (int j = 1; j <= i; j++)
+            for (int j = 1; j <= i; ++j) {
                 sum += j;
+            }
             result[i - 1] = sum;
         }
     }
