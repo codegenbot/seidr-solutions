221d69739f8b0179a7af25333c2e74a5b3dc0d3f
beep boop <githubbot@vadim.me>
2024-07-19 05:28:43 +0200

    Solution 46 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/14cfrwja Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4721a2bf-4197-4ee4-97d8-b58a2de33 821.py", line 18, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4721a2bf-4197-4ee4-97d8-b58a2de33 821.py", line 13, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4721a2bf-4197-4ee4-97d8-b58a2de33 821.py", line 3, in sort_third     [   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4721a2bf-4197-4ee4-97d8-b58a2de33 821.py", line 4, in <listcomp>     sorted(sublist, key=lambda x: x[2], reverse=True)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4721a2bf-4197-4ee4-97d8-b58a2de33 821.py", line 4, in <lambda>     sorted(sublist, key=lambda x: x[2], reverse=True)                                   ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -3,9 +3,11 @@ def sort_third(l: list):
         [
             sorted(sublist, key=lambda x: x, reverse=True)
             for sublist in [
-                l[i : i + 3] for i in range(0, len(l), 3) if len(l[i : i + 3]) >= 3
+                l[i : i + 3] 
+                for i in range(0, len(l), 3) 
+                if len(l[i : i + 3]) >= 3
             ]
         ],
-        key=lambda x: x,
+        key=lambda x: x[2],
         reverse=True
     )
