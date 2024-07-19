cd9aa36b13c6b8879d30a5dc3f597efe1e267f55
beep boop <githubbot@vadim.me>
2024-07-20 01:47:17 +0200

    Solution 20 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0vyyuw2q Prompt:       |                    ^~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:11: 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp: In function ‘int main()’: 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5fc98e9c-1a88-4c52-9bf6-069b6e3cf240.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,20 +1,8 @@
 #include <vector>
-#include <algorithm>
+#include <iostream>
 
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (abs(a[i] - b[i]) > 1e-9) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
