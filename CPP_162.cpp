bf4d139ce7746b4493878d958375643c0bf618d1
beep boop <githubbot@vadim.me>
2024-07-20 01:46:54 +0200

    Solution 63 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kywbzhfz Prompt: bad94281-641c-4ba2-bd06-403f426c5767.cpp:5:6: error: ‘string’ in namespace ‘std’  does not name a type     5 | std::string string_to_md5(const std::string& input) {       |      ^~~~~~ bad94281-641c-4ba2-bd06-403f426c5767.cpp:3:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     2 | #include <openssl/md5.h>   +++ |+#include <string>     3 | } In file included from bad94281-641c-4ba2-bd06-403f426c5767.cpp:19: bad94281-641c-4ba2-bd06-403f426c5767.cpp: In function ‘int main()’: bad94281-641c-4ba2-bd06-403f426c5767.cpp:21:13: error: ‘string_to_md5’ was not d eclared in this scope    21 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,16 +1,18 @@
 #include <string>
-#include <openssl/evp.h>
+
+extern "C" {
+    #include <openssl/md5.h>
+}
 
 std::string string_to_md5(const std::string& input) {
     unsigned char result[16];
-    EVP_MD_CTX mdctx;
-    EVP_MD_CTX_init(&mdctx);
-    EVP_Update(&mdctx, input.c_str(), input.size());
-    EVP_Finalize(&mdctx, result);
+    MD5_CTX md5ctx;
+    MD5_Init(&md5ctx);
+    MD5_Update(&md5ctx, input.c_str(), input.size());
+    MD5_Final(result, &md5ctx);
 
     char output[33];
     for (int i = 0; i < 16; i++) {
         sprintf(output + i*2, "%02x", result[i]);
     }
     return std::string(output);
-}
