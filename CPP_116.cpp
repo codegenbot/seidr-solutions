f59ad3a9fc3d1f905919c8fad3de2ee1563b6697
beep boop <githubbot@vadim.me>
2024-07-19 11:15:18 +0200

    Solution 44 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/p70n7sfp Prompt:       |                    ^~~ f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp:12: f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp: In function ‘int main()’: f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp:22:20: error: ‘sort_array’ was not decl ared in this scope    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ f948f31c-68a8-458c-a0bc-3cf676b9032e.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,19 +1,3 @@
 bool issame(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end(), [](int x, int y) {
-        int count_x = __builtin_popcount(x);
-        int count_y = __builtin_popcount(y);
-        if (count_x == count_y) {
-            return x < y;
-        }
-        return count_x < count_y;
-    });
-    sort(b.begin(), b.end(), [](int x, int y) {
-        int count_x = __builtin_popcount(x);
-        int count_y = __builtin_popcount(y);
-        if (count_x == count_y) {
-            return x < y;
-        }
-        return count_x < count_y;
-    });
-    return a == b;
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
