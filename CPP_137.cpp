26317a965cb12279b56fd454558cf401b28890a3
beep boop <githubbot@vadim.me>
2024-07-19 05:10:14 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/vocsxz1q Prompt:    28 | return "None";       | ^~~~~~ 6e93ceb0-bdff-4b16-9834-d330afc63e6d.cpp:29:1: error: expected declaration befor e ‘}’ token    29 | }       | ^ In file included from 6e93ceb0-bdff-4b16-9834-d330afc63e6d.cpp:31: 6e93ceb0-bdff-4b16-9834-d330afc63e6d.cpp: In function ‘int main()’: 6e93ceb0-bdff-4b16-9834-d330afc63e6d.cpp:33:13: error: ‘boost’ has not been decl ared    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 6e93ceb0-bdff-4b16-9834-d330afc63e6d.cpp:33:29: error: ‘string’ was not declared  in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 6e93ceb0-bdff-4b16-9834-d330afc63e6d.cpp:33:37: error: ‘compare_one’ was not dec lared in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -15,16 +15,18 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string str_a = boost::any_cast<std::string>(a);
-        std::string str_b = boost::any_cast<std::string>(b);
-        if (str_a.find(".") != std::string::npos || str_a.find(",") != std::string::npos) {
-            str_a.erase(std::remove(str_a.begin(), str_a.end(), ','), str_a.end());
-            str_b.erase(std::remove(str_b.begin(), str_b.end(), ','), str_b.end());
-        }
-        if (std::stof(str_a) > std::stof(str_b)) {
-            return a;
-        } else if (std::stof(str_a) < std::stof(str_b)) {
-            return b;
+        if (a.type().name() == typeid(std::string).name()) {
+            std::string str_a = boost::any_cast<std::string>(a);
+            std::string str_b = boost::any_cast<std::string>(b);
+            if (str_a.find(".") != std::string::npos || str_a.find(",") != std::string::npos) {
+                str_a.erase(std::remove(str_a.begin(), str_a.end(), ','), str_a.end());
+                str_b.erase(std::remove(str_b.begin(), str_b.end(), ','), str_b.end());
+            }
+            if (stof(str_a) > stof(str_b)) {
+                return a;
+            } else if (stof(str_a) < stof(str_b)) {
+                return b;
+            }
         }
     }
     return boost::any(nullptr);
