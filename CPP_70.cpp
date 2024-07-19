37d67ebaa1e876d29740c9603cc6f43efd98c822
beep boop <githubbot@vadim.me>
2024-07-19 18:49:53 +0200

    Solution 57 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/krf9kxcb Prompt:       |                                  ^~~ 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp:40:39: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp:39: 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp: In function ‘int main()’: 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp:49:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    49 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 57824dac-7739-46a7-a69d-84115cc7b9d5.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -25,32 +25,6 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
     return result;
 }
 
-std::vector<int> strange_sort_vector(std::vector<int> lst) {
-    std::vector<int> result;
-    if (lst.empty()) return result;
-
-    int min_val = *min_element(lst.begin(), lst.end());
-    int max_val = *max_element(lst.begin(), lst.end());
-
-    while (!lst.empty()) {
-        auto it_min = std::find_if(lst.begin(), lst.end(), [min_val](int x) { return x == min_val; });
-        if (it_min != lst.end()) {
-            result.push_back(*it_min);
-            lst.erase(it_min);
-        }
-
-        auto it_max = std::find_if(lst.begin(), lst.end(), [max_val](int x) { return x == max_val; });
-        if (it_max != lst.end()) {
-            result.push_back(*it_max);
-            lst.erase(it_max);
-            min_val = *min_element(lst.begin(), lst.end());
-            max_val = *max_element(lst.begin(), lst.end());
-        }
-    }
-
-    return result;
-}
-
 int main() {
     std::vector<int> a = {1, 2, 3};
     std::vector<int> b = strange_sort_vector(a);
