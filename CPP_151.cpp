f29c199c6aacfaddd46d0125efd78ced5c6c2975
beep boop <githubbot@vadim.me>
2024-07-19 22:32:37 +0200

    Solution 55 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/2ck4sdj5 Prompt: In file included from d8d72c84-28ae-4e38-a171-fc7df7a16fe0.cpp:14: d8d72c84-28ae-4e38-a171-fc7df7a16fe0.cpp: In function ‘int main()’: d8d72c84-28ae-4e38-a171-fc7df7a16fe0.cpp:16:35: error: ‘lst’ was not declared in  this scope    16 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ d8d72c84-28ae-4e38-a171-fc7df7a16fe0.cpp:16:43: error: ‘odd_sum’ was not declare d in this scope    16 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -14,7 +14,7 @@ long long double_the_difference(std::vector<float> lst){
 
 int main() {
     std::vector<float> lst = {1.0, 2.0, 3.0, 4.0, 5.0};
-    long long odd_sum = 29; 
+    long long odd_sum = 29;
 
     assert(double_the_difference(lst) == odd_sum);
 
