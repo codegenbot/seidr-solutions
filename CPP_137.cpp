787173a07077db6a6ebea758e4b82d2bae5daf82
beep boop <githubbot@vadim.me>
2024-07-19 20:39:21 +0200

    Solution 29 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pkupi6nj Prompt: 54d8c731-de01-455a-bdaa-15b3e0036e41.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -11,13 +11,13 @@ boost::any compare_one(boost::any a, boost::any b) {
         return (da > db) ? a : ((da < db) ? b : boost::any("None"));
     } else if (is_any_of<a>(std::string())) {
         std::string sa = any_cast<std::string>(a);
-        double da = std::stod(sa);
-        
         if (is_any_of<b>(double())) {
             double db = any_cast<double>(b);
+            double da = std::stod(sa);
             return (da > db) ? a : ((da < db) ? b : boost::any("None"));
         } else if (is_any_of<b>(std::string())) {
             std::string sb = any_cast<std::string>(b);
+            double da = std::stod(sa);
             double db = std::stod(sb);
             return (da > db) ? a : ((da < db) ? b : boost::any("None"));
         } else {
