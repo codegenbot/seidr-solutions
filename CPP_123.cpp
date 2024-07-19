3e418735dae28df2a448b103c0d8ad132c7add99
beep boop <githubbot@vadim.me>
2024-07-20 01:09:48 +0200

    Solution 76 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8msjkoba Prompt:       |                    ^~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:26:27: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:25: d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp: In function ‘int main()’: d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:35:20: error: ‘get_odd_collatz’ was not  declared in this scope    35 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ d1e24284-e29f-49e8-84fc-fbb3c35cfcf3.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,13 +4,32 @@
 
 using namespace std;
 
-bool issame(const vector<int>& a, const vector<int>& b){
-    return a == b;
-}
-
 vector<int> get_odd_collatz(int n){
     vector<int> result;
-    // implementation remains the same
+    while(n != 1){
+        result.push_back(n);
+        if(n % 2 == 0){
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    result.push_back(1);
+    
+    vector<int> odd_numbers;
+    for(int num : result){
+        if(num % 2 != 0){
+            odd_numbers.push_back(num);
+        }
+    }
+    
+    sort(odd_numbers.begin(), odd_numbers.end());
+    
+    return odd_numbers;
+}
+
+bool issame(const vector<int>& a, const vector<int>& b){
+    return a == b;
 }
 
 int main(){
