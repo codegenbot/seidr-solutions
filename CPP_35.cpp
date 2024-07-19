03dd304a61559155e0da6ec16e23623792c357ea
beep boop <githubbot@vadim.me>
2024-07-19 19:18:20 +0200

    Solution 86 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gahd0o9k Prompt: f ‘std’     2 | int max_value(std::vector<int> l) {       |                    ^~~~~~ 3960201f-a0a4-4785-a00a-f44c990768ad.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | ``` 3960201f-a0a4-4785-a00a-f44c990768ad.cpp:2:27: error: expected primary-expressio n before ‘int’     2 | int max_value(std::vector<int> l) {       |                           ^~~ In file included from 3960201f-a0a4-4785-a00a-f44c990768ad.cpp:6: 3960201f-a0a4-4785-a00a-f44c990768ad.cpp: In function ‘int main()’: 3960201f-a0a4-4785-a00a-f44c990768ad.cpp:8:17: error: ‘max_element’ was not decl ared in this scope     8 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 3960201f-a0a4-4785-a00a-f44c990768ad.cpp:8:13: error: ‘abs’ was not declared in this scope     8 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -5,3 +5,7 @@
 int max_value(std::vector<int> l) {
     return *std::max_element(l.begin(), l.end());
 }
+
+int main() {
+    assert (std::abs(*std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 124)<1e-4);
+}
