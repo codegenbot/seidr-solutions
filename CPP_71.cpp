806f0937a6e7a1692a62c44cd2f2072408447951
beep boop <githubbot@vadim.me>
2024-07-20 00:15:43 +0200

    Solution 9 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wuanxsfn Prompt: Complete the following code given the task description and function signature.

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,7 +1,8 @@
-if (a + b > c && a + c > b && b + c > a) {
+if(a + b > c && a + c > b && b + c > a) {
         float s = (a + b + c) / 2;
-        return roundf(sqrt(s * (s - a) * (s - b) * (s - c)) * 100) / 100;
-    } else {
+        return sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+    else {
         return -1;
     }
 }
