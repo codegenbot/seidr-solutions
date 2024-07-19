29f5175df9344c051ccd5d5059b326fe8d9c33b9
beep boop <githubbot@vadim.me>
2024-07-20 01:50:02 +0200

    Solution 89 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/j8nbwnoj Prompt: efore numeric constant    22 | assert(issame(intersperse(vector<int>{2, 2, 2}, 2), vector<int>{2, 2, 2,  2, 2}));       |                                                 ^ 10adb19a-f988-490b-8964-77beea9714ab.cpp:22:79: error: expected unqualified-id b efore ‘)’ token    22 | ame(intersperse(vector<int>{2, 2, 2}, 2), vector<int>{2, 2, 2, 2, 2}));       |                                                                     ^ In file included from 10adb19a-f988-490b-8964-77beea9714ab.cpp:24: 10adb19a-f988-490b-8964-77beea9714ab.cpp: In function ‘int main()’: 10adb19a-f988-490b-8964-77beea9714ab.cpp:34:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    34 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 10adb19a-f988-490b-8964-77beea9714ab.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 10adb19a-f988-490b-8964-77beea9714ab.cpp:25:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
-}
-
 vector<int> intersperse(const vector<int>& numbers, int delimiter) {
     vector<int> result;
     for (size_t i = 0; i < numbers.size(); ++i) {
@@ -19,7 +15,4 @@ vector<int> intersperse(const vector<int>& numbers, int delimiter) {
     return result;
 }
 
-int main() {
-    assert(issame(intersperse({2, 2, 2}, 2), vector<int>{2, 2, 2, 2, 2}));
-    return 0;
-}
+assert(issame(intersperse(vector<int>{2, 2, 2}, 2), vector<int>{2, 2, 2, 2, 2}));
