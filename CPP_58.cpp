f1061a8ef68bb7c662448a63b271a1db18f41b72
beep boop <githubbot@vadim.me>
2024-07-19 22:19:00 +0200

    Solution 35 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt: 6b598ca5-29a3-4f60-877a-4ccf7c84631e.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 6b598ca5-29a3-4f60-877a-4ccf7c84631e.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 6b598ca5-29a3-4f60-877a-4ccf7c84631e.cpp:34: 6b598ca5-29a3-4f60-877a-4ccf7c84631e.cpp: In function ‘int main()’: 6b598ca5-29a3-4f60-877a-4ccf7c84631e.cpp:44:20: error: ‘common’ was not declared  in this scope    44 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -17,11 +18,11 @@ bool isSame(vector<int> a, vector<int> b) {
 int main() {
     int n;
     cin >> n;
-    vector<int> user_input;
+    vector<int> user_input(n); 
     for (int i = 0; i < n; i++) {
         int x;
         cin >> x;
-        user_input.push_back(x);
+        user_input[i] = x; 
     }
     if (isSame(user_input, user_input)) {
         cout << "Yes" << endl;
