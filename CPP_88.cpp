b686be1339b86f2437944386ac2c73a08d564a73
beep boop <githubbot@vadim.me>
2024-07-19 22:33:39 +0200

    Solution 1 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0oblx7zu Prompt:       |                    ^~~ 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp:14: 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp: In function ‘int main()’: 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 26604d8f-6765-40ff-95c2-d4c138a04f03.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool issame(vector<int> a,vector<int>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0 ; i < a.size() ; i++ )
+        if(a[i] != b[i])
+            return false;
+    return true;
+}
+
 vector<int> sort_array(vector<int> array) {
     int sum = array[0] + array[array.size() - 1];
     if (sum % 2 == 0) {
@@ -9,4 +24,9 @@ vector<int> sort_array(vector<int> array) {
         sort(result.begin(), result.end());
         return result;
     }
+}
+
+int main() {
+    assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));
+    return 0;
 }
