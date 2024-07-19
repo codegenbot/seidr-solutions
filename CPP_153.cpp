790b2b16c8256a689899d8c31db92379369c2246
beep boop <githubbot@vadim.me>
2024-07-19 12:40:07 +0200

    Solution 64 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/balmv2m8 Prompt: 88039217-969f-45c4-85c9-67c1c666755c.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 88039217-969f-45c4-85c9-67c1c666755c.cpp:22: 88039217-969f-45c4-85c9-67c1c666755c.cpp: In function ‘int main()’: 88039217-969f-45c4-85c9-67c1c666755c.cpp:24:13: error: ‘Strongest_Extension’ was  not declared in this scope    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -2,9 +2,14 @@
 #include <vector>
 #include <string>
 #include <cctype>
+#include <climits>
 using namespace std;
 
 string Strongest_Extension(string class_name, vector<string> extensions);
+
+int main() {
+    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+}
 
 string Strongest_Extension(string class_name, vector<string> extensions) {
     string strongest_extension = "";
@@ -25,9 +30,4 @@ string Strongest_Extension(string class_name, vector<string> extensions) {
         }
     }
     return class_name + "." + strongest_extension;
-}
-
-int main() {
-    cout << Strongest_Extension("Sp", {"671235", "Bb"}) << endl; // Output: Sp.671235
-    return 0;
 }
