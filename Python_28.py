ed87357c650c424687bdb525c10b14f2504fa7bc
beep boop <githubbot@vadim.me>
2024-07-19 17:39:20 +0200

    Solution 21 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/7a0267dg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a86da1a-43eb-463d-8e84-da069fcde 781.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,11 +1,8 @@
 from typing import List
 
+
 def concatenate(strings: List[str]) -> str:
-    result = ''
-    for s in strings:
-        if s.startswith('http'):
-            continue
-        if len(s) > 10:
-            s = s[:10] + '...'
-        result += s + '\n'
+    result = ""
+    for string in strings:
+        result += string
     return result
