a07e02ac73d131da4c7f16a432827415d26e03e4
beep boop <githubbot@vadim.me>
2024-07-19 23:44:18 +0200

    Solution 37 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v06y9f55 Prompt:       |             ^~~~~~ 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp:27: 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp: In function ‘int main()’: 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp:37:20: error: ‘get_odd_collatz’ was not  declared in this scope    37 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 3c2fb09b-1fed-4048-932d-0c84b7b7eb13.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,24 +1,24 @@
-```cpp
 #include <vector>
 #include <assert.h>
-#define _SILENT 1
+#include <initializer_list>
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i])return false;
-    }
-    return true;
-}
-
-vector<int> get_odd_collatz(int n) {
-    vector<int> result = {1};
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> result;
     while(n!=1){
         if(n%2==0)n=n/2;
         else n=3*n+1;
         result.push_back(n);
     }
     return result;
+}
+
+bool issame(std::vector<int> a,std::vector<int>b){
+    if(a.size()!=b.size())return false;
+    for(int i=0;i<a.size();i++){
+        if(a[i]!=b[i])return false;
+    }
+    return true;
+
 }
 
 int main() {
