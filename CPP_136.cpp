3933def9f9e09b37a7a5f1edd091f1e078c5af14
beep boop <githubbot@vadim.me>
2024-07-19 04:07:01 +0200

    Solution 27 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mr70wchs Prompt:    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 33f05774-6b50-4ca0-97f6-eea65dfa9f43.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 33f05774-6b50-4ca0-97f6-eea65dfa9f43.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 33f05774-6b50-4ca0-97f6-eea65dfa9f43.cpp:14: 33f05774-6b50-4ca0-97f6-eea65dfa9f43.cpp: In function ‘int main()’: 33f05774-6b50-4ca0-97f6-eea65dfa9f43.cpp:24:20: error: ‘largest_smallest_integer s’ was not declared in this scope    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 33f05774-6b50-4ca0-97f6-eea65dfa9f43.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,14 +1,20 @@
 vector<int> largest_smallest_integers(vector<int> lst);
 
+bool issame(vector<int> a, vector<int> b) {
+    vector<int> result = largest_smallest_integers(a);
+    vector<int> expected = largest_smallest_integers(b);
+    return result == expected;
+}
+
 vector<int> largest_smallest_integers(vector<int> lst){
     int max_neg = INT_MIN, min_pos = INT_MAX;
     for(int num : lst){
-        if(num < 0 && num < max_neg){
+        if(num < 0 && num > max_neg){
             max_neg = num;
         }
-        if(num > 0 && (num < min_pos)){
+        if(num > 0 && num < min_pos){
             min_pos = num;
         }
     }
-    return {max_neg, min_pos};
+    return {max_neg, (min_pos == INT_MAX ? 0 : min_pos)};
 }
