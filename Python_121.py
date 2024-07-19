c58e0c7232e11879f9283b15c5192f52b9a2a387
beep boop <githubbot@vadim.me>
2024-07-19 17:19:35 +0200

    Solution 79 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fecc721d-ba68-43e9-a2e3-477fb3255 817.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,6 +1,5 @@
-def count_even_numbers(input_list):
-    return sum(i % 2 == 0 for i in input_list)
-
+def solution(input_list):
+    return sum(i for i in input_list if i % 2 == 0)
 
 input_list = [4, 2, 6]
-print(count_even_numbers(input_list))
+print(solution(input_list))
