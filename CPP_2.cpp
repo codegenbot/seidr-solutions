0465ceefb8898280eeddfca3bd24ba8e8b660996
beep boop <githubbot@vadim.me>
2024-07-19 21:03:07 +0200

    Solution 0 of CPP/2, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pjkzvu01 Prompt: Complete the following code given the task description and function signature.

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,8 +1,8 @@
-#include <cmath>
-
+#include<stdio.h>
+#include<math.h>
 using namespace std;
 
-float truncate_number(float number) {
-    int integerPart = (int)number;
-    return number - integerPart;
+float truncate_number(float number){
+    int integer_part = (int)number;
+    return number - integer_part;
 }
