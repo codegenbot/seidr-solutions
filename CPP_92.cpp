88bc4412a7f41ee9bcc77328f5ed476497644388
beep boop <githubbot@vadim.me>
2024-07-19 23:45:21 +0200

    Solution 80 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -4,15 +4,10 @@
 using namespace std;
 
 int mainFunction() {
-    string str1, str2, str3;
+    int a, b, c;
     cout << "Enter three numbers: ";
-    if (!(cin >> str1) || !(cin >> str2) || !(cin >> str3)) {
-        cout << "Error: Please enter valid integer values.\n";
-    } else {
-        int a = stoi(str1), b = stoi(str2), c = stoi(str3);
-        assert(a == 3 && b == 4 && c == 7);
-        cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
-    }
+    cin >> a >> b >> c; 
+    cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
 
     return 0;
 }
