b9b0a5a86632ac2cd55e6ed115568e0db6fdbc4f
beep boop <githubbot@vadim.me>
2024-07-19 22:17:22 +0200

    Solution 62 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eoo9avp7 Prompt:    10 |         if (abs(a[i] - b[i]) > 1e-6) {       |             ^~~ 269c2f97-0a87-478c-a663-6fd9ab684400.cpp: At global scope: 269c2f97-0a87-478c-a663-6fd9ab684400.cpp:18:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    18 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 269c2f97-0a87-478c-a663-6fd9ab684400.cpp:5:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     5 | bool issame(std::vector<float> a, std::vector<float> b) {       |      ^~~~~~ 269c2f97-0a87-478c-a663-6fd9ab684400.cpp: In function ‘bool issame(std::vector<f loat>, std::vector<float>)’: 269c2f97-0a87-478c-a663-6fd9ab684400.cpp:22:13: error: ‘abs’ was not declared in  this scope    22 |         if (abs(a[i]-b[i])>1e-4) return false;       |             ^~~ In file included from 269c2f97-0a87-478c-a663-6fd9ab684400.cpp:17: 269c2f97-0a87-478c-a663-6fd9ab684400.cpp: In function ‘int main()’: 269c2f97-0a87-478c-a663-6fd9ab684400.cpp:27:20: error: ‘get_positive’ was not de clared in this scope    27 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,45 +1,20 @@
-```
 #include <vector>
 #include <cmath>
 
 using namespace std;
-
-bool get_positive(float x) {
-    if (x > 0)
-        return true;
-    else
-        return false;
-}
-
-int main() {
-    vector<float> a;
-    float num;
-    
-    cout << "Enter the numbers: ";
-    while (cin >> num) {
-        a.push_back(num);
-    }
-    
-    vector<float> b(a);
-    reverse(b.begin(), b.end());
-    
-    if (issame(a, b)) {
-        cout << "The vectors are same." << endl;
-    } else {
-        cout << "The vectors are not same." << endl;
-    }
-    
-    return 0;
-}
 
 bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (std::abs(a[i] - b[i]) > 1e-6) {
+        if (abs(a[i] - b[i]) > 1e-6) {
             return false;
         }
     }
     return true;
+}
+
+float get_positive(float x) {
+    return x > 0 ? x : 0.0f;
 }
