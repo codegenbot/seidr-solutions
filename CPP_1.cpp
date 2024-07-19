c18fac652911120fc56fe39e296f7377593c9535
beep boop <githubbot@vadim.me>
2024-07-20 01:39:11 +0200

    Solution 59 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/l0ivx5b2 Prompt: his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cfab63d6-243d-4f66-8875-842830aa27e7.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ cfab63d6-243d-4f66-8875-842830aa27e7.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ cfab63d6-243d-4f66-8875-842830aa27e7.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from cfab63d6-243d-4f66-8875-842830aa27e7.cpp:39: cfab63d6-243d-4f66-8875-842830aa27e7.cpp: In function ‘int main()’: cfab63d6-243d-4f66-8875-842830aa27e7.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -27,11 +27,11 @@ std::vector<std::string> separate_paren_groups(std::string s) {
             }
         }
     }
-    
+
     return groups;
 }
 
 int main() {
-    assert(isSame(separate_paren_groups("( ) (( )) (( )( ))"), std::vector<std::string>{"()", "(())", "(()())"}));
+    assert(isSame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
     return 0;
 }
