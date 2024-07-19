9f2ff8b5572eee9ed886700634ac5a7753c21f8f
beep boop <githubbot@vadim.me>
2024-07-20 00:20:29 +0200

    Solution 43 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d7de4bj4 Prompt: 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp:1:1: error: stray ‘`’ in program     1 | ```c++       | ^ 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp:1:4: error: ‘c’ does not name a type     1 | ```c++       |    ^ In file included from 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp:4: 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp: In function ‘int main()’: 86b90cee-e0cc-49e3-af95-b3bbcf5df975.cpp:6:13: error: ‘fix_spaces’ was not decla red in this scope     6 |     assert (fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");       |             ^~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,1 +1,18 @@
-std::cout << fix_spaces("   Exa 1 2 2 mple");
+#include <iostream>
+#include <string>
+
+std::string fix_spaces(const std::string& str) {
+    size_t start = 0;
+    while (start < str.size() && isspace(str[start])) {
+        start++;
+    }
+    size_t end = str.size();
+    while (end > start && isspace(str[end - 1])) {
+        end--;
+    }
+    return str.substr(start, end - start);
+}
+
+int main() {
+    std::cout << fix_spaces("   Exa 1 2 2 mple");
+}
