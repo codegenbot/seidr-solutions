c891a5d063e2df70c031e50c8fcc97eb4054138e
beep boop <githubbot@vadim.me>
2024-07-19 02:02:10 +0200

    Solution 1 of Python/46, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/onxzfoqv Prompt: Complete the following code given the task description and function signature.

--- Python_46.py
+++ Python_46.py
@@ -1,12 +1,5 @@
 def fib4(n: int):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 0
-    if n == 2:
-        return 2
     a, b, c, d = 0, 0, 2, 0
-    for i in range(3, n+1):
-        d = a + b + c + d
-        a, b, c = b, c, d
+    for _ in range(4, n + 1):
+        a, b, c, d = b, c, d, a + b + c + d
     return d
