dd0fb216c1b5d246ff469ab877ec62c9b4ad31c4
beep boop <githubbot@vadim.me>
2024-07-19 13:45:20 +0200

    Solution 80 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6dh2sbiq Prompt:       |                                             ^ In file included from 2ccdca20-dfa7-497e-9198-1efdd22ac35a.cpp:9: 2ccdca20-dfa7-497e-9198-1efdd22ac35a.cpp: In function ‘int main()’: 2ccdca20-dfa7-497e-9198-1efdd22ac35a.cpp:19:39: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    19 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~       |                                       |       |                                       <brace-enclosed initializer list> 2ccdca20-dfa7-497e-9198-1efdd22ac35a.cpp:1:26: note:   initializing argument 1 o f ‘void filter_by_substring(int)’     1 | void filter_by_substring(const std::vector<std::string>& vec, const std: :string& substr) {       |                          ^~~~~~~~~~~~~~~~~ In file included from 2ccdca20-dfa7-497e-9198-1efdd22ac35a.cpp:9: 2ccdca20-dfa7-497e-9198-1efdd22ac35a.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -3,11 +3,11 @@ std::vector<std::string> filter_by_substring(const std::vector<std::string>& vec
     temp_vec.erase(std::remove_if(temp_vec.begin(), temp_vec.end(), [substr](const std::string& s) {
         return s.find(substr) != std::string::npos;
     }), temp_vec.end());
-    
     return temp_vec;
 }
 
 int main() {
     assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == std::vector<std::string>{"grunt", "prune"});
+    
     return 0;
 }
