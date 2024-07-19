06a2e1470aedf1078f5952432dec58c1ca5990d4
beep boop <githubbot@vadim.me>
2024-07-19 23:46:55 +0200

    Solution 81 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt: In file included from d1ebec86-a65b-4fc7-89b4-65b60d0ca973.cpp:20: d1ebec86-a65b-4fc7-89b4-65b60d0ca973.cpp: In function ‘int main()’: d1ebec86-a65b-4fc7-89b4-65b60d0ca973.cpp:22:13: error: ‘any_int’ was not declare d in this scope; did you mean ‘u_int’?    22 |     assert (any_int(3.01,4,7)==false);       |             ^~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,13 +1,19 @@
+```cpp
 #include <iostream>
 #include <cassert>
 
 using namespace std;
 
 int mainFunction() {
-    int a, b, c;
+    string str1, str2, str3;
     cout << "Enter three numbers: ";
-    cin >> a >> b >> c; 
-    cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
+    if (!(cin >> str1) || !(cin >> str2) || !(cin >> str3)) {
+        cout << "Error: Please enter valid integer values.\n";
+    } else {
+        int a = stoi(str1), b = stoi(str2), c = stoi(str3);
+        static_assert(a == 3 && b == 4 && c == 7, "Incorrect input values");
+        cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
+    }
 
     return 0;
 }
