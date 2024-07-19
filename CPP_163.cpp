004110659af91525b9644c716bccf5b6a738062e
beep boop <githubbot@vadim.me>
2024-07-19 06:08:59 +0200

    Solution 64 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/d15ibz0y Prompt:       |           ^~~~~~ In file included from d0c79482-cd3a-4deb-a923-950663f320ad.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d0c79482-cd3a-4deb-a923-950663f320ad.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d0c79482-cd3a-4deb-a923-950663f320ad.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d0c79482-cd3a-4deb-a923-950663f320ad.cpp:13: d0c79482-cd3a-4deb-a923-950663f320ad.cpp: In function ‘int main()’: d0c79482-cd3a-4deb-a923-950663f320ad.cpp:23:20: error: ‘generate_integers’ was n ot declared in this scope    23 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ d0c79482-cd3a-4deb-a923-950663f320ad.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,10 +1,19 @@
 #include <vector>
+#include <algorithm>
 
-vector<int> issame(int a, int b) {
-    vector<int> result;
-    for(int i = a; i <= b; ++i) {
-        if(i % 3 == 0) {
-            result.push_back(i);
+std::vector<int> generate_integers(int a, int b) {
+    std::vector<int> result;
+    for (int i = a; i <= b; ++i) {
+        result.push_back(i);
+    }
+    return result;
+}
+
+std::vector<int> issame(std::vector<int> a, std::vector<int> b) {
+    std::vector<int> result;
+    for (int num : a) {
+        if (std::find(b.begin(), b.end(), num) != b.end()) {
+            result.push_back(num);
         }
     }
     return result;
