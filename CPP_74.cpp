7a01782884a276b1ec53db86f32e9280b3b48c76
beep boop <githubbot@vadim.me>
2024-07-19 02:44:40 +0200

    Solution 50 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duyg3to1 Prompt:       |                                 ^~~~~~ In file included from d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp:22: d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp: In function ‘int main()’: d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp:32:20: error: ‘total_match’ was not dec lared in this scope    32 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ d6b3df10-7da5-4dfd-9dea-c78a89288918.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,8 +2,18 @@
 #include <string>
 #include <cassert>
 
+vector<string> total_match(vector<string> lst1, vector<string> lst2);
+
+int total_chars(vector<string> lst) {
+    int total = 0;
+    for (string s : lst) {
+        total += s.length();
+    }
+    return total;
+}
+
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    if (lst1.empty() || !lst2.empty()) {
+    if (total_chars(lst1) < total_chars(lst2)) {
         return lst1;
     } else {
         return lst2;
