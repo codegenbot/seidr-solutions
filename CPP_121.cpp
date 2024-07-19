9f22a2b19962e9fb887d54583871e184840bbcfd
beep boop <githubbot@vadim.me>
2024-07-19 23:56:59 +0200

    Solution 45 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bpf4p6hp Prompt: ic_string<char>*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allo cator<char> >}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator< std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::ve ctor<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char > >}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx 11::basic_string<char> >; std::vector<_Tp, _Alloc>::reference = std::__cxx11::ba sic_string<char>&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_strin g<char> >; std::vector<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<cha r>]’ f451fd70-8ea9-48ec-b7ee-9045425c1425.cpp:25:26:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::__cxx11::basic_string<char> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -13,16 +13,15 @@ int solution(std::vector<std::string> numbers) {
 }
 
 int main() {
-    std::vector<std::string> numbers; 
-    int n; 
+    size_t n;
     std::cout << "Enter the number of elements: ";
     std::cin >> n;
-    numbers.resize(n); 
-    for (int i = 0; i < n; i++) {
+    std::vector<std::string> numbers(n);  
+    for (size_t i = 0; i < n; i++) {
         int num;
         std::cout << "Enter element " << i + 1 << ": ";
         std::cin >> num;
-        numbers[i] = std::to_string(num);
+        numbers.push_back(std::to_string(num));
     }
     std::cout << "Sum of odd elements: " << solution(numbers) << std::endl;
 }
