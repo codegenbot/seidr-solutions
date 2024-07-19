f3699e297769d0b08b79c469e4ed76433bd3ca62
beep boop <githubbot@vadim.me>
2024-07-19 02:17:30 +0200

    Solution 5 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/118zuiik Prompt: in ‘brackets’, which is of non-class type ‘const int’     5 |     for (size_t i = 0; i < brackets.size(); ++i) {       |                                     ^~~~ 76bb426f-906d-4324-8db4-8c922e1aff5c.cpp:6:21: error: invalid types ‘const int[s ize_t {aka long unsigned int}]’ for array subscript     6 |         if (brackets[i] == '(') {       |                     ^ 76bb426f-906d-4324-8db4-8c922e1aff5c.cpp:8:28: error: invalid types ‘const int[s ize_t {aka long unsigned int}]’ for array subscript     8 |         } else if (brackets[i] == ')') {       |                            ^ In file included from 76bb426f-906d-4324-8db4-8c922e1aff5c.cpp:18: 76bb426f-906d-4324-8db4-8c922e1aff5c.cpp: In function ‘int main()’: 76bb426f-906d-4324-8db4-8c922e1aff5c.cpp:20:37: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    20 |     assert (not (correct_bracketing("()()(()())()))()")));       |                                     ^~~~~~~~~~~~~~~~~~       |                                     |       |                                     const char* 76bb426f-906d-4324-8db4-8c922e1aff5c.cpp:3:39: note:   initializing argument 1 o f ‘bool correct_bracketing(const int&)’     3 | bool correct_bracketing(const string& brackets) {       |                         ~~~~~~~~~~~~~~^~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,11 +1,11 @@
 #include <string>
 
-bool correct_bracketing(const string& brackets) {
+bool correct_bracketing(const std::string& brackets) {
     int count = 0;
-    for (char c : brackets) {
-        if (c == '(') {
+    for (size_t i = 0; i < brackets.size(); ++i) {
+        if (brackets[i] == '(') {
             count++;
-        } else if (c == ')') {
+        } else if (brackets[i] == ')') {
             count--;
             if (count < 0) {
                 return false;
@@ -13,4 +13,9 @@ bool correct_bracketing(const string& brackets) {
         }
     }
     return count == 0;
+}
+
+int main() {
+    assert(not (correct_bracketing("()()(()())()()"));
+    return 0;
 }
