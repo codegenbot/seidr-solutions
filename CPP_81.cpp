730b4a659c5f8f03c34f6a77719456b2def2ae8f
beep boop <githubbot@vadim.me>
2024-07-19 19:26:34 +0200

    Solution 54 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lz9mgys4 Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 218faeb5-47ac-456f-bb13-a42b1c8da1c5.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 218faeb5-47ac-456f-bb13-a42b1c8da1c5.cpp:50:44: error: ‘b’ was not declared in t his scope    50 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 218faeb5-47ac-456f-bb13-a42b1c8da1c5.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ 218faeb5-47ac-456f-bb13-a42b1c8da1c5.cpp:44:5: note: ‘int main()’ previously def ined here    44 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -3,8 +3,10 @@
 #include <string>
 #include <cassert>
 
-std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
-    std::vector<std::string> letter_grades;
+using string = std::string;
+
+std::vector<string> numerical_letter_grade(std::vector<float> grades) {
+    std::vector<string> letter_grades;
     for (float gpa : grades) {
         if (gpa == 4.0) {
             letter_grades.push_back("A+");
@@ -37,11 +39,11 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     return letter_grades;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(std::vector<string> a, std::vector<string> b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(numerical_letter_grade({0, 0.7}), std::vector<std::string>{"E", "D-"}));
+    assert(issame(numerical_letter_grade({0, 0.7}), std::vector<string>{"E", "D-"}));
     return 0;
 }
