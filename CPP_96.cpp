d5d59986ca0d71b244d26d357523ffe90a984a37
beep boop <githubbot@vadim.me>
2024-07-19 03:09:32 +0200

    Solution 11 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1hc4poj8 Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 60fd059a-9a19-48bc-b5e7-436f7892f3cc.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 60fd059a-9a19-48bc-b5e7-436f7892f3cc.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 60fd059a-9a19-48bc-b5e7-436f7892f3cc.cpp:17: 60fd059a-9a19-48bc-b5e7-436f7892f3cc.cpp: In function ‘int main()’: 60fd059a-9a19-48bc-b5e7-436f7892f3cc.cpp:27:20: error: ‘count_up_to’ was not dec lared in this scope    27 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 60fd059a-9a19-48bc-b5e7-436f7892f3cc.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,16 +1,1 @@
-bool issame(vector<int> a, vector<int> b) {    
-    vector<int> result;
-    for (int i = 2; i < n; ++i) {
-        bool isPrime = true;
-        for (int j = 2; j * j <= i; ++j) {
-            if (i % j == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {
-            result.push_back(i);
-        }
-    }
-    return result;
-}
+bool issame(vector<int> a, vector<int> b) {
