a5ca41394643df24abe36935af301904afdd4dea
beep boop <githubbot@vadim.me>
2024-07-19 05:30:43 +0200

    Solution 51 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/14cfrwja Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7f0fbb9-7807-48d2-81fc-169089035 7c4.py", line 19, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f7f0fbb9-7807-48d2-81fc-169089035 7c4.py", line 14, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,8 @@
 def sort_third(l: list) -> list:
-    return sorted(
-        [
-            sorted(sublist, key=lambda x: x, reverse=True)
-            for sublist in [
-                l[i : i + 3] for i in range(0, len(l), 3) if len(l[i : i + 3]) >= 3
-            ]
-        ],
-        key=lambda x: x[2]
-    )
+    l += [0] * (3 - len(l) % 3)
+    return [
+        sorted(sublist, key=lambda x: x[2], reverse=True)
+        for sublist in [
+            l[i : i + 3] for i in range(0, len(l), 3)
+        ]
+    ]
