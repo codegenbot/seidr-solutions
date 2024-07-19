02bad85436e83582392169c79a2fc524030a44ad
beep boop <githubbot@vadim.me>
2024-07-19 21:21:46 +0200

    Solution 11 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mq8vqgyj Prompt:       |                    ^~~ 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp:9: 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp: In function ‘int main()’: 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 70118e3d-2afc-463a-8c29-98b6879db2e6.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,15 +2,15 @@
 #include <algorithm>
 #include <cassert>
 
-vector<int> common(vector<int> l1, vector<int> l2) {
+vector<int> common(const vector<int>& l1, const vector<int>& l2) {
+    vector<int> result;
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
-    vector<int> result;
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
 }
 
-bool issame(const vector<int>& v1, const vector<int>& v2) {
-    return (v1 == v2);
+bool issame(const vector<int>& l1, const vector<int>& l2) {
+    return l1 == l2;
 }
