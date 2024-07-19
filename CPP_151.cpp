12bcf1ebf5f7e25d965f8d54fc00813873636d70
beep boop <githubbot@vadim.me>
2024-07-19 21:04:46 +0200

    Solution 72 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pqgva896 Prompt: 64f4b3ea-69be-4ae4-8ebd-2a1be7087e2b.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 64f4b3ea-69be-4ae4-8ebd-2a1be7087e2b.cpp:13: 64f4b3ea-69be-4ae4-8ebd-2a1be7087e2b.cpp: In function ‘int main()’: 64f4b3ea-69be-4ae4-8ebd-2a1be7087e2b.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 64f4b3ea-69be-4ae4-8ebd-2a1be7087e2b.cpp:15:13: error: ‘double_the_difference’ w as not declared in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 64f4b3ea-69be-4ae4-8ebd-2a1be7087e2b.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,12 +1,13 @@
 #include <vector>
 long long double_the_difference(vector<float> lst);
+
 int main() {
-    vector<float> lst = {1.5f, -2.0f, 3.25f, 4.0f};
+    vector<float> lst = {1.0f, 2.0f, 3.5f, -4.0f, 5.0f};
     int odd_sum = 0;
     for (float num : lst) {
         if (num > 0 && modf(num, &num) == 0) {
-            sum += pow(num, 2);
+            odd_sum += pow(num, 2);
         }
     }
-    assert (double_the_difference(lst) == odd_sum );
+    assert(double_the_difference(lst) == odd_sum);
 }
