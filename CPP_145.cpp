626c9e1a83f7a11d4cab2ce848d96770fce195a6
beep boop <githubbot@vadim.me>
2024-07-19 22:24:31 +0200

    Solution 50 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/iho33lfc Prompt: 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp:26:13: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp:26:20: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp:26:27: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp:25: 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp: In function ‘int main()’: 8b98c130-1165-4c9e-8007-a7378ab5a612.cpp:35:20: error: ‘order_by_points’ was not  declared in this scope    35 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -2,6 +2,12 @@
 
 bool issame(const vector<int> &a, const vector<int> &b);
 
+vector<int> order_by_points(vector<int> nums);
+
+bool issame(const vector<int> &a, const vector<int> &b) {
+    return a == b;
+}
+
 vector<int> order_by_points(vector<int> nums) {
     sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
