711efaca0c83e020a497ce32223e6eb8b2129d94
beep boop <githubbot@vadim.me>
2024-07-19 03:48:18 +0200

    Solution 13 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9xcpuj31 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:5: fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp: In function ‘int main()’: fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,26 +1,12 @@
 bool issame(vector<int> path1, vector<int> path2){
-        return path1 == path2;
-    }
-    
-    vector<int> minPath(vector<vector<int>> grid, int k){
-        // Implement the minPath function here
-        return {1, 2, 1};
-    }
+    return path1 == path2;
+}
 
-    int main() {
-        // Sample test cases
-        vector<vector<int>> grid = {{0, 1, 0},
-                                     {1, 1, 1},
-                                     {0, 0, 0}};
-      
-        vector<int> path1 = minPath(grid, 2);
-        vector<int> path2 = minPath(grid, 1);
-        
-        if (issame(path1, path2)) {
-            cout << "Paths are the same" << endl;
-        } else {
-            cout << "Paths are different" << endl;
-        }
-        
-        return 0;
-    }
+vector<int> minPath(vector<vector<int>> grid, int k){
+    // your implementation here
+}
+
+int main(){
+    // test your functions here
+    return 0;
+}
