520dcdc10a602a445a7a25f29a576aed4c11b5c3
beep boop <githubbot@vadim.me>
2024-07-19 08:48:51 +0200

    Solution 80 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9gsfhnr8 Prompt:       |                    ^~~~~ 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp:9: 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp: In function ‘int main()’: 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 0a099f45-0e09-4220-a125-22cec24a9ed1.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,9 +1,25 @@
 #include <vector>
 
+vector<float> derivative(vector<float> xs);
+
+bool issame(vector<float> a, vector<float> b){
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i){
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<float> derivative(vector<float> xs){
     vector<float> result;
-    for(int i=1; i<xs.size(); i++){
-        result.push_back(xs[i] - xs[i-1]);
+    for (int i = 1; i < xs.size(); ++i){
+        result.push_back(xs[i] * i);
     }
     return result;
+}
+
+int main(){
+    assert(issame(derivative({1}), {}));
+    
+    return 0;
 }
