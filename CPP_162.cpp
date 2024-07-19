1c0fed4a068f3bafe85e6fbfa8d5a6f7988acdd0
beep boop <githubbot@vadim.me>
2024-07-19 21:13:32 +0200

    Solution 68 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/tgj3zogd Prompt:       |     HMAC_Update 4e3a4eea-99d5-4b83-a62f-6e4d22a01c1e.cpp:17:5: error: ‘MD5_Final’ was not declar ed in this scope; did you mean ‘HMAC_Final’?    17 |     MD5_Final(md5, &ctx);       |     ^~~~~~~~~       |     HMAC_Final 4e3a4eea-99d5-4b83-a62f-6e4d22a01c1e.cpp:20:22: error: aggregate ‘std::stringstr eam ss’ has incomplete type and cannot be defined    20 |         stringstream ss;       |                      ^~ 4e3a4eea-99d5-4b83-a62f-6e4d22a01c1e.cpp:21:15: error: ‘hex’ was not declared in  this scope    21 |         ss << hex << setw(2) << setfill('0') << (int)md5[i];       |               ^~~ 4e3a4eea-99d5-4b83-a62f-6e4d22a01c1e.cpp:21:22: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    21 |         ss << hex << setw(2) << setfill('0') << (int)md5[i];       |                      ^~~~       |                      getw 4e3a4eea-99d5-4b83-a62f-6e4d22a01c1e.cpp:21:33: error: ‘setfill’ was not declare d in this scope    21 |         ss << hex << setw(2) << setfill('0') << (int)md5[i];       |                                 ^~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -7,19 +7,19 @@ using namespace std;
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    EVP_MD_CTX ctx;
+    MD5_CTX ctx;
     unsigned char md5[16];
     unsigned char *p = NULL;
     string result;
 
-    EVP_MD_CTX_init(&ctx);
+    MD5_Init(&ctx);
     p = reinterpret_cast<unsigned char*>(text.c_str());
-    EVP_MD_CTX_update(&ctx, p, text.size());
-    EVP_MD_CTX_final(&ctx, md5);
+    MD5_Update(&ctx, p, text.size());
+    EVP_MD_CTX_final(&ctx);
 
     for (int i = 0; i < 16; i++) {
-        ostringstream ss;
-        ss << hex << std::setw(2) << std::setfill('0') << (int)md5[i];
+        stringstream ss;
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(md5[i]);
         result += ss.str();
     }
 
