7cd0f9f436bd799cd5da8dd646b0ff1aad79e1e3
beep boop <githubbot@vadim.me>
2024-07-19 09:15:37 +0200

    Solution 31 of CPP/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/15izmf0e Prompt:     2 |     for (int i = 2; i <= a / 3; ++i) {       |     ^~~ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 2; i <= a / 3; ++i) {       |                     ^ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:2:33: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 2; i <= a / 3; ++i) {       |                                 ^~ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return false;       |     ^~~~~~ 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:15: 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp: In function ‘int main()’: 8e3458f9-b05a-43f0-993f-d037f8ca7b3f.cpp:17:13: error: ‘is_multiply_prime’ was n ot declared in this scope    17 |     assert (is_multiply_prime(11 * 13 * 7) == true);       |             ^~~~~~~~~~~~~~~~~

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -1,6 +1,6 @@
 bool is_prime(int num) {
     if (num < 2) return false;
-    for (int i = 2; i * i <= num; ++i) {
+    for (int i = 2; i <= sqrt(num); ++i) {
         if (num % i == 0) {
             return false;
         }
@@ -9,12 +9,24 @@ bool is_prime(int num) {
 }
 
 bool is_prime(int num) {
-  if (num < 2) return false;
-  for (int i = 2; i * i <= num; ++i) {
-    if (num % i == 0) {
-      return false;
+    if (num < 2) return false;
+    for (int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
-```
+
+if (a < 30) return false;
+for (int i = 2; i <= a / 3; ++i) {
+    if (a % i == 0) {
+        int b = a / i;
+        for (int j = i + 1; j <= b / 2; ++j) {
+            if (b % j == 0 && is_prime(i) && is_prime(j) && is_prime(b / j)) {
+                return true;
+            }
+        }
+    }
+}
+return false;
