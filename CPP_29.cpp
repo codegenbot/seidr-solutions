b153768a217c3599c8500f8589bdebc536e0c0f1
beep boop <githubbot@vadim.me>
2024-07-19 02:03:39 +0200

    Solution 15 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/olbrw399 Prompt:    92 | #  define assert(expr) \       | b9737cd7-e595-4bb8-a97b-803c1d26f8b9.cpp: In function ‘int main()’: b9737cd7-e595-4bb8-a97b-803c1d26f8b9.cpp:19:5: error: ‘assert’ was not declared in this scope    19 |     assert(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx") == vector<string>{"xxx", "xxxAAA", "xxx"});       |     ^~~~~~ b9737cd7-e595-4bb8-a97b-803c1d26f8b9.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | b9737cd7-e595-4bb8-a97b-803c1d26f8b9.cpp: At global scope: b9737cd7-e595-4bb8-a97b-803c1d26f8b9.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ b9737cd7-e595-4bb8-a97b-803c1d26f8b9.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -16,12 +16,6 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
 }
 
 int main() {
-    vector<string> filtered = filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx");
-    if(filtered == vector<string>{"xxx", "xxxAAA", "xxx"}) {
-        cout << "Test Passed" << endl;
-    } else {
-        cout << "Test Failed" << endl;
-    }
-
+    assert(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx") == vector<string>{"xxx", "xxxAAA", "xxx"});
     return 0;
 }
