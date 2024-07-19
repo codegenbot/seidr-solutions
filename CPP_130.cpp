3e9531843e30fc53cbca49c831eda8ca4c762413
beep boop <githubbot@vadim.me>
2024-07-19 04:06:08 +0200

    Solution 50 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sg453w14 Prompt:       |                           ^~~~~~ ffd95cac-47d1-4b85-83e2-a9168c7d7181.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ffd95cac-47d1-4b85-83e2-a9168c7d7181.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ffd95cac-47d1-4b85-83e2-a9168c7d7181.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ffd95cac-47d1-4b85-83e2-a9168c7d7181.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffd95cac-47d1-4b85-83e2-a9168c7d7181.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ ffd95cac-47d1-4b85-83e2-a9168c7d7181.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main(){       |     ^~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -5,7 +5,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-std::vector<int> tri(int n){
+std::vector <int> tri(int n){
     std::vector<int> tribonacci(n+1);
     tribonacci[0] = 3;
     if(n > 0) tribonacci[1] = 1;
@@ -23,5 +23,5 @@ std::vector<int> tri(int n){
 }
 
 int main(){
-    assert(std::issame(std::tri(1), {1, 3}));
+    assert(issame(tri(1), {1, 3}));
 }
