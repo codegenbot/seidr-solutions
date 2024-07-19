4f4340d876d47ca4937152fada4e7b5cb4f42471
beep boop <githubbot@vadim.me>
2024-07-20 01:43:57 +0200

    Solution 58 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/kywbzhfz Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from b09885b3-85fb-4ee8-9fd7-c920dc0f2c42.cpp:2: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ b09885b3-85fb-4ee8-9fd7-c920dc0f2c42.cpp:9:14: warning: ‘int MD5_Final(unsigned char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]     9 |     MD5_Final(ptr, &md5ctx);       |     ~~~~~~~~~^~~~~~~~~~~~~~ In file included from b09885b3-85fb-4ee8-9fd7-c920dc0f2c42.cpp:2: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/cczVevh9.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': b09885b3-85fb-4ee8-9fd7-c920dc0f2c42.cpp:(.text+0x49): undefined reference to `M D5_Init' /usr/bin/ld: b09885b3-85fb-4ee8-9fd7-c920dc0f2c42.cpp:(.text+0x82): undefined re ference to `MD5_Update' /usr/bin/ld: b09885b3-85fb-4ee8-9fd7-c920dc0f2c42.cpp:(.text+0x9b): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -2,15 +2,16 @@
 #include <openssl/md5.h>
 
 std::string string_to_md5(const std::string& input) {
-    unsigned char result[16];
+    unsigned char* ptr = (unsigned char*)malloc(16);
     MD5_CTX md5ctx;
     MD5_Init(&md5ctx);
     MD5_Update(&md5ctx, input.c_str(), input.size());
-    MD5_Final(result, &md5ctx);
+    MD5_Final(ptr, &md5ctx);
+    free(ptr);
 
     char output[33];
     for (int i = 0; i < 16; i++) {
-        sprintf(output + i*2, "%02x", result[i]);
+        sprintf(output + i*2, "%02x", ptr[i]);
     }
     return std::string(output);
 }
