22c6fa91077f6eac0b8379ddb538aaaeb1f28b35
beep boop <githubbot@vadim.me>
2024-07-19 23:51:48 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lp46jxd4 Prompt: e0b64a8b-efed-432c-a8af-b4e55b543bb7.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ e0b64a8b-efed-432c-a8af-b4e55b543bb7.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ e0b64a8b-efed-432c-a8af-b4e55b543bb7.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ e0b64a8b-efed-432c-a8af-b4e55b543bb7.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
 #include <algorithm>
@@ -68,4 +69,4 @@ boost::any compare_one(boost::any a, boost::any b) {
     else {
         return boost::any("None");
     }
-}
+}}
