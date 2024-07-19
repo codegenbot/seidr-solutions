2a8579e0aaeb2470712efc46d4ff2857f86dad41
beep boop <githubbot@vadim.me>
2024-07-19 18:29:09 +0200

    Solution 24 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/dilniizg Prompt: f22092d0-0674-418b-b208-b933dbf53c01.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,65 +1,22 @@
-#include <iostream>
+#include <boost/any.hpp>
 #include <string>
 #include <limits>
 
 using namespace std;
 
-int compare_one(int a, float b) {
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-
-int compare_one(float a, string b) {
-    stof(b);
-    if (a > stod(b))
-        return a;
-    else
-        return stod(b);
-}
-
-int compare_one(string a, string b) {
-    stof(a);
-    stof(b);
-    if (stod(a) > stod(b))
-        return stod(a);
-    else
-        return stod(b);
-}
-
-int compare_one(int a, int b) {
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-
-int compare_one(string a, float b) {
-    if (stod(a) > b)
-        return stod(a);
-    else
-        return b;
-}
-
-int compare_one(int a, string b) {
-    if (a > stod(b))
-        return a;
-    else
-        return stod(b);
-}
-
-int main() {
-    int num1, num2;
-    cout << "Enter first number: ";
-    cin >> num1;
-    cout << "Enter second number: ";
-    cin >> num2;
-
-    if (num1 > num2)
-        cout << "Greater number is: " << num1 << endl;
-    else
-        cout << "Greater number is: " << num2 << endl;
-
-    return 0;
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return max((float)a.convert_to<float>(), stof(b.convert_to<string>().c_str()));
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        return (stof(a.convert_to<string>().c_str()) > stof(b.convert_to<string>().c_str())) ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return max((int)a.convert_to<int>(), (int)b.convert_to<int>());
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        return (stof(a.convert_to<string>().c_str()) > (float)b.convert_to<float>()) ? a : boost::any((int)0);
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        return (int)a.convert_to<int>() > stof(b.convert_to<string>().c_str()) ? a : boost::any((int)0);
+    }
+    return boost::any((int)0);
 }
