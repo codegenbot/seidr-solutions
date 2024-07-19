3a6d501771b6962758afc1e2559abfc5c9988c8c
beep boop <githubbot@vadim.me>
2024-07-19 08:39:58 +0200

    Solution 80 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/yo8glcah Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ddf3e54a-e767-4f9c-bac3-a3a51d893 3bc.py", line 5     return result     ^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_5.py
+++ Python_5.py
@@ -1,6 +1,6 @@
-def modify_list(numbers, delimiter):
+def format_numbers(numbers, delimeter):
     result = []
     for num in numbers:
-        result.extend([num, delimiter])
+        result.extend([num, delimeter])
     result.pop()
     return result
