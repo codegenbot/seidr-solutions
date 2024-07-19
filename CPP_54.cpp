99d585d0991f67f139cbec8a626eb866dc97f146
beep boop <githubbot@vadim.me>
2024-07-19 04:21:01 +0200

    Solution 70 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/w9dthoxy Prompt: 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp:1:5: error: expected constructor, destr uctor, or type conversion before ‘(’ token     1 | sort(s0.begin(), s0.end());       |     ^ 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp:2:9: error: expected constructor, destr uctor, or type conversion before ‘(’ token     2 |     sort(s1.begin(), s1.end());       |         ^ 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp:4:5: error: expected unqualified-id bef ore ‘return’     4 |     return s0 == s1;       |     ^~~~~~ 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp:5:1: error: expected declaration before  ‘}’ token     5 | }       | ^ In file included from 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp:7: 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp: In function ‘int main()’: 1c0b55af-dac2-4bf7-8f04-f8cdf62715b0.cpp:9:13: error: ‘same_chars’ was not decla red in this scope     9 |     assert (same_chars("aabb", "aaccc") == false);       |             ^~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,15 +1,6 @@
-#include <iostream>
-#include <algorithm>
-
-bool same_chars(std::string s0, std::string s1) {
-    std::sort(s0.begin(), s0.end());
-    std::sort(s1.begin(), s1.end());
+bool same_chars(const string &s0, const string &s1) {
+    sort(s0.begin(), s0.end());
+    sort(s1.begin(), s1.end());
     
     return s0 == s1;
-}
-
-int main() {
-    assert(same_chars("aabb", "aaccc") == false);
-    
-    return 0;
 }
