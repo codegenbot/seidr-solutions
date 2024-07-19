d012a5122fef68bcb28b01d4841aa60d6fcedf37
beep boop <githubbot@vadim.me>
2024-07-19 15:04:49 +0200

    Solution 41 of CPP/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4wueoxoj Prompt: Program did not receive expected input

--- CPP_49.cpp
+++ CPP_49.cpp
@@ -17,8 +17,7 @@ long long int modp(long long int n, long long int p) {
 int main() {
     long long int n, p;
     std::cin >> n >> p;
-    
     std::cout << modp(n, p) << std::endl;
-
+    
     return 0;
 }
