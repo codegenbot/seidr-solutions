191c39be7a8b25f53ed9a676ecb0756ef2504916
beep boop <githubbot@vadim.me>
2024-07-19 04:50:35 +0200

    Solution 39 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/axv8otor Prompt:    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 110495c5-97e0-4b9d-ad54-d4f5bfbd9eeb.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 110495c5-97e0-4b9d-ad54-d4f5bfbd9eeb.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 110495c5-97e0-4b9d-ad54-d4f5bfbd9eeb.cpp:29: 110495c5-97e0-4b9d-ad54-d4f5bfbd9eeb.cpp: In function ‘int main()’: 110495c5-97e0-4b9d-ad54-d4f5bfbd9eeb.cpp:39:20: error: ‘select_words’ was not de clared in this scope    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 110495c5-97e0-4b9d-ad54-d4f5bfbd9eeb.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -5,43 +5,12 @@
 #include <algorithm>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
     return std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<string> select_words(string s, int n) {
-    vector<string> words;
-    string word = "";
-    int consonant_count = 0;
-
-    for (char c : s) {
-        if (c == ' ') {
-            if (consonant_count == n) {
-                words.push_back(word);
-            }
-            word = "";
-            consonant_count = 0;
-        } else if (isalpha(c)) {
-            char lowercase_c = tolower(c);
-            if (lowercase_c != 'a' && lowercase_c != 'e' && lowercase_c != 'i' && lowercase_c != 'o' && lowercase_c != 'u') {
-                consonant_count++;
-            }
-            word += c;
-        }
-    }
-
-    if (consonant_count == n) {
-        words.push_back(word);
-    }
-
-    return words;
-}
+vector<string> select_words(string s, int n);
 
 int main() {
     assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
-    
     return 0;
 }
