ea45d2f309865013523e8c067214de7e09fc6d10
beep boop <githubbot@vadim.me>
2024-07-19 17:22:15 +0200

    Solution 44 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/rr9jqcik Prompt: 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:30:19: error: ‘string’ has not been dec lared    30 | char next(char c, string s) {       |                   ^~~~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp: In function ‘char next(char, int)’: 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:31:15: error: request for member ‘find’  in ‘s’, which is of non-class type ‘int’    31 |     int i = s.find(c);       |               ^~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:32:13: error: invalid types ‘int[int]’ for array subscript    32 |     return s[i + 1];       |             ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,8 +1,9 @@
 #include <string>
+
 int bowlingScore(std::string s) {
     int score = 0;
-    int currentFrame = 1;
-    std::vector<int> frames(10);
+    int currentFrame = 0;
+    vector<int> frames(10);
     
     for (char c : s) {
         if (c == '/') {
@@ -10,31 +11,28 @@ int bowlingScore(std::string s) {
             continue;
         }
         
-        if (c >= '0' && c <= '9') {
-            int pinCount = c - '0';
-            
-            while (pinCount < 3 || s.find('/')) {
-                if (s.find('/', s.find('/') + 1)) {
-                    pinCount += (s.find('/', s.find('/') + 1) - s.find('/') - 1) - '0' + 1;
-                } else {
-                    pinCount++;
+        if (c >= '1' && c <= '9') {
+            int strike = false;
+            while (c != '/' && c >= '0' && c <= '9') {
+                frames[currentFrame] *= 10 + (c - '0');
+                s = s.substr(s.find(c) + 1);
+                char d = s[0];
+                if (d == '/') {
+                    strike = true;
+                    break;
+                } else if (d >= '1' && d <= '9') {
+                    frames[currentFrame] += (d - '0');
+                    frames[currentFrame] *= 2;
+                    s = s.substr(s.find(d) + 1);
+                    continue;
                 }
-                
-                c = next(c, s);
             }
             
-            if (currentFrame == 10 && pinCount < 2) {
-                score += 10 + frames[9] * 10;
-                return score;
-            }
-            
-            if (pinCount == 1 || (pinCount == 2 && s.find('/') - s.find(c) > 0)) {
-                score += 10 + (frames[currentFrame - 1] < 10 ? 10 : 10 + frames[9]);
+            if (!strike) {
+                score += frames[currentFrame];
             } else {
-                score += pinCount * 10;
+                score += 10;
             }
-            
-            currentFrame++;
         }
     }
     
