2718f5fa5cdedeaa2ae75b97995b1e57c3ac078f
beep boop <githubbot@vadim.me>
2024-07-20 01:32:28 +0200

    Solution 1 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fhn69njt Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,35 +1,43 @@
 #include <algorithm>
+#include <string>
+
 using namespace std;
 
 string sort_numbers(string numbers) {
-    map<string, int> numberMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                   {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                   {"eight", 8}, {"nine", 9}};
-
-    vector<string> numVector;
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
+    vector<string> numVec;
     string temp;
 
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
+    for (char &c : numbers) {
+        if (c == ' ') {
             continue;
-        } else {
-            temp += numbers[i];
         }
-
-        if (i == numbers.length() - 1 || numbers[i + 1] != ' ') {
-            numVector.push_back(temp);
-            temp = "";
+        temp += c;
+        bool found = false;
+        for (auto it = numMap.begin(); it != numMap.end(); ++it) {
+            if (it->first.find(temp) != string::npos) {
+                numVec.push_back(it->first);
+                found = true;
+                break;
+            }
         }
+        temp.clear();
     }
 
-    sort(numVector.begin(), numVector.end(), [&numberMap](const string &a, const string &b) {
-        return numberMap.at(a) < numberMap.at(b);
-    });
+    sort(numVec.begin(), numVec.end());
 
-    string result;
-    for (const auto &num : numVector) {
-        result += num + " ";
-    }
+    return string(join(numVec, " "));
+}
 
-    return result.substr(0, result.length() - 1);
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <iterator>
+
+int main() {
+    cout << sort_numbers("three one five") << endl;
+    return 0;
 }
