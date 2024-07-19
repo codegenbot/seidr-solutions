c3e2722fe3dee61f1f2746310c15ed2d8ef1a70f
beep boop <githubbot@vadim.me>
2024-07-19 21:08:24 +0200

    Solution 16 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mxcbr0ed Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ faa6ae25-d701-4f29-a8b2-15ad55065192.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ faa6ae25-d701-4f29-a8b2-15ad55065192.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from faa6ae25-d701-4f29-a8b2-15ad55065192.cpp:7: faa6ae25-d701-4f29-a8b2-15ad55065192.cpp: In function ‘int main()’: faa6ae25-d701-4f29-a8b2-15ad55065192.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ faa6ae25-d701-4f29-a8b2-15ad55065192.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -2,20 +2,19 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
+std::vector<int> incr_list(const std::vector<int>& l) {
+    std::vector<int> res = l;
+    for(auto& num : res){
+        num++;
+    }
+    return res;
+}
 
-bool issame(vector<int> a, vector<int> b){
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-vector<int> incr_list(vector<int> l){
-    for(int i = 0; i < l.size(); i++){
-        l[i]++;
-    }
-    return l;
-}
-
-int main(){
+int main() {
     assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
     
     return 0;
