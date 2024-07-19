b8425c74a36656f4e4b945d0418f43ddf836c541
beep boop <githubbot@vadim.me>
2024-07-19 16:58:40 +0200

    Solution 99 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vhbq78dq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ad4d56c-c4b1-4fce-b440-28774e460 cf1.py", line 28, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ad4d56c-c4b1-4fce-b440-28774e460 cf1.py", line 25, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,16 +1,15 @@
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, int(num**0.5) + 1):
+    for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
-
 
 def prime_fib(n: int):
     fib_list = [1, 1]
     while len(fib_list) < n:
         fib_list.append(fib_list[-1] + fib_list[-2])
-
+    
     prime_fib_list = [x for x in fib_list if is_prime(x)]
-    return prime_fib_list[n - 1]
+    return prime_fib_list[n-1]
