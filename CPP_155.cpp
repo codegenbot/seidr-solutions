87777795032ce3ec212cfd6e85e2c9be528ac29d
beep boop <githubbot@vadim.me>
2024-07-20 01:51:55 +0200

    Solution 64 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0ziv0fkb Prompt:       |                    ^~~ db45d980-4349-4a95-bdeb-f6125dc298c1.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ db45d980-4349-4a95-bdeb-f6125dc298c1.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ db45d980-4349-4a95-bdeb-f6125dc298c1.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from db45d980-4349-4a95-bdeb-f6125dc298c1.cpp:16: db45d980-4349-4a95-bdeb-f6125dc298c1.cpp: In function ‘int main()’: db45d980-4349-4a95-bdeb-f6125dc298c1.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ db45d980-4349-4a95-bdeb-f6125dc298c1.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,8 +1,7 @@
 #include <vector>
-#include <cassert>
 
-std::vector<int> even_odd_count(int num) {
-    std::vector<int> counts(2, 0);
+vector<int> even_odd_count(int num) {
+    vector<int> counts(2, 0);
     
     while (num != 0) {
         int digit = abs(num % 10);
@@ -17,11 +16,12 @@ std::vector<int> even_odd_count(int num) {
     return counts;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
 int main() {
-    assert (issame(even_odd_count(0) , {1, 0}));
+    assert(issame(even_odd_count(0), {1, 0}));
+    
     return 0;
 }
