8727123464568c31560eefa8903bd7de0a84be7f
beep boop <githubbot@vadim.me>
2024-07-19 18:11:29 +0200

    Solution 29 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4j6mhwyx Prompt:       |                                       ^ fc9ebb1d-93de-417c-b124-d2431b5d2773.cpp:37:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    37 | int main(){       |         ^~ fc9ebb1d-93de-417c-b124-d2431b5d2773.cpp:37:9: note: remove parentheses to defau lt-initialize a variable    37 | int main(){       |         ^~       |         -- fc9ebb1d-93de-417c-b124-d2431b5d2773.cpp:37:9: note: or replace parentheses with  braces to value-initialize a variable fc9ebb1d-93de-417c-b124-d2431b5d2773.cpp:37:11: error: a function-definition is not allowed here before ‘{’ token    37 | int main(){       |           ^ fc9ebb1d-93de-417c-b124-d2431b5d2773.cpp:39:2: error: expected ‘}’ at end of inp ut    39 | }       |  ^ fc9ebb1d-93de-417c-b124-d2431b5d2773.cpp:24:12: note: to match this ‘{’    24 | int main() {       |            ^

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,4 +1,8 @@
 #include <cassert>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a.size() == b.size() && a == b;
@@ -19,5 +23,6 @@ std::vector<int> remove_duplicates(std::vector<int> numbers) {
 }
 
 int main() {
-    assert(std::vector<int>({1, 2, 3, 2, 4, 3, 5}) == remove_duplicates({1, 2, 3, 2, 4, 3, 5}));
+    assert(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) == std::vector<int>({1, 2, 3, 4, 5}));
     return 0;
+}
