8beaf811b31e761e8d1e90ec18322d6becd9a2fc
beep boop <githubbot@vadim.me>
2024-07-19 06:52:32 +0200

    Solution 59 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/y3rlj0xf Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,27 +1,31 @@
-vector<string> words;
-    string word = "";
-    for (char c : txt) {
-        if (c == ' ') {
-            words.push_back(word);
-            word = "";
-        } else if (c == ',') {
-            words.push_back(word);
-            word = "";
-        } else {
-            word += c;
+vector<string> split_words(string txt){
+    vector<string> result;
+    if(txt.find(' ') != string::npos){
+        size_t pos = 0;
+        string token;
+        while ((pos = txt.find(' ')) != string::npos) {
+            token = txt.substr(0, pos);
+            result.push_back(token);
+            txt.erase(0, pos + 1);
         }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-    if (words.empty()) {
+        result.push_back(txt);
+    } else if(txt.find(',') != string::npos){
+        size_t pos = 0;
+        string token;
+        while ((pos = txt.find(',')) != string::npos) {
+            token = txt.substr(0, pos);
+            result.push_back(token);
+            txt.erase(0, pos + 1);
+        }
+        result.push_back(txt);
+    } else {
         int oddCount = 0;
-        for (char c : txt) {
-            if (islower(c) && ((c - 'a') % 2 == 1)) {
+        for(char c : txt){
+            if(islower(c) && (c - 'a') % 2 == 1){
                 oddCount++;
             }
         }
-        words.push_back(to_string(oddCount));
+        result.push_back(to_string(oddCount));
     }
-    return words;
+    return result;
 }
