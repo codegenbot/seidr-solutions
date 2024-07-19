38758975c789461a9200f895067f19746356be1d
beep boop <githubbot@vadim.me>
2024-07-19 18:09:13 +0200

    Solution 0 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/h9acqzp2 Prompt: Complete the following code given the task description and function signature.

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,28 +1,13 @@
-#include <vector>
-#include <cassert>
-using namespace std;
-
-bool vectorEqual(vector<int> a,vector<int>b){
-    return a == b;
-}
-
 vector<int> make_a_pile(int n) {
     vector<int> pile;
-    int stones = n;
-    while (stones > 0) {
-        pile.push_back(stones);
-        if (n % 2 == 1) {
-            n++;
-            stones = n;
+    int stones = 1;
+    for (int i = 0; i < n; ++i) {
+        if ((stones % 2) == 1) {
+            pile.push_back(stones);
         } else {
-            n--;
-            stones = n;
+            pile.push_back(stones + 1);
         }
+        stones += 2;
     }
     return pile;
-}
-
-int test_make_a_pile() {
-    assert(vectorEqual(make_a_pile(8), vector<int>({8, 10, 12, 14, 16, 18, 20, 22})) == true);
-    return 0;
 }
