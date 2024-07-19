98db1d9f6cdb9712cad05ff27d28d5ed061f79c1
beep boop <githubbot@vadim.me>
2024-07-19 21:43:30 +0200

    Solution 13 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4eo1nlj9 Prompt: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = __gnu_cxx::__normal_iterator<int*, st d::vector<int> >; _Tp = int; _Alloc = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:657:23:   required from ‘std::vector<_Tp, _Alloc>::vector(_InputIterator, _InputIterator, const allocator_type&) [with _In putIterator = __gnu_cxx::__normal_iterator<int*, std::vector<int> >; <template-p arameter-2-2> = void; _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int>]’ 8812279b-217e-4c0a-a1b8-1a17ba5eb051.cpp:9:129:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -5,15 +5,20 @@
 using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(std::remove_if(l.begin(), l.end(), [&l](int i){ return std::count(l.begin(), l.end(), i) > 1; }), l.end());
-    return result;
+    unordered_set<int> s(l.begin(), l.end());
+    return vector<int>(s.begin(), s.end());
 }
 
-int main() {
+int driver() {
     vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     vector<int> output = unique(input);
     for (int i : output) {
         cout << i << " ";
     }
+    return 0;
+}
+
+int main() {
+    driver();
     return 0;
 }
