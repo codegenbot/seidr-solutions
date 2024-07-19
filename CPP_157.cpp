aaf49f92f98747dbc7c7d697ee50d94e3dc15b55
beep boop <githubbot@vadim.me>
2024-07-19 07:17:42 +0200

    Solution 67 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/r8n81f4q Prompt: Complete the following code given the task description and function signature.

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,3 +1,4 @@
 return (pow(a, 2) + pow(b, 2) == pow(c, 2) ||
-        pow(b, 2) + pow(c, 2) == pow(a, 2) ||
-        pow(c, 2) + pow(a, 2) == pow(b, 2));
+            pow(a, 2) + pow(c, 2) == pow(b, 2) ||
+            pow(b, 2) + pow(c, 2) == pow(a, 2));
+}
