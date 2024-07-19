fde547e1e1bf9d42c501c9c77e82a299a96b6c6e
beep boop <githubbot@vadim.me>
2024-07-19 20:20:10 +0200

    Solution 16 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/u0hz0tk3 Prompt:       |                    ^~~ a334e829-0f23-4031-9365-046c9f774ba4.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a334e829-0f23-4031-9365-046c9f774ba4.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a334e829-0f23-4031-9365-046c9f774ba4.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a334e829-0f23-4031-9365-046c9f774ba4.cpp:14: a334e829-0f23-4031-9365-046c9f774ba4.cpp: In function ‘int main()’: a334e829-0f23-4031-9365-046c9f774ba4.cpp:24:20: error: ‘maximum’ was not declare d in this scope    24 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ a334e829-0f23-4031-9365-046c9f774ba4.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,14 +1,20 @@
-vector<int> maximum(vector<int> arr,int k){
-    vector<int> res;
-    for(int i = 0; i < k; i++){
-        int max_idx = -1;
-        for(int j = 0; j < arr.size(); j++){
-            if(arr[j] > arr[max_idx])
-                max_idx = j;
-        }
-        res.push_back(arr[max_idx]);
-        arr.erase(arr.begin() + max_idx);
+#include <vector>
+#include <algorithm>
+
+bool issame(vector<int> a,vector<int>b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
-    sort(res.begin(), res.end());
-    return res;
+    return true;
+}
+
+vector<int> maximum(vector<int> arr, int k) {
+    vector<int> result;
+    for (int i = 0; i < k; i++) {
+        auto it = std::max_element(arr.begin(), arr.end());
+        result.push_back(*it);
+        arr.erase(it);
+    }
+    return result;
 }
