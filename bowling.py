590999e1d3a08093b8b3dd8e77248cf2c50bb884
beep boop <githubbot@vadim.me>
2024-07-19 15:15:33 +0200

    Solution 0 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/g2v0za89 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,40 +1,14 @@
-def bowling_score(bowling):
+```
+def bowling_score(frames):
     score = 0
     frame = 1
-    for i in range(len(bowling)):
-        if bowling[i] == "X":
-            score += 30
+    for char in frames:
+        if char.isdigit():
+            score += int(char)
+            if frame == 10:
+                break
             frame += 1
-        elif bowling[i].isdigit():
-            if int(bowling[i]) == 10:
-                score += 20
-                frame += 1
-            else:
-                strike = False
-                for j in range(i + 1, len(bowling)):
-                    if bowling[j] == "X":
-                        strike = True
-                        break
-                    elif bowling[j].isdigit():
-                        if int(bowling[j]) + int(bowling[i]) <= 10:
-                            score += int(bowling[i]) + int(bowling[j])
-                            frame += 1
-                            i = j - 1
-                            break
-                if strike:
-                    score += 20
-                    frame += 1
-        else:
-            for j in range(i + 1, len(bowling)):
-                if bowling[j] == "X":
-                    score += 10 + int(bowling[i])
-                    frame += 1
-                    i = j - 1
-                    break
-                elif bowling[j].isdigit():
-                    if int(bowling[j]) + int(bowling[i]) <= 10:
-                        score += int(bowling[i]) + int(bowling[j])
-                        frame += 1
-                        i = j - 1
-                        break
+        elif char == '/':
+            score += 10 - (int(frames[frames.index(char) - 1]) + int(frames[frames.index(char) + 1]))
+            frame += 1
     return score
