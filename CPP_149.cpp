7bbe6e9922bd92ef80ce8cf5dc1058c5b5b03aaa
beep boop <githubbot@vadim.me>
2024-07-19 20:20:33 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ptyt07ic Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c4891f2b-ef9f-4084-81e8-1294f4461375.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c4891f2b-ef9f-4084-81e8-1294f4461375.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c4891f2b-ef9f-4084-81e8-1294f4461375.cpp:11: c4891f2b-ef9f-4084-81e8-1294f4461375.cpp: In function ‘int main()’: c4891f2b-ef9f-4084-81e8-1294f4461375.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ c4891f2b-ef9f-4084-81e8-1294f4461375.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,20 @@
-lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
-sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
-    if (a.length() == b.length()) {
-        return a < b;
-    }
-    return a.length() < b.length();
-});
-return issame(lst, a);
+bool issame(const vector<string>& a, const vector<string>& b) {
+    return a == b; 
+}
+
+vector<string> sorted_list_sum(const vector<string>& lst) {
+    vector<string> res = lst;
+    res.erase(remove_if(res.begin(), res.end(), [](const string& s) { return s.length() % 2 != 0; }), res.end());
+    sort(res.begin(), res.end(), [](const string& a, const string& b) {
+        if (a.length() == b.length()) {
+            return a < b;
+        }
+        return a.length() < b.length();
+    });
+    return res;
+}
+
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
+}
