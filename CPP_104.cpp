ae404d2ed476971d2122d448c1844de5fa63bfff
beep boop <githubbot@vadim.me>
2024-07-20 00:54:09 +0200

    Solution 76 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/whi9clwf Prompt:       |                    ^~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:21: ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp: In function ‘int main()’: ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ ab739751-adc0-4322-aae9-6a0a90fb09ae.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,41 +1,13 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
 bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    for (size_t i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
-}
-
-vector<int> unique_digits(vector<int> x){
-    vector<int> result;
-    for (int num : x) {
-        bool hasEvenDigit = false;
-        int n = num;
-        while (n > 0) {
-            if ((n % 10) % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            n /= 10;
-        }
-        if (!hasEvenDigit) {
-            result.push_back(num);
-        }
-    }
-    sort(result.begin(), result.end());
-    return result;
-}
-
-int main() {
-    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
 }
