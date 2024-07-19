edc814a94f9323dcc1b1b57696dd1ad1e89e1bcf
beep boop <githubbot@vadim.me>
2024-07-19 18:51:00 +0200

    Solution 8 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xf1ve6zq Prompt: f5f81861-d6dc-473e-9531-14325e7c494a.cpp: At global scope: f5f81861-d6dc-473e-9531-14325e7c494a.cpp:9:35: error: class template placeholder  ‘std::iterator’ not permitted in this context     9 | vector<int>::iterator unique_copy(iterator first, iterator last) {       |                                   ^~~~~~~~ f5f81861-d6dc-473e-9531-14325e7c494a.cpp:9:51: error: class template placeholder  ‘std::iterator’ not permitted in this context     9 | vector<int>::iterator unique_copy(iterator first, iterator last) {       |                                                   ^~~~~~~~ f5f81861-d6dc-473e-9531-14325e7c494a.cpp: In function ‘std::vector<int>::iterato r unique_copy(...)’: f5f81861-d6dc-473e-9531-14325e7c494a.cpp:10:10: error: ‘first’ was not declared in this scope    10 |     sort(first, last);       |          ^~~~~ f5f81861-d6dc-473e-9531-14325e7c494a.cpp:10:17: error: ‘last’ was not declared i n this scope    10 |     sort(first, last);       |                 ^~~~ f5f81861-d6dc-473e-9531-14325e7c494a.cpp:12:5: error: ‘erase’ was not declared i n this scope    12 |     erase(it, last);       |     ^~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,4 +1,14 @@
-auto unique_copy(vector<int>::iterator first, vector<int>::iterator last) {
-    std::vector<int> result(std::unique_copy(first, last));
+#include <algorithm>
+using namespace std;
+
+vector<int> unique(vector<int> l){
+    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()).end());
     return result;
+}
+
+vector<int>::iterator unique_copy(vector<int>::iterator first, vector<int>::iterator last) {
+    sort(first, last);
+    auto it = unique_copy(first, last); 
+    //it = unique_copy(first,last); // not necessary, remove this line
+    //return ++it; // not possible to erase an iterator, so this line is also unnecessary
 }
