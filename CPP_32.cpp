6917bddf543fe1e47a508fcd4029836f30d6c693
beep boop <githubbot@vadim.me>
2024-07-19 21:34:19 +0200

    Solution 3 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d91o4gsr Prompt: 9ba9c703-1e36-4954-823f-b861be1566d6.cpp:22:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    22 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 9ba9c703-1e36-4954-823f-b861be1566d6.cpp:25:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    25 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 9ba9c703-1e36-4954-823f-b861be1566d6.cpp:25:36: error: ‘find_zero’ cannot be use d as a function    25 |         solution = find_zero(coeffs);       |                                    ^ In file included from 9ba9c703-1e36-4954-823f-b861be1566d6.cpp:9: 9ba9c703-1e36-4954-823f-b861be1566d6.cpp:26:21: error: ‘poly’ was not declared i n this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 9ba9c703-1e36-4954-823f-b861be1566d6.cpp:26:17: error: ‘abs’ was not declared in  this scope    26 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,24 +1,10 @@
 #include <vector>
 #include <cmath>
 
-double find_zero(vector<double> coeffs) {
+double find_zero(vector<double> coeffs, vector<double> poly, double solution) {
     double sum = 0;
-    for (int i = 1; i < coeffs.size(); i += 2) {
-        sum -= coeffs[i] / coeffs[0];
+    for (int i = 2; i < poly.size(); i += 2) {
+        sum -= coeffs[i/2] * pow(solution, i) / (pow(solution, i+1));
     }
-    return -sum / coeffs[0];
-}
-
-int main() {
-    vector<double> coeffs;
-    // Read input from user
-    for(int i=0;i<5;++i){
-        double x;
-        std::cin >> x;
-        coeffs.push_back(x);
-    }
-
-    double solution = find_zero(coeffs);
-
-    return 0;
+    return -sum / pow(solution, 2);
 }
