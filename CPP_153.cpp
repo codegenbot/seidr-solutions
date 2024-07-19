fca420605f8f7b28634c394ebf599a67606f68ab
beep boop <githubbot@vadim.me>
2024-07-20 01:01:18 +0200

    Solution 30 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7gdaekmr Prompt:   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~ In file included from /usr/include/c++/11/cassert:44,                  from a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp:4: a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp:31:32: error: ‘Strongest_Extension’ can not be used as a function    31 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~ a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp: At global scope: a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main(){       |     ^~~~ In file included from a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp:36: a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp: In function ‘int main()’: a342a121-aa00-4b50-aa8a-ca6bf8f17542.cpp:38:32: error: ‘Strongest_Extension’ can not be used as a function    38 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -4,9 +4,9 @@
 #include <cassert>
 #include <string>
 
-int Strongest_Extension(string class_name, vector<string> extensions){
+std::string Strongest_Extension(const std::string& class_name, const std::vector<std::string>& extensions){
     int max_strength = 0;
-    string strongest_extension;
+    std::string strongest_extension;
 
     for(auto extension : extensions){
         int cap = 0, sm = 0;
@@ -25,8 +25,8 @@ int Strongest_Extension(string class_name, vector<string> extensions){
 }
 
 int main(){
-    string class_name = "My";
-    vector<string> extensions = {"Java", "Python", "C++"};
+    std::string class_name = "My";
+    std::vector<std::string> extensions = {"Java", "Python", "C++"};
     cout << Strongest_Extension(class_name, extensions) << endl;
     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
     return 0;
