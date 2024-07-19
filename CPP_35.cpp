a3604797625202c519b3ee04a9a5d2a9c2fefd65
beep boop <githubbot@vadim.me>
2024-07-19 19:11:29 +0200

    Solution 63 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gahd0o9k Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 9ba13d65-909f-4e20-88cd-c4f3d99d1d4d.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,8 +1,7 @@
 ```cpp
 #include <vector>
 #include <algorithm>
-#include <initializer_list>
 
-int max_value(std::vector<int> l) {
+int std::max_element(std::vector<int> l) {
     return *std::max_element(l.begin(), l.end());
 }
