6f88ed6078831176e5844c8e4f4f672f0cd328ad
beep boop <githubbot@vadim.me>
2024-07-19 06:14:26 +0200

    Solution 75 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/twbnhxvv Prompt: 64c5a059-5f50-4bc0-8c1e-4b6073a6d471.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 64c5a059-5f50-4bc0-8c1e-4b6073a6d471.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -30,8 +30,4 @@ std::string string_to_md5(const std::string& text) {
     return std::string(mdString);
 }
 
-int main() {
-    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-
-    return 0;
-}
+```
