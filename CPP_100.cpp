639d31e7a0f76b05224131eb9f4ccfe4bc6152a3
beep boop <githubbot@vadim.me>
2024-07-20 00:45:08 +0200

    Solution 31 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/t375mxig Prompt:       |                    ^~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:14: 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp: In function ‘int main()’: 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:24:20: error: ‘make_a_pile’ was not dec lared in this scope    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 8901f600-64ad-4a40-b4c8-49d948eff40d.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,28 +1,27 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-
     return true;
 }
 
-vector<int> make_a_pile(int n) {
-    vector<int> stones;
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> stones;
     stones.push_back(n);
-    for (int i = 1; i < n; ++i) {
-        if (stones[i - 1] % 2 == 0) {
-            stones.push_back(stones[i - 1] + 1);
+    for (int i = 2; i <= n; ++i) {
+        if (n % 2 == 0) {
+            n += 1;
         } else {
-            stones.push_back(stones[i - 1] + 2);
+            n += 2;
         }
+        stones.push_back(n);
     }
     return stones;
 }
