2af04516747bc7c43eeb7e8c10ba30fbf3c8a9c7
beep boop <githubbot@vadim.me>
2024-07-19 20:42:29 +0200

    Solution 14 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oojp8dsm Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,11 +1,21 @@
-#include <string>
-using namespace std;
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
 
-bool simplify(string x, string n) {
-    long long int a = atoll(strtok(x.c_str(), "/")) * atoll(strtok(NULL, "/")) +
-            atoll(strtok(n.c_str(), "/")) * atoll(strtok(NULL, "/"));
-    long long int b = atoll(strtok(x.c_str(), "/")) * atoll(strtok(NULL, "/")) /
-                      atoll(strtok(n.c_str(), "/")) * atoll(strtok(NULL, "/"));
+bool simplify(string x,string n){
+    int numerator1 = stoi(x.substr(0,x.find('/')));
+    int denominator1 = stoi(x.substr(x.find('/')+1));
+    int numerator2 = stoi(n.substr(0,n.find('/')));
+    int denominator2 = stoi(n.substr(n.find('/')+1));
 
-    return b == 1;
+    int newNumerator = numerator1 * denominator2;
+    int newDenominator = denominator1 * numerator2;
+
+    if(gcd(newNumerator, newDenominator) != 1)
+        return false;
+
+    return true;
 }
