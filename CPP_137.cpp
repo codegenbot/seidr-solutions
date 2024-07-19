d502bc7d2f74e525d8bf142bfd7f618412e59a63
beep boop <githubbot@vadim.me>
2024-07-19 11:17:13 +0200

    Solution 30 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/o50evrff Prompt:    17 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ 2a7a4214-141c-4489-892e-d9c5a6959650.cpp:25:3: error: expected unqualified-id be fore ‘else’    25 | } else {       |   ^~~~ In file included from 2a7a4214-141c-4489-892e-d9c5a6959650.cpp:29: 2a7a4214-141c-4489-892e-d9c5a6959650.cpp: In function ‘int main()’: 2a7a4214-141c-4489-892e-d9c5a6959650.cpp:31:13: error: ‘boost’ has not been decl ared    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 2a7a4214-141c-4489-892e-d9c5a6959650.cpp:31:29: error: ‘string’ was not declared  in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 2a7a4214-141c-4489-892e-d9c5a6959650.cpp:31:37: error: ‘compare_one’ was not dec lared in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <boost/any.hpp>
 #include <cassert>
-
+  
 boost::any compare_one(const boost::any& a, const boost::any& b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
