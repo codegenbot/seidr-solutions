cd48f39270b6081e4825b8c1f6d4428e1d7810ce
beep boop <githubbot@vadim.me>
2024-07-19 04:06:39 +0200

    Solution 27 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pkm4vxj8 Prompt:    17 | } else if(a.type() == typeid(string) && b.type() == typeid(string)){       |   ^~~~ 6e3334ee-81ec-495a-a1b9-ffaeb80402e7.cpp:33:3: error: expected unqualified-id be fore ‘else’    33 | } else {       |   ^~~~ In file included from 6e3334ee-81ec-495a-a1b9-ffaeb80402e7.cpp:37: 6e3334ee-81ec-495a-a1b9-ffaeb80402e7.cpp: In function ‘int main()’: 6e3334ee-81ec-495a-a1b9-ffaeb80402e7.cpp:39:13: error: ‘boost’ has not been decl ared    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 6e3334ee-81ec-495a-a1b9-ffaeb80402e7.cpp:39:29: error: ‘string’ was not declared  in this scope    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 6e3334ee-81ec-495a-a1b9-ffaeb80402e7.cpp:39:37: error: ‘compare_one’ was not dec lared in this scope    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,10 +2,10 @@
 #include <algorithm>
 #include <string>
 #include <cassert>
+#include <boost/lexical_cast.hpp>
 
-void replace(std::string::iterator begin, std::string::iterator end, char target, char replacement);
-
-boost::any compare_one(const boost::any &a, const boost::any &b);
+using namespace std;
+using namespace boost;
 
 void replace(std::string::iterator begin, std::string::iterator end, char target, char replacement) {
     std::replace(begin, end, target, replacement);
@@ -31,25 +31,16 @@ boost::any compare_one(const boost::any &a, const boost::any &b) {
     } else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
         std::string strA = boost::any_cast<std::string>(a);
         std::string strB = boost::any_cast<std::string>(b);
-        if(strA.find_first_of(".,") != std::string::npos) {
+        if(strA.find_first_of(".,") != string::npos) {
             replace(strA.begin(), strA.end(), ',', '.');
         }
-        if(strB.find_first_of(".,") != std::string::npos) {
+        if(strB.find_first_of(".,") != string::npos) {
             replace(strB.begin(), strB.end(), ',', '.');
         }
-        if(std::stof(strA) > std::stof(strB)){
-            return a;
-        } else if(std::stof(strA) < std::stof(strB)){
-            return b;
-        } else {
-            return "None";
-        }
+        return stof(strA) > stof(strB) ? a : (stof(strA) < stof(strB) ? b : "None");
     } else {
         return "None";
     }
 }
-
-using namespace std;
-using namespace boost;
 
 assert(boost::any_cast<std::string>(compare_one(std::string("1"), 1)) == "None");
