c197c1515df54ee5f788cac07614966a1e76a1b4
beep boop <githubbot@vadim.me>
2024-07-19 23:32:34 +0200

    Solution 3 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bw9nrda5 Prompt: d8b24b6a-2c3b-4c36-b026-0ccdb5f82d7e.cpp:3:22: error: ‘vector’ is not a member o f ‘std’     3 | int sum_squares(std::vector<float> lst){       |                      ^~~~~~ d8b24b6a-2c3b-4c36-b026-0ccdb5f82d7e.cpp:2:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?     1 | #include <cmath>   +++ |+#include <vector>     2 | d8b24b6a-2c3b-4c36-b026-0ccdb5f82d7e.cpp:3:29: error: expected primary-expressio n before ‘float’     3 | int sum_squares(std::vector<float> lst){       |                             ^~~~~ In file included from d8b24b6a-2c3b-4c36-b026-0ccdb5f82d7e.cpp:12: d8b24b6a-2c3b-4c36-b026-0ccdb5f82d7e.cpp: In function ‘int main()’: d8b24b6a-2c3b-4c36-b026-0ccdb5f82d7e.cpp:14:24: error: ‘sum_squares’ cannot be u sed as a function    14 |     assert (sum_squares({-1,1,0})==2);       |             ~~~~~~~~~~~^~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,12 +1,10 @@
-```cpp
 #include <vector>
 #include <cmath>
 
-int sum_squares(std::vector<float> lst){
+int sum_squares(std::vector<double> lst){
     int total = 0;
-    for(float num : lst){
-        int ceil_num = ceil(num);
-        total += pow(ceil_num,2);
+    for(double num : lst){
+        int ceil_num = std::ceil(num);
+        total += std::pow(ceil_num,2);
     }
     return total;
-}
