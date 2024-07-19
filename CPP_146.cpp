1ee65a379e7bb9109cc39f9165f81f3444edb322
beep boop <githubbot@vadim.me>
2024-07-20 00:38:14 +0200

    Solution 42 of CPP/146, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qy61hc4t Prompt: e94c81de-ba19-40a0-b40f-22c6064e6c93.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ e94c81de-ba19-40a0-b40f-22c6064e6c93.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ e94c81de-ba19-40a0-b40f-22c6064e6c93.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ e94c81de-ba19-40a0-b40f-22c6064e6c93.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ e94c81de-ba19-40a0-b40f-22c6064e6c93.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ e94c81de-ba19-40a0-b40f-22c6064e6c93.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,9 +1,14 @@
-int specialFilter(vector<int> nums) {
-    int count = 0;
+#include <initializer_list>
+#include <cassert>
+#include <vector>
+#include <iostream>
+
+int specialFilter(std::initializer_list<int> nums) {
+    int c = 0;
     for (int num : nums) {
-        if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
-            count++;
+        if (std::abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
+            c++;
         }
     }
-    return count;
+    return c; 
 }
