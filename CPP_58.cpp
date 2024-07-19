db6ea5cd67672c256569923e26d238a9360e43bf
beep boop <githubbot@vadim.me>
2024-07-19 21:55:34 +0200

    Solution 4 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt: 47fd5b5f-72e0-4abd-bbe3-e4b65b06f605.cpp:11:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 47fd5b5f-72e0-4abd-bbe3-e4b65b06f605.cpp:11:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 47fd5b5f-72e0-4abd-bbe3-e4b65b06f605.cpp:12:18: error: ‘intersection’ was not de clared in this scope    12 |         inserter(intersection, intersection.begin()));       |                  ^~~~~~~~~~~~ 47fd5b5f-72e0-4abd-bbe3-e4b65b06f605.cpp: At global scope: 47fd5b5f-72e0-4abd-bbe3-e4b65b06f605.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 47fd5b5f-72e0-4abd-bbe3-e4b65b06f605.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,11 +7,13 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(intersection, intersection.begin()));
+    vector<int> result(s1.begin(), s1.end());
+    for (int i : s1) {
+        if (find(s2.begin(), s2.end(), i) != s2.end()) {
+            result.push_back(i);
+        }
+    }
 
-    vector<int> result(intersection.begin(), intersection.end());
     return result;
 }
 
@@ -24,3 +26,4 @@ int main() {
         cout << i << " ";
     }
     return 0;
+}
