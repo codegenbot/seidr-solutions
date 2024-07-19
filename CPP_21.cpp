d6cc98ca5025b53a28a848d5d9d07ed18455fb12
beep boop <githubbot@vadim.me>
2024-07-19 14:15:11 +0200

    Solution 3 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/81l0jz2g Prompt:    15 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 56af1a68-e8b3-42f4-8bba-4a090542303b.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 56af1a68-e8b3-42f4-8bba-4a090542303b.cpp:15:43: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 56af1a68-e8b3-42f4-8bba-4a090542303b.cpp:14: 56af1a68-e8b3-42f4-8bba-4a090542303b.cpp: In function ‘int main()’: 56af1a68-e8b3-42f4-8bba-4a090542303b.cpp:24:20: error: ‘rescale_to_unit’ was not  declared in this scope    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 56af1a68-e8b3-42f4-8bba-4a090542303b.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,6 +1,8 @@
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
-}
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
 
 vector<float> rescale_to_unit(vector<float> numbers) {
     float min_num = *min_element(numbers.begin(), numbers.end());
@@ -13,4 +15,14 @@ vector<float> rescale_to_unit(vector<float> numbers) {
     }
     
     return rescaled_numbers;
+}
+
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+    
+    return 0;
 }
