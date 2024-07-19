721959cd9e7e80266704abdf3f4a5e0cc9fcca28
beep boop <githubbot@vadim.me>
2024-07-19 06:39:44 +0200

    Solution 47 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/kgtiz2j0 Prompt: Complete the following code given the task description and function signature.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,11 @@
-vector<string> reverse_delete(string s, string c) {
-        string res = "";
-        for (char ch : s) {
-            if (c.find(ch) == string::npos) {
-                res += ch;
-            }
+vector<string> reverse_delete(string s, string c){
+    string result = "";
+    for(char ch : s){
+        if(c.find(ch) == string::npos){
+            result += ch;
         }
-        string rev_res = res;
-        reverse(rev_res.begin(), rev_res.end());
-        return {res, res == rev_res ? "True" : "False"};
     }
+    string reversed = result;
+    reverse(reversed.begin(), reversed.end());
+    return {result, result == reversed ? "True" : "False"};
+}
