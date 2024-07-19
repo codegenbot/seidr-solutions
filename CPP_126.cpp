e230f7aca0e186f2866d9a2e612c352e4fece8a0
beep boop <githubbot@vadim.me>
2024-07-19 20:39:06 +0200

    Solution 47 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/h0fiaar5 Prompt: 373253d5-b489-4974-bc8c-fda77cc4e6bd.cpp:1:5: warning: missing terminating ' cha racter     1 | Here's the solution:       |     ^ 373253d5-b489-4974-bc8c-fda77cc4e6bd.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the solution:       |     ^~~~~~~~~~~~~~~~ 373253d5-b489-4974-bc8c-fda77cc4e6bd.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the solution:       | ^~~~ In file included from 373253d5-b489-4974-bc8c-fda77cc4e6bd.cpp:13: 373253d5-b489-4974-bc8c-fda77cc4e6bd.cpp: In function ‘int main()’: 373253d5-b489-4974-bc8c-fda77cc4e6bd.cpp:15:13: error: ‘is_sorted’ was not decla red in this scope    15 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,39 +1,36 @@
-```
 #include <iostream>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-int main() {
-    bool result;
-    std::vector<int> lst;
-    
-    int n;
-    std::cout << "Enter number of elements: ";
-    std::cin >> n;
-    
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> num;
-        lst.push_back(num);
-    }
-    
-    result = is_sorted(lst);
-    
-    if (result)
-        std::cout << "The list is sorted.\n";
-    else
-        std::cout << "The list is not sorted.\n";
-    
-    return 0;
-}
-
-bool is_sorted(vector<int> lst){
+bool is_sorted(vector<int> lst) {
     for(int i = 1; i < lst.size(); i++){
         if(lst[i] <= lst[i-1]){
-            if(count(lst.begin(), lst.end(), lst[i]) > 1)
+            bool has_duplicates = false;
+            for(int j = 0; j < lst.size(); j++){
+                if(j != i && lst[j] == lst[i])
+                    has_duplicates = true;
+            }
+            if(has_duplicates)
                 return false;
         }
     }
     return true;
+}
+
+int main() {
+    vector<int> lst;
+    int n;
+    cout << "Enter the number of elements in the list: ";
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cout << "Enter element " << i+1 << ": ";
+        cin >> x;
+        lst.push_back(x);
+    }
+    if(is_sorted(lst))
+        cout << "The list is sorted.\n";
+    else
+        cout << "The list is not sorted.\n";
+    return 0;
 }
