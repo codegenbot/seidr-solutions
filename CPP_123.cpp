c203ee1ee8eb64b4751459b9fafb45eff8ef1fbf
beep boop <githubbot@vadim.me>
2024-07-19 20:10:19 +0200

    Solution 35 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/j51oka9j Prompt:       |                           ^~~~~~ 8fc8ccc5-f1d9-4d73-9577-470ddd9222c1.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8fc8ccc5-f1d9-4d73-9577-470ddd9222c1.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8fc8ccc5-f1d9-4d73-9577-470ddd9222c1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8fc8ccc5-f1d9-4d73-9577-470ddd9222c1.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8fc8ccc5-f1d9-4d73-9577-470ddd9222c1.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 8fc8ccc5-f1d9-4d73-9577-470ddd9222c1.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -10,10 +10,15 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 std::vector<int> get_odd_collatz(int num) {
     std::vector<int> result;
     while (num != 1) {
-        result.push_back(num);
-        num = num % 2 == 0 ? num / 2 : 3 * num + 1;
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+            if (num % 2 != 0) {
+                result.push_back(num);
+            }
+        }
     }
-    result.push_back(1);
     return result;
 }
 
