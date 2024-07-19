2c5279cea85ff66c06087ac5c0e2144c44de7215
beep boop <githubbot@vadim.me>
2024-07-19 18:49:49 +0200

    Solution 0 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gahd0o9k Prompt: Complete the following code given the task description and function signature.

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,6 +1,1 @@
-#include <vector>
-#include <algorithm>
-
-float max_element(const std::vector<float>& l){
-    return *std::max_element(l.begin(), l.end());
-}
+vector<float> v = l; return *max_element(v.begin(), v.end());
