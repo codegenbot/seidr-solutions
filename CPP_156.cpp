07086d852341bc88f99300df78bd2f54f50f76be
beep boop <githubbot@vadim.me>
2024-07-19 18:47:30 +0200

    Solution 73 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/63mjzye5 Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,13 +1,14 @@
-vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-string int_to_mini_roman(int number) {
+string int_to_mini_roman(int num) {
+    string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     string result;
-    for (int i = 0; i < sizeof(values)/sizeof(int); i++) {
-        while (number >= values[i]) {
-            number -= values[i];
+
+    for (int i = 0; i < 13; i++) {
+        while (num >= val[i]) {
+            num -= val[i];
             result += roman[i];
         }
     }
-    return tolower(result);
+
+    return result;
 }
