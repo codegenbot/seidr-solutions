130d2d075e7cdde12b05aff49544dd76df8f9489
beep boop <githubbot@vadim.me>
2024-07-19 07:20:48 +0200

    Solution 91 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ldgewqe7 Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,15 +1,18 @@
-string int_to_mini_roman(int number){
-    vector<int> nums {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    vector<string> romans {"m", "cm", "d", "cd", "c", "xc", "l", "xl", "x", "ix", "v", "iv", "i"};
-    
+string int_to_mini_romank(int number){
+    vector<int> numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    vector<string> romans = {"m", "cm", "d", "cd", "c", "xc", "l", "xl", "x", "ix", "v", "iv", "i"};
+
     string result = "";
-    
-    for (int i = 0; i < nums.size(); i++) {
-        while (number >= nums[i]) {
+    int i = 0;
+
+    while (number > 0) {
+        if (number >= numbers[i]) {
             result += romans[i];
-            number -= nums[i];
+            number -= numbers[i];
+        } else {
+            i++;
         }
     }
-    
+
     return result;
 }
