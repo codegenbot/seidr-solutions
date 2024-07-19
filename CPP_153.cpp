b3e8fb28c4b881c744a970cd70a55eb98f1cb15b
beep boop <githubbot@vadim.me>
2024-07-20 01:21:10 +0200

    Solution 51 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7gdaekmr Prompt:       |                  ^~~~ In file included from /usr/include/c++/11/cassert:44,                  from ce864f02-6b7d-4d3c-a82a-107d5853baf9.cpp:1: ce864f02-6b7d-4d3c-a82a-107d5853baf9.cpp:19:1: error: expected unqualified-id be fore ‘static_cast’    19 | assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       | ^~~~~~ ce864f02-6b7d-4d3c-a82a-107d5853baf9.cpp:19:1: error: expected ‘)’ before ‘stati c_cast’    19 | assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       | ^~~~~~ ce864f02-6b7d-4d3c-a82a-107d5853baf9.cpp:19:50: error: expected unqualified-id b efore ‘)’ token    19 | assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |                                                  ^ ce864f02-6b7d-4d3c-a82a-107d5853baf9.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ ce864f02-6b7d-4d3c-a82a-107d5853baf9.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,4 +1,4 @@
-#include <assert.h>
+#include <cassert>
 #include <vector>
 #include <string>
 #include <iostream>
@@ -12,12 +12,8 @@ std::string Strongest_Extension(const std::string& class_name, const std::vector
     return class_name;
 }
 
-std::string class_name = "My";
-std::vector<std::string> extensions({"Java", "Python", "C++"}); 
-std::cout << Strongest_Extension(class_name, extensions) << std::endl;
-
-assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-
 int main() {
+    assert(Strongest_Extension("Sp", {"671235"}) == "Sp.671235");
+    std::cout << Strongest_Extension("My", {"Java", "Python", "C++"})) << std::endl;
     return 0;
 }
