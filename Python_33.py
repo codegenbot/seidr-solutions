c99c9882cbe9039c7c5cf7523016aae6be51d661
beep boop <githubbot@vadim.me>
2024-07-19 05:37:37 +0200

    Solution 67 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/14cfrwja Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f003b56-c1d2-4313-90db-2c2d91ff6 9ae.py", line 15, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f003b56-c1d2-4313-90db-2c2d91ff6 9ae.py", line 10, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,8 @@
 def sort_third(l: list) -> list:
     l += [0] * (3 - len(l) % 3)
     return [
-        sorted(sublist, key=lambda x: (x, x) if isinstance(x, int) else (float('inf'), x), reverse=False)
+        sorted(sublist, key=lambda x: (int(x), x) if isinstance(x, int) else (float('inf'), x), reverse=False)
         for sublist in [
             l[i:i + 3] for i in range(0, len(l), 3)
-            if len(l[i:i + 3]) == 3
         ]
     ]
