9e1a9cb7f19cea9b76cd52271e3d8a130e27ee1a
beep boop <githubbot@vadim.me>
2024-07-19 20:40:32 +0200

    Solution 9 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oojp8dsm Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
-    int b = stoi(x.substr(0, 1));
-    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
-    int d = stoi(n.substr(0, 1));
+    int a = 0, b = 1, c = 0, d = 1;
+    
+    // convert string to integer
+    size_t pos = x.find('/');
+    if (pos != string::npos) {
+        a = stoi(x.substr(0, pos));
+        b = stoi(x.substr(pos+1));
+    }
+    
+    pos = n.find('/');
+    if (pos != string::npos) {
+        c = stoi(n.substr(0, pos));
+        d = stoi(n.substr(pos+1));
+    }
 
-    if (a * d != b * c) {
-        return false;
-    }
-    else {
-        return true;
-    }
+    // check if x * n is a whole number
+    long long res = (long long)a*c + (long long)b*d;
+    long long gcd = __gcd(b*d, abs(res));
+    
+    return res / gcd == 0;
+}
+
+int main() {
+    cout << simplify("1/5", "5/1"); // prints: 1
+    cout << simplify("1/6", "2/1"); // prints: 0
+    cout << simplify("7/10", "10/2"); // prints: 0
 }
