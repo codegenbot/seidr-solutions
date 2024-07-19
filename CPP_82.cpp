5bc3e9630b777644ea11c1f0fb30c67fc61fb8d4
beep boop <githubbot@vadim.me>
2024-07-19 19:24:03 +0200

    Solution 72 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/h14ksvj2 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,29 +1,21 @@
-```cpp
 #include <iostream>
 #include <string>
 
 bool prime_length(std::string str){
-    auto end = str.begin() + str.size();
+    std::string strLen = std::to_string(str.length());
     bool isPrime = false;
 
-    if(end - str.begin() <= 1)
+    if(strLen.length() <= 1)
         return false;
     
-    for(auto it = str.begin(); it != end; ++it){
-        int i = std::stoi(string(it, end));
-        if(i * i > end - str.begin())
-            break;
-        if(i == (end - str.begin())){
-            isPrime = true;
-            break;
-        }
-        if((end - str.begin()) % i == 0){
+    for(int i = 2; i*i <= strLen.length(); i++){
+        if(strLen.length() % i == 0){
             isPrime = false;
             break;
         }
     }
 
-    return isPrime;
+    return true;
 
 }
 
@@ -31,10 +23,6 @@ int main() {
     std::string str;
     std::cout << "Enter a string: ";
     std::getline(std::cin, str);
-    
-    if(prime_length(str)){
-        std::cout << "The length of the given string is prime." << std::endl;
-    } else{
-        std::cout << "The length of the given string is not prime." << std::endl;
-    }
+    bool result = prime_length(str);
+    std::cout << "Is length prime? " << (result ? "Yes" : "No") << std::endl;
 }
