a69bf69265e28b355a9e045f46f41352dab408dc
beep boop <githubbot@vadim.me>
2024-07-20 00:58:52 +0200

    Solution 27 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7gdaekmr Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 6dceb96f-7216-4ab2-a9b3-4fac1e9eb164.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -4,17 +4,6 @@
 #include <cassert>
 #include <string>
 
-int Strongest_Extension(string class_name, vector<string> extensions);
-
-int main(){
-    string class_name = "My";
-    vector<string> extensions = {"Java", "Python", "C++"};
-    cout << Strongest_Extension(class_name, extensions) << endl;
-    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    return 0;
-
-}
-
 int Strongest_Extension(string class_name, vector<string> extensions){
     int max_strength = 0;
     string strongest_extension;
@@ -33,4 +22,13 @@ int Strongest_Extension(string class_name, vector<string> extensions){
     }
 
     return class_name + "." + strongest_extension;
+}
+
+int main(){
+    string class_name = "My";
+    vector<string> extensions = {"Java", "Python", "C++"};
+    cout << Strongest_Extension(class_name, extensions) << endl;
+    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
+
 }
