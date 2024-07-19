db9fabd7e9963074fb0ee47b6a8e46becc40bf89
beep boop <githubbot@vadim.me>
2024-07-19 05:35:18 +0200

    Solution 27 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/axkkygyw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9a24a73e-7332-44e4-ade5-057622d1a c67.py", line 7, in <module>     check(parse_music)           ^^^^^^^^^^^ NameError: name 'parse_music' is not defined

--- Python_17.py
+++ Python_17.py
@@ -1,2 +1,10 @@
+def parse_music():
+    music_strings = input().split(';')
+    notes = dict(input().split() for _ in range(int(input())))
+    
+    return get_notes(music_strings, notes)
+
 def get_notes(music_strings, notes):
     return [notes[note] for music_string in music_strings for note in music_string.split()]
+
+print(parse_music())
