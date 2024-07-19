eec2d7e211afb3949e5eaa48c94d4857365951a2
beep boop <githubbot@vadim.me>
2024-07-19 21:28:16 +0200

    Solution 47 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mq8vqgyj Prompt: 31afa147-7c21-4936-8077-740c52a3f7fe.cpp:27:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    27 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 31afa147-7c21-4936-8077-740c52a3f7fe.cpp:17:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    17 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 31afa147-7c21-4936-8077-740c52a3f7fe.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 31afa147-7c21-4936-8077-740c52a3f7fe.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -17,7 +17,7 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 int main() {
     assert(issame(common({4, 3, 2, 8}, {}), {}));
     assert(issame(common({1, 2, 3}, {3, 4, 5}), {3}));
-    assert(issame(common({1, 5, 3, 7}, {2, 4, 6}), {}));
+    assert(issame(common({1, 5, 3, 7}, {2, 4, 6}), {});	
     
     return 0;
 }
