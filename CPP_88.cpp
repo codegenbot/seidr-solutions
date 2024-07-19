36c70185cff041159dc1cff8af3fea50c39f1a38
beep boop <githubbot@vadim.me>
2024-07-19 18:56:59 +0200

    Solution 7 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gs4mjcnh Prompt:       |                    ^~~ 90a54363-aab7-488a-99bd-eb1f80385892.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 90a54363-aab7-488a-99bd-eb1f80385892.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 90a54363-aab7-488a-99bd-eb1f80385892.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 90a54363-aab7-488a-99bd-eb1f80385892.cpp:11: 90a54363-aab7-488a-99bd-eb1f80385892.cpp: In function ‘int main()’: 90a54363-aab7-488a-99bd-eb1f80385892.cpp:21:20: error: ‘sort_array’ was not decl ared in this scope    21 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 90a54363-aab7-488a-99bd-eb1f80385892.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -12,9 +12,9 @@ std::vector<int> sort_array(std::vector<int> array){
     if(array.empty()) return array;
     int sum = array.front() + array.back();
     if(sum % 2 == 0){
-        std::sort(array.begin(), array.end(), std::greater<int>());
+        sort(array.begin(), array.end(), std::greater<int>());
     } else {
-        std::sort(array.begin(), array.end());
+        sort(array.begin(), array.end());
     }
     return array;
 }
