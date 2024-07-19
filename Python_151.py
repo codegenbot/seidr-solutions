e3e82a1570c463b6ac5986cdbe43f16788a7df33
beep boop <githubbot@vadim.me>
2024-07-19 07:51:28 +0200

    Solution 4 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8yocbg9w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f31dad85-5da9-42d7-b908-404473a95 d9d.py", line 8     lst = [1_lst, 2, 3, 4, 5]             ^ SyntaxError: invalid decimal literal

--- Python_151.py
+++ Python_151.py
@@ -1,10 +1,8 @@
 def check(func, lst):
     return func(lst)
 
-
 def double_the_difference(lst):
     return sum(x**2 for x in lst if x > 0 and isinstance(x, int) and x % 2 != 4) * 2
-
 
 # Define the input list 'lst'
 lst = [1, 2, 3, 4, 5]
