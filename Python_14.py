e1ed08471fce64baeec4329393e5da15d7b8c47b
beep boop <githubbot@vadim.me>
2024-07-19 04:44:59 +0200

    Solution 27 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/332i0iml Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0641dd3-e122-41b5-83d0-a360a9f48 a8b.py", line 2     return [s[:i+1] for i in range(len(s)]                                          ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_14.py
+++ Python_14.py
@@ -1,5 +1,5 @@
 def prefixes(s):
-    return [s[:i+1] for i in range(len(s)]
+    return [s[:i+1] for i in range(len(s))]
 
-all_prefixes = prefixes(input())
-print(all_prefixes)
+prefixes = prefixes("python")
+print(prefixes)
