7f7e6750286e9dbe2c29ccb3d8a9606e0237ab5d
beep boop <githubbot@vadim.me>
2024-07-20 01:07:42 +0200

    Solution 3 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ewvvcvho Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,34 +1,20 @@
-int count_digits = 0;
-    int dot_position = -1;
-    
-    for (int i = 0; i < file_name.size(); ++i) {
-        if (isdigit(file_name[i])) {
-            count_digits++;
-            if (count_digits > 3) {
-                return "No";
-            }
-        } else if (file_name[i] == '.') {
-            if (dot_position != -1) {
-                return "No";
-            }
-            dot_position = i;
+int digit_count = 0;
+    for (char c : file_name) {
+        if (isdigit(c)) {
+            digit_count++;
         }
     }
-    
-    if (dot_position == -1 || dot_position == 0 || dot_position == file_name.size() - 1) {
+
+    size_t dot_pos = file_name.find(".");
+    if (digit_count > 3 || dot_pos == string::npos || dot_pos == 0 || dot_pos == file_name.size() - 1) {
         return "No";
     }
-    
-    string before_dot = file_name.substr(0, dot_position);
-    string after_dot = file_name.substr(dot_position + 1);
-    
-    if (!isalpha(before_dot[0])) {
+
+    string prefix = file_name.substr(0, dot_pos);
+    string suffix = file_name.substr(dot_pos + 1);
+    if (!isalpha(prefix[0]) || (suffix != "txt" && suffix != "exe" && suffix != "dll")) {
         return "No";
     }
-    
-    if (after_dot != "txt" && after_dot != "exe" && after_dot != "dll") {
-        return "No";
-    }
-    
+
     return "Yes";
 }
