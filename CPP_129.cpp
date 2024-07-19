2c8a6e08597048b26218c8ce2beb6c8c4a947bc2
beep boop <githubbot@vadim.me>
2024-07-19 03:47:28 +0200

    Solution 10 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9xcpuj31 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7ab1e049-0b1d-4d0f-8dc6-b477e8058ee5.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7ab1e049-0b1d-4d0f-8dc6-b477e8058ee5.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7ab1e049-0b1d-4d0f-8dc6-b477e8058ee5.cpp:5: 7ab1e049-0b1d-4d0f-8dc6-b477e8058ee5.cpp: In function ‘int main()’: 7ab1e049-0b1d-4d0f-8dc6-b477e8058ee5.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 7ab1e049-0b1d-4d0f-8dc6-b477e8058ee5.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -4,13 +4,10 @@ bool issame(vector<int> a, vector<int> b){
 
 vector<int> minPath(vector<vector<int>> grid, int k){
     // Your code here
-}
-
-vector<int> minPath(vector<vector<int>> grid, int k){
-    // Your code here
+    return {};
 }
 
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
+    assert (issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
     return 0;
 }
