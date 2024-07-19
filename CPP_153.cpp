19f7f0412c8f64656a3cad4e52fd3e78618e6a5e
beep boop <githubbot@vadim.me>
2024-07-19 05:16:51 +0200

    Solution 50 of CPP/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qbnsqv6m Prompt: 10473d84-385f-4722-bb90-0936fb4b1ecd.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions) {       | ^~~~~~ In file included from 10473d84-385f-4722-bb90-0936fb4b1ecd.cpp:25: 10473d84-385f-4722-bb90-0936fb4b1ecd.cpp: In function ‘int main()’: 10473d84-385f-4722-bb90-0936fb4b1ecd.cpp:27:13: error: ‘Strongest_Extension’ was  not declared in this scope    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,18 +1,16 @@
-#include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
+#include <cctype>
+#include <climits>
 
 using namespace std;
 
-string Strongest_Extension(string class_name, vector<string> extensions) {
-    string strongest_extension = "";
+string Strongest_Extension(string class_name, vector<string> extensions){
+    string strongest_extension;
     int max_strength = INT_MIN;
-
-    for (const string& ext : extensions) {
-        int cap_count = 0;
-        int sm_count = 0;
-
+    
+    for (const auto& ext : extensions) {
+        int cap_count = 0, sm_count = 0;
         for (char c : ext) {
             if (isupper(c)) {
                 cap_count++;
@@ -20,19 +18,13 @@ string Strongest_Extension(string class_name, vector<string> extensions) {
                 sm_count++;
             }
         }
-
+        
         int strength = cap_count - sm_count;
         if (strength > max_strength || (strength == max_strength && ext < strongest_extension)) {
             max_strength = strength;
             strongest_extension = ext;
         }
     }
-
+    
     return class_name + "." + strongest_extension;
-}
-
-int main() {
-    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-
-    return 0;
 }
