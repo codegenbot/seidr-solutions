ae9fba61c1cb86111574c9996dc6a9c950c5b57a
beep boop <githubbot@vadim.me>
2024-07-19 20:41:48 +0200

    Solution 20 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5qgavfh3 Prompt:       |                    ^~~ 941b1b1a-b102-4321-954a-6e2c642508d4.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 941b1b1a-b102-4321-954a-6e2c642508d4.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 941b1b1a-b102-4321-954a-6e2c642508d4.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 941b1b1a-b102-4321-954a-6e2c642508d4.cpp:10: 941b1b1a-b102-4321-954a-6e2c642508d4.cpp: In function ‘int main()’: 941b1b1a-b102-4321-954a-6e2c642508d4.cpp:20:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    20 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 941b1b1a-b102-4321-954a-6e2c642508d4.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,22 +1,11 @@
 #include <vector>
-#include <cassert>
 
-using namespace std;
-
-vector<int> issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> sum_product(const vector<int>& numbers) {
-    int sum = 0, product = 1;
+std::vector<int> sum_product(std::vector<int> numbers) {
+    int sum = 0;
+    int product = 1;
     for (int num : numbers) {
         sum += num;
         product *= num;
     }
-    return {sum, product};
-}
-
-int main() {
-    assert(issame(sum_product({10}), {10, 10}));
-    return 0;
+    return { sum, product };
 }
