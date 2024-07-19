202d98f92d749b8df1b5431ec6b9ce2797146c8b
beep boop <githubbot@vadim.me>
2024-07-20 00:56:14 +0200

    Solution 3 of CPP/160, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/t7siarcs Prompt: In file included from /usr/include/c++/11/vector:67,                  from 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp:6:49: error: expected primary-expressio n before ‘int’     6 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp:6:61: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp:45: 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp: In function ‘int main()’: 9d8b761a-8516-4363-8eeb-11af7367b4b9.cpp:47:23: error: ‘do_algebra’ cannot be us ed as a function    47 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,35 +1,41 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int do_algebra(vector<string> operators, vector<int> operands) {
     int result = 0;
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
-            result += operand[i];
-        } else if (operator_[i] == "-") {
-            result -= operand[i];
-        } else if (operator_[i] == "*") {
+    for (int i = 0; i < operators.size(); i++) {
+        if (operators[i] == "+") {
+            result += operands[i];
+        } else if (operators[i] == "-") {
+            result -= operands[i];
+        } else if (operators[i] == "*") {
             int temp = 0;
-            for (int j = i; j < operand.size(); j++) {
-                temp += operand[j];
-                if (j < operand.size() - 1) {
-                    if (operator_[j + 1] == "*") {
-                        temp *= operand[j + 1];
-                    } else if (operator_[j + 1] == "//") {
-                        temp = temp / operand[j + 1];
-                    } else if (operator_[j + 1] == "**") {
-                        temp = pow(temp, operand[j + 1]);
+            for (int j = i; j < operands.size(); j++) {
+                temp += operands[j];
+                if (j < operands.size() - 1) {
+                    if (operators[j + 1] == "*") {
+                        temp *= operands[j + 1];
+                    } else if (operators[j + 1] == "//") {
+                        temp = temp / operands[j + 1];
+                    } else if (operators[j + 1] == "**") {
+                        temp = pow(temp, operands[j + 1]);
                     }
                 }
             }
             result = temp;
-        } else if (operator_[i] == "//") {
-            int temp = operand[i - 1];
-            for (int j = i; j < operand.size(); j++) {
-                temp /= operand[j];
+        } else if (operators[i] == "//") {
+            int temp = operands[i - 1];
+            for (int j = i; j < operands.size(); j++) {
+                temp /= operands[j];
             }
             result = temp;
-        } else if (operator_[i] == "**") {
-            int temp = pow(operand[i - 1], 1);
-            for (int j = i; j < operand.size(); j++) {
-                temp = pow(temp, operand[j]);
+        } else if (operators[i] == "**") {
+            int temp = pow(operands[i - 1], 1);
+            for (int j = i; j < operands.size(); j++) {
+                temp = pow(temp, operands[j]);
             }
             result = temp;
         }
