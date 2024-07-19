58f8317e70343b72d1277f21b4098d389a02a097
beep boop <githubbot@vadim.me>
2024-07-19 21:52:34 +0200

    Solution 28 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jebzfru9 Prompt:       |                    ^~~ a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp:17: a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp: In function ‘int main()’: a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ a9130dc3-1c16-4eb4-bf2c-fbc50fbff4c8.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,27 +1,33 @@
 #include <vector>
-#include <cassert>
 
-using namespace std;
+vector<int> make_a_pile(int n);
 
 bool issame(vector<int> a, vector<int> b){
-    return a == b;
+    if(a.size() != b.size()){
+        return false;
+    }
+    
+    for(int i = 0; i < a.size(); ++i){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 vector<int> make_a_pile(int n){
-    vector<int> stones;
-    stones.push_back(n);
+    vector<int> result;
+    result.push_back(n);
+
     for(int i = 1; i < n; ++i){
         if(n % 2 == 0){
             n += 2;
         } else {
             n += 1;
         }
-        stones.push_back(n);
+        result.push_back(n);
     }
-    return stones;
-}
 
-int main(){
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
-    return 0;
+    return result;
 }
