34306a1b4a7aa4f8308ee7673db6d82aff31096b
beep boop <githubbot@vadim.me>
2024-07-19 02:24:27 +0200

    Solution 13 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yepgxqdx Prompt:       |                           ^~~~~~ d6fbd330-e16c-4ab5-99e2-900c4b6b6261.cpp:41:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d6fbd330-e16c-4ab5-99e2-900c4b6b6261.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d6fbd330-e16c-4ab5-99e2-900c4b6b6261.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d6fbd330-e16c-4ab5-99e2-900c4b6b6261.cpp:41:34: error: expected primary-expressi on before ‘int’    41 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d6fbd330-e16c-4ab5-99e2-900c4b6b6261.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ d6fbd330-e16c-4ab5-99e2-900c4b6b6261.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main(){       |     ^~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,16 +1,14 @@
 #include <vector>
 #include <cassert>
 
-namespace std {
-    bool issame(std::vector<int> a, std::vector<int> b){
-        if (a.size() != b.size()) 
+bool issame(std::vector<int> a, std::vector<int> b){
+    if (a.size() != b.size()) 
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
             return false;
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] != b[i])
-                return false;
-        }
-        return true;
     }
+    return true;
 }
 
 std::vector<int> pluck(std::vector<int> arr){
@@ -33,8 +31,8 @@ std::vector<int> pluck(std::vector<int> arr){
     return result;
 }
 
-// int main(){
-//     assert(issame(pluck({7, 9, 7, 1}), {}));
+int main(){
+    assert(issame(pluck({7, 9, 7, 1}), {}));
     
-//     return 0;
-//}
+    return 0;
+}
