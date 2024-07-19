af1f17b81340594e20ecff77783562ce07bd55dd
beep boop <githubbot@vadim.me>
2024-07-19 04:07:15 +0200

    Solution 28 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pkm4vxj8 Prompt:    21 | return "None";       | ^~~~~~ ced162f0-b48c-457f-8a29-bbe47cb367c2.cpp:22:1: error: expected declaration befor e ‘}’ token    22 | }       | ^ In file included from ced162f0-b48c-457f-8a29-bbe47cb367c2.cpp:24: ced162f0-b48c-457f-8a29-bbe47cb367c2.cpp: In function ‘int main()’: ced162f0-b48c-457f-8a29-bbe47cb367c2.cpp:26:13: error: ‘boost’ has not been decl ared    26 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ ced162f0-b48c-457f-8a29-bbe47cb367c2.cpp:26:29: error: ‘string’ was not declared  in this scope    26 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ ced162f0-b48c-457f-8a29-bbe47cb367c2.cpp:26:37: error: ‘compare_one’ was not dec lared in this scope    26 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,46 +1,26 @@
 #include <boost/any.hpp>
-#include <algorithm>
 #include <string>
-#include <cassert>
-#include <boost/lexical_cast.hpp>
 
-using namespace std;
-using namespace boost;
-
-void replace(std::string::iterator begin, std::string::iterator end, char target, char replacement) {
-    std::replace(begin, end, target, replacement);
-}
-
-boost::any compare_one(const boost::any &a, const boost::any &b) {
-    if(a.type() == typeid(int) && b.type() == typeid(int)){
-        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+boost::any compare_one(const boost::any& a, const boost::any& b) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
             return a;
-        } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
             return b;
-        } else {
-            return "None";
         }
-    } else if(a.type() == typeid(float) && b.type() == typeid(float)){
-        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
             return a;
-        } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+        } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
             return b;
-        } else {
-            return "None";
         }
-    } else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
-        std::string strA = boost::any_cast<std::string>(a);
-        std::string strB = boost::any_cast<std::string>(b);
-        if(strA.find_first_of(".,") != string::npos) {
-            replace(strA.begin(), strA.end(), ',', '.');
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        if (std::stof(boost::any_cast<std::string>(a)) > stof(boost::any_cast<std::string>(b))) {
+            return a;
+        } else if (std::stof(boost::any_cast<std::string>(a)) < std::stof(boost::any_cast<std::string>(b))) {
+            return b;
         }
-        if(strB.find_first_of(".,") != string::npos) {
-            replace(strB.begin(), strB.end(), ',', '.');
-        }
-        return stof(strA) > stof(strB) ? a : (stof(strA) < stof(strB) ? b : "None");
-    } else {
-        return "None";
     }
-}
 
-assert(boost::any_cast<std::string>(compare_one(std::string("1"), 1)) == "None");
+    return boost::any();
+}
