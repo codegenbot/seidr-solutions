42b01f29340c534a834c5aa3ce0a6ad06dcb958b
beep boop <githubbot@vadim.me>
2024-07-19 17:13:07 +0200

    Solution 46 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nz5dw6ud Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a4ad4fd-fe32-4e5d-a85f-57b8992c3 7dc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -2,8 +2,10 @@
 def sum_odd_digits(lst):
     return sum(int(d) for d in "".join(map(str, lst)) if int(d) % 2 != 0)
 
+
 def check(lst):
-    solution = sum_odd_digits(lst)
-    return solution == 9
+    total_sum = sum_odd_digits(lst)
+    return total_sum == 9
+
 
 print(check([1, 3, 5]))
