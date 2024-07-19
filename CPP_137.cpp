03fed83bbed7304d4fd05701f27b6cb58692b8fd
beep boop <githubbot@vadim.me>
2024-07-19 06:58:37 +0200

    Solution 36 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/riusp3s9 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,33 +1,29 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
         return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
         return b;
-    else
+    } else {
         return "None";
-}
-else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
+    }
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
         return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
         return b;
-    else
+    } else {
         return "None";
-}
-else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b)))
+    }
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    float val1 = stof(boost::any_cast<string>(a).replace(boost::any_cast<string>(a).find(','), 1, "."));
+    float val2 = stof(boost::any_cast<string>(b).replace(boost::any_cast<string>(b).find(','), 1, "."));
+    if (val1 > val2) {
         return a;
-    else if(stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b)))
+    } else if (val1 < val2) {
         return b;
-    else
+    } else {
         return "None";
-}
-else if((a.type() == typeid(int) && b.type() == typeid(string)) || (a.type() == typeid(string) && b.type() == typeid(int))){
-    if(stof(a.type() == typeid(int) ? to_string(boost::any_cast<int>(a)) : boost::any_cast<string>(a)) > stof(b.type() == typeid(int) ? to_string(boost::any_cast<int>(b)) : boost::any_cast<string>(b)))
-        return a;
-    else if(stof(a.type() == typeid(int) ? to_string(boost::any_cast<int>(a)) : boost::any_cast<string>(a)) < stof(b.type() == typeid(int) ? to_string(boost::any_cast<int>(b)) : boost::any_cast<string>(b)))
-        return b;
-    else
-        return "None";
-}
+    }
+} else {
+    return "None";
 }
