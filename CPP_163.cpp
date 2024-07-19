0f989154f3bb913b2d94468242b3fa724be96c70
beep boop <githubbot@vadim.me>
2024-07-20 01:46:25 +0200

    Solution 26 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mfpo1d5l Prompt:       |                    ^~~ 756d06c2-f37a-4143-b262-bb8157c36066.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 756d06c2-f37a-4143-b262-bb8157c36066.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 756d06c2-f37a-4143-b262-bb8157c36066.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 756d06c2-f37a-4143-b262-bb8157c36066.cpp:10: 756d06c2-f37a-4143-b262-bb8157c36066.cpp: In function ‘int main()’: 756d06c2-f37a-4143-b262-bb8157c36066.cpp:20:20: error: ‘generate_integers’ was n ot declared in this scope    20 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 756d06c2-f37a-4143-b262-bb8157c36066.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,9 +1,20 @@
+#include <iostream>
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()){
+      return false;
+    }
+    for(int i = 0; i < a.size(); ++i){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
 
-vector<int> generate_integers(int a, int b){
-    vector<int> result;
+std::vector<int> generate_integers(int a, int b) {
+    std::vector<int> result;
     for(int i = a; i <= b; ++i){
         if(i % 2 == 0){
             result.push_back(i);
