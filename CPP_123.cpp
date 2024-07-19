64632ab88628eac7b464155021c2ca4b14796850
beep boop <githubbot@vadim.me>
2024-07-19 04:55:54 +0200

    Solution 66 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uavzzxsu Prompt:       |                    ^~~ 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp:24: 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp: In function ‘int main()’: 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp:34:20: error: ‘get_odd_collatz’ was not  declared in this scope    34 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 0a70a39e-13e1-49b5-a962-716c37b8740c.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); ++i) {
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
@@ -13,7 +13,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return true;
 }
 
-std::vector<int> get_odd_collatz(int n){
+std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
     while (n != 1) {
         result.push_back(n);
