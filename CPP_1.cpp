1c963b10282d641bc3be34230422d5fb275ff22e
beep boop <githubbot@vadim.me>
2024-07-19 22:35:32 +0200

    Solution 47 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/imdtc467 Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 59a94736-a75b-4674-bd78-805ad081cab8.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 59a94736-a75b-4674-bd78-805ad081cab8.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 59a94736-a75b-4674-bd78-805ad081cab8.cpp:25: 59a94736-a75b-4674-bd78-805ad081cab8.cpp: In function ‘int main()’: 59a94736-a75b-4674-bd78-805ad081cab8.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 59a94736-a75b-4674-bd78-805ad081cab8.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,21 +1,23 @@
-vector<string> separate_paren_groups(vector<string> a, vector<string> b) {
+vector<string> separate_paren_groups(const vector<string>& a, vector<string> b) {
     vector<string> result;
     string current_group = "";
     int balance = 0;
 
-    for (char c : a[0]) {
-        if (c == '(') {
-            if (balance > 0) {
-                current_group += c;
-            }
-            balance++;
-        } else if (c == ')') {
-            balance--;
-            if (balance > 0) {
-                current_group += c;
-            } else if (balance == 0) {
-                result.push_back(current_group);
-                current_group = "";
+    for (string paren_string : b) {
+        for (char c : paren_string) {
+            if (c == '(') {
+                if (balance > 0) {
+                    current_group += c;
+                }
+                balance++;
+            } else if (c == ')') {
+                balance--;
+                if (balance > 0) {
+                    current_group += c;
+                } else if (balance == 0) {
+                    result.push_back(current_group);
+                    current_group = "";
+                }
             }
         }
     }
