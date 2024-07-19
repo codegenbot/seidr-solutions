5fdcd6a42041a7911dcd5b5f8be90f18a382fee4
beep boop <githubbot@vadim.me>
2024-07-19 04:56:51 +0200

    Solution 73 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uavzzxsu Prompt:       |                    ^~~ 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp:19: 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp: In function ‘int main()’: 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp:29:20: error: ‘get_odd_collatz’ was not  declared in this scope    29 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 47ead4fc-4269-41d2-a96a-b751cab93f9c.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -5,31 +5,34 @@ bool issame(int a, int b) {
     return a == b;
 }
 
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> result;
     while (n != 1) {
         result.push_back(n);
         n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
 
-    vector<int> odd_numbers;
+    std::vector<int> odd_numbers;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_numbers.push_back(num);
         }
     }
 
-    sort(odd_numbers.begin(), odd_numbers.end(), issame);
+    std::sort(odd_numbers.begin(), odd_numbers.end(), issame);
     return odd_numbers;
 }
 
 int main() {
     int n;
-    cin >> n;
-    vector<int> odd_collatz = get_odd_collatz(n);
-    for (int num : odd_collatz) {
-        cout << num << " ";
+    std::cin >> n;
+    
+    std::vector<int> result = get_odd_collatz(n);
+    
+    for (int num : result) {
+        std::cout << num << " ";
     }
+    
     return 0;
 }
