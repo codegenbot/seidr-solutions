66b6f93859fc63c955733eabf98ec3674b00fb5a
beep boop <githubbot@vadim.me>
2024-07-19 18:20:15 +0200

    Solution 39 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xbxthrl1 Prompt:       |           ^~~~~~ In file included from b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp:33:36: error: expected primary-expressi on before ‘float’    33 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp:33:43: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp:32: b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp: In function ‘int main()’: b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp:42:20: error: ‘get_positive’ was not de clared in this scope; did you mean ‘getPositive’?    42 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ b87cfac9-9658-49c0-b3eb-a7c4edeed8fb.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -24,36 +24,6 @@ std::vector<float> get_positive(std::vector<float> l) {
 }
 
 int main_func() {
-    assert(getPositive({}).size() == get_positive({}).size());
-    int n1, n2; 
-    std::cout << "Enter the number of elements for first list: ";
-    std::cin >> n1;
-    float arr1[n1];
-    for (int i = 0; i < n1; ++i) {
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> arr1[i];
-    }
-    std::vector<float> l1(arr1, arr1+n1);
-    std::cout << "The positive elements of the list are: ";
-    for (float f : get_positive(l1)) {
-        std::cout << f << " ";
-    }
-    std::cout << "\n";
-    
-    int n3, n4; 
-    std::cout << "Enter the number of elements for second list: ";
-    std::cin >> n3;
-    float arr2[n3];
-    for (int i = 0; i < n3; ++i) {
-        std::cout << "Enter element " << i + 1 << ": ";
-        std::cin >> arr2[i];
-    }
-    std::vector<float> l2(arr2, arr2+n3);
-    if (!issame(l1, l2)) {
-        std::cout << "Lists are not the same.\n";
-    } else {
-        std::cout << "Lists are the same.\n";
-    }
-    
+    assert(isSame(getPositive({}), {}));
     return 0;
 }
