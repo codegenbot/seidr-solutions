126d02fc35424617a61b896b864c55e496c7774f
beep boop <githubbot@vadim.me>
2024-07-19 22:37:50 +0200

    Solution 2 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r58ft6fs Prompt: 91e2e839-c013-495f-8c63-9aeadad08284.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int skjkasdkd(vector<int> lst){       |               ^~~~~~ 91e2e839-c013-495f-8c63-9aeadad08284.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int skjkasdkd(vector<int> lst){       |                      ^~~ In file included from 91e2e839-c013-495f-8c63-9aeadad08284.cpp:25: 91e2e839-c013-495f-8c63-9aeadad08284.cpp: In function ‘int main()’: 91e2e839-c013-495f-8c63-9aeadad08284.cpp:27:22: error: ‘skjkasdkd’ cannot be use d as a function    27 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int skjkasdkd(vector<int> lst){
+int calculateSumOfDigits(vector<int> lst){
     int maxPrime = 0;
     for(int i : lst){
         if(isPrime(i) && i > maxPrime)
@@ -22,8 +22,4 @@ bool isPrime(int n){
             return false;
     }
     return true;
-
-int main() {
-    assert (skjkasdkd({127, 97, 8192}) == 10);
-    return 0;
 }
