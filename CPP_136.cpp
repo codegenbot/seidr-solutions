b7832e7e2bf0e318568aeb3eb32b3b199c1a1927
beep boop <githubbot@vadim.me>
2024-07-19 04:59:30 +0200

    Solution 11 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2elf4yq3 Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b3346dd1-2d5d-42fc-87ef-60c307a6fc76.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b3346dd1-2d5d-42fc-87ef-60c307a6fc76.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b3346dd1-2d5d-42fc-87ef-60c307a6fc76.cpp:15: b3346dd1-2d5d-42fc-87ef-60c307a6fc76.cpp: In function ‘int main()’: b3346dd1-2d5d-42fc-87ef-60c307a6fc76.cpp:25:20: error: ‘largest_smallest_integer s’ was not declared in this scope    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ b3346dd1-2d5d-42fc-87ef-60c307a6fc76.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,12 +1,12 @@
 bool issame(vector<int> a, vector<int> b);
 
 vector<int> largest_smallest_integers(vector<int> lst){
-    int largest_negative = INT_MIN, smallest_positive = INT_MAX;
+    int largest_negative = 0, smallest_positive = 0;
     
     for (int num : lst) {
-        if (num < 0 && num > largest_negative) {
+        if (num < 0 && num < largest_negative) {
             largest_negative = num;
-        } else if (num > 0 && num < smallest_positive) {
+        } else if (num > 0 && (num < smallest_positive || smallest_positive == 0)) {
             smallest_positive = num;
         }
     }
