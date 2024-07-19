8a6d2f1db92590d15dbd1f2d30a8fc492bcd898f
beep boop <githubbot@vadim.me>
2024-07-19 05:38:26 +0200

    Solution 60 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9doarruo Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1fa9de8e-4028-4e72-856a-01e96a908057.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 1fa9de8e-4028-4e72-856a-01e96a908057.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 1fa9de8e-4028-4e72-856a-01e96a908057.cpp:22: 1fa9de8e-4028-4e72-856a-01e96a908057.cpp: In function ‘int main()’: 1fa9de8e-4028-4e72-856a-01e96a908057.cpp:32:20: error: ‘sorted_list_sum’ was not  declared in this scope    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 1fa9de8e-4028-4e72-856a-01e96a908057.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,9 +3,7 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b) {
+bool areSame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
@@ -21,7 +19,7 @@ vector<string> sorted_list_sum(vector<string> lst){
 }
 
 int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    assert(areSame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     
     return 0;
 }
