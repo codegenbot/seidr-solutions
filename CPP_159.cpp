f3f7a2fff9224a6e7acfefc5f4181ad2657cc05f
beep boop <githubbot@vadim.me>
2024-07-20 01:59:08 +0200

    Solution 96 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/okk9vzhs Prompt:       |                    ^~~ eaa9d138-268b-43ba-94fc-f5885b460c79.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eaa9d138-268b-43ba-94fc-f5885b460c79.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eaa9d138-268b-43ba-94fc-f5885b460c79.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eaa9d138-268b-43ba-94fc-f5885b460c79.cpp:7: eaa9d138-268b-43ba-94fc-f5885b460c79.cpp: In function ‘int main()’: eaa9d138-268b-43ba-94fc-f5885b460c79.cpp:17:20: error: ‘eat’ was not declared in  this scope    17 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ eaa9d138-268b-43ba-94fc-f5885b460c79.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,16 +1,13 @@
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+    return (a == b);
 }
 
 std::vector<int> eat(int number, int need, int remaining){
     int totalEatenCarrots = number + std::min(remaining, need);
     int carrotsLeft = std::max(0, remaining - (need - number));
     return {totalEatenCarrots, carrotsLeft};
-}
-
-int main(){
-    assert(issame(eat(4, 5, 1), {5, 0}));
-    return 0;
 }
