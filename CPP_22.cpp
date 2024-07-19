d56ac57a139294d3a3be9715fc61f532239d5de3
beep boop <githubbot@vadim.me>
2024-07-19 21:56:09 +0200

    Solution 68 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/utcbni4v Prompt: In file included from 172c5175-4b36-483e-bfa5-7a483dcbef3f.cpp:23: /usr/include/assert.h:361012:56: error: unterminated argument list invoking macr o "assert" 172c5175-4b36-483e-bfa5-7a483dcbef3f.cpp:21:1: error: ‘assert’ does not name a t ype    21 | assert(issame({1}, filter_integers({{1}, {2}, {3}, {"hello"}, {4}}));       | ^~~~~~ 172c5175-4b36-483e-bfa5-7a483dcbef3f.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <vector>   +++ |+#include <cassert>     5 | #include <initializer_list>

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -19,4 +19,4 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
-assert((issame({1}, filter_integers({{1}, {2}, {3}, {"hello"}, {4}})));
+assert(issame({1}, filter_integers({{1}, {2}, {3}, {"hello"}, {4}}));
