47715dfa7713ad3b05b08984ce1bf8bf15ca5230
beep boop <githubbot@vadim.me>
2024-07-19 21:50:55 +0200

    Solution 31 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d91o4gsr Prompt:    33 |         vector<double> coeffs = {};       |         ^~~~~~ 0505e9d3-a75c-4ac7-8d58-ae0e24ff52fd.cpp:33:9: note: ‘std::vector’ is defined in  header ‘<vector>’; did you forget to ‘#include <vector>’? 0505e9d3-a75c-4ac7-8d58-ae0e24ff52fd.cpp:33:16: error: expected primary-expressi on before ‘double’    33 |         vector<double> coeffs = {};       |                ^~~~~~ 0505e9d3-a75c-4ac7-8d58-ae0e24ff52fd.cpp:38:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    38 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 0505e9d3-a75c-4ac7-8d58-ae0e24ff52fd.cpp:41:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    41 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff In file included from 0505e9d3-a75c-4ac7-8d58-ae0e24ff52fd.cpp:25: 0505e9d3-a75c-4ac7-8d58-ae0e24ff52fd.cpp:42:21: error: ‘poly’ was not declared i n this scope    42 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,26 +1,22 @@
-```cpp
 #include <vector>
-#include <cmath>
-#include <algorithm>
-
-double poly(std::vector<double> coeffs, double x) {
-    double result = 0;
-    for (int i = 0; i <= coeffs.size() - 1; i++) {
-        result += coeffs[i] * pow(x, i);
-    }
-    return result;
-}
+using namespace std;
 
 double find_zero(int n) {
     double solution;
-    std::cin >> solution;
+    cin >> solution;
 
     // Initialize the vector coeffs with n+1 elements set to 0.0
-    std::vector<double> coeffs(n+1, 0.0);
+    vector<double> coeffs(n+1, 0.0);
 
     for (int i = 0; i <= n; i++) {
-        std::cin >> coeffs[i];
+        cin >> coeffs[i];
     }
 
-    return -pow(solution, 2) + poly(coeffs, solution);
+    double poly_value = 0;
+    double power = 1;
+    for (double coeff : coeffs) {
+        poly_value += coeff * pow(solution, power);
+        power++;
+    }
+    return -pow(solution, 2) + poly_value;
 }
