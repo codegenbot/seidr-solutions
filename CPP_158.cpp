3e1261310c5c55827d296d2904be34a5984735af
beep boop <githubbot@vadim.me>
2024-07-19 20:55:34 +0200

    Solution 19 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o6ixcyyd Prompt: a6fe7331-1d05-4021-b34d-d965a9b8cb3d.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string find_max(vector<string> words){       | ^~~~~~ In file included from a6fe7331-1d05-4021-b34d-d965a9b8cb3d.cpp:18: a6fe7331-1d05-4021-b34d-d965a9b8cb3d.cpp: In function ‘int main()’: a6fe7331-1d05-4021-b34d-d965a9b8cb3d.cpp:20:14: error: ‘find_max’ was not declar ed in this scope    20 |     assert ((find_max({"play", "play", "play"}) == "play"));       |              ^~~~~~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -4,27 +4,26 @@
 #include <string>
 
 std::string find_max(const std::vector<std::string>& words) {
-    string max_word = *max_element(words.begin(), words.end(),
+    string max_word = *min_element(words.begin(), words.end(),
         [](const string& a, const string& b) {
             if (a.length() == b.length()) {
-                return a > b;
+                return a < b;
             }
-            return a.length() < b.length();
+            return a.length() > b.length();
         });
+    
     for (string word : words) {
         set<char> char_set(word.begin(), word.end());
-        if (char_set.size() > (max_word).size()) {
+        if (char_set.size() > max_word.length()) {
             max_word = word;
-        } else if (char_set.size() == (max_word).size()) {
-            int a = 0, b = 0;
-            for (char c : word) {
-                if (c >= 'a' && c <= 'z') a++;
-                else b++;
-            }
-            if ((double)a / (a + b) > (double)(count(max_word.begin(), max_word.end(), (char)c('a', 'z')))/(count(max_word.begin(), max_word.end(), (char)c('A', 'Z')) + count(max_word.begin(), max_word.end(), (char)c('a', 'z'))) ) {
-                max_word = word;
-            }
+        } else if (char_set.size() == max_word.length() && word < max_word) {
+            max_word = word;
         }
     }
+    
     return max_word;
+}
+
+int main() {
+    assert(find_max({"play", "play", "play"}) == "play");
 }
