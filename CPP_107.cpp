6f000a7869c98ef77956204f538678875c371552
beep boop <githubbot@vadim.me>
2024-07-19 03:36:23 +0200

    Solution 14 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3sqmacux Prompt:       |                    ^~~ d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp:18: d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp: In function ‘int main()’: d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp:28:20: error: ‘even_odd_palindrome’ was  not declared in this scope    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ d22e3cfe-aff9-4b3d-a9d9-5f1c65f9c7a6.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -8,21 +8,21 @@ bool issame(vector<int> a, vector<int> b){
 }
 
 vector<int> even_odd_palindrome(int n){
-        vector<int> res(2, 0);
-        for (int i = 1; i <= n; ++i) {
-            string s = to_string(i);
-            string rev_s = s;
-            reverse(rev_s.begin(), rev_s.end());
-            if (s == rev_s) {
-                if (i % 2 == 0) {
-                    res[0]++;
-                } else {
-                    res[1]++;
-                }
+    vector<int> res(2, 0);
+    for (int i = 1; i <= n; ++i) {
+        string s = to_string(i);
+        string rev_s = s;
+        reverse(rev_s.begin(), rev_s.end());
+        if (s == rev_s) {
+            if (i % 2 == 0) {
+                res[0]++;
+            } else {
+                res[1]++;
             }
         }
-        return res;
     }
+    return res;
+}
 
 int main() {
     assert(issame(even_odd_palindrome(1), {0, 1}));
