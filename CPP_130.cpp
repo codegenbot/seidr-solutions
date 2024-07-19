8e901da933623af41314ccb8d5885a068db56c3c
beep boop <githubbot@vadim.me>
2024-07-19 20:30:40 +0200

    Solution 86 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tyf5i3ik Prompt:       |             ^~~~~~ 99060b9d-8f7e-4f14-aeeb-398a833d4385.cpp:13:20: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 99060b9d-8f7e-4f14-aeeb-398a833d4385.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 99060b9d-8f7e-4f14-aeeb-398a833d4385.cpp:13:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 99060b9d-8f7e-4f14-aeeb-398a833d4385.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 99060b9d-8f7e-4f14-aeeb-398a833d4385.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 99060b9d-8f7e-4f14-aeeb-398a833d4385.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -5,6 +5,6 @@ std::vector<int> tri(int n) {
     return {n, n+2};
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
