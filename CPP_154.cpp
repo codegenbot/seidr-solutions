9f5de42f8734b426b6247fcfa3c421de2329ff42
beep boop <githubbot@vadim.me>
2024-07-19 20:37:37 +0200

    Solution 37 of CPP/154, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/u7h5bvsp Prompt: 7ecb9d77-b3aa-444e-bb8f-ebb818cc7d0c.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 7ecb9d77-b3aa-444e-bb8f-ebb818cc7d0c.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -1,3 +1,11 @@
-string temp = b + b;
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool cycpattern_check(string a, string b) {
+    string temp = b + b;
     return (temp.find(a) != string::npos);
 }
+
+int main();
