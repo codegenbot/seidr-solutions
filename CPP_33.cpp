616a87a2fa4fd1f5da718827018eacdaeec45023
beep boop <githubbot@vadim.me>
2024-07-19 21:09:25 +0200

    Solution 58 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jr17mrsj Prompt:     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3d6bb1d0-e4ef-436e-89f8-9d300bd10123.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3d6bb1d0-e4ef-436e-89f8-9d300bd10123.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3d6bb1d0-e4ef-436e-89f8-9d300bd10123.cpp:6: 3d6bb1d0-e4ef-436e-89f8-9d300bd10123.cpp: In function ‘int main()’: 3d6bb1d0-e4ef-436e-89f8-9d300bd10123.cpp:16:20: error: ‘sort_third’ was not decl ared in this scope    16 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 3d6bb1d0-e4ef-436e-89f8-9d300bd10123.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,14 +1,13 @@
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b) {
+    return a % 3 == 0 ? b % 3 == 0 ? a < b : true : b % 3 == 0 ? false : a < b;
+}
 
-bool sort_third(vector<int>& l) {
-    sort(l.begin(), l.end(), [](int a, int b) {
-        return a % 3 == 0 ? b % 3 == 0 ? a < b : true : b % 3 == 0 ? false : a < b;
-    });
-    return l;
+void sort_third(vector<int>& l) {
+    sort(l.begin(), l.end(), issame);
 }
 
 int main() {
-    vector<int> l = {9, 4, 6, 12, 3};
+    vector<int> l = {3, 6, 9, 1, 4, 7};
     sort_third(l);
-    return 0;
+    return l;
 }
