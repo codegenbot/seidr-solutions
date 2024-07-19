909542b47cd4b1a9799dcf203dd3b3a197d177a1
beep boop <githubbot@vadim.me>
2024-07-19 04:48:55 +0200

    Solution 76 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/n4vldhqq Prompt:       |                    ^~~ 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp:19: 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp: In function ‘int main()’: 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp:29:20: error: ‘f’ was not declared in t his scope    29 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 6b6d703d-b202-4a22-990c-47b5f7eb4ef8.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -2,33 +2,7 @@
 #include <cassert>
 
 std::vector<int> f(int n);
+
 bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
-}
-
-int main() {
-    std::vector<int> result = f(5);
-    std::vector<int> expected = {1, 3, 2, 9, 4};
-    
-    assert(issame(result, expected));
-    
-    return 0;
-}
-
-std::vector<int> f(int n){
-    std::vector<int> result;
-    int sum = 0;
-    int fact = 1;
-    
-    for(int i = 1; i <= n; i++){
-        if(i % 2 == 0){
-            fact *= i;
-            result.push_back(fact);
-        } else {
-            sum += i;
-            result.push_back(sum);
-        }
-    }
-    
-    return result;
 }
