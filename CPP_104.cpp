47a411eb3a78b13525b3e1bf49f89417a5b597f3
beep boop <githubbot@vadim.me>
2024-07-19 04:48:35 +0200

    Solution 84 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9smdgoum Prompt:       |                    ^~~ ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp:27:27: error: ‘vector’ was not declared  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp:26: ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp: In function ‘int main()’: ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp:36:20: error: ‘unique_digits’ was not d eclared in this scope    36 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ ba261e8d-e7cb-42aa-87de-f1481a15143b.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,29 +1,16 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b);
-
-vector<int> unique_digits(vector<int> x){
-    vector<int> result;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
     
-    for (int num : x) {
-        bool hasEvenDigit = false;
-        int temp = num;
-        while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        
-        if (!hasEvenDigit) {
-            result.push_back(num);
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
     
-    sort(result.begin(), result.end());
-    
-    return result;
+    return true;
 }
