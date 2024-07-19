086087d2f7bf9e66f69420d9d768b66e0f0ba7fa
beep boop <githubbot@vadim.me>
2024-07-19 03:05:05 +0200

    Solution 3 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/a46achec Prompt:    29 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 8d26a9ed-4036-438b-8580-5a4ba673ad7e.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8d26a9ed-4036-438b-8580-5a4ba673ad7e.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 8d26a9ed-4036-438b-8580-5a4ba673ad7e.cpp:28: 8d26a9ed-4036-438b-8580-5a4ba673ad7e.cpp: In function ‘int main()’: 8d26a9ed-4036-438b-8580-5a4ba673ad7e.cpp:38:20: error: ‘sorted_list_sum’ was not  declared in this scope    38 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8d26a9ed-4036-438b-8580-5a4ba673ad7e.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,9 +1,36 @@
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b);
+
+vector<string> sorted_list_sum(vector<string> lst){
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2 != 0; }), lst.end());
+    sort(lst.begin(), lst.end(), [](const string& a, const string& b){
+        if (a.length() == b.length()) {
+            return a < b;
+        }
+        return a.length() < b.length();
+    });
+    return lst;
 }
 
-auto issame = [](vector<string> a, vector<string> b){
-    return a == b;
-};
+bool issame(vector<string> a, vector<string> b){
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
 
-assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+}
