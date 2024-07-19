8c22ceb411644d1496ca787497c5e3c7aba473df
beep boop <githubbot@vadim.me>
2024-07-19 19:14:08 +0200

    Solution 9 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4ticmyg3 Prompt:       |                  ^~~~~ 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp:3:5: error: expected unqualified-id bef ore ‘if’     3 |     if (num - intNum > 0.5) {       |     ^~ 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp:5:7: error: expected unqualified-id bef ore ‘else’     5 |     } else if (num - intNum < -0.5) {       |       ^~~~ 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp:7:7: error: expected unqualified-id bef ore ‘else’     7 |     } else {       |       ^~~~ 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp:16: 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp: In function ‘int main()’: 9efd5999-9b30-44ed-925d-981b90fc1c1f.cpp:18:13: error: ‘closest_integer’ was not  declared in this scope    18 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,5 +1,19 @@
-double num = stod(value);
-    int lower = floor(num);
-    int upper = ceil(num);
-    return (num - lower < upper - num) ? lower : upper;
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int closest_integer(std::string value) {
+    double num = std::stod(value);
+    int intNum = std::round(num);
+    if (num - intNum > 0.5) {
+        return std::ceil(num);
+    } else if (num - intNum < -0.5) {
+        return std::floor(num);
+    } else {
+        if (num >= 0) {
+            return std::ceil(num);
+        } else {
+            return std::floor(num);
+        }
+    }
 }
