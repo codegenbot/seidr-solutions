00a2348a141bcd600a823c36acf6c9bc5c188a50
beep boop <githubbot@vadim.me>
2024-07-19 03:40:32 +0200

    Solution 57 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1hc4poj8 Prompt:    11 |         for (int j = 2; j * j <= i; ++j) {       |              ^~~ 39a4d0d2-cb1f-4c9d-9912-deca73a4b8e7.cpp:11:25: error: ‘j’ was not declared in t his scope    11 |         for (int j = 2; j * j <= i; ++j) {       |                         ^ 39a4d0d2-cb1f-4c9d-9912-deca73a4b8e7.cpp: At global scope: 39a4d0d2-cb1f-4c9d-9912-deca73a4b8e7.cpp:35:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    35 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 39a4d0d2-cb1f-4c9d-9912-deca73a4b8e7.cpp:24:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    24 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 39a4d0d2-cb1f-4c9d-9912-deca73a4b8e7.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 39a4d0d2-cb1f-4c9d-9912-deca73a4b8e7.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-
-using namespace std;
-
 vector<int> count_up_to(int n){
     vector<int> result;
     for (int i = 2; i < n; ++i) {
@@ -23,10 +17,4 @@ vector<int> count_up_to(int n){
 
 bool issame(vector<int> a, vector<int> b){
     return a == b;
-}
-
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    
-    return 0;
 }
