339254e6f399f898cbb8f4425d4127afb1e8075e
beep boop <githubbot@vadim.me>
2024-07-19 20:13:02 +0200

    Solution 20 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/a5fpriio Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9bd6dbd5-5887-46e3-b26f-54ef29090aea.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 9bd6dbd5-5887-46e3-b26f-54ef29090aea.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 9bd6dbd5-5887-46e3-b26f-54ef29090aea.cpp:22: 9bd6dbd5-5887-46e3-b26f-54ef29090aea.cpp: In function ‘int main()’: 9bd6dbd5-5887-46e3-b26f-54ef29090aea.cpp:32:20: error: ‘largest_smallest_integer s’ was not declared in this scope    32 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 9bd6dbd5-5887-46e3-b26f-54ef29090aea.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,29 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b);
-
-std::vector<int> largest_smallest_integers(std::vector<int> a, std::vector<int> b);
-
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+   return a == b;
 }
 
-std::vector<int> largest_smallest_integers(std::vector<int> a, std::vector<int> b) {
-    int largest_negative = 0, smallest_positive = 0;
-    for (int num : a) {
-        if (num < 0 && num < largest_negative) {
-            largest_negative = num;
-        } else if (num > 0 && (num < smallest_positive || smallest_positive == 0)) {
-            smallest_positive = num;
-        }
-    }
-    return {largest_negative, smallest_positive};
-}
+vector<int> largest_smallest_integers(vector<int> a, vector<int> b);
 
 int main() {
-    std::vector<int> result = largest_smallest_integers({-6, -4, -4, -3, -100, 1}, {});
-    assert(issame(result, {-3, 1}));
+    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
     return 0;
 }
