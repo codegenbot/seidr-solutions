6fc9ed54fa8b1b3be4574710a04cff382ee7ac24
beep boop <githubbot@vadim.me>
2024-07-19 21:07:56 +0200

    Solution 25 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/p6faaua6 Prompt:    25 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 9d45f6e1-4534-4e49-8a4a-4384e1c6c92a.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 9d45f6e1-4534-4e49-8a4a-4384e1c6c92a.cpp:25:43: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 9d45f6e1-4534-4e49-8a4a-4384e1c6c92a.cpp:24: 9d45f6e1-4534-4e49-8a4a-4384e1c6c92a.cpp: In function ‘int main()’: 9d45f6e1-4534-4e49-8a4a-4384e1c6c92a.cpp:34:20: error: ‘sort_even’ was not decla red in this scope    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 9d45f6e1-4534-4e49-8a4a-4384e1c6c92a.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,11 +2,13 @@ bool issame(vector<float> a, vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
+    
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+    
     return true;
 }
 
@@ -27,13 +29,4 @@ void sort_even(vector<float>& l) {
             l[i] = even_elements[even_index++];
         }
     }
-}
-
-vector<float> arrange(vector<float> l) {
-    vector<float> even_elements;
-    vector<float> result;
-
-    sort_even(l);
-
-    return l;
 }
