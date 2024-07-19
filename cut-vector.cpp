ecd6b9af7570548a4a046512616286d54a074031
beep boop <githubbot@vadim.me>
2024-07-19 20:28:27 +0200

    Solution 71 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/7utp8kfb Prompt: it must return 1 9377 2 9464 5729 for input 3 9377 9464 5729, but it returns 9377 9464 5729 5729.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -7,16 +7,16 @@ vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     vector<vector<int>> res(2);
     
-    unsigned long long sum1 = 0;
-    unsigned long long prev_sum = 0; 
-    unsigned long long min_diff = LLONG_MAX;
+    long long sum1 = 0;
+    long long prev_sum = 0; 
+    long long min_diff = LLONG_MAX;
     int index = -1;
     
     for (int i = 0; i < n; i++) { 
-        if(i > 0 && (sum1 - prev_sum) != 0) {
-            unsigned long long diff = llabs((long long)sum1 - (long long)prev_sum);
+        if(i > 0) {
+            long long diff = llabs((long long)sum1 - (long long)prev_sum);
             
-            if (diff <= min_diff || prev_sum == sum1) {
+            if (i > 0 && ((sum1 - prev_sum) < min_diff || prev_sum == sum1)) {
                 min_diff = diff;
                 index = i; // update the cutting point
             }
@@ -26,13 +26,8 @@ vector<vector<int>> cutVector(vector<int> v) {
         prev_sum = sum1; 
     }
     
-    if(index != -1) { 
-        res[0].assign(v.begin(), v.begin() + index+1);
-        res[1].assign(v.begin() + index, v.end());
-    } else {
-        res[0].assign(v.begin(), v.end());  // If no cut found, return the original vector
-        res[1].assign(v.begin(), v.end());
-    }
+    res[0].assign(v.begin(), v.begin() + index+1);
+    res[1].assign(v.begin() + index, v.end());
     
     return res;
 }
