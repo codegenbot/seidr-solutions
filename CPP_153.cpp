72aaeda7d35e74b0d278bd10175485ce559dafac
beep boop <githubbot@vadim.me>
2024-07-19 12:30:01 +0200

    Solution 16 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/balmv2m8 Prompt: 9f155d64-50da-4fe0-b287-3dbf59f12124.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 9f155d64-50da-4fe0-b287-3dbf59f12124.cpp:24: 9f155d64-50da-4fe0-b287-3dbf59f12124.cpp: In function ‘int main()’: 9f155d64-50da-4fe0-b287-3dbf59f12124.cpp:26:13: error: ‘Strongest_Extension’ was  not declared in this scope    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,23 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <cctype>
+
+using namespace std;
+
 string Strongest_Extension(string class_name, vector<string> extensions) {
-    int max_strength = INT_MIN;
     string strongest_extension;
-    
-    for (const auto& extension : extensions) {
+    int max_strength = INT_MIN;
+
+    for (const string& ext : extensions) {
         int cap_count = 0, sm_count = 0;
-        for (const char& c : extension) {
+        for (char c : ext) {
             if (isupper(c)) {
                 cap_count++;
             } else if (islower(c)) {
                 sm_count++;
             }
         }
-        
         int strength = cap_count - sm_count;
-        if (strength > max_strength || (strength == max_strength && extension < strongest_extension)) {
+        if (strength > max_strength || (strength == max_strength && ext < strongest_extension)) {
             max_strength = strength;
-            strongest_extension = extension;
+            strongest_extension = ext;
         }
     }
-    
+
     return class_name + "." + strongest_extension;
+}
+
+int main() {
+    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+
+    return 0;
 }
