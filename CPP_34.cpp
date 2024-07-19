08c85d21dfffaaccb9dc518cd1d18a5ddb641a8b
beep boop <githubbot@vadim.me>
2024-07-19 18:57:03 +0200

    Solution 27 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xf1ve6zq Prompt:    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8995dcf2-c95c-4045-ba5e-624b0b39b622.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8995dcf2-c95c-4045-ba5e-624b0b39b622.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8995dcf2-c95c-4045-ba5e-624b0b39b622.cpp:11: 8995dcf2-c95c-4045-ba5e-624b0b39b622.cpp: In function ‘int main()’: 8995dcf2-c95c-4045-ba5e-624b0b39b622.cpp:21:20: error: ‘unique’ was not declared  in this scope    21 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 8995dcf2-c95c-4045-ba5e-624b0b39b622.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -17,7 +17,6 @@ vector<int> unique(vector<int> l){
 }
 
 int main() {
-    vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    assert(issame(unique(l) , unique({0, 2, 3, 5, 9, 123})));
+    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));
     return 0;
 }
