4988dffbb93dc887db202229ba78d214f066d23e
beep boop <githubbot@vadim.me>
2024-07-19 03:28:09 +0200

    Solution 9 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/g6gz22nc Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,35 +1,23 @@
-map<string, int> number_map = {
-    {"zero", 0},
-    {"one", 1},
-    {"two", 2},
-    {"three", 3},
-    {"four", 4},
-    {"five", 5},
-    {"six", 6},
-    {"seven", 7},
-    {"eight", 8},
-    {"nine", 9}
-};
+map<string, int> num_map = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}};
+map<int, string> rev_num_map = {{0, "zero"}, {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}};
 
-string sort_numbers(string numbers) {
-    map<int, string> sorted_numbers;
-    string result = "";
-    
-    string current_number = "";
-    for (char c : numbers) {
-        if (c == ' ') {
-            sorted_numbers[number_map[current_number]] = current_number;
-            current_number = "";
-        } else {
-            current_number += c;
-        }
-    }
-    sorted_numbers[number_map[current_number]] = current_number;
-    
-    for (const auto& pair : sorted_numbers) {
-        result += pair.second + " ";
-    }
-    
-    result.pop_back(); // Remove extra space at the end
-    return result;
+vector<int> nums;
+string result;
+
+size_t pos = 0, found;
+while ((found = numbers.find(" ", pos)) != string::npos) {
+    string num_str = numbers.substr(pos, found - pos);
+    nums.push_back(num_map[num_str]);
+    pos = found + 1;
+}
+nums.push_back(num_map[numbers.substr(pos)]);
+
+sort(nums.begin(), nums.end());
+
+for (int num : nums) {
+    result += rev_num_map[num] + " ";
+}
+
+result.pop_back(); // Remove the extra space at the end
+return result;
 }
