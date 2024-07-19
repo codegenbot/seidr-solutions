13362b44775985c85eaece1c6046c645a1075904
beep boop <githubbot@vadim.me>
2024-07-19 21:05:25 +0200

    Solution 76 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tam7yvui Prompt: 6c641f58-86b2-4086-9b06-64f0c2d84377.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    37 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 6c641f58-86b2-4086-9b06-64f0c2d84377.cpp:26:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    26 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 6c641f58-86b2-4086-9b06-64f0c2d84377.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 6c641f58-86b2-4086-9b06-64f0c2d84377.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main(){       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -6,10 +6,6 @@ using namespace std;
 
 vector<int> parse_music(string music_string);
 
-bool issame(const vector<int>& a, const vector<int>& b){
-    return a == b;
-}
-
 vector<int> parse_music(string music_string){
     vector<int> beats;
     int length = music_string.length();
@@ -28,7 +24,7 @@ vector<int> parse_music(string music_string){
 }
 
 int main(){
-    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
+    assert(parse_music("o| .| o| .| o o| o o|") == (vector<int>{2, 1, 2, 1, 4, 2, 4, 2}));
     
     return 0;
-}
+}  
