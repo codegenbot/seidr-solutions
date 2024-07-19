eaf986289eade4ae989bea61b0eea1b1f6abfab5
beep boop <githubbot@vadim.me>
2024-07-20 01:50:14 +0200

    Solution 1 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/u3n1yohp Prompt:       |                              ^~~~~~ d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp:9: d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp: In function ‘int main()’: d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ d6f5d7eb-6bde-487b-a4ba-e82ce7eae274.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 vector<string> all_prefixes(string str){
     vector<string> result;
     for (int i = 1; i <= str.size(); ++i) {
         result.push_back(str.substr(0, i));
     }
     return result;
+}
+
+bool are_same(const vector<string>& a, const vector<string>& b){
+    return a == b;
+}
+
+int main(){
+    assert(are_same(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    
+    return 0;
 }
