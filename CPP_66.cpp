0c200249278cd5715f8c301a3c18076afb17ab74
beep boop <githubbot@vadim.me>
2024-07-19 22:19:23 +0200

    Solution 32 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4xtlx02n Prompt:    15 |     for (char c : s) {       |                   ^       |                   std::end In file included from /usr/include/c++/11/string:54,                  from 92b54f6c-8329-4366-9517-64c0944cdc98.cpp:1: /usr/include/c++/11/bits/range_access.h:110:37: note: ‘std::end’ declared here   110 |   template<typename _Tp> const _Tp* end(const valarray<_Tp>&) noexcept;       |                                     ^~~ 92b54f6c-8329-4366-9517-64c0944cdc98.cpp: At global scope: 92b54f6c-8329-4366-9517-64c0944cdc98.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 92b54f6c-8329-4366-9517-64c0944cdc98.cpp:3:5: note: ‘int main()’ previously defi ned here     3 | int main() {       |     ^~~~ In file included from 92b54f6c-8329-4366-9517-64c0944cdc98.cpp:23: 92b54f6c-8329-4366-9517-64c0944cdc98.cpp: In function ‘int main()’: 92b54f6c-8329-4366-9517-64c0944cdc98.cpp:25:22: warning: ISO C++ forbids convert ing a string constant to ‘char*’ [-Wwrite-strings]    25 |     assert (digitSum("You arE Very Smart") == 327);       |                      ^~~~~~~~~~~~~~~~~~~~

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -1,25 +1,20 @@
-#include <iostream>
 #include <string>
 
-using namespace std;
+int main() {
+    char s[100];
+    int num;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, s);
+    num = digitSum(s);
+    std::cout << "Sum of digits: " << num << std::endl;
+    return 0;
 
-int digitSum(const string& s) {
+int digitSum(char* s) {
     int sum = 0;
-    for (char c : s) {
-        if (isdigit(c)) {
+    for (char c = *s; c != '\0'; ++c) {
+        if (std::isdigit(c)) {
             sum += (c - '0');
         }
     }
     return sum;
-}
-
-int main() {
-    char* s = new char[100];
-    int num;
-    cout << "Enter a string: ";
-    cin.getline(s, 99); 
-    num = digitSum(string(s));
-    cout << "Sum of digits: " << num << endl;
-    delete[] s; 
-    return 0;
 }
