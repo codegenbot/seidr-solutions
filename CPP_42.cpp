292e06aec2cd144e9b243c8de14246b23c4d17e3
beep boop <githubbot@vadim.me>
2024-07-19 14:46:23 +0200

    Solution 20 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9xs8yeft Prompt: ined here    15 | int main() {       |     ^~~~ In file included from 4749bfc8-ef04-46d2-92c6-a7fd58209078.cpp:22: 4749bfc8-ef04-46d2-92c6-a7fd58209078.cpp: In function ‘int main()’: 4749bfc8-ef04-46d2-92c6-a7fd58209078.cpp:32:29: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    32 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 4749bfc8-ef04-46d2-92c6-a7fd58209078.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 4749bfc8-ef04-46d2-92c6-a7fd58209078.cpp:5:34: note:   initializing argument 1 o f ‘void incr_list(std::vector<int>&)’     5 | void incr_list(std::vector<int>& l) {       |                ~~~~~~~~~~~~~~~~~~^

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <cassert>
 
-void incr_list(std::vector<int>&& l) {
-    for (int& num : l) {
-        num++;
+void incr_list(std::vector<int>& l) {
+    for (int i = 0; i < l.size(); i++) {
+        l[i]++;
     }
 }
 
@@ -13,8 +13,8 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 int main() {
-    std::vector<int> input = {5, 2, 5, 2, 3, 3, 9, 0, 123};
-    incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123});
-    assert(issame(input, {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    std::vector<int> input2 = {5, 2, 5, 2, 3, 3, 9, 0, 123};
+    incr_list(input2);
+    assert(issame(input2, {6, 3, 6, 3, 4, 4, 10, 1, 124}));
     return 0;
 }
