12c94832c8bce65057303841aa99d7240c6e9f52
beep boop <githubbot@vadim.me>
2024-07-19 20:41:19 +0200

    Solution 64 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/736r7wtz Prompt: cb41536f-bdf0-4f10-ab9f-dd9673e59f98.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ cb41536f-bdf0-4f10-ab9f-dd9673e59f98.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -19,7 +19,7 @@ std::string string_to_md5(const std::string& text) {
 
     char mdString[33];
     for (unsigned int i = 0; i < md_len; i++) {
-        snprintf(&mdString[i*2], 3, "%02x", (unsigned int)md_value[i]);
+        snprintf(&mdString[i * 2], 3, "%02x", (unsigned int)md_value[i]);
     }
 
     return std::string(mdString);
