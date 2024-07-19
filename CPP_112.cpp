ac1e66b424841958333cd47909d358791be03360
beep boop <githubbot@vadim.me>
2024-07-19 21:56:46 +0200

    Solution 12 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sruslc5g Prompt:       |                              ^~~~~~ f7a8d07a-060b-45a5-89d1-81934914f335.cpp:13:37: error: ‘string’ was not declared  in this scope    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f7a8d07a-060b-45a5-89d1-81934914f335.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f7a8d07a-060b-45a5-89d1-81934914f335.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f7a8d07a-060b-45a5-89d1-81934914f335.cpp:12: f7a8d07a-060b-45a5-89d1-81934914f335.cpp: In function ‘int main()’: f7a8d07a-060b-45a5-89d1-81934914f335.cpp:22:20: error: ‘reverse_delete’ was not declared in this scope    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ f7a8d07a-060b-45a5-89d1-81934914f335.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,28 +1,15 @@
 #include <string>
+#include <vector>
+#include <algorithm>
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-vector<string> reverse_delete(string s, string c) {
+bool issame(const vector<string>& a, const vector<string>& b) {
     string result = "";
     for (char ch : s) {
         if (c.find(ch) == string::npos) {
             result += ch;
         }
     }
-    string temp = result;
-    reverse(temp.begin(), temp.end());
-    vector<string> ans;
-    ans.push_back(result);
-    ans.push_back(issame(result, temp) ? "True" : "False");
-    return ans;
-}
-
-int main() {
-    vector<string> result = reverse_delete("programming", "g");
-    for (string str : result) {
-        cout << str << " ";
-    }
-    return 0;
+    string reversed = result;
+    reverse(reversed.begin(), reversed.end());
+    return {result, (result == reversed) ? "True" : "False"};
 }
