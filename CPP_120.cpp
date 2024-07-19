1905d1096fe0b97534bc0a29f9d3f9dfe4a5de8b
beep boop <githubbot@vadim.me>
2024-07-19 02:46:39 +0200

    Solution 3 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/86jrbnp6 Prompt:                  from 9529ce0c-f80a-4eb2-aff2-377e9e1dc8fb.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 9529ce0c-f80a-4eb2-aff2-377e9e1dc8fb.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9529ce0c-f80a-4eb2-aff2-377e9e1dc8fb.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9529ce0c-f80a-4eb2-aff2-377e9e1dc8fb.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 9529ce0c-f80a-4eb2-aff2-377e9e1dc8fb.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,11 +1,19 @@
 #include <algorithm>
+#include <iostream>
 #include <vector>
+#include <cassert>
 
-vector<int> maximum(vector<int> arr, int k) {
-    sort(arr.begin(), arr.end(), greater<int>());
-    return vector<int>(arr.begin(), arr.begin() + k);
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
+    return a == b;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::sort(arr.begin(), arr.end(), std::greater<int>());
+    return std::vector<int>(arr.begin(), arr.begin() + k);
+}
+
+int main() {
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), std::vector<int>{}));
+
+    return 0;
 }
