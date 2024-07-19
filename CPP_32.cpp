dea49f8753dd0d9257724fedaed2b74e806d00e4
beep boop <githubbot@vadim.me>
2024-07-19 21:35:28 +0200

    Solution 5 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d91o4gsr Prompt: 8f97fa73-8232-4acf-8fcb-1e743f0f4a74.cpp:20:16: error: expected primary-expressi on before ‘double’    20 |         vector<double> coeffs = {};       |                ^~~~~~ 8f97fa73-8232-4acf-8fcb-1e743f0f4a74.cpp:25:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    25 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 8f97fa73-8232-4acf-8fcb-1e743f0f4a74.cpp:28:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    28 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 8f97fa73-8232-4acf-8fcb-1e743f0f4a74.cpp:28:36: error: ‘find_zero’ cannot be use d as a function    28 |         solution = find_zero(coeffs);       |                                    ^ In file included from 8f97fa73-8232-4acf-8fcb-1e743f0f4a74.cpp:12: 8f97fa73-8232-4acf-8fcb-1e743f0f4a74.cpp:29:21: error: ‘poly’ was not declared i n this scope    29 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,7 +1,14 @@
-double find_zero(vector<double> coeffs) {
+double find_zero(int n) {
+    int degree;
+    vector<double> coeffs(n+1);
+    vector<int> poly(n);
+
+    double solution; // input from user
+    cin >> solution;
+
     double sum = 0;
-    for (int i = 2; i < coeffs.size(); i += 2) {
-        sum -= coeffs[i/2] * pow(1, i) / (pow(1, i+1));
+    for (int i = 2; i < n+1; i += 2) {
+        sum -= coeffs[i/2] * pow(solution, i) / (pow(solution, i+1));
     }
-    return -sum;
+    return -sum / pow(solution, 2);
 }
