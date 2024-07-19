b88dd1a9e33c859f8f15cab62536cfebe1ec96a4
beep boop <githubbot@vadim.me>
2024-07-19 22:15:41 +0200

    Solution 23 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8hrrqsp9 Prompt:    17 | } else if(a.type() == typeid(string) && b.type() == typeid(string)){       |   ^~~~ 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:29:3: error: expected unqualified-id be fore ‘else’    29 | } else {       |   ^~~~ In file included from 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:33: 4b349af5-749a-4950-9ec8-26e96296ac85.cpp: In function ‘int main()’: 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:35:13: error: ‘boost’ has not been decl ared    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:35:29: error: ‘string’ was not declared  in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 4b349af5-749a-4950-9ec8-26e96296ac85.cpp:35:37: error: ‘compare_one’ was not dec lared in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,36 +3,37 @@
 #include <algorithm>
 
 template <typename T>
-T compare_one(T a, T b) {
-    if constexpr(std::is_same_v<T, int>) {
-        if (a > b) {
+T compare_one(T a, T b){
+    if(typeid(a) == typeid(int)){
+        if(std::any_cast<int>(a) > std::any_cast<int>(b)){
             return a;
-        } else if (a < b) {
+        } else if(std::any_cast<int>(a) < std::any_cast<int>(b)){
             return b;
         } else {
-            return "None";
+            return T();
         }
-    } else if constexpr(std::is_same_v<T, float>) {
-        if (a > b) {
+    } else if(typeid(a) == typeid(float)){
+        if(std::any_cast<float>(a) > std::any_cast<float>(b)){
             return a;
-        } else if (a < b) {
+        } else if(std::any_cast<float>(a) < std::any_cast<float>(b)){
             return b;
         } else {
-            return "None";
+            return T();
         }
-    } else if constexpr(std::is_same_v<T, std::string>) {
-        std::replace(a.begin(), a.end(), ',', '.');
-        std::replace(b.begin(), b.end(), ',', '.');
-
-        if (std::stof(a) > std::stof(b)) {
+    } else if(typeid(a) == typeid(std::string)){
+        std::string strA = std::any_cast<std::string>(a);
+        std::string strB = std::any_cast<std::string>(b);
+        std::replace(strA.begin(), strA.end(), ',', '.');
+        std::replace(strB.begin(), strB.end(), ',', '.');
+        if(std::stof(strA) > std::stof(strB)){
             return a;
-        } else if (std::stof(a) < std::stof(b)) {
+        } else if(std::stof(strA) < std::stof(strB)){
             return b;
         } else {
-            return "None";
+            return T();
         }
     } else {
-        return "None";
+        return T();
     }
 }
 
