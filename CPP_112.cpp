d03dfd25319298f689ef5d545a79b47e45aa6664
beep boop <githubbot@vadim.me>
2024-07-19 18:15:21 +0200

    Solution 10 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/fhctker5 Prompt:       |                              ^~~~~~ 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp:37: 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp: In function ‘int main()’: 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp:47:20: error: ‘reverse_delete’ was not declared in this scope    47 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 83e3fe3e-387b-49f6-b7bc-8e63a2f002a5.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,32 +1,35 @@
-#include <vector>
-#include <string>
-
+```
 bool issame(char ch1, char ch2) {
-    if (ch1 == tolower(ch1) && ch2 == tolower(ch2)) {
-        return true;
-    }
-    return false;
+    if(ch1 >= 'a' && ch1 <= 'z') 
+        ch1 = (ch1 - 'a' + 26)%26 + 'a';
+    else if(ch1 >= 'A' && ch1 <= 'Z')
+        ch1 = (ch1 - 'A' + 26)%26 + 'A';
+    if(ch2 >= 'a' && ch2 <= 'z') 
+        ch2 = (ch2 - 'a' + 26)%26 + 'a';
+    else if(ch2 >= 'A' && ch2 <= 'Z')
+        ch2 = (ch2 - 'A' + 26)%26 + 'A';
+    return ch1 == ch2;
 }
 
-std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::vector<std::string> result;
+vector<string> reverse_delete(string s, string c) {
+    vector<string> result;
     for (char ch : s) {
         bool found = false;
         for (char cc : c) {
-            if (issame(ch, cc)) {
+            if (ch == cc) {
                 found = true;
                 break;
             }
         }
         if (!found) {
-            result.push_back(std::to_string(ch));
+            result.push_back(string(1, ch));
         }
     }
-    std::string temp = "";
+    string temp = "";
     for (int i = 0; i < result.size(); i++) {
         temp += result[i];
     }
-    if (temp == reverse(temp)) {
+    if (issame(temp[0],temp[temp.size()-1])) {
         result.push_back("True");
     } else {
         result.push_back("False");
@@ -34,8 +37,8 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
     return result;
 }
 
-std::string reverse(std::string s) {
-    std::string rev = "";
+string reverse(string s) {
+    string rev = "";
     for (int i = s.size() - 1; i >= 0; i--) {
         rev += s[i];
     }
