c839e7d4538f487c10b146e4810cfe8502702926
beep boop <githubbot@vadim.me>
2024-07-20 00:35:26 +0200

    Solution 45 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/72ots2es Prompt:       |                                   ^~~~~~ ccc7253e-957f-4696-9c79-962a086469a1.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ ccc7253e-957f-4696-9c79-962a086469a1.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ ccc7253e-957f-4696-9c79-962a086469a1.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from ccc7253e-957f-4696-9c79-962a086469a1.cpp:19: ccc7253e-957f-4696-9c79-962a086469a1.cpp: In function ‘int main()’: ccc7253e-957f-4696-9c79-962a086469a1.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ ccc7253e-957f-4696-9c79-962a086469a1.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -4,13 +4,8 @@ bool issame(vector<vector<int>> a, vector<vector<int>> b) {
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x);
 
-int main() {
-    // Add code to call get_row function here
-    return 0;
-}
-
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
-    vector<vector<int>> result;
+    vector<vector<vector<int>>> result;
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
             if (lst[i][j] == x) {
