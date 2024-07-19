42c2b6cb2aea657c2237699dc700257bcf43c8a3
beep boop <githubbot@vadim.me>
2024-07-19 20:51:44 +0200

    Solution 17 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tam7yvui Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 304c3b5b-5982-4b1e-bb1d-3d5b887c0e21.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 304c3b5b-5982-4b1e-bb1d-3d5b887c0e21.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 304c3b5b-5982-4b1e-bb1d-3d5b887c0e21.cpp:20: 304c3b5b-5982-4b1e-bb1d-3d5b887c0e21.cpp: In function ‘int main()’: 304c3b5b-5982-4b1e-bb1d-3d5b887c0e21.cpp:30:20: error: ‘parse_music’ was not dec lared in this scope    30 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 304c3b5b-5982-4b1e-bb1d-3d5b887c0e21.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -3,6 +3,18 @@
 
 bool issame(vector<int> a, vector<int> b);
 
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 vector<int> parse_music(string music_string){
     vector<int> beats;
     int duration = 0;
@@ -22,21 +34,7 @@ vector<int> parse_music(string music_string){
     return beats;
 }
 
-bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    vector<int> a = parse_music("o.|..||");
-    vector<int> b = {4, 1, 2, 0, 2};
-    assert(issame(a, b));
-    return 0;
+    vector<int> result = parse_music("o..|..|o");
+    assert(issame(result, {4, 1, 2, 0, 1, 2, 4}));
 }
