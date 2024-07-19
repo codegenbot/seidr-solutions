ff5bcd758348aa1002cd3999a82a0575346f3df8
beep boop <githubbot@vadim.me>
2024-07-20 00:57:33 +0200

    Solution 26 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7gdaekmr Prompt:       |           ^~~~~~ 5de6185e-23ab-4782-8eae-4eec321950c7.cpp:17:59: error: ‘extensions’ was not decl ared in this scope    17 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                           ^~~~~~~~~~ 5de6185e-23ab-4782-8eae-4eec321950c7.cpp:17:69: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                                     ^ 5de6185e-23ab-4782-8eae-4eec321950c7.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 5de6185e-23ab-4782-8eae-4eec321950c7.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main(){       |     ^~~~ In file included from 5de6185e-23ab-4782-8eae-4eec321950c7.cpp:37: 5de6185e-23ab-4782-8eae-4eec321950c7.cpp: In function ‘int main()’: 5de6185e-23ab-4782-8eae-4eec321950c7.cpp:39:32: error: ‘Strongest_Extension’ can not be used as a function    39 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,9 +1,19 @@
-```cpp
 #include <vector>
 #include <cctype>
 #include <iostream>
 #include <cassert>
 #include <string>
+
+int Strongest_Extension(string class_name, vector<string> extensions);
+
+int main(){
+    string class_name = "My";
+    vector<string> extensions = {"Java", "Python", "C++"};
+    cout << Strongest_Extension(class_name, extensions) << endl;
+    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
+
+}
 
 int Strongest_Extension(string class_name, vector<string> extensions){
     int max_strength = 0;
@@ -23,13 +33,4 @@ int Strongest_Extension(string class_name, vector<string> extensions){
     }
 
     return class_name + "." + strongest_extension;
-}
-
-int main(){
-    string class_name = "My";
-    vector<string> extensions = {"Java", "Python", "C++"};
-    cout << Strongest_Extension(class_name, extensions) << endl;
-    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    return 0;
-
 }
