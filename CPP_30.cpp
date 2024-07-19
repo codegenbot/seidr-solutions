79004e03086f23e3b67ac4f20f77bd95ae0cc38e
beep boop <githubbot@vadim.me>
2024-07-19 22:26:20 +0200

    Solution 75 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eoo9avp7 Prompt: 42d612d9-85cd-4ecc-bafd-21e1cde6cbcd.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    37 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 42d612d9-85cd-4ecc-bafd-21e1cde6cbcd.cpp:8:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     8 | bool issame(vector<float> v1, vector<float> v2) {       |      ^~~~~~ In file included from 42d612d9-85cd-4ecc-bafd-21e1cde6cbcd.cpp:36: 42d612d9-85cd-4ecc-bafd-21e1cde6cbcd.cpp: In function ‘int main()’: 42d612d9-85cd-4ecc-bafd-21e1cde6cbcd.cpp:46:20: error: ‘get_positive’ was not de clared in this scope    46 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -9,6 +9,16 @@ bool issame(vector<float> v1, vector<float> v2) {
     return v1 == v2;
 }
 
+vector<float> get_positive(vector<float> v) {
+    vector<float> pos_v;
+    for (float num : v) {
+        if (num >= 0) {
+            pos_v.push_back(num);
+        }
+    }
+    return pos_v;
+}
+
 int main() {
     vector<float> a;
     float num;
@@ -21,14 +31,14 @@ int main() {
         a.push_back(num);
     }
 
-    vector<float> b(a);
+    vector<float> b = get_positive(a);
     reverse(b.begin(), b.end());
 
-    if (a == b) {
+    if (issame(a, b)) {
         cout << "The vectors are same." << endl;
     } else {
         cout << "The vectors are not same." << endl;
     }
 
-    return 0;
+    return 0; 
 }
