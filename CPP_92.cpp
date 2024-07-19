5cea94bcdd3cb2fa90034c8a43602103b642521c
beep boop <githubbot@vadim.me>
2024-07-19 23:10:40 +0200

    Solution 40 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 5e1c311e-7339-493b-885d-cf96d30863cf.cpp:16:65:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 bool any_int(float a, float b, float c) {
-    return (a == (int)a) && (b == (int)b) && (c == (int)c);
+    const double epsilon = 1e-6;
+    return (abs(a - (int)a) < epsilon) && (abs(b - (int)b) < epsilon) && (abs(c - (int)c) < epsilon);
 }
 
 int main() {
@@ -13,7 +14,7 @@ int main() {
     cout << "Enter three numbers: ";
     cin >> a >> b >> c;
     if(any_int(a, b, c))
-        cout << "The numbers are integers: " << int(a) << ", " << int(b) << ", " << int(c) << "\n";
+        cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
     else
         cout << "The numbers are not integers: " << a << ", " << b << ", " << c << "\n";
     return 0;
