dab118218f1f0ae6c366b1611586ea74583f9cc1
beep boop <githubbot@vadim.me>
2024-07-19 19:46:58 +0200

    Solution 33 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b7uig4ct Prompt: 2c94a371-9126-4d1c-bd6f-9fdce1c7ce50.cpp:1:9: error: ‘vector’ was not declared i n this scope     1 | int add(vector<int> lst){       |         ^~~~~~ 2c94a371-9126-4d1c-bd6f-9fdce1c7ce50.cpp:1:16: error: expected primary-expressio n before ‘int’     1 | int add(vector<int> lst){       |                ^~~ In file included from 2c94a371-9126-4d1c-bd6f-9fdce1c7ce50.cpp:10: 2c94a371-9126-4d1c-bd6f-9fdce1c7ce50.cpp: In function ‘int main()’: 2c94a371-9126-4d1c-bd6f-9fdce1c7ce50.cpp:12:16: error: ‘add’ cannot be used as a  function    12 |     assert (add({4, 4, 6, 8}) == 12);       |             ~~~^~~~~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -2,12 +2,11 @@
 
 int add(vector<int> lst){
     if(lst.size() == 0) {
-        cout << "Error: The input vector is empty." << endl;
+        cout << "Error: Input list is empty." << endl;
         return -1;
     }
     int sum = 0;
     for(int i = 1; i < lst.size(); i += 2){
-        if(i >= lst.size()) break;
         if(lst[i] % 2 == 0)
             sum += lst[i];
     }
