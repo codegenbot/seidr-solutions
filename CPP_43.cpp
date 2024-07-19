8654f550104ed2ebc59f0ec1b14480d31f958df6
beep boop <githubbot@vadim.me>
2024-07-19 18:14:06 +0200

    Solution 4 of CPP/43, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vx3vu6gp Prompt: 6a774564-87e2-4519-80d3-8cf7fce23be2.cpp: In function ‘bool pairs_sum_to_zero(st d::vector<int>)’: 6a774564-87e2-4519-80d3-8cf7fce23be2.cpp:7:5: error: ‘set’ was not declared in t his scope     7 |     set<int> s(l.begin(), l.end());       |     ^~~ 6a774564-87e2-4519-80d3-8cf7fce23be2.cpp:3:1: note: ‘std::set’ is defined in hea der ‘<set>’; did you forget to ‘#include <set>’?     2 | #include <algorithm>   +++ |+#include <set>     3 | 6a774564-87e2-4519-80d3-8cf7fce23be2.cpp:7:9: error: expected primary-expression  before ‘int’     7 |     set<int> s(l.begin(), l.end());       |         ^~~ 6a774564-87e2-4519-80d3-8cf7fce23be2.cpp:10:13: error: ‘s’ was not declared in t his scope    10 |         if (s.find(complement) != s.end() && l[i] != complement) {       |             ^

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,15 +1,17 @@
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
 bool pairs_sum_to_zero(vector<int> l) {
-    set<int> s(l.begin(), l.end());
+    set<int> s;
     for (int i = 0; i < l.size(); i++) {
         int complement = -l[i];
         if (s.find(complement) != s.end() && l[i] != complement) {
             return true;
         }
+        s.insert(l[i]);
     }
     return false;
 }
