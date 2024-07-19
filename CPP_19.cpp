4748654acf29051c7d4f04e7f523b02deb9f169d
beep boop <githubbot@vadim.me>
2024-07-20 01:32:42 +0200

    Solution 2 of CPP/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fhn69njt Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,35 +1,23 @@
 #include <algorithm>
+#include <sstream>
+
 using namespace std;
 
 string sort_numbers(string numbers) {
-    map<string, int> numberMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                   {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                   {"eight", 8}, {"nine", 9}};
-
-    vector<string> numVector;
-    string temp;
-
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
-            continue;
-        } else {
-            temp += numbers[i];
-        }
-
-        if (i == numbers.length() - 1 || numbers[i + 1] != ' ') {
-            numVector.push_back(temp);
-            temp = "";
-        }
+    istringstream iss(numbers);
+    string num;
+    vector<string> nums;
+    
+    while (iss >> num) {
+        nums.push_back(num);
     }
-
-    sort(numVector.begin(), numVector.end(), [&numberMap](const string &a, const string &b) {
-        return numberMap.at(a) < numberMap.at(b);
-    });
-
-    string result;
-    for (const auto &num : numVector) {
-        result += num + " ";
+    
+    sort(nums.begin(), nums.end());
+    
+    stringstream ss;
+    for (const auto& n : nums) {
+        ss << n << " ";
     }
-
-    return result.substr(0, result.length() - 1);
+    
+    return ss.str();
 }
