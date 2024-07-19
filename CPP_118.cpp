1fe0f3075da8851cb970dc793a2f99f3bfd5694e
beep boop <githubbot@vadim.me>
2024-07-19 20:12:29 +0200

    Solution 0 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1sjex13i Prompt: Complete the following code given the task description and function signature.

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,21 +1,7 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-using namespace std;
-
-string get_closest_vowel(string word){
-    int left = 0;
-    while(word[left] != ' ' && !isalpha(word[left])) left++;
-    
-    for(int right = word.size() - 1; right >= 0 && !isalpha(word[right]); right--){
-        if(!isalpha(word[right-1]) && (word[right] == 'a' || word[right] == 'e' || 
-                                       word[right] == 'i' || word[right] == 'o' || 
-                                       word[right] == 'u'|| word[right] == 'A' || 
-                                       word[right] == 'E' || word[right] == 'I' || 
-                                       word[right] == 'O' || word[right] == 'U')) {
-            return string(1, word[right]);
-        }
+string get_closest_vowel(string word) {
+    for (int i = word.length() - 2; i > 0; --i) {
+        if (!isalpha(word[i])) continue;
+        if (!isalpha(word[i-1]) && !isalpha(word[i-2])) return tolower(word[i]);
     }
-    
     return "";
 }
