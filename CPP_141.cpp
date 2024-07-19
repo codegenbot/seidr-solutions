f1be0d9a0cb29597096eb2837c5f07074354dd7d
beep boop <githubbot@vadim.me>
2024-07-20 01:09:11 +0200

    Solution 13 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ewvvcvho Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,30 +1,18 @@
-int digits = 0;
-    int dotIndex = -1;
-    
-    for (int i = 0; i < file_name.size(); ++i) {
-        if (file_name[i] >= '0' && file_name[i] <= '9') {
-            digits++;
-            if (digits > 3) {
-                return "No";
-            }
-        } else if (file_name[i] == '.') {
-            if (dotIndex != -1) {
-                return "No";
-            }
-            dotIndex = i;
-        } else if (i == 0 && !(file_name[i] >= 'a' && file_name[i] <= 'z') && !(file_name[i] >= 'A' && file_name[i] <= 'Z')) {
-            return "No";
+int digits_count = 0;
+    for (char c : file_name) {
+        if (isdigit(c)) {
+            digits_count++;
         }
     }
     
-    if (digits > 3 || dotIndex == -1 || dotIndex == 0 || dotIndex == file_name.size() - 1) {
-        return "No";
+    size_t dot_pos = file_name.find('.');
+    if (digits_count <= 3 && dot_pos != string::npos &&
+        dot_pos > 0 && dot_pos < file_name.size() - 1) {
+        string file_ext = file_name.substr(dot_pos + 1);
+        if ((file_ext == "txt" || file_ext == "exe" || file_ext == "dll") && isalpha(file_name[0])) {
+            return "Yes";
+        }
     }
     
-    string extension = file_name.substr(dotIndex + 1);
-    if (extension != "txt" && extension != "exe" && extension != "dll") {
-        return "No";
-    }
-    
-    return "Yes";
+    return "No";
 }
