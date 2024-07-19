58a253c9903a8786a7aa01269ed14c9cefe69074
beep boop <githubbot@vadim.me>
2024-07-19 05:19:15 +0200

    Solution 70 of CPP/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pb9xdwys Prompt: Complete the following code given the task description and function signature.

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,4 +1,4 @@
-float int_part = floor(number);
-    float decimal_part = number - int_part;
+float integer_part = floor(number);
+    float decimal_part = number - integer_part;
     return decimal_part;
 }
