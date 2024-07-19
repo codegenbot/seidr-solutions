ccb8aedbc218850a1f30700118329534fdc6bd2a
beep boop <githubbot@vadim.me>
2024-07-19 03:17:45 +0200

    Solution 86 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/p7han3r6 Prompt:                  from ad4183b4-9f2e-47e4-8f67-bafd18bd2c52.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ad4183b4-9f2e-47e4-8f67-bafd18bd2c52.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from ad4183b4-9f2e-47e4-8f67-bafd18bd2c52.cpp:4: /usr/include/c++/11/variant: In instantiation of ‘constexpr _Tp&& std::get(std:: variant<_Types ...>&&) [with _Tp = std::__cxx11::basic_string<char>; _Types = {s td::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >} ]’: ad4183b4-9f2e-47e4-8f67-bafd18bd2c52.cpp:15:5:   required from here /usr/include/c++/11/variant:1128:42: error: static assertion failed: T must occu r exactly once in alternatives  1128 |       static_assert(__detail::__variant::__exactly_once<_Tp, _Types...>,       |                     ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/variant:1128:42: note: ‘std::__detail::__variant::__exactly_ once<std::__cxx11::basic_string<char>, std::__cxx11::basic_string<char, std::cha r_traits<char>, std::allocator<char> >, std::__cxx11::basic_string<char, std::ch ar_traits<char>, std::allocator<char> > >’ evaluates to false

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 #include <cassert>
 #include <variant>
+#include <string_view>
 
-std::variant<std::string, bool> compare_one(const std::string& s1, int number) {
+std::variant<std::string, std::string_view> compare_one(const std::string& s1, int number) {
     if (std::stoi(s1) == number) {
-        return {"Equal", true};
+        return {"Equal"sv};
     } else {
-        return {"Not equal", false};
+        return {"Not equal"sv};
     }
 }
 
 int main() {
-    assert(std::get<bool>(compare_one("1", 1)) == true);
+    assert(std::get<std::string_view>(compare_one("1", 1)) == "Not equal");
 }
