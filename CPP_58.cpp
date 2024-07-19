c9cf363719cfa965422b64c3c9edbff2852fc630
beep boop <githubbot@vadim.me>
2024-07-19 07:49:19 +0200

    Solution 47 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lyaescyd Prompt:       |                    ^~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:9: 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp: In function ‘int main()’: 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 1b51fd3f-3c8c-4256-9060-b752dc9e6dbd.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,17 @@
 #include <vector>
 #include <algorithm>
 
-vector<int> common(vector<int> l1, vector<int> l2) {
+bool issame(vector<int> a, vector<int> b) 
+{
+    // Define your logic here to compare vectors a and b
+}
+
+vector<int> common(vector<int> l1, vector<int> l2)
+{
     sort(l1.begin(), l1.end());
     sort(l2.begin(), l2.end());
     vector<int> result;
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
 }
