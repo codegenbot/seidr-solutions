4551b9d416de31b0b8737881a917b515711981f1
beep boop <githubbot@vadim.me>
2024-07-19 17:08:50 +0200

    Solution 27 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/rr9jqcik Prompt: 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:30:19: error: ‘string’ has not been dec lared    30 | char next(char c, string s) {       |                   ^~~~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp: In function ‘char next(char, int)’: 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:31:15: error: request for member ‘find’  in ‘s’, which is of non-class type ‘int’    31 |     int i = s.find(c);       |               ^~~~ 5674894c-357b-4422-bb5c-234d0ed5ade0.cpp:32:13: error: invalid types ‘int[int]’ for array subscript    32 |     return s[i + 1];       |             ^

--- bowling.cpp
+++ bowling.cpp
@@ -4,25 +4,23 @@ int bowlingScore(std::string s) {
     int score = 0;
     int currentFrame = 0;
     vector<int> frames(10);
-
+    
     for (char c : s) {
         if (c == '/') {
             currentFrame++;
             continue;
         }
-
+        
         if (c >= '1' && c <= '9') {
-            int strike = false;
             while (c != '/' && c >= '0' && c <= '9') {
-                frames[currentFrame] *= 10 + (c - '0');
-                size_t i = s.find(std::string(1, c));
-                if(i != string::npos) {
-                    c = s[i+1];
-                } else {
-                    c = '/';
+                frames[currentFrame] = frames[currentFrame] * 10 + (c - '0');
+                if (c == 'X') {
+                    score += 10;
+                    break;
                 }
+                c = next(c, s);
             }
-
+            
             if (!strike) {
                 score += frames[currentFrame];
             } else {
@@ -30,11 +28,11 @@ int bowlingScore(std::string s) {
             }
         }
     }
-
+    
     return score;
 }
 
-char next(char a, char b, std::string s) {
-    size_t i = s.find(std::string(1, a));
-    return (i != string::npos && (i+1 < s.length())) ? s[i+1] : b;
+char next(char c, std::string s) {
+    int i = s.find(c);
+    return s[i + 1];
 }
