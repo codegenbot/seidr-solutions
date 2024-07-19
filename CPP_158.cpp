4691796e20ce87571e148775652b7f82b56d8226
beep boop <githubbot@vadim.me>
2024-07-19 21:08:18 +0200

    Solution 78 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/o6ixcyyd Prompt: 00868ce6-fba9-4b55-8f65-41d0694b88dc.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string find_max(vector<string> words){       | ^~~~~~ In file included from 00868ce6-fba9-4b55-8f65-41d0694b88dc.cpp:10: 00868ce6-fba9-4b55-8f65-41d0694b88dc.cpp: In function ‘int main()’: 00868ce6-fba9-4b55-8f65-41d0694b88dc.cpp:12:14: error: ‘find_max’ was not declar ed in this scope    12 |     assert ((find_max({"play", "play", "play"}) == "play"));       |              ^~~~~~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-std::string findMax(vector<string> words){
+string find_max(vector<string> words){
     string max = *max_element(words.begin(), words.end(),
         [](const string& a, const string& b){
             if(a.size() == b.size()) return a < b;
@@ -12,6 +12,6 @@ std::string findMax(vector<string> words){
 }
 
 int main(){
-    assert ((findMax({"play", "play", "play"}) == "play"));
+    assert ((find_max({"play", "play", "play"}) == "play"));
     return 0;
 }
