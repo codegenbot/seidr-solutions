a82236c03d74e78c8045ad88d47b86ef5387720d
beep boop <githubbot@vadim.me>
2024-07-19 22:27:06 +0200

    Solution 84 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8hrrqsp9 Prompt: e");       |             ^~~~~ cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:23:29: error: ‘string’ was not declared  in this scope    23 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp: In function ‘std::any compare_one(std: :any, std::any)’: cc23bbf1-12a9-46bc-95e1-36ea46431815.cpp:19:1: warning: control reaches end of n on-void function [-Wreturn-type]    19 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,7 +1,7 @@
 #include <any>
 #include <string>
 #include <cassert>
-#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 
 std::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
@@ -9,13 +9,13 @@ std::any compare_one(std::any a, std::any b) {
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
         return std::any_cast<float>(a) > std::any_cast<float>(b) ? a : b;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        float valA = std::stof(std::any_cast<std::string>(a));
-        float valB = std::stof(std::any_cast<std::string>(b));
-        return valA > valB ? a : (valA < valB ? b : std::any(std::string("None")));
+        float valA = boost::lexical_cast<float>(std::any_cast<std::string>(a));
+        float valB = boost::lexical_cast<float>(std::any_cast<std::string>(b));
+        return valA > valB ? a : (valA < valB ? b : "None");
     } else if ((a.type() == typeid(int) && b.type() == typeid(std::string)) || (a.type() == typeid(std::string) && b.type() == typeid(int))) {
-        float valA = a.type() == typeid(int) ? std::any_cast<int>(a) : std::stof(std::any_cast<std::string>(a));
-        float valB = b.type() == typeid(int) ? std::any_cast<int>(b) : std::stof(std::any_cast<std::string>(b));
-        return valA > valB ? a : (valA < valB ? b : std::any(std::string("None")));
+        float valA = a.type() == typeid(int) ? std::any_cast<int>(a) : boost::lexical_cast<float>(std::any_cast<std::string>(a));
+        float valB = b.type() == typeid(int) ? std::any_cast<int>(b) : boost::lexical_cast<float>(std::any_cast<std::string>(b));
+        return valA > valB ? a : (valA < valB ? b : "None");
     }
     assert(false);
 }
