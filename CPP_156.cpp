2e3a1771acb065fc22587600ac988e97b1c207eb
beep boop <githubbot@vadim.me>
2024-07-19 20:49:03 +0200

    Solution 94 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/whuwvx27 Prompt: 5754bd58-0afb-4709-aa61-0aa141458cee.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 5754bd58-0afb-4709-aa61-0aa141458cee.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 5754bd58-0afb-4709-aa61-0aa141458cee.cpp:18: 5754bd58-0afb-4709-aa61-0aa141458cee.cpp: In function ‘int main()’: 5754bd58-0afb-4709-aa61-0aa141458cee.cpp:20:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    20 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -10,7 +10,4 @@ std::string int_to_mini_roman(int num) {
     return roman;
 }
 
-int main() {
-    assert(int_to_mini_roman(1000) == "m");
-    return 0;
-}
+assert(int_to_mini_roman(1000) == "m");
