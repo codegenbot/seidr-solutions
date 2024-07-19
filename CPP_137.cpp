04917da0b63dcc964f026a2b08741bbf95d26ac8
beep boop <githubbot@vadim.me>
2024-07-19 04:58:43 +0200

    Solution 5 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/vocsxz1q Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,29 +1,35 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
         return a;
-    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
         return b;
+    } else {
+        return "None";
     }
-} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+} else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
         return a;
-    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
         return b;
+    } else {
+        return "None";
     }
-} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    if (a.type().name() == typeid(string).name()) {
-        string str_a = boost::any_cast<string>(a);
-        string str_b = boost::any_cast<string>(b);
-        if (str_a.find(".") != string::npos || str_a.find(",") != string::npos) {
-            str_a.erase(std::remove(str_a.begin(), str_a.end(), ','), str_a.end());
-            str_b.erase(std::remove(str_b.begin(), str_b.end(), ','), str_b.end());
-        }
-        if (stof(str_a) > stof(str_b)) {
-            return a;
-        } else if (stof(str_a) < stof(str_b)) {
-            return b;
-        }
+} else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    string strA = boost::any_cast<string>(a);
+    string strB = boost::any_cast<string>(b);
+    if(strA.find_first_of(".,") != string::npos){
+        replace(strA.begin(), strA.end(), ',', '.');
     }
-}
-return "None";
+    if(strB.find_first_of(".,") != string::npos){
+        replace(strB.begin(), strB.end(), ',', '.');
+    }
+    if(stod(strA) > stod(strB)){
+        return a;
+    } else if(stod(strA) < stod(strB)){
+        return b;
+    } else {
+        return "None";
+    }
+} else {
+    return "None";
 }
