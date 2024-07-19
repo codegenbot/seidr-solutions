cce4d01fceb34da0ae25707332c8572f7fbe8022
beep boop <githubbot@vadim.me>
2024-07-19 21:54:02 +0200

    Solution 3 of CPP/47, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ky0ma2ep Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 8145b244-80b9-4431-8486-3c0fca150fdc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -1,10 +1,13 @@
+#include <algorithm>
 #include <vector>
+#include <initializer_list>
 
-float median(std::vector<float> l) {
-    std::sort(l.begin(), l.end());
-    int n = l.size();
+float calculateMedian(std::initializer_list<float> l) {
+    std::vector<float> v(l);
+    std::sort(v.begin(), v.end());
+    int n = v.size();
     if (n % 2 == 0)
-        return (l[n/2 - 1] + l[n/2]) / 2.0;
+        return (v[n/2 - 1] + v[n/2]) / 2.0;
     else
-        return l[n/2];
+        return v[n/2];
 }
