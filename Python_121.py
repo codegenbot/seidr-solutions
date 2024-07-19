450990600025d1da43278c7e2d2a60ce4958509d
beep boop <githubbot@vadim.me>
2024-07-19 20:06:34 +0200

    Solution 89 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/k58ffkb0 Prompt: Complete the following code given the task description and function signature.

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,8 @@
 def solution(lst):
-    return sum([i for i in lst[1::2] if i % 2 != 0])
+    return sum(
+        [
+            i
+            for i in lst
+            if i % 2 != 0 and (index := list(range(len(lst)))).remove(i // 2) == []
+        ]
+    )
