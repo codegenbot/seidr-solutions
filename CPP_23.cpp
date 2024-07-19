3d408be02353c8e0f1ee415366e3fa80d0fe56e9
beep boop <githubbot@vadim.me>
2024-07-19 17:16:20 +0200

    Solution 17 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duifepny Prompt: 81c9d717-1c51-4336-8881-cb76af05815c.cpp: In function ‘int main()’: 81c9d717-1c51-4336-8881-cb76af05815c.cpp:5:5: error: ‘assert’ was not declared i n this scope     5 |     assert(strlen(str) == 9);       |     ^~~~~~ 81c9d717-1c51-4336-8881-cb76af05815c.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <cstring>   +++ |+#include <cassert>     2 | 81c9d717-1c51-4336-8881-cb76af05815c.cpp: At global scope: 81c9d717-1c51-4336-8881-cb76af05815c.cpp:12:5: error: redefinition of ‘int main( )’    12 | int main(){       |     ^~~~ 81c9d717-1c51-4336-8881-cb76af05815c.cpp:3:5: note: ‘int main()’ previously defi ned here     3 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,9 +1,12 @@
-#include <cassert> 
+#include <cassert>
 #include <cstring>
 
 int main() {
     const char* str = "asdasnakj";
     assert(strlen(str) == 9);
 
+    str = "updated";
+    assert(strlen(str) == 7);
+    
     return strlen(str);
 }
