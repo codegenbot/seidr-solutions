d07a7332bfc82230ce9bd92282df99d42754b57d
beep boop <githubbot@vadim.me>
2024-07-19 02:41:52 +0200

    Solution 3 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/wzunz235 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp:25: 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp: In function ‘int main()’: 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp:35:20: error: ‘unique_digits’ was not d eclared in this scope    35 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 875d7b04-89f4-4f90-acf1-d9d6e4e11546.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,8 +1,9 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
 
 std::vector<int> unique_digits(std::vector<int> x) {
     std::vector<int> result;
@@ -24,13 +25,7 @@ std::vector<int> unique_digits(std::vector<int> x) {
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
 int main() {
     assert(issame(unique_digits({135, 103, 31}), {31, 135}));
+    return 0;
 }
