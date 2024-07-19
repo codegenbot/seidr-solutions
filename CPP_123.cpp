3de4956446bb5df68e78d4ac3bfd097028d25885
beep boop <githubbot@vadim.me>
2024-07-19 23:34:39 +0200

    Solution 25 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v06y9f55 Prompt: 7ba94251-27e7-41f0-8abc-dd790c2defe6.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 7ba94251-27e7-41f0-8abc-dd790c2defe6.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 7ba94251-27e7-41f0-8abc-dd790c2defe6.cpp:34: 7ba94251-27e7-41f0-8abc-dd790c2defe6.cpp: In function ‘int main()’: 7ba94251-27e7-41f0-8abc-dd790c2defe6.cpp:44:35: error: could not convert ‘get_od d_collatz(int)()’ from ‘std::deque<int>’ to ‘std::vector<int>’    44 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ~~~~~~~~~~~~~~~^~~       |                                   |       |                                   std::deque<int>

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -5,29 +5,29 @@
 
 using namespace std;
 
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+deque<int> get_odd_collatz(int n) {
+    deque<int> result;
     while (n != 1) {
         if (n % 2 == 0) {
             result.push_back(n);
             n /= 2;
         } else {
-            result.push_back(n);
             n = 3 * n + 1;
+            result.push_back(n);
         }
     }
     return result;
 
 }
 
-vector<int> main() {
+int main() {
     int n;
     cout << "Enter a positive integer: ";
     cin >> n;
-    vector<int> res = get_odd_collatz(n); 
+    vector<int> res(get_odd_collatz(n).begin(),get_odd_collatz(n).end());
     for (auto i : res) {
         cout << i << " ";
     }
     cout << endl;
-    return res;
+    return 0;
 }
