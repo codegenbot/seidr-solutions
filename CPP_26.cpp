7d340c04d4527219eb1ada8729de2e8a32aa47fb
beep boop <githubbot@vadim.me>
2024-07-19 18:28:08 +0200

    Solution 60 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4j6mhwyx Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ f1d377bd-c649-433b-850d-2ec6b7e577bc.cpp:18:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -3,6 +3,25 @@
 #include <unordered_map>
 #include <algorithm>
 
+namespace std {
+    template<>
+        struct hash<int> {
+            size_t operator()(int i) const {
+                return static_cast<size_t>(i);
+            }
+        };
+
+    template<>
+        bool operator==(const int& a, const int& b) {
+            return a == b;
+        }
+
+    template<>
+        bool operator!=(const int& a, const int& b) {
+            return !(a == b);
+        }
+};
+
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a.size() == b.size() && a == b;
 }
@@ -24,7 +43,7 @@ std::vector<int> remove_duplicates(std::vector<int> numbers) {
 int main() {
     assert(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) == std::vector<int>({1, 2, 3, 4, 5}));
 
-    std::vector<int> input(std::allocator<int>());
+    std::vector<int> input;
     int num;
 
     std::cout << "Enter numbers (enter -1 to finish):" << std::endl;
