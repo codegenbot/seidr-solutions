7c6a175a524ce53e7415285670a352244ed5a781
beep boop <githubbot@vadim.me>
2024-07-19 19:28:22 +0200

    Solution 13 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r4qp91p3 Prompt:       |           ^~~~~~ 1556e122-950d-4802-bb29-987f52973695.cpp:42:44: error: ‘b’ was not declared in t his scope    42 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1556e122-950d-4802-bb29-987f52973695.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 1556e122-950d-4802-bb29-987f52973695.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main(){       |     ^~~~ In file included from 1556e122-950d-4802-bb29-987f52973695.cpp:41: 1556e122-950d-4802-bb29-987f52973695.cpp: In function ‘int main()’: 1556e122-950d-4802-bb29-987f52973695.cpp:51:20: error: ‘by_length’ was not decla red in this scope    51 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 1556e122-950d-4802-bb29-987f52973695.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -10,14 +10,6 @@ bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-vector<string> by_length(vector<int> arr);
-
-int main(){
-    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
-    
-    return 0;
-}
-
 vector<string> by_length(vector<int> arr){
     vector<string> result;
     map<int, string> numToString = {
@@ -40,4 +32,10 @@ vector<string> by_length(vector<int> arr){
     }
     
     return result;
+}
+
+int main(){
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    
+    return 0;
 }
