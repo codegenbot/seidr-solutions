84808ef89fb5422d2f1fbe0584d1be1fc05bacbf
beep boop <githubbot@vadim.me>
2024-07-19 20:40:19 +0200

    Solution 8 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oojp8dsm Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,28 +1,16 @@
-```cpp
 #include<string>
 using namespace std;
 
-bool simplify(string x,string n){
-    int a=0,b=0,c=0,d=0;
-    string s1=x.substr(1,x.size()-2);
-    string s2=n.substr(1,n.size()-2);
-    int i=0;
-    while(i<s1.size()){
-        if(s1[i]=='/'){
-            a=stoi(s1.substr(0,i));
-            b=stoi(s1.substr(i+1));
-            break;
-        }
-        i++;
+bool simplify(string x, string n) {
+    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
+    int b = stoi(x.substr(0, 1));
+    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
+    int d = stoi(n.substr(0, 1));
+
+    if (a * d != b * c) {
+        return false;
     }
-    i=0;
-    while(i<s2.size()){
-        if(s2[i]=='/'){
-            c=stoi(s2.substr(0,i));
-            d=stoi(s2.substr(i+1));
-            break;
-        }
-        i++;
+    else {
+        return true;
     }
-    return (a*d) == (b*c);
 }
