7c4d6070a361377b117aa89454c800790eef6b91
beep boop <githubbot@vadim.me>
2024-07-19 20:10:39 +0200

    Solution 0 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wnm533cp Prompt: Complete the following code given the task description and function signature.

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,10 +1,17 @@
-#include <algorithm>
-#include <vector>
-#include <bitset>
-#include <numeric>
-
-using namespace std;
-
-vector<int> sort_array(vector<int> arr) {
-    return vector<int>(arr.begin(), arr.end());
+vector<int> sort_vector(vector<int> arr) {
+    vector<pair<int, int>> pairArr;
+    
+    for(int i = 0; i < arr.size(); i++) {
+        pairArr.push_back({__builtin_popcount(arr[i]), arr[i]});
+    }
+    
+    sort(pairArr.begin(), pairArr.end());
+    
+    vector<int> result;
+    
+    for(auto p : pairArr) {
+        result.push_back(p.second);
+    }
+    
+    return result;
 }
