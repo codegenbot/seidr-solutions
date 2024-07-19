d7a0e94b276788cc2b3265cf408720ba08ca6406
beep boop <githubbot@vadim.me>
2024-07-20 01:44:13 +0200

    Solution 95 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zmsfz8na Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ da6a6c2f-5608-4771-a64c-59f5e2603094.cpp:28:26:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-long long doubleTheDifference(vector<float> lst) {
+long long doubleTheDifference(vector<double> lst) {
     long long pos_sum = 0, neg_sum = 0;
-    for (float n : lst) {
+    for (double n : lst) {
         if (n > 0) {
             pos_sum += pow(n, 2);
         } else if (n < 0) {
@@ -17,12 +17,12 @@ long long doubleTheDifference(vector<float> lst) {
 }
 
 int main() {
-    vector<float> numbers;
+    vector<double> numbers;
     int n; 
     cin >> n;
 
     for(int i = 0; i < n; i++) {
-        float num; 
+        double num; 
         cin >> num;
         numbers.push_back(num);
     }
