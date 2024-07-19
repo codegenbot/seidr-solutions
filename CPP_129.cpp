9a2bf2e6ee9d7f9876e3e272728167b76a80d56d
beep boop <githubbot@vadim.me>
2024-07-19 22:10:30 +0200

    Solution 20 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/uhs5a7pn Prompt:    30 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 241bbd8d-9796-40e8-8368-d859d7790dd8.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 241bbd8d-9796-40e8-8368-d859d7790dd8.cpp:30:39: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 241bbd8d-9796-40e8-8368-d859d7790dd8.cpp:29: 241bbd8d-9796-40e8-8368-d859d7790dd8.cpp: In function ‘int main()’: 241bbd8d-9796-40e8-8368-d859d7790dd8.cpp:39:20: error: ‘minPath’ was not declare d in this scope    39 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 241bbd8d-9796-40e8-8368-d859d7790dd8.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,47 +1,37 @@
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+#include <vector>
+
+bool issame(vector<int> a, vector<int> b);
+
+vector<int> minPath(int k, vector<vector<int>> grid) {
+    int N = grid.size();
+    vector<int> result;
+    int row = 0, col = 0;
+    for (int i = 0; i < k; ++i) {
+        result.push_back(grid[row][col]);
+        if ((row + col) % 2 == 0) {
+            if (col == N - 1) {
+                ++row;
+            } else if (row == 0) {
+                ++col;
+            } else {
+                --row;
+                ++col;
+            }
+        } else {
+            if (row == N - 1) {
+                ++col;
+            } else if (col == 0) {
+                ++row;
+            } else {
+                ++row;
+                --col;
+            }
+        }
+    }
+    return result;
 }
 
-vector<int> minPath(vector<vector<int>> grid, int k){
-    vector<int> result;
-    if(grid.empty() || grid[0].empty()){
-        return result;
-    }
-    
-    int n = grid.size(), m = grid[0].size();
-    
-    for(int i = 0; i < k; ++i){
-        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
-        dp[0][0] = grid[0][0];
-        
-        for(int r = 0; r < n; ++r){
-            for(int c = 0; c < m; ++c){
-                if(r > 0){
-                    dp[r][c] = min(dp[r][c], dp[r-1][c] + grid[r][c]);
-                }
-                if(c > 0){
-                    dp[r][c] = min(dp[r][c], dp[r][c-1] + grid[r][c]);
-                }
-            }
-        }
-        
-        vector<int> path;
-        int r = n - 1, c = m - 1;
-        
-        while(r > 0 || c > 0){
-            path.push_back(grid[r][c]);
-            if(r > 0 && dp[r][c] == dp[r-1][c] + grid[r][c]){
-                --r;
-            } else {
-                --c;
-            }
-        }
-        path.push_back(grid[0][0]);
-        
-        reverse(path.begin(), path.end());
-        grid[0][0] = path[0];
-        result.insert(result.end(), path.begin() + 1, path.end());
-    }
-    
-    return result;
+bool issame(vector<int> a, vector<int> b) {
+    // Implement the function definition here
+    return a == b;
 }
