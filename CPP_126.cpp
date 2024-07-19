902e942646ce4938cc2cf0450ca0a38622c5f0d5
beep boop <githubbot@vadim.me>
2024-07-19 04:55:35 +0200

    Solution 25 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/cbyhcf5f Prompt: f807546e-051c-4af3-a8d5-0f2f50ccfb2f.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return is_sorted(lst.begin(), lst.end()) && (unique(lst.begin(), lst.end ()) == lst.end());       | ^~~~~~ f807546e-051c-4af3-a8d5-0f2f50ccfb2f.cpp:2:1: error: expected declaration before  ‘}’ token     2 | }       | ^ In file included from f807546e-051c-4af3-a8d5-0f2f50ccfb2f.cpp:4: f807546e-051c-4af3-a8d5-0f2f50ccfb2f.cpp: In function ‘int main()’: f807546e-051c-4af3-a8d5-0f2f50ccfb2f.cpp:6:13: error: ‘is_sorted’ was not declar ed in this scope     6 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -4,5 +4,5 @@
 
 int main() {
     std::vector<int> lst{1, 2, 3, 4};
-    return std::is_sorted(lst.begin(), lst.end()) && (std::unique(lst.begin(), lst.end()) == lst.end());
+    return is_sorted(lst.begin(), lst.end()) && (std::unique(lst.begin(), lst.end()) == lst.end());
 }
