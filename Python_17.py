151b61ce57a4a99592f2c759410e2d5cd687b173
beep boop <githubbot@vadim.me>
2024-07-19 18:18:13 +0200

    Solution 43 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/s9jiy2yu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40b08700-9ba7-47e1-bc09-f30f59808 937.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -5,23 +5,28 @@ def parse_music(music_string: str) -> List[int]:
     i = 0
     while i < len(music_string):
         if music_string[i] == '.':
-            if i + 3 <= len(music_string) and music_string[i:i+3] in notes:
-                for note in notes[music_string[i:i+3]]:
-                    beats.append(note)
-                i += 2
-            else:
-                beats.append(notes.get('.|', 1))
-                i += 1
+            temp = []
+            j = i
+            while j < len(music_string) and (music_string[j] == '.' or music_string[j] == 'o'):
+                if music_string[j] == 'o':
+                    temp += notes['o']
+                elif music_string[j] == '.':
+                    temp.append(1)
+                j += 1
+            i = j
+            beats.extend(temp)
         elif music_string[i] == 'o':
-            if i + 2 <= len(music_string) and music_string[i:i+3] in notes:
-                for note in notes[music_string[i:i+3]]:
-                    beats.append(note)
-                i += 2
-            else:
-                beats.append(notes.get('o', 4))
-                i += 1
+            temp = []
+            j = i
+            while j < len(music_string) and (music_string[j] == '.' or music_string[j] == 'o'):
+                if music_string[j] == 'o':
+                    temp += notes['o']
+                elif music_string[j] == '.':
+                    temp.append(4)
+                j += 1
+            i = j
+            beats.extend(temp)
         elif music_string[i] == '|':
-            if i + 1 < len(music_string):
-                beats.append(notes.get('o|', 2))
-                i += 1
+            i += 1
+            beats.append(notes.get('o|', [2]))
     return beats
