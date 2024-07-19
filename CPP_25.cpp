42b7c24e9cd74cc761220407b20735298c16dfa0
beep boop <githubbot@vadim.me>
2024-07-19 02:00:51 +0200

    Solution 5 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/t59cuiux Prompt: 22d4b3f4-47f2-4947-a170-53526a499f07.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 22d4b3f4-47f2-4947-a170-53526a499f07.cpp:9:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     9 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 22d4b3f4-47f2-4947-a170-53526a499f07.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 22d4b3f4-47f2-4947-a170-53526a499f07.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -4,19 +4,6 @@
 using namespace std;
 
 vector<int> factorize(int n);
-bool issame(vector<int> a, vector<int> b);
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
 
 vector<int> factorize(int n) {
     vector<int> factors;
@@ -34,6 +21,5 @@ vector<int> factorize(int n) {
 
 int main() {
     assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
-    
     return 0;
 }
