77fe579ceaeacee8981071b91fc0b15ec1ae6d02
beep boop <githubbot@vadim.me>
2024-07-19 20:06:01 +0200

    Solution 25 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/cl7x8o5k Prompt: 93fbf51c-efcf-4c99-9478-d130929a8ceb.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 93fbf51c-efcf-4c99-9478-d130929a8ceb.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -4,16 +4,17 @@
 using namespace std;
 
 int add_elements(const vector<int>& arr, int k) {
-    int sum = 0;
-    for (int i = 0; i < k; ++i) {
-        if (arr[i] >= 10 && arr[i] <= 99) {
-            sum += arr[i];
-        }
-    }
-    return sum;
+	int sum = 0;
+	for (int i = 0; i < k; ++i) {
+		if (arr[i] >= 10 && arr[i] <= 99) {
+			sum += arr[i];
+		}
+	}
+	return sum;
 }
 
 int main() {
-    assert(add_elements({1}, 1) == 0);
-    return 0;
+	assert(add_elements({1}, 1) == 0);
+	
+	return 0;
 }
