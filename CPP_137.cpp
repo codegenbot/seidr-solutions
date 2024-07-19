86b76063c05e061d172fa0d8fade8f4c4ca9bebd
beep boop <githubbot@vadim.me>
2024-07-19 04:59:03 +0200

    Solution 9 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/vocsxz1q Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,23 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
         return a;
-    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
         return b;
-    } else {
-        return "None";
     }
-} else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
         return a;
-    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
         return b;
-    } else {
-        return "None";
     }
-} else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    float val1, val2;
-    if(((string)a).find(",") != string::npos){
-        replace(((string)a).begin(), ((string)a).end(), ',', '.');
-    }
-    if(((string)b).find(",") != string::npos){
-        replace(((string)b).begin(), ((string)b).end(), ',', '.');
-    }
-    val1 = stof(boost::any_cast<string>(a));
-    val2 = stof(boost::any_cast<string>(b));
-    if(val1 > val2){
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    string str_a = boost::any_cast<string>(a);
+    string str_b = boost::any_cast<string>(b);
+    if (stod(str_a) > stod(str_b)) {
         return a;
-    } else if(val1 < val2){
+    } else if (stod(str_a) < stod(str_b)) {
         return b;
-    } else {
-        return "None";
     }
-} else {
-    return "None";
 }
+return "None";
 }
