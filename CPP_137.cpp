52e51fa3db29758995e8e742da8de6ef3cd5d4cb
beep boop <githubbot@vadim.me>
2024-07-19 20:33:45 +0200

    Solution 8 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pkupi6nj Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,27 +1,41 @@
-using namespace boost;
+#include <boost/any.hpp>
 
-any compare_one(any a, any b) {
-    if (is_any_of<double>(a)) {
-        if (is_any_of<double>(b)) {
-            return (get<double>(a) > get<double>(b)) ? a : ((get<double>(a) < get<double>(b)) ? b : any("None")));
-        } else {
-            double da = get<double>(a);
-            string db = boost::any_cast<string>(b);
-            if (stod(db) > da)
-                return b;
-            else if (stod(db) < da)
-                return a;
-            else
-                return any("None");
-        }
-    } else {
-        string sa = boost::any_cast<string>(a);
-        double db = get<double>(b);
-        if (stod(sa) > db)
-            return a;
-        else if (stod(sa) < db)
-            return b;
-        else
-            return any("None");
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int) a > (float) b ? a : b;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int) a > (double) b ? a : b;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(double)) {
+        return (float) a > (double) b ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string s1 = boost::any_cast<string>(a);
+        string s2 = boost::any_cast<string>(b);
+        return s1.compare(s2) > 0 ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string s = boost::any_cast<string>(a);
+        int i = boost::any_cast<int>(b);
+        return s.compare(to_string(i)) > 0 ? a : boost::any(b);
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        string s = boost::any_cast<string>(a);
+        double d = boost::any_cast<double>(b);
+        return s.compare(to_string(d)) > 0 ? a : boost::any(b);
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int i = boost::any_cast<int>(a);
+        string s = boost::any_cast<string>(b);
+        return i > stoi(s) ? a : boost::any(b);
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        double d = boost::any_cast<double>(a);
+        string s = boost::any_cast<string>(b);
+        return d > stod(s) ? a : boost::any(b);
+    }
+    else {
+        return boost::any("None");
     }
 }
