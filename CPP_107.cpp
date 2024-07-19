4eb2151d1cd115d0cce8f0da4d4647acb248b5b7
beep boop <githubbot@vadim.me>
2024-07-20 00:47:38 +0200

    Solution 4 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ecmegk27 Prompt: Complete the following code given the task description and function signature.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,29 +1,16 @@
-int count_palindromes(int num) {
-        int count = 0;
-        for (int i = 1; i <= num; ++i) {
-            string str = to_string(i);
-            string rev = str;
-            reverse(rev.begin(), rev.end());
-            if (str == rev) {
-                count++;
+vector<int> even_odd_palindrome(int n){
+    vector<int> result(2, 0);
+    for(int i=1; i<=n; ++i){
+        string s = to_string(i);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        if(s == rev){
+            if(i % 2 == 0){
+                result[0]++;
+            } else {
+                result[1]++;
             }
         }
-        return count;
     }
-
-    vector<int> even_odd_palindrome(int n) {
-        vector<int> result(2, 0);
-        for (int i = 1; i <= n; ++i) {
-            string str = to_string(i);
-            string rev = str;
-            reverse(rev.begin(), rev.end());
-            if (str == rev) {
-                if (i % 2 == 0) {
-                    result[0]++;
-                } else {
-                    result[1]++;
-                }
-            }
-        }
-        return result;
-    }
+    return result;
+}
