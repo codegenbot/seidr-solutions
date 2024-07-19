4e5ababc557aabfc53c7cb39ead277461f144bc8
beep boop <githubbot@vadim.me>
2024-07-19 17:05:34 +0200

    Solution 12 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/189ox018 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e925c02-9ae1-43ed-a202-de6ddf6c1 22c.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8e925c02-9ae1-43ed-a202-de6ddf6c1 22c.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,12 +3,12 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.lower() in 'aeiou':
-                result += chr((ord(char) - 97 + 3) % 26 + 97)
-            else:
                 if char.isupper():
-                    result += chr((ord(char.lower()) - 97 + 3) % 26 + 65)
+                    result += chr((ord(char) - 65 + 3) % 26 + 65)
                 else:
-                    result += char.swapcase()
+                    result += chr((ord(char) - 97 + 3) % 26 + 97)
+            else:
+                result += char.swapcase()
         else:
             result += char
     return result
