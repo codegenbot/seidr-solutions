04074bfdc6e067dca9b96244592c4b5606a8f65c
beep boop <githubbot@vadim.me>
2024-07-19 22:09:05 +0200

    Solution 4 of CPP/133, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8st2pwlm Prompt: 7f2d42c1-fe28-43dd-9e8e-e9ea555e26ab.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<float> lst){       |                 ^~~~~~ 7f2d42c1-fe28-43dd-9e8e-e9ea555e26ab.cpp:1:24: error: expected primary-expressio n before ‘float’     1 | int sum_squares(vector<float> lst){       |                        ^~~~~ In file included from 7f2d42c1-fe28-43dd-9e8e-e9ea555e26ab.cpp:10: 7f2d42c1-fe28-43dd-9e8e-e9ea555e26ab.cpp: In function ‘int main()’: 7f2d42c1-fe28-43dd-9e8e-e9ea555e26ab.cpp:12:24: error: ‘sum_squares’ cannot be u sed as a function    12 |     assert (sum_squares({-1,1,0})==2);       |             ~~~~~~~~~~~^~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,7 +1,10 @@
-int sum_squares(vector<float> lst){
+#include <vector>
+#include <cmath>
+
+int sum_squares(std::vector<float> lst){
     int sum = 0;
     for(float num : lst){
-        int rounded_num = ceil(num);
+        int rounded_num = std::ceil(num);
         sum += rounded_num * rounded_num;
     }
     return sum;
