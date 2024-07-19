41bb0963cfc02747897e22d614b9495dda3da8ea
beep boop <githubbot@vadim.me>
2024-07-19 04:58:21 +0200

    Solution 52 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9p9okni7 Prompt:       |                              ^~~~~~ d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp:29: d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp: In function ‘int main()’: d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ d3c14808-a028-4026-be64-8fdc72ccf7a6.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -3,32 +3,6 @@
 
 bool issame(vector<string> a, vector<string> b);
 
-vector<string> split_words(string txt);
-
-int main() {
-    string input;
-    getline(cin, input);
-    vector<string> words = split_words(input);
-    if(issame(words, {"hello", "world"})){
-        cout << "The words are the same." << endl;
-    } else {
-        cout << "The words are different." << endl;
-    }
-    return 0;
-}
-
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<string> split_words(string txt){
     vector<string> result;
     string word = "";
@@ -55,4 +29,9 @@ vector<string> split_words(string txt){
         result.push_back(to_string(odd_lower_case_count));
     }
     return result;
+}
+
+int main() {
+    // Main function logic here
+    return 0;
 }
