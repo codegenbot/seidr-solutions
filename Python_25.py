253c2b101a5688830624657d7194a707f6f93017
beep boop <githubbot@vadim.me>
2024-07-19 08:04:27 +0200

    Solution 58 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gspxhuw9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d8d1f44b-34bc-4451-929e-e208ebdbc 235.py", line 1, in <module>     print(factorize(int(input("Enter a number to factorize: "))))           ^^^^^^^^^ NameError: name 'factorize' is not defined

--- Python_25.py
+++ Python_25.py
@@ -1,9 +1,8 @@
 def factorize(n):
     factors = []
-    for i in range(2, n+1):
-        while n % i == 0:
+    for i in range(1, n + 1):
+        if n % i == 0:
             factors.append(i)
-            n //= i
     return factors
 
 print(factorize(int(input("Enter a number to factorize: ")))
