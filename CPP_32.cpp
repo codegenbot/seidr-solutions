04fe382e42b903dee458235838bd4d5aa0aa0514
beep boop <githubbot@vadim.me>
2024-07-19 22:30:55 +0200

    Solution 89 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/d91o4gsr Prompt: erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = double*; _Size = long unsigned int; _Tp = double ]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = double; _Alloc = std::allocator<double>; std::vector<_Tp, _Alloc>::size_ty pe = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = double; _Alloc = std::allocator<double>; std::vector<_Tp, _Alloc>::size_t ype = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocat or<double>]’ 2c8deec3-8042-4f39-a277-827b4e861617.cpp:23:28:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,27 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <iomanip>
-
-using namespace std;
-
-double find_zero(vector<double> coeffs, double x) {
-    double result = 0;
-    double power = 1;
-    for (double coeff : coeffs) {
-        result += coeff * pow(x, power);
-        power++;
-    }
-    return -pow(x,2) + result;
-}
-
 int main() {
     int n;
     cin >> n;
 
-    vector<double> coeffs(n); // initialize with n elements
+    vector<double> coeffs;
     for(int i = 0; i < n; i++) {
-        cin >> coeffs[i];
+        cin >> coeffs.push_back(i);
     }
 
     double x;
