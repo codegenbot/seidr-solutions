ee351e89c1af0bafe553c2e85fc95894db1881ec
beep boop <githubbot@vadim.me>
2024-07-19 02:01:10 +0200

    Solution 1 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/olbrw399 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3b3a9694-5eaa-4cb5-b02b-8a87c3fbca83.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3b3a9694-5eaa-4cb5-b02b-8a87c3fbca83.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3b3a9694-5eaa-4cb5-b02b-8a87c3fbca83.cpp:11: 3b3a9694-5eaa-4cb5-b02b-8a87c3fbca83.cpp: In function ‘int main()’: 3b3a9694-5eaa-4cb5-b02b-8a87c3fbca83.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 3b3a9694-5eaa-4cb5-b02b-8a87c3fbca83.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -7,3 +7,12 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     }
     return result;
 }
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+int main() {
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+    return 0;
+}
