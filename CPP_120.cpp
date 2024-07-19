629012010699c8ad626fbd0d90c5ebf4ecb311ed
beep boop <githubbot@vadim.me>
2024-07-19 19:58:37 +0200

    Solution 22 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/evobd31t Prompt: In file included from /usr/include/c++/11/cassert:44,                  from df2fb165-2d69-4494-88fc-0fa71afe016a.cpp:4: df2fb165-2d69-4494-88fc-0fa71afe016a.cpp: In function ‘int main()’: df2fb165-2d69-4494-88fc-0fa71afe016a.cpp:15:12: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    15 |     assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));       |            ^~~~~~ df2fb165-2d69-4494-88fc-0fa71afe016a.cpp: At global scope: df2fb165-2d69-4494-88fc-0fa71afe016a.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ df2fb165-2d69-4494-88fc-0fa71afe016a.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -5,10 +5,6 @@
 
 using namespace std;
 
-bool vector_comparison(const vector<int>& v1, const vector<int>& v2) {
-    return v1 == v2;
-}
-
 vector<int> maximum(vector<int> arr, int k) {
     sort(arr.begin(), arr.end(), greater<int>());
     vector<int> result(arr.begin(), arr.begin() + k);
@@ -16,7 +12,7 @@ vector<int> maximum(vector<int> arr, int k) {
 }
 
 int main() {
-    assert(vector_comparison(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
-
+    assert(equal(maximum({1, 2, 3, -23, 243, -400, 0}, 0).begin(), maximum({1, 2, 3, -23, 243, -400, 0}, 0).end(), {}).begin());
+    
     return 0;
 }
