0c4b590d8c1c1b2b4bed8062c752374bb28932ba
beep boop <githubbot@vadim.me>
2024-07-19 23:01:53 +0200

    Solution 91 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt:       |                    ^~~ ba74d782-18a6-4843-9fe7-11597be8c4af.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ba74d782-18a6-4843-9fe7-11597be8c4af.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ba74d782-18a6-4843-9fe7-11597be8c4af.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ba74d782-18a6-4843-9fe7-11597be8c4af.cpp:8: ba74d782-18a6-4843-9fe7-11597be8c4af.cpp: In function ‘int main()’: ba74d782-18a6-4843-9fe7-11597be8c4af.cpp:18:26: error: ‘common’ cannot be used a s a function    18 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~ ba74d782-18a6-4843-9fe7-11597be8c4af.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,6 @@
-```cpp
 #include <vector>
-#include <set>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    set<int> s1(a.begin(), a.end());
-    set<int> s2(b.begin(), b.end());
+bool common(vector<int> a, vector<int> b) {
+    std::set<int> s1(a.begin(), a.end());
+    std::set<int> s2(b.begin(), b.end());
     return (s1 == s2 || s1.issubset(s2) || s2.issubset(s1));
 }
