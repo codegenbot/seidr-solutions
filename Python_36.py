675593ac4aa09180b7855690ef2581fc7dd6b71e
beep boop <githubbot@vadim.me>
2024-07-19 05:27:08 +0200

    Solution 24 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r5iimeo4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d81bc9f-fd56-4e5e-a882-42047eb8a 988.py", line 10, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d81bc9f-fd56-4e5e-a882-42047eb8a 988.py", line 7, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,4 +1,8 @@
 def fizz_buzz(n: int):
-    digit_sequences = {'117', '137', '177', '777', '711', '171', '1177', '1777', '7777'}
-    count = sum(1 for i in range(1, n+1) if any(digit_sequence in str(i) for digit_sequence in digit_sequences))
+    count = 0
+    for i in range(1, n+1):
+        for digit_sequence in ['117', '137', '177', '777', '711', '171', '1177', '1777', '7777']:
+            if digit_sequence in str(i):
+                count += 1
+                break
     return count
