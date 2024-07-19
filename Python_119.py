f660ad6bac740d445853ca9d05ef130c2dc00e89
beep boop <githubbot@vadim.me>
2024-07-19 20:02:47 +0200

    Solution 56 of Python/119, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fhn5led7 Prompt: Complete the following code given the task description and function signature.

--- Python_119.py
+++ Python_119.py
@@ -1,19 +1,10 @@
-```
 def match_parens(lst):
     open_count = 0
     close_count = 0
-    
     for s in lst:
-        for c in s:
-            if c == '(':
+        for char in s:
+            if char == "(":
                 open_count += 1
-            elif c == ')':
+            elif char == ")":
                 close_count += 1
-                
-            if close_count > open_count:
-                return 'No'
-                
-    if open_count == close_count:
-        return 'Yes'
-    else:
-        return 'No'
+    return "Yes" if open_count == close_count else "No"
