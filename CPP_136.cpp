ebfc990435ff078fcbfd2dd0fd27236be0fd4ca8
beep boop <githubbot@vadim.me>
2024-07-19 22:17:14 +0200

    Solution 28 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/b4tchdp1 Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1f851ffa-18bf-4909-8238-15d9a67b9a5a.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1f851ffa-18bf-4909-8238-15d9a67b9a5a.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1f851ffa-18bf-4909-8238-15d9a67b9a5a.cpp:16: 1f851ffa-18bf-4909-8238-15d9a67b9a5a.cpp: In function ‘int main()’: 1f851ffa-18bf-4909-8238-15d9a67b9a5a.cpp:26:20: error: ‘largest_smallest_integer s’ was not declared in this scope    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 1f851ffa-18bf-4909-8238-15d9a67b9a5a.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,26 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
+pair<int, int> largest_smallest_integers(vector<int> lst){
+    int largest_negative = INT_MIN;
+    int smallest_positive = INT_MAX;
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-vector<int> largest_smallest_integers(vector<int> lst){
-    int largest_neg = INT_MIN, smallest_pos = INT_MAX;
     for(int num : lst){
-        if(num < 0 && num > largest_neg){
-            largest_neg = num;
-        }
-        if(num > 0 && num < smallest_pos){
-            smallest_pos = num;
+        if(num < 0 && num > largest_negative){
+            largest_negative = num;
+        } else if(num > 0 && num < smallest_positive){
+            smallest_positive = num;
         }
     }
-    return {largest_neg, smallest_pos};
-}
 
-int main() {
-    assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
-    
-    return 0;
+    return make_pair(largest_negative, smallest_positive);
 }
