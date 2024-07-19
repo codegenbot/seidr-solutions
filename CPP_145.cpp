8eb69238bd4d84bf953f21a7f834006d656fca6f
beep boop <githubbot@vadim.me>
2024-07-20 00:36:15 +0200

    Solution 38 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bg00m0r1 Prompt: In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 96ae40b9-88d7-4fdf-b1cd-0a85b1ea6a7d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 96ae40b9-88d7-4fdf-b1cd-0a85b1ea6a7d.cpp:57:34: error: expected primary-expressi on before ‘int’    57 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 96ae40b9-88d7-4fdf-b1cd-0a85b1ea6a7d.cpp:57:39: error: expression list treated a s compound expression in initializer [-fpermissive]    57 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 96ae40b9-88d7-4fdf-b1cd-0a85b1ea6a7d.cpp:56: 96ae40b9-88d7-4fdf-b1cd-0a85b1ea6a7d.cpp: In function ‘int main()’: 96ae40b9-88d7-4fdf-b1cd-0a85b1ea6a7d.cpp:66:19: error: ‘issame’ cannot be used a s a function    66 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -12,7 +12,7 @@ bool is_same(const std::vector<int>& a, const std::vector<int>& b) {
     return true;
 }
 
-bool same(const std::vector<int>& a, const std::vector<int>& b) {
+bool my_comparable(const std::vector<int>& a, const std::vector<int>& b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++) {
@@ -46,7 +46,7 @@ std::vector<int> order_by_points(std::vector<int> nums) {
 
 int main() {
     std::vector<int> nums = {0,6,6,-76,-21,23,4};
-    assert(same(order_by_points(nums), {4, -21, 0, 6, 6, 23, -76}));
+    assert(is_same(order_by_points(nums), {4, -21, 0, 6, 6, 23, -76}));
     for (int num : order_by_points(nums)) {
         std::cout << num << " ";
     }
