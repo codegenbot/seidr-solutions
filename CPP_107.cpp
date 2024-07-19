8fb6c2b700bae7d764306a2ef78526b87fdf3fa7
beep boop <githubbot@vadim.me>
2024-07-19 04:43:00 +0200

    Solution 15 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dx28t2x4 Prompt:       |                    ^~~ dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp:17: dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp: In function ‘int main()’: dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ dee7c544-f402-44dd-9dd5-e1bcbe1884e1.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,5 +1,25 @@
 bool issame(vector<int> a, vector<int> b); // Function prototype
 
+vector<int> even_odd_palindrome(int n) {
+    vector<int> result = {0, 0};
+    int even = 0, odd = 0;
+    for (int i = 1; i <= n; ++i) {
+        string num = to_string(i);
+        string rev_num = num;
+        reverse(rev_num.begin(), rev_num.end());
+        if (num == rev_num) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+    }
+    result[0] = even;
+    result[1] = odd;
+    return result;
+}
+
 int main() {
     assert(issame(even_odd_palindrome(1), {0, 1}));  // Test the function
 }
