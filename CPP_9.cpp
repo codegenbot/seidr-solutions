fba1ca1a3e49e44eaa2b5a3be50c73eb51c3c6db
beep boop <githubbot@vadim.me>
2024-07-19 17:03:01 +0200

    Solution 14 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/1uznveu6 Prompt: on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 22319f82-f119-484d-9866-9eb7aeb80aad.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 22319f82-f119-484d-9866-9eb7aeb80aad.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~ In file included from 22319f82-f119-484d-9866-9eb7aeb80aad.cpp:20: 22319f82-f119-484d-9866-9eb7aeb80aad.cpp: In function ‘int main()’: 22319f82-f119-484d-9866-9eb7aeb80aad.cpp:30:20: error: ‘rolling_max’ was not dec lared in this scope    30 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 22319f82-f119-484d-9866-9eb7aeb80aad.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,8 +1,8 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
 
-using namespace std;
+vector<int> rolling_max(vector<int> numbers);
+
+bool issame(vector<int> a, vector<int> b);
 
 vector<int> rolling_max(vector<int> numbers){
     vector<int> result;
@@ -19,6 +19,6 @@ bool issame(vector<int> a, vector<int> b){
 }
 
 int main(){
-    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
+    assert (issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
     return 0;
 }
