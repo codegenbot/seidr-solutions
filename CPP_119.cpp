b9f4fc07f2700c2dafd89ce0edd0760ab1eda7e7
beep boop <githubbot@vadim.me>
2024-07-19 18:53:49 +0200

    Solution 13 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/i6qiedr6 Prompt: ined here    18 | int main() {       |     ^~~~ In file included from 770597b1-3aec-4acb-b21b-e46c441b34c5.cpp:25: 770597b1-3aec-4acb-b21b-e46c441b34c5.cpp: In function ‘int main()’: 770597b1-3aec-4acb-b21b-e46c441b34c5.cpp:27:25: error: cannot bind non-const lva lue reference of type ‘std::vector<std::__cxx11::basic_string<char> >&’ to an rv alue of type ‘std::vector<std::__cxx11::basic_string<char> >’    27 |     assert (match_parens({")", "("}) == "Yes" );       |             ~~~~~~~~~~~~^~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 770597b1-3aec-4acb-b21b-e46c441b34c5.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<s td::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::allocator_type = st d::allocator<std::__cxx11::basic_string<char> >]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 770597b1-3aec-4acb-b21b-e46c441b34c5.cpp:5:45: note:   initializing argument 1 o f ‘bool match_parens(std::vector<std::__cxx11::basic_string<char> >&)’     5 | bool match_parens(std::vector<std::string>& lst) {       |                   ~~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -17,7 +17,7 @@ bool match_parens(const std::vector<std::string>& lst) {
 
 int main() {
     std::vector<std::string> lst = {"(a(b+c))", "(d(e(f(g)))", "((h(i(j))))"};
-    if(match_parens(lst)) std::cout << "Yes" << std::endl;
+    if(match_parens(std::move(lst))) std::cout << "Yes" << std::endl;
     else std::cout << "No" << std::endl;
     return 0;
 }
