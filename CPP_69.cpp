a5bdca3de4eb4b4905a4bbd0d55cb1dbfe7b5ffd
beep boop <githubbot@vadim.me>
2024-07-19 19:20:59 +0200

    Solution 74 of CPP/69, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ulptsl46 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 0e1988e2-c390-47c0-8386-b8ce8211d8b9.cpp:27:45:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,11 +1,25 @@
-Here is the completed code:
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
 
 int search(vector<int> lst) {
     int max = 0;
+    map<int, int> mp;
     for (int i : lst) {
-        if (i > 0 && i >= count(i)) {
+        if (i > 0 && ++mp[i] >= i) {
             max = i;
         }
     }
     return max == 0 ? -1 : max;
+}
+
+int problem69() {
+    vector<int> lst; 
+    for (int i = 0; i < 5; ++i) { 
+        int temp; cin >> temp; lst.push_back(temp); 
+    }
+    int result = search(lst);
+    cout << "The largest number in the list is: " << result << endl;
+    return 0;
 }
