b57eb6a6970aa51a07ef1f713245548bfa973777
beep boop <githubbot@vadim.me>
2024-07-19 22:53:59 +0200

    Solution 20 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt: c562eb13-0bb3-42e2-a279-9dc69f868bd4.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ c562eb13-0bb3-42e2-a279-9dc69f868bd4.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -10,7 +10,7 @@ int main() {
     float a, b, c;
     std::cin >> a >> b >> c;
     if(any_int(a, b, c))
-        std::cout << "The numbers are integers: " << std::to_string(a) << ", " << std::to_string(b) << ", " << std::to_string(c) << "\n";
+        std::cout << std::to_string(a) << ", " << std::to_string(b) << ", " << std::to_string(c) << "\n";
     else
         std::cout << "The numbers are not integers: " << std::to_string(a) << ", " << std::to_string(b) << ", " << std::to_string(c) << "\n";
     return 0;
