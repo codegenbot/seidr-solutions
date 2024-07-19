1ca1423105c6d34d5a63e70ce7971f9005fb4814
beep boop <githubbot@vadim.me>
2024-07-19 21:14:41 +0200

    Solution 56 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mxcbr0ed Prompt: In file included from af79f811-39eb-498a-92fb-638ab52ae2a2.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ af79f811-39eb-498a-92fb-638ab52ae2a2.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ af79f811-39eb-498a-92fb-638ab52ae2a2.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ af79f811-39eb-498a-92fb-638ab52ae2a2.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from af79f811-39eb-498a-92fb-638ab52ae2a2.cpp:23: af79f811-39eb-498a-92fb-638ab52ae2a2.cpp: In function ‘int main()’: af79f811-39eb-498a-92fb-638ab52ae2a2.cpp:33:20: error: ‘incr_list’ was not decla red in this scope    33 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -8,16 +8,18 @@ bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
+vector<int> incr_list(const vector<int>& l);
+
+int main() {
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    
+    return 0;
+}
+
 vector<int> incr_list(const vector<int>& l) {
     vector<int> result;
     for (int num : l) {
         result.push_back(num + 1);
     }
     return result;
-}
-
-int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
-    
-    return 0;
 }
