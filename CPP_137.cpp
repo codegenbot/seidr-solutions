d1cbd017a6647b26f857037c96eaf4fcc1baec2f
beep boop <githubbot@vadim.me>
2024-07-19 20:18:59 +0200

    Solution 26 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0w0ouwj6 Prompt:                  from 101a6cd9-8e03-43a9-a721-9c6df5d7ffb4.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 101a6cd9-8e03-43a9-a721-9c6df5d7ffb4.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 101a6cd9-8e03-43a9-a721-9c6df5d7ffb4.cpp:1: /usr/include/c++/11/any: In instantiation of ‘_ValueType std::any_cast(const std ::any&) [with _ValueType = std::__cxx11::basic_string<char>&]’: 101a6cd9-8e03-43a9-a721-9c6df5d7ffb4.cpp:25:40:   required from here /usr/include/c++/11/any:465:21: error: static assertion failed: Template argumen t must be constructible from a const value.   465 |       static_assert(is_constructible_v<_ValueType, const _Up&>,       |                     ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ /usr/include/c++/11/any:465:21: note: ‘std::is_constructible_v<std::__cxx11::bas ic_string<char>&, const std::__cxx11::basic_string<char, std::char_traits<char>,  std::allocator<char> >&>’ evaluates to false /usr/include/c++/11/any:469:16: error: invalid ‘static_cast’ from type ‘const st d::__cxx11::basic_string<char>’ to type ‘std::__cxx11::basic_string<char>&’   469 |         return static_cast<_ValueType>(*__p);       |                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -6,29 +6,29 @@ std::any compare_one(const std::any& a, const std::any& b) {
     assert(a.type() == b.type());
 
     if(a.type() == typeid(int)) {
-        if(std::any_cast<const int>(a) > std::any_cast<const int>(b))
+        if(std::any_cast<int>(a) > std::any_cast<int>(b))
             return a;
-        else if(std::any_cast<const int>(a) < std::any_cast<const int>(b))
+        else if(std::any_cast<int>(a) < std::any_cast<int>(b))
             return b;
         else
             return 0;
     }
     else if(a.type() == typeid(float)) {
-        if(std::any_cast<const float>(a) > std::any_cast<const float>(b))
+        if(std::any_cast<float>(a) > std::any_cast<float>(b))
             return a;
-        else if(std::any_cast<const float>(a) < std::any_cast<const float>(b))
+        else if(std::any_cast<float>(a) < std::any_cast<float>(b))
             return b;
         else
             return 0.0f;
     }
     else if(a.type() == typeid(std::string)) {
-        if(std::any_cast<const std::string>(a) > std::any_cast<const std::string>(b))
+        if(std::any_cast<std::string>(a) > std::any_cast<std::string>(b))
             return a;
-        else if(std::any_cast<const std::string>(a) < std::any_cast<const std::string>(b))
+        else if(std::any_cast<std::string>(a) < std::any_cast<std::string>(b))
             return b;
         else
-            return "";
+            return std::string("");
     }
 
-    return std::any(); // None
+    return std::any();
 }
