d01e3b88eb6520882433146d13e7eb6217336477
beep boop <githubbot@vadim.me>
2024-07-19 07:00:11 +0200

    Solution 48 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/riusp3s9 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,20 +1,32 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
         return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
         return b;
-}
-else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
+    }
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
         return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
         return b;
-}
-else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    if(boost::any_cast<string>(a) > boost::any_cast<string>(b))
+    }
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    float val1, val2;
+    if (boost::any_cast<string>(a).find_first_of(",.") != string::npos) {
+        val1 = stof(boost::any_cast<string>(a).replace(boost::any_cast<string>(a).find_first_of(","), 1, "."));
+    } else {
+        val1 = stof(boost::any_cast<string>(a));
+    }
+    if (boost::any_cast<string>(b).find_first_of(",.") != string::npos) {
+        val2 = stof(boost::any_cast<string>(b).replace(boost::any_cast<string>(b).find_first_of(","), 1, "."));
+    } else {
+        val2 = stof(boost::any_cast<string>(b));
+    }
+    if (val1 > val2) {
         return a;
-    else if(boost::any_cast<string>(a) < boost::any_cast<string>(b))
+    } else if (val1 < val2) {
         return b;
+    }
 }
 return "None";
 }
