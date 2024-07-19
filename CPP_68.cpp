79b0a9a0c91ebf572628ec9c14f5130bbe8ef03e
beep boop <githubbot@vadim.me>
2024-07-19 02:42:23 +0200

    Solution 83 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/yepgxqdx Prompt: 2bee558a-f6aa-4aa1-b8ae-5ab95da2b8a9.cpp:23:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    23 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 2bee558a-f6aa-4aa1-b8ae-5ab95da2b8a9.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 2bee558a-f6aa-4aa1-b8ae-5ab95da2b8a9.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 2bee558a-f6aa-4aa1-b8ae-5ab95da2b8a9.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,14 +1,14 @@
 #include <vector>
 #include <cassert>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> pluck(vector<int> nums) {
-    return nums.empty() ? nums : vector<int>(1, nums.front());
+vector<int> pluck(vector<int> v){
+    return v;
 }
 
-int main() {
-    assert(issame(pluck({7, 9, 7, 1}), {}));
-    
+int main(){
+    assert(pluck({7, 9, 7, 1}) == vector<int>{});
     return 0;
 }
