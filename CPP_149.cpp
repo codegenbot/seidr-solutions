123b0431560caedca82405d32ae925d137afa2e9
beep boop <githubbot@vadim.me>
2024-07-19 20:26:28 +0200

    Solution 33 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ptyt07ic Prompt: 7939a603-9790-41f9-bb92-db4b32cbbecd.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main(){       |     ^~~~ In file included from 7939a603-9790-41f9-bb92-db4b32cbbecd.cpp:33: 7939a603-9790-41f9-bb92-db4b32cbbecd.cpp: In function ‘int main()’: 7939a603-9790-41f9-bb92-db4b32cbbecd.cpp:43:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    43 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~ 7939a603-9790-41f9-bb92-db4b32cbbecd.cpp:8:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     8 | bool issame(const vector<string>& a, const vector<string>& b){       |      ^~~~~~ 7939a603-9790-41f9-bb92-db4b32cbbecd.cpp:34:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    34 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -17,24 +17,11 @@ vector<string> sorted_list_sum(vector<string> lst){
 }
 
 int main(){
-    vector<string> v1 = sorted_list_sum({"aaaa", "bbbb", "dd", "cc"});
-    vector<string> v2 = {"cc", "dd", "aaaa", "bbbb"};
-    for (int i = 0; i < v1.size(); ++i) {
-        assert(v1[i] == v2[i]);
-    }
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
 
     // Add more test cases here
-    vector<string> v3 = sorted_list_sum({"hello", "world", "abc"});
-    vector<string> v4 = {"abc", "world", "hello"};
-    for (int i = 0; i < v3.size(); ++i) {
-        assert(v3[i] == v4[i]);
-    }
-
-    vector<string> v5 = sorted_list_sum({"programming", "is", "fun"});
-    vector<string> v6 = {"is", "fun", "programming"};
-    for (int i = 0; i < v5.size(); ++i) {
-        assert(v5[i] == v6[i]);
-    }
+    assert(issame(sorted_list_sum({"hello", "world", "abc"}), {"abc", "world", "hello"}));
+    assert(issame(sorted_list_sum({"programming", "is", "fun"}), {"is", "fun", "programming"}));
 
     return 0;
 }
