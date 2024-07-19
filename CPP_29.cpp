40e40ed9e3f22a33ca18c2bc14ef4473204e5318
beep boop <githubbot@vadim.me>
2024-07-19 21:54:26 +0200

    Solution 30 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n2ai249y Prompt: 33c1eda0-8ee8-46c9-a34d-9e7aff086550.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 33c1eda0-8ee8-46c9-a34d-9e7aff086550.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 33c1eda0-8ee8-46c9-a34d-9e7aff086550.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 33c1eda0-8ee8-46c9-a34d-9e7aff086550.cpp:35: 33c1eda0-8ee8-46c9-a34d-9e7aff086550.cpp: In function ‘int main()’: 33c1eda0-8ee8-46c9-a34d-9e7aff086550.cpp:45:36: error: could not convert ‘{"xxx" , "asd", "xxy", "john doe", "xxxAAA", "xxx"}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<std::vector<std::__cxx11::basic_string<char> > >’    45 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~       |                                    |       |                                    <brace-enclosed initializer list>

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <vector>
+#include <string>
 
 bool issame(std::vector<std::string> vec1, std::vector<std::string> vec2) {
     if(vec1.size() != vec2.size()) return false;
@@ -30,4 +31,3 @@ int main() {
     std::vector<std::vector<std::string>> strings = {{{"xxx"}}, {{"asd"}}, {{"xxy"}}, {{{"john doe"}}}, {{"xxxxAAA"}}, {{"xxx"}}};
     assert(issame(filter_by_prefix(strings, "xxx"), {{"xxx"}, {"xxxAAA"}, {"xxx"}}));
     return 0;
-}
