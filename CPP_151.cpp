3a1e8d4980d1e1dbcb158c6e5e3d49f96033b3df
beep boop <githubbot@vadim.me>
2024-07-20 00:51:21 +0200

    Solution 36 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/zmsfz8na Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d68f40f1-6f79-473c-939d-09a177885f2f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,25 +4,25 @@
 
 using namespace std;
 
-long long doubleTheDifference(vector<float> lst) {
+long long doubleTheDifference(vector<int> lst) {
     long long pos_sum = 0, neg_sum = 0;
-    for (float n : lst) {
-        if (n > 0 && modf(n, &n) == 0) {
-            pos_sum += pow(n, 2);
-        } else if (n < 0 && modf(-n, &n) == 0) {
-            neg_sum -= pow(-n, 2);
+    for (int n : lst) {
+        if (n > 0) {
+            pos_sum += n;
+        } else if (n < 0) {
+            neg_sum -= n;
         }
     }
     return abs(pos_sum - neg_sum);
 }
 
-int start() { 
-    vector<float> lst;
-    float num;
+int main() { 
+    vector<int> lst;
+    int num;
     cout << "Enter numbers (enter 'stop' to finish): ";
     while (true) {
         cin >> num; 
-        if (cin >> num && !cin.fail()) break;
+        if (!cin) break;
         lst.push_back(num);
     }
     long long result = doubleTheDifference(lst);
