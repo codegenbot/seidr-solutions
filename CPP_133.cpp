c29b274b6612d6292b644f15113f368ead00886d
beep boop <githubbot@vadim.me>
2024-07-19 04:58:01 +0200

    Solution 20 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/9s24ad73 Prompt: 17af345d-e17c-451d-a271-776f7a4c17d9.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<float> lst){       |                 ^~~~~~ 17af345d-e17c-451d-a271-776f7a4c17d9.cpp:1:24: error: expected primary-expressio n before ‘float’     1 | int sum_squares(vector<float> lst){       |                        ^~~~~ In file included from 17af345d-e17c-451d-a271-776f7a4c17d9.cpp:9: 17af345d-e17c-451d-a271-776f7a4c17d9.cpp: In function ‘int main()’: 17af345d-e17c-451d-a271-776f7a4c17d9.cpp:11:24: error: ‘sum_squares’ cannot be u sed as a function    11 |     assert (sum_squares({-1,1,0})==2);       |             ~~~~~~~~~~~^~~~~~~~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,9 +1,6 @@
 #include <vector>
-#include <cmath>
 
-using namespace std;
-
-int sum_squares(vector<float> lst){
+int sum_squares(std::vector<float> lst){
     int sum = 0;
     for (float num : lst) {
         sum += ceil(num) * ceil(num);
@@ -11,7 +8,6 @@ int sum_squares(vector<float> lst){
     return sum;
 }
 
-int main(){
-    assert (sum_squares({-1,1,0})==2);
-    return 0;
+int main() {
+    assert(sum_squares({-1, 1, 0}) == 2);
 }
