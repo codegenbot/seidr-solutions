fe5a49d9ea4c277849a174a8036fe57401daf581
beep boop <githubbot@vadim.me>
2024-07-19 23:03:13 +0200

    Solution 1 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/sok6xucr Prompt:       |                             ^~~ 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp:18:33: error: expected primary-expressi on before ‘char’    18 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp:18:38: error: expected primary-expressi on before ‘int’    18 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp:18:44: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp:17: 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp: In function ‘int main()’: 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp:32:20: error: ‘histogram’ was not decla red in this scope    32 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 5afa4c7c-fec7-49ca-8507-59b2d934c4e4.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -13,3 +13,11 @@ map<char, int> histogram(string test) {
 
     return result;
 }
+
+bool issame(map<char,int> a,map<char,int> b){
+    if(a.size() != b.size()) return false;
+    for(auto p : a) {
+        if(b.find(p.first) == b.end() || b[p.first] != p.second) return false;
+    }
+    return true;
+}
