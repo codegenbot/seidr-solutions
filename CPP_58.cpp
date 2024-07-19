704dadfc1dec43bcdc2d961298bc2a58e5c33a92
beep boop <githubbot@vadim.me>
2024-07-19 23:06:17 +0200

    Solution 97 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ufmmdvbx Prompt:       |                                  ^~~ 6a7708e5-9959-47eb-a437-1aa529444748.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 6a7708e5-9959-47eb-a437-1aa529444748.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 6a7708e5-9959-47eb-a437-1aa529444748.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from 6a7708e5-9959-47eb-a437-1aa529444748.cpp:17: 6a7708e5-9959-47eb-a437-1aa529444748.cpp: In function ‘int main()’: 6a7708e5-9959-47eb-a437-1aa529444748.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 6a7708e5-9959-47eb-a437-1aa529444748.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,7 +2,14 @@
 #include <set>
 #include <algorithm>
 
-bool common(std::vector<int> a, std::vector<int> b) {
+bool areSetsEqual(std::vector<int> a, std::vector<int> b) {
+    std::set<int> s1(a.begin(), a.end());
+    std::set<int> s2(b.begin(), b.end());
+
+    return (s1 == s2 || s1.issubset(s2) || s2.issubset(s1));
+}
+
+int common(std::vector<int> a, std::vector<int> b) {
     std::set<int> s1(a.begin(), a.end());
     std::set<int> s2(b.begin(), b.end());
 
@@ -10,6 +17,6 @@ bool common(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(common({4, 3, 2, 8}, {}) == true); 
+    assert(common({4, 3, 2, 8}, {}) == false);
     return 0;
 }
