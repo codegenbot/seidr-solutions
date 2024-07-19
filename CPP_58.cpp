90c81576dfd7524aa96e8ebcb62d2d0af974ac86
beep boop <githubbot@vadim.me>
2024-07-19 04:15:04 +0200

    Solution 12 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/8m9vrwut Prompt:       |                    ^~~ f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp:12: f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp: In function ‘int main()’: f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f7f89e1c-c61d-4fc6-a098-9fc377058559.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,18 +3,18 @@
 #include <iterator>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
+std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
 
-    vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    std::vector<int> result;
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
 
-    result.erase(unique(result.begin(), result.end()), result.end());
+    result.erase(std::unique(result.begin(), result.end()), result.end());
 
     return result;
 }
