067e8c1db0a3bf750b1a055740e22a865b79264e
beep boop <githubbot@vadim.me>
2024-07-20 01:41:44 +0200

    Solution 60 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/u46z59hv Prompt: 6253af99-abc3-427e-afed-b4044f05fa78.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 6253af99-abc3-427e-afed-b4044f05fa78.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 6253af99-abc3-427e-afed-b4044f05fa78.cpp:29: 6253af99-abc3-427e-afed-b4044f05fa78.cpp: In function ‘int main()’: 6253af99-abc3-427e-afed-b4044f05fa78.cpp:31:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    31 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,5 +1,5 @@
-int main() {
-    assert(intToMiniRoman(4) == "IV");
-    cout << intToMiniRoman(1000) << endl;
+int main() { 
+    assert(intToMiniRoman(1000) == "M");
+    cout << intToMiniRoman(4) << endl;
     return 0;
 }
