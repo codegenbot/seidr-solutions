aca5812315c0bd7a9a8d642ce8890d8de81c4070
beep boop <githubbot@vadim.me>
2024-07-19 23:09:47 +0200

    Solution 39 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4vin7oy3 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 5e1c311e-7339-493b-885d-cf96d30863cf.cpp:16:65:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,7 +1,6 @@
 ```cpp
 #include <iostream>
 #include <iomanip>
-#include <string>
 
 using namespace std;
 
@@ -11,16 +10,10 @@ bool any_int(float a, float b, float c) {
 
 int main() {
     float a, b, c;
-    cout << "Enter three strings: ";
-    string str1 = to_string(a);
-    string str2 = to_string(b);
-    string str3 = to_string(c);
-    cin >> str1 >> str2 >> str3;
-    a = stod(str1);  
-    b = stod(str2);
-    c = stod(str3);
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
     if(any_int(a, b, c))
-        cout << "The numbers are integers: " << a << ", " << b << ", " << c << "\n";
+        cout << "The numbers are integers: " << int(a) << ", " << int(b) << ", " << int(c) << "\n";
     else
         cout << "The numbers are not integers: " << a << ", " << b << ", " << c << "\n";
     return 0;
