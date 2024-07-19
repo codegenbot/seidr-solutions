6037e8b7441ab30ee9e51c615ee77456197af07c
beep boop <githubbot@vadim.me>
2024-07-19 19:48:55 +0200

    Solution 6 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ttrkppob Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 17cb3430-c722-4c0d-97b5-2019da655d72.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 17cb3430-c722-4c0d-97b5-2019da655d72.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 17cb3430-c722-4c0d-97b5-2019da655d72.cpp:27: 17cb3430-c722-4c0d-97b5-2019da655d72.cpp: In function ‘int main()’: 17cb3430-c722-4c0d-97b5-2019da655d72.cpp:37:20: error: ‘select_words’ was not de clared in this scope    37 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 17cb3430-c722-4c0d-97b5-2019da655d72.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,15 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
+    return a == b;
 }
 
-vector<string> select_words(string s, int n);
+vector<string> select_words(string s, int n) {
+    vector<string> result;
+    string word = "";
+    int consonants = 0;
+    
+    for (char c : s) {
+        if (c != ' ') {
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                consonants++;
+            }
+            word += c;
+        } else {
+            if (consonants == n) {
+                result.push_back(word);
+            }
+            word = "";
+            consonants = 0;
+        }
+    }
+    
+    if (consonants == n) {
+        result.push_back(word);
+    }
+    
+    return result;
+}
+
+int main() {
+    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
+    
+    cout << "Test cases pass!";
+    
+    return 0;
+}
