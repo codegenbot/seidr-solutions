2d1e435198f08adf4bcc9560d26014f5a2169292
beep boop <githubbot@vadim.me>
2024-07-19 20:17:37 +0200

    Solution 13 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tukd3xas Prompt:   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6bf780c9-4db0-4453-865f-a840f54f1e3f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6bf780c9-4db0-4453-865f-a840f54f1e3f.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 6bf780c9-4db0-4453-865f-a840f54f1e3f.cpp:31: 6bf780c9-4db0-4453-865f-a840f54f1e3f.cpp: In function ‘int main()’: 6bf780c9-4db0-4453-865f-a840f54f1e3f.cpp:41:20: error: ‘order_by_points’ was not  declared in this scope    41 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 6bf780c9-4db0-4453-865f-a840f54f1e3f.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 vector<int> order_by_points(vector<int> nums);
 
+bool issame(vector<int> a, vector<int> b);
+
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
@@ -33,4 +35,5 @@ vector<int> order_by_points(vector<int> nums) {
 
 int main() {
     assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), vector<int>{-76, -21, 0, 4, 23, 6, 6}));
+    return 0;
 }
