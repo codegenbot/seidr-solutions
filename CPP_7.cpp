3f0d9dbbc3f9fd0f275b544e87d37749f6407a31
beep boop <githubbot@vadim.me>
2024-07-19 13:40:20 +0200

    Solution 75 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6dh2sbiq Prompt:     9 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5129d769-b169-44be-b69a-9782606c2303.cpp:9:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 5129d769-b169-44be-b69a-9782606c2303.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5129d769-b169-44be-b69a-9782606c2303.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5129d769-b169-44be-b69a-9782606c2303.cpp:9:44: error: ‘b’ was not declared in th is scope     9 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 5129d769-b169-44be-b69a-9782606c2303.cpp:8: 5129d769-b169-44be-b69a-9782606c2303.cpp: In function ‘int main()’: 5129d769-b169-44be-b69a-9782606c2303.cpp:18:20: error: ‘filter_by_substring’ was  not declared in this scope    18 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,3 +1,22 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
+}
+
+void filter_by_substring(std::vector<std::string>& vec, const std::string& substr) {
+    vec.erase(std::remove_if(vec.begin(), vec.end(), [substr](const std::string& s) {
+        return s.find(substr) != std::string::npos;
+    }), vec.end());
+}
+
+int main() {
+    std::vector<std::string> words = {"apple", "banana", "orange", "grape"};
+    filter_by_substring(words, "an");
+    for (const auto& word : words) {
+        std::cout << word << " ";
+    }
+    return 0;
 }
