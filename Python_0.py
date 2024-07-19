f106465d21cdee0bcea58664cd8ea8d5b10b267f
beep boop <githubbot@vadim.me>
2024-07-19 04:29:39 +0200

    Solution 37 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/iickrqpt Prompt: True Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a9ec0af-c8e7-4604-9fbc-fbb67cf38 96b.py", line 15, in <module>     check(has_close_elements)           ^^^^^^^^^^^^^^^^^^ NameError: name 'has_close_elements' is not defined

--- Python_0.py
+++ Python_0.py
@@ -1,1 +1,10 @@
+def check_numbers(numbers, threshold):
+    numbers.sort()
+    return any(
+        abs(numbers[i] - numbers[i + 1]) < threshold for i in range(len(numbers) - 1)
+    )
+
+numbers = [4, 2, 1, 3]
+threshold = 2
+result = check_numbers(numbers, threshold)
 print(result)
