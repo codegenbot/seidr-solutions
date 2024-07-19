84e6e9018284d585edc1265b05b33e9cde7d977b
beep boop <githubbot@vadim.me>
2024-07-19 18:13:26 +0200

    Solution 5 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4nydrkb3 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 42b969f7-ea00-4d6c-b601-a579e9ce32a9.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -2,25 +2,31 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> reverse_delete(const std::string& s1, const std::string& s2) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if(a.size() != b.size())
+        return false;
+    for(int i = 0; i<a.size(); i++)
+        if(a[i] != b[i])
+            return false;
+    return true;
+}
+
+std::vector<std::string> reverse_delete(const std::string& s, const std::string& t) {
     std::vector<std::string> result;
-    for (int i = 0; i < s1.size(); i++) {
-        if (s1[i] != s2[i]) {
-            result.push_back(std::string(1, s1[i]));
+    for(int i = 0; i < s.size(); i++) {
+        if(std::find(t.begin(), t.end(), s[i]) == t.end()) {
+            result.push_back(s.substr(i));
+            break;
+        }
+        else if(i == s.size() - 1) {
+            result.push_back(s);
         }
     }
     return result;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++)
-        if (a[i] != b[i])
-            return false;
-    return true;
-}
-
 int main() {
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
+    std::vector<std::string> a = reverse_delete("mamma", "mia");
+    std::vector<std::string> b = {"", "True"};
+    assert(issame(a, b));
 }
