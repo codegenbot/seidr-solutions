c2c0b0860a9853c828d7aacde0096e0f6b4a55a8
beep boop <githubbot@vadim.me>
2024-07-19 21:35:47 +0200

    Solution 3 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/eoo9avp7 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from c83927c7-5891-4481-b644-8e7e2af9bf96.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,26 +1,22 @@
+```cpp
 #include <vector>
-#include <cmath>
+#include <algorithm>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
+    return a == b;
 }
 
-int main() {
-    vector<float> l = {1.0f, -2.0f, 3.0f, -4.0f};
-    vector<float> result = get_positive(l);
-    
-    for (float num : result) {
-        cout << num << " ";
-    }
-    return 0;
-}
-
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
+}
+
+int main() {
+    assert(issame(get_positive({}), {}));
+    return 0;
 }
