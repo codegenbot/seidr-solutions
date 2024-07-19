5f8caa6606647e7bb4c45c48146ba55176d0739c
beep boop <githubbot@vadim.me>
2024-07-19 20:47:34 +0200

    Solution 60 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5qgavfh3 Prompt: d96520b9-f256-46b7-82d5-c5f14a6f5384.cpp:29:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    29 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ d96520b9-f256-46b7-82d5-c5f14a6f5384.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ d96520b9-f256-46b7-82d5-c5f14a6f5384.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ d96520b9-f256-46b7-82d5-c5f14a6f5384.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main(){       |     ^~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -3,11 +3,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-vector<int> sum_product(vector<int> numbers){
+vector<int> sum_product_and_product(vector<int> numbers){
     int sum = 0;
     int product = 1;
     
@@ -19,8 +15,12 @@ vector<int> sum_product(vector<int> numbers){
     return {sum, product};
 }
 
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 int main(){
-    assert(issame(sum_product({10}), {10, 10}));
+    assert(issame(sum_product_and_product({10}), {10, 10}));
     
     return 0;
 }
