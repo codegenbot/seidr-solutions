2ea1adac99991d73c40cb0641c0199c4d5e05ffb
beep boop <githubbot@vadim.me>
2024-07-19 07:02:52 +0200

    Solution 34 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/shrxd0mj Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,24 +1,7 @@
-#include <string>
-#include <iostream>
+int num1 = stoi(x.substr(0, x.find('/')));
+    int den1 = stoi(x.substr(x.find('/') + 1));
+    int num2 = stoi(n.substr(0, n.find('/')));
+    int den2 = stoi(n.substr(n.find('/') + 1));
 
-bool simplify(std::string x, std::string n) {
-    int numX = std::stoi(x.substr(0, x.find('/')));
-    int denX = std::stoi(x.substr(x.find('/') + 1));
-    int numN = std::stoi(n.substr(0, n.find('/')));
-    int denN = std::stoi(n.substr(n.find('/') + 1));
-
-    return (numX * numN) % (denX * denN) == 0;
-}
-
-int main() {
-    std::string x, n;
-    std::cin >> x >> n;
-
-    if (simplify(x, n)) {
-        std::cout << "true" << std::endl;
-    } else {
-        std::cout << "false" << std::endl;
-    }
-
-    return 0;
+    return (num1 * den2 == num2 * den1);
 }
