52ab58f1221b3caf9d265fd3425a204b8270647d
beep boop <githubbot@vadim.me>
2024-07-20 01:50:44 +0200

    Solution 79 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/22o47gku Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 83106a55-2f58-4099-94cd-cbc58359e40c.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 83106a55-2f58-4099-94cd-cbc58359e40c.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 83106a55-2f58-4099-94cd-cbc58359e40c.cpp:11: 83106a55-2f58-4099-94cd-cbc58359e40c.cpp: In function ‘int main()’: 83106a55-2f58-4099-94cd-cbc58359e40c.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 83106a55-2f58-4099-94cd-cbc58359e40c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <boost/any.hpp>
 #include <vector>
 #include <list>
@@ -10,18 +9,8 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value).type() == typeid(int)) {
-            int num = boost::any_cast<int>(value);
-            bool found = false;
-            for (int i : result) {
-                if (i == num) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                result.push_back(num);
-            }
+        if (boost::holds_alternative<int>(value)) {
+            result.push_back(boost::get<int>(value));
         }
     }
     return result;
