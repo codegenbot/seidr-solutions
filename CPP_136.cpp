db5a8ad000a5d2520d4ed5a4c890fd1bd5266374
beep boop <githubbot@vadim.me>
2024-07-19 20:21:44 +0200

    Solution 54 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/a5fpriio Prompt: on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3002ee8f-776b-4689-add5-172331641399.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 3002ee8f-776b-4689-add5-172331641399.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 3002ee8f-776b-4689-add5-172331641399.cpp:34: 3002ee8f-776b-4689-add5-172331641399.cpp: In function ‘int main()’: 3002ee8f-776b-4689-add5-172331641399.cpp:44:45: error: too few arguments to func tion ‘std::vector<int> largest_smallest_integers(std::vector<int>, std::vector<i nt>)’    44 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~ 3002ee8f-776b-4689-add5-172331641399.cpp:9:18: note: declared here     9 | std::vector<int> largest_smallest_integers(std::vector<int> a, std::vect or<int> b) {       |                  ^~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -11,7 +11,14 @@ std::vector<int> largest_smallest_integers(std::vector<int> a, std::vector<int> 
     for (int num : a) {
         if (num < 0 && num < largest_negative) {
             largest_negative = num;
-        } else if (num > 0 && (num < smallest_positive || smallest_positive < 0)) {
+        } else if (num > 0 && num < smallest_positive) {
+            smallest_positive = num;
+        }
+    }
+    for (int num : b) {
+        if (num < 0 && num < largest_negative) {
+            largest_negative = num;
+        } else if (num > 0 && num < smallest_positive) {
             smallest_positive = num;
         }
     }
@@ -19,7 +26,7 @@ std::vector<int> largest_smallest_integers(std::vector<int> a, std::vector<int> 
 }
 
 int main() {
-    std::vector<int> result = largest_smallest_integers({-6, -4, -4, -3, -100, 1}, {});
-    assert(issame(result, {-100, 1}));
+    std::vector<int> result = largest_smallest_integers({-6, -4, -4, -3, -100, 1}, {5, 7, -3, 9});
+    assert(issame(result, {-100, -3}));
     return 0;
 }
