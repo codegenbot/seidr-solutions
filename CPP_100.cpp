3f8dfb362146f1724e1359fa2f68aa553948a011
beep boop <githubbot@vadim.me>
2024-07-19 21:57:10 +0200

    Solution 64 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jebzfru9 Prompt:       |             ^~~~~~ b63f4d9c-211d-476c-abb6-cbf367123de1.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b63f4d9c-211d-476c-abb6-cbf367123de1.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ b63f4d9c-211d-476c-abb6-cbf367123de1.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from b63f4d9c-211d-476c-abb6-cbf367123de1.cpp:35: b63f4d9c-211d-476c-abb6-cbf367123de1.cpp: In function ‘int main()’: b63f4d9c-211d-476c-abb6-cbf367123de1.cpp:45:20: error: ‘make_a_pile’ was not dec lared in this scope    45 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ b63f4d9c-211d-476c-abb6-cbf367123de1.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -4,17 +4,14 @@
 std::vector<int> make_a_pile(int n) {
     std::vector<int> stones;
     stones.push_back(n);
-    
     for (int i = 1; i < n; ++i) {
         if (n % 2 == 0) {
             n += 2;
         } else {
             n += 1;
         }
-        
         stones.push_back(n);
     }
-    
     return stones;
 }
 
@@ -22,13 +19,11 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
 
