045460b005ad9a9c693c332cc066d4ec49e90bd3
beep boop <githubbot@vadim.me>
2024-07-19 18:26:28 +0200

    Solution 35 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xrtgbsd6 Prompt:  in this scope    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 750fad89-2d0c-40ae-aad7-ec5058f7f8b2.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 750fad89-2d0c-40ae-aad7-ec5058f7f8b2.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 750fad89-2d0c-40ae-aad7-ec5058f7f8b2.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 750fad89-2d0c-40ae-aad7-ec5058f7f8b2.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -5,16 +5,16 @@ bool areSame(const std::vector<float>& a, const std::vector<float>& b) {
     return a == b;
 }
 
-std::vector<float> sortEven(std::vector<float> l){
-    std::vector<float> l_prime;
+std::vector<float> sort_even(std::vector<float> l){
+    std::vector<float> result;
     for(int i = 0; i < l.size(); i++){
         if(i % 2 == 0){
-            std::vector<float> even_elements(l.begin() + i, l.end());
+            std::vector<float> even_elements(l.begin() + i, l.begin() + i+1);
             std::sort(even_elements.begin(), even_elements.end());
-            l_prime.push_back(*std::min_element(even_elements.begin(), even_elements.end()));
+            result.push_back(*std::min_element(even_elements.begin(), even_elements.end()));
         } else {
-            l_prime.push_back(l[i]);
+            result.push_back(l[i]);
         }
     }
-    return l_prime;
+    return result;
 }
