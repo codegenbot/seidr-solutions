f187a92462880ad3c83ef61143384c179a58cb3d
beep boop <githubbot@vadim.me>
2024-07-19 11:23:10 +0200

    Solution 74 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/qlb3aq2h Prompt:       |                              ^~~~~~ d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp:29: d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp: In function ‘int main()’: d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ d7e6f3b7-e554-48c4-a014-0016e87018ee.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,11 +1,37 @@
-#include <string>
+#include <string> // include this for string declaration
 
 bool issame(vector<string> a, vector<string> b){
     // function implementation
     return true;
 }
 
-vector<string> split_words(string txt);
+vector<string> split_words(string txt){
+    vector<string> result;
+    string word = "";
+    for(char c : txt){
+        if(c == ' ' || c == ','){
+            if(!word.empty()){
+                result.push_back(word);
+                word = "";
+            }
+        } else {
+            word += c;
+        }
+    }
+    if(!word.empty()){
+        result.push_back(word);
+    }
+    if(result.size() == 0){
+        int odd_count = 0;
+        for(char c : txt){
+            if(islower(c) && (c - 'a') % 2 == 1){
+                odd_count++;
+            }
+        }
+        result.push_back(to_string(odd_count));
+    }
+    return result;
+}
 
 int main(){
     assert(issame(split_words(""), {"0"}));
