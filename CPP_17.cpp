7b06cfed38bd4d60ea018cf6bcc1d03382aa267f
beep boop <githubbot@vadim.me>
2024-07-19 13:50:52 +0200

    Solution 1 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/e7hzo27n Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 667868ba-879d-40cc-a180-a31b0fdfa61a.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 667868ba-879d-40cc-a180-a31b0fdfa61a.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 667868ba-879d-40cc-a180-a31b0fdfa61a.cpp:19: 667868ba-879d-40cc-a180-a31b0fdfa61a.cpp: In function ‘int main()’: 667868ba-879d-40cc-a180-a31b0fdfa61a.cpp:29:20: error: ‘parse_music’ was not dec lared in this scope    29 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 667868ba-879d-40cc-a180-a31b0fdfa61a.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+vector<int> parse_music(string music_string);
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 vector<int> parse_music(string music_string){
     vector<int> beats;
     int i = 0;
@@ -14,4 +24,9 @@ vector<int> parse_music(string music_string){
         }
     }
     return beats;
+}
+
+int main(){
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
+    return 0;
 }
