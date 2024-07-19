7d9d2f9188957fba18dd2fb7f0c0e2f97c15e600
beep boop <githubbot@vadim.me>
2024-07-20 01:57:49 +0200

    Solution 87 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0vyyuw2q Prompt:       |                    ^~~~~ d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp:11: d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp: In function ‘int main()’: d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ d51391a4-18dc-491a-bddc-b160ac6d9c8b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,12 +1,27 @@
 #include <vector>
 #include <iostream>
 
-std::vector<float> get_positive(std::vector<float> l){
-    std::vector<float> result;
+using namespace std;
+
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
     for(float i : l) {
         if(i > 0) {
             result.push_back(i);
         }
     }
     return result;
+}
+
+int main() {
+    vector<float> numbers = {1, -2, 3, -4, 5};
+    vector<float> positive_numbers = get_positive(numbers);
+
+    cout << "Positive numbers: ";
+    for(float i : positive_numbers) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
