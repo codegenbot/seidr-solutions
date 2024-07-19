c860b2fcd096cd04a848c003aeb530a0aa414da6
beep boop <githubbot@vadim.me>
2024-07-19 22:04:24 +0200

    Solution 20 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6dg7d3f0 Prompt:       |                    ^~~ 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp:12: 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp: In function ‘int main()’: 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp:22:20: error: ‘get_odd_collatz’ was not  declared in this scope    22 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 77452d6e-fd1d-449e-a496-68a1fc9c8b57.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,23 +1,19 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> fixCode(int n) {
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
     while (n != 1) {
-        result.push_back(n);
-        if (n % 2 == 0) {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
+        if (n % 2 != 0) {
+            result.push_back(n);
         }
+        n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
-    std::vector<int> odd_result;
-    for (int num : result) {
-        if (num % 2 != 0) {
-            odd_result.push_back(num);
-        }
-    }
-    std::sort(odd_result.begin(), odd_result.end());
-    return odd_result;
+    std::sort(result.begin(), result.end());
+    return result;
 }
