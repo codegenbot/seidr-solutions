dc2ea05999c75e64c4f62975b22deb98cd2396b7
beep boop <githubbot@vadim.me>
2024-07-19 18:39:40 +0200

    Solution 41 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/krf9kxcb Prompt:       |                                  ^~~ ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp:41:39: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<int> a,vector<int>b){       |                                       ^ ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp:40: ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp: In function ‘int main()’: ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp:50:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    50 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ ca13b8e3-25ae-4da8-83dd-6e3296f57d17.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -27,8 +27,8 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
 }
 
 int main() {
-    std::vector<int> a = {1, 2, 3};
-    std::vector<int> b = strange_sort_vector(a);
+    std::vector<int> a = strange_sort_vector({1, 2, 3});
+    std::vector<int> b = strange_sort_vector({3, 2, 1});
     if (a != b) {
         for (size_t i = 0; i < a.size(); ++i) {
             assert(a[i] == b[i]);
