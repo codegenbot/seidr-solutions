d64eb186f39e2d79c50d3a64740f4500e4373b6e
beep boop <githubbot@vadim.me>
2024-07-19 19:28:55 +0200

    Solution 14 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/r4qp91p3 Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from d1128c0c-32b8-43d8-894a-0fa3d245cb99.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d1128c0c-32b8-43d8-894a-0fa3d245cb99.cpp:42:44: error: ‘b’ was not declared in t his scope    42 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d1128c0c-32b8-43d8-894a-0fa3d245cb99.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ d1128c0c-32b8-43d8-894a-0fa3d245cb99.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main(){       |     ^~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,28 +4,26 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
+bool issame(std::vector<std::string> a, std::vector<std::string> b){
     return a == b;
 }
 
-vector<string> by_length(vector<int> arr){
-    vector<string> result;
-    map<int, string> numToString = {
+std::vector<std::string> by_length(std::vector<int> arr){
+    std::vector<std::string> result;
+    std::map<int, std::string> numToString = {
         {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
         {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
     
-    vector<int> filtered;
+    std::vector<int> filtered;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             filtered.push_back(num);
         }
     }
     
-    sort(filtered.begin(), filtered.end());
-    reverse(filtered.begin(), filtered.end());
+    std::sort(filtered.begin(), filtered.end());
+    std::reverse(filtered.begin(), filtered.end());
     
     for (int num : filtered) {
         result.push_back(numToString[num]);
