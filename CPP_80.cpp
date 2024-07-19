a94e3379905be3102b4d5f2f1f28f727bda55633
beep boop <githubbot@vadim.me>
2024-07-19 22:12:04 +0200

    Solution 4 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/unqcf6rv Prompt: 803c7aa5-b165-466e-bde9-85e969da7a73.cpp:3:15: error: ‘string’ was not declared in this scope     3 | bool is_happy(string s) {       |               ^~~~~~ 803c7aa5-b165-466e-bde9-85e969da7a73.cpp:3:15: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 803c7aa5-b165-466e-bde9-85e969da7a73.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 803c7aa5-b165-466e-bde9-85e969da7a73.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 803c7aa5-b165-466e-bde9-85e969da7a73.cpp:26: 803c7aa5-b165-466e-bde9-85e969da7a73.cpp: In function ‘int main()’: 803c7aa5-b165-466e-bde9-85e969da7a73.cpp:28:21: error: ‘is_happy’ cannot be used  as a function    28 |     assert (is_happy("iopaxioi") == false );       |             ~~~~~~~~^~~~~~~~~~~~

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,10 +1,8 @@
-using namespace std;
-
-bool is_happy(string s) {
+bool is_happy(std::string s) {
     if (s.length() < 3)
         return false;
     for (int i = 0; i <= s.length() - 3; i++) {
-        string sub = s.substr(i, 3);
+        std::string sub = s.substr(i, 3);
         bool unique = true;
         for (char c : sub) {
             int count = 0;
