e88de0e63bd3d42eb8cf13dc8c1f8438b223995d
beep boop <githubbot@vadim.me>
2024-07-19 14:12:00 +0200

    Solution 22 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/e7hzo27n Prompt: on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a4534e7d-70c7-4be4-a86d-529793668a93.cpp:30:39: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a4534e7d-70c7-4be4-a86d-529793668a93.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ a4534e7d-70c7-4be4-a86d-529793668a93.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from a4534e7d-70c7-4be4-a86d-529793668a93.cpp:29: a4534e7d-70c7-4be4-a86d-529793668a93.cpp: In function ‘int main()’: a4534e7d-70c7-4be4-a86d-529793668a93.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <cassert>
 
-bool is_same(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
@@ -26,6 +26,6 @@ std::vector<int> parse_music(std::string music_string) {
 }
 
 int main() {
-    assert(is_same(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
     return 0;
 }
