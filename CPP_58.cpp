1ecc1fcb5ca8f3987de7e0f2cf60895fc4bbd0e1
beep boop <githubbot@vadim.me>
2024-07-19 21:22:52 +0200

    Solution 17 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mq8vqgyj Prompt:       |                    ^~~ 0603df1d-3659-4e62-a447-45a0d2924385.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0603df1d-3659-4e62-a447-45a0d2924385.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0603df1d-3659-4e62-a447-45a0d2924385.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0603df1d-3659-4e62-a447-45a0d2924385.cpp:9: 0603df1d-3659-4e62-a447-45a0d2924385.cpp: In function ‘int main()’: 0603df1d-3659-4e62-a447-45a0d2924385.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 0603df1d-3659-4e62-a447-45a0d2924385.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> issame(std::vector<int> l1, std::vector<int> l2){
-sort(l1.begin(), l1.end());
-sort(l2.begin(), l2.end());
-std::vector<int> result;
-std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-result.erase(std::unique(result.begin(), result.end()), result.end());
-return result;
+std::vector<int> issame(std::vector<int> l1, std::vector<int> l2) {
+    std::sort(l1.begin(), l1.end());
+    std::sort(l2.begin(), l2.end());
+    std::vector<int> result;
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    result.erase(std::unique(result.begin(), result.end()), result.end());
+    return result;
 }
