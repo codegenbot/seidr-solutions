fb67a98e57e4d727b3c7cc1f910c49f890f45529
beep boop <githubbot@vadim.me>
2024-07-19 19:45:38 +0200

    Solution 42 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bawfox83 Prompt: Complete the following code given the task description and function signature.

--- Python_93.py
+++ Python_93.py
@@ -5,8 +5,10 @@ def encode(message):
         if char.isalpha():
             if char.lower() in 'aeiou':
                 result += chr((ord(char) - 97 + 2) % 26 + 97)
+            elif char.isupper():
+                result += chr((ord(char) - 65 + 2) % 26 + 65)
             else:
-                result += char.swapcase()
+                result += char
         else:
             result += char
     return result
