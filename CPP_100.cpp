5486ff1324b453bdc33f67ea3662aaf381aee046
beep boop <githubbot@vadim.me>
2024-07-19 21:56:05 +0200

    Solution 56 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jebzfru9 Prompt:       |             ^~~~~~ 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp:35: 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp: In function ‘int main()’: 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp:45:20: error: ‘make_a_pile’ was not dec lared in this scope    45 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 01968bea-9bc4-4115-babb-0ebb7b99fe6d.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,9 +1,20 @@
 #include <vector>
 #include <cassert>
-using namespace std;
 
-vector<int> make_a_pile(int n) {
-    vector<int> stones;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::vector<int> make_a_pile(int n) {
+    std::vector<int> stones;
     stones.push_back(n);
     for (int i = 1; i < n; ++i) {
         if (n % 2 == 0) {
@@ -16,19 +27,7 @@ vector<int> make_a_pile(int n) {
     return stones;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    assert(issame(make_a_pile(8), vector<int>{8, 10, 12, 14, 16, 18, 20, 22}));
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
     return 0;
 }
