d597b430447ab0ef441d5a19b5d7249aecac4d9c
beep boop <githubbot@vadim.me>
2024-07-19 03:49:09 +0200

    Solution 15 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/9xcpuj31 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:5: fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp: In function ‘int main()’: fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ fcc80e54-cac7-4fdd-a1d4-1b6c27202af8.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,29 +1,25 @@
+
+vector<int> minPath(vector<vector<int>> grid, int k){
+    int n = grid.size();
+    vector<int> path;
+    for (int i = 0; i < n; ++i) {
+        path.push_back(grid[i][0]);
+    }
+    return path;
+}
+
 bool issame(vector<int> path1, vector<int> path2){
-        if(path1.size() != path2.size()){
-            return false;
-        }
-        
-        for(int i=0; i<path1.size(); i++){
-            if(path1[i] != path2[i]){
-                return false;
-            }
-        }
-        
-        return true;
-    }
+    return path1 == path2;
+}
 
-    vector<int> minPath(vector<vector<int>> grid, int k){
-        vector<int> path;
-        
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        // Add your code here to find the minimum path
-        
-        return path;
-    }
+int main(){
+    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int k = 3;
 
-    int main() {
-        // Add code to read input, call minPath function and then call issame function
-        return 0;
-    }
+    vector<int> result = minPath(grid, k);
+
+    vector<int> expected = {1, 4, 7};
+    bool same = issame(result, expected);
+
+    return 0;
+}
