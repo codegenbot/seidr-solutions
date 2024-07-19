7e7fb85a456893fa4ff9425d8107e5d36dc6e885
beep boop <githubbot@vadim.me>
2024-07-19 21:06:55 +0200

    Solution 19 of CPP/0, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/664ehzue Prompt: 3a2713ca-eeb9-47d5-8431-6e9066bb1345.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 3a2713ca-eeb9-47d5-8431-6e9066bb1345.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -4,15 +4,6 @@ using namespace std;
 
 bool has_close_elements(vector<float> numbers, float threshold);
 
-int main() {
-    vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-    if(has_close_elements(a, 0.1f)) {
-        cout << "The vector contains close elements." << endl;
-    } else {
-        cout << "The vector does not contain close elements." << endl;
-    }
-}
-
 bool has_close_elements(vector<float> numbers, float threshold) {
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
