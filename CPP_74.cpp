1f839e0afc8c4130387c7818d484691dbbfdfb60
beep boop <githubbot@vadim.me>
2024-07-19 03:12:52 +0200

    Solution 85 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/duyg3to1 Prompt:    41 | int main(){       |     ^~~~ 155c05a3-57ac-4739-9627-b33a3d5725da.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 155c05a3-57ac-4739-9627-b33a3d5725da.cpp:32: 155c05a3-57ac-4739-9627-b33a3d5725da.cpp: In function ‘int main()’: 155c05a3-57ac-4739-9627-b33a3d5725da.cpp:42:31: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    42 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ~~~~~~~~~~~^~~~~~~~~~~~~~       |                               |       |                               const char* 155c05a3-57ac-4739-9627-b33a3d5725da.cpp:19:43: note:   initializing argument 1 of ‘int total_match(int, int)’    19 | vector<string> total_match(vector<string> lst1, vector<string> lst2) {       |                            ~~~~~~~~~~~~~~~^~~~ 155c05a3-57ac-4739-9627-b33a3d5725da.cpp: In function ‘int total_chars(int)’: 155c05a3-57ac-4739-9627-b33a3d5725da.cpp:13:1: warning: control reaches end of n on-void function [-Wreturn-type]    13 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,31 +1,4 @@
-#include <vector>
-#include <cassert>
-
-using namespace std;
-
-vector<string> total_match(vector<string> lst1, vector<string> lst2);
-
-int total_chars(vector<string> lst) {
-    int total = 0;
-    for (string s : lst) {
-        total += s.length();
-    }
-    return total;
-}
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    if (total_chars(lst1) < total_chars(lst2)) {
-        return lst1;
-    } else {
-        return lst2;
-    }
-}
-
 int main() {
-    assert(issame(total_match({"this"}, {""}), {""}));
+    assert(issame(total_match({"this"}, {}), vector<string>{}));
     return 0;
 }
