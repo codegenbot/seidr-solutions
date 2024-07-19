4b3a874ad0e2946af315d8741e8505dd82ac9273
beep boop <githubbot@vadim.me>
2024-07-19 20:42:22 +0200

    Solution 13 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/oojp8dsm Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,23 +1,11 @@
 #include <string>
-#include <iostream>
-
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok((x).c_str(), "/"));
-    int b = stoi(strtok(NULL, "/"));
-    int c = stoi(strtok((n).c_str(), "/"));
-    int d = stoi(strtok(NULL, "/"));
+    long long int a = atoll(strtok(x.c_str(), "/")) * atoll(strtok(NULL, "/")) +
+            atoll(strtok(n.c_str(), "/")) * atoll(strtok(NULL, "/"));
+    long long int b = atoll(strtok(x.c_str(), "/")) * atoll(strtok(NULL, "/")) /
+                      atoll(strtok(n.c_str(), "/")) * atoll(strtok(NULL, "/"));
 
-    long long result = (long long)a*d + (long long)b*c;
-    long long commonDivisor = gcd(abs(result), abs(a*d));
-
-    return a*d / commonDivisor == b*c / commonDivisor;
-}
-
-long long gcd(long long a, long long b) {
-    if(b == 0)
-        return a;
-    else
-        return gcd(b, a%b);
+    return b == 1;
 }
