40a7b008fad7efb7023c3e8b3d5a9d124d1c3824
beep boop <githubbot@vadim.me>
2024-07-19 02:51:50 +0200

    Solution 2 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/n2xbtxty Prompt:       |                    ^~~ e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp:18: e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp: In function ‘int main()’: e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ e8b6ca59-1353-49b2-81f7-03e96dd82e35.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,18 +1,23 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
+vector<int> tri(int n) {
+    vector<int> result;
+    if (n >= 0) {
+        result.push_back(3);
+        if (n > 0) {
+            result.push_back(1);
+            for (int i = 2; i <= n; ++i) {
+                if (i % 2 == 0) {
+                    result.push_back(1 + i / 2);
+                } else {
+                    result.push_back(result[i - 1] + result[i - 2] + result[i + 1]);
+                }
+            }
         }
     }
-    return true;
+    return result;
 }
 
-int main() {
-    assert(issame(std::vector<int>{1, 3}, {1, 3}));
-    return 0;
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
 }
