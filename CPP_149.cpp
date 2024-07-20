f23d42c7ba18e0a18dcfcebf77e3b8bb855090fb
beep boop <githubbot@vadim.me>
2024-07-20 04:14:31 +0200

    Solution 6 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3ykkjyif Prompt: a2674979-ef08-4c16-8d66-5b21c93cd48c.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~ In file included from a2674979-ef08-4c16-8d66-5b21c93cd48c.cpp:30: a2674979-ef08-4c16-8d66-5b21c93cd48c.cpp: In function ‘int main()’: a2674979-ef08-4c16-8d66-5b21c93cd48c.cpp:40:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    40 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~ a2674979-ef08-4c16-8d66-5b21c93cd48c.cpp:8:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     8 | bool issame(const vector<string>& a, const vector<string>& b){       |      ^~~~~~ a2674979-ef08-4c16-8d66-5b21c93cd48c.cpp:31:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    31 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,15 +5,17 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
+bool issame(const vector<string>& a, const vector<string>& b);
 
 vector<string> sorted_list_sum(vector<string> lst);
 
 int main(){
     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
+}
+
+bool issame(const vector<string>& a, const vector<string>& b){
+    return a == b;
 }
 
 vector<string> sorted_list_sum(vector<string> lst){
