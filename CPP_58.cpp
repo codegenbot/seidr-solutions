61449cf6e766642750ed7282426fbe0f0e1431c0
beep boop <githubbot@vadim.me>
2024-07-20 05:45:50 +0200

    Solution 82 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gcmd9cu5 Prompt:       |                    ^~~ e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp:11: e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp: In function ‘int main()’: e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp:21:20: error: ‘common’ was not declared  in this scope    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ e6a2b0f5-dbd4-472f-9c1a-5c53da8e0ecd.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,10 +2,22 @@ bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); ++i) {
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
     return true;
+}
+
+vector<int> common(vector<int> l1, vector<int> l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
+
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+
+    result.erase(unique(result.begin(), result.end()), result.end());
+
+    return result;
 }
