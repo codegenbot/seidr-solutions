6f7fee598682fec8839c489c409537e5e149ac7e
beep boop <githubbot@vadim.me>
2024-07-20 03:24:19 +0200

    Solution 34 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/2atagc4i Prompt: 461acc70-e0d4-4837-87ce-4c470bd1cf02.cpp:65:5: error: redefinition of ‘int main( )’    65 | int main(){       |     ^~~~ 461acc70-e0d4-4837-87ce-4c470bd1cf02.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,4 +1,3 @@
-```
 #include <iostream>
 #include <vector>
 #include <string>
@@ -26,7 +25,7 @@ vector<string> select_words(string s, int n) {
 }
 
 int main() {
-    cout << "select_words(\"Mary had a little lamb\", 4) ==> \"{";
+    cout << "select_words(\"Mary had a little lamb\", 4) ==> \"";
     for (string s : select_words("Mary had a little lamb", 4)) {
         cout << "\"" << s << "\",";
     }
