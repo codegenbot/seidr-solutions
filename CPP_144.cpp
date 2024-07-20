e5ad86f129f74772cce329f357591505ce58f9e7
beep boop <githubbot@vadim.me>
2024-07-20 15:16:11 +0200

    Solution 51 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ax1djs0y Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,17 +1,7 @@
-int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
-
-    bool simplify(string x, string n){
-        int numerator1 = stoi(x.substr(0, x.find('/')));
-        int denominator1 = stoi(x.substr(x.find('/') + 1));
-        int numerator2 = stoi(n.substr(0, n.find('/')));
-        int denominator2 = stoi(n.substr(n.find('/') + 1));
-
-        int num = numerator1 * numerator2;
-        int den = denominator1 * denominator2;
-
-        int common = gcd(num, den);
-
-        return den / common == 1;
-    }
+int x_num = stoi(x.substr(0, x.find('/')));
+    int x_den = stoi(x.substr(x.find('/') + 1));
+    int n_num = stoi(n.substr(0, n.find('/')));
+    int n_den = stoi(n.substr(n.find('/') + 1));
+    
+    return (x_num * n_num) % (x_den * n_den) == 0;
+}
