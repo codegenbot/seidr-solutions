369a356e17ad9bc495d04a2314a66d7d1f609b35
beep boop <githubbot@vadim.me>
2024-07-20 15:05:39 +0200

    Solution 85 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4gclsr2g Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,30 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
+if(a.type() == typeid(int) && b.type() == typeid(int)) {
+        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
             return a;
-        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
+        } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
             return b;
+        } else {
+            return "None";
         }
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
+    } else if(a.type() == typeid(float) && b.type() == typeid(float)) {
+        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
             return a;
-        } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
+        } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
             return b;
+        } else {
+            return "None";
         }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
+    } else if(a.type() == typeid(string) && b.type() == typeid(string)) {
+        float value1 = stof(boost::any_cast<string>(a));
+        float value2 = stof(boost::any_cast<string>(b));
+        if(value1 > value2) {
             return a;
-        } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
+        } else if(value1 < value2) {
             return b;
+        } else {
+            return "None";
         }
+    } else {
+        return "None";
     }
-    return "None";
 }
