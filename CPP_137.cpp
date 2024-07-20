5ad29e22d290838fb7601a7c333bb922aed55ab6
beep boop <githubbot@vadim.me>
2024-07-20 03:39:43 +0200

    Solution 37 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y1nh82vq Prompt: 5a0ceebf-38b9-48d5-852c-b706ebfa0e42.cpp:4:9: fatal error: boost/any.hpp: No suc h file or directory     4 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,104 +2,33 @@
 #include <string>
 #include <algorithm>
 
-int compareOne(int a, float b) {
-    if (a > b) {
+using namespace std;
+
+string compare_one(string a, int b) {
+    if (a > to_string(b)) {
         return a;
     }
-    else if (a < b) {
-        return b;
+    else if (a < to_string(b)) {
+        return to_string(b);
     }
     else {
-        return a;
-    }
-}
-
-int compareOne(int a, std::string b) {
-    if (a > std::stoi(b)) {
-        return a;
-    }
-    else if (a < std::stoi(b)) {
-        return a;
-    }
-    else {
-        return a;
-    }
-}
-
-int compareOne(float a, int b) {
-    if (a > b) {
-        return (int)a;
-    }
-    else if (a < b) {
-        return b;
-    }
-    else {
-        return (int)a;
-    }
-}
-
-int compareOne(float a, std::string b) {
-    if (std::stof(b) > a) {
-        return (int)std::stof(b);
-    }
-    else if (std::stof(b) < a) {
-        return (int)a;
-    }
-    else {
-        return (int)a;
-    }
-}
-
-int compareOne(std::string a, int b) {
-    if (std::stoi(a) > b) {
-        return std::stoi(a);
-    }
-    else if (std::stoi(a) < b) {
-        return b;
-    }
-    else {
-        return std::stoi(a);
-    }
-}
-
-int compareOne(std::string a, float b) {
-    if (std::stof(a) > b) {
-        return std::stoi(a);
-    }
-    else if (std::stof(a) < b) {
-        return (int)b;
-    }
-    else {
-        return std::stoi(a);
-    }
-}
-
-int compareOne(std::string a, std::string b) {
-    if (std::stoi(a) > std::stoi(b)) {
-        return std::stoi(a);
-    }
-    else if (std::stoi(a) < std::stoi(b)) {
-        return std::stoi(b);
-    }
-    else {
-        return 0;
+        return "None";
     }
 }
 
 int main() {
-    int a, b;
-    std::cout << "Enter the first number: ";
-    std::cin >> a;
-    std::cout << "Enter the second number: ";
-    std::cin >> b;
-    if (a > b) {
-        std::cout << "The larger number is " << a << ".\n";
-    }
-    else if (a < b) {
-        std::cout << "The larger number is " << b << ".\n";
-    }
-    else {
-        std::cout << "Both numbers are the same.\n";
-    }
+    string inputA;
+    int inputB;
+
+    cout << "Enter the first value: ";
+    cin >> inputA;
+    
+    cout << "Enter the second value: ";
+    cin >> inputB;
+
+    string result = compare_one(inputA, inputB);
+
+    cout << "The comparison result is: " << result << endl;
+
     return 0;
 }
