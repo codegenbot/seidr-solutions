c26eefda9768f40f2fd0ad411187d29fefc90a6e
beep boop <githubbot@vadim.me>
2024-07-20 03:48:01 +0200

    Solution 44 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5z7o052u Prompt: 74f58040-934a-41e0-b89f-4621aa331058.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<int> lst){       |                 ^~~~~~ 74f58040-934a-41e0-b89f-4621aa331058.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int sum_squares(vector<int> lst){       |                        ^~~ In file included from 74f58040-934a-41e0-b89f-4621aa331058.cpp:10: 74f58040-934a-41e0-b89f-4621aa331058.cpp: In function ‘int main()’: 74f58040-934a-41e0-b89f-4621aa331058.cpp:12:24: error: ‘sum_squares’ cannot be u sed as a function    12 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int sum_squares(vector<int> lst){
+int sum_squares(vector<int> lst) {
     int result = 0;
     for(int i=0; i<lst.size(); i++){
         if(i%3==0 && i%4!=0) result += lst[i]*lst[i];
