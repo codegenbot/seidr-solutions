63ffe44c3f9dfe17eb5360b85b862f61aaed5a0d
beep boop <githubbot@vadim.me>
2024-07-20 02:55:30 +0200

    Solution 14 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5w4496gj Prompt: In file included from da03ec4b-303a-4784-9f66-16acb82a575c.cpp:21: da03ec4b-303a-4784-9f66-16acb82a575c.cpp:31:26: note:   candidate expects 4 argu ments, 1 provided    31 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from da03ec4b-303a-4784-9f66-16acb82a575c.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: candidate: ‘template <class _ExecutionPolicy, class _ForwardIterator> __pstl::__internal::__enable_if _execution_policy<_ExecutionPolicy, _ForwardIterator> std::unique(_ExecutionPoli cy&&, _ForwardIterator, _ForwardIterator)’   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note:   template argument deduction/substitution failed: In file included from da03ec4b-303a-4784-9f66-16acb82a575c.cpp:21: da03ec4b-303a-4784-9f66-16acb82a575c.cpp:31:26: note:   candidate expects 3 argu ments, 1 provided    31 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -13,7 +13,8 @@ int main() {
     vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     sort(l.begin(), l.end());
     
-    assert(issame(vector<int>(l.begin(), unique(l.begin(), l.end())), vector<int>({0, 2, 3, 5, 9, 123}));
+    vector<int> unique_l(l.begin(), unique(l.begin(), l.end()));
+    assert(issame(unique_l, {0, 2, 3, 5, 9, 123}));
     
     return 0;
 }
