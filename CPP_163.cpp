60702502960100a796d4e98dd3ada28850ce9bf6
beep boop <githubbot@vadim.me>
2024-07-20 04:46:44 +0200

    Solution 17 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gml63y5i Prompt:       |             ^~~~~~ 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp:23: 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp: In function ‘int main()’: 1f087aff-1067-4a20-abf1-3c8aac4b69d6.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-bool is_same(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
@@ -16,6 +16,6 @@ std::vector<int> generate_integers(int a, int b) {
 }
 
 int main() {
-    assert(is_same(generate_integers(17, 89), std::vector<int>{}));
+    assert(issame(generate_integers(17, 89), std::vector<int>{}));
     return 0;
 }
