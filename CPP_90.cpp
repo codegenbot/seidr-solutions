f2b0840bdeae2d4646e7f0682f0bc6c0a7526372
beep boop <githubbot@vadim.me>
2024-07-20 05:34:21 +0200

    Solution 86 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0z4ajnzm Prompt: Complete the following code given the task description and function signature.

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,11 +1,9 @@
-int next_smallest(vector<int> lst) {
-    if (lst.empty()) return -1; // returning -1 as per the problem statement it should return None
-    
-    vector<int> v(lst);
+int next_smallest(vector<int> lst){
+    if(lst.empty()) return -1; // 0 is also fine as per problem description
+    vector<int> v = lst;
     sort(v.begin(), v.end());
-    
     for(int i=0; i<v.size()-1; i++){
         if(v[i] < v[i+1]) return v[i];
     }
-    return -1;
+    return -1; // If all elements are equal, then return -1
 }
