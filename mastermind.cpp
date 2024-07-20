a58010689037e8faea58a8957e2980c2ca3ce35d
beep boop <githubbot@vadim.me>
2024-07-20 08:00:54 +0200

    Solution 84 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ko1ld40j Prompt:       |                         ^~~~~ 8bd74f80-9312-4cfe-adf6-43bb01994861.cpp:8:37: error: request for member ‘begin’  in ‘guess’, which is of non-class type ‘const int’     8 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                                     ^~~~~ 8bd74f80-9312-4cfe-adf6-43bb01994861.cpp:8:52: error: request for member ‘end’ i n ‘guess’, which is of non-class type ‘const int’     8 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                    ^~~ 8bd74f80-9312-4cfe-adf6-43bb01994861.cpp:8:63: error: invalid types ‘const int[i nt]’ for array subscript     8 |       } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                             ^ 8bd74f80-9312-4cfe-adf6-43bb01994861.cpp:13:26: error: expected ‘}’ at end of in put    13 |     return black + white;       |                          ^ 8bd74f80-9312-4cfe-adf6-43bb01994861.cpp:1:57: note: to match this ‘{’     1 | int mastermind(const string& code, const string& guess) {       |                                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,18 +1,23 @@
+#include <unordered_map>
+using namespace std;
+
 int mastermind(const string& code, const string& guess) {
+    unordered_map<char, int> counts;
+    for (char c : guess) {
+        ++counts[c];
+    }
+
     int white = 0;
     int black = 0;
 
     for (int i = 0; i < 4; ++i) {
-        if (code.substr(i, 1) == guess.substr(i, 1)) {
+        if (code[i] == guess[i]) {
+            --counts[guess[i]];
             ++black;
-        } else {
-            for (char c : guess) {
-                if (c == code[i]) {
-                    ++white;
-                    break;
-                }
-            }
+        } else if (--counts[guess[i]] > 0) {
+            ++white;
         }
     }
 
     return black + white;
+}
