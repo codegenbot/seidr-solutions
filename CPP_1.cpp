8c8e503701c8e6289843251ffb3d4bfd251aa4b0
beep boop <githubbot@vadim.me>
2024-07-20 04:52:03 +0200

    Solution 57 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/n30xl9zv Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e7eeae52-f904-46f5-a286-867aa418dce8.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e7eeae52-f904-46f5-a286-867aa418dce8.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e7eeae52-f904-46f5-a286-867aa418dce8.cpp:26: e7eeae52-f904-46f5-a286-867aa418dce8.cpp: In function ‘int main()’: e7eeae52-f904-46f5-a286-867aa418dce8.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ e7eeae52-f904-46f5-a286-867aa418dce8.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -7,13 +7,54 @@ bool issame(vector<string> a, vector<string> b);
 vector<string> separate_paren_groups(string paren_string);
 
 int main() {
-    // Main function code here
+    string input;
+    cout << "Enter a string with parentheses: ";
+    cin >> input;
+
+    vector<string> groups = separate_paren_groups(input);
+
+    for (const string& group : groups) {
+        cout << group << endl;
+    }
+
+    return 0;
 }
 
 bool issame(vector<string> a, vector<string> b) {
-    // issame function code here
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 vector<string> separate_paren_groups(string paren_string) {
-    // separate_paren_groups function code here
+    vector<string> result;
+    string group;
+    int balance = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (balance > 0) {
+                group += c;
+            }
+            balance++;
+        } else if (c == ')') {
+            balance--;
+            if (balance > 0) {
+                group += c;
+            } else if (balance == 0) {
+                result.push_back(group);
+                group = "";
+            }
+        }
+    }
+
+    return result;
 }
