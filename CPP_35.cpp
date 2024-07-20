62c7f5fa5007ba80bbdd8b4f005c0d26a2b1349f
beep boop <githubbot@vadim.me>
2024-07-20 02:01:01 +0200

    Solution 77 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/gtchbki1 Prompt: 11d14766-dfe6-4279-8687-8c028d573c54.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 11d14766-dfe6-4279-8687-8c028d573c54.cpp:3: 11d14766-dfe6-4279-8687-8c028d573c54.cpp: In function ‘int main()’: 11d14766-dfe6-4279-8687-8c028d573c54.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 11d14766-dfe6-4279-8687-8c028d573c54.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <cmath>
 
-int findMax(int arr[], int n) {
-    return *max_element(arr, arr+n);
+int maxAbsoluteSum(vector<int>& l) {
+    int currentMax = *max_element(l.begin(), l.end());
+    int currentMin = *min_element(l.begin(), l.end());
+
+    return (currentMax > currentMin) ? currentMax : -currentMin;
 }
