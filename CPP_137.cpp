e1523c2029e2b866289c97963d6fba50abd8db94
beep boop <githubbot@vadim.me>
2024-07-20 04:07:29 +0200

    Solution 65 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/q4j8hs8g Prompt:       |     ^~~~ In file included from e8fb246f-bbf9-4564-9789-580f879a8f22.cpp:22: e8fb246f-bbf9-4564-9789-580f879a8f22.cpp: In function ‘int main()’: e8fb246f-bbf9-4564-9789-580f879a8f22.cpp:24:13: error: ‘boost’ has not been decl ared    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ e8fb246f-bbf9-4564-9789-580f879a8f22.cpp:24:29: error: ‘string’ was not declared  in this scope    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ e8fb246f-bbf9-4564-9789-580f879a8f22.cpp:24:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from e8fb246f-bbf9-4564-9789-580f879a8f22.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e8fb246f-bbf9-4564-9789-580f879a8f22.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,19 +5,16 @@
 template<typename T>
 const T compare_one(const std::any& a, const std::any& b) {
     if (a.type() == typeid(T) && b.type() == typeid(T)) {
-        std::string a_str = std::any_cast<T>(a);
-        std::string b_str = std::any_cast<T>(b);
-
-        if (a_str < b_str) {
-            return std::any_cast<T>(b);
-        } else {
+        if (std::any_cast<T>(a) >= std::any_cast<T>(b)) {
             return std::any_cast<T>(a);
+        } else {
+            return std::any_cast<T>(b);
         }
     }
     return T();
 }
 
 int main() {
-    assert (std::any_cast<std::string>(compare_one<std::string>(std::string("1"), std::string("1"))) == "1");
+    assert(std::any_cast<std::string>(compare_one<std::string>(std::string("1"), std::string("1"))) == "1");
     return 0;
 }
