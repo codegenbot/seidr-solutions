3b8574013dff05b68a94272905f9108ab9adab8c
beep boop <githubbot@vadim.me>
2024-07-20 06:35:52 +0200

    Solution 87 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,46 +1,36 @@
-#include <string>
-#include <algorithm>
-#include <boost/any.hpp>
-
-using namespace std;
+```
+#include <boost/lexical_cast.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return greaterThan(a, b);
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return greaterThan(b, a);
-    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        return greaterThan<string>(a, to_string(b));
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return greaterThan(to_string(a), b);
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return greaterThan<string>(a, b);
-    }
-
-    return boost::any("None");
-}
-
-boost::any greaterThan(boost::any a, boost::any b) {
-    if (get<int>(a) > get<float>(b))
-        return a;
-    else if (get<float>(a) > get<int>(b))
-        return a;
-    else if (get<string>(a) > get<string>(b))
-        return a;
-    else
-        return boost::any("None");
-}
-
-boost::any greaterThan<string>(boost::any a, boost::any b) {
-    string s1 = get<string>(a);
-    string s2 = get<string>(b);
-
-    int comp = s1.compare(s2);
-
-    if (comp > 0)
-        return a;
-    else if (comp < 0)
         return b;
-    else
-        return boost::any("None");
+    }
+    if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return b;
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stof(a.convert_to<string>().erase(0, 1).erase(stoi(a.convert_to<string>()) == a.convert_to<string>() ? a.convert_to<string>().size() - 1 : a.convert_to<string>().find(','))) >= stof(b.convert_to<string>().erase(0, 1).erase(stoi(b.convert_to<string>()) == b.convert_to<string>() ? b.convert_to<string>().size() - 1 : b.convert_to<string>().find(',')))
+            return b;
+        else if (stof(a.convert_to<string>().erase(0, 1).erase(stoi(a.convert_to<string>()) == a.convert_to<string>() ? a.convert_to<string>().size() - 1 : a.convert_to<string>().find(','))) <= stof(b.convert_to<string>().erase(0, 1).erase(stoi(b.convert_to<string>()) == b.convert_to<string>() ? b.convert_to<string>().size() - 1 : b.convert_to<string>().find(','))
+            return a;
+        else
+            return boost::any("None");
+    }
+    if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        string str = b.convert_to<string>();
+        if (stof(str.erase(0, 1).erase(stoi(str) == str ? str.size() - 1 : str.find(','))) >= a.cast<int>())
+            return boost::any(b);
+        else
+            return a;
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str = a.convert_to<string>();
+        if (stof(str.erase(0, 1).erase(stoi(str) == str ? str.size() - 1 : str.find(','))) <= b.cast<int>())
+            return boost::any(a);
+        else
+            return boost::any(b);
+    }
+    if ((a.type() == typeid(int) && b.type() == typeid(int)) || (a.type() == typeid(float) && b.type() == typeid(float)))
+        return a < b ? b : a;
+    return a;
 }
