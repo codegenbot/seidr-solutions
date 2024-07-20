8b68892b072141d8d1a05ebade8787c94e7a3536
beep boop <githubbot@vadim.me>
2024-07-20 02:02:49 +0200

    Solution 6 of CPP/39, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s2nuwfc8 Prompt: Complete the following code given the task description and function signature.

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,38 +1,23 @@
-#include <iostream>
-#include <vector>
-
+#include <cmath>
 using namespace std;
 
-bool isPrime(int num) {
-    if (num <= 1)
-        return false;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0)
-            return false;
-    }
-    return true;
-}
-
 int prime_fib(int n) {
-    int fibNum = 0, prevFib = 1, count = 0;
-    while (true) {
-        int temp = fibNum + prevFib;
-        if (isPrime(temp)) {
-            if (++count == n)
-                return temp;
-            fibNum = prevFib;
-            prevFib = temp;
-        } else {
-            fibNum = prevFib;
-            prevFib = temp;
+    int a = 0, b = 1;
+    for (int i = 2; ; i++) {
+        int fib = a + b;
+        if (fib >= n)
+            return fib;
+        a = b;
+        b = fib;
+        bool isPrime = true;
+        for (int j = 2; j <= sqrt(fib); j++) {
+            if (fib % j == 0) {
+                isPrime = false;
+                break;
+            }
         }
+        if (!isPrime)
+            continue;
+        return fib;
     }
-}
-
-int main() {
-    int n;
-    cout << "Enter the number of Fibonacci primes: ";
-    cin >> n;
-    cout << "The " << n << "th prime Fibonacci number is: " << prime_fib(n) << endl;
-    return 0;
 }
