6961f3e835a5fea199a9984e8c435cf397d20787
beep boop <githubbot@vadim.me>
2024-07-20 05:47:22 +0200

    Solution 93 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gcmd9cu5 Prompt:       |                    ^~~ 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp:9: 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp: In function ‘int main()’: 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8993b0b9-faa6-410e-95b9-fb17ef7b240f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,21 +2,13 @@
 #include <set>
 
 bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
 }
 
 vector<int> common(const vector<int>& l1, const vector<int>& l2) {
     vector<int> result;
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
