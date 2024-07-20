7876cfec2510b354f563b8e982babbb92629db4b
beep boop <githubbot@vadim.me>
2024-07-20 06:43:14 +0200

    Solution 51 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/8tvckcxs Prompt: Complete the following code given the task description and function signature.

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -1,16 +1,8 @@
 string find_max(vector<string> words){
-    string max_word = *max_element(words.begin(), words.end(),
-        [](const string& a, const string& b) {
-            if (a.length() == b.length()) {
-                return a < b;
-            }
-            return a.length() > b.length();
-        });
-    for (int i = 0; i < max_word.size(); i++) {
-        set<char> s(max_word.begin(), max_word.begin() + i + 1);
-        if (s.size() == i + 1) {
-            return max_word;
-        }
-    }
-    return max_word;
+    string res = *max_element(words.begin(),words.end(),
+        [](const string& a,const string& b)->bool{
+            if(a.length() == b.length()) return a<b;
+            return a.length()>b.length();
+    });
+    return res;
 }
