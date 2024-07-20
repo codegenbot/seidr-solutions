9242602d717ccde533b4094c749374851f668fde
beep boop <githubbot@vadim.me>
2024-07-20 09:29:51 +0200

    Solution 70 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/orehbhjv Prompt:       |                    ^~~ 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp:10: 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp: In function ‘int main()’: 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp:20:20: error: ‘generate_integers’ was n ot declared in this scope    20 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 84d12f18-b186-41b5-a6c9-1b18b534c250.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,29 +1,11 @@
-#include <iostream>
 #include <vector>
-#include <cassert>
 
-using namespace std;
-
-vector<int> generate_integers(int a, int b){
-    vector<int> result;
-    
-    for(int i = a; i <= b; ++i){
-        if(i % 2 == 0){
+bool issame(std::vector<int> a, std::vector<int> b) {
+    std::vector<int> result;
+    for (int i = a; i <= b; ++i) {
+        if (i % 2 == 0) {
             result.push_back(i);
         }
     }
-    
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-int main(){
-    assert(issame(generate_integers(17, 89), {}));
-    
-    cout << "Test passed." << endl;
-    
-    return 0;
 }
