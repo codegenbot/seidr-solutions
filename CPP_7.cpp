fc297cc3df25c5d3a8c75800fa5bbe33f274e533
beep boop <githubbot@vadim.me>
2024-07-20 04:45:21 +0200

    Solution 12 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/met4bo10 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cc81b404-d645-4ad8-972b-0dc2ea1d6a1b.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cc81b404-d645-4ad8-972b-0dc2ea1d6a1b.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cc81b404-d645-4ad8-972b-0dc2ea1d6a1b.cpp:11: cc81b404-d645-4ad8-972b-0dc2ea1d6a1b.cpp: In function ‘int main()’: cc81b404-d645-4ad8-972b-0dc2ea1d6a1b.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ cc81b404-d645-4ad8-972b-0dc2ea1d6a1b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,11 +1,13 @@
-bool issame(const string& s1, const string& s2) {
-    return s1 == s2;
+vector<string> filter_by_substring(vector<string> strings, string substring);
+
+bool issame(const string& str, const string& substring) {
+    return str.find(substring) != string::npos;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring) {
     vector<string> result;
     for (const string& str : strings) {
-        if (str.find(substring) != string::npos) {
+        if (issame(str, substring)) {
             result.push_back(str);
         }
     }
