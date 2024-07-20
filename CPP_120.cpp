164c3ff3fcd332be4c26d53e1ddaa170e47148d3
beep boop <githubbot@vadim.me>
2024-07-20 03:47:40 +0200

    Solution 54 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0ihxhvl1 Prompt:       |                           ^~~~~~ f943a6d9-a11d-4cd5-ada5-edd7f071a711.cpp:23:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f943a6d9-a11d-4cd5-ada5-edd7f071a711.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f943a6d9-a11d-4cd5-ada5-edd7f071a711.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f943a6d9-a11d-4cd5-ada5-edd7f071a711.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f943a6d9-a11d-4cd5-ada5-edd7f071a711.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ f943a6d9-a11d-4cd5-ada5-edd7f071a711.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -12,3 +12,9 @@ std::vector<int> maximum(const std::vector<int>& arr, int k){
     std::partial_sort(sortedArr.begin(), sortedArr.begin() + k, sortedArr.end(), std::greater<int>());
     return std::vector<int>(sortedArr.begin(), sortedArr.begin() + k);
 }
+
+int main(){
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 3), std::vector<int>{243, 3, 2}));
+    
+    return 0;
+}
