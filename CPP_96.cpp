86e597faecbf1d301fdabbe52749f6f724595e15
beep boop <githubbot@vadim.me>
2024-07-20 04:13:35 +0200

    Solution 21 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/38kvdprn Prompt: on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 88d08ac6-1b4c-451e-936e-472710bb33ea.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 88d08ac6-1b4c-451e-936e-472710bb33ea.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 88d08ac6-1b4c-451e-936e-472710bb33ea.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 88d08ac6-1b4c-451e-936e-472710bb33ea.cpp:25: 88d08ac6-1b4c-451e-936e-472710bb33ea.cpp: In function ‘int main()’: 88d08ac6-1b4c-451e-936e-472710bb33ea.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool is_same(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -15,22 +15,22 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 std::vector<int> count_up_to(int n) {
     std::vector<int> result;
-    for (int i = 2; i <= n; ++i) {
+    for (int num = 2; num <= n; ++num) {
         bool is_prime = true;
-        for (int j = 2; j * j <= i; ++j) {
-            if (i % j == 0) {
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
                 is_prime = false;
                 break;
             }
         }
         if (is_prime) {
-            result.push_back(i);
+            result.push_back(num);
         }
     }
     return result;
 }
 
 int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    assert(is_same(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     return 0;
 }
