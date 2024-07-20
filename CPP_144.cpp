de6e31e49a1562620cca139a213dc502b25e1605
beep boop <githubbot@vadim.me>
2024-07-20 04:08:43 +0200

    Solution 3 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/otqy37kh Prompt: c03b59e9-69db-4994-b642-cc85cd9c0c23.cpp:1:15: error: ‘string’ was not declared in this scope     1 | bool simplify(string x, string n) {       |               ^~~~~~ c03b59e9-69db-4994-b642-cc85cd9c0c23.cpp:1:25: error: ‘string’ was not declared in this scope     1 | bool simplify(string x, string n) {       |                         ^~~~~~ c03b59e9-69db-4994-b642-cc85cd9c0c23.cpp:1:33: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool simplify(string x, string n) {       |                                 ^ In file included from c03b59e9-69db-4994-b642-cc85cd9c0c23.cpp:10: c03b59e9-69db-4994-b642-cc85cd9c0c23.cpp: In function ‘int main()’: c03b59e9-69db-4994-b642-cc85cd9c0c23.cpp:12:21: error: ‘simplify’ cannot be used  as a function    12 |     assert (simplify("1/5", "1/5") == false);       |             ~~~~~~~~^~~~~~~~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,8 +1,10 @@
-bool simplify(const string &x, const string &n) {
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
+#include <string>
+
+bool simplify(std::string x, std::string n) {
+    int num1 = std::stoi(x.substr(0, x.find('/')));
+    int den1 = std::stoi(x.substr(x.find('/') + 1));
+    int num2 = std::stoi(n.substr(0, n.find('/')));
+    int den2 = std::stoi(n.substr(n.find('/') + 1));
 
     return (num1 * num2) % (den1 * den2) == 0;
 }
