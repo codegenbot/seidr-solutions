1299bd81b581aec3bdaeeec1e0e43e73eabf4eb2
beep boop <githubbot@vadim.me>
2024-07-20 03:57:44 +0200

    Solution 72 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/iwcmdhgw Prompt: e13a2b91-6ac9-4106-9477-3235b18c5558.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from e13a2b91-6ac9-4106-9477-3235b18c5558.cpp:31: e13a2b91-6ac9-4106-9477-3235b18c5558.cpp: In function ‘int main()’: e13a2b91-6ac9-4106-9477-3235b18c5558.cpp:41:20: error: ‘minPath’ was not declare d in this scope; did you mean ‘CustomNamespace::minPath’?    41 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ e13a2b91-6ac9-4106-9477-3235b18c5558.cpp:13:22: note: ‘CustomNamespace::minPath’  declared here    13 |     std::vector<int> minPath(const std::vector<std::vector<int>>& grid, int k) {       |                      ^~~~~~~ In file included from e13a2b91-6ac9-4106-9477-3235b18c5558.cpp:31: e13a2b91-6ac9-4106-9477-3235b18c5558.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,4 +1,1 @@
-int main() {
-    assert(CustomNamespace::issame({1, 3, 1, 3, 1, 3, 1, 3, 1, 3}, CustomNamespace::minPath({{1, 3}, {3, 2}}, 10)));
-    return 0;
-}
+assert(CustomNamespace::issame({1, 3, 1, 3, 1, 3, 1, 3, 1, 3}, CustomNamespace::minPath({{1, 3}, {3, 2}}, 10)));
