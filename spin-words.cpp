de1d7c774e785bfb3c21c1561207e226516482fb
beep boop <githubbot@vadim.me>
2024-07-20 07:52:54 +0200

    Solution 2 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/2u5aocpk Prompt: Program did not receive expected input

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,39 +1,47 @@
 #include <iostream>
 #include <string>
 
-std::string spinWords(std::string sentence) {
-    std::string result = "";
-    int wordLength = 0;
-    bool inWord = false;
+std::string spinWords(std::string str) {
+    std::string result;
+    int length = 0;
 
-    for (char c : sentence) {
-        if (c == ' ') {
-            if (inWord && wordLength >= 5) {
-                for (int i = wordLength - 1; i >= 0; --i)
-                    result += sentence[wordLength - i - 1];
-            } else
-                result += c;
-            inWord = false;
+    for (int i = 0; i <= str.length(); i++) {
+        if (i == str.length()) {
+            if (length >= 5) {
+                std::string temp;
+                for (int j = length - 1; j >= 0; j--) {
+                    temp += str[i - j - 1];
+                }
+                result += temp + " ";
+            } else {
+                result += str.substr(i - length, length) + " ";
+            }
+        } else if (str[i] == ' ') {
+            if (length >= 5) {
+                std::string temp;
+                for (int j = length - 1; j >= 0; j--) {
+                    temp += str[i - j - 1];
+                }
+                result += temp + " ";
+            } else {
+                result += str.substr(i - length, length) + " ";
+            }
+            length = 0;
         } else {
-            inWord = true;
-            result += c;
-            wordLength++;
+            length++;
         }
     }
 
-    if (inWord && wordLength >= 5) {
-        for (int i = wordLength - 1; i >= 0; --i)
-            result += sentence[wordLength - i - 1];
-    } else
-        result = sentence;
-
-    return result;
+    return result.substr(0, result.length() - 1);
 }
 
 int main() {
-    std::cout << spinWords("a") << std::endl;
-    std::cout << spinWords("this is a test") << std::endl;
-    std::cout << spinWords("this is another test") << std::endl;
-    std::cout << spinWords("hi") << std::endl;
+    std::string str;
+    while (true) {
+        std::cout << "Enter a string: ";
+        std::cin >> str;
+        if (str == "exit") break;
+        std::cout << "Spin words: " << spinWords(str) << std::endl;
+    }
     return 0;
 }
