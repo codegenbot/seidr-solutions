18f762892243f9ada7a99fbee80d0ea141acfc14
beep boop <githubbot@vadim.me>
2024-07-20 03:18:24 +0200

    Solution 17 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bnfqx7vn Prompt:       |                    ^~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:9: 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp: In function ‘int main()’: 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -6,3 +6,13 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     std::sort(result.begin(), result.end());
     return result;
 }
+
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5};
+    int k = 3;
+    std::vector<int> output = maximum(arr, k);
+    for (int i : output) {
+        std::cout << i << " ";
+    }
+    return 0;
+}
