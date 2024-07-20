ee4889c37c1dfa23ad5fecd083480f60cd419b89
beep boop <githubbot@vadim.me>
2024-07-20 02:47:08 +0200

    Solution 70 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1qqqklir Prompt: b43bea94-0c83-4ebb-9b96-b81f162ded84.cpp:1:12: error: ‘vector’ was not declared in this scope     1 | int search(vector<int> lst){       |            ^~~~~~ b43bea94-0c83-4ebb-9b96-b81f162ded84.cpp:1:19: error: expected primary-expressio n before ‘int’     1 | int search(vector<int> lst){       |                   ^~~ In file included from b43bea94-0c83-4ebb-9b96-b81f162ded84.cpp:12: b43bea94-0c83-4ebb-9b96-b81f162ded84.cpp: In function ‘int main()’: b43bea94-0c83-4ebb-9b96-b81f162ded84.cpp:14:19: error: ‘search’ cannot be used a s a function    14 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,28 +1,13 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
 
-using namespace std;
-
-int count(int i) {
-    int cnt = 0;
-    while (i > 0) {
-        i /= 10;
-        cnt++;
-    }
-    return cnt;
-}
-
-int search(vector<int> lst) {
-    int max = 0;
-    for (int i : lst) {
-        if (i > 0 && i >= count(i)) {
-            max = i;
+int search(std::vector<int> lst){
+    int max = -1;
+    for(int num : lst){
+        if(num > 0 && num >= count(lst.begin(), lst.end(), num)){
+            max = num;
+            break;
         }
     }
-    return max == 0 ? -1 : max;
-}
-
-int main() {
-    assert(search({3, 10, 10, 9, 2}) == -1);
-    return 0;
+    return max;
 }
