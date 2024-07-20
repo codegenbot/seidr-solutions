2a65be005b1ee26ebe66b16d653e686879ff8e16
beep boop <githubbot@vadim.me>
2024-07-20 06:13:38 +0200

    Solution 20 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/sp5metma Prompt: Complete the following code given the task description and function signature.

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,12 +1,12 @@
-int prod_signs(vector<int> arr) {
+int prod_signs(vector<int> arr){
     int product = 1;
     long long sum = 0;
-    for (int num : arr) {
-        if (num == 0) {
-            return -32768;
-        }
-        product *= (num > 0 ? 1 : -1);
-        sum += abs(num);
+
+    for(int i: arr){
+        if(i == 0) continue;
+        product *= (i > 0 ? 1 : -1);
+        sum += abs(i);
     }
-    return product * sum;
+
+    return product * sum == 0 ? -32768 : product * sum;
 }
