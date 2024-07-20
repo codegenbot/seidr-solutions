73d89c535cca32a008ade5651c1ca8d2611889f6
beep boop <githubbot@vadim.me>
2024-07-20 05:23:50 +0200

    Solution 21 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7inv7d7v Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc85a024-563d-4910-898c-ee6f3421030d.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc85a024-563d-4910-898c-ee6f3421030d.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc85a024-563d-4910-898c-ee6f3421030d.cpp:10: fc85a024-563d-4910-898c-ee6f3421030d.cpp: In function ‘int main()’: fc85a024-563d-4910-898c-ee6f3421030d.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ fc85a024-563d-4910-898c-ee6f3421030d.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -5,32 +5,25 @@ void sort_third(vector<int>& v) {
 }
 
 bool issame(vector<int>& v1, vector<int>& v2) {
-    if (v1.size() != v2.size()) {
-        return false;
-    }
-    for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
+    return v1 == v2;
 }
 
 int main() {
     vector<int> result = l;
     sort_third(result);
     
-    vector<int> expected_result = l;
+    vector<int> expected = l;
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
-            sort(expected_result.begin() + i, expected_result.begin() + i + 3);
+            sort(expected.begin() + i, expected.begin() + i + 3);
         }
     }
     
-    if (issame(result, expected_result)) {
-        cout << "Output matches expected result." << endl;
+    if (issame(result, expected)) {
+        cout << "Output is correct" << endl;
     } else {
-        cout << "Output does not match expected result." << endl;
+        cout << "Output is incorrect" << endl;
     }
+    
     return 0;
 }
