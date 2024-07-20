3afcecbd897e34e62a1901224c93fec3059e7026
beep boop <githubbot@vadim.me>
2024-07-20 06:29:31 +0200

    Solution 79 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wadxo5dv Prompt:       |                             ^~~ b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp:22:33: error: expected primary-expressi on before ‘char’    22 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp:22:38: error: expected primary-expressi on before ‘int’    22 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp:22:44: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp:21: b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp: In function ‘int main()’: b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp:36:20: error: ‘histogram’ was not decla red in this scope    36 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ b986029b-a018-46eb-9fa2-a2c2e4ce8037.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -5,6 +5,32 @@
 
 using namespace std;
 
+map<char, int> histogram(string test);
+
 bool issame(map<char, int> a, map<char, int> b){
     return a == b;
+}
+
+int main() {
+    assert(issame(histogram("a"), {{'a', 1}}));
+}
+
+map<char, int> histogram(string test) {
+    map<char, int> freq;
+    for (char c : test) {
+        if (c != ' ') {
+            freq[c]++;
+        }
+    }
+    int maxFreq = 0;
+    for (const auto& p : freq) {
+        maxFreq = max(maxFreq, p.second);
+    }
+    map<char, int> result;
+    for (const auto& p : freq) {
+        if (p.second == maxFreq) {
+            result[p.first] = p.second;
+        }
+    }
+    return result;
 }
