ab2ef3351de8b06c84db9a9fa59f7bd44ea20af8
beep boop <githubbot@vadim.me>
2024-07-20 05:12:24 +0200

    Solution 60 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/onf9qcl6 Prompt:       |                    ^~~~~ ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp:11: ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp: In function ‘int main()’: ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ ab8c0a14-f002-44ec-b12a-acb1a185a097.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,19 +1,18 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<float> a, vector<float> b){
+    return a == b;
 }
 
-int main() {
-    assert(issame(get_positive({}), std::vector<float>{}));
-    return 0;
+vector<float> get_positive(vector<float> l);
+
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
+        }
+    }
+    return result;
 }
