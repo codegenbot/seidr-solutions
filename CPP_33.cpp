a01c3483bbb9c30bfb85d02a0e7def679b3b7ea8
beep boop <githubbot@vadim.me>
2024-07-20 02:53:21 +0200

    Solution 4 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3zst4mto Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6f7fcbb2-f278-42eb-90cf-8ba59395014b.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6f7fcbb2-f278-42eb-90cf-8ba59395014b.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6f7fcbb2-f278-42eb-90cf-8ba59395014b.cpp:9: 6f7fcbb2-f278-42eb-90cf-8ba59395014b.cpp: In function ‘int main()’: 6f7fcbb2-f278-42eb-90cf-8ba59395014b.cpp:19:20: error: ‘sort_third’ was not decl ared in this scope    19 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 6f7fcbb2-f278-42eb-90cf-8ba59395014b.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,16 +1,23 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
 
-bool issame(vector<int> a, vector<int> b){
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
 vector<int> sort_third(vector<int> l);
 
+int main() {
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    return 0;
+}
+
 vector<int> sort_third(vector<int> l) {
     vector<int> res = l;
-    for (size_t i = 0; i < l.size(); i += 3) {
-        sort(res.begin() + i, min(res.begin() + i + 3, res.end()));
+    for (int i = 0; i < l.size(); i += 3) {
+        sort(res.begin() + i, res.begin() + i + 3);
     }
     return res;
 }
