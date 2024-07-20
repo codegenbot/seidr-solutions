3407648a4b1946e11ebcc96d05a531e66bb7eeba
beep boop <githubbot@vadim.me>
2024-07-20 07:50:33 +0200

    Solution 83 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/bjeyu258 Prompt: 75a1e136-0b61-4fc3-a1b7-afc64ac2f326.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string frames) {       |                  ^~~~~~ 75a1e136-0b61-4fc3-a1b7-afc64ac2f326.cpp:24:19: error: ‘string’ was not declared  in this scope    24 | int bowlingStrike(string frames, int i) {       |                   ^~~~~~ 75a1e136-0b61-4fc3-a1b7-afc64ac2f326.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | int bowlingStrike(string frames, int i) {       |                                  ^~~ 75a1e136-0b61-4fc3-a1b7-afc64ac2f326.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | int bowlingStrike(string frames, int i) {       |                                       ^

--- bowling.cpp
+++ bowling.cpp
@@ -8,23 +8,18 @@ int bowlingScore(std::string frames) {
         if (frames[i] == 'X') { // Strike
             score += 10 + bowlingStrike(frames, i);
         } else if (frames[i] == '/') { // Spare
-            if (i < 9) {
-                score += 10 - (frames[i + 1] - '0' > 7 ? 10 : frames[i + 1] - '0');
-            } else {
-                score += 10;
-            }
+            score += 10 - frames[i + 1] - frames[i + 2];
         } else {
             roll1 = frames[i] - '0';
             if (i < 8) {
-                if (frames[i + 1] == 'X' || (frames[i + 1] == '/' && frames[i + 2] - '0' > 7)) {
-                    score += roll1 + 10;
+                roll2 = frames[i + 1] - '0';
+                if (roll1 + roll2 >= 10) { // Spare
+                    score += roll1 + roll2;
                 } else {
-                    roll2 = frames[i + 1] - '0';
                     score += roll1 + roll2;
                 }
             } else {
-                roll2 = frames[i + 1] - '0';
-                score += roll1 + roll2;
+                score += roll1;
             }
         }
     }
@@ -39,9 +34,9 @@ int bowlingStrike(std::string frames, int i) {
         if (frames[j] == 'X') { // Strike
             score += 10 + bowlingStrike(frames, j);
         } else if (frames[j] == '/') { // Spare
-            score += 10 - (frames[j + 1] - '0' > 7 ? 10 : frames[j + 1] - '0');
+            score += 10 - frames[j + 1] - frames[j + 2];
         } else {
-            score += frames[j] - '0';
+            score += frames[j] - '0' + frames[j + 1] - '0';
         }
     }
 
