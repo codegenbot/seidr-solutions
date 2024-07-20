f5697dcbe8748d019e346291d14701551575bf6d
beep boop <githubbot@vadim.me>
2024-07-20 02:20:23 +0200

    Solution 24 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9z0vt8xn Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_set.h:61,                  from /usr/include/c++/11/set:61,                  from 03f8792c-d30c-4d4a-824d-5c825e817050.cpp:4: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,9 @@
 #include <set>
 #include <vector>
 
-std::vector<int> common(std::initializer_list<int> l1, std::initializer_list<int> l2){
-    std::set<int> s1(l1.begin(), l1.end());
-    std::set<int> s2(l2.begin(), l2.end());
+std::vector<int> common(std::initializer_list<int> l1,std::initializer_list<int> l2){
+    std::set<int> s1(l1.begin(),l1.end());
+    std::set<int> s2(l2.begin(),l2.end());
 
     std::vector<int> result;
     for(int i : s1) {
@@ -12,6 +12,6 @@ std::vector<int> common(std::initializer_list<int> l1, std::initializer_list<int
         }
     }
 
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     return result;
 }
