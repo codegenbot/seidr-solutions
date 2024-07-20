1070859d986682684ff781fc9b596914ba71449e
beep boop <githubbot@vadim.me>
2024-07-20 03:37:53 +0200

    Solution 7 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cx2a3x3x Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,32 +1,34 @@
-Here is the completed code:
-
 string file_name_check(string file_name){
-    int dot_count = 0;
-    int digit_count = 0;
-    bool valid = true;
-
-    for(int i=0; i<file_name.length(); i++){
-        if(file_name[i] == '.'){
-            dot_count++;
-        }
-        else if(isdigit(file_name[i])){
-            digit_count++;
-        }
-        else if(!isalpha(file_name[i]) && i != 0){
-            valid = false;
+    int dot_position = file_name.find('.');
+    if (dot_position == -1) {
+        return "No";
+    }
+    string before_dot = file_name.substr(0, dot_position);
+    string after_dot = file_name.substr(dot_position + 1);
+    
+    bool valid_before = true;
+    for (char c : before_dot) {
+        if (!isalpha(c)) {
+            valid_before = false;
             break;
         }
     }
-
-    if(dot_count > 1 || digit_count > 3 || (dot_count == 0) || !valid){
-        return "No";
-    }
-    else{
-        size_t pos = file_name.find('.');
-        string ext = file_name.substr(pos+1);
-        if(ext != "txt" && ext != "exe" && ext != "dll"){
+    
+    int digit_count = 0;
+    for (char c : after_dot) {
+        if (isdigit(c)) {
+            digit_count++;
+            if (digit_count > 3) {
+                return "No";
+            }
+        } else if (c != '.' && c != 't' && c != 'x' && c != 'd' && c != 'l') {
             return "No";
         }
-        return "Yes";
     }
+    
+    if (!valid_before || after_dot.find('.') != string::npos) {
+        return "No";
+    }
+    
+    return "Yes";
 }
