c868924ca05b3dd18919fe2373122423c40cc3c0
beep boop <githubbot@vadim.me>
2024-07-20 07:56:49 +0200

    Solution 72 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ko1ld40j Prompt: ’ in ‘guess’, which is of non-class type ‘const int’     6 |         if (code.substr(i, 1) == guess.substr(i, 1)) {       |                                        ^~~~~~ 129c4db9-5c6b-4885-9e78-dd4080bf7f45.cpp:8:25: error: ‘count’ is not a member of  ‘std’     8 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                         ^~~~~ 129c4db9-5c6b-4885-9e78-dd4080bf7f45.cpp:8:37: error: request for member ‘begin’  in ‘guess’, which is of non-class type ‘const int’     8 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                                     ^~~~~ 129c4db9-5c6b-4885-9e78-dd4080bf7f45.cpp:8:52: error: request for member ‘end’ i n ‘guess’, which is of non-class type ‘const int’     8 |         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                    ^~~ 129c4db9-5c6b-4885-9e78-dd4080bf7f45.cpp:8:63: error: invalid types ‘const int[i nt]’ for array subscript     8 |       } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                             ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -2,13 +2,19 @@ int mastermind(const string& code, const string& guess) {
     int white = 0;
     int black = 0;
 
+    unordered_map<char, int> count_guess;
+
+    for (char c : guess) {
+        ++count_guess[c];
+    }
+
     for (int i = 0; i < 4; ++i) {
-        if (code.substr(i, 1) == guess.substr(i, 1)) {
-            ++black;
-        } else if (std::count(guess.begin(), guess.end(), code.substr(0, 1)) > 0) {
+        if (code[i] == guess[i]) {
+            --black;
+        } else if (count_guess[code[i]] > 0) {
+            --count_guess[code[i]];
             ++white;
         }
     }
 
     return black + white;
-}
