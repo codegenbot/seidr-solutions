18f552c11e0793e8f0e7a756efe0afd934cf08da
beep boop <githubbot@vadim.me>
2024-07-20 07:45:47 +0200

    Solution 48 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ko1ld40j Prompt: 3e8c990c-3f17-4945-b019-46421216c5d3.cpp:9:29: error: invalid types ‘const int[i nt]’ for array subscript     9 |         if (code[i] == guess[i]) {       |                             ^ 3e8c990c-3f17-4945-b019-46421216c5d3.cpp:11:32: error: request for member ‘begin ’ in ‘guess’, which is of non-class type ‘const int’    11 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                ^~~~~ 3e8c990c-3f17-4945-b019-46421216c5d3.cpp:11:47: error: request for member ‘end’ in ‘guess’, which is of non-class type ‘const int’    11 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                               ^~~ 3e8c990c-3f17-4945-b019-46421216c5d3.cpp:11:58: error: invalid types ‘const int[ int]’ for array subscript    11 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                          ^ 3e8c990c-3f17-4945-b019-46421216c5d3.cpp:16:30: error: expected ‘}’ at end of in put    16 |     return black + white - 2;       |                              ^ 3e8c990c-3f17-4945-b019-46421216c5d3.cpp:4:57: note: to match this ‘{’     4 | int mastermind(const string& code, const string& guess) {       |                                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -2,15 +2,16 @@ int mastermind(const string& code, const string& guess) {
     int white = 0;
     int black = 0;
 
+    vector<char> codeArray(code.begin(), code.end());
+    vector<char> guessArray(guess.begin(), guess.end());
+
     for (int i = 0; i < 4; ++i) {
-        if (code.substr(i, 1) == guess.substr(i, 1)) {
+        if (codeArray[i] == guessArray[i]) {
             ++black;
-        } else {
-            auto it = find(guess.begin(), guess.end(), code.substr(i, 1));
-            if (it != guess.end()) {
-                ++white;
-            }
+        } else if (count(guessArray.begin(), guessArray.end(), codeArray[i]) > 0) {
+            ++white;
         }
     }
 
     return black + white - 2;
+}
