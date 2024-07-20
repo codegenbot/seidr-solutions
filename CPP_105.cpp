62b3168d1c85d7a8377a553c7009fe1c0c7225c6
beep boop <githubbot@vadim.me>
2024-07-20 03:01:32 +0200

    Solution 0 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n10wl24y Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,60 +1,46 @@
-#include <algorithm>
-#include <vector>
 #include <string>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-std::vector<std::string> by_length(std::vector<int> arr) {
-    vector<string> numArr;
+vector<string> by_length(vector<int> arr) {
+    vector<int> nums;
     for (int i : arr) {
         if (i >= 1 && i <= 9)
-            numArr.push_back(to_string(i));
+            nums.push_back(i);
     }
-    sort(numArr.begin(), numArr.end());
-    reverse(numArr.begin(), numArr.end());
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
     vector<string> result;
-    for (string s : numArr) {
-        switch (stoi(s)) {
+    for (int num : nums) {
+        string str = "";
+        switch (num) {
             case 1:
-                result.push_back("One");
+                str = "One";
                 break;
             case 2:
-                result.push_back("Two");
+                str = "Two";
                 break;
             case 3:
-                result.push_back("Three");
+                str = "Three";
                 break;
             case 4:
-                result.push_back("Four");
+                str = "Four";
                 break;
             case 5:
-                result.push_back("Five");
+                str = "Five";
                 break;
             case 6:
-                result.push_back("Six");
+                str = "Six";
                 break;
             case 7:
-                result.push_back("Seven");
+                str = "Seven";
                 break;
             case 8:
-                result.push_back("Eight");
+                str = "Eight";
                 break;
             case 9:
-                result.push_back("Nine");
+                str = "Nine";
                 break;
         }
+        result.push_back(str);
     }
     return result;
 }
