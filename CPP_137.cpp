e11eea00b568abd5ffeb509700b8b13c2d69f2f7
beep boop <githubbot@vadim.me>
2024-07-20 06:30:56 +0200

    Solution 61 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,26 +1,28 @@
-if(a.type() == typeid(int) && b.type() == typeid(int))
-    return (int)max((int)a, (int)b);
-else if(a.type() == typeid(float) && b.type() == typeid(float))
-    return max((float)a, (float)b);
-else if(a.type() == typeid(string) && b.type() == typeid(string))
-{
-    string str_a = boost::any_cast<string>(a);
-    string str_b = boost::any_cast<string>(b);
+#include <boost/any_cast.hpp>
 
-    int pos_a = str_a.find(',');
-    int pos_b = str_b.find(',');
+boost::any compare_one(boost::any a, boost::any b) {
+    double da = 0;
+    double db = 0;
 
-    float num_a = stof(str_a.substr(0, pos_a));
-    float num_b = stof(str_b.substr(0, pos_b));
+    if (boost::any_cast<double>(a)) {
+        da = boost::any_cast<double>(a);
+    } else if (boost::any_cast<std::string>(a)) {
+        std::istringstream iss(std::any_cast<std::string>(a));
+        iss >> da;
+    }
 
-    if(num_a > num_b)
-        return str_a;
-    else
-        return str_b;
+    if (boost::any_cast<double>(b)) {
+        db = boost::any_cast<double>(b);
+    } else if (boost::any_cast<std::string>(b)) {
+        std::istringstream iss(std::any_cast<std::string>(b));
+        iss >> db;
+    }
+
+    if (da > db) {
+        return a;
+    } else if (db > da) {
+        return b;
+    } else {
+        return boost::any("None");
+    }
 }
-else if(a.type() == typeid(int) && b.type() == typeid(float))
-    return (b >> boost::any_cast<int>(a));
-else if(a.type() == typeid(float) && b.type() == typeid(int))
-    return (a >> boost::any_cast<float>(b));
-
-return "None";
