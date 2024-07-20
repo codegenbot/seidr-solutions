d8ed9d90337b399763cf88c1488185ee1b2978c8
beep boop <githubbot@vadim.me>
2024-07-20 05:54:30 +0200

    Solution 74 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v1zxwqb2 Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,24 +1,48 @@
 vector<string> by_length(vector<int> arr) {
-    vector<int> nums;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            nums.push_back(num);
-        }
+    vector<int> numbers;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9)
+            numbers.push_back(i);
     }
-
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
-
+    
+    sort(numbers.begin(), numbers.end());
+    
     vector<string> result;
-    map<int, string> numberMap = {{1, "One"}, {2, "Two"}, {3, "Three"},
-                                   {4, "Four"}, {5, "Five"}, {6, "Six"},
-                                   {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-
-    for (int num : nums) {
-        if (numberMap.count(num)) {
-            result.push_back(numberMap[num]);
+    for (int num : numbers) {
+        string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
         }
+        result.push_back(str);
     }
 
+    reverse(result.begin(), result.end());
+    
     return result;
 }
