68a8b544e0e21e040e5ea9877bc8c4ceae8d6ab3
beep boop <githubbot@vadim.me>
2024-07-20 06:16:04 +0200

    Solution 18 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/bqe4vu5i Prompt:       |                              ^~~~~~ 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp:21: 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp: In function ‘int main()’: 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp:31:20: error: ‘by_length’ was not decla red in this scope    31 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 3223169d-196c-4bf3-8537-b758e2bd6b38.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -11,9 +11,30 @@ bool issame(vector<string> a, vector<string> b){
 vector<string> by_length(vector<int> arr);
 
 int main() {
-    // Existing code block
+    vector<int> numbers = {9, 5, 2, 7, 3, 1, 8, 4, 6};
+    vector<string> result = by_length(numbers);
+    for (const string& word : result) {
+        cout << word << " ";
+    }
+    return 0;
 }
 
 vector<string> by_length(vector<int> arr){
-    // Existing function implementation
+    vector<string> result;
+    vector<int> filtered_arr;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            filtered_arr.push_back(num);
+        }
+    }
+    sort(filtered_arr.begin(), filtered_arr.end());
+    reverse(filtered_arr.begin(), filtered_arr.end());
+    map<int, string> num_to_word = {
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+    };
+    for (int num : filtered_arr) {
+        result.push_back(num_to_word[num]);
+    }
+    return result;
 }
