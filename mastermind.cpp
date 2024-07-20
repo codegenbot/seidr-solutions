170f11bd60c3529bfc2fe6756945cc0be4f90d2c
beep boop <githubbot@vadim.me>
2024-07-20 07:48:22 +0200

    Solution 54 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ko1ld40j Prompt: nt]’ for array subscript     6 |         if (code[i] == guess[i]) {       |                 ^ 7be5a59f-cd3d-4f29-aa40-feca71d930ec.cpp:6:29: error: invalid types ‘const int[i nt]’ for array subscript     6 |         if (code[i] == guess[i]) {       |                             ^ 7be5a59f-cd3d-4f29-aa40-feca71d930ec.cpp:8:32: error: request for member ‘begin’  in ‘guess’, which is of non-class type ‘const int’     8 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                ^~~~~ 7be5a59f-cd3d-4f29-aa40-feca71d930ec.cpp:8:47: error: request for member ‘end’ i n ‘guess’, which is of non-class type ‘const int’     8 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                               ^~~ 7be5a59f-cd3d-4f29-aa40-feca71d930ec.cpp:8:58: error: invalid types ‘const int[i nt]’ for array subscript     8 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                          ^ 7be5a59f-cd3d-4f29-aa40-feca71d930ec.cpp:8:20: error: ‘count’ was not declared i n this scope     8 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                    ^~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,13 +1,10 @@
-#include <algorithm>
-#include <string>
-
 int mastermind(const std::string& code, const std::string& guess) {
     int white = 0;
-    int black = 0;
+    int blacks = 0;
 
     for (int i = 0; i < 4; ++i) {
         if (code.substr(i, 1) == guess.substr(i, 1)) {
-            ++black;
+            ++blacks;
         } else if (std::count(guess.begin(), guess.end(), code.substr(i, 1)) > 0) {
             ++white;
         }
