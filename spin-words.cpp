0de60fc0bcb02343fdafac1d9f025951201da3ce
beep boop <githubbot@vadim.me>
2024-07-20 07:53:27 +0200

    Solution 3 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/an15tflx Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,3 +1,5 @@
+Here is the solution:
+
 #include <vector>
 #include <iostream>
 #include <string>
@@ -6,35 +8,22 @@ using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int wordLen;
-    string temp;
-    
+    int wordLength;
     for (int i = 0; i <= str.length(); i++) {
         if (i == str.length() || str[i] == ' ') {
-            wordLen = temp.length();
-            temp = temp.substr(rev(temp));
-            if (wordLen >= 5) {
-                temp = temp.substr(1) + temp[0];
-            }
-            result += temp + " ";
-            temp = "";
-        } else {
-            temp += str[i];
+            if ((wordLength = i - result.length()) >= 5)
+                reverse(result.rbegin(), result.rend());
+            result += str.substr(0, i) + " ";
+            str = str.substr(i);
         }
     }
-    
-    return result;
-}
-
-string rev(string s) {
-    string t = "";
-    for (int i = 0; i < s.length(); i++) {
-        t = s[i] + t;
-    }
-    return t;
+    return result.substr(0, result.length() - 1);
 }
 
 int main() {
-    // Your code here
+    string str;
+    while (cin >> str) {
+        cout << spinWords(str) << endl;
+    }
     return 0;
 }
