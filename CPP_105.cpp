13c3fc1164da2fb263633dcfa805388c050a4a93
beep boop <githubbot@vadim.me>
2024-07-20 03:05:06 +0200

    Solution 22 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n10wl24y Prompt:       |                                 ^~~~~~ In file included from 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp:49:45: error: expression list treated a s compound expression in initializer [-fpermissive]    49 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp:48: 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp: In function ‘int main()’: 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp:58:20: error: ‘by_length’ was not decla red in this scope    58 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 7400b5e0-5972-43fd-907f-7299ed4a73ae.cpp:58:19: error: ‘issame’ cannot be used a s a function    58 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -5,17 +5,51 @@
 bool issame(vector<string> a, vector<string>b);
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+    vector<string> nums;
     for (int i : arr) {
         if (i >= 1 && i <= 9)
-            result.push_back(to_string(i));
+            nums.push_back(to_string(i));
     }
-    sort(result.begin(), result.end());
-    reverse(result.begin(), result.end());
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+    vector<string> result;
+    for (string num : nums) {
+        string str = "";
+        switch (stoi(num)) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
+    }
     return result;
 }
 
 int main() {
-    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    assert (issame(by_length({9, 4, 8}) , {"Nine", "Four", "Eight"}));
     return 0;
 }
