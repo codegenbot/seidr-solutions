545161a8be96c86f6e1e887549f773b66239d8bb
beep boop <githubbot@vadim.me>
2024-07-20 02:54:48 +0200

    Solution 23 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/xpyxaxvq Prompt:                  from /usr/include/c++/11/iostream:39,                  from 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp:17: 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp: In function ‘int main()’: 3b8f29ed-efa7-4069-9f5a-820039947f0a.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -5,15 +5,11 @@
 #include <cassert>
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
-}
-
-bool total_match(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    return issame(a, b);
+    return a == b;
 }
 
 int main() {
-    assert(total_match(std::vector<std::string>{"this"}, std::vector<std::string>{}) == true);
-    
+    assert(issame(std::vector<std::string>{"this"}, std::vector<std::string>{}) == false);
+
     return 0;
 }
