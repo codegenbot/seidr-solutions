7c1a11471b4448c71cded30cdd8a6eb93a3dcd92
beep boop <githubbot@vadim.me>
2024-07-20 03:38:00 +0200

    Solution 8 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cx2a3x3x Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,34 +1,24 @@
 string file_name_check(string file_name){
-    int dot_position = file_name.find('.');
-    if (dot_position == -1) {
-        return "No";
-    }
-    string before_dot = file_name.substr(0, dot_position);
-    string after_dot = file_name.substr(dot_position + 1);
-    
-    bool valid_before = true;
-    for (char c : before_dot) {
-        if (!isalpha(c)) {
-            valid_before = false;
-            break;
-        }
-    }
-    
     int digit_count = 0;
-    for (char c : after_dot) {
-        if (isdigit(c)) {
+    bool found_dot = false;
+    for(int i=0; i<file_name.length(); i++){
+        if(isdigit(file_name[i])){
             digit_count++;
-            if (digit_count > 3) {
-                return "No";
-            }
-        } else if (c != '.' && c != 't' && c != 'x' && c != 'd' && c != 'l') {
+            if(digit_count > 3) return "No";
+        }
+        else if(file_name[i] == '.'){
+            found_dot = true;
+        }
+        else if(!found_dot && !isalpha((unsigned char)file_name[i])){
             return "No";
         }
     }
-    
-    if (!valid_before || after_dot.find('.') != string::npos) {
-        return "No";
+    if(found_dot){
+        string extension = file_name.substr(file_name.find('.')+1);
+        if(extension != "txt" && extension != "exe" && extension != "dll")
+            return "No";
     }
-    
+    else
+        return "No";
     return "Yes";
 }
