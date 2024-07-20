c2f6b8ec4021e32f180d2b95954e5809d24a5909
beep boop <githubbot@vadim.me>
2024-07-20 12:32:49 +0200

    Solution 97 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/qkfacgh4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/513263ca-b28b-41df-8137-3ecc5ae47 40a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- spin-words.py
+++ spin-words.py
@@ -1,6 +1,5 @@
-```
 def spin_words(sentence):
     return " ".join(
-        word.strip()[::-1] if len(word.strip()) >= 5 else word.lower() if len(word) < 5 else word
+        word.strip()[::-1] if len(word.strip()) >= 5 else word
         for word in sentence.split()
     )
