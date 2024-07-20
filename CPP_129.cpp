304b9322233f4f4c302d15b73433549f2f80e8f9
beep boop <githubbot@vadim.me>
2024-07-20 06:12:50 +0200

    Solution 0 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/p3k5yzno Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,50 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <algorithm>
-
 using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<int>> visited(n, vector<int>(n));
-    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>> > pq;
-    vector<int> res;
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (!visited[i][j]) {
-                pq.push({grid[i][j], {i, j}});
-                visited[i][j] = 1;
+    vector<vector<int>> dp(n, vector<int>(n));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = grid[i][j];
+            } else if (i > 0) {
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            } else {
+                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
             }
         }
     }
-
-    while (!pq.empty()) {
-        int val = pq.top().first;
-        pair<int, int> pos = pq.top().second;
-        res.push_back(val);
-        pq.pop();
-
-        if (k > 0) {
-            --k;
-
-            // Check all neighbors
-            for (int i = -1; i <= 1; ++i) {
-                for (int j = -1; j <= 1; ++j) {
-                    int ni = pos.first + i;
-                    int nj = pos.second + j;
-
-                    if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
-                        pq.push({grid[ni][nj], {ni, nj}});
-                        visited[ni][nj] = 1;
+    
+    int min_val = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dp[i][j] == dp[n-1][n-1]) {
+                vector<int> path;
+                int val = grid[i][j];
+                for (int l = 0; l <= k; l++) {
+                    path.push_back(val);
+                    if (l < k) {
+                        if (i > 0) i--;
+                        else if (j > 0) j--;
+                        else break;
                     }
                 }
+                return path;
             }
-        } else {
-            break;
         }
     }
-
-    return res;
+    
+    return {};
 }
