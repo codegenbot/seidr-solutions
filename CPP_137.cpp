3301e2e349d0398c7878f7869a7a16013281d9c2
beep boop <githubbot@vadim.me>
2024-07-20 06:33:35 +0200

    Solution 77 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
+#include <boost/any.hpp>
 #include <string>
 #include <algorithm>
-#include <boost/any.hpp>
+
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return boost::any(b.get<boost::any>());
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return a;
+    else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+        return (boost::any) (b > a ? b : a);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        if (stod(str1) > stod(str2))
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int num1 = boost::any_cast<int>(a);
-        string str = boost::any_cast<string>(b);
-        double d = stod(str);
-        if (num1 > d)
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        int num2 = boost::any_cast<int>(b);
-        string str = boost::any_cast<string>(a);
-        double d = stod(str);
-        if (d > num2)
-            return a;
-        else
-            return b;
+        string str_a = any_cast<string>(a);
+        string str_b = any_cast<string>(b);
+
+        int len_a = str_a.size();
+        int len_b = str_b.size();
+
+        for (int i = 0; i < len_a; ++i) {
+            if (str_a[i] == ',') {
+                --len_a;
+                break;
+            }
+        }
+
+        for (int i = 0; i < len_b; ++i) {
+            if (str_b[i] == ',') {
+                --len_b;
+                break;
+            }
+        }
+
+        str_a = str_a.substr(0, len_a);
+        str_b = str_b.substr(0, len_b);
+
+        return boost::any((boost::any_cast<string>(a) > boost::any_cast<string>(b)) ? b : a);
     }
     else {
         return boost::any("None");
