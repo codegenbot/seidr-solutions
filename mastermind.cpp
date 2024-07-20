a40923498bbe00c39336f65307e600e47cccd440
beep boop <githubbot@vadim.me>
2024-07-20 08:13:26 +0200

    Solution 0 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/y0m20dx3 Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,37 +1,18 @@
-#include <vector>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int getWhitePegs(string code, string guess) {
-    int whitePegs = 0;
-    for (char c : guess) {
-        if (count(code.begin(), code.end(), c) > 0) {
-            whitePegs++;
-            code.erase(remove(code.begin(), code.end(), c), code.end());
+int mastermind(string code, string guess) {
+    int white = 0;
+    int black = 0;
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
+            black++;
+        } else {
+            int count = 0;
+            for(int j=0; j<4; j++) {
+                if(code[i] == guess[j])
+                    count++;
+            }
+            if(count > 0)
+                white += count - 1;
         }
     }
-    return whitePegs;
-}
-
-int getBlackPegs(string code, string guess) {
-    int blackPegs = 0;
-    for (char c : guess) {
-        if (code[guess.find(c)] == c) {
-            blackPegs++;
-            code.erase(remove(code.begin(), code.end(), c), code.end());
-        }
-    }
-    return blackPegs;
-}
-
-int main() {
-    string code, guess;
-    cin >> code >> guess;
-    int whitePegs = getWhitePegs(code, guess);
-    int blackPegs = getBlackPegs(code, guess);
-    cout << whitePegs << endl;
-    cout << blackPegs << endl;
-    return 0;
+    return black + white;
 }
