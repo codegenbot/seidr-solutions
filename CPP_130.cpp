a3dc8f72e27c3195c0569b8cb9f96170bcbde47f
beep boop <githubbot@vadim.me>
2024-07-20 13:27:40 +0200

    Solution 9 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kakawaro Prompt: Complete the following code given the task description and function signature.

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -7,7 +7,7 @@ vector<int> result;
                 if (i % 2 == 0) {
                     result.push_back(1 + i / 2);
                 } else {
-                    result.push_back(result[i - 1] + result[i - 2] + result[i + 1]);
+                    result.push_back(result[i - 1] + result[i - 2] + result[i - 3]);
                 }
             }
         }
