6ee3d886e96ae5e2d147ea5f6859053bdeb6f235
beep boop <githubbot@vadim.me>
2024-07-20 06:09:36 +0200

    Solution 69 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6uquq1ll Prompt:       |                                   ^~~~~~ 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp:21:42: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp:21:49: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp:21:56: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp:20: 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp: In function ‘int main()’: 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp:33:20: error: ‘get_row’ was not declare d in this scope    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 4e4a1cde-9aa1-47b6-85cf-51c658113718.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,7 +1,15 @@
 #include <vector>
 
 bool issame(vector<vector<int>> a, vector<vector<int>> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
@@ -25,5 +33,4 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
 
 int main() {
     // Your main function code here
-    return 0;
 }
