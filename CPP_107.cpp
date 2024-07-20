cf87d677491c4cb526915ccd595ceacfc2e68ea7
beep boop <githubbot@vadim.me>
2024-07-20 04:24:45 +0200

    Solution 8 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/c1dovke2 Prompt:       |                           ^~~~~~ bad340a4-8541-4275-876c-8ac1d70c87b4.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bad340a4-8541-4275-876c-8ac1d70c87b4.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bad340a4-8541-4275-876c-8ac1d70c87b4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bad340a4-8541-4275-876c-8ac1d70c87b4.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bad340a4-8541-4275-876c-8ac1d70c87b4.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ bad340a4-8541-4275-876c-8ac1d70c87b4.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,29 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
-}
-
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> result(2, 0);
-    for (int i = 1; i <= n; ++i) {
-        std::string num = std::to_string(i);
-        std::string rev_num = num;
-        std::reverse(rev_num.begin(), rev_num.end());
-        if (num == rev_num) {
-            if (i % 2 == 0) {
-                result[0]++;
-            } else {
-                result[1]++;
-            }
-        }
-    }
-    return result;
 }
 
 int main() {
