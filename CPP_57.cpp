f127c459d1bbbadcf42708f6c9fea704fc809c64
beep boop <githubbot@vadim.me>
2024-07-20 02:16:21 +0200

    Solution 4 of CPP/57, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/a7ci0qm9 Prompt: Complete the following code given the task description and function signature.

--- CPP_57.cpp
+++ CPP_57.cpp
@@ -1,10 +1,16 @@
+#include <vector>
+using namespace std;
+
 bool monotonic(vector<float> l) {
-    bool inc = true, dec = true;
-    for(int i=1; i<l.size(); i++) {
-        if(l[i] > l[i-1])
-            dec = false;
-        else if(l[i] < l[i-1])
-            inc = false;
+    bool increasing = true;
+    bool decreasing = true;
+    
+    for (int i = 1; i < l.size(); i++) {
+        if (l[i] > l[i - 1])
+            decreasing = false;
+        else if (l[i] < l[i - 1])
+            increasing = false;
     }
-    return inc || dec;
+    
+    return increasing || decreasing;
 }
