423fcf43378611eb84cb1e03a17d003b0574341d
beep boop <githubbot@vadim.me>
2024-07-20 04:29:36 +0200

    Solution 2 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cxtjjmn9 Prompt: Complete the following code given the task description and function signature.

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,31 +1,35 @@
-#include <stdio.h>
-#include <vector>
-
+#include <iostream>
 using namespace std;
 
-int prime_fib(int n) {
-    vector<int> fib = {0, 1};
-    int i = 2;
-    while (fib.size() <= n) {
-        fib.push_back(fib[i-1] + fib[i]);
-        i++;
-    }
-    for (int j = fib.size()-1; j >= 0; j--) {
-        if (isPrime(fib[j])) {
-            return fib[j];
-        }
-    }
-    return -1;
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return false;
+    return true;
 }
 
-bool isPrime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return false;
+int prime_fib(int n) {
+    int a = 0, b = 1, fib = 0;
+    int count = 0;
+
+    while (true) {
+        fib = a + b;
+        a = b;
+        b = fib;
+        if (isPrime(fib)) {
+            count++;
+            if (count == n)
+                return fib;
         }
     }
-    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    cout << "The " << n << "th prime Fibonacci number is: " << prime_fib(n) << endl;
+    return 0;
 }
