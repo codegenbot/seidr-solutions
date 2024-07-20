d51a6e497429315e8792f10650fb6e41678f6b43
beep boop <githubbot@vadim.me>
2024-07-20 09:28:26 +0200

    Solution 70 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4wrdftbq Prompt: d08a2ce2-c070-4924-91be-7147ee7c6aac.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int pairedDigits(std::string s) {       |                       ^~~~~~ d08a2ce2-c070-4924-91be-7147ee7c6aac.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int pairedDigits(std::string s) {

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,21 +1,13 @@
-#include <iostream>
 #include <string>
 
 int pairedDigits(std::string s) {
     int sum = 0;
-    s = s.substr(1);
-    for (int i = 0; i < s.length() - 1; i++) {
-        if ((s[i] == '0') || (s[i] - '0' == s[i + 1] - '0')) {
-            sum += (s[i] - '0');
+    while (s[0] == '0' && s.length() > 1) s.erase(0, 1);
+    std::string str = s; 
+    for (int i = 0; i < str.length() - 1; i++) {
+        if ((i < str.length() - 1) && ((str[i] == '0') || (str[i] - '0' == str[i + 1] - '0'))) {
+            sum += (str[i] - '0');
         }
     }
     return sum;
-}
-
-int main() {
-    std::string s;
-    std::cout << "Enter a string of digits: ";
-    std::cin >> s;
-    int result = pairedDigits(s);
-    std::cout << "Sum of the paired digits is: " << result << std::endl;
 }
