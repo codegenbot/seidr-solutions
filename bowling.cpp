66fc1ce26c95008619cecf028f6ab908eae3df69
beep boop <githubbot@vadim.me>
2024-07-20 07:30:14 +0200

    Solution 12 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/bjeyu258 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,24 +1,30 @@
-int bowlingScore(string input) {
-    int total = 0;
-    bool prevStrike = false;
-    for (int i = 0; i < 10; i++) {
-        if (input[i] == 'X') {
-            total += 10 + (prevStrike ? 10 : 0);
-            prevStrike = true;
-        } else if (input[i] == '/') {
-            int firstRoll = input[i - 1] - '0';
-            int secondRoll = input[i + 1] - '0';
-            total += firstRoll + secondRoll;
-            prevStrike = false;
+int bowlingScore(string &s) {
+    int score = 0;
+    int i = 0;
+    while (i < s.length()) {
+        if (s[i] == 'X') {
+            score += 30;
+            i++;
+        } else if (s[i] == '/') {
+            string first = s.substr(i - 2, 1);
+            string second = s.substr(i + 1, 1);
+            int firstNum = first[0] - '0';
+            int secondNum = second[0] - '0';
+            score += firstNum * 10 + secondNum;
+            i++;
         } else {
-            int firstRoll = input[i] - '0';
-            if (prevStrike) {
-                total += firstRoll;
+            int num = s[i] - '0';
+            if (s[i+1] == 'X' || s[i-1] == '/') {
+                score += 10 + num*10;
+                i++;
             } else {
-                total += firstRoll + (input[i + 1] == '/' ? 0 : input[i + 1] - '0');
+                string nextTwo = s.substr(i + 1, 2);
+                int nextNum = nextTwo[0] - '0';
+                int nextNextNum = nextTwo[1] - '0';
+                score += num * 10 + nextNum + nextNextNum;
+                i += 2;
             }
-            prevStrike = false;
         }
     }
-    return total;
+    return score;
 }
