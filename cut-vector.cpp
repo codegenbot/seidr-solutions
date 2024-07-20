849f64b7a96defba1dc242abeb65511a1ae1696a
beep boop <githubbot@vadim.me>
2024-07-20 04:18:39 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/sif4p2ox Prompt:   +++ |+#include <iostream>     2 | using namespace std; fcd2fe4f-3b72-4f1f-adec-59d9fd2b0c5c.cpp:47:13: error: ‘cout’ was not declared i n this scope    47 |             cout << x << " ";       |             ^~~~ fcd2fe4f-3b72-4f1f-adec-59d9fd2b0c5c.cpp:47:13: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? fcd2fe4f-3b72-4f1f-adec-59d9fd2b0c5c.cpp:49:9: error: ‘cout’ was not declared in  this scope    49 |         cout << endl;       |         ^~~~ fcd2fe4f-3b72-4f1f-adec-59d9fd2b0c5c.cpp:49:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? fcd2fe4f-3b72-4f1f-adec-59d9fd2b0c5c.cpp:49:17: error: ‘endl’ was not declared i n this scope    49 |         cout << endl;       |                 ^~~~ fcd2fe4f-3b72-4f1f-adec-59d9fd2b0c5c.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,15 +1,54 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
-    vector<vector<int>> res(2);
-    for (int i = 1; i < n; i++) {
-        if (v[i] - v[0] <= v[n-1] - v[i]) {
-            res[0].insert(res[0].end(), v.begin(), v.begin() + i+1);
-            res[1].insert(res[1].begin(), v.begin() + i, v.end());
+    vector<vector<int>> result(2);
+    
+    for (int i = 0; i < n; i++) {
+        if (i == 0 || i == n - 1) {
+            result[0] = {v.begin(), v.end()};
+            result[1].clear();
+            break;
+        }
+        
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j < i; j++) {
+            leftSum += *v.begin() + j;
+        }
+        for (int j = i; j < n; j++) {
+            rightSum += *v.begin() + j;
+        }
+        
+        if (leftSum == rightSum) {
+            result[0] = vector<int>(v.begin(), v.begin() + i);
+            result[1] = vector<int>(v.begin() + i, v.end());
             break;
         }
     }
-    return res;
+    
+    return result;
+}
+
+int main() {
+    int num;
+    vector<int> v;
+
+    cin >> num;
+    while (num--) {
+        cin >> num;
+        v.push_back(num);
+    }
+
+    auto res = cutVector(v);
+
+    for (auto& vec : res) {
+        for (int x : vec) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
