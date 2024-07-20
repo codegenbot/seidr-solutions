7fcd9cb0b68e94dcb1b024bb590f5164f115b17d
beep boop <githubbot@vadim.me>
2024-07-20 05:24:03 +0200

    Solution 23 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7inv7d7v Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc85a024-563d-4910-898c-ee6f3421030d.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc85a024-563d-4910-898c-ee6f3421030d.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc85a024-563d-4910-898c-ee6f3421030d.cpp:10: fc85a024-563d-4910-898c-ee6f3421030d.cpp: In function ‘int main()’: fc85a024-563d-4910-898c-ee6f3421030d.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ fc85a024-563d-4910-898c-ee6f3421030d.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,19 +1,26 @@
-void sort_third(vector<int>& v) {
-    for (int i = 0; i < v.size(); i += 3) {
-        if (i + 2 < v.size()) {
-            sort(v.begin() + i, v.begin() + i + 3);
-        }
+void sort_third(vector<int>& l) {
+    for (int i = 0; i < l.size(); i += 3) {
+        sort(l.begin() + i, l.begin() + i + 3);
     }
 }
 
-bool issame(vector<int>& v1, vector<int>& v2) {
+bool issame(const vector<int>& v1, const vector<int>& v2) {
     return v1 == v2;
 }
 
 int main() {
+    vector<int> l = {9, 1, 3, 6, 8, 2, 5, 4, 7};
     vector<int> result = l;
+    
     sort_third(result);
-    vector<int> expected = l;
-    sort(expected.begin(), expected.end());
-    return issame(result, expected);
+    
+    vector<int> expected = {3, 1, 9, 2, 6, 8, 4, 5, 7};
+    
+    if (issame(result, expected)) {
+        cout << "Output is correct." << endl;
+    } else {
+        cout << "Output is incorrect." << endl;
+    }
+    
+    return 0;
 }
