a9a42e9b91f6b9519d6b1ae708506d32b6e760a2
beep boop <githubbot@vadim.me>
2024-07-20 04:50:23 +0200

    Solution 39 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/cmp1bynf Prompt: /usr/bin/ld: /tmp/ccLtrArL.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x38): undefined reference to `O PENSSL_init_crypto' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x47): undefined re ference to `OPENSSL_init_crypto' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x56): undefined re ference to `EVP_get_digestbyname' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x8e): undefined re ference to `EVP_MD_CTX_new' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0xaa): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x10d): undefined r eference to `EVP_DigestUpdate' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x151): undefined r eference to `EVP_DigestFinal_ex' /usr/bin/ld: 5247f61a-5fc1-44f5-8d12-7768029a98e1.cpp:(.text+0x18a): undefined r eference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,1 +1,26 @@
-#include <openssl/crypto.h>
+#include <string>
+#include <openssl/evp.h>
+#include <cassert>
+
+std::string string_to_md5(const std::string& input) {
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md;
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+
+    OpenSSL_add_all_digests();
+    OPENSSL_init_crypto(0, nullptr);
+
+    md = EVP_get_digestbyname("md5");
+    assert(md != nullptr);
+
+    mdctx = EVP_MD_CTX_new();
+    assert(EVP_DigestInit_ex(mdctx, md, nullptr) == 1);
+    assert(EVP_DigestUpdate(mdctx, input.c_str(), input.size()) == 1);
+    assert(EVP_DigestFinal_ex(mdctx, md_value, &md_len) == 1);
+    
+    EVP_MD_CTX_free(mdctx);
+
+    std::string md5_hash((char*)md_value, md_len);
+    return md5_hash;
+}
