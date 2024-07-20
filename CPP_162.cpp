51215679860540ded3dd5a59e46afb38fd1586bb
beep boop <githubbot@vadim.me>
2024-07-20 06:41:26 +0200

    Solution 22 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/h6gqhgq1 Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,24 +1,25 @@
+#include <string>
 #include <openssl/evp.h>
+
+using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
-    
-    unsigned char md5[MD5_DIGEST_LENGTH];
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
+
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5_CTX md5;
+    MD5_Init(&md5);
     const char* ptr = text.c_str();
     while (*ptr) {
-        MD5_Update(&ctx, ptr, 1);
+        MD5_Update(&md5, ptr, 1);
         ptr++;
     }
-    MD5_Final(md5, &ctx);
+    MD5_Final(result, &md5);
 
-    string result;
+    ostringstream oss;
     for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        char buff[3];
-        sprintf(buff, "%02x", md5[i]);
-        result += buff;
+        oss << setfill('0') << setw(2) << hex << (int)result[i];
     }
 
-    return result;
+    return oss.str();
 }
