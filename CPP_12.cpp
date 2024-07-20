b2c4772715cc1de7a76e76b402fd65ce83c45c91
beep boop <githubbot@vadim.me>
2024-07-20 04:21:01 +0200

    Solution 84 of CPP/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4ffsteye Prompt: Complete the following code given the task description and function signature.

--- CPP_12.cpp
+++ CPP_12.cpp
@@ -1,12 +1,8 @@
 string longest(vector<string> strings){
     if(strings.empty()) return "";
     string longest = strings[0];
-    for(auto s : strings){
-        if(s.length() > longest.length()){
-            longest = s;
-        } else if (s.length() == longest.length()){
-            longest = s;
-        }
+    for(auto s : strings) {
+        if(s.length() > longest.length()) longest = s;
     }
     return longest;
 }
