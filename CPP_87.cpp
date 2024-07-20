1f9566ab5e9395b37b44b8e2f2417a3cb1f5ad9a
beep boop <githubbot@vadim.me>
2024-07-20 03:02:33 +0200

    Solution 9 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/e03hsnz7 Prompt: 22593177-2ae8-4ac2-ae37-7f196052abba.cpp:36:6: error: redefinition of ‘bool issa me(std::vector<std::vector<int> >, std::vector<std::vector<int> >)’    36 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |      ^~~~~~ 22593177-2ae8-4ac2-ae37-7f196052abba.cpp:8:6: note: ‘bool issame(std::vector<std ::vector<int> >, std::vector<std::vector<int> >)’ previously defined here     8 | bool issame(vector<vector<int>> a, vector<vector<int>> b) {       |      ^~~~~~ 22593177-2ae8-4ac2-ae37-7f196052abba.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 22593177-2ae8-4ac2-ae37-7f196052abba.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -5,16 +5,12 @@
 
 using namespace std;
 
-bool issame(vector<vector<int>> a, vector<vector<int>> b) {
-    return a == b;
-}
-
 vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     vector<vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
             if (lst[i][j] == x) {
-                result.push_back({i, j});
+                result.push_back({ i, j });
             }
         }
     }
@@ -24,10 +20,5 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
         }
         return a[0] < b[0];
     });
-    return result;
-}
-
-int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
-    return 0;
+    assert(result == vector<vector<int>>{ { 2, 2 } });
 }
