9f8b82f6bf1a65ade46da3258d62d18bb1dc11f1
beep boop <githubbot@vadim.me>
2024-07-20 06:25:06 +0200

    Solution 33 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,68 +1,39 @@
-#include <boost/any.hpp>
-#include <string>
-#include <algorithm>
-
-using namespace std;
+```cpp
+#include <boost/any_cast.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max(a.convert_to<int>(), b.convert_to<float>());
+        return boost::any_cast<float>(b) > static_cast<int>(a) ? b : a;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return boost::any_cast<double>(b) > static_cast<int>(a) ? b : a;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return max(a.convert_to<float>(), b.convert_to<int>());
+        return boost::any_cast<float>(a) > static_cast<int>(b) ? a : b;
+    }
+    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return boost::any_cast<double>(a) > static_cast<int>(b) ? a : b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = a.convert_to<string>();
-        string str2 = b.convert_to<string>();
-
-        int num1 = 0, num2 = 0;
-        bool decimal1 = false, decimal2 = false;
-
-        for (int i = 0; i < str1.length(); i++) {
-            if (!decimal1 && !isdigit(str1[i])) {
-                decimal1 = true;
-                continue;
-            }
-            else if (str1[i] == '.') {
-                decimal1 = true;
-                continue;
-            }
-            num1 = num1 * 10 + (str1[i] - '0');
-        }
-
-        for (int i = 0; i < str2.length(); i++) {
-            if (!decimal2 && !isdigit(str2[i])) {
-                decimal2 = true;
-                continue;
-            }
-            else if (str2[i] == '.') {
-                decimal2 = true;
-                continue;
-            }
-            num2 = num2 * 10 + (str2[i] - '0');
-        }
-
-        if (!decimal1 && !decimal2) {
-            return str1 > str2 ? a : b;
-        }
-        else if (decimal1 && decimal2) {
-            return str1 > str2 ? a : b;
-        }
-        else if (!decimal1 && decimal2) {
-            return num1 > num2 ? a : b;
-        }
-        else {
-            return num1 < num2 ? a : b;
-        }
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        return str1 > str2 ? a : b;
+    }
+    else if ((a.type() == typeid(int) || a.type() == typeid(float) || a.type() == typeid(double)) &&
+             (b.type() == typeid(string))) {
+        string str = boost::any_cast<string>(b);
+        double num = boost::any_cast<double>(a);
+        return num > stod(str) ? a : b;
+    }
+    else if ((a.type() == typeid(string)) && (b.type() == typeid(int) || b.type() == typeid(float) || b.type() == typeid(double))) {
+        string str = boost::any_cast<string>(a);
+        double num = boost::any_cast<double>(b);
+        return stod(str) > num ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        return "None";
     }
     else {
-        int a_int = a.convert_to<int>();
-        int b_int = b.convert_to<int>();
-
-        if (a_int == b_int) {
-            return boost::any("None");
-        }
-
-        return a_int > b_int ? a : b;
+        return a > b ? a : b;
     }
 }
