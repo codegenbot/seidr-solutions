0da2096e84dd8569cb933ec8825689e18bca54b3
beep boop <githubbot@vadim.me>
2024-07-20 03:30:37 +0200

    Solution 69 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bnfqx7vn Prompt:       |                    ^~~ af821a87-8c4c-474d-bbc8-96211807e9e0.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ af821a87-8c4c-474d-bbc8-96211807e9e0.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ af821a87-8c4c-474d-bbc8-96211807e9e0.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from af821a87-8c4c-474d-bbc8-96211807e9e0.cpp:12: af821a87-8c4c-474d-bbc8-96211807e9e0.cpp: In function ‘int main()’: af821a87-8c4c-474d-bbc8-96211807e9e0.cpp:22:20: error: ‘maximum’ was not declare d in this scope    22 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ af821a87-8c4c-474d-bbc8-96211807e9e0.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,11 +2,30 @@
 
 using namespace std;
 
-vector<int> maximum(vector<int> arr, int k) {
-    vector<int> result;
-    sort(arr.begin(), arr.end());
-    for (int i = 0; i < k; i++) {
-        result.push_back(arr[arr.size() - 1 - i]);
+bool areEqual(vector<int> v1, vector<int> v2) {
+    if(v1.size() != v2.size())
+        return false;
+    
+    for(int i = 0; i < v1.size(); i++) {
+        if(v1[i] != v2[i])
+            return false;
     }
-    return result;
+    
+    return true;
+}
+
+int main() {
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {4, 5, 6};
+    vector<int> v3 = {7, 8, 9};
+    
+    bool result = areEqual(v1, v2);
+    
+    if(result) {
+        cout << "The two vectors are equal." << endl;
+    }
+    else
+        cout << "The two vectors are not equal." << endl;
+    
+    return 0;
 }
