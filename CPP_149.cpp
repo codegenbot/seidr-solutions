f84f8b442ca571842cf2b20e81f445bb6db04219
beep boop <githubbot@vadim.me>
2024-07-20 03:49:00 +0200

    Solution 26 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lpolsawb Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:19: 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp: In function ‘int main()’: 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,14 +1,9 @@
 bool issame(vector<string> a,vector<string>b){
-    if(a.size()!=b.size()) return false;
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
+    if(a.size()!=b.size())return false;
     for(int i=0;i<a.size();i++){
-        if(a[i]!=b[i]) return false;
+        for(int j=0;j<a[i].size();j++){
+            if(a[i][j]!=b[i][j])return false;
+        }
     }
     return true;
-}
-
-int main() {
-    assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cccc", "dd", "aaaa", "bbbb"}));
-    return 0;
 }
