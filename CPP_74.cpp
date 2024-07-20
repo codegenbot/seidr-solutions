dcf51581c8ca297e0678e6b4a98970ec2c665642
beep boop <githubbot@vadim.me>
2024-07-20 06:01:35 +0200

    Solution 32 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ethwtn1v Prompt:       |                              ^~~~~~ 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp:18: 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp: In function ‘int main()’: 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp:28:20: error: ‘total_match’ was not dec lared in this scope    28 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 2f19d711-e12c-4410-877c-36f1fbbeda39.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -2,25 +2,25 @@
 #include <string>
 #include <cassert>
 
-bool issame(const vector<string>& a, const vector<string>& b){
-    vector<string> total_match = a;
-    
+vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
     int total_chars1 = 0;
     int total_chars2 = 0;
     
-    for (const string& str : a) {
+    for (const string& str : lst1) {
         total_chars1 += str.size();
     }
     
-    for (const string& str : b) {
+    for (const string& str : lst2) {
         total_chars2 += str.size();
     }
     
     if (total_chars1 < total_chars2) {
-        return a;
+        return lst1;
     } else {
-        return b;
+        return lst2;
     }
 }
 
-assert(issame(total_match({"this"}, {}), {}) == true;
+bool issame(const vector<string>& a, const vector<string>& b){
+    return a == b;
+}
