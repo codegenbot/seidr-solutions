3d66f5fd80b73a3fed9147b6a73f1ddc09e51765
beep boop <githubbot@vadim.me>
2024-07-20 02:35:19 +0200

    Solution 0 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pgmeqbyq Prompt: Complete the following code given the task description and function signature.

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,6 +1,5 @@
 Here is the completed code:
 
 bool is_simple_power(int x, int n) {
-    double y = pow(n, log(x) / log(n));
-    return (y == (int)y);
+    return pow(n, ceil(log(x) / log(n))) == x;
 }
