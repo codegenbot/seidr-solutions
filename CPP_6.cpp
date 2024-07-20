a89aee4a9b044d869f000fbfac97eb4cc7974b95
beep boop <githubbot@vadim.me>
2024-07-20 04:52:35 +0200

    Solution 60 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lvaqpkwx Prompt:       |                    ^~~ 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp:20: 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp: In function ‘int main()’: 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp:30:20: error: ‘parse_nested_parens’ was  not declared in this scope    30 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 4f0fb9a2-2b22-4a17-b214-9803e9d36ede.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,18 +1,5 @@
 #include <vector>
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<int> parse_nested_parens(string paren_string) {
+vector<int> max_levels(string paren_string) {
     vector<int> levels;
     int max_level = 0;
     int current_level = 0;
@@ -30,8 +17,4 @@ vector<int> parse_nested_parens(string paren_string) {
     }
     levels.push_back(max_level);
     return levels;
-}
-
-int main() {
-    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
 }
