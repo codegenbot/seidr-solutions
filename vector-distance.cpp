53523d71f424341dae1f0fce5cb7d847145ff0b3
beep boop <githubbot@vadim.me>
2024-07-20 13:42:21 +0200

    Solution 76 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/z1wleq0u Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,8 +1,10 @@
 ```cpp
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <vector>
 
-double vectorDistance(int n, float v1[], float v2[]) {
+double vectorDistance(int n, std::vector<float> v1, std::vector<float> v2) {
     double sum = 0.0;
     for (int i = 0; i < n; i++) {
         double diff = v2[i] - v1[i];
@@ -17,20 +19,19 @@ int main() {
     std::cout << "Enter the dimension of the vectors: ";
     std::cin >> n;
 
-    float* v1 = new float[n], *v2 = new float[n];
-
+    std::vector<float> v1(n), v2(n);
     for (int i = 0; i < n; i++) {
+        float val1, val2;
         std::cout << "Enter element " << i+1 << " of vector 1: ";
-        std::cin >> v1[i];
+        std::cin >> val1;
         std::cout << "Enter element " << i+1 << " of vector 2: ";
-        std::cin >> v2[i];
+        std::cin >> val2;
+        v1[i] = val1; 
+        v2[i] = val2;
     }
 
     double distance = vectorDistance(n, v1, v2);
     std::cout << "Euclidean distance between the two vectors is: " << distance << std::endl;
-
-    delete[] v1;
-    delete[] v2;
 
     return 0;
 }
