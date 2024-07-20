47f1aa532ab0671e76d4f638b94ff6694533ca86
beep boop <githubbot@vadim.me>
2024-07-20 06:14:11 +0200

    Solution 10 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/x93c53t4 Prompt:       |                    ^~~ 81c11f01-9223-450c-892c-ce19748e4243.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 81c11f01-9223-450c-892c-ce19748e4243.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 81c11f01-9223-450c-892c-ce19748e4243.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 81c11f01-9223-450c-892c-ce19748e4243.cpp:20: 81c11f01-9223-450c-892c-ce19748e4243.cpp: In function ‘int main()’: 81c11f01-9223-450c-892c-ce19748e4243.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 81c11f01-9223-450c-892c-ce19748e4243.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,11 +1,17 @@
-vector<int> unique_digits(vector<int> x){
+#include <vector>
+#include <algorithm>
+
+bool issame(int a, int b) {
+    return a == b;
+}
+
+vector<int> unique_digits(const vector<int>& x) {
     vector<int> result;
     for (int num : x) {
-        bool hasEvenDigit = false;
         int temp = num;
+        bool hasEvenDigit = false;
         while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
+            if ((temp % 10) % 2 == 0) {
                 hasEvenDigit = true;
                 break;
             }
@@ -15,6 +21,6 @@ vector<int> unique_digits(vector<int> x){
             result.push_back(num);
         }
     }
-    sort(result.begin(), result.end());
+    sort(result.begin(), result.end(), issame);
     return result;
 }
