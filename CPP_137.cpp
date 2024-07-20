b5548bca5b439e0b2de36d4b85fd0fcbc22f4972
beep boop <githubbot@vadim.me>
2024-07-20 03:53:07 +0200

    Solution 89 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y1nh82vq Prompt: 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:3:1: error: stray ‘`’ in program     3 | ```cpp       | ^ 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:3:2: error: stray ‘`’ in program     3 | ```cpp       |  ^ 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,30 +5,30 @@ using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == b.type()) {
-        if (a.type() == type_id<int>()) {
+        if (a.type() == typeid(int)) {
             int x = boost::any_cast<int>(a);
             int y = boost::any_cast<int>(b);
             return (x > y) ? a : ((x < y) ? b : any("None"));
-        } else if (a.type() == type_id<double>()) {
+        } else if (a.type() == typeid(double)) {
             double x = boost::any_cast<double>(a);
             double y = boost::any_cast<double>(b);
             return (x > y) ? a : ((x < y) ? b : any("None"));
-        } else if (a.type() == type_id<string>()) {
+        } else if (a.type() == typeid(string)) {
             string x = boost::any_cast<string>(a);
             string y = boost::any_cast<string>(b);
             return (x > y) ? a : ((x < y) ? b : any("None"));
         }
     } else {
-        if (a.type() == type_id<int>()) {
+        if (a.type() == typeid(int)) {
             int x = boost::any_cast<int>(a);
             double y = boost::any_cast<double>(b);
             return (y < x) ? a : b;
-        } else if (a.type() == type_id<double>()) {
+        } else if (a.type() == typeid(double)) {
             double x = boost::any_cast<double>(a);
             string y = boost::any_cast<string>(b);
             if (stod(y) > x) return b;
             else return a;
-        } else if (a.type() == type_id<string>()) {
+        } else if (a.type() == typeid(string)) {
             string x = boost::any_cast<string>(a);
             double y = boost::any_cast<double>(b);
             if (stod(x) < y) return a;
