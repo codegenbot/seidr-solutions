e32d99e80ac9281f073bd88feb27fb1bae6c6e57
beep boop <githubbot@vadim.me>
2024-07-20 02:09:31 +0200

    Solution 8 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/oheqdp1c Prompt: d913fc69-2cba-4fb9-b2e4-e5636228c7ea.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ d913fc69-2cba-4fb9-b2e4-e5636228c7ea.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ d913fc69-2cba-4fb9-b2e4-e5636228c7ea.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ d913fc69-2cba-4fb9-b2e4-e5636228c7ea.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 vector<int> incr_list(vector<int> l) {
     for (int &num : l) {
         num++;
@@ -11,7 +15,7 @@ vector<int> incr_list(vector<int> l) {
 }
 
 int main() {
-    assert(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) == vector<int>({6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
 
     return 0;
 }
