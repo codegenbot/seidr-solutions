63beaa4bb9139db3cfc77bf9a2485ca74c421cb4
beep boop <githubbot@vadim.me>
2024-07-20 04:14:47 +0200

    Solution 23 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/38kvdprn Prompt: on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3f87e52b-5964-4550-979e-6d8213ce2248.cpp:39:39: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 3f87e52b-5964-4550-979e-6d8213ce2248.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 3f87e52b-5964-4550-979e-6d8213ce2248.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from 3f87e52b-5964-4550-979e-6d8213ce2248.cpp:38: 3f87e52b-5964-4550-979e-6d8213ce2248.cpp: In function ‘int main()’: 3f87e52b-5964-4550-979e-6d8213ce2248.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -14,9 +14,20 @@ bool is_same(std::vector<int> a, std::vector<int> b) {
 }
 
 std::vector<int> count_up_to(int n) {
-    std::vector<int> primes;
-    // Implement logic to generate prime numbers up to n and store them in "primes" vector
-    return primes;
+    std::vector<int> result;
+    for (int num = 2; num <= n; ++num) {
+        bool is_prime = true;
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) {
+            result.push_back(num);
+        }
+    }
+    return result;
 }
 
 int main() {
