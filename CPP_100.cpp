bea2a935e327a73ba058a1844f3ddf665c1ee819
beep boop <githubbot@vadim.me>
2024-07-20 02:56:24 +0200

    Solution 15 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5jyjnqic Prompt:       |                    ^~~ b225d747-0a36-4d60-a6e5-ee696acf188c.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b225d747-0a36-4d60-a6e5-ee696acf188c.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b225d747-0a36-4d60-a6e5-ee696acf188c.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b225d747-0a36-4d60-a6e5-ee696acf188c.cpp:19: b225d747-0a36-4d60-a6e5-ee696acf188c.cpp: In function ‘int main()’: b225d747-0a36-4d60-a6e5-ee696acf188c.cpp:29:20: error: ‘make_a_pile’ was not dec lared in this scope    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ b225d747-0a36-4d60-a6e5-ee696acf188c.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,11 +1,14 @@
-```cpp
 #include <vector>
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
@@ -28,4 +31,3 @@ std::vector<int> make_a_pile(int n) {
 int main() {
     assert(std::equal(make_a_pile(8).begin(), make_a_pile(8).end(), {8, 10, 12, 14, 16, 18, 20, 22}));
     return 0;
-}
