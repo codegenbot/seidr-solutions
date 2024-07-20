29394a6c72e28113bb2aad509b34d15e7ad76792
beep boop <githubbot@vadim.me>
2024-07-20 05:49:15 +0200

    Solution 30 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/atrqo8ra Prompt:       |                    ^~~ 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp:15: 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp: In function ‘int main()’: 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp:25:20: error: ‘strange_sort_list’ was n ot declared in this scope    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 6716c845-e983-4c27-82b3-bf7c117a7f4d.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,19 +1,32 @@
 #include <vector>
-using namespace std;
+#include <algorithm>
+#include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
+bool issame(vector<int> a, vector<int> b);
 
-vector<int> strange_sort_list(vector<int> lst){
+vector<int> strange_sort_list(vector<int> lst) {
     sort(lst.begin(), lst.end());
     vector<int> result;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
-        result.push_back(lst[left++]);
-        if (left <= right) {
-            result.push_back(lst[right--]);
+        if (left == right) {
+            result.push_back(lst[left]);
+            break;
         }
+        result.push_back(lst[left++]);
+        result.push_back(lst[right--]);
     }
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
