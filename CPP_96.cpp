be81e3a8232a2e87989de989ae6aa5688a31b251
beep boop <githubbot@vadim.me>
2024-07-20 03:56:24 +0200

    Solution 1 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/38kvdprn Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 31abe5b1-75ca-4cb5-9cf6-622829d41b6e.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 31abe5b1-75ca-4cb5-9cf6-622829d41b6e.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 31abe5b1-75ca-4cb5-9cf6-622829d41b6e.cpp:16: 31abe5b1-75ca-4cb5-9cf6-622829d41b6e.cpp: In function ‘int main()’: 31abe5b1-75ca-4cb5-9cf6-622829d41b6e.cpp:26:20: error: ‘count_up_to’ was not dec lared in this scope    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 31abe5b1-75ca-4cb5-9cf6-622829d41b6e.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,14 +1,28 @@
-vector<int> primes;
-    for (int i = 2; i < n; ++i) {
-        bool is_prime = true;
-        for (int j = 2; j * j <= i; ++j) {
-            if (i % j == 0) {
-                is_prime = false;
-                break;
-            }
-        }
-        if (is_prime) {
-            primes.push_back(i);
+bool issame(vector<int> a, vector<int> b);
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    return primes;
+    return true;
+}
+
+vector<int> primes;
+for (int i = 2; i < n; ++i) {
+    bool is_prime = true;
+    for (int j = 2; j * j <= i; ++j) {
+        if (i % j == 0) {
+            is_prime = false;
+            break;
+        }
+    }
+    if (is_prime) {
+        primes.push_back(i);
+    }
+}
+return primes;
