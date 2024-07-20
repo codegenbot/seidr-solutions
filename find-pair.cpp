dc2931d36a553dae6dc640ff476152a4837bf632
beep boop <githubbot@vadim.me>
2024-07-20 08:03:19 +0200

    Solution 15 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/sgrfa9sa Prompt: Given a vector of integers, return the two elements that sum to a target integer.

--- find-pair.cpp
+++ find-pair.cpp
@@ -3,31 +3,33 @@ using namespace std;
 
 vector<int> findPair(vector<int>& nums, int target) {
     vector<int> result;
-    unordered_map<int, int> numMap;
-    
     for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
-        if (numMap.count(complement)) {
-            result.push_back(nums[i]);
-            result.push_back(complement);
-            return result;
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                result.push_back(nums[i]);
+                result.push_back(nums[j]);
+                return result;
+            }
         }
-        numMap[nums[i]] = i;
     }
-    
     return result;
 }
 
 int main() {
-    int n;
+    int n, x;
     cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
+    vector<int> v;
+    while(n--) {
+        cin >> x;
+        v.push_back(x);
     }
+
     int target;
     cin >> target;
-    vector<int> res = findPair(nums, target);
-    cout << res[0] << "\n" << res[1];
+
+    vector<int> res = findPair(v, target);
+
+    cout << res[0] << endl << res[1] << endl;
+
     return 0;
 }
