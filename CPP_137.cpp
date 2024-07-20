246327dd57345f09d640323e6568d9259df93042
beep boop <githubbot@vadim.me>
2024-07-20 03:42:19 +0200

    Solution 47 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y1nh82vq Prompt: 5a0ceebf-38b9-48d5-852c-b706ebfa0e42.cpp:4:9: fatal error: boost/any.hpp: No suc h file or directory     4 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,8 @@
+#include <stdio.h>
+#include <string>
+#include <algorithm>
 #include <boost/any.hpp>
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
@@ -12,22 +16,22 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any((string)"None");
+            return boost::any("None");
         }
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        if ((int)a.convert_to<int>() > (string)b.convert_to<string>().erase(0, 1).erase((string)b.convert_to<string())).length()-1) {
+        // removed code here
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        if ((float)a.convert_to<float>() > (string)b.convert_to<string>().erase(0, 1).erase((string)b.convert_to<string())).length()-1) {
             return a;
         }
-        else if ((int)a.convert_to<int>() < (string)b.convert_to<string>().erase(0, 1).erase((string)b.convert_to<string()">)).length()-1) {
+        else if ((float)a.convert_to<float>() < (string)b.convert_to<string>().erase(0, 1).erase((string)b.convert_to<string()">)).length()-1) {
             return b;
         }
         else {
-            return boost::any((string)"None");
+            return boost::any("None");
         }
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
     }
     else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         if ((string)a.convert_to<string>() > to_string((int)b.convert_to<int>())) {
@@ -37,62 +41,35 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return boost::any((string)"None");
+            return boost::any("None");
         }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(float)) {
         if ((string)a.convert_to<string>() > to_string((float)b.convert_to<float>()).erase(0, 1).erase((string)b.convert_to<string()">)).length()-1) {
             return a;
         }
-        else if ((string)a.convert_to<string>() < to_string((float)b.convert_to<float>()).erase(0, 1).erase((string)b.convert_to<string()">)).length()-1) {
+        else if ((string)a.convert_to<string>() < to_string((float)b.convert_to<float>()).erase(0, 1).erase((string>b.convert_to<string()">))).length()-1) {
             return b;
         }
         else {
-            return boost::any((string)"None");
+            return boost::any("None");
         }
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        if ((float)a.convert_to<float>() > (string)b.convert_to<string>().erase(0, 1).erase((string)b.convert_to<string()">)).length()-1) {
-            return a;
-        }
-        else if ((float)a.convert_to<float>() < (string)b.convert_to<string>().erase(0, 1).erase((string)b.convert_to<string()">)).length()-1) {
-            return b;
-        }
-        else {
-            return boost::any((string)"None");
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        if ((string)a.convert_to<string>() > to_string((float)b.convert_to<float>())) {
-            return a;
-        }
-        else if ((string)a.convert_to<string>() < to_string((float)b.convert_to<float>())) {
-            return b;
-        }
-        else {
-            return boost::any((string)"None");
-        }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if ((string)a.convert_to<string>() > (string)b.convert_to<string>()) {
-            return a;
-        }
-        else if ((string)a.convert_to<string>() < (string)b.convert_to<string>()) {
-            return b;
-        }
-        else {
-            return boost::any((string)"None");
-        }
+        return (string)a.convert_to<string>() > (string)b.convert_to<string>()) ? a : ((string)a.convert_to<string>() < (string>b.convert_to<string>()) ? b : boost::any("None"));
     }
     else {
         if ((float)a.convert_to<float>() > (float)b.convert_to<float>()) {
             return a;
         }
-        else if ((float)a.convert_to<float>() < (float)b.convert_to<float>()) {
+        else if ((float)a.convert_to<float>() < (float>b.convert_to<float>())) {
             return b;
         }
         else {
-            return boost::any((string)"None");
+            return boost::any("None");
         }
     }
 }
