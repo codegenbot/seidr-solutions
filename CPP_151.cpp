c1a1587b74eb093b5a037da76666b815cf43e865
beep boop <githubbot@vadim.me>
2024-07-20 03:59:23 +0200

    Solution 60 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/nsq96stg Prompt: 258664a2-e64d-45a7-aa41-6df406184c02.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst) {       |                                 ^~~~~~ 258664a2-e64d-45a7-aa41-6df406184c02.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst) {       |                                        ^~~~~ In file included from 258664a2-e64d-45a7-aa41-6df406184c02.cpp:13: 258664a2-e64d-45a7-aa41-6df406184c02.cpp: In function ‘int main()’: 258664a2-e64d-45a7-aa41-6df406184c02.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 258664a2-e64d-45a7-aa41-6df406184c02.cpp:15:38: error: ‘double_the_difference’ c annot be used as a function    15 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 258664a2-e64d-45a7-aa41-6df406184c02.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,9 +1,12 @@
+#include <iostream>
 #include <vector>
 #include <cmath>
 
-long long double_the_difference(std::vector<float> lst) {
+using namespace std;
+
+long long double_the_difference(vector<double> lst) {
     long long sum = 0;
-    for (float num : lst) {
+    for (double num : lst) {
         if (num > 0 && floor(num) == num) { 
             if (fmod(num, 2.0) != 0.0) { 
                 sum += pow(num, 2); 
@@ -11,4 +14,18 @@ long long double_the_difference(std::vector<float> lst) {
         }
     }
     return sum;
+}
+
+int main() {
+    vector<double> lst = {1, 2, 3};
+    double odd_sum = 0;
+    for (double num : lst) {
+        if (num > 0 && floor(num) == num) { 
+            if (fmod(num, 2.0) != 0.0) { 
+                odd_sum += pow(num, 2); 
+            }
+        }
+    }
+    assert(double_the_difference(lst) == odd_sum );
+    return 0;
 }
