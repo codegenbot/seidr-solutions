dbdfc4024c685016277dbbdd7f38fb3ee72215a4
beep boop <githubbot@vadim.me>
2024-07-20 04:29:09 +0200

    Solution 90 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5ztigqco Prompt: Complete the following code given the task description and function signature.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,14 +1,7 @@
-#include <boost/lexical_cast.hpp>
-
-vector<int> filter_integers(list_any values) {
+vector<int> filter_integers(list_any values){
     vector<int> result;
-    for (auto& value : values) {
-        if (boost::any_cast<bool>(value)) {
-            try {
-                int i = boost::lexical_cast<int>(value);
-                result.push_back(i);
-            } catch (boost::bad_lexical_cast&) {}
-        }
+    for (const auto& value : values) {
+        boost::any_cast<int>(value));
     }
     return result;
 }
