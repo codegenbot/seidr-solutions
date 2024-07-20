5faf54ac03c1a115e24574003387e55d54b1cc87
beep boop <githubbot@vadim.me>
2024-07-20 10:54:11 +0200

    Solution 62 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/gywzt605 Prompt:   700 |       operator=(_CharT __c)       |                 ~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h:717:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>&&) [wit h _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char> ]’ (near match)   717 |       operator=(basic_string&& __str)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:717:7: note:   passing ‘const string*’ { aka ‘const std::__cxx11::basic_string<char>*’} as ‘this’ argument discards quali fiers /usr/include/c++/11/bits/basic_string.h:785:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’   785 |       operator=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:785:42: note:   no known conversion for argument 1 from ‘std::__cxx11::basic_string<char>’ to ‘std::initializer_list<cha r>’   785 |       operator=(initializer_list<_CharT> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -7,10 +7,29 @@ std::string toCamelCase(const std::string& s) {
             result += word;
             first = false;
         } else {
-            result += toupper(word[0]);
-            result += word.substr(1).erase(0, 1);
+            result += char(toupper((word)[0])) + word.substr(1);
         }
     }
 
     return result;
+}
+
+std::string split(const std::string& s, const std::string& delimiter) {
+    size_t pos = 0;
+    std::string token;
+
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        s = s.substr(pos + delimiter.size());
+    }
+
+    return token;
+}
+
+int main() {
+    std::string input;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, input);
+    std::cout << toCamelCase(input) << std::endl;
+    return 0;
 }
