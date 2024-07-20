59e6da3df033df1e6882255d6046271378de8914
beep boop <githubbot@vadim.me>
2024-07-20 02:00:27 +0200

    Solution 4 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/017be5e7 Prompt: cept       |              ~~~~~~~~~~~~~~~~~~~~~~~^~~~~ c2a47961-7607-4498-b6d9-bff6eec0616c.cpp: At global scope: c2a47961-7607-4498-b6d9-bff6eec0616c.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ c2a47961-7607-4498-b6d9-bff6eec0616c.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from c2a47961-7607-4498-b6d9-bff6eec0616c.cpp:27: c2a47961-7607-4498-b6d9-bff6eec0616c.cpp: In function ‘int main()’: c2a47961-7607-4498-b6d9-bff6eec0616c.cpp:37:41: error: could not convert ‘find_c losest_elements(std::vector<float>(std::initializer_list<float>{((const float*)( & const float [5]{1.10000002e+0f, 2.20000005e+0f, 3.0999999e+0f, 4.0999999e+0f, 5.0999999e+0f})), 5}, std::allocator<float>()))’ from ‘std::pair<float, float>’ to ‘std::vector<float>’    37 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                         |       |                                         std::pair<float, float>

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -15,11 +15,11 @@ pair<float, float> find_closest_elements(vector<float> numbers) {
             closest_elements = {numbers[i], numbers[i + 1]};
         }
     }
-    return {closest_elements.first, closest_elements.second};
+    return closest_elements;
 }
 
 int main() {
     assert(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) == make_pair(2.2, 3.1));
-
+    
     return 0;
 }
