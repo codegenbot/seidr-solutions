d8b6d031e4f59d61813a8178f0aff9ef46358a6e
beep boop <githubbot@vadim.me>
2024-07-20 02:53:34 +0200

    Solution 5 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3zst4mto Prompt:   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 29a64a98-74d8-4619-ac23-032fd90ebf73.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 29a64a98-74d8-4619-ac23-032fd90ebf73.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 29a64a98-74d8-4619-ac23-032fd90ebf73.cpp:18: 29a64a98-74d8-4619-ac23-032fd90ebf73.cpp: In function ‘int main()’: 29a64a98-74d8-4619-ac23-032fd90ebf73.cpp:28:20: error: ‘sort_third’ was not decl ared in this scope    28 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 29a64a98-74d8-4619-ac23-032fd90ebf73.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,23 +1,16 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
-}
-
-vector<int> sort_third(vector<int> l);
-
-int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    return 0;
 }
 
 vector<int> sort_third(vector<int> l) {
     vector<int> res = l;
-    for (int i = 0; i < l.size(); i += 3) {
-        sort(res.begin() + i, res.begin() + i + 3);
+    for (size_t i = 0; i < l.size(); i += 3) {
+        sort(res.begin() + i, min(res.begin() + i + 3, res.end()));
     }
     return res;
 }
