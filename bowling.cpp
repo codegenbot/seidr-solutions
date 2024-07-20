95274b368db716f744446a2fe0fc67133a93ff69
beep boop <githubbot@vadim.me>
2024-07-20 07:36:30 +0200

    Solution 29 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/bjeyu258 Prompt: 7c718ef8-5792-44ba-a819-dc4c24474ab8.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,17 +1,56 @@
 #include <string>
-
 using namespace std;
 
 int bowlingScore(string s) {
     int score = 0;
-    for (char c : s) {
-        if (c == 'X') {
-            score += 30;
-        } else if (c == '/') {
-            score += 10 + currentRoll;
-            currentRoll = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (i + 2 <= s.length()) {
+            if (s.substr(i, 2) == "X ") {
+                score += 30;
+                i++;
+            } else if (s[i] == 'X' || s[i] == '/') {
+                int currentRoll = (s[i] == 'X') ? 10 : 10 - (s[i + 1] - '0');
+                if (i < s.length() - 1) {
+                    score += currentRoll;
+                    i++;
+                    if (i < s.length()) {
+                        if (s.substr(i, 2) != "X ") {
+                            score += (10 - (s[i] - '0')) + (10 - (s[i + 1] - '0'));
+                            i++;
+                        } else {
+                            score += currentRoll;
+                            i++;
+                        }
+                    }
+                } else if (i == s.length() - 1) {
+                    score += currentRoll;
+                }
+            } else {
+                int currentRoll = 10 - (s[i] - '0');
+                if (i < s.length() - 1 && s[i + 1] != 'X' && s[i + 1] != '/') {
+                    score += currentRoll;
+                    i++;
+                    if (i < s.length()) {
+                        if (s[i] == '0') {
+                            score += 10 + (10 - (s[i + 1] - '0'));
+                            i++;
+                        } else {
+                            score += currentRoll;
+                        }
+                    }
+                } else {
+                    score += currentRoll;
+                }
+            }
         } else {
-            currentRoll = (currentRoll * 10 + (c - '0') * 10) % 11;
+            if (s[i] == 'X') {
+                score += 30;
+            } else if (s[i] == '/') {
+                score += 10 + (10 - (s[i + 1] - '0'));
+            } else {
+                int currentRoll = 10 - (s[i] - '0');
+                score += currentRoll;
+            }
         }
     }
     return score;
