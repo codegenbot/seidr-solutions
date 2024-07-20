dbd6bf6b1e34df9619f53516a80c553626ad6379
beep boop <githubbot@vadim.me>
2024-07-20 02:28:24 +0200

    Solution 4 of CPP/56, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8pa22i7l Prompt: header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | bool correct_bracketing(const std::string& brackets) { 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp: In function ‘bool correct_bracketing(c onst int&)’: 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp:3:19: error: ‘begin’ was not declared i n this scope     3 |     for (char c : brackets) {       |                   ^~~~~~~~ 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp:3:19: error: ‘end’ was not declared in this scope In file included from 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp:16: 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp: In function ‘int main()’: 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp:18:37: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    18 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                                     ^~~~~~~~~~~~~~~~~~       |                                     |       |                                     const char* 7442c080-85d7-40eb-92e6-ee9b326094a7.cpp:1:44: note:   initializing argument 1 o f ‘bool correct_bracketing(const int&)’     1 | bool correct_bracketing(const std::string& brackets) {       |                         ~~~~~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,9 +1,7 @@
 #include <string>
-#include <cassert>
-
 bool correct_bracketing(const std::string& brackets) {
     int count = 0;
-    for (char c : brackets) {
+    for (const char& c : brackets) {
         if (c == '<') {
             count++;
         } else if (c == '>') {
@@ -14,9 +12,4 @@ bool correct_bracketing(const std::string& brackets) {
         }
     }
     return count == 0;
-}
-
-int main() {
-    assert(not correct_bracketing("<><><<><>><>>><>"));
-    return 0;
 }
