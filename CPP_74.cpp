2289eeb1f55996011932b68ac69d37c64e5db2ae
beep boop <githubbot@vadim.me>
2024-07-20 03:03:08 +0200

    Solution 59 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/xpyxaxvq Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 66d8aaa9-b1e5-46f4-85cf-5be60c009d90.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 66d8aaa9-b1e5-46f4-85cf-5be60c009d90.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 66d8aaa9-b1e5-46f4-85cf-5be60c009d90.cpp:20: 66d8aaa9-b1e5-46f4-85cf-5be60c009d90.cpp: In function ‘int main()’: 66d8aaa9-b1e5-46f4-85cf-5be60c009d90.cpp:30:31: error: invalid initialization of  reference of type ‘const std::vector<std::__cxx11::basic_string<char> >&’ from expression of type ‘bool’    30 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ~~~~~~~~~~~^~~~~~~~~~~~~~ 66d8aaa9-b1e5-46f4-85cf-5be60c009d90.cpp:7:45: note: in passing argument 1 of ‘b ool issame(const std::vector<std::__cxx11::basic_string<char> >&, const std::vec tor<std::__cxx11::basic_string<char> >&)’     7 | bool issame(const std::vector<std::string>& a, const std::vector<std::st ring>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -13,6 +13,6 @@ bool total_match(const std::vector<std::string>& a, const std::vector<std::strin
 }
 
 int main() {
-    assert(issame(std::vector<std::string>{"this"}, std::vector<std::string>{}));
+    assert(total_match({"this"}, {}));
     return 0;
 }
