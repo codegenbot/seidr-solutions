374abe912363470c9fbee0406986cb7fe043bcde
beep boop <githubbot@vadim.me>
2024-07-20 06:14:04 +0200

    Solution 30 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gw16mzei Prompt:       |                    ^~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:14: d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp: In function ‘int main()’: d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:24:20: error: ‘make_a_pile’ was not dec lared in this scope    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -5,8 +5,8 @@ bool issame(vector<int> a, vector<int> b);
 vector<int> make_a_pile(int n){
     vector<int> stones;
     stones.push_back(n);
-    for(int i=1; i<n; i++){
-        if(n%2 == 0){
+    for(int i = 1; i < n; ++i){
+        if(n % 2 == 0){
             n += 2;
         } else {
             n += 1;
@@ -16,29 +16,11 @@ vector<int> make_a_pile(int n){
     return stones;
 }
 
-int main(){
-    vector<int> pile1 = make_a_pile(5);
-    vector<int> pile2 = make_a_pile(5);
-    
-    if(issame(pile1, pile2)){
-        cout << "Piles are the same" << endl;
-    } else {
-        cout << "Piles are different" << endl;
-    }
-    
-    return 0;
+bool issame(vector<int> a, vector<int> b){
+    // Implementation of issame function
 }
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    
-    return true;
+int main(){
+    // Call make_a_pile function and use issame function
+    return 0;
 }
