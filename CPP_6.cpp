b13406b2b42aac422ac4244d0e399e2cf1bd0539
beep boop <githubbot@vadim.me>
2024-07-20 04:56:23 +0200

    Solution 89 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lvaqpkwx Prompt:       |                    ^~~ 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp:24:27: error: ‘vector’ was not declared  in this scope    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp:23: 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp: In function ‘int main()’: 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp:33:20: error: ‘parse_nested_parens’ was  not declared in this scope    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 2503faf5-2e4e-48a9-9465-bfd838494db1.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -6,28 +6,6 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-vector<int> parse_nested_parens(string paren_string) {
-    vector<int> result;
-    int max_depth = 0;
-    int curr_depth = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            curr_depth++;
-            max_depth = max(max_depth, curr_depth);
-        } else if (c == ')') {
-            curr_depth--;
-        } else if (c == ' ') {
-            result.push_back(max_depth);
-            max_depth = 0;
-            curr_depth = 0;
-        }
-    }
-    result.push_back(max_depth);
-
-    return result;
-}
-
 int main() {
     assert(issame(parse_nested_parens("(()(())((())))"), {4}));
     return 0;
