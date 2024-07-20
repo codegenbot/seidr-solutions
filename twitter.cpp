d1c9992976bcd272d5907d4600aaa016ed893a28
beep boop <githubbot@vadim.me>
2024-07-20 11:56:46 +0200

    Solution 63 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jvjx9h77 Prompt: 6871186e-59aa-41d7-9f9e-201b171060aa.cpp: In function ‘std::string validateTweet (std::string)’: 6871186e-59aa-41d7-9f9e-201b171060aa.cpp:13:74: error: expected ‘}’ at end of in put    13 |     return "Your tweet has " + to_string(tweet.length()) + " characters" ;       |  ^ 6871186e-59aa-41d7-9f9e-201b171060aa.cpp:6:36: note: to match this ‘{’     6 | string validateTweet(string tweet) {       |                                    ^

--- twitter.cpp
+++ twitter.cpp
@@ -1,11 +1,8 @@
-#include <string>
-#include <sstream>
-
-string validateTweet(string tweet) {
+```string validateTweet(string tweet) {
     if (tweet.empty()) {
         return "You didn't type anything";
     }
     if (tweet.length() > 140) {
         return "Too many characters";
     }
-    return "Your tweet has " + to_string(tweet.length()) + " characters";
+    return "Your tweet has " + to_string(tweet.length()) + "\" characters";
