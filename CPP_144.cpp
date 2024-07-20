37cfd3828cc9b902d911477f590c725712317fd3
beep boop <githubbot@vadim.me>
2024-07-20 15:13:59 +0200

    Solution 32 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ax1djs0y Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,27 +1,7 @@
-#include <string>
-#include <sstream>
-#include <iostream>
+int num1 = stoi(x.substr(0, x.find('/')));
+    int den1 = stoi(x.substr(x.find('/') + 1));
+    int num2 = stoi(n.substr(0, n.find('/')));
+    int den2 = stoi(n.substr(n.find('/') + 1));
 
-bool simplify(std::string x, std::string n) {
-    std::stringstream ss_x(x), ss_n(n);
-    int num_x, den_x, num_n, den_n;
-    char slash;
-
-    ss_x >> num_x >> slash >> den_x;
-    ss_n >> num_n >> slash >> den_n;
-
-    return (num_x * den_n == num_n * den_x);
-}
-
-int main() {
-    std::string x, n;
-    std::cin >> x >> n;
-
-    if (simplify(x, n)) {
-        std::cout << "true" << std::endl;
-    } else {
-        std::cout << "false" << std::endl;
-    }
-
-    return 0;
+    return (num1 * num2) % (den1 * den2) == 0;
 }
