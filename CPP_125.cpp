df57d263fe4ef7a01819a96e31d20436ce86f0ab
beep boop <githubbot@vadim.me>
2024-07-20 08:35:08 +0200

    Solution 19 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/zzxdrr91 Prompt:       |                              ^~~~~~ a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp:34:37: error: ‘string’ was not declared  in this scope    34 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp:34:44: error: ‘b’ was not declared in t his scope    34 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp:34:45: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp:33: a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp: In function ‘int main()’: a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp:43:20: error: ‘split_words’ was not dec lared in this scope    43 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ a8be2ec6-a198-46e6-ad09-7d02892a5e13.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -43,10 +43,9 @@ vector<string> split_words(string txt) {
 
 int main() {
     assert(issame(split_words(""), {"0"}));
-    // Additional test cases
-    assert(issame(split_words("Hello,world"), {"Hello", "world"}));
-    assert(issame(split_words("Programming is fun"), {"Programming", "is", "fun"}));
+    assert(issame(split_words("hello,world"), {"hello", "world"}));
+    assert(issame(split_words("this is a test"), {"this", "is", "a", "test"}));
     assert(issame(split_words("123,456,789"), {"123", "456", "789"}));
-    
+    assert(issame(split_words("AbcDefGhi"), {"AbcDefGhi"}));
     return 0;
 }
