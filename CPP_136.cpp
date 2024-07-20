605980b5e68acc7cdb31e2b77d7d10ca71a02365
beep boop <githubbot@vadim.me>
2024-07-20 04:09:29 +0200

    Solution 79 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/g7b2ew9a Prompt:       |                           ^~~~~~ 6b21ba45-6ea9-4a92-ad15-9ac34f68f03e.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 6b21ba45-6ea9-4a92-ad15-9ac34f68f03e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6b21ba45-6ea9-4a92-ad15-9ac34f68f03e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6b21ba45-6ea9-4a92-ad15-9ac34f68f03e.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b21ba45-6ea9-4a92-ad15-9ac34f68f03e.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 6b21ba45-6ea9-4a92-ad15-9ac34f68f03e.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -11,7 +11,6 @@ std::vector<int> largest_smallest_integers(std::vector<int> nums) {
     return {nums.front(), nums.back()};
 }
 
-int main() {
+void main() {
     assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), std::vector<int>{-3, 1}));
-    return 0;
 }
