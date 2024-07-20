a0572543b1ce1b8213ec11ef4360ee45ac1f07ca
beep boop <githubbot@vadim.me>
2024-07-20 04:49:23 +0200

    Solution 38 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lvaqpkwx Prompt:       |                    ^~~ bd4d239b-83cd-4614-9af0-57744da64675.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bd4d239b-83cd-4614-9af0-57744da64675.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bd4d239b-83cd-4614-9af0-57744da64675.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bd4d239b-83cd-4614-9af0-57744da64675.cpp:24: bd4d239b-83cd-4614-9af0-57744da64675.cpp: In function ‘int main()’: bd4d239b-83cd-4614-9af0-57744da64675.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ bd4d239b-83cd-4614-9af0-57744da64675.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,3 +1,26 @@
 #include <vector>
 
-bool issame(vector<int> v1, vector<int> v2);
+bool issame(int a, int b);
+
+vector<int> parse_nested_parens(string paren_string){
+    vector<int> depths;
+    int max_depth = 0;
+    int current_depth = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            current_depth++;
+            max_depth = max(max_depth, current_depth);
+        } else if (c == ')') {
+            current_depth--;
+        } else if (c == ' ') {
+            depths.push_back(max_depth);
+            max_depth = 0;
+            current_depth = 0;
+        }
+    }
+
+    depths.push_back(max_depth);
+
+    return depths;
+}
