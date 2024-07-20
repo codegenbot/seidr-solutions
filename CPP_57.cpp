33f65f8d11c3b97d0d22fd0c632773412bea206e
beep boop <githubbot@vadim.me>
2024-07-20 05:40:17 +0200

    Solution 50 of CPP/57, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/cpx165g3 Prompt:     4 |     for (int i = 1; i < l.size(); ++i) {       |     ^~~ cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp:4:21: error: ‘i’ does not name a type     4 |     for (int i = 1; i < l.size(); ++i) {       |                     ^ cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp:4:35: error: expected unqualified-id be fore ‘++’ token     4 |     for (int i = 1; i < l.size(); ++i) {       |                                   ^~ cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return increasing || decreasing;       |     ^~~~~~ cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp:16: cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp: In function ‘int main()’: cd86e224-870f-4076-ab5e-7af4a569ae0c.cpp:18:13: error: ‘monotonic’ was not decla red in this scope    18 |     assert (monotonic({9, 9, 9, 9}) == true);       |             ^~~~~~~~~

--- CPP_57.cpp
+++ CPP_57.cpp
@@ -1,19 +1,15 @@
-#include <vector>
-using namespace std;
-
-bool monotonic(std::vector<float> l){
-    bool increasing = true, decreasing = true;
-    for(int i = 1; i < l.size(); i++){
-        if(l[i] > l[i-1]){
-            decreasing = false;
-        } else if(l[i] < l[i-1]){
+bool monotonic(vector<int>& l) {
+    bool increasing = true;
+    bool decreasing = true;
+    
+    for (int i = 1; i < l.size(); ++i) {
+        if (l[i] < l[i-1]) {
             increasing = false;
         }
+        if (l[i] > l[i-1]) {
+            decreasing = false;
+        }
     }
+    
     return increasing || decreasing;
-}
-
-int main() {
-    assert(monotonic({9.0, 9.0, 9.0, 9.0}) == true);
-    return 0;
 }
