c58d86621f65282d42d246ea72f6c5d80c638e8e
beep boop <githubbot@vadim.me>
2024-07-20 02:22:03 +0200

    Solution 5 of CPP/52, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/lyupy1jc Prompt: 199cb33e-1e2e-4bd5-ae7d-4ec0c8cf08a7.cpp: In function ‘bool below_threshold(cons t std::vector<int>&, int)’: 199cb33e-1e2e-4bd5-ae7d-4ec0c8cf08a7.cpp:3:17: error: ‘all_of’ is not a member o f ‘std’     3 |     return std::all_of(l.begin(), l.end(), [t](int num) { return num < t ; });       |                 ^~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,14 +1,5 @@
 #include <vector>
-
+#include <algorithm>
 bool below_threshold(const std::vector<int>& l, int t) {
-    for (int num : l) {
-        if (num >= t) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    assert (not below_threshold({1, 8, 4, 10}, 10));
+    return std::all_of(l.begin(), l.end(), [t](int num) { return num < t; });
 }
