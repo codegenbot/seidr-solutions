887c1e451083798c33de17aaaad23a8e4666e731
beep boop <githubbot@vadim.me>
2024-07-20 03:39:03 +0200

    Solution 10 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q9cl59bs Prompt: 715681e5-28a7-47e0-9e4f-06b62793e1f0.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string fix_spaces(string text) {       | ^~~~~~ In file included from 715681e5-28a7-47e0-9e4f-06b62793e1f0.cpp:17: 715681e5-28a7-47e0-9e4f-06b62793e1f0.cpp: In function ‘int main()’: 715681e5-28a7-47e0-9e4f-06b62793e1f0.cpp:19:13: error: ‘fix_spaces’ was not decl ared in this scope    19 |     assert (fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");       |             ^~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,16 +1,17 @@
-string fix_spaces(string text){
+```
+#include <string>
+using namespace std;
+
+string fix_spaces(string text) {
     string result = "";
-    for(int i=0; i<text.length(); i++){
-        if(text[i] == ' ' && (i==0 || text[i-1] != ' ')){
-            result += '_';
-        }
-        else if(text[i] == ' ' && i!=text.length()-1){
-            int j = i;
-            while(j < text.length() && text[j] == ' ') j++;
-            if(j-i>2) result += '-';
-            else result += '_';
-            i = j-1;
-        }else{
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] == ' ') {
+            if (i > 0 && text[i - 1] == ' ' && text[i + 1] == ' ') {
+                result += "-";
+            } else {
+                result += "_";
+            }
+        } else {
             result += text[i];
         }
     }
