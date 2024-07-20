bf9f430f47d62f17aefcfd77503ea4a32c85343e
beep boop <githubbot@vadim.me>
2024-07-20 04:43:12 +0200

    Solution 2 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gml63y5i Prompt:       |                    ^~~ b8e43612-5edf-415a-910a-620d67f0f4fd.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b8e43612-5edf-415a-910a-620d67f0f4fd.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b8e43612-5edf-415a-910a-620d67f0f4fd.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b8e43612-5edf-415a-910a-620d67f0f4fd.cpp:10: b8e43612-5edf-415a-910a-620d67f0f4fd.cpp: In function ‘int main()’: b8e43612-5edf-415a-910a-620d67f0f4fd.cpp:20:20: error: ‘generate_integers’ was n ot declared in this scope    20 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ b8e43612-5edf-415a-910a-620d67f0f4fd.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> generate_integers(int a, int b) {
-    std::vector<int> result;
+vector<int> generate_integers(int a, int b) {
+    vector<int> result;
     for (int i = a; i <= b; ++i) {
         if (i % 2 == 0) {
             result.push_back(i);
