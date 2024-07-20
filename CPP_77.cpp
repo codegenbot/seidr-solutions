5f5e3f30e6e4957a5f1c86c8b3f5527d19237519
beep boop <githubbot@vadim.me>
2024-07-20 02:36:13 +0200

    Solution 3 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/spccvq6e Prompt: Complete the following code given the task description and function signature.

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,7 +1,9 @@
 bool iscuber(int a){
     int i = 1;
-    while (pow(i, 3) <= a) {
-        if (pow(i, 3) == a) return true;
+    while (pow(i,3) <= a) {
+        if (pow(i,3) == a) {
+            return true;
+        }
         i++;
     }
     return false;
