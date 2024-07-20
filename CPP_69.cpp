13d011f949411c3ae8e7957ed32ab1e8df476f48
beep boop <githubbot@vadim.me>
2024-07-20 02:33:21 +0200

    Solution 35 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1qqqklir Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from ac9b5151-c663-4a32-85f1-c6f7ee0d32ae.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,9 +1,9 @@
 #include <vector>
 
-int search(std::vector<int> lst) {
+int search(vector<int> lst) {
     int max = -1;
     for (int i : lst) {
-        if (i > 0 && std::count(lst.begin(), lst.end(), i) == i) {
+        if (i > 0 && i <= count(lst.begin(), lst.end(), i)) {
             max = i;
         }
     }
