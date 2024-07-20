be39f24d17ab1bf8d68723c5f4a1aab3bcaa767b
beep boop <githubbot@vadim.me>
2024-07-20 04:01:16 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/q4j8hs8g Prompt:       |     ^~~~ In file included from fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp:22: fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp: In function ‘int main()’: fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp:24:13: error: ‘boost’ has not been decl ared    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp:24:29: error: ‘string’ was not declared  in this scope    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp:24:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from fcf14838-7326-44e1-9ab9-43e4d24a0c41.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,8 +2,8 @@
 #include <string>
 #include <cassert>
 
-template<typename T>
-const T compare_one(const std::any& a, const std::any& b) {
+template <typename T>
+const T compare_one(const std::any &a, const std::any &b) {
     if (a.type() == typeid(T) && b.type() == typeid(T)) {
         if (std::any_cast<T>(a) > std::any_cast<T>(b)) {
             return std::any_cast<T>(a);
@@ -15,6 +15,6 @@ const T compare_one(const std::any& a, const std::any& b) {
 }
 
 int main() {
-    assert(std::any_cast<std::string>(compare_one<std::string>(std::string("1"), std::string("1")) == std::string("1")));
+    assert(compare_one<std::string>(std::string("1"), std::string("1")) == std::string("1"));
     return 0;
 }
