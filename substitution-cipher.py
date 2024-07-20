c5fd3a3e54a5e06cad399f4b99f65ef5fabfd213
beep boop <githubbot@vadim.me>
2024-07-20 10:16:38 +0200

    Solution 69 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/7w42l4s7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a00c60d-328b-422d-81f2-c92cd4ab3 17f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,5 +1,6 @@
+```
 def substitution_cipher(cipher1, cipher2, message):
     if len(cipher1) != len(cipher2):
         print("Warning: cipher strings are not the same length")
     cipher_map = {k:v for k,v in zip(cipher1.lower(), cipher2.lower())}
-    return "".join([cipher_map.get(char.lower(), char) for char in message]).lower()
+    return "".join([cipher_map.get(char.lower(), '') or char for char in message]).lower()
