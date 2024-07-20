0e140f4af4f317bf066051cc45ac0e8f0bbc8f84
beep boop <githubbot@vadim.me>
2024-07-20 04:47:11 +0200

    Solution 10 of CPP/8, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/153dugop Prompt: In file included from ed4179ed-fa50-463b-b4a8-d4787926fb2a.cpp:17: ed4179ed-fa50-463b-b4a8-d4787926fb2a.cpp: In function ‘int main()’: ed4179ed-fa50-463b-b4a8-d4787926fb2a.cpp:27:31: error: could not convert ‘sum_pr oduct(std::vector<int>(std::initializer_list<int>{((const int*)(& const int [1]{ 10})), 1}, std::allocator<int>()))’ from ‘std::pair<int, int>’ to ‘std::vector<i nt>’    27 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ~~~~~~~~~~~^~~~~~       |                               |       |                               std::pair<int, int>

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -2,7 +2,7 @@
 #include<utility>
 using namespace std;
 
-pair<int, int> sum_product(vector<int> numbers){
+vector<int> sum_product(vector<int> numbers){
     int sum = 0;
     int product = 1;
     
@@ -10,6 +10,6 @@ pair<int, int> sum_product(vector<int> numbers){
         sum += num;
         product *= num;
     }
-    
-    return make_pair(sum, product);
+
+    return {sum, product};
 }
