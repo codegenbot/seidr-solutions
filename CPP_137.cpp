a2a814e2dca9f781d43181119f7f1fc84271257c
beep boop <githubbot@vadim.me>
2024-07-20 06:21:58 +0200

    Solution 14 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,44 +1,62 @@
 #include <boost/any.hpp>
+#include <string>
+#include <algorithm>
+
+using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)a > (float)b ? a : b;
+        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)a > (double)b ? a : b;
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int x = stoi(any_cast<string>(b));
-        return (int)a > x ? a : b;
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string strA = a.convert_to<string>();
+        string strB = b.convert_to<string>();
+
+        int pos1 = strA.find(',');
+        int pos2 = strB.find(',');
+
+        float numA = stof(strA.substr(0, pos1));
+        float numB = stof(strB.substr(0, pos2));
+
+        if (numA > numB) {
+            return a;
+        }
+        else if (numB > numA) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return (float)a > (double)b ? a : b;
+    else if (a.type() == typeid(string) && b.type() != typeid(string)) {
+        string str = a.convert_to<string>();
+
+        int pos = str.find(',');
+        float num = stof(str.substr(0, pos));
+
+        if ((int)b.convert_to<int>() > num || (float)b.convert_to<float>() > num) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        int x = any_cast<int>(a);
-        return x > (int)b ? a : b;
+    else if (b.type() == typeid(string) && a.type() != typeid(string)) {
+        string str = b.convert_to<string>();
+
+        int pos = str.find(',');
+        float num = stof(str.substr(0, pos));
+
+        if ((int)a.convert_to<int>() > num || (float)a.convert_to<float>() > num) {
+            return a;
+        }
+        else {
+            return boost::any("None");
+        }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        float y = stof(any_cast<string>(b));
-        return stof(any_cast<string>(a)) > y ? a : b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        double z = stod(any_cast<string>(b));
-        return stod(any_cast<string>(a)) > z ? a : b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int x = stoi(any_cast<string>(b));
-        return (int)a > x ? a : b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        float y = stof(any_cast<string>(b));
-        return (float)a > y ? a : b;
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double z = stod(any_cast<string>(b));
-        return (double)a > z ? a : b;
-    }
-    else {
-        return boost::any("None");
-    }
+
+    return boost::any("None");
 }
