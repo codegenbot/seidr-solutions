28fc2397eee53fc5b9c052c4a0c15d1d43419cf6
beep boop <githubbot@vadim.me>
2024-07-20 05:31:53 +0200

    Solution 94 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/69xo63xe Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ be08497c-a52b-4326-9723-b10335ea87ce.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ be08497c-a52b-4326-9723-b10335ea87ce.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from be08497c-a52b-4326-9723-b10335ea87ce.cpp:5: be08497c-a52b-4326-9723-b10335ea87ce.cpp: In function ‘int main()’: be08497c-a52b-4326-9723-b10335ea87ce.cpp:15:20: error: ‘unique’ was not declared  in this scope    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ be08497c-a52b-4326-9723-b10335ea87ce.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -12,7 +12,7 @@ int main() {
     l.erase(std::unique(l.begin(), l.end()), l.end());
     
     std::cout << std::boolalpha;
-    std::cout << issame(l, std::vector<int>{0, 2, 3, 5, 9, 123}) << std::endl; // Output result
+    std::cout << issame(l, std::vector<int>{0, 2, 3, 5, 9, 123}) << std::endl;
     
     return 0;
 }
