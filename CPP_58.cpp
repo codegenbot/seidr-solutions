f487fd59a9097402404c30079eec77c94d10c34a
beep boop <githubbot@vadim.me>
2024-07-20 04:50:19 +0200

    Solution 24 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7w3lxqqp Prompt: Complete the following code given the task description and function signature.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,14 +1,15 @@
-#include <set>
-using namespace std;
-
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
+    set<int> result;
 
-    set<int> intersection;
+    // Find common elements in two sets
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
+        inserter(result, result.begin()));
 
-    vector<int> result(intersection.begin(), intersection.end());
-    return result;
+    vector<int> res;
+    for (int i : result) {
+        res.push_back(i);
+    }
+    return res;
 }
