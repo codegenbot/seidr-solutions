90572dee7ec2a15400a8b387b43c6df90dd84c6b
beep boop <githubbot@vadim.me>
2024-07-20 02:31:35 +0200

    Solution 93 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9z0vt8xn Prompt:       |                    ^~~ 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp:16: 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp: In function ‘int main()’: 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8d6c4824-3be6-4f78-86e1-6dc046e09ecc.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,6 +5,11 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
+int main() {
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    return 0;
+}
+
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
@@ -16,9 +21,4 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     vector<int> result(intersection.begin(), intersection.end());
     sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-    return 0;
 }
