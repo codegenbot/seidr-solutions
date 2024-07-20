c5434c3887aaff601ed8a142c1019b54fb94a11c
beep boop <githubbot@vadim.me>
2024-07-20 03:39:08 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y1nh82vq Prompt: 5a0ceebf-38b9-48d5-852c-b706ebfa0e42.cpp:4:9: fatal error: boost/any.hpp: No suc h file or directory     4 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,95 +1,35 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+
 using namespace std;
 
-int compare_one(int a, float b) {
-    if (a > b) {
+string compare_one(string a, int b) {
+    if (a > to_string(b)) {
         return a;
     }
-    else if (a < b) {
-        return b;
-    }
-    else {
-        return 0;
-    }
-}
-
-int compare_one(int a, string b) {
-    if (to_string(a) > b) {
-        return 1;
-    }
-    else if (to_string(a) < b) {
-        return -1;
-    }
-    else {
-        return 0;
-    }
-}
-
-int compare_one(float a, int b) {
-    if (a > b) {
-        return 1;
-    }
-    else if (a < b) {
-        return -1;
-    }
-    else {
-        return 0;
-    }
-}
-
-int compare_one(string a, int b) {
-    if (a > to_string(b)) {
-        return 1;
-    }
     else if (a < to_string(b)) {
-        return -1;
+        return to_string(b);
     }
     else {
-        return 0;
-    }
-}
-
-int compare_one(float a, string b) {
-    if (to_string(a) > b) {
-        return 1;
-    }
-    else if (to_string(a) < b) {
-        return -1;
-    }
-    else {
-        return 0;
-    }
-}
-
-int compare_one(string a, float b) {
-    if (a > to_string(b)) {
-        return 1;
-    }
-    else if (a < to_string(b)) {
-        return -1;
-    }
-    else {
-        return 0;
+        return "None";
     }
 }
 
 int main() {
-    int a, b;
-    cout << "Enter the first number: ";
-    cin >> a;
-    cout << "Enter the second number: ";
-    cin >> b;
-    
-    if (a > b) {
-        cout << "The larger number is: " << a << endl;
-    }
-    else if (a < b) {
-        cout << "The larger number is: " << b << endl;
+    string input1, input2;
+    cout << "Enter the first value: ";
+    cin >> input1;
+    cout << "Enter the second value: ";
+    cin >> input2;
+
+    if (input2.length() > 0 && isdigit(input2[0])) {
+        int b = stoi(input2);
+        string output = compare_one(input1, b);
+        cout << output << endl;
     }
     else {
-        cout << "Both numbers are equal." << endl;
+        cout << "Invalid second value. Please enter an integer." << endl;
     }
 
     return 0;
