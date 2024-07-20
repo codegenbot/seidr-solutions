b35949eef06cc854a106e84f7ecfd8417d157936
beep boop <githubbot@vadim.me>
2024-07-20 03:15:25 +0200

    Solution 1 of CPP/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ty0hv1zn Prompt: 09b7f9c4-397d-4b00-adf1-2cc4ec199c0d.cpp:1:17: error: ‘string’ was not declared in this scope     1 | int count_upper(string s){       |                 ^~~~~~ In file included from 09b7f9c4-397d-4b00-adf1-2cc4ec199c0d.cpp:11: 09b7f9c4-397d-4b00-adf1-2cc4ec199c0d.cpp: In function ‘int main()’: 09b7f9c4-397d-4b00-adf1-2cc4ec199c0d.cpp:13:24: error: ‘count_upper’ cannot be u sed as a function    13 |     assert (count_upper("EEEE") == 2);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -1,4 +1,6 @@
-int count_upper(string s){
+#include <string>
+
+int count_upper(std::string s){
     int count = 0;
     for(int i = 0; i < s.length(); i += 2){
         if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
