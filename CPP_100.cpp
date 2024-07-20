f8b1f4cf52674a59c7171e912720e8c93489bcd4
beep boop <githubbot@vadim.me>
2024-07-20 06:15:06 +0200

    Solution 37 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gw16mzei Prompt:       |                    ^~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:14: d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp: In function ‘int main()’: d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:24:20: error: ‘make_a_pile’ was not dec lared in this scope    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ d32f6d78-1d3b-4996-8209-a4caaf9f3049.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -17,25 +17,17 @@ vector<int> make_a_pile(int n) {
 }
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
     vector<int> pile1 = make_a_pile(5);
     vector<int> pile2 = make_a_pile(5);
 
-    if(issame(pile1, pile2)){
-        cout << "Piles are the same." << endl;
+    if (issame(pile1, pile2)) {
+        cout << "The piles are the same." << endl;
     } else {
-        cout << "Piles are different." << endl;
+        cout << "The piles are different." << endl;
     }
 
     return 0;
