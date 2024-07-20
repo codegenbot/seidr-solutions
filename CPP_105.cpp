cb3a282cd7f70cba9db29d67e079b37e1544e539
beep boop <githubbot@vadim.me>
2024-07-20 05:54:51 +0200

    Solution 77 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v1zxwqb2 Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,13 +1,51 @@
+#include <string>
+
 vector<string> by_length(vector<int> arr) {
-    vector<int> nums;
-    for (int x : arr) {
-        if (x >= 1 && x <= 9)
-            nums.push_back(x);
+    vector<int> temp;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            temp.push_back(i);
+        }
     }
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
+    
+    sort(temp.begin(), temp.end());
+    
+    reverse(temp.begin(), temp.end());
+    
     vector<string> result;
-    for (int x : nums)
-        result.push_back({"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"}[x-1]);
+    for (int i : temp) {
+        string s = "";
+        switch (i) {
+            case 1:
+                s = "One";
+                break;
+            case 2:
+                s = "Two";
+                break;
+            case 3:
+                s = "Three";
+                break;
+            case 4:
+                s = "Four";
+                break;
+            case 5:
+                s = "Five";
+                break;
+            case 6:
+                s = "Six";
+                break;
+            case 7:
+                s = "Seven";
+                break;
+            case 8:
+                s = "Eight";
+                break;
+            case 9:
+                s = "Nine";
+                break;
+        }
+        result.push_back(s);
+    }
+    
     return result;
 }
