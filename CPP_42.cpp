a8a4586b054700cdafa479ec3f77346163e03ed7
beep boop <githubbot@vadim.me>
2024-07-20 02:09:14 +0200

    Solution 6 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/oheqdp1c Prompt: c9dac58a-79bc-4421-9f4a-6ca3e6d72420.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ c9dac58a-79bc-4421-9f4a-6ca3e6d72420.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ c9dac58a-79bc-4421-9f4a-6ca3e6d72420.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ c9dac58a-79bc-4421-9f4a-6ca3e6d72420.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
+bool issame(vector<int> a, vector<int> b);
 
 vector<int> incr_list(vector<int> l) {
     for (int &num : l) {
         num++;
     }
     return l;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 int main() {
