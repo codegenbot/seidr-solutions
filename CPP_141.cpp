fb63abac33ea1cbc4ddaa435448ccc09f586959e
beep boop <githubbot@vadim.me>
2024-07-20 06:25:42 +0200

    Solution 0 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/sp6lruof Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,45 +1,26 @@
-#include <string>
-using namespace std;
-
-string file_name_check(string file_name) {
-    int dot_count = 0;
-    bool has_dot = false;
+string file_name_check(string file_name){
     int digit_count = 0;
-    char prev_char = '\0';
-    
-    for (char c : file_name) {
-        if (c == '.') {
-            dot_count++;
-            has_dot = true;
-        } else if (isdigit(c)) {
+    bool found_dot = false;
+    for(int i=0; i<file_name.length(); i++){
+        if(isdigit(file_name[i])){
             digit_count++;
-        } else if (!has_dot && !isalpha(c)) {
-            return "No";
+            if(digit_count > 3) return "No";
         }
-        
-        prev_char = c;
+        else if(file_name[i] == '.'){
+            found_dot = true;
+        }
+        else if(found_dot && (file_name[i] == 't' || file_name[i] == 'x' || file_name[i] == 'd' || file_name[i] == 'l')){
+            string extension = "";
+            for(int j=i+1; j<file_name.length(); j++){
+                if(file_name[j] == '.')
+                    break;
+                else
+                    extension += file_name[j];
+            }
+            if(extension != "txt" && extension != "exe" && extension != "dll")
+                return "No";
+        }
     }
-    
-    if (dot_count > 1) {
-        return "No";
-    }
-    
-    string file_extension = "";
-    for (int i = file_name.find('.'); i < file_name.length(); i++) {
-        file_extension += file_name[i];
-    }
-    
-    if (file_extension != "txt" && file_extension != "exe" && file_extension != "dll") {
-        return "No";
-    }
-    
-    if (digit_count > 3) {
-        return "No";
-    }
-    
-    if (!has_dot) {
-        return "No";
-    }
-    
+    if(!found_dot || digit_count > 3) return "No";
     return "Yes";
 }
