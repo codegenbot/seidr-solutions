5ba55a459e44e46f71470058e1da59729b029046
beep boop <githubbot@vadim.me>
2024-07-20 07:42:58 +0200

    Solution 42 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/i2m4te3d Prompt:  this scope    15 |     cout << "Enter a string: ";       |     ^~~~ 9e970d6c-eb9f-404c-b771-53b270010587.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 9e970d6c-eb9f-404c-b771-53b270010587.cpp:16:5: error: ‘cin’ was not declared in this scope    16 |     cin >> str;       |     ^~~ 9e970d6c-eb9f-404c-b771-53b270010587.cpp:16:5: note: ‘std::cin’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’? 9e970d6c-eb9f-404c-b771-53b270010587.cpp:17:37: error: ‘endl’ was not declared i n this scope    17 |     cout << middleCharacter(str) << endl;       |                                     ^~~~ 9e970d6c-eb9f-404c-b771-53b270010587.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,17 +1,17 @@
 #include <string>
 #include <iostream>
 
-std::string middleCharacter(std::string str) {
+string middleCharacter(string str) {
     int len = str.length();
     if(len % 2 == 1) {
-        return std::string(1, str[len/2]);
+        return string(1, str[len/2]);
     } else {
-        return std::string(1, str[(len-1)/2]) + std::string(1, str[len/2]);
+        return string(1, str[(len-1)/2]) + string(1, str[len/2]);
     }
 }
 
 int main() {
-    std::string str;
+    string str;
     std::cout << "Enter a string: ";
     std::cin >> str;
     std::cout << middleCharacter(str) << std::endl;
