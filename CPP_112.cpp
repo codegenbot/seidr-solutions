6b621f166b67aa2a17bced4bc7fcedcd193bc558
beep boop <githubbot@vadim.me>
2024-07-20 03:26:39 +0200

    Solution 59 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/206enlog Prompt:       |                              ^~~~~~ c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp:29: c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp: In function ‘int main()’: c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp:39:20: error: ‘reverse_delete’ was not declared in this scope    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ c0e24146-b210-4a67-8e53-1af6d8d538bd.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,12 +1,12 @@
-#include <string>
 #include <vector>
+#include <string>
 
-bool issame(vector<string> s1, vector<string> s2) {
-    if (s1.size() != s2.size()) {
+bool issame(vector<string> v1, vector<string> v2) {
+    if (v1.size() != v2.size()) {
         return false;
     }
-    for (int i = 0; i < s1.size(); i++) {
-        if (s1[i] != s2[i]) {
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) {
             return false;
         }
     }
@@ -31,6 +31,10 @@ vector<string> reverse_delete(string s, string c) {
     result.push_back(temp);
     string rev = temp;
     reverse(rev.begin(), rev.end());
-    vector<string> output = {temp, (rev == temp ? "True" : "False")};
-    return output;
+    if (temp == rev) {
+        result.push_back("True");
+    } else {
+        result.push_back("False");
+    }
+    return result;
 }
