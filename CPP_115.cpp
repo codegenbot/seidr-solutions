b642cd05b1358a0b7e2fc8f56802f6f516a8ee2c
beep boop <githubbot@vadim.me>
2024-07-20 06:21:56 +0200

    Solution 12 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4014xqud Prompt:     6 |         for(int i = 0; i < rows; i++){       |                        ^ 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp:6:34: error: ‘i’ does not name a type     6 |         for(int i = 0; i < rows; i++){       |                                  ^ 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp:12:9: error: ‘total_buckets’ does not n ame a type    12 |         total_buckets = (total_water + capacity - 1) / capacity;       |         ^~~~~~~~~~~~~ 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp:14:9: error: expected unqualified-id be fore ‘return’    14 |         return total_buckets;       |         ^~~~~~ 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp:15:5: error: expected declaration befor e ‘}’ token    15 |     }       |     ^ In file included from 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp:17: 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp: In function ‘int main()’: 89fd02a1-9a60-41a4-8103-b118bba8f7c5.cpp:19:13: error: ‘max_fill’ was not declar ed in this scope    19 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ^~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -3,14 +3,14 @@ int max_fill(vector<vector<int>> grid, int capacity){
     int cols = grid[0].size();
     int total_water = 0;
     int total_buckets = 0;
-
+    
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
             total_water += grid[i][j];
         }
     }
-
+    
     total_buckets = (total_water + capacity - 1) / capacity;
-
+    
     return total_buckets;
 }
