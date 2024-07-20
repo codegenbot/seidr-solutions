7cd03339af422f2cea187c275c47a4e63253af60
beep boop <githubbot@vadim.me>
2024-07-20 05:55:01 +0200

    Solution 78 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/v1zxwqb2 Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,51 +1,22 @@
 #include <string>
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> temp;
+    vector<string> result;
+
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
-            temp.push_back(i);
+            int num = i;
+            string str = "";
+            map<int, string> numberMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+                                           {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
+            while (num > 0) {
+                str = numberMap[num % 10] + " " + str;
+                num /= 10;
+            }
+            result.push_back(str.substr(0, str.length() - 1));
         }
     }
-    
-    sort(temp.begin(), temp.end());
-    
-    reverse(temp.begin(), temp.end());
-    
-    vector<string> result;
-    for (int i : temp) {
-        string s = "";
-        switch (i) {
-            case 1:
-                s = "One";
-                break;
-            case 2:
-                s = "Two";
-                break;
-            case 3:
-                s = "Three";
-                break;
-            case 4:
-                s = "Four";
-                break;
-            case 5:
-                s = "Five";
-                break;
-            case 6:
-                s = "Six";
-                break;
-            case 7:
-                s = "Seven";
-                break;
-            case 8:
-                s = "Eight";
-                break;
-            case 9:
-                s = "Nine";
-                break;
-        }
-        result.push_back(s);
-    }
-    
+
+    reverse(result.begin(), result.end());
     return result;
 }
