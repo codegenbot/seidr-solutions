24339f1e1e22d381e5046c1f8a62ceae8e9cffde
beep boop <githubbot@vadim.me>
2024-07-20 04:39:43 +0200

    Solution 64 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/c1dovke2 Prompt:       |                           ^~~~~~ bbb64000-06de-4f10-b7c4-2ff4ba76d6fb.cpp:34:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bbb64000-06de-4f10-b7c4-2ff4ba76d6fb.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bbb64000-06de-4f10-b7c4-2ff4ba76d6fb.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bbb64000-06de-4f10-b7c4-2ff4ba76d6fb.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bbb64000-06de-4f10-b7c4-2ff4ba76d6fb.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ bbb64000-06de-4f10-b7c4-2ff4ba76d6fb.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -14,7 +14,6 @@ std::vector<int> even_odd_palindrome(int n) {
         std::string num = std::to_string(i);
         std::string rev_num = num;
         std::reverse(rev_num.begin(), rev_num.end());
-        
         if (num == rev_num) {
             if (i % 2 == 0) {
                 result[0]++;
