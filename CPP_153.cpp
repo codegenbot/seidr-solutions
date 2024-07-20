01f4538409b3faca493a7e64db60b7a72e3d8971
beep boop <githubbot@vadim.me>
2024-07-20 04:00:06 +0200

    Solution 40 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/7o2cgpbf Prompt:       |                         ^~~~~~ 3330f8b5-cd41-493c-b48b-cded0f271034.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 3330f8b5-cd41-493c-b48b-cded0f271034.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 3330f8b5-cd41-493c-b48b-cded0f271034.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 3330f8b5-cd41-493c-b48b-cded0f271034.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 3330f8b5-cd41-493c-b48b-cded0f271034.cpp:22: 3330f8b5-cd41-493c-b48b-cded0f271034.cpp: In function ‘int main()’: 3330f8b5-cd41-493c-b48b-cded0f271034.cpp:24:32: error: ‘Strongest_Extension’ can not be used as a function    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,17 +1,16 @@
 ```cpp
-#include <string>
 #include <vector>
-#include <cctype>
+#include <string>
 
-int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
-    std::string strongest_extension = "";
+int Strongest_Extension(string class_name, vector<string> extensions) {
+    string strongest_extension = "";
     int max_strength = 0;
     
     for(auto extension : extensions){
         int cap = 0, sm = 0;
         for(char c : extension){
-            if(std::isupper(c)) cap++;
-            else if(std::islower(c)) sm++;
+            if(isupper(c)) cap++;
+            else if(islower(c)) sm++;
         }
         int strength = cap - sm;
         
