f9d70a43f29bce642b893288ee48f6d138617e8c
beep boop <githubbot@vadim.me>
2024-07-20 04:03:30 +0200

    Solution 89 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/73i0cqow Prompt: 6948f21e-54e7-44af-b988-1512493040cd.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums){       |                   ^~~~~~ 6948f21e-54e7-44af-b988-1512493040cd.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums){       |                          ^~~ In file included from 6948f21e-54e7-44af-b988-1512493040cd.cpp:10: 6948f21e-54e7-44af-b988-1512493040cd.cpp: In function ‘int main()’: 6948f21e-54e7-44af-b988-1512493040cd.cpp:12:26: error: ‘specialFilter’ cannot be  used as a function    12 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -11,8 +11,6 @@ int specialFilter(vector<int> nums){
 }
 
 int main() {
-    int result = specialFilter(vector<int>({1,11,-12,21,-23,32,41,-42,51}));
-    cout << "The number of filtered integers is: " << result << endl;
     assert(specialFilter(vector<int>()) == 0);
     return 0;
 }
