280b7ac189a5f0f828c8c21178e17321ae54b23e
beep boop <githubbot@vadim.me>
2024-07-20 04:14:08 +0200

    Solution 4 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3ykkjyif Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5aba6933-80ec-451d-895a-22e370d0ac40.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 5aba6933-80ec-451d-895a-22e370d0ac40.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main(){       |     ^~~~ In file included from 5aba6933-80ec-451d-895a-22e370d0ac40.cpp:26: 5aba6933-80ec-451d-895a-22e370d0ac40.cpp: In function ‘int main()’: 5aba6933-80ec-451d-895a-22e370d0ac40.cpp:36:20: error: ‘sorted_list_sum’ was not  declared in this scope    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 5aba6933-80ec-451d-895a-22e370d0ac40.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -5,15 +5,8 @@
 
 using namespace std;
 
-bool issame(const vector<string>& a, const vector<string>& b){
+bool issame(vector<string> a, vector<string> b){
     return a == b;
-}
-
-vector<string> sorted_list_sum(vector<string> lst);
-
-int main(){
-    assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
-    return 0;
 }
 
 vector<string> sorted_list_sum(vector<string> lst){
@@ -25,4 +18,9 @@ vector<string> sorted_list_sum(vector<string> lst){
         return a.length() < b.length();
     });
     return lst;
+}
+
+int main(){
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
 }
