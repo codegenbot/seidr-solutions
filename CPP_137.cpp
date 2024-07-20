ad49992bd7b0b241cafe39cc0cd6b2b3e6aabd4b
beep boop <githubbot@vadim.me>
2024-07-20 03:49:55 +0200

    Solution 70 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/y1nh82vq Prompt: 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:3:1: error: stray ‘`’ in program     3 | ```cpp       | ^ 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:3:2: error: stray ‘`’ in program     3 | ```cpp       |  ^ 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ 269f1b0b-88f7-4811-9776-05a74e0e6f7e.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,39 +1,35 @@
 #include <boost/any.hpp>
-#include <string>
-
-using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<std::string>(a)) {
-        std::string str_a = any_cast<std::string>(a);
-        if (is_any_of<std::string>(b)) {
-            std::string str_b = any_cast<std::string>(b);
-            if (str_a == "None") return b;
-            if (str_b == "None") return a;
-            if (std::stod(str_a) > std::stod(str_b)) return a;
-            else if (std::stod(str_a) < std::stod(str_b)) return b;
-            else return boost::any("None");
-        } else {
-            double num_b = any_cast<double>(b);
-            if (str_a == "None") return b;
-            if (num_b > std::stod(str_a)) return a;
-            else if (num_b < std::stod(str_a)) return b;
-            else return boost::any("None");
+    if (a.type() == b.type()) {
+        if (a.type() == typeid(int)) {
+            int x = boost::any_cast<int>(a);
+            int y = boost::any_cast<int>(b);
+            return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+        } else if (a.type() == typeid(double)) {
+            double x = boost::any_cast<double>(a);
+            double y = boost::any_cast<double>(b);
+            return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+        } else if (a.type() == typeid(std::string)) {
+            std::string x = boost::any_cast<std::string>(a);
+            std::string y = boost::any_cast<std::string>(b);
+            return (x > y) ? a : ((x < y) ? b : boost::any("None"));
         }
     } else {
-        if (is_any_of<std::string>(b)) {
-            std::string str_b = any_cast<std::string>(b);
-            double num_a = any_cast<double>(a);
-            if (str_b == "None") return a;
-            if (num_a > std::stod(str_b)) return a;
-            else if (num_a < std::stod(str_b)) return b;
-            else return boost::any("None");
-        } else {
-            double num_a = any_cast<double>(a);
-            double num_b = any_cast<double>(b);
-            if (num_a > num_b) return a;
-            else if (num_a < num_b) return b;
-            else return boost::any("None");
+        if (a.type() == typeid(int)) {
+            int x = boost::any_cast<int>(a);
+            double y = boost::any_cast<double>(b);
+            return (y < x) ? a : b;
+        } else if (a.type() == typeid(double)) {
+            double x = boost::any_cast<double>(a);
+            std::string y = boost::any_cast<std::string>(b);
+            if (std::stod(y) > x) return b;
+            else return a;
+        } else if (a.type() == typeid(std::string)) {
+            std::string x = boost::any_cast<std::string>(a);
+            double y = boost::any_cast<double>(b);
+            if (std::stod(x) < y) return a;
+            else return b;
         }
     }
 }
