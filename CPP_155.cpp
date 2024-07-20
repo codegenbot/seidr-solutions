ea361e85baf7268a552efe141294432cfdbb0302
beep boop <githubbot@vadim.me>
2024-07-20 09:02:57 +0200

    Solution 10 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/u5ha7eg7 Prompt:       |                    ^~~ 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp:13: 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp: In function ‘int main()’: 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope; did you mean ‘even_count’?    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 0238c417-167f-478a-a3a5-43b0a8f35f32.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,11 +1,29 @@
-vector<int> counts(2, 0);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+vector<int> even_odd_count(int num) {
+    int even_count = 0, odd_count = 0;
     string num_str = to_string(abs(num));
-    for (char c : num_str) {
-        if ((c - '0') % 2 == 0) {
-            counts[0]++;
+    for (char digit : num_str) {
+        if ((digit - '0') % 2 == 0) {
+            even_count++;
         } else {
-            counts[1]++;
+            odd_count++;
         }
     }
-    return counts;
+    return {even_count, odd_count};
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(even_odd_count(0), {1, 0}));
+    
+    return 0;
 }
