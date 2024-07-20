9efd0114808bdbeb5521526d9fb85d669a45647d
beep boop <githubbot@vadim.me>
2024-07-20 03:59:16 +0200

    Solution 4 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/38kvdprn Prompt:    31 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 54f81313-c45d-4ab5-8860-799334aee54b.cpp:31:27: error: ‘vector’ was not declared  in this scope    31 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 54f81313-c45d-4ab5-8860-799334aee54b.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 54f81313-c45d-4ab5-8860-799334aee54b.cpp:30: 54f81313-c45d-4ab5-8860-799334aee54b.cpp: In function ‘int main()’: 54f81313-c45d-4ab5-8860-799334aee54b.cpp:40:20: error: ‘count_up_to’ was not dec lared in this scope    40 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 54f81313-c45d-4ab5-8860-799334aee54b.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,25 +1,9 @@
 #include <vector>
-#include <cassert>
-#include <cmath>
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(std::vector<int> a, std::vector<int> b);
 
-vector<int> count_up_to(int n);
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<int> count_up_to(int n) {
-    vector<int> primes;
+std::vector<int> count_up_to(int n) {
+    std::vector<int> primes;
     for (int i = 2; i < n; ++i) {
         bool is_prime = true;
         for (int j = 2; j * j <= i; ++j) {
