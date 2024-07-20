2637f2beda627bbfc8c2552e4db8203afe6f8953
beep boop <githubbot@vadim.me>
2024-07-20 07:24:56 +0200

    Solution 32 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/uun7xixa Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: d66d1d8f-5d33-49c7-ae39-eadd591f0639.cpp:36:37:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <string>
 
@@ -9,33 +10,31 @@ std::string spinWords(std::string sentence) {
         if(sentence[i] != ' ') {
             word += sentence[i];
         } else {
-            if(word.length() >= 4) {
+            if(word.length() >= 5) {
                 std::string reversedWord(word.rbegin(), word.rend());
-                result += reversedWord + " ";
+                result += (reversedWord + " ");
+                word = "";
             } else {
-                result += word + " ";
+                result += (word + " ");
+                word = "";
             }
-            word = "";
         }
     }
 
-    if(word.length() >= 4) {
-        std::string reversedWord(word.rbegin(), std::move(word).rend());
+    if(word.length() >= 5) {
+        std::string reversedWord(word.rbegin(), word.rend());
         result += reversedWord;
     } else {
         result += word;
     }
 
-    return result;
+    return result.substr(0, result.find_last_of(' '));
 }
 
 int main() {
     std::string input;
     while (std::cout << "Enter a string: ", std::getline(std::cin, input)) {
-        if(input.length() >= 4) {
-            input = spinWords(input);
-        }
-        std::cout << input << std::endl;
+        std::cout << spinWords(input) << std::endl;
     }
     return 0;
 }
