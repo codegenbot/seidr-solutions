4ed7f741e78430937828b2d585f53219ad97b513
beep boop <githubbot@vadim.me>
2024-07-20 03:01:36 +0200

    Solution 60 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hcqrfvib Prompt:    18 | assert(flip_case("Hello, World!") == "hELLO, wORLD!");       | ^~~~~~ cc37286a-c827-43a7-ab37-01a7bcf7b15e.cpp:18:1: error: expected ‘)’ before ‘stati c_cast’    18 | assert(flip_case("Hello, World!") == "hELLO, wORLD!");       | ^~~~~~ cc37286a-c827-43a7-ab37-01a7bcf7b15e.cpp:19:1: error: expected unqualified-id be fore ‘static_cast’    19 | assert(flip_case("These violent delights have violent ends") == "tHESE V IOLENT DELIGHTS HAVE VIOLENT ENDS");       | ^~~~~~ cc37286a-c827-43a7-ab37-01a7bcf7b15e.cpp:19:1: error: expected ‘)’ before ‘stati c_cast’    19 | assert(flip_case("These violent delights have violent ends") == "tHESE V IOLENT DELIGHTS HAVE VIOLENT ENDS");       | ^~~~~~ In file included from cc37286a-c827-43a7-ab37-01a7bcf7b15e.cpp:21: cc37286a-c827-43a7-ab37-01a7bcf7b15e.cpp: In function ‘int main()’: cc37286a-c827-43a7-ab37-01a7bcf7b15e.cpp:23:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    23 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -5,7 +5,7 @@
 std::string flip_case(std::string str);
 
 std::string flip_case(std::string str) {
-    for (char &c : str) {
+    for (char& c : str) {
         if (islower(c)) {
             c = toupper(c);
         } else if (isupper(c)) {
