2945872d6fce6e45f4167d9718eec4473961a429
beep boop <githubbot@vadim.me>
2024-07-20 07:31:52 +0200

    Solution 18 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/w5817v0o Prompt:       |                    ^~~ ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp:12: ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp: In function ‘int main()’: ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp:22:20: error: ‘get_odd_collatz’ was not  declared in this scope    22 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ ea1ea362-d1d6-4fe8-8afc-7f1cb254f72a.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -2,16 +2,7 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    vector<int> result;
-    while (n != 1) {
-        if (n % 2 == 1) {
-            result.push_back(n);
-        }
-        n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
-    }
-    result.push_back(1);
-    sort(result.begin(), result.end());
-    return result;
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+    return a == b;
 }
-
-assert (issame(collatz(1), {1}));
