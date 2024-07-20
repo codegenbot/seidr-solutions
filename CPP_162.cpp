ffb3ce8403d2a0cbf0bfe889b90a3224c2afc477
beep boop <githubbot@vadim.me>
2024-07-20 04:43:14 +0200

    Solution 6 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/cmp1bynf Prompt: 510ee08b-f068-4be9-bf1a-a14ec3a86a62.cpp: In function ‘std::string string_to_md5 (const string&)’: 510ee08b-f068-4be9-bf1a-a14ec3a86a62.cpp:11:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    11 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 510ee08b-f068-4be9-bf1a-a14ec3a86a62.cpp:3: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ /usr/bin/ld: /tmp/ccD8GfGx.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 510ee08b-f068-4be9-bf1a-a14ec3a86a62.cpp:(.text+0x97): undefined reference to `M D5' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <cassert>
-#include <openssl/evp.h>
 #include <openssl/md5.h>
+#include <openssl/evp.h>
 
 std::string string_to_md5(const std::string& text) {
     if (text.empty()) {
@@ -9,11 +9,9 @@ std::string string_to_md5(const std::string& text) {
     }
 
     unsigned char digest[MD5_DIGEST_LENGTH];
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md = EVP_md5();
-    mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, text.c_str(), text.size());
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
     EVP_DigestFinal_ex(mdctx, digest, NULL);
     EVP_MD_CTX_free(mdctx);
 
