8d82c7ec176c3906f81ec906fbd4a3332fb3d15b
beep boop <githubbot@vadim.me>
2024-07-20 04:04:53 +0200

    Solution 58 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/g7b2ew9a Prompt: In file included from 5e61a6bc-7b4d-4f94-aeeb-19f7f00f43dd.cpp:15: 5e61a6bc-7b4d-4f94-aeeb-19f7f00f43dd.cpp: In function ‘int main()’: 5e61a6bc-7b4d-4f94-aeeb-19f7f00f43dd.cpp:25:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~ 5e61a6bc-7b4d-4f94-aeeb-19f7f00f43dd.cpp:11:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    11 | bool issame(const std::vector<int>& a, const std::vector<int>& b) {       |      ^~~~~~ 5e61a6bc-7b4d-4f94-aeeb-19f7f00f43dd.cpp:16:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    16 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -14,6 +14,7 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 int main() {
-    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
+    
     return 0;
 }
