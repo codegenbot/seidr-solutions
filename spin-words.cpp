d003035228313154351d5059b5b1a1149af83a7f
beep boop <githubbot@vadim.me>
2024-07-20 02:27:23 +0200

    Solution 18 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/uun7xixa Prompt: 78997423-ae90-4755-af1e-62eb4428b629.cpp: In function ‘std::string spinWords(std ::string)’: 78997423-ae90-4755-af1e-62eb4428b629.cpp:11:42: error: ‘word’ was not declared i n this scope    11 |                 std::string reversedWord(word.rbegin(), word.rend());       |                                          ^~~~ 78997423-ae90-4755-af1e-62eb4428b629.cpp:14:27: error: ‘word’ was not declared i n this scope    14 |                 result += word + " ";       |                           ^~~~ 78997423-ae90-4755-af1e-62eb4428b629.cpp:16:13: error: ‘word’ was not declared i n this scope    16 |             word = "";       |             ^~~~ 78997423-ae90-4755-af1e-62eb4428b629.cpp:19:17: error: ‘word’ was not declared i n this scope    19 |                 word += sentence[i];       |                 ^~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,25 +1,41 @@
+#include <iostream>
+#include <string>
+
 std::string spinWords(std::string sentence) {
     std::string result = "";
-    int wordLength;
+    int i = 0;
     std::string word;
 
-    for (int i = 0; i < sentence.length(); i++) {
-        if (i == sentence.length() || sentence[i] == ' ') {
-            wordLength = i - word.length();
-            if (wordLength >= 5) {
-                std::string reversedWord = "";
-                for (int j = wordLength - 1; j >= 0; j--) {
-                    reversedWord += word[j];
-                }
+    while (i < sentence.length()) {
+        if (sentence[i] != ' ') {
+            word += sentence[i];
+        } else {
+            if (word.length() >= 4) {
+                std::string reversedWord(word.rbegin(), word.rend());
                 result += reversedWord + " ";
             } else {
                 result += word + " ";
             }
             word = "";
-        } else {
-            word += sentence[i];
         }
+        i++;
     }
 
-    return result;
+    // Process the last word
+    if (word.length() >= 4) {
+        std::string reversedWord(word.rbegin(), word.rend());
+        result += reversedWord;
+    } else {
+        result += word;
+    }
+
+    return result.substr(0, result.length() - 1);
+}
+
+int main() {
+    std::string input;
+    while (std::cout << "Enter a string: ", std::getline(std::cin, input)) {
+        std::cout << spinWords(input) << std::endl;
+    }
+    return 0;
 }
