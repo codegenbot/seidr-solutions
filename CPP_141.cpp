8219f214eaac52a035e0a5553f28172b5770db17
beep boop <githubbot@vadim.me>
2024-07-20 04:02:18 +0200

    Solution 2 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0absn9ns Prompt: 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp:18:5: error: ‘string’ does not name a t ype    18 |     string extension = file_name.substr(dotIndex + 1);       |     ^~~~~~ 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp:19:5: error: expected unqualified-id be fore ‘if’    19 |     if (extension != "txt" && extension != "exe" && extension != "dll") {       |     ^~ 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp:23:5: error: expected unqualified-id be fore ‘return’    23 |     return "Yes";       |     ^~~~~~ 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp:24:1: error: expected declaration befor e ‘}’ token    24 | }       | ^ In file included from 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp:26: 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp: In function ‘int main()’: 9fce171e-e967-4a29-b9ad-9bb4d6007c42.cpp:28:13: error: ‘file_name_check’ was not  declared in this scope    28 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,13 +1,10 @@
 #include <string>
-#include <cassert>
 
-using namespace std;
-
-string file_name_check(string file_name) {
+std::string checkFileName(std::string file_name) {
     int digitCount = 0;
     int dotCount = 0;
     int dotIndex = -1;
-
+    
     for (int i = 0; i < file_name.length(); i++) {
         if (file_name[i] >= '0' && file_name[i] <= '9') {
             digitCount++;
@@ -16,22 +13,15 @@ string file_name_check(string file_name) {
             dotIndex = i;
         }
     }
-
+    
     if (digitCount > 3 || dotCount != 1 || dotIndex == 0 || dotIndex == file_name.length() - 1) {
         return "No";
     }
-
-    string extension = file_name.substr(dotIndex + 1);
+    
+    std::string extension = file_name.substr(dotIndex + 1);
     if (extension != "txt" && extension != "exe" && extension != "dll") {
         return "No";
     }
-
+    
     return "Yes";
-}
-
-int main() {
-    assert(file_name_check("s.") == "No");
-    // Add more test cases here
-
-    return 0;
 }
