5e7d8a64f9f55a0bd269bc4fa12f34a496a4461e
beep boop <githubbot@vadim.me>
2024-07-20 02:29:42 +0200

    Solution 80 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9z0vt8xn Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 18d60228-aa2e-4f84-972c-2351d0578af6.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 18d60228-aa2e-4f84-972c-2351d0578af6.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 18d60228-aa2e-4f84-972c-2351d0578af6.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 18d60228-aa2e-4f84-972c-2351d0578af6.cpp:15: 18d60228-aa2e-4f84-972c-2351d0578af6.cpp: In function ‘int main()’: 18d60228-aa2e-4f84-972c-2351d0578af6.cpp:25:20: error: ‘common’ was not declared  in this scope    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 18d60228-aa2e-4f84-972c-2351d0578af6.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,13 +1,26 @@
 #include <set>
 #include <algorithm>
-#include <vector>
-#include <assert.h>
+
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      inserter(intersection, intersection.begin()));
+
+    return vector<int>(intersection.begin(), intersection.end());
+}
 
 bool issame(vector<int> a,vector<int>b){
-    return a.size() == b.size();
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size();i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 int main(){
-    assert (issame(common({4, 3, 2, 8}, {}), {}));
+    assert(issame(common({4, 3, 2, 8}, {}), {}));
     return 0;
 }
