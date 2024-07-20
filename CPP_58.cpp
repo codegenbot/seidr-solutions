cc5b1b9f6a7783fba512c52551bd450aab88f6be
beep boop <githubbot@vadim.me>
2024-07-20 05:39:48 +0200

    Solution 46 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gcmd9cu5 Prompt:       |                    ^~~ 33697b09-650b-449e-b276-998ad13c96ad.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 33697b09-650b-449e-b276-998ad13c96ad.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 33697b09-650b-449e-b276-998ad13c96ad.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 33697b09-650b-449e-b276-998ad13c96ad.cpp:12: 33697b09-650b-449e-b276-998ad13c96ad.cpp: In function ‘int main()’: 33697b09-650b-449e-b276-998ad13c96ad.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 33697b09-650b-449e-b276-998ad13c96ad.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -18,7 +18,3 @@ std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-}
