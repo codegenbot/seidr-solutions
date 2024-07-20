0575b8eda604ce01ec50aeab4b1f849573b67b28
beep boop <githubbot@vadim.me>
2024-07-20 08:46:11 +0200

    Solution 15 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/2dciqxaw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3166600a-7127-4c2d-8f34-0e7499191 318.py", line 1     ```     ^ SyntaxError: invalid syntax

--- camel-case.py
+++ camel-case.py
@@ -1,3 +1,5 @@
-```
+print("Please enter a string in kebab-case: ")
+s = input()
 def camel_case(s):
     return ''.join(word if i == 0 or not word else word.capitalize() for i, word in enumerate(s.replace('-', ' ').split()))
+print(camel_case(s))
