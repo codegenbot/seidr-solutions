43bb4b5be3431bf93ded298cbacf12277e9c1b64
beep boop <githubbot@vadim.me>
2024-07-20 06:13:02 +0200

    Solution 23 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/gw16mzei Prompt:       |                    ^~~ acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp:15: acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp: In function ‘int main()’: acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp:25:20: error: ‘make_a_pile’ was not dec lared in this scope    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ acd59a44-8d32-4841-a868-7e0c5d7aa1ef.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,5 +1,19 @@
 #include <vector>
 
+vector<int> make_a_pile(int n);
+
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 vector<int> make_a_pile(int n){
     vector<int> stones;
     stones.push_back(n);
@@ -14,19 +28,7 @@ vector<int> make_a_pile(int n){
     return stones;
 }
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-int main(){
+int main() {
     vector<int> pile1 = make_a_pile(5);
     vector<int> pile2 = make_a_pile(5);
     
