7a290ed43015fbed3a3c582f2b14990736a49822
beep boop <githubbot@vadim.me>
2024-07-20 08:05:30 +0200

    Solution 53 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jh9eeura Prompt: 11bcf96d-e078-4030-be86-f2144a60e975.cpp: In function ‘int main()’: 11bcf96d-e078-4030-be86-f2144a60e975.cpp:16:41: error: ‘pow’ was not declared in  this scope    16 |         totalDistance += (startHeight * pow(bouncinessIndex, i));       |                                         ^~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -14,7 +14,7 @@ int main() {
     
     double totalDistance = 0.0;
     for(int i=1; i<=numBounces; i++) {
-        totalDistance += (startHeight * pow(2, -i) * bouncinessIndex);
+        totalDistance += (startHeight * pow(bouncinessIndex, i));
     }
     
     cout.precision(5);
