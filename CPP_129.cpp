12000dcc5065b3c35f8a719d82ae6aacdd40eebf
beep boop <githubbot@vadim.me>
2024-07-20 03:24:48 +0200

    Solution 1 of CPP/129, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1clwza4l Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -3,51 +3,26 @@
 using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
-    int N = grid.size();
-    vector<vector<int>> dp(N, vector<int>(N));
-    vector<vector<bool>> visited(N, vector<bool>(N));
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (i == 0 && j == 0) {
-                dp[i][j] = grid[i][j];
-            } else if (i > 0 && j == 0) {
-                dp[i][j] = min({dp[i-1][j], dp[i-1][j+1]});
-            } else if (i == 0) {
-                dp[i][j] = min({dp[i][j-1], dp[i][j+1]});
-            } else {
-                dp[i][j] = min({dp[i-1][j], dp[i-1][j+1], dp[i][j-1], dp[i][j+1]});
+    int n = grid.size();
+    vector<int> res;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 1) {
+                res.push_back(1);
+                for (int l = 0; l < k - 1; l++) {
+                    int x = (i + 1) % n;
+                    int y = (j + 1) % n;
+                    if ((x != i || j == 0 && y == 1) && (y != j || i == 0 && x == 1)) {
+                        res.push_back(grid[x][y]);
+                        i = x;
+                        j = y;
+                    } else {
+                        break;
+                    }
+                }
+                return res;
             }
         }
     }
-
-    vector<int> res;
-    int i = N - 1, j = N - 1;
-    while (k > 0) {
-        if (!visited[i][j]) {
-            visited[i][j] = true;
-            res.push_back(grid[i][j]);
-            k--;
-        }
-        if (i > 0 && !visited[i-1][j]) {
-            i--;
-        } else if (j > 0 && !visited[i][j-1]) {
-            j--;
-        } else {
-            if (i > 0) i--;
-            else j--;
-        }
-    }
-
-    return res;
-}
-
-int main() {
-    vector<vector<int>> grid = {{5,9,3},{4,1,6},{7,8,2}};
-    int k = 1;
-    vector<int> res = minPath(grid, k);
-    for (int i : res) {
-        cout << i << " ";
-    }
-    return 0;
+    return {};
 }
