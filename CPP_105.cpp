7b0aeaf6a4f731918c3669f219ec47a58d1b1f53
beep boop <githubbot@vadim.me>
2024-07-20 06:19:51 +0200

    Solution 41 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/bqe4vu5i Prompt:       |                              ^~~~~~ f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp:26: f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp: In function ‘int main()’: f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f989a252-69af-47f1-9f04-a06ec7fc9d5c.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -14,7 +14,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 std::vector<std::string> by_length(std::vector<int> arr) {
     std::vector<std::string> result;
     std::map<int, std::string> num_to_word = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
+        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, 
         {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
 
@@ -34,14 +34,4 @@ std::vector<std::string> by_length(std::vector<int> arr) {
     }
 
     return result;
-}
-
-int main() {
-    std::vector<int> input = {4, 2, 7, 1, 5, 9};
-    std::vector<std::string> output = by_length(input);
-
-    std::vector<std::string> expected_output = {"Nine", "Seven", "Five", "Four", "Two", "One"};
-    assert(issame(output, expected_output));
-
-    return 0;
 }
