8e0fdd278faeaa86d81667f92a46331777998827
beep boop <githubbot@vadim.me>
2024-07-20 03:56:57 +0200

    Solution 10 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cp34wb3t Prompt: f0cab4ef-6f35-4565-ade4-ca9768d6c47c.cpp: In function ‘std::string string_to_md5 (std::string)’: f0cab4ef-6f35-4565-ade4-ca9768d6c47c.cpp:10:5: error: ‘MD5_CTX’ was not declared  in this scope    10 |     MD5_CTX ctx;       |     ^~~~~~~ f0cab4ef-6f35-4565-ade4-ca9768d6c47c.cpp:11:15: error: ‘ctx’ was not declared in  this scope    11 |     MD5_Init(&ctx);       |               ^~~ f0cab4ef-6f35-4565-ade4-ca9768d6c47c.cpp:11:5: error: ‘MD5_Init’ was not declare d in this scope    11 |     MD5_Init(&ctx);       |     ^~~~~~~~ f0cab4ef-6f35-4565-ade4-ca9768d6c47c.cpp:14:5: error: ‘MD5_Update’ was not decla red in this scope    14 |     MD5_Update(&ctx, ptr, len);       |     ^~~~~~~~~~ f0cab4ef-6f35-4565-ade4-ca9768d6c47c.cpp:15:5: error: ‘MD5_Final’ was not declar ed in this scope    15 |     MD5_Final(result, &ctx);       |     ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,33 +1,25 @@
-#include <openssl/evp.h>
+#include <openssl/md5.h>
+#include <string>
+
+using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    unsigned char md[MD5_DIGEST_LENGTH];
+    unsigned char result[16];
     MD5_CTX ctx;
     MD5_Init(&ctx);
-    const char* ptr = text.c_str();
-    while (*ptr) {
-        MD5_Update(&ctx, ptr, 1);
-        ptr++;
-    }
-    MD5_Final(md, &ctx);
+    const unsigned char* ptr = reinterpret_cast<const unsigned char*>(text.c_str());
+    size_t len = text.size();
+    MD5_Update(&ctx, ptr, len);
+    MD5_Final(result, &ctx);
 
-    string result;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i)
-        result.push_back(tohex((unsigned char)md[i]));
-
-    return result;
-}
-
-string tohex(unsigned int x) {
-    static const char* hexDigits = "0123456789abcdef";
-    unsigned int mask = 0xf;
-    string res;
-
-    for (int i = 3; i >= 0; --i, mask >>= 4) {
-        res.push_back(hexDigits[(x & mask) >> ((3 - i) * 4)]);
+    string md5;
+    for (int i = 0; i < 16; ++i) {
+        char buffer[3];
+        sprintf(buffer, "%02x", result[i]);
+        md5 += buffer;
     }
 
-    return res;
+    return md5;
 }
