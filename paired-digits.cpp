342839549c2d31a4da83a3313046218bb22d7ba3
beep boop <githubbot@vadim.me>
2024-07-20 08:47:35 +0200

    Solution 47 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4wrdftbq Prompt: Program did not receive expected input

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -3,7 +3,7 @@
 
 int pairedDigits(std::string s) {
     int sum = 0;
-    while (s.length() > 1) {
+    if (s.length() > 1) {
         for (int i = 0; i < s.length() - 1; i++) {
             if ((s[i] == '0') || (s[i] - '0' == s[i + 1] - '0')) {
                 sum += (s[i] - '0');
