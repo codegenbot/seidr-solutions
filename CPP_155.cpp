f9c2e79ae0311eca675186c69610f975ffdce016
beep boop <githubbot@vadim.me>
2024-07-20 04:23:17 +0200

    Solution 5 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/t0unynps Prompt: 38821842-d03b-414a-9edd-95a3f4da84ef.cpp:29:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    29 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 38821842-d03b-414a-9edd-95a3f4da84ef.cpp:20:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    20 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 38821842-d03b-414a-9edd-95a3f4da84ef.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 38821842-d03b-414a-9edd-95a3f4da84ef.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -4,10 +4,26 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+vector<int> even_odd_count(int num) {
+    vector<int> counts(2, 0);
+    string numStr = to_string(abs(num));
+    for (char c : numStr) {
+        if ((c - '0') % 2 == 0) {
+            counts[0]++;
+        } else {
+            counts[1]++;
+        }
+    }
+    return counts;
+}
+
+void assert_vector_equal(vector<int> a, vector<int> b) {
+    assert(a.size() == b.size());
+    for (size_t i = 0; i < a.size(); ++i) {
+        assert(a[i] == b[i]);
+    }
 }
 
 int main() {
-    assert(issame(even_odd_count(0), {1, 0}));
+    assert_vector_equal(even_odd_count(0), {1, 0});
 }
