4c3d5e2dc678d972296f27fab01f676335c73c57
beep boop <githubbot@vadim.me>
2024-07-20 02:04:49 +0200

    Solution 10 of CPP/43, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/80vpf3c3 Prompt: 0a922dc8-2713-43e9-abe0-79921ecbdda4.cpp:1:24: error: ‘vector’ was not declared in this scope     1 | bool pairs_sum_to_zero(vector<int> l) {       |                        ^~~~~~ 0a922dc8-2713-43e9-abe0-79921ecbdda4.cpp:1:31: error: expected primary-expressio n before ‘int’     1 | bool pairs_sum_to_zero(vector<int> l) {       |                               ^~~ In file included from 0a922dc8-2713-43e9-abe0-79921ecbdda4.cpp:11: 0a922dc8-2713-43e9-abe0-79921ecbdda4.cpp: In function ‘int main()’: 0a922dc8-2713-43e9-abe0-79921ecbdda4.cpp:13:30: error: ‘pairs_sum_to_zero’ canno t be used as a function    13 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,8 +1,10 @@
-bool pairs_sum_to_zero(vector<int> l) {
-    set<int> s(l.begin(), l.end());
-    for (auto it = s.begin(); it != s.end(); ++it) {
-        int complement = -(*it);
-        if (s.find(complement) != s.end() && *it != complement) {
+#include <vector>
+#include <set>
+
+bool pairs_sum_to_zero(std::vector<int> l) {
+    std::set<int> s(l.begin(), l.end());
+    for (int i : s) {
+        if (s.find(-i) != s.end()) {
             return true;
         }
     }
