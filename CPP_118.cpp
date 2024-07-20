3535c1f42256a66551102b4b886afeba8d1c7c9f
beep boop <githubbot@vadim.me>
2024-07-20 03:15:36 +0200

    Solution 11 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/djxkum1f Prompt: dff6e914-47bf-4a16-b5bd-43da25087bf2.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ In file included from dff6e914-47bf-4a16-b5bd-43da25087bf2.cpp:15: dff6e914-47bf-4a16-b5bd-43da25087bf2.cpp: In function ‘int main()’: dff6e914-47bf-4a16-b5bd-43da25087bf2.cpp:17:13: error: ‘get_closest_vowel’ was n ot declared in this scope    17 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -2,14 +2,19 @@
 #include <cctype>
 
 std::string get_closest_vowel(std::string word) {
+    int left = 0;
     for(int i = 0; i < word.length(); i++) {
-        if(isalpha(word[i])) {
-            if(isvowel(tolower(word[i]))) return std::string(1, tolower(word[i]));
+        if(!isalpha(word[i])) continue;
+        if(isvowel(word[i])) {
+            while(left < i && !isalpha(word[left]) && left < i) {
+                left++;
+            }
+            return string(1, tolower(word[i]));
         }
     }
     return "";
 }
 
 int main() {
-    assert(get_closest_vowel("Above") == "o");
+    assert(get_closest_vowel("Above") == "o");  
 }
