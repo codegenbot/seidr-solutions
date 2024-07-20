fb62cc5f666a96c29090cfa2ace40c3b67f2a71f
beep boop <githubbot@vadim.me>
2024-07-20 12:26:36 +0200

    Solution 41 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/z1wleq0u Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,7 +1,6 @@
 ```c++
 #include <iostream>
 #include <cmath>
-#include <string>
 
 double vectorDistance(int n, float v1[], float v2[]) {
     double sum = 0.0;
@@ -20,15 +19,10 @@ int main() {
     float v1[n], v2[n];
     std::cout << "Enter vector 1 (elements separated by spaces): ";
     for (int i = 0; i < n; i++) {
-        std::string s;
-        std::cin >> s;
-        v1[i] = stof(s); 
-    }
-    std::cout << "Enter vector 2 (elements separated by spaces): ";
-    for (int i = 0; i < n; i++) {
-        std::string s;
-        std::cin >> s;
-        v2[i] = stof(s);
+        std::cout << "Enter element " << i+1 << " of vector 1: ";
+        std::cin >> v1[i];
+        std::cout << "Enter element " << i+1 << " of vector 2: ";
+        std::cin >> v2[i];
     }
 
     double distance = vectorDistance(n, v1, v2);
