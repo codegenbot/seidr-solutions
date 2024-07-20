fdac01ae358bde038ba8bef7da579fbf8ed514c9
beep boop <githubbot@vadim.me>
2024-07-20 09:28:03 +0200

    Solution 50 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jncxsee7 Prompt: it must return True for input f&f&f|f|f|t|t&t|t&f|t&t&f|t&t|f, but it returns .

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,17 +1,28 @@
 def solve_boolean(expression):
-    stack = []
-    for char in expression:
-        if char in ['T', 'F']:
-            stack.append(char)
-        elif char in ['|', '&']:
-            while len(stack) >= 2 and stack[-1] == '&' and (char == '|' or stack[-2] == '|'):
-                a = stack.pop() == 'T'
-                b = stack.pop() == 'T'
-                stack.append(a and b)
-            if char == '&':
-                stack.append(stack.pop() == 'T')
-        elif len(stack) >= 1:
-            break
-    return stack.pop() == 'T'
+    def evaluate_operator(s, op):
+        if s[0] == '(':
+            return int(evaluate_expression(s[1:-1]))
+        elif s[0] in ['T', 'F']:
+            return {'T': 1, 'F': 0}[s[0]]
+        else:
+            a = evaluate_operator(s[:3], op)
+            b = evaluate_operator(s[4:], op)
+            return op == '&' and a & b or op == '|' and a | b
 
-print(solve_boolean('f&f&f|f|f|t|t&t|t&f|t&t&f|t&t|f'))
+    def evaluate_expression(expression):
+        result_str = ''
+        parsing = True
+        for char in expression[::-1]:
+            if char in ['T', 'F']:
+                if parsing:
+                    result_str += char
+                else:
+                    return str(evaluate_operator(result_str + char, '|'))
+            elif char in ['|', '&']:
+                if char == '&':
+                    result_str += '& '
+                else:
+                    result_str += '| '
+        return str(evaluate_operator(result_str, '|'))
+
+    return int(evaluate_expression(expression))
