5ac75ec947f1509a4fcd3050b89e773c294db2b2
beep boop <githubbot@vadim.me>
2024-07-20 06:46:59 +0200

    Solution 71 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lqf17hr8 Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,39 +1,16 @@
+Here is the solution:
+
+```cpp
 string int_to_mini_roman(int number) {
-    string romans[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", 
-                       "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX",
-                       "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX",
-                       "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX",
-                       "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX",
-                       "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX",
-                       "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX",
-                       "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX",
-                       "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX",
-                       "XC", "XCI", "XCII", "XCIII", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX",
-                       "C"};
-    string thousands = "M";
-    string hundreds[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCCC", "CM"};
-    string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string result;
 
-    int m = number / 1000;
-    int c = (number % 1000) / 100;
-    int x = (number % 100) / 10;
-    int i = number % 10;
-
-    string result = "";
-    if(m > 0) {
-        for(int k = 0; k < m; k++) {
-            result += thousands;
+    for (int i = 0; i < 13; i++) {
+        while (number >= value[i]) {
+            number -= value[i];
+            result += roman[i];
         }
-    }
-    if(c > 0) {
-        result += hundreds[c];
-    }
-    if(x > 0) {
-        result += tens[x];
-    }
-    if(i > 0) {
-        result += ones[i];
     }
 
     return result;
