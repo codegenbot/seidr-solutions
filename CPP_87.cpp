26cc26c97277de053758ec1b2a7aaea685e7599a
beep boop <githubbot@vadim.me>
2024-07-20 06:14:32 +0200

    Solution 92 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6uquq1ll Prompt:       |                                   ^~~~~~ 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp:22:42: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp:22:49: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp:22:56: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp:21: 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp: In function ‘int main()’: 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp:34:20: error: ‘get_row’ was not declare d in this scope    34 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 64a6b360-82be-4b5d-a6bf-0329ae8b31b7.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,16 +1,9 @@
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 bool are_same(vector<vector<int>> a, vector<vector<int>> b){
     return a == b;
-}
-
-vector<vector<int>> get_row(vector<vector<int>> lst, int x);
-
-int main() {
-    // Test cases
-    assert (are_same(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
-    
-    return 0;
 }
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
@@ -31,4 +24,11 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x){
     });
     
     return result;
+}
+
+int main() {
+    // Test cases
+    assert(are_same(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    
+    return 0;
 }
