f667504a58bca1cfbeeef642a6a9d8ba40d0bad4
beep boop <githubbot@vadim.me>
2024-07-20 05:07:10 +0200

    Solution 60 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ljpszng7 Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ f5a7b8c5-35c1-4587-a8fe-a663c10f5a8f.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ f5a7b8c5-35c1-4587-a8fe-a663c10f5a8f.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from f5a7b8c5-35c1-4587-a8fe-a663c10f5a8f.cpp:13: f5a7b8c5-35c1-4587-a8fe-a663c10f5a8f.cpp: In function ‘int main()’: f5a7b8c5-35c1-4587-a8fe-a663c10f5a8f.cpp:23:20: error: ‘rescale_to_unit’ was not  declared in this scope    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ f5a7b8c5-35c1-4587-a8fe-a663c10f5a8f.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -3,11 +3,13 @@
 #include <algorithm>
 #include <cassert>
 
-std::vector<float> rescale_to_unit(const std::vector<float>& numbers) {
-    float min_val = *std::min_element(numbers.begin(), numbers.end());
-    float max_val = *std::max_element(numbers.begin(), numbers.end());
+using namespace std;
+
+vector<float> rescale_to_unit(vector<float> numbers) {
+    float min_val = *min_element(numbers.begin(), numbers.end());
+    float max_val = *max_element(numbers.begin(), numbers.end());
     
-    std::vector<float> rescaled_numbers;
+    vector<float> rescaled_numbers;
     for (float num : numbers) {
         float rescaled_num = (num - min_val) / (max_val - min_val);
         rescaled_numbers.push_back(rescaled_num);
@@ -16,7 +18,7 @@ std::vector<float> rescale_to_unit(const std::vector<float>& numbers) {
     return rescaled_numbers;
 }
 
-bool issame(const std::vector<float>& a, const std::vector<float>& b) {
+bool issame(vector<float> a, vector<float> b){
     return a == b;
 }
 
