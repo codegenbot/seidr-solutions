4891b81707c44c8e3778b0ec0d8b572ea435ad61
beep boop <githubbot@vadim.me>
2024-07-20 04:19:54 +0200

    Solution 50 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/i8e22878 Prompt: In file included from 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp:40:34: error: expected primary-expressi on before ‘int’    40 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp:39: 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp: In function ‘int main()’: 205e4bb0-568a-4bc8-b952-ed3f6e30e737.cpp:49:20: error: ‘order_by_points’ was not  declared in this scope    49 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -13,33 +13,8 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return std::is_permutation(a_sorted.begin(), a_sorted.end(), b_sorted.begin());
 }
 
-int order_by_points(const std::vector<int>& points) {
-    int res = 0;
-    for (int i : points) {
-        res += i;
-    }
-    return res;
-}
-
 int main() {
-    int n, m;
-    std::cin >> n >> m;
-
-    std::vector<int> vec1(n), vec2(m);
-
-    for (int i = 0; i < n; ++i) {
-        std::cin >> vec1[i];
-    }
-    
-    for (int i = 0; i < m; ++i) {
-        std::cin >> vec2[i];
-    }
-
-    if (issame(vec1, vec2)) {
-        std::cout << "Vectors are permutations of each other.\n";
-    } else {
-        std::cout << "Vectors are not permutations of each other.\n";
-    }
+    assert(issame({0, 6, 6, -76, -21, 23, 4}, {-76, -21, 0, 4, 23, 6, 6}));
 
     return 0;
 }
