46693de8ce2d397f900297b845ee11b21493b11d
beep boop <githubbot@vadim.me>
2024-07-20 02:00:42 +0200

    Solution 97 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/u46z59hv Prompt: a9693445-39ea-4216-a783-d78decbc3b67.cpp: In function ‘std::string intToMiniRoma n(int)’: a9693445-39ea-4216-a783-d78decbc3b67.cpp:13:78: error: narrowing conversion of ‘ 17229’ from ‘int’ to ‘char’ [-Wnarrowing]    13 |                   {10, 'X'}, {9, 'IX'}, {5, 'V'}, {4, 'IV'}, {1, 'I'}};       |                                                                      ^ a9693445-39ea-4216-a783-d78decbc3b67.cpp:13:78: error: narrowing conversion of ‘ 17220’ from ‘int’ to ‘char’ [-Wnarrowing] a9693445-39ea-4216-a783-d78decbc3b67.cpp:13:78: error: narrowing conversion of ‘ 22595’ from ‘int’ to ‘char’ [-Wnarrowing] a9693445-39ea-4216-a783-d78decbc3b67.cpp:13:78: error: narrowing conversion of ‘ 22604’ from ‘int’ to ‘char’ [-Wnarrowing] a9693445-39ea-4216-a783-d78decbc3b67.cpp:13:78: error: narrowing conversion of ‘ 18776’ from ‘int’ to ‘char’ [-Wnarrowing] a9693445-39ea-4216-a783-d78decbc3b67.cpp:13:78: error: narrowing conversion of ‘ 18774’ from ‘int’ to ‘char’ [-Wnarrowing] In file included from a9693445-39ea-4216-a783-d78decbc3b67.cpp:31: a9693445-39ea-4216-a783-d78decbc3b67.cpp: In function ‘int main()’: a9693445-39ea-4216-a783-d78decbc3b67.cpp:33:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘intToMiniRoman’?    33 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,2 +1,29 @@
-{10, static_cast<char>('X')}, {9, static_cast<char>('IX')}, {5, static_cast<char>('V')}, 
-{4, static_cast<char>('IV')}, {1, static_cast<char>('I')}};
+#include <iostream>
+#include <string>
+
+struct RomanPair {
+    int value;
+    char symbol;
+};
+
+std::string intToMiniRoman(int num) {
+    std::string roman = "";
+    RomanPair pairs[] = {{1000, 'M'}, {900, 'CM'}, {500, 'D'}, {400, 'CD'},
+                          {100, 'C'}, {90, 'XC'}, {50, 'L'}, {40, 'XL'},
+                          {10, 'X'}, {9, 'IX'}, {5, 'V'}, {4, 'IV'}, {1, 'I'}};
+    int i = 0;
+    while (num > 0) {
+        if (num >= pairs[i].value) {
+            num -= pairs[i].value;
+            roman += pairs[i].symbol;
+        } else {
+            i++;
+        }
+    }
+    return roman;
+}
+
+int mainTest() {
+    std::cout << intToMiniRoman(1000) << std::endl;
+    return 0;
+}
