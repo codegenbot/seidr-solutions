97684e47ab17c5a9e5962053a385e30b35bf2c48
beep boop <githubbot@vadim.me>
2024-07-20 03:17:21 +0200

    Solution 11 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bnfqx7vn Prompt:       |                    ^~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:9: 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp: In function ‘int main()’: 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 7f28ef16-d605-4d45-8259-9d7f71a56597.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -6,28 +6,3 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     sort(result.begin(), result.end());
     return result;
 }
-
-int main() {
-    int n, k;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
-    std::cout << "Enter the number of maximum elements to find: ";
-    std::cin >> k;
-    
-    if(k > n) {
-        k = n;
-    }
-    
-    std::vector<int> arr(n);
-    for(int i=0; i<n; i++) {
-        std::cout << "Enter element " << (i+1) << ": ";
-        std::cin >> arr[i];
-    }
-    
-    std::vector<int> result = maximum(arr, k);
-    std::cout << "Maximum " << k << " elements are: ";
-    for(int i=0; i<k; i++) {
-        std::cout << result[i] << " ";
-    }
-    return 0;
-}
