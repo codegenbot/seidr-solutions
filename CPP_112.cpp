d2cf4e8fcd7a5eb178a9c71fccd108d9128311d8
beep boop <githubbot@vadim.me>
2024-07-20 05:52:17 +0200

    Solution 0 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wt5n3u3u Prompt: Complete the following code given the task description and function signature.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -16,10 +16,6 @@ vector<string> reverse_delete(string s, string c) {
     result.push_back(temp);
     string rev = temp;
     reverse(rev.begin(), rev.end());
-    if (temp == rev) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
-    }
+    result.push_back((temp == rev) ? "True" : "False");
     return result;
 }
