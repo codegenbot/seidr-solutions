7a56c09a6739f5a7f9cd08f1310d6ae0cb2539f4
beep boop <githubbot@vadim.me>
2024-07-20 04:57:51 +0200

    Solution 23 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/k36os6g3 Prompt:    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 688b4530-39b4-4785-ba85-743abcaba109.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 688b4530-39b4-4785-ba85-743abcaba109.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 688b4530-39b4-4785-ba85-743abcaba109.cpp:18: 688b4530-39b4-4785-ba85-743abcaba109.cpp: In function ‘int main()’: 688b4530-39b4-4785-ba85-743abcaba109.cpp:28:20: error: ‘parse_music’ was not dec lared in this scope    28 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 688b4530-39b4-4785-ba85-743abcaba109.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -14,7 +14,7 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
-vector<int> parse_music(string music_string) {
+vector<int> parse_music(string music_string){
     vector<int> beats;
     int length = music_string.length();
     for (int i = 0; i < length; ++i) {
@@ -22,12 +22,13 @@ vector<int> parse_music(string music_string) {
             beats.push_back(4);
         } else if (music_string[i] == '|') {
             if (beats.empty()) {
-                continue;
-            }
-            if (beats.back() == 4) {
-                beats.back() = 2;
+                beats.push_back(1);
             } else {
-                beats.back() = 1;
+                if (beats.back() == 4) {
+                    beats.back() = 2;
+                } else {
+                    beats.back() = 1;
+                }
             }
         }
     }
