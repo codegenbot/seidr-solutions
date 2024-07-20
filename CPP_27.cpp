a274f91f7917b10d350b469504add925117fcb8b
beep boop <githubbot@vadim.me>
2024-07-20 03:10:58 +0200

    Solution 78 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/hcqrfvib Prompt: 2a29265d-d369-4c1e-ad29-95f393e90d33.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 2a29265d-d369-4c1e-ad29-95f393e90d33.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 2a29265d-d369-4c1e-ad29-95f393e90d33.cpp:27: 2a29265d-d369-4c1e-ad29-95f393e90d33.cpp: In function ‘int main()’: 2a29265d-d369-4c1e-ad29-95f393e90d33.cpp:29:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    29 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -15,7 +15,7 @@ std::string flip_case(std::string str) {
 }
 
 int main() {
-    assert(flip_case("Hello, World!") == "hELLO, wORLD!");
+    assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
 
     std::string input_str;
     std::getline(std::cin, input_str);
