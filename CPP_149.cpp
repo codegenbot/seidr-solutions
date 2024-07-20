41c91c8325b7220f61a653989729d087550f6ee3
beep boop <githubbot@vadim.me>
2024-07-20 03:48:31 +0200

    Solution 21 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lpolsawb Prompt:    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:19: 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp: In function ‘int main()’: 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:29:20: error: ‘sorted_list_sum’ was not  declared in this scope    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8915eb58-1c1a-4814-bb7a-bae85cdf5f11.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,12 +1,7 @@
 bool issame(vector<string> a,vector<string>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        vector<string> v1,v2;
-        for(string s:a)if(s.length()==a[i].length())v1.push_back(s);
-        for(string s:b)if(s.length()==b[i].length())v2.push_back(s);
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-        if(v1!=v2)return false;
+    if(a.size() != b.size())return false;
+    for(int i=0; i<a.size(); i++){
+        if(sorted_list_sum({a[i]}).size() != sorted_list_sum({b[i]}).size())return false;
     }
     return true;
 }
