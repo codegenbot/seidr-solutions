80299503c31cc1cc74eb963f14be6a6992af8e6f
beep boop <githubbot@vadim.me>
2024-07-20 05:07:24 +0200

    Solution 64 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5fawgjpk Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9f0171ff-ca57-471d-a9e2-a63f2469b034.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9f0171ff-ca57-471d-a9e2-a63f2469b034.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9f0171ff-ca57-471d-a9e2-a63f2469b034.cpp:11: 9f0171ff-ca57-471d-a9e2-a63f2469b034.cpp: In function ‘int main()’: 9f0171ff-ca57-471d-a9e2-a63f2469b034.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 9f0171ff-ca57-471d-a9e2-a63f2469b034.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,29 +1,9 @@
 #include <vector>
-#include <list>
 #include <boost/any.hpp>
 
-using namespace std;
+bool issame(vector<int> a, vector<int> b);
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(size_t i=0; i<a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<int> filter_integers(list<boost::any> values);
-
-int main() {
-    // Main function code here
-    return 0;
-}
-
-vector<int> filter_integers(list<boost::any> values){
+vector<int> filter_integers(list_any values){
     vector<int> result;
     for(auto val : values){
         if(val.type() == typeid(int)){
