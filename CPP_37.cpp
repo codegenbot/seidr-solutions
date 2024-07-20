feba249b86413109a0500580cc7e3d845125ccca
beep boop <githubbot@vadim.me>
2024-07-20 05:11:15 +0200

    Solution 18 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/etghzrq2 Prompt:    26 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 3c0fcac8-3a4e-48b7-8e22-a16dd279a553.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 3c0fcac8-3a4e-48b7-8e22-a16dd279a553.cpp:26:43: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 3c0fcac8-3a4e-48b7-8e22-a16dd279a553.cpp:25: 3c0fcac8-3a4e-48b7-8e22-a16dd279a553.cpp: In function ‘int main()’: 3c0fcac8-3a4e-48b7-8e22-a16dd279a553.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 3c0fcac8-3a4e-48b7-8e22-a16dd279a553.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,21 +1,22 @@
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
-
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            even_values.push_back(l[i]);
-        }
+    vector<float> result;
+    
+    for (int i = 0; i < l.size(); i += 2) {
+        even_values.push_back(l[i]);
     }
-
+    
     sort(even_values.begin(), even_values.end());
-
+    
     int j = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            l[i] = even_values[j];
+            result.push_back(even_values[j]);
             j++;
+        } else {
+            result.push_back(l[i]);
         }
     }
-
-    return l;
+    
+    return result;
 }
