935f20b6014ba58597253fc8a9839cf196cb904c
beep boop <githubbot@vadim.me>
2024-07-20 02:45:12 +0200

    Solution 8 of CPP/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/jt8i5lsy Prompt: Complete the following code given the task description and function signature.

--- CPP_89.cpp
+++ CPP_89.cpp
@@ -2,25 +2,17 @@ string encrypt(string s){
     string result = "";
     for(int i=0; i<s.length(); i++){
         char c = s[i];
-        if(c >= 'a' && c <= 'i') {
-            if((c - 'a') % 26 < 2) {
-                result += (char)((c - 'a' - 2 + 26) % 26 + 'a');
-            } else {
-                result += (char)((c - 'a' - 2) % 26 + 'a');
-            }
-        } else if(c >= 'j' && c <= 'o') {
-            if((c - 'j' - 1) % 26 < 2) {
-                result += (char)((c - 'j' - 3 + 26) % 26 + 'j');
-            } else {
-                result += (char)((c - 'j' - 3) % 26 + 'j');
-            }
-        } else if(c >= 'p' && c <= 'z') {
-            if((c - 'p' - 1) % 26 < 2) {
-                result += (char)((c - 'p' - 4 + 26) % 26 + 'p');
-            } else {
-                result += (char)((c - 'p' - 4) % 26 + 'p');
-            }
-        } else {
+        if(c >= 'a' && c <= 'z'){
+            int val = (int)c - 97;
+            val = (val + 2*2) % 26;
+            result += (char)(val+97);
+        }
+        else if(c >= 'A' && c <= 'Z'){
+            int val = (int)c - 65;
+            val = (val + 2*2) % 26;
+            result += (char)(val+65);
+        }
+        else{
             result += c;
         }
     }
