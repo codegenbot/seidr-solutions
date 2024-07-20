c0b13efacde9e3f32b0151ef48142b25d99b4248
beep boop <githubbot@vadim.me>
2024-07-20 06:29:18 +0200

    Solution 55 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,32 +1,39 @@
-#include <string>
-#include <algorithm>
 #include <boost/any.hpp>
-
-using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(b);
+        return (int) a > (float) b ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int) a > (double) b ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (float) a > (int) b ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (double) a > (int) b ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string str = boost::any_cast<string>(a);
+        float f = boost::any_cast<float>(b);
+        return str > to_string(f) ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        string str = boost::any_cast<string>(a);
+        double d = boost::any_cast<double>(b);
+        return str > to_string(d) ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        float f = boost::any_cast<float>(a);
+        string str = boost::any_cast<string>(b);
+        return to_string(f) > str ? a : b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        double d = boost::any_cast<double>(a);
+        string str = boost::any_cast<string>(b);
+        return to_string(d) > str ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        if (str1 != str2) {
+            return str1 > str2 ? a : b;
+        } else {
+            return "None";
+        }
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (boost::lexical_cast<double>(a) > boost::lexical_cast<double>(b))
-            return a;
-        else if (boost::lexical_cast<double>(a) < boost::lexical_cast<double>(b))
-            return b;
-        else
-            return boost::any("None");
-    }
-    else {
-        double aDouble = boost::lexical_cast<double>(boost::any_cast<string>(a));
-        double bDouble = boost::lexical_cast<double>(boost::any_cast<string>(b));
-        if (aDouble > bDouble)
-            return a;
-        else if (aDouble < bDouble)
-            return b;
-        else
-            return boost::any("None");
-    }
+
+    return "None";
 }
