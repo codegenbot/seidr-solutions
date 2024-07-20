e1b23d9725fed200db94d2a8f7067ad4f5458439
beep boop <githubbot@vadim.me>
2024-07-20 13:23:57 +0200

    Solution 72 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sz5cj53h Prompt: Complete the following code given the task description and function signature.

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -6,8 +6,9 @@ int count = 0;
             sum += temp % 10;
             temp /= 10;
         }
-        if (sum > 0)
+        if (sum > 0) {
             count++;
+        }
     }
     return count;
 }
