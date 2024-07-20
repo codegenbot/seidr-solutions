1731da4c17fc6262317aa0843f59a1f4a9864de5
beep boop <githubbot@vadim.me>
2024-07-20 02:58:05 +0200

    Solution 80 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/s4cvw7va Prompt: 03045da4-93ec-413a-9ffb-d9b11b3c7c27.cpp: In function ‘int starts_one_ends(int)’ : 03045da4-93ec-413a-9ffb-d9b11b3c7c27.cpp:12:27: error: ‘pow’ was not declared in  this scope    12 |             int num = i * pow(10, j);       |                           ^~~ 03045da4-93ec-413a-9ffb-d9b11b3c7c27.cpp: At global scope: 03045da4-93ec-413a-9ffb-d9b11b3c7c27.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 03045da4-93ec-413a-9ffb-d9b11b3c7c27.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -9,11 +9,8 @@ int starts_one_ends(int n) {
         if (i == 1 || i % 10 == 1) {
             count++;
         }
-        for (int j = 0; j < n - 1; j++) {
-            int num = i * pow(10, j);
-            if ((num % 10) == 1) {
-                count++;
-            }
+        if ((i / pow(10, (n - 1))) % 10 == 1) {
+            count++;
         }
     }
     return count;
