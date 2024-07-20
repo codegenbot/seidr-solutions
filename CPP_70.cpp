a7695ae75e67ebdf0c6f1a997ea50a4726c2a751
beep boop <githubbot@vadim.me>
2024-07-20 02:33:39 +0200

    Solution 29 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/6jhfhacy Prompt:       |                    ^~~ 77045e59-8fdf-4f57-b7df-d9204e652840.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 77045e59-8fdf-4f57-b7df-d9204e652840.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77045e59-8fdf-4f57-b7df-d9204e652840.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77045e59-8fdf-4f57-b7df-d9204e652840.cpp:19: 77045e59-8fdf-4f57-b7df-d9204e652840.cpp: In function ‘int main()’: 77045e59-8fdf-4f57-b7df-d9204e652840.cpp:29:20: error: ‘strange_sort_list’ was n ot declared in this scope    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 77045e59-8fdf-4f57-b7df-d9204e652840.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,18 +1,17 @@
 #include <algorithm>
 #include <vector>
-#include <limits>
 
 std::vector<int> strange_sort_vector(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
-        int min_val = *min_element(lst.begin(), lst.end());
-        int max_val = *max_element(lst.begin(), lst.end());
+        int min_val = *std::min_element(lst.begin(), lst.end());
+        int max_val = *std::max_element(lst.begin(), lst.end());
         if (min_val == max_val) {
             result.push_back(min_val);
             lst.pop_back();
         } else {
             result.push_back(min_val);
-            auto it = find(lst.begin(), lst.end(), max_val);
+            auto it = std::find(lst.begin(), lst.end(), max_val);
             lst.erase(it);
             result.push_back(max_val);
         }
