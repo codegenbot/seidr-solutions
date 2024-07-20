0f8aa89e38b8b1a380fe7d62d0f90bfbd65cbe02
beep boop <githubbot@vadim.me>
2024-07-20 04:09:59 +0200

    Solution 17 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/38kvdprn Prompt:       |             ^~~~~~ e782cb2c-626b-4f7c-9695-b1329011452d.cpp:42:20: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ e782cb2c-626b-4f7c-9695-b1329011452d.cpp:42:27: error: ‘vector’ was not declared  in this scope    42 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e782cb2c-626b-4f7c-9695-b1329011452d.cpp:42:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from e782cb2c-626b-4f7c-9695-b1329011452d.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e782cb2c-626b-4f7c-9695-b1329011452d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e782cb2c-626b-4f7c-9695-b1329011452d.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,10 +1,3 @@
-#include <vector>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b); 
-
-std::vector<int> count_up_to(int n);
-
 bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
@@ -16,24 +9,3 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     }
     return true;
 }
-
-std::vector<int> count_up_to(int n) {
-    std::vector<int> primes;
-    for (int i = 2; i < n; ++i) {
-        bool is_prime = true;
-        for (int j = 2; j * j <= i; ++j) {
-            if (i % j == 0) {
-                is_prime = false;
-                break;
-            }
-        }
-        if (is_prime) {
-            primes.push_back(i);
-        }
-    }
-    return primes;
-}
-
-assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-
-return 0;
