a7ea1d3f39c0c773bc13437c327c97b677c35172
beep boop <githubbot@vadim.me>
2024-07-20 02:55:46 +0200

    Solution 15 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/5w4496gj Prompt:     5 | 26ef3afd-bc99-4f4f-b01c-2cb1f3b9fc39.cpp:30:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    30 | int main(){       |         ^~ 26ef3afd-bc99-4f4f-b01c-2cb1f3b9fc39.cpp:30:9: note: remove parentheses to defau lt-initialize a variable    30 | int main(){       |         ^~       |         -- 26ef3afd-bc99-4f4f-b01c-2cb1f3b9fc39.cpp:30:9: note: or replace parentheses with  braces to value-initialize a variable 26ef3afd-bc99-4f4f-b01c-2cb1f3b9fc39.cpp:30:11: error: a function-definition is not allowed here before ‘{’ token    30 | int main(){       |           ^ 26ef3afd-bc99-4f4f-b01c-2cb1f3b9fc39.cpp:32:2: error: expected ‘}’ at end of inp ut    32 | }       |  ^ 26ef3afd-bc99-4f4f-b01c-2cb1f3b9fc39.cpp:12:12: note: to match this ‘{’    12 | int main() {       |            ^

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -13,8 +13,7 @@ int main() {
     vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     sort(l.begin(), l.end());
     
-    vector<int> unique_l(l.begin(), unique(l.begin(), l.end()));
-    assert(issame(unique_l, {0, 2, 3, 5, 9, 123}));
+    assert(issame(vector<int>(l.begin(), unique(l.begin(), l.end())), vector<int>({0, 2, 3, 5, 9, 123}));
     
     return 0;
 }
