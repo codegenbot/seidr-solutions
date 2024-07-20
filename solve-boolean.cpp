741616ec566cee69b13c9f556d71940af3d4bf87
beep boop <githubbot@vadim.me>
2024-07-20 08:28:50 +0200

    Solution 11 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ogeq0x0x Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,14 +1,53 @@
-string solveBoolean(string s) {
-    bool res = (s[0] == 'T');
-    for(int i=1; i<s.length(); i++){
-        if(s[i]=='&') {
-            res &= (s[i+1] == 'T');
-            i++;
-        }
-        else if(s[i] == '|') {
-            res |= (s[i+1] == 'T');
-            i++;
+#include <string>
+using namespace std;
+
+bool evaluateBooleanExpression(string expression) {
+    bool result = true;
+    for (int i = 0; i < expression.size(); i++) {
+        switch (expression[i]) {
+            case 'T':
+                return true;
+            case 'F':
+                return false;
+            case '&': {
+                bool a = true, b = true;
+                if (i + 1 < expression.size()) {
+                    if (expression[i + 1] == 'T')
+                        a = true;
+                    else
+                        a = false;
+                    i++;
+                }
+                if (i + 1 < expression.size()) {
+                    if (expression[i + 1] == 'T')
+                        b = true;
+                    else
+                        b = false;
+                    i++;
+                }
+                result &= a && b;
+                break;
+            }
+            case '|': {
+                bool a = true, b = true;
+                if (i + 1 < expression.size()) {
+                    if (expression[i + 1] == 'T')
+                        a = true;
+                    else
+                        a = false;
+                    i++;
+                }
+                if (i + 1 < expression.size()) {
+                    if (expression[i + 1] == 'T')
+                        b = true;
+                    else
+                        b = false;
+                    i++;
+                }
+                result |= a || b;
+                break;
+            }
         }
     }
-    return res ? "True" : "False";
+    return result;
 }
