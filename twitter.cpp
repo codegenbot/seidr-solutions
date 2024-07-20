f5e8c34776fc9ea81827c2b6dff7f56246817e79
beep boop <githubbot@vadim.me>
2024-07-20 12:21:32 +0200

    Solution 75 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/jvjx9h77 Prompt: 2db6dc7d-239e-4a86-8fb4-491dd0004ece.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string validateTweet(std::string tweet) {       |      ^~~~~~ 2db6dc7d-239e-4a86-8fb4-491dd0004ece.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string validateTweet(std::string tweet) {

--- twitter.cpp
+++ twitter.cpp
@@ -1,11 +1,10 @@
 #include <string>
-using namespace std;
 
-string validateTweet(string tweet) {
+std::string validateTweet(std::string tweet) {
     if (tweet.empty()) {
         return "You didn't type anything";
     }
     if (tweet.length() > 140) {
         return "Too many characters";
     }
-    return "Your tweet has " + to_string(tweet.length()) + " characters";
+    return "Your tweet has " + std::to_string(tweet.length()) + " characters";
