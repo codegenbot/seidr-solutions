96b59c25609cfacea8acc1a6f12a66de90d5a868
beep boop <githubbot@vadim.me>
2024-07-20 09:23:52 +0200

    Solution 64 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4wrdftbq Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,8 +1,14 @@
+```c++
+#include <iostream>
+#include <string>
+
 int pairedDigits(std::string s) {
     int sum = 0;
-    for (int i = 0; i < s.length() - 1; i++) {
-        if ((s[i] == '0') || (s[i] - '0' == s[i + 1] - '0')) {
-            sum += (s[i] - '0');
+    std::string temp = s;
+    while (temp[0] == '0' && temp.length() > 1) temp.erase(0, 1);
+    for (int i = 0; i < temp.length() - 1; i++) {
+        if ((i < temp.length() - 1) && ((temp[i] == '0') || (temp[i] - '0' == temp[i + 1] - '0'))) {
+            sum += (temp[i] - '0');
         }
     }
     return sum;
