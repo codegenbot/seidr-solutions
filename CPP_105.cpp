194dd26d964ce141a7b1565137591284ac741fdf
beep boop <githubbot@vadim.me>
2024-07-20 03:10:06 +0200

    Solution 50 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n10wl24y Prompt:       |                              ^~~~~~ 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp:47:37: error: ‘string’ was not declared  in this scope    47 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp:47:44: error: ‘b’ was not declared in t his scope    47 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp:47:45: error: expression list treated a s compound expression in initializer [-fpermissive]    47 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp:46: 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp: In function ‘int main()’: 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp:56:20: error: ‘by_length’ was not decla red in this scope    56 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 7ceba687-9a41-4e8f-a690-287c21a36ed3.cpp:56:19: error: ‘issame’ cannot be used a s a function    56 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,10 +1,54 @@
-#include <string>
+#include <algorithm>
 #include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string> b);
+
+vector<string> by_length(vector<int> arr) {
+    vector<int> sortedArr;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9)
+            sortedArr.push_back(num);
+    }
+    sort(sortedArr.begin(), sortedArr.end());
+    vector<string> result;
+    for (int num : sortedArr) {
+        string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
 
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
-}
-
-int main() {
-    assert(issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
 }
