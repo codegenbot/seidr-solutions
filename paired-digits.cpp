45e2f02e8b28a177aa4b4701a27628e596c58a86
beep boop <githubbot@vadim.me>
2024-07-20 09:29:34 +0200

    Solution 72 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/4wrdftbq Prompt: d08a2ce2-c070-4924-91be-7147ee7c6aac.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int pairedDigits(std::string s) {       |                       ^~~~~~ d08a2ce2-c070-4924-91be-7147ee7c6aac.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int pairedDigits(std::string s) {

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,11 +1,12 @@
+```c++
 #include <string>
 
 int pairedDigits(std::string s) {
     int sum = 0;
     while (s[0] == '0' && s.length() > 1) s.erase(0, 1);
     for (int i = 0; i < s.length() - 1; i++) {
-        if ((str[i] == str[i+1])) {
-            sum += (str[i] - '0');
+        if ((s[i] == '0') || (s[i] - '0' == s[i + 1] - '0')) {
+            sum += s[i] - '0';
         }
     }
     return sum;
