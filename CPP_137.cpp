e62fbdf835d04b13dfb1e3d6be69571e2394adfe
beep boop <githubbot@vadim.me>
2024-07-20 06:28:48 +0200

    Solution 50 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,31 @@
+#include <string>
 #include <boost/any.hpp>
+
+using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
+        return (int)a > (double)b ? a : b;
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return b;
+        return (double)a > (int)b ? a : b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (stod(boost::any_cast<string>(a).erase(0, 1).erase(stod(boost::any_cast<string>(b)).length(), boost::any_cast<string>(b).length())) >= stod(boost::any_cast<string>(b))) {
-            return a;
-        }
-        else {
-            return b;
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        if (str1.size() > 0 && str2.size() > 0) {
+            return str1 > str2 ? a : b;
         }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        if (stod(boost::any_cast<string>(a).erase(0, 1)) >= b.cast<double>()) {
-            return a;
-        }
-        else {
-            return boost::any("None");
-        }
+        string str = boost::any_cast<string>(a);
+        double d = boost::any_cast<double>(b);
+        return str + " vs " + to_string(d);
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        if (a.cast<double>() >= stod(boost::any_cast<string>(b).erase(0, 1))) {
-            return a;
-        }
-        else {
-            return boost::any("None");
-        }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int i = boost::any_cast<int>(a);
+        string s = boost::any_cast<string>(b);
+        return i > 0 ? a : b;
     }
-    else {
-        return boost::any("None");
-    }
+    return "None";
 }
