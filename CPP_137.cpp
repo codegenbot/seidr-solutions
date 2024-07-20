f7725da2b697d3ab0d987023be375a957b76021d
beep boop <githubbot@vadim.me>
2024-07-20 14:52:27 +0200

    Solution 70 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/4gclsr2g Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,21 +3,23 @@ if (a.type() == typeid(int) && b.type() == typeid(int)) {
         return a;
     } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
         return b;
+    } else {
+        return "None";
     }
 } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
     if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
         return a;
     } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
         return b;
+    } else {
+        return "None";
     }
 } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-    float fa = stof(boost::any_cast<string>(a));
-    float fb = stof(boost::any_cast<string>(b));
-    if (fa > fb) {
+    if (stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
         return a;
-    } else if (fa < fb) {
+    } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) {
         return b;
+    } else {
+        return "None";
     }
-}
-return "None";
 }
