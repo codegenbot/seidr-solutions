94636cd63eba67f6ed0095d538060fa7f109fabb
beep boop <githubbot@vadim.me>
2024-07-20 10:44:35 +0200

    Solution 42 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/gywzt605 Prompt:   700 |       operator=(_CharT __c)       |                 ~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h:717:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>&&) [wit h _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char> ]’ (near match)   717 |       operator=(basic_string&& __str)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:717:7: note:   passing ‘const string*’ { aka ‘const std::__cxx11::basic_string<char>*’} as ‘this’ argument discards quali fiers /usr/include/c++/11/bits/basic_string.h:785:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’   785 |       operator=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:785:42: note:   no known conversion for argument 1 from ‘std::__cxx11::basic_string<char>’ to ‘std::initializer_list<cha r>’   785 |       operator=(initializer_list<_CharT> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,3 +1,7 @@
+```
+#include <iostream>
+#include <string>
+
 std::string toCamelCase(const std::string& s) {
     std::string result;
     bool first = true;
@@ -18,7 +22,7 @@ std::string split(const std::string& s, const std::string& delimiter) {
     size_t pos = 0;
     std::string token;
 
-    while ((pos = s.find(delimiter)) != 0) {
+    while ((pos = s.find(delimiter)) != std::string::npos) {
         token = s.substr(0, pos);
         s = s.substr(pos + delimiter.size());
     }
