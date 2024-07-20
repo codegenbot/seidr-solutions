619a4cb03a490eae7bbfcff4307dd7182c7a4b0e
beep boop <githubbot@vadim.me>
2024-07-20 05:37:52 +0200

    Solution 63 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/97iwl66i Prompt: Complete the following code given the task description and function signature.

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,10 +1,10 @@
 int skjkasdkd(vector<int> lst){
     int maxPrime = 0;
-    for(int i : lst){
+    for(int i: lst){
         if(i > 1){
             bool isPrime = true;
-            for(int j = 2; j * j <= i; j++){
-                if(i % j == 0){
+            for(int j=2; j*j<=i; j++){
+                if(i%j == 0){
                     isPrime = false;
                     break;
                 }
