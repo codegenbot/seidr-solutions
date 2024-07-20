ed722422a0beeb2e596cbbeb95e3ee222d31a947
beep boop <githubbot@vadim.me>
2024-07-20 03:48:43 +0200

    Solution 3 of CPP/133, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/aluzswjj Prompt: 56718915-8907-4c24-a83e-65e7bd3eba30.cpp: In function ‘int sum_squares(std::vect or<float>)’: 56718915-8907-4c24-a83e-65e7bd3eba30.cpp:6:16: error: ‘ceil’ was not declared in  this scope     6 |         sum += ceil(num) * ceil(num);       |                ^~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,4 +1,7 @@
-int sum_squares(vector<float> lst){
+#include <vector>
+#include <cmath>
+
+int sum_squares(std::vector<float> lst){
     int sum = 0;
     for (float num : lst) {
         sum += ceil(num) * ceil(num);
