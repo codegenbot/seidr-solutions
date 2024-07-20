aa1ad3d91369164b59d6a8c1da746713e6e426f9
beep boop <githubbot@vadim.me>
2024-07-20 03:09:33 +0200

    Solution 27 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qlsied6b Prompt: 9a8838f1-2879-4b40-817d-20cd3418bcdd.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from 9a8838f1-2879-4b40-817d-20cd3418bcdd.cpp:11: 9a8838f1-2879-4b40-817d-20cd3418bcdd.cpp: In function ‘int main()’: 9a8838f1-2879-4b40-817d-20cd3418bcdd.cpp:13:13: error: ‘exchange’ was not declar ed in this scope    13 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,4 +1,3 @@
-#include <vector>
 #include <string>
 
 string exchange(vector<int> lst1, vector<int> lst2) {
@@ -8,5 +7,5 @@ string exchange(vector<int> lst1, vector<int> lst2) {
             oddCount++;
         }
     }
-    return to_string(oddCount == 0) + "YES";
+    return to_string(oddCount == 0);
 }
