6478c3d5d07eaa0aff18a988e49a908c49550542
beep boop <githubbot@vadim.me>
2024-07-20 07:41:12 +0200

    Solution 36 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ko1ld40j Prompt: 99cba5e4-4006-4ad4-9994-033da8888f81.cpp:9:29: error: invalid types ‘const int[i nt]’ for array subscript     9 |         if (code[i] == guess[i]) {       |                             ^ 99cba5e4-4006-4ad4-9994-033da8888f81.cpp:11:32: error: request for member ‘begin ’ in ‘guess’, which is of non-class type ‘const int’    11 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                ^~~~~ 99cba5e4-4006-4ad4-9994-033da8888f81.cpp:11:47: error: request for member ‘end’ in ‘guess’, which is of non-class type ‘const int’    11 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                               ^~~ 99cba5e4-4006-4ad4-9994-033da8888f81.cpp:11:58: error: invalid types ‘const int[ int]’ for array subscript    11 |         } else if (count(guess.begin(), guess.end(), code[i]) > 0) {       |                                                          ^ 99cba5e4-4006-4ad4-9994-033da8888f81.cpp:16:30: error: expected ‘}’ at end of in put    16 |     return black + white - 2;       |                              ^ 99cba5e4-4006-4ad4-9994-033da8888f81.cpp:4:57: note: to match this ‘{’     4 | int mastermind(const string& code, const string& guess) {       |                                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,17 +1,14 @@
-#include <string>
-#include <algorithm>
-
-int mastermind(string code, string guess) {
-    int black = 0;
+int mastermind(const string& code, const string& guess) {
     int white = 0;
+    int black = 0;
 
     for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            black++;
+        if (code.substr(i, 1) == guess.substr(i, 1)) {
+            ++black;
         } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {
-            white++;
+            ++white;
         }
     }
 
-    return black << 1 | white;
+    return black + white;
 }
