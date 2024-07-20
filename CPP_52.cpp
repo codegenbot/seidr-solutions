b54b4a2f66aaa70392a980ba7837b6668bc5c9bb
beep boop <githubbot@vadim.me>
2024-07-20 05:28:56 +0200

    Solution 4 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ftt0b7y7 Prompt: Complete the following code given the task description and function signature.

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,7 +1,6 @@
-for (int num : l) {
-        if (num >= t) {
-            return false;
-        }
-    }
-    return true;
+for(int num : l){
+  if(num >= t){
+    return false;
+  }
 }
+return true;
