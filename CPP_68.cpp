7d43af47bfd8586bbe96aa240ff46fe876a6ad21
beep boop <githubbot@vadim.me>
2024-07-20 02:36:03 +0200

    Solution 68 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/mgon0lw3 Prompt:       |                    ^~~ 62692b14-17e4-4960-8fe8-f979a50a4694.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 62692b14-17e4-4960-8fe8-f979a50a4694.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 62692b14-17e4-4960-8fe8-f979a50a4694.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 62692b14-17e4-4960-8fe8-f979a50a4694.cpp:20: 62692b14-17e4-4960-8fe8-f979a50a4694.cpp: In function ‘int main()’: 62692b14-17e4-4960-8fe8-f979a50a4694.cpp:30:20: error: ‘pluck’ was not declared in this scope    30 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 62692b14-17e4-4960-8fe8-f979a50a4694.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -4,10 +4,11 @@
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     // Your logic to check if vectors are same
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<std::pair<int, int>> pluck(std::vector<int> arr) {
-    std::vector<std::pair<int, int>> evenNodes;
+std::vector<int> pluck(std::vector<int> arr) {
+    vector<pair<int, int>> evenNodes;
 
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] % 2 == 0) {
@@ -18,7 +19,12 @@ std::vector<std::pair<int, int>> pluck(std::vector<int> arr) {
     if (evenNodes.empty()) {
         return {};
     } else {
-        std::sort(evenNodes.begin(), evenNodes.end());
-        return {evenNodes[0]};
+        sort(evenNodes.begin(), evenNodes.end());
+        return {{evenNodes[0].first, evenNodes[0].second}};
     }
+}
+
+int main() {
+    assert(issame(pluck({7, 9, 7, 1}), {}));
+    return 0;
 }
