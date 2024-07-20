7f91ad3d76028cd1306782c836ebb43a632cb359
beep boop <githubbot@vadim.me>
2024-07-20 02:21:32 +0200

    Solution 32 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9z0vt8xn Prompt:       |                    ^~~ 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp:17: 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp: In function ‘int main()’: 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8820a6d1-24c2-4924-b677-508fabdce9b3.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,18 +1,18 @@
 #include <vector>
 #include <set>
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> s1(l1.begin(), l1.end());
-    std::set<int> s2(l2.begin(), l2.end());
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
 
-    std::set<int> result;
+    set<int> result;
     for (auto x : s1) {
         if (s2.count(x)) {
             result.insert(x);
         }
     }
 
-    std::vector<int> v(result.begin(), result.end());
+    vector<int> v(result.begin(), result.end());
     sort(v.begin(), v.end());
     return v;
 }
