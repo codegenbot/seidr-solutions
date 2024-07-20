6d9cd594f906d28c94f5497bd3e8ac6a28ad1bb4
beep boop <githubbot@vadim.me>
2024-07-20 08:16:54 +0200

    Solution 11 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/y0m20dx3 Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,36 +1,72 @@
-int mastermind(string code, string guess) {
-    int white = 0;
-    int black = 0;
+#include <vector>
+using namespace std;
 
-    // Count the number of each color in the code and guess
-    map<char, int> codeColorCount, guessColorCount;
-    for (char c : code) {
-        codeColorCount[c]++;
-    }
-    for (char c : guess) {
-        guessColorCount[c]++;
-    }
-
-    // Calculate the number of white pegs
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            black++;
-        } else {
-            codeColorCount[code[i]]--;
-            guessColorCount[guess[i]]--;
-            white += min(codeColorCount[code[i]], guessColorCount[guess[i]]);
+int whitePegs(string code, string guess) {
+    int count = 0;
+    for(int i = 0; i<4; ++i){
+        if(code[i] == guess[i]){
+            // Correct place and color. Count as black peg.
+            return -1;
+        }
+        else{
+            // Check the remaining 3 characters of code at correct position in guess
+            int j=0;
+            while(j<3 && guess[j+i+1]!=code[i])
+                j++;
+            if(j<3)
+                count++; 
         }
     }
+    return count;
+}
 
-    // Calculate the number of black pegs
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            continue;
-        } else {
-            codeColorCount[code[i]]--;
-            white += min(codeColorCount[code[i]], 1);
+int blackPegs(string code, string guess) {
+    int count = 0;
+    for(int i = 0; i<4; ++i){
+        if(code[i] == guess[i]){
+            // Correct place and color. Count as black peg.
+            count++;
         }
     }
+    return count;
+}
 
-    return make_pair(white, black).second;
+int main() {
+    // Test cases
+    string code1 = "RRRR";
+    string guess1 = "RRRR";
+    int result1[] = {0, 4};
+    if ((whitePegs(code1, guess1) != result1[0]) || (blackPegs(code1, guess1) != result1[1])) {
+        cout << "Test case failed!" << endl;
+    }
+
+    string code2 = "BOYG";
+    string guess2 = "GYOB";
+    int result2[] = {4, 0};
+    if ((whitePegs(code2, guess2) != result2[0]) || (blackPegs(code2, guess2) != result2[1])) {
+        cout << "Test case failed!" << endl;
+    }
+
+    string code3 = "WYYW";
+    string guess3 = "BBOG";
+    int result3[] = {0, 0};
+    if ((whitePegs(code3, guess3) != result3[0]) || (blackPegs(code3, guess3) != result3[1])) {
+        cout << "Test case failed!" << endl;
+    }
+
+    string code4 = "GGGB";
+    string guess4 = "BGGG";
+    int result4[] = {2, 2};
+    if ((whitePegs(code4, guess4) != result4[0]) || (blackPegs(code4, guess4) != result4[1])) {
+        cout << "Test case failed!" << endl;
+    }
+
+    string code5 = "BBBB";
+    string guess5 = "OOOO";
+    int result5[] = {0, 0};
+    if ((whitePegs(code5, guess5) != result5[0]) || (blackPegs(code5, guess5) != result5[1])) {
+        cout << "Test case failed!" << endl;
+    }
+
+    return 0;
 }
