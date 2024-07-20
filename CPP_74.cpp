ee69f128290089ebf4ba1911fc2ddd3ad873f6ae
beep boop <githubbot@vadim.me>
2024-07-20 02:33:57 +0200

    Solution 7 of CPP/74, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/vtopgtvr Prompt: Complete the following code given the task description and function signature.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,3 +1,8 @@
+#include<stdio.h>
+#include<vector>
+#include<string>
+using namespace std;
+
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
@@ -7,6 +12,7 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     for (const string& s : lst2) {
         sum2 += s.length();
     }
+
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
     else return lst1;
