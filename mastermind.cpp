7accca386f6862816f95c3796678890173840aec
beep boop <githubbot@vadim.me>
2024-07-20 08:44:17 +0200

    Solution 51 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/y0m20dx3 Prompt: 94065564-9ba5-412e-9bb2-04bc809edf93.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 94065564-9ba5-412e-9bb2-04bc809edf93.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 94065564-9ba5-412e-9bb2-04bc809edf93.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -6,26 +6,17 @@ int mastermind(string code, string guess) {
     int codeCount[6] = {0};
     int guessCount[6] = {0};
 
-    // Count the occurrences of each character in the code and guess
-    for(int i=0; i<4; i++) {
-        codeCount[code[i]-'0']++;
-        guessCount[guess[i]-'0']++;
-    }
-
-    // Calculate the number of black pegs (correct color, correct place)
     for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
             black++;
-            codeCount[code[i]-'0']--;
-            guessCount[guess[i]-'0']--;
+        } else {
+            codeCount[code[i] - '0']++;
+            guessCount[guess[i] - '0']++;
         }
     }
 
-    // Calculate the number of white pegs (correct color, wrong place)
     for(int i=0; i<6; i++) {
-        if(codeCount[i] > 0 && guessCount[i] > 0) {
-            white += min(codeCount[i], guessCount[i]);
-        }
+        white += min(codeCount[i], guessCount[i]);
     }
 
     return black + white;
