04d8589f27713b964af5b6c78f28e49271225903
beep boop <githubbot@vadim.me>
2024-07-20 06:23:50 +0200

    Solution 26 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4uhrwgn Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,6 @@
+Here is the completed code:
+
+```cpp
 #include <boost/any.hpp>
 #include <string>
 #include <algorithm>
@@ -6,21 +9,39 @@ using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return max((float)a.convert_to<float>(), stof(b.convert_to<string>().c_str()));
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (stof(a.convert_to<string>().c_str()) > stof(b.convert_to<string>().c_str())) ? a : b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return (int)a.convert_to<int>() > (int)b.convert_to<int>() ? a : b;
-    }
-    else if ((int)a.convert_to<int>() == (int)b.convert_to<int>()) {
-        return boost::any("None");
-    }
-    else {
-        return a > b ? a : b;
+        return boost::any_cast<float>(b);
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = boost::any_cast<string>(b);
+        int num;
+        size_t pos = 0;
+        while ((pos = str.find_first_of(",.")) != string::npos) {
+            str.replace(pos, 1, "");
+        }
+        if (str.length() > 0) {
+            num = stoi(str);
+            return a < num ? b : a;
+        } else {
+            return "None";
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        int num1, num2;
+        size_t pos1 = 0, pos2 = 0;
+        while ((pos1 = str1.find_first_of(",.")) != string::npos) {
+            str1.replace(pos1, 1, "");
+        }
+        while ((pos2 = str2.find_first_of(",.")) != string::npos) {
+            str2.replace(pos2, 1, "");
+        }
+        if (str1.length() > 0 && str2.length() > 0) {
+            num1 = stoi(str1);
+            num2 = stoi(str2);
+            return boost::any(a < b ? a : b);
+        } else {
+            return "None";
+        }
+    } else {
+        return "None";
     }
 }
