e67d962f36046ef96cec81602144752632c68688
beep boop <githubbot@vadim.me>
2024-07-20 03:33:39 +0200

    Solution 80 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bnfqx7vn Prompt:       |                    ^~~ e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp:9: e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp: In function ‘int main()’: e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ e43c5cbe-1244-4fc6-8ca6-eb1a1282e250.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,21 +1,16 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) return false;
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i]) return false;
     }
     return true;
 }
 
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::vector<int> result(arr.begin(), arr.begin() + k);
+vector<int> maximum(vector<int> arr, int k) {
+    vector<int> result(arr.begin(), arr.begin() + k);
     sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 3) , {0, 1, 2}));
-    return 0;
 }
