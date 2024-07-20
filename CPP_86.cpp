ebea1a4b7d7577bdee0a1028675ac50533d69121
beep boop <githubbot@vadim.me>
2024-07-20 02:41:12 +0200

    Solution 0 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/b6ybs26i Prompt: Complete the following code given the task description and function signature.

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,24 +1,17 @@
-#include<string>
-#include<algorithm>
-using namespace std;
-
-string anti_shuffle(string s) {
+string anti_shuffle(string s){
     string result = "";
-    size_t pos = 0;
-    
-    while (pos < s.length()) {
-        size_t nextSpace = s.find(' ', pos);
-        
-        if (nextSpace == string::npos) {
-            result += s.substr(pos);
-            break;
+    for(int i=0; i<s.length(); i++){
+        if(s[i] == ' '){
+            result += " ";
+        } else {
+            string temp = "";
+            for(int j=i; j<s.length() && s[j] != ' '; j++){
+                temp += s[j];
+                i = j;
+            }
+            sort(temp.begin(), temp.end());
+            result += temp;
         }
-        
-        string word = s.substr(pos, nextSpace - pos);
-        sort(word.begin(), word.end());
-        result += word + " ";
-        pos = nextSpace + 1;
     }
-    
     return result;
 }
