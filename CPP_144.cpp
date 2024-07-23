8699948588ce5d889d20210d9e6fc9fda5d869e9
beep boop <githubbot@vadim.me>
2024-07-23 17:56:06 +0200

    Solution 2 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts52pt24 Prompt: Program failed

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,22 +1,27 @@
-#include <string>
+#include<string>
 using namespace std;
 
 bool simplify(string x, string n) {
-    int numerator1 = 0, denominator1 = 0;
-    int numerator2 = 0, denominator2 = 0;
-
-    // Convert strings to integers
+    int a = 1, b = 1, c = 1, d = 1;
+    
+    // Split the fractions into numerator and denominator
     size_t pos = x.find('/');
-    numerator1 = stoi(x.substr(0, pos));
-    denominator1 = stoi(x.substr(pos + 1));
+    a = stoi(x.substr(0, pos));
+    b = stoi(x.substr(pos + 1));
 
-    pos = n.find('/');
-    numerator2 = stoi(n.substr(0, pos));
-    denominator2 = stoi(n.substr(pos + 1));
+    pos = n.find '/';
+    c = stoi(n.substr(0, pos));
+    d = stoi(n.substr(pos + 1));
 
-    // Check if the product is a whole number
-    int productNumerator = numerator1 * numerator2;
-    int productDenominator = denominator1 * denominator2;
+    // Check if the product is an integer
+    return a / gcd(b, a) == c / gcd(d, c);
+}
 
-    return productNumerator % productDenominator == 0;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
