12ab306169f203e28c2dfcc0d49d944da8debc20
beep boop <githubbot@vadim.me>
2024-07-23 07:00:18 +0200

    Solution 2 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -9,28 +9,17 @@ boost::any compare_one(boost::any a, boost::any b) {
         return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return (b.convert_to<string>() > to_string(a.convert_to<int())) ? b : "None";
+        return (string)b;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = b.convert_to<string>();
-        if (str.find('.') != string::npos || str.find(',') != string::npos)
-            return a;
-        else
-            return (a.convert_to<float>() > stod(str)) ? a : "None";
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        return (b.convert_to<int>() > stoi(a.convert_to<string()))) ? a : "None";
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string str = a.convert_to<string>();
-        if (str.find('.') != string::npos || str.find(',') != string::npos)
-            return b;
-        else
-            return (b.convert_to<float>() > stod(str)) ? b : "None";
+        string str = (string)a;
+        float aFloat = stof(str);
+        return max(aFloat, stof((string)b));
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (b.convert_to<string>() > a.convert_to<string>()) ? b : "None";
+        return (max((string)a, (string)b) == (string)a ? "None" : (string)b);
     }
-    else
+    else {
         return "None";
+    }
 }
