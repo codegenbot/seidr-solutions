dac731b5ecb359a778033bb76294bc0a506bbc33
beep boop <githubbot@vadim.me>
2024-07-23 14:12:00 +0200

    Solution 89 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4d2aq60w Prompt: In file included from /usr/include/c++/11/string:39,                  from b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp:55:44: error: ‘b’ was not declared in t his scope    55 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp:54: b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp: In function ‘int main()’: b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp:64:20: error: ‘total_match’ was not dec lared in this scope    64 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ b22d0b60-c79e-461d-a3dc-159bf26ae59e.cpp:64:19: error: ‘issame’ cannot be used a s a function    64 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,17 +1,21 @@
+```cpp
 #include <string>
 #include <vector>
 
-bool issame(vector<string> a, vector<string> bstr) {
+bool issame(std::vector<std::string> a, std::vector<std::string> bstr) {
     transform(a.begin(), a.end(), a.begin(), ::tolower);
     transform(bstr.begin(), bstr.end(), bstr.begin(), ::tolower);
 
     sort(a.begin(), a.end());
     sort(bstr.begin(), bstr.end());
 
-    int i = 0;
-    for (int j = 0; i < a.size() && j < bstr.size(); ++i, ++j) {
+    int i = 0, j = 0;
+    
+    while (i < a.size() && j < bstr.size()) {
         if (a[i] == bstr[j]) {
-            return true;
+            return true; 
+            i++;
+            j++;
         } else if (a[i] < bstr[j]) {
             i++;
         } else {
@@ -22,8 +26,8 @@ bool issame(vector<string> a, vector<string> bstr) {
     return false;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    vector<string> result;
+std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+    std::vector<std::string> result;
 
     transform(lst1.begin(), lst1.end(), lst1.begin(), ::tolower);
     transform(lst2.begin(), lst2.end(), lst2.begin(), ::toupper);
@@ -31,13 +35,14 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     sort(lst1.begin(), lst1.end());
     sort(lst2.begin(), lst2.end());
 
-    int i = 0;
-    for (int j = 0; i < lst1.size() && j < lst2.size(); ++i, ++j) {
-        if (lst1[i] == lst2[j]) {
-            result.push_back(string(&lst1[i][0])); 
+    int i = 0, j = 0;
+    
+    while (i < lst1.size() && j < lst2.size()) {
+        if (lst1[i] == std::string(&lst2[j][0]).substr(0)).toupper())) {
+            result.push_back(std::string(&lst1[i][0])); 
             i++;
             j++;
-        } else if (lst1[i] < lst2[j]) {
+        } else if (lst1[i] < std::string(&lst2[j][0]).substr(0)).toUpperCase()) {
             i++;
         } else {
             j++;
