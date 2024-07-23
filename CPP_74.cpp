9992bee0efde3b3c9f36ac23e768e082a7b28b05
beep boop <githubbot@vadim.me>
2024-07-24 00:02:04 +0200

    Solution 34 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jao9uj68 Prompt:       |                              ^~~~~~ e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp:21: e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp: In function ‘int main()’: e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp:31:20: error: ‘total_match’ was not dec lared in this scope    31 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ e33ed4c9-358a-40b1-9878-988cbd9a3e7f.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,25 +1,20 @@
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
-    int sum1 = 0, sum2 = 0;
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    vector<string> result;
 
-    for (const std::string& s : lst1) {
-        sum1 += s.length();
+    for (const string& s : lst1) {
+        sum += s.length();
     }
-    for (const std::string& s : lst2) {
-        sum2 += s.length();
+    for (const string& s : lst2) {
+        sum += s.length();
     }
 
-    if (sum1 < sum2) {
-        return issame(lst1, vector<string>(), vector<string>()) ? lst1 : lst2;
-    } else if (sum1 > sum2) {
-        return issame(vector<string>(), vector<string>(), lst2) ? lst2 : lst1;
+    if (!issame(lst1, lst2)) {
+        return issame({lst1}, {lst2}, {}) ? lst1 : lst2;
     } else {
-        return issame(lst1, vector<string>(), vector<string>()) ? lst1 : lst2;
+        return lst1;
     }
 }
