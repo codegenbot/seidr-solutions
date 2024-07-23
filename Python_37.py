b0a330dc0c91ad9b7db539b840317a27c09b4f25
beep boop <githubbot@vadim.me>
2024-07-23 10:19:37 +0200

    Solution 68 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5fq9li0d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6755dd0b-7db3-4c3b-b1f9-b5ca246b2 01e.py", line 2, in <module>     result = check(your_list)              ^^^^^ NameError: name 'check' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,9 @@
 def check(lst):
-    return [x for x in lst if x % 2 != 0]
+    even_count = 0
+    for num in lst:
+        if num % 2 == 0:
+            even_count += 1
+    return even_count
 
 your_list = [1, 2, 3, 4]
 result = check(your_list)
