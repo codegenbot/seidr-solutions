d7701b2be0304fb4efcd54a6f56edfb8ff0f9ef9
beep boop <githubbot@vadim.me>
2024-07-23 11:21:11 +0200

    Solution 41 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/dto1a8ig Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b253ee80-54b1-46a7-8b3e-7706b0ac2 b42.py", line 1     ```     ^ SyntaxError: invalid syntax

--- snow-day.py
+++ snow-day.py
@@ -1,3 +1,5 @@
 def snow_day(hours, snow_on_ground, rate_of_snow_fall, proportion_melting_per_hour):
     snow_on_ground = snow_on_ground * (1 - proportion_melting_per_hour) + rate_of_snow_fall
+    for _ in range(hours-1):
+        snow_on_ground = snow_on_ground * (1 - proportion_melting_per_hour) + rate_of_snow_fall
     return snow_on_ground
