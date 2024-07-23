5b2beb907e9c9553b64f85a1356c5ab43c4508db
beep boop <githubbot@vadim.me>
2024-07-23 13:42:36 +0200

    Solution 74 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/24fbb5a1-837e-44b5-8486-5b7e9f236 6b7.py", line 1     ```print(bowling_score('9-2/38/X3/7/5/4/6/22/3/2/5'))     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,88 +1,12 @@
-```
 def bowling_score(game):
     score = 0
-    game_list = [int(i) if i.isdigit() else i for i in game.replace('/', '-').split()]
-    frame = 0
-    while len(game_list) > 1:
-        if game_list[0] == 'X':
-            score += 10
-            if game_list[1] == 'X':
-                score += 10
-                game_list.pop(0)
-                game_list.pop(0)
-            elif game_list[1] in ['/']:
-                score += 10 - int(game_list[1][1])
-                while game_list[1] in ['/'] or game_list[1].isdigit():
-                    if game_list[1] == 'X':
-                        score += 10
-                        game_list.pop(0)
-                    elif game_list[1] in ['/']:
-                        score += 10 - int(game_list[1][1])
-                        game_list.pop(0)
-                    else:
-                        score += sum(map(int, game_list[:2]))
-                        game_list = game_list[2:]
-                game_list.pop(0)
-            else:
-                frame_score = 10 + sum(map(int, game_list[1:3]))
-                game_list = game_list[3:]
-        elif game_list[0] in ['/']:
-            if game_list[1] == 'X':
-                score += 10 - int(game_list[0][1])
-                while game_list[1] in ['/'] or game_list[1].isdigit():
-                    if game_list[1] == 'X':
-                        score += 10
-                        game_list.pop(0)
-                    elif game_list[1] in ['/']:
-                        score += 10 - int(game_list[1][1])
-                        game_list.pop(0)
-                    else:
-                        score += sum(map(int, game_list[:2]))
-                        game_list = game_list[2:]
-                game_list.pop(0)
-                game_list.pop(0)
-            else:
-                frame_score = 10 - int(game_list[0][1]) + sum(map(int, game_list[1:3]))
-                while game_list[1] in ['/'] or game_list[1].isdigit():
-                    if game_list[1] == 'X':
-                        score += 10
-                        game_list.pop(0)
-                    elif game_list[1] in ['/']:
-                        score += 10 - int(game_list[1][1])
-                        game_list.pop(0)
-                    else:
-                        score += sum(map(int, game_list[:2]))
-                        game_list = game_list[2:]
-                game_list = game_list[3:]
+    roll = game.split('/')
+    for i in range(0, len(roll), 2):
+        if roll[i].startswith('X'):
+            score += 10 + int(roll[i+1])
+        elif '-' not in roll[i]:
+            score += int(roll[i]) + int(roll[i+1])
         else:
-            if len(game_list) >= 2 and sum(map(int, game_list[:2])) == 10:
-                score += 10
-                while game_list[1] in ['/'] or game_list[1].isdigit():
-                    if game_list[1] == 'X':
-                        score += 10
-                        game_list.pop(0)
-                    elif game_list[1] in ['/']:
-                        score += 10 - int(game_list[1][1])
-                        game_list.pop(0)
-                    else:
-                        score += sum(map(int, game_list[:2]))
-                        game_list = game_list[2:]
-                game_list = game_list[2:]
-            else:
-                frame_score = sum(map(int, game_list[:2]))
-                while game_list[1] in ['/'] or game_list[1].isdigit():
-                    if game_list[1] == 'X':
-                        score += 10
-                        game_list.pop(0)
-                    elif game_list[1] in ['/']:
-                        score += 10 - int(game_list[1][1])
-                        game_list.pop(0)
-                    else:
-                        score += sum(map(int, game_list[:2]))
-                        game_list = game_list[2:]
-                game_list = game_list[2:]
-        score += frame_score
-    if len(game_list) > 0:
-        for i in range(len(game_list)):
-            score += int(game_list[i])
+            split_roll = map(int, roll[i].split('-'))
+            score += sum(split_roll) + int(roll[i+1])
     return score
