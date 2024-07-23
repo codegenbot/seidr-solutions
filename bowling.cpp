7cb82871eb955b863ef00b06711b7855f09ecafc
beep boop <githubbot@vadim.me>
2024-07-23 19:34:47 +0200

    Solution 11 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/bteeah24 Prompt: 41bb8273-1652-44e0-8a24-c7887c410a8a.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,54 +1,40 @@
+```c++
 #include <iostream>
 #include <string>
 
-using namespace std;
-
-int bowlingScore(string s) {
+int bowlingScore(std::string s) {
     int score = 0;
+    int roll1 = 0; 
+    int roll2 = 0; 
+    bool isStrike = false;
+
     for (int i = 0; i < s.length(); i++) {
         char c = s[i];
         if (c == 'X') {
             score += 30;
+            isStrike = true;
         } else if (c == '/') {
-            score += 10 + bowlingScoreHelper(s, i+1);
-        } else {
-            int points = c - '0';
-            if (i+2 < s.length() && s[i+2] != '/') {
-                points += s[i+1] - '0';
-                if (s[i+1] == '/') {
-                    score += 10 + (points*10);
-                } else {
-                    if (points+10 > 10) {
-                        score += 10;
-                    } else {
-                        score += points+10;
-                    }
-                }
+            if (isStrike) {
+                score += 10 + roll1 * 2 + roll2;
+                isStrike = false;
             } else {
-                score += points * 10;
+                score += 10 + roll1 + roll2;
+                roll1 = 0; 
+                roll2 = 0;
+            }
+        } else {
+            int temp = c - '0';
+            if (!isStrike) {
+                roll1 += temp;
+                if (roll1 < 2) {
+                    roll2 = 0; 
+                }
+            } else { 
+                score += 30;
+                roll2 = 0; 
             }
         }
     }
-    return score;
-}
 
-int bowlingScoreHelper(string s, int i) {
-    int score = 0;
-    while (i < s.length() && s[i] != '/') {
-        if (s[i] == 'X') {
-            score += 30;
-        } else {
-            int points = s[i] - '0';
-            score += points * 10;
-        }
-        i++;
-    }
     return score;
-}
-
-int main() {
-    string input = "XXXX/0/00"; 
-    int score = bowlingScore(input);
-    cout << "The score is: " << score << endl;
-    return 0;
 }
