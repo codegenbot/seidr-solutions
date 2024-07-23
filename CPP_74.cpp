1f1cda45dd6cba5c51f0a430ab6632ffd751589e
beep boop <githubbot@vadim.me>
2024-07-23 13:31:27 +0200

    Solution 69 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4d2aq60w Prompt:       |                              ^~~~~~ 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp:28: 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp: In function ‘int main()’: 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp:38:20: error: ‘total_match’ was not dec lared in this scope    38 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 4c4e7473-b7da-467f-8181-451c89f88eaa.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,28 +1,28 @@
+```cpp
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cctype>
 #include <string>
 
-using namespace std;
-
-bool issame(const string& s1, const string& s2) {
-    return s1.size() == s2.size();
+bool issame(char a, char b) {
+    return std::tolower(a) == std::tolower(b);
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    vector<string> result;
-    
-    transform(lst1.begin(), lst1.end(), lst1.begin(), ::tolower);
-    transform(lst2.begin(), lst2.end(), lst2.begin(), ::toupper);
+std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
+    std::vector<std::string> result;
 
-    sort(lst1.begin(), lst1.end());
-    sort(lst2.begin(), lst2.end());
+    std::transform(lst1.begin(), lst1.end(), lst1.begin(), ::tolower);
+    std::transform(lst2.begin(), lst2.end(), lst2.begin(), ::toupper);
+
+    std::sort(lst1.begin(), lst1.end());
+    std::sort(lst2.begin(), lst2.end());
 
     int i = 0, j = 0;
-    
+
     while (i < lst1.size() && j < lst2.size()) {
-        if (issame(lst1[i], lst2[j])) {
-            result.push_back(lst1[i]);
+        if (issame(lst1[i][0], lst2[j][0])) {
+            result.push_back(string(lst1[i])); 
             i++;
             j++;
         } else if (lst1[i] < lst2[j]) {
@@ -33,4 +33,15 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     }
 
     return result;
+}
+
+int main() {
+    std::vector<std::string> lst1 = {"cat", "dog", "bird"};
+    std::vector<std::string> lst2 = {"CAT", "DOG", "BIRD"};
+
+    for (const auto& s : total_match(lst1, lst2)) {
+        std::cout << s << '\n';
+    }
+
+    return 0;
 }
