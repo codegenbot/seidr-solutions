2b157aa80e49b58a0cbf502f994ee6ee5cd14484
beep boop <githubbot@vadim.me>
2024-07-23 21:47:40 +0200

    Solution 10 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8dwusk5j Prompt: 5eeba194-9521-4eac-97d9-73925a472488.cpp:11:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 5eeba194-9521-4eac-97d9-73925a472488.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5eeba194-9521-4eac-97d9-73925a472488.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5eeba194-9521-4eac-97d9-73925a472488.cpp:10: 5eeba194-9521-4eac-97d9-73925a472488.cpp: In function ‘int main()’: 5eeba194-9521-4eac-97d9-73925a472488.cpp:20:20: error: ‘unique’ was not declared  in this scope    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 5eeba194-9521-4eac-97d9-73925a472488.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -5,21 +5,10 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-vector<int> unique(vector<int> l) {
-    set<int> s(l.begin(), l.end());
-    vector<int> res(s.begin(), s.end());
-    return res;
-}
-
 int main() {
-    vector<int> v = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    if (issame(v, unique(v))) {
-        for (int i : v) {
-            cout << i << " ";
-        }
-        cout << endl;
-    } else {
-        cout << "Vectors are not same" << endl;
+    vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    if (!issame({},{0, 2, 3, 5, 9, 123},l)) {
+        l = unique(l);
     }
     return 0;
 }
