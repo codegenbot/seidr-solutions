5988ff8e94bae9eecf34b6ec471ee345de4ab4a0
beep boop <githubbot@vadim.me>
2024-07-23 13:16:15 +0200

    Solution 36 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76992eca-8a6c-4048-9b3b-4b0c96162 660.py", line 39, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/76992eca-8a6c-4048-9b3b-4b0c96162 660.py", line 9, in check     sum_squares( TypeError: sum_squares() takes 0 positional arguments but 1 was given

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,5 @@
-def sum_squares(user_input):
+```
+def sum_squares():
+    user_input = input("Enter numbers separated by space : ")
     numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split())))
     print(sum([n ** 2 for n in numbers]))
