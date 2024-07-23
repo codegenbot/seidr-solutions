15d57c004c4422ae3264f0029a5029e3df33d0f9
beep boop <githubbot@vadim.me>
2024-07-24 01:48:35 +0200

    Solution 45 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf7094f8-9e33-4c44-a5ba-914ac0bb2 a3e.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf7094f8-9e33-4c44-a5ba-914ac0bb2 a3e.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,10 +1,5 @@
-```
 def sort_even(l: list):
-    evens = sorted([x for x in l if x % 2 == 0])
-    odds = sorted([x for x in l if x % 2 != 0])
-    
-    max_len = max(len(evens), len(odds))
-    evens += [None] * (max_len - len(evens))
-    odds += [None] * (max_len - len(odds))
-    
-    return [(i, o) for i, o in zip(evens, odds) if o is not None]
+    l.sort()
+    evens = [x for x in l if x % 2 == 0]
+    odds = [x for x in l if x % 2 != 0]
+    return [(i, o) for i, o in zip(evens, odds)]
