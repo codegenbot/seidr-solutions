77f13d1ecd0bd0a4f6b96380de0094859885642c
beep boop <githubbot@vadim.me>
2024-07-23 16:17:16 +0200

    Solution 82 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s3w5awhe Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65b426b2-79d6-4ff8-96e4-2d0fb27fe 696.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_3.py
+++ Python_3.py
@@ -1,10 +1,14 @@
 ```
+from typing import List
+
+
 operations = list(map(int, input("Enter space-separated integers: ").split()))
-print(below_zero(operations))
-def below_zero(ops):
+def below_zero(ops: List[int]) -> bool:
     balance = 0
     for op in ops:
         balance += op
         if balance < 0:
             return True
     return False
+
+print(below_zero(operations))
