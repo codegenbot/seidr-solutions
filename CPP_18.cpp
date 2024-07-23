a17899c35b536b8ebe0094df0dd163460fc45016
beep boop <githubbot@vadim.me>
2024-07-23 08:49:19 +0200

    Solution 25 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/qzd19lh8 Prompt: 4744c7fd-90cc-4443-a86d-a215211e85f3.cpp: In function ‘int main()’: 4744c7fd-90cc-4443-a86d-a215211e85f3.cpp:15:5: error: ‘assert’ was not declared in this scope    15 |     assert (how_many_times("john doe", "john") == 1);       |     ^~~~~~ 4744c7fd-90cc-4443-a86d-a215211e85f3.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <iostream>   +++ |+#include <cassert>     3 | 4744c7fd-90cc-4443-a86d-a215211e85f3.cpp: At global scope: 4744c7fd-90cc-4443-a86d-a215211e85f3.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 4744c7fd-90cc-4443-a86d-a215211e85f3.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -5,7 +5,7 @@
 int how_many_times(std::string str, std::string substring) {
     int count = 0;
     size_t pos = 0;
-    while ((pos = str.find(substring, pos)) != std::string::npos) {
+    while ((pos = str.find(substring, pos)) != stdstring::npos) {
         ++count;
         pos += substring.size();
     }
