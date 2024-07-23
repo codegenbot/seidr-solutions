55e77bdd6ade7992ea7eb763ea0d15b65918d00c
beep boop <githubbot@vadim.me>
2024-07-23 12:28:49 +0200

    Solution 57 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/47b4aaef-f82f-4470-b7d0-d282baca4 5cc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,18 @@
 s = input("Enter a sentence: ")
 n = int(input("Enter a number: "))
-n -= sum(int(d) for d in ''.join(filter(str.isdigit, s)))
-n += sum(int(d) for d in ''.join(filter(str.isdigit, [c for c in s if c != ' ' and (s[s.index(c)+1:] == 'of' or s[s.index(c)+1:] == 'on')])) - 2
+n -= sum(int(d) for d in "".join(filter(str.isdigit, s)))
+n -= sum(
+    int(d)
+    for d in "".join(
+        filter(
+            str.isdigit,
+            [
+                c
+                for c in s
+                if c != " " and c not in "oa" and c.lower() not in ["of", "on"]
+            ],
+        )
+    )
+)
+
+print(n)
