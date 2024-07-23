61f7d2b86f75c0debd473ae44cf2361709d8d6ff
beep boop <githubbot@vadim.me>
2024-07-23 13:27:26 +0200

    Solution 73 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt: it must return 114 for input 9/-39/X3/7/54622325, but it returns .

--- bowling.py
+++ bowling.py
@@ -11,44 +11,75 @@ def bowling_score(game):
                 game_list.pop(0)
                 game_list.pop(0)
             elif game_list[1] in ['/']:
-                frame_score = 10 - int(game_list[1][1])
-                game_list.pop(0)
-                while game_list and (game_list[0] == 'X' or not str.isdigit(game_list[0])):
-                    if game_list[0] == 'X':
+                score += 10 - int(game_list[1][1])
+                while game_list[1] in ['/'] or game_list[1].isdigit():
+                    if game_list[1] == 'X':
                         score += 10
                         game_list.pop(0)
+                    elif game_list[1] in ['/']:
+                        score += 10 - int(game_list[1][1])
+                        game_list.pop(0)
                     else:
-                        frame_score += int(game_list.pop(0))
-                score += frame_score
+                        score += sum(map(int, game_list[:2]))
+                        game_list = game_list[2:]
+                game_list.pop(0)
             else:
                 frame_score = 10 + sum(map(int, game_list[1:3]))
                 game_list = game_list[3:]
         elif game_list[0] in ['/']:
             if game_list[1] == 'X':
                 score += 10 - int(game_list[0][1])
-                game_list.pop(0)
-                while game_list and (game_list[0] == 'X' or not str.isdigit(game_list[0])):
-                    if game_list[0] == 'X':
+                while game_list[1] in ['/'] or game_list[1].isdigit():
+                    if game_list[1] == 'X':
                         score += 10
                         game_list.pop(0)
+                    elif game_list[1] in ['/']:
+                        score += 10 - int(game_list[1][1])
+                        game_list.pop(0)
                     else:
-                        frame_score = int(game_list.pop(0))
-                        while game_list and (game_list[0] in ['/'] or int(game_list[0]) < 10):
-                            if game_list[0] in ['/']:
-                                frame_score += 10 - int(game_list[0][1])
-                                game_list.pop(0)
-                            else:
-                                frame_score += int(game_list.pop(0))
-                        score += frame_score
+                        score += sum(map(int, game_list[:2]))
+                        game_list = game_list[2:]
+                game_list.pop(0)
+                game_list.pop(0)
             else:
                 frame_score = 10 - int(game_list[0][1]) + sum(map(int, game_list[1:3]))
+                while game_list[1] in ['/'] or game_list[1].isdigit():
+                    if game_list[1] == 'X':
+                        score += 10
+                        game_list.pop(0)
+                    elif game_list[1] in ['/']:
+                        score += 10 - int(game_list[1][1])
+                        game_list.pop(0)
+                    else:
+                        score += sum(map(int, game_list[:2]))
+                        game_list = game_list[2:]
                 game_list = game_list[3:]
         else:
             if len(game_list) >= 2 and sum(map(int, game_list[:2])) == 10:
                 score += 10
+                while game_list[1] in ['/'] or game_list[1].isdigit():
+                    if game_list[1] == 'X':
+                        score += 10
+                        game_list.pop(0)
+                    elif game_list[1] in ['/']:
+                        score += 10 - int(game_list[1][1])
+                        game_list.pop(0)
+                    else:
+                        score += sum(map(int, game_list[:2]))
+                        game_list = game_list[2:]
                 game_list = game_list[2:]
             else:
                 frame_score = sum(map(int, game_list[:2]))
+                while game_list[1] in ['/'] or game_list[1].isdigit():
+                    if game_list[1] == 'X':
+                        score += 10
+                        game_list.pop(0)
+                    elif game_list[1] in ['/']:
+                        score += 10 - int(game_list[1][1])
+                        game_list.pop(0)
+                    else:
+                        score += sum(map(int, game_list[:2]))
+                        game_list = game_list[2:]
                 game_list = game_list[2:]
         score += frame_score
     if len(game_list) > 0:
