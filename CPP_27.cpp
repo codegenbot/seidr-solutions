902c2e4d86c6f1469847e0d3dfe8eaa78b143807
beep boop <githubbot@vadim.me>
2024-07-23 22:42:48 +0200

    Solution 79 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0w182pf9 Prompt: 46895076-ad03-45cd-9fe8-7b9cd4da3c42.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 46895076-ad03-45cd-9fe8-7b9cd4da3c42.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~ In file included from 46895076-ad03-45cd-9fe8-7b9cd4da3c42.cpp:25: 46895076-ad03-45cd-9fe8-7b9cd4da3c42.cpp: In function ‘int main()’: 46895076-ad03-45cd-9fe8-7b9cd4da3c42.cpp:27:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    27 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,19 +1,3 @@
-#include <iostream>
-#include <cctype>
-using namespace std;
-
-string flip_case(string str){
-    string result = "";
-    for(int i=0; i<str.length(); i++){
-        if(islower(str[i])){
-            result += toupper(str[i]);
-        } else {
-            result += tolower(str[i]);
-        }
-    }
-    return result;
-}
-
 int main(){
     string s;
     cout << "Enter a sentence: ";
