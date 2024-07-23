050d1ded9d4da7941b4103c22925d0e8e31e4165
beep boop <githubbot@vadim.me>
2024-07-23 14:16:46 +0200

    Solution 6 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fifgt0ej Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4869e6d4-01b8-41b0-963e-ea160707c 5d1.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4869e6d4-01b8-41b0-963e-ea160707c 5d1.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,3 @@
 def sort_even(l: list):
-    return [
-        sorted([x for x in l if i % 2 == 0])[::-1][j] if i % 2 == 0 else x
-        for i, x in enumerate(l)
-        for j in range(len(l))
-        if i % 2 == 0
-    ]
+    return [sorted([x for x in l if i % 2 == 0])[::-1][j] if i % 2 == 0 else x 
+            for i, x in enumerate(l) for j in range(len(l)) if i % 2 == 0]
