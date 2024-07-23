b423c675e33128998c776be8102f597f89d501cd
beep boop <githubbot@vadim.me>
2024-07-23 07:33:14 +0200

    Solution 94 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,59 +1,27 @@
-#include<stdio.h>
-#include<string>
-#include<algorithm>
-#include<boost/any.hpp>
-#include<boost/more_traits.hpp>
+```cpp
+#include <string>
+#include <algorithm>
+#include <boost/any.hpp>
 
 using namespace std;
-namespace boost {
-    namespace more_traits {
-
-        template<>
-        struct class_tag<std::string> : public class_tag<const char*> {};
-    }
-}
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
-    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string sa = boost::any_cast<string>(a);
-        string sb = boost::any_cast<string>(b);
-
-        if (sa.empty()) {
-            return b;
-        }
-
-        size_t comma_pos = sa.find(',');
-        if (comma_pos != string::npos) {
-            double a_val = stod(sa.substr(0, comma_pos));
-            double b_val = stod(sb);
-            if (a_val > b_val) {
-                return a;
-            } else {
-                return b;
-            }
-        }
-
-        if (stod(sa) > stod(sb)) {
-            return a;
-        } else {
-            return b;
-        }
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int a_val = boost::any_cast<int>(a);
-        string sb = boost::any_cast<string>(b);
-
-        size_t comma_pos = sb.find(',');
-        if (comma_pos != string::npos) {
-            double b_val = stod(sb.substr(0, comma_pos));
-            return (a_val > b_val ? a : b);
-        } else {
-            return (a_val > stod(sb) ? a : b);
-        }
-    } else {
-        return "None";
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return greater<float>()(boost::get<float>(b), boost::get<int>(a)) ? &b : &a;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return greater<int>()(boost::get<int>(a), boost::get<float>(b)) ? &b : &a;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        return a.convert_to<string>() > b.convert_to<string>() ? a : b;
+    }
+    else if ((a.type() == typeid(float) || a.type() == typeid(int)) && 
+             (b.type() == typeid(float) || b.type() == typeid(int))) {
+        auto fa = boost::get<double>(a);
+        auto fb = boost::get<double>(b);
+        return fa > fb ? &a : (fa < fb ? &b : boost::any("None")));
+    }
+    else {
+        return boost::any("None");
     }
 }
