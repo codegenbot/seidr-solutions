d90d4d7d3f2ae0bd0279e4fffb1885d2b1ce93e4
beep boop <githubbot@vadim.me>
2024-07-23 09:40:52 +0200

    Solution 22 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt: it must return 114 for input 9/-39/X3/7/54622325, but it returns .

--- bowling.py
+++ bowling.py
@@ -1,39 +1,27 @@
 ```
 def bowling_score(game):
     score = 0
-    frame = 1
-    for bowl in game.split(' '):
-        if len(bowl) == 2 and bowl[1] == '/':
-            strike_frame = int(frame)
-            for _ in range(2):
-                next_bowl = game.split(' ')[strike_frame]
-                if len(next_bowl) > 2 and next_bowl[1] == '/':
-                    score += 10 + int(next_bowls.split(' ')[0])
-                elif next_bowl[1] == 'X':
-                    score += 10 + (10 - int(next_bowl[0])) * 2
-                else:
-                    score += int(next_bowl[:1]) * 2 + int(next_bowl[1:])
-                frame += 1
-        elif bowl[0] == 'X':
-            if len(bowl) > 1 and bowl[1] != '/':
-                score += 10 + int(bowl[1])
-            else:
-                strike_frame = frame
-                for _ in range(2):
-                    next_bowl = game.split(' ')[strike_frame]
-                    if len(next_bowl) > 2 and next_bowl[1] == '/':
-                        score += 10 + int(next_bowls.split(' ')[0])
-                    elif next_bowl[1] == 'X':
-                        score += 10 + (10 - int(next_bowl[0])) * 2
-                    else:
-                        score += int(next_bowl[:1]) * 2 + int(next_bowl[1:])
-                    frame += 1
-        elif bowl == 'X X':
-            score += 20
+    i = 0
+    while i < len(game):
+        if game[i] == 'X':
+            score += 10
+            i += 1
+            if i < len(game) and game[i] == '/':
+                score += 10 - int(game[i-1])
+                i += 1
+        elif game[i] == '/':
+            score += 10 - int(game[i-1])
+            i += 2
         else:
-            if len(bowl) > 1 and bowl[0] == '/':
-                score += int(bowl[:1]) * 2 + int(bowl[1:])
+            frame_score = 0
+            if len(game[i]) > 1 and game[i][0] == 'X':
+                frame_score = 10 + int(game[i][1:])
+            elif len(game[i]) == 2:
+                frame_score = int(game[i][0]) * 2 + int(game[i][1])
             else:
-                score += int(bowl)
-        frame += 1
+                frame_score = int(game[i])
+
+            score += frame_score
+            i += 1
+
     return score
