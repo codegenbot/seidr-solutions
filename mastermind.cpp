1118e8837f2e0cf91a5f6aafa82f0db050eb7075
beep boop <githubbot@vadim.me>
2024-07-23 13:43:17 +0200

    Solution 39 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/jwsg3b1w Prompt:       |                                 ^~~~~~ In file included from f70f4658-e439-4cee-b2e9-d0092cdaf05b.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ f70f4658-e439-4cee-b2e9-d0092cdaf05b.cpp:3:29: error: ‘string’ was not declared in this scope     3 | int mastermind(string code, string guess) {       |                             ^~~~~~ f70f4658-e439-4cee-b2e9-d0092cdaf05b.cpp:3:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from f70f4658-e439-4cee-b2e9-d0092cdaf05b.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from f70f4658-e439-4cee-b2e9-d0092cdaf05b.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ f70f4658-e439-4cee-b2e9-d0092cdaf05b.cpp:3:41: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,26 +1,28 @@
+```cpp
 #include <string>
-#include <algorithm>
+using namespace std;
 
 int mastermind(string code, string guess) {
-    int white = 0;
     int black = 0;
+    int white = 0;
+    int codeCount[6] = {0};
 
+    // Count the occurrences of each character in the code
+    for (int i = 0; i < 4; ++i) {
+        codeCount[code[i] - '0']++;
+    }
+
+    // Count the black pegs (correct color, correct place)
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
+            codeCount[guess[i] - '0']--;
         }
     }
 
-    for (char c : code) {
-        int count = 0;
-        for (int i = 0; i < 4; ++i) {
-            if (c == guess[i]) {
-                count++;
-            }
-        }
-        if (count > 1) {
-            white++;
-        }
+    // Count the white pegs (correct color, wrong place)
+    for (int i = 0; i < 6; ++i) {
+        white += min(codeCount[i], count(guess.begin(), guess.end(), i + '0'));
     }
 
     return black + white;
