cd5fc3578db0d60483d81575c8a66b1f5c13da45
beep boop <githubbot@vadim.me>
2024-07-23 10:19:38 +0200

    Solution 24 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/835050c8-87ac-4dd6-884b-360382bf3 f1c.py", line 15, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/835050c8-87ac-4dd6-884b-360382bf3 f1c.py", line 12, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,9 +1,12 @@
-```
 def modp(n: int, p: int):
-    if not is_prime(p):
-        return None
     if gcd(n, p) == 1:
-        return pow(n, (p - 1), p)
+        result = 1
+        while p > 1:
+            if p & 1:
+                result = (result * n) % p
+            p >>= 1
+            n = (n * n) % p
+        return result
     else:
         return None
 
@@ -11,11 +14,3 @@ def gcd(a: int, b: int):
     while b:
         a, b = b, a % b
     return a
-
-def is_prime(num: int):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
