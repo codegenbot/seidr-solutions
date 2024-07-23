beb0d70f03eb17ff5db9510cb09ccd6c7be1b108
beep boop <githubbot@vadim.me>
2024-07-23 09:39:21 +0200

    Solution 2 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/texleggy Prompt: 3d4e2f3d-1f3f-4922-8e38-d23de1ae7c37.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 3d4e2f3d-1f3f-4922-8e38-d23de1ae7c37.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 3d4e2f3d-1f3f-4922-8e38-d23de1ae7c37.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,40 +1,25 @@
-int whitePegs(string code, string guess) {
-    int count = 0;
-    for(int i=0; i<4; i++) {
+#include <string>
+using namespace std;
+
+pair<int, int> mastermind(string code, string guess) {
+    int white = 0;
+    int black = 0;
+
+    for(int i = 0; i < 4; i++) {
         if(code[i] == guess[i]) {
-            count++;
+            black++;
         }
     }
-    return 4 - count;
-}
 
-int blackPegs(string code, string guess) {
-    int count = 0;
-    vector<char> codeVector(code.begin(), code.end());
-    for(int i=0; i<4; i++) {
-        if(countOf(codeVector, guess[i]) > 0) {
-            count++;
-            replaceElement(codeVector, guess[i]);
+    for(int i = 0; i < 6; i++) {
+        int count = 0;
+        for(int j = 0; j < 4; j++) {
+            if(guess[j] == (char)(i + 65) && code.find((char)(i + 65)) != string::npos) {
+                count++;
+            }
         }
+        white += count - black;
     }
-    return count;
-}
 
-int countOf(vector<char> &vec, char ch) {
-    int count = 0;
-    for(int i=0; i<vec.size(); i++) {
-        if(vec[i] == ch) {
-            count++;
-        }
-    }
-    return count;
-}
-
-void replaceElement(vector<char> &vec, char ch) {
-    for(int i=0; i<vec.size(); i++) {
-        if(vec[i] == ch) {
-            vec[i] = 'X';
-            break;
-        }
-    }
+    return make_pair(black, white);
 }
