1811436cb89e96a60f206a0d23657f539ffa3e66
beep boop <githubbot@vadim.me>
2024-07-23 09:14:13 +0200

    Solution 46 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0d0ub3e7 Prompt: 89b19aa7-394d-4070-b7fa-ccfaf444cf63.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 89b19aa7-394d-4070-b7fa-ccfaf444cf63.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,14 +1,12 @@
-```cpp
 #include <sstream>
 #include <iostream>
 
 using namespace std;
 
 string string_xor(string a, string b) {
-    stringstream s_a(a), s_b(b);
+    istringstream is_a(a), is_b(b);
     string result = "";
-    char c;
-    while (s_a >> c && s_b >> c) {
+    for (char c; is_a >> c && is_b >> c; ) {
         if ((c - '0') ^ ('0' - '0')) {
             result += "1";
         } else {
