f08b6b3225c444ec35a93cc47e09391794a36092
beep boop <githubbot@vadim.me>
2024-07-23 21:07:29 +0200

    Solution 10 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/a95dyu1w Prompt: it must return N for input c=88}~^EZCByAZk[R4GNy'^%J)[AfJcr].D/s%c6O,eps4mNRW[4$ZJ>Zto_.K3ct{'&j}Y)Vk6%voe[!\:wBZhNnnAbq R, but it returns m.

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,4 +1,8 @@
 std::string middleCharacter(std::string s) {
     int len = s.length();
-    return s.substr(len/2, len % 2);
+    if(len % 2 == 0) {
+        return s.substr(len/2-1, 1) + s.substr(len/2, 1);
+    } else {
+        return s.substr(len/2, 1);
+    }
 }
