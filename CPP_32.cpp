c588018f2173f219782319f16398f7d2c9fb9ab8
beep boop <githubbot@vadim.me>
2024-07-23 10:06:21 +0200

    Solution 73 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/sw8yxj3m Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from bab83a78-3cd6-4f21-86a6-f165d4a365fc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -8,7 +8,7 @@ using namespace std;
 double poly(initializer_list<double> xs, double x){
     double sum=0;
     for (auto i : xs) {
-        sum+=i*pow(x,sizeof(i));
+        sum+=i*pow(x,i);
     }
     return sum;
 }
@@ -17,7 +17,7 @@ double find_zero(initializer_list<double> xs){
     if(xs.size() % 2 != 0) return -1; 
     double x = 1.0; 
     while(abs(poly(xs, x)) > 1e-9){ 
-        x -= poly({xs.begin()[1], xs.end()[0]}, x); 
+        x -= poly(xs, x)/poly({xs.begin()[1], *(xs.end()-1)})/pow(x,i);
     }
     ostringstream oss;
     oss << fixed << setprecision(6) << x;
