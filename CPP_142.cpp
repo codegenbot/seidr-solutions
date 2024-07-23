c3424318b20baba304ca2d79ac1a3a1e83e0e8fa
beep boop <githubbot@vadim.me>
2024-07-23 18:26:44 +0200

    Solution 13 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xmp0d2uq Prompt: Program failed

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -2,9 +2,8 @@ int sum_squares(std::vector<int> lst) {
     int result = 0;
     for (int i = 0; i < lst.size(); i++) {
         if ((i + 1) % 3 == 0 && (i + 1) % 4 != 0)
-            result += std::pow(lst[i], 3); 
+            result += std::pow(lst[i], 3);
         else if ((i + 1) % 3 == 0)
-            result += std::pow(lst[i], 2); 
+            result += std::pow(lst[i], 2);
     }
     return result;
-}
