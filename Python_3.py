8372e8f7f264cc9acd5a8706c66de9ac7e9bad05
beep boop <githubbot@vadim.me>
2024-07-23 23:35:26 +0200

    Solution 18 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4u8mdrkz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5e92557-aaba-4da2-8891-376faf25b 7b2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_3.py
+++ Python_3.py
@@ -1,8 +1,10 @@
-```
 from typing import List
 
 def below_zero(operations: List[int]) -> bool:
-    return any(operation < 0 for operation in operations)
+    for operation in operations:
+        if operation < 0:
+            return True
+    return False
 
 if __name__ == "__main__":
     while True:
