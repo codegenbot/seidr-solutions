63181bd2e3ec0802eadbfedeb2e4d836455a4bdb
beep boop <githubbot@vadim.me>
2024-07-23 13:09:36 +0200

    Solution 58 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fedc048-1f8a-485d-96f8-1f8b541e4 e50.py", line 14, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9fedc048-1f8a-485d-96f8-1f8b541e4 e50.py", line 10, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,5 @@
-```
-def double_the_difference(list1, list2):
-    difference = sum(list1) - sum(list2)
-    return 2 * abs(difference)
-
-first_list = [5, 10, 15]
-second_list = [5, 20, 30]
-
-print(double_the_difference(first_list, second_list))
+def double_the_difference(lst):
+    if len(set(map(int, lst))) != len(lst):
+        return "Lists must not contain duplicate elements"
+    differences = [(int(a) - int(b)) ** 2 for a, b in zip(map(str, lst)[::2], map(str, lst)[1::2])]
+    return sum(differences) * 8
