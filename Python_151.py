5f873ef1daaed95b7340e3e4b153c6db5504d007
beep boop <githubbot@vadim.me>
2024-07-23 12:50:21 +0200

    Solution 40 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/75f06945-73a3-4d1b-8f29-db4d2d8b5 827.py", line 13, in <module>     check(double_the_difference)           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,8 @@
-def double_the_difference(list1, list2):
-    sum_of_lists = sum(sum(pair) for pair in list1)
-    total_sum = sum(list2)
-    difference = abs((sum_of_lists - total_sum))
-    return 3 * difference
+```
+def double_the_difference(lst1, lst2):
+    return sum(x * y for x, y in zip(lst1[0], lst2)) - sum(x * y for x, y in zip(lst1[1], lst2))
 
 def check():
     first_list = [[1, 2], [3, 4]]
     second_list = [-1, 0, 1]
     print(double_the_difference(first_list, second_list))
-
-check()
