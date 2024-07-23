b8d29bb36587ff387b6a7e746ef8b83cb4bf24ec
beep boop <githubbot@vadim.me>
2024-07-24 01:12:37 +0200

    Solution 7 of CPP/97, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8d0uzbyq Prompt: Program failed

--- CPP_97.cpp
+++ CPP_97.cpp
@@ -1,5 +1,15 @@
-int multiply(int a, int b){
-    int unit_a = a % 10;
-    int unit_b = b % 10;
-    return (unit_a * unit_b);
+int multiply(int a, int b) {
+    if (b == 0)
+        return 0;
+    
+    int result = 0;
+    while(b > 0) {
+        if (b & 1)
+            result += a;
+        
+        a <<= 1; 
+        b >>= 1; 
+    }
+    
+    return result;
 }
