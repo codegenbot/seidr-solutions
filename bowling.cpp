e69d02031dbc3fabea62da8fee8e0078bc71b1d0
beep boop <githubbot@vadim.me>
2024-07-24 00:59:17 +0200

    Solution 25 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/rl9rebse Prompt: 91c3b78e-8be9-4799-abf1-d8d9cea17989.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string str) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,26 +2,15 @@
 
 int bowlingScore(string str) {
     int score = 0;
-    int i = 0;
-    while (i < 10) {
+    for (int i = 0; i < 10; i++) {
         if (str[i] == '/') {
             string firstPart = str.substr(0, i);
             string secondPart = str.substr(i + 1);
-            int firstScore = (firstPart[0] - '0') * 10 + (firstPart.length() > 1 ? (firstPart[1] - '0') : 10);
-            if (secondPart.length() == 2) {
-                int secondScore = (secondPart[0] - '0') * 10 + (secondPart[1] - '0');
-                score += firstScore + secondScore;
-            } else {
-                score += firstScore + (secondPart[0] - '0') * 10;
-            }
-            i++;
+            int firstScore = (firstPart[0] - '0') * 10 + (firstPart.length() > 1 ? (firstPart[1] - '0') : 0);
+            int secondScore = (secondPart[0] - '0') * 5;
+            score += max(firstScore, secondScore) + min(firstScore, secondScore);
         } else {
-            int frameScore = (str[i] - '0') * 10;
-            if (i < 8 && str[i + 1] != '/') {
-                frameScore += (str[i + 1] - '0') * 10;
-            }
-            score += frameScore;
-            i++;
+            score += (str[i] - '0') * 10;
         }
     }
     return score;
