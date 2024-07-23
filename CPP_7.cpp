b59ab572db0de6683952c4960679925c11b8a636
beep boop <githubbot@vadim.me>
2024-07-23 20:32:33 +0200

    Solution 18 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4yu5auqo Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7671bb39-ab9b-4856-896f-3a753b803c3c.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7671bb39-ab9b-4856-896f-3a753b803c3c.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7671bb39-ab9b-4856-896f-3a753b803c3c.cpp:10: 7671bb39-ab9b-4856-896f-3a753b803c3c.cpp: In function ‘int main()’: 7671bb39-ab9b-4856-896f-3a753b803c3c.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 7671bb39-ab9b-4856-896f-3a753b803c3c.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,83 +1,26 @@
+```cpp
 #include <vector>
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    bool b_exists = false;
-    for(string s : b) {
-        if(a.size() > 0 && !b_exists && a[0] == s)
-            b_exists = true;
+    if(b.size() != a.size()) {
+        return false;
     }
-    return b_exists;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
-    bool is_found = false;
-    
     for(string s : strings) {
-        if(s.find(substring) != string::npos) {
+        int found = s.find(substring);
+        if(found != string::npos)
             result.push_back(s);
-            is_found = true;
-        }
-    }
-    
-    if(is_found) {
-        for(auto b : {"before", "after"}) {
-            vector<string> temp;
-            if(b == "before") {
-                for(string s : strings) {
-                    if(!s.find(substring))
-                        temp.push_back(s);
-                }
-            } else if(b == "after") {
-                vector<string> remaining = strings;
-                while(!is_found) {
-                    string next;
-                    for(string s : remaining) {
-                        if(s.find(substring) != string::npos) {
-                            next = s;
-                            is_found = true;
-                            break;
-                        }
-                    }
-                    if(is_found)
-                        break;
-                    remaining.erase(std::remove(remaining.begin(), remaining.end(), next), remaining.end());
-                }
-                temp = remaining;
-            }
-            for(string s : temp)
-                result.push_back(s);
-        }
-    } else {
-        vector<string> remaining = strings;
-        while(!is_found) {
-            string next;
-            for(string s : remaining) {
-                if(s.find(substring) != string::npos) {
-                    next = s;
-                    is_found = true;
-                    break;
-                }
-            }
-            if(is_found)
-                break;
-            remaining.erase(std::remove(remaining.begin(), remaining.end(), next), remaining.end());
-        }
-        for(string s : remaining)
+        else if (!issame({s}, {"")) // Check if string does not contain substring
             result.push_back(s);
     }
-
-    vector<string> result_final;
-    bool found_again = false;
-
-    for(string s : result) {
-        if(!found_again && s.find(substring) != string::npos) 
-            found_again = true;
-        else if(found_again && s.find(substring) == string::npos)
-            break;
-        result_final.push_back(s);
-    }
-    
-    return result_final;
+    return result;
 }
