1b17a4b0837785899cfb74af99b6439bcd862457
beep boop <githubbot@vadim.me>
2024-07-23 23:59:34 +0200

    Solution 17 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/t4sl8j0j Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3ba4b4a2-754e-4362-a3df-6b2b9eb50 72d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -1,13 +1,14 @@
+```
 def validate_tweet(tweet):
     if not tweet:
         return "You didn't type anything"
-    elif len(tweet.replace(' ', '')) > 140:
+    elif len(tweet) > 140:
         return "Too many characters"
     else:
-        return f"Your tweet has {len(tweet.replace(' ', ''))} characters"
+        return f"Your tweet has {len(tweet)} characters"
 
-print(validate_tweet("")) 
-print(validate_tweet("1")) 
-print(validate_tweet("max length tweet...")) 
-print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``))"))
-print(validate_tweet("Tooooooooooolong1"))
+print('''{}'''.format(validate_tweet("")))
+print('''{}'''.format(validate_tweet("1")))
+print('''{}'''.format(validate_tweet("max length tweet...")))
+print('''{}'''.format(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``))"))
+print('''{}'''.format(validate_tweet("Tooooooooooolong1")))
