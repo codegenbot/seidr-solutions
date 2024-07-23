09991f2ad3c55d28a714c076168eb3dd89426f3a
beep boop <githubbot@vadim.me>
2024-07-23 22:10:51 +0200

    Solution 44 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8dwusk5j Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_set.h:61,                  from /usr/include/c++/11/set:61,                  from 01cffaa2-8caa-4083-ba4e-d84a4bb8f285.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,19 +1,18 @@
-#include <vector>
+```cpp
 #include <set>
+#include <vector>
 
 using namespace std;
 
-bool issame(vector<int> v1, vector<int> v2){
-    if(v1.size() != v2.size())return false;
-    for(int i = 0; i<v1.size();i++){
-        if(find(v2.begin(),v2.end(),v1[i]) == v2.end())
-            return false;
-    }
-    return true;
+bool issame(vector<int> v1, vector<int> v2) {
+    if (v1.size() != v2.size())
+        return false;
+    set<vector<int>> s({v1, v2});
+    return s.size() == 1;
 }
 
 vector<int> unique(vector<int> l) {
     set<int> s(l.begin(), l.end());
-    vector<int> res(s.begin(), s.end());
+    vector<int> res(std::initializer_list<int>{s.begin(), s.end()});
     return res;
 }
