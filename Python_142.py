acc04dd3be83d7a440fd594c2c1fc3d7588660b0
beep boop <githubbot@vadim.me>
2024-07-23 14:01:25 +0200

    Solution 72 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3c8bbaf1-ccfe-453f-ba76-f2db75c78 f11.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,8 @@
-```
 def sum_squares(user_input):
     if not isinstance(user_input, str) and not isinstance(user_input, tuple) and not hasattr(user_input, "__iter__"):
         return "Error: Input must be a string or iterable."
     if isinstance(user_input, list):
-        numbers = [n ** 2 for n in map(int, filter(str.isnumeric, user_input))]
+        numbers = [n ** 2 for n in map(int, filter(str.isnumeric, (map(str, user_input))))]
     else:
         numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split())))
     print(sum(numbers))
