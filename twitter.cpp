9f2b16a0df0942bdade7858421007be3296c7f59
beep boop <githubbot@vadim.me>
2024-07-23 11:41:22 +0200

    Solution 5 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/aqybqb2v Prompt: it must return Your tweet has 125 characters for input /]6r&ATIJX}qx&q\Q.:x`"*_`:_X`JM!ph}xzKz)=lqHB*nWp[&jW#p%o/2gUxf/SO2qlZc;ibYAh7!.euix;0l+H$3#]Klx=]x\uoM7gFX"Knk-gEEr{6kUeFEX2, but it returns Enter a tweet: Your tweet has 125 characters.

--- twitter.cpp
+++ twitter.cpp
@@ -7,7 +7,7 @@ string validateTweet(string tweet) {
     if (tweet.find_first_not_of(" \t") == string::npos) {
         return "You didn't type anything";
     }
-    
+
     size_t len = 0;
     for (char c : tweet) {
         if (!isspace(c)) {
