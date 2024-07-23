35646ac6419afd1f258f67aac54056e19238168a
beep boop <githubbot@vadim.me>
2024-07-24 01:36:58 +0200

    Solution 18 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1g16sxbf Prompt:       |                              ^~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:23: 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp: In function ‘int main()’: 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:33:20: error: ‘by_length’ was not decla red in this scope    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,24 +1,55 @@
-bool issame(vector<string> v1, vector<string> v2) {
-    if (v1.size() != v2.size()) 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+bool issame(vector<int> arr1, vector<int> arr2) {
+    if (arr1.size() != arr2.size())
         return false;
     
-    for(int i = 0; i < v1.size(); ++i)
-        if(v1[i] != v2[i])
+    for (int i = 0; i < arr1.size(); i++) {
+        if (arr1[i] != arr2[i])
             return false;
+    }
     
     return true;
 }
 
-int main() {
-    vector<int> arr = {1, 3, 5};
-    vector<string> result = by_length(arr);
-    vector<string> arr2 = {1, 4, 9};
-    vector<string> result2 = by_length(arr2);
+vector<string> by_length(vector<int> arr) {
+    vector<int> temp;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9)
+            temp.push_back(i);
+    }
+    
+    sort(temp.begin(), temp.end());
+    reverse(temp.begin(), temp.end());
+    
+    vector<string> result;
+    for (int i : temp) {
+        string s = to_string(i);
+        map<char, string> m = {{'1', "One"}, {'2', "Two"}, {'3', "Three"}, 
+                                {'4', "Four"}, {'5', "Five"}, {'6', "Six"}, 
+                                {'7', "Seven"}, {'8', "Eight"}, {'9', "Nine"}};
+        result.push_back(m.at(s[0]));
+    }
+    
+    return result;
+}
 
-    if (issame(result, result2)) 
-        cout << "The output vectors are same.\n";
+int main() {
+    vector<int> arr1 = {3, 5, 2, 4};
+    vector<int> arr2 = {5, 4, 2, 3};
+    vector<string> output = by_length(arr1);
+    for (string str : output) 
+        cout << str << " ";
+    cout << endl;
+    
+    if(issame(arr1,arr2))
+        cout << "The two arrays are the same." << endl;
     else
-        cout << "The output vectors are not the same.\n";
+        cout << "The two arrays are not the same." << endl;
 
     return 0;
 }
