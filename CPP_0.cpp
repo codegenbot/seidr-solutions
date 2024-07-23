17971eb6b35cfbc8280674e59912de9d578ba05f
beep boop <githubbot@vadim.me>
2024-07-23 23:03:07 +0200

    Solution 91 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/b67etnp4 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from a9933043-29df-4e8b-90c2-2beb3d1d82d4.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -12,8 +12,7 @@ bool has_close_elements(vector<float> numbers, float threshold){
     return false;
 
 int main() {
-    vector<float> a(6);
-    a[0]=1.0f; a[1]=2.0f; a[2]=3.9f; a[3]=4.0f; a[4]=5.0f; a[5]=2.2f;
+    vector<float> a({1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f});
     cout << has_close_elements(a, 1.0) << endl;
     return 0;
 }
