42252122eab8ee2ac5fca866c9bd854fef108be1
beep boop <githubbot@vadim.me>
2024-07-23 10:51:27 +0200

    Solution 14 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/039ea61b-cf6e-405c-bdd4-c52d368b8 57a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,7 +1,5 @@
 ```
 def fruit_distribution(s,n):
     s = s.lower()
-    count_o = s.count('o')
-    count_a = s.count('a')
-    num_fruit = int(''.join(filter(str.isdigit, s)))
-    return n - (count_o + count_a + num_fruit)
+    n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a'])))
+    return """{}```
