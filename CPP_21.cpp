55a63456fca6605f0915a1945e019fb41b5317e0
beep boop <githubbot@vadim.me>
2024-07-23 09:09:39 +0200

    Solution 7 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/19htxffx Prompt:     5 | 4c4aed00-13e3-4f8b-8ee9-9026e1557d4f.cpp:36:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    36 | int main(){       |         ^~ 4c4aed00-13e3-4f8b-8ee9-9026e1557d4f.cpp:36:9: note: remove parentheses to defau lt-initialize a variable    36 | int main(){       |         ^~       |         -- 4c4aed00-13e3-4f8b-8ee9-9026e1557d4f.cpp:36:9: note: or replace parentheses with  braces to value-initialize a variable 4c4aed00-13e3-4f8b-8ee9-9026e1557d4f.cpp:36:11: error: a function-definition is not allowed here before ‘{’ token    36 | int main(){       |           ^ 4c4aed00-13e3-4f8b-8ee9-9026e1557d4f.cpp:38:2: error: expected ‘}’ at end of inp ut    38 | }       |  ^ 4c4aed00-13e3-4f8b-8ee9-9026e1557d4f.cpp:23:12: note: to match this ‘{’    23 | int main() {       |            ^

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -20,8 +20,4 @@ bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
 
-int main() {
-    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), std::vector<float>({0.25, 0.0, 1.0, 0.5, 0.75}));
-
-    return 0;
-}
+assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), std::vector<float>({0.25, 0.0, 1.0, 0.5, 0.75}));
