9eefaf6ba9811041f481337258c998dbc2b27950
beep boop <githubbot@vadim.me>
2024-07-23 08:34:27 +0200

    Solution 9 of Python/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yngnl4id Prompt: Program did not receive expected input

--- Python_11.py
+++ Python_11.py
@@ -3,6 +3,6 @@ from typing import List
 
 def string_xor(a: str, b: str) -> str:
     max_len = max(len(a), len(b))
-    a += "0" * (max_len - len(a))  # pad with zeros if necessary
-    b += "0" * (max_len - len(b))  # pad with zeros if necessary
+    a += '0' * (max_len - len(a))  # pad with zeros if necessary
+    b += '0' * (max_len - len(b))  # pad with zeros if necessary
     return "".join(str(int(x) ^ int(y)) for x, y in zip(a, b))
