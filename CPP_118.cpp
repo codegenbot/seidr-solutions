f0218b951ff5e245aa834a8cba729ffe45d4b291
beep boop <githubbot@vadim.me>
2024-07-23 17:24:28 +0200

    Solution 34 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/dwqrfb6s Prompt: mbuf_iterator<_CharT, std::char_traits<_CharT> >’     7 |     return (find(vowels.begin(), vowels.end(), tolower(c)) != vowels.end ());       |             ~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 92337945-d8c0-4cf0-9296-e33eca7ab2fc.cpp: In function ‘int main()’: 92337945-d8c0-4cf0-9296-e33eca7ab2fc.cpp:20:5: error: ‘assert’ was not declared in this scope    20 |     assert(get_closest_vowel("Above") == "o");       |     ^~~~~~ 92337945-d8c0-4cf0-9296-e33eca7ab2fc.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <iostream>   +++ |+#include <cassert>     2 | #include <string> 92337945-d8c0-4cf0-9296-e33eca7ab2fc.cpp: At global scope: 92337945-d8c0-4cf0-9296-e33eca7ab2fc.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 92337945-d8c0-4cf0-9296-e33eca7ab2fc.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,8 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+bool isvowl(char c) {
+    string vowels = "aeiouAEIOU";
+    return (find(vowels.begin(), vowels.end(), tolower(c)) != vowels.end());
+}
+
 std::string get_closest_vowel(std::string word) {
+    int closest = -1;
     for (int i = 0; i < word.size(); ++i) {
-        if (isvowel(word[i])) {
-            return std::string(1, tolower(word[i]));
+        if (isvowl(word[i])) {
+            if (closest == -1)
+                closest = i;
+            else
+                return (word[closest] <= word[i]) ? std::string(1, tolower(word[closest])) : std::string(1, tolower(word[i]));
         }
     }
     return "";
+}
+
+int main() {
+    assert(get_closest_vowel("Above") == "o");
+    cout << get_closest_vowel("Apple") << endl;
+    return 0;
 }
