8a87b100c8ed7a8359e4a2139dafb95eb6957289
beep boop <githubbot@vadim.me>
2024-07-24 01:40:15 +0200

    Solution 7 of CPP/102, ratio 0.5 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/srxqhfiv Prompt: Program failed

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,8 +1,7 @@
 int choose_num(int x, int y) {
     int last_even = -1;
     for (int i = x; i <= y; ++i) {
-        if (i % 2 == 0)
-            last_even = i;
+        if (i % 2 == 0) last_even = i;
     }
     return last_even;
 }
