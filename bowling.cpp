fd0edeaa3354380643701b820fc82f71ea839adb
beep boop <githubbot@vadim.me>
2024-07-23 11:08:21 +0200

    Solution 87 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt:  in this scope    20 | int getRoll(string s, int i) {       |             ^~~~~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:20:23: error: expected primary-expressi on before ‘int’    20 | int getRoll(string s, int i) {       |                       ^~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:20:28: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | int getRoll(string s, int i) {       |                            ^ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:33:18: error: ‘string’ was not declared  in this scope    33 | int getBonusRoll(string s, int i) {       |                  ^~~~~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:33:28: error: expected primary-expressi on before ‘int’    33 | int getBonusRoll(string s, int i) {       |                            ^~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:33:33: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | int getBonusRoll(string s, int i) {       |                                 ^

--- bowling.cpp
+++ bowling.cpp
@@ -4,38 +4,58 @@ using namespace std;
 int score = 0;
 string s = "X/X/5+7-3-1/2+4/X/2-8-X/2+6/X";
 
-int main() {
-    for(int i = 0; i < s.length(); i++) {
+void bowling() {
+    int i = 0, frameScore = 0;
+    while(i < s.length()) {
         char c = s[i];
         if (c == '/') {
-            score += getRoll(s, i) + getBonusRoll(s, i);
+            frameScore += getRoll(s, i);
+            score += frameScore;
+            frameScore = 0;
         } else if (c == 'X' || c == 'x') {
-            score += min(i+1, 10);
+            frameScore += min(i+1, 10);
+            score += frameScore;
+            frameScore = 0;
+            i++;
+            continue;
         } else if (isdigit(c)) {
             int roll = getRoll(s, i);
             if (s[i+1] == '+') {
-                score += roll + max(min(getBonusRoll(s, i) - roll - 1, 9), 0);
+                frameScore += roll + max(min(getBonusRoll(s, i) - roll - 1, 9), 0);
             } else {
-                score += roll;
+                frameScore += roll;
             }
+            score += frameScore;
+            frameScore = 0;
         }
+        i++;
     }
-    return score;
+    cout << "Your total bowling score is: " << score << endl;
 }
 
 int getRoll(string s, int i) {
-    if(i >= s.length() - 2) return 10; 
-    for(int j = i + 1; ; j++) { 
-        if (!isdigit(s[j])) {
-            return min(j - i, 10);
+    if(i >= s.length() - 1) return 0; 
+    if (s[i+1] == '/') {
+        return 10;
+    } else {
+        for(int j = i + 1; ; j++) { 
+            if (!isdigit(s[j])) {
+                return min(j - i, 10);
+            }
         }
     }
 }
 
 int getBonusRoll(string s, int i) {
+    if(i >= s.length() - 1) return 0; 
     for(int j = i + 2; ; j++) { 
         if (!isdigit(s[j]) && (s[j] == '+' || s[j] == '-')) {
             return min(j - i - 1, 9);
         }
     }
+}
+
+int main() {
+    bowling();
+    return 0;
 }
