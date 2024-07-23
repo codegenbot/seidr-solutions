8b693c9edaaef7ee874f595178afee93806beb3e
beep boop <githubbot@vadim.me>
2024-07-23 16:38:43 +0200

    Solution 54 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xc29kah6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce67ab30-97d5-4bc9-826c-ee3fbd1e5 d2e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,2 +1,8 @@
-def digitSum(s):
-    return sum(int(c) if c.isdigit() else 0 for c in s)
+def findMissingNumber(nums):
+    if len(nums) == 0:
+        return 1
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if nums[i+1] - nums[i] > 1:
+            return nums[i] + 1
+    return nums[-1] + 1
