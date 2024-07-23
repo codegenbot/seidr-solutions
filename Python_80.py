ba0ae284583690d365c89b9be0bc2e327eeb0c0b
beep boop <githubbot@vadim.me>
2024-07-23 11:06:08 +0200

    Solution 1 of Python/80, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5g6b5duy Prompt: Complete the following code given the task description and function signature.

--- Python_80.py
+++ Python_80.py
@@ -1,2 +1,5 @@
 def is_happy(s):
-    return len(s) >= 3 and len(set(s[i : i + 3] for i in range(0, len(s), 3))) == 3
+    return (
+        len(s) >= 3
+        and len(set(s[i : i + 3] for i in range(len(s) - 2))) == (len(s) - 2) // 3 + 1
+    )
