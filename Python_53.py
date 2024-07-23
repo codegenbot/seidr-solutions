29963d04790c1f43f122fe22cf16e0fe068e79af
beep boop <githubbot@vadim.me>
2024-07-23 18:36:55 +0200

    Solution 90 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/djnymu5v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1ad733c-b1e3-4897-83f2-1f85b2f87 f23.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,10 @@
 ```
-def add(x: int, y: int):
-    return x + y
+def find_largest_number():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    if num1 > num2:
+        print(f"The largest number is {num1}.")
+    else:
+        print(f"The largest number is {num2}.")
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-result = add(num1, num2)
-print(result)
+find_largest_number()
