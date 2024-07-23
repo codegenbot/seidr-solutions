5b1033813da8cab2f275b5133a0ed7a63cef3801
beep boop <githubbot@vadim.me>
2024-07-23 12:53:56 +0200

    Solution 25 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a4sa2iva Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -1,7 +1,11 @@
 def fix_spaces(text):
     new_text = ""
     for char in text:
-        if char == " " and not new_text or new_text[-1:] != " ":
+        if char == " " and not new_text:
+            new_text += "_"
+        elif char == " " and new_text[-1:] == " ":
+            new_text += "-"
+        elif char == " ":
             new_text += "_"
         else:
             new_text += char
