c913788f47d7745af46a8453cba0a3140d2001ae
beep boop <githubbot@vadim.me>
2024-07-24 00:47:42 +0200

    Solution 2 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lmu3upf6 Prompt: Complete the following code given the task description and function signature.

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,28 +1,23 @@
-int skjkasdkd(vector<int> lst) {
-    int max_prime = 0;
-    for (int num : lst) {
-        if (isPrime(num)) {
-            if (num > max_prime) {
-                max_prime = num;
-            }
-        }
-    }
-    int sum_of_digits = 0;
-    while (max_prime > 0) {
-        sum_of_digits += max_prime % 10;
-        max_prime /= 10;
-    }
-    return sum_of_digits;
-}
+Here is the completed code:
 
-bool isPrime(int n) {
-    if (n <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return false;
+int skjkasdkd(vector<int> lst){
+    int maxPrime = 0;
+    for(int i : lst){
+        if(i > 1){
+            bool isPrime = true;
+            for(int j = 2; j*j <= i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime && i > maxPrime)maxPrime = i;
         }
     }
-    return true;
+    int sumOfDigits = 0;
+    while(maxPrime > 0){
+        sumOfDigits += maxPrime % 10;
+        maxPrime /= 10;
+    }
+    return sumOfDigits;
 }
