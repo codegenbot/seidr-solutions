956cf46d756d1ea5c5825c6036e0036382dc7167
beep boop <githubbot@vadim.me>
2024-07-23 13:55:39 +0200

    Solution 26 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wxae0lqy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d53eb4cc-d461-4921-a793-58621c9b6 96a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,18 +1,17 @@
+```
 def parse_music(music_string: str) -> List[int]:
-    note_lengths = {"o": 4, "o|": 2, ".|": 1}
+    note_lengths = {'o': [4], 'o|': [2], '.|': [1]}
     result = []
     i = 0
     while i < len(music_string):
-        if music_string[i] == ".":
-            if i + 3 > len(music_string) - 1:
-                break
-            note_type = music_string[i : i + 3]
-            i += 3
-        else:
-            if i + 2 > len(music_string) - 1:
-                break
-            note_type = music_string[i : i + 2]
+        if music_string[i] == '.':
+            j, k = i+1, min(i+3, len(music_string))
+            for note in note_lengths['.|']:
+                result.extend([note]*((k-j)//2 + (k-j)%2))
             i += 2
-        if note_type in note_lengths:
-            result.append(note_lengths[note_type])
+        else:
+            j, k = i-1, i+1
+            for note in note_lengths.get(music_string[j:k], [4]):
+                result.append(note[0])
+            i += 2
     return result
