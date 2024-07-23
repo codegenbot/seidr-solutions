1a38c72caae8ff9d7e4930ddf2a1faa2ff22877e
beep boop <githubbot@vadim.me>
2024-07-24 01:43:39 +0200

    Solution 21 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/walwymsv Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f5668169-8898-4bab-8ac2-2b13b1846 476.py", line 13, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f5668169-8898-4bab-8ac2-2b13b1846 476.py", line 10, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,13 +1,11 @@
 def fizz_buzz(n: int):
     count = 0
-    for i in range(1, n+1):
-        output = ""
-        if i % 3 == 0:
-            output += "Fizz"
-        if i % 5 == 0:
-            output += "Buzz"
-        if not output:
-            output = str(i)
+    for i in range(1, n + 1):
+        output = (
+            "FizzBuzz"
+            if i % 15 == 0
+            else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
+        )
         print(output)
-        count += (output == str(i))
+        count += 1 if output != str(i) else 0
     return count
