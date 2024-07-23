80858112956463b7c5e0709a11ad2b576382d7fa
beep boop <githubbot@vadim.me>
2024-07-23 21:17:25 +0200

    Solution 75 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7vsawre Prompt:       |                                  ^~~ a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp:6: a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp: In function ‘int main()’: a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp:16:20: error: ‘eat’ was not declared in  this scope    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ a6b4c139-a443-478b-8f47-d7ed9b889d41.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,18 +1,20 @@
 int eat(int a, int b, int c) {
-    return (a + b + c) / 3;
+    return a * b + c;
 }
 
 bool areSameVectors(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) 
+    if (a.size() != b.size()) {
         return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i])
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
 
 int main() {
-    assert(areSameVectors(std::vector<int>( {eat(4, 5, 1), 0})));
+    assert(areSameVectors(std::vector<int>({36,0})));
     return 0;
 }
