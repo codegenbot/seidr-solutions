248a40b6dd820fd9300251f76403c1e66765b713
beep boop <githubbot@vadim.me>
2024-07-23 09:02:04 +0200

    Solution 30 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/60t1ulm2 Prompt:    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5fd8b875-f04a-48a3-a475-324498886512.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5fd8b875-f04a-48a3-a475-324498886512.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5fd8b875-f04a-48a3-a475-324498886512.cpp:11: 5fd8b875-f04a-48a3-a475-324498886512.cpp: In function ‘int main()’: 5fd8b875-f04a-48a3-a475-324498886512.cpp:21:20: error: ‘order_by_points’ was not  declared in this scope    21 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 5fd8b875-f04a-48a3-a475-324498886512.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,23 +1,15 @@
 bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> order_by_points(vector<int> nums) {
+bool order_by_points(const vector<int>& nums) {
     sort(nums.begin(), nums.end(), [](int a, int b) {
-        int sum_a = 0, sum_b = 0;
-        int num_a = abs(a), num_b = abs(b);
-        while (num_a > 0) {
-            sum_a += num_a % 10;
-            num_a /= 10;
-        }
-        while (num_b > 0) {
-            sum_b += num_b % 10;
-            num_b /= 10;
-        }
-        if (sum_a == sum_b) {
-            return a < b;
-        }
-        return sum_a < sum_b;
+        int sumA = 0, sumB = 0;
+        int tempA = abs(a), tempB = abs(b);
+        while (tempA > 0) {sumA += tempA % 10; tempA /= 10;}
+        while (tempB > 0) {sumB += tempB % 10; tempB /= 10;}
+        if (sumA == sumB) return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+        return sumA < sumB;
     });
     return nums;
 }
