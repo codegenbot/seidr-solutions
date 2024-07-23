e16641ef26ef558b29afbe77be1b292304555c7d
beep boop <githubbot@vadim.me>
2024-07-23 14:11:01 +0200

    Solution 19 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fnj1jefp Prompt: Program did not receive expected input

--- Python_21.py
+++ Python_21.py
@@ -3,8 +3,8 @@ from typing import List
 
 def rescale_to_unit(numbers: List[float]) -> List[float]:
     if len(set(numbers)) == 1:
-        return [0.0] * len(numbers)
+        return [0.0] * len(numbers)  
     elif len(numbers) == 1:
-        return [0.0]
+        return [0.0]  
     else:
         return [(x - min(numbers)) / (max(numbers) - min(numbers)) for x in numbers]
