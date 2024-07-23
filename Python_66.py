4db77a90df62c5bb85dbb55cc9afeceaeb3829a4
beep boop <githubbot@vadim.me>
2024-07-23 17:10:03 +0200

    Solution 77 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xc29kah6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bdb61c5f-6325-4dd5-9695-5131cba79 999.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,10 +1,1 @@
-```
-print("This is a digit sum calculator program.")
-n = int(input("Please enter a number: "))
-def digitSum(s):
-    total_sum = 0
-    for i in s:
-        if i.isdigit():
-            total_sum += int(i)
-    print(f"The sum of digits is {total_sum}.")
-digitSum(str(n))
+print("Hello World!")
