cf24eaf7a39b113e8d90c9debcc1294b763bbef0
beep boop <githubbot@vadim.me>
2024-07-23 13:04:53 +0200

    Solution 26 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/2x3zn4j9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,45 +1,28 @@
-```cpp
 #include <vector>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> nums) {
     int n = nums.size();
     vector<vector<int>> res(2);
-    
-    for(int i = 0; i < n; i++) {
-        if(i == n - 1 || nums[i] != nums[i + 1]) {
-            res[0].push_back(nums[i]);
-        }
+
+    int i;
+    for (i = 0; i < n - 1; i++) {
+        if (nums[i] != nums[i + 1]) break;
     }
-    
+    res[0].reserve(i + 1);
+    copy(nums.begin(), nums.begin() + i, back_inserter(res[0]));
+
     int j = n - 1;
-    while(j > 0 && nums[j] == nums.back()) {
-        j--;
-    }
-    
-    for(int i = j; i >= 0; i--) {
-        if(i == 0 || nums[i] != nums[i - 1]) {
-            res[1].push_back(nums[i]);
-        }
-    }
-    
+    for (; j > 0 && nums[j] == nums.back(); j--) {}
+    res[1].reserve(n - j);
+    copy(nums.begin() + i, nums.end(), back_inserter(res[1]));
+
     return res;
 }
 
 int main() {
     vector<int> input = {1, 2, 2, 3, 4, 5};
     vector<vector<int>> output = cutVector(input);
-    cout << "Input: ";
-    for(int i : input) {
-        cout << i << " ";
-    }
-    cout << endl;
-    cout << "Output: " << endl;
-    for(auto v : output) {
-        for(int i : v) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
+
     return 0;
 }
