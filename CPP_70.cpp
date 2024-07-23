b2cc3f533a832a341b260de00c0faee14750ec63
beep boop <githubbot@vadim.me>
2024-07-23 13:47:55 +0200

    Solution 86 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4e3v8dte Prompt:       |                           ^~~~~~ 4388ccd7-8c5b-4dee-820a-504604c5c0e4.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4388ccd7-8c5b-4dee-820a-504604c5c0e4.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4388ccd7-8c5b-4dee-820a-504604c5c0e4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4388ccd7-8c5b-4dee-820a-504604c5c0e4.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4388ccd7-8c5b-4dee-820a-504604c5c0e4.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 4388ccd7-8c5b-4dee-820a-504604c5c0e4.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,9 +1,8 @@
-using namespace std;
-
+```cpp
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
+bool std::issame(std::vector<int> a, std::vector<int> b) {
     if(a.size()!=b.size())
         return false;
     for(int i=0;i<a.size();i++){
@@ -13,26 +12,25 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
-vector<int> strange_sort_list(vector<int> list) {
-    sort(list.begin(),list.end());
+std::vector<int> strange_sort_list(std::vector<int> list) {
+    std::sort(list.begin(),list.end());
     int index = 0;
     for(int i=0;i<list.size();i++){
         if(index % 3 == 0){
-            reverse(list.begin()+index,list.begin()+(index+1));
+            std::reverse(list.begin()+index,list.begin()+(index+1));
         }
         index++;
     }
     return list;
 }
 
-int main1() {
-    vector<int> list;
+int main() {
+    std::vector<int> list;
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
     list.push_back(4);
     list.push_back(5);
     list.push_back(6);
-    assert(issame(strange_sort_list(list), list));
-    return 0;
+    assert(std::issame(strange_sort_list(list), list));
 }
