1e90415a17524b204a6396af56436484d591720a
beep boop <githubbot@vadim.me>
2024-07-23 09:15:29 +0200

    Solution 16 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/c5dpupn5 Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 1405a523-50ad-47c7-b278-833e045489a6.cpp:3: 1405a523-50ad-47c7-b278-833e045489a6.cpp: In function ‘int main()’: 1405a523-50ad-47c7-b278-833e045489a6.cpp:15:12: error: ‘issame’ was not declared  in this scope    15 |     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));       |            ^~~~~~ 1405a523-50ad-47c7-b278-833e045489a6.cpp: At global scope: 1405a523-50ad-47c7-b278-833e045489a6.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 1405a523-50ad-47c7-b278-833e045489a6.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,15 +3,19 @@
 #include <cassert>
 using namespace std;
 
+bool issame(const vector<int>& a, const vector<int>& b) {
+    return a == b;
+}
+
 vector<int> sort_third(vector<int> l){
     vector<int> res = l;
-    for (int i = 0; i < l.size(); i+=3){
+    for (int i = 0; i < l.size(); i += 3){
         sort(res.begin() + i, min(res.begin() + i + 3, res.end()));
     }
     return res;
 }
 
 int main(){
-    assert(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) == vector<int>({2, 6, 3, 4, 8, 9, 5, 1}));
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
     return 0;
 }
