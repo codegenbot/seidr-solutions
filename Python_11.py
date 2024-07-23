9267998e157652e6c0186cc6720597a3b76f374b
beep boop <githubbot@vadim.me>
2024-07-23 08:38:39 +0200

    Solution 13 of Python/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yngnl4id Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/67b5db33-eaf1-4104-9dd1-59bcacb0d 375.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_11.py
+++ Python_11.py
@@ -1,20 +1,23 @@
+Here is the corrected code:
+
+```
 def string_xor(a: str, b: str) -> str:
-    a = bin(int(a or "0", 2))[2:]
-    b = bin(int(b or "0", 2))[2:]
+    a = bin(int(a or '0', 2))[2:]
+    b = bin(int(b or '0', 2))[2:]
 
     result = ""
     for i in range(max(len(a), len(b))):
         if i < len(a) and i < len(b):
-            if a[-1 - i] == "1" and b[-1 - i] == "0":
+            if a[-1-i] == "1" and b[-1-i] == "0":
                 result = "1" + result
-            elif a[-1 - i] == "0" and b[-1 - i] == "1":
+            elif a[-1-i] == "0" and b[-1-i] == "1":
                 result = "1" + result
             else:
                 result = "0" + result
         else:
             if i < len(a):
-                result = a[-1 - i] + result
+                result = a[-1-i] + result
             elif i < len(b):
-                result = b[-1 - i] + result
+                result = b[-1-i] + result
 
     return result
