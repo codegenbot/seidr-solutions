59c19048e92498990108e39ae2d37694a78f663a
beep boop <githubbot@vadim.me>
2024-07-23 16:33:39 +0200

    Solution 10 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/r8ql53vt Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 56eeb783-8e59-48ee-bc4b-4f6c79af0ec9.cpp:27:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <algorithm>
 
-bool is_sorted(vector<int> lst) {
-    if (lst.size() <= 1)
-        return true;
-
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] >= lst[i - 1])
-            continue;
-        else
-            break;
+bool is_sorted(std::vector<int> lst) {
+    if(lst.size() <= 1) return true;
+    for(int i = 1; i < lst.size(); i++){
+        if(lst[i-1] >= lst[i]) return false;
     }
-    return false;
+    std::vector<int> unique_lst(lst.begin(), std::unique(lst.begin(), lst.end()));
+    return true; // You might need to adjust this condition based on your problem description
 }
 
 int main() {
-    assert (is_sorted({1, 2, 3, 4}) == true);
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+    
+    std::vector<int> vec;
+    
+    for(int i = 0; i < n; i++) {
+        int val;
+        std::cout << "Enter element " << (i+1) << ": ";
+        std::cin >> val;
+        vec.push_back(val);
+    }
+    
+    if(is_sorted(vec)) {
+        std::cout << "The vector is sorted.\n";
+    } else {
+        std::cout << "The vector is not sorted.\n";
+    }
+    
     return 0;
 }
