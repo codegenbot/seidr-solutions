f7228ba7c83ed67731f60ee1769dceab63bada78
beep boop <githubbot@vadim.me>
2024-07-23 16:01:11 +0200

    Solution 15 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/l4zby1s0 Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,10 +1,31 @@
 int main() {
     string expr;
     cin >> expr;
-    if (expr == "t") {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
+    
+    stack<char> stk;
+    for (char c : expr) {
+        if (c == '|') {
+            char op2 = stk.top(); stk.pop();
+            char op1 = stk.top(); stk.pop();
+            if (op1 == 'T' || op2 == 'T') {
+                stk.push('T');
+            } else {
+                stk.push('F');
+            }
+        } else if (c == '&') {
+            char op2 = stk.top(); stk.pop();
+            char op1 = stk.top(); stk.pop();
+            if (op1 == 'T' && op2 == 'T') {
+                stk.push('T');
+            } else {
+                stk.push('F');
+            }
+        } else {
+            stk.push(c);
+        }
     }
+    
+    cout << (stk.top() == 'T' ? "True" : "False") << endl;
+    
     return 0;
 }
