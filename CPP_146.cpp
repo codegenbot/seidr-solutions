40eaa316a9806f7d12f0c45503b6f1d34ab60a9a
beep boop <githubbot@vadim.me>
2024-07-23 17:52:40 +0200

    Solution 6 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/u9x1te1q Prompt: f04e7963-23cc-43f6-bd49-7df03f5b05ca.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums){       |                   ^~~~~~ f04e7963-23cc-43f6-bd49-7df03f5b05ca.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums){       |                          ^~~ In file included from f04e7963-23cc-43f6-bd49-7df03f5b05ca.cpp:11: f04e7963-23cc-43f6-bd49-7df03f5b05ca.cpp: In function ‘int main()’: f04e7963-23cc-43f6-bd49-7df03f5b05ca.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,30 +1,10 @@
-```cpp
 #include <vector>
-#include <iostream>
 
-using namespace std;
-
-int specialFilter(vector<int> nums) {
+int specialFilter(vector<int> nums){
     int count = 0;
-    for (int num : nums) {
-        if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
+    for(int num : nums){
+        if(num > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0){
             count++;
         }
     }
     return count;
-}
-
-int main() {
-    vector<int> numbers;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> x;
-        numbers.push_back(x);
-    }
-    cout << "Number of special numbers: " << specialFilter(numbers) << endl;
-    return 0;
-}
