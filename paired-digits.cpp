1c121cd94c10c6b33f6fc821210db8010fdfa20c
beep boop <githubbot@vadim.me>
2024-07-23 10:27:56 +0200

    Solution 14 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/e0j78rco Prompt: 7eb3e879-f229-4961-9e08-702357ab2c1b.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int pairedDigits(string str) {       |                  ^~~~~~ 7eb3e879-f229-4961-9e08-702357ab2c1b.cpp:3:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 7eb3e879-f229-4961-9e08-702357ab2c1b.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 7eb3e879-f229-4961-9e08-702357ab2c1b.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,8 +1,11 @@
-int pairedDigits(std::string str) {
+#include <string>
+using namespace std;
+
+int pairedDigits(string str) {
     int sum = 0;
     for(int i=0; i<str.length()-1; i++) {
         if(str[i] == str[i+1]) {
-            sum += str[i] - '0';
+            sum += (str[i] - '0');
         }
     }
     return sum;
