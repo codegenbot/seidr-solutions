b112229bd4ea3f609c6442e265d57a817dfac5b3
beep boop <githubbot@vadim.me>
2024-07-23 13:20:54 +0200

    Solution 60 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2rg9bln6 Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const int*; _Tp = int; _Alloc = std:: allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = i nt; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std ::allocator<int>]’ fa81f556-41cd-46c6-95bc-f7502b6ec417.cpp:21:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -4,13 +4,12 @@
 using namespace std;
 
 int findSmallest(vector<int> lst) {
-    vector<int> sorted(lst);
-    sort(sorted.begin(), sorted.end());
-    if (sorted.size() < 2)
+    sort(lst.begin(), lst.end());
+    if (lst.size() < 2)
         return -1;
-    for (int i = 0; i < sorted.size() - 1; i++) {
-        if (sorted[i] != sorted[i + 1])
-            return sorted[i + 1];
+    for (int i = 0; i < lst.size() - 1; i++) {
+        if (lst[i] != lst[i + 1])
+            return lst[i + 1];
     }
     return -1;
 
@@ -19,3 +18,4 @@ int findSmallest(vector<int> lst) {
 int main() {
     cout << findSmallest({-35, 34, 12, -45}) << endl;
     return 0;
+}
