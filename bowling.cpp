1fa1460c2e9119ae1d0beb54f19382684fd4d784
beep boop <githubbot@vadim.me>
2024-07-23 13:24:32 +0200

    Solution 84 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/amuba6ku Prompt: 683552df-8448-4dc7-945a-28ed5ae3e206.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ 683552df-8448-4dc7-945a-28ed5ae3e206.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std;

--- bowling.cpp
+++ bowling.cpp
@@ -1,6 +1,8 @@
 #include <string>
 
-int bowlingScore(const char* s) {
+using namespace std;
+
+int bowlingScore(string s) {
     int score = 0;
     for (int i = 0; i < 10; i++) {
         if (s[i] == 'X') {
@@ -15,7 +17,7 @@ int bowlingScore(const char* s) {
             }
         } else if (s[i] == '/') {
             int j = i + 1;
-            while (j < 11 && !isdigit(s[j])) {
+            while (j < s.length() && !isdigit(s[j])) {
                 j++;
             }
             int pins = s[j] - '0';
