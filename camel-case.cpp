fa3fb4174e43293bc13ccd2987c6e92c101af4fc
beep boop <githubbot@vadim.me>
2024-07-23 10:50:48 +0200

    Solution 62 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/7qodu2v8 Prompt: In file included from /usr/include/c++/11/string:43,                  from f1c1b443-0ce6-4d59-923e-9d44502b1023.cpp:1: /usr/include/c++/11/bits/localefwd.h:118:5: note: candidate: ‘template<class _Ch arT> _CharT std::toupper(_CharT, const std::locale&)’   118 |     toupper(_CharT, const locale&);       |     ^~~~~~~ /usr/include/c++/11/bits/localefwd.h:118:5: note:   template argument deduction/ substitution failed: f1c1b443-0ce6-4d59-923e-9d44502b1023.cpp:21:26: note:   candidate expects 2 argu ments, 1 provided    21 |         result += toupper(words[i].substr(0, 1)) + words[i].substr(1);       |                   ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/cctype:42,                  from /usr/include/c++/11/bits/localefwd.h:42,                  from /usr/include/c++/11/string:43,                  from f1c1b443-0ce6-4d59-923e-9d44502b1023.cpp:1: /usr/include/ctype.h:125:12: note: candidate: ‘int toupper(int)’   125 | extern int toupper (int __c) __THROW;       |            ^~~~~~~ /usr/include/ctype.h:125:25: note:   no known conversion for argument 1 from ‘st d::__cxx11::basic_string<char>’ to ‘int’   125 | extern int toupper (int __c) __THROW;       |                     ~~~~^~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -15,7 +15,7 @@ std::string camelCase(std::string str) {
 
     std::string result = tolower(words[0]);
     for (int i = 1; i < words.size(); i++) {
-        result += std::tolower(words[i].substr(0, 1)) + words[i].substr(1);
+        result += std::toupper(words[i].substr(0, 1)) + words[i].substr(1);
     }
     return result;
 }
