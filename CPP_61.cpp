2cf4e9a8bc7ccde90821d9b145e6f14da742fbe2
beep boop <githubbot@vadim.me>
2024-07-23 12:48:48 +0200

    Solution 44 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9iauoaap Prompt: e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp:1:4: error: ‘cpp’ does not name a type     1 | ```cpp       |    ^~~ In file included from e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp:11: e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp: In function ‘int main()’: e3ddd634-8bc3-497a-a4c4-0363786626ff.cpp:13:18: error: ‘correct_bracketing’ was not declared in this scope    13 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,3 +1,4 @@
+```cpp
 bool correct_bracketing(const std::string& brackets) {
     int count = 0;
     for(int i=0; i<brackets.length(); i++){
@@ -5,9 +6,4 @@ bool correct_bracketing(const std::string& brackets) {
         else if(brackets[i] == ')')if(count > 0)count--; 
     }
     return count==0;
-}
-
-int main() {
-    assert(not (correct_bracketing("()()(()())()))()")));
-    return 0;
 }
