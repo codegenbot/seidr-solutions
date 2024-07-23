2f3d8dc6e79717ac658ad7f4143db1b80ea3a197
beep boop <githubbot@vadim.me>
2024-07-23 08:55:40 +0200

    Solution 3 of mastermind, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/mc26y5k7 Prompt: n before ‘int’     6 |     vector<int> count(6, 0);       |            ^~~ 036a080f-912d-4e25-b9cb-a56a9f9066af.cpp:12:13: error: ‘count’ was not declared in this scope    12 |             count[code[i] - 'A']++;       |             ^~~~~ 036a080f-912d-4e25-b9cb-a56a9f9066af.cpp:18:13: error: ‘count’ was not declared in this scope    18 |         if (count[i] > 0) {       |             ^~~~~ 036a080f-912d-4e25-b9cb-a56a9f9066af.cpp:23:13: error: ‘max’ was not declared in  this scope    23 |     white = max(white, 0);       |             ^~~ 036a080f-912d-4e25-b9cb-a56a9f9066af.cpp:25:5: error: ‘cout’ was not declared in  this scope    25 |     cout << white << endl;       |     ^~~~ 036a080f-912d-4e25-b9cb-a56a9f9066af.cpp:25:22: error: ‘endl’ was not declared i n this scope    25 |     cout << white << endl;       |                      ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
     string code, guess;
     cin >> code >> guess;
-    
+
     int white = 0, black = 0;
     vector<int> count(6, 0);
-    
+
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
@@ -17,17 +17,17 @@ int main() {
             count[guess[i] - 'A']--;
         }
     }
-    
+
     for (int i = 0; i < 6; ++i) {
         if (count[i] > 0) {
             white += count[i];
         }
     }
-    
+
     white = max(white, 0);
-    
+
     cout << white << '\n';
     cout << black << '\n';
-    
+
     return 0;
 }
