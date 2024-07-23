39c5133ec31b67dea3089abf7d316a24164ad41e
beep boop <githubbot@vadim.me>
2024-07-23 14:37:55 +0200

    Solution 51 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/sh3yahhh Prompt:     3 |     getline(cin, input);       |             ^~~ faf4ac6f-97ef-4435-af07-e27427d5ef34.cpp:3:18: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     getline(cin, input);       |                  ^~~~~       |                  int faf4ac6f-97ef-4435-af07-e27427d5ef34.cpp:3:5: error: ‘getline’ was not declared in this scope     3 |     getline(cin, input);       |     ^~~~~~~ faf4ac6f-97ef-4435-af07-e27427d5ef34.cpp:10:13: error: ‘cout’ was not declared i n this scope    10 |             cout << (char)toupper(c);       |             ^~~~ faf4ac6f-97ef-4435-af07-e27427d5ef34.cpp:10:27: error: ‘toupper’ was not declare d in this scope    10 |             cout << (char)toupper(c);       |                           ^~~~~~~ faf4ac6f-97ef-4435-af07-e27427d5ef34.cpp:13:13: error: ‘cout’ was not declared i n this scope    13 |             cout << c;       |             ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -5,7 +5,7 @@ int main() {
     string input;
     getline(cin, input);
     
-    bool capitalize = true;
+    bool capitalize = false;
     for (char &c : input) {
         if (c == '-') {
             capitalize = true;
