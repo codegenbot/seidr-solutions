dc17956226f51d1ab0fc4639b3ada0cc05dcdfaf
beep boop <githubbot@vadim.me>
2024-07-23 10:20:21 +0200

    Solution 64 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tqhh0d5i Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 4044b969-446b-4b9c-af0c-3890a52fce19.cpp:3: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 4044b969-446b-4b9c-af0c-3890a52fce19.cpp:32:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,12 +2,11 @@
 #include <cctype>
 #include <string>
 #include <iostream>
-#include <cassert>
 
 std::string flip_case(std::string str){
     std::string result;
     for(int i=0; i<str.length(); i++){
-        char c = str[i];
+        char c = str[i]; // Remove const here
         if(c >= 'a' && c <= 'z'){
             result += std::toupper(c);
         }
@@ -22,14 +21,11 @@ std::string flip_case(std::string str){
 }
 
 int main() {
-    char buffer[256];
-    std::cin.get(buffer, 256);
-    std::string str = std::string(buffer);
-    if (!(std::cin).good()) {
+    std::string str;
+    std::cout << "Enter a string: ";
+    if (!(std::cin >> str).good()) {
         std::cerr << "Invalid input." << std::endl;
         return 1;
     }
-    std::cout << "Enter a string: ";
     std::cout << "Flipped case: " << flip_case(str) << std::endl;
-    assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
 }
