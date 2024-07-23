63dd6f43a12994bb9ff8f55a7e0dc9642eea9949
beep boop <githubbot@vadim.me>
2024-07-24 00:51:03 +0200

    Solution 87 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/71sehrmu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0e8a8e3c-8d17-4db7-a068-f11fe8bc3 91d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_4.py
+++ Python_4.py
@@ -15,22 +15,31 @@ else:
         while True:
             if cont.lower() != "yes":
                 break
-            new_numbers_str = input("Enter comma-separated float values (e.g., 1,2,3): ")
-
-            if not new_numbers_str or len(new_numbers_str.split(",")) < 2:
-                print(
-                    "Invalid input. Please enter at least two comma-separated float values."
-                )
-            else:
-                new_numbers_list = [float(num) for num in new_numbers_str.split(",")]
-
-                mean = sum(new_numbers_list) / len(new_numbers_list)
-                result = (
-                    sum([abs(num - mean) for num in new_numbers_list])
-                    + result * (len(new_numbers_list) - 1)
-                ) / len(new_numbers_list)
-
             cont = input("Do you want to enter another set of numbers? (yes/no): ")
+            if cont.lower() != "yes":
+                break
+            while True:
+                new_numbers_str = input(
+                    "Enter comma-separated float values (e.g., 1,2,3): "
+                )
+
+                if not new_numbers_str or len(new_numbers_str.split(",")) < 2:
+                    print(
+                        "Invalid input. Please enter at least two comma-separated float values."
+                    )
+                else:
+                    new_numbers_list = [
+                        float(num) for num in new_numbers_str.split(",")
+                    ]
+
+                    mean = sum(new_numbers_list) / len(new_numbers_list)
+                    result = (
+                        sum([abs(num - mean) for num in new_numbers_list])
+                        + result * (len(new_numbers_list) - 1)
+                    ) / len(new_numbers_list)
+
+                    break
+
     except ValueError:
         print("Invalid input. Please enter comma-separated float values.")
 
