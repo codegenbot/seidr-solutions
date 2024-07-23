e0fd3f09363e14b9356eb13727803c2d47e34260
beep boop <githubbot@vadim.me>
2024-07-23 20:52:19 +0200

    Solution 27 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/14zv9x1k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/646d0380-277d-4b73-9af2-858c86a16 f5e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,19 +1,14 @@
-```python
-import re
-
+````
 def bowling_score(frames):
     score = 0
-    roll = [int(x) for x in re.sub('/|-', '', frames)]
-    for i in range(10):
-        if '/' in str(i+1)+str(roll[i]):
-            if '/' in str(i+2)+str(roll[i+1]) and '/' in str(i+3)+str(roll[i+2]):
-                score += 30
-            elif '/' in str(i+2)+str(roll[i+1]):
-                score += 20 + roll[i+2]
-            else:
-                score += 10 + roll[i+1]+roll[i+2]
-        elif '-' in str(i+1)+str(roll[i]):
-            score += 10 + roll[i+1]
-        else:
+    roll = [int(x) for x in frames.replace('/|-', '')]
+    for i in range(0, len(roll), 2):
+        if i+1 < len(roll) and i+2 < len(roll) and roll[i] == 10:
+            score += 10 + roll[i+1]+roll[i+2]
+            i += 2
+        elif i+1 < len(roll) and roll[i] + roll[i+1] <= 10:
             score += roll[i] + roll[i+1]
+        else:
+            score += roll[i] + (10 - roll[i])
     return score
+```
