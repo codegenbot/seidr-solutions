0bd810ed3e88ac2ae5344b9c60c00218ec4f098c
beep boop <githubbot@vadim.me>
2024-07-23 09:56:38 +0200

    Solution 45 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ql9snb5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bede26fd-592b-4efb-a4e1-d94413f5b 8e9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -4,11 +4,9 @@ def find_zero(xs):
     n = len(xs)
     assert n % 2 == 0
     degree = n // 2
-    a = xs[::2]
     coefficients = [0] * (degree + 1)
     for i in range(degree + 1):
-        if i < len(a):
-            coefficients[degree - i] = a[i]
-
-    root = coefficients[-1] / coefficients[0]
+        if i < len(xs):
+            coefficients[i] = xs[i]
+    root = coefficients[0]
     return round(root, 2)
