c4af779682bb727157e1dbb8fcb0d1b92c9a7dce
beep boop <githubbot@vadim.me>
2024-07-23 15:22:03 +0200

    Solution 94 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/78ecuzfn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/121d5bfe-069f-4ae1-9627-0c5ac0ca8 316.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/121d5bfe-069f-4ae1-9627-0c5ac0ca8 316.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,11 +1,7 @@
 def sort_third(l: list):
-    even_indices = {
-        val: i
-        for i, val in enumerate(
-            [x for t in ([y for y in l[i : i + 3]] for i in range(0, len(l), 3))]
-        )
-    }
+    l = [x for t in zip(*[iter(l)] * 3) for x in t]
+    original_indices = {val: i for i, val in enumerate(l)}
     return [
-        val if i % 3 != 0 else min([x for x in l if even_indices[x] % 3 != 0])
+        val if i % 3 != 0 else min([x for x in l if original_indices[x] % 3 != 0])
         for i, val in enumerate(l)
     ]
