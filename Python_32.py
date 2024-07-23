8c9803bf0574cbbe2864e49c63d5319c384bc12c
beep boop <githubbot@vadim.me>
2024-07-23 19:43:40 +0200

    Solution 11 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/umrsxmsp Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,17 +1,12 @@
-import math
-
-
-def poly(xs: list, x: float):
-    return sum([coeff * math.pow(x, i) for i, coeff in enumerate(xs)])
-
-
 def find_zero(xs: list):
     n = len(xs)
-    max_coeff_idx = 0
-    for i in range(1, n):
-        if abs(xs[i]) > abs(xs[max_coeff_idx]):
-            max_coeff_idx = i
-    if xs[max_coeff_idx] < 0:
-        return -xs[1] / xs[0]
+    if n % 2 == 1:
+        return "Error! Number of coefficients must be even."
+
+    a0 = xs[0]
+    an = xs[-1]
+
+    if a0 == 0:
+        return -an / (2 * xs[1])
     else:
-        return -xs[1] / xs[0]
+        return (-an - xs[1]) / (2 * a0)
