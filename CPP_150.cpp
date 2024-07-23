2acdf73370209290bb96c333e33518345e03c8ff
beep boop <githubbot@vadim.me>
2024-07-23 17:54:21 +0200

    Solution 2 of CPP/150, ratio 0.9 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ctu4egsb Prompt: 5e1fa506-2fb2-49c7-a2bf-05e424d24972.cpp: In function ‘int x_or_y(int, int, int) ’: 5e1fa506-2fb2-49c7-a2bf-05e424d24972.cpp:6:9: error: ‘isPrime’ was not declared in this scope     6 |     if (isPrime(n)) {       |         ^~~~~~~ 5e1fa506-2fb2-49c7-a2bf-05e424d24972.cpp:11:1: warning: control reaches end of n on-void function [-Wreturn-type]    11 | }       | ^

--- CPP_150.cpp
+++ CPP_150.cpp
@@ -1,14 +1,5 @@
 #include <cmath>
-
 using namespace std;
-
-int x_or_y(int n, int x, int y) {
-    if (isPrime(n)) {
-        return x;
-    } else {
-        return y;
-    }
-}
 
 bool isPrime(int num) {
     if (num <= 1)
@@ -18,4 +9,12 @@ bool isPrime(int num) {
             return false;
     }
     return true;
+}
+
+int x_or_y(int n, int x, int y) {
+    if (isPrime(n)) {
+        return x;
+    } else {
+        return y;
+    }
 }
