62ceb325028b54118989695a2dc8da38d5130e02
beep boop <githubbot@vadim.me>
2024-07-23 10:53:33 +0200

    Solution 67 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/7qodu2v8 Prompt: bf39c15b-35c6-4215-b6c6-5009c7c0b6ae.cpp:1:6: error: ‘string’ in namespace ‘std’  does not name a type     1 | std::string camelCase(std::string str) {       |      ^~~~~~ bf39c15b-35c6-4215-b6c6-5009c7c0b6ae.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | std::string camelCase(std::string str) {

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,23 +1,3 @@
 #include <string>
 
 std::string camelCase(std::string str) {
-    std::vector<std::string> words;
-    size_t pos = 0, start = 0;
-
-    while ((pos = str.find_first_of(" -")) != std::string::npos) {
-        if (pos > start) {
-            words.push_back(str.substr(start, pos - start));
-        }
-        start = pos + 1;
-    }
-
-    if (start < str.length()) {
-        words.push_back(str.substr(start));
-    }
-
-    std::string result = tolower(words[0]);
-    for (int i = 1; i < words.size(); i++) {
-        result += std::toupper(words[i][0]) + words[i].substr(1);
-    }
-    return result;
-}
