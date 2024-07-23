e308e29461948be98dbce49876fe39fac8b30e51
beep boop <githubbot@vadim.me>
2024-07-23 07:05:38 +0200

    Solution 45 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,49 +1,41 @@
+#include <boost/any.hpp>
 #include <string>
 #include <algorithm>
-#include <boost/any.hpp>
-#include <boost/lexical_cast.hpp>
 
 using namespace std;
+
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any(b);
+        return b;
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        if (boost::lexical_cast<int>(b.convert_to<string>()) > a.convert_to<int>())
+    if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        if (stoi(to_string(a.convert_to<int>())) > stof(to_string(b.convert_to<float>())))
+            return a;
+        else
             return b;
+    }
+    if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max(a, b);
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        if (stof(to_string(b.convert_to<int>())) > stod(to_string(a)))
+            return a;
         else
             return boost::any("None");
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        if (boost::lexical_cast<float>(b.convert_to<string>()) > a.convert_to<float>())
+    if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        if (stod(to_string(a)) > tolf(to_string(b)))
+            return a;
+        else
+            return boost::any("None");
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stod(to_string(b)) > stod(to_string(a)))
             return b;
-        else
-            return boost::any("None");
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        if (boost::lexical_cast<int>(a.convert_to<string>()) > b.convert_to<int>())
+        else if (stod(to_string(a)) > stod(to_string(b)))
             return a;
         else
             return boost::any("None");
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        if (boost::lexical_cast<float>(a.convert_to<string>()) > b.convert_to<float>())
-            return a;
-        else
-            return boost::any("None");
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (boost::lexical_cast<int>(a.convert_to<string>()) > boost::lexical_cast<int>(b.convert_to<string>()))
-            return a;
-        else if (boost::lexical_cast<int>(a.convert_to<string>()) < boost::lexical_cast<int>(b.convert_to<string>()))
-            return b;
-        else
-            return boost::any("None");
-    }
-    else {
-        if (boost::any_cast<float>(a) > boost::any_cast<float>(b))
-            return a;
-        else
-            return boost::any("None");
-    }
+    return max(a, b);
 }
