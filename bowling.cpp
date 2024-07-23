8e9060a4cc64927691a69c0ba3a1ebed3d0397b9
beep boop <githubbot@vadim.me>
2024-07-23 10:44:45 +0200

    Solution 65 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt: 45397109-f5e1-4515-b2da-a941c4017a46.cpp: In function ‘int main()’: 45397109-f5e1-4515-b2da-a941c4017a46.cpp:2:17: error: ‘bowlingScore’ was not dec lared in this scope     2 |     int score = bowlingScore("X/XXXX/8+5,X");       |                 ^~~~~~~~~~~~ 45397109-f5e1-4515-b2da-a941c4017a46.cpp:3:10: error: ‘cout’ is not a member of ‘std’     3 |     std::cout << "The score is: " << score << std::endl;       |          ^~~~ 45397109-f5e1-4515-b2da-a941c4017a46.cpp:1:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?   +++ |+#include <iostream>     1 | int main() { 45397109-f5e1-4515-b2da-a941c4017a46.cpp:3:52: error: ‘endl’ is not a member of ‘std’     3 |     std::cout << "The score is: " << score << std::endl;       |                                                    ^~~~ 45397109-f5e1-4515-b2da-a941c4017a46.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | int main() {

--- bowling.cpp
+++ bowling.cpp
@@ -3,24 +3,32 @@
 using namespace std;
 
 int bowlingScore(string s) {
-    int score = 0;
-    bool firstRoll = true;
-    for (char c : s) {
-        if (c == 'X') {
-            score += 10;
-            firstRoll = false;
-        } else if (c == '/') {
-            if (!firstRoll) score -= 5;
-            firstRoll = false;
-        } else if (c == '+') {
-            score += 5;
-        } else if (isdigit(c)) {
-            int strike = c - '0';
-            if (strike > 9 || !firstRoll) score -= strike;
-            if (!firstRoll) score += strike;
+    int total = 0;
+    bool strike = false;
+    string frame[11];
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '/') {
+            frame[i/2].push_back(s[i]);
+            if (i/2 >= 4 && frame[(i-1)/2][0] == 'X' && frame[(i-1)/2][1] == 'X') 
+                strike = true;
+        } else if (s[i] == '+') {
+            int bonus = stoi(frame[i/2].substr(0,frame[i/2].length()-1));
+            total += 10 + bonus;
+            for (int j = i+1; j < s.length(); j++) 
+                if (s[j] == '/') break;
+            frame[i/2].clear();
+        } else {
+            frame[i/2].push_back(s[i]);
         }
     }
-    return score;
+
+    for (int i = 0; i < 10; i++) {
+        if (!strike) total += stoi(frame[i]) + stoi(frame[i+1]);
+        else if (i == 9) total += 10;
+        strike = false;
+    }
+
+    return total;
 }
 
 int main() {
