25ee16bf1119def7880573912c2390e9235fa366
beep boop <githubbot@vadim.me>
2024-07-23 21:05:02 +0200

    Solution 4 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6vgs0yaa Prompt: e78186f6-c32e-4f0e-826d-2ac23c228711.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from e78186f6-c32e-4f0e-826d-2ac23c228711.cpp:34: e78186f6-c32e-4f0e-826d-2ac23c228711.cpp: In function ‘int main()’: e78186f6-c32e-4f0e-826d-2ac23c228711.cpp:36:13: error: ‘sort_numbers’ was not de clared in this scope    36 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,28 +1,35 @@
-#include <vector>
 #include <algorithm>
-
-using namespace std;
+#include <map>
+#include <vector>
+#include <string>
 
 string sort_numbers(string numbers) {
+    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                {"eight", 8}, {"nine", 9}};
     vector<string> numVec;
-    string temp = "";
-    
-    for (char c : numbers) {
-        if (c == ' ') {
-            numVec.push_back(temp);
-            temp = "";
-        } else {
-            temp += c;
+    string num;
+
+    for (int i = 0; i < numbers.length(); i++) {
+        if (numbers[i] == ' ') {
+            continue;
         }
+        int j = i + 1;
+        while (j <= numbers.length() && !isdigit(numbers[j])) {
+            j++;
+        }
+        num = numbers.substr(i, j - i);
+        i = j;
+        numVec.push_back(num);
     }
-    numVec.push_back(temp);
 
-    sort(numVec.begin(), numVec.end());
-    
-    string result = "";
-    for (string s : numVec) {
-        result += s + " ";
+    sort(numVec.begin(), numVec.end(), [&numMap](string a, string b) {
+        return to_string(numMap.at(a)) < to_string(numMap.at(b));
+    });
+
+    string result;
+    for (const auto& num : numVec) {
+        result += num + " ";
     }
-
     return result.substr(0, result.length() - 1);
 }
