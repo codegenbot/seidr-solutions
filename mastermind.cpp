1eac396ff88e9a2251d3d824b8147936086ec5a5
beep boop <githubbot@vadim.me>
2024-07-23 18:47:18 +0200

    Solution 16 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/dbfw81d6 Prompt: 65af9992-5332-4e9e-84c5-882f7e96359e.cpp:1:1: error: expected unqualified-id bef ore ‘for’     1 | for (int i = 0; i < 6; ++i) {       | ^~~ 65af9992-5332-4e9e-84c5-882f7e96359e.cpp:1:17: error: ‘i’ does not name a type     1 | for (int i = 0; i < 6; ++i) {       |                 ^ 65af9992-5332-4e9e-84c5-882f7e96359e.cpp:1:24: error: expected unqualified-id be fore ‘++’ token     1 | for (int i = 0; i < 6; ++i) {       |                        ^~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,28 +1,11 @@
 #include <iostream>
 #include <algorithm>
 
-int main() {
-    std::string code, guess;
-    std::cin >> code >> guess;
+int white = 0, black = 0;
+int codeFreq[6] = {0}, guessFreq[6] = {0};
 
-    int codeFreq[6] = {0};
-    int guessFreq[6] = {0};
-    int white = 0, black = 0;
-
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            ++black;
-        } else {
-            ++codeFreq[code[i] - 'A'];
-            ++guessFreq[guess[i] - 'A'];
-        }
-    }
-
-    for (int i = 0; i < 6; ++i) {
-        white += std::min(codeFreq[i], guessFreq[i]);
-    }
-
-    std::cout << black << " " << white << std::endl;
-
-    return 0;
+for (int i = 0; i < 6; ++i) {
+    white += min(codeFreq[i], guessFreq[i]);
 }
+
+return white;
