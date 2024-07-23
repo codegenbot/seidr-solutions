cd2b178526caf1e6337abaa62f7fd4632e091704
beep boop <githubbot@vadim.me>
2024-07-23 18:00:45 +0200

    Solution 20 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wz6rl5y0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e1b61d5e-0734-4c2d-84a3-04a67df45 0cd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,11 +1,6 @@
-```Python
 def sorted_list_sum(lst):
-    lst = [word for word in lst if len(word) % 2 == 0]
-    return sorted(set(lst), key=lambda x: (len(x), x))
-
-# Provide input from the user
-user_input = input("Please enter a list of words separated by commas: ")
-
-# Convert user input to a list and process the task
-words_list = [word.strip() for word in user_input.split(",")]
-sorted_list_sum(words_list)
+    lst = list(map(str, input().split()))
+    return sum(
+        int(i)
+        for i in sorted([i for i in lst if len(i) % 2 == 0], key=lambda x: (len(x), x))
+    )
