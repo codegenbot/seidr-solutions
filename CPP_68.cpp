5ddf9cbcbb05f131d3ffac9f7acf68824b68079a
beep boop <githubbot@vadim.me>
2024-07-23 13:17:49 +0200

    Solution 78 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8r8xdew6 Prompt:       |                                  ^~~ da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp:10: da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp: In function ‘int main()’: da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp:20:20: error: ‘pluck’ was not declared in this scope    20 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ da8275d5-ecd3-4c7d-955a-bd1cf2b98eb8.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,21 +1,45 @@
 ```
-#include <vector>
 #include <iostream>
-#include <initializer_list>
+#include <vector>
+#include <utility>
 
-std::vector<std::pair<int, int>> result(const std::vector<int>& arr) {
-    std::vector<std::pair<int, int>> output;
-    for (int i = 0; i < arr.size(); ++i) {
-        output.push_back({arr[i], i});
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) 
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) 
+            return false;
+    }
+    return true;
+}
+
+vector<int> pluck(vector<int> arr) {
+    vector<pair<int, int>> output;
+    for (int i = 0; i < arr.size(); i++) {
+        pair<int, int> p = make_pair(arr[i], i);
+        output.push_back(p);
+    }
+    return output;
+}
+
+vector<pair<int, int>> result(vector<int> arr) {
+    vector<pair<int, int>> output = pluck(arr);
+    for (int i = 0; i < output.size(); i++) {
+        for (int j = i + 1; j < output.size(); j++) {
+            if (!issame({output[i].first}, {output[j].first})) {
+                swap(output[i], output[j]);
+                break;
+            }
+        }
     }
     return output;
 }
 
 int main() {
-    std::vector<int> arr = {1, 2, 3, 4};
-    std::vector<std::pair<int, int>> output = result(arr);
+    vector<int> arr = {1, 2, 3, 4};
+    vector<pair<int, int>> output = result(arr);
     for (auto p : output) {
-        std::cout << "Number: " << p.first << ", Index: " << p.second << std::endl;
+        cout << "Number: " << p.first << ", Index: " << p.second << endl;
     }
     return 0;
 }
