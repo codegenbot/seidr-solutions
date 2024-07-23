7c5a9ddffee29df01db4423e1ca10d2b16234086
beep boop <githubbot@vadim.me>
2024-07-23 18:12:07 +0200

    Solution 31 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/arjvjjnh Prompt: d41fed34-4c04-4a27-9077-467face127e0.cpp:1:1: error: stray ‘`’ in program     1 | ```cpp       | ^ d41fed34-4c04-4a27-9077-467face127e0.cpp:1:2: error: stray ‘`’ in program     1 | ```cpp       |  ^ d41fed34-4c04-4a27-9077-467face127e0.cpp:1:3: error: stray ‘`’ in program     1 | ```cpp       |   ^ d41fed34-4c04-4a27-9077-467face127e0.cpp:2:10: fatal error: boost/lexical_cast.h pp: No such file or directory     2 | #include <boost/lexical_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <boost/lexical_cast.hpp>
 #include <boost/any.hpp>
 #include <string>
@@ -8,25 +9,39 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else if (b.type() == typeid(std::string))
             return b;
-    } 
-    else if (a.type() == typeid(float)) {
-        if (b.type() == typeid(std::string))
-            return a;
-    }
-    else if (a.type() == typeid(std::string)) {
+    } else if (a.type() == typeid(float)) {
         if (b.type() == typeid(int))
             return a;
-        else if (b.type() == typeid(float))
-            return b;
+        else if (b.type() == typeid(std::string)) {
+            std::string str1 = boost::lexical_cast<std::string>(a);
+            std::string str2 = boost::lexical_cast<std::string>(b);
+            if (std::stod(str1) > std::stod(str2))
+                return a;
+            else
+                return b;
+        }
+    } else {
+        if (b.type() == typeid(int)) {
+            std::string str = boost::lexical_cast<std::string>(b);
+            if (std::stod(a.convert_to<std::string>()) > std::stod(str))
+                return a;
+            else
+                return b;
+        } else if (b.type() == typeid(float)) {
+            std::string str1 = boost::lexical_cast<std::string>(a);
+            std::string str2 = boost::lexical_cast<std::string>(b);
+            if (std::stod(str1) > std::stod(str2))
+                return a;
+            else
+                return b;
+        } else {
+            // If both are strings, compare them.
+            std::string str1 = boost::lexical_cast<std::string>(a);
+            std::string str2 = boost::lexical_cast<std::string>(b);
+            if (str1 > str2)
+                return a;
+            else
+                return b;
+        }
     }
-
-    std::string str1 = boost::lexical_cast<std::string>(a);
-    std::string str2 = boost::lexical_cast<std::string>(b);
-
-    if (str1 > str2)
-        return a;
-    else if (str1 < str2)
-        return b;
-    else
-        return typeid(std::string);
 }
