4e765bd1a36018f44c88dce6a1f0bee523feb426
beep boop <githubbot@vadim.me>
2024-07-23 09:12:55 +0200

    Solution 9 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/c5dpupn5 Prompt:       |                           ^~~~~~ d77df871-9f90-402f-aba4-c711d304ce2d.cpp:23:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d77df871-9f90-402f-aba4-c711d304ce2d.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d77df871-9f90-402f-aba4-c711d304ce2d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d77df871-9f90-402f-aba4-c711d304ce2d.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d77df871-9f90-402f-aba4-c711d304ce2d.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ d77df871-9f90-402f-aba4-c711d304ce2d.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -2,13 +2,13 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-std::vector<int> sort_third(std::vector<int> l){
-    std::vector<int> res = l;
-    for (size_t i = 0; i < l.size(); i += 3){
+vector<int> sort_third(vector<int> l){
+    vector<int> res = l;
+    for (int i = 0; i < l.size(); i+=3){
         std::sort(res.begin() + i, std::min(res.begin() + i + 3, res.end()));
     }
     return res;
