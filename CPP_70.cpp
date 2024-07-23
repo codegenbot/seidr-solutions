0d5cb9beccc382f3532006368a6b35e5b93ec31e
beep boop <githubbot@vadim.me>
2024-07-23 10:59:10 +0200

    Solution 4 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4e3v8dte Prompt:       |                    ^~~ d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp:18: d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp: In function ‘int main()’: d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp:28:20: error: ‘strange_sort_list’ was n ot declared in this scope    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ d5655b6b-058a-483c-a56b-30fb633bb8f6.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,16 +1,41 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size() != b.size())
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size())
         return false;
-    for(int i = 0; i<a.size(); i++)
-        if(a[i] != b[i])
+    
+    for (int i = 0; i < a.size(); i++)
+        if (a[i] != b[i])
             return false;
+
     return true;
 }
 
+vector<int> strange_sort_list(vector<int> lst) {
+    vector<int> result;
+
+    while (!lst.empty()) {
+        int minVal = *min_element(lst.begin(), lst.end());
+        lst.erase(remove(lst.begin(), lst.end(), minVal), lst.end());
+
+        if (!lst.empty())
+            minVal = *min_element(lst.begin(), lst.end());
+        lst.erase(remove(lst.begin(), lst.end(), minVal), lst.end());
+
+        result.insert(result.end(), minVal);
+    }
+
+    return result;
+}
+
 int main() {
-    assert(issame({1,2,3}, {1,2,3})); 
+    using namespace std;
+
+    assert(issame(strange_sort_list({111111}), {111111}));
+    
     return 0;
 }
