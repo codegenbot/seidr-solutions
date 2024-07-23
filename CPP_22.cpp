be32276ee4bae3908ebb0ef578c66f0c63753c0f
beep boop <githubbot@vadim.me>
2024-07-23 21:42:21 +0200

    Solution 28 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/o4zk40q6 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 68bf0841-ec50-41e5-bbc3-f45a6e2ea79e.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 68bf0841-ec50-41e5-bbc3-f45a6e2ea79e.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 68bf0841-ec50-41e5-bbc3-f45a6e2ea79e.cpp:11: 68bf0841-ec50-41e5-bbc3-f45a6e2ea79e.cpp: In function ‘int main()’: 68bf0841-ec50-41e5-bbc3-f45a6e2ea79e.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 68bf0841-ec50-41e5-bbc3-f45a6e2ea79e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,12 +1,13 @@
 #include <boost/any.hpp>
-#include <boost/type_traits.hpp>
+#include <vector>
+#include <list>
 
-vector<int> filter_integers(boost::any_list values) {
-    vector<int> result;
+std::vector<int> filter_integers(std::list<boost::any> values) {
+    std::vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<bool>(value)) {
-            int val = boost::any_cast<int>(value);
-            result.push_back(val);
+        if (boost::any_cast<int>(value).good()) {
+            result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
+}
