43e4602b50880c14b128787488d9c81cfb195fec
beep boop <githubbot@vadim.me>
2024-07-23 09:46:53 +0200

    Solution 12 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ebm570ta Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d9fc6cce-0149-4ef4-a925-c66b38716 2a7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- basement.py
+++ basement.py
@@ -1,8 +1,8 @@
-```
+```python
 def basement(vector):
     total = 0
     for i, num in enumerate(vector):
         total += num
         if total < 0:
             return i
-    return -1```
+    return -1
