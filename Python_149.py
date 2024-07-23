cafe9636078ab5dc9a09846fa9180e11ff33da15
beep boop <githubbot@vadim.me>
2024-07-23 12:27:11 +0200

    Solution 9 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/023c30aa-5404-40b2-ac3e-14675dea5 b63.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,2 +1,4 @@
 def sorted_list_sum(lst):
-    return sum(int(word) for word in lst if word.isdigit())
+    return sum(
+        sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x))
+    )
