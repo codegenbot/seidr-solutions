62f67c1231764a756596772ea9e2e37aec9c677d
beep boop <githubbot@vadim.me>
2024-07-23 12:43:03 +0200

    Solution 32 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7bacd73-9f95-4c53-bcdc-cd4e01b23 eb8.py", line 15, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7bacd73-9f95-4c53-bcdc-cd4e01b23 eb8.py", line 11, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,11 @@
 ```
-def double_the_difference():
-    first_list = [[int(i) for i in input("Enter numbers for the list: ").split()], 
-                 [int(i) for i in input("Enter numbers for the list: ").split()]
-    second_list = [int(i) for i in input("Enter numbers for the list: ").split()]
-
+def double_the_difference(lst):
     total = 0
-    for num in first_list[0]:
+    first_list = lst[0]
+    second_list = lst[1]
+    
+    for num in first_list:
         if isinstance(num, int) and num >= 0:
             total += num**2
-
+    
     return abs(total - sum(second_list)) // len(second_list) * 2
