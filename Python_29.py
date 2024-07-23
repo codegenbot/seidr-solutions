2e5e667d678490b2dcb4c3f15fe8a33974fb2c10
beep boop <githubbot@vadim.me>
2024-07-23 10:16:46 +0200

    Solution 51 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/txje1r3n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b15d408c-1a20-486c-944c-a0d445b69 354.py", line 7     while not strings := input(           ^^^^^^^^^^^ SyntaxError: cannot use assignment expressions with expression

--- Python_29.py
+++ Python_29.py
@@ -6,13 +6,17 @@ def filter_by_prefix() -> List[str]:
         try:
             user_input = input("Enter a list of strings (space-separated): ")
             strings = user_input.strip()
-            
+
+            if not strings:
+                print("Please enter valid inputs!")
+
             prefix = input("Enter a prefix: ")
 
             if not prefix:
                 raise ValueError("Please enter valid prefix!")
 
             filtered_strings = [s.strip() for s in strings.split() if s.strip().startswith(prefix)]
+
             return filtered_strings
         except ValueError as e:
             print(f"Error: {e}")
