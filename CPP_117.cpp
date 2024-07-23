2e3c023a00c7fba7aa14da4f946b1bc18487009a
beep boop <githubbot@vadim.me>
2024-07-23 16:22:32 +0200

    Solution 36 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kile2wnq Prompt: his scope    32 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cc0b1dbd-8815-45be-b885-32a26d77a9f5.cpp:32:45: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<string> a,vector<string>b){       |                                             ^ cc0b1dbd-8815-45be-b885-32a26d77a9f5.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ cc0b1dbd-8815-45be-b885-32a26d77a9f5.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main(){       |     ^~~~ In file included from cc0b1dbd-8815-45be-b885-32a26d77a9f5.cpp:31: cc0b1dbd-8815-45be-b885-32a26d77a9f5.cpp: In function ‘int main()’: cc0b1dbd-8815-45be-b885-32a26d77a9f5.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -4,6 +4,17 @@
 #include <string>
 #include <initializer_list>
 
+std::vector<std::string> select_words(std::string s, int k) {
+    std::vector<std::string> words;
+    std::istringstream iss(s);
+    for (std::string word; iss >> word;) {
+        if (word.size() >= k) {
+            words.push_back(word.substr(0, k));
+        }
+    }
+    return words;
+}
+
 bool isSame(std::vector<std::string> a, std::vector<std::string> b){
     if(a.size() != b.size())
         return false;
@@ -14,18 +25,6 @@ bool isSame(std::vector<std::string> a, std::vector<std::string> b){
     return true;
 }
 
-std::vector<std::string> select_words(std::string s, int k) {
-    std::vector<std::string> words;
-    std::istringstream iss(s);
-    for (std::string word; iss >> word;) {
-        if (word.size() >= k) {
-            words.push_back(word.substr(0, k));
-        }
-    }
-    return words;
-
-}
-
 int main(){
-    assert(isSame(select_words("a b c d e f", 1) , std::vector<std::string>({"b", "c", "d", "f"})));
+    assert(isSame(select_words("a b c d e f", 1), std::vector<std::string>({"b", "c", "d", "f"})));
 }
