00b249414fb775314a97a5f906a7db7b55a127a4
beep boop <githubbot@vadim.me>
2024-07-23 11:21:35 +0200

    Solution 6 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1oyb0r05 Prompt: 2b1fe79d-2fdb-4e97-ae9e-ed0d10fa4eec.cpp: In function ‘int search(std::vector<in t>)’: 2b1fe79d-2fdb-4e97-ae9e-ed0d10fa4eec.cpp:7:27: error: ‘count’ was not declared i n this scope     7 |         if (i > 0 && i >= count(i, lst)) {       |                           ^~~~~ 2b1fe79d-2fdb-4e97-ae9e-ed0d10fa4eec.cpp:10:13: error: ‘count’ was not declared in this scope    10 |         if (count(i, lst) > max) {       |             ^~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,36 +1,15 @@
-#include <iostream>
 #include <vector>
-#include <map>
+using namespace std;
 
-int search(std::vector<int> lst) {
-    std::map<int,int> countMap;
+int search(vector<int> lst) {
     int max = 0;
-    for (int i : lst) {
-        if(i > 0){
-            if(countMap.find(i) != countMap.end()){
-                countMap[i]++;
-            } else{
-                countMap[i] = 1;
-            }
-            if(i >= countMap[i]){
-                max = i;
-            }
+    for (auto i : lst) {
+        if (i > 0 && i >= count(lst.begin(), lst.end(), i)) {
+            return i;
+        }
+        if (count(lst.begin(), lst.end(), i) > max) {
+            max = count(lst.begin(), lst.end(), i);
         }
     }
-    return max == 0 ? -1 : max;
-}
-
-int main() {
-    std::vector<int> lst = {};
-    int n;
-    std::cout << "Enter the number of elements in the list: ";
-    std::cin >> n;
-    for(int i=0; i<n; i++){
-        int val;
-        std::cout << "Enter element " << i+1 << ": ";
-        std::cin >> val;
-        lst.push_back(val);
-    }
-    std::cout << "Maximum positive number that has a frequency equal to its value: " << search(lst) << std::endl;
-    return 0;
+    return -1;
 }
