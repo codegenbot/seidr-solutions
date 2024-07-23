1483d4d1172bbb86db0d2c2dc6d77d18c5757b29
beep boop <githubbot@vadim.me>
2024-07-23 18:22:10 +0200

    Solution 74 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/alytn3e3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03a45dd6-6720-42a8-971b-03f302034 9d9.py", line 36, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03a45dd6-6720-42a8-971b-03f302034 9d9.py", line 33, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03a45dd6-6720-42a8-971b-03f302034 9d9.py", line 6, in minPath     (0, 0, [i * j for i in range(1, n + 1) for j in range(1, n + 1)], {i * j: 1} )                                                                        ^ NameError: name 'i' is not defined. Did you mean: 'id'?

--- Python_129.py
+++ Python_129.py
@@ -12,15 +12,15 @@ def minPath(grid, k):
         if len(path) == k + 1:
             res = path
             break
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx, ny = x + dx, y + dy
             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
                 queue.append(
                     (
                         nx,
                         ny,
-                        path + [num for num in count.keys()],
-                        {num: count.get(num, 0) + 1 for num in count}
+                        path + [i * j for i in range(1, n + 1) for j in range(1, n + 1)],
+                        {i * j: count.get(i * j, 0) + 1 for i in range(1, n + 1) for j in range(1, n + 1)},
                     )
                 )
                 visited[nx][ny] = True
