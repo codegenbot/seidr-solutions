e07bccab8358dd067923ff954cf008587fa6ba02
beep boop <githubbot@vadim.me>
2024-07-23 17:00:40 +0200

    Solution 1 of Python/126, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ksz3ksos Prompt: Complete the following code given the task description and function signature.

--- Python_126.py
+++ Python_126.py
@@ -1,9 +1,4 @@
-```
 def is_sorted(lst):
-    for i in range(len(lst)-1):
-        if lst[i] >= lst[i+1]:
-            if len(set(lst)) != len(lst):
-                return False
-            else:
-                return False
-    return True
+    return all(lst[i] <= lst[i + 1] for i in range(len(lst) - 1)) and len(
+        set(lst)
+    ) == len(lst)
