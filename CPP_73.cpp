9c1ca8a3d485babc44c79fb9894682608b871a38
beep boop <githubbot@vadim.me>
2024-07-23 13:38:08 +0200

    Solution 70 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/80nl5k8u Prompt: Program failed

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -2,15 +2,6 @@ int smallest_change(std::vector<int> arr) {
     int n = arr.size();
     assert(n > 0);
 
-    int changes = 0;
-    int last_unique = arr[0];
-    for (int i = 1; i < n; i++) {
-        if (arr[i] == last_unique) {
-            continue;
-        }
-        last_unique = arr[i];
-        changes++;
-    }
-
-    return changes;
+    std::set<int> setArr(arr.begin(), arr.end());
+    return setArr.size() - 1;
 }
