6f982f74ebb2b4e96ce7a59565e0f863d19e12e4
beep boop <githubbot@vadim.me>
2024-07-23 19:06:07 +0200

    Solution 33 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cd9hr4r0 Prompt:       |                                  ^~~ 24a390e0-e08a-43c7-b227-07dc4e982394.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 24a390e0-e08a-43c7-b227-07dc4e982394.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 24a390e0-e08a-43c7-b227-07dc4e982394.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 24a390e0-e08a-43c7-b227-07dc4e982394.cpp:35: 24a390e0-e08a-43c7-b227-07dc4e982394.cpp: In function ‘int main()’: 24a390e0-e08a-43c7-b227-07dc4e982394.cpp:45:20: error: ‘even_odd_count’ was not declared in this scope; did you mean ‘get_even_odd_count’?    45 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 24a390e0-e08a-43c7-b227-07dc4e982394.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -26,5 +26,14 @@ bool is_same(std::vector<int> a, std::vector<int> b) {
     }
     return true;
 }
-
-assert(is_same(get_even_odd_count(0), std::vector<int>(1,0)));
+bool is_same(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
