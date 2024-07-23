35880b8ae656b5d1b9196e8c26c289719af91d0b
beep boop <githubbot@vadim.me>
2024-07-24 01:42:22 +0200

    Solution 89 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jao9uj68 Prompt:       |                                     ^~~~~~ 2c438ab4-369f-4d21-84f5-3bf3835a2ef0.cpp:44:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 2c438ab4-369f-4d21-84f5-3bf3835a2ef0.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 2c438ab4-369f-4d21-84f5-3bf3835a2ef0.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 2c438ab4-369f-4d21-84f5-3bf3835a2ef0.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2c438ab4-369f-4d21-84f5-3bf3835a2ef0.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 2c438ab4-369f-4d21-84f5-3bf3835a2ef0.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,4 +1,3 @@
-```c++
 #include <vector>
 #include <string>
 
@@ -32,7 +31,7 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
     } else if (sum1 > sum2) {
         return lst2;
     } else {
-        return issame(lst1, lst2) ? lst1 : total_match(total_match(lst1, std::move(lst2)), {});
+        return issame(lst1, lst2) ? lst1 : total_match(total_match(lst1, lst2), {});
     }
 }
 
