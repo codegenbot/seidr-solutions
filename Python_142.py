06b225e719e429516cbb1d8759f21d65ffa99b5e
beep boop <githubbot@vadim.me>
2024-07-23 13:44:24 +0200

    Solution 53 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6793f97d-cc1b-4995-b3b0-a07f63c46 ed7.py", line 40, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6793f97d-cc1b-4995-b3b0-a07f63c46 ed7.py", line 10, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6793f97d-cc1b-4995-b3b0-a07f63c46 ed7.py", line 4, in sum_squares     numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split( ))))                                                                ^^^^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_142.py
+++ Python_142.py
@@ -1,7 +1,7 @@
 ```
 def sum_squares(user_input):
     if not isinstance(user_input, str) and not isinstance(user_input, tuple) and not hasattr(user_input, "__iter__"):
-        return
+        return 
     if isinstance(user_input, str):
         numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split())))
     elif isinstance(user_input, tuple) or hasattr(user_input, "__iter__"):
