14a0329dcd1623a13de86a4ed528b0132f8b86af
beep boop <githubbot@vadim.me>
2024-07-23 10:10:27 +0200

    Solution 66 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ec669sg0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0acd1810-5a6b-42fb-b0e6-c8c619044 c29.py", line 9, in <module>     check(longest)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0acd1810-5a6b-42fb-b0e6-c8c619044 c29.py", line 6, in check     assert longest(["x", "yyy", "zzzz", "www", "kkkk", "abc"]) == "zzzz"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_12.py
+++ Python_12.py
@@ -1,16 +1,5 @@
-from typing import List, Optional
-
-
-def longest(strings: List[object]) -> Optional[str]:
+```
+def longest(strings):
     if not strings:
         return None
-    return "".join(
-        map(
-            str,
-            sorted(
-                set([s for s in map(str, strings) if isinstance(s, str)]),
-                key=lambda x: len(x),
-                reverse=True,
-            ),
-        )
-    )
+    return max(map(str, set(strings)), key=len)
