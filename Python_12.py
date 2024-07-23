a885ff7abf8017b9de12a8b8a1b7efff0ceee89f
beep boop <githubbot@vadim.me>
2024-07-23 10:00:17 +0200

    Solution 58 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ec669sg0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5563ab42-07b5-4f44-8f1c-6e1616962 6b0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,1 +1,4 @@
-list_of_strings = ["dog", "cat", "fish", "bird", "elephant"]
+def longest(strings):
+    if not strings:
+        return None
+    return ''.join(map(str, sorted(set(map(str, strings)), key=lambda x: len(x), reverse=True)))
