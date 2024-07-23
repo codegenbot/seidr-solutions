a4e44899e0c699eb42044bf7148730547a4d8511
beep boop <githubbot@vadim.me>
2024-07-23 19:07:12 +0200

    Solution 33 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/dbfw81d6 Prompt:       | ^~~ 6ea6f437-ba8a-4bd6-a8fe-236b21379c39.cpp:3:17: error: ‘i’ does not name a type     3 | for (int i = 0; i < 26; ++i) {       |                 ^ 6ea6f437-ba8a-4bd6-a8fe-236b21379c39.cpp:3:25: error: expected unqualified-id be fore ‘++’ token     3 | for (int i = 0; i < 26; ++i) {       |                         ^~ 6ea6f437-ba8a-4bd6-a8fe-236b21379c39.cpp:6:1: error: expected unqualified-id bef ore ‘for’     6 | for (int i = 0; i < 4; ++i) {       | ^~~ 6ea6f437-ba8a-4bd6-a8fe-236b21379c39.cpp:6:17: error: ‘i’ does not name a type     6 | for (int i = 0; i < 4; ++i) {       |                 ^ 6ea6f437-ba8a-4bd6-a8fe-236b21379c39.cpp:6:24: error: expected unqualified-id be fore ‘++’ token     6 | for (int i = 0; i < 4; ++i) {       |                        ^~ 6ea6f437-ba8a-4bd6-a8fe-236b21379c39.cpp:13:1: error: ‘cout’ does not name a typ e    13 | cout << whitePegs << " " << blackPegs << endl;       | ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,7 +1,29 @@
-std::string code, guess;
-int codeFreq[26] = {0};
-int guessFreq[26] = {0};
-for (int i = 0; i < 4; ++i) {
-    ++codeFreq[code.at(i) - 'A'];
-    ++guessFreq[guess.at(i) - 'A'];
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string code, guess;
+    cin >> code >> guess;
+
+    int codeFreq[26] = {0};
+    int guessFreq[26] = {0};
+
+    for (char c : code) ++codeFreq[c - 'A'];
+    for (char c : guess) ++guessFreq[c - 'A'];
+
+    int whitePegs = 0, blackPegs = 0;
+    for (int i = 0; i < 26; ++i) whitePegs += min(codeFreq[i], guessFreq[i]);
+
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] == guess[i]) {
+            ++blackPegs;
+            --whitePegs;
+        }
+    }
+
+    cout << whitePegs << " " << blackPegs << endl;
+
+    return 0;
 }
