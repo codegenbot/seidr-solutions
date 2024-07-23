928e1a489f65291347431445c95a72058bbdedfb
beep boop <githubbot@vadim.me>
2024-07-23 07:19:04 +0200

    Solution 85 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,33 +1,49 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    if (get<int>(a) > get<int>(b))
-        return a;
-    else if (get<int>(a) < get<int>(b))
+Here is the completed code:
+
+```cpp
+using namespace boost;
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return b;
-    else
-        return boost::any("None");
-} else if ((a.type() == typeid(float) || a.type() == typeid(double)) &&
-           (b.type() == typeid(float) || b.type() == typeid(double))) {
-    if (get<float>(a) > get<float>(b))
-        return a;
-    else if (get<float>(a) < get<float>(b))
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         return b;
-    else
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stof(a.convert_to<string>().replace(",", ".")) > stof(b.convert_to<string>().replace(",", ".")))
+            return a;
+        else if (stof(a.convert_to<string>().replace(",", ".")) < stof(b.convert_to<string>().replace(",", ".")))
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = a.convert_to<int>();
+        int y = b.convert_to<int>();
+        if (x > y)
+            return a;
+        else if (x < y)
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        float x = a.convert_to<float>();
+        float y = b.convert_to<float>();
+        if (x > y)
+            return a;
+        else if (x < y)
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string s = a.convert_to<string>().replace(",", ".");
+        float x = stof(s);
+        int y = b.convert_to<int>();
+        if (x > y)
+            return a;
+        else if (x < y)
+            return boost::any(b);
+        else
+            return boost::any("None");
+    } else {
         return boost::any("None");
-} else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-    string s1 = get<string>(a);
-    string s2 = get<string>(b);
-    size_t pos = 0;
-    while ((pos = s2.find(".")) != string::npos)
-        s2.replace(pos, 1, ",");
-    while ((pos = s2.find(",")) != string::npos)
-        s2.replace(pos, 1, ".");
-    if (stod(s1) > stod(s2))
-        return a;
-    else if (stod(s1) < stod(s2))
-        return b;
-    else
-        return boost::any("None");
-} else {
-    return boost::any("None");
+    }
 }
