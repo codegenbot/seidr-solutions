9d97668de34dbb4a922ab6129d37a23feba897bf
beep boop <githubbot@vadim.me>
2024-07-23 09:15:41 +0200

    Solution 44 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/qzd19lh8 Prompt: 4209d3de-9e68-41b5-a27f-fe2a637d410b.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 4209d3de-9e68-41b5-a27f-fe2a637d410b.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,5 +1,8 @@
+```
 #include <string>
 #include <iostream>
+#include <cassert>
+#include <iterator>
 
 int how_many_times(std::string str, std::string substring) {
     int count = 0;
@@ -16,4 +19,3 @@ int main() {
     assert (how_many_times("john doe", "john") == 1);
     std::cout << how_many_times("hello world", "world") << std::endl;
     return 0;
-}
