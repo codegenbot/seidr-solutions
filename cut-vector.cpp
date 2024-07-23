516fbcd4c62fabbf828d685d082cf81522cf025c
beep boop <githubbot@vadim.me>
2024-07-23 16:31:51 +0200

    Solution 54 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/xc7ir70c Prompt: this scope    24 |     cin >> n;       |     ^~~ b3e41634-f62a-4112-8680-985da164d387.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; b3e41634-f62a-4112-8680-985da164d387.cpp:30:5: error: ‘cout’ was not declared in  this scope    30 |     cout << res.first.size() << endl;       |     ^~~~ b3e41634-f62a-4112-8680-985da164d387.cpp:30:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? b3e41634-f62a-4112-8680-985da164d387.cpp:30:33: error: ‘endl’ was not declared i n this scope    30 |     cout << res.first.size() << endl;       |                                 ^~~~ b3e41634-f62a-4112-8680-985da164d387.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -5,16 +5,36 @@ pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int n = v.size();
     int minDiff = INT_MAX;
     int pos = 0;
-
+    
     for (int i = 1; i <= n / 2; ++i) {
         if (abs(v[i] - v[n-i-1]) < minDiff) {
             minDiff = abs(v[i] - v[n-i-1]);
             pos = i;
         }
     }
-
+    
     vector<int> left(v.begin(), v.begin() + pos);
     vector<int> right(v.begin() + pos, v.end());
-
+    
     return {left, right};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    for (int i = 0; i <= n; ++i) {
+        cin >> v[i];
+    }
+    pair<vector<int>, vector<int>> res = cutVector(v);
+    cout << res.first.size() << endl;
+    for (int num : res.first) {
+        cout << num << " ";
+    }
+    cout << endl;
+    cout << res.second.size() << endl;
+    for (int num : res.second) {
+        cout << num << " ";
+    }
+    return 0;
 }
