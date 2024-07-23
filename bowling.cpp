b7439355aedf99471bb0d9966e128776ec3a0bda
beep boop <githubbot@vadim.me>
2024-07-23 10:20:03 +0200

    Solution 38 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt: d681c5fc-3dcb-4482-8c63-26614eccee0d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,59 +1,35 @@
-#include <iostream>
 #include <string>
-
-using namespace std;
 
 int bowlingScore(string s) {
     int score = 0;
-    int currentRoll1 = 0;
-    int currentRoll2 = 0;
-    bool strike = false;
-    
+    vector<int> frameScores;
+
     for (char c : s) {
-        if (c == '/') {
-            if (!strike) {
-                if (currentRoll1 + currentRoll2 < 10) {
-                    score += 10 - (currentRoll1 + currentRoll2);
-                } else if (currentRoll1 > 0 && currentRoll2 > 0) {
-                    score += currentRoll1 + currentRoll2;
-                } else if (currentRoll1 > 0) {
-                    score += currentRoll1;
-                }
-            } else {
-                strike = false;
-                score += 10;
-                if (s.size() - s.find('/') > 3) {
-                    score += stoi(string(1, c) + string(1, s[s.find('/') + 1]));
-                }
-            }
-            currentRoll1 = 0;
-            currentRoll2 = 0;
-        } else if (c == 'X') {
+        if (c == 'X') {
             score += 10;
-            strike = true;
-            if (s.size() - s.find('X') > 2) {
-                score += stoi(string(1, s[s.find('X') + 1]) + string(1, s[s.find('X') + 2]));
+            if (!frameScores.empty()) {
+                score += *max_element(frameScores.begin(), frameScores.end());
             }
+            frameScores.clear();
+        } else if (c == '/') {
+            int spareRoll1 =stoi(string(1, s[s.find('/')-1]));
+            int spareRoll2 = stoi(string(1, s[s.find('/')-2]));
+            score += 10 - spareRoll1 - spareRoll2;
+            frameScores.clear();
         } else {
-            if (!strike) {
-                currentRoll1++;
-                if (currentRoll1 + currentRoll2 > 10) {
-                    score += 10;
-                    currentRoll1 = 0;
-                    currentRoll2 = 0;
-                } else if (currentRoll1 == 10) {
-                    score += 10;
-                    strike = true;
+            if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9') {
+                int roll = stoi(string(1, c));
+                if (!frameScores.empty()) {
+                    frameScores.back() += roll;
+                } else {
+                    frameScores.push_back(roll);
                 }
-            } else {
-                currentRoll2++;
             }
         }
     }
-    
-    return score;
-}
 
-int main() {
-    cout << bowlingScore("XXX/") << endl;
-}
+    if (frameScores.size() > 0) {
+        score += accumulate(frameScores.begin(), frameScores.end(), 0);
+    }
+
+    return score;
