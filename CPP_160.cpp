e39241bf60172139c54ac92cab4d40fe99a309e6
beep boop <githubbot@vadim.me>
2024-07-23 18:59:49 +0200

    Solution 13 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/g3awebm0 Prompt: in this scope    23 |     assert (do_algebra({string("//"), string("*")}, {7, 3, 4}) == 8);       |     ^~~~~~ f035e81f-89b9-424e-98e4-9062febf7525.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cmath>   +++ |+#include <cassert>     3 | f035e81f-89b9-424e-98e4-9062febf7525.cpp: At global scope: f035e81f-89b9-424e-98e4-9062febf7525.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ f035e81f-89b9-424e-98e4-9062febf7525.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from f035e81f-89b9-424e-98e4-9062febf7525.cpp:26: f035e81f-89b9-424e-98e4-9062febf7525.cpp: In function ‘int main()’: f035e81f-89b9-424e-98e4-9062febf7525.cpp:28:23: error: ‘do_algebra’ cannot be us ed as a function    28 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -12,15 +12,14 @@ int do_algebra(vector<string> operato, vector<int> operand) {
         } else if (operato[i] == "*") {
             result *= operand[i + 1];
         } else if (operato[i] == "//") {
-            result = result / static_cast<int>(operand[i + 1]);
+            result = result / operand[i + 1];
         } else if (operato[i] == "**") {
-            result = pow(result, static_cast<double>(operand[i + 1]));
+            result = pow(result, operand[i + 1]);
         }
     }
     return result;
 }
 
-int test_algebra() {
+int problem_main() {
     assert (do_algebra({string("//"), string("*")}, {7, 3, 4}) == 8);
-    return 0;
 }
