28db48143a056ad0980f22dfc3f43ace3c5b8a07
beep boop <githubbot@vadim.me>
2024-07-23 16:05:06 +0200

    Solution 62 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ghingowb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0db3da5b-7019-4e9b-b73a-200904e18 d41.py", line 1, in <module>     def calculate_average(input_list: List[int]) -> float:                                       ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_25.py
+++ Python_25.py
@@ -1,7 +1,6 @@
-```
 from typing import list
 
-def calculate_average(input_list: list[int]) -> float:
+def calculate_average(input_list: list) -> float:
     if not input_list:
         return 0
     return sum(input_list) / len(input_list)
