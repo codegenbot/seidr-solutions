0c1d04276de0fa5ef1c71b824d81dccc23f7bdc0
beep boop <githubbot@vadim.me>
2024-07-23 09:50:54 +0200

    Solution 7 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/zo2vrh6d Prompt: 6e51c532-3075-427b-95c0-411832775b25.cpp: In function ‘std::pair<std::vector<int >, std::vector<int> > cutVector(std::vector<int>)’: 6e51c532-3075-427b-95c0-411832775b25.cpp:5:20: error: ‘INT_MAX’ was not declared  in this scope     5 |     int min_diff = INT_MAX;       |                    ^~~~~~~ 6e51c532-3075-427b-95c0-411832775b25.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 6e51c532-3075-427b-95c0-411832775b25.cpp:9:20: error: ‘abs’ was not declared in this scope     9 |         int diff = abs(v[i] - v[0]);       |                    ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -7,7 +7,7 @@ using namespace std;
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
     int split_index = 0;
-    
+
     for (int i = 1; i < v.size(); i++) {
         int diff = abs(v[i] - v[0]);
         if (diff <= min_diff) {
@@ -15,9 +15,9 @@ pair<vector<int>, vector<int>> cutVector(vector<int> v) {
             split_index = i;
         }
     }
-    
+
     vector<int> left(v.begin(), v.begin() + split_index);
     vector<int> right(v.begin() + split_index, v.end());
-    
+
     return make_pair(left, right);
 }
