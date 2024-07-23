811f9ba795fe7438e90f8f86d4cda95cc2cb9ff2
beep boop <githubbot@vadim.me>
2024-07-23 13:46:20 +0200

    Solution 57 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ac97558-d0f5-400f-9912-a52df646f a20.py", line 40, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ac97558-d0f5-400f-9912-a52df646f a20.py", line 10, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7ac97558-d0f5-400f-9912-a52df646f a20.py", line 4, in sum_squares     numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split( ))))                                                                ^^^^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_142.py
+++ Python_142.py
@@ -1,7 +1,7 @@
 ```
 def sum_squares(user_input):
     if not isinstance(user_input, str) and not isinstance(user_input, tuple) and not hasattr(user_input, "__iter__"):
-        return 
+        return "Error: Input must be a string or iterable."
     numbers = []
     if isinstance(user_input, str):
         numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split())))
