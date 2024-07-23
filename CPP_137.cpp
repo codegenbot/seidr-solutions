4346267cc0f027e58cb5895df91aaea9f8ec25d5
beep boop <githubbot@vadim.me>
2024-07-23 21:16:19 +0200

    Solution 92 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/arjvjjnh Prompt:    37 | int main(){       |     ^~~~ 511695f1-105a-4946-8ef7-264397e6a06e.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 511695f1-105a-4946-8ef7-264397e6a06e.cpp:36: 511695f1-105a-4946-8ef7-264397e6a06e.cpp: In function ‘int main()’: 511695f1-105a-4946-8ef7-264397e6a06e.cpp:38:13: error: ‘boost’ has not been decl ared    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 511695f1-105a-4946-8ef7-264397e6a06e.cpp:38:29: error: ‘string’ was not declared  in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 511695f1-105a-4946-8ef7-264397e6a06e.cpp:38:37: error: ‘compare_one’ was not dec lared in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -10,12 +10,17 @@ int maxIncreaseKeepingSingle(std::vector<std::vector<int>>& grid) {
     int sum = 0;
     for (int i = 1; i < n - 1; i++) {
         for (int j = 1; j < n - 1; j++) {
-            int leftMax = (i > 0) ? grid[i-1][0] : grid[i][0];
-            int rightMax = (i < n - 1) ? grid[i+1][n-1] : grid[i][n-1];
-            int topMax = (j > 0) ? grid[0][j-1] : grid[0][j];
-            int bottomMax = (j < n - 1) ? grid[n-1][j+1] : grid[n-1][j];
+            int leftMax = grid[i][0];
+            int rightMax = grid[i][n-1];
+            int topMax = grid[0][j];
+            int bottomMax = grid[n-1][j];
             
-            sum += min({leftMax, rightMax}) + min({topMax, bottomMax}) - grid[i][j];
+            if (i > 0) leftMax = grid[i-1][0];
+            if (i < n - 1) rightMax = grid[i+1][n-1];
+            if (j > 0) topMax = grid[0][j-1];
+            if (j < n - 1) bottomMax = grid[n-1][j+1];
+            
+            sum += std::min({leftMax, rightMax}) + std::min({topMax, bottomMax}) - grid[i][j];
         }
     }
     
