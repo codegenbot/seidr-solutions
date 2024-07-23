47e13815a1ed08aa81fb29bb626682deed9696e9
beep boop <githubbot@vadim.me>
2024-07-23 18:26:10 +0200

    Solution 50 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/2cjj34pf Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 68b767e5-d02d-4bff-8a7b-3e0e439f225c.cpp:14:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -1,23 +1,30 @@
+vector<int> result(1);  
 ```cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+
 vector<int> indicesOfSubstring(string text, string target) {
-    vector<int> result;
+    vector<int> result(1);
     int n = text.size(), m = target.size();
     
-    for (int i = 0; i <= n - m) {
+    int i = 0;
+    while (i <= n - m) {
         if (text.substr(i, m) == target)
             result.push_back(i);
-        
-        // KMP search
-        int j = 0, k = 0;
-        while (i + j < n && k < m && text[i + j] == target[k]) {
-            j++, k++;
-            if (k == m) {
-                k = 0;
-                i += j; 
-                break;
-            }
-        }
-    }
+        i += m; 
+   
     
     return result;
+}
+
+int main() {
+    string text, target;
+    cin >> text >> target;
+    vector<int> res = indicesOfSubstring(text, target);
+    for (int x : res) {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
 }
