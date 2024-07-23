826b9b31225206d95f50b09afc50f0a59d816b99
beep boop <githubbot@vadim.me>
2024-07-24 01:10:11 +0200

    Solution 34 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/di8vbgyq Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 0206aec1-c960-4114-aa0d-5298c8e1b726.cpp:27:32:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 string validateTweet(string& tweet) {
-    string trimmedTweet = tweet;
-    while (!trimmedTweet.empty() && trimmedTrimmedTweet.back() == '\n') {
+    string trimmedTweet(tweet);
+    while (!trimmedTweet.empty() && trimmedTweet.back() == '\n') {
         trimmedTweet.erase(trimmedTweet.end() - 1);
     }
     if (tweet.empty()) {
         return "You didn't type anything";
     }
-    int count = static_cast<string::size_type>(trimmedTweet.size());
+    int count = trimmedTweet.size();
     if (count > 140) {
         return "Too many characters";
     }
