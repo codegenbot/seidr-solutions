18e95f7591322e6e09cffbb1efafa8269601eb71
beep boop <githubbot@vadim.me>
2024-07-23 09:09:13 +0200

    Solution 25 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kfhnwzu9 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -13,8 +13,6 @@ def separate_paren_groups(paren_string: str) -> List[str]:
         elif char == ')':
             while stack and (stack[-1] != '(' or not current_group):
                 current_group += stack.pop()
-            if stack: 
-                current_group += stack.pop()
             groups.append(current_group)
             current_group = ''
 
