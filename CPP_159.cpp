f6adc6319f7ef6f5989154e4896c8b6132822b99
beep boop <githubbot@vadim.me>
2024-07-23 21:30:13 +0200

    Solution 81 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7vsawre Prompt:       |                                  ^~~ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:6: 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp: In function ‘int main()’: 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:16:20: error: ‘eat’ was not declared in  this scope    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -2,14 +2,14 @@
 #include <cassert>
 
 std::vector<int> eat(int x, int y, int z) {
-    return {x+y, z};
+    return {x + y, z};
 }
 
-bool isSameVector(std::vector<int> a, std::vector<int> b) {
+bool areSameVectors(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
 int main() {
-    assert(isSameVector(eat(4, 5, 1), {9,0})); 
+    assert(areSameVectors(eat(4, 5, 1), {9,0})); 
     return 0;
 }
