0a293433eb668a0c51340e906503a1769824e948
beep boop <githubbot@vadim.me>
2024-07-23 09:28:22 +0200

    Solution 27 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33gq36ka Prompt: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 38a32d87-18af-4149-ae72-c4c06f865806.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 38a32d87-18af-4149-ae72-c4c06f865806.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 38a32d87-18af-4149-ae72-c4c06f865806.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 38a32d87-18af-4149-ae72-c4c06f865806.cpp:13: 38a32d87-18af-4149-ae72-c4c06f865806.cpp: In function ‘int main()’: 38a32d87-18af-4149-ae72-c4c06f865806.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,8 +1,19 @@
 #include <vector>
 #include <string>
 
+std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix) {
+    std::vector<std::string> result;
+    for (std::string s : strings) {
+        if (s.find(prefix) == 0)
+            result.push_back(s);
+    }
+    return result;
+
+}
+
 bool issame(vector<string> a, vector<string>b){
     return a==b;
+
 }
 
 int main() {
