26814812f07fa1af9449036fa4efc4c57db5e08c
beep boop <githubbot@vadim.me>
2024-07-23 13:38:18 +0200

    Solution 25 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/h1g35jgb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from ce08da5c-4cee-4c24-b8de-d23b17e5c3b1.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
@@ -5,7 +6,7 @@ using namespace std;
 
 vector<int> count_up_to(int n) {
     vector<int> result;
-    for (int i = 1; i <= n; ++i) {
+    for (int i = 2; i <= n; ++i) {
         bool isPrime = true;
         for (int j = 2; j * j <= i; ++j) {
             if (i % j == 0) {
