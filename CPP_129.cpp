18b872882143f14f6195ab28f3ec6365128c714a
beep boop <githubbot@vadim.me>
2024-07-23 18:21:41 +0200

    Solution 49 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7e6wh1b Prompt: _Tp>&) [with _ForwardIterator = std::vector<std::pair<int, int> >*; _Size = long  unsigned int; _Tp = std::vector<std::pair<int, int> >]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = std::vector<std::pair<int, int> >; _Alloc = std::allocator<std::vector<std ::pair<int, int> > >; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = std::vector<std::pair<int, int> >; _Alloc = std::allocator<std::vector<st d::pair<int, int> > >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::vector<std::pair< int, int> > >]’ 22879287-0faa-426a-b522-b87fe295da94.cpp:16:47:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,13 +1,28 @@
-vector<vector<int>> minPath(vector<vector<int>> grid, int k) {
+```cpp
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct cmp {
+    bool operator()(const pair<int, pair<int, int>>& p1, const pair<int, pair<int, int>>& p2) {
+        return p1.second > p2.second;
+    }
+};
+
+vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
     vector<vector<pair<int, int>>> neighbors(n);
     for (int i = 0; i < n; ++i) {
         neighbors[i].resize(n);
         for (int j = 0; j < n; ++j) {
-            if (i > 0) neighbors[i][j].push_back({make_pair(i-1, j), grid[i][j]});
-            if (i < n-1) neighbors[i][j].push_back({make_pair(i+1, j), grid[i][j]});
-            if (j > 0) neighbors[i][j].push_back({make_pair(i, j-1), grid[i][j]});
-            if (j < n-1) neighbors[i][j].push_back({make_pair(i, j+1), grid[i][j]});
+            if (i > 0) neighbors[i][j] = {make_pair(i-1, j), grid[i][j]};
+            else neighbors[i][j] = {{i, j}, grid[i][j]};
+            if (i < n-1) neighbors[i][j].push_back({{i+1, j}, grid[i][j]});
+            if (j > 0) neighbors[i][j].push_back({{i, j-1}, grid[i][j]});
+            else neighbors[i][j].push_back({{i, j}, grid[i][j]});
+            if (j < n-1) neighbors[i][j].push_back({{i, j+1}, grid[i][j]});
         }
     }
 
@@ -33,4 +48,22 @@ vector<vector<int>> minPath(vector<vector<int>> grid, int k) {
     }
 
     return {};
+}
+
+int mainTest() {
+    vector<vector<int>> grid1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    cout << "{";
+    for (int val : minPath(grid1, 3)) {
+        cout << val << " ";
+    }
+    cout << "}\n";
+
+    vector<vector<int>> grid2 = {{5, 9, 3}, {4, 1, 6}, {7, 8, 2}};
+    cout << "{";
+    for (int val : minPath(grid2, 1)) {
+        cout << val << " ";
+    }
+    cout << "}\n";
+
+    return 0;
 }
