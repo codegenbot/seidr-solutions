78f7c68495a43624450374fb3ef80404822d660e
beep boop <githubbot@vadim.me>
2024-07-23 10:36:17 +0200

    Solution 74 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tqhh0d5i Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from fe8d0ec8-3357-4d66-8057-1bbf6b6165b8.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ fe8d0ec8-3357-4d66-8057-1bbf6b6165b8.cpp:6:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,21 +1,20 @@
 #include <string>
-using namespace std;
+#include <cctype>
 
-string flip_case(string str) {
-    string result;
-    for (char c : str) {
-        if (isalpha(c)) {
-            if (isupper(c))
-                result += tolower(c);
-            else
-                result += toupper(c);
-        } else
+std::string flip_case(std::string& s) {
+    std::string result = "";
+    for (char c : s) {
+        if (isupper(c)) {
+            result += tolower(c);
+        } else if (islower(c)) {
+            result += toupper(c);
+        } else {
             result += c;
+        }
     }
     return result;
 }
 
 int main() {
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-    return 0;
 }
