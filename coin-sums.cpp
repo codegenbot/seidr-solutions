29092b1b998ce6ae12f258d70d29b39cb5663ba0
beep boop <githubbot@vadim.me>
2024-07-23 19:46:01 +0200

    Solution 27 of coin-sums, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/lq93r6ya Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- coin-sums.cpp
+++ coin-sums.cpp
@@ -1,18 +1,33 @@
+#include <iostream>
+using namespace std;
+
 int main() {
     int cents;
     cin >> cents;
 
     int quarters = cents / 25;
-    cents %= 25;
+    while (cents >= 25) {
+        quarters++;
+        cents -= 25;
+    }
+
+    int dimes = cents / 10;
+    while (cents >= 10) {
+        dimes++;
+        cents -= 10;
+    }
 
     int nickles = cents / 5;
-    cents %= 5;
+    while (cents >= 5) {
+        nickles++;
+        cents -= 5;
+    }
 
-    int pennies = cents;
+    int pennies = (cents % 5);
 
     cout << quarters << endl;
     cout << nickles << endl;
-    cout << dimes << endl; // You need to calculate the number of dimes
+    cout << dimes << endl;
     cout << pennies << endl;
 
     return 0;
