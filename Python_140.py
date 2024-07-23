a1b71332e26432d60381c46aaa9b5e16d68255a7
beep boop <githubbot@vadim.me>
2024-07-23 14:38:26 +0200

    Solution 92 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a4sa2iva Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4bf9337d-ca6d-470d-bb43-324c545e5 a5e.py", line 10, in <module>     check(fix_spaces("   Hello World!   ")) TypeError: check() missing 1 required positional argument: 'arg'

--- Python_140.py
+++ Python_140.py
@@ -1,2 +1,10 @@
-```
-print(check(fix_spaces, "   Hello World!   "))
+def fix_spaces(s):
+    words = s.split()
+    new_s = ' '.join(words)
+    return new_s
+
+def check(function, arg):
+    result = function(arg)
+    return len(result)
+
+check(fix_spaces, "   Hello World!   ")
