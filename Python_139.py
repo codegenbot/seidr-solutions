8f06787a48aee3ca53796f4632644be0c6b34346
beep boop <githubbot@vadim.me>
2024-07-23 17:25:30 +0200

    Solution 4 of Python/139, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zli8p3zd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc0a0f7b-3a3b-4668-a2e1-ed3c26e07 b35.py", line 14, in <module>     check(special_factorial)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc0a0f7b-3a3b-4668-a2e1-ed3c26e07 b35.py", line 11, in check     assert special_factorial(1) == 1, "Test 1"            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc0a0f7b-3a3b-4668-a2e1-ed3c26e07 b35.py", line 6, in special_factorial     result *= math.factorial(i)               ^^^^ NameError: name 'math' is not defined

--- Python_139.py
+++ Python_139.py
@@ -1,5 +1,10 @@
+import math
+
+
 def special_factorial(n):
+    if n <= 0:
+        return 1
     result = 1
-    for i in range(1, n + 1):
+    for i in range(n, 0, -1):
         result *= math.factorial(i)
     return result
