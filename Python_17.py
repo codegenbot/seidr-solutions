5bfc7d7d56f12ee0ddf66b8be7bbe7dd29d76a06
beep boop <githubbot@vadim.me>
2024-07-24 01:54:44 +0200

    Solution 71 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/mkfx5cx8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/563e0ee0-ede0-477d-8e60-66473dad5 67e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,4 +1,3 @@
-```
 def parse_music(music_string):
     if not isinstance(music_string, str):
         raise ValueError("Invalid input type")
@@ -20,6 +19,9 @@ def parse_music(music_string):
             else:
                 music_list.append(1)
             i += 8
+        elif music_string[i:i+2] == "e|":
+            music_list.append(2)
+            i += 2
         else:
             raise ValueError("Invalid input format")
     return music_list
