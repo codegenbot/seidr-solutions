a0b646b36ad30d990c6a59c2c3a1c68a80d85c71
beep boop <githubbot@vadim.me>
2024-07-23 22:18:04 +0200

    Solution 89 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cd9hr4r0 Prompt:       |             ^~~~~~ 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp:35: 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp: In function ‘int main()’: 5df2a87f-2db9-4c74-be02-34a29f3c1cd0.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -16,12 +16,16 @@ std::vector<int> even_odd_count(int num) {
 }
 
 bool same(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) 
+    if (a.size() != b.size()) {
         return false;
-    for (int i = 0; i < a.size(); ++i) 
-        if (a[i] != b[i]) 
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
+        }
+    }
     return true;
+
 }
 
 int main() {
