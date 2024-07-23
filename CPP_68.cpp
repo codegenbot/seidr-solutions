e17a2066f5c1984dd9187fd1a8ae6b68723ce9e8
beep boop <githubbot@vadim.me>
2024-07-23 23:14:36 +0200

    Solution 11 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/svx0cuc6 Prompt: 1575e35e-45ed-45b3-beb4-29b9f66042cc.cpp: In function ‘std::vector<std::pair<int , int> > pluck(std::vector<int>)’: 1575e35e-45ed-45b3-beb4-29b9f66042cc.cpp:8:26: error: ‘min_element_if’ was not d eclared in this scope     8 |     int smallest_even = *min_element_if([](int a, int b) { return ((a % 2 == 0) && (b % 2 != 0)); }, arr.begin(), arr.end());       |                          ^~~~~~~~~~~~~~ In file included from 1575e35e-45ed-45b3-beb4-29b9f66042cc.cpp:15: 1575e35e-45ed-45b3-beb4-29b9f66042cc.cpp: In function ‘int main()’: 1575e35e-45ed-45b3-beb4-29b9f66042cc.cpp:25:25: error: could not convert ‘pluck( std::vector<int>)()’ from ‘vector<std::pair<int, int>>’ to ‘vector<int>’    25 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ~~~~~^~~~~~~~~~~~~~       |                         |       |                         vector<std::pair<int, int>>

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -5,7 +5,7 @@ vector<pair<int, int>> pluck(vector<int> arr) {
     vector<pair<int, int>> result;
     if (arr.empty()) return {};
 
-    int smallest_even = *min_element(arr.begin(), arr.end(), [](int a, int b){return a%2==0 && b%2!=0;});
+    int smallest_even = *min_element_if([](int x) {return x%2==0;}, arr.begin(), arr.end());
     
     auto it = find(arr.begin(), arr.end(), smallest_even);
     result.push_back({smallest_even, distance(arr.begin(), it)});
