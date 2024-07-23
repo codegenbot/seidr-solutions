18c281b4c21459542fa66c8a72b59bc7cb22d254
beep boop <githubbot@vadim.me>
2024-07-23 13:13:17 +0200

    Solution 38 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt: Program did not receive expected input

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,3 @@
+```
 def sorted_list_sum(lst):
-    lst = [
-        int(i) for i in input("Enter a list of integers separated by spaces: ").split()
-    ]
-    return " ".join(map(str, sorted([i for i in lst if i % 2 == 0])))
+    return " ".join(sorted([int(i) for i in lst if int(i) % 2 == 0]))
