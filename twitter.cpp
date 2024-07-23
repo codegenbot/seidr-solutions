842fca813f1689bf5579afc6f90a9c4e25d6a510
beep boop <githubbot@vadim.me>
2024-07-23 13:17:30 +0200

    Solution 59 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/aqybqb2v Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <cctype>
 #include <iostream>
 #include <string>
@@ -8,19 +9,19 @@ int main() {
     cout << "Enter a tweet: ";
     getline(cin, tweet);
     if (tweet.empty()) {
-        cout << "You didn't type anything";
-    } else {
-        int charCount = 0;
-        for (int i = 0; i < tweet.length(); i++) {
-            if (!isspace(tweet[i])) {
-                charCount++;
-            }
-        }
-        if (charCount > 140) {
-            cout << "Too many characters. Try to keep it under 140, okay?";
-        } else {
-            cout << "Your tweet has " << charCount << " characters";
+        cout << "You didn't type anything" << endl;
+        return 0;
+    }
+    int charCount = 0;
+    for (int i = 0; i < tweet.length(); i++) {
+        if (!isspace(tweet[i])) {
+            charCount++;
         }
     }
+    if (charCount > 140) {
+        cout << "Too many characters" << endl;
+        return 0;
+    }
+    cout << "Your tweet has " << charCount << " characters" << endl;
     return 0;
 }
