83584c59f9b3924c62ac9c442126f65194a4c12a
beep boop <githubbot@vadim.me>
2024-07-23 21:46:27 +0200

    Solution 7 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8dwusk5j Prompt: 5eeba194-9521-4eac-97d9-73925a472488.cpp:11:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 5eeba194-9521-4eac-97d9-73925a472488.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5eeba194-9521-4eac-97d9-73925a472488.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5eeba194-9521-4eac-97d9-73925a472488.cpp:10: 5eeba194-9521-4eac-97d9-73925a472488.cpp: In function ‘int main()’: 5eeba194-9521-4eac-97d9-73925a472488.cpp:20:20: error: ‘unique’ was not declared  in this scope    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 5eeba194-9521-4eac-97d9-73925a472488.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,11 +1,14 @@
-```
+```cpp
 #include <set>
 #include <vector>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return (a.size() == b.size()) && (a==b);
+bool issame(vector<int> v1, vector<int> v2) {
+    if (v1.size() != v2.size()) return false;
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    return v1 == v2;
 }
 
 vector<int> unique(vector<int> l) {
