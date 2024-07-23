db1993f960213730225c1ac24915c860e9497ede
beep boop <githubbot@vadim.me>
2024-07-24 01:11:03 +0200

    Solution 3 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3tlymlm5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8de09e26-a7dc-458b-892c-54e861ff9 ad0.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8de09e26-a7dc-458b-892c-54e861ff9 ad0.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,16 +1,5 @@
 ```
 def sort_third(l: list):
-    result = []
-    i = 0
-    while i < len(l):
-        if l[i] % 3 != 0:
-            result.append(l[i])
-        else:
-            temp = [x for x in l if x % 3 == 0]
-            if temp:
-                last_multiple_of_three = temp[-1]
-                temp.remove(last_multiple_of_three)
-                result.extend(temp)
-                result.append(last_multiple_of_three)
-        i += 1
-    return result
+    third = [i for i in l if i % 3 == 0]
+    first = [i for i in l if i % 3 != 0]
+    return [i for i in sorted([i if i not in third else min(third) for i in (first + third)])]
