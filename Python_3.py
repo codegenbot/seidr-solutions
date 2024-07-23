6932b86893254405ee7f7fe177d59707a2f95bda
beep boop <githubbot@vadim.me>
2024-07-23 13:45:22 +0200

    Solution 23 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s3w5awhe Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/239fcc9a-67d5-4cb2-bb1f-97cf0e80f 448.py", line 1, in <module>     def below_zero(operations: List[int]) -> bool:                                ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_3.py
+++ Python_3.py
@@ -1,5 +1,6 @@
-from typing import list
-def below_zero(operations: list[int]) -> bool:
+from typing import List
+
+def below_zero(operations: List[int]) -> bool:
     balance = 0
     for operation in operations:
         balance += operation
