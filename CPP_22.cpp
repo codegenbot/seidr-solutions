1c089461bbefe7c0a774c36b76ef2ab9d133db49
beep boop <githubbot@vadim.me>
2024-07-23 22:49:21 +0200

    Solution 89 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/o4zk40q6 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -7,47 +7,39 @@ bool issame(std::vector<int> a, std::vector<int> b) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (!issamehelper(a[i], b[i])) {
+        if (!(std::to_string(a[i]) + std::to_string(b[i])).compare("")) {
             return false;
         }
     }
     return true;
 }
 
-bool issamehelper(int a, int b) {
-    std::string str1 = std::to_string(a);
-    std::string str2 = std::to_string(b);
-    
-    return (str1 == str2);
-}
-
 int main() {
-    std::vector<int> vec1;
-    std::cout << "Enter the size of vector 1: ";
-    int n1;
-    std::cin >> n1;
-    for(int i=0; i<n1; ++i) {
-        std::cout << "Enter element " << i+1 << ": ";
-        int x;
-        std::cin >> x;
-        vec1.push_back(x);
-    }
-    
-    std::vector<int> vec2;
-    std::cout << "Enter the size of vector 2: ";
-    int n2;
-    std::cin >> n2;
-    for(int i=0; i<n2; ++i) {
-        std::cout << "Enter element " << i+1 << ": ";
-        int y;
-        std::cin >> y;
-        vec2.push_back(y);
-    }
-    
-    if(issame(vec1,vec2))
-        std::cout << "Vectors are same."<<std::endl;
-    else
-        std::cout << "Vectors are not same."<<std::endl;
+    int n;
+    std::cin >> n;
 
-    return 0;
+    for (int i = 0; i < n; i++) {
+        std::vector<int> a, b;
+        char ch;
+        while ((ch = getchar()) != '\n') {
+            if (ch == ' ') {
+                break;
+            }
+            int x = ch - '0';
+            a.push_back(x);
+        }
+        while ((ch = getchar()) != '\n') {
+            if (ch == ' ') {
+                break;
+            }
+            int x = ch - '0';
+            b.push_back(x);
+        }
+
+        if (!issame(a, b)) {
+            std::cout << "Not equal" << std::endl;
+        } else {
+            std::cout << "Equal" << std::endl;
+        }
+    }
 }
