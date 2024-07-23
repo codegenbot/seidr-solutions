bd8498687a3a65b599a6fe0125dc9021c8c9f14b
beep boop <githubbot@vadim.me>
2024-07-23 09:20:06 +0200

    Solution 48 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8ks3fd7x Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f023453c-1cae-40f1-b2da-4bb9ff423 b15.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f023453c-1cae-40f1-b2da-4bb9ff423 b15.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,14 @@
 def is_palindrome(input_string: str) -> bool:
     return input_string == input_string[::-1]
 
-
 def make_palindrome(input_string: str) -> str:
     if is_palindrome(input_string):
-        return input_string + input_string
-    else:
-        return input_string + input_string[::-1]
+        return input_string
+
+    for i in range(len(input_string)):
+        char = input_string[i]
+        postfix = input_string[i+1:]
+        suffix = ''.join(reversed(postfix))
+        new_input = char + input_string + suffix
+        if is_palindrome(new_input):
+            return new_input
