92316addd9830079752cbc943aa89d7779310f2a
beep boop <githubbot@vadim.me>
2024-07-23 12:05:40 +0200

    Solution 55 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0cww0g70 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9e8abe8e-d28c-48e2-ab5b-0b538333d d40.py", line 10, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9e8abe8e-d28c-48e2-ab5b-0b538333d d40.py", line 7, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9e8abe8e-d28c-48e2-ab5b-0b538333d d40.py", line 3, in solve     return str(ones).zfill(len(bin(N-1))[-2:])                            ~~~~~~~~~~~~~^^^^^ TypeError: 'int' object is not subscriptable

--- Python_84.py
+++ Python_84.py
@@ -1,3 +1,4 @@
+```
 def solve(N):
-    ones = bin(N).count("1")
-    return str(ones).zfill(len(str(bin(N - 1))[-2:]))
+    ones = bin(N).count('1')
+    return str(ones).zfill(len(str(bin(N-1)))[-2:])
