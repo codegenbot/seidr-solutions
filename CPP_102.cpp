a150b50bed7fa503fc9a0c34ea8f61b014e6db92
beep boop <githubbot@vadim.me>
2024-07-23 13:31:53 +0200

    Solution 4 of CPP/102, ratio 0.5 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wwv6blty Prompt: Program failed

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,7 +1,8 @@
 int choose_num(int x, int y){
-    if((x|y)%2==1) return -1; 
     if(x%2==0 && y%2==0){
         return (x>y)?y:x;
+    }else if((x%2!=0&&y%2!=0)){
+        return -1;
     }else{
         for(int i=x; i<=y; i++){
             if(i%2==0)
