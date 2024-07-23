e7dc5e235bc24dd05942e6e44467b4f4de629069
beep boop <githubbot@vadim.me>
2024-07-23 19:14:59 +0200

    Solution 9 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/17agihlg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bdb6407c-1088-4ccd-834b-53ff3af2e 297.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,12 +1,10 @@
-```
 def bowling_score(frames):
     score = 0
     frame = 1
     for i in range(len(frames)):
         if frames[i] == 'X':
+            score += 10
             if frame < 10:
-                score += 10
-                frame += 1
                 if frames[(i + 1):].count('X') > 0:
                     score += int(frames[(i + 2):].index('X') - i - 1) * 10
                 else:
@@ -14,16 +12,7 @@ def bowling_score(frames):
             frame += 1
         elif frames[i] in '1234567890/':
             if frames[i] == '/':
-                if frame < 10:
-                    score += sum(int(x) for x in frames[(i + 1):].lstrip('/').replace('-', '')) * (frame != 10)
-                else:
-                    score += sum(int(x) for x in frames[(i + 1):].lstrip('/').replace('-', '')) * (frame != 10)
-            elif frames[i] == '0':
-                if frame < 10:
-                    score += 10
-                    score += sum(int(x) for x in frames[(i + 2):(i + 3)].lstrip('/').replace('-', '')) * (frame != 10)
-                else:
-                    score += 10
+                score += sum(int(x) for x in frames[(i + 1):].lstrip('/').replace('-', '')) * (frame != 10)
             else:
                 score += int(frames[i]) + sum(int(x) for x in frames[(i + 1)].lstrip('/').replace('-', ''))
         frame += 1
