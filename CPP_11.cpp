9e3c7393dddacf8efe57fc65a3062ce70996478a
beep boop <githubbot@vadim.me>
2024-07-23 20:36:59 +0200

    Solution 8 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9lpi2l56 Prompt: 9c9e29c4-2fcb-4200-a560-401f88a1f698.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string string_xor(string a, string b) {       | ^~~~~~ In file included from 9c9e29c4-2fcb-4200-a560-401f88a1f698.cpp:16: 9c9e29c4-2fcb-4200-a560-401f88a1f698.cpp: In function ‘int main()’: 9c9e29c4-2fcb-4200-a560-401f88a1f698.cpp:18:13: error: ‘string_xor’ was not decl ared in this scope    18 |     assert (string_xor("0101", "0000") == "0101");       |             ^~~~~~~~~~

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,19 +1,16 @@
 #include <string>
 
-char* string_xor(char* a, char* b) {
-    int len1 = strlen(a);
-    int len2 = (b)?strlen(b):0;
-    char* result = new char[len1+1];
-    for(int i = 0; i < len1; i++) {
+string string_xor(string a, string b) {
+    string result;
+    for(int i = 0; i < a.length(); i++) {
         char c1 = a[i];
-        char c2 = (i < len2)?b[i]:'0';
+        char c2 = (i < b.length()) ? b[i] : '0';
         if(c1 == '0' && c2 == '0')
-            result[i] = '0';
+            result += '0';
         else if(c1 == '0' || c2 == '0')
-            result[i] = '1';
+            result += '1';
         else
-            result[i] = '0';
+            result += '0';
     }
-    result[len1] = '\0';
     return result;
 }
