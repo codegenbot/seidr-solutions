f0802fda063bd4b7554aa36c8e8f695589e88ff2
beep boop <githubbot@vadim.me>
2024-07-24 00:17:11 +0200

    Solution 45 of CPP/79, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zwcvnncc Prompt: a56c308c-f380-40db-90f6-721cfcf39c27.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string decimal_to_binary(int decimal){       | ^~~~~~       | stdin In file included from a56c308c-f380-40db-90f6-721cfcf39c27.cpp:15: a56c308c-f380-40db-90f6-721cfcf39c27.cpp: In function ‘int main()’: a56c308c-f380-40db-90f6-721cfcf39c27.cpp:17:13: error: ‘decimal_to_binary’ was n ot declared in this scope    17 |     assert (decimal_to_binary(15) == "db1111db");       |             ^~~~~~~~~~~~~~~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,7 +1,8 @@
-#include <string>
+#include <bits/stdc++.h>
+using namespace std;
 
-std::string decimal_to_binary(int decimal) {
-    std::string binary = "";
+string decimal_to_binary(int decimal){
+    string binary = "";
     while(decimal > 0) {
         if (decimal % 2 == 0)
             binary = "0" + binary;
@@ -9,5 +10,5 @@ std::string decimal_to_binary(int decimal) {
             binary = "1" + binary;
         decimal /= 2;
     }
-    return "db" + binary + "db";
+    return binary;
 }
