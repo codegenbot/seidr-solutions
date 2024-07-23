30214f83f0daefc53f60a680ad024b51aa6047af
beep boop <githubbot@vadim.me>
2024-07-23 17:33:02 +0200

    Solution 4 of Python/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/t9rdiblw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a90dfc6-3a4b-48cd-90e6-97e899d9c 97d.py", line 7     if not file_name[:file_name.index('.')).isalpha():                                           ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- Python_141.py
+++ Python_141.py
@@ -1,11 +1,12 @@
-```
 def file_name_check(file_name):
     if len([c for c in file_name if c.isdigit()]) > 3:
         return 'No'
     if file_name.count('.') != 1:
         return 'No'
-    if not file_name[:file_name.index('.')).isalpha():
+    prefix = file_name[:file_name.index('.')]
+    if not prefix.isalpha():
         return 'No'
-    if file_name[file_name.index('.') + 1:].lower() not in ['txt', 'exe', 'dll']:
+    suffix = file_name[file_name.index('.') + 1:].lower()
+    if suffix not in ['txt', 'exe', 'dll']:
         return 'No'
     return 'Yes'
