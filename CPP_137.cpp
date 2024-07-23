a4dd05501ff5dfda911216d1004bbcacfcaa9bca
beep boop <githubbot@vadim.me>
2024-07-23 07:04:17 +0200

    Solution 34 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,27 @@
-#include <boost/any.hpp>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return (b.convert_to<string>() > to_string(a.convert_to<int())) ? b : "None";
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (a.convert_to<float>() > (int)b.convert_to<int>()) ? a : "None";
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (b.convert_to<string>() > a.convert_to<string>()) ? b : "None";
-    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return (a.convert_to<int>() > b.convert_to<int>()) ? a : "None";
-    } else {
+        return (int)b > (int)a ? b : a;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = boost::any_cast<string>(b);
+        float f = boost::any_cast<float>(a);
+        return f > stof(str) ? b : a;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = boost::any_cast<string>(a), str2 = boost::any_cast<string>(b);
+        return str2.compare(str1) > 0 ? b : a;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int i = boost::any_cast<int>(a);
+        string s = boost::any_cast<string>(b);
+        return stof(s) > i ? b : a;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        float f = boost::any_cast<float>(a);
+        int i = boost::any_cast<int>(b);
+        return f > i ? a : b;
+    }
+    else {
         return "None";
     }
 }
