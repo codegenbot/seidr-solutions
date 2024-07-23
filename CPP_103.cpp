a783086f5e845552ad4c2d09ee1b0f9eca027763
beep boop <githubbot@vadim.me>
2024-07-24 01:58:11 +0200

    Solution 20 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/toc2yftm Prompt: 76d90a42-d0e3-440b-9d00-4a56dd9a5390.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string rounded_avg(int n, int m) {       | ^~~~~~ In file included from 76d90a42-d0e3-440b-9d00-4a56dd9a5390.cpp:22: 76d90a42-d0e3-440b-9d00-4a56dd9a5390.cpp: In function ‘int main()’: 76d90a42-d0e3-440b-9d00-4a56dd9a5390.cpp:24:13: error: ‘rounded_avg’ was not dec lared in this scope    24 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,21 +1,19 @@
 #include <string>
-#include <algorithm>
-using namespace std;
 
-string rounded_avg(int n,int m){
-    if(n > m) return "-1";
-    int sum = 0;
-    for(int i=n; i<=m; i++) {
+string rounded_avg(int n, int m) {
+    if (n > m)
+        return "-1";
+    long sum = 0;
+    for (int i = n; i <= m; i++)
         sum += i;
+    double avg = round((double)sum / ((long)(m - n + 1)));
+    string bin = "";
+    while (avg > 0) {
+        if (avg >= 2)
+            bin += '1', avg -= 2;
+        else
+            bin += '0';
+        avg /= 2;
     }
-    double avg = (double)sum / (m-n+1);
-    avg = floor(avg + 0.5); 
-    string res = "";
-    while(avg > 0){
-        if((int) avg & 1) res.push_back('1');
-        else res.push_back('0');
-        (int) avg >>= 1;
-    }
-    reverse(res.begin(), res.end());
-    return res;
-}
+    reverse(bin.begin(), bin.end());
+    return bin;
