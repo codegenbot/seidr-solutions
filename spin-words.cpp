d026673312757f22f1a5bc8e7086a9b6ac1f8487
beep boop <githubbot@vadim.me>
2024-07-23 18:34:54 +0200

    Solution 42 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ia9na0d3 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 5e39f851-6fa9-4218-9086-fd26fb24e990.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- spin-words.cpp
+++ spin-words.cpp
@@ -6,20 +6,20 @@
 std::string spinWords(std::string str) {
     std::string result = "";
     int start = 0;
-
+    
     for (int i = 0; i <= str.length(); i++) {
         if (i == str.length() || str[i] == ' ') {
             std::string word = str.substr(start, i - start);
-
+            
             if (word.length() >= 5) {
                 std::reverse(word.begin(), word.end());
             }
-
+            
             result += word + " ";
             start = i + 1;
         }
     }
-
+    
     return result.substr(0, result.length() - 1);
 }
 
