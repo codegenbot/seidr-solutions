14b82f8e9e23c02738e39d090edc2c61b910e310
beep boop <githubbot@vadim.me>
2024-07-24 01:11:38 +0200

    Solution 49 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lqjbjsij Prompt: Program did not receive expected input

--- Python_14.py
+++ Python_14.py
@@ -1,10 +1,17 @@
-```
 from typing import List
 
 
 def all_prefixes(string: str) -> List[str]:
-    return [""] + [string[:i].lower() for i in range(1, len(string) + 1)]
-
-
-print(all_prefixes("hello"))
-print(all_prefixes("HELLO"))
+    if string.isupper():
+        return [""] + [string[:i].lower() for i in range(1, len(string) + 1)]
+    elif string.islower():
+        return [""] + [string[:i].lower() for i in range(1, len(string) + 1)]
+    else:
+        upper = "".join(e for e in string if e.isupper())
+        lower = "".join(e for e in string if e.islower())
+        return (
+            [""]
+            + [upper[:i].lower() for i in range(1, len(upper) + 1)]
+            + [""]
+            + [lower[:i].lower() for i in range(1, len(lower) + 1)]
+        )
