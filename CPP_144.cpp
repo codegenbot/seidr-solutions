2d02e1377a26be2435d7d4d212f49c184962bc2c
beep boop <githubbot@vadim.me>
2024-07-23 18:42:45 +0200

    Solution 16 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts52pt24 Prompt: 484e6ce4-ef6f-49d1-ac66-ebe477a9223f.cpp: In function ‘bool simplify(std::string , std::string)’: 484e6ce4-ef6f-49d1-ac66-ebe477a9223f.cpp:12:13: error: cannot resolve overloaded  function ‘find’ based on conversion to type ‘size_t’ {aka ‘long unsigned int’}    12 |     pos = n.find '/';       |             ^~~~ 484e6ce4-ef6f-49d1-ac66-ebe477a9223f.cpp:17:16: error: ‘gcd’ was not declared in  this scope    17 |     return a / gcd(b, a) == c / gcd(d, c);       |                ^~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,20 +1,6 @@
+```
 #include<string>
-#include<cmath> 
 using namespace std;
-
-bool simplify(string x, string n) {
-    int a = 1, b = 1, c = 1, d = 1;
-    
-    size_t pos = x.find('/');
-    a = stoi(x.substr(0, pos));
-    b = stoi(x.substr(pos + 1));
-
-    pos = n.find('/');
-    c = stoi(n.substr(0, pos));
-    d = stoi(n.substr(pos + 1));
-
-    return a / gcd(b, a) == c / gcd(d, c);
-}
 
 int gcd(int a, int b) {
     while (b != 0) {
@@ -23,4 +9,24 @@ int gcd(int a, int b) {
         a = temp;
     }
     return a;
+}
+
+bool simplify(string x, string n) {
+    int a = 1, b = 1, c = 1, d = 1;
+    
+    // Split the fractions into numerator and denominator
+    std::string numerator = x.substr(0, x.find('/'));
+    std::string denominator = x.substr(x.find('/') + 1);
+    
+    a = stoi(numerator);
+    b = stoi(denominator);
+
+    std::string num = n.substr(0, n.find('/'));
+    std::string denom = n.substr(n.find('/') + 1);
+    
+    c = stoi(num);
+    d = stoi(denom);
+
+    // Check if the product is an integer
+    return a / gcd(b, a) == c / gcd(d, c);
 }
