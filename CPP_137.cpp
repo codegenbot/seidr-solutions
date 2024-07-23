206d80cac3dfdc62e729e7a4bfdc6c8043de7a67
beep boop <githubbot@vadim.me>
2024-07-23 17:27:45 +0200

    Solution 3 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/arjvjjnh Prompt: 927a1818-eb88-46f7-8f6b-4e8880fcdb0b.cpp:1:10: fatal error: boost/lexical_cast.h pp: No such file or directory     1 | #include <boost/lexical_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -6,39 +6,39 @@ boost::any compare_one(boost::any a, boost::any b) {
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         return b;
     } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        boost::string str1 = boost::lexical_cast<string>(a);
-        boost::string str2 = boost::lexical_cast<string>(b);
+        string str1 = boost::lexical_cast<string>(a);
+        string str2 = boost::lexical_cast<string>(b);
         if (stod(str1) > stod(str2))
             return a;
         else if (stod(str1) < stod(str2))
             return b;
         else
-            return any("None");
+            return a;
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        boost::string str = boost::lexical_cast<string>(b);
+        string str = boost::lexical_cast<string>(b);
         if (stod(a.cast<string>()) > stod(str))
             return a;
         else if (stod(a.cast<string>()) < stod(str))
             return b;
         else
-            return any("None");
+            return a;
     } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        boost::string str = boost::lexical_cast<string>(b);
+        string str = boost::lexical_cast<string>(b);
         if (stod(a.cast<string>()) > stod(str))
             return a;
         else if (stod(a.cast<string>()) < stod(str))
             return b;
         else
-            return any("None");
+            return a;
     } else {
         // If both are strings, compare them.
-        boost::string str1 = boost::lexical_cast<string>(a);
-        boost::string str2 = boost::lexical_cast<string>(b);
+        string str1 = boost::lexical_cast<string>(a);
+        string str2 = boost::lexical_cast<string>(b);
         if (str1 > str2)
             return a;
         else if (str1 < str2)
             return b;
         else
-            return any("None");
+            return a;
     }
 }
