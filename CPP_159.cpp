a05e7f2ba250502c2fecfd7cf9183aaae57a01a3
beep boop <githubbot@vadim.me>
2024-07-23 22:19:28 +0200

    Solution 98 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7vsawre Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from dbdb7871-20ba-429f-8cce-a839c84beb74.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,23 +1,4 @@
-#include <vector>
-#include <cassert>
-
-using namespace std;
-
-int eat(int a, int b, int c) {
-    return (a + b + c) / 3;
-}
-
-bool areSameVectors(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) 
-        return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
 int main() {
-    assert(eat(4, 5, 1) == 3); 
+    assert(areSameVectors({eat(4, 5, 1)}, vector<int>({0}))); 
     return 0;
 }
