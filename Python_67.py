0d531b43058f4efe9b29d58a5cd5bd2705a0d37f
beep boop <githubbot@vadim.me>
2024-07-23 11:22:53 +0200

    Solution 31 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f53688fe-885c-4bd4-8441-98f2e2055 130.py", line 1, in <module>     n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a'])))     ^ NameError: name 'n' is not defined

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,5 @@
-n = 100
-s = input()
-n -= int("".join(filter(str.isdigit, s))) + int(
-    "".join(filter(str.isdigit, [c for c in s if c != " " and c != "o" and c != "a"]))
-)
+```
+def number_line_jumps(s):
+    n = 5
+    n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a'])))
+    return n
