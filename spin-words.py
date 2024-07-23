5e68b997ac309493c221f5834df3e70563251739
beep boop <githubbot@vadim.me>
2024-07-23 23:39:18 +0200

    Solution 38 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/6ep5hnzg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a7c325f1-a5f4-4b11-9b17-57fae52c6 b2c.py", line 3     return ' '.join((word.strip().lower().translate(str.maketrans("", "", ",.!?" ")).[::-1] if len(word.strip()) >= 5 else word) for word in sentence.split()) ^ SyntaxError: unterminated string literal (detected at line 3)

--- spin-words.py
+++ spin-words.py
@@ -1,2 +1,1 @@
-def spin_words(sentence):
-    return ' '.join((word.strip().lower().translate(str.maketrans("", "", ",.!?")))[::-1] if len(word.strip()) >= 5 else word for word in sentence.split())
+return " ".join((word.strip().lower().translate(str.maketrans("", "", ",.!?"")).[::-1] if len(word.strip()) >= 5 else word) for word in sentence.split())
