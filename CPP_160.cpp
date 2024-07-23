f48cca69394055342e1e9549da63ec617a61ef5d
beep boop <githubbot@vadim.me>
2024-07-23 19:16:25 +0200

    Solution 27 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/g3awebm0 Prompt:       |                ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~ 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp: At global scope: 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp:27: 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp: In function ‘int main()’: 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp:29:23: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    29 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~       |                       |       |                       <brace-enclosed initializer list> 9da5b65f-c839-4297-a8b6-1c67efb26114.cpp:5:41: note:   initializing argument 1 o f ‘int do_algebra(int, std::vector<int>)’     5 | int do_algebra(std::vector<std::string> operato, std::vector<int> operan d) {       |                ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -21,4 +21,4 @@ int do_algebra(std::vector<std::string> operato, std::vector<int> operand) {
     return result;
 }
 
-assert( do_algebra({"/","*"}, {7,3,4}) == 8);
+assert(do_algebra({"/", "*"}, {7, 3, 4}) == 8);
