029940efc6b458808518c42b3ed9ad624c71987b
beep boop <githubbot@vadim.me>
2024-07-23 09:14:46 +0200

    Solution 6 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/sw8yxj3m Prompt: fc749c4b-2d8f-49c4-8d98-0bad58fb700d.cpp:19:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    19 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff fc749c4b-2d8f-49c4-8d98-0bad58fb700d.cpp:22:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    22 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff fc749c4b-2d8f-49c4-8d98-0bad58fb700d.cpp:22:36: error: ‘find_zero’ cannot be use d as a function    22 |         solution = find_zero(coeffs);       |                                    ^ In file included from fc749c4b-2d8f-49c4-8d98-0bad58fb700d.cpp:6: fc749c4b-2d8f-49c4-8d98-0bad58fb700d.cpp:23:21: error: ‘poly’ was not declared i n this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ fc749c4b-2d8f-49c4-8d98-0bad58fb700d.cpp:23:17: error: ‘abs’ was not declared in  this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,1 +1,17 @@
-return std::round(std::fixedpoint::round(x * 100.0) / 100.0);
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double find_zero(vector<double> coeffs, double x) {
+    double denominator = 2 * coeffs[0] * x - coeffs[1];
+    return round(-coeffs[1] / denominator, 2);
+}
+
+int main() {
+    vector<double> coeffs = {1, -3, 2}; // coefficients of the polynomial
+    double x = -coeffs[1]/(2*coeffs[0]); // initial guess for the root
+    // find the zero of the polynomial using the find_zero function
+    double solution = find_zero(coeffs, x);
+    return 0;
+}
