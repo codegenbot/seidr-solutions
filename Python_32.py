e03e944f117102ea95b97de48d9cd04b9f51193c
beep boop <githubbot@vadim.me>
2024-07-23 14:33:23 +0200

    Solution 54 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ek0jmw7w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e3f827e-984d-4aa9-a2f3-04dd20fba 5ce.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -2,9 +2,10 @@
 def find_zero(xs: list):
     if len(xs) < 3:
         raise ValueError("xs must have at least three coefficients")
+
     a = xs[0]
     b = xs[1]
     c = xs[2]
 
-    x = (-b + (b**2 - 4*a*c)**0.5) / (2*a)
+    x = (-b + ((b ** 2 - 4 * a * c) ** 0.5)) / (2 * a)
     return round(x, 10)
