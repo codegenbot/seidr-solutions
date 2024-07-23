bc40e4eca6760ce77f67b0ae22f5a73c7667538f
beep boop <githubbot@vadim.me>
2024-07-23 16:27:24 +0200

    Solution 6 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jjvvtoab Prompt: Program did not receive expected input

--- Python_69.py
+++ Python_69.py
@@ -1,9 +1,10 @@
+```
 def search(lst):
     from collections import Counter
 
     freq = Counter(lst)
     if max(freq) > 0:
-        for num in range(1, max(freq)):
+        for num in range(1, max(freq)+1):
             if freq[num] >= num:
                 return num
     return -1
