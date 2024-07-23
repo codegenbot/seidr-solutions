af6e8c4458c23939a52a7e58f7062addde1258bd
beep boop <githubbot@vadim.me>
2024-07-23 18:26:26 +0200

    Solution 63 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7ctzsi89 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b8de15a0-cf16-4677-ad1f-754b592ab 1c9.py", line 12, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b8de15a0-cf16-4677-ad1f-754b592ab 1c9.py", line 8, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,7 @@
+```
+print(double_the_difference([1,2,3]))
+
 def double_the_difference(lst):
-    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i % 2 != 0)
-    even_sum = sum(i for i in lst if isinstance(i, int))
-    return 2 * odd_sum - even_sum
+    odd_numbers = [i for i in lst if isinstance(i, int) and i % 2 != 0]
+    even_sum = sum(i for i in lst if isinstance(i, int) and i % 2 == 0)
+    return (sum(odd_numbers)) * 2 - even_sum
