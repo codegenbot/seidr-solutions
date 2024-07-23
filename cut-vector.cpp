da4161719a1a1ee0974a953265c0662593f0a9ea
beep boop <githubbot@vadim.me>
2024-07-23 19:58:02 +0200

    Solution 53 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/go1mi1xb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from c4351835-de0e-4839-b3c5-3832568d83f3.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,4 +1,17 @@
 ```cpp
-auto [left, right] = {std::vector<int>(v.begin(), v.begin() + cutIndex),
-                         std::vector<int>(v.begin() + cutIndex, v.end())};
-return left, right;
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int min_diff = INT_MAX;
+    int cut_index = 0;
+
+    for(int i = 1; i < v.size(); i++) {
+        int diff = abs(v[i] - v[0]);
+        if(diff <= min_diff) {
+            min_diff = diff;
+            cut_index = i;
+        }
+    }
+
+    vector<int> left(v.begin(), v.begin() + cut_index);
+    vector<int> right(v.begin() + cut_index, v.end());
+    return {left, right};
+}
