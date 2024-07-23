5197ce8261f74598783f3700fa8397115c04d412
beep boop <githubbot@vadim.me>
2024-07-23 13:37:10 +0200

    Solution 94 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fc010b2-a669-4162-930f-a0e43dfa4 106.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,7 @@
+```Python
 def double_the_difference(lst):
     if len(set(map(int, lst))) != len(lst):
-        return
+        return "Lists must not contain duplicate elements"
     differences = [(int(a) - int(b)) ** 2 for a, b in zip(lst[::2], lst[1::2])]
     return sum(differences) * 8
 
