88b3732ecc20f29cea82fef00ff49db7452df4b5
beep boop <githubbot@vadim.me>
2024-07-23 16:59:17 +0200

    Solution 64 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l17t97y6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be657985-165d-4af6-946d-7fb328d2f 91d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,2 +1,8 @@
+```python
 def solve(N):
-    return bin(sum(int(i) for i in str(bin(N)[2:])))
+    total = sum(int(digit) for digit in str(N))
+    result = bin(total)
+    if total == 0:
+        return '0'
+    else:
+        return result[2:]
