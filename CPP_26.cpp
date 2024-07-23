f51e15b20642200020198450733d5c2396bde173
beep boop <githubbot@vadim.me>
2024-07-23 21:52:39 +0200

    Solution 28 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rig1j6it Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ff40a264-637f-4f21-b1e0-62d307d3e528.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ff40a264-637f-4f21-b1e0-62d307d3e528.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ff40a264-637f-4f21-b1e0-62d307d3e528.cpp:11: ff40a264-637f-4f21-b1e0-62d307d3e528.cpp: In function ‘int main()’: ff40a264-637f-4f21-b1e0-62d307d3e528.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ ff40a264-637f-4f21-b1e0-62d307d3e528.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,14 +1,21 @@
-```cpp
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> remove_duplicates(vector<int> numbers){
-    vector<int> unique_numbers;
-    for(int num : numbers){
-        if(find(unique_numbers.begin(), unique_numbers.end(), num) == unique_numbers.end()){
-            unique_numbers.push_back(num);
+bool isSame(vector<int> v1, vector<int> v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+    for (int i = 0; i < v1.size(); ++i) {
+        if (v1[i] != v2[i]) {
+            return false;
         }
     }
-    return unique_numbers;
+    return true;
+}
+
+vector<int> removeDuplicates(vector<int> numbers) {
+    vector<int> result;
+    for (int num : numbers) {
+        if (find(result.begin(), result.end(), num) == result.end()) {
+            result.push_back(num);
+        }
+    }
+    return result;
 }
