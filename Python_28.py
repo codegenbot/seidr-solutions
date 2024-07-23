8ff869297f8b8c0e1d4c4097241937c04e177b88
beep boop <githubbot@vadim.me>
2024-07-23 14:15:22 +0200

    Solution 18 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7svxfb8n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57ba779b-d93d-4711-830e-0c5dc2923 f89.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,6 +1,4 @@
 from typing import List
 
 def concatenate(strings: List[str]) -> str:
-    user_input = input("Please enter a list of strings separated by commas (no spaces): ")
-    strings = [s.strip() for s in user_input.split(",")]
-    return ''.join(strings)
+    print(' '.join(input().split(',')))
