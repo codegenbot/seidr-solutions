3bb65c33bffab32bff7f78c2203d73b05e14f48c
beep boop <githubbot@vadim.me>
2024-07-23 14:16:04 +0200

    Solution 86 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt: d3d.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4cc0f110-2471-48e3-a0ac-3eb73832d d3d.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4cc0f110-2471-48e3-a0ac-3eb73832d d3d.py", line 2, in sorted_list_sum     even_numbers = [str(num) for num in numbers if isinstance(num, str) and int( num) % 2 == 0]                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4cc0f110-2471-48e3-a0ac-3eb73832d d3d.py", line 2, in <listcomp>     even_numbers = [str(num) for num in numbers if isinstance(num, str) and int( num) % 2 == 0]                                                                             ^^^^ ^^^^ ValueError: invalid literal for int() with base 10: 'aaaa'

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,5 @@
 def sorted_list_sum(numbers):
-    even_numbers = [str(num) for num in numbers if isinstance(num, int) and num % 2 == 0]
+    even_numbers = [
+        num for num in numbers if isinstance(num, str) and int(str(int(num)) % 2 == 0)
+    ]
     return " ".join(sorted(even_numbers))
