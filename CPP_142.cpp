2bbef49c0508a4bad8e8eabf3d11d34fb95b5696
beep boop <githubbot@vadim.me>
2024-07-23 08:55:35 +0200

    Solution 20 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7v867dq2 Prompt: ddb4fabf-3e2b-4324-812c-4e68c52e1346.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<int> lst){       |                 ^~~~~~ ddb4fabf-3e2b-4324-812c-4e68c52e1346.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int sum_squares(vector<int> lst){       |                        ^~~ In file included from ddb4fabf-3e2b-4324-812c-4e68c52e1346.cpp:12: ddb4fabf-3e2b-4324-812c-4e68c52e1346.cpp: In function ‘int main()’: ddb4fabf-3e2b-4324-812c-4e68c52e1346.cpp:14:24: error: ‘sum_squares’ cannot be u sed as a function    14 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,13 +1,18 @@
 #include <vector>
+#include <cassert>
 
-int sum_squares(const std::vector<int>& lst) {
+int sum_squares(const std::vector<int>& lst){
     int sum = 0;
     for(size_t i = 0; i < lst.size(); ++i){
-        if(i % 3 == 0) {
+        if(i % 3 == 0)
             sum += lst[i] * lst[i];
-        } else if(i % 4 == 0) {
+        else if(i % 4 == 0)
             sum += lst[i] * lst[i] * lst[i];
-        }
     }
     return sum;
+}
+
+int main(){
+    assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);
+    return 0;
 }
