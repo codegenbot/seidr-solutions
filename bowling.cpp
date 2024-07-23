f1c2987cef17fe39502e778a32d5092219841efe
beep boop <githubbot@vadim.me>
2024-07-23 13:22:18 +0200

    Solution 79 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/amuba6ku Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,31 +2,39 @@
 
 int bowlingScore(string s) {
     int score = 0;
-    int frame = 1;
-    int roll1, roll2;
-
-    for (int i = 0; i < s.length(); i++) {
+    int i = 0;
+    while (i < s.size()) {
         if (s[i] == 'X') {
-            score += 10 + 10;
-            frame++;
-        } else if (isdigit(s[i])) {
-            if (s[i+1] == '/') {
-                roll1 = s[i] - '0';
-                score += 10 - roll1;
-                i++;
-            } else {
-                roll1 = roll2 = s[i] - '0';
-                score += roll1 + roll2;
-            }
-            frame++;
+            score += 30;
+            i++;
         } else if (s[i] == '/') {
-            roll1 = roll2 = s[i-1] - '0';
-            if (roll1 + 5 < 10) {
-                score += 10;
-            } else {
-                score += roll1 + 10;
+            i++;
+            int j = i + 1;
+            while (j < s.size() && !isdigit(s[j])) {
+                j++;
             }
-            frame++;
+            int pins = s[j] - '0';
+            score += 10 - pins;
+            i = j;
+        } else if (isdigit(s[i])) {
+            int j = i;
+            while (j < s.size() && isdigit(s[j])) {
+                j++;
+            }
+            int pins = 0;
+            for (; i < j; i++) {
+                pins = pins * 10 + (s[i] - '0');
+            }
+            if (i == j) {
+                score += pins;
+            } else if (j - i > 1) {
+                score += max(pins, 10);
+                if (pins < 10) {
+                    score += pins;
+                }
+            } else {
+                score += pins;
+            }
         }
     }
     return score;
