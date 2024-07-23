ecaa88234e2f1785bd5aad5c3e83e4d93144de18
beep boop <githubbot@vadim.me>
2024-07-23 20:19:11 +0200

    Solution 54 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7vsawre Prompt:       |             ^~~~~~ 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp:25: 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp: In function ‘int main()’: 4962fcd9-4903-465e-a0d6-348ea666b0b5.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
 
-bool sameVectors(std::vector<int> a, std::vector<int> b) {
+bool isSame(std::vector<int> a, std::vector<int> b) {
     return (a[0] == b[0]) && (a[1] == b[1]);
 }
 
-std::vector<int> eatFood(int number, int need, int remaining) {
+std::vector<int> eat(int number, int need, int remaining) {
     int total = number + need;
     int left = total - 1000;
     if (left < 0) {
@@ -18,6 +18,6 @@ std::vector<int> eatFood(int number, int need, int remaining) {
 }
 
 int main() {
-    assert(sameVectors(eatFood(4, 5, 1), std::vector<int>({9, 0})));
+    assert(isSame({eat(4, 5, 1)[0], eat(4, 5, 1)[1]}, {9, 0}));
     return 0;
 }
