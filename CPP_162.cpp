be5a3a8ac5919bcbd40242c453462c6d2ece6938
beep boop <githubbot@vadim.me>
2024-07-23 18:36:14 +0200

    Solution 75 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kc6xlx4i Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,14 +1,12 @@
-if(text.empty()){
-    return "None";
+if(text.empty()) {
+        return "None";
+    }
+    unsigned char result[MD5_DIGEST_LENGTH];
+    MD5((unsigned char*)text.c_str(), text.length(), result);
+    std::ostringstream sout;
+    sout<<std::hex<<std::setfill('0');
+    for(long long c: result) {
+        sout<<std::setw(2)<<(long long)c;
+    }
+    return sout.str();
 }
-
-unsigned char digest[MD5_DIGEST_LENGTH];
-MD5_CTX ctx;
-MD5_Init(&ctx);
-MD5_Update(&ctx, text.c_str(), text.length());
-MD5_Final(digest, &ctx);
-char mdString[33];
-for(int i = 0; i < 16; i++){
-    sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
-}
-return mdString;
