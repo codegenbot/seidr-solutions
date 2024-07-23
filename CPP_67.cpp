7157caca00a03d871559fe59a53ae72f9ac4492a
beep boop <githubbot@vadim.me>
2024-07-23 23:10:00 +0200

    Solution 4 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2p00522t Prompt: Program failed

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,19 +1,21 @@
-#include<stdio.h>
-#include<string>
-using namespace std;
-
 int fruit_distribution(string s, int n) {
-    size_t pos = 0;
-    int apples = 0, oranges = 0;
+    int total_apples = 0;
+    int total_oranges = 0;
+    string temp;
 
-    while ((pos = s.find(" apples", pos)) != string::npos) {
-        apples = stoi(s.substr(0, pos).erase().substr(0, s.find(" ")).erase());
-        break;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            if (temp.find("apples") != string::npos)
+                temp.erase(0, temp.find(" ") + 1);
+                total_apples = stoi(temp);
+            else if (temp.find("oranges") != string::npos)
+                temp.erase(0, temp.find(" ") + 1);
+                total_oranges = stoi(temp);
+            temp = "";
+        } else {
+            temp += s[i];
+        }
     }
 
-    while ((pos = s.find(" oranges", pos)) != string::npos) {
-        oranges = stoi(s.substr(pos + 8).erase().substr(0, s.find(" ") - 1));
-    }
-
-    return n - apples - oranges;
+    return n - total_apples - total_oranges;
 }
