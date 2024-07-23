f2dea30558b37c2e6085c92b9a29d3949dda8547
beep boop <githubbot@vadim.me>
2024-07-23 10:59:49 +0200

    Solution 24 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/edc67a6e-549b-49e1-acaf-dae6bd4ea 4d3.py", line 3     n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a']))))                                                        ^ SyntaxError: unmatched ')'

--- Python_67.py
+++ Python_67.py
@@ -1,4 +1,4 @@
-def fruit_distribution(s,n):
+def fruit_distribution(s, n):
     s = s.lower()
-    n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(lambda x: str.isdigit(x), [c for c in s if c != ' ' and c != 'o' and c != 'a'])))
+    n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a']))))
     return n
