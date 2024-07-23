6a40aa090f0ec9120b593cdaa092749416195e3f
beep boop <githubbot@vadim.me>
2024-07-23 10:01:24 +0200

    Solution 54 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ql9snb5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6e2dec63-456d-43b5-8780-2b9cea0f9 4ce.py", line 32, in <module>     check(find_zero)           ^^^^^^^^^ NameError: name 'find_zero' is not defined

--- Python_32.py
+++ Python_32.py
@@ -1,18 +1,14 @@
-```Python
-def find_zero(nums):
-    return sum(num for num in nums if num == 0)
-
-def sum_even_and_odd(nums):
-    even_sum = 0
-    odd_sum = 0
-    
-    for num in nums:
-        if num % 2 == 0:
-            even_sum += num
+def find_largest_sum_of_subarray(arr):
+    max_ending_here = max_so_far = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] > max_ending_here + arr[i]:
+            max_ending_here = arr[i]
+            max_so_far = max_ending_here
         else:
-            odd_sum += num
-            
-    return even_sum, odd_sum
+            max_ending_here += arr[i]
+        if max_ending_here > max_so_far:
+            max_so_far = max_ending_here
+    return max_so_far
 
-nums = [1, 2, 3, 4]
-print(find_zero(nums))
+
+print(find_largest_sum_of_subarray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
