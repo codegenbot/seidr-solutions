56c01e7342563f7b2dd974469e0f0a61cd77adca
beep boop <githubbot@vadim.me>
2024-07-23 09:28:53 +0200

    Solution 58 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8ks3fd7x Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be5d6537-b2b5-426b-a0dd-27ccc42f4 dd8.py", line 14, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be5d6537-b2b5-426b-a0dd-27ccc42f4 dd8.py", line 11, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,6 @@
 def make_palindrome(string: str) -> str:
-    for i in range(len(string)):
-        postfix = string[i:]
-        if postfix == postfix[::-1]:
-            return string + postfix[::-1]
-    return string + string[::-1]
+    for i in range(len(string), 0, -1):
+        postfix = string[:i]
+        if is_palindrome(postfix + postfix[::-1]):
+            return postfix + postfix[::-1]
+    return string
