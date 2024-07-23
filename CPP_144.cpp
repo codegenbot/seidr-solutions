b6a460021256c61a76b304feee924fd763ef63ea
beep boop <githubbot@vadim.me>
2024-07-23 19:38:49 +0200

    Solution 26 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts52pt24 Prompt: 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp:1:26: error: ‘denominator1’ was not dec lared in this scope     1 | int productDenominator = denominator1 * denominator2;       |                          ^~~~~~~~~~~~ 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp:1:41: error: ‘denominator2’ was not dec lared in this scope     1 | int productDenominator = denominator1 * denominator2;       |                                         ^~~~~~~~~~~~ 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp:2:1: error: expected unqualified-id bef ore ‘if’     2 | if (productDenominator == 0)       | ^~ 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp:4:1: error: expected unqualified-id bef ore ‘return’     4 | return productNumerator % productDenominator == 0;       | ^~~~~~ In file included from 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp:6: 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp: In function ‘int main()’: 336e808c-5bc1-4d8b-919d-9f3d3964990a.cpp:8:13: error: ‘simplify’ was not declare d in this scope     8 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,42 +1,28 @@
+Here is the complete function:
+
+```cpp
 #include <string>
-using namespace std;
+#include <algorithm>
 
-bool simplify(string x, string n) {
-    int numerator1 = 0, denominator1 = 0;
-    int numerator2 = 0, denominator2 = 0;
-
-    size_t pos = x.find('/');
-    if (pos == string::npos || stoi(x.substr(0, pos)) >stoi(x.substr(pos + 1))) {
-        // improper fraction
+bool simplify(const std::string& numeratorStr, const std::string& denominatorStr) {
+    int numerator = 0, denominator1 = 0, denominator2 = 0;
+    
+    size_t numeratorPos = numeratorStr.find('/');
+    size_t denominator1Pos = denominatorStr.find('/');
+    
+    std::string numeratorPart = numeratorStr.substr(0, numeratorPos);
+    std::string denominator1Part = denominatorStr.substr(0, denominator1Pos);
+    std::string denominator2Part = denominatorStr.substr(denominator1Pos + 1);
+    
+    if (numeratorPart.empty() || denominator1Part.empty() || denominator2Part.empty())
         return false;
-    }
-    numerator1 = stoi(x.substr(0, pos));
-    denominator1 = stoi(x.substr(pos + 1));
-
-    pos = n.find('/');
-    if (pos == string::npos || stoi(n.substr(0, pos)) >stoi(n.substr(pos + 1))) {
-        // improper fraction
-        return false;
-    }
-    numerator2 = stoi(n.substr(0, pos));
-    denominator2 = stoi(n.substr(pos + 1));
-
-    int productNumerator = numerator1 * numerator2;
-    if (denominator2 != 0) {
-        int productDenominator = denominator1 * denominator2;
-
-        // find the GCD of numerators and denominators
-        int gcd = __gcd(abs(productNumerator), abs(productDenominator));
-        
-        return productDenominator / gcd == productNumerator / gcd;
-    }
-    return false;
-}
-
-// Function to calculate GCD
-int __gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    else
-        return __gcd(b, a % b);
+    
+    numerator = std::stoi(numeratorPart);
+    denominator1 = std::stoi(denominator1Part);
+    denominator2 = std::stoi(denominator2Part);
+    
+    int productDenominator = denominator1 * denominator2;
+    if (productDenominator == 0)
+        return false; 
+    return numerator % productDenominator == 0;
 }
