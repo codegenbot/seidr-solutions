abcfe5e783db9e3fefbc3dec53ed60912892efcf
beep boop <githubbot@vadim.me>
2024-07-23 21:38:44 +0200

    Solution 17 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0w182pf9 Prompt: c7b5f4aa-252c-41fa-a3ac-6f3b207fc509.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string filp_case(string str){       | ^~~~~~ In file included from c7b5f4aa-252c-41fa-a3ac-6f3b207fc509.cpp:14: c7b5f4aa-252c-41fa-a3ac-6f3b207fc509.cpp: In function ‘int main()’: c7b5f4aa-252c-41fa-a3ac-6f3b207fc509.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -4,7 +4,7 @@ string filp_case(string str){
     string result = "";
     for(int i=0; i<str.length(); i++){
         char c = str[i];
-        if(c>=97&&c<=122){ // Check if character is lowercase
+        if(c>=97&&c<=122){ 
             result += toupper(c); 
         }else{
             result += tolower(c);
