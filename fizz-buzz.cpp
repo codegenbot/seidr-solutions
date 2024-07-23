6582fe259772b98a00476d6b603da4536f87bdd4
beep boop <githubbot@vadim.me>
2024-07-23 15:16:16 +0200

    Solution 25 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fofqfh0u Prompt: header ‘<iostream>’; did you forget to ‘#include <iostream>’? 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:25:35: error: ‘endl’ was not declared i n this scope    25 |                 cout << "Buzz" << endl;       |                                   ^~~~ 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:25:35: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’? 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:27:17: error: ‘cout’ was not declared i n this scope    27 |                 cout << numberToWords(i) << endl;       |                 ^~~~ 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:27:17: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:27:45: error: ‘endl’ was not declared i n this scope    27 |                 cout << numberToWords(i) << endl;       |                                             ^~~~ 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:27:45: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’? 623ef5ea-6db7-429f-9012-926f60fd48d3.cpp:29:5: warning: no return statement in f unction returning non-void [-Wreturn-type]    29 |     }       |     ^

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -2,28 +2,31 @@
 #include <iostream>
 using namespace std;
 
-string numberToWords(int num) {
-    if (num == 0) return "0";
-    vector<string> lessThan20 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    vector<string> tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+class Solution {
+public:
+    string numberToWords(int num) {
+        if (num == 0) return "0";
+        vector<string> lessThan20 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        vector<string> tens = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
-    if (num < 20) {
-        return lessThan20[num];
-    } else if (num < 100) {
-        return tens[num / 10] + (num % 10 == 0 ? "" : " " + lessThan20[num % 10]);
+        if (num < 20) {
+            return lessThan20[num];
+        } else if (num < 100) {
+            return tens[num / 10] + (num % 10 == 0 ? "" : " " + lessThan20[num % 10]);
+        }
     }
-}
 
-string fizzBuzz(int n) {
-    for (int i = 1; i <= n; i++) {
-        if (i % 3 == 0 && i % 5 == 0)
-            cout << "FizzBuzz" << endl;
-        else if (i % 3 == 0)
-            cout << "Fizz" << endl;
-        else if (i % 5 == 0)
-            cout << "Buzz" << endl;
-        else
-            cout << numberToWords(i) << endl;
+    string fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                cout << "FizzBuzz" << endl;
+            else if (i % 3 == 0)
+                cout << "Fizz" << endl;
+            else if (i % 5 == 0)
+                cout << "Buzz" << endl;
+            else
+                cout << numberToWords(i) << endl;
+        }
+        return "";
     }
-    return "";
-}
+};
