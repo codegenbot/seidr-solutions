13e19e768626e9fe24de587f8915836ade4082a4
beep boop <githubbot@vadim.me>
2024-07-23 14:26:47 +0200

    Solution 94 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eed1426f-dd95-4aa9-ac0c-259d2c2ba f92.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,2 @@
 def sorted_list_sum(numbers):
-    odd_numbers = [str(num) for num in numbers if isinstance(num, int) and num % 2 != 0]
-    odd_numbers.sort()
-    return " ".join(odd_numbers)
+    return ' '.join(str(int(num)) for num in numbers if isinstance(num, int))
