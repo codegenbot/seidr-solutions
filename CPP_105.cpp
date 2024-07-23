e6212788fdfbf9431ea5e34c08ce25479a2b48ba
beep boop <githubbot@vadim.me>
2024-07-24 01:38:52 +0200

    Solution 20 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1g16sxbf Prompt:       |                              ^~~~~~ 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp:50: 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp: In function ‘int main()’: 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 520119c8-a6a8-4365-a9e4-6f3ef7be88ab.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,37 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) 
-        return false;
-    
-    for (int i = 0; i < a.size(); ++i)
-        if (a[i] != b[i]) 
-            return false;
-
-    return true;
+```cpp
+bool issame(vector<string> a, vector<string>b) {
+    return a == b;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<int> temp;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9)
-            temp.push_back(i);
-    }
-    
-    sort(temp.begin(), temp.end());
-    reverse(temp.begin(), temp.end());
-    
     vector<string> result;
-    for (int i : temp) {
-        string s = to_string(i);
-        map<char, string> m = {{'1', "One"}, {'2', "Two"}, {'3', "Three"}, 
-                                {'4', "Four"}, {'5', "Five"}, {'6', "Six"}, 
-                                {'7', "Seven"}, {'8', "Eight"}, {'9', "Nine"}};
-        result.push_back(m.at(s[0]));
+    for (int i : arr) {
+        if (i >= 1 && i <= 9) {
+            string str = "";
+            switch (i) {
+                case 1:
+                    str = "One";
+                    break;
+                case 2:
+                    str = "Two";
+                    break;
+                case 3:
+                    str = "Three";
+                    break;
+                case 4:
+                    str = "Four";
+                    break;
+                case 5:
+                    str = "Five";
+                    break;
+                case 6:
+                    str = "Six";
+                    break;
+                case 7:
+                    str = "Seven";
+                    break;
+                case 8:
+                    str = "Eight";
+                    break;
+                case 9:
+                    str = "Nine";
+                    break;
+            }
+            result.push_back(str);
+        }
     }
-    
     return result;
 }
