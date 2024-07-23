57f6f76b258f60aff2f55f2b2ce05b3bfe1337ff
beep boop <githubbot@vadim.me>
2024-07-23 11:07:25 +0200

    Solution 86 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt:  in this scope    20 | int getRoll(string s, int i) {       |             ^~~~~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:20:23: error: expected primary-expressi on before ‘int’    20 | int getRoll(string s, int i) {       |                       ^~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:20:28: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | int getRoll(string s, int i) {       |                            ^ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:33:18: error: ‘string’ was not declared  in this scope    33 | int getBonusRoll(string s, int i) {       |                  ^~~~~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:33:28: error: expected primary-expressi on before ‘int’    33 | int getBonusRoll(string s, int i) {       |                            ^~~ 2c7de2e1-d5cb-49c7-8735-95744497dcec.cpp:33:33: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | int getBonusRoll(string s, int i) {       |                                 ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,12 +1,10 @@
-#include <string>
+#include <iostream>
 using namespace std;
 
-int getRoll(string, int);
-int getBonusRoll(string, int);
+int score = 0;
+string s = "X/X/5+7-3-1/2+4/X/2-8-X/2+6/X";
 
 int main() {
-    int score = 0;
-    string s = "X/X/5+7-3-1/2+4/X/2-8-X/2+6/X";
     for(int i = 0; i < s.length(); i++) {
         char c = s[i];
         if (c == '/') {
@@ -26,28 +24,18 @@ int main() {
 }
 
 int getRoll(string s, int i) {
-    if (s[i+1] == '/') {
-        return 10;
-    } else {
-        for(int j = i + 1; j < s.length(); j++) {
-            if (!isdigit(s[j])) {
-                return min(j - i, 10);
-            }
-            if (j >= s.length() || !isdigit(s[j])) {
-                return min(j - i, 10);
-            }
+    if(i >= s.length() - 2) return 10; 
+    for(int j = i + 1; ; j++) { 
+        if (!isdigit(s[j])) {
+            return min(j - i, 10);
         }
     }
 }
 
 int getBonusRoll(string s, int i) {
-    for(int j = i + 2; ; j++) {
-        if (!isdigit(s[j])) {
-            if (s[j] == '+' || s[j] == '-') {
-                return min(j - i - 1, 9);
-            } else {
-                return 0;
-            }
+    for(int j = i + 2; ; j++) { 
+        if (!isdigit(s[j]) && (s[j] == '+' || s[j] == '-')) {
+            return min(j - i - 1, 9);
         }
     }
 }
