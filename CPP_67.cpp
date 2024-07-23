20be066a1ef2cd4c566403941bfe4fbae09f2a36
beep boop <githubbot@vadim.me>
2024-07-23 23:18:56 +0200

    Solution 8 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2p00522t Prompt: Program failed

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -9,9 +9,9 @@ int fruit_distribution(string s, int n) {
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == ' ') {
             if (temp.find("apples") != string::npos)
-                total_apples = stoi(temp.substr(0, temp.find(" ")).erase());
+                total_apples = stoi(temp.substr(0, temp.find(" ")).c_str());
             else if (temp.find("oranges") != string::npos)
-                total_oranges = stoi(temp.substr(0, temp.find(" ")).erase());
+                total_oranges = stoi(temp.substr(0, temp.find(" ")).c_str());
             temp = "";
         } else {
             temp += s[i];
