17cd01a09f06cb0db512b7c8c1d70a781a25a830
beep boop <githubbot@vadim.me>
2024-07-24 01:47:43 +0200

    Solution 24 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/walwymsv Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ef86c294-35b8-4038-a4c5-94179fd74 d6f.py", line 16, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ef86c294-35b8-4038-a4c5-94179fd74 d6f.py", line 13, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,12 +1,10 @@
-```
 def fizz_buzz(n: int):
-    count = 0
     for i in range(1, n + 1):
         output = (
             "FizzBuzz"
-            if i % 15 == 0
-            else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
+            if i % 15 == 0 
+            else "Fizz" if i % 3 == 0 
+            else "Buzz" if i % 5 == 0 
+            else str(i)
         )
         print(output)
-        count += 1 if output != str(i) else 0
-    return 0
