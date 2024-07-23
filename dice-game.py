9239a9cd06fd5643c907abf16e244a413300b696
beep boop <githubbot@vadim.me>
2024-07-23 20:01:50 +0200

    Solution 80 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/vkjupje3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6e03824-340c-417d-914a-e0cb3cd1c 8cf.py", line 1     ```     ^ SyntaxError: invalid syntax

--- dice-game.py
+++ dice-game.py
@@ -1,8 +1,6 @@
 ```
-def """dice_game(n, m):
-    if m == 1:
-        return 1
-    elif n < m:
+def dice_game(n, m):
+    if n < m:
         return 0
     else:
-        return round(1 - (m)/(n), 6)"""
+        return round((n-m)/(n*m), 6)
