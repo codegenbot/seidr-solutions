10f58fa1d00f279a5e07ba2934d65ecffb077bf5
beep boop <githubbot@vadim.me>
2024-07-23 17:41:17 +0200

    Solution 87 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/gdumde1a Prompt:  = std::allocator<std::pair<const char, int> >; std::__enable_if_t<std::is_same< _Val, typename std::iterator_traits<_InputIterator>::value_type>::value> = void;  typename std::iterator_traits<_InputIterator>::value_type = std::pair<const cha r, int>]’ /usr/include/c++/11/bits/stl_map.h:232:36:   required from ‘std::map<_Key, _Tp, _Compare, _Alloc>::map(std::initializer_list<std::pair<const _Key, _Tp> >, const  _Compare&, const allocator_type&) [with _Key = char; _Tp = int; _Compare = std: :less<char>; _Alloc = std::allocator<std::pair<const char, int> >; std::map<_Key , _Tp, _Compare, _Alloc>::allocator_type = std::allocator<std::pair<const char, int> >]’ ef9d4d18-e9a4-470e-a4bb-3555d2c49f2b.cpp:36:58:   required from here /usr/include/c++/11/bits/stl_tree.h:594:15: error: no matching function for call  to ‘operator new(sizetype, std::_Rb_tree_node<std::pair<const char, int> >*&)’   594 |               ::new(__node) _Rb_tree_node<_Val>;       |               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘std::_Rb_tree<char,  std::pair<const char, int>, std::_Select1st<std::pair<const char, int> >, std:: less<char>, std::allocator<std::pair<const char, int> > >::_Link_type’ {aka ‘std ::_Rb_tree_node<std::pair<const char, int> >*’} to ‘std::align_val_t’

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <string>
 #include <map>
@@ -15,7 +14,7 @@ bool issame(map<char, int> a, map<char, int> b) {
 }
 
 map<char, int> histogram(const string& str) {
-    map<char, int> result = {};
+    map<char, int> result;
 
     for (char c : str) {
         if (!isalpha(c)) continue; 
@@ -32,7 +31,7 @@ map<char, int> histogram(const string& str) {
 }
 
 int main() {
-    assert(issame(histogram("a"), map<char, int>()));
+    assert(issame(histogram("a"), map<char, int>())); 
     cout << "Success!" << endl;
     return 0;
 }
