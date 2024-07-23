d7a6a0788dceb89f44938a941fb801786e5daccd
beep boop <githubbot@vadim.me>
2024-07-23 10:56:36 +0200

    Solution 76 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s9lxoq63 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/553d0b8e-aec5-41f2-9e33-da82e18da 2e3.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/553d0b8e-aec5-41f2-9e33-da82e18da 2e3.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -10,4 +10,4 @@ def fizz_buzz(n: int):
             result.append("Buzz" if i % 3 != 0 else "FizzBuzz")
         else:
             result.append(str(i))
-    return sum(1 for x in result if x == "FizzBuzz")
+    return len([i for i in result if i == "FizzBuzz"])
