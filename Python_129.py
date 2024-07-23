25dcc26bc60eaff8289f843b152590b4904944bd
beep boop <githubbot@vadim.me>
2024-07-23 22:11:31 +0200

    Solution 1 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ty2cq1m8 Prompt: Complete the following code given the task description and function signature.

--- Python_129.py
+++ Python_129.py
@@ -1,26 +1,24 @@
+Here is the solution:
+
 def minPath(grid, k):
     n = len(grid)
-    visited = [[False] * n for _ in range(n)]
-    path = []
-
-    def dfs(i, j, curr_path):
-        nonlocal path
-        if len(curr_path) == k:
-            path = curr_path[:]
-            return True
-
-        for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+    m = [[i * n + j for j in range(n)] for i in range(n)]
+    memo = {}
+    
+    def dfs(i, j, path, cnt):
+        if (i, j) in memo:
+            return memo[(i, j)]
+        
+        if cnt == k:
+            return path
+        
+        res = []
+        for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             ni, nj = i + x, j + y
-            if 0 <= ni < n and 0 <= nj < n and not visited[ni][nj]:
-                visited[ni][nj] = True
-                if dfs(ni, nj, curr_path + [grid[ni][nj]]):
-                    return True
-                visited[ni][nj] = False
-
-        return False
-
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = True
-            if dfs(i, j, [grid[i][j]]):
-                return path
+            if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in memo:
+                res.append(dfs(ni, nj, path + [m[ni][nj]], cnt + 1))
+        
+        memo[(i, j)] = min(res, key=lambda x: x if len(x) == k else float('inf'))
+        return memo[(i, j)]
+    
+    return dfs(0, 0, [], 0)[1:]
