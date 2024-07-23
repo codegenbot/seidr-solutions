2d768215f2e031143079653542a5a2faaf9dea91
beep boop <githubbot@vadim.me>
2024-07-23 09:59:41 +0200

    Solution 62 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/sw8yxj3m Prompt: fb7e7a99-3914-408f-802a-ee885d69d3d9.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ fb7e7a99-3914-408f-802a-ee885d69d3d9.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,3 +1,32 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <initializer_list>
+
+using namespace std;
+
+double poly(vector<double> xs, double x){
+    double sum=0;
+    int i;
+    for (i=0;i<xs.size();i++)
+    {
+        sum+=xs[i]*pow(x,i);
+    }
+    return sum;
+}
+
+double find_zero(vector<double> xs){
+    if(xs.size() % 2 != 0) return -1; 
+    double x = 1.0; 
+    while(abs(poly(xs, x)) > 1e-9){ 
+        x -= poly(xs, x)/poly({xs[1], xs.back()}, x); 
+    }
+    ostringstream oss;
+    oss << fixed << setprecision(6) << x;
+    return stod(oss.str());
+}
+
 int main() {
     vector<double> xs = {1, -6, 11, -6};
     double zero = find_zero(xs);
