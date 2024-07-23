3276cd465255fc5c8706e4916ba4792f5d48873c
beep boop <githubbot@vadim.me>
2024-07-23 09:53:48 +0200

    Solution 40 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6uj4cw5r Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -12,10 +12,12 @@ def parse_music(music_string: str) -> List[int]:
             music_list.append(1)
             i += 4
         elif music_string[i] == 'o':
-            if i + 1 <= len(music_string) and music_string[i:i+2] == 'oou':
+            if i + 5 <= len(music_string) and music_string[i:i+6] == 'oooou':
                 music_list.append(2)
-                i += 2
+                i += 6
             else:
                 music_list.append(4)
                 i += 1
+        else:
+            break
     return music_list
