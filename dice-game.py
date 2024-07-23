5b65a163d331fab3c55a983d43e7d5e7e8531cce
beep boop <githubbot@vadim.me>
2024-07-23 19:39:23 +0200

    Solution 40 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/vkjupje3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90b5aead-7905-4ad2-b47d-63570e9f4 356.py", line 1     ```     ^ SyntaxError: invalid syntax

--- dice-game.py
+++ dice-game.py
@@ -1,6 +1,5 @@
 def dice_game(n, m):
     total_prob = 0
     for i in range(1, min(m, n)):
-        if i < m and i < n:
-            total_prob += (n - i) * (m - i) / (n * m)
+        total_prob += (n - i) * (m - i) / (n * m)
     return round(total_prob,6)
