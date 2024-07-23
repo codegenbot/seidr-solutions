c7e745a5974dc875f613926fb2b10e3bd778efde
beep boop <githubbot@vadim.me>
2024-07-23 11:22:27 +0200

    Solution 30 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f53688fe-885c-4bd4-8441-98f2e2055 130.py", line 1, in <module>     n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a'])))     ^ NameError: name 'n' is not defined

--- Python_67.py
+++ Python_67.py
@@ -1,2 +1,5 @@
-n = int(input())  
-n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a'])))
+n = 100
+s = input()
+n -= int("".join(filter(str.isdigit, s))) + int(
+    "".join(filter(str.isdigit, [c for c in s if c != " " and c != "o" and c != "a"]))
+)
