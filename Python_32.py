35c66bb5fc382d3c2cc58fc6dffeb78c81c22019
beep boop <githubbot@vadim.me>
2024-07-23 14:55:54 +0200

    Solution 86 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ek0jmw7w Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -7,7 +7,6 @@ def find_zero():
             a = xs[0]
             b = xs[1]
             c = sum(xs[2:]) / (len(xs) - 2)
-
             x = (-b + ((b ** 2 - 4 * a * c) ** 0.5)) / (2 * a)
             return round(x, 10)
         except ValueError as e:
