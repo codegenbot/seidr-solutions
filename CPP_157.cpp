f5add2d13fe2e618d6d2df0106ae6397dc0ecedf
beep boop <githubbot@vadim.me>
2024-07-23 18:55:57 +0200

    Solution 11 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/mla9a8kf Prompt: Program failed

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,5 +1,4 @@
-bool right_angle_triangle(float a, float b, float c) {
-    if (a == 0 || b == 0 || c == 0) 
+if (a == 0 || b == 0 || c == 0) 
         return false; 
     if ((a*a + b*b == c*c) || (a*a + c*c == b*b) || (b*b + c*c == a*a))
         return true;
