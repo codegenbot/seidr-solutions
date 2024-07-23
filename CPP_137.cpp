59c29a8f046d1303746099d6c1aca411608852ba
beep boop <githubbot@vadim.me>
2024-07-23 18:22:03 +0200

    Solution 35 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/arjvjjnh Prompt: 4d6518d1-40f0-4214-affd-e55d65aeb9f2.cpp:1:10: fatal error: boost/lexical_cast.h pp: No such file or directory     1 | #include <boost/lexical_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,1 +1,44 @@
 #include <boost/lexical_cast.hpp>
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
+        return b.convert_to<boost::any>();
+    } else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
+        std::string str1 = boost::lexical_cast<std::string>(a);
+        std::string str2 = boost::lexical_cast<std::string>(b);
+        if (std::stod(str1) > std::stod(str2))
+            return a;
+        else if (std::stod(str1) < std::stod(str2))
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
+        std::string str = boost::lexical_cast<std::string>(b);
+        if (std::stod(a.convert_to<std::string>()) > std::stod(str))
+            return a;
+        else if (std::stod(a.convert_to<std::string>()) < std::stod(str))
+            return b;
+        else
+            return boost::any("None");
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
+        std::string str = boost::lexical_cast<std::string>(b);
+        if (std::stod(a.convert_to<std::string>()) > std::stod(str))
+            return a;
+        else if (std::stod(a.convert_to<std::string>()) < std::stod(str))
+            return b;
+        else
+            return boost::any("None");
+    } else {
+        // If both are strings, compare them.
+        std::string str1 = boost::lexical_cast<std::string>(a);
+        std::string str2 = boost::lexical_cast<std::string>(b);
+        if (str1 > str2)
+            return a;
+        else if (str1 < str2)
+            return b;
+        else
+            return boost::any("None");
+    }
+}
