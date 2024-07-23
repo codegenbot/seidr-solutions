1c650f3fa9371b17ed03861613dc5b055d1349ab
beep boop <githubbot@vadim.me>
2024-07-23 14:00:47 +0200

    Solution 92 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4e3v8dte Prompt:       |             ^~~~~~ 8c6901ca-a145-4e21-a14e-4341d6fd653e.cpp:40:20: error: expected primary-expressi on before ‘int’    40 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 8c6901ca-a145-4e21-a14e-4341d6fd653e.cpp:40:27: error: ‘vector’ was not declared  in this scope    40 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8c6901ca-a145-4e21-a14e-4341d6fd653e.cpp:40:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8c6901ca-a145-4e21-a14e-4341d6fd653e.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8c6901ca-a145-4e21-a14e-4341d6fd653e.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8c6901ca-a145-4e21-a14e-4341d6fd653e.cpp:40:34: error: expected primary-expressi on before ‘int’    40 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool isSame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if(a.size()!=b.size())
         return false;
     for(int i=0;i<a.size();i++){
@@ -12,7 +12,7 @@ bool isSame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> strangeSortList(std::vector<int> list) {
+std::vector<int> strange_sort_list(std::vector<int> list) {
     std::sort(list.begin(),list.end());
     int index = 0;
     for(int i=0;i<list.size();i++){
@@ -24,7 +24,7 @@ std::vector<int> strangeSortList(std::vector<int> list) {
     return list;
 }
 
-int main() {
+int main1() {
     std::vector<int> list;
     list.push_back(1);
     list.push_back(2);
@@ -32,6 +32,6 @@ int main() {
     list.push_back(4);
     list.push_back(5);
     list.push_back(6);
-    assert(isSame(strangeSortList(list), list));
+    assert(issame(strange_sort_list(list), list));
     return 0;
 }
