11c13ec1af272367d34c63d7aff4a85293909ced
beep boop <githubbot@vadim.me>
2024-07-23 22:22:17 +0200

    Solution 13 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/uuakr13h Prompt: Complete the following code given the task description and function signature.

--- Python_124.py
+++ Python_124.py
@@ -1,28 +1,10 @@
 ```
 def valid_date(date):
-    mm/dd/yyyy not supported. Only 'mm-dd-yyyy' is allowed.
-    if '-' not in date:
+    day, month, year = map(int, date.split('-'))
+    if len(str(date)) < 9:
         return False
-    
-    day_month_year = date.split('-')
-    
-    month, day, year = map(int, day_month_year)
-    
-    if 1 > month or month > 12:
+    if month < 1 or month > 12:
         return False
-    
-    if [4, 6, 9, 11].count(month) and 0 > day or day > 30:
+    if (month in [1,3,5,7,8,10,12] and day < 1) or (month in [4,6,9,11] and day > 30) or (month == 2 and day > 29):
         return False
-    
-    if [1, 3, 5, 7, 8, 10, 12].count(month) and 0 > day or day > 31:
-        return False
-    
-    if month == 2 and (0 > day or day > 29):
-        # check for leap year
-        if (year % 4 != 0) or ((year % 100 == 0) and (year % 400 != 0)):
-            return False
-        else:
-            if 0 > day or day > 29:
-                return False
-    
     return True
