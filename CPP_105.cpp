95736a86933c8efcdd7e541af466604d036480fb
beep boop <githubbot@vadim.me>
2024-07-24 01:33:17 +0200

    Solution 12 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1g16sxbf Prompt:       |                              ^~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:23: 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp: In function ‘int main()’: 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:33:20: error: ‘by_length’ was not decla red in this scope    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,98 +1,13 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-bool issame(vector<string> a, vector<string> b) {
-    return (a.size() == b.size()) && equal(a.begin(), a.end(), b.begin());
-}
-
-int main() {
-    vector<int> arr;
-    cout << "Enter numbers: ";
-    int n; 
-    while (cin >> n) {
-        if (n >= 0 && n <= 9) {
-            arr.push_back(n);
-        }
+vector<string> by_length(vector<int> arr) {
+    vector<string> result;
+    
+    for (int i : arr) {
+        string s = to_string(i);
+        map<char, string> m = {{'1', "One"}, {'2', "Two"}, {'3', "Three"}, 
+                                {'4', "Four"}, {'5', "Five"}, {'6', "Six"}, 
+                                {'7', "Seven"}, {'8', "Eight"}, {'9', "Nine"}};
+        result.push_back(m.at(s[0]));
     }
     
-    // sort in descending order
-    sort(arr.begin(), arr.end());
-    reverse(arr.begin(), arr.end());
-
-    vector<string> words;
-    for (int i : arr) {
-        switch (i) {
-            case 0:
-                words.push_back("Zero");
-                break;
-            case 1:
-                words.push_back("One");
-                break;
-            case 2:
-                words.push_back("Two");
-                break;
-            case 3:
-                words.push_back("Three");
-                break;
-            case 4:
-                words.push_back("Four");
-                break;
-            case 5:
-                words.push_back("Five");
-                break;
-            case 6:
-                words.push_back("Six");
-                break;
-            case 7:
-                words.push_back("Seven");
-                break;
-            case 8:
-                words.push_back("Eight");
-                break;
-            case 9:
-                words.push_back("Nine");
-                break;
-        }
-    }
-
-    cout << "Input: ";
-    for (int i : arr) {
-        switch(i){
-            case 0:
-                cout << "Zero";
-                break;
-            case 1:
-                cout << "One";
-                break;
-            case 2:
-                cout << "Two";
-                break;
-            case 3:
-                cout << "Three";
-                break;
-            case 4:
-                cout << "Four";
-                break;
-            case 5:
-                cout << "Five";
-                break;
-            case 6:
-                cout << "Six";
-                break;
-            case 7:
-                cout << "Seven";
-                break;
-            case 8:
-                cout << "Eight";
-                break;
-            case 9:
-                cout << "Nine";
-                break;
-        }
-        cout << " ";
-    }
-    cout << endl;
-    
-    return 0;
+    return result;
 }
