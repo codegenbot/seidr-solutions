ebca11d9ec0a21825172f31af53369031e2bc547
beep boop <githubbot@vadim.me>
2024-07-23 12:25:52 +0200

    Solution 6 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/023c30aa-5404-40b2-ac3e-14675dea5 b63.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,2 @@
-```
 def sorted_list_sum(lst):
-    return sum(sorted(len(x) for x in set(lst)),0)
+    return sum(int(word) for word in sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x)))
