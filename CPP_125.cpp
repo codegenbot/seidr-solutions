d380baa406c3dd43dfc4cc71d2aa70bd7d4ccef3
beep boop <githubbot@vadim.me>
2024-07-23 16:37:23 +0200

    Solution 14 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7spnmcqq Prompt:       |                                              ^ 24659545-9ca0-48ef-bcda-01cbcd491cc4.cpp:46:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    46 | int main(){       |         ^~ 24659545-9ca0-48ef-bcda-01cbcd491cc4.cpp:46:9: note: remove parentheses to defau lt-initialize a variable    46 | int main(){       |         ^~       |         -- 24659545-9ca0-48ef-bcda-01cbcd491cc4.cpp:46:9: note: or replace parentheses with  braces to value-initialize a variable 24659545-9ca0-48ef-bcda-01cbcd491cc4.cpp:46:11: error: a function-definition is not allowed here before ‘{’ token    46 | int main(){       |           ^ 24659545-9ca0-48ef-bcda-01cbcd491cc4.cpp:48:2: error: expected ‘}’ at end of inp ut    48 | }       |  ^ 24659545-9ca0-48ef-bcda-01cbcd491cc4.cpp:7:40: note: to match this ‘{’     7 | vector<string> split_words(string txt) {       |                                        ^

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,9 +1,1 @@
-while (pos != txt.length()) {
-    result.push_back(txt.substr(0, pos));
-    txt.erase(0, pos + 1);
-}
-
-while ((pos = txt.find(',')) != std::string::npos) {
-    result.push_back(txt.substr(0, pos));
-    txt.erase(0, pos + 1);
-}
+int main() {
