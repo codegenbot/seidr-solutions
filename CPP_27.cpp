65ccb825173170408905510702ad1605338be7bb
beep boop <githubbot@vadim.me>
2024-07-23 22:37:45 +0200

    Solution 74 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0w182pf9 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: 2d6e45c9-21e8-4bf8-a70d-baa89dc2f998.cpp:18:42:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,9 +1,20 @@
-std::string result = "";
-for(int i=0; i<str.length(); i++){
-    if(islower(str[i])){
-        result += toupper(str[i]);
-    } else {
-        result += tolower(str[i]);
+#include <iostream>
+#include <string>
+
+std::string& flip_case(std::string& str) {
+    for(int i=0; i<str.length(); i++){
+        if(islower(str[i])){
+            str[i] = toupper(str[i]);
+        } else {
+            str[i] = tolower(str[i]);
+        }
     }
+    return str;
 }
-return result;
+
+int main() {
+    std::string str = "Hello World!";
+    flip_case(str);
+    std::cout << str << std::endl;
+    return 0;
+}
