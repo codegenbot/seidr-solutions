78bcb61fb4173be04a7ad8ed3ad2fdb2df3f1bbc
beep boop <githubbot@vadim.me>
2024-07-23 10:50:18 +0200

    Solution 62 of CPP/13, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/qgeexnq9 Prompt: Complete the following code given the task description and function signature.

--- CPP_13.cpp
+++ CPP_13.cpp
@@ -1,4 +1,4 @@
-if (b == 0)
-        return a;
-    return greatest_common_divisor(b, a % b);
+int greatest_common_divisor(int a, int b){
+    if(b==0) return a;
+    return greatest_common_divisor(b, a%b);
 }
