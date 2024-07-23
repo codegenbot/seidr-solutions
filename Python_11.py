6d09ed651a24dcef92126f1e2c1140e8f9f98006
beep boop <githubbot@vadim.me>
2024-07-23 08:45:30 +0200

    Solution 18 of Python/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yngnl4id Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cee158d6-a4b6-427e-9a2f-7cd6168c6 851.py", line 25, in <module>     check(string_xor)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cee158d6-a4b6-427e-9a2f-7cd6168c6 851.py", line 22, in check     assert string_xor("0101", "0000") == "0101"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_11.py
+++ Python_11.py
@@ -1,20 +1,21 @@
 def string_xor(a: str, b: str) -> str:
     a = bin(int(a or '0', 2))[2:]
     b = bin(int(b or '0', 2))[2:]
-
+    
     result = ""
-    for i in range(max(len(a), len(b))):
+    max_length = max(len(a), len(b))
+    for i in range(max_length):
         if i < len(a) and i < len(b):
-            if a[-1-i] == "1" and b[-1-i] == "1":
-                result = "0" + result
-            elif a[-1-i] == "1" or b[-1-i] == "1":
+            if a[i] == "1" and b[i] == "0":
+                result = "1" + result
+            elif a[i] == "0" and b[i] == "1":
                 result = "1" + result
             else:
                 result = "0" + result
         else:
             if i < len(a):
-                result = a[-1-i] + result
+                result = a[i] + result
             elif i < len(b):
-                result = b[-1-i] + result
-
+                result = b[i] + result
+    
     return result
