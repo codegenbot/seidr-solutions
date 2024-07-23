81e155253251e69b9cea638a212b3bb97c72b986
beep boop <githubbot@vadim.me>
2024-07-23 12:16:32 +0200

    Solution 8 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/si5gzmww Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ab9ced4-f6f7-449b-bd20-fe1def648 988.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,10 +1,10 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        return a if a.replace(',', '.') > str(b).replace(',', '.'): b
+        return a if a.replace(',', '.').replace(' ', '') > str(b).replace(',', '.').replace(' ', ''):
+        else: b
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        return b if b.replace(',', '.') > str(a).replace(',', '.'): a
-    elif isinstance(a, str) and isinstance(b, str):
-        return a if a.replace(',', '.') > b.replace(',', '.'): b
+        return b if b.replace(',', '.').replace(' ', '') > str(a).replace(',', '.').replace(' ', ''):
+        else: a
     else:
         return a if a > b: b if a < b: None
