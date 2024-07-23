14d20bc23ed5f50b7af959aeceb5bf66d3e781d8
beep boop <githubbot@vadim.me>
2024-07-23 10:17:39 +0200

    Solution 73 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/pfzpyce6 Prompt: Complete the following code given the task description and function signature.

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,6 +1,5 @@
-string string_xor(string a, string b){
-    string result = "";
-    for(int i=0; i<a.size(); i++){
+string result = "";
+    for(int i=0; i<a.length(); i++){
         if(a[i] != b[i]){
             result += "1";
         } else {
