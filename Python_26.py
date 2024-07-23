55d86f1dce26694a383f0cf939e629b0cc374d96
beep boop <githubbot@vadim.me>
2024-07-23 10:41:57 +0200

    Solution 48 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/mbm4rwyy Prompt: Program did not receive expected input

--- Python_26.py
+++ Python_26.py
@@ -1,11 +1,5 @@
 from typing import List
 
-
-def remove_duplicates() -> List[int]:
-    numbers = list(
-        map(int, input("Enter the list of numbers separated by space: ").split())
-    )
+def remove_duplicates(numbers: List[str]) -> List[int]:
+    numbers = list(map(int, input("Enter the list of numbers separated by space: ").split()))
     return sorted(list(set(numbers)))
-
-
-remove_duplicates()
