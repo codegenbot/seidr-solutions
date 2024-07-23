87e56532d4512726b7a2cfaf4d221938a578b162
beep boop <githubbot@vadim.me>
2024-07-23 13:36:59 +0200

    Solution 72 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4d2aq60w Prompt:  [with _InputIterator = __gnu_cxx::__normal_iterator<const std::__cxx11::basic_s tring<char>*, std::vector<std::__cxx11::basic_string<char> > >; _ForwardIterator  = std::__cxx11::basic_string<char>*]’ /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = __gnu_cxx::__normal_iterator<c onst std::__cxx11::basic_string<char>*, std::vector<std::__cxx11::basic_string<c har> > >; _ForwardIterator = std::__cxx11::basic_string<char>*; _Tp = std::__cxx 11::basic_string<char>]’ /usr/include/c++/11/bits/stl_vector.h:558:31:   required from ‘std::vector<_Tp, _Alloc>::vector(const std::vector<_Tp, _Alloc>&) [with _Tp = std::__cxx11::basic _string<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >]’ c9fe1da3-7917-4710-854f-76a389ea9d38.cpp:39:40:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -7,10 +7,10 @@
 using namespace std;
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    vector<string> result;
+    vector<string> result(lst1.size());
     
-    transform(lst1.begin(), lst1.end(), lst1.begin(), std::back_inserter(string()), ::tolower);
-    transform(lst2.begin(), lst2.end(), std::back_inserter(string()), ::toupper);
+    transform(lst1.begin(), lst1.end(), lst1.begin(), ::tolower);
+    transform(lst2.begin(), lst2.end(), lst2.begin(), ::toupper);
 
     sort(lst1.begin(), lst1.end());
     sort(lst2.begin(), lst2.end());
@@ -19,7 +19,7 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     
     while (i < lst1.size() && j < lst2.size()) {
         if (lst1[i] == lst2[j]) {
-            result.push_back(string(lst1[i]));
+            result[i] = string(lst1[i]); 
             i++;
             j++;
         } else if (lst1[i] < lst2[j]) {
