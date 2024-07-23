d8c88002c0ded3bf9f27b1eba32c6d57608c8292
beep boop <githubbot@vadim.me>
2024-07-23 13:39:05 +0200

    Solution 40 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s3hdzb6p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/492a65c6-251e-4c02-8cd0-5cd82a815 15e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_162.py
+++ Python_162.py
@@ -1,4 +1,3 @@
-```Python
 import hashlib
 
 def string_to_md5(text):
@@ -7,11 +6,8 @@ def string_to_md5(text):
     return hashlib.md5(text.encode()).hexdigest()
 
 if __name__ == "__main__":
-    def process_input():
-        try:
-            user_text = input("Enter a string: ")
-            print(string_to_md5(user_text))
-        except TypeError:
-            print("Invalid input. Please enter a string.")
-
-    process_input()
+    text = input("Enter a string: ")
+    try:
+        print(string_to_md5(text))
+    except TypeError:
+        print("Invalid input. Please enter a string.")
