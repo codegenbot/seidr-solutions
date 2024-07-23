edae06ee23bebc9ac4c0a59f9999d4a111e0c563
beep boop <githubbot@vadim.me>
2024-07-23 22:42:54 +0200

    Solution 98 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cd9hr4r0 Prompt:       |             ^~~~~~ f7cca275-85c8-4510-814d-44a36f12720c.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f7cca275-85c8-4510-814d-44a36f12720c.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ f7cca275-85c8-4510-814d-44a36f12720c.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ f7cca275-85c8-4510-814d-44a36f12720c.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from f7cca275-85c8-4510-814d-44a36f12720c.cpp:35: f7cca275-85c8-4510-814d-44a36f12720c.cpp: In function ‘int main()’: f7cca275-85c8-4510-814d-44a36f12720c.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,3 +1,6 @@
+#include <cassert>
+#include <vector>
+
 std::vector<int> even_odd_count(int num) {
     std::vector<int> result({0, 0});
     int temp = abs(num);
@@ -12,18 +15,19 @@ std::vector<int> even_odd_count(int num) {
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool same(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i])
+        if (a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
 
 int main() {
-    assert(issame(even_odd_count(0), std::vector<int>{1, 0})); 
+    assert(same(even_odd_count(0), std::vector<int>{0,1})); 
     return 0;
 }
