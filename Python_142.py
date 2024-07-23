4008af5f290fa7ad67ff170578bf42df371c501a
beep boop <githubbot@vadim.me>
2024-07-23 18:00:06 +0200

    Solution 22 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/glt8anq7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/738c452a-8a39-4e8c-bdca-179a74ab1 511.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,8 @@
-def sum_squares(lst):
-    total_sum = sum([i**2 if (i % 3 == 0 and i % 4 != 0) else i**3 for i in lst])
-    return total_sum
+```
+total_sum = 0
+for num in lst:
+    if isinstance(num, (int, float)):
+        total_sum += num ** 2
+    else:
+        raise TypeError("The list contains non-numeric values")
+return total_sum
