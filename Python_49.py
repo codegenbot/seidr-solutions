cc32b0cc0a545045fd8859ad5dd27d1da0aefdc9
beep boop <githubbot@vadim.me>
2024-07-23 10:46:37 +0200

    Solution 45 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df1ca766-b3ba-4238-9f78-2e5150e1b 71b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,12 +1,14 @@
+```
 def is_prime(n: int):
-    if n < 2:
+    if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
-
-def next_prime(n: int):
-    while not is_prime(n):
-        n += 1
-    return n
