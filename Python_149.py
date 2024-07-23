343a3fce4ce105a4446d79224a841a79426e4417
beep boop <githubbot@vadim.me>
2024-07-23 14:08:34 +0200

    Solution 81 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56d25329-adc2-4dde-9d9f-5c78e8ec1 9dd.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56d25329-adc2-4dde-9d9f-5c78e8ec1 9dd.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56d25329-adc2-4dde-9d9f-5c78e8ec1 9dd.py", line 2, in sorted_list_sum     even_numbers = [str(num) for num in numbers if int(num) % 2 == 0]                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56d25329-adc2-4dde-9d9f-5c78e8ec1 9dd.py", line 2, in <listcomp>     even_numbers = [str(num) for num in numbers if int(num) % 2 == 0]                                                    ^^^^^^^^ ValueError: invalid literal for int() with base 10: 'aaaa'

--- Python_149.py
+++ Python_149.py
@@ -1,4 +1,3 @@
-```python
-def sorted_list_sum(numbers):
+def sorted_list_sum(numbers): 
     even_numbers = [num for num in numbers if set(num).issubset('0123456789')]
     return " ".join(sorted(even_numbers))
