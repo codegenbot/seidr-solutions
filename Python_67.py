42f7f7e9e02cb1e9282d2693b74954992019cb16
beep boop <githubbot@vadim.me>
2024-07-23 10:46:58 +0200

    Solution 8 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02986a36-1d20-45d3-9501-50ba59609 705.py", line 11, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02986a36-1d20-45d3-9501-50ba59609 705.py", line 8, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/02986a36-1d20-45d3-9501-50ba59609 705.py", line 4, in fruit_distribution     return n - int(s.split(", ")[0].split(" ")[0]) - int(s.split(", ")[1].split( " ")[0])                                                          ~~~~~~~~~~~~~^^^ IndexError: list index out of range

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,5 @@
 def fruit_distribution(s, n):
     s = s.replace("apples and ", "").replace("oranges ", ",")
     n = int(n)
-    fruits = [x.split()[1] for x in s.split(", ") if x]
-    return n - sum(map(int, fruits))
+    numbers = [num for num in s.split(", ") if num]
+    return n - int(numbers[0].split()[0]) - int(numbers[-1].split()[0])
