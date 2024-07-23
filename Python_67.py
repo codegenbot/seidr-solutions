d4992e85e9af66b5c4b8fba8f406cdf855fbba63
beep boop <githubbot@vadim.me>
2024-07-23 10:52:35 +0200

    Solution 16 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/039ea61b-cf6e-405c-bdd4-c52d368b8 57a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,7 @@
-``` 
+```
 def fruit_distribution(s,n):
     s = s.lower()
+    if not s.isalpha():
+        return "Invalid input: Please enter a string containing only letters."
     n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a'])))
-    return """{}""".format(n)
+    return n
