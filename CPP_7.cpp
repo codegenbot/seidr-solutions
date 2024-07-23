169f8ec990dd08a93ede33399776b4282215e8cc
beep boop <githubbot@vadim.me>
2024-07-23 09:00:52 +0200

    Solution 31 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/klkuaq7w Prompt:       |                                            ^ 867ab6d0-9bbc-4781-af57-675daeb06e12.cpp:4:45: error: expression list treated as  compound expression in initializer [-fpermissive]     4 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 867ab6d0-9bbc-4781-af57-675daeb06e12.cpp:3: 867ab6d0-9bbc-4781-af57-675daeb06e12.cpp: In function ‘int main()’: 867ab6d0-9bbc-4781-af57-675daeb06e12.cpp:13:20: error: ‘filter_by_substring’ was  not declared in this scope    13 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 867ab6d0-9bbc-4781-af57-675daeb06e12.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 867ab6d0-9bbc-4781-af57-675daeb06e12.cpp:13:5: error: ‘__assert_fail’ was not de clared in this scope    13 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |     ^~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,21 +1,7 @@
 ```cpp
-int main() {
-    vector<string> str = {"hello", "world", "hello", "abc", "world"};
-    string sub = "world";
-    bool same = issame(str, {sub, sub});
-    vector<string> filtered = filter_by_substring(str, sub);
-    
-    if (same) {
-        cout << "Vectors are the same." << endl;
-    } else {
-        cout << "Vectors are not the same." << endl;
-    }
-    
-    cout << "Filtered strings: ";
-    for(auto x : filtered) {
-        cout << x << " ";
-    }
-    cout << endl;
+#include <vector>
+#include <string>
+using namespace std;
 
-    return 0;
+int main() {
 }
