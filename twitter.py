bef24fca70801c49339adcee179e6e022d833523
beep boop <githubbot@vadim.me>
2024-07-24 00:01:23 +0200

    Solution 21 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/t4sl8j0j Prompt: it must return Your tweet has 3 characters for input ',U, but it returns You didn't type anything Your tweet has 1 characters Your tweet has 19 characters Your tweet has 91 characters Your tweet has 17 characters.

--- twitter.py
+++ twitter.py
@@ -1,14 +1,18 @@
-```
 def validate_tweet(tweet):
     if not tweet:
         return "You didn't type anything"
-    elif len(tweet) > 140:
+    elif len(tweet.replace(" ", "")) > 140:
         return "Too many characters"
     else:
         return f"Your tweet has {len(tweet)} characters"
 
-print('{}'.format(validate_tweet("")))
-print('{}'.format(validate_tweet("1")))
-print('{}'.format(validate_tweet("max length tweet...")))
-print('{}'.format(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``))"))
-print('{}'.format(validate_tweet("Tooooooooooolong1")))
+
+print(validate_tweet(""))
+print(validate_tweet("1"))
+print(validate_tweet("max length tweet..."))
+print(
+    validate_tweet(
+        "40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``))"
+    )
+)
+print(validate_tweet("Tooooooooooolong1"))
