d56eee476430bdfae198e6a788f57dc917117c55
beep boop <githubbot@vadim.me>
2024-07-23 11:14:16 +0200

    Solution 14 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4xxec241 Prompt: 15c00fc4-daf4-47d7-a62d-1bdb35ec9e5d.cpp: In function ‘int main()’: 15c00fc4-daf4-47d7-a62d-1bdb35ec9e5d.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(abs(triangle_area(2, 2, 10)) + 1 < 0.01);       |     ^~~~~~ 15c00fc4-daf4-47d7-a62d-1bdb35ec9e5d.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <iostream>   +++ |+#include <cassert>     3 | 15c00fc4-daf4-47d7-a62d-1bdb35ec9e5d.cpp: At global scope: 15c00fc4-daf4-47d7-a62d-1bdb35ec9e5d.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 15c00fc4-daf4-47d7-a62d-1bdb35ec9e5d.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,12 +1,11 @@
-```cpp
+#include <cassert>
 #include <cmath>
 #include <iostream>
-#include <cassert>
 
 float triangle_area(float a, float b, float c) {
     if ((a + b > c) && (a + c > b) && (b + c > a)) {
         float s = (a + b + c) / 2;
-        return std::round(std::pow(s * (s - a) * (s - b) * (s - c), 0.5) * 100.0) / 100.0;
+        return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
     } else {
         return -1;
     }
