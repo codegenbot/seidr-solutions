bb2fec1898cfa414831f4bfe4cb0ef0cb6eab352
beep boop <githubbot@vadim.me>
2024-07-23 09:40:36 +0200

    Solution 85 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ck6ppyb7 Prompt: a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp:25: a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp: In function ‘int main()’: a589d2af-7e10-4555-bd69-f5ab1e4f85f1.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -10,7 +10,7 @@ bool is_same(const std::vector<int>& a, const std::vector<int>& b) {
 std::vector<int> filter_integers(const std::vector<int>& mixedVector) {
     std::vector<int> result;
     for (int num : mixedVector) {
-        if (std::is_same_v<decltype(num), int>) {
+        if (std::is_same_v<std::decay_t<decltype(num)>, int>) {
             result.push_back(num);
         }
     }
