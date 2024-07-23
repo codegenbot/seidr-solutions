51631bf5148af2a2288d0a9de851c134abed679d
beep boop <githubbot@vadim.me>
2024-07-23 12:55:54 +0200

    Solution 75 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/k1fnh68u Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,2 +1,2 @@
-sort(l.begin(), l.end(), [](int a, int b) { return a % 3 == 0 ? (a < b) : false; });
+sort(l.begin(), l.end(), [](int a, int b){return a%3 == 0 && b%3 == 0 ? a < b : a%3 == 0;});
 return l;
