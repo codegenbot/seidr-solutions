32d9de1ae5033ba1845fa00c4feeae6fc4e33482
beep boop <githubbot@vadim.me>
2024-07-24 01:41:12 +0200

    Solution 28 of CPP/99, ratio 0.8 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/044d1264 Prompt: cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:3:21: error: ‘string’ was not declared in this scope     3 | int closest_integer(string value) {       |                     ^~~~~~ cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:3:21: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:9: cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp: In function ‘int main()’: cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:11:28: error: ‘closest_integer’ cannot be used as a function    11 |     assert (closest_integer("0") == 0);       |             ~~~~~~~~~~~~~~~^~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,10 +1,8 @@
 #include <string>
+#include <cmath>
 
-int closest_integer(string value) {
+int closest_integer(std::string value) {
     double num = stod(value);
-    int integerPart = (int)num;
-    if (abs(num - integerPart) >= 0.5)
-        return (num > 0) ? integerPart + 1 : integerPart - 1;
-    else
-        return integerPart;
+    int integer = (num >= 0) ? static_cast<int>(ceil(num)) : static_cast<int>(floor(num));
+    return integer;
 }
