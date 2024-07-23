e8fc1df736d59be2d438c04e104af9782ede749c
beep boop <githubbot@vadim.me>
2024-07-23 23:52:05 +0200

    Solution 31 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ooca07p6 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -1,7 +1,7 @@
+```
 from typing import List
 
-def separate_paren_groups():
-    paren_string = input("Enter a string: ")
+def separate_paren_groups(paren_string: str) -> List[str]:
     paren_string = ' '.join(paren_string.split())
     result = []
     temp = ''
@@ -14,12 +14,15 @@ def separate_paren_groups():
         elif char in ['(', ')']:
             if char == '(':
                 temp += char
-            elif temp and (temp[-1] != '(' or temp[-1] != ')'):
-                result.append(temp)
-                temp = ''
             else:
-                temp += char
-                
+                while temp and (temp[-1] != '('):
+                    result.append(temp[:-1])
+                    temp = temp[:-1].rstrip()
+                if temp and temp[-1] == '(':
+                    temp += char
+                elif not temp:
+                    temp += char
+
     if temp:
         result.append(temp)
 
