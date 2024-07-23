f202e0b6bc13aedc29b5efa262fc41888011f634
beep boop <githubbot@vadim.me>
2024-07-23 23:39:18 +0200

    Solution 9 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zwcvnncc Prompt: 46115333-ad9c-46e1-b591-578913af9ce3.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string decimal_to_binary(int decimal){       | ^~~~~~ In file included from 46115333-ad9c-46e1-b591-578913af9ce3.cpp:13: 46115333-ad9c-46e1-b591-578913af9ce3.cpp: In function ‘int main()’: 46115333-ad9c-46e1-b591-578913af9ce3.cpp:15:13: error: ‘decimal_to_binary’ was n ot declared in this scope    15 |     assert (decimal_to_binary(15) == "db1111db");       |             ^~~~~~~~~~~~~~~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-string decimal_to_binary(int decimal){
+string decimal_to_binary(int decimal) {
     string binary = "";
     while(decimal > 0) {
         if (decimal % 2 == 0)
