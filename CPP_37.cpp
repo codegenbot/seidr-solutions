2cc2ab722f1b02b13c4c8dcc1a3210bc7db9ac7f
beep boop <githubbot@vadim.me>
2024-07-23 09:55:24 +0200

    Solution 24 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4qm5mikn Prompt:                  from 05920bda-290d-4e14-910a-c40fa8926275.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 05920bda-290d-4e14-910a-c40fa8926275.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 05920bda-290d-4e14-910a-c40fa8926275.cpp:41:36: error: expected primary-expressi on before ‘float’    41 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 05920bda-290d-4e14-910a-c40fa8926275.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 05920bda-290d-4e14-910a-c40fa8926275.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <vector>
 
-bool std::issame(std::vector<float> a, std::vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -14,7 +14,7 @@ bool std::issame(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-std::vector<float> std::sort_even(std::vector<float> l) {
+std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -33,7 +33,7 @@ std::vector<float> std::sort_even(std::vector<float> l) {
     return result;
 }
 
-int main_function() {
-    assert(std::issame(std::sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
     return 0;
 }
