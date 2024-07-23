ef5fc4d75846be198b8360d99c4f3bed24633df3
beep boop <githubbot@vadim.me>
2024-07-23 10:48:50 +0200

    Solution 10 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85964f58-a016-42a1-8d66-575731c7e 000.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,4 @@
-```
-def fruit_distribution(s, n):
-    s = s.replace("apples and ", "").replace("oranges ", ",")
-    n = int(n)
-    numbers = [num for num in s.split(", ") if num]
-    return n - int(numbers[0].split()[0]) - int(numbers[-1].split()[0])
+def fruit_distribution(s,n):
+    s = s.lower()
+    n -= int(''.join(filter(str.isdigit, s))) + int(''.join(filter(str.isdigit, [c for c in s if c != ' ' and c != 'o' and c != 'a']))))
+    return n
