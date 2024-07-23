4614c389fa5c752465f515f208ab521a0ac88f06
beep boop <githubbot@vadim.me>
2024-07-24 01:14:28 +0200

    Solution 77 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ooca07p6 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -4,6 +4,7 @@ from typing import List
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
     temp = ""
+    count = 0
 
     for char in paren_string:
         if char == " ":
@@ -11,28 +12,19 @@ def separate_paren_groups(paren_string: str) -> List[str]:
                 result.append(temp)
                 temp = ""
         elif char in ["(", ")"]:
-            if not temp or (temp and temp[-1] in ["(", "))"]):
-                if char == "(":
-                    temp += char
-                else:
-                    while temp and (temp[-1] != "("):
-                        result.append(temp[:-1])
-                        temp = temp[:-1].rstrip()
-                    if temp and temp[-1] == "(":
-                        temp += ")"
-                    elif not temp:
-                        temp += ")"
+            if char == "(":
+                count += 1
+                temp += char
+            else:
+                count -= 1
+                temp += char
+            if count == 0:
+                result.append(temp)
+                temp = ""
         else:
             temp += char
 
     if temp:
-        while temp and (temp[-1] != "("):
-            result.append(temp[:-1])
-            temp = temp[:-1].rstrip()
-        if temp and temp[-1] == "(":
-            temp += ")"
-        elif not temp:
-            temp += ")"
         result.append(temp)
 
     return result
