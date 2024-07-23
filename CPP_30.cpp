ba0228fcd51f00fd217746601ab1bf3abae3191c
beep boop <githubbot@vadim.me>
2024-07-23 09:20:09 +0200

    Solution 10 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/dkygeo55 Prompt: In file included from /usr/include/c++/11/vector:67,                  from 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp:13:36: error: expected primary-expressi on before ‘float’    13 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp:13:43: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp:12: 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp: In function ‘int main()’: 782ad7cb-c233-4093-8a4f-24a625c6b889.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -2,15 +2,10 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<float> a, vector<float> b) {
-    return a.size() == b.size() && std::all_of(a.begin(), a.end(),
-        [&](float x) { return std::find(b.begin(), b.end(), x) != b.end(); });
+bool issame(const std::vector<float>& a, const std::vector<float>& b) {
+    return a.size() == b.size();
 }
 
-vector<float> get_positive(vector<float> l){
-    vector<float> result;
-    for(float i : l){
-        if(i > 0) result.push_back(i);
-    }
-    return result;
+int main() {
+    // your code here
 }
