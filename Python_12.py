93d0d411ad3f2d2cf25a4748264d05389aeeae6e
beep boop <githubbot@vadim.me>
2024-07-23 09:31:38 +0200

    Solution 40 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ec669sg0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6b9266d3-58cd-48a9-8dac-44f7395fd 380.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -4,3 +4,10 @@ def longest(strings: List[str]) -> Optional[str]:
     if not strings:
         return None
     return ''.join(sorted(set(strings), key=len, reverse=True))
+
+if __name__ == "__main__":
+    user_input = input("Enter the list of strings separated by space: ")
+    input_list = [i for i in user_input.split() if i]
+    longest_str = longest(input_list)
+    
+    print(f"The longest string is: {longest_str}")
