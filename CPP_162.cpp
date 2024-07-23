cf5b988638a8a89d230625d11e14a81853355c64
beep boop <githubbot@vadim.me>
2024-07-23 08:57:59 +0200

    Solution 28 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/70xd2i5s Prompt:     9 | for(int i = 0; i < MD5_DIGEST_LENGTH; i++){       | ^~~ e83a0832-461b-4aac-be36-de0f70774527.cpp:9:16: error: ‘i’ does not name a type     9 | for(int i = 0; i < MD5_DIGEST_LENGTH; i++){       |                ^ e83a0832-461b-4aac-be36-de0f70774527.cpp:9:39: error: ‘i’ does not name a type     9 | for(int i = 0; i < MD5_DIGEST_LENGTH; i++){       |                                       ^ e83a0832-461b-4aac-be36-de0f70774527.cpp:13:1: error: expected unqualified-id be fore ‘return’    13 | return md5_hash;       | ^~~~~~ e83a0832-461b-4aac-be36-de0f70774527.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from e83a0832-461b-4aac-be36-de0f70774527.cpp:16: e83a0832-461b-4aac-be36-de0f70774527.cpp: In function ‘int main()’: e83a0832-461b-4aac-be36-de0f70774527.cpp:18:13: error: ‘string_to_md5’ was not d eclared in this scope    18 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -21,4 +21,7 @@ string string_to_md5(const string& text) {
     return md5_hash;
 }
 
-assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+int main() {
+    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+    return 0;
+}
