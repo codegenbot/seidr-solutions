1cbc480ac26cc225e6511be0aa9fe820374516f8
beep boop <githubbot@vadim.me>
2024-07-24 00:20:25 +0200

    Solution 83 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9p3ktnil Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7d37f669-bc80-4b58-980c-be1618882a32.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -41,6 +41,6 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 }
 
 int main() {
-    std::vector<float> test = {0, 0.7};
-    assert(std::equal(numerical_letter_grade(test).begin(), numerical_letter_grade(test).end(), {"E", "D-"}.begin(), {"E", "D-").end(), compare));
+    std::vector<float> testGrades = {0, 0.7};
+    assert(std::equal(numerical_letter_grade(testGrades).begin(), numerical_letter_grade(testGrades).end(), {"E", "D-"}.begin(), {"E", "D-").end(), compare));
 }
