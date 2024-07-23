8541cfff36f285b06b6ef6f6646380e8bd5908af
beep boop <githubbot@vadim.me>
2024-07-23 09:46:17 +0200

    Solution 27 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/txje1r3n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/91f67307-4b0f-47ee-b7be-a072096b1 09e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -10,8 +10,10 @@ def filter_by_prefix() -> List[str]:
             if not strings or not prefix:
                 raise ValueError("Please enter valid inputs!")
 
-            filtered_strings = [s.strip() for s in strings.split() if s.strip().startswith(prefix)]
-            
+            filtered_strings = [
+                s.strip() for s in strings.split() if s.strip().startswith(prefix)
+            ]
+
             return filtered_strings
         except ValueError as e:
             print(f"Error: {e}")
