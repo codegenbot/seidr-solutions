d6e8d81dc6b38b9cc66549c8a851f3b0767138e5
beep boop <githubbot@vadim.me>
2024-07-23 10:18:07 +0200

    Solution 22 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/835050c8-87ac-4dd6-884b-360382bf3 f1c.py", line 15, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/835050c8-87ac-4dd6-884b-360382bf3 f1c.py", line 12, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,7 +1,17 @@
-```
 def modp(n: int, p: int):
-    if gcd(n, p) == 1:
-        return pow(n, p-1, p)
+    def is_prime(x: int) -> bool:
+        if x < 2:
+            return False
+        for i in range(2, int(x**0.5) + 1):
+            if x % i == 0:
+                return False
+        return True
+
+    if is_prime(p):
+        if gcd(n, p) == 1:
+            return pow(n, (p - 1), p)
+        else:
+            return None
     else:
         return None
 
