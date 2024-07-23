297acac10e68b0de91c339faf398c5fb652fd1a6
beep boop <githubbot@vadim.me>
2024-07-23 09:15:31 +0200

    Solution 23 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ck6ppyb7 Prompt:       |                           ^~~~~~ 6eedad0d-9cde-45de-9ea0-029c875bcc04.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 6eedad0d-9cde-45de-9ea0-029c875bcc04.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 6eedad0d-9cde-45de-9ea0-029c875bcc04.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 6eedad0d-9cde-45de-9ea0-029c875bcc04.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6eedad0d-9cde-45de-9ea0-029c875bcc04.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 6eedad0d-9cde-45de-9ea0-029c875bcc04.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -3,7 +3,7 @@
 #include <cassert>
 #include <type_traits>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+int issame(std::vector<int> a, std::vector<int> b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
     return a == b;
@@ -20,7 +20,7 @@ std::vector<int> filter_integers(std::vector<int> mixedVector) {
 }
 
 int main() {
-    assert(issame(filter_integers({3, 3, 3}), {3, 3, 3}));
-    
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+
     return 0;
 }
