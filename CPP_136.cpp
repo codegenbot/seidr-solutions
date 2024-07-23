3b45c48aca5480af25ab6d2ef6064c47f6025dc2
beep boop <githubbot@vadim.me>
2024-07-23 09:45:46 +0200

    Solution 52 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ghl9lyyn Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4710e90e-f7bb-4b97-aef5-fdbfdc953b04.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4710e90e-f7bb-4b97-aef5-fdbfdc953b04.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4710e90e-f7bb-4b97-aef5-fdbfdc953b04.cpp:13: 4710e90e-f7bb-4b97-aef5-fdbfdc953b04.cpp: In function ‘int main()’: 4710e90e-f7bb-4b97-aef5-fdbfdc953b04.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 4710e90e-f7bb-4b97-aef5-fdbfdc953b04.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -14,15 +14,15 @@ bool issame(pair<int, int> a, pair<int, int> b){
     return a.first == b.first && a.second == b.second;
 }
 
-int main() {
+int main(){
     pair<int, int> result = largest_smallest_integers(lst);
-    pair<int, int> expected_result = largest_smallest_integers_sum(lst);
-
+    pair<int, int> expected_result = {expected_smallest_positive, expected_largest_negative};
+    
     if (issame(result, expected_result)) {
-        cout << "Results match!" << endl;
+        cout << "Test passed!" << endl;
     } else {
-        cout << "Results do not match!" << endl;
+        cout << "Test failed!" << endl;
     }
-
+    
     return 0;
 }
