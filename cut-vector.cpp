3a46538311d1fcd78fc69be8bc2d28c152b301f3
beep boop <githubbot@vadim.me>
2024-07-23 18:04:25 +0200

    Solution 24 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/lb0t46iz Prompt:  this scope    22 |         cout << nums[i] << endl;       |         ^~~~ ec7f71b7-3430-4cf9-beea-db33929c89e2.cpp:22:17: error: ‘nums’ was not declared i n this scope    22 |         cout << nums[i] << endl;       |                 ^~~~ ec7f71b7-3430-4cf9-beea-db33929c89e2.cpp:22:28: error: ‘endl’ was not declared i n this scope    22 |         cout << nums[i] << endl;       |                            ^~~~ ec7f71b7-3430-4cf9-beea-db33929c89e2.cpp:24:5: error: ‘cout’ was not declared in  this scope    24 |     cout << endl;       |     ^~~~ ec7f71b7-3430-4cf9-beea-db33929c89e2.cpp:24:13: error: ‘endl’ was not declared i n this scope    24 |     cout << endl;       |             ^~~~ ec7f71b7-3430-4cf9-beea-db33929c89e2.cpp:27:17: error: ‘nums’ was not declared i n this scope    27 |         cout << nums[i] << endl;       |                 ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <numeric>
-#include <climits>
-
-using namespace std;
 
 int main() {
+    using namespace std;
+
     int n;
     cin >> n;
     vector<int> nums(n);
     for (int i = 0; i < n; ++i) {
         cin >> nums[i];
     }
-    
+
     int diff = INT_MAX;
     int cutIndex = -1;
     for (int i = 1; i < n; ++i) {
@@ -24,15 +24,15 @@ int main() {
             cutIndex = i;
         }
     }
-    
+
     for (int i = 0; i < cutIndex; ++i) {
         cout << nums[i] << endl;
     }
     cout << endl;
-    
+
     for (int i = cutIndex; i < n; ++i) {
         cout << nums[i] << endl;
     }
-    
+
     return 0;
 }
