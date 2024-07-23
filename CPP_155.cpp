6e6a05bbb5f96f216e7b57e997086db1b0269494
beep boop <githubbot@vadim.me>
2024-07-23 21:50:07 +0200

    Solution 79 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cd9hr4r0 Prompt:       |           ^~~~~~ In file included from 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp:35: 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp: In function ‘int main()’: 29cd7ccf-ed68-45c8-8426-02504e53ac23.cpp:45:20: error: ‘even_odd_count’ was not declared in this scope    45 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -8,7 +8,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
-        }
     }
     return true;
 }
@@ -30,4 +29,3 @@ std::vector<int> even_odd_count(int num) {
 int main() {
     assert(issame(even_odd_count(0), std::vector<int>{0, 1})); 
     return 0;
-}
