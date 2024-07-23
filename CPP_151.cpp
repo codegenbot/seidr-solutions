7e6faf970758332754c9739546efc8174f1072aa
beep boop <githubbot@vadim.me>
2024-07-23 09:35:09 +0200

    Solution 90 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/azhv2xns Prompt: e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst){       |                                 ^~~~~~ e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst){       |                                        ^~~~~ In file included from e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp:11: e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp: In function ‘int main()’: e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ e1232c26-bf60-40b1-9c07-daae99fb3d32.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,17 +1,18 @@
 #include <vector>
+#include <cmath>
 
 long long double_the_difference(std::vector<float> lst){
-    long long sum = 0;
+    long long odd_sum = 0;
     for (float num : lst) {
-        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
-            sum += num * num;
+        if (num > 0 && std::fmod(num, 1) == 0 && ((int)num) % 2 != 0) {
+            odd_sum += (long long)std::pow(num, 2);
         }
     }
-    return sum;
+    return odd_sum;
 }
 
 int main() {
-    std::vector<float> lst = {1.0, 2.5, 3.0, 4.0, 5.5};
-    long long odd_sum = double_the_difference(lst);
+    std::vector<float> lst = {1.5, 2.0, 3.0, 4.5};
+    long long result = double_the_difference(lst);
     return 0;
 }
