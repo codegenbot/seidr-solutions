adccedb251fe2cd69b97c486647ce16d846607fe
beep boop <githubbot@vadim.me>
2024-07-23 20:36:33 +0200

    Solution 67 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/5ulxc7kw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/600952c0-5303-4332-a2da-47665fac2 116.py", line 1, in <module>     print(fuel_cost((input("Enter space-separated positive integers:").split())) )           ^^^^^^^^^ NameError: name 'fuel_cost' is not defined

--- fuel-cost.py
+++ fuel-cost.py
@@ -1,2 +1,4 @@
-def fuel_cost(numbers):
-    return sum((n // 3 - 2) for n in map(int, numbers.split()))
+def fuel_cost(vector):
+    return sum((i // 3 - 2) for i in vector)
+
+print(fuel_cost(input("Enter space-separated positive integers: ").split()))
