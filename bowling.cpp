8756f25a9ac08f00b80cbafc93a553f63f9e0b08
beep boop <githubbot@vadim.me>
2024-07-24 00:58:39 +0200

    Solution 24 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/rl9rebse Prompt: 91c3b78e-8be9-4799-abf1-d8d9cea17989.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string str) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -3,29 +3,25 @@
 int bowlingScore(string str) {
     int score = 0;
     int i = 0;
-    while (i < str.length()) {
+    while (i < 10) {
         if (str[i] == '/') {
             string firstPart = str.substr(0, i);
             string secondPart = str.substr(i + 1);
-            int firstScore = (firstPart[0] - '0') * 10 + (firstPart.length() > 1 ? (firstPart[1] - '0') : 0);
-            if (secondPart.length() >= 2) {
+            int firstScore = (firstPart[0] - '0') * 10 + (firstPart.length() > 1 ? (firstPart[1] - '0') : 10);
+            if (secondPart.length() == 2) {
                 int secondScore = (secondPart[0] - '0') * 10 + (secondPart[1] - '0');
-                score += max(firstScore, secondScore) + min(firstScore, secondScore);
+                score += firstScore + secondScore;
             } else {
-                score += firstScore;
+                score += firstScore + (secondPart[0] - '0') * 10;
             }
             i++;
         } else {
-            if (i < str.length() - 1 && str[i+1] == '/') {
-                int currentScore = (str[i] - '0') * 10;
-                score += currentScore;
-                i++;
-            } else {
-                int currentScore = (str[i] - '0') * 10 + (i < str.length() - 1 && str[i+1] == '/' ? 10 : 0);
-                score += currentScore;
-                i++;
+            int frameScore = (str[i] - '0') * 10;
+            if (i < 8 && str[i + 1] != '/') {
+                frameScore += (str[i + 1] - '0') * 10;
             }
+            score += frameScore;
+            i++;
         }
     }
     return score;
-}
