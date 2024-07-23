d9444fc03a7c54e1df6d29ae28c292d877265f44
beep boop <githubbot@vadim.me>
2024-07-23 12:59:50 +0200

    Solution 6 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/h1g35jgb Prompt: In file included from 7d6d5973-e7ad-4494-9c7f-cc2d4cb5cbbf.cpp:23: 7d6d5973-e7ad-4494-9c7f-cc2d4cb5cbbf.cpp: In function ‘int main()’: 7d6d5973-e7ad-4494-9c7f-cc2d4cb5cbbf.cpp:33:20: error: ‘count_up_to’ was not dec lared in this scope; did you mean ‘count_upto’?    33 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,20 +1,30 @@
+```
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> count_up_to(int n) {
-    vector<int> result;
-    for (int i = 2; i < n; ++i) {
+vector<int> count_upto(int n) {
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
         bool isPrime = true;
-        for (int j = 2; j <= sqrt(double(i)); ++j) {
+        for (int j = 2; j * j <= i; ++j) {
             if (i % j == 0) {
                 isPrime = false;
                 break;
             }
         }
-        if (isPrime)
-            result.push_back(i);
+        if (isPrime) {
+            primes.push_back(i);
+        }
     }
-    return result;
+    return primes;
+}
+
+int main() {
+    vector<int> result = count_upto(101);
+    for (int prime : result) {
+        cout << prime << " ";
+    }
+    return 0;
 }
