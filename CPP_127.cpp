85a60b90069ce9fa6d391aea6c8cee9690ce1078
beep boop <githubbot@vadim.me>
2024-07-23 16:29:55 +0200

    Solution 9 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/msigrzkd Prompt: fc857b1c-3833-409a-8c37-eef14bb3f23c.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string intersection(vector<int> interval1, vector<int> interval2) {       | ^~~~~~ In file included from fc857b1c-3833-409a-8c37-eef14bb3f23c.cpp:21: fc857b1c-3833-409a-8c37-eef14bb3f23c.cpp: In function ‘int main()’: fc857b1c-3833-409a-8c37-eef14bb3f23c.cpp:23:13: error: ‘intersection’ was not de clared in this scope    23 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,15 +1,5 @@
 #include <string>
 
-bool isPrime(int n) {
-    if (n <= 1)
-        return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
-            return false;
-    }
-    return true;
-}
-
 string intersection(vector<int> interval1, vector<int> interval2) {
     int start = max(interval1[0], interval2[0]);
     int end = min(interval1[1], interval2[1]);
@@ -19,5 +9,13 @@ string intersection(vector<int> interval1, vector<int> interval2) {
 
     int len = end - start + 1;
 
-    return isPrime(len) ? "YES" : "NO";
+    bool isPrime = true;
+    for (int i = 2; i * i <= len; i++) {
+        if (len % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    return isPrime ? "YES" : "NO";
 }
