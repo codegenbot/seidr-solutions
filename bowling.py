494852d4637235c709e03d5f5185414a4f1476ae
beep boop <githubbot@vadim.me>
2024-07-23 20:28:53 +0200

    Solution 21 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/14zv9x1k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/646d0380-277d-4b73-9af2-858c86a16 f5e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,17 +1,25 @@
 ```
 import re
+
 def bowling_score(frames):
     score = 0
     roll = [int(x) for x in re.sub('/|-', '', frames)]
-    i = 0
-    while i < len(roll):
-        if roll[i] == 10:
-            score += roll[i]
-            i += 1
-        elif i + 1 < len(roll):
-            score += min(roll[i], 10-roll[i-1]) + roll[i]
-            i += 2
+    for i in range(10):
+        if i < len(roll) - 1 and i < len(roll) - 2:
+            if roll[i] == 10:
+                score += roll[i]
+                score += roll[i+1]+roll[i+2]
+                i += 2
+            elif roll[i] + roll[i+1] <= 10:
+                score += roll[i] + roll[i+1]
+            else:
+                score += 10
+                score += roll[i+2]
         else:
-            score += roll[i]
-            i += 1
+            if roll[i] == 10:
+                score += roll[i]
+                if i < len(roll) - 2:
+                    score += roll[i+1]+roll[i+2]
+            elif roll[i] + (i < len(roll) - 1 and roll[i+1]) <= 10:
+                score += roll[i] + (i < len(roll) - 1 and roll[i+1])
     return score
