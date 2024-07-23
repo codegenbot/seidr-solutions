772514a99e9241ff04895ce908a3d2e50e567a80
beep boop <githubbot@vadim.me>
2024-07-23 13:01:44 +0200

    Solution 79 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/k1fnh68u Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,16 +1,10 @@
-for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            vector<int> temp;
-            for (int j = i; j < l.size(); j += 3) {
-                temp.push_back(l[j]);
-            }
-            sort(temp.begin(), temp.end());
-            int index = 0;
-            for (int j = i; j < l.size(); j += 3) {
-                l[j] = temp[index];
-                ++index;
-            }
+vector<int> res = l;
+    for(int i = 0; i < l.size(); ++i){
+        if(i % 3 == 0){
+            int start = i;
+            int end = min((int)l.size(), i + 3);
+            sort(res.begin() + start, res.begin() + end);
         }
     }
-    return l;
+    return res;
 }
