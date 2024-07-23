7b64d1716ac5ef6d2d68a4736d2997f9227abecd
beep boop <githubbot@vadim.me>
2024-07-23 11:42:08 +0200

    Solution 9 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/fi5yu695 Prompt:  this scope    21 |         cout << nums[i] << endl;       |         ^~~~ 6dfe0548-94f2-4655-bdd7-39c0a5bfc793.cpp:21:17: error: ‘nums’ was not declared i n this scope    21 |         cout << nums[i] << endl;       |                 ^~~~ 6dfe0548-94f2-4655-bdd7-39c0a5bfc793.cpp:21:28: error: ‘endl’ was not declared i n this scope    21 |         cout << nums[i] << endl;       |                            ^~~~ 6dfe0548-94f2-4655-bdd7-39c0a5bfc793.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << endl;       |     ^~~~ 6dfe0548-94f2-4655-bdd7-39c0a5bfc793.cpp:23:13: error: ‘endl’ was not declared i n this scope    23 |     cout << endl;       |             ^~~~ 6dfe0548-94f2-4655-bdd7-39c0a5bfc793.cpp:25:17: error: ‘nums’ was not declared i n this scope    25 |         cout << nums[i] << endl;       |                 ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -9,6 +9,7 @@ int main() {
     int n;
     cin >> n;
     vector<int> nums(n);
+
     for (int i = 0; i < n; ++i) {
         cin >> nums[i];
     }
@@ -28,6 +29,7 @@ int main() {
         cout << nums[i] << endl;
     }
     cout << endl;
+    
     for (int i = cut_index; i < n; ++i) {
         cout << nums[i] << endl;
     }
