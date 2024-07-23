df80902ebc735b974240fbc1cc8e31e457f51ed4
beep boop <githubbot@vadim.me>
2024-07-23 17:07:45 +0200

    Solution 16 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/gaof38ml Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9e1904ed-cfbf-46ce-8a0f-b212e05cf 042.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -2,10 +2,10 @@ def match_parens(lst):
     stack = []
     for s in lst:
         for c in s:
-            if c == "(":
+            if c == '(':
                 stack.append(c)
-            elif c == ")":
+            elif c == ')':
                 if len(stack) == 0:
-                    return "No"
+                    return 'No'
                 stack.pop()
-    return "Yes" if len(stack) == 0 else "No"
+    return 'Yes' if len(stack) == 0 else 'No'
