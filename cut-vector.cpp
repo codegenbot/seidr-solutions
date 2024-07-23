31d1108fd526ac149aa30f0f6724bc475d9f84c9
beep boop <githubbot@vadim.me>
2024-07-23 13:10:18 +0200

    Solution 32 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/2x3zn4j9 Prompt: d149f711-5d85-4fcc-99f3-30d687b42128.cpp: In function ‘std::vector<int> cutVecto r(std::vector<int>&)’: d149f711-5d85-4fcc-99f3-30d687b42128.cpp:10:19: error: ‘INT_MAX’ was not declare d in this scope    10 |     int minDiff = INT_MAX;       |                   ^~~~~~~ d149f711-5d85-4fcc-99f3-30d687b42128.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; d149f711-5d85-4fcc-99f3-30d687b42128.cpp:14:20: error: ‘abs’ was not declared in  this scope    14 |         int diff = abs(leftSum - rightSum);       |                    ^~~ d149f711-5d85-4fcc-99f3-30d687b42128.cpp:23:30: error: could not convert ‘{leftV ec, rightVec}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<int>’    23 |     return {leftVec, rightVec};       |                              ^       |                              |       |                              <brace-enclosed initializer list>

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,10 +1,9 @@
 #include <vector>
 #include <climits>
 #include <cmath>
-
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
+vector<vector<int>> cutVector(vector<int>& nums) {
     int leftSum = 0;
     for (int i = 0; i < nums.size(); i++) {
         leftSum += nums[i];
@@ -14,14 +13,14 @@ pair<vector<int>, vector<int>> cutVector(vector<int>& nums) {
     int cutIndex = -1;
     for (int i = 0; i < nums.size() - 1; i++) {
         int rightSum = leftSum - nums[i];
-        int diff = std::abs(leftSum - rightSum);
+        int diff = abs(leftSum - rightSum);
         if (diff <= minDiff) {
             minDiff = diff;
             cutIndex = i;
         }
     }
 
-    vector<int> leftVec(nums.begin(), nums.begin() + cutIndex + 1);
-    vector<int> rightVec(nums.begin() + cutIndex + 1, nums.end());
+    vector<int> leftVec(nums.begin(), nums.begin() + cutIndex+1);
+    vector<int> rightVec(nums.begin()+cutIndex, nums.end());
     return {leftVec, rightVec};
 }
