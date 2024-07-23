3ce8715f0968b286a75e56a5c69c15ad8618935f
beep boop <githubbot@vadim.me>
2024-07-23 19:16:39 +0200

    Solution 57 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jovpljj4 Prompt:       |                                  ^~~ 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp:22: 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp: In function ‘int main()’: 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp:32:20: error: ‘compare’ was not declare d in this scope    32 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 9165f149-0a31-4158-8ed6-c0ce5bb99336.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -10,20 +10,24 @@ std::vector<int> compareVect(const std::vector<int>& game, const std::vector<int
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
             result.push_back(2);
-        } else if (std::find(game.begin(), game.end(), guess[i]) != game.end()) {
-            result.push_back(1);
         } else {
-            result.push_back(0);
+            for (int j = 1; j <= 9; j++) {
+                if (std::to_string(j) == std::to_string(guess[i]).substr(game[i] - j > 0 ? game[i] - j : j)) {
+                    result.push_back(1);
+                    break;
+                }
+                else if(std::to_string(j) == std::to_string(guess[i]).substr(game[i] + j, std::string::npos)) {
+                    result.push_back(1);
+                    break;
+                }
+            }
+            result.push_back(std::abs(game[i] - guess[i]));
         }
     }
     return result;
 }
 
 int main() {
-    assert(isSame(compareVect({1,2,3,5},{-1,2,3,4}),{2,1,1,1}));
-    std::cout << "Comparison vector: ";
-    for (int i : compareVect({1,2,3,5},{-1,2,3,4})) {
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
+    assert(issame(compareVect({1,2,3,5},{-1,2,3,4}),{2,1,1,1}));
+    //...
 }
