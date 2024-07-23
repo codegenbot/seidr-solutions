85bba5abc41a8e88ef48f7eff57344a3847dae54
beep boop <githubbot@vadim.me>
2024-07-23 10:07:22 +0200

    Solution 62 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/n2pjcdj2 Prompt: Program did not receive expected input

--- Python_5.py
+++ Python_5.py
@@ -3,10 +3,9 @@ from typing import List
 
 def intersperse(numbers: List[int], delimiter: int) -> List[int]:
     if len(numbers) == 1:
-        result = [numbers[0]]
-        for i in range(1, len(numbers) - 1):
-            result.extend([delimiter])
-            result.append(numbers[0])
+        result = [numbers[0]] * (len(numbers) - 1)
+        result.insert(0, numbers[0])
+        result.extend([delimiter] + ([numbers[0]] * (len(numbers) - 2)))
     else:
         result = [numbers[0]]
         for i in range(1, len(numbers)):
