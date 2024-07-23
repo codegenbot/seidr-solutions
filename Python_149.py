5da9bf12f85cd4fcf88ce654e9eb1654d14485e9
beep boop <githubbot@vadim.me>
2024-07-23 17:53:57 +0200

    Solution 18 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wz6rl5y0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a740d0dd-2b4e-4532-aea7-f674d5e58 de6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,16 +1,16 @@
+```Python
 def sorted_list_sum(lst):
     odd_word_count = 0
     even_word_count = 0
-
+    
     for word in lst:
-        if len(word) % 2 == 1:
+        if len(word) % 2 == 1: 
             odd_word_count += 1
         else:
             even_word_count += 1
-
+    
     print(f"Number of words with odd length: {odd_word_count}")
     print(f"Number of words with even length: {even_word_count}")
-
 
 user_input = input("Please enter a list of words separated by commas: ")
 
