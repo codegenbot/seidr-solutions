1a9e2ab4b1b2ec35213f13ce20180b17ec5dcffd
beep boop <githubbot@vadim.me>
2024-07-23 12:03:02 +0200

    Solution 14 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yt1rdxkw Prompt:  int)’: 639c42c7-1d9b-484e-b935-c8b5eac5d136.cpp:9:23: error: ‘log’ was not declared in this scope; did you mean ‘long’?     9 |     double y = pow(n, log(x) / log(n));       |                       ^~~       |                       long 639c42c7-1d9b-484e-b935-c8b5eac5d136.cpp:9:16: error: ‘pow’ was not declared in this scope     9 |     double y = pow(n, log(x) / log(n));       |                ^~~ 639c42c7-1d9b-484e-b935-c8b5eac5d136.cpp:10:12: error: ‘round’ was not declared in this scope    10 |     return round(y) == x;       |            ^~~~~ 639c42c7-1d9b-484e-b935-c8b5eac5d136.cpp: At global scope: 639c42c7-1d9b-484e-b935-c8b5eac5d136.cpp:14:5: error: redefinition of ‘int main( )’    14 | int main(){       |     ^~~~ 639c42c7-1d9b-484e-b935-c8b5eac5d136.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,9 +1,13 @@
 #include <cmath>
+#include <assert.h>
 
-bool is_simple_power(int x) {
-    for (int n = 1; n <= sqrt(x); n++) {
-        double y = pow(n, ceil(log2(x)/log2(n)));
-        if (y == x) return true;
-    }
-    return false;
+bool is_simple_power(int x, int n);
+
+int main() {
+    assert(is_simple_power(1, 12));
+}
+
+bool is_simple_power(int x, int n){
+    double y = pow(n, std::log(x) / std::log(n));
+    return (y + 0.5) == x;
 }
