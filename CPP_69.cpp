74bccbde79109b0928229d72d4ebfad527bfce7e
beep boop <githubbot@vadim.me>
2024-07-23 14:57:58 +0200

    Solution 38 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1oyb0r05 Prompt: in this scope     4 | int search(vector<int> lst) {       |            ^~~~~~ 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp:4:12: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp:4:19: error: expected primary-expressio n before ‘int’     4 | int search(vector<int> lst) {       |                   ^~~ In file included from 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp:16: 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp: In function ‘int main()’: 86dbbce0-f384-4696-9bf8-2294a109cb21.cpp:18:19: error: ‘search’ cannot be used a s a function    18 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,15 +1,11 @@
-#include <map>
-#include <vector>
-
-int search(std::vector<int> lst) {
-    std::map<int, int> countMap;
-    for (auto i : lst) {
+int search(vector<int> myVector) {
+    map<int, int> countMap;
+    for (auto i : myVector) {
         countMap[i]++;
     }
     
     for (auto p : countMap) {
-        if (p.second > 1) 
-            return p.first; 
+        if (p.second > 1) return p.first; 
     }
     return -1;
 }
