a1a419bf6a7f849093aba234c3344dff8582640a
beep boop <githubbot@vadim.me>
2024-07-23 13:37:07 +0200

    Solution 28 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/kha5sf0r Prompt: this scope    21 |     cin >> n;       |     ^~~ be40d090-0b20-465e-aebb-ce46f53a36d6.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; be40d090-0b20-465e-aebb-ce46f53a36d6.cpp:26:5: error: ‘cout’ was not declared in  this scope    26 |     cout << luhn(card) << endl;       |     ^~~~ be40d090-0b20-465e-aebb-ce46f53a36d6.cpp:26:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? be40d090-0b20-465e-aebb-ce46f53a36d6.cpp:26:27: error: ‘endl’ was not declared i n this scope    26 |     cout << luhn(card) << endl;       |                           ^~~~ be40d090-0b20-465e-aebb-ce46f53a36d6.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- luhn.cpp
+++ luhn.cpp
@@ -1,15 +1,12 @@
-#include <vector>
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int luhn(vector<int> card) {
     int sum = 0;
-    for(int i = 0; i < card.size(); i++) {
-        if(i % 2 == 1) {
-            int double_digit = card[i] * 2;
-            if(double_digit > 9) double_digit -= 9;
-            sum += double_digit;
+    for (int i = card.size() - 1; i >= 0; i--) {
+        if ((i % 2 != 0) || (card[i] * 2 > 9)) {
+            sum += ((card[i] * 2) % 10) + (((card[i] * 2) / 10));
         } else {
             sum += card[i];
         }
@@ -21,7 +18,7 @@ int main() {
     int n;
     cin >> n;
     vector<int> card(n);
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> card[i];
     }
     cout << luhn(card) << endl;
