a7e80154b0fd3306d2c8a2d646a447da9fc11711
beep boop <githubbot@vadim.me>
2024-07-23 07:07:54 +0200

    Solution 61 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,64 +1,53 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <boost/any.hpp>
-using namespace std;
-
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return max((int)a.to_type<int>(), (double)b.to_type<double>());
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        if ((string)a.to_type<string>() > b.to_type<string>()) {
-            return a;
-        }
-        else if ((string)a.to_type<string>() < b.to_type<string>()) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        if ((double)a.to_type<double>() > stod((string)b.to_type<string>())) {
-            return a;
-        }
-        else if ((double)a.to_type<double>() < stod((string)b.to_type<string>())) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        if ((string)a.to_type<string>() > to_string((int)b.to_type<int>())) {
-            return a;
-        }
-        else if ((string)a.to_type<string>() < to_string((int)b.to_type<int>())) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        if ((string)a.to_type<string>() > to_string((double)b.to_type<double>())) {
-            return a;
-        }
-        else if ((string)a.to_type<string>() < to_string((double)b.to_type<double>())) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
-    else if (a.type() == typeid(int) && a.to_type<int>() > b.to_type<int>()) {
-        return a;
-    }
-    else if (b.type() == typeid(int) && b.to_type<int>() > a.to_type<int>()) {
         return b;
-    }
-    else {
-        return boost::any("None");
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = any_cast<string>(a);
+        string str2 = any_cast<string>(b);
+        size_t pos1 = str1.find(',');
+        size_t pos2 = str2.find(',');
+        double num1 = stod(str1.substr(0, pos1));
+        double num2 = stod(str2.substr(0, pos2));
+        if (num1 > num2) {
+            return a;
+        } else if (num1 < num2) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
+    } else if (a.type() == typeid(string)) {
+        string str = any_cast<string>(a);
+        size_t pos = str.find(',');
+        double num = stod(str.substr(0, pos));
+        if (num > b.convert_to<double>()) {
+            return a;
+        } else if (num < b.convert_to<double>()) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
+    } else if (b.type() == typeid(string)) {
+        string str = any_cast<string>(b);
+        size_t pos = str.find(',');
+        double num = stod(str.substr(0, pos));
+        if (a.convert_to<double>() > num) {
+            return a;
+        } else if (a.convert_to<double>() < num) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
+    } else {
+        double num1 = any_cast<double>(a);
+        double num2 = any_cast<double>(b);
+        if (num1 > num2) {
+            return a;
+        } else if (num1 < num2) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
     }
 }
