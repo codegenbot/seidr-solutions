eb7a77e9fd1828aece92480094a7163a89d16623
beep boop <githubbot@vadim.me>
2024-07-23 09:55:38 +0200

    Solution 14 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- bowling.cpp
+++ bowling.cpp
@@ -1,6 +1,41 @@
+```c++
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int bowlingScore(string s) {
+    int score = 0;
+    string frames[] = {"X", "X", "X", "X", "X", "X", "X", "X", "X", "X"};
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'X') {
+            score += 10;
+        } else if (s[i] == '/') {
+            score += 10 - ((frames[9-i][0] - '0') + (frames[9-i][1] - '0'));
+        } else {
+            frames[9-i] = s.substr(i, 2);
+        }
+    }
+    
+    for (int i = 0; i < 9; i++) {
+        if (frames[i] == "X") {
+            score += 10;
+        } else if (frames[i][1] == '/') {
+            score += 10 - ((frames[i][0] - '0') + (frames[i][1] - '0'));
+        } else {
+            score += (frames[i][0] - '0') + (frames[i][1] - '0');
+        }
+    }
+    
+    return score;
+}
+
 int main() {
-    string s = "XXXX/X/9-8/6/-5-X/4-X/3-2/1-";
-    int score = bowlingScore(s);
+    string input;
+    cout << "Enter the scores of each frame separated by spaces or '/' for strikes or spares: ";
+    getline(cin, input);
+    int score = bowlingScore(input);
     cout << "Score: " << score << endl;
     return 0;
 }
