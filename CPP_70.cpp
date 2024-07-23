5ac7f4633854710474a8b085a57a334e0efd16c7
beep boop <githubbot@vadim.me>
2024-07-23 11:05:32 +0200

    Solution 9 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4e3v8dte Prompt:       |                    ^~~ 91220b46-18d6-4f32-9487-73597a348ddb.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 91220b46-18d6-4f32-9487-73597a348ddb.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 91220b46-18d6-4f32-9487-73597a348ddb.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 91220b46-18d6-4f32-9487-73597a348ddb.cpp:24: 91220b46-18d6-4f32-9487-73597a348ddb.cpp: In function ‘int main()’: 91220b46-18d6-4f32-9487-73597a348ddb.cpp:34:20: error: ‘strange_sort_list’ was n ot declared in this scope    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 91220b46-18d6-4f32-9487-73597a348ddb.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,3 +1,15 @@
+```cpp
+#include <vector>
+#include <algorithm>
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
@@ -6,26 +18,19 @@ vector<int> strange_sort_vector(vector<int> lst) {
     min_max.push_back(*min_element(lst.begin(), lst.end()));
     min_max.push_back(*max_element(lst.begin(), lst.end()));
 
-    while (!lst.empty() && *min_element(lst.begin(), lst.end()) != *min_max[0] && *min_element(lst.begin(), lst.end()) != *min_max[1]) {
-        result.push_back(*min_element(lst.begin(), lst.end()));
-        lst.erase(std::remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
-    }
-
-    while (!lst.empty() && *max_element(lst.begin(), lst.end()) != *min_max[0] && *max_element(lst.begin(), lst.end()) != *min_max[1]) {
-        result.push_back(*max_element(lst.begin(), lst.end()));
-        lst.erase(std::remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
+    while (!lst.empty()) {
+        int temp = *min_element(lst.begin(), lst.end());
+        for (int i = 0; i < lst.size(); i++) {
+            if (lst[i] == temp) {
+                result.push_back(temp);
+                lst.erase(std::remove(lst.begin(), lst.end(), temp), lst.end());
+                break;
+            }
+        }
     }
 
     for (int i = 0; i < min_max.size(); i++) {
         result.push_back(min_max[i]);
     }
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
 }
