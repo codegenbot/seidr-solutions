434fda6c60713f82d087aa678f1c34340e7fbd50
beep boop <githubbot@vadim.me>
2024-07-23 22:17:42 +0200

    Solution 93 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0wkes509 Prompt: 606ed251-9bb5-4a38-a246-55f43b90f9b7.cpp:1:21: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                     ^~~~~~ 606ed251-9bb5-4a38-a246-55f43b90f9b7.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int mastermind(std::string code, std::string guess) { 606ed251-9bb5-4a38-a246-55f43b90f9b7.cpp:1:39: error: ‘string’ is not a member o f ‘std’     1 | int mastermind(std::string code, std::string guess) {       |                                       ^~~~~~ 606ed251-9bb5-4a38-a246-55f43b90f9b7.cpp:1:39: note: ‘std::string’ is defined in  header ‘<string>’; did you forget to ‘#include <string>’? 606ed251-9bb5-4a38-a246-55f43b90f9b7.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(std::string code, std::string guess) {       |                                                   ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
@@ -10,18 +12,18 @@ int mastermind(std::string code, std::string guess) {
     }
 
     // Count the number of white pegs (correct color, wrong place)
-    int code_counts[6] = {0};
+    int code_counts[6] = {0}; 
     for (char c : code) {
-        code_counts[c - 'a']++;
-    }
+        code_counts[c - 'a']++; 
+    } 
 
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
-            continue;
+            black--;
         }
-        if (code_counts[guess[i] - 'a']) {
-            white++;
-            code_counts[guess[i] - 'a']--;
+        else {
+            white += code_counts[guess[i] - 'a']; 
+            code_counts[guess[i] - 'a']--; 
         }
     }
 
