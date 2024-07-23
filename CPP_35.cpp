a8bbe72e4cfb4ab25f1c78c3811da32ba8612862
beep boop <githubbot@vadim.me>
2024-07-23 09:14:10 +0200

    Solution 3 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0t2383je Prompt: 9f99e7a5-9962-4a4e-94dc-9dfcc5e18748.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return *max_element(l.begin(), l.end());       | ^~~~~~ In file included from 9f99e7a5-9962-4a4e-94dc-9dfcc5e18748.cpp:3: 9f99e7a5-9962-4a4e-94dc-9dfcc5e18748.cpp: In function ‘int main()’: 9f99e7a5-9962-4a4e-94dc-9dfcc5e18748.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 9f99e7a5-9962-4a4e-94dc-9dfcc5e18748.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,5 +2,6 @@
 #include <cmath>
 
 int main() {
-    int l[] = {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10};
-    return abs(*std::max_element(l, l + sizeof(l) / sizeof(l[0])));
+    int max = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
+    return abs(max- 124);
+}
