20b5a688d1b24968be9b469f15c08a66e307ab48
beep boop <githubbot@vadim.me>
2024-07-23 22:29:22 +0200

    Solution 20 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/nwdcmyyq Prompt: ccumulate(nums.begin() + i, nums.end(), 0));       |                        ^~~~~~~~~~ 51d1d592-38a7-4e1a-81bb-1233591cfca2.cpp:14:20: error: ‘abs’ was not declared in  this scope    14 |         int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - a ccumulate(nums.begin() + i, nums.end(), 0));       |                    ^~~ 51d1d592-38a7-4e1a-81bb-1233591cfca2.cpp:22:9: error: ‘cout’ was not declared in  this scope    22 |         cout << nums[i] << endl;       |         ^~~~ 51d1d592-38a7-4e1a-81bb-1233591cfca2.cpp:22:28: error: ‘endl’ was not declared i n this scope    22 |         cout << nums[i] << endl;       |                            ^~~~ 51d1d592-38a7-4e1a-81bb-1233591cfca2.cpp:24:5: error: ‘cout’ was not declared in  this scope    24 |     cout << endl;       |     ^~~~ 51d1d592-38a7-4e1a-81bb-1233591cfca2.cpp:24:13: error: ‘endl’ was not declared i n this scope    24 |     cout << endl;       |             ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 #include <algorithm>
 
+using namespace std;
+
 int main() {
-    std::vector<int> nums;
+    vector<int> nums;
     int num;
     
-    while (std::cin >> num) {
+    while (cin >> num) {
         nums.push_back(num);
+    }
+    
+    if (nums.empty()) {
+        return 0;
     }
     
     int n = nums.size();
@@ -15,7 +22,7 @@ int main() {
     int cut_index = 0;
     
     for (int i = 1; i < n; ++i) {
-        int diff = std::abs(std::accumulate(nums.begin(), nums.begin() + i, 0) - std::accumulate(nums.begin() + i, nums.end(), 0));
+        int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - accumulate(nums.begin() + i, nums.end(), 0));
         if (diff < min_diff) {
             min_diff = diff;
             cut_index = i;
@@ -23,14 +30,14 @@ int main() {
     }
     
     for (int i = 0; i < cut_index; ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << "\n";
     }
-    std::cout << std::endl;
+    cout << "\n";
     
     for (int i = cut_index; i < n; ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << "\n";
     }
-    std::cout << std::endl;
+    cout << "\n";
     
     return 0;
 }
