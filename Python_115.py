ab7eae967fa9416ace273b3a74ede17430bedcad
beep boop <githubbot@vadim.me>
2024-07-23 17:36:34 +0200

    Solution 65 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/m7io6786 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/775b77ae-08ce-427b-b27e-4f18015e9 7fb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_115.py
+++ Python_115.py
@@ -1,7 +1,9 @@
-```
+import math
+
+
 def max_fill(grid, capacity):
     n = len(grid)
     m = len(grid[0])
     total_water = sum(sum(row) for row in grid)
-    total_buckets = -(-total_water // capacity) if total_water % capacity != 0 else total_water // capacity
+    total_buckets = math.ceil(total_water / float(capacity))
     return total_buckets
