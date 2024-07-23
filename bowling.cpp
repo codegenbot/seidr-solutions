ec7d90bf67eb62bbfa258a29a3ff811eca667852
beep boop <githubbot@vadim.me>
2024-07-23 21:43:00 +0200

    Solution 10 of bowling, ratio 0.86 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/c758c5l3 Prompt: in this scope    20 |     string s;       |     ^~~~~~ 6332176c-9dd3-4ea8-a9a7-0d234192d413.cpp:21:5: error: ‘cin’ was not declared in this scope    21 |     cin >> s;       |     ^~~ 6332176c-9dd3-4ea8-a9a7-0d234192d413.cpp:21:12: error: ‘s’ was not declared in t his scope    21 |     cin >> s;       |            ^ 6332176c-9dd3-4ea8-a9a7-0d234192d413.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << getScore(s) << endl;       |     ^~~~ 6332176c-9dd3-4ea8-a9a7-0d234192d413.cpp:22:21: error: ‘getScore’ cannot be used  as a function    22 |     cout << getScore(s) << endl;       |             ~~~~~~~~^~~ 6332176c-9dd3-4ea8-a9a7-0d234192d413.cpp:22:28: error: ‘endl’ was not declared i n this scope    22 |     cout << getScore(s) << endl;       |                            ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,28 +1,27 @@
-int score(const string& s) {
-    int total = 0;
-    int frame = 1;
-    int i = 0;
-    while (frame <= 10) {
+#include <iostream>
+using namespace std;
+
+int getScore(string s) {
+    int score = 0, frame = 0, i = 0;
+    while (frame < 10) {
         if (s[i] == 'X') {
-            total += 10;
-            total += (s[i + 1] == 'X') ? 10 : (s[i + 1] == '/' ? 10 - (s[i + 2] - '0') : s[i + 1] - '0' + s[i + 2] - '0');
+            score += 10 + (s[i+1] == 'X' ? 10 + (s[i+2] == 'X' ? 10 : (s[i+2] - '0')) : (s[i+2] == '/' ? 10 : (s[i+1] - '0' + s[i+2] - '0')));
             i++;
-        } else if (s[i + 1] == '/') {
-            total += 10;
-            total += (s[i + 2] == 'X') ? 10 : s[i + 2] - '0';
-            i += 2;
+        } else if (s[i+1] == '/') {
+            score += 10 + (s[i+2] == 'X' ? 10 : (s[i+2] - '0'));
+            i += 3;
         } else {
-            total += s[i] - '0' + s[i + 1] - '0';
+            score += (s[i] == '-' ? 0 : (s[i] - '0')) + (s[i+1] == '-' ? 0 : (s[i+1] - '0'));
             i += 2;
         }
         frame++;
     }
-    return total;
+    return score;
 }
 
 int main() {
     string s;
     cin >> s;
-    cout << score(s) << endl;
+    cout << getScore(s) << endl;
     return 0;
 }
