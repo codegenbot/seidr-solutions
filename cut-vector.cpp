40e0d5ec2a49c75ae31a0dece42c327190cc58f8
beep boop <githubbot@vadim.me>
2024-07-23 13:01:35 +0200

    Solution 22 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/2x3zn4j9 Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,38 @@
-```cpp
 #include <vector>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> nums) {
     int n = nums.size();
     vector<vector<int>> res(2);
-
+    
     for(int i = 0; i < n; i++) {
         if(i == n - 1 || nums[i] != nums[i + 1]) {
             res[0].push_back(nums[i]);
             break;
         }
     }
-
+    
     int j = n - 1;
     while(j > 0 && nums[j] == nums.back()) {
         j--;
     }
-
+    
     for(int i = j; i >= 0; i--) {
         if(i == 0 || nums[i] != nums[i - 1]) {
             res[1].push_back(nums[i]);
             break;
         }
     }
-
+    
     return res;
 }
 
 int main() {
-    vector<vector<int>> result = cutVector({5, 3, 3, 4, 5, 5});
+    vector<vector<int>> result = cutVector({10, 20, 30, 40, 50});
     cout << "Result: ";
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < result[i].size(); j++) {
-            cout << result[i][j] << " ";
-        }
+    for (auto& v : result) {
+        for (int x : v)
+            cout << x << " ";
         cout << endl;
     }
     return 0;
