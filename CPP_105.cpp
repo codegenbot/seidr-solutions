e2e7e9f1334c2adfc6e44645a22c8b97ecf914f9
beep boop <githubbot@vadim.me>
2024-07-24 01:50:15 +0200

    Solution 35 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1g16sxbf Prompt:       |                              ^~~~~~ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:52:37: error: ‘string’ was not declared  in this scope    52 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:52:45: error: expression list treated a s compound expression in initializer [-fpermissive]    52 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:51: e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp: In function ‘int main()’: e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:61:20: error: ‘by_length’ was not decla red in this scope    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,109 +1,85 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-bool issame(const std::vector<std::string>& v1, const std::vector<std::string>& v2) {
-    if (v1.size() != v2.size()) return false;
-    for (int i = 0; i < v1.size(); ++i)
-        if (v1[i] != v2[i]) return false;
+bool issame(vector<string> a, vector<string> b) {
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i])
+            return false;
+    }
     return true;
 }
 
-std::vector<string> by_length(vector<int> arr) {
+vector<string> by_length(vector<int> arr) {
     vector<int> numArr;
     for (int i : arr) {
-        if (i >= 10 && i <= 99) {
-            int tens = i / 10;
-            int ones = i % 10;
-            string str = "";
-            switch (tens) {
-                case 1:
-                    str += "Ten";
-                    break;
-                case 2:
-                    str += "Twenty";
-                    break;
-                case 3:
-                    str += "Thirty";
-                    break;
-                case 4:
-                    str += "Forty";
-                    break;
-                case 5:
-                    str += "Fifty";
-                    break;
-                case 6:
-                    str += "Sixty";
-                    break;
-                case 7:
-                    str += "Seventy";
-                    break;
-                case 8:
-                    str += "Eighty";
-                    break;
-                case 9:
-                    str += "Ninety";
-                    break;
-            }
-            switch (ones) {
-                case 0: str += ""; break;
-                case 1:
-                    str += "One";
-                    break;
-                case 2:
-                    str += "Two";
-                    break;
-                case 3:
-                    str += "Three";
-                    break;
-                case 4:
-                    str += "Four";
-                    break;
-                case 5:
-                    str += "Five";
-                    break;
-                case 6:
-                    str += "Six";
-                    break;
-                case 7:
-                    str += "Seven";
-                    break;
-                case 8:
-                    str += "Eight";
-                    break;
-                case 9:
-                    str += "Nine";
-                    break;
-            }
-            result.push_back(str);
+        if (i >= 1 && i <= 9) {
+            numArr.push_back(i);
         }
+    }
+
+    sort(numArr.begin(), numArr.end());
+
+    reverse(numArr.begin(), numArr.end());
+
+    vector<string> result;
+    for (int i : numArr) {
+        string str = "";
+        switch (i) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
     }
 
     return result;
 }
 
 int main() {
-    std::vector<int> input;
-    int num;
-    while (std::cin >> num) {
-        input.push_back(num);
+    vector<int> arr;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
     }
-    vector<string> output = by_length(input);
+    vector<string> output = by_length(arr);
 
-    if (output.empty()) {
-        std::cout << "No numbers to convert." << std::endl;
+    if (output.size() == 9) {
+        cout << "Yes" << endl;
     } else {
-        for (const auto& str : output)
-            std::cout << str << " ";
-        std::cout << std::endl;
-
-        bool same = issame(output, output);
-        if (same) {
-            std::cout << "All numbers are the same." << std::endl;
-        } else {
-            std::cout << "Numbers are not all the same." << std::endl;
-        }
+        cout << "No" << endl;
     }
+
+    for (string str : output)
+        cout << str << " ";
+    cout << endl;
 
     return 0;
 }
