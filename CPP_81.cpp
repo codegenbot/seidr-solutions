a8cccc8f181286ca1c9887454846ce1b3b5911f5
beep boop <githubbot@vadim.me>
2024-07-23 14:31:53 +0200

    Solution 90 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/00657p5h Prompt: Complete the following code given the task description and function signature.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,6 +1,5 @@
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
-
     for (float gpa : grades) {
         if (gpa == 4.0) letter_grades.push_back("A+");
         else if (gpa > 3.7) letter_grades.push_back("A");
@@ -16,6 +15,5 @@ vector<string> numerical_letter_grade(vector<float> grades) {
         else if (gpa > 0.0) letter_grades.push_back("D-");
         else letter_grades.push_back("E");
     }
-
     return letter_grades;
 }
