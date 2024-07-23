a0b37f3d649ebba091e0db231e868840525825b7
beep boop <githubbot@vadim.me>
2024-07-23 17:19:57 +0200

    Solution 82 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l17t97y6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5587226d-6cef-4b85-8536-4c9cefa58 e72.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,7 +1,4 @@
 def solve(N):
     return str(bin(N))[2:].zfill(len(str(bin(N))[2:])) + bin(
         sum(int(digit) for digit in str(N))
-    )[2:].zfill(len(bin(sum(int(digit) for digit in str(N)))[2:]))
-
-
-print(solve(12345))
+    )[2:].zfill(len(str(bin(N))[2:]))
