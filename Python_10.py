be1f7f9557947eb0380bc81e9c814d792b5333ca
beep boop <githubbot@vadim.me>
2024-07-23 23:48:29 +0200

    Solution 24 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/d90l9i2w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae699d7c-ea11-4577-93b9-f2dc163f4 1b7.py", line 11, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae699d7c-ea11-4577-93b9-f2dc163f4 1b7.py", line 8, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,5 +1,8 @@
 def make_palindrome(s: str) -> str:
     for i in range(len(s), -1, -1):
         if s[:i] == s[:i][::-1]:
-            mid = len(s) // 2
-            return s[:mid].lower() + s[mid].upper() + s[:i][::-1].lower()
+            return (
+                s[: len(s) // 2].lower()
+                + (s[len(s) // 2 : i][0]).upper()
+                + s[i:][::-1].lower()
+            )
