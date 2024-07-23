a80f9c60fae60758dafdc5facac15e5071b9822f
beep boop <githubbot@vadim.me>
2024-07-24 01:54:48 +0200

    Solution 76 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ltngqvn8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c24ccff0-d0d0-42c4-b460-bd01f1300 be2.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c24ccff0-d0d0-42c4-b460-bd01f1300 be2.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,4 @@
 def remove_duplicates(input_list=None):
     if input_list is None:
-        input_list = list(map(int, input("Enter numbers separated by space: ").split()))
-    if not input_list:  
-        return []  
-    return sorted(list(set(input_list)))
+        input_list = sorted(map(int, input("Enter numbers separated by space: ").split()))
+    return list(dict.fromkeys(input_list))
