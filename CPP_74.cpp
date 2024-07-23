200af0d436d3916fa1f6bd0ee30e50ae43ad7e2d
beep boop <githubbot@vadim.me>
2024-07-23 11:04:41 +0200

    Solution 6 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4d2aq60w Prompt:       |                              ^~~~~~ e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp:21: e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp: In function ‘int main()’: e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp:31:20: error: ‘total_match’ was not dec lared in this scope    31 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ e29221c1-683a-49e6-951b-8b0a71a6d7ef.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,48 +1,28 @@
-```
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
+bool issame(vector<string> a,vector<string>b){
+    return (a==b);
+}
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    vector<string> result;
-    int len1 = 0, len2 = 0;
-
-    // Calculate the total length of strings in both vectors
+    int sum1 = 0;
     for (const string& s : lst1) {
-        len1 += s.length();
+        sum1 += s.length();
     }
+
+    int sum2 = 0;
     for (const string& s : lst2) {
-        len2 += s.length();
+        sum2 += s.length();
     }
 
-    // Compare the lengths and add strings to result vector accordingly
-    if (len1 < len2) {
-        for (const string& s : lst1) {
-            result.push_back(s);
-        }
-    } else if (len2 < len1) {
-        for (const string& s : lst2) {
-            result.push_back(s);
-        }
+    if (sum1 < sum2) {
+        return lst1;
+    } else if (sum1 > sum2) {
+        return lst2;
     } else {
-        for (const string& s : lst1) {
-            result.push_back(s);
-        }
+        return lst1;
     }
-
-    return result;
 }
 
 int main() {
-    vector<string> vec1 = {"hi", "admin"};
-    vector<string> vec2 = {"hI", "Hi"};
-
-    vector<string> result = total_match(vec1, vec2);
-
-    for (const string& s : result) {
-        cout << s << endl;
-    }
-
+    assert(issame(total_match({"this"}, {}), {""}));
     return 0;
 }
