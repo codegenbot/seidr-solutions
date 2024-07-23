67832442fe2e954903ca516630c4064262f7bb08
beep boop <githubbot@vadim.me>
2024-07-24 01:15:38 +0200

    Solution 46 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/s1xrt7nh Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from d1ccd510-108b-43f0-a1f0-d4e0003c09cf.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,14 +1,11 @@
-#include <vector>
-#include <cmath>
-#include <initializer_list>
-
+```cpp
 double vectorDistance(std::vector<float> v1, std::vector<float> v2) {
     if (v1.size() != v2.size()) {
         throw std::invalid_argument("Vectors must be the same size");
     }
     double sum = 0.0;
-    for (int i = 0; i < v1.size(); i++) {
-        double diff = v2[i] - v1[i];
+    for (auto& elem : v1) {
+        auto diff = v2.at(std::distance(v1.begin(), std::prev(v1.end())) - 1) - elem; 
         sum += diff * diff;
     }
     
