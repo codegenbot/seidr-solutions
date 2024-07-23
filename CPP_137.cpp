ac46c43a9cf0255e188a9dd617d930b942a4c769
beep boop <githubbot@vadim.me>
2024-07-23 08:32:51 +0200

    Solution 5 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/fsg8yz42 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,26 +1,28 @@
-if(a == b) {
-    return "None";
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    int val_a = boost::any_cast<int>(a);
+    int val_b = boost::any_cast<int>(b);
+    if(val_a > val_b) return val_a;
+    else if(val_b > val_a) return val_b;
+    else return "None";
 }
-if(a.type() == typeid(int) && b.type() == typeid(int)) {
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
-        return a;
-    } else {
-        return b;
-    }
+else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    float val_a = boost::any_cast<float>(a);
+    float val_b = boost::any_cast<float>(b);
+    if(val_a > val_b) return val_a;
+    else if(val_b > val_a) return val_b;
+    else return "None";
 }
-if(a.type() == typeid(float) && b.type() == typeid(float)) {
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
-        return a;
-    } else {
-        return b;
-    }
+else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    string val_a = boost::any_cast<string>(a);
+    string val_b = boost::any_cast<string>(b);
+    if(stof(val_a) > stof(val_b)) return val_a;
+    else if(stof(val_b) > stof(val_a)) return val_b;
+    else return "None";
 }
-if(a.type() == typeid(string) && b.type() == typeid(string)) {
-    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) {
-        return a;
-    } else {
-        return b;
-    }
+else if((a.type() == typeid(int) && b.type() == typeid(string)) || (a.type() == typeid(string) && b.type() == typeid(int))){
+    if(a == b) return "None";
+    else if(a.type() == typeid(int)) return boost::any_cast<int>(a);
+    else return boost::any_cast<string>(b);
 }
 return "None";
 }
