1bf7a07780781a2cfba5b55fae1dab0050a9a72c
beep boop <githubbot@vadim.me>
2024-07-24 00:25:56 +0200

    Solution 25 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/hjse8qaa Prompt: de05f3d9-2c2b-4cb9-9d0a-afd377d1d629.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string solve(int N){       | ^~~~~~ In file included from de05f3d9-2c2b-4cb9-9d0a-afd377d1d629.cpp:18: de05f3d9-2c2b-4cb9-9d0a-afd377d1d629.cpp: In function ‘int main()’: de05f3d9-2c2b-4cb9-9d0a-afd377d1d629.cpp:20:13: error: ‘solve’ was not declared in this scope    20 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,9 +1,9 @@
 #include <string>
 
-string solve(int N){
+string solve(int N) {
     int sum = 0;
     string result = "";
-    
+
     while(N > 0) {
         int remainder = N % 2;
         if (remainder == 1)
@@ -15,3 +15,4 @@ string solve(int N){
     }
     
     return result;
+}
