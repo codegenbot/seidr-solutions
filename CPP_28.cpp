57b09434549cda3c6ecdf07cf2176c38ff4fea6d
beep boop <githubbot@vadim.me>
2024-07-23 09:10:44 +0200

    Solution 6 of CPP/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/6sj1xqmz Prompt: ab6535a7-a42c-4285-ac23-bc64463f898e.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ ab6535a7-a42c-4285-ac23-bc64463f898e.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main(){       |     ^~~~

--- CPP_28.cpp
+++ CPP_28.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <cassert>
@@ -12,6 +11,6 @@ std::string concatenate(std::vector<std::string> strings){
 }
 
 int main(){
-    assert(concatenate({"x", "y", "z", "w", "k"}) == "xyzwk");
+    assert (concatenate({"x", "y", "z", "w", "k"}) == "xyzwk");
     return 0;
 }
