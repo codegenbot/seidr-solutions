f24a19118015d17db5247edab53101f3f78b75c0
beep boop <githubbot@vadim.me>
2024-07-23 14:29:16 +0200

    Solution 86 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a4sa2iva Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bd63c374-47a3-4569-8f22-f6eff2473 e9d.py", line 10, in <module>     check(fix_spaces)           ^^^^^^^^^^ NameError: name 'fix_spaces' is not defined

--- Python_140.py
+++ Python_140.py
@@ -1,16 +1,2 @@
-def fix_spaces(text):
-    return text.replace(" ", "%20")
-
-
-def check(text):
-    if len(text) < 5:
-        return "Too short"
-    elif len(text) > 10:
-        return "Too long"
-    else:
-        return "Just right"
-
-
-text = input()
-fixed_text = fix_spaces(text)
-print(check(fixed_text))
+def fix_spaces(s):
+    return ' '.join(s.split())
