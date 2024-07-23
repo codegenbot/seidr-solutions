31e4ba7cc98d812f57bf403175e50be8b450d028
beep boop <githubbot@vadim.me>
2024-07-23 23:24:06 +0200

    Solution 12 of CPP/73, ratio 0.625 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s130dyz8 Prompt: subscript    16 |                 dp[i][j][0] = 0;       |                         ^ fd2437e1-bed3-49db-ae44-cb83b2fec43a.cpp:18:25: error: invalid types ‘__gnu_cxx: :__alloc_traits<std::allocator<int>, int>::value_type {aka int}[int]’ for array subscript    18 |                 dp[i][j][0] = 1 + min(dp[i][j-1][0], dp[i+1][j][0]);       |                         ^ fd2437e1-bed3-49db-ae44-cb83b2fec43a.cpp:18:49: error: invalid types ‘__gnu_cxx: :__alloc_traits<std::allocator<int>, int>::value_type {aka int}[int]’ for array subscript    18 |                 dp[i][j][0] = 1 + min(dp[i][j-1][0], dp[i+1][j][0]);       |                                                 ^ fd2437e1-bed3-49db-ae44-cb83b2fec43a.cpp:18:64: error: invalid types ‘__gnu_cxx: :__alloc_traits<std::allocator<int>, int>::value_type {aka int}[int]’ for array subscript    18 |                 dp[i][j][0] = 1 + min(dp[i][j-1][0], dp[i+1][j][0]);       |                                                                ^ fd2437e1-bed3-49db-ae44-cb83b2fec43a.cpp:23:22: error: invalid types ‘__gnu_cxx: :__alloc_traits<std::allocator<int>, int>::value_type {aka int}[int]’ for array subscript    23 |     return dp[0][n-1][0];       |                      ^

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,22 +1,24 @@
+#include <vector>
+using namespace std;
+
 int smallest_change(vector<int> arr) {
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(n));
+    int dp[n][n];
     
     for (int i = 0; i < n; i++) {
         dp[i][i] = 0;
     }
     
-    for (int length = 2; length <= n; length++) {
-        for (int i = 0; i < n - length + 1; i++) {
-            int j = i + length - 1;
-            
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i < n - len + 1; i++) {
+            int j = i + len - 1;
             if (arr[i] == arr[j]) {
-                dp[i][j][0] = 0;
+                dp[i][j] = 0;
             } else {
-                dp[i][j][0] = 1 + min(dp[i + 1][j - 1][0], dp[i][j - 1][0]);
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i+1][j]);
             }
         }
     }
     
-    return dp[0][n - 1][0];
+    return dp[0][n-1];
 }
