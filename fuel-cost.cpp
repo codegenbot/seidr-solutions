60694ffcbaf28494de28be76cbac4ac04c26d95c
beep boop <githubbot@vadim.me>
2024-07-24 01:55:36 +0200

    Solution 10 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/eoutbb05 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3c963b76-d8f1-45eb-8c5b-1ed4fba57642.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,26 +1,12 @@
 #include <vector>
 using namespace std;
 
-int fuelCost(vector<int> v) {
+int fuelCost(vector<int> numbers) {
     int sum = 0;
-    for (int i : v) {
-        int x = i / 3;
-        x = floor(x);
-        x -= 2;
-        sum += x;
+    for (int num : numbers) {
+        int result = ((num / 3) - 2);
+        if (result < 0) result = 0;
+        sum += result;
     }
     return sum;
-}
-
-int main() {
-    vector<int> input;
-    int n;
-    cin >> n;
-    for(int i=0; i<n; ++i){
-        int temp;
-        cin >> temp;
-        input.push_back(temp);
-    }
-    cout << fuelCost(input) << endl;
-    return 0;
 }
