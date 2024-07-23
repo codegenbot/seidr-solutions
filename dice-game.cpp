c65fb4d963a1a732fabe348e642cf434ef61f990
beep boop <githubbot@vadim.me>
2024-07-24 00:12:10 +0200

    Solution 48 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/nbe9mbei Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,12 +1,9 @@
 ```cpp
-#include <iostream>
-#include <cmath>
-
 double diceGame(int n, int m) {
     double total = (double)n * m;
     double p = 0.0;
 
-    for(int i = 1; i <= std::min(n, m); i++) {
+    for(int i = 1; i <= min(n, m); i++) {
         if(m - i > n) {
             p -= (i) / total;
         } else {
@@ -14,17 +11,11 @@ double diceGame(int n, int m) {
         }
     }
 
-    p += ((double)(n - m)) / total;
+    if(n > m) {
+        p += ((double)(m - n)) / total;
+    } else {
+        p += ((double)(n - m)) / total;
+    }
 
     return p;
-}
-
-int main() {
-    int n, m;
-    std::cout << "Enter the number of sides for Peter's die: ";
-    std::cin >> n;
-    std::cout << "Enter the number of sides for Colin's die: ";
-    std::cin >> m;
-    std::cout << "The probability that Peter rolls strictly higher than Colin is: " << diceGame(n, m) << std::endl;
-    return 0;
 }
