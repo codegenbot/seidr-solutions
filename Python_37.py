87df93eb4d822a4a4f957bee80dc1c1f7df7455e
beep boop <githubbot@vadim.me>
2024-07-23 09:45:08 +0200

    Solution 24 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5fq9li0d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5735260-46e9-44d8-81d8-1fad3d04c 20f.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5735260-46e9-44d8-81d8-1fad3d04c 20f.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,12 @@
-```Python
 def sort_even(l: list):
-    even_nums = [x for x in l if x % 2 == 0]
-    even_num = min(even_nums) if even_nums else None
-    return [even_num] * len(l) if even_num is not None else l
+    return [
+        min(
+            [
+                x,
+                l[i - 1] if i > 0 and x < l[i - 1] else None,
+                l[(i + 1) % len(l)] if x < l[(i + 1) % len(l)] else None,
+            ]
+        )
+        for i, x in enumerate(l)
+        if x % 2 == 0
+    ]
