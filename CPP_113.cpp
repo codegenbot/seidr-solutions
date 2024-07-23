40033fb8d0ad98c50fa543f44b580f7e4013e694
beep boop <githubbot@vadim.me>
2024-07-23 14:52:57 +0200

    Solution 29 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k0yp7gj7 Prompt:       |                                             ^ 60a8555d-8be8-411e-b953-5bf501d3a2f7.cpp:44:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    44 | int main(){       |         ^~ 60a8555d-8be8-411e-b953-5bf501d3a2f7.cpp:44:9: note: remove parentheses to defau lt-initialize a variable    44 | int main(){       |         ^~       |         -- 60a8555d-8be8-411e-b953-5bf501d3a2f7.cpp:44:9: note: or replace parentheses with  braces to value-initialize a variable 60a8555d-8be8-411e-b953-5bf501d3a2f7.cpp:44:11: error: a function-definition is not allowed here before ‘{’ token    44 | int main(){       |           ^ 60a8555d-8be8-411e-b953-5bf501d3a2f7.cpp:50:2: error: expected ‘}’ at end of inp ut    50 | }       |  ^ 60a8555d-8be8-411e-b953-5bf501d3a2f7.cpp:27:12: note: to match this ‘{’    27 | int main() {       |            ^

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,1 +1,9 @@
-int main
+int main 
+{
+    vector<string> test = {"271", "137", "314"};
+    vector<string> expected = {"the number of odd elements 2 in the string 1", 
+                               "the number of odd elements 1 in the string 2", 
+                               "the number of odd elements 0 in the string 3"};
+    assert(issame(odd_count(test), expected));
+    return 0;
+}
