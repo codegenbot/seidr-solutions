8436d31454ed77d424bc45c0927326ff80c33799
beep boop <githubbot@vadim.me>
2024-07-23 11:03:43 +0200

    Solution 75 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4qm5mikn Prompt: b4e7baff-3c3e-4422-86a0-966148ec003a.cpp: In function ‘int main()’: b4e7baff-3c3e-4422-86a0-966148ec003a.cpp:31:167: error: expected ‘)’ before ‘;’ token    31 | , {-12.0f, 4.0f, 2.0f, 8.0f, 3.0f, 5.0f, 11.0f, 23.0f, 12.0f, -10.0f});       |                                                                       ^       |                                                                       ) b4e7baff-3c3e-4422-86a0-966148ec003a.cpp: At global scope: b4e7baff-3c3e-4422-86a0-966148ec003a.cpp:36:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    36 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ b4e7baff-3c3e-4422-86a0-966148ec003a.cpp:6:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     6 | bool issame(vector<float> a, vector<float> b) {       |      ^~~~~~ b4e7baff-3c3e-4422-86a0-966148ec003a.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ b4e7baff-3c3e-4422-86a0-966148ec003a.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -26,6 +26,6 @@ vector<float> sort_even(vector<float> l) {
 }
 
 int main() {
-    assert(std::same(sort_even({5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f}), {-12.0f, 4.0f, 2.0f, 8.0f, 3.0f, 5.0f, 11.0f, 23.0f, 12.0f, -10.0f});
+    assert(same({sort_even({5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f})},{-12.0f, 4.0f, 2.0f, 8.0f, 3.0f, 5.0f, 11.0f, 23.0f, 12.0f, -10.0f});
     return 0;
 }
