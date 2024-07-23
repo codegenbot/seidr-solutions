366026836deb222ae33c3cc9087f7dca4bdb5fa9
beep boop <githubbot@vadim.me>
2024-07-23 18:10:58 +0200

    Solution 8 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts52pt24 Prompt: Program failed

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <string>
 using namespace std;
 
@@ -5,25 +6,16 @@ bool simplify(string x, string n) {
     int numerator1 = 0, denominator1 = 0;
     int numerator2 = 0, denominator2 = 0;
 
-    // Convert strings to integers
     size_t pos = x.find('/');
-    if (pos != string::npos) {
-        numerator1 = stoi(x.substr(0, pos));
-        denominator1 = stoi(x.substr(pos + 1));
+    numerator1 = stoi(x.substr(0, pos));
+    denominator1 = stoi(x.substr(pos + 1));
+
+    pos = n.find('/');
+    numerator2 = stoi(n.substr(0, pos));
+    denominator2 = stoi(n.substr(pos + 1));
+
+    if (denominator2 != 0) {
+        int productDenominator = denominator1 * denominator2;
+        return productNumerator % productDenominator == 0;
     }
-
-    pos = n.find '/';
-    if (pos != string::npos) {
-        numerator2 = stoi(n.substr(0, pos));
-        denominator2 = stoi(n.substr(pos + 1));
-    } else {
-        // If the input is a whole number
-        return false;
-    }
-
-    // Check if the product is a whole number
-    int productNumerator = numerator1 * numerator2;
-    int productDenominator = denominator1 * denominator2;
-
-    return productNumerator % productDenominator == 0;
 }
