564c16aae7ed71d999dc22364a9112555b47f992
beep boop <githubbot@vadim.me>
2024-07-23 15:12:22 +0200

    Solution 81 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/78ecuzfn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d91f58f0-c243-43d5-bd1a-205802237 c06.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d91f58f0-c243-43d5-bd1a-205802237 c06.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,14 @@
 def sort_third(l: list):
     original_indices = {val: i for i, val in enumerate(l)}
     return [
-        [x for x in l if original_indices[x] % 3 != 0][0] if i % 3 == 0 else val
+        (
+            [x for x in l if original_indices[x] % 3 != 0][0]
+            if i % 3 == 0
+            else (
+                min([x for x in l if original_indices[x] % 3 != 0])
+                if i % 3 == 1
+                else val
+            )
+        )
         for i, val in enumerate(l)
     ]
