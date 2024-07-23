2535e5be7cea2cb04a14533dc4a17669db557383
beep boop <githubbot@vadim.me>
2024-07-23 11:26:44 +0200

    Solution 19 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4e3v8dte Prompt:       |           ^~~~~~ In file included from 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp:29: 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp: In function ‘int main()’: 158305b1-ccb9-48d0-9141-6f9c3e76b425.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -15,7 +15,7 @@ std::vector<int> strange_sort_list(std::vector<int> list) {
     std::sort(list.begin(),list.end());
     for(int i=0;i<list.size();i++){
         if((i+1)%3==0){
-            reverse(list.begin()+i,list.begin()+(i+1));
+            std::reverse(list.begin()+i,list.begin()+(i+1));
         }
     }
     return list;
