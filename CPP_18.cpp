a807ad0c01cd20082b52aab0679b9bc170af2ba8
beep boop <githubbot@vadim.me>
2024-07-23 09:34:33 +0200

    Solution 60 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/qzd19lh8 Prompt: d1dc376e-766b-4e8a-8be7-d44ece0b0dd1.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ d1dc376e-766b-4e8a-8be7-d44ece0b0dd1.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,3 +1,4 @@
+```
 #include <string>
 #include <iostream>
 #include <cassert>
@@ -16,5 +17,4 @@ int how_many_times(std::string str, std::string substring) {
 int main() {
     assert (how_many_times("john doe", "john") == 1);
     std::cout << how_many_times("hello world", "world") << std::endl;
-    return 0;
-}
+    //std::cout << how_many_times("test", "t");
