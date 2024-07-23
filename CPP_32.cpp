78e2a732b06acd55c762d71b7a57d9bb8c3a05ac
beep boop <githubbot@vadim.me>
2024-07-23 21:49:07 +0200

    Solution 20 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3hjsd5tf Prompt: 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:35:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    35 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:38:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    38 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:38:36: error: ‘find_zero’ cannot be use d as a function    38 |         solution = find_zero(coeffs);       |                                    ^ In file included from 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:22: 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:39:21: error: ‘poly’ was not declared i n this scope    39 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:39:17: error: ‘abs’ was not declared in  this scope    39 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,21 +1,56 @@
+```cpp
 #include <vector>
-using namespace std;
+#include <cmath>
 
-double poly(vector<double> xs, double x) {
-    return xs[0] + xs[1]*x;
+double find_zero(std::vector<double> coeffs){
+    double sum = 0;
+    double product = 1;
+    int i;
+    std::vector<double> x_coords; // declare variable
+
+    for (i=0;i<coeffs.size();i++)
+    {
+        if(i%2==0)
+            sum += coeffs[i];
+        else
+            product *= -coeffs[i]/(double)fact((int)i);
+    }
+    return -product/sum;
 }
 
-double find_zero(vector<double> xs){
-    double x = -xs[1] / (2*xs[0]);
-    return x;
+int fact(int n){
+    int i,fact=1;
+    for (i=1;i<=n;i++)
+        fact*=i;
+    return fact;
+}
+
+// declare variable and function
+std::vector<double> coeffs;
+void poly(std::vector<double> coeffs, double solution) {
+    double val = 0;
+    for(int i = 0; i < coeffs.size(); i++) {
+        if(i % 2 == 0)
+            val += coeffs[i];
+        else
+            val += -coeffs[i] / fact(i) * pow(solution, i);
+    }
+    cout << "Polynomial evaluated at solution is: " << val << endl;
 }
 
 int main() {
-    vector<double> xs;
-    xs.push_back(1); // coefficient of x^2
-    xs.push_back(-4); // coefficient of x
-    xs.push_back(3);  // constant term
+    int n; // declare variable
+    std::cout << "Enter the degree of polynomial (n): ";
+    cin >> n;
 
-    double solution = find_zero(xs);
-    assert(abs(poly(xs, solution)) < 1e-3);
+    coeffs.resize(n+1); // declare variable
+    for(int i = 0; i <= n; i++) {
+        std::cout << "Enter coefficient at power " << i << ": ";
+        cin >> coeffs[i];
+    }
+
+    double solution = find_zero(coeffs);
+    poly(coeffs, solution);
+
+    return 0;
 }
