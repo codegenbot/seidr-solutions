acab5c44c062b62c9d26391077b9f940bd645b1d
beep boop <githubbot@vadim.me>
2024-07-23 10:41:30 +0200

    Solution 71 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/txje1r3n Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/156934bd-e968-4602-9890-f84a796e2 c85.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,18 +1,21 @@
 from typing import List
 
+
 def filter_by_prefix() -> List[str]:
     filtered_strings = []
     while True:
         try:
             user_input = input("Enter a list of strings (space-separated): ")
             strings = user_input.strip()
-            
+
             prefix = input("Enter a prefix: ")
 
             if not prefix:
                 raise ValueError("Please enter valid prefix!")
 
-            filtered_strings = [s.strip() for s in strings.split() if s.strip().startswith(prefix)]
+            filtered_strings = [
+                s.strip() for s in strings.split() if s.strip().startswith(prefix)
+            ]
             return filtered_strings
         except ValueError as e:
             print(f"Error: {e}")
