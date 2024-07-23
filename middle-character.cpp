4715d6414d8deeddd07c88aa047118b80c61f50b
beep boop <githubbot@vadim.me>
2024-07-23 12:38:12 +0200

    Solution 7 of middle-character, ratio 0.82 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/r2njffg6 Prompt: e940ba41-867a-40b0-8662-38f245cd8b31.cpp: In function ‘int main()’: e940ba41-867a-40b0-8662-38f245cd8b31.cpp:2:5: error: ‘string’ was not declared i n this scope     2 |     string s;       |     ^~~~~~ e940ba41-867a-40b0-8662-38f245cd8b31.cpp:3:5: error: ‘cin’ was not declared in t his scope     3 |     cin >> s;       |     ^~~ e940ba41-867a-40b0-8662-38f245cd8b31.cpp:3:12: error: ‘s’ was not declared in th is scope     3 |     cin >> s;       |            ^ e940ba41-867a-40b0-8662-38f245cd8b31.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << s.substr(len/2 - 1, 2);       |         ^~~~ e940ba41-867a-40b0-8662-38f245cd8b31.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << s[len/2];       |         ^~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <string>
 
-std::string middleCharacter(std::string s) {
+int main() {
+    std::string s;
+    std::cin >> s;
     int len = s.length();
     if (len % 2 == 0) {
-        return s.substr(len/2 - 1, 2);
+        std::cout << s.substr(len/2 - 1, 2);
     } else {
-        return s.substr(len/2, 1);
+        std::cout << s[len/2];
     }
-}
-
-int main() {
-    std::string input;
-    std::cin >> input;
-    std::cout << middleCharacter(input) << std::endl;
     return 0;
 }
