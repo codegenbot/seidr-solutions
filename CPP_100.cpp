618cea66b050443cc7e51a7ab4f2176508bbc15a
beep boop <githubbot@vadim.me>
2024-07-24 01:43:05 +0200

    Solution 28 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0eegrvg0 Prompt:       |                    ^~~ 276825da-2d1e-4571-9c19-0a600b5759e6.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 276825da-2d1e-4571-9c19-0a600b5759e6.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 276825da-2d1e-4571-9c19-0a600b5759e6.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 276825da-2d1e-4571-9c19-0a600b5759e6.cpp:17: 276825da-2d1e-4571-9c19-0a600b5759e6.cpp: In function ‘int main()’: 276825da-2d1e-4571-9c19-0a600b5759e6.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 276825da-2d1e-4571-9c19-0a600b5759e6.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,23 +1,22 @@
+```cpp
 #include <vector>
-
-bool isSame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i<a.size(); ++i) {
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
 
 std::vector<int> make_a_pile(int n) {
     std::vector<int> pile;
-    for (int i = 1; i <= n; ++i) {
-        if (i % 2 == 0) {
-            pile.push_back(i * 2 - 1);
+    int stones = n;
+    while (stones > 0) {
+        if (n % 2 == 1) {
+            pile.push_back(stones);
+            n++;
         } else {
-            pile.push_back(i * 2);
+            pile.push_back(stones);
+            n++;
         }
+        stones /= 2;
     }
     return pile;
 }
 
-assert(isSame(make_a_pile(8) , std::vector<int>{8, 10, 12, 14, 16, 18, 20, 22}));
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
