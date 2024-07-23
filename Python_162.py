4ad0fe81ec394bfec64db7fc0a11d55007360713
beep boop <githubbot@vadim.me>
2024-07-23 19:19:09 +0200

    Solution 52 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lkb9rtzq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6e0227e-a824-4503-8359-29a7d7067 739.py", line 5     if not text := input():        ^^^^^^^^ SyntaxError: cannot use assignment expressions with expression

--- Python_162.py
+++ Python_162.py
@@ -1,7 +1,7 @@
 import hashlib
 
 def string_to_md5():
-    text = input("Enter a text: ")
+    text = input()
     if not text:
         return None
     return hashlib.md5(text.encode()).hexdigest()
