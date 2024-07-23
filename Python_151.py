7903468bbaa7864f44f6c2beae54597818063b3e
beep boop <githubbot@vadim.me>
2024-07-23 13:04:43 +0200

    Solution 55 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c22cf04-93e3-42d1-81a3-484df66d0 b9a.py", line 1, in <module>     print(double_the_difference(first_list, second_list))           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,10 @@
 def double_the_difference(list1, list2):
+    list1.sort()
+    list2.sort()
     if len(list1) != len(list2):
-        raise ValueError("Lists must be of equal length")
-    difference = sum(min(i, j) for i, j in zip(list1, list2)) - sum(max(i, j) for i, j in zip(list1, list2))
+        return None
+    difference = sum(x - y for x, y in zip(list1, list2))
     return difference * 2
+
+
+print(double_the_difference([7, 1], [5, 3, 2]))
