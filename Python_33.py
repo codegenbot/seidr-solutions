1cc44f5579e693c5b9e008d78f5057c8ad000384
beep boop <githubbot@vadim.me>
2024-07-23 09:24:05 +0200

    Solution 10 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/phujfvw4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/47b76d1f-2bdb-4ba6-9caa-abc8cc945 4b8.py", line 10, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/47b76d1f-2bdb-4ba6-9caa-abc8cc945 4b8.py", line 5, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,4 @@
+```
 def sort_third(l: list):
-    return [
-        (
-            x
-            if i % 3 != 0
-            else sorted([y for y in l[: min(i // 3 * 3 + i % 3, len(l)) ] if y % 3 != 0])[-1]
-        )
-        for i, x in enumerate(l)
-    ]
+    sorted_values = sorted([x for i, x in enumerate(l) if i % 3 == 0])
+    return [sorted_values[i//3] if i % 3 == 0 else x for i, x in enumerate(l)]
