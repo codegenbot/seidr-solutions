6584970d4c6aa6feb6c0bb1b2dcd35cbdb1fe3b3
beep boop <githubbot@vadim.me>
2024-07-23 15:10:15 +0200

    Solution 90 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cd53a52c-9dfa-4bee-97ed-07e9fed2e e33.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,13 +1,13 @@
 import re
 
 def sum_squares(user_input):
-    if not isinstance(user_input, (str, tuple)):
-        return "Invalid input type"
-
-    numbers = []
-    if isinstance(user_input, str):
-        numbers = list(map(int, filter(lambda x: re.match(r'\d+', str(x)), user_input.split())))
+    if isinstance(user_input, (int, float)):
+        numbers = [n ** 2]
+    elif not isinstance(user_input, str) and not isinstance(user_input, tuple) and not hasattr(user_input, "__iter__"):
+        return
     else:
-        numbers = [n ** 2 for n in map(int, filter(lambda x: re.match(r'\d+', str(x)), user_input))]
-        
+        if isinstance(user_input, list):
+            numbers = [n ** 2 for n in map(int, filter(lambda x: re.match(r'\d+', str(x)), user_input))]
+        else:
+            numbers = list(map(int, filter(lambda x: re.match(r'\d+', str(x)), user_input.split())))
     return sum(numbers)
