933d991b68c039030267b4d5df6e34b047f6a381
beep boop <githubbot@vadim.me>
2024-07-23 13:25:34 +0200

    Solution 12 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/jwsg3b1w Prompt: c2924a6f-4135-4765-a739-b508668a46cb.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ c2924a6f-4135-4765-a739-b508668a46cb.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ c2924a6f-4135-4765-a739-b508668a46cb.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,16 +1,31 @@
 #include <string>
 
-int mastermind(std::string code, std::string guess) {
+int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
             black++;
-        } else if (std::count(guess.begin(), guess.end(), code[i]) > 0) {
-            white++;
         }
     }
 
-    return black;
+    vector<char> code_chars(code.begin(), code.end());
+    vector<char> guess_chars(guess.begin(), guess.end());
+
+    for (int i = 0; i < 4; i++) {
+        int count = 0;
+        for (int j = 0; j < 4; j++) {
+            if (code_chars[j] == guess[i]) {
+                count++;
+            }
+        }
+        if (count > 1) {
+            black += count - 1;
+        } else if (count > 0) {
+            white += count - 1;
+        }
+    }
+
+    return black + white;
 }
