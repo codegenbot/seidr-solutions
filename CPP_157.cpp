07df51ffefa5fe6212fece1621c3ef13fa83545f
beep boop <githubbot@vadim.me>
2024-07-23 18:39:44 +0200

    Solution 7 of CPP/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/mla9a8kf Prompt: 4f57a15c-9b10-4b59-8f84-fe6c19418d1e.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 4f57a15c-9b10-4b59-8f84-fe6c19418d1e.cpp:10: 4f57a15c-9b10-4b59-8f84-fe6c19418d1e.cpp: In function ‘int main()’: 4f57a15c-9b10-4b59-8f84-fe6c19418d1e.cpp:12:13: error: ‘right_angle_triangle’ wa s not declared in this scope    12 |     assert (right_angle_triangle(2, 2, 10) == false);       |             ^~~~~~~~~~~~~~~~~~~~

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,21 +1,19 @@
-bool right_angle_triangle(float a, float b, float c) {
-    if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
+if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
         return true;
     else
         return false;
 }
-
 int main() {
-    float sideA, sideB, sideC;
-    
-    std::cout << "Enter the lengths of three sides: ";
-    std::cin >> sideA >> sideB >> sideC;
-    
-    if (right_angle_triangle(sideA, sideB, sideC)) {
-        std::cout << "The triangle is a right-angled triangle.\n";
-    } else {
-        std::cout << "The triangle is not a right-angled triangle.\n";
-    }
-    
+    float a, b, c;
+    cout << "Enter side 1: ";
+    cin >> a;
+    cout << "Enter side 2: ";
+    cin >> b;
+    cout << "Enter side 3: ";
+    cin >> c;
+    if (right_angle_triangle(a,b,c))
+        cout << "It is a right angle triangle";
+    else
+        cout << "It is not a right angle triangle";
     return 0;
 }
