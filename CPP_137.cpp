7d54c14a7794d0cce1103ce7a27d55709cc5db4c
beep boop <githubbot@vadim.me>
2024-07-23 07:00:33 +0200

    Solution 5 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -12,20 +12,58 @@ boost::any compare_one(boost::any a, boost::any b) {
         return (string)b;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)a;
-        float a_float = stof(str);
-        return max(a_float, stof((string)b));
+        string strA = boost::any_cast<string>(a);
+        float fB = boost::any_cast<float>(b);
+        if (strA == "0,") {
+            return a;
+        }
+        else if (fB > stof(strA)) {
+            return b;
+        }
+        else {
+            return "None";
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string strA = boost::any_cast<string>(a);
+        int iB = boost::any_cast<int>(b);
+        if (strA == "0,") {
+            return a;
+        }
+        else if (stof(strA) > iB) {
+            return a;
+        }
+        else {
+            return "None";
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string strA = boost::any_cast<string>(a);
+        float fB = boost::any_cast<float>(b);
+        if (strA == "0,") {
+            return a;
+        }
+        else if (stof(strA) > fB) {
+            return a;
+        }
+        else {
+            return "None";
+        }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (max)((string)a > (string)b) ? (string)a : (string)b;
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return boost::any("None");
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return boost::any((double)a.convert_to<double>() > (double)b.convert_to<double>()) ? a : b;
+        string strA = boost::any_cast<string>(a);
+        string strB = boost::any_cast<string>(b);
+        if (strA == "0," && strB != "0,") {
+            return b;
+        }
+        else if (strA != "0," && stof(strA) > stof(strB)) {
+            return a;
+        }
+        else {
+            return "None";
+        }
     }
     else {
-        return boost::any("None");
+        return "None";
     }
 }
