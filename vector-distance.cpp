ba0db1e37aa9111a62ff5ff2d2a0a6448c0ca8b0
beep boop <githubbot@vadim.me>
2024-07-24 01:36:20 +0200

    Solution 63 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/s1xrt7nh Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <cmath>
 
@@ -15,20 +14,17 @@ int main() {
     std::cout << "Enter the number of dimensions: ";
     std::cin >> n;
 
-    float* v1 = new float[n], *v2 = new float[n];
+    float v1[n], v2[n];
     for (int i = 0; i < n; i++) {
         std::cout << "Enter value for dimension " << i + 1 << " of vector 1: ";
-        std::cin >> (float)v1[i];
+        std::cin >> v1[i];
 
         std::cout << "Enter value for dimension " << i + 1 << " of vector 2: ";
-        std::cin >> (float)v2[i];
+        std::cin >> v2[i];
     }
 
     float distance = calculateEuclideanDistance(v1, v2, n);
     std::cout << "The Euclidean distance between the two vectors is: " << distance << endl;
-
-    delete[] v1;
-    delete[] v2;
 
     return 0;
 }
