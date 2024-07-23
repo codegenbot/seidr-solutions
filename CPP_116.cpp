c4e8d5b474b305ece750813bc980e211bb370afe
beep boop <githubbot@vadim.me>
2024-07-24 01:55:52 +0200

    Solution 4 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xl3y50v3 Prompt:       |           ^~~~~~ In file included from 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp:14: 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp: In function ‘int main()’: 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 95062e7f-5f1c-411f-9ebf-a464ecc1c610.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,12 +1,29 @@
-Here is the solution:
+#include <vector>
+#include <algorithm>
+#include <bitset>
 
-vector<int> sort_array(vector<int> arr){
-    sort(arr.begin(), arr.end(), 
-         [](int a, int b) {
-             if (bitset<32>(a).count() != bitset<32>(b).count())
-                 return bitset<32>(a).count() < bitset<32>(b).count();
-             else
-                 return a < b;
-         });
+int main() {
+    std::vector<int> arr = {2,4,8,16,32};
+    int res = sort_array(arr).size();
+    assert(issame(arr, sort_array(arr)));
+    return res;
+}
+
+std::vector<int> sort_array(std::vector<int> arr) {
+    std::sort(arr.begin(), arr.end(), [](int a, int b){
+        if (std::bitset<32>(a).count() != std::bitset<32>(b).count())
+            return std::bitset<32>(a).count() < std::bitset<32>(b).count();
+        else
+            return a < b;
+    });
     return arr;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for(int i = 0; i < a.size(); i++)
+        if(a[i] != b[i])
+            return false;
+    return true;
 }
