8d2e3fb3f982179f66d5facbc5e69a680eb53f9a
beep boop <githubbot@vadim.me>
2024-07-23 22:44:16 +0200

    Solution 16 of CPP/48, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3ne9zl5d Prompt: 6aced324-17e9-49dd-b59a-db1daa4905ae.cpp:1:20: error: ‘string’ was not declared in this scope     1 | bool is_palindrome(string text){       |                    ^~~~~~ In file included from 6aced324-17e9-49dd-b59a-db1daa4905ae.cpp:15: 6aced324-17e9-49dd-b59a-db1daa4905ae.cpp: In function ‘int main()’: 6aced324-17e9-49dd-b59a-db1daa4905ae.cpp:17:26: error: ‘is_palindrome’ cannot be  used as a function    17 |     assert (is_palindrome("xywzx") == false);       |             ~~~~~~~~~~~~~^~~~~~~~~

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -1,15 +1,20 @@
 #include <string>
 
-bool is_palindrome(string text){
+bool is_palindrome(std::string text){
     int start = 0;
     int end = text.length() - 1;
 
     while(start < end){
         if(text[start] != text[end])
-            return false;
+            return 0; 
         start++;
         end--;
     }
 
-    return true;
+    return 1; 
+}
+
+int main() {
+    assert (is_palindrome("xywzx") == false);
+    // rest of your code
 }
