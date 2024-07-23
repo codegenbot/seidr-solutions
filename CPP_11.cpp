84b16b4f0b9a7f01d637ba8a536fd4b79c0dc867
beep boop <githubbot@vadim.me>
2024-07-23 09:10:06 +0200

    Solution 42 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0d0ub3e7 Prompt: /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from 67d8a1e3-2b55-413c-89a1-d9bf2ba2d9b5.cpp:2: /usr/include/c++/11/sstream: In instantiation of ‘std::__cxx11::basic_stringbuf< _CharT, _Traits, _Alloc>::basic_stringbuf(const __string_type&, std::ios_base::o penmode) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::al locator<char>; std::__cxx11::basic_stringbuf<_CharT, _Traits, _Alloc>::__string_ type = std::__cxx11::basic_string<char>; std::ios_base::openmode = std::ios_base ::openmode]’: /usr/include/c++/11/sstream:607:27:   required from ‘std::__cxx11::basic_istring stream<_CharT, _Traits, _Alloc>::basic_istringstream(const __string_type&, std:: ios_base::openmode) [with _CharT = char; _Traits = std::char_traits<char>; _Allo c = std::allocator<char>; std::__cxx11::basic_istringstream<_CharT, _Traits, _Al loc>::__string_type = std::__cxx11::basic_string<char>; std::ios_base::openmode = std::ios_base::openmode]’ 67d8a1e3-2b55-413c-89a1-d9bf2ba2d9b5.cpp:8:25:   required from here /usr/include/c++/11/sstream:142:39: error: ‘const __string_type’ {aka ‘const cla ss std::__cxx11::basic_string<char>’} has no member named ‘size’   142 |         _M_string(__str.data(), __str.size(), __str.get_allocator())       |                                 ~~~~~~^~~~

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,24 +1,16 @@
 ```cpp
 #include <sstream>
 #include <iostream>
-#include <bitset>
+#include <algorithm>
 
 using namespace std;
 
-string toBinary(string s) {
-    bitset<32> bs;
-    for (char c : s) {
-        if (c == '1') bs.flip();
-    }
-    return bs.to_string();
-}
-
 string string_xor(string a, string b) {
-    istringstream is_a(toBinary(a)), is_b(toBinary(b));
+    istringstream is_a(a), is_b(b);
     string result = "";
-    char c, d;
-    while (is_a >> c && is_b >> d) {
-        if ((c - '0') ^ (d - '0')) {
+    char c;
+    while (is_a >> c && is_b >> c) {
+        if ((c - '0') ^ ('0' - '0')) {
             result += "1";
         } else {
             result += "0";
@@ -30,6 +22,8 @@ string string_xor(string a, string b) {
 int main() {
     string a = "0101";
     string b = "0000";
+    a.erase(std::remove(a.begin(), a.end(), ' '), a.end()); 
+    b.erase(std::remove(b.begin(), b.end(), ' '), b.end());
     cout << string_xor(a, b) << endl;
     return 0;
 }
