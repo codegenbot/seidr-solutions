f38a233a8dbf4db5a946723cb50580a30d766d02
beep boop <githubbot@vadim.me>
2024-07-23 23:49:06 +0200

    Solution 39 of CPP/65, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ds1lnt91 Prompt: df7d10e2-4ab0-409c-af81-3a864a791525.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string circular_shift(int x, int shift) {       | ^~~~~~ In file included from df7d10e2-4ab0-409c-af81-3a864a791525.cpp:6: df7d10e2-4ab0-409c-af81-3a864a791525.cpp: In function ‘int main()’: df7d10e2-4ab0-409c-af81-3a864a791525.cpp:8:13: error: ‘circular_shift’ was not d eclared in this scope     8 |     assert (circular_shift(11, 101) == "11");       |             ^~~~~~~~~~~~~~

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,18 +1,7 @@
+#include <iostream>
 #include <string>
 
-using namespace std;
-
-string circular_shift(int x, int shift) {
-    string str = to_string(x);
-    int n = str.length();
-    if (shift >= n)
-        return str;
-    else {
-        string result = "";
-        for (int i = shift; i < n; i++)
-            result += str[i];
-        for (int i = 0; i < shift; i++)
-            result += str[i];
-        return result;
-    }
+std::string circular_shift(int x, int shift) {
+    std::string str = std::to_string(x);
+    return str.substr(shift % str.length()) + str.substr(0, shift % str.length());
 }
