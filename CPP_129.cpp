95588da3c3b5a514f9a2c147a7e94d9c91ec2134
beep boop <githubbot@vadim.me>
2024-07-23 18:31:07 +0200

    Solution 51 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7e6wh1b Prompt: _Tp = std::vector<std::pair<int, std::pair<int, int> >, std::allocator<std::pair <int, std::pair<int, int> > > >; _Alloc = std::allocator<std::vector<std::pair<i nt, std::pair<int, int> >, std::allocator<std::pair<int, std::pair<int, int> > >  > >; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = std::vector<std::pair<int, std::pair<int, int> >, std::allocator<std::pai r<int, std::pair<int, int> > > >; _Alloc = std::allocator<std::vector<std::pair< int, std::pair<int, int> >, std::allocator<std::pair<int, std::pair<int, int> > > > >; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp,  _Alloc>::allocator_type = std::allocator<std::vector<std::pair<int, std::pair<i nt, int> >, std::allocator<std::pair<int, std::pair<int, int> > > > >]’ 9aabfa31-9830-400c-a093-863b175411c2.cpp:18:96:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -18,10 +17,10 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
     vector<vector<pair<int, pair<int, int>>, std::allocator<std::pair<int, pair<int, int>>>>> neighbors(n);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (i > 0) neighbors[i].push_back({{make_pair(i-1, j), grid[i][j]}});
-            if (i < n-1) neighbors[i].push_back({{make_pair(i+1, j), grid[i][j]}});
-            if (j > 0) neighbors[i].push_back({{make_pair(i, j-1), grid[i][j]}});
-            if (j < n-1) neighbors[i].push_back({{make_pair(i, j+1), grid[i][j]}});
+            if (i > 0) neighbors[i].push_back({make_pair(i-1, j), grid[i][j]});
+            if (i < n-1) neighbors[i].push_back({make_pair(i+1, j), grid[i][j]});
+            if (j > 0) neighbors[i].push_back({make_pair(i, j-1), grid[i][j]});
+            if (j < n-1) neighbors[i].push_back({make_pair(i, j+1), grid[i][j]});
         }
     }
 
