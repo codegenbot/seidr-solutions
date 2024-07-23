05e1f420cca124a79c5dc91cedc129848c0fbe75
beep boop <githubbot@vadim.me>
2024-07-23 11:37:53 +0200

    Solution 10 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/b1y7xy27 Prompt: 4ff36ac9-8c5e-42d5-9aac-bdde247231c3.cpp: In function ‘int main()’: 4ff36ac9-8c5e-42d5-9aac-bdde247231c3.cpp:22:14: error: expected ‘}’ at end of in put    22 |     return 0;       |              ^ 4ff36ac9-8c5e-42d5-9aac-bdde247231c3.cpp:6:12: note: to match this ‘{’     6 | int main() {       |            ^

--- twitter.cpp
+++ twitter.cpp
@@ -1,1 +1,22 @@
-std::cout << validateTweet(tweet);
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string tweet;
+    cout << "Enter a tweet: ";
+    getline(cin, tweet);
+
+    if (tweet.empty()) {
+        cout << "You didn't type anything" << endl;
+        return 0;
+    }
+
+    if (tweet.length() > 140) {
+        cout << "Too many characters" << endl;
+        return 0;
+    }
+
+    cout << "Your tweet has " << tweet.length() << " characters" << endl;
+    return 0; }
