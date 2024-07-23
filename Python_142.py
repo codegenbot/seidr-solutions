555362c41f5d5b9488725e078413f3ff2f476ef1
beep boop <githubbot@vadim.me>
2024-07-23 13:38:45 +0200

    Solution 48 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544a1f00-260f-43b6-af34-5fbbdc0cb 14c.py", line 38, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544a1f00-260f-43b6-af34-5fbbdc0cb 14c.py", line 8, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544a1f00-260f-43b6-af34-5fbbdc0cb 14c.py", line 2, in sum_squares     numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split( ))))                                                                ^^^^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_142.py
+++ Python_142.py
@@ -1,4 +1,4 @@
-```
-def sum_squares(user_input):
+def sum_squares():
+    user_input = input("Enter some numbers separated by spaces: ")
     numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split())))
     print(sum([n ** 2 for n in numbers]))
