0ab624816abb4e9bb99085f8bb3374bb75aede3e
beep boop <githubbot@vadim.me>
2024-07-23 12:51:23 +0200

    Solution 57 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xfvdw2bt Prompt:       |           ^~~~~~ 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp:60:5: error: redefinition of ‘int main( )’    60 | int main(){       |     ^~~~ 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp:44:5: note: ‘int main()’ previously def ined here    44 | int main() {       |     ^~~~ In file included from 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp:51: 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp: In function ‘int main()’: 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp:61:20: error: ‘numerical_letter_grade’ was not declared in this scope    61 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 18ec1b3c-df56-4abd-9717-aec02a50e122.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <string>
 
-std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
-    std::vector<std::string> letter_grades;
+std::vector<string> numerical_letter_grade(std::vector<float> grades) {
+    std::vector<string> letter_grades;
     for (float grade : grades) {
         if (grade >= 4.0)
             letter_grades.push_back("A+");
@@ -35,7 +35,7 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 
 int main() {
     std::vector<float> grades = {0, 0.7};
-    std::vector<std::string> expectedLetterGrades = {"F", "D"};
+    std::vector<string> expectedLetterGrades = {"F", "D"};
     assert(numerical_letter_grade(grades) == expectedLetterGrades);
     return 0;
 }
