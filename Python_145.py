1b2460e551106b58d74fcdda8078e9d999759d1d
beep boop <githubbot@vadim.me>
2024-07-23 22:36:16 +0200

    Solution 4 of Python/145, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yei9azxh Prompt: Complete the following code given the task description and function signature.

--- Python_145.py
+++ Python_145.py
@@ -1,3 +1,4 @@
-```
 def order_by_points(nums):
-    return sorted((num, sum(int(digit) for digit in str(abs(num)))) for num in nums)
+    return sorted(
+        (num and sum(int(digit)) for num in nums), key=lambda x: (x[1], -x[0])
+    )
