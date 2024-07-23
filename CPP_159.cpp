299f792d3d9aa3442c9e8850313137a9525f4dec
beep boop <githubbot@vadim.me>
2024-07-23 21:28:25 +0200

    Solution 80 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7vsawre Prompt:       |                                  ^~~ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:6: 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp: In function ‘int main()’: 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:16:20: error: ‘eat’ was not declared in  this scope    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 0b7b8a11-5011-41af-911c-62145b1ebe3e.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,29 +1,15 @@
 #include <vector>
-#include <algorithm>
+#include <cassert>
 
-std::vector<int> eat(int a, int b, int c) {
-    std::vector<int> result;
-    for (int i = 0; i < a + b + c; ++i) {
-        if (i < a)
-            result.push_back(i);
-        else if (i >= a && i < a + b)
-            result.push_back(a + b - 1);
-        else
-            result.push_back(c - 1);
-    }
-    return result;
+std::vector<int> eat(int x, int y, int z) {
+    return {x+y, z};
 }
 
-bool areSameVectors(const std::vector<int>& v1, const std::vector<int>& v2) {
-    if (v1.size() != v2.size())
-        return false;
-    for (int i = 0; i < v1.size(); ++i)
-        if (v1[i] != v2[i])
-            return false;
-    return true;
+bool isSameVector(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
 
 int main() {
-    assert(areSameVectors(eat(4, 5, 1), std::vector<int>({9,0})));
+    assert(isSameVector(eat(4, 5, 1), {9,0})); 
     return 0;
 }
