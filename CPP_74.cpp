c801f79ba074e7530734e3ea7741c7506ac3ea8a
beep boop <githubbot@vadim.me>
2024-07-24 00:23:28 +0200

    Solution 46 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jao9uj68 Prompt:       |                                     ^~~~~~ 34ec1f20-00e2-4a45-91e0-f4cb8dd53c86.cpp:44:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 34ec1f20-00e2-4a45-91e0-f4cb8dd53c86.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 34ec1f20-00e2-4a45-91e0-f4cb8dd53c86.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 34ec1f20-00e2-4a45-91e0-f4cb8dd53c86.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 34ec1f20-00e2-4a45-91e0-f4cb8dd53c86.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 34ec1f20-00e2-4a45-91e0-f4cb8dd53c86.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,9 +1,8 @@
-```cpp
 #include <vector>
 #include <string>
 
 bool issame(std::vector<std::string> a, std::string b) {
-    if (a.size() != 1 || b.empty()) {
+    if (a.size() != 1 || a[0] != b) {
         return false;
     }
     
@@ -28,11 +27,11 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
     }
 
     if (sum1 < sum2) {
-        return issame(lst1, "") ? lst1 : lst2;
+        return issame(lst1, *lst2.begin()) ? lst1 : lst2;
     } else if (sum1 > sum2) {
-        return issame({lst2[0]}, "") ? {lst2[0]} : lst1;
+        return issame(*lst1.begin(), std::string("")); ? lst2 : lst1;
     } else {
-        return issame(lst1, "") ? lst1 : lst2;
+        return issame(lst1, std::string("")) ? lst1 : lst2;
     }
 }
 
