1cda4e05ebed28921e62ce2294d70f500c53b406
beep boop <githubbot@vadim.me>
2024-07-23 23:41:28 +0200

    Solution 41 of CPP/52, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jpwz8nr7 Prompt: Program failed

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -3,8 +3,8 @@
 bool below_threshold(std::vector<int> l, int t) {
     for (int num : l) {
         if (num >= t) {
-            return false;
+            return true;
         }
     }
-    return true;
+    return false;
 }
