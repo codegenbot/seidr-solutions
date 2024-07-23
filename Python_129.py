097ad8f1a92198f21ed12c28bda632c3b34f3a6f
beep boop <githubbot@vadim.me>
2024-07-23 22:45:52 +0200

    Solution 36 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ty2cq1m8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/defbf39b-0d55-481d-b608-889f9ff3d 421.py", line 34, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/defbf39b-0d55-481d-b608-889f9ff3d 421.py", line 31, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -2,19 +2,19 @@
 def minPath(grid, k):
     n = len(grid)
     visited = [[False] * n for _ in range(n)]
-    shortest_path = []
+    short_path = []
     max_len = 0
 
     def dfs(i, j, curr_path):
         nonlocal path
-        nonlocal shortest_path
+        nonlocal short_path
         nonlocal max_len
         if len(curr_path) > k:
             return False
 
         if len(curr_path) > max_len:
             max_len = len(curr_path)
-            shortest_path = [grid[i][j]] + curr_path[:]
+            short_path = [grid[i][j]] + curr_path[:]
             return True
         
         for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
@@ -31,4 +31,4 @@ def minPath(grid, k):
         for j in range(n):
             visited[i][j] = True
             if dfs(i, j, [grid[i][j]]):
-                return shortest_path
+                return short_path
