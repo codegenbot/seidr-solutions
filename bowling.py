ebc1d2cf90e75b381e9282170ce45765f05bf92a
beep boop <githubbot@vadim.me>
2024-07-23 08:52:35 +0200

    Solution 11 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/014d0188-acab-46f6-8617-b8159a39e 633.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,79 +1,20 @@
 def bowling_score(game):
     score = 0
     frame = 1
-    for bowl in game.split(' '):
-        if len(bowl) == 2 and bowl[1] == '/':
-            strike_frame = int(frame)
-            while True:
-                next_bowl = input(f"Enter the bowls for frame {strike_frame}: ")
-                if len(next_bowl) > 2 and next_bowl[1] == '/':
-                    score += 10 + int(next_bowls.split(' ')[0])
-                    strike_frame += 1
-                elif next_bowl[1] == 'X':
-                    score += 10 + (10 - int(next_bowl[0])) * 2
-                    strike_frame += 1
-                else:
-                    score += int(next_bowl[:1]) * 2 + int(next_bowl[1:])
-                    break
-        elif bowl[0] == 'X':
-            if len(bowl) > 1 and bowl[1] != '/':
-                score += 10 + int(bowl[1])
-            else:
-                strike_frame = frame
-                while True:
-                    next_bowl = input(f"Enter the bowls for frame {strike_frame}: ")
-                    if len(next_bowl) > 2 and next_bowl[1] == '/':
-                        score += 10 + int(next_bowls.split(' ')[0])
-                        break
-                    elif next_bowl[1] == 'X':
-                        score += 10 + (10 - int(next_bowl[0])) * 2
-                        break
-                    else:
-                        score += int(next_bowl[:1]) * 2 + int(next_bowl[1:])
-                        break
+    for bowl in game:
+        if len(bowl) == 1:
+            score += int(bowl)
             frame += 1
-        elif bowl == 'X X':
-            score += 20
-        elif bowl[1] == '/':
-            if len(bowl) > 2 and bowl[2] != '/':
-                score += int(bowl[:1]) * 2 + int(bowl[1:])
-            else:
-                strike_frame = frame
-                while True:
-                    next_bowl = input(f"Enter the bowls for frame {strike_frame}: ")
-                    if len(next_bowl) > 2 and next_bowl[1] == '/':
-                        score += 10 + int(next_bowls.split(' ')[0])
-                        break
-                    elif next_bowl[1] == 'X':
-                        score += 10 + (10 - int(next_bowl[0])) * 2
-                        break
-                    else:
-                        score += int(next_bowl[:1]) * 2 + int(next_bowl[1:])
-                        break
+        elif bowl == 'X':
+            score += 10
             frame += 1
-        elif bowl[0] == '/':
-            if len(bowl) > 2 and bowl[2] != '/':
-                score += int(bowl[:1]) * 2 + int(bowl[1:])
-            else:
-                strike_frame = frame
-                while True:
-                    next_bowl = input(f"Enter the bowls for frame {strike_frame}: ")
-                    if len(next_bowl) > 2 and next_bowl[1] == '/':
-                        score += 10 + int(next_bowls.split(' ')[0])
-                        break
-                    elif next_bowl[1] == 'X':
-                        score += 10 + (10 - int(next_bowl[0])) * 2
-                        break
-                    else:
-                        score += int(next_bowl[:1]) * 2 + int(next_bowl[1:])
-                        break
+        elif bowl == '/':
+            score += 10 - 5 + int(game[game.index(bowl)-1])
             frame += 1
         else:
             if len(bowl) > 1 and bowl[0] == 'X':
                 score += 10 + int(bowl[1:])
+                frame += 1
             elif len(bowl) > 1:
                 score += int(bowl[:1]) * 2 + int(bowl[1:])
-            else:
-                score += int(bowl)
-        frame += 1
-    return score
+                frame += 1
