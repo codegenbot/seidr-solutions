c6b7ee8fed5405591361d0e2ab3843778acb4962
beep boop <githubbot@vadim.me>
2024-07-23 10:01:33 +0200

    Solution 8 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/c6yj8oxo Prompt: Program failed

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,12 +1,8 @@
 bool pairs_sum_to_zero(vector<int> l) {
-    if (l.size() < 2)
-        return false;
     sort(l.begin(), l.end());
-    for (int i = 0; i < l.size() - 1; i++) {
-        if (l[i] + l[i+1] == 0)
+    for (int i = 0; i < l.size(); i++) {
+        if (binary_search(l.begin()+i+1, l.end(), -l[i]))
             return true;
-        else if (l[i] > 0)
-            break;
     }
     return false;
 }
