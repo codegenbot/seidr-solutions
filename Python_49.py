db81d10445b56ae0b7dee6e9e3d186721cb1bc57
beep boop <githubbot@vadim.me>
2024-07-23 10:47:36 +0200

    Solution 46 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c97a7dd4-e751-40d8-a541-208e92e9c f71.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,14 +1,5 @@
-```
-def is_prime(n: int):
-    if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def modp(n: int, p: int):
+    if gcd(n, p) == 1:
+        return pow(n, (p - 1), p)
+    else:
+        return None
