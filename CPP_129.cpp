7d83ab7ea1e6c0d2a4ff3b0e8ab764d57a8f4dda
beep boop <githubbot@vadim.me>
2024-07-23 19:03:59 +0200

    Solution 62 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w7e6wh1b Prompt:    38 |             return {sum};       |                        ^       |                        |       |                        <brace-enclosed initializer list> 5435bcc5-b037-4684-a286-a0ccf01e9517.cpp:40:41: error: ‘i’ was not declared in t his scope    40 |         for (auto& neighbor : neighbors[i]) {       |                                         ^ 5435bcc5-b037-4684-a286-a0ccf01e9517.cpp:42:36: error: ‘j’ was not declared in t his scope    42 |             int ns = sum - grid[i][j] + neighbor.second;       |                                    ^ 5435bcc5-b037-4684-a286-a0ccf01e9517.cpp:44:15: error: request for member ‘push’  in ‘q’, which is of non-class type ‘std::priority_queue<std::pair<int, std::pai r<int, int> >, std::vector<std::pair<int, std::pair<int, int> >, std::allocator< std::pair<int, std::pair<int, int> > > >, cmp>(cmp)’    44 |             q.push({ns, make_pair(ni, nj)});       |               ^~~~ 5435bcc5-b037-4684-a286-a0ccf01e9517.cpp:44:39: error: ‘nj’ was not declared in this scope; did you mean ‘ns’?    44 |             q.push({ns, make_pair(ni, nj)});       |                                       ^~       |                                       ns

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,3 +1,4 @@
+```
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -14,14 +15,13 @@ struct cmp {
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<pair<int, pair<int, int>>>> neighbors;
-
+    vector<vector<pair<int, pair<int, int>>, std::allocator<std::pair<int, pair<int, int>>>>> neighbors(n);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (i > 0) neighbors[i].push_back({{make_pair(i-1, j), grid[i][j]}});
-            if (i < n-1) neighbors[i].push_back({{make_pair(i+1, j), grid[i][j]}});
-            if (j > 0) neighbors[i].push_back({{make_pair(i, j-1), grid[i][j]}});
-            if (j < n-1) neighbors[i].push_back({{make_pair(i, j+1), grid[i][j]}});
+            if (i > 0) neighbors[i].push_back({make_pair(i-1, j), grid[i][j]});
+            if (i < n-1) neighbors[i].push_back({make_pair(i+1, j), grid[i][j]});
+            if (j > 0) neighbors[i].push_back({make_pair(i, j-1), grid[i][j]});
+            if (j < n-1) neighbors[i].push_back({make_pair(i, j+1), grid[i][j]});
         }
     }
 
@@ -34,15 +34,14 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
 
     vector<int> res;
     while (!q.empty()) {
-        auto [sum, [i, j]] = q.top(); q.pop();
+        auto [sum, p] = q.top(); q.pop();
         if (k == 0) {
             return {sum};
         }
-        for (auto& neighbor : neighbors[i]) {
-            int ni = neighbor.first.first, nj = neighbor.first.second;
-            int ns = sum - grid[i][j] + neighbor.second;
+        for (auto& neighbor : neighbors[p.first]) {
+            int ns = sum - grid[p.first][p.second] + neighbor.second;
             k--;
-            q.push({ns, make_pair(ni, nj)});
+            q.push({ns, neighbor.first});
         }
     }
 
