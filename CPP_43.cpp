158c58a11776495a895c4ba9b280077cf57c5248
beep boop <githubbot@vadim.me>
2024-07-23 02:41:53 +0200

    Solution 5 of CPP/43, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0wf5pr4l Prompt: a6413219-36c6-482f-8d66-3ac150ef8635.cpp: In function ‘bool pairs_sum_to_zero(st d::vector<int>)’: a6413219-36c6-482f-8d66-3ac150ef8635.cpp:10:28: error: no match for ‘operator[]’  (operand types are ‘std::set<int>’ and ‘int’)    10 |         int complement = -s[i];       |                            ^

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -5,10 +5,10 @@
 using namespace std;
 
 bool pairs_sum_to_zero(vector<int> l) {
-    set<int> s(l.begin(), l.end());
+    vector<int> s(l.begin(), l.end()); // Convert set to vector
     for (int i = 0; i < s.size(); i++) { 
         int complement = -s[i];
-        if (find(s.rbegin() + i, s.rend(), complement) != s.rend()) {
+        if (find(std::next(s.begin(), i + 1), s.end(), complement) != s.end()) {
             return true;
         }
     }
