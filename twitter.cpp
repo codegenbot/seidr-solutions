d101d389a1b8107c4302f8bb62fef208ab800e29
beep boop <githubbot@vadim.me>
2024-07-23 12:03:09 +0200

    Solution 18 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/aqybqb2v Prompt: it must return Your tweet has 125 characters for input /]6r&ATIJX}qx&q\Q.:x`"*_`:_X`JM!ph}xzKz)=lqHB*nWp[&jW#p%o/2gUxf/SO2qlZc;ibYAh7!.euix;0l+H$3#]Klx=]x\uoM7gFX"Knk-gEEr{6kUeFEX2, but it returns Enter a tweet: Your tweet has 125 characters.

--- twitter.cpp
+++ twitter.cpp
@@ -1,22 +1,18 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-string validateTweet(string tweet) {
-    if (tweet.empty()) {
-        return "You didn't type anything";
-    }
-    if (tweet.length() > 140) {
-        return "Too many characters";
-    }
-    return "Your tweet has " + to_string(tweet.length() - (tweet.find('\n') != string::npos ? 1 : 0)) + " characters";
-}
-
 int main() {
     string tweet;
     cout << "Enter a tweet: ";
     getline(cin, tweet);
-    cout << validateTweet(tweet) << endl;
-    return 0;
+    if (tweet.empty()) {
+        return "You didn't type anything";
+    }
+    int charCount = 0;
+    for (char c : tweet) {
+        if (isprint(c)) {
+            charCount++;
+        }
+    }
+    if (charCount > 140) {
+        return "Too many characters";
+    }
+    return "Your tweet has " + to_string(charCount) + " characters";
 }
