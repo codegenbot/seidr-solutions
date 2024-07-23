d12e7154dd5fac594f21e37faa72af34034cf674
beep boop <githubbot@vadim.me>
2024-07-23 19:41:03 +0200

    Solution 1 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5lqk7n16 Prompt: Complete the following code given the task description and function signature.

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,5 @@
 def sort_third(l: list):
-    return [i if i % 3 != 0 else sorted([x for x in l if x % 3 == 0])[0] for i in l]
+    return [
+        x if i % 3 != 0 else sorted([y for y in l[: i // 3 * 3 + (i + 1) % 3 :]])[0]
+        for i, x in enumerate(l)
+    ]
