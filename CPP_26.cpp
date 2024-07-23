b471fd459f11728803c66421de21639a856477d1
beep boop <githubbot@vadim.me>
2024-07-23 10:58:35 +0200

    Solution 94 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3o8fpvty Prompt:  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 61bd7e3e-8ab3-4dc9-be56-527c946941b5.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 61bd7e3e-8ab3-4dc9-be56-527c946941b5.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 61bd7e3e-8ab3-4dc9-be56-527c946941b5.cpp:6: 61bd7e3e-8ab3-4dc9-be56-527c946941b5.cpp: In function ‘int main()’: 61bd7e3e-8ab3-4dc9-be56-527c946941b5.cpp:16:20: error: ‘remove_duplicates’ was n ot declared in this scope    16 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 61bd7e3e-8ab3-4dc9-be56-527c946941b5.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,29 +1,27 @@
 ```cpp
 #include <vector>
 #include <algorithm>
-#include <assert.h>
 
-std::vector<int> removeDuplicates(const std::vector<int>& nums) {
-    if (nums.empty()) {
-        return nums;
-    }
+std::vector<int> removeDuplicates(std::vector<int> vec) {
+    if (vec.empty())
+        return vec;
 
-    std::vector<int> result = {nums[0]};
+    std::vector<int> result(1, vec[0]);
 
-    for (int i = 1; i < nums.size(); ++i) {
-        if (nums[i] != result.back()) {
-            result.push_back(nums[i]);
+    for (int i = 1; i < vec.size(); i++) {
+        if (vec[i] != result.back()) {
+            result.push_back(vec[i]);
         }
     }
 
     return result;
 }
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
+bool same(std::vector<int> a, std::vector<int> b) {
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
-int main() {
-    assert(issame(removeDuplicates({1, 2, 3, 2, 4, 3, 5}), {1, 2, 3, 4, 5}));
+int solve() {
+    assert(same(removeDuplicates({1, 2, 3, 2, 4, 3, 5}), {1, 2, 3, 4, 5}));
     return 0;
 }
