1d2489336661d110c8c5a7fc9c60841345afc74f
beep boop <githubbot@vadim.me>
2024-07-23 21:00:17 +0200

    Solution 1 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6vgs0yaa Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,32 +1,26 @@
+#include <vector>
 #include <algorithm>
 
 string sort_numbers(string numbers) {
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                {"eight", 8}, {"nine", 9}};
-    vector<string> numVec;
+    vector<string> nums;
     string num;
 
-    for (int i = 0; i < numbers.length(); i++) {
-        if (numbers[i] == ' ') {
-            continue;
+    for (char c : numbers) {
+        if (c == ' ') {
+            nums.push_back(num);
+            num = "";
+        } else {
+            num += c;
         }
-        int j = i + 1;
-        while (j <= numbers.length() && !isdigit(numbers[j])) {
-            j++;
-        }
-        num = numbers.substr(i, j - i);
-        i = j;
-        numVec.push_back(num);
+    }
+    nums.push_back(num);
+
+    sort(nums.begin(), nums.end());
+
+    string result = "";
+    for (string n : nums) {
+        result += n + " ";
     }
 
-    sort(numVec.begin(), numVec.end(), [&numMap](string a, string b) {
-        return to_string(numMap.at(a)) < to_string(numMap.at(b));
-    });
-
-    string result;
-    for (const auto& num : numVec) {
-        result += num + " ";
-    }
     return result.substr(0, result.length() - 1);
 }
