49f9a7fb318304b9134c0ae0ea0454448a2cacc3
beep boop <githubbot@vadim.me>
2024-07-23 09:36:43 +0200

    Solution 14 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s9lxoq63 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1dc1307c-0b42-4593-a4e4-845d483fb b08.py", line 11, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1dc1307c-0b42-4593-a4e4-845d483fb b08.py", line 8, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,11 +1,10 @@
-```
 def fizz_buzz(n: int):
-    for i in range(1, n + 1):
+    count = 0
+    for i in range(1, n + 1): 
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            count += 1
         elif i % 3 == 0:
-            print("Fizz")
+            count += 1
         elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+            count += 1
+    return count
