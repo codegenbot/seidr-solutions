a88cd6ef26e4807d8b7e93e785ace12153b2f05e
beep boop <githubbot@vadim.me>
2024-07-24 01:49:54 +0200

    Solution 47 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64c7d3d7-1bef-4d29-a2a4-c3eca275a 1f2.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64c7d3d7-1bef-4d29-a2a4-c3eca275a 1f2.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,5 @@
 def sort_even(l: list):
-    result = []
-    i = 0
-    for x in l:
-        if x % 2 == 0:
-            result.insert(i, [x, None])
-            i += 1
-        else:
-            while len(result) > i and result[i][0] % 2 != 0:
-                i += 1
-            result.insert(i, [None, x])
-    return [[x[0], x[1]] for x in result if x[0] is not None or x[1] is not None]
+    l.sort()
+    pos_evens = [x for x in l if 0 <= x % 2]
+    neg_odds = sorted([x for x in l if x % 2 != 0], reverse=True)
+    return [(i, o) for i, o in zip(pos_evens, neg_odds)]
