8f03719dd904777a2f9b7a5293be14b9e8fc0606
beep boop <githubbot@vadim.me>
2024-07-23 09:17:43 +0200

    Solution 34 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ghl9lyyn Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 29af2653-ab37-4eba-b9ae-61959cbc4619.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 29af2653-ab37-4eba-b9ae-61959cbc4619.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 29af2653-ab37-4eba-b9ae-61959cbc4619.cpp:13: 29af2653-ab37-4eba-b9ae-61959cbc4619.cpp: In function ‘int main()’: 29af2653-ab37-4eba-b9ae-61959cbc4619.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 29af2653-ab37-4eba-b9ae-61959cbc4619.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -3,17 +3,13 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 vector<int> largest_smallest_integers(vector<int> lst){
-    int max_negative = 0, min_positive = 0;
+    int max_negative = INT_MIN, min_positive = INT_MAX;
     for (int num : lst) {
-        if (num < 0 && num < max_negative) {
+        if (num < 0 && num > max_negative) {
             max_negative = num;
-        } else if (num > 0 && (num < min_positive || min_positive == 0)) {
+        } else if (num > 0 && num < min_positive) {
             min_positive = num;
         }
     }
     return {max_negative, min_positive};
-}
-
-int main() {
-    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
 }
