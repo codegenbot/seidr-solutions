744295d79d870b39ab5823e5aca4cf4ff6c4ff26
beep boop <githubbot@vadim.me>
2024-07-23 23:27:35 +0200

    Solution 13 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4u8mdrkz Prompt: Program did not receive expected input

--- Python_3.py
+++ Python_3.py
@@ -2,11 +2,9 @@
 from typing import List
 
 def below_zero(operations: List[int]) -> bool:
-    balance = 0
     for operation in operations:
         if operation < 0:
             return True
-        balance += operation
     return False
 
 if __name__ == "__main__":
