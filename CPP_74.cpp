1827a5c8fecda5b9ea62797f19fa221b7b6d2206
beep boop <githubbot@vadim.me>
2024-07-23 23:41:55 +0200

    Solution 22 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jao9uj68 Prompt:       |                              ^~~~~~ cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp:20: cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp: In function ‘int main()’: cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp:30:20: error: ‘total_match’ was not dec lared in this scope    30 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ cbbfb487-b9f4-4a01-a0ea-7df7da0b8469.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,12 +1,14 @@
-```cpp
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a,vector<string> b){
     return a.size() == b.size();
 }
 
+int main(){
+    assert(issame(total_match({"this"}, {}), {}));
+}
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    bool issame(vector<string> a, vector<string> b){
+        return a.size() == b.size();
+    }
     int sum1 = 0;
     for (const string& str : lst1) {
         sum1 += str.length();
@@ -16,7 +18,7 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
         sum2 += str.length();
     }
     
-    if (issame(lst1, lst2)) {
+    if (issame({lst1}, {lst2})) {
         return lst1;
     } else if (sum1 < sum2) {
         return lst1;
