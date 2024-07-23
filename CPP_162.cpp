5c3a08a8c00ec811c8565afd90cda074c08f87c9
beep boop <githubbot@vadim.me>
2024-07-23 19:46:11 +0200

    Solution 0 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a2njg5yl Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_162.cpp
@@ -0,0 +1,22 @@
+#include <openssl/ssl.h>
+#include <openssl/sha.h>
+
+string string_to_md5(string text) {
+    if (text.empty()) return "None";
+
+    unsigned char md[16];
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    const char* cstr = text.c_str();
+    MD5_Update(&ctx, cstr, text.size());
+    MD5_Final(md, &ctx);
+
+    string result;
+    for (int i = 0; i < 16; ++i) {
+        char buff[3];
+        sprintf(buff, "%02x", md[i]);
+        result += buff;
+    }
+
+    return result;
+}
