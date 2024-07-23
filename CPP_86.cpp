840cd21dda7846f6473aae252b0d7205fa598797
beep boop <githubbot@vadim.me>
2024-07-24 00:24:16 +0200

    Solution 12 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9be4i5dy Prompt: e37cd6cd-af13-467e-9e7c-1269eb4397f8.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string anti_shuffle(string s) {       | ^~~~~~ In file included from e37cd6cd-af13-467e-9e7c-1269eb4397f8.cpp:21: e37cd6cd-af13-467e-9e7c-1269eb4397f8.cpp: In function ‘int main()’: e37cd6cd-af13-467e-9e7c-1269eb4397f8.cpp:23:13: error: ‘anti_shuffle’ was not de clared in this scope    23 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,19 +1,25 @@
-#include <string>
+#include <algorithm>
+using namespace std;
 
-string anti_shuffle(string s) {
-    string result = "";
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == ' ') {
-            result += " ";
-            continue;
+std::string anti_shuffle(std::string s) {
+    std::string result = "";
+    for (int i = 0; i <= s.length(); i++) {
+        if (i == s.length() || s[i] == ' ') {
+            for (char c : s.substr(i)) {
+                result += to_string(c);
+            }
+            result += ' ';
+        } else {
+            char c = s[i];
+            while (i < s.length() && s[i] <= c) i++;
+            result += string(1, c);
         }
-        string word = "";
-        for (int j = i; j < s.length() && s[j] != ' '; j++) {
-            word += s[j];
-        }
-        sort(word.begin(), word.end());
-        result += word;
-        i += word.length() - 1;
     }
     return result;
+}
+
+int main() {
+    assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==
+".Hi My aemn is Meirst .Rboot How aer ?ouy");
+    return 0;
 }
