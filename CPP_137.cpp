bb2f6f3fb4c91d555d1e27f9b75c036693d880b0
beep boop <githubbot@vadim.me>
2024-07-23 08:52:47 +0200

    Solution 20 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/fsg8yz42 Prompt:     7 | return "None";       | ^~~~~~ 00d9b962-a3a2-45fd-a806-ea0c7d684ce1.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from 00d9b962-a3a2-45fd-a806-ea0c7d684ce1.cpp:10: 00d9b962-a3a2-45fd-a806-ea0c7d684ce1.cpp: In function ‘int main()’: 00d9b962-a3a2-45fd-a806-ea0c7d684ce1.cpp:12:13: error: ‘boost’ has not been decl ared    12 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 00d9b962-a3a2-45fd-a806-ea0c7d684ce1.cpp:12:29: error: ‘string’ was not declared  in this scope    12 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 00d9b962-a3a2-45fd-a806-ea0c7d684ce1.cpp:12:37: error: ‘compare_one’ was not dec lared in this scope    12 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,13 +1,15 @@
-#include <boost/any.hpp>
+#include <iostream>
 #include <string>
+#include <boost/any.hpp>
+
 using namespace boost;
 
-boost::any solve(const boost::any& a, const boost::any& b) {
+boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float))
         return max(boost::any_cast<int>(a), boost::any_cast<float>(b));
     if (a.type() == typeid(float) && b.type() == typeid(int))
         return max(boost::any_cast<float>(a), boost::any_cast<int>(b));
     if (a.type() == typeid(std::string) && b.type() == typeid(std::string))
         return (boost::any_cast<std::string>(a) > boost::any_cast<std::string>(b)) ? a : b;
-    return boost::any("None");
+    return boost::any();
 }
