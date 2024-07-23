8cbc18dbaad6b60a122aa582b78d9157c4bbe0a5
beep boop <githubbot@vadim.me>
2024-07-23 10:31:08 +0200

    Solution 77 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ebm570ta Prompt: it must return 13 for input 14 21 77 50 -24 52 -70 47 57 -9 -64 -76 -27 -5 -31, but it returns .

--- basement.py
+++ basement.py
@@ -5,4 +5,4 @@ def basement(vector):
         if total < 0:
             min_index = i
         total += num
-    return min_index if min_index != len(vector) else -1
+    return min_index if min_index != len(vector) else -1.
