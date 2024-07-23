b537fd133ba3b77640296dc7e9f2e18d0c88873e
beep boop <githubbot@vadim.me>
2024-07-24 00:56:12 +0200

    Solution 95 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/d90l9i2w Prompt: madam helll Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/648b466d-41a0-4e38-a065-9fb8db283 1b4.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/648b466d-41a0-4e38-a065-9fb8db283 1b4.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,12 +1,10 @@
-```
 def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s
     mid = len(s) // 2
     first_half = s[:mid + 1]
-    second_half = s[mid:-1] if len(s) % 2 != 0 else s[mid:][::-1]
+    second_half = first_half[::-1] + (first_half if len(s) % 2 != 0 else "")
     return first_half + second_half
-
 
 print(make_palindrome("madam"))
 print(make_palindrome("hello"))
