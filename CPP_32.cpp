94057256113de274466b10505fc85eb4fe773365
beep boop <githubbot@vadim.me>
2024-07-23 21:54:27 +0200

    Solution 31 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3hjsd5tf Prompt: e50f5486-09b1-4e10-92cd-b68490159306.cpp:19:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    19 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff e50f5486-09b1-4e10-92cd-b68490159306.cpp:22:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    22 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff e50f5486-09b1-4e10-92cd-b68490159306.cpp:22:36: error: ‘find_zero’ cannot be use d as a function    22 |         solution = find_zero(coeffs);       |                                    ^ In file included from e50f5486-09b1-4e10-92cd-b68490159306.cpp:6: e50f5486-09b1-4e10-92cd-b68490159306.cpp:23:21: error: ‘poly’ was not declared i n this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ e50f5486-09b1-4e10-92cd-b68490159306.cpp:23:17: error: ‘abs’ was not declared in  this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,24 +1,16 @@
 double find_zero(vector<double> xs) {
-    vector<double> coeffs;
-    for (int i = 0; i < xs.size(); i++) {
-        if (i == 1)
-            continue;
-        for (int j = i; j < xs.size(); j++) {
-            if (j == i)
-                coeffs.push_back(xs[i]);
-            else
-                coeffs.push_back(xs[j] * pow(-1, j - i));
-        }
+    vector<double> coeffs(xs.size());
+    for(int i = 0; i < xs.size(); ++i) {
+        coeffs[i] = xs[(xs.size()-1)-i];
     }
-
-    double x = -xs[1] / (2 * xs[0]);
-    return poly(coeffs, x) == 0 ? x : 0;
+    double x = -coeffs[1]/(2*coeffs[0]);
+    return poly(coeffs, x);
 }
 
 double poly(vector<double> coeffs, double x) {
     double result = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        result += coeffs[i] * pow(x, i);
+    for(int i = 0; i < coeffs.size(); ++i) {
+        result += coeffs[i]*pow(x,(coeffs.size()-1)-i);
     }
     return result;
 }
