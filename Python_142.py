9cb60894bf4883e3e951ce7046a173ff09e7170c
beep boop <githubbot@vadim.me>
2024-07-23 13:37:37 +0200

    Solution 46 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544a1f00-260f-43b6-af34-5fbbdc0cb 14c.py", line 38, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544a1f00-260f-43b6-af34-5fbbdc0cb 14c.py", line 8, in check     sum_squares(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/544a1f00-260f-43b6-af34-5fbbdc0cb 14c.py", line 2, in sum_squares     numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split( ))))                                                                ^^^^^^^^^^^^^^^^ AttributeError: 'list' object has no attribute 'split'

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,3 @@
 def sum_squares(user_input):
-    numbers = list(map(int, filter(lambda x: str.isnumeric(x), ' '.join(map(str, user_input)).split())))
+    numbers = list(map(int, filter(lambda x: str.isnumeric(x), user_input.split())))
     print(sum([n ** 2 for n in numbers]))
