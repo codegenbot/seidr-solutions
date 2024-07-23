de0a4f4a04857c0f301ea4bf1a850ba43b8876a0
beep boop <githubbot@vadim.me>
2024-07-23 12:17:10 +0200

    Solution 9 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/si5gzmww Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8ab9ced4-f6f7-449b-bd20-fe1def648 988.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,10 +1,8 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        return a if a.replace(',', '.').replace(' ', '') > str(b).replace(',', '.').replace(' ', ''):
-        else: b
+        return a if float(a.replace(',', '.')) > b: b
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        return b if b.replace(',', '.').replace(' ', '') > str(a).replace(',', '.').replace(' ', ''):
-        else: a
+        return b if float(b.replace(',', '.')) > a: a
     else:
         return a if a > b: b if a < b: None
