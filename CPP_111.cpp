0cdd29c3da3d1f6b161ecbc355e15fc8fb698816
beep boop <githubbot@vadim.me>
2024-07-23 17:53:41 +0200

    Solution 94 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/gdumde1a Prompt:  = std::allocator<std::pair<const char, int> >; std::__enable_if_t<std::is_same< _Val, typename std::iterator_traits<_InputIterator>::value_type>::value> = void;  typename std::iterator_traits<_InputIterator>::value_type = std::pair<const cha r, int>]’ /usr/include/c++/11/bits/stl_map.h:232:36:   required from ‘std::map<_Key, _Tp, _Compare, _Alloc>::map(std::initializer_list<std::pair<const _Key, _Tp> >, const  _Compare&, const allocator_type&) [with _Key = char; _Tp = int; _Compare = std: :less<char>; _Alloc = std::allocator<std::pair<const char, int> >; std::map<_Key , _Tp, _Compare, _Alloc>::allocator_type = std::allocator<std::pair<const char, int> >]’ af616339-4863-4257-b018-e94b5e3d5d5e.cpp:33:18:   required from here /usr/include/c++/11/bits/stl_tree.h:594:15: error: no matching function for call  to ‘operator new(sizetype, std::_Rb_tree_node<std::pair<const char, int> >*&)’   594 |               ::new(__node) _Rb_tree_node<_Val>;       |               ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘std::_Rb_tree<char,  std::pair<const char, int>, std::_Select1st<std::pair<const char, int> >, std:: less<char>, std::allocator<std::pair<const char, int> > >::_Link_type’ {aka ‘std ::_Rb_tree_node<std::pair<const char, int> >*’} to ‘std::align_val_t’

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -11,19 +11,27 @@ std::map<char, int> histogram(std::string s) {
             res[c]++;
         }
     }
+    return res;
 }
 
 bool issame(std::map<char, int> a, std::map<char, int> b) {
-    if (a.size() != b.size())
+    if (a.size() != b.size()) {
         return false;
-
-    for (auto& pair : a) {
-        if (b.find(pair.first) == b.end() || pair.second != b[pair.first])
+    }
+    for (auto p : a) {
+        if (b.find(p.first) == b.end() || p.second != b[p.first]) {
             return false;
+        }
+    }
+    for (auto p : b) {
+        if (a.find(p.first) == a.end()) {
+            return false;
+        }
     }
     return true;
 }
 
 int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
+    assert(issame(histogram("a"), histogram("a")));
     return 0;
+}
