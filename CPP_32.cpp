7db63bb8a897b3e818668191d42301ca4d22a59c
beep boop <githubbot@vadim.me>
2024-07-23 21:47:19 +0200

    Solution 17 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3hjsd5tf Prompt: 0b42e397-e93d-4cbb-bd1d-540ccb646c72.cpp:19:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    19 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 0b42e397-e93d-4cbb-bd1d-540ccb646c72.cpp:22:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    22 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 0b42e397-e93d-4cbb-bd1d-540ccb646c72.cpp:22:36: error: ‘find_zero’ cannot be use d as a function    22 |         solution = find_zero(coeffs);       |                                    ^ In file included from 0b42e397-e93d-4cbb-bd1d-540ccb646c72.cpp:6: 0b42e397-e93d-4cbb-bd1d-540ccb646c72.cpp:23:21: error: ‘poly’ was not declared i n this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 0b42e397-e93d-4cbb-bd1d-540ccb646c72.cpp:23:17: error: ‘abs’ was not declared in  this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 
 double poly(vector<double> coeffs, double x) {
-    double result = 0;
-    for (int i = 0; i < coeffs.size(); i++) {
+    double result = coeffs[0];
+    for (int i = 1; i < coeffs.size(); i++) {
         result += coeffs[i] * pow(x, i);
     }
     return result;
 }
 
-double find_zero(vector<double> xs) {
+double find_zero(vector<double> xs){
     double x = -xs[1] / (2*xs[0]);
     return x;
 }
 
 int main() {
-    vector<double> xs = {1, 3};
-    
+    vector<double> xs = {1.0, -3.0, 2.0};  // input your coefficients here...
     double solution = find_zero(xs); 
     assert(abs(poly(xs, solution)) < 1e-3);
 }
