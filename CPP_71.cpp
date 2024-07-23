1e067072cfe8716cf92d57f4182e65b31eb5f2c9
beep boop <githubbot@vadim.me>
2024-07-23 11:08:38 +0200

    Solution 10 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4xxec241 Prompt: cc0149cf-450d-45e9-8e45-654abb28349a.cpp: In function ‘int main()’: cc0149cf-450d-45e9-8e45-654abb28349a.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(abs(triangle_area(2, 2, 10)) + 1 < 0.01);       |     ^~~~~~ cc0149cf-450d-45e9-8e45-654abb28349a.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <iostream>   +++ |+#include <cassert>     3 | cc0149cf-450d-45e9-8e45-654abb28349a.cpp: At global scope: cc0149cf-450d-45e9-8e45-654abb28349a.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ cc0149cf-450d-45e9-8e45-654abb28349a.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,8 +1,17 @@
+#include <cmath>
+#include <iostream>
+#include <cassert>
+
 float triangle_area(float a, float b, float c) {
     if ((a + b > c) && (a + c > b) && (b + c > a)) {
-        float s = (a + b + c) / 2.0;
-        return round((s * (s - a) * (s - b) * (s - c)) * 100.0) / 100.0;
+        float s = (a + b + c) / 2;
+        return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
     } else {
-        return -1.0f;
+        return -1;
     }
+}
+
+int main() {
+    assert(abs(triangle_area(2, 2, 10)) + 1 < 0.01);
+    return 0;
 }
