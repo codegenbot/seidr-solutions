4c3c80227140a4d87353cf02f268903c896f2a8d
beep boop <githubbot@vadim.me>
2024-07-24 01:49:09 +0200

    Solution 46 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf7094f8-9e33-4c44-a5ba-914ac0bb2 a3e.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf7094f8-9e33-4c44-a5ba-914ac0bb2 a3e.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,12 @@
 def sort_even(l: list):
-    l.sort()
-    evens = [x for x in l if x % 2 == 0]
-    odds = [x for x in l if x % 2 != 0]
-    return [(i, o) for i, o in zip(evens, odds)]
+    result = []
+    i = 0
+    for x in l:
+        if x % 2 == 0:
+            result.insert(i, [x, None])
+            i += 1
+        else:
+            while len(result) > i and result[i][0] % 2 != 0:
+                i += 1
+            result.insert(i, [None, x])
+    return [[x[0], x[1]] for x in result if x[0] is not None or x[1] is not None]
