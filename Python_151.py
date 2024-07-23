0565966bb8292f90d571fc5672f28848579886d8
beep boop <githubbot@vadim.me>
2024-07-23 12:27:02 +0200

    Solution 7 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a2a47e75-73d1-4dd4-a21d-f781e12cf c77.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a2a47e75-73d1-4dd4-a21d-f781e12cf c77.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,6 @@
-def find_mean(lst):
-    if len(lst) == 1:
-        return None
-    return sum(i for i in lst) / len(lst)
+def double_the_difference(n):
+    if len([i for i in n if isinstance(i, int)]) != 1:
+        return "Input should have exactly one integer"
+    else:
+        num = [i for i in n if isinstance(i, int)][0]
+        return abs((sum(i**2 for i in n) - (num**2)) // 2)
