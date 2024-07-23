0538c719a61df600b9ce9bb69ae2a458be1fff93
beep boop <githubbot@vadim.me>
2024-07-23 21:11:58 +0200

    Solution 70 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8lab5b84 Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -2,4 +2,4 @@ def decode_shift(s: str):
     if 'z' in s.lower():
         return "".join([chr(((ord(ch) - 97) % 26) + 97)) for ch in s.lower()])
     else:
-        return "".join([chr(((ord(ch) - 97) % 26) + 97)) for ch in s.lower()])
+        return "Invalid input. Contains characters out of alphabet."
