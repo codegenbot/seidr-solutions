5f8e407a972ade9d3df61fafa5fcdf42c90f2683
beep boop <githubbot@vadim.me>
2024-07-23 14:40:30 +0200

    Solution 29 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ghingowb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2131e0ae-01c4-4b00-aaff-cba541fb6 fe1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,4 +1,18 @@
-def find_gcd(a: int, b: int) -> int:
-    while b != 0:
-        a, b = b, a % b
-    return abs(a)
+from typing import List
+import math
+
+
+def factorize(n: int) -> List[int]:
+    factors = []
+    i = 2
+    while n > 1:
+        if n % i:
+            i += 1
+        else:
+            count = 0
+            while n % i == 0:
+                n //= i
+                count += 1
+            for _ in range(count):
+                factors.append(i)
+    return sorted(factors)
