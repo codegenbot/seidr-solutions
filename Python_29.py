5e0fe942ad9ffaf728b1e615683320484fb9c7b0
beep boop <githubbot@vadim.me>
2024-07-23 10:02:42 +0200

    Solution 41 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/txje1r3n Prompt: Program did not receive expected input

--- Python_29.py
+++ Python_29.py
@@ -10,7 +10,7 @@ def filter_by_prefix() -> List[str]:
 
             if not strings.strip() or not prefix:
                 raise ValueError("Please enter valid inputs!")
-            
+        
             filtered_strings = [s.strip() for s in strings.split() if s.strip().startswith(prefix)]
 
             return filtered_strings
