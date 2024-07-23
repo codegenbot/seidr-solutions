1b2ff7cd0f89097368a72bd1a9ee33f1393d570c
beep boop <githubbot@vadim.me>
2024-07-23 14:38:41 +0200

    Solution 88 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/832kffqc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8dce0b4d-0f34-4fc2-b80a-f981c8e75 e57.py", line 1     ```     ^ SyntaxError: invalid syntax

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,6 +1,7 @@
+```
 cipher_dict = {}
 for i, char in enumerate(input("Enter the first string of characters to substitute: ").upper()):
-    cipher_dict[char] = input(f"{'Enter the character substitution ('+char+')': }")
+    cipher_dict[char] = input(f"Enter the character substitution ({char}/): ")
 
 text_to_decrypt = input("Enter the text to decrypt: ")
 decrypted_text = ""
