925511f942982ef99476cf3284bf572e4eba63a3
beep boop <githubbot@vadim.me>
2024-07-23 13:55:58 +0200

    Solution 63 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9303066b-4abf-4c81-b6c2-bcaf67285 e20.py", line 15, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9303066b-4abf-4c81-b6c2-bcaf67285 e20.py", line 12, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: sorted_list_sum() takes 0 positional arguments but 1 was given

--- Python_149.py
+++ Python_149.py
@@ -1,13 +1,12 @@
-```
 def sorted_list_sum():
     numbers = input("Enter a list of integers separated by spaces: ")
     if numbers:
         numbers = [int(i) for i in numbers.split()]
+        even_numbers = []
+        try:
+            even_numbers = [num for num in numbers if num % 2 == 0]
+        except ValueError as e:
+            return str(e)
+        return " ".join(map(str, sorted(even_numbers)))
     else:
         return "Please enter some numbers."
-    even_numbers = []
-    try:
-        even_numbers = [num for num in numbers if num % 2 == 0]
-    except ValueError as e:
-        return str(e)
-    return " ".join(map(str, sorted(even_numbers)))
