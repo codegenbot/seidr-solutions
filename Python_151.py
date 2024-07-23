7b43b1bd6d5ae508bead90c3c2e52e6d5026c6e4
beep boop <githubbot@vadim.me>
2024-07-23 17:51:40 +0200

    Solution 11 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7ctzsi89 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c19933fc-c0c2-4d26-9caa-c82b6fd3b 6be.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c19933fc-c0c2-4d26-9caa-c82b6fd3b 6be.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,5 @@
-```
-def double_the_difference(a, b):
-    return sum(i**2 for i in [a, b] if isinstance(i, int) and i % 2 != 0)
-
-user_input_a = int(input("Enter first number: "))
-user_input_b = int(input("Enter second number: "))
-
-result = double_the_difference(user_input_a, user_input_b)
-print(result)
+def double_the_difference(lst1):
+    lst2 = input("Enter a list of integers: ").split()
+    return sum(
+        i**2 for i in map(int, lst2) if isinstance(int(i), int) and int(i) % 2 != 0
+    )
