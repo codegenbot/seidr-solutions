bf7529f0c54e0dc562b24dc43d5f4d07866d2802
beep boop <githubbot@vadim.me>
2024-07-23 22:03:00 +0200

    Solution 31 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/qa4wotyf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9593b70-d56d-4001-9e29-4694c1e54 28f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,13 +1,14 @@
 def smallest_change(arr):
-    arr = list(map(int, input("Enter an integer array: ").split()))
+    arr.sort()
     n = len(arr)
-    rev_arr = arr[::-1]
+    rev_arr = str(arr)[::-1]
     
     changes = 0
     for i in range(n // 2):
-        if arr[i] != rev_arr[n - i - 1]:
+        if str(arr[i]) != rev_arr[n - i - 1]:
             changes += 1
     
     return changes
 
-print(smallest_change([]))
+input_array = list(map(int, input("Enter the array elements separated by space: ").split()))
+print(smallest_change(input_array))
