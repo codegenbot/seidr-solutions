595279bbc5b67b1ba25c397f42f56d328d084bfe
beep boop <githubbot@vadim.me>
2024-07-23 08:24:04 +0200

    Solution 6 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8ks3fd7x Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37bca061-d127-4806-9310-26325c4db ef0.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/37bca061-d127-4806-9310-26325c4db ef0.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,9 @@
+```
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
 def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
-        return string + string
-    for i in range(len(string)):
-        postfix = string[i:]
-        if is_palindrome(postfix):
-            return string + postfix[::-1]
-    return string + string[::-1]
+    if len(string) % 2 == 0:
+        return string + string[::-1]
+    else:
+        return string[:len(string)//2] + string + string[:len(string)//2][::-1]
