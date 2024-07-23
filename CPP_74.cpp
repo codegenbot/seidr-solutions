3bfa21c6d9af99cb406b75f35ff591633a1f80b9
beep boop <githubbot@vadim.me>
2024-07-24 01:21:55 +0200

    Solution 72 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jao9uj68 Prompt:       |           ^~~~~~ In file included from 1a794b13-f4fe-42d0-bd35-b25d0d775834.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1a794b13-f4fe-42d0-bd35-b25d0d775834.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1a794b13-f4fe-42d0-bd35-b25d0d775834.cpp:33:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 1a794b13-f4fe-42d0-bd35-b25d0d775834.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 1a794b13-f4fe-42d0-bd35-b25d0d775834.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 1a794b13-f4fe-42d0-bd35-b25d0d775834.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -25,6 +25,6 @@ std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<
     } else if (sum1 > sum2) {
         return lst2;
     } else {
-        return issame(lst1, lst2) ? lst1 : total_match(total_match(lst1, std::vector<std::string>()), std::vector<std::string>());
+        return issame(lst1, lst2) ? lst1 : total_match(total_match(lst1, lst2), std::vector<std::string>());
     }
 }
