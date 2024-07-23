8a381940673bfefc2141da00d5804e85008ae64f
beep boop <githubbot@vadim.me>
2024-07-23 22:34:57 +0200

    Solution 71 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/b67etnp4 Prompt:   +++ |+#include <ostream>     3 | 333fe128-c365-4b84-a920-52b7651609a5.cpp:21:9: error: ‘cout’ was not declared in  this scope    21 |         cout << "No close elements found." << endl;       |         ^~~~ 333fe128-c365-4b84-a920-52b7651609a5.cpp:21:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 333fe128-c365-4b84-a920-52b7651609a5.cpp:21:47: error: ‘endl’ was not declared i n this scope    21 |         cout << "No close elements found." << endl;       |                                               ^~~~ 333fe128-c365-4b84-a920-52b7651609a5.cpp:21:47: note: ‘std::endl’ is defined in header ‘<ostream>’; did you forget to ‘#include <ostream>’? 333fe128-c365-4b84-a920-52b7651609a5.cpp: At global scope: 333fe128-c365-4b84-a920-52b7651609a5.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 333fe128-c365-4b84-a920-52b7651609a5.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
 #include <vector>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-bool has_close_elements(std::vector<float> numbers, float threshold){
+bool has_close_elements(vector<float> numbers, float threshold){
     for(int i = 0; i < numbers.size(); i++){
         for(int j = i + 1; j < numbers.size(); j++){
             if(abs(numbers[i] - numbers[j]) <= threshold)
@@ -15,7 +15,7 @@ bool has_close_elements(std::vector<float> numbers, float threshold){
 }
 
 int main() {
-    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
+    vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
     if (has_close_elements(a, 0.5)) {
         cout << "There are close elements." << endl;
     } else {
