9657747be76c00763179fd0973786304812db914
beep boop <githubbot@vadim.me>
2024-07-23 08:28:12 +0200

    Solution 2 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/slhrroqw Prompt:  this scope    20 |         cout << nums[i] << endl;       |         ^~~~ ae3e8dd4-a69c-47f7-8e20-a6a2a82804ae.cpp:20:17: error: ‘nums’ was not declared i n this scope    20 |         cout << nums[i] << endl;       |                 ^~~~ ae3e8dd4-a69c-47f7-8e20-a6a2a82804ae.cpp:20:28: error: ‘endl’ was not declared i n this scope    20 |         cout << nums[i] << endl;       |                            ^~~~ ae3e8dd4-a69c-47f7-8e20-a6a2a82804ae.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << endl;       |     ^~~~ ae3e8dd4-a69c-47f7-8e20-a6a2a82804ae.cpp:22:13: error: ‘endl’ was not declared i n this scope    22 |     cout << endl;       |             ^~~~ ae3e8dd4-a69c-47f7-8e20-a6a2a82804ae.cpp:24:17: error: ‘nums’ was not declared i n this scope    24 |         cout << nums[i] << endl;       |                 ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,28 +1,29 @@
+#include <iostream>
+
 int main() {
     int n;
-    cin >> n;
-    vector<int> vec(n);
-    for (int i = 0; i < n; i++) {
-        cin >> vec[i];
+    std::cin >> n;
+    std::vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> nums[i];
     }
     
-    int minDiff = INT_MAX;
-    int cutIndex = 0;
-    
-    for (int i = 1; i < n; i++) {
-        int diff = abs(accumulate(vec.begin(), vec.begin() + i, 0) - accumulate(vec.begin() + i, vec.end(), 0));
-        if (diff == 0 || diff < minDiff) {
-            minDiff = diff;
-            cutIndex = i;
+    int min_diff = INT_MAX;
+    int cut_idx = -1;
+    for (int i = 1; i < n; ++i) {
+        int diff = std::abs(std::accumulate(nums.begin(), nums.begin() + i, 0) - std::accumulate(nums.begin() + i, nums.end(), 0));
+        if (diff < min_diff) {
+            min_diff = diff;
+            cut_idx = i;
         }
     }
     
-    for (int i = 0; i < cutIndex; i++) {
-        cout << vec[i] << endl;
+    for (int i = 0; i < cut_idx; ++i) {
+        std::cout << nums[i] << std::endl;
     }
-    cout << endl;
-    for (int i = cutIndex; i < n; i++) {
-        cout << vec[i] << endl;
+    std::cout << std::endl;
+    for (int i = cut_idx; i < n; ++i) {
+        std::cout << nums[i] << std::endl;
     }
     
     return 0;
