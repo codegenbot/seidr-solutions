fcc7240d4bdbc95ac7b6e0c7edde77416a735052
beep boop <githubbot@vadim.me>
2024-07-23 18:33:38 +0200

    Solution 41 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ia9na0d3 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 5e39f851-6fa9-4218-9086-fd26fb24e990.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- spin-words.cpp
+++ spin-words.cpp
@@ -3,30 +3,29 @@
 #include <string>
 #include <algorithm>
 
-using namespace std;
-
-string spinWords(string str) {
-    string result = "";
+std::string spinWords(std::string str) {
+    std::string result = "";
     int start = 0;
-    
+
     for (int i = 0; i <= str.length(); i++) {
         if (i == str.length() || str[i] == ' ') {
-            string word = str.substr(start, i - start);
-            
+            std::string word = str.substr(start, i - start);
+
             if (word.length() >= 5) {
-                reverse(word.begin(), word.end());
+                std::reverse(word.begin(), word.end());
             }
-            
+
             result += word + " ";
             start = i + 1;
         }
     }
-    
+
     return result.substr(0, result.length() - 1);
 }
 
 int main() {
-    string str;
-    cin >> str;
-    cout << spinWords(str) << endl;
+    std::string str;
+    std::cin >> str;
+    std::cout << spinWords(str) << std::endl;
     return 0;
+}
