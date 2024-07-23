e58a451c70ba45d6a320e92f2d7cf46003004c58
beep boop <githubbot@vadim.me>
2024-07-23 13:03:13 +0200

    Solution 52 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jsxsfwkb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a72c4561-8741-4d2b-9041-dc231c568 e2c.py", line 1, in <module>     print(double_the_difference(first_list, second_list))           ^^^^^^^^^^^^^^^^^^^^^ NameError: name 'double_the_difference' is not defined

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,5 @@
 def double_the_difference(first_list, second_list):
-    if not first_list or not second_list:
-        return 0
-    absolute_diff = abs(sum(first_list) - sum(second_list))
-    return absolute_diff * 2
+    difference = sum(set(first_list).symmetric_difference(second_list))
+    return 2 * difference
+
+print(double_the_difference([1, 3], [2, 4]))
