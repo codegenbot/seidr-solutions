8a7b4a9e8b3f1189363bc812a3f9ad7883862c99
beep boop <githubbot@vadim.me>
2024-07-23 20:06:28 +0200

    Solution 65 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/go1mi1xb Prompt: 37efac46-195d-4634-817d-4d4021c5f4d4.cpp:3:11: error: ‘vector’ was not declared in this scope     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |           ^~~~~~ 37efac46-195d-4634-817d-4d4021c5f4d4.cpp:3:21: error: wrong number of template a rguments (1, should be 2)     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |                     ^ In file included from /usr/include/c++/11/utility:70,                  from 37efac46-195d-4634-817d-4d4021c5f4d4.cpp:1: /usr/include/c++/11/bits/stl_pair.h:211:12: note: provided for ‘template<class _ T1, class _T2> struct std::pair’   211 |     struct pair       |            ^~~~ 37efac46-195d-4634-817d-4d4021c5f4d4.cpp:3:22: error: expected unqualified-id be fore ‘,’ token     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |                      ^ 37efac46-195d-4634-817d-4d4021c5f4d4.cpp:3:30: error: expected initializer befor e ‘<’ token     3 | std::pair<vector<int>, vector<int>> cutVector(vector<int> v) {       |                              ^

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,8 +1,7 @@
-```cpp
+#include <vector>
 #include <utility>
-using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+std::pair<std::vector<int>, std::vector<int>> cutVector(std::vector<int> v) {
     int min_diff = INT_MAX;
     int cut_index = 0;
 
@@ -14,7 +13,7 @@ pair<vector<int>, vector<int>> cutVector(vector<int> v) {
         }
     }
 
-    vector<int> left(v.begin(), v.begin() + cut_index);
-    vector<int> right(v.begin() + cut_index, v.end());
+    std::vector<int> left(v.begin(), v.begin() + cut_index);
+    std::vector<int> right(v.begin() + cut_index, v.end());
     return {left, right};
 }
