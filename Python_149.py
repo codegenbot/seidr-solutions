de162d89eff78f9543d9836ae2df3a2f0bf4f961
beep boop <githubbot@vadim.me>
2024-07-23 17:40:35 +0200

    Solution 3 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wz6rl5y0 Prompt: Complete the following code given the task description and function signature.

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,4 @@
 ```
 def sorted_list_sum(lst):
-    return sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), x))
+    lst = [s for s in lst if len(s) % 2 == 0]
+    return sorted(set(lst), key=lambda x: (len(x), x))
