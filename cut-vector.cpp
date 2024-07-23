4e189466ac89a47594abb485c5ee228f918326ab
beep boop <githubbot@vadim.me>
2024-07-23 19:44:27 +0200

    Solution 37 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/go1mi1xb Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7e4cad0d-b688-404d-927a-b0649e0faf2a.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,19 +1,3 @@
 ```cpp
-#include <vector>
-#include <algorithm>
-#include <climits>
-
-std::vector<std::vector<int>> cutVector(const std::vector<int>& v) {
-    int minDiff = INT_MAX;
-    int cutIndex = -1;
-
-    for (int i = 0; i < v.size() - 1; ++i) {
-        int diff = std::abs(v[i] - v[i + 1]);
-        if (diff < minDiff) {
-            minDiff = diff;
-            cutIndex = i;
-        }
-    }
-
-    return {{std::vector<int>(v.begin(), v.begin() + cutIndex + 1)}},
-           {{std::vector<int>(v.begin() + cutIndex + 1, v.end())}}};
+return {{std::vector<int>(v.begin(), v.begin() + cutIndex + 1))},
+        {std::vector<int>(v.begin() + cutIndex + 1, v.end())}};
