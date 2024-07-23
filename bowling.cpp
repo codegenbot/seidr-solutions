a23d5c4dc067abb55dcf3354ba023e98f33610de
beep boop <githubbot@vadim.me>
2024-07-23 18:40:38 +0200

    Solution 72 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/hnop9txo Prompt: - '0'));       |                                                             ^ 5c7b257e-3a9c-47b6-94ee-2d67ce78b706.cpp:4:39: error: invalid types ‘const int[i nt]’ for array subscript     4 |     } else if (index % 2 == 1 && bowls[index] == '/') {       |                                       ^ 5c7b257e-3a9c-47b6-94ee-2d67ce78b706.cpp:5:27: error: invalid types ‘const int[i nt]’ for array subscript     5 |         return 10 - (bowls[index-1] - '0') + ((bowls[index+1] == 'X') ? 10 : (bowls[index+1] - '0'));       |                           ^ 5c7b257e-3a9c-47b6-94ee-2d67ce78b706.cpp:5:53: error: invalid types ‘const int[i nt]’ for array subscript     5 |         return 10 - (bowls[index-1] - '0') + ((bowls[index+1] == 'X') ? 10 : (bowls[index+1] - '0'));       |                                                     ^ 5c7b257e-3a9c-47b6-94ee-2d67ce78b706.cpp:5:84: error: invalid types ‘const int[i nt]’ for array subscript     5 | wls[index-1] - '0') + ((bowls[index+1] == 'X') ? 10 : (bowls[index+1] - '0'));       |                                                             ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,4 +1,4 @@
-int calculateFrameScore(int bowlScore, int index, const std::string& bowls) {
+char calculateFrameScore(char bowlScore, int index, const std::string& bowls) {
     if (bowls[index] == 'X') {
         return 10 + ((bowls[index + 1] == 'X') ? 10 : (bowls[index + 1] - '0')) + ((bowls[index + 2] == 'X') ? 10 : (bowls[index + 2] - '0'));
     } else if (index % 2 == 1 && bowls[index] == '/') {
