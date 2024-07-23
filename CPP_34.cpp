ef252aa5bd1002313f619bc9dc5d9aef67c577a7
beep boop <githubbot@vadim.me>
2024-07-23 22:16:54 +0200

    Solution 52 of CPP/34, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8dwusk5j Prompt: 1b02e512-639f-453b-9223-d04ae49e1dd4.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 1b02e512-639f-453b-9223-d04ae49e1dd4.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 1b02e512-639f-453b-9223-d04ae49e1dd4.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 1b02e512-639f-453b-9223-d04ae49e1dd4.cpp:18:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    18 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 1b02e512-639f-453b-9223-d04ae49e1dd4.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,5 +1,11 @@
 #include <set>
+#include <vector>
+
 using namespace std;
+
+bool isSame(vector<int> a, vector<int> b) {
+    return (a.size() == b.size()) && (a==b);
+}
 
 vector<int> unique(vector<int> l) {
     set<int> s(l.begin(), l.end());
