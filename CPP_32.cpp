e8d325a9367ef5f42a14136ebb38528d0c83b459
beep boop <githubbot@vadim.me>
2024-07-23 21:50:08 +0200

    Solution 22 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3hjsd5tf Prompt: 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:35:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    35 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:38:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    38 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:38:36: error: ‘find_zero’ cannot be use d as a function    38 |         solution = find_zero(coeffs);       |                                    ^ In file included from 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:22: 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:39:21: error: ‘poly’ was not declared i n this scope    39 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 34ad9474-f217-4825-a5c1-40fa86aafa68.cpp:39:17: error: ‘abs’ was not declared in  this scope    39 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,37 +1,23 @@
-#include <iostream>
 #include <vector>
 using namespace std;
 
-int fact(int n) {
-    int i,fact=1;
-    for (i=1;i<=n;i++)
-        fact*=i;
-    return fact;
-}
-
-double find_zero(vector<double> coeffs, double solution){
-    vector<double> xs(5);
+double find_zero(vector<double> xs, vector<int> coeffs, double solution){
     double sum = 0;
     double product = 1;
     int i;
-    for (i=0;i<coeffs.size();i++)
+    for (i=0;i<xs.size();i++)
     {
-        xs[i] = 2.0 - (double)i; // Initialize the x values
-    }
-    
-    for (i=0;i<xsize();i++) {
         if(i%2==0)
-            sum += f(poly, coeffs, solution);
+            sum += xs[i]*coeffs[i];
         else
-            product *= -f(poly, coeffs, solution)/(double)fact((int)i);
+            product *= -xs[i]/(double)fact((int)i);
     }
     return -product/sum;
 }
 
-double poly(vector<double> coeffs, double x){
-    double result = 0;
-    for(int i=0; i<coeffs.size();i++) {
-        result += coeffs[i]*pow(x,(coeffs.size()-1-i));
-    }
-    return result;
+int fact(int n){
+    int i,fact=1;
+    for (i=1;i<=n;i++)
+        fact*=i;
+    return fact;
 }
