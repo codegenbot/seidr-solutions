fa4466d12c3e003110a79101e5b2eb5ddc9222d4
beep boop <githubbot@vadim.me>
2024-07-23 14:18:44 +0200

    Solution 12 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fifgt0ej Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c07c2431-60a6-474d-ad47-822d27a1c 07c.py", line 14, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c07c2431-60a6-474d-ad47-822d27a1c 07c.py", line 9, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c07c2431-60a6-474d-ad47-822d27a1c 07c.py", line 5, in sort_even     sorted((y, x) for x, y in zip(l[1::2], l - none)), start=1                                                ^^^^ NameError: name 'none' is not defined. Did you mean: 'None'?

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,8 @@
-```Python
+```
 def sort_even(l: list):
-    return [sorted([x for x in l if i % 2 == 0])[0] if i % 2 == 0 else x 
-            for i, x in enumerate(l)]
+    return [
+        x if i % 2 != 0 else y
+        for i, (x, y) in enumerate(
+            sorted((y, x) for x, y in zip(l[1::2], [x for x in l])), start=1
+        )
+    ]
