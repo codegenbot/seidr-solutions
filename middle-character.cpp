21f17c1c1721d8776c6b8606bf9af3ad6b13781f
beep boop <githubbot@vadim.me>
2024-07-23 12:05:26 +0200

    Solution 96 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8uq8rzer Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,16 +1,19 @@
+```cpp
 #include <string>
 #include <iostream>
 
 int main() {
-    std::string s;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, s);
+    using namespace std;
+
+    string s;
+    cout << "Enter a string: ";
+    getline(cin, s);
 
     int length = s.length();
     if (length % 2 == 0) {
-        std::cout << "Middle characters are: " << s.substr(length / 2, 2) << std::endl;
+        cout << "Middle characters are: " << s.at(length / 2 - 1) + s.at(length / 2) << endl;
     } else {
-        std::cout << "Middle character is: " << s.substr(length / 2, 1) << std::endl;
+        cout << "Middle character is: " << s.at(length / 2) << endl;
     }
     
     return 0;
