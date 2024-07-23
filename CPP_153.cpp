57f9cdae7ce77a642550bfc5749a322fc5491639
beep boop <githubbot@vadim.me>
2024-07-23 09:29:28 +0200

    Solution 70 of CPP/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sjvdprsu Prompt: 92d50446-4d55-4382-bc7d-12c84f39fbee.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~ In file included from 92d50446-4d55-4382-bc7d-12c84f39fbee.cpp:24: 92d50446-4d55-4382-bc7d-12c84f39fbee.cpp: In function ‘int main()’: 92d50446-4d55-4382-bc7d-12c84f39fbee.cpp:26:13: error: ‘Strongest_Extension’ was  not declared in this scope    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,19 +1,29 @@
-string Strongest_Extension(string class_name, vector<string> extensions){
+#include <string>
+#include <vector>
+#include <cctype>
+#include <climits>
+
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions);
+
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
+    std::string strongest_extension = "";
     int max_strength = INT_MIN;
-    string strongest_extension = "";
-    
-    for(const string& ext : extensions){
+
+    for (const std::string& ext : extensions) {
         int cap_count = 0, sm_count = 0;
-        for(char c : ext){
-            if(isupper(c)) cap_count++;
-            if(islower(c)) sm_count++;
+        for (char c : ext) {
+            if (std::isupper(c)) {
+                cap_count++;
+            } else if (std::islower(c)) {
+                sm_count++;
+            }
         }
         int strength = cap_count - sm_count;
-        if(strength > max_strength){
+        if (strength > max_strength || (strength == max_strength && ext < strongest_extension)) {
             max_strength = strength;
             strongest_extension = ext;
         }
     }
-    
+
     return class_name + "." + strongest_extension;
 }
