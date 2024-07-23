7987c82903b11aeac158844fa8adbd8b8da88bc6
beep boop <githubbot@vadim.me>
2024-07-23 14:47:40 +0200

    Solution 95 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/74due0ay Prompt: Complete the following code given the task description and function signature.

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,10 +1,11 @@
 sort(lst.begin(), lst.end());
-    vector<int> res;
-    int i = 0, j = lst.size() - 1;
-    while (i <= j) {
-        res.push_back(lst[i++]);
-        if (i <= j)
-            res.push_back(lst[j--]);
+vector<int> res;
+int i = 0, j = lst.size() - 1;
+while (i <= j) {
+    res.push_back(lst[i++]);
+    if (i <= j) {
+        res.push_back(lst[j--]);
     }
-    return res;
+}
+return res;
 }
