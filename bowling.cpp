dc3abdafc088493cec80630d2d79e4ddaaefad78
beep boop <githubbot@vadim.me>
2024-07-23 10:25:33 +0200

    Solution 46 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt: bb9e6a97-dd79-4812-9142-91c55b100878.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ bb9e6a97-dd79-4812-9142-91c55b100878.cpp: In function ‘int main()’: bb9e6a97-dd79-4812-9142-91c55b100878.cpp:54:5: error: ‘cout’ was not declared in  this scope    54 |     cout << bowlingScore("XXX/") << endl;       |     ^~~~ bb9e6a97-dd79-4812-9142-91c55b100878.cpp:54:25: error: ‘bowlingScore’ cannot be used as a function    54 |     cout << bowlingScore("XXX/") << endl;       |             ~~~~~~~~~~~~^~~~~~~~ bb9e6a97-dd79-4812-9142-91c55b100878.cpp:54:37: error: ‘endl’ was not declared i n this scope    54 |     cout << bowlingScore("XXX/") << endl;       |                                     ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,12 +1,13 @@
-using namespace std;
+#include <string>
+#include <iostream>
 
 int bowlingScore(string s) {
     int score = 0;
     int currentRoll1 = 0;
     int currentRoll2 = 0;
     bool strike = false;
-    int frameScore = 0;
-    
+    int bonusRoll = 0;
+
     for (char c : s) {
         if (c == '/') {
             if (!strike) {
@@ -16,53 +17,55 @@ int bowlingScore(string s) {
                     score += 10;
                 }
             } else {
-                frameScore = 10;
-                score += frameScore;
+                score += 10;
                 strike = false;
             }
             currentRoll1 = 0;
             currentRoll2 = 0;
         } else if (c == 'X') {
             score += 10;
-            frameScore = 10;
             strike = true;
+            currentRoll1 = 1;
         } else {
             if (!strike) {
                 currentRoll1++;
-                if (currentRoll1 >= 10) {
-                    if (currentRoll1 + currentRoll2 <= 10) {
-                        score += 10 - currentRoll1;
-                        currentRoll2++;
+                if (currentRoll1 == 10) {
+                    if (currentRoll2 + 1 <= 10) {
+                        score += 10 + currentRoll2;
                     } else {
-                        score += 10;
-                        currentRoll1 = 0;
+                        score += 10 + (10 - currentRoll2);
                     }
+                    strike = true;
+                } else if (currentRoll1 + currentRoll2 >= 10) {
+                    if (currentRoll1 == 9 && c != '/') {
+                        frameScore = 10;
+                    } else {
+                        frameScore = 10 - currentRoll1;
+                    }
+                    score += frameScore;
+                    strike = true;
+                } 
+                else {
+                    currentRoll2++;
                 }
             } else {
-                currentRoll2++;
-            }
-        }
-    }
-    
-    if (strike) {
-        int bonusRoll1 = 0, bonusRoll2 = 0;
-        for (char c : s.substr(s.find('/'))) {
-            if (c == 'X') {
-                score += 10 + bonusRoll1;
-                return score;
-            } else if (c == '/') {
-                score += 10 + bonusRoll1;
-                return score;
-            } else {
-                if (bonusRoll1 < 2) {
-                    bonusRoll1++;
+                if (currentRoll2 < 9) {
+                    currentRoll2++;
                 } else {
-                    bonusRoll1 = 0;
-                    bonusRoll2++;
+                    bonusRoll++;
                 }
             }
         }
     }
-    
+
+    if (strike) {
+        score += 10 + bonusRoll;
+    }
+
     return score;
+
+}
+
+int main() {
+    std::cout << bowlingScore("XXX/") << std::endl;
 }
