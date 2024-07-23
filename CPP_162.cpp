5823289d68451fd4ea4fbbebffccc8e0fd59dc79
beep boop <githubbot@vadim.me>
2024-07-23 09:00:58 +0200

    Solution 30 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/70xd2i5s Prompt:     9 | for(int i = 0; i < 16; i++) {       | ^~~ cdec52cb-8f55-43ef-8705-b195a7404851.cpp:9:16: error: ‘i’ does not name a type     9 | for(int i = 0; i < 16; i++) {       |                ^ cdec52cb-8f55-43ef-8705-b195a7404851.cpp:9:24: error: ‘i’ does not name a type     9 | for(int i = 0; i < 16; i++) {       |                        ^ cdec52cb-8f55-43ef-8705-b195a7404851.cpp:13:1: error: expected unqualified-id be fore ‘return’    13 | return string(mdString);       | ^~~~~~ cdec52cb-8f55-43ef-8705-b195a7404851.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from cdec52cb-8f55-43ef-8705-b195a7404851.cpp:16: cdec52cb-8f55-43ef-8705-b195a7404851.cpp: In function ‘int main()’: cdec52cb-8f55-43ef-8705-b195a7404851.cpp:18:13: error: ‘string_to_md5’ was not d eclared in this scope    18 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <cassert>
-#include <cstring>
+#include <string>
 #include <openssl/md5.h>
 
-using namespace std;
-
-string string_to_md5(const string& text){
-    if(text.empty()){
+std::string string_to_md5(const std::string& text) {
+    if(text.empty()) {
         return "None";
     }
 
-    unsigned char result[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)text.c_str(), text.length(), result);
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    MD5((unsigned char*)text.c_str(), text.length(), digest);
 
-    char md5_hash[MD5_DIGEST_LENGTH*2 + 1];
-    for(int i = 0; i < MD5_DIGEST_LENGTH; i++){
-        sprintf(&md5_hash[i*2], "%02x", result[i]);
+    char mdString[33];
+    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
 
-    return md5_hash;
+    return std::string(mdString);
 }
 
 int main() {
