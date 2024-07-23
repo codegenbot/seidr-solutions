241bbf6e4da0f07caba934bc35c7ddcab2dbf298
beep boop <githubbot@vadim.me>
2024-07-23 08:18:02 +0200

    Solution 1 of Python/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yngnl4id Prompt: Complete the following code given the task description and function signature.

--- Python_11.py
+++ Python_11.py
@@ -2,12 +2,4 @@ from typing import List
 
 
 def string_xor(a: str, b: str) -> str:
-    result = ""
-    for i in range(len(a)):
-        if a[i] == "1" and b[i] == "0":
-            result += "1"
-        elif a[i] == "0" and b[i] == "1":
-            result += "1"
-        else:
-            result += "0"
-    return result
+    return "".join(str(int(x) ^ int(y)) for x, y in zip(a, b))
