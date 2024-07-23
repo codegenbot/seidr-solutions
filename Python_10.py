1501cb09e94a566c737680a624d78f06f7940115
beep boop <githubbot@vadim.me>
2024-07-24 00:00:31 +0200

    Solution 37 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/d90l9i2w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb59210e-ad84-491f-a903-2d1b5152c 02d.py", line 10, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb59210e-ad84-491f-a903-2d1b5152c 02d.py", line 7, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,6 +1,7 @@
+```
 def make_palindrome(s: str) -> str:
     if len(s) % 2 == 0:
-        return s[:len(s)//2] + s[:len(s)//2][::-1]
+        return s[:len(s) // 2] + s[:len(s) // 2][::-1]
     else:
-        mid = s[len(s)//2].upper()
-        return s[0:len(s)//2] + s[0+len(s)//2][::-1] + mid
+        middle_char = s[len(s) // 2]
+        return s + middle_char + s[::-1]
