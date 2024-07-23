a182a2e5bf8320771fb67192a2fbe7132a2ea367
beep boop <githubbot@vadim.me>
2024-07-24 01:34:28 +0200

    Solution 15 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1g16sxbf Prompt:       |                              ^~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:23: 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp: In function ‘int main()’: 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:33:20: error: ‘by_length’ was not decla red in this scope    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 98fe0c43-b0e2-4b03-8a7e-3abbafe90e83.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,15 +1,12 @@
+```
 vector<string> by_length(vector<int> arr) {
     vector<string> result;
+    map<int, string> m = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
+                          {4, "Four"}, {5, "Five"}, {6, "Six"}, 
+                          {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
     for (int i : arr) {
-        string s = to_string(i);
-        map<char, string> m = {{'1', "One"}, {'2', "Two"}, {'3', "Three"}, 
-                                {'4', "Four"}, {'5', "Five"}, {'6', "Six"}, 
-                                {'7', "Seven"}, {'8', "Eight"}, {'9', "Nine"}};
-        if (i >= 1 && i <= 9) {
-            for (int j = 0; j < s.size(); j++) {
-                result.push_back(m.at(s[j]));
-            }
-        } 
+        result.push_back(m.at(i));
     }
+    
     return result;
 }
