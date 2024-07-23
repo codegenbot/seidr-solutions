850a3516c6641a83e5ea94a8fc592cfe5aa9e9d3
beep boop <githubbot@vadim.me>
2024-07-24 01:38:38 +0200

    Solution 33 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3tlymlm5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/428bc2a8-352b-46e6-b264-6e7fd86e1 fd2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,2 @@
-def sort_third(l: list):
-    return sorted((int(str(i)[-1]), i) for i in l)
-
-numbers = [1234, 5678, 9012, 1111]
-print(sort_third(numbers))
+def sort_third(lst: list) -> list:
+    return sorted((int(str(i)[2]), i) for i in lst if len(str(i)) > 2)
