7f6587f46d63cb24d1497ce17564da8b70a3eec8
beep boop <githubbot@vadim.me>
2024-07-23 10:24:29 +0200

    Solution 72 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/upufmjtl Prompt:     3 |     cin >> input;       |     ^~~ 387353b7-30a0-4bd8-903b-8d106087ed62.cpp:3:12: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     cin >> input;       |            ^~~~~       |            int 387353b7-30a0-4bd8-903b-8d106087ed62.cpp:6:9: error: ‘cout’ was not declared in this scope     6 |         cout << "True" << endl;       |         ^~~~ 387353b7-30a0-4bd8-903b-8d106087ed62.cpp:6:27: error: ‘endl’ was not declared in  this scope     6 |         cout << "True" << endl;       |                           ^~~~ 387353b7-30a0-4bd8-903b-8d106087ed62.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << "False" << endl;       |         ^~~~ 387353b7-30a0-4bd8-903b-8d106087ed62.cpp:8:28: error: ‘endl’ was not declared in  this scope     8 |         cout << "False" << endl;       |                            ^~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main() {
     string input;
     cin >> input;
-    
+
     if (input == "t") {
         cout << "True" << endl;
     } else {
         cout << "False" << endl;
     }
-    
+
     return 0;
 }
