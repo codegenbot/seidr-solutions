d50692c59ef6deeb7b5d2639efcfcc71927f73ce
beep boop <githubbot@vadim.me>
2024-07-23 16:47:04 +0200

    Solution 24 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/dwqrfb6s Prompt: Program failed

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -2,9 +2,9 @@
 int left = 0;
 for (int right = word.size() - 1; right >= 0; --right) {
     if (!isvowel(word[right])) {
-        left++;
-    } else if (left > 0) { 
+        ++left;
+    } else if (left > 0 && isvowel(word[right-1])) {
         return std::string(1, tolower(word[right]));
     }
 }
-return "";
+return isvowel(word[0]) ? tolower(word[0]) : "";
