5fca320d0f7a1987f42f9bd9e0a9f3854af0e438
beep boop <githubbot@vadim.me>
2024-07-23 09:43:36 +0200

    Solution 39 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33gq36ka Prompt: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 52aee6c4-4ab8-4991-887c-8f5cb80529d4.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 52aee6c4-4ab8-4991-887c-8f5cb80529d4.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 52aee6c4-4ab8-4991-887c-8f5cb80529d4.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 52aee6c4-4ab8-4991-887c-8f5cb80529d4.cpp:24: 52aee6c4-4ab8-4991-887c-8f5cb80529d4.cpp: In function ‘int main()’: 52aee6c4-4ab8-4991-887c-8f5cb80529d4.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -13,13 +13,7 @@ std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std:
 }
 
 bool sameVectors(const std::vector<std::string>& a, const std::vector<std::string>& b){
-    if(a.size()!=b.size())
-        return false;
-    for(size_t i=0;i<a.size();i++){
-        if(a[i]!=b[i])
-            return false;
-    }
-    return true;
+    return a==b;
 
 }
 
