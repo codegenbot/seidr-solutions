36cd6d5e1564f25b69163f6e14777b424be72c5a
beep boop <githubbot@vadim.me>
2024-07-24 00:00:46 +0200

    Solution 41 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9p3ktnil Prompt:       |                              ^~~~~~ af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp:36:37: error: ‘string’ was not declared  in this scope    36 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp:36:45: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp:35: af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp: In function ‘int main()’: af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp:45:20: error: ‘numerical_letter_grade’ was not declared in this scope    45 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ af0021fa-dad7-4aa0-a5f1-ed7272629bdf.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,45 +1,38 @@
-```
-bool isSame(string s1, string s2) {
-    if (s1.size() != s2.size())
+bool issame(float x, float y) {
+    if (abs(x-y)<0.01)
+        return true;
+    else
         return false;
-    for(int i = 0; i < s1.size(); ++i)
-        if(s1[i] != s2[i])
-            return false;
-    return true;
 }
 
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
     for (float grade : grades) {
-        string letter_grade = "";
-        if (grade >= 4.0) {
-            letter_grade = "A+";
-        } else if (grade > 3.7) {
-            letter_grade = "A";
-        } else if (grade > 3.3) {
-            letter_grade = "A-";
-        } else if (grade > 3.0) {
-            letter_grade = "B+";
-        } else if (grade > 2.7) {
-            letter_grade = "B";
-        } else if (grade > 2.3) {
-            letter_grade = "B-";
-        } else if (grade > 2.0) {
-            letter_grade = "C+";
-        } else if (grade > 1.7) {
-            letter_grade = "C";
-        } else if (grade > 1.3) {
-            letter_grade = "C-";
-        } else if (grade > 1.0) {
-            letter_grade = "D+";
-        } else if (grade > 0.7) {
-            letter_grade = "D";
-        } else if (grade > 0.0) {
-            letter_grade = "D-";
-        } else {
-            letter_grade = "E";
+        if (issame(grade, 4.0)) {
+            letter_grades.push_back("A+");
+        } else if (issame(grade, 3.7)) {
+            letter_grades.push_back("A");
+        } else if (issame(grade, 3.3)) {
+            letter_grades.push_back("A-");
+        } else if (issame(grade, 3.0)) {
+            letter_grades.push_back("B+");
+        } else if (issame(grade, 2.7)) {
+            letter_grades.push_back("B");
+        } else if (issame(grade, 2.3)) {
+            letter_grades.push_back("B-");
+        } else if (issame(grade, 2.0)) {
+            letter_grades.push_back("C+");
+        } else if (issame(grade, 1.7)) {
+            letter_grades.push_back("C");
+        } else if (issame(grade, 1.3)) {
+            letter_grades.push_back("C-");
+        } else if (issame(grade, 1.0)) {
+            letter_grades.push_back("D+");
+        } else if (issame(grade, 0.7)) {
+            letter_grades.push_back("D");
+        } else if (issame(grade, 0.0)) {
+            letter_grades.push_back("E");
         }
-        letter_grades.push_back(letter_grade);
     }
     return letter_grades;
 }
