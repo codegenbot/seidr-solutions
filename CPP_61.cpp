8df790f387c06b666dccb5e03540610f276e5f7c
beep boop <githubbot@vadim.me>
2024-07-23 15:32:05 +0200

    Solution 91 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9iauoaap Prompt: Program failed

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,17 +1,8 @@
-```c
-#include <cassert>
-#include <string>
-
 bool correct_bracketing(const std::string& brackets) {
     int count = 0;
     for(int i=0; i<brackets.length(); i++){
         if(brackets[i] == '(')count++;
-        else if (brackets[i] == ')') count--;
+        else if(brackets[i] == ')') count--;
     }
     return count==0;
-}
-
-int main_test() {
-    assert(!correct_bracketing("()()(()())"));
-    return 0;
 }
