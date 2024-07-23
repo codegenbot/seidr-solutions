6c7222319730c8afd8045c254584facf4506c76e
beep boop <githubbot@vadim.me>
2024-07-23 09:59:30 +0200

    Solution 32 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/s9lxoq63 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0321188-db05-4a1f-a568-614956f65 b40.py", line 16, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d0321188-db05-4a1f-a568-614956f65 b40.py", line 13, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,11 +1,10 @@
-```
 def fizz_buzz(n: int):
     count = 0
     for i in range(1, n + 1):  
         if i % 3 == 0 and i % 5 == 0:
             count += 1
         elif i % 3 == 0:
-            count += (str(i).count('3') > str(i).count('6'))
+            count += (str(i).count('3') - str(i).count('6') > 0)
         elif i % 5 == 0:
-            count += (str(i).count('5') > str(i).count('50'))
+            count += (str(i).count('5') - str(i).count('50') > 0)
     return count
