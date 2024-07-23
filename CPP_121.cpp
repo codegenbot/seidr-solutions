ec5c3889c23a606690573300a12aa7ed6ef53d36
beep boop <githubbot@vadim.me>
2024-07-23 17:14:00 +0200

    Solution 41 of CPP/121, ratio 0.7142857142857143 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cwsaqt2y Prompt: Program failed

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 #include <vector>
 
 using namespace std;
@@ -6,15 +7,15 @@ using namespace std;
 int solutions(vector<int> lst) {
     int sum = 0;
     for (int i = 1; i < lst.size(); i++) {
-        if (lst[i-1]%2 == 0 && lst[i]%2 != 0) {
+        if ((i % 2 != 0 && lst[i] % 2 == 0) || (i % 2 == 0 && lst[i] % 2 != 0)) {
             sum += lst[i];
         }
     }
     return sum;
 }
 
-int main() {
-    cout << solutions({3, 13, 2, 9}) << endl;
+int calculateSolutions(vector<int> lst) {
+    assert(solutions({3, 13, 2, 9}) == 3);
     cout << solutions({1, 4, 5, 10}) << endl;
     return 0;
 }
