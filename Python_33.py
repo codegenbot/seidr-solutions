fff1d5a2d044d2de4a95d752f4d66d61553f190c
beep boop <githubbot@vadim.me>
2024-07-23 20:40:00 +0200

    Solution 70 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5lqk7n16 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/148236ff-0c07-4084-9108-455d6ceaa 8a4.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/148236ff-0c07-4084-9108-455d6ceaa 8a4.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,5 @@
 def sort_third(l: list):
     odd = sorted([x for i, x in enumerate(l) if i % 2 != 0])
     even = [x for i, x in enumerate(l) if i % 2 == 0]
-    return [
-        x if i % 2 != 0 else odd[0] if j < len(odd) else even[j // 2]
-        for i, x in enumerate(l)
-        for j in range(len(l))
-    ]
+    return [x if i % 2 != 0 else odd[0] if j < len(odd) else even[j//2] 
+            for i, x in enumerate(l) for j in range(len(l))]
