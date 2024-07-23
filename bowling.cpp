b4225ece048a536cd91a5d4b856203fbc7cb550a
beep boop <githubbot@vadim.me>
2024-07-24 01:59:55 +0200

    Solution 70 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/rl9rebse Prompt: eb9e8988-775d-4d88-9210-b8ef3097712e.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string str) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,28 +1,18 @@
-```
-using namespace std;
+#include <string>
 
-int bowlingScore(string s) {
+int bowlingScore(string str) {
     int score = 0;
-    int currentFrame = 1;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '/') {
-            string firstRollStr = s.substr(0, i);
-            string secondRollStr = s.substr(i + 1);
-            int firstRoll = stoi(firstRollStr);
-            int secondRoll = stoi(secondRollStr);
-            score += getPointsForFrame(firstRoll, secondRoll);
-            currentFrame++;
+    int currentRoll = 0;
+    for (char c : str) {
+        if (c == 'X') {
+            score += 30;
+            currentRoll = 0;
+        } else if (c == '/') {
+            score += currentRoll + 10;
+            currentRoll = 0;
+        } else if (isdigit(c)) {
+            currentRoll *= 10;
+            currentRoll += c - '0';
         }
     }
     return score;
-}
-
-int getPointsForFrame(int first, int second) {
-    if (first == 10) {
-        return 10 + second;
-    } else if (first + second >= 10) {
-        return 10 + (second - 10);
-    } else {
-        return first + second;
-    }
-}
