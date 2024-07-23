2106087c867802824c5ba8dbdbfe91248c31218b
beep boop <githubbot@vadim.me>
2024-07-23 07:20:39 +0200

    Solution 87 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,53 +1,38 @@
 #include <boost/any.hpp>
-#include <string>
-
-using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
-    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if ((int)a > (int)b) {
-            return a;
-        } else if ((int)a < (int)b) {
-            return b;
-        } else {
-            return boost::any("None");
-        }
-    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        if ((double)a > (double)b) {
-            return a;
-        } else if ((double)a < (double)b) {
-            return b;
-        } else {
-            return boost::any("None");
-        }
-    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        try {
-            double da = stod(boost::any_cast<string>(a).erase(0, 1));
-            double db = (double)b;
-            if (da > db) {
-                return a;
-            } else if (da < db) {
-                return b;
-            } else {
-                return boost::any("None");
-            }
-        } catch (...) {
-            return b;
-        }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        size_t pos = boost::any_cast<string>(a).find(',');
-        string sa = boost::any_cast<string>(a).substr(0, pos);
-        size_t pos2 = boost::any_cast<string>(b).find(',');
-        string sb = boost::any_cast<string>(b).substr(0, pos2);
-        if (stod(sa) > stod(sb)) {
-            return a;
-        } else if (stod(sa) < stod(sb)) {
-            return b;
-        } else {
-            return boost::any("None");
-        }
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return (std::get<int>(a) > std::get<int>(b)) ? a : ((std::get<int>(a) < std::get<int>(b)) ? b : boost::any("None"));
     }
-    return b;
+    if (a.type() == typeid(double) && b.type() == typeid(double)) {
+        return (std::get<double>(a) > std::get<double>(b)) ? a : ((std::get<double>(a) < std::get<double>(b)) ? b : boost::any("None"));
+    }
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (std::get<int>(a) > std::get<double>(b)) ? a : ((std::get<int>(a) < std::get<double>(b)) ? b : boost::any("None"));
+    }
+    if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (std::get<double>(a) > std::get<int>(b)) ? a : ((std::get<double>(a) < std::get<int>(b)) ? b : boost::any("None"));
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        double da = stod(std::any_cast<string>(a).c_str());
+        double db = stod(std::any_cast<string>(b).c_str());
+        return (da > db) ? a : ((da < db) ? b : boost::any("None"));
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        double da = stod(std::any_cast<string>(a).c_str());
+        return (da > static_cast<double>(std::get<int>(b))) ? a : ((da < static_cast<double>(std::get<int>(b))) ? boost::any(b) : boost::any("None"));
+    }
+    if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        double da = stod(std::any_cast<string>(a).c_str());
+        return (da > std::get<double>(b)) ? a : ((da < std::get<double>(b)) ? boost::any(b) : boost::any("None"));
+    }
+    if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        double da = stod(std::any_cast<string>(b).c_str());
+        return (std::get<int>(a) > da) ? a : ((std::get<int>(a) < da) ? boost::any(b) : boost::any("None"));
+    }
+    if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        double da = stod(std::any_cast<string>(b).c_str());
+        return (std::get<double>(a) > da) ? a : ((std::get<double>(a) < da) ? boost::any(b) : boost::any("None"));
+    }
+    return "None";
 }
