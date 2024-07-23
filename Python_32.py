87d57824057250f67f1bdeee381f036cb75a7bd9
beep boop <githubbot@vadim.me>
2024-07-24 01:31:48 +0200

    Solution 26 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k5eqj4r7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b7d38d20-de14-4b6a-9d2d-c9077983c 80f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -14,7 +14,7 @@ def find_zero():
     discriminant = b**2 - 4*a*c
 
     if discriminant < 0:
-        return "The program did not receive expected input (no real solutions)."
+        return f"The program did not receive expected input (no real solutions)."
     else:
         x1 = (b + math.sqrt(discriminant)) / (2 * a)
         x2 = (b - math.sqrt(discriminant)) / (2 * a)
