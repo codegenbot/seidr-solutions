6d38c9a224f54a1a0fe38f62b7b7aaf2eb983276
beep boop <githubbot@vadim.me>
2024-07-23 10:05:52 +0200

    Solution 72 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/sw8yxj3m Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from bab83a78-3cd6-4f21-86a6-f165d4a365fc.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,27 +1,23 @@
-```
-#include <vector>
+#include <initializer_list>
 #include <cmath>
 #include <iostream>
 #include <iomanip>
-#include <initializer_list>
 
 using namespace std;
 
-double poly(vector<double> xs, double x){
+double poly(initializer_list<double> xs, double x){
     double sum=0;
-    int i;
-    for (i=0;i<xs.size();i++)
-    {
-        sum+=xs[i]*pow(x,i);
+    for (auto i : xs) {
+        sum+=i*pow(x,sizeof(i));
     }
     return sum;
 }
 
-double find_zero(vector<double> xs){
+double find_zero(initializer_list<double> xs){
     if(xs.size() % 2 != 0) return -1; 
     double x = 1.0; 
     while(abs(poly(xs, x)) > 1e-9){ 
-        x -= poly(xs, x)/poly({xs[1], xs.back()}, x); 
+        x -= poly({xs.begin()[1], xs.end()[0]}, x); 
     }
     ostringstream oss;
     oss << fixed << setprecision(6) << x;
@@ -29,7 +25,7 @@ double find_zero(vector<double> xs){
 }
 
 int main() {
-    vector<double> xs = {1, -6, 11, -6};
+    initializer_list<double> xs = {1, -6, 11, -6};
     double zero = find_zero(xs);
     cout << "Zero is: " << zero << endl; 
     return 0;
