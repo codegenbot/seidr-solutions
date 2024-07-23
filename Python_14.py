901b1c382133919555abcd045fac1dc5b3ddc27f
beep boop <githubbot@vadim.me>
2024-07-24 00:57:02 +0200

    Solution 37 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lqjbjsij Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c4e75f8e-2399-48b5-b1ee-01fcfedf0 218.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_14.py
+++ Python_14.py
@@ -1,13 +1,9 @@
-```
 from typing import List
 
 
 def all_prefixes(string: str) -> List[str]:
-    return ["" + string[:i].lower() for i in range(1, len(string) + 1)]
+    return [""] + [string[:i].lower() for i in range(1, len(string) + 1)]
 
 
-string1 = input("Enter a string (lowercase): ")
-print(all_prefixes(string1.lower()))
-
-string2 = input("Enter a string (uppercase): ")
-print(all_prefixes(string2.upper()))
+print(all_prefixes(input("Enter a string (lowercase): ").lower()))
+print(all_prefixes(input("Enter a string (uppercase): ").upper()))
