014181a42155b52dc07525451486b0275b38cd45
beep boop <githubbot@vadim.me>
2024-07-23 10:06:13 +0200

    Solution 2 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/fi5yu695 Prompt: d in this scope    27 |     for (int num : subvec1) {       |                    ^~~~~~~ 2e377f23-8a16-41f6-87bf-74ba400ae45c.cpp:28:9: error: ‘cout’ was not declared in  this scope    28 |         cout << num << endl;       |         ^~~~ 2e377f23-8a16-41f6-87bf-74ba400ae45c.cpp:28:24: error: ‘endl’ was not declared i n this scope    28 |         cout << num << endl;       |                        ^~~~ 2e377f23-8a16-41f6-87bf-74ba400ae45c.cpp:30:20: error: ‘subvec2’ was not declare d in this scope    30 |     for (int num : subvec2) {       |                    ^~~~~~~ 2e377f23-8a16-41f6-87bf-74ba400ae45c.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << num << endl;       |         ^~~~ 2e377f23-8a16-41f6-87bf-74ba400ae45c.cpp:31:24: error: ‘endl’ was not declared i n this scope    31 |         cout << num << endl;       |                        ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,28 +1,37 @@
+#include <iostream>
+#include <vector>
+
 int main() {
+    std::vector<int> nums;
     int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
+    while (std::cin >> n) {
+        nums.push_back(n);
     }
     
-    int min_diff = INT_MAX;
-    int cut_index = -1;
+    int sum = 0;
+    for (int num : nums) {
+        sum += num;
+    }
     
-    for (int i = 1; i < n; ++i) {
-        int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - accumulate(nums.begin() + i, nums.end(), 0));
-        if (diff < min_diff) {
-            min_diff = diff;
-            cut_index = i;
+    int target = sum / 2;
+    int prefixSum = 0;
+    int idx = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        prefixSum += nums[i];
+        if (prefixSum >= target) {
+            idx = i;
+            break;
         }
     }
     
-    for (int i = 0; i < cut_index; ++i) {
-        cout << nums[i] << endl;
+    std::vector<int> subvec1(nums.begin(), nums.begin() + idx + 1);
+    std::vector<int> subvec2(nums.begin() + idx + 1, nums.end());
+    
+    for (int num : subvec1) {
+        std::cout << num << std::endl;
     }
-    cout << endl;
-    for (int i = cut_index; i < n; ++i) {
-        cout << nums[i] << endl;
+    for (int num : subvec2) {
+        std::cout << num << std::endl;
     }
     
     return 0;
