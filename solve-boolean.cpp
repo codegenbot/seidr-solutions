30c1466a1a2fa9558b65e25e297243f0f6b69657
beep boop <githubbot@vadim.me>
2024-07-23 11:11:07 +0200

    Solution 10 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/rmug2bur Prompt:    37 |             if (!operators.empty() && ((expression[i] == 't' && operator s.top() == '|') ||       |                  ^~~~~~~~~ 20debf16-fa4a-4d6f-ad68-24b066fbcc69.cpp:43:13: error: ‘operators’ was not decla red in this scope    43 |             operators.push(expression[i]);       |             ^~~~~~~~~ 20debf16-fa4a-4d6f-ad68-24b066fbcc69.cpp:45:20: error: ‘operators’ was not decla red in this scope    45 |             while (operators.top() != '(') {       |                    ^~~~~~~~~ 20debf16-fa4a-4d6f-ad68-24b066fbcc69.cpp:62:13: error: ‘operators’ was not decla red in this scope    62 |             operators.pop();       |             ^~~~~~~~~ 20debf16-fa4a-4d6f-ad68-24b066fbcc69.cpp:66:13: error: ‘operators’ was not decla red in this scope    66 |     while (!operators.empty()) {       |             ^~~~~~~~~ 20debf16-fa4a-4d6f-ad68-24b066fbcc69.cpp:67:24: error: ‘i’ was not declared in t his scope    67 |         if (expression[i] == '|') {       |                        ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,34 +1,64 @@
-```cpp
+#include <iostream>
 #include <stack>
 using namespace std;
 
-bool solveBoolean(string s) {
-    stack<char> st;
-    bool res = false;
-    
-    for(int i=s.length()-1; i>=0; i--) {
-        if(s[i] == '&') {
-            while(!st.empty() && st.top() == '&') {
-                st.pop();
+bool evaluateBooleanExpression(string expression) {
+    stack<char> operators;
+    string operand = "";
+    int i = 0; 
+
+    for (i = 0; i < expression.length(); i++) {
+        if (expression[i] == '&') {
+            while (!operators.empty() && operators.top() == '|') {
+                operators.pop();
             }
-            if(st.empty()) {
-                res = true;
-            } else {
-                res = false;
+            if (!operand.empty()) {
+                return operand == "True";
             }
-        } else if(s[i] == '|') {
-            while(!st.empty() && st.top() == '|') {
-                st.pop();
+        } else if (expression[i] == '|') {
+            while (!operators.empty()) operators.pop();
+            if (!operand.empty()) {
+                return operand == "True";
             }
-            if(st.empty()) {
-                res = false;
-            } else {
-                res = true;
+        } else if (expression[i] == 't' || expression[i] == 'f') {
+            if (!operators.empty() && ((expression[i] == 't' && operators.top() == '|') ||
+                                        (expression[i] == 'f' && operators.top() == '&'))) {
+                while (!operators.empty()) operators.pop();
             }
-        } else {
-            st.push(s[i]);
+            operand += (expression[i] == 't'? "T" : "F");
+        } else if (expression[i] == '(') {
+            operators.push(expression[i]);
+        } else if (expression[i] == ')') {
+            while (operators.top() != '(') {
+                if (expression[i] == '|') {
+                    while (!operators.empty() && operators.top() == '|') {
+                        operators.pop();
+                    }
+                    return operand == "T";
+                } else if (expression[i] == '&') {
+                    while (!operators.empty()) operators.pop();
+                    return operand == "T";
+                }
+
+                operators.pop();
+            }
+            operators.pop();
         }
     }
-    
-    return res;
+
+    while (!operators.empty()) {
+        if (expression[i] == '|') {
+            while (!operators.empty() && operators.top() == '|') {
+                operators.pop();
+            }
+            return operand == "T";
+        } else if (expression[i] == '&') {
+            while (!operators.empty()) operators.pop();
+            return operand == "T";
+        }
+
+        operators.pop();
+    }
+
+    return operand == "True";
 }
