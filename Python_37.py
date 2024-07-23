91f40135794b24d708077153685332b46123309b
beep boop <githubbot@vadim.me>
2024-07-23 09:27:14 +0200

    Solution 4 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5fq9li0d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/183baf78-d493-4f3b-b18d-f4560cbe4 8d6.py", line 14, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/183baf78-d493-4f3b-b18d-f4560cbe4 8d6.py", line 9, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/183baf78-d493-4f3b-b18d-f4560cbe4 8d6.py", line 5, in sort_even     range(len(l)), sorted((l[i], l[i + 1]) if i < len(l) - 1 else (l[i],))                                               ^ NameError: name 'i' is not defined. Did you mean: 'id'?

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,8 @@
 def sort_even(l: list):
     return [
-        x if i % 2 != 0 else sorted((l[i], l[(i + 1) % len(l)]))[0]
-        for i, (x,) in enumerate(l)
-    ]
+        x if i % 2 != 0 else y
+        for (i, (x, y)) in zip(
+            range(len(l)), 
+            [(l[i], l[i + 1]) if i < len(l) - 1 else (l[i],) for i in range(len(l))]
+        )
+    ][::2] + l[1::2]
