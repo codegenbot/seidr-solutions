6d17639e122f4a820d57101f6431de83f5e73947
beep boop <githubbot@vadim.me>
2024-07-23 15:05:12 +0200

    Solution 68 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wxae0lqy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f8e24e9-657f-4423-9b14-48d3c33eb f3d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,14 +1,13 @@
-```
-def parse_music(music_string: str) -> list:
-    beats = [4, 2, 1]
+from typing import List
+
+def parse_music(music_string: str) -> List[int]:
+    beats = [4, 2, 1]  
     result = []
 
     for note in music_string.split():
         if note == 'o':
             result.append(beats[0])
         elif note == 'o|':
-            result.extend([beats[1]]*2)
+            result.append(beats[1])
         elif note == '.|':
-            result.extend([beats[2]]*3)
-
-    return result
+            result.append(beats[2])
