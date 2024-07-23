bc4c0620be405fd0c9a4b81e61c47299f618d3a1
beep boop <githubbot@vadim.me>
2024-07-23 23:03:57 +0200

    Solution 60 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/to14fhkk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80e005dc-4d98-4e46-8e00-f24b55208 5a8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,3 +1,10 @@
 ```
-def solution(lst):
+def solution():
+    n = int(input("Enter number of elements: "))
+    lst = []
+    
+    for i in range(n):
+        ele = int(input(f"Enter element {i+1}: "))
+        lst.append(ele)
+        
     return sum(i for i in lst[1::2] if i % 2 != 0)
