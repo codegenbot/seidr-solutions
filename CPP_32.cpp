3d263c4e2b7bcfdb542835a6eb95a4bf53677643
beep boop <githubbot@vadim.me>
2024-07-23 21:36:18 +0200

    Solution 1 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3hjsd5tf Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,9 +1,4 @@
 double find_zero(vector<double> xs){
-    double a = xs[0];
-    double b = 0;
-    for(int i=1; i<xs.size();i++){
-        if(i%2==0) b+=xs[i]*pow(xs.back(),(i-1)/2);
-        else a+=(-1)*xs[i]/(double)(i)*(pow(xs.back(),(i-1)/2));
-    }
-    return -b/(2*a);
+    double x = -xs[1] / (2*xs[0]);
+    return x;
 }
