914e65f82121b520554c4c11f2aa1639f95f02b1
beep boop <githubbot@vadim.me>
2024-07-23 14:21:31 +0200

    Solution 78 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kwqqotc6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fa6cc9c-37bc-4695-8ee5-7eafe1f1a 611.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,11 @@
+```
+import re
+
 def sum_squares(user_input):
-    numbers = [int(i) ** 2 for i in re.findall(r'\d+', str(user_input))]
+    if not isinstance(user_input, str) and not isinstance(user_input, tuple) and not hasattr(user_input, "__iter__"):
+        return
+    if isinstance(user_input, list):
+        numbers = [n ** 2 for n in map(int, filter(lambda x: re.match(r'\d+', str(x)), user_input))]
+    else:
+        numbers = list(map(int, filter(lambda x: re.match(r'\d+', str(x)), user_input.split())))
     return sum(numbers)
