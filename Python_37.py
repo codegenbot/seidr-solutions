9a9ebd7e4707836121b754ff09eaa5f5b22b3a35
beep boop <githubbot@vadim.me>
2024-07-23 10:20:26 +0200

    Solution 70 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5fq9li0d Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1bc336a-fa24-4f97-8391-a323eee75 fae.py", line 2, in <module>     result = check(your_list)              ^^^^^ NameError: name 'check' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,12 +1,5 @@
-```
 def check(lst):
-    if len(set(lst)) == 1:
-        return "The list contains all the same numbers"
-    elif len(lst) == len(set(lst)):
-        return "The list is a set of unique numbers"
-    else:
-        return "The list contains duplicate numbers"
-
+    return lst[-1] if len(lst) > 0 else None
 your_list = [1, 2, 3, 4]
 result = check(your_list)
 print(result)
