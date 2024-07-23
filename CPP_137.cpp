6256d9932a7cc80b8c2ff1c583ac2a1f7bb60215
beep boop <githubbot@vadim.me>
2024-07-23 17:34:05 +0200

    Solution 9 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/arjvjjnh Prompt: 7ef1681c-f107-41e9-9c22-73e4b98430bb.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,13 +1,11 @@
 #include <boost/any.hpp>
 #include <string>
 #include <typeinfo>
-#include <iostream>
-#include <vector>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
-using boost::any;
 
-any compare_one(any a, any b) {
+boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
     } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
@@ -16,17 +14,17 @@ any compare_one(any a, any b) {
         string str1 = any_cast<string>(a);
         string str2 = any_cast<string>(b);
 
-        int num1 = stoi(str1.substr(0, str1.find(',')));
-        double num2 = stod(str2.substr(0, str2.find(',')));
+        int num1 = boost::lexical_cast<int>(str1.substr(0, str1.find(',')));
+        double num2 = boost::lexical_cast<double>(str2.substr(0, str2.find(',')));
 
         if (num1 > num2) {
             return a;
         } else if (num1 < num2) {
             return b;
         } else {
-            return "None";
+            return any(boost::any_cast<string>("None"));
         }
     } else {
-        return "None";
+        return any(boost::any_cast<string>("None"));
     }
 }
