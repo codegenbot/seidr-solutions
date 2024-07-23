76a8fb594e8e040e22276f19c91f64109087393b
beep boop <githubbot@vadim.me>
2024-07-23 11:56:56 +0200

    Solution 46 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nh3kmq1p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7221dd1b-9cae-461c-a590-717c3fb46 bf7.py", line 4     n -= sum(int(d) for d in ''.join(filter(str.isdigit, s))) + sum(int(d) for d  in ''.join(filter(str.isdigit, [c for c in s if c != ' ' and c not in 'oa']))))                                                                                ^ SyntaxError: unmatched ')'

--- Python_67.py
+++ Python_67.py
@@ -1,3 +1,5 @@
+```python
 s = input("Enter a sentence: ")
 n = int(input("Enter a number: "))
 n -= sum(int(d) for d in ''.join(filter(str.isdigit, s))) + sum(int(d) for d in ''.join(filter(str.isdigit, [c for c in s if c != ' ' and c not in 'oa'])))
+print(n)
