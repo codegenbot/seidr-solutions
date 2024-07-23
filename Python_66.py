cc180af4339c92647677170e562e0f1ad238f1de
beep boop <githubbot@vadim.me>
2024-07-23 16:20:30 +0200

    Solution 33 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xc29kah6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/75d3095a-78f7-4c80-9b7c-084f6d5ad 7af.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,2 +1,7 @@
-def sumOfDigits(n):
-    return sum(int(digit) for digit in str(n))
+def calculate_sum_of_digits(n):
+    sum = 0
+    while n:
+        digit = n % 10
+        sum += digit
+        n //= 10
+    return sum
