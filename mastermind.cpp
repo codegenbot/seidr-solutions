69829aa943c4934a6c26d50e05f53b95447e809d
beep boop <githubbot@vadim.me>
2024-07-23 21:13:48 +0200

    Solution 49 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/dbfw81d6 Prompt: it must return 2 0 for input YGWB OBGR, but it returns 1 0.

--- mastermind.cpp
+++ mastermind.cpp
@@ -7,8 +7,8 @@ int main() {
     string code, guess;
     cin >> code >> guess;
 
-    int codeFreq[26] = {0};
-    int guessFreq[26] = {0};
+    int codeFreq[6] = {0};
+    int guessFreq[6] = {0};
 
     int whitePegs = 0, blackPegs = 0;
 
@@ -20,15 +20,14 @@ int main() {
         }
     }
 
-    whitePegs -= blackPegs;
-    if (whitePegs < 0) whitePegs = 0;
-
-    for (char c : code) ++codeFreq[c - 'A'];
-    for (char c : guess) ++guessFreq[c - 'A];
-
-    for (int i = 0; i < 4; ++i) whitePegs += min(codeFreq[code[i] - 'A'], guessFreq[guess[i] - 'A'])
-                                          - (codeFreq[code[i] - 'A'] + guessFreq[guess[i] - 'A'] >= 2 ? 
-                                             min(codeFreq[code[i] - 'A'], guessFreq[guess[i] - 'A']) - 1 : 0);
+    whitePegs = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] != guess[i] && codeFreq[code[i] - 'A'] > 0 && guessFreq[guess[i] - 'A'] > 0) {
+            ++whitePegs;
+            --codeFreq[code[i] - 'A'];
+            --guessFreq[guess[i] - 'A'];
+        }
+    }
 
     cout << whitePegs << " " << blackPegs << endl;
 
