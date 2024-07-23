eab4a1bfc8534bafc8b575d61dcc8984828aae56
beep boop <githubbot@vadim.me>
2024-07-24 00:55:35 +0200

    Solution 18 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/i6ntyhvg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17c09535-c66a-4126-a92e-3194bd039 5c7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -5,7 +5,13 @@ def filter_integers() -> list[int]:
     while True:
         try:
             values = input("Enter a list of values (separated by commas): ")
-            num_list = [int(x) for x in values.split(",") if x.strip() and x.strip().replace(",", "").isdigit()]
+            num_list = [
+                int(x)
+                for x in values.split(",")
+                if x.strip().replace(",", "").isdigit()
+            ]
+            if not all(isinstance(i, int) for i in num_list):
+                raise ValueError
             return num_list
         except ValueError:
             print("Invalid input. Please enter numbers separated by commas.")
