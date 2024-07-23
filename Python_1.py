96a17ef7adb4617f5c77151fd299890716f2b50a
beep boop <githubbot@vadim.me>
2024-07-24 00:31:56 +0200

    Solution 58 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ooca07p6 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -2,37 +2,33 @@
 from typing import List
 
 def separate_paren_groups(paren_string: str) -> List[str]:
-    while True:
+    paren_string = ''
+    while not paren_string:
         paren_string = input("Enter a string of parentheses (separated by spaces): ")
         if all(c in ['(', ')'] for c in paren_string):
             break
         print("Invalid input. Try again.")
-
-    paren_string = ' '.join(paren_string.split())
     
-    while len(paren_string) > 0:
-        temp = ''
-        for char in paren_string:
-            if char == ' ':
+    paren_string = ' '.join(paren_string.split())
+    result = []
+    temp = ''
+    
+    for char in paren_string:
+        if char == ' ':
+            if temp:
                 result.append(temp)
                 temp = ''
-            elif char in ['(', ')']:
-                if char == '(':
-                    temp += char
-                else:
-                    while temp and (temp[-1] != '('):
-                        result.append(temp[:-1])
-                        temp = temp[:-1].rstrip()
-                    if temp and temp[-1] == '(':
-                        temp += char
-                    elif not temp:
-                        temp += char
-            else:
+        elif char in ['(', ')']:
+            if char == '(':
                 temp += char
-        paren_string = paren_string.lstrip().rstrip()
-        while len(paren_string) > 0 and paren_string[0] in ['(', ')']:
-            result.append(paren_string[0])
-            paren_string = paren_string[1:].lstrip().rstrip()
+            else:
+                while temp and (temp[-1] != '('):
+                    result.append(temp[:-1])
+                    temp = temp[:-1].rstrip()
+                if temp and temp[-1] == '(':
+                    temp += char
+                elif not temp:
+                    temp += char
 
     if temp:
         result.append(temp)
