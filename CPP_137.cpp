b42e10e1e71d25ea6a6d7d20ebaf385476113888
beep boop <githubbot@vadim.me>
2024-07-23 07:00:38 +0200

    Solution 6 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -6,64 +6,65 @@ using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
+        return max(a.convert_to<int>(), b.convert_to<float>());
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return (string)b;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string strA = boost::any_cast<string>(a);
-        float fB = boost::any_cast<float>(b);
-        if (strA == "0,") {
-            return a;
-        }
-        else if (fB > stof(strA)) {
-            return b;
-        }
-        else {
-            return "None";
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string strA = boost::any_cast<string>(a);
-        int iB = boost::any_cast<int>(b);
-        if (strA == "0,") {
-            return a;
-        }
-        else if (stof(strA) > iB) {
-            return a;
-        }
-        else {
-            return "None";
-        }
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string strA = boost::any_cast<string>(a);
-        float fB = boost::any_cast<float>(b);
-        if (strA == "0,") {
-            return a;
-        }
-        else if (stof(strA) > fB) {
-            return a;
-        }
-        else {
-            return "None";
-        }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max(a.convert_to<float>(), b.convert_to<int>());
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = boost::any_cast<string>(a);
-        string strB = boost::any_cast<string>(b);
-        if (strA == "0," && strB != "0,") {
-            return b;
+        string str1 = a.convert_to<string>();
+        string str2 = b.convert_to<string>();
+
+        int num1 = 0;
+        int num2 = 0;
+
+        bool dotFound = false;
+        for (char c : str1) {
+            if (!dotFound && !isdigit(c)) {
+                continue;
+            }
+            else if (!dotFound) {
+                dotFound = true;
+                continue;
+            }
+            num1 = num1 * 10 + (c - '0');
         }
-        else if (strA != "0," && stof(strA) > stof(strB)) {
-            return a;
+
+        dotFound = false;
+        for (char c : str2) {
+            if (!dotFound && !isdigit(c)) {
+                continue;
+            }
+            else if (!dotFound) {
+                dotFound = true;
+                continue;
+            }
+            num2 = num2 * 10 + (c - '0');
         }
-        else {
-            return "None";
-        }
+
+        return (num1 > num2) ? a : ((num1 < num2) ? b : boost::any("None"));
     }
-    else {
-        return "None";
+    else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(float))) {
+        string str = a.convert_to<string>();
+
+        int num = 0;
+        bool dotFound = false;
+        for (char c : str) {
+            if (!dotFound && !isdigit(c)) {
+                continue;
+            }
+            else if (!dotFound) {
+                dotFound = true;
+                continue;
+            }
+            num = num * 10 + (c - '0');
+        }
+
+        return (boost::any(b).convert_to<int>() > num) ? b : ((boost::any(b).convert_to<int>() < num) ? a : boost::any("None"));
     }
+    else if ((a.type() == typeid(int) || a.type() == typeid(float)) && b.type() == typeid(string)) {
+        return (compare_one(b, a)).type() == typeid(string) ? b : a;
+    }
+
+    return boost::any("None");
 }
