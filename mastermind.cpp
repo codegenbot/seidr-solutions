ab96a0cab29e29ace9a01c0a5ea16cb66a6ebe1a
beep boop <githubbot@vadim.me>
2024-07-23 09:37:11 +0200

    Solution 1 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/texleggy Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,22 +1,40 @@
-int mastermind(string code, string guess) {
-    int white = 0;
-    int black = 0;
-
-    for(int i = 0; i < 4; i++) {
+int whitePegs(string code, string guess) {
+    int count = 0;
+    for(int i=0; i<4; i++) {
         if(code[i] == guess[i]) {
-            black++;
+            count++;
         }
     }
+    return 4 - count;
+}
 
-    for(int i = 0; i < 6; i++) {
-        int count = 0;
-        for(int j = 0; j < 4; j++) {
-            if(guess[j] == (char)(i + 65) && code.find((char)(i + 65)) != string::npos) {
-                count++;
-            }
+int blackPegs(string code, string guess) {
+    int count = 0;
+    vector<char> codeVector(code.begin(), code.end());
+    for(int i=0; i<4; i++) {
+        if(countOf(codeVector, guess[i]) > 0) {
+            count++;
+            replaceElement(codeVector, guess[i]);
         }
-        white += count - black;
     }
+    return count;
+}
 
-    return {black, white};
+int countOf(vector<char> &vec, char ch) {
+    int count = 0;
+    for(int i=0; i<vec.size(); i++) {
+        if(vec[i] == ch) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void replaceElement(vector<char> &vec, char ch) {
+    for(int i=0; i<vec.size(); i++) {
+        if(vec[i] == ch) {
+            vec[i] = 'X';
+            break;
+        }
+    }
 }
