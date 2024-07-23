c274f1c83327e1bbe367fe953520a0dbc00028b4
beep boop <githubbot@vadim.me>
2024-07-24 01:54:13 +0200

    Solution 34 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/044d1264 Prompt: cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:3:21: error: ‘string’ was not declared in this scope     3 | int closest_integer(string value) {       |                     ^~~~~~ cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:3:21: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:9: cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp: In function ‘int main()’: cd61759d-6803-47fe-b9cc-49c0344d0a6e.cpp:11:28: error: ‘closest_integer’ cannot be used as a function    11 |     assert (closest_integer("0") == 0);       |             ~~~~~~~~~~~~~~~^~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 int closest_integer(std::string value) {
-    double num = stod(value);
-    int integer = (num >= 0) ? static_cast<int>(ceil(num)) : static_cast<int>(floor(num));
+    double num = std::stod(value);
+    int integer = (num >= 0) ? static_cast<int>(std::ceil(num)) : static_cast<int>(std::floor(num));
     return integer;
 }
