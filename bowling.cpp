eb2ed327ba4e4dfe6499e5330fde7849a9d26e43
beep boop <githubbot@vadim.me>
2024-07-23 11:17:03 +0200

    Solution 95 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt: /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 07d73f1f-b68e-45ba-9145-45bf0ad5fd01.cpp:10:16:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’ 07d73f1f-b68e-45ba-9145-45bf0ad5fd01.cpp: In function ‘int getRoll(std::string, int)’: 07d73f1f-b68e-45ba-9145-45bf0ad5fd01.cpp:42:1: warning: control reaches end of n on-void function [-Wreturn-type]    42 | }       | ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,3 +1,42 @@
+int getRoll(string s, int i);
+int getBonusRoll(string s, int i);
+
+int main() {
+    int score = 0;
+    string s = "X/X/5+7-3-1/2+4/X/2-8-X/2+6/X";
+    for(int i = 0; i < s.length(); i++) {
+        char c = s[i];
+        if (c == '/') {
+            score += getRoll(s, i) + getBonusRoll(s, i);
+        } else if (c == 'X' || c == 'x') {
+            score += min(i+1, 10);
+        } else if (isdigit(c)) {
+            int roll = getRoll(s, i);
+            if (s[i+1] == '+') {
+                score += roll + max(min(getBonusRoll(s, i) - roll - 1, 9), 0);
+            } else {
+                score += roll;
+            }
+        }
+    }
+    return score;
+}
+
+int getRoll(string s, int i) {
+    if (s[i+1] == '/') {
+        return 10;
+    } else {
+        for(int j = i + 1; j < s.length(); j++) {
+            if (!isdigit(s[j])) {
+                return min(j - i, 10);
+            }
+            if (j >= s.length() || !isdigit(s[j])) {
+                return min(j - i, 10);
+            }
+        }
+    }
+}
+
 int getBonusRoll(string s, int i) {
     for(int j = i + 2; ; j++) {
         if (!isdigit(s[j])) {
