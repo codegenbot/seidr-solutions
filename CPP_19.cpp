520451306fb21f71d6d58eff812c67f324b9d018
beep boop <githubbot@vadim.me>
2024-07-23 08:15:23 +0200

    Solution 1 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/gydaa6jb Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,40 +1,26 @@
-#include <algorithm>
-#include <string>
-
-using namespace std;
-
 string sort_numbers(string numbers) {
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                {"eight", 8}, {"nine", 9}};
-
-    vector<string> numVec;
-    string temp;
-
+    map<string, int> numberMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                  {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                  {"eight", 8}, {"nine", 9}};
+    vector<string> numList;
+    string num;
     for (int i = 0; i < numbers.length(); i++) {
         if (numbers[i] == ' ') {
-            continue;
-        }
-        for (int j = i + 1; j <= numbers.length(); j++) {
-            if (j == numbers.length() || numbers[j] != numbers[i]) {
-                temp = numbers.substr(i, j - i);
-                numVec.push_back(temp);
-                break;
-            }
+            numList.push_back(num);
+            num = "";
+        } else {
+            num += numbers[i];
         }
     }
+    numList.push_back(num);
 
-    sort(numVec.begin(), numVec.end(), [&numMap](const string &a, const string &b) {
-        return to_string(numMap[a]) < to_string(numMap[b]);
+    sort(numList.begin(), numList.end(), [&numberMap](string a, string b) {
+        return numberMap[a] < numberMap[b];
     });
 
-    string result = "";
-    for (auto str : numVec) {
-        result += str + " ";
+    string result;
+    for (const auto& num : numList) {
+        result += num + " ";
     }
-    if (!result.empty()) {
-        result.pop_back();
-    }
-
-    return result;
+    return result.substr(0, result.length() - 1);
 }
