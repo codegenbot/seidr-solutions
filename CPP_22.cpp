15bb7e3a3ab3061b74e2d4912425020e4997b81b
beep boop <githubbot@vadim.me>
2024-07-23 09:11:52 +0200

    Solution 12 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ck6ppyb7 Prompt:       |                           ^~~~~~ 8e1f0197-1634-441c-8663-a0fd30ec4530.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8e1f0197-1634-441c-8663-a0fd30ec4530.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8e1f0197-1634-441c-8663-a0fd30ec4530.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8e1f0197-1634-441c-8663-a0fd30ec4530.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8e1f0197-1634-441c-8663-a0fd30ec4530.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 8e1f0197-1634-441c-8663-a0fd30ec4530.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -18,3 +18,9 @@ std::vector<int> filter_integers(const std::vector<std::any>& values) {
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
+
+int main() {
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+
+    return 0;
+}
