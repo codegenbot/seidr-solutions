f9e2203c2ca9cf994580ecffe05858e54250fbae
beep boop <githubbot@vadim.me>
2024-07-23 09:23:43 +0200

    Solution 44 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/fsg8yz42 Prompt:    20 |     return "None";       |     ^~~~~~ 58ea75f8-7ee7-4cc9-8c64-766fd05c810b.cpp:21:1: error: expected declaration befor e ‘}’ token    21 | }       | ^ In file included from 58ea75f8-7ee7-4cc9-8c64-766fd05c810b.cpp:23: 58ea75f8-7ee7-4cc9-8c64-766fd05c810b.cpp: In function ‘int main()’: 58ea75f8-7ee7-4cc9-8c64-766fd05c810b.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 58ea75f8-7ee7-4cc9-8c64-766fd05c810b.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 58ea75f8-7ee7-4cc9-8c64-766fd05c810b.cpp:25:37: error: ‘compare_one’ was not dec lared in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,7 +2,7 @@
 #include <typeinfo>
 #include <string>
 
-boost::any compare_one(boost::any a, boost::any b) {
+boost::any compare_one(const boost::any& a, const boost::any& b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
             return a;
