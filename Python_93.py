b1842d8ed9fa4d74d2c0d6fb6ae29fa185134c77
beep boop <githubbot@vadim.me>
2024-07-23 11:30:18 +0200

    Solution 22 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/30rlwqnr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97db714d-3323-416a-a41e-4b8bc4479 d05.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97db714d-3323-416a-a41e-4b8bc4479 d05.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,9 +3,9 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.isupper():
-                encoded_char = chr((ord(char) - 65 + 2) % 26 + 65)
+                encoded_char = chr((ord(char) - 65 + 3) % 26 + 65)
             else:
-                encoded_char = chr(ord(char) - 96 + 2)
+                encoded_char = chr((ord(char) - 97) % 26 + 97)
             encoded_message += encoded_char
         else:
             encoded_message += char
