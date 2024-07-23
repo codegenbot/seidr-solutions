7b865351560be03baf8926f364c2d4bdd1a512cd
beep boop <githubbot@vadim.me>
2024-07-24 01:57:38 +0200

    Solution 58 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e7255ba-4fae-43e0-81ef-68c8b39a0 d14.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e7255ba-4fae-43e0-81ef-68c8b39a0 d14.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,11 +1,4 @@
 def sort_even(l: list):
     evens = sorted([x for x in l if 0 <= x % 2])
     odds = sorted([x for x in l if x % 2 != 0], reverse=True)
-    result = []
-    len_evens, len_odds = len(evens), len(odds)
-    min_len = min(len_evens, len_odds)
-    for i in range(min_len):
-        result.append((evens[i], odds[i]))
-    result += [(evens[j], None) for j in range(min_len, len_evens)] + \
-               [(None, odds[k]) for k in range(min_len, len_odds)]
-    return tuple(result)
+    return list(zip(evens, odds))
