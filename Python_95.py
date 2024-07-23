539cf133b75286803520c7ce340325dc2aff82be
beep boop <githubbot@vadim.me>
2024-07-23 16:32:33 +0200

    Solution 4 of Python/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jxk1vclb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/908c349a-f5ef-42d4-bf82-2aa408d6d 803.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_95.py
+++ Python_95.py
@@ -1,7 +1,7 @@
-```
+```Python
 def check_dict_case(dict):
-    if len(dict) == 0:
+    if not dict:
         return False
-    all_lower = all(isinstance(key, str) and key.islower() for key in dict)
-    all_upper = all(isinstance(key, str) and key.isupper() for key in dict)
-    return all_lower or all_upper
+    all_lower = all(isinstance(key, str) and key.islower() for key in dict.keys())
+    all_upper = all(isinstance(key, str) and key.isupper() for key in dict.keys())
+    return all_lower or all_upper```
