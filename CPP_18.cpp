860347ac4fd30cc652ede28570a0638704fa3879
beep boop <githubbot@vadim.me>
2024-07-23 08:35:07 +0200

    Solution 14 of CPP/18, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/qzd19lh8 Prompt:       |                                 ^~~~~~ In file included from 87060ee9-cf14-41bb-b672-addffc2c6645.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 87060ee9-cf14-41bb-b672-addffc2c6645.cpp:3:32: error: ‘string’ was not declared in this scope     3 | int how_many_times(string str, string substring) {       |                                ^~~~~~ 87060ee9-cf14-41bb-b672-addffc2c6645.cpp:3:32: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 87060ee9-cf14-41bb-b672-addffc2c6645.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 87060ee9-cf14-41bb-b672-addffc2c6645.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 87060ee9-cf14-41bb-b672-addffc2c6645.cpp:3:48: error: expression list treated as  compound expression in initializer [-fpermissive]     3 | int how_many_times(string str, string substring) {       |                                                ^

--- CPP_18.cpp
+++ CPP_18.cpp
@@ -1,12 +1,10 @@
-```cpp
 #include <string>
 
-int how_many_times(std::string str, std::string substring) {
+int how_many_times(string str, string substring) {
     int count = 0;
     size_t pos = 0;
-    while ((pos = str.find(substring, pos)) != std::string::npos) {
+    while ((pos = str.find(substring, pos)) != string::npos) {
         ++count;
         pos += substring.size();
     }
     return count;
-}
