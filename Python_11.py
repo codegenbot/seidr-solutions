120cb111b7d00edba230a23ed66abc95ad1ad06d
beep boop <githubbot@vadim.me>
2024-07-23 08:48:38 +0200

    Solution 21 of Python/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yngnl4id Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cee158d6-a4b6-427e-9a2f-7cd6168c6 851.py", line 25, in <module>     check(string_xor)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cee158d6-a4b6-427e-9a2f-7cd6168c6 851.py", line 22, in check     assert string_xor("0101", "0000") == "0101"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_11.py
+++ Python_11.py
@@ -1,16 +1,12 @@
-```
 def string_xor(a: str, b: str) -> str:
-    a = bin(int(a or '0', 2))[2:]
-    b = bin(int(b or '0', 2))[2:]
-    
+    a = bin(int(a or "0", 2))[2:]
+    b = bin(int(b or "0", 2))[2:]
+
     result = ""
     for i in range(max(len(a), len(b))):
         if i < len(a) and i < len(b):
-            result += '1' if (a[-1-i] == "1" and not b[-1-i]) else '0'
+            result += "1" if (a[-1 - i] == "1" and not b[-1 - i]) else "0"
         else:
-            if i < len(a):
-                result = a[-1-i] + result
-            elif i < len(b):
-                result = b[-1-i] + result
-    
+            result = a[-1 - i] + result if i < len(a) else b[-1 - i] + result
+
     return result
