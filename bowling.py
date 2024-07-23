8295072a555c112a8e55b3712ff9ace9df319297
beep boop <githubbot@vadim.me>
2024-07-23 12:10:07 +0200

    Solution 51 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc0babd6-3675-412c-9022-c0877d53b 816.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,35 +1,21 @@
-```
 def bowling_score(game):
     score = 0
-    game_list = [int(i) if i.isdigit() else i for i in game.replace('-', '/').split()]
-    frame = 0
-    roll = 0
-    while len(game_list) > 1:
-        if game_list[0] == 'X':
-            score += 10
-            game_list.pop(0)
-        elif game_list[0] == '/':
-            score += 10 - game_list[1]
-            game_list.pop(0)
-            game_list.pop(0)
-        else:
-            frame_score = sum(game_list[:2])
-            game_list = game_list[2:]
-            if frame_score == 10:
-                score += frame_score
-            elif frame_score < 10:
-                score += frame_score
-                if game_list and game_list[0] + game_list[1] == str(10):
-                    score += 10
-                    game_list.pop(0)
-                    game_list.pop(0)
-                else:
-                    while len(game_list) > 0 and game_list[0] < 3:
-                        score += game_list.pop(0)
-        frame += 1
-    if game_list:
-        for i in range(len(game_list)):
-            score += game_list[i]
-    return score
+    game_list = [int(i) if i.isdigit() else i for i in game.replace('-', '/').split(' ')]
 
-print(bowling_score('9/-39/X3/7/54622325'))
+    for i in range(0, len(game_list), 2):
+        if game_list[i] == 'X':
+            if i < len(game_list) - 1 and game_list[i+1] == 'X':
+                score += 10 + 10
+            elif i < len(game_list) - 1 and game_list[i+1] == '/':
+                score += 10 + game_list[i-1]
+            else:
+                score += 10
+        elif game_list[i] == '/':
+            score += 10 - game_list[i-1]
+        else:
+            if i < len(game_list) - 1 and game_list[i+1].isdigit():
+                score += game_list[i] + game_list[i+1]
+            else:
+                score += game_list[i]
+
+    return score
