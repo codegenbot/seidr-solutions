7cdeac5563d945b16184e849058cd8f57c6cbe0e
beep boop <githubbot@vadim.me>
2024-07-23 14:21:25 +0200

    Solution 44 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wxae0lqy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b326e2e4-2c7c-4a1e-8fb1-1df7db9b9 d79.py", line 1, in <module>     def parse_music(music_string: str) -> List[int]:                                           ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_17.py
+++ Python_17.py
@@ -1,12 +1,13 @@
 from typing import List
+
 def parse_music(music_string: str) -> List[int]:
     beats = [4, 2, 1]  
     result = []
-
+    
     for note in music_string.split():
         if note == 'o':
-            result.append(beats[0])
+            result.extend(beats)
         elif note == 'o|':
-            result.append(beats[1])
+            result.extend(beats[1:2])
         elif note == '.|':
-            result.append(beats[2])
+            result.append(1)
