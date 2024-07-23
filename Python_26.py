39342802b0b6f162d11fd098edfa32407024cca4
beep boop <githubbot@vadim.me>
2024-07-24 01:58:15 +0200

    Solution 78 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ltngqvn8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d92f7680-dd81-4096-afa4-2d4b2af7c 744.py", line 11, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d92f7680-dd81-4096-afa4-2d4b2af7c 744.py", line 8, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,8 +1,7 @@
-from collections import OrderedDict
-
+```
 def remove_duplicates(input_list=None):
     if input_list is None:
         input_list = list(map(int, input("Enter numbers separated by space: ").split()))
     if not input_list:  
         return []  
-    return list(OrderedDict.fromkeys(input_list))
+    return sorted([i for i in set(input_list) if (input_list.count(i) == 1)])
