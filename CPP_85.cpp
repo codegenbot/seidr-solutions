ca12bab4001b6a761f7b69588ebd6f3cca12186c
beep boop <githubbot@vadim.me>
2024-07-23 12:27:11 +0200

    Solution 31 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a1rebcqs Prompt: aa66a897-aa6a-447f-9418-1b1c8387741e.cpp:26:22: error: ‘numbers’ was not declare d in this scope    26 |     int result = add(numbers);       |                      ^~~~~~~ aa66a897-aa6a-447f-9418-1b1c8387741e.cpp:26:29: error: ‘add’ cannot be used as a  function    26 |     int result = add(numbers);       |                             ^ aa66a897-aa6a-447f-9418-1b1c8387741e.cpp: At global scope: aa66a897-aa6a-447f-9418-1b1c8387741e.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ aa66a897-aa6a-447f-9418-1b1c8387741e.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from aa66a897-aa6a-447f-9418-1b1c8387741e.cpp:32: aa66a897-aa6a-447f-9418-1b1c8387741e.cpp: In function ‘int main()’: aa66a897-aa6a-447f-9418-1b1c8387741e.cpp:34:16: error: ‘add’ cannot be used as a  function    34 |     assert (add({4, 4, 6, 8}) == 12);       |             ~~~^~~~~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -16,13 +16,12 @@ int add(vector<int> lst){
 int main() {
     vector<int> numbers;
     int num;
-    
+
     cout << "Enter some numbers (enter -1 when you are done):" << endl;
-    
-    for(; ;){
-        cin >> num;
+
+    for(num = 0; ; num++){
+        cin >> numbers.push_back(num);
         if(num == -1) break;
-        numbers.push_back(num);
     }
 
     int result = add(numbers);
