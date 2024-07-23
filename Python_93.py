553aeb50e4372d2e95a70839c2f39e5cf1bd4674
beep boop <githubbot@vadim.me>
2024-07-23 12:20:32 +0200

    Solution 76 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/30rlwqnr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7d63c0dd-b659-408b-bf50-876c0a25c 5c0.py", line 26, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7d63c0dd-b659-408b-bf50-876c0a25c 5c0.py", line 22, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,17 +2,15 @@ def encode(message):
     encoded_message = ""
     for char in message:
         if char.isalnum():
-            if char.isdigit(): 
-                encoded_char = char
-            elif char.isalpha():
+            if char.isalpha():
                 if char.isupper():
                     encoded_char = chr((ord(char) - 65 + 3) % 26 + 65)
                 else:
                     encoded_char = chr((ord(char) - 97 + 3) % 26 + 97)
             else:  
-                encoded_char = char
+                encoded_char = chr((ord(char) - 48 + 3) % 10 + 48) if char.isdigit() else char
         else:  
-            encoded_char = char
+            encoded_message += char
         
         encoded_message += encoded_char
     
