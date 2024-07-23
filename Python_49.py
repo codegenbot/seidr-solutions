f2d2abffb30abea4015c621e730fdadbc4f45392
beep boop <githubbot@vadim.me>
2024-07-23 21:19:22 +0200

    Solution 53 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vigbqdjx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afc6e7e1-9732-4fac-abb4-cffe3eaec f09.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/afc6e7e1-9732-4fac-abb4-cffe3eaec f09.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,8 +1,15 @@
 ```
 def modp(n: int, p: int):
-    if n == 1:
-        return 1
-    elif n % 2 == 0:
-        return modp(n // 2, p) * modp(n // 2, p) % p
-    else:
-        return (n * modp(n, p - 2, p)) % p
+    if n == 0:
+        return 0
+    r, t = 1, 0
+    newt = 1
+    newr = n % p
+    while newr != 0 and newr != 1:
+        quotient = p // newr
+        temp = newt
+        newt = (temp - temp * quotient) % p
+        temp = r
+        r = (temp - temp * quotient) % p
+        newr, temp = temp, newr
+    return (r + p) % p if n < p else r
