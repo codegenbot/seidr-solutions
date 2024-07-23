ac6b1fe8158e5b1e569040c5ad593f5ac3888e0a
beep boop <githubbot@vadim.me>
2024-07-23 09:45:06 +0200

    Solution 60 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/fsg8yz42 Prompt:     9 | return "None";       | ^~~~~~ 153821bd-7177-45a9-b539-963d0cee74e7.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from 153821bd-7177-45a9-b539-963d0cee74e7.cpp:12: 153821bd-7177-45a9-b539-963d0cee74e7.cpp: In function ‘int main()’: 153821bd-7177-45a9-b539-963d0cee74e7.cpp:14:13: error: ‘boost’ has not been decl ared    14 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 153821bd-7177-45a9-b539-963d0cee74e7.cpp:14:29: error: ‘string’ was not declared  in this scope    14 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 153821bd-7177-45a9-b539-963d0cee74e7.cpp:14:37: error: ‘compare_one’ was not dec lared in this scope    14 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,14 +1,14 @@
 #include <boost/any.hpp>
-using namespace std;
+#include <string>
 
-boost::any compare(boost::any a, boost::any b) {
+boost::any compare_one(const boost::any& a, const boost::any& b) {
     if(a == b)
-        return boost::any{};
+        return "None";
     if(a.type() == typeid(int) && b.type() == typeid(float))
-        return max(boost::any_cast<int>(a), boost::any_cast<float>(b));
+        return std::max(boost::any_cast<int>(a), boost::any_cast<float>(b));
     if(a.type() == typeid(float) && b.type() == typeid(int))
-        return max(boost::any_cast<float>(a), boost::any_cast<int>(b));
-    if(a.type() == typeid(string) && b.type() == typeid(string))
-        return (boost::any_cast<string>(a) > boost::any_cast<string>(b)) ? a : b;
-    return boost::any{};
+        return std::max(boost::any_cast<float>(a), boost::any_cast<int>(b));
+    if(a.type() == typeid(std::string) && b.type() == typeid(std::string))
+        return (boost::any_cast<std::string>(a) > boost::any_cast<std::string>(b)) ? a : b;
+    return "None";
 }
