1aaa39cf96aeb9ad51abb92dde35a8b9bb1e9cd7
beep boop <githubbot@vadim.me>
2024-07-23 14:35:38 +0200

    Solution 57 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/yse8rt41 Prompt: it must return False for input t&f&f|f|f, but it returns .

--- solve-boolean.py
+++ solve-boolean.py
@@ -6,6 +6,6 @@ def solve_boolean(input):
     elif '&' in input:
         left, right = input.split('&')
         return (solve_boolean(left) and solve_boolean(right))
-    elif '|'in input:
+    elif '|' in input:
         left, right = input.split('|')
         return (solve_boolean(left)) or (solve_boolean(right))
