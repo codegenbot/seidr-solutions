1f559d4cf357090fdfd6734bec2f0c8f41144a8e
beep boop <githubbot@vadim.me>
2024-07-23 09:38:36 +0200

    Solution 24 of Python/22, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/b5eqdghe Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/087ac3f3-b024-42e7-a62e-9d9ae4cfd f2e.py", line 6, in <module>     check(filter_integers)           ^^^^^^^^^^^^^^^ NameError: name 'filter_integers' is not defined

--- Python_22.py
+++ Python_22.py
@@ -1,2 +1,4 @@
-def filter_integers(values: List[Any]) -> List[int]:
-    return [value for value in values if isinstance(value, int)]
+def filter_integers(lst):
+    return [x for x in lst if isinstance(x, int)]
+
+filter_integers([1, 2, 'a', 3.0, {'x': 4}, '@#'])
