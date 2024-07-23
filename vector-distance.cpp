1bb8fe5dd8203d7bbdd1a5dfe2e066e30d5abbfc
beep boop <githubbot@vadim.me>
2024-07-24 01:58:53 +0200

    Solution 79 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/s1xrt7nh Prompt:       |                                            ^~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_uninitialized.h:64,                  from /usr/include/c++/11/vector:66,                  from 4b10cb0c-a70b-4cfb-9e12-b4bbbed8dbf4.cpp:3: /usr/include/c++/11/ext/alloc_traits.h: In instantiation of ‘struct __gnu_cxx::_ _alloc_traits<std::allocator<float>, float>’: /usr/include/c++/11/bits/stl_vector.h:87:21:   required from ‘struct std::_Vecto r_base<float, std::allocator<float> >’ /usr/include/c++/11/bits/stl_vector.h:389:11:   required from ‘class std::vector <float>’ 4b10cb0c-a70b-4cfb-9e12-b4bbbed8dbf4.cpp:8:31:   required from here /usr/include/c++/11/ext/alloc_traits.h:59:53: error: no type named ‘size_type’ i n ‘struct std::allocator_traits<std::allocator<float> >’    59 |     typedef typename _Base_type::size_type          size_type;       |                                                     ^~~~~~~~~ /usr/include/c++/11/ext/alloc_traits.h:60:53: error: no type named ‘difference_t ype’ in ‘struct std::allocator_traits<std::allocator<float> >’    60 |     typedef typename _Base_type::difference_type    difference_type;       |                                                     ^~~~~~~~~~~~~~~ /usr/include/c++/11/ext/alloc_traits.h:68:23: error: ‘max_size’ has not been dec lared in ‘__gnu_cxx::__alloc_traits<std::allocator<float>, float>::_Base_type’    68 |     using _Base_type::max_size;       |                       ^~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,10 +1,12 @@
-```c++
-#include <vector>
+```cpp
 #include <cmath>
+#include <vector>
 
-float calculateEuclideanDistance(const std::vector<float>& v1, const std::vector<float>& v2) {
+float calculateEuclideanDistance(int n, const std::vector<float>& v1, const std::vector<float>& v2) {
     float sumOfSquares = 0;
-    for (int i = 0; i < v1.size(); i++) {
+    for (int i = 0; i < n; i++) {
+        std::vector<float> v1(n, 0.0); // Initialize with n elements of default value (0)
+        std::vector<float> v2(n);
         float diff = pow((v1[i] - v2[i]), 2);
         sumOfSquares += diff;
     }
