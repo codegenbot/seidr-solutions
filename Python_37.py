0481a417f5e2826a37f648dcf62b920415f50b9c
beep boop <githubbot@vadim.me>
2024-07-24 01:56:18 +0200

    Solution 56 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/84459085-7807-46fb-8ff4-67ad76d48 30a.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/84459085-7807-46fb-8ff4-67ad76d48 30a.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,5 @@
 def sort_even(l: list):
-    evens = sorted([x for x in l if 0 <= x % 2])
-    odds = sorted([x for x in l if x % 2 != 0], reverse=True)
-    return [(i, o) for i in evens for o in odds]
+    l.sort()
+    pos_evens = [x for x in l if 0 <= x % 2]
+    neg_odds = sorted([x for x in l if x % 2 != 0], reverse=True)
+    return [(i, o) for i in pos_evens for o in neg_odds]
