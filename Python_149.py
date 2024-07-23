e216038536d24d810978ffbf8e5035981eeca929
beep boop <githubbot@vadim.me>
2024-07-23 12:46:57 +0200

    Solution 28 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6r4kpwz3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/726ff283-9f33-4423-afcd-6b17950f0 fff.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,3 +1,2 @@
 def sorted_list_sum(lst):
-    lst = input("Enter a list of integers separated by spaces: ").split()
-    return " ".join(sorted([int(i) for i in lst if int(i) % 2 == 0]))
+    return sum(int(i) for i in sorted([i for i in lst if len(str(i)) % 2 == 0]))
