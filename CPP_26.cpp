bf3d67c509e8bdda6a1230bafb50d4edfbc2424b
beep boop <githubbot@vadim.me>
2024-07-23 09:09:26 +0200

    Solution 3 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3o8fpvty Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ edf0dd3e-4056-4381-8dd8-b62eba3c4bbb.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ edf0dd3e-4056-4381-8dd8-b62eba3c4bbb.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from edf0dd3e-4056-4381-8dd8-b62eba3c4bbb.cpp:11: edf0dd3e-4056-4381-8dd8-b62eba3c4bbb.cpp: In function ‘int main()’: edf0dd3e-4056-4381-8dd8-b62eba3c4bbb.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ edf0dd3e-4056-4381-8dd8-b62eba3c4bbb.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,14 +1,15 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
+using namespace std;
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-std::vector<int> remove_duplicates(std::vector<int> numbers) {
-    std::vector<int> result;
+vector<int> remove_duplicates(vector<int> numbers){
+    vector<int> result;
     for(int num : numbers){
-        if(std::find(result.begin(), result.end(), num) == result.end()){
+        if(find(result.begin(), result.end(), num) == result.end()){
             result.push_back(num);
         }
     }
@@ -16,6 +17,6 @@ std::vector<int> remove_duplicates(std::vector<int> numbers) {
 }
 
 int main(){
-    assert(isame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
+    assert(isame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), vector<int> {1, 4, 5}));
     return 0;
 }
