6aaa2d3b4505b749413a9dd26356ccc6e58938f8
beep boop <githubbot@vadim.me>
2024-07-24 00:05:22 +0200

    Solution 24 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/c444kx7a Prompt: 071ce760-bb85-4d20-8bb6-116ae1535c9a.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,41 +1,28 @@
-```
-#include <string>
+```cpp
+using namespace std;
 
 int bowlingScore(string s) {
     int score = 0;
     int currentRolls = 0;
     int currentFrame = 1;
 
-    for (int i = 0; i <= s.length(); i++) {
-        if (i < s.length() && s[i] == 'X') {
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'X') {
             score += 10 + (currentRolls > 0 ? 10 : 0);
             currentRolls = 0;
             currentFrame++;
-        } else if ((i+1) < s.length() && (s[i] == '/' || s[i] == 'X')) {
-            int roll1, roll2;
-            if (s[i] == '/') {
-                roll1 = s[i - 1] - '0';
-                roll2 = 10 - roll1;
-            } else {
-                roll1 = s[i] - '0';
-                roll2 = 10 - roll1;
-            }
+        } else if (s[i] == '/') {
+            int roll1 = s[i - 1] - '0';
+            int roll2 = s[i + 1] - '0';
             score += roll1 + roll2;
             currentRolls = 0;
             currentFrame++;
-        } else if (i < s.length()) {
+        } else {
             int roll = s[i] - '0';
             currentRolls++;
 
             if (currentRolls == 2) {
-                if (s[i-1] == 'X' && s[i-2] == 'X') {
-                    score += 30;
-                } else if (s[i-1] == '/') {
-                    int roll3 = 10 - (s[i-1] - '/');
-                    score += 10 + roll;
-                } else {
-                    score += roll * 2;
-                }
+                score += roll;
                 currentRolls = 0;
                 currentFrame++;
             }
