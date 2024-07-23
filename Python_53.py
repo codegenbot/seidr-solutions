82976a195cc0ad863a49baa139df09c530ebbf77
beep boop <githubbot@vadim.me>
2024-07-23 18:31:27 +0200

    Solution 84 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/djnymu5v Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1ad733c-b1e3-4897-83f2-1f85b2f87 f23.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,8 +1,9 @@
-```
-def add(x: int, y: int):
+def add(x: int, y: int) -> int:
     return x + y
 
+
 num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-result = add(num1, num2)
-print(result)```
+num2 = int(input("Enter the second numbers separated by space: "))
+numbers = list(map(int, num2.split()))
+result = sum([add(num1, i) for i in numbers])
+print(result)
