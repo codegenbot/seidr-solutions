d0770e2ea712b0b96343d028b7f44c7a45877ef1
beep boop <githubbot@vadim.me>
2024-07-23 22:36:50 +0200

    Solution 10 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/hlor11dd Prompt: 8e4c6539-1af5-4160-81bf-e8ef3acbf40e.cpp: In function ‘int main()’: 8e4c6539-1af5-4160-81bf-e8ef3acbf40e.cpp:3:5: error: ‘cin’ was not declared in t his scope     3 |     cin >> n >> m;       |     ^~~ 8e4c6539-1af5-4160-81bf-e8ef3acbf40e.cpp:10:5: error: ‘cout’ was not declared in  this scope    10 |     cout << fixed << probability << endl;       |     ^~~~ 8e4c6539-1af5-4160-81bf-e8ef3acbf40e.cpp:10:13: error: ‘fixed’ was not declared in this scope    10 |     cout << fixed << probability << endl;       |             ^~~~~ 8e4c6539-1af5-4160-81bf-e8ef3acbf40e.cpp:10:37: error: ‘endl’ was not declared i n this scope    10 |     cout << fixed << probability << endl;       |                                     ^~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int n, m;
-    std::cin >> n >> m;
-
-    double probability = 0.0;
+    cin >> n >> m;
+    double total_outcomes = n * m;
+    double favorable_outcomes = 0;
     for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            if (i > j) {
-                probability += 1.0 / (n * m);
-            }
-        }
+        favorable_outcomes += (double)(i - 1) * (m) / total_outcomes;
     }
-
-    std::cout << probability << std::endl;
-
+    double probability = favorable_outcomes;
+    cout << fixed << probability << endl;
     return 0;
 }
