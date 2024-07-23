a9e23e021a320df019554d6b26075026201e8337
beep boop <githubbot@vadim.me>
2024-07-24 01:52:09 +0200

    Solution 43 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3tlymlm5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0bf0a25a-d7cf-4059-8b7a-0e18be7fe 4fc.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0bf0a25a-d7cf-4059-8b7a-0e18be7fe 4fc.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,2 @@
 def sort_third(lst: list) -> list:
-    return sorted(lst)
-
-lst = input("Enter a list of integers separated by spaces: ")
-lst = [int(i) for i in lst.split()]
-print(sort_third(lst))
+    return sorted(lst, key=lambda x: (x[2], x))
