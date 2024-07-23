f366b1c84ddc51107e7a48e7a10ecf5b4ad22edd
beep boop <githubbot@vadim.me>
2024-07-23 20:13:10 +0200

    Solution 71 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lkb9rtzq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a10c082-9c4c-406a-adb3-48dd7b7e2 026.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_162.py
+++ Python_162.py
@@ -1,8 +1,8 @@
-```
+```python
 import hashlib
 
-"""def string_to_md5():
+def string_to_md5():
     text = input()
     if not text:
         return None
-    return hashlib.md5(text.encode()).hexdigest()"""
+    return hashlib.md5(text.encode()).hexdigest()
