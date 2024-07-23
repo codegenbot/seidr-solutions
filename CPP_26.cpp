23d8223816c56f0164e97818e48336a503a2cf8d
beep boop <githubbot@vadim.me>
2024-07-23 22:29:24 +0200

    Solution 57 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rig1j6it Prompt:  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0a208927-9d9f-4c5a-bf4a-bed645d5005c.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0a208927-9d9f-4c5a-bf4a-bed645d5005c.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0a208927-9d9f-4c5a-bf4a-bed645d5005c.cpp:15: 0a208927-9d9f-4c5a-bf4a-bed645d5005c.cpp: In function ‘int main()’: 0a208927-9d9f-4c5a-bf4a-bed645d5005c.cpp:25:20: error: ‘remove_duplicates’ was n ot declared in this scope    25 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 0a208927-9d9f-4c5a-bf4a-bed645d5005c.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,7 +1,4 @@
 ```c++
-#include <vector>
-#include <unordered_map>
-
 bool issame(vector<int> a, vector<int> b) {
     if(a.size()!=b.size()) return false;
     for(int i=0; i<a.size(); i++) {
@@ -12,19 +9,4 @@ bool issame(vector<int> a, vector<int> b) {
 
 int main() {
     assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), vector<int>{1, 4, 5}));
-    // ... 
-}
-
-vector<int> remove_duplicates(vector<int> numbers) {
-    unordered_map<int, bool> map;
-    vector<int> result;
-
-    for (int num : numbers) {
-        if (!map[num]) {
-            map[num] = true;
-            result.push_back(num);
-        }
-    }
-
-    return result;
 }
