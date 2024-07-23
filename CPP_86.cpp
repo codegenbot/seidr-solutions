8249fd5a64f14a2c022712de6b1e0f0e6ff753f2
beep boop <githubbot@vadim.me>
2024-07-24 00:48:02 +0200

    Solution 28 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9be4i5dy Prompt: 653d2455-3f07-4f2a-9a9c-1299c9415442.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string anti_shuffle(string s) {       | ^~~~~~ 653d2455-3f07-4f2a-9a9c-1299c9415442.cpp:16:1: error: ‘string’ does not name a t ype    16 | string split(const string& s, char c) {       | ^~~~~~ 653d2455-3f07-4f2a-9a9c-1299c9415442.cpp:31:1: error: ‘string’ does not name a t ype    31 | string join(const vector<string>& v, char c) {       | ^~~~~~ In file included from 653d2455-3f07-4f2a-9a9c-1299c9415442.cpp:39: 653d2455-3f07-4f2a-9a9c-1299c9415442.cpp: In function ‘int main()’: 653d2455-3f07-4f2a-9a9c-1299c9415442.cpp:41:13: error: ‘anti_shuffle’ was not de clared in this scope    41 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,34 +1,38 @@
+```
 #include <algorithm>
-#include <string>
-
 using namespace std;
 
-string anti_shuffle(string s) {
-    string result = "";
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == ' ') {
-            result += s[i];
-        } else {
-            string word;
-            for (int j = i; j < s.length() && s[j] != ' '; j++) {
-                word += s[j];
-            }
-            string newWord;
-            for (char c : word) {
-                if (newWord.empty()) {
-                    newWord += c;
-                } else {
-                    newWord.push_back(c);
-                }
-            }
-            result += newWord;
-            i = j - 1;
+std::string anti_shuffle(std::string s) {
+    std::string result = "";
+    for (const auto& word : split(s, ' ')) {
+        for (char c : word) {
+            result += (c <= result.back()) ? c : " ";
+            for (; c > result.back(); ++result.back());
         }
+        result += ' ';
     }
-    return result;
+    return result.substr(0, result.size() - 1);
 }
 
-int main() {
-    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
-    return 0;
+std::string split(const std::string& s, char c) {
+    vector<string> v;
+    string temp = "";
+    for (const auto& ch : s) {
+        if (ch == c) {
+            v.push_back(temp);
+            temp = "";
+        } else {
+            temp += ch;
+        }
+    }
+    v.push_back(temp);
+    return join(v, ' ');
+}
+
+std::string join(const vector<string>& v, char c) {
+    std::string result = "";
+    for (const auto& s : v) {
+        result += s + c;
+    }
+    return result.substr(0, result.size() - 1);
 }
