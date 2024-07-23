630f3cbbb5e06530d7802032b9334a7778bdfffb
beep boop <githubbot@vadim.me>
2024-07-23 15:45:04 +0200

    Solution 64 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rdyg9tmk Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,6 +1,3 @@
-```
-def decode_shift(s: str):
-    if not set(s).issubset('abcdefghijklmnopqrstuvwxyz'):
-        return "Invalid Input"
-    else:
-        return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
+def decode_shift():
+    s = input("Enter a string: ")
+    print(decode_shift(s))
