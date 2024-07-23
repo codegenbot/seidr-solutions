957141128aff88b10aaac166749a050e6e536e33
beep boop <githubbot@vadim.me>
2024-07-23 11:15:58 +0200

    Solution 14 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/rmug2bur Prompt:    30 |             if (!operators.empty() && ((expression[i] == 't' && operator s.top() == '|') ||       |                  ^~~~~~~~~ fb1c33aa-a34c-4b63-8769-b4fda5e4abaf.cpp:40:13: error: ‘operators’ was not decla red in this scope    40 |             operators.push(expression[i]);       |             ^~~~~~~~~ fb1c33aa-a34c-4b63-8769-b4fda5e4abaf.cpp:42:20: error: ‘operators’ was not decla red in this scope    42 |             while (operators.top() != '(') {       |                    ^~~~~~~~~ fb1c33aa-a34c-4b63-8769-b4fda5e4abaf.cpp:65:13: error: ‘operators’ was not decla red in this scope    65 |             operators.pop();       |             ^~~~~~~~~ fb1c33aa-a34c-4b63-8769-b4fda5e4abaf.cpp:69:13: error: ‘operators’ was not decla red in this scope    69 |     while (!operators.empty()) {       |             ^~~~~~~~~ fb1c33aa-a34c-4b63-8769-b4fda5e4abaf.cpp:70:24: error: ‘i’ was not declared in t his scope    70 |         if (expression[i] == '|') {       |                        ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
 #include <string>
 #include <stack>
+
 using namespace std;
 
 bool evaluateBooleanExpression(string expression) {
     stack<char> operators;
-    int i = 0; // size_t i = 0 if you're using C++11
     string operand = "";
 
-    for (; i < expression.length(); i++) {
+    for (int i = 0; i < expression.length(); i++) {
         if (expression[i] == '&') {
             while (!operators.empty() && operators.top() == '|') {
                 operators.pop();
             }
             if (!operand.empty()) {
-                if (operand == "T")
-                    operand = "True";
-                else
-                    operand = "False";
+                if (operand == "T") operand = "False";
+                else operand = "True";
 
-                return operand == "True";
+                return (operand == "True");
             }
         } else if (expression[i] == '|') {
             while (!operators.empty()) operators.pop();
             if (!operand.empty()) {
-                if (operand == "T")
-                    operand = "True";
-                else
-                    operand = "False";
+                if (operand == "T") operand = "True";
+                else operand = "False";
 
-                return operand == "True";
+                return (operand == "True");
             }
         } else if (expression[i] == 't' || expression[i] == 'f') {
             if (!operators.empty() && ((expression[i] == 't' && operators.top() == '|') ||
                                         (expression[i] == 'f' && operators.top() == '&'))) {
                 while (!operators.empty()) operators.pop();
             }
-            operand += (expression[i] == 't'? "T" : "F");
-        } else if (expression[i] == '(') {
-            operators.push(expression[i]);
+            operand += expression[i];
+        } else {
+            continue;
+        }
+
+        if (expression[i] == '(') {
+            operators.push('(');
         } else if (expression[i] == ')') {
             while (operators.top() != '(') {
                 if (expression[i] == '|') {
@@ -46,12 +46,18 @@ bool evaluateBooleanExpression(string expression) {
                         operators.pop();
                     }
                     if (!operand.empty()) {
-                        return operand == "T";
+                        if (operand == "T") operand = "True";
+                        else operand = "False";
+
+                        return (operand == "True");
                     }
                 } else if (expression[i] == '&') {
                     while (!operators.empty()) operators.pop();
                     if (!operand.empty()) {
-                        return operand == "T";
+                        if (operand == "T") operand = "True";
+                        else operand = "False";
+
+                        return (operand == "True");
                     }
                 }
 
@@ -67,20 +73,30 @@ bool evaluateBooleanExpression(string expression) {
                 operators.pop();
             }
             if (!operand.empty()) {
-                return operand == "T";
+                if (operand == "T") operand = "True";
+                else operand = "False";
+
+                return (operand == "True");
             }
         } else if (expression[i] == '&') {
             while (!operators.empty()) operators.pop();
             if (!operand.empty()) {
-                return operand == "T";
+                if (operand == "T") operand = "True";
+                else operand = "False";
+
+                return (operand == "True");
             }
         }
 
         operators.pop();
     }
 
-    if (!operand.empty())
-        return operand == "T";
+    if (!operand.empty()) {
+        if (operand == "T") operand = "True";
+        else operand = "False";
+
+        return (operand == "True");
+    }
 
     return true;
 }
