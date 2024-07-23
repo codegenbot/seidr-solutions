bab0ed89aef89e38b23a784886925a215b8eb826
beep boop <githubbot@vadim.me>
2024-07-23 10:48:42 +0200

    Solution 60 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4qm5mikn Prompt: In file included from /usr/include/c++/11/algorithm:62,                  from 9fbf897c-a5dc-44d4-8f64-c179bcf869fd.cpp:1: /usr/include/c++/11/bits/stl_algo.h:1955:50: note:   ‘std::_List_iterator<float> ’ is not derived from ‘const std::reverse_iterator<_Iterator>’  1955 |                                 std::__lg(__last - __first) * 2,       |                                           ~~~~~~~^~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/algorithm:61,                  from 9fbf897c-a5dc-44d4-8f64-c179bcf869fd.cpp:1: /usr/include/c++/11/bits/stl_iterator.h:1693:5: note: candidate: ‘template<class  _IteratorL, class _IteratorR> constexpr decltype ((__x.base() - __y.base())) st d::operator-(const std::move_iterator<_IteratorL>&, const std::move_iterator<_It eratorR>&)’  1693 |     operator-(const move_iterator<_IteratorL>& __x,       |     ^~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h:1693:5: note:   template argument deduct ion/substitution failed: In file included from /usr/include/c++/11/algorithm:62,                  from 9fbf897c-a5dc-44d4-8f64-c179bcf869fd.cpp:1: /usr/include/c++/11/bits/stl_algo.h:1955:50: note:   ‘std::_List_iterator<float> ’ is not derived from ‘const std::move_iterator<_IteratorL>’  1955 |                                 std::__lg(__last - __first) * 2,       |                                           ~~~~~~~^~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 bool issame(vector<float> a, vector<float> b) {
     return a.size() == b.size() && all_of(a.begin(), a.end(),
-          [b](float x) { return abs(x - *min_element(b.begin(), back_inserter(accumulate(b.begin(), b.end(), list<float>(), [](list<float>& l, float n){l.push_back(n);return l;})[0].begin())) ) <= 1e-6; });
+          [&b](float x) { return abs(x - *find_if(b.begin(), b.end(), [&](float y) { return abs(y-x) < 1e-6; }).first) <= 1e-6; });
 }
 
 vector<float> sort_even(vector<float> l) {
@@ -18,8 +18,9 @@ vector<float> sort_even(vector<float> l) {
                     even.push_back(x);
                 }
             }
-            sort(even.begin(), even.end());
-            result.push_back(*even.begin());
+            std::list<float> sortedEven = even;
+            sortedEven.sort();
+            result.push_back(*sortedEven.begin());
         } else {
             result.push_back(l[i]);
         }
