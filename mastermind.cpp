51f12ad97bbb82438b711c96ce65656612efe3c5
beep boop <githubbot@vadim.me>
2024-07-23 19:04:46 +0200

    Solution 29 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/dbfw81d6 Prompt: 89a77c65-a2aa-4d02-9e00-82cad6bb6b01.cpp:1:1: error: expected unqualified-id bef ore ‘++’ token     1 | ++codeFreq[code[i] - 'A'];       | ^~ 89a77c65-a2aa-4d02-9e00-82cad6bb6b01.cpp:2:1: error: expected unqualified-id bef ore ‘++’ token     2 | ++guessFreq[guess[i] - 'A'];       | ^~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,32 +1,6 @@
-#include <iostream>
-#include <algorithm>
-
-int main() {
-    std::string code, guess;
-    std::cin >> code >> guess;
-
-    int codeFreq[6] = {0};
-    int guessFreq[6] = {0};
-    int black = 0, white = 0;
-
-    for (int i = 0; i < 4; ++i) {
-        for (int j = 0; j < 4; ++j) {
-            if (i != j && code[i] == guess[j] && codeFreq[i] < 1 && guessFreq[j] < 1) {
-                ++white;
-                codeFreq[i] = 1;
-                guessFreq[j] = 1;
-                break;
-            }
-        }
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            ++black;
-        }
-    }
-
-    std::cout << black << " " << white << std::endl;
-
-    return 0;
+int codeFreq[26] = {0};
+int guessFreq[26] = {0};
+for (int i = 0; i < 4; ++i) {
+    ++codeFreq[code[i] - 'A'];
+    ++guessFreq[guess[i] - 'A'];
 }
