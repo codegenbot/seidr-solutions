eb4d4764a16deed856e99f543d93742d53e8d437
beep boop <githubbot@vadim.me>
2024-07-24 00:18:28 +0200

    Solution 4 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/di8vbgyq Prompt: 109ca980-ae1e-407e-a83b-0c80578142f9.cpp: In function ‘std::string validateTweet (std::string)’: 109ca980-ae1e-407e-a83b-0c80578142f9.cpp:11:14: error: ‘count_if’ is not a membe r of ‘std’    11 |     if (std::count_if(tweet.begin(), tweet.end(), ::isalnum) > 140) {       |              ^~~~~~~~ 109ca980-ae1e-407e-a83b-0c80578142f9.cpp:14:47: error: ‘count_if’ is not a membe r of ‘std’    14 |     return "Your tweet has " + to_string(std::count_if(tweet.begin(), tw eet.end(), ::isalnum)) + " characters";       |                                               ^~~~~~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -7,10 +8,10 @@ string validateTweet(string tweet) {
     if (tweet.empty()) {
         return "You didn't type anything";
     }
-    if (std::count(tweet.begin(), tweet.end(), '\0') > 140) {
+    if (std::count(tweet.begin(), tweet.end(), [](char c){ return std::isalnum(c); }) > 140) {
         return "Too many characters";
     }
-    return "Your tweet has " + to_string(std::count(tweet.begin(), tweet.end(), '\0')) + " characters";
+    return "Your tweet has " + to_string(std::count(tweet.begin(), tweet.end(), [](char c){ return std::isalnum(c); })) + " characters";
 }
 
 int main() {
