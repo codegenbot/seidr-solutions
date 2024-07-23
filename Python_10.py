d0bd2194db4dcdf29f4bd41391af28dbe36361b9
beep boop <githubbot@vadim.me>
2024-07-23 09:17:23 +0200

    Solution 44 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8ks3fd7x Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3c912d77-3396-497a-b6ab-9142c336d b86.py", line 11, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3c912d77-3396-497a-b6ab-9142c336d b86.py", line 8, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3c912d77-3396-497a-b6ab-9142c336d b86.py", line 4, in make_palindrome     if is_palindrome(postfix):        ^^^^^^^^^^^^^ NameError: name 'is_palindrome' is not defined. Did you mean: 'make_palindrome'?

--- Python_10.py
+++ Python_10.py
@@ -2,6 +2,9 @@
 def make_palindrome(string: str) -> str:
     for i in range(len(string)):
         postfix = string[i:]
-        if postfix == postfix[::-1]:
+        if is_palindrome(postfix):
             return string + postfix[::-1]
     return string + string[::-1]
+
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
