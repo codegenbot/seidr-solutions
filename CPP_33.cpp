a0103ebfd6ccaaaf1674b2912a5ff869ee339d18
beep boop <githubbot@vadim.me>
2024-07-23 09:14:43 +0200

    Solution 14 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/c5dpupn5 Prompt: ca167e63-56ec-4fc6-92fb-a6d81af0e575.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ ca167e63-56ec-4fc6-92fb-a6d81af0e575.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ ca167e63-56ec-4fc6-92fb-a6d81af0e575.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ ca167e63-56ec-4fc6-92fb-a6d81af0e575.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,10 +3,14 @@
 #include <cassert>
 using namespace std;
 
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 vector<int> sort_third(vector<int> l){
     vector<int> res = l;
     for (int i = 0; i < l.size(); i+=3){
-        sort(res.begin() + i, min(res.begin() + i + 3, res.end()));
+        std::sort(res.begin() + i, std::min(res.begin() + i + 3, res.end()));
     }
     return res;
 }
