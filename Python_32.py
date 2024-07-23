959d735a8bad1e32dd1bdeb71fd600e8acbd317a
beep boop <githubbot@vadim.me>
2024-07-23 14:23:27 +0200

    Solution 35 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ek0jmw7w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/47d16a4a-6a9a-430e-8781-cb1ebd221 390.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,13 +1,19 @@
 def main():
-    m = float(input("Enter the slope of a line: "))
-    c = float(input("Enter the y-intercept of a line: "))
+    x1 = float(input("Enter x1: "))
+    x2 = float(input("Enter x2: "))
+    y1 = float(input("Enter y1: "))
+    y2 = float(input("Enter y2: "))
 
-    x = float(
-        input("Enter the value for which you want to find the corresponding y-value: ")
-    )
+    def find_zero(xs):
+        return -xs[1] / xs[0]
 
-    y = m * x + c
-    print(f"The corresponding y-value is {y}.")
+    if (x1 * y2) - (y1 * x2) == 0:
+        print(
+            "The line is vertical and passes through the point ({}, {})".format(x1, y1)
+        )
+    elif (x1 * y2) - (y1 * x2) != 0:
+        x = find_zero((x1, x2))
+        print("The line crosses the x-axis at the point ({})".format(x))
 
 
 if __name__ == "__main__":
