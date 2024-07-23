99a21c3032b70ff7735d5de2bed48716fc3db37e
beep boop <githubbot@vadim.me>
2024-07-23 11:02:04 +0200

    Solution 1 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/rmug2bur Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,127 +1,33 @@
 #include <string>
 using namespace std;
 
-bool evaluateBooleanExpression(string expression) {
-    stack<char> operators;
-    string operand = "";
-
-    for (int i = 0; i < expression.length(); i++) {
-        if (expression[i] == '&') {
-            while (!operators.empty() && operators.top() == '|') {
-                operators.pop();
+bool solveBoolean(string s) {
+    stack<char> st;
+    bool res = false;
+    
+    for(int i=s.length()-1; i>=0; i--) {
+        if(s[i] == '&') {
+            while(!st.empty() && st.top() == '&') {
+                st.pop();
             }
-            if (!operand.empty()) {
-                if (operand == "T")
-                    operand = "True";
-                else
-                    operand = "False";
-
-                cout << "output: " << operand << endl;
-
-                return (operand == "True");
+            if(st.empty()) {
+                res = true;
+            } else {
+                res = false;
             }
-        } else if (expression[i] == '|') {
-            while (!operators.empty()) operators.pop();
-            if (!operand.empty()) {
-                if (operand == "T")
-                    operand = "True";
-                else
-                    operand = "False";
-
-                cout << "output: " << operand << endl;
-
-                return (operand == "True");
+        } else if(s[i] == '|') {
+            while(!st.empty() && st.top() == '|') {
+                st.pop();
             }
-        } else if (expression[i] == 't' || expression[i] == 'f') {
-            if (!operators.empty() && ((expression[i] == 't' && operators.top() == '|') ||
-                                        (expression[i] == 'f' && operators.top() == '&'))) {
-                while (!operators.empty()) operators.pop();
+            if(st.empty()) {
+                res = false;
+            } else {
+                res = true;
             }
-            operand += expression[i];
         } else {
-            continue;
-        }
-
-        if (expression[i] == '(') {
-            operators.push(expression[i]);
-        } else if (expression[i] == ')') {
-            while (operators.top() != '(') {
-                if (expression[i] == '|') {
-                    while (!operators.empty() && operators.top() == '|') {
-                        operators.pop();
-                    }
-                    if (!operand.empty()) {
-                        if (operand == "T")
-                            operand = "True";
-                        else
-                            operand = "False";
-
-                        cout << "output: " << operand << endl;
-
-                        return (operand == "True");
-                    }
-                } else if (expression[i] == '&') {
-                    while (!operators.empty()) operators.pop();
-                    if (!operand.empty()) {
-                        if (operand == "T")
-                            operand = "True";
-                        else
-                            operand = "False";
-
-                        cout << "output: " << operand << endl;
-
-                        return (operand == "True");
-                    }
-                }
-
-                operators.pop();
-            }
-            operators.pop();
+            st.push(s[i]);
         }
     }
-
-    while (!operators.empty()) {
-        if (expression[i] == '|') {
-            while (!operators.empty() && operators.top() == '|') {
-                operators.pop();
-            }
-            if (!operand.empty()) {
-                if (operand == "T")
-                    operand = "True";
-                else
-                    operand = "False";
-
-                cout << "output: " << operand << endl;
-
-                return (operand == "True");
-            }
-        } else if (expression[i] == '&') {
-            while (!operators.empty()) operators.pop();
-            if (!operand.empty()) {
-                if (operand == "T")
-                    operand = "True";
-                else
-                    operand = "False";
-
-                cout << "output: " << operand << endl;
-
-                return (operand == "True");
-            }
-        }
-
-        operators.pop();
-    }
-
-    if (!operand.empty()) {
-        if (operand == "T")
-            operand = "True";
-        else
-            operand = "False";
-
-        cout << "output: " << operand << endl;
-
-        return (operand == "True");
-    }
-
-    return true;
+    
+    return res;
 }
