e68e2491f370bedb45cc7193594ee5fa22061c81
beep boop <githubbot@vadim.me>
2024-07-23 12:12:57 +0200

    Solution 7 of CPP/91, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/niockw5x Prompt: 56cda7d4-74f4-419d-b047-209432a6cf5e.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int is_bored(string S){       |              ^~~~~~ In file included from 56cda7d4-74f4-419d-b047-209432a6cf5e.cpp:14: 56cda7d4-74f4-419d-b047-209432a6cf5e.cpp: In function ‘int main()’: 56cda7d4-74f4-419d-b047-209432a6cf5e.cpp:16:21: error: ‘is_bored’ cannot be used  as a function    16 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,11 +1,14 @@
-int is_bored(string S) {
+#include <string>
+
+int is_bored(std::string S){
     int count = 0;
+    std::string boredom = "I";
     size_t pos = 0;
-    while ((pos = S.find("I", pos)) != string::npos) {
-        if (S[pos] == '!' || S[pos] == '?' || S[pos] == '.') {
+
+    while ((pos = S.find(". ", pos)) != std::string::npos || (pos = S.find("? ", pos)) != std::string::npos || (pos = S.find("! ", pos)) != std::string::npos) {
+        if (S.substr(0, boredom.size()).compare(boredom) == 0)
             count++;
-        }
-        pos++;
+        pos += 2;
     }
     return count;
 }
