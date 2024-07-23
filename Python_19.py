260048e36da2468605756a88dd9410526822fa15
beep boop <githubbot@vadim.me>
2024-07-23 18:49:35 +0200

    Solution 0 of Python/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vtk2wngd Prompt: Complete the following code given the task description and function signature.

---
+++ Python_19.py
@@ -0,0 +1,23 @@
+def sort_numbers(numbers: str) -> str:
+    num_dict = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+    sorted_nums = sorted([num_dict[i] for i in numbers.split()])
+    return " ".join(
+        [
+            i
+            for i in map(
+                lambda x: list(num_dict.keys())[list(num_dict.values()).index(x)],
+                sorted_nums,
+            )
+        ]
+    )
