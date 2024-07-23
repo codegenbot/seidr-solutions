8090b7d686e568259c5cdc3f74b49bb7d20e6740
beep boop <githubbot@vadim.me>
2024-07-23 13:27:29 +0200

    Solution 0 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2jk3dhyi Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_105.cpp
@@ -0,0 +1,44 @@
+vector<string> by_length(vector<int> arr) {
+    vector<int> nums;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9)
+            nums.push_back(i);
+    }
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+    vector<string> result;
+    for (int num : nums) {
+        string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
+    }
+    return result;
+}
