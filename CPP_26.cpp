b2c92e80a49dd4297e35fd2fdbf024385544b743
beep boop <githubbot@vadim.me>
2024-07-23 09:21:22 +0200

    Solution 16 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3o8fpvty Prompt:       |     ^~~~~~ b70b81c4-5df7-4d78-befe-e85f87fd8c86.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | using namespace std; b70b81c4-5df7-4d78-befe-e85f87fd8c86.cpp: At global scope: b70b81c4-5df7-4d78-befe-e85f87fd8c86.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ b70b81c4-5df7-4d78-befe-e85f87fd8c86.cpp:5:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     5 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ b70b81c4-5df7-4d78-befe-e85f87fd8c86.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ b70b81c4-5df7-4d78-befe-e85f87fd8c86.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main(){       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
-
 using namespace std;
 
-bool isSame(vector<int> a, vector<int> b){
+bool issueSame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-vector<int> unique(vector<int> numbers){
+vector<int> remove_duplicates(vector<int> numbers){
     vector<int> result;
     for(int num : numbers){
         if(find(result.begin(), result.end(), num) == result.end()){
@@ -19,6 +18,5 @@ vector<int> unique(vector<int> numbers){
 }
 
 int main(){
-    assert(isSame(unique({1, 2, 3, 2, 4, 3, 5}), vector<int> {1, 4, 5}));
+    assert(issueSame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), vector<int> {1, 4, 5}));
     return 0;
-}
