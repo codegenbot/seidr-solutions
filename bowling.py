46624a09f3341343e5cfafe44a3c75e610ad9fad
beep boop <githubbot@vadim.me>
2024-07-23 12:43:38 +0200

    Solution 62 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt: it must return 114 for input 9/-39/X3/7/54622325, but it returns .

--- bowling.py
+++ bowling.py
@@ -1,34 +1,24 @@
 def bowling_score(game):
-    score = 0
-    game_list = [int(i) if i.isdigit() else i for i in game.replace('-', '/').split()]
-    frame = 0
-    roll = 0
-    while len(game_list) > 1:
-        if game_list[0] == 'X':
-            score += 10
-            game_list.pop(0)
-        elif game_list[0] == '/':
-            score += 10 - game_list[1]
-            game_list.pop(0)
-            game_list.pop(0)
-        else:
-            frame_score = sum(game_list[:2])
-            game_list = game_list[2:]
-            if frame_score == 10:
-                score += frame_score
-            elif frame_score < 10:
-                score += frame_score
-                if game_list and game_list[0] + game_list[1] == str(10):
-                    score += 10
-                    game_list.pop(0)
-                    game_list.pop(0)
-                else:
-                    while len(game_list) > 0 and game_list[0] < 3:
-                        score += game_list.pop(0)
-        frame += 1
-    if game_list:
-        for i in range(len(game_list)):
-            score += game_list[i]
-    return score
+    game_list = [int(i) if i.isdigit() else i for i in game.replace('-', '/').split(' ')]
 
-print(bowling_score('9/-39/X3/7/54622325'))
+    score = 0
+    frame = 1
+
+    for i in range(len(game_list)):
+        if game_list[i] == 'X':
+            score += 10
+            frame += 1
+        elif game_list[i] == '/':
+            score += 10 - int(game_list[i-1])
+            frame += 1
+        else:
+            if i < len(game_list) - 1 and (game_list[i].isdigit() and game_list[i+1] == 'X'):
+                score += int(game_list[i]) + 9
+                frame += 2
+            elif i < len(game_list) - 1 and (game_list[i].isdigit() and game_list[i+1] == '/'):
+                score += int(game_list[i]) + 10 - int(game_list[i-1])
+                frame += 2
+            else:
+                score += int(game_list[i])
+
+    return score
