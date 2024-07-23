24362e3d37122f0d4bd2401dc17b501bbea54f59
beep boop <githubbot@vadim.me>
2024-07-23 21:23:31 +0200

    Solution 22 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/nbe9mbei Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 652cc066-3940-440e-b4ba-7921e3e0c094.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- dice-game.cpp
+++ dice-game.cpp
@@ -6,9 +6,7 @@ double diceGame(int n, int m) {
     double p = 0.0;
 
     for(int i = 1; i <= std::min(n, m); i++) {
-        if(i < m) {
-            p += (n - i) / total;
-        }
+        p += (n - i) / total;
     }
 
     return p;
