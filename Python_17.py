3e75fe858565edf646b81314ee639b2887777e24
beep boop <githubbot@vadim.me>
2024-07-24 00:59:07 +0200

    Solution 33 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/mkfx5cx8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1b13ba40-4bc9-4902-ab3e-4cb17d108 2d9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,14 +1,21 @@
+```
 def parse_music(music_string: str) -> list:
     music_list = []
     i = 0
     while i < len(music_string):
-        if i+2 <= len(music_string) and music_string[i:i+2].lower() == 'o|':
+        if i+2 <= len(music_string) and music_string[i:i+2] == 'o|':
             music_list.append(4)
             i += 2
-        elif i+6 <= len(music_string) and music_string[i:i+6].lower() == '.|..|.||.|..|':
-            music_list.append(1)
+        elif i+6 <= len(music_string) and (music_string[i:i+6] in ['.|..|.||.|..|', '.|..|..|...|..|..']):
+            if music_string[i:i+6] == '.|..|.||.|..|':
+                music_list.append(1)
+            elif music_string[i:i+6] == '.|..|..|...|..|..':
+                music_list.append(0.5)
             i += 6
-        elif i+8 <= len(music_string) and music_string[i:i+8].lower() == '..|.|..|...|..|.|..|':
-            music_list.append(0.5)
+        elif i+8 <= len(music_string) and (music_string[i:i+8] in ['..|.|..|...|..|.|..|', '..|..|..|...|..|..|..']):
+            if music_string[i:i+8] == '..|.|..|...|..|.|..|':
+                music_list.append(0.5)
+            elif music_string[i:i+8] == '..|..|..|...|..|..|..':
+                music_list.append(1)
             i += 8
     return music_list
