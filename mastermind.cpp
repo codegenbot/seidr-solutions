63339428827b162ba9faba66730156054e272e16
beep boop <githubbot@vadim.me>
2024-07-23 13:13:14 +0200

    Solution 24 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/mp1y7rw1 Prompt:    13 |             ++guessFreq[guess[i] - 'A'];       |               ^~~~~~~~~ 985ed9ca-30ab-4fb3-b64a-96b244da9985.cpp:18:26: error: ‘codeFreq’ was not declar ed in this scope    18 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                          ^~~~~~~~ 985ed9ca-30ab-4fb3-b64a-96b244da9985.cpp:18:39: error: ‘guessFreq’ was not decla red in this scope    18 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                                       ^~~~~~~~~ 985ed9ca-30ab-4fb3-b64a-96b244da9985.cpp:18:22: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    18 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                      ^~~       |                      main 985ed9ca-30ab-4fb3-b64a-96b244da9985.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << whitePegs << endl << blackPegs << endl;       |     ^~~~ 985ed9ca-30ab-4fb3-b64a-96b244da9985.cpp:21:26: error: ‘endl’ was not declared i n this scope    21 |     cout << whitePegs << endl << blackPegs << endl;       |                          ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int main() {
@@ -13,8 +16,8 @@ int main() {
         if (code[i] == guess[i]) {
             ++blackPegs;
         } else {
-            ++codeFreq[code[i] - '0'];
-            ++guessFreq[guess[i] - '0'];
+            ++codeFreq[code[i] - 'A'];
+            ++guessFreq[guess[i] - 'A'];
         }
     }
     
@@ -23,5 +26,6 @@ int main() {
     }
     
     cout << whitePegs << endl << blackPegs << endl;
+    
     return 0;
 }
