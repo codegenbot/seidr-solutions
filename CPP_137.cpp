689c6a50e896625be381626cf49c820fcac1e8ae
beep boop <githubbot@vadim.me>
2024-07-23 07:08:06 +0200

    Solution 63 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/eq1t7en3 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,35 +1,35 @@
-```
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-    int x = boost::any_cast<int>(a);
-    int y = boost::any_cast<int>(b);
-    if (x > y)
-        return a;
-    else if (y > x)
-        return b;
-    else
-        return "None";
-}
-else if ((a.type() == typeid(double) || a.type() == typeid(float)) &&
-         (b.type() == typeid(double) || b.type() == typeid(float))) {
-    double x = boost::any_cast<double>(a);
-    double y = boost::any_cast<double>(b);
-    if (x > y)
-        return a;
-    else if (y > x)
-        return b;
-    else
-        return "None";
-}
-else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-         (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-    string s1 = boost::any_cast<string>(a);
-    string s2 = boost::any_cast<string>(b);
-    if (stod(s1) > stod(s2))
-        return a;
-    else if (stod(s2) > stod(s1))
-        return b;
-    else
-        return "None";
-}
-else
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<boost/any.hpp>
+using namespace std;
+boost::any compare_one(boost::any a, boost::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int)b > a.convert_to<int>() ? b : a;
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        string s = boost::any_cast<string>(b);
+        int b_int = atoi(strchr(s.c_str(), ',') + 1);
+        return a.convert_to<int>() > b_int ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string s = boost::any_cast<string>(b);
+        float b_float = stof(s);
+        return a.convert_to<float>() > b_float ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return a.convert_to<int>() > b.convert_to<int>() ? a : b;
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        return a.convert_to<float>() > b.convert_to<float>() ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        int b_int = boost::any_cast<int>(b);
+        string s = boost::any_cast<string>(a);
+        int a_int = atoi(strchr(s.c_str(), ',') + 1);
+        return a_int > b_int ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        float b_float = boost::any_cast<float>(b);
+        string s = boost::any_cast<string>(a);
+        int a_int = atoi(strchr(s.c_str(), ',') + 1);
+        return a_int > b_float ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        return boost::any_cast<string>(a) > boost::any_cast<string>(b) ? a : b;
+    }
     return "None";
+}
