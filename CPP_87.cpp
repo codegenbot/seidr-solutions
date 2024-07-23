7457b5912197c14a8fc1125ebab8fccb83a865c9
beep boop <githubbot@vadim.me>
2024-07-24 00:44:40 +0200

    Solution 18 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y8n5n4ed Prompt:       |                                   ^~~~~~ 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp:16:42: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp:16:49: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp:16:56: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp:15: 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp: In function ‘int main()’: 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp:28:20: error: ‘get_row’ was not declare d in this scope    28 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 2c5eec1a-fd91-477c-89ab-a23a065e2bc6.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,16 +1,11 @@
 #include <vector>
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
-    if (x < 1 || x > lst.size()) {
-        vector<vector<int>> result;
-        result.push_back({-1, "Invalid row number."});
-        return result;
-    }
-    vector<vector<int>> result;
+std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
+    std::vector<std::vector<int>> result;
     for (int i = 0; i < lst.size(); ++i) {
         if (x == i + 1) {
             for (int j = lst[i].size() - 1; j >= 0; --j) {
-                result.push_back({i, j});
+                result.push_back({lst[i][j]});
             }
         }
     }
