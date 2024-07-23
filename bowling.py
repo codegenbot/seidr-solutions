a0a144c72eb6895331eb7a76bf29f5c82bfd96b0
beep boop <githubbot@vadim.me>
2024-07-23 12:39:33 +0200

    Solution 60 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/7w0u2ghf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eabb2618-e1bb-46e7-bab5-a706fcedf 983.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,39 +1,35 @@
+```
 def bowling_score(game):
     score = 0
-    game_list = [int(i) if i.isdigit() else i for i in game.replace('/', '-').split()]
+    game_list = [int(i) if i.isdigit() else i for i in game.replace('-', '/').split()]
     frame = 0
+    roll = 0
     while len(game_list) > 1:
         if game_list[0] == 'X':
             score += 10
-            if game_list[1] == 'X':
-                score += 10
-                game_list.pop(0)
-                game_list.pop(0)
-            elif game_list[1] in ['/']:
-                score += 10 - int(game_list[1][1])
-                game_list.pop(0)
-                game_list.pop(0)
-            else:
-                frame_score = 10 + sum(map(int, game_list[1:3]))
-                game_list = game_list[3:]
-        elif game_list[0] in ['/']:
-            if game_list[1] == 'X':
-                score += 10 - int(game_list[0][1])
-                game_list.pop(0)
-                game_list.pop(0)
-                game_list.pop(0)
-            else:
-                frame_score = 10 - int(game_list[0][1]) + sum(map(int, game_list[1:3]))
-                game_list = game_list[3:]
+            game_list.pop(0)
+        elif game_list[0] == '/':
+            score += 10 - game_list[1]
+            game_list.pop(0)
+            game_list.pop(0)
         else:
-            if len(game_list) >= 2 and sum(map(int, game_list[:2])) == 10:
-                score += 10
-                game_list = game_list[2:]
-            else:
-                frame_score = sum(map(int, game_list[:2]))
-                game_list = game_list[2:]
-        score += frame_score
-    if len(game_list) > 0:
+            frame_score = sum(game_list[:2])
+            game_list = game_list[2:]
+            if frame_score == 10:
+                score += frame_score
+            elif frame_score < 10:
+                score += frame_score
+                if game_list and game_list[0] + game_list[1] == str(10):
+                    score += 10
+                    game_list.pop(0)
+                    game_list.pop(0)
+                else:
+                    while len(game_list) > 0 and game_list[0] < 3:
+                        score += game_list.pop(0)
+        frame += 1
+    if game_list:
         for i in range(len(game_list)):
-            score += int(game_list[i])
+            score += game_list[i]
     return score
+
+print(bowling_score('9/-39/X3/7/54622325'))
