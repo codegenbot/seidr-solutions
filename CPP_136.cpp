d1333aad715c87af29051c0145d5544556331b52
beep boop <githubbot@vadim.me>
2024-07-23 09:03:00 +0200

    Solution 25 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ghl9lyyn Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e1e222e2-35a6-4f99-b5b5-0913b60d2f38.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e1e222e2-35a6-4f99-b5b5-0913b60d2f38.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e1e222e2-35a6-4f99-b5b5-0913b60d2f38.cpp:13: e1e222e2-35a6-4f99-b5b5-0913b60d2f38.cpp: In function ‘int main()’: e1e222e2-35a6-4f99-b5b5-0913b60d2f38.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ e1e222e2-35a6-4f99-b5b5-0913b60d2f38.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,17 +1,5 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a[0] == b[0] && a[1] == b[1];
-} 
-
-vector<int> largest_smallest_integers(vector<int> lst){
-    int largest_negative = INT_MIN, smallest_positive = INT_MAX;
-    for (int num : lst) {
-        if (num < 0 && num > largest_negative) {
-            largest_negative = num;
-        } else if (num > 0 && num < smallest_positive) {
-            smallest_positive = num;
-        }
-    }
-    return {largest_negative, smallest_positive};
 }
