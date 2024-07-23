77a7777c6661714ac3db8ffa8e7816393292aa20
beep boop <githubbot@vadim.me>
2024-07-23 15:15:23 +0200

    Solution 10 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/d77g68f6 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,26 +1,50 @@
-int bowlingScore(string s) {
+int bowlingScore(string str) {
     int score = 0;
-    int frame = 1;
-    for (char c : s) {
-        if (c == 'X') {
-            score += 30;
-            frame++;
-        } else if (c == '/') {
-            score += 10 + (frame < 9 ? 10 - ('0' - c) : 10);
-            frame++;
-        } else {
-            int points = '0' - c;
-            if (points == 1 || points == 2) {
-                score += 10 + points;
-                frame++;
-            } else {
-                score += 10 + points;
-                points--;
-                if (frame < 9) {
-                    score += 10 + points;
-                    frame++;
+    int roll1, roll2;
+
+    for (int frame = 0; frame < 10; frame++) {
+        if (str[0] == '/') {
+            string spareStr = "";
+            for (char c : str.substr(1)) {
+                if (c != '/') {
+                    spareStr += c;
+                } else {
+                    break;
                 }
             }
+            score += 10 + calculateScore(spareStr);
+            str.erase(0, spareStr.length() + 1);
+        } else if (str[0] == 'X') {
+            score += 10;
+            str.erase(0, 2);
+        } else {
+            roll1 = str[0] - '0';
+            str.erase(0, 1);
+
+            if (str[0] == '/') {
+                score += roll1 + 10;
+                str.erase(0, 1);
+            } else if (str[0] == 'X') {
+                score += roll1 + 10;
+                str.erase(0, 2);
+            } else {
+                roll2 = str[0] - '0';
+                str.erase(0, 1);
+                score += roll1 + roll2;
+            }
+        }
+    }
+
+    return score;
+}
+
+int calculateScore(string str) {
+    int score = 0;
+    for (char c : str) {
+        if (c == '/') {
+            break;
+        } else {
+            score += c - '0';
         }
     }
     return score;
