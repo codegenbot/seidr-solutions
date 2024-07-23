7bfb844c9d12f2d60366b77f4bebe47ad7715265
beep boop <githubbot@vadim.me>
2024-07-23 09:19:45 +0200

    Solution 14 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/sw8yxj3m Prompt: 51724715-f653-409e-9529-6df8268b72dd.cpp: In function ‘double find_zero(std::vec tor<double>)’: 51724715-f653-409e-9529-6df8268b72dd.cpp:23:1: warning: control reaches end of n on-void function [-Wreturn-type]    23 | }       | ^ Program did not receive expected input

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <math.h>
-#include <vector>
-#include <iostream>
+```
+#include<stdio.h>
+#include<math.h>
+#include<vector>
 using namespace std;
 
 double poly(vector<double> xs, double x){
@@ -15,11 +15,11 @@ double poly(vector<double> xs, double x){
 }
 
 double find_zero(vector<double> xs){
-    if(xs.size() % 2 != 0) return -1; 
+    if(xs.size() % 2 != 0) 
+        return -1; 
     double x = 1.0; 
     while(abs(poly(xs, x)) > 1e-9){ 
         x -= poly(xs, x)/poly(vector<double>(xs.begin()+1, xs.end()), x); 
     }
-    cout << fixed << setprecision(2) << x << endl;
     return x;
 }
