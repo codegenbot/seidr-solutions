ff4f88d755bfe8caf142394ee4918431ade2fd6e
beep boop <githubbot@vadim.me>
2024-07-23 08:53:41 +0200

    Solution 24 of Python/11, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yngnl4id Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6767b5f8-74f9-4634-ac35-5c3557e01 0b0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_11.py
+++ Python_11.py
@@ -1,13 +1,8 @@
-from typing import List
-
-
 def string_xor(a: str, b: str) -> str:
     result = ""
-    for i in range(len(a)):
-        if a[i] == "1" and b[i] == "0":
-            result += "1"
-        elif a[i] == "0" and b[i] == "1":
-            result += "1"
+    for i in range(max(len(a), len(b))):
+        if i < len(a) and i < len(b):
+            result = str(int(a[-1-i] != b[-1-i])) + result
         else:
-            result += "0"
+            result = a[i] if i < len(a) else b[i]
     return result
