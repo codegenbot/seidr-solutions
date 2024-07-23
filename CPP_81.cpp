5d1259245ea2f57f9aa7252379ca0a2151e3b965
beep boop <githubbot@vadim.me>
2024-07-23 13:16:57 +0200

    Solution 71 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xfvdw2bt Prompt:       |                                     ^~~~~~ dd4949b9-f33f-46d3-9cb1-fada83e352d5.cpp:53:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from dd4949b9-f33f-46d3-9cb1-fada83e352d5.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from dd4949b9-f33f-46d3-9cb1-fada83e352d5.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ dd4949b9-f33f-46d3-9cb1-fada83e352d5.cpp:53:44: error: ‘b’ was not declared in t his scope    53 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dd4949b9-f33f-46d3-9cb1-fada83e352d5.cpp:61:5: error: redefinition of ‘int main( )’    61 | int main(){       |     ^~~~ dd4949b9-f33f-46d3-9cb1-fada83e352d5.cpp:43:5: note: ‘int main()’ previously def ined here    43 | int main() {       |     ^~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -40,11 +40,10 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     return letter_grades;
 }
 
-int main_test() {
+int main() {
     std::vector<float> grades = {0, 0.7};
     std::vector<std::string> expectedLetterGrades = {"F", "D"};
     if (!issame(numerical_letter_grade(grades), expectedLetterGrades)) {
         return 1;
     }
     return 0;
-}
