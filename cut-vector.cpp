85d04c58a7441fcf7cd844a9657689856b91ae11
beep boop <githubbot@vadim.me>
2024-07-23 14:02:10 +0200

    Solution 88 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/2x3zn4j9 Prompt: 5ba71bb7-a089-4d05-87ed-ca4e155335ea.cpp:1:1: error: ‘pair’ does not name a type     1 | pair<vector<int>, vector<int>> cutVector(vector<int> v) {       | ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,30 +1,30 @@
 #include <utility>
 
-vector<int> left;
-vector<int> right;
-
-int minDiff = INT_MAX;
-int cutIndex = 0;
-for (int i = 1; i <= v.size(); i++) {
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int minDiff = INT_MAX;
+    int cutIndex = 0;
+    for (int i = 1; i <= v.size(); i++) {
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j < i; j++) {
+            leftSum += v[j];
+        }
+        for (int j = i; j < v.size(); j++) {
+            rightSum += v[j];
+        }
+        if (leftSum == rightSum) {
+            return {vector<int>(), vector<int>(v.begin(), v.end())};
+        } else if (abs(leftSum - rightSum) < minDiff) {
+            minDiff = abs(leftSum - rightSum);
+            cutIndex = i;
+        }
+    }
     int leftSum = 0, rightSum = 0;
-    for (int j = 0; j < i; j++) {
+    for (int j = 0; j < cutIndex; j++) {
         leftSum += v[j];
     }
-    for (int j = i; j < v.size(); j++) {
+    for (int j = cutIndex; j < v.size(); j++) {
         rightSum += v[j];
     }
-    if (leftSum == rightSum) {
-        return {vector<int>(v.begin(), v.begin()), vector<int>(v.begin(), v.end())};
-    } else if (abs(leftSum - rightSum) < minDiff) {
-        minDiff = abs(leftSum - rightSum);
-        cutIndex = i;
-    }
+    return {vector<int>(v.begin(), v.begin() + cutIndex),
+            vector<int>(v.begin() + cutIndex, v.end())};
 }
-int leftSum = 0, rightSum = 0;
-for (int j = 0; j < cutIndex; j++) {
-    leftSum += v[j];
-}
-for (int j = cutIndex; j < v.size(); j++) {
-    rightSum += v[j];
-}
-return {vector<int>(v.begin(), v.begin() + cutIndex), vector<int>(v.begin() + cutIndex, v.end())};
