d35900c66f4b624488f949f01e9882587bb5521a
beep boop <githubbot@vadim.me>
2024-07-23 23:24:52 +0200

    Solution 24 of CPP/65, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ds1lnt91 Prompt: 17de868d-8dc3-4fc4-86ce-53cad6085e1b.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string circular_shift(int x, int shift) {       | ^~~~~~ In file included from 17de868d-8dc3-4fc4-86ce-53cad6085e1b.cpp:12: 17de868d-8dc3-4fc4-86ce-53cad6085e1b.cpp: In function ‘int main()’: 17de868d-8dc3-4fc4-86ce-53cad6085e1b.cpp:14:13: error: ‘circular_shift’ was not declared in this scope    14 |     assert (circular_shift(11, 101) == "11");       |             ^~~~~~~~~~~~~~

--- CPP_65.cpp
+++ CPP_65.cpp
@@ -1,22 +1,12 @@
-#include <iostream>
-#include <sstream>
 #include <string>
 
-using namespace std;
-
 string circular_shift(int x, int shift) {
-    ostringstream str;
-    str << x; 
-    int n = str.str().length();
+    string str = to_string(x);
+    int n = str.length();
     if (shift >= n)
-        return str.str();
+        return str;
     else {
-        string result = str.str().substr(shift) + str.str().substr(0, shift);
+        string result = str.substr(shift) + str.substr(0, shift);
         return result;
     }
-}
-
-int main() {
-    assert(circular_shift(11, 101) == "11");
-    return 0;
 }
