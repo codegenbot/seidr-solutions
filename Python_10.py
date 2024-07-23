2ebddeb9ac188ab715fe38d565b79b10640edcc3
beep boop <githubbot@vadim.me>
2024-07-23 23:58:41 +0200

    Solution 35 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/d90l9i2w Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c9d67915-e844-416c-8c13-fbdc27fb5 0fe.py", line 10, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c9d67915-e844-416c-8c13-fbdc27fb5 0fe.py", line 7, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,15 +1,5 @@
 def make_palindrome(s: str) -> str:
     if len(s) % 2 == 0:
-        middle_index = len(s) // 2
-        return (
-            s[:middle_index].lower() + s[middle_index].lower() + s[:middle_index][::-1]
-        )
+        return s[: len(s) // 2] + s[: len(s) // 2][::-1]
     else:
-        middle_index = len(s) // 2
-        return (
-            s[0].lower()
-            + s[1 : middle_index + 1].lower()
-            + s[middle_index].lower()
-            + s[middle_index::-1].lower()
-            + s[0].upper()
-        )
+        return s + s[::-1]
