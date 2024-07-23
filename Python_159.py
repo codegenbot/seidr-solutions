d748e713d795a547d49b0e5970aab04e7c924ae5
beep boop <githubbot@vadim.me>
2024-07-23 17:52:37 +0200

    Solution 10 of Python/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2p8e5qf6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/42c11a86-f9d0-4180-932e-ec82b25e7 fa1.py", line 10, in <module>     check(eat)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/42c11a86-f9d0-4180-932e-ec82b25e7 fa1.py", line 7, in check     assert eat(4, 5, 1) == [5, 0], "Error"            ^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_159.py
+++ Python_159.py
@@ -1,3 +1,3 @@
 def eat(number, need, remaining):
-    total_eaten = min(number, need) if number <= remaining else min(need, remaining)
+    total_eaten = min(number, need)
     return [total_eaten, remaining - total_eaten]
