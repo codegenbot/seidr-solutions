bc2042d79444f2075f8c8cd55a78622cc78fc15d
beep boop <githubbot@vadim.me>
2024-07-23 14:05:40 +0200

    Solution 21 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ie0wp8mq Prompt: this scope    26 |     cin >> s;       |     ^~~ e8e2cc49-b876-48bb-9567-ea3f2d74aa7b.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; e8e2cc49-b876-48bb-9567-ea3f2d74aa7b.cpp:27:5: error: ‘cout’ was not declared in  this scope    27 |     cout << (solveBoolean(s) ? "True" : "False") << endl;       |     ^~~~ e8e2cc49-b876-48bb-9567-ea3f2d74aa7b.cpp:27:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? e8e2cc49-b876-48bb-9567-ea3f2d74aa7b.cpp:27:53: error: ‘endl’ was not declared i n this scope    27 |     cout << (solveBoolean(s) ? "True" : "False") << endl;       |                                                     ^~~~ e8e2cc49-b876-48bb-9567-ea3f2d74aa7b.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,24 +1,25 @@
-````
 #include <iostream>
 #include <string>
 using namespace std;
 
 bool solveBoolean(string s) {
-    bool res = false;
+    stack<char> st;
     for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T')
-            res = true;
-        else if (s[i] == 'F')
-            return !res;
-        else if (s[i] == '|') {
-            bool temp = res;
-            res = false;
-            while (i < s.length() && s[i] != '&') i++;
+        if (s[i] == '&') {
+            while (!st.empty() && st.top() == '&') {
+                st.pop();
+            }
+            if (st.empty()) return false;
+        } else if (s[i] == '|') {
+            while (!st.empty() && st.top() == '|') {
+                st.pop();
+            }
+            if (st.empty()) return true;
+        } else {
+            st.push(s[i]);
         }
-        else if (s[i] == '&') 
-            res = res & (i == s.length() - 1 || s[i+1] == 'F');
     }
-    return res;
+    return !st.empty();
 }
 
 int main() {
@@ -26,4 +27,3 @@ int main() {
     cin >> s;
     cout << (solveBoolean(s) ? "True" : "False") << endl;
     return 0;
-}
