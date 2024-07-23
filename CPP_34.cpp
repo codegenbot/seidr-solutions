aae4fe871404357844c91dfdde828954e5b3e8fe
beep boop <githubbot@vadim.me>
2024-07-23 22:13:28 +0200

    Solution 48 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8dwusk5j Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_set.h:61,                  from /usr/include/c++/11/set:61,                  from 01cffaa2-8caa-4083-ba4e-d84a4bb8f285.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <set>
 #include <vector>
 
@@ -6,12 +7,14 @@ using namespace std;
 bool issame(vector<int> v1, vector<int> v2) {
     if (v1.size() != v2.size())
         return false;
-    set<vector<int>> s({v1, v2});
+    set<vector<int>> s;
+    s.insert(v1);
+    s.insert(v2);
     return s.size() == 1;
 }
 
 vector<int> unique(vector<int> l) {
     set<int> s(l.begin(), l.end());
-    vector<int> res(std::initializer_list<int>{s.begin(), s.end()});
+    vector<int> res(s.begin(), s.end());
     return res;
 }
