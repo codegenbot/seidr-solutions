45ee297267032a6cdf2201b55788bce441e25e4a
beep boop <githubbot@vadim.me>
2024-07-23 18:59:16 +0200

    Solution 36 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/u9x1te1q Prompt: Program failed

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,8 +1,9 @@
 int specialFilter(std::vector<int> nums) {
     int count = 0;
     for (int x : nums) {
-        if ((std::abs(x) % 11) == 1 || (std::abs(x) % 11) == 0) {
+        if ((x % 11) % 2 != 0) { 
             count++;
         }
     }
     return count;
+}
