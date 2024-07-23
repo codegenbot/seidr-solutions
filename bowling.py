54dcbc90b32c80fdb2f763305313e5c510222554
beep boop <githubbot@vadim.me>
2024-07-23 20:25:05 +0200

    Solution 20 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/14zv9x1k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/646d0380-277d-4b73-9af2-858c86a16 f5e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -1,34 +1,17 @@
 ```
-def bowling_score(game):
+import re
+def bowling_score(frames):
     score = 0
-    frame = 0
-    roll = 0
+    roll = [int(x) for x in re.sub('/|-', '', frames)]
     i = 0
-    while i < len(game):
-        if game[i] == "X":
-            score += 30
-            frame += 1
+    while i < len(roll):
+        if roll[i] == 10:
+            score += roll[i]
             i += 1
-            if frame < 10:
-                if game[i] == "X":
-                    score += 30
-                    frame += 1
-                else:
-                    score += int(game[i])
-                i += 1
-        elif game[i] == "/":
-            score += roll * (int(game[i-1]) + int(game[i+1]))
-            frame += 1
+        elif i + 1 < len(roll):
+            score += min(roll[i], 10-roll[i-1]) + roll[i]
             i += 2
         else:
-            if game[i] == "X" or game[i+1] == "X" or game[i:i+2] == "//":
-                continue
-            score += int(game[i])
-            roll += 1
-            if roll < 2:
-                i += 1
-            elif game[i] == "/":
-                score += roll * (int(game[i-1]) + int(game[i+1]))
-                frame += 1
-                i += 2
+            score += roll[i]
+            i += 1
     return score
