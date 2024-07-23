f63d33dbe891e040af83cdbf4bce6a1c6e87009b
beep boop <githubbot@vadim.me>
2024-07-24 01:21:04 +0200

    Solution 32 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bjwpnh7h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/150d5d3b-427f-4721-b925-3c8b3ab52 52f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,5 +1,5 @@
 ```
-def find_closest_elements(numbers):
+def find_closest_elements() -> tuple:
     while True:
         user_input = input("Enter space-separated numbers or two specific numbers separated by comma (,): ")
         try:
