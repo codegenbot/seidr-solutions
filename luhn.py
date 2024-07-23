b97367869760535574774a6e5307c39cc38cb08e
beep boop <githubbot@vadim.me>
2024-07-23 13:23:51 +0200

    Solution 21 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/5poiajdc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/412583f9-4ce9-43ed-9bfb-b8c9d70b7 9e7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -1,8 +1,8 @@
 def luhn(card):
-    card = list(map(int, card.replace(" ", "")))
+    card = list(map(int, str(card).split()))
     total = 0
     for i in range(len(card)):
-        if (i % 2) == 1:
+        if i % 2 == 1:
             card[i] *= 2
             if card[i] > 9:
                 card[i] -= 9
