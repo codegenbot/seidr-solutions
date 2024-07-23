b16397b929c700da02ad001cf92693112b602702
beep boop <githubbot@vadim.me>
2024-07-23 10:52:49 +0200

    Solution 51 of Python/49, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d32772e-012a-4a81-81cb-77026d4eb 1bb.py", line 13, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d32772e-012a-4a81-81cb-77026d4eb 1bb.py", line 10, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d32772e-012a-4a81-81cb-77026d4eb 1bb.py", line 2, in modp     if gcd(n, p) != 1:        ^^^ NameError: name 'gcd' is not defined

--- Python_49.py
+++ Python_49.py
@@ -1,2 +1,13 @@
 def modp(n: int, p: int):
-    return pow(n, p - 2, p) * n % p
+    def gcd(a: int, b: int) -> int:
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+    if gcd(n, p) != 1:
+        return None
+    result = pow(n, (p - 1), p)
+    for _ in range(p - 2):
+        n = (n * result) % p
+        result = (result * 2) % p
+    return n
