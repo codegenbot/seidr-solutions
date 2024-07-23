c0b13ff7b65f744b164f087a190d884183433c44
beep boop <githubbot@vadim.me>
2024-07-23 16:37:31 +0200

    Solution 52 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xc29kah6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce67ab30-97d5-4bc9-826c-ee3fbd1e5 d2e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,2 +1,16 @@
-def digitSum(s):
-    return sum(int(c) for c in s if c.isdigit())
+def findErrorNums(nums):
+    seen = {}
+    repeat = None
+    missing = None
+
+    for num in nums:
+        if num not in seen:
+            seen[num] = True
+        else:
+            repeat = num
+
+    for i in range(1, len(nums) + 2):
+        if i not in seen:
+            missing = i
+
+    return [repeat, missing]
