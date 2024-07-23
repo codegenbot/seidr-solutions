ea4fc2bc9b30db578a9376219473491f52c0752b
beep boop <githubbot@vadim.me>
2024-07-23 09:12:13 +0200

    Solution 22 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/7glmws9i Prompt:       |                    ^~~ 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp:9: 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp: In function ‘int main()’: 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp:19:20: error: ‘compare’ was not declare d in this scope    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 743ce00f-d772-4be4-8e9b-73e773d241f7.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,7 +1,5 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b);
-
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); ++i) {
@@ -10,19 +8,9 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
+bool issame(vector<int> a, vector<int> b);
 
 int main() {
-    vector<int> game = {1, 2, 3};
-    vector<int> guess = {2, 3, 4};
-
-    vector<int> result = compare(game, guess);
-
-    for (int num : result) {
-        cout << num << " ";
-    }
-
+    // Code for main function
     return 0;
 }
