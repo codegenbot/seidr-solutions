b070cc64322708c6cbeb7fafe996ff29d53e9b1d
beep boop <githubbot@vadim.me>
2024-07-23 12:35:43 +0200

    Solution 40 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a1rebcqs Prompt:       |     ^~~~ f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp:17:5: error: expected unqualified-id be fore ‘return’    17 |     return 0;       |     ^~~~~~ f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp:18:1: error: expected declaration befor e ‘}’ token    18 | }       | ^ f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp:21:10: error: ‘int main()’ redeclared a s different kind of entity    21 | int main(){       |          ^ f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main       |     ^~~~ In file included from f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp:20: f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp: In function ‘int main()’: f8f22a3e-995a-4df6-82aa-fe3de111217d.cpp:22:13: error: ‘add’ was not declared in  this scope    22 |     assert (add({4, 4, 6, 8}) == 12);       |             ^~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -1,19 +1,10 @@
-int add(const vector<int>& numbers) {
-    int sum = 0;
-    for(int i=0; i<numbers.size(); i++) {
-        if(i % 2 != 0) 
-            sum += numbers[i];
-    }
-    return sum;
-}
-
 int main() {
     vector<int> numbers;
     int num;
-
+    
     cout << "Enter some numbers (enter -1 when you are done):" << endl;
-
-    for(;;){
+    
+    for(;;) {
         cin >> num;
         if(num == -1) break;
         numbers.push_back(num);
@@ -23,4 +14,14 @@ int main() {
     cout << "Sum of odd-indexed numbers: " << result << endl;
 
     return 0;
+}
+
+int add(const vector<int>& numbers) {
+    int sum = 0;
+    for(int i = 0; i < numbers.size(); i++) {
+        if(i % 2 != 0) { 
+            sum += numbers[i];
+        }
+    }
+    return sum;
 }
