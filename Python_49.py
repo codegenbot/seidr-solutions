f4551d6a19ccce00baeb49517c6280113c0b3ac0
beep boop <githubbot@vadim.me>
2024-07-23 10:23:59 +0200

    Solution 26 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a34041df-f04a-423a-bd9f-e1c890709 a6b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,13 +1,7 @@
 ```
 def modp(n: int, p: int):
     if gcd(n, p) == 1:
-        result = 1
-        while p > 1:
-            if p & 1:
-                result = (result * n) % p
-            p >>= 1
-            n = (n * n) % p
-        return result
+        return pow(n, inv((p - 1), p), p)
     else:
         return None
 
@@ -15,3 +9,16 @@ def gcd(a: int, b: int):
     while b:
         a, b = b, a % b
     return a
+
+def inv(a: int, m: int):
+    t, newt = 0, 1
+    r, newr = m, a
+    while newr != 0:
+        quotient = r // newr
+        t, newt = newt, t - quotient * newt
+        r, newr = newr, r - quotient * newr
+    if r > 1:
+        return None
+    if t < 0: 
+        t = t + m
+    return t
