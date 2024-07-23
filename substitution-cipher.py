d4318ce8a9b5a0e55cd2d8c61b81f0ac04423fce
beep boop <githubbot@vadim.me>
2024-07-23 15:22:07 +0200

    Solution 63 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/yxmn0web Prompt: it must return ffkkhdvpdpvtdzdkffwliwzxl for input srodaptbmuljw cdthviwfkzplx bbmmdralrlaorurmbbtjptuwj, but it returns .

--- substitution-cipher.py
+++ substitution-cipher.py
@@ -1,13 +1,17 @@
 ```
 def substitution_cipher():
-    cipher_map = input().split(' ')
-    text_to_decrypt = input()
+    cipher_map = str(input())
+    text_to_decrypt = str(input())
     decrypted_text = ""
 
+    map_list = [i for i in zip(*[list(cipher_map[i:i+2]) for i in range(0, len(cipher_map), 2)]])
+    
     for char in text_to_decrypt:
-        if char in cipher_map[0]:
-            index = cipher_map[0].index(char)
-            decrypted_text += cipher_map[1][index]
+        if char in cipher_map:
+            for pair in map_list:
+                if char in pair:
+                    decrypted_text += ''.join(pair).replace(char,'')
+                    break
         else:
             decrypted_text += char
 
