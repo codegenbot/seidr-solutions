7553cf7e04716952ad83907486b74172eff7d272
beep boop <githubbot@vadim.me>
2024-07-23 23:20:09 +0200

    Solution 14 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ooca07p6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a64da7b-8bd6-48c4-8e98-c3bc727a2 956.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,24 +1,45 @@
+from typing import List
+
+
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
-    temp = ''
-    
-    for char in paren_string:
-        if char == ' ':
-            continue
-        
-        if char == '(':
-            if temp and temp[-1] != '(':
-                result.append(temp)
-                temp = ''
-            temp += char
-        elif char == ')':
-            if not temp or temp[-1] != '(':
-                result.append(temp + ')')
-                temp = ''
-            else:
-                temp += char
-                
-    if temp:
-        result.append(temp)
+    stack = []
+    temp = ""
 
-    return result
+    for char in paren_string:
+        if char == " ":
+            continue
+        if char == "(":
+            if stack and stack[-1] == ")":
+                temp += char
+            else:
+                stack.append(char)
+                temp += char
+        elif char == ")":
+            if not stack or stack[-1] != "(":
+                result.append(temp)
+                temp = ""
+                while stack:
+                    if stack[0] == "(":
+                        result.append(")")
+                    else:
+                        result[-1] += ")"
+                    stack.pop()
+                return []
+            stack.pop()
+            temp += char
+        else:
+            while stack and char != stack[-1]:
+                temp += char
+                stack.pop()
+            if not stack:
+                result.append(temp)
+                temp = ""
+    while stack:
+        if stack[0] == "(":
+            result.append(")")
+        else:
+            result[-1] += ")"
+        stack.pop()
+
+    return [temp] + result
