6f92b9d5f671485a116c31d19471748f57ab2776
beep boop <githubbot@vadim.me>
2024-07-24 01:21:59 +0200

    Solution 65 of Python/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lqjbjsij Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c918bb15-f0b6-4f82-9517-345749b62 15e.py", line 1     return [     ^^^^^^^^ SyntaxError: 'return' outside function

--- Python_14.py
+++ Python_14.py
@@ -1,14 +1,2 @@
-def all_prefixes(string: str) -> list:
-    result = []
-    for i in range(len(string)+1):
-        if i == 0:
-            result.append("")
-        else:
-            prefix = string[:i]
-            first_letter_upper = string[0].isupper()
-            remaining_lowercase = all(c.islower() for c in string[1:i])
-            if not first_letter_upper or remaining_lowercase:
-                result.append(prefix.lower())
-            else:
-                result.append(prefix)
-    return result
+def solution(s):
+    return [s[:i].lower() if s[i - 1].islower() else s[:i].upper() for i in range(1, len(s) + 1)]
