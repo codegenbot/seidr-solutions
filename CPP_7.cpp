366e807665e5484c9bc4ca73f5b017e3b0f83a9a
beep boop <githubbot@vadim.me>
2024-07-23 09:07:11 +0200

    Solution 36 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/klkuaq7w Prompt: )’    34 | int main(){       |     ^~~~ 564c8804-715e-4d37-89d0-a02377133ffd.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~ In file included from 564c8804-715e-4d37-89d0-a02377133ffd.cpp:25: 564c8804-715e-4d37-89d0-a02377133ffd.cpp: In function ‘int main()’: 564c8804-715e-4d37-89d0-a02377133ffd.cpp:35:39: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    35 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~       |                                       |       |                                       <brace-enclosed initializer list> 564c8804-715e-4d37-89d0-a02377133ffd.cpp:12:71: note:   initializing argument 1 of ‘int filter_by_substring(int, int)’    12 | r<std::string> filter_by_substring(std::vector<std::string> arr, std::st ring sub) {       |                                    ~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,4 +1,4 @@
-```c++
+```cpp
 #include <vector>
 #include <assert.h>
 
@@ -16,9 +16,10 @@ std::vector<std::string> filter_by_substring(std::vector<std::string> arr, std::
         if (str.find(sub) != std::string::npos) result.push_back(str);
     }
     return result;
+
 }
 
 int main() {
-    assert(issame(filter_by_substring({{"grunt", "trumpet", "prune", "gruesome"}}, {"grunt", "prune"}));
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
     return 0;
 }
