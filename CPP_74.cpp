b2423db735d12c8af36c4ba6c957a1388f67816d
beep boop <githubbot@vadim.me>
2024-07-23 23:27:29 +0200

    Solution 14 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jao9uj68 Prompt:       |                              ^~~~~~ 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp:20:37: error: ‘string’ was not declared  in this scope    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp:19: 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp: In function ‘int main()’: 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp:29:20: error: ‘total_match’ was not dec lared in this scope    29 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 85c78c29-91e8-4458-b70c-4c431f57cda1.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,3 +1,11 @@
+bool issame(vector<string> a,vector<string>b) {
+    for (int i = 0; i < a.size() || i < b.size(); i++) {
+        if (i >= a.size()) return false;
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
@@ -11,5 +19,12 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else return lst1;
+    else {
+        if(issame(lst1,lst2)) return lst1;
+        for(const string& s : lst1){
+            vector<string> v = {s};
+            v.insert(v.end(),lst2.begin(),lst2.end());
+            return v;
+        }
+    }
 }
