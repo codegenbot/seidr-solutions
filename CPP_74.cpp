c54f73b40f6d3143fa6c8cdb6bf3e958ebd510ed
beep boop <githubbot@vadim.me>
2024-07-23 14:47:26 +0200

    Solution 98 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4d2aq60w Prompt:       |           ^~~~~~ 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp:62:44: error: ‘b’ was not declared in t his scope    62 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp:70:5: error: redefinition of ‘int main( )’    70 | int main(){       |     ^~~~ 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp:55:5: note: ‘int main()’ previously def ined here    55 | int main() {       |     ^~~~ In file included from 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp:61: 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp: In function ‘int main()’: 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp:71:20: error: ‘total_match’ was not dec lared in this scope    71 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 7b9d1897-72de-45bd-b9f5-7f33f598599b.cpp:71:19: error: ‘issame’ cannot be used a s a function    71 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -9,23 +9,43 @@ bool issame(vector<string> a, vector<string> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
 
-    return a == b;
+    int i = 0, j = 0;
+    
+    while (i < a.size() && j < b.size()) {
+        if (a[i] == b[j]) {
+            return true; 
+            i++;
+            j++;
+        } else if (a[i] < b[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    return false;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+vector<string> total_match(vector<vector<string>> lst) {
     vector<string> result;
 
-    for (const auto& str : lst1) {
+    for (const auto& sublst : lst) {
+        vector<string> temp;
+        transform(sublst.begin(), sublst.end(), back_inserter(temp), ::tolower);
+        sort(temp.begin(), temp.end());
+        
         bool found = false;
-        for (const auto& word : lst2) {
-            if (issame({str}, {word})) {
+        for (int i = 0; i < result.size(); i++) {
+            if (issame(result[i], temp)) {
                 found = true;
                 break;
             }
         }
 
         if (!found) {
-            result.push_back(str);
+            for (const auto& str : temp) {
+                result.push_back(str);
+            }
         }
     }
 
@@ -33,9 +53,9 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
 }
 
 int main() {
-    vector<string> result = total_match({{"this"}}, {});
-    for (const auto& str : result) {
-        cout << str << endl;
-    }
+    vector<vector<string>> lst = {{"this"}};
+    
+    vector<string> result = total_match(lst);
+    
     return 0;
 }
