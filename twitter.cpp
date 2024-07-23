913200d740e565515c0c8a4ea508e40f108ca5a3
beep boop <githubbot@vadim.me>
2024-07-23 23:18:16 +0200

    Solution 10 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/e1s71gpw Prompt: it must return Too many characters for input iV^'FFFoac`+bm]Jku2S5N6Z_j\"ETGh5}g\)U:n4$gRGuCYy)Wd5vR^rw|s(%^uY&2V],FDkrPGFS+axpwt]'G7X_]|vY<98]6a4g].EXU>{UM[E<{q7EHnm,6}yn-CcD7SqBbg2SI<ou?tUHQbHQ]VLxy]h#]'{t/O]5}=V, but it returns .

--- twitter.cpp
+++ twitter.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <string>
-
 int main() {
     std::string tweet;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     getline(std::cin, tweet);
-
     if (tweet.empty()) {
         std::cout << "You didn't type anything" << std::endl;
     } else if (tweet.length() > 140) {
@@ -12,6 +10,5 @@ int main() {
     } else {
         std::cout << "Your tweet has " << tweet.length() << " characters" << std::endl;
     }
-
     return 0;
 }
