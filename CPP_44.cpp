b7082d957fef9f5d807c7914fb2f80a7dc7c814e
beep boop <githubbot@vadim.me>
2024-07-23 09:54:21 +0200

    Solution 7 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9spe98jc Prompt: e, std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type, _CharT) [with  _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>;  std::__cxx11::basic_string<_CharT, _Traits, _Alloc>::size_type = long unsigned int]’  1749 |       insert(size_type __pos, size_type __n, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1749:53: note:   no known conversion for  argument 3 from ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’} to ‘char ’  1749 |       insert(size_type __pos, size_type __n, _CharT __c)       |                                              ~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h:1767:7: note: candidate: ‘std::__cxx11:: basic_string<_CharT, _Traits, _Alloc>::iterator std::__cxx11::basic_string<_Char T, _Traits, _Alloc>::insert(std::__cxx11::basic_string<_CharT, _Traits, _Alloc>: :__const_iterator, _CharT) [with _CharT = char; _Traits = std::char_traits<char> ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:7: note:   candidate expects 2 argu ments, 3 provided

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,11 +1,2 @@
-string change_base(int x, int base) {
-    if (x == 0)
-        return "0";
-    string res = "";
-    while (x > 0) {
-        int r = x % base;
-        res.insert(0, 1, r % 10 + '0');
-        x /= base;
-    }
-    return res;
-}
+``` 
+res.insert(0, 1, r % 10 + '0');
