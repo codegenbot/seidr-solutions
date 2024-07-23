53503c1b853988538141e04004e3f28bb6be6856
beep boop <githubbot@vadim.me>
2024-07-23 22:42:02 +0200

    Solution 28 of paired-digits, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/jj3t4l1e Prompt:     1 | ```cpp ac6d762f-24ef-4260-b22a-c0163c91c22a.cpp: In function ‘int pairedDigits(const in t&)’: ac6d762f-24ef-4260-b22a-c0163c91c22a.cpp:9:27: error: request for member ‘size’ in ‘s’, which is of non-class type ‘const int’     9 |     for (int i = 0; i < s.size() - 1; i++) {       |                           ^~~~ ac6d762f-24ef-4260-b22a-c0163c91c22a.cpp:10:16: error: request for member ‘subst r’ in ‘s’, which is of non-class type ‘const int’    10 |         if ((s.substr(i, 1)) == (s.substr(i+1, 1))) {       |                ^~~~~~ ac6d762f-24ef-4260-b22a-c0163c91c22a.cpp:10:36: error: request for member ‘subst r’ in ‘s’, which is of non-class type ‘const int’    10 |         if ((s.substr(i, 1)) == (s.substr(i+1, 1))) {       |                                    ^~~~~~ ac6d762f-24ef-4260-b22a-c0163c91c22a.cpp:11:22: error: invalid types ‘const int[ int]’ for array subscript    11 |             sum += (s[i] - '0') + (s[i + 1] - '0');       |                      ^ ac6d762f-24ef-4260-b22a-c0163c91c22a.cpp:11:37: error: invalid types ‘const int[ int]’ for array subscript    11 |             sum += (s[i] - '0') + (s[i + 1] - '0');       |                                     ^

--- paired-digits.cpp
+++ paired-digits.cpp
@@ -1,3 +1,6 @@
+```cpp
+#include <string>
+
 int main() {
     int result = pairedDigits("123333");
     return 0;
@@ -6,8 +9,8 @@ int main() {
 int pairedDigits(const std::string& s) { 
     int sum = 0;
     for (int i = 0; i < s.size() - 1; i++) {
-        if ((s.substr(i, 1)) == (s.substr(i+1, 1))) {
-            sum += (s[i] - '0') + (s[i + 1] - '0');
+        if (s[i] == s[i+1]) {
+            sum += (s[i] - '0') * 2;
         }
     }
     return sum;
