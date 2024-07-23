06f19ff23f814229e8256b0a3b413e098d5be14a
beep boop <githubbot@vadim.me>
2024-07-23 22:25:16 +0200

    Solution 94 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/tgdi9g84 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7847ca74-a3c8-42a8-b825-1e77f6c94 9cd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,5 +1,5 @@
+```
 import ast
-
 
 def solve_boolean(expression):
     if expression == "T":
@@ -7,26 +7,23 @@ def solve_boolean(expression):
     elif expression == "F":
         return False
 
-    while True:
+    while "|" in expression or "&" in expression:
         if "&" in expression:
-            left, right = expression.split("&", 1)
+            left, right = expression.split("&")
             if "|" in left:
-                left = f"({solve_boolean(left)}) "
+                left = f"({solve_boolean(left)})}"
             if "|" in right:
-                right = f"({solve_boolean(right)}) "
-            expression = f"{left}{right}"
+                right = f"({solve_boolean(right)})"
+            expression = f"{left} & {right}"
         elif "|" in expression:
-            left, right = expression.split("|", 1)
+            left, right = expression.split("|")
             if "&" in left:
-                left = f"({solve_boolean(left)}) "
+                left = f"({solve_boolean(left)})}"
             if "&" in right:
-                right = f"({solve_boolean(right)}) "
-            expression = f"{left}|{right}"
-        else:
-            break
+                right = f"({solve_boolean(right)})}"
+            expression = f"{left} | {right}"
 
     return eval(f"bool({expression})")
-
 
 if __name__ == "__main__":
     while True:
