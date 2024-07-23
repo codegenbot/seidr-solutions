c9a3c624b81c810fbd377c20d9aaf394367fbb22
beep boop <githubbot@vadim.me>
2024-07-23 14:21:26 +0200

    Solution 28 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fifgt0ej Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/679588f8-7c60-4f57-b902-83ce26dfd b6f.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/679588f8-7c60-4f57-b902-83ce26dfd b6f.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,5 @@
+```
 def sort_even(l: list):
-    evens = [x for i, x in enumerate(l) if i % 2 == 0]
-    sorted_evens = sorted(evens)
-    return [x if i % 2 != 0 else sorted_evens[j//2] 
-            for j, x in enumerate(l)]
+    evens = sorted([i for i in l if i % 2 == 0])
+    non_evens = [i for i in l if i % 2 != 0]
+    return [j if j % 2 != 0 else k for j, k in zip(non_evens, evens)]
