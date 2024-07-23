0c1e46d7e7ccc6d7d6d023e2c67cb9ce2e9fc092
beep boop <githubbot@vadim.me>
2024-07-23 20:07:40 +0200

    Solution 31 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/bteeah24 Prompt: 3b58c0c6-9e44-4236-aca9-d99a5506ba5d.cpp:1:23: error: ‘string’ is not a member o f ‘std’     1 | int bowlingScore(std::string s) {       |                       ^~~~~~ 3b58c0c6-9e44-4236-aca9-d99a5506ba5d.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int bowlingScore(std::string s) {

--- bowling.cpp
+++ bowling.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 
 int bowlingScore(std::string s) {
@@ -11,13 +10,17 @@ int bowlingScore(std::string s) {
         if (c == 'X') {
             score += 30;
         } else if (c == '/') {
-            if (roll1 > 0) {
-                score += 10 + roll1;
+            if (i == s.length() - 1) {
+                score += 10 + roll1 * 2;
             } else {
-                score += 10;
+                if (roll1 + roll2 >= 10) {
+                    score += 10 + roll1 * 2;
+                } else {
+                    score += 10 + roll1 + roll2;
+                }
+                roll1 = 0; 
+                roll2 = 0;
             }
-            roll1 = 0; 
-            roll2 = 0;
         } else {
             int val = c - '0';
             if (roll1 == 0) {
@@ -29,11 +32,4 @@ int bowlingScore(std::string s) {
     }
 
     return score;
-}
-
-int main() {
-    std::string s = "X/X/5-8/4/6/4/7/3/-9/6";
-    int score = bowlingScore(s);
-    std::cout << "The total score is: " << score << std::endl;
-    return 0;
 }
