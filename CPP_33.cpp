48dd2001644f0a5ed8ed7aa549c4f14796559390
beep boop <githubbot@vadim.me>
2024-07-23 12:00:03 +0200

    Solution 22 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/k1fnh68u Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,8 +1,8 @@
-vector<int> res = l;
+vector<int> l_prime = l;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
-            sort(res.begin() + i, res.begin() + i + 3);
+            sort(l_prime.begin() + i, l_prime.begin() + i + 3);
         }
     }
-    return res;
+    return l_prime;
 }
