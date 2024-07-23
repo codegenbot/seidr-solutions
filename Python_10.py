a7ca5a486906adbec0c8707ad7c53e5d8487ec26
beep boop <githubbot@vadim.me>
2024-07-24 00:26:57 +0200

    Solution 66 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/d90l9i2w Prompt: madam helloolleh Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1800fa97-3358-4b80-b4f1-120581d38 a92.py", line 13, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1800fa97-3358-4b80-b4f1-120581d38 a92.py", line 10, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,13 @@
-```Python
+```
 def make_palindrome(s: str) -> str:
     if s == s[::-1]:
         return s
-    if len(s) < len(s + s[::-1]):
-        palindrome = s[::-1] + s
+    palindrome = s + s[::-1]
+    half_len = len(palindrome) // 2
+    if len(palindrome) % 2 != 0:
+        palindrome = s[:half_len+1] + s[-1::-1][:half_len][::-1]
     else:
-        palindrome = s + s[::-1]
+        palindrome = s + s[-1::-1]
     return palindrome
 
 print(make_palindrome("madam"))  
