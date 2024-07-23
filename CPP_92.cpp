555b4ea965d6331d88c2aeb88579687289e0f70c
beep boop <githubbot@vadim.me>
2024-07-23 12:36:19 +0200

    Solution 10 of CPP/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3r0xfz7t Prompt: Program failed

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,7 +1,7 @@
 bool any_int(float a, float b, float c) {
-    int ai = a;
-    int bi = b;
-    int ci = c;
+    long long ai = (long long)a;
+    long long bi = (long long)b;
+    long long ci = (long long)c;
 
     if (ai == bi + ci || ai == ci + bi || bi == ai + ci || bi == ci + ai || ci == ai + bi || ci == bi + ai)
         return true;
