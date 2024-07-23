2aedefcd6d7e3ceb2b76b2e8aa0d302a82c37876
beep boop <githubbot@vadim.me>
2024-07-24 01:45:21 +0200

    Solution 28 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1g16sxbf Prompt:       |                              ^~~~~~ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:52:37: error: ‘string’ was not declared  in this scope    52 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:52:44: error: ‘b’ was not declared in t his scope    52 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:52:45: error: expression list treated a s compound expression in initializer [-fpermissive]    52 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:51: e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp: In function ‘int main()’: e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:61:20: error: ‘by_length’ was not decla red in this scope    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ e77350c0-1a4d-4195-895d-09ac4f161fcd.cpp:61:19: error: ‘issame’ cannot be used a s a function    61 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,60 +1,63 @@
-bool issame(vector<string> a,vector<string>b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0;i<a.size();i++) {
-        if(a[i].compare(b[i])!=0) {
-            return false;
-        }
-    }
-    return true;
-}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 
-vector<string> by_length(vector<int> arr) {
-    vector<int> nums;
+namespace {
+
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<string> numArr;
     for (int i : arr) {
         if (i >= 1 && i <= 9) {
-            nums.push_back(i);
+            numArr.push_back(to_string(i));
         }
     }
 
-    sort(nums.begin(), nums.end());
-    reverse(nums.begin(), nums.end());
+    sort(numArr.begin(), numArr.end());
 
-    vector<string> result;
-    for (int num : nums) {
-        string str = "";
-        switch (num) {
-            case 1:
-                str = "One";
-                break;
-            case 2:
-                str = "Two";
-                break;
-            case 3:
-                str = "Three";
-                break;
-            case 4:
-                str = "Four";
-                break;
-            case 5:
-                str = "Five";
-                break;
-            case 6:
-                str = "Six";
-                break;
-            case 7:
-                str = "Seven";
-                break;
-            case 8:
-                str = "Eight";
-                break;
-            case 9:
-                str = "Nine";
-                break;
+    reverse(numArr.begin(), numArr.end());
+
+    std::vector<string> result;
+    for (string str : numArr) {
+        if (str == "One") {
+            str = "One";
+        } else if (str == "Two") {
+            str = "Two";
+        } else if (str == "Three") {
+            str = "Three";
+        } else if (str == "Four") {
+            str = "Four";
+        } else if (str == "Five") {
+            str = "Five";
+        } else if (str == "Six") {
+            str = "Six";
+        } else if (str == "Seven") {
+            str = "Seven";
+        } else if (str == "Eight") {
+            str = "Eight";
+        } else if (str == "Nine") {
+            str = "Nine";
         }
         result.push_back(str);
     }
 
     return result;
+}
+
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    if (a.size() != b.size())
+        return false;
+
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i])
+            return false;
+
+    return true;
+}
+
+} // namespace
+
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Four", "Eight"}));
+    return 0;
 }
