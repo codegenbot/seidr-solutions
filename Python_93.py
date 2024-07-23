6859d019520b20bd9cf991162bc4a6d9951e4ec6
beep boop <githubbot@vadim.me>
2024-07-23 12:28:38 +0200

    Solution 88 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/30rlwqnr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/342dc195-657c-4c77-9337-4d0992a15 8a5.py", line 28, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/342dc195-657c-4c77-9337-4d0992a15 8a5.py", line 24, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,18 +2,18 @@ def encode(message):
     encoded_message = ""
     for char in message:
         if char.isalnum():
-            if char.isdigit():
+            if char.isdigit(): 
                 encoded_char = char
             elif char.isalpha():
                 if char.isupper():
                     encoded_char = chr((ord(char) - 65 + 3) % 26 + 65)
                 else:
                     encoded_char = chr((ord(char) - 97 + 3) % 26 + 97)
-            else:
+            else:  
                 encoded_char = char
-        else:
-            encoded_char = char
-
+        else:  
+            encoded_message += char
+        
         encoded_message += encoded_char
-
+    
     return encoded_message
