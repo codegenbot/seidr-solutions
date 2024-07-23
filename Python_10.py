02398245bc42c19d612d93e1f69226f889452fba
beep boop <githubbot@vadim.me>
2024-07-23 13:45:39 +0200

    Solution 56 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tfdeqh91 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ff82d1ee-e922-4c37-9e32-c33f59b6f 718.py", line 1     Problem description:             ^^^^^^^^^^^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,10 @@
-Here is the modified code:
-
 def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
+
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
-        return string + string[::-1]
+        return string + string
     for i in range(len(string)):
         postfix = string[i:]
         if postfix == postfix[::-1]:
