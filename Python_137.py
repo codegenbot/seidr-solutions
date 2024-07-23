542ca5de4047919ca1fa91c9dc2d50aac7df0e54
beep boop <githubbot@vadim.me>
2024-07-23 12:13:47 +0200

    Solution 5 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/si5gzmww Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e3343e44-f232-40f2-a2ec-0e5603bef 8e8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_137.py
+++ Python_137.py
@@ -1,10 +1,14 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        return a if float(a.replace(",", ".")) > b else None
+        a = float(a.replace(",", "."))
+        return a if a > b else None
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        return b if float(b.replace(",", ".")) > a else None
+        b = float(b.replace(",", "."))
+        return b if b > a else None
     elif isinstance(a, str) and isinstance(b, str):
-        return a if float(a.replace(",", ".")) > float(b.replace(",", ".")) else None
+        a = float(a.replace(",", "."))
+        b = float(b.replace(",", "."))
+        return a if a > b else None
     elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return b if a < b else None
