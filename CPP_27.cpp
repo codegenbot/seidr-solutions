21d895d6631b840577c69595f9adcd35d8fcd88d
beep boop <githubbot@vadim.me>
2024-07-23 12:10:24 +0200

    Solution 71 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/yzca40c6 Prompt: Complete the following code given the task description and function signature.

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,7 +2,8 @@ string flip_case(string str){
     for(int i=0; i<str.length(); i++){
         if(islower(str[i])){
             str[i] = toupper(str[i]);
-        } else if(isupper(str[i])){
+        }
+        else if(isupper(str[i])){
             str[i] = tolower(str[i]);
         }
     }
