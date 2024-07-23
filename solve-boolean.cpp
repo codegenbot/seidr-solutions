a4e3eb8eba134ab3885e9c44b29a8a01ed21dd8c
beep boop <githubbot@vadim.me>
2024-07-23 14:12:31 +0200

    Solution 28 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ie0wp8mq Prompt: this scope    21 |     cin >> s;       |     ^~~ f470d7c5-01d7-4a9e-8b42-e2047b8e2ee6.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; f470d7c5-01d7-4a9e-8b42-e2047b8e2ee6.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << (solveBoolean(s) ? "True" : "False") << endl;       |     ^~~~ f470d7c5-01d7-4a9e-8b42-e2047b8e2ee6.cpp:22:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? f470d7c5-01d7-4a9e-8b42-e2047b8e2ee6.cpp:22:53: error: ‘endl’ was not declared i n this scope    22 |     cout << (solveBoolean(s) ? "True" : "False") << endl;       |                                                     ^~~~ f470d7c5-01d7-4a9e-8b42-e2047b8e2ee6.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,25 +1,20 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 
 bool solveBoolean(string s) {
-    stack<char> st;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '&') {
-            while (!st.empty() && st.top() == '&') {
-                st.pop();
-            }
-            if (st.empty()) return false;
-        } else if (s[i] == '|') {
-            while (!st.empty() && st.top() == '|') {
-                st.pop();
-            }
-            if (st.empty()) return true;
-        } else {
-            st.push(s[i]);
+    bool res = true;
+    for (char c : s) {
+        if (c == 'F') {
+            res = false;
+            break;
+        } else if (c == '&') {
+            res &= true;
+        } else if (c == '|') {
+            res |= true;
         }
     }
-    return !st.empty();
+    return res;
 }
 
 int main() {
