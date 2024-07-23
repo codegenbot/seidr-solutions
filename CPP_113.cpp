327f02d862d9ee00191aa3fc4b5975f2d16ea415
beep boop <githubbot@vadim.me>
2024-07-23 14:33:16 +0200

    Solution 11 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k0yp7gj7 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from f9c11431-af6f-48bb-8d57-a0c53c0c6c2c.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -26,10 +26,8 @@ vector<string> odd_count(vector<string> lst) {
 }
 
 int main() {
-    vector<string> test = {"271", "137", "314"};
-    vector<string> expected = {"the number of odd elements 2 in the string 1", 
-                               "the number of odd elements 1 in the string 2", 
-                               "the number of odd elements 0 in the string 3"};
-    assert(issame(odd_count(test), expected));
+    assert(issame(odd_count({"271", "137", "314"}) , {"the number of odd elements 2 in the string 1", 
+                                                         "the number of odd elements 1 in the string 2", 
+                                                         "the number of odd elements 0 in the string 3"}));
     return 0;
 }
