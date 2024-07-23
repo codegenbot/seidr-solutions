aeae9805a629bddd9df14fb906348f85e8501c47
beep boop <githubbot@vadim.me>
2024-07-23 16:11:03 +0200

    Solution 32 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/xc7ir70c Prompt: 2295a749-cf84-48d9-9fcb-4745b74d6f38.cpp: In function ‘std::vector<int> cutVecto r(std::vector<int>&)’: 2295a749-cf84-48d9-9fcb-4745b74d6f38.cpp:5:19: error: ‘INT_MAX’ was not declared  in this scope     5 |     int minDiff = INT_MAX;       |                   ^~~~~~~ 2295a749-cf84-48d9-9fcb-4745b74d6f38.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 2295a749-cf84-48d9-9fcb-4745b74d6f38.cpp:9:20: error: ‘abs’ was not declared in this scope     9 |         int diff = abs(nums[i] - nums[i+1]);       |                    ^~~ 2295a749-cf84-48d9-9fcb-4745b74d6f38.cpp:20:24: error: could not convert ‘{left,  right}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<int>’    20 |     return {left, right};       |                        ^       |                        |       |                        <brace-enclosed initializer list>

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,25 +1,24 @@
 #include <vector>
 #include <climits>
 #include <cmath>
+
 using namespace std;
 
-vector<pair<vector<int>, vector<int>>> cutVector(vector<int>& nums) {
+vector<vector<int>> cutVector(vector<int>& nums) {
     int minDiff = INT_MAX;
     int pos = -1;
     
     for(int i=0; i<nums.size()-1; ++i) {
-        int diff = std::abs(nums[i] - nums[i+1]);
+        int diff = abs(nums[i] - nums[i+1]);
         if(diff < minDiff) {
             minDiff = diff;
             pos = i;
         }
     }
     
-    vector<pair<vector<int>, vector<int>>> result;
-    pair<vector<int>, vector<int>> cutted;
-    cutted.first.assign(nums.begin(), nums.begin()+pos);
-    cutted.second.assign(nums.begin()+pos, nums.end());
-    result.push_back(cutted);
+    vector<int> left, right;
+    left.assign(nums.begin(), nums.begin()+pos);
+    right.assign(nums.begin()+pos, nums.end());
     
-    return result;
+    return {{left}, {right}};
 }
