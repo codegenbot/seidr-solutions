b7fc86769373665c9b167d95b8fa3e6e2cb51fbf
beep boop <githubbot@vadim.me>
2024-07-23 10:22:42 +0200

    Solution 18 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/2z7ojd2e Prompt: Complete the following code given the task description and function signature.

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -5,12 +5,11 @@ vector<int> parse_music(string music_string){
         if (music_string[i] == 'o') {
             beats.push_back(4);
             i += 2;
-        } else if (music_string[i] == '.') {
+        } else if (music_string[i] == ' ') {
+            i++;
+        } else {
             beats.push_back(1);
             i += 2;
-        } else {
-            beats.push_back(2);
-            i += 3;
         }
     }
     return beats;
