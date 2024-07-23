021d0a29e123ad909a0ea0a3d0af9f5e6b1fb790
beep boop <githubbot@vadim.me>
2024-07-23 14:18:22 +0200

    Solution 47 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ixfhowvx Prompt: Program did not receive expected input

--- Python_12.py
+++ Python_12.py
@@ -5,6 +5,6 @@ def longest(strings: list[str]) -> Optional[str]:
     if not strings:
         return None
     elif all(not s for s in strings):
-        return ""
+        return ''
     else:
         return max(strings, key=len)
