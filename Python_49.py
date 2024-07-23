f1aa37cf337120710600095a69e7778ec46a87e9
beep boop <githubbot@vadim.me>
2024-07-23 10:44:18 +0200

    Solution 42 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/33p1n75y Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/df1ca766-b3ba-4238-9f78-2e5150e1b 71b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,6 +1,8 @@
-```
 def modp(n: int, p: int):
-    if gcd(n, p) == 1:
-        return pow(n, inv((p - 1), p), p)
-    else:
-        raise ValueError("n and p are not coprime")
+    if gcd(n, p) != 1:
+        return None
+    result = pow(n, (p - 1), p)
+    for _ in range(p - 2):
+        n = (n * result) % p
+        result = (result * 2) % p
+    return n
