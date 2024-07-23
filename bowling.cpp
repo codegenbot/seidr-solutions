f8191fe619c9f6bd2175f16c1d15d673179bb26c
beep boop <githubbot@vadim.me>
2024-07-23 10:39:32 +0200

    Solution 60 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8xs6nks1 Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 2c708ef7-b02b-420e-8133-271c2059aadf.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 2c708ef7-b02b-420e-8133-271c2059aadf.cpp:6:21:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,59 +1,80 @@
+```c++
 #include <string>
-#include <vector>
+#include <cctype>
 
-int bowlingScore(std::string s) {
+int main() {
     int score = 0;
-    std::vector<int> frames;
+    std::string s = "X/X/5+7-3-1/2+4/X/2-8-X/2+6/X"; 
     for (char c : s) {
         if (c == '/') {
-            if (!score % 10 > 0 && score % 10 < 10) {
-                if (s.find('X') != std::string::npos || s.find('x') != std::string::npos) {
-                    frames.push_back(10);
+            if (score % 10 > 0 && score % 10 < 10) {
+                int roll1 = -1;
+                int roll2 = -1;
+                int foundX = s.find('X');
+                if (foundX != std::string::npos) {
+                    if (roll1 == -1) {
+                        roll1 = foundX;
+                    }
                 } else {
-                    int roll1 = -1;
-                    int roll2 = -1;
-                    for(int i = score % 10; i < s.length(); i++) {
-                        if (s[i] == '1' || s[i] == 'x') {
-                            roll1 = i + 1;
-                        } else if (s[i] == '2') {
-                            roll2 = i;
+                    size_t pos = 0;
+                    while ((pos = s.find('/', pos)) != std::string::npos) {
+                        int val1 = std::stoi(s.substr(0, pos).erase(0, 1));
+                        if (val1 > 10) {
+                            roll1 = 10;
+                            break;
+                        } else if (val1 < 2) {
+                            roll1 = val1;
+                            break;
+                        }
+                        int val2 = std::stoi(s.substr(pos + 1).erase(0, 1));
+                        if (val2 > 10) {
+                            roll2 = 10 - roll1;
+                            break;
+                        } else if (val2 < 2) {
+                            roll2 = val2;
                             break;
                         }
                     }
-                    frames.push_back(std::min(roll1, 10) + std::min(roll2 - 1, 9));
                 }
+                score += roll1 + roll2;
             } else {
-                int roll1 = -1;
-                int roll2 = -1;
-                for(int i = score % 10; i < s.length(); i++) {
-                    if (s[i] == 'X' || s[i] == 'x') {
-                        roll1 = i + 1;
-                    } else if (s[i] == '/') {
-                        roll2 = i;
-                        break;
-                    }
+                int foundX = s.find('X');
+                int foundSlash = s.find('/');
+                int val1 = std::stoi(s.substr(0, foundSlash).erase(0, 1));
+                if (val1 > 10) {
+                    score += 10;
+                } else if (val1 < 2) {
+                    score += val1;
                 }
-                frames.push_back(std::min(roll1, 10) + std::min(roll2 - 1, 9));
             }
         } else if (c == 'X' || c == 'x') {
-            for(int i = score % 10; i < s.length(); i++) {
-                if (s[i] == 'X' || s[i] == 'x') {
-                    frames.push_back(10);
-                    break;
+            int roll = -1;
+            size_t pos = 0;
+            while ((pos = s.find(c, pos)) != std::string::npos) {
+                if (c == 'x') {
+                    if (std::stoi(s.substr(0, pos).erase(0, 1)) < 6) {
+                        throw "Invalid input";
+                    }
                 }
+                score += min(pos, 10);
             }
-        } else if (c == '1' || c == '2' || c == '+') {
-            for(int i = score % 10; i < s.length(); i++) {
-                if (s[i] == 'X' || s[i] == 'x' || s[i] == '+' || s[i] == '-') {
-                    break;
-                } else if (c == '1' && s[i] >= '3') {
+        } else {
+            int val = std::stoi(s.erase(0, 1));
+            if (val > 10 || val < 2) {
+                int bonusRoll1 = s.find('+');
+                int bonusRoll2 = s.find('-');
+                if (bonusRoll1 == -1) {
+                    score += min(val, 10);
+                } else if (bonusRoll2 != -1) {
+                    score += min(val, 10) + max(min(bonusRoll1 - val - 1, 9), 0) +
+                        max(min(bonusRoll2 - val - 1, 9), 0);
+                } else {
                     throw "Invalid input";
                 }
+            } else {
+                score += val;
             }
         }
-    }
-    for(int i = 0; i < frames.size(); i++) {
-        score += frames[i];
     }
     return score;
 }
