2c62770f6b3c56f5a16cec67e8488036d829f20e
beep boop <githubbot@vadim.me>
2024-07-23 09:52:44 +0200

    Solution 9 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/ds0ken1u Prompt: in this scope    46 |     string s;       |     ^~~~~~ c9b3c5a4-f965-40d3-9f31-75493293642d.cpp:47:5: error: ‘cin’ was not declared in this scope    47 |     cin >> s;       |     ^~~ c9b3c5a4-f965-40d3-9f31-75493293642d.cpp:47:12: error: ‘s’ was not declared in t his scope    47 |     cin >> s;       |            ^ c9b3c5a4-f965-40d3-9f31-75493293642d.cpp:48:5: error: ‘cout’ was not declared in  this scope    48 |     cout << score(s) << endl;       |     ^~~~ c9b3c5a4-f965-40d3-9f31-75493293642d.cpp:48:18: error: ‘score’ cannot be used as  a function    48 |     cout << score(s) << endl;       |             ~~~~~^~~ c9b3c5a4-f965-40d3-9f31-75493293642d.cpp:48:25: error: ‘endl’ was not declared i n this scope    48 |     cout << score(s) << endl;       |                         ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -8,20 +8,33 @@ int score(std::string s) {
     while (frame <= 10) {
         if (s[i] == 'X') {
             total += 10;
-            if (s[i + 2] == 'X') total += 10;
-            else if (s[i + 2] == '/') total += 10;
-            else total += s[i + 2] - '0';
-
-            if (s[i + 4] == 'X') total += 10;
-            else if (s[i + 4] == '/') total += 10;
-            else total += s[i + 4] - '0';
-
+            if (s[i + 2] == 'X') {
+                total += 10;
+                if (s[i + 4] == 'X') {
+                    total += 10;
+                } else if (s[i + 4] == '/') {
+                    total += 10;
+                } else {
+                    total += s[i + 4] - '0';
+                }
+            } else if (s[i + 2] == '/') {
+                total += 10;
+            } else {
+                total += s[i + 2] - '0';
+                if (s[i + 4] == '/') {
+                    total += 10;
+                } else {
+                    total += s[i + 4] - '0';
+                }
+            }
             i += 2;
         } else if (s[i + 1] == '/') {
             total += 10;
-            if (s[i + 3] == 'X') total += 10;
-            else total += s[i + 3] - '0';
-
+            if (s[i + 3] == 'X') {
+                total += 10;
+            } else {
+                total += s[i + 3] - '0';
+            }
             i += 2;
         } else {
             total += s[i] - '0' + s[i + 1] - '0';
