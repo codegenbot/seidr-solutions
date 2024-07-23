427d845d4fcbd9dde00fcf9cbbc15ff7831f6ab0
beep boop <githubbot@vadim.me>
2024-07-23 11:12:50 +0200

    Solution 10 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ydxowk1s Prompt: it must return vcxax fs xmx ckq for input xaxcv fs xmx ckq, but it returns this is a this is rehtona.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,31 +1,33 @@
-#include <string>
-#include <cstring>
-#include <algorithm>
+```
+std::string spinWords(std::string str) {
+    std::string result = "";
+    bool longWord = false;
 
-using namespace std;
-
-string spinWords(string sentence) {
-    string result = "";
-    char* word = strtok(const_cast<char*>(sentence.c_str()), " ");
-    
-    while(word != NULL) {
-        int wordLength = strlen(word);
-        
-        if(wordLength >= 5)
-            result += (string(word) + " ") + string(rotate(word, 0, wordLength - 1)) + "\n";
-        else
-            result += string(word) + " ";
-        
-        word = strtok(NULL, " ");
+    for (char c : str) {
+        if (c == ' ') {
+            if (!longWord) {
+                result += word + " ";
+                word = "";
+            } else {
+                std::reverse(word.begin(), word.end());
+                result += word + " ";
+                word = "";
+                longWord = false;
+            }
+        } else {
+            word += c;
+            if (word.length() >= 5) {
+                longWord = true;
+            }
+        }
     }
-    
-    return result;
-}
 
-string rotate(const string& str, int start, int length) {
-    if(start >= length)
-        return str;
-    if(length == 0)
-        return "";
-    return str.substr(length) + str.substr(0, length);
+    if (!longWord) {
+        result += word;
+    } else {
+        std::reverse(word.begin(), word.end());
+        result += word;
+    }
+
+    return result;
 }
