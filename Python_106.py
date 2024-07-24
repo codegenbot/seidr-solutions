ce0d2e6d86afba3d0daac9108eb76b0397f40c38
beep boop <githubbot@vadim.me>
2024-07-24 05:38:20 +0200

    Solution 82 of Python/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/58kkygmq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a3685ff2-c4ac-41aa-b3b9-21b143426 6fc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_106.py
+++ Python_106.py
@@ -1,22 +1,20 @@
+```
 import math
-
 
 def factorial(x):
     if x == 0:
         return 1
     else:
-        return x * factorial(x - 1)
-
+        return x * factorial(x-1)
 
 def f(n):
     result = 1
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         if i % 2 == 0:
             result *= math.factorial(i)
         else:
             result *= i
     return result
-
 
 while True:
     try:
