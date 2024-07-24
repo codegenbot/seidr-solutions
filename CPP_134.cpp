163d1fdd74602b5feb97fec83262be1755b6c569
beep boop <githubbot@vadim.me>
2024-07-24 14:38:20 +0200

    Solution 21 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6zjxuet3 Prompt: 16acc813-86b5-4d5d-b791-aa5d43bcc984.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 16acc813-86b5-4d5d-b791-aa5d43bcc984.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 16acc813-86b5-4d5d-b791-aa5d43bcc984.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 16acc813-86b5-4d5d-b791-aa5d43bcc984.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 16acc813-86b5-4d5d-b791-aa5d43bcc984.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,4 +1,3 @@
-```
 #include <cctype>
 #include <cassert>
 #include <string>
@@ -10,11 +9,14 @@ bool check_if_last_char_is_a_letter(const std::string& str) {
     return std::isalpha(lastChar);
 }
 
-std::cout << "Check if the last character of a string is a letter: ";
-std::string txt;
-std::cin >> txt;
-if (check_if_last_char_is_a_letter(txt))
-    std::cout << "The last character of the input string is a letter.\n";
-else
-    std::cout << "The last character of the input string is not a letter.\n";
-return 0;
+int main() {
+    assert(check_if_last_char_is_a_letter("apple pi e ") == true); 
+    std::cout << "Check if the last character of a string is a letter: ";
+    std::string txt;
+    std::cin >> txt;
+    if (check_if_last_char_is_a_letter(txt))
+        std::cout << "The last character of the input string is a letter.\n";
+    else
+        std::cout << "The last character of the input string is not a letter.\n";
+    return 0;
+}
