bfeae527472205ca782d2e08f0358c1fdd2bfb0e
beep boop <githubbot@vadim.me>
2024-07-24 05:22:24 +0200

    Solution 92 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y8l1vktq Prompt:       |                    ^~~~~ be256c51-a853-441c-b50c-eaf8bc2750a8.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ be256c51-a853-441c-b50c-eaf8bc2750a8.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ be256c51-a853-441c-b50c-eaf8bc2750a8.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from be256c51-a853-441c-b50c-eaf8bc2750a8.cpp:11: be256c51-a853-441c-b50c-eaf8bc2750a8.cpp: In function ‘int main()’: be256c51-a853-441c-b50c-eaf8bc2750a8.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ be256c51-a853-441c-b50c-eaf8bc2750a8.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -5,22 +5,6 @@ bool issame(vector<float> a, vector<float> b) {
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
-int main() {
-    assert(std::all_of(get_positive({}).begin(), get_positive({}).end(), [](float x){return x > 0;})== {});
-    vector<float> l;
-    cout << "Enter some numbers (negative to stop): ";
-    while(cout >> l) {
-        if(l[0] <= 0.0f) break;
-        l = get_positive(l);
-        cout << "Positive numbers: ";
-        for(auto x : l) {
-            cout << x << ' ';
-        }
-        cout << endl;
-    }
-    return 0;
-}
-
 vector<float> get_positive(vector<float> l){
     vector<float> result;
     for(auto x : l) {
