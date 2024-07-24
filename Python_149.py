7659ea57ee5b76fd91a8509dee8d9e4468e02be0
beep boop <githubbot@vadim.me>
2024-07-24 15:33:23 +0200

    Solution 22 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b70d53fd-1a10-4ff1-86b6-ffae6f9ca ce8.py", line 9, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b70d53fd-1a10-4ff1-86b6-ffae6f9ca ce8.py", line 6, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,3 @@
 ```
-def sorted_list_sum(input_string):
-    words = input_string.split()
-    even_length_words = [word for word in words if len(word) % 2 == 0]
-    return str(sum(int(word) for word in sorted(even_length_words)))
+def sorted_list_sum(lst):
+    return [sum(len(word) for word in lst if len(word) % 2 == i) for i in range(2)]
