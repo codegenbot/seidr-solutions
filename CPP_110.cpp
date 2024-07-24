ff8cd70d7389f7a9a7e227da70367027fe196f4c
beep boop <githubbot@vadim.me>
2024-07-24 20:53:03 +0200

    Solution 30 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/uqsk0gt9 Prompt: 0b8679fa-eac7-437b-8883-3f9e7f0ab57c.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from 0b8679fa-eac7-437b-8883-3f9e7f0ab57c.cpp:20: 0b8679fa-eac7-437b-8883-3f9e7f0ab57c.cpp: In function ‘int main()’: 0b8679fa-eac7-437b-8883-3f9e7f0ab57c.cpp:22:13: error: ‘exchange’ was not declar ed in this scope    22 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,13 +1,21 @@
-#include<string>
-std::string exchange(vector<int> lst1, vector<int> lst2) {
-    int oddCount = 0;
+#include <vector>
+#include <string>
+
+std::string exchange(std::vector<int> lst1, std::vector<int> lst2) {
+    int evenCount = 0;
     for (int num : lst1) {
-        if (num % 2 != 0)
-            oddCount++;
+        if (num % 2 == 0) {
+            evenCount++;
+        }
     }
-    int evenCount = lst2.size() - oddCount;
-    if (oddCount > evenCount)
-        return "NO";
-    else
+    if (evenCount == lst1.size()) {
         return "YES";
+    } else {
+        for (int num : lst2) {
+            if (std::find(lst1.begin(), lst1.end(), num) != lst1.end() && num % 2 != 0) {
+                return "NO";
+            }
+        }
+    }
+    return "YES";
 }
