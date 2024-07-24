a479e0a800284e7bd327f7a7d055b40e54d9a090
beep boop <githubbot@vadim.me>
2024-07-24 17:54:46 +0200

    Solution 80 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ry5ssmq6 Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c49d9082-574d-4348-945b-0f817b6a30ca.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c49d9082-574d-4348-945b-0f817b6a30ca.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c49d9082-574d-4348-945b-0f817b6a30ca.cpp:10: c49d9082-574d-4348-945b-0f817b6a30ca.cpp: In function ‘int main()’: c49d9082-574d-4348-945b-0f817b6a30ca.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ c49d9082-574d-4348-945b-0f817b6a30ca.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,28 +3,21 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-vector<int> sort_third(vector<int> l){
-    
-    for(int i = 0; i < l.size(); i++){
-        if(i % 3 == 0){
-            vector<int> temp;
-            for(int j = i; j < l.size() && j % 3 == 0; j++){
-                temp.push_back(l[j]);
-            }
-            sort(temp.begin(), temp.end());
-            for(int j = i; j < l.size() && j % 3 == 0; j++){
-                l[j] = temp[j - i];
-            }
-        }
-    }
-    return l;
+bool issame(const std::vector<int> &a, const std::vector<int> &b) {
+    return a == b;
 }
 
-bool issame(vector<int> a, vector<int> b);
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> res = l;
+    for (int i = 0; i < l.size(); ++i) {
+        if ((i + 1) % 3 == 0) {
+            std::sort(res.begin() + i - 2, res.begin() + i + 1);
+        }
+    }
+    return res;
+}
 
 int main() {
-    assert(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) == vector<int>({2, 6, 3, 4, 8, 9, 5, 1}));
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
     return 0;
 }
