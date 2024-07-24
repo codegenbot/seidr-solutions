41d659bea7f1228af83fef688f3995e1c8b9603c
beep boop <githubbot@vadim.me>
2024-07-24 08:23:12 +0200

    Solution 41 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/n60d1r45 Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,29 +2,42 @@
 using namespace std;
 
 bool solveBoolean(string s) {
-    bool result = true;
+    bool res = true;
     int i = 0;
-    while(i < s.length()) {
-        if(s[i] == 'T') {
-            return true;
-        }
-        else if(s[i] == 'F') {
-            return false;
-        }
-        else if(s[i] == '|') {
+    while (i < s.size()) {
+        if (s[i] == 'T') {
+            res = true;
             i++;
-            bool left = solveBoolean(s.substr(0, i));
+        } else if (s[i] == 'F') {
+            res = false;
             i++;
-            bool right = solveBoolean(s.substr(i));
-            result = (left || right);
-        }
-        else if(s[i] == '&') {
-            i++;
-            bool left = solveBoolean(s.substr(0, i));
-            i++;
-            bool right = solveBoolean(s.substr(i));
-            result = (left && right);
+        } else if (s[i] == '&') {
+            bool temp = res;
+            while (i < s.size() && s[i] != '&') {
+                if (s[i] == 'T') {
+                    res = true;
+                } else if (s[i] == 'F') {
+                    res = false;
+                }
+                i++;
+            }
+            if (!temp) {
+                return false;
+            }
+        } else if (s[i] == '|') {
+            bool temp = res;
+            while (i < s.size() && s[i] != '|') {
+                if (s[i] == 'T') {
+                    res = true;
+                } else if (s[i] == 'F') {
+                    res = false;
+                }
+                i++;
+            }
+            if (temp) {
+                return false;
+            }
         }
     }
-    return result;
+    return res;
 }
