8ef642b5b863c914b2fd7730843ca8f7bb5aae77
beep boop <githubbot@vadim.me>
2024-07-24 20:59:43 +0200

    Solution 8 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/h61prole Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -2,21 +2,20 @@
 from typing import List
 
 def separate_paren_groups(paren_string: str) -> List[str]:
+    result = []
     stack = []
     groups = []
-    current_group = []
-
+    
     for char in paren_string:
-        if char == ' ':
-            continue
         if char == '(':
             stack.append(char)
-            current_group.append(char)
+            groups.append('(')
         elif char == ')':
             stack.pop()
-            current_group.append(char)
-            if not stack:
-                groups.append(''.join(current_group))
-                current_group = []
+            groups.append(')')
+        
+        if not stack:
+            result.append(''.join(groups))
+            groups = []
 
-    return groups
+    return result
