fc210a8107bb253e00f2bfa2c3a5443764fe7b8c
beep boop <githubbot@vadim.me>
2024-07-24 12:30:51 +0200

    Solution 76 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/48n9pw6w Prompt:   +++ |+#include <iostream>     3 | 10041c42-1723-4e44-84a8-093e08e269ff.cpp:25:5: error: ‘cin’ was not declared in this scope    25 |     cin >> a >> b;       |     ^~~ 10041c42-1723-4e44-84a8-093e08e269ff.cpp:25:5: note: ‘std::cin’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’? 10041c42-1723-4e44-84a8-093e08e269ff.cpp:27:57: error: ‘gcd’ was not declared in  this scope    27 |     cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << end l;       |                                                         ^~~ 10041c42-1723-4e44-84a8-093e08e269ff.cpp:27:70: error: ‘endl’ was not declared i n this scope    27 |  cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;       |                                                                   ^~~~ 10041c42-1723-4e44-84a8-093e08e269ff.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <string>   +++ |+#include <ostream>     3 |

--- gcd.cpp
+++ gcd.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -5,4 +11,30 @@ int gcd(int a, int b) {
         a = temp;
     }
     return a;
+}
+
+vector<int> indicesOfSubstring(string text, string target) {
+    vector<int> result;
+    int n = text.length();
+    int m = target.length();
+
+    for (int i = 0; i <= n - m; i++) {
+        if (text.substr(i, m) == target) {
+            result.push_back(i);
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    int a = 0;
+    int b = 0;
+
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+
+    return 0;
 }
