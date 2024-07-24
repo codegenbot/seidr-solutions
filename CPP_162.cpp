dc871f6efde4ff0ee0a5c7bac1915e61f8e46ab7
beep boop <githubbot@vadim.me>
2024-07-25 01:32:06 +0200

    Solution 95 of CPP/162, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l257dgnb Prompt:    18 |     MD5_Final(md5, &ctx);       |     ~~~~~~~~~^~~~~~~~~~~ In file included from 5efe0f40-20f8-4f72-8acb-e87d04ca4349.cpp:4: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ 5efe0f40-20f8-4f72-8acb-e87d04ca4349.cpp:20:18: error: aggregate ‘std::stringstr eam ss’ has incomplete type and cannot be defined    20 |     stringstream ss;       |                  ^~ 5efe0f40-20f8-4f72-8acb-e87d04ca4349.cpp:22:15: error: ‘setfill’ was not declare d in this scope    22 |         ss << setfill('0') << setw(2) << hex << (int)md5[i];       |               ^~~~~~~ 5efe0f40-20f8-4f72-8acb-e87d04ca4349.cpp:22:31: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    22 |         ss << setfill('0') << setw(2) << hex << (int)md5[i];       |                               ^~~~       |                               getw 5efe0f40-20f8-4f72-8acb-e87d04ca4349.cpp:22:42: error: ‘hex’ was not declared in  this scope    22 |         ss << setfill('0') << setw(2) << hex << (int)md5[i];       |                                          ^~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,5 @@
-#include <string>
-#include <openssl/ssl.h>
-#include <openssl/x509v3.h>
 #include <openssl/md5.h>
+#include <string>
 #include <sstream>
 #include <iomanip>
 
@@ -9,14 +7,21 @@ using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
-    
-    unsigned char md[MD5_DIGEST_LENGTH];
-    MD5((const unsigned char*)text.c_str(), text.length(), md);
-    
+
+    unsigned char md5[MD5_DIGEST_LENGTH];
+    MD5_CTX *ctx = new MD5_CTX;
+    MD5_Init(ctx);
+    const char* ptr = text.c_str();
+    int len = text.size();
+    while(len--) MD5_Update(ctx, ptr++, 1);
+    MD5_Final(md5, ctx);
+
     stringstream ss;
     for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        ss << setfill('0') << setw(2) << hex << uppercase << (int)md[i];
+        ss << setfill('0') << setw(2) << hex << (int)md5[i];
     }
-    
+
+    delete ctx;
+
     return ss.str();
 }
