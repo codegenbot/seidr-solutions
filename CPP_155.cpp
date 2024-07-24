f53daa9ce8f3634247a3bf3b4bf826c48116b6f3
beep boop <githubbot@vadim.me>
2024-07-24 15:15:16 +0200

    Solution 1 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/avs6upup Prompt: all to ‘stoi(char&)’     9 |         if (stoi(c) % 2 == 0)       |             ~~~~^~~ In file included from /usr/include/c++/11/string:55,                  from 1836d836-257e-44b9-a056-3094a1f606d9.cpp:1: /usr/include/c++/11/bits/basic_string.h:6619:3: note: candidate: ‘int std::__cxx 11::stoi(const string&, std::size_t*, int)’  6619 |   stoi(const string& __str, size_t* __idx = 0, int __base = 10)       |   ^~~~ /usr/include/c++/11/bits/basic_string.h:6619:22: note:   no known conversion for  argument 1 from ‘char’ to ‘const string&’ {aka ‘const std::__cxx11::basic_strin g<char>&’}  6619 |   stoi(const string& __str, size_t* __idx = 0, int __base = 10)       |        ~~~~~~~~~~~~~~^~~~~ /usr/include/c++/11/bits/basic_string.h:6750:3: note: candidate: ‘int std::__cxx 11::stoi(const wstring&, std::size_t*, int)’  6750 |   stoi(const wstring& __str, size_t* __idx = 0, int __base = 10)       |   ^~~~ /usr/include/c++/11/bits/basic_string.h:6750:23: note:   no known conversion for  argument 1 from ‘char’ to ‘const wstring&’ {aka ‘const std::__cxx11::basic_stri ng<wchar_t>&’}  6750 |   stoi(const wstring& __str, size_t* __idx = 0, int __base = 10)       |        ~~~~~~~~~~~~~~~^~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,15 +1,7 @@
-#include<string>
-#include<vector>
-using namespace std;
-
-vector<int> even_odd_count(int num) {
-    vector<int> result(2, 0);
-    string str = to_string(abs(num));
-    for (char c : str) {
-        if (stoi(c) % 2 == 0)
-            result[0]++;
-        else
-            result[1]++;
-    }
-    return result;
+for (char c : str) {
+    int digit = c - '0';
+    if (digit % 2 == 0)
+        result[0]++;
+    else
+        result[1]++;
 }
