5757a75b1eb8d1de281030a308ce00a70bb4305b
beep boop <githubbot@vadim.me>
2024-07-24 06:12:00 +0200

    Solution 35 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a37f7f0f-12f8-4cb7-b8d0-1bb80ddef 4ef.py", line 13, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a37f7f0f-12f8-4cb7-b8d0-1bb80ddef 4ef.py", line 10, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,6 +1,6 @@
 def sorted_list_sum(input_data):
     word_dict = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 
                  'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    nums = sorted([word_dict.get(word) for word in input_data if word in word_dict])
+    nums = sorted([(word_dict.get(word), index) for index, word in enumerate(input_data) if word in word_dict], key=lambda x: x[1])
     
-    return sum(num for num in nums)
+    return sum([num for num, _ in nums])
