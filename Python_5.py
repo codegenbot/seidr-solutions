c3970172da4bb0909ae8293ba46c384cc6478026
beep boop <githubbot@vadim.me>
2024-07-24 10:48:32 +0200

    Solution 50 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/a8daa1c5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11f2e462-77ee-4023-8f41-8a6302a89 459.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,10 +1,13 @@
 from typing import List
 
-
 def intersperse(numbers: List[int], delimeter: int) -> List[int]:
-    if len(numbers) > 1:
-        numbers[1:-1] = [delimeter] + [x for x in numbers[1:-1]]
     result = [numbers[0]]
-    result.extend([str(num) for num in numbers[1:]])
-    result.insert(1, str(delimeter))
-    return [int(i) for i in result]
+    for i in range(1, len(numbers)):
+        result.extend([delimeter])
+        result.extend([numbers[i]])
+    return result
+
+if __name__ == "__main__":
+    input_numbers = list(map(int, input("Enter comma-separated numbers: ").split(',')))
+    delimeter = int(input("Enter the delimeter you want to use: "))
+    print(intersperse(input_numbers, delimeter))
