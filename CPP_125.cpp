55104be75af7d099a544a5166fb2eb1cff6b2305
beep boop <githubbot@vadim.me>
2024-07-24 02:51:08 +0200

    Solution 20 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yf6fnfpd Prompt:       |                              ^~~~~~ ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp:39:37: error: ‘string’ was not declared  in this scope    39 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp:39:44: error: ‘b’ was not declared in t his scope    39 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp:39:45: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp:38: ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp: In function ‘int main()’: ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp:48:20: error: ‘split_words’ was not dec lared in this scope    48 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ ac0c305d-837b-4de3-b6d3-6e6fb45b0d15.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,48 +1,36 @@
-#include <vector>
-#include <string>
-
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-std::vector<std::string> split_words(string txt) {
+vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0, prev_pos = 0;
 
-    while (pos != string::npos) {
-        pos = txt.find(' ', prev_pos);
-        if (pos == string::npos) {
-            result.push_back(txt.substr(prev_pos));
-            break;
-        }
-        if (txt.find(',', pos) != string::npos) {
-            size_t comma_pos = txt.find(',', pos);
-            if (comma_pos < pos) {
-                pos = comma_pos;
-            }
-        }
-        result.push_back(txt.substr(prev_pos, pos - prev_pos));
-        prev_pos = pos + 1;
+    size_t pos = 0;
+    while ((pos = txt.find(' ')) != std::string::npos) {
+        result.push_back(txt.substr(0, pos));
+        txt.erase(0, pos + 1);
     }
 
-    if (result.empty()) {
-        int count = 0;
+    if (txt.empty()) {
+        return result;
+    }
+
+    pos = txt.find(',');
+    if (pos == std::string::npos) {
+        int oddCount = 0;
         for (char c : txt) {
-            if ((int)c >= 97 && (int)c <= 122 && (count & 1)) {
-                result.push_back(to_string(count));
+            if (std::isalpha(c)) {
+                if ((c - 'a') % 2 != 0) {
+                    oddCount++;
+                }
             }
-            count++;
+        }
+        result.push_back(std::to_string(oddCount));
+    } else {
+        while ((pos = txt.find(',')) != std::string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + 1);
+        }
+        if (!txt.empty()) {
+            result.push_back(txt);
         }
     }
 
     return result;
-}
-
-int main() {
-    assert (issame(split_words(""), {"0"}));
-    return 0;
 }
