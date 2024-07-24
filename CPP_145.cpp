5730e82a00db96d19ab832f6c1f11c7fb5367f8e
beep boop <githubbot@vadim.me>
2024-07-24 23:08:03 +0200

    Solution 28 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lmv36xmy Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cbe4ad7d-2f26-49a1-89dd-c846e371c373.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cbe4ad7d-2f26-49a1-89dd-c846e371c373.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cbe4ad7d-2f26-49a1-89dd-c846e371c373.cpp:26: cbe4ad7d-2f26-49a1-89dd-c846e371c373.cpp: In function ‘int main()’: cbe4ad7d-2f26-49a1-89dd-c846e371c373.cpp:36:20: error: ‘order_by_points’ was not  declared in this scope    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ cbe4ad7d-2f26-49a1-89dd-c846e371c373.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -10,30 +10,28 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
-vector<int> order_by_points(vector<int> nums) {
+vector<vector<int>> order_by_points(vector<vector<int>> nums) {
     vector<pair<int, int>> numSumIndex;
-    
+
     for (int i = 0; i < nums.size(); i++) {
         int sum = 0;
-        int n = nums[i];
-        while (n > 0) {
-            sum += n % 10;
-            n /= 10;
+        int n = 0;
+        for(int j = 0; j < nums[i].size(); j++) {
+            n = nums[i][j];
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
         }
         numSumIndex.push_back({sum, i});
     }
 
     sort(numSumIndex.begin(), numSumIndex.end());
     
-    vector<int> result;
+    vector<vector<int>> result;
     for (auto& p : numSumIndex) {
         result.push_back(nums[p.second]);
     }
     
     return result;
-}
-
-int main() {
-    // Your code to call order_by_points function
-    return 0;
 }
