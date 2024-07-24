2b89ffb9bcf25e1aae867865c9e70614fb512820
beep boop <githubbot@vadim.me>
2024-07-24 15:21:33 +0200

    Solution 24 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cs4nkzhf Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 2731541b-5946-48f3-bd08-d7c8751ecc78.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,17 +1,17 @@
-```cpp
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <initializer_list>
 
 using namespace std;
 
 int sum_squares(initializer_list<int> lst) {
     int total = 0;
-    for (int i : lst) {
+    for (int i = 0; i < lst.size(); i++) {
         if ((i + 1) % 3 == 0 && (i + 1) % 4 != 0)
-            total += pow(i, 2);
+            total += pow(lst.begin()[i], 2);
         else if ((i + 1) % 4 == 0 && (i + 1) % 3 != 0)
-            total += pow(i, 3);
+            total += pow(lst.begin()[i], 3);
     }
     return total;
 
