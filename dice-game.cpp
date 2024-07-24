1150473a2548c5a4b4230da7ef4189b958825687
beep boop <githubbot@vadim.me>
2024-07-24 04:50:57 +0200

    Solution 86 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/kq8h089b Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,24 +1,19 @@
-#include <iostream>
-using namespace std;
-
 int main() {
     int n, m;
-    cout << "Enter number of sides for Peter's die: ";
-    cin >> n;
-    cout << "Enter number of sides for Colin's die: ";
-    cin >> m;
+    std::cout << "Enter number of sides for Peter's die: ";
+    std::cin >> n;
+    std::cout << "Enter number of sides for Colin's die: ";
+    std::cin >> m;
     double result = getProbability(n, m);
-    cout << "The probability that Peter rolls strictly higher than Colin is: " << result << endl;
+    std::cout << "The probability that Peter rolls strictly higher than Colin is: " << result << std::endl;
     return 0;
 }
 
 double getProbability(int n, int m) {
-    double num = 0.0;
+    double totalPossibleOutcomes = (double)n*m;
+    double num = 0;
     for(int i=1; i<m; ++i){
-        num += (double)1/(m);
-    }
-    for(int i=m+1; i<=n; ++i){
-        num += (double)1/(n*m);
+        num += (double)1/totalPossibleOutcomes;
     }
     return num;
 }
