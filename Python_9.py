08b3433ac21bdde2e3f6f7741619bbcdf376ac1c
beep boop <githubbot@vadim.me>
2024-07-24 10:47:50 +0200

    Solution 46 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/0qnnz9fl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e886fd06-87e6-446f-b025-de9c14f05 d15.py", line 15, in <module>     check(rolling_max)           ^^^^^^^^^^^ NameError: name 'rolling_max' is not defined

--- Python_9.py
+++ Python_9.py
@@ -1,10 +1,14 @@
 from typing import List
 
+
 def rolling_max(numbers: List[int]) -> List[int]:
     max_seen_so_far = float("-inf")
     result = []
     for num in numbers:
         max_seen_so_far = max(num, max_seen_so_far)
         result.append(max_seen_so_far)
-
     return result
+
+
+def check(func):
+    print(func([1, 3, -5, 0, 4]))
