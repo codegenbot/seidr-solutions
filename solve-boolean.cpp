7f53d037bf054848e79cd68e421a9d2b60c36f7f
beep boop <githubbot@vadim.me>
2024-07-24 04:29:25 +0200

    Solution 39 of solve-boolean, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/lwsrif8y Prompt: bb11d7b9-6e80-4e0c-a51c-370592bf2ecb.cpp: In function ‘int main()’: bb11d7b9-6e80-4e0c-a51c-370592bf2ecb.cpp:22:5: error: ‘cin’ was not declared in this scope    22 |     cin >> expression;       |     ^~~ bb11d7b9-6e80-4e0c-a51c-370592bf2ecb.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; bb11d7b9-6e80-4e0c-a51c-370592bf2ecb.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << (solveBoolean(expression) ? "True" : "False");       |     ^~~~ bb11d7b9-6e80-4e0c-a51c-370592bf2ecb.cpp:23:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,20 +1,27 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
-bool solveBoolean(string s) {
+bool solveBoolean(string expression) {
     bool result = false;
-    for (char c : s) {
-        if (c == 'T' || c == 't') {
-            return true;
-        } else if (c == 'F' || c == 'f') {
-            return false;
-        } else if (c == '&') {
-            result = false;
+    for (char c : expression) {
+        if (c == 't') {
+            result = true;
+        } else if (c == 'f') {
+            return !result;
         } else if (c == '|') {
             bool temp = result;
-            result = false;
-            result |= temp;
+            result = !temp;
+        } else if (c == '&') {
+            result &= false; // & has lower precedence than |
         }
     }
     return result;
+}
+
+int main() {
+    string expression;
+    cin >> expression;
+    cout << (solveBoolean(expression) ? "True" : "False");
+    return 0;
 }
