5c01f5074d657ab763aba275d9d60d699e453dab
beep boop <githubbot@vadim.me>
2024-07-24 02:24:08 +0200

    Solution 49 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/walwymsv Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03d5331d-2c9f-4bca-b1b3-ec1b5a190 c12.py", line 11, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03d5331d-2c9f-4bca-b1b3-ec1b5a190 c12.py", line 8, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,13 +1,5 @@
-```
 def fizz_buzz(n: int):
+    output = ""
     for i in range(1, n + 1):
-        output = ""
-        if i % 15 == 0:
-            output = "FizzBuzz"
-        elif i % 3 == 0:
-            output = "Fizz"
-        elif i % 5 == 0:
-            output = "Buzz"
-        else:
-            output = str(i)
-        print(output)
+        output += "FizzBuzz" if i % 15 == 0 else ("Fizz" if i % 3 == 0 and i % 5 != 0 else "Buzz" if i % 5 == 0 and i % 3 != 0 else str(i))
+    return output
