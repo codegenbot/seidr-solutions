b15263789f7137cf5128de2fe0251c1d5ba09870
beep boop <githubbot@vadim.me>
2024-07-24 15:55:17 +0200

    Solution 34 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ho1yy1q3 Prompt: 35 is the result Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dc6e246a-782c-4aaf-ba7e-08005bbba 136.py", line 18, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dc6e246a-782c-4aaf-ba7e-08005bbba 136.py", line 14, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,10 @@
-def difference(lst):
-    odd_sum = sum(num ** 2 for num in lst if isinstance(num, int) and num >= 0 and num % 2 != 0)
-    average_diff = (sum(lst) - odd_sum) / len(lst) if len(lst) > 0 else None
-    return abs(average_diff - 2 * odd_sum)
+def double_the_difference():
+    lst = [1, 3, 5]
+    total = 0
+    for num in lst:
+        if isinstance(num, int) and num >= 0:
+            if num % 2 != 0:
+                total += num**2
+    return total
+
+print("{} is the result".format(double_the_difference()))
