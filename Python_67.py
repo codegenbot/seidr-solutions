2a4fa7545b77794a7b02b5910ad1c508b647258c
beep boop <githubbot@vadim.me>
2024-07-24 02:48:08 +0200

    Solution 20 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v4oavres Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a93cd78-2335-4407-b6fb-0d371ee95 418.py", line 18, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a93cd78-2335-4407-b6fb-0d371ee95 418.py", line 15, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -6,7 +6,7 @@ def fruit_distribution(s, n):
         if word.lower() == "apple" or word.lower() == "orange":
             total_fruit += 1
 
-    max_per_box = min(total_fruit // (n + 1), n)
-    remaining_fruits = total_fruit - (max_per_box * (n + 1))
+    max_per_box = min(total_fruit, n)
+    remaining_fruits = total_fruit - (max_per_box * n)
 
-    return int(-(-total_fruit / float(max_per_box)) + 0.5) if max_per_box != 0 else 0
+    return int(((total_fruit - 1) / max_per_box) + 0.5) if max_per_box != 0 else 0
