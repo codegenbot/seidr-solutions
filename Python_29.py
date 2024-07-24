5c5d7db27f7f86808973f5ceb774b9d9b946722b
beep boop <githubbot@vadim.me>
2024-07-24 11:11:12 +0200

    Solution 42 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/tf2z1fy4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/266e88a5-afdb-4db8-b2c2-02a8741f5 25f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,11 +1,7 @@
 from typing import List
 
-def filter_by_prefix(strings: List[str], prefix: str) -> None:
-    if strings and prefix:
-        result = [s for s in strings if s.startswith(prefix)]
-        print(result)
-    else:
-        print("Program did not receive expected input")
+def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
+    return [s for s in strings if s.startswith(prefix)]
 
-filter_by_prefix(["apple", "banana", "apricot"], "ap")
+print(filter_by_prefix(["apple", "banana", "apricot"], "ap"))
 print(filter_by_prefix(["apple", "banana", "cherry"], "ap"))
