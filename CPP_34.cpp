aab1f74c7752fc3727ca5f8560b0700c4a044622
beep boop <githubbot@vadim.me>
2024-07-25 01:05:01 +0200

    Solution 32 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/avztuzwn Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp:17:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main(){       |     ^~~~ In file included from fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp:16: fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp: In function ‘int main()’: fa52ffe0-d946-4ae2-b83b-3763e4647b41.cpp:26:20: error: ‘unique’ was not declared  in this scope    26 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -2,15 +2,14 @@
 #include <vector>
 #include <unordered_set>
 #include <cassert>
-#include <algorithm>
 
 using namespace std;
 
-bool issame(unordered_set<int> a, unordered_set<int> b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b){
+    return unordered_set<int>(a.begin(), a.end()) == unordered_set<int>(b.begin(), b.end());
 }
 
 int main(){
-    assert(issame(unordered_set<int>({5, 3, 5, 2, 3, 3, 9, 0, 123}), unordered_set<int>{0, 2, 3, 5, 9, 123}));
+    assert(issame(vector<int>({5, 3, 5, 2, 3, 3, 9, 0, 123}), vector<int>({0, 2, 3, 5, 9, 123})));
     return 0;
 }
