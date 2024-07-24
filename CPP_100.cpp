5be4c0fa9beb2cb3eefe95dac2d326bf9d49e31a
beep boop <githubbot@vadim.me>
2024-07-24 02:37:17 +0200

    Solution 69 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0eegrvg0 Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ 2ca9c939-dfea-4d2a-b42c-3e760b34e003.cpp:20:27:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 
@@ -11,22 +12,29 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> make_a_pile(int n) {
-    std::vector<std::vector<int>> pile;
-    int num = 1;
+std::vector<std::vector<int>> make_a_pile(int n) {
+    std::vector<std::vector<int>> piles;
     for (int i = 0; i < n; i++) {
+        std::vector<int> pile;
         if (i % 2 == 0) {
-            pile.push_back({num});
-            num += 2;
+            int num = 1;
+            for (int j = 0; j <= i / 2; j++) {
+                pile.push_back(num);
+                num += 2;
+            }
         } else {
-            pile.push_back({num});
-            num++;
+            int num = 1;
+            for (int j = 0; j < (i + 1) / 2; j++) {
+                pile.push_back(num);
+                num++;
+            }
         }
+        piles.push_back(pile);
     }
-    return pile[0];
+    return piles;
 }
 
 int main() {
-    assert(issame(make_a_pile(8), {1, 2, 3, 4, 5, 6, 7, 8}));
+    assert(issame(make_a_pile(8)[7], {1, 2, 3, 4, 5, 6, 7, 8}));
     return 0;
 }
