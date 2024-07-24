d278d1ff2626df36a94220454f47983678b28322
beep boop <githubbot@vadim.me>
2024-07-24 17:48:16 +0200

    Solution 76 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/iqu0ssj4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/71a20e3c-142e-4572-a80c-c2a88179d e45.py", line 12     def check(rounded_avg): SyntaxError: expected 'except' or 'finally' block

--- Python_103.py
+++ Python_103.py
@@ -1,22 +1,11 @@
 import statistics
 
-
-def check(rounded_avg):
-    if int(rounded_avg, 2) % 2 == 0:
-        print("The average is even.")
-    else:
-        print("The average is odd.")
-
-
 def average_rounded():
     while True:
         try:
             n = int(input("Enter first number: "))
             m = int(input("Enter second number: "))
             if n > m:
-                return -1
+                return 'Error'
             avg = round((n + m) / 2)
-            rounded_avg = bin(avg)[2:]
-            check(rounded_avg)
-        except ValueError:
-            print("Invalid input. Please enter integers.")
+            return bin(avg)[2:]
