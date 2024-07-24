d1a054a0d705f3d94023e5ac87f61b97bd0fcc9e
beep boop <githubbot@vadim.me>
2024-07-24 21:24:20 +0200

    Solution 90 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mas2a78y Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/111dbde5-fd37-4c14-a30a-2e5555b8f 7ea.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,18 +1,19 @@
 from typing import List
 
 
-def factorize(n: int) -> List[List[int]]:
+def prime_factors(n: int) -> List[List[int]]:
     factors = []
     i = 2
     while i * i <= n:
         if n % i:
             i += 1
         else:
+            n //= i
             count = 0
             while n % i == 0:
                 n //= i
                 count += 1
             factors.append([i] * count)
     if n > 1:
-        factors.append([n])
-    return [list(factor) for factor in set(tuple(sorted(factor)) for factor in factors)]
+        factors.append([[n]])
+    return [factor_list for factor_list in set(map(tuple, factors))]
