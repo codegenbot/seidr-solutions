9feaf12eaf50e223134626c4858f2dc634090c3e
beep boop <githubbot@vadim.me>
2024-07-24 03:33:24 +0200

    Solution 16 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1592ce6e-41c9-4061-9e60-bf042026f 7d6.py", line 16, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1592ce6e-41c9-4061-9e60-bf042026f 7d6.py", line 13, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1592ce6e-41c9-4061-9e60-bf042026f 7d6.py", line 4, in solve     if N is None:        ^ UnboundLocalError: cannot access local variable 'N' where it is not associated w ith a value

--- Python_84.py
+++ Python_84.py
@@ -1,7 +1,5 @@
 def solve(K=None):
-    K = int(input("Enter a number: ") if K is None else K)
-    N = int(input("Enter another number: ") if not isinstance(K, int) else 0)
-    N = int(N)  
-    if N < 1:
-        return str(0)
-    return str(K * ((N - 1) // K))
+    if K is None:
+        K = int(input("Enter a number: "))
+    result = str(K * ((N := int(input("Enter another number: "))) - 1) // K)
+    return result
