03ac95b151182241be087ac0a37da055b15b5017
beep boop <githubbot@vadim.me>
2024-07-24 16:03:28 +0200

    Solution 85 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pzxlydrl Prompt: In file included from /usr/include/c++/11/vector:67,                  from eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp:12: eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp: In function ‘int main()’: eed39a71-337d-4a34-bda9-c5c8c3aef081.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,14 +1,5 @@
-#include <vector>
-#include <utility>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<std::pair<int, int>> make_a_pile(int n) {
-    std::vector<std::pair<int, int>> pile;
+std::pair<int, int> make_a_pile(int n) {
     for (int i = 1; i <= n; i++) {
-        pile.push_back({2*i - 1, 2*i});
+        return {2*i - 1, 2*i};
     }
-    return pile;
 }
