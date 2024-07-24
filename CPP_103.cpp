a4cef3ff2c9b9b60d2ba100aba9ff1ba6c35e488
beep boop <githubbot@vadim.me>
2024-07-24 08:45:45 +0200

    Solution 48 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1yyc4nvv Prompt: 94da5e2e-30ee-4044-9c3f-4679d5ddd239.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string rounded_avg(int n,int m){       | ^~~~~~ In file included from 94da5e2e-30ee-4044-9c3f-4679d5ddd239.cpp:17: 94da5e2e-30ee-4044-9c3f-4679d5ddd239.cpp: In function ‘int main()’: 94da5e2e-30ee-4044-9c3f-4679d5ddd239.cpp:19:13: error: ‘rounded_avg’ was not dec lared in this scope    19 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,14 +1,20 @@
-#include <cmath>
+```cpp
+#include <string>
+#include <cmath> 
+string rounded_avg(int n,int m); 
 
-using namespace std;
-
-string rounded_avg(int n, int m) {
-    if (n > m) return "-1";
-    double sum = 0;
-    for (int i = n; i <= m; ++i) {
-        sum += i;
+string rounded_avg(int n,int m){
+    if(n > m) return "-1";
+    int sum = 0;
+    for(int i=n; i<=m; i++) sum += i;
+    double avg = (double)sum/(m-n+1);
+    int r = (int)floor(avg+0.5);
+    string res = "";
+    while(r > 0){
+        if(r & 1) res.push_back('1');
+        else res.push_back('0');
+        r >>= 1;
     }
-    double avg = floor((sum + 0.5) / (m - n + 1));
-    string res = to_string(avg);
-    return res.substr(0, res.length() - 2) + "b";
+    reverse(res.begin(),res.end());
+    return res;
 }
