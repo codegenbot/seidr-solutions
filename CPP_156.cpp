734aa520e6011bb3d3a7661f94906caf61e96c69
beep boop <githubbot@vadim.me>
2024-07-24 09:42:43 +0200

    Solution 34 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/puryu2f2 Prompt:       | ^ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp: In function ‘int testMain()’: 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:20:5: error: ‘assert’ was not declared in this scope    20 |     assert(int_to_mini_roman(1000) == "M");       |     ^~~~~~ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <string>   +++ |+#include <cassert>     4 | In file included from 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:26: 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp: In function ‘int main()’: 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:28:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    28 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,13 +1,12 @@
-```cpp
+```c++
 #include <vector>
 #include <string>
 #include <cassert>
-#include <iostream>
 
 std::vector<std::string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-std::string int_to_mini_roman(int number) {
+std::string int_to_roman(int number) {
     std::string result;
     for (int i = 0; i < 13; i++) {
         while (number >= val[i]) {
@@ -18,28 +17,9 @@ std::string int_to_mini_roman(int number) {
     return result;
 }
 
-std::string int_to_mini_roman(int n) {
-    if(n <= 0) return "Invalid input. Please enter a positive integer.";
-
-    std::string result = "";
-    int i = 12;
-
-    while (n > 0)
-    {
-        if (n >= val[i]) {
-            n -= val[i];
-            result += roman[i];
-        }
-        else {
-            i--;
-        }
-    }
-
-    return result;
-}
-
 int main() {
-    assert(int_to_mini_roman(1000) == "M");
-    std::cout << int_to_mini_roman(1999);
+    assert(int_to_roman(1000) == "M");
+    assert(int_to_roman(1999) == "MCMXCIX");
+    assert(int_to_roman(2024) == "MMXXIV");
     return 0;
 }
