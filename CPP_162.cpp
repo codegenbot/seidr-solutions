f90f707a6a5db9458a868426676b20b674cad922
beep boop <githubbot@vadim.me>
2024-07-24 14:38:54 +0200

    Solution 56 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/evk8w1n3 Prompt: 2cb5cbdb-cb8a-4937-a8b3-ad434765ab16.cpp: In function ‘std::string string_to_md5 (const string&)’: 2cb5cbdb-cb8a-4937-a8b3-ad434765ab16.cpp:12:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    12 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 2cb5cbdb-cb8a-4937-a8b3-ad434765ab16.cpp:4: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ 2cb5cbdb-cb8a-4937-a8b3-ad434765ab16.cpp: At global scope: 2cb5cbdb-cb8a-4937-a8b3-ad434765ab16.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 2cb5cbdb-cb8a-4937-a8b3-ad434765ab16.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,26 +8,20 @@ std::string string_to_md5(const std::string& text) {
         return "None";
     }
 
-    unsigned char digest[EVP_MAX_MD_SIZE];
     EVP_MD_CTX *mdctx;
-    const EVP_MD *md = EVP_md5();
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
 
     mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal_ex(mdctx, digest, NULL);
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
     EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
     for (int i = 0; i < 16; i++) {
-        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
+        sprintf(&mdString[i * 2], "%02x", (unsigned int)md_value[i]);
     }
 
     return std::string(mdString);
-}
-
-int main() {
-    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-
-    return 0;
 }
