66c18c8dd2d8180889f480ab208b03b6b255252f
beep boop <githubbot@vadim.me>
2024-07-24 04:06:13 +0200

    Solution 1 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/13qyj88i Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,12 +1,4 @@
 double find_zero(vector<double> xs){
-    double x = 1;
-    for (int i = 2; i < xs.size(); i += 2) {
-        while (poly(xs, x) > 0) {
-            x -= 0.001;
-        }
-        while (poly(xs, x) < 0) {
-            x += 0.001;
-        }
-    }
-    return round(x, 2);
+    double x = -xs[1] / (2*xs[0]);
+    return poly(xs,x) == 0 ? x : 0;
 }
