86d82d4b8c6949caf0479e6d46647fe229e8d7f5
beep boop <githubbot@vadim.me>
2024-07-24 23:25:00 +0200

    Solution 45 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/51vrqbmc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5530c814-0065-4f6f-bd8a-4179695a5 532.py", line 9, in <module>     check(right_angle_triangle)           ^^^^^^^^^^^^^^^^^^^^ NameError: name 'right_angle_triangle' is not defined

--- Python_157.py
+++ Python_157.py
@@ -1,7 +1,6 @@
 def right_angle_triangle(a, b, c):
     return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
 
-# Example usage:
 a = int(input("Enter the first side length: "))
 b = int(input("Enter the second side length: "))
 c = int(input("Enter the third side length: "))
