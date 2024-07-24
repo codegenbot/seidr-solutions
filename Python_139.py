59d8aafa2ebda3030cb995abd05ed58399bf1e00
beep boop <githubbot@vadim.me>
2024-07-24 04:48:36 +0200

    Solution 1 of Python/139, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2mapxuz4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/538354a6-e588-4da0-a791-efc221193 23f.py", line 15, in <module>     check(special_factorial)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/538354a6-e588-4da0-a791-efc221193 23f.py", line 12, in check     assert special_factorial(1) == 1, "Test 1"            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/538354a6-e588-4da0-a791-efc221193 23f.py", line 7, in special_factorial     result *= math.factorial(i)               ^^^^ NameError: name 'math' is not defined

--- Python_139.py
+++ Python_139.py
@@ -1,8 +1,11 @@
+import math
+
+
 def special_factorial(n):
     if n == 0:
         return 1
     else:
         result = 1
-        for i in range(1, n+1):
+        for i in range(1, n + 1):
             result *= math.factorial(i)
         return result
