edb1d22dc1d2ff0b1ace5e3ee79b85535bca53b6
beep boop <githubbot@vadim.me>
2024-07-24 11:32:09 +0200

    Solution 16 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/oe267eq1 Prompt:       |             ^~~~~~ 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp:4:20: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp:4:27: error: ‘vector’ was not declared in this scope     4 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp:3: 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp: In function ‘int main()’: 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp:13:20: error: ‘parse_nested_parens’ was  not declared in this scope    13 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 2198a50b-842b-47fa-889c-c4e612e94ae1.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,6 +1,5 @@
 #include <vector>
-#include <string>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
+    return a == b;
 }
