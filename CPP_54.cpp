141b96c1eee59581d36232aa7ff26bdcec6831b2
beep boop <githubbot@vadim.me>
2024-07-24 22:29:37 +0200

    Solution 30 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/w71tam8o Prompt:       |                                     ^ In file included from /usr/include/c++/11/cassert:44,                  from 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp:3: 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp: In function ‘int main()’: 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp:12:23: error: ‘same_chars’ cannot be us ed as a function    12 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~ 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp: At global scope: 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp:16: 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp: In function ‘int main()’: 231ebea7-a503-4eb2-b8db-890c7cf216d0.cpp:18:23: error: ‘same_chars’ cannot be us ed as a function    18 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 #include <string>
 #include <cassert>
-#include <iostream>
 
 bool same_chars(string s0, string s1) {
     sort(s0.begin(), s0.end());
@@ -11,6 +10,6 @@ bool same_chars(string s0, string s1) {
 }
 
 int main() {
-    assert (same_chars("aabb", "aaccc") == false);
-    cout << same_chars("abcde", "edcba");
+    assert (!same_chars("aabb", "aaccc"));  
+    return 0;
 }
