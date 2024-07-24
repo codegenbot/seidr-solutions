2d64f28d7b2e870a3ca479b22b60c86139bd7d0b
beep boop <githubbot@vadim.me>
2024-07-24 05:26:48 +0200

    Solution 28 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pbvkyomh Prompt:       |                    ^~~ 60026901-2d45-40a4-a63e-89c22e5f9650.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 60026901-2d45-40a4-a63e-89c22e5f9650.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 60026901-2d45-40a4-a63e-89c22e5f9650.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 60026901-2d45-40a4-a63e-89c22e5f9650.cpp:15: 60026901-2d45-40a4-a63e-89c22e5f9650.cpp: In function ‘int main()’: 60026901-2d45-40a4-a63e-89c22e5f9650.cpp:25:20: error: ‘strange_sort_list’ was n ot declared in this scope    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 60026901-2d45-40a4-a63e-89c22e5f9650.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
@@ -10,4 +14,15 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
         lst.erase(std::remove(lst.begin(), lst.end(), max_val), lst.end());
     }
     return result;
+}
+
+int main() {
+    std::vector<int> lst = {1, 3, 5, 7};
+    std::vector<int> result = strange_sort_list(lst);
+
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+
+    return 0;
 }
