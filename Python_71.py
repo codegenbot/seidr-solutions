d72f709d63f775c3610ac6a9830ebd9a69fd694c
beep boop <githubbot@vadim.me>
2024-07-24 02:39:09 +0200

    Solution 1 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/g50mmdtt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f0dddaa-7267-4662-bcac-d4de41ce1 c85.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_71.py
+++ Python_71.py
@@ -1,9 +1,20 @@
 ```
+"""
+Input: This program should read three sides of a triangle from user input.
+Task: Calculate and return the area of the triangle if the entered sides form a valid triangle. If not, return -1.
+Function signature: 
+def triangle_area(a, b, c):
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return round(area, 2)
+"""
+
 import math
 
 def triangle_area(a, b, c):
     if a + b > c and a + c > b and b + c > a:
         s = (a + b + c) / 2
-        return round(math.sqrt(s * (s - a) * (s - b) * (s - c)), 2)
+        area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+        return round(area, 2)
     else:
         return -1
