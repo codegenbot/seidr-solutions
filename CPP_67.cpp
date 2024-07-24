d259fd72638096fe01a7cb6aee702535f878feb3
beep boop <githubbot@vadim.me>
2024-07-24 10:14:21 +0200

    Solution 7 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fc5kxsq1 Prompt: cbc92117-ab16-4336-aba7-af6171014097.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ cbc92117-ab16-4336-aba7-af6171014097.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,15 +1,8 @@
-#include <string>
 #include <cstdio>
-#include <cassert>
 
-int fruit_distribution(const std::string& s, int n) {
+int fruit_distribution(const char* s, int n) {
     int apples, oranges, mango;
-    std::sscanf(s.c_str(), "%d apples and %d oranges", &apples, &oranges);
+    std::sscanf(s, "%d apples and %d oranges", &apples, &oranges);
     mango = n - apples - oranges;
     return mango;
-}
-
-int main() {
-    assert(fruit_distribution("1 apples and 100 oranges", 120) == 19);
-    return 0;
 }
