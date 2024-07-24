fbef1a77885959f826bc09d3c2fd8de39a76ce4f
beep boop <githubbot@vadim.me>
2024-07-24 04:05:50 +0200

    Solution 12 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/531to4j1 Prompt: Complete the following code given the task description and function signature.

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,9 +1,6 @@
-Here is the solution:
-
 string filp_case(string str){
     string result = "";
-    for(int i=0; i<str.length();i++){
-        char c = str[i];
+    for(char c : str){
         if(islower(c))
             result += toupper(c);
         else
