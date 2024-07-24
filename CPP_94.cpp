3f8291d7d8678be3f0b52a0560f2fc29369c920d
beep boop <githubbot@vadim.me>
2024-07-24 15:35:58 +0200

    Solution 63 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/allb7foy Prompt:       |     ^~~~ f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp:21:5: error: expected unqualified-id be fore ‘return’    21 |     return 0;       |     ^~~~~~ f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp:22:1: error: expected declaration befor e ‘}’ token    22 | }       | ^ f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp:25:10: error: ‘int main()’ redeclared a s different kind of entity    25 | int main(){       |          ^ f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp:1:5: note: previous declaration ‘int ma in’     1 | int main {       |     ^~~~ In file included from f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp:24: f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp: In function ‘int main()’: f0cb0614-efe4-49fb-92b5-bff33a5a3482.cpp:26:13: error: ‘skjkasdkd’ was not decla red in this scope    26 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,41 +1,18 @@
-```cpp
-#include <iostream>
-#include <vector>
-using namespace std;
-
-bool isPrime(int n) {
-    if (n <= 1)
-        return false;
-    for (int i = 2; i * i <= n; i++)
-        if (n % i == 0)
-            return false;
-    return true;
-}
-
-int sumOfDigits(int n) {
-    int sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n /= 10;
-    }
-    return sum;
-}
-
 int main() {
     vector<int> lst;
     int num, i;
     cout << "Enter the number of elements in the list: ";
     cin >> num;
-    for(int i = 0; i < num; i++){
+    for(i = 0; i < num; i++){
         cout << "Enter element " << i+1 << ": ";
         int x; 
         cin >> x; 
         lst.push_back(x);
     }
     int maxPrimeNum = 0;
-    for(int i : lst){
-        if(isPrime(i) && i > maxPrimeNum)
-            maxPrimeNum = i;
+    for(int j : lst){
+        if(isPrime(j) && j > maxPrimeNum)
+            maxPrimeNum = j;
     }
     if(maxPrimeNum > 0)
         cout << "The sum of digits of the maximum prime is: " << sumOfDigits(maxPrimeNum) << endl;
