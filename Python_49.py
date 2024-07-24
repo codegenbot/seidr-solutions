684bd41f06dd58da42ad9c1e44c6e397d4907902
beep boop <githubbot@vadim.me>
2024-07-24 23:41:26 +0200

    Solution 68 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/hqexafdi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b088044-171e-40ec-9c31-2a0bb774b b43.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b088044-171e-40ec-9c31-2a0bb774b b43.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,13 +1,11 @@
 def modp(n: int, p: int):
-    def extended_euclidean(a, b):
+    def extended_euclidean(a: int, b: int) -> tuple:
         if a == 0:
-            return b, 0, 1
-        else:
-            gcd, x, y = extended_euclidean(b % a, a)
-            return gcd, y - (b // a) * x, x
+            return (b, 0, 1)
+        gcd, x, y = extended_euclidean(b % a, a)
+        return (gcd, y - (b // a) * x, x)
 
-    gcd, x, _ = extended_euclidean(p, n)
-    if gcd != 1:
+    g, x, _ = extended_euclidean(p, n)
+    if g != 1:
         return None
-    else:
-        return pow(n, p - 2, p) * n % p
+    return pow(n, p - 2, p) * n % p
