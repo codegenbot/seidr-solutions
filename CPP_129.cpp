b50e071a89e04552161cebb0cd5ec81681390e88
beep boop <githubbot@vadim.me>
2024-07-24 10:25:54 +0200

    Solution 48 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1hf2owjk Prompt:    31 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:31:39: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:30: 77c002fb-0745-4df8-8cda-c590ececacdb.cpp: In function ‘int main()’: 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:40:20: error: ‘minPath’ was not declare d in this scope    40 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,38 +1,36 @@
-vector<int> minPath(vector<vector<int>> grid, int k) {
+#include <vector>
+#include <algorithm>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) return false;
+    return a == b;
+}
+
+std::vector<int> minPath(std::vector<std::vector<int>>& grid, int k){
     int n = grid.size();
-    vector<vector<int>> visited(n, vector<int>(n));
-    queue<pair<pair<int, int>, vector<int>>> q;
     vector<int> res;
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (!visited[i][j]) {
-                visited[i][j] = true;
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            if(res.empty() || res.back() != grid[i][j]) {
                 res.push_back(grid[i][j]);
-                q.push({{i, j}, res});
-                
-                while (!q.empty()) {
-                    auto [pos, path] = q.front();
-                    q.pop();
-                    
-                    for (int dir = 0; dir < 4; dir++) {
-                        int x = pos.first + (dir % 2 ? -1 : 1);
-                        int y = pos.second + (dir == 0 ? -1 : 1);
-                        
-                        if (x >= 0 && x < n && y >= 0 && y < n && !visited[x][y]) {
-                            visited[x][y] = true;
-                            res.push_back(grid[x][y]);
-                            q.push({{-path.size(), {x, y}, path}});
-                            
-                            if ((int)res.size() == k) {
-                                return res;
-                            }
+                k--;
+                if(k == 0) return res;
+                int dir[8] = {1, -1, n, -n, n+1, -n-1, n+1, -n-1};
+                for(int d = 0; d < 8; ++d) {
+                    int ni = i + dir[d]/n, nj = j + dir[d]%n;
+                    if(ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                        if(k > 1) {
+                            res.push_back(grid[ni][nj]);
+                            k--;
+                            if(k == 0) return res;
+                        } else {
+                            res.push_back(grid[ni][nj]);
+                            return res;
                         }
                     }
                 }
             }
         }
     }
-    
-    return {};
+    return res;
 }
