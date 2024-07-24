538b95c0379316b5ea262a1a6309616eaed04045
beep boop <githubbot@vadim.me>
2024-07-24 03:50:29 +0200

    Solution 12 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f29592c6-5bfd-4744-9a5f-337f4a8ae e7f.py", line 34, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f29592c6-5bfd-4744-9a5f-337f4a8ae e7f.py", line 31, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f29592c6-5bfd-4744-9a5f-337f4a8ae e7f.py", line 27, in by_length     return [name_dict[i] for i in result]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f29592c6-5bfd-4744-9a5f-337f4a8ae e7f.py", line 27, in <listcomp>     return [name_dict[i] for i in result]             ~~~~~~~~~^^^ KeyError: '9'

--- Python_105.py
+++ Python_105.py
@@ -1,2 +1,4 @@
 ```
-return [''.join([name_dict[i] if i < 10 else name_dict[int(i)] for i in [str(i) for i in arr if 1 <= i <= 19]]).replace("Twenty", "Tw").replace("Eighteen", "Eight")
+def by_length(arr):
+    result = sorted([str(i) for i in arr if 1 <= i <= 19], key=len)
+    return [name_dict[int(x)] for x in result]
