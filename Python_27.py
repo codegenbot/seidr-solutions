8af905b58c1fc2d6448b2613454e21d9738f0174
beep boop <githubbot@vadim.me>
2024-07-24 13:16:47 +0200

    Solution 35 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/u59jyrm1 Prompt: Program did not receive expected input

--- Python_27.py
+++ Python_27.py
@@ -2,12 +2,8 @@ def check():
     string = input("Enter a string: ").strip()
     return modify_string(string)
 
-
 def modify_string(string):
-    return "".join(
-        [char.lower() if char.isupper() else char.upper() for char in string]
-    )
-
+    return "".join([char.lower() if char.isupper() else char.upper() for char in string])
 
 output = check()
 print(output)
