f53f2b835f1690ae724ab144c1dacb7ffba31beb
beep boop <githubbot@vadim.me>
2024-07-24 19:57:20 +0200

    Solution 20 of CPP/77, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2fuq328c Prompt: Program failed

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -3,6 +3,8 @@ bool iscuber(int a){
     while (i * i * i <= a) {
         if (i * i * i == a)
             return true;
+        if (i * i * i > a)
+            return false;
         i++;
     }
     return false;
