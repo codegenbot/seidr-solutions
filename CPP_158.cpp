b390b27319e73dbec6a282d0d66c93da24590b11
beep boop <githubbot@vadim.me>
2024-07-24 03:30:12 +0200

    Solution 6 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bp8gwipc Prompt: d8ffb61c-0828-4a69-aea6-276a55792cd4.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string find_max(vector<string> words){       | ^~~~~~ In file included from d8ffb61c-0828-4a69-aea6-276a55792cd4.cpp:10: d8ffb61c-0828-4a69-aea6-276a55792cd4.cpp: In function ‘int main()’: d8ffb61c-0828-4a69-aea6-276a55792cd4.cpp:12:14: error: ‘find_max’ was not declar ed in this scope    12 |     assert ((find_max({"play", "play", "play"}) == "play"));       |              ^~~~~~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -1,9 +1,8 @@
-#include <algorithm>
 #include <string>
 
-std::string find_max(std::vector<std::string> words){
-    std::string result = *minmax_element(words.begin(), words.end(),
-        [](const std::string& a, const std::string& b) {
+string find_max(vector<string> words){
+    string result = *minmax_element(words.begin(), words.end(),
+        [](const string& a, const string& b) {
             return (a.size() << 8) + count(a.begin(), a.end(), unique_copy(a.begin(), a.end())) <
                    (b.size() << 8) + count(b.begin(), b.end(), unique_copy(b.begin(), b.end()));
         }).first;
