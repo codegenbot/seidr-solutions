cee895c73f3b91ead7c7d75a41d3e6126e728ba5
beep boop <githubbot@vadim.me>
2024-07-24 06:53:48 +0200

    Solution 12 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wrmqzne4 Prompt: Complete the following code given the task description and function signature.

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,14 +1,8 @@
-#include <vector>
-
-using namespace std;
-
 int add_elements(vector<int> arr, int k) {
     int sum = 0;
-    for (int i = 0; i < min(k, (int)arr.size()); i++) {
-        if ((long)abs(arr[i]) <= 99) {
+    for (int i = 0; i < k; i++) {
+        if ((arr[i] >= 10 && arr[i] <= 99)) {
             sum += arr[i];
-        } else {
-            break;
         }
     }
     return sum;
