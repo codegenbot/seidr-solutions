d93afaf9cd4a72e128fc96a4a90bda99998ed815
beep boop <githubbot@vadim.me>
2024-07-24 09:09:39 +0200

    Solution 9 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1hf2owjk Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -4,60 +4,42 @@ using namespace std;
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n, 1e9));
-    vector<vector<pair<int, int>>> adj(n, vector<pair<int, int>>(n));
-
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j) {
-            if (i > 0)
-                adj[i][j].push_back({dp[i - 1][j], 1});
-            if (i < n - 1)
-                adj[i][j].push_back({dp[i + 1][j], 2});
-            if (j > 0)
-                adj[i][j].push_back({dp[i][j - 1], 3});
-            if (j < n - 1)
-                adj[i][j].push_back({dp[i][j + 1], 4});
-        }
-
-    priority_queue<pair<int, int>> pq;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (grid[i][j] == 1) {
-                pq.push({-grid[i][j], i * n + j});
-                dp[i][j] = 0;
+    vector<vector<int>> dp(n, vector<int>(n));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = grid[i][j];
+            } else if (i > 0 && j > 0) {
+                dp[i][j] = min({grid[i][j], dp[i-1][j], dp[i][j-1]});
+            } else if (i > 0) {
+                dp[i][j] = dp[i-1][j];
+            } else {
+                dp[i][j] = dp[i][j-1];
             }
         }
     }
 
-    vector<int> res(k);
-    for (int i = 0; i < k; ++i) {
-        int val, x, y;
-        pq.pop();
-        val = -pq.top().first;
-        tie(x, y) = make_pair(pq.top().second / n, pq.top().second % n);
-
-        res[i] = val;
-
-        for (auto &p : adj[x][y]) {
-            int nx = p.first == 1 ? x - 1 : (p.first == 2 ? x + 1 : x);
-            int ny = p.second == 3 ? y - 1 : (p.second == 4 ? y + 1 : y);
-
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                dp[nx][ny] = min(dp[nx][ny], dp[x][y] + val);
-                pq.push({-dp[nx][ny], nx * n + ny});
-            }
-        }
+    vector<int> res;
+    int i = n - 1, j = n - 1;
+    for (int l = 0; l < k; l++) {
+        res.push_back(grid[i][j]);
+        if (i > 0 && j > 0) {
+            if (grid[i-1][j] <= grid[i][j-1]) i--;
+            else j--;
+        } else if (i > 0) i--;
+        else j--;
     }
-
+    
     return res;
 }
 
 int main() {
-    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    vector<vector<int>> grid = {{1,2,3}, {4,5,6}, {7,8,9}};
     int k = 3;
     vector<int> result = minPath(grid, k);
-    for (auto &x : result) {
-        cout << x << " ";
+    for (int i : result) {
+        cout << i << " ";
     }
     return 0;
 }
