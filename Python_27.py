7d019e2e40b44ae04d407d486934f31103462fa9
beep boop <githubbot@vadim.me>
2024-07-24 15:07:30 +0200

    Solution 89 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/u59jyrm1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1e7b44f-c651-4642-93e2-ec12d7681 4c4.py", line 11, in <module>     check(flip_case)           ^^^^^^^^^ NameError: name 'flip_case' is not defined

--- Python_27.py
+++ Python_27.py
@@ -1,11 +1,14 @@
 def flip_case(char):
     return char.upper() if char.islower() else char.lower()
 
+
 def modify_string(string):
-    return ''.join(flip_case(char) for char in string)
+    return "".join(flip_case(char) for char in string)
+
 
 def check():
     string = input("Enter a string containing both upper and lower case letters: ")
     return modify_string(string)
+
 
 print(check())
