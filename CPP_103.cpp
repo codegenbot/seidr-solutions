0733899b3781352649d9010b96cdca0b45bf395e
beep boop <githubbot@vadim.me>
2024-07-24 02:27:54 +0200

    Solution 35 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/toc2yftm Prompt: 064e97d1-7320-402b-8a60-46ee78e94fc8.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string rounded_avg(int n, int m) {       | ^~~~~~ In file included from 064e97d1-7320-402b-8a60-46ee78e94fc8.cpp:24: 064e97d1-7320-402b-8a60-46ee78e94fc8.cpp: In function ‘int main()’: 064e97d1-7320-402b-8a60-46ee78e94fc8.cpp:26:13: error: ‘rounded_avg’ was not dec lared in this scope    26 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <cmath>
 
 string rounded_avg(int n, int m) {
     if (n > m) return "-1";
@@ -12,11 +13,10 @@ string rounded_avg(int n, int m) {
     while (avg > 0) {
         if (avg >= 2) {
             avg -= 2;
-            bin.append("1");
+            bin.push_back('1');
         } else {
             avg++;
-            bin.append("0");
+            bin.push_back('0');
         }
     }
     return bin;
-}
