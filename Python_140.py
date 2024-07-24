5968acb8df45d15b1e3ac2cba08094eff0fb7845
beep boop <githubbot@vadim.me>
2024-07-24 16:37:25 +0200

    Solution 50 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/7kk15bku Prompt: Program did not receive expected input

--- Python_140.py
+++ Python_140.py
@@ -1,10 +1,13 @@
-```
+```Python
 import re
 
 def fix_spaces(text):
-    if not text:  
-        return "Invalid input"
-    text = text.strip().replace(" ", "_").replace("\n", "")
+    if not text:
+        return ""
+
+    text = text.replace("\n", " ")
+
+    text = text.strip().replace(" ", "_").replace('"', '').replace("'", '')
     while "__" in text:
         text = text.replace("__", "_-")
     return text
