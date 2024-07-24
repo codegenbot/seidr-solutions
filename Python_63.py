c525cae8038079739e115dd208f1791fc7a82418
beep boop <githubbot@vadim.me>
2024-07-24 11:56:00 +0200

    Solution 16 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xckmmzw6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee58e35e-12bd-453e-bb82-6ead56708 661.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,14 +1,10 @@
-def find_sum_of_fibonacci(n: int):
+def fibfib(n: int):
     if n <= 1:
         return 0
     elif n == 2:
         return 1
     else:
-        a, b = 0, 1
-        total_sum = 0
+        a, b, c = 0, 1, 1
         for _ in range(2, n + 1):
-            a, b = b, a + b
-            if a > 4000000:  # If the number becomes greater than 4,000,000 break the loop.
-                break
-            total_sum += a
-        return total_sum
+            a, b, c = b, c, a + b
+        return c
