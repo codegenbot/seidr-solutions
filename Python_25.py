2737cecb521d3c34d88cfb6f87867652502b6a6e
beep boop <githubbot@vadim.me>
2024-07-24 09:40:54 +0200

    Solution 35 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zuwq41q4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9ab5499-2d5e-4c1d-89b3-be98924a1 d73.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,6 +1,14 @@
-def find_max_value(lst: List[int]) -> int:
-    max_val = float("-inf")
-    for num in lst:
-        if num > max_val:
-            max_val = num
-    return max_val
+from typing import List
+import math
+
+
+def factorize(n: int) -> List[int]:
+    factors = []
+    for i in range(2, n + 1):
+        if n % i == 0:
+            count = 0
+            while n % i == 0:
+                n //= i
+                count += 1
+            factors.extend([i] * count)
+    return factors
