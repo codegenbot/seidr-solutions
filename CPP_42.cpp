e06a50078ffd603530476b6ace772078b9577c9e
beep boop <githubbot@vadim.me>
2024-07-24 10:12:05 +0200

    Solution 89 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/l5wh96cl Prompt:       |                                  ^~~ 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp:26: 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp: In function ‘int main()’: 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp:36:20: error: ‘incr_list’ was not decla red in this scope; did you mean ‘std::incr_list’?    36 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp:4:22: note: ‘std::incr_list’ declared h ere     4 |     std::vector<int> incr_list(std::vector<int> l) {       |                      ^~~~~~~~~ In file included from 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp:26: 79ad5e54-dbc1-4e72-bc49-fce3ede50a0e.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,22 +1,30 @@
 #include <vector>
+#include <cassert>
 
-std::vector<int> incr_list(std::vector<int> l) {
-    for (int i = 0; i < l.size(); i++) {
-        l[i]++;
-    }
-    return l;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
+namespace my_namespace {
+    std::vector<int> incr_list(std::vector<int> l) {
+        for (int i = 0; i < l.size(); i++) {
+            l[i]++;
+        }
+        return l;
     }
 
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
+    bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+        if (a.size() != b.size()) {
             return false;
         }
-    }
 
-    return true;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+int main() {
+    using namespace my_namespace;
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
 }
