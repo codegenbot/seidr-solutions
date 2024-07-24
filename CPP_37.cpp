f4852f88fc8a8c7c7a30c5ea92ea0e20ec52dae3
beep boop <githubbot@vadim.me>
2024-07-24 19:14:51 +0200

    Solution 91 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cvfj3xyj Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 36c1af89-d7f6-48b5-8a29-917df7e48404.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -13,15 +13,13 @@ std::vector<float> sort_even(std::vector<float> vec) {
     std::vector<float> result;
     for (int i = 0; i < vec.size(); i++) {
         if (i % 2 == 0) {
-            std::vector<float> temp;
             int j = i;
             while (j < vec.size() && j % 2 == 0) {
-                temp.push_back(vec[j++]);
+                result.push_back(vec[j++]);
             }
-            std::sort(temp.begin(), temp.end());
-            for (int k = 0; k < temp.size(); k++) {
-                result.push_back(temp[k]);
-                j++;
+            std::sort(result.begin(), result.end());
+            for (int k = 0; k < result.size(); k++) {
+                result.push_back(result[k]);
             }
             while (j < vec.size()) {
                 result.push_back(vec[j++]);
