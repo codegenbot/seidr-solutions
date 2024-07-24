a4e949f9946a09cd25e757d67e4e2ebbc5b5b165
beep boop <githubbot@vadim.me>
2024-07-24 13:14:58 +0200

    Solution 94 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/2yk639c5 Prompt: 150c58c9-6d11-49a0-a136-fb4f3e62c20a.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string middleCharacter(string s) {       | ^~~~~~       | stdin

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,16 +1,16 @@
 #include <sstream>
-char* middleCharacter(char *s) {
+
+char* middleCharacter(char* s) {
     int length = 0;
-    for(int i=0; s[i]; i++) {
+    for(int i = 0; s[i] != '\0'; i++) {
         length++;
     }
     if (length % 2 == 0) {
-        ostringstream os;
-        os << s[length/2-1] << s[length/2];
-        return strdup(os.str().c_str());
+        return &s[length / 2 - 1];
     } else {
-        ostringstream os;
-        os << s[length/2];
-        return strdup(os.str().c_str());
+        char c;
+        c = s[length / 2];
+        c += '\0';
+        return &c;
     }
 }
