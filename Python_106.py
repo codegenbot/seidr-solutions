3371ee561a792dae21af5d61bd1e11b761546662
beep boop <githubbot@vadim.me>
2024-07-24 16:13:07 +0200

    Solution 26 of Python/106, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6j1r4qn6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/243c659c-6714-44f7-b8b1-890e1c742 da7.py", line 13, in <module>     check(f)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/243c659c-6714-44f7-b8b1-890e1c742 da7.py", line 10, in check     assert f(3) == [1, 2, 6]            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_106.py
+++ Python_106.py
@@ -1,5 +1,8 @@
+from math import factorial
+
+
 def f(n):
     return [
-        1 if i == 0 else math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1))
-        for i in range(n)
+        1 if i == 0 else factorial(i) if i % 2 == 0 else sum(range(1, i + 1))
+        for i in range(1, n + 1)
     ]
