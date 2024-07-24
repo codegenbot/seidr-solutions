bf71d2e7feca13a52f26710b8a67829ec819b107
beep boop <githubbot@vadim.me>
2024-07-24 04:46:57 +0200

    Solution 44 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zgcn7j6z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f834799-6ba2-4e8f-8561-d68c80979 8d9.py", line 24, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f834799-6ba2-4e8f-8561-d68c80979 8d9.py", line 21, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f834799-6ba2-4e8f-8561-d68c80979 8d9.py", line 16, in max_fill     min_height_right = grid[i][right]                        ~~~~~~~^^^^^^^ IndexError: list index out of range

--- Python_115.py
+++ Python_115.py
@@ -3,7 +3,7 @@ def max_fill(grid, capacity):
     total_water = 0
     left = 0
     right = len(grid[0]) - 1
-    for i in range(len(grid)):
+    while left <= right:
         min_height_left = grid[i][left]
         min_height_right = grid[i][right]
         while min_height_left <= min_height_right:
@@ -13,10 +13,6 @@ def max_fill(grid, capacity):
             else:
                 total_water += capacity
                 right -= 1
-            if right > 0 and min_height_left <= min_height_right:
-                min_height_left = grid[i][left]
-                min_height_right = grid[i][right]
-            else:
-                min_height_left = grid[i][left + 1]
-                min_height_right = grid[i][right - 1]
+            min_height_left = grid[i][left]
+            min_height_right = grid[i][right]
     return total_water
