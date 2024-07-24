4a75916fef30834344a840a8bf836394de036cde
beep boop <githubbot@vadim.me>
2024-07-24 05:59:56 +0200

    Solution 53 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pbvkyomh Prompt:       |                    ^~~ 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp:19: 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp: In function ‘int main()’: 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp:29:20: error: ‘strange_sort_list’ was n ot declared in this scope    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 4e6682a7-3814-4a42-b3a1-37f53bd62f20.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,16 +1,40 @@
 #include <vector>
 #include <algorithm>
 
-bool areSame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
+using namespace std;
+
+vector<int> strange_sort_vector(vector<int> lst) {
+    vector<int> result;
+    while (!lst.empty()) {
+        int min_val = *min_element(lst.begin(), lst.end());
+        int max_val = *max_element(lst.begin(), lst.end());
+        if (min_val == max_val) {
+            result.push_back(min_val);
+            lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+        } else {
+            result.push_back(min_val);
+            result.push_back(max_val);
+            lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+            lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
         }
     }
+    return result;
+}
+
+int main() {
+    vector<int> input;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        input.push_back(num);
+    }
     
-    return true;
+    vector<int> output = strange_sort_vector(input);
+    
+    for(int i : output) {
+        cout << i << " ";
+    }
+    return 0;
 }
