cb1d9dbf20fcfadff30b4487858af81ad4881733
beep boop <githubbot@vadim.me>
2024-07-24 08:01:28 +0200

    Solution 0 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ln0czjdy Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

---
+++ spin-words.cpp
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+
+string spinWords(string str) {
+    string result = "";
+    int i = 0;
+    while (i < str.length()) {
+        if (str.find(" ", i) == -1 || i + 1 >= str.length())
+            result += str.substr(i);
+        else {
+            string word = str.substr(i, str.find(" ", i) - i);
+            if (word.length() >= 5)
+                result += string(word.rbegin(), word.rend()) + " ";
+            else
+                result += word + " ";
+        }
+        i = str.find(" ", i) + 1;
+    }
+    return result;
+}
