ebe280b44c00c6c93d38a2810f3cceb48565267c
beep boop <githubbot@vadim.me>
2024-07-25 01:04:24 +0200

    Solution 33 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/y2ckq095 Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘int’ and ‘const int’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,14 +1,8 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int>& a, std::vector<int>& b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
     return a == b;
-}
-
-int main() {
-    assert(issame({5, 6, 3, 4, 8, 9, 2, 1}, {2, 6, 3, 4, 8, 9, 5, 1}));
-    return 0;
 }
