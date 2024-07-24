6f9cbf1657ee41ba039e153a7d6c141664e1dfad
beep boop <githubbot@vadim.me>
2024-07-24 16:03:39 +0200

    Solution 48 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf049cb0-80eb-4b7e-be98-ea5fe7de2 b94.py", line 13, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf049cb0-80eb-4b7e-be98-ea5fe7de2 b94.py", line 10, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -3,4 +3,4 @@ def sorted_list_sum(words=None):
         lst = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
     else:
         lst = [word.strip().lower() for word in words]
-    return sum(len(word) for word in sorted(lst))
+    return sum([int(''.join(c for c in word)) for word in sorted(lst) if len(word) % 2 == 0])
