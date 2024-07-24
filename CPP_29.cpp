ee69220521e0a8bb4ba43cac3aaac7b44ec5f223
beep boop <githubbot@vadim.me>
2024-07-24 17:50:48 +0200

    Solution 31 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/r99dr0xy Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6951cac2-60fd-4505-a23b-831269fcf87a.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6951cac2-60fd-4505-a23b-831269fcf87a.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6951cac2-60fd-4505-a23b-831269fcf87a.cpp:10: 6951cac2-60fd-4505-a23b-831269fcf87a.cpp: In function ‘int main()’: 6951cac2-60fd-4505-a23b-831269fcf87a.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 6951cac2-60fd-4505-a23b-831269fcf87a.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,12 +1,9 @@
 vector<bool> issame(vector<string> a, vector<string> b) {
     vector<bool> result;
-    for(int i = 0; i < a.size(); i++){
-        bool same = true;
-        for(int j = 0; j < a[i].size(); j++){
-            if(a[i][j] != b[i][j]){
-                same = false;
-                break;
-            }
+    for(int i = 0; i < a.size(); i++) {
+        bool same = false;
+        if(i < b.size()) {
+            same = (a[i] == b[i]);
         }
         result.push_back(same);
     }
