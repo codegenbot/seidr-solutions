4eb41436e9d4950cf471749233fbbbe85b17c0ff
beep boop <githubbot@vadim.me>
2024-07-24 03:05:16 +0200

    Solution 33 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yf6fnfpd Prompt:       |                              ^~~~~~ 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp:43:37: error: ‘string’ was not declared  in this scope    43 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp:43:44: error: ‘b’ was not declared in t his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp:43:45: error: expression list treated a s compound expression in initializer [-fpermissive]    43 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp:42: 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp: In function ‘int main()’: 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp:52:20: error: ‘split_words’ was not dec lared in this scope    52 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 87400dd2-ee6d-4d8c-b95b-13fe7e8d1801.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,23 +1,21 @@
-```cpp
 #include <string>
 #include <vector>
 
-bool issame(vector<string> a,vector<string>b){
-    if(a==b) return true;
-    else return false;
-}
-
-vector<string> split_words(string txt) {
-    vector<string> result;
+std::vector<std::string> split_words(std::string txt) {
+    std::vector<std::string> result;
     size_t pos = 0, prev_pos = 0;
 
-    while ((pos = txt.find_first_of(" ,")) != string::npos) {
+    while ((pos = txt.find_first_of(" ,")) != std::string::npos) {
         if (prev_pos == pos) {
             break;
         }
-        if (txt.substr(prev_pos, pos - prev_pos).find_first_of(' ') == string::npos) {
+        if (txt.substr(prev_pos, pos - prev_pos).find_first_not_of(' ') == std::string::npos) {
+            result.push_back(txt.substr(prev_pos, pos - prev_pos));
+            prev_pos = pos + 1;
+        } else {
+            string word = txt.substr(prev_pos, pos - prev_pos);
             int count = 0;
-            for (char c : txt.substr(prev_pos, pos - prev_pos)) {
+            for (char c : word) {
                 if (c >= 'a' && c <= 'z') {
                     count++;
                 }
@@ -25,9 +23,6 @@ vector<string> split_words(string txt) {
             result.push_back(to_string(count));
             break;
         }
-        string word = txt.substr(prev_pos, pos - prev_pos);
-        result.push_back(word);
-        prev_pos = pos + 1;
     }
 
     if (prev_pos < txt.size()) {
