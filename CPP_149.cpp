0401c81e880695ad4b1710298148c492c374ce62
beep boop <githubbot@vadim.me>
2024-07-24 18:13:11 +0200

    Solution 96 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cmheh2hi Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from be28ff2e-dbe3-41ef-b738-64621e9098a7.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,10 +1,7 @@
+```
 #include <algorithm>
 #include <vector>
 #include <string>
-#include <initializer_list>
-
-using namespace std;
-typedef size_t initializer_list(size_type);
 
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
