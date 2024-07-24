cc41e71f55f8c26c5ffbd096684cd8a61cda2e2b
beep boop <githubbot@vadim.me>
2024-07-24 20:35:04 +0200

    Solution 4 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mpmfr9pr Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b99bd757-55ef-4a97-a316-6c84dd471 9ab.py", line 16, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b99bd757-55ef-4a97-a316-6c84dd471 9ab.py", line 12, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,8 +1,7 @@
 def fix_spaces(text):
-    text = text.replace(" ", "_")
-    while "__" in text or "--" in text:
-        if "--" in text:
-            text = text.replace("--", "-")
-        else:
-            text = text.replace("__", "_")
+    text = text.strip().replace(" ", "_")
+    while "--" in text or "---" in text or "----" in text:
+        text = text.replace("--", "-")
+        text = text.replace("---", "-")
+        text = text.replace("----", "-")
     return text
