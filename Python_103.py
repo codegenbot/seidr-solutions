e13113a3ade26c0634a4bc7b516916c66d53be85
beep boop <githubbot@vadim.me>
2024-07-24 15:18:28 +0200

    Solution 34 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/iqu0ssj4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c2920f5-f128-4de7-9156-dd6cbc3aa af8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_103.py
+++ Python_103.py
@@ -1,9 +1,5 @@
 def rounded_avg(n, m):
     if n > m:
         return -1
-    avg = (n + m) / 2.0  
-    if int(avg) == avg:
-        return bin(int(round(avg)))[2:]
-    else:
-        avg_int = int(round(avg))
-        return bin(avg_int)[2:]
+    avg = (n + m) / 2  
+    return bin(int(round(avg)))[2:]
