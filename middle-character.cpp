056988b818af102ce5dfff1dd6d84b18118bc534
beep boop <githubbot@vadim.me>
2024-07-24 05:27:40 +0200

    Solution 17 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/2yk639c5 Prompt: b22222f0-5e6e-437d-81b4-b1f933361b2e.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string middleCharacter(string str) {       | ^~~~~~       | stdin

--- middle-character.cpp
+++ middle-character.cpp
@@ -1,10 +1,12 @@
+```cpp
+#include <string>
 using namespace std;
 
 string middleCharacter(string str) {
     int len = str.length();
     if (len % 2 != 0) {
-        return string(1, str.substr(len / 2, 1));
+        return string(1, str[len / 2]);
     } else {
-        return str.substr((len - 1) / 2, 1) + str.substr(len / 2, 1);
+        return string(1, str[(len - 1) / 2]) + string(1, str[len / 2]);
     }
 }
