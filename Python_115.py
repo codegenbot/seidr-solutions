c35f76c5cbfdef15a75407dc4e2e7586c6016880
beep boop <githubbot@vadim.me>
2024-07-24 14:45:54 +0200

    Solution 16 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/915ue9pj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03a6579c-4469-451c-b164-1264a2693 8d4.py", line 13, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03a6579c-4469-451c-b164-1264a2693 8d4.py", line 10, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,7 +1,4 @@
-import math
-
-
 def max_fill(grid, capacity):
     total_water = sum(sum(row) for row in grid)
-    total_buckets = -(-total_water // float(capacity))
-    return total_buckets
+    min_buckets = math.ceil(total_water / float(capacity))
+    return min_buckets
