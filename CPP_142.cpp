a3d95198802e74762aec362cbade0f3a81561cbe
beep boop <githubbot@vadim.me>
2024-07-24 16:52:40 +0200

    Solution 61 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cs4nkzhf Prompt: 819bde12-7807-479d-b145-238bb680b0d3.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ 819bde12-7807-479d-b145-238bb680b0d3.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from 819bde12-7807-479d-b145-238bb680b0d3.cpp:17: 819bde12-7807-479d-b145-238bb680b0d3.cpp: In function ‘int main()’: 819bde12-7807-479d-b145-238bb680b0d3.cpp:19:24: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    19 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                        |       |                        <brace-enclosed initializer list> 819bde12-7807-479d-b145-238bb680b0d3.cpp:4:21: note:   initializing argument 1 o f ‘int sum_squares(int)’     4 | int sum_squares(int n) {       |                 ~~~~^

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -10,6 +10,7 @@ int sum_squares(int n) {
 }
 
 int main() {
-    std::cout << "Sum of squares is: " << sum_squares(1*1 + 3*3 + 17*17 + 1*1 + 15*15 + 13*13 + 1*1 + 14*14 + 14*14 + 12*12 + 5*5) << std::endl;
+    int numbers[] = {1*1 + 3*3 + 17*17 + 1*1 + 15*15 + 13*13 + 1*1 + 14*14 + 14*14 + 12*12 + 5*5};
+    std::cout << "Sum of squares is: " << sum_squares(*numbers) << std::endl;
     return 0;
 }
