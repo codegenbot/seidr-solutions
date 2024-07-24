d8176831b3625fd09716f518301ae65190b0fc65
beep boop <githubbot@vadim.me>
2024-07-24 06:00:13 +0200

    Solution 64 of Python/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7178n3hw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f05dd9dc-5bcc-422a-9546-b6103152e fe5.py", line 10, in <module>     check(count_upper)           ^^^^^^^^^^^ NameError: name 'count_upper' is not defined. Did you mean: 'count_uppercase'?

--- Python_98.py
+++ Python_98.py
@@ -1,3 +1,3 @@
-```
-def count_upper(s):
-    return sum(1 for i, c in enumerate(s) if 'A' <= c <= 'E' and i % 2 == 0)
+def count_upper():
+    s = input()
+    print(sum(1 for c in s if c.isupper()))
