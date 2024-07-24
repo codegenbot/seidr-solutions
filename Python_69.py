4fb6e9d0b0a94e6eee01f37cabb313c8d1bf8a52
beep boop <githubbot@vadim.me>
2024-07-24 17:48:18 +0200

    Solution 84 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/h8ma863b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e078286e-21d8-4798-b343-e9adc1dc7 ab1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,4 +1,12 @@
 ```
+def search(lst):
+    max_prime = -1
+    for num in lst:
+        if isinstance(num, int) and is_prime(num) and num > max_prime:
+            max_prime = num
+    return max_prime
+
+
 def is_prime(n):
     if n <= 1:
         return False
@@ -7,13 +15,6 @@ def is_prime(n):
             return False
     return True
 
-
-def search(lst):
-    max_prime = -1
-    for num in lst:
-        if isinstance(num, int) and is_prime(num) and num > max_prime:
-            max_prime = num
-    return max_prime
 
 nums = list(map(int, input("Enter a series of space-separated numbers: ").split()))
 if not nums or not all(isinstance(num, int) for num in nums):
