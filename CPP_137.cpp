1f2a9508e6ca87a0e1ed9a1863b5af03409b8ff8
beep boop <githubbot@vadim.me>
2024-07-24 16:14:30 +0200

    Solution 59 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7vjwb7qj Prompt:       |       ^ In file included from f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp:43: f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp: In function ‘int main()’: f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp:45:13: error: ‘boost’ has not been decl ared    45 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp:45:29: error: ‘string’ was not declared  in this scope    45 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp:45:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from f4852f56-5bba-4373-a0cd-8e6e5a683549.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -4,18 +4,30 @@
 
 std::variant<std::string, int, float> compare_one(std::variant<std::any> a, std::variant<std::any> b) {
     return std::visit([&](auto&& a, auto&& b) -> std::variant<std::string, int, float> {
-        if (std::holds_alternative<std::any>(a) && std::holds_alternative<int>(b)) {
-            if (std::any_cast<int>(a) > std::any_cast<int>(b)) {
+        if (std::holds_alternative<std::string>(a) && std::holds_alternative<std::string>(b)) {
+            if (std::any_cast<std::string>(a) > std::any_cast<std::string>(b)) {
                 return "Greater";
-            } else if (std::any_cast<int>(a) < std::any_cast<int>(b)) {
+            } else if (std::any_cast<std::string>(a) < std::any_cast<std::string>(b)) {
                 return "Less";
             } else {
                 return "Equal";
             }
-        } else if (std::holds_alternative<std::any>(a) && std::holds_alternative<float>(b)) {
-            if (std::any_cast<int>(a) > std::any_cast<float>(b)) {
+        } else if (std::holds_alternative<int>(a) && std::holds_alternative<int>(b)) {
+            int aInt = std::any_cast<int>(a);
+            int bInt = std::any_cast<int>(b);
+            if (aInt > bInt) {
                 return "Greater";
-            } else if (std::any_cast<int>(a) < std::any_cast<float>(b)) {
+            } else if (aInt < bInt) {
+                return "Less";
+            } else {
+                return "Equal";
+            }
+        } else if (std::holds_alternative<float>(a) && std::holds_alternative<float>(b)) {
+            float aFloat = std::any_cast<float>(a);
+            float bFloat = std::any_cast<float>(b);
+            if (aFloat > bFloat) {
+                return "Greater";
+            } else if (aFloat < bFloat) {
                 return "Less";
             } else {
                 return "Equal";
@@ -24,4 +36,29 @@ std::variant<std::string, int, float> compare_one(std::variant<std::any> a, std:
             return "Non";
         }
     }, a, b);
+}
+
+int main() {
+    std::string input1;
+    int input2;
+
+    std::cout << "Enter the first value: ";
+    std::cin >> input1;
+
+    std::cout << "Enter the second value: ";
+    std::cin >> input2;
+
+    auto result = compare_one(std::any(input1), std::any(input2));
+
+    if (std::holds_alternative<stdstring>(result)) {
+        std::cout << "The comparison is: " << std::any_cast<std::string>(result) << std::endl;
+    } else if (std::holds_alternative<int>(result)) {
+        int value = std::any_cast<int>(result);
+        std::cout << "The comparison is: " << value << std::endl;
+    } else {
+        float value = std::any_cast<float>(result);
+        std::cout << "The comparison is: " << value << std::endl;
+    }
+
+    return 0;
 }
