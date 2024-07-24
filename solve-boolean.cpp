91ba6a4d7512cdb104f0a52443b86c1c5de18bdc
beep boop <githubbot@vadim.me>
2024-07-24 15:15:38 +0200

    Solution 41 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/sysiwsqx Prompt: 8b0b5b30-6468-4de2-a7a4-f527fb51964d.cpp: In function ‘bool solveBoolean(std::st ring)’: 8b0b5b30-6468-4de2-a7a4-f527fb51964d.cpp:5:53: error: ‘find_first_not_of’ is not  a member of ‘std’     5 |     size_t left_end = std::distance(s.begin(), std::find_first_not_of(s,  '|', s.length()));       |                                                     ^~~~~~~~~~~~~~~~~ 8b0b5b30-6468-4de2-a7a4-f527fb51964d.cpp:11:58: error: ‘find_first_not_of’ is no t a member of ‘std’    11 |         size_t right_end = std::distance(s.begin(), std::find_first_not_ of(s, '&', s.length()));       |                                                          ^~~~~~~~~~~~~~~ ~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,1 +1,16 @@
-size_t left_end = std::distance(s.begin(), std::find_first_not_of(s, '|', s.length()));
+bool solveBoolean(std::string s) {
+    size_t left_end = std::distance(s.begin(), std::find_first_of(s, "TF"));
+    if (left_end == s.length()) {
+        return s[0] == 'T';
+    } else {
+        bool left = s.substr(0, left_end).back() == 'T';
+        size_t right_start = left_end;
+        size_t right_end = std::distance(s.begin(), std::find_first_of(s, "TF"));
+        if (right_end == s.length()) {
+            return s[0] == 'T' ? !left : left;
+        } else {
+            bool right = s.substr(right_start, right_end - right_start).back() == 'T';
+            return (s[left_end-1] == '&') ? (left && right) : (left || right);
+        }
+    }
+}
