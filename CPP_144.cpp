442eff7f4f68b0ed83ebe5ba8a88d85ab3e36782
beep boop <githubbot@vadim.me>
2024-07-24 11:33:14 +0200

    Solution 11 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yhgwwqcf Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,19 +1,17 @@
-int gcd(int a, int b) {
-    if (b == 0)
+int getGCD(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
         return a;
-    return gcd(b, a % b);
-}
+    }
 
-bool simplify(string x, string n){
-    int numerator1 = stoi(x.substr(0, x.find("/")));
-    int denominator1 = stoi(x.substr(x.find("/") + 1));
-    int numerator2 = stoi(n.substr(0, n.find("/")));
-    int denominator2 = stoi(n.substr(n.find("/") + 1));
-    
-    int product_numerator = numerator1 * numerator2;
-    int product_denominator = denominator1 * denominator2;
-    
-    int common_factor = gcd(product_numerator, product_denominator);
-    
-    return (product_denominator / common_factor == 1);
-}
+    bool simplify(string x, string n){
+        int num1 = stoi(x.substr(0, x.find('/')));
+        int den1 = stoi(x.substr(x.find('/') + 1));
+        int num2 = stoi(n.substr(0, n.find('/')));
+        int den2 = stoi(n.substr(n.find('/') + 1));
+
+        return (num1 * den2 == num2 * den1);
+    }
