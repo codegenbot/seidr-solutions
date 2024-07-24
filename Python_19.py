c947f04e4f2f30cc2df6624d2ebf5e8c3479009e
beep boop <githubbot@vadim.me>
2024-07-24 17:44:09 +0200

    Solution 20 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2bi7dxo6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6998eb8-cdb2-4baa-b181-807c32e25 c64.py", line 23, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6998eb8-cdb2-4baa-b181-807c32e25 c64.py", line 18, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,6 +1,20 @@
 ```
+import re
+
 def sort_numbers(numbers: str) -> str:
-    num_dict = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
-                'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
-    nums = [num_dict[i] for i in numbers.split()]
-    return ' '.join(map(str, sorted(nums)))
+    num_dict = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    }
+    
+    nums = [num_dict.get(re.match(r"(\bzero|one|two|three|four|five|six|seven|eight|nine\b)(s?)\b", word).group(1)) for word in numbers.split()]
+    
+    return " ".join(map(str, sorted(nums)))
