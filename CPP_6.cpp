7ceb8a4cf18d722dd37ab489f3d9b437db9f070d
beep boop <githubbot@vadim.me>
2024-07-24 15:05:39 +0200

    Solution 58 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/v7yn6lhf Prompt:       |                    ^~~ 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp:24: 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp: In function ‘int main()’: 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 46f3da17-bd8d-4cec-af87-f06bebbae633.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -2,23 +2,9 @@
 #include <algorithm>
 #include <string>
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<int> parse_nested_parens(string paren_string);
 
-int main() {
-    // Main function implementation (if any)
-}
+bool issame(vector<int> a, vector<int> b) {
 
 vector<int> parse_nested_parens(string paren_string) {
     vector<int> depths;
