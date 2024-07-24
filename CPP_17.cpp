3bd33d4fb0827cf26334956d6d9d22a45fee65dd
beep boop <githubbot@vadim.me>
2024-07-24 15:42:10 +0200

    Solution 18 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/dnhp00qb Prompt:    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7290d2c2-ddcb-476a-9e5f-80fdc1a69db6.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7290d2c2-ddcb-476a-9e5f-80fdc1a69db6.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7290d2c2-ddcb-476a-9e5f-80fdc1a69db6.cpp:23: 7290d2c2-ddcb-476a-9e5f-80fdc1a69db6.cpp: In function ‘int main()’: 7290d2c2-ddcb-476a-9e5f-80fdc1a69db6.cpp:33:20: error: ‘parse_music’ was not dec lared in this scope    33 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 7290d2c2-ddcb-476a-9e5f-80fdc1a69db6.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,21 +1,4 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
+bool issame(const vector<int>& a, const vector<int>& b);
 
 vector<int> parse_music(string music_string);
 
@@ -39,17 +22,4 @@ vector<int> parse_music(string music_string){
     beats.push_back(note_duration + 1);
     
     return beats;
-}
-
-int main() {
-    string music_string;
-    cin >> music_string;
-    
-    vector<int> result = parse_music(music_string);
-    
-    for (int beat : result) {
-        cout << beat << " ";
-    }
-    
-    return 0;
 }
