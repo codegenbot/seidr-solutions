71bbea9d598ae4af037aa8a99089ebc7c1967d89
beep boop <githubbot@vadim.me>
2024-07-24 06:32:16 +0200

    Solution 97 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4pstys41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bdf238e9-ea47-4870-9f03-5781591dd 7e7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_80.py
+++ Python_80.py
@@ -7,7 +7,6 @@ def next_digit_sum(n):
         n //= 10
     return total
 
-
 def is_happy(n):
     seen = set()
     while True:
@@ -19,6 +18,5 @@ def is_happy(n):
             return False
         seen.add(str(n))
         n = next_digit_sum(n)
-
 
 print(is_happy(int(input("Enter a number: "))))
