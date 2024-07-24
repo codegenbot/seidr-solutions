dfd4929bdfabd54704eed66a0f568cf563f28d0f
beep boop <githubbot@vadim.me>
2024-07-24 05:29:55 +0200

    Solution 92 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/qwyfb73y Prompt: d5c2a420-e92b-4458-a822-f265dc7a5ef1.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string camelCase(string s) {       | ^~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,32 +1,21 @@
-#include <iostream>
-#include <string>
-
 using namespace std;
 
 string camelCase(string s) {
-    int i = 0;
+    if (s[0] == '-') s = s.substr(1);
     string result = "";
-    while (i < s.length()) {
+    bool capitalizeNext = true;
+    
+    for (int i = 0; i < s.length(); i++) {
         if (s[i] == '-') {
-            i++;
-            while (i < s.length() && s[i] != ' ') {
-                result += toupper(s[i]);
-                i++;
-            }
-            if (result.size() > 0) result += '';
+            s = s.substr(i + 1);
+            capitalizeNext = true;
+        } else if (capitalizeNext) {
+            result += toupper(s[i]);
+            capitalizeNext = false;
         } else {
             result += tolower(s[i]);
         }
-        i++;
     }
+    
     return result;
-}
-
-int main() {
-    string input;
-    cout << "Enter a string in kebab-case: ";
-    getline(cin, input);
-    string output = camelCase(input);
-    cout << "CamelCase output: " << output << endl;
-    return 0;
 }
