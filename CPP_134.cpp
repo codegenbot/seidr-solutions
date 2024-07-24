6af429d71b6fba84260c9e8e6862a844e09d1332
beep boop <githubbot@vadim.me>
2024-07-24 21:58:39 +0200

    Solution 14 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9pc9qxd3 Prompt: 29ef368e-607d-4007-b176-f75d481a24d6.cpp:1:37: error: ‘string’ was not declared in this scope     1 | bool check_if_last_char_is_a_letter(string txt) {       |                                     ^~~~~~ In file included from 29ef368e-607d-4007-b176-f75d481a24d6.cpp:24: 29ef368e-607d-4007-b176-f75d481a24d6.cpp: In function ‘int main()’: 29ef368e-607d-4007-b176-f75d481a24d6.cpp:26:43: error: ‘check_if_last_char_is_a_ letter’ cannot be used as a function    26 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,9 +1,8 @@
-```cpp
 #include <string>
 
-bool check_if_last_char_is_a_letter(std::string txt) {
+bool check_if_last_char_is_a_letter(string txt) {
     if (txt.empty()) return false;
-    std::string last_char = txt.substr(txt.length()-1, 1);
+    string last_char = txt.substr(txt.length()-1, 1);
     for(int i=0; i<txt.length()-1; i++){
         if(txt[i] == ' '){
             if(last_char == txt.substr(i+1,1)){
