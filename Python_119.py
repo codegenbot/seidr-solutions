fb89c48ada1971f71dd495f98a10cd3bab2cfd3d
beep boop <githubbot@vadim.me>
2024-07-24 18:17:04 +0200

    Solution 74 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fdajg309 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d20c7fe7-50ed-4771-9eb2-47af2b222 d2b.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,23 +1,13 @@
-```
-import re
-
-
+```Python
 def match_parens(lst):
     for s in lst:
         open_count = 0
         close_count = 0
-        nesting_diff = 0
         for char in s:
             if char == "(":
                 open_count += 1
-                nesting_diff += 1
             elif char == ")":
                 close_count += 1
-                if open_count > 0:
-                    open_count -= 1
-                    nesting_diff -= 1
-                else:
-                    return "No"
-        if nesting_diff != 0:
+        if open_count != close_count:
             return "No"
     return "Yes"
