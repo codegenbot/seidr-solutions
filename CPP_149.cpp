57bded903ff3d559158b197c8a90fd1d56c0a986
beep boop <githubbot@vadim.me>
2024-07-24 09:52:51 +0200

    Solution 66 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7meh2yxx Prompt: )’    37 | int main(){       |     ^~~~ 7932e8f6-a2a2-4397-9084-c372117feafb.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 7932e8f6-a2a2-4397-9084-c372117feafb.cpp:28: 7932e8f6-a2a2-4397-9084-c372117feafb.cpp: In function ‘int main()’: 7932e8f6-a2a2-4397-9084-c372117feafb.cpp:38:35: error: could not convert ‘{"aaaa ", "bbbb", "dd", "cc"}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector <std::vector<std::__cxx11::basic_string<char> > >’    38 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   <brace-enclosed initializer list> 7932e8f6-a2a2-4397-9084-c372117feafb.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,35 +1,5 @@
-#include <vector>
-#include <string>
-
-bool issame(const vector<string>& a, const vector<string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int sorted_list_sum(const vector<string>& lst) {
-    int sum = 0;
-    for (const auto& str : lst) {
-        std::vector<std::string> split_str = split(str, ' ');
-        if(issame({{"hello", "world"}}, vector<string>(split_str.begin() + 1, split_str.end()))) {
-            sum += std::stoi(str.substr(6));
-        }
-    }
-    return sum;
-}
-
-std::vector<std::string> split(const std::string& s, char delimiter) {
-    std::stringstream ss(s);
-    std::string item;
-    std::vector<std::string> items;
-    while (std::getline(ss, item, delimiter)) {
-        items.push_back(item);
-    }
-    return items;
+int main() {
+    std::vector<std::vector<std::string>> lst = {{{"aaaa", "bbbb"}, {"dd"}, {"cc"}}};
+    assert(sorted_list_sum(lst) == 2);
+    return 0;
 }
