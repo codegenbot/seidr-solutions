071d76a049ee3aa1aec8e44d20082ded8a3373e8
beep boop <githubbot@vadim.me>
2024-07-24 05:05:53 +0200

    Solution 67 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Program did not receive expected input

--- Python_84.py
+++ Python_84.py
@@ -1,6 +1,6 @@
-K = int(input("Enter a number (0 or 1): "))
-if K == 0 or K == 1:
-    if K == 1:
+K = input("Enter a number (0 or 1): ")
+if K == "0" or K == "1":
+    if K == "1":
         print("0")
     else:
         print("1")
