8df48c4c2441baefea689443d2e78832d3d7f2fe
beep boop <githubbot@vadim.me>
2024-07-24 02:47:54 +0200

    Solution 14 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt: 8030b972-b9da-4c2c-b15d-a6eb5e248257.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,41 +1,42 @@
 #include <boost/any.hpp>
 #include <string>
 #include <typeinfo>
+#include <iostream>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)a > (float)b ? a : b;
+        return ((int)a > (float)b ? a : b).type() == typeid(int) ? a : b;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         string str = any_cast<string>(b);
         size_t pos = str.find(',');
         int num = stoi(str.substr(0, pos));
-        return (int)a > num ? a : b;
+        return ((int)a > num ? a : b).type() == typeid(int) ? a : b;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (float)a > (int)b ? a : b;
+        return ((float)a > (int)b ? a : b);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         size_t posA = any_cast<string>(a).find(',');
         size_t posB = any_cast<string>(b).find(',');
         int numA = stoi(any_cast<string>(a).substr(0, posA));
         int numB = stoi(any_cast<string>(b).substr(0, posB));
-        return numA > numB ? a : b;
+        return (numA > numB ? a : b);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(float)) {
         size_t pos = any_cast<string>(a).find(',');
         int num = (int)b;
         string str = any_cast<string>(a);
         size_t pos2 = str.find(',');
-        return stoi(str.substr(0, pos2)) > num ? a : b;
+        return (stoi(str.substr(0, pos2)) > num ? a : b);
     }
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         int num = (int)b;
         string str = any_cast<string>(a);
         size_t pos = str.find(',');
-        return (float)a > stoi(str.substr(0, pos)) ? a : b;
+        return ((float)a > stoi(str.substr(0, pos)) ? a : b);
     }
     else {
         return boost::any("None");
