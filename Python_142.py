4382bfdc69271004f3996ba0f74c60b026ce5e9c
beep boop <githubbot@vadim.me>
2024-07-24 20:48:01 +0200

    Solution 24 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9wzm29ar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56a0e32a-4b92-4064-9bd8-abad81002 88d.py", line 39, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56a0e32a-4b92-4064-9bd8-abad81002 88d.py", line 9, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,8 +1,3 @@
 ```Python
 def sum_squares(lst):
-    if all(isinstance(i, int) for i in lst):  
-        return sum(
-            i**3 if i % 4 == 0 and i % 3 != 0 else i**2 if i % 3 == 0 else i for i in lst
-        )
-    else:
-        return "Error: Input should be a list of integers."
+    return sum(i**2 if i % 4 == 0 or i % 3 == 0 else i for i in lst)
