7d45b44365de753d41fdcc5167c0af0f8ed7f6d1
beep boop <githubbot@vadim.me>
2024-07-24 02:13:21 +0200

    Solution 77 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a4620e0-790d-420e-ba52-670629006 ce2.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a4620e0-790d-420e-ba52-670629006 ce2.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,13 +1,2 @@
 def sort_even(l: list):
-    evens = sorted([x for x in l if 0 <= x % 2], key=lambda x: abs(x))
-    odds = [x for x in l if abs(x) % 2 != 0]
-    odds.sort(reverse=True)
-    result = []
-    for x in l:
-        if 0 <= x % 2:
-            result.append(next((y for y in evens if abs(y) == abs(x)), None))
-            evens.remove(result[-1])
-        else:
-            result.append(next((y for y in odds if abs(y) == abs(x)), None))
-            odds.remove(result[-1])
-    return tuple(result)
+    return tuple(sorted([x for x in l if 0 <= x % 2], key=abs)) + tuple(sorted([x for x in l if 0 > x % 2], reverse=True))
