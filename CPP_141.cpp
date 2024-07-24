e3677a8248e5e0adb59e0ac2057867b9b1ce1533
beep boop <githubbot@vadim.me>
2024-07-24 23:07:46 +0200

    Solution 12 of CPP/141, ratio 0.8461538461538461 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/738552qk Prompt:     1 | ```       |  ^ 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp:3:1: error: expected unqualified-id bef ore ‘while’     3 | while (i < file_name.size()) {       | ^~~~~ 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp:15:1: error: expected unqualified-id be fore ‘if’    15 | if (j != file_name.size()) {       | ^~ 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp:22:1: error: expected unqualified-id be fore ‘return’    22 | return "Yes";       | ^~~~~~ In file included from 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp:24: 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp: In function ‘int main()’: 4fa3ca06-edde-4fb7-9067-fc1ddfa02cd2.cpp:26:13: error: ‘file_name_check’ was not  declared in this scope    26 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,22 +1,26 @@
-```
-int i = 0, j = 0, digit_count = 0;
-while (i < file_name.size()) {
-    if (isdigit(file_name[i])) {
-        digit_count++;
-        i++;
-        while (i < file_name.size() && isdigit(file_name[i]))
+#include <string>
+#include <algorithm>
+
+std::string file_name_check(const std::string& filename) {
+    int i = 0, j = 0, digit_count = 0;
+    while (i < filename.size()) {
+        if (isdigit(filename[i])) {
+            digit_count++;
             i++;
-    } else if (file_name[i] == '.') {
-        j = i + 1;
-        break;
-    } else
-        i++;
+            while (i < filename.size() && isdigit(filename[i]))
+                i++;
+        } else if (filename[i] == '.') {
+            j = i + 1;
+            break;
+        } else
+            i++;
+    }
+    if (j != filename.size()) {
+        std::string before_dot = filename.substr(0, j);
+        std::string after_dot = filename.substr(j + 1);
+        if (digit_count > 3 || before_dot.empty() || !isalpha(before_dot[0]) ||
+            (after_dot != "txt" && after_dot != "exe" && after_dot != "dll"))
+            return "No";
+    }
+    return "Yes";
 }
-if (j != file_name.size()) {
-    string before_dot = file_name.substr(0, j);
-    string after_dot = file_name.substr(j + 1);
-    if (digit_count > 3 || before_dot.empty() || !isalpha(before_dot[0]) ||
-        (after_dot != "txt" && after_dot != "exe" && after_dot != "dll"))
-        return "No";
-}
-return "Yes";
