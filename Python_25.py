6b8ec43cfed9d081bae6ed55799555505a68b802
beep boop <githubbot@vadim.me>
2024-07-24 11:39:41 +0200

    Solution 34 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/tvxz0bbe Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c79d835-b842-4112-a867-f81a05aac 162.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -2,7 +2,7 @@ from typing import List
 
 def factorize(n: int) -> List[int]:
     if n <= 0:
-        return ["Invalid input"]
+        raise ValueError("Expected positive integer")
     factors = []
     i = 2
     while i * i <= n:
@@ -16,6 +16,3 @@ def factorize(n: int) -> List[int]:
     if n > 1:
         factors.append(n)
     return factors
-
-n = int(input("Enter a number: "))
-print(factorize(n))
