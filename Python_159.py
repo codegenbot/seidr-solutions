dead173b6dfb0703ec06c03c5d23b000ee4e2c70
beep boop <githubbot@vadim.me>
2024-07-25 01:18:49 +0200

    Solution 2 of Python/159, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4ysda40q Prompt: Complete the following code given the task description and function signature.

--- Python_159.py
+++ Python_159.py
@@ -1,5 +1,5 @@
 def eat(number, need, remaining):
     return [
-        number + need - remaining if number + need > remaining else number + need,
-        remaining - (number + need - remaining) if number + need > remaining else 0,
+        number + need - remaining if remaining >= need else number + remaining,
+        remaining - (need - remaining) if remaining >= need else 0,
     ]
