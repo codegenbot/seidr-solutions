8df3c1f48068b5546fbbae2fc352bd12aab90fa8
beep boop <githubbot@vadim.me>
2024-07-24 19:29:09 +0200

    Solution 5 of Python/78, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/e1ui40z5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/74edaa17-d7e0-4aab-b33b-16b1757b6 4a2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_78.py
+++ Python_78.py
@@ -1,10 +1,17 @@
 ```
 def hex_key(num):
+    def is_hex(digit):
+        if '0' <= digit <= '9':
+            return True
+        elif 'A' <= digit <= 'F':
+            return True
+        elif 'a' <= digit <= 'f':
+            return True
+        else:
+            return False
+
     count = 0
-    for digit in num.upper():
-        if digit.isdigit() and digit in '123':
-            if int(digit) % 2 == 1:
-                count += 1
-        elif digit in 'BDF':
+    for char in num.upper():
+        if is_hex(char):
             count += 1
     return count
