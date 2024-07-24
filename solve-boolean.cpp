89b8dfa71057650a317a55017bb6b75897553b6a
beep boop <githubbot@vadim.me>
2024-07-24 14:19:54 +0200

    Solution 48 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/0nczt1hw Prompt: e9ae1972-04b0-4589-a159-3836993b543a.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string s) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,33 +1,29 @@
+#include <stack>
 #include <string>
-using namespace std;
 
 bool solveBoolean(string s) {
-    bool res = true;
-    int i = 0;
-    while (i < s.length()) {
-        if (s[i] == 'T') {
-            i++;
-        } else if (s[i] == 'F') {
-            i++;
-        } else if (s[i] == '|') {
-            res |= s[i + 1] == 'T';
-            i += 2;
-        } else if (s[i] == '&') {
-            res &= s[i + 1] == 'T';
-            i += 2;
+    stack<char> st;
+    bool res = false;
+
+    for(int i=s.length()-1; i>=0; i--) {
+        if(s[i] == '&') {
+            res = (st.top() == 'T');
+            st.pop();
+        } else if(s[i] == '|') {
+            while(!st.empty() && st.top() == 'F') {
+                st.pop();
+            }
+            if(st.empty()) {
+                res = true;
+            } else {
+                res = (st.top() == 'T');
+            }
+            st.pop();
+        } else if(s[i] == 't' || s[i] == 'T') {
+            st.push('T');
+        } else if(s[i] == 'f' || s[i] == 'F') {
+            st.push('F');
         }
     }
-    return res;
-}
 
-int main() {
-    string input;
-    cout << "Enter the Boolean expression: ";
-    cin >> input;
-    bool result = solveBoolean(input);
-    if (result)
-        cout << "The resulting Boolean is true." << endl;
-    else
-        cout << "The resulting Boolean is false." << endl;
-    return 0;
-}
+    return res;
