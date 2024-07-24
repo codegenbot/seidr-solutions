a894a8385a04a928d89aaa02ac4d3e0bf9194e7a
beep boop <githubbot@vadim.me>
2024-07-24 23:19:04 +0200

    Solution 2 of Python/56, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ja8t4ybe Prompt: Complete the following code given the task description and function signature.

--- Python_56.py
+++ Python_56.py
@@ -1,10 +1,9 @@
-```
 def correct_bracketing(brackets: str):
     stack = []
     for bracket in brackets:
         if bracket == "<":
             stack.append("<")
         elif bracket == ">":
-            if not stack or stack.pop() != "<":
+            if len(stack) == 0 or stack.pop() != "<":
                 return False
-    return not stack
+    return len(stack) == 0
