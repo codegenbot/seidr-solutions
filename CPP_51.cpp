a386a4f9334482d310d045971edea27810d217e3
beep boop <githubbot@vadim.me>
2024-07-24 19:36:25 +0200

    Solution 10 of CPP/51, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ljx1tcu9 Prompt: d9fea811-dd7c-43b5-a285-bc45aef7076c.cpp: In function ‘int main()’: d9fea811-dd7c-43b5-a285-bc45aef7076c.cpp:20:5: error: ‘assert’ was not declared in this scope    20 |     assert(remove_vowels("ybcd") == "ybcd");       |     ^~~~~~ d9fea811-dd7c-43b5-a285-bc45aef7076c.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cctype>   +++ |+#include <cassert>     3 | d9fea811-dd7c-43b5-a285-bc45aef7076c.cpp: At global scope: d9fea811-dd7c-43b5-a285-bc45aef7076c.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ d9fea811-dd7c-43b5-a285-bc45aef7076c.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_51.cpp
+++ CPP_51.cpp
@@ -1,9 +1,22 @@
+#include <assert.h>
 #include <string>
+#include <cctype>
 
-string remove_vowels(string text){
-    string result = "";
-    for(char c : text){
-        if(!strchr("aeiouAEIOU",c))
+bool isvowel(char c) {
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+std::string remove_vowels(std::string text){
+    std::string result = "";
+    for (char c : text) {
+        if (!isvowel(c)) {
             result += c;
+        }
     }
     return result;
+}
+
+int main() {
+    assert(remove_vowels("ybcd") == "ybcd");
+}
