0ac0703d4050487aaadca31e43bd39de8484c21e
beep boop <githubbot@vadim.me>
2024-07-24 16:19:30 +0200

    Solution 58 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1e0d95d-bfb4-46c7-977e-480740bf4 e4e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,8 +1,7 @@
-```
-import heapq
 def sorted_list_sum(words=None):
     if words is None:
         lst = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
     else:
         lst = [word.strip().lower() for word in words]
-    return list(heapq.nsmallest(len([word for word in lst if len(word) % 2 == 0]), [word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), lst.index(x))))
+    
+    return sum(len(word) for word in lst if len(word) % 2 == 0)
