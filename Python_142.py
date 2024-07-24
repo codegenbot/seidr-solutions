a1cb0584efb2ca981abcf12ab30444a558e7b3fe
beep boop <githubbot@vadim.me>
2024-07-24 05:49:09 +0200

    Solution 43 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/004da057-a2ce-41ed-88e9-b9b073d69 592.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,6 +1,14 @@
-```
 def sum_of_squares(n):
     return n ** 2
 
-result = sum_of_squares(11)
-print(result)
+def check(x):
+    if x > 100:
+        return "Greater than 100"
+    elif x == 100:
+        return "Equal to 100"
+    else:
+        return "Less than or equal to 99"
+
+n = int(input("Enter a number: "))
+result = sum_of_squares(n)
+print(check(result))
