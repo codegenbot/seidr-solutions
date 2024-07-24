1f0ecc054f67df9aa8bb36503ee94c597643a977
beep boop <githubbot@vadim.me>
2024-07-24 18:54:08 +0200

    Solution 46 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/lnucize6 Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,7 +1,7 @@
 def decode_cyclic(s: str, n: int):
     groups = [s[i : i + n] for i in range(0, len(s), n)]
     groups = [
-        ((group[n - 1:] + group[:n - 1]) if len(group) == n else group)
+        ((group[n - 1 :] + group[: n - 1]) if len(group) == n else group)
         for group in groups
     ]
     return "".join(groups)
