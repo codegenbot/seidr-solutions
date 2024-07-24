50f61ddec9857dd9b69dacabe796dcb4d79efe6f
beep boop <githubbot@vadim.me>
2024-07-24 03:33:58 +0200

    Solution 53 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8vy4sey4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/486bdb46-add4-42de-9502-7f885a2b5 949.py", line 9, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/486bdb46-add4-42de-9502-7f885a2b5 949.py", line 6, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_66.py
+++ Python_66.py
@@ -1,2 +1,3 @@
+```
 def digitSum(s):
-    return sum(int(c) if c.isdigit() else ord(c.lower()) - 96 if c.isalpha() else ord(c) for c in s).sum((ord(i.upper())-64 for i in str(sum)))
+    return sum(int(c) if c.isdigit() else ord(c.upper()) - 64 for c in s)
