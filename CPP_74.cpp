097385f19a834250bd54955628b6f9c6e0bc980c
beep boop <githubbot@vadim.me>
2024-07-24 18:47:47 +0200

    Solution 31 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/es458wm4 Prompt: 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp:16: 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp: In function ‘int main()’: 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp: In function ‘int count_chars(int)’: 90cacc78-e88c-41ba-88ca-e281fe62f4ee.cpp:7:5: warning: control reaches end of no n-void function [-Wreturn-type]     7 |     }       |     ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -10,7 +10,7 @@ int count_chars(const vector<string>& lst) {
 }
 
 bool issame(const vector<string>& a, const vector<string>& b) {
-    return count_chars(a) == count_chars(b);
+    return a == b;
 }
 
 vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
