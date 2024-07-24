33cc4507a4805cc1ba0eccd0b5e90fd5d1aa3486
beep boop <githubbot@vadim.me>
2024-07-24 04:01:25 +0200

    Solution 35 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k4a5f9yk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/348c9c26-94b1-44fd-b1b7-4895f34f5 ac0.py", line 1     def encode(message): IndentationError: unexpected indent

--- Python_93.py
+++ Python_93.py
@@ -4,11 +4,11 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if 'aeiou'.find(char.lower()) != -1:  
-                result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))  
+                result += chr((ord(char) - 97 + 2) % 26 + 97)  
             elif char.isupper():  
-                result += chr((ord(char) - ord('A') + 13) % 26 + ord('A'))  
+                result += chr((ord(char) - 65 + 13) % 26 + 65)  
             else:
-                result += chr((ord(char) - ord('a') + 3) % 26 + ord('a'))
+                result += chr((ord(char) - 97 + 3) % 26 + 97)
         else:
             result += char
     return result
