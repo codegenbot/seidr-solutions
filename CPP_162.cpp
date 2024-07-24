8dba9bdd8155770860ef6804c6b2f64b4f3188d5
beep boop <githubbot@vadim.me>
2024-07-24 16:57:29 +0200

    Solution 33 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt:    15 |     unsigned char *digest = EVP_DigestFinal(md_ctx, NULL, NULL);       |                             ^~~~~~~~~~~~~~~ 317979e3-c2d8-418e-b978-985b76dc5704.cpp:18:19: error: ‘setfill’ was not declare d in this scope    18 |             ss << setfill('0') << setw(2) << hex << (int)digest[i];       |                   ^~~~~~~ 317979e3-c2d8-418e-b978-985b76dc5704.cpp:18:35: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    18 |             ss << setfill('0') << setw(2) << hex << (int)digest[i];       |                                   ^~~~       |                                   getw 317979e3-c2d8-418e-b978-985b76dc5704.cpp:18:46: error: ‘hex’ was not declared in  this scope    18 |             ss << setfill('0') << setw(2) << hex << (int)digest[i];       |                                              ^~~ 317979e3-c2d8-418e-b978-985b76dc5704.cpp:21:5: error: ‘OPENSSL_free’ was not dec lared in this scope    21 |     OPENSSL_free(digest);       |     ^~~~~~~~~~~~ 317979e3-c2d8-418e-b978-985b76dc5704.cpp:22:5: error: ‘EVP_MD_CTX_free’ was not declared in this scope    22 |     EVP_MD_CTX_free(md_ctx);       |     ^~~~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
-#include <string>
-#include <sstream>
+#include <iomanip>
+#include <openssl/errstack.h>
 
 using namespace std;
-
-class MD5 {
-public:
-    MD5(string text) : text_(text) {}
-
-    string hash() {
-        stringstream ss;
-        for (int i = 0; i < text_.size(); i++) {
-            ss << setfill('0') << setw(2) << hex << (int)(text_[i]);
-        }
-        return ss.str();
-    }
-
-private:
-    string text_;
-};
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    MD5 md(text);
+    unsigned char md[16];
+    stringstream ss;
 
-    return md.hash();
-}
-
-int main() {
-    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-    return 0;
+    EVP_MD_CTX* md_ctx = EVP_MD_CTX_new();
+    EVP_MD *md_algorithm = EVP_md5();
+    EVP_DigestInit(md_ctx, md_algorithm);
+    EVP_DigestUpdate(md_ctx, text.c_str(), text.size());
+    unsigned char *digest = EVP_DigestFinal(md_ctx, NULL, NULL);
+    if (digest != NULL) {
+        for (int i = 0; i < 16; ++i) {
+            ss << setfill('0') << setw(2) << hex << (int)digest[i];
+        }
+    }
+    OPENSSL_free(digest);
+    EVP_MD_CTX_free(md_ctx);
+    return ss.str();
 }
