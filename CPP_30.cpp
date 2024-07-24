80fcd31b0f4fcc964b9b2e007899523273af05ee
beep boop <githubbot@vadim.me>
2024-07-25 01:28:12 +0200

    Solution 24 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ybysd7qk Prompt:       |                    ^~~~~ 5598c108-e7ad-4850-85a3-b60b45849a05.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5598c108-e7ad-4850-85a3-b60b45849a05.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5598c108-e7ad-4850-85a3-b60b45849a05.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5598c108-e7ad-4850-85a3-b60b45849a05.cpp:11: 5598c108-e7ad-4850-85a3-b60b45849a05.cpp: In function ‘int main()’: 5598c108-e7ad-4850-85a3-b60b45849a05.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5598c108-e7ad-4850-85a3-b60b45849a05.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
 #include <vector>
+#include <assert.h>
 
-bool issame(float f1, float f2) {
-    return (f1 == f2);
+bool issame(vector<float> a, vector<float>b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
+vector<float> get_positive(vector<float> l) {
+    vector<float> result;
     for(float x : l) {
         if(x > 0) {
             result.push_back(x);
@@ -16,13 +20,6 @@ std::vector<float> get_positive(std::vector<float> l) {
 }
 
 int main() {
-    std::vector<float> numbers = {1, -2, 3, -4, 5};
-    std::vector<float> positive_numbers = get_positive(numbers);
-    for (float x : positive_numbers) {
-        if (issame(x, 5.0)) {
-            std::cout << "The first and the last number are same.\n";
-            break;
-        }
-    }
+    assert(issame(get_positive({}), {}));
     return 0;
 }
