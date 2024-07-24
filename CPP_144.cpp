d7b3059c8b30c80b46e17f2b202c29c267b9ce3c
beep boop <githubbot@vadim.me>
2024-07-24 11:33:41 +0200

    Solution 12 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yhgwwqcf Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,17 +1,22 @@
-int getGCD(int a, int b){
-        while(b != 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
+int gcd(int a, int b) {
+    if (b == 0)
         return a;
-    }
+    return gcd(b, a % b);
+}
 
-    bool simplify(string x, string n){
-        int num1 = stoi(x.substr(0, x.find('/')));
-        int den1 = stoi(x.substr(x.find('/') + 1));
-        int num2 = stoi(n.substr(0, n.find('/')));
-        int den2 = stoi(n.substr(n.find('/') + 1));
-
-        return (num1 * den2 == num2 * den1);
-    }
+bool simplify(string x, string n) {
+    int num1 = stoi(x.substr(0, x.find("/")));
+    int denom1 = stoi(x.substr(x.find("/") + 1));
+    int num2 = stoi(n.substr(0, n.find("/")));
+    int denom2 = stoi(n.substr(n.find("/") + 1));
+    
+    int num = num1 * num2;
+    int denom = denom1 * denom2;
+    
+    int common = gcd(num, denom);
+    
+    if (denom / common == 1)
+        return true;
+    else
+        return false;
+}
