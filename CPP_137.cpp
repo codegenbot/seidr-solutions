83e0f2e625c9d41d66778cb5d572088b27218519
beep boop <githubbot@vadim.me>
2024-07-24 02:43:45 +0200

    Solution 7 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt: 7cadf8be-63e6-4681-aa6a-f63f42967380.cpp:4:9: fatal error: boost/any.hpp: No suc h file or directory     4 | #include<boost/any.hpp>       |         ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,50 +5,31 @@
 
 using namespace std;
 
-std::any compare_one(std::any a, std::any b) {
-    if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double da = std::any_cast<double>(a);
-        string db = std::any_cast<string>(b);
-        if (da > stod(db))
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        double da = std::any_cast<double>(b);
-        string db = std::any_cast<string>(a);
-        if (stod(db) > da)
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string da = std::any_cast<string>(a);
-        string db = std::any_cast<string>(b);
-        if (da > db)
-            return a;
-        else if (db > da)
-            return b;
-        else
-            return any("None");
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double da = std::any_cast<double>(a);
-        double db = std::any_cast<double>(b);
-        if (da > db)
-            return a;
-        else if (db > da)
-            return b;
-        else
-            return any("None");
-    }
-    return any("None");
+any compare_one(any a, any b) {
+    if (any_cast<double>(a) > stod(any_cast<string>(b)))
+        return a;
+    else
+        return b;
+
+    if (stod(any_cast<string>(a)) > any_cast<double>(b))
+        return a;
+    else
+        return b;
+
+    string da = any_cast<string>(a);
+    string db = any_cast<string>(b);
+
+    if (da > db)
+        return a;
+    else if (db > da)
+        return b;
+    throw invalid_argument("Tie");
 }
 
 int main() {
-    cout << std::any_cast<string>(compare_one(1, 2.5)) << endl;
-    cout << std::any_cast<string>(compare_one(1, "2,3")) << endl;
-    cout << std::any_cast<string>(compare_one("5,1", "6")) << endl;
-    cout << std::any_cast<string>(compare_one("1", 1)) << endl;
+    cout << any_cast<double>(compare_one(1, 2.5)) << endl;
+    cout << any_cast<double>(compare_one(1, "2,3")) << endl;
+    cout << any_cast<string>(compare_one("5,1", "6")) << endl;
+    cout << any_cast<double>(compare_one("1", 1)) << endl;
     return 0;
 }
