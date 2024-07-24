0da829c899337dec34fbf38fd2bb3776dcb7d70a
beep boop <githubbot@vadim.me>
2024-07-24 04:18:44 +0200

    Solution 28 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8r60mo93 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/75383117-ead9-4ba1-992e-4fa9488fc 6f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- solve-boolean.py
+++ solve-boolean.py
@@ -9,9 +9,9 @@ def solve_boolean(expression=""):
     elif '&' in user_input and '|' in user_input:
         raise ValueError('Invalid expression')
     else:
-        for i in range(len(user_input)):
-            if user_input[i] == '&':
-                result = result and (user_input[i+1] == 'T')
-            elif user_input[i] == '|':
-                result = result or (user_input[i+1] == 'T')
+        for i, c in enumerate(user_input):
+            if c == '&':
+                result = result and (user_input[i+1:] == 'T')
+            elif c == '|':
+                result = result or (user_input[i+1:] == 'T')
         return result
