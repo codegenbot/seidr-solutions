7a3917a2186eecf8b21a79979d92adf5765dd3c4
beep boop <githubbot@vadim.me>
2024-07-24 18:45:35 +0200

    Solution 42 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/y8aamh4m Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = int]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const int&}; _Tp = int; _Alloc = std::allocator<int>; std::vector< _Tp, _Alloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = int; _Alloc = std::alloc ator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ e2c13a48-4f4b-4d2f-bf06-d33a00863a35.cpp:11:29:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,20 +1,24 @@
-```cpp
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(l);
-    sort(result.begin(), result.end());
-    result.erase(unique(result.begin(), result.end()), result.end());
+    vector<int> result;
+    for (int x : l) {
+        if (find(result.begin(), result.end(), x) == result.end())
+            result.push_back(x);
+    }
     return result;
 }
 
 int main() {
     vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> result = unique(l);
+    vector<int> result(l.size()); // Initialize with the size of input
+    for (int i : l) {
+        if (find(result.begin(), result.end(), i) == result.end())
+            result.push_back(i);
+    }
     for (int i : result) {
         cout << i << " ";
     }
