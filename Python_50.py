2178303bf6fd64e50604dd3b1f32d9af4414fd1e
beep boop <githubbot@vadim.me>
2024-07-24 12:15:12 +0200

    Solution 86 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/t346cxd3 Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -6,6 +6,6 @@ def decode_shift(encoded_string):
             decoded_string += char
         else:
             ascii_offset = 65 if char.isupper() else 97
-            decoded_char = chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
+            decoded_char = chr((ord(char) - ascii_offset - 3) % 26 + ascii_offset)
             decoded_string += decoded_char
     return decoded_string
