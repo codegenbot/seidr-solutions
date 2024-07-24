bf19c884fc7ab02dd4760014e39b14e9461477de
beep boop <githubbot@vadim.me>
2024-07-24 08:27:05 +0200

    Solution 1 of CPP/43, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/iszpfcq1 Prompt:     1 | for (int i = 0; i < l.size(); ++i) {       | ^~~ b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp:1:17: error: ‘i’ does not name a type     1 | for (int i = 0; i < l.size(); ++i) {       |                 ^ b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp:1:31: error: expected unqualified-id be fore ‘++’ token     1 | for (int i = 0; i < l.size(); ++i) {       |                               ^~ b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp:8:5: error: expected unqualified-id bef ore ‘return’     8 |     return false;       |     ^~~~~~ b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp:9:1: error: expected declaration before  ‘}’ token     9 | }       | ^ In file included from b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp:11: b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp: In function ‘int main()’: b2d27653-09d2-4e7e-8144-a717dc9bf525.cpp:13:13: error: ‘pairs_sum_to_zero’ was n ot declared in this scope    13 |     assert (pairs_sum_to_zero({-3, 9, -1, 4, 2, 31}) == false);       |             ^~~~~~~~~~~~~~~~~

--- CPP_43.cpp
+++ CPP_43.cpp
@@ -1,9 +1,10 @@
-for (int i = 0; i < l.size(); ++i) {
-        for (int j = i + 1; j < l.size(); ++j) {
-            if (l[i] + l[j] == 0) {
-                return true;
-            }
+bool pairs_sum_to_zero(const vector<int>& l) {
+    unordered_set<int> nums;
+    for (int num : l) {
+        if (nums.count(-num) > 0) {
+            return true;
         }
+        nums.insert(num);
     }
     return false;
 }
