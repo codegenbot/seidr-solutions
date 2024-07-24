f04d40e35e006db23eabbad18e11404266ce851a
beep boop <githubbot@vadim.me>
2024-07-24 12:18:00 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d13a58a0-c83c-44e5-bf28-69e83385bdfa.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d13a58a0-c83c-44e5-bf28-69e83385bdfa.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d13a58a0-c83c-44e5-bf28-69e83385bdfa.cpp:11: d13a58a0-c83c-44e5-bf28-69e83385bdfa.cpp: In function ‘int main()’: d13a58a0-c83c-44e5-bf28-69e83385bdfa.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ d13a58a0-c83c-44e5-bf28-69e83385bdfa.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,10 @@
-lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
-sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
-    if (a.length() == b.length()) {
-        return a < b;
-    }
-    return a.length() < b.length();
-});
-return lst;
+vector<string> sortAndFilterStrings(vector<string>& lst) {
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
+    sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
+        if (a.length() == b.length()) {
+            return a < b;
+        }
+        return a.length() < b.length();
+    });
+    return lst;
+}
