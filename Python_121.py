29e1289ac73a3631dc959db2df780b41f8ac2a65
beep boop <githubbot@vadim.me>
2024-07-25 01:09:12 +0200

    Solution 61 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fz47n6iq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2434a451-fcd9-48ae-a5a2-ea53c8303 ee2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,4 +1,3 @@
+```
 def solution(lst):
-    odd_sum = sum(i for i in lst[1::2] if i % 2 != 0)
-    even_sum = sum(i for i in lst[::2] if i % 2 == 0)
-    return [odd_sum, even_sum]
+    return sum(i for i in lst[1::2] if i % 2 != 0)
