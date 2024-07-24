037aecb0c5bfcec93581f371c8c8b027af4259dc
beep boop <githubbot@vadim.me>
2024-07-24 23:59:32 +0200

    Solution 57 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/51vrqbmc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9b87d4da-7de2-44fd-a279-3e57eb3a5 f8e.py", line 9, in <module>     check(right_angle_triangle)           ^^^^^^^^^^^^^^^^^^^^ NameError: name 'right_angle_triangle' is not defined

--- Python_157.py
+++ Python_157.py
@@ -1,7 +1,6 @@
 def is_pythagorean_triple(a, b, c):
     return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
 
-
 def right_angle_triangle():
     a = int(input("Enter the first side length: "))
     b = int(input("Enter the second side length: "))
@@ -11,6 +10,5 @@ def right_angle_triangle():
         print("Yes, it is a right angle triangle.")
     else:
         print("No, it is not a right angle triangle.")
-
 
 right_angle_triangle()
