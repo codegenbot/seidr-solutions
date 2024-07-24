f145730784cf16c55ae0d6ec0548d720d08578fc
beep boop <githubbot@vadim.me>
2024-07-24 18:19:12 +0200

    Solution 12 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/to49ik7c Prompt:  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 58cde982-cf92-4347-b5ff-d344e1f92aeb.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 58cde982-cf92-4347-b5ff-d344e1f92aeb.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 58cde982-cf92-4347-b5ff-d344e1f92aeb.cpp:18: 58cde982-cf92-4347-b5ff-d344e1f92aeb.cpp: In function ‘int main()’: 58cde982-cf92-4347-b5ff-d344e1f92aeb.cpp:28:20: error: ‘remove_duplicates’ was n ot declared in this scope    28 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 58cde982-cf92-4347-b5ff-d344e1f92aeb.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,21 +1,3 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
-}
-
-std::vector<int> remove_duplicates(std::vector<int> numbers){
-    std::vector<int> result;
-    for(int num : numbers){
-        if(find(result.begin(), result.end(), num) == result.end()){
-            result.push_back(num);
-        }
-    }
-    return result;
-}
-
-int main() {
-    assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
-    return 0;
+bool issame(vector<int> a, vector<int> b) {
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
