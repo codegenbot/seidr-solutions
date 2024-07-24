f1e2c1ffc4c4594c369a1078e96c5536c3e57e17
beep boop <githubbot@vadim.me>
2024-07-24 03:55:48 +0200

    Solution 4 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/lwsrif8y Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,24 +1,30 @@
-#include <string>
-using namespace std;
+bool solveBoolean(string booleanExpression) {
+    stack<char> operationStack;
+    stack<bool> valueStack;
 
-bool solveBoolean(string s) {
-    bool result = false;
-    bool temp = true;
-    
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T' || s[i] == 't') 
-            temp = true;
-        else if (s[i] == 'F' || s[i] == 'f') 
-            temp = false;
-        else if (s[i] == '&') {
-            result &= temp;
-            temp = false;
-        } 
-        else if (s[i] == '|') {
-            result |= temp;
-            temp = false;
+    for (int i = 0; i < booleanExpression.size(); i++) {
+        char c = booleanExpression[i];
+
+        if (c == 'T' || c == 't') {
+            valueStack.push(true);
+        } else if (c == 'F' || c == 'f') {
+            valueStack.push(false);
+        } else if (c == '&') {
+            bool right = valueStack.top();
+            valueStack.pop();
+            bool left = valueStack.top();
+            valueStack.pop();
+
+            valueStack.push(left && right);
+        } else if (c == '|') {
+            bool right = valueStack.top();
+            valueStack.pop();
+            bool left = valueStack.top();
+            valueStack.pop();
+
+            valueStack.push(left || right);
         }
     }
-    
-    return result;
+
+    return valueStack.top();
 }
