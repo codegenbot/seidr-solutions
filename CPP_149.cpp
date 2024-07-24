16c487976c4a4b07eb8e7e807caa63fd7b74afac
beep boop <githubbot@vadim.me>
2024-07-24 08:15:41 +0200

    Solution 22 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7meh2yxx Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 12c9ce69-d912-41bd-a8f7-6e51c19ebdcb.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,29 +1,7 @@
 #include <vector>
 #include <string>
+#include <initializer_list>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
+// existing code...
 
-int sorted_list_sum(const std::vector<std::vector<std::string>>& lst) {
-    int sum = 0;
-    for (const auto& sublst : lst) {
-        std::sort(sublst.begin(), sublst.end());
-        if (issame({{"hello", "world"}}, sublst)) {
-            sum += std::stoi(sublst[1].substr(7));
-        }
-    }
-    return sum;
-}
-
-int main() {
-    assert(std::equal(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc","dd","aaaa","bbbb"}));
-    return 0;
+```
