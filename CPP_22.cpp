4b2d9bd455ad06f357dd1d48d2e501bdc1a46e8e
beep boop <githubbot@vadim.me>
2024-07-24 17:45:13 +0200

    Solution 30 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/hvhyshua Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 968c0012-c3f1-440b-8133-7e2475a5324b.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 968c0012-c3f1-440b-8133-7e2475a5324b.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 968c0012-c3f1-440b-8133-7e2475a5324b.cpp:11: 968c0012-c3f1-440b-8133-7e2475a5324b.cpp: In function ‘int main()’: 968c0012-c3f1-440b-8133-7e2475a5324b.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 968c0012-c3f1-440b-8133-7e2475a5324b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,27 +1,14 @@
+#include <boost/any.hpp>
 #include <vector>
 #include <list>
-#include <any>
-
-namespace std {
-    template<>
-    struct any_cast<std::any> {
-        friend auto operator<<(ostream& os, const any_cast& a) -> ostream& {
-            return os << "any";
-        }
-    };
-}
 
 using namespace std;
 
-std::vector<int> filter_integers(std::list<any> values) {
+vector<int> filter_integers(list<boost::any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (any_cast<std::any>(value).type() == typeid(int)) {
-            try {
-                result.push_back(any_cast<std::any>(value).get<int>());
-            } catch (bad_any_cast) {
-                //handle bad any cast here
-            }
+        if (boost::any_cast<int>(value).good()) {
+            result.push_back(boost::any_cast<int>(value).get());
         }
     }
     return result;
