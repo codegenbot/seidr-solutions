3506cf50147046a90dea54bcce251b00f0ff2892
beep boop <githubbot@vadim.me>
2024-07-24 03:24:03 +0200

    Solution 86 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/ncmy02p4 Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:704:44:   required from ‘_ForwardIt erator std::__uninitialized_default_n_a(_ForwardIterator, _Size, std::allocator< _Tp>&) [with _ForwardIterator = int*; _Size = long unsigned int; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:1606:36:   required from ‘void std::vector <_Tp, _Alloc>::_M_default_initialize(std::vector<_Tp, _Alloc>::size_type) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = l ong unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:512:9:   required from ‘std::vector<_Tp, _ Alloc>::vector(std::vector<_Tp, _Alloc>::size_type, const allocator_type&) [with  _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::size_type = long unsigned int; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int >]’ 20502a70-ea55-4c15-80cd-f6a60caed6d3.cpp:25:20:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- leaders.cpp
+++ leaders.cpp
@@ -1,11 +1,33 @@
-```cpp
-for (size_t i = v.size() - 1; i >= 0; --i) {
-    bool leader = true;
-    for (int j = i + 1; j < v.size(); ++j) {
-        if (v[j] >= v[i]) {
-            leader = false;
-            break;
+```c++
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> leaders(vector<int>& v) {
+    vector<int> res;
+    int max_right = v.back();
+    for (int i = v.size() - 1; i >= 0; --i) {
+        if (v[i] >= max_right) {
+            res.push_back(v[i]);
+            max_right = v[i];
         }
     }
-    if (leader) res.push_back(v[i]);
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v; 
+    v.reserve(n); 
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    vector<int> res = leaders(v);
+    for (int i = 0; i < res.size(); ++i) {
+        cout << res[i] << " ";
+    }
 }
