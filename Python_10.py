660673a2e524c072a2dea01cfbf03f7013f59a1d
beep boop <githubbot@vadim.me>
2024-07-24 09:37:16 +0200

    Solution 10 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cst1c9qx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04c9b89b-d64e-46a8-96ab-023f9f4e4 e52.py", line 17, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04c9b89b-d64e-46a8-96ab-023f9f4e4 e52.py", line 14, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,17 @@
 ```
 def make_palindrome(string: str) -> str:
-    string = "".join(e for e in string.lower() if e.isalnum())
-    for i in range(len(string), 0, -1):
-        if string[:i] == string[:i][::-1]:
-            return string + string[:i][::-1]
-    return string + string[::-1]
+    if string == string[::-1]:
+        return string
+    for i in range(len(string)):
+        postfix = string[i:]
+        if postfix == postfix[::-1]:
+            left_half = string[:i]
+            right_half = postfix[::-1]
+            middle_chars = len(postfix) // 2 * [postfix[0]]
+            return ''.join([left_half] + middle_chars + [right_half])
+    half = len(string) // 2
+    left_half = string[:half]
+    right_half = string[half:][::-1]
+    if len(left_half) < len(right_half):
+        return left_half + left_half[::-1] + right_half
+    return left_half + right_half
