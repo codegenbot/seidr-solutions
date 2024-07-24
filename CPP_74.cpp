bacc557f1dc41807ae31ddb5503049c9c1f68ce6
beep boop <githubbot@vadim.me>
2024-07-24 12:59:02 +0200

    Solution 1 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8o9kzi1s Prompt:       |                              ^~~~~~ 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp:16:37: error: ‘string’ was not declared  in this scope    16 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp:16:44: error: ‘b’ was not declared in t his scope    16 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp:15: 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp: In function ‘int main()’: 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp:25:20: error: ‘total_match’ was not dec lared in this scope    25 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 0cd5a0f4-13bf-40f8-a50b-95067b4601ea.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,3 +1,14 @@
+#include <string>
+#include <vector>
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
@@ -9,5 +20,8 @@ vector<string> total_match(vector<string> lst1, vector<string> lst2) {
     }
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else return lst1;
+    else {
+        if (issame(lst1, lst2)) return lst1;
+        else return lst2;
+    }
 }
