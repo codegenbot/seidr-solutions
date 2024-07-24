abd526499b83ec298f3e7eb8a061e8ca159d16ce
beep boop <githubbot@vadim.me>
2024-07-24 23:49:40 +0200

    Solution 80 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/hqexafdi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fdd36256-2217-46c2-97be-6b42063d1 9b6.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fdd36256-2217-46c2-97be-6b42063d1 9b6.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,17 +1,2 @@
 def modp(n: int, p: int):
-    if gcd(n, p) != 1:
-        raise ValueError("No modular inverse")
-    else:
-
-        def extended_gcd(a, b):
-            if a == 0:
-                return b, 0, 1
-            else:
-                g, y, x = extended_gcd(b % a, a)
-                return g, x - (b // a) * y, y
-
-        g, x, _ = extended_gcd(p, n)
-        if g != 1:
-            raise ValueError("No modular inverse")
-        else:
-            return pow(n, p - 2, p) * n % p
+    return pow(n, p - 2) % p * n % p
