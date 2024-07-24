b75be2f98c91791e1705ca7d31be1e43229452df
beep boop <githubbot@vadim.me>
2024-07-24 04:24:57 +0200

    Solution 51 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k4a5f9yk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/339f03eb-ab65-43c2-b1d8-ff644af3e 8a6.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/339f03eb-ab65-43c2-b1d8-ff644af3e 8a6.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,13 +3,10 @@ def encode(message):
     for char in message:
         if char.isalnum():
             if char.isalpha():
-                ascii_offset = 97 if char.islower() else 65
+                ascii_offset = 97 if char.islower() else 64
                 result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
             else: 
-                if char.isdigit():
-                    result += str(int(char) + 3)
-                else:
-                    result += char
+                result += char.lower()
         else:
             result += char
     return result
