59f374dd087e598918b402ad19485f2e7086a249
beep boop <githubbot@vadim.me>
2024-07-24 18:35:22 +0200

    Solution 69 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/hvhyshua Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cd83f00a-99bd-48c4-8d67-2f5f07afff2e.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cd83f00a-99bd-48c4-8d67-2f5f07afff2e.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cd83f00a-99bd-48c4-8d67-2f5f07afff2e.cpp:11: cd83f00a-99bd-48c4-8d67-2f5f07afff2e.cpp: In function ‘int main()’: cd83f00a-99bd-48c4-8d67-2f5f07afff2e.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ cd83f00a-99bd-48c4-8d67-2f5f07afff2e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,16 +1,22 @@
-#include <vector>
-#include <list>
-#include <boost/any.hpp>
-
 bool issame(vector<int> a,vector<int>b){
     return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> filter_integers(list<boost::any> values) {
+vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (auto& value : values) {
         if (boost::any_cast<int>(value)) {
-            result.push_back(boost::any_cast<int>(value));
+            int val = boost::any_cast<int>(value);
+            bool isInt = true;
+            for (int num : values) {
+                if (!issame({val}, {num})) {
+                    isInt = false;
+                    break;
+                }
+            }
+            if (isInt) {
+                result.push_back(val);
+            }
         }
     }
     return result;
