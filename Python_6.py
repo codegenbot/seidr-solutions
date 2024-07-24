7d95c71c72d89c098296a09b48c64e71f3d36902
beep boop <githubbot@vadim.me>
2024-07-24 19:34:26 +0200

    Solution 81 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/l7bpaez2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ca6b3b42-d880-4aa3-9d66-cbccaa2c6 225.py", line 1     return max([paren_string.count("(" * i) for i in range(1, len(paren_string) // 2 + 1)])     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_6.py
+++ Python_6.py
@@ -1,6 +1,5 @@
 def max_nested_paren(paren_string):
     return max([paren_string.count("(" * i) for i in range(1, len(paren_string) // 2 + 1)])
 
-# Call the function with the input parameter
 result = max_nested_paren(input())
 print(result)
