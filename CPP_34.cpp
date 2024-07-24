808fe8d349d2e347889152554c4c7de00b0bcc6d
beep boop <githubbot@vadim.me>
2024-07-25 01:24:35 +0200

    Solution 69 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/avztuzwn Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ ebeba701-0a9e-4e75-a736-a3b5780e8d54.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ ebeba701-0a9e-4e75-a736-a3b5780e8d54.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from ebeba701-0a9e-4e75-a736-a3b5780e8d54.cpp:10: ebeba701-0a9e-4e75-a736-a3b5780e8d54.cpp: In function ‘int main()’: ebeba701-0a9e-4e75-a736-a3b5780e8d54.cpp:20:20: error: ‘unique’ was not declared  in this scope    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ ebeba701-0a9e-4e75-a736-a3b5780e8d54.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
-#include <set>
 #include <vector>
+#include <set>
+#include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(const set<int>& a, const set<int>& b) {
+bool issame(const std::set<int>& a, const std::set<int>& b) {
     return a == b;
 }
 
 int main() {
-    vector<int> nums;
-    int num;
+    std::vector<int> nums = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    std::set<int> num_set(nums.begin(), nums.end());
     
-    cout << "Enter numbers (Enter -1 to end input): ";
-    while (true) {
-        cin >> num;
-        if (num == -1) {
-            break;
-        }
-        nums.push_back(num);
-    }
+    assert(issame(num_set, std::set<int>({0, 2, 3, 5, 9, 123}));
 
-    set<int> num_set(nums.begin(), nums.end());
-    
-    assert(issame(num_set, set<int>({0, 2, 3, 5, 9, 123}));
+    std::vector<int> unique_nums(nums);
+    std::sort(unique_nums.begin(), unique_nums.end());
+    auto it = std::unique(unique_nums.begin(), unique_nums.end());
+    unique_nums.erase(it, unique_nums.end());
+
+    assert(issame(std::set<int>(unique_nums.begin(), unique_nums.end()), std::set<int>({0, 2, 3, 5, 9, 123}));
     
     return 0;
 }
