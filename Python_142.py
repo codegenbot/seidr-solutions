26a921448f723f3c2419f014ff1f93b409e5c108
beep boop <githubbot@vadim.me>
2024-07-24 05:29:37 +0200

    Solution 31 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt: Bigger Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90d77511-05ee-44c4-a392-6930ecf84 dce.py", line 49, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90d77511-05ee-44c4-a392-6930ecf84 dce.py", line 19, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,12 +1,2 @@
-def sum_squares(lst):
-    return sum(i**2 for i in lst)
-
-
-def check(result):
-    if result > 25:
-        return "Result too high"
-    else:
-        return "Result is low"
-
-
-print(check(sum_squares([1, 2, 3, 11, 12])))
+result = sum_squares([1, 2, 3, 11, 12])
+print(check(result))
