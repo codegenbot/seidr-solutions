29a214667f433ba18925a43bb193c7019228db17
beep boop <githubbot@vadim.me>
2024-07-24 19:03:25 +0200

    Solution 61 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/to49ik7c Prompt: f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp:16:13: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp:16:20: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp:15: f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp: In function ‘int main()’: f8076c91-f1fa-41b3-82d7-7034f4f73166.cpp:25:20: error: ‘remove_duplicates’ was n ot declared in this scope    25 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -7,15 +7,7 @@ std::vector<int> remove_duplicates(std::vector<int> numbers){
 }
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size())
-        return false;
-    
-    for (int i = 0; i < a.size(); i++) {
-        if (std::find(b.begin(), b.end(), a[i]) == b.end())
-            return false;
-    }
-    
-    return true;
+    return (a == b);
 }
 
 int main2() {
