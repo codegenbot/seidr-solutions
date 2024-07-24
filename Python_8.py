2d7a1da039c8084307b72e4f1c2a812eb32ee425
beep boop <githubbot@vadim.me>
2024-07-24 11:57:52 +0200

    Solution 67 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vv9tfrbf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96949af7-42f0-4e72-bea4-56f8dd121 ae7.py", line 1     Here is the modified code:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,5 +1,5 @@
+```
 from typing import List, Tuple
-
 
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
     if not isinstance(numbers, list):
@@ -10,6 +10,7 @@ def sum_product(numbers: List[int]) -> Tuple[int, int]:
 
     total_sum = 0
     product = 1
+
     for num in numbers:
         total_sum += num
         product *= num
