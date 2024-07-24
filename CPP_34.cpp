ebf7862e0083850f0536bcecc5c76af1627eccd0
beep boop <githubbot@vadim.me>
2024-07-24 18:18:32 +0200

    Solution 6 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/y8aamh4m Prompt: a9e7436a-feae-45a7-b448-b19c4f92f635.cpp:7:46: note:   candidate expects 4 argum ents, 2 provided     7 |     vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));       |                                   ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~ a9e7436a-feae-45a7-b448-b19c4f92f635.cpp: At global scope: a9e7436a-feae-45a7-b448-b19c4f92f635.cpp:12:35: error: class template placeholde r ‘std::iterator’ not permitted in this context    12 | vector<int>::iterator unique_copy(iterator first, iterator last) {       |                                   ^~~~~~~~ a9e7436a-feae-45a7-b448-b19c4f92f635.cpp:12:51: error: class template placeholde r ‘std::iterator’ not permitted in this context    12 | vector<int>::iterator unique_copy(iterator first, iterator last) {       |                                                   ^~~~~~~~ a9e7436a-feae-45a7-b448-b19c4f92f635.cpp: In function ‘std::vector<int>::iterato r unique_copy(...)’: a9e7436a-feae-45a7-b448-b19c4f92f635.cpp:13:18: error: ‘first’ was not declared in this scope    13 |     iterator i = first - 1;       |                  ^~~~~ a9e7436a-feae-45a7-b448-b19c4f92f635.cpp:14:20: error: ‘last’ was not declared i n this scope    14 |     for(; first != last; ++first) {       |                    ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,22 +1,21 @@
-```cpp
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), l.end());
+    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
     sort(result.begin(), result.end());
-    result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
 }
 
-int main() {
-    vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> result = unique(l);
-    for (int i : result) {
-        cout << i << " ";
+vector<int>::iterator unique_copy(vector<int>::iterator first, vector<int>::iterator last) {
+    vector<int>::iterator i = first - 1;
+    for(; first != last; ++first) {
+        if(i == first || *i != *first) {
+            ++i;
+            *++i = *first;
+        }
     }
-    return 0;
+    return ++i;
 }
