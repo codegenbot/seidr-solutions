9cb0f73bf9a7bcb7c8a9c1c235afd13676aa7408
beep boop <githubbot@vadim.me>
2024-07-24 14:59:51 +0200

    Solution 61 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8o9kzi1s Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ bc5a4f69-8104-41a8-b811-2f52d5a0da21.cpp:52:23:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -5,7 +5,6 @@
 #include <limits>
 
 using namespace std;
-using namespace std::allocator; // add this line
 
 bool same(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) return false;
@@ -15,7 +14,7 @@ bool same(vector<string> a, vector<string> b) {
     return true;
 }
 
-vector<string, allocator<string>> match(vector<string, allocator<string>> lst1, vector<string, allocator<string>> lst2) {
+vector<string> match(vector<string> lst1, vector<string> lst2) {
     int sum1 = 0;
     for (const string& s : lst1) {
         sum1 += s.length();
@@ -39,7 +38,7 @@ int main() {
     int n1, n2;
     cout << "Enter the number of strings for list 1: ";
     cin >> n1;
-    vector<string, allocator<string>> lst1;
+    vector<string> lst1;
     cout << "Enter string " << n1 << " for list 1:\n";
     for (int i = 0; i < n1; i++) {
         string s;
@@ -53,7 +52,7 @@ int main() {
 
     cout << "Enter the number of strings for list 2: ";
     cin >> n2;
-    vector<string, allocator<string>> lst2;
+    vector<string> lst2;
     cout << "Enter string " << n2 << " for list 2:\n";
     for (int i = 0; i < n2; i++) {
         string s;
@@ -68,7 +67,7 @@ int main() {
     if (same(lst1, lst2)) {
         cout << "The lists are the same.\n";
     } else {
-        vector<string, allocator<string>> result = match(lst1, lst2);
+        vector<string> result = match(lst1, lst2);
         if (equal(result.begin(), result.end(), lst1.begin())) {
             cout << "List 1 is the match of list 2.\n";
         } else {
