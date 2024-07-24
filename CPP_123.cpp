217dd86192f306be25b889e7581125da428b5bf9
beep boop <githubbot@vadim.me>
2024-07-24 09:20:17 +0200

    Solution 80 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/v8x4car6 Prompt:       |                    ^~~ 5741e296-5691-44ca-9200-f8d439267b6a.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5741e296-5691-44ca-9200-f8d439267b6a.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5741e296-5691-44ca-9200-f8d439267b6a.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5741e296-5691-44ca-9200-f8d439267b6a.cpp:21: 5741e296-5691-44ca-9200-f8d439267b6a.cpp: In function ‘int main()’: 5741e296-5691-44ca-9200-f8d439267b6a.cpp:31:20: error: ‘get_odd_collatz’ was not  declared in this scope    31 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 5741e296-5691-44ca-9200-f8d439267b6a.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,31 +1,21 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); ++i){
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
-}
-
-vector<int> get_odd_collatz(int n) {
+vector<int> issame(int n) {
     vector<int> result;
-    while(n != 1){
-        if(n % 2 != 0){
-            result.push_back(n);
+    while (n != 1) {
+        result.push_back(n);
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
         }
-        n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
-    sort(result.begin(), result.end());
-    return result;
-}
-
-int main() {
-    assert(issame(get_odd_collatz(1) , {1}));
-    return 0;
+    vector<int> odd_nums;
+    for (int num : result) {
+        if (num % 2 == 1) {
+            odd_nums.push_back(num);
+        }
+    }
+    sort(odd_nums.begin(), odd_nums.end());
+    return odd_nums;
 }
