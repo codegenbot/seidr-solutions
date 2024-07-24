73718333ebf8319a0988703de9246dd49fd34333
beep boop <githubbot@vadim.me>
2024-07-24 09:29:46 +0200

    Solution 50 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7meh2yxx Prompt:    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e7763c08-aba0-433c-a542-28574331c0bf.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ e7763c08-aba0-433c-a542-28574331c0bf.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from e7763c08-aba0-433c-a542-28574331c0bf.cpp:32: e7763c08-aba0-433c-a542-28574331c0bf.cpp: In function ‘int main()’: e7763c08-aba0-433c-a542-28574331c0bf.cpp:42:35: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    42 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   <brace-enclosed initializer list> e7763c08-aba0-433c-a542-28574331c0bf.cpp:16:21: note:   initializing argument 1 of ‘int sorted_list_sum(int)’    16 | int sorted_list_sum(const vector<vector<string>>& lst) {       |                     ^~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,30 +1,5 @@
-#include <vector>
-#include <string>
-
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int sorted_list_sum(const std::vector<std::vector<std::string>>& lst) {
-    int sum = 0;
-    for (const auto& sublst : lst) {
-        std::sort(sublst.begin(), sublst.end());
-        if (issame({{"hello", "world"}}, std::vector<std::string>(sublst.begin() + 1, sublst.end()))) {
-            sum += std::stoi(sublst[0].substr(6));
-        }
-    }
-    return sum;
-}
-
 int main() {
-    assert(sorted_list_sum({{"aaaa", "bbbb"}, {"dd"}, {"cc"}}) == 0);
+    vector<vector<string>> lst = {{"aaaa", "bbbb"}, {"dd"}, {"cc"}};
+    assert(sorted_list_sum(lst) == 2);
     return 0;
 }
