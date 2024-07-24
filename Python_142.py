9babb121f887cdeb5e81b9a6b333d74949d48add
beep boop <githubbot@vadim.me>
2024-07-24 22:10:49 +0200

    Solution 95 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9wzm29ar Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/15fc403f-6433-4345-8a56-28a3c8460 58b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,6 +1,3 @@
 def sum_of_odd_squares(lst):
-    total_sum = sum(i**2 for i in lst if i % 2 != 0)
+    total_sum = sum([i**2 for i in lst if i % 2 != 0])
     return total_sum
-
-
-print(sum_of_odd_squares([1, 2, 3, 4, 5]))
