a04de08db61d1a4a88ced1915dcb269690613ed6
beep boop <githubbot@vadim.me>
2024-07-24 18:36:15 +0200

    Solution 24 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/796qfzqz Prompt:       |                              ^~~~~~ ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp:21: ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp: In function ‘int main()’: ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ ecabd98e-eaff-4bac-baf9-e02e0d01bd2c.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,16 +1,18 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <string>
+#include <cassert>
 
-bool issame(float a, float b, float epsilon = 0.0001) {
-    return fabs(a - b) < epsilon;
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
 }
-
-vector<string> numerical_letter_grade(vector<float> grades);
 
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
     for (float gpa : grades) {
-        if (issame(gpa, 4.0)) letter_grades.push_back("A+");
+        if (gpa == 4.0) letter_grades.push_back("A+");
         else if (gpa > 3.7) letter_grades.push_back("A");
         else if (gpa > 3.3) letter_grades.push_back("A-");
         else if (gpa > 3.0) letter_grades.push_back("B+");
@@ -25,4 +27,9 @@ vector<string> numerical_letter_grade(vector<float> grades) {
         else letter_grades.push_back("E");
     }
     return letter_grades;
+}
+
+int main() {
+    assert(issame(numerical_letter_grade({0, 0.7}), {"E", "D-"}));
+    return 0;
 }
