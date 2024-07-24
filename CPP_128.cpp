01b501f21cc9a4b98cf51d4a995b3bc9f8a7b201
beep boop <githubbot@vadim.me>
2024-07-24 09:27:27 +0200

    Solution 26 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/c6avqie1 Prompt: 244e9280-6a01-4456-83d1-0415a66a1a1c.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr){       |                ^~~~~~ 244e9280-6a01-4456-83d1-0415a66a1a1c.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr){       |                       ^~~ In file included from 244e9280-6a01-4456-83d1-0415a66a1a1c.cpp:12: 244e9280-6a01-4456-83d1-0415a66a1a1c.cpp: In function ‘int main()’: 244e9280-6a01-4456-83d1-0415a66a1a1c.cpp:14:23: error: ‘prod_signs’ cannot be us ed as a function    14 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int prod_signs(vector<int> arr){
+int prod_signs(vector<int> arr) {
     int product = 1;
     long long sum = 0;
     for(int i : arr){
@@ -9,3 +9,4 @@ int prod_signs(vector<int> arr){
         sum += abs(i);
     }
     return product * sum == 0 ? -32768 : product * sum;
+}
