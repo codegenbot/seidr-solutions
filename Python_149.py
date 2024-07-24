9223c9f7d98c2d5442d3805e04d261a2d3accc31
beep boop <githubbot@vadim.me>
2024-07-24 16:34:06 +0200

    Solution 68 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcd557a2-89c3-44d6-99be-a11dbd86b f92.py", line 14, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcd557a2-89c3-44d6-99be-a11dbd86b f92.py", line 11, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -5,4 +5,5 @@ def sorted_list_sum(words=None):
     else:
         lst = [word.strip().lower() for word in words]
     
-    return sorted([word for word in lst if len(word) % 2 == 0])
+    even_length_words = [word for word in sorted(lst, key=len) if len(word) % 2 == 0]
+    return sum(len(word) for word in even_length_words)
