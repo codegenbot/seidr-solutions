e7413d6699bc8c63f46601ba39aa2c1b7abec485
beep boop <githubbot@vadim.me>
2024-07-24 13:25:56 +0200

    Solution 25 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qhgsa44k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2811a367-79e8-4a9e-8d71-5e395c4e6 138.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_71.py
+++ Python_71.py
@@ -10,9 +10,3 @@ height = float(input("Enter the height of the triangle: "))
 triangle_area = (base*height)/2
 
 check(triangle_area)
-
-def sum_of_squares(n):
-    result = 0
-    for i in range(1, n + 1):
-        result += i**2
-    return result
