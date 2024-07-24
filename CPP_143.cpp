8fa3ac92510705df19b651664b8ac1201e5aca1b
beep boop <githubbot@vadim.me>
2024-07-24 07:48:11 +0200

    Solution 10 of CPP/143, ratio 0.5714285714285714 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8icb4usg Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7af1183e-c81e-4a9c-8bf8-375de1755af1.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,17 +1,8 @@
 #include <vector>
 #include <string>
+#include <bits/stdc++.h>
 
-vector<string> split(const string& str, const string& delimiter) {
-    vector<string> tokens;
-    size_t pos = 0;
-    size_t prevPos = 0;
-    while ((pos = str.find(delimiter, prevPos)) != string::npos) {
-        tokens.push_back(str.substr(prevPos, pos - prevPos));
-        prevPos = pos + delimiter.length();
-    }
-    tokens.push_back(str.substr(prevPos));
-    return tokens;
-}
+using namespace std;
 
 bool is_prime(int n) {
     if (n <= 1) return false;
@@ -21,12 +12,28 @@ bool is_prime(int n) {
     return true;
 }
 
-string words_in_sentence(string sentence){
-    string result = "";
-    for (const auto& word : split(sentence, " ")) {
+vector<string> split(const string& str, char delimiter) {
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do {
+        pos = str.find(delimiter, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos - prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + 1;
+    } while (pos != string::npos);
+    return tokens;
+}
+
+string words_in_sentence(string sentence) {
+    vector<string> words = split(sentence, ' ');
+    string result;
+    
+    for (const auto& word : words) {
         if (is_prime(word.length())) {
             result += word + " ";
         }
     }
-    return result;
+    
+    return result.substr(0, result.size() - 1);
 }
