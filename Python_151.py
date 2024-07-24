633f528ef8d3635e9ff80b764557e86e01c855a8
beep boop <githubbot@vadim.me>
2024-07-24 05:32:27 +0200

    Solution 11 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vixjqopt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d093ba4-855a-4796-afae-cbc9520a0 c94.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,10 @@
+```
 def double_the_difference(input_list):
-    if len(input_list) > 0:
-        diff = sum(input_list) - len(input_list)
-        return 2 * diff
-    else:
-        return None
+    diff = sum(input_list) - len(input_list)
+    return 2 * diff
 
 input_list = list(map(int, input("Enter numbers separated by space: ").split()))
-print(double_the_difference(input_list))
+if len(input_list) < 1:
+    print("Invalid Input. Please enter at least one number.")
+else:
+    print(double_the_difference(input_list))
