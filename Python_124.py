9142e6fdaf4fd60d59f490103c4009205d2f066a
beep boop <githubbot@vadim.me>
2024-07-24 15:25:34 +0200

    Solution 25 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jfnmht7o Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/106c6932-96cd-42ce-a6dc-1f3f38c68 518.py", line 20, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/106c6932-96cd-42ce-a6dc-1f3f38c68 518.py", line 17, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/106c6932-96cd-42ce-a6dc-1f3f38c68 518.py", line 2, in valid_date     month, day, year = date.split('-')     ^^^^^^^^^^^^^^^^ ValueError: not enough values to unpack (expected 3, got 2)

--- Python_124.py
+++ Python_124.py
@@ -1,21 +1,18 @@
 ```
 def valid_date(date):
-    month_day_year = date.split('-')
-    month, day, year = map(int, month_day_year)
-    if len(str(month)) == 1:
-        month = '0' + str(month)
-    if len(str(day)) == 1:
-        day = '0' + str(day)
+    day, month, year = date.split('-')
+    if len(month) == 1:
+        month = '0' + month
+    if len(day) == 1:
+        day = '0' + day
     if int(month) < 1 or int(month) > 12:
         return False
-    if (int(month) in [4,6,9,11] and int(day) > 30) or (int(month) == 2 and int(day) > 28):
-        if int(year) % 4 != 0 and int(day) > 29:
-            return False
-        elif int(year) % 4 == 0 and int(day) > 29:
-            if int(year) % 100 == 0 and int(day) > 28:
+    if (int(month) in [4,6,9,11] and int(day) > 30) or (int(month) == 2 and int(day) > 29):
+        try:
+            if int(year) < 0:
                 return False
-            elif int(year) % 400 == 0 and int(day) > 29:
-                return False
+        except ValueError:
+            pass
     if int(day) < 1 or int(day) > 31:
         return False
     return True
