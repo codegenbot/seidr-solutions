ff8931863e5515d386380fa08a500fa88c597819
beep boop <githubbot@vadim.me>
2024-07-24 03:11:33 +0200

    Solution 53 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/c444kx7a Prompt: 1d5d9575-70fb-4be7-a588-ea453bac26b6.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ 1d5d9575-70fb-4be7-a588-ea453bac26b6.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std;

--- bowling.cpp
+++ bowling.cpp
@@ -9,28 +9,14 @@ int bowlingScore(string s) {
             score += 30;
             roll++;
         } else if (c == '/') {
-            if (s.find('/') != s.rfind('/')) {
-                string temp = s.substr(s.find('/'), s.rfind('/') - s.find('/') + 1);
-                int count = stoi(temp) * 2;
-                if (stoi(temp) < 5) {
-                    count += 10;
-                } else {
-                    score += 20;
-                    roll++;
-                }
-                score += count;
-            } else {
-                string temp = s.substr(s.find('/'));
-                int count = stoi(temp);
-                score += 10 + count * 2;
+            if (s[s.find('/')-1] != 'X') {
+                score += 10 - (roll % 2);
+                roll++;
             }
         } else {
-            int count = (s[s.length() - 1] - '0') * 10 + (s[s.length() - 2] - '0');
-            if (count < 10) {
-                score += count;
-            } else {
-                score += 10 + (count - 10) * 2;
-            }
+            int count = s.find('/') - s.find('0');
+            score += count;
+            roll++;
         }
     }
     return score;
