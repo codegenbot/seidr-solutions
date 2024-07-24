1967e8311a454ba4aadcf2b1cabaf404019ecedc
beep boop <githubbot@vadim.me>
2024-07-24 04:42:58 +0200

    Solution 70 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/qwyfb73y Prompt: 021d45e1-0e32-422f-9953-bdbf25b4f7e2.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string camelCase(string s) {       | ^~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,31 +1,17 @@
-#include <string>
-#include <cctype>
-
 std::string camelCase(std::string inputString) {
-    if (inputString[0] == '-') {
-        for (++inputString.begin(); inputString.begin() != inputString.end(); ++inputString.begin()) {
-            if (*inputString.begin() == '-') break;
-            *inputString.begin() = std::toupper(*inputString.begin());
-        }
-    }
-
-    size_t start = 0;
-    for (size_t i = 0; i < inputString.size(); ++i) {
-        if (inputString[i] == '-' || inputString[i] == ' ') {
-            for (++i; i < inputString.size() && inputString[i] != '-'; ++i);
-            inputString.erase(i, inputString.size());
-            size_t length = i - start;
-            std::string temp = inputString.substr(start, length).toupper();
-            if (start > 0) {
-                inputString.insert(start++, 1, temp[0]);
-                for (size_t j = 1; j < length; ++j)
-                    inputString.insert(i, 1, tolower(temp[j]));
-            } else {
-                inputString.replace(0, length, temp);
+    if (inputString[0] == '-') inputString.erase(0, 1); 
+    std::string result = "";
+    for (int i = 0; i < inputString.length(); i++) {
+        if (inputString[i] == '-') {
+            i++;
+            while (i < inputString.length() && inputString[i] != ' ') {
+                result += std::toupper(inputString[i]);
+                i++;
             }
-            start = i;
+            result += " ";
+        } else {
+            result += std::tolower(inputString[i]);
         }
     }
-
-    return inputString;
+    return result;
 }
