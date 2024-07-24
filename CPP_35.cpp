80d50b721f49a5853f0c9b5d46661f1edc8de72d
beep boop <githubbot@vadim.me>
2024-07-24 18:48:15 +0200

    Solution 34 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nwhbfoew Prompt: 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp: At global scope: 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp:11:5: error: redefinition of ‘int main( )’    11 | int main(){       |     ^~~~ 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~ In file included from 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp:10: 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp: In function ‘int main()’: 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp:12:17: error: ‘max_element’ was not dec lared in this scope; did you mean ‘std::max_element’?    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 809eb5ba-e1b0-49b9-9424-bd63139e4a7e.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: ‘std::max_element’ d eclared here   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 
 int main() {
-    int max_val = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.begin(), {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.end());
-    assert(abs(std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.begin(), {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.end())-124)<1e-4);
+    int max_val = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
+    int abs_max_val = std::abs(*std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}));
     return 0;
-}
