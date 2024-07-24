b53d55881ede8c5e9b9826c7de4bd642944da141
beep boop <githubbot@vadim.me>
2024-07-24 04:05:58 +0200

    Solution 40 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/57e56d4a-46ba-4d46-8f7f-d8fe16623 14c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -3,8 +3,8 @@ import math
 
 def solve(K):
     if K < 1:
-        return "0"
+        return '0b0'
     if math.log2(K) % 1 == 0:
-        return bin(int(math.pow(2, math.ceil(math.log2(K)))) - 1)[2:]
+        return '0b' + bin(int(math.pow(2, math.ceil(math.log2(K)))) - 1)[2:]
     else:
-        return bin(K + 1)[2:]
+        return '0b' + bin(K + 1)[2:]
