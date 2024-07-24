cb24f84e4bc06a4b5d2b93f3b1ffcef3b42b9643
beep boop <githubbot@vadim.me>
2024-07-24 20:03:40 +0200

    Solution 38 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ax209ivw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f294e46-a611-4f08-8a72-e26f6ff27 578.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f294e46-a611-4f08-8a72-e26f6ff27 578.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,14 +1,13 @@
 def encode(message):
     result = ""
-    result += " "
+    if message:
+        result += " "
     for char in message:
-        if char == " ":
-            result += " "
-        elif char.isalpha():
+        if char.isalpha():
             if char.isupper():
                 result += chr((ord(char) - ord("A") + 3) % 26 + ord("A"))
             else:
                 result += chr((ord(char) - ord("a") + 3) % 26 + ord("a"))
         else:
-            result += char
+            result += " "
     return result
