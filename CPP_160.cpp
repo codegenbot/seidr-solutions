84c82eefc4e024ea8eb36356406afc5f99eb8946
beep boop <githubbot@vadim.me>
2024-07-24 03:29:52 +0200

    Solution 4 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/02oov1vw Prompt:       |                       ^~~~~~ 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp:19: 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp: In function ‘int main()’: 11fd56f4-e081-4d0b-8e4a-b745180537f8.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,42 +1,18 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = 0;
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
-            result += operand[i];
-        } else if (operator_[i] == "-") {
-            result -= operand[i];
-        } else if (operator_[i] == "*") {
-            int temp = 0;
-            for (int j = i; j < operator_.size(); j++) {
-                if (operator_[j] == "*") {
-                    temp *= operand[j + 1];
-                } else if (operator_[j] == "+") {
-                    result += temp * operand[j + 1];
-                    break;
-                }
-            }
-        } else if (operator_[i] == "//") {
-            int temp = operand[i + 1];
-            for (int j = i + 1; j < operator_.size(); j++) {
-                if (operator_[j] == "//") {
-                    result /= temp;
-                    break;
-                } else if (operator_[j] == "+") {
-                    result += temp / operand[j + 1];
-                    break;
-                }
-            }
-        } else if (operator_[i] == "**") {
-            int temp = pow(operand[i + 1], 1);
-            for (int j = i + 1; j < operator_.size(); j++) {
-                if (operator_[j] == "**") {
-                    result = pow(result, operand[j + 1]);
-                    break;
-                } else if (operator_[j] == "+") {
-                    result += temp * pow(operand[j + 1], 1);
-                    break;
-                }
-            }
+using namespace std;
+
+int do_algebra(vector<string> operators, vector<int> operand) {
+    int result = operand[0];
+    for (int i = 0; i < operators.size(); i++) {
+        if (operators[i] == "+") {
+            result += operand[i + 1];
+        } else if (operators[i] == "-") {
+            result -= operand[i + 1];
+        } else if (operators[i] == "*") {
+            result *= operand[i + 1];
+        } else if (operators[i] == "//") {
+            result = result / static_cast<int>(operand[i + 1]);
+        } else if (operators[i] == "**") {
+            result = pow(result, static_cast<double>(operand[i + 1]));
         }
     }
     return result;
