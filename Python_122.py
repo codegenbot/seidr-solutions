25fe9e87db30406ce88adbf2771f2c5942bf66ce
beep boop <githubbot@vadim.me>
2024-07-24 10:31:03 +0200

    Solution 10 of Python/122, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v29q0c0i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f24f222d-3cb4-4fc7-9ba4-c9c708b68 e71.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_122.py
+++ Python_122.py
@@ -1,3 +1,6 @@
-```
 def add_elements(arr, k):
-    return sum(i for i in arr[:k] if 10 > len(str(i)) or len(str(i)) == 2)
+    result = 0
+    for x in arr[:k]:
+        if 10 > len(str(abs(x))) or len(str(abs(x))) == 2:
+            result += x
+    return result
