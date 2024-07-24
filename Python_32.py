4fe987b3bc734929faf291baf44426726f8798f6
beep boop <githubbot@vadim.me>
2024-07-24 22:46:50 +0200

    Solution 30 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/z0rl6fru Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b87e7700-43c3-4bfe-a1e5-cfb20af29 bed.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -13,10 +13,10 @@ def find_zero(xs: list):
     return round(x, 2)
 
 
-def main():
-    xs = list(map(float, input("Enter coefficients: ").split()))
-    print(find_zero(xs))
-
-
 if __name__ == "__main__":
-    main()
+    xs = input("Enter coefficients: ").split()
+    try:
+        xs = list(map(float, xs))
+        print(find_zero(xs))
+    except ValueError as e:
+        print(f"Error: {e}")
