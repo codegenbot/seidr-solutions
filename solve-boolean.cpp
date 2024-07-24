65e51c2c86b834b6a21de6ad909a09d001acc276
beep boop <githubbot@vadim.me>
2024-07-24 06:32:31 +0200

    Solution 84 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/lxcqhlyr Prompt: Program did not receive expected input

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -3,35 +3,90 @@
 using namespace std;
 
 bool solveBoolean(string expression) {
-    int pos = expression.find('|');
-    while (pos != string::npos) {
-        string left = expression.substr(0, pos);
-        string right = expression.substr(pos + 1);
+    int pos = 0;
+    while (pos < expression.size()) {
+        if (expression[pos] == '(') {
+            pos++;
+            string nested = "";
+            bool insideParentheses = true;
+            while (insideParentheses) {
+                if (expression[pos] == '(')
+                    pos++;
+                else if (expression[pos] == ')') {
+                    pos++;
+                    insideParentheses = false;
+                }
+                else
+                    nested += expression[pos++];
+            }
+            if (solveBoolean(nested))
+                return true;
 
-        if (solveBoolean(left) || solveBoolean(right))
-            return true;
-        
-        pos = expression.find('|', pos);
+        }
+        else if (expression[pos] == '|') {
+            int leftPos = 0, rightPos = 0;
+            for (int i = pos; i < expression.size(); i++) {
+                if (expression[i] == '|') {
+                    rightPos = i;
+                    break;
+                }
+                if (expression[i] == '(') {
+                    leftPos = i + 1;
+                    for (int j = i + 1; j < expression.size(); j++) {
+                        if (expression[j] == ')') {
+                            rightPos = j;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            string left = expression.substr(leftPos, rightPos - leftPos);
+            string right = expression.substr(rightPos + 1);
+
+            if (solveBoolean(left) || solveBoolean(right))
+                return true;
+
+        }
+        else if (expression[pos] == '&') {
+            int leftPos = 0, rightPos = 0;
+            for (int i = pos; i < expression.size(); i++) {
+                if (expression[i] == '&') {
+                    rightPos = i;
+                    break;
+                }
+                if (expression[i] == '(') {
+                    leftPos = i + 1;
+                    for (int j = i + 1; j < expression.size(); j++) {
+                        if (expression[j] == ')') {
+                            rightPos = j;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            string left = expression.substr(leftPos, rightPos - leftPos);
+            string right = expression.substr(rightPos + 1);
+
+            if (solveBoolean(left) && solveBoolean(right))
+                return true;
+
+        }
+        else if (expression[pos] == 'F' || expression[pos] == 'T') {
+            pos++;
+            while (pos < expression.size() && (expression[pos] == '|' || expression[pos] == '&')) {
+                pos++;
+            }
+        }
     }
-
-    pos = expression.find('&');
-    while (pos != string::npos) {
-        string left = expression.substr(0, pos);
-        string right = expression.substr(pos + 1);
-
-        if (solveBoolean(left) && solveBoolean(right))
-            return true;
-        
-        pos = expression.find('&', pos);
-    }
-
     return !expression.empty() && tolower(expression[0]) == 't';
 }
 
 int main() {
     string expression;
     cout << "Enter a Boolean expression: ";
-    getline(cin, expression);
+    cin >> expression;
     bool result = solveBoolean(expression);
     if (result)
         cout << "True";
