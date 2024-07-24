99f38670ebf232017eda70e450fb9f59561e8d0c
beep boop <githubbot@vadim.me>
2024-07-24 20:01:53 +0200

    Solution 55 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/u85n1iif Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98e51a56-6f4a-4619-8fed-87771d409 e93.py", line 13, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98e51a56-6f4a-4619-8fed-87771d409 e93.py", line 10, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98e51a56-6f4a-4619-8fed-87771d409 e93.py", line 4, in fruit_distribution     "".join(filter(str.isdigit, s.split(" ")[0].split(" ")[1]))                                 ~~~~~~~~~~~~~~~~~~~~~~~~~~^^^ IndexError: list index out of range

--- Python_67.py
+++ Python_67.py
@@ -1,7 +1,6 @@
-```
 def fruit_distribution(s, n):
     s = s.replace("apples and", "apples").replace("oranges", "oranges apples")
     n -= int("".join(filter(str.isdigit, s))) 
     if len(s.split()) > 1:
-        n -= int("".join(filter(str.isdigit, s.split()[-1])))
+        n -= int("".join(filter(str.isdigit, s.split()[1])))
     return n
