3fbdc9e649c64ff5a3157c0ada03f4ee761a9e8f
beep boop <githubbot@vadim.me>
2024-07-24 06:24:14 +0200

    Solution 88 of Python/140, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3a3c7f88-63c9-4f5a-b24c-ee53f016c 9f8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,13 +1,17 @@
-```
 def fix_spaces(text):
     new_text = ""
-    for i in range(len(text)):
-        if text[i] == " ":
-            if i < len(text) - 1 and text[i+1] == " ":
-                if new_text and new_text[-1] != "-":
-                    new_text += "-"
-            else:
+    add_hyphen = False
+    for char in text:
+        if char.isalnum():
+            new_text += char
+            add_hyphen = False
+        elif (not new_text or not new_text[-1].isalnum()) and not add_hyphen:
+            new_text += "-"
+            add_hyphen = True
+        elif new_text and new_text[-1].isalnum():
+            if not add_hyphen:
                 new_text += "_"
-        else:
-            new_text += text[i]
+                add_hyphen = True
+            else:
+                new_text += "-"
     return new_text
