04a96450456b8ec45c3b26fff95e5b7c68ba6af7
beep boop <githubbot@vadim.me>
2024-07-24 11:03:26 +0200

    Solution 8 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/hhr1riau Prompt: a8462b9a-232e-4b11-a0a9-a2bd301df2c8.cpp:28:12: error: expected primary-expressi on before ‘float’    28 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from a8462b9a-232e-4b11-a0a9-a2bd301df2c8.cpp:26: a8462b9a-232e-4b11-a0a9-a2bd301df2c8.cpp:29:31: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    29 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from a8462b9a-232e-4b11-a0a9-a2bd301df2c8.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ a8462b9a-232e-4b11-a0a9-a2bd301df2c8.cpp:6:45: note:   initializing argument 1 o f ‘bool has_close_elements(std::vector<float>&, float)’     6 | bool has_close_elements(std::vector<float>& numbers, float threshold) {       |                         ~~~~~~~~~~~~~~~~~~~~^~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -15,9 +15,9 @@ bool has_close_elements(std::vector<float>& numbers, float threshold) {
 
 int main() {
     std::vector<float> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    assert(has_close_elements(a, 0.5) == false);
-    
     std::vector<float> b = {1.1, 2.2, 3.1, 4.1, 5.1};
+    
+    assert(has_close_elements(a, 0.5) == false);
     assert(has_close_elements(b, 0.5) == false);
     
     return 0;
