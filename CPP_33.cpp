753f38a0453a48a4fe2566a62263250c63cbe8af
beep boop <githubbot@vadim.me>
2024-07-24 07:58:09 +0200

    Solution 6 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/w8xozsn6 Prompt:    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b5b6d3d4-2e3e-4aa4-9695-f1188060c266.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b5b6d3d4-2e3e-4aa4-9695-f1188060c266.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b5b6d3d4-2e3e-4aa4-9695-f1188060c266.cpp:11: b5b6d3d4-2e3e-4aa4-9695-f1188060c266.cpp: In function ‘int main()’: b5b6d3d4-2e3e-4aa4-9695-f1188060c266.cpp:21:20: error: ‘sort_third’ was not decl ared in this scope    21 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ b5b6d3d4-2e3e-4aa4-9695-f1188060c266.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,48 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-vector<int> sort_third(vector<int> l) {
-    vector<int> l_divisible_by_three;
-    vector<int> l_not_divisible_by_three;
-    vector<int> l_sorted_divisible_by_three;
-
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            l_divisible_by_three.push_back(l[i]);
-        } else {
-            l_not_divisible_by_three.push_back(l[i]);
-        }
+std::vector<int> sorted_third(std::vector<int> l) {
+    std::vector<int> l_prime = l;
+    for (int i = 0; i < l.size(); i += 3) {
+        std::sort(l_prime.begin() + i, l_prime.begin() + std::min(i + 3, static_cast<int>(l_prime.size())));
     }
-
-    l_sorted_divisible_by_three = l_divisible_by_three;
-    sort(l_sorted_divisible_by_three.begin(), l_sorted_divisible_by_three.end());
-
-    vector<int> result;
-    int j = 0;
-    int k = 0;
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            result.push_back(l_sorted_divisible_by_three[j]);
-            ++j;
-        } else {
-            result.push_back(l_not_divisible_by_three[k]);
-            ++k;
-        }
-    }
-
-    return result;
+    return l_prime;
 }
 
 int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-
+    assert(issame(sorted_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
     return 0;
 }
