e60ebb894d50c3fcf9126da626289462fbb64f2b
beep boop <githubbot@vadim.me>
2024-07-24 12:45:02 +0200

    Solution 20 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/evzpu1rl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/13f3f00b-e069-4215-a085-6d9672d98 b99.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,16 +1,16 @@
 def smallest_change(arr):
-    if isinstance(arr[0], list):
+    if isinstance(arr[0], list):  
         n = len(arr[0])
-        arr_str = "".join([str(x) for x in arr[0] + arr[1]])
-    else:
+        arr_str = ''.join([str(x) for x in arr[0] + arr[1]])
+    else:  
         n = 2
         arr_str = str(arr[0]) + str(arr[1])
 
     rev_arr_str = arr_str[::-1]
     diff_count = 0
 
-    for i in range(n // 2):
-        if arr_str[i] != rev_arr_str[n - i - 1]:
+    for i in range(n//2):
+        if arr_str[i] != rev_arr_str[n-i-1]:
             diff_count += 1
 
     return diff_count
