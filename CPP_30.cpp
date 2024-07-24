a567bd2f927477f241ec7becedae5cf10e5ed884
beep boop <githubbot@vadim.me>
2024-07-24 04:04:52 +0200

    Solution 2 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y8l1vktq Prompt: Complete the following code given the task description and function signature.

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,9 @@
 vector<float> get_positive(vector<float> l){
     vector<float> result;
-    for(float i : l){
-        if(i > 0) result.push_back(i);
+    for(auto x : l) {
+        if(x > 0) {
+            result.push_back(x);
+        }
     }
     return result;
 }
