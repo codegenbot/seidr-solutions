201da97c9db157603f2eb6265537fde626cc479e
beep boop <githubbot@vadim.me>
2024-07-24 02:01:33 +0200

    Solution 20 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/eoutbb05 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 3c963b76-d8f1-45eb-8c5b-1ed4fba57642.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -1,4 +1,15 @@
-int fuelCost(vector<int> numbers) {
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> numbers;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        numbers.push_back(num);
+    }
     int sum = 0;
     for (int num : numbers) {
         int result = (num / 3) - 2;
