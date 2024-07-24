bcb6ae6d8fc426649f0c4552c4a932dee02dc443
beep boop <githubbot@vadim.me>
2024-07-24 11:53:36 +0200

    Solution 56 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/t346cxd3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fb673b41-e2fc-496d-9292-be5b3bc1e e4f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,10 +1,11 @@
-def decode_shift(encoded_string):
-    decoded_string = ""
-    for char in str(encoded_string):  
-        if char.isalpha():
-            ascii_offset = 65 if char.isupper() else 97
-            decoded_char = chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
-            decoded_string += decoded_char
-        else:
-            decoded_string += char
-    return decoded_string
+```
+    def decode_shift(encoded_string):
+        decoded_string = ""
+        for char in encoded_string:
+            if char.isalpha():
+                ascii_offset = 65 if char.isupper() else 97
+                decoded_char = chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
+                decoded_string += decoded_char
+            else:
+                decoded_string += char
+        return decoded_string
