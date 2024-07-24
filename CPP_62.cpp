ccb37b34b8c429f4ac06f17502a788239b6fe098
beep boop <githubbot@vadim.me>
2024-07-24 10:29:28 +0200

    Solution 24 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zu0hhoe8 Prompt:       |             ^~~~~~ 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp:27:36: error: expected primary-expressi on before ‘float’    27 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp:27:43: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main(){       |     ^~~~ In file included from 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp:26: 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp: In function ‘int main()’: 4e2dba88-c41e-4431-aa7b-d4a4e0b96b67.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -12,8 +12,8 @@ std::vector<float> derivative(const std::vector<float>& xs){
     }
     
     std::vector<float> result;
-    for(size_t i = 1; i < xs.size(); i++){
-        result.push_back(xs[i] - xs[i-1]);
+    for(size_t i=1; i<xs.size(); i++){
+        result.push_back(xs[i] * i);
     }
     return result;
 }
