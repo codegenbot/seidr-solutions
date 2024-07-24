f639328087f1800612b500633f803708825fea2b
beep boop <githubbot@vadim.me>
2024-07-24 02:38:30 +0200

    Solution 54 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/z7qa9oa3 Prompt: f2496138-564a-4423-b80f-81399fa0a6ae.cpp: In function ‘bool evalOp(std::string)’ : f2496138-564a-4423-b80f-81399fa0a6ae.cpp:31:12: error: ‘evalBoolean’ was not dec lared in this scope    31 |     return evalBoolean(s.substr(j));       |            ^~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,42 +2,6 @@
 #include <string>
 using namespace std;
 
-bool evalBoolean(string s) {
-    if (s.empty())
-        return false;
-
-    int j = 0;
-    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
-        j++;
-
-    string opStr = s.substr(0, j);
-    bool result = false;
-    if (opStr[0] == '|') {
-        for (int i = 0; i < j; i++) {
-            if (s[i] == 'T')
-                result = true;
-            else if (s[i] == 'F')
-                return false;
-        }
-    } else {
-        for (int i = 0; i < j; i++) {
-            if (s[i] == 'T' && opStr[1] != '|')
-                result = true;
-            else if (s[i] == 'F' && opStr[1] != '&')
-                return false;
-        }
-    }
-
-    if (!result)
-        return false;
-
-    string rest = s.substr(j);
-    if (rest.empty())
-        return !result; 
-    else
-        return evalBoolean(rest); 
-}
-
 bool evalOp(string s) {
     int j = 0;
     while (j < s.length() && (s[j] == '|' || s[j] == '&'))
@@ -67,11 +31,26 @@ bool evalOp(string s) {
     return evalBoolean(s.substr(j));
 }
 
+bool evalBoolean(string s) {
+    if (s.empty())
+        return false;
+
+    int j = 0;
+    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+        j++;
+
+    string opStr = s.substr(0, j);
+    bool result = evalOp(s);
+
+    // recursive call for the remaining part
+    return !result; 
+}
+
 int main() {
     string s;
     cout << "Enter a Boolean expression: ";
     cin >> s;
-    bool result = evalOp(s);
+    bool result = evalBoolean(s);
     if (result)
         cout << "True";
     else
