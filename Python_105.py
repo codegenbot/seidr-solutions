67cbc2721831770bb18ff48eef103b5d67bea466
beep boop <githubbot@vadim.me>
2024-07-24 14:14:16 +0200

    Solution 3 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/dcwu1w7c Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e28728a9-9c83-4864-996f-437beeb02 bfc.py", line 15, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e28728a9-9c83-4864-996f-437beeb02 bfc.py", line 12, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e28728a9-9c83-4864-996f-437beeb02 bfc.py", line 3, in by_length     return [num_to_name[i - 1] for i in reversed(sorted_arr)]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e28728a9-9c83-4864-996f-437beeb02 bfc.py", line 3, in <listcomp>     return [num_to_name[i - 1] for i in reversed(sorted_arr)]             ~~~~~~~~~~~^^^^^^^ TypeError: 'function' object is not subscriptable

--- Python_105.py
+++ Python_105.py
@@ -3,6 +3,7 @@ def by_length(arr):
     sorted_arr = sorted([str(i) for i in arr if 1 <= i <= 9])
     return [num_to_name[int(i)] for i in reversed(sorted_arr)]
 
+
 def num_to_name(num):
     names = ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
     return names[num - 1]
