dd8efd941c3c774481ea9333a6c283cb71ab77db
beep boop <githubbot@vadim.me>
2024-07-24 18:23:11 +0200

    Solution 66 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/r99dr0xy Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 99928be8-a052-46a0-a9fa-35a6a2c241de.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 99928be8-a052-46a0-a9fa-35a6a2c241de.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 99928be8-a052-46a0-a9fa-35a6a2c241de.cpp:11: 99928be8-a052-46a0-a9fa-35a6a2c241de.cpp: In function ‘int main()’: 99928be8-a052-46a0-a9fa-35a6a2c241de.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 99928be8-a052-46a0-a9fa-35a6a2c241de.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,17 +1,7 @@
-```cpp
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-vector<string> filter_by_prefix(vector<string> strings, string prefix){
-    vector<string> result;
-    for (string s : strings) {
-        if (s.find(prefix) == 0) {
-            result.push_back(s);
-        }
-    }
-    return result;
-}
 int main(){
     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}) );
     return 0;
