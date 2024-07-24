d5e007beab716b26f3df6e895fe124d4d9df7acf
beep boop <githubbot@vadim.me>
2024-07-24 03:28:31 +0200

    Solution 56 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/dk4hok2d Prompt: 42a4748b-1827-44f1-bf0e-b247c5f93120.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>&)’: 42a4748b-1827-44f1-bf0e-b247c5f93120.cpp:8:20: error: ‘INT_MAX’ was not declared  in this scope     8 |     int min_diff = INT_MAX;       |                    ^~~~~~~ 42a4748b-1827-44f1-bf0e-b247c5f93120.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 42a4748b-1827-44f1-bf0e-b247c5f93120.cpp:12:13: error: ‘abs’ was not declared in  this scope    12 |         if (abs(diff1-diff2) < abs(min_diff)) {       |             ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,7 +1,5 @@
-```c++
 #include <vector>
 #include <climits>
-#include <cmath>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int>& nums) {
@@ -10,9 +8,9 @@ vector<vector<int>> cutVector(vector<int>& nums) {
     
     int min_diff = INT_MAX;
     for (int i = 1; i < n; i++) {
-        int diff1 = abs(nums[i] - nums[0]);
-        int diff2 = abs(nums[n-1] - nums[i-1]);
-        if (abs(diff1-diff2) < abs(min_diff)) {
+        int diff1 = nums[i] - nums[0];
+        int diff2 = nums[n-1] - nums[i-1];
+        if (std::abs(diff1-diff2) < std::abs(min_diff)) {
             min_diff = diff1 - diff2;
             result.push_back(vector<int>(nums.begin(), nums.begin() + i));
             nums.erase(nums.begin());
