2f7d8d4c3fa6c11154c87fbcf9e5892e3c620898
beep boop <githubbot@vadim.me>
2024-07-24 18:23:37 +0200

    Solution 12 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cvfj3xyj Prompt:                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 64a17885-1a80-407b-ae9c-ae8443b1cefa.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 64a17885-1a80-407b-ae9c-ae8443b1cefa.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 64a17885-1a80-407b-ae9c-ae8443b1cefa.cpp:25: 64a17885-1a80-407b-ae9c-ae8443b1cefa.cpp: In function ‘int main()’: 64a17885-1a80-407b-ae9c-ae8443b1cefa.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 64a17885-1a80-407b-ae9c-ae8443b1cefa.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,17 +1,8 @@
+```cpp
 #include <algorithm>
 #include <vector>
 
-bool std::issame(std::vector<float> a, std::vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (std::abs(a[i] - b[i]) > 1e-5) {
-            return false;
-        }
-    }
-    return true;
-}
+bool issame(std::vector<float> a, std::vector<float> b);
 
 std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> result;
@@ -30,4 +21,21 @@ std::vector<float> sort_even(std::vector<float> l) {
         }
     }
     return result;
+}
+
+bool issame(std::vector<float> a, std::vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
 }
