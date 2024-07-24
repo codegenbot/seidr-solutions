d6720f6afd77f0b236ebdcfb52d6eb91a1cf7ca8
beep boop <githubbot@vadim.me>
2024-07-24 06:35:23 +0200

    Solution 73 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,27 +1,83 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> nums) {
+pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
     int n = nums.size();
-    vector<vector<int>> res;
-    
-    for (int i = 1; i <= n/2; i++) {
-        int leftSum = 0, rightSum = 0;
-        for (int j = 0; j < i; j++) {
+    for (int i = 0; i < n; ++i) {
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int j = 0; j < i; ++j)
             leftSum += nums[j];
-        }
-        for (int k = i; k < n; k++) {
-            rightSum += nums[k];
-        }
-        
-        if (abs(leftSum - rightSum) <= abs(nums[i] - nums[n-i-1])) {
-            res.push_back(vector<int>(nums.begin(), nums.begin() + i));
-            res.push_back(vector<int>(nums.begin() + i, nums.end()));
-            return res;
+        for (int j = i; j < n; ++j)
+            rightSum += nums[j];
+        if (leftSum == rightSum) {
+            return {{}, {nums.begin(), nums.end()}};
         }
     }
-    
-    // If no cut found, return the original vector
-    res.push_back(nums);
-    return res;
+    int minDiff = INT_MAX;
+    int index = 0;
+    for (int i = 1; i < n; ++i) {
+        int leftSum = 0;
+        for (int j = 0; j < i; ++j)
+            leftSum += nums[j];
+        int rightSum = 0;
+        for (int j = i; j < n; ++j)
+            rightSum += nums[j];
+        int diff = abs(leftSum - rightSum);
+        if (diff < minDiff) {
+            minDiff = diff;
+            index = i;
+        }
+    }
+    return {{nums.begin(), nums.begin() + index}, {nums.begin() + index, nums.end()}};
+}
+
+int main() {
+    // Example inputs
+    vector<int> input1 = {1};
+    vector<int> result1 = cutVector(input1);
+    for (int num : result1[0]) 
+        cout << num << " ";
+    cout << endl;
+    for (int num : result1[1]) 
+        cout << num << " ";
+    cout << endl;
+
+    vector<int> input2 = {1, 10};
+    vector<int> result2 = cutVector(input2);
+    for (int num : result1[0])
+        cout << num << " ";
+    cout << endl;
+    for (int num : result1[1]) 
+        cout << num << " ";
+    cout << endl;
+
+    vector<int> input3 = {1, 100};
+    vector<int> result3 = cutVector(input3);
+    for (int num : result1[0])
+        cout << num << " ";
+    cout << endl;
+    for (int num : result1[1]) 
+        cout << num << " ";
+    cout << endl;
+
+    vector<int> input4 = {1, 1000};
+    vector<int> result4 = cutVector(input4);
+    for (int num : result1[0])
+        cout << num << " ";
+    cout << endl;
+    for (int num : result1[1]) 
+        cout << num << " ";
+    cout << endl;
+
+    vector<int> input5 = {1, 10000};
+    vector<int> result5 = cutVector(input5);
+    for (int num : result1[0])
+        cout << num << " ";
+    cout << endl;
+    for (int num : result1[1]) 
+        cout << num << " ";
+    cout << endl;
+
+    return 0;
 }
