6d44330d480d21651bc228116c86d0e44682f69c
beep boop <githubbot@vadim.me>
2024-07-24 03:23:10 +0200

    Solution 7 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k4a5f9yk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b8a86b31-69b3-4b25-8450-b3dfc9cf7 42c.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b8a86b31-69b3-4b25-8450-b3dfc9cf7 42c.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,11 +1,12 @@
+```
 def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if char.lower() in 'aeiou':
+            if ('aeiou'.find(char.lower()) != -1):
                 result += chr((ord(char) - 97 + 2) % 26 + 97)
             else:
-                result += char.swapcase()
+                result += (char.swapcase())
         else:
             result += char
     return result
