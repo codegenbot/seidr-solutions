443ba47b5d389e512536e99bc059a9d11784ee0a
beep boop <githubbot@vadim.me>
2024-07-24 02:08:57 +0200

    Solution 39 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/walwymsv Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a4c7358-7895-42cc-92d2-8fad31ed3 f20.py", line 12, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a4c7358-7895-42cc-92d2-8fad31ed3 f20.py", line 9, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,9 +1,8 @@
 def fizz_buzz(n: int):
-    count = 0
-    fizzbuzz_count = 0
+    output_count = 0
     for i in range(1, n + 1):
         output = "FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
         print(output)
-        if output == "FizzBuzz":
-            fizzbuzz_count += 1
-    return fizzbuzz_count
+        if output in ["Fizz", "Buzz", "FizzBuzz"]:
+            output_count += 1
+    return output_count
