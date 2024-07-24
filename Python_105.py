4e9d92c0315f57d12586a00276acdeb3e7231174
beep boop <githubbot@vadim.me>
2024-07-24 04:01:51 +0200

    Solution 20 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dab896a8-eff7-4f9c-920c-25e635742 d00.py", line 9, in <module>     check(by_length)           ^^^^^^^^^ NameError: name 'by_length' is not defined

--- Python_105.py
+++ Python_105.py
@@ -1,2 +1,36 @@
-by_length = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-check(solve_task(by_length))
+name_dict = {
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    10: "Ten",
+    11: "Eleven",
+    12: "Twelve",
+    13: "Thirteen",
+    14: "Fourteen",
+    15: "Fifteen",
+    16: "Sixteen",
+    17: "Seventeen",
+    18: "Eighteen",
+    19: "Nineteen",
+}
+
+
+def solve_task(by_length):
+    return (
+        [
+            "".join(
+                [
+                    name_dict[i] if i < 10 else name_dict[int(i)]
+                    for i in [str(i) for i in arr if 1 <= int(i) <= 19]
+                ]
+            )
+        ]
+        .replace("Twenty", "Tw")
+        .replace("Eighteen", "Eight")
+    )
