200a7bb4ea8df7e77aa8af43a7d2dcc901eb2b48
beep boop <githubbot@vadim.me>
2024-07-24 21:07:19 +0200

    Solution 52 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9wzm29ar Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94d8701f-88a6-4563-8c17-f628f9829 7f1.py", line 41, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/94d8701f-88a6-4563-8c17-f628f9829 7f1.py", line 11, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,6 +1,9 @@
-```
 def sum_squares(lst):
     total_sum = 0
-    for num in lst:
-        total_sum += num ** 2
+    for i in lst:
+        if isinstance(i, (int, float)) and i % 2 != 0:
+            try:
+                total_sum += i**2
+            except TypeError:
+                return "Invalid input. Please enter numeric values."
     return total_sum
