0085fb06d870bf442c9149de43d922cf4a769fe6
beep boop <githubbot@vadim.me>
2024-07-24 09:27:32 +0200

    Solution 20 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/cfsyn1qm Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,17 +3,39 @@ using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
-    vector<vector<int>> res;
-    
-    for (int i = 1; i < n; i++) {
-        if (v[i] - v[0] <= v[n-1] - v[i]) {
-            res.push_back({v.begin(), v.begin() + i});
-            res.push_back({v.begin() + i, v.end()});
+    vector<vector<int>> res(2);
+    int minDiff = INT_MAX;
+    int pos = -1;
+
+    for (int i = 0; i < n; i++) {
+        int leftSum = 0, rightSum = 0;
+        for (int j = 0; j < i; j++) {
+            leftSum += v[j];
+        }
+        for (int j = i; j < n; j++) {
+            rightSum += v[j];
+        }
+
+        if (leftSum == rightSum) {
+            res[0] = vector<int>(v.begin(), v.begin() + i);
+            res[1] = vector<int>(v.begin() + i, v.end());
             return res;
+        } else if (abs(leftSum - rightSum) < minDiff) {
+            minDiff = abs(leftSum - rightSum);
+            pos = i;
         }
     }
-    
-    res.push_back(v);
+
+    int leftSum = 0, rightSum = 0;
+    for (int i = 0; i < pos; i++) {
+        leftSum += v[i];
+    }
+    for (int i = pos; i < n; i++) {
+        rightSum += v[i];
+    }
+
+    res[0] = vector<int>(v.begin(), v.begin() + pos);
+    res[1] = vector<int>(v.begin() + pos, v.end());
     return res;
 }
 
@@ -24,12 +46,15 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
+    
     vector<vector<int>> result = cutVector(v);
+
     for (auto &vec : result) {
         for (int num : vec) {
             cout << num << " ";
         }
         cout << endl;
     }
+
     return 0;
 }
