a0c5954b380150f5afa06d7900df37161dab6989
beep boop <githubbot@vadim.me>
2024-07-24 20:45:55 +0200

    Solution 19 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/f4gnlvy7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a62f2557-1e32-4aca-b265-a8738372d 876.py", line 16, in <module>     check(simplify)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a62f2557-1e32-4aca-b265-a8738372d 876.py", line 13, in check     assert simplify("1/5", "1/5") == False, "test13"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test13

--- Python_144.py
+++ Python_144.py
@@ -1,5 +1,10 @@
+```
 def simplify(x, n):
     x_num, x_denom = map(int, x.split("/"))
     n_num, n_denom = map(int, n.split("/"))
+
+    common_divisor = min(x_denom, n_denom)
+    while common_divisor > 1 and (x_num * n_num) % common_divisor != 0:
+        common_divisor -= 1
 
     return x_num / x_denom == n_num / n_denom
