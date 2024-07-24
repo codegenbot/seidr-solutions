e8be0148bc119bf6e83092f23b5c7c14282ce1b5
beep boop <githubbot@vadim.me>
2024-07-24 02:34:19 +0200

    Solution 53 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/z7qa9oa3 Prompt: f2496138-564a-4423-b80f-81399fa0a6ae.cpp: In function ‘bool evalOp(std::string)’ : f2496138-564a-4423-b80f-81399fa0a6ae.cpp:31:12: error: ‘evalBoolean’ was not dec lared in this scope    31 |     return evalBoolean(s.substr(j));       |            ^~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,34 +2,76 @@
 #include <string>
 using namespace std;
 
-bool solveBoolean(string s) {
-    bool result = true;
-    char lastOp = ' ';
+bool evalBoolean(string s) {
+    if (s.empty())
+        return false;
 
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T' || s[i] == 'F') {
-            result = (s[i] == 'T') ? true : false;
-        } else if (s[i] == '|' || s[i] == '&') {
-            if (lastOp == '|') {
-                return result;
-            } else if (lastOp == '&') {
-                return result && true;  // Assuming `&` has higher precedence
-            }
-        } else {
-            // Ignore invalid characters
+    int j = 0;
+    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+        j++;
+
+    string opStr = s.substr(0, j);
+    bool result = false;
+    if (opStr[0] == '|') {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T')
+                result = true;
+            else if (s[i] == 'F')
+                return false;
         }
-
-        lastOp = s[i];
+    } else {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T' && opStr[1] != '|')
+                result = true;
+            else if (s[i] == 'F' && opStr[1] != '&')
+                return false;
+        }
     }
 
-    return result;
+    if (!result)
+        return false;
+
+    string rest = s.substr(j);
+    if (rest.empty())
+        return !result; 
+    else
+        return evalBoolean(rest); 
+}
+
+bool evalOp(string s) {
+    int j = 0;
+    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+        j++;
+
+    string opStr = s.substr(0, j);
+    bool left = false;
+    if (opStr[0] == '|') {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T')
+                left = true;
+            else if (s[i] == 'F')
+                return false;
+        }
+    } else {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T' && opStr[1] != '|')
+                left = true;
+            else if (s[i] == 'F' && opStr[1] != '&')
+                return false;
+        }
+    }
+
+    if (!left)
+        return false;
+
+    return evalBoolean(s.substr(j));
 }
 
 int main() {
     string s;
     cout << "Enter a Boolean expression: ";
     cin >> s;
-    bool result = solveBoolean(s);
+    bool result = evalOp(s);
     if (result)
         cout << "True";
     else
