2d4479a4e51da9e911925c9f50d962a57987a414
beep boop <githubbot@vadim.me>
2024-07-24 21:38:13 +0200

    Solution 4 of CPP/125, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ddy65jvb Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -6,42 +6,31 @@ using namespace std;
 
 vector<string> split_words(string txt) {
     vector<string> result;
-    size_t pos = 0, prevPos = 0;
-    
-    while ((pos = txt.find_first_of(" ,")) != string::npos) {
-        if (pos == 0) {
-            // Split on commas
-            if (txt[0] == ',') {
-                result.push_back(txt.substr(1));
-                txt.erase(0, 1);
-                prevPos = 0;
-            } else {
-                break;
-            }
-        }
-        
-        size_t word = txt.find_first_not_of(" ,", prevPos);
-        if (word != string::npos) {
-            result.push_back(txt.substr(prevPos, word - prevPos));
-            prevPos = word + 1;
-        } else {
-            result.push_back(txt.substr(prevPos));
-            break;
-        }
+    size_t pos = 0;
+    while ((pos = txt.find(" ")) != string::npos) {
+        result.push_back(txt.substr(0, pos));
+        txt.erase(0, pos + 1);
     }
-    
-    if (result.empty()) {
-        int count = 0;
+    if (txt.empty()) {
+        return result;
+    }
+    size_t commaPos = txt.find(",");
+    if (commaPos == string::npos) {
+        int oddCount = 0;
         for (char c : txt) {
-            if (c >= 'a' && c <= 'z') {
-                count++;
-                if ((int)c % 2 != 0) {
-                    result.push_back(to_string(count));
-                    break;
-                }
+            if (c >= 'a' && c <= 'z' && (int(c) % 2 != 0)) {
+                oddCount++;
             }
         }
+        result.push_back(to_string(oddCount));
+    } else {
+        while ((pos = txt.find(" ")) != string::npos) {
+            result.push_back(txt.substr(0, pos));
+            txt.erase(0, pos + 1);
+        }
+        if (!txt.empty()) {
+            result.push_back(txt);
+        }
     }
-    
     return result;
 }
