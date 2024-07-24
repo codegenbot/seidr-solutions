4fc267110140aa137b3a2b8880dbb041b06e464b
beep boop <githubbot@vadim.me>
2024-07-24 15:36:01 +0200

    Solution 6 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/apf6ljhk Prompt: 01d3b14b-abc2-46e6-a85d-70c434ca0ae8.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 01d3b14b-abc2-46e6-a85d-70c434ca0ae8.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 01d3b14b-abc2-46e6-a85d-70c434ca0ae8.cpp:31: 01d3b14b-abc2-46e6-a85d-70c434ca0ae8.cpp: In function ‘int main()’: 01d3b14b-abc2-46e6-a85d-70c434ca0ae8.cpp:33:13: error: ‘int_to_mini_romank’ was not declared in this scope    33 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -3,12 +3,11 @@
 #include <vector>
 using namespace std;
 
-vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
-                                        {400, "CD"}, {100, "C"}, {90, "XC"},
-                                        {50, "L"}, {40, "XL"}, {10, "X"},
-                                        {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-
 string romanToInt(int number) {
+    vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
+                                            {400, "CD"}, {100, "C"}, {90, "XC"},
+                                            {50, "L"}, {40, "XL"}, {10, "X"},
+                                            {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
     string roman = "";
     
     for (const auto& pair : romanMap) {
@@ -25,6 +24,6 @@ int main() {
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    cout << "The mini Roman numeral is: " << romanToInt(num) << std::endl;
+    cout << "The Roman numeral is: " << romanToInt(num) << endl;
     return 0;
 }
