dc34f7a2d51fe186add9e5729906135daffead9c
beep boop <githubbot@vadim.me>
2024-07-24 07:52:48 +0200

    Solution 63 of Python/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fqpdhp0g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1458c741-e351-4d79-80cf-9336713c6 bb8.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_133.py
+++ Python_133.py
@@ -1,7 +1,4 @@
 ```
-def sum_squares(numbers):
-    return sum([n ** 2 for n in numbers])
-
 while True:
     choice = input("Do you want to calculate sum of squares (yes/no)? ")
     if choice.lower() != 'yes':
@@ -20,3 +17,6 @@ while True:
         print(f"The sum of squares is {total_sum_of_squares}")
 
     break
+
+def sum_squares(numbers):
+    return sum([n ** 2 for n in numbers])
