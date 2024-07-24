6b233f6443cfc447029ec3b1ce5a0adeebab59ee
beep boop <githubbot@vadim.me>
2024-07-24 17:28:03 +0200

    Solution 48 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts9ewlh2 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5f0b2a9d-bd4c-4b5b-9036-67ef5ada907c.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5f0b2a9d-bd4c-4b5b-9036-67ef5ada907c.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5f0b2a9d-bd4c-4b5b-9036-67ef5ada907c.cpp:10: 5f0b2a9d-bd4c-4b5b-9036-67ef5ada907c.cpp: In function ‘int main()’: 5f0b2a9d-bd4c-4b5b-9036-67ef5ada907c.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 5f0b2a9d-bd4c-4b5b-9036-67ef5ada907c.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,19 +1,12 @@
-```cpp
 #include <vector>
 using namespace std;
 
 bool issame(vector<vector<string>> a, vector<vector<string>> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i].size() != b[i].size()) {
-            return false;
-        }
-        sort(a[i].begin(), a[i].end());
-        sort(b[i].begin(), b[i].end());
-        if(a[i] != b[i]) {
-            return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i].size() != b[i].size()) return false;
+        for(int j = 0; j < a[i].size(); j++){
+            if(find(b[i].begin(), b[i].end(), a[i][j]) == b[i].end()) return false;
         }
     }
     return true;
