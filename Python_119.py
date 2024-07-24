4f97b626f0ff92e0a6d4904f9fb2eb33cd33c7af
beep boop <githubbot@vadim.me>
2024-07-24 15:11:23 +0200

    Solution 26 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fdajg309 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1eeec01-5856-4c4d-b737-46e50a047 297.py", line 18, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1eeec01-5856-4c4d-b737-46e50a047 297.py", line 15, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,12 +1,8 @@
 ```
+import re
+
 def match_parens(lst):
-    stack = []
     for s in lst:
-        for c in s:
-            if c.lower() == '(':
-                stack.append(c)
-            elif c.lower() == ')':
-                if len(stack) == 0:
-                    return 'No'
-                stack.pop()
-    return 'Yes' if len(stack) == 0 else 'No'
+        if len(re.findall('\(', s)) != len(re.findall('\)', s)):
+            return 'No'
+    return 'Yes'
