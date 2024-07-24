32996281bfd525699975e2263a36b75e70de04d3
beep boop <githubbot@vadim.me>
2024-07-24 14:10:56 +0200

    Solution 0 of Python/102, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vshxxjr4 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_102.py
@@ -0,0 +1,8 @@
+def choose_num(x, y):
+    if x % 2 == 0 and y % 2 == 0:
+        return max(x, y)
+    else:
+        for i in range(max(x, y), min(x, y), -1):
+            if i % 2 == 0:
+                return i
+        return -1
