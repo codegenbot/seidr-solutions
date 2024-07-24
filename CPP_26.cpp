044edc348e3f4d8baed2bff922a20e924cf8c163
beep boop <githubbot@vadim.me>
2024-07-24 18:53:37 +0200

    Solution 50 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/to49ik7c Prompt:       |             ^~~~~~ 2857b768-cb17-444c-bb20-5afb0cad0532.cpp:20:20: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 2857b768-cb17-444c-bb20-5afb0cad0532.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2857b768-cb17-444c-bb20-5afb0cad0532.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 2857b768-cb17-444c-bb20-5afb0cad0532.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 2857b768-cb17-444c-bb20-5afb0cad0532.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2857b768-cb17-444c-bb20-5afb0cad0532.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,16 +1,13 @@
-#include <vector>
-#include <set>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return (a == b);
-}
-
 std::vector<int> remove_duplicates(std::vector<int> numbers){
     std::set<int> unique_numbers(numbers.begin(), numbers.end());
     return std::vector<int>(unique_numbers.begin(), unique_numbers.end());
 }
 
-int main() {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return (a == b);
+}
+
+int main2() {
     assert((issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5})));
     return 0;
 }
