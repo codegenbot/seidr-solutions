cf8fd4696f490f56863f124d0c62de74acbec839
beep boop <githubbot@vadim.me>
2024-07-24 05:16:53 +0200

    Solution 46 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/afsfrk7g Prompt:       |                    ^~~ 15edec52-bdac-44da-9276-6ff686b9c70a.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 15edec52-bdac-44da-9276-6ff686b9c70a.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 15edec52-bdac-44da-9276-6ff686b9c70a.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 15edec52-bdac-44da-9276-6ff686b9c70a.cpp:10: 15edec52-bdac-44da-9276-6ff686b9c70a.cpp: In function ‘int main()’: 15edec52-bdac-44da-9276-6ff686b9c70a.cpp:20:20: error: ‘common’ was not declared  in this scope    20 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 15edec52-bdac-44da-9276-6ff686b9c70a.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,8 +3,11 @@
 #include <set>
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) if (a[i] != b[i]) return false;
+    if (a.size() != b.size())
+        throw runtime_error("Vectors are not the same size.");
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i])
+            throw runtime_error("Vectors contain different elements.");
     return true;
 }
 
