1d2b40809f0b3959ef69ed5352145b31c8149174
beep boop <githubbot@vadim.me>
2024-07-24 16:13:17 +0200

    Solution 12 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/hiqe3yie Prompt: Complete the following code given the task description and function signature.

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,9 +1,3 @@
-vector<int> remove_duplicates(vector<int> numbers){
-    vector<int> uniqueNumbers;
-    for(auto num : numbers){
-        if (count(numbers.begin(), numbers.end(), num) == 1){
-            uniqueNumbers.push_back(num);
-        }
-    }
-    return uniqueNumbers;
+numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
+    return numbers;
 }
