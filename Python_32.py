2c2b0a881d5773d19438bbd1cf61a645ef3f971c
beep boop <githubbot@vadim.me>
2024-07-24 23:19:19 +0200

    Solution 77 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/z0rl6fru Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d69277bf-c615-4d62-ac2b-780bd79a2 c2f.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,10 +1,10 @@
-def find_zero(*coefficients: list):
-    n = len(coefficients)
+def find_zero(xs):
+    n = len(xs)
     if n % 2 != 0:
         raise ValueError("xs must have even number of coefficients")
-    a = coefficients[0]
-    b = coefficients[1]
-    c = coefficients[2] if n > 2 else 0
-    d = coefficients[3] if n > 3 else 0
+    a = xs[0]
+    b = xs[1]
+    c = xs[2] if n > 2 else 0
+    d = xs[3] if n > 3 else 0
     x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
     return round(x, 2)
