c3b01e601675eb5ef66dc4f6d0f301374354e99b
beep boop <githubbot@vadim.me>
2024-07-25 01:40:31 +0200

    Solution 5 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/c7fceeas Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03886303-b593-4fc8-a19c-0e7493fab bf9.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03886303-b593-4fc8-a19c-0e7493fab bf9.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,13 +1,6 @@
-def is_palindrome(string: str) -> bool:
-    return string == string[::-1]
-
-
 def make_palindrome(string: str) -> str:
     if string == string[::-1]:
-        return string + string
+        return string
     for i in range(len(string)):
-        if (
-            string[: i + 1] == string[: i + 1][::-1]
-            and string[i + 1 :] == string[i + 1 :][::-1]
-        ):
-            return string[: i + 1] + string[i + 1 :][::-1] + string[i + 1 :]
+        if string[i:] == string[i:][::-1]:
+            return string[:i] + string[i:][::-1] + string[i]
