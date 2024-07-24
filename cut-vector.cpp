891f816666966d8c96faccae7ed0bb326636861d
beep boop <githubbot@vadim.me>
2024-07-24 06:32:59 +0200

    Solution 94 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/eiqa17vm Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { 0e59209c-6d6d-432f-bd2e-39b49a308c28.cpp:24:20: error: ‘abs’ was not declared in  this scope    24 |         int diff = abs(leftSum - rightSum);       |                    ^~~ 0e59209c-6d6d-432f-bd2e-39b49a308c28.cpp:33:9: error: ‘cout’ was not declared in  this scope    33 |         cout << nums[i] << endl;       |         ^~~~ 0e59209c-6d6d-432f-bd2e-39b49a308c28.cpp:33:28: error: ‘endl’ was not declared i n this scope    33 |         cout << nums[i] << endl;       |                            ^~~~ 0e59209c-6d6d-432f-bd2e-39b49a308c28.cpp:36:5: error: ‘cout’ was not declared in  this scope    36 |     cout << 0 << endl;       |     ^~~~ 0e59209c-6d6d-432f-bd2e-39b49a308c28.cpp:36:18: error: ‘endl’ was not declared i n this scope    36 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -8,45 +8,45 @@ using namespace std;
 int main() {
     vector<int> nums;
     int num;
-    
+
     while (cin >> num) {
         nums.push_back(num);
     }
-    
+
     int n = nums.size();
-    int prefixSum[n+1];
+    int prefixSum[n + 1];
     prefixSum[0] = 0;
-    
+
     for (int i = 0; i < n; ++i) {
-        prefixSum[i+1] = prefixSum[i] + nums[i];
+        prefixSum[i + 1] = prefixSum[i] + nums[i];
     }
-    
+
     int totalSum = prefixSum[n];
     int minDiff = INT_MAX;
     int cutPoint = -1;
-    
+
     for (int i = 1; i < n; ++i) {
         int leftSum = prefixSum[i];
         int rightSum = totalSum - leftSum;
         int diff = abs(leftSum - rightSum);
-        
+
         if (diff < minDiff) {
             minDiff = diff;
             cutPoint = i;
         }
     }
-    
+
     for (int i = 0; i < cutPoint; ++i) {
         cout << nums[i] << endl;
     }
-    
+
     cout << 0 << endl;
-    
+
     for (int i = cutPoint; i < n; ++i) {
         cout << nums[i] << endl;
     }
-    
+
     cout << 0 << endl;
-    
+
     return 0;
 }
