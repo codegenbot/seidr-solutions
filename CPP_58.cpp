9d57d9fd99b68d4a683333cda38a4723cea2c352
beep boop <githubbot@vadim.me>
2024-07-24 20:15:02 +0200

    Solution 96 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bpeo6ouc Prompt:       |                    ^~~ 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp:21: 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp: In function ‘int main()’: 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp:31:20: error: ‘common’ was not declared  in this scope    31 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 468a7f80-17a3-408b-a4e0-bebcd7468ee2.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,32 +1,28 @@
 ```cpp
 #include <vector>
-#include <set>
-
 bool issame(vector<int> a,vector<int>b){
     return (a==b);
 }
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-    set<int> result;
-    
-    for (auto x : s1) {
-        if (s2.find(x) != s2.end()) {
-            result.insert(x);
-        }
-    }
-    
-    vector<int> res;
-    for (auto x : result) {
-        res.push_back(x);
-    }
-    
-    sort(res.begin(), res.end());
-    return res;
-}
-
 int main() {
     assert (issame(common({4, 3, 2, 8}, {}) , {}));
-    // Rest of your code...
+    vector<int> common(vector<int> l1, vector<int> l2) {
+        set<int> s1(l1.begin(), l1.end());
+        set<int> s2(l2.begin(), l2.end());
+        set<int> result;
+        
+        for (auto x : s1) {
+            if (s2.find(x) != s2.end()) {
+                result.insert(x);
+            }
+       }
+        
+        vector<int> res;
+        for (auto x : result) {
+            res.push_back(x);
+        }
+        
+        sort(res.begin(), res.end());
+        return res;
+    }
 }
