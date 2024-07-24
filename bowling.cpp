17564f8d5b825d8b79b124dfa25a6db73c219526
beep boop <githubbot@vadim.me>
2024-07-24 06:00:04 +0200

    Solution 64 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/bder8du4 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,21 +1,54 @@
-int bowlingScore(string s) {
+#include <vector>
+using namespace std;
+
+int bowlingScore(string bowls) {
     int score = 0;
-    for (int i = 0; i < 10; ++i) {
-        if (s[i] == 'X') {
-            score += 30;
-        } else if (s[i] == '/') {
-            score += 10 + (s[i+1] - '0');
-            i++;
-        } else {
-            int thisFrame = s[i] - '0';
-            if (i < 8 && s[i+1] != '/') {
-                thisFrame *= 2;
-                if (s[i+2] == '/') {
-                    thisFrame += s[i+3] - '0';
+    bool firstRollInFrame = true;
+    vector<int> frameScores;
+
+    for (char bowl : bowls) {
+        if (bowl == '/') {
+            if (firstRollInFrame) {
+                frameScores.push_back(10);
+                firstRollInFrame = false;
+            } else {
+                int roll1 = stoi(string() + rolls.back());
+                rolls.pop_back();
+                int roll2 = stoi(string() + bowl);
+                if (roll2 == 0) {
+                    frameScores.back() += 10;
+                } else {
+                    frameScores.back() += roll1 + roll2;
                 }
             }
-            score += thisFrame;
+        } else {
+            int roll = stoi(string() + bowl);
+            rolls.push_back(roll);
+            if (rolls.size() == 2) {
+                int roll1 = stoi(string() + rolls.front());
+                rolls.pop_front();
+                int roll2 = stoi(string() + bowl);
+                if (roll2 == 0) {
+                    frameScores.push_back(10 + roll1);
+                } else {
+                    frameScores.push_back(roll1 + roll2);
+                }
+            }
         }
     }
+
+    for (int i = 0; i < frameScores.size(); i++) {
+        score += frameScores[i];
+    }
+
     return score;
+}
+
+int main() {
+    string bowls;
+    cout << "Enter the bowling strings separated by spaces: ";
+    cin >> bowls;
+    int score = bowlingScore(bows);
+    cout << "The bowling score is: " << score << endl;
+    return 0;
 }
