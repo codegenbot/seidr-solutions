b9ffe592f9eacf9f1769396d755a921a95458f88
beep boop <githubbot@vadim.me>
2024-07-24 16:31:56 +0200

    Solution 56 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/iqu0ssj4 Prompt: Program did not receive expected input

--- Python_103.py
+++ Python_103.py
@@ -1,11 +1,11 @@
+```
 import statistics
 
 def rounded_avg():
-    n = int(input("Enter first number: "))
-    m = int(input("Enter second number: "))
+    n = float(input("Enter first number: "))
+    m = float(input("Enter second number: "))
     
     if n > m:
         return -1
-    else:
-        avg = int(statistics.mean([n, m])) 
-        return bin(avg)[2:]
+    avg = int(statistics.mean([n, m])) 
+    return bin(avg)[2:]
