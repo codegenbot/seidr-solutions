c1a3ed5fdd8f19fea1cd2965766c509165d6cf00
beep boop <githubbot@vadim.me>
2024-07-24 21:27:01 +0200

    Solution 16 of Python/7, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jusl6unk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe8449a8-a8b7-4e0f-ae95-388a82e0d a03.py", line 1, in <module>     def filter_by_substring(strings: List[str], substring: str) -> List[str]:                                      ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_7.py
+++ Python_7.py
@@ -1,2 +1,2 @@
-def filter_by_substring(strings: List[str], substring: str) -> List[str]:
+def filter_by_substring(strings: list, substring: str) -> list:
     return [s for s in strings if substring in s]
