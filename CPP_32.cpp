44a064e12b3bbf8a5eaebfa6063e2ae517e176cb
beep boop <githubbot@vadim.me>
2024-07-24 05:29:45 +0200

    Solution 97 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/13qyj88i Prompt: bce73718-92de-4fe3-a963-7704ca90f112.cpp:19:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    19 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff bce73718-92de-4fe3-a963-7704ca90f112.cpp:22:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    22 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff bce73718-92de-4fe3-a963-7704ca90f112.cpp:22:36: error: ‘find_zero’ cannot be use d as a function    22 |         solution = find_zero(coeffs);       |                                    ^ In file included from bce73718-92de-4fe3-a963-7704ca90f112.cpp:6: bce73718-92de-4fe3-a963-7704ca90f112.cpp:23:21: error: ‘poly’ was not declared i n this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ bce73718-92de-4fe3-a963-7704ca90f112.cpp:23:17: error: ‘abs’ was not declared in  this scope    23 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
 #include <vector>
-using namespace std;
 
-vector<double> coeffs;
-double poly(vector<double>, double);
+double poly(vector<double> c, double x) {
+    double result = 0.0;
+    for (int i = 0; i < c.size(); i++) {
+        result += c[i] * pow(x, i);
+    }
+    return result;
+}
 
-double find_zero(vector<double> xs){
-    double x = -xs[1] / (2*xs[0]);
+double find_zero(vector<double> coeffs){
+    vector<double> xs(2); // Initialize a vector of size 2
+    double x = -coeffs[1] / (2*coeffs[0]);
+    double solution = poly(coeffs, x); 
+    if (solution > 1e-6) { // Check the accuracy of the result
+        return find_zero(coeffs); // Recursively call the function until the desired accuracy is reached
+    }
     return x;
 }
