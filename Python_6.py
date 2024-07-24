2da4a11ae986fddbbc485f18f0252dc6355c6b13
beep boop <githubbot@vadim.me>
2024-07-24 17:31:25 +0200

    Solution 48 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cgipld2i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f1c75b32-496e-4f1f-8e9e-82006b97f bee.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -10,5 +10,6 @@ def parse_nested_parens(paren_string: str) -> List[int]:
             level -= 1
         if level > max_level:
             max_level = level
+        result.append(max_level)
     
-    return [max_level]
+    return result
