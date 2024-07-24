eb1bede52d37aedf84f26c8eae771fca66311783
beep boop <githubbot@vadim.me>
2024-07-24 09:22:47 +0200

    Solution 94 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/n60d1r45 Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,48 +1,23 @@
-#include <string>
-using namespace std;
+Here is the solution:
 
-bool solveBoolean(string expression) {
-    if (expression == "T" || expression == "t")
-        return true;
-    if (expression == "F" || expression == "f")
-        return false;
-    
-    int i = 0, j = 0;
-    while(j < expression.size()) {
-        if(expression[j] == '&') {
-            j++;
-            continue;
-        }
-        
-        string operand1 = "";
-        while(i < expression.size() && expression[i] != '&' && expression[i] != '|') {
-            operand1 += expression[i];
-            i++;
-        }
-        i--;
-        
-        bool op1 = (operand1 == "T" || operand1 == "t");
-        
-        j++;
-        
-        if(j >= expression.size())
-            return op1;
-        
-        string operand2 = "";
-        while(i < expression.size() && expression[i] != '&' && expression[i] != '|') {
-            operand2 += expression[i];
-            i++;
-        }
-        i--;
-        
-        bool op2 = (operand2 == "T" || operand2 == "t");
-        
-        if(expression[j] == '&') {
-            return op1 && op2;
+bool solveBoolean(string s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '|') {
+            char op1 = st.top();
+            st.pop();
+            char op2 = st.top();
+            st.pop();
+            st.push(c == 'T' ? true : c == 'F' ? false : op1 || op2);
+        } else if (c == '&') {
+            char op1 = st.top();
+            st.pop();
+            char op2 = st.top();
+            st.pop();
+            st.push(c == 'T' ? true : c == 'F' ? false : op1 && op2);
         } else {
-            return op1 | op2;
+            st.push(c == 'T');
         }
     }
-    
-    return false;
+    return (bool)st.top();
 }
