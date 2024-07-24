1fe77264b55560aacc4ade11d942a5260855f07d
beep boop <githubbot@vadim.me>
2024-07-24 22:50:25 +0200

    Solution 53 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2knu02au Prompt:    40 |         tie(i, j) = make_pair(nextPoses[0].second, nextPoses[0].third);       |                                                                 ^~~~~ In file included from 4d15dd9e-99f0-4d05-adb7-6161239c77fd.cpp:46: 4d15dd9e-99f0-4d05-adb7-6161239c77fd.cpp: In function ‘int main()’: 4d15dd9e-99f0-4d05-adb7-6161239c77fd.cpp:56:27: error: cannot bind non-const lva lue reference of type ‘std::vector<std::vector<int> >&’ to an rvalue of type ‘st d::vector<std::vector<int> >’    56 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 4d15dd9e-99f0-4d05-adb7-6161239c77fd.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = std::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<std::vector<int> >] ’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 4d15dd9e-99f0-4d05-adb7-6161239c77fd.cpp:5:42: note:   initializing argument 1 o f ‘std::vector<int> minPath(std::vector<std::vector<int> >&, int)’     5 | vector<int> minPath(vector<vector<int>>& grid, int k) {       |                     ~~~~~~~~~~~~~~~~~~~~~^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -44,6 +44,6 @@ vector<int> minPath(vector<vector<int>>& grid, int k) {
 }
 
 int main() {
-    assert(minPath({{1, 3}, {3, 2}}, 10) == vector<int>(1, 3, 1, 3, 1, 3, 1, 3, 1, 3));
+    assert (minPath({{1, 3}, {3, 2}}, 10) == vector<int>(1, 3, 1, 3, 1, 3, 1, 3, 1, 3));
     return 0;
 }
