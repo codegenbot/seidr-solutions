f8b4c52666e5108d5f81d81193de82e801caa2f2
beep boop <githubbot@vadim.me>
2024-07-24 06:44:44 +0200

    Solution 20 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/2te213o7 Prompt: n this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                    ^~~~ 89792728-b726-45b5-a376-5f07f52c477b.cpp:17:30: error: ‘vec2’ was not declared i n this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                              ^~~~ 89792728-b726-45b5-a376-5f07f52c477b.cpp:17:16: error: ‘pow’ was not declared in  this scope    17 |         sum += pow(vec1[i] - vec2[i], 2);       |                ^~~ 89792728-b726-45b5-a376-5f07f52c477b.cpp:20:22: error: ‘sqrt’ was not declared i n this scope    20 |     float distance = sqrt(sum);       |                      ^~~~ 89792728-b726-45b5-a376-5f07f52c477b.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << distance << endl;       |     ^~~~ 89792728-b726-45b5-a376-5f07f52c477b.cpp:21:25: error: ‘endl’ was not declared i n this scope    21 |     cout << distance << endl;       |                         ^~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -2,17 +2,20 @@
 #include <vector>
 #include <cmath>
 
+using namespace std;
+
 int main() {
     int n;
-    std::cin >> n;
-    std::vector<float> vec1(n);
+    cin >> n;
+    vector<float> vec1(n);
     for (int i = 0; i < n; ++i) {
-        std::cin >> vec1[i];
+        cin >> vec1[i];
     }
     
-    std::vector<float> vec2(n);
+    cin >> n;
+    vector<float> vec2(n);
     for (int i = 0; i < n; ++i) {
-        std::cin >> vec2[i];
+        cin >> vec2[i];
     }
     
     float sum = 0.0;
@@ -20,10 +23,8 @@ int main() {
         sum += pow(vec1[i] - vec2[i], 2);
     }
     
-    n = vec1.size(); // Update n to ensure correct size for further calculations
-    
     float distance = sqrt(sum);
-    std::cout << distance << std::endl;
+    cout << distance << endl;
     
     return 0;
 }
