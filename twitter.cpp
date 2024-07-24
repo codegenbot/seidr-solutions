5acfbfbb3ee94c699541892f8d20b9f2b612f207
beep boop <githubbot@vadim.me>
2024-07-24 21:04:27 +0200

    Solution 20 of twitter, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/kqc23wft Prompt:       |         ^~~~ 06cd98f2-7255-4643-b3ab-e702a1b32c61.cpp:6:47: error: ‘endl’ was not declared in  this scope     6 |         cout << "You didn't type anything" << endl;       |                                               ^~~~ 06cd98f2-7255-4643-b3ab-e702a1b32c61.cpp:8:9: error: ‘cout’ was not declared in this scope     8 |         cout << "Too many characters" << endl;       |         ^~~~ 06cd98f2-7255-4643-b3ab-e702a1b32c61.cpp:8:42: error: ‘endl’ was not declared in  this scope     8 |         cout << "Too many characters" << endl;       |                                          ^~~~ 06cd98f2-7255-4643-b3ab-e702a1b32c61.cpp:10:9: error: ‘cout’ was not declared in  this scope    10 |         cout << "Your tweet has " << tweet.length() << " characters" << endl;       |         ^~~~ 06cd98f2-7255-4643-b3ab-e702a1b32c61.cpp:10:73: error: ‘endl’ was not declared i n this scope    10 |   cout << "Your tweet has " << tweet.length() << " characters" << endl;       |                                                                   ^~~~

--- twitter.cpp
+++ twitter.cpp
@@ -1,14 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 int main() {
     string tweet;
     getline(cin, tweet);
 
-    if (tweet.empty()) {
-        cout << "You didn't type anything" << endl;
-    } else if (tweet.length() > 140) {
-        cout << "Too many characters" << endl;
-    } else {
-        cout << "Your tweet has " << tweet.length() << " characters" << endl;
-    }
+    if (tweet.empty()) 
+        cout << "You didn't type anything" << "\n";
+    else if (tweet.length() > 140) 
+        cout << "Too many characters" << "\n";
+    else 
+        cout << "Your tweet has " << tweet.length() << " characters" << "\n";
 
     return 0;
 }
