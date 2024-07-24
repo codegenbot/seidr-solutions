134a5b9bbb0b2a10a002f631617b4bd265bdf7eb
beep boop <githubbot@vadim.me>
2024-07-24 17:25:53 +0200

    Solution 33 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/8xzfuftn Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ fa944e18-fb9f-411d-84e5-f47132d4aaec.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fa944e18-fb9f-411d-84e5-f47132d4aaec.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from fa944e18-fb9f-411d-84e5-f47132d4aaec.cpp:23: fa944e18-fb9f-411d-84e5-f47132d4aaec.cpp: In function ‘int main()’: fa944e18-fb9f-411d-84e5-f47132d4aaec.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ fa944e18-fb9f-411d-84e5-f47132d4aaec.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -2,18 +2,14 @@ bool issame(float a, float b) {
     return abs(a - b) < 1e-9;
 }
 
-bool sort_even(float a, float b) {
-    return a < b;
-}
-
-vector<float> main(vector<float>& l) {
+void sort_even(vector<float>& l) {
     vector<float> even_indices;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_indices.push_back(l[i]);
         }
     }
-    sort(even_indices.begin(), even_indices.end(), sort_even);
+    sort(even_indices.begin(), even_indices.end());
 
     vector<float> result;
     int even_index = 0;
@@ -26,5 +22,10 @@ vector<float> main(vector<float>& l) {
         }
     }
 
-    return result;
+    l = result;
+}
+
+int main() {
+    // Code for main function
+    return 0;
 }
