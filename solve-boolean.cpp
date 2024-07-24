043060f21821f55ce27e8addc897f43d556ee9c1
beep boop <githubbot@vadim.me>
2024-07-24 08:09:56 +0200

    Solution 19 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/n60d1r45 Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,27 +2,22 @@
 using namespace std;
 
 bool solveBoolean(string s) {
-    stack<char> st;
-    bool res = false;
-    
-    for(int i=0; i<s.length(); i++) {
-        if(s[i] == 'T')
-            res = true;
-        else if(s[i] == 'F')
+    bool res = true;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'f') {
             res = false;
-        else if(s[i] == '|') {
-            while(!st.empty()) st.pop();
-            bool temp = res;
-            res = (res) ? true : false;
-        }
-        else if(s[i] == '&') {
-            while(!st.empty()) st.push(st.top());
-            bool temp = res;
-            res = (res) ? true : false;
-            while(!st.empty()) st.pop();
-            res = temp && res;
+            break;
+        } else if (s[i] == '|') {
+            res = true;
+            break;
         }
     }
-    
     return res;
+}
+
+int main() {
+    string input;
+    cin >> input;
+    cout << (solveBoolean(input) ? "True" : "False") << endl;
+    return 0;
 }
