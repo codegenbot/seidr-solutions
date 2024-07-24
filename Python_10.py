7baa21b361d7d1189facdaca2e411de6e0333e6d
beep boop <githubbot@vadim.me>
2024-07-24 10:27:09 +0200

    Solution 68 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cst1c9qx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4849062b-0c0b-4e12-91b6-5efdbe87b d49.py", line 28, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4849062b-0c0b-4e12-91b6-5efdbe87b d49.py", line 25, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,18 +1,20 @@
-def make_palindrome(s):
-    if s == s[::-1]:
-        return s
-    for char in range(len(s)):
-        postfix = s[char:]
+```
+def make_palindrome(string: str) -> str:
+    if string == string[::-1]:
+        return string
+    for i in range(len(string)):
+        postfix = string[i:]
         if postfix == postfix[::-1]:
-            left_half = s[:char]
+            left_half = string[:i]
             right_half = postfix[::-1]
             half_length = len(postfix) // 2
             left_middle_chars = postfix[:half_length]
             right_middle_chars = postfix[half_length:]
-            return "".join([left_half, (left_middle_chars + right_middle_chars)[::-1]])
-    half = len(s) // 2
-    left_half = s[:half]
-    right_half = s[half:][::-1]
+            return "".join([left_half] + [char for char in left_middle_chars + right_middle_chars][::-1])
+    half = len(string) // 2
+    left_half = string[:half]
+    right_half = string[half:][::-1]
     if len(left_half) < len(right_half):
-        return "".join([left_half + left_half[::-1], right_half[1:]])
-    return "".join([left_half, right_half[1:]]);
+        return "".join([char for char in left_half + left_half[::-1]] + list(right_half))
+    else:
+        return left_half + right_half
