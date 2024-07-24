ef93974a8e7061fb06019d123cf6930026a4bc02
beep boop <githubbot@vadim.me>
2024-07-24 06:05:23 +0200

    Solution 1 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vv9eja35 Prompt: Complete the following code given the task description and function signature.

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 vector<int> sort_array(vector<int> arr) {
-    sort(arr.begin(), arr.end(), 
-        [](int a, int b) {
-            if (bitset<32>(a).count() == bitset<32>(b).count()) {
-                return a < b;
-            }
-            return bitset<32>(a).count() < bitset<32>(b).count();
+    sort(arr.begin(), arr.end(), [](int a, int b) {
+        if (bitset<64>(a).count() != bitset<64>(b).count()) {
+            return bitset<64>(a).count() < bitset<64>(b).count();
+        }
+        return a < b;
     });
     return arr;
 }
