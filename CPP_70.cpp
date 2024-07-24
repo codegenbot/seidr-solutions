0ddcf26bd0690e5fa8f38e697d990d5acaed9017
beep boop <githubbot@vadim.me>
2024-07-24 09:18:29 +0200

    Solution 50 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/29lhgvm6 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 88cafcb1-8345-4c88-9371-b2adc4e7d0e9.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,7 +1,5 @@
-using namespace std;
-
 #include <algorithm>
-#include <vector>
+using namespace std;
 
 bool issame(vector<int> a, vector<int> b) {
     if (a.size() != b.size())
@@ -19,13 +17,13 @@ vector<int> strange_sort_vector(vector<int> lst) {
         int min_val = *min_element(lst.begin(), lst.end());
         int max_val = *max_element(lst.begin(), lst.end());
 
-        auto it_min = std::find_if(lst.begin(), lst.end(), [min_val](int val) { return val == min_val; });
-        auto it_max = std::find_if(lst.begin(), lst.end(), [max_val](int val) { return val == max_val; });
+        auto it_min = find_if(lst.begin(), lst.end(), [min_val](int val) { return val == min_val; });
+        auto it_max = find_if(lst.begin(), lst.end(), [max_val](int val) { return val == max_val; });
 
         result.push_back(*it_min);
         lst.erase(it_min);
         result.push_back(*it_max);
-        lst.erase(it_max);
+        lst.erase(distance(it_max, end(lst)), 1);
     }
 
     return result;
