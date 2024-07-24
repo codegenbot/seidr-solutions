1981fbf2d374f122eaba7df1858fceb231c39d57
beep boop <githubbot@vadim.me>
2024-07-24 13:23:26 +0200

    Solution 42 of CPP/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/q6l7iwc6 Prompt: In file included from 73645057-0464-44bc-b913-ce58a17869fb.cpp:5: 73645057-0464-44bc-b913-ce58a17869fb.cpp: In function ‘int main()’: 73645057-0464-44bc-b913-ce58a17869fb.cpp:7:31: error: invalid conversion from ‘c onst char*’ to ‘int’ [-fpermissive]     7 |     assert  (cycpattern_check("winemtt","tinem") == true );       |                               ^~~~~~~~~       |                               |       |                               const char* 73645057-0464-44bc-b913-ce58a17869fb.cpp:1:42: note:   initializing argument 1 o f ‘bool cycpattern_check(const int&, const int&)’     1 | bool cycpattern_check(const std::string& a, const std::string& b) {       |                       ~~~~~~~~~~~~~~~~~~~^ In file included from 73645057-0464-44bc-b913-ce58a17869fb.cpp:5: 73645057-0464-44bc-b913-ce58a17869fb.cpp:7:41: error: invalid conversion from ‘c onst char*’ to ‘int’ [-fpermissive]     7 |     assert  (cycpattern_check("winemtt","tinem") == true );       |                                         ^~~~~~~       |                                         |       |                                         const char* 73645057-0464-44bc-b913-ce58a17869fb.cpp:1:64: note:   initializing argument 2 o f ‘bool cycpattern_check(const int&, const int&)’     1 | bool cycpattern_check(const std::string& a, const std::string& b) {       |                                             ~~~~~~~~~~~~~~~~~~~^

--- CPP_154.cpp
+++ CPP_154.cpp
@@ -7,5 +7,5 @@ bool cycpattern_check(const std::string& a, const std::string& b) {
 }
 
 int main() {
-    assert(cycpattern_check("winemtt", "tinem") == true);
+    assert(cycpattern_check(std::string("winemtt"), std::string("tinem")) == true);
 }
