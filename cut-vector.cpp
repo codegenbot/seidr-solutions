49b3577f74ba072c79871842ef355129b7da0847
beep boop <githubbot@vadim.me>
2024-07-24 06:49:14 +0200

    Solution 94 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,16 +1,22 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<vector<int>> cutVector(vector<int> v) {
     int min_diff = INT_MAX;
-    int cut_index = -1;
+    int cut_index = 0;
 
-    for (int i = 0; i < v.size(); ++i) {
-        int left_sum = accumulate(v.begin(), v.begin() + i, 0);
-        int right_sum = accumulate(v.begin() + i + 1, v.end(), 0);
-
-        if (left_sum == right_sum) {
-            return {{v[0], v[0]}, v.substr(1)};
+    for (int i = 1; i <= v.size(); i++) {
+        int left_sum = 0, right_sum = 0;
+        if (i < v.size()) {
+            for (int j = 0; j < i; j++) {
+                left_sum += v[j];
+            }
+            for (int j = i; j < v.size(); j++) {
+                right_sum += v[j];
+            }
+        } else {
+            left_sum = accumulate(v.begin(), v.end(), 0);
+            right_sum = 0;
         }
 
         int diff = abs(left_sum - right_sum);
@@ -20,7 +26,25 @@ pair<vector<int>, vector<int>> cutVector(vector<int> v) {
         }
     }
 
-    vector<int> left(v.begin(), v.begin() + cut_index);
-    vector<int> right(v.begin() + cut_index, v.end());
-    return {left, right};
+    return {{vector<int>(v.begin(), v.begin() + cut_index)},
+            {vector<int>(v.begin() + cut_index, v.end())}};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    
+    vector<vector<int>> res = cutVector(v);
+    for (vector<int> &v : res) {
+        for (int x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
