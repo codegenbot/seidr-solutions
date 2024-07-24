bb5846e074c632f82f871530b7c0759ae657e5ab
beep boop <githubbot@vadim.me>
2024-07-24 08:51:01 +0200

    Solution 61 of CPP/114, ratio 0.08333333333333333 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/dwfa9r3k Prompt: 1e83ad23-4b3d-4b6d-b09f-1bb28b930e20.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 1e83ad23-4b3d-4b6d-b09f-1bb28b930e20.cpp:25: 1e83ad23-4b3d-4b6d-b09f-1bb28b930e20.cpp: In function ‘int main()’: 1e83ad23-4b3d-4b6d-b09f-1bb28b930e20.cpp:27:13: error: ‘minSubArraySum’ was not declared in this scope    27 |     assert (minSubArraySum({1, -1}) == -1);       |             ^~~~~~~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -5,13 +5,16 @@ long long minSubArraySum(vector<long long> nums) {
     for (int i = 0; i < nums.size(); i++) {
         current_sum += nums[i];
 
-        if (current_sum < min_sum)
-            min_sum = current_sum;
-
-        if (current_sum > 0)
+        if (current_sum > min_sum)
             continue;
 
-        current_sum = 0;
+        if (i == 0 || current_sum < min_sum) {
+            min_sum = current_sum;
+        }
+
+        if (current_sum < 0) {
+            current_sum = 0;
+        }
     }
 
     return min_sum;
