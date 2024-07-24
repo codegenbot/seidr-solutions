51bd771b1fe16f7544d5b8a61d3b4a9197115736
beep boop <githubbot@vadim.me>
2024-07-24 03:37:55 +0200

    Solution 20 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3925fb09-8add-489c-a2d5-6cea6bab7 10d.py", line 17, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3925fb09-8add-489c-a2d5-6cea6bab7 10d.py", line 14, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,9 +1,7 @@
 ```
 def solve(K=None):
+    N = int(input("Enter a number: "))
     if K is None:
-        N = int(input("Enter another number: "))
-        return ''.join('1' if i % (2**k) < 2**(k-1) else '0' for k in range(1, N.bit_length()+1))
-    N = int(input("Enter a number: ") if K is None else K)
-    if N < 1:
-        return str(0)
-    return str(K * ((N - 1) // K))
+        return bin(N)[2:]
+    else:
+        return str(K * ((N - 1) // K))
