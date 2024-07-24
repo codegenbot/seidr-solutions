6ebc924346e03187886b73f7121e8cbd4a8a82da
beep boop <githubbot@vadim.me>
2024-07-24 03:42:09 +0200

    Solution 23 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/19ed3443-da4f-449e-b5d0-54490c499 3f8.py", line 19, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/19ed3443-da4f-449e-b5d0-54490c499 3f8.py", line 16, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,8 +1,8 @@
-def solve(K=None):
-    if K is None:
-        N = int(input("Enter another number: "))
-        return ''.join('1' if i % (2**k) < 2**(k-1) else '0' for k in range(1, N.bit_length()+1))
-    N = int(input("Enter a number: ") if K is None else K)
-    if N < 1:
-        return str(0)
-    return str(K * ((N - 1) // K))
+def solve(K):
+    if K < 1:
+        return '0'
+    binary = ''
+    while K > 0:
+        binary = str(K % 2) + binary
+        K //= 2
+    return binary
