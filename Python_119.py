62f4639e5b4727c9c1675692a7388ce86479dfc9
beep boop <githubbot@vadim.me>
2024-07-24 20:33:23 +0200

    Solution 44 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/dwghi1j9 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e5c966cd-73da-4f26-89d1-39a0515f9 e43.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,3 +1,10 @@
+```
+"""
+Check if two strings of parentheses match.
+The function takes a list of two strings as input. It checks if the first string contains an opening parenthesis for every closing parenthesis in the second string, and vice versa.
+
+Return 'Yes' if the parentheses match, 'No' otherwise.
+"""
 def match_parens(lst):
     s1 = lst[0]
     s2 = lst[1]
