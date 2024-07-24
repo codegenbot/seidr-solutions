343f988f35df33f7eb64e2b500189c30e91d2f4e
beep boop <githubbot@vadim.me>
2024-07-24 11:15:52 +0200

    Solution 56 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/t6325142 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ae8f5f50-5972-4588-92bb-d021c50b7 290.py", line 4     return [max([sum([1 for char in paren_string if char == '('][:i]) for i in r ange(1, paren_string.count('(') + 1])] for paren_string in paren_string.split()]                                    ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_6.py
+++ Python_6.py
@@ -1,4 +1,13 @@
 from typing import List
 
+
 def parse_nested_parens(paren_string: str) -> List[int]:
-    return [max([sum([1 for char in paren_string if char == '('][:i]) for i in range(1, paren_string.count('(') + 1)]) for paren_string in paren_string.split()]
+    return [
+        max(
+            [
+                sum([1 for char in paren_string if char == "("][:i])
+                for i in range(1, paren_string.count("(") + 1)
+            ]
+        )
+        for paren_string in paren_string.split()
+    ]
