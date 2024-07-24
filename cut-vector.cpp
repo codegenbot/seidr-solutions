f75acee5f2d504541b2d24fb26f2cb40c1a0e74b
beep boop <githubbot@vadim.me>
2024-07-24 10:17:36 +0200

    Solution 47 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/cfsyn1qm Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,30 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
+vector<int> cutVector(vector<int>& vec) {
     int minDiff = INT_MAX;
     int cutIndex = -1;
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < vec.size() - 1; i++) {
         int leftSum = 0, rightSum = 0;
         
-        for (int j = 0; j < i; j++) {
-            leftSum += v[j];
+        for (int j = 0; j <= i; j++) {
+            leftSum += vec[j];
         }
-        for (int j = i; j < n; j++) {
-            rightSum += v[j];
+        
+        for (int j = i + 1; j < vec.size(); j++) {
+            rightSum += vec[j];
         }
         
         int diff = abs(leftSum - rightSum);
-        if (diff <= minDiff) {
+        if (diff < minDiff) {
             minDiff = diff;
             cutIndex = i;
         }
     }
     
-    vector<int> left, right;
-    for (int i = 0; i < cutIndex; i++) {
-        left.push_back(v[i]);
-    }
-    for (int i = cutIndex; i < n; i++) {
-        right.push_back(v[i]);
-    }
+    vector<int> leftVec(vec.begin(), vec.begin() + cutIndex + 1);
+    vector<int> rightVec(vec.begin() + cutIndex + 1, vec.end());
     
-    return {left, right};
+    return {leftVec, rightVec};
 }
