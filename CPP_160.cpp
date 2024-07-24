da07cd01c8b8cfededd1fa7b6c18902f38e2da34
beep boop <githubbot@vadim.me>
2024-07-24 15:47:51 +0200

    Solution 4 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rec3ih7w Prompt:       |                        ^~~~~ 398b6719-daa7-403f-8264-a83ec70152c9.cpp:9:31: error: request for member ‘begin’  in ‘operators’, which is of non-class type ‘int’     9 |         } else if (*operators.begin() == "-") {       |                               ^~~~~ 398b6719-daa7-403f-8264-a83ec70152c9.cpp:11:31: error: request for member ‘begin ’ in ‘operators’, which is of non-class type ‘int’    11 |         } else if (*operators.begin() == "*") {       |                               ^~~~~ 398b6719-daa7-403f-8264-a83ec70152c9.cpp:13:31: error: request for member ‘begin ’ in ‘operators’, which is of non-class type ‘int’    13 |         } else if (*operators.begin() == "//") {       |                               ^~~~~ 398b6719-daa7-403f-8264-a83ec70152c9.cpp:15:31: error: request for member ‘begin ’ in ‘operators’, which is of non-class type ‘int’    15 |         } else if (*operators.begin() == "**") {       |                               ^~~~~ In file included from 398b6719-daa7-403f-8264-a83ec70152c9.cpp:22: 398b6719-daa7-403f-8264-a83ec70152c9.cpp: In function ‘int main()’: 398b6719-daa7-403f-8264-a83ec70152c9.cpp:24:13: error: ‘do_algebra’ was not decl ared in this scope; did you mean ‘doAlgebra’?    24 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ^~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,20 +1,14 @@
-#include <vector>
-#include <cmath>
-
-int doAlgebra(std::vector<char> operators, std::vector<int> operands) {
-    int result = 0;
-    for (int i = 1; i < operands.size(); i++) {
-        if (operators[0] == '+') {
-            result += operands[i];
-        } else if (operators[0] == '-') {
-            result -= operands[i];
-        } else if (operators[0] == '*') {
-            result *= operands[i];
-        } else if (operators[0] == '/') {
-            result = result / operands[i];
-        } else if (operators[0] == '**') {
-            result = pow(result, operands[i]);
-        }
+for (int i = 1; i < operators.size(); i++) {
+    string op = operators[i];
+    if (op == "+") {
+        result += operands[i];
+    } else if (op == "-") {
+        result -= operands[i];
+    } else if (op == "*") {
+        result *= operands[i];
+    } else if (op == "//") {
+        result = result / operands[i];
+    } else if (op == "**") {
+        result = pow(result, operands[i]);
     }
-    return result;
 }
