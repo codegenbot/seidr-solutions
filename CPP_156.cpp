8d7b75f5e2a27fa707823936d64f148e3a275a74
beep boop <githubbot@vadim.me>
2024-07-24 09:48:03 +0200

    Solution 37 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/puryu2f2 Prompt:       | ^ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp: In function ‘int testMain()’: 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:20:5: error: ‘assert’ was not declared in this scope    20 |     assert(int_to_mini_roman(1000) == "M");       |     ^~~~~~ 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <string>   +++ |+#include <cassert>     4 | In file included from 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:26: 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp: In function ‘int main()’: 604b527d-292f-4d95-b2f3-e0d0493867bc.cpp:28:13: error: ‘int_to_mini_romank’ was not declared in this scope; did you mean ‘int_to_mini_roman’?    28 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -18,7 +18,7 @@ std::string int_to_mini_roman(int number) {
 
 int main() {
     assert(int_to_mini_roman(1000) == "M");
-    assert(int_to_mini_roman(1999) == "1999");
+    assert(int_to_mini_roman(1999) == "MCMXCIX");
     assert(int_to_mini_roman(2024) == "MMXXIV");
     return 0;
 }
