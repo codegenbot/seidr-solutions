5eecb00a9e1ed4d8a8cfb4e33f1ffc07a061e20a
beep boop <githubbot@vadim.me>
2024-07-24 16:00:34 +0200

    Solution 39 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/z12svfy3 Prompt: ('0', '1', '0') Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0e419349-e9ee-4788-83a2-fec96990d 42b.py", line 26, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0e419349-e9ee-4788-83a2-fec96990d 42b.py", line 23, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -7,8 +7,13 @@ def minPath(grid, k):
         row, col, path = queue.pop(0)
         if (row == N-1 and col == N-1) and len(path) == k:
             res = tuple(path)
-        elif len(path) < k:
+        elif (row == N-1 or col == N-1):
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < N and 0 <= nc < N:
+                    queue.append((nr, nc, path + [grid[nr][nc]]))
+        else:
+            for dr, dc in [(0, 1), (1, 0)]:
                 nr, nc = row + dr, col + dc
                 if 0 <= nr < N and 0 <= nc < N:
                     queue.append((nr, nc, path + [grid[nr][nc]]))
