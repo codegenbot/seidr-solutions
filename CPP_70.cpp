48e37267ea6b6713de29148b2e98fda40f092cfe
beep boop <githubbot@vadim.me>
2024-07-24 07:09:46 +0200

    Solution 94 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pbvkyomh Prompt:       |                    ^~~ ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp:27:27: error: ‘vector’ was not declared  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp:26: ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp: In function ‘int main()’: ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp:36:20: error: ‘strange_sort_list’ was n ot declared in this scope    36 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ ed640e99-0279-4f9e-a3bf-89f116c950f0.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,10 +3,12 @@
 #include <algorithm>
 
 bool issame(int a, int b) {
-    if (a % 2 == 0 && b % 2 != 0 || a % 2 != 0 && b % 2 == 0) {
+    if (a > b)
+        return false;
+    else if (b > a)
         return true;
-    }
-    return false;
+    else
+        return true; // For equality or the same value
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
@@ -14,34 +16,32 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
     if (lst.empty()) {
         return result;
     }
-
+    
     while (!lst.empty()) {
-        int min = *min_element(lst.begin(), lst.end());
-        int max = *max_element(lst.begin(), lst.end());
-
+        int min = *std::min_element(lst.begin(), lst.end());
+        int max = *std::max_element(lst.begin(), lst.end());
+        
         if (issame(min, max)) {
             result.insert(result.begin(), min);
             lst.clear();
         } else {
             result.push_back(min);
             lst.erase(std::remove(lst.begin(), lst.end(), min), lst.end());
-
+            
             result.push_back(max);
             lst.erase(std::remove(lst.begin(), lst.end(), max), lst.end());
         }
     }
-
+    
     return result;
 }
 
 int main() {
-    std::vector<int> input = {1, 2, 3, 4, 5};
-    std::vector<int> output = strange_sort_list(input);
-
-    for (auto i : output) {
+    std::vector<int> lst = {2, 6, 4, 7, 1};
+    for (auto i : strange_sort_list(lst)) {
         std::cout << i << " ";
     }
     std::cout << std::endl;
-
+    
     return 0;
 }
