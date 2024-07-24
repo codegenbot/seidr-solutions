fe365964cfa0718df9dc18f9b57afe1684e40a98
beep boop <githubbot@vadim.me>
2024-07-24 04:13:53 +0200

    Solution 45 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k4a5f9yk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/75821edd-6587-4188-b884-83e99a85e 49d.py", line 26, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/75821edd-6587-4188-b884-83e99a85e 49d.py", line 22, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,14 +2,12 @@
 def encode(message):
     result = ""
     for char in message:
-        if char.isalpha():
-            if char.isupper():
-                result += chr((ord(char.lower()) - 97 + 3) % 26 + 65)
-            elif char.islower():
-                result += chr((ord(char) - 97 + 2) % 26 + 97)
-        else:
-            if char == " ":  
-                result += " "
-            else:  
+        if char.isalnum():
+            if char.isalpha():
+                ascii_offset = 97 if char.islower() else 65
+                result += chr((ord(char) - ascii_offset + 3) % 26 + ascii_offset)
+            else: 
                 result += char
+        else:
+            result += char
     return result
