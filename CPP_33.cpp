011d59bd083e9aa3b418a4eff7ed26bb5f7ab0db
beep boop <githubbot@vadim.me>
2024-07-24 08:02:07 +0200

    Solution 16 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/w8xozsn6 Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 19eccd17-807c-47dc-af86-23ad05ab79e8.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 19eccd17-807c-47dc-af86-23ad05ab79e8.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~ In file included from 19eccd17-807c-47dc-af86-23ad05ab79e8.cpp:17: 19eccd17-807c-47dc-af86-23ad05ab79e8.cpp: In function ‘int main()’: 19eccd17-807c-47dc-af86-23ad05ab79e8.cpp:27:20: error: ‘sort_third’ was not decl ared in this scope    27 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 19eccd17-807c-47dc-af86-23ad05ab79e8.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
@@ -8,14 +7,12 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 std::vector<int> sort_third(std::vector<int> l) {
-    for (size_t i = 2; i < l.size(); i += 3) {
-        std::sort(l.begin() + i - 2, l.begin() + i + 1);
+    for (int i = 2; i < l.size(); i += 3) {
+        std::sort(l.begin() + i, l.begin() + i + 1);
     }
     return l;
 }
 
 int main() {
     assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    std::cout << "Test Passed!" << std::endl;
-    return 0;
 }
