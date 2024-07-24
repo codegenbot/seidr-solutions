a727b3f3463ce34163235f666272380aa495bd84
beep boop <githubbot@vadim.me>
2024-07-24 07:53:45 +0200

    Solution 9 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/puryu2f2 Prompt: 90d0eab4-3b6c-47f1-be5e-99de84d5a3c7.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "X L", "X", "IX", "V", "IV", "I"};       | ^~~~~~ 90d0eab4-3b6c-47f1-be5e-99de84d5a3c7.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 90d0eab4-3b6c-47f1-be5e-99de84d5a3c7.cpp:15: 90d0eab4-3b6c-47f1-be5e-99de84d5a3c7.cpp: In function ‘int main()’: 90d0eab4-3b6c-47f1-be5e-99de84d5a3c7.cpp:17:13: error: ‘int_to_mini_romank’ was not declared in this scope    17 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,9 +1,9 @@
 ```cpp
+#include <iostream>
 #include <vector>
-#include <string>
-
 using namespace std;
-vector<string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+std::vector<std::string> romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
 std::string int_to_mini_romank(int number) {
@@ -19,8 +19,8 @@ std::string int_to_mini_romank(int number) {
 
 int main() {
     int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    cout << "Roman numeral for " << num << ": " << int_to_mini_romank(num) << endl;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::cout << "The mini Roman representation of the number is: " << int_to_mini_romank(num) << endl;
     return 0;
 }
