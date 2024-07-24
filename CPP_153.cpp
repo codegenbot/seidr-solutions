128f30d975236be44bea917810c23a974481bb96
beep boop <githubbot@vadim.me>
2024-07-24 15:11:37 +0200

    Solution 5 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vylqhex4 Prompt: 1fee65bb-eb8f-4f07-9cb1-c1660c919840.cpp: In function ‘int Strongest_Extension(s td::string, std::vector<std::__cxx11::basic_string<char> >)’: 1fee65bb-eb8f-4f07-9cb1-c1660c919840.cpp:22:29: error: cannot convert ‘std::__cx x11::basic_string<char>’ to ‘int’ in return    22 |     return class_name + "." + strongest_extension;       |            ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                             |       |                             std::__cxx11::basic_string<char> In file included from 1fee65bb-eb8f-4f07-9cb1-c1660c919840.cpp:25: 1fee65bb-eb8f-4f07-9cb1-c1660c919840.cpp: In function ‘int main()’: 1fee65bb-eb8f-4f07-9cb1-c1660c919840.cpp:27:57: error: ISO C++ forbids compariso n between pointer and integer [-fpermissive]    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
 int Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int max_strength = 0;
     std::string strongest_extension;
@@ -13,16 +9,11 @@ int Strongest_Extension(std::string class_name, std::vector<std::string> extensi
             else if(islower(c)) sm++;
         }
         int strength = cap - sm;
-        if(strength > max_strength || (strength == max_strength && extension < strongest_extension)){
+        if(strength > max_strength || (strength == max_strength && strongest_extension.compare(extension) <= 0)){
             max_strength = strength;
             strongest_extension = extension;
         }
     }
 
-    return class_name + "." + strongest_extension;
-}
-
-int main() {
-    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    return 0;
+    return class_name + "." + strongest_extension + std::to_string(max_strength);
 }
