035dd18b0aacd9b33353c84d9e755467c1cc3e79
beep boop <githubbot@vadim.me>
2024-07-24 11:15:07 +0200

    Solution 60 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zu0hhoe8 Prompt:       |             ^~~~~~ ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp:28:36: error: expected primary-expressi on before ‘float’    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp:28:43: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<float> a,vector<float>b){       |                                           ^ ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp:27: ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp: In function ‘int main()’: ee77f114-35f9-48a5-a6b1-beaa732d1a2d.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 
 bool is_same(const std::vector<float>& a, const std::vector<float>& b) {
-    return a.size() == b.size() && std::equal(a, a.end(), b.begin());
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
 std::vector<float> derivative(const std::vector<float>& xs) {
