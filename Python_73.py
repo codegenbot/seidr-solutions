bcbdad22ea17b34bb4d98c9aef643c26711366bf
beep boop <githubbot@vadim.me>
2024-07-24 12:56:07 +0200

    Solution 26 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/evzpu1rl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5c6ff14c-f286-4fa9-b8f2-af86ec5ca 79b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,7 +1,17 @@
 ```
-def find_difference(p, q):
+def smallest_change(arr):
+    if isinstance(arr[0], list):  
+        n = len(arr[0])
+        arr_str = ''.join([str(x) for x in arr[0] + arr[1]])
+    else:  
+        n = 2
+        arr_str = str(arr[0]) + str(arr[1])
+
+    rev_arr_str = arr_str[::-1]
     diff_count = 0
-    for i in range(len(str(p))):
-        if str(p)[i] != str(q)[len(str(q))-i-1]:
+
+    for i in range(n//2):
+        if arr_str[i] != rev_arr_str[n-i-1]:
             diff_count += 1
+
     return diff_count
