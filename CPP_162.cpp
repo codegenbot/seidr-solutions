40448b7465c0f7358633b9ee70fe538564d4a1f6
beep boop <githubbot@vadim.me>
2024-07-24 15:52:23 +0200

    Solution 3 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 5340f4a1-0038-435e-900f-6db88acf6b29.cpp:4: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ 5340f4a1-0038-435e-900f-6db88acf6b29.cpp:17:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    17 |     MD5_Final(md, &ctx);       |     ~~~~~~~~~^~~~~~~~~~ In file included from 5340f4a1-0038-435e-900f-6db88acf6b29.cpp:4: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccteCE2b.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': 5340f4a1-0038-435e-900f-6db88acf6b29.cpp:(.text+0x9d): undefined reference to `M D5_Init' /usr/bin/ld: 5340f4a1-0038-435e-900f-6db88acf6b29.cpp:(.text+0xd6): undefined re ference to `MD5_Update' /usr/bin/ld: 5340f4a1-0038-435e-900f-6db88acf6b29.cpp:(.text+0xec): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,23 +1,22 @@
 #include <string>
 #include <sstream>
-#include <openssl/md5.h>
+#include <iomanip>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    MD5_CTX ctx;
     unsigned char md[16];
     stringstream ss;
 
+    MD5_CTX ctx;
     MD5_Init(&ctx);
     MD5_Update(&ctx, text.c_str(), text.size());
     MD5_Final(md, &ctx);
 
     for (int i = 0; i < 16; ++i) {
-        sprintf(ss.str().c_str(), "%02x", md[i]);
-        ss.seekp(0);
+        ss << setfill('0') << setw(2) << hex << (int)md[i];
     }
 
     return ss.str();
