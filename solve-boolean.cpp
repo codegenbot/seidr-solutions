d7f9dd0eb865c19fd37002b3da74cc6891d6ed1e
beep boop <githubbot@vadim.me>
2024-07-24 08:40:12 +0200

    Solution 7 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/sysiwsqx Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,16 +1,38 @@
-bool solveBoolean(std::string s) {
-    bool result = true;
+```cpp
+#include <string>
+#include <vector>
+
+bool solveBoolean(const std::string& s) {
+    if (s.empty()) return true;
+
+    std::vector<std::string> tokens;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == '|') {
-            result = !result;
+            tokens.push_back(s.substr(0, i));
+            s = s.substr(i + 1);
+            i = -1;
         } else if (s[i] == '&') {
             while (i + 1 < s.length() && s[i+1] == '&') i++;
-            if (i + 1 < s.length() && s[i+1] == 't') result = true;
-            else if (i + 1 < s.length()) result = false;
-        } else if (s[i] == 'f' || s[i] == 't') {
-            if (result) return s[i] == 't';
-            else return s[i] == 'f';
+            if (i + 1 < s.length()) {
+                tokens.push_back(s.substr(0, i + 1));
+                s = s.substr(i + 1);
+                i = -1;
+            }
         }
     }
-    return result;
+    tokens.push_back(s);
+
+    for (int i = 0; i < tokens.size(); i++) {
+        if (tokens[i] == "t") return true;
+        else if (tokens[i] == "f") return false;
+    }
+
+    bool left = solveBoolean(tokens[0]);
+    bool right = solveBoolean(tokens[1]);
+
+    if (tokens[0].find('|') != std::string::npos) {
+        return left || right;
+    } else {
+        return left && right;
+    }
 }
