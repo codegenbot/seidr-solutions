4a134008c4337a1966ad8dd75d36622631cdd9d8
beep boop <githubbot@vadim.me>
2024-07-24 22:28:51 +0200

    Solution 24 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z78os2pt Prompt: 567aa5f3-07a1-461c-b407-7e407b124799.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -6,38 +6,35 @@
 using namespace std;
 
 any compare_one(any a, any b) {
-    if (holds_alternative<int, double>(a) && holds_alternative<double>(b)) {
+    if (holds_alternative<int, float>(a) && holds_alternative<float, int>(b)) {
         return b;
     }
-    else if (holds_alternative<double>(a) && holds_alternative<int, double>(b)) {
-        return max(a, b);
-    }
     else if (holds_alternative<string>(a) && holds_alternative<string>(b)) {
-        string sa = any_cast<string>(a);
-        string sb = any_cast<string>(b);
-        if (stod(sa) > stod(sb))
+        string sa = get<string>(a);
+        string sb = get<string>(b);
+        if (stof(sa) > stof(sb))
             return a;
-        else if (stod(sa) < stod(sb))
+        else if (stof(sa) < stof(sb))
             return b;
         else
             return "None";
     }
-    else if (holds_alternative<string>(a) && holds_alternative<int, double>(b)) {
-        string sa = any_cast<string>(a);
-        double sb = any_cast<double>(b);
-        if (stod(sa) > sb)
+    else if (holds_alternative<string>(a) && holds_alternative<int, float>(b)) {
+        string sa = get<string>(a);
+        double sb = get<double>(b);
+        if (stof(sa) > sb)
             return a;
-        else if (stod(sa) < sb)
+        else if (stof(sa) < sb)
             return b;
         else
             return "None";
     }
-    else if (holds_alternative<int, double>(a) && holds_alternationg<string>(b)) {
-        double sa = any_cast<double>(a);
-        string sb = any_cast<string>(b);
-        if (sa > stod(sb))
+    else if (holds_alternative<int, float>(a) && holds_alternation<string>(b)) {
+        double sa = get<double>(a);
+        string sb = get<string>(b);
+        if (sa > stof(sb))
             return a;
-        else if (sa < stod(sb))
+        else if (sa < stof(sb))
             return b;
         else
             return "None";
