9b395772da7cd211b60d96bf32426c160fd95fe1
beep boop <githubbot@vadim.me>
2024-07-24 05:31:59 +0200

    Solution 14 of Python/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fqpdhp0g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b34f1735-3d0f-4663-b99f-e3f138936 ef0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_133.py
+++ Python_133.py
@@ -1,10 +1,11 @@
+```
 def sum_squares(lst):
     return round(sum([round(int(x))**2 for x in lst]))
 
 while True:
     numbers = input("Enter numbers separated by space: ").split()
     try:
-        total_sum_of_squares = sum_squares(map(str, numbers))
+        total_sum_of_squares = sum_squares(numbers)
         break
     except ValueError:
         print("Invalid input. Please enter valid numbers separated by space:")
