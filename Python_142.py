b9d2e58bf1805b94b187f380d6beae0ed857b032
beep boop <githubbot@vadim.me>
2024-07-24 05:42:00 +0200

    Solution 42 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt: 121 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1bd90641-0d81-4a2a-ba1d-c58fb658e 5a9.py", line 40, in <module>     check(sum_squares)           ^^^^^^^^^^^ NameError: name 'sum_squares' is not defined. Did you mean: 'sum_of_squares'?

--- Python_142.py
+++ Python_142.py
@@ -2,13 +2,5 @@
 def sum_of_squares(n):
     return n ** 2
 
-def check(x):
-    if x > 100:
-        return "Greater than 100"
-    elif x == 100:
-        return "Equal to 100"
-    else:
-        return "Less than or equal to 99"
-
 result = sum_of_squares(11)
-print(check(result))
+print(result)
