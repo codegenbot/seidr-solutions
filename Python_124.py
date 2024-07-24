8a1de775f567d2d17a133cdf00a6033820b5e094
beep boop <githubbot@vadim.me>
2024-07-24 04:29:53 +0200

    Solution 13 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tbflllxq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/002f5258-e009-4d47-ae65-e50ddf9f0 2a5.py", line 10, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/002f5258-e009-4d47-ae65-e50ddf9f0 2a5.py", line 7, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/002f5258-e009-4d47-ae65-e50ddf9f0 2a5.py", line 3, in valid_date     year = int(date.split("-")[2])                ~~~~~~~~~~~~~~~^^^ IndexError: list index out of range

--- Python_124.py
+++ Python_124.py
@@ -1,8 +1,4 @@
-```
 def valid_date(date):
-    day, month, year_str = map(int, date.split("-")[:2])
-    if len(date.split("-")) == 3:
-        day, month, year = map(int, date.split("-"))
-        if day > 0 and day <= 31 and (month >=1 and month <=12) and (year % 4 == 0 or (year % 100 != 0 and year % 400 == 0)):
-            return True
-    return False
+    parts = date.split("-")
+    day, month = map(int, parts[:2])
+    year = int(parts[-1]) if len(parts) > 2 else None
