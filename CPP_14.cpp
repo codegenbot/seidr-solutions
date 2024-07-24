52bfc97d693251c257d2f8a8e74c9fcb55cba83e
beep boop <githubbot@vadim.me>
2024-07-24 15:41:28 +0200

    Solution 32 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/5vpxt1px Prompt:       |                              ^~~~~~ b35350a6-7380-48d0-8292-1ef19151cf9e.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b35350a6-7380-48d0-8292-1ef19151cf9e.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b35350a6-7380-48d0-8292-1ef19151cf9e.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b35350a6-7380-48d0-8292-1ef19151cf9e.cpp:11: b35350a6-7380-48d0-8292-1ef19151cf9e.cpp: In function ‘int main()’: b35350a6-7380-48d0-8292-1ef19151cf9e.cpp:21:20: error: ‘all_prefixes’ was not de clared in this scope    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ b35350a6-7380-48d0-8292-1ef19151cf9e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,21 +1,11 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <cassert>
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b){
-    return a == b;
-}
-
-vector<string> all_prefixes(string str);
-
-int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    return 0;
-}
-
-vector<string> all_prefixes(string str) {
+vector<string> all_prefixes(string str){
     vector<string> result;
     string prefix = "";
     for (char c : str) {
@@ -23,4 +13,14 @@ vector<string> all_prefixes(string str) {
         result.push_back(prefix);
     }
     return result;
+}
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+int main(){
+    assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));
+    
+    return 0;
 }
