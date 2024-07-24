e70b98e4446cb843a21bf446410557ffcd9a3b16
beep boop <githubbot@vadim.me>
2024-07-24 05:14:34 +0200

    Solution 20 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9e6afcfc-dd8a-45d1-8fd0-ac7dfd3ce 2e2.py", line 1, in <module>     print(check(sum_squares([1, 2, 3])))           ^^^^^ NameError: name 'check' is not defined

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,12 @@
-def check(x):
-    return "yes" if x > 10 else "no"
-
-
 def sum_squares(lst):
-    return sum(i**2 for i in lst)
+    return sum([i**2 for i in lst])
+
+
+def check(num):
+    if num % 3 == 0:
+        return "SolveMeFirst"
+    else:
+        return "NotHuge"
 
 
 print(check(sum_squares([1, 2, 3])))
