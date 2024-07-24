8673b911e0526cd2c9fca2c190adfdfddb3f98f0
beep boop <githubbot@vadim.me>
2024-07-24 21:15:01 +0200

    Solution 80 of CPP/78, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/t1vtktn0 Prompt: b7981add-43a4-4cfd-824f-efac64387ac7.cpp:1:13: error: ‘string’ was not declared in this scope     1 | int hex_key(string num) {       |             ^~~~~~ In file included from b7981add-43a4-4cfd-824f-efac64387ac7.cpp:10: b7981add-43a4-4cfd-824f-efac64387ac7.cpp: In function ‘int main()’: b7981add-43a4-4cfd-824f-efac64387ac7.cpp:12:20: error: ‘hex_key’ cannot be used as a function    12 |     assert (hex_key("") == 0);       |             ~~~~~~~^~~~

--- CPP_78.cpp
+++ CPP_78.cpp
@@ -1,13 +1,10 @@
 #include <string>
 
-using namespace std;
-
-int hex_key(string num) {
+int hex_key(std::string num) {
     int count = 0;
     for (char c : num) {
-        if ((c >= '2' && c <= '7') || (c == 'B' || c == 'C' || c == 'D' || c == 'F')) {
-            count++;
-        }
+        if (c >= '2' && c <= '7') count++;
+        else if (c == 'B' || c == 'D' || c == 'F') count++;
     }
     return count;
 }
