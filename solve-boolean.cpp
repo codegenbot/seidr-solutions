e947168d6884c5e707a2853e02568346dbb72db8
beep boop <githubbot@vadim.me>
2024-07-24 15:42:12 +0200

    Solution 46 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/sysiwsqx Prompt: 606abf7b-ec02-4b45-aeff-4c45624524c1.cpp: In function ‘bool solveBoolean(std::st ring)’: 606abf7b-ec02-4b45-aeff-4c45624524c1.cpp:4:53: error: ‘find_first_of’ is not a m ember of ‘std’     4 |     size_t left_end = std::distance(s.begin(), std::find_first_of(s, "TF "));       |                                                     ^~~~~~~~~~~~~ 606abf7b-ec02-4b45-aeff-4c45624524c1.cpp:10:58: error: ‘find_first_of’ is not a member of ‘std’    10 |         size_t right_end = std::distance(s.begin(), std::find_first_of(s , "TF"));       |                                                          ^~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,11 +1,11 @@
 bool solveBoolean(std::string s) {
     size_t left_end = s.find_first_of("TF");
     if (left_end == std::string::npos) {
-        return s[0] == 'T';
+        return false;
     } else {
         bool left = s.substr(0, left_end).back() == 'T';
-        size_t right_start = (left_end == std::string::npos) ? 0 : left_end + 1;
-        size_t right_end = s.find_first_of("TF", right_start);
+        size_t right_start = left_end;
+        size_t right_end = s.find_first_of("TF");
         if (right_end == std::string::npos) {
             return s[0] == 'T' ? !left : left;
         } else {
