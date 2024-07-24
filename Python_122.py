6979ca3e3c435dd12c990617ca4739180e748b86
beep boop <githubbot@vadim.me>
2024-07-24 14:41:09 +0200

    Solution 2 of Python/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/gthuw6rh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54f27319-f656-4647-a1e8-fceb8e61d 5d5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_122.py
+++ Python_122.py
@@ -1,3 +1,10 @@
-```
-def add_elements(arr, k):
-    return sum(i for i in arr[:k] if 10 > abs(i) >= 1)
+def add_elements():
+    n = int(input("Enter number of elements: "))
+    arr = [int(input(f"Enter element {i+1}: ")) for i in range(n)]
+    k = int(input("Enter the value to split array: "))
+
+    sum_of_positive_and_negative_integers = sum(x for x in arr[:k] if 10 > abs(x) >= 1)
+
+    print(
+        f"The sum of all positive and negative integers less than or equal to 10 from the first {k} elements is: {sum_of_positive_and_negative_integers}."
+    )
