2c3a92631f28c63c40cb4a34e7a3f4f35ae43b7b
beep boop <githubbot@vadim.me>
2024-07-24 11:56:36 +0200

    Solution 87 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vhuqvhyo Prompt:     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c55e779d-d23c-4017-92e5-4464d503d2ae.cpp:9:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c55e779d-d23c-4017-92e5-4464d503d2ae.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c55e779d-d23c-4017-92e5-4464d503d2ae.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c55e779d-d23c-4017-92e5-4464d503d2ae.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from c55e779d-d23c-4017-92e5-4464d503d2ae.cpp:8: c55e779d-d23c-4017-92e5-4464d503d2ae.cpp: In function ‘int main()’: c55e779d-d23c-4017-92e5-4464d503d2ae.cpp:18:20: error: ‘rolling_max’ was not dec lared in this scope    18 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -4,3 +4,8 @@
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
+
+int main() {
+    issame({3, 2, 3, 100, 3}, {3, 3, 3, 100, 100});
+    return 0;
+}
