0b4040d5ae0a03c7761e784c7dd24d0e49c962b1
beep boop <githubbot@vadim.me>
2024-07-24 16:36:48 +0200

    Solution 68 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6zjxuet3 Prompt: dd104334-62fd-4de8-bc9c-0df0b6e9cdb0.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ dd104334-62fd-4de8-bc9c-0df0b6e9cdb0.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -11,14 +11,15 @@ bool check_if_last_char_is_a_letter(const std::string& str) {
 }
 
 int main() {
-    std::cout << "Enter a string: ";
-    std::string line; getline(std::cin, line); 
-    size_t pos = std::distance(line.begin(), std::find_if_not(line.begin(), line.end(), [](char c){return std::isspace(c);}));
-    if (pos == line.size())
-        line.clear();
+    assert(check_if_last_char_is_a_letter("apple pi e ") == true); 
+    std::cout << "Check if the last character of a string is a letter: ";
+    std::string line; getline(std::cin, line); std::string input = line;
+    size_t pos = std::distance(input.begin(), std::find_if_not(input.begin(), input.end(), [](char c){return std::isspace(c);}));
+    if (pos == input.size())
+        input.clear();
     else
-        line.erase(pos, 1);
-    if (check_if_last_char_is_a_letter(line))
+        input.erase(pos, 1);
+    if (check_if_last_char_is_a_letter(input))
         std::cout << "The last character of the input string is a letter.\n";
     else
         std::cout << "The last character of the input string is not a letter.\n";
