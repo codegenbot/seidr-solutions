7884278a2040f0b9bb4f2b611c19f67bdf0d9f19
beep boop <githubbot@vadim.me>
2024-07-24 11:22:19 +0200

    Solution 92 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ueww3qod Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2b9513c6-dc69-43e3-8cbe-726e9f36e c76.py", line 5     num_dict.get(num, int(num)) for num in numbers.split()])                                                           ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '(' on l ine 4

--- Python_19.py
+++ Python_19.py
@@ -1,5 +1,5 @@
 def sort_numbers(numbers: str) -> str:
     numbers = numbers.replace("-", " ")
     numbers = " ".join(filter(None, numbers.split()))
-    sorted_numbers = sorted([(10 * (num == "twenty" and 2 or (num == "ten" and 1 or 0)) + num_dict.get(num, int(num))) for num in numbers.split()])
+    sorted_numbers = sorted([10 * (num == "twenty" and 2 or num == "ten" and 1 or 0) + num_dict.get(num, int(num)) for num in numbers.split()])
     return " ".join(map(str, sorted_numbers))
