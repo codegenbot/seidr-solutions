f18c4ba2e4d2456d9881d23e0c985cecaf19d35f
beep boop <githubbot@vadim.me>
2024-07-24 18:59:45 +0200

    Solution 8 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/lopngezl Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -4,35 +4,55 @@
 
 using namespace std;
 
-bool solveBoolean(string s) {
-    int i = 0;
-    while (i < s.length()) {
-        if (s[i] == 't') return true;
-        else if (s[i] == 'f') return false;
-        else if (s[i] == '|') {
-            i++;
-            bool first = solveBoolean(s.substr(i));
-            i += s.find('&', i) - 1;
-            bool second = solveBoolean(s.substr(i));
-            return first || second;
+bool evaluateBooleanExpression(string expression) {
+    stack<char> operatorStack;
+    stack<string> valueStack;
+
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '&' || expression[i] == '|') {
+            while (!operatorStack.empty() && operatorStack.top() != '(') {
+                char op = operatorStack.top();
+                operatorStack.pop();
+                string right = valueStack.top();
+                valueStack.pop();
+                string left = valueStack.top();
+                valueStack.pop();
+
+                if (op == '&') {
+                    valueStack.push(to_string((left == "True" && right == "True") ? "True" : "False"));
+                } else {
+                    valueStack.push(to_string((left == "True" || right == "True") ? "True" : "False"));
+                }
+            }
+            operatorStack.pop(); // pop the '('
+        } else if (expression[i] == '(') {
+            operatorStack.push(expression[i]);
+        } else if (expression[i] == 'T' || expression[i] == 'F') {
+            string value = "";
+            while (i < expression.length() && (expression[i] == 'T' || expression[i] == 'F')) {
+                value += expression[i];
+                i++;
+            }
+            i--; // backtrack
+            valueStack.push(value);
         }
-        else if (s[i] == '&') {
-            i++;
-            bool first = solveBoolean(s.substr(i));
-            i += s.find('|', i) - 1;
-            bool second = solveBoolean(s.substr(i));
-            return first && second;
-        }
-        i++;
     }
-    return false; // default to False if no T or F found
+
+    return valueStack.top() == "True";
 }
 
 int main() {
     string input;
-    cout << "Enter a Boolean expression (T/F/|/&): ";
+    cout << "Enter a Boolean expression: ";
     cin >> input;
-    bool output = solveBoolean(input);
-    cout << (output ? "True" : "False") << endl;
+
+    bool result = evaluateBooleanExpression(input);
+
+    if (result) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+
     return 0;
 }
