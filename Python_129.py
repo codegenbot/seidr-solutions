0d2bc3f15745fb00ebbdec9d335201a2b6862406
beep boop <githubbot@vadim.me>
2024-07-25 01:37:24 +0200

    Solution 31 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/b21j1a4k Prompt: Program did not receive expected input

--- Python_129.py
+++ Python_129.py
@@ -1,4 +1,3 @@
-```
 def minPath(grid, k):
     N = len(grid)
     values = [[i * j for j in range(1, N + 1)] for i in range(1, N + 1)]
@@ -25,7 +24,6 @@ def minPath(grid, k):
                     break
 
     return path
-
 
 grid = []
 k = 0
