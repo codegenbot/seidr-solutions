a9224bf9d2d6a5e8bd2cc5b069df8ad263844bcf
beep boop <githubbot@vadim.me>
2024-07-24 05:51:34 +0200

    Solution 33 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/62y31j2y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56cc407d-66f0-40b1-9b77-cda1fabb9 31c.py", line 9, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/56cc407d-66f0-40b1-9b77-cda1fabb9 31c.py", line 6, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,7 +1,2 @@
 def cycpattern_check(a, b):
-    str_a = str(a)
-    str_b = str(b)
-    if len(str_a) % len(str_b) != 0:
-        return False
-    check_str = str_b * (len(str_a) // len(str_b)) + str_b[:len(str_a) % len(str_b)]
-    return check_str == str_a
+    return str(b) * (len(str(a)) // len(str(b))) + str(b)[:len(str(a)) % len(str(b))] == str(a)
