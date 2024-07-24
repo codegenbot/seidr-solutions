7e43a7fa1c506d8fd743a9bd8ce6ca6a7ab1590e
beep boop <githubbot@vadim.me>
2024-07-24 16:25:05 +0200

    Solution 32 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/ixs2ihlg Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { 0bf0ee02-4971-4915-b6a5-422e761ccbca.cpp:24:13: error: ‘abs’ was not declared in  this scope    24 |         if (abs(left_sum - right_sum) < diff) {       |             ^~~ 0bf0ee02-4971-4915-b6a5-422e761ccbca.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << nums[i] << endl;       |         ^~~~ 0bf0ee02-4971-4915-b6a5-422e761ccbca.cpp:31:28: error: ‘endl’ was not declared i n this scope    31 |         cout << nums[i] << endl;       |                            ^~~~ 0bf0ee02-4971-4915-b6a5-422e761ccbca.cpp:34:5: error: ‘cout’ was not declared in  this scope    34 |     cout << "0" << endl;       |     ^~~~ 0bf0ee02-4971-4915-b6a5-422e761ccbca.cpp:34:20: error: ‘endl’ was not declared i n this scope    34 |     cout << "0" << endl;       |                    ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,48 +1,49 @@
+#include <climits>
 #include <iostream>
 #include <vector>
-#include <climits>
+
 using namespace std;
 
 int main() {
     vector<int> nums;
     int num;
-
+    
     while (cin >> num) {
         nums.push_back(num);
     }
-
+    
     int n = nums.size();
     int diff = INT_MAX;
     int idx = -1;
-
+    
     for (int i = 1; i < n; ++i) {
         int left_sum = 0, right_sum = 0;
-
+        
         for (int j = 0; j < i; ++j) {
             left_sum += nums[j];
         }
-
+        
         for (int j = i; j < n; ++j) {
             right_sum += nums[j];
         }
-
-        if (std::abs(left_sum - right_sum) < diff) {
-            diff = std::abs(left_sum - right_sum);
+        
+        if (abs(left_sum - right_sum) < diff) {
+            diff = abs(left_sum - right_sum);
             idx = i;
         }
     }
-
+    
     for (int i = 0; i < idx; ++i) {
-        cout << nums[i] << "\n";
+        cout << nums[i] << endl;
     }
-
-    cout << "0\n";
-
+    
+    cout << "0" << endl;
+    
     for (int i = idx; i < n; ++i) {
-        cout << nums[i] << "\n";
+        cout << nums[i] << endl;
     }
-
-    cout << "0\n";
-
+    
+    cout << "0" << endl;
+    
     return 0;
 }
