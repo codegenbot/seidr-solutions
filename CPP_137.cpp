06018d95df6d4127d547f9973150e19e4430806c
beep boop <githubbot@vadim.me>
2024-07-24 03:03:29 +0200

    Solution 33 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt: c97439e4-92eb-4c4c-b637-cb7c11898c5f.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,38 +1,28 @@
 #include <iostream>
 #include <string>
 #include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (boost::any_cast<int>(a) > static_cast<float>(boost::any_cast<double>(b))) ? a : b;
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return boost::any((int) a > (int) b ? a : b);
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (static_cast<float>(boost::any_cast<double>(a)) > boost::any_cast<int>(b)) ? a : b;
+        return boost::any((float) a > (int) b ? a : b);
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return boost::any((int) a > (float) b ? a : b);
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        return boost::any((float) a > (float) b ? a : b);
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        return str1.compare(str2) > 0 ? a : b;
+        return boost::any((string) a > (string) b ? a : b);
     }
     else {
         return "None";
     }
-}
-
-int main() {
-    // test the function
-    boost::any a = 5; // int
-    boost::any b = 3.5; // float
-    boost::any result = compare_one(a, b);
-    
-    if (result.type() == typeid(string)) {
-        cout << "Result: " << boost::any_cast<string>(result) << endl;
-    }
-    else {
-        cout << "Result: " << boost::any_cast<int>(result) << endl;
-    }
-    return 0;
 }
