8edef37fe4b56785768dda0d3f88b690ee6371f1
beep boop <githubbot@vadim.me>
2024-07-24 23:52:26 +0200

    Solution 10 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/eq7jsb05 Prompt: Complete the following code given the task description and function signature.

--- Python_73.py
+++ Python_73.py
@@ -1,11 +1,2 @@
 def smallest_change(arr):
-    n = len(arr)
-    forward = arr.copy()
-    backward = arr.copy()[::-1]
-
-    changes = 0
-    for i in range(n):
-        if forward[i] != backward[n - i - 1]:
-            changes += 1
-
-    return changes
+    return sum(1 for a, b in zip(arr, reversed(arr)) if a != b)
