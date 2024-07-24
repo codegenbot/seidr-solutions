15f6f137f121a2274a9a57bd7a69774a74af8c27
beep boop <githubbot@vadim.me>
2024-07-24 20:04:25 +0200

    Solution 65 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d8blgpbm Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b48be66-8be8-4e92-9162-0069b62c3 af0.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b48be66-8be8-4e92-9162-0069b62c3 af0.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,4 @@
 def sort_third(l: list):
-    return [sorted(l[i:i+3]) for i in range(0, len(l), 3) if i + 2 < len(l)] + [sorted(l[len(l) - (len(l) % 3):]]
-
-# Example usage
-print(sort_third([5, 6, 3, 4, 8, 9, 2, 1]))
+    return [sorted(l[i : i + 3]) for i in range(0, len(l), 3) if i + 2 < len(l)] + [
+        sorted(l[len(l) - (len(l) % 3) :])
+    ]
