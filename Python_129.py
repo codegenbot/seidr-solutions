fc6dac8177292a014d9a4857405fa60220904a65
beep boop <githubbot@vadim.me>
2024-07-24 20:54:51 +0200

    Solution 63 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ba7grvne Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0a8a3948-4812-45da-b26f-7134b970c 373.py", line 20     if len(p[:k]) == k and (not min_path or sum(grid[i][j] for i, j in zip(*[[(i //n,j//n) for i, j in enumerate(path)])]) < sum(grid[i][j] for i, j in zip(*[[(i //n,j//n) for i, j in enumerate(min_path)]]))):                                       ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- Python_129.py
+++ Python_129.py
@@ -18,3 +18,4 @@ def minPath(grid, k):
     for p in res:
         if len(p[:k]) == k and (not min_path or sum(grid[i][j] for i, j in zip(*[[(i//n,j//n) for i, j in enumerate(path)] ])) < sum(grid[i][j] for i, j in zip(*[[(i//n,j//n) for i, j in enumerate(min_path)]]))):
             min_path = p
+    return min_path
