03fb5c1afd23c116b19d00b49697192ea9e3035b
beep boop <githubbot@vadim.me>
2024-07-24 17:11:12 +0200

    Solution 43 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rec3ih7w Prompt: 5bbc130a-c528-4b3a-913e-244e05d565a0.cpp:1:29: error: ‘expressions’ was not decl ared in this scope     1 | double output = do_algebra({expressions[0], expressions[1], "42"});       |                             ^~~~~~~~~~~ 5bbc130a-c528-4b3a-913e-244e05d565a0.cpp:1:45: error: ‘expressions’ was not decl ared in this scope     1 | double output = do_algebra({expressions[0], expressions[1], "42"});       |                                             ^~~~~~~~~~~ 5bbc130a-c528-4b3a-913e-244e05d565a0.cpp:1:17: error: ‘do_algebra’ was not decla red in this scope     1 | double output = do_algebra({expressions[0], expressions[1], "42"});       |                 ^~~~~~~~~~ In file included from 5bbc130a-c528-4b3a-913e-244e05d565a0.cpp:3: 5bbc130a-c528-4b3a-913e-244e05d565a0.cpp: In function ‘int main()’: 5bbc130a-c528-4b3a-913e-244e05d565a0.cpp:5:13: error: ‘do_algebra’ was not decla red in this scope     5 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ^~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,95 +1,25 @@
+```cpp
 #include <iostream>
-#include <cmath>
 #include <vector>
-#include <string>
 
-using namespace std;
-
-vector<string> splitOperator(vector<string>& expressions) {
-    vector<string> operators;
-    string temp;
-
-    for (int i = 0; i < expressions.size(); i++) {
-        if (expressions[i] == "+-*/" || expressions[i] == "42" || expressions[i] == "47") {
-            if (!temp.empty()) {
-                operators.push_back(temp);
-                temp = "";
-            }
-            operators.push_back(expressions[i]);
-        } else {
-            temp += expressions[i];
+std::vector<std::string> expressions;
+double do_algebra(const std::vector<std::string>& ops, const std::vector<int>& args) {
+    double result = args[2];
+    
+    for(int i = 0; i < ops.size(); i++) {
+        if(ops[i] == "//") {
+            result /= args[i+1];
+        } else if(ops[i] == "*") {
+            result *= args[i+1];
         }
     }
-    if (!temp.empty()) {
-        operators.push_back(temp);
-    }
-
-    return operators;
-}
-
-vector<string> splitExpressions(vector<string>& input) {
-    vector<string> expressions;
-
-    for (int i = 0; i < input.size(); i++) {
-        if (input[i] == "42" || input[i] == "47") {
-            continue;
-        }
-        expressions.push_back(input[i]);
-    }
-
-    return expressions;
-}
-
-double do_algebra(vector<string> expressions) {
-    double result = 0.0;
-
-    vector<string> op = splitOperator(expressions);
-
-    if (stod(op[2]) == 42) {
-        if (expressions[1] == "0") {
-            if (op[0][0] == '+') {
-                result = pow(stod(expressions[0]), stod(expressions[1]));
-            } else if (op[0][0] == '-') {
-                result = pow(stod(expressions[0]), -stod(expressions[1]));
-            }
-        } else if (expressions[1] != "0") {
-            if (op[0][0] == '+') {
-                result = stod(expressions[0]) + stod(expressions[1]);
-            } else if (op[0][0] == '-') {
-                result = stod(expressions[0]) - stod(expressions[1]);
-            }
-        }
-    } else if (stod(op[2]) == 47) {
-        if (expressions[1] == "0") {
-            if (op[0][0] == '+') {
-                result = -stod(expressions[0]);
-            } else if (op[0][0] == '-') {
-                result = stod(expressions[0]);
-            }
-        } else if (expressions[1] != "0") {
-            if (op[0][0] == '+') {
-                result = stod(expressions[0]) + (-stod(expressions[1]));
-            } else if (op[0][0] == '-') {
-                result = stod(expressions[0]) - stod(expressions[1]);
-            }
-        }
-    }
-
+    
     return result;
 }
 
 int main() {
-    vector<string> input;
-
-    cout << "Enter the expressions (space separated): ";
-    getline(cin, input);
-    if (!input.empty()) {
-        vector<string> expressions = splitExpressions(input);
-
-        double output = do_algebra(expressions);
-        cout << "Result: " << output << endl;
-        return 0; // Added return statement to indicate successful execution
-    }
-
+    expressions = {"//", "*"};
+    double output = do_algebra({expressions[0], expressions[1], 42});
+    std::cout << "Output: " << output << std::endl;
     return 0;
 }
