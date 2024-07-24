5ab429cbee25a9e34b292bd7aa40fb29b0c7baf7
beep boop <githubbot@vadim.me>
2024-07-24 03:52:18 +0200

    Solution 99 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3tlymlm5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c2fb5369-7b38-46b5-87b1-93f003d0e 938.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,10 @@
-```
 def sort_third(lst):
-    return tuple(sorted(lst, key=lambda x: x[2]))
+    return tuple(sorted(lst, key=lambda x: sum(x)))
+
 
 n = int(input("Enter number of elements: "))
-user_input = [list(map(int, input(f"Enter element {i+1} (space separated): ").split())) for i in range(n)]
+user_input = [
+    list(map(int, input(f"Enter element {i+1} (space separated): ").split()))
+    for i in range(n)
+]
 print(sort_third(tuple(user_input)))
