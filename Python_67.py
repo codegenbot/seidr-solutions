33afa94f190bcea4d474c5f90bce82789485e6cc
beep boop <githubbot@vadim.me>
2024-07-24 02:43:09 +0200

    Solution 15 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v4oavres Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3675a9ca-a313-43ba-a0a8-84835996a 272.py", line 18, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3675a9ca-a313-43ba-a0a8-84835996a 272.py", line 15, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3675a9ca-a313-43ba-a0a8-84835996a 272.py", line 11, in fruit_distribution     return int(((total_fruit - 1) / max_per_box) + 0.5)                 ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~ ZeroDivisionError: division by zero

--- Python_67.py
+++ Python_67.py
@@ -1,3 +1,4 @@
+```
 def fruit_distribution(s, n):
     s = s.replace("apples", "apple").replace("oranges", "orange")
     total_fruit = 0
@@ -8,4 +9,4 @@ def fruit_distribution(s, n):
     max_per_box = min(total_fruit // (n + 1), n)
     remaining_fruits = total_fruit - (max_per_box * (n + 1))
 
-    return int(((total_fruit - 1) / max_per_box) + 0.5)
+    return int(((total_fruit - 1) / max_per_box) + 0.5) if max_per_box != 0 else 0
