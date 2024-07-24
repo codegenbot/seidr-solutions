3d71fbdc890bb5640abc6fa9498411fbf7e565c5
beep boop <githubbot@vadim.me>
2024-07-24 07:22:04 +0200

    Solution 98 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c4e8a90-e925-4048-b6ac-b18dd5b72 2f5.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c4e8a90-e925-4048-b6ac-b18dd5b72 2f5.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c4e8a90-e925-4048-b6ac-b18dd5b72 2f5.py", line 2, in sorted_list_sum     if all(x.strip().isalpha().lower() for x in lst) and len(set(len(x) for x in  lst)) == 1:        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7c4e8a90-e925-4048-b6ac-b18dd5b72 2f5.py", line 2, in <genexpr>     if all(x.strip().isalpha().lower() for x in lst) and len(set(len(x) for x in  lst)) == 1:            ^^^^^^^^^^^^^^^^^^^^^^^^^ AttributeError: 'bool' object has no attribute 'lower'

--- Python_149.py
+++ Python_149.py
@@ -1,7 +1,7 @@
-```Python
 def sorted_list_sum(lst):
-    if all(x.strip().isalpha() for x in lst) and len(set(len(x.strip()) for x in lst)) == 1:
-        max_len = max(len(x.strip()) for x in lst)
-        return [''.join(sorted(i.ljust(max_len))) for i in set(''.join(sorted(i)) for i in lst)]
+    if all(isinstance(x, str) for x in lst) and len(set(len(x) for x in lst)) == 1:
+        return sum(
+            sorted(map(int, [i * int("0" * (len(max(lst)) - len(i))) for i in lst]))
+        )
     else:
         print("Invalid input. Please enter a list of strings.")
