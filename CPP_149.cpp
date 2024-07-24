6a2f445fe59e1ff7679681ea2dba32ece5f7b320
beep boop <githubbot@vadim.me>
2024-07-24 12:56:17 +0200

    Solution 40 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt: 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:28: error: ‘a’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                            ^ 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:30: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:37: error: ‘string’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:44: error: ‘b’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:5: 8831b01d-337d-4a1e-a309-816ba04faff7.cpp: In function ‘int main()’: 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:15:20: error: ‘sorted_list_sum’ was not  declared in this scope    15 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,11 +1,3 @@
-#include <vector>
-#include <string>
-#include <cassert>
-
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
-}
-
-int main() {
-    assert(issame({"aaaa", "bbbb", "dd", "cc"}, {"cc", "dd", "aaaa", "bbbb"}));
 }
