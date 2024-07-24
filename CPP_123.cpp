7844a5c73d1ced38f89028c1755903ebae28630e
beep boop <githubbot@vadim.me>
2024-07-24 12:36:27 +0200

    Solution 44 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/rig8dcou Prompt:       |             ^~~~~~ 08c25967-24dc-48a1-81f7-e39849419310.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 08c25967-24dc-48a1-81f7-e39849419310.cpp:28:39: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 08c25967-24dc-48a1-81f7-e39849419310.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 08c25967-24dc-48a1-81f7-e39849419310.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 08c25967-24dc-48a1-81f7-e39849419310.cpp:27: 08c25967-24dc-48a1-81f7-e39849419310.cpp: In function ‘int main()’: 08c25967-24dc-48a1-81f7-e39849419310.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -20,6 +20,6 @@ bool is_same(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    assert(is_same(get_odd_collatz(1), std::vector<int>{1}));
+    assert(is_same(get_odd_collatz(1), {1}));
     return 0;
 }
