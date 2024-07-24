345ed3212e235e70d039a14b8980ebf951d8185d
beep boop <githubbot@vadim.me>
2024-07-24 04:17:33 +0200

    Solution 22 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/13qyj88i Prompt: 7ef136e4-cf34-4d6d-92d4-9fa0c3515d79.cpp:27:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    27 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 7ef136e4-cf34-4d6d-92d4-9fa0c3515d79.cpp:30:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    30 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 7ef136e4-cf34-4d6d-92d4-9fa0c3515d79.cpp:30:36: error: ‘find_zero’ cannot be use d as a function    30 |         solution = find_zero(coeffs);       |                                    ^ In file included from 7ef136e4-cf34-4d6d-92d4-9fa0c3515d79.cpp:14: 7ef136e4-cf34-4d6d-92d4-9fa0c3515d79.cpp:31:21: error: ‘poly’ was not declared i n this scope    31 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ 7ef136e4-cf34-4d6d-92d4-9fa0c3515d79.cpp:31:17: error: ‘abs’ was not declared in  this scope    31 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,20 +1,20 @@
-double find_zero(vector<double> coeffs,vector<double> xs){
+double find_zero(vector<double> coeffs, vector<double> xs){
     double x = 1;
     for (int i = 2; i < xs.size(); i += 2) {
-        while (poly(coeffs,xs,x) > 0) {
+        while (poly(coeffs, x) > 0) {
             x -= 0.001;
         }
-        while (poly(coeffs,xs,x) < 0) {
+        while (poly(coeffs, x) < 0) {
             x += 0.001;
         }
     }
     return round(x, 2);
 }
 
-double poly(vector<double> coeffs,double x){
+double poly(vector<double> coeffs, double x){
     double result = 0;
-    for(int i=0;i<coeffs.size();i++){
-        result+=coeffs[i]*pow(x,i);
+    for(int i=0; i<coeffs.size(); i++){
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
