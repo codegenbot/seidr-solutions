4a5a4e57a35190a06e6f99065b25f2531a2adae8
beep boop <githubbot@vadim.me>
2024-07-24 23:14:12 +0200

    Solution 80 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/wbt0y349 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf7ebd18-ea74-4ab1-8705-97eee0200 e8b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,16 +1,19 @@
-def factorize(n: int) -> list[int]:
+```
+from typing import List
+
+def factorize(n: int) -> List[int]:
     factors = []
     i = 2
     while i * i <= n:
         if n % i:
             i += 1
         else:
+            n //= i
             count = 0
             while n % i == 0:
                 n //= i
                 count += 1
-            factors.append(i ** count)
-            i += 1
+            factors.extend([i] * count)
     if n > 1:
         factors.append(n)
-    return list(set(factors))
+    return factors
