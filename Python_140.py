c25d8219f8982341df6097743bee7666e9d0dac9
beep boop <githubbot@vadim.me>
2024-07-24 05:32:16 +0200

    Solution 32 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72752490-a4cb-4cda-a6d1-ef6088ccc f88.py", line 24, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72752490-a4cb-4cda-a6d1-ef6088ccc f88.py", line 20, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,14 +1,12 @@
 def fix_spaces(text):
     new_text = ""
     add_hyphen = False
-    for char in text.strip():
+    for char in text.strip(): 
         if char.isalnum() or char in ["-", "_"]:
             new_text += char
-            add_hyphen = False
+        elif char == " " and not add_hyphen:
+            new_text += "-"
+            add_hyphen = True
         elif char == " ":
-            if not add_hyphen:
-                new_text += "-"
-                add_hyphen = True
-            else:
-                new_text += " "
+            add_hyphen = True
     return new_text
