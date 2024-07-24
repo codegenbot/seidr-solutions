15f362ed5ef6dd8432328bb5e8dbea49df16c12e
beep boop <githubbot@vadim.me>
2024-07-24 21:23:22 +0200

    Solution 82 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ba7grvne Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f17c99f-dace-4572-882e-e7f8f453b e66.py", line 32, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f17c99f-dace-4572-882e-e7f8f453b e66.py", line 29, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f17c99f-dace-4572-882e-e7f8f453b e66.py", line 16, in minPath     res = dfs(0, 0, grid[0][0], set())           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6f17c99f-dace-4572-882e-e7f8f453b e66.py", line 6, in dfs     if len(path) == k:        ^^^^^^^^^ TypeError: object of type 'int' has no len()

--- Python_129.py
+++ Python_129.py
@@ -1,4 +1,3 @@
-```
 def minPath(grid, k):
     n = len(grid)
     m = [[i * n + j for j in range(n)] for i in range(n)]
@@ -11,7 +10,7 @@ def minPath(grid, k):
         for x, y in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             ni, nj = i + x, j + y
             if 0 <= ni < n and 0 <= nj < n and (ni, nj) not in visited:
-                res.extend(dfs(ni, nj, path + [None], visited))
+                res.extend(dfs(ni, nj, path + [m[ni][nj]], visited))
         return res
 
     res = dfs(0, 0, None, set())
