816342c76cf48cd8f96b9e661a6bf1527b9c36f0
beep boop <githubbot@vadim.me>
2024-07-24 06:21:47 +0200

    Solution 6 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/y75kjqs5 Prompt: Given a vector of integers, return the two elements that sum to a target integer.

--- find-pair.cpp
+++ find-pair.cpp
@@ -6,17 +6,16 @@ int main() {
         cin >> nums[i];
     }
     cin >> target;
-    
-    map<int, int> numIndex;
+
+    map<int, int> mp;
     for (int i = 0; i < n; ++i) {
-        int complement = target - nums[i];
-        if (numIndex.find(complement) != numIndex.end()) {
-            cout << complement << endl;
+        if (mp.find(target - nums[i]) != mp.end()) {
             cout << nums[i] << endl;
+            cout << target - nums[i] << endl;
             break;
         }
-        numIndex[nums[i]] = i;
+        mp[nums[i]] = i;
     }
-    
+
     return 0;
 }
