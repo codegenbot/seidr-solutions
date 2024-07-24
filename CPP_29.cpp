1d5cf3e3e1f11ab0c0add27aeb59809c4fb882cd
beep boop <githubbot@vadim.me>
2024-07-24 04:48:49 +0200

    Solution 48 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/q7azetgh Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9113288b-bb41-4a0c-bfba-de1df5776fe7.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9113288b-bb41-4a0c-bfba-de1df5776fe7.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9113288b-bb41-4a0c-bfba-de1df5776fe7.cpp:10: 9113288b-bb41-4a0c-bfba-de1df5776fe7.cpp: In function ‘int main()’: 9113288b-bb41-4a0c-bfba-de1df5776fe7.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 9113288b-bb41-4a0c-bfba-de1df5776fe7.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,30 +1,8 @@
-bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
-        return false;
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for(string s : strings) {
+        if(s.find(prefix) == 0)
+            result.push_back(s);
     }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    vector<string> strings;
-    string prefix;
-
-    cout << "Enter the list of strings: ";
-    while(getline(cin, prefix)) {
-        strings.push_back(prefix);
-    }
-
-    cout << "Enter the prefix to filter by: ";
-    getline(cin, prefix);
-
-    vector<string> result = filter_by_prefix(strings, prefix);
-
-    assert(issame(result, {"hello1", "hello2"})); // This is where the issue was!
-
-    return 0;
+    return result;
 }
