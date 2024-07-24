7200f9d92cdf140ab3882c0888c5190c1fd50ae6
beep boop <githubbot@vadim.me>
2024-07-24 03:41:17 +0200

    Solution 1 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ulm9ijxo Prompt: Complete the following code given the task description and function signature.

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,3 +1,4 @@
+#include<stdio.h>
 #include<string>
 using namespace std;
 
@@ -7,15 +8,12 @@ bool is_palindrome(string str){
 }
 
 string make_palindrome(string str){
-    if(is_palindrome(str)) return str; // base case: if input is already palindrome
-    
-    int i = 0, j = str.size() - 1; // initialize pointers for finding palindromic suffix
-    while(i < j && str[i] == str[j]) {
-        i++; j--;
+    int i = 0, j = str.length() - 1;
+    while(i <= j && str[i] == str[j]){
+        i++;
+        j--;
     }
-    
-    string prefix = str.substr(0, i); // get the prefix of non-palindromic part
-    string postfix = str.substr(j+1); // get the postfix of palindromic part
-    
-    return prefix + str.substr(i) + reverse(postfix);
+    string rev = str.substr(i, j-i+1);
+    reverse(rev.begin(), rev.end());
+    return str + rev;
 }
