6ba35ce1dc11883ccfafbd811c9f6f96a4d3cd6a
beep boop <githubbot@vadim.me>
2024-07-24 05:51:47 +0200

    Solution 5 of CPP/102, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nc0ia1vo Prompt: Complete the following code given the task description and function signature.

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,7 +1,14 @@
+#include<stdio.h>
+using namespace std;
+
 int choose_num(int x, int y) {
+    if (x > y) {
+        return -1;
+    }
     for (int i = x; i <= y; i++) {
-        if (i % 2 == 0)
+        if (i % 2 == 0) {
             return i;
+        }
     }
     return -1;
 }
