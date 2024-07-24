acc16f8a9602e8242f1d0940840d2490942a44dc
beep boop <githubbot@vadim.me>
2024-07-24 09:02:47 +0200

    Solution 67 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/i8qfruf6 Prompt:       |                              ^~~~~~ 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp:16: 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp: In function ‘int main()’: 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp:26:20: error: ‘reverse_delete’ was not declared in this scope    26 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 1808bfe0-69f5-47c0-be3d-a59a1ce197b9.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,38 +1,21 @@
-```cpp
-#include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
-
 bool issame(vector<string> a, vector<string> b) {
     return (a == b);
 }
-
-std::vector<std::string> reverse_delete(std::string s, std::string c) {
-    std::vector<std::string> result;
-    std::string temp = "";
+vector<string> reverse_delete(string s, string c) {
+    vector<string> result;
+    string temp = "";
     for (int i = 0; i < s.length(); i++) {
         if (find(c.begin(), c.end(), s[i]) == c.end()) {
             temp += s[i];
         }
     }
     result.push_back(temp);
-    std::string rev = temp;
-    std::reverse(rev.begin(), rev.end());
-    result.push_back((temp == rev) ? "True" : "False");
+    string rev = temp;
+    reverse(rev.begin(), rev.end());
+    if (issame(vector<string>{temp, rev}, vector<string>{result[0], result[1]}))
+        result.push_back("True");
+    else
+        result.push_back("False");
     return result;
-}
-
-int main() {
-    std::cout << "[Output] ";
-    std::string str, ch;
-    std::cin >> str >> ch;
-
-    std::vector<std::string> output = reverse_delete(str, ch);
-    for (const auto& o : output) {
-        std::cout << o << " ";
-    }
-    std::cout << std::endl;
-    
-    return 0;
 }
