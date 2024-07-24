e528e117bea148644fd10838e96f3a7da0781d53
beep boop <githubbot@vadim.me>
2024-07-24 15:57:50 +0200

    Solution 5 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt: 04639f36-8e9e-4cfd-9f3e-5cf74bda9966.cpp: In function ‘std::string string_to_md5 (std::string)’: 04639f36-8e9e-4cfd-9f3e-5cf74bda9966.cpp:13:5: error: ‘MD5_CTX’ was not declared  in this scope    13 |     MD5_CTX ctx;       |     ^~~~~~~ 04639f36-8e9e-4cfd-9f3e-5cf74bda9966.cpp:14:15: error: ‘ctx’ was not declared in  this scope    14 |     MD5_Init(&ctx);       |               ^~~ 04639f36-8e9e-4cfd-9f3e-5cf74bda9966.cpp:14:5: error: ‘MD5_Init’ was not declare d in this scope    14 |     MD5_Init(&ctx);       |     ^~~~~~~~ 04639f36-8e9e-4cfd-9f3e-5cf74bda9966.cpp:15:5: error: ‘MD5_Update’ was not decla red in this scope    15 |     MD5_Update(&ctx, text.c_str(), text.size());       |     ^~~~~~~~~~ 04639f36-8e9e-4cfd-9f3e-5cf74bda9966.cpp:16:5: error: ‘MD5_Final’ was not declar ed in this scope    16 |     MD5_Final(md, &ctx);       |     ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,10 +8,10 @@ using namespace std;
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    MD5_CTX ctx;
     unsigned char md[16];
     stringstream ss;
 
+    MD5_CTX ctx;
     MD5_Init(&ctx);
     MD5_Update(&ctx, text.c_str(), text.size());
     MD5_Final(md, &ctx);
