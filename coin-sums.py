35d6104bd18bb6d5bc0f0ef78c3889657937e855
beep boop <githubbot@vadim.me>
2024-07-24 06:12:19 +0200

    Solution 63 of coin-sums, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ech70kmn Prompt: Given a number of cents, ﬁnd the fewest number of US coins (pennies, nickles, dimes, quarters) needed to make that amount, and return the number of each type of coin as a separate output.

--- coin-sums.py
+++ coin-sums.py
@@ -1,14 +1,12 @@
 def coin_sums(cents):
     coins = [25, 10, 5, 1]
-    result = [0, 0, 0, 0]
-
+    res = [0, 0, 0, 0]
     for i in range(len(coins)):
         while cents >= coins[i]:
             cents -= coins[i]
-            result[i] += 1
-
-    return result
+            res[i] += 1
+    return str(res[3]) + "\n" + str(res[2]) + "\n" + str(res[1]) + "\n" + str(res[0])
 
 
 cents = int(input())
-print(*coin_sums(cents))
+print(coin_sums(cents))
