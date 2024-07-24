5db6a86a97d18ed9beec287c1b578b0f64e8122f
beep boop <githubbot@vadim.me>
2024-07-24 22:57:41 +0200

    Solution 11 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/5v9y8ph3 Prompt: Complete the following code given the task description and function signature.

--- Python_38.py
+++ Python_38.py
@@ -1,6 +1,27 @@
+def encode_cyclic(s: str):
+    return "".join(
+        [
+            (
+                s[i : i + 3][1:] + s[i : i + 3][0]
+                if len(s[i : i + 3]) == 3
+                else s[i : i + 3]
+            )
+            for i in range((len(s) + 2) // 3)
+        ]
+    )
+
+
 def decode_cyclic(s: str):
-    groups = [s[i : i + 3] for i in range(0, len(s), 3)]
-    groups = [
-        (group[-1] + group[:-1]) if len(group) == 3 else group for group in groups
-    ]
-    return "".join(groups)
+    return "".join(
+        [
+            (
+                "".join(
+                    [s[3 * i % len(s) : 3 * (i + 1) % len(s)][-1]]
+                    + [s[3 * i % len(s) : 3 * (i + 1) % len(s)][:-1]]
+                )
+                if len(s[3 * i % len(s) : 3 * (i + 1) % len(s)]) == 3
+                else s[3 * i % len(s) : 3 * (i + 1) % len(s)]
+            )
+            for i in range((len(s) + 2) // 3)
+        ]
+    )
