8ac7db10d6c4be94b425aa2898af3236c99403df
beep boop <githubbot@vadim.me>
2024-07-24 02:57:56 +0200

    Solution 47 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yphimha5 Prompt: In file included from /usr/include/c++/11/string:39,                  from bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp:44:44: error: ‘b’ was not declared in t his scope    44 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp:43: bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp: In function ‘int main()’: bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp:53:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    53 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ bc4bf6db-4eb5-47b7-9a3a-3913d4172292.cpp:53:19: error: ‘issame’ cannot be used a s a function    53 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,4 +1,3 @@
-```cpp
 vector<string> reverse_delete(string s, string c) {
     vector<string> result;
     string temp = "";
@@ -22,4 +21,5 @@ vector<string> reverse_delete(string s, string c) {
     } else {
         result.push_back("False");
     }
-    return result;
+    return result; 
+}
