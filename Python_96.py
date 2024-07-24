57b17ffbea9b4aa90300dedaa0cd36333c6228c9
beep boop <githubbot@vadim.me>
2024-07-24 10:29:29 +0200

    Solution 33 of Python/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/51nqom9z Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22237172-d9c5-4f5a-939b-a00f2da1c c54.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_96.py
+++ Python_96.py
@@ -1,11 +1,8 @@
 ```
 def count_up_to(n):
     primes = []
-    for i in range(2, n+1):
+    for i in range(2, n):
         is_prime = True
-        if i == 2:
-            primes.append(i)
-            continue
         for j in range(2, int(i**0.5) + 1):
             if i % j == 0:
                 is_prime = False
