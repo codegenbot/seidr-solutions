fdff09cddd3f1c3b59677ec69499a9d8f8029a54
beep boop <githubbot@vadim.me>
2024-07-24 03:07:34 +0200

    Solution 35 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v4oavres Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f282ba0a-3ad7-4043-9777-c660878e1 297.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,5 @@
-def fruit_distribution():
-    s = input()
-    n = int(input())
+```
+def fruit_distribution(s, n):
     s = s.replace("apples", "1 apple").replace("oranges", "1 orange")
     total_fruit = 0
     for word in s.lower().split():
