ce6df495e0ffe7c85dd14dab759c881beaa623b9
beep boop <githubbot@vadim.me>
2024-07-24 07:18:04 +0200

    Solution 94 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt: Invalid input. Please enter a list of numbers. Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb96a94b-5d52-46dd-8e41-57c6f8dbf 699.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cb96a94b-5d52-46dd-8e41-57c6f8dbf 699.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,5 @@
 def sorted_list_sum(lst):
-    if all(i.isnumeric() for i in lst) and len(set(len(x) for x in lst)) == 1:
-        return sum(sorted(map(int, [i * int('0' * (len(max(lst)) - len(i))) for i in lst])))
+    if all(i.isnumeric() for i in lst):
+        return int(sum(sorted(map(int, lst))))
     else:
-        print("Invalid input. Please enter a list of numbers.")
+        return ''.join(sorted(lst))
