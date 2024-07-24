34989632d97d8d228457fa5722ed87c7d776a0bc
beep boop <githubbot@vadim.me>
2024-07-25 00:31:11 +0200

    Solution 74 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/51vrqbmc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bb10e8c7-a195-4b91-b895-406e01fd9 458.py", line 3, in <module>     check(is_pythagorean_triple)     ^^^^^ NameError: name 'check' is not defined

--- Python_157.py
+++ Python_157.py
@@ -1,12 +1,5 @@
-def is_pythagorean_triple(a, b, c):
-    return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
-
-
 def check(func):
     if func(3, 4, 5):
-        print("It's a Pythagorean triple.")
+        print("Valid Pythagorean Triple")
     else:
-        print("It's not a Pythagorean triple.")
-
-
-check(is_pythagorean_triple)
+        print("Invalid Pythagorean Triple")
