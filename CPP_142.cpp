d82befa3ba30fc20ae24774479a83363a8b69a20
beep boop <githubbot@vadim.me>
2024-07-24 10:14:01 +0200

    Solution 17 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/15isnrcr Prompt: 20a982cf-5c76-4a76-92e2-7c959cf0896d.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<int> lst){       |                 ^~~~~~ 20a982cf-5c76-4a76-92e2-7c959cf0896d.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int sum_squares(vector<int> lst){       |                        ^~~ In file included from 20a982cf-5c76-4a76-92e2-7c959cf0896d.cpp:12: 20a982cf-5c76-4a76-92e2-7c959cf0896d.cpp: In function ‘int main()’: 20a982cf-5c76-4a76-92e2-7c959cf0896d.cpp:14:24: error: ‘sum_squares’ cannot be u sed as a function    14 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,7 +1,6 @@
-```
 #include <vector>
 
-int sum_squares(std::vector<int> lst){
+int sum_squares(vector<int> lst) {
     int total = 0;
     for(int i = 0; i < lst.size(); i++){
         if(i % 3 == 0 && i % 4 != 0) 
