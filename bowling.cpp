a8f503a8ac6d95f91452b6bcbdc049f5aee6afae
beep boop <githubbot@vadim.me>
2024-07-25 01:50:34 +0200

    Solution 74 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/iut9oi2w Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,37 +1,49 @@
-int bowling(string s) {
-    int score = 0, frame = 0, ball = 0;
-    for (char c : s) {
-        if (c == 'X') {
+int bowlingScore(string s) {
+    int score = 0;
+    int frame = 0;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'X') {
             score += 10;
-            if (frame < 9) {
-                score += (s[ball + 1] == 'X') ? 10 + ((s[ball + 2] == 'X') ? 10 : s[ball + 2] - '0') : (s[ball + 2] == '/') ? 10 : s[ball + 1] - '0';
-            }
-            ball++;
-            frame++;
-        } else if (c == '/') {
-            score += 10 - (s[ball - 1] - '0');
-            score += (frame < 9) ? (s[ball + 1] == 'X') ? 10 : s[ball + 1] - '0' : 0;
-            ball++;
-        } else if (c == '-') {
-            ball++;
-        } else {
-            score += c - '0';
-            if (frame < 9 && c != '0') {
-                if (s[ball + 1] == '/') {
-                    score += 10 - (c - '0');
+            if (s[i+2] == 'X') {
+                score += 10;
+                if (s[i+4] == 'X') {
+                    score += 10;
+                } else if (s[i+4] == '/') {
+                    score += 10 - (s[i+3] - '0');
+                } else {
+                    score += s[i+3] - '0' + s[i+4] - '0';
                 }
+            } else if (s[i+2] == '/') {
+                score += 10;
+            } else {
+                score += s[i+2] - '0' + s[i+3] - '0';
             }
-            ball++;
-            if (c != '0') {
-                frame++;
+            frame++;
+        } else if (s[i] == '/') {
+            score += 10 - (s[i-1] - '0');
+            if (s[i+1] == 'X') {
+                score += 10;
+            } else {
+                score += s[i+1] - '0';
             }
+            frame++;
+        } else {
+            score += s[i] - '0';
+            if (s[i+1] == '/') {
+                score += 10 - (s[i] - '0');
+            }
+            frame++;
+        }
+        if (frame == 10) {
+            break;
         }
     }
     return score;
 }
+
 int main() {
     string s;
     cin >> s;
-    cout << bowling(s) << endl;
+    cout << bowlingScore(s) << endl;
     return 0;
 }
