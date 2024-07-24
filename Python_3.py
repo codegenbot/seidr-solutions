3530137c9cc1988c71fc385dd40016a1eeb012b4
beep boop <githubbot@vadim.me>
2024-07-24 06:22:26 +0200

    Solution 27 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ol4po7z6 Prompt: Program did not receive expected input

--- Python_3.py
+++ Python_3.py
@@ -1,4 +1,3 @@
-```
 from typing import List
 
 
@@ -6,7 +5,7 @@ def below_zero(operations: List[int]) -> bool:
     balance = 0
     for operation in operations:
         if not isinstance(operation, int):
-            raise ValueError("Invalid input")
+            raise ValueError("List should contain integers only")
         balance += operation
         if balance < 0:
             return True
