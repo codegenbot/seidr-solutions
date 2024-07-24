145e43f587c7e165d6891048a301e1b978115561
beep boop <githubbot@vadim.me>
2024-07-25 01:37:50 +0200

    Solution 2 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/uz7danoh Prompt:    26 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ fb11522a-4d3a-477a-b11c-47dd0732cc98.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fb11522a-4d3a-477a-b11c-47dd0732cc98.cpp:26:43: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from fb11522a-4d3a-477a-b11c-47dd0732cc98.cpp:25: fb11522a-4d3a-477a-b11c-47dd0732cc98.cpp: In function ‘int main()’: fb11522a-4d3a-477a-b11c-47dd0732cc98.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ fb11522a-4d3a-477a-b11c-47dd0732cc98.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,3 +1,13 @@
+bool issame(vector<float> a,vector<float> b) {
+    if (a.size() != b.size()) return false;
+    
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
+    
+    return true;
+}
+
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
     vector<float> result;
@@ -23,6 +33,8 @@ vector<float> sort_even(vector<float> l) {
     return result;
 }
 
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
+int main(){
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    
+    return 0;
 }
