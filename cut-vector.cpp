be195fcbbcccff2f87f102aebac8f006bd2c64a1
beep boop <githubbot@vadim.me>
2024-07-24 05:56:42 +0200

    Solution 1 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,46 +3,34 @@ using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
     int n = v.size();
-    vector<vector<int>> res(2);
-    for (int i = 0; i < n; i++) {
-        if (i == 0 || i == n - 1) {
-            res[0].push_back(v[i]);
-            res[1].push_back(v[i]);
-        } else {
-            int leftSum = 0, rightSum = 0;
-            for (int j = 0; j < i; j++) {
-                leftSum += v[j];
-            }
-            for (int j = i; j < n; j++) {
-                rightSum += v[j];
-            }
-            if (leftSum == rightSum) {
-                res[0].push_back(v[i]);
-                res[1].push_back(v[i]);
-            } else {
-                int minDiff = abs(leftSum - rightSum);
-                for (int j = 0; j < i; j++) {
-                    leftSum -= v[j];
-                    if (abs(leftSum - rightSum) < minDiff) {
-                        minDiff = abs(leftSum - rightSum);
-                        res[0].clear();
-                        res[1].clear();
-                        res[0].push_back(v[j]);
-                        for (int k = j; k < i; k++) {
-                            leftSum -= v[k];
-                        }
-                        res[0].push_back(0);
-                        for (int k = i; k < n; k++) {
-                            rightSum += v[k];
-                        }
-                        res[1].push_back(v[i]);
-                        for (int k = i; k < n; k++) {
-                            res[1].push_back(0);
-                        }
-                    }
-                }
-            }
+    vector<vector<int>> res;
+    
+    for (int i = 1; i < n; i++) {
+        if (v[i] - v[0] <= v[n-1] - v[i]) {
+            res.push_back({v.begin(), v.begin() + i});
+            res.push_back({v.begin() + i, v.end()});
+            return res;
         }
     }
+    
+    res.push_back(v);
+    res.push_back({});
     return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+    for (int i = 0; i <= n; i++) {
+        cin >> v[i];
+    }
+    vector<vector<int>> result = cutVector(v);
+    for (auto &vec : result) {
+        for (int num : vec) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
