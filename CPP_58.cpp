7f73b880d90d3bea0407a4791ea0d323cb1e8f23
beep boop <githubbot@vadim.me>
2024-07-24 19:57:06 +0200

    Solution 72 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bpeo6ouc Prompt:       |                    ^~~ 01159ce7-0a05-4609-8457-b7138a0113ed.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 01159ce7-0a05-4609-8457-b7138a0113ed.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 01159ce7-0a05-4609-8457-b7138a0113ed.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 01159ce7-0a05-4609-8457-b7138a0113ed.cpp:15: 01159ce7-0a05-4609-8457-b7138a0113ed.cpp: In function ‘int main()’: 01159ce7-0a05-4609-8457-b7138a0113ed.cpp:25:20: error: ‘common’ was not declared  in this scope    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 01159ce7-0a05-4609-8457-b7138a0113ed.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -6,17 +6,19 @@ bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-vector<std::vector<int>> common(std::vector<std::vector<int>>& list1, std::vector<std::vector<int>>& list2) {
-    std::vector<std::vector<int>> result;
+vector<std::vector<int>> common(std::vector<int> l1, std::vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
 
-    for (const auto& vec1 : list1) {
-        for (const auto& vec2 : list2) {
-            if (issame(vec1, vec2)) {
-                result.push_back(vec1);
-                break;
-            }
+    set<int> intersection(s1.begin(), s1.end());
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(intersection, intersection.begin()));
+
+    vector<std::vector<int>> result;
+    for (int i : intersection) {
+        if (issame({i}, l1) && issame({i}, l2)) {
+            result.push_back({i});
         }
     }
-
     return result;
 }
