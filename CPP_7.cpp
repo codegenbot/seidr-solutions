35ac6c2d122a4a3c00b881b95809ae0f73d44f08
beep boop <githubbot@vadim.me>
2024-07-24 17:38:48 +0200

    Solution 60 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts9ewlh2 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3011b2c3-d2bd-4972-b0b2-8706995286ed.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3011b2c3-d2bd-4972-b0b2-8706995286ed.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3011b2c3-d2bd-4972-b0b2-8706995286ed.cpp:10: 3011b2c3-d2bd-4972-b0b2-8706995286ed.cpp: In function ‘int main()’: 3011b2c3-d2bd-4972-b0b2-8706995286ed.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 3011b2c3-d2bd-4972-b0b2-8706995286ed.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,51 +1,14 @@
-#include <iostream>
 #include <vector>
-#include <set>
+#include <string>
 
-bool issame(vector<string> a, vector<string> b) {
-    set<string> setA(a.begin(), a.end());
-    set<string> setB(b.begin(), b.end());
-
-    return setA == setB;
+bool issame(vector<string> a,vector<string>b){
+    for(string s : b){
+        if(find(a.begin(),a.end(),s) == a.end())
+            return false;
+    }
+    return true;
 }
 
-int main() {
-    int n;
-    std::cout << "Enter the number of strings: ";
-    std::cin >> n;
-
-    vector<string> strings(n);
-    for(int i = 0; i < n; i++) {
-        std::cout << "Enter string " << (i+1) << ": ";
-        cin >> strings[i];
-    }
-
-    int m;
-    std::cout << "Enter the number of substrings: ";
-    std::cin >> m;
-
-    vector<string> sub(m);
-    for(int i = 0; i < m; i++) {
-        std::cout << "Enter substring " << (i+1) << ": ";
-        cin >> sub[i];
-    }
-
-    vector<string> filtered_strings = filter_by_substring(strings, sub[0]);
-
-    if(issame(filtered_strings, strings)) {
-        cout << "The filtered strings are the same as the original strings." << endl;
-    } else {
-        cout << "The filtered strings are different from the original strings." << endl;
-    }
-
-    return 0;
-}
-
-vector<string> filter_by_substring(vector<string> strings, string substring){
-    vector<string> result;
-    for(string s : strings){
-        if(s.find(substring) != string::npos)
-            result.push_back(s);
-    }
-    return result;
+int main(){
+    assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") ,{"grunt", "prune"}));
 }
