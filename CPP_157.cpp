dadfe3f851df1ab88426d0cd3868d886f4789799
beep boop <githubbot@vadim.me>
2024-07-24 23:47:12 +0200

    Solution 2 of CPP/157, ratio 0.7272727272727273 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/b1ovreda Prompt: Complete the following code given the task description and function signature.

--- CPP_157.cpp
+++ CPP_157.cpp
@@ -1,10 +1,9 @@
 #include <cmath>
 
-using namespace std;
-
 bool right_angle_triangle(float a, float b, float c) {
-    if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a)
+    if (a*a + b*b == c*c || a*a + c*c == b*b || b*b + c*c == a*a) {
         return true;
-    else
+    } else {
         return false;
+    }
 }
