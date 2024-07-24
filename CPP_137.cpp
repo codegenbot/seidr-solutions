0193200e05fb38bca274adfbefdbe65dbd5c1627
beep boop <githubbot@vadim.me>
2024-07-24 23:55:21 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z78os2pt Prompt: d800ded4-c609-4a70-adb0-d942f39cc515.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from d800ded4-c609-4a70-adb0-d942f39cc515.cpp:36: d800ded4-c609-4a70-adb0-d942f39cc515.cpp: In function ‘int main()’: d800ded4-c609-4a70-adb0-d942f39cc515.cpp:38:13: error: ‘boost’ has not been decl ared    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ d800ded4-c609-4a70-adb0-d942f39cc515.cpp:38:29: error: ‘string’ was not declared  in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ d800ded4-c609-4a70-adb0-d942f39cc515.cpp:38:37: error: ‘compare_one’ was not dec lared in this scope    38 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -8,13 +8,18 @@ boost::any compare_one(boost::any a, boost::any b) {
         return (double)b > a.convert_to<int>() ? b : a;
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (int)a > b.convert_to<int>() ? a : boost::any(int(0));
+        return (int)a > b.convert_to<int>() ? a : b;
     }
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return (int)b > a.convert_to<double>() ? b : boost::any(int(0));
+        return (int)b > a.convert_to<double>() ? b : a;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return boost::any_cast<string>(b) > boost::any_cast<string>(a) ? b : boost::any(int(0));
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
+        if (str1 > str2)
+            return a;
+        else
+            return b;
     }
     else if ((a.type() == typeid(int) || a.type() == typeid(double)) &&
              (b.type() == typeid(string))) {
@@ -22,7 +27,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         if (str.find('.') != string::npos || str.find(',') != string::npos)
             return b;
         else
-            return a > boost::any_cast<int>(b) ? a : boost::any(int(0));
+            return a > boost::any_cast<int>(b) ? a : b;
     }
     else if ((a.type() == typeid(string)) &&
              (b.type() == typeid(int) || b.type() == typeid(double))) {
@@ -30,7 +35,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         if (str.find('.') != string::npos || str.find(',') != string::npos)
             return a;
         else
-            return boost::any(int(0));
+            return b;
     }
     return b;
 }
