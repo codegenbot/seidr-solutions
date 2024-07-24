cf7f53293b867d87b8dbafaadbef0d213a9fd95b
beep boop <githubbot@vadim.me>
2024-07-24 03:39:07 +0200

    Solution 16 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/02oov1vw Prompt:       |                       ^~~~~~ dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp:41: dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp: In function ‘int main()’: dbdd525a-22e8-4a4c-8984-3cc78273b46b.cpp:43:23: error: ‘do_algebra’ cannot be us ed as a function    43 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,7 +1,7 @@
 #include <vector>
-#include <cmath>
+using namespace std;
 
-int doAlgebra(vector<string> operator_, vector<int> operand) {
+int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
     for (int i = 0; i < operator_.size(); i++) {
         if (operator_[i] == "+") {
@@ -9,12 +9,12 @@ int doAlgebra(vector<string> operator_, vector<int> operand) {
         } else if (operator_[i] == "-") {
             result -= operand[i];
         } else if (operator_[i] == "*") {
-            int temp = 0;
-            for (int j = i; j < operator_.size(); j++) {
+            int temp = operand[i + 1];
+            for (int j = i + 1; j < operator_.size(); j++) {
                 if (operator_[j] == "*") {
                     temp *= operand[j + 1];
                 } else if (operator_[j] == "+") {
-                    result += temp * operand[j + 1];
+                    result += temp;
                     break;
                 }
             }
