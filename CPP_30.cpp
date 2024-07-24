1bd20c3696f8a9f59c82ca186c1a8b37af2b8837
beep boop <githubbot@vadim.me>
2024-07-25 00:25:00 +0200

    Solution 4 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xo6etbfh Prompt: In file included from /usr/include/c++/11/vector:67,                  from 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp:31:36: error: expected primary-expressi on before ‘float’    31 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp:30: 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp: In function ‘int main()’: 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp:40:20: error: ‘get_positive’ was not de clared in this scope    40 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 134fea71-60a4-4ec5-a71a-7c99b84ab5ab.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -3,7 +3,7 @@
 
 bool issame(std::vector<float> a, std::vector<float> b);
 
-std::vector<float> get_positive(std::vector<float> l) {
+std::vector<float> get_positive(std::vector<float> l){
     std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
@@ -25,7 +25,4 @@ bool issame(std::vector<float> a, std::vector<float> b) {
     return true;
 }
 
-int main() {
-    assert(issame(get_positive({}), {}));
-    return 0;
-}
+assert(issame(get_positive({}), {}));
