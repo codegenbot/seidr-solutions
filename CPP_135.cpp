6b3272acdf66796cac68c9dff995a5cd4faa9432
beep boop <githubbot@vadim.me>
2024-07-24 02:41:21 +0200

    Solution 5 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/q8epvrud Prompt: 85a689cb-6233-4363-9a5d-366336e2eb50.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int can_arrange(vector<int> arr) {       |                 ^~~~~~ 85a689cb-6233-4363-9a5d-366336e2eb50.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int can_arrange(vector<int> arr) {       |                        ^~~ In file included from 85a689cb-6233-4363-9a5d-366336e2eb50.cpp:10: 85a689cb-6233-4363-9a5d-366336e2eb50.cpp: In function ‘int main()’: 85a689cb-6233-4363-9a5d-366336e2eb50.cpp:12:24: error: ‘can_arrange’ cannot be u sed as a function    12 |     assert (can_arrange({})==-1);       |             ~~~~~~~~~~~^~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,9 +1,12 @@
 #include <vector>
+
 int can_arrange(vector<int> arr) {
+    int result = -1;
     for (int i = 1; i < arr.size(); i++) {
         if (arr[i] <= arr[i-1]) {
-            return i;
+            result = i;
+            break;
         }
     }
-    return -1;
+    return result;
 }
