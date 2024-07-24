7d5845807c867e145c43150fe9955b4a2a8547ab
beep boop <githubbot@vadim.me>
2024-07-25 01:11:34 +0200

    Solution 6 of Python/33, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xil8qoac Prompt: Complete the following code given the task description and function signature.

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,8 @@
 def sort_third(l: list):
-    return [sorted(l[i:i+3]) if i % 3 == 0 else l[i] for i in range(len(l))]
+    l_copy = l[:]
+    l_divisible_by_three = [val for idx, val in enumerate(l_copy) if idx % 3 == 0]
+    l_divisible_by_three.sort()
+    for idx, val in enumerate(l_copy):
+        if idx % 3 == 0:
+            l_copy[idx] = l_divisible_by_three.pop(0)
+    return l_copy
