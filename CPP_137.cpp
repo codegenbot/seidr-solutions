31c69245f1047be6242241b6d51b18688dec4024
beep boop <githubbot@vadim.me>
2024-07-24 03:01:25 +0200

    Solution 31 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt: c97439e4-92eb-4c4c-b637-cb7c11898c5f.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,20 +1,26 @@
+#include <iostream>
+#include <string>
 #include <boost/config.hpp>
 #include <boost/any.hpp>
-#include <string>
-#include <iostream>
 
 using namespace std;
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return any_cast<int>(a) > any_cast<int>(b) ? a : b;
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (boost::any_cast<int>(a) > boost::any_cast<float>(b)) ? a : b;
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return any_cast<float>(a) > any_cast<float>(b) ? a : b;
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (boost::any_cast<float>(a) > boost::any_cast<int>(b)) ? a : b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return any_cast<string>(a) > any_cast<string>(b) ? a : b;
+        return a.convert<string>().compare(b.convert<string>()) > 0 ? a : b;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return (boost::any_cast<float>(a) > stod(b.convert<string>().c_str())) ? a : b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        return stod(a.convert<string>().c_str()) > boost::any_cast<float>(b) ? a : b;
     }
     else {
         return "None";
