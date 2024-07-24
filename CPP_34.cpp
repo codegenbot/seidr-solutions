5cc18a1a66ae424b31349be1d52390a6857d6ac1
beep boop <githubbot@vadim.me>
2024-07-24 17:41:41 +0200

    Solution 53 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/epim71sb Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fadb0421-1e7b-4f5d-aa45-6437865962a7.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fadb0421-1e7b-4f5d-aa45-6437865962a7.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fadb0421-1e7b-4f5d-aa45-6437865962a7.cpp:5: fadb0421-1e7b-4f5d-aa45-6437865962a7.cpp: In function ‘int main()’: fadb0421-1e7b-4f5d-aa45-6437865962a7.cpp:15:20: error: ‘unique’ was not declared  in this scope    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ fadb0421-1e7b-4f5d-aa45-6437865962a7.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -2,16 +2,16 @@
 #include <vector>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+bool issame(std::vector<int> a, std::vector<int> b){
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
     return a == b;
 }
 
 int main(){
-    vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    sort(l.begin(), l.end());
-    l.erase(unique(l.begin(), l.end()), l.end());
+    std::vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    std::sort(l.begin(), l.end());
+    l.erase(std::unique(l.begin(), l.end()), l.end());
     assert(issame(l, {0, 2, 3, 5, 9, 123}));
     return 0;
 }
