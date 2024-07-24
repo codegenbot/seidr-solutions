7ce4e8f177fc0c722e9631c11c241fec85eae0e2
beep boop <githubbot@vadim.me>
2024-07-24 03:04:54 +0200

    Solution 33 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v4oavres Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/683bf7f8-da80-4177-b3ef-6bdbc2bad 85c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,6 +1,16 @@
+```
 def fruit_distribution(s, n):
     s = s.replace("apples", "1 apple").replace("oranges", "1 orange")
     total_fruit = 0
     for word in s.lower().split():
         if "apple" in word or "orange" in word:
-            total_fruit += int("".join(filter(str.isdigit, word))) or 0
+            digit_str = "".join(filter(str.isdigit, word)).lstrip("0123456789")
+            total_fruit += int(digit_str) if digit_str else 0
+    max_per_box = min(total_fruit // (n + 1), n)
+    remaining_fruits = total_fruit - (max_per_box * (n + 1))
+
+    return (
+        int(-(-total_fruit / float(max_per_box)) + 0.5)
+        if max_per_box != 0
+        else total_fruit
+    )
