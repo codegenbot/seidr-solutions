e19cf4ca4a286283a39469c2c545babafb0abea2
beep boop <githubbot@vadim.me>
2024-07-24 04:08:04 +0200

    Solution 97 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt:    57 | int main(){       |     ^~~~ 39724fba-ec90-4c1b-83ef-2d2ace023a8c.cpp:48:5: note: ‘int main()’ previously def ined here    48 | int main() {       |     ^~~~ In file included from 39724fba-ec90-4c1b-83ef-2d2ace023a8c.cpp:56: 39724fba-ec90-4c1b-83ef-2d2ace023a8c.cpp: In function ‘int main()’: 39724fba-ec90-4c1b-83ef-2d2ace023a8c.cpp:58:13: error: ‘boost’ has not been decl ared    58 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 39724fba-ec90-4c1b-83ef-2d2ace023a8c.cpp:58:35: error: expected primary-expressi on before ‘>’ token    58 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 39724fba-ec90-4c1b-83ef-2d2ace023a8c.cpp:58:37: error: ‘compare_one’ was not dec lared in this scope    58 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,54 +1,22 @@
 #include <iostream>
 #include <string>
 #include <any>
-#include <sstream>
 
 using namespace std;
 
 any compare_one(any a, any b) {
-    if (a.type() == any_type<double> && b.type() == any_type<string>) {
-        double da = any_cast<double>(a);
-        string db = any_cast<string>(b);
-        if (da > stod(db))
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == any_type<string> && b.type() == any_type<double>) {
-        double da = any_cast<double>(b);
-        string db = any_cast<string>(a);
-        if (stod(db) > da)
-            return a;
-        else
-            return b;
-    }
-    else if (a.type() == any_type<string> && b.type() == any_type<string>) {
-        string da = any_cast<string>(a);
-        string db = any_cast<string>(b);
-        if (da > db)
-            return a;
-        else if (db > da)
-            return b;
-        else
-            return "None";
-    }
-    else if (a.type() == any_type<double> && b.type() == any_type<double>) {
-        double da = any_cast<double>(a);
-        double db = any_cast<double>(b);
-        if (da > db)
-            return a;
-        else if (db > da)
-            return b;
-        else
-            return "None";
-    }
-    return "None";
+    if (any_cast<double>(a) > any_cast<double>(b))
+        return a;
+    else if (any_cast<double>(b) > any_cast<double>(a))
+        return b;
+    else
+        return "None";
 }
 
 int main() {
-    cout << any_cast<string>(compare_one(1, 2.5)) << endl;
-    cout << any_cast<string>(compare_one(1, "2,3")) << endl;
-    cout << any_cast<string>(compare_one("5,1", "6")) << endl;
-    cout << any_cast<string>(compare_one("1", 1)) << endl;
+    cout << compare_one(1.0, 2.5) << endl;
+    cout << compare_one(1, 2.5) << endl; // This will throw an error.
+    cout << compare_one(5.1, 6.0) << endl;
+    cout << compare_one(1.0, 1) << endl;
     return 0;
 }
