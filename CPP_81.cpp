927f0c0176b10fc14e274986ab235d30a1ba307d
beep boop <githubbot@vadim.me>
2024-07-24 14:27:57 +0200

    Solution 41 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/movnupb1 Prompt:       |           ^~~~~~ In file included from 5dad40a8-c2ee-471c-9e96-97ce1265c467.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5dad40a8-c2ee-471c-9e96-97ce1265c467.cpp:69:37: error: ‘string’ was not declared  in this scope    69 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5dad40a8-c2ee-471c-9e96-97ce1265c467.cpp:69:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 5dad40a8-c2ee-471c-9e96-97ce1265c467.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 5dad40a8-c2ee-471c-9e96-97ce1265c467.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 5dad40a8-c2ee-471c-9e96-97ce1265c467.cpp:69:44: error: ‘b’ was not declared in t his scope    69 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <string>
 
@@ -38,29 +37,30 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 int main_test() {
     std::vector<float> grades = {4.0, 2.7};
     std::vector<std::string> a = numerical_letter_grade(grades);
-    if (a[0] == "A+")
+    if (a[0] >= "A+")
         a[0] = "A+";
-    else if (a[0] == "A")
+    else if (a[0] > "A")
         a[0] = "A";
-    else if (a[0] == "A-")
+    else if (a[0] > "A-")
         a[0] = "A-";
-    else if (a[0] == "B+")
+    else if (a[0] > "B+")
         a[0] = "B+";
-    else if (a[0] == "B")
+    else if (a[0] > "B")
         a[0] = "B";
-    else if (a[0] == "B-")
+    else if (a[0] > "B-")
         a[0] = "B-";
-    else if (a[0] == "C+")
+    else if (a[0] > "C+")
         a[0] = "C+";
-    else if (a[0] == "C")
+    else if (a[0] > "C")
         a[0] = "C";
-    else if (a[0] == "C-")
+    else if (a[0] > "C-")
         a[0] = "C-";
-    else if (a[0] == "D+")
+    else if (a[0] > "D+")
         a[0] = "D+";
     else
         a[0] = "F";
     std::vector<std::string> b(a);
-    bool same = issame({a}, {b});
+    std::vector<std::string> c = {a};
+    bool same = issame(c, b);
     return 0;
 }
