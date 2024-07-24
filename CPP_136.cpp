970e0e1576e7818e9da647a3713701f46b482a19
beep boop <githubbot@vadim.me>
2024-07-24 22:18:03 +0200

    Solution 15 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xcwm9szj Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 114656f9-bda3-46e3-9788-70c369c0252f.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 114656f9-bda3-46e3-9788-70c369c0252f.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 114656f9-bda3-46e3-9788-70c369c0252f.cpp:17: 114656f9-bda3-46e3-9788-70c369c0252f.cpp: In function ‘int main()’: 114656f9-bda3-46e3-9788-70c369c0252f.cpp:27:20: error: ‘largest_smallest_integer s’ was not declared in this scope    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 114656f9-bda3-46e3-9788-70c369c0252f.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -19,6 +19,6 @@ vector<int> find_max_neg_min_pos(vector<int> lst) {
 }
 
 int main() {
-    assert(issame(find_max_neg_min_pos({-6, -4, -4, -3, -100, 1}), vector<int>({{-3}, {1}})));
+    assert(issame(find_max_neg_min_pos({-6, -4, -4, -3, -100, 1}), vector<int> {-3, 1}));
     return 0;
 }
