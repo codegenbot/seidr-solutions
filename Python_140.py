be4ad3a7ac955ab0f3bce084745bd8f9014d64c8
beep boop <githubbot@vadim.me>
2024-07-24 21:48:07 +0200

    Solution 91 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mpmfr9pr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcf1151e-eea9-47c4-b01e-20f1f56fb 912.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,7 +1,4 @@
 ```
-def check(input_text):
-    assert fix_spaces(input_text.strip().replace(" ", "_")) == "-Exa_1_2_2_mple"
-
 def fix_spaces(text):
     text = text.strip().replace(" ", "_")
     while "--" in text or "---" in text or "----" in text:
@@ -9,3 +6,6 @@ def fix_spaces(text):
         text = text.replace("---", "-")
         text = text.replace("----", "-")
     return text
+
+def check(input_text):
+    assert fix_spaces(input_text.strip().replace(" ", "_")) == "-Exa_1_2_2_mple"
