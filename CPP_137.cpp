3a786ea75ef7eed5b89206386006018d11706bb1
beep boop <githubbot@vadim.me>
2024-07-24 10:24:31 +0200

    Solution 49 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/m3e7eqps Prompt: less_type) {       | ^~ 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:22:3: error: expected unqualified-id be fore ‘else’    22 | } else {       |   ^~~~ In file included from 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:31: 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp: In function ‘int main()’: 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:33:13: error: ‘boost’ has not been decl ared    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:33:29: error: ‘string’ was not declared  in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:33:37: error: ‘compare_one’ was not dec lared in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,8 @@
-```
+```cpp
 #include <boost/any.hpp>
 #include <string>
 
-boost::any compare_one(boost::any a, boost::any b) {
+boost::any compareOne(const boost::any& a, const boost::any& b) {
     if(a.type() == boost::any::typeless_type || b.type() == boost::any::typeless_type) {
         if(a.type() == boost::any::typeless_type && b.type() == boost::any::typeless_type) {
             string a_s = any_cast<string>(a);
