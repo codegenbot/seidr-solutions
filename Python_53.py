bd56171f9dba3a36e6123d503625300dc57421ad
beep boop <githubbot@vadim.me>
2024-07-24 04:16:58 +0200

    Solution 62 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2yrlv8y8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e100333-a6da-4f14-8cd3-6cb695f9e 13c.py", line 1, in <module>     def calculate_area_and_perimeter() -> tuple(float, float):                                           ^^^^^^^^^^^^^^^^^^^ TypeError: tuple expected at most 1 argument, got 2

--- Python_53.py
+++ Python_53.py
@@ -1,10 +1,10 @@
-def calculate_area_and_perimeter() -> tuple(float, float):
-    def check(length: int, width: int):
+def calculate_area_and_perimeter() -> float:
+    def check(length: int, width: int) -> tuple(float, float):
         if length < 0 or width < 0:
             return None
         area = length * width
         perimeter = 2 * (length + width)
-        return tuple([area, perimeter])
+        return area, perimeter
 
     length = float(input("Enter the length of the rectangle: "))
     width = float(input("Enter the width of the rectangle: "))
