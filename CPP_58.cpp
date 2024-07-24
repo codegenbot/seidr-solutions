a16d9f8d290c094a7a20d5986ddffa786b63e073
beep boop <githubbot@vadim.me>
2024-07-24 05:32:46 +0200

    Solution 63 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/afsfrk7g Prompt:       |                    ^~~ 15b21833-bb24-4c03-bdcf-68b69428b344.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 15b21833-bb24-4c03-bdcf-68b69428b344.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 15b21833-bb24-4c03-bdcf-68b69428b344.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 15b21833-bb24-4c03-bdcf-68b69428b344.cpp:14: 15b21833-bb24-4c03-bdcf-68b69428b344.cpp: In function ‘int main()’: 15b21833-bb24-4c03-bdcf-68b69428b344.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 15b21833-bb24-4c03-bdcf-68b69428b344.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,24 +1,25 @@
+```
 #include <vector>
 #include <set>
+#include <algorithm>
+
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
 
 std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
     std::set<int> s1(l1.begin(), l1.end());
     std::set<int> s2(l2.begin(), l2.end());
 
     std::set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
         inserter(intersection, intersection.begin()));
 
     std::vector<int> result(intersection.begin(), intersection.end());
     sort(result.begin(), result.end());
     return result;
-
-bool issame(const vector<int>& a,const vector<int>& b){
-    if(a.size()!=b.size())
-        return false;
-    for(int i=0;i<a.size();i++){
-        if(find(b.begin(),b.end(),a[i]) == b.end())
-            return false;
-    }
-    return true;
 }
