5f01bf83aef15dc494f18aa4f10c35887e5aa842
beep boop <githubbot@vadim.me>
2024-07-24 20:34:00 +0200

    Solution 85 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/f7p37lef Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9504008f-5b2d-4824-ac51-bfc94b9b4 350.py", line 8, in <module>     print(luhn(card_numbers))           ^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9504008f-5b2d-4824-ac51-bfc94b9b4 350.py", line 2, in luhn     card_number = [int(x) for x in str(card_number)[1:]]                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9504008f-5b2d-4824-ac51-bfc94b9b4 350.py", line 2, in <listcomp>     card_number = [int(x) for x in str(card_number)[1:]]                    ^^^^^^ ValueError: invalid literal for int() with base 10: ']'

--- luhn.py
+++ luhn.py
@@ -1,6 +1,6 @@
 def luhn(card_number):
-    card_number = list(map(int, filter(str.isdigit, str(card_number)[1:])))
-    result = sum(sum(divmod(d * 2, 10)) + d for d in card_number[:-1]) + card_number[-1]
+    card_number = [int(x) for x in str(card_number)[1:]]
+    result = sum(sum(divmod(d * 2, 10)) + d for d in filter(lambda x: int(x), card_number[:-1])) + card_number[-1]
     return abs(result) % 10
 
 card_numbers = list(map(int, filter(str.isdigit, input().split())))
