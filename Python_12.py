cc905136a00e1954280573e096babcf27efd4fec
beep boop <githubbot@vadim.me>
2024-07-24 09:57:56 +0200

    Solution 15 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/65b9ihed Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/411532d6-cc12-4ca6-be4e-9a7486f66 09e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -5,13 +5,12 @@ def main():
         if not strings:
             return None
         max_length = 0
-        result = ""
+        result = ''
         for s in strings:
             if len(s) > max_length:
                 max_length = len(s)
                 result = s
         return result
-
 
 if __name__ == "__main__":
     main()
