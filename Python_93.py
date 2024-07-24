8be3b402dd4f30020a4ff6e2973772e8ab6db3fa
beep boop <githubbot@vadim.me>
2024-07-24 15:07:20 +0200

    Solution 56 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cnzbwpl6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72e7eab2-b589-41a7-aa22-e895e51ad 75d.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72e7eab2-b589-41a7-aa22-e895e51ad 75d.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,9 +3,11 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.isupper():
-                result += chr((ord(char.lower()) - ord('a') + 3) % 26 + ord('A'))
+                result += chr((ord(char) - 65 + 3) % 26 + 65)
             else:
-                result += chr((ord(char.lower()) - ord('a') + 3) % 26 + ord('a'))
-        elif char.isdigit() or not char.isalnum():  
+                result += chr((ord(char) - 97 + 3) % 26 + 97)
+        elif char.isdigit():
+            result += str(int(char) + 3)
+        elif not char.isalnum():  
             result += char
     return result
