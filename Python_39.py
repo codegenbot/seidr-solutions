c7c0082e5aee87d71a74300b4856cc4d4ef220c5
beep boop <githubbot@vadim.me>
2024-07-24 14:50:42 +0200

    Solution 51 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/m4bicfcs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11d63d88-5e47-4ccd-8dbc-62c5ccc8e 23d.py", line 27, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/11d63d88-5e47-4ccd-8dbc-62c5ccc8e 23d.py", line 24, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -1,6 +1,8 @@
 def prime_fib(n: int):
     fib = [0, 1]
-    while len(fib) < n:  # Updated condition to generate Fibonacci numbers up to the nth index
+    while (
+        len(fib) < n
+    ):  # Updated condition to generate Fibonacci numbers up to the nth index
         fib.append(fib[-1] + fib[-2])
 
     prime_fib_nums = [num for num in fib if is_prime(num)]
