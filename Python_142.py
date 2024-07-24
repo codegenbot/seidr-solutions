62d94e3d13a2327fdc68aece66b917bcefc3d598
beep boop <githubbot@vadim.me>
2024-07-24 05:34:48 +0200

    Solution 36 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4d1aed26-f64d-4061-bf38-36b61a199 2e4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,5 +1,9 @@
+"""Write a Python function sum_squares(lst) that takes a list of integers as input and returns the sum of squares of all numbers in the list."""
+
+
 def sum_squares(lst):
-    return sum([n ** 2 for n in lst])
+    return sum([n**2 for n in lst])
+
 
 result = sum_squares([1, 2, 3, 11, 12])
 print(result)
