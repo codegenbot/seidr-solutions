bfc623318cbc580b04141e81ec1bbc70ac5dcee1
beep boop <githubbot@vadim.me>
2024-07-24 15:53:24 +0200

    Solution 33 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ho1yy1q3 Prompt: 35 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51f58f06-27e5-49b4-ba44-bc67cf50d a88.py", line 19, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51f58f06-27e5-49b4-ba44-bc67cf50d a88.py", line 15, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,4 @@
-def double_the_difference(lst):
-    odd_sum = sum(num ** 2 for num in lst if isinstance(num, int) and num % 2 != 0)
-    total_diff = abs((sum(x for x in lst) - len(lst)) * 2)
-    return (total_diff + odd_sum)
+def difference(lst):
+    odd_sum = sum(num ** 2 for num in lst if isinstance(num, int) and num >= 0 and num % 2 != 0)
+    average_diff = (sum(lst) - odd_sum) / len(lst) if len(lst) > 0 else None
+    return abs(average_diff - 2 * odd_sum)
