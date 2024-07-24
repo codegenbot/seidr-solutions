f974627035a99c0820d41b3517092e36e328747d
beep boop <githubbot@vadim.me>
2024-07-25 01:42:14 +0200

    Solution 7 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/c7fceeas Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/220e5bcf-ea3a-450a-8a20-8eec78633 30f.py", line 11, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/220e5bcf-ea3a-450a-8a20-8eec78633 30f.py", line 8, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,8 +1,6 @@
-```
-def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
-        return string + string
-    for i in range(len(string)):
-        mid = len(string) // 2
-        if string[:i] == string[i:][::-1]:
-            return string[:mid] + string[i:][::-1] + string[i:]
+def make_palindrome(s: str) -> str:
+    for i in range(len(s)):
+        for j in range(i+1):
+            new_s = s[:j] + s[i] + s[i:j:-1] + s[i:]
+            if new_s == new_s[::-1]:
+                return new_s
