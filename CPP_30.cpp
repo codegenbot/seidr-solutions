3c8af7bff54ebdbd3dcd5d77df9ca745d04d4ccc
beep boop <githubbot@vadim.me>
2024-07-25 01:30:22 +0200

    Solution 28 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ybysd7qk Prompt:       |                    ^~~~~ 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp:11:29: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp:11:36: error: expected primary-expressi on before ‘float’    11 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp:11:43: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp:10: 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp: In function ‘int main()’: 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp:20:20: error: ‘get_positive’ was not de clared in this scope    20 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 54565a9d-8d7f-4321-b6da-d901fef45ca5.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,19 +1,30 @@
 #include <vector>
+using namespace std;
 
-bool issame(vector<float> a, vector<float>b) {
+bool issame(vector<float> a,vector<float>b){
     if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+    for(float i : a) {
+        bool found = false;
+        for(float j : b) {
+            if(i == j) {
+                found = true; break;
+            }
+        }
+        if(!found) return false;
     }
     return true;
 }
 
 vector<float> get_positive(vector<float> l){
     vector<float> result;
-    for(float x : l) {
-        if(x > 0) {
-            result.push_back(x);
-        }
+    for (float i : l) {
+        if(i > 0)
+            result.push_back(i);
     }
     return result;
+}
+
+int main() {
+    assert(issame(get_positive({}), {})); 
+    return 0;
 }
