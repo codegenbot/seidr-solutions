92dc0f9c5229a7e5487dc7f85426098d69264f00
beep boop <githubbot@vadim.me>
2024-07-24 05:01:04 +0200

    Solution 1 of CPP/71, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xnp0qg28 Prompt: Complete the following code given the task description and function signature.

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,12 +1,12 @@
-```c++
 #include <cmath>
 
 using namespace std;
 
 float triangle_area(float a, float b, float c) {
-    if ((a + b > c) && (a + c > b) && (b + c > a)) { // Check if the three sides form a valid triangle
-        return 0.5 * sqrt(pow(a, 2) * pow(b, 2) - pow(c, 2)); // Calculate the area using Heron's formula
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        float s = (a + b + c) / 2.0;
+        return round((sqrt(s * (s - a) * (s - b) * (s - c))) * 100.0) / 100.0;
     } else {
-        return -1; // Return -1 if the three sides do not form a valid triangle
+        return -1.0f;
     }
 }
