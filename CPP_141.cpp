2c6d5a57bd27c0fd638f87b3603a1d4a18b97093
beep boop <githubbot@vadim.me>
2024-07-24 11:49:34 +0200

    Solution 28 of CPP/141, ratio 0.9230769230769231 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/42gtbgjv Prompt: 53a93441-38fe-4dca-87bf-6a3a7eeda0df.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string file_name_check(string file_name){       | ^~~~~~ In file included from 53a93441-38fe-4dca-87bf-6a3a7eeda0df.cpp:26: 53a93441-38fe-4dca-87bf-6a3a7eeda0df.cpp: In function ‘int main()’: 53a93441-38fe-4dca-87bf-6a3a7eeda0df.cpp:28:13: error: ‘file_name_check’ was not  declared in this scope    28 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 std::string file_name_check(std::string file_name){
     int digit_count = 0;
     bool found_dot = false;
-    std::string before_dot;
+    string before_dot;
 
     for(int i=0; i<file_name.size(); i++){
         if(isdigit(file_name[i])){
@@ -18,9 +19,9 @@ std::string file_name_check(std::string file_name){
     }
 
     if(before_dot.empty()) return "No";
-    if(found_dot && (file_name.find("txt") == std::string::npos &&
-                     file_name.find("exe") == std::string::npos &&
-                     file_name.find("dll") == std::string::npos)) 
+    if(found_dot && (file_name.find("txt") == string::npos &&
+                     file_name.find("exe") == string::npos &&
+                     file_name.find("dll") == string::npos)) 
         return "No";
 
     return "Yes";
