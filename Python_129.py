d70a87c4aaaf0a82f41a551679f072ab353e6f55
beep boop <githubbot@vadim.me>
2024-07-24 15:50:16 +0200

    Solution 30 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/z12svfy3 Prompt: 0 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/31928adc-ef4e-4758-9358-f01938aa6 ff5.py", line 26, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/31928adc-ef4e-4758-9358-f01938aa6 ff5.py", line 23, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,21 +1,20 @@
-```
 def minPath(grid, k):
     N = len(grid)
     queue = [(0, 0, [])]
-    res = float('inf')
+    res = float("inf")
+    path_set = set()
     while queue:
         row, col, path = queue.pop(0)
         if len(path) == k:
-            cost = sum(map(int, path))
-            if cost < res:
-                res = cost
+            current_sum = sum(map(int, path))
+            if current_sum < res:
+                res = current_sum
+                path_set = {tuple(path)}
+            elif current_sum == res:
+                path_set.add(tuple(path))
         else:
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nr, nc = row + dr, col + dc
                 if 0 <= nr < N and 0 <= nc < N:
                     queue.append((nr, nc, path + [grid[nr][nc]]))
-    return res
-
-grid = [["1", "0", "1"], ["0", "0", "0"], ["1", "0", "1"]]
-k = 3
-print(minPath(grid, k))
+    return [[i for i in p] for p in path_set]
