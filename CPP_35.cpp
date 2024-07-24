0548aa5aa80bd99b46cb8cf8fcd4ddb9b8ea1276
beep boop <githubbot@vadim.me>
2024-07-24 19:04:10 +0200

    Solution 51 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nwhbfoew Prompt:       | ^~~~~~~~~~~~~ 3293a548-653f-47ad-8e17-13ba26126d6b.cpp:8:9: warning: empty parentheses were di sambiguated as a function declaration [-Wvexing-parse]     8 | int main(){       |         ^~ 3293a548-653f-47ad-8e17-13ba26126d6b.cpp:8:9: note: remove parentheses to defaul t-initialize a variable     8 | int main(){       |         ^~       |         -- 3293a548-653f-47ad-8e17-13ba26126d6b.cpp:8:9: note: or replace parentheses with braces to value-initialize a variable 3293a548-653f-47ad-8e17-13ba26126d6b.cpp:8:11: error: a function-definition is n ot allowed here before ‘{’ token     8 | int main(){       |           ^ 3293a548-653f-47ad-8e17-13ba26126d6b.cpp:10:2: error: expected ‘}’ at end of inp ut    10 | }       |  ^ 3293a548-653f-47ad-8e17-13ba26126d6b.cpp:2:8: note: to match this ‘{’     2 | main() {       |        ^

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,5 +1,4 @@
-int main() {
+int main() { 
     int max_val = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
-    int abs_max_val = std::abs(*std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}));
+    int abs_max_val = std::abs(*std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})); 
     return 0;
-}
