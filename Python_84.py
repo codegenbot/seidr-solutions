cd7df092e74d4a6f4fd648a86f4e321bc15b9cd5
beep boop <githubbot@vadim.me>
2024-07-24 03:40:07 +0200

    Solution 22 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a9a86c0-88d1-47d1-9408-ddaa2e9c3 861.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,12 +1,8 @@
 def solve(K=None):
     if K is None:
         N = int(input("Enter another number: "))
-        binary = ''
-        while N > 0:
-            binary = str(N % 2) + binary
-            N //= 2
-        return binary
+        return ''.join('1' if i % (2**k) < 2**(k-1) else '0' for k in range(1, N.bit_length()+1))
     N = int(input("Enter a number: ") if K is None else K)
     if N < 1:
-        return '0'
+        return str(0)
     return str(K * ((N - 1) // K))
