75b5c9e635ee88c3832d8d76aa3029e330655a52
beep boop <githubbot@vadim.me>
2024-07-24 13:27:47 +0200

    Solution 92 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/3t3j46u9 Prompt: )’    44 | int main(){       |     ^~~~ c1b01234-e017-4806-81ef-620032b0a37e.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from c1b01234-e017-4806-81ef-620032b0a37e.cpp:35: c1b01234-e017-4806-81ef-620032b0a37e.cpp: In function ‘int main()’: c1b01234-e017-4806-81ef-620032b0a37e.cpp:45:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    45 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~ c1b01234-e017-4806-81ef-620032b0a37e.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ c1b01234-e017-4806-81ef-620032b0a37e.cpp:36:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    36 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> order_by_points(vector<int> nums) {
     sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
@@ -21,7 +17,7 @@ vector<int> order_by_points(vector<int> nums) {
             temp_b /= 10;
         }
         if (sum_a == sum_b) {
-            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+            return a < b;
         }
         return sum_a < sum_b;
     });
@@ -29,5 +25,5 @@ vector<int> order_by_points(vector<int> nums) {
 }
 
 int main() {
-    assert(issame({-76, -21, 0, 4, 23, 6, 6}, order_by_points({0, 6, 6, -76, -21, 23, 4})));
+    assert(order_by_points({0, 6, 6, -76, -21, 23, 4}) == vector<int>{-76, -21, 0, 4, 23, 6, 6});
 }
