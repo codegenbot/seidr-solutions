6bd4d85246242605a4121c96d621522834e52151
beep boop <githubbot@vadim.me>
2024-07-24 13:05:55 +0200

    Solution 49 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt: 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp:6:28: error: ‘a’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                            ^ 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp:6:30: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp:6:37: error: ‘string’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp:6:44: error: ‘b’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp:5: 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp: In function ‘int main()’: 9c7ecd3c-b746-4095-ad15-ca6016613092.cpp:15:20: error: ‘sorted_list_sum’ was not  declared in this scope    15 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -6,15 +6,15 @@
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     std::vector<std::string> a_sorted = a;
     std::vector<std::string> b_sorted = b;
-    
+
     std::sort(a_sorted.begin(), a_sorted.end());
     std::sort(b_sorted.begin(), b_sorted.end());
-    
+
     return a_sorted == b_sorted;
 }
 
 int main() {
     assert(issame({"aaaa", "bbbb", "dd", "cc"}, {"cc", "dd", "aaaa", "bbbb"}));
-    
+
     return 0;
 }
