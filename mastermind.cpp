d26ab0ecba8a4a464014cd8cba8e621893caf29e
beep boop <githubbot@vadim.me>
2024-07-24 04:31:15 +0200

    Solution 18 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/yc2w6m01 Prompt: in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 0d2b9782-3ad2-4fb7-abb7-8831c2f3b2c9.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 0d2b9782-3ad2-4fb7-abb7-8831c2f3b2c9.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^ 0d2b9782-3ad2-4fb7-abb7-8831c2f3b2c9.cpp:36:15: error: ‘string’ was not declared  in this scope    36 | bool contains(string s, char c) {       |               ^~~~~~ 0d2b9782-3ad2-4fb7-abb7-8831c2f3b2c9.cpp:36:25: error: expected primary-expressi on before ‘char’    36 | bool contains(string s, char c) {       |                         ^~~~ 0d2b9782-3ad2-4fb7-abb7-8831c2f3b2c9.cpp:36:31: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool contains(string s, char c) {       |                               ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,3 +1,6 @@
+#include <map>
+#include <string>
+
 int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
@@ -5,6 +8,7 @@ int mastermind(std::string code, std::string guess) {
     map<char, int> code_count;
     map<char, int> guess_count;
 
+    // Count occurrences of each character in the code and guess
     for (char c : code) {
         code_count[c]++;
     }
@@ -12,14 +16,48 @@ int mastermind(std::string code, std::string guess) {
         guess_count[c]++;
     }
 
-    for (int i = 0; i < 4; i++) {
+    // Count black pegs (correct color, correct place)
+    int i = 0;
+    while (i < 4) {
         if (code[i] == guess[i]) {
             black++;
-        } else if (guess_count[code[i]] > 0) {
-            guess_count[code[i]]--;
-            white++;
+            code_count[guess[i]]--;
+            guess_count[guess[i]]--;
+            i++;
+        } else {
+            i++;
+        }
+    }
+
+    // Count remaining characters in the code that are not in the guess
+    for (auto& pair : code_count) {
+        while (pair.second > 0 && !contains(guess, pair.first)) {
+            pair.second--;
+        }
+    }
+
+    // Count white pegs (correct color, wrong place)
+    for (int i = 0; i < 4; i++) {
+        if (code[i] != guess[i]) {
+            for (int j = 0; j < 6; j++) {
+                char c = 'A' + j;
+                if (guess[j] == c && code_count[c] > 0) {
+                    white++;
+                    code_count[c]--;
+                    break;
+                }
+            }
         }
     }
 
     return black * 4 + white;
+}
+
+bool contains(std::string s, char c) {
+    for (char ch : s) {
+        if (ch == c) {
+            return true;
+        }
+    }
+    return false;
 }
