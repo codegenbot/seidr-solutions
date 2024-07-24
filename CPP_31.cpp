f4bf657bf13c1d6195f9ab9e9f1918f42ff448de
beep boop <githubbot@vadim.me>
2024-07-24 07:42:33 +0200

    Solution 1 of CPP/31, ratio 0.46153846153846156 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4tge55yi Prompt: Complete the following code given the task description and function signature.

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -3,7 +3,7 @@
 bool is_prime(long long n) {
     if (n <= 1)
         return false;
-    for (long long i = 2; i <= sqrt(n); i++) {
+    for (long long i = 2; i * i <= n; i++) {
         if (n % i == 0)
             return false;
     }
