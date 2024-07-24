af7d62b442f1cb7432b7ba8baa57ee04b679473e
beep boop <githubbot@vadim.me>
2024-07-24 06:55:03 +0200

    Solution 13 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/4vklj0xo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/15ae1178-7136-4b0a-9225-d123372be 57e.py", line 2     return sum(int(x) for x in str(sum(map(lambda x: x*2 if (i:=len(card)-1)//2! =i else x, card)) if int(x)>9 then int(x)-9 else int(x)))                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: expected 'else' after 'if' expression

--- luhn.py
+++ luhn.py
@@ -1,2 +1,2 @@
 def luhn(card):
-    return sum(int(d) for d in str(sum(int(x) if i%2 else int(x)*2 - 9 if (int(x)*2) > 9 else int(x)*2 for i,x in enumerate(reversed(card))))
+    return sum(int(d) for d in str(sum(int(x) if i%2 else int(x)*2 if int(x)*2>9 else int(x)*2 for i,x in enumerate(map(str,card[::-1])))))
