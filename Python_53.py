ade8af80794de29b368957ca923351ab0596f8a0
beep boop <githubbot@vadim.me>
2024-07-24 02:19:26 +0200

    Solution 6 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2yrlv8y8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/58e5b16e-0bf9-43d1-80c0-a09665148 cd6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -2,8 +2,7 @@ def add(x: int, y: int):
     while True:
         try:
             return x + y
-        except ValueError:
-            if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
-                print("Both inputs must be numbers")
-                x = float(input("Enter a number for x: "))
-                y = float(input("Enter a number for y: "))
+        except TypeError:
+            print("Both inputs must be numbers")
+            x = float(input("Enter a number for x: "))
+            y = float(input("Enter a number for y: "))
