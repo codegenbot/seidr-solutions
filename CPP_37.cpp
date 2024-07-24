4aec410beca6a78fbd46d3f9068d3846a947fbdd
beep boop <githubbot@vadim.me>
2024-07-24 18:15:41 +0200

    Solution 2 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cvfj3xyj Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9bee23e1-25d7-4f54-8a9f-225f8a4a2042.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 9bee23e1-25d7-4f54-8a9f-225f8a4a2042.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 9bee23e1-25d7-4f54-8a9f-225f8a4a2042.cpp:22: 9bee23e1-25d7-4f54-8a9f-225f8a4a2042.cpp: In function ‘int main()’: 9bee23e1-25d7-4f54-8a9f-225f8a4a2042.cpp:32:20: error: ‘sort_even’ was not decla red in this scope    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 9bee23e1-25d7-4f54-8a9f-225f8a4a2042.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,15 +1,20 @@
+#include <algorithm>
+#include <vector>
+
+bool issame(vector<float> a, vector<float> b);
+
 vector<float> sort_even(vector<float> l) {
     vector<float> result;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            vector<float> evenVals;
+            vector<float> even;
             for (int j = 0; j < l.size(); j++) {
                 if (j % 2 == 0) {
-                    evenVals.push_back(l[j]);
+                    even.push_back(l[j]);
                 }
             }
-            sort(evenVals.begin(), evenVals.end());
-            result.push_back(evenVals[0]);
+            sort(even.begin(), even.end());
+            result.push_back(even[0]);
         } else {
             result.push_back(l[i]);
         }
