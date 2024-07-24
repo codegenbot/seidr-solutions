4f90de431b622cde4613e903cae1792e6a7ca95d
beep boop <githubbot@vadim.me>
2024-07-24 11:34:55 +0200

    Solution 52 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/qmupjb4k Prompt: ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2bb0740e-f033-4666-bb79-9659dbdb5809.cpp:21:66: error: invalid conversion from ‘ __gnu_cxx::__alloc_traits<std::allocator<char>, char>::value_type*’ {aka ‘char*’ } to ‘int’ [-fpermissive]    21 | esult += capitalizeNext ? toupper(currentWord[0]) + tolower(&currentWord [1]) : currentWord; In file included from /usr/include/c++/11/cctype:42,                  from /usr/include/c++/11/bits/localefwd.h:42,                  from /usr/include/c++/11/ios:41,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 2bb0740e-f033-4666-bb79-9659dbdb5809.cpp:1: /usr/include/ctype.h:122:25: note:   initializing argument 1 of ‘int tolower(int )’   122 | extern int tolower (int __c) __THROW;       |                     ~~~~^~~ 2bb0740e-f033-4666-bb79-9659dbdb5809.cpp:21:30: error: operands to ‘?:’ have dif ferent types ‘int’ and ‘std::string’ {aka ‘std::__cxx11::basic_string<char>’}    21 |     result += capitalizeNext ? toupper(currentWord[0]) + tolower(&curren tWord[1]) : currentWord;       |               ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -8,11 +8,7 @@ std::string kebabToCamel(const std::string& str) {
 
     for (char c : str) {
         if (c == '-') {
-            if(capitalizeNext) {
-                result += toupper(currentWord[0]) + tolower(&currentWord[1]);
-            } else {
-                result += currentWord + " ";
-            }
+            result += capitalizeNext ? toupper(currentWord[0]) + tolower(&currentWord[1]) : currentWord + " ";
             currentWord.clear();
             capitalizeNext = true;
         } else {
@@ -22,11 +18,7 @@ std::string kebabToCamel(const std::string& str) {
     }
 
     // Add the last word
-    if(capitalizeNext) {
-        result += toupper(currentWord[0]) + tolower(&currentWord[1]);
-    } else {
-        result += currentWord;
-    }
+    result += capitalizeNext ? toupper(currentWord[0]) + std::tolower(currentWord.substr(1)) : currentWord;
 
     return result;
 }
