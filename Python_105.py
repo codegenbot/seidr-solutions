54ad5fb1325e937951dfcaf1db9ac3adf2e62c4a
beep boop <githubbot@vadim.me>
2024-07-24 05:55:45 +0200

    Solution 75 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d960d08d-48f0-4d8d-8b25-aa10db3eb ee6.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_105.py
+++ Python_105.py
@@ -3,18 +3,19 @@ def check_sum_in_range():
     while True:
         try:
             num1, num2, num3 = [int(input(f"Enter {i+1} number: ")) for i in range(3)]
-            if any(i < 1 or i > 18 for i in (num1, num2, num3)) or set([num1, num2, num3]) != {num1, num2, num3}:
-                print("Invalid input. Please enter numbers from 1 to 18 and three unique numbers.")
+            if min(num1, num2, num3) < 1 or max(num1, num2, num3) > 18:
+                print("Invalid input. Please enter numbers from 1 to 18.")
+            elif len(set([num1, num2, num3])) != 3:
+                print("Invalid input. Please enter unique numbers.")
             else:
                 break
         except ValueError:
             print("Invalid input. Please enter a number.")
 
     total_sum = sum([num1, num2, num3])
-    if total_sum in range(1, 19):
+    if 17 < total_sum < 55:
         return "Correct"
     else:
         return "Incorrect"
-
 
 print(check_sum_in_range())
