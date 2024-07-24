ab3d195584867b19517f616e34996ff2ae612248
beep boop <githubbot@vadim.me>
2024-07-24 13:04:00 +0200

    Solution 7 of Python/40, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4apaev7t Prompt: Complete the following code given the task description and function signature.

--- Python_40.py
+++ Python_40.py
@@ -1,15 +1,7 @@
-```
 def triples_sum_to_zero(l: list):
-    l.sort()
-    for i in range(len(l) - 2):
-        left = i + 1
-        right = len(l) - 1
-        while left < right:
-            total = l[i] + l[left] + l[right]
-            if total < 0:
-                left += 1
-            elif total > 0:
-                right -= 1
-            else:
+    l = set(l)
+    for i in l:
+        for j in l:
+            if i + j in l and i != j and j != i:
                 return True
     return False
