d745a116770133b70125e2fc76aa5683563de8bc
beep boop <githubbot@vadim.me>
2024-07-24 02:52:19 +0200

    Solution 26 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v4oavres Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/119c5647-9cb6-431a-b088-831bf075d 029.py", line 22, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/119c5647-9cb6-431a-b088-831bf075d 029.py", line 19, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,12 +1,13 @@
-```Python
+```
 def fruit_distribution(s, n):
-    s = s.replace("apples", "1 apple").replace("oranges", "1 orange")
+    s = s.replace("apples", "apple").replace("oranges", "orange")
     total_fruit = 0
     for word in s.lower().split():
-        if 'apple' in word or 'orange' in word:
-            total_fruit += int(''.join(filter(str.isdigit, word)))
-    max_per_box = min(total_fruit // (n + 1), n)
-    remaining_fruits = total_fruit - (max_per_box * (n + 1))
+        if word == "apple" or word == "orange":
+            total_fruit += 1
+
+    max_per_box = min(total_fruit // n, n)
+    remaining_fruits = total_fruit - (max_per_box * n)
 
     return (
         int(-(-total_fruit / float(max_per_box)) + 0.5)
