e2077e59dc1d834cd434999e4ab14f0b25f31968
beep boop <githubbot@vadim.me>
2024-07-24 03:46:41 +0200

    Solution 9 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt: ['One', 'Two', 'Three', 'Five', 'Six', 'Eight'] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96bf4033-aff7-4d4a-92ae-98329cd6c bfa.py", line 16, in <module>     check(by_length)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96bf4033-aff7-4d4a-92ae-98329cd6c bfa.py", line 13, in check     assert by_length([9, 4, 8]) == ["Nine", "Eight", "Four"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_105.py
+++ Python_105.py
@@ -1,3 +1,27 @@
+name_dict = {
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine",
+    10: "Ten",
+    11: "Eleven",
+    12: "Twelve",
+    13: "Thirteen",
+    14: "Fourteen",
+    15: "Fifteen",
+    16: "Sixteen",
+    17: "Seventeen",
+    18: "Eighteen",
+    19: "Nineteen",
+    20: "Twenty",
+}
+
+
 def by_length(arr):
-    result = sorted([str(i) for i in arr if 1 <= i <= 9], key=len)
-    return [name_dict[int(i)] for i in result]
+    result = sorted([str(i) for i in arr if 1 <= i <= 19], key=len)
+    return [name_dict[i] for i in result]
