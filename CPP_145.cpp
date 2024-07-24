6c668c90be3a7661450c6c520687f08621953183
beep boop <githubbot@vadim.me>
2024-07-24 12:24:39 +0200

    Solution 37 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/3t3j46u9 Prompt: )’    44 | int main(){       |     ^~~~ 6048bc3b-f191-4b86-b2e2-246e18c344a1.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 6048bc3b-f191-4b86-b2e2-246e18c344a1.cpp:35: 6048bc3b-f191-4b86-b2e2-246e18c344a1.cpp: In function ‘int main()’: 6048bc3b-f191-4b86-b2e2-246e18c344a1.cpp:45:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    45 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~ 6048bc3b-f191-4b86-b2e2-246e18c344a1.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 6048bc3b-f191-4b86-b2e2-246e18c344a1.cpp:36:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    36 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -28,4 +28,6 @@ vector<int> order_by_points(vector<int> nums) {
     return nums;
 }
 
-assert(issame({-76, -21, 0, 4, 23, 6, 6}, order_by_points({0, 6, 6, -76, -21, 23, 4}));
+int main() {
+    assert(issame({-76, -21, 0, 4, 23, 6, 6}, order_by_points({0, 6, 6, -76, -21, 23, 4})));
+}
