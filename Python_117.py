9647de851d62b7c3c7197086dbcf871157bde7e1
beep boop <githubbot@vadim.me>
2024-07-24 20:12:00 +0200

    Solution 8 of Python/117, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/85ggkn6m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0fef9e75-8fb4-42df-8674-b1d65d850 7f5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_117.py
+++ Python_117.py
@@ -1,9 +1,10 @@
-```
 def select_words(s, n):
     words = s.split()
     result = []
     for word in words:
-        consonants = sum(1 for char in word if not char.isalpha() or char.lower() not in 'aeiou')
+        consonants = sum(
+            1 for char in word if not char.isalpha() or char.lower() not in "aeiou"
+        )
         if consonants == n:
             result.append(word)
     return result
