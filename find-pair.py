874dbd15c921ec338823ba03744d22f1798d15ae
beep boop <githubbot@vadim.me>
2024-07-24 13:49:43 +0200

    Solution 5 of find-pair, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/n6eww5ge Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1eff6d29-0c5f-456f-a93f-d84dabe20 df0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- find-pair.py
+++ find-pair.py
@@ -1,10 +1,11 @@
 def find_pair(n, nums, target):
-    seen = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return str(num) + "\n" + str(complement)
-        seen[num] = i
+    seen = set()
+    for num in nums:
+        if target - num in seen:
+            return str(target - num) + "\n" + str(num)
 
+n = int(input())
+nums = [int(x) for x in input().split()]
+target = int(input())
 
-print(find_pair(int(input()), [int(x) for x in input().split()], int(input())))
+print(find_pair(n, nums, target))
