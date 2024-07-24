2cd317a17ca6661aff9d48898a4e727f5befe766
beep boop <githubbot@vadim.me>
2024-07-24 04:31:29 +0200

    Solution 63 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/531to4j1 Prompt: 7e8cf1c8-9915-4547-8b34-591692113759.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string filp_case(string str){       | ^~~~~~ In file included from 7e8cf1c8-9915-4547-8b34-591692113759.cpp:14: 7e8cf1c8-9915-4547-8b34-591692113759.cpp: In function ‘int main()’: 7e8cf1c8-9915-4547-8b34-591692113759.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <algorithm>
-#include <cctype>
-#include <string>
-
 std::string filp_case(std::string str) {
     std::string result = "";
     for(int i=0; i<str.length(); i++){
@@ -14,10 +9,4 @@ std::string filp_case(std::string str) {
             result += str[i];
     }
     return result;
-}
-
-int main() {
-    assert(filp_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
-    // You can test your function here with different inputs.
-    return 0;
 }
