6155058762e98a12c63b7858cd3c8ef26e531bad
beep boop <githubbot@vadim.me>
2024-07-24 04:08:14 +0200

    Solution 41 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k4a5f9yk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e12c616f-1b42-4882-b437-fdf085764 42c.py", line 25, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e12c616f-1b42-4882-b437-fdf085764 42c.py", line 21, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,14 @@
-```
 def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
             if char.isupper():
-                result += chr((ord(char) - 65 + 3) % 26 + 65)
-            else:
+                result += chr((ord(char.lower()) - 97 + 13) % 26 + 65)
+            elif char.islower():
                 result += chr((ord(char) - 97 + 2) % 26 + 97)
         else:
-            result += char
+            if char == " ":  
+                result += " "
+            else:  
+                result += char
     return result
