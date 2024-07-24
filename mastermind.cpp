b59a1d49b04bdb4980329008e9e5226f0609c55f
beep boop <githubbot@vadim.me>
2024-07-24 04:34:52 +0200

    Solution 20 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/yc2w6m01 Prompt: 9c1f03e1-702f-4415-9836-badd1603f285.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 9c1f03e1-702f-4415-9836-badd1603f285.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 9c1f03e1-702f-4415-9836-badd1603f285.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,44 +1,25 @@
-#include <map>
 #include <string>
 
-int mastermind(std::string code, std::string guess) {
-    int white = 0;
-    int black = 0;
+int mastermind(string code, string guess) {
+    int blackPegs = 0;
+    int whitePegs = 0;
 
-    map<char, int> code_count;
-    map<char, int> guess_count;
-
-    // Count occurrences of each character in the code and guess
-    for (char c : code) {
-        code_count[c]++;
-    }
-    for (char c : guess) {
-        guess_count[c]++;
-    }
-
-    // Count black pegs (correct color, correct place)
     for (int i = 0; i < 4; i++) {
         if (code[i] == guess[i]) {
-            black++;
+            blackPegs++;
+        } else {
+            bool found = false;
+            for (int j = 0; j < 4; j++) {
+                if (guess[j] == code[i] && i != j) {
+                    whitePegs++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                blackPegs++;
+            }
         }
     }
 
-    // Count white pegs (correct color, wrong place)
-    for (int c : code) {
-        if (guess_count[c] > 0) {
-            white++;
-            guess_count[c]--;
-        }
-    }
-
-    return black * 4 + white;
-}
-
-bool contains(std::string s, char c) {
-    for (char ch : s) {
-        if (ch == c) {
-            return true;
-        }
-    }
-    return false;
-}
+    return blackPegs - whitePegs;
