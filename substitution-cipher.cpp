32ab6694251fb20ec3f7c01176703dfe6b639159
beep boop <githubbot@vadim.me>
2024-07-24 12:47:10 +0200

    Solution 32 of substitution-cipher, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/36y888ms Prompt: n this scope     2 |     string cipher1, cipher2, message;       |     ^~~~~~ 3f6167bc-ee4c-4747-8103-64cb6a2c9597.cpp:3:5: error: ‘cin’ was not declared in t his scope     3 |     cin >> cipher1 >> cipher2 >> message;       |     ^~~ 3f6167bc-ee4c-4747-8103-64cb6a2c9597.cpp:3:12: error: ‘cipher1’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |            ^~~~~~~ 3f6167bc-ee4c-4747-8103-64cb6a2c9597.cpp:3:23: error: ‘cipher2’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |                       ^~~~~~~ 3f6167bc-ee4c-4747-8103-64cb6a2c9597.cpp:3:34: error: ‘message’ was not declared  in this scope     3 |     cin >> cipher1 >> cipher2 >> message;       |                                  ^~~~~~~ 3f6167bc-ee4c-4747-8103-64cb6a2c9597.cpp:11:5: error: ‘cout’ was not declared in  this scope    11 |     cout << message;       |     ^~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -5,19 +5,15 @@ using namespace std;
 int main() {
     string cipher1, cipher2, message;
     cin >> cipher1 >> cipher2 >> message;
-    
-    for (char &c : message) {
-        size_t pos1 = cipher1.find(c);
-        size_t pos2 = cipher2.find(c);
-        
-        if (pos1 != string::npos) {
-            c = cipher2[pos1];
-        } else if (pos2 != string::npos) {
-            c = cipher1[pos2];
+
+    for (char& c : message) {
+        size_t index = cipher1.find(c);
+        if (index != string::npos) {
+            c = cipher2[index];
         }
     }
-    
-    cout << message << endl;
-    
+
+    cout << message;
+
     return 0;
 }
