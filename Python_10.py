735417caf15d5f38f8337c95881502cf36d038d6
beep boop <githubbot@vadim.me>
2024-07-24 17:43:01 +0200

    Solution 46 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/g5a98x2j Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf5a288f-f38a-4210-854a-4723bf392 f52.py", line 15, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cf5a288f-f38a-4210-854a-4723bf392 f52.py", line 12, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -7,5 +7,5 @@ def make_palindrome(string: str) -> str:
         return string
     for i in range(len(string), 0, -1):
         if is_palindrome(string[:i]):
-            return string + string[i - 1 :: -1]  # Update the slicing indices here
+            return string + string[i - 1 :: -1]
     return string + string[:-1][::-1]
