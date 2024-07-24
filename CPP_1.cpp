44c37f50031905efc703e9323e45620e033c6372
beep boop <githubbot@vadim.me>
2024-07-24 23:55:24 +0200

    Solution 2 of CPP/1, ratio 0.25 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w6e0bhrd Prompt: Complete the following code given the task description and function signature.

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -24,15 +23,4 @@ vector<string> separate_paren_groups(string paren_string) {
     }
 
     return result;
-}
-
-int main() {
-    string paren_string = "( ) (( )) (( )( ))";
-    vector<string> groups = separate_paren_groups(paren_string);
-
-    for (string group : groups) {
-        cout << group << endl;
-    }
-
-    return 0;
 }
