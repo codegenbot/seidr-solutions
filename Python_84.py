140c2abbcd14556f2ed214826ff2424cb1cea520
beep boop <githubbot@vadim.me>
2024-07-24 04:17:34 +0200

    Solution 49 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/95580381-b976-4e02-9c63-d654fdf37 007.py", line 23, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/95580381-b976-4e02-9c63-d654fdf37 007.py", line 20, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,17 +1,3 @@
 ```
-import math
-
-
 def solve(K):
-    if K < 2:
-        return "1"
-    if K == 2:
-        return "10"
-    result = ""
-    while K > 0:
-        if (K & 1) == 1:
-            result = "1" + result
-        else:
-            result = "0" + result
-        K >>= 1
-    return result
+    return bin(K)[2:]
