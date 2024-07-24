1af60b6258d95bb69fb534c593496cefddb67900
beep boop <githubbot@vadim.me>
2024-07-24 05:30:16 +0200

    Solution 32 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt: Bigger Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90d77511-05ee-44c4-a392-6930ecf84 dce.py", line 49, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/90d77511-05ee-44c4-a392-6930ecf84 dce.py", line 19, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -1,2 +1,14 @@
-result = sum_squares([1, 2, 3, 11, 12])
-print(check(result))
+def sum_squares(numbers):
+    return sum(i**2 for i in numbers)
+
+
+def check(result):
+    if result > 100:
+        return "Bigger"
+    elif result == 100:
+        return "Equal"
+    else:
+        return "Smaller"
+
+
+print(check(sum_squares([1, 2, 3, 11, 12])))
