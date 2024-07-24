792ef1a6eb8e7550d3574d6722c9bf686fce87aa
beep boop <githubbot@vadim.me>
2024-07-25 01:00:15 +0200

    Solution 43 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fz47n6iq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/489c24aa-429a-47e3-a666-ff6a039ed 38e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,6 +1,2 @@
-def odd_sum(lst):
-    total = 0
-    for num in lst:
-        if num % 2 != 0:
-            total += num
-    return total
+def count_odd_numbers_in_uneven_indices(lst):
+    return sum(i for i in lst[1::2] if i % 2 != 0)
