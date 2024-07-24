5e3ff15e1d4e94685d30fa5fc9b88093d464cedf
beep boop <githubbot@vadim.me>
2024-07-24 08:07:33 +0200

    Solution 7 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/ln0czjdy Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,27 +1,52 @@
+#include <iostream>
 #include <string>
-using namespace std;
 
-string spinWords(string str) {
-    string result = "";
-    int wordStart = 0;
-    for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i] == ' ') {
-            string word = str.substr(wordStart, i - wordStart);
-            if (word.length() >= 5) {
-                result += reverse(word) + " ";
-            } else {
-                result += word + " ";
-            }
-            wordStart = i + 1;
+std::string spinWords(std::string str) {
+    std::string result = "";
+    std::string word;
+    
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ') {
+            result += word + " ";
+            word = "";
+        } else {
+            word += str[i];
         }
     }
-    return result.substr(0, result.length() - 1);
+    
+    result += word;
+    
+    std::string newResult = "";
+    for (int i = 0; i < result.length(); i++) {
+        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
+            int j = i;
+            while ((j + 1 < result.length()) && (result[j + 1] == ' ')) {
+                j++;
+            }
+            if (j - i >= 5) {
+                for (int k = j; k > i; k--) {
+                    newResult += result[k - 1];
+                }
+                newResult += " ";
+                i = j;
+            } else {
+                newResult += result[i];
+                for (i++; i < j; i++) {
+                    newResult += result[i];
+                }
+            }
+        } else {
+            newResult += result[i];
+        }
+    }
+    
+    return newResult;
 }
 
-string reverse(string str) {
-    string rev = "";
-    for (int i = str.length() - 1; i >= 0; i--) {
-        rev += str[i];
-    }
-    return rev;
+int main() {
+    std::cout << spinWords("a") << std::endl;
+    std::cout << spinWords("this is a test") << std::endl;
+    std::cout << spinWords("this is another test") << std::endl;
+    std::cout << spinWords("hi") << std::endl;
+    return 0;
 }
