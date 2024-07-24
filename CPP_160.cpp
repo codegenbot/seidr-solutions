8902f2fe760a626a6f3104cb44c3baa3b1b8ee25
beep boop <githubbot@vadim.me>
2024-07-24 17:06:01 +0200

    Solution 40 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rec3ih7w Prompt: Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:461:34:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const std::__cxx11::basic_string<char, std::char_traits<char>, std ::allocator<char> >&}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::all ocator<std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::vector<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = std::__cxx11::basic_stri ng<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std::vecto r<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<char>]’ d2d77f70-4a9f-4721-bf29-f4dd2ca09375.cpp:17:33:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,94 +1,95 @@
+```cpp
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-vector<string> splitOperator(vector<string>& expressions) {
-    vector<string> operators;
+vector<string> split(string str) {
+    vector<string> result;
     string temp;
 
-    for (int i = 0; i < expressions.size(); i++) {
-        if (expressions[i] == "+-*/" || expressions[i] == "42" || expressions[i] == "47") {
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ') {
             if (!temp.empty()) {
-                operators.push_back(temp);
+                result.push_back(temp);
                 temp = "";
             }
-            operators.push_back(expressions[i]);
         } else {
-            temp += expressions[i];
+            temp += str[i];
         }
     }
     if (!temp.empty()) {
-        operators.push_back(temp);
+        result.push_back(temp);
     }
-
-    return operators;
-}
-
-vector<string> splitExpressions(vector<string>& input) {
-    vector<string> expressions;
-
-    for (int i = 0; i < input.size(); i++) {
-        if (input[i] == "42" || input[i] == "47") {
-            continue;
+    for (int i = 0; i < result.size(); i++) {
+        if (result[i].empty()) {
+            result.erase(result.begin() + i);
+            i--; 
+        } else if (result[i] == "") { 
+            result.erase(result.begin() + i);
+            i--;
         }
-        expressions.push_back(input[i]);
     }
-
-    return expressions;
+    return result;
 }
 
 double do_algebra(vector<string> expressions) {
-    double result = 0.0;
+    double result = 0;
+    string operators = expressions[2];
 
-    vector<string> op = splitOperator(expressions);
-
-    if (stod(op[2]) == 42) {
+    if (stod(operators) == 0) {
+        if (expressions[2][0] == '+') {
+            result = pow(stod(expressions[0]), stod(expressions[1]));
+        } else if (expressions[2][0] == '-') {
+            result = pow(stod(expressions[0]), -stod(expressions[1]));
+        }
+    } else if (stod(operators) == 42) { 
         if (expressions[1] == "0") {
-            if (op[0][0] == '+') {
-                result = pow(stod(expressions[0]), stod(expressions[1]));
-            } else if (op[0][0] == '-') {
-                result = pow(stod(expressions[0]), -stod(expressions[1]));
+            if (expressions[2][0] == '+') {
+                result = stod(expressions[0]);
+            } else if (expressions[2][0] == '-') {
+                result = -stod(expressions[0]);
             }
         } else if (expressions[1] != "0") {
-            if (op[0][0] == '+') {
+            if (expressions[2][0] == '+') {
                 result = stod(expressions[0]) + stod(expressions[1]);
-            } else if (op[0][0] == '-') {
+            } else if (expressions[2][0] == '-') {
                 result = stod(expressions[0]) - stod(expressions[1]);
+            } else if (expressions[2][0] == '*') {
+                result = stod(expressions[0]) * stod(expressions[1]);
             }
         }
-    } else if (stod(op[2]) == 47) {
+    } else if (stod(operators) == 47) { 
         if (expressions[1] == "0") {
-            if (op[0][0] == '+') {
+            if (expressions[2][0] == '+') {
                 result = -stod(expressions[0]);
-            } else if (op[0][0] == '-') {
+            } else if (expressions[2][0] == '-') {
                 result = stod(expressions[0]);
             }
         } else if (expressions[1] != "0") {
-            if (op[0][0] == '+') {
+            if (expressions[2][0] == '+') {
                 result = stod(expressions[0]) + (-stod(expressions[1]));
-            } else if (op[0][0] == '-') {
+            } else if (expressions[2][0] == '-') {
                 result = stod(expressions[0]) - stod(expressions[1]);
             }
         }
     }
-
     return result;
 }
 
 int main() {
-    vector<string> input;
+    vector<string> expressions;
+    string input;
 
     cout << "Enter the expressions (space separated): ";
     getline(cin, input);
     if (!input.empty()) {
-        vector<string> expressions = splitExpressions(input);
+        expressions = split(input);
 
-        double output = do_algebra(expressions);
-        cout << "Result: " << output << endl;
-    }
-
+    double output = do_algebra({expressions[0], expressions[1], expressions[2]});
+    cout << "Result: " << output << endl;
     return 0;
 }
