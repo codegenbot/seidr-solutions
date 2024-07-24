4cfa49be682a7758354356cc9a2eaf7d15427f45
beep boop <githubbot@vadim.me>
2024-07-24 14:35:02 +0200

    Solution 32 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cnzbwpl6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f824e247-bc1d-44aa-ab4d-88bf3c5f8 aab.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f824e247-bc1d-44aa-ab4d-88bf3c5f8 aab.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,9 +2,10 @@ def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            result += chr((ord(char.lower()) - 97 + 3) % 26 + 97)
-        elif char.isspace():  
-            result += ' '
+            if char.isupper():
+                result += chr((ord(char) - 65 + 3) % 26 + 65)
+            else:
+                result += chr((ord(char) - 97 + 3) % 26 + 97)
         else:
             result += char
     return result
