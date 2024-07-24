56e6654ce95fe6c2bbfb738327ed2b694b84def0
beep boop <githubbot@vadim.me>
2024-07-24 05:58:20 +0200

    Solution 61 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/bder8du4 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,24 +1,54 @@
 int bowlingScore(string s) {
     int score = 0;
-    int currentFrame = 1;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '/') {
-            string str = s.substr(0, i);
-            s.erase(0, i+1);
-            int strike = (str[0] == 'X');
-            int spare = false;
-            for (int j = 0; j < str.length(); j++) {
-                if (isdigit(str[j])) {
-                    int pin = str[j] - '0';
-                    if (!strike) {
-                        score += pin;
-                        currentFrame++;
-                        if (currentFrame > 10) break;
-                    }
-                    else strike--;
-                }   
+    bool inFrame = false;
+    int currentFrame = 0;
+    int totalInFrame = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (isdigit(s[i])) {
+            int pin = s[i] - '0';
+            if (inFrame) {
+                totalInFrame += pin;
+                if (totalInFrame == 2) {
+                    inFrame = false;
+                    score += calculateScore(currentFrame, totalInFrame);
+                    currentFrame++;
+                    totalInFrame = 0;
+                }
+            } else {
+                totalInFrame = pin;
+                inFrame = true;
+            }
+        } else {
+            if (s[i] == '/') {
+                int firstPin = s[i - 1] - '0';
+                int secondPin = s[i + 1] - '0';
+                score += calculateScore(currentFrame, firstPin);
+                totalInFrame = secondPin;
+                inFrame = true;
+                currentFrame++;
             }
         }
     }
+
+    if (inFrame) {
+        score += calculateScore(10, totalInFrame);
+    } else {
+        for (int i = 0; i < currentFrame; i++) {
+            score += calculateScore(i + 1, totalInFrame);
+            totalInFrame = 0;
+        }
+    }
+
     return score;
+}
+
+int calculateScore(int frame, int pins) {
+    if (frame == 10) {
+        return 10 + pins;
+    } else if (pins >= 2) {
+        return 10;
+    } else {
+        return pins;
+    }
 }
