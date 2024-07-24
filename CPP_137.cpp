f4daab1c9cbbb20e2cf2ab12a3d28cd4fd5511f5
beep boop <githubbot@vadim.me>
2024-07-24 12:22:58 +0200

    Solution 76 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mycbubp5 Prompt:   +++ |+#include <cassert>     4 | 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp: At global scope: 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp:37: 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp: In function ‘int main()’: 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp:39:13: error: ‘boost’ has not been decl ared    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 631199bd-b4d4-47cc-ab0e-03eea75f7762.cpp:39:35: error: expected primary-expressi on before ‘>’ token    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,21 +2,23 @@
 #include <string>
 #include <iostream>
 #include <cassert>
+#include <boost/any.hpp>
 
 using namespace std;
+using namespace boost;
 
 std::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int int_a = std::any_cast<int>(a);
-        int int_b = std::any_cast<int>(b);
+        int int_a = boost::any_cast<int>(a);
+        int int_b = boost::any_cast<int>(b);
         return (int_a > int_b) ? int_a : int_b;
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float float_a = std::any_cast<float>(a);
-        float float_b = std::any_cast<float>(b);
+        float float_a = boost::any_cast<float>(a);
+        float float_b = boost::any_cast<float>(b);
         return (float_a > float_b) ? float_a : float_b;
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = std::any_cast<string>(a);
-        string str_b = std::any_cast<string>(b);
+        string str_a = boost::any_cast<string>(a);
+        string str_b = boost::any_cast<string>(b);
         if (str_a.find_first_of(".,") != string::npos) {
             str_a.erase(std::remove(str_a.begin(), str_a.end(), ','), str_a.end());
             str_a.replace(str_a.find('.'), 1, ",");
@@ -31,6 +33,6 @@ std::any compare_one(std::any a, std::any b) {
 }
 
 int main() {
-    assert(std::any_cast<string>(compare_one(string("1"), string("1"))) == "None");
+    assert(boost::any_cast<string>(compare_one(string("1"), 1)) == "None");
     return 0;
 }
