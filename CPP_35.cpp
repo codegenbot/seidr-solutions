4da518d6fac473ffba57d80bab6790e7e5b1ca24
beep boop <githubbot@vadim.me>
2024-07-24 18:39:21 +0200

    Solution 24 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nwhbfoew Prompt: e259aff8-7711-4de3-b180-7a72c14ce653.cpp: At global scope: e259aff8-7711-4de3-b180-7a72c14ce653.cpp:11:5: error: redefinition of ‘int main( )’    11 | int main(){       |     ^~~~ e259aff8-7711-4de3-b180-7a72c14ce653.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~ In file included from e259aff8-7711-4de3-b180-7a72c14ce653.cpp:10: e259aff8-7711-4de3-b180-7a72c14ce653.cpp: In function ‘int main()’: e259aff8-7711-4de3-b180-7a72c14ce653.cpp:12:17: error: ‘max_element’ was not dec lared in this scope; did you mean ‘std::max_element’?    12 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from e259aff8-7711-4de3-b180-7a72c14ce653.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: ‘std::max_element’ d eclared here   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,9 +1,9 @@
 #include <algorithm>
 #include <cmath>
+#include <assert.h>
 
+int max_val = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.begin(), {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.end());
+assert(std::abs(max_val-124)<1e-4);
 int main() {
-    int max_val = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.begin(),
-                                     {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}.end());
-    assert(std::abs(max_val-124)<1e-4);
     return 0;
 }
