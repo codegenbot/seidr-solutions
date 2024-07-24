c455447eda3f8d62a08b316eeaa93f0f0fd33747
beep boop <githubbot@vadim.me>
2024-07-24 12:36:23 +0200

    Solution 78 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/48n9pw6w Prompt:   +++ |+#include <iostream>     3 | 10041c42-1723-4e44-84a8-093e08e269ff.cpp:25:5: error: ‘cin’ was not declared in this scope    25 |     cin >> a >> b;       |     ^~~ 10041c42-1723-4e44-84a8-093e08e269ff.cpp:25:5: note: ‘std::cin’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’? 10041c42-1723-4e44-84a8-093e08e269ff.cpp:27:57: error: ‘gcd’ was not declared in  this scope    27 |     cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << end l;       |                                                         ^~~ 10041c42-1723-4e44-84a8-093e08e269ff.cpp:27:70: error: ‘endl’ was not declared i n this scope    27 |  cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;       |                                                                   ^~~~ 10041c42-1723-4e44-84a8-093e08e269ff.cpp:3:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     2 | #include <string>   +++ |+#include <ostream>     3 |

--- gcd.cpp
+++ gcd.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int gcd(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -12,28 +10,14 @@ int gcd(int a, int b) {
     return a;
 }
 
-vector<int> indicesOfSubstring(string text, string target) {
-    vector<int> result;
-    int n = text.length();
-    int m = target.length();
-
-    for (int i = 0; i <= n - m; i++) {
-        if (text.substr(i, m) == target) {
-            result.push_back(i);
-        }
-    }
-
-    return result;
-}
-
 int main() {
     int a = 0;
     int b = 0;
 
-    cout << "Enter two integers: ";
-    cin >> a >> b;
+    std::cout << "Enter two integers: ";
+    std::cin >> a >> b;
 
-    cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+    std::cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << std::endl;
 
     return 0;
 }
