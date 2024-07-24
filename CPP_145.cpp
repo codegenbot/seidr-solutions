18646ccb64c0e52e502117bcb0b17f7a01e89efb
beep boop <githubbot@vadim.me>
2024-07-24 23:42:31 +0200

    Solution 60 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lmv36xmy Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 75ff46fb-9d54-47be-bf6f-0d472ed32c20.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 75ff46fb-9d54-47be-bf6f-0d472ed32c20.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 75ff46fb-9d54-47be-bf6f-0d472ed32c20.cpp:23: 75ff46fb-9d54-47be-bf6f-0d472ed32c20.cpp: In function ‘int main()’: 75ff46fb-9d54-47be-bf6f-0d472ed32c20.cpp:33:20: error: ‘order_by_points’ was not  declared in this scope    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 75ff46fb-9d54-47be-bf6f-0d472ed32c20.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,31 +1,7 @@
-#include <algorithm>
-#include <vector>
-#include <cassert>
-
-bool issame(vector<int> a,vector<int>b){
-    return a==b;
-}
-
-int main(){
-    assert (issame({},{}) );
-    assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0, 4, 23, 6, 6}));
-}
-
-vector<int> order_by_points(vector<int> nums) {
-    vector<pair<int, int>> pairs;
-    for (int i = 0; i < nums.size(); ++i) {
-        int num = nums[i];
-        int sumOfDigits = 0;
-        while (num) {
-            sumOfDigits += num % 10;
-            num /= 10;
-        }
-        pairs.push_back({sumOfDigits, i});
+bool isSame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) return false;
     }
-    sort(pairs.begin(), pairs.end());
-    vector<int> result;
-    for (const auto& pair : pairs) {
-        result.push_back(nums[pair.second]);
-    }
-    return result;
+    return true;
 }
