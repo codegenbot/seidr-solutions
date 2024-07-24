43bc0491440e24eb18b6a26e24d54435bc707aa9
beep boop <githubbot@vadim.me>
2024-07-24 10:06:46 +0200

    Solution 12 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/42gtbgjv Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,39 +1,17 @@
+Here is the completed code:
+
 string file_name_check(string file_name){
     int digit_count = 0;
     bool found_dot = false;
-
-    for (int i = 0; i < file_name.size(); i++) {
-        char c = file_name[i];
-        if (!found_dot && c == '.') {
-            found_dot = true;
-        } else if (isdigit(c)) {
+    bool valid_prefix = true;
+    for(int i = 0; i < file_name.length(); i++){
+        if(isdigit(file_name[i])){
             digit_count++;
-            if (digit_count > 3) {
-                return "No";
-            }
-        } else if (found_dot && (c != 'a' && c != 'A' && c != 'b' && c != 'B' && 
-                                   c != 'c' && c != 'C' && c != 'd' && c != 'D' &&
-                                   c != 'e' && c != 'E' && c != 'f' && c != 'F' &&
-                                   c != 'g' && c != 'G' && c != 'h' && c != 'H' &&
-                                   c != 'i' && c != 'I' && c != 'j' && c != 'J' &&
-                                   c != 'k' && c != 'K' && c != 'l' && c != 'L' &&
-                                   c != 'm' && c != 'M' && c != 'n' && c != 'N' &&
-                                   c != 'o' && c != 'O' && c != 'p' && c != 'P' &&
-                                   c != 'q' && c != 'Q' && c != 'r' && c != 'R' &&
-                                   c != 's' && c != 'S' && c != 't' && c != 'T' &&
-                                   c != 'u' && c != 'U' && c != 'v' && c != 'V' &&
-                                   c != 'w' && c != 'W' && c != 'x' && c != 'X' &&
-                                   c != 'y' && c != 'Y' && c != 'z' && c != 'Z') {
-            return "No";
+        }else if(file_name[i] == '.'){
+            found_dot = true;
+        } else if(!found_dot && !isalpha((unsigned char)file_name[i])) {
+            valid_prefix = false;
         }
     }
-
-    if (!found_dot || file_name.find(file_name) == -1 || 
-       (file_name.size() > 4 && (file_name.substr(found_dot + 1).find("txt") == -1 &&
-                                  file_name.substr(found_dot + 1).find("exe") == -1 &&
-                                  file_name.substr(found_dot + 1).find("dll") == -1)) {
-        return "No";
-    }
-
-    return "Yes";
+    return (digit_count <= 3 && found_dot && valid_prefix) ? "Yes" : "No";
 }
