b96ddd04838f64ff6b6155982e6f4f71d99c24e4
beep boop <githubbot@vadim.me>
2024-07-24 09:03:31 +0200

    Solution 15 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/ixs2ihlg Prompt:  this scope    19 |         int currentDiff = abs(leftSum - rightSum);       |                           ^~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:28:13: error: ‘cout’ was not declared i n this scope    28 |             cout << nums[i] << endl;       |             ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:28:21: error: ‘nums’ was not declared i n this scope    28 |             cout << nums[i] << endl;       |                     ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:28:32: error: ‘endl’ was not declared i n this scope    28 |             cout << nums[i] << endl;       |                                ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:30:13: error: ‘cout’ was not declared i n this scope    30 |             cout << 0 << endl;       |             ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:30:26: error: ‘endl’ was not declared i n this scope    30 |             cout << 0 << endl;       |                          ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -8,6 +8,7 @@ int main() {
     int n;
     cin >> n;
     vector<int> nums(n);
+    
     for (int i = 0; i < n; ++i) {
         cin >> nums[i];
     }
@@ -15,22 +16,23 @@ int main() {
     int diff = INT_MAX;
     int cutIndex = 0;
     int leftSum = 0, rightSum = 0;
-
-    for (int i = 1; i < n; ++i) {
-        rightSum += nums[i];
-    }
-
+    
     for (int i = 1; i < n; ++i) {
         leftSum += nums[i - 1];
-        rightSum -= nums[i];
+        rightSum = 0;
 
+        for (int j = i; j < n; ++j) {
+            rightSum += nums[j];
+        }
+        
         int currentDiff = abs(leftSum - rightSum);
+        
         if (currentDiff < diff) {
             diff = currentDiff;
             cutIndex = i;
         }
     }
-
+    
     for (int i = 0; i < n; ++i) {
         if (i < cutIndex) {
             cout << nums[i] << endl;
@@ -38,6 +40,6 @@ int main() {
             cout << nums[i] << endl;
         }
     }
-
+    
     return 0;
 }
