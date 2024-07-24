3caf9d204bb031967c1af35e36232e08148b52c0
beep boop <githubbot@vadim.me>
2024-07-24 06:04:40 +0200

    Solution 9 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/i8qfruf6 Prompt: Complete the following code given the task description and function signature.

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,25 +1,31 @@
+Here is the completed code:
+
 vector<string> reverse_delete(string s, string c) {
     vector<string> result;
     string temp = "";
-    for (char ch : s) {
+    for (char x : s) {
         bool flag = false;
-        for (char cc : c) {
-            if (ch == cc) {
+        for (char y : c) {
+            if (x == y) {
                 flag = true;
                 break;
             }
         }
-        if (!flag) {
-            temp += ch;
-        }
+        if (!flag)
+            temp += x;
     }
-    result.push_back(temp);
-    string rev = temp;
-    reverse(rev.begin(), rev.end());
-    if (temp == rev) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
-    }
+    if (temp.length() > 0 && isPalindrome(temp))
+        result.push_back(temp + "," + "True");
+    else
+        result.push_back(temp + "," + "False");
     return result;
+}
+
+bool isPalindrome(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    if (s == rev)
+        return true;
+    else
+        return false;
 }
