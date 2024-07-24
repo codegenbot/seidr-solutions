f5c1969f51c63428ed132274fec624b821492091
beep boop <githubbot@vadim.me>
2024-07-24 22:15:38 +0200

    Solution 9 of CPP/124, ratio 0.875 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/p3rv1azm Prompt: Complete the following code given the task description and function signature.

--- CPP_124.cpp
+++ CPP_124.cpp
@@ -2,43 +2,36 @@
 using namespace std;
 
 bool valid_date(string date) {
-    int month, day, year;
-    char dash[3] = {'-', 0, '-'};
-    size_t pos1 = date.find_first_of(dash);
-    size_t pos2 = date.find_last_not_of(dash);
+    int day, month, year;
+    size_t pos = 0;
+    pos = date.find('-');
+    string day_month = date.substr(0, pos);
+    pos += 1;
+    string month_year = date.substr(pos);
+    pos = month_year.find('-');
+    month = stoi(day_month.substr(0, 2));
+    day = stoi(day_month.substr(3, 2));
+    year = stoi(month_year.substr(0, 4));
 
-    if (pos1 != string::npos && pos2 != string::npos) {
-        string month_str = date.substr(0, pos1);
-        string day_str = date.substr(pos1 + 1, pos2 - pos1 - 1);
-        string year_str = date.substr(pos2 + 1);
-
-        try {
-            month = stoi(month_str);
-            day = stoi(day_str);
-            year = stoi(year_str);
-        } catch (...) {
-            return false;
-        }
-
-        if (month < 1 || month > 12)
-            return false;
-
-        if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) &&
-            (day < 1 || day > 31))
-            return false;
-
-        if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30))
-            return false;
-
-        if (month == 2) {
-            if (year % 4 != 0)
-                return day > 28;
-            else if (year % 100 != 0)
-                return day > 29;
-            else if (year % 400 != 0)
-                return day > 28;
-        }
+    if (date.empty() || month < 1 || month > 12) {
+        return false;
     }
 
-    return false;
+    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            && day > 31) {
+        return false;
+    }
+    if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30) {
+        return false;
+    }
+    if (month == 2) {
+        if (year % 4 != 0)
+            return day > 28;
+        else if (year % 100 != 0)
+            return day > 29;
+        else if (year % 400 != 0)
+            return day > 28;
+    }
+
+    return true;
 }
