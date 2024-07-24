233ecaba48a6ee1e65363fabd4169b3152814d63
beep boop <githubbot@vadim.me>
2024-07-24 02:01:44 +0200

    Solution 55 of Python/20, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bjwpnh7h Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ec4cb70-7c9a-469e-a12d-0d3d58aed 6da.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,5 +1,8 @@
-def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
-    min_diff = float("inf")
+def find_closest_elements(numbers):
+    if len(numbers) < 2:
+        return "List should have at least two elements"
+
+    min_diff = float('inf')
     closest_pair = ()
 
     for i in range(len(numbers)):
