ce6e73153d9de4e35ea56fb7a025cf9129a13104
beep boop <githubbot@vadim.me>
2024-07-24 03:35:40 +0200

    Solution 70 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/lxcqhlyr Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,47 +1,41 @@
-```cpp
-#include <iostream>
-#include <string>
-using namespace std;
-
 bool solveBoolean(string expression) {
-    if (expression.size() > 0) {
-        if (expression[0] == 'T' || expression[0] == 't')
-            return true;
-        else if (expression[0] == 'F' || expression[0] == 'f')
-            return false;
+    int pos = 0;
+
+    while (pos < expression.size()) {
+        size_t start = pos;
+        if (expression[pos] == '|') {
+            pos++;
+            bool left = solveBoolean(string(1, expression[pos]));
+            bool right = true;
+            for (++pos; pos < expression.size() && expression[pos] != '&'; ++pos)
+                ;
+            if (pos < expression.size())
+                right = solveBoolean(string(&expression[pos], 1));
+            return left || right;
+        } else if (expression[pos] == '&') {
+            pos += 2;
+            bool left = true, right = true;
+            for (; pos < expression.size() && expression[pos] != '|';) {
+                if (expression[pos] == 'F' || expression[pos] == 'f')
+                    left = false;
+                else
+                    break;
+                ++pos;
+            }
+            for (++pos; pos < expression.size() && expression[pos] != '|'; ++pos)
+                ;
+            if (pos < expression.size())
+                right = solveBoolean(string(&expression[pos], 1));
+            return left && right;
+        } else {
+            if (expression[start] == 'T' || expression[start] == 't')
+                return true;
+            else if (expression[start] == 'F' || expression[start] == 'f')
+                return false;
+            pos++;
+        }
     }
-
-    int start = 0, end = 0;
-    while ((end = expression.find('|', start)) != string::npos) {
-        string left = expression.substr(start, end - start);
-        string right = expression.substr(end);
-
-        if (solveBoolean(left) || solveBoolean(right))
-            return true;
-
-        start = end + 1;
-    }
-
-    while ((end = expression.find('&', start)) != string::npos) {
-        string left = expression.substr(start, end - start);
-        string right = expression.substr(end);
-
-        if (solveBoolean(left) && solveBoolean(right))
-            return true;
-
-        start = end + 1;
-    }
-
-    if (start < expression.size()) {
-        string last = expression.substr(start);
-
-        if (last.size() > 0 && (last[0] == 'T' || last[0] == 't'))
-            return true;
-
-        return false;
-    }
-
-    return false;
+    return true;
 }
 
 int main() {
