cc9660f2abf4e2a65b03e47f739212f04ed8526f
beep boop <githubbot@vadim.me>
2024-07-24 13:34:19 +0200

    Solution 44 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a1i1ncrr Prompt:       |           ^~~~~~ In file included from 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp:21: 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp: In function ‘int main()’: 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp:31:20: error: ‘even_odd_count’ was not declared in this scope    31 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 2c10db84-4bf0-4f82-b31a-ced0c52202f4.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,6 +1,25 @@
 #include <vector>
 #include <cassert>
 
-using namespace std;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
 
-vector<int> counts(vector<int> b, int num) {
+std::vector<int> count_even_and_odd_digits(int num) {
+    std::vector<int> counts(2, 0);
+    std::string numStr = std::to_string(std::abs(num));
+    
+    for (char c : numStr) {
+        if ((c - '0') % 2 == 0) {
+            counts[0]++;
+        } else {
+            counts[1]++;
+        }
+    }
+    
+    return counts;
+}
+
+int main() {
+    assert(issame(count_even_and_odd_digits(0), {1, 0}));
+}
