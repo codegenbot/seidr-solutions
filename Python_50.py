ee929262c27d23cf5b660e8d17b4a02103546990
beep boop <githubbot@vadim.me>
2024-07-24 02:13:34 +0200

    Solution 23 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/46b9z7v9 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee73e4c5-c640-4165-9742-419458186 e63.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,18 +1,14 @@
-```
 def decode_shift():
     while True:
         try:
             shift = int(input("Enter the shift amount: "))
-            if 1 <= shift <= 25:
-                break
-            else:
-                print("Invalid input. Please enter an integer between 1 and 25.")
+            break
         except ValueError:
             print("Invalid input. Please enter an integer.")
     
     s = input().lower()
     
-    if s != "":
-        return "".join([chr((ord(ch) - 97 - shift) % 26 + 97) if ch.isalpha() else ch for ch in s])
+    if not set(s).issubset('abcdefghijklmnopqrstuvwxyz'):
+        return "Please provide some text to decode."
     else:
-        print("Please provide some text to decode.")
+        return "".join([chr((ord(ch) - 97 - shift) % 26 + 97) if ch.isalpha() else ch for ch in s])
