f5478aff43f68fef7023e1f3c2153fd0d6298a4f
beep boop <githubbot@vadim.me>
2024-07-24 16:59:47 +0200

    Solution 16 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ry5ssmq6 Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 79ad2906-770c-495c-95ad-6fb39a13ae0b.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 79ad2906-770c-495c-95ad-6fb39a13ae0b.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 79ad2906-770c-495c-95ad-6fb39a13ae0b.cpp:20: 79ad2906-770c-495c-95ad-6fb39a13ae0b.cpp: In function ‘int main()’: 79ad2906-770c-495c-95ad-6fb39a13ae0b.cpp:30:20: error: ‘sort_third’ was not decl ared in this scope    30 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 79ad2906-770c-495c-95ad-6fb39a13ae0b.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,9 +1,23 @@
-for (int i = 2; i < l.size(); i += 3) {
-        int j = i;
-        while (j > 0 && l[j] < l[j - 1]) {
-            swap(l[j], l[j - 1]);
-            j--;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> sort_third(vector<int> l) {
+    vector<int> sorted_indices;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            sorted_indices.push_back(i);
         }
     }
-    return l;
+
+    sort(sorted_indices.begin(), sorted_indices.end(), [&](int a, int b) {
+        return l[a] < l[b];
+    });
+
+    vector<int> l_prime = l;
+    for (int i = 0; i < sorted_indices.size(); ++i) {
+        l_prime[sorted_indices[i]] = l[sorted_indices[i]];
+    }
+
+    return l_prime;
 }
