6e3748fbdae6c4781cbe8a3fa0c8e98454af140f
beep boop <githubbot@vadim.me>
2024-07-24 15:49:22 +0200

    Solution 49 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6zjxuet3 Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -12,9 +12,15 @@ bool check_if_last_char_is_a_letter(const std::string& str) {
 int main() {
     assert(check_if_last_char_is_a_letter("apple pi e ") == true); 
     std::cout << "Check if the last character of a string is a letter: ";
-    std::string str;
-    getline(std::cin >> std::ws, str); // Remove leading and trailing whitespaces
-    if (check_if_last_char_is_a_letter(str))
+    std::string input;
+    getline(std::cin, input); 
+    while (input.find_first_not_of(" ") != std::string::npos) {
+        if (input[0] == ' ')
+            input.erase(0, 1);
+        else
+            break;
+    }
+    if (check_if_last_char_is_a_letter(input))
         std::cout << "The last character of the input string is a letter.\n";
     else
         std::cout << "The last character of the input string is not a letter.\n";
