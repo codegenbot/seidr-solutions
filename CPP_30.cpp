d4743918e5c616a7403603908af6fb9c62c1d933
beep boop <githubbot@vadim.me>
2024-07-24 18:17:01 +0200

    Solution 4 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xev7vum1 Prompt:       |                    ^~~~~ 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp:11: 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp: In function ‘int main()’: 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 0d97fd49-1b1a-4c99-bb4d-2caf683eabd1.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,42 +1,28 @@
-#include <vector>
+```cpp
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <assert.h>
 
-bool isSame(vector<float> l) {
-    if (l.size() == 0)
-        return true;
-    float num = l[0];
-    for (float i : l) {
-        if (i != num)
+bool issame(std::vector<float> a, std::vector<float> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i])
             return false;
-    }
     return true;
 }
 
-int main() {
-    vector<float> list;
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        float x;
-        cin >> x;
-        list.push_back(x);
-    }
-    vector<float> posList = get_positive(list);
-    if (!isSame(posList)) {
-        assert(false);
-    } else {
-        cout << "The positive numbers are same." << endl;
-    }
-    return 0;
-}
-
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
+}
+
+int main() {
+    assert(get_positive({}) == {});
+    return 0;
 }
