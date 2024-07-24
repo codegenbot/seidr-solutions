33a8420a73361540f1d600d0d92cd3063fc7518d
beep boop <githubbot@vadim.me>
2024-07-24 05:48:22 +0200

    Solution 79 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33ef98d7-168f-4e57-966b-9a9f6dba5 4c3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,7 +1,6 @@
-```
 while True:
-    K = int(input("Enter a number (0 or 1): "))
-    if K in [0, 1]:
+    K = input("Enter a number (0 or 1): ")
+    if K in ['0', '1']:
         print(K)
         break
     else:
