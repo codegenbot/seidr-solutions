cf3654dc9f12694e7a83bc08ec7525415e038e89
beep boop <githubbot@vadim.me>
2024-07-24 02:25:15 +0200

    Solution 40 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1bwzxr1e Prompt: 0bb48599-a5f3-4fde-8631-429c8f6afea3.cpp:1:26: error: ‘vector’ was not declared in this scope     1 | long long minSubArraySum(vector<long long> nums){       |                          ^~~~~~ 0bb48599-a5f3-4fde-8631-429c8f6afea3.cpp:1:33: error: expected primary-expressio n before ‘long’     1 | long long minSubArraySum(vector<long long> nums){       |                                 ^~~~ In file included from 0bb48599-a5f3-4fde-8631-429c8f6afea3.cpp:12: 0bb48599-a5f3-4fde-8631-429c8f6afea3.cpp: In function ‘int main()’: 0bb48599-a5f3-4fde-8631-429c8f6afea3.cpp:14:27: error: ‘minSubArraySum’ cannot b e used as a function    14 |     assert (minSubArraySum({1, -1}) == -1);       |             ~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -1,12 +1,12 @@
-#include<vector>
+#include <vector>
 
-long long minSubArraySum(std::vector<long long> nums) {
+long long minSubArraySum(vector<long long> nums) {
     long long sum = 0;
     long long min_sum = LLONG_MAX;
     for(int i=0; i<nums.size();i++){
         sum += nums[i];
         if(sum < min_sum) min_sum = sum;
-        if(sum > 0) sum = 0;
+        else if(sum > 0) sum = 0;
     }
-    return min_sum;
+    return (min_sum == LLONG_MAX)? 0 : min_sum;
 }
