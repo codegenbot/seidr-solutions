bb8cc6d5c4527273eb57e83b4a73415fa0143384
beep boop <githubbot@vadim.me>
2024-07-24 05:17:15 +0200

    Solution 8 of Python/150, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6p116xf4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8466cd03-5ec3-49bc-bad8-f4d69202e e63.py", line 12, in <module>     check(x_or_y)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8466cd03-5ec3-49bc-bad8-f4d69202e e63.py", line 9, in check     assert x_or_y(2, 2, 0) == 2            ^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_150.py
+++ Python_150.py
@@ -1,12 +1,5 @@
-```
-def check():
-    n = int(input("Enter a number: "))
-    x = int(input("Enter the first value: "))
-    y = int(input("Enter the second value: "))
-    print(x_or_y(n, x, y))
-
 def x_or_y(n, x, y):
-    if n % 2 == 0:
+    if n % 2 != 0:
         return y
     else:
         return x
