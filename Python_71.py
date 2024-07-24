d2730da6f01a42cd4c676943066d8349aacd6069
beep boop <githubbot@vadim.me>
2024-07-24 13:08:16 +0200

    Solution 22 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qhgsa44k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2811a367-79e8-4a9e-8d71-5e395c4e6 138.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_71.py
+++ Python_71.py
@@ -1,12 +1,9 @@
-def sum_of_squares(n):
-    result = 0
-    for i in range(1, n + 1):
-        result += i**2
-    return result
+def check(area):
+    print(f"Triangle area is {area:.2f}")
+
 
 base = float(input("Enter the base of the triangle: "))
 height = float(input("Enter the height of the triangle: "))
 
-triangle_area = (base*height)/2
-
-print("Valid input" if 0 < triangle_area <= (base**2)/4 else "Invalid input")
+triangle_area = (base * height) / 2
+check(triangle_area)
