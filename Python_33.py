fba842d2fc9355dd34f9f7ee2977148803c50dd4
beep boop <githubbot@vadim.me>
2024-07-24 15:05:52 +0200

    Solution 95 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l4ntiuzj Prompt: Complete the following code given the task description and function signature.

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,5 @@
 def sort_third(l: list):
-    l_prime = []
-    for i in range(len(l)):
-        if i % 3 == 0:
-            l_prime.append(sorted([x for x in l if i % 3 == 0])))
-        else:
-            l_prime.append(l[i])
-    return l_prime
+    return [
+        sorted([x for x in l if i % 3 == 0], reverse=True)[i % 3] if i % 3 == 0 else x
+        for i, x in enumerate(l)
+    ]
