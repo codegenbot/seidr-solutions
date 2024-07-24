53b1db3b787409d6c77629f68ee2076dc8e547b5
beep boop <githubbot@vadim.me>
2024-07-24 14:02:59 +0200

    Solution 56 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/u59jyrm1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/732e4421-3150-4a89-b1a2-36992ea27 02c.py", line 10, in <module>     output = check()              ^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/732e4421-3150-4a89-b1a2-36992ea27 02c.py", line 2, in check     string = raw_input("Enter a string: ").strip()              ^^^^^^^^^ NameError: name 'raw_input' is not defined

--- Python_27.py
+++ Python_27.py
@@ -2,10 +2,12 @@ def check():
     string = input("Enter a string: ").strip()
     return modify_string(string)
 
+
 def modify_string(string):
     return "".join(
         [char.lower() if char.isupper() else char.upper() for char in string]
     )
+
 
 output = check()
 print(output)
