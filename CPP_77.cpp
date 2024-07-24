6eab9551a8f76dc2f09f7b782ed5e066d65273d3
beep boop <githubbot@vadim.me>
2024-07-24 13:07:22 +0200

    Solution 4 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pm3603ol Prompt: afff4d0c-7751-400b-bf4e-f07c9084c5d0.cpp: In function ‘int main()’: afff4d0c-7751-400b-bf4e-f07c9084c5d0.cpp:16:4: error: ‘assert’ was not declared in this scope    16 |    assert(iscuber(1729) == false);       |    ^~~~~~ afff4d0c-7751-400b-bf4e-f07c9084c5d0.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <cmath>   +++ |+#include <cassert>     2 | using namespace std; afff4d0c-7751-400b-bf4e-f07c9084c5d0.cpp: At global scope: afff4d0c-7751-400b-bf4e-f07c9084c5d0.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ afff4d0c-7751-400b-bf4e-f07c9084c5d0.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,5 +1,5 @@
 #include <cmath>
-#include <cassert>
+#include <cassert> 
 using namespace std;
 
 bool iscuber(int a){
@@ -15,3 +15,4 @@ bool iscuber(int a){
 
 int main(){
    assert(iscuber(1729) == false);
+}
