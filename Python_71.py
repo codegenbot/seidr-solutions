3c20ba88a71f07190190d939fc4298e4363a3fb3
beep boop <githubbot@vadim.me>
2024-07-24 12:07:47 +0200

    Solution 4 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qhgsa44k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6a9830a-2030-4dc2-bccc-5b7041e1c 96a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_71.py
+++ Python_71.py
@@ -1,10 +1,13 @@
-def area():
-    a = float(input("Enter side A of triangle: "))
-    b = float(input("Enter side B of triangle: "))
-    c = float(input("Enter side C of triangle: "))
+```
+import math
 
-    if math.isclose(a + b, c) and math.isclose(a + c, b) and math.isclose(b + c, a):
+def triangle_area():
+    a = float(input("Enter side A: "))
+    b = float(input("Enter side B: "))
+    c = float(input("Enter side C: "))
+
+    if a + b > c and a + c > b and b + c > a:
         s = (a + b + c) / 2
-        return round((math.sqrt(s * (s - a) * (s - b) * (s - c))), 2)
+        return round(math.sqrt(s * (s - a) * (s - b) * (s - c)), 2)
     else:
         return -1
