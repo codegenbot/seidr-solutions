85c2cab0280aaadf0a8cdb43863bb4d040d51dcf
beep boop <githubbot@vadim.me>
2024-07-24 10:30:36 +0200

    Solution 45 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7cpgcdxq Prompt: Program failed

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,5 +1,4 @@
-bool check_if_last_char_is_a_letter(string txt) {
-    if (txt.empty()) return false;
+if (txt.empty()) return false;
     char lastChar = txt.back();
     return isalpha(lastChar);
 }
