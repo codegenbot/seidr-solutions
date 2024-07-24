ce4f87526f8f7bf27b2d53bb1a551af8d95f963f
beep boop <githubbot@vadim.me>
2024-07-24 04:12:16 +0200

    Solution 13 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/13qyj88i Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,6 +1,8 @@
 double find_zero(vector<double> xs){
-    int n = xs.size();
-    if(n%2!=0) return -1; //no solution exists for odd degree polynomial
-    double x=-xs[n-1]/(xs[0]*n);
-    return x;
+    double sum = xs[0];
+    for(int i = 1; i < xs.size(); i+=2) {
+        if(i == 1 && (sum * -1 > 1 || sum < 1)) return (-xs[i]/sum);
+        else if(sum * -1 > 1 || sum < 1) return ((-xs[i] + sqrt(pow(-xs[i],2) - 4*sum*xs[i-1]))/(2*sum));
+    }
+    return -1;
 }
