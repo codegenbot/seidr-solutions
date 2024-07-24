f357abf89d9edf9672db50157dbd35e20eb6cbd3
beep boop <githubbot@vadim.me>
2024-07-24 06:13:09 +0200

    Solution 52 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7499b897-fdbd-4f61-a2d7-f42e875ac f47.py", line 36, in <module>     check(sum_squares)           ^^^^^^^^^^^ NameError: name 'sum_squares' is not defined

--- Python_142.py
+++ Python_142.py
@@ -1,5 +1,6 @@
-def sum_squares(n):
-    return sum(i**2 for i in range(1, n+1))
+```
+def sum_squares():
+    n = int(input("Enter the number: "))
+    return sum([i**2 for i in range(1, n+1)])
 
-n = int(input("Enter a positive integer: "))
-print(sum_squares(n))
+check(sum_squares())
