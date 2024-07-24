ccf1fd5911813660192f879db063a22c44709c9f
beep boop <githubbot@vadim.me>
2024-07-24 20:10:43 +0200

    Solution 57 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ax209ivw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3ccb148-936a-4cad-a9e6-c84bff905 7cf.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f3ccb148-936a-4cad-a9e6-c84bff905 7cf.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,9 +1,5 @@
-```
 def encode(message):
     result = ""
     for char in message:
-        if char.isalpha():
-            result += chr((ord(char) - ord('A') + 3) % 26 + ord('A')) if char.isupper() else chr((ord(char) - ord('a') + 3) % 26 + ord('a'))
-        else:
-            result += char
+        result += chr((ord(char) - ord('A') + 3) % 26 + ord('A')) if char.isupper() else chr((ord(char) - ord('a') + 3) % 26 + ord('a'))
     return result
