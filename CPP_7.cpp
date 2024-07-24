025cbfcd4957173eb0cf88da57637b0e43745829
beep boop <githubbot@vadim.me>
2024-07-24 04:42:44 +0200

    Solution 79 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/15xh99es Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 583c45bc-1915-4881-b8b3-3ca4aa7906f0.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 583c45bc-1915-4881-b8b3-3ca4aa7906f0.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 583c45bc-1915-4881-b8b3-3ca4aa7906f0.cpp:11: 583c45bc-1915-4881-b8b3-3ca4aa7906f0.cpp: In function ‘int main()’: 583c45bc-1915-4881-b8b3-3ca4aa7906f0.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 583c45bc-1915-4881-b8b3-3ca4aa7906f0.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,11 +1,14 @@
-#include <vector>
-#include <string>
-
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
-    return 0;
+    bool result = true;
+    if (a.size() != b.size()) {
+        result = false;
+    } else {
+        for (int i = 0; i < a.size(); i++) {
+            if (!(a[i] == b[i])) {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
 }
