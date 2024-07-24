248ecafaa62fec5cd18e4c73931bdbddf3c9a392
beep boop <githubbot@vadim.me>
2024-07-24 16:43:09 +0200

    Solution 60 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ho1yy1q3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1f33b227-c68a-4a1f-ad82-3d1243a6e 7d5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,10 @@
-```
 def double_the_difference(numbers):
     total = 0
-    average = sum(num for num in numbers if isinstance(num, int) and num >= 0) / len([num for num in numbers if isinstance(num, int) and num >= 0])
     for num in numbers:
         if isinstance(num, int) and num >= 0:
-            total += (num - average) ** 2
-    return total * 2
+            if num % 2 != 0:
+                total += num**2
+    return total
+
+
+print("{} is the result".format(double_the_difference([1, 3, 5])))
