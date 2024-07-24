0cc7b26fa7dbf517be2b9f4d37ac4f2d344cb117
beep boop <githubbot@vadim.me>
2024-07-24 11:26:17 +0200

    Solution 98 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/cfsyn1qm Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,48 +1,15 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<vector<int>> cutVector(vector<int>& nums) {
+    int n = nums.size();
     int min_diff = INT_MAX;
-    pair<vector<int>, vector<int>> result;
-    for (int i = 1; i <= v.size(); i++) {
-        int left_sum = 0, right_sum = 0;
-        for (int j = 0; j < i; j++) {
-            left_sum += v[j];
-        }
-        for (int j = i; j < v.size(); j++) {
-            right_sum += v[j];
-        }
-        if (left_sum == right_sum) {
-            return {{v[0]}, v.substr(1)};
-        } else {
-            int diff = abs(left_sum - right_sum);
-            if (diff < min_diff) {
-                min_diff = diff;
-                result.first = vector<int>(v.begin(), v.begin() + i);
-                result.second = vector<int>(v.begin() + i, v.end());
-            }
+    int split_index = 0;
+    for (int i = 1; i < n; ++i) {
+        int diff = nums[i] - nums[0];
+        if (diff <= min_diff) {
+            min_diff = diff;
+            split_index = i;
         }
     }
-    return result;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    pair<vector<int>, vector<int>> res = cutVector(v);
-    cout << "[";
-    for (int i = 0; i < res.first.size(); i++) {
-        cout << res.first[i] << " ";
-    }
-    cout << "]" << endl;
-    cout << "[";
-    for (int i = 0; i < res.second.size(); i++) {
-        cout << res.second[i] << " ";
-    }
-    cout << "0]" << endl;
-    return 0;
-}
+    return {{nums.begin(), nums.begin() + split_index}, {nums.begin() + split_index, nums.end()}};
