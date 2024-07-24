ee1314072f7c0755218d26c5b6209c033f37877c
beep boop <githubbot@vadim.me>
2024-07-24 10:55:46 +0200

    Solution 54 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/0qnnz9fl Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b0b5e49e-cc39-4249-a3c7-27ea4fd13 01e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_9.py
+++ Python_9.py
@@ -1,14 +1,12 @@
 from typing import List
 
-
 def rolling_max(numbers: List[int]) -> List[int]:
     max_seen_so_far = float("-inf")
     result = []
     for num in numbers:
-        if num > max_seen_so_far:
-            max_seen_so_far = num
+        max_seen_so_far = max(num, max_seen_so_far)
         result.append(max_seen_so_far)
     return result
 
-
-print(rolling_max([1, 3, -1, 5, -2]))
+numbers = input("Enter the list of numbers separated by space: ").split()
+print(rolling_max(list(map(int, numbers))))
