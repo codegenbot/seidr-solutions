aa44764b3f75493a9ab2c6212eb75b1d611040d3
beep boop <githubbot@vadim.me>
2024-07-24 06:15:21 +0200

    Solution 35 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,25 +1,42 @@
 #include <vector>
 using namespace std;
 
-vector<int> cutVector(vector<int>& nums) {
-    int minDiff = INT_MAX;
-    int splitIndex = 0;
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int n = v.size();
+    pair<vector<int>, vector<int>> res;
     
-    for (int i = 1; i <= nums.size(); i++) {
-        if (i == nums.size() || nums[i] != nums[0]) {
-            int leftSum = accumulate(nums.begin(), nums.begin() + i, 0);
-            int rightSum = accumulate(nums.begin() + i, nums.end(), 0);
-            
-            int diff = abs(leftSum - rightSum);
-            if (diff < minDiff) {
-                minDiff = diff;
-                splitIndex = i;
-            }
+    for(int i=0; i<n-1; i++) {
+        if(abs(v[i] - v[i+1]) <= 0 || abs(v[0]-v[i+1]) <= abs(v[0]-v[i])) {
+            res.first = vector<int>(v.begin(), v.begin() + i+1);
+            res.second = vector<int>(v.begin() + i+1, v.end());
+            return res;
         }
     }
     
-    vector<int> leftVec(nums.begin(), nums.begin() + splitIndex);
-    vector<int> rightVec(nums.begin() + splitIndex, nums.end());
+    res.first = vector<int>(v.begin(), v.end());
+    res.second = vector<int>();
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++) {
+        cin >> v[i];
+    }
     
-    return {leftVec, rightVec};
+    pair<vector<int>, vector<int>> res = cutVector(v);
+    cout << "[";
+    for(auto x : res.first) {
+        cout << x << " ";
+    }
+    cout << "]" << endl;
+    cout << "[";
+    for(auto x : res.second) {
+        cout << x << " ";
+    }
+    cout << "]" << endl;
+    
+    return 0;
 }
