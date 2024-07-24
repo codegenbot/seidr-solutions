a05839b3fb54bdb4bd08502581c5a847ab5065e6
beep boop <githubbot@vadim.me>
2024-07-24 03:56:21 +0200

    Solution 15 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9862e9d-93ac-4a7f-8f00-63d3bdc3f f78.py", line 8     return [''.join([name_dict[i] if i < 10 else name_dict[int(i)] for i in [str (i) for i in arr if 1 <= int(i) <= 19]]).replace("Twenty", "Tw").replace("Eighte en", "Eight")            ^ SyntaxError: '[' was never closed

--- Python_105.py
+++ Python_105.py
@@ -1,2 +1,1 @@
-```
-return ''.join([name_dict[i] if i < 10 else name_dict[int(i)] for i in [str(i) for i in arr if 1 <= i <= 19]]).replace("Twenty", "Tw").replace("Eighteen", "Eight")
+return [''.join([name_dict[i] if i < 10 else name_dict[int(i)] for i in [str(i) for i in arr if 1 <= int(i) <= 19]])].replace("Twenty", "Tw").replace("Eighteen", "Eight")
