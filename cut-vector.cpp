0d7377f5131695754739be08fb90fb8ba085f32e
beep boop <githubbot@vadim.me>
2024-07-24 08:34:13 +0200

    Solution 14 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/ixs2ihlg Prompt:  this scope    19 |         int currentDiff = abs(leftSum - rightSum);       |                           ^~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:28:13: error: ‘cout’ was not declared i n this scope    28 |             cout << nums[i] << endl;       |             ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:28:21: error: ‘nums’ was not declared i n this scope    28 |             cout << nums[i] << endl;       |                     ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:28:32: error: ‘endl’ was not declared i n this scope    28 |             cout << nums[i] << endl;       |                                ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:30:13: error: ‘cout’ was not declared i n this scope    30 |             cout << 0 << endl;       |             ^~~~ e4c57506-684b-4bd4-8311-5094212af678.cpp:30:26: error: ‘endl’ was not declared i n this scope    30 |             cout << 0 << endl;       |                          ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -15,27 +15,29 @@ int main() {
     int diff = INT_MAX;
     int cutIndex = 0;
     int leftSum = 0, rightSum = 0;
-    for (int i = 0; i < n; ++i) {
+
+    for (int i = 1; i < n; ++i) {
         rightSum += nums[i];
     }
-    
-    for (int i = 0; i < n; ++i) {
+
+    for (int i = 1; i < n; ++i) {
+        leftSum += nums[i - 1];
         rightSum -= nums[i];
+
         int currentDiff = abs(leftSum - rightSum);
         if (currentDiff < diff) {
             diff = currentDiff;
             cutIndex = i;
         }
-        leftSum += nums[i];
     }
-    
+
     for (int i = 0; i < n; ++i) {
         if (i < cutIndex) {
             cout << nums[i] << endl;
         } else {
-            cout << 0 << endl;
+            cout << nums[i] << endl;
         }
     }
-    
+
     return 0;
 }
