0bc1c19ddba4e297ca27db49f453f7b8c5b1d33c
beep boop <githubbot@vadim.me>
2024-07-24 05:31:06 +0200

    Solution 33 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bvlxkbcg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/89b44e1b-3b73-486e-9b1a-95017bafb 970.py", line 1, in <module>     result = sum_squares([1, 2, 3, 11, 12])              ^^^^^^^^^^^ NameError: name 'sum_squares' is not defined

--- Python_142.py
+++ Python_142.py
@@ -1,14 +1,6 @@
-def sum_squares(numbers):
-    return sum(i**2 for i in numbers)
+```
+def sum_squares(lst):
+    return sum([n ** 2 for n in lst])
 
-
-def check(result):
-    if result > 100:
-        return "Bigger"
-    elif result == 100:
-        return "Equal"
-    else:
-        return "Smaller"
-
-
-print(check(sum_squares([1, 2, 3, 11, 12])))
+result = sum_squares([1, 2, 3, 11, 12])
+print(result)
