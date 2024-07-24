9fd88401b61e624c1f4d2f8f40485f4926739a55
beep boop <githubbot@vadim.me>
2024-07-24 05:56:57 +0200

    Solution 6 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/hrg8wku9 Prompt: Complete the following code given the task description and function signature.

--- CPP_109.cpp
+++ CPP_109.cpp
@@ -1,12 +1,9 @@
-#include <vector>
-using namespace std;
-
 bool move_one_ball(vector<int> arr) {
-    int n = arr.size();
-    for (int i = 1; i < n; i++) {
-        if (arr[i] >= arr[i - 1]) {
-            return true;
+    int max = *max_element(arr.begin(), arr.end());
+    for (int i = 0; i < arr.size() - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
         }
     }
-    return false;
+    return true;
 }
