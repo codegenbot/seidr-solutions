d19eb299fe62508e907e384cf58144475eba4ccc
beep boop <githubbot@vadim.me>
2024-07-24 06:42:22 +0200

    Solution 83 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,31 +1,35 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> nums) {
-    int n = nums.size();
-    vector<vector<int>> res(2);
-    
-    for (int i = 0; i < n - 1; i++) {
-        if (abs(nums[i] - nums[i + 1]) <= abs(nums[0] - nums[n - 1])) {
-            res[0].insert(res[0].end(), nums.begin(), nums.begin() + (i + 1));
-            res[1].insert(res[1].begin(), nums.begin() + i, nums.end());
-            break;
-        }
-    }
-    
-    if (res[0].size() == 0) {
-        res[0] = nums;
-        res[1].clear();
-    } else {
-        int diff = abs(nums[0] - nums[n - 1]);
-        for (int i = 0; i < n - 1; i++) {
-            if (abs(nums[i] - nums[i + 1]) <= diff) {
-                res[0].insert(res[0].end(), nums.begin(), nums.begin() + (i + 1));
-                res[1].insert(res[1].begin(), nums.begin() + i, nums.end());
-                break;
+pair<vector<int>, vector<int>> cutVector(vector<int> &nums) {
+    int minDiff = INT_MAX;
+    int pos = 0;
+    for (int i = 1; i <= nums.size(); ++i) {
+        long leftSum = 0, rightSum = 0;
+        if (i < nums.size()) {
+            for (int j = 0; j < i; ++j) {
+                leftSum += nums[j];
             }
+            for (int j = i; j < nums.size(); ++j) {
+                rightSum += nums[j];
+            }
+        } else {
+            long sum = 0;
+            for (int j = 0; j < i; ++j) {
+                sum += nums[j];
+            }
+            leftSum = sum;
+            rightSum = 0;
+        }
+        int diff = abs(leftSum - rightSum);
+        if (diff < minDiff) {
+            minDiff = diff;
+            pos = i;
         }
     }
     
-    return res;
+    vector<int> left(nums.begin(), nums.begin() + pos);
+    vector<int> right(nums.begin() + pos, nums.end());
+    
+    return {left, right};
 }
