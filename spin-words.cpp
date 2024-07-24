da5abbf76f3b2ec9674a3185e71afe8b648f140e
beep boop <githubbot@vadim.me>
2024-07-24 09:47:00 +0200

    Solution 80 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/n4u00cex Prompt:  this scope    29 |         cout << "Enter a string: ";       |         ^~~~ db603fc0-8cc3-42f9-a688-4d09c5155118.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; db603fc0-8cc3-42f9-a688-4d09c5155118.cpp:30:9: error: ‘cin’ was not declared in this scope    30 |         cin >> input;       |         ^~~ db603fc0-8cc3-42f9-a688-4d09c5155118.cpp:30:9: note: ‘std::cin’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’? db603fc0-8cc3-42f9-a688-4d09c5155118.cpp:32:37: error: ‘endl’ was not declared i n this scope    32 |         cout << spinWords(input) << endl;       |                                     ^~~~ db603fc0-8cc3-42f9-a688-4d09c5155118.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+using namespace std;
 
-std::string spinWords(std::string str) {
-    std::string result = "";
-    std::string word;
-
-    for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i + 1] == ' ') {
-            word = str.substr(i - (word.empty() ? 0 : 1), i - i);
-            if (word.length() >= 5) {
-                std::reverse(word.begin(), word.end());
+string spinWords(string sentence) {
+    string result = "";
+    int count = 0;
+    for (int i = 0; i < sentence.length(); i++) {
+        if (sentence[i] == ' ') {
+            result += " ";
+            count = 0;
+        } else {
+            count++;
+            if (count >= 5) {
+                for (int j = i; j >= i - count + 1; j--) {
+                    result += sentence[j];
+                }
+                i -= count - 1;
+            } else {
+                result += sentence[i];
             }
-            result += word + " ";
         }
     }
-
     return result;
 }
 
-int main() {
-    std::string str;
-    std::cout << "Enter a string: ";
-    std::getline(std::cin, str);
-    std::cout << spinWords(str) << std::endl;
-
-    return 0;
+string spinWords(string sentence) {
+    string reversed = "";
+    int start = 0;
+    for (int end = 0; end <= sentence.length(); end++) {
+        if (end == sentence.length() || sentence[end] == ' ') {
+            string word = sentence.substr(start, end - start);
+            if (word.length() >= 5) {
+                reversed += std::string(word.rbegin(), word.rend()) + " ";
+            } else {
+                reversed += word + " ";
+            }
+            start = end + 1;
+        }
+    }
+    return reversed;
 }
+
+int main() {
+    string input;
+    while (true) {
+        cout << "Enter a string: ";
+        cin >> input;
+        if (input == "exit") break;
+        cout << spinWords(input) << endl;
+    }
+    return 0;
