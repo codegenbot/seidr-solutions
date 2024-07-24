686ccb1fb50307b92a56035d7348c127eb62c738
beep boop <githubbot@vadim.me>
2024-07-24 23:41:28 +0200

    Solution 58 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/v440md5l Prompt: e0a1596b-0dd8-468c-ac95-490b65e52729.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ e0a1596b-0dd8-468c-ac95-490b65e52729.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from e0a1596b-0dd8-468c-ac95-490b65e52729.cpp:11: e0a1596b-0dd8-468c-ac95-490b65e52729.cpp: In function ‘int main()’: e0a1596b-0dd8-468c-ac95-490b65e52729.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -14,6 +14,6 @@ int specialFilter(std::vector<int> nums) {
 int main() {
     assert(specialFilter({}) == 0);
     assert(specialFilter({11, 12, 13, 14, 15, 16, 17, 18, 19, 20}) == 10);
-    assert(specialFilter({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}) == 20);
+    assert(specialFilter({-11, -12, -13, -14, -15, -16, -17, -18, -19, -20}) == 0);
     return 0;
 }
