def29bec3519950e1368e04fd6bb61187e93f98b
beep boop <githubbot@vadim.me>
2024-07-24 21:29:30 +0200

    Solution 90 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ba7grvne Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b14441f-68d8-4279-98af-cd7081909 434.py", line 16, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b14441f-68d8-4279-98af-cd7081909 434.py", line 13, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8b14441f-68d8-4279-98af-cd7081909 434.py", line 8, in minPath     return [f"{grid[i][0]} {grid[i][1]} ..."] if res == minPathSum else []                                                         ^^^^^^^^^^ NameError: name 'minPathSum' is not defined. Did you mean: 'minPath'?

--- Python_129.py
+++ Python_129.py
@@ -2,7 +2,6 @@
 def minPath(grid, k):
     n = len(grid)
     res = float("inf")
-    minPathSum = min(temp for temp in (sum(grid[i][j] for j in range(k)) for i in range(n - k + 1)))
     for i in range(n - k + 1):
         temp = sum(grid[i][j] for j in range(k))
         if temp < res:
