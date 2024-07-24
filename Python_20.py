8ae68135142a843a88fb226f9f34b5152488b0de
beep boop <githubbot@vadim.me>
2024-07-24 10:13:04 +0200

    Solution 10 of Python/20, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bs481l9q Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29f2bb8d-d9cf-425b-8dea-2491b5833 7ea.py", line 7, in <module>     check(find_closest_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29f2bb8d-d9cf-425b-8dea-2491b5833 7ea.py", line 4, in check     assert find_closest_elements([1.1, 2.2, 3.1, 4.1, 5.1]) == (2.2, 3.1)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_20.py
+++ Python_20.py
@@ -1,2 +1,4 @@
-def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
-    return tuple(sorted((x, y) for x in numbers for y in numbers if x < y)[:1])
+def find_closest_elements(numbers: list[float]) -> tuple[float, float]:
+    pairs = [(x, y) for x in numbers for y in numbers if x < y]
+    sorted_pairs = sorted(pairs, key=lambda pair: abs(pair[1] - pair[0]))
+    return sorted_pairs[0]
