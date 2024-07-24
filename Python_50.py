5a01c09e7401f6f5a227807e19a46292f7afa5b6
beep boop <githubbot@vadim.me>
2024-07-24 19:04:43 +0200

    Solution 24 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/th2aq4py Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/380d6527-4dc7-4219-bfd3-078957834 619.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,3 +1,2 @@
 def decode_shift(s: str):
-    result = "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
-    return result
+    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
