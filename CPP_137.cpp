956e8108b056534080370f042e1a00e61a222170
beep boop <githubbot@vadim.me>
2024-07-24 15:52:39 +0200

    Solution 50 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7vjwb7qj Prompt:       |                                ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp:40: d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp: In function ‘int main()’: d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp:42:13: error: ‘boost’ has not been decl ared    42 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp:42:29: error: ‘string’ was not declared  in this scope    42 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp:42:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d2457df9-eaa0-4836-8f9a-8c0f50808c33.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,9 +1,9 @@
-#include <variant>
 #include <string>
+#include <variant>
 
 std::variant<std::string, int, float> compare_one(std::variant<std::any> a, std::variant<std::any> b) {
     return std::visit([&](auto&& a, auto&& b) -> std::variant<std::string, int, float> {
-        if (std::holds_alternative<std::string>(a) && std::holds_alternative<std::string>(b)) {
+        if (std::holds_alternative<std::any>(a) && std::holds_alternative<std::any>(b)) {
             if (std::get<std::string>(a) > std::get<std::string>(b)) {
                 return "Greater";
             } else if (std::get<std::string>(a) < std::get<std::string>(b)) {
@@ -12,8 +12,8 @@ std::variant<std::string, int, float> compare_one(std::variant<std::any> a, std:
                 return "Equal";
             }
         } else if (std::holds_alternative<int>(a) && std::holds_alternative<int>(b)) {
-            int aInt = std::any_cast<int>(a);
-            int bInt = std::any_cast<int>(b);
+            int aInt = std::get<int>(a);
+            int bInt = std::get<int>(b);
             if (aInt > bInt) {
                 return "Greater";
             } else if (aInt < bInt) {
@@ -22,8 +22,8 @@ std::variant<std::string, int, float> compare_one(std::variant<std::any> a, std:
                 return "Equal";
             }
         } else if (std::holds_alternative<float>(a) && std::holds_alternative<float>(b)) {
-            float aFloat = std::any_cast<float>(a);
-            float bFloat = std::any_cast<float>(b);
+            float aFloat = std::get<float>(a);
+            float bFloat = std::get<float>(b);
             if (aFloat > bFloat) {
                 return "Greater";
             } else if (aFloat < bFloat) {
@@ -36,5 +36,3 @@ std::variant<std::string, int, float> compare_one(std::variant<std::any> a, std:
         }
     }, a, b);
 }
-
-assert(std::get<std::string>(compare_one("1", 1)) == "Non");
