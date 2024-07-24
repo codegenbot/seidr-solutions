11fb37c6b43d108be4f5214b9adf71a8c91eaa95
beep boop <githubbot@vadim.me>
2024-07-24 10:37:43 +0200

    Solution 96 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7meh2yxx Prompt:    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 01e6e266-e2e1-424e-84c6-8831c53a2111.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 01e6e266-e2e1-424e-84c6-8831c53a2111.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 01e6e266-e2e1-424e-84c6-8831c53a2111.cpp:22: 01e6e266-e2e1-424e-84c6-8831c53a2111.cpp: In function ‘int main()’: 01e6e266-e2e1-424e-84c6-8831c53a2111.cpp:32:35: error: ‘sorted_list_sum’ cannot be used as a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 01e6e266-e2e1-424e-84c6-8831c53a2111.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -15,6 +15,6 @@ int sorted_list_sum(std::vector<std::vector<std::string>> lst) {
 
 int main() {
     std::vector<std::vector<std::string>> lst = {{{"aaaa", "bbbb"}, {"dd"}, {"cc"}}};
-    assert(sorted_list_sum({{{"cc", "dd", "aaaa", "bbbb"}}}) == 2);
+    assert(sorted_list_sum(lst) == 2);
     return 0;
 }
