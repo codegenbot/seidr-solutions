b6105f71ff7ce790f0776f7bcf2364b729b381ee
beep boop <githubbot@vadim.me>
2024-07-24 21:22:50 +0200

    Solution 52 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/u1j6qydu Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f07facf2-8e1e-49ed-93a4-5d012078d 2d5.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f07facf2-8e1e-49ed-93a4-5d012078d 2d5.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,3 @@
-```
 def double_the_difference(lst):
-    odd_numbers = [i**2 for i in lst if isinstance(i, int) and i % 2 != 0]
-    avg = sum(i for i in lst if isinstance(i, int)) / len(lst)
-    return abs(sum(odd_numbers) - 2 * avg)
+    odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i % 2 != 0)
+    return 2 * (max(lst) - min(lst)) - odd_sum
