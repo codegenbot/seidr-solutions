c1d3ae928a3a32936ecf8b725aed92f9c1d5a9d2
beep boop <githubbot@vadim.me>
2024-07-24 18:45:25 +0200

    Solution 38 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/w0p6u1nr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/746394a4-a3b5-4912-853f-ce8b17643 5be.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,13 +1,4 @@
 from typing import List, Any
 
 def filter_integers(values: List[Any]) -> List[int]:
-    result = []
-    for value in values:
-        if isinstance(value, int):
-            result.append(int(value))
-        else:
-            try:
-                result.append(int(str(value)))
-            except ValueError:
-                pass
-    return result
+    return [value for value in values if isinstance(value, int)]
