b04f7d582068bea37d18d5d4f71a95b6753eab17
beep boop <githubbot@vadim.me>
2024-07-24 12:27:09 +0200

    Solution 3 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/aa0x6h6n Prompt: In file included from /usr/include/c++/11/vector:67,                  from d45c9b34-21ac-4727-b41f-0d606abe5868.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d45c9b34-21ac-4727-b41f-0d606abe5868.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d45c9b34-21ac-4727-b41f-0d606abe5868.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d45c9b34-21ac-4727-b41f-0d606abe5868.cpp:28: d45c9b34-21ac-4727-b41f-0d606abe5868.cpp: In function ‘int main()’: d45c9b34-21ac-4727-b41f-0d606abe5868.cpp:38:20: error: ‘strange_sort_list’ was n ot declared in this scope    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ d45c9b34-21ac-4727-b41f-0d606abe5868.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,6 +1,7 @@
 #include <vector>
+#include <algorithm>
 
-bool issame(vector<int> a, vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b){
     if (a.size() != b.size()) {
         return false;
     }
@@ -12,9 +13,9 @@ bool issame(vector<int> a, vector<int> b){
     return true;
 }
 
-vector<int> strange_sort_list(vector<int> lst){
-    sort(lst.begin(), lst.end());
-    vector<int> result;
+std::vector<int> strange_sort_list(std::vector<int> lst){
+    std::sort(lst.begin(), lst.end());
+    std::vector<int> result;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
         result.push_back(lst[left++]);
@@ -23,8 +24,4 @@ vector<int> strange_sort_list(vector<int> lst){
         }
     }
     return result;
-}
-
-int main(){
-    assert(issame(strange_sort_list({111111}), {111111}));
 }
