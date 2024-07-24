e533296dfdf5986acb745967c80118de49ffca38
beep boop <githubbot@vadim.me>
2024-07-24 22:42:08 +0200

    Solution 35 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z78os2pt Prompt:    19 | } else if (a.type() == typeid(string) && b.type() == typeid(string)) {       |   ^~~~ c76fdae9-03d5-4448-8ce0-4faa1eb11e81.cpp:28:3: error: expected unqualified-id be fore ‘else’    28 | } else {       |   ^~~~ In file included from c76fdae9-03d5-4448-8ce0-4faa1eb11e81.cpp:32: c76fdae9-03d5-4448-8ce0-4faa1eb11e81.cpp: In function ‘int main()’: c76fdae9-03d5-4448-8ce0-4faa1eb11e81.cpp:34:13: error: ‘boost’ has not been decl ared    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ c76fdae9-03d5-4448-8ce0-4faa1eb11e81.cpp:34:29: error: ‘string’ was not declared  in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ c76fdae9-03d5-4448-8ce0-4faa1eb11e81.cpp:34:37: error: ‘compare_one’ was not dec lared in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,7 @@
-```
 #include <boost/any.hpp>
 #include <string>
 
-boost::any compareOne(boost::any a, boost::any b) {
+boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
@@ -11,7 +10,7 @@ boost::any compareOne(boost::any a, boost::any b) {
         else if (y > x)
             return b;
         else
-            return a; // Changed to return a instead of "None"
+            return a;
     } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
         double x = boost::any_cast<double>(a);
         double y = boost::any_cast<double>(b);
@@ -20,7 +19,7 @@ boost::any compareOne(boost::any a, boost::any b) {
         else if (y > x)
             return b;
         else
-            return a; // Changed to return a instead of "None"
+            return a;
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string x = boost::any_cast<std::string>(a);
         std::string y = boost::any_cast<std::string>(b);
@@ -29,8 +28,8 @@ boost::any compareOne(boost::any a, boost::any b) {
         else if (std::stod(y) > std::stod(x))
             return b;
         else
-            return a; // Changed to return a instead of "None"
+            return a;
     } else {
-        return a; // Changed to return a for all other cases
+        return a;
     }
 }
