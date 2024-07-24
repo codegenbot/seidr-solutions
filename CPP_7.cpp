792e299e86f68ee228b2d5a6b0c99928b06ad2b4
beep boop <githubbot@vadim.me>
2024-07-24 11:44:29 +0200

    Solution 42 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/83sl7mwz Prompt: d3f23504-ddf4-45e5-82a4-6da9af4e392d.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from d3f23504-ddf4-45e5-82a4-6da9af4e392d.cpp:27: d3f23504-ddf4-45e5-82a4-6da9af4e392d.cpp: In function ‘int main()’: d3f23504-ddf4-45e5-82a4-6da9af4e392d.cpp:37:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    37 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ d3f23504-ddf4-45e5-82a4-6da9af4e392d.cpp:17:6: note: candidate: ‘bool issame(con st std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx 11::basic_string<char> >&)’    17 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ d3f23504-ddf4-45e5-82a4-6da9af4e392d.cpp:28:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    28 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,25 +1,9 @@
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-vector<string> filter_by_substring(const vector<string>& vec, const string& substr) {
-    vector<string> result;
-    for (const auto& str : vec) {
-        if (str.find(substr) != string::npos) {
-            result.push_back(str);
-        }
-    }
-    return result;
-}
-
 bool issame(const vector<string>& a, const vector<string>& b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), vector<string>{"grunt", "prune"}));
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), (vector<string>{"grunt", "prune"}));
     
     return 0;
 }
