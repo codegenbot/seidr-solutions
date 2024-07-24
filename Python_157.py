30b3270e7a20577c09f7101b3fcfef80dc4a703b
beep boop <githubbot@vadim.me>
2024-07-25 00:17:31 +0200

    Solution 66 of Python/157, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/51vrqbmc Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/97a929ce-52a7-410e-9060-a48988035 ef7.py", line 9, in <module>     check(right_angle_triangle)           ^^^^^^^^^^^^^^^^^^^^ NameError: name 'right_angle_triangle' is not defined

--- Python_157.py
+++ Python_157.py
@@ -1,3 +1,3 @@
-def check(right_angle_triangle, a, b, c):
-    if right_angle_triangle == "pythagorean":
-        return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
+def check(right_angle_triangle):
+    a, b, c = right_angle_triangle
+    return a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2
