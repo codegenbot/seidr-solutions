85e9a7cc6caf47abcbbfc6abfc3903309b7b8f23
beep boop <githubbot@vadim.me>
2024-07-24 22:10:14 +0200

    Solution 31 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lgm8k84y Prompt: 86680508-f963-43d7-b127-ef54e3051a62.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string intersection(vector<int> interval1, vector<int> interval2) {       | ^~~~~~ In file included from 86680508-f963-43d7-b127-ef54e3051a62.cpp:20: 86680508-f963-43d7-b127-ef54e3051a62.cpp: In function ‘int main()’: 86680508-f963-43d7-b127-ef54e3051a62.cpp:22:13: error: ‘intersection’ was not de clared in this scope    22 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -2,11 +2,11 @@
 #include <string> 
 using namespace std;
 
-bool intersection(vector<int> interval1, vector<int> interval2) {
+int intersection(vector<int> interval1, vector<int> interval2) {
     int start = max(interval1[0], interval2[0]);
     int end = min(interval1[1], interval2[1]);
 
-    if (start > end) return false;
+    if (start > end) return 0;
 
     int length = end - start + 1;
 
