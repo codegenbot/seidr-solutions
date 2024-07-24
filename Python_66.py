26de01d78e9dac9f8aedb095a638d637febe321b
beep boop <githubbot@vadim.me>
2024-07-24 12:03:48 +0200

    Solution 33 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/e7z5dgq0 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30700b83-0a4c-476a-8549-ef65d68d3 ced.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,4 +1,8 @@
 ```python
 def digitSum(n):
-    s = str(abs(n))
-    return sum(int(digit) for digit in s)
+    s = str(n)
+    ans = 0
+    while s:
+        ans += int(s[0])
+        s = s[1:]
+    return ans
