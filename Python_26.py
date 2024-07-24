6e7c422fb7b6e5e0451a865a4917e4a2aef1eea4
beep boop <githubbot@vadim.me>
2024-07-24 02:11:49 +0200

    Solution 90 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ltngqvn8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6403849f-e845-4594-b292-6b21e2b3e 960.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,8 +1,8 @@
-from collections import OrderedDict
-    def remove_duplicates(input_list=None):
-        if input_list is None:
-            values = input("Enter numbers separated by space: ").strip()
-            input_list = [int(value) for value in values.split()]
-        if not input_list:  
-            return []  
-        return list(OrderedDict.fromkeys(input_list))
+```
+def remove_duplicates(input_list=None):
+    if input_list is None:
+        values = input("Enter numbers separated by space: ").strip()
+        input_list = [int(value) for value in values.split()]
+    if not input_list:  
+        return []  
+    return list(OrderedDict.fromkeys(input_list))
