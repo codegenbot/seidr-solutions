26f497a00930ab6f6f848de07ee63831f22dc401
beep boop <githubbot@vadim.me>
2024-07-24 03:36:21 +0200

    Solution 18 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28f57a22-1d8e-407c-88b8-122988ed6 dd3.py", line 14, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28f57a22-1d8e-407c-88b8-122988ed6 dd3.py", line 11, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,10 +1,5 @@
+```Python
 def solve(K=None):
-    if K is None:
-        N = int(input("Enter another number: "))
-        return ''.join('1' if i % (2**k) < 2**(k-1) else '0' for k in range(1, N.bit_length()+1))
     K = int(input("Enter a number: ") if K is None else K)
-    N = int(input("Enter another number: ") if not isinstance(K, int) else 0)
-    N = int(N)  
-    if N < 1:
-        return str(0)
-    return str(K * ((N - 1) // K))
+    N = int(input("Enter another number: "))
+    return str(bin(K)[2:].zfill(N))
