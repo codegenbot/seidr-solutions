e430c1c4d46dcb4dea14a0ea9ed32b98efb743b8
beep boop <githubbot@vadim.me>
2024-07-24 19:57:16 +0200

    Solution 15 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2xgesiut Prompt:       |                              ^~~~~~ 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp:23: 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp: In function ‘int main()’: 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp:33:20: error: ‘numerical_letter_grade’ was not declared in this scope    33 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 2f094fd0-2bf0-429c-bb1f-63e1defc6b74.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 bool issame(float a, float b) {
-    if (a - b > 0.0001 || b - a > 0.0001)
+    if (a - b > 0.0001)
         return false;
     else
         return true;
@@ -14,16 +14,16 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     for (float g : grades) {
         std::string letter;
         if (g > 4.0) g = 4.0; // cap at 4.0
-        if (g >= 3.7) {letter = "A"; g -= 3.7;}
-        else if (g >= 3.3) {letter = "A-"; g -= 3.3;}
-        else if (g >= 3.0) {letter = "B+"; g -= 3.0;}
-        else if (g >= 2.7) {letter = "B"; g -= 2.7;}
-        else if (g >= 2.3) {letter = "B-"; g -= 2.3;}
-        else if (g >= 2.0) {letter = "C+"; g -= 2.0;}
-        else if (g >= 1.7) {letter = "C"; g -= 1.7;}
-        else if (g >= 1.3) {letter = "C-"; g -= 1.3;}
-        else if (g >= 1.0) {letter = "D+"; g -= 1.0;}
-        else if (g >= 0.7) {letter = "D"; g -= 0.7;}
+        if (g >= 3.7) { letter = "A"; g -= 3.7; }
+        else if (g >= 3.3) { letter = "A-"; g -= 3.3; }
+        else if (g >= 3.0) { letter = "B+"; g -= 3.0; }
+        else if (g >= 2.7) { letter = "B"; g -= 2.7; }
+        else if (g >= 2.3) { letter = "B-"; g -= 2.3; }
+        else if (g >= 2.0) { letter = "C+"; g -= 2.0; }
+        else if (g >= 1.7) { letter = "C"; g -= 1.7; }
+        else if (g >= 1.3) { letter = "C-"; g -= 1.3; }
+        else if (g >= 1.0) { letter = "D+"; g -= 1.0; }
+        else if (g >= 0.7) { letter = "D"; g -= 0.7; }
         else letter = "E";
         if (g > 0.0) letter += "+";
         letter_grades.push_back(letter);
