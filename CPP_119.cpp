2f5e126db399755f8d8ab2a874a2360e3f1aa37d
beep boop <githubbot@vadim.me>
2024-07-24 07:07:23 +0200

    Solution 25 of CPP/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/si66ipfj Prompt: 97585964-52d9-461e-b2c4-e99d4869e448.cpp:3:1: error: expected unqualified-id bef ore ‘for’     3 | for (const string& s : lst) {       | ^~~ 97585964-52d9-461e-b2c4-e99d4869e448.cpp:17:1: error: expected unqualified-id be fore ‘if’    17 | if (count_open == 0 && count_close == 0) {       | ^~ 97585964-52d9-461e-b2c4-e99d4869e448.cpp:19:3: error: expected unqualified-id be fore ‘else’    19 | } else {       |   ^~~~ In file included from 97585964-52d9-461e-b2c4-e99d4869e448.cpp:23: 97585964-52d9-461e-b2c4-e99d4869e448.cpp: In function ‘int main()’: 97585964-52d9-461e-b2c4-e99d4869e448.cpp:25:13: error: ‘match_parens’ was not de clared in this scope    25 |     assert (match_parens({")", "("}) == "Yes" );       |             ^~~~~~~~~~~~

--- CPP_119.cpp
+++ CPP_119.cpp
@@ -27,6 +27,6 @@ std::string match_parens(std::vector<std::string> lst) {
 }
 
 int main() {
-    std::vector<std::string> input = {"())(", "((()))"};
-    cout << match_parens(input);
+    std::vector<std::string> input = {"())((()))", "()"};
+    std::cout << match_parens(input);
 }
