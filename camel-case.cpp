1dc5ca7c75f278eae88e073fcf464d4fe0d0c4bc
beep boop <githubbot@vadim.me>
2024-07-24 02:17:38 +0200

    Solution 4 of camel-case, ratio 0.58 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/fx3q2b6k Prompt:     3 |     getline(cin, input);       |             ^~~ 03609b60-681c-4dd5-b4ee-eb009eb07983.cpp:3:18: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     3 |     getline(cin, input);       |                  ^~~~~       |                  int 03609b60-681c-4dd5-b4ee-eb009eb07983.cpp:3:5: error: ‘getline’ was not declared in this scope     3 |     getline(cin, input);       |     ^~~~~~~ 03609b60-681c-4dd5-b4ee-eb009eb07983.cpp:10:13: error: ‘cout’ was not declared i n this scope    10 |             cout << (char)toupper(c);       |             ^~~~ 03609b60-681c-4dd5-b4ee-eb009eb07983.cpp:10:27: error: ‘toupper’ was not declare d in this scope    10 |             cout << (char)toupper(c);       |                           ^~~~~~~ 03609b60-681c-4dd5-b4ee-eb009eb07983.cpp:13:13: error: ‘cout’ was not declared i n this scope    13 |             cout << c;       |             ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,20 +1,22 @@
-int main() {
-    string input;
-    getline(cin, input);
+#include <iostream>
+#include <string>
+#include <cctype>
 
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+    
     bool capitalize = false;
     for (char& c : input) {
         if (c == '-') {
             capitalize = true;
-        } else if (c != ' ') {
-            if (capitalize) {
-                cout << (char)toupper(c);
-                capitalize = false;
-            } else {
-                cout << c;
-            }
+        } else if (capitalize) {
+            std::cout << (char)std::toupper(c);
+            capitalize = false;
+        } else {
+            std::cout << c;
         }
     }
-
+    
     return 0;
 }
