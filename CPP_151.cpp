72d50bb9e347dcf9f9b29c4f54377def59458ab6
beep boop <githubbot@vadim.me>
2024-07-24 23:39:14 +0200

    Solution 23 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/b5s4nq0j Prompt:     1 | Here's the completed code:       |     ^ a1c5f845-82a7-472a-bea2-d35915e89822.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the completed code:       |     ^~~~~~~~~~~~~~~~~~~~~~ a1c5f845-82a7-472a-bea2-d35915e89822.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the completed code:       | ^~~~ In file included from a1c5f845-82a7-472a-bea2-d35915e89822.cpp:13: a1c5f845-82a7-472a-bea2-d35915e89822.cpp: In function ‘int main()’: a1c5f845-82a7-472a-bea2-d35915e89822.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ a1c5f845-82a7-472a-bea2-d35915e89822.cpp:15:13: error: ‘double_the_difference’ w as not declared in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ a1c5f845-82a7-472a-bea2-d35915e89822.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -16,12 +16,10 @@ int main() {
     int odd_sum = 0; 
 
     float num;
-    cin >> num;
-    while(num != -1.0f){
+    while(std::cin >> num){
         if(int(num) > 0 && int(num) == (int)num){
             odd_sum += pow(int(num), 2);
         }
-        cin >> num;
     }
 
     assert (double_the_difference(lst) == odd_sum ); 
