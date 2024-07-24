e4a10900755bbe220afa6b855730c59d2cedb5aa
beep boop <githubbot@vadim.me>
2024-07-24 15:02:00 +0200

    Solution 84 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/evk8w1n3 Prompt: /usr/bin/ld: /tmp/ccrP9Bji.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': cdc74eed-7eca-409f-aacb-15d355145e6c.cpp:(.text+0x6c): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: cdc74eed-7eca-409f-aacb-15d355145e6c.cpp:(.text+0x75): undefined re ference to `EVP_md5' /usr/bin/ld: cdc74eed-7eca-409f-aacb-15d355145e6c.cpp:(.text+0x87): undefined re ference to `EVP_DigestInit' /usr/bin/ld: cdc74eed-7eca-409f-aacb-15d355145e6c.cpp:(.text+0xb7): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: cdc74eed-7eca-409f-aacb-15d355145e6c.cpp:(.text+0xcf): undefined re ference to `EVP_DigestFinal' /usr/bin/ld: cdc74eed-7eca-409f-aacb-15d355145e6c.cpp:(.text+0xdb): undefined re ference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -10,17 +10,18 @@ std::string string_to_md5(const std::string& text) {
     }
 
     unsigned char digest[MD5_DIGEST_LENGTH];
-    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    
+    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
     EVP_DigestInit(mdctx, EVP_md5());
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal(mdctx, digest, nullptr);
+    EVP_DigestFinal(mdctx, digest, NULL);
+    
+    EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
     for (int i = 0; i < 16; i++) {
         sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
     }
-
-    EVP_MD_CTX_free(mdctx);
 
     return std::string(mdString);
 }
