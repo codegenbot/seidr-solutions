f2e807442edc2fdf28248ff7213b6c3303581c12
beep boop <githubbot@vadim.me>
2024-07-24 21:01:51 +0200

    Solution 15 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/h61prole Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -1,20 +1,6 @@
-```
 from typing import List
+import re
 
 def separate_paren_groups(paren_string: str) -> List[str]:
-    result = []
-    group = ''
-    count = 0
-    for char in paren_string:
-        if char == ' ':
-            continue
-        if char == '(':
-            count += 1
-            group += char
-        elif char == ')':
-            count -= 1
-            group += char
-            if count == 0:
-                result.append(group)
-                group = ''
-    return result
+    groups = re.split('(?<=\\))|(?<=\\(')', paren_string)
+    return [g for g in groups if g]
