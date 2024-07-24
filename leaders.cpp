9152f837981250248dab7be756c20053afd6b400
beep boop <githubbot@vadim.me>
2024-07-24 03:55:59 +0200

    Solution 87 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/sv2y1qem Prompt: caaafacb-9048-4cc3-9761-3430a83ff996.cpp:1:9: error: ‘nums’ was not declared in this scope     1 | int n = nums.size();       |         ^~~~ caaafacb-9048-4cc3-9761-3430a83ff996.cpp:2:16: error: ‘nums’ was not declared in  this scope     2 | int maxRight = nums[n-1];       |                ^~~~ caaafacb-9048-4cc3-9761-3430a83ff996.cpp:3:1: error: ‘vector’ does not name a ty pe     3 | vector<int> leaders;       | ^~~~~~ caaafacb-9048-4cc3-9761-3430a83ff996.cpp:5:1: error: expected unqualified-id bef ore ‘for’     5 | for (int i = n-2; i >= 0; i--) {       | ^~~ caaafacb-9048-4cc3-9761-3430a83ff996.cpp:5:19: error: ‘i’ does not name a type     5 | for (int i = n-2; i >= 0; i--) {       |                   ^ caaafacb-9048-4cc3-9761-3430a83ff996.cpp:5:27: error: ‘i’ does not name a type     5 | for (int i = n-2; i >= 0; i--) {       |                           ^

--- leaders.cpp
+++ leaders.cpp
@@ -1,9 +1,30 @@
-int i;
-int maxRight = nums[n-1];
-leaders.push_back(maxRight);
-for (i = n-2; i >= 0; i--) {
-    if (nums[i] > maxRight) {
-        maxRight = nums[i];
-        leaders.push_back(maxRight);
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> nums;
+    int num;
+    
+    while (cin >> num) {
+        nums.push_back(num);
     }
+    
+    int n = nums.size();
+    int maxRight = nums[n-1];
+    vector<int> leaders;
+
+    for (int i = n-2; i >= 0; i--) {
+        if (nums[i] >= maxRight) {
+            maxRight = nums[i];
+            leaders.push_back(maxRight);
+        }
+    }
+
+    for (int leader : leaders) {
+        cout << leader << " ";
+    }
+
+    return 0;
 }
