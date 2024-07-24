905f371c3a01e5f6cef4fbd594930b7ecc9a5b7a
beep boop <githubbot@vadim.me>
2024-07-24 21:54:20 +0200

    Solution 9 of CPP/130, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a4p75i0w Prompt: Complete the following code given the task description and function signature.

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,21 +1,19 @@
-#include<stdio.h>
-#include<vector>
+#include <stdio.h>
+#include <vector>
 using namespace std;
 
 vector<int> tri(int n) {
-    vector<int> result(0);
-    if (n == 0) return result;
-    result.push_back(1);
-    if (n > 1) {
-        result.push_back(3);
-        for (int i = 2; i < n; i++) {
-            int sum = 0;
-            if (i % 2 == 0) {
-                sum += 1 + i / 2;
-            } else {
-                sum = result[i - 1] + result[i - 2] + result[i];
+    vector<int> result(n + 1);
+    if (n >= 0) {
+        result[0] = 3;
+        if (n >= 1) {
+            result[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                if (i % 2 == 0)
+                    result[i] = 1 + i / 2;
+                else
+                    result[i] = result[i - 1] + result[i - 2] + result[i - 3];
             }
-            result.push_back(sum);
         }
     }
     return result;
