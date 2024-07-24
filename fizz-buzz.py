8ba6a11565b07917426e31dc814916d59258d733
beep boop <githubbot@vadim.me>
2024-07-24 17:25:44 +0200

    Solution 90 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/c76migli Prompt: it must return Fizz for input 575814, but it returns .

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,9 +1,9 @@
 def fizz_buzz(x):
     if x % 15 == 0:
-        return "FizzBuzz\n"
+        return "FizzBuzz"
     elif x % 3 == 0:
-        return "Fizz\n"
+        return "Fizz"
     elif x % 5 == 0:
-        return "Buzz\n"
+        return "Buzz"
     else:
         return str(x)
