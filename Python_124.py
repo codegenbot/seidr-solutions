56c5bf929b40c3dd1ed44f0be3a4dc59a3599395
beep boop <githubbot@vadim.me>
2024-07-24 04:32:48 +0200

    Solution 15 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tbflllxq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/973937b2-6ba6-4877-bc2b-9ea762630 371.py", line 11, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/973937b2-6ba6-4877-bc2b-9ea762630 371.py", line 8, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_124.py
+++ Python_124.py
@@ -1,6 +1,16 @@
+```
 def valid_date(date):
-    day_month = date.split("-")[:2]
-    if len(day_month) == 2:
-        day, month = map(int, day_month)
-        year = int(date.split("-")[2])
+    parts = date.split("-")
+    day, month = map(int, parts[:2])
+    year = int(parts[-1]) if len(parts) > 2 else None
+    
+    if len(parts) != 3:
+        return False
+    if year and (len(str(year)) != 4 or not str(year).isdigit()):
+        return False
+    if day < 1 or day > 31:
+        return False
+    if month < 1 or month > 12:
+        return False
+    
     return True
