4ec511b9de3d0a88f8bab18649a88d5b976a999f
beep boop <githubbot@vadim.me>
2024-07-24 03:54:19 +0200

    Solution 66 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/9x2ymadf Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -4,25 +4,19 @@
 
 int main() {
     const char* str = "camel-case example-test-string";
-    std::string result;
+    std::string result = str;
     bool capitalizeNext = true;
 
     for (const char* p = str; *p; p++) {
         if (*p == '-') {
             p++; // Skip the '-'
-            if (!capitalizeNext) {
-                result += tolower(*p);
-            } else {
-                result += toupper(*p);
-                capitalizeNext = false;
-            }
+        } else if (!strchr(str, '-')) { 
+            capitalizeNext = true;
+        } else if (capitalizeNext) {
+            result += toupper(*p);
+            capitalizeNext = false;
         } else {
-            if (capitalizeNext) {
-                result += toupper(*p);
-                capitalizeNext = false;
-            } else {
-                result += tolower(*p);
-            }
+            result += tolower(*p);
         }
     }
 
