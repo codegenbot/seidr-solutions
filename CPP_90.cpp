f0fa1b93069924bf56d26b0b8e237f8560786132
beep boop <githubbot@vadim.me>
2024-07-24 21:51:17 +0200

    Solution 97 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cspj3ka5 Prompt: f8398ce1-dea9-490e-8910-68ea76a635aa.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst) {       |                   ^~~~~~ f8398ce1-dea9-490e-8910-68ea76a635aa.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst) {       |                          ^~~ In file included from f8398ce1-dea9-490e-8910-68ea76a635aa.cpp:9: f8398ce1-dea9-490e-8910-68ea76a635aa.cpp: In function ‘int main()’: f8398ce1-dea9-490e-8910-68ea76a635aa.cpp:11:26: error: ‘next_smallest’ cannot be  used as a function    11 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,9 +1,10 @@
 #include <vector>
+#include <algorithm>
 
 int next_smallest(vector<int> lst) {
-    if (lst.size() < 2) return -1; 
+    if (lst.size() < 2) return -1; // None
     vector<int>::iterator it = min_element(lst.begin(), lst.end());
     vector<int>::iterator it2 = find(it, lst.end(), *it);
-    if (it2 == lst.end()) return *it; 
+    if (it2 == lst.end()) return *it; // Return the smallest element as there is no second smallest
     return *(it2 + 1); 
 }
