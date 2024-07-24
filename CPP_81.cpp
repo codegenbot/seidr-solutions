8541f4b8c0d4b51da2628fd9991b45ecb9d8bece
beep boop <githubbot@vadim.me>
2024-07-24 13:39:28 +0200

    Solution 18 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/movnupb1 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 7514f60b-d13d-43e6-9390-db1e75aff046.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,7 +1,6 @@
 ```cpp
 #include <vector>
 #include <string>
-#include <initializer_list>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
@@ -41,29 +40,33 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
 int main_test() {
     std::vector<float> grades = {0, 0.7};
     std::vector<std::string> a = numerical_letter_grade(grades);
-    if (a[0] >= 4.0)
-        a[0] = "A+";
-    else if (a[0] > 3.7)
-        a[0] = "A";
-    else if (a[0] > 3.3)
-        a[0] = "A-";
-    else if (a[0] > 3.0)
-        a[0] = "B+";
-    else if (a[0] > 2.7)
-        a[0] = "B";
-    else if (a[0] > 2.3)
-        a[0] = "B-";
-    else if (a[0] > 2.0)
-        a[0] = "C+";
-    else if (a[0] > 1.7)
-        a[0] = "C";
-    else if (a[0] > 1.3)
-        a[0] = "C-";
-    else if (a[0] > 1.0)
-        a[0] = "D+";
-    else
-        a[0] = "D";
-    std::vector<std::string> b = {a[0]};
-    bool same = issame({a}, b);
+    std::vector<std::string> b;
+    for (float g : grades) {
+        if (g >= 4.0)
+            b.push_back("A+");
+        else if (g > 3.7)
+            b.push_back("A");
+        else if (g > 3.3)
+            b.push_back("A-");
+        else if (g > 3.0)
+            b.push_back("B+");
+        else if (g > 2.7)
+            b.push_back("B");
+        else if (g > 2.3)
+            b.push_back("B-");
+        else if (g > 2.0)
+            b.push_back("C+");
+        else if (g > 1.7)
+            b.push_back("C");
+        else if (g > 1.3)
+            b.push_back("C-");
+        else if (g > 1.0)
+            b.push_back("D+");
+        else if (g > 0.7)
+            b.push_back("D");
+        else
+            b.push_back("E");
+    }
+    bool same = issame(a, b);
     return 0;
 }
