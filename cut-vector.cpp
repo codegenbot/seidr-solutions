388d3b5aa8fe1cf5d4422a3828f57b976bcc4a1d
beep boop <githubbot@vadim.me>
2024-07-24 06:05:05 +0200

    Solution 16 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,42 +1,48 @@
 #include <vector>
 using namespace std;
 
-vector<int> cutVector(vector<int>& nums) {
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
     int minDiff = INT_MAX;
     int splitIndex = 0;
     
-    for (int i = 1; i < nums.size(); i++) {
-        int diff = abs(nums[i] - nums[0]);
+    for (int i = 1; i < vec.size(); i++) {
+        int leftSum = 0, rightSum = 0;
+        
+        for (int j = 0; j < i; j++) {
+            leftSum += vec[j];
+        }
+        
+        for (int j = i; j < vec.size(); j++) {
+            rightSum += vec[j];
+        }
+        
+        int diff = abs(leftSum - rightSum);
+        
         if (diff <= minDiff) {
             minDiff = diff;
             splitIndex = i;
         }
     }
     
-    return {vector<int>(nums.begin(), nums.begin() + splitIndex), vector<int>(nums.begin() + splitIndex, nums.end())};
+    vector<int> leftVec(vec.begin(), vec.begin() + splitIndex);
+    vector<int> rightVec(vec.begin() + splitIndex, vec.end());
+    
+    return {leftVec, rightVec};
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+    vector<int> vec(n);
+    for (auto &x : vec) cin >> x;
     
-    vector<int> result = cutVector(nums);
+    pair<vector<int>, vector<int>> result = cutVector(vec);
     
-    cout << "1 ";
-    for (int num : result[0]) {
-        cout << num << " ";
-    }
-    cout << endl;
-    
-    cout << "0 ";
-    for (int i = 0; i < nums.size() - result[0].size(); i++) {
-        cout << "0 ";
-    }
-    cout << endl;
+    cout << "[";
+    for (const auto &x : result.first) cout << x << " ";
+    cout << "] ["; 
+    for (const auto &x : result.second) cout << x << " ";
+    cout << "]" << endl;
     
     return 0;
 }
