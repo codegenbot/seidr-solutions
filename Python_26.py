a40419f433014f47191601a2cc264705dbe9aa02
beep boop <githubbot@vadim.me>
2024-07-24 11:09:09 +0200

    Solution 38 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bharn9m1 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a012846e-eb93-4c84-be63-1e363e642 3dc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,6 @@
-```
 from typing import List
 
 
 def remove_duplicates(numbers: List[int]) -> List[int]:
     unique_numbers = list(set(numbers))
-    return [i for i in numbers if i in unique_numbers]
+    return unique_numbers
