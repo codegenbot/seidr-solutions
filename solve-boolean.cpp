902c7779021278ffc9a09cbb04771ac33585cf58
beep boop <githubbot@vadim.me>
2024-07-24 04:53:53 +0200

    Solution 74 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/z7qa9oa3 Prompt: pe     8 |     string opStr;       |     ^~~~~~ 98c8b9ea-9e91-4d52-9bc0-e1c40ee1872e.cpp:9:5: error: expected unqualified-id bef ore ‘for’     9 |     for (char c : s.substr(0, j)) {       |     ^~~ 98c8b9ea-9e91-4d52-9bc0-e1c40ee1872e.cpp:13:5: error: expected unqualified-id be fore ‘if’    13 |     if (opStr[0] == '|') {       |     ^~ 98c8b9ea-9e91-4d52-9bc0-e1c40ee1872e.cpp:22:7: error: expected unqualified-id be fore ‘else’    22 |     } else {       |       ^~~~ 98c8b9ea-9e91-4d52-9bc0-e1c40ee1872e.cpp:43:5: error: expected unqualified-id be fore ‘return’    43 |     return result;       |     ^~~~~~ 98c8b9ea-9e91-4d52-9bc0-e1c40ee1872e.cpp:44:1: error: expected declaration befor e ‘}’ token    44 | }       | ^

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,1 +1,47 @@
+```cpp
+if (s.empty())
+    return false;
+
+int j = 0;
+while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+    j++;
+
 string opStr;
+for (char c : s.substr(0, j)) {
+    if(c == '|')
+        opStr = "|";
+    else
+        opStr = "&";
+}
+
+bool result = false;
+if (opStr[0] == '|') {
+    for (int k = j; k < s.length(); k++) {
+        if (s[k] == 'T')
+            result = true;
+        else if (s[k] == 'F') {
+            result = false;
+            return result;
+        }
+    }
+} else {
+    bool left = false, right = false;
+    for (int i = 0; i < j; i++) {
+        if (s[i] == 'T' && opStr[1] != '|')
+            left = true;
+        else if (s[i] == 'F' && opStr[1] != '&') {
+            return false;
+        }
+    }
+    for (int i = j; i < s.length(); i++) {
+        if (s[i] == 'T' && opStr[0] == '|')
+            right = true;
+        else if (s[i] == 'F' && opStr[0] == '&') {
+            right = false;
+            return right;
+        }
+    }
+    result = (opStr[0] == '|' ? left || right : left && right);
+}
+
+return result;
