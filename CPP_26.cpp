cdc05fded562c99dfb3751ba86458e6d48b86afb
beep boop <githubbot@vadim.me>
2024-07-24 19:13:29 +0200

    Solution 88 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/to49ik7c Prompt: 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp:19: 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp: In function ‘int main()’: 44f9b78f-0668-4d23-97b6-16d56cc85ad5.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -10,8 +10,3 @@ std::vector<int> remove_duplicates(std::vector<int> numbers){
 bool same(const std::vector<int>& a, const std::vector<int>& b) {
     return (a == b);
 }
-
-int main() {
-    assert(same(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5})); 
-    return 0;
-}
