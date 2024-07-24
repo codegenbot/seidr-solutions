7bc4780b0ed9ebc85a872053d8b44ea4ee86f727
beep boop <githubbot@vadim.me>
2024-07-24 23:39:02 +0200

    Solution 64 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/hqexafdi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b088044-171e-40ec-9c31-2a0bb774b b43.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b088044-171e-40ec-9c31-2a0bb774b b43.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,3 +1,13 @@
-```
 def modp(n: int, p: int):
-    return pow(n, p - 1 + 1, p)
+    def egcd(a: int, b: int) -> tuple:
+        if a == 0:
+            return b, 0, 1
+        else:
+            g, y, x = egcd(b % a, a)
+            return g, x - (b // a) * y, y
+
+    g, x, _ = egcd(p, n)
+    if g != 1:
+        raise ValueError("Modular inverse does not exist")
+    else:
+        return pow(n, p - 2, p) * n % p
