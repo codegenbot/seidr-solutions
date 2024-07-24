9f0b279d77d15070ce494511df384e460b362914
beep boop <githubbot@vadim.me>
2024-07-24 03:59:56 +0200

    Solution 18 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8c90227-32d4-403e-96a1-7245dd993 2e4.py", line 1     return [''.join([name_dict[i] if i < 10 else name_dict[int(i)] for i in [str (i) for i in arr if 1 <= int(i) <= 19]])].replace("Twenty", "Tw").replace("Eight een", "Eight")     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_105.py
+++ Python_105.py
@@ -1,2 +1,2 @@
-def solve_task(arr):
+def solve(arr):
     return [''.join([name_dict[i] if i < 10 else name_dict[int(i)] for i in [str(i) for i in arr if 1 <= int(i) <= 19]])].replace("Twenty", "Tw").replace("Eighteen", "Eight")
