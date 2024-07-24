a5ebb042f9a3f9a84cb294aca15ec286059f391d
beep boop <githubbot@vadim.me>
2024-07-24 08:33:35 +0200

    Solution 52 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/osxtbk4w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/db1c3ee6-5612-4a33-963c-610e60365 27f.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -6,7 +6,7 @@ def decode_cyclic(s: str):
         else:
             group = s[i:i+3]
         if len(group) == 1:
-            result += group[0] * (2 + len(group)) 
+            result += group[0] * (len(group) // 2 + 1)
         else:
             result += group[2] + group[:2]
     return result
