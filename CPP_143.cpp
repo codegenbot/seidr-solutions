847a106f6bc642a041859d218361efd1a4638204
beep boop <githubbot@vadim.me>
2024-07-24 22:30:58 +0200

    Solution 7 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/eqowgi31 Prompt: Complete the following code given the task description and function signature.

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,40 +1,40 @@
 #include <vector>
+#include <algorithm>
 
 string words_in_sentence(string sentence) {
     vector<string> words = split(sentence, ' ');
     string result;
 
-    for (const auto& word : words) {
-        if (is_prime(word.length())) {
-            result += word + " ";
+    for (string word : words) {
+        int len = word.length();
+        bool isPrime = true;
+        if (len > 1) {
+            for (int i = 2; i * i <= len; i++) {
+                if (len % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                result += word + " ";
         }
     }
 
     return result.substr(0, result.size() - 1);
 }
 
-bool is_prime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= num; ++i) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<string> split(const string& str, char delimiter) {
+vector<string> split(string str, char delimiter) {
     vector<string> tokens;
+    string token;
     size_t pos = 0;
-    size_t prev = 0;
 
-    while ((pos = str.find(delimiter, prev)) != string::npos) {
-        tokens.push_back(str.substr(prev, pos - prev));
-        prev = pos + 1;
+    while ((pos = str.find(delimiter)) != string::npos) {
+        token = str.substr(0, pos);
+        tokens.push_back(token);
+        str.erase(0, pos + 1);
     }
-    tokens.push_back(str.substr(prev));
+
+    tokens.push_back(str);
 
     return tokens;
 }
