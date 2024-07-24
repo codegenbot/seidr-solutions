9107c88eafdf327df268cb774bf23621855dd5f1
beep boop <githubbot@vadim.me>
2024-07-24 15:46:48 +0200

    Solution 32 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0e2bea4c-18c6-40e3-a4ef-8f21a85e4 459.py", line 10, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0e2bea4c-18c6-40e3-a4ef-8f21a85e4 459.py", line 7, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: sorted_list_sum() takes 0 positional arguments but 1 was given

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,4 @@
-```
-def sorted_list_sum(lst=None):
-    if lst is None:
-        lst = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
-    return "".join(sorted([word for word in lst if len(word) % 2 == 0], key=len))
+def sorted_list_sum(words=None):
+    if words is None:
+        words = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
+    return "".join(sorted([word for word in words if len(word) % 2 == 0], key=len))
