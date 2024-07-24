85f82b4bf5db739c7ab0e49eb177463b7153b37e
beep boop <githubbot@vadim.me>
2024-07-24 11:39:09 +0200

    Solution 58 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bharn9m1 Prompt: Program did not receive expected input

--- Python_26.py
+++ Python_26.py
@@ -1,7 +1,2 @@
-```Python
-from typing import List
-
-
 def remove_duplicates(numbers: List[int]) -> List[int]:
-    unique_numbers = list(set(numbers))
-    return [num for num in sorted(unique_numbers)]
+    return list(set(numbers))
