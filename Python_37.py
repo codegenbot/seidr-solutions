b7c85ff65c7f64794264bc6f2196fe6f2a613d9a
beep boop <githubbot@vadim.me>
2024-07-24 10:43:15 +0200

    Solution 7 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/t0b75pbn Prompt: 6cc.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d2eaa9b-44c6-4489-a76f-e66713a25 6cc.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d2eaa9b-44c6-4489-a76f-e66713a25 6cc.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d2eaa9b-44c6-4489-a76f-e66713a25 6cc.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if (i - 1) // 2 * 2 + 1 == j])[0]                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2d2eaa9b-44c6-4489-a76f-e66713a25 6cc.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if (i - 1) // 2 * 2 + 1 == j])[0]                                                                          ^ NameError: name 'j' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,11 @@
 def sort_even(l: list):
-    return [x if i % 2 != 0 else sorted([y for y, _ in zip(l, range(len(l))) if (_ - (i//2)*2) % 2 == 1])[0]
-           for i, x in enumerate(l)]
+    return [
+        (
+            x
+            if i % 2 != 0
+            else sorted(
+                [y for y, _ in zip(l, range(len(l))) if (_ - (i // 2) * 2) % 2 == 1]
+            )[0]
+        )
+        for i, x in enumerate(l)
+    ]
