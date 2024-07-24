24b3777131070ea91cc941221e7df0ca328aad37
beep boop <githubbot@vadim.me>
2024-07-24 08:36:01 +0200

    Solution 4 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fm33adyh Prompt: it must return 0 2 for input WBWO WWWR, but it returns .

--- mastermind.py
+++ mastermind.py
@@ -1,24 +1,18 @@
 def mastermind(code, guess):
-    black = 0
     white = 0
+    black = 0
     code_count = [0] * 6
     guess_count = [0] * 6
 
     for i in range(4):
         if code[i] == guess[i]:
             black += 1
-        else:
+        elif code_count[ord(code[i]) - ord("A")] < 1:
             code_count[ord(code[i]) - ord("A")] += 1
+        elif guess_count[ord(guess[i]) - ord("A")] < 1:
             guess_count[ord(guess[i]) - ord("A")] += 1
 
     for i in range(6):
         white += min(code_count[i], guess_count[i])
-
-    remaining_code = [c for c in code if c != guess[code.index(c)]]
-    remaining_guess = [c for c in guess if c != code[guess.index(c)]]
-
-    for char in remaining_code:
-        if char in remaining_guess:
-            white += 1
 
     return str(black) + "\n" + str(white)
