89ea9f4d1a3965f23d42e96a28660d49e7cfa813
beep boop <githubbot@vadim.me>
2024-07-24 15:20:14 +0200

    Solution 6 of Python/150, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/v6hir98b Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21a85103-c42b-4e80-a75e-4d03632ef c6d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_150.py
+++ Python_150.py
@@ -1,4 +1,4 @@
-```
+"""
 def x_or_y(n, x, y):
     def is_prime(num):
         if num < 2:
@@ -7,16 +7,9 @@ def x_or_y(n, x, y):
             if num % i == 0:
                 return False
         return True
-    
-    count = 0
-    while x_or_y_helper(n, count) <= n:
-        if is_prime(x_or_y_helper(n, count)):
-            return str(count)
-        count += 1
 
-def x_or_y_helper(n, count):
-    sum = 0
-    for i in range(2, count + 1):
-        if is_prime(i):
-            sum += i
-    return sum
+    if is_prime(n):
+        return x
+    else:
+        return y
+"""
