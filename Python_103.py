03d30c825ee6d107f1c16f360918ff988479bf7f
beep boop <githubbot@vadim.me>
2024-07-24 17:29:44 +0200

    Solution 70 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/iqu0ssj4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3cfa8c02-a5c0-4112-a21d-6d496601f 624.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_103.py
+++ Python_103.py
@@ -1,11 +1,11 @@
-import statistics
-
-def average():
+def rounded_avg():
     while True:
         try:
             n = int(input("Enter first number: "))
             m = int(input("Enter second number: "))
             if n > m:
                 return -1
-            avg = int(statistics.mean([n, m]) + 0.5) 
-            return bin(int(avg))[2:]
+            avg = (n + m) // 2 
+            return bin(avg)[2:]
+        except ValueError:
+            print("Invalid input. Please enter integers.")
