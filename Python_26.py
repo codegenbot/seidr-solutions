2d6109758a4621ae44cc1543f6819497eb7d9a0c
beep boop <githubbot@vadim.me>
2024-07-24 02:14:56 +0200

    Solution 91 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ltngqvn8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be76626f-1916-4603-a3bf-9c79f0c3e f3d.py", line 2     def remove_duplicates(input_list=None): IndentationError: unexpected indent

--- Python_26.py
+++ Python_26.py
@@ -1,5 +1,5 @@
-```
 def remove_duplicates(input_list=None):
+    from collections import OrderedDict
     if input_list is None:
         values = input("Enter numbers separated by space: ").strip()
         input_list = [int(value) for value in values.split()]
