6d5cd894e8cb546dad58a824380f52fc687a7b89
beep boop <githubbot@vadim.me>
2024-07-24 21:43:24 +0200

    Solution 26 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/w71tam8o Prompt: ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     3 | #include <cassert>   +++ |+#include <iostream>     4 | e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp:13:28: error: ‘same_chars’ cannot be us ed as a function    13 |     std::cout << same_chars("abcde", "edcba");       |                  ~~~~~~~~~~^~~~~~~~~~~~~~~~~~ e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp: At global scope: e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp:17: e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp: In function ‘int main()’: e64bbc1a-892c-41dc-8db1-83b54bd8f602.cpp:19:23: error: ‘same_chars’ cannot be us ed as a function    19 |     assert (same_chars("aabb", "aaccc") == false);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <string>
 #include <cassert>
-#include <initializer_list>
+#include <iostream>
 
 bool same_chars(string s0, string s1) {
     sort(s0.begin(), s0.end());
@@ -11,5 +11,6 @@ bool same_chars(string s0, string s1) {
 
 int main() {
     assert (same_chars("aabb", "aaccc") == false);
+    std::cout << same_chars("abcde", "edcba") << std::endl;
     return 0;
 }
