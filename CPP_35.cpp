f55fc75fba82956cf78d163a92a9b71e5ff3dd4f
beep boop <githubbot@vadim.me>
2024-07-24 18:32:23 +0200

    Solution 17 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nwhbfoew Prompt: 96a87154-21fc-4ab3-9205-5652cca938d2.cpp: At global scope: 96a87154-21fc-4ab3-9205-5652cca938d2.cpp:10:5: error: redefinition of ‘int main( )’    10 | int main(){       |     ^~~~ 96a87154-21fc-4ab3-9205-5652cca938d2.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~ In file included from 96a87154-21fc-4ab3-9205-5652cca938d2.cpp:9: 96a87154-21fc-4ab3-9205-5652cca938d2.cpp: In function ‘int main()’: 96a87154-21fc-4ab3-9205-5652cca938d2.cpp:11:17: error: ‘max_element’ was not dec lared in this scope; did you mean ‘std::max_element’?    11 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 96a87154-21fc-4ab3-9205-5652cca938d2.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: ‘std::max_element’ d eclared here   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,9 +1,8 @@
-```c
 #include <algorithm>
 #include <cmath>
 
 int main() {
-    int tempMax;
-    tempMax = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
-    return std::abs(tempMax);
+    int max;
+    max = *std::max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10});
+    return std::abs(max);
 }
