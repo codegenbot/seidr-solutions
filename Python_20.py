2d9148288effa6c48935706bfe72f01c3ef05c9d
beep boop <githubbot@vadim.me>
2024-07-24 17:32:00 +0200

    Solution 5 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2bdc0vp3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/299f9385-7083-455d-b19c-f52c76742 e04.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,3 +1,4 @@
+```
 # Find all pairs of numbers in two lists that are within a given distance.
 def find_closest_elements(numbers: List[float], target, num_to_include):
     sorted_numbers = sorted(set(numbers))
@@ -11,13 +12,11 @@ def find_closest_elements(numbers: List[float], target, num_to_include):
                 closest_nums.append((num, pair_num))
     return tuple(closest_nums)
 
-
 def main():
     numbers = [int(i.strip()) for i in input().split()]
     target = float(input())
     num_to_include = int(input())
     print(find_closest_elements(numbers, target, num_to_include))
-
 
 if __name__ == "__main__":
     main()
