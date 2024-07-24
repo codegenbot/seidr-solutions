766d2494baf15c0a4d9c6ea5331fd566b29abff4
beep boop <githubbot@vadim.me>
2024-07-24 22:40:37 +0200

    Solution 30 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xcwm9szj Prompt: 1a18a384-ba68-457d-81fb-0ab2815ad6cf.cpp: In function ‘std::vector<int> largest_ smallest_integers(std::vector<int>)’: 1a18a384-ba68-457d-81fb-0ab2815ad6cf.cpp:7:24: error: ‘INT_MAX’ was not declared  in this scope     7 |     int min_positive = INT_MAX;       |                        ^~~~~~~ 1a18a384-ba68-457d-81fb-0ab2815ad6cf.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <vector>   +++ |+#include <climits>     3 | using namespace std; 1a18a384-ba68-457d-81fb-0ab2815ad6cf.cpp: At global scope: 1a18a384-ba68-457d-81fb-0ab2815ad6cf.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 1a18a384-ba68-457d-81fb-0ab2815ad6cf.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,15 +1,37 @@
-std::vector<int> largest_smallest_integers(std::vector<int> lst) {
-    int maxNeg = 0;
-    int minPos = INT_MAX;
+```cpp
+#include <iostream>
+#include <vector>
+#include <climits> // Include this for INT_MAX
+using namespace std;
 
+vector<int> largest_smallest_integers(vector<int> lst) {
+    int max_negative = 0;
+    int min_positive = INT_MAX;
+    
     for (int num : lst) {
-        if (num < 0 && num > maxNeg) {
-            maxNeg = num;
+        if (num < 0 && num > max_negative) {
+            max_negative = num;
         }
-        else if (num > 0 && num < minPos) {
-            minPos = num;
+        else if (num > 0 && num < min_positive) {
+            min_positive = num;
         }
     }
+    
+    return {(max_negative >= 0)? 0 : max_negative, (min_positive <= 0)? 0 : min_positive};
+}
 
-    return {(maxNeg < 0 ? maxNeg : 0), (minPos > 0 ? minPos : 0)};
+int main() {
+    vector<int> lst1 = {2, 4, 1, 3, 5, 7};
+    vector<int> result1 = largest_smallest_integers(lst1);
+    cout << "largest_smallest_integers({2, 4, 1, 3, 5, 7}) == {" << result1[0] << "," << result1[1] << "}\n";
+    
+    vector<int> lst2 = {};
+    vector<int> result2 = largest_smallest_integers(lst2);
+    cout << "largest_smallest_integers({}) == {" << result2[0] << "," << result2[1] << "}\n";
+    
+    vector<int> lst3 = {0};
+    vector<int> result3 = largest_smallest_integers(lst3);
+    cout << "largest_smallest_integers({0}) == {" << result3[0] << "," << result3[1] << "}\n";
+    
+    return 0;
 }
