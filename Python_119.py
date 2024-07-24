0fe5e837c6d82b9c5e0c7a1c44d6dd7513a355a7
beep boop <githubbot@vadim.me>
2024-07-24 05:52:18 +0200

    Solution 59 of Python/119, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7s6an846 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b16b645-d195-4d8e-b0d0-bcd0e33bd 6cd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,18 +1,15 @@
-```
 def match_parens(s):
     stack = []
-    for char in s:
+    for char in str(s):
         if char == "(":
             stack.append(char)
         elif char == ")":
             if len(stack) > 0 and stack[-1] == "(":
                 stack.pop()
             else:
-                return "No"
-    if not stack:
-        return "Yes"
-    else:
-        return "No"
+                return "No" if stack else "Yes"
+    return "Yes" if not stack else "No"
+
 
 def check():
     while True:
@@ -20,6 +17,6 @@ def check():
         if s.lower() == "q":
             break
         elif not s.isalpha():
-            print(match_parens(s))
+            print(match_parens(s.replace("[", "")))
         else:
             print("Invalid input. Please enter a string or type 'q' to quit.")
