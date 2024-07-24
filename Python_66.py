8999f8db232f474ecb8cf7ec966a65c1d273e05e
beep boop <githubbot@vadim.me>
2024-07-24 05:16:41 +0200

    Solution 96 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8vy4sey4 Prompt: Program did not receive expected input

--- Python_66.py
+++ Python_66.py
@@ -1,5 +1,6 @@
+```
 def digitSum(s):
-    return sum(int(c) for c in s if c.isdigit()) or -1
+    return sum(int(c) for c in s if c.isdigit())
 
-input_s = str(input("Enter an integer: "))
+input_s = str(input("Enter a string: "))
 print(digitSum(input_s))
