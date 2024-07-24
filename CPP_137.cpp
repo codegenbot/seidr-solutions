acd9677422048d862fd5acd612a1ffe0b0cbcdf9
beep boop <githubbot@vadim.me>
2024-07-24 11:07:36 +0200

    Solution 86 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/m3e7eqps Prompt: 80edfc08-b964-4795-ba2b-55f44cf802cc.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -9,17 +9,17 @@ boost::any compare_one(boost::any a, boost::any b) {
         return (int)boost::any_cast<int>(a) > boost::any_cast<double>(b) ? a : b;
     } else if (a.type() == typeid(float) && b.type() == typeid(double)) {
         return boost::any_cast<float>(a) > boost::any_cast<double>(b) ? a : b;
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string strA = boost::any_cast<std::string>(a);
-        std::string strB = boost::any_cast<std::string>(b);
-        return std::stod(strA) > std::stod(strB) ? a : b;
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
-        return std::stod(boost::any_cast<std::string>(a)) > boost::any_cast<float>(b) ? a : b;
-    } else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        return (int)boost::any_cast<int>(a) > std::stod(boost::any_cast<std::string>(b)) ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string strA = boost::any_cast<string>(a);
+        string strB = boost::any_cast<string>(b);
+        return stod(strA) > stod(strB) ? a : b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        return stof(boost::any_cast<string>(a)) > boost::any_cast<float>(b) ? a : b;
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        return (int)boost::any_cast<int>(a) > stod(boost::any_cast<string>(b)) ? a : b;
     } else if (a.type() == typeid(double) && b.type() == typeid(float)) {
         return boost::any_cast<double>(a) > boost::any_cast<float>(b) ? a : b;
     } else {
-        return typeid(int);
+        return a;
     }
 }
