dbfe694083afda4651a920e95dd3f205e73d7f18
beep boop <githubbot@vadim.me>
2024-07-24 09:14:36 +0200

    Solution 2 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dk4zfmad Prompt:       |                    ^~~ f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp:9: f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp: In function ‘int main()’: f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f671a60f-1618-4c91-aa14-b57c1b5b4396.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,20 +1,15 @@
 #include <vector>
+#include <algorithm>
 
-vector<int> common(vector<int> l1, vector<int> l2) {
-    sort(l1.begin(), l1.end());
-    sort(l2.begin(), l2.end());
-
+bool issame(vector<int> a, vector<int> b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
     vector<int> result;
-    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-    return 0;
+vector<int> common(vector<int> l1, vector<int> l2) {
+    return issame(l1, l2);
 }
