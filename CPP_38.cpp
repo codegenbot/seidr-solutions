63ae87dac63ed59452902fe401b1e233caccd3fd
beep boop <githubbot@vadim.me>
2024-07-24 17:59:11 +0200

    Solution 1 of CPP/38, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/st75v7r7 Prompt: Complete the following code given the task description and function signature.

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -2,14 +2,26 @@
 #include<string>
 using namespace std;
 
-string decode_cyclic(string s){ 
+string encode_cyclic(string s){ 
     int l=s.length();
     int num=(l+2)/3;
-    string output,x;
-    for (int i=0;i<num;i++){
+    string x,output;
+    int i;
+    for (i=0;i*3<l;i++)
+    {
         x=s.substr(i*3,3);
-        if(x.length()==1)output+=x+x[0];
-        else output+=x;
+        if (x.length()==3) x=x.substr(1)+x[0];
+        output=output+x;
     }
     return output;
+}
+
+string decode_cyclic(string s){ 
+    int l=s.length();
+    string output;
+    for(int i=0;i<l;i+=2){
+        if(i>0)s.insert(i,1,s[i-1]);
+        if(i+l-i>3)s.erase(l-i);
+    }
+    return s;
 }
