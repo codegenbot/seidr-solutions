4ff7e769da4d4b13d2f0e245c9a143c2dd240fca
beep boop <githubbot@vadim.me>
2024-07-24 17:37:09 +0200

    Solution 98 of Python/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nspix4rs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7282d17a-dca7-48ca-bc75-ac6c178c6 bd3.py", line 12, in <module>     check(add_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7282d17a-dca7-48ca-bc75-ac6c178c6 bd3.py", line 8, in check     add_elements([1], 1) == 1 AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_122.py
+++ Python_122.py
@@ -1,3 +1,5 @@
-def add_elements(arr: list[int], k: int) -> int:
-    sorted_nums = sorted([num for num in arr if isinstance(num, int) and 10 <= num <= 100], reverse=True)
+from typing import Union
+
+def add_elements(arr: list[Union[int, float]], k: int) -> int:
+    sorted_nums = sorted([num for num in arr if 10 <= num <= 100], reverse=True)
     return sum(sorted_nums[:k])
