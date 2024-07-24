589d64051c7ef9184513e7d9ad31aaadb42a877d
beep boop <githubbot@vadim.me>
2024-07-24 10:13:13 +0200

    Solution 30 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/8amqp7ul Prompt: b88a1d16-98eb-43ad-8f6d-6fab2d6c5115.cpp: In function ‘double vector_distance(in t)’: b88a1d16-98eb-43ad-8f6d-6fab2d6c5115.cpp:14:16: error: ‘pow’ was not declared in  this scope    14 |         sum += pow(x2[i] - x1[i], 2);       |                ^~~ b88a1d16-98eb-43ad-8f6d-6fab2d6c5115.cpp:17:12: error: ‘sqrt’ was not declared i n this scope    17 |     return sqrt(sum);       |            ^~~~ b88a1d16-98eb-43ad-8f6d-6fab2d6c5115.cpp: In function ‘int main()’: b88a1d16-98eb-43ad-8f6d-6fab2d6c5115.cpp:23:22: error: ‘setprecision’ was not de clared in this scope    23 |     cout << fixed << setprecision(10) << vector_distance(n) << endl;       |                      ^~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -2,22 +2,26 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-
 using namespace std;
 
-double vectorDistance(int n) {
-    double sum = 0;
+double vector_distance(int n) {
+    float x1[n], x2[n];
+    
     for (int i = 0; i < n; i++) {
-        double x1, x2;
-        cin >> x1 >> x2;
-        sum += pow(x2 - x1, 2);
+        cin >> x1[i] >> x2[i];
     }
+    
+    double sum = 0.0;
+    for (int i = 0; i < n; i++) {
+        sum += pow(x2[i] - x1[i], 2);
+    }
+    
     return sqrt(sum);
 }
 
 int main() {
     int n;
     cin >> n;
-    cout << fixed << setprecision(10) << vectorDistance(n) << endl;
+    cout << fixed << setprecision(10) << vector_distance(n) << endl;
     return 0;
 }
