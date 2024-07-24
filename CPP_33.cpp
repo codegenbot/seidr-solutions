fe72f205dd4113d54cf397e402e5a574c62b3349
beep boop <githubbot@vadim.me>
2024-07-25 00:50:22 +0200

    Solution 5 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/y2ckq095 Prompt:       |             ^~~~~~ 5eb1c080-807a-4b42-88a2-535b99d8de85.cpp:19:20: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 5eb1c080-807a-4b42-88a2-535b99d8de85.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5eb1c080-807a-4b42-88a2-535b99d8de85.cpp:19:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 5eb1c080-807a-4b42-88a2-535b99d8de85.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 5eb1c080-807a-4b42-88a2-535b99d8de85.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5eb1c080-807a-4b42-88a2-535b99d8de85.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,27 +1,16 @@
 #include <vector>
-#include <iostream>
 #include <algorithm>
-#include <cassert>
 
-using namespace std;
+std::vector<int> sort_third(std::vector<int> l);
 
-vector<int> sort_third(vector<int> l);
-
-bool issame(vector<int> a, vector<int> b);
-
-vector<int> sort_third(vector<int> l) {
-    vector<int> res = l;
-    for (int i = 0; i < l.size(); i += 3) {
-        sort(res.begin() + i, res.begin() + i + 3);
-    }
-    return res;
-}
-
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-int main() {
-    assert((issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), vector<int>({2, 6, 3, 4, 8, 9, 5, 1})) == true);
-    return 0;
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> res = l;
+    for (int i = 0; i < l.size(); i += 3) {
+        std::sort(res.begin() + i, res.begin() + i + 3);
+    }
+    return res;
 }
