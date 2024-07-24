a1cf635d01374ed377409cd09a000030cd237c23
beep boop <githubbot@vadim.me>
2024-07-24 09:45:49 +0200

    Solution 17 of Python/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/456cmhkh Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b5f460e-e5a4-4e3a-87ba-7857ad172 b3f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_75.py
+++ Python_75.py
@@ -9,11 +9,9 @@ def is_multiply_prime(a):
         return True
 
     factors = []
-    while a > 1:
-        for i in range(2, int(a**0.5) + 1):
-            if a % i == 0:
-                factors.extend([i, a//i])
-                a //= i
-                break
+    for i in range(2, a+1):
+        while a % i == 0:
+            factors.append(i)
+            a //= i
     prime_factors = [f for f in set(factors) if is_prime(f)]
     return len(prime_factors) >= 3
