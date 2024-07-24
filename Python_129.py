f01b1c1a78ba1b508c17247f5914118003e4dfe5
beep boop <githubbot@vadim.me>
2024-07-24 16:26:42 +0200

    Solution 1 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4y3woj1a Prompt: Complete the following code given the task description and function signature.

--- Python_129.py
+++ Python_129.py
@@ -1,24 +1,25 @@
 def minPath(grid, k):
     N = len(grid)
+    visited = [[False] * N for _ in range(N)]
 
-    def dfs(i, j, visited):
-        if len(visited) == k:
-            return visited
-        visited.add((i, j))
+    def dfs(i, j, path):
+        if len(path) == k:
+            return path
+
+        visited[i][j] = True
         neighbors = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
-        neighbors = [
-            (x, y)
-            for x, y in neighbors
-            if 0 <= x < N and 0 <= y < N and (x, y) not in visited
-        ]
         neighbors.sort(key=lambda x: (grid[x[0]][x[1]], x))
-        for x, y in neighbors:
-            res = dfs(x, y, visited.copy())
-            if res:
-                return [grid[i][j]] + res
+
+        for ni, nj in neighbors:
+            if 0 <= ni < N and 0 <= nj < N and not visited[ni][nj]:
+                res = dfs(ni, nj, path + [grid[ni][nj]])
+                if res:
+                    return res
+
+        visited[i][j] = False
 
     for i in range(N):
         for j in range(N):
-            res = dfs(i, j, set())
+            res = dfs(i, j, [grid[i][j]])
             if res:
                 return res
