d8e7feee25128e46f08b7f1fa253817bbae53ea4
beep boop <githubbot@vadim.me>
2024-07-24 16:24:33 +0200

    Solution 47 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fdajg309 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b3056af-7e7b-4262-b7b9-358c2ee46 261.py", line 12, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b3056af-7e7b-4262-b7b9-358c2ee46 261.py", line 9, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3b3056af-7e7b-4262-b7b9-358c2ee46 261.py", line 3, in match_parens     if len(re.findall("\(", s)) != len(re.findall("\)", s)):            ^^ NameError: name 're' is not defined

--- Python_119.py
+++ Python_119.py
@@ -1,7 +1,8 @@
-```
 import re
+
+
 def match_parens(lst):
     for s in lst:
         if len(re.findall("\(", s)) != len(re.findall("\)", s)):
-            return "No"
-    return "Yes"
+            return False
+    return True
