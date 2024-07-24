b386b08bf7e062272b53a6f69e6dabcf709381a6
beep boop <githubbot@vadim.me>
2024-07-24 10:23:18 +0200

    Solution 21 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vxes4a9z Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64807d1b-5a87-4b7f-a36c-54a9d97c7 253.py", line 11, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64807d1b-5a87-4b7f-a36c-54a9d97c7 253.py", line 8, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/64807d1b-5a87-4b7f-a36c-54a9d97c7 253.py", line 3, in max_fill     water_per_cycle = min(capacity, math.gcd(*[sum(row) for row in grid]))                                     ^^^^ NameError: name 'math' is not defined

--- Python_115.py
+++ Python_115.py
@@ -1,7 +1,8 @@
 ```
 import math
+from math import gcd
+
 def max_fill(grid, capacity):
     total_water = sum(sum(row) for row in grid)
-    from math import gcd
     water_per_cycle = min(capacity, gcd(*[sum(row) for row in grid]))
     return -(-total_water // water_per_cycle)
