fb6fb838f3e9cfa814cb4b4d71b8837551e59c08
beep boop <githubbot@vadim.me>
2024-07-24 19:44:37 +0200

    Solution 20 of CPP/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vqbrraj4 Prompt: Program failed

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -2,8 +2,7 @@ int digitSum(string s) {
     int sum = 0;
     for (char c : s) {
         if (isupper(c)) {
-            sum += tolower(c) - 'a' + 1; 
+            sum += c - 'A' + 1;
         }
     }
     return sum;
-}
