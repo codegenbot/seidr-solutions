b832011e42d11525bacee4d8f7b475c0f99ca59b
beep boop <githubbot@vadim.me>
2024-07-24 14:43:18 +0200

    Solution 77 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/u59jyrm1 Prompt: Program did not receive expected input

--- Python_27.py
+++ Python_27.py
@@ -2,12 +2,10 @@ def check():
     string = input("Enter a string containing both upper and lower case letters: ")
     return modify_string(string)
 
-
 def modify_string(string):
     return "".join(
         [char.lower() if char.isupper() else char.upper() for char in string.strip()]
     )
-
 
 output = check()
 print(output)
