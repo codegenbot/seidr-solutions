45c362e93290becb07e5865925b3892be536c21b
beep boop <githubbot@vadim.me>
2024-07-24 15:18:31 +0200

    Solution 12 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cmheh2hi Prompt:       |                                            ^ bec2ffaa-5faa-4e82-880d-a25c2ef1dbd3.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ bec2ffaa-5faa-4e82-880d-a25c2ef1dbd3.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~ In file included from bec2ffaa-5faa-4e82-880d-a25c2ef1dbd3.cpp:41: bec2ffaa-5faa-4e82-880d-a25c2ef1dbd3.cpp: In function ‘int main()’: bec2ffaa-5faa-4e82-880d-a25c2ef1dbd3.cpp:51:35: error: invalid initialization of  reference of type ‘const std::vector<std::vector<std::__cxx11::basic_string<cha r> > >&’ from expression of type ‘<brace-enclosed initializer list>’    51 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ bec2ffaa-5faa-4e82-880d-a25c2ef1dbd3.cpp:17:87: note: in passing argument 1 of ‘ std::vector<std::__cxx11::basic_string<char> > sorted_list_sum(const std::vector <std::vector<std::__cxx11::basic_string<char> > >&)’    17 | ng> sorted_list_sum(const std::vector<std::vector<std::string>>& lst) {       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,4 +1,4 @@
-```
+```cpp
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -34,9 +34,6 @@ std::vector<std::string> sorted_list_sum(const std::vector<std::vector<std::stri
     return temp;
 }
 
-int main() {
-    std::vector<std::string> lst = {{"aaaa"}, {"bbbb"}, {"dd"}, {"cc"}};
-    std::vector<std::string> expected = {{"cc"}, {"dd"}, {"aaaa"}, {"bbbb"}};
-    assert(issame(sorted_list_sum(lst), expected));
-    return 0;
-}
+std::vector<std::vector<std::string>> input = {{ "aaaa" }, { "bbbb" }, { "dd" }, { "cc" }};
+std::vector<std::string> expected = sorted_list_sum(input);
+assert(issame(expected, {{"cc"}, {"dd"}, {"aaaa"}, {"bbbb"}}));
