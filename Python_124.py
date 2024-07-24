55463d0570af1beba5eb519438acacc8dd0af5b1
beep boop <githubbot@vadim.me>
2024-07-24 17:47:33 +0200

    Solution 75 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jfnmht7o Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8164a63a-e087-4683-8acb-4f1d2e4be 4b7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -1,29 +1,36 @@
 ```
 def valid_date(date):
-    if len(date) != 10 or not date.replace("-", "").isdigit():
-        return False
-    year, month_day = date.split("-")
-    year = int(year)
-    if not (1900 <= year <= 2100): 
-        return False
-
-    month_day = int(month_day)
-    if not (1 <= month_day <= 366): 
-        return False
-
-    if month_day > 365: 
-        months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
-        days_in_months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        for month in range(1, 13):
-            if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-                days_in_months[2] = 29
-            elif month == 2:
-                break
-            if month in months_with_31_days:
-                if int(month_day) > days_in_months[month]:
-                    return False
-
-    if int(int(month_day) / 100) != int(year/100): 
-        if len(str(int(month_day/100)))>3: 
+    while True:
+        if len(date) != 10 or not date.replace("-", "").isdigit():
             return False
-    return True
+        year, month_day = date.split("-")
+        year = int(year)
+        if not (1900 <= year <= 2100): 
+            return False
+
+        month_day = int(month_day)
+        if not (1 <= month_day <= 366): 
+            return False
+
+        if month_day > 365: 
+            months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
+            days_in_months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            for month in range(1, 13):
+                if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+                    days_in_months[2] = 29
+                elif month == 2:
+                    break
+                if month in months_with_31_days:
+                    if int(month_day) > days_in_months[month]:
+                        return False
+
+        if month_day >= 100: 
+            if len(str(month_day))>3: 
+                return False
+            year, month = divmod(month_day, 100)
+            if (year < 1 or year > 21): 
+                return False
+            if not (1 <= month <= 12): 
+                return False
+
+        return True
