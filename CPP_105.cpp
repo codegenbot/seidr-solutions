488eacf8aa40c6ce67126f95286a3d083ac47bf8
beep boop <githubbot@vadim.me>
2024-07-24 13:34:18 +0200

    Solution 2 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/dv11s4o6 Prompt:       |                              ^~~~~~ 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp:50: 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp: In function ‘int main()’: 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 3be017d6-8696-41dc-b34a-bb7b6d8aa4d7.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,16 +1,27 @@
 #include <vector>
 #include <string>
-#include <algorithm>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string>b) {
-    return a == b;
+bool issame(vector<string> a,vector<string>b){
+    if(a.size()!=b.size())return false;
+    for(int i=0;i<a.size();i++){
+        if(a[i]!=b[i])return false;
+    }
+    return true;
 }
 
 vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+    vector<int> nums;
     for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            nums.push_back(num);
+        }
+    }
+
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+
+    vector<string> result;
+    for (int num : nums) {
         string str = "";
         switch (num) {
             case 1:
@@ -48,6 +59,6 @@ vector<string> by_length(vector<int> arr) {
 }
 
 int main() {
-    assert(by_length({9, 4, 8}) == vector<string>({"Nine", "Four", "Eight"}));
-    // ...
+    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
+    return 0;
 }
