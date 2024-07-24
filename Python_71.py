f39c4ec140575ab3c1b63bc3c5ed72ce8432268d
beep boop <githubbot@vadim.me>
2024-07-24 12:07:22 +0200

    Solution 3 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qhgsa44k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d6a9830a-2030-4dc2-bccc-5b7041e1c 96a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_71.py
+++ Python_71.py
@@ -1,5 +1,10 @@
-def sum_of_squares(n):
-    result = 0
-    for i in range(1, n + 1):
-        result += i**2
-    return result
+def area():
+    a = float(input("Enter side A of triangle: "))
+    b = float(input("Enter side B of triangle: "))
+    c = float(input("Enter side C of triangle: "))
+
+    if math.isclose(a + b, c) and math.isclose(a + c, b) and math.isclose(b + c, a):
+        s = (a + b + c) / 2
+        return round((math.sqrt(s * (s - a) * (s - b) * (s - c))), 2)
+    else:
+        return -1
