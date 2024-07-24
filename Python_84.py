205d197880fe730f455711192247aaadb87fdf9c
beep boop <githubbot@vadim.me>
2024-07-24 03:19:37 +0200

    Solution 12 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d2991b3-6970-4e67-90ba-81e4abdc3 db1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,9 +1,6 @@
-```
-def solve(K=None):
-    if K is None:
-        K = int(input("Enter a number: "))
+def solve(N=None):
     if N is None:
-        N = int(input("Enter another number: "))
+        N = int(input("Enter a number: "))
     if N < 1:
         return str(0)
-    return str(K * ((N - 1) // K))
+    return bin(N)[2:]
