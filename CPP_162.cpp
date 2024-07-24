d5274d2cd537ea8e54b2d1c4277ba2db45c64781
beep boop <githubbot@vadim.me>
2024-07-24 16:54:30 +0200

    Solution 31 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt: 7ee2b03f-3584-4c75-8bdc-da32927e43f0.cpp:6:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     6 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 7ee2b03f-3584-4c75-8bdc-da32927e43f0.cpp:27: 7ee2b03f-3584-4c75-8bdc-da32927e43f0.cpp: In function ‘int main()’: 7ee2b03f-3584-4c75-8bdc-da32927e43f0.cpp:29:13: error: ‘string_to_md5’ was not d eclared in this scope; did you mean ‘string_to_hex’?    29 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,31 +1,24 @@
 #include <string>
-#include <sstream>
-#include <iomanip>
-#include <openssl/err.h>
-#include <openssl/evp.h>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    OpenSSL_add_all_algorithms();
     unsigned char md[16];
     stringstream ss;
-    
-    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
-    EVP_MD *md_algorithm = NULL;
-    int rc = EVP_DigestInit_ex(ctx, EVP_md5(), 0);
-    if (rc == 1) {
-        EVP_DigestUpdate(ctx, text.c_str(), text.size());
-        unsigned char *digest = NULL;
-        rc = EVP_DigestFinal_ex(ctx, md, &digest);
-        if (rc == 1) {
-            for (int i = 0; i < 16; ++i) {
-                ss << setfill('0') << setw(2) << hex << (int)md[i];
-            }
+
+    EVP_MD_CTX* md_ctx = EVP_MD_CTX_new();
+    EVP_MD *md_algorithm = EVP_md5();
+    EVP_DigestInit(md_ctx, md_algorithm);
+    EVP_DigestUpdate(md_ctx, text.c_str(), text.size());
+    unsigned char *digest = EVP_DigestFinal(md_ctx, NULL, NULL);
+    if (digest != NULL) {
+        for (int i = 0; i < 16; ++i) {
+            ss << setfill('0') << setw(2) << hex << (int)digest[i];
         }
     }
-    EVP_MD_CTX_free(ctx);
+    OPENSSL_free(digest);
+    EVP_MD_CTX_free(md_ctx);
     return ss.str();
 }
