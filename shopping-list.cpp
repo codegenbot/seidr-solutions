c8255c53002c90611e3c94d1766fa20e99710709
beep boop <githubbot@vadim.me>
2024-07-24 04:12:09 +0200

    Solution 31 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0zyf2bau Prompt:       |     ^~~~ 05386485-01fb-45e5-9848-e57afe3e3d23.cpp:25:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 05386485-01fb-45e5-9848-e57afe3e3d23.cpp:25:13: error: ‘fixed’ was not declared in this scope    25 |     cout << fixed << setprecision(1) << shoppingList(&prices[0], &discou nts[0], n) << endl;       |             ^~~~~ 05386485-01fb-45e5-9848-e57afe3e3d23.cpp:25:22: error: ‘setprecision’ was not de clared in this scope    25 |     cout << fixed << setprecision(1) << shoppingList(&prices[0], &discou nts[0], n) << endl;       |                      ^~~~~~~~~~~~ 05386485-01fb-45e5-9848-e57afe3e3d23.cpp:25:87: error: ‘endl’ was not declared i n this scope    25 |  setprecision(1) << shoppingList(&prices[0], &discounts[0], n) << endl;       |                                                                   ^~~~ 05386485-01fb-45e5-9848-e57afe3e3d23.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- shopping-list.cpp
+++ shopping-list.cpp
@@ -1,7 +1,4 @@
 #include <vector>
-#include <iostream>
-#include <iomanip>
-
 using namespace std;
 
 double shoppingList(double prices[], double discounts[], int n) {
@@ -13,6 +10,9 @@ double shoppingList(double prices[], double discounts[], int n) {
     }
     return total;
 }
+
+#include <iostream>
+#include <iomanip>
 
 int main() {
     int n;
@@ -27,4 +27,3 @@ int main() {
     }
     cout << fixed << setprecision(1) << shoppingList(&prices[0], &discounts[0], n) << endl;
     return 0;
-}
