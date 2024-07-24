579286b43ef75e868579b5939d591876acffc01d
beep boop <githubbot@vadim.me>
2024-07-24 17:29:18 +0200

    Solution 90 of Python/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nspix4rs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4a65295-e3fb-478f-8a81-dc17fea5a db2.py", line 12, in <module>     check(add_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4a65295-e3fb-478f-8a81-dc17fea5a db2.py", line 8, in check     add_elements([1], 1) == 1 AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_122.py
+++ Python_122.py
@@ -1,3 +1,3 @@
-def add_elements(arr: list, k: int) -> int:
-    sorted_nums = sorted([num for num in arr if 10 <= num < 100], reverse=True)
-    return sum(sorted_nums) if len(sorted_nums) <= k else sum(sorted_nums[:k])
+def add_elements(arr: list[int], k: int) -> int:
+    sorted_nums = sorted([num for num in arr if 10 <= num <= 100], reverse=True)
+    return sum(sorted_nums[:k])
