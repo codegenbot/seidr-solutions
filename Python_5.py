81334ad1bbb4410bba3b4d0af2e615467ba363f8
beep boop <githubbot@vadim.me>
2024-07-24 11:29:40 +0200

    Solution 76 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/a8daa1c5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/84f42334-74e6-4158-a7eb-4744a765c d77.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,10 +1,11 @@
+```
 from typing import List
 
 def intersperse(numbers: List[int], delimeter: int) -> List[int]:
     result = [numbers[0]]
     for i in range(1, len(numbers)):
         result.extend([delimeter])
-        result.extend([str(numbers[i])])
+        result.extend([numbers[i]])
     return result
 
 if __name__ == "__main__":
@@ -16,7 +17,7 @@ if __name__ == "__main__":
             input_numbers = list(map(int, input_numbers_str.split(',')))
             delimeter = int(delimeter_str)
             
-            print(', '.join(str(x) for x in intersperse(input_numbers, delimeter)))
+            print(intersperse(input_numbers, delimeter))
             break
         except ValueError:
             print("Invalid number(s) entered. Please enter valid numbers.")
