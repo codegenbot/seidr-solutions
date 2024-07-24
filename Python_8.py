355ddbfde7c8bef8deac63412a5ecc9743a6ca3d
beep boop <githubbot@vadim.me>
2024-07-24 12:43:23 +0200

    Solution 80 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vv9tfrbf Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c649a720-43a3-41b7-9e93-1db8ad00e b5d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,17 +1,16 @@
-from typing import List, Tuple
+from typing import List
 
-def sum_product(numbers: List[int]) -> Tuple[int, int]:
+def sum_product(numbers: List[int]) -> int:
     if not isinstance(numbers, list):
         raise ValueError("Input must be a list")
-    for num in numbers:
-        if not isinstance(num, int):
-            raise ValueError("List elements must be integers")
-
+    
     total_sum = 0
     product = 1
 
     for num in numbers:
+        if not isinstance(num, int):
+            raise ValueError("List elements must be integers")
         total_sum += num
         product *= num
 
-    return total_sum, product
+    return total_sum
