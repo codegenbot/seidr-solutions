4212d5274176376993511810eed9e54c4465522f
beep boop <githubbot@vadim.me>
2024-07-24 20:13:54 +0200

    Solution 12 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vokrmzzs Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0197c0f1-6f88-4f74-9bf5-ba50ca82a 349.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,6 +1,2 @@
-def solution(lst):
-    odd_sum = sum(i for i in lst[1::2] if i % 2 != 0)
-    even_product = 1
-    for i in lst[::2]:
-        even_product *= i
-    return odd_sum, even_product
+def sum_odd_indices(lst):
+    return sum(lst[i] for i in range(1, len(lst), 2))
