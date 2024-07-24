6fad78307c215eeee6e0ea1256b47d17fd6b0fb6
beep boop <githubbot@vadim.me>
2024-07-24 02:08:50 +0200

    Solution 51 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/z7qa9oa3 Prompt: Program did not receive expected input

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,50 +1,59 @@
-```cpp
 #include <iostream>
 #include <string>
 using namespace std;
 
-bool solveBoolean(string s) {
-    bool result = true;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'T')
-            return true;
-        else if (s[i] == 'F')
-            return false;
+bool evalOp(string s) {
+    int j = 0;
+    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+        j++;
 
-        int j = i + 1;
-        while (j <= s.length() && (s[j] == '|' || s[j] == '&'))
-            j++;
-
-        string opStr = s.substr(i, j - i);
-        i = j - 1;
-
-        if (opStr[0] == '|') {
-            for (; i >= 0; i--) {
-                if (s[i] == 'T')
-                    return true;
-                else if (s[i] == 'F')
-                    return false;
-            }
-            result = true;
-        } else {
-            for (; i >= 0; i--) {
-                if (s[i] == 'T' && opStr[1] != '|')
-                    return true;
-                else if (s[i] == 'F' && opStr[1] != '&')
-                    return false;
-            }
-            result = false;
+    string opStr = s.substr(0, j);
+    bool left = false;
+    if (opStr[0] == '|') {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T')
+                left = true;
+            else if (s[i] == 'F')
+                return false;
+        }
+    } else {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T' && opStr[1] != '|')
+                left = true;
+            else if (s[i] == 'F' && opStr[1] != '&')
+                return false;
         }
     }
 
-    return result;
+    if (!left)
+        return false;
+
+    return evalBoolean(s.substr(j));
+}
+
+bool evalBoolean(string s) {
+    if (s.empty())
+        return false;
+
+    int j = 0;
+    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+        j++;
+
+    string opStr = s.substr(0, j);
+    bool result = evalOp(s);
+
+    // recursive call for the remaining part
+    return !result; 
 }
 
 int main() {
     string s;
     cout << "Enter a Boolean expression: ";
     cin >> s;
-    bool result = solveBoolean(s);
-    cout << "Result: " << (result ? "True" : "False") << endl;
+    bool result = evalBoolean(s);
+    if (result)
+        cout << "True";
+    else
+        cout << "False";
     return 0;
 }
