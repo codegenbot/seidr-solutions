269510e72368efbb9af5748fde739f5b055a95ec
beep boop <githubbot@vadim.me>
2024-07-24 18:52:12 +0200

    Solution 72 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/u5k8ib4u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87c9ef0b-cef5-44f0-8d4c-478e3b877 279.py", line 13, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/87c9ef0b-cef5-44f0-8d4c-478e3b877 279.py", line 10, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,4 +1,2 @@
-from collections import OrderedDict
-
 def remove_duplicates(numbers: list[int]) -> list[int]:
-    return list(OrderedDict.fromkeys(numbers))
+    return list(dict.fromkeys(numbers))
