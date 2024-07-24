c6f59e05e461d6dcdeddc8cf455540dc03368db1
beep boop <githubbot@vadim.me>
2024-07-24 03:39:43 +0200

    Solution 31 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/xbs3avqp Prompt:    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0a8ec777-5ae8-43cf-813c-d3b443902038.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0a8ec777-5ae8-43cf-813c-d3b443902038.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0a8ec777-5ae8-43cf-813c-d3b443902038.cpp:16: 0a8ec777-5ae8-43cf-813c-d3b443902038.cpp: In function ‘int main()’: 0a8ec777-5ae8-43cf-813c-d3b443902038.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 0a8ec777-5ae8-43cf-813c-d3b443902038.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,28 +1,23 @@
-#include <vector>
+```cpp
 #include <algorithm>
+#include <vector>
 #include <string>
 
 bool issame(const std::string& a, const std::string& b) {
-    int countA = 0, countB = 0;
-    for (char c : a) if (c == '1') countA++;
-    for (char c : b) if (c == '1') countB++;
-
-    return countA == countB;
+    return (a.size() % 2 == 0 && b.size() % 2 != 0) || 
+           (a.size() % 2 != 0 && b.size() % 2 == 0);
 }
 
 std::vector<std::string> sorted_list_sum(std::vector<std::string> lst) {
-    auto it = std::unique(lst.begin(), lst.end(),
-                          [](const std::string& a, const std::string& b) {
-                              return !issame(a, b);
-                          });
+    auto it = std::unique(lst.begin(), lst.end(), issame);
     lst.erase(it, lst.end());
-
+    
     std::sort(lst.begin(), lst.end(),
               [](const std::string& a, const std::string& b) {
                   if (a.size() < b.size()) return true;
                   else if (a.size() > b.size()) return false;
                   else return a < b;
               });
-
+    
     return lst;
 }
