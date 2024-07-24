13ddf85c0a28aebcb88007cb54a580dd987510d4
beep boop <githubbot@vadim.me>
2024-07-24 05:45:09 +0200

    Solution 42 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8fb6d51-b1bc-4fe5-b3c4-b9fede570 aa6.py", line 26, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f8fb6d51-b1bc-4fe5-b3c4-b9fede570 aa6.py", line 22, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -2,17 +2,23 @@ def fix_spaces(text):
     new_text = ""
     add_hyphen = False
     for char in text:
-        if char.isalnum() or char in ["-", "_"]:
-            new_text += char
-            add_hyphen = False
-        elif char.isspace():
+        if char.isspace():
             if not add_hyphen:
                 new_text += "-"
                 add_hyphen = True
             else:
                 new_text += " "
-        else:
-            if add_hyphen:
-                new_text += "-"
                 add_hyphen = False
+        elif char.isalnum() or char in ["-", "_"]:
+            if not add_hyphen and char.isspace():
+                new_text += "-"
+                add_hyphen = True
+            else:
+                new_text += char
+                add_hyphen = False
+        else:
             new_text += char
+    if not text.strip().endswith("-"):
+        new_text += "-"
+
+    return new_text
