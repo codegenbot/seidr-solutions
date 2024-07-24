4dcc853de8dbb73e02a6a0028a796857ba11778d
beep boop <githubbot@vadim.me>
2024-07-24 05:49:53 +0200

    Solution 32 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/62y31j2y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e9e8e7a-ae81-46f6-951e-046b59876 825.py", line 9, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e9e8e7a-ae81-46f6-951e-046b59876 825.py", line 6, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -1,2 +1,7 @@
 def cycpattern_check(a, b):
-    return str(b) * (len(str(a)) // len(str(b))) + str(b)[:len(str(a)) % len(str(b))] == str(a)
+    str_a = str(a)
+    str_b = str(b)
+    if len(str_a) % len(str_b) != 0:
+        return False
+    check_str = str_b * (len(str_a) // len(str_b)) + str_b[:len(str_a) % len(str_b)]
+    return check_str == str_a
