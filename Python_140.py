810ecce30bee0048842501497ad6ffb86f9d3db7
beep boop <githubbot@vadim.me>
2024-07-24 21:43:26 +0200

    Solution 84 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mpmfr9pr Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcf1151e-eea9-47c4-b01e-20f1f56fb 912.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -5,7 +5,3 @@ def fix_spaces(text):
         text = text.replace("---", "-")
         text = text.replace("----", "-")
     return text
-
-def check(input_text):
-    assert fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple"
-    check(fix_spaces("   Exa 1 2 2 mple"))
