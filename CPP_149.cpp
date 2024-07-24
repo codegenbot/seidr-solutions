47bb51c613c06903362f677f50472c39ad327d72
beep boop <githubbot@vadim.me>
2024-07-24 12:45:15 +0200

    Solution 31 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7b22b251-41d2-4b84-b618-631f5a3dbb27.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7b22b251-41d2-4b84-b618-631f5a3dbb27.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7b22b251-41d2-4b84-b618-631f5a3dbb27.cpp:11: 7b22b251-41d2-4b84-b618-631f5a3dbb27.cpp: In function ‘int main()’: 7b22b251-41d2-4b84-b618-631f5a3dbb27.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 7b22b251-41d2-4b84-b618-631f5a3dbb27.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -13,3 +13,8 @@ vector<string> sorted_list_sum(const vector<string>& lst) {
     });
     return result;
 }
+
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
+}
