db5dcc536c549f153754c88d86e23e3c4719a9d4
beep boop <githubbot@vadim.me>
2024-07-24 10:48:50 +0200

    Solution 54 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ueww3qod Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/076a7411-29e3-4bc7-9e5b-7b3969f5b 1c7.py", line 42, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/076a7411-29e3-4bc7-9e5b-7b3969f5b 1c7.py", line 37, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,4 +1,41 @@
+import re
+
+
 def sort_numbers(numbers: str) -> str:
-    num_dict = { "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10, "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14, "fifteen": 15, "sixteen": 16, "seventeen": 17, "eighteen": 18, "nineteen": 19, "twenty": 20, "thirty": 30, "forty": 40, "fifty": 50, "sixty": 60, "seventy": 70, "eighty": 80, "ninety": 90, "ninety-nine": 99}
-    sorted_numbers = sorted([num_dict[num] for num in numbers.split() if num.replace(" ", "") in num_dict])
-    return ' '.join(map(str, sorted_numbers))
+    num_dict = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+        "ten": 10,
+        "eleven": 11,
+        "twelve": 12,
+        "thirteen": 13,
+        "fourteen": 14,
+        "fifteen": 15,
+        "sixteen": 16,
+        "seventeen": 17,
+        "eighteen": 18,
+        "nineteen": 19,
+        "twenty": 20,
+        "thirty": 30,
+        "forty": 40,
+        "fifty": 50,
+        "sixty": 60,
+        "seventy": 70,
+        "eighty": 80,
+        "ninety": 90,
+        "ninety-nine": 99,
+    }
+    numbers_list = [
+        num_dict[num]
+        for num in re.split(" |-", numbers)
+        if num.replace(" ", "") in num_dict
+    ]
+    return " ".join(map(str, sorted(numbers_list)))
