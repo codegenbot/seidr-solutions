a81deff31854f935f765605ddbc271928714e2ea
beep boop <githubbot@vadim.me>
2024-07-24 02:19:32 +0200

    Solution 3 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/yf6fnfpd Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,35 +1,39 @@
 vector<string> split_words(string txt) {
     vector<string> result;
+    size_t pos = 0, prev_pos = 0;
 
-    size_t pos = 0;
-    while ((pos = txt.find(' ')) != string::npos) {
-        result.push_back(txt.substr(0, pos));
-        txt.erase(0, pos + 1);
-    }
-
-    if (txt.empty()) {
-        return result;
-    }
-
-    pos = txt.find(',');
-    if (pos == string::npos) {
-        int oddCount = 0;
-        for (char c : txt) {
-            if (c >= 'a' && c <= 'z') {
-                if ((ord(c) - ord('a')) % 2 != 0) {
-                    oddCount++;
+    while ((pos = txt.find_first_of(" ,")) != string::npos) {
+        if (prev_pos == pos) {
+            break;
+        }
+        if (txt.substr(prev_pos, pos - prev_pos).find_first_of(' ') == string::npos) {
+            int count = 0;
+            for (char c : txt.substr(prev_pos, pos - prev_pos)) {
+                if (c >= 'a' && c <= 'z') {
+                    count++;
                 }
             }
+            result.push_back(to_string(count));
+            break;
         }
-        result.push_back(to_string(oddCount));
-    } else {
-        while ((pos = txt.find(',')) != string::npos) {
-            result.push_back(txt.substr(0, pos));
-            txt.erase(0, pos + 1);
+        string word = txt.substr(prev_pos, pos - prev_pos);
+        result.push_back(word);
+        prev_pos = pos + 1;
+    }
+
+    if (prev_pos < txt.size()) {
+        size_t start = prev_pos;
+        while (start < txt.size() && !isalnum(txt[start])) {
+            start++;
         }
-        if (!txt.empty()) {
-            result.push_back(txt);
+        string word = txt.substr(start);
+        int count = 0;
+        for (char c : word) {
+            if (c >= 'a' && c <= 'z') {
+                count++;
+            }
         }
+        result.push_back(to_string(count));
     }
 
     return result;
