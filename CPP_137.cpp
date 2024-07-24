f7e279efbe6755a9d6192cdb88ca339026bbdf37
beep boop <githubbot@vadim.me>
2024-07-24 03:32:17 +0200

    Solution 62 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt:    38 | int main(){       |     ^~~~ 91c5340a-e3ce-45b9-b0ad-292861827c2c.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~ In file included from 91c5340a-e3ce-45b9-b0ad-292861827c2c.cpp:37: 91c5340a-e3ce-45b9-b0ad-292861827c2c.cpp: In function ‘int main()’: 91c5340a-e3ce-45b9-b0ad-292861827c2c.cpp:39:13: error: ‘boost’ has not been decl ared    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 91c5340a-e3ce-45b9-b0ad-292861827c2c.cpp:39:35: error: expected primary-expressi on before ‘>’ token    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 91c5340a-e3ce-45b9-b0ad-292861827c2c.cpp:39:37: error: ‘compare_one’ was not dec lared in this scope    39 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,37 +1,41 @@
+```cpp
 #include <iostream>
 #include<string>
 #include<algorithm>
 #include<stdexcept>
-#include< boost/any>
+#include<boost/any>
 
 using namespace std;
 
+using namespace boost;
+
 any compare_one(any a, any b) {
-    if (boost::any_cast<double>(a) > stod(boost::any_cast<string>(b)))
-        return a;
+    if (is_stateless_wrapper<double>(a)) {
+        double da = any_cast<double>(a);
+        double db = any_cast<double>(b);
+        if (da > db)
+            return a;
+        else
+            return b;
+    }
+    else if (is_stateless_wrapper<string>(a)) {
+        string da = any_cast<string>(a);
+        string db = any_cast<string>(b);
+
+        if (da > db)
+            return a;
+        else if (db > da)
+            return b;
+        throw invalid_argument("Tie");
+    }
     else
-        return b;
-
-    if (stod(boost::any_cast<string>(a)) > boost::any_cast<double>(b))
-        return a;
-    else
-        return b;
-
-    string da = boost::any_cast<string>(a);
-    string db = boost::any_cast<string>(b);
-
-    if (da > db)
-        return a;
-    else if (db > da)
-        return b;
-    throw invalid_argument("Tie");
-
+        throw invalid_argument("Invalid argument type");
 }
 
 int main() {
     cout << boost::any_cast<double>(compare_one(1, 2.5)) << endl;
-    cout << boost::any_cast<double>(compare_one(1, "2,3")) << endl;
-    cout << boost::any_cast<string>(compare_one("5,1", "6")) << endl;
-    cout << boost::any_cast<double>(compare_one("1", 1)) << endl;
+    cout << boost::any_cast<double>(compare_one(1, "2.3")) << endl;
+    cout << boost::any_cast<string>(compare_one("5", "6")) << endl;
+    cout << boost::any_cast<double>(compare_one("1", 1.0)) << endl;
     return 0;
 }
