018667f431a8a5860cc770fc5b6c2e83d4e197eb
beep boop <githubbot@vadim.me>
2024-07-24 10:50:15 +0200

    Solution 63 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1hf2owjk Prompt:    31 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:31:39: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:30: 77c002fb-0745-4df8-8cda-c590ececacdb.cpp: In function ‘int main()’: 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:40:20: error: ‘minPath’ was not declare d in this scope    40 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 77c002fb-0745-4df8-8cda-c590ececacdb.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
@@ -7,17 +6,15 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> minPath(std::vector<std::vector<int>>& grid, int k) {
+std::vector<int> minPath(std::vector<std::vector<int>>& grid, int k){
     int n = grid.size();
     vector<int> res;
-    
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
             if(res.empty() || res.back() != grid[i][j]) {
                 res.push_back(grid[i][j]);
                 k--;
                 if(k == 0) return res;
-                
                 int dir[8] = {1, -1, n, -n, n+1, -n-1, n+1, -n-1};
                 for(int d = 0; d < 8; ++d) {
                     int ni = i + dir[d]/n, nj = j + dir[d]%n;
@@ -35,6 +32,5 @@ std::vector<int> minPath(std::vector<std::vector<int>>& grid, int k) {
             }
         }
     }
-    
     return res;
 }
