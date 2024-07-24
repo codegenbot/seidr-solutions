7c8502e69fb4ccecf360b922b005549888696688
beep boop <githubbot@vadim.me>
2024-07-24 11:29:14 +0200

    Solution 13 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vhuqvhyo Prompt: cdb76d18-e9a9-472f-8483-ebf32fc30ecd.cpp:28:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    28 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ cdb76d18-e9a9-472f-8483-ebf32fc30ecd.cpp:8:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     8 | bool issame(std::vector<int> a, std::vector<int> b){       |      ^~~~~~ cdb76d18-e9a9-472f-8483-ebf32fc30ecd.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ cdb76d18-e9a9-472f-8483-ebf32fc30ecd.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main(){       |     ^~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,11 +1,6 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 #include <climits>
-
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
 
 std::vector<int> rolling_max(std::vector<int> numbers){
     std::vector<int> result;
@@ -15,9 +10,4 @@ std::vector<int> rolling_max(std::vector<int> numbers){
         result.push_back(max_num);
     }
     return result;
-}
-
-int main(){
-    assert(issame(rolling_max({3, 2, 3, 100, 3}), std::vector<int>({3, 3, 3, 100, 100})));
-    return 0;
 }
