363309d97e4c97145d0a74a5911b887e89076bbe
beep boop <githubbot@vadim.me>
2024-07-24 09:20:02 +0200

    Solution 2 of CPP/136, ratio 0.6363636363636364 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3ykwtl6w Prompt: Complete the following code given the task description and function signature.

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,12 +1,29 @@
-#include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int maxNeg = 0, minPos = INT_MAX;
-    for (int num : lst) {
-        if (num < 0 && num > maxNeg) maxNeg = num;
-        if (num > 0 && num < minPos) minPos = num;
+    vector<int> result;
+    
+    if(!lst.empty()) {
+        auto max_negative = *max_element(lst.begin(), lst.end(), [](int a, int b){return a > 0;});
+        
+        if(max_negative < 0) {
+            result.push_back(max_negative);
+        } else {
+            result.push_back(0);
+        }
+        
+        auto min_positive = *min_element(lst.begin(), lst.end(), [](int a, int b){return a < 0;});
+        
+        if(min_positive > 0) {
+            result.push_back(min_positive);
+        } else {
+            result.push_back(0);
+        }
+    } else {
+        result.push_back(0);
+        result.push_back(0);
     }
-    return {(maxNeg == 0 ? 0 : maxNeg), (minPos == INT_MAX ? 0 : minPos)};
+    
+    return result;
 }
