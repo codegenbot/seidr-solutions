fb17dea56ff08b51853db1b06af76c2f592ffd50
beep boop <githubbot@vadim.me>
2024-07-24 09:30:05 +0200

    Solution 0 of Python/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jlxodr5y Prompt: Complete the following code given the task description and function signature.

---
+++ Python_74.py
@@ -0,0 +1,9 @@
+def total_match(lst1, lst2):
+    return min(
+        [
+            lst
+            for lst in [lst1, lst2]
+            if sum(len(s) for s in lst) < sum(len(s) for s in lst2)
+        ],
+        default=lst1,
+    )
