7ad2be19c68c826d32a534d247dbb712d956ce50
beep boop <githubbot@vadim.me>
2024-07-24 16:01:54 +0200

    Solution 7 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from c52e21bd-b88d-4a17-9b15-929983d27269.cpp:4: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ c52e21bd-b88d-4a17-9b15-929983d27269.cpp:17:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    17 |     MD5_Final(md, &ctx);       |     ~~~~~~~~~^~~~~~~~~~ In file included from c52e21bd-b88d-4a17-9b15-929983d27269.cpp:4: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccts3vD7.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': c52e21bd-b88d-4a17-9b15-929983d27269.cpp:(.text+0x9d): undefined reference to `M D5_Init' /usr/bin/ld: c52e21bd-b88d-4a17-9b15-929983d27269.cpp:(.text+0xd6): undefined re ference to `MD5_Update' /usr/bin/ld: c52e21bd-b88d-4a17-9b15-929983d27269.cpp:(.text+0xec): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -11,10 +11,12 @@ string string_to_md5(string text) {
     unsigned char md[16];
     stringstream ss;
 
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-    MD5_Update(&ctx, text.c_str(), text.size());
-    MD5_Final(md, &ctx);
+    MD5_CTX* ctx = new MD5_CTX;
+    MD5_Init(ctx);
+    MD5_Update(ctx, text.c_str(), text.size());
+    MD5_Final(md, ctx);
+
+    delete ctx;
 
     for (int i = 0; i < 16; ++i) {
         ss << setfill('0') << setw(2) << hex << (int)md[i];
