1f669a3a179874f17f6a50360c61a7e541b334bc
beep boop <githubbot@vadim.me>
2024-07-25 01:48:25 +0200

    Solution 22 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/aw3o62pc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/89893568-901f-4674-8268-0fab3cb69 3dd.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,29 +1,28 @@
-```
-from typing import List
+    from typing import List
 
 
-def separate_paren_groups(paren_string: str) -> List[str]:
-    result = []
-    stack = []
-    temp = ""
+    def separate_paren_groups(paren_string: str) -> List[str]:
+        result = []
+        stack = []
+        temp = ""
 
-    for char in paren_string:
-        if char == " ":
-            continue
-        if char == "(":
-            if temp:  
-                result.append(temp)
-                temp = ""
-            stack.append(char)
-            temp += char
-        elif char == ")":
-            stack.pop()
-            temp += char
-            if not stack:
-                result.append(temp)
-                temp = ""
+        for char in paren_string:
+            if char == " ":
+                continue
+            if char == "(":
+                if temp:  
+                    result.append(temp)
+                    temp = ""
+                stack.append(char)
+                temp += char
+            elif char == ")":
+                stack.pop()
+                temp += char
+                if not stack:
+                    result.append(temp)
+                    temp = ""
 
-    if not stack:
-        result.append(temp)
+        if not stack:
+            result.append(temp)
 
-    return result
+        return result
