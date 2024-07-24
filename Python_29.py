706e350aa5ab7233622c22e7f47d94025ade8974
beep boop <githubbot@vadim.me>
2024-07-24 11:45:12 +0200

    Solution 67 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/tf2z1fy4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6372096-0d77-45ad-b427-bbe6244ed 0e3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,4 +1,4 @@
-```
+````
 from typing import List
 
 def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
@@ -10,3 +10,4 @@ prefix = input("Enter the prefix to filter by: ")
 strings_list = [string.strip() for string in strings.split(",")]
 
 print(filter_by_prefix(strings_list, prefix))
+```
