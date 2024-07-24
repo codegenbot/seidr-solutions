c6896e47abdcc4eab1117c72040d7f5a8cfcaf6f
beep boop <githubbot@vadim.me>
2024-07-24 10:41:49 +0200

    Solution 4 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/t0b75pbn Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40aacc43-5894-4e16-b648-afbe360e4 330.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/40aacc43-5894-4e16-b648-afbe360e4 330.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,11 @@
 def sort_even(l: list):
-    return [
-        x if i % 2 != 0 else y
-        for i, (x, y) in enumerate(sorted((x, y) for x, y in zip(l[1::2], l[::2])))
-    ]
+    evens = sorted(x for i, x in enumerate(l[::2]) if i % 2 == 0)
+    result = []
+    even_index = 0
+    for i, x in enumerate(l):
+        if i % 2 != 0:
+            result.append(x)
+        else:
+            result.append(next(y for _y in l[1::2] if _y == evens[even_index]))
+            even_index += 1
+    return result
