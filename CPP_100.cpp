5dd82f0676331a98f18856b8ac8f963279275c32
beep boop <githubbot@vadim.me>
2024-07-24 15:27:42 +0200

    Solution 69 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pzxlydrl Prompt:       |             ^~~~~~ 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp:35: 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp: In function ‘int main()’: 5acf1af0-d22c-481c-ae01-460a450ffd84.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -13,7 +13,19 @@ std::vector<std::pair<int, int>> make_a_pile(int n) {
     return pile;
 }
 
-bool samePiles(std::vector<std::pair<int, int>> p1, std::vector<std::pair<int, int>> p2) {
+bool isSame(std::vector<std::pair<int, int>> p1, std::vector<std::pair<int, int>> p2) {
+    if (p1.size() != p2.size()) {
+        return false;
+    }
+    for (int i = 0; i < p1.size(); i++) {
+        if (p1[i] != p2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool checkPiles(std::vector<std::pair<int, int>> p1, std::vector<std::pair<int, int>> p2) {
     if (p1.size() != p2.size()) {
         return false;
     }
@@ -28,6 +40,5 @@ bool samePiles(std::vector<std::pair<int, int>> p1, std::vector<std::pair<int, i
 int main() {
     int n = 8;
     std::vector<std::pair<int, int>> pile = make_a_pile(n);
-    assert(samePiles(pile, {{1, 2}, {4, 5}, {8, 10}, {12, 14}, {16, 18}, {20, 22}}));
+    assert(checkPiles(pile, {{1,2},{4,5},{8,10},{12,14},{16,18},{20,22}}));
     return 0;
-}
