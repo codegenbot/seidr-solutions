0d75adccd8be04687259563d7751416a4ec070e4
beep boop <githubbot@vadim.me>
2024-07-24 03:49:06 +0200

    Solution 40 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/gr7gq9fd Prompt: f432e364-30fd-4fe3-8eab-c883b11e3d2a.cpp: In function ‘int main()’: f432e364-30fd-4fe3-8eab-c883b11e3d2a.cpp:7:5: error: ‘assert’ was not declared i n this scope     7 |     assert(specialFilter({}) == 0);       |     ^~~~~~ f432e364-30fd-4fe3-8eab-c883b11e3d2a.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <iostream>   +++ |+#include <cassert>     3 | f432e364-30fd-4fe3-8eab-c883b11e3d2a.cpp: At global scope: f432e364-30fd-4fe3-8eab-c883b11e3d2a.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ f432e364-30fd-4fe3-8eab-c883b11e3d2a.cpp:6:5: note: ‘int main()’ previously defi ned here     6 | int main() {       |     ^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,12 +1,10 @@
 #include <cassert>
-#include <vector>
 #include <iostream>
-
-int specialFilter(std::vector<int> nums);
 
 int main() {
     assert(specialFilter({}) == 0);
-};
+    return 0;
+}
 
 int specialFilter(std::vector<int> nums){
     int count = 0;
@@ -16,4 +14,4 @@ int specialFilter(std::vector<int> nums){
         }
     }
     return count;
-};
+}
