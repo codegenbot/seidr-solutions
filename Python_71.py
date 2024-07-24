2b37a0496abdb8d334e68743e26e21e85a6bbb3f
beep boop <githubbot@vadim.me>
2024-07-24 12:38:59 +0200

    Solution 14 of Python/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qhgsa44k Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/272a75b3-6bd8-48d1-880c-51e348b7c 765.py", line 17, in <module>     check(triangle_area)           ^^^^^^^^^^^^^ NameError: name 'triangle_area' is not defined

--- Python_71.py
+++ Python_71.py
@@ -1,2 +1,17 @@
-def sum_of_squares(n):
-    return n*(n+1)*(2*n+1)//3
+import math
+
+def triangle_area():
+    a = float(input("Enter side A of triangle: "))
+    b = float(input("Enter side B of triangle: "))
+    c = float(input("Enter side C of triangle: "))
+
+    if math.isclose(a + b, c) and math.isclose(a + c, b) and math.isclose(b + c, a):
+        s = (a + b + c) / 2
+        return round((math.sqrt(s * (s - a) * (s - b) * (s - c))), 2)
+    else:
+        return -1
+
+def check():
+    print(triangle_area())
+
+check()
