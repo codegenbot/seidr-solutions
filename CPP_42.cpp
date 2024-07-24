f5f13f14734ea7e7d62a6b7906237abfa0f80aee
beep boop <githubbot@vadim.me>
2024-07-24 10:14:19 +0200

    Solution 91 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/l5wh96cl Prompt:       |     ^~~~ 615cb519-dd93-4b69-960f-5740019166b6.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 615cb519-dd93-4b69-960f-5740019166b6.cpp:32: 615cb519-dd93-4b69-960f-5740019166b6.cpp: In function ‘int main()’: 615cb519-dd93-4b69-960f-5740019166b6.cpp:42:20: error: ‘incr_list’ was not decla red in this scope; did you mean ‘my_namespace::incr_list’?    42 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 615cb519-dd93-4b69-960f-5740019166b6.cpp:5:22: note: ‘my_namespace::incr_list’ d eclared here     5 |     std::vector<int> incr_list(std::vector<int> l) {       |                      ^~~~~~~~~ In file included from 615cb519-dd93-4b69-960f-5740019166b6.cpp:32: 615cb519-dd93-4b69-960f-5740019166b6.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,26 +1,4 @@
-#include <vector>
-
-std::vector<int> incr_list(std::vector<int> l);
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b);
-
-std::vector<int> incr_list(std::vector<int> l) {
-    for (int i = 0; i < l.size(); i++) {
-        l[i]++;
-    }
-    return l;
-}
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
+int main() {
+    using namespace my_namespace;
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
 }
