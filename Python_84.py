e5819986c6918b966e239e6dd317c2fd576423b2
beep boop <githubbot@vadim.me>
2024-07-24 03:49:56 +0200

    Solution 28 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcef9d8b-f9ae-4b73-8c2b-82b5f17b9 6be.py", line 18, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcef9d8b-f9ae-4b73-8c2b-82b5f17b9 6be.py", line 15, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,13 +1,8 @@
 def solve(K):
     if K < 1:
-        return '0'
-    if math.log2(K) % 1 == 0: 
-        return str(bin(int(math.pow(2, math.ceil(math.log2(K))))-1)[2:])
+        return str(K)
     binary = ''
     while K > 0:
         binary = str(K % 2) + binary
         K //= 2
-    if int(binary, 2) != K: 
-        return str(int(binary, 2)) 
-    else:
-        return binary
+    return binary
