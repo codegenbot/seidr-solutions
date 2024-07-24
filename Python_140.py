c19923650b24fd12afd73bd61f388e1f98ef41ba
beep boop <githubbot@vadim.me>
2024-07-24 04:59:29 +0200

    Solution 12 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72c0458e-87b8-4a75-bc6c-5878a0bf8 3aa.py", line 25, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/72c0458e-87b8-4a75-bc6c-5878a0bf8 3aa.py", line 21, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,15 +1,14 @@
-```
 def fix_spaces(text):
     new_text = ""
-    for i, char in enumerate(text):
+    for char in text:
         if char == " ":
             if len(new_text) > 0 and new_text[-1] != " ":
                 if not ((new_text[-1].isalnum() or new_text[-1] in ["-", "_"]) 
                         or (len(new_text) == 1 and new_text[0].isalnum()) ):
-                    if i < len(text)-1:
+                    if new_text:
                         new_text += "-"
                     else:
-                        new_text += " "
+                        new_text = "-"
                 else:
                     new_text += " "
         else:
