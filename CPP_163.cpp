b6f611f88d8faf8257fbb068a0701bd827a7ca8c
beep boop <githubbot@vadim.me>
2024-07-24 14:48:59 +0200

    Solution 60 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/twdz8p39 Prompt:       |                           ^~~~~~ ef30bbde-36ca-4820-b941-18d28972301b.cpp:24:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ef30bbde-36ca-4820-b941-18d28972301b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ef30bbde-36ca-4820-b941-18d28972301b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ef30bbde-36ca-4820-b941-18d28972301b.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ef30bbde-36ca-4820-b941-18d28972301b.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ ef30bbde-36ca-4820-b941-18d28972301b.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
@@ -16,7 +15,7 @@ std::vector<int> generate_integers(int a, int b) {
     return result;
 }
 
-int main() {
-    assert(issame(generate_integers(17, 89), {}));
+int main(){
+    assert(issame(generate_integers(17, 89), std::vector<int>{}));
     return 0;
 }
