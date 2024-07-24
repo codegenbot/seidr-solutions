38d2ffe28ccd2e67d595706a8d1da8547990e321
beep boop <githubbot@vadim.me>
2024-07-24 05:14:41 +0200

    Solution 4 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vixjqopt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1c0f6d52-0514-4cbc-867d-68fe28fff a97.py", line 12, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1c0f6d52-0514-4cbc-867d-68fe28fff a97.py", line 8, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,4 @@
-def double_the_difference(lst):
-    odd_sum = sum(i for i in lst if isinstance(i, int) and i % 2 != 0)
-    return abs(odd_sum - len(lst)) * (len(lst) + odd_sum)
-
-input_list = [1, 2, 3, 4, 5]
-print(double_the_difference(input_list))
+def check(double_the_difference):
+    result = double_the_difference([1,2,3,4,5])
+    assert result == [10], f"Expected {result} to equal [10]"
+    print("Test passed!")
