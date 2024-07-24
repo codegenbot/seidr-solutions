2c4ed081283b108b0db0f8a7389c64729737c9b0
beep boop <githubbot@vadim.me>
2024-07-24 17:59:02 +0200

    Solution 84 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ts9ewlh2 Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1fde9d69-61cc-4598-b6ff-98a6f7058406.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1fde9d69-61cc-4598-b6ff-98a6f7058406.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1fde9d69-61cc-4598-b6ff-98a6f7058406.cpp:11: 1fde9d69-61cc-4598-b6ff-98a6f7058406.cpp: In function ‘int main()’: 1fde9d69-61cc-4598-b6ff-98a6f7058406.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 1fde9d69-61cc-4598-b6ff-98a6f7058406.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,5 +1,3 @@
-```cpp
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -7,6 +5,16 @@ bool containSameStrings(vector<string> a, vector<string> b) {
     return a.size() == b.size() && std::all_of(a.begin(), a.end(), [&](const string& s) { 
         return find(b.begin(), b.end(), s) != b.end(); 
     });
+}
+
+int main() {
+    vector<string> strings = {"grunt", "trumpet", "prune", "gruesome"};
+    string substring = "run";
+    vector<string> result = filter_by_substring(strings, substring);
+    
+    assert(containSameStrings(result, {"grunt", "prune"}));
+    
+    return 0;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
@@ -17,21 +25,4 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
         }
     }
     return result;
-}
-
-int main() {
-    vector<string> input = {"grunt", "trumpet", "prune", "gruesome"};
-    string substring = "run";
-    
-    vector<string> output = filter_by_substring(input, substring);
-
-    bool sameStrings = containSameStrings(output, {"grunt", "prune"});
-
-    if(sameStrings) {
-        std::cout << "The vectors contain the same strings." << std::endl;
-    } else {
-        std::cout << "The vectors do not contain the same strings." << std::endl;
-    }
-
-    return 0;
 }
