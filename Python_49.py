22b0adce2d6ec22e1108e9a291d4ca1accc86bba
beep boop <githubbot@vadim.me>
2024-07-24 23:47:02 +0200

    Solution 76 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/hqexafdi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b088044-171e-40ec-9c31-2a0bb774b b43.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7b088044-171e-40ec-9c31-2a0bb774b b43.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,16 +1,15 @@
-```
 def modp(n: int, p: int):
     def extended_gcd(a, b):
         if a == 0:
-            return (b, 0)
+            return b, 0, 1
         else:
-            g, y = extended_gcd(b % a, a)
-            return (g, b // a - y * (b // a))
+            gcd, y, x = extended_gcd(b % a, a)
+            return gcd, x - (b // a) * y, y
 
-    g, x = extended_gcd(p, n)
+    g, x, y = extended_gcd(p, n)
     if p == 1:
-        return n
+        return 1
     elif g != 1:
         return None
     else:
-        return pow(n, p-2, p) * n % p
+        return x % p
