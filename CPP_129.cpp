b8fb24f891dbe695c77ca9cb7496e2dd976166a7
beep boop <githubbot@vadim.me>
2024-07-24 23:56:58 +0200

    Solution 95 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2knu02au Prompt:    34 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b3c977a7-01c1-4b32-b8a8-0223c51640ac.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b3c977a7-01c1-4b32-b8a8-0223c51640ac.cpp:34:39: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b3c977a7-01c1-4b32-b8a8-0223c51640ac.cpp:33: b3c977a7-01c1-4b32-b8a8-0223c51640ac.cpp: In function ‘int main()’: b3c977a7-01c1-4b32-b8a8-0223c51640ac.cpp:43:20: error: ‘minPath’ was not declare d in this scope    43 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ b3c977a7-01c1-4b32-b8a8-0223c51640ac.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,7 +1,39 @@
-bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+vector<int> minPath(vector<vector<int>> grid, int k) {
+    int n = grid.size();
+    vector<vector<bool>> visited(n, vector<bool>(n, false));
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>>
+            pq({{grid[0][0], {0, 0}}});
+    vector<int> path;
+    
+    bool issame(vector<int> a, vector<int> b) {
+        if(a.size() != b.size()) return false;
+        for(int i = 0; i < a.size(); i++) {
+            if(a[i] != b[i]) return false;
+        }
+        return true;
     }
-    return true;
+    
+    while (!pq.empty()) {
+        auto [val, pos] = pq.top(); pq.pop();
+        if (k-- == 0) break;
+        
+        for (auto &dir : directions) {
+            int x = pos.first + dir.first;
+            int y = pos.second + dir.second;
+            
+            if (x >= 0 && x < n && y >= 0 && y < n && !visited[x][y]) {
+                visited[x][y] = true;
+                pq.push({grid[x][y], {x, y}});
+            }
+        }
+    }
+    
+    while (!pq.empty()) {
+        path.push_back(pq.top().first);
+        pq.pop();
+    }
+    
+    reverse(path.begin(), path.end());
+    return path;
 }
