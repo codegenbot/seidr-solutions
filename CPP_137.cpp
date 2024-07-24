a862ccb3ed8767459f35a783daae43188998265d
beep boop <githubbot@vadim.me>
2024-07-24 10:56:55 +0200

    Solution 76 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/m3e7eqps Prompt: less_type) {       | ^~ 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:22:3: error: expected unqualified-id be fore ‘else’    22 | } else {       |   ^~~~ In file included from 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:31: 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp: In function ‘int main()’: 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:33:13: error: ‘boost’ has not been decl ared    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:33:29: error: ‘string’ was not declared  in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 27374b2e-3ed7-46c1-acdb-22ea436eb819.cpp:33:37: error: ‘compare_one’ was not dec lared in this scope    33 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,6 +1,6 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
-#include <iostream>
 
 bool compare_one(boost::any a, boost::any b) {
     if(a.type() == boost::any::typeless_type || b.type() == boost::any::typeless_type) {
@@ -14,7 +14,7 @@ bool compare_one(boost::any a, boost::any b) {
             else if(a_int < b_int)
                 return false;
             else
-                return true; // Changed this line to compare the integers only.
+                return a_int == 0;
         } else {
             std::string a_s = boost::any_cast<std::string>(a);
             double b_d = boost::any_cast<double>(b);
@@ -30,6 +30,6 @@ bool compare_one(boost::any a, boost::any b) {
         else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
             return false;
         else
-            return false; // Changed this line to compare the integers only.
+            return true; // Changed this line to compare the integers only.
     }
 }
