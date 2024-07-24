bb3445da27ad0d1fe9e1c7826c74850a49eaeb54
beep boop <githubbot@vadim.me>
2024-07-24 18:41:29 +0200

    Solution 73 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jniv13q0 Prompt: 18bed9dc-0c93-4cce-b28c-b3fa7f61c29d.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 18bed9dc-0c93-4cce-b28c-b3fa7f61c29d.cpp:14: 18bed9dc-0c93-4cce-b28c-b3fa7f61c29d.cpp: In function ‘int main()’: 18bed9dc-0c93-4cce-b28c-b3fa7f61c29d.cpp:16:13: error: ‘filp_case’ was not decla red in this scope    16 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -4,10 +4,10 @@ string flip_case(string str){
     string result = "";
     for(int i=0; i<str.length(); i++){
         if(str[i] >= 'a' && str[i] <= 'z'){
-            result += toupper(str[i]);
+            result += toupper((char)str[i]);
         }
         else{
-            result += tolower(str[i]);
+            result += tolower((char)str[i]);
         }
     }
     return result;
