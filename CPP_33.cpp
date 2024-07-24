7652cebe9002677fa69168c5130b62a6cc08956d
beep boop <githubbot@vadim.me>
2024-07-24 17:39:22 +0200

    Solution 60 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ry5ssmq6 Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1861ab7e-711e-4aaa-8c46-a74fdb97ffe3.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1861ab7e-711e-4aaa-8c46-a74fdb97ffe3.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1861ab7e-711e-4aaa-8c46-a74fdb97ffe3.cpp:16: 1861ab7e-711e-4aaa-8c46-a74fdb97ffe3.cpp: In function ‘int main()’: 1861ab7e-711e-4aaa-8c46-a74fdb97ffe3.cpp:26:20: error: ‘sort_third’ was not decl ared in this scope    26 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 1861ab7e-711e-4aaa-8c46-a74fdb97ffe3.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,24 +1,24 @@
-bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a.size() != b.size()) {
+bool issame(const vector<int>& a, const vector<int>& b){
+    if(a.size() != b.size()){
         return false;
     }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
             return false;
         }
     }
     return true;
 }
 
-void sort_third(vector<int>& l) {
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 3 == 0) {
+void sort_third(vector<int>& l){
+    for(int i = 0; i < l.size(); i++){
+        if(i % 3 == 0){
             vector<int> temp;
-            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+            for(int j = i; j < l.size() && j % 3 == 0; j++){
                 temp.push_back(l[j]);
             }
             sort(temp.begin(), temp.end());
-            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+            for(int j = i; j < l.size() && j % 3 == 0; j++){
                 l[j] = temp[j - i];
             }
         }
