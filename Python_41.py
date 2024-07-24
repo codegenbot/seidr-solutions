de70d35aa386266a5911d3cafd86b93b03f672e2
beep boop <githubbot@vadim.me>
2024-07-24 19:26:53 +0200

    Solution 76 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cwccil6q Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/53b6267a-1cf2-4e7c-a381-3f148a8e4 0c0.py", line 1     ```Python     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,9 +1,14 @@
-```
+```'python
 def car_race_collision(n: int):
     if n < 2:
         return False
-    current_car = 0
-    for i in range(1, n + 1):
-        if (current_car + i) % 2 == 0:
-            return i
-        current_car = (current_car + 1) % 2
+    first_car = 0
+    second_car = 1
+    while True:
+        if first_car == second_car:
+            return True
+        if first_car + 1 >= n or second_car + 1 >= n:
+            return False
+        first_car += 1
+        second_car += 1
+'```
