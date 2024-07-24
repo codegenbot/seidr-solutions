486ae211fe4e622d4dd89dbeeb7035c970f83cd7
beep boop <githubbot@vadim.me>
2024-07-24 07:09:26 +0200

    Solution 95 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/53lmbp7o Prompt: c6abf7ed-ce83-49a0-9617-1dcaa7f48150.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst) {       |                   ^~~~~~ c6abf7ed-ce83-49a0-9617-1dcaa7f48150.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst) {       |                          ^~~ In file included from c6abf7ed-ce83-49a0-9617-1dcaa7f48150.cpp:13: c6abf7ed-ce83-49a0-9617-1dcaa7f48150.cpp: In function ‘int main()’: c6abf7ed-ce83-49a0-9617-1dcaa7f48150.cpp:15:26: error: ‘next_smallest’ cannot be  used as a function    15 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,8 +1,9 @@
 #include <vector>
+
 int next_smallest(std::vector<int> lst) {
     if (lst.size() < 2) return -1; 
     std::vector<int> v(lst);
-    sort(v.begin(), v.end());
+    std::sort(v.begin(), v.end());
     for (int i = 0; i < v.size(); i++) {
         if (v[i] != v[0]) {
             return v[i];
