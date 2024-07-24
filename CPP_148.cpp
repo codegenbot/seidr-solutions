df285cea2b341aa78b3c89625f661b3142eb7a2d
beep boop <githubbot@vadim.me>
2024-07-24 12:15:24 +0200

    Solution 8 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mcc4lf58 Prompt:       |                              ^~~~~~ ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp:26: ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp: In function ‘int main()’: ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp:36:20: error: ‘bf’ was not declared in this scope    36 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ ea4fe159-434c-4ee8-a8cb-bb1e9830adb6.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,32 +1,16 @@
-#include <vector>
 #include <string>
-#include <cassert>
-
-bool issame(vector<string> a, vector<string> b);
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
 
 vector<string> bf(string planet1, string planet2);
-
-bool issame(vector<string> a, vector<string> b) {
-   if (a.size() != b.size()) return false;
-   
-   for (int i = 0; i < a.size(); ++i) {
-       if (a[i] != b[i]) return false;
-   }
-   
-   return true;
-}
-
-int main() {
-    assert(issame(bf("Jupiter", "Makemake"), {}));
-    return 0;
-}
 
 vector<string> bf(string planet1, string planet2){
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     vector<string> result;
 
     int start = -1, end = -1;
-    for(int i=0; i<planets.size(); i++){
+    for(int i = 0; i < planets.size(); i++){
         if(planets[i] == planet1) start = i;
         if(planets[i] == planet2) end = i;
     }
@@ -34,11 +18,11 @@ vector<string> bf(string planet1, string planet2){
     if(start == -1 || end == -1) return {};
 
     if(start < end){
-        for(int i=start+1; i<end; i++){
+        for(int i = start + 1; i < end; i++){
             result.push_back(planets[i]);
         }
     } else {
-        for(int i=start-1; i>end; i--){
+        for(int i = start - 1; i > end; i--){
             result.push_back(planets[i]);
         }
     }
