71f084ae8e076fee8ecdf3cced364567a4f9f000
beep boop <githubbot@vadim.me>
2024-07-24 04:21:50 +0200

    Solution 51 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c8b61187-181a-4f26-ba6f-ac91a6fe6 14d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_84.py
+++ Python_84.py
@@ -1,14 +1,2 @@
 def solve(K):
-    if K < 2:
-        return "1"
-    if K == 2:
-        return "10"
-    result = ""
-    while K > 0:
-        remainder = K % 2
-        if remainder == 1:
-            result = "1" + result
-        else:
-            result = "0" + result
-        K = math.floor(K / 2)
-    return result
+    return bin(K)[2:]
