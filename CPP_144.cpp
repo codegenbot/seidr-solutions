cd504090c1dcc8622bce38735567713a0106e8e9
beep boop <githubbot@vadim.me>
2024-07-24 14:50:34 +0200

    Solution 2 of CPP/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lz26xidy Prompt: f98ba643-b07a-4803-99d7-c35e7e578a5b.cpp: In function ‘bool simplify(std::string , std::string)’: f98ba643-b07a-4803-99d7-c35e7e578a5b.cpp:7:18: error: ‘strtok’ was not declared in this scope; did you mean ‘strtoq’?     7 |     int a = stoi(strtok((x.substr(1)).c_str(), "/"));       |                  ^~~~~~       |                  strtoq

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,13 +1,16 @@
-#include <string>
-#include <vector>
+#include <sstream>
 
 using namespace std;
 
 bool simplify(string x, string n) {
-    int a = stoi(strtok((x.substr(1)).c_str(), "/"));
-    int b = stoi(strtok(NULL, "/"));
-    int c = stoi(strtok((n.substr(1)).c_str(), "/"));
-    int d = stoi(strtok(NULL, "/"));
+    int a = 0, b = 0, c = 0, d = 0;
+    std::istringstream iss(x.substr(1), std::ios_base::in);
+    iss >> a;
+    iss >> b;
+
+    std::istringstream iss2(n.substr(1), std::ios_base::in);
+    iss2 >> c;
+    iss2 >> d;
 
     return (a * c) == (b * d);
 }
