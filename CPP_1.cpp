3bf7a8811603c4f1bc044685518ff312eb9274b2
beep boop <githubbot@vadim.me>
2024-07-24 14:46:35 +0200

    Solution 52 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/26zn1fkk Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 35b0d63a-ecf7-419a-89c5-6d9c5c13545c.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 35b0d63a-ecf7-419a-89c5-6d9c5c13545c.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 35b0d63a-ecf7-419a-89c5-6d9c5c13545c.cpp:25: 35b0d63a-ecf7-419a-89c5-6d9c5c13545c.cpp: In function ‘int main()’: 35b0d63a-ecf7-419a-89c5-6d9c5c13545c.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 35b0d63a-ecf7-419a-89c5-6d9c5c13545c.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -28,3 +28,22 @@ vector<string> separate_paren_groups(string paren_string) {
     
     return groups;
 }
+
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    // Your main function code here
+    return 0;
+}
