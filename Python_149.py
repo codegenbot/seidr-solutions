cd4bd0a3d1260ca612bf62d1aa31600adb61cbe5
beep boop <githubbot@vadim.me>
2024-07-24 16:40:31 +0200

    Solution 74 of Python/149, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/988d35f4-3a20-4e08-b902-7fa688711 54c.py", line 14, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/988d35f4-3a20-4e08-b902-7fa688711 54c.py", line 11, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,8 +1,10 @@
-```
 def sorted_list_sum(words=None):
     if words is None:
-        lst = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
+        lst = [
+            word.strip().lower()
+            for word in input("Enter words separated by spaces: ").split()
+        ]
     else:
         lst = [word.strip().lower() for word in words]
-    
-    return sum(len(word) for word in lst if len(word) % 2 == 0)
+
+    return [x[1] for x in sorted([(sum(ord(c) for c in word), word) for word in lst])]
