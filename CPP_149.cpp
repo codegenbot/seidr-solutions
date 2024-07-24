db12356a38f906c6024809484bc64a06d4ba3a78
beep boop <githubbot@vadim.me>
2024-07-24 12:50:52 +0200

    Solution 36 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt: 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:28: error: ‘a’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                            ^ 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:30: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:37: error: ‘string’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:6:44: error: ‘b’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:5: 8831b01d-337d-4a1e-a309-816ba04faff7.cpp: In function ‘int main()’: 8831b01d-337d-4a1e-a309-816ba04faff7.cpp:15:20: error: ‘sorted_list_sum’ was not  declared in this scope    15 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,17 +1,11 @@
-bool issame(const vector<string>& a, const vector<string>& b) {
+#include <vector>
+#include <string>
+#include <cassert>
+
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
 
-vector<string> sorted_list_sum(vector<string> lst) {
-    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
-    sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
-        if (a.length() == b.length()) {
-            return a < b;
-        }
-        return a.length() < b.length();
-    });
-
-    vector<string> sorted_list = lst;
-    
-    return sorted_list;
+int main() {
+    assert(issame({"aaaa", "bbbb", "dd", "cc"}, {"cc", "dd", "aaaa", "bbbb"}));
 }
