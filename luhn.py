01f5c759eafc2fe19744c3ecb4b5281b70746993
beep boop <githubbot@vadim.me>
2024-07-24 04:41:53 +0200

    Solution 52 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/wpijjksa Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6d23b98e-0983-4cda-bc7f-c53d47da4 117.py", line 1     ```     ^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -1,10 +1,4 @@
-```
 def luhn(card_number):
-    card_number = [int(x) for x in card_number]
-    result = 0
-    for i, digit in enumerate(card_number[::-1]):
-        if (i % 2 == 0 and digit * 2 >= 10):
-            result += digit * 2 // 10 + digit * 2 % 10
-        else:
-            result += digit
+    card_number = [int(x) for x in str(card_number)]
+    result = sum([i * 2 if i % 2 == 0 else i for i in card_number[1:]] + [card_number[0]])
     return str(result)
