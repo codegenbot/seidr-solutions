2961671bf3c3d25ce678cbfbe3ae5105898aa7ac
beep boop <githubbot@vadim.me>
2024-07-24 09:01:41 +0200

    Solution 5 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/1hf2owjk Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,40 +1,14 @@
-#include <vector>
-using namespace std;
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
+vector<int> minPath(vector<vector<int>> grid, int k){
     int n = grid.size();
-    vector<vector<int>> dp(n, vector<int>(n));
     vector<int> res;
-    
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (i == 0 && j == 0) {
-                dp[i][j] = grid[i][j];
-            } else if (i > 0) {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
-            } else {
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(res.size() < k){
+                res.push_back(grid[i][j]);
+            }else{
+                break;
             }
         }
     }
-    
-    int i = n - 1, j = n - 1;
-    while (k > 0) {
-        res.push_back(grid[i][j]);
-        if (i > 0 && j > 0) {
-            if (dp[i-1][j] < dp[i][j-1]) {
-                i -= 1;
-            } else {
-                j -= 1;
-            }
-        } else if (i > 0) {
-            i -= 1;
-        } else {
-            j -= 1;
-        }
-        k--;
-    }
-    
-    reverse(res.begin(), res.end());
     return res;
 }
