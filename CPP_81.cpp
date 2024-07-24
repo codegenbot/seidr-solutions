bbcd3075d815c0e8e7b9f6bec533a2f2e4778803
beep boop <githubbot@vadim.me>
2024-07-24 20:41:26 +0200

    Solution 58 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2xgesiut Prompt:       |                              ^~~~~~ a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp:32: a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp: In function ‘int main()’: a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ a2b9ff0c-5888-4384-9d69-ac7843a10603.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -19,25 +19,25 @@ vector<string> numerical_letter_grade(vector<float> grades) {
     for (float grade : grades) {
         string letter_grade;
         if (grade >= 4.0)
-            letter_grade = "A+";
+            letter_grade = "A+");
         else if (grade > 3.7)
             letter_grade = "A";
         else if (grade > 3.3)
             letter_grade = "A-";
         else if (grade > 3.0)
-            letter_grade = "B+";
+            letter_grade = "B+");
         else if (grade > 2.7)
             letter_grade = "B";
         else if (grade > 2.3)
             letter_grade = "B-";
         else if (grade > 2.0)
-            letter_grade = "C+";
+            letter_grade = "C+");
         else if (grade > 1.7)
             letter_grade = "C";
         else if (grade > 1.3)
             letter_grade = "C-";
         else if (grade > 1.0)
-            letter_grade = "D+";
+            letter_grade = "D+");
         else if (grade > 0.7)
             letter_grade = "D";
         else
