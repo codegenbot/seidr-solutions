ea53e572ff664784a1b8c26f23ea6ed1783ffa8d
beep boop <githubbot@vadim.me>
2024-07-24 04:15:05 +0200

    Solution 16 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y8l1vktq Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp:13: 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp: In function ‘int main()’: 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 2cc1e5f3-53e6-4103-bfe2-d0642e74177f.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,8 +1,16 @@
-vector<float> get_positive(vector<float> l) {
-    vector<float> result;
-    for (float i : l) {
-        if (i > 0) {
-            result.push_back(i);
+```cpp
+#include <algorithm>
+#include <vector>
+
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a == b;
+}
+
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
         }
     }
     return result;
