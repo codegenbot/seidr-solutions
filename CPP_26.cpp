4eb7a1d45b6ee909733aae493041e5d814fb6737
beep boop <githubbot@vadim.me>
2024-07-24 18:56:32 +0200

    Solution 54 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/to49ik7c Prompt:       |             ^~~~~~ d199dc59-3a1b-4ea3-9667-b905763fd9c6.cpp:20:20: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ d199dc59-3a1b-4ea3-9667-b905763fd9c6.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d199dc59-3a1b-4ea3-9667-b905763fd9c6.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d199dc59-3a1b-4ea3-9667-b905763fd9c6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d199dc59-3a1b-4ea3-9667-b905763fd9c6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d199dc59-3a1b-4ea3-9667-b905763fd9c6.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -2,18 +2,16 @@
 #include <set>
 #include <cassert>
 
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return (a == b);
 }
 
-vector<int> remove_duplicates(vector<int> numbers){
-    set<int> unique_numbers(numbers.begin(), numbers.end());
-    return vector<int>(unique_numbers.begin(), unique_numbers.end());
+std::vector<int> remove_duplicates(std::vector<int> numbers){
+    std::set<int> unique_numbers(numbers.begin(), numbers.end());
+    return std::vector<int>(unique_numbers.begin(), unique_numbers.end());
 }
 
-int main2() {
+int main() { 
     assert((issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5})));
     return 0;
 }
