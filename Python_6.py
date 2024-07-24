d94d635755caef0875ba4b98492e31c2faecd991
beep boop <githubbot@vadim.me>
2024-07-24 10:21:24 +0200

    Solution 42 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/5zvyvtsl Prompt: Program did not receive expected input

--- Python_6.py
+++ Python_6.py
@@ -1,13 +1,13 @@
 from typing import List
 
-
 def parse_nested_parens(paren_string: str) -> List[int]:
     result = []
     stack = []
     for char in paren_string:
-        if char == "(":
+        if char == '(':
             stack.append(char)
-        elif char == ")":
+        elif char == ')':
             result.append(len(stack))
             stack.pop()
+    while stack: result.append(len(stack)); stack.pop()
     return result
