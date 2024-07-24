4fe50084af192aadb068ea82b7245aaf886a5087
beep boop <githubbot@vadim.me>
2024-07-24 05:02:01 +0200

    Solution 10 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/w9musam2 Prompt: 4e28cad4-5412-4842-a684-223b07209f0d.cpp: In function ‘double vectorDistance(int )’: 4e28cad4-5412-4842-a684-223b07209f0d.cpp:17:21: error: ‘pow’ was not declared in  this scope    17 |         distance += pow(vec1[i] - vec2[i], 2);       |                     ^~~ 4e28cad4-5412-4842-a684-223b07209f0d.cpp:19:12: error: ‘sqrt’ was not declared i n this scope    19 |     return sqrt(distance);       |            ^~~~ 4e28cad4-5412-4842-a684-223b07209f0d.cpp: In function ‘int main()’: 4e28cad4-5412-4842-a684-223b07209f0d.cpp:26:22: error: ‘setprecision’ was not de clared in this scope    26 |     cout << fixed << setprecision(10) << vectorDistance(n) << endl;       |                      ^~~~~~~~~~~~

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -1,11 +1,21 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-double calculateEuclideanDistance(vector<float> vector1, vector<float> vector2) {
-    double distance = 0;
-    for (int i = 0; i < vector1.size(); i++) {
-        distance += pow((vector1[i] - vector2[i]), 2);
+double vectorDistance(int n) {
+    float vec1[n];
+    float vec2[n];
+
+    // Read input vectors
+    for (int i = 0; i < n; i++) {
+        cin >> vec1[i] >> vec2[i];
+    }
+
+    double distance = 0.0;
+    for (int i = 0; i < n; i++) {
+        distance += pow(vec1[i] - vec2[i], 2);
     }
     return sqrt(distance);
 }
@@ -13,16 +23,8 @@ double calculateEuclideanDistance(vector<float> vector1, vector<float> vector2) 
 int main() {
     int n;
     cin >> n;
-    vector<float> vec1(n);
-    for (float &val : vec1) {
-        cin >> val;
-    }
-    cin.ignore();
-    cin >> n;
-    vector<float> vec2(n);
-    for (float &val : vec2) {
-        cin >> val;
-    }
-    cout << fixed << setprecision(10) << calculateEuclideanDistance(vec1, vec2) << endl;
+
+    cout << fixed << setprecision(10) << vectorDistance(n) << endl;
+
     return 0;
 }
