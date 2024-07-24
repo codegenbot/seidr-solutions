5725fc71aecd7e8a49b823cd854d7c0603b7645c
beep boop <githubbot@vadim.me>
2024-07-24 02:12:35 +0200

    Solution 16 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cx7mc75r Prompt: 0b369216-3695-431b-a160-bad273484bc4.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | int add_elements(vector<int> arr, int k) {       |                  ^~~~~~ 0b369216-3695-431b-a160-bad273484bc4.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr, int k) {       |                         ^~~ 0b369216-3695-431b-a160-bad273484bc4.cpp:1:35: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr, int k) {       |                                   ^~~ 0b369216-3695-431b-a160-bad273484bc4.cpp:1:40: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int add_elements(vector<int> arr, int k) {       |                                        ^ In file included from 0b369216-3695-431b-a160-bad273484bc4.cpp:11: 0b369216-3695-431b-a160-bad273484bc4.cpp: In function ‘int main()’: 0b369216-3695-431b-a160-bad273484bc4.cpp:13:25: error: ‘add_elements’ cannot be used as a function    13 |     assert (add_elements({1}, 1) == 1);       |             ~~~~~~~~~~~~^~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <vector>
 
-int add_elements(std::vector<int> arr, int k) {
+int add_elements(vector<int> arr, int k) {
     int sum = 0;
     for (int i = 0; i < min(k, (int)arr.size()); i++) {
         if (to_string(arr[i]).size() <= 2) {
@@ -9,9 +8,3 @@ int add_elements(std::vector<int> arr, int k) {
         }
     }
     return sum;
-}
-
-int main() {
-    assert(add_elements({1}, 1) == 1);
-    return 0;
-}
