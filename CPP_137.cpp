c6976e97cb1aa7a23af4b9d690ae6436c72a7fe4
beep boop <githubbot@vadim.me>
2024-07-24 17:29:42 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7vjwb7qj Prompt: f52a0d83-96a2-41e0-ad7d-4d13f90a677d.cpp:1:10: fatal error: boost/variant.hpp: N o such file or directory     1 | #include <boost/variant.hpp>       |          ^~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,7 @@
-#include <variant>
-#include <string>
+```
 #include <boost/variant.hpp>
+#include <string>
 
-using namespace std;
 using namespace boost;
 
 variant<string, int, float> compare_one(variant<any> a, variant<any> b) {
@@ -17,28 +16,4 @@ variant<string, int, float> compare_one(variant<any> a, variant<any> b) {
             } else {
                 return "Equal";
             }
-        } else if (holds_alternative<int>(a) && holds_alternative<int>(b)) {
-            int aInt = get<int>(a);
-            int bInt = get<int>(b);
-            if (aInt > bInt) {
-                return "Greater";
-            } else if (aInt < bInt) {
-                return "Less";
-            } else {
-                return "Equal";
-            }
-        } else if (holds_alternative<float>(a) && holds_alternative<float>(b)) {
-            float aFloat = get<float>(a);
-            float bFloat = get<float>(b);
-            if (aFloat > bFloat) {
-                return "Greater";
-            } else if (aFloat < bFloat) {
-                return "Less";
-            } else {
-                return "Equal";
-            }
-        } else {
-            return "Non";
-        }
-    }, a, b);
-}
+        } else if (holds_alternative<int>(a) && holds_alternation
