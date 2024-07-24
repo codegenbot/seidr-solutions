aea5b54dda90acf421df21d99529c463cac778c6
beep boop <githubbot@vadim.me>
2024-07-24 14:09:59 +0200

    Solution 12 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/m4bicfcs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22804d7f-d7d8-4483-a2aa-336aa643e 84b.py", line 13, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22804d7f-d7d8-4483-a2aa-336aa643e 84b.py", line 10, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22804d7f-d7d8-4483-a2aa-336aa643e 84b.py", line 5, in prime_fib     prime_fib_nums = [num for num in fib if is_prime(num)]                      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/22804d7f-d7d8-4483-a2aa-336aa643e 84b.py", line 5, in <listcomp>     prime_fib_nums = [num for num in fib if is_prime(num)]                                             ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_39.py
+++ Python_39.py
@@ -1,16 +1,17 @@
-def is_prime(num):
+def is_prime(num: int) -> bool:
     if num < 2:
         return False
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
 
-def prime_fib(n: int):
+
+def prime_fib(n: int) -> int:
     fib = [0, 1]
     while len(fib) <= n:
         fib.append(fib[-1] + fib[-2])
     prime_fib_nums = [num for num in fib if is_prime(num)]
-    if n-1 >= len(prime_fib_nums):
+    if n - 1 >= len(prime_fib_nums):
         return None
-    return prime_fib_nums[n-1]
+    return prime_fib_nums[n - 1]
