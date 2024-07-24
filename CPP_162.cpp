3fc4fcafff2e773f42782dcca39a51a891ff13a2
beep boop <githubbot@vadim.me>
2024-07-24 16:29:53 +0200

    Solution 18 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt:       |     ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from a2507c2e-8fad-4b23-8e08-68490fd1b417.cpp:5: /usr/include/openssl/md5.h:50:27: note: declared here    50 | OSSL_DEPRECATEDIN_3_0 int MD5_Update(MD5_CTX *c, const void *data, size_ t len);       |                           ^~~~~~~~~~ a2507c2e-8fad-4b23-8e08-68490fd1b417.cpp:18:14: warning: ‘int MD5_Final(unsigned  char*, MD5_CTX*)’ is deprecated: Since OpenSSL 3.0 [-Wdeprecated-declarations]    18 |     MD5_Final(md, &ctx);       |     ~~~~~~~~~^~~~~~~~~~ In file included from a2507c2e-8fad-4b23-8e08-68490fd1b417.cpp:5: /usr/include/openssl/md5.h:51:27: note: declared here    51 | OSSL_DEPRECATEDIN_3_0 int MD5_Final(unsigned char *md, MD5_CTX *c);       |                           ^~~~~~~~~ /usr/bin/ld: /tmp/ccyhOezj.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> >)': a2507c2e-8fad-4b23-8e08-68490fd1b417.cpp:(.text+0x9d): undefined reference to `M D5_Init' /usr/bin/ld: a2507c2e-8fad-4b23-8e08-68490fd1b417.cpp:(.text+0xd6): undefined re ference to `MD5_Update' /usr/bin/ld: a2507c2e-8fad-4b23-8e08-68490fd1b417.cpp:(.text+0xec): undefined re ference to `MD5_Final' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,8 +1,10 @@
+```cpp
 #include <string>
 #include <sstream>
 #include <iomanip>
 #include <openssl/err.h>
-#include <openssl/evp.h>
+#include <openssl/md5.h>
+#pragma comment(lib, "libcrypto.lib")
 
 using namespace std;
 
@@ -12,14 +14,14 @@ string string_to_md5(string text) {
     unsigned char md[16];
     stringstream ss;
 
-    EVP_MD_CTX ctx;
-    EVP_MD *md_algorithm = EVP_md5();
-    EVP_DigestInit(&ctx, md_algorithm);
-    EVP_DigestUpdate(&ctx, text.c_str(), text.size());
-    unsigned char *digest = EVP_DigestFinal(&ctx, NULL, NULL);
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, text.c_str(), text.size());
+    MD5_Final(md, &ctx);
+
     for (int i = 0; i < 16; ++i) {
-        ss << setfill('0') << setw(2) << hex << (int)digest[i];
+        ss << setfill('0') << setw(2) << hex << (int)md[i];
     }
-    OPENSSL_free(digest);
+
     return ss.str();
 }
