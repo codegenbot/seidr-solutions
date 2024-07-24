725b5f2ba9d149afb44343843a0b9b2a1e7ae39b
beep boop <githubbot@vadim.me>
2024-07-24 18:55:27 +0200

    Solution 58 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/y8aamh4m Prompt: /usr/include/c++/11/bits/streambuf_iterator.h:421:5: note: candidate: ‘template< class _CharT2> typename __gnu_cxx::__enable_if<std::__is_char<_CharT2>::__value,  std::istreambuf_iterator<_CharT, std::char_traits<_CharT> > >::__type std::find (std::istreambuf_iterator<_CharT, std::char_traits<_CharT> >, std::istreambuf_it erator<_CharT, std::char_traits<_CharT> >, const _CharT2&)’   421 |     find(istreambuf_iterator<_CharT> __first,       |     ^~~~ /usr/include/c++/11/bits/streambuf_iterator.h:421:5: note:   template argument d eduction/substitution failed: 5d7ca642-d34b-480b-ab9b-f6237b3b15b0.cpp:9:17: note:   ‘__gnu_cxx::__normal_iter ator<int*, std::vector<int> >’ is not derived from ‘std::istreambuf_iterator<_Ch arT, std::char_traits<_CharT> >’     9 |         if (find(result.begin(), result.end(), x) == result.end())       |             ~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 5d7ca642-d34b-480b-ab9b-f6237b3b15b0.cpp: At global scope: 5d7ca642-d34b-480b-ab9b-f6237b3b15b0.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 5d7ca642-d34b-480b-ab9b-f6237b3b15b0.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -7,8 +7,7 @@ using namespace std;
 vector<int> unique(vector<int> l) {
     vector<int> result;
     for (int x : l) {
-        auto it = std::find(result.begin(), result.end(), x);
-        if (it == result.end())
+        if (find(result.begin(), result.end(), x) == result.end())
             result.push_back(x);
     }
     return result;
@@ -16,13 +15,7 @@ vector<int> unique(vector<int> l) {
 
 int main() {
     vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    vector<int> result(l.size()); 
-    for (int i : l) {
-        auto it = std::find(result.begin(), result.end(), i);
-        if (it == result.end())
-            result.push_back(i);
-    }
-    for (int i : result) {
+    for (int i : unique(l)) {
         cout << i << " ";
     }
     return 0;
