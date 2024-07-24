c0132dbb82e6a5169e5006b23fd006a36e85a3ae
beep boop <githubbot@vadim.me>
2024-07-24 03:48:28 +0200

    Solution 78 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/32qqhgdi Prompt:    57 | int main(){       |     ^~~~ 822ecfb3-b413-4414-8112-43e526143d13.cpp:48:5: note: ‘int main()’ previously def ined here    48 | int main() {       |     ^~~~ In file included from 822ecfb3-b413-4414-8112-43e526143d13.cpp:56: 822ecfb3-b413-4414-8112-43e526143d13.cpp: In function ‘int main()’: 822ecfb3-b413-4414-8112-43e526143d13.cpp:58:13: error: ‘boost’ has not been decl ared    58 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 822ecfb3-b413-4414-8112-43e526143d13.cpp:58:35: error: expected primary-expressi on before ‘>’ token    58 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                   ^ 822ecfb3-b413-4414-8112-43e526143d13.cpp:58:37: error: ‘compare_one’ was not dec lared in this scope    58 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,31 +1,31 @@
+```
 #include <iostream>
 #include<string>
 #include<algorithm>
 #include<stdexcept>
-#include<boost/any.hpp>
 
 using namespace std;
 
 std::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        double da = boost::any_cast<double>(a);
-        string db = boost::any_cast<string>(b);
+        double da = std::any_cast<double>(a);
+        string db = std::any_cast<string>(b);
         if (da > stod(db))
             return a;
         else
             return b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        double da = boost::any_cast<double>(b);
-        string db = boost::any_cast<string>(a);
+        double da = std::any_cast<double>(b);
+        string db = std::any_cast<string>(a);
         if (stod(db) > da)
             return a;
         else
             return b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string da = boost::any_cast<string>(a);
-        string db = boost::any_cast<string>(b);
+        string da = std::any_cast<string>(a);
+        string db = std::any_cast<string>(b);
         if (da > db)
             return a;
         else if (db > da)
@@ -34,8 +34,8 @@ std::any compare_one(std::any a, std::any b) {
             return "None";
     }
     else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double da = boost::any_cast<double>(a);
-        double db = boost::any_cast<double>(b);
+        double da = std::any_cast<double>(a);
+        double db = std::any_cast<double>(b);
         if (da > db)
             return a;
         else if (db > da)
@@ -47,9 +47,8 @@ std::any compare_one(std::any a, std::any b) {
 }
 
 int main() {
-    cout << boost::any_cast<any>(compare_one(1, 2.5)) << endl;
-    cout << boost::any_cast<string>(compare_one(1, "2,3")) << endl;
-    cout << boost::any_cast<string>(compare_one("5,1", "6")) << endl;
-    cout << boost::any_cast<string>(compare_one("1", 1)) << endl;
-    return 0;
+    cout << compare_one(1, 2.5) << endl;
+    cout << compare_one(1, "2,3") << endl;
+    cout << compare_one("5,1", "6") << endl;
+    cout << compare_one("1", 1) << endl;
 }
