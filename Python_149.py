6d9506953850f72598e5eab10257709817c006e8
beep boop <githubbot@vadim.me>
2024-07-24 16:13:05 +0200

    Solution 53 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dab91ed7-e34f-4df1-b275-7bbe091ce b50.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,10 +1,13 @@
-```
 def sorted_list_sum(words=None):
     if words is None:
-        lst = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
+        lst = [
+            word.strip().lower()
+            for word in input("Enter words separated by spaces: ").split()
+        ]
     else:
         lst = [word.strip().lower() for word in words]
-    return sorted([word for word in lst if len(word) % 2 == 0], key=lambda x:len(x))
+    return sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: len(x))
+
 
 if __name__ == "__main__":
     print(sorted_list_sum())
