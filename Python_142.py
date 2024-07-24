fc183b5dc447cb602a15ed1139cb38ea3e9c09c7
beep boop <githubbot@vadim.me>
2024-07-24 18:12:12 +0200

    Solution 96 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/8k3kmxe9 Prompt: Program did not receive expected input

--- Python_142.py
+++ Python_142.py
@@ -1,9 +1,8 @@
-```
 def sum_squares(numbers):
     total_sum = 0
     for num in numbers:
         total_sum += num ** 2
     return total_sum
 
-numbers = [int(x) for x in (input()).replace(" ", "").split(',')]
+numbers = [int(x) for x in input().split(', ')]
 print(sum_squares(list(map(int, numbers))))
