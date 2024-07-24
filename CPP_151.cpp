3b65345d0bc101f3a473a572573a78f604481389
beep boop <githubbot@vadim.me>
2024-07-24 11:20:47 +0200

    Solution 65 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/fjuf9l4y Prompt: 19821eca-6318-4b6e-b121-be75d0837806.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 19821eca-6318-4b6e-b121-be75d0837806.cpp:13: 19821eca-6318-4b6e-b121-be75d0837806.cpp: In function ‘int main()’: 19821eca-6318-4b6e-b121-be75d0837806.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 19821eca-6318-4b6e-b121-be75d0837806.cpp:15:13: error: ‘double_the_difference’ w as not declared in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 19821eca-6318-4b6e-b121-be75d0837806.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -13,17 +13,20 @@ long long double_the_difference(std::vector<float> lst){
 
 int main() {
     std::vector<float> lst;
-    float n;
-    std::cout << "Enter the number of elements in the list: ";
-    std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        std::cout << "Enter element " << i+1 << ": ";
-        float num;
-        std::cin >> num;
-        lst.push_back(num);
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        float x;
+        cin >> x;
+        lst.push_back(x);
     }
-    
+
     long long odd_sum = 0; 
+    for (float num : lst) {
+        if (num > 0 && floor(num) == num) {
+            odd_sum += pow(num, 2);
+        }
+    }
     assert(double_the_difference(lst) == odd_sum);
 
     return 0;
