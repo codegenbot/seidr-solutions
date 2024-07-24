db3cf086804a36e23c94eebebcd9937940fe3a95
beep boop <githubbot@vadim.me>
2024-07-24 18:30:41 +0200

    Solution 21 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bssem1dj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8397c467-42f0-46c5-87e0-e85e7d350 f21.py", line 6     else sorted([x for x in l if (index := i) % 2 == 0])[::-1][index % 2 != 0]                                   ^^^^^ SyntaxError: assignment expression cannot rebind comprehension iteration variabl e 'index'

--- Python_37.py
+++ Python_37.py
@@ -1,10 +1,5 @@
-```
 def sort_even(l: list):
     return [
-        (
-            i
-            if i % 2 != 0
-            else sorted([x for x in l1 if x % 2 == 0])[::-1][i % 2 != 0]
-        )
+        (i if i % 2 != 0 else sorted([x for x in l1 if x % 2 == 0])[::-1][i % 2 != 0])
         for i in enumerate(l)
     ]
