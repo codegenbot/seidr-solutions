d035b4002141367de40f4db412d805f58239d064
beep boop <githubbot@vadim.me>
2024-07-24 03:17:52 +0200

    Solution 85 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/walwymsv Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34ee530a-7e1f-4b5b-ab4a-a04ee9f6d 1de.py", line 15, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/34ee530a-7e1f-4b5b-ab4a-a04ee9f6d 1de.py", line 12, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,10 +1,12 @@
 def fizz_buzz(n: int):
+    count = 0
     for i in range(1, n + 1):
         if i % 15 == 0:
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz" if i % 5 != 0 else "FizzBuzz")
-        elif i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+            count += 1
+        elif i % 3 == 0 and i % 5 != 0:
+            count += (i // 3)
+        elif i % 5 == 0 and i % 3 != 0:
+            count += (i // 5)
+        elif i % 15 == 0:
+            count += 1
+    return count
