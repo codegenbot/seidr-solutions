7c9cac514bf6fa4099be75512773f3c06baaafc8
beep boop <githubbot@vadim.me>
2024-07-24 11:33:26 +0200

    Solution 30 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/gq1o6afn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6a50f2f8-d7ab-47e6-ac69-a2ec5f8f7 46a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,11 +1,2 @@
-def modp(a: int, n: int):
-    """Calculate a^(p-1) % p * a % p using exponentiation by squaring.
-    
-    Args:
-        a (int): Base number for exponentiation.
-        n (int): Power to which 'a' is raised and the modulo value.
-        
-    Returns:
-        int: The result of a^(p-1) % p * a % p.
-    """
-    return pow(a, n-1, n) * a % n
+def modp(n: int, p: int):
+    return (1 - pow(n, p-1, p)) % p
