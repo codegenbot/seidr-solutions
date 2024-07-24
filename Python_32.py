8b89fe315d043213a451756fa55cfb8794588bb6
beep boop <githubbot@vadim.me>
2024-07-24 08:20:54 +0200

    Solution 40 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/789pe6ps Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0372701a-8f11-4aa5-b564-15c77dcf8 6ea.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,9 +1,3 @@
 def find_zero(xs: list):
-    n = len(xs)
-    if n % 2 == 1:
-        x = xs[n // 2]
-        return -x / xs[-1]
-    else:
-        m1 = xs[(n - 1) // 2]
-        m2 = xs[n // 2]
-        return -(m1 + m2) / (2 * xs[-1])
+    n = len(xs) // 2
+    return -xs[n-1] / xs[-1]
