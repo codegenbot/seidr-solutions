2d2539999d3146363a7446f6f2352486d4238ce0
beep boop <githubbot@vadim.me>
2024-07-24 20:19:18 +0200

    Solution 5 of Python/125, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/l2jaermk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2fb940c9-7762-4d55-baf7-efd89a6d0 d5a.py", line 15, in <module>     check(split_words)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2fb940c9-7762-4d55-baf7-efd89a6d0 d5a.py", line 12, in check     assert split_words("") == 0            ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2fb940c9-7762-4d55-baf7-efd89a6d0 d5a.py", line 3, in split_words     if any(char.isalnum() for char in words[0]):                                       ~~~~~^^^ IndexError: list index out of range

--- Python_125.py
+++ Python_125.py
@@ -1,10 +1,15 @@
 def split_words(txt):
+    if not txt:  
+        return 0
     words = txt.split()
-    if any(char.isalnum() for char in words[0]):
-        return words
+    if words:
+        if any(char.isalnum() for char in words[0]):
+            return len(words)
+        else:
+            return sum(
+                1
+                for i in range(ord("a"), ord("z") + 1)
+                if txt.lower().count(chr(i)) % 2 == 1
+            )
     else:
-        return sum(
-            1
-            for i in range(ord("a"), ord("z") + 1)
-            if txt.lower().count(chr(i)) % 2 == 1
-        )
+        return 0
