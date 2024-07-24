86d8ea6e1e7ffa348f4d46e7d2bcf3ea46eaceea
beep boop <githubbot@vadim.me>
2024-07-24 20:12:28 +0200

    Solution 63 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ax209ivw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98602261-c6bb-4659-9a07-962eed995 af5.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/98602261-c6bb-4659-9a07-962eed995 af5.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,7 +2,7 @@ def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            result += chr((ord(char) - ord("A") + 3) % 26 + ord("A")) if char.isupper() else chr((ord(char) - ord("a") + 3) % 26 + ord("a"))
+            result += chr((ord(char) - ord('a') + 3) % 26 + ord('a')) if char.islower() else chr((ord(char) - ord('A') + 3) % 26 + ord('A'))
         else:
             result += char
     return result
