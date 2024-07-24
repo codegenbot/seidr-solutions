62d54341050d2a330df8852a2ee7c6bebbcab7eb
beep boop <githubbot@vadim.me>
2024-07-24 19:37:54 +0200

    Solution 89 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/apf6ljhk Prompt:       |    ^~~ f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp:4:6: error: ‘string’ in namespace ‘std’  does not name a type     4 | std::string int_to_roman(int num) {       |      ^~~~~~ f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp:3:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     2 | #include <cassert>   +++ |+#include <string>     3 | In file included from /usr/include/c++/11/cassert:44,                  from f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp:2: f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp: In function ‘int testMain()’: f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp:23:12: error: ‘int_to_roman’ was not de clared in this scope    23 |     assert(int_to_roman(1000) == "M");       |            ^~~~~~~~~~~~ In file included from f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp:27: f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp: In function ‘int main()’: f3f6af9c-22be-4339-8d80-cc7de0c03335.cpp:29:13: error: ‘int_to_mini_romank’ was not declared in this scope    29 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,44 +1,20 @@
 #include <cassert>
 #include <string>
 
-std::string int_to_roman(int num) {
+std::string romanToInt(std::int32_t num) {
     if (num <= 0)
-        return "0";
-    std::string roman = "";
-    int thousands = num / 1000;
-    for (int i = 0; i < thousands; i++)
-        roman += "M";
-    num %= 1000;
-    int hundreds = num / 100;
-    if (hundreds > 3)
-        roman += "D" + std::string(4 - hundreds, 'C');
-    else
-        for (int i = 0; i < hundreds; i++)
-            roman += "C";
-    num %= 100;
-    int tens = num / 10;
-    if (tens == 9)
-        roman += "CM";
-    else if (tens >= 5) {
-        roman += "D";
-        num -= 50;
-    }
-    for (int i = 0; i < tens; i++)
-        roman += "X";
-    num %= 10;
-    int ones = num;
-    if (ones == 9)
-        roman += "IX";
-    else if (ones >= 5) {
-        roman += "V";
-        num -= 5;
-    }
-    for (int i = 0; i < ones; i++)
-        roman += "I";
-    return roman;
-}
+        return "Invalid input";
 
-int testMain() {
-    assert(int_to_roman(1000) == "M");
-    return 0;
+    std::string roman = "";
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    for (int i = 0; i < sizeof(values) / sizeof(int); i++) {
+        while (num >= values[i]) {
+            num -= values[i];
+            roman += symbols[i];
+        }
+    }
+
+    return roman;
 }
