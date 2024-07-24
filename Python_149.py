e5061f7baaa6868b2bb71f01c0aa18cd35921f79
beep boop <githubbot@vadim.me>
2024-07-24 07:15:57 +0200

    Solution 92 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/982b793c-7448-4343-8937-78f876c0d 51e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,5 @@
 def sorted_list_sum(lst):
-    if all(i.isnumeric() for i in lst):
-        return sum(sorted(map(int, lst)))
+    if all(i.isdigit() for i in lst):  
+        return sum(sorted(map(int, lst)))  
     else:
         print("Invalid input. Please enter a list of numbers.")
