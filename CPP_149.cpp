f6d356ceb0599ea4bf0aab069eb7ee1801a0593c
beep boop <githubbot@vadim.me>
2024-07-24 17:32:34 +0200

    Solution 72 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cmheh2hi Prompt:       |                                            ^ cca33f03-0467-4520-b093-b31de728af6e.cpp:77:5: error: redefinition of ‘int main( )’    77 | int main(){       |     ^~~~ cca33f03-0467-4520-b093-b31de728af6e.cpp:60:5: note: ‘int main()’ previously def ined here    60 | int main() {       |     ^~~~ In file included from cca33f03-0467-4520-b093-b31de728af6e.cpp:68: cca33f03-0467-4520-b093-b31de728af6e.cpp: In function ‘int main()’: cca33f03-0467-4520-b093-b31de728af6e.cpp:78:35: error: invalid initialization of  reference of type ‘const std::vector<std::vector<std::__cxx11::basic_string<cha r> > >&’ from expression of type ‘<brace-enclosed initializer list>’    78 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ cca33f03-0467-4520-b093-b31de728af6e.cpp:17:87: note: in passing argument 1 of ‘ std::vector<std::__cxx11::basic_string<char> > sorted_list_sum(const std::vector <std::vector<std::__cxx11::basic_string<char> > >&)’    17 | ng> sorted_list_sum(const std::vector<std::vector<std::string>>& lst) {       |                     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <algorithm>
 #include <vector>
 #include <string>
@@ -58,7 +59,6 @@ std::vector<std::string> sorted_list_sum(const std::vector<std::vector<std::stri
 }
 
 int main() {
-    if (!issame(sorted_list_sum({{"aaaa"}, {"bbbb"}, {"dd"}, {"cc"}}), {"even", "even", "odd", "even"})) {
-        return 1;
-    }
-    return 0;
+    std::vector<std::vector<std::string>> input = {{"aaaa"}, {"bbbb"}, {"dd"}, {"cc"}};
+    assert(issame(sorted_list_sum(input), {"even", "even", "odd", "even"}));
+}
