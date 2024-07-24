fffe0f24c71d5ac28a32aacd6a00e0f2e9c2a9a5
beep boop <githubbot@vadim.me>
2024-07-24 16:57:17 +0200

    Solution 80 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/z12svfy3 Prompt: None Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96f25f36-039a-4924-9879-00b5f922d a7d.py", line 26, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96f25f36-039a-4924-9879-00b5f922d a7d.py", line 23, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -2,19 +2,17 @@
 def minPath(grid, k):
     N = len(grid)
     queue = [(0, 0, [])]
-    res = None
-    mindist = float('inf')
+    res = []
     while queue:
         row, col, path = queue.pop(0)
         if len(path) == k:
-            res = tuple(sorted(set(path)))
-            mindist = min(mindist, len(path))
+            res.append(tuple(sorted(set(path))))
         else:
             for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nr, nc = row + dr, col + dc
                 if 0 <= nr < N and 0 <= nc < N and grid[nr][nc] != '0':
                     queue.append((nr, nc, path + [grid[nr][nc]]))
-    return res if mindist == len(res) else None
+    return min(res) if res else None
 
 grid = [["1", "0", "1"], ["0", "0", "0"], ["1", "0", "1"]]
 k = 3
