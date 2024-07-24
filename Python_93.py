abc69d2132db713ed4b9d7bc30363478861603d2
beep boop <githubbot@vadim.me>
2024-07-24 03:46:12 +0200

    Solution 24 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/k4a5f9yk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d001a440-8559-4445-a2ee-3ebfb1748 a6d.py", line 22, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d001a440-8559-4445-a2ee-3ebfb1748 a6d.py", line 18, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,13 +1,16 @@
+```
 def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if ('aeiou'.find(char.lower()) != -1):  
+            if 'aeiou'.find(char.lower()) != -1:  
                 result += chr((ord(char) - 97 + 2) % 26 + 97)  
             elif char.isupper():  
                 result += chr((ord(char) - 65 + 2) % 26 + 65)  
             else:
-                result += char
+                result += (char.swapcase())  
         else:
+            if result and result[-1].isalpha():
+                result += ' '
             result += char
     return result
