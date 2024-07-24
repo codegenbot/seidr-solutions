df3f516f4e1e4c282011628dce9939cf6048d1b4
beep boop <githubbot@vadim.me>
2024-07-24 17:03:43 +0200

    Solution 80 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4rw3ddrj Prompt:       |                    ^~~ d69e3b11-ffea-44e1-b9c4-835e27872016.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d69e3b11-ffea-44e1-b9c4-835e27872016.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d69e3b11-ffea-44e1-b9c4-835e27872016.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d69e3b11-ffea-44e1-b9c4-835e27872016.cpp:12: d69e3b11-ffea-44e1-b9c4-835e27872016.cpp: In function ‘int main()’: d69e3b11-ffea-44e1-b9c4-835e27872016.cpp:22:20: error: ‘factorize’ was not decla red in this scope    22 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ d69e3b11-ffea-44e1-b9c4-835e27872016.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,27 +1,22 @@
 #include <vector>
+#include <cassert>
 
-vector<int> factorize(int n) {
+bool issame(const vector<int>& a, const vector<int>& b){
+    return a == b;
+}
+
+vector<int> factorize(int n){
     vector<int> factors;
-    for (int i = 2; i * i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         while (n % i == 0) {
             factors.push_back(i);
             n /= i;
         }
     }
-    if (n > 1) {
-        factors.push_back(n);
-    }
     return factors;
 }
 
-bool issame(vector<int> v1, vector<int> v2) {
-    if (v1.size() != v2.size()) {
-        return false;
-    }
-    for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
+int main() {
+    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
+    return 0;
 }
