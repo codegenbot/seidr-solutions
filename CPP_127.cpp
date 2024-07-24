b5b8b243c409c206aca90287ab676622aa75f62f
beep boop <githubbot@vadim.me>
2024-07-24 21:59:17 +0200

    Solution 24 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lgm8k84y Prompt: Program failed

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -8,9 +8,10 @@ string intersection(vector<int> interval1, vector<int> interval2) {
     int length = end - start + 1;
 
     bool isPrime = true;
-    for(int i=2; i*i<=length && isPrime; i++) {
+    for(int i=2; i*i<=length; i++) {
         if(length%i==0) {
             isPrime=false;
+            break;
         }
     }
 
