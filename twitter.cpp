7f3e8119193d330e7e35bc03f02e8d4260adbd16
beep boop <githubbot@vadim.me>
2024-07-24 04:21:45 +0200

    Solution 84 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/b1y7xy27 Prompt: it must return Your tweet has 77 characters for input ~&&aZ1_W)]]Piik5\o$[grWWO`~Q4N{}@wy4(bh'MA;N.<RinSO6@,Z`<w-caq)V#7M$|e!_AjK<L, but it returns Enter a tweet: Your tweet has 77 characters.

--- twitter.cpp
+++ twitter.cpp
@@ -7,13 +7,18 @@ int main() {
     string tweet;
     cout << "Enter a tweet: ";
     getline(cin, tweet);
-    
-    tweet.erase(tweet.find_last_of('\n'));
+
+    if (tweet.find_last_of('\n') != string::npos) {
+        tweet.erase(tweet.find_last_of('\n'));
+    }
 
     if (tweet.empty()) {
         cout << "You didn't type anything" << endl;
         return 0;
     }
+
+    tweet.erase(0, tweet.find_first_not_of(' ')); // remove leading whitespace
+    tweet.erase(tweet.find_last_not_of(' ') + 1); // remove trailing whitespace
 
     if (tweet.size() > 140) {
         cout << "Too many characters" << endl;
