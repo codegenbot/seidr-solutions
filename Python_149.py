8a09b7cd5f4ca29f2127d29d7fdc58c0802b0150
beep boop <githubbot@vadim.me>
2024-07-24 06:25:21 +0200

    Solution 49 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2dbec219-8eac-446e-b5cd-aa37fb7a5 33d.py", line 9, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2dbec219-8eac-446e-b5cd-aa37fb7a5 33d.py", line 6, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2dbec219-8eac-446e-b5cd-aa37fb7a5 33d.py", line 2, in sorted_list_sum     return sum(int(num) for num in input_data)            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2dbec219-8eac-446e-b5cd-aa37fb7a5 33d.py", line 2, in <genexpr>     return sum(int(num) for num in input_data)                ^^^^^^^^ ValueError: invalid literal for int() with base 10: 'aaaa'

--- Python_149.py
+++ Python_149.py
@@ -1,10 +1,3 @@
+```
 def sorted_list_sum(input_data):
-    word_dict = {}
-    for word in input_data:
-        if word not in word_dict:
-            word_dict[word] = len(word)
-        else:
-            word_dict[word] = len(word)
-
-    nums = sorted(word_dict.items(), key=lambda x: (-x[1], x[0]))
-    return [num[1] for num in nums]
+    return [''.join(sorted(x)) for x in sorted(input_data, key=len)]
