7ae180a603adca2e7f5ecdff0e96bcd84c09a54f
beep boop <githubbot@vadim.me>
2024-07-24 03:43:26 +0200

    Solution 21 of CPP/160, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/02oov1vw Prompt: d52e4b69-5788-4644-a81f-380b802d7cb1.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator, vector<int> operand) {       |                ^~~~~~ d52e4b69-5788-4644-a81f-380b802d7cb1.cpp:1:23: error: ‘string’ was not declared in this scope     1 | int do_algebra(vector<string> operator, vector<int> operand) {       |                       ^~~~~~ d52e4b69-5788-4644-a81f-380b802d7cb1.cpp:1:39: error: ‘operator,’ not defined     1 | int do_algebra(vector<string> operator, vector<int> operand) {       |                                       ^ In file included from d52e4b69-5788-4644-a81f-380b802d7cb1.cpp:28: d52e4b69-5788-4644-a81f-380b802d7cb1.cpp: In function ‘int main()’: d52e4b69-5788-4644-a81f-380b802d7cb1.cpp:30:23: error: ‘do_algebra’ cannot be us ed as a function    30 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,16 +1,51 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = operand[0];
+#include <iostream>
+#include <vector>
+#include <string>
+
+int do_algebra(std::vector<std::string> operator_, std::vector<int> operand_) {
+    int result = 0;
     for (int i = 0; i < operator_.size(); i++) {
         if (operator_[i] == "+") {
-            result += operand[i + 1];
+            result += operand_[i];
         } else if (operator_[i] == "-") {
-            result -= operand[i + 1];
+            result -= operand_[i];
         } else if (operator_[i] == "*") {
-            result *= operand[i + 1];
-        } else if (operator_[i] == "//") {
-            result = result / operand[i + 1];
-        } else if (operator_[i] == "**") {
-            result = pow(result, operand[i + 1]);
+            int temp = 1;
+            for (int j = 0; j <= i; j++) {
+                temp *= operand_[j];
+            }
+            result += temp;
+        } else if (operator_[i] == "/" || operator_[i] == "//" || operator_[i] == "**") {
+            int temp = 0;
+            switch (operator_[i].c_str()[0]) {
+                case '/':
+                    temp = operand_[i - 1] / operand_[i];
+                    break;
+                case '*':
+                    for (int j = 0; j < operand_[i]; j++) {
+                        temp *= operand_[0];
+                    }
+                    result += temp;
+                    break;
+                case 'S':
+                    if (operator_[i].size() == 2) {
+                        int a = operand_[0], b = operand_[1];
+                        temp = 1;
+                        for (int j = 0; j < b; j++) {
+                            temp *= a;
+                        }
+                    } else {
+                        int a = operand_[0], b = operand_[i + 1];
+                        temp = 1;
+                        for (int j = 0; j < b; j++) {
+                            temp *= a;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+            result += temp;
         }
     }
     return result;
