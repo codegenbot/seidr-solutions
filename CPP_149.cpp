fff3da00bce40e6f7cb422fc72e9cf534cbd9438
beep boop <githubbot@vadim.me>
2024-07-24 09:07:18 +0200

    Solution 39 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7meh2yxx Prompt:    33 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ b9a4fb7f-93af-4e53-8b70-1cab190d0fc4.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b9a4fb7f-93af-4e53-8b70-1cab190d0fc4.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from b9a4fb7f-93af-4e53-8b70-1cab190d0fc4.cpp:32: b9a4fb7f-93af-4e53-8b70-1cab190d0fc4.cpp: In function ‘int main()’: b9a4fb7f-93af-4e53-8b70-1cab190d0fc4.cpp:42:35: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    42 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                   |       |                                   <brace-enclosed initializer list> b9a4fb7f-93af-4e53-8b70-1cab190d0fc4.cpp:21:21: note:   initializing argument 1 of ‘int sorted_list_sum(int)’    21 | int sorted_list_sum(const vector<vector<string>>& lst) {       |                     ^~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -7,7 +7,7 @@ bool issame(const vector<vector<string>>& a, const vector<vector<string>>& b) {
             return false;
         }
         for(int j=0; j<a[i].size(); j++) {
-            if (a[i][j] != b[i][j]) {
+            if(a[i][j] != b[i][j]) {
                 return false;
             }
         }
@@ -18,4 +18,15 @@ bool issame(const vector<vector<string>>& a, const vector<vector<string>>& b) {
 int main() {
     int sum = sorted_list_sum({{"aaaa", "bbbb"}, {"dd"}, {"cc"}});
     return 0;
+}
+
+int sorted_list_sum(const vector<vector<string>>& lst) {
+    int sum = 0;
+    for (const auto& sublst : lst) {
+        std::sort(sublst.begin(), sublst.end());
+        if (issame({{ "hello", "world" }}, {sublst})) {
+            sum += std::stoi(sublst[1].substr(7));
+        }
+    }
+    return sum;
 }
