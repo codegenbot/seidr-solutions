98a56bbbb4e9f66817f1177b9f98c77c94d25b71
beep boop <githubbot@vadim.me>
2024-07-24 07:37:39 +0200

    Solution 5 of CPP/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zqprr0ap Prompt: Complete the following code given the task description and function signature.

--- CPP_124.cpp
+++ CPP_124.cpp
@@ -2,52 +2,37 @@
 using namespace std;
 
 bool valid_date(string date) {
-    string month_day_year[3];
-    int year, month, day;
-    size_t pos = 0;
-    
-    while ((pos = date.find('-')) != string::npos) {
-        month_day_year[0] = date.substr(0, pos);
-        date.erase(0, pos + 1);
-        
-        if (date.empty())
-            break;
-        
-        pos = date.find('-');
-        month_day_year[1] = date.substr(0, pos);
-        date.erase(0, pos + 1);
+    int day, month, year;
+    string temp = "";
+    for (int i = 0; i < date.length(); i++) {
+        if (isdigit(date[i])) {
+            temp += date[i];
+        } else if (temp != "") {
+            if (i == 2) {
+                year = stoi(temp);
+                temp = "";
+            } else if (i == 5) {
+                month = stoi(temp);
+                temp = "";
+            } else if (i == date.length() - 4) {
+                day = stoi(temp);
+                break;
+            }
+        }
     }
-    
-    month_day_year[2] = date;
-    
-    year = stoi(month_day_year[2]);
-    string temp = month_day_year[1];
-    month_day_year[1] = month_day_year[0];
-    month_day_year[0] = temp;
-    int m = stoi(month_day_year[0]);
-    day = stoi(month_day_year[1]);
-    
-    if (m < 1 || m > 12)
-        return false;
-    
-    if ((m == 2 && day > 29) || (m == 4 || m == 6 || m == 9 || m == 11) && day > 30)
-        return false;
-    
-    else if ((m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) && day > 31)
-        return false;
-    
-    return true;
-}
 
-int main() {
-    cout << valid_date("03-11-2000"); // true
-    cout << endl;
-    cout << valid_date("15-01-2012"); // false
-    cout << endl;
-    cout << valid_date("04-0-2040"); // false
-    cout << endl;
-    cout << valid_date("06-04-2020"); // true
-    cout << endl;
-    cout << valid_date("06/04/2020"); // false
-    return 0;
+    if (temp != "") {
+        return false;
+    }
+
+    if (month < 1 || month > 12)
+        return false;
+
+    if ((month == 2 && day > 29) || (month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
+        return false;
+
+    if (month in {1,3,5,7,8,10,12} && day > 31)
+        return false;
+
+    return true;
 }
