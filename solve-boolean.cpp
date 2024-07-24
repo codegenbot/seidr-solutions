57ae5c9a6186e18a719d8e5a6c01eda12d5f005f
beep boop <githubbot@vadim.me>
2024-07-24 03:27:34 +0200

    Solution 62 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/z7qa9oa3 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -10,39 +10,70 @@ bool evalBoolean(string s) {
     while (j < s.length() && (s[j] == '|' || s[j] == '&'))
         j++;
 
-    string opStr;
-    for (char c : s.substr(0, j)) {
-        opStr += c;
-    }
+    string opStr = s.substr(0, j);
     bool result = false;
     if (opStr[0] == '|') {
-        for (char c : s.substr(j)) {
-            if (c == 'T')
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T')
                 result = true;
-            else if (c == 'F') {
+            else if (s[i] == 'F') {
                 result = false;
-                return result;
             }
         }
     } else {
-        bool left = false;
-        for (char c : s.substr(0, j)) {
-            if (c == 'T' && opStr[1] != '|')
-                left = true;
-            else if (c == 'F' && opStr[1] != '&')
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T' && opStr[1] != '|')
+                result = true;
+            else if (s[i] == 'F' && opStr[1] != '&')
                 return false;
         }
-        result = left;
     }
 
-    return result;
+    if (!result)
+        return false;
+
+    string rest = s.substr(j);
+    if (rest.empty())
+        return !result; 
+    else
+        return evalBoolean(rest); 
+}
+
+bool evalOp(string s) {
+    int j = 0;
+    while (j < s.length() && (s[j] == '|' || s[j] == '&'))
+        j++;
+
+    string opStr = s.substr(0, j);
+    bool left = false;
+    if (opStr[0] == '|') {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T')
+                left = true;
+            else if (s[i] == 'F') {
+                left = false;
+            }
+        }
+    } else {
+        for (int i = 0; i < j; i++) {
+            if (s[i] == 'T' && opStr[1] != '|')
+                left = true;
+            else if (s[i] == 'F' && opStr[1] != '&')
+                return false;
+        }
+    }
+
+    if (!left)
+        return false;
+
+    return evalBoolean(s.substr(j));
 }
 
 int main() {
     string s;
     cout << "Enter a Boolean expression: ";
     cin >> s;
-    bool result = evalBoolean(s);
+    bool result = evalOp(s);
     if (result)
         cout << "True";
     else
