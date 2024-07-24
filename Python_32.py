b7d855cbd94b2e64208c2e0bafe1edb8dc5468ed
beep boop <githubbot@vadim.me>
2024-07-24 22:37:25 +0200

    Solution 20 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/z0rl6fru Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b87e7700-43c3-4bfe-a1e5-cfb20af29 bed.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -14,8 +14,8 @@ def find_zero(xs: list):
 
 
 if __name__ == "__main__":
-    xs = list(map(float, input("Enter coefficients: ").split()))
+    xs = list(map(int, input("Enter coefficients: ").split()))
     try:
         print(find_zero(xs))
     except ValueError as e:
-        print(f"Error: {e}")
+        print(e)
