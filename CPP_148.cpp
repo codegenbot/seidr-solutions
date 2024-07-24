547d39b81e9f975ed8bbf81b9a4425c829382748
beep boop <githubbot@vadim.me>
2024-07-24 15:03:09 +0200

    Solution 6 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/30jpob9f Prompt:       |           ^~~~ f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp:31:21: error: request for member ‘size’  in ‘b’, which is of non-class type ‘int’    31 |     if (a.size()!=b.size()) return false;       |                     ^~~~ f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp:32:22: error: request for member ‘size’  in ‘a’, which is of non-class type ‘int’    32 |     for (int i=0;i<a.size();i++)       |                      ^~~~ f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp:34:10: error: invalid types ‘int[int]’ for array subscript    34 |     if (a[i]!=b[i]) return false;       |          ^ f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp:34:16: error: invalid types ‘int[int]’ for array subscript    34 |     if (a[i]!=b[i]) return false;       |                ^ In file included from f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp:29: f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp: In function ‘int main()’: f370f97f-a0d5-42dd-873b-c3c5f368fc07.cpp:39:22: error: ‘bf’ cannot be used as a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,6 +1,8 @@
+#include <vector>
+using namespace std;
+
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    
     int index1 = -1, index2 = -1;
     
     for (int i = 0; i < planets.size(); i++) {
