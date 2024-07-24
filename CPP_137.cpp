6e52ec9f68738669425e450038c57ecdcf897c0b
beep boop <githubbot@vadim.me>
2024-07-24 09:34:25 +0200

    Solution 12 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/m3e7eqps Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
-#include<string>
-#include<boost/any.hpp>
-
-using namespace std;
-using namespace boost;
-
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<float>(a) && is_any_of<float>(b)) {
-        return max(get<float>(a), get<float>(b));
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y ? &a : (y > x ? &b : boost::any("None")));
     }
-    else if (is_any_of<string>(a) && is_any_of<string>(b)) {
-        return max(a, b);
-    } 
-    else if (is_any_of<float>(a) && is_any_of<string>(b)) {
-        return (get<float>(a) > stod(string(get<string>(b)).erase(0,1).erase(-2))) ? a : b;
+    else if ((a.type() == typeid(double) && b.type() == typeid(int)) ||
+             (a.type() == typeid(int) && b.type() == typeid(double))) {
+        double x = boost::any_cast<double>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y ? &a : (y > x ? &b : boost::any("None")));
     }
-    else if (is_any_of<string>(a) && is_any_of<float>(b)) {
-        return (stod(string(a).erase(0,1).erase(-2)) > get<float>(b)) ? a : b;
+    else if ((a.type() == typeid(string) && b.type() == typeid(double)) ||
+             (a.type() == typeid(double) && b.type() == typeid(string))) {
+        string x = boost::any_cast<string>(a);
+        double y = boost::any_cast<double>(b);
+        return (stod(x) > y ? &a : (y > stod(x) ? &b : boost::any("None")));
+    }
+    else if ((a.type() == typeid(string) && b.type() == typeid(string))) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        return (stod(y) > stod(x) ? &b : (stod(x) > stod(y) ? &a : boost::any("None")));
     }
     else {
-        return "None";
+        throw invalid_argument("Invalid types");
     }
 }
