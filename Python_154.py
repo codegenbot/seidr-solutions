6b49a384014a63053dea6de68e1eed9cf9f4ebba
beep boop <githubbot@vadim.me>
2024-07-24 05:35:34 +0200

    Solution 10 of Python/154, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/62y31j2y Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c6c4bd4-2cc9-4b17-84c6-e81d5f69d aa2.py", line 14, in <module>     check(cycpattern_check)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8c6c4bd4-2cc9-4b17-84c6-e81d5f69d aa2.py", line 11, in check     assert cycpattern_check("winemtt", "tinem") == True, "test #5"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: test #5

--- Python_154.py
+++ Python_154.py
@@ -3,9 +3,6 @@ def cycpattern_check(a, b):
     if len(b) > len(a):
         return False
     for i in range(len(a)):
-        if a[i:] + a[:i] == b:
-            return True
-    for i in range(len(a)):
-        if a[i:i+len(b)] == b:
+        if a[i:] + a[:i] == b or a[a.index(b[0:i]):] + a[:a.index(b[0:i])] == b:
             return True
     return False
