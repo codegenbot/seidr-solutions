d453ec8ca320a6eefd7b722fb5863f54faef4910
beep boop <githubbot@vadim.me>
2024-07-24 04:29:09 +0200

    Solution 85 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/dk4hok2d Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <climits>
+#include <cmath>
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int>& nums) {
@@ -8,20 +9,13 @@ vector<vector<int>> cutVector(vector<int>& nums) {
     
     int min_diff = INT_MAX;
     for (int i = 1; i < n; i++) {
-        int diff1 = 0, diff2 = 0;
-        for (int j = 0; j < i; j++) {
-            diff1 += nums[j];
-        }
-        for (int j = i; j < n; j++) {
-            diff2 += nums[j];
-        }
-        
-        if (abs(diff1 - diff2) < abs(min_diff)) {
+        int diff1 = abs(nums[i] - nums[0]);
+        int diff2 = abs(nums[n-1] - nums[i-1]);
+        if ((diff1 == diff2) || (abs(diff1-diff2) < abs(min_diff))) {
             min_diff = diff1 - diff2;
             result.push_back(vector<int>(nums.begin(), nums.begin() + i));
-            vector<int> temp(nums.begin() + i, nums.end());
-            result.push_back(temp);
-            return result;
+            vector<int> rest(nums.begin() + i, nums.end());
+            nums = rest;
         }
     }
     
@@ -35,11 +29,12 @@ vector<vector<int>> cutVector(vector<int>& nums) {
 int main() {
     vector<int> nums = {1, 2, 3};
     vector<vector<int>> result = cutVector(nums);
-    for (auto& vec : result) {
-        for (int i : vec) {
-            cout << i << " ";
+    for (auto& v : result) {
+        cout << "[";
+        for (int num : v) {
+            cout << num << " ";
         }
-        cout << endl;
+        cout << "]"<<endl;
     }
     return 0;
 }
