2572bda59cb1f3af02f81e292cccb65342af09dc
beep boop <githubbot@vadim.me>
2024-07-24 16:42:26 +0200

    Solution 2 of CPP/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/p5a7uo6j Prompt: Complete the following code given the task description and function signature.

--- CPP_4.cpp
+++ CPP_4.cpp
@@ -1,14 +1,14 @@
 float mean_absolute_deviation(vector<float> numbers) {
-    float mean = 0.0;
+    float mean = 0.0f;
     for (float num : numbers) {
         mean += num;
     }
     mean /= numbers.size();
 
-    float sum = 0.0;
+    float mad = 0.0f;
     for (float num : numbers) {
-        sum += abs(num - mean);
+        mad += abs(num - mean);
     }
-
-    return sum / numbers.size();
+    mad /= numbers.size();
+    return mad;
 }
