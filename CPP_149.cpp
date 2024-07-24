881aba086225e39317f8449372e1e83ddf32312f
beep boop <githubbot@vadim.me>
2024-07-24 13:37:30 +0200

    Solution 81 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt: In file included from f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp:13: f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp: In function ‘int main()’: f58f4b0b-1e2c-4893-825a-f8c1ea91a399.cpp:23:20: error: ‘sorted_list_sum’ was not  declared in this scope    23 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -4,12 +4,10 @@
 #include <cassert>
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    std::vector<std::string> sorted_a(a.begin(), a.end());
-    std::vector<std::string> sorted_b(b.begin(), b.end());
-    
+    std::vector<std::string> sorted_a = a;
+    std::vector<std::string> sorted_b = b;
     std::sort(sorted_a.begin(), sorted_a.end());
     std::sort(sorted_b.begin(), sorted_b.end());
-    
     return sorted_a == sorted_b;
 }
 
