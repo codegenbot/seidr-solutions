43b7c0bb9b4d8d900083e041fa2757eb2a4c7bfb
beep boop <githubbot@vadim.me>
2024-07-24 05:59:11 +0200

    Solution 25 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a1842feb-6a71-4dbc-9cd5-a27ae592e cf2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_149.py
+++ Python_149.py
@@ -1,12 +1,7 @@
-```
 def sorted_list_sum(input_data):
-    try:
-        input_string = ' '.join(map(str, input_data))
-        nums = sorted([int(num) for num in input_string.split() if num.isdigit()])
-    except ValueError as e:
-        print("Invalid input. Please enter a valid integer or digits separated by spaces.")
-        return -1
-
+    input_string = ' '.join(map(str, input_data))
+    nums = sorted([int(num) for num in input_string.split() if num.isdigit()])
+    
     while True:
         target = input("Enter a number (or 'q' to quit): ")
         if target == 'q':
