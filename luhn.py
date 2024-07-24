9d5945111d6edbb5264efecd2b3e43c8b85ddfd2
beep boop <githubbot@vadim.me>
2024-07-24 06:57:36 +0200

    Solution 20 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/4vklj0xo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cef98bc5-c083-4b08-a3fa-d9923f270 070.py", line 1     return sum(int(x) for x in str(sum(map(lambda x: 2*x if (i:=len(card)-1)//2! =i else x, card))) if int(x)>9 then int(x)-9 else int(x))                                ^^^^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -1,2 +1,1 @@
-return sum(int(x) for x in str(sum(map(lambda x: 2*x if i % 2 == 0 else x, card))) 
-          if int(x) > 9 else int(x))
+return sum(int(d) for d in str(sum(map(lambda x: (2*x) if len(card)-1-i%2 else x, range(len(card)))))) if int(d)>9 else int(d)-9)
