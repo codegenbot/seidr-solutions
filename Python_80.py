7fb898cffa2f8368a89e784cd5d6531f95711420
beep boop <githubbot@vadim.me>
2024-07-24 03:45:10 +0200

    Solution 22 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4pstys41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eae641c5-ac8a-49b9-bbaa-c7fbf0e3f 81c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_80.py
+++ Python_80.py
@@ -1,11 +1,16 @@
+```Python
+def sum_of_squares(digits):
+    return sum(int(digit) ** 2 for digit in str(digits))
+
 def is_happy_number(n):
     if n < 1:
         return False
+
     seen = set()
     while n != 1 and n not in seen:
         seen.add(n)
-        sum_of_digits = sum(int(digit) ** 2 for digit in str(n))
-        n = sum_of_digits
+        n = sum_of_squares(n)
+
     return n == 1
 
 while True:
