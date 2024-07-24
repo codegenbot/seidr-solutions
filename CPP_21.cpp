18d7d21035de20da99da5bffaf8cceca7180ac82
beep boop <githubbot@vadim.me>
2024-07-24 15:56:31 +0200

    Solution 3 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/v0hwca1q Prompt: Complete the following code given the task description and function signature.

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,19 +1,19 @@
-float min_val = numbers[0];
-    float max_val = numbers[0];
+float min_num = numbers[0];
+    float max_num = numbers[0];
     
     for (float num : numbers) {
-        if (num < min_val) {
-            min_val = num;
+        if (num < min_num) {
+            min_num = num;
         }
-        if (num > max_val) {
-            max_val = num;
+        if (num > max_num) {
+            max_num = num;
         }
     }
     
     vector<float> rescaled_numbers;
     
     for (float num : numbers) {
-        float rescaled_num = (num - min_val) / (max_val - min_val);
+        float rescaled_num = (num - min_num) / (max_num - min_num);
         rescaled_numbers.push_back(rescaled_num);
     }
     
