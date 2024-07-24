fc1805eba02c79f30fc1945159b949f6d5dc37b9
beep boop <githubbot@vadim.me>
2024-07-24 06:09:47 +0200

    Solution 71 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ab8c04c-b754-4e86-9600-5d4431528 aee.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,25 +1,20 @@
 def fix_spaces(text):
-    if text.strip().startswith(" "):
-        new_text = "-"
-    elif text.isspace():
-        return " "
-    else:
-        new_text = ""
-        add_hyphen = False
-        for char in text:
-            if char.isalnum():
-                new_text += char
-                add_hyphen = False
-            elif char in ["-", "_"]:
-                new_text += char
-                add_hyphen = True
-            elif char.isspace() or (char.isspace() and not add_hyphen):
-                if len(new_text) > 0:
-                    new_text += "-"
-                else:
-                    new_text += " "
-                add_hyphen = False
-            else:
+    new_text = ""
+    add_hyphen = False
+    for char in text:
+        if char.isalnum():
+            new_text += char
+            add_hyphen = False
+        elif char in ["-", "_"]:
+            new_text += char
+            add_hyphen = True
+        elif char.isspace() or (char.isspace() and not add_hyphen):
+            if len(new_text) > 0:
                 new_text += "-"
-                add_hyphen = True
+            else:
+                new_text += " "
+            add_hyphen = False
+        else:
+            new_text += "-"
+            add_hyphen = True
     return new_text
