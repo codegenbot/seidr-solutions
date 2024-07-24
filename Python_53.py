5ff05afe2af1033bd04005c0d4a0db1be87cfd69
beep boop <githubbot@vadim.me>
2024-07-24 12:58:18 +0200

    Solution 33 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/8i90rjyu Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/61527979-2277-4767-b031-408073f41 6a3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_53.py
+++ Python_53.py
@@ -1,7 +1,5 @@
-def add(x=None, y=None):
+def add(x=None, y=None): 
     print("Please enter two numbers.")
-    if x is None:
-        x = int(input("Enter first number: "))
-    if y is None:
-        y = int(input("Enter second number: "))
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
     return x + y
