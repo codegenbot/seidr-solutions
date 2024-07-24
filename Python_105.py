4bfac42f6898741f2bdf4cf3894a73a0fbe81bb2
beep boop <githubbot@vadim.me>
2024-07-24 03:45:14 +0200

    Solution 8 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ec410d4-9ce2-4cfe-9352-eeea6da39 b2b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_105.py
+++ Python_105.py
@@ -1,9 +1,3 @@
-name_dict = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 
-             6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
-
 def by_length(arr):
     result = sorted([str(i) for i in arr if 1 <= i <= 9], key=len)
     return [name_dict[int(i)] for i in result]
-
-test_arr = [1, 2, 3, 5, 6, 8]
-print(by_length(test_arr))
