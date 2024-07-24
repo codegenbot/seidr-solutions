f12a01c64a710a61ab7647cc33e9ca94136ef141
beep boop <githubbot@vadim.me>
2024-07-24 03:52:46 +0200

    Solution 96 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/ncmy02p4 Prompt: e7e32ef2-eebe-4602-8b9f-95e41d0825fc.cpp:1:1: error: stray ‘`’ in program     1 | ```c++       | ^ e7e32ef2-eebe-4602-8b9f-95e41d0825fc.cpp:1:2: error: stray ‘`’ in program     1 | ```c++       |  ^ e7e32ef2-eebe-4602-8b9f-95e41d0825fc.cpp:1:3: error: stray ‘`’ in program     1 | ```c++       |   ^ e7e32ef2-eebe-4602-8b9f-95e41d0825fc.cpp:1:4: error: ‘c’ does not name a type     1 | ```c++       |    ^

--- leaders.cpp
+++ leaders.cpp
@@ -2,8 +2,10 @@ vector<int> leaders(vector<int>& v) {
     vector<int> res;
     int max_right = v.back();
     for(int i=v.size()-1; i>=0; --i){
-        if(v[i] >= max_right) max_right = v[i];
-        else res.push_back(max_right);
+        if(v[i]>=max_right){
+            res.push_back(v[i]);
+            max_right = v[i];
+        }
     }
     return res;
 }
