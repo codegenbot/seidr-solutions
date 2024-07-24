6fc9ffbfd9686598a10a7cb2e93c371c9e4e5ad5
beep boop <githubbot@vadim.me>
2024-07-25 01:58:21 +0200

    Solution 18 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/c7fceeas Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7924ce5f-de89-472e-a48f-671ceea79 fe0.py", line 11, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7924ce5f-de89-472e-a48f-671ceea79 fe0.py", line 8, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,7 @@
 def make_palindrome(s: str) -> str:
-    s = s.lower()
     for i in range(len(s)):
-        new_s = s[:i] + s[i] * 2 + s[:i][::-1] + s[i+1:]
-        if new_s == new_s[::-1]:
-            return new_s
+        for char in [s[i].lower(), s[i].upper()]:
+            new_s = s[:i] + char * 2 + s[i:][::-1]
+            if new_s == new_s[::-1]:
+                return new_s
     return ""
