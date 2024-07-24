8161daa9101f4526aa4f524ba70c370cdbd1fec9
beep boop <githubbot@vadim.me>
2024-07-24 13:35:24 +0200

    Solution 10 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/pzxlydrl Prompt:       |             ^~~~~~ 4c8ba6b8-0599-4fa3-aae5-df8161c51b5a.cpp:22:20: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 4c8ba6b8-0599-4fa3-aae5-df8161c51b5a.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4c8ba6b8-0599-4fa3-aae5-df8161c51b5a.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4c8ba6b8-0599-4fa3-aae5-df8161c51b5a.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4c8ba6b8-0599-4fa3-aae5-df8161c51b5a.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4c8ba6b8-0599-4fa3-aae5-df8161c51b5a.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -2,15 +2,21 @@
 #include <initializer_list>
 #include <vector>
 
-bool operator==(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) return false;
+using namespace std;
+
+bool isSame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
 
-std::vector<int> make_a_pile(int n) {
+std::vector<int> makeAPile(int n) {
     std::vector<int> pile;
     int stones = 0;
     for (int i = 1; i <= n; i++) {
