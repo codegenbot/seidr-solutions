74f2d09a0dcfa171e8ef01d099e2abeb68d2ae15
beep boop <githubbot@vadim.me>
2024-07-24 18:22:32 +0200

    Solution 10 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/dhjenigx Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,10 +1,3 @@
-#include <string>
-
-int myStrlen(std::string str){
+int my_strlen(const std::string& str){
     return str.length();
-}
-
-int main() {
-    assert(myStrlen("asdasnakj") == 12); 
-    return 0;
 }
