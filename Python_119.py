5d7d007caaa10669ec68a97bc86ccf197696b953
beep boop <githubbot@vadim.me>
2024-07-24 15:53:07 +0200

    Solution 40 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fdajg309 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/816ece0f-6572-4d51-9a96-dde4b0ce2 3d7.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_119.py
+++ Python_119.py
@@ -1,8 +1,10 @@
-```
 import re
 
+
 def match_parens(lst):
+    count_open = 0
+    count_close = 0
     for s in lst:
-        if len(re.findall('\(', s)) != len(re.findall('\)', s)):
-            return 'No'
-    return 'Yes'
+        count_open += len(re.findall("\(", s))
+        count_close += len(re.findall("\)", s))
+    return "Yes" if count_open == count_close else "No"
