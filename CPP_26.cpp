1b3a62bbd5929779950b09d2fbe7369584a8993e
beep boop <githubbot@vadim.me>
2024-07-24 16:25:22 +0200

    Solution 32 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/hiqe3yie Prompt: in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e81d2144-bca0-4348-bb26-992da9613a8f.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e81d2144-bca0-4348-bb26-992da9613a8f.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e81d2144-bca0-4348-bb26-992da9613a8f.cpp:5: e81d2144-bca0-4348-bb26-992da9613a8f.cpp: In function ‘int main()’: e81d2144-bca0-4348-bb26-992da9613a8f.cpp:15:20: error: ‘remove_duplicates’ was n ot declared in this scope    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ e81d2144-bca0-4348-bb26-992da9613a8f.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,11 +1,12 @@
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int>& a, vector<int>& b){
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
     return a == b;
 }
 
-vector<int> remove_duplicates(vector<int> numbers) {
-    sort(numbers.begin(), numbers.end());
-    numbers.erase(unique(numbers.begin(), numbers.end()), numbers.end());
-    return numbers;
+vector<int> remove_duplicates(const vector<int>& numbers){
+    vector<int> uniqueNumbers = numbers;
+    sort(uniqueNumbers.begin(), uniqueNumbers.end());
+    uniqueNumbers.erase(unique(uniqueNumbers.begin(), uniqueNumbers.end()), uniqueNumbers.end());
+    return uniqueNumbers;
 }
