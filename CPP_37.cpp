9b795633fe112e187e50fd624e2af2e636820c6d
beep boop <githubbot@vadim.me>
2024-07-24 19:11:25 +0200

    Solution 84 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cvfj3xyj Prompt:    27 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 89c18532-0839-4a2c-aac8-725f6effa5be.cpp:27:36: error: expected primary-expressi on before ‘float’    27 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 89c18532-0839-4a2c-aac8-725f6effa5be.cpp:27:43: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 89c18532-0839-4a2c-aac8-725f6effa5be.cpp:26: 89c18532-0839-4a2c-aac8-725f6effa5be.cpp: In function ‘int main()’: 89c18532-0839-4a2c-aac8-725f6effa5be.cpp:36:20: error: ‘sort_even’ was not decla red in this scope    36 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 89c18532-0839-4a2c-aac8-725f6effa5be.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,7 +1,29 @@
+```cpp
 bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (abs(a[i] - b[i]) > 0.0001f) return false;
-    }
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++)
+        if (a[i] != b[i])
+            return false;
     return true;
+}
+
+std::vector<float> sort_even(std::vector<float> vec) {
+    std::vector<float> result;
+    int j = 0;
+    for (int i = 0; i < vec.size(); i++) {
+        if (i % 2 == 0) {
+            while (j < vec.size() && j % 2 != 0) {
+                j++;
+            }
+            for (; j < vec.size() && j % 2 == 0; j++) {
+                result.push_back(vec[j]);
+            }
+        } else {
+            result.push_back(vec[i]);
+            if (i < vec.size() - 1)
+                j++;
+        }
+    }
+    return result;
 }
