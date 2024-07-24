b51515906424a74ae71efb5d46c79a68a1846364
beep boop <githubbot@vadim.me>
2024-07-24 18:56:46 +0200

    Solution 60 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cvfj3xyj Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ 0a9612de-5f78-4aee-9e4e-01c69d02e35f.cpp:25:35:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,70 +1,24 @@
 ```cpp
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-bool checkVectors(const std::vector<float>& a, const std::vector<float>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 std::vector<float> sort_even(std::vector<float> vec) {
     std::vector<float> result;
-    for (int i = 0; i < vec.size(); i++) {
-        if (i % 2 == 0) {
-            auto even = vec.begin();
-            std::advance(even, i);
-            auto end = even;
-            std::advance(end, 1);
-            while (end != vec.end()) {
-                if (*even <= *end) {
-                    result.push_back(*even);
-                    ++even;
-                    ++end;
-                } else {
-                    std::swap(*even, *end);
-                    ++even;
-                    --end;
-                }
-            }
-        } else {
-            result.push_back(vec[i]);
-        }
-    }
-    return result;
-}
+    int i = 0, j = 0;
 
-std::vector<float> sort_even(std::vector<float> vec) {
-    std::vector<float> result;
-    for (int i = 0; i < vec.size(); i++) {
-        if (i % 2 == 0) {
-            float temp;
-            bool flag = true;
-            for (int j = 0; j < vec.size(); j++) {
+    while (i < vec.size()) {
+        if (i % 2 == 0) { // even-indexed elements
+            for (; j < vec.size(); j++) {
                 if (j % 2 == 0) {
-                    if (flag) {
-                        result.push_back(vec[j]);
-                        flag = false;
-                    } else {
-                        if (temp > vec[j]) {
-                            temp = vec[j];
-                        }
-                    }
+                    result.push_back(vec[j]);
                 }
             }
-            result.push_back(temp);
-        } else {
-            result.push_back(vec[i]);
+            std::sort(result.begin(), result.end());
+            while (!result.empty()) {
+                vec[i++] = result.back();
+                result.pop_back();
+            }
+        } else { // odd-indexed elements
+            vec[i++] = vec[j++];
         }
     }
-    return result;
-}
 
-assert(checkVectors(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return vec;
+}
