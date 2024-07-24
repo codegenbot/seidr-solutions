c8ab04d94c8525d392dfee2e2d4439b3c5317e26
beep boop <githubbot@vadim.me>
2024-07-24 20:33:57 +0200

    Solution 68 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/6xzwic9d Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from e1e2049a-d8d5-40d0-9234-9f3108541c2d.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<const char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ e1e2049a-d8d5-40d0-9234-9f3108541c2d.cpp:11:26:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<const char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -2,11 +2,6 @@
 #include <string>
 #include <algorithm>
 
-int main() {
-    std::cout << spinWords("Hello World") << std::endl;
-    return 0;
-}
-
 std::string spinWords(std::string str) {
     std::string result = "";
     std::string word;
@@ -14,14 +9,9 @@ std::string spinWords(std::string str) {
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == ' ') {
             if (word.length() >= 5) {
-                char* revStr = new char[word.length() + 1];
-                strcpy(revStr, word.c_str());
-                for (int j = word.length(); j >= 0; --j) {
-                    result += revStr[j];
-                }
-                delete[] revStr;
-                result += " ";
-                word.clear();
+                std::string reversedWord(word);
+                std::reverse(reversedWord.begin(), reversedWord.end());
+                result += (reversedWord + " ");
             } else {
                 result += (word + " ");
             }
@@ -32,14 +22,11 @@ std::string spinWords(std::string str) {
     }
 
     if (word.length() >= 5) {
-        char* tempRevStr = new char[word.length() + 1];
-        strcpy(tempRevStr, word.c_str());
-        for (int j = word.length(); j >= 0; --j) {
-            result += tempRevStr[j];
-        }
-        delete[] tempRevStr;
+        std::string tempWord = word;
+        std::reverse(tempWord.begin(), tempWord.end());
+        result += (tempWord);
     } else {
-        result += (word);
+        result += word;
     }
 
     return result;
