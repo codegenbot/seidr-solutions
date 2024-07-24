c8f14bdace9164aa2ac16beb041db821164d5955
beep boop <githubbot@vadim.me>
2024-07-24 16:02:40 +0200

    Solution 6 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0hlo4o4l Prompt: Complete the following code given the task description and function signature.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,8 +1,8 @@
 vector<int> filter_integers(list_any values){
     vector<int> result;
     for(auto val : values){
-        if(typeid(int) == val.type()){
-            result.push_back(boost::any_cast<int>(val));
+        if(auto int_val = boost::any_cast<int>(&val)){
+            result.push_back(*int_val);
         }
     }
     return result;
