d120b29da9e6e2d227ea526c2fc13474160ff840
beep boop <githubbot@vadim.me>
2024-07-24 21:31:25 +0200

    Solution 62 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9wzm29ar Prompt: 35 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1df7c1d2-2e6c-4350-a2c2-cd3e3133c 590.py", line 44, in <module>     check(sum_squares)           ^^^^^^^^^^^ NameError: name 'sum_squares' is not defined

--- Python_142.py
+++ Python_142.py
@@ -1,13 +1,5 @@
-```
-def sum_of_odd_squares(lst):
-    total_sum = 0
-    for i in range(len(lst)):
-        if lst[i] % 2 != 0:
-            total_sum += lst[i]**2
-    return total_sum
-
 def check(result):
-    print("The sum of squares is:", result)
+    print("The sum of squares is: ", result)
 
 input_list = [1, 2, 3, 4, 5]
-check(sum_of_odd_squares(input_list))
+print(check(sum_of_odd_squares(input_list)))
