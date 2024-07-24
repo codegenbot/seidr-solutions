a8c77ea9cdad9dfcd300cf21ef353fb89c23bb28
beep boop <githubbot@vadim.me>
2024-07-24 19:02:19 +0200

    Solution 55 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/lnucize6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b3fb7e7-a82b-4b19-b554-efa6972a8 284.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_38.py
+++ Python_38.py
@@ -1,13 +1,16 @@
 def encode_cyclic(s: str, n: int):
     groups = [s[i : i + n] for i in range(0, len(s), n)]
-    groups = [(group[1:] + group[0]) if len(group) == n else group for group in groups]
+    groups = [
+        ("".join([group[-1]] + group[:-1])) if len(group) == n else group
+        for group in groups
+    ]
     return "".join(groups)
 
 
 def decode_cyclic(s: str, n: int):
     groups = [s[i : i + n] for i in range(0, len(s), n)]
     groups = [
-        (groups[-1] + groups[0][: -n + 1]) if len(group) == n else group
+        ("".join([groups[0]] + groups[-1][n - 1 :])) if len(group) == n else group
         for group in groups
     ]
     return "".join(groups)
