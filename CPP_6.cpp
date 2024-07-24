05f66efb7e08f3dd229e9624beccbd455977ddc4
beep boop <githubbot@vadim.me>
2024-07-24 12:10:58 +0200

    Solution 73 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/oe267eq1 Prompt:       |                           ^~~~~~ fa8868d7-4aa2-4114-b377-81547aa4a9bb.cpp:35:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from fa8868d7-4aa2-4114-b377-81547aa4a9bb.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fa8868d7-4aa2-4114-b377-81547aa4a9bb.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fa8868d7-4aa2-4114-b377-81547aa4a9bb.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fa8868d7-4aa2-4114-b377-81547aa4a9bb.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ fa8868d7-4aa2-4114-b377-81547aa4a9bb.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -21,7 +21,12 @@ std::vector<int> parse_nested_parens(std::string s) {
     return result;
 }
 
-int main() {
+int solveProblem() {
     assert(issame(parse_nested_parens("(()(())(()))"), std::vector<int>{4, 2, 1}));
+    return 0;
+}
+
+int main() {
+    solveProblem();
     return 0;
 }
