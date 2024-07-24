d158398f36bc616148a8e5ce236c49f8f3264536
beep boop <githubbot@vadim.me>
2024-07-24 06:34:27 +0200

    Solution 71 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,70 +1,21 @@
 #include <vector>
 using namespace std;
 
-vector<int> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<int> res[2];
-    
-    if (n == 1) {
-        res[0] = v;
-        return res;
-    }
-    
-    for (int i = 0; i < n - 1; i++) {
-        int sumLeft = 0, sumRight = 0;
-        
-        for (int j = 0; j <= i; j++)
-            sumLeft += v[j];
-        for (int j = i + 1; j < n; j++)
-            sumRight += v[j];
-        
-        if (sumLeft == sumRight) {
-            res[0] = vector<int>(v.begin(), v.begin() + i + 1);
-            res[1] = vector<int>(v.begin() + i, v.end());
-            return res;
-        }
-    }
-    
-    int minDiff = INT_MAX, idx = -1;
-    for (int i = 0; i < n - 1; i++) {
-        int sumLeft = 0, sumRight = 0;
-        
-        for (int j = 0; j <= i; j++)
-            sumLeft += v[j];
-        for (int j = i + 1; j < n; j++)
-            sumRight += v[j];
-        
-        int diff = abs(sumLeft - sumRight);
-        if (diff < minDiff) {
-            minDiff = diff;
-            idx = i;
-        }
-    }
-    
-    res[0] = vector<int>(v.begin(), v.begin() + idx + 1);
-    res[1] = vector<int>(v.begin() + idx, v.end());
-    return res;
-}
+vector<int> cutVector(vector<int>& nums) {
+    int minDiff = INT_MAX;
+    int pos = 0;
+    vector<int> left, right;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    
-    for (auto& x : v) {
-        cin >> x;
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs(nums[i] - nums[i-1]);
+        if (diff <= minDiff) {
+            minDiff = diff;
+            pos = i;
+        }
     }
-    
-    auto result = cutVector(v);
-    cout << "[";
-    for (const auto& x : result[0]) {
-        cout << x << " ";
-    }
-    cout << "]\n[";
-    for (const auto& x : result[1]) {
-        cout << x << " ";
-    }
-    cout << "] 0\n";
-    
-    return 0;
+
+    left = vector<int>(nums.begin(), nums.begin() + pos);
+    right = vector<int>(nums.begin() + pos, nums.end());
+
+    return {left, right};
 }
