a7aacae2b386fa1dfe9c3d6cb7379636022349b9
beep boop <githubbot@vadim.me>
2024-07-24 08:29:02 +0200

    Solution 30 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/29lhgvm6 Prompt:       |                    ^~~ fe7b21eb-e397-4934-b857-898f404626a9.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fe7b21eb-e397-4934-b857-898f404626a9.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fe7b21eb-e397-4934-b857-898f404626a9.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fe7b21eb-e397-4934-b857-898f404626a9.cpp:18: fe7b21eb-e397-4934-b857-898f404626a9.cpp: In function ‘int main()’: fe7b21eb-e397-4934-b857-898f404626a9.cpp:28:20: error: ‘strange_sort_list’ was n ot declared in this scope    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ fe7b21eb-e397-4934-b857-898f404626a9.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,17 +1,16 @@
 vector<int> strange_sort_list(vector<int> lst) {
-    int result[10000]; // declaring the size in case you're unsure about input
-    if (lst.empty()) return {};
+    vector<int> result;
+    if (lst.empty()) return result;
 
     sort(lst.begin(), lst.end());
     
     while (!lst.empty()) {
-        result[0] = *lst.begin();
+        result.push_back(*lst.begin());
         lst.erase(lst.begin());
         
         if (!lst.empty())
-            sort(lst.begin(), lst.end());
+            sort(lst.rbegin(), lst.rend());  // removed the line number 19
     }
     
-    vector<int> ans(result, result + sizeof(result)/sizeof(result[0]));
-    return ans;
+    return result;
 }
