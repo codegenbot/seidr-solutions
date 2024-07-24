be2aed0b801ad3ec3f79f602da66f22c942d848c
beep boop <githubbot@vadim.me>
2024-07-24 08:22:56 +0200

    Solution 70 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/1xwtvadf Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,28 +1,42 @@
+int blackPegs(string code, string guess) {
+    int count = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] == guess[i]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int whitePegs(string code, string guess) {
+    int count = 0;
+    map<char, int> codeMap;
+    map<char, int> guessMap;
+    
+    for (int i = 0; i < 4; ++i) {
+        if (code[i] == guess[i]) {
+            codeMap[code[i]]++;
+        } else {
+            codeMap[code[i]]++;
+            guessMap[guess[i]]++;
+        }
+    }
+    
+    for (auto it = guessMap.begin(); it != guessMap.end(); ++it) {
+        if (codeMap.find(it->first) != codeMap.end()) {
+            count += min(it->second, codeMap[it->first]);
+        }
+    }
+    
+    return count - blackPegs(code, guess);
+}
+
 int main() {
     string code, guess;
     cin >> code >> guess;
     
-    int white = 0, black = 0;
-    vector<int> count(6, 0);
-    
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            ++black;
-        } else {
-            count[code[i] - 'A']++;
-            count[guess[i] - 'A']--;
-        }
-    }
-    
-    for (int i = 0; i < 6; ++i) {
-        if (count[i] > 0) {
-            white += count[i];
-        }
-    }
-    
-    white = max(white, 0);
-    
-    cout << white << "\n" << black << "\n";
+    cout << whitePegs(code, guess) << endl;
+    cout << blackPegs(code, guess) << endl;
     
     return 0;
 }
