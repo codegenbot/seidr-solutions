80c63b47b4c47cab4e6b9c7ca0896c09ba0640c0
beep boop <githubbot@vadim.me>
2024-07-24 06:06:32 +0200

    Solution 30 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kewfhjv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26478a77-d6b7-4c9b-a65e-37af7912e 0e3.py", line 12, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26478a77-d6b7-4c9b-a65e-37af7912e 0e3.py", line 9, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,5 +1,5 @@
 def sorted_list_sum(input_data):
-    input_string = ' '.join(map(str, input_data))
-    nums = sorted([num for num in input_string.split() if num.isdigit()])
-    
-    return sum(num for num in nums)
+    input_string = " ".join(map(str, input_data))
+    nums = sorted([num for num in input_string.split() if num.isdigit()], key=len)
+
+    return sum(int(num) for num in nums)
