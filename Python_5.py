b3fbb0e9cd5beb8b881e6d8ed8aaf48987ccaed4
beep boop <githubbot@vadim.me>
2024-07-24 17:23:37 +0200

    Solution 18 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/omao2xnn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9dcac7e9-5545-4f3b-84cb-147cfd5b8 206.py", line 2     for num in numbers: IndentationError: unexpected indent

--- Python_5.py
+++ Python_5.py
@@ -4,9 +4,8 @@ def join_with_delimeter(numbers, delimeter):
         result.extend([num, delimeter])
     return result[:-1]
 
-# Input
+
 numbers = input("Enter numbers separated by space: ").split()
 delimeter = input("Enter the delimiter: ")
 
-# Output
 print(join_with_delimeter(numbers, delimeter))
