240e8df99a75c26f8159a3a32e0b3941f470b36b
beep boop <githubbot@vadim.me>
2024-07-24 12:14:40 +0200

    Solution 25 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/3t3j46u9 Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fa3c39fc-6efb-4238-8f7e-6cd3094a8c9b.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fa3c39fc-6efb-4238-8f7e-6cd3094a8c9b.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fa3c39fc-6efb-4238-8f7e-6cd3094a8c9b.cpp:19: fa3c39fc-6efb-4238-8f7e-6cd3094a8c9b.cpp: In function ‘int main()’: fa3c39fc-6efb-4238-8f7e-6cd3094a8c9b.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ fa3c39fc-6efb-4238-8f7e-6cd3094a8c9b.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,9 +1,42 @@
 bool issame(vector<int> a, vector<int> b) {
-    int sum_a = accumulate(a.begin(), a.end(), 0);
-    int sum_b = accumulate(b.begin(), b.end(), 0);
+    int sum_a = 0, sum_b = 0;
+    for (int num : a) {
+        sum_a += abs(num);
+    }
+    for (int num : b) {
+        sum_b += abs(num);
+    }
     return sum_a == sum_b;
 }
 
-bool order_by_points(const vector<int>& v1, const vector<int>& v2) {
-    return v1 < v2;
+bool order_by_points(vector<int> a, vector<int> b) {
+    int sum_a = 0, sum_b = 0;
+    for (int num : a) {
+        sum_a += abs(num);
+    }
+    for (int num : b) {
+        sum_b += abs(num);
+    }
+    if (sum_a == sum_b) {
+        return a < b;
+    }
+    return sum_a < sum_b;
 }
+
+sort(nums.begin(), nums.end(), [](int a, int b) {
+    int sum_a = 0, sum_b = 0;
+    int temp_a = abs(a), temp_b = abs(b);
+    while (temp_a > 0) {
+        sum_a += temp_a % 10;
+        temp_a /= 10;
+    }
+    while (temp_b > 0) {
+        sum_b += temp_b % 10;
+        temp_b /= 10;
+    }
+    if (sum_a == sum_b) {
+        return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+    }
+    return sum_a < sum_b;
+});
+return nums;
