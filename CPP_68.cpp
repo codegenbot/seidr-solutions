2424ec86ba65baf7dea2037391d9af337022e3c7
beep boop <githubbot@vadim.me>
2024-07-24 14:14:10 +0200

    Solution 60 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/o20kbqqj Prompt:       |                   ^~~~~~~ 8cb0013e-c917-4370-9eac-3455d778e927.cpp:3:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     2 | #include <iostream>   +++ |+#include <climits>     3 | 8cb0013e-c917-4370-9eac-3455d778e927.cpp: At global scope: 8cb0013e-c917-4370-9eac-3455d778e927.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ 8cb0013e-c917-4370-9eac-3455d778e927.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from 8cb0013e-c917-4370-9eac-3455d778e927.cpp:49: 8cb0013e-c917-4370-9eac-3455d778e927.cpp: In function ‘int main()’: 8cb0013e-c917-4370-9eac-3455d778e927.cpp:59:25: error: could not convert ‘pluck( std::vector<int>)()’ from ‘vector<std::pair<int, int>>’ to ‘vector<int>’    59 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ~~~~~^~~~~~~~~~~~~~       |                         |       |                         vector<std::pair<int, int>>

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,7 +1,6 @@
 #include <vector>
 #include <iostream>
 #include <climits>
-#include <limits>
 
 using namespace std;
 
@@ -46,5 +45,3 @@ int main() {
     for (const auto& pair : output) {
         cout << "Pair: (" << pair.first << ", " << pair.second << ")" << endl;
     }
-    return 0;
-}
