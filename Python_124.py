7f491ef05193accf68dd74bc0d4048152878314a
beep boop <githubbot@vadim.me>
2024-07-24 04:21:43 +0200

    Solution 8 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tbflllxq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c22c0cde-6b9b-4bdb-9c13-d37c069e1 904.py", line 21, in <module>     check(valid_date)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c22c0cde-6b9b-4bdb-9c13-d37c069e1 904.py", line 18, in check     assert valid_date("04-2003") == False            ^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c22c0cde-6b9b-4bdb-9c13-d37c069e1 904.py", line 2, in valid_date     day, month, year_str = map(int, date.split('-'))     ^^^^^^^^^^^^^^^^^^^^ ValueError: not enough values to unpack (expected 3, got 2)

--- Python_124.py
+++ Python_124.py
@@ -1,6 +1,8 @@
 def valid_date(date):
-    day, month, year_str = map(int, date.split('-')[0].split())
-    if len(str(year_str)) != 4:
+    day_month_year = map(int, date.split('-'))
+    day, month = day_month_year[0], day_month_year[1]
+    year_str = str(day_month_year[2])
+    if len(year_str) != 4:
         return False
     year = int(year_str)
     if len(str(month)) > 2 or len(str(day)) > 2:
