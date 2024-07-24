c351711e9a7d2bb317bdfc88746f33d49bdbaa4c
beep boop <githubbot@vadim.me>
2024-07-24 14:08:52 +0200

    Solution 21 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/26zn1fkk Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 83ed1d85-3b4c-4ccb-886b-c7beac1e3078.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 83ed1d85-3b4c-4ccb-886b-c7beac1e3078.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 83ed1d85-3b4c-4ccb-886b-c7beac1e3078.cpp:26: 83ed1d85-3b4c-4ccb-886b-c7beac1e3078.cpp: In function ‘int main()’: 83ed1d85-3b4c-4ccb-886b-c7beac1e3078.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 83ed1d85-3b4c-4ccb-886b-c7beac1e3078.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -11,3 +11,28 @@ bool issame(vector<string> a, vector<string> b) {
     }
     return true;
 }
+
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group;
+    int open_braces = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            open_braces++;
+            if (open_braces > 1) {
+                current_group += c;
+            }
+        } else if (c == ')') {
+            open_braces--;
+            if (open_braces > 0) {
+                current_group += c;
+            } else if (open_braces == 0) {
+                result.push_back(current_group);
+                current_group = "";
+            }
+        }
+    }
+
+    return result;
+}
