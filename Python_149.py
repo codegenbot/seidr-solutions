534310fe4a745a9c6f84763aca093cfeb7aa8b8b
beep boop <githubbot@vadim.me>
2024-07-24 16:16:40 +0200

    Solution 56 of Python/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vu8mlkq8 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f211dd2-ec21-447f-8558-6eb98ed79 2b1.py", line 13, in <module>     check(sorted_list_sum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f211dd2-ec21-447f-8558-6eb98ed79 2b1.py", line 10, in check     assert sorted_list_sum(["aaaa", "bbbb", "dd", "cc"]) == ["cc", "dd", "aaaa",  "bbbb"]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^ AssertionError

--- Python_149.py
+++ Python_149.py
@@ -1,7 +1,7 @@
-```Python
+import heapq
 def sorted_list_sum(words=None):
     if words is None:
         lst = [word.strip().lower() for word in input("Enter words separated by spaces: ").split()]
     else:
         lst = [word.strip().lower() for word in words]
-    return sorted([word for word in lst if len(word) % 2 == 0], key=lambda x: (len(x), list(lst).index(x)))
+    return [word for word in sorted((i, word) for i, word in enumerate(lst) if len(word) % 2 == 0), key=lambda x: (x[1], x[0])]
