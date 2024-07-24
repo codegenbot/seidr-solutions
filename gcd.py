2beed40670925fec1ffd0fe8462e671de76828d4
beep boop <githubbot@vadim.me>
2024-07-24 21:05:04 +0200

    Solution 80 of gcd, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/reumo09e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/efad5e32-866d-43f7-8f4a-e6ea1f750 483.py", line 1     ```     ^ SyntaxError: invalid syntax

--- gcd.py
+++ gcd.py
@@ -1,23 +1,22 @@
+```
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return abs(a)
 
-
 def indices_of_substring(text, target):
     result = []
-    pos = 0
-    while True:
-        pos = text.find(target, pos)
+    i = 0
+    while i < len(text):
+        pos = text.find(target)
         if pos == -1:
             break
         result.append(pos + 1)
-        pos += len(target)
+        i += pos + 1
     return result
 
-
-print(gcd(1, 1))
-print(gcd(4, 400000))
-print(gcd(54, 24))
-print(gcd(4200, 3528))
+print(gcd(1, 1))  
+print(gcd(4, 400000))  
+print(gcd(54, 24))  
+print(gcd(4200, 3528))  
 print(gcd(820000, 63550))
