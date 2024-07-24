65777a081d4ce60e0e91ae0bc6b36a53ba5f0d50
beep boop <githubbot@vadim.me>
2024-07-24 07:35:36 +0200

    Solution 93 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vixjqopt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4c5fb43d-a880-48f8-8310-7e9086fb0 088.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,1 @@
-```
-def double_the_difference(lst):
-    if len(lst) < 2:
-        return 0
-    avg = sum(lst) / len(lst)
-    diff = abs(max(lst) - min(lst)) - abs(avg - min(lst))
-    return 2 * diff
-
-def check(func, lst):
-    return func(lst)
-
 print(check(double_the_difference, [1, 5, 3, 19, 18, 25]))
