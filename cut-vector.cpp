1df97a84b15a874b9879340c617fbd56926a2869
beep boop <githubbot@vadim.me>
2024-07-24 06:28:14 +0200

    Solution 59 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,34 +1,51 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int>& nums) {
+vector<int> cutVector(vector<int>& nums) {
     int n = nums.size();
     int minDiff = INT_MAX;
-    int splitIndex = -1;
+    int cutIndex = 0;
     
-    for (int i = 0; i < n; ++i) {
-        int sumLeft = 0, sumRight = 0;
-        for (int j = 0; j <= i; ++j) {
-            sumLeft += nums[j];
-        }
-        for (int j = i + 1; j < n; ++j) {
-            sumRight += nums[j];
-        }
-        
-        int diff = abs(sumLeft - sumRight);
+    for (int i = 1; i < n; ++i) {
+        int diff = abs(nums[i] - nums[0]);
         if (diff < minDiff) {
             minDiff = diff;
-            splitIndex = i;
+            cutIndex = i;
         }
     }
     
-    vector<vector<int>> result(2);
-    for (int i = 0; i <= splitIndex; ++i) {
-        result[0].push_back(nums[i]);
+    return {nums.begin(), nums.begin() + cutIndex} | {nums.begin() + cutIndex, nums.end()};
+}
+
+int main() {
+    // Example inputs
+    vector<int> nums1 = {1};
+    vector<int> result1 = cutVector(nums1);
+    cout << "Input: ";
+    for (int num : nums1) {
+        cout << num << " ";
     }
-    for (int i = splitIndex + 1; i < n; ++i) {
-        result[1].push_back(nums[i]);
+    cout << endl;
+    cout << "Output: ";
+    for (int num : result1) {
+        cout << num << " ";
     }
+    cout << endl;
+
+    vector<int> nums2 = {1, 10};
+    vector<int> result2 = cutVector(nums2);
+    cout << "Input: ";
+    for (int num : nums2) {
+        cout << num << " ";
+    }
+    cout << endl;
+    cout << "Output: ";
+    for (int num : result2) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // Add more inputs here...
     
-    return result;
+    return 0;
 }
