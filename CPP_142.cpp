9cc4bee60fbffde1915efa9f1cdf4a7f344d77f1
beep boop <githubbot@vadim.me>
2024-07-24 14:56:22 +0200

    Solution 13 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cs4nkzhf Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from a3cb2652-642c-4526-badf-a95bdb4292ea.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#define _cplusplus
 using namespace std;
 
-int sum_squares(vector<int> lst) {
+int sum_squares(std::initializer_list<int> lst) {
     int total = 0;
-    for (int i = 0; i < lst.size(); i++) {
+    for (int i = 0, n = lst.size(); i < n; i++) {
         if ((i + 1) % 3 == 0 && (i + 1) % 4 != 0)
-            total += pow(lst[i], 2);
+            total += pow(lst.begin()[i], 2);
         else if ((i + 1) % 4 == 0 && (i + 1) % 3 != 0)
-            total += pow(lst[i], 3);
+            total += pow(lst.begin()[i], 3);
     }
     return total;
 
@@ -18,4 +19,4 @@ int main() {
     int result = sum_squares({1, 2, 3, 4, 5}); 
     cout << "The sum of squares is: " << result << endl;
     return 0;
-};
+}
