361dc722bdc8dd624915242bdc4c3f7d804d7ad8
beep boop <githubbot@vadim.me>
2024-07-25 01:19:30 +0200

    Solution 62 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/avztuzwn Prompt: d61026fe-d895-4237-9f7c-d78a0d35ec1d.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ d61026fe-d895-4237-9f7c-d78a0d35ec1d.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main() {       |     ^~~~ In file included from d61026fe-d895-4237-9f7c-d78a0d35ec1d.cpp:21: d61026fe-d895-4237-9f7c-d78a0d35ec1d.cpp: In function ‘int main()’: d61026fe-d895-4237-9f7c-d78a0d35ec1d.cpp:31:20: error: ‘unique’ was not declared  in this scope    31 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -13,7 +13,7 @@ int main() {
     vector<int> nums = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     set<int> num_set(nums.begin(), nums.end());
     
-    assert(issame(num_set, set<int>({0, 2, 3, 5, 9, 123})));
+    assert(issame(num_set, set<int>({0, 2, 3, 5, 9, 123}));
     
     return 0;
 }
