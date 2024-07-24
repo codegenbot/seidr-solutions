626761a1cf27b43a712dc5269b9238f1615818af
beep boop <githubbot@vadim.me>
2024-07-24 07:48:57 +0200

    Solution 31 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bwy1niy3 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8b27cdc4-2ab1-49e5-8a4a-f22d4ee211f1.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8b27cdc4-2ab1-49e5-8a4a-f22d4ee211f1.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8b27cdc4-2ab1-49e5-8a4a-f22d4ee211f1.cpp:10: 8b27cdc4-2ab1-49e5-8a4a-f22d4ee211f1.cpp: In function ‘int main()’: 8b27cdc4-2ab1-49e5-8a4a-f22d4ee211f1.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 8b27cdc4-2ab1-49e5-8a4a-f22d4ee211f1.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,3 +1,4 @@
+```c++
 #include <vector>
 #include <string>
 
@@ -13,14 +14,13 @@ std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std:
 bool checkEqual(std::vector<std::string> a, std::vector<std::string> b){
     if(a.size() != b.size())
         return false;
-    for(int i = 0; i < a.size(); i++) {
+    for(int i = 0; i < a.size(); i++)
         if(a[i] != b[i])
             return false;
-    }
     return true;
 }
 
 int main() {
-    assert(checkEqual(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAZA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}) );
+    assert(checkEqual(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAZA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
     return 0;
 }
