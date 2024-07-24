290cca0866d114d5374e4caab73f801f298b6b4e
beep boop <githubbot@vadim.me>
2024-07-24 11:22:46 +0200

    Solution 95 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/cfsyn1qm Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,84 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
+pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
     int minDiff = INT_MAX;
-    int cutIndex = -1;
+    int splitIndex = 0;
     
-    for (int i = 0; i < n; ++i) {
-        if ((v[i] == v[0]) || (abs(v[i] - v[0]) <= minDiff)) {
-            minDiff = abs(v[i] - v[0]);
-            cutIndex = i;
+    for (int i = 1; i < nums.size(); i++) {
+        int diff = abs((long long)nums[i] - (long long)nums[0]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            splitIndex = i;
         }
     }
     
-    vector<int> left(v.begin(), v.begin() + cutIndex);
-    vector<int> right(v.begin() + cutIndex, v.end());
-    
-    return {left, right};
+    return {vector<int>(nums.begin(), nums.begin() + splitIndex), vector<int>(nums.begin() + splitIndex, nums.end())};
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (auto &x : v) {
-        cin >> x;
+    // Test cases
+    vector<int> test1 = {1};
+    vector<int> test2 = {1, 10};
+    vector<int> test3 = {1, 100};
+    vector<int> test4 = {1, 1000};
+    vector<int> test5 = {1, 10000};
+
+    pair<vector<int>, vector<int>> result1 = cutVector(test1);
+    pair<vector<int>, vector<int>> result2 = cutVector(test2);
+    pair<vector<int>, vector<int>> result3 = cutVector(test3);
+    pair<vector<int>, vector<int>> result4 = cutVector(test4);
+    pair<vector<int>, vector<int>> result5 = cutVector(test5);
+
+    cout << "Test1: ";
+    for (int num : result1.first) {
+        cout << num << " ";
     }
-    pair<vector<int>, vector<int>> result = cutVector(v);
-    cout << "0\n";
-    for (const auto &x : result.first) {
-        cout << x << "\n";
+    cout << endl;
+    for (int num : result1.second) {
+        cout << num << " ";
     }
+    cout << endl;
+
+    cout << "Test2: ";
+    for (int num : result2.first) {
+        cout << num << " ";
+    }
+    cout << endl;
+    for (int num : result2.second) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Test3: ";
+    for (int num : result3.first) {
+        cout << num << " ";
+    }
+    cout << endl;
+    for (int num : result3.second) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Test4: ";
+    for (int num : result4.first) {
+        cout << num << " ";
+    }
+    cout << endl;
+    for (int num : result4.second) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Test5: ";
+    for (int num : result5.first) {
+        cout << num << " ";
+    }
+    cout << endl;
+    for (int num : result5.second) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
