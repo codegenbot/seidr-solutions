72ba980aacd3aed0e405355288849b939c473697
beep boop <githubbot@vadim.me>
2024-07-24 13:33:03 +0200

    Solution 82 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/qb3ffmkz Prompt:       |                           ^~~~~~ a7601312-2b01-450e-83b8-e73b069591b1.cpp:43:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from a7601312-2b01-450e-83b8-e73b069591b1.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a7601312-2b01-450e-83b8-e73b069591b1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a7601312-2b01-450e-83b8-e73b069591b1.cpp:43:34: error: expected primary-expressi on before ‘int’    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a7601312-2b01-450e-83b8-e73b069591b1.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ a7601312-2b01-450e-83b8-e73b069591b1.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -23,16 +23,18 @@ std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
             if (numCount[guess[i]]-- > 0) cows++;
         }
     }
-    
+
     result.push_back(bulls);
     result.push_back(cows);
 
     return result;
 }
 
-std::vector<int> test1 = {1, 2, 3};
-std::vector<int> test2 = {1, 4, 3};
-std::vector<int> result = compare(test1, test2);
-for (int i : result) {
-    std::cout << i << " ";
+int main() {
+    std::vector<int> test1 = {1, 2, 3};
+    std::vector<int> test2 = {1, 4, 3};
+    std::vector<int> result = compare(test1, test2);
+    for (int i : result) {
+        std::cout << i << " ";
+    }
 }
