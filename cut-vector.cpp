139ee77111bb3f28eecddf6808d0b48df27c2a6d
beep boop <githubbot@vadim.me>
2024-07-24 02:25:44 +0200

    Solution 36 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/dk4hok2d Prompt: 614824af-4972-4b8e-a351-c6c2bbc3a5db.cpp: In function ‘std::vector<std::vector<i nt> > cutVector(std::vector<int>&)’: 614824af-4972-4b8e-a351-c6c2bbc3a5db.cpp:8:20: error: ‘INT_MAX’ was not declared  in this scope     8 |     int min_diff = INT_MAX;       |                    ^~~~~~~ 614824af-4972-4b8e-a351-c6c2bbc3a5db.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; 614824af-4972-4b8e-a351-c6c2bbc3a5db.cpp:12:13: error: ‘abs’ was not declared in  this scope    12 |         if (abs(diff1-diff2) < abs(min_diff)) {       |             ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,33 +1,26 @@
-#include <iostream>
 #include <vector>
+#include <climits>
+#include <cmath>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
-    int n = vec.size();
-    pair<int, int> diff_pair;
+vector<vector<int>> cutVector(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> result;
+    
     int min_diff = INT_MAX;
     for (int i = 1; i < n; i++) {
-        int diff = abs(vec[i] - vec[i-1]);
-        if (diff <= min_diff) {
-            min_diff = diff;
-            diff_pair.first = i;
-            diff_pair.second = i;
+        int diff1 = nums[i] - nums[0];
+        int diff2 = nums[n-1] - nums[i-1];
+        if (abs(diff1-diff2) < abs(min_diff)) {
+            min_diff = diff1 - diff2;
+            result.push_back(vector<int>(nums.begin(), nums.begin() + i));
+            nums.erase(nums.begin());
         }
     }
-    return {{vec.begin(), vec.begin() + diff_pair.first}}, {vec.begin() + diff_pair.first, vec.end()}};
-}
-
-int main() {
-    vector<int> vec;
-    int num;
-    cin >> num;
-    while(num--) {
-        cin >> num;
-        vec.push_back(num);
+    
+    if (!nums.empty()) {
+        result.push_back(vector<int>(nums.begin(), nums.end()));
     }
-    pair<vector<int>, vector<int>> res = cutVector(vec);
-    for (auto x : res.first) cout << x << " ";
-    cout << endl;
-    for (auto x : res.second) cout << x << " ";
-    return 0;
+
+    return result;
 }
