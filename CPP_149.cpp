d6822ed1bdcaffb18230f92a6f354d14d9272fdd
beep boop <githubbot@vadim.me>
2024-07-24 12:31:57 +0200

    Solution 19 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9xcaayog Prompt:    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6efa29e5-8d87-4c3b-8222-99561384498b.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6efa29e5-8d87-4c3b-8222-99561384498b.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6efa29e5-8d87-4c3b-8222-99561384498b.cpp:12: 6efa29e5-8d87-4c3b-8222-99561384498b.cpp: In function ‘int main()’: 6efa29e5-8d87-4c3b-8222-99561384498b.cpp:22:20: error: ‘sorted_list_sum’ was not  declared in this scope    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 6efa29e5-8d87-4c3b-8222-99561384498b.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -11,8 +11,8 @@ bool issame(vector<string> a, vector<string> b){
 
 vector<string> sorted_list_sum(vector<string> lst){
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2 != 0; }), lst.end());
-    sort(lst.begin(), lst.end(), [](const string& a, const string& b){
-        if(a.length() == b.length()){
+    sort(lst.begin(), lst.end(), [] (const string& a, const string& b) {
+        if (a.length() == b.length()) {
             return a < b;
         }
         return a.length() < b.length();
