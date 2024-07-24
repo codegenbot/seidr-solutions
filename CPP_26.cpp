2f89159b3dd87368a79ea0e08448fb874cc9366f
beep boop <githubbot@vadim.me>
2024-07-24 18:51:00 +0200

    Solution 46 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/to49ik7c Prompt:       |                           ^~~~~~ c190e9c2-aecf-4780-b3ee-50aec9945ff1.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c190e9c2-aecf-4780-b3ee-50aec9945ff1.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c190e9c2-aecf-4780-b3ee-50aec9945ff1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c190e9c2-aecf-4780-b3ee-50aec9945ff1.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c190e9c2-aecf-4780-b3ee-50aec9945ff1.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ c190e9c2-aecf-4780-b3ee-50aec9945ff1.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <set>
+#include <cassert>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return (a == b);
@@ -7,10 +8,10 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 
 std::vector<int> remove_duplicates(std::vector<int> numbers){
     std::set<int> unique_numbers(numbers.begin(), numbers.end());
-    return std::vector(unique_numbers.begin(), unique_numbers.end());
+    return std::vector<int>(unique_numbers.begin(), unique_numbers.end());
 }
 
 int main() {
-    assert((issame({1, 2, 3, 2, 4, 3, 5}, {1, 4, 5})));
+    assert((issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5})));
     return 0;
 }
