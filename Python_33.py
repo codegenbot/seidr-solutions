78db5d1d2b09a528f95341b4b142ad1136d520b4
beep boop <githubbot@vadim.me>
2024-07-24 18:06:38 +0200

    Solution 6 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qx5ldngd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1acbf64b-d88c-4af3-8326-50d42a24f a7f.py", line 16, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1acbf64b-d88c-4af3-8326-50d42a24f a7f.py", line 11, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,16 @@
+```
 def sort_third(l: list):
-    third_element_list = [x[2] for x in l]
-    return sorted([x for x in l if x[2] == third_element_list.sort()[0]])
+    result = [0] * len(l)
+    index = 0
+    for i in range(len(l)):
+        if i % 3 == 0:
+            result[index], (result[index+1]) = l[i], sorted([x for x in l if x != l[i]])[0]
+            index += 2
+        elif i % 3 == 1:
+            result[index] = l[i]
+            index += 1
+        else:
+            result[index] = l[i]
+            index += 1
+
+    return result
