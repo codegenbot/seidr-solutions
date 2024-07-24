6468aecc8230d864744de236f10b0472579c818c
beep boop <githubbot@vadim.me>
2024-07-24 03:55:38 +0200

    Solution 31 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ckyfqnk4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ccb8d90-6b0b-4ec8-a135-178bc2630 36a.py", line 23, in <module>     check(solve)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ccb8d90-6b0b-4ec8-a135-178bc2630 36a.py", line 20, in check     assert solve(963) == "10010", "Error"            ^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_84.py
+++ Python_84.py
@@ -1,15 +1,16 @@
 import math
 
+
 def solve(K):
     if K < 1:
-        return '0'
-    if math.log2(K) % 1 == 0: 
-        return str(bin(int(math.pow(2, math.ceil(math.log2(K))))-1)[2:])
-    binary = ''
-    while K > 0:
+        return str(0)
+    if math.log2(K) % 1 == 0:
+        return bin(int(math.pow(2, math.ceil(math.log2(K)))) - 1)[2:]
+    binary = "1"
+    while K > 1:
         binary = str(K % 2) + binary
         K //= 2
-    if int(binary, 2) != K: 
-        return str(int(binary, 2)) 
+    if int(binary, 2) != K:
+        return str(int(binary, 2))
     else:
         return binary
