e688ca2da4844e91c8971d53e6701372e3f5f0c9
beep boop <githubbot@vadim.me>
2024-07-24 18:07:21 +0200

    Solution 85 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4ya1rfao Prompt: 4724604d-d5d5-44ec-86cb-7e7c20647d47.cpp:1:10: fatal error: \/usr\/include\/boos t\/uuids\/uuid.hpp: No such file or directory     1 | #include <\/usr\/include\/boost\/uuids\/uuid.hpp>       |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,9 +1,1 @@
-#include <boost/uuids/uuid.hpp>
-#include <boost/uuids/uuid_generators.hpp>
-#include <boost/uuids/uuid_io.hpp>
-
-int main() {
-    boost::uuids::uuid uuid = boost::uuids::random_generator()();
-    std::cout << uuid << std::endl;
-    return 0;
-}
+#include <\\usr\\include\\boost\\uuids\\uuid.hpp>
