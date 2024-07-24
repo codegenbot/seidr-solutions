cca69b0ae9db710d5b1bdbd4d2d0b33984aecf2d
beep boop <githubbot@vadim.me>
2024-07-24 03:37:24 +0200

    Solution 2 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/bw99tny2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/baa69486-4e8c-449f-9218-0f1cdebd1 c4b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_105.py
+++ Python_105.py
@@ -1,3 +1,6 @@
 def by_length(arr):
-    result = sorted([str(i) for i in arr if 1 <= i <= 9], key=len)
+    result = sorted([str(i) for i in arr if 1 <= i <= 9])
+    result.reverse()
     return [name_dict[int(i)] for i in result]
+
+name_dict = {"1": "One", "2": "Two", "3": "Three", "4": "Four", "5": "Five", "6": "Six", "7": "Seven", "8": "Eight", "9": "Nine"}
