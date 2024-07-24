f7833cfc35389190d3d47df511f1bda6ec9a9af7
beep boop <githubbot@vadim.me>
2024-07-24 12:15:41 +0200

    Solution 32 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/ef3cxxxn Prompt: 0dc8230f-9feb-4524-998b-aa31d3fff7f0.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string s) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,23 +1,17 @@
-#include <string>
-using namespace std;
-
-bool solveBoolean(string s) {
+bool solveBoolean(std::string s) {
     bool res = true;
     for (int i = 0; i < s.size(); ++i) {
         if (s[i] == 'T') {
             return true;
         }
-        else if (s[i] == 'F') {
+        if (s[i] == 'F') {
             return false;
         }
-        else if (s[i] == '&') {
-            while(i + 1 < s.size() && s[i+1] != '&') i++;
-            while(i + 1 < s.size() && s[i+1] != '|') i++;
+        if (s[i] == '|') {
+            res = true;
         }
-        else if (s[i] == '|') {
-            while(i + 1 < s.size() && s[i+1] != '|') i++;
+        if (s[i] == '&') {
             res = false;
         }
     }
     return res;
-}
