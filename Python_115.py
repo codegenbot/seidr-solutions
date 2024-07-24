e0ee01ffea48e9a227b4820cc8189b96a5959dd1
beep boop <githubbot@vadim.me>
2024-07-24 05:32:13 +0200

    Solution 72 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zgcn7j6z Prompt: 6 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/014b984f-6048-4011-95a6-e75113b6d acc.py", line 24, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/014b984f-6048-4011-95a6-e75113b6d acc.py", line 21, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: max_fill() takes 1 positional argument but 2 were given

--- Python_115.py
+++ Python_115.py
@@ -1,5 +1,3 @@
-```
-print(max_fill([[1, 1, 1, 1], [1, 1, 1, 1]]))
 def max_fill(grid):
     total_water = 0
     left = 0
@@ -15,3 +13,5 @@ def max_fill(grid):
             else:
                 right -= 1
     return total_water
+
+print(max_fill([[1, 1, 1, 1], [1, 1, 1, 1]]))
