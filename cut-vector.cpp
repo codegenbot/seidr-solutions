f272d6a1247931da9f9ddad0586478ade2472b3d
beep boop <githubbot@vadim.me>
2024-07-24 06:09:54 +0200

    Solution 25 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/fpqby96g Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,67 +1,36 @@
 #include <vector>
 using namespace std;
 
-vector<vector<int>> cutVector(vector<int> v) {
-    int n = v.size();
-    vector<vector<int>> res(2);
-    
-    for (int i = 0; i < n; ++i) {
-        if (i == 0 || v[i] != v[0]) {
-            int leftSum = 0, rightSum = 0;
-            int leftCount = 0, rightCount = 0;
-            
-            for (int j = 0; j <= i; ++j) {
-                leftSum += v[j];
-                leftCount++;
-            }
-            
-            for (int j = i + 1; j < n; ++j) {
-                rightSum += v[j];
-                rightCount++;
-            }
-            
-            if (leftCount == rightCount && leftSum == rightSum) {
-                res[0] = vector<int>(v.begin(), v.begin() + i);
-                res[1] = vector<int>(v.begin() + i, v.end());
-                return res;
-            } else if (abs(leftSum - rightSum) < abs(leftSum - (n - 1 - rightCount)*v[0])) {
-                res[0] = vector<int>(v.begin(), v.begin() + i);
-                res[1] = vector<int>(v.begin() + i, v.end());
-                return res;
-            }
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int minDiff = INT_MAX;
+    int splitIndex = 0;
+    for (int i = 1; i < v.size(); i++) {
+        int diff = abs(v[i] - v[0]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            splitIndex = i;
         }
     }
-    
-    res[0] = v;
-    res[1].clear();
-    return res;
+    return {vector<int>(v.begin(), v.begin() + splitIndex), vector<int>(v.begin() + splitIndex, v.end())};
 }
 
 int main() {
     int n;
     cin >> n;
     vector<int> v(n);
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; i++) {
         cin >> v[i];
     }
-    
-    vector<vector<int>> res = cutVector(v);
-    
-    cout << "[";
-    for (int i = 0; i < res[0].size(); ++i) {
-        cout << res[0][i];
-        if (i < res[0].size() - 1)
-            cout << " ";
+    pair<vector<int>, vector<int>> result = cutVector(v);
+    cout << "First subvector: ";
+    for (int num : result.first) {
+        cout << num << " ";
     }
-    cout << "]\n";
-    
-    cout << "[";
-    for (int i = 0; i < res[1].size(); ++i) {
-        cout << res[1][i];
-        if (i < res[1].size() - 1)
-            cout << " ";
+    cout << endl;
+    cout << "Second subvector: ";
+    for (int num : result.second) {
+        cout << num << " ";
     }
-    cout << "]\n";
-    
+    cout << endl;
     return 0;
 }
