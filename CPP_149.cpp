11f2b9e3f84087056692252b753f601fc120b2b2
beep boop <githubbot@vadim.me>
2024-07-24 15:29:31 +0200

    Solution 16 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/cmheh2hi Prompt: &)> >]’ /usr/include/c++/11/bits/stl_algo.h:1954:25:   required from ‘void std::__sort(_ RandomAccessIterator, _RandomAccessIterator, _Compare) [with _RandomAccessIterat or = __gnu_cxx::__normal_iterator<std::__cxx11::basic_string<char>*, std::vector <std::__cxx11::basic_string<char> > >; _Compare = __gnu_cxx::__ops::_Iter_comp_i ter<sorted_list_sum(const std::vector<std::vector<std::__cxx11::basic_string<cha r> > >&)::<lambda(const string&, const string&)> >]’ /usr/include/c++/11/bits/stl_algo.h:4875:18:   required from ‘void std::sort(_RA Iter, _RAIter, _Compare) [with _RAIter = __gnu_cxx::__normal_iterator<std::__cxx 11::basic_string<char>*, std::vector<std::__cxx11::basic_string<char> > >; _Comp are = sorted_list_sum(const std::vector<std::vector<std::__cxx11::basic_string<c har> > >&)::<lambda(const string&, const string&)>]’ 37d5dcd2-4f86-4eb6-ad37-a2938ea6fc35.cpp:33:14:   required from here /usr/include/c++/11/bits/stl_heap.h:257:9: error: no type named ‘value_type’ in ‘struct std::iterator_traits<__gnu_cxx::__normal_iterator<std::__cxx11::basic_st ring<char>*, std::vector<std::__cxx11::basic_string<char> > > >’   257 |         _ValueType;       |         ^~~~~~~~~~ /usr/include/c++/11/bits/stl_heap.h:259:9: error: no type named ‘difference_type ’ in ‘struct std::iterator_traits<__gnu_cxx::__normal_iterator<std::__cxx11::bas ic_string<char>*, std::vector<std::__cxx11::basic_string<char> > > >’   259 |         _DistanceType;       |         ^~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -16,7 +16,7 @@ bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b
 }
 
 std::vector<std::string> sorted_list_sum(const std::vector<std::vector<std::string>>& lst) {
-    std::vector<int> sums;
+    std::vector<std::string> result;
     for (const auto& str : lst[0]) {
         int sum = 0;
         for (int i = 1; i < lst.size(); i++) {
@@ -25,20 +25,13 @@ std::vector<std::string> sorted_list_sum(const std::vector<std::vector<std::stri
                 break;
             }
         }
-        sums.push_back(sum);
-    }
-    
-    std::vector<std::string> result;
-    for (int sum : std::vector<int>(std::sort(sums.begin(), sums.end()))) {
-        for (const auto& str : lst[0]) {
-            if (stoi(str) == sum) {
-                result.push_back(str);
-                break;
-            }
+        if (sum > 0) {
+            result.push_back(str);
         }
     }
-    
-    return result;
+    std::vector<std::string> temp(result.begin(), result.end());
+    std::sort(temp.begin(), temp.end());
+    return std::vector<std::string>(temp.begin(), temp.end());
 }
 
 int main() {
