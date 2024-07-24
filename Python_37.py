d6663716701f64151d439851297a2c51d3dba403
beep boop <githubbot@vadim.me>
2024-07-24 02:01:34 +0200

    Solution 63 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a1511cb-c951-4ce9-8500-e25d4c47f 31c.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1a1511cb-c951-4ce9-8500-e25d4c47f 31c.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,6 @@
+```
 def sort_even(l: list):
     evens = sorted([x for x in l if 0 <= x % 2])
-    odds = sorted([x for x in l if x % 2 != 0], reverse=True)
-    result = evens + odds
+    odds = sorted([x for x in l if abs(x) % 2 != 0], reverse=True)
+    result = [x for x in l if 0 <= x % 2] + sorted([x for x in l if abs(x) % 2 != 0 and x < 0], reverse=True)
     return tuple(result)
