d6ca45a9f09666443af125ef88611bfeabefc448
beep boop <githubbot@vadim.me>
2024-07-24 05:41:53 +0200

    Solution 39 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51a36d97-904f-4f02-8e5f-65d1a91cb b67.py", line 25, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51a36d97-904f-4f02-8e5f-65d1a91cb b67.py", line 21, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,17 +1,17 @@
-```
 def fix_spaces(text):
     new_text = ""
     add_hyphen = False
-    for char in text.strip():
+    for char in text:
         if char.isalnum() or char in ["-", "_"]:
             new_text += char
             add_hyphen = False
-        elif char == " ":
+        elif char.isspace():
             if not add_hyphen:
                 new_text += "-"
                 add_hyphen = True
             else:
                 new_text += " "
+                add_hyphen = False
         else:
             new_text += char
     return new_text
