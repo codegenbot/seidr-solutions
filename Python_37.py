44da403d2fd025240f66f44b8b0b4889edf8d6dc
beep boop <githubbot@vadim.me>
2024-07-24 02:20:40 +0200

    Solution 87 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/l8aaa03l Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25c048ac-ff9a-45c0-84b3-9cdedeb3b c64.py", line 11, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/25c048ac-ff9a-45c0-84b3-9cdedeb3b c64.py", line 6, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,3 +1,4 @@
 def sort_even(l: list):
-    all_numbers = sorted([x for x in l], key=abs)
-    return tuple([num for num in all_numbers if 0 <= num % 2]), tuple([num for num in all_numbers if 0 > num % 2])
+    even_numbers = sorted([x for x in l if 0 <= x % 2], key=abs)
+    odd_numbers = sorted([x for x in l if 0 > x % 2], key=abs)
+    return even_numbers + odd_numbers
