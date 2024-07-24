1568e506ea621d882177134411ce7703d4b6bbf9
beep boop <githubbot@vadim.me>
2024-07-24 17:23:56 +0200

    Solution 72 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/7kk15bku Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3a5de05-fe8d-461b-9493-c25d5408b 858.py", line 1     Here is the modified code:                 ^^^^^^^^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,13 +1,2 @@
 ```
-import re
-
-def fix_spaces(text):
-    if not text:
-        return ""
-
-    text = text.replace("\n", " ")
-
-    text = text.strip().replace(" ", "_").replace('"', '').replace("'", '')
-    while "__" in text:
-        text = text.replace("__", "_-")
-    return text
+SyntaxError!
