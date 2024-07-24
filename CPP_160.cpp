721b0fc3153710eb9039092dbc4952ef7c8f6549
beep boop <githubbot@vadim.me>
2024-07-24 15:40:52 +0200

    Solution 1 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rec3ih7w Prompt:       |                       ^~~~~~ 17ddf91d-6915-412c-af3c-187476748c69.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                               ^~~~~~~~~ 17ddf91d-6915-412c-af3c-187476748c69.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                          ^~~~~~ 17ddf91d-6915-412c-af3c-187476748c69.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                 ^~~ 17ddf91d-6915-412c-af3c-187476748c69.cpp:1:61: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operand) {       |                                                             ^ In file included from 17ddf91d-6915-412c-af3c-187476748c69.cpp:19: 17ddf91d-6915-412c-af3c-187476748c69.cpp: In function ‘int main()’: 17ddf91d-6915-412c-af3c-187476748c69.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,16 +1,19 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
+#include <vector>
+#include <cmath>
+
+int doAlgebra(std::vector<std::string> operators, std::vector<int> operands) {
     int result = 0;
-    for (int i = 1; i < operand.size(); i++) {
-        if (*operator_.begin() == "+") {
-            result += operand[i];
-        } else if (*operator_.begin() == "-") {
-            result -= operand[i];
-        } else if (*operator_.begin() == "*") {
-            result *= operand[i];
-        } else if (*operator_.begin() == "//") {
-            result = result / operand[i];
-        } else if (*operator_.begin() == "**") {
-            result = pow(result, operand[i]);
+    for (int i = 1; i < operands.size(); i++) {
+        if (*operators.begin() == "+") {
+            result += operands[i];
+        } else if (*operators.begin() == "-") {
+            result -= operands[i];
+        } else if (*operators.begin() == "*") {
+            result *= operands[i];
+        } else if (*operators.begin() == "//") {
+            result = result / operands[i];
+        } else if (*operators.begin() == "**") {
+            result = pow(result, operands[i]);
         }
     }
     return result;
