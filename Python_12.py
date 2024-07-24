0117263800e133b05aa961b50afcaaa7e6a52653
beep boop <githubbot@vadim.me>
2024-07-24 10:27:12 +0200

    Solution 36 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/65b9ihed Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1d731f4-a7ef-4a29-8ce5-1d57d122c d2a.py", line 14, in <module>     check(longest)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1d731f4-a7ef-4a29-8ce5-1d57d122c d2a.py", line 11, in check     assert longest(["x", "yyy", "zzzz", "www", "kkkk", "abc"]) == "zzzz"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: longest() takes 0 positional arguments but 1 was given

--- Python_12.py
+++ Python_12.py
@@ -1,9 +1,9 @@
 ```
-def longest(*strings):
+def longest(strings = []):
     while True:
         s = input("Enter a string (or 'stop' to finish): ")
         if s.lower() == 'stop':
             break
-        strings += (s,)
+        strings.append(s)
     result = max(strings, key=len) if strings else None
     print(result)
