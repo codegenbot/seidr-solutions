9366cc6ae105e97f7452e1d7e142644c5212ff8f
beep boop <githubbot@vadim.me>
2024-07-24 18:53:03 +0200

    Solution 11 of CPP/49, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/if5uxxy8 Prompt: Program failed

--- CPP_49.cpp
+++ CPP_49.cpp
@@ -1,7 +1,7 @@
 int modp(int n, int p) {
     if (n < 0) return modp(-n, p);
     long long res = 1;
-    for (; n; n >>= 1) {
+    for (; n; n >>= 1LL) { // <--- Cast n to long long here
         if (n & 1) res = (long long)res * p % p;
         p = (long long)p * p % p;
     }
