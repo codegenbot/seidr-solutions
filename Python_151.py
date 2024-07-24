f701ec92f586a7019a2e202a95f82e54b7aa9221
beep boop <githubbot@vadim.me>
2024-07-24 17:18:07 +0200

    Solution 89 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ho1yy1q3 Prompt: 26.0 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f5fb29d-b7f6-460f-a489-ab0abab1b e75.py", line 15, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f5fb29d-b7f6-460f-a489-ab0abab1b e75.py", line 11, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,6 @@
-```
 def double_the_difference(numbers):
-    total_square = sum(i**2 for i in numbers if isinstance(i, int) and i >= 0)
-    half_total = (sum(numbers) / len([i for i in numbers if i >= 0])) * len([i for i in numbers if i >= 0])
-    return abs(total_square - half_total)
+    total = sum(i**2 for i in numbers if isinstance(i, int) and i >= 0 and i % 2 != 0)
+    half_sum = (sum(i for i in numbers)) / len([i for i in numbers if i >= 0])
+    return abs(total - half_sum * 2)
 
 print(double_the_difference([int(i) for i in input().split(',') if i.isdigit()]))
