103d5e5aeb1499b8f5868d302dd80a8d0b7f55b5
beep boop <githubbot@vadim.me>
2024-07-24 05:32:17 +0200

    Solution 65 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4pstys41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5ba2e58c-5600-4470-830f-1fb2dc69c 409.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_80.py
+++ Python_80.py
@@ -1,9 +1,5 @@
 ```
-def check(func):
-    num = int(input("Enter a number: "))
-    print(func(num))
-
-def is_happy_number(n):
+def is_happy(n):
     seen = set()
     while n != 1 and n not in seen:
         if str(n) in seen:
@@ -12,4 +8,5 @@ def is_happy_number(n):
         n = sum(int(digit) ** 2 for digit in str(n))
     return True
 
-check(is_happy_number)
+num = int(input("Enter a number: "))
+print(is_happy(num))
