f3c8fbe314923e132b46c6695bc2d0a0dbfd413a
beep boop <githubbot@vadim.me>
2024-07-24 10:29:13 +0200

    Solution 37 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/a8daa1c5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9de1eb7d-09c4-499f-8b75-02409b5fe e81.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_5.py
+++ Python_5.py
@@ -1,9 +1,8 @@
-def intersperse_numbers() -> list:
-    numbers = input("Enter space separated integers: ")
-    numbers = [int(num) for num in numbers.split()]
-    delimeter = int(input("Enter the delimeter: "))
+from typing import list
+
+def intersperse(numbers: list[int], delimeter: int) -> list[int]:
     result = [numbers[0]]
     for i in range(1, len(numbers)):
         result.extend([delimeter])
-        result.extend([numbers[i]])
+        result.append(numbers[i])
     return result
