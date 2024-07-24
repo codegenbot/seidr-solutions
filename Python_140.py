50307e313facbfe9583f60239649753a41cec066
beep boop <githubbot@vadim.me>
2024-07-24 04:57:43 +0200

    Solution 10 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z96qpxjj Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33b470bd-45c1-42ae-a34a-d8f5e5182 790.py", line 23, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33b470bd-45c1-42ae-a34a-d8f5e5182 790.py", line 19, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -2,15 +2,16 @@ def fix_spaces(text):
     new_text = ""
     for char in text:
         if char == " ":
-            if len(new_text) > 0 and new_text[-1] != " ":
-                if not ((new_text[-1].isalnum() or new_text[-1] in ["-", "_"]) 
-                        or (len(new_text) == 1 and new_text[0].isalnum()) ):
-                    if new_text:
+            if len(new_text) > 0 and (not new_text[-1].isalnum() and not new_text[-1] in ["-", "_"]):
+                if new_text[-1] != " ":
+                    if new_text[-1]:
                         new_text += "-"
                     else:
-                        new_text = "-"
+                        new_text += " "
                 else:
                     new_text += " "
+            else:
+                new_text += char
         else:
             new_text += char
     return new_text
