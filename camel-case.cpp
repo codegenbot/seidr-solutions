811901ea97bd1cbc2d3337af13226a02e328b5b8
beep boop <githubbot@vadim.me>
2024-07-24 17:46:07 +0200

    Solution 84 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/5b510qsh Prompt: c49f81f6-d589-40ee-9040-f65b24fce6db.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -12,11 +12,9 @@ int main() {
                     result += s.substr(i, s.find(' ', i) - i);
                 else
                     result += s.substr(i, s.find(' ', i) - i).substr(1);
-            } 
-            else if (!result.length()) {
+            } else if (!result.length()) {
                 result = s.substr(i, 1);
-            } 
-            else {
+            } else {
                 if (s[i] == ' ')
                     continue;
                 result += toupper(s[i]);
@@ -26,8 +24,8 @@ int main() {
         return result;
     }
     string input;
-    cout << "Enter a sentence in kebab-case: ";
+    cout << "Enter a kebab-case string: ";
     cin >> input;
-    cout << "Camel case conversion: " << camelCase(input) << endl;
+    cout << "CamelCase: " << camelCase(input) << endl;
     return 0;
 }
