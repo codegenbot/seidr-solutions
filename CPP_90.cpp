9ff1efb8ae3255fc4cd8200e95f5e9fe64c626e4
beep boop <githubbot@vadim.me>
2024-07-24 06:01:44 +0200

    Solution 39 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/53lmbp7o Prompt: 53516968-f35a-4c8a-bd2b-807a27e59dd7.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst) {       |                   ^~~~~~ 53516968-f35a-4c8a-bd2b-807a27e59dd7.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst) {       |                          ^~~ In file included from 53516968-f35a-4c8a-bd2b-807a27e59dd7.cpp:13: 53516968-f35a-4c8a-bd2b-807a27e59dd7.cpp: In function ‘int main()’: 53516968-f35a-4c8a-bd2b-807a27e59dd7.cpp:15:26: error: ‘next_smallest’ cannot be  used as a function    15 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,6 +1,7 @@
 #include <vector>
+
 int next_smallest(vector<int> lst) {
-    if (lst.empty()) return -1; 
+    if (lst.empty()) return -1;
     vector<int> copy = lst;
     sort(copy.begin(), copy.end());
     int first = *copy.begin();
@@ -8,5 +9,5 @@ int next_smallest(vector<int> lst) {
         if (i != first)
             return i;
     }
-    return -1; 
+    return -1;
 }
