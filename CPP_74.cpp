74cd9386ea31b7530e22796b14b39633de520efb
beep boop <githubbot@vadim.me>
2024-07-24 13:01:58 +0200

    Solution 3 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/8o9kzi1s Prompt: In file included from /usr/include/c++/11/string:39,                  from ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp:29: ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp: In function ‘int main()’: ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp:39:20: error: ‘total_match’ was not dec lared in this scope    39 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ ca5b427c-5dc3-4bb5-a160-0937ae53e9fb.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,34 +1,26 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <algorithm>
-using namespace std;
 
-bool issame(vector<string> a,vector<string>b){
-    if(a.size() != b.size())
-        return false;
-    sort(a.begin(),a.end());
-    sort(b.begin(),b.end());
-    for(int i=0; i<a.size(); i++)
-        if(a[i] != b[i])
-            return false;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) return false;
+    }
     return true;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+std::vector<std::string> total_match(std::vector<std::string> lst1, std::vector<std::string> lst2) {
     int sum1 = 0;
-    for (const string& s : lst1) {
+    for (const std::string& s : lst1) {
         sum1 += s.length();
     }
     int sum2 = 0;
-    for (const string& s : lst2) {
+    for (const std::string& s : lst2) {
         sum2 += s.length();
     }
     if (sum1 < sum2) return lst1;
     else if (sum1 > sum2) return lst2;
-    else return lst1;
-}
-
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
-    return 0;
-}
+    else {
+        if (issame(lst1, lst2)) return lst1;
+        else return lst2;
+    }
