30c02cfc70225c4166b48bd1c5a539523e59a601
beep boop <githubbot@vadim.me>
2024-07-24 19:49:19 +0200

    Solution 6 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2xgesiut Prompt: Complete the following code given the task description and function signature.

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,30 +1,30 @@
 vector<string> numerical_letter_grade(vector<float> grades) {
-    vector<string> letterGrades;
+    vector<string> result;
     for (float grade : grades) {
         if (grade >= 4.0)
-            letterGrades.push_back("A+");
+            result.push_back("A+");
         else if (grade > 3.7)
-            letterGrades.push_back("A");
+            result.push_back("A");
         else if (grade > 3.3)
-            letterGrades.push_back("A-");
+            result.push_back("A-");
         else if (grade > 3.0)
-            letterGrades.push_back("B+");
+            result.push_back("B+");
         else if (grade > 2.7)
-            letterGrades.push_back("B");
+            result.push_back("B");
         else if (grade > 2.3)
-            letterGrades.push_back("B-");
+            result.push_back("B-");
         else if (grade > 2.0)
-            letterGrades.push_back("C+");
+            result.push_back("C+");
         else if (grade > 1.7)
-            letterGrades.push_back("C");
+            result.push_back("C");
         else if (grade > 1.3)
-            letterGrades.push_back("C-");
+            result.push_back("C-");
         else if (grade > 1.0)
-            letterGrades.push_back("D+ ");
+            result.push_back("D+");
         else if (grade > 0.7)
-            letterGrades.push_back("D");
+            result.push_back("D");
         else
-            letterGrades.push_back("E");
+            result.push_back("E");
     }
-    return letterGrades;
+    return result;
 }
