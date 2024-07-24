b96a87c203645426c8ae51cc7d96d21db505f3e0
beep boop <githubbot@vadim.me>
2024-07-25 00:45:35 +0200

    Solution 10 of Python/125, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/s2q5cskb Prompt: Complete the following code given the task description and function signature.

--- Python_125.py
+++ Python_125.py
@@ -1,7 +1,11 @@
-```
 def split_words(txt):
-    words = txt.split()
-    if len(words) > 0 and any(char.isalnum() for char in words[0]):
-        return words
+    if any(char.isalnum() for char in txt) and any(char.isspace() for char in txt):
+        return txt.split()
+    elif all(not char.isspace() for char in txt):
+        return sum(
+            ord(char) - 96
+            for char in txt.lower().replace(",", "")
+            if ord("a") <= ord(char) - 96 <= ord("z")
+        )
     else:
-        return sum(1 for i in range(ord('a'), ord('z')+1) if txt.count(chr(i)) % 2 != 0)
+        return sum(1 for char in txt.lower().replace(",", "").replace(" ", "."))
