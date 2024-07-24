797ce3e399e2274bcc72fda6717d98d10e65c76f
beep boop <githubbot@vadim.me>
2024-07-24 17:55:06 +0200

    Solution 24 of Python/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2bi7dxo6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6998eb8-cdb2-4baa-b181-807c32e25 c64.py", line 23, in <module>     check(sort_numbers)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e6998eb8-cdb2-4baa-b181-807c32e25 c64.py", line 18, in check     sort_numbers("six five four three two one zero") AssertionError

--- Python_19.py
+++ Python_19.py
@@ -1,5 +1,4 @@
-import re
-
+```
 def sort_numbers(numbers: str) -> str:
     num_dict = {
         "zero": 0,
@@ -11,9 +10,7 @@ def sort_numbers(numbers: str) -> str:
         "six": 6,
         "seven": 7,
         "eight": 8,
-        "nine": 9
+        "nine": 9,
     }
-    
-    nums = [num_dict.get(re.match(r"(\bzero|one|two|three|four|five|six|seven|eight|nine\b)(s?)\b", word).group(1)) for word in numbers.split()]
-    
-    return " ".join(map(str, sorted(nums)))
+    nums = list(map(lambda x: num_dict[x], numbers.replace(" ", "").split()))
+    return " ".join(sorted(map(str, nums)))
