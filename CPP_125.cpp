b3eb614c3e83a18d0bd29b53c0f5df2dfd18dae1
beep boop <githubbot@vadim.me>
2024-07-24 21:33:56 +0200

    Solution 3 of CPP/125, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/ddy65jvb Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,37 +1,47 @@
+#include<stdio.h>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
 vector<string> split_words(string txt) {
     vector<string> result;
-
-    if (txt.find(' ') != string::npos) {
-        size_t pos = 0;
-        while ((pos = txt.find(' ')) != string::npos) {
-            result.push_back(txt.substr(0, pos));
-            txt.erase(0, pos + 1);
-        }
-        if (!txt.empty()) {
-            result.push_back(txt);
-        }
-    } else {
-        size_t pos = 0;
-        while ((pos = txt.find(',')) != string::npos) {
-            result.push_back(txt.substr(0, pos));
-            txt.erase(0, pos + 1);
-        }
-        if (!txt.empty()) {
-            int oddLowercaseLetters = 0;
-            for (char c : txt) {
-                if (c >= 'a' && c <= 'z') {
-                    if ((ord(c) - ord('a')) % 2 != 0) {
-                        oddLowercaseLetters++;
-                    }
-                } else if (c >= 'A' && c <= 'Z') {
-                    if ((ord(c) - ord('A')) % 2 != 0) {
-                        oddLowercaseLetters++;
-                    }
-                }
+    size_t pos = 0, prevPos = 0;
+    
+    while ((pos = txt.find_first_of(" ,")) != string::npos) {
+        if (pos == 0) {
+            // Split on commas
+            if (txt[0] == ',') {
+                result.push_back(txt.substr(1));
+                txt.erase(0, 1);
+                prevPos = 0;
+            } else {
+                break;
             }
-            result.push_back(to_string(oddLowercaseLetters));
+        }
+        
+        size_t word = txt.find_first_not_of(" ,", prevPos);
+        if (word != string::npos) {
+            result.push_back(txt.substr(prevPos, word - prevPos));
+            prevPos = word + 1;
+        } else {
+            result.push_back(txt.substr(prevPos));
+            break;
         }
     }
-
+    
+    if (result.empty()) {
+        int count = 0;
+        for (char c : txt) {
+            if (c >= 'a' && c <= 'z') {
+                count++;
+                if ((int)c % 2 != 0) {
+                    result.push_back(to_string(count));
+                    break;
+                }
+            }
+        }
+    }
+    
     return result;
 }
