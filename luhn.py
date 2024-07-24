fcc71ebe7fc395f8456f1489693b72fbaa6e1916
beep boop <githubbot@vadim.me>
2024-07-24 07:07:16 +0200

    Solution 39 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/4vklj0xo Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b1af61dc-dcb0-49fc-b0c1-58b060675 c6c.py", line 2     return sum(int(digit) if int(digit) * (i % 2 == 0) * 2 > 9 else int(digit) *  (i % 2 == 0) * 2 else int(digit) for i, digit in enumerate(str(card)))                   ^^^^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -1,2 +1,2 @@
 def luhn(card):
-    return sum((int(digit) * 2 if i % 2 == 0 else int(digit)) - 9 if (int(digit) * 2 if i % 2 == 0 else int(digit)) > 9 else int(digit) for i, digit in enumerate(str(card)))
+    return sum((int(digit) * 2 if i % 2 else int(digit)) - 9 if (int(digit) * 2 if i % 2 else int(digit)) > 9 else int(digit) for i, digit in enumerate(str(card)))
