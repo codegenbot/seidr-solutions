33028429304cffcd475b608bb6ed1ea05179ec40
beep boop <githubbot@vadim.me>
2024-07-24 11:55:38 +0200

    Solution 15 of Python/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xckmmzw6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee58e35e-12bd-453e-bb82-6ead56708 661.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_63.py
+++ Python_63.py
@@ -1,5 +1,14 @@
-def count_squares(n: int):
-    if n < 0:
-        return "Input should be a non-negative integer."
+def find_sum_of_fibonacci(n: int):
+    if n <= 1:
+        return 0
+    elif n == 2:
+        return 1
     else:
-        return sum([i**2 for i in range(n+1)])
+        a, b = 0, 1
+        total_sum = 0
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+            if a > 4000000:  # If the number becomes greater than 4,000,000 break the loop.
+                break
+            total_sum += a
+        return total_sum
