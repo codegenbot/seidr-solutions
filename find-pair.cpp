1a8c7e1bb75d144c446dc25054ca8e3188a4080e
beep boop <githubbot@vadim.me>
2024-07-24 08:39:34 +0200

    Solution 74 of find-pair, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/37wwh14u Prompt: this scope    18 |     cin >> n;       |     ^~~ 8a5cb976-a79b-42ed-9f9c-c2e08957c025.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 8a5cb976-a79b-42ed-9f9c-c2e08957c025.cpp:26:5: error: ‘cout’ was not declared in  this scope    26 |     cout << result.first << endl;       |     ^~~~ 8a5cb976-a79b-42ed-9f9c-c2e08957c025.cpp:26:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 8a5cb976-a79b-42ed-9f9c-c2e08957c025.cpp:26:29: error: ‘endl’ was not declared i n this scope    26 |     cout << result.first << endl;       |                             ^~~~ 8a5cb976-a79b-42ed-9f9c-c2e08957c025.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- find-pair.cpp
+++ find-pair.cpp
@@ -1,9 +1,9 @@
-Here is the solution:
-
 #include <vector>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
 
-vector<int> findPair(vector<int>& nums, int target) {
+pair<int, int> findPair(vector<int>& nums, int target) {
     unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
@@ -12,7 +12,7 @@ vector<int> findPair(vector<int>& nums, int target) {
         }
         numMap[nums[i]] = i;
     }
-    return {};
+    return {0, 0};
 }
 
 int main() {
@@ -24,11 +24,8 @@ int main() {
     }
     int target;
     cin >> target;
-    vector<int> result = findPair(nums, target);
-    if (!result.empty()) {
-        cout << result[0] << " " << result[1] << endl;
-    } else {
-        cout << "No two sum" << endl;
-    }
+    pair<int, int> result = findPair(nums, target);
+    cout << result.first << endl;
+    cout << result.second << endl;
     return 0;
 }
