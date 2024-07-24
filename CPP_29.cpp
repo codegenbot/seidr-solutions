d9d76323e97c77d7c82744d1ce67ae077f04a8be
beep boop <githubbot@vadim.me>
2024-07-24 16:40:07 +0200

    Solution 45 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/reaxh04u Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 314afc53-349d-4e1d-93c6-6066e5127a4b.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 314afc53-349d-4e1d-93c6-6066e5127a4b.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 314afc53-349d-4e1d-93c6-6066e5127a4b.cpp:11: 314afc53-349d-4e1d-93c6-6066e5127a4b.cpp: In function ‘int main()’: 314afc53-349d-4e1d-93c6-6066e5127a4b.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 314afc53-349d-4e1d-93c6-6066e5127a4b.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b);
+bool issame(vector<string> a, vector<string> b); 
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
@@ -16,7 +16,7 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix){
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b){ 
     if (a.size() != b.size()) {
         return false;
     }
