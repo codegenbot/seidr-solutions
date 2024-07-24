4d459e8f9cdc2625ab61926780d3e7c74a1d1c0b
beep boop <githubbot@vadim.me>
2024-07-24 11:00:58 +0200

    Solution 50 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/woh5txid Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee6d94d8-2e5b-4e59-a1d2-647ad9de1 c5e.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ee6d94d8-2e5b-4e59-a1d2-647ad9de1 c5e.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,5 @@
 def sort_third(l: list):
     return [
-        (
-            sum(sorted([x, l[i - 1], l[i + 1]])) / 3
-            if 0 < i < len(l) - 1 and (l[i - 1] <= x <= l[i + 1])
-            else x
-        )
+        sum(sorted([x for x in l[i - 1:i + 2] if 0 <= i < len(l)])) if i+1 < len(l) else x
         for i, x in enumerate(l)
     ]
