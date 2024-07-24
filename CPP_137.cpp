1fab1c1d81f504ad07cde56c5f8f1ad0689e3d2b
beep boop <githubbot@vadim.me>
2024-07-24 14:23:32 +0200

    Solution 9 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7vjwb7qj Prompt:                  from 71cb1e2a-afe9-45f8-98d4-e91c22427ff0.cpp:3: /usr/include/c++/11/bits/stl_iterator.h:426:5: note: candidate: ‘template<class _Iterator> constexpr bool std::operator<(const std::reverse_iterator<_Iterator>& , const std::reverse_iterator<_Iterator>&)’   426 |     operator<(const reverse_iterator<_Iterator>& __x,       |     ^~~~~~~~ /usr/include/c++/11/bits/stl_iterator.h:426:5: note:   template argument deducti on/substitution failed: In file included from 71cb1e2a-afe9-45f8-98d4-e91c22427ff0.cpp:2: /usr/include/c++/11/variant:1230:3: note:   ‘const std::any’ is not derived from  ‘const std::reverse_iterator<_Iterator>’  1230 |   _VARIANT_RELATION_FUNCTION_TEMPLATE(<, less)       |   ^ /usr/include/c++/11/variant:1272:18: note: candidate: ‘constexpr bool std::opera tor<(std::monostate, std::monostate)’  1272 |   constexpr bool operator<(monostate, monostate) noexcept { return false ; }       |                  ^~~~~~~~ /usr/include/c++/11/variant:1272:28: note:   no known conversion for argument 1 from ‘const std::any’ to ‘std::monostate’  1272 |   constexpr bool operator<(monostate, monostate) noexcept { return false ; }       |                            ^~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,22 +1,19 @@
-#include <variant>
-#include <string>
-#include <any>
-
 std::variant<std::string, int, float> compare_one(std::variant<any> a, std::variant<any> b) {
-    if (holds_alternative<int>(a) && holds_alternation<float>(b)) {
+    if (holds_alternative<int>(a) && holds_alternative<float>(b)) {
         return to_string(max(get<int>(a), get<float>(b)));
-    } else if (holds_alternation<float>(a) && holds_alternation<std::string>(b)) {
+    } else if (holds_alternative<float>(a) && holds_alternative<std::string>(b)) {
         std::string s = get<std::string>(b);
         return to_string(max(get<float>(a), stof(s.erase(0, 1).erase(s.length() - 1))));
     } 
-    else if (holds_alternation<std::string>(a) && holds_alternation<std::string>(b)) {
+    else if (holds_alternative<std::string>(a) && holds_alternative<std::string>(b)) {
         return max(a, b).template get<std::string>();
     } 
-    else if (holds_alternation<int>(a) && holds_alternation<int>(b)) {
+    else if (holds_alternative<int>(a) && holds_alternative<int>(b)) {
         int aInt = get<int>(a);
         int bInt = get<int>(b);
         return to_string((aInt > bInt) ? aInt : bInt);
+    } else {
+        // Handle all other cases or unexpected inputs
+        return 0;
     }
-    // Handle all other cases or unexpected inputs
-    return 0;
 }
