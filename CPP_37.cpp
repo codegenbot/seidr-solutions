6205cfd70b5994fe4f02b937c77102d5cd13a448
beep boop <githubbot@vadim.me>
2024-07-24 18:51:56 +0200

    Solution 52 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/cvfj3xyj Prompt: on before ‘float’    42 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 091670ec-4d59-473c-935b-218edad1b208.cpp:42:43: error: expression list treated a s compound expression in initializer [-fpermissive]    42 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 091670ec-4d59-473c-935b-218edad1b208.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 091670ec-4d59-473c-935b-218edad1b208.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~ In file included from 091670ec-4d59-473c-935b-218edad1b208.cpp:41: 091670ec-4d59-473c-935b-218edad1b208.cpp: In function ‘int main()’: 091670ec-4d59-473c-935b-218edad1b208.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <vector>
 
-bool checkVectors(const std::vector<float>& a, const std::vector<float>& b) {
+bool vectorSame(const std::vector<float>& a, const std::vector<float>& b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -34,4 +34,7 @@ std::vector<float> sort_even(std::vector<float> vec) {
     return result;
 }
 
-assert(checkVectors(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+int main() {
+    assert(vectorSame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
+}
