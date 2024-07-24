a04b3fdd9736c593316e86b4e3288218a415531b
beep boop <githubbot@vadim.me>
2024-07-24 07:37:54 +0200

    Solution 32 of CPP/116, ratio 0.75 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vv9eja35 Prompt: Program failed

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 vector<int> sort_array(vector<int> arr) {
-    vector<int> sorted_arr = arr;
-    sort(sorted_arr.begin(), sorted_arr.end());
-    return sorted_arr;
+    sort(arr.begin(), arr.end());
+    return arr;
 }
