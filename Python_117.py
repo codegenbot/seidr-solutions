5ad6709e735ca248f9875313dddceabc34da1c9a
beep boop <githubbot@vadim.me>
2024-07-24 10:17:24 +0200

    Solution 1 of Python/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/uluu31pu Prompt: Complete the following code given the task description and function signature.

--- Python_117.py
+++ Python_117.py
@@ -5,7 +5,7 @@ def select_words(s, n):
     for word in words:
         consonants = 0
         for char in word:
-            if char.lower() not in 'aeiou ':
+            if char.lower() not in 'aeiou':
                 consonants += 1
         if consonants == n:
             result.append(word)
