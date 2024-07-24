c64898c44168ceb75e679349cc82bc92c9df03a2
beep boop <githubbot@vadim.me>
2024-07-24 02:06:48 +0200

    Solution 38 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/walwymsv Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/86ad60bf-8cb0-4762-824a-368350dcc 8aa.py", line 12, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/86ad60bf-8cb0-4762-824a-368350dcc 8aa.py", line 9, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,7 +1,9 @@
 def fizz_buzz(n: int):
-    output_count = 0
+    count = 0
+    fizzbuzz_count = 0
     for i in range(1, n + 1):
         output = "FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
         print(output)
-        output_count += 1 if output in ["Fizz", "Buzz", "FizzBuzz"] else 0
-    return output_count
+        if output == "FizzBuzz":
+            fizzbuzz_count += 1
+    return fizzbuzz_count
