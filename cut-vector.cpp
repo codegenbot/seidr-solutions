77d4b7e979ce9811c06cf06fd4e77b5818022ebf
beep boop <githubbot@vadim.me>
2024-07-25 08:33:20 +0200

    Solution 76 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run7/runs/ixs2ihlg Prompt: Program did not receive expected input

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -9,32 +9,31 @@ int main() {
     while (cin >> n) {
         nums.push_back(n);
     }
-    if (cin.eof()) {
-        int sum = 0, halfSum = 0;
-        for (int num : nums) {
-            sum += num;
-        }
-        halfSum = sum / 2;
-        
-        int currSum = 0, idx = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            currSum += nums[i];
-            if (currSum >= halfSum) {
-                idx = i;
-                break;
-            }
-        }
-        
-        vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
-        vector<int> subvector2(nums.begin() + idx + 1, nums.end());
-        
-        for (int num : subvector1) {
-            cout << num << endl;
-        }
-        for (int num : subvector2) {
-            cout << num << endl;
+
+    int sum = 0, halfSum = 0;
+    for (int num : nums) {
+        sum += num;
+    }
+    halfSum = sum / 2;
+    
+    int currSum = 0, idx = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        currSum += nums[i];
+        if (currSum >= halfSum) {
+            idx = i;
+            break;
         }
     }
     
+    vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
+    vector<int> subvector2(nums.begin() + idx + 1, nums.end());
+    
+    for (int num : subvector1) {
+        cout << num << endl;
+    }
+    for (int num : subvector2) {
+        cout << num << endl;
+    }
+
     return 0;
 }
