15717fa2fe3820df67036b5ac1053fc5bced8c37
beep boop <githubbot@vadim.me>
2024-07-25 04:04:59 +0200

    Solution 79 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/a2edab92 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f406ca2b-798c-43a5-a4c8-cb32ecb6f 9ce.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_7.py
+++ Python_7.py
@@ -1,4 +1,4 @@
-```
+````
 def calculate_total_cost(items: list[dict], tax_rate: float) -> float:
     total_cost = sum(item["price"] * item["quantity"] for item in items)
     return total_cost + (total_cost * tax_rate)
