3ad0a78101251c568334bf53aba450d1aa86f2e7
beep boop <githubbot@vadim.me>
2024-07-25 06:37:14 +0200

    Solution 41 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d88c865f-c567-4ead-bc9a-3ac371079 cb8.py", line 17, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d88c865f-c567-4ead-bc9a-3ac371079 cb8.py", line 14, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,9 +1,13 @@
 ```
+import re
+
 def fruit_distribution(s, n):
     s = s.replace("apples", "1").replace("oranges", "0")
-    parts = s.split()
-    total_count = set()
+    parts = [x for x in re.split(r"(\d+|\band\))", s) if x.isdigit() or x == 'and']
+    ones = int(s.count('1'))
+    zeros = int(s.count('0'))
+    total_count = 0
     for i in range(len(parts)):
-        words = [x for x in parts[i].split() if x.isdigit()]
-        total_count.update(words)
-    return n - len(total_count)
+        if parts[i].isdigit():
+            total_count += int(parts[i])
+    return n - total_count - ones - zeros
