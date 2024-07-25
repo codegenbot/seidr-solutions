e6d5fdb2efc63f110975e8a6a820d37122b3e8a2
beep boop <githubbot@vadim.me>
2024-07-25 05:35:07 +0200

    Solution 32 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: aff9b6e6-fae9-459e-877a-510cfcdec28c.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from aff9b6e6-fae9-459e-877a-510cfcdec28c.cpp:39: aff9b6e6-fae9-459e-877a-510cfcdec28c.cpp: In function ‘int main()’: aff9b6e6-fae9-459e-877a-510cfcdec28c.cpp:41:13: error: ‘boost’ has not been decl ared    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ aff9b6e6-fae9-459e-877a-510cfcdec28c.cpp:41:29: error: ‘string’ was not declared  in this scope    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ aff9b6e6-fae9-459e-877a-510cfcdec28c.cpp:41:37: error: ‘compare_one’ was not dec lared in this scope    41 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,5 +1,4 @@
 #include <boost/any.hpp>
-#include <string>
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
@@ -8,30 +7,30 @@ boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return b;
     }
-    if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        std::string strA = boost::any_cast<std::string>(a);
-        std::string strB = boost::any_cast<std::string>(b);
+    if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string strA = boost::any_cast<string>(a);
+        string strB = boost::any_cast<string>(b);
         size_t pos = strB.find(',');
-        if (pos != std::string::npos) {
+        if (pos != string::npos) {
             strB.erase(pos, 1);
         }
-        return (std::stod(strA) > std::stod(strB)) ? a : b;
+        return (stod(strA) > stod(strB)) ? a : b;
     }
-    if (a.type() == typeid(std::string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
-        std::string strA = boost::any_cast<std::string>(a);
+    if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
+        string strA = boost::any_cast<string>(a);
         size_t pos = strA.find(',');
-        if (pos != std::string::npos) {
+        if (pos != string::npos) {
             strA.erase(pos, 1);
         }
-        return (std::stod(strA) > boost::any_cast<double>(b)) ? a : b;
+        return (stod(strA) > boost::any_cast<double>(b)) ? a : b;
     }
-    if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(std::string)) {
-        std::string strB = boost::any_cast<std::string>(b);
+    if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(string)) {
+        string strB = boost::any_cast<string>(b);
         size_t pos = strB.find(',');
-        if (pos != std::string::npos) {
+        if (pos != string::npos) {
             strB.erase(pos, 1);
         }
-        return (boost::any_cast<double>(a) > std::stod(strB)) ? a : b;
+        return (boost::any_cast<double>(a) > stod(strB)) ? a : b;
     }
     if (boost::any_cast<int>(a) == boost::any_cast<int>(b)) {
         return boost::any("None");
