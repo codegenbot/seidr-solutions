a41912fd135860ec83e2341903de093fc76627ee
beep boop <githubbot@vadim.me>
2024-07-25 03:51:50 +0200

    Solution 6 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xpbcy5mf Prompt:       |                              ^~~~~~ 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp:34: 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp: In function ‘int main()’: 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp:44:20: error: ‘numerical_letter_grade’ was not declared in this scope    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 37838a7e-17cb-4490-a0da-aa9a7598c5d6.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,30 +1,37 @@
 #include <vector>
 #include <string>
 
+bool issame(float a, float b) {
+    if (abs(a - b) <= 0.001)
+        return true;
+    else
+        return false;
+}
+
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> letterGrades;
     for (float grade : grades) {
-        if (grade >= 4.0)
+        if (issame(grade, 4.0))
             letterGrades.push_back("A+");
-        else if (grade > 3.7)
+        else if (issame(grade, 3.7))
             letterGrades.push_back("A");
-        else if (grade > 3.3)
+        else if (issame(grade, 3.3))
             letterGrades.push_back("A-");
-        else if (grade > 3.0)
+        else if (issame(grade, 3.0))
             letterGrades.push_back("B+");
-        else if (grade > 2.7)
+        else if (issame(grade, 2.7))
             letterGrades.push_back("B");
-        else if (grade > 2.3)
+        else if (issame(grade, 2.3))
             letterGrades.push_back("B-");
-        else if (grade > 2.0)
+        else if (issame(grade, 2.0))
             letterGrades.push_back("C+");
-        else if (grade > 1.7)
+        else if (issame(grade, 1.7))
             letterGrades.push_back("C");
-        else if (grade > 1.3)
+        else if (issame(grade, 1.3))
             letterGrades.push_back("C-");
-        else if (grade > 1.0)
+        else if (issame(grade, 1.0))
             letterGrades.push_back("D+");
-        else if (grade > 0.7)
+        else if (issame(grade, 0.7))
             letterGrades.push_back("D");
         else
             letterGrades.push_back("E");
