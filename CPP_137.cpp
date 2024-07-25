3e079aa63fcfde259be1a75823d145ce5d9b9498
beep boop <githubbot@vadim.me>
2024-07-25 06:37:50 +0200

    Solution 97 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt:    33 | } else if ((a.type() == typeid(double)) &&       |   ^~~~ 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:40:3: error: expected unqualified-id be fore ‘else’    40 | } else {       |   ^~~~ In file included from 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:45: 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp: In function ‘int main()’: 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:47:13: error: ‘boost’ has not been decl ared    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:47:29: error: ‘string’ was not declared  in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:47:37: error: ‘compare_one’ was not dec lared in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,74 +1,58 @@
 #include <any>
 #include <string>
-#include <locale>
+#include <wstring>
 using namespace std;
-
-namespace std {
 
 any a;
 any b;
 
-bool compareAny(const any& x) {
-    if (x.type() == typeid(int)) {
-        int value = any_cast<int>(x);
-        return &compareAnyImpl<int>;
-    } else if (x.type() == typeid(double)) {
-        double value = any_cast<double>(x);
-        return &compareAnyImpl<double>;
-    } else if (x.type() == typeid(string) || x.type() == typeid(wstring)) {
-        string value = any_cast<string>(x);
-        return &compareAnyImpl<string>;
-    }
+namespace std {
 
-    // default case
-    return &compareAnyImpl<int>;
-}
-
-template<typename T>
-bool compareAnyImpl(T value) {
-    if (!a.type() || !b.type()) {
-        return false;
-    }
-
-    bool isInt = (a.type() == typeid(int)) && (b.type() == typeid(int));
-    bool isDouble = (a.type() == typeid(double)) && (b.type() == typeid(double));
-    bool isString = ((a.type() == typeid(string)) || (a.type() == typeid(wstring))) &&
-                    ((b.type() == typeid(string)) || (b.type() == typeid(wstring)));
-
-    if (isInt) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = any_cast<int>(a);
         int y = any_cast<int>(b);
-        return x > y;
-    } else if (isDouble) {
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return a;
+    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
         double x = any_cast<double>(a);
         double y = any_cast<double>(b);
-        return x > y;
-    } else if (isString) {
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return a;
+    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
+           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
         string x = any_cast<string>(a);
         string y = any_cast<string>(b);
-        return x > y;
+        if (x > y) return a;
+        else if (x < y) return b;
+        else return a;
+    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
+           (b.type() == typeid(double))) {
+        string x = any_cast<string>(a);
+        double y = any_cast<double>(b);
+        if(stod(x) > y) return a;
+        else if(stod(x) < y) return b;
+        else return a;
+    } else if ((a.type() == typeid(double)) &&
+           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
+        double x = any_cast<double>(a);
+        string y = any_cast<string>(b);
+        if(x > stod(y)) return a;
+        else if(x < stod(y)) return b;
+        else return a;
+    } else {
+        return a;
     }
-
-    // default case
-    return true;
 }
 
 int main() {
-    // read input from user
-    cout << "Enter two values to compare: ";
-    cin >> a >> b;
+    // Input values for 'a' and 'b'
+    a = any(string("hello"));
+    b = any(string("world"));
 
-    if (compareAny(a) && compareAny(b)) {
-        if (a > b) {
-            return 1;
-        } else if (a < b) {
-            return -1;
-        }
-        return 0;
-    }
-
-    // handle invalid input
-    cout << "Invalid input. Please enter two values of the same type." << endl;
-
-    return 2;
+    // Call the function with 'a' and 'b' as arguments
+    any result = your_function(a, b);
+    
+    return 0;
 }
