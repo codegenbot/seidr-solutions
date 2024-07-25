9123ae492e582af3ebc5f461171862d4b2642fcd
beep boop <githubbot@vadim.me>
2024-07-25 03:21:25 +0200

    Solution 4 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/n9dzm1sg Prompt: d5d22337-79c6-4d7f-809f-c1d7a0466c87.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int smallest_change(vector<int> arr) {       |                     ^~~~~~ d5d22337-79c6-4d7f-809f-c1d7a0466c87.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int smallest_change(vector<int> arr) {       |                            ^~~ In file included from d5d22337-79c6-4d7f-809f-c1d7a0466c87.cpp:24: d5d22337-79c6-4d7f-809f-c1d7a0466c87.cpp: In function ‘int main()’: d5d22337-79c6-4d7f-809f-c1d7a0466c87.cpp:26:28: error: ‘smallest_change’ cannot be used as a function    26 |     assert (smallest_change({0, 1}) == 1);       |             ~~~~~~~~~~~~~~~^~~~~~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,17 +1,24 @@
+#include <vector>
+
 int smallest_change(vector<int> arr) {
-    int left = 0, right = arr.size() - 1, changes = 0;
+    int n = arr.size();
+    string str = "";
+    for (int i : arr) {
+        str += to_string(i);
+    }
+    
+    int left = 0, right = n - 1;
+    int changes = 0;
+    
     while (left < right) {
-        if (arr[left] != arr[right]) {
+        if (str[left] != str[right]) {
             changes++;
-            break;
-        }
-        left++;
-        right--;
-    }
-    for (int i = left; i <= right; i++) {
-        if (arr[i] != arr[(i - left + arr.size() - 1) % arr.size()]) {
-            changes++;
+            left++;
+        } else {
+            left++;
+            right--;
         }
     }
+    
     return changes;
 }
