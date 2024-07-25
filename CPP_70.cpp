ea555ec51379f3cebcfc259a0055e8c9b15261f7
beep boop <githubbot@vadim.me>
2024-07-25 04:04:57 +0200

    Solution 45 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 912240c5-fbcf-482c-a063-6486c01459ca.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 912240c5-fbcf-482c-a063-6486c01459ca.cpp:36:34: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 912240c5-fbcf-482c-a063-6486c01459ca.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 912240c5-fbcf-482c-a063-6486c01459ca.cpp:35: 912240c5-fbcf-482c-a063-6486c01459ca.cpp: In function ‘int main()’: 912240c5-fbcf-482c-a063-6486c01459ca.cpp:45:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    45 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 912240c5-fbcf-482c-a063-6486c01459ca.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,4 +1,3 @@
-```
 #include <algorithm>
 #include <vector>
 
@@ -9,7 +8,17 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
     }
     
     std::sort(lst.begin(), lst.end());
+    result.push_back(*std::min_element(lst.begin(), lst.end()));
+    lst.erase(std::remove(lst.begin(), lst.end(), *std::min_element(lst.begin(), lst.end())), lst.end());
+    
+    if (!lst.empty()) {
+        std::sort(lst.begin(), lst.end());
+        result.push_back(*std::max_element(lst.begin(), lst.end()));
+        lst.erase(std::remove(lst.begin(), lst.end(), *std::max_element(lst.begin(), lst.end())), lst.end());
+    }
+    
     while (!lst.empty()) {
+        std::sort(lst.begin(), lst.end());
         result.push_back(*std::min_element(lst.begin(), lst.end()));
         lst.erase(std::remove(lst.begin(), lst.end(), *std::min_element(lst.begin(), lst.end())), lst.end());
         
@@ -21,4 +30,3 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
     }
     
     return result;
-}
