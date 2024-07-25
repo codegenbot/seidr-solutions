c28e8d6bc28853ef1190ec047ada62c3123712f5
beep boop <githubbot@vadim.me>
2024-07-25 20:56:20 +0200

    Solution 37 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/45mz2zsa Prompt:       |                    ^~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:21: 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp: In function ‘int main()’: 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,19 +1,19 @@
 #include <vector>
 #include <algorithm>
 
-vector<int> unique_digits(vector<int> x){
+vector<int> unique_digits(vector<int> b) {
     vector<int> result;
-    for(int num : x){
+    for (int num : b) {
         int digit = num;
         bool hasEvenDigit = false;
-        while(digit > 0){
-            if((digit % 10) % 2 == 0){
+        while (digit > 0) {
+            if ((digit % 10) % 2 == 0) {
                 hasEvenDigit = true;
                 break;
             }
             digit /= 10;
         }
-        if(!hasEvenDigit){
+        if (!hasEvenDigit) {
             result.push_back(num);
         }
     }
