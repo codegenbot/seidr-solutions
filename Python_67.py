b00ffdc5121fef5d44e67a16b3126b33645dd1cc
beep boop <githubbot@vadim.me>
2024-07-25 06:19:19 +0200

    Solution 21 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26635d46-7c8a-4465-972f-c0f74ba1f 3fd.py", line 15, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26635d46-7c8a-4465-972f-c0f74ba1f 3fd.py", line 12, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26635d46-7c8a-4465-972f-c0f74ba1f 3fd.py", line 7, in fruit_distribution     count += int(parts[i].lstrip("0123456789abcdefghijklmnopqrstuvwxyz").replace ('a', '1').replace('o', '0'))              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: ''

--- Python_67.py
+++ Python_67.py
@@ -3,10 +3,8 @@ def fruit_distribution(s, n):
     s = s.replace("apples", "1").replace("oranges", "0")
     parts = s.split()
     count = 0
-    i = 2
-    while i < len(parts):
-        if parts[i].lower() == "and":
-            break
-        count += int(parts[i].lstrip("0123456789").replace('a', '1').replace('o', '0'))
-        i += 1
+    for i in range(len(parts)):
+        if 'and' not in parts[i]:
+            if parts[i].lstrip("0123456789abcdefghijklmnopqrstuvwxyz").replace('a', '1').replace('o', '0'):
+                count += int(parts[i].lstrip("0123456789abcdefghijklmnopqrstuvwxyz").replace('a', '1').replace('o', '0'))
     return n - count
