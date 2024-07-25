5d9de967b117b8fbf7e3081349e91dad08ac68ac
beep boop <githubbot@vadim.me>
2024-07-25 20:55:27 +0200

    Solution 30 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/45mz2zsa Prompt:       |                    ^~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:21: 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp: In function ‘int main()’: 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 2140e945-7b2d-42b8-83d4-70d7d240c348.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,38 +1,22 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b);
-
-vector<int> unique_digits(vector<int> x){
+vector<int> unique_digits(vector<int> b){
     vector<int> result;
-    for (int num : x) {
+    for(int num : b){
+        int digit = num;
         bool hasEvenDigit = false;
-        int temp = num;
-        while (temp > 0) {
-            if (temp % 2 == 0) {
+        while(digit > 0){
+            if((digit % 10) % 2 == 0){
                 hasEvenDigit = true;
                 break;
             }
-            temp /= 10;
+            digit /= 10;
         }
-        if (!hasEvenDigit) {
+        if(!hasEvenDigit){
             result.push_back(num);
         }
     }
     sort(result.begin(), result.end());
     return result;
 }
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
-        return false;
-
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i])
-            return false;
-    }
-
-    return true;
-}
-
-assert(std::equal(unique_digits({135, 103, 31}).begin(), unique_digits({135, 103, 31}).end(), {31, 135}.begin()));
