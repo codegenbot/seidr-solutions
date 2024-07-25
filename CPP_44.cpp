5da7c0fad0366555f56b48b0c8031b833b1d2ba7
beep boop <githubbot@vadim.me>
2024-07-25 02:56:33 +0200

    Solution 42 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xr4m8yr6 Prompt: 62246140-73bb-42d0-8551-28fd2a9f5a5c.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 62246140-73bb-42d0-8551-28fd2a9f5a5c.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 62246140-73bb-42d0-8551-28fd2a9f5a5c.cpp:20: 62246140-73bb-42d0-8551-28fd2a9f5a5c.cpp: In function ‘int main()’: 62246140-73bb-42d0-8551-28fd2a9f5a5c.cpp:24:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    24 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from 62246140-73bb-42d0-8551-28fd2a9f5a5c.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -4,7 +4,7 @@
 
 std::string change_base(int x, int base) {
     std::string result = "";
-    while(x > 0) {
+    while (x > 0) {
         result = std::to_string(x % base) + result;
         x /= base;
     }
