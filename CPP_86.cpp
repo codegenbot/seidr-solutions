63d4799ae30a9e588ab661fc3ef83d934620f58c
beep boop <githubbot@vadim.me>
2024-07-25 04:28:27 +0200

    Solution 4 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a4yl2ltl Prompt: f52a3192-11f7-4e75-8607-aac24e4d0dbd.cpp:5:5: error: ‘string’ does not name a ty pe     5 |     string anti_shuffle(string s) {       |     ^~~~~~ In file included from f52a3192-11f7-4e75-8607-aac24e4d0dbd.cpp:23: f52a3192-11f7-4e75-8607-aac24e4d0dbd.cpp: In function ‘int main()’: f52a3192-11f7-4e75-8607-aac24e4d0dbd.cpp:25:13: error: ‘anti_shuffle’ was not de clared in this scope    25 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -3,25 +3,22 @@
 #include <cassert>
 
 namespace Contest {
-    std::string anti_shuffle(std::string s) {
-        std::string result = "";
-        std::string word = "";
+    using namespace std;
+
+    string anti_shuffle(string s) {
+        string result = "";
+        string word = "";
         for (char c : s) {
             if (c == ' ') {
-                std::sort(word.begin(), word.end());
+                sort(word.begin(), word.end());
                 result += word + ' ';
                 word = "";
             } else {
                 word += c;
             }
         }
-        std::sort(word.begin(), word.end());
+        sort(word.begin(), word.end());
         result += word;
         return result;
     }
-}
-
-int main() {
-    assert(Contest::anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
-    return 0;
 }
