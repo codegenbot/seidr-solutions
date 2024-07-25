a45f25ab04c863e112e089bdbb0570cce574c92a
beep boop <githubbot@vadim.me>
2024-07-25 07:12:33 +0200

    Solution 16 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4xpbjgo0 Prompt:       |                    ^~~ f75b39de-0353-4b3a-9274-58ad805ba524.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f75b39de-0353-4b3a-9274-58ad805ba524.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f75b39de-0353-4b3a-9274-58ad805ba524.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f75b39de-0353-4b3a-9274-58ad805ba524.cpp:17: f75b39de-0353-4b3a-9274-58ad805ba524.cpp: In function ‘int main()’: f75b39de-0353-4b3a-9274-58ad805ba524.cpp:27:20: error: ‘common’ was not declared  in this scope    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f75b39de-0353-4b3a-9274-58ad805ba524.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,11 +1,34 @@
-Here is the completed code:
+#include <vector>
+#include <set>
+
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-    set<int> intersection;
+    set<int> result;
+    
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
-    vector<int> result(intersection.begin(), intersection.end());
-    sort(result.begin(), result.end());
-    return result;
+        inserter(result, result.begin()));
+    
+    vector<int> res;
+    for (int i : result) {
+        res.push_back(i);
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
