44105e22881f6164a2c64e4221cc73fabdcd7c17
beep boop <githubbot@vadim.me>
2024-07-25 09:06:07 +0200

    Solution 17 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/abxe0rwc Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 43b7199d-e1db-4e34-afdc-f90a0b2a668b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 43b7199d-e1db-4e34-afdc-f90a0b2a668b.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 43b7199d-e1db-4e34-afdc-f90a0b2a668b.cpp:25: 43b7199d-e1db-4e34-afdc-f90a0b2a668b.cpp: In function ‘int main()’: 43b7199d-e1db-4e34-afdc-f90a0b2a668b.cpp:35:20: error: ‘rolling_max’ was not dec lared in this scope    35 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 43b7199d-e1db-4e34-afdc-f90a0b2a668b.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-bool same(vector<int> a, vector<int> b) {
+bool same(vector<int> a, vector<int> b) { 
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i)
         if (a[i] != b[i]) return false;
