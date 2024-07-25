7e4025ddd2f996873dc95bac5845c30b2406da5a
beep boop <githubbot@vadim.me>
2024-07-25 05:15:51 +0200

    Solution 68 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/nvtu1uy7 Prompt: Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.

--- leaders.py
+++ leaders.py
@@ -1,7 +1,4 @@
-Here is the solution:
-
-def leaders(input_list):
-    return [x for x in input_list[::-1] if all(x >= y for y in input_list[input_list.index(x) + 1:][::-1])]
-
-input_list = list(map(int, input().split()))
-print(' '.join(str(i) for i in leaders(input_list)))
+def leaders(input):
+    return [
+        i for i in reversed(input) if all(i >= j for j in input[input.index(i) + 1 :])
+    ]
