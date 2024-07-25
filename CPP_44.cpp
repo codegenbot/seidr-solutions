ad776c13b81e8f02b757fd44fd645a91d691cc42
beep boop <githubbot@vadim.me>
2024-07-25 03:03:11 +0200

    Solution 48 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xr4m8yr6 Prompt: 1d7b0643-5576-4fbf-a1dc-7f678051ff89.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 1d7b0643-5576-4fbf-a1dc-7f678051ff89.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -13,8 +13,8 @@ std::string change_base(int x, int base) {
     return result;
 }
 
-int main(){
+int main() {
     int x = 10;
-    assert(change_base(x, x + 1) == std::to_string(x));
+    assert(change_base(x, x + 1) == to_string(x));
     return 0;
 }
