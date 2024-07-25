79a4cc853c9122020afcc23ae44eb67a3354ce86
beep boop <githubbot@vadim.me>
2024-07-25 05:18:40 +0200

    Solution 69 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xpbcy5mf Prompt: :allocator<_CharT> >::allocator_type&, int) [with _Tp = char; std::allocator_tra its<std::allocator<_CharT> >::pointer = char*; std::allocator_traits<std::alloca tor<_CharT> >::allocator_type = std::allocator<char>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = char; _Alloc = std::allocator<char>; std::_Vector_base<_Tp, _Alloc>::poin ter = char*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {char}; _Tp = char; _Alloc = std::allocator<char>; std::vector<_Tp,  _Alloc>::iterator = std::vector<char>::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {char}; _Tp = char; _Alloc = std::allocator<char>; std::vector<_Tp, _Alloc>:: reference = char&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = cha r; _Alloc = std::allocator<char>; std::vector<_Tp, _Alloc>::value_type = char]’ 139f4f82-9213-4d49-9c2e-7be457709e34.cpp:9:35:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<char>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,30 +1,23 @@
-```cpp
 #include <iostream>
 #include <vector>
 
 std::vector<char> numerical_letter_grade(std::vector<char> grades) {
     std::vector<char> letterGrades;
     for (char grade : grades) {
-        if (grade >= 'A' && grade <= 'F')
-            letterGrades.push_back(grade);
-        else if (grade > 90)
+        if (grade >= 'D' + 1)
             letterGrades.push_back('A');
-        else if (grade > 87)
-            letterGrades.push_back('B+');
-        else if (grade > 83)
-            letterGrades.push_back('B');
-        else if (grade > 80)
-            letterGrades.push_back('+');
-        else if (grade > 77)
-            letterGrades.push_back('C+');
-        else if (grade > 73)
-            letterGrades.push_back('C');
-        else if (grade > 70)
+        else if (grade > 'D')
             letterGrades.push_back('-');
-        else if (grade > 67)
-            letterGrades.push_back('D+');
-        else if (grade > 63)
-            letterGrades.push_back('D');
+        else if (grade > 'C')
+            letterGrades.push_back('+');
+        else if (grade > 'B-')
+            letterGrades.push_back('B');
+        else if (grade > 'B')
+            letterGrades.push_back('-');
+        else if (grade > 'A-')
+            letterGrades.push_back('A');
+        else if (grade > 'A')
+            letterGrades.push_back('+');
         else
             letterGrades.push_back('F');
     }
@@ -32,18 +25,39 @@ std::vector<char> numerical_letter_grade(std::vector<char> grades) {
 }
 
 int main() {
-    std::vector<char> grades = {'C', 'A', 'B'};
+    std::vector<char> grades = {'C', '+', '-'};
     std::cout << "The letter grades for these numerical grades are: ";
     for (const auto& grade : numerical_letter_grade(grades)) {
         switch (grade) {
             case 'A':
+                std::cout << "A+ ";
+                break;
+            case 'A'-:
                 std::cout << "A  ";
+                break;
+            case '-':
+                std::cout << "A- ";
                 break;
             case '+':
                 std::cout << "B+ ";
                 break;
+            case 'B':
+                std::cout << "B  ";
+                break;
+            case '-':
+                std::cout << "B- ";
+                break;
+            case 'C':
+                std::cout << "C+ ";
+                break;
+            case 'D':
+                std::cout << "D+ ";
+                break;
             case '-':
                 std::cout << "C- ";
+                break;
+            case '+':
+                std::cout << "D  ";
                 break;
             default:
                 std::cout << "F  ";
