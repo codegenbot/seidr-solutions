5647670602f1f4b6958d930a719a8f20967fc560
beep boop <githubbot@vadim.me>
2024-07-25 03:14:11 +0200

    Solution 77 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/awnx1f09 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,64 +1,21 @@
-```
-def bowling_score(game):
-    score = 0
-    roll_count = 1
-    current_frame = [0, 0]
-    
-    for i in range(0, len(game), 2):
-        if game[i].isdigit():
-            if int(game[i]) == 10:
-                score += 10 + (10 - 1) * 2
-                roll_count += 1
-            elif game[i+1].isdigit() and int(game[i:i+2]) <= 10:
-                score += int(game[i:i+2])
-                current_frame[0] = int(game[i:i+2])
-                roll_count += 1
-            else:
-                current_frame[1] = int(game[i])
-                if current_frame[0] + current_frame[1] == 10:
-                    score += 10 + (10 - 1) * 2
-                    roll_count += 1
-                elif current_frame[0] + current_frame[1] > 10:
-                    score += 10 + current_frame[0]
-                    current_frame = [0, 0]
-                else:
-                    score += current_frame[0] + current_frame[1]
-                    if roll_count < 10:
-                        while game[i+2].isdigit() and int(game[i+2]) != 10:
-                            i += 1
-                            if i >= len(game):
-                                break
-                            current_frame[0] = int(game[i])
-                            score += current_frame[0]
-                            roll_count += 1
-                    current_frame = [0, 0]
-        else:
-            if game[i] == 'X':
-                score += 10 + (10 - 1) * 2
-                roll_count += 1
-            elif game[i+1].isdigit() and int(game[i:i+2]) <= 10:
-                current_frame[0] = int(game[i:i+2])
-                score += int(game[i:i+2])
-                roll_count += 1
-            else:
-                if game[i-1].isdigit():
-                    current_frame[1] = int(game[i-1])
-                    if current_frame[0] + current_frame[1] == 10:
-                        score += 10 + (10 - 1) * 2
-                        roll_count += 1
-                    elif current_frame[0] + current_frame[1] > 10:
-                        score += 10 + current_frame[0]
-                        current_frame = [0, 0]
-                    else:
-                        score += current_frame[0] + current_frame[1]
-                        if roll_count < 10:
-                            while game[i+2].isdigit() and int(game[i+2]) != 10:
-                                i += 1
-                                if i >= len(game):
-                                    break
-                                current_frame[0] = int(game[i])
-                                score += current_frame[0]
-                                roll_count += 1
-                    current_frame = [0, 0]
+Here is the solution:
 
+def bowling(score):
+    frames = score.split('/')
+    score = 0
+    for i in range(len(frames)):
+        if len(frames[i]) == 1:
+            score += int(frames[i])
+        elif len(frames[i]) == 2:
+            score += int(frames[i][0]) + int(frames[i][1])
+        else:
+            first_roll = int(frames[i][0])
+            second_roll = int(frames[i][1])
+            third_roll = int(frames[i][2])
+            if first_roll + second_roll == 10:
+                score += first_roll + third_roll
+            elif second_roll == 10:
+                score += first_roll + 10
+            else:
+                score += first_roll + second_roll
     return score
