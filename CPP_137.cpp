24e8817fc4310a32fd0ef7d3b0076d6ae23062a0
beep boop <githubbot@vadim.me>
2024-07-25 06:30:16 +0200

    Solution 90 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt:    34 | } else if ((a.type() == typeid(double)) &&       |   ^~~~ 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:41:3: error: expected unqualified-id be fore ‘else’    41 | } else {       |   ^~~~ In file included from 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:46: 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp: In function ‘int main()’: 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:48:13: error: ‘boost’ has not been decl ared    48 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:48:29: error: ‘string’ was not declared  in this scope    48 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:48:37: error: ‘compare_one’ was not dec lared in this scope    48 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -18,14 +18,14 @@ if (a.type() == typeid(int) && b.type() == typeid(int)) {
     if (x > y) return a;
     else if (x < y) return b;
     else return a;
-} else if ((a.type() == typeid(std::string) || a.type() == typeid(boost::wstring)) &&
-           (b.type() == typeid(std::string) || b.type() == typeid(boost::wstring))) {
+} else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
+           (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
     std::string x = std::any_cast<std::string>(a);
     std::string y = std::any_cast<std::string>(b);
     if (x > y) return a;
     else if (x < y) return b;
     else return a;
-} else if ((a.type() == typeid(std::string) || a.type() == typeid(boost::wstring)) &&
+} else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
            (b.type() == typeid(double))) {
     std::string x = std::any_cast<std::string>(a);
     double y = std::any_cast<double>(b);
@@ -33,7 +33,7 @@ if (a.type() == typeid(int) && b.type() == typeid(int)) {
     else if (std::stod(x) < y) return b;
     else return a;
 } else if ((a.type() == typeid(double)) &&
-           (b.type() == typeid(std::string) || b.type() == typeid(boost::wstring))) {
+           (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
     double x = std::any_cast<double>(a);
     std::string y = std::any_cast<std::string>(b);
     if (x > std::stod(y)) return a;
