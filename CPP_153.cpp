f6a9fe31c08c1b3f90607675fa83ee499dd74300
beep boop <githubbot@vadim.me>
2024-07-25 05:50:49 +0200

    Solution 6 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4swgnmk1 Prompt:       |                         ^~~~~~ 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp:20: 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp: In function ‘int main()’: 82993e9a-3f28-4ad9-be9e-6a0a2f1000ba.cpp:22:32: error: ‘Strongest_Extension’ can not be used as a function    22 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,15 +1,20 @@
+```cpp
+#include <vector>
+#include <string>
+#include <cctype>
+
 int Strongest_Extension(std::string class_name, std::vector<std::string> extensions) {
     int strongest = -1;
     std::string result;
-    for(auto& extension:extensions){
+    for (auto &extension : extensions) {
         int cap = 0;
         int sm = 0;
-        for(char c:extension){
-            if(std::isupper(c))cap++;
-            else if(std::islower(c))sm++;
+        for (char c : extension) {
+            if (std::isupper(c)) cap++;
+            else if (std::islower(c)) sm++;
         }
         int strength = cap - sm;
-        if(strength > strongest){
+        if (strength > strongest) {
             strongest = strength;
             result = extension;
         }
