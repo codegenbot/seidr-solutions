e4f6060e4f106b31b901d609a6dfc2088cdb7a02
beep boop <githubbot@vadim.me>
2024-07-25 06:43:28 +0200

    Solution 93 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/33hnxnvj Prompt:       |                           ^~~~~~ 7f8724b5-c990-4003-bd71-ea42cc6afd73.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 7f8724b5-c990-4003-bd71-ea42cc6afd73.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 7f8724b5-c990-4003-bd71-ea42cc6afd73.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7f8724b5-c990-4003-bd71-ea42cc6afd73.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7f8724b5-c990-4003-bd71-ea42cc6afd73.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 7f8724b5-c990-4003-bd71-ea42cc6afd73.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool isSame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size())
         return false;
     for (int i = 0; i < a.size(); i++) {
@@ -21,8 +21,8 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     return result;
 }
 
-int problemMain() {
+std::vector<int> mainFunction() {
     std::vector<int> test = maximum({1, 2, 3, -23, 243, -400, 0}, 5);
-    assert(issame(test, {3,243,0,-23,-400}));
-    return 0;
+    assert(isSame(test, {3,243,0,-23,-400}));
+    return test;
 }
