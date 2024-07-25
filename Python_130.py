a60e713fee2a8a8b911709b7fb31967d6adcf945
beep boop <githubbot@vadim.me>
2024-07-25 17:14:21 +0200

    Solution 22 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/zx3lswkr Prompt: Complete the following code given the task description and function signature.

--- Python_130.py
+++ Python_130.py
@@ -2,10 +2,10 @@ def tri(n):
     if n == 0:
         return [0]
     elif n == 1:
-        return [3]
+        return [1]
     else:
-        tribonacci = [3, 1]
-        for i in range(2, n + 1):
+        tribonacci = [0, 1, 1]
+        for i in range(3, n + 1):
             if i % 2 == 0:
                 tribonacci.append(1 + i // 2)
             else:
