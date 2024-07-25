8c969e65bc1b29a9f4ce16571626aaa3e50724d2
beep boop <githubbot@vadim.me>
2024-07-25 13:06:52 +0200

    Solution 30 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1zrsnvr3 Prompt:    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1e490b48-1c6a-435f-95b1-828c4b1b4b5e.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1e490b48-1c6a-435f-95b1-828c4b1b4b5e.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1e490b48-1c6a-435f-95b1-828c4b1b4b5e.cpp:12: 1e490b48-1c6a-435f-95b1-828c4b1b4b5e.cpp: In function ‘int main()’: 1e490b48-1c6a-435f-95b1-828c4b1b4b5e.cpp:22:20: error: ‘sorted_list_sum’ was not  declared in this scope    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 1e490b48-1c6a-435f-95b1-828c4b1b4b5e.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,23 +1,19 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
 bool issame(const vector<string>& a, const vector<string>& b) {
     return a == b;
 }
 
 vector<string> sorted_list_sum(const vector<string>& lst) {
-    vector<string> result = lst;
-    result.erase(remove_if(result.begin(), result.end(), 
-        [](const string& s){ return s.length() % 2 != 0; }), result.end());
-    
-    sort(result.begin(), result.end(), 
-        [](const string& a, const string& b) {
-            if (a.length() == b.length()) {
-                return a < b;
-            }
-            return a.length() < b.length();
-        });
-    
-    return result;
+    vector<string> filtered_lst = lst;
+    filtered_lst.erase(remove_if(filtered_lst.begin(), filtered_lst.end(),
+            [](const string& s){ return s.length() % 2 != 0;}), filtered_lst.end());
+
+    sort(filtered_lst.begin(), filtered_lst.end(), 
+            [](const string& a, const string& b) {
+        if (a.length() == b.length()) {
+            return a < b;
+        }
+        return a.length() < b.length();
+    });
+
+    return filtered_lst;
 }
