462488a81cd41c902391a69b4a86c0c0a3b93704
beep boop <githubbot@vadim.me>
2024-07-25 07:25:12 +0200

    Solution 79 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: b/python3.11/site-packages/programlib/programs/e90c0069-3708-4382-9e62-b024f1b0d 096.py", line 19, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e90c0069-3708-4382-9e62-b024f1b0d 096.py", line 14, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e90c0069-3708-4382-9e62-b024f1b0d 096.py", line 5, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e90c0069-3708-4382-9e62-b024f1b0d 096.py", line 6, in <listcomp>     sorted(   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e90c0069-3708-4382-9e62-b024f1b0d 096.py", line 8, in <lambda>     key=lambda x: x[2] if len(x) >= 3 else float("inf")                           ^^^^^^ TypeError: object of type 'int' has no len()

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,12 @@
 def sort_third(l: list):
     remainder = (3 - len(l) % 3) % 3
     l += [float("inf")] * remainder
-    tuples_list = [tuple(l[i : i + 3]) for i in range(0, len(l), 3)]
+    tuples_list = [tuple(l[i: i + 3]) for i in range(0, len(l), 3)]
     return [
-        sorted(sub_list, key=lambda x: x[2] if len(x) >= 3 else float("inf"))
+        sorted(
+            sub_list,
+            key=lambda x: x[2] if len(x) >= 3 else float("inf")
+        )
+        if sub_list else []
         for sub_list in tuples_list
-    ]
+   ]
