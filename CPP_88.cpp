2713ab36041b8cc06f59c8b1d0e133ef20311f23
beep boop <githubbot@vadim.me>
2024-07-25 09:58:30 +0200

    Solution 86 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/s4dkfdw7 Prompt:       |                    ^~~ 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp:11: 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp: In function ‘int main()’: 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp:21:20: error: ‘sort_array’ was not decl ared in this scope    21 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 7f0059b6-6bb4-4957-b630-a0725ee6771a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -2,12 +2,17 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.empty()) return false;
-    int sum = a.front() + a.back();
-    if(sum % 2 == 0){
-        sort(a.begin(), a.end(), std::greater<int>());
-    } else {
-        sort(a.begin(), a.end());
-    }
+    if(a.empty() || b.empty()) return false;
     return a == b;
+}
+
+std::vector<int> sort_array(std::vector<int> array) {
+    if(array.empty()) return array;
+    int sum = array.front() + array.back();
+    if(sum % 2 == 0){
+        sort(array.begin(), array.end(), std::greater<int>());
+    } else {
+        sort(array.begin(), array.end());
+    }
+    return array;
 }
