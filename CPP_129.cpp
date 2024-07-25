87033a7276543d4cb6931d004540d687d2d463fc
beep boop <githubbot@vadim.me>
2024-07-25 05:30:08 +0200

    Solution 7 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xqrhykk2 Prompt:     4 | In file included from /usr/include/c++/11/cassert:44,                  from 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp:3: 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp: In function ‘int main()’: 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp:51:26: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    51 |     assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3 , 1, 3}));       |                   ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp:7:26: note:   initializing argument 1 o f ‘bool issame(std::vector<int>&, std::vector<int>&)’     7 | bool issame(vector<int>& a, vector<int>& b){       |             ~~~~~~~~~~~~~^ 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp: At global scope: 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp:65:5: error: redefinition of ‘int main( )’    65 | int main(){       |     ^~~~ 2e1255c1-4b40-4fb4-9a4a-d01df845933f.cpp:50:5: note: ‘int main()’ previously def ined here    50 | int main(){       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,52 +1,53 @@
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <cassert>
 
 using namespace std;
 
-vector<int> minPath(vector<vector<int>> grid, int k);
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(const vector<int>& a, const vector<int>& b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
-vector<int> minPath(vector<vector<int>> grid, int k) {
+vector<int> minPath(vector<vector<int>> grid, int k){
     int n = grid.size();
     int m = grid[0].size();
-
+    
     vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
     dp[0][0] = grid[0][0];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (i > 0) dp[i][j] = std::min(dp[i][j], dp[i - 1][j] + grid[i][j]);
-            if (j > 0) dp[i][j] = std::min(dp[i][j], dp[i][j - 1] + grid[i][j]);
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(i > 0) dp[i][j] = min(dp[i][j], dp[i-1][j] + grid[i][j]);
+            if(j > 0) dp[i][j] = min(dp[i][j], dp[i][j-1] + grid[i][j]);
         }
     }
-
+    
     vector<int> path;
-    int i = n - 1, j = m - 1;
-    while (i >= 0 && j >= 0) {
+    int i = n-1, j = m-1;
+    while(i >= 0 && j >= 0){
         path.push_back(grid[i][j]);
-        if (i == 0 && j == 0) break;
-        if (i > 0 && dp[i][j] == dp[i - 1][j] + grid[i][j]) {
+        if(i == 0 && j == 0) break;
+        if(i > 0 && dp[i][j] == dp[i-1][j] + grid[i][j]){
             i--;
         } else {
             j--;
         }
     }
-
+    
     reverse(path.begin(), path.end());
-
-    if (dp[n - 1][m - 1] <= k) {
+    
+    if(dp[n-1][m-1] <= k){
         return path;
     } else {
         return {};
     }
 }
 
-int main() {
+int main(){
     assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
     
     return 0;
