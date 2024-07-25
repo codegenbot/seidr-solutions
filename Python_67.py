3eb89648139f0de8d326c61e1036cd2191417c22
beep boop <githubbot@vadim.me>
2024-07-25 06:58:10 +0200

    Solution 57 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/475470b8-969d-447d-ab41-27d1fc008 4af.py", line 23, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/475470b8-969d-447d-ab41-27d1fc008 4af.py", line 20, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: fruit_distribution() takes 1 positional argument but 2 were given

--- Python_67.py
+++ Python_67.py
@@ -1,17 +1,16 @@
-```
-def fruit_distribution(sentence):
-    sentence = sentence.lower()
-    if "and" in sentence:
-        parts = []
-        for x in re.split(r"(\d+|\band\))", sentence):
-            if x.isdigit() or x == "and":
-                parts.append(x)
-    else:
-        parts = [x for x in re.split(r"\d+", sentence)]
-    ones = sentence.count("1")
-    zeros = sentence.count("0")
-    total_count = 0
-    for i in range(len(parts)):
-        if parts[i].isdigit():
-            total_count += int(parts[i])
+def fruit_distribution(s): 
+    s = s.lower() 
+    if "and" in s: 
+        parts = [] 
+        for x in re.split(r"(\d+|\band\))", s): 
+            if x.isdigit() or x == "and": 
+                parts.append(x) 
+    else: 
+        parts = [x for x in re.split(r"\d+", s)] 
+    ones = s.count("1") 
+    zeros = s.count("0") 
+    total_count = 0 
+    for i in range(len(parts)): 
+        if parts[i].isdigit(): 
+            total_count += int(parts[i]) 
     return max(0, 5 - total_count - ones - zeros)
