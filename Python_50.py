613f2ee28e3c944743917d69bba066f7a62127cf
beep boop <githubbot@vadim.me>
2024-07-25 05:27:31 +0200

    Solution 15 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bhiqujov Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,10 +1,15 @@
 def decode_shift(s: str):
     result = ""
     i = 0
+    shift = -1 
     while i < len(s):
-        if s[i].isalpha():
+        if s[i].isalpha() and not ('z' == s[i] or 'Z' == s[i]):
             ascii_offset = ord("a") if s[i].islower() else ord("A")
-            result += chr(((ord(s[i]) - ascii_offset) % 26) + ascii_offset)
+            shift = int(s[i-1:i])
+            if ((ord(s[i]) - ascii_offset - shift) % 26) <= 0:
+                result += chr(((ord(s[i]) - ascii_offset - shift + 26) % 26) + ascii_offset)
+            else:
+                result += chr(((ord(s[i]) - ascii_offset - shift) % 26) + ascii_offset)
         else:
             result += s[i]
         i += 1
