f9462535d55d0fd2a2e046de23c853aefca89d12
beep boop <githubbot@vadim.me>
2024-07-25 07:10:14 +0200

    Solution 2 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mj4y6a3y Prompt: Complete the following code given the task description and function signature.

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,9 +1,9 @@
 int count = 0;
 for(int i=0; i<brackets.length(); i++){
-    if(brackets[i] == '(') count++;
-    else if(brackets[i] == ')') {
+    if(brackets[i] == '(')
+        count++;
+    else if(brackets[i] == ')')
         if(count <= 0) return false;
-        count--;
-    }
+        else count--;
 }
-return count == 0;
+return (count==0);
