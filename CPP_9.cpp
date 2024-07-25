52a3aa670301d1c9b6e5b7417ea6c8b2394afc37
beep boop <githubbot@vadim.me>
2024-07-25 08:58:39 +0200

    Solution 2 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/abxe0rwc Prompt: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 34d84da6-3844-46e9-af23-727537142088.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 34d84da6-3844-46e9-af23-727537142088.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 34d84da6-3844-46e9-af23-727537142088.cpp:17: 34d84da6-3844-46e9-af23-727537142088.cpp: In function ‘int main()’: 34d84da6-3844-46e9-af23-727537142088.cpp:27:20: error: ‘rolling_max’ was not dec lared in this scope    27 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 34d84da6-3844-46e9-af23-727537142088.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,11 +1,27 @@
+#include <vector>
+#include <assert.h>
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i]) return false;
+    return true;
+}
+
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
     int max = INT_MIN;
+
     for (int num : numbers) {
         if (num > max) {
             max = num;
         }
         result.push_back(max);
     }
+
     return result;
+}
+
+int main() {
+    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
 }
