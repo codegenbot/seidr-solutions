0157f5fdc633e24a0eede5c6b2c616b4b56c5606
beep boop <githubbot@vadim.me>
2024-07-25 09:34:30 +0200

    Solution 14 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fj63u79l Prompt: 9787921b-b599-4f3a-9f9b-d12bfea4d5aa.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<int> pluck(vector<int> arr) {       | ^~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,28 +1,20 @@
-```cpp
-#include <vector>
+```
+using namespace std;
 
 vector<int> pluck(vector<int> arr) {
     vector<int> result = {};
     if (arr.empty()) return result;
 
     int smallest_even_value = INT_MAX;
+    int index = -1;
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] % 2 == 0 && arr[i] < smallest_even_value) {
             smallest_even_value = arr[i];
-        }
-    }
-
-    bool found_smallest_even = false;
-    int smallest_even_index = -1;
-    for (int i = 0; i < arr.size(); i++) {
-        if (!found_smallest_even && arr[i] == smallest_even_value) {
-            smallest_even_index = i;
-            found_smallest_even = true;
+            index = i;
         }
     }
 
     result.push_back(smallest_even_value);
-    if (found_smallest_even)
-        result.push_back(smallest_even_index);
+    result.push_back(index);
 
     return result;
