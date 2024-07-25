f0ffbe332eb7dcf6e67fa018aadf3ce0d3574405
beep boop <githubbot@vadim.me>
2024-07-25 03:10:41 +0200

    Solution 9 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1jrj2z50 Prompt: fore ‘static_cast’    18 | assert(common({4, 3, 2, 8}, {}) == vector<int>{});       | ^~~~~~ 27bc8671-9af2-4d0c-91bd-fbcd7d702cc5.cpp:18:1: error: expected ‘)’ before ‘stati c_cast’    18 | assert(common({4, 3, 2, 8}, {}) == vector<int>{});       | ^~~~~~ 27bc8671-9af2-4d0c-91bd-fbcd7d702cc5.cpp:18:27: error: expected unqualified-id b efore ‘,’ token    18 | assert(common({4, 3, 2, 8}, {}) == vector<int>{});       |                           ^ 27bc8671-9af2-4d0c-91bd-fbcd7d702cc5.cpp:18:29: error: expected unqualified-id b efore ‘{’ token    18 | assert(common({4, 3, 2, 8}, {}) == vector<int>{});       |                             ^ 27bc8671-9af2-4d0c-91bd-fbcd7d702cc5.cpp:18:31: error: expected unqualified-id b efore ‘)’ token    18 | assert(common({4, 3, 2, 8}, {}) == vector<int>{});       |                               ^ 27bc8671-9af2-4d0c-91bd-fbcd7d702cc5.cpp:18:1: error: expected unqualified-id be fore ‘)’ token    18 | assert(common({4, 3, 2, 8}, {}) == vector<int>{});       | ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
-#include <cassert>
+#include <cassert> // Add this header
 
 using namespace std;
 
@@ -16,6 +16,6 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    assert((common({4, 3, 2, 8}, {}) == vector<int>{}));
+    assert(common({4, 3, 2, 8}, {}) == vector<int>{});
     return 0;
 }
