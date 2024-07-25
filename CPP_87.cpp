3d672f38595206f3cdca2a96ba8ae4f1e7e2535e
beep boop <githubbot@vadim.me>
2024-07-25 05:28:09 +0200

    Solution 76 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |     ^~~~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | c9a42ac9-ce53-4983-90d3-c73091640876.cpp: At global scope: c9a42ac9-ce53-4983-90d3-c73091640876.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<std::vector<int> >, std::vector<std::vector<int> >)’    39 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |      ^~~~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:6:6: note: ‘bool issame(std::vector<std ::vector<int> >, std::vector<std::vector<int> >)’ previously defined here     6 | bool issame(vector<vector<int>> a, vector<vector<int>> b) {       |      ^~~~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -19,17 +20,11 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     for (int i = 0; i < lst.size(); i++) {
         if (x == 1) {
             for (int j = lst[i].size() - 1; j >= 0; j--) {
-                vector<int> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                result.push_back(temp);
+                result.push_back({i, j});
             }
         } else {
             for (int j = 0; j < lst[i].size(); j++) {
-                vector<int> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                result.push_back(temp);
+                result.push_back({i, j});
             }
         }
     }
@@ -37,6 +32,5 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
 }
 
 int main() {
-    assert(issame(get_row({},{1},{1,2,3}),{{2,2}}));
+    assert(issame(get_row({},1,1,2,3), 3) == {{2,2}});
     return 0;
-}
