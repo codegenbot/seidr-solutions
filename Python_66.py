4c49b2b60ab17ccc51be722381b4a317dc0f26e1
beep boop <githubbot@vadim.me>
2024-07-25 06:44:39 +0200

    Solution 55 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/j8yxpsc6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4f3bcd3d-3da1-40fa-a63c-0e906bfe6 ca3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_66.py
+++ Python_66.py
@@ -1,11 +1,9 @@
+```
 def digitSum(s):
     total = 0
     for char in s.lower():
-        if char.isalnum():
-            if char.isdigit():
-                total += int(char)
-            else:
-                total += ord(char) - 96
+        if char.isdigit():
+            total += int(char)
     return total
 
 print(digitSum("You arE Very Smart"))
