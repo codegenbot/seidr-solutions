b12b0ed25e513866f47eac5b60ff14aa325de12f
beep boop <githubbot@vadim.me>
2024-07-25 06:51:50 +0200

    Solution 36 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/59uyt18e Prompt:       |                    ^~~ b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp:12: b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp: In function ‘int main()’: b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ b50627a6-11a9-44df-a9cf-12f1b7a9b7a5.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -1,7 +1,5 @@
 #include <vector>
 
-vector<int> intersperse(vector<int> numbers, int delimeter);
-
 vector<int> intersperse(vector<int> numbers, int delimeter){
     vector<int> result;
     for(size_t i = 0; i < numbers.size(); ++i){
