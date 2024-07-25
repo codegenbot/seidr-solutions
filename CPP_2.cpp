d0ac6d6e42e1df20465b2ef728dee85d84134a51
beep boop <githubbot@vadim.me>
2024-07-25 06:16:09 +0200

    Solution 3 of CPP/2, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/wq4319if Prompt: Complete the following code given the task description and function signature.

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,1 +1,8 @@
-return fmod(number, 1);
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+
+float truncate_number(float number){
+    int integer_part = (int)number;
+    return number - integer_part;
+}
