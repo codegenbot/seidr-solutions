2a39319e07e60a1d4a24d94ca74ec2725fb041ab
beep boop <githubbot@vadim.me>
2024-07-25 22:28:49 +0200

    Solution 90 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/mv4dgipi Prompt:  this scope    23 |         cout << nums[i] << endl;       |         ^~~~ 97bfe299-4fa5-4914-8069-ea9cc09924e6.cpp:23:17: error: ‘nums’ was not declared i n this scope    23 |         cout << nums[i] << endl;       |                 ^~~~ 97bfe299-4fa5-4914-8069-ea9cc09924e6.cpp:23:28: error: ‘endl’ was not declared i n this scope    23 |         cout << nums[i] << endl;       |                            ^~~~ 97bfe299-4fa5-4914-8069-ea9cc09924e6.cpp:25:5: error: ‘cout’ was not declared in  this scope    25 |     cout << endl;       |     ^~~~ 97bfe299-4fa5-4914-8069-ea9cc09924e6.cpp:25:13: error: ‘endl’ was not declared i n this scope    25 |     cout << endl;       |             ^~~~ 97bfe299-4fa5-4914-8069-ea9cc09924e6.cpp:27:17: error: ‘nums’ was not declared i n this scope    27 |         cout << nums[i] << endl;       |                 ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <climits>
-#include <cmath>
+#include <numeric>
 
 using namespace std;
 
 int main() {
-    vector<int> nums;
-    int num;
-    
-    while (cin >> num) {
-        nums.push_back(num);
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
     }
     
-    int n = nums.size();
     int min_diff = INT_MAX;
-    int cut_index = 0;
+    int cut_index = -1;
     
     for (int i = 1; i < n; ++i) {
-        int left_sum = 0, right_sum = 0;
-        
-        for (int j = 0; j < i; ++j) {
-            left_sum += nums[j];
-        }
-        
-        for (int j = i; j < n; ++j) {
-            right_sum += nums[j];
-        }
-        
-        int diff = abs(left_sum - right_sum);
-        
+        int sum_left = accumulate(nums.begin(), nums.begin() + i, 0);
+        int sum_right = accumulate(nums.begin() + i, nums.end(), 0);
+        int diff = abs(sum_left - sum_right);
         if (diff < min_diff) {
             min_diff = diff;
             cut_index = i;
@@ -39,9 +28,7 @@ int main() {
     for (int i = 0; i < cut_index; ++i) {
         cout << nums[i] << endl;
     }
-    
     cout << endl;
-    
     for (int i = cut_index; i < n; ++i) {
         cout << nums[i] << endl;
     }
