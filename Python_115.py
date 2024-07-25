7e3693cc82f5b3bf45e61f4972aaad3a73f6291f
beep boop <githubbot@vadim.me>
2024-07-25 07:23:49 +0200

    Solution 20 of Python/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qitbv21o Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcb71007-939b-4331-8270-778211fc7 6c2.py", line 25, in <module>     check(max_fill)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcb71007-939b-4331-8270-778211fc7 6c2.py", line 22, in check     assert max_fill([[1, 1, 1, 1], [1, 1, 1, 1]], 9) == 2, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_115.py
+++ Python_115.py
@@ -1,12 +1,10 @@
-import math
-
-
 def max_fill(grid, capacity):
     rows = len(grid)
     cols = len(grid[0])
-    total_water = sum(
-        grid[i].count(c) for i in range(rows) for c in range(cols) if grid[i][c] > 0
-    )
+    total_water = sum(sum(row) for row in grid)
+
+    if capacity < total_water:
+        return -1 
 
     for i in range(cols):
         for j in range(rows):
@@ -15,6 +13,4 @@ def max_fill(grid, capacity):
                 grid[j][i] -= amount_to_fill
                 capacity -= amount_to_fill
 
-    return math.ceil(
-        total_water / (capacity + sum(1 for row in grid for _ in row) - total_water)
-    )
+    return math.ceil(total_water / (capacity + sum(1 for row in grid for _ in row) - total_water))
