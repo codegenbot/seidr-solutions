54e76585fde43d1669575c5c7dadbb643161870f
beep boop <githubbot@vadim.me>
2024-07-25 07:53:22 +0200

    Solution 41 of Python/119, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jyo7qqnt Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1341e223-4cab-4abe-a090-62018f843 b84.py", line 21, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1341e223-4cab-4abe-a090-62018f843 b84.py", line 18, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,16 +1,16 @@
-```
 def match_parens(lst):
     stack = []
     for s in lst:
         for c in s:
-            if c == '(':
+            if c == "(":
                 stack.append(c)
-            elif c == ')':
+            elif c == ")":
+                while len(stack) > 0 and stack.pop() != "(":
+                    pass
                 if len(stack) == 0:
-                    return 'No'
+                    return "Yes"
                 else:
-                    stack.pop()
-    if len(stack) == 0:
-        return 'Yes'
-    else:
-        return 'No'
+                    return "No"
+    if all(x != "(" for x in stack):
+        return "Yes"
+    return "No"
