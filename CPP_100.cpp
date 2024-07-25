93a154626bda54b621c0f0ae7ef3d61188ba5294
beep boop <githubbot@vadim.me>
2024-07-25 20:57:24 +0200

    Solution 55 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7w803xub Prompt:       |                    ^~~ 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp:13: 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp: In function ‘int main()’: 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp:23:20: error: ‘make_a_pile’ was not dec lared in this scope    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 2be03bbf-568f-46bb-80b6-e7e34c71624b.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -2,11 +2,11 @@
 #include <cassert>
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()){
+    if(a.size() != b.size()) {
         return false;
     }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
+    for(int i=0; i<a.size(); i++) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
@@ -16,11 +16,11 @@ bool issame(vector<int> a, vector<int> b) {
 vector<int> make_a_pile(int n) {
     vector<int> stones;
     stones.push_back(n);
-    while(n != 1){
-        if(n % 2 == 0){
-            n /= 2;
+    for(int i=1; i<n; i++) {
+        if(n % 2 == 0) {
+            n += 2;
         } else {
-            n = 3 * n + 1;
+            n += 1;
         }
         stones.push_back(n);
     }
