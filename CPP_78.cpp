80f6ab7c312c38f7ac6dcb90d1f002301d4f1388
beep boop <githubbot@vadim.me>
2024-07-25 03:08:21 +0200

    Solution 44 of CPP/78, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/196hbpzl Prompt: Complete the following code given the task description and function signature.

--- CPP_78.cpp
+++ CPP_78.cpp
@@ -3,20 +3,8 @@ int hex_key(string num) {
     for (char c : num) {
         if (c >= '2' && c <= '7') {
             count++;
-        } else if ((c >= 'A' && c <= 'F')) {
-            switch (c - 'A') {
-                case 0: // 'A'
-                    count++;
-                    break;
-                case 1: // 'B'
-                    count++;
-                    break;
-                case 2: // 'C'
-                case 3: // 'D'
-                case 4: // 'E'
-                case 5: // 'F'
-                    break;
-            }
+        } else if ((c >= 'B' && c <= 'F')) {
+            count++;
         }
     }
     return count;
