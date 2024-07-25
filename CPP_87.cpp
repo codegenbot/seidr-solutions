4936352b1a9abbac7569ee3ab2248a67b37bf76c
beep boop <githubbot@vadim.me>
2024-07-25 04:58:55 +0200

    Solution 46 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from a2955ece-dfe4-4462-aca7-61049955435e.cpp:17: a2955ece-dfe4-4462-aca7-61049955435e.cpp: In function ‘int main()’: a2955ece-dfe4-4462-aca7-61049955435e.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,21 +1,19 @@
-std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
-    std::vector<std::vector<int>> result;
-    for (int i = 0; i < lst.size(); i++) {
-        if (x == 1) {
-            for (int j = lst[i].size() - 1; j >= 0; j--) {
-                vector<int> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                result.push_back(temp);
-            }
-        } else {
-            for (int j = 0; j < lst[i].size(); j++) {
-                vector<int> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                result.push_back(temp);
+#include <iostream>
+#include <vector>
+
+bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
+    if (a.size() != b.size) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i].size() != b[i].size()) {
+            return false;
+        }
+        for (int j = 0; j < a[i].size(); j++) {
+            if (a[i][j] != b[i][j]) {
+                return false;
             }
         }
     }
-    return result;
+    return true;
 }
