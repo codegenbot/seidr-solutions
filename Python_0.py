eea7cd308ec455cd8f4155c4d0cdfa0b6d469921
beep boop <githubbot@vadim.me>
2024-07-25 06:41:46 +0200

    Solution 48 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/niporhik Prompt: 3 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/477995f0-eff5-4469-9669-430d88825 802.py", line 14, in <module>     check(has_close_elements)           ^^^^^^^^^^^^^^^^^^ NameError: name 'has_close_elements' is not defined

--- Python_0.py
+++ Python_0.py
@@ -1,7 +1,7 @@
 def check_numbers_threshold(numbers, threshold):
     count = 0
     for num in numbers:
-        if num > threshold:
+        if num >= threshold:
             count += 1
     return count
 
