15225c20a5ddcda2d3ca57c72144583dca3f4377
beep boop <githubbot@vadim.me>
2024-07-25 09:22:49 +0200

    Solution 44 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/l1mka7my Prompt:       |                              ^~~~~~ 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp:17:45: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp:16: 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp: In function ‘int main()’: 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp:26:20: error: ‘total_match’ was not dec lared in this scope    26 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 7410f2e1-4b72-491e-bcde-5775f94a0610.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,33 +1,7 @@
-#include <iostream>
-#include <vector>
 #include <string>
-#include <cassert>
-
-using namespace std;
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2);
 
 bool issame(vector<string> a, vector<string> b) {
     return a == b;
-}
-
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    int total_chars1 = 0, total_chars2 = 0;
-    for (const string& str : lst1) {
-        total_chars1 += str.size();
-    }
-    for (const string& str : lst2) {
-        total_chars2 += str.size();
-    }
-    if (total_chars1 < total_chars2 || (total_chars1 == total_chars2 && !lst1.empty())) {
-        return lst1;
-    } else {
-        return lst2;
-    }
-}
-
-int main() {
-    assert(issame(total_match({"this"}, {}), {}));
-    
-    return 0;
 }
