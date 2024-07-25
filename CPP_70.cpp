a84f4309da70f308b46ed1e9198c42fa288c3174
beep boop <githubbot@vadim.me>
2024-07-25 03:45:04 +0200

    Solution 33 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:       |                    ^~~ 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp:15: 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp: In function ‘int main()’: 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp:25:20: error: ‘strange_sort_list’ was n ot declared in this scope    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 6a44fdcd-7142-4db3-94b5-deffc0cef274.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -4,13 +4,13 @@
 std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
-        int min = *std::min_element(lst.begin(), lst.end());
+        int min = *min_element(lst.begin(), lst.end());
         result.push_back(min);
-        lst.erase(std::remove(lst.begin(), lst.end(), min), lst.end());
+        lst.erase(remove(lst.begin(), lst.end(), min), lst.end());
         if (!lst.empty())
-            min = *std::min_element(lst.begin(), lst.end());
+            min = *min_element(lst.begin(), lst.end());
         result.push_back(min);
-        lst.erase(std::remove(lst.begin(), lst.end(), min), lst.end());
+        lst.erase(remove(lst.begin(), lst.end(), min), lst.end());
     }
     return result;
 }
