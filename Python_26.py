c93453a4463d2f941e60cb588dba7173f45f76ef
beep boop <githubbot@vadim.me>
2024-07-25 11:45:16 +0200

    Solution 22 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/q9kaavhc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05369f73-6a13-4168-8cc3-566d3d48c 881.py", line 1     return [num for num in numbers if numbers.count(num) == 1]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_26.py
+++ Python_26.py
@@ -1,5 +1,4 @@
 def find_numbers_appeared_only_once(numbers):
     return [num for num in numbers if numbers.count(num) == 1]
 
-# Call the function with the list of numbers as input
-find_numbers_appeared_only_once([1, 2, 2, 3, 4, 4, 5])
+find_numbers_appeared_only_once([1, 2, 2, 3, 4, 4, 5, 6, 6])
