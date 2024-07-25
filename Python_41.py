d3ad7c71abd4167186f1687ee7bfa50b88ed7d7d
beep boop <githubbot@vadim.me>
2024-07-25 04:47:19 +0200

    Solution 25 of Python/41, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/m7j9ls35 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2e54c508-bb0b-4061-8c83-50964c3ed 6cb.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_41.py
+++ Python_41.py
@@ -1,8 +1,7 @@
 def car_race_collision(n):
     total_distance = 0
     for _ in range(n):
-        print("Enter the speed of a car (in km/h): ")
-        speed = int(input())
+        speed = int(input("Enter the speed of a car (in km/h): "))
         time = 100 / speed  
         distance = speed * time
         total_distance += distance
