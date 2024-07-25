40dde95ec685adb8c5c5e4ba4be048e989646397
beep boop <githubbot@vadim.me>
2024-07-25 18:08:01 +0200

    Solution 97 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a9jr89yt Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,17 +1,26 @@
-if(a.index() == b.index()){
-    if(a.type() == typeid(int)){
-        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) return a;
-        else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) return b;
-        else return "None";
-    }
-    else if(a.type() == typeid(float)){
-        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) return a;
-        else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) return b;
-        else return "None";
-    }
-    else if(a.type() == typeid(string)){
-        if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))) return a;
-        else if(stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))) return b;
-        else return "None";
-    }
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
+        return a;
+    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
+        return b;
+}
+else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
+        return a;
+    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
+        return b;
+}
+else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    string str_a = boost::any_cast<string>(a);
+    string str_b = boost::any_cast<string>(b);
+    if(str_a.find('.') != string::npos || str_a.find(',') != string::npos)
+        return b;
+    else if(str_b.find('.') != string::npos || str_b.find(',') != string::npos)
+        return a;
+    else if(stof(str_a) > stof(str_b))
+        return a;
+    else if(stof(str_a) < stof(str_b))
+        return b;
+}
+return "None";
 }
