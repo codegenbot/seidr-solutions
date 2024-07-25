3e60671c68891f335121bc933861a574e6cdd79b
beep boop <githubbot@vadim.me>
2024-07-25 15:09:26 +0200

    Solution 63 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/37m1h32b Prompt: Complete the following code given the task description and function signature.

--- Python_6.py
+++ Python_6.py
@@ -1,10 +1,5 @@
 def parse_nested_parens(paren_string: str) -> List[int]:
-    levels = []
-    current_level = 0
-    for char in paren_string:
-        if char == "(":
-            current_level += 1
-            levels.append(current_level)
-        elif char == ")":
-            current_level -= 1
-    return levels
+    return [
+        max([(s.count("(") - s.count(")")) for s in group.split()])
+        for group in paren_string.split()
+    ]
