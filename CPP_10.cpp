1f09e620dae906da6c684945cfe5b2935990b80f
beep boop <githubbot@vadim.me>
2024-07-25 09:30:37 +0200

    Solution 58 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from 031c682f-a133-4717-a535-438e38466465.cpp:2: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ 031c682f-a133-4717-a535-438e38466465.cpp:7:23:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,15 +1,20 @@
 ```cpp
 #include <string>
+#include <iostream>
 using namespace std;
 
 string make_palindrome(string input) {
-    // implementation
+    string reversed = "";
+    for(int i=input.length()-1; i>=0; i--) {
+        reversed += input[i];
+    }
+    return input + std::string(reversed);
 }
 
 int main() {
     string input, output;
     cout << "Enter a string: ";
-    getline(cin, input);
+    cin >> input;
     output = make_palindrome(input);
     cout << "Palindrome of the given string is: " << output << endl;
     return 0;
