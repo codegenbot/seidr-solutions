b0ceb27a1fcd4c277c5117bbab9100bfef934515
beep boop <githubbot@vadim.me>
2024-07-25 03:20:49 +0200

    Solution 12 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/kwj3d9s9 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3a6fd40e-9afa-4b46-aa20-7e17a0157 aff.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -4,7 +4,7 @@ def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
     return [s for s in strings if s.startswith(prefix)]
 
 if __name__ == "__main__":
-    input_list = input("Enter space-separated string: ").split()
+    input_list = input("""Enter space-separated string: """).split()
     while True:
         prefix = input("Enter prefix: ")
         if prefix:
