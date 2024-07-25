2836653156f431b201d75e18728b1914db486119
beep boop <githubbot@vadim.me>
2024-07-25 06:45:43 +0200

    Solution 3 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/78h3qo1h Prompt: Complete the following code given the task description and function signature.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,6 +1,7 @@
 vector<int> unique(vector<int> l){
-    vector<int> result = l;
-    sort(result.begin(), result.end());
-    result.erase(unique(result.begin(), result.end()), result.end());
+    vector<int> result(l.begin(),l.end());
+    sort(result.begin(),result.end());
+    auto end = unique(result.begin(),result.end());
+    result.erase(end,result.end());
     return result;
 }
