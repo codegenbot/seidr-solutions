63c0e54e2bb74b039452e3656602b3a5cc084e38
beep boop <githubbot@vadim.me>
2024-07-25 17:59:10 +0200

    Solution 55 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gqa9ys7i Prompt: Complete the following code given the task description and function signature.

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -4,14 +4,15 @@ string result = "";
         if (c == ' ') {
             count++;
             if (count > 2) {
+                result.pop_back();
+                result.pop_back();
                 result += "-";
-                count = 1;
             } else {
                 result += '_';
             }
         } else {
-            result += c;
             count = 0;
+            result += c;
         }
     }
     return result;
