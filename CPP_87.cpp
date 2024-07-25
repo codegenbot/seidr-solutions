1241905fdf1a0fb6d9c9abbc7b1ee7b7bb0059fd
beep boop <githubbot@vadim.me>
2024-07-25 04:53:03 +0200

    Solution 42 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:17: 76338ae4-6f26-473c-9024-d28dc80a1961.cpp: In function ‘int main()’: 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,13 +1,12 @@
-```cpp
 #include <vector>
 #include <iostream>
 
 bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i].size() != b[i].size()) return false;
-        for (int j = 0; j < a[i].size(); j++) {
-            if (a[i][j] != b[i][j]) return false;
+    if(a.size() != b.size())return false;
+    for(int i = 0; i<a.size();i++) {
+        if(a[i].size() != b[i].size())return false;
+        for(int j = 0; j<a[i].size();j++) {
+            if(a[i][j] != b[i][j])return false;
         }
     }
     return true;
@@ -15,14 +14,14 @@ bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
 
 std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
     std::vector<std::vector<int>> result;
-    for (int i = 0; i < lst.size(); i++) {
-        if (x == 1) {
-            for (int j = lst[i].size() - 1; j >= 0; j--) {
-                result.push_back({i, j});
+    for(int i = 0; i<lst.size();i++) {
+        if(x == 1) {
+            for(int j = lst[i].size() - 1;j>=0;j--) {
+                result.push_back({i,j});
             }
         } else {
-            for (int j = 0; j < lst[i].size(); j++) {
-                result.push_back({i, j});
+            for(int j = 0;j<lst[i].size();j++) {
+                result.push_back({i,j});
             }
         }
     }
@@ -30,6 +29,6 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) 
 }
 
 int main() {
-    assert(issame(get_row({{},{1},{1,2,3}}, 3), {{2,2}}));
+    assert(issame(get_row({},{1},{1,2,3}),{{2,2}}));
     return 0;
 }
