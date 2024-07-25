373afc825df76ce6cb05cc3409eb2a6bf954fa17
beep boop <githubbot@vadim.me>
2024-07-25 06:23:34 +0200

    Solution 17 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4xzmx2u7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce263c6d-2635-4ce8-a08d-705b0629b 566.py", line 10, in <module>     check(rolling_max)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce263c6d-2635-4ce8-a08d-705b0629b 566.py", line 7, in check     assert rolling_max([3, 2, 3, 100, 3]) == [3, 3, 3, 100, 100]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ce263c6d-2635-4ce8-a08d-705b0629b 566.py", line 4, in rolling_max     max_values.append(max(numbers[i], numbers[i+1], numbers[i+2]))                                                     ~~~~~~~^^^^^ IndexError: list index out of range

--- Python_9.py
+++ Python_9.py
@@ -2,5 +2,4 @@ def rolling_max(numbers):
     max_values = []
     for i in range(len(numbers) - 2):
         max_values.append(max(numbers[i], numbers[i+1], numbers[i+2]))
-    max_values.extend(numbers[-2:]) # include the last two elements as it is
     return max_values
