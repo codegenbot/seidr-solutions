f64022df92ff229a34e4489df03a1cda77b05ba2
beep boop <githubbot@vadim.me>
2024-07-25 02:03:18 +0200

    Solution 23 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/c7fceeas Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/886eb47e-1f76-4784-b6ee-5667ec5f9 92c.py", line 7     return new_s     ^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_10.py
+++ Python_10.py
@@ -1,4 +1,11 @@
-```Python
-def make_palindrome(s: str) -> str:
-    s = s.lower()
-    return s + s[::-1]
+```
+def solve_task(s):
+    palindrome = "".join(c for c in s if c.isalpha()) + "".join(
+        c for c in s[-1::-1] if c.isalpha()
+    )
+    new_s = ""
+    for char in palindrome:
+        new_s += char * 2
+    return new_s
+
+print(solve_task("hello"))
