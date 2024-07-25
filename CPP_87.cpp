f4d164072c9e38717f3325e553894a78c1b2e393
beep boop <githubbot@vadim.me>
2024-07-25 05:32:12 +0200

    Solution 80 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |     ^~~~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | c9a42ac9-ce53-4983-90d3-c73091640876.cpp: At global scope: c9a42ac9-ce53-4983-90d3-c73091640876.cpp:39:6: error: redefinition of ‘bool issa me(std::vector<std::vector<int> >, std::vector<std::vector<int> >)’    39 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |      ^~~~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:6:6: note: ‘bool issame(std::vector<std ::vector<int> >, std::vector<std::vector<int> >)’ previously defined here     6 | bool issame(vector<vector<int>> a, vector<vector<int>> b) {       |      ^~~~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ c9a42ac9-ce53-4983-90d3-c73091640876.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -32,6 +32,5 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
 }
 
 int main() {
-    assert(issame(get_row({},1,1,2,3), 3) == {{2,2}});
+    assert(issame(get_row({}, 1), 3) == {{2,2}});
     return 0;
-}
