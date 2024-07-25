8873743624cf756f3163ef39e833f2930f18f15a
beep boop <githubbot@vadim.me>
2024-07-25 19:18:47 +0200

    Solution 51 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7mpsnsqw Prompt:       |                    ^~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:12: 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp: In function ‘int main()’: 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-void issame(std::vector<int> l1, std::vector<int> l2) {
+void issame(std::vector<int>& l1, std::vector<int>& l2) {
     std::sort(l1.begin(), l1.end());
     std::sort(l2.begin(), l2.end());
 
@@ -9,17 +9,4 @@ void issame(std::vector<int> l1, std::vector<int> l2) {
     std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
 
     result.erase(std::unique(result.begin(), result.end()), result.end());
-
-    for (int num : result) {
-        std::cout << num << " ";
-    }
-}
-
-int main() {
-    std::vector<int> l1 = {1, 2, 3, 4, 5};
-    std::vector<int> l2 = {3, 4, 5, 6, 7};
-
-    issame(l1, l2);
-
-    return 0;
 }
