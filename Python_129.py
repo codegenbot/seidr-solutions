9bd0075b83197b274993974af9ef62630bf648fe
beep boop <githubbot@vadim.me>
2024-07-25 10:49:37 +0200

    Solution 84 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/wud8cdrq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcb8447a-d7cf-48fe-9464-20830878d 1fa.py", line 35, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dcb8447a-d7cf-48fe-9464-20830878d 1fa.py", line 32, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -14,7 +14,7 @@ def minPath(grid, k):
             new_pos = path[-1] + di * n + dj
             x, y = new_pos // n, new_pos % n
             if 0 <= x < n and 0 <= y < n and new_pos not in path and (x, y) not in visited:
-                if grid[x][y] <= min_val:
+                if grid[x][y] <= min_val and grid[x][y] <= start + k:
                     next_move = new_pos
                     min_val = grid[x][y]
 
