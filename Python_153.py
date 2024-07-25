9e4ef42cf289e58c6f399a84bd3721e90b5660cf
beep boop <githubbot@vadim.me>
2024-07-25 10:46:02 +0200

    Solution 1 of Python/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9kud0e8r Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55133441-bb1f-4dd5-8aa5-9928766eb 785.py", line 13, in <module>     check(Strongest_Extension)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55133441-bb1f-4dd5-8aa5-9928766eb 785.py", line 10, in check     assert Strongest_Extension("Sp", ["671235", "Bb"]) == "Sp.671235"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_153.py
+++ Python_153.py
@@ -2,5 +2,7 @@ def Strongest_Extension(class_name, extensions):
     def strength(extension):
         return sum(1 for char in extension if char.isupper()) - sum(1 for char in extension if char.islower())
 
-    strongest_extension = max(extensions, key=lambda x: (strength(x), extensions.index(x)))
+    sorted_extensions = sorted(extensions, key=lambda x: (strength(x), extensions.index(x)))
+    strongest_extension = sorted_extensions[-1]
+
     return f"{class_name}.{strongest_extension}"
