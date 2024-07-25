7d4934db048350497f8be62f4722b74cc934148f
beep boop <githubbot@vadim.me>
2024-07-25 07:26:16 +0200

    Solution 87 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nwokb2ad Prompt:       |                                  ^~~ fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp:31:39: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<int> a,vector<int>b){       |                                       ^ fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp:30: fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp: In function ‘int main()’: fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp:40:20: error: ‘generate_integers’ was n ot declared in this scope    40 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ fb9e4b73-a042-41f0-9e24-668785ff33a9.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -23,6 +23,6 @@ public:
 };
 
 int main() {
-    assert(Solution::is_same(Solution::generate_integers(17, 89), Solution::filter_even_numbers(Solution::generate_integers(17, 89))));
+    assert(Solution::is_same(Solution::generate_integers(17, 89), Solution::filter_even_numbers(Solution::generate_integers(17, 89)));
     return 0;
 }
