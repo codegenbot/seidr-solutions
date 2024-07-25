f0b5298118f14a1dc72567b3b4a204a9e08c54ad
beep boop <githubbot@vadim.me>
2024-07-25 05:04:55 +0200

    Solution 52 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                           ^~~ 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp:28:35: error: ‘vector’ was not declared  in this scope    28 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                   ^~~~~~ 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp:28:42: error: ‘vector’ was not declared  in this scope    28 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp:28:49: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ In file included from 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp:27: 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp: In function ‘int main()’: 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp:40:20: error: ‘get_row’ was not declare d in this scope    40 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 4a19b933-69ec-48fb-8c7d-1fe3ec8ba4f1.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,18 +1,42 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
-    std::vector<std::vector<int>> result;
+using namespace std;
+
+bool compare(vector<int> v1, vector<int> v2) {
+    if (v1.size() != v2.size())
+        return false;
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i])
+            return false;
+    }
+    return true;
+}
+
+vector<vector<int>> get_row(vector<vector<int>>& lst, int x) {
+    vector<vector<int>> result;
     for (int i = 0; i < lst.size(); i++) {
         if (x == 1) {
-            result.push_back(lst[i]);
-        } else if (x == 2) {
-            vector<int> row;
-            for (int j = 0; j < lst[0].size(); j++) {
-                row.push_back(lst[i][j]);
+            for (int j = lst[i].size() - 1; j >= 0; j--) {
+                result.push_back({i, j});
             }
-            result.push_back(row);
+        } else {
+            for (int j = 0; j < lst[i].size(); j++) {
+                result.push_back({i, j});
+            }
         }
     }
     return result;
+}
+
+int main() {
+    vector<vector<int>> v1 = {{1, 2}, {3, 4}};
+    vector<vector<int>> v2 = {{3, 4}, {1, 2}};
+    assert(compare(v1, v2));
+    
+    vector<vector<int>> lst;
+    int x = 1;
+    vector<vector<int>> result = get_row(lst, x);
+    return 0;
 }
