deb49c20144321bb43b76de584c2c70369d90a4f
beep boop <githubbot@vadim.me>
2024-07-25 09:09:18 +0200

    Solution 1 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/761rt4ln Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,45 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
-    int minDiff = INT_MAX;
-    int cutIndex = 0;
-    for (int i = 1; i < nums.size(); ++i) {
-        int diff = abs(nums[i] - nums[0]);
-        if (diff <= minDiff) {
-            minDiff = diff;
-            cutIndex = i;
+pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int min_diff = INT_MAX;
+    pair<vector<int>, vector<int>> result;
+    
+    for (int i = 1; i <= v.size(); i++) {
+        int left_sum = 0, right_sum = 0;
+        
+        for (int j = 0; j < i; j++) {
+            left_sum += v[j];
+        }
+        
+        for (int j = i; j < v.size(); j++) {
+            right_sum += v[j];
+        }
+        
+        int diff = abs(left_sum - right_sum);
+        
+        if (diff < min_diff) {
+            min_diff = diff;
+            result = {{v.begin(), v.begin() + i}, {v.begin() + i, v.end()}};
         }
     }
-    return {{nums.begin(), nums.begin() + cutIndex}, {nums.begin() + cutIndex, nums.end()}};
+    
+    return result;
 }
 
 int main() {
     int n;
     cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
-    }
-    pair<vector<int>, vector<int>> result = cutVector(nums);
+    vector<int> v(n);
+    for (auto &x : v) cin >> x;
+
+    pair<vector<int>, vector<int>> res = cutVector(v);
+
     cout << "[";
-    for (int num : result.first) {
-        cout << num << " ";
-    }
-    cout << "] [" << "[";
-    for (int num : result.second) {
-        cout << num << " ";
-    }
-    cout << "]";
+    for (const auto &x : res.first) cout << x << " ";
+    cout << "]\n[";
+    for (const auto &x : res.second) cout << x << " ";
+    cout << "]\n";
+
     return 0;
 }
