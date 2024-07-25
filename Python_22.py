d7002ec04299f0111a5c90277b84007788d791e5
beep boop <githubbot@vadim.me>
2024-07-25 03:19:33 +0200

    Solution 33 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/q2oxtv4a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c15910f6-05af-48b6-b4e2-e9797d80f 95d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,17 +1,16 @@
-```
 from typing import List
 
 
 def filter_integers() -> List[int]:
     values = []
-    while True:
+    while len(input("Are you done? (yes/no): ").lower()) != 'yes':
         try:
-            input_val = input(
+            value = input(
                 "Enter an integer (space-separated) or type 'done' to finish: "
-            )
-            if input_val.lower() == "done":
+            ).split()
+            if value[0].lower() == "done":
                 break
-            values.extend([int(i) for i in input_val.split()])
+            values.extend([int(i) for i in value])
         except ValueError:
             print("Invalid input. Please enter integers separated by spaces or 'done'.")
     return values
