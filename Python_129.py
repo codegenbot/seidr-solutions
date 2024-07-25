7f299bd4df41b8835ccb270b7180a77244d138c5
beep boop <githubbot@vadim.me>
2024-07-25 08:02:59 +0200

    Solution 53 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fnrxux41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e30280dd-27d9-4bf1-bba9-e0e9ae13d c6d.py", line 1     print(sorted(['{}, {}'.format(*path) for _, path in sorted((sum(path), path)  for path in min_paths[:k]))])                            ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- Python_129.py
+++ Python_129.py
@@ -1,6 +1,1 @@
-```
-k = 3
-min_paths = [[1, 2], [2, 3], [3, 4], [4, 5]]
-
-result = sorted([f"{path[1]}, {path[0]}" for _, path in sorted((sum(path), path) for path in min_paths[:k])])
-print(result)
+print(', '.join(['{}, {}'.format(*path) for path in sorted(min_paths[:k], key=sum)])
