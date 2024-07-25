8f5a883d28bce94339bfa75ac9c42124de87209c
beep boop <githubbot@vadim.me>
2024-07-25 09:43:32 +0200

    Solution 80 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt:       | ^~~~~~~~~~~~~ d487d10c-8209-445c-97eb-a8bef28a5a98.cpp:22:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    22 | int main(){       |         ^~ d487d10c-8209-445c-97eb-a8bef28a5a98.cpp:22:9: note: remove parentheses to defau lt-initialize a variable    22 | int main(){       |         ^~       |         -- d487d10c-8209-445c-97eb-a8bef28a5a98.cpp:22:9: note: or replace parentheses with  braces to value-initialize a variable d487d10c-8209-445c-97eb-a8bef28a5a98.cpp:22:11: error: a function-definition is not allowed here before ‘{’ token    22 | int main(){       |           ^ d487d10c-8209-445c-97eb-a8bef28a5a98.cpp:24:2: error: expected ‘}’ at end of inp ut    24 | }       |  ^ d487d10c-8209-445c-97eb-a8bef28a5a98.cpp:13:12: note: to match this ‘{’    13 | int main() {       |            ^

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,7 +1,21 @@
-int main {
-    string input;
+#include <string>
+#include <iostream>
+using namespace std;
+
+string make_palindrome(string input) {
+    string reversed = "";
+    for(int i=input.length()-1; i>=0; i--) {
+        reversed += input.substr(i, 1);
+    }
+    return input + reversed;
+}
+
+int main() 
+{
+    string input, output;
     cout << "Enter a string: ";
     cin >> input;
-    string output = make_palindrome(input);
+    output = make_palindrome(input);
     cout << "Palindrome of the given string is: " << output << endl;
+    return 0;
 }
