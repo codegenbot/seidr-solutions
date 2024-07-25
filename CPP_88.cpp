d496097ca83fe8cef92c64f93da338dea3ea501c
beep boop <githubbot@vadim.me>
2024-07-25 04:37:09 +0200

    Solution 28 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nba6qfus Prompt:       |                    ^~~ e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp:11: e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp: In function ‘int main()’: e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp:21:20: error: ‘sort_array’ was not decl ared in this scope    21 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ e3d57d85-a750-4c1f-8397-cb146862d1dd.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,13 +1,27 @@
-```cpp
 #include <vector>
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+#include <algorithm>
+using namespace std;
+
+bool issame(vector<int> a,vector<int>b){
+    if(a.size() != b.size())
+        return false;
+    for(int i=0; i<a.size();i++){
+        if(a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
-vector<int> sort_array(vector<int> array){
-    int sum = array[0] + array[array.size()-1];
-    if(sum % 2 == 0)
-        return vector<int>(array.begin(), array.end());
-    else
-        return vector<int>(array.rbegin(), array.rend());
+vector<int> sort_array(vector<int> array) {
+    int sum = accumulate(array.begin(), array.end(), 0);
+    if (sum % 2 == 1) {
+        sort(array.begin(), array.end());
+    } else {
+        sort(array.rbegin(), array.rend());
+    }
+    return array;
+}
+
+int main() {
+     assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
 }
