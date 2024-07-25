bf70d3ab380fa0f67643537eafc0ef693d6632f6
beep boop <githubbot@vadim.me>
2024-07-25 05:55:23 +0200

    Solution 76 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17485a35-76fc-446f-918e-ce966763e 070.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/17485a35-76fc-446f-918e-ce966763e 070.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: 'NoneType' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,11 @@
+```
 def sort_third(lst):
     result = []
     if len(lst) % 3 != 0:
-        result = []
+        return [result]
     while len(lst) > 0:
         lst = [list(map(int, i.split(','))) for i in [i for i in (str(j) for j in lst) if isinstance(j, int)]]
         temp = [int(i[2]) for i in sorted(lst)]
         del lst[:]
         result.append(temp)
-    if len(result) < (len(lst) // 3):
-        result.append([])
     return tuple(result)
