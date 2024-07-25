24638dbe083e36a61f7df4fa161d4a38c370ff2a
beep boop <githubbot@vadim.me>
2024-07-25 02:59:55 +0200

    Solution 25 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ugkwvgh2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/af777e9e-feda-4ab4-ad4b-a1270c939 d0c.py", line 20, in <module>     check(rescale_to_unit)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/af777e9e-feda-4ab4-ad4b-a1270c939 d0c.py", line 11, in check     assert rescale_to_unit([12.0, 11.0, 15.0, 13.0, 14.0]) == [            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: rescale_to_unit() takes 0 positional arguments but 1 was given

--- Python_21.py
+++ Python_21.py
@@ -1,8 +1,12 @@
-````
-def rescale_to_unit():
+def rescale_to_unit(input_numbers=None):
     while True:
         numbers = input("Enter space-separated float numbers: ")
         try:
-            return [(float(x) - min(map(float, numbers.split()))) / (max(map(float, numbers.split())) - min(map(float, numbers.split()))) for x in numbers.split()]
+            result = [(float(x) - min(map(float, numbers.split()))) / (max(map(float, numbers.split())) - min(map(float, numbers.split())))
+                      for x in numbers.split()]
+            if input_numbers is None:
+                return result
+            else:
+                return [i for i in result if i not in input_numbers]
         except ValueError:
             print("Invalid input. Try again!")
