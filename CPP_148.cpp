24f53dcc4693d790c90b3fcac667003870810f85
beep boop <githubbot@vadim.me>
2024-07-25 06:23:32 +0200

    Solution 24 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vx29nlf6 Prompt: 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp:45:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    45 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp:45:45: error: expression list treated a s compound expression in initializer [-fpermissive]    45 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~ In file included from 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp:44: 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp: In function ‘int main()’: 29620297-a4ae-4b57-b8c2-f3ef851c54ae.cpp:54:19: error: ‘issame’ cannot be used a s a function    54 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -4,15 +4,7 @@
 #include <algorithm>
 
 bool is_same(const std::vector<std::string>& a, const std::vector<std::string>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 std::vector<std::string> bf(std::string planet1, std::string planet2) {
