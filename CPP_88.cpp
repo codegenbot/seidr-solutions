9524a416e6670874f4488eae36303d11109b7042
beep boop <githubbot@vadim.me>
2024-07-25 04:25:21 +0200

    Solution 20 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nba6qfus Prompt:       |                    ^~~ 67f26c69-1364-459d-9b71-33b91e281354.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 67f26c69-1364-459d-9b71-33b91e281354.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 67f26c69-1364-459d-9b71-33b91e281354.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 67f26c69-1364-459d-9b71-33b91e281354.cpp:9: 67f26c69-1364-459d-9b71-33b91e281354.cpp: In function ‘int main()’: 67f26c69-1364-459d-9b71-33b91e281354.cpp:19:20: error: ‘sort_array’ was not decl ared in this scope    19 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 67f26c69-1364-459d-9b71-33b91e281354.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,12 +1,9 @@
-#include <algorithm>
 #include <vector>
 
-std::vector<int> &sort_array(std::vector<int>& array) {
-    int sum = array[0] + array[array.size() - 1];
-    if (sum % 2 == 1) {
-        std::sort(array.begin(), array.end());
-    } else {
-        std::sort(array.rbegin(), array.rend());
-    }
-    return array;
+std::vector<int> sort_array(std::vector<int> array){
+    int sum = array[0] + array[array.size()-1];
+    if(sum % 2 == 0)
+        return array;
+    else
+        return vector<int>(array.rbegin(), array.rend());
 }
