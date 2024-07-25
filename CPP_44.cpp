6a7e809451f162eb66aa8948b9e9948efbb22d33
beep boop <githubbot@vadim.me>
2024-07-25 09:21:03 +0200

    Solution 2 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/qe6ebqu3 Prompt: 2b8bad6e-eddc-4792-9a64-2794ce14f10b.cpp: In function ‘std::string change_base(i nt, int)’: 2b8bad6e-eddc-4792-9a64-2794ce14f10b.cpp:16:5: error: ‘reverse’ was not declared  in this scope    16 |     reverse(res.begin(), res.end());       |     ^~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,15 +1,18 @@
-#include<string>
+#include <algorithm>
+
 using namespace std;
 
 string change_base(int x, int base) {
     string res = "";
     while (x > 0) {
-        int r = x % base;
-        if (r < 10)
-            res = to_string(r) + res;
-        else
-            res = 'A' + (r - 10) + res;
+        int rem = x % base;
+        if (rem >= 10) {
+            res += ((char)(48 + rem));
+        } else {
+            res += ((char)(48 + rem));
+        }
         x /= base;
     }
+    std::reverse(res.begin(), res.end());
     return res;
 }
