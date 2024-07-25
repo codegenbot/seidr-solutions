f27604d6332546412c27c55d68c3beeca74b48c3
beep boop <githubbot@vadim.me>
2024-07-25 04:01:08 +0200

    Solution 8 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/heghcwjh Prompt:                  from e0641e54-31ab-45c2-81a7-200995be2519.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from e0641e54-31ab-45c2-81a7-200995be2519.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e0641e54-31ab-45c2-81a7-200995be2519.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e0641e54-31ab-45c2-81a7-200995be2519.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ e0641e54-31ab-45c2-81a7-200995be2519.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -19,9 +19,8 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
     return result;
 }
 
-#define MAINFUN_CPP_70
 int main() {
-    assert (issame(strange_sort_list({111111}) , {111111}));
+    assert(issame(strange_sort_list({111111}), {111111}));
     
     return 0;
 }
