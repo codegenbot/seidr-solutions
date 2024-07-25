fb299e812d4bbdd0be13b93d91fe1a1e6a0c29d9
beep boop <githubbot@vadim.me>
2024-07-25 06:04:29 +0200

    Solution 16 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4swgnmk1 Prompt:       |                         ^~~~~~ 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp:23: 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp: In function ‘int main()’: 9684f20b-71c1-442e-b1fe-9a8b7a2e1afc.cpp:25:32: error: ‘Strongest_Extension’ can not be used as a function    25 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,10 +1,10 @@
-#include <vector>
+#include <iostream>
 #include <string>
-using namespace std;
+#include <vector>
 
-int Strongest_Extension(string class_name,vector<string> extensions){
+int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
     int max_strength = 0;
-    string strongest_extension;
+    std::string strongest_extension;
 
     for (const auto& extension : extensions) {
         int cap = 0, sm = 0;
