22d078e3f43b6cadf9e8227cb475f14a5d0be56b
beep boop <githubbot@vadim.me>
2024-07-25 07:40:26 +0200

    Solution 22 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/wce3dxni Prompt:       |                    ^~~~~ 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp:11: 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp: In function ‘int main()’: 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 5e5b9af9-ea0e-4600-a9ac-fc00bcc268f2.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-std::vector<float> get_positive(std::vector<float> l){
+std::vector<float> get_positive(std::vector<float> l) {
     std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
@@ -11,10 +11,10 @@ std::vector<float> get_positive(std::vector<float> l){
     return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b){
+bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
 
 int main() {
-    assert (issame(get_positive({}) , {}));
+    assert(issame(get_positive({}), {}));
 }
