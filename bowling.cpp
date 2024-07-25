b0d2c3004451102150e9a76b69fbb9ca6a032b06
beep boop <githubbot@vadim.me>
2024-07-25 03:13:34 +0200

    Solution 83 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/9vlt1woe Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,29 +1,50 @@
 int bowlingScore(string s) {
     int score = 0;
-    int roll1, roll2;
-
-    for (int i = 0; i < 10; i++) {
-        if (s[i] == 'X') { // strike
-            score += 10 + 30;
-            continue;
-        }
-
-        if (s[i] == '/') { // spare
-            score += 10 + s[i+1] - '0';
-            i++;
-            continue;
-        }
-
-        roll1 = s[i] - '0';
-        roll2 = s[i+1] - '0';
-
-        if (roll1 + roll2 == 10) { // spare
-            score += 10;
-            i++;
+    int roll = 0;
+    vector<int> rolls(21);
+    
+    for(int i=0; i<s.size(); i++) {
+        if(s[i] == '/') {
+            if(roll < 2) {
+                int frame;
+                try{
+                    frame = stoi(s.substr(i-1, i));
+                    i++;
+                }catch(...){
+                    frame = 10;
+                }
+                
+                for(int j=0; j<frame; j++) {
+                    rolls[roll] += 10;
+                    roll++;
+                }
+            }
+        } else if(s[i] == 'X') {
+            rolls[roll] = 10;
+            roll +=1;
         } else {
-            score += roll1 + roll2;
+            int frameScore = 0;
+            try{
+                frameScore = stoi(s.substr(i, s.find('/', i)-i));
+                i = s.find('/', i);
+            }catch(...){
+                frameScore = 10 -stoi(s.substr(i-1, i-1));
+                i++;
+            }
+            
+            if(frameScore < 10) {
+                rolls[roll] = frameScore + stoi(s.substr(i, 1));
+                roll +=2;
+            } else {
+                rolls[roll] = frameScore;
+                roll +=1;
+            }
         }
     }
-
+    
+    for(int i=0; i<21; i++) {
+        score += rolls[i];
+    }
+    
     return score;
 }
