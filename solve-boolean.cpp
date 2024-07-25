50c0e99b59d03b5291204996c8c6b41860976af2
beep boop <githubbot@vadim.me>
2024-07-25 06:05:29 +0200

    Solution 33 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/y6oyyquw Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,69 +2,26 @@
 using namespace std;
 
 bool solveBoolean(string s) {
-    if(s == "T" || s == "true") return true;
-    if(s == "F" || s == "false") return false;
-    
-    int i = 0, j = 0, k = 0;
-    while(i < s.length()) {
-        if(s[i] == '&') {
-            j = i + 1;
-            break;
-        }
-        i++;
-    }
-    while(j < s.length()) {
-        if(s[j] == '|') {
-            k = j;
-            break;
-        }
-        j++;
-    }
-    
-    string op1 = s.substr(0, i);
-    string op2 = s.substr(i + 1, k - i - 1);
-    string operand = s.substr(k + 1);
-    
-    if(op1 == "T" || op1 == "true") {
-        op1 = "True";
-    }
-    else if(op1 == "F" || op1 == "false") {
-        op1 = "False";
-    }
-    
-    if(op2 == "T" || op2 == "true") {
-        op2 = "True";
-    }
-    else if(op2 == "F" || op2 == "false") {
-        op2 = "False";
-    }
-    
-    if(operand == "T" || operand == "true") {
-        operand = "True";
-    }
-    else if(operand == "F" || operand == "false") {
-        operand = "False";
-    }
-    
-    if(op1 == "True" && op2 == "True")
-        return true;
-    else if(op1 == "True" && op2 == "False")
-        return false;
-    else if(op1 == "False" && op2 == "True")
-        return false;
-    else if(op1 == "False" && op2 == "False")
-        return true;
-    
-    if(operand == "True") {
-        if(op1 == "True" || op2 == "True")
-            return true;
-        else
+    bool result = true;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'F') {
             return false;
+        }
+        if (s[i] == '&') {
+            result &= (i + 1 < s.length() && s[i + 1] != 'F');
+            i++;
+        }
+        if (s[i] == '|') {
+            result |= (i + 1 < s.length() && s[i + 1] != 'F');
+            i++;
+        }
     }
-    else {
-        if(op1 == "True" || op2 == "True")
-            return false;
-        else
-            return true;
-    }
+    return result;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << (solveBoolean(s) ? "True" : "False");
+    return 0;
 }
