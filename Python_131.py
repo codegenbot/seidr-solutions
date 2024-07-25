02d5c155a2ff6e3bed8b8b59210e19aa6a0f2fdf
beep boop <githubbot@vadim.me>
2024-07-25 08:00:04 +0200

    Solution 32 of Python/131, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/1wn453gt Prompt: 2 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6566f129-9432-4981-9829-a0463e868 ec5.py", line 16, in <module>     check(digits)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6566f129-9432-4981-9829-a0463e868 ec5.py", line 13, in check     assert digits(2468) == 0            ^^^^^^^^^^^^^^^^^ AssertionError

--- Python_131.py
+++ Python_131.py
@@ -3,7 +3,6 @@ def digits(n):
     for digit in str(n):
         if int(digit) % 2 == 0:
             count_even_digits += 1
-    if count_even_digits > 0:
-        return "Even"
-    else:
-        return "Odd"
+    return str(count_even_digits)
+
+print(digits(12345))
