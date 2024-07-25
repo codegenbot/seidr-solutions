b113456662c2be6179600ec28a57c2f79faa2fd7
beep boop <githubbot@vadim.me>
2024-07-25 06:26:37 +0200

    Solution 10 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85c280ce-a830-4112-bc83-8cdd99af0 53b.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85c280ce-a830-4112-bc83-8cdd99af0 53b.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 def sort_third(l: list):
-    remainder = 3 - len(l) % 3
-    l += [float('inf')] * remainder if remainder != 3 else []
-    return [sorted(l[i:i+3]) for i in range(0, len(l), 3)]
+    remainder = len(l) % 3
+    l += [float('inf')] * (3 - remainder) if remainder != 0 else []
+    return [sorted(sublist)[:3] for sublist in [l[i:i+3] for i in range(0, len(l), 3]]
