9a096c8ae1778750046b4f895e552ff053d2261a
beep boop <githubbot@vadim.me>
2024-07-25 07:16:22 +0200

    Solution 65 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Program did not receive expected input

--- Python_67.py
+++ Python_67.py
@@ -1,19 +1,7 @@
 import re
-
-
 def fruit_distribution(s):
     s = s.lower()
-    if "and" in s:
-        parts = []
-        for x in re.split(r"(\d+|\band\))", s):
-            if x.isdigit() or x == "and":
-                parts.append(x)
+    if re.search("and", s):
+        return "mixed"
     else:
-        parts = [x for x in re.split(r"\d+", s)]
-    ones = s.count("1")
-    zeros = s.count("0")
-    total_count = 0
-    for i in range(len(parts)):
-        if parts[i].isdigit():
-            total_count += int(parts[i])
-    return max(0, 5 - total_count - ones - zeros)
+        return "unknown"
