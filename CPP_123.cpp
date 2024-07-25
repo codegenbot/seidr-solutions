3a94c7f86d97006b6223c52923a095bcbe7690eb
beep boop <githubbot@vadim.me>
2024-07-25 12:39:49 +0200

    Solution 38 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mwczrhys Prompt:       |                    ^~~ 5e078f63-b726-4a4a-a494-948e6915267c.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5e078f63-b726-4a4a-a494-948e6915267c.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5e078f63-b726-4a4a-a494-948e6915267c.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5e078f63-b726-4a4a-a494-948e6915267c.cpp:13: 5e078f63-b726-4a4a-a494-948e6915267c.cpp: In function ‘int main()’: 5e078f63-b726-4a4a-a494-948e6915267c.cpp:23:20: error: ‘get_odd_collatz’ was not  declared in this scope    23 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 5e078f63-b726-4a4a-a494-948e6915267c.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -14,28 +14,3 @@ vector<int> get_odd_collatz(int n) {
     sort(result.begin(), result.end());
     return result;
 }
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    // Test the function get_odd_collatz()
-    vector<int> test_input = {10, 20, 30, 40};
-    vector<vector<int>> expected_outputs = {{1, 3, 5, 7, 9, 10}, {1, 3, 5, 7, 9, 10, 16, 18, 20}, {1, 3, 5, 7, 9, 10, 16, 18, 20, 22, 28, 30}, {1, 3, 5, 7, 9, 10, 16, 18, 20, 22, 28, 30, 32, 34, 40}};
-
-    for (int i = 0; i < test_input.size(); ++i) {
-        vector<int> result = get_odd_collatz(test_input[i]);
-        assert(issame(result, expected_outputs[i]));
-    }
-
-    return 0;
-}
