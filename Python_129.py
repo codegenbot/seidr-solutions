f3d8657926628aa8d519d83ef5a305723ead9bcb
beep boop <githubbot@vadim.me>
2024-07-25 08:05:19 +0200

    Solution 61 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fnrxux41 Prompt: 1, 2, 3, 4, 5, 6, 7, 8 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1ade80de-82c4-474d-812c-cd0510d50 16d.py", line 15, in <module>     check(minPath)           ^^^^^^^ NameError: name 'minPath' is not defined

--- Python_129.py
+++ Python_129.py
@@ -1,3 +1,8 @@
-```
-minPaths = [[1, 2], [3, 4], [5, 6]]
-print(shortest_paths(minPaths))
+def shortest_paths(min_paths):
+    k = len(min_paths)
+    sorted_paths = sorted(min_paths[:k], key=sum)
+    return ", ".join(["{}, {}".format(*path) for path in sorted_paths])
+
+input_list = [(1, 2), (3, 4), (5, 6), (7, 8)] 
+output = shortest_paths(input_list)
+print(output)
