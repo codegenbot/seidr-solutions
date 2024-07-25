a24360fac0f8b94516766df7fc8cdf6857f93c64
beep boop <githubbot@vadim.me>
2024-07-25 09:35:07 +0200

    Solution 31 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/z4p2p0d9 Prompt: ipher(std::string, std::string, std::string)’: 32bbbdc9-4494-40ed-868a-021a39da864a.cpp:4:5: error: ‘string’ was not declared i n this scope     4 |     string decipheredMessage;       |     ^~~~~~ 32bbbdc9-4494-40ed-868a-021a39da864a.cpp:4:5: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 32bbbdc9-4494-40ed-868a-021a39da864a.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 32bbbdc9-4494-40ed-868a-021a39da864a.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 32bbbdc9-4494-40ed-868a-021a39da864a.cpp:9:17: error: ‘decipheredMessage’ was no t declared in this scope     9 |                 decipheredMessage += cipherKey2[index];       |                 ^~~~~~~~~~~~~~~~~ 32bbbdc9-4494-40ed-868a-021a39da864a.cpp:15:12: error: ‘decipheredMessage’ was n ot declared in this scope    15 |     return decipheredMessage;       |            ^~~~~~~~~~~~~~~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -3,7 +3,7 @@
 std::string substitutionCipher(std::string cipherKey1, std::string cipherKey2, std::string message) {
     std::string decipheredMessage;
     for (int i = 0; i < message.length(); i++) {
-        for (int j = 0; j < cipherKey1.length(); j++) {
+        for (int j = 0; j < cipherKey2.length(); j++) {
             if (message[i] == cipherKey1[j]) {
                 decipheredMessage += cipherKey2[j];
                 break;
