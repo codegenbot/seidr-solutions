a073d0c86f508a176362d48efa956214ab7362bf
beep boop <githubbot@vadim.me>
2024-07-25 20:54:06 +0200

    Solution 21 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jhoh5zna Prompt: b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp:1:14: error: ‘stod’ was not declared in  this scope; did you mean ‘std’?     1 | double num = stod(value);       |              ^~~~       |              std b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp:2:5: error: expected unqualified-id bef ore ‘if’     2 |     if (num < 0) {       |     ^~ b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp:4:7: error: expected unqualified-id bef ore ‘else’     4 |     } else {       |       ^~~~ b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp:9: b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp: In function ‘int main()’: b7e2799a-b39d-4f18-b8ac-d7e63d258e15.cpp:11:13: error: ‘closest_integer’ was not  declared in this scope    11 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-int closest_integer(std::string value) {
+double closest_integer(std::string value) {
     double num = stod(value);
     if (num < 0) {
         return ceil(num - 0.5);
