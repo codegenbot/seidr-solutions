8c553e365f8af4bed1f26fc9acaa5a066bb8aa23
beep boop <githubbot@vadim.me>
2024-07-25 02:59:49 +0200

    Solution 2 of CPP/57, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1f7esfc1 Prompt:     3 |     for (int i = 1; i < l.size(); ++i) {       |     ^~~ d771f756-b6b1-4478-b937-790f5ee78a40.cpp:3:21: error: ‘i’ does not name a type     3 |     for (int i = 1; i < l.size(); ++i) {       |                     ^ d771f756-b6b1-4478-b937-790f5ee78a40.cpp:3:35: error: expected unqualified-id be fore ‘++’ token     3 |     for (int i = 1; i < l.size(); ++i) {       |                                   ^~ d771f756-b6b1-4478-b937-790f5ee78a40.cpp:11:5: error: expected unqualified-id be fore ‘return’    11 |     return increasing || decreasing;       |     ^~~~~~ d771f756-b6b1-4478-b937-790f5ee78a40.cpp:12:1: error: expected declaration befor e ‘}’ token    12 | }       | ^ In file included from d771f756-b6b1-4478-b937-790f5ee78a40.cpp:14: d771f756-b6b1-4478-b937-790f5ee78a40.cpp: In function ‘int main()’: d771f756-b6b1-4478-b937-790f5ee78a40.cpp:16:13: error: ‘monotonic’ was not decla red in this scope    16 |     assert (monotonic({9, 9, 9, 9}) == true);       |             ^~~~~~~~~

--- CPP_57.cpp
+++ CPP_57.cpp
@@ -2,10 +2,10 @@
 #include <vector>
 #include <cassert>
 
-bool monotonic(const std::vector<int>& l) {
+bool monotonic(std::vector<int> l) {
     bool increasing = true;
     bool decreasing = true;
-    for (int i = 1; i < l.size(); ++i) {
+    for (size_t i = 1; i < l.size(); ++i) {
         if (l[i] < l[i - 1]) {
             increasing = false;
         }
@@ -18,5 +18,13 @@ bool monotonic(const std::vector<int>& l) {
 
 int main() {
     assert(monotonic({9, 9, 9, 9}) == true);
+    std::cout << "Input values for l: ";
+    std::vector<int> l;
+    int val;
+    while(std::cin >> val) {
+        l.push_back(val);
+    }
+    std::cout << "Is the sequence monotonic? " << (monotonic(l) ? "true" : "false") << std::endl;
+
     return 0;
 }
