645b8d37d09dc9d116baba5f827b3a6226c37311
beep boop <githubbot@vadim.me>
2024-07-25 06:26:57 +0200

    Solution 86 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt:    34 | } else if ((a.type() == typeid(double)) &&       |   ^~~~ 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:41:3: error: expected unqualified-id be fore ‘else’    41 | } else {       |   ^~~~ In file included from 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:46: 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp: In function ‘int main()’: 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:48:13: error: ‘boost’ has not been decl ared    48 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:48:29: error: ‘string’ was not declared  in this scope    48 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 650c3a0d-85a2-4143-9b88-ddfb9114ba51.cpp:48:37: error: ‘compare_one’ was not dec lared in this scope    48 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <any>
 #include <string>
 #include <type_traits>
@@ -8,85 +7,41 @@ namespace std {
 std::any a;
 std::any b;
 
-auto compare_one(auto& x, auto& y) -> std::any {
-    if (x.type() == typeid(int) && y.type() == typeid(int)) {
-        int X = any_cast<int>(x);
-        int Y = any_cast<int>(y);
-        if (X > Y) return x;
-        else if (X < Y) return y;
-        else return x;
-    } else if (x.type() == typeid(double) && y.type() == typeid(double)) {
-        double X = any_cast<double>(x);
-        double Y = any_cast<double>(y);
-        if (X > Y) return x;
-        else if (X < Y) return y;
-        else return x;
-    } else if ((x.type() == typeid(std::string) || x.type() == typeid(std::wstring)) &&
-               (y.type() == typeid(std::string) || y.type() == typeid(std::wstring))) {
-        std::string X = any_cast<std::string>(x);
-        std::string Y = any_cast<std::string>(y);
-        if (X > Y) return x;
-        else if (X < Y) return y;
-        else return x;
-    } else if ((x.type() == typeid(std::string) || x.type() == typeid(std::wstring)) &&
-               (y.type() == typeid(double))) {
-        std::string X = any_cast<std::string>(x);
-        double Y = any_cast<double>(y);
-        if (std::stod(X) > Y) return x;
-        else if (std::stod(X) < Y) return y;
-        else return x;
-    } else if ((x.type() == typeid(double)) &&
-               (y.type() == typeid(std::string) || y.type() == typeid(std::wstring))) {
-        double X = any_cast<double>(x);
-        std::string Y = any_cast<std::string>(y);
-        if (X > std::stod(Y)) return x;
-        else if (X < std::stod(Y)) return y;
-        else return x;
-    } else {
-        return x;
-    }
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    int x = any_cast<int>(a);
+    int y = any_cast<int>(b);
+    if (x > y) return a;
+    else if (x < y) return b;
+    else return a;
+} else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+    double x = any_cast<double>(a);
+    double y = any_cast<double>(b);
+    if (x > y) return a;
+    else if (x < y) return b;
+    else return a;
+} else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
+           (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
+    std::string x = any_cast<std::string>(a);
+    std::string y = any_cast<std::string>(b);
+    if (x > y) return a;
+    else if (x < y) return b;
+    else return a;
+} else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
+           (b.type() == typeid(double))) {
+    std::string x = any_cast<std::string>(a);
+    double y = any_cast<double>(b);
+    if (std::stod(x) > y) return a;
+    else if (std::stod(x) < y) return b;
+    else return a;
+} else if ((a.type() == typeid(double)) &&
+           (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
+    double x = any_cast<double>(a);
+    std::string y = any_cast<std::string>(b);
+    if (x > std::stod(y)) return a;
+    else if (x < std::stod(y)) return b;
+    else return a;
+} else {
+    return a;
 }
-
-auto compare(auto& a, auto& b) -> std::any {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int X = any_cast<int>(a);
-        int Y = any_cast<int>(b);
-        if (X > Y) return a;
-        else if (X < Y) return b;
-        else return a;
-    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double X = any_cast<double>(a);
-        double Y = any_cast<double>(b);
-        if (X > Y) return a;
-        else if (X < Y) return b;
-        else return a;
-    } else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
-               (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
-        std::string X = any_cast<std::string>(a);
-        std::string Y = any_cast<std::string>(b);
-        if (X > Y) return a;
-        else if (X < Y) return b;
-        else return a;
-    } else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
-               (b.type() == typeid(double))) {
-        std::string X = any_cast<std::string>(a);
-        double Y = any_cast<double>(b);
-        if (std::stod(X) > Y) return a;
-        else if (std::stod(X) < Y) return b;
-        else return a;
-    } else if ((a.type() == typeid(double)) &&
-               (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
-        double X = any_cast<double>(a);
-        std::string Y = any_cast<std::string>(b);
-        if (X > std::stod(Y)) return a;
-        else if (X < std::stod(Y)) return b;
-        else return a;
-    } else if ((a.type() == typeid(std::any)) &&
-               (b.type() == typeid(std::any))) {
-        return compare_one(a, b);
-    } else {
-        return a;
-    }
-}
-
+return b; // Added the missing else
 }
