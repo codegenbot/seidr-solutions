ce6ac8911fd69031485f5038ad76be8ae17211c1
beep boop <githubbot@vadim.me>
2024-07-25 02:54:15 +0200

    Solution 22 of Python/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ugkwvgh2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b72419ef-2b9f-44c9-990e-b4fcddc91 5be.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_21.py
+++ Python_21.py
@@ -1,8 +1,8 @@
-```
 def rescale_to_unit():
     while True:
         numbers = input("Enter space-separated float numbers: ")
         try:
-            return [(float(x) - min(map(float, numbers.split()))) / (max(map(float, numbers.split())) - min(map(float, numbers.split()))) for x in numbers.split()]
+            return_list = [(float(x) - min(map(float, numbers.split()))) / (max(map(float, numbers.split())) - min(map(float, numbers.split()))) for x in numbers.split()]
+            return return_list
         except ValueError:
             print("Invalid input. Try again!")
