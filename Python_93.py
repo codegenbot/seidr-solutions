f3c387c95074bc2d8bbd1b4cee54a6023b0ef0f3
beep boop <githubbot@vadim.me>
2024-07-25 06:52:54 +0200

    Solution 10 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/k1onw7hx Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5b8dd13-5bd8-4d12-93ee-662229c9c 0ba.py", line 20, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5b8dd13-5bd8-4d12-93ee-662229c9c 0ba.py", line 16, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -2,7 +2,12 @@ def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            result += chr((ord(char.lower()) - ord('a') + 2) % 26 + ord('a')) if 'a' <= char and char <= 'z' or char.isupper() else chr((ord(char) - ord('A') + 13) % 26 + ord('A'))
+            if char.lower() == 'a':
+                result += chr((ord(char) - 65 + 2) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + 2) % 26 + 97)
+            elif char.lower() == 'e':
+                result += chr((ord(char) - 69) % 26 + 69) if char.isupper() else chr((ord(char) - 101) % 26 + 101)
+            else:
+                result += chr((ord(char.lower()) - 97 + 13) % 26 + 97).upper() if char.isupper() else chr((ord(char.lower()) - 97 + 13) % 26 + 97)
         else:
             result += char
     return result
