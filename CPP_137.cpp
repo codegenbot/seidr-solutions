4f3b6a4fac4e482517f48cd9cebad3cd0d882ed7
beep boop <githubbot@vadim.me>
2024-07-25 05:28:11 +0200

    Solution 24 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: e");       |             ^~~~~ 1acf9063-052f-450d-b78f-03188ce67911.cpp:27:29: error: ‘string’ was not declared  in this scope    27 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 1acf9063-052f-450d-b78f-03188ce67911.cpp:27:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 1acf9063-052f-450d-b78f-03188ce67911.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 1acf9063-052f-450d-b78f-03188ce67911.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 1acf9063-052f-450d-b78f-03188ce67911.cpp:25: 1acf9063-052f-450d-b78f-03188ce67911.cpp:27:37: error: ‘compare_one’ was not dec lared in this scope    27 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,21 +1,23 @@
 #include <string>
 #include <vector>
-#include <any>
 
-using namespace std;
+using namespace boost;
 
-any compare_one(any a, any b) {
-    if (any_cast<int>(a) > any_cast<int>(b)) {
-        return a;
-    } else if (any_cast<int>(a) < any_cast<int>(b)) {
-        return b;
-    } else if (any_cast<string>(a) > any_cast<string>(b)) {
-        return a;
-    } else if (any_cast<string>(a) < any_cast<string>(b)) {
-        return b;
-    } else if (stod(any_cast<string>(a)) > stod(any_cast<string>(b))) {
-        return a;
-    } else {
-        return boost::any("None");
+boost::any compare_one(boost::any a, boost::any b) {
+    if (is_any_of<a>(int.class)) {
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
+    } else if (is_any_of<a>(double.class)) {
+        double x = boost::any_cast<double>(a);
+        double y = boost::any_cast<double>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any(std::string("None")));
+    } else if (is_any_of<a>(std::string.class)) {
+        std::string x = boost::any_cast<std::string>(a);
+        std::string y = boost::any_cast<std::string>(b);
+        double ax = std::stod(x);
+        double ay = std::stod(y);
+        return (ax > ay) ? a : ((ax < ay) ? b : boost::any(std::string("None")));
     }
+    return a;
 }
