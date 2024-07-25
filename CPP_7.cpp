2ab7b7c231ddb3815f5fb51ea6b59b7acaa38af8
beep boop <githubbot@vadim.me>
2024-07-25 04:41:56 +0200

    Solution 41 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9pn8zd27 Prompt:       |                                     ^~~~~~ 493aa17c-075e-4b8e-9cc9-ef233940f1cc.cpp:28:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 493aa17c-075e-4b8e-9cc9-ef233940f1cc.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 493aa17c-075e-4b8e-9cc9-ef233940f1cc.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 493aa17c-075e-4b8e-9cc9-ef233940f1cc.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 493aa17c-075e-4b8e-9cc9-ef233940f1cc.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 493aa17c-075e-4b8e-9cc9-ef233940f1cc.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -18,8 +18,8 @@ std::vector<std::string> filter_by_substring(const std::vector<std::string>& str
     return result;
 }
 
-int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
+bool main() {
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), std::vector<std::string>{"grunt", "prune"}));
 
     return 0;
 }
