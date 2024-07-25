2f4fded9db0a46ed6947d2e25be9e50e8bfd98a9
beep boop <githubbot@vadim.me>
2024-07-25 17:45:29 +0200

    Solution 31 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/mv4dgipi Prompt: n this scope    21 |         cout << nums[i] << endl;       |                 ^~~~ c1628fb1-f1a3-4adb-9fcc-cce387fbc28a.cpp:21:28: error: ‘endl’ was not declared i n this scope    21 |         cout << nums[i] << endl;       |                            ^~~~ c1628fb1-f1a3-4adb-9fcc-cce387fbc28a.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << 0 << endl;       |     ^~~~ c1628fb1-f1a3-4adb-9fcc-cce387fbc28a.cpp:23:18: error: ‘endl’ was not declared i n this scope    23 |     cout << 0 << endl;       |                  ^~~~ c1628fb1-f1a3-4adb-9fcc-cce387fbc28a.cpp:24:18: error: ‘cut_index’ was not decla red in this scope    24 |     for (int i = cut_index; i < n; ++i) {       |                  ^~~~~~~~~ c1628fb1-f1a3-4adb-9fcc-cce387fbc28a.cpp:25:17: error: ‘nums’ was not declared i n this scope    25 |         cout << nums[i] << endl;       |                 ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -12,19 +12,16 @@ int main() {
     int n;
     cin >> n;
     nums.resize(n);
-    
     for (int i = 0; i < n; ++i) {
         cin >> nums[i];
     }
     
     int min_diff = INT_MAX;
     cut_index = 0;
-    
     for (int i = 1; i < n; ++i) {
         int left_sum = accumulate(nums.begin(), nums.begin() + i, 0);
         int right_sum = accumulate(nums.begin() + i, nums.end(), 0);
         int diff = abs(left_sum - right_sum);
-        
         if (diff < min_diff) {
             min_diff = diff;
             cut_index = i;
@@ -35,7 +32,6 @@ int main() {
         cout << nums[i] << "\n";
     }
     cout << 0 << "\n";
-    
     for (int i = cut_index; i < n; ++i) {
         cout << nums[i] << "\n";
     }
