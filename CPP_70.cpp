ac6d719be6c5962e692d949e377797ad5afbd498
beep boop <githubbot@vadim.me>
2024-07-25 04:16:01 +0200

    Solution 54 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:37:39: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:36: 9138727f-a8da-4091-a29b-b710cf67fd58.cpp: In function ‘int main()’: 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:46:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    46 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -4,37 +4,27 @@
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i)
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) return false;
+    }
     return true;
 }
 
 std::vector<int> strange_sort_vector(std::vector<int> lst) {
     std::vector<int> result;
-    if (lst.empty()) {
-        return result;
-    }
-    
-    sort(lst.begin(), lst.end());
-    result.push_back(*min_element(lst.begin(), lst.end()));
-    lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
-    
-    if (!lst.empty()) {
-        sort(lst.begin(), lst.end());
-        result.push_back(*max_element(lst.begin(), lst.end()));
-        lst.erase(remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
-    }
-    
-    while (!lst.empty() && !issame({*min_element(lst.begin(), lst.end())}, result)) {
-        sort(lst.begin(), lst.end());
-        result.push_back(*min_element(lst.begin(), lst.end()));
-        lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+    while (!lst.empty()) {
+        int min_val = *min_element(lst.begin(), lst.end());
+        int max_val = *max_element(lst.begin(), lst.end());
         
-        if (!lst.empty() && !issame({*max_element(lst.begin(), lst.end())}, &result[result.size()-1])) {
-            sort(lst.begin(), lst.end());
-            result.push_back(*max_element(lst.begin(), lst.end()));
-            lst.erase(remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
+        result.push_back(min_val);
+        lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+        
+        if (issame({min_val, max_val}, {*min_element(lst.begin(), lst.end()), *max_element(lst.begin(), lst.end())})) {
+            break;
         }
+        
+        result.push_back(max_val);
+        lst.erase(remove(lst.begin(), lst.end(), max_val), lst.end());
     }
     
     return result;
