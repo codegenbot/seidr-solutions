95719af38ee8ff20221f0fb35097175cca900630
beep boop <githubbot@vadim.me>
2024-07-25 02:52:32 +0200

    Solution 1 of CPP/55, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/wz9w1yj3 Prompt: Complete the following code given the task description and function signature.

--- CPP_55.cpp
+++ CPP_55.cpp
@@ -1,23 +1,10 @@
-#include <stdio.h>
-
-using namespace std;
-
 int fib(int n) {
-    if (n <= 1)
-        return n;
-    int a = 0, b = 1, result = 0;
-    for (int i = 2; i <= n; i++) {
+    if (n <= 1) return n;
+    int a = 0, b = 1, result = 1;
+    for (int i = 2; i <= n; ++i) {
         result = a + b;
         a = b;
         b = result;
     }
     return result;
-}
-
-int main() {
-    int num;
-    printf("Enter the number: ");
-    scanf("%d", &num);
-    printf("Fibonacci Number is %d\n", fib(num));
-    return 0;
 }
