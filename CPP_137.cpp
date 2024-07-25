6f0893a4fdb769c62e22c4d23d65dabbcff8bd49
beep boop <githubbot@vadim.me>
2024-07-25 06:22:25 +0200

    Solution 80 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:47:29: error: ‘string’ was not declared  in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:47:37: error: ‘compare_one’ was not dec lared in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp: At global scope: dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:48:2: error: expected ‘}’ at end of inp ut    48 | }       |  ^ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:3:15: note: to match this ‘{’     3 | namespace std {       |               ^ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp: In function ‘int std::main()’: dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:48:1: warning: control reaches end of n on-void function [-Wreturn-type]    48 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,35 +1,39 @@
-#include <any>
-#include <boost/any.hpp>
-#include <string>
+std::any a;
+std::any b;
 
-using namespace std;
-using namespace boost;
-
-std::any compare(const std::any& a, const std::any& b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = boost::any_cast<int>(a);
-        int y = boost::any_cast<int>(b);
-        return a.type() == typeid(x) ? a : b;
-    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double x = boost::any_cast<double>(a);
-        double y = boost::any_cast<double>(b);
-        return x > y ? a : b;
-    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-               (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        string x = boost::any_cast<string>(a);
-        string y = boost::any_cast<string>(b);
-        return x > y ? a : b;
-    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-               (b.type() == typeid(double))) {
-        string x = boost::any_cast<string>(a);
-        double y = boost::any_cast<double>(b);
-        return stod(x) > y ? a : b;
-    } else if ((a.type() == typeid(double)) &&
-               (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        double x = boost::any_cast<double>(a);
-        string y = boost::any_cast<string>(b);
-        return x > stod(y) ? a : b;
-    } else {
-        return a;
-    }
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    int x = boost::any_cast<int>(a);
+    int y = boost::any_cast<int>(b);
+    if (x > y) return a;
+    else if (x < y) return b;
+    else return a;
+} else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+    double x = boost::any_cast<double>(a);
+    double y = boost::any_cast<double>(b);
+    if (x > y) return a;
+    else if (x < y) return b;
+    else return a;
+} else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
+           (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
+    std::string x = boost::any_cast<std::string>(a);
+    std::string y = boost::any_cast<std::string>(b);
+    if (x > y) return a;
+    else if (x < y) return b;
+    else return a;
+} else if ((a.type() == typeid(std::string) || a.type() == typeid(std::wstring)) &&
+           (b.type() == typeid(double))) {
+    std::string x = boost::any_cast<std::string>(a);
+    double y = boost::any_cast<double>(b);
+    if (std::stod(x) > y) return a;
+    else if (std::stod(x) < y) return b;
+    else return a;
+} else if ((a.type() == typeid(double)) &&
+           (b.type() == typeid(std::string) || b.type() == typeid(std::wstring))) {
+    double x = boost::any_cast<double>(a);
+    std::string y = boost::any_cast<std::string>(b);
+    if (x > std::stod(y)) return a;
+    else if (x < std::stod(y)) return b;
+    else return a;
+} else {
+    return a;
 }
