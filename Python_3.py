9cde3eede1ca99751658b915deab87235216136b
beep boop <githubbot@vadim.me>
2024-07-25 05:39:23 +0200

    Solution 75 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/n0dgx01d Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/426e9937-12ab-4ca6-b5a3-6e72c83e4 393.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_3.py
+++ Python_3.py
@@ -1,4 +1,4 @@
-```
+"""
 from typing import List
 
 
@@ -49,3 +49,4 @@ def below_zero() -> None:
         cont = input("Continue? (yes/no): ")
         if cont.lower() == "no":
             break
+"""
