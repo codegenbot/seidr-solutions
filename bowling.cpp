06e396001acee97be3f4014a233c63fc0f1647b9
beep boop <githubbot@vadim.me>
2024-07-25 02:02:38 +0200

    Solution 42 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/9vlt1woe Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,47 +1,42 @@
 int bowlingScore(string s) {
     int score = 0;
-    int currentFrame = 1;
-    int rollsLeftInFrame = 2;
-
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '/') {
-            // Store the number of pins knocked down in this frame
-            int pinsKnockedDown = stoi(s.substr(i - 2, 1));
-            if (currentFrame == 10) {
-                rollsLeftInFrame = 1;
-            }
-            currentFrame++;
-
-            if (rollsLeftInFrame == 1) {
-                // If we're on the last roll of a frame and it's not a strike
-                if (pinsKnockedDown < 10) {
-                    score += pinsKnockedDown + bowlingScore(s.substr(i + 1));
-                    break;
+    int roll = 0;
+    vector<int> rolls(10);
+    
+    for (char c : s) {
+        if (c == '/') {
+            if (roll > 0) {
+                if (roll < 2) {
+                    score += 10 - rolls[0];
+                } else {
+                    score += sumOfRolls(rolls, 1, roll);
                 }
+                roll = 0;
             }
-
-            rollsLeftInFrame = 2;
-        } else if (s[i] == 'X') {
-            // If we're on a strike
-            if (currentFrame < 10) {
-                score += 10 + bowlingScore(s.substr(i + 4));
-            } else {
-                score += 10;
-            }
-            currentFrame++;
-            rollsLeftInFrame = 2;
+        } else if (c == 'X') {
+            score += 30;
+            roll = 0;
         } else {
-            int pinsKnockedDown = stoi(s.substr(i, 1));
-            if (currentFrame == 10) {
-                rollsLeftInFrame = 1;
-            }
-
-            if (rollsLeftInFrame == 1) {
-                // If we're on the last roll of a frame and it's not a strike
-                score += pinsKnockedDown + bowlingScore(s.substr(i + 2));
-                break;
-            }
+            int val = c - '0';
+            rolls[roll++] = val;
         }
     }
+    
+    if (roll > 0) {
+        if (roll < 2) {
+            score += 10 - rolls[0];
+        } else {
+            score += sumOfRolls(rolls, 1, roll);
+        }
+    }
+    
     return score;
+}
+
+int sumOfRolls(vector<int> rolls, int start, int end) {
+    int sum = 0;
+    for (int i = start; i < end; i++) {
+        sum += rolls[i];
+    }
+    return sum * 2 + rolls[end - 1];
 }
