8a335e041479c186cb1f1e29a21024998109277a
beep boop <githubbot@vadim.me>
2024-07-25 03:55:24 +0200

    Solution 3 of CPP/71, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pyv0ujqd Prompt: 2e1ee514-113a-4e95-be2c-473c43f3fdd0.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ 2e1ee514-113a-4e95-be2c-473c43f3fdd0.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,16 +1,12 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
+#include <cassert>
 
-float triangle_area(int a, int b, int c) {
+float triangle_area(float a, float b, float c) {
     if (a + b > c && a + c > b && b + c > a) {
-        float s = (a + b + c) / 2.0f;
+        float s = (a + b + c) / 2;
         return sqrt(s * (s - a) * (s - b) * (s - c));
     } else {
         return -1;
     }
-}
-
-int main() {
-    assert(abs(triangle_area(2, 2, 10) + 1) < 0.01);
-    return 0;
 }
