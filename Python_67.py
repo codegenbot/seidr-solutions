c600265d5dad1bfa29d3e6171787b6168d2fdd86
beep boop <githubbot@vadim.me>
2024-07-25 06:55:27 +0200

    Solution 54 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fe2a5504-f592-4597-9095-efb1837cf 4b1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,16 +1,19 @@
-def fruit_distribution(s):
-    s = s.lower()
-    if "and" in s:
+```
+import re
+
+def fruit_distribution(s, n):
+    s = s.replace("apples", "1").replace("oranges", "0")
+    if 'and' in s:
         parts = []
         for x in re.split(r"(\d+|\band\))", s):
-            if x.isdigit() or x == "and":
+            if x.isdigit() or x == 'and':
                 parts.append(x)
     else:
         parts = [x for x in re.split(r"\d+", s)]
-    ones = s.count("1")
-    zeros = s.count("0")
+    ones = int(s.count('1'))
+    zeros = int(s.count('0'))
     total_count = 0
     for i in range(len(parts)):
         if parts[i].isdigit():
             total_count += int(parts[i])
-    return max(0, 5 - total_count - ones - zeros)
+    return max(0, n - total_count - ones - zeros)
