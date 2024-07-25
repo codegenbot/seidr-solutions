2cb7c5c7695eeb29b2e1e4e55c252517be238760
beep boop <githubbot@vadim.me>
2024-07-25 04:53:50 +0200

    Solution 55 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/artnav66 Prompt: Complete the following code given the task description and function signature.

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,6 +1,6 @@
-int count_nums(vector<int> n) {
+int count_nums(vector<int> nums) {
     int count = 0;
-    for (int num : n) {
+    for (int num : nums) {
         if (num < 0) {
             num = -num;
         }
