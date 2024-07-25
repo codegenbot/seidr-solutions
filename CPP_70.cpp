13a0e9a511f3afa3479c0f683c98f1095b2b93df
beep boop <githubbot@vadim.me>
2024-07-25 03:26:41 +0200

    Solution 15 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:       |                    ^~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:14: 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp: In function ‘int main()’: 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:24:20: error: ‘strange_sort_list’ was n ot declared in this scope    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,14 +1,15 @@
-#include <algorithm>
-#include <vector>
-
-bool issame(int x, int y) {
-    if (x <= y)
-        return true;
-    return false;
+```
+bool issame(const vector<int> &v1, const vector<int> &v2) {
+    if (v1.size() != v2.size())
+        return false;
+    for (int i = 0; i < v1.size(); i++)
+        if (v1[i] != v2[i])
+            return false;
+    return true;
 }
 
-std::vector<int> strange_sort_vector(std::vector<int> lst) {
-    std::vector<int> result;
+vector<int> strange_sort_vector(vector<int> lst) {
+    vector<int> result;
     while (!lst.empty()) {
         int min_val = *min_element(lst.begin(), lst.end());
         result.push_back(min_val);
