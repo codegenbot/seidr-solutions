8204e11d42167991163125b229d97ff7feaaa093
beep boop <githubbot@vadim.me>
2024-07-25 04:11:25 +0200

    Solution 18 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ocr7xb1v Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp:7: b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp: In function ‘int main()’: b83112cb-2ecf-4a21-a8ce-2c53569b92b0.cpp:17:20: error: ‘parse_nested_parens’ was  not declared in this scope    17 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -5,8 +5,24 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-int main(){
-    assert (issame(std::vector<int>{1, 2, 3}, std::vector<int>{1, 2, 3}));
+std::vector<int> parse_nested_parens(const std::string& input) {
+    int count = 0;
+    std::vector<int> result;
     
+    for (char c : input) {
+        if (c == '(') {
+            count++;
+        } else if (c == ')') {
+            count--;
+        }
+    }
+    
+    result.push_back(count);
+    
+    return result;
+}
+
+int main() {
+    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
     return 0;
 }
