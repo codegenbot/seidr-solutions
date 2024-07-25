e30322c18f33ee9c3062a31f5a073c01851c1e91
beep boop <githubbot@vadim.me>
2024-07-25 05:51:07 +0200

    Solution 64 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/meimvkzd Prompt: 8c5ac96b-f495-4f20-ba28-1fc4f53e140f.cpp: In function ‘int main()’: 8c5ac96b-f495-4f20-ba28-1fc4f53e140f.cpp:19:5: error: ‘assert’ was not declared in this scope    19 |     assert (minSubArraySum({1, -1}) == -1);       |     ^~~~~~ 8c5ac96b-f495-4f20-ba28-1fc4f53e140f.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <climits>   +++ |+#include <cassert>     4 | 8c5ac96b-f495-4f20-ba28-1fc4f53e140f.cpp: At global scope: 8c5ac96b-f495-4f20-ba28-1fc4f53e140f.cpp:24:5: error: redefinition of ‘int main( )’    24 | int main(){       |     ^~~~ 8c5ac96b-f495-4f20-ba28-1fc4f53e140f.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -10,12 +10,13 @@ int minSubArraySum(std::vector<long long> nums){
     for(long long num : nums){
         currentSum += num;
         if(currentSum < minSum) minSum = currentSum;
+        if(currentSum > 0) currentSum = 0;
     }
 
     return minSum;
 }
 
 int main(){
-    assert (minSubArraySum({1, -1}) == -1);
+    assert(minSubArraySum({1, -1}) == -1);
     return 0;
 }
