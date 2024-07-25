aaae8fb58cc44753a2c4011d1fc7069dd896020e
beep boop <githubbot@vadim.me>
2024-07-25 12:41:27 +0200

    Solution 28 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/hg8xbg5u Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4372b63c-779d-441c-96c4-43ae16478715.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4372b63c-779d-441c-96c4-43ae16478715.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4372b63c-779d-441c-96c4-43ae16478715.cpp:27: 4372b63c-779d-441c-96c4-43ae16478715.cpp: In function ‘int main()’: 4372b63c-779d-441c-96c4-43ae16478715.cpp:37:20: error: ‘select_words’ was not de clared in this scope    37 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 4372b63c-779d-441c-96c4-43ae16478715.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -1,7 +1,5 @@
-bool issame(string s1, string s2) {
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
-    return s1 == s2;
+bool issame(string s, char c) {
+    return s.find(c) != string::npos;
 }
 
 vector<string> select_words(string s, int n) {
@@ -11,7 +9,7 @@ vector<string> select_words(string s, int n) {
 
     for (char c : s) {
         if (c != ' ') {
-            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' && c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+            if (!issame("aeiouAEIOU", c)) {
                 consonantCount++;
             }
             currentWord += c;
