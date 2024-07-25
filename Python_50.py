32c9f637d78b1da48454552b490f7c7c6a37f833
beep boop <githubbot@vadim.me>
2024-07-25 06:24:03 +0200

    Solution 61 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bhiqujov Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4370847c-f402-4d45-b482-20b912ec4 d9b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -1,28 +1,26 @@
 ```
-def decode_shift():
+def decode_shift(encoded_string):
     shift = int(input("Enter the shift value (0-25): "))
     if shift < 0 or shift > 25:
         return str(shift) + " is not a valid shift value. Please enter a shift value between 0 and 25."
 
-    s = input("Enter the encoded string: ")
-    
     result = ""
     i = 0
-    while i < len(s):
-        if s[i].isalpha():
-            ascii_offset = ord("a") if s[i].islower() else ord("A")
-            if i > 0 and s[i-1].isdigit():
-                shift = int(s[i-1])
+    while i < len(encoded_string):
+        if encoded_string[i].isalpha():
+            ascii_offset = ord("a") if encoded_string[i].islower() else ord("A")
+            if i > 0 and encoded_string[i-1].isdigit():
+                shift = int(encoded_string[i-1])
                 i -= 1
             elif i > 0:
                 shift = int(result[-1]) % 26
             else: 
                 shift = -1
-            if ((ord(s[i]) - ascii_offset - shift) % 26) <= 0:
-                result += chr(((ord(s[i]) - ascii_offset - shift + 26) % 26) + ascii_offset)
+            if ((ord(encoded_string[i]) - ascii_offset - shift) % 26) <= 0:
+                result += chr(((ord(encoded_string[i]) - ascii_offset - shift + 26) % 26) + ascii_offset)
             else:
-                result += chr(((ord(s[i]) - ascii_offset - shift) % 26) + ascii_offset)
+                result += chr(((ord(encoded_string[i]) - ascii_offset - shift) % 26) + ascii_offset)
         else:
-            result += s[i]
+            result += encoded_string[i]
         i += 1
     return result
