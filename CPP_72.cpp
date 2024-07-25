54ce1e38e1ac62de316af33362ae7a400ea2db98
beep boop <githubbot@vadim.me>
2024-07-25 09:42:44 +0200

    Solution 2 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/40sbgpb8 Prompt: 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | bool will_it_fly(vector<int> q, int w) {       |                  ^~~~~~ 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^ In file included from 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp:14: 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp: In function ‘int main()’: 39c2a3f0-7eb7-46f9-a67a-525172a0e312.cpp:16:24: error: ‘will_it_fly’ cannot be u sed as a function    16 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,20 +1,13 @@
+#include <vector>
 bool will_it_fly(vector<int> q, int w) {
     string str = "";
     for (int i : q) {
         str += to_string(i);
     }
-    if (!is_palindrome(str)) {
-        return false;
-    }
+    if (str != string(str.rbegin(), str.rend()).s) return false;
     int sum = 0;
     for (int i : q) {
         sum += i;
     }
     return sum <= w;
-}
-
-bool is_palindrome(string s) {
-    string rev = s;
-    reverse(rev.begin(), rev.end());
-    return s == rev;
 }
