5c0349658bcd783bb9fafa09fd2c6640c95494ee
beep boop <githubbot@vadim.me>
2024-07-25 09:23:59 +0200

    Solution 48 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt: 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6236:37: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6236 |     { return (__lhs.size() == __rhs.size()       |                               ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h:6238:59: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6238 |                                                     __lhs.size())); }       |                                                     ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -4,18 +4,18 @@
 using namespace std;
 
 bool is_palindrome(string str){
-    string s(str.rbegin(),str.rend());
-    return s == std::string(str.rbegin(), str.rend());
+    string s((string(str.rbegin(),str.rend())));
+    return s==str;
 }
 
 string make_palindrome(string str){
     if(is_palindrome(str))return str;
-    for(int i=str.size()-1;i>=0;--i)
+    for(int i=str.length()-1;i>=0;--i)
         if(!is_palindrome(str.substr(0,i)+str.substr(i)))
             return str+string(str.substr(0,i))+string(str.substr(i)).reverse();
 }
 
-int main() {
+int main(){
     string input, output;
     cout << "Enter a string: ";
     cin >> input;
