dd2b19ff103695bed561bfe5316ef79114852f66
beep boop <githubbot@vadim.me>
2024-07-25 04:19:54 +0200

    Solution 57 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:37:39: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:36: 9138727f-a8da-4091-a29b-b710cf67fd58.cpp: In function ‘int main()’: 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:46:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    46 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 9138727f-a8da-4091-a29b-b710cf67fd58.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,15 +2,59 @@
 #include <vector>
 #include <limits>
 
-bool issame(std::vector<int> a,std::vector<int> b){
+bool issame(vector<int> a,vector<int>b){
     return a == b;
 }
 
 int strange_sort_list() {
+    std::vector<int> result;
+
     // Your current function implementation here...
+    // For example:
+    int input;
+    cin >> input;
+
+    if(input <= 0) {
+        cout << "Input should be positive integer." << endl;
+        return 0;
+    }
+
+    std::vector<int> lst(input);
+    for(int i=0; i<input; i++) {
+        cin >> lst[i];
+    }
+
+    result = strange_sort_vector(lst);
+
+    for(auto x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 1;
+}
+
+std::vector<int> strange_sort_vector(std::vector<int> lst) {
+    std::vector<int> result;
+    if (lst.empty()) {
+        return result;
+    }
+
+    sort(lst.begin(), lst.end());
+    result.push_back(*min_element(lst.begin(), lst.end()));
+    lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+
+    while (!lst.empty()) {
+        sort(lst.begin(), lst.end());
+        result.push_back(*min_element(lst.begin(), lst.end()));
+        lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
+    }
+
+    return result;
 }
 
 int main() {
-    assert(issame(strange_sort_vector({1,2}), {1}));
+    assert(issame(strange_sort_vector({11111}) , {1,2,3,4,5}));
+    strange_sort_list();
     return 0;
 }
