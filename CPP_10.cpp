f5dca0ba3edb1efc7af0217ac8db09e711e3c4a5
beep boop <githubbot@vadim.me>
2024-07-25 08:57:17 +0200

    Solution 0 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_10.cpp
@@ -0,0 +1,15 @@
+#include <string>
+using namespace std;
+
+bool is_palindrome(string str){
+    string s(str.rbegin(),str.rend());
+    return s==str;
+}
+
+string make_palindrome(string str){
+    if(is_palindrome(str))return str;
+    for(int i=str.length()-1;i>=0;--i)
+        if(!is_palindrome(str.substr(0,i)+str.substr(i)))
+            return str+string(str.substr(0,i)).reverse();
+    return str+string(str).reverse();
+}
