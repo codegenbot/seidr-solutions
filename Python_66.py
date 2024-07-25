8b6e34cb1bc807920e38faf22f947f6b2a64173b
beep boop <githubbot@vadim.me>
2024-07-25 06:34:37 +0200

    Solution 46 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/j8yxpsc6 Prompt: 0 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4fda12d9-2aa8-4f95-8940-a73165b5d 65a.py", line 22, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4fda12d9-2aa8-4f95-8940-a73165b5d 65a.py", line 19, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_66.py
+++ Python_66.py
@@ -1,15 +1,24 @@
+```
 def digitSum(s):
     total = 0
     temp = ""
-    for char in s.lower():
-        if char.isalnum() and char.isdigit():
+    for char in s:
+        if char.isdigit():
             temp += char
         else:
             if temp != "":
-                total += int(temp)
+                while len(temp) > 1:
+                    total += int(temp[0])
+                    temp = temp[1:]
+                if temp != "":
+                    total += int(temp)
                 temp = ""
+    while len(temp) > 1:
+        total += int(temp[0])
+        temp = temp[1:]
     if temp != "":
-        total += int(temp)
-    return total
+        return int(temp)
+    else:
+        return 0
 
 print(digitSum("You arE Very Smart"))
