e509995f01ef9ba561e0d3b188d21d255609a3a6
beep boop <githubbot@vadim.me>
2024-07-25 04:35:13 +0200

    Solution 32 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vrgqwq18 Prompt: 68b110a7-c32d-42b2-b8e1-e91cf4e5b7ad.cpp: In function ‘std::string string_to_md5 (std::string)’: 68b110a7-c32d-42b2-b8e1-e91cf4e5b7ad.cpp:7:16: error: aggregate ‘EVP_MD_CTX ctx’  has incomplete type and cannot be defined     7 |     EVP_MD_CTX ctx;       |                ^~~ 68b110a7-c32d-42b2-b8e1-e91cf4e5b7ad.cpp:17:5: error: ‘EVP_DigestFinal_ctx’ was not declared in this scope; did you mean ‘EVP_DigestFinal_ex’?    17 |     EVP_DigestFinal_ctx(&ctx, &md, NULL);       |     ^~~~~~~~~~~~~~~~~~~       |     EVP_DigestFinal_ex 68b110a7-c32d-42b2-b8e1-e91cf4e5b7ad.cpp: At global scope: 68b110a7-c32d-42b2-b8e1-e91cf4e5b7ad.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 68b110a7-c32d-42b2-b8e1-e91cf4e5b7ad.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,10 +1,11 @@
 #include <openssl/ssl.h>
 #include <openssl/x509v3.h>
+#include <openssl/evp.h>
 #include <string>
 #include <cassert>
 
 std::string string_to_md5(std::string text) {
-    EVP_MD_CTX* ctx = NULL;
+    EVP_MD_CTX ctx;
     unsigned char md[16];
     const char* str = text.c_str();
     size_t len = text.length();
@@ -13,8 +14,8 @@ std::string string_to_md5(std::string text) {
         return "";
 
     EVP_MD_CTX_init(&ctx);
-    EVP_DigestUpdate(ctx, str, len);
-    EVP_DigestFinal_ex(ctx, &md, NULL);
+    EVP_DigestUpdate(&ctx, str, len);
+    EVP_DigestFinal_ctx(&ctx, &md, NULL);
 
     std::string result;
     for (int i = 0; i < 16; i++) {
