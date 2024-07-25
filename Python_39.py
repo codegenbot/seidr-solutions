b80b59c4b7f47249258b33f022448ac8d8968507
beep boop <githubbot@vadim.me>
2024-07-25 12:11:41 +0200

    Solution 72 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7xprh2dv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e4aa20c5-631e-4dad-be5d-c9f974f76 411.py", line 17, in <module>     check(prime_fib)     ^^^^^ NameError: name 'check' is not defined

--- Python_39.py
+++ Python_39.py
@@ -1,24 +1,25 @@
 mem = {0: 0, 1: 1}
+
+
 def fibonacci(num):
     if num in mem:
         return mem[num]
     mem[num] = fibonacci(num - 1) + fibonacci(num - 2)
     return mem[num]
 
+
 def prime_fib(num):
     fib_value = fibonacci(num)
     if fib_value <= 1:
         return False
-    for i in range(2, int(fib_value ** 0.5) + 1):
+    for i in range(2, int(fib_value**0.5) + 1):
         if fib_value % i == 0:
             return False
     return True
 
+
 def check(func):
-    for i in range(0, 11):
-        if func(i):
-            print(f"{i}: Prime Fibonacci")
-        else:
-            print(f"{i}: Not a Prime Fibonacci")
+    return func(10)
+
 
 check(prime_fib)
