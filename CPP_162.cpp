e107e95109705027c42d951001e891cdcc79bfe1
beep boop <githubbot@vadim.me>
2024-07-25 04:09:45 +0200

    Solution 15 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vrgqwq18 Prompt:    16 |     MD5_Final(md, &ctx);       |     ^~~~~~~~~       |     HMAC_Final c1375220-876a-459a-95b8-8a4706d6c8c6.cpp: In function ‘int main()’: c1375220-876a-459a-95b8-8a4706d6c8c6.cpp:28:5: error: ‘assert’ was not declared in this scope    28 |     assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf9 9");       |     ^~~~~~ c1375220-876a-459a-95b8-8a4706d6c8c6.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <string>   +++ |+#include <cassert>     4 | c1375220-876a-459a-95b8-8a4706d6c8c6.cpp: At global scope: c1375220-876a-459a-95b8-8a4706d6c8c6.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ c1375220-876a-459a-95b8-8a4706d6c8c6.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -24,3 +24,8 @@ std::string string_to_md5(std::string text) {
     }
     return result;
 }
+
+int main() {
+    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+    return 0;
+}
