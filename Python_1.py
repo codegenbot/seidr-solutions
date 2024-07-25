517aa4e7f3c81a771f44ac77735d1c8781013e2e
beep boop <githubbot@vadim.me>
2024-07-25 15:08:43 +0200

    Solution 79 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/unwg6occ Prompt: Complete the following code given the task description and function signature.

--- Python_1.py
+++ Python_1.py
@@ -1,11 +1,15 @@
 def separate_paren_groups(paren_string: str) -> List[str]:
-    result = []
+    res = []
+    count = 0
     temp = ""
     for char in paren_string:
         if char == "(":
+            count += 1
             temp += char
         elif char == ")":
+            count -= 1
             temp += char
-            result.append(temp)
-            temp = ""
-    return result
+            if count == 0:
+                res.append(temp)
+                temp = ""
+    return res
