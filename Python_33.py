72d0bec28162e14c1bed08588222916546277087
beep boop <githubbot@vadim.me>
2024-07-25 06:34:08 +0200

    Solution 17 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: 7f9.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7dcb378e-2768-4905-821a-8bc166c8d 7f9.py", line 5, in sort_third     return [sorted(l[i:i+3], key=lambda x: x[0][2]) for i in range(0, len(l), 3) ]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7dcb378e-2768-4905-821a-8bc166c8d 7f9.py", line 5, in <listcomp>     return [sorted(l[i:i+3], key=lambda x: x[0][2]) for i in range(0, len(l), 3) ]             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7dcb378e-2768-4905-821a-8bc166c8d 7f9.py", line 5, in <lambda>     return [sorted(l[i:i+3], key=lambda x: x[0][2]) for i in range(0, len(l), 3) ]                                            ~~~~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,5 @@
 def sort_third(l: list):
     remainder = 3 - len(l) % 3
     l += [(float('inf'), float('inf'), float('inf'))] * remainder if remainder != 3 else []
+    l = [tuple([elem]) if isinstance(elem, int) else elem for elem in l]
     return [sorted(l[i:i+3], key=lambda x: x[2]) for i in range(0, len(l), 3)]
