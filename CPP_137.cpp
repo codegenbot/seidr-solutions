4111cdb4d7e735a3c87c2fb07c4098581a0b999b
beep boop <githubbot@vadim.me>
2024-07-25 05:49:47 +0200

    Solution 53 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y5jhcg27 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,18 +1,33 @@
-#include <boost/any.hpp>
-
-using namespace boost;
+#include <boost/any_cast.hpp>
 
 boost::any compare_one(boost::any a, boost::any b) {
-    if (is_any_of<a>(float_class())) && is_any_of<b>(float_class()) {
-        return (any_cast<float>(a) > any_cast<float>(b)) ? a : b;
-    } else if ((is_string<a>()) && (is_string<b>())) {
-        string str1 = any_cast<string>(a);
-        string str2 = any_cast<string>(b);
-        return (stof(str1) > stof(str2)) ? a : b;
-    } else if ((is_any_of<a>(int_class())) && (is_any_of<b>(int_class()))) {
-        int x = any_cast<int>(a);
-        int y = any_cast<int>(b);
-        return (x > y) ? a : b;
+    double da, db;
+    std::string sa, sb;
+
+    try {
+        da = boost::any_cast<double>(a);
+        db = boost::any_cast<double>(b);
+
+        if (da > db)
+            return a;
+        else if (db > da)
+            return b;
+        else
+            return "None";
+    } catch (...) {
+        try {
+            sa = boost::any_cast<std::string>(a);
+            sb = boost::any_cast<std::string>(b);
+
+            if (sa.compare(sb) > 0)
+                return a;
+            else if (sb.length() > sa.length())
+                return b;
+            else
+                return "None";
+        } catch (...) {
+            // If neither cast worked, it means the values are not numbers or strings
+            return "None";
+        }
     }
-    return "None";
 }
