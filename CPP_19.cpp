962ee790805110714e1c5baf0666ff0edd5e6e61
beep boop <githubbot@vadim.me>
2024-07-25 14:13:55 +0200

    Solution 81 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ypcpjthd Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,34 +1,24 @@
-map<string, int> numMap = {
-    {"zero", 0},
-    {"one", 1},
-    {"two", 2},
-    {"three", 3},
-    {"four", 4},
-    {"five", 5},
-    {"six", 6},
-    {"seven", 7},
-    {"eight", 8},
-    {"nine", 9}
-};
-
-string sort_numbers(string numbers) {
-    map<int, string> revMap;
+map<string, int> num_map = {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9}
+    };
+    
     string result = "";
-    string temp = "";
-    for (char c : numbers) {
-        if (c != ' ') {
-            temp += c;
-        } else {
-            int num = numMap[temp];
-            revMap[num] = temp;
-            temp = "";
+    for (auto const & [word, value] : num_map) {
+        size_t pos = numbers.find(word);
+        while (pos != string::npos) {
+            result += word + " ";
+            pos = numbers.find(word, pos + word.size());
         }
     }
-    int num = numMap[temp];
-    revMap[num] = temp;
-
-    for (auto it = revMap.begin(); it != revMap.end(); ++it) {
-        result += it->second + " ";
-    }
+    
     return result;
 }
