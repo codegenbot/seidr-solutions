ea97b51f08952e04034dc2dc49b6cd0ebf120f6a
beep boop <githubbot@vadim.me>
2024-07-25 07:24:10 +0200

    Solution 82 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48f08ac3-d81e-4e4c-8474-687dd932e b6a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,21 +1,18 @@
+```
+import re
 def fruit_distribution(s):
     s = s.lower()
     fruit_counts = {"apple": 0, "banana": 0}
-
+    
     for fruit in fruit_counts:
         if re.search(fruit, s):
             fruit_counts[fruit] += len(re.findall(fruit, s))
-
-    return (
-        "mixed"
-        if fruit_counts["apple"] > 0 and fruit_counts["banana"] > 0
-        else (
-            "apples"
-            if "apple" in fruit_counts and fruit_counts["apple"] > 0
-            else (
-                "bananas"
-                if "banana" in fruit_counts and fruit_counts["banana"] > 0
-                else "unknown"
-            )
-        )
-    )
+    
+    if fruit_counts["apple"] > 0 and fruit_counts["banana"] > 0:
+        return "mixed"
+    elif "apple" in fruit_counts and fruit_counts["apple"] > 0:
+        return "apples"
+    elif "banana" in fruit_counts and fruit_counts["banana"] > 0:
+        return "bananas"
+    else:
+        return "unknown"
