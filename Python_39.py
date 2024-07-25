60791be82f431aa84d7aaa04b7dea8f6ee0b6ffb
beep boop <githubbot@vadim.me>
2024-07-25 12:13:29 +0200

    Solution 77 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7xprh2dv Prompt: 0: Not a Prime Fibonacci 1: Not a Prime Fibonacci 2: Not a Prime Fibonacci 3: Prime Fibonacci 4: Prime Fibonacci 5: Prime Fibonacci 6: Not a Prime Fibonacci 7: Prime Fibonacci 8: Not a Prime Fibonacci 9: Not a Prime Fibonacci 10: Not a Prime Fibonacci Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d81b2f3-1570-4aad-8010-351903553 924.py", line 29, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0d81b2f3-1570-4aad-8010-351903553 924.py", line 26, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -2,12 +2,13 @@ mem = {0: 0, 1: 1}
 def fibonacci(num):
     if num in mem:
         return mem[num]
+    if num == 0:
+        return 0
     mem[num] = fibonacci(num - 1) + fibonacci(num - 2)
     return mem[num]
 
 def prime_fib(num):
     fib_num = fibonacci(num)
-    
     if fib_num <= 1:
         return False
     for i in range(2, int(fib_num ** 0.5) + 1):
