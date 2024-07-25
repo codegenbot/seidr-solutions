eaac6a324c7b673e0f43d00d117167071de831b9
beep boop <githubbot@vadim.me>
2024-07-25 08:21:36 +0200

    Solution 16 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/dymhg4v1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4379e8b-c83f-460a-9baf-52ba9e829 a03.py", line 25, in <module>     check(fix_spaces)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d4379e8b-c83f-460a-9baf-52ba9e829 a03.py", line 21, in check     fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple" AssertionError: This prints if this assert fails 4 (good for debugging!)

--- Python_140.py
+++ Python_140.py
@@ -1,16 +1,15 @@
+```
 def fix_spaces(text):
     new_text = ""
-    prev_was_space = False
-    for char in text.replace("\t", " ").replace(" +", " "):
+    prev_was_space = True
+    for char in text.replace("\t", ""):
         if char == " ":
-            if not prev_was_space: 
+            if prev_was_space: 
                 new_text += "_"
             else: 
                 new_text += " "
-            prev_was_space = True
-        elif prev_was_space:
-            new_text += "_"
-            prev_was_space = False
-        else:
+            prev_was_space = (char == " ")
+        elif not prev_was_space:
             new_text += char
+            prev_was_space = False
     return new_text
