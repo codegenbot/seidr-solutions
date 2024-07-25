1b02e0fc3605bbd16769966df2ceabafd0f99b86
beep boop <githubbot@vadim.me>
2024-07-25 04:08:01 +0200

    Solution 43 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/38y3f6um Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3bc205d4-7283-4074-a006-0c89e803a 97c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,4 +1,3 @@
-```
 from typing import List
 
 def concatenate(strings: List[str]) -> str:
@@ -6,18 +5,13 @@ def concatenate(strings: List[str]) -> str:
 
 if __name__ == "__main__":
     strings_num = int(input("Enter the number of strings (1-9): "))
-    while True: 
-        try: 
-            if 1 <= strings_num <= 9:
-                break; 
-        except ValueError:
-            print("Error! Please enter a number between 1 and 9.")
-    if strings_num < 1 or strings_num > 9:
+    
+    if not 1 <= strings_num <= 9:
         print("Error! Please enter a number between 1 and 9.")
     else: 
         strings = []
         
-        for i in range(strings_num):
+        for i in range(min(9, strings_num)):
             s = input(f"Enter string {i+1}: ")
             strings.append(s)
         
