00c10f8ed28f3b8c6e36e876e596ab39e6512710
beep boop <githubbot@vadim.me>
2024-07-25 06:45:43 +0200

    Solution 1 of Python/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/8pjr3li2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d795dd4e-c762-4b65-86ed-a15f3248f 690.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_99.py
+++ Python_99.py
@@ -1,7 +1,2 @@
-```
 def closest_integer(value):
-    num = float(value)
-    if num % 1 == 0:
-        return int(num)
-    else:
-        return round(num) if num > 0 else -round(-num)
+    return int(round(float(value)))
