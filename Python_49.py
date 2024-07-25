e28405ef9d463b898bf270d61acaa290f7e66128
beep boop <githubbot@vadim.me>
2024-07-25 06:49:24 +0200

    Solution 53 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/uvslt5zt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/565ee06b-c536-4227-8336-6ccb58729 692.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -1,4 +1,3 @@
-```
 def is_prime(num: int) -> bool:
     if num < 2:
         return False
@@ -9,10 +8,12 @@ def is_prime(num: int) -> bool:
 
 
 def main():
-    print("Please enter a series of integers to check if they're prime, or '0' to quit.")
+    print(
+        "Please enter a series of integers to check if they're prime, or '0' to quit."
+    )
     while True:
         prompt = input("Enter a number (or 0 to quit): ")
-        if prompt == '0':
+        if prompt == "0":
             break
         try:
             num = int(prompt)
