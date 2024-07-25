fe5565ada8fe0e9e6ba8b99f73cabb2394f8dcae
beep boop <githubbot@vadim.me>
2024-07-25 09:01:24 +0200

    Solution 7 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/clo4i8hv Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ceef5cdb-c966-47a9-9eee-72746b69a048.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ceef5cdb-c966-47a9-9eee-72746b69a048.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ceef5cdb-c966-47a9-9eee-72746b69a048.cpp:10: ceef5cdb-c966-47a9-9eee-72746b69a048.cpp: In function ‘int main()’: ceef5cdb-c966-47a9-9eee-72746b69a048.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ ceef5cdb-c966-47a9-9eee-72746b69a048.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,7 +1,6 @@
-```cpp
-#include <iostream>
 #include <vector>
 #include <string>
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b){
     if(a.size() != b.size()) return false;
@@ -9,44 +8,6 @@ bool issame(vector<string> a, vector<string> b){
         if(std::find(b.begin(),b.end(),str) == b.end()) return false;
     }
     return true;
-}
-
-int main(){
-    int n;
-    std::cout << "Enter the number of strings: ";
-    std::cin >> n;
-    
-    vector<string> strings;
-    for(int i=0; i<n; i++){
-        string temp;
-        cout << "Enter string " << i+1 << ": ";
-        cin >> temp;
-        strings.push_back(temp);
-    }
-    
-    int m;
-    cout << "Enter the number of substrings: ";
-    cin >> m;
-    
-    vector<string> subs;
-    for(int i=0; i<m; i++){
-        string temp;
-        cout << "Enter substring " << i+1 << ": ";
-        cin >> temp;
-        subs.push_back(temp);
-    }
-    
-    vector<string> filtered = filter_by_substring(strings, subs[0]);
-    
-    bool same = issame(subs, filtered);
-    
-    if(same){
-        cout << "The filtered strings are the same as the given substrings." << endl;
-    } else {
-        cout << "The filtered strings are different from the given substrings." << endl;
-    }
-    
-    return 0;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
