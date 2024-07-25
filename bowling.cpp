5510d0daeeb9f956f0e6d900113f3323d0d1ef48
beep boop <githubbot@vadim.me>
2024-07-25 09:10:32 +0200

    Solution 3 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/txuepyg8 Prompt: 65dfe094-3deb-4932-8e05-0e68a2d0962b.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,29 +1,49 @@
-```cpp
 #include <string>
 
 int bowlingScore(string s) {
     int score = 0;
-    int i = 0;
-    while (i < s.size()) {
-        if (s[i] == 'X') {
-            score += 10;
-            i++;
-            if (i < s.size() && s[i] != '/') {
-                score += 10;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == '/') {
+            string first = s.substr(0, i);
+            string second = s.substr(i + 1);
+            int firstPart = 0;
+            if (first[0] != 'X') {
+                for (char c : first) {
+                    firstPart += c - '0';
+                }
+            } else {
+                firstPart = 10;
+            }
+            int secondPart = 0;
+            if (second[0] != 'X') {
+                for (char c : second) {
+                    secondPart += c - '0';
+                }
+            } else {
+                secondPart = 10;
+            }
+            score += getScore(firstPart, secondPart);
+        } else {
+            int frame = 0;
+            if (s[i] == 'X') {
+                frame = 10;
+            } else if (s[i] - '0' + s[i+1] - '0' > 10) {
+                frame = 10;
+                i++;
+            } else {
+                frame = s[i] - '0' + s[i+1] - '0';
                 i++;
             }
-        } else if (s[i] - '0' + s[i+1] - '0' > 10) {
-            int frame = 10;
-            if (i+2 < s.size()) {
-                frame = 10 - (s[i] - '0' + s[i+1] - '0');
-            }
             score += frame;
-            i+=2;
-        } else {
-            int frame = s[i] - '0' + s[i+1] - '0';
-            score += frame;
-            i+=2;
         }
     }
     return score;
+}
+
+int getScore(int first, int second) {
+    if (first + second > 10) {
+        return 10 + second;
+    } else {
+        return first + second;
+    }
 }
