917c4015229e431d4181f691490c6c5cc921a418
beep boop <githubbot@vadim.me>
2024-07-25 02:53:21 +0200

    Solution 71 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/uz7danoh Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 5f03c33c-c308-4eb6-85e9-1080156000b3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5f03c33c-c308-4eb6-85e9-1080156000b3.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5f03c33c-c308-4eb6-85e9-1080156000b3.cpp:22:43: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5f03c33c-c308-4eb6-85e9-1080156000b3.cpp:21: 5f03c33c-c308-4eb6-85e9-1080156000b3.cpp: In function ‘int main()’: 5f03c33c-c308-4eb6-85e9-1080156000b3.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,17 +1,3 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(const std::vector<float>& a, const std::vector<float>& b) {
+bool is_same_vector(const std::vector<float>& a, const std::vector<float>& b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
-}
-
-std::vector<float> sort_even(std::vector<float> vec) {
-    std::vector<float> even_vec;
-    for (float num : vec) {
-        if (static_cast<int>(num) % 2 == 0) {
-            even_vec.push_back(num);
-        }
-    }
-    std::sort(even_vec.begin(), even_vec.end());
-    return even_vec;
 }
