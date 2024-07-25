41190f37fb2783b9da0284cbf91e435901ce4be6
beep boop <githubbot@vadim.me>
2024-07-25 13:24:45 +0200

    Solution 20 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ri1mx6h4 Prompt:       |                    ^~~ 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp:8: 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp: In function ‘int main()’: 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp:18:20: error: ‘eat’ was not declared in  this scope    18 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 3161203d-a02f-4ddb-be16-191b5fff54f6.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,18 +1,8 @@
 #include <vector>
 
-bool issame(const vector<int>& a, const vector<int>& b){
-    return a == b;
-}
-
-vector<int> eat(int number, int need, int remaining){
+vector<int> eat(int number, int need, int remaining) {
     int totalCarrots = number + need;
     int eatenCarrots = min(totalCarrots, remaining);
     int leftCarrots = max(0, remaining - totalCarrots);
     return {eatenCarrots, leftCarrots};
-}
-
-int main(){
-    assert(issame(eat(4, 5, 1), {5, 0}));
-    
-    return 0;
 }
