8bab18fb2a45b9cbc72b625fc4615bf36e35589e
beep boop <githubbot@vadim.me>
2024-07-25 09:35:22 +0200

    Solution 18 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xfgmwr5g Prompt: Program did not receive expected input

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,18 +1,32 @@
 #include <vector>
+#include <set>
 #include <algorithm>
+
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+int main() {
+    vector<int> l1, l2;
+    int n1, n2;
+    while (cin >> n1) {
+        l1.push_back(n1);
+        if (n1 == -1) break;  
+    }
+    while (cin >> n2) {
+        l2.push_back(n2);
+        if (n2 == -1) break;
+    }
+    vector<int> result = common(l1, l2);
+    return 0;
 }
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    vector<int> result;
+    set<int> common;
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        back_inserter(result));
+        inserter(common, next(common.end())));
 
+    vector<int> result(common.begin(), common.end());
     return result;
 }
