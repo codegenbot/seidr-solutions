f6a483d19eb4751849f2a67ada6a6cb41e09d99b
beep boop <githubbot@vadim.me>
2024-07-25 09:17:30 +0200

    Solution 37 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/abxe0rwc Prompt: on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d0814822-b42b-4c9c-8aba-4ac1eecc757a.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ d0814822-b42b-4c9c-8aba-4ac1eecc757a.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from d0814822-b42b-4c9c-8aba-4ac1eecc757a.cpp:32: d0814822-b42b-4c9c-8aba-4ac1eecc757a.cpp: In function ‘int main()’: d0814822-b42b-4c9c-8aba-4ac1eecc757a.cpp:42:20: error: ‘rolling_max’ was not dec lared in this scope    42 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ d0814822-b42b-4c9c-8aba-4ac1eecc757a.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -24,10 +24,38 @@ vector<int> rolling_max(vector<int> numbers) {
     }
 
     return result;
+} 
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i]) return false;
+    return true;
 }
-vector<int>({3, 2, 3, 100, 3}), vector<int>({3, 3, 3, 100, 100})) 
-    cout << "The rolling max for this input is: " << endl;
-    vector<int> result = rolling_max(user_input);
-    for (int i : result) {
-        cout << i << " ";
+
+vector<int> rolling_max(vector<int> numbers) {
+    vector<int> result;
+    int max = INT_MIN;
+
+    for (int num : numbers) {
+        if (num > max) {
+            max = num;
+        }
+        result.push_back(max);
     }
+
+    return result;
+} 
+
+std::cout << "Enter a list of numbers (space separated): ";
+vector<int> user_input;
+int num;
+while (cin >> num) {
+    user_input.push_back(num);
+}
+std::cout << "The rolling max for this input is: " << std::endl;
+vector<int> result = rolling_max(user_input);
+for (int i : result) {
+    std::cout << i << " ";
+}
+return 0;
