1dafe6a1e635c436a705848ccd55db865e86a259
beep boop <githubbot@vadim.me>
2024-07-25 05:01:22 +0200

    Solution 49 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vrgqwq18 Prompt: d7bf02c9-3ba1-4b43-98a0-39e4676e3e2e.cpp: In function ‘std::string string_to_md5 (std::string)’: d7bf02c9-3ba1-4b43-98a0-39e4676e3e2e.cpp:6:16: error: aggregate ‘EVP_MD_CTX mdct x’ has incomplete type and cannot be defined     6 |     EVP_MD_CTX mdctx;       |                ^~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,7 +1,7 @@
 #include <openssl/ssl.h>
 #include <openssl/err.h>
-#include <openssl/evp.h>
 #include <string>
+#include <openssl/evp.h>
 
 std::string string_to_md5(std::string text) {
     EVP_MD_CTX mdctx;
@@ -17,7 +17,7 @@ std::string string_to_md5(std::string text) {
     EVP_DigestUpdate(&mdctx, str, len);
     unsigned char mdSig[EVP_MAX_BLOCK_LENGTH];
     int len_output = EVP_DigestFinal_ex(&mdctx, mdSig, &len);
-    mdctx = EVP_MD_CTX_free(mdctx);
+    EVP_MD_CTX_free(&mdctx);
 
     std::string result;
     for (int i = 0; i < len; i++) {
@@ -26,4 +26,3 @@ std::string string_to_md5(std::string text) {
         result += buffer;
     }
     return result;
-}
