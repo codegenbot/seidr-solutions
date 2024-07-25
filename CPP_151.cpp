ad24305b59e2cdfc93394310bc6c25dc6d96ee0b
beep boop <githubbot@vadim.me>
2024-07-25 06:01:45 +0200

    Solution 7 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ltsby1nh Prompt: 4602e97e-217a-450b-99cf-ec0299f4855d.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 4602e97e-217a-450b-99cf-ec0299f4855d.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 4602e97e-217a-450b-99cf-ec0299f4855d.cpp:22: 4602e97e-217a-450b-99cf-ec0299f4855d.cpp: In function ‘int main()’: 4602e97e-217a-450b-99cf-ec0299f4855d.cpp:24:35: error: ‘lst’ was not declared in  this scope    24 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 4602e97e-217a-450b-99cf-ec0299f4855d.cpp:24:43: error: ‘odd_sum’ was not declare d in this scope    24 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,7 +4,7 @@
 
 long long double_the_difference(std::vector<double> lst){
     long long sum = 0;
-    for (double num : lst) {
+    for (double num : lst){
         if (num > 0 && num == (int)num && (int)num % 2 != 0) {
             sum += (long long)pow(num, 2);
         }
