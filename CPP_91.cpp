3dc95d5987e05cdf9e3aaf392ba8e91a62295a83
beep boop <githubbot@vadim.me>
2024-07-25 07:36:29 +0200

    Solution 0 of CPP/91, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/i3x23kve Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_91.cpp
@@ -0,0 +1,11 @@
+int is_bored(string S){
+    int count = 0;
+    size_t pos = 0;
+    while((pos = S.find("I", pos)) != string::npos) {
+        if (S.substr(pos).find_first_of(".!?") == string::npos) {
+            count++;
+        }
+        pos += 1;
+    }
+    return count;
+}
