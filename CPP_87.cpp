b57a9d1070eabc960be0b86fca266d39b29a0f2a
beep boop <githubbot@vadim.me>
2024-07-25 04:56:21 +0200

    Solution 45 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from a2955ece-dfe4-4462-aca7-61049955435e.cpp:17: a2955ece-dfe4-4462-aca7-61049955435e.cpp: In function ‘int main()’: a2955ece-dfe4-4462-aca7-61049955435e.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ a2955ece-dfe4-4462-aca7-61049955435e.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -3,11 +3,17 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) 
     for (int i = 0; i < lst.size(); i++) {
         if (x == 1) {
             for (int j = lst[i].size() - 1; j >= 0; j--) {
-                result.push_back({i, j});
+                vector<int> temp;
+                temp.push_back(i);
+                temp.push_back(j);
+                result.push_back(temp);
             }
         } else {
             for (int j = 0; j < lst[i].size(); j++) {
-                result.push_back({i, j});
+                vector<int> temp;
+                temp.push_back(i);
+                temp.push_back(j);
+                result.push_back(temp);
             }
         }
     }
