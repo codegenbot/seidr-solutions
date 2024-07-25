07cc3d7b2d0293f30d5d3b85102ac7c930c59c64
beep boop <githubbot@vadim.me>
2024-07-25 06:29:16 +0200

    Solution 23 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4xzmx2u7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d282a8a-2442-44ba-825f-90b4f6eb3 f09.py", line 11, in <module>     check(rolling_max)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d282a8a-2442-44ba-825f-90b4f6eb3 f09.py", line 8, in check     assert rolling_max([3, 2, 3, 100, 3]) == [3, 3, 3, 100, 100]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_9.py
+++ Python_9.py
@@ -1,5 +1,5 @@
 def rolling_max(numbers):
     max_values = []
     for i in range(len(numbers) - 1):
-        max_values.append(max(numbers[i], numbers[i+1]))
+        max_values.append(max(numbers[i], numbers[i + 1]))
     return max_values
