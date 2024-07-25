3d09d3d88edf5c4129a570471d8ff3b9e2151d80
beep boop <githubbot@vadim.me>
2024-07-25 08:11:15 +0200

    Solution 1 of Python/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mg7ofog7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bcde08f1-28bb-4e07-a38d-eeed192fc bfc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_133.py
+++ Python_133.py
@@ -1,3 +1,9 @@
-```
 def sum_squares(lst):
     return round(sum([round(x)**2 for x in lst]))
+
+input_str = "Enter numbers (separated by commas): "
+user_input = input(input_str)
+
+numbers = [int(num) for num in user_input.split(",")]
+
+print(f"The sum of squares is: {sum_squares(numbers)}")
