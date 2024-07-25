bd6e6d63ea17df32c6d2710a201961c9ac859c3c
beep boop <githubbot@vadim.me>
2024-07-25 03:34:18 +0200

    Solution 22 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:       |                    ^~~ c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp:29:27: error: ‘vector’ was not declared  in this scope    29 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp:29:39: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp:28: c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp: In function ‘int main()’: c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp:38:20: error: ‘strange_sort_list’ was n ot declared in this scope    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ c1c04b71-1e10-4c61-ad35-1580ce723dc4.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
+```cpp
 #include <vector>
 #include <algorithm>
 
-bool areSame(std::vector<int> vec1, std::vector<int> vec2) {
-    if(vec1.size() != vec2.size()) return false;
-    for(int i = 0; i < vec1.size(); i++) {
-        if(vec1[i] != vec2[i]) return false;
-    }
-    return true;
-}
-
-std::vector<int> strange_sort_vector(std::vector<int> lst) {
+std::vector<int> strange_sort_vector(const std::vector<int>& lst) {
     std::vector<int> result;
     if (lst.empty()) return result;
 
     std::vector<int> min_max;
     for (int num : lst) {
-        if (num < *min_max.begin() || min_max.empty())
+        if (num <= *min_max.begin() || min_max.empty())
             min_max.push_back(num);
         else if (*min_max.rbegin() > num)
             min_max.push_back(num);
@@ -24,10 +16,11 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
 
     while (!lst.empty()) {
         result.push_back(*min_max.begin());
-        lst.erase(std::remove(lst.begin(), lst.end(), *min_max.begin()), lst.end());
+        auto it = std::remove(lst.begin(), lst.end(), *min_max.begin());
+        lst.erase(it, lst.end());
         min_max.clear();
         for (int num : lst) {
-            if (num < *min_max.begin() || min_max.empty())
+            if (num <= *min_max.begin() || min_max.empty())
                 min_max.push_back(num);
             else if (*min_max.rbegin() > num)
                 min_max.push_back(num);
@@ -35,9 +28,4 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
     }
 
     return result;
-}
-
-int main() {
-    assert(std::equal(strange_sort_vector({1, 2, 3}).begin(), strange_sort_vector({1, 2, 3}).end(), {1, 2, 3}.begin()) && "The input vector should not have any duplicate values");
-    return 0;
 }
