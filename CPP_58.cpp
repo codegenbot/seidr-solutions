ffe78db79db1c764063a3412696399b4d20b795c
beep boop <githubbot@vadim.me>
2024-07-25 19:18:54 +0200

    Solution 52 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7mpsnsqw Prompt:       |                    ^~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:12: 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp: In function ‘int main()’: 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 00d4bd93-6f41-446a-b01f-90feab33ab1f.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,12 +1,21 @@
 #include <vector>
 #include <algorithm>
 
-void issame(std::vector<int>& l1, std::vector<int>& l2) {
-    std::sort(l1.begin(), l1.end());
-    std::sort(l2.begin(), l2.end());
+void issame(vector<int>& l1, vector<int>& l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
 
-    std::vector<int> result;
-    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
 
-    result.erase(std::unique(result.begin(), result.end()), result.end());
+    result.erase(unique(result.begin(), result.end()), result.end());
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+}
+
+int main() {
+    // Main function code here
+    return 0;
 }
