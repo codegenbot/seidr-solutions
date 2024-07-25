ab27ee9fd85a3b4fadd898a3feaf47d7ca33cc62
beep boop <githubbot@vadim.me>
2024-07-25 09:00:12 +0200

    Solution 5 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/v2bh0ofi Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ec678bff-aacb-4b72-a5c4-5a01baaf2c88.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ ec678bff-aacb-4b72-a5c4-5a01baaf2c88.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from ec678bff-aacb-4b72-a5c4-5a01baaf2c88.cpp:23: ec678bff-aacb-4b72-a5c4-5a01baaf2c88.cpp: In function ‘int main()’: ec678bff-aacb-4b72-a5c4-5a01baaf2c88.cpp:33:20: error: ‘separate_paren_groups’ w as not declared in this scope    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ ec678bff-aacb-4b72-a5c4-5a01baaf2c88.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,17 +1,17 @@
 bool issame(vector<string> a,vector<string>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        string str1=a[i];
-        string str2=b[i];
-        int j=0,k=0;
-        while(j<str1.size()&&k<str2.size()){
-            if(str1[j]=='(')j++;
-            else if(str1[j]==')')j++;
-            if(str2[k]=='(')k++;
-            else if(str2[k]==')')k++;
-            if(j==str1.size()-1&&k==str2.size()-1)break;
+    if(a.size()!=b.size()) return false;
+    for(int i=0; i<a.size();i++){
+        if(!a[i].compare(0,b[i].size(),b[i])) continue;
+        int j=0;
+        while(j<b[i].size()){
+            if(a[i][j]!=b[i][j]) break;
+            j++;
         }
-        if(j!=str1.size()||k!=str2.size())return false;
+        if(j!=b[i].size()) return false;
     }
     return true;
+}
+
+int main(){
+    assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", "(())", "(()())"}));
 }
