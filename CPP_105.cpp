242c48e6c0ce3c0837c9b6bb43940c65b2b929b2
beep boop <githubbot@vadim.me>
2024-07-25 05:02:13 +0200

    Solution 40 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/sds1xqg8 Prompt: In file included from /usr/include/c++/11/string:39,                  from 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp:27: 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp: In function ‘int main()’: 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp:37:20: error: ‘by_length’ was not decla red in this scope    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 4e5060af-c458-42aa-94e6-7119fcd9bcab.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,8 +1,7 @@
-#include <algorithm>
-#include <string>
 #include <vector>
+#include <string>
 
-bool issame(vector<string> a, vector<string> b) {
+bool same(vector<string> a,vector<string>b){
     if(a.size()!=b.size()) return false;
     for(int i=0;i<a.size();i++){
         if(a[i]!=b[i]) return false;
@@ -10,16 +9,22 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-vector<string> by_length(vector<int> arr) {
-    vector<string> numbers;
+void by_length(vector<int> arr, vector<string>& result) {
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            numbers.push_back(to_string(num));
+            result.push_back(to_string(num));
         }
     }
 
-    sort(numbers.begin(), numbers.end());
+    sort(result.begin(), result.end());
 
-    reverse(numbers.begin(), numbers.end());
+    reverse(result.begin(), result.end());
+}
 
-    return numbers;
+int main() {
+    vector<int> input = {9, 4, 8};
+    vector<string> output;
+    by_length(input, output);
+    assert(same(output , {"Nine", "Eight", "Four"}));
+    return 0;
+}
