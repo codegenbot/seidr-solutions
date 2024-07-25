8534d3a9cda544db4e7aced0098c10541a2128df
beep boop <githubbot@vadim.me>
2024-07-25 04:07:03 +0200

    Solution 12 of CPP/158, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/htdsdawz Prompt: fa77326e-64a1-4126-ac53-08f058d2b05c.cpp:4:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     3 | #include <string>   +++ |+#include <iostream>     4 | fa77326e-64a1-4126-ac53-08f058d2b05c.cpp:24:32: error: ‘endl’ was not declared i n this scope    24 |     cout << find_max(words) << endl;       |                                ^~~~ fa77326e-64a1-4126-ac53-08f058d2b05c.cpp:4:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     3 | #include <string>   +++ |+#include <ostream>     4 | fa77326e-64a1-4126-ac53-08f058d2b05c.cpp: At global scope: fa77326e-64a1-4126-ac53-08f058d2b05c.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ fa77326e-64a1-4126-ac53-08f058d2b05c.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -1,3 +1,10 @@
+#include <vector>
+#include <set>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 string find_max(vector<string> words) {
     string max_word = *words.begin();
     int max_unique_chars = 0;
