b99d9a5da04854d0c45ed942a782974565b1a7b4
beep boop <githubbot@vadim.me>
2024-07-25 03:19:32 +0200

    Solution 69 of CPP/48, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vnqpjro7 Prompt: Complete the following code given the task description and function signature.

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -1,8 +1,14 @@
 bool is_palindrome(string text) {
-    int i = 0, j = text.length() - 1;
-    while (i < j) {
-        if (text[i] != text[j]) return false;
-        i++; j--;
+    int start = 0;
+    int end = text.size() - 1;
+
+    while (start < end) {
+        if (text[start] != text[end]) {
+            return false;
+        }
+        start++;
+        end--;
     }
+
     return true;
 }
