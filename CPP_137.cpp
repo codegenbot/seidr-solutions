fe0d467c4e51b3caae60517398fa262d107cf8ef
beep boop <githubbot@vadim.me>
2024-07-25 06:17:30 +0200

    Solution 75 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt:    12 |     } else if (any_cast<int>(a) < any_cast<int>(b)) {       |       ^~~~ 31073df3-bf6d-4eda-8bea-34dd4e858ebb.cpp:14:7: error: expected unqualified-id be fore ‘else’    14 |     } else {       |       ^~~~ In file included from 31073df3-bf6d-4eda-8bea-34dd4e858ebb.cpp:19: 31073df3-bf6d-4eda-8bea-34dd4e858ebb.cpp: In function ‘int main()’: 31073df3-bf6d-4eda-8bea-34dd4e858ebb.cpp:21:13: error: ‘boost’ has not been decl ared    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 31073df3-bf6d-4eda-8bea-34dd4e858ebb.cpp:21:29: error: ‘string’ was not declared  in this scope    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 31073df3-bf6d-4eda-8bea-34dd4e858ebb.cpp:21:37: error: ‘compare_one’ was not dec lared in this scope    21 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,22 +1,35 @@
+```cpp
 #include <any>
 #include <string>
 
 std::string compare_one(const std::string& s, int i);
 
 int main() {
-    string input;
-    cin >> input;
+    std::string str;
     int num;
-    cin >> num;
 
-    a = any(input);
-    b = any(num);
+    // Read input from user
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, str);
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
 
-    if (any_cast<string>(a) > any_cast<int>(b)) {
-        return a;
-    } else if (any_cast<string>(a) < any_cast<int>(b)) {
-        return b;
+    if (num > 0) {
+        return compare_one(str, num);
     } else {
-        return a;
+        return "";
+    }
+}
+
+std::string compare_one(const std::string& s, int i) {
+    any a(s);
+    any b(i);
+
+    if (any_cast<std::string>(a) > any_cast<int>(b)) {
+        return "Greater";
+    } else if (any_cast<std::string>(a) < any_cast<int>(b)) {
+        return "Less";
+    } else {
+        return "Equal";
     }
 }
