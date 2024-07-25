038fa039912617c7a80d0eb99984e1a8258d8efd
beep boop <githubbot@vadim.me>
2024-07-25 02:08:10 +0200

    Solution 26 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/c7fceeas Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8552ca94-d6bc-4675-a609-515577e8c 343.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,7 +1,8 @@
+```
 def solve_task(s):
-    palindrome = "".join(c for c in s if c.isalpha()) + "".join(
+    palindrome = "".join(c for c in s if c.isalpha()).lower() + "".join(
         c for c in s[::-1] if c.isalpha()
-    )
+    ).lower()
     new_s = ""
     for char in palindrome:
         new_s += char * 2
