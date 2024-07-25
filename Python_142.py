33197ac133df1aa0a399c42d33933926870321d4
beep boop <githubbot@vadim.me>
2024-07-25 03:43:22 +0200

    Solution 96 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/1hqn6c6o Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7612f04-82a5-4fdd-b7b5-3e3429adc ff2.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,3 +1,4 @@
+```
 def sum_squares(lst):
     total = 0
     for i in range(len(lst)):
@@ -6,7 +7,3 @@ def sum_squares(lst):
         elif i % 4 == 0 and i % 3 != 0:
             total += lst[i] ** 3
     return total
-
-lst = input("Enter a list of numbers separated by space: ")
-lst = [int(i) for i in lst.split()]
-print(sum_squares(lst))
