e6f7f1319d6763cc5b19389b0a44db953631f49f
beep boop <githubbot@vadim.me>
2024-07-25 17:16:50 +0200

    Solution 33 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/zx3lswkr Prompt: Complete the following code given the task description and function signature.

--- Python_130.py
+++ Python_130.py
@@ -1,13 +1,12 @@
 def tri(n):
     if n == 0:
         return [0]
-    elif n == 1:
+    if n == 1:
         return [3]
-    else:
-        sequence = [3, 1]
-        for i in range(2, n + 1):
-            if i % 2 == 0:
-                sequence.append(1 + i // 2)
-            else:
-                sequence.append(sequence[i - 1] + sequence[i - 2] + sequence[i - 3])
-        return sequence
+    res = [3, 0, 0]
+    for i in range(3, n + 1):
+        if i % 2 == 0:
+            res.append(1 + i // 2)
+        else:
+            res.append(res[i - 1] + res[i - 2] + res[i - 3])
+    return res
