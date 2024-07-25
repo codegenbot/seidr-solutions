0adc4b5334e86f743c26c32dbea2656d0e38d2b3
beep boop <githubbot@vadim.me>
2024-07-25 05:32:11 +0200

    Solution 19 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bhiqujov Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33ef3407-990b-40ac-afa9-f5d3c6bac 03f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_50.py
+++ Python_50.py
@@ -2,18 +2,14 @@
 def decode_shift(s: str):
     result = ""
     i = 0
+    shift = -1 
     while i < len(s):
-        if not s[i].isalnum():
-            result += s[i]
-            i += 1
-            continue
-        if not (s[i-1:i].isdigit() or i == 0):
-            result += s[i]
-            i += 1
-            continue
-        shift = int(s[i-1:i])
-        if i < len(s) - 1 and s[i+1].isalpha():
-            ascii_offset = ord("a") if s[i+1].islower() else ord("A")
+        if s[i].isalpha() and not ('z' == s[i] or 'Z' == s[i]):
+            ascii_offset = ord("a") if s[i].islower() else ord("A")
+            if i > 0:
+                shift = int(s[i-1])
+            else: 
+                shift = -1
             if ((ord(s[i]) - ascii_offset - shift) % 26) <= 0:
                 result += chr(((ord(s[i]) - ascii_offset - shift + 26) % 26) + ascii_offset)
             else:
