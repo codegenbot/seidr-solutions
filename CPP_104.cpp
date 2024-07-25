295088aba49c5ab3bf7e792b083d6653ad398f81
beep boop <githubbot@vadim.me>
2024-07-25 20:58:35 +0200

    Solution 54 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/45mz2zsa Prompt:       |                    ^~~ cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp:20: cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp: In function ‘int main()’: cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ cf8d4eb9-39c8-4c2c-8e04-87e4deebf3fc.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,22 +1,2 @@
 #include <vector>
 #include <algorithm>
-
-std::vector<int> unique_digits(std::vector<int> x) {
-    std::vector<int> result;
-    for (int num : x) {
-        int temp = num;
-        bool hasEvenDigit = false;
-        while (temp > 0) {
-            if ((temp % 10) % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        if (!hasEvenDigit) {
-            result.push_back(num);
-        }
-    }
-    std::sort(result.begin(), result.end());
-    return result;
-}
