f77d96e525eb5423d1b6edc2ef8db288a92e9ca1
beep boop <githubbot@vadim.me>
2024-07-25 03:24:52 +0200

    Solution 13 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:       |                    ^~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:14: 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp: In function ‘int main()’: 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:24:20: error: ‘strange_sort_list’ was n ot declared in this scope    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 30f32bf5-0a24-4f30-a21d-2b4ccd7baa78.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 bool issame(int a, int b) {
-    if (a == 2 && b == 5)
+    if (a == b)
         return true;
     else
         return false;
@@ -13,23 +13,10 @@ std::vector<int> strange_sort_vector(std::vector<int> lst) {
     std::vector<int> result;
     while (!lst.empty()) {
         int min_val = *min_element(lst.begin(), lst.end());
-        if (issame(min_val, 2)) {
-            int max_val = *max_element(lst.begin(), lst.end());
-            if (issame(max_val, 5))
-                break;
-            else
-                result.push_back(max_val);
-        }
-        else
-            result.push_back(min_val);
-
+        result.push_back(min_val);
         lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
-
         if (!lst.empty())
-            if (issame(*max_element(lst.begin(), lst.end()), 2))
-                break;
-            else
-                result.push_back(*max_element(lst.begin(), lst.end()));
+            result.push_back(*max_element(lst.begin(), lst.end()));
         lst.erase(remove(lst.begin(), lst.end(), *max_element(lst.begin(), lst.end())), lst.end());
     }
     return result;
