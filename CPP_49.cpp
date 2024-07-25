d92536b349089a1504b268d07f550a35bbd9aaeb
beep boop <githubbot@vadim.me>
2024-07-25 03:14:59 +0200

    Solution 22 of CPP/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2ycpu1nz Prompt: 75d9a328-2b0b-454b-9cdf-cce641fbb610.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 75d9a328-2b0b-454b-9cdf-cce641fbb610.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 75d9a328-2b0b-454b-9cdf-cce641fbb610.cpp:26: 75d9a328-2b0b-454b-9cdf-cce641fbb610.cpp: In function ‘int main()’: 75d9a328-2b0b-454b-9cdf-cce641fbb610.cpp:28:13: error: ‘modp’ was not declared i n this scope; did you mean ‘Solution::modp’?    28 |     assert (modp(31, 5) == 3);       |             ^~~~ 75d9a328-2b0b-454b-9cdf-cce641fbb610.cpp:5:9: note: ‘Solution::modp’ declared he re     5 |     int modp(int n, int p) {       |         ^~~~

--- CPP_49.cpp
+++ CPP_49.cpp
@@ -20,6 +20,5 @@ int main() {
     int n, p;
     std::cin >> n >> p;
     assert(Solution::modp(n, p) == Solution::modp(31, 5));
-    std::cout << Solution::modp(n, p) << std::endl;
     return 0;
 }
