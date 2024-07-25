c634711f285dfe6da60589d13283501ceab355a8
beep boop <githubbot@vadim.me>
2024-07-25 04:15:37 +0200

    Solution 34 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eb2ed480-c2da-473d-85f2-0e6bc7a6a 1fb.py", line 6     temp = sorted([int(''.join(map(str, [l[i], l[i+1], l[i+2]])))]])                                                                   ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_33.py
+++ Python_33.py
@@ -2,6 +2,6 @@ def sort_third(l):
     result = []
     for i in range(0, len(l), 3):
         if i + 2 < len(l): 
-            temp = sorted([int(''.join(map(str, [l[i], l[i+1], l[i+2]])))])]) 
+            temp = sorted(map(lambda x: int(''.join(map(str, list(x)))), zip(*[iter(l[i:i+3]) for i in range(0, len(l), 3)])))
             result.append(temp[0])
     return tuple(result)
