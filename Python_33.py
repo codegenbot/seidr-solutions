8db53911c1eb04527e7b46f788e80d97e8559396
beep boop <githubbot@vadim.me>
2024-07-25 04:27:07 +0200

    Solution 39 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/de3b0aa7-2ba8-468b-8e2c-52f3594bc 609.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/de3b0aa7-2ba8-468b-8e2c-52f3594bc 609.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,9 @@
-```
 def sort_third(l):
     result = []
     for i in range(0, len(l), 3):
-        if i + 2 < len(l): 
-            temp = sorted([int(''.join(map(str, [l[i], l[i+1], l[i+2]])))])
-            result.append(temp[0])
+        temp = [l[i], l[i+1]]
+        if i + 2 < len(l):
+            temp.append(l[i+2])
+        temp.sort()
+        result.append(temp[0])
     return tuple(result)
