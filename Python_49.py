0f06194ea7b1ad3de7084d1dee54cb8b0d23558f
beep boop <githubbot@vadim.me>
2024-07-25 06:13:09 +0200

    Solution 35 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/uvslt5zt Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/862e6f05-ba3a-4910-86c2-ccdbf1e3d ac4.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_49.py
+++ Python_49.py
@@ -8,16 +8,12 @@ def is_prime(num: int) -> bool:
     return True
 
 num = None
-while num is None:
+while num is None or (num and num < 2):
     try:
         num = int(input("Enter a number: "))
-    except ValueError as e:
+    except ValueError:
         print("Invalid input. Please enter a valid integer.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-if num < 2:
-    print("Please enter a number greater than or equal to 2.")
-elif not isinstance(num, int):
+if not isinstance(num, int):
     print("Please enter an integer.")
 else:
     print(is_prime(num))
