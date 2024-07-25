652d47f953a35c762e2ac31c8b3dcf1a2fcb424c
beep boop <githubbot@vadim.me>
2024-07-25 04:27:25 +0200

    Solution 9 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/sds1xqg8 Prompt:       |                                 ^~~~~~ In file included from 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp:54:44: error: ‘b’ was not declared in t his scope    54 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp:54:45: error: expression list treated a s compound expression in initializer [-fpermissive]    54 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp:53: 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp: In function ‘int main()’: 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp:63:20: error: ‘by_length’ was not decla red in this scope    63 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 642f022f-c997-491a-89ac-1f976c9dbc9e.cpp:63:19: error: ‘issame’ cannot be used a s a function    63 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -8,7 +8,58 @@ bool issame(vector<string> a, vector<string>b) {
     return a == b;
 }
 
+vector<string> by_length(vector<int> arr) {
+    vector<string> numbers;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            numbers.push_back(to_string(num));
+        }
+    }
+
+    sort(numbers.begin(), numbers.end());
+
+    reverse(numbers.begin(), numbers.end());
+
+    vector<string> result;
+    for (string str : numbers) {
+        if (str.length() == 1) {
+            switch (stoi(str)) {
+                case 1:
+                    result.push_back("One");
+                    break;
+                case 2:
+                    result.push_back("Two");
+                    break;
+                case 3:
+                    result.push_back("Three");
+                    break;
+                case 4:
+                    result.push_back("Four");
+                    break;
+                case 5:
+                    result.push_back("Five");
+                    break;
+                case 6:
+                    result.push_back("Six");
+                    break;
+                case 7:
+                    result.push_back("Seven");
+                    break;
+                case 8:
+                    result.push_back("Eight");
+                    break;
+                case 9:
+                    result.push_back("Nine");
+            }
+        } else {
+            result.push_back(str);
+        }
+    }
+
+    return result;
+}
+
 int main() {
-    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
+    assert (issame(by_length({9, 4, 8}) , vector<string> {"Nine", "Eight", "Four"}));
     return 0;
 }
