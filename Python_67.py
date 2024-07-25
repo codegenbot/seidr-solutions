5fe85ac7997b9027c97c3c3d82291563a76bbacf
beep boop <githubbot@vadim.me>
2024-07-25 07:18:33 +0200

    Solution 68 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Program did not receive expected input

--- Python_67.py
+++ Python_67.py
@@ -2,17 +2,8 @@
 import re
 def fruit_distribution(s):
     s = s.lower()
-    fruit_counts = {"apple": 0, "banana": 0}
-    
-    for fruit in fruit_counts:
+    fruits = ["apple", "banana", "orange", "grapes"]
+    for fruit in fruits:
         if re.search(fruit, s):
-            fruit_counts[fruit] += re.findall(fruit, s).__len__()
-    
-    if fruit_counts["apple"] > 0 and fruit_counts["banana"] > 0:
-        return "mixed"
-    elif "apple" in fruit_counts and fruit_counts["apple"] > 0:
-        return "apples"
-    elif "banana" in fruit_counts and fruit_counts["banana"] > 0:
-        return "bananas"
-    else:
-        return "unknown"
+            return fruit
+    return "unknown"
