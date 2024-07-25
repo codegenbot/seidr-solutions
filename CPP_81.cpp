7cec8369d0cba077d01ca3336e83270c984aea00
beep boop <githubbot@vadim.me>
2024-07-25 05:44:28 +0200

    Solution 86 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xpbcy5mf Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const float&}; _Tp = float; _Alloc = std::allocator<float>; std::v ector<_Tp, _Alloc>::iterator = std::vector<float>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = float; _Alloc = std::all ocator<float>; std::vector<_Tp, _Alloc>::value_type = float]’ 887ee146-5ea6-490e-a7f5-9271e7b867dc.cpp:11:35:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,7 +1,3 @@
-```
-#include <iostream>
-#include <vector>
-
 std::vector<float> numerical_letter_grade(std::vector<float> grades) {
     std::vector<float> letterGrades = {};
     for (float grade : grades) {
@@ -29,34 +25,4 @@ std::vector<float> numerical_letter_grade(std::vector<float> grades) {
             letterGrades.push_back(1.0);
     }
     return letterGrades;
-}
-
-int main() {
-    std::vector<float> grades = {3.9, 4.1, 2.8};
-    std::cout << "The letter grades for these numerical grades are: ";
-    for (const auto& grade : numerical_letter_grade(grades)) {
-        if (grade >= 4.0)
-            std::cout << "A+ ";
-        else if (grade > 3.7)
-            std::cout << "A ";
-        else if (grade > 3.3)
-            std::cout << "A- ";
-        else if (grade > 3.0)
-            std::cout << "B+ ";
-        else if (grade > 2.7)
-            std::cout << "B ";
-        else if (grade > 2.3)
-            std::cout << "B- ";
-        else if (grade > 2.0)
-            std::cout << "C+ ";
-        else if (grade > 1.7)
-            std::cout << "C ";
-        else if (grade > 1.3)
-            std::cout << "C- ";
-        else if (grade > 1.0)
-            std::cout << "D+ ";
-        else
-            std::cout << "F ";
-    }
-    return 0;
 }
