b2f4de52dbb05dda0ab21124ddd822c5eb692774
beep boop <githubbot@vadim.me>
2024-07-25 06:36:12 +0200

    Solution 30 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4xzmx2u7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0031f32-7d67-4260-84c0-eb4c86d97 11b.py", line 14, in <module>     check(rolling_max)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a0031f32-7d67-4260-84c0-eb4c86d97 11b.py", line 11, in check     assert rolling_max([3, 2, 3, 100, 3]) == [3, 3, 3, 100, 100]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_9.py
+++ Python_9.py
@@ -1,10 +1,11 @@
 def rolling_max(numbers):
     max_values = []
+
     for i in range(len(numbers)):
         if i == 0:
-            max_values.append(max(numbers[i], numbers[i + 1]))
+            max_values.append(max(numbers[i], numbers[i+1]))
         elif i == len(numbers) - 1:
-            max_values.append(max(numbers[i - 1], numbers[i]))
+            max_values.append(max(numbers[i-1], numbers[i]))
         else:
             max_values.append(max(numbers[i - 1], numbers[i], numbers[i + 1]))
 
