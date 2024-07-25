438045ac1aa744a3e013422e8c107664e43ffb9b
beep boop <githubbot@vadim.me>
2024-07-25 07:02:21 +0200

    Solution 57 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nwokb2ad Prompt: 9e9a1761-9bba-43b8-8281-491c5c3f317a.cpp:56:5: error: redefinition of ‘int main( )’    56 | int main(){       |     ^~~~ 9e9a1761-9bba-43b8-8281-491c5c3f317a.cpp:41:5: note: ‘int main()’ previously def ined here    41 | int main() {       |     ^~~~ In file included from 9e9a1761-9bba-43b8-8281-491c5c3f317a.cpp:47: 9e9a1761-9bba-43b8-8281-491c5c3f317a.cpp: In function ‘int main()’: 9e9a1761-9bba-43b8-8281-491c5c3f317a.cpp:57:20: error: ‘generate_integers’ was n ot declared in this scope    57 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -39,7 +39,7 @@ struct Solution {
 };
 
 int main() {
-    assert(Solution::issame(Solution::generate_integers(17, 89), Solution::generate_integers(17, 89)));
+    assert(Solution::issame(Solution::generate_integers(17, 89), Solution::filter_even_numbers({})));
     
     return 0;
 }
