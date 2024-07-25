9af7e4909d47aed7a3562bf6a07dc33a1ed3c37a
beep boop <githubbot@vadim.me>
2024-07-25 12:19:50 +0200

    Solution 96 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7xprh2dv Prompt: 0: Not a Prime Fibonacci 1: Not a Prime Fibonacci 2: Not a Prime Fibonacci 3: Prime Fibonacci 4: Prime Fibonacci 5: Prime Fibonacci 6: Not a Prime Fibonacci 7: Prime Fibonacci 8: Not a Prime Fibonacci 9: Not a Prime Fibonacci 10: Not a Prime Fibonacci Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f812fcad-0329-4334-8183-3f8e0b820 948.py", line 33, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f812fcad-0329-4334-8183-3f8e0b820 948.py", line 30, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -8,7 +8,9 @@ def is_prime(num):
             return False
     return True
 
+
 mem = {0: 0, 1: 1}
+
 
 def fibonacci(num):
     if num in mem:
@@ -16,9 +18,11 @@ def fibonacci(num):
     mem[num] = fibonacci(num - 1) + fibonacci(num - 2)
     return mem[num]
 
+
 def prime_fib(num):
     fib_num = fibonacci(num)
     return is_prime(fib_num)
+
 
 def check(func):
     for i in range(0, 11):
@@ -26,5 +30,6 @@ def check(func):
             print(f"{i}: Prime Fibonacci")
         else:
             print(f"{i}: Not a Prime Fibonacci")
+
 
 check(prime_fib)
