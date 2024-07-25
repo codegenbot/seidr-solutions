8ca5d7ac219e2bb14817800daad2b21573975373
beep boop <githubbot@vadim.me>
2024-07-25 07:27:51 +0200

    Solution 83 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ed05e2c-05a3-4e3b-be57-928b036f3 d27.py", line 19, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4ed05e2c-05a3-4e3b-be57-928b036f3 d27.py", line 14, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -3,10 +3,7 @@ def sort_third(l: list):
     l += [float("inf")] * remainder
     tuples_list = [tuple(l[i: i + 3]) for i in range(0, len(l), 3)]
     return [
-        sorted(
-            sub_list,
-            key=lambda x: x[2] if len(x) >= 3 else float("inf")
-        )
+        sorted(sub_list, key=lambda x: x.__getitem__(2) if isinstance(x, tuple) and len(x) >= 3 else float("inf"))
         if sub_list else []
         for sub_list in tuples_list
-   ]
+    ]
