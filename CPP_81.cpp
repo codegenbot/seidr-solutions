d621697b6e02fddf4faeae5693e5590af4420c31
beep boop <githubbot@vadim.me>
2024-07-25 20:44:54 +0200

    Solution 14 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/oib76rjm Prompt:       |                              ^~~~~~ 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp:21: 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp: In function ‘int main()’: 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 4c1db0dd-ddd7-4b1b-b0e1-7a1fe157b0df.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -22,18 +22,10 @@ vector<string> numerical_letter_grade(vector<float> grades) {
 }
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
-    // Main function code goes here
+    // Main function implementation
     return 0;
 }
