42bb76d786f5117126af566c07995200e16de615
beep boop <githubbot@vadim.me>
2024-07-25 20:55:25 +0200

    Solution 56 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/oib76rjm Prompt:       |                              ^~~~~~ bd73404d-39b4-4589-9899-184deed3c47d.cpp:36:37: error: ‘string’ was not declared  in this scope    36 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bd73404d-39b4-4589-9899-184deed3c47d.cpp:36:44: error: ‘b’ was not declared in t his scope    36 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bd73404d-39b4-4589-9899-184deed3c47d.cpp:36:45: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bd73404d-39b4-4589-9899-184deed3c47d.cpp:35: bd73404d-39b4-4589-9899-184deed3c47d.cpp: In function ‘int main()’: bd73404d-39b4-4589-9899-184deed3c47d.cpp:45:20: error: ‘numerical_letter_grade’ was not declared in this scope    45 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ bd73404d-39b4-4589-9899-184deed3c47d.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -10,7 +10,7 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades);
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> letter_grades;
     for (float gpa : grades) {
-        if (issame(gpa, 4.0)) {
+        if (gpa == 4.0) {
             letter_grades.push_back("A+");
         } else if (gpa > 3.7) {
             letter_grades.push_back("A");
