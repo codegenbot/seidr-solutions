9aae328c1a63ae2f725aff4e85d6aacc11f1d033
beep boop <githubbot@vadim.me>
2024-07-25 05:14:31 +0200

    Solution 4 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/tpyvwa7a Prompt: 47ef362d-baa4-48fd-a061-8d8d367c8779.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 47ef362d-baa4-48fd-a061-8d8d367c8779.cpp:22: 47ef362d-baa4-48fd-a061-8d8d367c8779.cpp: In function ‘int main()’: 47ef362d-baa4-48fd-a061-8d8d367c8779.cpp:32:27: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    32 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 47ef362d-baa4-48fd-a061-8d8d367c8779.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 47ef362d-baa4-48fd-a061-8d8d367c8779.cpp:8:34: note:   initializing argument 1 o f ‘std::vector<int> maximum(std::vector<int>&, int)’     8 | vector<int> maximum(vector<int>& arr, int k) {       |                     ~~~~~~~~~~~~~^~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -5,12 +5,16 @@
 
 using namespace std;
 
-vector<int> maximum(const vector<int>& arr, int k) {
+vector<int> maximum(vector<int> arr, int k) {
     sort(arr.begin(), arr.end(), greater<int>());
     return vector<int>(arr.begin(), arr.begin() + k);
 }
 
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 int main() {
-    assert(issame(maximum(vector<int>{1, 2, 3, -23, 243, -400, 0}, 0), {}));
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
     return 0;
 }
