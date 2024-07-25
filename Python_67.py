03139316d17db2786f9df7e52b2b279ea7115229
beep boop <githubbot@vadim.me>
2024-07-25 06:31:05 +0200

    Solution 34 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/098b7de7-d5fb-4551-a2fa-af790da1c 5e1.py", line 17, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/098b7de7-d5fb-4551-a2fa-af790da1c 5e1.py", line 14, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,10 +1,12 @@
 def fruit_distribution(s, n):
     s = s.replace("apples", "1").replace("oranges", "0")
     parts = s.split()
-    total_count = 0
+    count = 0
     for i in range(len(parts)):
         if "and" not in parts[i]:
             continue
         words = [x for x in parts[i].split() if x.isdigit()]
-        total_count += sum(int(word) for word in words)
-    return n - total_count
+        if len(words) == 1 and words[0] == "and":
+            continue
+        count += sum(int(word) for word in words)
+    return n - count
