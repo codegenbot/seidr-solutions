f7109150552169f23d81c91f794ced0ccd344e31
beep boop <githubbot@vadim.me>
2024-07-25 06:09:07 +0200

    Solution 72 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y5jhcg27 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,11 +1,39 @@
-#include <boost/any_cast.hpp>
-
 boost::any compare_one(boost::any a, boost::any b) {
-    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
-        return a;
-    } else if (boost::any_cast<int>(b) > boost::any_cast<int>(a)) {
+    if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
+    } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return b;
+    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        if (stod(b.convert_to<string>().c_str()) > a.convert_to<string>().c_str()) {
+            return b;
+        } else if (stod(b.convert_to<string>().c_str()) < a.convert_to<string>().c_str()) {
+            return a;
+        } else {
+            return boost::any("None");
+        }
+    } else if (a.type() == typeid(double) && b.type() == typeid(string)) {
+        if (stod(a.convert_to<string>().c_str()) > stod(b.convert_to<string>().c_str())) {
+            return a;
+        } else if (stod(a.convert_to<string>().c_str()) < stod(b.convert_to<string>().c_str())) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
+    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stod(a.convert_to<string>().c_str()) > stod(b.convert_to<string>().c_str())) {
+            return a;
+        } else if (stod(a.convert_to<string>().c_str()) < stod(b.convert_to<string>().c_str())) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
     } else {
-        return "None";
+        if (a.convert_to<int>() > b.convert_to<int>()) {
+            return a;
+        } else if (a.convert_to<int>() < b.convert_to<int>()) {
+            return b;
+        } else {
+            return boost::any("None");
+        }
     }
 }
