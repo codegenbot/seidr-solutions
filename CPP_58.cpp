0739cc0b0850b3961a895f2d1bf23a3638217ac4
beep boop <githubbot@vadim.me>
2024-07-25 04:21:16 +0200

    Solution 97 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1mwn9h8n Prompt:       |                    ^~~ 247340e2-73d7-4173-8046-176796d544a4.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 247340e2-73d7-4173-8046-176796d544a4.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 247340e2-73d7-4173-8046-176796d544a4.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 247340e2-73d7-4173-8046-176796d544a4.cpp:9: 247340e2-73d7-4173-8046-176796d544a4.cpp: In function ‘int main()’: 247340e2-73d7-4173-8046-176796d544a4.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 247340e2-73d7-4173-8046-176796d544a4.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
     std::vector<int> result;
-    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(result));
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
     result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
 }
