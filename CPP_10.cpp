39df375d45edfdbd0923643af02b136fbd38e502
beep boop <githubbot@vadim.me>
2024-07-25 09:32:45 +0200

    Solution 62 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt:       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: c4210853-3622-4f82-95b1-3e4c44899c28.cpp:17:54:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -4,17 +4,15 @@
 using namespace std;
 
 string make_palindrome(string input) {
-    string reversed = "";
-    for(int i=input.length()-1; i>=0; i--) {
-        reversed += input[i];
-    }
-    return input + reversed;
+    string output = input;
+    reverse(output.begin(), output.end());
+    return input + output;
 }
 
 int main() {
     string input, output;
     cout << "Enter a string: ";
-    getline(cin, input);  
+    cin >> input;
     output = make_palindrome(input);
     cout << "Palindrome of the given string is: " << output << endl;
     return 0;
