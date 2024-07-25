fa0da2a1d7dd6fdc563ddecef3ba2790ab180a7d
beep boop <githubbot@vadim.me>
2024-07-25 04:35:12 +0200

    Solution 29 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:15:42: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:15:49: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:15:56: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 1c71487e-af05-4b34-8de4-15758289db85.cpp:14: 1c71487e-af05-4b34-8de4-15758289db85.cpp: In function ‘int main()’: 1c71487e-af05-4b34-8de4-15758289db85.cpp:27:20: error: ‘get_row’ was not declare d in this scope    27 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-bool issame(int a, int b) {
-    if (a == b)
-        return true;
-    else
+bool isSame(const vector<int>& v1, const vector<int>& v2) {
+    if (v1.size() != v2.size())
         return false;
+    for (int i = 0; i < v1.size(); i++)
+        if (v1[i] != v2[i])
+            return false;
+    return true;
 }
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x){
+vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     vector<vector<int>> result;
-    for(int i = 0; i < lst.size(); i++){
-        if(lst[i].size() > 0 && issame(x, lst[i][0])){
-            for(int j = 0; j < lst[i].size(); j++){
-                result.push_back({i, lst[i].size()-j-1});
+    for (int i = 0; i < lst.size(); i++) {
+        if (lst[i].size() > 0 && lst[i][0] == x) {
+            vector<int> row(lst[i]);
+            bool found = false;
+            for (vector<vector<int>>::iterator it = result.begin();
+                 it != result.end(); ++it) {
+                if (isSame(row, *it))
+                    found = true;
+                break;
             }
+            if (!found)
+                result.push_back(row);
         }
     }
-    sort(result.begin(), result.end());
     return result;
 }
