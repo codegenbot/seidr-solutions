6bde12c4918eefc606a1f2d1922cfbfefcffa928
beep boop <githubbot@vadim.me>
2024-07-25 07:22:39 +0200

    Solution 75 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73e75cf1-9dcd-43d1-aca9-6964165a4 631.py", line 4     tuples_list = [tuple(l[i: i + 3]) for i in range(0, len(l), 3]                                                                  ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,12 @@
 def sort_third(l: list):
     remainder = (3 - len(l) % 3) % 3
     l += [float("inf")] * remainder
-    tuple_list = [tuple(l[i: i + 3]) for i in range(0, len(l), 3)]
+    tuples_list = [tuple(l[i: i + 3]) for i in range(0, len(l), 3)]
     return [
-        sorted(sub_list, key=lambda x: x[2] if len(x) >= 3 else float("inf"))
+        sorted(
+            sub_list,
+            key=lambda x: x[2] if isinstance(x, tuple) and len(x) >= 3 else float("inf")
+        )
         if sub_list else []
-        for sub_list in tuple_list
+        for sub_list in tuples_list
     ]
