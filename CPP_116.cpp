cec8e25b6b1259e945d89f92c215986477fe38ff
beep boop <githubbot@vadim.me>
2024-07-25 04:41:42 +0200

    Solution 8 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bxl6o77x Prompt:       |                    ^~~ f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp:13: f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp: In function ‘int main()’: f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp:23:20: error: ‘sort_array’ was not decl ared in this scope    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ f0de97ca-90a7-4c9a-81be-f76ac5cb5542.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-#include <algorithm>
+```
 #include <vector>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-std::vector<int> sort_array(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end());
-    for (int i = 0; i < arr.size() - 1; ++i) {
-        if ((arr[i] & (arr[i + 1] - 1)) != 0)
-            return {};
+vector<int> sort_array(vector<int> arr){
+    sort(arr.begin(), arr.end(), [](int a, int b) {
+        if (bitset<64>(a).count() == bitset<64>(b).count()) {
+            return a < b;
+        }
+        return bitset<64>(a).count() < bitset<64>(b).count();
+    });
+    vector<int> sortedArr = arr;
+    sort(sortedArr.begin(), sortedArr.end());
+    for (int i = 0; i < sortedArr.size(); i++) {
+        auto it = equal_range(arr.begin(), arr.end(), sortedArr[i]);
+        arr.erase(it.first, it.second);
     }
     return arr;
-}
-
-int main() {
-    assert(issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));
-    return 0;
 }
