b7e4b2c6150a7e0dfc545147e739ab0b52534824
beep boop <githubbot@vadim.me>
2024-07-25 09:05:30 +0200

    Solution 14 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt: 7a80867f-9bb7-4bb3-88da-02d40885c6d7.cpp:3:20: error: ‘string’ was not declared in this scope     3 | bool is_palindrome(string str){       |                    ^~~~~~ 7a80867f-9bb7-4bb3-88da-02d40885c6d7.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | using namespace std; 7a80867f-9bb7-4bb3-88da-02d40885c6d7.cpp:8:1: error: ‘string’ does not name a ty pe     8 | string make_palindrome(string str){       | ^~~~~~ In file included from 7a80867f-9bb7-4bb3-88da-02d40885c6d7.cpp:15: 7a80867f-9bb7-4bb3-88da-02d40885c6d7.cpp: In function ‘int main()’: 7a80867f-9bb7-4bb3-88da-02d40885c6d7.cpp:17:13: error: ‘make_palindrome’ was not  declared in this scope; did you mean ‘is_palindrome’?    17 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,14 +1,19 @@
 #include <string>
 using namespace std;
 
-bool is_palindrome(std::string str){
-    std::string s(str.rbegin(),str.rend());
+bool is_palindrome(string str){
+    string s(str.rbegin(),str.rend());
     return s==str;
 }
 
-std::string make_palindrome(std::string str){
+string make_palindrome(string str){
     if(is_palindrome(str))return str;
     for(int i=str.length()-1;i>=0;--i)
         if(!is_palindrome(str.substr(0,i)+str.substr(i)))
-            return str+std::string(str.substr(0,i))+std::string(str.substr(i)).reverse();
+            return str+string(str.substr(0,i))+string(str.substr(i)).reverse();
+}
+
+int main() {
+    assert (make_palindrome("jerry") == "jerryyerri");
+    return 0;
 }
