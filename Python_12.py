51507e63944b7ee9355ba2541b34dc848fbd8783
beep boop <githubbot@vadim.me>
2024-07-25 03:41:44 +0200

    Solution 83 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/j87okes2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/da7f480a-48e6-4bfb-9b49-b3e2cf529 6c5.py", line 19, in <module>     check(longest)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/da7f480a-48e6-4bfb-9b49-b3e2cf529 6c5.py", line 16, in check     assert longest(["x", "yyy", "zzzz", "www", "kkkk", "abc"]) == "zzzz"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/da7f480a-48e6-4bfb-9b49-b3e2cf529 6c5.py", line 6, in longest     for s in [i.strip() for i in input_strs]:              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/da7f480a-48e6-4bfb-9b49-b3e2cf529 6c5.py", line 6, in <listcomp>     for s in [i.strip() for i in input_strs]:               ^^^^^^^ AttributeError: 'list' object has no attribute 'strip'

--- Python_12.py
+++ Python_12.py
@@ -4,12 +4,12 @@ def longest(*input_strs):
         print(f"No strings were entered.")
     else:
         result = []
-        for s in [i.strip() for i in input_strs]:
-            if s:
+        for s in input_strs:
+            if s.strip():
                 result.append((s, len(s)))
         if result:
             print("The longest strings are:")
             for s, length in sorted(result, key=lambda x: x[1], reverse=True):
                 print(f"'{s}' with {length} characters")
         else:
-            print(f"Single string '{' '.join(input_strs)}' with {len(' '.join(input_strs))} characters.")
+            print(f"Single string {' '.join(input_strs)} with {len(' '.join(input_strs))} characters.")
