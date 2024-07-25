36a31ddac87751ba3a6d370ed34ced7c0fcd4726
beep boop <githubbot@vadim.me>
2024-07-25 07:30:29 +0200

    Solution 4 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vhc5957x Prompt: Complete the following code given the task description and function signature.

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,15 +1,19 @@
 bool is_happy(string s){
     if(s.length() < 3) return false;
     for(int i = 0; i <= s.length()-3; i++){
-        string temp = s.substr(i, 3);
+        string sub = s.substr(i, 3);
         bool unique = true;
-        for(char c : temp){
-            if(temp.find(c) != temp.npos && temp.find(c) != i){
+        for(char c : sub){
+            int count = 0;
+            for(int j = i; j <= i+2; j++){
+                if(s[j] == c) count++;
+            }
+            if(count > 1) {
                 unique = false;
                 break;
             }
         }
-        if(unique) return true;
+        if(!unique) return false;
     }
-    return false;
+    return true;
 }
