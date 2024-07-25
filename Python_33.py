d3c053123e90eb5d72a97e1536b3bf83904b2e1b
beep boop <githubbot@vadim.me>
2024-07-25 06:39:53 +0200

    Solution 24 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d1b4073-f81e-4116-bb79-3335db88c 830.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1d1b4073-f81e-4116-bb79-3335db88c 830.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,5 +1,4 @@
 def sort_third(l: list):
-    remainder = (3 - len(l) % 3) % 3
-    l += [(float('inf'), float('inf'), float('inf'))] * remainder if remainder != 0 else []
-    l = [tuple([elem]) if isinstance(elem, int) else elem for elem in l]
-    return [sorted(l[i:i+3], key=lambda x: x[-1] if len(x) >= 3 else float('inf')) for i in range(0, len(l), 3)]
+    remainder = 3 - len(l) % 3
+    l += [(float('inf'), float('inf'), float('inf'))] * remainder if remainder != 3 else []
+    return [sorted(l[i:i+3], key=lambda x: x[-1] if len(x) == 3 else float('inf')) for i in range(0, len(l), 3)]
