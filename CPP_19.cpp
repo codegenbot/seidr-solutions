a2b96ba9313790ba97d7d26257bd524b7714312d
beep boop <githubbot@vadim.me>
2024-07-25 14:12:14 +0200

    Solution 70 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ypcpjthd Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,26 +1,35 @@
-map<string, int> number_map = {
-    {"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-    {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-    {"eight", 8}, {"nine", 9}
+map<string, int> num_map = {
+    {"zero", 0},
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9}
 };
 
-string sort_numbers(string numbers) {
+string sort_numbers(string numbers){
+    string result = "";
     map<int, string> sorted_numbers;
-    string result;
 
-    while (numbers.find(' ') != string::npos) {
-        string num_str = numbers.substr(0, numbers.find(' '));
-        int num = number_map[num_str];
-        sorted_numbers[num] = num_str;
-        numbers = numbers.substr(numbers.find(' ') + 1);
+    string num;
+    for (int i = 0; i < numbers.size(); i++) {
+        if (numbers[i] == ' ') {
+            sorted_numbers[num_map[num]] = num;
+            num = "";
+        }
+        else {
+            num += numbers[i];
+        }
     }
-    
-    sorted_numbers[number_map[numbers]] = numbers;
-    
-    for (const auto& pair : sorted_numbers) {
-        result += pair.second + " ";
+    sorted_numbers[num_map[num]] = num;
+
+    for (auto it = sorted_numbers.begin(); it != sorted_numbers.end(); it++) {
+        result += it->second + " ";
     }
 
-    result.pop_back(); // Remove the extra space at the end
     return result;
 }
