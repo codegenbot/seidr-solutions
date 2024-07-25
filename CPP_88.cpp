d958f8a2c344044e888775a77e651bca5961457b
beep boop <githubbot@vadim.me>
2024-07-25 04:54:54 +0200

    Solution 24 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lh88s7u2 Prompt: 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:30: 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp: In function ‘int main()’: 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:40:20: error: ‘sort_array’ was not decl ared in this scope; did you mean ‘ArrayUtils::sort_array’?    40 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:15:22: note: ‘ArrayUtils::sort_array’ d eclared here    15 |     std::vector<int> sort_array(std::vector<int> array){       |                      ^~~~~~~~~~ In file included from 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:30: 4d0e1525-df30-458c-b8ca-49ecc30d7f7b.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -23,8 +23,6 @@ namespace ArrayUtils {
 }
 
 int main() {
-    using ArrayUtils::issame;
-    using ArrayUtils::sort_array;
-    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
+    assert(ArrayUtils::issame(ArrayUtils::sort_array({21, 14, 23, 11}), {23, 21, 14, 11});
     return 0;
 }
