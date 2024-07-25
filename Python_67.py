a2a361f4176214252a48266ba177aa39064db361
beep boop <githubbot@vadim.me>
2024-07-25 06:16:58 +0200

    Solution 19 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/539db52c-8f12-4ee4-a977-4454f5383 11b.py", line 18, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/539db52c-8f12-4ee4-a977-4454f5383 11b.py", line 15, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,12 +1,8 @@
-```
 def fruit_distribution(s, n):
-    s = s.lower().replace("apples", "1").replace("oranges", "0")
+    s = s.replace("apples", "1").replace("oranges", "0")
     parts = s.split()
     count = 0
     for i in range(len(parts)):
-        if i < 2:
-            continue
         if 'and' not in parts[i]:
-            if parts[i].lstrip("0123456789").isalpha():
-                count += int(parts[i].lstrip("0123456789"))
+            count += int(parts[i].lstrip("0123456789abcdefghijklmnopqrstuvwxyz").replace('a', '1').replace('o', '0'))
     return n - count
