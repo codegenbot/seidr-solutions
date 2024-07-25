c19d15c7a2c4d9ff61ee1093e29d0826eb9f71c7
beep boop <githubbot@vadim.me>
2024-07-25 07:16:48 +0200

    Solution 60 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/u89dug4s Prompt:       |                              ^~~~~~ aa87a578-32c3-4071-a923-00d67e79590e.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ aa87a578-32c3-4071-a923-00d67e79590e.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ aa87a578-32c3-4071-a923-00d67e79590e.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from aa87a578-32c3-4071-a923-00d67e79590e.cpp:9: aa87a578-32c3-4071-a923-00d67e79590e.cpp: In function ‘int main()’: aa87a578-32c3-4071-a923-00d67e79590e.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ aa87a578-32c3-4071-a923-00d67e79590e.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,16 +1,22 @@
 #include <vector>
 #include <string>
+#include <cassert>
+
+using namespace std;
+
+vector<string> all_prefixes(string str){
+    vector<string> prefixes;
+    for (int i = 1; i <= str.size(); ++i) {
+        prefixes.push_back(str.substr(0, i));
+    }
+    return prefixes;
+}
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+    return a == b;
+}
+
+int main() {
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    return 0;
 }
