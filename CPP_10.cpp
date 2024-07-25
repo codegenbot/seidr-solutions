0b4a95a69e7c7824e8266b6d543d87a4c5a66d2e
beep boop <githubbot@vadim.me>
2024-07-25 09:11:40 +0200

    Solution 26 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt: bc84645d-f98c-4693-be31-18e09882fa10.cpp:20:64: error: ‘endl’ was not declared i n this scope    20 |     cout << "Palindrome of the given string is: " << output << endl;       |                                                                ^~~~ bc84645d-f98c-4693-be31-18e09882fa10.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | using namespace std; bc84645d-f98c-4693-be31-18e09882fa10.cpp: At global scope: bc84645d-f98c-4693-be31-18e09882fa10.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ bc84645d-f98c-4693-be31-18e09882fa10.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from bc84645d-f98c-4693-be31-18e09882fa10.cpp:24: bc84645d-f98c-4693-be31-18e09882fa10.cpp: In function ‘int main()’: bc84645d-f98c-4693-be31-18e09882fa10.cpp:26:13: error: ‘make_palindrome’ was not  declared in this scope; did you mean ‘is_palindrome’?    26 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,4 +1,4 @@
-```cpp
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -10,6 +10,14 @@ bool is_palindrome(string str){
 string make_palindrome(string str){
     if(is_palindrome(str))return str;
     for(int i=str.length()-1;i>=0;--i)
-        if(!is_palindrome(str.substr(0,i)+string(str.substr(i)).substr(str.rbegin(),str.rend())))
-            return str+string(str.substr(0,i))+string(str.substr(i)).substr();
+        if(!is_palindrome(str.substr(0,i)+str.substr(i)))
+            return str+string(str.substr(0,i))+string(str.substr(i)).reverse();
 }
+
+int main() {
+    string input, output;
+    cout << "Enter a string: ";
+    cin >> input;
+    output = make_palindrome(input);
+    cout << "Palindrome of the given string is: " << output << endl;
+    return 0;
