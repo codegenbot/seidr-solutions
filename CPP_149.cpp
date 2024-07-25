7a75f9e1ccb64455ceac4b9f32b49e23db92aaa0
beep boop <githubbot@vadim.me>
2024-07-25 05:58:40 +0200

    Solution 5 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/uoawipev Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 24f5bc2b-8c0e-4bc2-9752-8b3ff4c9d321.cpp:4: 24f5bc2b-8c0e-4bc2-9752-8b3ff4c9d321.cpp: In function ‘int main()’: 24f5bc2b-8c0e-4bc2-9752-8b3ff4c9d321.cpp:19:13: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    19 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc",  "dd", "aaaa", "bbbb"}));       |             ^~~~~~ 24f5bc2b-8c0e-4bc2-9752-8b3ff4c9d321.cpp: At global scope: 24f5bc2b-8c0e-4bc2-9752-8b3ff4c9d321.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 24f5bc2b-8c0e-4bc2-9752-8b3ff4c9d321.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -4,8 +4,6 @@
 #include <cassert>
 using namespace std;
 
-vector<string> sorted_list_sum(vector<string> lst);
-
 vector<string> sorted_list_sum(vector<string> lst){
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2 != 0; }), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b){
@@ -18,7 +16,7 @@ vector<string> sorted_list_sum(vector<string> lst){
 }
 
 int main(){
-    assert(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) == vector<string>{"cc", "dd", "aaaa", "bbbb"});
+    assert (sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) == vector<string>{"cc", "dd", "aaaa", "bbbb"});
     cout << "Test Passed!" << endl;
     return 0;
 }
