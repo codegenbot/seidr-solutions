3f344644e73af9f202c4a008c1c069d32ab0ff77
beep boop <githubbot@vadim.me>
2024-07-25 09:19:38 +0200

    Solution 79 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/w50xc56y Prompt:       |                    ^~~~~ a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp:9: a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp: In function ‘int main()’: a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ a3247439-0c3b-4cff-8d84-f22aea16a4ad.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -12,15 +12,13 @@ vector<float> derivative(vector<float> xs){
 }
 
 bool issame(vector<float> a, vector<float> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
-assert(issame(vector<float>{}, vector<float>{}));
+int main() {
+    vector<float> test1 = {1.0, 2.0, 3.0};
+    vector<float> test2 = {1.0, 4.0, 9.0};
+    assert(issame(derivative(test1), test2));
+    assert(issame(derivative(vector<float>{}), vector<float>{}));
+    return 0;
+}
