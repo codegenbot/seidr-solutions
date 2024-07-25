83a15c0c0739c67d0ef24bed08106f0899e559a2
beep boop <githubbot@vadim.me>
2024-07-25 04:11:23 +0200

    Solution 11 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ bd7227c2-88d1-44ca-80a2-63cda159672b.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ bd7227c2-88d1-44ca-80a2-63cda159672b.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ bd7227c2-88d1-44ca-80a2-63cda159672b.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from bd7227c2-88d1-44ca-80a2-63cda159672b.cpp:19: bd7227c2-88d1-44ca-80a2-63cda159672b.cpp: In function ‘int main()’: bd7227c2-88d1-44ca-80a2-63cda159672b.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ bd7227c2-88d1-44ca-80a2-63cda159672b.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -13,7 +13,6 @@ bool issame(vector<vector<int>> a,vector<vector<int>> b){
         }
     }
     return true;
-}
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     vector<vector<int>> result;
@@ -29,9 +28,8 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
         }
     }
     return result;
-}
 
 int main() {
-    assert(issame(get_row({},{1,{1,2,3}}),{{2,2}}));
+    assert(issame(get_row({},{1},{1,2,3}),{{2,2}}));
     return 0;
 }
