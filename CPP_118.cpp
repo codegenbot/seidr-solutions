d68f405efccfcba22f570c9e21b101b85aeedb7f
beep boop <githubbot@vadim.me>
2024-07-25 04:40:18 +0200

    Solution 4 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jfp35lqu Prompt: 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp: In function ‘std::string get_closest_v owel(std::string)’: 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp:9:26: error: request for member ‘find’ in ‘"aeiouAEIOU"’, which is of non-class type ‘const char [11]’     9 |         if ("aeiouAEIOU".find(word[i]) != string::npos && isalpha(word[i -1]) && !isvowel(word[i-1])) {       |                          ^~~~ 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp:9:82: error: ‘isvowel’ was not declared  in this scope; did you mean ‘islower’?     9 | OU".find(word[i]) != string::npos && isalpha(word[i-1]) && !isvowel(word [i-1])) {       |                                                             ^~~~~~~       |                                                             islower 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp: In function ‘bool isvowel(char)’: 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp:17:25: error: request for member ‘find’  in ‘"aeiouAEIOU"’, which is of non-class type ‘const char [11]’    17 |     return "aeiouAEIOU".find(c) != string::npos;       |                         ^~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -1,13 +1,18 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool isvowel(char c) {
+    string vowels = "aeiouAEIOU";
+    return vowels.find(to_string(c)) != string::npos;
+}
+
 string get_closest_vowel(string word) {
-    for (int i = word.length() - 1; i >= 0; i--) {
-        if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' ||
-            word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U') {
-            for (int j = i - 1; j >= 0; j--) {
-                if ((word[j] != 'a' && word[j] != 'e' && word[j] != 'i' && word[j] != 'o' && word[j] != 'u' &&
-                    (word[j] != 'A' && word[j] != 'E' && word[j] != 'I' && word[j] != 'O' && word[j] != 'U')) {
-                    return string(1, word[i]);
-                }
-            }
+    int n = word.size();
+    for (int i = n - 1; i > 0; --i) {
+        if ("aeiouAEIOU".find(1, 1, to_string(word[i])) == string::npos && isalpha(word[i-1]) && !isvowel(word[i-1])) {
+            return string(1, word[i]);
         }
     }
     return "";
