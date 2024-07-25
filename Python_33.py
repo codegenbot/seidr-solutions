ebca045acf8cb729fc39a0a3fc99b6df763952a6
beep boop <githubbot@vadim.me>
2024-07-25 05:44:34 +0200

    Solution 69 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7a503a2-1bb0-44b0-905b-a866c0957 a82.py", line 14, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7a503a2-1bb0-44b0-905b-a866c0957 a82.py", line 9, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7a503a2-1bb0-44b0-905b-a866c0957 a82.py", line 4, in sort_third     temp = [i[2] for i in sorted([x for x in lst])]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d7a503a2-1bb0-44b0-905b-a866c0957 a82.py", line 4, in <listcomp>     temp = [i[2] for i in sorted([x for x in lst])]             ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,7 @@
 def sort_third(lst):
     result = []
-    while len(lst) >= 3:
-        temp = [i[2] for i in sorted([(a, b, c) for (a, b), c in zip(*[iter(lst)]*3)])]
-        del lst[:3]
-    else:
-        temp = [lst.pop(0)]
-        if not lst: del lst[:]
-    result.append(tuple(temp))
+    while len(lst) > 0:
+        temp = [i[2] for i in sorted([tuple(x) for x in lst])]
+        del lst[:]
+        result.append(temp)
     return tuple(result)
