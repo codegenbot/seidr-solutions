af851a982f02af5765492fa8e9b30cd406f36790
beep boop <githubbot@vadim.me>
2024-07-25 07:23:34 +0200

    Solution 80 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0c546103-693a-43e8-baea-6e4298c40 cf0.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -7,7 +7,7 @@ def fruit_distribution(s):
     for fruit in fruit_counts:
         if re.search(fruit, s):
             fruit_counts[fruit] += len(re.findall(fruit, s))
-    
+        
     if fruit_counts["apple"] > 0 and fruit_counts["banana"] > 0:
         return "mixed"
     elif "apple" in fruit_counts and fruit_counts["apple"] > 0:
