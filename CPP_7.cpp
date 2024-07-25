22d3f5e537b968372c48f19177624dddd22e067d
beep boop <githubbot@vadim.me>
2024-07-25 08:57:40 +0200

    Solution 1 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/clo4i8hv Prompt: Complete the following code given the task description and function signature.

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,8 +1,8 @@
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
-    for(string s : strings){
-        if(s.find(substring) != string::npos)
-            result.push_back(s);
+    for(auto& str : strings){
+        if(str.find(substring) != string::npos)
+            result.push_back(str);
     }
     return result;
 }
