f871aedfa7e4aea4d048d4dc4c9726081b0b5b7b
beep boop <githubbot@vadim.me>
2024-07-25 05:50:53 +0200

    Solution 48 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: 0fc73c98-db91-424b-877b-a894e52fd732.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,27 +1,32 @@
-#include <iostream>
-#include <memory>
+#include "boost/any.hpp"
 
-int compare(std::any a, std::any b) {
+namespace boost {
+
+int compare(boost::any& a, boost::any& b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = std::any_cast<int>(a);
-        int y = std::any_cast<int>(b);
-        if (x > y) return 1;
-        else if (x < y) return -1;
-        else return 0; 
+        int x = boost::any_cast<int>(a);
+        int y = boost::any_cast<int>(b);
+        return (x > y) - (x < y);
     } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double x = std::any_cast<double>(a);
-        double y = std::any_cast<double>(b);
-        if (x > y) return 1;
-        else if (x < y) return -1;
-        else return 0; 
-    } else if ((a.type() == typeid(std::string) || a.type() == typeid(std::u16string)) &&
-               (b.type() == typeid(std::string) || b.type() == typeid(std::u16string)))) {
-        std::string x = std::any_cast<std::string>(a);
-        std::string y = std::any_cast<std::string>(b);
-        if (x > y) return 1;
-        else if (x < y) return -1;
-        else return 0; 
+        double x = boost::any_cast<double>(a);
+        double y = boost::any_cast<double>(b);
+        return (x > y) - (x < y);
+    } else if ((a.type() == typeid(std::string) || a.type() == typeid(wstring)) &&
+               (b.type() == typeid(std::string) || b.type() == typeid(wstring)))) {
+        std::string x = boost::any_cast<std::string>(a);
+        std::string y = boost::any_cast<std::string>(b);
+        return x.compare(y);
+    } else if ((a.type() == typeid(std::string) || a.type() == typeid(wstring)) &&
+               (b.type() == typeid(double)))) {
+        std::string x = boost::any_cast<std::string>(a);
+        double y = boost::any_cast<double>(b);
+        return x.compare(std::to_string(y));
+    } else if ((a.type() == typeid(double)) &&
+               (b.type() == typeid(std::string) || b.type() == typeid(wstring)))) {
+        double x = boost::any_cast<double>(a);
+        std::string y = boost::any_cast<std::string>(b);
+        return std::to_string(x).compare(y);
     } else {
-        return 0; 
+        return 0;
     }
 }
