59eb7604a90cb28911abf6c5a9b7ccd45b1da5b1
beep boop <githubbot@vadim.me>
2024-07-25 05:37:30 +0200

    Solution 36 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/33hnxnvj Prompt:       |             ^~~~~~ 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp:29: 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp: In function ‘int main()’: 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp:39:20: error: ‘maximum’ was not declare d in this scope    39 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 158f86c6-f7f7-476f-b1fc-2a329430a442.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,15 +2,31 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
-vector<int> maximum(vector<int> arr, int k) {
-    if (k > arr.size()) {
-        return {};
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::vector<int> result;
+    for (int i = 0; i < k; ++i) {
+        auto it = std::max_element(arr.begin(), arr.end());
+        result.push_back(*it);
+        arr.erase(it);
     }
-    vector<int> result(arr.begin(), arr.begin() + k);
-    sort(result.begin(), result.end());
     return result;
+}
+
+int main() {
+    std::vector<int> test = maximum({1, 2, 3, -23, 243, -400, 0}, 5);
+    if (issame(test, {3, 243, 0, -23, -400})) {
+        std::cout << "Test passed.\n";
+    } else {
+        std::cout << "Test failed.\n";
+    }
 }
