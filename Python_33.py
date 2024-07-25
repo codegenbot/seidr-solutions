e4caa3cbe7a8a11dc8e225aadd3fea177e8234ff
beep boop <githubbot@vadim.me>
2024-07-25 05:11:10 +0200

    Solution 57 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3ef8828d-201b-4d31-895b-a4215d21f 448.py", line 19, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3ef8828d-201b-4d31-895b-a4215d21f 448.py", line 14, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,12 +1,8 @@
-def sort_third(l):
+def sort_third(lst):
     result = []
-    while len(l) > 0:
-        if len(l) >= 3:
-            temp = l[:3]
-            del l[:3]
-            temp.sort()
-            result.append(tuple(temp))
-        else:
-            result.append(tuple(sorted(l)))
-            break
+    while len(lst) > 0:
+        temp = lst[:3]
+        del lst[:3]
+        temp.sort()
+        result.append(tuple(temp))
     return tuple(result)
