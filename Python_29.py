a4347c7abf23a92a0629634ad089546ecb4b73d3
beep boop <githubbot@vadim.me>
2024-07-25 04:18:01 +0200

    Solution 46 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/kwj3d9s9 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8430a254-4a25-4723-bd4b-702d3e54e 66b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -1,11 +1,15 @@
 from typing import List
 
+
 def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
     return [s for s in strings if s.startswith(prefix)]
 
+
 if __name__ == "__main__":
     try:
-        input_list = [i.strip().lower() for i in input("Enter space-separated string: ").split()]
+        input_list = [
+            i.strip().lower() for i in input("Enter space-separated string: ").split()
+        ]
     except ValueError:
         print("Invalid input. Please enter space-separated strings.")
         exit()
