9260c8333bb5adfded8f77ef2eb0a05609ebe754
beep boop <githubbot@vadim.me>
2024-07-25 04:41:52 +0200

    Solution 33 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:15:42: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:15:49: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:15:56: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 1c71487e-af05-4b34-8de4-15758289db85.cpp:14: 1c71487e-af05-4b34-8de4-15758289db85.cpp: In function ‘int main()’: 1c71487e-af05-4b34-8de4-15758289db85.cpp:27:20: error: ‘get_row’ was not declare d in this scope    27 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 1c71487e-af05-4b34-8de4-15758289db85.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,12 +1,11 @@
 #include <vector>
-#include <algorithm>
 
 vector<vector<int>> get_row(vector<vector<int>> lst, int x){
     vector<vector<int>> result;
     for(int i = 0; i < lst.size(); i++){
         if(lst[i].size() > 0 && lst[i][0] == x){
             for(int j = 0; j < lst[i].size(); j++){
-                result.push_back({i, lst[i].size()-j-1});
+                result.push_back({i, j});
             }
         }
     }
