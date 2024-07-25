731b5948caf917871e4ed9d89afd20339d48b513
beep boop <githubbot@vadim.me>
2024-07-25 09:20:14 +0200

    Solution 13 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/txuepyg8 Prompt: /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::__cxx11::basi c_string<_CharT, _Traits, _Alloc>::basic_string(std::__cxx11::basic_string<_Char T, _Traits, _Alloc>&&) [with _CharT = char; _Traits = std::char_traits<char>; _A lloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:6667:12:   required from here /usr/include/c++/11/bits/basic_string.h:576:31: error: ‘class std::__cxx11::basi c_string<char>’ has no member named ‘_M_allocated_capacity’; did you mean ‘_S_lo cal_capacity’?   576 |             _M_capacity(__str._M_allocated_capacity);       |                         ~~~~~~^~~~~~~~~~~~~~~~~~~~~       |                         _S_local_capacity /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,17 +2,17 @@
 int bowlingScore(const char* s) {
     int score = 0;
     int i = 0;
-    while (i < strlen((char*)s)) {
+    while (i < strlen(s)) {
         if (s[i] == 'X') {
             score += 10;
             i++;
-            if (i < strlen((char*)s) && s[i] != '/') {
+            if (i < strlen(s) && s[i] != '/') {
                 score += 10;
                 i++;
             }
-        } else if ((s[i] - '0' + s[i+1] - '0') > 10) {
+        } else if (s[i] - '0' + s[i+1] - '0' > 10) {
             int frame = 10;
-            if (i+2 < strlen((char*)s)) {
+            if (i+2 < strlen(s)) {
                 frame = 10 - (s[i] - '0' + s[i+1] - '0');
             }
             score += frame;
