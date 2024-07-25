09d2c2a3e10020b8b66cb890bee2398c92b0e01a
beep boop <githubbot@vadim.me>
2024-07-25 02:35:55 +0200

    Solution 16 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/q2oxtv4a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/85c04646-6338-48e0-9002-f2ac6a709 62c.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_22.py
+++ Python_22.py
@@ -1,13 +1,14 @@
-```python
-from typing import List, Any
+```
+from typing import List
 
 def filter_integers():
-    values = []
+    values: List[Any] = []
     while True:
         try:
             value = int(input("Enter an integer (or 'done' if finished): "))
             values.append(value)
         except ValueError:
-            if input("Enter 'done' if you want to finish: ") == 'done':
+            user_input = input("Enter 'done' if you want to finish: ")
+            if user_input == 'done':
                 break
-    return [value for value in values]
+    return [value for value in values if isinstance(value, int)]
