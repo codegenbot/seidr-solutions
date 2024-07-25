a4157ab318b5c54465bd721512ce39075c6f827e
beep boop <githubbot@vadim.me>
2024-07-25 09:09:25 +0200

    Solution 22 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/abxe0rwc Prompt: on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e128aa31-01c3-4f7f-87e5-e43c4ea37b49.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ e128aa31-01c3-4f7f-87e5-e43c4ea37b49.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from e128aa31-01c3-4f7f-87e5-e43c4ea37b49.cpp:32: e128aa31-01c3-4f7f-87e5-e43c4ea37b49.cpp: In function ‘int main()’: e128aa31-01c3-4f7f-87e5-e43c4ea37b49.cpp:42:20: error: ‘rolling_max’ was not dec lared in this scope    42 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ e128aa31-01c3-4f7f-87e5-e43c4ea37b49.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -26,5 +26,5 @@ vector<int> rolling_max(vector<int> numbers) {
 }
 
 int main() {
-    assert(issame(rolling_max({3, 2, 3, 100, 3}), vector<int>({3, 3, 3, 100, 100})));
+    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
 }
