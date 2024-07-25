416d29670f6fbdc4d0fd99dd18075d79083356c4
beep boop <githubbot@vadim.me>
2024-07-25 03:14:27 +0200

    Solution 57 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1mwn9h8n Prompt:       |                    ^~~ 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp:12: 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp: In function ‘int main()’: 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 86b4adb2-f8be-42be-8815-67bc79968e0f.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 #include <algorithm>
 
-sort(l1.begin(), l1.end());
-sort(l2.begin(), l2.end());
+vector<int> findCommonElements(vector<int>& l1, vector<int>& l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
 
-vector<int> result;
-set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
 
-result.erase(unique(result.begin(), result.end()), result.end());
+    result.erase(unique(result.begin(), result.end()), result.end());
 
-return result;
+    return result;
+}
