af726bef44da9b2752e9871947332ce421fbb9f2
beep boop <githubbot@vadim.me>
2024-07-25 07:03:46 +0200

    Solution 49 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt:     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8160a57-15bc-4eaf-9cbe-8cc5ca9f8 52b.py", line 4, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8160a57-15bc-4eaf-9cbe-8cc5ca9f8 52b.py", line 6, in <listcomp>     [tuple([i[j] for j in range(3)]) for i in l[i : i + 3]],     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8160a57-15bc-4eaf-9cbe-8cc5ca9f8 52b.py", line 6, in <listcomp>     [tuple([i[j] for j in range(3)]) for i in l[i : i + 3]],            ^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a8160a57-15bc-4eaf-9cbe-8cc5ca9f8 52b.py", line 6, in <listcomp>     [tuple([i[j] for j in range(3)]) for i in l[i : i + 3]],             ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,8 @@
 def sort_third(l: list):
     remainder = (3 - len(l) % 3) % 3
-    l += [(float("inf"), float("inf"), float("inf"))] * remainder
+    l += [float("inf")] * remainder
+    tuples_list = [tuple(l[i : i + 3]) for i in range(0, len(l), 3) ]    
     return [
-        sorted(
-            [tuple([i[j] for j in range(3)]) for i in l[i : i + 3]],
-            key=lambda x: x[2] if isinstance(x, tuple) and len(x) == 3 else float("inf")
-        )
-        for i in range(0, len(l), 3)
+        sorted(sub_list, key=lambda x: x[2] if len(x) == 3 else float("inf"))
+        for sub_list in tuples_list
     ]
