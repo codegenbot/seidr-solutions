a76bc6655a88f7d67eb6bd9e0ad5064bee2ec4d7
beep boop <githubbot@vadim.me>
2024-07-25 02:04:31 +0200

    Solution 34 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/uz7danoh Prompt:    30 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 64caad3b-ef97-4789-b2fb-1870c67986d9.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 64caad3b-ef97-4789-b2fb-1870c67986d9.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 64caad3b-ef97-4789-b2fb-1870c67986d9.cpp:29: 64caad3b-ef97-4789-b2fb-1870c67986d9.cpp: In function ‘int main()’: 64caad3b-ef97-4789-b2fb-1870c67986d9.cpp:39:29: error: cannot convert ‘std::vect or<float>’ to ‘int’    39 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                             |       |                             std::vector<float> 64caad3b-ef97-4789-b2fb-1870c67986d9.cpp:14:13: note:   initializing argument 1 of ‘bool issame(int)’    14 | bool issame(const vector<float>& a, const vector<float>& b) {       |             ^~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -21,7 +21,7 @@ int main() {
 
     assert(issame(sorted_even, std::vector<float>{1.2f, 2.0f, 3.5f, 4.8f, 5.3f}));
 
-    assert(issame(sort_even({5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f}), 
-                  std::vector<float>{-12.0f, 8.0f, 3.0f, 4.0f, 5.0f, 2.0f, 12.0f, 11.0f, 23.0f, -10.0f}));
+    assert(issame(sort_even({5.0f, 8.0f, -12.0f, 4.0f, 23.0f, 2.0f, 3.0f, 11.0f, 12.0f, -10.0f}), std::vector<float>{-12.0f, 8.0f, 3.0f, 4.0f, 5.0f, 2.0f, 12.0f, 11.0f, 23.0f, -10.0f}));
+
     return 0;
 }
