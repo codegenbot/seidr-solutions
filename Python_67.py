4bd7af779e019086aa0cfcd52924c18f38720e75
beep boop <githubbot@vadim.me>
2024-07-25 06:26:32 +0200

    Solution 30 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21b6a453-8904-469f-98f8-1c47fdc15 490.py", line 26, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21b6a453-8904-469f-98f8-1c47fdc15 490.py", line 23, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,11 +1,8 @@
-```
 def fruit_distribution(s, n):
     s = s.replace("apples", "1").replace("oranges", "0")
     parts = s.split()
     count = 0
     for i in range(len(parts)):
-        if "and" not in parts[i]:
-            continue
-        words = [x for x in parts[i].split() if x.isdigit()]
-        count += sum(int(word) for word in words)
+        if parts[i].startswith('1'):
+            count += int(parts[i].lstrip('0123456789abcdefghijklmnopqrstuvwxyz'))
     return n - count
