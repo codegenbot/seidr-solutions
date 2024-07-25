d9d7a2a23a68c5174775189579c291c3f5bb35c4
beep boop <githubbot@vadim.me>
2024-07-25 06:24:33 +0200

    Solution 6 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/opploh68 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e9c7faa7-51cc-4695-96bb-e85716002 a65.py", line 8     result = find_zero(list(map(float, input().split()))                       ^ SyntaxError: '(' was never closed

--- Python_32.py
+++ Python_32.py
@@ -1,10 +1,2 @@
-def find_zero(xs: list):
-    n = len(xs) - 1
-    a = xs[-1]
-    b = xs[-2]
-    return -b / a
-
-
-# Call the function with input
 result = find_zero(list(map(float, input().split())))
 print(result)
