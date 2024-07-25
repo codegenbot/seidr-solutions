ba918980231e8c1c1c78f887f67529464bfffec3
beep boop <githubbot@vadim.me>
2024-07-25 06:29:43 +0200

    Solution 13 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: a07.py", line 11, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac0c496b-817a-4a6b-b686-8201edef3 a07.py", line 6, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac0c496b-817a-4a6b-b686-8201edef3 a07.py", line 4, in sort_third     return [sorted(l[i:i+3], key=lambda x: x[2]) for i in range(0, len(l), 3)]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac0c496b-817a-4a6b-b686-8201edef3 a07.py", line 4, in <listcomp>     return [sorted(l[i:i+3], key=lambda x: x[2]) for i in range(0, len(l), 3)]             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ac0c496b-817a-4a6b-b686-8201edef3 a07.py", line 4, in <lambda>     return [sorted(l[i:i+3], key=lambda x: x[2]) for i in range(0, len(l), 3)]                                            ~^^^ TypeError: 'int' object is not subscriptable

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,4 @@
 def sort_third(l: list):
     remainder = 3 - len(l) % 3
-    l += [float('inf')] * remainder if remainder != 3 else []
+    l += [(float('inf'), float('inf'), float('inf'))] * remainder if remainder != 3 else []
     return [sorted(l[i:i+3], key=lambda x: x[2]) for i in range(0, len(l), 3)]
