f5c7991fb4fe0f553b8e81aa5764071afa7cc109
beep boop <githubbot@vadim.me>
2024-07-25 08:16:11 +0200

    Solution 98 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fnrxux41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88e0a94b-4ffc-42d6-b78f-b9fb11205 5a9.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,6 +1,6 @@
 def shortest_paths(paths):
     min_path = min(paths, key=sum)
-    return ", ".join(map(str, min_path))
+    return ", ".join(str(x) for x in min_path)
 
 input_list = [(1, 2), (3, 4), (5, 6), (7, 8)]
 minPathStr = shortest_paths(input_list)
