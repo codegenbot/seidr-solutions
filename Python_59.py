baf997bda91e230b2b1916a36085e7ca0273d13f
beep boop <githubbot@vadim.me>
2024-07-25 05:45:09 +0200

    Solution 2 of Python/59, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/yamirj8i Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3f35820f-9714-4d22-9796-392dcc413 e2e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_59.py
+++ Python_59.py
@@ -1,9 +1,8 @@
-```
 def largest_prime_factor(n: int):
     i = n - 1
     while i > 1:
         if n % i == 0:
-            for j in range(2, int(i ** 0.5) + 1):
+            for j in range(2, int(i**0.5) + 1):
                 if i % j == 0:
                     i -= 1
                     break
