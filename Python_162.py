c849884f85209982c738986ce72fdd84ca487ee9
beep boop <githubbot@vadim.me>
2024-07-25 20:11:23 +0200

    Solution 57 of Python/162, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/b5nwmlsi Prompt: Complete the following code given the task description and function signature.

--- Python_162.py
+++ Python_162.py
@@ -1,7 +1,6 @@
-import hashlib
-
 def string_to_md5(text):
-    if text == '':
-        return None
-    else:
+    if text:
+        import hashlib
         return hashlib.md5(text.encode()).hexdigest()
+    else:
+        return None
