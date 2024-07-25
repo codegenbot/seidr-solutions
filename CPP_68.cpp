272cd2d5e9a0ffa650a279b4b5f2fb4e1c344d61
beep boop <githubbot@vadim.me>
2024-07-25 03:31:14 +0200

    Solution 43 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/k4v1zmsq Prompt: In file included from /usr/include/c++/11/vector:67,                  from 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp:3: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp:3: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp:25: 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp: In function ‘int main()’: 90feca35-121d-4932-ba3d-c9fbf3053cd3.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -8,12 +8,15 @@ std::vector<int> pluck(std::vector<int> arr) {
     }
     
     int minEven = INT_MAX;
+    int minIndex = 0;
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] % 2 == 0 && arr[i] < minEven) {
             minEven = arr[i];
+            minIndex = i;
         }
     }
     
     result.push_back(minEven);
+    result.push_back(minIndex);
     return result;
 }
