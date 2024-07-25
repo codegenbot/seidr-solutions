8e5991acba21b8209ba603e2cf76bd5424acea65
beep boop <githubbot@vadim.me>
2024-07-25 07:04:40 +0200

    Solution 61 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nwokb2ad Prompt:       |                                  ^~~ 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp:7: 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp: In function ‘int main()’: 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp:17:20: error: ‘generate_integers’ was n ot declared in this scope    17 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ 9a258cbf-6a0a-448b-85ce-4dbb90788d59.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,45 +1,33 @@
-#include <iostream>
 #include <vector>
 #include <cassert>
 
-using namespace std;
-
-struct Solution {
-    static bool issame(vector<int> a, vector<int> b) {
-        if (a.size() != b.size()) {
-            return false;
-        }
-        
-        for (size_t i = 0; i < a.size(); ++i) {
-            if (a[i] != b[i]) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-
-    static vector<int> generate_integers(int a, int b) {
-        vector<int> result;
-        for (int i = a; i <= b; ++i) {
+class Solution {
+public:
+    static std::vector<int> generate_integers(int start, int end) {
+        std::vector<int> result;
+        for (int i = start; i <= end; ++i) {
             result.push_back(i);
         }
         return result;
     }
 
-    static vector<int> filter_even_numbers(vector<int> a) {
-        vector<int> result;
-        for (int i : a) {
-            if (i % 2 == 0) {
-                result.push_back(i);
+    static std::vector<int> filter_even_numbers(const std::vector<int>& numbers) {
+        std::vector<int> result;
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                result.push_back(num);
             }
         }
         return result;
     }
+
+    static bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+        return a == b;
+    }
 };
 
 int main() {
-    assert(Solution::issame(Solution::generate_integers(17, 89), Solution::filter_even_numbers(Solution::generate_integers(17, 89))));
+    assert(Solution::issame(Solution::generate_integers(17, 89), Solution::filter_even_numbers(Solution::generate_integers(17, 89)));
     
     return 0;
 }
