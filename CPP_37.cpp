f473f54bd297fbfb8f1dde18c8b36ea028835a58
beep boop <githubbot@vadim.me>
2024-07-25 08:42:46 +0200

    Solution 23 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/8tqbyw58 Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 58a7768e-1c88-44bb-9498-99be0e06a202.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 58a7768e-1c88-44bb-9498-99be0e06a202.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 58a7768e-1c88-44bb-9498-99be0e06a202.cpp:23: 58a7768e-1c88-44bb-9498-99be0e06a202.cpp: In function ‘int main()’: 58a7768e-1c88-44bb-9498-99be0e06a202.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 58a7768e-1c88-44bb-9498-99be0e06a202.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,6 +1,5 @@
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
-    vector<float> result(l);
 
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -13,10 +12,24 @@ vector<float> sort_even(vector<float> l) {
     int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            result[i] = even_values[even_index];
+            l[i] = even_values[even_index];
             even_index++;
         }
     }
 
-    return result;
+    return l;
+}
+
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
