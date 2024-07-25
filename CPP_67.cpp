ea410ad0622872f1e2dc814e994faad884ed6632
beep boop <githubbot@vadim.me>
2024-07-25 19:55:00 +0200

    Solution 30 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/g1je2k2o Prompt: 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:1:24: error: ‘string’ was not declared in this scope     1 | int fruit_distribution(string s, int n){       |                        ^~~~~~ 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:1:34: error: expected primary-expressio n before ‘int’     1 | int fruit_distribution(string s, int n){       |                                  ^~~ 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:1:39: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int fruit_distribution(string s, int n){       |                                       ^ In file included from 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:9: 2f76daae-ae12-4295-9430-b2c70381dde1.cpp: In function ‘int main()’: 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:11:31: error: ‘fruit_distribution’ cann ot be used as a function    11 |     assert (fruit_distribution("1 apples and 100 oranges",120) == 19);       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,15 +1,1 @@
-#include <cassert>
-#include <cstdio>
 #include <string>
-
-int fruit_distribution(const std::string& s, int n) {
-    int apples = 0, oranges = 0;
-    sscanf(s.c_str(), "%d apples and %d oranges", &apples, &oranges);
-    return n - apples - oranges;
-}
-
-int main() {
-    assert(fruit_distribution("1 apples and 100 oranges", 120) == 19);
-    
-    return 0;
-}
