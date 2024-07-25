a89198b636682db4993d6f23b48cea9b8a0fa81b
beep boop <githubbot@vadim.me>
2024-07-25 04:44:46 +0200

    Solution 8 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jfp35lqu Prompt: 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp: In function ‘std::string get_closest_v owel(std::string)’: 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp:9:26: error: request for member ‘find’ in ‘"aeiouAEIOU"’, which is of non-class type ‘const char [11]’     9 |         if ("aeiouAEIOU".find(word[i]) != string::npos && isalpha(word[i -1]) && !isvowel(word[i-1])) {       |                          ^~~~ 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp:9:82: error: ‘isvowel’ was not declared  in this scope; did you mean ‘islower’?     9 | OU".find(word[i]) != string::npos && isalpha(word[i-1]) && !isvowel(word [i-1])) {       |                                                             ^~~~~~~       |                                                             islower 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp: In function ‘bool isvowel(char)’: 681b1817-6e0d-4a1a-8407-6a87c2a32f32.cpp:17:25: error: request for member ‘find’  in ‘"aeiouAEIOU"’, which is of non-class type ‘const char [11]’    17 |     return "aeiouAEIOU".find(c) != string::npos;       |                         ^~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -3,17 +3,18 @@
 
 using namespace std;
 
-bool isvowel(char c) {
-    string vowels = "aeiouAEIOU";
-    return vowels.find(to_string(c)) != string::npos;
-}
-
 string get_closest_vowel(string word) {
     int n = word.size();
+    string vowels("aeiouAEIOU");
     for (int i = n - 1; i > 0; --i) {
-        if ("aeiouAEIOU".find(1, 1, to_string(word[i])) == string::npos && isalpha(word[i-1]) && !isvowel(word[i-1])) {
+        if (vowels.find(word[i]) != string::npos && isalpha(word[i-1]) && !isvowel(word[i-1])) {
             return string(1, word[i]);
         }
     }
     return "";
+}
+
+bool isvowel(char c) {
+    string vowels("aeiouAEIOU");
+    return vowels.find(tolower(c)) != string::npos;
 }
