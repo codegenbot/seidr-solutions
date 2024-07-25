a6c913c3c1bebf6182a71dc1b064da82ac6444f7
beep boop <githubbot@vadim.me>
2024-07-25 09:14:37 +0200

    Solution 4 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/761rt4ln Prompt:     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; f09bd3eb-aa52-48b9-bf0c-405ba40d3409.cpp:8:20: error: ‘abs’ was not declared in this scope     8 |         int diff = abs(nums[i] - nums[0]);       |                    ^~~ f09bd3eb-aa52-48b9-bf0c-405ba40d3409.cpp: In function ‘int main()’: f09bd3eb-aa52-48b9-bf0c-405ba40d3409.cpp:19:5: error: ‘cin’ was not declared in this scope    19 |     cin >> n;       |     ^~~ f09bd3eb-aa52-48b9-bf0c-405ba40d3409.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; f09bd3eb-aa52-48b9-bf0c-405ba40d3409.cpp:25:5: error: ‘cout’ was not declared in  this scope    25 |     cout << "[";       |     ^~~~ f09bd3eb-aa52-48b9-bf0c-405ba40d3409.cpp:25:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,14 +1,13 @@
 #include <vector>
-#include <climits>
+#include <cmath>
 #include <iostream>
-
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
     int minDiff = INT_MAX;
     int cutIndex = 0;
     for (int i = 1; i < nums.size(); ++i) {
-        int diff = abs(nums[i] - nums[0]);
+        int diff = std::abs(nums[i] - nums[0]);
         if (diff <= minDiff) {
             minDiff = diff;
             cutIndex = i;
