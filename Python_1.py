10963990d58f8716285c84c30bf4b617cc228ce4
beep boop <githubbot@vadim.me>
2024-07-25 02:22:23 +0200

    Solution 42 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/aw3o62pc Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a6954fbd-9625-4035-a70c-21edb1f46 409.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -8,22 +8,20 @@ def separate_paren_groups(paren_string: str) -> List[str]:
     temp = ""
 
     for char in paren_string:
-        if not char.isprintable():  # handle edge case of empty string or only spaces
-            continue
         if char == " ":
+            continue
+        if char == "(":
             if temp:  
                 result.append(temp)
                 temp = ""
-        elif char == "(":
             stack.append(char)
             temp += char
         elif char == ")":
-            if stack and stack[-1] == "(":
-                stack.pop()
-                temp += char
-                if not stack and temp:
-                    result.append(temp)
-                    temp = ""
+            stack.pop()
+            temp += char
+            if not stack and temp:
+                result.append(temp)
+                temp = ""
 
     if not stack and temp:
         result.append(temp)
