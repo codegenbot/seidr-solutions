1ee6b0ab4a589ecce4a5e30c36603b7d0f4b1308
beep boop <githubbot@vadim.me>
2024-07-25 08:02:24 +0200

    Solution 51 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fnrxux41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3a32b5de-527c-4ffc-9659-2303e0400 a45.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_129.py
+++ Python_129.py
@@ -1,8 +1,1 @@
-min_paths = [[1, 3], [7, 4], [5, 6]]
-k = 2
-sorted(
-    [
-        f"{path[1]}, {path[0]}"
-        for _, path in sorted((sum(path), path) for path in min_paths[:k])
-    ]
-)
+print(sorted(['{}, {}'.format(*path) for _, path in sorted((sum(path), path) for path in min_paths[:k]))])
