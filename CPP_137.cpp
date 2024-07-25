4df5bc7e2d4ce56fb78ae99dc757750948c5039d
beep boop <githubbot@vadim.me>
2024-07-25 06:20:58 +0200

    Solution 81 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y5jhcg27 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,24 +1,27 @@
-#include <boost/any.hpp>
-
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any_cast<float>(b);
-    } else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        return boost::any_cast<std::string>(b);
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return a;
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
-        return b;
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        if (boost::any_cast<std::string>(a) >= boost::any_cast<std::string>(b))
-            return a;
-        else
-            return b;
+if (a.type() == typeid(int) && b.type() == typeid(int)) {
+    return boost::any(((int)a) > ((int)b) ? a : b);
+} else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+    return boost::any(((float)a) > ((float)b) ? a : b);
+} else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+    return boost::any(((double)a) > ((double)b) ? a : b);
+} else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+    string s1 = any_cast<string>(a), s2 = any_cast<string>(b);
+    size_t p1 = s1.find('.');
+    size_t p2 = s2.find('.');
+    if (p1 != string::npos) {
+        s1 = s1.substr(0, p1);
     }
-    if (boost::any_cast<float>(a) > boost::any_cast<float>(b))
+    if (p2 != string::npos) {
+        s2 = s2.substr(0, p2);
+    }
+    return boost::any(s1 > s2 ? a : b);
+} else if (a.type() == typeid(string)) {
+    return b;
+} else if (b.type() == typeid(string)) {
+    return a;
+} else {
+    if (any_cast<double>(a) > any_cast<double>(b))
         return a;
-    else if (boost::any_cast<float>(a) < boost::any_cast<float>(b))
-        return b;
     else
-        return "None";
+        return b;
 }
