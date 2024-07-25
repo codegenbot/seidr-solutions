fb1d40f0dba272c6ecd90f8eab076e24751b6eb9
beep boop <githubbot@vadim.me>
2024-07-25 19:59:59 +0200

    Solution 34 of CPP/67, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/g1je2k2o Prompt: 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:1:24: error: ‘string’ was not declared in this scope     1 | int fruit_distribution(string s, int n){       |                        ^~~~~~ 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:1:34: error: expected primary-expressio n before ‘int’     1 | int fruit_distribution(string s, int n){       |                                  ^~~ 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:1:39: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int fruit_distribution(string s, int n){       |                                       ^ In file included from 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:9: 2f76daae-ae12-4295-9430-b2c70381dde1.cpp: In function ‘int main()’: 2f76daae-ae12-4295-9430-b2c70381dde1.cpp:11:31: error: ‘fruit_distribution’ cann ot be used as a function    11 |     assert (fruit_distribution("1 apples and 100 oranges",120) == 19);       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,1 +1,9 @@
 #include <string>
+
+int fruit_distribution(std::string s, int n){
+    size_t pos = s.find("apples");
+    int apples = std::stoi(s.substr(0, pos));
+    pos = s.find("oranges");
+    int oranges = std::stoi(s.substr(s.find("apples") + 7, pos - s.find("apples") - 8));
+    return n - apples - oranges;
+}
