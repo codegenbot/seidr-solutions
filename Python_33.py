08d258147ab43cbbaf51dce86fe9b71115f380c8
beep boop <githubbot@vadim.me>
2024-07-25 06:59:11 +0200

    Solution 45 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: 145.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e65654c7-86ec-4eac-858a-f46e8d84c 145.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e65654c7-86ec-4eac-858a-f46e8d84c 145.py", line 4, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e65654c7-86ec-4eac-858a-f46e8d84c 145.py", line 6, in <listcomp>     [tuple(i) for i in l[i : i + 3]],     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e65654c7-86ec-4eac-858a-f46e8d84c 145.py", line 6, in <listcomp>     [tuple(i) for i in l[i : i + 3]],      ^^^^^^^^ TypeError: 'int' object is not iterable

--- Python_33.py
+++ Python_33.py
@@ -3,8 +3,8 @@ def sort_third(l: list):
     l += [(float("inf"), float("inf"), float("inf"))] * remainder
     return [
         sorted(
-            l[i : i + 3],
-            key=lambda x: x[2] if isinstance(x, tuple) and len(x) == 3 else float("inf"),
+            [tuple([i[j] for j in range(3)]) for i in l[i : i + 3],
+            key=lambda x: x[2] if isinstance(x, tuple) and len(x) == 3 else float("inf")
         )
         for i in range(0, len(l), 3)
     ]
