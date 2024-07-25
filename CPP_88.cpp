c27219773c1ec80ab8f3bdefa90bf047d0f08e18
beep boop <githubbot@vadim.me>
2024-07-25 06:17:09 +0200

    Solution 90 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lh88s7u2 Prompt: 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:31: 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp: In function ‘int main()’: 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:41:20: error: ‘sort_array’ was not decl ared in this scope; did you mean ‘ArrayUtils::sort_array’?    41 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:16:22: note: ‘ArrayUtils::sort_array’ d eclared here    16 |     std::vector<int> sort_array(std::vector<int> array) {       |                      ^~~~~~~~~~ In file included from 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:31: 6293cd0c-263b-4292-b160-2cdf64b2cd51.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -23,5 +23,7 @@ namespace ArrayUtils {
     }
 }
 
-assert(ArrayUtils::issame(ArrayUtils::sort_array({21, 14, 23, 11}), std::vector<int>{23, 21, 14, 11}));
-return 0;
+int main() {
+    assert(ArrayUtils::issame(ArrayUtils::sort_array({21, 14, 23, 11}), std::vector<int>{23, 21, 14, 11}));
+    return 0;
+}
