861ed51381200d5ee09cbe0c77d738c29abff11d
beep boop <githubbot@vadim.me>
2024-07-25 09:02:05 +0200

    Solution 9 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/v2bh0ofi Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c39e78ab-d36d-4351-99ed-c8447e0038db.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c39e78ab-d36d-4351-99ed-c8447e0038db.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c39e78ab-d36d-4351-99ed-c8447e0038db.cpp:23: c39e78ab-d36d-4351-99ed-c8447e0038db.cpp: In function ‘int main()’: c39e78ab-d36d-4351-99ed-c8447e0038db.cpp:33:20: error: ‘separate_paren_groups’ w as not declared in this scope    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ c39e78ab-d36d-4351-99ed-c8447e0038db.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,31 +1,21 @@
-#include <vector>
-#include <string>
-
-bool issame(vector<string> a, vector<string> b);
-
-int main() {
-    assert(issame({separate_paren_groups("( ) (( )) (( )( ))")}, {{"()", "(())", "(()())"}});
-    // ...
-}
-
-vector<string> separate_paren_groups(string paren_string) {
-    vector<string> result;
-    string temp = "";
-    int count = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            count++;
-            temp += c;
-        } else if (c == ')') {
-            count--;
-            temp += c;
-            if (count == 0) {
-                result.push_back(temp);
-                temp = "";
-            }
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        string str1 = a[i];
+        string str2 = b[i];
+        if (str1.length() != str2.length())
+            return false;
+        int count = 0, j = 0;
+        while (j < str1.length()) {
+            if (str1[j] == '(')
+                count++;
+            else if (str1[j] == ')')
+                count--;
+            j++;
         }
+        if (count != 0)
+            return false;
     }
-
-    return result;
+    return true;
 }
