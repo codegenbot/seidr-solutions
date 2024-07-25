05ca1af721042739168035dcb0bd6001eb56c248
beep boop <githubbot@vadim.me>
2024-07-25 04:54:38 +0200

    Solution 23 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bxl6o77x Prompt:       |                    ^~~ 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp:15: 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp: In function ‘int main()’: 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp:25:20: error: ‘sort_array’ was not decl ared in this scope    25 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 9d5f44f5-40cd-4331-8f11-310a77057f9d.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,14 +1,21 @@
-int issame(int a, int b) {
-    int ones_a = __builtin_popcount(a);
-    int ones_b = __builtin_popcount(b);
-    if (ones_a == ones_b)
-        return a < b;
-    else
-        return ones_a < ones_b;
+int __builtin_popcount(int x) {
+    int count = 0;
+    for (int i = 0; i < 32; i++) {
+        count += ((x >> i) & 1);
+    }
+    return count;
 }
 
 vector<int> sort_array(vector<int> arr) {
     vector<int> result = arr;
-    sort(result.begin(), result.end(), issame);
+    sort(result.begin(), result.end(),
+         [](int a, int b) {
+             int ones_a = __builtin_popcount(a);
+             int ones_b = __builtin_popcount(b);
+             if (ones_a == ones_b)
+                 return a < b;
+             else
+                 return ones_a < ones_b;
+         });
     return result;
 }
