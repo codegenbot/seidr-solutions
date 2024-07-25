3b889c96e9a61cc6cb89d38096db04b83f909e34
beep boop <githubbot@vadim.me>
2024-07-25 06:24:05 +0200

    Solution 34 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/j8yxpsc6 Prompt: 226 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51eb9b15-7cc7-4852-871a-cfc7e6d60 dd7.py", line 20, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/51eb9b15-7cc7-4852-871a-cfc7e6d60 dd7.py", line 17, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_66.py
+++ Python_66.py
@@ -1,8 +1,19 @@
 def digitSum(s):
-    total = 0
-    for char in s:
-        if char.isdigit():
-            total += int(char)
-    return total
+    def get_digit_sum(sub_s):
+        total = 0
+        for char in sub_s:
+            if char.isdigit():
+                total += int(char)
+        return total
+
+    def get_char_sum(sub_s):
+        total = 0
+        for char in sub_s:
+            if not char.isalnum():
+                continue
+            total += (ord(char.lower()) - 96) if char.islower() else (ord(char) - 64)
+        return total
+
+    return get_digit_sum(s) + get_char_sum(s)
 
 print(digitSum("You arE Very Smart"))
