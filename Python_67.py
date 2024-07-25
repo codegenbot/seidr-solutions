5e8c955f7607e0e17aeb1510314deb4671565e29
beep boop <githubbot@vadim.me>
2024-07-25 06:34:34 +0200

    Solution 38 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d0d317f-0d4c-4829-9091-5a5747d90 dc6.py", line 15, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d0d317f-0d4c-4829-9091-5a5747d90 dc6.py", line 12, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,8 +1,8 @@
 def fruit_distribution(s, n):
     s = s.replace("apples", "1").replace("oranges", "0")
     parts = s.split()
-    total_count = 0
+    total_count = int(n)  
     for i in range(len(parts)):
         words = [x for x in parts[i].split() if x.isdigit()]
         total_count += sum(int(word) for word in words)
-    return n - total_count - int(s.count('1')) - int(s.count('0'))
+    return n - total_count
