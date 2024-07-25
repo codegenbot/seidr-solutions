55f2b62102b660c3a5871bb42f8bc2bd646e6d42
beep boop <githubbot@vadim.me>
2024-07-25 17:58:38 +0200

    Solution 64 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a9jr89yt Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,27 +1,22 @@
 if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
-        return a;
-    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)){
-        return b;
-    } else {
-        return "None";
-    }
-} else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
-        return a;
-    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)){
-        return b;
-    } else {
-        return "None";
-    }
-} else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))){
-        return a;
-    } else if (stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))){
-        return b;
-    } else {
-        return "None";
-    }
-} else {
-    return "None";
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) return a;
+    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) return b;
+    else return "None";
+}
+else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) return a;
+    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) return b;
+    else return "None";
+}
+else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    float float_a, float_b;
+    if(a.type() == typeid(int)) float_a = boost::any_cast<int>(a);
+    else float_a = stof(boost::any_cast<string>(a));
+    if(b.type() == typeid(int)) float_b = boost::any_cast<int>(b);
+    else float_b = stof(boost::any_cast<string>(b));
+    
+    if(float_a > float_b) return a;
+    else if(float_a < float_b) return b;
+    else return "None";
+}
 }
