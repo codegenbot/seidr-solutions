d95f346b499ebe94ec61abf92cb86d804ba9911b
beep boop <githubbot@vadim.me>
2024-07-25 03:29:53 +0200

    Solution 8 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/433ac7db-8a6d-4673-b2ee-9cd8c60ff 9b5.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/433ac7db-8a6d-4673-b2ee-9cd8c60ff 9b5.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,2 +1,7 @@
 def sort_third(l):
-    return [x if i % 3 != 0 else sorted([y for j, y in enumerate(l) if (j - i) % 3 != 0 and j % 3 != 0]) for i, x in enumerate(l)]
+    sorted_middle = sorted([x for i, x in enumerate(l) if i % 3 != 0])
+    return (
+        [x for i, x in enumerate(l) if i % 3 == 0]
+        + [y for y in sorted_middle]
+        + [x for i, x in enumerate(l) if i % 3 != 0 and x not in sorted_middle]
+    )
