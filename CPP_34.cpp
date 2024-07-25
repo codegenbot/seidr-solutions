3d49adc5519ac44ac896394b9f495e153f72ac3a
beep boop <githubbot@vadim.me>
2024-07-25 02:06:48 +0200

    Solution 52 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ib89wtms Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 242f5e0d-7aa8-40ca-a6a4-1eb68f07655d.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 242f5e0d-7aa8-40ca-a6a4-1eb68f07655d.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 242f5e0d-7aa8-40ca-a6a4-1eb68f07655d.cpp:13: 242f5e0d-7aa8-40ca-a6a4-1eb68f07655d.cpp: In function ‘int main()’: 242f5e0d-7aa8-40ca-a6a4-1eb68f07655d.cpp:23:20: error: ‘unique’ was not declared  in this scope    23 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 242f5e0d-7aa8-40ca-a6a4-1eb68f07655d.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,11 +1,11 @@
-```cpp
 vector<int> removeDuplicates(vector<int> l){
     sort(l.begin(), l.end());
-    l.erase(unique(l.begin(), l.end()), l.end());
+    vector<int> temp = l;
+    l.erase(unique(temp.begin(), temp.end()), l.end()); 
     return l;
 }
 
-bool areSetsSame(vector<int> a, vector<int> b) {
+bool checkEquality(vector<int> a, vector<int> b) {
     set<int> s1(a.begin(), a.end());
     set<int> s2(b.begin(), b.end());
     return s1 == s2;
