46773a1e1d1109ce868de34a6b61c77248058d3c
beep boop <githubbot@vadim.me>
2024-07-25 17:58:46 +0200

    Solution 65 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a9jr89yt Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,22 +1,25 @@
 if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)) return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)) return b;
-    else return "None";
-}
-else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)) return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)) return b;
-    else return "None";
-}
-else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    float float_a, float_b;
-    if(a.type() == typeid(int)) float_a = boost::any_cast<int>(a);
-    else float_a = stof(boost::any_cast<string>(a));
-    if(b.type() == typeid(int)) float_b = boost::any_cast<int>(b);
-    else float_b = stof(boost::any_cast<string>(b));
-    
-    if(float_a > float_b) return a;
-    else if(float_a < float_b) return b;
-    else return "None";
-}
+        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+            return a;
+        }else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+            return b;
+        }
+    }else if(a.type() == typeid(float) && b.type() == typeid(float)){
+        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+            return a;
+        }else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+            return b;
+        }
+    }else if(a.type() == typeid(string) && b.type() == typeid(string)){
+        string strA = boost::any_cast<string>(a);
+        string strB = boost::any_cast<string>(b);
+        if(strA.find(',') != string::npos) replace(strA.begin(), strA.end(), ',', '.');
+        if(strB.find(',') != string::npos) replace(strB.begin(), strB.end(), ',', '.');
+        if(stof(strA) > stof(strB)){
+            return a;
+        }else if(stof(strA) < stof(strB)){
+            return b;
+        }
+    }
+    return "None";
 }
