076e03e972e1d341b82c18a22e72a3c559988d39
beep boop <githubbot@vadim.me>
2024-07-25 06:21:34 +0200

    Solution 22 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2wc0s9lo Prompt:       |                       ^~~~~~ 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp:1:31: error: ‘operato’ was not declared  in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                               ^~~~~~~ 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp:1:40: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                        ^~~~~~ 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp:1:47: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                               ^~~ 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp:1:59: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operato, vector<int> operand) {       |                                                           ^ In file included from 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp:47: 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp: In function ‘int main()’: 51818dd5-ada3-4e2e-8710-2889aca1016c.cpp:49:23: error: ‘do_algebra’ cannot be us ed as a function    49 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <string>
 #include <cmath>
@@ -11,88 +12,31 @@ int do_algebra(vector<string> operato, vector<int> operand) {
             result -= operand[i];
         } else if (operato[i] == "*") {
             int temp = 1;
-            for (int j = i; j < operato.size(); ) {
-                if (operato[j] == "+") {
-                    ++j;
-                    continue;
-                }
-                if (operato[j] == "-") {
-                    ++j;
-                    continue;
-                }
-                if (operato[j] == "*") {
+            for (int j = i; j < operato.size(); j++) {
+                if (operato[j] == "*" || operato[j] == "**") {
                     temp *= operand[j];
-                    ++j;
                 } else if (operato[j] == "/") {
                     temp /= operand[j];
-                    ++j;
-                } else if (operato[j] == "//") {
-                    temp /= operand[j];
-                    while(j < operato.size() && operato[j] == "//")
-                        j++;
-                } else if (operato[j] == "**") {
-                    temp = pow(temp, operand[j]);
-                    while(j < operato.size() && operato[j] == "**")
-                        j++;
                 }
             }
             result += temp;
         } else if (operato[i] == "//") {
             int temp = 1;
-            for (int j = i; j < operato.size(); ) {
-                if (operato[j] == "+") {
-                    ++j;
-                    continue;
-                }
-                if (operato[j] == "-") {
-                    ++j;
-                    continue;
-                }
-                if (operato[j] == "*") {
-                    ++j;
-                    continue;
-                } else if (operato[j] == "/") {
+            for (int j = i; j < operato.size(); j++) {
+                if (operato[j] == "//") {
                     temp /= operand[j];
-                    while(j < operato.size() && operato[j] == "//")
-                        j++;
-                } else if (operato[j] == "//") {
-                    temp /= operand[j];
-                    while(j < operato.size() && operato[j] == "**")
-                        j++;
                 } else if (operato[j] == "**") {
                     temp = pow(temp, operand[j]);
-                    while(j < operato.size() && operato[j] == "//")
-                        j++;
                 }
             }
             result += temp;
         } else if (operato[i] == "**") {
             int temp = 1;
-            for (int j = i; j < operato.size(); ) {
-                if (operato[j] == "+") {
-                    ++j;
-                    continue;
-                }
-                if (operato[j] == "-") {
-                    ++j;
-                    continue;
-                }
-                if (operato[j] == "*") {
-                    temp *= operand[j];
-                    while(j < operato.size() && operato[j] == "**")
-                        j++;
+            for (int j = i; j < operato.size(); j++) {
+                if (operato[j] == "*" || operato[j] == "//" || operato[j] == "**") {
+                    temp *= pow(temp, operand[j]);
                 } else if (operato[j] == "/") {
                     temp /= operand[j];
-                    while(j < operato.size() && operato[j] == "**")
-                        j++;
-                } else if (operato[j] == "//") {
-                    temp /= operand[j];
-                    while(j < operato.size() && operato[j] == "*")
-                        j++;
-                } else if (operato[j] == "**") {
-                    temp = pow(temp, operand[j]);
-                    while(j < operato.size() && operato[j] == "*")
-                        j++;
                 }
             }
             result += temp;
