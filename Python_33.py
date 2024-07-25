8f27a0136dc22c5930e0e155726cb9b8a70c8fdb
beep boop <githubbot@vadim.me>
2024-07-25 04:46:15 +0200

    Solution 48 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e9451b8-76bd-4741-a164-67fe418ab e5d.py", line 21, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7e9451b8-76bd-4741-a164-67fe418ab e5d.py", line 16, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -2,14 +2,11 @@ def sort_third(l):
     result = []
     while len(l) > 0:
         if len(l) >= 3:
-            while len(l) >= 3:
-                temp = l[:3]
-                del l[:3]
-                temp.sort()
-                result.append(tuple(temp))
+            temp = l[:3]
+            del l[:3]
+            temp.sort()
+            result.append(tuple(temp))
         else:
-            if len(l) > 0:
-                temp = tuple(sorted(l))
-                del l[:]
-                result.append(temp)
+            result.append(tuple(sorted(l)))
+            break
     return tuple(result)
