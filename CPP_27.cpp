cca7f02712f77da140fd57c551ffcc8f539e5ca3
beep boop <githubbot@vadim.me>
2024-07-25 02:16:38 +0200

    Solution 69 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/p676hkjs Prompt: Complete the following code given the task description and function signature.

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,10 +1,7 @@
-string filp_case(string str){
-    string result = "";
-    for(int i=0; i<str.length(); i++){
-        if(str[i] >= 'a' && str[i] <= 'z')
-            result += toupper(str[i]);
-        else
-            result += tolower(str[i]);
-    }
-    return result;
+for(int i=0; i<str.length(); i++){
+    if(str[i] >= 'a' && str[i] <= 'z')
+        str[i] = toupper(str[i]);
+    else
+        str[i] = tolower(str[i]);
 }
+return str;
