19ce60e9e2c83356c912ee944254cb1d53e03a90
beep boop <githubbot@vadim.me>
2024-07-25 09:02:10 +0200

    Solution 12 of CPP/15, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/5ikpqz8r Prompt: 48e17acb-749b-4a6d-b0a1-63f40a5f93b1.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string string_sequence(int n) {       | ^~~~~~ In file included from 48e17acb-749b-4a6d-b0a1-63f40a5f93b1.cpp:9: 48e17acb-749b-4a6d-b0a1-63f40a5f93b1.cpp: In function ‘int main()’: 48e17acb-749b-4a6d-b0a1-63f40a5f93b1.cpp:11:13: error: ‘string_sequence’ was not  declared in this scope    11 |     assert (string_sequence(10) == "0 1 2 3 4 5 6 7 8 9 10");       |             ^~~~~~~~~~~~~~~

--- CPP_15.cpp
+++ CPP_15.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <sstream>
-using namespace std;
 
 string string_sequence(int n) {
     ostringstream oss;
@@ -8,9 +7,4 @@ string string_sequence(int n) {
         oss << i << " ";
     }
     return oss.str();
-}
-
-int main() {
-    assert(string_sequence(10) == "0 1 2 3 4 5 6 7 8 9 10");
-    return 0;
 }
