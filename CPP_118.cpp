be6fd9f8de114aac01eb30e6ba6890d8b769aec2
beep boop <githubbot@vadim.me>
2024-07-25 05:35:12 +0200

    Solution 47 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/jfp35lqu Prompt: e76ef45f-c437-4f04-a179-a6fa6e55cfbd.cpp: In function ‘std::string get_closest_v owel(std::string)’: e76ef45f-c437-4f04-a179-a6fa6e55cfbd.cpp:14:26: error: request for member ‘find’  in ‘"aeiouAEIOU"’, which is of non-class type ‘const char [11]’    14 |         if ("aeiouAEIOU".find(1, 1, to_string(word[i])) == string::npos && isalpha(word[i-1]) && !isvowel(word[i-1])) {       |                          ^~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 bool isvowel(char c) {
     string vowels = "aeiouAEIOU";
-    return vowels.find(string(1, c)) != string::npos;
+    return vowels.find(string(1,c)) != string::npos;
 }
 
 string get_closest_vowel(string word) {
     int n = word.size();
     for (int i = n - 1; i > 0; --i) {
-        if ("aeiouAEIOU".find(string(1, word[i])) == string::npos && isalpha(word[i-1]) && !isvowel(word[i-1])) {
+        if ("aeiouAEIOU".find(string(1,word[i])) == string::npos && isalpha(word[i-1]) && !isvowel(word[i-1])) {
             return string(1, word[i]);
         }
     }
