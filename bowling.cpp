0c5d47a01701160fb275e7f6f99d222de56eab91
beep boop <githubbot@vadim.me>
2024-07-25 09:09:38 +0200

    Solution 2 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/txuepyg8 Prompt: 65dfe094-3deb-4932-8e05-0e68a2d0962b.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,24 +1,29 @@
-int bowlingScore(string input) {
-    int score = 0;
-    int roll1 = 0, roll2 = 0;
+```cpp
+#include <string>
 
-    for (int i = 0; i < 10; ++i) {
-        if (input[i] == 'X') { // strike
-            score += 10 + bowlingScore(input.substr(i+1));
-        } else if (input[i] == '/') { // spare
-            roll1 = input[i+2] - '0';
+int bowlingScore(string s) {
+    int score = 0;
+    int i = 0;
+    while (i < s.size()) {
+        if (s[i] == 'X') {
             score += 10;
-            i++; // move to the next frame
-        } else {
-            roll1 = input[i] - '0';
-            roll2 = input[i+1] - '0';
-            if (i < 8 && input[i+2] == '/') { // spare in next frame, add it now
-                score += roll1 + roll2;
-            } else {
-                score += roll1 + roll2;
+            i++;
+            if (i < s.size() && s[i] != '/') {
+                score += 10;
+                i++;
             }
+        } else if (s[i] - '0' + s[i+1] - '0' > 10) {
+            int frame = 10;
+            if (i+2 < s.size()) {
+                frame = 10 - (s[i] - '0' + s[i+1] - '0');
+            }
+            score += frame;
+            i+=2;
+        } else {
+            int frame = s[i] - '0' + s[i+1] - '0';
+            score += frame;
+            i+=2;
         }
     }
-
     return score;
 }
