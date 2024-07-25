0dd20869b18493aaf7d9ae0193b482a8e84f172b
beep boop <githubbot@vadim.me>
2024-07-25 09:21:23 +0200

    Solution 36 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/wr6it0u6 Prompt:       |                              ^~~~~~ f2436593-74df-490f-a2bc-7732a3c6999f.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f2436593-74df-490f-a2bc-7732a3c6999f.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f2436593-74df-490f-a2bc-7732a3c6999f.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f2436593-74df-490f-a2bc-7732a3c6999f.cpp:21: f2436593-74df-490f-a2bc-7732a3c6999f.cpp: In function ‘int main()’: f2436593-74df-490f-a2bc-7732a3c6999f.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ f2436593-74df-490f-a2bc-7732a3c6999f.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,19 +1,11 @@
 #include <string>
 #include <vector>
 
-vector<string> numerical_letter_grade(vector<float> grades);
-
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
+
+vector<string> numerical_letter_grade(vector<float> grades);
 
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
