7f37afaca7d230dcf63c24324e69628135bbd2ae
beep boop <githubbot@vadim.me>
2024-07-25 06:52:54 +0200

    Solution 49 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nwokb2ad Prompt: 5150a456-8133-434d-bf0b-1c902fc1ea39.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 5150a456-8133-434d-bf0b-1c902fc1ea39.cpp:40:5: note: ‘int main()’ previously def ined here    40 | int main() {       |     ^~~~ In file included from 5150a456-8133-434d-bf0b-1c902fc1ea39.cpp:46: 5150a456-8133-434d-bf0b-1c902fc1ea39.cpp: In function ‘int main()’: 5150a456-8133-434d-bf0b-1c902fc1ea39.cpp:56:20: error: ‘generate_integers’ was n ot declared in this scope    56 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -3,40 +3,38 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+struct Solution {
+    static bool issame(vector<int> a, vector<int> b) {
+        if (a.size() != b.size()) {
             return false;
         }
+        
+        for (size_t i = 0; i < a.size(); ++i) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        
+        return true;
     }
-    
-    return true;
-}
 
-vector<int> generate_integers(int a, int b) {
-    vector<int> result;
-    for (int i = a; i <= b; ++i) {
-        result.push_back(i);
-    }
-    return result;
-}
-
-vector<int> filter_even_numbers(vector<int> a) {
-    vector<int> result;
-    for (int i : a) {
-        if (i % 2 == 0) {
+    static vector<int> generate_integers(int a, int b) {
+        vector<int> result;
+        for (int i = a; i <= b; ++i) {
             result.push_back(i);
         }
+        return result;
     }
-    return result;
-}
 
-int main() {
-    assert(issame(filter_even_numbers(generate_integers(17, 89)), vector<int>{}));
-    
-    return 0;
-}
+    static vector<int> filter_even_numbers(vector<int> a) {
+        vector<int> result;
+        for (int i : a) {
+            if (i % 2 == 0) {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
+
+return 0;
