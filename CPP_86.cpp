7e33ae00ecdab0bf0b40ddd397df5c7951ce19ac
beep boop <githubbot@vadim.me>
2024-07-25 05:24:15 +0200

    Solution 52 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a4yl2ltl Prompt: In file included from 17da0ba8-2bb7-459e-bfb0-7425112aad3b.cpp:29: 17da0ba8-2bb7-459e-bfb0-7425112aad3b.cpp: In function ‘int main()’: 17da0ba8-2bb7-459e-bfb0-7425112aad3b.cpp:31:13: error: ‘anti_shuffle’ was not de clared in this scope; did you mean ‘Contest::anti_shuffle’?    31 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~ 17da0ba8-2bb7-459e-bfb0-7425112aad3b.cpp:6:17: note: ‘Contest::anti_shuffle’ dec lared here     6 |     std::string anti_shuffle(std::string s) {       |                 ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,1 +1,27 @@
-assert(Contest::anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
+#include <algorithm>
+#include <string>
+#include <cassert>
+
+namespace Contest {
+    std::string anti_shuffle(std::string s) {
+        std::string result = "";
+        std::string word = "";
+        for (char c : s) {
+            if (c == ' ') {
+                std::sort(word.begin(), word.end(), std::greater<char>());
+                result += word + ' ';
+                word = "";
+            } else {
+                word += c;
+            }
+        }
+        std::sort(word.begin(), word.end(), std::greater<char>());
+        result += word;
+        return result;
+    }
+}
+
+int main() {
+    assert(Contest::anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
+    return 0;
+}
