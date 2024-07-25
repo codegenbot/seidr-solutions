8fc0b0b924b17713418d270f0d621f0779227990
beep boop <githubbot@vadim.me>
2024-07-25 08:02:00 +0200

    Solution 50 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fnrxux41 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c523479f-e1e1-4b8a-b48e-1ab4b6877 00c.py", line 4, in <module>     for _, path in sorted((sum(path), path) for path in min_paths[:k])                                                         ^^^^^^^^^ NameError: name 'min_paths' is not defined

--- Python_129.py
+++ Python_129.py
@@ -1,5 +1,8 @@
-```
-k = 3
-min_paths = [[1, 2], [2, 3], [3, 4], [4, 5]]
-
-sorted([f"{path[1]}, {path[0]}" for _, path in sorted((sum(path), path) for path in min_paths[:k])])
+min_paths = [[1, 3], [7, 4], [5, 6]]
+k = 2
+sorted(
+    [
+        f"{path[1]}, {path[0]}"
+        for _, path in sorted((sum(path), path) for path in min_paths[:k])
+    ]
+)
