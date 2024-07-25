82ed0915f5cb6a8ab515b24c8b0cbb4f7c4a7e11
beep boop <githubbot@vadim.me>
2024-07-25 05:51:32 +0200

    Solution 90 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vrgqwq18 Prompt: ea200b5a-3eeb-4bd5-963c-25e5d95b7de2.cpp: In function ‘std::string string_to_md5 (std::string)’: ea200b5a-3eeb-4bd5-963c-25e5d95b7de2.cpp:24:55: error: cannot convert ‘size_t*’ {aka ‘long unsigned int*’} to ‘unsigned int*’    24 |     int len_output = EVP_DigestFinal_ex(mdctx, mdSig, &len);       |                                                       ^~~~       |                                                       |       |                                                       size_t* {aka long unsigned int*} In file included from ea200b5a-3eeb-4bd5-963c-25e5d95b7de2.cpp:3: /usr/include/openssl/evp.h:713:49: note:   initializing argument 3 of ‘int EVP_D igestFinal_ex(EVP_MD_CTX*, unsigned char*, unsigned int*)’   713 |                                   unsigned int *s);       |                                   ~~~~~~~~~~~~~~^ ea200b5a-3eeb-4bd5-963c-25e5d95b7de2.cpp: At global scope: ea200b5a-3eeb-4bd5-963c-25e5d95b7de2.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ ea200b5a-3eeb-4bd5-963c-25e5d95b7de2.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -3,32 +3,37 @@
 #include <openssl/evp.h>
 #include <openssl/md5.h>
 
-int main() {
-    std::cout << string_to_md5("password") << std::endl;
-    return 0;
-}
+using namespace std;
 
 std::string string_to_md5(std::string text) {
-    EVP_MD_CTX mdctx; 
-    EVP_MD_CTX_init(&mdctx);
+    EVP_MD_CTX* mdctx;
     unsigned char md[16];
-    std::string str = text;  
-    size_t len = str.length();
+    string str = text;  
+    unsigned int len = str.length();
 
     if (len == 0)
         return "";
 
-    EVP_DigestInit_ex(&mdctx, EVP_md5(), NULL);
-    EVP_DigestUpdate(&mdctx, str.c_str(), len);
+    if ((mdctx = EVP_MD_CTX_new()) == NULL)
+        return "";
+    if (1 != EVP_DigestInit_ex(mdctx, EVP_md5(), NULL))
+        return "";
+    if (1 != EVP_DigestUpdate(mdctx, str.c_str(), len))
+        return "";
     unsigned char mdSig[EVP_MAX_BLOCK_LENGTH];
-    int len_output = EVP_DigestFinal_ex(&mdctx, mdSig, &len);
-    EVP_MD_CTX_free(&mdctx);
+    unsigned int len_output = EVP_DigestFinal_ex(mdctx, mdSig, &len);
+    EVP_MD_CTX_free(mdctx);
 
-    std::string result;
+    string result;
     for (int i = 0; i < len; i++) {
         char buffer[3];
         sprintf(buffer, "%02x", mdSig[i]);
-        result += buffer;
+        result += string(buffer);
     }
     return result;
+}
+
+int main() {
+    cout << string_to_md5("password") << endl;
+    return 0;
 }
