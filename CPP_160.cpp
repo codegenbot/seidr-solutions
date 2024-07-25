b0a1d5b3bb055a9d73f63fe1acbe0a05b47d4789
beep boop <githubbot@vadim.me>
2024-07-25 03:52:01 +0200

    Solution 1 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/54zgao8i Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,15 +1,15 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
+int do_algebra(vector<string> operato, vector<int> operand) {
     int result = operand[0];
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
+    for (int i = 0; i < operato.size(); i++) {
+        if (operato[i] == "+") {
             result += operand[i + 1];
-        } else if (operator_[i] == "-") {
+        } else if (operato[i] == "-") {
             result -= operand[i + 1];
-        } else if (operator_[i] == "*") {
+        } else if (operato[i] == "*") {
             result *= operand[i + 1];
-        } else if (operator_[i] == "//") {
+        } else if (operato[i] == "//") {
             result = result / operand[i + 1];
-        } else if (operator_[i] == "**") {
+        } else if (operato[i] == "**") {
             result = pow(result, operand[i + 1]);
         }
     }
