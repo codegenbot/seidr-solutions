fffc7881d86e98a9b7bb36f9862d7c8a7ade0e7f
beep boop <githubbot@vadim.me>
2024-07-25 02:09:38 +0200

    Solution 27 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/c7fceeas Prompt: hheelllloooolllleehh Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1b533114-f8e5-4106-86aa-052bb2380 3fb.py", line 15, in <module>     check(make_palindrome)           ^^^^^^^^^^^^^^^ NameError: name 'make_palindrome' is not defined

--- Python_10.py
+++ Python_10.py
@@ -1,11 +1,12 @@
 ```
 def solve_task(s):
-    palindrome = "".join(c for c in s if c.isalpha()).lower() + "".join(
+    palindrome = "".join(c for c in s if c.isalpha()) + "".join(
         c for c in s[::-1] if c.isalpha()
-    ).lower()
+    )
     new_s = ""
     for char in palindrome:
         new_s += char * 2
     return new_s
 
-print(solve_task("hello"))
+input_str = input("Enter a string: ")
+print(solve_task(input_str))
