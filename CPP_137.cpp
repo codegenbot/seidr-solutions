fef21a080116ae5fffc37a2bec0e30abf2bf97fe
beep boop <githubbot@vadim.me>
2024-07-25 18:02:20 +0200

    Solution 87 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a9jr89yt Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,53 +1,26 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    int val1 = boost::any_cast<int>(a);
-    int val2 = boost::any_cast<int>(b);
-    if(val1 > val2)
-        return val1;
-    else if(val2 > val1)
-        return val2;
-    else
-        return "None";
-}
-else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    float val1 = boost::any_cast<float>(a);
-    float val2 = boost::any_cast<float>(b);
-    if(val1 > val2)
-        return val1;
-    else if(val2 > val1)
-        return val2;
-    else
-        return "None";
-}
-else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
-    std::string val1 = boost::any_cast<std::string>(a);
-    std::string val2 = boost::any_cast<std::string>(b);
-    float f1 = std::stof(val1);
-    float f2 = std::stof(val2);
-    if(f1 > f2)
-        return val1;
-    else if(f2 > f1)
-        return val2;
-    else
-        return "None";
-}
-else if(a.type() == typeid(std::string) && b.type() != typeid(std::string)){
-    std::string val1 = boost::any_cast<std::string>(a);
-    float f1 = std::stof(val1);
-    if(f1 > boost::any_cast<float>(b))
-        return val1;
-    else if(boost::any_cast<float>(b) > f1)
-        return b;
-    else
-        return "None";
-}
-else if(a.type() != typeid(std::string) && b.type() == typeid(std::string)){
-    std::string val2 = boost::any_cast<std::string>(b);
-    float f2 = std::stof(val2);
-    if(boost::any_cast<float>(a) > f2)
-        return a;
-    else if(f2 > boost::any_cast<float>(a))
-        return val2;
-    else
-        return "None";
-}
+if (a.type() == typeid(int)) {
+        if (b.type() == typeid(int)) {
+            return boost::any_cast<int>(a) > boost::any_cast<int>(b) ? a : (boost::any_cast<int>(a) < boost::any_cast<int>(b) ? b : "None");
+        } else if (b.type() == typeid(float)) {
+            return boost::any_cast<int>(a) > boost::any_cast<float>(b) ? a : (boost::any_cast<int>(a) < boost::any_cast<float>(b) ? b : "None");
+        } else if (b.type() == typeid(string)) {
+            return boost::any_cast<int>(a) > std::stof(boost::any_cast<string>(b)) ? a : (boost::any_cast<int>(a) < std::stof(boost::any_cast<string>(b)) ? b : "None");
+        }
+    } else if (a.type() == typeid(float)) {
+        if (b.type() == typeid(int)) {
+            return boost::any_cast<float>(a) > boost::any_cast<int>(b) ? a : (boost::any_cast<float>(a) < boost::any_cast<int>(b) ? b : "None");
+        } else if (b.type() == typeid(float)) {
+            return boost::any_cast<float>(a) > boost::any_cast<float>(b) ? a : (boost::any_cast<float>(a) < boost::any_cast<float>(b) ? b : "None");
+        } else if (b.type() == typeid(string)) {
+            return boost::any_cast<float>(a) > std::stof(boost::any_cast<string>(b)) ? a : (boost::any_cast<float>(a) < std::stof(boost::any_cast<string>(b)) ? b : "None");
+        }
+    } else if (a.type() == typeid(string)) {
+        if (b.type() == typeid(int)) {
+            return std::stof(boost::any_cast<string>(a)) > boost::any_cast<int>(b) ? a : (std::stof(boost::any_cast<string>(a)) < boost::any_cast<int>(b) ? b : "None");
+        } else if (b.type() == typeid(float)) {
+            return std::stof(boost::any_cast<string>(a)) > boost::any_cast<float>(b) ? a : (std::stof(boost::any_cast<string>(a)) < boost::any_cast<float>(b) ? b : "None");
+        } else if (b.type() == typeid(string)) {
+            return std::stof(boost::any_cast<string>(a)) > std::stof(boost::any_cast<string>(b)) ? a : (std::stof(boost::any_cast<string>(a)) < std::stof(boost::any_cast<string>(b)) ? b : "None");
+        }
+    }
 }
