948239f76f243d12f06d0617e48931126239fd69
beep boop <githubbot@vadim.me>
2024-07-25 09:00:12 +0200

    Solution 6 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/abxe0rwc Prompt:  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 26f9628b-fe7a-4ab0-a00c-27354a85fcb1.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 26f9628b-fe7a-4ab0-a00c-27354a85fcb1.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 26f9628b-fe7a-4ab0-a00c-27354a85fcb1.cpp:13: 26f9628b-fe7a-4ab0-a00c-27354a85fcb1.cpp: In function ‘int main()’: 26f9628b-fe7a-4ab0-a00c-27354a85fcb1.cpp:23:20: error: ‘rolling_max’ was not dec lared in this scope    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 26f9628b-fe7a-4ab0-a00c-27354a85fcb1.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,27 +1,23 @@
 #include <vector>
-#include <algorithm>
+#include <limits>
 
-using namespace std;
-
-bool issame(vector<int> a,vector<int>b){
-    return a.size() == b.size() && a==b;
+bool isSame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
     int max = INT_MIN;
-
     for (int num : numbers) {
         if (num > max) {
             max = num;
         }
         result.push_back(max);
     }
-
     return result;
 }
 
 int main() {
-    assert(issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100}));
+    assert(isSame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100}));
     return 0;
 }
