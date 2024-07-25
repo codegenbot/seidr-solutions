86dd9082c46ec1655642a3e8590ccf5433c61c76
beep boop <githubbot@vadim.me>
2024-07-25 05:56:35 +0200

    Solution 33 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xqrhykk2 Prompt:    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 207ddaf4-1f67-46c9-aebe-b4ccc2b2cb69.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 207ddaf4-1f67-46c9-aebe-b4ccc2b2cb69.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~ In file included from 207ddaf4-1f67-46c9-aebe-b4ccc2b2cb69.cpp:23: 207ddaf4-1f67-46c9-aebe-b4ccc2b2cb69.cpp: In function ‘int main()’: 207ddaf4-1f67-46c9-aebe-b4ccc2b2cb69.cpp:33:20: error: ‘minPath’ was not declare d in this scope    33 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 207ddaf4-1f67-46c9-aebe-b4ccc2b2cb69.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+vector<int> minPath(vector<vector<int>> grid, int k);
+
+int main(){
+    assert(minPath({{1, 3}, {3, 2}}, 10) == vector<int>{1, 3, 1, 3, 1, 3, 1, 3, 1, 3});
+
+    return 0;
+}
+
 vector<int> minPath(vector<vector<int>> grid, int k) {
     vector<int> result;
     for (int i = 0; i < k; ++i) {
@@ -14,10 +22,4 @@ vector<int> minPath(vector<vector<int>> grid, int k) {
         }
     }
     return result;
-}
-
-int main() {
-    assert(minPath({{1, 3}, {3, 2}}, 10) == vector<int>{1, 3, 1, 3, 1, 3, 1, 3, 1, 3});
-
-    return 0;
 }
