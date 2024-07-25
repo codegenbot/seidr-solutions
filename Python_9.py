d2220192dc46e8f1f5c0599354431ec440a34b9c
beep boop <githubbot@vadim.me>
2024-07-25 07:06:03 +0200

    Solution 66 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4xzmx2u7 Prompt: [3, 100, 100, 100] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eebf82e9-5c3c-453b-8c59-ed5ca6ac4 294.py", line 12, in <module>     check(rolling_max)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/eebf82e9-5c3c-453b-8c59-ed5ca6ac4 294.py", line 9, in check     assert rolling_max([3, 2, 3, 100, 3]) == [3, 3, 3, 100, 100]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_9.py
+++ Python_9.py
@@ -1,10 +1,4 @@
 def rolling_max(numbers):
-    if len(numbers) < 3:
-        return numbers
+    return [max(*numbers[i:i+3]) for i in range(len(numbers) - 2)]
 
-    return [
-        max(numbers[i], numbers[i + 1], numbers[i + 2]) for i in range(len(numbers) - 2)
-    ] + [max(numbers[-2], numbers[-1])]
-
-
-print(rolling_max([3, 2, 3, 100, 3]))  # Output: [3, 100, 100, 100, 3]
+print(rolling_max([3, 2, 3, 100, 3]))
