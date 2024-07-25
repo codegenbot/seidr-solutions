e7d3023ad65bceb0f0448a21232bc699e6ba9a85
beep boop <githubbot@vadim.me>
2024-07-25 06:56:09 +0200

    Solution 42 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kgkbuznd Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cdbd05e4-e639-40da-a408-5706814ce cf1.py", line 17, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cdbd05e4-e639-40da-a408-5706814ce cf1.py", line 12, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -3,8 +3,8 @@ def sort_third(l: list):
     l += [(float("inf"), float("inf"), float("inf"))] * remainder
     return [
         sorted(
-            l[i : i + 3],
-            key=lambda x: x[2] if isinstance(x, tuple) and len(x) == 3 else float("inf")
+            [x for x in l[i : i + 3] if isinstance(x, tuple) and len(x) == 3],
+            key=lambda x: x[2]
         )
         for i in range(0, len(l), 3)
     ]
