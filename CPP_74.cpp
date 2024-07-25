f8c2029ba626ae7cf53ef5a2121c5874b30e7a72
beep boop <githubbot@vadim.me>
2024-07-25 20:44:23 +0200

    Solution 16 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/62lponr9 Prompt:       |                              ^~~~~~ 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp:17: 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp: In function ‘int main()’: 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 055ad7eb-a2c9-4c47-90f9-cbbfb7c92e1c.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,28 +1,10 @@
-#include <string> // Add necessary include header
+#include <string>
 
-bool issame(const vector<string>& a, const vector<string>& b){ // Add bool return type and use const references for vector parameters
+bool issame(const vector<string>& a, const vector<string>& b){
     // Function body
 }
 
-vector<string> total_match(const vector<string>& lst1, const vector<string>& lst2) {
-    int total_chars1 = 0;
-    for (const string& str : lst1) {
-        total_chars1 += str.size();
-    }
-
-    int total_chars2 = 0;
-    for (const string& str : lst2) {
-        total_chars2 += str.size();
-    }
-
-    if (total_chars1 < total_chars2) {
-        return lst1;
-    } else {
-        return lst2;
-    }
-}
-
 int main() {
-    assert(issame(total_match(vector<string>{"this"}, vector<string>{}), vector<string>{})); // Fix function invocation
+    assert(issame(total_match(vector<string>{"this"}, vector<string>{}), vector<string>{})); 
     return 0;
 }
