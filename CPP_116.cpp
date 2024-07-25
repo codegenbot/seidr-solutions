46759bbc0ed8f50055bc24400831c932deaa4971
beep boop <githubbot@vadim.me>
2024-07-25 05:04:49 +0200

    Solution 33 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bxl6o77x Prompt:       |                    ^~~ 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp:13: 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp: In function ‘int main()’: 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp:23:20: error: ‘sort_array’ was not decl ared in this scope    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 8fe0397b-4481-4b55-bf84-fe01d9ae5234.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,17 +1,11 @@
-```cpp
-#include <vector>
-#include <algorithm>
-#include <bitset>
-
 bool issame(int a, int b) {
-    // Function body here.
-    return false;
+    return a == b;
 }
 
-std::vector<int> sort_array(std::vector<int> arr) {
-    std::sort(arr.begin(), arr.end(), [](int a, int b) {
-        if (std::bitset<32>(a).count() != std::bitset<32>(b).count())
-            return std::bitset<32>(a).count() < std::bitset<32>(b).count();
+vector<int> sort_array(vector<int> arr){
+    sort(arr.begin(), arr.end(), [](int a, int b) {
+        if (bitset<32>(a).count() != bitset<32>(b).count())
+            return bitset<32>(a).count() < bitset<32>(b).count();
         else
             return issame(a, b);
     });
