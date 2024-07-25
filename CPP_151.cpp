5661509d37a7a6048ea9043d4ca3ccf4c00f01fa
beep boop <githubbot@vadim.me>
2024-07-25 13:07:17 +0200

    Solution 35 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/l5jlfc5n Prompt:       |     ^~~ c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return sum;       |     ^~~~~~ c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp:10: c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp: In function ‘int main()’: c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp:12:35: error: ‘lst’ was not declared in  this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp:12:13: error: ‘double_the_difference’ w as not declared in this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ c7a76ebf-a50a-4199-a2d6-511136fa8d0a.cpp:12:43: error: ‘odd_sum’ was not declare d in this scope    12 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -3,12 +3,10 @@
 
 double double_the_difference(std::list<double> lst) {
     double odd_sum = 0;
-    
     for (auto num : lst) {
         if (num > 0 && num == (int)num && (int)num % 2 != 0) {
             odd_sum += num * num;
         }
     }
-    
-    return odd_sum * 2;
+    return odd_sum;
 }
