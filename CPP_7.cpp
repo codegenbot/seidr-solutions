a1f2cb5e1b50dd1373aee1fdd4fca2c021402125
beep boop <githubbot@vadim.me>
2024-07-25 09:07:57 +0200

    Solution 19 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/clo4i8hv Prompt:     9 | bool issame(vector<string> a,vector<string>b){       |                                             ^ c398b1ca-bae2-47b9-aa4e-ed3bb0a5694e.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ c398b1ca-bae2-47b9-aa4e-ed3bb0a5694e.cpp:3:5: note: ‘int main()’ previously defi ned here     3 | int main() {       |     ^~~~ In file included from c398b1ca-bae2-47b9-aa4e-ed3bb0a5694e.cpp:8: c398b1ca-bae2-47b9-aa4e-ed3bb0a5694e.cpp: In function ‘int main()’: c398b1ca-bae2-47b9-aa4e-ed3bb0a5694e.cpp:18:20: error: ‘filter_by_substring’ was  not declared in this scope    18 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ c398b1ca-bae2-47b9-aa4e-ed3bb0a5694e.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,10 +1,15 @@
+#include <vector>
+#include <string>
+
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size())
+    if(a.size() != b.size()) 
         return false;
+        
     for(int i = 0; i < a.size(); i++) {
         if(a[i] != b[i])
             return false;
     }
+    
     return true;
 }
 
