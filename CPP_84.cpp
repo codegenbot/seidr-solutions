03db00205a5bf9bed1aeb5374b21faea2ef2f115
beep boop <githubbot@vadim.me>
2024-07-25 20:52:02 +0200

    Solution 30 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ryjebxws Prompt:       |     ^~~~~ efe6e87e-1f08-4483-a755-7486a7f199c4.cpp:6:5: error: ‘string’ does not name a ty pe     6 |     string binarySum = "";       |     ^~~~~~ efe6e87e-1f08-4483-a755-7486a7f199c4.cpp:7:5: error: expected unqualified-id bef ore ‘while’     7 |     while (sum > 0) {       |     ^~~~~ efe6e87e-1f08-4483-a755-7486a7f199c4.cpp:11:5: error: expected unqualified-id be fore ‘return’    11 |     return binarySum;       |     ^~~~~~ efe6e87e-1f08-4483-a755-7486a7f199c4.cpp:12:1: error: expected declaration befor e ‘}’ token    12 | }       | ^ In file included from efe6e87e-1f08-4483-a755-7486a7f199c4.cpp:14: efe6e87e-1f08-4483-a755-7486a7f199c4.cpp: In function ‘int main()’: efe6e87e-1f08-4483-a755-7486a7f199c4.cpp:16:13: error: ‘solve’ was not declared in this scope    16 |     assert (solve(963) == "10010");       |             ^~~~~

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <bitset>
+#include <cassert>
 
 std::string solve(int N) {
     int sum = 0;
@@ -8,7 +8,12 @@ std::string solve(int N) {
         sum += N % 2;
         N /= 2;
     }
-    return bitset<32>(sum).to_string().substr(32 - sum);
+    std::string binarySum = "";
+    while (sum > 0) {
+        binarySum = std::to_string(sum % 2) + binarySum;
+        sum /= 2;
+    }
+    return binarySum;
 }
 
 int main() {
