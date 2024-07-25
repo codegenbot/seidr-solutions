6e640d0968fd7afcf0435daee0891a33eae6d836
beep boop <githubbot@vadim.me>
2024-07-25 02:02:57 +0200

    Solution 24 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/3hl1q4dl Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/43849be3-6732-4420-bc8f-50249cb22 654.py", line 23, in <module>     check(parse_nested_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/43849be3-6732-4420-bc8f-50249cb22 654.py", line 20, in check     assert parse_nested_parens("(()(())((())))") == [4]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_6.py
+++ Python_6.py
@@ -1,11 +1,11 @@
-```
 def parse_nested_parens(paren_string: str) -> int:
-    max_depth = 0
-    current_depth = 0
+    result = 0
+    level = 0
     for char in paren_string:
         if char == '(':
-            current_depth += 1
+            level += 1
         elif char == ')':
-            current_depth -= 1
-        max_depth = max(max_depth, current_depth)
-    return max_depth
+            level -= 1
+        if level > result:
+            result = level
+    return result
