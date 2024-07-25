3072cb2363ec0d71d8b45af1aff6d0956a229ac5
beep boop <githubbot@vadim.me>
2024-07-25 06:36:01 +0200

    Solution 40 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc95937f-5d9d-4dd8-ac40-9dfe215fd b31.py", line 15, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cc95937f-5d9d-4dd8-ac40-9dfe215fd b31.py", line 12, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_67.py
+++ Python_67.py
@@ -1,10 +1,9 @@
+```
 def fruit_distribution(s, n):
     s = s.replace("apples", "1").replace("oranges", "0")
     parts = s.split()
-    ones = int(s.count('1'))
-    zeros = int(s.count('0'))
-    total_count = 0
+    total_count = set()
     for i in range(len(parts)):
         words = [x for x in parts[i].split() if x.isdigit()]
-        total_count += sum(int(word) for word in words)
-    return n - total_count - ones - zeros
+        total_count.update(words)
+    return n - len(total_count)
