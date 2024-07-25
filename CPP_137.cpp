263b36d9a1cd29770fa63d614a492e5ec3382387
beep boop <githubbot@vadim.me>
2024-07-25 05:12:07 +0200

    Solution 2 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,28 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace boost;
+
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return boost::any_cast<double>(b);
+    if (is_any_of<a>(int.class)) {
+        int x = any_cast<int>(a);
+        int y = any_cast<int>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (is_any_of<a>(double.class)) {
+        double x = any_cast<double>(a);
+        double y = any_cast<double>(b);
+        return (x > y) ? a : ((x < y) ? b : boost::any("None"));
+    } else if (is_any_of<a>(string.class)) {
+        string x = any_cast<string>(a);
+        string y = any_cast<string>(b);
+        double ax = stod(x);
+        double ay = stod(y);
+        return (ax > ay) ? a : ((ax < ay) ? b : boost::any("None"));
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return boost::any_cast<double>(a);
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        
-        int comp = strcmp(str1.c_str(), str2.c_str());
-        if (comp > 0)
-            return a;
-        else if (comp < 0)
-            return b;
-        else
-            return "None";
-    }
-    else {
-        // If both values are equal, compare their types.
-        if (a.type() == typeid(int) && b.type() == typeid(int))
-            return "None";
-        else
-            return boost::any_cast<double>(a) > boost::any_cast<double>(b)
-                ? a : b;
-    }
+    return a;
 }
