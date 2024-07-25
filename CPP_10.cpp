33ab4cf07bb9bb79c125644826a6c59d96336432
beep boop <githubbot@vadim.me>
2024-07-25 09:19:35 +0200

    Solution 41 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/weoizrnt Prompt:       | ^~~~~~~~~~~~~ e6bc806e-b916-44e5-a854-45c31c6371e7.cpp:26:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    26 | int main(){       |         ^~ e6bc806e-b916-44e5-a854-45c31c6371e7.cpp:26:9: note: remove parentheses to defau lt-initialize a variable    26 | int main(){       |         ^~       |         -- e6bc806e-b916-44e5-a854-45c31c6371e7.cpp:26:9: note: or replace parentheses with  braces to value-initialize a variable e6bc806e-b916-44e5-a854-45c31c6371e7.cpp:26:11: error: a function-definition is not allowed here before ‘{’ token    26 | int main(){       |           ^ e6bc806e-b916-44e5-a854-45c31c6371e7.cpp:28:2: error: expected ‘}’ at end of inp ut    28 | }       |  ^ e6bc806e-b916-44e5-a854-45c31c6371e7.cpp:17:12: note: to match this ‘{’    17 | int main() {       |            ^

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,8 +1,8 @@
-int main 
+int main
 {
-    string input;
+    string input, output;
     cout << "Enter a string: ";
     cin >> input;
-    string output = make_palindrome(input);
+    output = make_palindrome(input);
     cout << "Palindrome of the given string is: " << output << endl;
 }
