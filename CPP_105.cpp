c1002ba665dba7b6ca8481c519f796a5a2e895e0
beep boop <githubbot@vadim.me>
2024-07-25 04:44:44 +0200

    Solution 24 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/sds1xqg8 Prompt:       |                              ^~~~~~ 46daf4c9-6849-4326-bc96-eb91933abeab.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 46daf4c9-6849-4326-bc96-eb91933abeab.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 46daf4c9-6849-4326-bc96-eb91933abeab.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 46daf4c9-6849-4326-bc96-eb91933abeab.cpp:18: 46daf4c9-6849-4326-bc96-eb91933abeab.cpp: In function ‘int main()’: 46daf4c9-6849-4326-bc96-eb91933abeab.cpp:28:20: error: ‘by_length’ was not decla red in this scope    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 46daf4c9-6849-4326-bc96-eb91933abeab.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,15 +1,12 @@
-#include <vector>
 #include <string>
-#include <map>
+#include <vector>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size())
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return true;
+    return a == b;
+}
+
+int main() {
+    assert(issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
 }
 
 vector<string> by_length(vector<int> arr) {
@@ -27,10 +24,4 @@ vector<string> by_length(vector<int> arr) {
         result.push_back(num_map[i]);
     }
     return result;
-}
-
-int main() {
-    vector<int> arr = {9, 4, 8};
-    assert(issame(by_length(arr), {"Nine", "Four", "Eight"}));
-    return 0;
 }
