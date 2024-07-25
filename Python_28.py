2806ea0393cba693b30a1790b77e98661116f46c
beep boop <githubbot@vadim.me>
2024-07-25 11:52:04 +0200

    Solution 34 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pzpkp3la Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b08c3dea-7ce7-475c-b69c-ecc149224 483.py", line 1     return "".join(strings)     ^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_28.py
+++ Python_28.py
@@ -2,4 +2,4 @@ def concatenate_strings(strings):
     return "".join(strings)
 
 # Call the function with appropriate input strings
-print(concatenate_strings(["Hello", "World"]))
+concatenate_strings(["Hello", "World"])
