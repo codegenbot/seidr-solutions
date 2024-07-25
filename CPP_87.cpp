20082c9265aef90fa8147c7cb498149ed9cc1365
beep boop <githubbot@vadim.me>
2024-07-25 05:24:59 +0200

    Solution 74 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = const std::vector<int>*; _Forw ardIterator = std::vector<int>*; _Tp = std::vector<int>]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const std::vector<int>*; _Tp = std::v ector<int>; _Alloc = std::allocator<std::vector<int> >]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = s td::vector<int>; _Alloc = std::allocator<std::vector<int> >; std::vector<_Tp, _A lloc>::allocator_type = std::allocator<std::vector<int> >]’ c1ec522a-2749-4ef9-ac88-b2677ce65d03.cpp:35:26:   required from here /usr/include/c++/11/bits/stl_construct.h:119:7: error: no matching function for call to ‘operator new(sizetype, void*)’   119 |       ::new((void*)__p) _Tp(std::forward<_Args>(__args)...);       |       ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 
@@ -20,17 +19,17 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     for (int i = 0; i < lst.size(); i++) {
         if (x == 1) {
             for (int j = lst[i].size() - 1; j >= 0; j--) {
-                vector<int> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                result.push_back(temp);
+                result.push_back({}); 
+                for (int k = 0; k < 2; k++) {
+                    result.back().push_back((x == 1)? i : j);
+                }
             }
         } else {
             for (int j = 0; j < lst[i].size(); j++) {
-                vector<int> temp;
-                temp.push_back(i);
-                temp.push_back(j);
-                result.push_back(temp);
+                result.push_back({}); 
+                for (int k = 0; k < 2; k++) {
+                    result.back().push_back((x == 1)? i : j);
+                }
             }
         }
     }
@@ -38,6 +37,6 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
 }
 
 int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
+    assert(issame(get_row({},{}, {1,2,3}), {{2,2}}));
     return 0;
 }
