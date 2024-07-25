31a485ad93ba66dcdcaab92bc05ba050efdb23d8
beep boop <githubbot@vadim.me>
2024-07-25 04:08:08 +0200

    Solution 30 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c0181640-5b9a-42fb-b399-5cfb09e52 b81.py", line 13, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c0181640-5b9a-42fb-b399-5cfb09e52 b81.py", line 8, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c0181640-5b9a-42fb-b399-5cfb09e52 b81.py", line 4, in sort_third     temp = sorted([int(''.join(map(str, [l[i], l[i+1], l[i+2]])))])                                                        ~^^^^^ IndexError: list index out of range

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,7 @@
 def sort_third(l):
     result = []
     for i in range(0, len(l), 3):
-        if i + 2 < len(l): 
-            temp = sorted([int(''.join(map(str, [l[i], l[i+1], l[i+2]]))))])
+        if i+2 < len(l): 
+            temp = sorted([int(''.join(map(str, [l[i], l[i+1], l[i+2]])))]])
             result.append(temp[0])
     return tuple(result)
