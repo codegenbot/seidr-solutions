88a99d110e0bd98cd729b098fe0bbeba5b73d815
beep boop <githubbot@vadim.me>
2024-07-25 20:38:55 +0200

    Solution 8 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/62lponr9 Prompt: Complete the following code given the task description and function signature.

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -6,9 +6,9 @@ int total_chars(const vector<string>& lst) {
     return total;
 }
 
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+vector<string> total_match(vector<string> lst1, vector<string> lst2){
     if (total_chars(lst1) < total_chars(lst2)) {
         return lst1;
     }
-    return total_chars(lst1) == total_chars(lst2) ? lst1 : lst2;
+    return lst2;
 }
