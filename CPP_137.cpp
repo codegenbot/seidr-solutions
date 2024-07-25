9c4daf8f4b94662fabbc321adba22231623fdfad
beep boop <githubbot@vadim.me>
2024-07-25 04:59:05 +0200

    Solution 8 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y5jhcg27 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,6 @@
+Here is the completed code:
+
+```cpp
 #include <boost/any.hpp>
 #include <string>
 #include <algorithm>
@@ -7,15 +10,108 @@ using namespace std;
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        return max((float)a.convert_to<float>(), stof(b.convert_to<string>().c_str()));
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return (stof(a.convert_to<string>().c_str()) > stof(b.convert_to<string>().c_str())) ? a : ((stof(a.convert_to<string>().c_str()) < stof(b.convert_to<string>().c_str())) ? b : boost::any("None")));
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return (int)a.convert_to<int>() > stof(b.convert_to<string>().c_str()) ? a : ((int)a.convert_to<int>() < stof(b.convert_to<string>().c_str())) ? b : boost::any("None");
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
-    } else {
-        return boost::any("None");
     }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        string str1 = a.convert_to<string>();
+        string str2 = b.convert_to<string>();
+
+        int i = 0;
+        while (i < str1.length() && i < str2.length()) {
+            if (str1[i] != str2[i]) {
+                return max(str1, str2);
+            }
+            i++;
+        }
+
+        return "None";
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string str = a.convert_to<string>();
+        float f = (float)b.convert_to<float>();
+
+        int i = 0;
+        while (i < str.length()) {
+            if (str[i] == '.') {
+                return max(str, boost::any(f));
+            }
+            i++;
+        }
+
+        return "None";
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str = a.convert_to<string>();
+        int i = (int)b.convert_to<int>();
+
+        int j = 0;
+        while (j < str.length()) {
+            if (str[j] == '.') {
+                return max(str, boost::any(i));
+            }
+            j++;
+        }
+
+        return "None";
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        float f = (float)a.convert_to<float>();
+        string str = b.convert_to<string>();
+
+        int i = 0;
+        while (i < str.length()) {
+            if (str[i] == '.') {
+                return max(boost::any(f), str);
+            }
+            i++;
+        }
+
+        return "None";
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        int i = (int)a.convert_to<int>();
+        string str = b.convert_to<string>();
+
+        int j = 0;
+        while (j < str.length()) {
+            if (str[j] == '.') {
+                return max(boost::any(i), str);
+            }
+            j++;
+        }
+
+        return "None";
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        float f = (float)a.convert_to<float>();
+        int i = (int)b.convert_to<int>();
+
+        if (f > i) {
+            return a;
+        }
+        else if (i > f) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        int i = (int)a.convert_to<int>();
+        float f = (float)b.convert_to<float>();
+
+        if (i > f) {
+            return a;
+        }
+        else if (f > i) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+
+    return "None";
 }
