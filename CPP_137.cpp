a3665e1540d0820d61c5aefc645e5f4453007ddf
beep boop <githubbot@vadim.me>
2024-07-25 06:21:37 +0200

    Solution 79 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:47:29: error: ‘string’ was not declared  in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:47:37: error: ‘compare_one’ was not dec lared in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp: At global scope: dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:48:2: error: expected ‘}’ at end of inp ut    48 | }       |  ^ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:3:15: note: to match this ‘{’     3 | namespace std {       |               ^ dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp: In function ‘int std::main()’: dff6b2e2-92b3-42d2-b992-b1b238b3429e.cpp:48:1: warning: control reaches end of n on-void function [-Wreturn-type]    48 | }       | ^

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,45 +2,33 @@
 #include <boost/any.hpp>
 #include <string>
 
-namespace std {
-    using namespace boost;
+using namespace std;
+using namespace boost;
 
-    std::any a;
-    std::any b;
-
+std::any compare(const std::any& a, const std::any& b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
-        if (x > y) return a;
-        else if (x < y) return b;
-        else return a;
+        return a.type() == typeid(x) ? a : b;
     } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
         double x = boost::any_cast<double>(a);
         double y = boost::any_cast<double>(b);
-        if (x > y) return a;
-        else if (x < y) return b;
-        else return a;
-    } else if ((a.type() == typeid(std::string) || a.type() == typeid(wstring)) &&
-               (b.type() == typeid(std::string) || b.type() == typeid(wstring))) {
-        std::string x = boost::any_cast<std::string>(a);
-        std::string y = boost::any_cast<std::string>(b);
-        if (x > y) return a;
-        else if (x < y) return b;
-        else return a;
-    } else if ((a.type() == typeid(std::string) || a.type() == typeid(wstring)) &&
+        return x > y ? a : b;
+    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
+               (b.type() == typeid(string) || b.type() == typeid(wstring))) {
+        string x = boost::any_cast<string>(a);
+        string y = boost::any_cast<string>(b);
+        return x > y ? a : b;
+    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
                (b.type() == typeid(double))) {
-        std::string x = boost::any_cast<std::string>(a);
+        string x = boost::any_cast<string>(a);
         double y = boost::any_cast<double>(b);
-        if (std::stod(x) > y) return a;
-        else if (std::stod(x) < y) return b;
-        else return a;
+        return stod(x) > y ? a : b;
     } else if ((a.type() == typeid(double)) &&
-               (b.type() == typeid(std::string) || b.type() == typeid(wstring))) {
+               (b.type() == typeid(string) || b.type() == typeid(wstring))) {
         double x = boost::any_cast<double>(a);
-        std::string y = boost::any_cast<std::string>(b);
-        if (x > std::stod(y)) return a;
-        else if (x < std::stod(y)) return b;
-        else return a;
+        string y = boost::any_cast<string>(b);
+        return x > stod(y) ? a : b;
     } else {
         return a;
     }
