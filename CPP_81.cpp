53818b33e1118e72ed5483a5e808c1bfa017ee72
beep boop <githubbot@vadim.me>
2024-07-25 09:33:48 +0200

    Solution 63 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/wr6it0u6 Prompt:       |                              ^~~~~~ 07921062-a0c5-41db-81d2-90008344d007.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 07921062-a0c5-41db-81d2-90008344d007.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 07921062-a0c5-41db-81d2-90008344d007.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 07921062-a0c5-41db-81d2-90008344d007.cpp:21: 07921062-a0c5-41db-81d2-90008344d007.cpp: In function ‘int main()’: 07921062-a0c5-41db-81d2-90008344d007.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 07921062-a0c5-41db-81d2-90008344d007.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,9 +1,7 @@
 #include <vector>
 #include <string>
 
-bool issame(const vector<string> &a, const vector<string> &b) {
-    return a == b;
-}
+bool issame(const vector<string> &a, const vector<string> &b);
 
 vector<string> numerical_letter_grade(vector<float> grades){
     vector<string> letter_grades;
@@ -23,13 +21,4 @@ vector<string> numerical_letter_grade(vector<float> grades){
         else letter_grades.push_back("E");
     }
     return letter_grades;
-}
-
-int main() {
-    vector<float> grades = {3.8, 2.5, 4.0, 1.7, 3.2};
-    vector<string> result = numerical_letter_grade(grades);
-    for (const string& grade : result) {
-        cout << grade << " ";
-    }
-    return 0;
 }
