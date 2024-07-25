949e60fe2823a498337260ebf63a3415c69d4096
beep boop <githubbot@vadim.me>
2024-07-25 02:00:47 +0200

    Solution 41 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/9vlt1woe Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,26 +1,47 @@
 int bowlingScore(string s) {
     int score = 0;
-    int currentRolls = 0;
-    int totalRolls = 0;
+    int currentFrame = 1;
+    int rollsLeftInFrame = 2;
 
-    for (char c : s) {
-        if (c == 'X') {
-            score += 30;
-            currentRolls = 3;
-        } else if (c == '/') {
-            score += 10 + (10 - (currentRolls % 2));
-            currentRolls = 0;
-        } else if (isdigit(c)) {
-            int roll = c - '0';
-            score += roll;
-            currentRolls++;
-            totalRolls++;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '/') {
+            // Store the number of pins knocked down in this frame
+            int pinsKnockedDown = stoi(s.substr(i - 2, 1));
+            if (currentFrame == 10) {
+                rollsLeftInFrame = 1;
+            }
+            currentFrame++;
 
-            if (totalRolls == 10) {
+            if (rollsLeftInFrame == 1) {
+                // If we're on the last roll of a frame and it's not a strike
+                if (pinsKnockedDown < 10) {
+                    score += pinsKnockedDown + bowlingScore(s.substr(i + 1));
+                    break;
+                }
+            }
+
+            rollsLeftInFrame = 2;
+        } else if (s[i] == 'X') {
+            // If we're on a strike
+            if (currentFrame < 10) {
+                score += 10 + bowlingScore(s.substr(i + 4));
+            } else {
+                score += 10;
+            }
+            currentFrame++;
+            rollsLeftInFrame = 2;
+        } else {
+            int pinsKnockedDown = stoi(s.substr(i, 1));
+            if (currentFrame == 10) {
+                rollsLeftInFrame = 1;
+            }
+
+            if (rollsLeftInFrame == 1) {
+                // If we're on the last roll of a frame and it's not a strike
+                score += pinsKnockedDown + bowlingScore(s.substr(i + 2));
                 break;
             }
         }
     }
-
     return score;
 }
