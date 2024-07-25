b7ae37792ad6c7ef3ce0c1e976b9ed2d7725b982
beep boop <githubbot@vadim.me>
2024-07-25 04:19:20 +0200

    Solution 59 of Python/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/q2oxtv4a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/46c9855e-57da-4dfc-b1e8-cffb2487b 6a1.py", line 1, in <module>     def get_names() -> List[str]:                        ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_22.py
+++ Python_22.py
@@ -1,18 +1,10 @@
-```Python
+```
 from typing import List
 
-def filter_integers() -> List[int]:
-    values = []
-    while True:
-        user_input = input("Enter an integer or type 'done' to finish: ")
-        if user_input.lower() == "done":
-            break
-        try:
-            value = int(user_input)
-            values.append(value)  # added condition here 
-        except ValueError:
-            print("Invalid input. Please enter a number or 'done'. Try again!")
-    return values
+def get_names() -> List[str]:
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    return [first_name, last_name]
 
 
-print(filter_integers())
+print(get_names())
