7f76cc3eaf148342b580a8f0019041d44a035340
beep boop <githubbot@vadim.me>
2024-07-25 07:27:09 +0200

    Solution 89 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2a5f1d51-8500-47c2-80a9-66f49a93d 39f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_67.py
+++ Python_67.py
@@ -1,5 +1,14 @@
 ```
-def check(func):
-    return func("1 apples and 100 oranges")
+import re
 
-print(check(fruit_distribution))
+def fruit_distribution(s):
+    s = s.lower()
+    fruit_counts = {"apple": 0, "banana": 0}
+
+    for fruit in fruit_counts:
+        if re.search(fruit, s):
+            fruit_counts[fruit] += len(re.findall(fruit, s))
+
+    return "mixed" if fruit_counts["apple"] > 0 and fruit_counts["banana"] > 0 else ("apples" if "apple" in fruit_counts and fruit_counts["apple"] > 0 else ("bananas" if "banana" in fruit_counts and fruit_counts["banana"] > 0 else "unknown"))
+
+print(fruit_distribution("1 apples and 100 oranges"))
