bfbec91708d49da280f4ceb186fe6027b3c3f26b
beep boop <githubbot@vadim.me>
2024-07-25 06:00:24 +0200

    Solution 57 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt: 0fe90774-db3d-4d61-8586-e076e35e0d34.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,62 +1,17 @@
-#include <iostream>
 #include <string>
 #include <cmath>
 
-using namespace std;
-
-namespace boost {
-}
-
-namespace boost {
-    class any_cast { };
-}
-
-any compare_any(any a, any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = any_cast<any_cast>(a);
-        int y = any_cast<any_cast>(b);
-        if (x > y) return a;
-        else if (x < y) return b;
-        else return a; 
+any_to_string(any a) {
+    if (a.type() == typeid(int)) {
+        int x = boost::any_cast<int>(a);
+        return to_string(x);
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double x = any_cast<any_cast>(a);
-        double y = any_cast<any_cast>(b);
-        if (x > y) return a;
-        else if (x < y) return b;
-        else return a; 
+    else if (a.type() == typeid(double)) {
+        double x = boost::any_cast<double>(a);
+        return to_string(x);
     }
-    else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        string x = any_cast<any_cast>(a);
-        string y = any_cast<any_cast>(b);
-        if (x > y) return a;
-        else if (x < y) return b;
-        else return a; 
+    else if (a.type() == typeid(string) || a.type() == typeid(wstring)) {
+        string x = boost::any_cast<string>(a);
+        return x;
     }
-    else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-           (b.type() == typeid(double))) {
-        string x = any_cast<any_cast>(a);
-        double y = any_cast<any_cast>(b);
-        if (stod(x) > y) return a;
-        else if (stod(x) < y) return b;
-        else return a; 
-    }
-    else if ((a.type() == typeid(double)) &&
-           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        double x = any_cast<any_cast>(a);
-        string y = any_cast<any_cast>(b);
-        if (x > stod(y)) return a;
-        else if (x < stod(y)) return b;
-        else return a; 
-    }
-    else {
-        return a; 
-    }
-}
-
-int main() {
-    cout << "Result: ";
-    cout << compare_any(a, b) << endl;
-    return 0;
 }
