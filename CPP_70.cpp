34b55379671f2ea27911cdf184718144589b6ca8
beep boop <githubbot@vadim.me>
2024-07-25 03:20:41 +0200

    Solution 9 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fg7ucw1n Prompt:                  from /usr/include/c++/11/algorithm:74,                  from ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp:35:39: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp:34: ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp: In function ‘int main()’: ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp:44:20: error: ‘strange_sort_list’ was n ot declared in this scope    44 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ ffe6507e-4e0d-4593-95a9-bd1397c1ccee.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <vector>
-#include <iterator>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
@@ -8,24 +7,31 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 std::vector<int> strange_sort_vector(std::vector<int> lst) {
     std::vector<int> result;
-
     if (lst.empty()) {
         return result;
     }
-
-    sort(lst.begin(), lst.end());
-    result.push_back(*min_element(lst.begin(), lst.end()));
-    lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
-
+    
+    std::sort(lst.begin(), lst.end());
+    result.push_back(*std::min_element(lst.begin(), lst.end()));
+    lst.erase(std::remove(lst.begin(), lst.end(), *std::min_element(lst.begin(), lst.end())), lst.end());
+    
+    if (!lst.empty()) {
+        std::sort(lst.begin(), lst.end());
+        result.push_back(*std::max_element(lst.begin(), lst.end()));
+        lst.erase(std::remove(lst.begin(), lst.end(), *std::max_element(lst.begin(), lst.end())), lst.end());
+    }
+    
     while (!lst.empty()) {
-        sort(lst.begin(), lst.end());
-        result.push_back(*min_element(lst.begin(), lst.end()));
-        lst.erase(remove(lst.begin(), lst.end(), *min_element(lst.begin(), lst.end())), lst.end());
-
-        if (lst.size() == 1) {
-            break;
+        std::sort(lst.begin(), lst.end());
+        result.push_back(*std::min_element(lst.begin(), lst.end()));
+        lst.erase(std::remove(lst.begin(), lst.end(), *std::min_element(lst.begin(), lst.end())), lst.end());
+        
+        if (!lst.empty()) {
+            std::sort(lst.begin(), lst.end());
+            result.push_back(*std::max_element(lst.begin(), lst.end()));
+            lst.erase(std::remove(lst.begin(), lst.end(), *std::max_element(lst.begin(), lst.end())), lst.end());
         }
     }
-
+    
     return result;
 }
