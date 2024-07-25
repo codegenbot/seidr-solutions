fd1f93a608a8c8ba0a91b4649b2d17649fbdcbec
beep boop <githubbot@vadim.me>
2024-07-25 09:35:43 +0200

    Solution 19 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xfgmwr5g Prompt: Program did not receive expected input

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+
+    set<int> common;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(common, next(common.end())));
+
+    vector<int> result(common.begin(), common.end());
+    return result;
+}
+
 int main() {
     vector<int> l1, l2;
     int n1, n2;
@@ -17,16 +29,4 @@ int main() {
     }
     vector<int> result = common(l1, l2);
     return 0;
-}
-
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-
-    set<int> common;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        inserter(common, next(common.end())));
-
-    vector<int> result(common.begin(), common.end());
-    return result;
 }
