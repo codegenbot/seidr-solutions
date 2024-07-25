0c08588763d07357de813bac93703e9525ef2aec
beep boop <githubbot@vadim.me>
2024-07-25 09:21:35 +0200

    Solution 14 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/txuepyg8 Prompt: afde7d2c-f170-4911-a2c0-f9a9a89afafe.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ afde7d2c-f170-4911-a2c0-f9a9a89afafe.cpp:3:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from afde7d2c-f170-4911-a2c0-f9a9a89afafe.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from afde7d2c-f170-4911-a2c0-f9a9a89afafe.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,27 +1,47 @@
-```cpp
-int bowlingScore(const char* s) {
+int bowlingScore(std::string s) {
     int score = 0;
-    int i = 0;
-    while (i < strlen(s)) {
-        if (s[i] == 'X') {
-            score += 10;
-            i++;
-            if (i < strlen(s) && s[i] != '/') {
-                score += 10;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == '/') {
+            string first = s.substr(0, i);
+            string second = s.substr(i + 1);
+            int firstPart = 0;
+            if (first[0] != 'X') {
+                for (char c : first) {
+                    firstPart += c - '0';
+                }
+            } else {
+                firstPart = 10;
+            }
+            int secondPart = 0;
+            if (second[0] != 'X') {
+                for (char c : second) {
+                    secondPart += c - '0';
+                }
+            } else {
+                secondPart = 10;
+            }
+            score += getScore(firstPart, secondPart);
+        } else {
+            int frame = 0;
+            if (s[i] == 'X') {
+                frame = 10;
+            } else if (s[i] - '0' + s[i+1] - '0' > 10) {
+                frame = 10;
+                i++;
+            } else {
+                frame = s[i] - '0' + s[i+1] - '0';
                 i++;
             }
-        } else if (s[i] - '0' + s[i+1] - '0' > 10) {
-            int frame = 10;
-            if (i+2 < strlen(s)) {
-                frame = 10 - (s[i] - '0' + s[i+1] - '0');
-            }
             score += frame;
-            i+=2;
-        } else {
-            int frame = s[i] - '0' + s[i+1] - '0';
-            score += frame;
-            i+=2;
         }
     }
     return score;
+}
+
+int getScore(int first, int second) {
+    if (first + second > 10) {
+        return 10 + second;
+    } else {
+        return first + second;
+    }
 }
