621ab908c15c911681b571e5b65f1823d96ff6fd
beep boop <githubbot@vadim.me>
2024-07-25 05:13:35 +0200

    Solution 78 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4fg0mdlb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/aa1b2875-2a3c-419e-b3f8-3bdd21e9d c34.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -3,7 +3,7 @@ def process_numbers():
     numbers_str = input("Enter a string of numbers (e.g., 1-2.3 4 5-6): ")
     numbers = []
     
-    for num in numbers_str.split():
+    for num in numbers_str.split('" "'):
         if "-" in num:
             start_num, end_num = map(float, num.split("-"))
             numbers.extend([round(i, 1) for i in [float(j) for j in (str(int(start_num)), str(int(end_num))) if int(str(j).split('.')[0]) <= int(str(int(start_num)).split('.')[0]) and int(str(j).split('.')[0]) >= int(str(int(start_num)).split('.')[0])] + [float(j) for j in (str(int(start_num)), str(int(end_num))) if int(str(j).split('.')[0]) > int(str(int(start_num)).split('.')[0])]
