91f1db1503ec4f49fbcf7d9a13bec2c6e0cfaba9
beep boop <githubbot@vadim.me>
2024-07-25 03:59:57 +0200

    Solution 50 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pzj2ppnn Prompt:     3 | 97dc505b-67cf-43af-ae9e-244419fb23b0.cpp:36:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    36 | int main(){       |         ^~ 97dc505b-67cf-43af-ae9e-244419fb23b0.cpp:36:9: note: remove parentheses to defau lt-initialize a variable    36 | int main(){       |         ^~       |         -- 97dc505b-67cf-43af-ae9e-244419fb23b0.cpp:36:9: note: or replace parentheses with  braces to value-initialize a variable 97dc505b-67cf-43af-ae9e-244419fb23b0.cpp:36:11: error: a function-definition is not allowed here before ‘{’ token    36 | int main(){       |           ^ 97dc505b-67cf-43af-ae9e-244419fb23b0.cpp:38:2: error: expected ‘}’ at end of inp ut    38 | }       |  ^ 97dc505b-67cf-43af-ae9e-244419fb23b0.cpp:21:12: note: to match this ‘{’    21 | int main() {       |            ^

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -18,7 +18,7 @@ std::vector<float> derivative(std::vector<float> a) {
     return result;
 }
 
-int main {
+int main() {
     assert(issame(std::vector<float>({1}), std::vector<float>({1})));
     assert(issame(derivative(std::vector<float>({1})), std::vector<float>{});  
     return 0;
