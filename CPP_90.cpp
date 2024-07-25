1f7535c202a8c86b16cdce898adc876b900033a0
beep boop <githubbot@vadim.me>
2024-07-25 04:01:29 +0200

    Solution 84 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/80392nj9 Prompt: Complete the following code given the task description and function signature.

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -1,11 +1,7 @@
 int next_smallest(vector<int> lst) {
-    if (lst.empty()) return -1; // Return None as per problem statement
-    vector<int> sorted = lst;
-    sort(sorted.begin(), sorted.end());
-    for (int i = 0; i < sorted.size() - 1; ++i)
-        if (sorted[i] != sorted[i + 1]) {
-            if (i == sorted.size() - 2) return -1; // Return None as per problem statement
-            else return sorted[i + 1];
-        }
-    return -1; // Return None as per problem statement
+    vector<int> copy = lst; 
+    if(copy.size() < 2) return -1;  
+    std::sort(copy.begin(),copy.end());
+    if(copy[0] == copy[1]) return -1;
+    return copy[1];
 }
