b391cb87e584fd50c3c4045aff8be1cf2e924206
beep boop <githubbot@vadim.me>
2024-07-25 08:29:22 +0200

    Solution 71 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d7zz7t47 Prompt: in this scope     5 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 273d7e25-5822-4dc5-9afc-7c0fc6b26689.cpp:5:34: error: expected primary-expressio n before ‘int’     5 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 273d7e25-5822-4dc5-9afc-7c0fc6b26689.cpp:5:39: error: expression list treated as  compound expression in initializer [-fpermissive]     5 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 273d7e25-5822-4dc5-9afc-7c0fc6b26689.cpp:4: 273d7e25-5822-4dc5-9afc-7c0fc6b26689.cpp: In function ‘int main()’: 273d7e25-5822-4dc5-9afc-7c0fc6b26689.cpp:14:20: error: ‘remove_duplicates’ was n ot declared in this scope    14 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 273d7e25-5822-4dc5-9afc-7c0fc6b26689.cpp:14:19: error: ‘issame’ cannot be used a s a function    14 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,13 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 bool issame(int a, int b) {
     return a == b;
 }
 
-void remove_duplicates(vector<int>& numbers) {
-    numbers.erase(unique(numbers.begin(), numbers.end(), issame), numbers.end());
+void remove_duplicates(std::vector<int>& numbers) {
+    numbers.erase(std::unique(numbers.begin(), numbers.end(), issame), numbers.end());
 }
 
 int main() {
-    vector<int> numbers = {1, 2, 2, 3, 4, 4, 5};
+    std::vector<int> numbers = {1, 2, 2, 3, 4, 4, 5};
     remove_duplicates(numbers);
-    return numbers;
+
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
 }
