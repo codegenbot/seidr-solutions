dc565a8fa6b379964fa617144b391e529d00d15e
beep boop <githubbot@vadim.me>
2024-07-25 06:21:48 +0200

    Solution 31 of Python/66, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/j8yxpsc6 Prompt: 354 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/324547d9-aa1d-4412-a33b-6f41ebb8c 06d.py", line 20, in <module>     check(digitSum)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/324547d9-aa1d-4412-a33b-6f41ebb8c 06d.py", line 17, in check     assert digitSum("You arE Very Smart") == 327, "Error"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_66.py
+++ Python_66.py
@@ -1,8 +1,13 @@
 def digitSum(s):
-    total = 0
-    for char in s.lower():
-        if char.isdigit():
-            total += int(char)
-    return total
+    def get_digit_sum(sub_s):
+        total = 0
+        for char in sub_s:
+            if char.isdigit():
+                total += int(char)
+            elif char.isalpha():
+                total += (ord(char.lower()) - 96) if char.islower() else (ord(char) - 64)
+        return total
+
+    return get_digit_sum(s)
 
 print(digitSum("You arE Very Smart"))
