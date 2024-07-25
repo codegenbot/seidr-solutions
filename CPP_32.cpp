f6b618764b813279841d9f739cb5d50777d4bd9b
beep boop <githubbot@vadim.me>
2024-07-25 14:30:32 +0200

    Solution 39 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4wb2r1ji Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,7 +1,5 @@
 double find_zero(vector<double> xs){
     double a = xs[0];
     double b = xs[1];
-    double discriminant = b*b - 4*a;
-    double x = -b/(2*a);
-    return x;
+    return -b/a;
 }
