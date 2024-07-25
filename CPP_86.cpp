4c1ca4f02f9fbb19cf93208703d9f8e080548b93
beep boop <githubbot@vadim.me>
2024-07-25 04:44:18 +0200

    Solution 17 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a4yl2ltl Prompt: 1bbeeeca-07db-498c-8e7d-09bca1223c62.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 1bbeeeca-07db-498c-8e7d-09bca1223c62.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from 1bbeeeca-07db-498c-8e7d-09bca1223c62.cpp:31: 1bbeeeca-07db-498c-8e7d-09bca1223c62.cpp: In function ‘int main()’: 1bbeeeca-07db-498c-8e7d-09bca1223c62.cpp:33:13: error: ‘anti_shuffle’ was not de clared in this scope; did you mean ‘Contest::anti_shuffle’?    33 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~ 1bbeeeca-07db-498c-8e7d-09bca1223c62.cpp:8:17: note: ‘Contest::anti_shuffle’ dec lared here     8 |     std::string anti_shuffle(std::string s) {       |                 ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <string>
-#include <cassert>
 
 namespace Contest {
     std::string anti_shuffle(std::string s);
@@ -22,5 +21,3 @@ namespace Contest {
         return result;
     }
 }
-
-assert(Contest::anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
