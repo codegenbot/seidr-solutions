fb96973484d793f1cb06942eeb726c95f8fbd473
beep boop <githubbot@vadim.me>
2024-07-25 03:06:40 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1jrj2z50 Prompt: In file included from /usr/include/c++/11/cassert:44,                  from c747d2e8-7c27-4213-b57f-d3f31ecba6b4.cpp:5: c747d2e8-7c27-4213-b57f-d3f31ecba6b4.cpp: In function ‘int main()’: c747d2e8-7c27-4213-b57f-d3f31ecba6b4.cpp:19:12: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    19 |     assert(issame(common({4, 3, 2, 8}, {}) , {}));       |            ^~~~~~ c747d2e8-7c27-4213-b57f-d3f31ecba6b4.cpp: At global scope: c747d2e8-7c27-4213-b57f-d3f31ecba6b4.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ c747d2e8-7c27-4213-b57f-d3f31ecba6b4.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <iterator>
-#include <cassert>
 
 using namespace std;
 
@@ -15,11 +14,4 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-    return 0;
-}
+assert((common({4, 3, 2, 8}, {}) == vector<int>{}));
