2ec4fa46a2f13fdeaf83ca47b6bdfeb10297df23
beep boop <githubbot@vadim.me>
2024-07-25 08:06:43 +0200

    Solution 90 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0sy5cl9f Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7bbb3d79-d30d-41e6-bc60-2c31e7517f77.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7bbb3d79-d30d-41e6-bc60-2c31e7517f77.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7bbb3d79-d30d-41e6-bc60-2c31e7517f77.cpp:19: 7bbb3d79-d30d-41e6-bc60-2c31e7517f77.cpp: In function ‘int main()’: 7bbb3d79-d30d-41e6-bc60-2c31e7517f77.cpp:29:20: error: ‘sort_third’ was not decl ared in this scope    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 7bbb3d79-d30d-41e6-bc60-2c31e7517f77.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,15 +1,26 @@
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    return a[2] == b[2];
 }
 
-vector<int> sort_third(vector<int> l) {
-    vector<int> l_copy = l;
+bool sort_third(const vector<int>& a, const vector<int>& b) {
+    return a[2] < b[2];
+}
+
+vector<vector<int>> sort_by_third(const vector<vector<int>>& l) {
+    vector<int> sorted_indices;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
-            sort(l_copy.begin() + i, l_copy.begin() + i + 3);
+            sorted_indices.push_back(i);
         }
     }
-    return l_copy;
-}
+    sort(sorted_indices.begin(), sorted_indices.end(), [&l](int a, int b) {
+        return l[a][2] < l[b][2];
+    });
 
-assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    vector<vector<int>> result = l;
+    for (int i = 0; i < sorted_indices.size(); ++i) {
+        result[sorted_indices[i]] = l[sorted_indices[i]];
+    }
+
+    return result;
+}
