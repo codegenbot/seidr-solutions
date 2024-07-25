0685a5d81440f49a7f098789148b4341d270f89e
beep boop <githubbot@vadim.me>
2024-07-25 04:04:03 +0200

    Solution 36 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run7/runs/rb611o0t Prompt: Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -3,28 +3,64 @@ using namespace std;
 
 vector<int> indicesOfSubstring(string text, string target) {
     vector<int> result;
-    for (int i = 0; i <= text.size() - target.size(); i++) {
-        bool found = true;
-        for (int j = 0; j < target.size(); j++) {
-            if (text[i + j] != target[j]) {
-                found = false;
-                break;
-            }
-        }
-        if (found) {
+    int n = text.length();
+    int m = target.length();
+
+    for(int i = 0; i <= n - m; i++) {
+        if(text.substr(i, m) == target) {
             result.push_back(i);
         }
     }
+
     return result;
 }
 
 int main() {
-    string text, target;
-    cin >> text >> target;
-    vector<int> res = indicesOfSubstring(text, target);
-    for (int i : res) {
+    // test cases
+    string text1 = "a";
+    string target1 = "5";
+    vector<int> result1 = indicesOfSubstring(text1, target1);
+    cout << "Indices of substring in '" << text1 << "' for target '" << target1 << "': ";
+    for(int i : result1) {
         cout << i << " ";
     }
     cout << endl;
+
+    string text2 = "!";
+    string target2 = "!!";
+    vector<int> result2 = indicesOfSubstring(text2, target2);
+    cout << "Indices of substring in '" << text2 << "' for target '" << target2 << "': ";
+    for(int i : result2) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    string text3 = "r";
+    string target3 = "nm,xcnwqnd@#$fwkdjn3";
+    vector<int> result3 = indicesOfSubstring(text3, target3);
+    cout << "Indices of substring in '" << text3 << "' for target '" << target3 << "': ";
+    for(int i : result3) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    string text4 = "hi";
+    string target4 = "hihihihihihihihihihi";
+    vector<int> result4 = indicesOfSubstring(text4, target4);
+    cout << "Indices of substring in '" << text4 << "' for target '" << target4 << "': ";
+    for(int i : result4) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    string text5 = "############";
+    string target5 = "#";
+    vector<int> result5 = indicesOfSubstring(text5, target5);
+    cout << "Indices of substring in '" << text5 << "' for target '" << target5 << "': ";
+    for(int i : result5) {
+        cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
