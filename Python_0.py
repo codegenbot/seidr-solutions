3a05f8f666e87d64411ab433545354538ceb8aee
beep boop <githubbot@vadim.me>
2024-07-25 06:35:30 +0200

    Solution 42 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/niporhik Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fa52f6e5-c33e-478b-9f9f-9daed5523 db1.py", line 1, in <module>     result = check_numbers_threshold([1, 5, 10, 15, 20], 5)              ^^^^^^^^^^^^^^^^^^^^^^^ NameError: name 'check_numbers_threshold' is not defined

--- Python_0.py
+++ Python_0.py
@@ -1,9 +1,5 @@
 def check_numbers_threshold(numbers, threshold):
-    count = 0
-    for num in numbers:
-        if num > threshold:
-            count += 1
-    return count
+    return len([num for num in numbers if num > threshold])
 
 result = check_numbers_threshold([1, 5, 10, 15, 20], 5)
 print(result)
