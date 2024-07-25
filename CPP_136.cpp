2bc61777d6559a2ce9d4d3dffe9e367d1136a442
beep boop <githubbot@vadim.me>
2024-07-25 12:54:40 +0200

    Solution 68 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/61348jqr Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8faced45-8769-4a88-8142-89f5f92ba1f1.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8faced45-8769-4a88-8142-89f5f92ba1f1.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8faced45-8769-4a88-8142-89f5f92ba1f1.cpp:17: 8faced45-8769-4a88-8142-89f5f92ba1f1.cpp: In function ‘int main()’: 8faced45-8769-4a88-8142-89f5f92ba1f1.cpp:27:20: error: ‘largest_smallest_integer s’ was not declared in this scope    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 8faced45-8769-4a88-8142-89f5f92ba1f1.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,12 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <cassert>
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-std::vector<int> largest_smallest_integers(std::vector<int> lst){
+vector<int> largest_smallest_integers(vector<int> lst){
     int largest_negative = INT_MIN;
     int smallest_positive = INT_MAX;
     
@@ -20,11 +12,4 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst){
     }
     
     return {largest_negative, smallest_positive};
-}
-
-int main(){
-    std::vector<int> result = largest_smallest_integers({-6, -4, -4, -3, -100, 1});
-    assert(issame(result, {-3, 1}));
-    
-    return 0;
 }
