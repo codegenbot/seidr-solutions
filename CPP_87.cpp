f876c0041b578f7e556940a6f5962db03f770ca9
beep boop <githubbot@vadim.me>
2024-07-25 04:47:58 +0200

    Solution 38 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xdkb89ot Prompt:       |                                   ^~~~~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:18:42: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:18:49: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:18:56: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:17: 76338ae4-6f26-473c-9024-d28dc80a1961.cpp: In function ‘int main()’: 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:30:20: error: ‘get_row’ was not declare d in this scope    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 76338ae4-6f26-473c-9024-d28dc80a1961.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -31,6 +31,6 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
 }
 
 int main() {
-    assert(issame(get_row({},{1},{1,2,3}), 3) == {{2,2}});
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{0, 2}}));
     return 0;
 }
