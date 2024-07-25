590b8cfd49d024fe3b4775bcc8b113f5d2f97704
beep boop <githubbot@vadim.me>
2024-07-25 04:30:51 +0200

    Solution 58 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/3qqyav9a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/064fcb39-770e-4e44-adfd-8b10f47a2 29d.py", line 8, in <module>     check(remove_duplicates)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/064fcb39-770e-4e44-adfd-8b10f47a2 29d.py", line 5, in check     assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_26.py
+++ Python_26.py
@@ -1,4 +1,4 @@
 ```
 def remove_duplicates(numbers: list[int]) -> list[int]:
     seen = set()
-    return [x for x in numbers if x not in seen and not seen.add(x)]
+    return [x for x in numbers if x not in seen and not (seen.add(x),)]
