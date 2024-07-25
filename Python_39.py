a0236078a6f9db20e6af1fd1407ea4733fb5f21a
beep boop <githubbot@vadim.me>
2024-07-25 12:15:39 +0200

    Solution 83 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7xprh2dv Prompt: 0: Not a Prime Fibonacci 1: Not a Prime Fibonacci 2: Not a Prime Fibonacci 3: Prime Fibonacci 4: Prime Fibonacci 5: Prime Fibonacci 6: Not a Prime Fibonacci 7: Prime Fibonacci 8: Not a Prime Fibonacci 9: Not a Prime Fibonacci 10: Not a Prime Fibonacci Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5b935cf-d801-4980-b0d5-5d71d2a65 e8c.py", line 41, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a5b935cf-d801-4980-b0d5-5d71d2a65 e8c.py", line 38, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_39.py
+++ Python_39.py
@@ -2,17 +2,9 @@ import math
 
 mem = {0: 0, 1: 1}
 
-def fibonacci(num):
-    if num in mem:
-        return mem[num]
-    mem[num] = fibonacci(num - 1) + fibonacci(num - 2)
-    return mem[num]
-
 def is_prime(num):
     if num <= 1:
         return False
-    if num == 2:
-        return True
     for i in range(2, int(math.sqrt(num)) + 1):
         if num % i == 0:
             return False
