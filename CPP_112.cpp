5785b31b74813388bee3a9022d62da4a0d577f8a
beep boop <githubbot@vadim.me>
2024-07-25 10:18:19 +0200

    Solution 34 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vg8r057o Prompt:       |                              ^~~~~~ 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp:13: 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp: In function ‘int main()’: 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 5cb72899-68cd-49be-8cdd-cadab0515bff.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,34 +1,14 @@
 #include <string>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
 bool issame(std::vector<string> a, std::vector<string> b){
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
-std::vector<string> reverse_delete(string s, string c){
-    string result = "";
-    for(char ch : s){
-        if(c.find(ch) == string::npos){
-            result += ch;
-        }
-    }
-    string reversed = result;
-    reverse(reversed.begin(), reversed.end());
-    return {result, result == reversed ? "True" : "False"};
-}
-
-int main(){
-    string s = "hello";
-    string c = "l";
-    auto res = reverse_delete(s, c);
-    for(auto str : res){
-        std::cout << str << " ";
-    }
+int main() {
+    string s, c;
+    cin >> s >> c;
+    auto result = reverse_delete(s, c);
+    cout << result[0] << endl;
+    cout << result[1] << endl;
     return 0;
 }
