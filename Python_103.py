a60e06f8cf0fa571ddf2df157cb89e0f91aed018
beep boop <githubbot@vadim.me>
2024-07-25 07:20:36 +0200

    Solution 23 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/engi6rh6 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65362a03-b6f5-4977-b44f-a4722ba9b 1e8.py", line 13, in <module>     check(rounded_avg)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65362a03-b6f5-4977-b44f-a4722ba9b 1e8.py", line 10, in check     assert rounded_avg(5, 5) == "0b101"            ^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/65362a03-b6f5-4977-b44f-a4722ba9b 1e8.py", line 6, in rounded_avg     return str(bin(int(binary, 8))[2:])                    ^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 8: '0b101'

--- Python_103.py
+++ Python_103.py
@@ -1,5 +1,6 @@
 def rounded_avg(n, m):
     if n > m:
         return -1
-    avg = (n + m) / 2.0  
-    return bin(int(avg))[2:]
+    avg = (n + m) // 2  
+    binary = '0b' + bin(avg)[2:]
+    return str(bin(int(binary, 2))[2:])
