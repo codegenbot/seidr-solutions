34affbd501e6571ebd53e3c5f7dfdb645a419272
beep boop <githubbot@vadim.me>
2024-07-25 06:02:27 +0200

    Solution 56 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/33hnxnvj Prompt:       |             ^~~~~~ 2c02fc1c-24c6-4795-a258-393802fa3892.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2c02fc1c-24c6-4795-a258-393802fa3892.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 2c02fc1c-24c6-4795-a258-393802fa3892.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 2c02fc1c-24c6-4795-a258-393802fa3892.cpp:29: 2c02fc1c-24c6-4795-a258-393802fa3892.cpp: In function ‘int main()’: 2c02fc1c-24c6-4795-a258-393802fa3892.cpp:39:20: error: ‘maximum’ was not declare d in this scope    39 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 2c02fc1c-24c6-4795-a258-393802fa3892.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -21,27 +21,11 @@ vector<int> maximum(vector<int> arr, int k) {
     return result;
 }
 
-vector<int> getTopKFrequent(vector<vector<int>> nums, int k) {
-    map<int,int> freqMap;
-    for(int i=0; i<nums.size();i++){
-        for(int j=0;j<nums[i].size();j++){
-            freqMap[nums[i][j]]++;
-        }
-    }
-    vector<pair<int,int>> freqVec(freqMap.begin(),freqMap.end());
-    sort(freqVec.rbegin(),freqVec.rend());
-    vector<int> res;
-    for(int i=0;i<k;i++){
-        res.push_back(freqVec[i].first);
-    }
-    return res;
+int main_function() {
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 5) , {3,243,0,-23,-400}));
 }
 
 int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 5) , {3,243,0,-23,-400});
-    vector<vector<int>> nums = {{1,1},{4,2,2,3},{3,4,3,1,2,2}};
-    vector<int> result = getTopKFrequent(nums, 2);
-    for(int i=0; i<result.size();i++){
-        cout<<result[i]<<endl;
-    }
+    main_function();
+    return 0;
 }
