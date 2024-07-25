ba195fc301dfbf73d03f468591ae30c0d120d4fb
beep boop <githubbot@vadim.me>
2024-07-25 04:56:24 +0200

    Solution 53 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/heghcwjh Prompt:     5 | 3d65608a-e62f-425d-a411-7da48702f52b.cpp:31:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    31 | int main(){       |         ^~ 3d65608a-e62f-425d-a411-7da48702f52b.cpp:31:9: note: remove parentheses to defau lt-initialize a variable    31 | int main(){       |         ^~       |         -- 3d65608a-e62f-425d-a411-7da48702f52b.cpp:31:9: note: or replace parentheses with  braces to value-initialize a variable 3d65608a-e62f-425d-a411-7da48702f52b.cpp:31:11: error: a function-definition is not allowed here before ‘{’ token    31 | int main(){       |           ^ 3d65608a-e62f-425d-a411-7da48702f52b.cpp:33:2: error: expected ‘}’ at end of inp ut    33 | }       |  ^ 3d65608a-e62f-425d-a411-7da48702f52b.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -13,7 +13,7 @@ bool strange_sort_list(const std::vector<int>& list){
     return issame(list, sorted_list);
 }
 
-int main(){
+int main() {
     assert(issame(strange_sort_list({5, 3, 7, 1}), std::vector<int>({1, 3, 5, 7})));
     assert(issame(strange_sort_list({111111}), std::vector<int>({111111}));
     return 0;
