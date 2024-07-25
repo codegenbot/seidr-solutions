b4e2864648d7ce9e56950dc6f68a9ed2f16c6621
beep boop <githubbot@vadim.me>
2024-07-25 03:22:36 +0200

    Solution 38 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4fg0mdlb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8dc618f3-afb3-4508-ac2a-26348c9e9 02e.py", line 3, in <module>     for num in numbers_str.split()                ^^^^^^^^^^^ NameError: name 'numbers_str' is not defined

--- Python_8.py
+++ Python_8.py
@@ -1,8 +1,7 @@
-numbers_str = input("Enter a string of numbers (e.g., 1-2.3 4 5-6): ")
-numbers = [
-    int(num)
-    for num in numbers_str.split()
-    if num.replace("-", "", 1).replace(".", "").isdigit()
-    or num.startswith("-.")
-    and num[2:].isdigit()
+numbers_str = input()  
+numbers = [ 
+    int(num) 
+    for num in numbers_str.split() 
+    if num.replace("-", "", 1).replace(".", "").isdigit() 
+    or num.startswith("-.") and num[2:].isdigit() 
 ]
