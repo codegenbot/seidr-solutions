ead073c12bd826ca2d73be3e3d6682bb92dc759f
beep boop <githubbot@vadim.me>
2024-07-25 05:47:12 +0200

    Solution 71 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bxl6o77x Prompt:       |                                  ^~~ fc9e4822-85d0-4d27-89c5-e6ea9c29ab9d.cpp:29:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    29 | int main(){       |         ^~ fc9e4822-85d0-4d27-89c5-e6ea9c29ab9d.cpp:29:9: note: remove parentheses to defau lt-initialize a variable    29 | int main(){       |         ^~       |         -- fc9e4822-85d0-4d27-89c5-e6ea9c29ab9d.cpp:29:9: note: or replace parentheses with  braces to value-initialize a variable fc9e4822-85d0-4d27-89c5-e6ea9c29ab9d.cpp:29:11: error: a function-definition is not allowed here before ‘{’ token    29 | int main(){       |           ^ fc9e4822-85d0-4d27-89c5-e6ea9c29ab9d.cpp:31:2: error: expected ‘}’ at end of inp ut    31 | }       |  ^ fc9e4822-85d0-4d27-89c5-e6ea9c29ab9d.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -15,6 +15,6 @@ std::vector<int> sort_array(std::vector<int> arr) {
 }
 
 int main() {
-    assert(issame({2,4,8,16,32}, {2, 4, 8, 16, 32}));
+    assert(issame({2,4,8,16,32} , {2, 4, 8, 16, 32}));
     return 0;
 }
