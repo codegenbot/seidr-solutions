e06e09dc68ed2a6ac2a39c5991be20d13b8eb4c3
beep boop <githubbot@vadim.me>
2024-07-25 02:13:26 +0200

    Solution 63 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ib89wtms Prompt:    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 31723ddb-d8c9-4d2b-9b7f-acc95b914a2f.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 31723ddb-d8c9-4d2b-9b7f-acc95b914a2f.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 31723ddb-d8c9-4d2b-9b7f-acc95b914a2f.cpp:14: 31723ddb-d8c9-4d2b-9b7f-acc95b914a2f.cpp: In function ‘int main()’: 31723ddb-d8c9-4d2b-9b7f-acc95b914a2f.cpp:24:20: error: ‘unique’ was not declared  in this scope    24 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 31723ddb-d8c9-4d2b-9b7f-acc95b914a2f.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,10 +1,14 @@
-vector<int> removeDuplicates(vector<int> l){
+#include <vector>
+#include <algorithm>
+#include <set>
+
+std::vector<int> removeDuplicates(std::vector<int> l){
     sort(l.begin(), l.end());
     l.erase(unique(l.begin(), l.end()), l.end());
     return l;
 }
 
-bool isSame(vector<int> a,vector<int>b){
+bool isSame(std::vector<int> a, std::vector<int> b){
     set<int> s1(a.begin(), a.end());
     set<int> s2(b.begin(), b.end());
     return s1 == s2;
