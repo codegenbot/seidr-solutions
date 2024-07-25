e19f9823b672aad522e3b8c0255cfd08da6c7533
beep boop <githubbot@vadim.me>
2024-07-25 06:05:17 +0200

    Solution 5 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4dok0qij Prompt: 1cf90aca-0987-4c41-aea8-bfe26a6c1134.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 1cf90aca-0987-4c41-aea8-bfe26a6c1134.cpp:21:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    21 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 1cf90aca-0987-4c41-aea8-bfe26a6c1134.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 1cf90aca-0987-4c41-aea8-bfe26a6c1134.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <string>
-#include <iostream>
 #include <cassert>
 #include <cmath>
 
@@ -19,11 +18,11 @@ vector<int> even_odd_count(int num) {
     return counts;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool areEqual(vector<int> a, vector<int> b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(even_odd_count(0), {1, 0}));
+    assert(areEqual(even_odd_count(0), {1, 0}));
     return 0;
 }
