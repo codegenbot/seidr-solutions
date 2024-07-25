9159eaba315ea3381db5c0b1f23b4053885cad4f
beep boop <githubbot@vadim.me>
2024-07-25 09:20:13 +0200

    Solution 18 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8rb6f6gi Prompt: it must return Your tweet has 22 characters for input w{8(h\\J$fZ_7q?oeh,/Z6, but it returns You didn't type anything Your tweet has 1 characters Your tweet has 140 characters Your tweet has 85 characters Too many characters.

--- twitter.py
+++ twitter.py
@@ -1,11 +1,11 @@
 ```
 def validate_tweet(tweet):
-    if not tweet:
+    elif not all(tweet[i].isalnum() or tweet[i] == ' ' for i in range(len(tweet))):
         return "You didn't type anything"
-    elif len(tweet.replace(' ', '')) > 140:
+    elif len(''.join(char for char in tweet if char.isalnum() or char == ' ')) > 140:
         return "Too many characters"
     else:
-        return f"Your tweet has {len(tweet.replace(' ', ''))} characters"
+        return f"Your tweet has {len(''.join(char for char in tweet if char.isalnum() or char == ' '))} characters"
 
 print(validate_tweet("")) 
 print(validate_tweet("1")) 
