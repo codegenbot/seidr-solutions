2ed61ccbd7349ae43df0fb9ec89a75c8335af09e
beep boop <githubbot@vadim.me>
2024-07-25 04:07:03 +0200

    Solution 39 of Python/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/kwj3d9s9 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b9957fa6-a75d-4e5b-802b-98eb0be60 815.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_29.py
+++ Python_29.py
@@ -3,11 +3,13 @@ from typing import List
 def filter_by_prefix(strings: List[str], prefix: str) -> List[str]:
     return [s for s in strings if s.startswith(prefix)]
 
-strings = [i.strip().lower() for i in input("Enter space-separated string: ").split()]
-while True:
-    prefix = input("Enter prefix: ").strip().lower()
-    if len(prefix) > 0:
-        break
-if prefix not in [s for s in strings]:
-    print("Error: Prefix is not present in the list. Please enter a valid prefix.")
-print(filter_by_prefix(strings, prefix))
+if __name__ == "__main__":
+    input_list = [i.strip().lower() for i in input("Enter space-separated string: ").split()]
+    while True:
+        prefix = input("Enter prefix: ").strip().lower()
+        if len(prefix) > 0:
+            break
+    if prefix not in [s for s in input_list]:
+        print("Error: Prefix is not present in the list. Please enter a valid prefix.")
+        prefix = input("Enter prefix: ").strip().lower()
+    print(filter_by_prefix(input_list, prefix))
