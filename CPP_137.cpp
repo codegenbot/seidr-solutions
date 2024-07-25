7edfd0bfc2e8ac80f97bc93b6bb1ce567e633902
beep boop <githubbot@vadim.me>
2024-07-25 05:30:37 +0200

    Solution 39 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y5jhcg27 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,54 +1,43 @@
-#include <iostream>
-#include<string>
-#include<boost/any.hpp>
+#include <string>
+#include <algorithm>
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return greater<int>(get<int>(a), get<float>(b));
-    }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        return a;
+        return max(a.convert_to<int>(), b.convert_to<float>());
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return b;
+        return max(a, boost::any(b.convert_to<int>()));
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (get<string>(a) > get<string>(b))
-            return a;
-        else
-            return b;
+        string str1 = a.convert_to<string>();
+        string str2 = b.convert_to<string>();
+        int n1 = stoi(str1.substr(0, str1.find(',')));
+        int n2 = stoi(str2.substr(0, str2.find(',')));
+        if (n1 > n2) return a;
+        else if (n1 < n2) return b;
+        else return boost::any("None");
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = get<int>(a);
-        int y = get<int>(b);
-        if (x > y)
-            return a;
-        else if (x < y)
-            return b;
-        else
-            return boost::any("None");
+    else if (a.type() == typeid(string) && b.type() != typeid(string)) {
+        string str = a.convert_to<string>();
+        int n = stoi(str.substr(0, str.find(',')));
+        if (b.convert_to<int>() > n) return a;
+        else if (b.convert_to<int>() < n) return b;
+        else return boost::any("None");
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        float x = get<float>(a);
-        float y = get<float>(b);
-        if (x > y)
-            return a;
-        else if (x < y)
-            return b;
-        else
-            return boost::any("None");
+    else if (a.type() != typeid(string) && b.type() == typeid(string)) {
+        string str = b.convert_to<string>();
+        int n = stoi(str.substr(0, str.find(',')));
+        if (a.convert_to<int>() > n) return a;
+        else if (a.convert_to<int>() < n) return b;
+        else return boost::any("None");
     }
     else {
-        return boost::any("None");
+        if (a.convert_to<int>() > b.convert_to<int>()) return a;
+        else if (a.convert_to<int>() < b.convert_to<int>()) return b;
+        else return boost::any("None");
     }
-}
-
-int main() {
-    cout << boost::any_cast<string>(compare_one(1, 2.5)) << endl;
-    cout << boost::any_cast<string>(compare_one(1, "2,3")) << endl;
-    cout << boost::any_cast<string>(compare_one("5,1", "6")) << endl;
-    cout << boost::any_cast<string>(compare_one("1", 1)) << endl;
-    return 0;
 }
