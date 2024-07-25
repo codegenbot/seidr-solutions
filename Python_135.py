e845bf35c0fd8e045c33784a0ac368ff907fb1ae
beep boop <githubbot@vadim.me>
2024-07-25 08:07:10 +0200

    Solution 1 of Python/135, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/4y7k6i88 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3e093ec3-3048-4964-9211-695c69f1b 86c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_135.py
+++ Python_135.py
@@ -1,6 +1,6 @@
-```
 def can_arrange(arr):
+    arr.sort()
     for i in range(1, len(arr)):
-        if arr[i] <= arr[i-1]:
-            return i-1
+        if arr[i] <= arr[i - 1]:
+            return i
     return -1
