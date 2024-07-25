5ceb6880606ff71c87eb3ed2cd9e2ceef420b1c7
beep boop <githubbot@vadim.me>
2024-07-25 05:35:07 +0200

    Solution 65 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2xjiqqsy Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/194009bf-2434-4dc4-b6c8-721990a4a cbf.py", line 4     temp = [i for i in sorted([i for _ in range(len(lst)), i])]                                                          ^ SyntaxError: invalid syntax

--- Python_33.py
+++ Python_33.py
@@ -1,7 +1,7 @@
 def sort_third(lst):
     result = []
     while len(lst) > 0:
-        temp = [i for _ in range(len(lst))] + sorted(lst)
+        temp = [i[2] for i in sorted([(a, b, c) for (a, b), c in zip(*[iter(lst)]*3)])]
         del lst[:]
         result.append(tuple(temp))
     return tuple(result)
