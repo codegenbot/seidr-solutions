6bac41b87b12a6826cd268f8b6dd22a9cedc4829
beep boop <githubbot@vadim.me>
2024-07-25 05:03:46 +0200

    Solution 58 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xpbcy5mf Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ead70796-104b-4206-9897-f6fe93ed6e5e.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ead70796-104b-4206-9897-f6fe93ed6e5e.cpp:43:44: error: ‘b’ was not declared in t his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from ead70796-104b-4206-9897-f6fe93ed6e5e.cpp:42: ead70796-104b-4206-9897-f6fe93ed6e5e.cpp: In function ‘int main()’: ead70796-104b-4206-9897-f6fe93ed6e5e.cpp:52:42: error: cannot convert ‘std::vect or<std::__cxx11::basic_string<char> >’ to ‘float’    52 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~       |                                          |       |                                          std::vector<std::__cxx11::basic _string<char> > ead70796-104b-4206-9897-f6fe93ed6e5e.cpp:4:19: note:   initializing argument 1 o f ‘bool issame(float, float)’     4 | bool issame(float a, float b) {       |             ~~~~~~^

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <string>
 
@@ -12,27 +11,27 @@ bool issame(float a, float b) {
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
     std::vector<std::string> letterGrades;
     for (float grade : grades) {
-        if (issame(grade, 4.0f))
+        if (issame(grade, 4.0))
             letterGrades.push_back("A+");
-        else if (issame(grade, 3.7f))
+        else if (issame(grade, 3.7))
             letterGrades.push_back("A");
-        else if (issame(grade, 3.3f))
+        else if (issame(grade, 3.3))
             letterGrades.push_back("A-");
-        else if (issame(grade, 3.0f))
+        else if (issame(grade, 3.0))
             letterGrades.push_back("B+");
-        else if (issame(grade, 2.7f))
+        else if (issame(grade, 2.7))
             letterGrades.push_back("B");
-        else if (issame(grade, 2.3f))
+        else if (issame(grade, 2.3))
             letterGrades.push_back("B-");
-        else if (issame(grade, 2.0f))
+        else if (issame(grade, 2.0))
             letterGrades.push_back("C+");
-        else if (issame(grade, 1.7f))
+        else if (issame(grade, 1.7))
             letterGrades.push_back("C");
-        else if (issame(grade, 1.3f))
+        else if (issame(grade, 1.3))
             letterGrades.push_back("C-");
-        else if (issame(grade, 1.0f))
+        else if (issame(grade, 1.0))
             letterGrades.push_back("D+");
-        else if (issame(grade, 0.7f))
+        else if (issame(grade, 0.7))
             letterGrades.push_back("D");
         else
             letterGrades.push_back("E");
