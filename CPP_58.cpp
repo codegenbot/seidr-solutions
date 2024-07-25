50aa4de26e5b5bf75d58e99beca48447bd14e84d
beep boop <githubbot@vadim.me>
2024-07-25 09:33:31 +0200

    Solution 15 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/xfgmwr5g Prompt: aa995be8-9331-4e21-8368-a3485f4d0185.cpp:13:5: error: ‘set_intersection’ was not  declared in this scope    13 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |     ^~~~~~~~~~~~~~~~ aa995be8-9331-4e21-8368-a3485f4d0185.cpp:16:31: error: request for member ‘begin ’ in ‘common’, which is of non-class type ‘std::vector<int>(std::vector<int>, st d::vector<int>)’    16 |     vector<int> result(common.begin(), common.end());       |                               ^~~~~ aa995be8-9331-4e21-8368-a3485f4d0185.cpp:16:47: error: request for member ‘end’ in ‘common’, which is of non-class type ‘std::vector<int>(std::vector<int>, std: :vector<int>)’    16 |     vector<int> result(common.begin(), common.end());       |                                               ^~~ aa995be8-9331-4e21-8368-a3485f4d0185.cpp: At global scope: aa995be8-9331-4e21-8368-a3485f4d0185.cpp:21:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    21 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ aa995be8-9331-4e21-8368-a3485f4d0185.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -10,9 +10,9 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s2(l2.begin(), l2.end());
 
     set<int> common;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+    set_difference(s1.begin(), s1.end(), s2.begin(), s2.end(),
         back_inserter(common));
 
-    vector<int> result(common.begin(), common.end()-1);
+    vector<int> result(common.begin(), common.end() - 1);
     return result;
 }
