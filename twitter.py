321fd89c27dc7a37723a555dd6975bc02a3137af
beep boop <githubbot@vadim.me>
2024-07-25 09:33:10 +0200

    Solution 32 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/8rb6f6gi Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/28b87007-5438-4434-9117-e09b49d97 961.py", line 1     ```     ^ SyntaxError: invalid syntax

--- twitter.py
+++ twitter.py
@@ -1,1 +1,14 @@
-'print("Your tweet has " + str(len(input("Enter a tweet: "))) + " characters" if len(input("Enter a tweet: ")) <= 140 else "Too many characters" if not input("Enter a tweet: ") else "You didn't type anything")'
+def validate_tweet(tweet):
+    tweet = ''.join(e for e in tweet if e.isalnum() or e in '@# ')
+    if not tweet:
+        return "You didn't type anything"
+    elif len(tweet) > 140:
+        return "Too many characters"
+    else:
+        return f"Your tweet has {len(tweet)} characters"
+
+print(validate_tweet("")) 
+print(validate_tweet("1")) 
+print(validate_tweet("max length tweet..."))
+print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234"))
+print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"))
