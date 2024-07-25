1d4d9e9201492b5ad9fecb63a4bb23df99689edd
beep boop <githubbot@vadim.me>
2024-07-25 06:06:20 +0200

    Solution 5 of Python/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/484w4pp4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bd60bcb0-6427-40dd-b84f-784c4cbe6 15b.py", line 17, in <module>     check(fruit_distribution)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bd60bcb0-6427-40dd-b84f-784c4cbe6 15b.py", line 14, in check     assert fruit_distribution("1 apples and 100 oranges", 120) == 19            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bd60bcb0-6427-40dd-b84f-784c4cbe6 15b.py", line 9, in fruit_distribution     count += int(parts[i].lstrip("0123456789"))              ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: ''

--- Python_67.py
+++ Python_67.py
@@ -1,3 +1,11 @@
 def fruit_distribution(s, n):
     s = s.replace("apples", "a").replace("oranges", "o")
-    return n - int(s.split()[2].split(" ")[0].lstrip("0123456789")) if 'and' not in s.split()[2].split(" ")[0] else n
+    parts = s.split()
+    count = 0
+    for i in range(len(parts)):
+        if i < 2:
+            continue
+        if 'and' not in parts[i]:
+            if parts[i].lstrip("0123456789"):
+                count += int(parts[i].lstrip("0123456789"))
+    return n - count
