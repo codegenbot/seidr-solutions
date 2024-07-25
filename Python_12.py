3e8a3a78dc219c623f8b50d111358d3bb45cc0a6
beep boop <githubbot@vadim.me>
2024-07-25 02:44:08 +0200

    Solution 43 of Python/12, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/j87okes2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/42cb28db-1534-47ef-ae6f-95305d1da 852.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_12.py
+++ Python_12.py
@@ -1,12 +1,10 @@
 ```
-result = []
-
 input_strs = input("Enter some strings (separated by spaces): ")
+result = []
 for s in [i.strip() for i in input_strs.split()]:
     if s:
         result.append((s, len(s)))
-
-if len(result) > 0:  
+if result:  
     print("The longest strings are:")
     for s, length in sorted(result, key=lambda x:x[1], reverse=True):
         print(f"'{s}' with {length} characters")
