2388fce80e32fe596150f217b1e6a242a797ee2b
beep boop <githubbot@vadim.me>
2024-07-25 06:38:23 +0200

    Solution 98 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/6nymn7m1 Prompt:    33 | } else if ((a.type() == typeid(double)) &&       |   ^~~~ 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:40:3: error: expected unqualified-id be fore ‘else’    40 | } else {       |   ^~~~ In file included from 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:45: 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp: In function ‘int main()’: 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:47:13: error: ‘boost’ has not been decl ared    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:47:29: error: ‘string’ was not declared  in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 70b07646-1937-44ee-ae7a-70b2165b37e5.cpp:47:37: error: ‘compare_one’ was not dec lared in this scope    47 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,58 +1,55 @@
+#include <iostream>
 #include <any>
 #include <string>
-#include <wstring>
-using namespace std;
+#include <memory>
 
-any a;
-any b;
-
-namespace std {
-
+std::any get_larger(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        int x = any_cast<int>(a);
-        int y = any_cast<int>(b);
+        int x = std::any_cast<int>(a);
+        int y = std::any_cast<int>(b);
         if (x > y) return a;
         else if (x < y) return b;
         else return a;
-    } else if (a.type() == typeid(double) && b.type() == typeid(double)) {
-        double x = any_cast<double>(a);
-        double y = any_cast<double>(b);
+    } 
+    else if (a.type() == typeid(double) && b.type() == typeid(double)) {
+        double x = std::any_cast<double>(a);
+        double y = std::any_cast<double>(b);
         if (x > y) return a;
         else if (x < y) return b;
         else return a;
-    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        string x = any_cast<string>(a);
-        string y = any_cast<string>(b);
+    } 
+    else if ((a.type() == typeid(std::string) || a.type() == typeid(char*)) &&
+             (b.type() == typeid(std::string) || b.type() == typeid(char*))) {
+        std::string x = std::any_cast<std::string>(a);
+        std::string y = std::any_cast<std::string>(b);
         if (x > y) return a;
         else if (x < y) return b;
         else return a;
-    } else if ((a.type() == typeid(string) || a.type() == typeid(wstring)) &&
-           (b.type() == typeid(double))) {
-        string x = any_cast<string>(a);
-        double y = any_cast<double>(b);
-        if(stod(x) > y) return a;
-        else if(stod(x) < y) return b;
+    } 
+    else if ((a.type() == typeid(std::string) || a.type() == typeid(char*)) &&
+             (b.type() == typeid(double))) {
+        std::string x = std::any_cast<std::string>(a);
+        double y = std::any_cast<double>(b);
+        if (std::stod(x) > y) return a;
+        else if (std::stod(x) < y) return b;
         else return a;
-    } else if ((a.type() == typeid(double)) &&
-           (b.type() == typeid(string) || b.type() == typeid(wstring))) {
-        double x = any_cast<double>(a);
-        string y = any_cast<string>(b);
-        if(x > stod(y)) return a;
-        else if(x < stod(y)) return b;
+    } 
+    else if ((a.type() == typeid(double)) &&
+             (b.type() == typeid(std::string) || b.type() == typeid(char*))) {
+        double x = std::any_cast<double>(a);
+        std::string y = std::any_cast<std::string>(b);
+        if (x > std::stod(y)) return a;
+        else if (x < std::stod(y)) return b;
         else return a;
-    } else {
+    } 
+    else {
         return a;
     }
 }
 
 int main() {
-    // Input values for 'a' and 'b'
-    a = any(string("hello"));
-    b = any(string("world"));
-
-    // Call the function with 'a' and 'b' as arguments
-    any result = your_function(a, b);
-    
-    return 0;
+    std::any a, b; // variables declared here
+    // your logic goes here
+    // you may want to get the user input in this place.
+    std::cout << "Larger Value: " << get_larger(a, b) << std::endl;
 }
