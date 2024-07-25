c14e075ca6b98d463cc219153f8182357745a715
beep boop <githubbot@vadim.me>
2024-07-25 05:14:28 +0200

    Solution 21 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/y5jhcg27 Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -7,30 +7,63 @@ using namespace std;
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        size_t pos = str.find(',');
-        if (pos != string::npos) {
-            str = str.substr(0, pos);
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        if ((string)a.convert_to<string>() > (string)b.convert_to<string>()) {
+            return a;
         }
-        return max((float)a.convert_to<float>(), stof(str));
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        size_t pos1 = (string)a.convert_to<string>().find(',');
-        size_t pos2 = (string)b.convert_to<string>().find(',');
-        string str1 = (pos1 != string::npos) ? (string)a.convert_to<string>().substr(0, pos1) : (string)a.convert_to<string>();
-        string str2 = (pos2 != string::npos) ? (string)b.convert_to<string>().substr(0, pos2) : (string)b.convert_to<string>();
-        return max(str1 > str2 ? a : b);
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        size_t pos = (string)b.convert_to<string>().find(',');
-        string str = (pos != string::npos) ? (string)b.convert_to<string>().substr(0, pos) : (string)b.convert_to<string>();
-        return max((int)a.convert_to<int>(), stof(str));
-    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        size_t pos = (string)a.convert_to<string>().find(',');
-        string str = (pos != string::npos) ? (string)a.convert_to<string>().substr(0, pos) : (string)a.convert_to<string>();
-        return max(stof(str), (int)b.convert_to<int>());
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return max((float)a.convert_to<float>(), (int)b.convert_to<int>());
-    } else {
+        else if ((string)a.convert_to<string>() < (string)b.convert_to<string>()) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        if ((float)a.convert_to<float>() > (string)b.convert_to<string>().c_str()) {
+            return a;
+        }
+        else if ((float)a.convert_to<float>() < (string)b.convert_to<string>().c_str()) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        if ((string)a.convert_to<string>() > to_string((int)b.convert_to<int>())) {
+            return a;
+        }
+        else if ((string)a.convert_to<string>() < to_string((int)b.convert_to<int>())) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        if ((string)a.convert_to<string>() > to_string((float)b.convert_to<float>())) {
+            return a;
+        }
+        else if ((string)a.convert_to<string>() < to_string((float)b.convert_to<float>())) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if ((string)a.convert_to<string>() > (string)b.convert_to<string>()) {
+            return a;
+        }
+        else if ((string)a.convert_to<string>() < (string)b.convert_to<string>()) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
+    }
+    else {
         return boost::any("None");
     }
 }
