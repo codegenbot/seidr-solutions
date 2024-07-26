e0d3297253e7c0dc2a27441e290cd68d04c9be4c
beep boop <githubbot@vadim.me>
2024-07-26 09:21:27 +0200

    Solution 43 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/2uiygj4p Prompt:       |                    ^~~ 399a478b-9015-4cf4-9866-571e6e1cd957.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 399a478b-9015-4cf4-9866-571e6e1cd957.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 399a478b-9015-4cf4-9866-571e6e1cd957.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 399a478b-9015-4cf4-9866-571e6e1cd957.cpp:6: 399a478b-9015-4cf4-9866-571e6e1cd957.cpp: In function ‘int main()’: 399a478b-9015-4cf4-9866-571e6e1cd957.cpp:16:20: error: ‘eat’ was not declared in  this scope    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ^~~ 399a478b-9015-4cf4-9866-571e6e1cd957.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,5 +1,5 @@
 bool issame(std::vector<int> a, std::vector<int> b){
     int total = a[0] + a[1];
-    int left = std::max(0, b[1] - a[1]);
+    int left = std::max(0, b[0] - b[1]);
     return {total, left};
 }
