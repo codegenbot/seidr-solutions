e20820334da89eef8a22a49f2a03b4414cc3c613
beep boop <githubbot@vadim.me>
2024-07-26 08:37:54 +0200

    Solution 4 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/646k5bnu Prompt: 635a64d4-3f00-43d0-8411-9905a9bd6dab.cpp:1:20: error: ‘vector’ does not name a t ype     1 | int basement(const vector<int>& nums) {       |                    ^~~~~~ 635a64d4-3f00-43d0-8411-9905a9bd6dab.cpp:1:26: error: expected ‘,’ or ‘...’ befo re ‘<’ token     1 | int basement(const vector<int>& nums) {       |                          ^ 635a64d4-3f00-43d0-8411-9905a9bd6dab.cpp: In function ‘int basement(int)’: 635a64d4-3f00-43d0-8411-9905a9bd6dab.cpp:3:25: error: ‘nums’ was not declared in  this scope     3 |     for (int i = 0; i < nums.size(); ++i) {       |                         ^~~~

--- basement.cpp
+++ basement.cpp
@@ -1,9 +1,5 @@
-#include <iostream>
 #include <vector>
-
-using namespace std;
-
-int basement(const std::vector<int>& nums) {
+int basement(const vector<int>& nums) {
     int sum = 0;
     for (int i = 0; i < nums.size(); ++i) {
         sum += nums[i];
