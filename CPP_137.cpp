9b9bfc65ddc2350647181838847f8514a888efe3
beep boop <githubbot@vadim.me>
2024-07-26 09:49:54 +0200

    Solution 52 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/5gyhh2hk Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,35 +1,28 @@
-if(a.type() == typeid(int) && b.type() == typeid(int)){
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b))
-        return a;
-    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b))
-        return b;
-    else
+if(typeid(a) == typeid(int) && typeid(b) == typeid(int)){
+        if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+            return a;
+        } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+            return b;
+        } else {
+            return "None";
+        }
+    } else if(typeid(a) == typeid(float) && typeid(b) == typeid(float)){
+        if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+            return a;
+        } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+            return b;
+        } else {
+            return "None";
+        }
+    } else if(typeid(a) == typeid(string) && typeid(b) == typeid(string)){
+        if(stod(boost::any_cast<string>(a)) > stod(boost::any_cast<string>(b))){
+            return a;
+        } else if(stod(boost::any_cast<string>(a)) < stod(boost::any_cast<string>(b))){
+            return b;
+        } else {
+            return "None";
+        }
+    } else {
         return "None";
-}
-else if(a.type() == typeid(float) && b.type() == typeid(float)){
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b))
-        return a;
-    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b))
-        return b;
-    else
-        return "None";
-}
-else if(a.type() == typeid(string) && b.type() == typeid(string)){
-    float num1, num2;
-    if(((string)boost::any_cast<string>(a)).find(',') != string::npos){
-        replace(boost::any_cast<string>(a).begin(), boost::any_cast<string>(a).end(), ',', '.');
     }
-    if(((string)boost::any_cast<string>(b)).find(',') != string::npos){
-        replace(boost::any_cast<string>(b).begin(), boost::any_cast<string>(b).end(), ',', '.');
-    }
-    num1 = stof(boost::any_cast<string>(a));
-    num2 = stof(boost::any_cast<string>(b));
-    if(num1 > num2)
-        return a;
-    else if(num1 < num2)
-        return b;
-    else
-        return "None";
-}
-return "None";
 }
