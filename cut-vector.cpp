e26364fcf54cd6f587855808937a6d4278609ab4
beep boop <githubbot@vadim.me>
2024-07-26 19:47:16 +0200

    Solution 35 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/w32lba64 Prompt: er ‘<climits>’; did you forget to ‘#include <climits>’?   +++ |+#include <climits>     1 | int main() { dd738b8e-bfc7-4ab7-8b94-a144846e5754.cpp:19:28: error: ‘abs’ was not declared in  this scope    19 |         int current_diff = abs(left_sum - right_sum);       |                            ^~~ dd738b8e-bfc7-4ab7-8b94-a144846e5754.cpp:27:9: error: ‘cout’ was not declared in  this scope    27 |         cout << nums[i] << endl;       |         ^~~~ dd738b8e-bfc7-4ab7-8b94-a144846e5754.cpp:27:28: error: ‘endl’ was not declared i n this scope    27 |         cout << nums[i] << endl;       |                            ^~~~ dd738b8e-bfc7-4ab7-8b94-a144846e5754.cpp:30:5: error: ‘cout’ was not declared in  this scope    30 |     cout << 0 << endl;       |     ^~~~ dd738b8e-bfc7-4ab7-8b94-a144846e5754.cpp:30:18: error: ‘endl’ was not declared i n this scope    30 |     cout << 0 << endl;       |                  ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -7,39 +7,39 @@
 int main() {
     std::vector<int> nums;
     int num;
-    
+
     while (std::cin >> num) {
         nums.push_back(num);
     }
-    
+
     int total_sum = std::accumulate(nums.begin(), nums.end(), 0);
     int left_sum = 0;
     int right_sum = total_sum;
     int min_diff = INT_MAX;
     int cut_index = 0;
-    
+
     for (int i = 0; i < nums.size(); ++i) {
         left_sum += nums[i];
         right_sum -= nums[i];
-        
+
         int current_diff = std::abs(left_sum - right_sum);
         if (current_diff < min_diff) {
             min_diff = current_diff;
             cut_index = i;
         }
     }
-    
+
     for (int i = 0; i <= cut_index; ++i) {
         std::cout << nums[i] << std::endl;
     }
-    
+
     std::cout << 0 << std::endl;
-    
+
     for (int i = cut_index + 1; i < nums.size(); ++i) {
         std::cout << nums[i] << std::endl;
     }
-    
+
     std::cout << 0 << std::endl;
-    
+
     return 0;
 }
