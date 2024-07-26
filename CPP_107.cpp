ff3751a94b8f64049db9322e0245dd79c0f34279
beep boop <githubbot@vadim.me>
2024-07-26 08:52:55 +0200

    Solution 87 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/w3orlipy Prompt: 04117f2b-ddce-4765-8f95-e7148999d2fb.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 04117f2b-ddce-4765-8f95-e7148999d2fb.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> even_odd_palindrome(int n) {
     vector<int> palindrome;
 
@@ -22,5 +18,8 @@ vector<int> even_odd_palindrome(int n) {
 }
 
 int main() {
-    assert(issame(even_odd_palindrome(1), vector<int>{0, 1, 0}));
+    vector<int> expected{0, 1, 0};
+    vector<int> result = even_odd_palindrome(1);
+    assert(result == expected);
+    return 0;
 }
