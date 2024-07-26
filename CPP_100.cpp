2def9ddb48e7934efbd3ed1140e4a43c04bf9c0b
beep boop <githubbot@vadim.me>
2024-07-26 08:21:24 +0200

    Solution 2 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/tgnjn9ln Prompt:       |                    ^~~ c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp:14: c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp: In function ‘int main()’: c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp:24:20: error: ‘make_a_pile’ was not dec lared in this scope    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ c5b1256d-cf38-4f1c-b031-97f233f1095a.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,19 +1,15 @@
 #include <vector>
 
-vector<int> stones;
-stones.push_back(n);
-for(int i = 1; i < n; ++i){
-    if(n % 2 == 0){
-        n += 2;
-    } else {
-        n += 1;
-    }
-    stones.push_back(n);
-}
-return stones;
-
 bool issame(vector<int> a, vector<int> b){
-    return a == b;
+    if(a.size() != b.size()){
+        return false;
+    }
+    for(int i = 0; i < a.size(); ++i){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<int> make_a_pile(int n){
@@ -28,17 +24,4 @@ vector<int> make_a_pile(int n){
         stones.push_back(n);
     }
     return stones;
-}
-
-int main(){
-    vector<int> pile1 = make_a_pile(5);
-    vector<int> pile2 = make_a_pile(7);
-    
-    if(issame(pile1, pile2)){
-        cout << "Piles are the same" << endl;
-    } else {
-        cout << "Piles are different" << endl;
-    }
-    
-    return 0;
 }
