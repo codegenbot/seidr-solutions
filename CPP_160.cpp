abbf577731aff7d852964114c4e8c2552e03098b
beep boop <githubbot@vadim.me>
2024-07-26 09:17:42 +0200

    Solution 26 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/g1qqwnol Prompt:       |                       ^~~~~~ 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp:1:31: error: ‘operato’ was not declared  in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand){       |                               ^~~~~~~ 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp:1:40: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operato, vector<int> operand){       |                                        ^~~~~~ 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp:1:47: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operato, vector<int> operand){       |                                               ^~~ 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp:1:59: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operato, vector<int> operand){       |                                                           ^ In file included from 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp:19: 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp: In function ‘int main()’: 2cbbe478-1999-417d-813e-d7ae67024d8e.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -24,7 +24,6 @@ int do_algebra(const vector<string>& operato, const vector<int>& operand) {
 
 int main() {
     assert(do_algebra({"//", "*"}, {7, 3, 4}) == 8);
-    // Additional test cases can be added here
 
     return 0;
 }
