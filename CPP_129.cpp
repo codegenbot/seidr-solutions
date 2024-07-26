b10cb0cb5926bbd94d63bf3a8315126f82559b87
beep boop <githubbot@vadim.me>
2024-07-26 08:55:55 +0200

    Solution 2 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/v7ck3kaw Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 927ca7c4-211c-44ee-9804-bfe012736e7d.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 927ca7c4-211c-44ee-9804-bfe012736e7d.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 927ca7c4-211c-44ee-9804-bfe012736e7d.cpp:5: 927ca7c4-211c-44ee-9804-bfe012736e7d.cpp: In function ‘int main()’: 927ca7c4-211c-44ee-9804-bfe012736e7d.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 927ca7c4-211c-44ee-9804-bfe012736e7d.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,38 +1,45 @@
-#include <vector>
-#include <cassert>
-
-using namespace std;
-
 vector<int> minPath(vector<vector<int>> grid, int k){
-    if(grid.empty()) return {};
-    
-    int m = grid.size();
-    int n = grid[0].size();
-    
     vector<int> result;
+    int n = grid.size();
+    int m = grid[0].size();
     
-    for(int i = 0; i < m; ++i){
-        for(int j = 0; j < n; ++j){
-            result.push_back(grid[i][j]);
-            if(k > 0 && j < n - 1){
-                result.push_back(grid[i][j]);
-                k--;
+    vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+    dp[0][0] = grid[0][0];
+    
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+    pq.push({grid[0][0], {0, 0}});
+    
+    vector<pair<int, int>> dirs = {{0, 1}, {1, 0}};
+    
+    while(!pq.empty()){
+        auto curr = pq.top();
+        pq.pop();
+        
+        int x = curr.second.first;
+        int y = curr.second.second;
+        
+        for(auto dir : dirs){
+            int nx = x + dir.first;
+            int ny = y + dir.second;
+            
+            if(nx >= 0 && nx < n && ny >= 0 && ny < m){
+                int new_dist = dp[x][y] + grid[nx][ny];
+                
+                if(new_dist < dp[nx][ny]){
+                    dp[nx][ny] = new_dist;
+                    pq.push({new_dist, {nx, ny}});
+                }
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(dp[i][j] <= k){
+                result.push_back(dp[i][j]);
             }
         }
     }
     
     return result;
-}
-
-bool issame(vector<int> v1, vector<int> v2){
-    if(v1.size() != v2.size()) return false;
-    for(int i = 0; i < v1.size(); ++i){
-        if(v1[i] != v2[i]) return false;
-    }
-    return true;
-}
-
-int main(){
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
-    return 0;
 }
