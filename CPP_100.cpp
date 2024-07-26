c15ccc1673564c3cc680b189f830b373be88f32e
beep boop <githubbot@vadim.me>
2024-07-26 09:05:24 +0200

    Solution 86 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/tgnjn9ln Prompt:       |                                  ^~~ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:20: 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp: In function ‘int main()’: 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:30:20: error: ‘make_a_pile’ was not dec lared in this scope    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <vector>
 
-bool is_same(int a, int b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
 std::vector<int> make_a_pile(int n) {
     std::vector<int> result;
     for (int i = 1; i <= n; ++i) {
-        if (is_same(i, n)) {
-            result.push_back(i);
-        }
+        result.push_back(i);
     }
     return result;
 }
@@ -19,13 +17,13 @@ int solve_problem_cpp100() {
     int n;
     std::cin >> n;
     std::vector<int> result = make_a_pile(n);
-    
+
     for (int i = 0; i < result.size(); ++i) {
         std::cout << result[i] << " ";
     }
-    
+
     std::cout << "\n";
-    
+
     return 0;
 }
 
