33fa9e5b907eaa811dd0dcb13ad3eaabeb94b343
beep boop <githubbot@vadim.me>
2024-07-26 09:50:05 +0200

    Solution 54 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/5gyhh2hk Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,23 +1,26 @@
-if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
-            return a;
-        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
-            return b;
-        }
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
-            return a;
-        } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
-            return b;
-        }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        float val_a = stof(boost::any_cast<string>(a).replace(boost::any_cast<string>(a).find(','), 1, "."));
-        float val_b = stof(boost::any_cast<string>(b).replace(boost::any_cast<string>(b).find(','), 1, "."));
-        if (val_a > val_b) {
-            return a;
-        } else if (val_a < val_b) {
-            return b;
-        }
+if(a.type() == typeid(int) && b.type() == typeid(int)){
+    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+        return a;
     }
-    return "None";
+    else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+        return b;
+    }
+}
+else if(a.type() == typeid(float) && b.type() == typeid(float)){
+    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+        return a;
+    }
+    else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+        return b;
+    }
+}
+else if(a.type() == typeid(string) && b.type() == typeid(string)){
+    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))){
+        return a;
+    }
+    else if(stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))){
+        return b;
+    }
+}
+return "None";
 }
