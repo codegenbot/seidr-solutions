a7a01a7c62e383b5281ec3adbe26441bc713c894
beep boop <githubbot@vadim.me>
2024-07-26 08:33:16 +0200

    Solution 91 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fhw6w0do Prompt:     5 | 649bc859-181f-446c-9ad6-fa8c557b4ca2.cpp:32:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    32 | int main(){       |         ^~ 649bc859-181f-446c-9ad6-fa8c557b4ca2.cpp:32:9: note: remove parentheses to defau lt-initialize a variable    32 | int main(){       |         ^~       |         -- 649bc859-181f-446c-9ad6-fa8c557b4ca2.cpp:32:9: note: or replace parentheses with  braces to value-initialize a variable 649bc859-181f-446c-9ad6-fa8c557b4ca2.cpp:32:11: error: a function-definition is not allowed here before ‘{’ token    32 | int main(){       |           ^ 649bc859-181f-446c-9ad6-fa8c557b4ca2.cpp:34:2: error: expected ‘}’ at end of inp ut    34 | }       |  ^ 649bc859-181f-446c-9ad6-fa8c557b4ca2.cpp:17:12: note: to match this ‘{’    17 | int main() {       |            ^

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -14,8 +14,5 @@ namespace contest {
     }
 }
 
-int main() {
-    assert(contest::issame(contest::sort_array({21, 14, 23, 11}), contest::sort_array({11, 14, 21, 23}));
-    std::cout << "Arrays are the same" << std::endl;
-    return 0;
-}
+assert(contest::issame(contest::sort_array({21, 14, 23, 11}), contest::sort_array({11, 14, 21, 23}));
+std::cout << "Arrays are the same" << std::endl;
