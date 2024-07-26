6d4c99e42fa7f62a35385179269c314d42885340
beep boop <githubbot@vadim.me>
2024-07-26 09:34:23 +0200

    Solution 50 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/06jbcg5l Prompt: 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:18:5: error: ‘string’ does not name a t ype    18 |     string extension = file_name.substr(dot_position + 1);       |     ^~~~~~ 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:20:5: error: expected unqualified-id be fore ‘if’    20 |     if (extension != "txt" && extension != "exe" && extension != "dll") {       |     ^~ 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:24:5: error: expected unqualified-id be fore ‘return’    24 |     return "Yes";       |     ^~~~~~ 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:25:1: error: expected declaration befor e ‘}’ token    25 | }       | ^ In file included from 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:27: 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp: In function ‘int main()’: 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:29:13: error: ‘file_name_check’ was not  declared in this scope    29 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,10 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
 string file_name_check(const string& file_name) {
-    if(file_name.empty()) return "No";
-    size_t dotPos = file_name.find(".");
-    if(dotPos == string::npos || dotPos == 0 || dotPos == file_name.size()-1) return "No";
-    string beforeDot = file_name.substr(0, dotPos);
-    string afterDot = file_name.substr(dotPos + 1);
-    if(beforeDot.find_first_of("0123456789") != string::npos || beforeDot.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") == string::npos) return "No";
-    if(afterDot != "txt" && afterDot != "exe" && afterDot != "dll") return "No";
+    int digits_count = 0;
+    int dot_count = 0;
+    int dot_position = -1;
+    
+    for (int i = 0; i < file_name.length(); i++) {
+        if (file_name[i] >= '0' && file_name[i] <= '9') {
+            digits_count++;
+        } else if (file_name[i] == '.') {
+            dot_count++;
+            dot_position = i;
+        }
+    }
+    
+    if (digits_count > 3 || dot_count != 1 || dot_position == 0 || dot_position == file_name.length() - 1) {
+        return "No";
+    }
+    
+    string extension = file_name.substr(dot_position + 1);
+    
+    if (extension != "txt" && extension != "exe" && extension != "dll") {
+        return "No";
+    }
+    
     return "Yes";
+}
+
+int main() {
+    assert(file_name_check("s.") == "No");
+    
+    return 0;
 }
