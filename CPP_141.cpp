354b467e7aa7273dd0f37e52d48d0cdbb3b90e3e
beep boop <githubbot@vadim.me>
2024-07-26 09:39:45 +0200

    Solution 61 of CPP/141, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/06jbcg5l Prompt: 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:18:5: error: ‘string’ does not name a t ype    18 |     string extension = file_name.substr(dot_position + 1);       |     ^~~~~~ 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:20:5: error: expected unqualified-id be fore ‘if’    20 |     if (extension != "txt" && extension != "exe" && extension != "dll") {       |     ^~ 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:24:5: error: expected unqualified-id be fore ‘return’    24 |     return "Yes";       |     ^~~~~~ 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:25:1: error: expected declaration befor e ‘}’ token    25 | }       | ^ In file included from 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:27: 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp: In function ‘int main()’: 95626e8d-8c8f-4d07-9c9e-f274400d35f7.cpp:29:13: error: ‘file_name_check’ was not  declared in this scope    29 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,26 +1,27 @@
-int digitCount = 0;
-    for (char c : file_name) {
-        if (isdigit(c)) {
-            digitCount++;
+#include <string>
+using namespace std;
+
+string file_name_check(string file_name) {
+    int digits_count = 0;
+    int dot_count = 0;
+    int dot_position = -1;
+    
+    for (int i = 0; i < file_name.length(); i++) {
+        if (file_name[i] >= '0' && file_name[i] <= '9') {
+            digits_count++;
+        } else if (file_name[i] == '.') {
+            dot_count++;
+            dot_position = i;
         }
     }
-    if (digitCount > 3) {
+    
+    if (digits_count > 3 || dot_count != 1 || dot_position == 0 || dot_position == file_name.length() - 1) {
         return "No";
     }
     
-    size_t dotPos = file_name.find('.');
-    if (dotPos == string::npos || dotPos == 0 || dotPos == file_name.size()-1) {
-        return "No";
-    }
+    string extension = file_name.substr(dot_position + 1);
     
-    string beforeDot = file_name.substr(0, dotPos);
-    string afterDot = file_name.substr(dotPos + 1);
-    
-    if (!isalpha(beforeDot[0])) {
-        return "No";
-    }
-    
-    if (afterDot != "txt" && afterDot != "exe" && afterDot != "dll") {
+    if (extension != "txt" && extension != "exe" && extension != "dll") {
         return "No";
     }
     
