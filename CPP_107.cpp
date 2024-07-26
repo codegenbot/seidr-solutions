510296b9a4decb54e556195b84ccbb94cdfbe6f0
beep boop <githubbot@vadim.me>
2024-07-26 08:30:51 +0200

    Solution 1 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/w3orlipy Prompt:       |                    ^~~ 32729440-1e5b-4833-bd21-daf100102107.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 32729440-1e5b-4833-bd21-daf100102107.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 32729440-1e5b-4833-bd21-daf100102107.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 32729440-1e5b-4833-bd21-daf100102107.cpp:17: 32729440-1e5b-4833-bd21-daf100102107.cpp: In function ‘int main()’: 32729440-1e5b-4833-bd21-daf100102107.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 32729440-1e5b-4833-bd21-daf100102107.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,15 +1,5 @@
-vector<int> result(2, 0);
-    for (int i = 1; i <= n; ++i) {
-        string num = to_string(i);
-        string rev_num = num;
-        reverse(rev_num.begin(), rev_num.end());
-        if (num == rev_num) {
-            if (i % 2 == 0) {
-                result[0]++;
-            } else {
-                result[1]++;
-            }
-        }
-    }
-    return result;
+#include <vector>
+
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
 }
