495a41db241117da0db6df1a196c86b872703cf2
beep boop <githubbot@vadim.me>
2024-07-26 09:05:14 +0200

    Solution 85 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/tgnjn9ln Prompt:       |                                  ^~~ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:20: 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp: In function ‘int main()’: 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:30:20: error: ‘make_a_pile’ was not dec lared in this scope    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 4764f76b-50e0-4f3f-a8ea-14a1b51719e0.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <cassert>
-#include <algorithm>
 
-using namespace std;
+bool is_same(int a, int b) {
+    return a == b;
+}
 
 std::vector<int> make_a_pile(int n) {
-    std::vector<int> stones;
-    stones.push_back(n);
-    while (n % 2 == 0) {
-        n += 2;
-        stones.push_back(n);
+    std::vector<int> result;
+    for (int i = 1; i <= n; ++i) {
+        if (is_same(i, n)) {
+            result.push_back(i);
+        }
     }
-    return stones;
+    return result;
 }
 
 int solve_problem_cpp100() {
     int n;
-    cin >> n;
+    std::cin >> n;
     std::vector<int> result = make_a_pile(n);
     
     for (int i = 0; i < result.size(); ++i) {
-        cout << result[i] << " ";
+        std::cout << result[i] << " ";
     }
     
+    std::cout << "\n";
+    
+    return 0;
+}
+
+int main() {
+    solve_problem_cpp100();
     return 0;
 }
