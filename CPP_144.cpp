0295c55d6e84f8c14e4c22cd9ddc1e5902422770
beep boop <githubbot@vadim.me>
2024-07-26 09:38:14 +0200

    Solution 90 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6k8xy6a6 Prompt:       |                     ^ 24834b98-68aa-4a39-92de-c8853fd168a0.cpp:4:30: error: ‘n’ was not declared in th is scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                              ^ 24834b98-68aa-4a39-92de-c8853fd168a0.cpp:4:16: error: ‘stoi’ was not declared in  this scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                ^~~~ 24834b98-68aa-4a39-92de-c8853fd168a0.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (num1 * den2 == num2 * den1);       |     ^~~~~~ 24834b98-68aa-4a39-92de-c8853fd168a0.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 24834b98-68aa-4a39-92de-c8853fd168a0.cpp:9: 24834b98-68aa-4a39-92de-c8853fd168a0.cpp: In function ‘int main()’: 24834b98-68aa-4a39-92de-c8853fd168a0.cpp:11:13: error: ‘simplify’ was not declar ed in this scope    11 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
-#include <cassert>
 
-bool simplify(const std::string& x, const std::string& n) {
-    int x_num = std::stoi(x.substr(0, x.find('/')));
-    int x_denom = std::stoi(x.substr(x.find('/') + 1));
-    int n_num = std::stoi(n.substr(0, n.find('/')));
-    int n_denom = std::stoi(n.substr(n.find('/') + 1));
+bool simplify(std::string x, std::string n) {
+    int num1 = std::stoi(x.substr(0, x.find('/')));
+    int den1 = std::stoi(x.substr(x.find('/') + 1));
+    int num2 = std::stoi(n.substr(0, n.find('/')));
+    int den2 = std::stoi(n.substr(n.find('/') + 1));
 
-    return (x_num * n_num) % (x_denom * n_denom) == 0;
+    return (num1 * den2 == num2 * den1);
 }
 
 int main() {
-    assert(simplify("1/5", "1/5") == false);
+    std::string x = "2/4";
+    std::string n = "1/2";
+
+    std::cout << simplify(x, n) << std::endl;
 
     return 0;
 }
