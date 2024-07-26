ca3e33a03f8a6ae24bc1866daef8ebf2ce5189ce
beep boop <githubbot@vadim.me>
2024-07-26 08:29:26 +0200

    Solution 2 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/6f2ewmxp Prompt:       |                    ^~~ d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp:21: d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp: In function ‘int main()’: d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ d0cdcc8b-3303-4036-bb69-50769f78fe89.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,25 +1,7 @@
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<int> unique_digits(vector<int> x);
-
-int main() {
-    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
-    return 0;
-}
+bool issame(vector<int> a, vector<int> b);
 
 vector<int> unique_digits(vector<int> x){
     vector<int> result;
