d92d38eb5afeeb5fb343e88207eee1e1a1c809bc
beep boop <githubbot@vadim.me>
2024-07-26 08:32:20 +0200

    Solution 9 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/w3orlipy Prompt:       |             ^~~~~~ fe5e24c3-972c-4e78-8518-4810b4b8671b.cpp:23:20: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ fe5e24c3-972c-4e78-8518-4810b4b8671b.cpp:23:27: error: ‘vector’ was not declared  in this scope    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fe5e24c3-972c-4e78-8518-4810b4b8671b.cpp:23:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from fe5e24c3-972c-4e78-8518-4810b4b8671b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fe5e24c3-972c-4e78-8518-4810b4b8671b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fe5e24c3-972c-4e78-8518-4810b4b8671b.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,26 +1,21 @@
 #include <vector>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> palindrome;
-
-    for (int i = 0; i <= n; ++i) {
-        palindrome.push_back(i);
+namespace std {
+    bool issame(std::vector<int> a, std::vector<int> b){
+        return a == b;
     }
 
-    for (int i = n - 1; i >= 0; --i) {
-        palindrome.push_back(i);
+    std::vector<int> even_odd_palindrome(int n){
+        std::vector<int> palindrome;
+
+        for(int i = 0; i <= n; ++i) {
+            palindrome.push_back(i);
+        }
+
+        for(int i = n - 1; i >= 0; --i) {
+            palindrome.push_back(i);
+        }
+
+        return palindrome;
     }
-
-    return palindrome;
-}
-
-int main() {
-    assert(issame(even_odd_palindrome(1), std::vector<int>{0, 1}));
-
-    return 0;
 }
