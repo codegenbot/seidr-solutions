171027b2b53fa0f1a91eb6f6816268d27c9c20bf
beep boop <githubbot@vadim.me>
2024-07-26 09:36:09 +0200

    Solution 75 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/3ueeai5w Prompt: Complete the following code given the task description and function signature.

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,7 +1,3 @@
 double num = stod(value);
-    if (num < 0) {
-        return ceil(num - 0.5);
-    } else {
-        return floor(num + 0.5);
-    }
+    return (num >= 0) ? floor(num + 0.5) : ceil(num - 0.5);
 }
