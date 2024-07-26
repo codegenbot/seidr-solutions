622ca514c7051af318b49cdd0ae3c83c1b63bd27
beep boop <githubbot@vadim.me>
2024-07-26 08:19:40 +0200

    Solution 36 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fhw6w0do Prompt: 606eecd5-ce9a-4fed-b196-519852309560.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 606eecd5-ce9a-4fed-b196-519852309560.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~ In file included from 606eecd5-ce9a-4fed-b196-519852309560.cpp:26: 606eecd5-ce9a-4fed-b196-519852309560.cpp: In function ‘int main()’: 606eecd5-ce9a-4fed-b196-519852309560.cpp:36:20: error: ‘sort_array’ was not decl ared in this scope; did you mean ‘contest::sort_array’?    36 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 606eecd5-ce9a-4fed-b196-519852309560.cpp:10:22: note: ‘contest::sort_array’ decl ared here    10 |     std::vector<int> sort_array(const std::vector<int>& array) {       |                      ^~~~~~~~~~ In file included from 606eecd5-ce9a-4fed-b196-519852309560.cpp:26: 606eecd5-ce9a-4fed-b196-519852309560.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -10,10 +10,15 @@ namespace contest {
     std::vector<int> sort_array(const std::vector<int>& array) {
         if (array.empty()) return array;
         auto temp = array;
-        if (contest::issame(std::vector<int>{temp.front()}, std::vector<int>{temp.back()}) || (temp.front() + temp.back()) % 2 == 0)
+        if (issame(std::vector<int>{temp.front()}, std::vector<int>{temp.back()}) || (temp.front() + temp.back()) % 2 == 0)
             std::sort(temp.begin(), temp.end(), std::greater<int>());
         else
             std::sort(temp.begin(), temp.end());
         return temp;
     }
+}
+
+int main1() {
+    assert(contest::issame(contest::sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
+    return 0;
 }
