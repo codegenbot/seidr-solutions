e54e17942efb7811e07b457d37dbe5877d6a1a85
beep boop <githubbot@vadim.me>
2024-07-26 08:33:54 +0200

    Solution 94 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fhw6w0do Prompt: r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | In file included from 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp:20: 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp: In function ‘int main()’: 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp:30:20: error: ‘sort_array’ was not decl ared in this scope; did you mean ‘contest::sort_array’?    30 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp:7:22: note: ‘contest::sort_array’ decla red here     7 |     std::vector<int> sort_array(std::vector<int> arr) {       |                      ^~~~~~~~~~ In file included from 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp:20: 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp:30:13: error: ‘issame’ was not declared  in this scope; did you mean ‘contest::issame’?    30 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ^~~~~~ 73a9b3d9-1ddb-49b7-b67c-01a44a141ee9.cpp:12:10: note: ‘contest::issame’ declared  here    12 |     bool issame(std::vector<int> a, std::vector<int> b) {       |          ^~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -14,5 +14,8 @@ namespace contest {
     }
 }
 
-assert(contest::issame(contest::sort_array({21, 14, 23, 11}), contest::sort_array({11, 14, 21, 23}));
-std::cout << "Arrays are the same" << std::endl;
+int main() {
+    assert(contest::issame(contest::sort_array({21, 14, 23, 11}), contest::sort_array({11, 14, 21, 23}));
+    std::cout << "Arrays are the same" << std::endl;
+    return 0;
+}
