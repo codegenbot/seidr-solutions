d8411d8609243c6aa75b528be418d24a73cd4145
beep boop <githubbot@vadim.me>
2024-07-26 09:06:36 +0200

    Solution 47 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/heahydfc Prompt: 60c46b6e-4cc4-4ac0-ac97-21677e42a619.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 60c46b6e-4cc4-4ac0-ac97-21677e42a619.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -3,11 +3,15 @@
 
 using namespace std;
 
+bool compare(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 vector<int> tri(int a) {
-    return {a, a+2, a+4};
+    return {a, a+1, a+2};
 }
 
 int main() {
-    assert(issame(tri(1), vector<int>{1, 3, 5}));
+    assert(compare(tri(1), vector<int>{1, 3}));
     return 0;
 }
