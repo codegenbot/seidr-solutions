158b8401f15c5a6172960fc44aeb753f1dfb96d9
beep boop <githubbot@vadim.me>
2024-07-26 09:14:45 +0200

    Solution 3 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6k8xy6a6 Prompt: Complete the following code given the task description and function signature.

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,7 +1,7 @@
 int x_num = stoi(x.substr(0, x.find('/')));
-    int x_den = stoi(x.substr(x.find('/') + 1));
+    int x_denom = stoi(x.substr(x.find('/') + 1));
     int n_num = stoi(n.substr(0, n.find('/')));
-    int n_den = stoi(n.substr(n.find('/') + 1));
+    int n_denom = stoi(n.substr(n.find('/') + 1));
 
-    return (x_num * n_num) % (x_den * n_den) == 0;
+    return (x_num * n_num) % (x_denom * n_denom) == 0;
 }
