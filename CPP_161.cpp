6ef06929602bfbeee321aac280a88b1ef494db34
beep boop <githubbot@vadim.me>
2024-07-26 09:09:21 +0200

    Solution 27 of CPP/161, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vjz4wlgx Prompt:       |                 ^ 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp:1:33: error: expected unqualified-id be fore ‘++’ token     1 | for (int i = 0; i < s.length(); ++i) {       |                                 ^~ 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp:6:5: error: expected unqualified-id bef ore ‘if’     6 |     if (count_if(s.begin(), s.end(), ::isalpha) == 0) {       |     ^~ 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp:9:5: error: expected unqualified-id bef ore ‘return’     9 |     return s;       |     ^~~~~~ 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp:12: 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp: In function ‘int main()’: 6257f99d-90b3-458e-aa14-dd50da1fbf3a.cpp:14:13: error: ‘solve’ was not declared in this scope    14 |     assert (solve("#ccc") == "#CCC");       |             ^~~~~

--- CPP_161.cpp
+++ CPP_161.cpp
@@ -1,11 +1,11 @@
 string solve(string s) {
-    for (int i = 0; i < s.length(); ++i) {
-        if (isalpha(s[i])) {
-            s[i] = islower(s[i]) ? toupper(s[i]) : tolower(s[i]);
+    for (char &c : s) {
+        if (isalpha(c)) {
+            c = islower(c) ? toupper(c) : tolower(c);
         }
     }
 
-    if (count_if(s.begin(), s.end(), ::isalpha) == 0) {
+    if (none_of(s.begin(), s.end(), ::isalpha)) {
         reverse(s.begin(), s.end());
     }
 
