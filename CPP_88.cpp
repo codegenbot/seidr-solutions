d8a27e999c349c73110ca206ea791500376ade81
beep boop <githubbot@vadim.me>
2024-07-26 08:19:55 +0200

    Solution 37 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/fhw6w0do Prompt: 689ea009-3314-4dba-a500-4816077b6bf8.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 689ea009-3314-4dba-a500-4816077b6bf8.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 689ea009-3314-4dba-a500-4816077b6bf8.cpp:21: 689ea009-3314-4dba-a500-4816077b6bf8.cpp: In function ‘int main()’: 689ea009-3314-4dba-a500-4816077b6bf8.cpp:31:20: error: ‘sort_array’ was not decl ared in this scope; did you mean ‘contest::sort_array’?    31 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 689ea009-3314-4dba-a500-4816077b6bf8.cpp:10:22: note: ‘contest::sort_array’ decl ared here    10 |     std::vector<int> sort_array(const std::vector<int>& array) {       |                      ^~~~~~~~~~ In file included from 689ea009-3314-4dba-a500-4816077b6bf8.cpp:21: 689ea009-3314-4dba-a500-4816077b6bf8.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -4,13 +4,13 @@
 
 namespace contest {
     bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-        return a == b;
+        return std::equal(a.begin(), a.end(), b.begin());
     }
 
     std::vector<int> sort_array(const std::vector<int>& array) {
         if (array.empty()) return array;
         auto temp = array;
-        if (issame(std::vector<int>{temp.front()}, std::vector<int>{temp.back()}) || (temp.front() + temp.back()) % 2 == 0)
+        if (contest::issame(std::vector<int>{temp.front()}, std::vector<int>{temp.back()}) || (temp.front() + temp.back()) % 2 == 0)
             std::sort(temp.begin(), temp.end(), std::greater<int>());
         else
             std::sort(temp.begin(), temp.end());
@@ -18,7 +18,7 @@ namespace contest {
     }
 }
 
-int main1() {
+int main() {
     assert(contest::issame(contest::sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
     return 0;
 }
