bac0edf463381923999ca4e1198a25d9043ba04a
beep boop <githubbot@vadim.me>
2024-07-26 09:16:28 +0200

    Solution 10 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/6k8xy6a6 Prompt:       |                      ^ 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp:4:31: error: ‘n’ was not declared in th is scope     4 |     int n_den = stoi(n.substr(n.find("/") + 1));       |                               ^ 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp:4:17: error: ‘stoi’ was not declared in  this scope     4 |     int n_den = stoi(n.substr(n.find("/") + 1));       |                 ^~~~ 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (x_num * n_num) % (x_den * n_den) == 0;       |     ^~~~~~ 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp:9: 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp: In function ‘int main()’: 97edcbe8-8d7b-4c88-ae9f-38e966beb9bb.cpp:11:13: error: ‘simplify’ was not declar ed in this scope    11 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,7 +1,18 @@
-int x_num = stoi(x.substr(0, x.find('/')));
-    int x_denom = stoi(x.substr(x.find('/') + 1));
-    int n_num = stoi(n.substr(0, n.find('/')));
-    int n_denom = stoi(n.substr(n.find('/') + 1));
 
-    return (x_num * n_num) % (x_denom * n_denom) == 0;
+#include <iostream>
+#include <string>
+
+bool simplify(std::string x, std::string n) {
+    int x_num = std::stoi(x.substr(0, x.find("/")));
+    int x_den = std::stoi(x.substr(x.find("/") + 1));
+    int n_num = std::stoi(n.substr(0, n.find("/")));
+    int n_den = std::stoi(n.substr(n.find("/") + 1));
+
+    return (x_num * n_num) % (x_den * n_den) == 0;
+}
+
+int main() {
+    assert(simplify("1/5", "1/5") == false);
+    
+    return 0;
 }
