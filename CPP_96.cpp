84d526e590cf5d7fba10e3e141449035e60e7d6c
beep boop <githubbot@vadim.me>
2024-07-26 08:21:53 +0200

    Solution 1 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gza0btde Prompt:    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8d1cfe9d-0da3-46e1-8bca-e30788ca94a8.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8d1cfe9d-0da3-46e1-8bca-e30788ca94a8.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8d1cfe9d-0da3-46e1-8bca-e30788ca94a8.cpp:26: 8d1cfe9d-0da3-46e1-8bca-e30788ca94a8.cpp: In function ‘int main()’: 8d1cfe9d-0da3-46e1-8bca-e30788ca94a8.cpp:36:20: error: ‘count_up_to’ was not dec lared in this scope    36 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 8d1cfe9d-0da3-46e1-8bca-e30788ca94a8.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,4 +1,11 @@
-vector<int> result;
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+vector<int> count_up_to(int n) {
+    vector<int> result;
     if (n < 2) {
         return result;
     }
@@ -21,4 +28,14 @@ vector<int> result;
     }
     
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    
+    return 0;
 }
