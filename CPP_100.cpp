d64cbbca6c08d6fc1cee73ad3b12b5782c6c7a8a
beep boop <githubbot@vadim.me>
2024-07-26 08:46:19 +0200

    Solution 72 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/tgnjn9ln Prompt: d8c8fcbe-155c-47a1-92ba-cf367da23cd9.cpp:29:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    29 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ d8c8fcbe-155c-47a1-92ba-cf367da23cd9.cpp:8:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     8 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ d8c8fcbe-155c-47a1-92ba-cf367da23cd9.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ d8c8fcbe-155c-47a1-92ba-cf367da23cd9.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -14,3 +14,9 @@ std::vector<int> make_a_pile(int n) {
     }
     return stones;
 }
+
+int main() {
+    assert(issame(make_a_pile(8), vector<int>{8, 10, 12, 14, 16, 18, 20, 22}));
+    
+    return 0;
+}
