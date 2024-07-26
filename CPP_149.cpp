41642437f68b6aae83a2af83ebd6d4a9c9ebbe71
beep boop <githubbot@vadim.me>
2024-07-26 09:20:03 +0200

    Solution 60 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/b9k33c5y Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a64e5bae-2e33-4525-8da8-f5cbb2f7d816.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a64e5bae-2e33-4525-8da8-f5cbb2f7d816.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a64e5bae-2e33-4525-8da8-f5cbb2f7d816.cpp:11: a64e5bae-2e33-4525-8da8-f5cbb2f7d816.cpp: In function ‘int main()’: a64e5bae-2e33-4525-8da8-f5cbb2f7d816.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ a64e5bae-2e33-4525-8da8-f5cbb2f7d816.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,25 +1,3 @@
-#include <algorithm>
-#include <iostream>
-#include <numeric>
-#include <vector>
-
-bool issame(const std::string& a, const std::string& b) {
-    return a.length() == b.length();
-}
-
-std::vector<std::string> sorted_list_sum(const std::vector<std::string>& lst) {
-    std::vector<std::string> sorted_list = lst;
-    
-    sorted_list.erase(std::remove_if(sorted_list.begin(), sorted_list.end(), [](const std::string& s) {
-        return s.length() % 2 != 0;
-    }), sorted_list.end());
-    
-    std::sort(sorted_list.begin(), sorted_list.end(), [](const std::string& a, const std::string& b) {
-        if (issame(a, b)) {
-            return a < b;
-        }
-        return a.length() < b.length();
-    });
-
-    return sorted_list;
+bool issame(const vector<string>& a, const vector<string>& b) {
+    return a == b;
 }
