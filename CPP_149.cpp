51ce3a3d413313f751875abd643d8f8cfed7a227
beep boop <githubbot@vadim.me>
2024-07-26 09:16:23 +0200

    Solution 39 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/b9k33c5y Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:11: 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp: In function ‘int main()’: 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -7,22 +7,14 @@ bool issame(const vector<int>& a, const vector<int>& b) {
 }
 
 int sorted_list_sum(const vector<int>& lst) {
-    return accumulate(lst.begin(), lst.end(), 0);
+    int sum = 0;
+    for (int num : lst) {
+        sum += num;
+    }
+    return sum;
 }
 
 int main() {
-    vector<int> vec1 = {1, 2, 3, 4, 5};
-    vector<int> vec2 = {1, 2, 3, 4, 5};
-    
-    if (issame(vec1, vec2)) {
-        cout << "The vectors are the same." << endl;
-    } else {
-        cout << "The vectors are different." << endl;
-    }
-
-    vector<int> lst = {5, 2, 8, 1, 3};
-    sort(lst.begin(), lst.end());
-    cout << "Sum of sorted list: " << sorted_list_sum(lst) << endl;
-
+    // Main function code here
     return 0;
 }
