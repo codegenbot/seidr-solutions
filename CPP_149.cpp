11a71ce2f9a5ffd5ed6dcae64b97162191937ad4
beep boop <githubbot@vadim.me>
2024-07-26 09:17:06 +0200

    Solution 45 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/b9k33c5y Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:11: 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp: In function ‘int main()’: 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 5c29f527-a28e-4ee7-8e45-8402467ffe96.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -12,3 +12,19 @@ vector<string> sorted_list_sum(const vector<string>& lst) {
     });
     return lst;
 }
+
+int main() {
+    vector<string> list1 = {"apple", "banana", "orange"};
+    vector<string> list2 = {"kiwi", "pear", "grape"};
+    
+    vector<string> result1 = sorted_list_sum(list1);
+    vector<string> result2 = sorted_list_sum(list2);
+    
+    if (issame(result1, result2)) {
+        cout << "The sorted lists have the same elements." << endl;
+    } else {
+        cout << "The sorted lists have different elements." << endl;
+    }
+    
+    return 0;
+}
