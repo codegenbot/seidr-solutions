856c85c2ec33a98e953377eb88ba71928ad69d41
beep boop <githubbot@vadim.me>
2024-07-26 09:04:29 +0200

    Solution 33 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/heahydfc Prompt: 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp:25:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp:24: 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp: In function ‘int main()’: 2f1e9b09-0665-43d7-9cd2-565584850dd0.cpp:34:20: error: ‘tri’ was not declared in  this scope    34 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -3,18 +3,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
     assert(issame(vector<int>{1}, vector<int>{1}));
     assert(issame(vector<int>{1, 3}, vector<int>{1, 3}));
