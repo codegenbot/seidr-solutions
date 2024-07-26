2e58e0ee3d19764edce902e34930c1b134c13e8e
beep boop <githubbot@vadim.me>
2024-07-26 08:51:53 +0200

    Solution 3 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/kepsn9x8 Prompt: 314c73d2-8c61-47b2-ba3e-a4b6ca807ed3.cpp:55:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    55 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 314c73d2-8c61-47b2-ba3e-a4b6ca807ed3.cpp:8:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     8 | bool issame(vector<string> a, vector<string> b){       |      ^~~~~~ 314c73d2-8c61-47b2-ba3e-a4b6ca807ed3.cpp:63:5: error: redefinition of ‘int main( )’    63 | int main(){       |     ^~~~ 314c73d2-8c61-47b2-ba3e-a4b6ca807ed3.cpp:48:5: note: ‘int main()’ previously def ined here    48 | int main(){       |     ^~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,5 +1,9 @@
-#include <string>
+#include <iostream>
 #include <vector>
+#include <string>
+#include <cassert>
+
+using namespace std;
 
 bool issame(vector<string> a, vector<string> b){
     if(a.size() != b.size()){
@@ -39,4 +43,10 @@ vector<string> split_words(string txt){
         result.push_back(to_string(count));
     }
     return result;
+}
+
+int main(){
+    assert(issame(split_words(""), {"0"}));
+    
+    return 0;
 }
