8809427cc4add0143af30e41c13104c22e8cac5f
beep boop <githubbot@vadim.me>
2024-07-27 08:09:48 +0200

    Solution 92 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/y8nwpmwp Prompt: 80207381-59fe-4d0e-bdc4-ecf28bab73a7.cpp:22:30: error: expected unqualified-id b efore ‘++’ token    22 | for (int i = 0; i < cutSpot; ++i) {       |                              ^~ 80207381-59fe-4d0e-bdc4-ecf28bab73a7.cpp:25:6: error: ‘cout’ in namespace ‘std’ does not name a type    25 | std::cout << std::endl;       |      ^~~~ 80207381-59fe-4d0e-bdc4-ecf28bab73a7.cpp:25:1: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’?    25 | std::cout << std::endl;       | ^~~ 80207381-59fe-4d0e-bdc4-ecf28bab73a7.cpp:26:1: error: expected unqualified-id be fore ‘for’    26 | for (int i = cutSpot; i < n; ++i) {       | ^~~ 80207381-59fe-4d0e-bdc4-ecf28bab73a7.cpp:26:23: error: ‘i’ does not name a type    26 | for (int i = cutSpot; i < n; ++i) {       |                       ^ 80207381-59fe-4d0e-bdc4-ecf28bab73a7.cpp:26:30: error: expected unqualified-id b efore ‘++’ token    26 | for (int i = cutSpot; i < n; ++i) {       |                              ^~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,2 +1,9 @@
-5
-1 2 3 4 5
+#include <iostream>
+
+for (int i = 0; i < cutSpot; ++i) {
+    std::cout << nums[i] << " ";
+}
+std::cout << std::endl;
+for (int i = cutSpot; i < n; ++i) {
+    std::cout << nums[i] << " ";
+}
