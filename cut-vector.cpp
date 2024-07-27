9aa948e79473dc19c38fe5f1373044fd2fbadbc5
beep boop <githubbot@vadim.me>
2024-07-27 02:14:54 +0200

    Solution 50 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/w32lba64 Prompt:       |         ^~~~ 20ecb017-2e74-4ef0-ad74-844e8c0e4292.cpp:19:17: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    19 |         cout << nums[i] << endl;       |                 ^~~~       |                 num 20ecb017-2e74-4ef0-ad74-844e8c0e4292.cpp:19:28: error: ‘endl’ was not declared i n this scope    19 |         cout << nums[i] << endl;       |                            ^~~~ 20ecb017-2e74-4ef0-ad74-844e8c0e4292.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << 0 << endl;       |     ^~~~ 20ecb017-2e74-4ef0-ad74-844e8c0e4292.cpp:21:18: error: ‘endl’ was not declared i n this scope    21 |     cout << 0 << endl;       |                  ^~~~ 20ecb017-2e74-4ef0-ad74-844e8c0e4292.cpp:22:33: error: ‘nums’ was not declared i n this scope; did you mean ‘num’?    22 |     for (int i = cut_index; i < nums.size(); ++i) {       |                                 ^~~~       |                                 num

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,46 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <numeric>
-#include <limits>
+#include <climits>
 
 using namespace std;
 
 int main() {
     vector<int> nums;
     int num;
-    
     while (cin >> num) {
         nums.push_back(num);
     }
     
-    int total_sum = accumulate(nums.begin(), nums.end(), 0);
-    int left_sum = 0;
-    int right_sum = total_sum;
-    int min_diff = numeric_limits<int>::max();
+    int min_diff = INT_MAX;
     int cut_index = 0;
-    
-    for (int i = 0; i < nums.size(); ++i) {
-        left_sum += nums[i];
-        right_sum -= nums[i];
-        
-        int current_diff = abs(left_sum - right_sum);
-        if (current_diff < min_diff) {
-            min_diff = current_diff;
+    for (int i = 1; i < nums.size(); ++i) {
+        int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - accumulate(nums.begin() + i, nums.end(), 0));
+        if (diff < min_diff) {
+            min_diff = diff;
             cut_index = i;
         }
     }
     
-    for (int i = 0; i <= cut_index; ++i) {
+    for (int i = 0; i < cut_index; ++i) {
         cout << nums[i] << endl;
     }
-    
     cout << 0 << endl;
-    
-    for (int i = cut_index + 1; i < nums.size(); ++i) {
+    for (int i = cut_index; i < nums.size(); ++i) {
         cout << nums[i] << endl;
     }
-    
     cout << 0 << endl;
     
     return 0;
