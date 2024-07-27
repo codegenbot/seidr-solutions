999ce88ae93c3012f63ba6ae03ab0e4b1e71f50a
beep boop <githubbot@vadim.me>
2024-07-27 06:37:28 +0200

    Solution 70 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/w32lba64 Prompt:       |            ^~~ 425e4d5d-7d4e-4c2a-89a9-d539f9d67bea.cpp:5:12: error: ‘cin’ was not declared in this scope     5 |     while (cin >> num) {       |            ^~~ 425e4d5d-7d4e-4c2a-89a9-d539f9d67bea.cpp:6:9: error: ‘nums’ was not declared in this scope; did you mean ‘num’?     6 |         nums.push_back(num);       |         ^~~~       |         num 425e4d5d-7d4e-4c2a-89a9-d539f9d67bea.cpp:9:13: error: ‘nums’ was not declared in  this scope; did you mean ‘num’?     9 |     int n = nums.size();       |             ^~~~       |             num 425e4d5d-7d4e-4c2a-89a9-d539f9d67bea.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << "1" << endl;       |     ^~~~ 425e4d5d-7d4e-4c2a-89a9-d539f9d67bea.cpp:23:20: error: ‘endl’ was not declared i n this scope    23 |     cout << "1" << endl;       |                    ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <numeric>
 
 using namespace std;
 
 int main() {
     vector<int> nums;
     int num;
+    
     while (cin >> num) {
         nums.push_back(num);
     }
     
-    int min_diff = INT_MAX;
-    int cut_index = 0;
-    for (int i = 1; i < nums.size(); ++i) {
-        int diff = abs(accumulate(nums.begin(), nums.begin() + i, 0) - accumulate(nums.begin() + i, nums.end(), 0));
-        if (diff < min_diff) {
-            min_diff = diff;
-            cut_index = i;
+    int n = nums.size();
+    int left_sum = 0, right_sum = 0;
+    int left_idx = 0, right_idx = n - 1;
+    
+    while (left_idx < right_idx) {
+        if (left_sum < right_sum) {
+            left_sum += nums[left_idx];
+            ++left_idx;
+        } else {
+            right_sum += nums[right_idx];
+            --right_idx;
         }
     }
     
-    for (int i = 0; i < cut_index; ++i) {
+    cout << "1" << endl;
+    
+    for (int i = 0; i < left_idx; ++i) {
         cout << nums[i] << endl;
     }
-    cout << 0 << endl;
-    for (int i = cut_index; i < nums.size(); ++i) {
-        cout << nums[i] << endl;
-    }
-    cout << 0 << endl;
+    
+    cout << "0" << endl;
     
     return 0;
 }
