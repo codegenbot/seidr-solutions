590c5d184656e2ca8f4a80f6ec6e19879b6e8081
beep boop <githubbot@vadim.me>
2024-07-30 13:09:26 +0200

    Solution 40 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic6n61ds Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7383fd2-b03c-4e65-8cb4-f3ca03504 68c.py", line 29, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c7383fd2-b03c-4e65-8cb4-f3ca03504 68c.py", line 26, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -1,21 +1,17 @@
 def separate_paren_groups(s):
     result = []
     current_group = ""
-    open_count = 0
 
     for char in s:
         if char == "(":
-            if open_count == 0:
-                if current_group:
-                    result.append(current_group)
+            if current_group.count('(') + 1 == current_group.count(')'):
+                result.append(current_group)
                 current_group = "("
             else:
                 current_group += "("
-            open_count += 1
         elif char == ")":
             current_group += ")"
-            open_count -= 1
-            if open_count == 0:
+            if current_group.count('(') == current_group.count(')'):
                 result.append(current_group)
                 current_group = ""
         else:
