94ac8b8ebc01b542fa029231bb6b19351581f3ec
beep boop <githubbot@vadim.me>
2024-07-30 15:45:46 +0200

    Solution 92 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gljjzkkt Prompt: 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5117b911-5024-4c5e-911c-b4e1c5985 f09.py", line 20, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5117b911-5024-4c5e-911c-b4e1c5985 f09.py", line 17, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^ TypeError: fizz_buzz() takes 0 positional arguments but 1 was given

--- Python_36.py
+++ Python_36.py
@@ -1,5 +1,5 @@
-def fizz_buzz():
-    for i in range(1, 101):
+def fizz_buzz(n):
+    for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
@@ -9,4 +9,9 @@ def fizz_buzz():
         else:
             print(i)
 
-fizz_buzz()
+result = ["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+          "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz",
+          "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29", "FizzBuzz"]
+print(*result, sep='\n')
+
+fizz_buzz(100)
