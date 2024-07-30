5a7628076e78e698374b5ba7fa4767b35fcea0df
beep boop <githubbot@vadim.me>
2024-07-30 12:59:36 +0200

    Solution 46 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gljjzkkt Prompt: 99989 FizzBuzz 99991 99992 Fizz 99994 Buzz Fizz 99997 99998 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5981c1a-1b70-4954-bc9d-cbc68b68d 6ff.py", line 20, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5981c1a-1b70-4954-bc9d-cbc68b68d 6ff.py", line 17, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,18 +1,8 @@
-from typing import List
-
-def fizz_buzz(n: int) -> List[str]:
-    result = []
+def fizz_buzz(n: int) -> int:
     count = 0
     
     for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
-            result.append("FizzBuzz")
             count += 1
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
     
-    return result, count
+    return count
