917869a65d3a959e73f3d026bc6373b30d08098b
beep boop <githubbot@vadim.me>
2024-07-30 15:46:34 +0200

    Solution 94 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gljjzkkt Prompt: 89 FizzBuzz 91 92 Fizz 94 Buzz Fizz 97 98 Fizz Buzz Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5de1b4b-e0f1-464d-83fc-dd3678ef9 066.py", line 17, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d5de1b4b-e0f1-464d-83fc-dd3678ef9 066.py", line 14, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^ TypeError: fizz_buzz() takes 0 positional arguments but 1 was given

--- Python_36.py
+++ Python_36.py
@@ -1,5 +1,5 @@
-def fizz_buzz(limit=100):
-    for i in range(1, limit + 1):
+def fizz_buzz(n):
+    for i in range(1, n+1):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
         elif i % 3 == 0:
