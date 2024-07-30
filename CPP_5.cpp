71545d4734fe1ab055027ffbd6051384a9e4e46f
beep boop <githubbot@vadim.me>
2024-07-30 12:12:26 +0200

    Solution 9 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/490be1o5 Prompt:       |                    ^~~ e0991143-16fa-466c-bef6-2e69475f81e0.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e0991143-16fa-466c-bef6-2e69475f81e0.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e0991143-16fa-466c-bef6-2e69475f81e0.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e0991143-16fa-466c-bef6-2e69475f81e0.cpp:12: e0991143-16fa-466c-bef6-2e69475f81e0.cpp: In function ‘int main()’: e0991143-16fa-466c-bef6-2e69475f81e0.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ e0991143-16fa-466c-bef6-2e69475f81e0.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -7,7 +7,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
 
 std::vector<int> intersperse(std::vector<int> numbers, int delimiter){
     std::vector<int> result;
-    for (int i = 0; i < numbers.size(); ++i) {
+    for (size_t i = 0; i < numbers.size(); ++i) {
         result.push_back(numbers[i]);
         if (i != numbers.size() - 1) {
             result.push_back(delimiter);
@@ -17,5 +17,5 @@ std::vector<int> intersperse(std::vector<int> numbers, int delimiter){
 }
 
 int main() {
-    assert (issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
+    assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
 }
