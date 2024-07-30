be577f0a1f4100dd92f76c7b3e979120fe05ec7d
beep boop <githubbot@vadim.me>
2024-07-30 12:14:12 +0200

    Solution 22 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/490be1o5 Prompt: /usr/include/assert.h:92: note: macro "assert" defined here    92 | #  define assert(expr) \       | 8b43c4be-2dd9-4ebf-aedc-c554c47b0439.cpp: In function ‘int main()’: 8b43c4be-2dd9-4ebf-aedc-c554c47b0439.cpp:19:5: error: ‘assert’ was not declared in this scope    19 |     assert(intersperse({2, 2, 2}, 2) == vector<int>{2, 2, 2, 2, 2});       |     ^~~~~~ 8b43c4be-2dd9-4ebf-aedc-c554c47b0439.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <cassert>   +++ |+#include <cassert>     4 | 8b43c4be-2dd9-4ebf-aedc-c554c47b0439.cpp: At global scope: 8b43c4be-2dd9-4ebf-aedc-c554c47b0439.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 8b43c4be-2dd9-4ebf-aedc-c554c47b0439.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -15,8 +15,4 @@ vector<int> intersperse(vector<int> numbers, int delimiter){
     return result;
 }
 
-int main(){
-    assert(intersperse({2, 2, 2}, 2) == vector<int>{2, 2, 2, 2, 2});
-    
-    return 0;
-}
+assert(intersperse({2, 2, 2}, 2) == vector<int>{2, 2, 2, 2, 2});
