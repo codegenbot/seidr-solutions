570c60a679709d56fdae0806011612203757aa1e
beep boop <githubbot@vadim.me>
2024-07-30 12:11:57 +0200

    Solution 4 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/490be1o5 Prompt:       |                    ^~~ 8be2be7f-7920-4c9c-a043-0187854aa045.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8be2be7f-7920-4c9c-a043-0187854aa045.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8be2be7f-7920-4c9c-a043-0187854aa045.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8be2be7f-7920-4c9c-a043-0187854aa045.cpp:12: 8be2be7f-7920-4c9c-a043-0187854aa045.cpp: In function ‘int main()’: 8be2be7f-7920-4c9c-a043-0187854aa045.cpp:22:20: error: ‘intersperse’ was not dec lared in this scope    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |                    ^~~~~~~~~~~ 8be2be7f-7920-4c9c-a043-0187854aa045.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -4,27 +4,19 @@
 
 using namespace std;
 
-vector<int> intersperse(vector<int> numbers, int delimiter){
+vector<int> intersperse(vector<int> numbers, int delimiter){ 
     vector<int> result;
-    for(int i = 0; i < numbers.size(); ++i){
+    for(int i = 0; i < numbers.size(); ++i){ 
         result.push_back(numbers[i]);
-        if(i < numbers.size() - 1){
-            result.push_back(delimiter);
-        }
-    }
-    return result;
+        if(i < numbers.size() - 1){ 
+            result.push_back(delimiter); 
+        } 
+    } 
+    return result; 
 }
 
 bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i=0; i<a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 int main(){
