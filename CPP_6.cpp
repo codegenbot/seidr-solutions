982149f25ebd7136b2dc357a517acdb24214aae6
beep boop <githubbot@vadim.me>
2024-07-30 12:19:30 +0200

    Solution 68 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/bfw34z6f Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp:7: 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp: In function ‘int main()’: 665c8933-cd62-4ad9-b37e-efb367d709e4.cpp:17:20: error: ‘parse_nested_parens’ was  not declared in this scope    17 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,11 +1,5 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
-
-std::string parse_nested_parens(std::string s);
-
-```
