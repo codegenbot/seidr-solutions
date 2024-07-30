bd4a54d54784174b055e0cf9924543921eacb7e9
beep boop <githubbot@vadim.me>
2024-07-30 12:21:52 +0200

    Solution 70 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/tmaimv02 Prompt:    34 | assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), { "()", "(()) ", "(()())" }));       | ^~~~~~ 84dea042-5980-4761-93ec-3de631c55f20.cpp:34:1: error: expected ‘)’ before ‘stati c_cast’    34 | assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), { "()", "(()) ", "(()())" }));       | ^~~~~~ 84dea042-5980-4761-93ec-3de631c55f20.cpp:34:86: error: expected unqualified-id b efore ‘)’ token    34 | arate_paren_groups("( ) (( )) (( )( ))"), { "()", "(())", "(()())" }));       |                                                                     ^ 84dea042-5980-4761-93ec-3de631c55f20.cpp:37:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    37 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 84dea042-5980-4761-93ec-3de631c55f20.cpp:8:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     8 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -5,14 +5,6 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     stringstream ss(paren_string);
@@ -31,4 +23,8 @@ vector<string> separate_paren_groups(string paren_string) {
     return result;
 }
 
-assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), vector<string>({ "()", "(())", "(()())" }));
+int main() {
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), { "()", "(())", "(()())" }));
+    
+    return 0;
+}
