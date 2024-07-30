260897f0bbc4bf157fefef4ad0a33833e2a2cafb
beep boop <githubbot@vadim.me>
2024-07-30 13:07:30 +0200

    Solution 30 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic6n61ds Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73ee2407-79ce-46bd-80f8-21beba29a 084.py", line 23, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73ee2407-79ce-46bd-80f8-21beba29a 084.py", line 20, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -3,18 +3,15 @@ def separate_paren_groups(s):
     current_group = ""
 
     for char in s:
-        if char == "(":
-            if current_group:
-                result.append(current_group)
-            current_group = "("
-        elif char == ")":
-            current_group += ")"
+        if char == "(" and current_group != "":
             result.append(current_group)
             current_group = ""
-        else:
-            current_group += char
+        current_group += char
+        if char == ")" and current_group != "":
+            result.append(current_group)
+            current_group = ""
 
-    if current_group:
+    if current_group != "":
         result.append(current_group)
 
     return result
