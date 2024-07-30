b7e49b5ea112c1883b8ee2713a532534503e2569
beep boop <githubbot@vadim.me>
2024-07-30 12:53:22 +0200

    Solution 41 of Python/36, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gljjzkkt Prompt: 53322 53323 53324 53325 53326 53327 53328 53329 53330 53331 53332 53333 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4c1d7da-6ca7-4b70-a1aa-05a0e917b 563.py", line 21, in <module>     check(fizz_buzz)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f4c1d7da-6ca7-4b70-a1aa-05a0e917b 563.py", line 18, in check     assert fizz_buzz(100000) == 8026            ^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_36.py
+++ Python_36.py
@@ -1,3 +1,15 @@
 def fizz_buzz(n: int) -> int:
-    count = sum(1 for i in range(1, n+1) if i % 3 == 0 and i % 5 == 0)
+    count = 0
+    
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+            count += 1
+    
     return count
