c468ba80bc06be48e2529532becf78fec0b9e9a0
beep boop <githubbot@vadim.me>
2024-07-30 12:20:26 +0200

    Solution 60 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vsuhmjgj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9d0a0ec4-8890-4399-8477-25cb035a6 590.py", line 12     s[(3 * i): min((3 * i + 3), len(s)] for i in range((len(s) + 2) // 3)                                       ^ SyntaxError: closing parenthesis ']' does not match opening parenthesis '('

--- Python_38.py
+++ Python_38.py
@@ -1,22 +1,7 @@
-def encode_cyclic(s: str):
-    groups = [s[(3 * i): min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
-    groups = [(group[1:] + group[0]) if len(group) == 3 else group for group in groups]
-    return "".join(groups)
-
 def decode_cyclic(s: str):
     return "".join(
         [
             (group[-1] + group[:-1]) if len(group) == 3 else group
-            for group in [
-                s[(3 * i): min((3 * i + 3), len(s)) for i in range((len(s) + 2) // 3)]
-            ]
-        )
-
-if __name__ == "__main__":
-    s = input("Enter string to encode:").strip()
-    encoded_output = encode_cyclic(s)
-    print(encoded_output)
-
-    s = input("Enter string to decode:").strip()
-    decoded_output = decode_cyclic(s)
-    print(decoded_output)
+            for group in [s[(3 * i): min((3 * i + 3), len(s)) for i in range((len(s) + 2) // 3)]
+        ]
+    )
