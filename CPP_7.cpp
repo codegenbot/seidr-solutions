b308e455cc0012b78e45409310c5aaa5a52d8fab
beep boop <githubbot@vadim.me>
2024-07-30 12:12:40 +0200

    Solution 8 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mru5g34f Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1c7c217f-37eb-459c-bc5f-3db2c7a912f5.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1c7c217f-37eb-459c-bc5f-3db2c7a912f5.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1c7c217f-37eb-459c-bc5f-3db2c7a912f5.cpp:11: 1c7c217f-37eb-459c-bc5f-3db2c7a912f5.cpp: In function ‘int main()’: 1c7c217f-37eb-459c-bc5f-3db2c7a912f5.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 1c7c217f-37eb-459c-bc5f-3db2c7a912f5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -4,29 +4,10 @@
 #include <cassert>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<std::string> filter_by_substring(std::vector<std::string> strings, std::string substring){
-    std::vector<std::string> result;
-    for(const auto &str : strings){
-        if(str.find(substring) != std::string::npos){
-            result.push_back(str);
-        }
-    }
-    return result;
+    return a == b;
 }
 
 int main(){
     assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
-
     return 0;
 }
