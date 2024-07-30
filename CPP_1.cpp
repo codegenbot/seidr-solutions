9e554f9650bf5b82db4428cb2884fc457154f642
beep boop <githubbot@vadim.me>
2024-07-30 12:14:31 +0200

    Solution 18 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/tmaimv02 Prompt:       |                                     ^~~~~~ 41ce8c95-063a-463b-8a91-f896d0a7af9e.cpp:38:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 41ce8c95-063a-463b-8a91-f896d0a7af9e.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 41ce8c95-063a-463b-8a91-f896d0a7af9e.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 41ce8c95-063a-463b-8a91-f896d0a7af9e.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 41ce8c95-063a-463b-8a91-f896d0a7af9e.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 41ce8c95-063a-463b-8a91-f896d0a7af9e.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -30,6 +30,6 @@ std::vector<std::string> separate_paren_groups(std::string paren_string) {
 }
 
 int main() {
-    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), std::vector<std::string>{"()", "(())", "(()())"}));
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), { "()", "(())", "(()())" }));
     return 0;
 }
