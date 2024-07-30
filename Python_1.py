941c4c14517ae1126a1a234ebde1ad82382715ae
beep boop <githubbot@vadim.me>
2024-07-30 13:05:03 +0200

    Solution 19 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic6n61ds Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea0474fe-30f8-47d5-8ed7-a9af45db0 26a.py", line 24, in <module>     check(separate_paren_groups)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ea0474fe-30f8-47d5-8ed7-a9af45db0 26a.py", line 21, in check     assert separate_paren_groups("( ) (( )) (( )( ))") == ["()", "(())", "(()()) "]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ ^^ AssertionError

--- Python_1.py
+++ Python_1.py
@@ -5,15 +5,16 @@ def separate_paren_groups(s):
     
     for char in s:
         if char == '(':
+            if count > 0:
+                result.append(current_group)
+                current_group = ""
             count += 1
         elif char == ')':
             count -= 1
         
         current_group += char
         
-        if count == 0:
-            result.append(current_group)
-            current_group = ""
-            count = 0
-    
+    if current_group:
+        result.append(current_group)
+        
     return result
