468c02c3bad213233561cff2efaf97a3b98d6301
beep boop <githubbot@vadim.me>
2024-07-30 12:19:20 +0200

    Solution 52 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/tmaimv02 Prompt:       |                                     ^~~~~~ c17a8144-eeaa-4a8b-89ac-95a42f3f0a3c.cpp:38:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from c17a8144-eeaa-4a8b-89ac-95a42f3f0a3c.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from c17a8144-eeaa-4a8b-89ac-95a42f3f0a3c.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ c17a8144-eeaa-4a8b-89ac-95a42f3f0a3c.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c17a8144-eeaa-4a8b-89ac-95a42f3f0a3c.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ c17a8144-eeaa-4a8b-89ac-95a42f3f0a3c.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b){
     if (a.size() != b.size()) return false;
     for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) return false;
@@ -11,17 +11,17 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return true;
 }
 
-std::vector<std::string> separate_paren_groups(std::string paren_string) {
+std::vector<std::string> separate_paren_groups(std::string paren_string){
     std::vector<std::string> result;
     std::stringstream ss(paren_string);
     std::string temp;
-    while (getline(ss, temp, '(')) {
+    while(getline(ss, temp, '(')) {
         if (temp.empty()) continue;
         temp = "(" + temp;
         size_t pos = temp.find(')');
-        if (pos != std::string::npos) {
+        if (pos != std::string::npos){
             result.push_back(temp.substr(0, pos + 1));
-            if (pos + 1 < temp.size()) {
+            if (pos + 1 < temp.size()){
                 result.push_back(temp.substr(pos + 1));
             }
         }
@@ -29,4 +29,7 @@ std::vector<std::string> separate_paren_groups(std::string paren_string) {
     return result;
 }
 
-assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), { "()", "(())", "(()())" }));
+int main(){
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
+    return 0;
+}
