2dbcf95b922dde54b05228cf6826040d559f33a7
beep boop <githubbot@vadim.me>
2024-07-31 07:33:37 +0200

    Solution 42 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/twfl8r4i Prompt:                  from 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp:13: 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp: In function ‘int main()’: 5162ebb0-73b9-42a0-987f-e60aa0f5da8a.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,11 +1,5 @@
 #include <vector>
-#include <algorithm>
 #include <cassert>
-#include <climits>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
 
 std::vector<int> rolling_max(std::vector<int> numbers) {
     std::vector<int> result;
@@ -15,4 +9,12 @@ std::vector<int> rolling_max(std::vector<int> numbers) {
         result.push_back(max_num);
     }
     return result;
+}
+
+bool is_same(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(is_same(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
 }
