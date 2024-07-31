6e7eafa9392812375c6568487ee0eafd669ea3be
beep boop <githubbot@vadim.me>
2024-07-31 18:05:10 +0200

    Solution 30 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/stkq92uv Prompt: d6202474-7cf2-436c-971e-2352c213fe07.cpp:38:5: note: ‘int main()’ previously def ined here    38 | int main() {       |     ^~~~ In file included from d6202474-7cf2-436c-971e-2352c213fe07.cpp:43: d6202474-7cf2-436c-971e-2352c213fe07.cpp: In function ‘int main()’: d6202474-7cf2-436c-971e-2352c213fe07.cpp:53:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    53 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~ d6202474-7cf2-436c-971e-2352c213fe07.cpp:9:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     9 | bool issame(const vector<string>& a, const vector<string>& b){       |      ^~~~~~ d6202474-7cf2-436c-971e-2352c213fe07.cpp:44:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    44 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -6,10 +6,6 @@
 
 using namespace std;
 
-bool issame(const vector<string>& a, const vector<string>& b){
-    return a == b;
-}
-
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     string current_group;
@@ -36,6 +32,6 @@ vector<string> separate_paren_groups(string paren_string) {
 }
 
 int main() {
-    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), vector<string>{"()", "(())", "(()())"}));
     return 0;
 }
