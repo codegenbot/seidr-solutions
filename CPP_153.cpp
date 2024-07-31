45f0233cf0e9c80225711a7c69566806824cb533
beep boop <githubbot@vadim.me>
2024-07-31 17:50:31 +0200

    Solution 80 of CPP/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xklmbquw Prompt:     1 | int max_strength = INT_MIN; fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp:2:5: error: ‘string’ does not name a ty pe     2 |     string strongest_extension;       |     ^~~~~~ fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp:3:5: error: expected unqualified-id bef ore ‘for’     3 |     for (const auto& extension : extensions) {       |     ^~~ fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp:15:5: error: expected unqualified-id be fore ‘return’    15 |     return class_name + "." + strongest_extension;       |     ^~~~~~ fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp:16:1: error: expected declaration befor e ‘}’ token    16 | }       | ^ In file included from fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp:18: fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp: In function ‘int main()’: fcf161b9-4d89-4a6d-95e4-08a39cdf7e69.cpp:20:13: error: ‘Strongest_Extension’ was  not declared in this scope    20 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,22 +1,25 @@
-string Strongest_Extension(string class_name, vector<string> extensions){
+#include <string>
+#include <vector>
+#include <cctype>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+
+std::string Strongest_Extension(const std::string& class_name, const std::vector<std::string>& extensions) {
     int max_strength = INT_MIN;
-    string strongest_extension = "";
-    
-    for(auto extension : extensions){
+    string strongest_extension;
+    for (const auto& extension : extensions) {
         int CAP = 0, SM = 0;
-        for(auto ch : extension){
-            if(isupper(ch))
-                CAP++;
-            else if(islower(ch))
-                SM++;
+        for (char c : extension) {
+            if (isupper(c)) CAP++;
+            if (islower(c)) SM++;
         }
-        
         int strength = CAP - SM;
-        if(strength > max_strength || (strength == max_strength && extension < strongest_extension)){
+        if (strength > max_strength || (strength == max_strength && extension < strongest_extension)) {
             max_strength = strength;
             strongest_extension = extension;
         }
     }
-    
     return class_name + "." + strongest_extension;
 }
