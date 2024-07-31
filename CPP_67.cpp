dba7db34e62e8eca24b29c3ff6424ca62dc0b7ec
beep boop <githubbot@vadim.me>
2024-07-31 15:04:46 +0200

    Solution 10 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0j81itq0 Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,5 +1,4 @@
-int fruit_distribution(string s, int n){
-    int apples, oranges;
+int apples = 0, oranges = 0;
     sscanf(s.c_str(), "%d apples and %d oranges", &apples, &oranges);
     return n - apples - oranges;
 }
