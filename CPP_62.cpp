ec45330298f5caeb49b0f5c8a446ee3f7e034139
beep boop <githubbot@vadim.me>
2024-07-31 12:43:03 +0200

    Solution 8 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7j2wj684 Prompt:       |                    ^~~~~ 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp:10:29: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp:10:36: error: expected primary-expressi on before ‘float’    10 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp:10:43: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp:9: 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp: In function ‘int main()’: 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp:19:20: error: ‘derivative’ was not decl ared in this scope    19 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~ 04a7cee0-2b24-42be-8cc8-3b4ac6d53296.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(derivative({1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,26 +1,10 @@
 #include <vector>
 #include <cassert>
 
-vector<float> derivative(vector<float> xs);
-
 vector<float> derivative(vector<float> xs){
     vector<float> result;
     for(int i=1; i<xs.size(); i++){
-        result.push_back(xs[i] * i);
+        result.push_back(xs[i] - xs[i-1]);
     }
     return result;
-}
-
-bool issame(vector<float> a, vector<float> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-    
-    for(int i=0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    
-    return true;
 }
