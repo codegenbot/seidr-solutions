1508f7605e82eaf7f090ff35f064bf250e31fe77
beep boop <githubbot@vadim.me>
2024-07-31 12:12:14 +0200

    Solution 49 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mv1rwjoo Prompt:    31 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 87cae0bc-fc1f-4082-98d8-3ad8c1d9fdbb.cpp:31:29: error: ‘vector’ was not declared  in this scope    31 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 87cae0bc-fc1f-4082-98d8-3ad8c1d9fdbb.cpp:31:36: error: expected primary-expressi on before ‘float’    31 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 87cae0bc-fc1f-4082-98d8-3ad8c1d9fdbb.cpp:30: 87cae0bc-fc1f-4082-98d8-3ad8c1d9fdbb.cpp: In function ‘int main()’: 87cae0bc-fc1f-4082-98d8-3ad8c1d9fdbb.cpp:40:20: error: ‘sort_even’ was not decla red in this scope    40 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 87cae0bc-fc1f-4082-98d8-3ad8c1d9fdbb.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -30,3 +30,16 @@ void sort_even(vector<float>& l) {
     
     l = result;
 }
+
+int main()
+{
+    vector<float> vec1 = {4.5, 2.3, 1.2, 3.7, 5.2, 6.1};
+    vector<float> vec2 = {3.2, 2.8, 1.4, 4.5, 7.1, 6.9};
+    
+    sort_even(vec1);
+    sort_even(vec2);
+    
+    bool result = issame(vec1, vec2);
+    
+    return 0;
+}
