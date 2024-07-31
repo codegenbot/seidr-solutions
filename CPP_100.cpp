afa5f04516b0bbb5772c35a50b67d564f2f7f10b
beep boop <githubbot@vadim.me>
2024-07-31 16:10:43 +0200

    Solution 64 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/q95xtk3y Prompt:       |                    ^~~ b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp:15: b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp: In function ‘int main()’: b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp:25:20: error: ‘make_a_pile’ was not dec lared in this scope    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ b5f6ef33-5307-4ccc-a841-e11c5f9fb03f.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,26 +1,15 @@
+#include <cassert>
 #include <vector>
 
-vector<int> make_a_pile(int n);
-
-bool issame(vector<int> a, vector<int> b){
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
 }
 
-vector<int> make_a_pile(int n){
-    vector<int> result;
+std::vector<int> make_a_pile(int n){
+    std::vector<int> result;
     result.push_back(n);
-    for(int i = 0; i < n-1; i++){
-        if (n % 2 == 0) {
+    for(int i=1; i<n; i++){
+        if(n%2 == 0){
             n += 2;
         } else {
             n += 1;
@@ -28,4 +17,9 @@ vector<int> make_a_pile(int n){
         result.push_back(n);
     }
     return result;
+}
+
+int main() {
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    return 0;
 }
