e3b338a7a54b1f2bb02b2860c07cfada929bb5f2
beep boop <githubbot@vadim.me>
2024-07-31 13:19:57 +0200

    Solution 4 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jyynd4th Prompt: c0725c5f-0025-407e-b9d8-5d24a175d740.cpp:3:14: error: ‘vector’ was not declared in this scope     3 | int solution(vector<int> lst) {       |              ^~~~~~ c0725c5f-0025-407e-b9d8-5d24a175d740.cpp:3:14: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from c0725c5f-0025-407e-b9d8-5d24a175d740.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c0725c5f-0025-407e-b9d8-5d24a175d740.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c0725c5f-0025-407e-b9d8-5d24a175d740.cpp:3:21: error: expected primary-expressio n before ‘int’     3 | int solution(vector<int> lst) {       |                     ^~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,6 +1,4 @@
-#include <vector>
-
-int solution(vector<int> lst) {
+int solution(std::vector<int> lst) {
     int sum = 0;
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
