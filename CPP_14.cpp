c893e088e17dd5c9c26f0971159c99fd168e6536
beep boop <githubbot@vadim.me>
2024-07-31 11:28:58 +0200

    Solution 30 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/db1u7vge Prompt:       |                              ^~~~~~ 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp:10:37: error: ‘string’ was not declared  in this scope    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp:10:45: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp:9: 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp: In function ‘int main()’: 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp:19:20: error: ‘all_prefixes’ was not de clared in this scope    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 998171b5-7feb-473d-93c9-8bae524b7e6f.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -15,6 +15,6 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b){
 }
 
 int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    assert(issame(all_prefixes("WWW"), std::vector<std::string>{"W", "WW", "WWW"}));
     return 0;
 }
