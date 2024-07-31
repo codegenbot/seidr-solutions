706218558b2b1fc863e17f1aebc1dfad44f69028
beep boop <githubbot@vadim.me>
2024-07-31 13:23:11 +0200

    Solution 13 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jyynd4th Prompt: :allocator<_CharT> >::allocator_type&, int) [with _Tp = int; std::allocator_trai ts<std::allocator<_CharT> >::pointer = int*; std::allocator_traits<std::allocato r<_CharT> >::allocator_type = std::allocator<int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = int; _Alloc = std::allocator<int>; std::_Vector_base<_Tp, _Alloc>::pointe r = int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {int}; _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _A lloc>::iterator = std::vector<int>::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {int}; _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::ref erence = int&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = int ; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc>::value_type = int]’ b47c3d72-a896-4a14-b765-6a44e0844536.cpp:14:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -6,10 +7,12 @@ int main() {
     int sum = 0;
     vector<int> lst;
     cout << "Enter numbers (space-separated): ";
-    char c; cin >> c; while(c != '\n') {c=getchar();}
+    char c;
+    for (; cin >> c && c != '\n';) {}
+    cin.ignore(); // add this line here
     while (cin >> c) {
+        if (c == ' ') break; 
         lst.push_back(atoi(&c));
-        c = ' ';
     }
     for (int i = 1; i < lst.size(); i++) {
         if (lst[i] % 2 != 0) {
