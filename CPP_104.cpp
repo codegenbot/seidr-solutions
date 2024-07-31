b0edf050f5238f065723dae7624cfa690d821400
beep boop <githubbot@vadim.me>
2024-07-31 13:32:27 +0200

    Solution 4 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9jmpqyhl Prompt:       |                    ^~~ 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp:21: 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp: In function ‘int main()’: 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 942d1061-0217-4e9a-8a8c-ad686739edf2.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,20 +1,13 @@
+#include <vector>
+
+bool issame(vector<int> a, vector<int> b){
+    // Function implementation here
+}
+
 vector<int> unique_digits(vector<int> x){
-    vector<int> result;
-    for (int num : x) {
-        int temp = num;
-        bool hasEvenDigit = false;
-        while (temp > 0) {
-            int digit = temp % 10;
-            if (digit % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        if (!hasEvenDigit) {
-            result.push_back(num);
-        }
-    }
-    sort(result.begin(), result.end());
-    return result;
+    // Function implementation here
+}
+
+int main() {
+    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
 }
