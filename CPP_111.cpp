821afa740709cff94abf796c9be388fe781b22e0
beep boop <githubbot@vadim.me>
2024-07-31 09:55:13 +0200

    Solution 96 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/q43pw8l8 Prompt: 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ In file included from 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp:41: 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp: In function ‘int main()’: 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp:56:19: error: call of overloaded ‘issam e(std::map<char, int>, <brace-enclosed initializer list>)’ is ambiguous    56 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp:10:6: note: candidate: ‘bool issame(con st std::map<char, int>&, const std::map<char, int>&)’    10 | bool issame(const map<char, int>& a, const map<char, int>& b) {       |      ^~~~~~ 794d0a4d-3b0e-4b68-a93a-2e8f61f979ae.cpp:42:6: note: candidate: ‘bool issame(std ::map<char, int>, std::map<char, int>)’    42 | bool issame(map<char,int> a,map<char,int> b){       |      ^~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -7,15 +7,12 @@
 
 using namespace std;
 
+bool issame(const map<char, int>&, const map<char, int>&);
+
 map<char, int> histogram(string test);
 
 bool issame(const map<char, int>& a, const map<char, int>& b) {
     return a == b;
-}
-
-int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
-    return 0;
 }
 
 map<char, int> histogram(string test) {
