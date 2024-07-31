91b405da09773a5a666fc0be1afc3ff8f9d1ee7b
beep boop <githubbot@vadim.me>
2024-07-31 10:58:18 +0200

    Solution 3 of CPP/48, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/viv1zjs6 Prompt: 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp:3:20: error: ‘string’ was not declared in this scope     3 | bool is_palindrome(string text){       |                    ^~~~~~ 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp:3:20: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp:17: 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp: In function ‘int main()’: 65832fbf-4994-45bd-9c11-5db8da2c8107.cpp:19:26: error: ‘is_palindrome’ cannot be  used as a function    19 |     assert (is_palindrome("xywzx") == false);       |             ~~~~~~~~~~~~~^~~~~~~~~

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -1,6 +1,3 @@
-#include <string>
-#include <cassert>
-
 bool is_palindrome(std::string text){
     int start = 0;
     int end = text.length() - 1;
@@ -13,3 +10,4 @@ bool is_palindrome(std::string text){
     }
 
     return true;
+}
