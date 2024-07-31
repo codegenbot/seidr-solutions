5ec51fd9408789e4423ef3e7217401296fdf5c83
beep boop <githubbot@vadim.me>
2024-07-31 12:30:22 +0200

    Solution 77 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/gkmh7eou Prompt: ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = long unsigned int; std:: allocator_traits<std::allocator<_CharT> >::pointer = long unsigned int*; std::al locator_traits<std::allocator<_CharT> >::allocator_type = std::allocator<long un signed int>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = long unsigned int; _Alloc = std::allocator<long unsigned int>; std::_Vect or_base<_Tp, _Alloc>::pointer = long unsigned int*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const long unsigned int&}; _Tp = long unsigned int; _Alloc = std:: allocator<long unsigned int>; std::vector<_Tp, _Alloc>::iterator = std::vector<l ong unsigned int>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = long unsigned int; _Allo c = std::allocator<long unsigned int>; std::vector<_Tp, _Alloc>::value_type = lo ng unsigned int]’ 22bc8d0d-3aa3-4b27-8cf7-ca2d7204530d.cpp:31:22:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<long unsigned int>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -6,8 +6,11 @@ int add(const std::vector<size_t>&);
 
 int main() {
     size_t n;
+
+    std::cout << "Enter the number of elements: ";
     while (!(std::cin >> n)) { 
         std::cerr << "Invalid input. Try again.\n";
+        std::cout << "Enter the number of elements: ";
         (std::cin >> std::ws).clear();
         if ((std::cin).eof()) {
             return 1; 
@@ -16,9 +19,10 @@ int main() {
 
     std::vector<size_t> lst;
     for(size_t i = 0; i < n; i++){
-        size_t num;
+        long unsigned int num;
         while (!(std::cin >> num)) { 
             std::cerr << "Invalid input. Try again.\n";
+            std::cout << "Enter element " << i+1 << ": ";
             (std::cin >> std::ws).clear();
             if ((std::cin).eof()) {
                 return 1; 
