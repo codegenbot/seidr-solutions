4c3380a6584d8ab9f4263d1b7f8ad7682f19b8a2
beep boop <githubbot@vadim.me>
2024-07-31 10:19:10 +0200

    Solution 10 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic89b93p Prompt: 7a01317b-0955-477b-b568-ddc65de74d78.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,36 +1,18 @@
-#include <boost/any.hpp>
-#include <cassert>
+#include <iostream>
 #include <string>
-using namespace std;
 
-boost::any compare_one(boost::any a, boost::any b);
-
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        if (boost::any_cast<int>(a) > boost::any_cast<int>(b)) {
-            return a;
-        } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)) {
-            return b;
-        } else {
-            return "None";
-        }
-    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        if (boost::any_cast<float>(a) > boost::any_cast<float>(b)) {
-            return a;
-        } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
-            return b;
-        } else {
-            return "None";
-        }
-    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        if (stof(boost::any_cast<std::string>(a)) > stof(boost::any_cast<std::string>(b))) {
-            return a;
-        } else if (stof(boost::any_cast<std::string>(a)) < stof(boost::any_cast<std::string>(b))) {
-            return b;
-        } else {
-            return "None";
-        }
-    } else {
-        return "None";
+template <typename T>
+T compare_one(const T& a, const T& b) {
+    if (a > b) {
+        return a;
+    } else if (a < b) {
+        return b;
     }
+    return T();
+}
+
+int main() {
+    assert (compare_one(std::string("1"), std::to_string(1)).empty());
+    
+    return 0;
 }
