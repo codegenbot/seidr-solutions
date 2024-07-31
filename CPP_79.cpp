b94007b32fbcfd8b789122fb7f6e6064ecf02447
beep boop <githubbot@vadim.me>
2024-07-31 11:53:08 +0200

    Solution 10 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/40vatql0 Prompt: fcae598f-b7e4-4275-8231-2b984c473a6f.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ fcae598f-b7e4-4275-8231-2b984c473a6f.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,5 +1,6 @@
-```
+```cpp
 #include <string>
+#include <cassert>
 using namespace std;
 
 string decimal_to_binary(int decimal){
@@ -15,6 +16,7 @@ string decimal_to_binary(int decimal){
 }
 
 int main(){
-    assert (decimal_to_binary(15) == "db1111db");
+    string result = decimal_to_binary(15);
+    assert (result == "db1111db");
     return 0;
 }
