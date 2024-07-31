c2c907b5c30cdc21a9e620927f2440177f5253ac
beep boop <githubbot@vadim.me>
2024-07-31 19:57:49 +0200

    Solution 25 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/zklk1ci3 Prompt:       |                    ^~~ 908b28c0-f646-445b-93cf-08b94faed2ce.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 908b28c0-f646-445b-93cf-08b94faed2ce.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 908b28c0-f646-445b-93cf-08b94faed2ce.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 908b28c0-f646-445b-93cf-08b94faed2ce.cpp:15: 908b28c0-f646-445b-93cf-08b94faed2ce.cpp: In function ‘int main()’: 908b28c0-f646-445b-93cf-08b94faed2ce.cpp:25:20: error: ‘tri’ was not declared in  this scope    25 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 908b28c0-f646-445b-93cf-08b94faed2ce.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -12,6 +12,16 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-int main() {
-    // Rest of the main function
+std::vector<int> tri(int n){
+    std::vector<int> tribonacci(n + 1);
+    tribonacci[0] = 3;
+    if (n > 0) tribonacci[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        if (i % 2 == 0) {
+            tribonacci[i] = 1 + i / 2;
+        } else {
+            tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i + 1];
+        }
+    }
+    return tribonacci;
 }
