ad8d8d0e3ee30ec1e60a0d9221e5c705322b50ad
beep boop <githubbot@vadim.me>
2024-07-31 10:14:08 +0200

    Solution 86 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/bj9qgde7 Prompt:       |           ^~~~~~ In file included from d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp:23: d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp: In function ‘int main()’: d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp:33:20: error: ‘get_odd_collatz’ was not  declared in this scope    33 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ d7e7f13d-a175-4f92-b29d-1bde8e7a8b19.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -6,6 +6,13 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
+std::vector<int> get_odd_collatz(int n);
+
+int main() {
+    assert(issame(get_odd_collatz(1), std::vector<int>{1}) == true);
+    return 0;
+}
+
 std::vector<int> get_odd_collatz(int n) {
     std::vector<int> result;
     while (n != 1) {
@@ -18,9 +25,4 @@ std::vector<int> get_odd_collatz(int n) {
     }
     result.push_back(1);
     return result;
-}
-
-int main() {
-    assert(issame(get_odd_collatz(1), std::vector<int>{1}));
-    return 0;
 }
