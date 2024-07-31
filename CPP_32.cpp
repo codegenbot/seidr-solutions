1e36a6440b962bda55d4d9e2212c70781c2819ed
beep boop <githubbot@vadim.me>
2024-07-31 10:39:08 +0200

    Solution 86 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3iy35mq Prompt: In file included from /usr/include/c++/11/vector:67,                  from c4170249-05bc-49b5-be28-2f81ee91d401.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from c4170249-05bc-49b5-be28-2f81ee91d401.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c4170249-05bc-49b5-be28-2f81ee91d401.cpp:48:16: error: expected primary-expressi on before ‘double’    48 |         vector<double> coeffs = {};       |                ^~~~~~ c4170249-05bc-49b5-be28-2f81ee91d401.cpp:53:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    53 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff c4170249-05bc-49b5-be28-2f81ee91d401.cpp:56:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    56 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -25,7 +25,7 @@ double find_zero(const std::vector<double>& polyCoeffs) {
 int main() {
     int n;
     std::cin >> n;
-    std::vector<double> polyCoeffs(n);  
+    std::vector<double> polyCoeffs(n, 0.0);  
     for (int i = 0; i < n; i++) {
         double val;
         std::cin >> val;
