7b5e65a9669f75037a34044227dc16cd3d1be694
beep boop <githubbot@vadim.me>
2024-07-31 19:07:41 +0200

    Solution 25 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/hp1oe9tv Prompt:       |     ^~~~ In file included from 2509866a-1db4-4dd7-8262-115444af9901.cpp:23: 2509866a-1db4-4dd7-8262-115444af9901.cpp: In function ‘int main()’: 2509866a-1db4-4dd7-8262-115444af9901.cpp:25:13: error: ‘boost’ has not been decl ared    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 2509866a-1db4-4dd7-8262-115444af9901.cpp:25:29: error: ‘string’ was not declared  in this scope    25 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 2509866a-1db4-4dd7-8262-115444af9901.cpp:25:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 2509866a-1db4-4dd7-8262-115444af9901.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 2509866a-1db4-4dd7-8262-115444af9901.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -17,5 +17,6 @@ std::any compare_one(std::any a, std::any b) {
 int main() {
     assert(std::any_cast<float>(compare_one(1.5f, 2.3f)).type() == typeid(float));
     assert(std::any_cast<float>(compare_one(std::to_string(1.5f), std::to_string(2.3f))).type() == typeid(float));
+    assert(std::any_cast<std::string>(compare_one(std::to_string(1), 1)).compare("None") == 0);
     return 0;
 }
