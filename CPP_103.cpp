5aa67e7791798b58e06cc6a522a1bc43d16af959
beep boop <githubbot@vadim.me>
2024-07-31 16:18:28 +0200

    Solution 61 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/i5s6klbr Prompt:       |                                        ^ 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp:10:15: error: ‘round’ was not declared in this scope    10 |     int avg = round((double)sum / (m - n + 1));       |               ^~~~~ 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp:11:5: error: ‘string’ does not name a t ype    11 |     string binary_avg = bitset<32>(avg).to_string();       |     ^~~~~~ 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return binary_avg.substr(binary_avg.find('1'));       |     ^~~~~~ 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp:15: 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp: In function ‘int main()’: 3b29eb98-50f9-422b-b1c5-258a8bd3a776.cpp:17:13: error: ‘rounded_avg’ was not dec lared in this scope    17 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -1,21 +1,5 @@
 #include <cmath>
 #include <bitset>
 
-int calculateAverage(int n, int m) {
-    if (n > m) {
-        return "-1";
-    }
-
-    int sum = 0;
-    for (int i = n; i <= m; i++) {
-        sum += i;
-    }
-
-    int avg = static_cast<int>(round((double)sum / (m - n + 1)));
-    return std::to_string(avg);
-}
-
-int main() {
-    assert(calculateAverage(1, 5) == "3");
-    return 0;
-}
+int avg = static_cast<int>(round((double)sum / (m - n + 1)));
+return std::to_string(avg);
