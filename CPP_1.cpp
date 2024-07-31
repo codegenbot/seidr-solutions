ae92e6dbce5be2c1b1258ddd92fa3220e814510f
beep boop <githubbot@vadim.me>
2024-07-31 18:01:09 +0200

    Solution 14 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/stkq92uv Prompt:       |                                     ^~~~~~ a54a14f7-c24a-49a2-a89c-ff8b9fc8e1d7.cpp:41:37: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a54a14f7-c24a-49a2-a89c-ff8b9fc8e1d7.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a54a14f7-c24a-49a2-a89c-ff8b9fc8e1d7.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ a54a14f7-c24a-49a2-a89c-ff8b9fc8e1d7.cpp:41:44: error: ‘b’ was not declared in t his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -3,40 +3,36 @@
 #include <string>
 #include <cassert>
 
-namespace std {
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
+    return a == b;
+}
 
-    bool issame(const vector<string>& a, const vector<string>& b){
-        return a == b;
-    }
+std::vector<std::string> separate_paren_groups(std::string paren_string) {
+    std::vector<std::string> result;
+    std::string current_group;
+    int open_braces = 0;
 
-    vector<string> separate_paren_groups(string paren_string) {
-        vector<string> result;
-        string current_group;
-        int open_braces = 0;
-
-        for (char c : paren_string) {
-            if (c == '(') {
-                if (open_braces > 0) {
-                    current_group += c;
-                }
-                open_braces++;
-            } else if (c == ')') {
-                open_braces--;
-                if (open_braces > 0) {
-                    current_group += c;
-                } else if (open_braces == 0) {
-                    result.push_back(current_group);
-                    current_group = "";
-                }
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (open_braces > 0) {
+                current_group += c;
+            }
+            open_braces++;
+        } else if (c == ')') {
+            open_braces--;
+            if (open_braces > 0) {
+                current_group += c;
+            } else if (open_braces == 0) {
+                result.push_back(current_group);
+                current_group = "";
             }
         }
-
-        return result;
     }
 
+    return result;
 }
 
 int main() {
-    assert(std::issame(std::separate_paren_groups("( ) (( )) (( )( ))"), std::vector<std::string>{"()", "(())", "(()())"}));
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), std::vector<std::string>{"()", "(())", "(()())"}));
     return 0;
 }
