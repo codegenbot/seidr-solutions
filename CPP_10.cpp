b5a1c30da0fe55ec15da929e6e4bf91602e7f636
beep boop <githubbot@vadim.me>
2024-07-31 11:18:35 +0200

    Solution 6 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4bdknmn6 Prompt: 70260c75-baca-42f2-8505-f79e930fcc9d.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string make_palindrome(string str){       | ^~~~~~ In file included from 70260c75-baca-42f2-8505-f79e930fcc9d.cpp:14: 70260c75-baca-42f2-8505-f79e930fcc9d.cpp: In function ‘int main()’: 70260c75-baca-42f2-8505-f79e930fcc9d.cpp:16:13: error: ‘make_palindrome’ was not  declared in this scope    16 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,9 +1,8 @@
 #include <string>
 
 bool is_palindrome(string str){
-    int n = str.size();
-    for(int i=0; i<n/2; ++i){
-        if(str[i] != str[n-1-i]) {
+    for(int i=0; i<str.size()/2; ++i){
+        if(str[i] != str[str.size()-1-i]){
             return false;
         }
     }
@@ -14,7 +13,7 @@ string make_palindrome(string str){
     if(str.empty()) return "";
     int n = str.size();
     for(int i=n-1; i>=0; --i){
-        if(is_palindrome(str.substr(i))) {
+        if(is_palindrome(str.substr(i))){
             string prefix = str.substr(0, i);
             reverse(prefix.begin(), prefix.end());
             return str + prefix;
