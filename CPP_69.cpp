69875315365830a2167b464b23a68f9044eebf0d
beep boop <githubbot@vadim.me>
2024-07-31 08:49:34 +0200

    Solution 2 of CPP/69, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lk0y68ds Prompt: 7a3f6590-c46a-4e2b-bb18-d4bd27f3f337.cpp:1:12: error: ‘vector’ was not declared in this scope     1 | int search(vector<int> lst) {       |            ^~~~~~ 7a3f6590-c46a-4e2b-bb18-d4bd27f3f337.cpp:1:19: error: expected primary-expressio n before ‘int’     1 | int search(vector<int> lst) {       |                   ^~~ In file included from 7a3f6590-c46a-4e2b-bb18-d4bd27f3f337.cpp:17: 7a3f6590-c46a-4e2b-bb18-d4bd27f3f337.cpp: In function ‘int main()’: 7a3f6590-c46a-4e2b-bb18-d4bd27f3f337.cpp:19:19: error: ‘search’ cannot be used a s a function    19 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,15 +1,22 @@
-int search(vector<int> lst) {
-        int res = -1;
-        for (int i = 1; i <= lst.size(); ++i) {
-            int count = 0;
-            for (int num : lst) {
-                if (num == i) {
-                    count++;
-                }
-            }
-            if (count >= i && i > res) {
-                res = i;
+#include <vector>
+
+int search(std::vector<int> lst);
+
+int search(std::vector<int> lst) {
+    int res = -1;
+    for (int i = 1; i <= lst.size(); ++i) {
+        int count = 0;
+        for (int num : lst) {
+            if (num == i) {
+                count++;
             }
         }
-        return res;
+        if (count >= i && i > res) {
+            res = i;
+        }
     }
+    return res;
+} 
+
+int main_solution() {
+}
