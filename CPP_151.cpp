f45b6bb7f02071598a6867feb1612da445787678
beep boop <githubbot@vadim.me>
2024-07-31 10:34:04 +0200

    Solution 1 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/8sjma19o Prompt: Complete the following code given the task description and function signature.

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,6 +1,6 @@
 long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
+        if (num > 0 && num == (int)num && ((int)num) % 2 != 0) {
             sum += (long long)pow(num, 2);
         }
     }
