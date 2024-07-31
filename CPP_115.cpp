b9c4182daa359bc85cafd45867faa17225f7b2b1
beep boop <githubbot@vadim.me>
2024-07-31 15:32:11 +0200

    Solution 37 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z3erxm97 Prompt:       |              ^~~~~~ 02e64a1d-1747-4c46-939f-420634fceaa7.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                     ^~~~~~ 02e64a1d-1747-4c46-939f-420634fceaa7.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                            ^~~ 02e64a1d-1747-4c46-939f-420634fceaa7.cpp:1:40: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                        ^~~ 02e64a1d-1747-4c46-939f-420634fceaa7.cpp:1:52: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                                    ^ In file included from 02e64a1d-1747-4c46-939f-420634fceaa7.cpp:12: 02e64a1d-1747-4c46-939f-420634fceaa7.cpp: In function ‘int main()’: 02e64a1d-1747-4c46-939f-420634fceaa7.cpp:14:21: error: ‘max_fill’ cannot be used  as a function    14 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -1,5 +1,5 @@
 #include <vector>
-#include <numeric>
+#include <algorithm>
 
 int max_fill(std::vector<std::vector<int>> grid, int capacity) {
     int n = grid.size();
@@ -8,6 +8,6 @@ int max_fill(std::vector<std::vector<int>> grid, int capacity) {
         total_water += std::accumulate(grid[i].begin(), grid[i].end(), 0);
     }
     
-    int num_buckets = std::ceil((double)total_water / capacity);
+    int num_buckets = ceil((double)total_water / capacity);
     return num_buckets;
 }
