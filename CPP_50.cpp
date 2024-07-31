a2ec5f7cbf3ed5db3bc14f31eb8911b761a065cc
beep boop <githubbot@vadim.me>
2024-07-31 10:59:51 +0200

    Solution 3 of CPP/50, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w5rz9ear Prompt:  noexcept, const int&, const wchar_t [4], long double&)’  6846 |     return __gnu_cxx::__to_xstring<wstring>(&std::vswprintf, __n,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~  6847 |                                             L"%Lf", __val);       |                                             ~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/basic_string.h:6608,                  from /usr/include/c++/11/string:55,                  from 9e6d2d06-97f5-4f06-87d8-be9fb6120865.cpp:3: /usr/include/c++/11/ext/string_conversions.h:99:5: note: candidate: ‘template<cl ass _String, class _CharT> _String __gnu_cxx::__to_xstring(int (*)(_CharT*, std: :size_t, const _CharT*, __va_list_tag*), std::size_t, const _CharT*, ...)’    99 |     __to_xstring(int (*__convf) (_CharT*, std::size_t, const _CharT*,       |     ^~~~~~~~~~~~ /usr/include/c++/11/ext/string_conversions.h:99:5: note:   template argument ded uction/substitution failed: In file included from /usr/include/c++/11/string:55,                  from 9e6d2d06-97f5-4f06-87d8-be9fb6120865.cpp:3: /usr/include/c++/11/bits/basic_string.h:6846:44: note:   mismatched types ‘std:: size_t’ {aka ‘long unsigned int’} and ‘int’  6846 |     return __gnu_cxx::__to_xstring<wstring>(&std::vswprintf, __n,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~  6847 |                                             L"%Lf", __val);       |                                             ~~~~~~~~~~~~~~

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include<string>
 using namespace std;
 
@@ -6,7 +5,7 @@ string encode_shift(string s) {
     string out;
     int i;
     for (i=0;i<s.length();i++) {
-        int w=((int)s[i]+5-(int)'a')%26+(int)'a';   
+        int w = (((int)s[i]) - (int)'a' + 5) % 26 + (int)'a';
         out=out+(char)w;
     }
     return out;
@@ -16,7 +15,7 @@ string decode_shift(string s){
     string out;
     int i;
     for (i=0;i<s.length();i++) {
-        int w(((int)s[i])-(int)'a'+5)%26+(int)'a';
+        int w = (((int)s[i]) - (int)'a' + 5) % 26 + (int)'a';
         out=out+(char)w;
     }
     return out;
