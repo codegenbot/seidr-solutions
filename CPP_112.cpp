dbdc7343694d19c08ea3be300d9ebfd795fd3dbe
beep boop <githubbot@vadim.me>
2024-07-31 16:31:28 +0200

    Solution 28 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/p5eryh4s Prompt:       |                              ^~~~~~ c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp:13:37: error: ‘string’ was not declared  in this scope    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp:12: c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp: In function ‘int main()’: c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp:22:20: error: ‘reverse_delete’ was not declared in this scope    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ c0b2f5fd-b880-4e64-95c8-818d7b2afa96.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -6,27 +6,22 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
 
 std::pair<std::string, std::string> reverse_delete(std::string s, std::string c) {
     std::string result = "";
-    
     for (char ch : s) {
         if (c.find(ch) == std::string::npos) {
             result += ch;
         }
     }
-    
     std::string reverse_result = result;
     std::reverse(reverse_result.begin(), reverse_result.end());
-    
     return {result, result == reverse_result ? "True" : "False"};
 }
