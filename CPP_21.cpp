c29765a7520dc5a1652381aedd7cf69ab6d63e5d
beep boop <githubbot@vadim.me>
2024-07-31 11:41:07 +0200

    Solution 22 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pbiwwlnd Prompt:    15 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 5261de68-9338-445a-8c4b-4432d73da3b0.cpp:15:36: error: expected primary-expressi on before ‘float’    15 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 5261de68-9338-445a-8c4b-4432d73da3b0.cpp:15:43: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 5261de68-9338-445a-8c4b-4432d73da3b0.cpp:14: 5261de68-9338-445a-8c4b-4432d73da3b0.cpp: In function ‘int main()’: 5261de68-9338-445a-8c4b-4432d73da3b0.cpp:24:20: error: ‘rescale_to_unit’ was not  declared in this scope    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |                    ^~~~~~~~~~~~~~~ 5261de68-9338-445a-8c4b-4432d73da3b0.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}) ,{0.2 5, 0.0, 1.0, 0.5, 0.75}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -1,11 +1,13 @@
 bool issame(vector<float> a, vector<float> b) {
-    float min_num_a = *min_element(a.begin(), a.end());
-    float max_num_a = *max_element(a.begin(), a.end());
-    float range_a = max_num_a - min_num_a;
-
-    float min_num_b = *min_element(b.begin(), b.end());
-    float max_num_b = *max_element(b.begin(), b.end());
-    float range_b = max_num_b - min_num_b;
-
-    return min_num_a == min_num_b && max_num_a == max_num_b && range_a == range_b;
+    float min_num = *min_element(a.begin(), a.end());
+    float max_num = *max_element(a.begin(), a.end());
+    float range = max_num - min_num;
+    
+    vector<float> result;
+    for (float num : a) {
+        float scaled_num = (num - min_num) / range;
+        result.push_back(scaled_num);
+    }
+    
+    return result;
 }
