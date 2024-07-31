151f166ebcd37e68685ab1501823b63e33799478
beep boop <githubbot@vadim.me>
2024-07-31 13:05:55 +0200

    Solution 11 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3jcrapl Prompt:       |           ^~~~~~ d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main() {       |     ^~~~ In file included from d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp:39: d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp: In function ‘int main()’: d2e03b83-eb51-430c-b2ff-0f53d58d9af5.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <algorithm>
 
-bool checkResult(vector<string> a, vector<string> b){
+bool isSimilar(vector<string> a, vector<string> b){
     if(a.size() != b.size()) return false;
     for(int i=0; i<a.size();i++){
         if(a[i] != b[i]) return false;
@@ -28,15 +28,10 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
     result.push_back(temp);
     std::string rev = temp;
     std::reverse(rev.begin(), rev.end());
-    if (temp == rev) {
-        result.push_back("True");
-    } else {
-        result.push_back("False");
-    }
-    return {result[0], (temp == rev)}; 
+    return {temp, (temp == rev)}; 
 }
 
 int main() {
-    assert(checkResult(reverse_delete("mamma", "mia"), {"", "True"}));
+    assert(isSimilar(reverse_delete("mamma", "mia"), vector<string>{ "", "True" }));
     return 0;
 }
