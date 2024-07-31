122e0550dc99968b14b859ca455dc08d840f804d
beep boop <githubbot@vadim.me>
2024-07-31 19:13:50 +0200

    Solution 13 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jwjhex66 Prompt: /usr/include/assert.h:92: note: macro "assert" defined here    92 | #  define assert(expr) \       | 51a37ec9-e9e0-4722-be4d-349ad13ea691.cpp: In function ‘int main()’: 51a37ec9-e9e0-4722-be4d-349ad13ea691.cpp:18:5: error: ‘assert’ was not declared in this scope    18 |     assert(result == std::vector<int>{2, 0, 0, 1});       |     ^~~~~~ 51a37ec9-e9e0-4722-be4d-349ad13ea691.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cmath>   +++ |+#include <cassert>     5 | 51a37ec9-e9e0-4722-be4d-349ad13ea691.cpp: At global scope: 51a37ec9-e9e0-4722-be4d-349ad13ea691.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 51a37ec9-e9e0-4722-be4d-349ad13ea691.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -7,14 +7,15 @@ using namespace std;
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
-    for (int i = 0; i < game.size(); ++i)
+    for (int i = 0; i < game.size(); ++i) {
         result.push_back(abs(game[i] - guess[i]));
+    }
     return result;
 }
 
 int main() {
     vector<int> result = compare({1, 2, 3, 5}, {-1, 2, 3, 4});
-    assert(result == vector<int>{2, 0, 0, 1});
+    assert(result == std::vector<int>{2, 0, 0, 1});
 
     return 0;
 }
