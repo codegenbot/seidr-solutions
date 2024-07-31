eeed56a20b97901280bdc7e746611d23a4738051
beep boop <githubbot@vadim.me>
2024-07-31 10:59:25 +0200

    Solution 1 of CPP/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w5rz9ear Prompt: 0de06e76-9a68-4b0b-9185-1323c7bb914c.cpp: In function ‘std::string decode_shift( std::string)’: 0de06e76-9a68-4b0b-9185-1323c7bb914c.cpp:19:38: error: expected ‘,’ or ‘;’ befor e ‘%’ token    19 |         int w(((int)s[i])-(int)'a'+5)%26+(int)'a';       |                                      ^

--- CPP_50.cpp
+++ CPP_50.cpp
@@ -1,3 +1,4 @@
+```c++
 #include<stdio.h>
 #include<string>
 using namespace std;
@@ -6,7 +7,7 @@ string encode_shift(string s) {
     string out;
     int i;
     for (i=0;i<s.length();i++) {
-        int w=((int)s[i]+5-(int)'a')%26+(int)'a';   
+        int w = (((int)s[i]) - (int)'a' + 5) % 26 + (int)'a';
         out=out+(char)w;
     }
     return out;
@@ -16,7 +17,7 @@ string decode_shift(string s){
     string out;
     int i;
     for (i=0;i<s.length();i++) {
-        int w(((int)s[i])-(int)'a'+5)%26+(int)'a';
+        int w = (((int)s[i]) - (int)'a' + 5) % 26 + (int)'a';
         out=out+(char)w;
     }
     return out;
