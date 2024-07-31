3248ec6f08063b1f4d484d54c15c3eecccde3514
beep boop <githubbot@vadim.me>
2024-07-31 09:40:01 +0200

    Solution 76 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gd9vdsvs Prompt:       |                           ^~~~~~ ab70df58-57d1-42f7-96d1-ce14d2e9c7fb.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ab70df58-57d1-42f7-96d1-ce14d2e9c7fb.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ab70df58-57d1-42f7-96d1-ce14d2e9c7fb.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ab70df58-57d1-42f7-96d1-ce14d2e9c7fb.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ab70df58-57d1-42f7-96d1-ce14d2e9c7fb.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ ab70df58-57d1-42f7-96d1-ce14d2e9c7fb.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -4,7 +4,6 @@
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) return false;
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
     return a == b;
@@ -13,16 +12,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 std::vector<int> unique_digits(std::vector<int> x) {
     std::vector<int> result;
     for (int num : x) {
-        bool hasEvenDigit = false;
-        int temp = num;
-        while (temp > 0) {
-            if (temp % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        if (!hasEvenDigit) {
+        if (std::to_string(num).find_first_of("2468") == std::string::npos) {
             result.push_back(num);
         }
     }
@@ -31,6 +21,6 @@ std::vector<int> unique_digits(std::vector<int> x) {
 }
 
 int main() {
-    assert(issame(unique_digits({135, 103, 31}), std::vector<int>{31, 135}));
+    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
     return 0;
 }
