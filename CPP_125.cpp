41bf089b5b6696774fd1556f4266d72e2c12b82d
beep boop <githubbot@vadim.me>
2024-07-31 14:17:13 +0200

    Solution 23 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6qw5jxbx Prompt:       |                              ^~~~~~ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:29: d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp: In function ‘int main()’: d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -2,15 +2,12 @@
 #include <cassert>
 
 vector<string> split_words(string txt);
+bool issame(vector<string> a, vector<string> b);
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+bool issame(vector<string> a, vector<string> b){
+    if(a.size() != b.size()) return false;
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
