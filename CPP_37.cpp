1b1606bb27b9cc4374b2fc4fce4cd8c2d21772cd
beep boop <githubbot@vadim.me>
2024-07-31 12:06:00 +0200

    Solution 6 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mv1rwjoo Prompt:    24 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 22f0c479-383a-410b-b237-b9fdb8e5fb7a.cpp:24:36: error: expected primary-expressi on before ‘float’    24 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 22f0c479-383a-410b-b237-b9fdb8e5fb7a.cpp:24:43: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 22f0c479-383a-410b-b237-b9fdb8e5fb7a.cpp:23: 22f0c479-383a-410b-b237-b9fdb8e5fb7a.cpp: In function ‘int main()’: 22f0c479-383a-410b-b237-b9fdb8e5fb7a.cpp:33:20: error: ‘sort_even’ was not decla red in this scope    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 22f0c479-383a-410b-b237-b9fdb8e5fb7a.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -6,17 +6,17 @@ bool issame(vector<float> a, vector<float> b) {
 
 void sort_even(vector<float>& l) {
     vector<float> even_elems;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
+    for(int i = 0; i < l.size(); i++) {
+        if(i % 2 == 0) {
             even_elems.push_back(l[i]);
         }
     }
     sort(even_elems.begin(), even_elems.end());
-
+    
     vector<float> result;
     int j = 0;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
+    for(int i = 0; i < l.size(); i++) {
+        if(i % 2 == 0) {
             result.push_back(even_elems[j]);
             j++;
         } else {
