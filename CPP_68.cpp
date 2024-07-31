66c9f5934de1e796e5ef562c0e8af5f776565111
beep boop <githubbot@vadim.me>
2024-07-31 12:51:14 +0200

    Solution 14 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pzdn32ib Prompt:       |                    ^~~ 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp:21: 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp: In function ‘int main()’: 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 9741a20b-cbc2-47fe-afeb-5a4c11ce7ee8.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -4,14 +4,13 @@
 bool issame(std::vector<int> a, std::vector<int> b);
 std::vector<int> pluck(std::vector<int> arr);
 
-
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
+    if(a.size() != b.size()) {
         return false;
     }
     
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
             return false;
         }
     }
@@ -24,14 +23,14 @@ std::vector<int> pluck(std::vector<int> arr) {
     int smallest_even = -1;
     int smallest_index = -1;
     
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] % 2 == 0 && (smallest_even == -1 || arr[i] < smallest_even)) {
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] % 2 == 0 && (smallest_even == -1 || arr[i] < smallest_even)) {
             smallest_even = arr[i];
             smallest_index = i;
         }
     }
     
-    if (smallest_even != -1) {
+    if(smallest_even != -1) {
         result.push_back(smallest_even);
         result.push_back(smallest_index);
     }
