e32618860dbb6cf431897fcf62a756d638f65a6c
beep boop <githubbot@vadim.me>
2024-07-31 10:01:52 +0200

    Solution 30 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xqvp2n7z Prompt:       |           ^~~~~~ In file included from 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp:14: 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp: In function ‘int main()’: 7edb6da0-2d75-4d6a-beb8-e07c28346f7f.cpp:24:20: error: ‘maximum’ was not declare d in this scope    24 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -6,11 +6,8 @@ bool issame(int a, int b) {
     return a == b;
 }
 
-bool maximum(const std::vector<int>& vec, int val) {
-    return (*std::max_element(vec.begin(), vec.end()) == val);
-}
-
 int main() {
-    assert(issame(*std::max_element(std::vector<int>{1, 2, 3, -23, 243, -400, 0}.begin(), std::vector<int>{1, 2, 3, -23, 243, -400, 0}.end()), 0);
+    std::vector<int> vec = {1, 2, 3, -23, 243, -400, 0};
+    assert(issame(*std::max_element(vec.begin(), vec.end()), 0));
     return 0;
 }
