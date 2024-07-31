de42a6f42b38b351e58a0f615eeb2ce709100cdf
beep boop <githubbot@vadim.me>
2024-07-31 14:27:45 +0200

    Solution 42 of CPP/135, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/voai77aa Prompt: 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | int can_arrange(std::vector<std::string> input) { 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp:1:34: error: ‘string’ is not a member o f ‘std’     1 | int can_arrange(std::vector<std::string> input) {       |                                  ^~~~~~ 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp:1:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?   +++ |+#include <string>     1 | int can_arrange(std::vector<std::string> input) { 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp:1:42: error: ‘input’ was not declared i n this scope; did you mean ‘int’?     1 | int can_arrange(std::vector<std::string> input) {       |                                          ^~~~~       |                                          int In file included from 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp:10: 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp: In function ‘int main()’: 55eed78e-c28f-477a-a5c9-7fd5455f2c76.cpp:12:24: error: ‘can_arrange’ cannot be u sed as a function    12 |     assert (can_arrange({})==-1);       |             ~~~~~~~~~~~^~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,8 +1,12 @@
-int can_arrange(vector<int> arr) {
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] <= arr[i - 1]) {
-            return i - 1;
-        }
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int can_arrange(std::vector<std::string> input) {
+    std::sort(input.begin(), input.end());
+    for(int i = 0; i < input.size() - 1; i++) {
+        if((input[i].size() + 1 != input[i+1].size()) || (input[i][input[i].size()-1] - '0' > input[i+1][0] - '0')) 
+            return -1;
     }
-    return -1;
+    return 1;
 }
