62ff6a486c6344f7636bebfb2970dff69e43381a
beep boop <githubbot@vadim.me>
2024-07-31 08:42:22 +0200

    Solution 4 of CPP/63, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2mlkggby Prompt: ore ‘for’     8 |     for(int i = 3; i <= n; i++){       |     ^~~ f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp:8:20: error: ‘i’ does not name a type     8 |     for(int i = 3; i <= n; i++){       |                    ^ f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp:8:28: error: ‘i’ does not name a type     8 |     for(int i = 3; i <= n; i++){       |                            ^ f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return d;       |     ^~~~~~ f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp:17: f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp: In function ‘int main()’: f50cd205-eae0-4c99-9b3f-412ee682d3a0.cpp:19:13: error: ‘fibfib’ was not declared  in this scope    19 |     assert (fibfib(14) == 927);       |             ^~~~~~

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,14 +1,11 @@
-#include <iostream>
-#include <cassert>
-
-int fibfib(int n){
+int fibfib(int n) {
     if(n == 0 || n == 1){
         return 0;
     }
     if(n == 2){
         return 1;
     }
-    int a = 0, b = 0, c = 1, d = 0;
+    int a = 0, b = 0, c = 1, d;
     for(int i = 3; i <= n; i++){
         d = a + b + c;
         a = b;
@@ -16,9 +13,4 @@ int fibfib(int n){
         c = d;
     }
     return d;
-}
-
-int main(){
-    assert (fibfib(14) == 927);
-    return 0;
 }
