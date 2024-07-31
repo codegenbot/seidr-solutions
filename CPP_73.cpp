5399c94c183b321dfb66d4627191192b80da41f2
beep boop <githubbot@vadim.me>
2024-07-31 11:48:45 +0200

    Solution 31 of CPP/73, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rimxs9mf Prompt: 6caf72ef-e6dc-40c3-8059-a43cb9878c31.cpp:25:50: error: request for member ‘size’  in ‘(& dp.std::vector<std::vector<int> >::operator[](((std::vector<std::vector< int> >::size_type)i)))->std::vector<int>::operator[](((std::vector<int>::size_ty pe)(j - 1)))’, which is of non-class type ‘__gnu_cxx::__alloc_traits<std::alloca tor<int>, int>::value_type’ {aka ‘int’}    25 |                     options.push_back(dp[i][j-1].size() + (k == 1));       |                                                  ^~~~ 6caf72ef-e6dc-40c3-8059-a43cb9878c31.cpp:26:52: error: request for member ‘size’  in ‘(& dp.std::vector<std::vector<int> >::operator[](((std::vector<std::vector< int> >::size_type)(i + 1))))->std::vector<int>::operator[](((std::vector<int>::s ize_type)(j - 1)))’, which is of non-class type ‘__gnu_cxx::__alloc_traits<std:: allocator<int>, int>::value_type’ {aka ‘int’}    26 |                     options.push_back(dp[i+1][j-1].size() + 2);       |                                                    ^~~~ 6caf72ef-e6dc-40c3-8059-a43cb9878c31.cpp: At global scope: 6caf72ef-e6dc-40c3-8059-a43cb9878c31.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 6caf72ef-e6dc-40c3-8059-a43cb9878c31.cpp:36:5: note: ‘int main()’ previously def ined here    36 | int main() { return smallest_change({0, 1}); }       |     ^~~~

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,22 +1,34 @@
-int smallest_change(vector<int> arr) {
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef int size_type;
+
+int smallest_change(std::vector<int> arr) {
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(n));
-    
-    for (int i = 0; i < n; i++) {
+    std::vector<std::vector<size_type>> dp(n+1, std::vector<size_type>(n+1));
+
+    for (int i = 0; i <= n; i++) {
         dp[i][i] = 0;
     }
-    
+
     for (int len = 2; len <= n; len++) {
         for (int i = 0; i < n - len + 1; i++) {
             int j = i + len - 1;
-            
+
             if (arr[i] == arr[j]) {
-                dp[i][j].push_back(dp[i+1][j-1].size());
+                dp[i][j] = std::vector<size_type>(dp[i+1][j-1]).size();
             } else {
-                dp[i][j].push_back(min({dp[i+1][j].size() + 1, dp[i][j-1].size() + 1, dp[i+1][j-1].size() + 2}));
+                vector<size_type> options;
+                for(int k = 0; k < 3; k++) {
+                    options.push_back(std::vector<size_type>(dp[i+1][j]).size() + (k == 0));
+                    options.push_back(std::vector<size_type>(dp[i][j-1]).size() + (k == 1));
+                    options.push_back(std::vector<size_type>(dp[i+1][j-1]).size() + 2);
+                }
+                dp[i][j] = *min_element(options.begin(), options.end());
             }
         }
     }
-    
-    return dp[0][n-1].size();
+
+    return dp[0].at(n-1);
 }
