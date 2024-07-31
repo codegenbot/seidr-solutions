3bdde36f67c0436be95a1dd20b7f31a6c93d89c1
beep boop <githubbot@vadim.me>
2024-07-31 09:22:23 +0200

    Solution 6 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/z79oxzny Prompt:       |                    ^~~ 07c48792-38ee-4c6b-af23-55350c1759e5.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 07c48792-38ee-4c6b-af23-55350c1759e5.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 07c48792-38ee-4c6b-af23-55350c1759e5.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 07c48792-38ee-4c6b-af23-55350c1759e5.cpp:13: 07c48792-38ee-4c6b-af23-55350c1759e5.cpp: In function ‘int main()’: 07c48792-38ee-4c6b-af23-55350c1759e5.cpp:23:20: error: ‘make_a_pile’ was not dec lared in this scope    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 07c48792-38ee-4c6b-af23-55350c1759e5.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,25 +1,17 @@
 #include <vector>
 
-bool issame(int a, int b);
+bool issame(std::vector<int> a, std::vector<int> b);
 
-vector<int> make_a_pile(int n){
-    vector<int> stones;
+std::vector<int> make_a_pile(int n){
+    std::vector<int> stones;
     stones.push_back(n);
-    for(int i = 1; i < n; ++i){
-        if(n % 2 == 0)
+    for(int i = 1; i < n; i++){
+        if(n % 2 == 0){
             n += 2;
-        else
+        } else {
             n += 1;
+        }
         stones.push_back(n);
     }
     return stones;
-}
-
-bool issame(int a, int b){
-    return a == b;
-}
-
-int main(){
-    // Main function code if needed
-    return 0;
 }
