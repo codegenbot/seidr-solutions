fd6cf5cbbbcd87654961ddec2a4d41fc92ae5cd6
beep boop <githubbot@vadim.me>
2024-07-31 15:56:10 +0200

    Solution 1 of Python/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/lo2jznyy Prompt: Complete the following code given the task description and function signature.

--- Python_105.py
+++ Python_105.py
@@ -9,5 +9,4 @@ def by_length(arr):
             f"Six" if i == '6' else
             f"Seven" if i == '7' else
             f"Eight" if i == '8' else
-            f"Nine" if i == '9' else "Invalid number"
-            for i in result]
+            f"Nine" for i in result]
