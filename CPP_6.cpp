c4f3345b4f1a598fbb92533fdfe2dcdbeba6eb8b
beep boop <githubbot@vadim.me>
2024-07-31 11:19:03 +0200

    Solution 16 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/eweb77sr Prompt:       |                    ^~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 350ea264-ea71-4cd1-a913-805535bdce88.cpp:24: 350ea264-ea71-4cd1-a913-805535bdce88.cpp: In function ‘int main()’: 350ea264-ea71-4cd1-a913-805535bdce88.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -6,3 +6,28 @@ std::vector<int> parse_nested_parens(std::string paren_string);
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
+
+std::vector<int> parse_nested_parens(string paren_string) {
+    std::vector<int> depths;
+    int max_depth = 0;
+    int current_depth = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            current_depth++;
+            max_depth = std::max(max_depth, current_depth);
+        } else if (c == ')') {
+            current_depth--;
+        } else if (c == ' ') {
+            depths.push_back(max_depth);
+            max_depth = 0;
+            current_depth = 0;
+        }
+    }
+
+    depths.push_back(max_depth);
+
+    return depths;
+}
+
+assert(issame(parse_nested_parens("(()(())((())))"), {4}));
