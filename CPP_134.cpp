892849b2b93961c88ecc8ec8c846dea672f4bdae
beep boop <githubbot@vadim.me>
2024-07-31 14:51:02 +0200

    Solution 52 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kt3hhe6w Prompt: 0b6a9ae8-4404-47b4-a5b1-614aca7c4de4.cpp: In function ‘int main()’: 0b6a9ae8-4404-47b4-a5b1-614aca7c4de4.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(check_if_last_char_is_a_letter("apple pi e ") == false);       |     ^~~~~~ 0b6a9ae8-4404-47b4-a5b1-614aca7c4de4.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <iostream>   +++ |+#include <cassert>     2 | #include <string> 0b6a9ae8-4404-47b4-a5b1-614aca7c4de4.cpp: At global scope: 0b6a9ae8-4404-47b4-a5b1-614aca7c4de4.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 0b6a9ae8-4404-47b4-a5b1-614aca7c4de4.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <string>
-#include <cassert>
+#include <cassert> 
+
 using namespace std;
 
 bool check_if_last_char_is_a_letter(const string& txt) {
     if(txt.empty())
         return false;
+
     char lastChar = txt.back();
     if(isalpha(lastChar)){
         if(txt.find_last_of(" ") == txt.length()-1)
             return true;
     }
+
     return false;
 }
 
 int main(){
-    assert(check_if_last_char_is_a_letter("apple pi e ") == false);
-    
+    assert(check_if_last_char_is_a_letter("apple pi e ") == false); 
+
     return 0;
 }
