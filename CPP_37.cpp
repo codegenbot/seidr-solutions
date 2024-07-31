eca7087b6a301db84d933af1546daa0c672f54ce
beep boop <githubbot@vadim.me>
2024-07-31 08:28:48 +0200

    Solution 98 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/26vdj7a8 Prompt: ined here    37 | int main() {       |     ^~~~ In file included from b80bf6e7-1e90-4420-9ca7-1d68f0a47b4e.cpp:45: b80bf6e7-1e90-4420-9ca7-1d68f0a47b4e.cpp: In function ‘int main()’: b80bf6e7-1e90-4420-9ca7-1d68f0a47b4e.cpp:55:29: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    55 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from b80bf6e7-1e90-4420-9ca7-1d68f0a47b4e.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ b80bf6e7-1e90-4420-9ca7-1d68f0a47b4e.cpp:12:31: note:   initializing argument 1 of ‘void sort_even(std::vector<float>&)’    12 | void sort_even(vector<float>& l) {       |                ~~~~~~~~~~~~~~~^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -5,12 +5,16 @@
 
 using namespace std;
 
-void sort_even(vector<float>& l) {
+bool issame(const vector<float>& a, const vector<float>& b) {
+    return a == b;
+}
+
+void sort_even(vector<float> l) {
     vector<float> even_values;
 
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            even_values.push_back(l[i]);
+            even_values.push_back(move(l[i]));
         }
     }
 
@@ -19,21 +23,21 @@ void sort_even(vector<float>& l) {
     int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            l[i] = even_values[even_index];
+            l[i] = move(even_values[even_index]);
             even_index++;
         }
     }
 }
 
 vector<float> sort_even_elements(vector<float> l) {
-    sort_even(l);
+    sort_even(move(l));
     return l;
 }
 
 int main() {
     vector<float> result = sort_even_elements({5, 8, -12, 4, 23, 2, 3, 11, 12, -10});
     vector<float> expected_result = {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10};
-    assert(result == expected_result);
+    assert(issame(result, expected_result));
 
     return 0;
 }
