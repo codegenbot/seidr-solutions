fce26672743e2e65a520642aa7b7546f83667a4f
beep boop <githubbot@vadim.me>
2024-07-31 12:35:16 +0200

    Solution 4 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xfipzq0t Prompt: 1002fe38-e65c-4073-a42f-8c8d6a6504b1.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for (char bracket : brackets) {       |     ^~~ 1002fe38-e65c-4073-a42f-8c8d6a6504b1.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return count == 0;       |     ^~~~~~ 1002fe38-e65c-4073-a42f-8c8d6a6504b1.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from 1002fe38-e65c-4073-a42f-8c8d6a6504b1.cpp:15: 1002fe38-e65c-4073-a42f-8c8d6a6504b1.cpp: In function ‘int main()’: 1002fe38-e65c-4073-a42f-8c8d6a6504b1.cpp:17:18: error: ‘correct_bracketing’ was not declared in this scope    17 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,13 +1,14 @@
-int count = 0;
-for (char bracket : brackets) {
-    if (bracket == '<') {
-        count++;
-    } else if (bracket == '>') {
-        count--;
+bool correct_bracketing(const std::string &brackets) {
+    int count = 0;
+    for (char bracket : brackets) {
+        if (bracket == '<') {
+            count++;
+        } else if (bracket == '>') {
+            count--;
+            if (count < 0) {
+                return false;
+            }
+        }
     }
-    if (count < 0) {
-        return false;
-    }
-}
-return count == 0;
+    return count == 0;
 }
