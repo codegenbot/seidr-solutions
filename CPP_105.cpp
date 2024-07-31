fa1ede31cef138f8913389545f4a055182d975cd
beep boop <githubbot@vadim.me>
2024-07-31 13:44:56 +0200

    Solution 51 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0x42macp Prompt:       |                                 ^~~~~~ In file included from 50104312-63b2-4846-927f-90018ee83591.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 50104312-63b2-4846-927f-90018ee83591.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 50104312-63b2-4846-927f-90018ee83591.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 50104312-63b2-4846-927f-90018ee83591.cpp:39: 50104312-63b2-4846-927f-90018ee83591.cpp: In function ‘int main()’: 50104312-63b2-4846-927f-90018ee83591.cpp:49:20: error: ‘by_length’ was not decla red in this scope    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 50104312-63b2-4846-927f-90018ee83591.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -6,17 +6,49 @@
 
 using namespace std;
 
-bool issame(const vector<string>& vec1, const vector<string>& vec2){
-    return vec1 == vec2;
+bool issame(const vector<int>& arr1, const vector<int>& arr2){
+    return arr1 == arr2;
+}
+
+vector<string> by_length(vector<int> arr){
+    vector<int> sorted_arr;
+    vector<string> result;
+    map<int, string> num_to_name = {
+        {1, "One"},
+        {2, "Two"},
+        {3, "Three"},
+        {4, "Four"},
+        {5, "Five"},
+        {6, "Six"},
+        {7, "Seven"},
+        {8, "Eight"},
+        {9, "Nine"}
+    };
+
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            sorted_arr.push_back(num);
+        }
+    }
+
+    sort(sorted_arr.begin(), sorted_arr.end());
+
+    reverse(sorted_arr.begin(), sorted_arr.end());
+
+    for (int num : sorted_arr) {
+        result.push_back(num_to_name[num]);
+    }
+
+    return result;
 }
 
 int main() {
-    vector<int> numbers = {5, 2, 9, 4, 3, 8, 1, 7};
-    vector<string> result = by_length(numbers);
-  
-    vector<string> expected = {"Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"};
+    vector<int> input = {9, 3, 7, 2, 5};
+    vector<string> output = by_length(input);
 
-    assert(issame(result, expected));
+    for (const string& str : output) {
+        cout << str << " ";
+    }
 
     return 0;
 }
