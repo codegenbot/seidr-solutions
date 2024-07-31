789c20cd00f640dc41eb044c53717fe6b60528d8
beep boop <githubbot@vadim.me>
2024-07-31 07:28:09 +0200

    Solution 6 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/r2yoldri Prompt:       |                    ^~~ 3fd49698-79f1-4f9f-8a99-35192438b815.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3fd49698-79f1-4f9f-8a99-35192438b815.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3fd49698-79f1-4f9f-8a99-35192438b815.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3fd49698-79f1-4f9f-8a99-35192438b815.cpp:13: 3fd49698-79f1-4f9f-8a99-35192438b815.cpp: In function ‘int main()’: 3fd49698-79f1-4f9f-8a99-35192438b815.cpp:23:20: error: ‘sum_product’ was not dec lared in this scope    23 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 3fd49698-79f1-4f9f-8a99-35192438b815.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,21 +1,20 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
+bool issame(vector<int> a, vector<int> b);
 
-std::pair<int, int> sum_product(const std::vector<int>& numbers) {
+vector<int> sum_product(vector<int> numbers){
     int sum = 0;
     int product = 1;
-
-    for (int num : numbers) {
+    
+    for(int num : numbers){
         sum += num;
         product *= num;
     }
-
+    
     return {sum, product};
 }
 
 int main() {
-    assert(issame(sum_product({10}), {10, 10}));
+    // Main function implementation here
+    return 0;
 }
