5c3deda4e0b4af1bad8bd42f731bd0fdfba11ade
beep boop <githubbot@vadim.me>
2024-07-31 17:13:56 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/clevaarj Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,30 +1,26 @@
-if(typeid(a) == typeid(int) && typeid(b) == typeid(int)) {
-    if(boost::any_cast<int>(a) > boost::any_cast<int>(b)){
+if (a.type() == typeid(int) && b.type() == typeid(int)){
+    if (boost::any_cast<int>(a) > boost::any_cast<int>(b)){
         return a;
-    } else if(boost::any_cast<int>(a) < boost::any_cast<int>(b)){
+    } else if (boost::any_cast<int>(a) < boost::any_cast<int>(b)){
         return b;
-    } else {
-        return "None";
+    }
+} else if (a.type() == typeid(float) && b.type() == typeid(float)){
+    if (boost::any_cast<float>(a) > boost::any_cast<float>(b)){
+        return a;
+    } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)){
+        return b;
+    }
+} else if (a.type() == typeid(string) && b.type() == typeid(string)){
+    string strA = boost::any_cast<string>(a);
+    string strB = boost::any_cast<string>(b);
+    strA.erase(remove(strA.begin(), strA.end(), ','), strA.end());
+    strB.erase(remove(strB.begin(), strB.end(), ','), strB.end());
+    if (stof(strA) > stof(strB)){
+        return a;
+    } else if (stof(strA) < stof(strB)){
+        return b;
     }
 }
 
-if(typeid(a) == typeid(float) && typeid(b) == typeid(float)) {
-    if(boost::any_cast<float>(a) > boost::any_cast<float>(b)){
-        return a;
-    } else if(boost::any_cast<float>(a) < boost::any_cast<float>(b)){
-        return b;
-    } else {
-        return "None";
-    }
-}
-
-if(typeid(a) == typeid(string) && typeid(b) == typeid(string)) {
-    if(stof(boost::any_cast<string>(a)) > stof(boost::any_cast<string>(b))){
-        return a;
-    } else if(stof(boost::any_cast<string>(a)) < stof(boost::any_cast<string>(b))){
-        return b;
-    } else {
-        return "None";
-    }
-}
+return "None";
 }
