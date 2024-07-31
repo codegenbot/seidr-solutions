dfdbd9fecf46cad1955b5b6abea2da940d800126
beep boop <githubbot@vadim.me>
2024-07-31 10:49:43 +0200

    Solution 5 of CPP/45, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vpljbp97 Prompt: 0cea4749-c4ef-4136-a8ed-0abc32b12c9b.cpp: In function ‘float triangle_area(float , float)’: 0cea4749-c4ef-4136-a8ed-0abc32b12c9b.cpp:4:22: error: ‘abs’ is not a member of ‘ std’     4 |     return (a * std::abs(h)) / 2.0f;       |                      ^~~ In file included from 0cea4749-c4ef-4136-a8ed-0abc32b12c9b.cpp:7: 0cea4749-c4ef-4136-a8ed-0abc32b12c9b.cpp: In function ‘int main()’: 0cea4749-c4ef-4136-a8ed-0abc32b12c9b.cpp:9:13: error: ‘abs’ was not declared in this scope     9 |     assert (abs(triangle_area(10, 8) - 40.0)<1e-4);       |             ^~~

--- CPP_45.cpp
+++ CPP_45.cpp
@@ -1,5 +1,10 @@
-#include <cstddef>
+#include <cmath>
 
 float triangle_area(float a, float h) {
     return (a * std::abs(h)) / 2.0f;
+}
+
+int main() {
+    assert(std::abs(triangle_area(10, 8) - 40.0)<1e-4);
+    // do other things
 }
