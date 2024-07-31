b1b49e5041307f0e240f53d3cbbf27b9f9ceaeab
beep boop <githubbot@vadim.me>
2024-07-31 12:04:10 +0200

    Solution 12 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/n4ee1ehh Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c269c743-525f-4029-b7c1-ea1ace514be7.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c269c743-525f-4029-b7c1-ea1ace514be7.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c269c743-525f-4029-b7c1-ea1ace514be7.cpp:5: c269c743-525f-4029-b7c1-ea1ace514be7.cpp: In function ‘int main()’: c269c743-525f-4029-b7c1-ea1ace514be7.cpp:15:20: error: ‘unique’ was not declared  in this scope    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ c269c743-525f-4029-b7c1-ea1ace514be7.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -6,10 +6,11 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-int main() {
-    std::vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    std::sort(l.begin(), l.end());
-    l.erase(std::unique(l.begin(), l.end()), l.end());
-    assert(issame(l, std::vector<int>{0, 2, 3, 5, 9, 123}));
-    return 0;
+std::vector<int> fixCode(const std::vector<int>& l) {
+    std::vector<int> res = l;
+    sort(res.begin(), res.end());
+    res.erase(unique(res.begin(), res.end()), res.end());
+    return res;
 }
+
+assert(issame(fixCode({5, 3, 5, 2, 3, 3, 9, 0, 123}), std::vector<int>{0, 2, 3, 5, 9, 123}));
