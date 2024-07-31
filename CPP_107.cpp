8aa6eb87b123fb68905faddfdb928ba12c449864
beep boop <githubbot@vadim.me>
2024-07-31 09:37:30 +0200

    Solution 38 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/cuo3c03d Prompt: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e8040e5b-9cf1-4124-8ef5-7e690a722e2e.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e8040e5b-9cf1-4124-8ef5-7e690a722e2e.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ e8040e5b-9cf1-4124-8ef5-7e690a722e2e.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from e8040e5b-9cf1-4124-8ef5-7e690a722e2e.cpp:32: e8040e5b-9cf1-4124-8ef5-7e690a722e2e.cpp: In function ‘int main()’: e8040e5b-9cf1-4124-8ef5-7e690a722e2e.cpp:42:39: error: could not convert ‘even_o dd_palindrome(1)’ from ‘std::pair<int, int>’ to ‘std::vector<int>’    42 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ~~~~~~~~~~~~~~~~~~~^~~       |                                       |       |                                       std::pair<int, int>

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -25,6 +25,5 @@ std::pair<int, int> even_odd_palindrome(int n) {
 }
 
 int main() {
-    assert(issame(std::vector<int>{even_odd_palindrome(1).first, even_odd_palindrome(1).second}, std::vector<int>{0, 1}));
-    return 0;
+    return issame(std::vector<int>{even_odd_palindrome(1).first, even_odd_palindrome(1).second}, std::vector<int>{0, 1});
 }
