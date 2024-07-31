8324dbfcb209a06b53a3dbd8e708d3cf7638be11
beep boop <githubbot@vadim.me>
2024-07-31 14:19:52 +0200

    Solution 14 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3aukpuda Prompt: 9678e1b5-4e12-4dae-98ca-c7c1d730e875.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 9678e1b5-4e12-4dae-98ca-c7c1d730e875.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 9678e1b5-4e12-4dae-98ca-c7c1d730e875.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 9678e1b5-4e12-4dae-98ca-c7c1d730e875.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 9678e1b5-4e12-4dae-98ca-c7c1d730e875.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,4 +1,4 @@
-```c++
+```cpp
 #include <string>
 #include <iostream>
 #include <cassert>
@@ -23,5 +23,6 @@ string fix_spaces(string text){
 
 int main() {
     assert(fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");
+    cout << fix_spaces("Hello World") << endl;
     return 0;
 }
