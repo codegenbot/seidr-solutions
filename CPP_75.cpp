82c4ffaa90eed600651e695b60f109628a542824
beep boop <githubbot@vadim.me>
2024-07-31 08:54:08 +0200

    Solution 5 of CPP/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jptyl66i Prompt: 6380016d-f76c-4fca-9d11-4f5b7af551f4.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 6380016d-f76c-4fca-9d11-4f5b7af551f4.cpp:38:5: note: ‘int main()’ previously def ined here    38 | int main() {       |     ^~~~

---
+++ CPP_75.cpp
@@ -0,0 +1,11 @@
+int main() {
+    int num;
+    cin >> num;
+    if (is_multiply_prime(num)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
