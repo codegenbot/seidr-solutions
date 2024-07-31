32daca7c33834fcd548f645ec83ede8a7b09498c
beep boop <githubbot@vadim.me>
2024-07-31 12:03:22 +0200

    Solution 51 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/yfxpvr0v Prompt:       |             ^~~~~~ 1dfae936-dc14-481b-862f-452c22e61b42.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 1dfae936-dc14-481b-862f-452c22e61b42.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 1dfae936-dc14-481b-862f-452c22e61b42.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 1dfae936-dc14-481b-862f-452c22e61b42.cpp:21: 1dfae936-dc14-481b-862f-452c22e61b42.cpp: In function ‘int main()’: 1dfae936-dc14-481b-862f-452c22e61b42.cpp:31:20: error: ‘get_positive’ was not de clared in this scope    31 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 1dfae936-dc14-481b-862f-452c22e61b42.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -18,8 +18,8 @@ vector<float> get_positive(vector<float> l) {
 int main() {
     vector<float> numbers = {3.0, -2.0, 5.0, 0.0, 1.0};
     vector<float> positive_numbers = get_positive(numbers);
-    
+
     assert(issame(get_positive({}), {}));
-    
+
     return 0;
 }
