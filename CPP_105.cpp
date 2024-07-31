f873bc44fa1265a798c446aaa1bfc6b571750963
beep boop <githubbot@vadim.me>
2024-07-31 16:24:01 +0200

    Solution 91 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/zyb4htcy Prompt:       |                              ^~~~~~ feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp:25: feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp: In function ‘int main()’: feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp:35:20: error: ‘by_length’ was not decla red in this scope    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ feb104bb-7c71-43ca-a2f3-f35b585f561d.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,33 +1,9 @@
-#include <iostream>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <cassert>
 
-bool issame(const std::string &str1, const std::string &str2) {
-    return str1 == str2;
+bool issame(int a, int b){
+    return a == b;
 }
-
-std::vector<std::string> by_length(std::vector<int> arr){
-    std::vector<std::string> result;
-    std::map<int, std::string> numToName = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
-    };
-
-    std::vector<int> sortedArr;
-    for (int num : arr) {
-        if (num >= 1 && num <= 9) {
-            sortedArr.push_back(num);
-        }
-    }
-
-    std::sort(sortedArr.begin(), sortedArr.end());
-    std::reverse(sortedArr.begin(), sortedArr.end());
-
-    for (int num : sortedArr) {
-        result.push_back(numToName[num]);
-    }
-
-    return result;
-}
+```
