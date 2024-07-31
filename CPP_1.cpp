710bdadd86817380e2b64ef4397a9c6a904326a8
beep boop <githubbot@vadim.me>
2024-07-31 10:58:42 +0200

    Solution 5 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ltdegq00 Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 6e749c2c-e604-4018-9c5b-071f3a900bc8.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6e749c2c-e604-4018-9c5b-071f3a900bc8.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 6e749c2c-e604-4018-9c5b-071f3a900bc8.cpp:26: 6e749c2c-e604-4018-9c5b-071f3a900bc8.cpp: In function ‘int main()’: 6e749c2c-e604-4018-9c5b-071f3a900bc8.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 6e749c2c-e604-4018-9c5b-071f3a900bc8.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <cassert>
 
 using namespace std;
 
 vector<string> separate_paren_groups(string paren_string);
 
-bool issame(vector<string> a, vector<string> b);
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
-    vector<string> result = separate_paren_groups("((group 1)(group 2))");
-    assert(issame(result, {"(group 1)", "(group 2)"}));
-    
-    result = separate_paren_groups("(a(b(c)d)e)");
-    assert(issame(result, {"(a(b(c)d)e)"}));
-    
-    result = separate_paren_groups("(hello)(world)");
-    assert(issame(result, {"(hello)", "(world)"}));
-    
-    cout << "All test cases passed." << endl;
-    
+    vector<string> res = separate_paren_groups("((hello)(world))");
+    assert(issame(res, {"(hello)", "(world)"}));
+
     return 0;
 }
 
@@ -47,14 +50,4 @@ vector<string> separate_paren_groups(string paren_string) {
     }
 
     return result;
-}
-
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
-    }
-
-    return true;
 }
