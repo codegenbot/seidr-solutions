2bb80299059da0b8e13d799529ed63f11dfffc76
beep boop <githubbot@vadim.me>
2024-07-31 07:52:53 +0200

    Solution 5 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4om60edx Prompt:  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0e96429b-c064-4d0a-b99f-246f9cfd28d2.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0e96429b-c064-4d0a-b99f-246f9cfd28d2.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0e96429b-c064-4d0a-b99f-246f9cfd28d2.cpp:10: 0e96429b-c064-4d0a-b99f-246f9cfd28d2.cpp: In function ‘int main()’: 0e96429b-c064-4d0a-b99f-246f9cfd28d2.cpp:20:20: error: ‘filter_integers’ was not  declared in this scope    20 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 0e96429b-c064-4d0a-b99f-246f9cfd28d2.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <vector>
 #include <list>
+#include <vector>
 #include <any>
 
-bool issame(std::vector<int> a, std::vector<int> b){}
+bool issame(vector<int> a, vector<int> b){}
 
-std::vector<int> filter_integers(std::list<std::any> values){
+vector<int> filter_integers(std::list<std::any> values){
     std::vector<int> result;
     for(auto val : values){
         if(typeid(int) == val.type())
             result.push_back(std::any_cast<int>(val));
     }
     return result;
-}
-
-int main() {
-    // Rest of the main function
-    return 0;
 }
