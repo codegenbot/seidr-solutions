fc39da74e2be2a38be0f061c1e3a62ef189564f2
beep boop <githubbot@vadim.me>
2024-07-31 08:56:14 +0200

    Solution 2 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gi7mtptd Prompt: b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp:17: b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp: In function ‘int main()’: b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp: In function ‘int total_chars(int)’: b3fa4b5a-6dd8-490a-a72b-b215b0b3451e.cpp:7:1: warning: control reaches end of no n-void function [-Wreturn-type]     7 | }       | ^

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,18 +1,32 @@
-int countTotalChars(vector<string> lst) {
-    int totalChars = 0;
-    for (string str : lst) {
-        totalChars += str.size();
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+bool issame(vector<string> a, vector<string> b);
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
+int total_chars(const vector<string>& lst) {
+    int total = 0;
+    for (const string& str : lst) {
+        total += str.size();
     }
-    return totalChars;
+    return total;
 }
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    int totalChars1 = countTotalChars(lst1);
-    int totalChars2 = countTotalChars(lst2);
-
-    if (totalChars1 < totalChars2) {
+    if (total_chars(lst1) < total_chars(lst2)) {
         return lst1;
     } else {
-        return totalChars1 == totalChars2 ? lst1 : lst2;
+        return lst2;
     }
+}
+
+int main() {
+    assert(issame(total_match({"this"}, {}), {}));
+    
+    return 0;
 }
