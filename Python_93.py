739863eb950039aad6d0f2dc48ab70dbeb381368
beep boop <githubbot@vadim.me>
2024-07-31 15:47:08 +0200

    Solution 63 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tq42c3it Prompt: Complete the following code given the task description and function signature.

--- Python_93.py
+++ Python_93.py
@@ -4,9 +4,9 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.isupper():
-                result += chr((ord(char) - 65 + 2) % 26 + 65)
+                result += chr((ord(char) - ord('A') + 2) % 26 + ord('A'))
             else:
-                result += chr((ord(char) - 97 + 2) % 26 + 97)
+                result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
         else:
             result += char
     return result
