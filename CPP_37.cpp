5ab5ca56150be5ea7dc95675663ad6192d93068f
beep boop <githubbot@vadim.me>
2024-07-31 12:13:22 +0200

    Solution 59 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mv1rwjoo Prompt:    31 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 161469b7-c5ae-4678-b499-a2c8737f48bb.cpp:31:29: error: ‘vector’ was not declared  in this scope    31 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 161469b7-c5ae-4678-b499-a2c8737f48bb.cpp:31:36: error: expected primary-expressi on before ‘float’    31 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 161469b7-c5ae-4678-b499-a2c8737f48bb.cpp:30: 161469b7-c5ae-4678-b499-a2c8737f48bb.cpp: In function ‘int main()’: 161469b7-c5ae-4678-b499-a2c8737f48bb.cpp:40:20: error: ‘sort_even’ was not decla red in this scope    40 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 161469b7-c5ae-4678-b499-a2c8737f48bb.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -4,11 +4,7 @@
 
 void sort_even(std::vector<float>& l);
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    sort_even(a);
-    sort_even(b);
-    return a == b;
-}
+bool issame(std::vector<float> a, std::vector<float> b);
 
 void sort_even(std::vector<float>& l) {
     std::vector<float> even_elems;
@@ -31,4 +27,10 @@ void sort_even(std::vector<float>& l) {
     }
     
     l = result;
+}
+
+bool issame(std::vector<float> a, std::vector<float> b) {
+    sort_even(a);
+    sort_even(b);
+    return a == b;
 }
