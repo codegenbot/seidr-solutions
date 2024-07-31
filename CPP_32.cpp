feee46f0480a45e416371b70a0dfbe881365b73a
beep boop <githubbot@vadim.me>
2024-07-31 10:33:54 +0200

    Solution 66 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3iy35mq Prompt: ca2f9d18-e377-4445-927c-0a0e0c38a990.cpp:52:16: error: expected primary-expressi on before ‘double’    52 |         vector<double> coeffs = {};       |                ^~~~~~ ca2f9d18-e377-4445-927c-0a0e0c38a990.cpp:57:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    57 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff ca2f9d18-e377-4445-927c-0a0e0c38a990.cpp:60:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    60 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff ca2f9d18-e377-4445-927c-0a0e0c38a990.cpp:60:36: error: ‘find_zero’ cannot be use d as a function    60 |         solution = find_zero(coeffs);       |                                    ^ In file included from ca2f9d18-e377-4445-927c-0a0e0c38a990.cpp:44: ca2f9d18-e377-4445-927c-0a0e0c38a990.cpp:61:25: error: ‘poly’ cannot be used as a function    61 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,39 +1,14 @@
-#include <cassert>
-#include <iostream>
-#include <iomanip>
-
-using namespace std;
-
-double poly(vector<double> coefficients, double x) {
-    double result = 0;
-    for (int i = 0; i < coefficients.size(); i++) {
-        result += coefficients[i] * pow(x, i);
-    }
-    return result;
-}
-
-double find_zero(vector<double> coefficients) {
-    vector<double> values;
-    double sum = 0;
-    for (int i = 1; i < coefficients.size(); i++) {
-        if (i % 2 == 0) {
-            values.push_back(coefficients[i] / coefficients[0]);
-        }
-    }
-    double x = -values[0];
-    return poly(coefficients, x); 
-}
-
+```cpp
 int main() {
     int n;
     cin >> n;
-    vector<double> coeffs = {};  
+    vector<double> coeffs = {}; 
+    double val;
     for (int i = 0; i < n; i++) {
-        double val;
         cin >> val;
         coeffs.push_back(val); 
     }
-    double solution = find_zero(coeffs); 
+    double solution = find_zero(coeffs);
     assert(abs(poly(coeffs, solution)) < 1e-3);
     cout << fixed << setprecision(2) << solution << endl;
     return 0;
