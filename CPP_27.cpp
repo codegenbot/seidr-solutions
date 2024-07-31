524138f645070960fc4a54d4636546ddb09fd0fa
beep boop <githubbot@vadim.me>
2024-07-31 09:59:24 +0200

    Solution 49 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vp4ox56h Prompt: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:67,                  from /usr/include/c++/11/bits/char_traits.h:39,                  from /usr/include/c++/11/string:40,                  from f5607fe9-88a7-47d6-bfa1-1b797492e41f.cpp:3: /usr/include/c++/11/bits/stl_iterator.h: In instantiation of ‘class __gnu_cxx::_ _normal_iterator<char*, std::__cxx11::basic_string<char> >’: /usr/include/c++/11/bits/basic_string.tcc:225:25:   required from ‘void std::__c xx11::basic_string<_CharT, _Traits, _Alloc>::_M_construct(_InIterator, _InIterat or, std::forward_iterator_tag) [with _FwdIterator = const char*; _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:539:14:   required from ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>::basic_string(const _CharT*, const _Alloc&)  [with <template-parameter-2-1> = std::allocator<char>; _CharT = char; _Traits =  std::char_traits<char>; _Alloc = std::allocator<char>]’ f5607fe9-88a7-47d6-bfa1-1b797492e41f.cpp:23:5:   required from here /usr/include/c++/11/bits/stl_iterator.h:1015:57: error: no type named ‘differenc e_type’ in ‘struct std::iterator_traits<char*>’  1015 |       typedef typename __traits_type::difference_type   difference_type;       |                                                         ^~~~~~~~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -2,13 +2,15 @@
 bool flip_case(const std::string& str) {
     std::string result;
     for (char c : str) {
-        if (std::isupper(c)) {
-            result += std::tolower(c);
-        } else if (std::islower(c)) {
-            result += std::toupper(c);
-        } else {
+        if(std::isalpha(c)) { 
+            if (std::isupper(c)) {
+                result += std::tolower(c);
+            } else if (std::islower(c)) {
+                result += std::toupper(c);
+            }
+        } else { 
             result += c;
         }
     }
-    return result;
+    return true; 
 }
