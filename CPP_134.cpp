34185f0a871cdef8c738d3f635f519a29c983471
beep boop <githubbot@vadim.me>
2024-07-31 14:48:25 +0200

    Solution 37 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kt3hhe6w Prompt: 867f8208-1e95-4486-82ff-664d5e7602cd.cpp: In function ‘int main()’: 867f8208-1e95-4486-82ff-664d5e7602cd.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(check_if_last_char_is_a_letter("apple pi e ") == false);       |     ^~~~~~ 867f8208-1e95-4486-82ff-664d5e7602cd.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <iostream>   +++ |+#include <cassert>     2 | #include <string> 867f8208-1e95-4486-82ff-664d5e7602cd.cpp: At global scope: 867f8208-1e95-4486-82ff-664d5e7602cd.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 867f8208-1e95-4486-82ff-664d5e7602cd.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -7,8 +7,10 @@ bool check_if_last_char_is_a_letter(const string& txt) {
     if(txt.empty())
         return false;
     char lastChar = txt.back();
-    if(isalpha(lastChar) && txt.find_last_not_of(" ") == txt.length() - 1)
-        return true;
+    if(isalpha(lastChar)){
+        if(txt.find_last_of(" ") == txt.length()-1)
+            return true;
+    }
     return false;
 }
 
