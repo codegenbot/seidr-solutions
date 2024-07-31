dc04e7cf80b2fe63ae25f3ba86f3672df00e4813
beep boop <githubbot@vadim.me>
2024-07-31 12:22:28 +0200

    Solution 50 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jjikknkv Prompt:    12 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 1df38dcb-b6a2-4e6f-a60b-4ee925bd6649.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1df38dcb-b6a2-4e6f-a60b-4ee925bd6649.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 1df38dcb-b6a2-4e6f-a60b-4ee925bd6649.cpp:11: 1df38dcb-b6a2-4e6f-a60b-4ee925bd6649.cpp: In function ‘int main()’: 1df38dcb-b6a2-4e6f-a60b-4ee925bd6649.cpp:21:20: error: ‘incr_list’ was not decla red in this scope    21 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~ 1df38dcb-b6a2-4e6f-a60b-4ee925bd6649.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -2,8 +2,8 @@
 #include <cassert>
 
 std::vector<int> incr_list(std::vector<int> a) {
-    for (int& num : a) {
-        num++;
+    for (int i = 0; i < a.size(); i++) {
+        a[i]++;
     }
     
     return a;
