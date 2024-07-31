2d243c0ff259ccb5e31080cc89efd6451ee2f516
beep boop <githubbot@vadim.me>
2024-07-31 14:20:17 +0200

    Solution 8 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4hbehiix Prompt: w_allocator<_Tp>::allocate(int, const void*) [with _Tp = float]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = float; std::allocator_tr aits<std::allocator<_CharT> >::pointer = float*; std::allocator_traits<std::allo cator<_CharT> >::allocator_type = std::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = float; _Alloc = std::allocator<float>; std::_Vector_base<_Tp, _Alloc>::po inter = float*; size_t = long unsigned int]’ /usr/include/c++/11/bits/stl_vector.h:1582:25:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = const float*; _Tp = float; _Alloc = s td::allocator<float>]’ /usr/include/c++/11/bits/stl_vector.h:629:21:   required from ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocator_type&) [with _Tp = f loat; _Alloc = std::allocator<float>; std::vector<_Tp, _Alloc>::allocator_type =  std::allocator<float>]’ 9096a252-ed07-4179-b82e-e82ed2f4dc97.cpp:21:49:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<float>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-long long double_the_difference(vector<float> lst) {
+long long double_the_difference(vector<int> lst) {
     long long sum = 0;
-    for (float num : lst) {
-        if (num > 0 && floor(num) == num) { 
-            if (fmod(num, 2.0) != 0.0) { 
-                sum += pow((long long)num, 2);
+    for (int num : lst) {
+        if (num > 0 && floor((double)num) == num) { 
+            if ((double)num % 2.0 != 0.0) { 
+                sum += pow((double)num, 2);
             }
         }
     }
@@ -18,7 +18,7 @@ long long double_the_difference(vector<float> lst) {
 }
 
 int main_func() {
-    vector<float> lst = {1.5f, 3.0f, 4.25f, 6.0f};
+    vector<int> lst = {1, 3, 4, 6};
     long long result = double_the_difference(lst);
     cout << "The difference is: " << result << endl;
     return 0;
