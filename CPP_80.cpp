edfce73cf65bf1350e6d24feb06c6ae19c5f59fe
beep boop <githubbot@vadim.me>
2024-07-31 15:28:52 +0200

    Solution 12 of CPP/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/87x1xg80 Prompt: Complete the following code given the task description and function signature.

--- CPP_80.cpp
+++ CPP_80.cpp
@@ -1,8 +1,6 @@
-if(s.length() < 3) {
-        return false;
-    }
-    for(int i=0; i<=s.length()-3; i++) {
-        if(s[i] == s[i+1] || s[i] == s[i+2] || s[i+1] == s[i+2]) {
+if(s.length() < 3) return false;
+    for(int i = 0; i <= s.length() - 3; i++){
+        if(s[i] == s[i+1] || s[i] == s[i+2] || s[i+1] == s[i+2]){
             return false;
         }
     }
