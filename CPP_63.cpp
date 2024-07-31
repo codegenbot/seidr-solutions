19644f6d86a2185e87063014d6a53ce7c06813f7
beep boop <githubbot@vadim.me>
2024-07-31 15:02:41 +0200

    Solution 6 of CPP/63, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xvl6jcax Prompt: Complete the following code given the task description and function signature.

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,16 +1,14 @@
-if(n == 0 || n == 1){
+if (n == 0 || n == 1) {
     return 0;
-}
-if(n == 2){
+  } else if (n == 2) {
     return 1;
-}
-
-int a = 0, b = 0, c = 1, result = 0;
-for(int i = 3; i <= n; ++i){
-    result = a + b + c;
+  }
+  int a = 0, b = 0, c = 1, res;
+  for (int i = 3; i <= n; ++i) {
+    res = a + b + c;
     a = b;
     b = c;
-    c = result;
-}
-return result;
+    c = res;
+  }
+  return res;
 }
