cfe72e7467fe9e02f0e84c4668675018aea58b60
beep boop <githubbot@vadim.me>
2024-07-31 11:31:08 +0200

    Solution 4 of CPP/64, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zyxnochj Prompt: b467b1c1-4780-40ca-978b-d597598d5865.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int vowels_count(string s) {       |                  ^~~~~~ b467b1c1-4780-40ca-978b-d597598d5865.cpp:3:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from b467b1c1-4780-40ca-978b-d597598d5865.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from b467b1c1-4780-40ca-978b-d597598d5865.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -1,7 +1,4 @@
-#include <string>
-using namespace std;
-
-int vowels_count(string s) {
+int vowels_count(std::string s) {
     int count = 0;
     for (char c : s) {
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
