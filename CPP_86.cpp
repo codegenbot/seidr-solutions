a035321384864519b7167dbd8f3ab63ce1a29337
beep boop <githubbot@vadim.me>
2024-07-31 13:01:10 +0200

    Solution 8 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/5c9um55e Prompt: ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp:3:5: error: expected unqualified-id bef ore ‘for’     3 |     for (char c : s) {       |     ^~~ ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp:12:5: error: expected unqualified-id be fore ‘if’    12 |     if (!word.empty()) {       |     ^~ ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp:16:5: error: expected unqualified-id be fore ‘return’    16 |     return result;       |     ^~~~~~ ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp:17:1: error: expected declaration befor e ‘}’ token    17 | }       | ^ In file included from ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp:19: ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp: In function ‘int main()’: ecdf4dc4-491a-4c80-b961-6a68a3d155da.cpp:21:13: error: ‘anti_shuffle’ was not de clared in this scope    21 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <algorithm>
 #include <cassert>
+
+using namespace std;
 
 string anti_shuffle(string s) {
     string result = "";
     string word = "";
+    
     for (char c : s) {
         if (c == ' ') {
             sort(word.begin(), word.end());
@@ -15,14 +18,17 @@ string anti_shuffle(string s) {
             word += c;
         }
     }
+    
     if (!word.empty()) {
         sort(word.begin(), word.end());
         result += word;
     }
+    
     return result;
 }
 
 int main() {
     assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
+    
     return 0;
 }
