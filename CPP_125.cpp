dab42aa7f33a6da087ca3cc52b28e64ff59049d4
beep boop <githubbot@vadim.me>
2024-07-31 14:16:38 +0200

    Solution 20 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6qw5jxbx Prompt:       |                              ^~~~~~ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:29: d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp: In function ‘int main()’: d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:39:20: error: ‘split_words’ was not dec lared in this scope    39 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ d43865a5-1c5b-4230-9e96-f02cd0195bd0.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,21 +1,7 @@
-#include <iostream>
-#include <vector>
 #include <string>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    return true;
-}
+vector<string> split_words(string txt);
 
 vector<string> split_words(string txt){
     vector<string> result;
@@ -36,7 +22,7 @@ vector<string> split_words(string txt){
     if(result.size() == 0){
         int count = 0;
         for(char c : txt){
-            if(islower(c) && (c - 'a') % 2 == 1){
+            if(islower(c) && ((c - 'a') % 2 == 1)){
                 count++;
             }
         }
@@ -45,8 +31,9 @@ vector<string> split_words(string txt){
     return result;
 }
 
-int main(){
-    assert(issame(split_words("") ,{"0"}));
-    
-    return 0;
+void issame(vector<string> a, vector<string> b){
+    assert(a.size() == b.size());
+    for(size_t i = 0; i < a.size(); i++){
+        assert(a[i] == b[i]);
+    }
 }
