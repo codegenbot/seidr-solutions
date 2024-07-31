291341bc428401a62c31717719d1f6ea18c2e9a2
beep boop <githubbot@vadim.me>
2024-07-31 15:22:26 +0200

    Solution 16 of CPP/76, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic7mc9mz Prompt: b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp:5:31: error: ‘n’ was not declared in th is scope     5 |     double res = log(x) / log(n);       |                               ^ b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp:5:27: error: ‘log’ was not declared in this scope; did you mean ‘long’?     5 |     double res = log(x) / log(n);       |                           ^~~       |                           long b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (res - (int)res) == 0;       |     ^~~~~~ b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp:9: b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp: In function ‘int main()’: b8ba0ca4-f218-43da-b16d-6bbf64d23867.cpp:11:13: error: ‘is_simple_power’ was not  declared in this scope    11 |     assert (is_simple_power(1, 12)==true);       |             ^~~~~~~~~~~~~~~

--- CPP_76.cpp
+++ CPP_76.cpp
@@ -1,6 +1,8 @@
-if (n <= 1) {
-        return false;
-    }
+bool is_simple_power(int x, int n) {
+    if (n == 1) return x == 1;
+    if (n == 0) return x == 1;
+    if (x == 1) return true;
+    
     double res = log(x) / log(n);
-    return (res == (int)res);
+    return (res - (int)res) == 0;
 }
