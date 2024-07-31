a2d76d1683912948b0c8f6c9b83deee2d5b7698b
beep boop <githubbot@vadim.me>
2024-07-31 15:25:12 +0200

    Solution 81 of Python/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zwkgv2m4 Prompt: Complete the following code given the task description and function signature.

--- Python_84.py
+++ Python_84.py
@@ -1,4 +1,2 @@
 def solve(N):
-    return bin(sum(int(x) for x in str(bin(N)[2:])))[2:].zfill(
-        len(str(bin(N)[2:])).replace("b", "")
-    )
+    return bin(sum(int(i) for i in str(bin(N)[2:])))[2:]
