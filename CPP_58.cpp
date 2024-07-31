c16e53f954b6556316d5637480a8970c7308acff
beep boop <githubbot@vadim.me>
2024-07-31 08:40:30 +0200

    Solution 24 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vemj32qk Prompt:       |                           ^~~~~~ 401ab281-dd4b-482f-b671-2b9df3e322ac.cpp:28:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 401ab281-dd4b-482f-b671-2b9df3e322ac.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 401ab281-dd4b-482f-b671-2b9df3e322ac.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 401ab281-dd4b-482f-b671-2b9df3e322ac.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 401ab281-dd4b-482f-b671-2b9df3e322ac.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 401ab281-dd4b-482f-b671-2b9df3e322ac.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -13,17 +13,14 @@ std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
     std::sort(l2.begin(), l2.end());
 
     std::vector<int> result;
-    std::set<int> set1(l1.begin(), l1.end());
-    for (int element : l2) {
-        if (set1.find(element) != set1.end()) {
-            result.push_back(element);
-        }
-    }
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
+
+    result.erase(std::unique(result.begin(), result.end()), result.end());
 
     return result;
 }
 
 int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    assert(issame(common({4, 3, 2, 8}, std::vector<int>{}), {}));
     return 0;
 }
