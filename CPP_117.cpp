64e854265968dedb0f4ec7cd9c6f153a8272b16a
beep boop <githubbot@vadim.me>
2024-07-31 09:45:00 +0200

    Solution 3 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/k5zrd6up Prompt:    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 603e1d73-adbb-42f9-ae37-5db9c2134bcd.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 603e1d73-adbb-42f9-ae37-5db9c2134bcd.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 603e1d73-adbb-42f9-ae37-5db9c2134bcd.cpp:29: 603e1d73-adbb-42f9-ae37-5db9c2134bcd.cpp: In function ‘int main()’: 603e1d73-adbb-42f9-ae37-5db9c2134bcd.cpp:39:20: error: ‘select_words’ was not de clared in this scope    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 603e1d73-adbb-42f9-ae37-5db9c2134bcd.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -5,39 +5,12 @@ bool issame(vector<string> a, vector<string> b) {
 vector<string> select_words(string s, int n);
 
 int main() {
-    string s;
+    string input_string;
     int n;
-    cin >> s >> n;
+    getline(cin, input_string);
+    cin >> n;
     
-    vector<string> words = select_words(s, n);
+    vector<string> selected_words = select_words(input_string, n);
     
     return 0;
-}
-
-vector<string> select_words(string s, int n) {
-    vector<string> result;
-    string word;
-    
-    for (char c : s) {
-        if (c == ' ') {
-            int consonant_count = count_if(word.begin(), word.end(), [](char x) {
-                return !strchr("aeiouAEIOU", x) && isalpha(x);
-            });
-            if (consonant_count == n) {
-                result.push_back(word);
-            }
-            word = "";
-        } else {
-            word += c;
-        }
-    }
-    
-    int consonant_count = count_if(word.begin(), word.end(), [](char x) {
-        return !strchr("aeiouAEIOU", x) && isalpha(x);
-    });
-    if (consonant_count == n) {
-        result.push_back(word);
-    }
-    
-    return result;
 }
