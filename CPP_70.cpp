6a1d4bfb15f7dca85315208065ab1c4f7ca43f3a
beep boop <githubbot@vadim.me>
2024-07-31 09:03:03 +0200

    Solution 73 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d9oacl39 Prompt:       |                           ^~~~~~ 0f56bb15-6174-47de-b705-50f183f9af98.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0f56bb15-6174-47de-b705-50f183f9af98.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0f56bb15-6174-47de-b705-50f183f9af98.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0f56bb15-6174-47de-b705-50f183f9af98.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0f56bb15-6174-47de-b705-50f183f9af98.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ 0f56bb15-6174-47de-b705-50f183f9af98.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,16 +2,16 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> strange_sort_list(std::vector<int> lst){
+std::vector<int> strange_sort_list(std::vector<int> lst) {
     std::sort(lst.begin(), lst.end());
     return lst;
 }
 
-int main(){
+int main() {
     assert(issame(strange_sort_list({111111}), {111111}));
     return 0;
 }
