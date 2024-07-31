3b87203d50754b2c0cfbec04a477a25f8c1c5f4e
beep boop <githubbot@vadim.me>
2024-07-31 09:43:54 +0200

    Solution 8 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3pq4ubz6 Prompt: on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 85a4d319-b1d0-4b56-b430-cf3906056912.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 85a4d319-b1d0-4b56-b430-cf3906056912.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 85a4d319-b1d0-4b56-b430-cf3906056912.cpp:30: 85a4d319-b1d0-4b56-b430-cf3906056912.cpp: In function ‘int main()’: 85a4d319-b1d0-4b56-b430-cf3906056912.cpp:40:20: error: ‘remove_duplicates’ was n ot declared in this scope    40 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 85a4d319-b1d0-4b56-b430-cf3906056912.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -18,7 +18,6 @@ vector<int> remove_duplicates(vector<int> numbers){
         }
     }
     return result;
-}
 
 int main() {
     assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1,4,5}));
