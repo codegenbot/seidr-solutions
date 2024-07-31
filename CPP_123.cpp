ba8375efeaacb8c3ca1ebeda5d169ca48cd01741
beep boop <githubbot@vadim.me>
2024-07-31 14:31:54 +0200

    Solution 88 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/azv1r5gd Prompt:  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d9554465-bcd6-4eed-9442-e01b1330f14e.cpp:11:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d9554465-bcd6-4eed-9442-e01b1330f14e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d9554465-bcd6-4eed-9442-e01b1330f14e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d9554465-bcd6-4eed-9442-e01b1330f14e.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d9554465-bcd6-4eed-9442-e01b1330f14e.cpp:10: d9554465-bcd6-4eed-9442-e01b1330f14e.cpp: In function ‘int main()’: d9554465-bcd6-4eed-9442-e01b1330f14e.cpp:20:20: error: ‘get_odd_collatz’ was not  declared in this scope    20 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -2,7 +2,21 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    std::sort(a.begin(), a.end());
-    std::sort(b.begin(), b.end());
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
     return a == b;
+}
+
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> collatz_seq;
+    while (n != 1) {
+        collatz_seq.push_back(n);
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+    }
+    collatz_seq.push_back(1); // Adding the last element which is 1
+    return collatz_seq;
 }
