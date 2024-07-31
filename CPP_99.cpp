753e9155a2edaebc1b7fe63e24d9b332a0fc5a35
beep boop <githubbot@vadim.me>
2024-07-31 13:26:49 +0200

    Solution 16 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/l9kr54qn Prompt:       |              std 55998921-49fb-444a-8849-fee1eccfcaba.cpp:2:17: error: ‘floor’ was not declared i n this scope     2 |     int lower = floor(num);       |                 ^~~~~ 55998921-49fb-444a-8849-fee1eccfcaba.cpp:3:17: error: ‘ceil’ was not declared in  this scope     3 |     int upper = ceil(num);       |                 ^~~~ 55998921-49fb-444a-8849-fee1eccfcaba.cpp:4:5: error: expected unqualified-id bef ore ‘return’     4 |     return (num - lower) < (upper - num) ? lower : upper;       |     ^~~~~~ 55998921-49fb-444a-8849-fee1eccfcaba.cpp:5:1: error: expected declaration before  ‘}’ token     5 | }       | ^ In file included from 55998921-49fb-444a-8849-fee1eccfcaba.cpp:7: 55998921-49fb-444a-8849-fee1eccfcaba.cpp: In function ‘int main()’: 55998921-49fb-444a-8849-fee1eccfcaba.cpp:9:13: error: ‘closest_integer’ was not declared in this scope     9 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,11 +1,8 @@
 #include <cmath>
-int closest_integer(std::string value) {
-    double num = stod(value);
+
+int closest_integer(const std::string& value) {
+    double num = std::stod(value);
     int lower = std::floor(num);
     int upper = std::ceil(num);
     return (num - lower) < (upper - num) ? lower : upper;
-}
-
-int main() {
-    assert(closest_integer("0") == 0);
 }
