3aa02898d504e2ced8b525aa130fb9195a25d919
beep boop <githubbot@vadim.me>
2024-07-31 13:02:12 +0200

    Solution 12 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/5c9um55e Prompt: 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp:12:9: error: expected constructor, dest ructor, or type conversion before ‘(’ token    12 |     sort(word.begin(), word.end());       |         ^ 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp:13:5: error: ‘result’ does not name a t ype    13 |     result += word;       |     ^~~~~~ 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return result;       |     ^~~~~~ 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp:17: 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp: In function ‘int main()’: 762ea1ae-3b0e-4be4-bfd1-e100726ff584.cpp:19:13: error: ‘anti_shuffle’ was not de clared in this scope    19 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <cassert>
-
-using namespace std;
-
 string anti_shuffle(string s) {
     string result = "";
     string word = "";
+    
     for (char c : s) {
         if (c == ' ') {
             sort(word.begin(), word.end());
@@ -17,16 +11,9 @@ string anti_shuffle(string s) {
             word += c;
         }
     }
-    if (!word.empty()) {
-        sort(word.begin(), word.end());
-        result += word;
-    }
+    
+    sort(word.begin(), word.end());
+    result += word;
+    
     return result;
-}
-
-int main() {
-    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
-    // Additional test cases can be added here
-
-    return 0;
 }
