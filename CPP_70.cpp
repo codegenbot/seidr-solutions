b7571d63e97e369bb564a821b2a1348c8c7dee6e
beep boop <githubbot@vadim.me>
2024-07-31 15:25:06 +0200

    Solution 87 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/eli9o2f6 Prompt:       |                    ^~~ dd34336e-d023-4a6d-b773-9179394bf4be.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ dd34336e-d023-4a6d-b773-9179394bf4be.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dd34336e-d023-4a6d-b773-9179394bf4be.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from dd34336e-d023-4a6d-b773-9179394bf4be.cpp:14: dd34336e-d023-4a6d-b773-9179394bf4be.cpp: In function ‘int main()’: dd34336e-d023-4a6d-b773-9179394bf4be.cpp:24:20: error: ‘strange_sort_list’ was n ot declared in this scope    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ dd34336e-d023-4a6d-b773-9179394bf4be.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
@@ -14,5 +14,16 @@ bool issame(vector<int> a, vector<int> b){
 }
 
 int main() {
-    // Main function implementation here
+    vector<int> strange_sort_list(vector<int> lst){
+        vector<int> result;
+        sort(lst.begin(), lst.end());
+        int left = 0, right = lst.size() - 1;
+        while (left <= right) {
+            result.push_back(lst[left++]);
+            if (left <= right) {
+                result.push_back(lst[right--]);
+            }
+        }
+        return result;
+    }
 }
