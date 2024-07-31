f94dcb664fb8c74bb8e82431e90bada6e262fafc
beep boop <githubbot@vadim.me>
2024-07-31 11:10:35 +0200

    Solution 61 of CPP/47, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/zlhuuu51 Prompt: d2aef181-d332-4206-9734-d747f6e07b36.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ d2aef181-d332-4206-9734-d747f6e07b36.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ d2aef181-d332-4206-9734-d747f6e07b36.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ d2aef181-d332-4206-9734-d747f6e07b36.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ d2aef181-d332-4206-9734-d747f6e07b36.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_47.cpp
+++ CPP_47.cpp
@@ -1,7 +1,14 @@
-```
 #include <algorithm>
 #include <vector>
 #include <iostream>
+
+double median(std::vector<int> numbers) {
+    std::sort(numbers.begin(), numbers.end());
+    if (numbers.size() % 2 == 0)
+        return (numbers.at(numbers.size()/2 - 1) + numbers.at(numbers.size()/2)) / 2.0;
+    else
+        return numbers.at(numbers.size()/2);
+}
 
 int main() {
     std::vector<int> numbers;
@@ -21,13 +28,3 @@ int main() {
     std::cout << "The median of the given array is: " << result << std::endl;
 
     return 0;
-
-}
-
-double median(std::vector<int> numbers) {
-    std::sort(numbers.begin(), numbers.end());
-    if (numbers.size() % 2 == 0)
-        return (numbers.at(numbers.size()/2 - 1) + numbers.at(numbers.size()/2)) / 2.0;
-    else
-        return numbers.at(numbers.size()/2);
-}
