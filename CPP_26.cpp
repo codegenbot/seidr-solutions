f94d6cbbf3d862dee57da23e09b73ee0fd183dbf
beep boop <githubbot@vadim.me>
2024-07-31 09:42:38 +0200

    Solution 1 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3pq4ubz6 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7a7b7c13-d614-4b3a-91e2-82aae532f09c.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7a7b7c13-d614-4b3a-91e2-82aae532f09c.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7a7b7c13-d614-4b3a-91e2-82aae532f09c.cpp:11: 7a7b7c13-d614-4b3a-91e2-82aae532f09c.cpp: In function ‘int main()’: 7a7b7c13-d614-4b3a-91e2-82aae532f09c.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 7a7b7c13-d614-4b3a-91e2-82aae532f09c.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,3 +1,8 @@
+```cpp
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
 vector<int> remove_duplicates(vector<int> numbers){
     vector<int> result;
     for(int num : numbers){
