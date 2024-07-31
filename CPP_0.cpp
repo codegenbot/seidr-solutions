5afc4be31782434e660176b086b601cbd0ec3753
beep boop <githubbot@vadim.me>
2024-07-31 17:59:53 +0200

    Solution 14 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/aeplitlt Prompt: 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp: In function ‘int main()’: 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp:27:5: error: ‘vector’ was not declared in this scope    27 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp:27:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4e7ef22c-310b-4eaa-895a-f1deef17c30f.cpp:27:12: error: expected primary-expressi on before ‘float’    27 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -17,7 +17,7 @@ bool has_close_elements(const std::vector<float>& numbers, float threshold) {
 
 int main() {
     std::vector<float> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    std::cassert(has_close_elements(a, 0.5) == false);
+    assert(has_close_elements(a, 0.5) == false);
 
     return 0;
 }
