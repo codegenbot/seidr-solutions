b7e886269a44d61e9d0cd64b3ea32d1065531adc
beep boop <githubbot@vadim.me>
2024-07-31 17:41:52 +0200

    Solution 43 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/rmq1lq6t Prompt:       |                    ^~~ 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp:16: 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp: In function ‘int main()’: 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp:26:20: error: ‘even_odd_count’ was not declared in this scope    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 4973a412-f4c5-4bea-aa5c-65c16c4de3f3.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,37 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cmath>
-
-using namespace std;
 
 bool issame(vector<int> a, vector<int> b);
 
-vector<int> even_odd_count(int num){
-    vector<int> result(2, 0);
-    string num_str = to_string(abs(num));
-    
-    for(char c : num_str){
-        if((c - '0') % 2 == 0){
+std::vector<int> even_odd_count(int num) {
+    std::vector<int> result(2, 0);
+    std::string num_str = std::to_string(std::abs(num));
+
+    for (char c : num_str) {
+        if ((c - '0') % 2 == 0) {
             result[0]++;
         } else {
             result[1]++;
         }
     }
-    
+
     return result;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a[0] == b[0] && a[1] == b[1];
-}
-
-int main() {
-    int num;
-    cin >> num;
-    vector<int> result = even_odd_count(num);
-
-    cout << result[0] << " " << result[1] << endl;
-
-    return 0;
 }
