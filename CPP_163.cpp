05ab3e13a50e1d0690d73580d0896fc849cc4a36
beep boop <githubbot@vadim.me>
2024-07-31 11:04:01 +0200

    Solution 32 of CPP/163, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6r1l81ou Prompt: c8604b3a-5056-424a-81fd-586670bdf724.cpp:29:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    29 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ c8604b3a-5056-424a-81fd-586670bdf724.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ c8604b3a-5056-424a-81fd-586670bdf724.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ c8604b3a-5056-424a-81fd-586670bdf724.cpp:12:5: note: ‘int main()’ previously def ined here    12 | int main(){       |     ^~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -3,19 +3,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b);
-
 vector<int> generate_integers(int a, int b);
-
-int main(){
-    assert(issame(generate_integers(17, 89), vector<int>{}));
-
-    return 0;
-}
-
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
 
 vector<int> generate_integers(int a, int b){
     vector<int> result;
