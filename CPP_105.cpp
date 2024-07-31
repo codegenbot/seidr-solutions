49733060bc30f0c54826b8164179f00cc6ce5a80
beep boop <githubbot@vadim.me>
2024-07-31 13:44:46 +0200

    Solution 50 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0x42macp Prompt:       |                                 ^~~~~~ In file included from 50104312-63b2-4846-927f-90018ee83591.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 50104312-63b2-4846-927f-90018ee83591.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 50104312-63b2-4846-927f-90018ee83591.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 50104312-63b2-4846-927f-90018ee83591.cpp:39: 50104312-63b2-4846-927f-90018ee83591.cpp: In function ‘int main()’: 50104312-63b2-4846-927f-90018ee83591.cpp:49:20: error: ‘by_length’ was not decla red in this scope    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 50104312-63b2-4846-927f-90018ee83591.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,19 +3,20 @@
 #include <map>
 #include <algorithm>
 #include <cassert>
+
 using namespace std;
 
-bool issame(const string& s1, const string& s2) {
-    return s1 == s2;
+bool issame(const vector<string>& vec1, const vector<string>& vec2){
+    return vec1 == vec2;
 }
 
 int main() {
-    vector<int> arr = {1, 5, 3, 2, 4, 8};
-    vector<string> result = by_length(arr);
-    for (const string& s : result) {
-        cout << s << " ";
-    }
-    cout << endl;
+    vector<int> numbers = {5, 2, 9, 4, 3, 8, 1, 7};
+    vector<string> result = by_length(numbers);
+  
+    vector<string> expected = {"Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"};
+
+    assert(issame(result, expected));
 
     return 0;
 }
