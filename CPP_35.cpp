8be09f8bde341bae9335bdb5f895691997a2638b
beep boop <githubbot@vadim.me>
2024-07-31 10:28:17 +0200

    Solution 17 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/d7x4f8kk Prompt: 1c91ddfd-8698-41da-9309-577d9984d26a.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 1c91ddfd-8698-41da-9309-577d9984d26a.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from 1c91ddfd-8698-41da-9309-577d9984d26a.cpp:25: 1c91ddfd-8698-41da-9309-577d9984d26a.cpp: In function ‘int main()’: 1c91ddfd-8698-41da-9309-577d9984d26a.cpp:27:17: error: ‘max_element’ was not dec lared in this scope; did you mean ‘std::max_element’?    27 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 1c91ddfd-8698-41da-9309-577d9984d26a.cpp:3: /usr/include/c++/11/pstl/glue_algorithm_defs.h:528:1: note: ‘std::max_element’ d eclared here   528 | max_element(_ExecutionPolicy&& __exec, _ForwardIterator __first, _Forwar dIterator __last);       | ^~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,26 +2,20 @@
 #include <vector>
 #include <algorithm>
 
-int maxElement(const std::vector<int>& vec) {
-    return *std::max_element(vec.begin(), vec.end());
+int n;
+std::cout << "Enter the number of elements: ";
+std::cin >> n;
+
+std::vector<int> input;
+input.resize(n, 0);
+
+for(int i = 0; i < n; i++) {
+    int temp;
+    std::cout << "Enter element " << (i+1) << ": ";
+    std::cin >> temp;
+    input.push_back(temp);
 }
 
-int main() {
-    int n;
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
+std::cout << "Maximum element is: " << *std::max_element(input.begin(), input.end()) << std::endl;
 
-    std::vector<int> input;
-    input.resize(n);
-
-    for(int i = 0; i < n; i++) {
-        int temp;
-        std::cout << "Enter element " << (i+1) << ": ";
-        std::cin >> temp;
-        input[i] = temp;
-    }
-
-    std::cout << "Maximum element is: " << maxElement(input) << std::endl;
-
-    return 0;
-}
+return 0;
