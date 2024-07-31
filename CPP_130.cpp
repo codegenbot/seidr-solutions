6cd63d72e0a48b8cfdfd1603dc19ea91ac14c9c7
beep boop <githubbot@vadim.me>
2024-07-31 17:03:35 +0200

    Solution 41 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/bdgmxa97 Prompt:       |                    ^~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:13: 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp: In function ‘int main()’: 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:23:20: error: ‘tri’ was not declared in  this scope    23 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -20,7 +20,7 @@ std::vector<int> tri(int n) {
         if (i % 2 == 0) {
             res.push_back(1 + i / 2);
         } else {
-            res.push_back(res[i - 1] + res[i - 2] + res[i + 1]);
+            res.push_back(res[i - 1] + res[i - 2] + res[i - 3]);
         }
     }
     return res;
