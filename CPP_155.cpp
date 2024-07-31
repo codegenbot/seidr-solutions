f32c82b69419337c0919e0910dc04e3999aa69cc
beep boop <githubbot@vadim.me>
2024-07-31 17:49:16 +0200

    Solution 79 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/rmq1lq6t Prompt:       |                    ^~~ 01f59594-6043-4b59-b821-41097a10c814.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 01f59594-6043-4b59-b821-41097a10c814.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 01f59594-6043-4b59-b821-41097a10c814.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 01f59594-6043-4b59-b821-41097a10c814.cpp:13: 01f59594-6043-4b59-b821-41097a10c814.cpp: In function ‘int main()’: 01f59594-6043-4b59-b821-41097a10c814.cpp:23:20: error: ‘even_odd_count’ was not declared in this scope    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 01f59594-6043-4b59-b821-41097a10c814.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -18,8 +18,3 @@ std::vector<int> even_odd_count(int num) {
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
-
-int main() {
-    // Your test cases here
-    return 0;
-}
