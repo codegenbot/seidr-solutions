3879c3cb09d52d0522ce4e7daa479926f0d03944
beep boop <githubbot@vadim.me>
2024-07-31 09:25:38 +0200

    Solution 7 of CPP/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2veasiic Prompt:       |                                    ^ bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp:10:11: error: ‘round’ was not declared in this scope    10 | int avg = round((double)sum / (m - n + 1));       |           ^~~~~ bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp:11:1: error: ‘string’ does not name a t ype    11 | string binary_avg = bitset<32>(avg).to_string();       | ^~~~~~ bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp:12:1: error: expected unqualified-id be fore ‘return’    12 | return binary_avg.substr(binary_avg.find('1'));       | ^~~~~~ bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp:15: bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp: In function ‘int main()’: bcbb23bd-1675-4c0c-9480-11dccb5ddb1c.cpp:17:13: error: ‘rounded_avg’ was not dec lared in this scope    17 |     assert (rounded_avg(5, 5) == "101");       |             ^~~~~~~~~~~

--- CPP_103.cpp
+++ CPP_103.cpp
@@ -3,17 +3,15 @@
 
 using namespace std;
 
-string fix_code(int n, int m) {
-    if (n > m) {
-        return "-1";
-    }
-
-    int sum = 0;
-    for (int i = n; i <= m; i++) {
-        sum += i;
-    }
-
-    int avg = round((double)sum / (m - n + 1));
-    string binary_avg = bitset<32>(avg).to_string();
-    return binary_avg.substr(binary_avg.find('1'));
+if (n > m) {
+    return "-1";
 }
+
+int sum = 0;
+for (int i = n; i <= m; i++) {
+    sum += i;
+}
+
+int avg = round((double)sum / (m - n + 1));
+string binary_avg = bitset<32>(avg).to_string();
+return binary_avg.substr(binary_avg.find('1'));
