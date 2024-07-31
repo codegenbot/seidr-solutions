b5cfa1f3ae85aae59c19451cc070eea661647722
beep boop <githubbot@vadim.me>
2024-07-31 07:57:30 +0200

    Solution 56 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6dbunna4 Prompt: ined here    24 | int main() {       |     ^~~~ In file included from 5426d0df-79d9-49ae-af0c-800546df9ae0.cpp:30: 5426d0df-79d9-49ae-af0c-800546df9ae0.cpp: In function ‘int main()’: 5426d0df-79d9-49ae-af0c-800546df9ae0.cpp:40:41: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    40 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 5426d0df-79d9-49ae-af0c-800546df9ae0.cpp:2: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 5426d0df-79d9-49ae-af0c-800546df9ae0.cpp:8:57: note:   initializing argument 1 o f ‘std::pair<float, float> find_closest_elements(std::vector<float>&)’     8 | pair<float, float> find_closest_elements(vector<float>& numbers) {       |                                          ~~~~~~~~~~~~~~~^~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-pair<float, float> find_closest_elements(vector<float> numbers) {
+pair<float, float> find_closest_elements(const vector<float>& numbers) {
     sort(numbers.begin(), numbers.end());
     float min_diff = numbers[1] - numbers[0];
     pair<float, float> closest_elements = make_pair(numbers[0], numbers[1]);
@@ -23,6 +23,6 @@ pair<float, float> find_closest_elements(vector<float> numbers) {
 
 int main() {
     assert(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) == make_pair(3.1, 4.1));
-    
+
     return 0;
 }
