007139aefb9f7bd9a7c912ce5afaec6401a98326
beep boop <githubbot@vadim.me>
2024-07-31 10:35:12 +0200

    Solution 71 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3iy35mq Prompt: /usr/include/c++/11/ext/new_allocator.h: In instantiation of ‘_Tp* __gnu_cxx::ne w_allocator<_Tp>::allocate(int, const void*) [with _Tp = double]’: /usr/include/c++/11/bits/alloc_traits.h:464:28:   required from ‘static _Tp* std ::allocator_traits<std::allocator<_CharT> >::allocate(std::allocator_traits<std: :allocator<_CharT> >::allocator_type&, int) [with _Tp = double; std::allocator_t raits<std::allocator<_CharT> >::pointer = double*; std::allocator_traits<std::al locator<_CharT> >::allocator_type = std::allocator<double>]’ /usr/include/c++/11/bits/stl_vector.h:346:33:   required from ‘std::_Vector_base <_Tp, _Alloc>::pointer std::_Vector_base<_Tp, _Alloc>::_M_allocate(size_t) [with  _Tp = double; _Alloc = std::allocator<double>; std::_Vector_base<_Tp, _Alloc>:: pointer = double*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {const double&}; _Tp = double; _Alloc = std::allocator<double>; std ::vector<_Tp, _Alloc>::iterator = std::vector<double>::iterator]’ /usr/include/c++/11/bits/stl_vector.h:1198:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(const value_type&) [with _Tp = double; _Alloc = std::al locator<double>; std::vector<_Tp, _Alloc>::value_type = double]’ 235801ba-c869-4c0a-a701-139f8b1dc52e.cpp:38:25:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<double>’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,49 +1,18 @@
-```cpp
 #include <iostream>
 #include <vector>
-using namespace std;
-
-double find_zero(vector<double> coeffs);
-double poly(vector<double> coeffs, double x);
+#include <cmath>
 
 int main() {
     int n;
-    cin >> n;
-    vector<double> coeffs;  
+    std::cin >> n;
+    std::vector<double> coeffs;  
     for (int i = 0; i < n; i++) {
-        double vals;
-        cin >> vals;
-        coeffs.push_back(vals); 
+        double val;
+        std::cin >> val;
+        coeffs.push_back(val); 
     }
     double solution = find_zero(coeffs); 
     assert(abs(poly(coeffs, solution)) < 1e-3);
-    cout << fixed << setprecision(2) << solution << endl;
+    std::cout << std::fixed << std::setprecision(2) << solution << std::endl;
     return 0;
-
-}
-
-double poly(vector<double> coeffs, double x) {
-    double sum = 0.0;
-    for (int i = 0; i < coeffs.size(); i++) {
-        sum += coeffs[i] * pow(x, i);
-    }
-    return sum;
-}
-
-double find_zero(vector<double> coeffs) {
-    double solution = 1.0;
-    double tolerance = 1e-6;
-    double max_iter = 100;
-
-    for (int i = 0; i < max_iter; i++) {
-        double new_solution = 0.0;
-        for (int j = 0; j < coeffs.size(); j++) {
-            new_solution += coeffs[j] * pow(solution, j);
-        }
-        if (abs(new_solution) < tolerance)
-            return solution;
-        solution = new_solution;
-    }
-
-    return -1.0;
 }
