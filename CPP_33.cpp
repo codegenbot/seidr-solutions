434db08453aff32588b6d5f95bd4fe54f5bd9009
beep boop <githubbot@vadim.me>
2024-07-31 12:22:55 +0200

    Solution 81 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2zg37ebo Prompt:       |                           ^~~~~~ 7ca46748-ed32-4698-8e62-1376a866400f.cpp:35:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 7ca46748-ed32-4698-8e62-1376a866400f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 7ca46748-ed32-4698-8e62-1376a866400f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 7ca46748-ed32-4698-8e62-1376a866400f.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7ca46748-ed32-4698-8e62-1376a866400f.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ 7ca46748-ed32-4698-8e62-1376a866400f.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -26,7 +26,7 @@ std::vector<int> sort_third(std::vector<int> l) {
 }
 
 int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), std::vector<int>({2, 6, 3, 4, 8, 9, 5, 1})));
-    
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), std::vector<int>({2, 6, 3, 4, 8, 9, 5, 1}));
+
     return 0;
 }
