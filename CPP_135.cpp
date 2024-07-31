565b12168c8f0eb82019c68be151e6a957493b89
beep boop <githubbot@vadim.me>
2024-07-31 14:26:57 +0200

    Solution 40 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/voai77aa Prompt: ic_string<char>*; size_t = long unsigned int]’ /usr/include/c++/11/bits/vector.tcc:440:44:   required from ‘void std::vector<_T p, _Alloc>::_M_realloc_insert(std::vector<_Tp, _Alloc>::iterator, _Args&& ...) [ with _Args = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allo cator<char> >}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator< std::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::iterator = std::ve ctor<std::__cxx11::basic_string<char> >::iterator]’ /usr/include/c++/11/bits/vector.tcc:121:21:   required from ‘std::vector<_Tp, _A lloc>::reference std::vector<_Tp, _Alloc>::emplace_back(_Args&& ...) [with _Args  = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char > >}; _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx 11::basic_string<char> >; std::vector<_Tp, _Alloc>::reference = std::__cxx11::ba sic_string<char>&]’ /usr/include/c++/11/bits/stl_vector.h:1204:21:   required from ‘void std::vector <_Tp, _Alloc>::push_back(std::vector<_Tp, _Alloc>::value_type&&) [with _Tp = std ::__cxx11::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_strin g<char> >; std::vector<_Tp, _Alloc>::value_type = std::__cxx11::basic_string<cha r>]’ 724ac3a0-01d1-4c07-9e05-95ebc28686c0.cpp:13:24:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::__cxx11::basic_string<char> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -1,7 +1,14 @@
+```cpp
 int can_arrange(std::vector<std::string> input) {
-    std::sort(input.begin(), input.end());
-    for(int i = 0; i < input.size() - 1; i++) {
-        if((input[i].size() + 1 != input[i+1].size()) || (input[i][input[i].size()-1] - '0' > input[i+1][0] - '0')) 
+    int count[10] = {0};
+    for (const auto& str : input) {
+        int num = std::stoi(str);
+        if (num < 1 || num > 9)
+            return -1;
+        count[num - 1]++;
+    }
+    for (int i = 0; i < 9; i++) {
+        if (count[i] != 1)
             return -1;
     }
     return 1;
