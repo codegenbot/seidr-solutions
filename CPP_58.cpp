8577cd7de61f76dd559384a0eb9acd994084a81a
beep boop <githubbot@vadim.me>
2024-07-31 12:50:23 +0200

    Solution 84 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/yvg8xybk Prompt: 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp:31:6: error: ambiguating new declaratio n of ‘bool issame(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp:7:13: note: old declaration ‘std::vecto r<int> issame(std::vector<int>, std::vector<int>)’     7 | vector<int> issame(vector<int> a, vector<int> b) {       |             ^~~~~~ 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp:30: 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp: In function ‘int main()’: 5c2e30c8-fa1e-42ad-8997-46397cba6741.cpp:40:20: error: ‘common’ was not declared  in this scope    40 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-vector<int> findCommonElements(vector<int> a, vector<int> b) {
+vector<int> find_common_elements(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
 
@@ -18,7 +18,7 @@ int main() {
     vector<int> l1 = {1, 2, 3, 4, 5};
     vector<int> l2 = {1, 3, 5, 7, 9};
 
-    vector<int> res = findCommonElements(l1, l2);
+    vector<int> res = find_common_elements(l1, l2);
 
     for (int num : res) {
         cout << num << " ";
