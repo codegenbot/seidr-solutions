97f8f2192e8198e67b647538ca5e6d9f3a27e6fa
beep boop <githubbot@vadim.me>
2024-07-31 09:43:13 +0200

    Solution 42 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/z79oxzny Prompt: d2979d9b-9729-40a7-be6a-5583f2dd4750.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ d2979d9b-9729-40a7-be6a-5583f2dd4750.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-vector<int> make_a_pile(int n) {
+vector<int> make_a_pile(int n){
     vector<int> stones;
     stones.push_back(n);
-    for (int i = 1; i < n; ++i) {
-        if (n % 2 == 0)
+    for(int i = 1; i < n; ++i){
+        if(n % 2 == 0)
             n += 2;
         else
             n += 1;
@@ -19,14 +19,4 @@ vector<int> make_a_pile(int n) {
 
 bool is_same(vector<int> a, vector<int> b) {
     return a == b;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> result = make_a_pile(n);
-    for (int stone : result) {
-        cout << stone << " ";
-    }
-    return 0;
 }
