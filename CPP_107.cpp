8d50bea61560c1fe36df382dc1c2de3c824d6795
beep boop <githubbot@vadim.me>
2024-07-31 16:09:31 +0200

    Solution 12 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/8mmi9kam Prompt: Complete the following code given the task description and function signature.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,13 +1,14 @@
-vector<int> countEvenOddPalindromes(int n) {
-            vector<int> res(2, 0);
-            for (int i = 1; i <= n; ++i) {
-                string num = to_string(i);
-                string rev = num;
-                reverse(rev.begin(), rev.end());
-                if (num == rev) {
-                    if (i % 2 == 0) res[0]++;
-                    else res[1]++;
-                }
-            }
-            return res;
+vector<int> result(2, 0);
+    for(int i=1; i<=n; i++){
+        string s = to_string(i);
+        string rev(s);
+        reverse(rev.begin(), rev.end());
+        if(s == rev){
+            if(i % 2 == 0)
+                result[0]++;
+            else
+                result[1]++;
         }
+    }
+    return result;
+}
