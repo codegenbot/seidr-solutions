602735f50143be352ac40951fece149e3b363859
beep boop <githubbot@vadim.me>
2024-07-31 09:00:00 +0200

    Solution 2 of Python/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/681tlryu Prompt: Complete the following code given the task description and function signature.

--- Python_137.py
+++ Python_137.py
@@ -1,16 +1,14 @@
 ```
 def compare_one(a, b):
     if isinstance(a, str) and isinstance(b, (int, float)):
-        return a if a.replace('.', '', 1).replace(',', '') < str(b) else b
+        a = [float(x.replace(',', '.')) for x in a.split('.')]
+        a = sum([x[1:] for x in a]) + float('0.' + a[0][:-1])
     elif isinstance(b, str) and isinstance(a, (int, float)):
-        return b if b.replace('.', '', 1).replace(',', '') < str(a) else a
-    elif isinstance(a, str):
-        return a if a.replace('.', '', 1).replace(',', '') > b.replace('.', '', 1).replace(',', '') else None
-    elif isinstance(b, str):
-        return b if b.replace('.', '', 1).replace(',', '') > a.replace('.', '', 1).replace(',', '') else None
-    elif a > b:
+        b = [float(x.replace(',', '.')) for x in b.split('.')]
+        b = sum([x[1:] for x in b]) + float('0.' + b[0][:-1])
+    if a > b:
         return a
-    elif a < b:
+    elif b > a:
         return b
     else:
         return None
