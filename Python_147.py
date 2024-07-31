6d10cbef29d35830127d3419da7a8f05e75a98df
beep boop <githubbot@vadim.me>
2024-07-31 07:43:03 +0200

    Solution 6 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/45stoybq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82f2c3fa-bf5a-4308-b2d0-a121b76b2 b67.py", line 20, in <module>     check(get_max_triples)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82f2c3fa-bf5a-4308-b2d0-a121b76b2 b67.py", line 17, in check     assert get_max_triples(100) == 53361            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/82f2c3fa-bf5a-4308-b2d0-a121b76b2 b67.py", line 9, in get_max_triples     while k >= i:           ^ UnboundLocalError: cannot access local variable 'k' where it is not associated w ith a value

--- Python_147.py
+++ Python_147.py
@@ -4,10 +4,11 @@ def get_max_triples(n):
     max_sum = 0
     for i in range(len(a)):
         for j in range(i, len(a)):
-            if j > i:
-                k = j - (j-i)
-            total = a[i] + a[j] + a[k]
-            if total % 3 == 0:
-                count += 1
-                max_sum = max(max_sum, total)
+            k = j - i
+            while k >= i:
+                total = a[i] + a[j] + a[k]
+                if total % 3 == 0:
+                    count += 1
+                    max_sum = max(max_sum, total)
+                k -= 1
     return count, max_sum
