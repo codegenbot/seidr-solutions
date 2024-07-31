6d2da971344a087391757e57d11d91687722a4a1
beep boop <githubbot@vadim.me>
2024-07-31 19:03:47 +0200

    Solution 15 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/hp1oe9tv Prompt:       |     ^~~~ In file included from a047a076-7cea-4149-a100-47c0c72b82da.cpp:22: a047a076-7cea-4149-a100-47c0c72b82da.cpp: In function ‘int main()’: a047a076-7cea-4149-a100-47c0c72b82da.cpp:24:13: error: ‘boost’ has not been decl ared    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ a047a076-7cea-4149-a100-47c0c72b82da.cpp:24:29: error: ‘string’ was not declared  in this scope    24 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ a047a076-7cea-4149-a100-47c0c72b82da.cpp:24:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from a047a076-7cea-4149-a100-47c0c72b82da.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from a047a076-7cea-4149-a100-47c0c72b82da.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,20 +1,20 @@
 #include <any>
-#include <string>
+#include <string.h>
 #include <cassert>
 #include <algorithm>
 
 std::any compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
-        return std::max(std::any_cast<int>(a), std::any_cast<int>(b));
+        return std::any(std::max(std::any_cast<int>(a), std::any_cast<int>(b)));
     } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
-        return std::max(std::any_cast<float>(a), std::any_cast<float>(b));
+        return std::any(std::max(std::any_cast<float>(a), std::any_cast<float>(b)));
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        return std::max(std::stof(std::any_cast<std::string>(a)), std::stof(std::any_cast<std::string>(b)));
+        return std::any(std::max(std::stof(std::any_cast<std::string>(a)), std::stof(std::any_cast<std::string>(b))));
     }
     return std::any();
 }
 
 int main() {
-    assert(std::any_cast<float>(compare_one(1.5f, 2.3f)) == 2.3f);
+    assert(std::any_cast<float>(std::any_cast<std::any>(compare_one(1.5f, 2.3f)).type() == typeid(float));
     return 0;
 }
