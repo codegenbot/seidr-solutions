10147c1228e7821447df4a77f70b9dbc5087b35e
beep boop <githubbot@vadim.me>
2024-07-31 15:45:01 +0200

    Solution 44 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/tq42c3it Prompt: Complete the following code given the task description and function signature.

--- Python_93.py
+++ Python_93.py
@@ -3,16 +3,10 @@ def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if char.islower():
-                if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
-                    result += chr((ord(char) - 97 + 3) % 26 + 97)
-                else:
-                    result += char.upper()
-            elif char.isupper():
-                if char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U':
-                    result += chr((ord(char) - 65 + 3) % 26 + 65)
-                else:
-                    result += char.lower()
+            if char.isupper():
+                result += chr((ord(char) - 65 + 2) % 26 + 65)
+            else:
+                result += chr((ord(char) - 97 + 2) % 26 + 97)
         else:
             result += char
     return result
