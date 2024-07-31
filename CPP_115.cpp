406111d6500cd4f6c8bd9826131c2e22dda45f92
beep boop <githubbot@vadim.me>
2024-07-31 16:41:09 +0200

    Solution 70 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lxwxx5js Prompt:       |                                   ^~ dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp:11:44: error: ‘capacity’ was not declar ed in this scope    11 |         int buckets_needed = total_water / capacity;       |                                            ^~~~~~~~ dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp:12:9: error: expected unqualified-id be fore ‘if’    12 |         if (total_water % capacity != 0) {       |         ^~ dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp:16:9: error: expected unqualified-id be fore ‘return’    16 |         return buckets_needed;       |         ^~~~~~ dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp:17:5: error: expected declaration befor e ‘}’ token    17 |     }       |     ^ In file included from dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp:19: dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp: In function ‘int main()’: dbf84dfb-61c5-4bad-a6ab-2e34d6bd7de3.cpp:21:13: error: ‘max_fill’ was not declar ed in this scope    21 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ^~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -3,17 +3,17 @@ int max_fill(vector<vector<int>>& grid, int capacity) {
     int cols = grid[0].size();
     int total_water = 0;
     int buckets_needed = 0;
-    
+
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             total_water += grid[i][j];
         }
     }
-    
+
     buckets_needed = total_water / capacity;
     if (total_water % capacity != 0) {
         buckets_needed++;
     }
-    
+
     return buckets_needed;
 }
