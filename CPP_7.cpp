7ade8145be6413b320ed97bdf3965fb0c75abfe4
beep boop <githubbot@vadim.me>
2024-07-31 07:28:44 +0200

    Solution 24 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6dt4y4mw Prompt:    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 6a46e158-3e25-485d-a632-c14ddfb3f022.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 6a46e158-3e25-485d-a632-c14ddfb3f022.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 6a46e158-3e25-485d-a632-c14ddfb3f022.cpp:30: 6a46e158-3e25-485d-a632-c14ddfb3f022.cpp: In function ‘int main()’: 6a46e158-3e25-485d-a632-c14ddfb3f022.cpp:40:20: error: ‘filter_by_substring’ was  not declared in this scope    40 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 6a46e158-3e25-485d-a632-c14ddfb3f022.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -3,9 +3,7 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
-
-bool is_same(string a, string b){
+bool is_same_string(string a, string b){
     return a == b;
 }
 
@@ -20,10 +18,9 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
 }
 
 int main() {
-    assert(is_same("apple", "apple"));
-    assert(!is_same("apple", "banana"));
+    vector<string> filtered_strings = filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run");
 
-    assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == vector<string>{"grunt", "prune"});
+    assert(filtered_strings == vector<string>{"grunt", "prune"});
 
     return 0;
 }
