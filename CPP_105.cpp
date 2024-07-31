9550f78b24b15c4515081b9cfd2430a825537a25
beep boop <githubbot@vadim.me>
2024-07-31 15:20:06 +0200

    Solution 75 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jcyebif8 Prompt:       |                              ^~~~~~ f49e29db-3854-4104-b741-7261ecd2028e.cpp:21:37: error: ‘string’ was not declared  in this scope    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f49e29db-3854-4104-b741-7261ecd2028e.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f49e29db-3854-4104-b741-7261ecd2028e.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f49e29db-3854-4104-b741-7261ecd2028e.cpp:20: f49e29db-3854-4104-b741-7261ecd2028e.cpp: In function ‘int main()’: f49e29db-3854-4104-b741-7261ecd2028e.cpp:30:20: error: ‘by_length’ was not decla red in this scope    30 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ f49e29db-3854-4104-b741-7261ecd2028e.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,13 +1,12 @@
+bool issame(vector<string> a, vector<string> b) {
+    return a == b;
+}
+
 vector<string> sortStrings(vector<string> arr) {
     sort(arr.begin(), arr.end());
     return arr;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
 int main() {
     assert(issame(sortStrings(by_length({9, 4, 8})), sortStrings({"Nine", "Eight", "Four"})));
-    // rest of your code...
 }
