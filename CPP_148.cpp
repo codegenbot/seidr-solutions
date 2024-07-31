9514fcf35a44d14c3312e2600955705f3ce2e0f5
beep boop <githubbot@vadim.me>
2024-07-31 10:31:53 +0200

    Solution 8 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ygx48vwa Prompt:       |                              ^~~~~~ a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp:29: a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp: In function ‘int main()’: a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ a7854e84-0cbf-457b-8a5c-1b204955ce75.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,14 +1,26 @@
 #include <vector>
-#include <string>
+#include <algorithm>
 
-bool issame(vector<string> a, vector<string> b){
-    // Implementation of function
-}
-
-vector<string> bf(string planet1, string planet2) {
-    // Implementation of function
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 int main() {
-    // Test cases
+    vector<string> planetList = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    vector<string> planetsBetween = bf("Venus", "Mars");
+    vector<string> expectedPlanets = {"Earth"};
+
+    assert(issame(planetsBetween, expectedPlanets));
+
+    return 0;
 }
