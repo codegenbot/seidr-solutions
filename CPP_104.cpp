a4704fc58969554d379f72e67626b8ee05c72301
beep boop <githubbot@vadim.me>
2024-07-31 13:40:47 +0200

    Solution 46 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9jmpqyhl Prompt:       |             ^~~~~~ 4b67b605-0b84-4b4f-a8e3-1bf6af4194e8.cpp:38:20: error: expected primary-expressi on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 4b67b605-0b84-4b4f-a8e3-1bf6af4194e8.cpp:38:27: error: ‘vector’ was not declared  in this scope    38 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4b67b605-0b84-4b4f-a8e3-1bf6af4194e8.cpp:38:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 4b67b605-0b84-4b4f-a8e3-1bf6af4194e8.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 4b67b605-0b84-4b4f-a8e3-1bf6af4194e8.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4b67b605-0b84-4b4f-a8e3-1bf6af4194e8.cpp:38:34: error: expected primary-expressi on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::std::vector<int> a, std::std::vector<int> b){
     if (a.size() != b.size()) {
         return false;
     }
@@ -12,8 +12,8 @@ bool issame(std::vector<int> a, std::vector<int> b){
     return true;
 }
 
-std::vector<int> unique_digits(std::vector<int> x){
-    std::vector<int> result;
+std::std::vector<int> unique_digits(std::std::vector<int> x){
+    std::std::vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
