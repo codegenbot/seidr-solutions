f41590fa468727f6d7621a3d6c3be44b38015a36
beep boop <githubbot@vadim.me>
2024-07-31 10:40:12 +0200

    Solution 35 of CPP/33, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/6grvlc1f Prompt: 82ad8186-beb8-4c0a-a908-14fb2090dc0f.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 82ad8186-beb8-4c0a-a908-14fb2090dc0f.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 82ad8186-beb8-4c0a-a908-14fb2090dc0f.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 82ad8186-beb8-4c0a-a908-14fb2090dc0f.cpp:32:13: error: redefinition of ‘std::vec tor<int> sort_third(std::vector<int>)’    32 | vector<int> sort_third(vector<int> l) {       |             ^~~~~~~~~~ 82ad8186-beb8-4c0a-a908-14fb2090dc0f.cpp:8:13: note: ‘std::vector<int> sort_thir d(std::vector<int>)’ previously defined here     8 | vector<int> sort_third(vector<int> l) {       |             ^~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -6,53 +6,24 @@ using namespace std;
 
 vector<int> sort_third(vector<int> l) {
     vector<int> result;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 3 == 0) {
-            vector<int> temp;
-            for (int j = i; j < l.size() && j % 3 == 0; j++) {
-                temp.push_back(l[j]);
-            }
-            sort(temp.begin(), temp.end());
-            for (int k = 0; k < temp.size(); k++) {
-                result.push_back(temp[k]);
-            }
-        } else {
+    int i = 0; 
+
+    for (i = 0; i < l.size(); ) {
+        if ((i + 1) % 3 == 0 && !result.empty()) { 
+            sort(result.begin(), result.end());
+            result.clear(); 
+        }
+        
+        if (!result.empty() || i % 3 != 0) {
             result.push_back(l[i]);
         }
+        i++;
     }
+    
+    // add one last time just in case
+    if (!result.empty()) {
+        sort(result.begin(), result.end());
+    }
+
     return result;
-}
-
-int main() {
-    vector<int> l1 = {1, 2, 3};
-    vector<int> l2 = {5, 6, 3, 4, 8, 9, 2};
-
-    cout << "l1: ";
-    for (int i : l1) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "l2: ";
-    for (int i : l2) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    vector<int> result1 = sort_third(l1);
-    vector<int> result2 = sort_third(l2);
-
-    cout << "result1: ";
-    for (int i : result1) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "result2: ";
-    for (int i : result2) {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    return 0;
 }
