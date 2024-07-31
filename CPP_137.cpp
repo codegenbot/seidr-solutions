a8d09a8f2df45da85307b592658d94b27913918a
beep boop <githubbot@vadim.me>
2024-07-31 10:43:09 +0200

    Solution 82 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic89b93p Prompt: a81cc54a-04c0-4d66-88b8-eefb84b594aa.cpp:23:29: error: ‘string’ was not declared  in this scope    23 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ a81cc54a-04c0-4d66-88b8-eefb84b594aa.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a81cc54a-04c0-4d66-88b8-eefb84b594aa.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from a81cc54a-04c0-4d66-88b8-eefb84b594aa.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <string>
-#include <any>
 #include <cassert>
 
 template <typename T>
-T compare_one(const T& a, const T& b) {
+std::string compare_one(const std::string& a, const std::string& b) {
     if (a > b) {
         return a;
     } else if (a < b) {
         return b;
     }
-    return T();
+    return "None";
 }
 
 int main() {
-    assert(compare_one<std::string>("1", std::to_string(1)) == "");
-
+    assert(compare_one(std::string("1"), std::to_string(1)) == "None");
+    
     return 0;
 }
