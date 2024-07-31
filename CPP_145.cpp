6b32c8eedfcd7df004988e5115f7612c71fcd0fd
beep boop <githubbot@vadim.me>
2024-07-31 17:26:09 +0200

    Solution 27 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nuhxd9r6 Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 82aab3ac-6a5b-4fe0-bd37-14f0c45f149f.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 82aab3ac-6a5b-4fe0-bd37-14f0c45f149f.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 82aab3ac-6a5b-4fe0-bd37-14f0c45f149f.cpp:19: 82aab3ac-6a5b-4fe0-bd37-14f0c45f149f.cpp: In function ‘int main()’: 82aab3ac-6a5b-4fe0-bd37-14f0c45f149f.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 82aab3ac-6a5b-4fe0-bd37-14f0c45f149f.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 
 vector<int> order_by_points(vector<int> nums) {
     sort(nums.begin(), nums.end(), [](int a, int b) {
@@ -13,11 +14,11 @@ vector<int> order_by_points(vector<int> nums) {
             temp_b /= 10;
         }
         if (sum_a == sum_b) {
-            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
+            return a < b;
         }
         return sum_a < sum_b;
     });
     return nums;
 }
 
-assert(order_by_points({0,6,6,-76,-21,23,4}) == vector<int>{-76, -21, 0, 4, 23, 6, 6});
+assert(order_by_points({0, 6, 6, -76, -21, 23, 4}) == vector<int>{-76, -21, 0, 4, 23, 6, 6});
