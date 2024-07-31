bc6dbaf3846d481a02b59942a31752fd1cab6052
beep boop <githubbot@vadim.me>
2024-07-31 16:10:58 +0200

    Solution 2 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/tasd1fh5 Prompt: Complete the following code given the task description and function signature.

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -3,8 +3,13 @@ int count_nums(vector<int> n){
     for (int num : n) {
         int sum = 0;
         int temp = num;
-        while (temp != 0) {
-            sum += abs(temp % 10);
+        if (temp < 0) {
+            temp *= -1;
+            sum -= temp % 10;
+            temp /= 10;
+        }
+        while (temp > 0) {
+            sum += temp % 10;
             temp /= 10;
         }
         if (sum > 0) {
