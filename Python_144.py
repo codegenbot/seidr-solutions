465d70632a9f7a42fd8ab14186b368970ce5120e
beep boop <githubbot@vadim.me>
2024-07-31 09:59:55 +0200

    Solution 93 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0cl83asd Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6c2353e8-aecf-4891-bb9a-e7d6e4503 470.py", line 1     ```python     ^ SyntaxError: invalid syntax

--- Python_144.py
+++ Python_144.py
@@ -1,12 +1,21 @@
 ```
+import math
+
 def simplify():
     numerator1 = int(input("Enter first fraction numerator: "))
     denominator1 = int(input(f"Enter first fraction denominator: "))
 
     numerator2 = int(input(f"Enter second fraction numerator: "))
-    denominator2 = int(input(f"Enter second fraction denominator: "))
+    denominator22 = int(input(f"Enter second fraction denominator: "))
 
-    new_numerator = math.gcd(numerator1*denominator2, numerator2*denominator1)
-    new_denominator = math.gcd(denominator1 * denominator2)
+    new_numerator = numerator1*denominator22
+    new_denominator = denominator1 * denominator22
 
-    return """{}""".format(f"{new_numerator}/{new_denominator}")
+    greatest_common_divisor = math.gcd(new_numerator, new_denominator)
+
+    simplified_numerator = new_numerator // greatest_common_divisor
+    simplified_denominator = new_denominator // greatest_common_divisor
+
+    return f"{simplified_numerator}/{simplified_denominator}"
+
+print(simplify())
