ea6854604d91286e0fa1bd7221a275b5c77855c2
beep boop <githubbot@vadim.me>
2024-07-31 07:33:34 +0200

    Solution 46 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1gr15hyt Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘float’ and ‘const float’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -13,7 +13,7 @@ bool has_close_elements(const std::vector<float>& numbers, float threshold) {
 }
 
 int main() {
-    std::vector<float> a = {1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
-    assert(has_close_elements(a, 0.5f) == false);
+    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
+    assert(has_close_elements(std::vector<float>{1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5f) == false);
     return 0;
 }
