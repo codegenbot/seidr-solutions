9008800c740e9c810d1ac343015339cfeaa4b343
beep boop <githubbot@vadim.me>
2024-07-31 08:20:43 +0200

    Solution 54 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/26vdj7a8 Prompt: vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});       |     ^~~~~~ 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp: At global scope: 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp:39: 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp: In function ‘int main()’: 41f68aae-8c79-43c3-b45b-5bbf555faee1.cpp:49:29: error: invalid use of void expre ssion    49 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -32,6 +32,5 @@ vector<float> sort_even_elements(vector<float> l) {
 
 int main() {
     assert(sort_even_elements({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) == vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
-
     return 0;
 }
