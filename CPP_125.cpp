ca036c16520e3cb6f1b61749165d2710ffdfc82f
beep boop <githubbot@vadim.me>
2024-07-31 14:24:16 +0200

    Solution 49 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6qw5jxbx Prompt:       |                                 ^~~~~~ In file included from 9b83d8b7-bf57-4689-b9e7-699092441924.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9b83d8b7-bf57-4689-b9e7-699092441924.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9b83d8b7-bf57-4689-b9e7-699092441924.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9b83d8b7-bf57-4689-b9e7-699092441924.cpp:25: 9b83d8b7-bf57-4689-b9e7-699092441924.cpp: In function ‘int main()’: 9b83d8b7-bf57-4689-b9e7-699092441924.cpp:35:20: error: ‘split_words’ was not dec lared in this scope    35 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 9b83d8b7-bf57-4689-b9e7-699092441924.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -5,16 +5,16 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
+bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-vector<string> split_words(std::pmr::string txt) {
+vector<string> split_words(std::pmr::string txt){
     vector<string> result;
     string word = "";
-    for (char c : txt) {
-        if (c == ' ' || c == ',') {
-            if (!word.empty()) {
+    for(char c : txt){
+        if(c == ' ' || c == ','){
+            if(!word.empty()){
                 result.push_back(word);
                 word = "";
             }
@@ -22,16 +22,16 @@ vector<string> split_words(std::pmr::string txt) {
             word += c;
         }
     }
-    if (!word.empty()) {
+    if(!word.empty()){
         result.push_back(word);
     }
-    if (result.size() == 0) {
-        result.push_back(to_string(count_if(txt.begin(), txt.end(), [](char c) { return islower(c) && (c - 'a') % 2 == 1; })));
+    if(result.size() == 0){
+        result.push_back(to_string(count_if(txt.begin(), txt.end(), [](char c){return islower(c) && (c-'a') % 2 == 1;})));
     }
     return result;
 }
 
-int main() {
-    assert(issame(split_words(""), { "0" }));
+int main(){
+    assert(issame(split_words(""), {"0"}));
     return 0;
 }
