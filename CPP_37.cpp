f2667aae6cba92a626c869731a1f7e8afe691c4e
beep boop <githubbot@vadim.me>
2024-07-31 08:17:38 +0200

    Solution 38 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/26vdj7a8 Prompt: 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp:44:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    44 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp:8:6: note: ‘bool issame(std::vector<flo at>, std::vector<float>)’ previously defined here     8 | bool issame(std::vector<float> a, std::vector<float> b) {       |      ^~~~~~ 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~ In file included from 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp:43: 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp: In function ‘int main()’: 11ec1e42-d9f2-44b2-b3ce-6a10b792ee2e.cpp:53:29: error: invalid use of void expre ssion    53 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -5,9 +5,9 @@
 
 using namespace std;
 
-void sort_even(vector<float>& l) {
+void sort_even(const vector<float>& l) {
     vector<float> even_values;
-
+    
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_values.push_back(l[i]);
@@ -15,7 +15,7 @@ void sort_even(vector<float>& l) {
     }
 
     sort(even_values.begin(), even_values.end());
-
+    
     int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -25,11 +25,12 @@ void sort_even(vector<float>& l) {
     }
 }
 
+vector<float> sort_even_elements(vector<float> l) {
+    sort_even(l);
+    return l;
+}
+
 int main() {
-    vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
-    sort_even(input);
-
-    assert(input == vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
-
+    assert(sort_even_elements({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) == vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
     return 0;
 }
