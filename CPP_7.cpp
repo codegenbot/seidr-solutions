b8bcd462068b38cf6f16b3204d07bb7557639549
beep boop <githubbot@vadim.me>
2024-07-31 11:21:20 +0200

    Solution 47 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/oelal4sy Prompt: 948164e5-6f4e-4f4e-825f-d85c8a030805.cpp:30:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    30 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 948164e5-6f4e-4f4e-825f-d85c8a030805.cpp:20:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    20 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 948164e5-6f4e-4f4e-825f-d85c8a030805.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 948164e5-6f4e-4f4e-825f-d85c8a030805.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -16,10 +16,6 @@ vector<string> filter_by_substring(vector<string> strings, string substring) {
 }
 
 int main() {
-    vector<string> a = {"grunt", "trumpet", "prune", "gruesome"};
-    vector<string> b = filter_by_substring(a, "run");
-    
-    assert(a == b);
-    
+    assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == vector<string>{"grunt", "prune"});
     return 0;
 }
