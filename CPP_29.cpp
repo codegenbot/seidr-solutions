d5439a637c1a1bab22e9a58412c5231e001b11a6
beep boop <githubbot@vadim.me>
2024-07-31 08:03:32 +0200

    Solution 73 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d15gsk7m Prompt:       |                                     ^~~~~~ 27da9463-244d-4219-b536-7d9936688c7b.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 27da9463-244d-4219-b536-7d9936688c7b.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 27da9463-244d-4219-b536-7d9936688c7b.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 27da9463-244d-4219-b536-7d9936688c7b.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 27da9463-244d-4219-b536-7d9936688c7b.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 27da9463-244d-4219-b536-7d9936688c7b.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -17,4 +17,7 @@ std::vector<std::string> filter_by_prefix(const std::vector<std::string>& string
     return filtered_strings;
 }
 
-assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+int main() {
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+    return 0;
+}
