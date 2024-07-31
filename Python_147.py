80be44225f89e0d59501da018436c4ef2e4a8eaa
beep boop <githubbot@vadim.me>
2024-07-31 08:15:08 +0200

    Solution 95 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/45stoybq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/77f4da36-d950-4a11-9ec8-77283e2e9 9d1.py", line 22, in <module>     check(get_max_triples)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/77f4da36-d950-4a11-9ec8-77283e2e9 9d1.py", line 19, in check     assert get_max_triples(100) == 53361            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_147.py
+++ Python_147.py
@@ -1,17 +1,14 @@
-```
 def get_max_triples(n):
-    max_count = 0
-    max_total = 0
+    max_sum = 0
     for k in range(1, n + 1):
         a = k * k - k + 1
         d = 2 * k - 1
-        total = 0
-        triples_count = 0
-        for i in range(k):
-            total += (d*i) + a 
-        if total > max_total:
-            max_count = 1
-            max_total = total
-        elif total == max_total:
-            max_count += 1
-    return max_count, max_total
+        total = sum((d*i) + a for i in range(k))
+        if total > max_sum:
+            max_sum = total
+            triples_count = 1
+        elif total == max_sum:
+            triples_count += 1
+        else:
+            triples_count = 0
+    return max_sum, triples_count
