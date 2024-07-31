8861448abb3d9ad1f75e108eff2f0cc083ea91c9
beep boop <githubbot@vadim.me>
2024-07-31 13:36:27 +0200

    Solution 55 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/613gihaj Prompt:    30 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 5789e212-f7e3-4c7e-88c5-3ada5fcc904c.cpp:30:27: error: ‘vector’ was not declared  in this scope    30 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5789e212-f7e3-4c7e-88c5-3ada5fcc904c.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 5789e212-f7e3-4c7e-88c5-3ada5fcc904c.cpp:29: 5789e212-f7e3-4c7e-88c5-3ada5fcc904c.cpp: In function ‘int main()’: 5789e212-f7e3-4c7e-88c5-3ada5fcc904c.cpp:39:20: error: ‘count_up_to’ was not dec lared in this scope    39 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 5789e212-f7e3-4c7e-88c5-3ada5fcc904c.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -2,7 +2,9 @@
 
 vector<int> count_up_to(int n);
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
 vector<int> count_up_to(int n) {
     vector<int> result;
@@ -28,11 +30,6 @@ vector<int> count_up_to(int n) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 int main() {
     assert(issame(count_up_to(101), vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    return 0;
 }
