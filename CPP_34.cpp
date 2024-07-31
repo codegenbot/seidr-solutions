6434409205b06d2a73e29c892cc106abfc646a5f
beep boop <githubbot@vadim.me>
2024-07-31 10:24:34 +0200

    Solution 18 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/m2yq3lcb Prompt: t>; _Compare = std::less<int>; _Alloc = std::allocator<int>; std::_Rb_tree<_Key,  _Val, _KeyOfValue, _Compare, _Alloc>::iterator = std::_Rb_tree<int, int, std::_ Identity<int>, std::less<int>, std::allocator<int> >::iterator; std::_Rb_tree<_K ey, _Val, _KeyOfValue, _Compare, _Alloc>::const_iterator = std::_Rb_tree<int, in t, std::_Identity<int>, std::less<int>, std::allocator<int> >::const_iterator]’ /usr/include/c++/11/bits/stl_tree.h:1102:23:   required from ‘std::__enable_if_t <std::is_same<_Val, typename std::iterator_traits<_InputIterator>::value_type>:: value> std::_Rb_tree<_Key, _Val, _KeyOfValue, _Compare, _Alloc>::_M_insert_range _unique(_InputIterator, _InputIterator) [with _InputIterator = __gnu_cxx::__norm al_iterator<int*, std::vector<int> >; _Key = int; _Val = int; _KeyOfValue = std: :_Identity<int>; _Compare = std::less<int>; _Alloc = std::allocator<int>; std::_ _enable_if_t<std::is_same<_Val, typename std::iterator_traits<_InputIterator>::v alue_type>::value> = void; typename std::iterator_traits<_InputIterator>::value_ type = int]’ /usr/include/c++/11/bits/stl_set.h:193:31:   required from ‘std::set<_Key, _Comp are, _Alloc>::set(_InputIterator, _InputIterator) [with _InputIterator = __gnu_c xx::__normal_iterator<int*, std::vector<int> >; _Key = int; _Compare = std::less <int>; _Alloc = std::allocator<int>]’ 4bda7214-f792-47cb-92fe-6e9a9487963c.cpp:10:34:   required from here /usr/include/c++/11/ext/new_allocator.h:111:42: error: ‘class __gnu_cxx::new_all ocator<std::_Rb_tree_node<int> >’ has no member named ‘_M_max_size’   111 |         if (__builtin_expect(__n > this->_M_max_size(), false))       |                                    ~~~~~~^~~~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,17 +1,22 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
-vector<int> unique(vector<int> l) {
-    l.erase(unique(l.begin(), l.end()), l.end());
-    return l;
+set<int> unique(vector<int> l) {
+    set<int> s(l.begin(), l.end());
+    return s;
 }
 
 int main() {
     vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    l = unique(l);
-    for(int i: l) cout << i << " ";
+    std::sort(l.begin(), l.end());
+    auto end = std::unique(l.begin(), l.end());
+    l.erase(end, l.end());
+    set<int> result(l.begin(), l.end());
+    for(int i: result) cout << i << " ";
     return 0;
 }
