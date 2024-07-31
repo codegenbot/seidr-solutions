3972aed0e4779ac4636c813807951431f6b1cbda
beep boop <githubbot@vadim.me>
2024-07-31 07:49:43 +0200

    Solution 16 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6dbunna4 Prompt:     5 | a02ff132-f830-46c6-9a2b-ed20beae63be.cpp:43:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    43 | int main(){       |         ^~ a02ff132-f830-46c6-9a2b-ed20beae63be.cpp:43:9: note: remove parentheses to defau lt-initialize a variable    43 | int main(){       |         ^~       |         -- a02ff132-f830-46c6-9a2b-ed20beae63be.cpp:43:9: note: or replace parentheses with  braces to value-initialize a variable a02ff132-f830-46c6-9a2b-ed20beae63be.cpp:43:11: error: a function-definition is not allowed here before ‘{’ token    43 | int main(){       |           ^ a02ff132-f830-46c6-9a2b-ed20beae63be.cpp:45:2: error: expected ‘}’ at end of inp ut    45 | }       |  ^ a02ff132-f830-46c6-9a2b-ed20beae63be.cpp:24:12: note: to match this ‘{’    24 | int main() {       |            ^

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <utility>
 
 using namespace std;
 
@@ -27,5 +28,6 @@ int main() {
     };
 
     assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), make_pair(3.1, 4.1));
+
     return 0;
 }
