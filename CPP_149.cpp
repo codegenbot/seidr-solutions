230924196c927477e3627bd841ac56f8471bd9da
beep boop <githubbot@vadim.me>
2024-07-31 10:39:22 +0200

    Solution 24 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mg2dpesc Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 0ddf90a8-0f42-435f-a63c-9deb34c1af60.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 0ddf90a8-0f42-435f-a63c-9deb34c1af60.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0ddf90a8-0f42-435f-a63c-9deb34c1af60.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 0ddf90a8-0f42-435f-a63c-9deb34c1af60.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -3,16 +3,18 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b){
+using std::vector;
+
+bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-std::vector<std::string> sorted_list_sum(std::vector<std::string> lst){
+vector<string> sorted_list_sum(vector<string> lst){
     std::sort(lst.begin(), lst.end());
     return lst;
 }
 
 int main(){
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
 }
