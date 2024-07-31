e5723947e5ecaae8a94871a529b40a11fbe2d76a
beep boop <githubbot@vadim.me>
2024-07-31 11:45:40 +0200

    Solution 25 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/mfn4ol0e Prompt:       |                    ^~~ 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp:10: 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp: In function ‘int main()’: 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp:20:20: error: ‘factorize’ was not decla red in this scope    20 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 62e89b22-43a2-4a37-9128-f9b50dc963de.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -12,13 +12,5 @@ vector<int> factorize(int n) {
 }
 
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    // implementation of issame
 }
