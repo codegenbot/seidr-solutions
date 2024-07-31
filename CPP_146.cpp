d5d5afe9d849e116d475ca256ee40ba1251c75e7
beep boop <githubbot@vadim.me>
2024-07-31 17:23:49 +0200

    Solution 25 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/fp2dxtin Prompt: 51c5dd31-ad4e-4592-b825-f10d4e0bbb41.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums){       |                   ^~~~~~ 51c5dd31-ad4e-4592-b825-f10d4e0bbb41.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums){       |                          ^~~ In file included from 51c5dd31-ad4e-4592-b825-f10d4e0bbb41.cpp:11: 51c5dd31-ad4e-4592-b825-f10d4e0bbb41.cpp: In function ‘int main()’: 51c5dd31-ad4e-4592-b825-f10d4e0bbb41.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,9 +1,9 @@
 #include <vector>
 
-int specialFilter(vector<int> nums){
+int specialFilter(std::vector<int> nums){
     int count = 0;
     for (int num : nums) {
-        if (num > 10 && (abs(num) % 10 % 2 == 1) && (to_string(abs(num))[0] % 2 == 1)) {
+        if (num > 10 && (std::abs(num) % 10 % 2 == 1) && (std::to_string(std::abs(num))[0] % 2 == 1)) {
             count++;
         }
     }
