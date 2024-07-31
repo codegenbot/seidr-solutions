cf28c0f8f45d3e1f1d0639444cd5de8a641c269a
beep boop <githubbot@vadim.me>
2024-07-31 13:16:41 +0200

    Solution 64 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/5c9um55e Prompt:                  from a974dd60-aa06-40fc-99f7-492f3cccb592.cpp:4: a974dd60-aa06-40fc-99f7-492f3cccb592.cpp: In function ‘int main()’: a974dd60-aa06-40fc-99f7-492f3cccb592.cpp:26:12: error: ‘anti_shuffle’ was not de clared in this scope    26 |     assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |            ^~~~~~~~~~~~ a974dd60-aa06-40fc-99f7-492f3cccb592.cpp: At global scope: a974dd60-aa06-40fc-99f7-492f3cccb592.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ a974dd60-aa06-40fc-99f7-492f3cccb592.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from a974dd60-aa06-40fc-99f7-492f3cccb592.cpp:30: a974dd60-aa06-40fc-99f7-492f3cccb592.cpp: In function ‘int main()’: a974dd60-aa06-40fc-99f7-492f3cccb592.cpp:32:13: error: ‘anti_shuffle’ was not de clared in this scope    32 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <cassert>
 
-std::string anti_shuffle(std::string s) {
-    std::string result = "";
-    std::string word = "";
+using namespace std;
+
+string anti_shuffle(string s);
+
+string anti_shuffle(string s) {
+    string result = "";
+    string word = "";
     for (char c : s) {
         if (c == ' ') {
-            std::sort(word.begin(), word.end());
+            sort(word.begin(), word.end());
             result += word + ' ';
             word = "";
         } else {
@@ -15,13 +20,13 @@ std::string anti_shuffle(std::string s) {
         }
     }
     if (!word.empty()) {
-        std::sort(word.begin(), word.end());
+        sort(word.begin(), word.end());
         result += word;
     }
     return result;
 }
 
 int main() {
-    std::cout << (anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy") << std::endl;
+    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
     return 0;
 }
