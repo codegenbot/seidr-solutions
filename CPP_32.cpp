180a51758eedcfe40669605013c210d13d5dac90
beep boop <githubbot@vadim.me>
2024-07-31 10:32:09 +0200

    Solution 59 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3iy35mq Prompt: 4426ad39-62aa-475b-941f-827657c0f39e.cpp:52:16: error: expected primary-expressi on before ‘double’    52 |         vector<double> coeffs = {};       |                ^~~~~~ 4426ad39-62aa-475b-941f-827657c0f39e.cpp:57:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    57 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 4426ad39-62aa-475b-941f-827657c0f39e.cpp:60:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    60 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 4426ad39-62aa-475b-941f-827657c0f39e.cpp:60:36: error: ‘find_zero’ cannot be use d as a function    60 |         solution = find_zero(coeffs);       |                                    ^ In file included from 4426ad39-62aa-475b-941f-827657c0f39e.cpp:44: 4426ad39-62aa-475b-941f-827657c0f39e.cpp:61:25: error: ‘poly’ cannot be used as a function    61 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -7,7 +7,8 @@ double find_zero(std::vector<double> coefficients);
 int main() {
     int n;
     cin >> n;
-    std::vector<double> coeffs(n); 
+    std::vector<double> coeffs;
+    coeffs.resize(n);
     double val;
     for (int i = 0; i < n; i++) {
         cin >> val;
