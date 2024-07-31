f03a92be07b7201a81aa54aca1d2bffcb931958f
beep boop <githubbot@vadim.me>
2024-07-31 08:54:49 +0200

    Solution 98 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vemj32qk Prompt:       |                           ^~~~~~ 02959372-dda2-4a70-8b77-3bdf781e65de.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 02959372-dda2-4a70-8b77-3bdf781e65de.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 02959372-dda2-4a70-8b77-3bdf781e65de.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 02959372-dda2-4a70-8b77-3bdf781e65de.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 02959372-dda2-4a70-8b77-3bdf781e65de.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 02959372-dda2-4a70-8b77-3bdf781e65de.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -21,6 +21,6 @@ std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
 }
 
 int main() {
-    std::assert(issame(common({4, 3, 2, 8}, {}), {}));
+    assert(issame(common({4, 3, 2, 8}, {}), std::vector<int>{}));
     return 0;
 }
