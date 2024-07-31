913995bffa845ae3e3118b11ce2e5f7cd12a8893
beep boop <githubbot@vadim.me>
2024-07-31 14:12:57 +0200

    Solution 4 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6qw5jxbx Prompt:       |                              ^~~~~~ 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp:23:37: error: ‘string’ was not declared  in this scope    23 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp:23:44: error: ‘b’ was not declared in t his scope    23 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp:23:45: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp:22: 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp: In function ‘int main()’: 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp:32:20: error: ‘split_words’ was not dec lared in this scope    32 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 9fa878f6-6fd2-419e-98a6-e30a7172d8c7.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,4 +1,7 @@
-vector<string> result;
+#include <string>
+
+vector<string> split_words(string txt){
+    vector<string> result;
     string word = "";
     for(char c : txt){
         if(c == ' ' || c == ','){
@@ -14,13 +17,7 @@ vector<string> result;
         result.push_back(word);
     }
     if(result.size() == 0){
-        int count = 0;
-        for(char c : txt){
-            if(islower(c) && (c - 'a') % 2 == 1){
-                count++;
-            }
-        }
-        result.push_back(to_string(count));
+        result.push_back(to_string(count_if(txt.begin(), txt.end(), [](char c){return islower(c) && (c-'a') % 2 == 1;})));
     }
     return result;
 }
