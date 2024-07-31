cb70bb9e55d166b58bd638ef95ffa7a9aa5394f4
beep boop <githubbot@vadim.me>
2024-07-31 09:49:41 +0200

    Solution 23 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/k5zrd6up Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 70a7ddd9-8957-4bb8-80e9-a0a7a3c54869.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 70a7ddd9-8957-4bb8-80e9-a0a7a3c54869.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 70a7ddd9-8957-4bb8-80e9-a0a7a3c54869.cpp:59:5: error: redefinition of ‘int main( )’    59 | int main(){       |     ^~~~ 70a7ddd9-8957-4bb8-80e9-a0a7a3c54869.cpp:38:5: note: ‘int main()’ previously def ined here    38 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -13,7 +13,7 @@ bool issame(vector<string> a, vector<string> b) {
 vector<string> select_words(string s, int n) {
     vector<string> result;
     string word;
-
+    
     for (char c : s) {
         if (c == ' ') {
             int consonant_count = count_if(word.begin(), word.end(), [](char x) {
@@ -27,14 +27,14 @@ vector<string> select_words(string s, int n) {
             word += c;
         }
     }
-
+    
     int consonant_count = count_if(word.begin(), word.end(), [](char x) {
         return !strchr("aeiouAEIOU", x) && isalpha(x);
     });
     if (consonant_count == n) {
         result.push_back(word);
     }
-
+    
     return result;
 }
 
@@ -42,10 +42,10 @@ int main() {
     string s;
     int n;
     cin >> s >> n;
-
+    
     vector<string> words = select_words(s, n);
 
-    assert(issame(select_words("a b c d e f", 1), vector<string>{"b", "c", "d", "f"}));
-
+    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
+    
     return 0;
 }
