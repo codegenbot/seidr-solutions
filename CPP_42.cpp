68143985f92d6377dd1c728d0f71545a43f5af62
beep boop <githubbot@vadim.me>
2024-07-31 08:21:26 +0200

    Solution 52 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pv69h4z1 Prompt:       |                           ^~~~~~ 457df551-58d0-4f43-a72c-a7c4deb72fd7.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 457df551-58d0-4f43-a72c-a7c4deb72fd7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 457df551-58d0-4f43-a72c-a7c4deb72fd7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 457df551-58d0-4f43-a72c-a7c4deb72fd7.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 457df551-58d0-4f43-a72c-a7c4deb72fd7.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 457df551-58d0-4f43-a72c-a7c4deb72fd7.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -3,14 +3,14 @@
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+		return false;
+	}
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != b[i]) {
+			return false;
+		}
+	}
+	return true;
 }
 
 std::vector<int> incr_list(const std::vector<int>& input) {
@@ -19,9 +19,4 @@ std::vector<int> incr_list(const std::vector<int>& input) {
         output.push_back(num + 1);
     }
     return output;
-}
-
-int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), incr_list({6, 3, 6, 3, 4, 4, 10, 1, 124})));
-    return 0;
 }
