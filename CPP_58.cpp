80c3031281d55c1f3f395256c34099052bd2f2ec
beep boop <githubbot@vadim.me>
2024-07-31 12:37:19 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/yvg8xybk Prompt:       |                    ^~~ d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp:12: d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp: In function ‘int main()’: d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ d3b6b392-2125-4d7c-b7a0-d43e98e7a15b.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,27 +1,14 @@
 #include <vector>
 #include <algorithm>
 
-vector<int> issame(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+vector<int> issame(vector<int> l1, vector<int> l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
 
     vector<int> result;
-    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
-    
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+
     result.erase(unique(result.begin(), result.end()), result.end());
 
     return result;
-}
-
-int main() {
-    vector<int> l1 = {1, 2, 3, 4, 5};
-    vector<int> l2 = {1, 3, 5, 7, 9};
-
-    vector<int> res = issame(l1, l2);
-
-    for (int num : res) {
-        cout << num << " ";
-    }
-
-    return 0;
 }
