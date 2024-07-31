73ed45cbfed7a9cb0a37b57ea22a0324d08ad1d8
beep boop <githubbot@vadim.me>
2024-07-31 10:00:43 +0200

    Solution 69 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/k5zrd6up Prompt:     5 | #include <string> 0455dd09-ff84-4671-a760-bd8f99e158d4.cpp:68:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    68 | int main(){       |         ^~ 0455dd09-ff84-4671-a760-bd8f99e158d4.cpp:68:9: note: remove parentheses to defau lt-initialize a variable    68 | int main(){       |         ^~       |         -- 0455dd09-ff84-4671-a760-bd8f99e158d4.cpp:68:9: note: or replace parentheses with  braces to value-initialize a variable 0455dd09-ff84-4671-a760-bd8f99e158d4.cpp:68:11: error: a function-definition is not allowed here before ‘{’ token    68 | int main(){       |           ^ 0455dd09-ff84-4671-a760-bd8f99e158d4.cpp:70:2: error: expected ‘}’ at end of inp ut    70 | }       |  ^ 0455dd09-ff84-4671-a760-bd8f99e158d4.cpp:45:12: note: to match this ‘{’    45 | int main() {       |            ^

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -4,7 +4,7 @@ int main() {
     std::cin >> s >> n;
 
     assert(issame(select_words("a b c d e f", 1), std::vector<std::string>{"b", "c", "d", "f"})); 
-    assert(issame(select_words(s, n), select_words(s, n)));
+    assert(issame(select_words(s, n), select_words(s, n));
 
     return 0;
 }
