28bcff9d3ddc22d96ab18334d5b1e213df587dad
beep boop <githubbot@vadim.me>
2024-07-31 17:25:12 +0200

    Solution 88 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/85x4gmk1 Prompt: Complete the following code given the task description and function signature.

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,16 +1,29 @@
-#include <openssl/ssl.h>
+#include<string>
+#include<openssl/evp.h>
+
+using namespace std;
 
 string string_to_md5(string text) {
-    if (text.empty()) return "";
-    unsigned char md[MD5_DIGEST_LENGTH];
-    MD5((const unsigned char*)text.c_str(), text.size(), md);
-    string ret;
-    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) 
-        ret.push_back( int2hex(md[i]));
-    return ret;
-}
+    if (text.empty()) {
+        return "";
+    }
 
-string int2hex(unsigned char i) {
-    static char hex[]="0123456789abcdef";
-    return &hex[i%16];
+    unsigned char md[16];
+    EVP_MD_CTX md_ctx;
+    const EVP_MD* md_type = EVP_md5();
+    const void* data = text.c_str();
+    size_t data_len = text.length();
+
+    EVP_DigestInit_ex(&md_ctx, md_type, 0);
+    EVP_DigestUpdate(&md_ctx, data, data_len);
+    EVP_DigestFinal_ex(&md_ctx, md, nullptr);
+
+    string result;
+    for (int i = 0; i < 16; ++i) {
+        char buf[3];
+        sprintf(buf, "%02x", md[i]);
+        result += string(buf);
+    }
+
+    return result;
 }
