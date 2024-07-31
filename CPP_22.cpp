4f1276278ae02fff8f2d7a518565b86bfdf244f0
beep boop <githubbot@vadim.me>
2024-07-31 11:42:22 +0200

    Solution 38 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ibnyda3o Prompt: on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1c4cc68c-6dae-4f85-a0a0-b0629e43f455.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 1c4cc68c-6dae-4f85-a0a0-b0629e43f455.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~ In file included from 1c4cc68c-6dae-4f85-a0a0-b0629e43f455.cpp:20: 1c4cc68c-6dae-4f85-a0a0-b0629e43f455.cpp: In function ‘int main()’: 1c4cc68c-6dae-4f85-a0a0-b0629e43f455.cpp:30:20: error: ‘filter_integers’ was not  declared in this scope    30 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 1c4cc68c-6dae-4f85-a0a0-b0629e43f455.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -3,8 +3,15 @@
 #include <list>
 #include <cassert>
 
+vector<int> filter_integers(std::list<std::any> values);
+
 bool issame(vector<int>& a, vector<int>& b){
     return a == b;
+}
+
+int main(){
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
+    return 0;
 }
 
 vector<int> filter_integers(std::list<std::any> values){
@@ -15,9 +22,4 @@ vector<int> filter_integers(std::list<std::any> values){
         }
     }
     return result;
-}
-
-int main(){
-    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
-    return 0;
 }
