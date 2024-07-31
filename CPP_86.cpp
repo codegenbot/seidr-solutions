f3ea9580f39f661d702b0e0ad33b638c75e23afa
beep boop <githubbot@vadim.me>
2024-07-31 12:21:20 +0200

    Solution 37 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kekrq434 Prompt:  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘std::basic_ostream <_CharT, _Traits>& std::operator<<(std::basic_ostream<_CharT, _Traits>&, const s td::__cxx11::basic_string<_CharT, _Traits, _Allocator>&) [with _CharT = char; _T raits = std::char_traits<char>; _Alloc = std::allocator<char>]’: c2763354-fea9-4591-a494-98e102f22384.cpp:23:59:   required from here /usr/include/c++/11/bits/basic_string.h:6536:57: error: ‘const class std::__cxx1 1::basic_string<char>’ has no member named ‘size’  6536 |       return __ostream_insert(__os, __str.data(), __str.size());       |                                                   ~~~~~~^~~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -3,23 +3,23 @@
 #include <string>
 using namespace std;
 
-std::string anti_shuffle(std::string str) {
-    std::string result = "";
+string anti_shuffle(const string str) {
+    string result = "";
     int i = str.length() - 1;
     while (i >= 0) {
         int j = i;
         for (; j >= 0 && str[j] == str[i]; --j)
             --i;
-        result += str.substr(j + 1, i - j);
+        result += str.substr(j + 1, i - j + 1);
         --i;
     }
     return result;
 }
 
 int main() {
-    std::string str;
-    std::cout << "Enter a sentence: ";
+    string str;
+    cout << "Enter a sentence: ";
     getline(cin, str);
-    std::cout << "Anti-shuffled sentence: " << anti_shuffle(str) << std::endl;
+    cout << "Anti-shuffled sentence: " << anti_shuffle(str) << endl;
     return 0;
 }
