41be9d698f9701f89a6fb8732a83396f39a4b116
beep boop <githubbot@vadim.me>
2024-07-31 12:48:05 +0200

    Solution 70 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/yvg8xybk Prompt: 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp:32:6: error: ambiguating new declaratio n of ‘bool issame(std::vector<int>, std::vector<int>)’    32 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp:7:13: note: old declaration ‘std::vecto r<int> issame(std::vector<int>, std::vector<int>)’     7 | vector<int> issame(vector<int> a, vector<int> b) {       |             ^~~~~~ 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp:31: 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp: In function ‘int main()’: 734a3dea-5291-4bc3-994e-f77aae4cc6f0.cpp:41:20: error: ‘common’ was not declared  in this scope    41 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+vector<int> issame(vector<int> a, vector<int> b);
+
+int main() {
+    vector<int> l1 = {1, 2, 3, 4, 5};
+    vector<int> l2 = {1, 3, 5, 7, 9};
+
+    vector<int> res = issame(l1, l2);
+
+    for (int num : res) {
+        cout << num << " ";
+    }
+
+    return 0;
+}
+
 vector<int> issame(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
