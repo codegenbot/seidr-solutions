a188b06ad5795e01540a1c753dc22149ed526f9b
beep boop <githubbot@vadim.me>
2024-07-31 11:55:27 +0200

    Solution 19 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/40vatql0 Prompt: 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp: In function ‘int main()’: 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp:23:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    23 | int main(){       |         ^~ 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp:23:9: note: remove parentheses to defau lt-initialize a variable    23 | int main(){       |         ^~       |         -- 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp:23:9: note: or replace parentheses with  braces to value-initialize a variable 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp:23:11: error: a function-definition is not allowed here before ‘{’ token    23 | int main(){       |           ^ 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp:26:2: error: expected ‘}’ at end of inp ut    26 | }       |  ^ 980546d2-bc59-4e52-a8e6-7c6b49ca68f8.cpp:18:11: note: to match this ‘{’    18 | int main(){       |           ^

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,19 +1,4 @@
-#include <string>
-#include <cassert>
-using namespace std;
-
-string decimal_to_binary(int decimal){
-    string binary = "";
-    while(decimal > 0){
-        if(decimal % 2 == 0)
-            binary = "0" + binary;
-        else
-            binary = "1" + binary;
-        decimal /= 2;
-    }
-    return "db" + binary + "db";
-}
-
-int main(){
+int main {
     assert (decimal_to_binary(15) == "db1111db");
     return 0;
+}
