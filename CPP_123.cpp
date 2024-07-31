61dee4f983203626342831ad3ceda45071a3d5b5
beep boop <githubbot@vadim.me>
2024-07-31 14:11:03 +0200

    Solution 15 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/azv1r5gd Prompt:       |                    ^~~ 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp:20: 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp: In function ‘int main()’: 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp:30:20: error: ‘get_odd_collatz’ was not  declared in this scope    30 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 3905bd0f-511a-4c38-81eb-40d9a6f02ec9.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,11 +4,13 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); ++i) {
+    
+    for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+    
     return true;
 }
 
@@ -23,12 +25,14 @@ std::vector<int> get_odd_collatz(int n) {
         }
     }
     result.push_back(1);
+    
     std::vector<int> odd_result;
     for (int num : result) {
         if (num % 2 != 0) {
             odd_result.push_back(num);
         }
     }
+    
     sort(odd_result.begin(), odd_result.end());
     return odd_result;
 }
