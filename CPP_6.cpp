3f266c98d5ac2e4d6fc5949ed20f4e39b952612b
beep boop <githubbot@vadim.me>
2024-07-31 08:42:22 +0200

    Solution 2 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/nj1f2l9u Prompt: 33c89714-293a-45f7-b9df-c58234461541.cpp:6:33: error: ‘string’ was not declared in this scope     6 | vector<int> parse_nested_parens(string paren_string) {       |                                 ^~~~~~ 33c89714-293a-45f7-b9df-c58234461541.cpp:3:1: note: ‘std::string’ is defined in header ‘<string>’; did you forget to ‘#include <string>’?     2 | #include <vector>   +++ |+#include <string>     3 | 33c89714-293a-45f7-b9df-c58234461541.cpp:6:54: error: expected ‘,’ or ‘;’ before  ‘{’ token     6 | vector<int> parse_nested_parens(string paren_string) {       |                                                      ^ In file included from 33c89714-293a-45f7-b9df-c58234461541.cpp:31: 33c89714-293a-45f7-b9df-c58234461541.cpp: In function ‘int main()’: 33c89714-293a-45f7-b9df-c58234461541.cpp:41:39: error: no match for call to ‘(st d::vector<int>) (const char [15])’    41 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-vector<int> parse_nested_parens(const string &paren_string) {
+vector<int> parse_nested_parens(string paren_string) {
     vector<int> result;
     stack<int> nesting_levels;
     int max_nesting = 0;
