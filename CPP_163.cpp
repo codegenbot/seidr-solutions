975ddfa649155fa9fa53dfdf6cf8c070efe9eddf
beep boop <githubbot@vadim.me>
2024-07-31 17:57:43 +0200

    Solution 30 of CPP/163, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ok8jtspz Prompt:       |                    ^~~ a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp:11: a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp: In function ‘int main()’: a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp:21:20: error: ‘generate_integers’ was n ot declared in this scope    21 |     assert (issame(generate_integers(17,89) , {}));       |                    ^~~~~~~~~~~~~~~~~ a76152fd-5460-4eb8-9e85-25053f9fb5fd.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(generate_integers(17,89) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_163.cpp
+++ CPP_163.cpp
@@ -1,20 +1,6 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 vector<int> generate_integers(int a, int b) {
     vector<int> result;
     for (int i = a; i <= b; ++i) {
@@ -23,6 +9,10 @@ vector<int> generate_integers(int a, int b) {
         }
     }
     return result;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
 int main() {
