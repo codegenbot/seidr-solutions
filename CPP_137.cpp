1390fb78fcb0ea8cf07599f6d4d44846aa0cbc2c
beep boop <githubbot@vadim.me>
2024-07-31 10:24:48 +0200

    Solution 29 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ic89b93p Prompt: 978bef91-8f73-4e99-b35f-ae2b470ac319.cpp:22:29: error: ‘string’ was not declared  in this scope    22 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 978bef91-8f73-4e99-b35f-ae2b470ac319.cpp:22:29: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 978bef91-8f73-4e99-b35f-ae2b470ac319.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 978bef91-8f73-4e99-b35f-ae2b470ac319.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <boost/any.hpp>
+#include <boost/any.hpp> // Include the <boost/any.hpp> header
 
 template <typename T>
 T compare_one(const T& a, const T& b) {
@@ -13,7 +13,7 @@ T compare_one(const T& a, const T& b) {
 }
 
 int main() {
-    assert(boost::any_cast<std::string>(compare_one(boost::any(std::string("1")), boost::any(std::to_string(1))).empty());
-    
+    assert(compare_one(boost::any_cast<std::string>(std::string("1")), boost::any_cast<std::string>(std::to_string(1))).empty());
+
     return 0;
 }
