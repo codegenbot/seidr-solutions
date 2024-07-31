8dde3a8f62eee0a442949cfde79c84e1992a982a
beep boop <githubbot@vadim.me>
2024-07-31 08:08:43 +0200

    Solution 52 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/l0eke88p Prompt:       |                                  ^~~ 4a4a7c93-2deb-4561-b539-44100cf655ee.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 4a4a7c93-2deb-4561-b539-44100cf655ee.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~ In file included from 4a4a7c93-2deb-4561-b539-44100cf655ee.cpp:20: 4a4a7c93-2deb-4561-b539-44100cf655ee.cpp: In function ‘int main()’: 4a4a7c93-2deb-4561-b539-44100cf655ee.cpp:30:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    30 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 4a4a7c93-2deb-4561-b539-44100cf655ee.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -13,6 +13,7 @@ std::vector<int> removeDuplicates(std::vector<int> l){
 }
 
 int main(){
-    assert(issame(removeDuplicates({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
+    std::vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
+    assert(issame(removeDuplicates(input), {0, 2, 3, 5, 9, 123}));
     return 0;
 }
