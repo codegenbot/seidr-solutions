1e17b4e5f21c592ef8c16c7f7e1a464a1c830dfd
beep boop <githubbot@vadim.me>
2024-07-31 09:55:19 +0200

    Solution 90 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5x53byvq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8fa9338e-55f8-42c2-b055-deece3a01 0e2.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8fa9338e-55f8-42c2-b055-deece3a01 0e2.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,4 +1,3 @@
 def double_the_difference(lst):
-    even_squares = sum(i ** 2 for i in [x for x in lst if isinstance(x, int) and x >= 0 and x % 2 != 0])
-    diff = (max(lst) - min(lst)) if len(lst) > 1 else 0
-    return 2 * even_squares - abs(diff)
+    result = sum(i**2 for i in lst if isinstance(i, int) and i >= 0 and i % 2 != 0)
+    return result
