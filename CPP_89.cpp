66bc67880a7607fe9e4eebbe00465d557a7af5e3
beep boop <githubbot@vadim.me>
2024-07-31 09:11:40 +0200

    Solution 19 of CPP/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/11t1zk10 Prompt: 6c7f47de-d38b-439f-b971-64652e9b3083.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 6c7f47de-d38b-439f-b971-64652e9b3083.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main(){       |     ^~~~

--- CPP_89.cpp
+++ CPP_89.cpp
@@ -5,23 +5,23 @@
 std::string encrypt(std::string s){
     std::string result = "";
     for(char& c : s){
-        if(std::isalpha(c)){
-            char encrypted = c + 4;
-            if(std::islower(c)){
-                if(encrypted > 'z'){
-                    encrypted = 'a' + (encrypted - 'z' - 1);
-                }
-            } else {
-                if(encrypted > 'Z'){
-                    encrypted = 'A' + (encrypted - 'Z' - 1);
-                }
-            }
-            result += encrypted;
-        } else {
-            result += c;
-        }
-    }
-    return result;
+      if(std::isalpha(c)){
+          char encrypted = c + 4;
+          if(std::islower(c)){
+              if(encrypted > 'z'){
+                  encrypted = 'a' + (encrypted - 'z' - 1);
+              }
+          } else {
+              if(encrypted > 'Z'){
+                  encrypted = 'A' + (encrypted - 'Z' - 1);
+              }
+          }
+          result += encrypted;
+      } else {
+          result += c;
+      }
+  }
+  return result;
 }
 
 int main(){
