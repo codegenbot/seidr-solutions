811af74f0b417a29c9fa320386a047c17ceec325
beep boop <githubbot@vadim.me>
2024-07-31 12:30:59 +0200

    Solution 60 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kekrq434 Prompt:     7 |         if (str[i] == str[0]) {       |                          ^ a7eb1a25-5c71-4b3e-9e2f-d3a850b51e12.cpp:8:24: error: request for member ‘c_str’  in ‘str’, which is of non-class type ‘const int’     8 |             return str.c_str();       |                        ^~~~~ a7eb1a25-5c71-4b3e-9e2f-d3a850b51e12.cpp:12:16: error: request for member ‘c_str ’ in ‘str’, which is of non-class type ‘const int’    12 |     return str.c_str();       |                ^~~~~ In file included from a7eb1a25-5c71-4b3e-9e2f-d3a850b51e12.cpp:15: a7eb1a25-5c71-4b3e-9e2f-d3a850b51e12.cpp: In function ‘int main()’: a7eb1a25-5c71-4b3e-9e2f-d3a850b51e12.cpp:17:26: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    17 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |                          ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                          |       |                          const char* a7eb1a25-5c71-4b3e-9e2f-d3a850b51e12.cpp:1:44: note:   initializing argument 1 o f ‘const char* anti_shuffle(int)’     1 | const char* anti_shuffle(const std::string str) {       |                          ~~~~~~~~~~~~~~~~~~^~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -5,8 +5,9 @@ const char* anti_shuffle(const std::string& str) {
     int i = 1;
     while (i < str.length()) {
         if (str[i] == str[0]) {
-            return NULL; 
+            return str.c_str();
         }
         i++;
     }
     return str.c_str();
+}
