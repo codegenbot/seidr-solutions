93338f8477c304a0865f37af4d8fa0b026d4e205
beep boop <githubbot@vadim.me>
2024-07-31 14:14:26 +0200

    Solution 7 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/hlhp3ev7 Prompt: bca97664-4d2f-4974-8135-0c8039d44dce.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ bca97664-4d2f-4974-8135-0c8039d44dce.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,17 +1,12 @@
+```c++
 #include <vector>
 #include <cmath>
-#include <cassert>
 
 int sum_squares(std::vector<float> lst) {
     int total = 0;
     for(float x : lst) {
         int ceil_x = (int)std::ceil(x);
-        total += pow(ceil_x,2);
+        total += std::pow(ceil_x,2);
     }
     return total;
-}
-
-int main() {
-    assert(sum_squares({-1.0f, 1.0f, 0.0f}) == 2);
-    return 0;
 }
