7dfdfebb50454738bba2f2635e7d89a362b3ed43
beep boop <githubbot@vadim.me>
2024-07-31 10:05:12 +0200

    Solution 49 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5vphnzb8 Prompt:       |             ^~~~~~ fd5a7d7e-aa5b-40f4-98f6-e3491aad96cf.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fd5a7d7e-aa5b-40f4-98f6-e3491aad96cf.cpp:33:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from fd5a7d7e-aa5b-40f4-98f6-e3491aad96cf.cpp:5: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from fd5a7d7e-aa5b-40f4-98f6-e3491aad96cf.cpp:5: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ fd5a7d7e-aa5b-40f4-98f6-e3491aad96cf.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ fd5a7d7e-aa5b-40f4-98f6-e3491aad96cf.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <algorithm>
 
-bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) return false;
@@ -22,8 +22,9 @@ std::vector<std::string> filter_by_prefix(const std::vector<std::string>& string
     return result;
 }
 
+std::vector<std::string> b = {"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"};
 int main() {
     std::vector<std::string> a = {"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"};
-    const std::vector<std::string>& b = filter_by_prefix(a, "xxx");
+    b = filter_by_prefix(a, "xxx");
     assert (issame(b , {"xxx", "xxxAAA", "xxx"}));
 }
