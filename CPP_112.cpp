21f4c6a0e6f8d0032be26ad813f51186adf849d5
beep boop <githubbot@vadim.me>
2024-07-31 13:35:32 +0200

    Solution 64 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3jcrapl Prompt:  <brace-enclosed initializer list>)’    40 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 242b1b60-5c28-4b7e-a8be-603fce239e2b.cpp:5:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     5 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 242b1b60-5c28-4b7e-a8be-603fce239e2b.cpp:5:35: note:   no known conversion for a rgument 1 from ‘std::pair<std::vector<std::__cxx11::basic_string<char> >, bool>’  to ‘const std::vector<std::__cxx11::basic_string<char> >&’     5 | bool issame(const vector<string>& a, const vector<string>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~^ 242b1b60-5c28-4b7e-a8be-603fce239e2b.cpp:31:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    31 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 242b1b60-5c28-4b7e-a8be-603fce239e2b.cpp:31:28: note:   no known conversion for argument 1 from ‘std::pair<std::vector<std::__cxx11::basic_string<char> >, bool> ’ to ‘std::vector<std::__cxx11::basic_string<char> >’    31 | bool issame(vector<string> a,vector<string>b){       |             ~~~~~~~~~~~~~~~^

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -23,6 +23,6 @@ pair<vector<string>, bool> reverse_delete(string s1, string s2) {
 
 int main() {
     auto res = reverse_delete("mamma", "mia");
-    assert((issame({""}, res.first) && res.second));
+    assert((res.second) && (issame({""}, res.first)));
     return 0;
 }
