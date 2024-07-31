903b0e64c07d93144f43641132892b37f24f4e90
beep boop <githubbot@vadim.me>
2024-07-31 07:58:53 +0200

    Solution 63 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/6dbunna4 Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘float’ and ‘const float’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -8,7 +8,7 @@ using namespace std;
 pair<float, float> find_closest_elements(const vector<const float>& numbers) {
     sort(numbers.begin(), numbers.end());
     float min_diff = numbers[1] - numbers[0];
-    pair<float, float> closest_elements = make_pair(numbers[0], numbers[1]);
+    pair<const float, const float> closest_elements = make_pair(numbers[0], numbers[1]);
 
     for (int i = 1; i < numbers.size() - 1; ++i) {
         float diff = numbers[i + 1] - numbers[i];
