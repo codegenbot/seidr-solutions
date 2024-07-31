a55515ba116b64ca0e3c4859c17a28f7bfc86ef6
beep boop <githubbot@vadim.me>
2024-07-31 11:19:14 +0200

    Solution 17 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/eweb77sr Prompt:       |                    ^~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 350ea264-ea71-4cd1-a913-805535bdce88.cpp:24: 350ea264-ea71-4cd1-a913-805535bdce88.cpp: In function ‘int main()’: 350ea264-ea71-4cd1-a913-805535bdce88.cpp:34:20: error: ‘parse_nested_parens’ was  not declared in this scope    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ 350ea264-ea71-4cd1-a913-805535bdce88.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -7,7 +7,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> parse_nested_parens(string paren_string) {
+std::vector<int> parse_nested_parens(std::string paren_string) {
     std::vector<int> depths;
     int max_depth = 0;
     int current_depth = 0;
@@ -30,4 +30,8 @@ std::vector<int> parse_nested_parens(string paren_string) {
     return depths;
 }
 
-assert(issame(parse_nested_parens("(()(())((())))"), {4}));
+int main() {
+    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
+
+    return 0;
+}
