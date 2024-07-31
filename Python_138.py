1cc8e1591040c46c42123c03f2b4421b318ba341
beep boop <githubbot@vadim.me>
2024-07-31 07:42:17 +0200

    Solution 4 of Python/138, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vvjqeam9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a68a6e43-6840-4ac0-b507-833655bd5 e53.py", line 10, in <module>     check(is_equal_to_sum_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a68a6e43-6840-4ac0-b507-833655bd5 e53.py", line 7, in check     assert is_equal_to_sum_even(16) == True            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_138.py
+++ Python_138.py
@@ -1,6 +1,6 @@
 ```
 def is_equal_to_sum_even(n):
     for i in range(2, n+1, 2):
-        if sum(j for j in range(2, (i+n)//2+1, 2) if i == n-j) and len([j for j in range(2, (i+n)//2+1, 2) if i == n-j]) == 4:
+        if sum(j for j in range(2, (i+n)//2+1, 2) if i == j + (n - i)) and len([j for j in range(2, (i+n)//2+1, 2) if i == j + (n - i)]) == 4:
             return True
     return False
