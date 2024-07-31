84a8dbe223c88828a776acdbf2a50a40d9cf7cf4
beep boop <githubbot@vadim.me>
2024-07-31 16:26:57 +0200

    Solution 0 of Python/118, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/me4i5e14 Prompt: Complete the following code given the task description and function signature.

---
+++ Python_118.py
@@ -0,0 +1,6 @@
+def get_closest_vowel(word):
+    word = word[::-1]
+    for i in range(len(word) - 2):
+        if word[i].lower() in "aeiou" and not word[i + 1].isalpha():
+            return word[i].lower()
+    return ""
