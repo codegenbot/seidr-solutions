dc5ae8846b55422d6dafe310d085719d3c80a5f5
beep boop <githubbot@vadim.me>
2024-07-31 10:50:37 +0200

    Solution 10 of CPP/45, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vpljbp97 Prompt: 154d03fd-1f32-4072-a98f-1253f28c6950.cpp:13:5: error: redefinition of ‘int main( )’    13 | int main(){       |     ^~~~ 154d03fd-1f32-4072-a98f-1253f28c6950.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~

--- CPP_45.cpp
+++ CPP_45.cpp
@@ -1,7 +1,11 @@
-```
+```cpp
 #include <cmath>
 #include <cassert>
 
 float triangle_area(float a, float h) {
     return (a * std::abs(h)) / 2.0f;
+}
+
+int main() {
+    assert(std::abs(triangle_area(10, 8) - 40.0)<1e-4);
 }
