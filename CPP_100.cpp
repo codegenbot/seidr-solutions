d02e85cb8980c53187c32ee992857a762bf48f26
beep boop <githubbot@vadim.me>
2024-07-31 14:48:37 +0200

    Solution 30 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2qkg41co Prompt:       |                    ^~~ e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp:11: e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp: In function ‘int main()’: e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp:21:20: error: ‘make_a_pile’ was not dec lared in this scope    21 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ e1bc45cd-eede-45c0-8273-03fb1e196b91.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,11 +1,18 @@
 #include <vector>
 
-bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a.size() != b.size()) 
-        return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) 
-            return false;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> make_a_pile(int n) {
+    vector<int> pile;
+    for (int i = 1; i <= n; ++i) {
+        pile.push_back(i % 2 == 0 ? i + 1 : i);
     }
-    return true;
+    return pile;
+}
+
+int main() {
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    return 0;
 }
