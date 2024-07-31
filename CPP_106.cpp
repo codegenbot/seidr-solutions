150d653cf719508cf725a7ee92ec6b439edf07de
beep boop <githubbot@vadim.me>
2024-07-31 16:10:38 +0200

    Solution 26 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/sbirh8qw Prompt:       |                    ^~~ 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp:21: 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp: In function ‘int main()’: 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp:31:20: error: ‘f’ was not declared in t his scope    31 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 68a3357e-fe90-4081-8bb3-a68d028b6508.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,10 +1,6 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
-
 std::vector<int> f(int n){
     std::vector<int> result(n);
     for(int i = 0; i < n; ++i){
@@ -25,7 +21,11 @@ std::vector<int> f(int n){
     return result;
 }
 
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
+
 int main(){
-    assert (issame(f(3) , {1, 2, 6}));
+    assert(issame(f(3) , {1, 2, 6}));
     return 0;
 }
