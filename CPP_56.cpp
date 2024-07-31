7ddfb5431de39a62be7183e644961ba74046b68e
beep boop <githubbot@vadim.me>
2024-07-31 12:00:42 +0200

    Solution 73 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/z3f4ff2t Prompt: 0338ad96-1fd2-4071-a383-89f2cb79d71b.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 0338ad96-1fd2-4071-a383-89f2cb79d71b.cpp:45:5: note: ‘int main()’ previously def ined here    45 | int main() {       |     ^~~~ 0338ad96-1fd2-4071-a383-89f2cb79d71b.cpp: In function ‘bool correct_bracketing(s td::string)’: 0338ad96-1fd2-4071-a383-89f2cb79d71b.cpp:43:1: warning: control reaches end of n on-void function [-Wreturn-type]    43 | }       | ^

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,10 +1,4 @@
-#include <initializer_list>
-#include <cassert>
-#include <stack>
-#include <iostream>
-
-int main() {
-    if (!correct_bracketing("<><><<><>><>>><>") ) {
+if (!correct_bracketing("<><><<><>><>>><>") ) {
         std::cout << "Invalid bracketing" << std::endl;
     }
 }
