65143b7e439a3c1acc6dff564f7cccf39ffce8b1
beep boop <githubbot@vadim.me>
2024-07-31 11:23:52 +0200

    Solution 28 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4bdknmn6 Prompt: f5038d25-1a7f-48d1-8e5d-8253c065ad87.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string make_palindrome(string str){       | ^~~~~~ In file included from f5038d25-1a7f-48d1-8e5d-8253c065ad87.cpp:11: f5038d25-1a7f-48d1-8e5d-8253c065ad87.cpp: In function ‘int main()’: f5038d25-1a7f-48d1-8e5d-8253c065ad87.cpp:13:13: error: ‘make_palindrome’ was not  declared in this scope    13 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -2,13 +2,15 @@
 #include <string>
 #include <cassert>
 
-using namespace std;
+bool is_palindrome(const std::string& str) {
+    return str == std::string(str.rbegin(), str.rend());
+}
 
-string make_palindrome(string str){
-    string rev_str(str.rbegin(), str.rend());
-    for (int i = 0; i < str.length(); ++i) {
-        if (str.substr(0, str.length() - i) == rev_str.substr(i)) {
-            return str + rev_str.substr(0, i);
+std::string make_palindrome(const std::string& str) {
+    std::string rev_str(str.rbegin(), str.rend());
+    for (int i = str.size(); i >= 0; i--) {
+        if (is_palindrome(str.substr(i))) {
+            return str + rev_str.substr(0, str.size() - i);
         }
     }
     return str;
@@ -16,6 +18,5 @@ string make_palindrome(string str){
 
 int main() {
     assert(make_palindrome("jerry") == "jerryrrej");
-    
     return 0;
 }
