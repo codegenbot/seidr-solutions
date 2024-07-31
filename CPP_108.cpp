66ea6cc3e7b6d4bfe2a9182060297efe15095dfb
beep boop <githubbot@vadim.me>
2024-07-31 16:26:45 +0200

    Solution 80 of CPP/108, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/tasd1fh5 Prompt: 488657bd-bf14-40e9-9d87-33720ea1b196.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int count_nums(vector<int> n){       |                ^~~~~~ 488657bd-bf14-40e9-9d87-33720ea1b196.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int count_nums(vector<int> n){       |                       ^~~ In file included from 488657bd-bf14-40e9-9d87-33720ea1b196.cpp:22: 488657bd-bf14-40e9-9d87-33720ea1b196.cpp: In function ‘int main()’: 488657bd-bf14-40e9-9d87-33720ea1b196.cpp:24:23: error: ‘count_nums’ cannot be us ed as a function    24 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,8 +1,16 @@
-int count = 0;
+#include <vector>
+
+int count_nums(std::vector<int> n){
+    int count = 0;
     for (int num : n) {
         int sum = 0;
-        int temp = abs(num);
-        while (temp != 0) {
+        int temp = num;
+        if (temp < 0) {
+            temp *= -1;
+            sum -= temp % 10;
+            temp /= 10;
+        }
+        while (temp > 0) {
             sum += temp % 10;
             temp /= 10;
         }
