03ff086094f6f7b2662728dd12c866577d9f2925
beep boop <githubbot@vadim.me>
2024-07-31 15:23:42 +0200

    Solution 30 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/02lyfggr Prompt:       |                             ^~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:23:33: error: expected primary-expressi on before ‘char’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:23:38: error: expected primary-expressi on before ‘int’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:23:44: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 091f7155-73b7-49a0-ac02-a02706913611.cpp:22: 091f7155-73b7-49a0-ac02-a02706913611.cpp: In function ‘int main()’: 091f7155-73b7-49a0-ac02-a02706913611.cpp:37:20: error: ‘histogram’ was not decla red in this scope    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,53 +1,24 @@
-map<char,int> histogram(string test){
-    map<char,int> result;
-    string temp = "";
-    int maxCount = 0;
+map<char, int> histogram(string test) {
+    map<char, int> result;
+    string str = test + " ";
+    char prev = ' ';
+    int count = 0;
 
-    for(int i=0; i<test.length(); i++){
-        if(test[i] == ' ' || i == test.length() - 1){
-            if(temp.length() > 0){
-                if(result.find(temp[0]) != result.end()){
-                    result[temp[0]]++;
-                } else {
-                    result[temp[0]] = 1;
-                }
-                temp = "";
+    for (char c : str) {
+        if (c == prev) {
+            count++;
+        } else {
+            if (count > 0) {
+                result[prev] = count;
             }
-        } else {
-            temp += test[i];
+            prev = c;
+            count = 1;
         }
     }
 
-    map<char,int> maxMap;
-
-    for(auto it = result.begin(); it != result.end(); it++){
-        if(it->second > maxCount){
-            maxCount = it->second;
-            maxMap.clear();
-            maxMap[it->first] = it->second;
-        } else if(it->second == maxCount){
-            maxMap[it->first] = it->second;
-        }
-    }
-
-    bool isSame = false;
-
-    while(!isSame){
-        map<char,int> tempMap = histogram(test);
-        if(issame(result, tempMap)){
-            isSame = true;
-        } else {
-            test.pop_back();
-        }
-    }
-
-    return maxMap;
+    return result;
 }
 
-bool issame(map<char,int> a, map<char,int> b){
-    if(a.size() != b.size()) return false;
-    for(auto it = a.begin(); it != a.end(); it++){
-        if(it->second != b[it->first]) return false;
-    }
-    return true;
+bool issame(map<char, int> a, map<char, int> b) {
+    return a == b;
 }
