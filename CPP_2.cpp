fc1c02ec388a37ca9f15388f3b2a81693ecfb9b8
beep boop <githubbot@vadim.me>
2024-07-31 18:08:27 +0200

    Solution 61 of CPP/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7sgxhaht Prompt: 79aeef1e-747d-471d-a42a-ad4c0fd5d242.cpp:8:1: error: ‘::main’ must return ‘int’     8 | void main() {       | ^~~~ 79aeef1e-747d-471d-a42a-ad4c0fd5d242.cpp:16:5: error: redefinition of ‘int main( )’    16 | int main(){       |     ^~~~ 79aeef1e-747d-471d-a42a-ad4c0fd5d242.cpp:8:6: note: ‘int main()’ previously defi ned here     8 | void main() {       |      ^~~~

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cassert>
 #include <cmath>
 
 double truncate_number(double number) {
@@ -11,6 +10,5 @@ int main() {
     std::cin >> number;
     double fractionalPart = truncate_number(number);
     std::cout << fractionalPart << std::endl;
-    
-    assert(std::abs(truncate_number(123.456) - 0.456) < 1e-4);
+    return 0;
 }
