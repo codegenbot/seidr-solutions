ec3bdd0183c9e6a5e508f694ed54683fa1161372
beep boop <githubbot@vadim.me>
2024-07-31 17:04:04 +0200

    Solution 44 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/bdgmxa97 Prompt:       |                    ^~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:13: 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp: In function ‘int main()’: 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:23:20: error: ‘tri’ was not declared in  this scope    23 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 7e098ad3-dfc3-4cfb-acf8-542256db5888.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,27 +1,35 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 std::vector<int> tri(int n) {
     std::vector<int> res;
     res.push_back(3);
+
     for (int i = 1; i <= n; ++i) {
         if (i % 2 == 0) {
             res.push_back(1 + i / 2);
         } else {
-            res.push_back(res[i - 1] + res[i - 2] + res[i + 1]);
+            res.push_back(res[i - 1] + res[i - 2] + res[i - 3]);
         }
     }
     return res;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    // Test your functions here
+    return 0;
 }
