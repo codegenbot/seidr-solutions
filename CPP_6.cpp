f49b2bc2ed0847659860712fca9f6ebbe7507d40
beep boop <githubbot@vadim.me>
2024-07-31 11:32:09 +0200

    Solution 94 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/eweb77sr Prompt:       |                           ^~~~~~ 0b06772e-eb3d-49cf-bb2e-e0ffffc6b01e.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0b06772e-eb3d-49cf-bb2e-e0ffffc6b01e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0b06772e-eb3d-49cf-bb2e-e0ffffc6b01e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0b06772e-eb3d-49cf-bb2e-e0ffffc6b01e.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0b06772e-eb3d-49cf-bb2e-e0ffffc6b01e.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 0b06772e-eb3d-49cf-bb2e-e0ffffc6b01e.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 #include <string>
 
-bool issame(const std::vector<int> &a, const std::vector<int> &b) {
+bool issame(const std:: vector<int>& a, const std:: vector<int>& b) {
     return a == b;
 }
 
