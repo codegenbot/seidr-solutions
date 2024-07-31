59475f6df39c07fd6c3be246d51c5fcff3c71f10
beep boop <githubbot@vadim.me>
2024-07-31 14:27:58 +0200

    Solution 46 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vd6gxvdo Prompt: 7da82e7d-2d39-412e-ac73-ae7dcf426163.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 7da82e7d-2d39-412e-ac73-ae7dcf426163.cpp:39:5: note: ‘int main()’ previously def ined here    39 | int main() {       |     ^~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -36,8 +36,4 @@ string intersection(vector<int> interval1, vector<int> interval2) {
     }
 }
 
-int main() {
-    assert(intersection({-2, -2}, {-3, -2}) == "NO");
-
-    return 0;
-}
+assert(intersection({-2, -2}, {-3, -2}) == "NO");
