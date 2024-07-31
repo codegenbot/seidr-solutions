75246bd205aa4cc8582f41e0f97bf1afd679c648
beep boop <githubbot@vadim.me>
2024-07-31 14:22:19 +0200

    Solution 16 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vd6gxvdo Prompt:       |                           ^~~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:9:5: error: expected unqualified-id bef ore ‘if’     9 |     if (intersectionStart > intersectionEnd) {       |     ^~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:15:5: error: expected unqualified-id be fore ‘if’    15 |     if (isPrime(intersectionLength)) {       |     ^~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:17:7: error: expected unqualified-id be fore ‘else’    17 |     } else {       |       ^~~~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:20:1: error: expected declaration befor e ‘}’ token    20 | }       | ^ In file included from 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:22: 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp: In function ‘int main()’: 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:24:13: error: ‘intersection’ was not de clared in this scope; did you mean ‘intersectionEnd’?    24 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,28 +1,11 @@
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <cassert>
-
-bool isPrime(int num) {
-    if (num < 2) {
-        return false;
-    }
-    for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-std::string intersection(std::pair<int, int> interval1, std::pair<int, int> interval2) {
-    int start1 = interval1.first;
-    int end1 = interval1.second;
-    int start2 = interval2.first;
-    int end2 = interval2.second;
+string intersection(vector<int> interval1, vector<int> interval2) {
+    int start1 = interval1[0];
+    int end1 = interval1[1];
+    int start2 = interval2[0];
+    int end2 = interval2[1];
     
-    int intersectionStart = std::max(start1, start2);
-    int intersectionEnd = std::min(end1, end2);
+    int intersectionStart = max(start1, start2);
+    int intersectionEnd = min(end1, end2);
     
     if (intersectionStart > intersectionEnd) {
         return "NO";
@@ -35,10 +18,4 @@ std::string intersection(std::pair<int, int> interval1, std::pair<int, int> inte
     } else {
         return "NO";
     }
-}
-
-int main() {
-    assert(intersection({-2, -2}, {-3, -2}) == "NO");
-    
-    return 0;
 }
