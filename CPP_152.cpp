7837c7f0ba7d5e2ceacf178350e1d574f9fd6295
beep boop <githubbot@vadim.me>
2024-07-31 19:12:55 +0200

    Solution 8 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jwjhex66 Prompt: 475e686d-feb7-480f-aebe-37e5b7b9d0eb.cpp:27:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    27 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 475e686d-feb7-480f-aebe-37e5b7b9d0eb.cpp:16:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    16 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 475e686d-feb7-480f-aebe-37e5b7b9d0eb.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 475e686d-feb7-480f-aebe-37e5b7b9d0eb.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -13,9 +13,4 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     return result;
 }
 
-int main() {
-    vector<int> result = compare({1,2,3,5}, {-1,2,3,4});
-    assert(result == vector<int>{2, 0, 0, 1});
-    
-    return 0;
-}
+```
