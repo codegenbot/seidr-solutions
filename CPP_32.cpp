0fbf0e71450e08d054aa07da8d22e5059a35fad6
beep boop <githubbot@vadim.me>
2024-07-31 10:34:05 +0200

    Solution 67 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f3iy35mq Prompt: 72b36d6f-013e-4865-a8a2-82012d3fdab5.cpp:51:16: error: expected primary-expressi on before ‘double’    51 |         vector<double> coeffs = {};       |                ^~~~~~ 72b36d6f-013e-4865-a8a2-82012d3fdab5.cpp:56:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    56 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 72b36d6f-013e-4865-a8a2-82012d3fdab5.cpp:59:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    59 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff 72b36d6f-013e-4865-a8a2-82012d3fdab5.cpp:59:36: error: ‘find_zero’ cannot be use d as a function    59 |         solution = find_zero(coeffs);       |                                    ^ In file included from 72b36d6f-013e-4865-a8a2-82012d3fdab5.cpp:43: 72b36d6f-013e-4865-a8a2-82012d3fdab5.cpp:60:25: error: ‘poly’ cannot be used as a function    60 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ~~~~^~~~~~~~~~~~~~~~~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,13 +2,13 @@
 int main() {
     int n;
     cin >> n;
-    vector<double> coeffs = {}; 
-    double val;
+    vector<double> coeffs;  
     for (int i = 0; i < n; i++) {
-        cin >> val;
-        coeffs.push_back(val); 
+        double vals;
+        cin >> vals;
+        coeffs.push_back(vals); 
     }
-    double solution = find_zero(coeffs);
+    double solution = find_zero(coeffs); 
     assert(abs(poly(coeffs, solution)) < 1e-3);
     cout << fixed << setprecision(2) << solution << endl;
     return 0;
