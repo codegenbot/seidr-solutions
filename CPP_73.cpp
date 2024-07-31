31ba403efac3c7b14ce6d0c11419685e60e61f81
beep boop <githubbot@vadim.me>
2024-07-31 11:46:35 +0200

    Solution 27 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/rimxs9mf Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from bfb2f073-f806-4f10-b1a6-d216bddda291.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,10 +1,15 @@
+Here is the modified code:
+
 ```cpp
 #include <vector>
 #include <algorithm>
 
+using namespace std;
+typedef long size_type;
+
 int smallest_change(std::vector<int> arr) {
     int n = arr.size();
-    std::vector<std::vector<int>> dp(n+1, std::vector<int>(n+1));
+    std::vector<std::vector<size_type>> dp(n+1, std::vector<size_type>(n+1));
 
     for (int i = 0; i <= n; i++) {
         dp[i][i] = 0;
@@ -17,16 +22,18 @@ int smallest_change(std::vector<int> arr) {
             if (arr[i] == arr[j]) {
                 dp[i][j] = dp[i+1][j-1].size();
             } else {
-                for(int k = 0; k <= 2; k++){
-                    if(k == 0) dp[i][j] = dp[i+1][j].size() + 1;
-                    else if(k == 1) dp[i][j] = dp[i][j-1].size() + 1;
-                    else dp[i][j] = dp[i+1][j-1].size() + 2;
+                std::vector<size_type> options;
+                for(int k = 0; k < 3; k++) {
+                    options.push_back(dp[i+1][j].size() + (k == 0));
+                    options.push_back(dp[i][j-1].size() + (k == 1));
+                    options.push_back(dp[i+1][j-1].size() + 2);
                 }
+                dp[i][j] = *std::min_element(options.begin(), options.end());
             }
         }
     }
 
-    return dp[0].at(n);
+    return dp[0].at(n-1);
 }
 
 int main() { return smallest_change({0, 1}); }
