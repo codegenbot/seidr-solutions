e8d7abce62f24a95aaf9eb14bd5f43a7e8f64747
beep boop <githubbot@vadim.me>
2024-07-31 14:47:29 +0200

    Solution 24 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2qkg41co Prompt:       |                    ^~~ 38790da5-610e-471e-bfb2-8a246848ffce.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 38790da5-610e-471e-bfb2-8a246848ffce.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 38790da5-610e-471e-bfb2-8a246848ffce.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 38790da5-610e-471e-bfb2-8a246848ffce.cpp:17: 38790da5-610e-471e-bfb2-8a246848ffce.cpp: In function ‘int main()’: 38790da5-610e-471e-bfb2-8a246848ffce.cpp:27:20: error: ‘make_a_pile’ was not dec lared in this scope    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 38790da5-610e-471e-bfb2-8a246848ffce.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,37 +1,10 @@
 #include <vector>
-
 bool issame(const vector<int>& a, const vector<int>& b) {
     if (a.size() != b.size())
         return false;
-    for (int i = 0; i < a.size(); i++)
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i])
             return false;
-    return true;
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> pile = make_a_pile(n);
-    cout << "Pile: ";
-    for (int x : pile)
-        cout << x << " ";
-    cout << endl;
-    if (!issame({1, 2}, pile))
-        assert(false);
-    return 0;
-}
-
-vector<int> make_a_pile(int n) {
-    vector<int> pile;
-    int count = 1;
-    while (n > 0) {
-        if (n % 2 == 1)
-            pile.push_back(count);
-        else
-            pile.push_back(count + 1);
-        count += 2;
-        n--;
     }
-    return pile;
+    return true;
 }
