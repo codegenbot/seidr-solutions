b3330b287a71c094dd376b496bf9c0882d375e15
beep boop <githubbot@vadim.me>
2024-07-31 13:49:37 +0200

    Solution 8 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4as0kbes Prompt:       |                             ^~~ 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp:23:33: error: expected primary-expressi on before ‘char’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp:23:38: error: expected primary-expressi on before ‘int’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp:23:44: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp:22: 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp: In function ‘int main()’: 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp:37:20: error: ‘histogram’ was not decla red in this scope    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 88cec04b-5fa4-4b74-aeb1-c0f87d68c7f5.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
 #include <map>
 #include <cassert>
 
 using namespace std;
 
+bool issame(map<char, int> a, map<char, int> b) {
+    return a == b;
+}
+
+map<char, int> histogram(string test);
+
+int main() {
+    assert(issame(histogram("a"), {{'a', 1}}));
+    return 0;
+}
+
 map<char, int> histogram(string test) {
     map<char, int> result;
     map<char, int> count;
     int maxCount = 0;
-    
+
     for (char c : test) {
         if (c != ' ') {
             count[c]++;
             maxCount = max(maxCount, count[c]);
         }
     }
-    
+
     for (auto& entry : count) {
         if (entry.second == maxCount) {
             result[entry.first] = entry.second;
         }
     }
-    
+
     return result;
-}
-
-bool issame(map<char, int> a, map<char, int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(histogram("a"), {{'a', 1}}));
-    
-    return 0;
 }
