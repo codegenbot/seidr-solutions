7491fc06281a691a7576521abbb04e361760012f
beep boop <githubbot@vadim.me>
2024-07-31 08:54:30 +0200

    Solution 6 of CPP/75, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jptyl66i Prompt: ore ‘for’     2 |     for (int i = 2; i * i <= a; i++) {       |     ^~~ 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 2; i * i <= a; i++) {       |                     ^ 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp:2:33: error: ‘i’ does not name a type     2 |     for (int i = 2; i * i <= a; i++) {       |                                 ^ 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp:7:5: error: expected unqualified-id bef ore ‘return’     7 |     return true;       |     ^~~~~~ 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp:8:1: error: expected declaration before  ‘}’ token     8 | }       | ^ In file included from 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp:10: 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp: In function ‘int main()’: 2f7de904-281b-4eaa-b9f2-681ee729a444.cpp:12:13: error: ‘is_multiply_prime’ was n ot declared in this scope    12 |     assert (is_multiply_prime(11 * 13 * 7) == true);       |             ^~~~~~~~~~~~~~~~~

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -1,48 +1,9 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool is_prime(int num) {
-    if (num < 2) {
-        return false;
-    }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
+bool is_multiply_prime(int a) {
+    if (a <= 1) return false;
+    for (int i = 2; i * i <= a; i++) {
+        if (a % i == 0) {
             return false;
         }
     }
     return true;
-}
-
-bool is_multiply_prime(int a) {
-    vector<int> primes;
-    for (int i = 2; i <= a; i++) {
-        if (is_prime(i)) {
-            primes.push_back(i);
-        }
-    }
-
-    for (int i = 0; i < primes.size(); i++) {
-        for (int j = i; j < primes.size(); j++) {
-            for (int k = j; k < primes.size(); k++) {
-                if (primes[i] * primes[j] * primes[k] == a) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
-int main() {
-    int num;
-    cin >> num;
-    if (is_multiply_prime(num)) {
-        cout << "true" << endl;
-    } else {
-        cout << "false" << endl;
-    }
-
-    return 0;
 }
