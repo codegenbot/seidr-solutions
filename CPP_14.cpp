6aaf96be2c093ad4d47cff0f7e05a2be269e19ad
beep boop <githubbot@vadim.me>
2024-07-31 07:40:35 +0200

    Solution 18 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a18f4cm5 Prompt:    34 | int main(){       |     ^~~~ 048b4702-3348-4282-8c8d-cd6edaccd7ac.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main(){       |     ^~~~ In file included from 048b4702-3348-4282-8c8d-cd6edaccd7ac.cpp:25: 048b4702-3348-4282-8c8d-cd6edaccd7ac.cpp: In function ‘int main()’: 048b4702-3348-4282-8c8d-cd6edaccd7ac.cpp:35:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    35 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 048b4702-3348-4282-8c8d-cd6edaccd7ac.cpp:15:6: note: candidate: ‘bool issame(con st std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx 11::basic_string<char> >&)’    15 | bool issame(const vector<string>& a, const vector<string>& b){       |      ^~~~~~ 048b4702-3348-4282-8c8d-cd6edaccd7ac.cpp:26:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    26 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-vector<string> all_prefixes(string str) {
+vector<string> all_prefixes(string str){
     vector<string> result;
     for (int i = 1; i <= str.length(); ++i) {
         result.push_back(str.substr(0, i));
@@ -12,12 +12,12 @@ vector<string> all_prefixes(string str) {
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b) {
+bool issame(const vector<string>& a, const vector<string>& b){
     return a == b;
 }
 
-int main() {
-    assert(issame(all_prefixes("WWW"), { "W", "WW", "WWW" }));
+int main(){
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
 
     return 0;
 }
