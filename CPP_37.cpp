59954e8d4553ab4feeaf872b27f5bcd1f610f4f7
beep boop <githubbot@vadim.me>
2024-07-31 08:12:13 +0200

    Solution 8 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/26vdj7a8 Prompt:    21 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ e6da62f8-9dde-4745-a43d-6eba549f3094.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ e6da62f8-9dde-4745-a43d-6eba549f3094.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from e6da62f8-9dde-4745-a43d-6eba549f3094.cpp:20: e6da62f8-9dde-4745-a43d-6eba549f3094.cpp: In function ‘int main()’: e6da62f8-9dde-4745-a43d-6eba549f3094.cpp:30:20: error: ‘sort_even’ was not decla red in this scope    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ e6da62f8-9dde-4745-a43d-6eba549f3094.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,16 +1,12 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
-
-bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
-}
+#include <cassert>
 
 std::vector<float> sort_even(std::vector<float> l) {
-    std::vector<float> even_indices;
     std::vector<float> sorted_even_indices;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            even_indices.push_back(l[i]);
             sorted_even_indices.push_back(l[i]);
         }
     }
@@ -26,6 +22,6 @@ std::vector<float> sort_even(std::vector<float> l) {
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    assert(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) == std::vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10});
     return 0;
 }
