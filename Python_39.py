27f9d4c16b45e939acfb7eaa574128ba298beb13
beep boop <githubbot@vadim.me>
2024-07-31 13:55:01 +0200

    Solution 72 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/j5escoff Prompt: Complete the following code given the task description and function signature.

--- Python_39.py
+++ Python_39.py
@@ -1,17 +1,17 @@
-```
 def prime_fib(n: int):
-    fib_num = 0
     a, b = 0, 1
     count = 0
     while True:
-        fib_num += 1
-        if is_prime(fib_num):
+        if b > n:
+            return a
+        if is_prime(b):
             count += 1
             if count == n:
-                return fib_num
+                return b
         a, b = b, a + b
 
-def is_prime(num: int) -> bool:
+
+def is_prime(num: int):
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
