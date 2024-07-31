51e6472efb965044cc9d7047f7265a943b7926f7
beep boop <githubbot@vadim.me>
2024-07-31 14:25:34 +0200

    Solution 34 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vd6gxvdo Prompt:       |                           ^~~ 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp:9:5: error: expected unqualified-id bef ore ‘if’     9 |     if (intersectionStart > intersectionEnd) {       |     ^~ 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp:15:5: error: expected unqualified-id be fore ‘if’    15 |     if (isPrime(intersectionLength)) {       |     ^~ 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp:17:7: error: expected unqualified-id be fore ‘else’    17 |     } else {       |       ^~~~ 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp:20:1: error: expected declaration befor e ‘}’ token    20 | }       | ^ In file included from 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp:22: 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp: In function ‘int main()’: 084113b9-0ad8-472e-a4e6-96fe349a72e1.cpp:24:13: error: ‘intersection’ was not de clared in this scope; did you mean ‘intersectionEnd’?    24 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -1,17 +1,5 @@
 string intersection(vector<int> interval1, vector<int> interval2);
 
-bool isPrime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 string intersection(vector<int> interval1, vector<int> interval2) {
     int start1 = interval1[0];
     int end1 = interval1[1];
