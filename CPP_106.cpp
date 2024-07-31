5911ecbfa0f565b82c525d905a70c18d20e4815b
beep boop <githubbot@vadim.me>
2024-07-31 13:37:29 +0200

    Solution 17 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2ovmguks Prompt:       |                    ^~~ 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp:24: 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp: In function ‘int main()’: 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp:34:20: error: ‘f’ was not declared in t his scope    34 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 74c7eaec-def4-46fe-ba1b-dd0727574b21.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,35 +1,33 @@
 #include <vector>
 #include <cassert>
 
-std::vector<int> f(int n) {
+std::vector<int> f(int n){
     std::vector<int> result(n);
     int sum = 0;
     int factorial = 1;
-
-    for(int i = 0; i < n; i++) {
-        if(i % 2 == 0) {
+    
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0){
             if(i == 0)
                 result[i] = 1;
-            else {
+            else{
                 factorial *= i;
                 result[i] = factorial;
             }
         }
-        else {
+        else{
             sum += i;
             result[i] = sum;
         }
     }
-
+    
     return result;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-int main() {
+int main(){
     assert(issame(f(3), {1, 2, 6}));
-
-    return 0;
 }
