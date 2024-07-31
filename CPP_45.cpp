e6e01a709771e6fa26bee99a2d4868d367292c3e
beep boop <githubbot@vadim.me>
2024-07-31 10:50:06 +0200

    Solution 7 of CPP/45, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vpljbp97 Prompt: f7ceac44-0c70-43a4-8158-30677051e30b.cpp: In function ‘int main()’: f7ceac44-0c70-43a4-8158-30677051e30b.cpp:8:5: error: ‘assert’ was not declared i n this scope     8 |     assert(std::abs(triangle_area(10, 8) - 40.0)<1e-4);       |     ^~~~~~ f7ceac44-0c70-43a4-8158-30677051e30b.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <cmath>   +++ |+#include <cassert>     2 | f7ceac44-0c70-43a4-8158-30677051e30b.cpp: At global scope: f7ceac44-0c70-43a4-8158-30677051e30b.cpp:13:5: error: redefinition of ‘int main( )’    13 | int main(){       |     ^~~~ f7ceac44-0c70-43a4-8158-30677051e30b.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main() {       |     ^~~~

--- CPP_45.cpp
+++ CPP_45.cpp
@@ -1,10 +1,10 @@
 #include <cmath>
+#include <cassert>
 
 float triangle_area(float a, float h) {
-    return (a * std::absf(h)) / 2.0f;
+    return (a * std::abs(h)) / 2.0f;
 }
 
 int main() {
     assert(std::abs(triangle_area(10, 8) - 40.0)<1e-4);
-    return 0;
 }
