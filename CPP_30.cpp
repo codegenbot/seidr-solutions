286b4adb0ebc552b26bd03c196ed5c56da1f8f59
beep boop <githubbot@vadim.me>
2024-07-31 09:58:30 +0200

    Solution 7 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/909kifjo Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from e704fee0-c0f4-4fea-b2fb-32fb7d9a7c2f.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,24 +1,35 @@
 #include <vector>
 
-bool isSame(vector<float> a, vector<float> b) {
+bool issame(vector<float> a, vector<float> b);
+vector<float> get_positive(vector<float> l);
+
+bool issame(vector<float> a, vector<float> b) {
     if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size();i++){
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<float> getPositive(vector<float> l){
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
-    for(float num : l) {
-        if(num > 0) {
+    for (float num : l) {
+        if (num > 0) {
             result.push_back(num);
         }
     }
     return result;
 }
 
-int main(){
-    assert(isSame(getPositive({}), {}) == false); 
+int main() {
+    vector<float> input;
+    cout << "Enter numbers separated by space: ";
+    while (cin >> input.push_back(stof)) {}
+    vector<float> positive = get_positive(input);
+    if (issame(positive, input)) {
+        cout << "All numbers are positive." << endl;
+    } else {
+        cout << "Not all numbers are positive." << endl;
+    }
     return 0;
 }
