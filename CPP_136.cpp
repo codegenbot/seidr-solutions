b9056a54bc3d48c24f48dc7162685b44d8ce4d8f
beep boop <githubbot@vadim.me>
2024-07-31 17:15:12 +0200

    Solution 60 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/dap2gvdl Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 13bd9e96-0d8a-4df3-b837-86ae01ff38c2.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 13bd9e96-0d8a-4df3-b837-86ae01ff38c2.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 13bd9e96-0d8a-4df3-b837-86ae01ff38c2.cpp:20: 13bd9e96-0d8a-4df3-b837-86ae01ff38c2.cpp: In function ‘int main()’: 13bd9e96-0d8a-4df3-b837-86ae01ff38c2.cpp:30:20: error: ‘largest_smallest_integer s’ was not declared in this scope    30 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 13bd9e96-0d8a-4df3-b837-86ae01ff38c2.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -9,17 +9,20 @@ bool issame(vector<int> a, vector<int> b){
 }
 
 vector<int> largest_smallest_integers_cpp136(vector<int> lst){
-    vector<int> result = {0, 0};
-    int largest_negative = 0;
-    int smallest_positive = 0;
+    vector<int> result = {INT_MIN, INT_MAX};
+    int largest_negative = INT_MIN;
+    int smallest_positive = INT_MAX;
 
     for (int num : lst) {
-        if (num < 0 && num < largest_negative) {
+        if (num < 0 && num > largest_negative) {
             largest_negative = num;
-        } else if (num > 0 && (num < smallest_positive || smallest_positive == 0)) {
+        } else if (num > 0 && num < smallest_positive) {
             smallest_positive = num;
         }
     }
+
+    if (largest_negative == INT_MIN) largest_negative = 0;
+    if (smallest_positive == INT_MAX) smallest_positive = 0;
 
     result[0] = largest_negative;
     result[1] = smallest_positive;
