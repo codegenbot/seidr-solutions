4b991b79363d8dbfd49570bfe7db8add94a23e15
beep boop <githubbot@vadim.me>
2024-07-31 15:31:09 +0200

    Solution 3 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/4rm5otal Prompt: Complete the following code given the task description and function signature.

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,8 +1,16 @@
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
 vector<int> sort_vector(vector<int> arr) {
-    return vector<int>(arr.begin(), arr.end());
+    sort(arr.begin(), arr.end(),
+         [](int a, int b) {
+             int ones_a = __builtin_popcount(a);
+             int ones_b = __builtin_popcount(b);
+
+             if (ones_a == ones_b)
+                 return a < b;
+             else
+                 return ones_a < ones_b;
+         });
+    return arr;
 }
