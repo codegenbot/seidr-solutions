430122ee528b7ac78ef3b3b16b1abb10cc60c587
beep boop <githubbot@vadim.me>
2024-07-31 10:08:55 +0200

    Solution 1 of Python/163, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/26mc5fz6 Prompt: Complete the following code given the task description and function signature.

--- Python_163.py
+++ Python_163.py
@@ -1,3 +1,8 @@
-```
 def generate_integers(a, b):
-    return sorted([i for i in range(min(a,b),max(a,b)+1) if str(i).count('0')%2==0])
+    return sorted(
+        [
+            i
+            for i in range(min(a, b), max(a, b) + 1)
+            if str(i).isdigit() and int(str(i)) % 2 == 0
+        ]
+    )
