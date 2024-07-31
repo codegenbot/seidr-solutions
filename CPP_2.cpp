2bfd972e9098c717552ddc5e87d0162883121f43
beep boop <githubbot@vadim.me>
2024-07-31 18:09:36 +0200

    Solution 67 of CPP/2, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7sgxhaht Prompt: 2dc237ce-c178-4476-a914-a1ecb648ae2e.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ 2dc237ce-c178-4476-a914-a1ecb648ae2e.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~

--- CPP_2.cpp
+++ CPP_2.cpp
@@ -5,11 +5,9 @@ double truncate_number(double number) {
     return number - std::floor(number);
 }
 
-int main() {
+void main() {
     double number;
     std::cin >> number;
     double fractionalPart = truncate_number(number);
     std::cout << fractionalPart << std::endl;
-    
-    return 0;
 }
