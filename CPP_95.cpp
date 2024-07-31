1372777b5ac446f47c2486774f1a4f4a693aa93a
beep boop <githubbot@vadim.me>
2024-07-31 15:56:21 +0200

    Solution 31 of CPP/95, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/h9ij34ie Prompt:       |                      ^~~ 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp:1:26: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string,string> dict){       |                          ^~~~~~ 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp:1:33: error: ‘string’ was not declared in this scope     1 | bool check_dict_case(map<string,string> dict){       |                                 ^~~~~~ 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp:1:41: error: ‘dict’ was not declared in  this scope     1 | bool check_dict_case(map<string,string> dict){       |                                         ^~~~ 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp:1:45: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool check_dict_case(map<string,string> dict){       |                                             ^ In file included from 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp:20: 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp: In function ‘int main()’: 5a515ec5-b8e8-48f8-aaad-f3a064049d45.cpp:22:28: error: ‘check_dict_case’ cannot be used as a function    22 |     assert (check_dict_case({}) == false);       |             ~~~~~~~~~~~~~~~^~~~

--- CPP_95.cpp
+++ CPP_95.cpp
@@ -1,7 +1,4 @@
 #include <string>
-#include <map>
-#include <iostream>
-
 using namespace std;
 
 bool check_dict_case(map<string,string> dict){
@@ -24,8 +21,7 @@ bool check_dict_case(map<string,string> dict){
 }
 
 int main() {
-    map<string, string> empty_dict;
-    cout << check_dict_case(empty_dict) << endl;
-    
+    std::map<string, string> empty_dict;
+    check_dict_case(empty_dict);
     return 0;
 }
