16c484a2a2b5ee6104d1943619356946e2b65a25
beep boop <githubbot@vadim.me>
2024-07-31 14:56:15 +0200

    Solution 92 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5ztgbbje Prompt:       |                    ^~~ f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp:19: f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp: In function ‘int main()’: f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp:29:20: error: ‘strange_sort_list’ was n ot declared in this scope    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ f72e2c66-c892-42b0-801b-c4d0a06adb51.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -9,7 +9,7 @@ vector<int> strange_sort_vector(vector<int> lst);
 
 int main() {
     // You can use this function to test the output
-    vector<int> result = strange_sort_vector({1, 2, 3, 4, 5});
+    vector<int> result = strange_sort_vector({1, 2, 3});
     for (int i : result) {
         cout << i << " ";
     }
@@ -21,14 +21,13 @@ vector<int> strange_sort_vector(vector<int> lst) {
     vector<int> result;
     if (lst.empty()) return result;
 
-    vector<int> minMax;
     while (!lst.empty()) {
         int min = *min_element(lst.begin(), lst.end());
         int max = *max_element(lst.begin(), lst.end());
-        minMax.push_back(min);
-        minMax.push_back(max);
+        result.push_back(min);
+        result.push_back(max);
         lst.erase(remove(lst.begin(), lst.end(), min), lst.end());
         lst.erase(remove(lst.begin(), lst.end(), max), lst.end());
     }
-    return minMax;
+    return result;
 }
