373083d071ca681290042ee2d7f954b149d13ecb
beep boop <githubbot@vadim.me>
2024-07-31 10:06:14 +0200

    Solution 2 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jiiiryu1 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e486cf26-d666-4aae-9628-71d10798d8f8.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e486cf26-d666-4aae-9628-71d10798d8f8.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e486cf26-d666-4aae-9628-71d10798d8f8.cpp:5: e486cf26-d666-4aae-9628-71d10798d8f8.cpp: In function ‘int main()’: e486cf26-d666-4aae-9628-71d10798d8f8.cpp:15:20: error: ‘minPath’ was not declare d in this scope    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ e486cf26-d666-4aae-9628-71d10798d8f8.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,30 +1,22 @@
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 vector<int> minPath(vector<vector<int>> grid, int k){
-    int n = grid.size();
-    int row = 0, col = 0;
-    vector<int> path;
+    vector<int> result;
     
-    for (int i = 0; i < k; ++i) {
-        path.push_back(grid[row][col]);
-        if ((row + col) % 2 == 0) {
-            if (col == n - 1) {
-                ++row;
-            } else if (row == 0) {
-                ++col;
-            } else {
-                --row;
-                ++col;
-            }
-        } else {
-            if (row == n - 1) {
-                ++col;
-            } else if (col == 0) {
-                ++row;
-            } else {
-                ++row;
-                --col;
+    for(int i = 0; i < k / grid.size(); ++i){
+        for(const auto& row : grid){
+            for(int num : row){
+                result.push_back(num);
             }
         }
     }
     
-    return path;
+    return result;
+}
+
+int main(){
+    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
+    return 0;
 }
