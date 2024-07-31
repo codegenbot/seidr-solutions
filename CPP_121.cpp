099c598abe9bfc0e11ba5a914c809883c188a531
beep boop <githubbot@vadim.me>
2024-07-31 13:25:59 +0200

    Solution 19 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jyynd4th Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,16 +1,22 @@
+```cpp
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int solutions(vector<int> numbers) {
-    int sum = 0;
-    for (char c : numbers) {
-        if (c == ' ') break; 
-        sum += c - '0';
+vector<int> lst;
+cout << "Enter numbers (space-separated): ";
+string str;
+getline(cin, str);
+int num = 0;
+for (char c : str) {
+    if (c == ' ') break; 
+    num = num * 10 + (c - '0');
+    lst.push_back(num);
+}
+int sum = 0;
+for (int i = 1; i < lst.size(); i++) {
+    if (lst[i] % 2 != 0) {
+        sum += lst[i];
     }
-    return sum;
 }
-
-int main() {
-    cout << "Sum of odd numbers: " << solutions({3, 13, 2, 9}) << endl;
-}
+cout << "Sum of odd numbers: " << sum << endl;
