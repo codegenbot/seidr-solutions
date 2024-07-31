d64cf12cb50a55f314f7a3a60a51c33bfec36126
beep boop <githubbot@vadim.me>
2024-07-31 14:22:40 +0200

    Solution 57 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/azv1r5gd Prompt:  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d31ee697-a4e3-4c71-a136-0d291e424766.cpp:11:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d31ee697-a4e3-4c71-a136-0d291e424766.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d31ee697-a4e3-4c71-a136-0d291e424766.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d31ee697-a4e3-4c71-a136-0d291e424766.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d31ee697-a4e3-4c71-a136-0d291e424766.cpp:10: d31ee697-a4e3-4c71-a136-0d291e424766.cpp: In function ‘int main()’: d31ee697-a4e3-4c71-a136-0d291e424766.cpp:20:20: error: ‘get_odd_collatz’ was not  declared in this scope    20 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,21 +1,8 @@
 #include <vector>
 #include <algorithm>
 
-int countUniquePairs(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
-    int uniquePairs = 0;
-    for (int i = 0; i < a.size(); ++i) {
-        if (i == 0 || a[i] != a[i - 1]) {
-            for (int j = 0; j < b.size(); ++j) {
-                if (j == 0 || b[j] != b[j - 1]) {
-                    if (a[i] == b[j]) {
-                        ++uniquePairs;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    return uniquePairs;
+    return a == b;
 }
