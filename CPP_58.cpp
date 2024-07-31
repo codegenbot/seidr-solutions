e94156aba1f211a747d3bebaddf05d011d8e3e8b
beep boop <githubbot@vadim.me>
2024-07-31 11:27:41 +0200

    Solution 25 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7rx4riev Prompt: ecf85df7-bb56-4d39-8414-521d421538f2.cpp:36:27: error: invalid initialization of  reference of type ‘std::vector<std::vector<int> >&’ from expression of type ‘__ gnu_cxx::__alloc_traits<std::allocator<std::vector<int> >, std::vector<int> >::v alue_type’ {aka ‘std::vector<int>’}    36 |             same &= issame(v[0], v[i]);       |                     ~~~~~~^~~~~~~~~~~~ ecf85df7-bb56-4d39-8414-521d421538f2.cpp:9:34: note: in passing argument 1 of ‘b ool issame(std::vector<std::vector<int> >&, std::vector<std::vector<int> >&)’     9 | bool issame(vector<vector<int>>& a, vector<vector<int>>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~^ ecf85df7-bb56-4d39-8414-521d421538f2.cpp:40:8: error: ‘same’ was not declared in  this scope    40 |     if(same) {       |        ^~~~ ecf85df7-bb56-4d39-8414-521d421538f2.cpp: At global scope: ecf85df7-bb56-4d39-8414-521d421538f2.cpp:78:5: error: redefinition of ‘int main( )’    78 | int main(){       |     ^~~~ ecf85df7-bb56-4d39-8414-521d421538f2.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -27,41 +27,56 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 }
 
 int main() {
-    vector<vector<int>> v;
+    int n;
+    cin >> n;
 
-    bool same = true;
+    vector<vector<int>> v(n);
 
-    if(v.size() > 1) {
-        same = issame(v[0], v[1]);
-        for(int i=2; i<v.size(); i++) {
-            same &= issame(v[0], v[i]);
+    for(int i=0; i<n; i++) {
+        int m;
+        cin >> m;
+
+        v[i].resize(m);
+
+        for(int j=0; j<m; j++) {
+            cin >> v[i][j];
         }
     }
 
-    if(same) {
-        vector<int> commonList;
-        for(int i=0; i<v[0].size(); i++) {
-            bool found = true;
+    if(v.size() > 1) {
+        bool same = true;
+        for(int i=2; i<v.size(); i++) {
+            same &= issame(v[0], v[i]);
+        }
 
-            for(int j=1; j<v.size(); j++) {
-                if(find(v[j].begin(), v[j].end(), v[0][i]) == v[j].end()) {
-                    found = false;
-                    break;
+        if(same) {
+            vector<int> commonList;
+
+            for(int i=0; i<v[0].size(); i++) {
+                bool found = true;
+
+                for(int j=1; j<v.size(); j++) {
+                    if(find(v[j].begin(), v[j].end(), v[0][i]) == v[j].end()) {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if(found) {
+                    commonList.push_back(v[0][i]);
                 }
             }
 
-            if(found) {
-                commonList.push_back(v[0][i]);
+            cout << "Common elements: ";
+            for(int i=0; i<commonList.size(); i++) {
+                cout << commonList[i] << " ";
             }
+            cout << endl;
+        } else {
+            cout << "No common elements." << endl;
         }
-
-        cout << "Common elements: ";
-        for(int i=0; i<commonList.size(); i++) {
-            cout << commonList[i] << " ";
-        }
-        cout << endl;
     } else {
-        cout << "No common elements." << endl;
+        cout << "At least two lists required to find common elements." << endl;
     }
 
     return 0;
