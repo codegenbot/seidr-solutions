808f16c36e65d2198384fc27de1f5152fc6794a0
beep boop <githubbot@vadim.me>
2024-07-31 18:26:10 +0200

    Solution 70 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/3oljmzxa Prompt:   731 |         __copy_move_b(const _Tp* __first, const _Tp* __last, _Tp* __resu lt)       |         ^~~~~~~~~~~~~ /usr/include/c++/11/bits/stl_algobase.h:731:9: note:   template argument deducti on/substitution failed: /usr/include/c++/11/bits/stl_algobase.h:760:65: note:   deduced conflicting type s for parameter ‘_Tp’ (‘float’ and ‘const float’)   758 |     return std::__copy_move_backward<_IsMove,       |            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   759 |                                      __memcpyable<_BI2, _BI1>::__value,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   760 |                                      _Category>::__copy_move_b(__first,       |                                      ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~   761 |                                                                __last,       |                                                                ~~~~~~~   762 |                                                                __result) ;       |                                                                ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -10,9 +10,15 @@ std::vector<float> find_closest_elements(const std::vector<float>& nums) {
         return result;
     }
 
-    std::sort(nums.begin(), nums.end());
-    result.push_back(nums[0]);
-    result.push_back(nums[1]);
+    std::vector<std::pair<float, float>> diff_nums;
+    for (size_t i = 0; i < nums.size() - 1; ++i) {
+        diff_nums.emplace_back(fabs(nums[i] - nums[i + 1]), nums[i]);
+    }
+
+    std::sort(diff_nums.begin(), diff_nums.end());
+
+    result.push_back(diff_nums[0].second);
+    result.push_back(diff_nums[1].second);
 
     return result;
 }
@@ -22,7 +28,7 @@ bool issame(const std::vector<float>& a, const std::vector<float>& b) {
 }
 
 int main() {
-    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {1.1, 2.2});
+    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {3.1, 4.1});
 
     return 0;
 }
