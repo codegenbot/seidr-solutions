e5c10d0aece7da540ee3b925a16991608ad667e4
beep boop <githubbot@vadim.me>
2024-07-31 09:40:57 +0200

    Solution 31 of CPP/23, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/h1jig7as Prompt: 61b393f4-93ed-4fd2-b57c-2754df8364ae.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 61b393f4-93ed-4fd2-b57c-2754df8364ae.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,3 +1,16 @@
-int strlen(string str){
-    return str.length();
+#include <cassert>
+#include <cstring>
+
+int my_strlen(const char* str){
+    int len = 0;
+    while (*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+void testMy_strlen() {
+    const char input[] = "asdasnakj";
+    assert(my_strlen(input) == 11);
 }
