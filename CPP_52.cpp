7c70a68e7048ebe24a4f61dc789cffa4410f08c2
beep boop <githubbot@vadim.me>
2024-07-31 11:07:53 +0200

    Solution 3 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9kgsvap9 Prompt: 077cefdc-03b1-4717-9a4e-4111e27cca2a.cpp: In function ‘int main()’: 077cefdc-03b1-4717-9a4e-4111e27cca2a.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(not(below_threshold({1, 8, 4, 10}, 10)));       |     ^~~~~~ 077cefdc-03b1-4717-9a4e-4111e27cca2a.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | using namespace std; 077cefdc-03b1-4717-9a4e-4111e27cca2a.cpp: At global scope: 077cefdc-03b1-4717-9a4e-4111e27cca2a.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 077cefdc-03b1-4717-9a4e-4111e27cca2a.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,7 +1,8 @@
 #include <vector>
+#include <cassert> 
 using namespace std;
 
-bool below_threshold(vector<std::int> l, int t) {
+bool below_threshold(vector<int> l, int t) {
     for (int num : l) {
         if (num >= t) {
             return false;
@@ -12,4 +13,5 @@ bool below_threshold(vector<std::int> l, int t) {
 
 int main() {
     assert(not(below_threshold({1, 8, 4, 10}, 10)));
+    return 0;
 }
