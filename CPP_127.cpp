21cb16201056e90a02ae0ee8ef50d98818c641d8
beep boop <githubbot@vadim.me>
2024-07-31 14:22:27 +0200

    Solution 17 of CPP/127, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vd6gxvdo Prompt:       |                           ^~~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:9:5: error: expected unqualified-id bef ore ‘if’     9 |     if (intersectionStart > intersectionEnd) {       |     ^~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:15:5: error: expected unqualified-id be fore ‘if’    15 |     if (isPrime(intersectionLength)) {       |     ^~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:17:7: error: expected unqualified-id be fore ‘else’    17 |     } else {       |       ^~~~ 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:20:1: error: expected declaration befor e ‘}’ token    20 | }       | ^ In file included from 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:22: 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp: In function ‘int main()’: 4a0f8adf-f385-47c9-b98b-42094a65be16.cpp:24:13: error: ‘intersection’ was not de clared in this scope; did you mean ‘intersectionEnd’?    24 |     assert (intersection({-2, -2}, {-3, -2}) == "NO");       |             ^~~~~~~~~~~~

--- CPP_127.cpp
+++ CPP_127.cpp
@@ -3,19 +3,25 @@ string intersection(vector<int> interval1, vector<int> interval2) {
     int end1 = interval1[1];
     int start2 = interval2[0];
     int end2 = interval2[1];
-    
+
     int intersectionStart = max(start1, start2);
     int intersectionEnd = min(end1, end2);
-    
+
     if (intersectionStart > intersectionEnd) {
         return "NO";
     }
-    
+
     int intersectionLength = intersectionEnd - intersectionStart;
-    
-    if (isPrime(intersectionLength)) {
-        return "YES";
-    } else {
+
+    if (intersectionLength <= 1) {
         return "NO";
     }
+
+    for (int i = 2; i * i <= intersectionLength; ++i) {
+        if (intersectionLength % i == 0) {
+            return "NO";
+        }
+    }
+
+    return "YES";
 }
