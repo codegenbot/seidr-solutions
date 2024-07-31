1d4a5f59b7e8d12016c78dee28c4feef3896aec7
beep boop <githubbot@vadim.me>
2024-07-31 15:57:43 +0200

    Solution 31 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1kbv1qb4 Prompt: 59bf7bf6-053a-41f9-bd09-70fc1f9886ea.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if ((int)a == a && (int)b == b && (int)c == c) {       | ^~ 59bf7bf6-053a-41f9-bd09-70fc1f9886ea.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return false;       |     ^~~~~~ 59bf7bf6-053a-41f9-bd09-70fc1f9886ea.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 59bf7bf6-053a-41f9-bd09-70fc1f9886ea.cpp:9: 59bf7bf6-053a-41f9-bd09-70fc1f9886ea.cpp: In function ‘int main()’: 59bf7bf6-053a-41f9-bd09-70fc1f9886ea.cpp:11:13: error: ‘any_int’ was not declare d in this scope    11 |     assert (any_int(3.01,4,7)==false);       |             ^~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 
 bool any_int(double a, double b, double c) {
-    return ((int)a == a && (int)b == b && (int)c == c) && (a == b + c || b == a + c || c == a + b);
+    return (int)a == a && (int)b == b && (int)c == c && (a == b + c || b == a + c || c == a + b);
 }
 
 int main() {
