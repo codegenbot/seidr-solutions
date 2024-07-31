486d134426a3eb40cb298b6b267d8d50110d3634
beep boop <githubbot@vadim.me>
2024-07-31 09:31:48 +0200

    Solution 4 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0six8pda Prompt: Complete the following code given the task description and function signature.

--- Python_144.py
+++ Python_144.py
@@ -1,8 +1,4 @@
 def simplify(x, n):
-    x_numer, x_denom = map(int, x.split("/"))
-    y_numer, y_denom = map(int, n.split("/"))
-
-    numerator = x_numer * y_denom
-    denominator = x_denom * y_numer
-
-    return numerator == denominator
+    a, b = map(int, x.split("/"))
+    c, d = map(int, n.split("/"))
+    return (a * d) % (b * c) == 0
