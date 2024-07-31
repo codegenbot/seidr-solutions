e52a5e0ed627218e1d80b4baeea44d7ebea5eaa9
beep boop <githubbot@vadim.me>
2024-07-31 19:53:49 +0200

    Solution 81 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/invesemi Prompt:       |                    ^~~ 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp:18: 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp: In function ‘int main()’: 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp:28:20: error: ‘f’ was not declared in t his scope    28 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 51787992-ae13-4b8b-b29e-e1c731a7da65.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,19 +1,8 @@
 #include <vector>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    std::vector<int> result;
-    int sum = 0;
-    int factorial = 1;
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
     
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 0) {
-            factorial *= i;
-            result.push_back(factorial);
-        } else {
-            sum += i;
-            result.push_back(sum);
-        }
-    }
-    
-    return result;
+    return a == b;
 }
