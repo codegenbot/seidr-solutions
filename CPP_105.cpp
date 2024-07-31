c1b37a51720768e2d350a3dffe281104ae7aeaeb
beep boop <githubbot@vadim.me>
2024-07-31 13:47:31 +0200

    Solution 60 of CPP/105, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0x42macp Prompt:       |                                 ^~~~~~ In file included from 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp:40:44: error: ‘b’ was not declared in t his scope    40 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp:40:45: error: expression list treated a s compound expression in initializer [-fpermissive]    40 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp:39: 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp: In function ‘int main()’: 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp:49:20: error: ‘by_length’ was not decla red in this scope    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 2e245e0a-6c16-4287-8d62-355c023f64a1.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,3 +1,13 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+bool issame(vector<string> a, vector<string> b);
+
 vector<string> by_length(vector<int> arr){
     vector<int> sorted_arr;
     vector<string> result;
