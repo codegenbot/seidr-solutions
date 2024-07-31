bea35a50e4d68b953371e2d76e257e26af52f414
beep boop <githubbot@vadim.me>
2024-07-31 09:32:00 +0200

    Solution 6 of Python/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/0six8pda Prompt: Complete the following code given the task description and function signature.

--- Python_144.py
+++ Python_144.py
@@ -1,17 +1,5 @@
-```
 def simplify(x, n):
-    x_num, x_denom = map(int, x.split('/'))
-    n_num, n_denom = map(int, n.split('/'))
-    
-    common_divisor = min(x_denom, n_denom)
-    
-    while common_divisor > 0:
-        if x_num % common_divisor == 0 and n_num % common_divisor == 0:
-            x_num //= common_divisor
-            n_num //= common_divisor
-        else:
-            break
-        
-        common_divisor = min(x_denom, n_denom)
-    
-    return x_num * n_num == x_denom * n_denom
+    x_num, x_denom = map(int, x.split("/"))
+    n_num, n_denom = map(int, n.split("/"))
+
+    return (x_num * n_denom) % (x_denom * n_num) == 0
