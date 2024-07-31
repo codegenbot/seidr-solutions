a65e4b8b6487d6a8c5bdd3027cda883b17bf8a25
beep boop <githubbot@vadim.me>
2024-07-31 15:20:50 +0200

    Solution 10 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/02lyfggr Prompt:       |                             ^~~ bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp:37:33: error: expected primary-expressi on before ‘char’    37 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp:37:38: error: expected primary-expressi on before ‘int’    37 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp:37:44: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp:36: bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp: In function ‘int main()’: bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp:51:20: error: ‘histogram’ was not decla red in this scope    51 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ bf3bba21-a2b9-4cd0-bcc1-69aad4748664.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,29 +1,34 @@
-map<char, int> histogram(string test) {
-    map<char, int> result;
-    if (test.empty()) return result;
-
-    string str = test;
-    for (char c : unique(str.begin(), str.end())) {
-        int count = 0;
-        size_t pos = 0;
-        while ((pos = str.find(c, pos)) != string::npos) {
-            count++;
-            pos++;
-        }
-        if (count > 0) result[c] = count;
-    }
-
-    map<char, int> maxCountMap;
+map<char,int> histogram(string test){
+    map<char,int> result;
+    string temp = "";
     int maxCount = 0;
-    for (auto it = result.begin(); it != result.end(); ++it) {
-        if (it->second > maxCount) {
-            maxCount = it->second;
-            maxCountMap.clear();
-            maxCountMap[it->first] = it->second;
-        } else if (it->second == maxCount) {
-            maxCountMap[it->first] = it->second;
+
+    for(int i=0; i<test.length(); i++){
+        if(test[i] == ' ' || i == test.length() - 1){
+            if(temp.length() > 0){
+                if(result.find(temp[0]) != result.end()){
+                    result[temp[0]]++;
+                } else {
+                    result[temp[0]] = 1;
+                }
+                temp = "";
+            }
+        } else {
+            temp += test[i];
         }
     }
 
-    return maxCountMap;
+    map<char,int> maxMap;
+
+    for(auto it = result.begin(); it != result.end(); it++){
+        if(it->second > maxCount){
+            maxCount = it->second;
+            maxMap.clear();
+            maxMap[it->first] = it->second;
+        } else if(it->second == maxCount){
+            maxMap[it->first] = it->second;
+        }
+    }
+
+    return maxMap;
 }
