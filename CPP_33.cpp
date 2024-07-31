988b210c806f1eec4146e5c84369ccc2354e2d12
beep boop <githubbot@vadim.me>
2024-07-31 12:10:29 +0200

    Solution 40 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2zg37ebo Prompt:       |                           ^~~~~~ 9a3890a3-f670-447c-b065-88e9223878c6.cpp:37:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 9a3890a3-f670-447c-b065-88e9223878c6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 9a3890a3-f670-447c-b065-88e9223878c6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 9a3890a3-f670-447c-b065-88e9223878c6.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9a3890a3-f670-447c-b065-88e9223878c6.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 9a3890a3-f670-447c-b065-88e9223878c6.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -2,14 +2,12 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
+bool issame(std::vector<int> a, std::vector<int> b);
 
 std::vector<int> sort_third(std::vector<int> l) {
     std::vector<int> l_divisible_by_three;
     for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
+        if (l[i] % 3 == 0) {
             l_divisible_by_three.push_back(l[i]);
         }
     }
@@ -17,7 +15,7 @@ std::vector<int> sort_third(std::vector<int> l) {
     
     std::vector<int> l_prime = l;
     for (int i = 0, j = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
+        if (l[i] % 3 == 0) {
             l_prime[i] = l_divisible_by_three[j++];
         }
     }
@@ -25,8 +23,12 @@ std::vector<int> sort_third(std::vector<int> l) {
     return l_prime;
 }
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
 int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), std::vector<int>({2, 6, 3, 4, 8, 9, 5, 1})));
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
     
     return 0;
 }
