eab3a18c11cec780d87cda10d58363a0bc0c230f
beep boop <githubbot@vadim.me>
2024-07-31 08:47:45 +0200

    Solution 31 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/sub4a649 Prompt: ec2cffcf-8490-434d-a540-92ccddd2d25b.cpp: In function ‘int main()’: ec2cffcf-8490-434d-a540-92ccddd2d25b.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(has_close_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5f) == f alse);       |     ^~~~~~ ec2cffcf-8490-434d-a540-92ccddd2d25b.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <vector>   +++ |+#include <cassert>     3 | #include <ext/new_allocator.h> ec2cffcf-8490-434d-a540-92ccddd2d25b.cpp: At global scope: ec2cffcf-8490-434d-a540-92ccddd2d25b.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ ec2cffcf-8490-434d-a540-92ccddd2d25b.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <ext/new_allocator.h>
+#include <cassert>
 
 using namespace std;
 
@@ -11,15 +13,5 @@ bool has_close_elements(vector<float> v, float threshold) {
     return false;
 }
 
-int main() {
-    vector<float> a;
-    float temp;
-    
-    while (cin >> temp) {
-        a.push_back(temp);
-    }
-
-    assert(has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false);
-
-    return 0;
-}
+assert(has_close_elements({1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f}, 0.0f) == true);
+assert(has_close_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}, 0.5f) == false);
