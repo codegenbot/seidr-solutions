c55c781c54905a748b516ef791fbd4ae0e7b90cb
beep boop <githubbot@vadim.me>
2024-07-31 10:05:25 +0200

    Solution 88 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/rezdslsf Prompt:       |           ^~~~~~ In file included from 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp:27: 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp: In function ‘int main()’: 41566a73-3fd8-4ef2-a6d1-58dc98bd6fd6.cpp:37:20: error: ‘sort_array’ was not decl ared in this scope    37 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -19,4 +19,7 @@ std::vector<int> custom_sort(std::vector<int> arr) {
     return arr;
 }
 
-assert(issame(custom_sort({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
+int main() {
+    assert(issame(custom_sort({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
+    return 0;
+}
