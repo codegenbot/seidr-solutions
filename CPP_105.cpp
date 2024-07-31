0077d51bd88c3b6b396a2214f0c00bc795c27643
beep boop <githubbot@vadim.me>
2024-07-31 15:12:09 +0200

    Solution 31 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jcyebif8 Prompt:       |                              ^~~~~~ 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp:51:37: error: ‘string’ was not declared  in this scope    51 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp:51:45: error: expression list treated a s compound expression in initializer [-fpermissive]    51 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp:50: 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp: In function ‘int main()’: 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp:60:20: error: ‘by_length’ was not decla red in this scope    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 9d000712-bc5e-4648-81ab-4df510c0ddf0.cpp:60:19: error: ‘issame’ cannot be used a s a function    60 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,10 +1,51 @@
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<int> numbers;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            numbers.push_back(num);
+        }
+    }
 
-int main() {
-    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    sort(numbers.begin(), numbers.end());
+    reverse(numbers.begin(), numbers.end());
+
+    std::vector<std::string> result;
+    for (int num : numbers) {
+        std::string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
+    }
+
+    return result;
 }
