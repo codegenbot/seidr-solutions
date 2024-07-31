ed961e97a0715c54f7571f022be98b277fa86d59
beep boop <githubbot@vadim.me>
2024-07-31 11:40:20 +0200

    Solution 20 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ibnyda3o Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d68969cb-08e8-430d-b12d-425079f03460.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d68969cb-08e8-430d-b12d-425079f03460.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d68969cb-08e8-430d-b12d-425079f03460.cpp:11: d68969cb-08e8-430d-b12d-425079f03460.cpp: In function ‘int main()’: d68969cb-08e8-430d-b12d-425079f03460.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ d68969cb-08e8-430d-b12d-425079f03460.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,18 +1,24 @@
 #include <vector>
+#include <list>
 #include <boost/any.hpp>
+#include <cassert>
 
 using namespace std;
 
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
+}
+
 vector<int> filter_integers(list<boost::any> values){
     vector<int> result;
-    for (const auto &value : values) {
-        if (auto ptr = boost::any_cast<int>(&value)) {
-            result.push_back(*ptr);
+    for (const auto &val : values) {
+        if (val.type() == typeid(int)) {
+            result.push_back(boost::any_cast<int>(val));
         }
     }
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+int main(){
+    assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
 }
