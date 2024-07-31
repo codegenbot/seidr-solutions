b780489bb5cb578079ea8020322c70a0966fd0f6
beep boop <githubbot@vadim.me>
2024-07-31 09:17:01 +0200

    Solution 19 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/f92ajteq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fca6f2b3-2dd7-46c1-88f4-10b2c6a98 4cf.py", line 43, in <module>     check(sum_squares)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fca6f2b3-2dd7-46c1-88f4-10b2c6a98 4cf.py", line 13, in check     sum_squares( AssertionError

--- Python_142.py
+++ Python_142.py
@@ -2,16 +2,8 @@
 def sum_squares(lst):
     total = 0
     for i in range(len(lst)):
-        if i % 6 == 0:
+        if (i + 1) % 3 == 0 and (i + 1) % 4 == 0:
             total += lst[i] ** 2
-        elif i % 6 == 1:
+        elif (i + 1) % 3 != 0 and (i + 1) % 4 == 0:
             total += lst[i] ** 3
-        elif i % 6 == 2:
-            total += lst[i] ** 4
-        elif i % 6 == 3:
-            total += lst[i] ** 5
-        elif i % 6 == 4:
-            total += lst[i] ** 1
-        else:
-            total += lst[i] ** 0
     return total
