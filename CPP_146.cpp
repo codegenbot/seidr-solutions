fa16faace042c8ad9b8507b01094e8d88d4d8815
beep boop <githubbot@vadim.me>
2024-07-31 08:21:36 +0200

    Solution 56 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9smmea2z Prompt: 8aa00dd5-b683-49fe-aa55-15f20141c3f0.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ 8aa00dd5-b683-49fe-aa55-15f20141c3f0.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from 8aa00dd5-b683-49fe-aa55-15f20141c3f0.cpp:11: 8aa00dd5-b683-49fe-aa55-15f20141c3f0.cpp: In function ‘int main()’: 8aa00dd5-b683-49fe-aa55-15f20141c3f0.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -12,8 +12,8 @@ int specialFilter(std::vector<int> nums) {
 }
 
 int main() {
-    std::vector<int> numbers = {1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    std::vector<int> numbers = {1, 11, -12, 13, -14, 15};
     int result = specialFilter(numbers);
-    std::cout << "The number of special filter is: " << result << std::endl;
+    std::cout << "The number of elements in the vector that meet the condition is: " << result << std::endl;
     return 0;
 }
