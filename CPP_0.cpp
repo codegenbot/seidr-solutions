a5c69fccc08eb71f9945c26174a4b7ff1099e10b
beep boop <githubbot@vadim.me>
2024-07-31 07:42:28 +0200

    Solution 88 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1gr15hyt Prompt: faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp: In function ‘int main()’: faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp:24:5: error: ‘vector’ was not declared in this scope    24 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp:24:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ faf854fc-54c1-41cd-9811-1c8dbd073f12.cpp:24:12: error: expected primary-expressi on before ‘float’    24 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,7 +1,5 @@
+#include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cassert>
-#include <cmath>
 
 bool has_close_elements(std::vector<float> numbers, float threshold) {
     std::sort(numbers.begin(), numbers.end());
