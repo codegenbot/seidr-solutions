cefc5869084df66d4f49e7b44c6ff88eab31e227
beep boop <githubbot@vadim.me>
2024-07-31 11:37:03 +0200

    Solution 48 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7rx4riev Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/utility:76,                  from /usr/include/c++/11/algorithm:60,                  from 36fa38d7-d377-45be-b2b1-e223cc55af49.cpp:2: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -3,7 +3,6 @@
 #include <set>
 #include <vector>
 #include <iostream>
-#include <initializer_list>
 
 using namespace std;
 
@@ -19,12 +18,21 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
+    vector<int> temp;
+    for(auto e : s1) {
+        bool found = false;
+        for(auto f : s2) {
+            if(e == f) {
+                found = true;
+                break;
+            }
+        }
+        if(found) {
+            temp.push_back(e);
+        }
+    }
 
-    vector<int> result(intersection.begin(), intersection.end());
-    return result;
+    return vector<int>(temp.begin(), temp.end());
 }
 
 int main() { 
