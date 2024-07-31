383a866115b8cce763698613daf40cb9e1bdb281
beep boop <githubbot@vadim.me>
2024-07-31 15:28:27 +0200

    Solution 91 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/95kurt7j Prompt:       |     ^~ 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp:10:5: error: ‘vector’ does not name a t ype    10 |     vector<int> reversed_q = q;       |     ^~~~~~ 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp:11:12: error: expected constructor, des tructor, or type conversion before ‘(’ token    11 |     reverse(reversed_q.begin(), reversed_q.end());       |            ^ 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp:13:5: error: expected unqualified-id be fore ‘return’    13 |     return q == reversed_q;       |     ^~~~~~ 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp:16: 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp: In function ‘int main()’: 6849a059-27eb-40be-8bd5-58d3ef8a93a7.cpp:18:13: error: ‘will_it_fly’ was not dec lared in this scope    18 |     assert (will_it_fly({5}, 5) == true);       |             ^~~~~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,18 +1,18 @@
 #include <vector>
 using namespace std;
 
-bool checkPalindrome(vector<int> q, int w) {
+bool check_palindrome(const vector<int>& q, int w) {
     int sum = 0;
     for (int i = 0; i < q.size(); i++) {
         sum += q[i];
     }
-    
+
     if (sum > w) {
         return false;
     }
-    
+
     vector<int> reversed_q = q;
     reverse(reversed_q.begin(), reversed_q.end());
-    
+
     return q == reversed_q;
 }
