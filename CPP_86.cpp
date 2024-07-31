3f2c71cac17bd923e0cfdf5616aa88cc4bf7de68
beep boop <githubbot@vadim.me>
2024-07-31 13:04:53 +0200

    Solution 24 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/5c9um55e Prompt: 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp:12:9: error: expected constructor, dest ructor, or type conversion before ‘(’ token    12 |     sort(word.begin(), word.end());       |         ^ 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp:13:5: error: ‘result’ does not name a t ype    13 |     result += word;       |     ^~~~~~ 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return result;       |     ^~~~~~ 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp:17: 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp: In function ‘int main()’: 92aa2e77-9427-4b01-be6c-320a46ed3751.cpp:19:13: error: ‘anti_shuffle’ was not de clared in this scope    19 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,16 +1,24 @@
-std::string anti_shuffle(const std::string& s) {
-    std::string result = "";
-    std::string word = "";
+#include <algorithm>
+#include <string>
+
+string anti_shuffle(string s) {
+    string result = "";
+    string word = "";
     for (char c : s) {
         if (c == ' ') {
-            std::sort(word.begin(), word.end());
+            sort(word.begin(), word.end());
             result += word + ' ';
             word = "";
         } else {
             word += c;
         }
     }
-    std::sort(word.begin(), word.end());
+    sort(word.begin(), word.end());
     result += word;
     return result;
+}
+
+int main() {
+    assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
+    return 0;
 }
