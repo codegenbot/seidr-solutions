e4dafd4909b4a4aea916ec13afada19cd5ebff75
beep boop <githubbot@vadim.me>
2024-07-31 10:38:17 +0200

    Solution 28 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ygx48vwa Prompt: d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp:54:10: error: invalid types ‘int[int]’ for array subscript    54 |     if (a[i]!=b[i]) return false;       |          ^ d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp:54:16: error: invalid types ‘int[int]’ for array subscript    54 |     if (a[i]!=b[i]) return false;       |                ^ d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp: At global scope: d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp:44:5: note: ‘int main()’ previously def ined here    44 | int main() {       |     ^~~~ In file included from d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp:49: d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp: In function ‘int main()’: d87bf8ef-94c6-4c19-8879-d1e2785d6623.cpp:59:22: error: ‘bf’ cannot be used as a function    59 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool issame(const vector<string> &a, const vector<string> &b){
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i) {
-        if (a.at(i) != b.at(i)) return false;
+        if (a[i] != b[i]) return false;
     }
     return true;
 }
