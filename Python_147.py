85d42298b3ec7013cf9403475bac30387ab965e2
beep boop <githubbot@vadim.me>
2024-07-31 08:14:29 +0200

    Solution 91 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/45stoybq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fb70cf6-92e6-4181-b608-62a35c13c 393.py", line 21, in <module>     check(get_max_triples)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7fb70cf6-92e6-4181-b608-62a35c13c 393.py", line 18, in check     assert get_max_triples(100) == 53361            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_147.py
+++ Python_147.py
@@ -1,17 +1,17 @@
-```
 def get_max_triples(n):
     max_sum = 0
-    count = 0
     for k in range(1, n + 1):
         a = k * k - k + 1
         d = 2 * k - 1
         total = 0
         triples_count = 0
         for i in range(k):
-            total = a + d * (2 * i + 1)
+            total += (d*i) + a 
         if total > max_sum:
             max_sum = total
             count = 1
         elif total == max_sum:
             triples_count += 1
+        else:
+            count = 0
     return count, max_sum
