0aa581576c6a1ce3c4804f850e0b9b4163eb8552
beep boop <githubbot@vadim.me>
2024-07-31 07:25:06 +0200

    Solution 4 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/drrcdfel Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e57372ba-4cbf-4835-9fc3-d4db70723fe5.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e57372ba-4cbf-4835-9fc3-d4db70723fe5.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e57372ba-4cbf-4835-9fc3-d4db70723fe5.cpp:25: e57372ba-4cbf-4835-9fc3-d4db70723fe5.cpp: In function ‘int main()’: e57372ba-4cbf-4835-9fc3-d4db70723fe5.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ e57372ba-4cbf-4835-9fc3-d4db70723fe5.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,19 +1,5 @@
-#include <vector>
-#include <string>
-
-bool compare_vectors(vector<int> a, vector<int> b) {
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
-}
-
-vector<string> separate_paren_groups(string paren_string);
-
-int main() {
-    string input;
-    cin >> input;
-
-    vector<string> groups = separate_paren_groups(input);
-
-    return 0;
 }
 
 vector<string> separate_paren_groups(string paren_string) {
@@ -21,13 +7,13 @@ vector<string> separate_paren_groups(string paren_string) {
     string current_group;
 
     for (char c : paren_string) {
-        if (c == '{') {
+        if (c == '(') {
             if (!current_group.empty()) {
                 result.push_back(current_group);
             }
-            current_group = "{";
-        } else if (c == '}') {
-            current_group += "}";
+            current_group = "(";
+        } else if (c == ')') {
+            current_group += ")";
             result.push_back(current_group);
             current_group = "";
         }
