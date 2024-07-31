f5705ab61a2698a22812e62f778adfa0a0bf64ab
beep boop <githubbot@vadim.me>
2024-07-31 09:08:48 +0200

    Solution 5 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/j9798b6f Prompt:       |                    ^~~ 3229f2b7-808f-439d-899c-44ea79dd587d.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3229f2b7-808f-439d-899c-44ea79dd587d.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3229f2b7-808f-439d-899c-44ea79dd587d.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3229f2b7-808f-439d-899c-44ea79dd587d.cpp:14: 3229f2b7-808f-439d-899c-44ea79dd587d.cpp: In function ‘int main()’: 3229f2b7-808f-439d-899c-44ea79dd587d.cpp:24:20: error: ‘sort_array’ was not decl ared in this scope    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |                    ^~~~~~~~~~ 3229f2b7-808f-439d-899c-44ea79dd587d.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sort_array({21, 14, 23, 11}) , {23, 21, 14, 11}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
 using namespace std;
 
@@ -13,18 +12,18 @@ vector<int> sort_array(vector<int> array) {
     if (array.empty()) {
         return array;
     }
-
+    
     if ((array.front() + array.back()) % 2 == 0) {
         sort(array.begin(), array.end(), greater<int>());
     } else {
         sort(array.begin(), array.end());
     }
-
+    
     return array;
 }
 
 int main() {
     assert (issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
-
+    
     return 0;
 }
