c1b5c587b5b612b72e473b83a99f97e358f00340
beep boop <githubbot@vadim.me>
2024-07-31 14:21:12 +0200

    Solution 86 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/4gg5kc38 Prompt: in this scope    25 |     assert(sort_array({2, 4, 8, 16, 32}) == vector<int>{2, 4, 8, 16, 32} );       |     ^~~~~~ faab71b9-df63-4a66-9d01-8e55e08d522c.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <algorithm>   +++ |+#include <cassert>     4 | using namespace std; In file included from faab71b9-df63-4a66-9d01-8e55e08d522c.cpp:30: faab71b9-df63-4a66-9d01-8e55e08d522c.cpp: In function ‘int main()’: faab71b9-df63-4a66-9d01-8e55e08d522c.cpp:40:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    40 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ faab71b9-df63-4a66-9d01-8e55e08d522c.cpp:6:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     6 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ faab71b9-df63-4a66-9d01-8e55e08d522c.cpp:31:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>  
-
+#include <cassert>
 using namespace std;
 
 bool issame(const vector<int>& a, const vector<int>& b) {
