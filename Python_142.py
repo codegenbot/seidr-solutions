b0adb607fc8af9411721ccbf69fd2df4a8fb6c43
beep boop <githubbot@vadim.me>
2024-07-31 17:34:32 +0200

    Solution 46 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/wi67tfur Prompt: Complete the following code given the task description and function signature.

--- Python_142.py
+++ Python_142.py
@@ -6,4 +6,6 @@ def sum_squares(lst):
             total += lst[i] ** 2
         elif i % 4 == 0 and i % 3 != 0:
             total += lst[i] ** 3
+        else:
+            total += lst[i]
     return total
