f3c59dd148c1c6b0da2710d3eb2d94984d8f9f4f
beep boop <githubbot@vadim.me>
2024-07-31 10:06:29 +0200

    Solution 3 of CPP/31, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/9p4zrsd5 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_31.cpp
+++ CPP_31.cpp
@@ -1,8 +1,9 @@
-#include <cassert>
+#include <iostream>
+using namespace std;
 
 bool is_prime(int n) {
     bool is_divisor = false;
-    for (long long i = 2; i * i <= n && !is_divisor; ++i) {
+    for (int i = 2; i * i <= n && !is_divisor; ++i) {
         if (n % i == 0) {
             is_divisor = true;
         }
@@ -11,6 +12,5 @@ bool is_prime(int n) {
 }
 
 int main() {
-    assert(is_prime(13441 * 19) == false);
-    // ...
+    cout << is_prime(13441 * 19) << endl;
 }
