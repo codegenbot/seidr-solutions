43871088c6d3f0a018859937836f5f4c56e1fa5c
beep boop <githubbot@vadim.me>
2024-07-31 18:38:08 +0200

    Solution 90 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/8l5hx4ue Prompt: In file included from 99f9d8bc-9dcc-4e69-8558-a902ee4e05b8.cpp:24: 99f9d8bc-9dcc-4e69-8558-a902ee4e05b8.cpp: In function ‘int main()’: 99f9d8bc-9dcc-4e69-8558-a902ee4e05b8.cpp:26:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    26 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -5,7 +5,7 @@
 
 std::string flip_case(std::string str);
 
-int solveProblem() {
+int main() {
     assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
     return 0;
 }
