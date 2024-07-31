5b0d70136d52c24404a388498acd283afa8038a1
beep boop <githubbot@vadim.me>
2024-07-31 08:20:22 +0200

    Solution 44 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pv69h4z1 Prompt: In file included from 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp:20: 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp: In function ‘int main()’: 2a7b09a8-ce1c-4e89-a9a9-fd4d3a42e61d.cpp:30:20: error: ‘incr_list’ was not decla red in this scope    30 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -2,26 +2,10 @@
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<int> incr_list(const std::vector<int> &a) {
-    std::vector<int> result;
-    for (int i = 0; i < a.size(); i++) {
-        result.push_back(a[i] + 1);
-    }
-    return result;
+    return a == b;
 }
 
 int main() {
-    assert(issame(incr_list(std::vector<int>{5, 2, 5, 2, 3, 3, 9, 0, 123}), incr_list(std::vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124})));
+    assert(issame(std::vector<int>{5, 2, 5, 2, 3, 3, 9, 0, 123}, std::vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124}));
     return 0;
 }
