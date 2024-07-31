872e72ad30e0ec0651c6e49f7f202f546003c078
beep boop <githubbot@vadim.me>
2024-07-31 15:19:47 +0200

    Solution 45 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/eli9o2f6 Prompt:       |                    ^~~ acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp:17: acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp: In function ‘int main()’: acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp:27:20: error: ‘strange_sort_list’ was n ot declared in this scope    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ acfc5ef7-adc3-4fb0-b783-39aebff39f62.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -4,22 +4,12 @@
 bool issame(vector<int> a, vector<int> b);
 
 bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+    return std::equal(a.begin(), a.end(), b.begin());
 }
 
-vector<int> strange_sort_list(vector<int>& lst) {
+std::vector<int> strange_sort_list(std::vector<int> lst) {
     sort(lst.begin(), lst.end());
-    vector<int> result;
+    std::vector<int> result;
     int left = 0, right = lst.size() - 1;
     while (left <= right) {
         if (left == right) {
