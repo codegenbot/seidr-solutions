ffa9e82783a4f64c4de4a596b882065fdf84c829
beep boop <githubbot@vadim.me>
2024-07-31 14:21:49 +0200

    Solution 20 of CPP/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/3aukpuda Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void fix_spaces(const char* text) {
+void fix_spaces(char* text) {
     bool isPrevSpace = false;
-    int i = 0;
-    while (text[i] != '\0') {
+    for (int i = 0; i < strlen(text); i++) {
         if (text[i] == ' ') {
             if (!isPrevSpace) {
                 text[i] = '_';
@@ -14,15 +13,16 @@ void fix_spaces(const char* text) {
             text[i] = text[i];
             isPrevSpace = false;
         }
-        i++;
     }
 }
 
 int main() {
-    char text[] = "   Exa 1 2 2 mple";
+    char* text = new char[strlen("   Exa 1 2 2 mple") + 1];
+    strcpy(text, "   Exa 1 2 2 mple");
     fix_spaces(text);
     cout << "Fixed string: ";
     for (char c : text) cout << c;
     cout << endl;
+    delete[] text;
     return 0;
 }
