ccbc123fc829fe93a73febdfb904d7ed42655319
beep boop <githubbot@vadim.me>
2024-07-31 10:00:18 +0200

    Solution 6 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jxqpfa3d Prompt:       |                              ^~~~~~ 4d7f7589-360c-4435-8dca-11d3bad97962.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4d7f7589-360c-4435-8dca-11d3bad97962.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4d7f7589-360c-4435-8dca-11d3bad97962.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4d7f7589-360c-4435-8dca-11d3bad97962.cpp:28: 4d7f7589-360c-4435-8dca-11d3bad97962.cpp: In function ‘int main()’: 4d7f7589-360c-4435-8dca-11d3bad97962.cpp:38:20: error: ‘split_words’ was not dec lared in this scope    38 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ 4d7f7589-360c-4435-8dca-11d3bad97962.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,16 +1,13 @@
-#include <vector>
 #include <string>
-#include <cassert>
 
-using namespace std;
+bool issame(vector<string> a, vector<string> b);
 
-vector<string> split_words(string txt){
+vector<string> split_text(const string& txt) {
     vector<string> result;
     string word = "";
-    
-    for(char c : txt){
-        if(c == ' ' || c == ','){
-            if(!word.empty()){
+    for (char c : txt) {
+        if (c == ' ' || c == ',') {
+            if (!word.empty()) {
                 result.push_back(word);
                 word = "";
             }
@@ -18,34 +15,17 @@ vector<string> split_words(string txt){
             word += c;
         }
     }
-    
-    if(!word.empty()){
+    if (!word.empty()) {
         result.push_back(word);
     }
-    
-    if(result.size() == 1 && !isalpha(result[0][0])){
-        int count = 0;
-        for(char c : result[0]){
-            if(islower(c) && (c - 'a') % 2 == 1){
-                count++;
+    if (result.empty()) {
+        int oddCount = 0;
+        for (char c : txt) {
+            if (islower(c) && (c - 'a') % 2 == 1) {
+                oddCount++;
             }
         }
-        result[0] = to_string(count);
+        result.push_back(to_string(oddCount));
     }
-    
     return result;
-}
-
-bool is_same(vector<string> a, vector<string> b){
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-int main(){
-    assert(is_same(split_words("") ,{"0"}));
-    
-    return 0;
 }
