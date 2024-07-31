aa7b9f6666e919ce08137403a87b279987473801
beep boop <githubbot@vadim.me>
2024-07-31 17:47:39 +0200

    Solution 90 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jbccvt5l Prompt:       |                              ^~~~~~ 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp:29: 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp: In function ‘int main()’: 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 7dffa0e5-c905-4819-86ec-3910e8630ecc.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,6 +1,10 @@
 #include <vector>
 using namespace std;
 
+bool issame(vector<string> v1, vector<string> v2) {
+    return v1 == v2;
+}
+
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     vector<string> result;
@@ -15,7 +19,7 @@ vector<string> bf(string planet1, string planet2) {
         }
     }
 
-    if (index1 == -1 || index2 == -1) {
+    if (index1 == -1 || index2 == -1 || issame(vector<string>{planet1}, vector<string>{planet2})) {
         return result;
     }
 
@@ -27,4 +31,4 @@ vector<string> bf(string planet1, string planet2) {
     }
 
     return result;
-}
+}  
