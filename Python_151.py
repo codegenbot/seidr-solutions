c92872dd36a4ec087cece20a3cae8332bfc00390
beep boop <githubbot@vadim.me>
2024-07-31 09:54:20 +0200

    Solution 80 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/5x53byvq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a4f05e4d-0f3b-40d1-81eb-bca6c0d09 d2c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,2 +1,4 @@
 def double_the_difference(lst):
-    return sum(i ** 2 for i in [x for x in lst if isinstance(x, int) and x >= 0 and x % 2 != 0])
+    even_squares = sum(i ** 2 for i in [x for x in lst if isinstance(x, int) and x >= 0 and x % 2 != 0])
+    diff = (max(lst) - min(lst)) if len(lst) > 1 else 0
+    return 2 * even_squares - abs(diff)
