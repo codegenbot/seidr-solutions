53089ca4c806548c7ac8aa389d79cc4bea608edf
beep boop <githubbot@vadim.me>
2024-07-31 12:16:41 +0200

    Solution 25 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kekrq434 Prompt: 89c6c1d2-7a29-44f6-a191-56d484434e50.cpp: In function ‘std::string anti_shuffle( std::string)’: 89c6c1d2-7a29-44f6-a191-56d484434e50.cpp:11:30: error: ‘j’ was not declared in t his scope    11 |         result += str.substr(j + 1, i - j);       |                              ^ 89c6c1d2-7a29-44f6-a191-56d484434e50.cpp: At global scope: 89c6c1d2-7a29-44f6-a191-56d484434e50.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ 89c6c1d2-7a29-44f6-a191-56d484434e50.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,13 +1,14 @@
-#include <string>
+```c
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 string anti_shuffle(string str) {
     string result = "";
     int i = str.length() - 1;
     while (i >= 0) {
-        for (int j = i; j >= 0 && str[j] == str[i]; --j)
+        int j = i;
+        for (; j >= 0 && str[j] == str[i]; --j)
             --i;
         result += str.substr(j + 1, i - j);
         --i;
@@ -21,3 +22,4 @@ int main() {
     cin >> str;
     cout << "Anti-shuffled sentence: " << anti_shuffle(str) << endl;
     return 0;
+}
