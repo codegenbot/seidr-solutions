461a0516d0af0c6a51a8b7b0998d1c9ef63e8ef7
beep boop <githubbot@vadim.me>
2024-07-31 09:49:11 +0200

    Solution 21 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/k5zrd6up Prompt:    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 667c97d0-348a-4292-9a75-c9763995e6b3.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 667c97d0-348a-4292-9a75-c9763995e6b3.cpp:51:44: error: ‘b’ was not declared in t his scope    51 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 667c97d0-348a-4292-9a75-c9763995e6b3.cpp:59:5: error: redefinition of ‘int main( )’    59 | int main(){       |     ^~~~ 667c97d0-348a-4292-9a75-c9763995e6b3.cpp:38:5: note: ‘int main()’ previously def ined here    38 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -44,6 +44,6 @@ int main() {
     std::vector<std::string> words = select_words(s, n);
 
     assert(issame(select_words("a b c d e f", 1), std::vector<std::string>{"b", "c", "d", "f"}));
-
+    
     return 0;
 }
