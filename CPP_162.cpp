c90088d7e8380036bb9da276ccb1806d9ace9df8
beep boop <githubbot@vadim.me>
2024-07-31 08:40:56 +0200

    Solution 44 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2lzva72q Prompt: 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp:1:1: error: stray ‘`’ in program     1 | ```       | ^ 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp:1:2: error: stray ‘`’ in program     1 | ```       |  ^ 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp:1:3: error: stray ‘`’ in program     1 | ```       |   ^ 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp:4:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     4 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp:30: 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp: In function ‘int main()’: 74ea88a3-d982-45de-a32c-c9cecbee7cd2.cpp:32:13: error: ‘string_to_md5’ was not d eclared in this scope    32 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,25 +1,25 @@
+```
 #include <iostream>
 #include <string>
-#include <iomanip>
 #include <openssl/md5.h>
 
 std::string string_to_md5(std::string text) {
     if (text.empty()) return "";
 
-    unsigned char hash[MD5_DIGEST_LENGTH];
+    unsigned char hash[16];
     MD5_CTX mdContext;
     MD5_Init(&mdContext);
     const char* ptr = text.c_str();
-    size_t len = text.size();
-    while (len > 0) {
-        MD5_Update(&mdContext, ptr, len);
-        len = 0;
-        ptr += len;
+    size_t len = 0;
+    while (*ptr != '\0') {
+        MD5_Update(&mdContext, ptr, 1);
+        ptr++;
+        len++;
     }
     MD5_Final(hash, &mdContext);
 
     std::string result;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+    for (int i = 0; i < 16; ++i) {
         char buff[3];
         sprintf(buff, "%02x", hash[i]);
         result.append(buff);
@@ -29,6 +29,10 @@ std::string string_to_md5(std::string text) {
 }
 
 int main() {
-    std::cout << string_to_md5("Hello World!") << std::endl;
+    std::string text;
+    std::cout << "Enter a string: ";
+    std::getline(std::cin, text);
+    std::string md5 = string_to_md5(text);
+    std::cout << "MD5 hash: " << md5 << std::endl;
     return 0;
 }
