938a1204e778da8f3ed483d24a5f24d985941ea9
beep boop <githubbot@vadim.me>
2024-07-31 07:43:38 +0200

    Solution 38 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/a18f4cm5 Prompt:    34 | int main(){       |     ^~~~ c8946bdf-a714-4a0a-976d-abf4b05df51e.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main(){       |     ^~~~ In file included from c8946bdf-a714-4a0a-976d-abf4b05df51e.cpp:25: c8946bdf-a714-4a0a-976d-abf4b05df51e.cpp: In function ‘int main()’: c8946bdf-a714-4a0a-976d-abf4b05df51e.cpp:35:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    35 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ c8946bdf-a714-4a0a-976d-abf4b05df51e.cpp:15:6: note: candidate: ‘bool issame(con st std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx 11::basic_string<char> >&)’    15 | bool issame(const vector<string>& a, const vector<string>& b){       |      ^~~~~~ c8946bdf-a714-4a0a-976d-abf4b05df51e.cpp:26:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    26 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -12,8 +12,12 @@ vector<string> all_prefixes(string str){
     return result;
 }
 
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
+
 int main(){
-    assert(issame(all_prefixes("WWW"), vector<string>{"W", "WW", "WWW"}));
+    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
 
     return 0;
 }
