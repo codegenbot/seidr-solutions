e986176749c74fd8a6088e9b127967568040be77
beep boop <githubbot@vadim.me>
2024-07-31 11:48:48 +0200

    Solution 60 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pt96coqc Prompt: fa3a3bbc-a2c3-46ae-9190-db32e7f88759.cpp:11:5: error: redefinition of ‘int main( )’    11 | int main(){       |     ^~~~ fa3a3bbc-a2c3-46ae-9190-db32e7f88759.cpp:3:5: note: ‘int main()’ previously defi ned here     3 | int main() {       |     ^~~~ In file included from fa3a3bbc-a2c3-46ae-9190-db32e7f88759.cpp:10: fa3a3bbc-a2c3-46ae-9190-db32e7f88759.cpp: In function ‘int main()’: fa3a3bbc-a2c3-46ae-9190-db32e7f88759.cpp:12:13: error: ‘strlen’ was not declared  in this scope    12 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ fa3a3bbc-a2c3-46ae-9190-db32e7f88759.cpp:11:1: note: ‘strlen’ is defined in head er ‘<cstring>’; did you forget to ‘#include <cstring>’?    10 | #include<assert.h>   +++ |+#include <cstring>    11 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -4,7 +4,7 @@
 int main() {
     std::string str;
     std::cin >> str;
-    assert( strlen("asdasnakj") == 9 );
     std::cout << str.length();
+    assert(strlen(str.c_str()) == 9);
     return 0;
 }
