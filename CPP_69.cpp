aa65c92860283d4cd753f544473870e28baabd34
beep boop <githubbot@vadim.me>
2024-07-31 13:06:58 +0200

    Solution 27 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d3xs01pj Prompt: ebbf8e6a-1f25-4fec-8f38-21c09328f2c4.cpp:1:12: error: ‘vector’ was not declared in this scope     1 | int search(vector<int> lst) {       |            ^~~~~~ ebbf8e6a-1f25-4fec-8f38-21c09328f2c4.cpp:1:19: error: expected primary-expressio n before ‘int’     1 | int search(vector<int> lst) {       |                   ^~~ In file included from ebbf8e6a-1f25-4fec-8f38-21c09328f2c4.cpp:11: ebbf8e6a-1f25-4fec-8f38-21c09328f2c4.cpp: In function ‘int main()’: ebbf8e6a-1f25-4fec-8f38-21c09328f2c4.cpp:13:19: error: ‘search’ cannot be used a s a function    13 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -3,8 +3,8 @@
 int search(std::vector<int> lst) {
     int res = -1;
     for (int num : lst) {
-        if (num > 0 && std::count(lst.begin(), lst.end(), num) >= num) {
-            res = std::max(res, num);
+        if (num > 0 && count(lst.begin(), lst.end(), num) >= num) {
+            res = max(res, num);
         }
     }
     return res;
