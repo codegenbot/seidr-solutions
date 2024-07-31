774fb5615fdfe2fe365f4504b36b282fac12fd68
beep boop <githubbot@vadim.me>
2024-07-31 11:58:52 +0200

    Solution 96 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7rx4riev Prompt: /usr/include/c++/11/bits/predefined_ops.h: In instantiation of ‘constexpr bool _ _gnu_cxx::__ops::_Iter_comp_iter<_Compare>::operator()(_Iterator1, _Iterator2) [ with _Iterator1 = __gnu_cxx::__normal_iterator<int*, std::vector<int> >; _Iterat or2 = __gnu_cxx::__normal_iterator<int*, std::vector<int> >; _Compare = __gnu_cx x::__normal_iterator<int*, std::vector<int> >]’: /usr/include/c++/11/bits/stl_algo.h:5240:12:   required from ‘_OutputIterator st d::__set_intersection(_InputIterator1, _InputIterator1, _InputIterator2, _InputI terator2, _OutputIterator, _Compare) [with _InputIterator1 = __gnu_cxx::__normal _iterator<int*, std::vector<int> >; _InputIterator2 = __gnu_cxx::__normal_iterat or<int*, std::vector<int> >; _OutputIterator = __gnu_cxx::__normal_iterator<int* , std::vector<int> >; _Compare = __gnu_cxx::__ops::_Iter_comp_iter<__gnu_cxx::__ normal_iterator<int*, std::vector<int> > >]’ /usr/include/c++/11/bits/stl_algo.h:5346:48:   required from ‘_OIter std::set_in tersection(_IIter1, _IIter1, _IIter2, _IIter2, _OIter, _Compare) [with _IIter1 =  __gnu_cxx::__normal_iterator<int*, std::vector<int> >; _IIter2 = __gnu_cxx::__n ormal_iterator<int*, std::vector<int> >; _OIter = __gnu_cxx::__normal_iterator<i nt*, std::vector<int> >; _Compare = __gnu_cxx::__normal_iterator<int*, std::vect or<int> >]’ c04ac30f-f3e9-4d5a-9116-3e477880f88a.cpp:37:65:   required from here /usr/include/c++/11/bits/predefined_ops.h:158:30: error: no match for call to ‘( __gnu_cxx::__normal_iterator<int*, std::vector<int> >) (int&, int&)’   158 |         { return bool(_M_comp(*__it1, *__it2)); }       |                       ~~~~~~~^~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -32,25 +32,26 @@ int main() {
 
         if(same) {
             std::vector<int> common;
-
-            for(int i=0; i<v[0].size(); i++) {
+            if (!v.empty() && std::next(v.begin()) != v.end()) {
                 auto it = std::unique_copy(std::set_intersection(v[0].begin(), v[0].end(),
-                                                                  v[1].begin(), v[1].end(),
-                                                                  v[0].begin(), v[0].end()),
-                                            v[0].begin());
-                if(it != v[0].end()) {
-                    common.push_back(*it);
+                                                                  v[1].begin(), v[1].end()),
+                                            common.begin());
+                if (it != common.end()) {
+                    for (int i = 0; i < common.size(); i++) {
+                        std::cout << common[i] << " ";
+                    }
+                    std::cout << std::endl;
+                } else {
+                    std::cout << "No common elements." << std::endl;
                 }
+            } else {
+                std::cout << "At least two lists required to find common elements." << std::endl;
             }
+        } 
+        else { 
+            std::cout << "No common elements." << std::endl; 
+        } 
 
-            std::cout << "Common elements: ";
-            for(int i=0; i<common.size(); i++) {
-                std::cout << common[i] << " ";
-            }
-            std::cout << std::endl;
-        } else {
-            std::cout << "No common elements." << std::endl;
-        }
     } 
     else { 
         std::cout << "At least two lists required to find common elements." << std::endl; 
