831c8ad27c2cc19bde9566b89c20d269f3df4cd4
beep boop <githubbot@vadim.me>
2024-07-31 07:28:16 +0200

    Solution 5 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/twfl8r4i Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6124fe10-6b3f-45b1-a16f-d0885c55d6cf.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6124fe10-6b3f-45b1-a16f-d0885c55d6cf.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6124fe10-6b3f-45b1-a16f-d0885c55d6cf.cpp:11: 6124fe10-6b3f-45b1-a16f-d0885c55d6cf.cpp: In function ‘int main()’: 6124fe10-6b3f-45b1-a16f-d0885c55d6cf.cpp:21:20: error: ‘rolling_max’ was not dec lared in this scope    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |                    ^~~~~~~~~~~ 6124fe10-6b3f-45b1-a16f-d0885c55d6cf.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -1,9 +1,12 @@
+// Add missing semicolon after function signature in line 12
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
+// Declare 'rolling_max' function above 'issame' function
 vector<int> rolling_max(vector<int> numbers);
 
+// Implement 'rolling_max' function below 'issame' function
 vector<int> rolling_max(vector<int> numbers) {
     vector<int> result;
     int max_num = INT_MIN;
