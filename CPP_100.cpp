075f35f48f4e14d5a7f6e13f6245de24ac4ad449
beep boop <githubbot@vadim.me>
2024-07-31 14:44:04 +0200

    Solution 3 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/2qkg41co Prompt: Complete the following code given the task description and function signature.

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,11 +1,12 @@
 vector<int> make_a_pile(int n) {
     vector<int> pile;
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 0) {
-            pile.push_back(i * 2 - 1);
-        } else {
-            pile.push_back(i * 2);
-        }
+    for (int i = 0; ; i++) {
+        if ((n & 1)) // check if n is odd
+            pile.push_back(2 * i + 1);
+        else
+            pile.push_back(2 * i + 2);
+        if (++n > i) continue;
+        break;
     }
     return pile;
 }
