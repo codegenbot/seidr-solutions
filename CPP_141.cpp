529c42990af0d8e253c27396ab356106ba1b9266
beep boop <githubbot@vadim.me>
2024-07-31 10:21:56 +0200

    Solution 3 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2geucryd Prompt: e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp:18:5: error: ‘string’ does not name a t ype    18 |     string extension = file_name.substr(dotPosition + 1);       |     ^~~~~~ e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp:20:5: error: expected unqualified-id be fore ‘if’    20 |     if (extension != "txt" && extension != "exe" && extension != "dll") {       |     ^~ e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp:24:5: error: expected unqualified-id be fore ‘return’    24 |     return "Yes";       |     ^~~~~~ e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp:25:1: error: expected declaration befor e ‘}’ token    25 | }       | ^ In file included from e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp:27: e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp: In function ‘int main()’: e7ab231a-c21d-471d-a76d-5d11b7fe2049.cpp:29:13: error: ‘file_name_check’ was not  declared in this scope    29 |     assert (file_name_check("s.") == "No");       |             ^~~~~~~~~~~~~~~

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -6,7 +6,7 @@ std::string file_name_check(const std::string &file_name) {
     int digitCount = 0;
     int dotCount = 0;
     int dotPosition = -1;
-
+    
     for (int i = 0; i < file_name.size(); ++i) {
         if (isdigit(file_name[i])) {
             digitCount++;
@@ -15,22 +15,22 @@ std::string file_name_check(const std::string &file_name) {
             dotPosition = i;
         }
     }
-
+    
     if (digitCount > 3 || dotCount != 1 || dotPosition == 0 || dotPosition == file_name.size() - 1) {
         return "No";
     }
-
+    
     std::string extension = file_name.substr(dotPosition + 1);
-
+    
     if (extension != "txt" && extension != "exe" && extension != "dll") {
         return "No";
     }
-
+    
     return "Yes";
 }
 
 int main() {
     assert (file_name_check("s.") == "No");
-
+    
     return 0;
 }
