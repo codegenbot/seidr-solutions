cb24d536c565e4e73ae0872c6063f08def1d7c1e
beep boop <githubbot@vadim.me>
2024-07-31 19:46:36 +0200

    Solution 1 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/p57hloho Prompt: Complete the following code given the task description and function signature.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,15 +1,15 @@
 int even = 0, odd = 0;
-        for (int i = 1; i <= n; ++i) {
-            string str = to_string(i);
-            string rev = str;
-            reverse(rev.begin(), rev.end());
-            if (str == rev) {
-                if (i % 2 == 0) {
-                    even++;
-                } else {
-                    odd++;
-                }
+    for (int i = 1; i <= n; ++i) {
+        string num = to_string(i);
+        string rev_num = num;
+        reverse(rev_num.begin(), rev_num.end());
+        if (num == rev_num) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
             }
         }
-        return {even, odd};
     }
+    return {even, odd};
+}
