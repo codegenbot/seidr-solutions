c03911c78445548fd40db2e9bae3707253c2af16
beep boop <githubbot@vadim.me>
2024-07-31 17:21:29 +0200

    Solution 6 of CPP/141, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ovw0b22h Prompt: Complete the following code given the task description and function signature.

--- CPP_141.cpp
+++ CPP_141.cpp
@@ -1,35 +1,27 @@
-int digitCount = 0;
-    int dotCount = 0;
-    int dotIndex = -1;
-    string validExtensions[] = {"txt", "exe", "dll"};
-
-    for (int i = 0; i < file_name.length(); i++) {
-        if (file_name[i] >= '0' && file_name[i] <= '9') {
-            digitCount++;
-        } else if (file_name[i] == '.') {
-            dotCount++;
-            dotIndex = i;
+int digit_count = 0;
+    for (char c : file_name) {
+        if (isdigit(c)) {
+            digit_count++;
         }
     }
 
-    if (digitCount > 3 || dotCount != 1 || dotIndex == 0 || dotIndex == file_name.length() - 1) {
+    if (digit_count > 3) {
         return "No";
     }
 
-    string extension = file_name.substr(dotIndex + 1);
-    bool validExtension = false;
-    for (int i = 0; i < 3; i++) {
-        if (extension == validExtensions[i]) {
-            validExtension = true;
-            break;
-        }
-    }
-
-    if (!validExtension) {
+    size_t dot_pos = file_name.find('.');
+    if (dot_pos == string::npos || dot_pos == 0 || dot_pos == file_name.size() - 1) {
         return "No";
     }
 
-    if ((file_name[0] < 'a' || file_name[0] > 'z') && (file_name[0] < 'A' || file_name[0] > 'Z')) {
+    string prefix = file_name.substr(0, dot_pos);
+    string suffix = file_name.substr(dot_pos + 1);
+    
+    if (!isalpha(prefix[0])) {
+        return "No";
+    }
+
+    if (suffix != "txt" && suffix != "exe" && suffix != "dll") {
         return "No";
     }
 
