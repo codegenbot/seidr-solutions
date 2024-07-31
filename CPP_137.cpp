b44cf74d930c49039cbbdf80ea0eeba5a8a83fa0
beep boop <githubbot@vadim.me>
2024-07-31 19:33:25 +0200

    Solution 91 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/hp1oe9tv Prompt:       |     ^~~~ In file included from 833c5185-8763-4503-9c03-c1bd628b03db.cpp:29: 833c5185-8763-4503-9c03-c1bd628b03db.cpp: In function ‘int main()’: 833c5185-8763-4503-9c03-c1bd628b03db.cpp:31:13: error: ‘boost’ has not been decl ared    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 833c5185-8763-4503-9c03-c1bd628b03db.cpp:31:29: error: ‘string’ was not declared  in this scope    31 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 833c5185-8763-4503-9c03-c1bd628b03db.cpp:31:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 833c5185-8763-4503-9c03-c1bd628b03db.cpp:4: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 833c5185-8763-4503-9c03-c1bd628b03db.cpp:4: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -21,7 +21,7 @@ int main() {
     assert(std::any_cast<float>(compare_one(1.5f, 2.3f)).type() == typeid(float));
     assert(std::any_cast<float>(compare_one(std::to_string(1.5f), std::to_string(2.3f)).type()) == typeid(float));
     assert(compare_one(std::to_string(1), 1).type() == typeid(std::any));
-    assert(std::any_cast<std::any>(compare_one(std::string("1"), std::string("1"))).has_value() == false);
-    assert(std::any_cast<std::any>(compare_one(std::string("1"), std::string("2"))).has_value() == false);
+    assert(!std::any_cast<std::any>(compare_one(std::string("1"), 1)).has_value());
+    assert(!std::any_cast<std::any>(compare_one(std::string("1"), std::string("2"))).has_value());
     return 0;
 }
