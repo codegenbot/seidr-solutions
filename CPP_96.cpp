6167074e9b3111c94440fcad870f587e7f3d8f8c
beep boop <githubbot@vadim.me>
2024-07-31 13:23:29 +0200

    Solution 4 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/613gihaj Prompt:    24 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ff44d81b-40a1-4f00-a4ab-bc81af573533.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ff44d81b-40a1-4f00-a4ab-bc81af573533.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ff44d81b-40a1-4f00-a4ab-bc81af573533.cpp:23: ff44d81b-40a1-4f00-a4ab-bc81af573533.cpp: In function ‘int main()’: ff44d81b-40a1-4f00-a4ab-bc81af573533.cpp:33:20: error: ‘count_up_to’ was not dec lared in this scope    33 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ ff44d81b-40a1-4f00-a4ab-bc81af573533.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,16 +1,27 @@
-vector<int> count_up_to(int n){
-    vector<int> primes;
-    for(int i=2; i<n; i++){
-        bool isPrime = true;
-        for(int j=2; j*j<=i; j++){
-            if(i % j == 0){
-                isPrime = false;
-                break;
+vector<int> count_up_to(int n) {
+    vector<int> result;
+    if(n < 2) return result;
+    
+    vector<bool> isPrime(n, true);
+    isPrime[0] = isPrime[1] = false;
+    
+    for(int i = 2; i * i < n; ++i){
+        if(isPrime[i]){
+            for(int j = i * i; j < n; j += i){
+                isPrime[j] = false;
             }
         }
-        if(isPrime){
-            primes.push_back(i);
+    }
+    
+    for(int i = 2; i < n; ++i){
+        if(isPrime[i]){
+            result.push_back(i);
         }
     }
-    return primes;
+    
+    return result;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
