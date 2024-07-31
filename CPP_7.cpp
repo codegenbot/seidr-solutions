04026c44d3fd687fe07546cde0bc575b6919eac2
beep boop <githubbot@vadim.me>
2024-07-31 08:50:13 +0200

    Solution 22 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/vuufezmj Prompt: /usr/include/c++/11/bits/stl_vector.h:469:23:   required from ‘static std::vecto r<_Tp, _Alloc>::pointer std::vector<_Tp, _Alloc>::_S_relocate(std::vector<_Tp, _ Alloc>::pointer, std::vector<_Tp, _Alloc>::pointer, std::vector<_Tp, _Alloc>::po inter, std::vector<_Tp, _Alloc>::_Tp_alloc_type&) [with _Tp = std::__cxx11::basi c_string<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> >; std: :vector<_Tp, _Alloc>::pointer = std::__cxx11::basic_string<char>*; std::vector<_ Tp, _Alloc>::_Tp_alloc_type = std::vector<std::__cxx11::basic_string<char> >::_T p_alloc_type]’ /usr/include/c++/11/bits/vector.tcc:79:19:   required from ‘void std::vector<_Tp , _Alloc>::reserve(std::vector<_Tp, _Alloc>::size_type) [with _Tp = std::__cxx11 ::basic_string<char>; _Alloc = std::allocator<std::__cxx11::basic_string<char> > ; std::vector<_Tp, _Alloc>::size_type = long unsigned int]’ d5e3c412-6d19-4974-ac1a-ff39b97e854f.cpp:19:19:   required from here /usr/include/c++/11/ext/new_allocator.h:162:11: error: no matching function for call to ‘operator new(sizetype, void*)’   162 |         { ::new((void *)__p) _Up(std::forward<_Args>(__args)...); }       |           ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ <built-in>: note: candidate: ‘void* operator new(long unsigned int)’ <built-in>: note:   candidate expects 1 argument, 2 provided <built-in>: note: candidate: ‘void* operator new(long unsigned int, std::align_v al_t)’ <built-in>: note:   no known conversion for argument 2 from ‘void*’ to ‘std::ali gn_val_t’

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -16,6 +16,7 @@ bool issame(vector<string> a, vector<string> b) {
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result; 
+    //result.reserve(strings.size()); // Reserve space for elements
     for(string s : strings){
         if(s.find(substring) != string::npos)
             result.push_back(s);
