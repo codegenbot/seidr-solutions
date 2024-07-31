d6feff8c14f4720c98273d3bf355fd8823225e9d
beep boop <githubbot@vadim.me>
2024-07-31 07:44:14 +0200

    Solution 13 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/45stoybq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96c72423-1ea8-4480-90b9-acfb92f01 206.py", line 17, in <module>     check(get_max_triples)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96c72423-1ea8-4480-90b9-acfb92f01 206.py", line 14, in check     assert get_max_triples(100) == 53361            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/96c72423-1ea8-4480-90b9-acfb92f01 206.py", line 8, in get_max_triples     total = a[i] + a[j] + a[k]                           ~^^^ IndexError: list index out of range

--- Python_147.py
+++ Python_147.py
@@ -2,9 +2,9 @@ def get_max_triples(n):
     a = [i * i - i + 1 for i in range(1, n + 1)]
     count = 0
     max_sum = 0
-    for i in range(len(a)):
-        for j in range(i, len(a)):
-            k = j
+    for k in range(1, n + 1): 
+        for i in range(k-1):
+            j = i+1
             total = a[i] + a[j] + a[k]
             if total % 3 == 0:
                 count += 1
