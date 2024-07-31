bf3b472d577e950921564a2db86555de17ea7639
beep boop <githubbot@vadim.me>
2024-07-31 15:17:14 +0200

    Solution 0 of CPP/109, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/kd66kobj Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_109.cpp
@@ -0,0 +1,9 @@
+bool move_one_ball(vector<int> arr) {
+    int n = arr.size();
+    if (n == 0) return true;
+    
+    for(int i=1; i<n; i++) {
+        if(arr[i] < arr[0]) return false;
+    }
+    return true;
+}
