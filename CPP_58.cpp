5542be7aa0ddd051bf575d831c8fbec43114cfac
beep boop <githubbot@vadim.me>
2024-07-31 11:20:45 +0200

    Solution 6 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/7rx4riev Prompt: on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp:21:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp:20: 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp: In function ‘int main()’: 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp:30:20: error: ‘common’ was not declared  in this scope    30 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 369f33b0-1497-404c-a8a5-219a6b1a5a28.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,23 +1,6 @@
-#include <set>
 #include <vector>
 using namespace std;
 
-bool issame(vector<vector<int>> a, vector<vector<int>> b) {
-    if(a.size() != b.size()) return false;
-    for(int i=0; i<a.size(); i++) {
-        if(a[i].size() != b[i].size()) return false;
-    }
-    return true;
-}
-
-vector<int> common(vector<int> l1, vector<int> l2) {
-    set<int> s1(l1.begin(), l1.end());
-    set<int> s2(l2.begin(), l2.end());
-
-    set<int> intersection;
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      inserter(intersection, intersection.begin()));
-
-    vector<int> result(intersection.begin(), intersection.end());
-    return result;
+bool issame(vector<int> a, vector<int> b) {
+    return (a.size() == 0 && b.size() == 0) || (a.size() == b.size());
 }
