bfb5512041b56a54391af3a68e5fdb11de7c3a1a
beep boop <githubbot@vadim.me>
2024-07-31 16:21:46 +0200

    Solution 96 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/sbirh8qw Prompt:       |                    ^~~ 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp:21: 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp: In function ‘int main()’: 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp:31:20: error: ‘f’ was not declared in t his scope    31 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^ 46204301-fa96-4788-af4b-9cb1f8dc561b.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(f(3) , {1, 2, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,15 +1,17 @@
 #include <vector>
 #include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()) {
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i]) {
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
@@ -31,9 +33,4 @@ vector<int> f(int n){
         }
     }
     return result;
-}
-
-int main() {
-    assert(issame(f(3), {1, 2, 6}));
-    return 0;
 }
