0c0a468462d3febc0d5ef4e17da3ccf114a41771
beep boop <githubbot@vadim.me>
2024-07-31 19:07:18 +0200

    Solution 2 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/nfufi7dv Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e70d2dfe-4b53-4dc9-b1f5-d651593e7b00.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e70d2dfe-4b53-4dc9-b1f5-d651593e7b00.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e70d2dfe-4b53-4dc9-b1f5-d651593e7b00.cpp:19: e70d2dfe-4b53-4dc9-b1f5-d651593e7b00.cpp: In function ‘int main()’: e70d2dfe-4b53-4dc9-b1f5-d651593e7b00.cpp:29:20: error: ‘order_by_points’ was not  declared in this scope    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ e70d2dfe-4b53-4dc9-b1f5-d651593e7b00.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,15 +1,5 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b) {
-    return a == b;
-}
-
-vector<int> order_by_points(const vector<int>& nums) {
-    vector<int> sorted_nums = nums;
-    sort(sorted_nums.begin(), sorted_nums.end(), [](int a, int b) {
+auto order_by_points = [](vector<int> nums) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         int temp_a = abs(a), temp_b = abs(b);
         while (temp_a > 0) {
@@ -25,10 +15,5 @@ vector<int> order_by_points(const vector<int>& nums) {
         }
         return sum_a < sum_b;
     });
-    return sorted_nums;
-}
-
-int main() {
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
-}
+    return nums;
+};
