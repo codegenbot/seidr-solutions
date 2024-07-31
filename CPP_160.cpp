f0e4c12a81e7be96e825f74f7d17fe96b61a0582
beep boop <githubbot@vadim.me>
2024-07-31 08:29:46 +0200

    Solution 3 of CPP/160, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/w08zezyf Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,22 +1,36 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
+#include<stdio.h>
+#include<math.h>
+#include<vector>
+#include<string>
+using namespace std;
+#include<algorithm>
+#include<stdlib.h>
+
+int do_algebra(vector<string> operato, vector<int> operand) {
     int result = 0;
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
-            result += operand[i + 1];
-        } else if (operator_[i] == "-") {
-            result -= operand[i + 1];
-        } else if (operator_[i] == "*") {
-            int temp = 1;
-            for (int j = i + 1; j <= operand.size() - 1; j++) {
-                temp *= operand[j];
+    for(int i=0; i<operato.size(); i++) {
+        if(operato[i] == "+") {
+            result += operand[i+1];
+        } else if(operato[i] == "-") {
+            result -= operand[i+1];
+        } else if(operato[i] == "*") {
+            int temp = 0;
+            for(int j=i+1; j<operand.size(); j++) {
+                temp += operand[j];
             }
-            result += temp;
-        } else if (operator_[i] == "//") {
-            int temp = operand[i + 1] / 1;
-            result += temp;
-        } else if (operator_[i] == "**") {
-            int temp = pow(operand[i + 1], 1);
-            result += temp;
+            result *= temp;
+        } else if(operato[i] == "//") {
+            int temp = 0;
+            for(int j=i+1; j<operand.size(); j++) {
+                temp += operand[j];
+            }
+            result /= temp;
+        } else if(operato[i] == "**") {
+            int temp = 0;
+            for(int j=i+1; j<operand.size(); j++) {
+                temp += operand[j];
+            }
+            result = pow(result, temp);
         }
     }
     return result;
