b82be27d4f2d778e7e381824d94f6968dd6dd645
beep boop <githubbot@vadim.me>
2024-07-31 15:41:36 +0200

    Solution 48 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/zm4tznio Prompt:       |                                   ^~~~~~ 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp:19: 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp: In function ‘int main()’: 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 73803a38-7d22-4d2e-b9ee-0af6bbe30630.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,24 +1,23 @@
-#include <vector>
-#include <algorithm>
-
-bool issame(vector<vector<int>> a, vector<vector<int>> b);
-
-bool issame(vector<vector<int>> a, vector<vector<int>> b){
+bool issame(vector<vector<int>> a, vector<vector<int>> b) {
     return a == b;
 }
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x){
+vector<vector<int>> get_row(vector<vector<int>> lst, int x);
+
+vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
     vector<vector<int>> result;
-    for (int i = 0; i < lst.size(); ++i) {
-        for (int j = 0; j < lst[i].size(); ++j) {
+    for (int i = 0; i < lst.size(); i++) {
+        for (int j = 0; j < lst[i].size(); j++) {
             if (lst[i][j] == x) {
                 result.push_back({i, j});
             }
         }
     }
-    sort(result.begin(), result.end());
-    for (int i = 0; i < result.size(); ++i) {
-        sort(result[i].begin(), result[i].end(), greater<int>());
-    }
+    sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b) {
+        if (a[0] == b[0]) {
+            return a[1] > b[1];
+        }
+        return a[0] < b[0];
+    });
     return result;
 }
