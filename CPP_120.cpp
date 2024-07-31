b904283c4609217a4a1dde608dac2bc5798e86b6
beep boop <githubbot@vadim.me>
2024-07-31 14:08:37 +0200

    Solution 24 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7q7tu4pk Prompt:       |                    ^~~ 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp:6: 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp: In function ‘int main()’: 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp:16:20: error: ‘maximum’ was not declare d in this scope    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 148f6e29-05e4-42cf-8676-e166ee5013c0.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,17 +1,18 @@
-#include <vector>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
 
 vector<int> maximum(vector<int> arr, int k) {
     sort(arr.begin(), arr.end(), greater<int>());
     vector<int> result(arr.begin(), arr.begin() + k);
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 int main() {
