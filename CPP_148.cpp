d467e9f5fe444b52e9ec711924b4d9adaa9d24a6
beep boop <githubbot@vadim.me>
2024-07-31 10:53:17 +0200

    Solution 82 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ygx48vwa Prompt:       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from 300d7d7a-ed5d-4dcf-bf46-e194dcefe861.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 300d7d7a-ed5d-4dcf-bf46-e194dcefe861.cpp:24:44: error: ‘b’ was not declared in t his scope; did you mean ‘bf’?    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^       |                                            bf 300d7d7a-ed5d-4dcf-bf46-e194dcefe861.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 300d7d7a-ed5d-4dcf-bf46-e194dcefe861.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -7,10 +7,8 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return a == b;
 }
 
-std::vector<std::string> bf(std::string a, std::string b) {
-    if(a == "Jupiter" && b == "Makemake") {
-        return std::vector<std::string>();
-    } else {
-        return std::vector<std::string>{"Some", "output"};
-    }
+std::vector<std::string> bf() {
+    return std::vector<std::string>{"Some", "output"};
 }
+
+assert(issame(bf(), std::vector<std::string>{}));
