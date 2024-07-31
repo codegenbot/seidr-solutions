641fa9be977c6bf191687dad0cd74869d7ceb467
beep boop <githubbot@vadim.me>
2024-07-31 12:52:14 +0200

    Solution 96 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/yvg8xybk Prompt: d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp:31:6: error: ambiguating new declaratio n of ‘bool issame(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp:7:13: note: old declaration ‘std::vecto r<int> issame(std::vector<int>, std::vector<int>)’     7 | vector<int> issame(vector<int> a, vector<int> b) {       |             ^~~~~~ d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp:30: d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp: In function ‘int main()’: d978fd9d-ce5a-45df-baca-812dbcaabfb9.cpp:40:20: error: ‘common’ was not declared  in this scope    40 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,27 +4,24 @@
 
 using namespace std;
 
-bool is_same(vector<int> a, vector<int> b) {
+vector<int> issame(vector<int> a, vector<int> b) {
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
-    
-    for (int num : a) {
-        if (binary_search(b.begin(), b.end(), num)) {
-            return true;
-        }
-    }
-    
-    return false;
+
+    vector<int> result;
+    set_intersection(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
+
+    return result;
 }
 
 int main() {
     vector<int> l1 = {1, 2, 3, 4, 5};
     vector<int> l2 = {1, 3, 5, 7, 9};
 
-    if (is_same(l1, l2)) {
-        cout << "Lists have at least one common element." << endl;
-    } else {
-        cout << "Lists have no common elements." << endl;
+    vector<int> res = issame(l1, l2);
+
+    for (int num : res) {
+        cout << num << " ";
     }
 
     return 0;
