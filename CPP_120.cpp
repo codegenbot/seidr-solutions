7f61ef6f58ed4fafd3a12a6e0f1864fcea2e9b4b
beep boop <githubbot@vadim.me>
2024-07-31 14:04:55 +0200

    Solution 9 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/7q7tu4pk Prompt:       |                    ^~~ 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp:6: 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp: In function ‘int main()’: 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp:16:20: error: ‘maximum’ was not declare d in this scope    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 49f5a178-05ea-41bc-a8c9-18849d26f34a.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,27 +1,11 @@
 #include <vector>
 
-using namespace std;
-
-bool issame(const int& a, const int& b)
-{
-    return a == b;
+bool issame(int x, int y) {
+    return x == y;
 }
 
-vector<int> maximum(vector<int>& arr, int k)
-{
+vector<int> maximum(vector<int> arr, int k) {
     sort(arr.begin(), arr.end(), greater<int>());
     vector<int> result(arr.begin(), arr.begin() + k);
     return result;
-}
-
-int main()
-{
-    vector<int> arr = {2, 4, 1, 6, 3, 5};
-    int k = 3;
-    vector<int> result = maximum(arr, k);
-    for (int num : result)
-    {
-        cout << num << " ";
-    }
-    return 0;
 }
