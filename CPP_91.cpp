30750549d4e1ccdb1a1829da018081538b07e68c
beep boop <githubbot@vadim.me>
2024-07-31 13:15:27 +0200

    Solution 4 of CPP/91, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/p3hxfjq1 Prompt: e919e4ac-0042-4a05-9f36-a8458b408eae.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int is_bored(string S){       |              ^~~~~~ In file included from e919e4ac-0042-4a05-9f36-a8458b408eae.cpp:15: e919e4ac-0042-4a05-9f36-a8458b408eae.cpp: In function ‘int main()’: e919e4ac-0042-4a05-9f36-a8458b408eae.cpp:17:21: error: ‘is_bored’ cannot be used  as a function    17 |     assert (is_bored("You and I are going for a walk") == 0);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_91.cpp
+++ CPP_91.cpp
@@ -1,4 +1,7 @@
-int is_bored(string S){
+#include <string>
+using namespace std;
+
+int is_bored(std::string S){
     int count = 0;
     bool isI = false;
     for (int i = 0; i < S.length(); ++i) {
