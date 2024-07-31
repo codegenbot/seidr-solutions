227eaffb86ec1a9b9348d4e18f1b794bd650adcd
beep boop <githubbot@vadim.me>
2024-07-31 19:33:35 +0200

    Solution 73 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/qwxms0um Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 31c9f4c2-0303-4f10-b108-6880589c8210.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 31c9f4c2-0303-4f10-b108-6880589c8210.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 31c9f4c2-0303-4f10-b108-6880589c8210.cpp:19: 31c9f4c2-0303-4f10-b108-6880589c8210.cpp: In function ‘int main()’: 31c9f4c2-0303-4f10-b108-6880589c8210.cpp:29:20: error: ‘parse_music’ was not dec lared in this scope    29 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                    ^~~~~~~~~~~ 31c9f4c2-0303-4f10-b108-6880589c8210.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -1,15 +1,29 @@
-bool issame(vector<int> a, vector<int> b){
+#include <iostream>
+#include <vector>
+#include <string>
+
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-vector<int> parse_music(string music_string);
+std::vector<int> parse_music(std::string music_string);
 
-vector<int> parse_music(string music_string){
-    vector<int> beats;
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+    std::vector<int> result = parse_music(input);
+    for (int i : result) {
+        std::cout << i << " ";
+    }
+    return 0;
+}
+
+std::vector<int> parse_music(std::string music_string){
+    std::vector<int> beats;
     size_t pos = 0;
-    string delimiter = " ";
-    while ((pos = music_string.find(delimiter)) != string::npos) {
-        string note = music_string.substr(0, pos);
+    std::string delimiter = " ";
+    while ((pos = music_string.find(delimiter)) != std::string::npos) {
+        std::string note = music_string.substr(0, pos);
         if (note == "o") {
             beats.push_back(4);
         } else if (note == "o|") {
