b4318a9e4bcd027ac59f0a8797a5e9aca0424727
beep boop <githubbot@vadim.me>
2024-07-31 07:57:28 +0200

    Solution 32 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d15gsk7m Prompt:       |                                     ^~~~~~ d523fc45-2951-4007-90a7-06d23ee1787f.cpp:26:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from d523fc45-2951-4007-90a7-06d23ee1787f.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from d523fc45-2951-4007-90a7-06d23ee1787f.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d523fc45-2951-4007-90a7-06d23ee1787f.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d523fc45-2951-4007-90a7-06d23ee1787f.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ d523fc45-2951-4007-90a7-06d23ee1787f.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <string>
-#include <cassert>
 #include <algorithm>
+#include <cassert>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
