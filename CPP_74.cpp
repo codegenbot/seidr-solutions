d80ff56d498a36913f9a674a56f1bfd940ef0584
beep boop <githubbot@vadim.me>
2024-07-31 15:29:05 +0200

    Solution 58 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jadset2t Prompt:       |                              ^~~~~~ 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp:18:37: error: ‘string’ was not declared  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp:17: 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp: In function ‘int main()’: 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp:27:20: error: ‘total_match’ was not dec lared in this scope    27 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 33d22011-15bb-4d55-958e-3f46c9ed4636.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -3,23 +3,27 @@
 #include <string>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+std::vector<std::string> cmp(const std::vector<std::string>& lst1, const std::vector<std::string>& lst2) {
     int totalChars1 = 0;
-    for (std::string str : a) {
+    for (const std::string& str : lst1) {
         totalChars1 += str.size();
     }
 
     int totalChars2 = 0;
-    for (std::string str : b) {
+    for (const std::string& str : lst2) {
         totalChars2 += str.size();
     }
 
-    return totalChars1 == totalChars2;
+    if (totalChars1 < totalChars2) {
+        return lst1;
+    } else {
+        return lst2;
+    }
 }
 
 int main() {
-    assert(issame({"hello", "world"}, {"hi", "there"}));
-    assert(!issame({"apple", "orange"}, {"banana"}));
+    assert(cmp({"hello", "world"}, {"hi", "there"}) == std::vector<std::string>{"hi", "there"});
+    assert(cmp({"apple", "orange"}, {"banana"}) == std::vector<std::string>{"banana"});
 
     return 0;
 }
