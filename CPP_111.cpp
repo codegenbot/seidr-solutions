469eae6ccce8102a9b96abdcdef7c1e81b1941c6
beep boop <githubbot@vadim.me>
2024-07-31 15:25:53 +0200

    Solution 48 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/02lyfggr Prompt:       |                             ^~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:23:33: error: expected primary-expressi on before ‘char’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:23:38: error: expected primary-expressi on before ‘int’    23 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:23:44: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from 091f7155-73b7-49a0-ac02-a02706913611.cpp:22: 091f7155-73b7-49a0-ac02-a02706913611.cpp: In function ‘int main()’: 091f7155-73b7-49a0-ac02-a02706913611.cpp:37:20: error: ‘histogram’ was not decla red in this scope    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ 091f7155-73b7-49a0-ac02-a02706913611.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,32 +1,8 @@
-#include <map>
-#include <string>
-
 bool issame(map<char,int> a, map<char,int> b) {
     return a == b;
 }
 
-map<char, int> histogram(string test) {
-    map<char, int> result;
-    string str = test + " ";
-    char prev = ' ';
-    int count = 0;
-
-    for (char c : str) {
-        if (c == prev) {
-            count++;
-        } else {
-            if (count > 0) {
-                result[prev] = count;
-            }
-            prev = c;
-            count = 1;
-        }
-    }
-
-    return result;
-}
-
 int main() {
-    assert(histogram("a") == ({{'a', 1}}));
-    return 0;
+    assert(histogram("a") == ((map<char,int>{{'a', 1}})));
+    // Rest of your code...
 }
