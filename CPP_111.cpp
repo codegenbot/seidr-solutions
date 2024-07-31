c2deb58e8e19195e44e42f1063732a307f21bc8c
beep boop <githubbot@vadim.me>
2024-07-31 16:35:21 +0200

    Solution 63 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/0oqol6n4 Prompt:       |                             ^~~ a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp:24:33: error: expected primary-expressi on before ‘char’    24 | bool issame(map<char,int> a,map<char,int> b){       |                                 ^~~~ a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp:24:38: error: expected primary-expressi on before ‘int’    24 | bool issame(map<char,int> a,map<char,int> b){       |                                      ^~~ a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp:24:44: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(map<char,int> a,map<char,int> b){       |                                            ^ In file included from a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp:23: a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp: In function ‘int main()’: a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp:38:20: error: ‘histogram’ was not decla red in this scope    38 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~ a85a7e6c-6b2c-492a-a3cd-fda87e5abf0f.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,11 +1,13 @@
 #include <algorithm>
 #include <cassert>
 
+bool issame(unordered_map<char, int> a, unordered_map<char, int> b);
+
+unordered_map<char, int> histogram(string test);
+
 bool issame(unordered_map<char, int> a, unordered_map<char, int> b) {
     return a == b;
 }
-
-unordered_map<char, int> histogram(string test);
 
 unordered_map<char, int> histogram(string test) {
     unordered_map<char, int> result;
