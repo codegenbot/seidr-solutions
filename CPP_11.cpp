2238a54247d085e60162467c42453ba2f22a477b
beep boop <githubbot@vadim.me>
2024-07-31 09:02:12 +0200

    Solution 2 of CPP/11, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/jhcoc9f1 Prompt: c984c3d3-35f4-40f8-81c1-62ba8f681aff.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string string_xor(string a, string b) {       | ^~~~~~ In file included from c984c3d3-35f4-40f8-81c1-62ba8f681aff.cpp:13: c984c3d3-35f4-40f8-81c1-62ba8f681aff.cpp: In function ‘int main()’: c984c3d3-35f4-40f8-81c1-62ba8f681aff.cpp:15:13: error: ‘string_xor’ was not decl ared in this scope    15 |     assert (string_xor("0101", "0000") == "0101");       |             ^~~~~~~~~~

--- CPP_11.cpp
+++ CPP_11.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
 #include <string>
 
-string string_xor(string a, string b) {
-    string result = "";
+using namespace std;
+
+std_string string_xor(std_string a, std_string b) {
+    std_string result = "";
     for (int i = 0; i < a.length(); i++) {
         if ((a[i] - '0') ^ (b[i] - '0')) {
             result += "1";
@@ -10,4 +13,8 @@ string string_xor(string a, string b) {
         }
     }
     return result;
+}
+
+int main() {
+    assert (string_xor("0101", "0000") == "0101");
 }
