35191d867ddb4cb019e8276fde79a5485358cc97
beep boop <githubbot@vadim.me>
2024-07-31 10:25:48 +0200

    Solution 8 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/dcm7ao8l Prompt:       |     ^~~ eb46a395-f6f4-41a3-b728-bd57bd550182.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 0; i < lst.size(); ++i) {       |                     ^ eb46a395-f6f4-41a3-b728-bd57bd550182.cpp:2:37: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 0; i < lst.size(); ++i) {       |                                     ^~ eb46a395-f6f4-41a3-b728-bd57bd550182.cpp:10:5: error: expected unqualified-id be fore ‘return’    10 |     return sum;       |     ^~~~~~ eb46a395-f6f4-41a3-b728-bd57bd550182.cpp:11:1: error: expected declaration befor e ‘}’ token    11 | }       | ^ In file included from eb46a395-f6f4-41a3-b728-bd57bd550182.cpp:13: eb46a395-f6f4-41a3-b728-bd57bd550182.cpp: In function ‘int main()’: eb46a395-f6f4-41a3-b728-bd57bd550182.cpp:15:13: error: ‘sum_squares’ was not dec lared in this scope    15 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ^~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -10,9 +10,3 @@ int sum_squares(const std::vector<int>& lst) {
     }
     return sum;
 }
-
-int main() {
-    assert(sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);
-
-    return 0;
-}
