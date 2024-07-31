a595af14ca107f106d6996e3f78696c1e6b88b9e
beep boop <githubbot@vadim.me>
2024-07-31 08:17:08 +0200

    Solution 21 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pv69h4z1 Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ daec94c7-41b0-4423-a952-bd665d715da2.cpp:16:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from daec94c7-41b0-4423-a952-bd665d715da2.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from daec94c7-41b0-4423-a952-bd665d715da2.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ daec94c7-41b0-4423-a952-bd665d715da2.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from daec94c7-41b0-4423-a952-bd665d715da2.cpp:15: daec94c7-41b0-4423-a952-bd665d715da2.cpp: In function ‘int main()’: daec94c7-41b0-4423-a952-bd665d715da2.cpp:25:20: error: ‘incr_list’ was not decla red in this scope    25 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,16 +1,13 @@
-namespace NS {
-    #include <vector>
-    
-    std::vector<int> incr_list(std::vector<int> v) {
-        std::vector<int> result;
-        for(int i = 0; i < v.size(); i++) {
-            result.push_back(v[i] + 1);
+namespace detail {
+    std::vector<int> incr_list(std::vector<int> arr) {
+        for(int i = 0; i < arr.size(); i++) {
+            arr[i]++;
         }
-        return result;
+        return arr;
     }
 }
 
-bool NS::issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b){
     if(a.size() != b.size()) {
         return false;
     }
@@ -23,6 +20,6 @@ bool NS::issame(std::vector<int> a, std::vector<int> b){
 }
 
 int main() {
-    assert(NS::issame(NS::incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    assert(issame(detail::incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
     return 0;
 }
