903820423a2a042eae8eede87973eefec1727f33
beep boop <githubbot@vadim.me>
2024-07-31 17:21:32 +0200

    Solution 78 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/a77wu89g Prompt: Complete the following code given the task description and function signature.

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,15 +1,13 @@
+vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"},
+                                         {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+
 string int_to_mini_roman(int number) {
-    string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    
-    string result = "";
-    
-    for (int i = 0; i < 13; i++) {
-        while (number >= val[i]) {
-            number -= val[i];
-            result += roman[i];
+    string roman = "";
+    for (auto p : romanMap) {
+        while (number >= p.first) {
+            roman += p.second;
+            number -= p.first;
         }
     }
-    
-    return result;
+    return roman;
 }
