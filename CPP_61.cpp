16713ba6f3f96dcbeb88a0ecc9b6a7f5bc0038fe
beep boop <githubbot@vadim.me>
2024-07-31 11:35:11 +0200

    Solution 14 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run7/runs/axunvr1h Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -1,14 +1,13 @@
 ```cpp
 #include <iostream>
 #include <string>
-#include <cassert>
 
-bool correct_bracketing(std::string brackets){
+bool correct_bracketing(std::string brackets) {
     int count = 0;
-    for(int i=0; i<brackets.size(); i++){
-        if('(' == brackets[i]) count++;
-        else if(')' == brackets[i]) {
-            if(count <= 0) return false;
+    for (int i = 0; i < brackets.size(); i++) {
+        if (brackets[i] == '(') count++;
+        else if (brackets[i] == ')') {
+            if (count <= 0) return false;
             count--;
         }
     }
@@ -18,7 +17,7 @@ bool correct_bracketing(std::string brackets){
 int main() {
     std::string brackets = "(()())()))));";
     assert(!correct_bracketing(brackets));
-    if(correct_bracketing(brackets)) {
+    if (correct_bracketing(brackets)) {
         std::cout << "The bracket sequence is correct." << std::endl;
     } else {
         std::cout << "The bracket sequence contains incorrect pairings." << std::endl;
