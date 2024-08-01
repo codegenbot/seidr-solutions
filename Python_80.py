e7b2f1439a3e473e44a9cdbe7afe6cc1e7db4041
beep boop <githubbot@vadim.me>
2024-08-01 22:19:26 +0200

    Solution 5 of Python/80, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/70vn67bc Prompt: Complete the following code given the task description and function signature.

--- Python_80.py
+++ Python_80.py
@@ -3,7 +3,7 @@ def is_happy(s):
     if len(s) < 3:
         return False
     for i in range(0, len(s), 3):
-        sub_str = s[i:i+3]
-        if sub_str.count(sub_str[0]) != 1:
+        chunk = s[i:i+3]
+        if len(set(chunk)) != 3:
             return False
     return True
