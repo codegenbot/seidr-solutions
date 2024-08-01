66c87e3ef4101ab0ceec15b11a2930cf79c43f70
beep boop <githubbot@vadim.me>
2024-08-01 20:24:26 +0200

    Solution 8 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/a1otzl2y Prompt: Complete the following code given the task description and function signature.

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,18 +1,46 @@
-string anti_shuffle(string s){
+#include <algorithm>
+using namespace std;
+
+string anti_shuffle(string s) {
     string result = "";
-    for(int i=0; i<s.length(); i++){
-        if(s[i] == ' '){
-            result += ' ';
+    for (const auto& word : split(s, " ")) {
+        for (char c : word) {
+            result += c;
         }
-        else{
-            string word = "";
-            for(int j=i; j<=s.find(' ',i); j++){
-                word += s[j];
+        for (char c : word) {
+            size_t pos = 0;
+            while ((pos = word.find(c)) != string::npos) {
+                word.replace(pos, 1, "");
+                break;
             }
-            sort(word.begin(),word.end());
-            result += word;
-            i = s.find(' ',i);
         }
+        if (!result.empty()) {
+            result += " ";
+        }
+    }
+    return result;
+}
+
+string split(const string& str, const string& delimiters) {
+    size_t start = 0;
+    size_t end = str.find(delimiters);
+    vector<string> tokens;
+    while (end != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+        start = end + delimiters.size();
+        end = str.find(delimiters, start);
+    }
+    tokens.push_back(str.substr(start));
+    return join(tokens, " ");
+}
+
+string join(const vector<string>& strs, const string& delimiter) {
+    string result;
+    for (const auto& str : strs) {
+        if (!result.empty()) {
+            result += delimiter;
+        }
+        result += str;
     }
     return result;
 }
