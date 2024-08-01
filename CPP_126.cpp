029eb20501bf0f1fe98510bb4d18254e5cf4630a
beep boop <githubbot@vadim.me>
2024-08-01 22:37:38 +0200

    Solution 73 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j010drr1 Prompt: 90dc709f-125f-4a6a-956e-657172a5ce9c.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 90dc709f-125f-4a6a-956e-657172a5ce9c.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst){       |                       ^~~ In file included from 90dc709f-125f-4a6a-956e-657172a5ce9c.cpp:15: 90dc709f-125f-4a6a-956e-657172a5ce9c.cpp: In function ‘int main()’: 90dc709f-125f-4a6a-956e-657172a5ce9c.cpp:17:22: error: ‘is_sorted’ cannot be use d as a function    17 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -5,24 +5,32 @@
 bool is_sorted(vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
         if(lst[i] <= lst[i-1]){
-            return false;
+            vector<int> temp;
+            for(int j = 0; j < i; j++)
+                temp.push_back(lst[j]);
+            if(count(temp.begin(), temp.end(), lst[i]) > 1)
+                return false;
+            break;
         }
     }
     return true;
 }
 
 int main() {
-    vector<int> input;
-    int num;
-    std::cout << "Enter numbers (enter -1 to stop): ";
-    while(true){
-        std::cin >> num;
-        if(num == -1) break;
-        input.push_back(num);
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+
+    vector<int> lst(n);
+    for(int i = 0; i < n; i++){
+        std::cout << "Enter element " << i+1 << ": ";
+        std::cin >> lst[i];
     }
-    if(is_sorted(input))
-        std::cout << "The list is sorted.\n";
+
+    if(is_sorted(lst))
+        std::cout << "The list is sorted." << std::endl;
     else
-        std::cout << "The list is not sorted.\n";
+        std::cout << "The list is not sorted." << std::endl;
+
     return 0;
 }
