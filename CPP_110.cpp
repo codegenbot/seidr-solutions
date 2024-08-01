0ca4654b8c979e61bba4c66fb95932c6a61e8804
beep boop <githubbot@vadim.me>
2024-08-01 22:08:30 +0200

    Solution 8 of CPP/110, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2r0ka5k7 Prompt: Complete the following code given the task description and function signature.

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,15 +1,6 @@
 string exchange(vector<int> lst1, vector<int> lst2) {
-    int evenCount = 0;
     for (int num : lst1) {
-        if (num % 2 == 0)
-            evenCount++;
+        if (num % 2 != 0) return "NO";
     }
-    for (int num : lst2) {
-        if (num % 2 == 0) {
-            evenCount--;
-            break;
-        } else
-            evenCount++;
-    }
-    return evenCount == 0 ? "YES" : "NO";
+    return "YES";
 }
