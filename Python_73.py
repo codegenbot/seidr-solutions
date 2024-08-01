8171fedc204a9dcf337f549604369e4c8465c810
beep boop <githubbot@vadim.me>
2024-08-01 22:07:41 +0200

    Solution 30 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nbqcl5il Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a24e266c-e882-46f5-933f-ab483ef87 89e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,17 +1,23 @@
+```
 def smallest_change(arr):
     n = len(arr)
-    left = right = 0
-    changes = 0
+    arr_pos = [*enumerate(arr)]
+    pos_arr = {v:i for i,v in enumerate(arr)}
     
-    while left < n // 2:
-        if arr[left] == arr[n - 1 - right]:
-            left += 1
-            right -= 1
-        else:
-            changes += 1
-            right -= 1
-    
-    return changes
-
-arr = list(map(int, input().split()))
-print(smallest_change(arr))
+    ans = 0
+    while arr_pos:
+        max_length = 0
+        end = -1
+        for i, val in list(arr_pos):
+            if val == end:
+                del arr_pos[i]
+                continue
+            if pos_arr[val] != i:
+                temp = pos_arr[val]
+                pos_arr[val] = i
+                arr_pos[i], arr_pos[temp] = arr_pos[temp], arr_pos[i]
+                ans += 1
+            else:
+                max_length = max(max_length, i+1)
+        end = arr[pos_arr[max(arr_pos)]]
+    return ans
