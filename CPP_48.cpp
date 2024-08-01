9601f804bb945331f0cf546eebfa491fbdec2d08
beep boop <githubbot@vadim.me>
2024-08-01 16:35:27 +0200

    Solution 4 of CPP/48, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nvod4964 Prompt: Complete the following code given the task description and function signature.

--- CPP_48.cpp
+++ CPP_48.cpp
@@ -1,3 +1,6 @@
-string temp = text;
-reverse(temp.begin(), temp.end());
-return text == temp;
+int len = text.length();
+for(int i=0; i<len/2; i++){
+    if(text[i] != text[len-i-1])
+        return false;
+}
+return true;
