7bf1a059e52e086037aefe036a31d07376b96ab9
beep boop <githubbot@vadim.me>
2024-08-01 17:13:40 +0200

    Solution 20 of CPP/38, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gf7y04ly Prompt: 191deae7-594c-42bc-8457-73ecd5302cda.cpp:35:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    35 |             str+=chr;       |             ^~~       |             std 191deae7-594c-42bc-8457-73ecd5302cda.cpp:38:15: error: expected ‘;’ before ‘enco ded_str’    38 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 191deae7-594c-42bc-8457-73ecd5302cda.cpp:25: 191deae7-594c-42bc-8457-73ecd5302cda.cpp:39:31: error: ‘encoded_str’ was not dec lared in this scope    39 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 191deae7-594c-42bc-8457-73ecd5302cda.cpp:39:17: error: ‘decode_cyclic’ was not d eclared in this scope    39 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ 191deae7-594c-42bc-8457-73ecd5302cda.cpp:39:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    39 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,12 +1,27 @@
-string decode_cyclic(string s){ 
-    int l=s.length();
-    string x,output;
-    int i;
-    for (i=0;i*3<l;i++)
-    {
-        x=s.substr(i*3,3);
-        if (x.length()==3) x=x[2]+x.substr(0,2);
-        output=output+x;
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+string encode_cyclic(string s) {
+    int l = s.length();
+    string encoded_str;
+    for (int i = 0; i * 3 < l; i++) {
+        string x = s.substr(i * 3, 3);
+        if (x.length() == 3) x = x[1] + x.substr(0, 2);
+        encoded_str += x;
     }
-    return output;
+    return encoded_str;
+}
+
+string decode_cyclic(string s) {
+    int l = s.length();
+    string decoded_str;
+    for (int i = 0; i * 3 < l; i++) {
+        string x = s.substr(i * 3, 3);
+        if (x.length() == 3) x = x[2] + x.substr(0, 2);
+        decoded_str += x;
+    }
+    return decoded_str;
 }
