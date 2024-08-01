521bcdfd6995bb08f1e690320b654d66a8dc90c6
beep boop <githubbot@vadim.me>
2024-08-01 21:40:36 +0200

    Solution 57 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e9kg7thn Prompt:       |                    ^~~ 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp:7: 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp: In function ‘int main()’: 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 91d63b3a-d3e2-40ca-ba12-fb201978fa7d.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -15,12 +16,12 @@ bool issame(vector<int> a, vector<int> b) {
 }
 
 vector<int> maximum(vector<vector<int>> arr, int k) {
-    vector<int> result(arr[0].begin(), arr[0].begin() + k);
+    vector<int> result = arr[0].begin(), arr[0].begin() + k);
     sort(result.begin(), result.end());
     return result;
 }
 
 int main() {
-    assert(issame(maximum({{1, 2, 3, -23, 243, -400, 0},{-10,11,-20,21,22,23,24},{5,6}}, 7) , {0,1,2,-23,243,-400});
+    assert(issame(maximum({{1, 2, 3, -23, 243, -400, 0}, 7) , {0,1,2,-23,243,-400}));
     return 0;
 }
