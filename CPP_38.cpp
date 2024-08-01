aa6e93b942025ac6a540abb81a8b473083dfd89e
beep boop <githubbot@vadim.me>
2024-08-01 22:09:08 +0200

    Solution 11 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/8zqia25g Prompt: 5320012d-a4fc-45e7-bcd1-76dc1644d0b1.cpp:30:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    30 |             str+=chr;       |             ^~~       |             std 5320012d-a4fc-45e7-bcd1-76dc1644d0b1.cpp:33:15: error: expected ‘;’ before ‘enco ded_str’    33 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 5320012d-a4fc-45e7-bcd1-76dc1644d0b1.cpp:20: 5320012d-a4fc-45e7-bcd1-76dc1644d0b1.cpp:34:31: error: ‘encoded_str’ was not dec lared in this scope    34 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 5320012d-a4fc-45e7-bcd1-76dc1644d0b1.cpp:34:17: error: ‘decode_cyclic’ was not d eclared in this scope    34 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ 5320012d-a4fc-45e7-bcd1-76dc1644d0b1.cpp:34:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    34 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -3,37 +3,9 @@
 
 string encode_cyclic(string s){
     string output;
-    string x;
-    int l = s.length();
-    
-    for (int i = 0; i < l; i++)
+    for (int i = 0; i < s.length(); i++)
     {
-        x = s.substr(i, 1);
-        int pos = (i * 3) % l;
-        output += x;
-        output += s[pos];
+        output += s[(i + 2) % s.length()];
     }
-    
     return output;
-}
-
-string decode_cyclic(string s){ 
-    string output;
-    string x;
-    int l = s.length();
-    
-    for (int i = 0; i < l; i += 2)
-    {
-        x = s.substr(i, 1);
-        output += x;
-    }
-    
-    return output;
-}
-
-int main() {
-    string str = "input_string";
-    string encoded_str = encode_cyclic(str);
-    assert(decode_cyclic(encoded_str) == str);
-    return 0;
 }
