b87230bd5aa6d2c8a2a885913fdedadc55669636
beep boop <githubbot@vadim.me>
2024-08-01 20:13:38 +0200

    Solution 1 of Python/57, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/fjsr8bpg Prompt: Complete the following code given the task description and function signature.

--- Python_57.py
+++ Python_57.py
@@ -1,9 +1,4 @@
-```
 def monotonic(l: list):
-    if len(l) < 2:
-        return True
-    inc = l[1] > l[0]
-    for i in range(2, len(l)):
-        if (inc and l[i] <= l[i-1]) or (not inc and l[i] >= l[i-1]):
-            return False
-    return True
+    increasing = all(x <= y for x, y in zip(l, l[1:]))
+    decreasing = all(x >= y for x, y in zip(l, l[1:]))
+    return increasing or decreasing
