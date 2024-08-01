1a959f85be1f6d5840a3297b484eebc25dbc46ff
beep boop <githubbot@vadim.me>
2024-08-01 14:03:15 +0200

    Solution 10 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/h9r3al0w Prompt: fd22fdc0-66ec-46d9-b8a4-970a613cb987.cpp:3:1: error: ‘string’ does not name a ty pe     3 | string sort_numbers(string numbers) {       | ^~~~~~ In file included from fd22fdc0-66ec-46d9-b8a4-970a613cb987.cpp:35: fd22fdc0-66ec-46d9-b8a4-970a613cb987.cpp: In function ‘int main()’: fd22fdc0-66ec-46d9-b8a4-970a613cb987.cpp:37:13: error: ‘sort_numbers’ was not de clared in this scope    37 |     assert (sort_numbers("six five four three two one zero") == "zero on e two three four five six");       |             ^~~~~~~~~~~~

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,29 +1,37 @@
-map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                           {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                           {"eight", 8}, {"nine", 9}};
+```
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
 
-string sort_numbers(string numbers) {
-    vector<string> numVec;
-    string temp;
+std::string sort_numbers(std::string numbers) {
+    std::map<std::string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
+                                         {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
+                                         {"eight", 8}, {"nine", 9}};
+    std::vector<std::string> numVec;
+    std::string temp;
 
     for (int i = 0; i < numbers.length(); i++) {
         if (numbers[i] == ' ') {
-            numVec.push_back(temp);
-            temp = "";
-        } else
-            temp += numbers[i];
-    }
-    numVec.push_back(temp);
-
-    sort(numVec.begin(), numVec.end());
-
-    string result;
-    for (auto &num : numVec) {
-        for (const auto &pair : numMap) {
-            if (pair.first == num)
-                result += pair.first + " ";
+            continue;
         }
+        for (int j = i + 1; j <= numbers.length(); j++) {
+            if (j == numbers.length() || numbers[j] != numbers[i]) {
+                temp = numbers.substr(i, j - i);
+                i = j - 1;
+                break;
+            }
+        }
+        numVec.push_back(temp);
     }
 
+    std::sort(numVec.begin(), numVec.end(), [&](std::string a, std::string b) {
+        return numMap.at(a) < numMap.at(b);
+    });
+
+    std::string result;
+    for (auto &num : numVec) {
+        result += num + " ";
+    }
     return result.substr(0, result.length() - 1);
 }
