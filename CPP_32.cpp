dbe98cb15fb7da1381c5240a630c679f76cd2bcf
beep boop <githubbot@vadim.me>
2024-08-01 21:54:08 +0200

    Solution 93 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4r2oc36c Prompt: In file included from /usr/include/c++/11/vector:67,                  from 537082ac-cf2b-4d79-9229-407ef53a7ad2.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 537082ac-cf2b-4d79-9229-407ef53a7ad2.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 537082ac-cf2b-4d79-9229-407ef53a7ad2.cpp:37:16: error: expected primary-expressi on before ‘double’    37 |         vector<double> coeffs = {};       |                ^~~~~~ 537082ac-cf2b-4d79-9229-407ef53a7ad2.cpp:42:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    42 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff 537082ac-cf2b-4d79-9229-407ef53a7ad2.cpp:45:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    45 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -2,17 +2,17 @@
 #include <cmath>
 #include <cassert>
 
-double poly(const std::vector<double>& coefficients, double x){
+double poly(const std::vector<double>& coeffs, double x) {
     double result = 0.0;
-    for (int i = 0; i < coefficients.size(); i++) {
-        result += coefficients[i] * pow(x, i);
+    for (size_t i = 0; i < coeffs.size(); i++) {
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
 
-double find_zero(const std::vector<double>& coefficients){
-    double a = coefficients[0];
-    double b = coefficients[1];
+double find_zero(const std::vector<double>& coeffs) {
+    double a = coeffs[0];
+    double b = coeffs[1];
     return -b / a;
 }
 
