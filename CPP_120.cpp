233da496912c189fd4ac74e930dacd259fc7dd4f
beep boop <githubbot@vadim.me>
2024-08-01 23:39:05 +0200

    Solution 36 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cbcj0kw7 Prompt:       |                    ^~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:8: 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp: In function ‘int main()’: 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:18:20: error: ‘maximum’ was not declare d in this scope    18 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,15 +1,18 @@
 vector<int> maximum(vector<int> arr, int k) {
     vector<int> res;
     unordered_set<int> unique;
-    
-    for(int i = 0; i < arr.size(); i++) {
-        if(unique.find(arr[i]) == unique.end()) {
-            res.push_back(arr[i]);
-            if(res.size() == k)
-                break;
-            unique.insert(arr[i]);
+
+    for (int num : arr) {
+        if (unique.size() < k) {
+            unique.insert(num);
+            res.push_back(num);
+        } else if (unique.find(num) == unique.end()) {
+            unique.erase(res.back());
+            res.pop_back();
+            unique.insert(num);
+            res.push_back(num);
         }
     }
-    
+
     return res;
 }
