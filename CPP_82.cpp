da6dbc90b8322d671a2b65fec6fdde1699fabd9a
beep boop <githubbot@vadim.me>
2024-08-01 19:45:30 +0200

    Solution 3 of CPP/82, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/98mfqnmu Prompt: Complete the following code given the task description and function signature.

--- CPP_82.cpp
+++ CPP_82.cpp
@@ -1,10 +1,11 @@
-bool prime_length(string str) {
+bool prime_length(string str){
     int len = str.length();
-    if (len <= 1)
-        return false;
-    for (int i = 2; i * i <= len; i++) {
-        if (len % i == 0)
-            return false;
+    bool isPrime = true;
+
+    for(int i=2; i*i<=len && isPrime; i++){
+        if(len%i==0)
+            isPrime=false;
     }
-    return true;
+
+    return isPrime;
 }
