e5f7b209444c436b19523a33e767dda8cce0a3a9
beep boop <githubbot@vadim.me>
2024-08-01 18:55:56 +0200

    Solution 21 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/hx9fx2ps Prompt: on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a3dd6035-843a-44af-9db0-a63a5d90a3da.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ a3dd6035-843a-44af-9db0-a63a5d90a3da.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~ In file included from a3dd6035-843a-44af-9db0-a63a5d90a3da.cpp:22: a3dd6035-843a-44af-9db0-a63a5d90a3da.cpp: In function ‘int main()’: a3dd6035-843a-44af-9db0-a63a5d90a3da.cpp:32:26: error: cannot convert ‘std::vect or<int>’ to ‘int’    32 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ~~~~~~^~~~~~~~~~~~~~~~~~       |                          |       |                          std::vector<int> a3dd6035-843a-44af-9db0-a63a5d90a3da.cpp:6:17: note:   initializing argument 1 o f ‘bool issame(int, int)’     6 | bool issame(int a, int b) {       |             ~~~~^

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -17,4 +17,4 @@ std::vector<int> common(const std::vector<int>& l1, const std::vector<int>& l2) 
 int main() {
     assert(issame(static_cast<int>(common({4, 3, 2, 8},{}).size()), 0));
     return 0;
-}
+}  
