1294befe776a505bf919e9988fcd4c30bd5ed3f9
beep boop <githubbot@vadim.me>
2024-08-01 18:11:52 +0200

    Solution 2 of CPP/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/gt83sj1e Prompt: Complete the following code given the task description and function signature.

--- CPP_73.cpp
+++ CPP_73.cpp
@@ -1,20 +1,27 @@
 int smallest_change(vector<int> arr) {
     int n = arr.size();
-    vector<vector<bool>> dp(n, vector<bool>(n));
-    
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            if (i == j)
-                dp[i][j] = true;
-            else
-                dp[i][j] = arr[i] == arr[n - 1 - i];
-        }
+    string str = "";
+    for (int i : arr) {
+        str += to_string(i);
     }
     
+    int left = 0;
+    int right = n - 1;
     int changes = 0;
-    for (int i = 0; i < n; i++) {
-        if (!dp[i][n - 1 - i])
-            changes++;
+    
+    while (left < right) {
+        if (str[left] != str[right]) {
+            if (str[left] == str[right-1]) {
+                right--;
+            } else if (str[right] == str[left+1]) {
+                left++;
+            } else {
+                changes += 2;
+                break;
+            }
+        }
+        left++;
+        right--;
     }
     
     return changes;
