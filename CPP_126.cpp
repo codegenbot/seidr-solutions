4a1841a9779af4edeb7af96b91d912e23523adb5
beep boop <githubbot@vadim.me>
2024-08-02 00:39:19 +0200

    Solution 40 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nh1fhueo Prompt: 1c29a025-9ef3-4a07-a115-aaa9b2f53c13.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 1c29a025-9ef3-4a07-a115-aaa9b2f53c13.cpp:13: 1c29a025-9ef3-4a07-a115-aaa9b2f53c13.cpp: In function ‘int main()’: 1c29a025-9ef3-4a07-a115-aaa9b2f53c13.cpp:15:13: error: ‘is_sorted’ was not decla red in this scope    15 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,17 +1,9 @@
-#include <vector>
-#include <algorithm>
-
-bool is_sorted(std::vector<int> lst) {
+bool is_sorted(vector<int> lst){
     for(int i = 0; i < lst.size() - 1; i++){
         if(lst[i] >= lst[i+1]){
-            if(std::count(lst.begin(), lst.end(), lst[i]) > 1)
+            if(count(lst.begin(), lst.end(), lst[i]) > 1)
                 return false;
         }
     }
     return true;
-}
-
-int main() {
-    assert (is_sorted({1, 2, 3, 4}) == true);
-    return 0;
 }
