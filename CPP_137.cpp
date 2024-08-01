c9992541a984b001d6dadbfa1706e2ccf58dd1bf
beep boop <githubbot@vadim.me>
2024-08-02 01:52:39 +0200

    Solution 92 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/l6hmhmtf Prompt: e59fcd14-5260-480d-8c55-6948b04c0d6c.cpp:3:1: error: stray ‘`’ in program     3 | ```cpp       | ^ e59fcd14-5260-480d-8c55-6948b04c0d6c.cpp:3:2: error: stray ‘`’ in program     3 | ```cpp       |  ^ e59fcd14-5260-480d-8c55-6948b04c0d6c.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ e59fcd14-5260-480d-8c55-6948b04c0d6c.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -9,58 +9,58 @@ boost::any compare_one(boost::any a, boost::any b) {
     else if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        if (stod(b.to_string()) > a.convert_to<int>()) {
+    else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
+        if (std::stod(b.convert_to<std::string>()) > a.convert_to<int>()) {
             return b;
         } else {
-            return boost::any((int)0);
+            return boost::any(boost::any_cast<int>(b));
         }
     }
     else if (a.type() == typeid(float) && b.type() == typeid(double)) {
         return boost::any_cast<double>(b);
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        if (stod(a.convert_to<string>()) > b.convert_to<int>()) {
+    else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
+        if (std::stod(a.convert_to<std::string>()) > b.convert_to<int>()) {
             return a;
         } else {
-            return boost::any((int)0);
+            return boost::any(boost::any_cast<int>(b));
         }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        if (stod(a.convert_to<string>()) > boost::any_cast<float>(b)) {
+    else if (a.type() == typeid(std::string) && b.type() == typeid(float)) {
+        if (std::stod(a.convert_to<std::string>()) > boost::any_cast<float>(b)) {
             return a;
         } else {
-            return boost::any((int)0);
+            return boost::any(boost::any_cast<float>(b));
         }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        if (stod(a.convert_to<string>()) > boost::any_cast<double>(b)) {
+    else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
+        if (std::stod(a.convert_to<std::string>()) > boost::any_cast<double>(b)) {
             return a;
         } else {
-            return boost::any((int)0);
+            return boost::any(boost::any_cast<double>(b));
         }
     }
-    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        if (boost::any_cast<float>(a) > stod(b.convert_to<string>())) {
+    else if (a.type() == typeid(float) && b.type() == typeid(std::string)) {
+        if (boost::any_cast<float>(a) > std::stod(b.convert_to<std::string>())) {
             return a;
         } else {
-            return boost::any((int)0);
+            return boost::any(boost::any_cast<int>(b));
         }
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        if (boost::any_cast<double>(a) > stod(b.convert_to<string>())) {
+    else if (a.type() == typeid(double) && b.type() == typeid(std::string)) {
+        if (boost::any_cast<double>(a) > std::stod(b.convert_to<std::string>())) {
             return a;
         } else {
-            return boost::any((int)0);
+            return boost::any(boost::any_cast<int>(b));
         }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        if (stod(a.convert_to<string>()) > stod(b.convert_to<string>())) {
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        if (std::stod(a.convert_to<std::string>()) > std::stod(b.convert_to<std::string>())) {
             return a;
-        } else if (stod(a.convert_to<string>()) < stod(b.convert_to<string>())) {
+        } else if (std::stod(a.convert_to<std::string>()) < std::stod(b.convert_to<std::string>()))) {
             return b;
         } else {
-            return boost::any((int)0);
+            return boost::any("None");
         }
     }
     else {
@@ -69,7 +69,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         } else if (boost::any_cast<float>(a) < boost::any_cast<float>(b)) {
             return b;
         } else {
-            return boost::any((int)0);
+            return boost::any("None");
         }
     }
 }
