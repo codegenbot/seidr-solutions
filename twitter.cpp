913a31fa69ba52b5908d31a4e40aa5070cc4c588
beep boop <githubbot@vadim.me>
2024-08-01 23:12:26 +0200

    Solution 13 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/yg7gjjaz Prompt: Given a string representing a tweet, validate whether the tweet meets Twitter’s original character requirements. If the tweet has more than 140 characters, return the string "Too many characters". If the tweet is empty, return the string "You didn't type anything". Otherwise, return "Your tweet has X characters", where the X is the number of characters in the tweet.

--- twitter.cpp
+++ twitter.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <string>
 
-std::string tweetValidator(std::string tweet) {
+std::string validateTweet(const std::string& tweet) {
     if (tweet.empty()) {
         return "You didn't type anything";
     }
-    
     if (tweet.length() > 140) {
         return "Too many characters";
     }
-    
     return "Your tweet has " + std::to_string(tweet.length()) + " characters";
 }
 
 int main() {
-    // Test cases
-    std::cout << tweetValidator("") << std::endl;
-    std::cout << tweetValidator("1") << std::endl;
-    std::cout << tweetValidator("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it") << std::endl;
-    std::cout << tweetValidator("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)") << std::endl;
-    std::cout << tweetValidator("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1") << std::endl;
-
+    std::string input;
+    while (true) {
+        std::cout << "Enter a tweet (or 'q' to quit): ";
+        std::cin >> input;
+        if (input == "q") {
+            break;
+        }
+        std::cout << validateTweet(input) << "\n";
+    }
     return 0;
 }
