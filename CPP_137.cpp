6c727a47681b82eee203601bd79bb9174e52bd1d
beep boop <githubbot@vadim.me>
2024-08-02 01:39:46 +0200

    Solution 37 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/l6hmhmtf Prompt: f29f0d17-cfc8-4add-9b20-3aad7dedd936.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,21 +3,20 @@ using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return boost::any((int)std::max((double)a.convert_to<int>(), b.convert_to<double>()));
+        return (int)std::max((double)a.convert_to<int>(), b.convert_to<double>());
     }
     else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return std::max(a, b);
+        return std::any_cast<double>(b);
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        return boost::any((std::string)std::max(a.convert_to<std::string>(), b.convert_to<std::string>()));
+        return a > b ? a : b;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        int x = (int)a.convert_to<int>();
-        std::string y = b.convert_to<std::string>();
-        if (x > std::stoi(y)) {
+        int x = std::stoi(b.convert_to<std::string>());
+        if ((int)a.convert_to<int>() > x) {
             return a;
         }
-        else if (x < std::stoi(y)) {
+        else if ((int)a.convert_to<int>() < x) {
             return b;
         }
         else {
@@ -25,16 +24,15 @@ boost::any compare_one(boost::any a, boost::any b) {
         }
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
-        std::string x = a.convert_to<std::string>();
-        int y = (int)b.convert_to<int>();
-        if (std::stoi(x) > y) {
+        int x = (int)b.convert_to<int>();
+        if (std::stoi(a.convert_to<std::string>()) > x) {
             return a;
         }
-        else if (std::stoi(x) < y) {
-            return b;
+        else if (std::stoi(a.convert_to<std::string>()) < x) {
+            return boost::any("None");
         }
         else {
-            return boost::any("None");
+            return b;
         }
     }
     else {
