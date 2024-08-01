97deb7e07c257929b6eaf9b4e40885f16d76ef44
beep boop <githubbot@vadim.me>
2024-08-01 13:46:26 +0200

    Solution 29 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/guwo9mq2 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d1d84329-0460-4c42-bd13-399637e1f796.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d1d84329-0460-4c42-bd13-399637e1f796.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d1d84329-0460-4c42-bd13-399637e1f796.cpp:11: d1d84329-0460-4c42-bd13-399637e1f796.cpp: In function ‘int main()’: d1d84329-0460-4c42-bd13-399637e1f796.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ d1d84329-0460-4c42-bd13-399637e1f796.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,12 +1,18 @@
 #include <vector>
 #include <boost/any.hpp>
 
+bool issame(vector<int> a,vector<int>b){
+    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
+}
+
 vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
-        if(boost::any_cast<optional<int>>(value).has_value()) {
-            int integer = boost::any_cast<optional<int>>(value).value();
-            result.push_back(integer);
+        if (boost::any_cast<int>(value).which() == boost::any::typeid(int)) {
+            int casted = boost::any_cast<int>(value);
+            if (casted.good()) {
+                result.push_back(casted);
+            }
         }
     }
     return result;
