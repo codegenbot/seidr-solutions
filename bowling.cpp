d232cc89bcec5549019430b6d141a4073a1ca592
beep boop <githubbot@vadim.me>
2024-08-01 18:41:06 +0200

    Solution 54 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/fjsag2tn Prompt: 326af16b-87b6-4fc9-848f-8142da87cd07.cpp:2:1: error: ‘i’ does not name a type     2 | i += 2;       | ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,21 +1,2 @@
-int score = 0;
-std::string input;
-std::cin >> input;
-
-for (int i = 0; i < input.size(); ++i) {
-    if (input[i] == 'X') {
-        score += 10;
-        if (i < input.size() - 3) {
-            if (input[i + 2] == 'X')
-                score += 10 + ((input[i + 4] == 'X') ? 10 : (input[i + 4] - '0'));
-            else
-                score += ((input[i + 2] == '/') ? 10 : (input[i + 2] - '0') + (input[i + 4] == '/' ? 10 : (input[i + 4] - '0')));
-        }
-    } else if (input[i] == '/') {
-        score += 10 - (input[i - 1] - '0');
-        score += (i < input.size() - 1) ? (input[i + 1] == 'X' ? 10 : (input[i + 1] - '0')) : 0;
-    } else {
-        score += (input[i] == '-' ? 0 : (input[i] - '0'));
-    }
-}
-std::cout << score;
+int i = 0;
+i += 2;
