354e2b2ee9ac46e5658ddcec175ce37a097541f8
beep boop <githubbot@vadim.me>
2024-08-02 00:07:04 +0200

    Solution 83 of CPP/120, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cbcj0kw7 Prompt:       |                    ^~~ 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp:19: 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp: In function ‘int main()’: 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp:29:20: error: ‘maximum’ was not declare d in this scope    29 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 9fb03995-b1d0-451d-b8e8-3f9ae0ff3587.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
+
+using namespace std;
 
 bool issame(int a, int b) {
     return a == b;
 }
 
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::priority_queue<int> pq;
+vector<int> maximum(vector<int> arr, int k) {
+    priority_queue<int> pq;
     for (int i : arr) {
         pq.push(i);
         if (pq.size() > k) {
             pq.pop();
         }
     }
-    std::vector<int> res(k);
+    vector<int> res(k);
     for (int i = k - 1; i >= 0; --i) {
         res[i] = pq.top();
         pq.pop();
