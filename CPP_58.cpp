75f3307d58db7db6b2a2910e83741bab9b58df41
beep boop <githubbot@vadim.me>
2024-08-01 23:24:05 +0200

    Solution 4 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/j4jvgpo2 Prompt:       |                    ^~~ eaa673f3-883f-4a10-b5be-53a5601c1794.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ eaa673f3-883f-4a10-b5be-53a5601c1794.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ eaa673f3-883f-4a10-b5be-53a5601c1794.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from eaa673f3-883f-4a10-b5be-53a5601c1794.cpp:9: eaa673f3-883f-4a10-b5be-53a5601c1794.cpp: In function ‘int main()’: eaa673f3-883f-4a10-b5be-53a5601c1794.cpp:19:20: error: ‘common’ was not declared  in this scope    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ eaa673f3-883f-4a10-b5be-53a5601c1794.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,9 +1,11 @@
-#include <algorithm>
 #include <vector>
-#include <set>
-#include <cassert>
+#include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return includes(a.begin(), a.end(), b.begin(), b.end());
+}
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     sort(l1.begin(), l1.end());
@@ -12,9 +14,4 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
     result.erase(unique(result.begin(), result.end()), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
-    return 0;
 }
