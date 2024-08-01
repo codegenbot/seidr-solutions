887eb8b15449760193f175e6ca3407a79aa5fdf2
beep boop <githubbot@vadim.me>
2024-08-01 18:44:10 +0200

    Solution 95 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ncsz9ma9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d54730ad-9c43-4725-895b-be8f5957e d0a.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d54730ad-9c43-4725-895b-be8f5957e d0a.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,8 +1,12 @@
+```
 def sort_third(l: list):
     result = []
-    for i in range(0, len(l), 3):
-        if l[i] % 3 != 0:
-            result.append(sorted([x for x in l[: i + 1] if x % 3 != 0])[::-1])
+    for i in range(len(l)):
+        if i % 3 == 0:
+            if l[i] % 3 != 0:
+                result.append(sorted([x for x in l[:i+1] if x % 3 != 0])[::-1])
+            else:
+                result.append([l[i]])
         else:
             result.append([l[i]])
     return result
