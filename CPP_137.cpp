9e86df572bc9f916a3f45adf9908966b82917b35
beep boop <githubbot@vadim.me>
2024-08-02 00:05:35 +0200

    Solution 99 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rvo61gzr Prompt: adb390b5-cccc-48d2-928d-e648964cc554.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,5 +1,5 @@
-#include <boost/any.hpp>
 #include <string>
+#include < boost/any.hpp>
 
 using namespace boost;
 
@@ -12,27 +12,27 @@ boost::any compare_one(boost::any a, boost::any b) {
         if (std::stod(b.convert_to<std::string>().c_str()) > std::stod(a.convert_to<std::string>().c_str())) {
             return b;
         } else {
-            return boost::any("None");
+            return a;
         }
     } else if (a.type() == typeid(double) && b.type() == typeid(std::string)) {
         if (std::stod(b.convert_to<std::string>().c_str()) > std::stod(a.convert_to<std::string>().c_str())) {
             return b;
         } else {
-            return boost::any("None");
+            return a;
         }
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         if (std::stoi(b.convert_to<std::string>().erase(0, 1).erase(b.convert_to<std::string>().find(',') + 1).c_str()) > std::stoi(a.convert_to<std::string>().erase(0, 1).erase(a.convert_to<std::string>().find(',') + 1).c_str()) {
             return b;
         } else if (std::stoi(b.convert_to<std::string>().erase(0, 1).erase(b.convert_to<std::string>().find(',') + 1).c_str()) == std::stoi(a.convert_to<std::string>().erase(0, 1).erase(a.convert_to<std::string>().find(',') + 1).c_str())) {
-            return boost::any("None");
+            return a;
         } else {
-            return b;
+            return a;
         }
     } else if (a.type() == typeid(double) && a.convert_to<double>() > b.convert_to<double>()) {
         return a;
     } else if (b.type() == typeid(double) && b.convert_to<double>() > a.convert_to<double>()) {
         return b;
     } else {
-        return boost::any("None");
+        return a;
     }
 }
