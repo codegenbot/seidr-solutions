4a1ce2648e3b8ddff32fbf7a8534ea744c71dac7
beep boop <githubbot@vadim.me>
2024-08-01 18:14:31 +0200

    Solution 70 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pjjwa92u Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4e82c1c2-1c47-4be8-8786-fe94bdaa7 30f.py", line 1, in <module>     def concatenate(strings: List[str]) -> str:                              ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_28.py
+++ Python_28.py
@@ -3,6 +3,3 @@ from typing import List
 
 def concatenate(strings: List[str]) -> str:
     return ''.join(strings)
-
-strings = input("Enter strings separated by space: ").split()
-print(concatenate(strings))
