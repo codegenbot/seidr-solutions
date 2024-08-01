090cc9cc5ba7a268da610b9e0464b46b37a515c0
beep boop <githubbot@vadim.me>
2024-08-02 01:48:13 +0200

    Solution 74 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/l6hmhmtf Prompt: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:64: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp: In function ‘int main()’: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:13: error: ‘boost’ has not been decl ared    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:29: error: ‘string’ was not declared  in this scope    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:37: error: ‘compare_one’ was not dec lared in this scope    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,35 +3,39 @@
 #include <string>
 
 using boost::any;
+using namespace std;
 
 any compare_one(any a, any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)a.convert_to<int>() < b.convert_to<float>() ? &b : &a;
+        return (int)a < b ? &b : &a;
     }
     else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)a.convert_to<int>() < b.convert_to<double>() ? &b : &a;
+        return (int)a < b ? &b : &a;
     }
-    else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        int ai = stoi(b.convert_to<string>().c_str());
-        return ai > (int)a.convert_to<int>() ? &b : &a;
-    }
-    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return a.convert_to<float>() < b.convert_to<int>() ? &b : &a;
-    }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return a.convert_to<double>() > b.convert_to<int>() ? &b : &a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        double af = stod(a.convert_to<string>().c_str());
-        return af < b.convert_to<float>() ? &b : &a;
-    }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        double af = stod(a.convert_to<string>().c_str());
-        return af > b.convert_to<double>() ? &b : &a;
+    else if (a.type() == typeid(string) && (b.type() == typeid(float) || b.type() == typeid(double))) {
+        double val = stod(b.convert_to<string>().c_str());
+        if ((int)a < val) {
+            return a;
+        }
+       	else if ((int)a > val) {
+            return b;
+        }
+		else {
+            return any("None");
+        }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        double ai = stod(a.convert_to<string>().c_str()), bi = stod(b.convert_to<string>().c_str());
-        return ai < bi ? &b : &a;
+        double val1 = stod(a.convert_to<string>().c_str());
+        double val2 = stod(b.convert_to<string>().c_str());
+        if (val1 < val2) {
+            return a;
+        }
+		else if (val1 > val2) {
+            return b;
+        }
+		else {
+            return any("None");
+        }
     }
-    return any();
+	return any();
 }
