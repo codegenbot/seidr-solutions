b011f8172dde20fc8924330523eaabc249f1802e
beep boop <githubbot@vadim.me>
2024-08-01 21:44:03 +0200

    Solution 6 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xvkcn3c9 Prompt: Complete the following code given the task description and function signature.

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,21 +1,48 @@
 vector<string> by_length(vector<int> arr) {
-    vector<int> numbers;
-    for (int i : arr) {
-        if (i >= 1 && i <= 9) {
-            numbers.push_back(i);
+    vector<int> nums;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            nums.push_back(num);
         }
     }
-    
-    sort(numbers.begin(), numbers.end());
-    reverse(numbers.begin(), numbers.end());
+
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
 
     vector<string> result;
-    map<int, string> numMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-            {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    
-    for (int i : numbers) {
-        result.push_back(numMap[i]);
+    for (int num : nums) {
+        string str = "";
+        switch (num) {
+            case 1:
+                str = "One";
+                break;
+            case 2:
+                str = "Two";
+                break;
+            case 3:
+                str = "Three";
+                break;
+            case 4:
+                str = "Four";
+                break;
+            case 5:
+                str = "Five";
+                break;
+            case 6:
+                str = "Six";
+                break;
+            case 7:
+                str = "Seven";
+                break;
+            case 8:
+                str = "Eight";
+                break;
+            case 9:
+                str = "Nine";
+                break;
+        }
+        result.push_back(str);
     }
-    
+
     return result;
 }
