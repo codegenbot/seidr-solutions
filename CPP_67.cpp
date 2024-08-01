28977d8028aaa751c614200b66154bced6902d8e
beep boop <githubbot@vadim.me>
2024-08-01 17:36:40 +0200

    Solution 8 of CPP/67, ratio 0.42857142857142855 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vt4u5cry Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,32 +1,25 @@
-#include <stdio.h>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 int fruit_distribution(string s, int n) {
+    int total_apples = 0;
+    int total_oranges = 0;
+
     size_t pos = 0;
-    int apples = 0, oranges = 0;
-
-    while ((pos = s.find("apples", pos)) != string::npos) {
-        if (s.substr(pos, "apples".length()).find("and") == string::npos) {
-            apples = stoi(s.substr(0, pos).erase().substr(0, s.substr(0, pos).find(" ")).c_str());
-        }
-        else {
-            apples = stoi(s.substr(0, pos).erase().substr(0, s.substr(0, pos).find("and")).c_str());
-        }
-
-        pos += (s.find("apples", pos) == string::npos ? s.length() : s.find("apples", pos));
+    while ((pos = s.find(" apples")) != string::npos) {
+        int num_apples = stoi(s.substr(0, pos).erase(0, 5));
+        total_apples += num_apples;
+        s.erase(0, pos + 7);
     }
 
-    while ((pos = s.find("oranges", 0)) != string::npos) {
-        if (s.substr(pos, "oranges".length()).find("and") == string::npos) {
-            oranges = stoi(s.substr(0, pos).erase().substr(0, s.substr(0, pos).find(" ")).c_str());
-        }
-        else {
-            oranges = stoi(s.substr(0, pos).erase().substr(0, s.substr(0, pos).find("and")).c_str());
-        }
-
-        pos += (s.find("oranges", pos) == string::npos ? s.length() : s.find("oranges", pos));
+    pos = 0;
+    while ((pos = s.find(" oranges")) != string::npos) {
+        int num_oranges = stoi(s.substr(0, pos).erase(0, 8));
+        total_oranges += num_oranges;
+        s.erase(0, pos + 7);
     }
 
-    return n - apples - oranges;
+    return n - total_apples - total_oranges;
 }
