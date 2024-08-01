e95f201308a847f9b71678c9065a854eeae17b27
beep boop <githubbot@vadim.me>
2024-08-01 23:48:49 +0200

    Solution 19 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6oau1e9r Prompt:       |                             ^~~~~~ 0bb1744f-65cd-4e08-ab07-09fb2751e3c4.cpp:22:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0bb1744f-65cd-4e08-ab07-09fb2751e3c4.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0bb1744f-65cd-4e08-ab07-09fb2751e3c4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0bb1744f-65cd-4e08-ab07-09fb2751e3c4.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0bb1744f-65cd-4e08-ab07-09fb2751e3c4.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 0bb1744f-65cd-4e08-ab07-09fb2751e3c4.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -14,6 +14,6 @@ bool issame(const std::vector<float>& a, const std::vector<float>& b){
 }
 
 int main(){
-    assert( issame(derivative({1.0, 2.0, 4.0}), {1.0, 2.0}) );
+    assert(issame(derivative({1.0, 2.0, 4.0}), {1.0, 2.0}));
     return 0;
 }
