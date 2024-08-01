59515b4f8237341ade21ae54bed41964ee53cd1d
beep boop <githubbot@vadim.me>
2024-08-01 21:59:17 +0200

    Solution 34 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/njbxukso Prompt:       |                    ^~~ bc951c85-9c0c-4390-aa02-f18e0237b126.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bc951c85-9c0c-4390-aa02-f18e0237b126.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bc951c85-9c0c-4390-aa02-f18e0237b126.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bc951c85-9c0c-4390-aa02-f18e0237b126.cpp:19: bc951c85-9c0c-4390-aa02-f18e0237b126.cpp: In function ‘int main()’: bc951c85-9c0c-4390-aa02-f18e0237b126.cpp:29:20: error: ‘get_odd_collatz’ was not  declared in this scope    29 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ bc951c85-9c0c-4390-aa02-f18e0237b126.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,19 +1,8 @@
+#include <iostream>
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
+std::vector<int> get_odd_collatz(int n) {
+    std::vector<int> result;
     while (n != 1) {
         if (n % 2 == 0) {
             n /= 2;
@@ -24,18 +13,17 @@ vector<int> get_odd_collatz(int n) {
             result.push_back(n);
         }
     }
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     return result;
 }
 
 int main() {
     int num;
-    cout << "Enter a number: ";
-    cin >> num;
-    vector<int> res = get_odd_collatz(num);
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::vector<int> res = get_odd_collatz(num);
     for (int i : res) {
-        cout << i << " ";
+        std::cout << i << " ";
     }
-    cout << endl;
     return 0;
 }
