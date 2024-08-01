238f8da0cfab9d8aeb82257678a6fa20f7eea58d
beep boop <githubbot@vadim.me>
2024-08-01 18:47:47 +0200

    Solution 8 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/5p2n75m6 Prompt: be9a9024-a918-4d77-af2b-d12b704acea5.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for(char c : brackets){       |     ^~~ be9a9024-a918-4d77-af2b-d12b704acea5.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return count == 0;       |     ^~~~~~ be9a9024-a918-4d77-af2b-d12b704acea5.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from be9a9024-a918-4d77-af2b-d12b704acea5.cpp:15: be9a9024-a918-4d77-af2b-d12b704acea5.cpp: In function ‘int main()’: be9a9024-a918-4d77-af2b-d12b704acea5.cpp:17:18: error: ‘correct_bracketing’ was not declared in this scope    17 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,4 +1,7 @@
-bool correct_bracketing(const string& brackets) {
+#include <iostream>
+#include <assert.h>
+
+bool correct_bracketing(const std::string& brackets) {
     int count = 0;
     for(char c : brackets){
         if(c == '<'){
@@ -11,4 +14,9 @@ bool correct_bracketing(const string& brackets) {
         }
     }
     return count == 0;
+}
+
+int main() {
+    assert(!correct_bracketing("<><><<><>><>>><>"));
+    return 0;
 }
