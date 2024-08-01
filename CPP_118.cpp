56378821c489dd064f643cfec94463508c93e720
beep boop <githubbot@vadim.me>
2024-08-01 21:21:39 +0200

    Solution 46 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mk2hgjp8 Prompt: 34dfe17a-3480-4b2d-97b0-413850391d6d.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ 34dfe17a-3480-4b2d-97b0-413850391d6d.cpp: In function ‘bool isvowel(char)’: 34dfe17a-3480-4b2d-97b0-413850391d6d.cpp:11:9: error: ‘toupper’ was not declared  in this scope    11 |     c = toupper(c);       |         ^~~~~~~ In file included from 34dfe17a-3480-4b2d-97b0-413850391d6d.cpp:15: 34dfe17a-3480-4b2d-97b0-413850391d6d.cpp: In function ‘int main()’: 34dfe17a-3480-4b2d-97b0-413850391d6d.cpp:17:13: error: ‘get_closest_vowel’ was n ot declared in this scope    17 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -5,11 +5,12 @@ std::string get_closest_vowel(std::string word) {
     int n = word.size();
     for (int i = 0; i < n; ++i) {
         if (!isalpha(word[i])) continue;
-        if (isvowel(toupper(word[i]))) return to_string(word[std::find_if(word.begin() + i + 1, word.end(), isvowel)-word.begin() - 1]);
+        if (isvowel(toupper(word[i]))) return to_string(word[0]);
     }
     return "";
 }
 
 bool isvowel(char c) {
+    c = toupper(c);
     return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 }
