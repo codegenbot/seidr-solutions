80aa32f491082441de84da8a3eba579e79c8674e
beep boop <githubbot@vadim.me>
2024-08-01 20:33:17 +0200

    Solution 76 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yjaxga41 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/dfa003d9-99f3-4c60-a12f-c9c2cbbd3 07f.py", line 16     print(find_closest_elements(numbers))                                          ^ SyntaxError: invalid non-printable character U+2028

--- Python_20.py
+++ Python_20.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+
 def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
     numbers.sort()
     min_diff = float("inf")
@@ -11,6 +12,7 @@ def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
             result = (numbers[i], numbers[i + 1])
     return result
 
-numbers = list(map(float, input().split()))
-output = find_closest_elements(numbers)
-print(*output)
+
+n = input()
+numbers = list(map(float, n.split()))
+print(find_closest_elements(numbers))
