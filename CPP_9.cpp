f71390225ce8cb38bf19f098690f6624ed82e70b
beep boop <githubbot@vadim.me>
2024-08-01 13:15:06 +0200

    Solution 0 of CPP/9, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/v1217mib Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_9.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> rolling_max(vector<int> numbers) {
+    vector<int> result;
+    int max = numbers[0];
+    for (int i = 0; i < numbers.size(); i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+        result.push_back(max);
+    }
+    return result;
+}
