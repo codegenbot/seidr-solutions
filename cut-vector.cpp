167604c0a8c72a4627658c893d916733ebc077eb
beep boop <githubbot@vadim.me>
2024-08-01 18:40:48 +0200

    Solution 96 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/j82kg6th Prompt: ae4e83a3-99e7-45fc-9c8e-d66f1e031d08.cpp: In function ‘std::vector<int> cutVecto r(std::vector<int>)’: ae4e83a3-99e7-45fc-9c8e-d66f1e031d08.cpp:5:19: error: ‘INT_MAX’ was not declared  in this scope     5 |     int minDiff = INT_MAX;       |                   ^~~~~~~ ae4e83a3-99e7-45fc-9c8e-d66f1e031d08.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <vector>   +++ |+#include <climits>     2 | using namespace std; ae4e83a3-99e7-45fc-9c8e-d66f1e031d08.cpp:9:20: error: ‘abs’ was not declared in this scope     9 |         int diff = abs(v[i-1] - v[i]);       |                    ^~~ ae4e83a3-99e7-45fc-9c8e-d66f1e031d08.cpp:18:24: error: could not convert ‘{left,  right}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<int>’    18 |     return {left, right};       |                        ^       |                        |       |                        <brace-enclosed initializer list>

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -17,4 +17,4 @@ vector<int> cutVector(vector<int> v) {
     
     vector<int> left(v.begin(), v.begin() + cutIndex);
     vector<int> right(v.begin() + cutIndex, v.end());
-    return {vector<int>{left.begin(), left.end()}, vector<int>{right.begin(), right.end()}};
+    return vector<int>(left.begin(), (left.end() + right.size()) - 1);
