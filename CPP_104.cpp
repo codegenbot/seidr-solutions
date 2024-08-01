d1b07e78a29c67fd20827a2b1e96b41dc6a832bf
beep boop <githubbot@vadim.me>
2024-08-01 23:02:45 +0200

    Solution 2 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sb52l8o7 Prompt:       |                    ^~~ bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp:20: bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp: In function ‘int main()’: bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ bfaed7f9-44eb-4c3c-b92f-765d565e0790.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,7 +1,15 @@
-vector<int> result;
+#include <vector>
+#include <algorithm>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> unique_digits(std::vector<int> x) {
+    std::vector<int> result;
     for (int num : x) {
-        int temp = num;
         bool hasEvenDigit = false;
+        int temp = num;
         while (temp > 0) {
             if (temp % 2 == 0) {
                 hasEvenDigit = true;
@@ -13,6 +21,6 @@ vector<int> result;
             result.push_back(num);
         }
     }
-    sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end());
     return result;
 }
