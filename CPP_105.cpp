c678708976edbdb0d574aa9fa2024734b18588aa
beep boop <githubbot@vadim.me>
2024-08-01 23:36:36 +0200

    Solution 81 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jf4bwywo Prompt:       |                                 ^~~~~~ In file included from 04323398-dc23-4b3b-a179-916ccb5ca201.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 04323398-dc23-4b3b-a179-916ccb5ca201.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 04323398-dc23-4b3b-a179-916ccb5ca201.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 04323398-dc23-4b3b-a179-916ccb5ca201.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 04323398-dc23-4b3b-a179-916ccb5ca201.cpp:14: 04323398-dc23-4b3b-a179-916ccb5ca201.cpp: In function ‘int main()’: 04323398-dc23-4b3b-a179-916ccb5ca201.cpp:24:20: error: ‘by_length’ was not decla red in this scope    24 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -4,8 +4,8 @@
 
 bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     std::vector<std::string> sorted_a = a;
-    std::vector<std::string> sorted_b = b;
     std::sort(sorted_a.begin(), sorted_a.end());
+    std::vector<std::string> sorted_b = b;
     std::sort(sorted_b.begin(), sorted_b.end());
     return sorted_a == sorted_b;
 }
