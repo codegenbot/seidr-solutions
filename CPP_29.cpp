ab8be4993b7a1155d9198a5b69a0998e2627ce23
beep boop <githubbot@vadim.me>
2024-08-01 21:27:18 +0200

    Solution 47 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qvlwmlb6 Prompt: /usr/include/assert.h:92: note: macro "assert" defined here    92 | #  define assert(expr) \       | 23364765-ebbf-4b4c-b961-03b0b3ffde2e.cpp: In function ‘int main()’: 23364765-ebbf-4b4c-b961-03b0b3ffde2e.cpp:21:5: error: ‘assert’ was not declared in this scope    21 |     assert(filtered == vector<string>{"xxx", "xxxAAA", "xxx"});       |     ^~~~~~ 23364765-ebbf-4b4c-b961-03b0b3ffde2e.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cassert>   +++ |+#include <cassert>     5 | 23364765-ebbf-4b4c-b961-03b0b3ffde2e.cpp: At global scope: 23364765-ebbf-4b4c-b961-03b0b3ffde2e.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 23364765-ebbf-4b4c-b961-03b0b3ffde2e.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -18,7 +18,6 @@ vector<string> filter_by_prefix(vector<string> strings, string prefix) {
 int main() {
     // Test the function
     vector<string> filtered = filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx");
-    // assert(filtered == vector<string>{"xxx", "xxxAAA", "xxx"});
     
     return 0;
 }
