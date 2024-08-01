a7685dc690779d7dc574d21b8ae1c85e2d264885
beep boop <githubbot@vadim.me>
2024-08-01 14:36:03 +0200

    Solution 20 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qmabel6g Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f0c9fc42-aa8e-4bc3-835d-47295b85532d.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f0c9fc42-aa8e-4bc3-835d-47295b85532d.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f0c9fc42-aa8e-4bc3-835d-47295b85532d.cpp:11: f0c9fc42-aa8e-4bc3-835d-47295b85532d.cpp: In function ‘int main()’: f0c9fc42-aa8e-4bc3-835d-47295b85532d.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ f0c9fc42-aa8e-4bc3-835d-47295b85532d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,30 +1,17 @@
-```cpp
 #include <boost/any.hpp>
 #include <vector>
 #include <list>
 
-bool issame(vector<int> a,vector<int>b) {
-    if(a.size()!=b.size()) {
-        return false;
-    }
-    for(int i=0;i<a.size();i++) {
-        if(a[i]!=b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(vector<int> a,vector<int> b){
+    return a==b;
 }
 
 vector<int> filter_integers(list<boost::any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if(boost::any_cast<int>(value).good()) {
+        if (boost::any_cast<int>(value).good()) {
             result.push_back(boost::any_cast<int>(value));
         }
     }
     return result;
-}
-
-int main() {
-    // your code here
 }
