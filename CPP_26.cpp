3b22cc60ace9d397bd625d5400a210e0aae5dc7d
beep boop <githubbot@vadim.me>
2024-08-01 21:07:32 +0200

    Solution 94 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/lim3q57i Prompt:       |             ^~~~~~ 69ed1b6e-daee-4da7-bf55-417c6e237898.cpp:14:20: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 69ed1b6e-daee-4da7-bf55-417c6e237898.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 69ed1b6e-daee-4da7-bf55-417c6e237898.cpp:14:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 69ed1b6e-daee-4da7-bf55-417c6e237898.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 69ed1b6e-daee-4da7-bf55-417c6e237898.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 69ed1b6e-daee-4da7-bf55-417c6e237898.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 
 std::vector<int> remove_duplicates(std::vector<int> numbers) {
-    std::sort(numbers.begin(), numbers.end());
     numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
     return numbers;
 }
