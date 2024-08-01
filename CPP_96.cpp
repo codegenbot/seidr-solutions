ce12c0f8300bfe36103feb7dd197bdd839efa47c
beep boop <githubbot@vadim.me>
2024-08-01 22:34:08 +0200

    Solution 69 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ekj9m2sb Prompt:       |                           ^~~~~~ 1298acc3-6331-4a7b-ad0e-ea2de6c0ebd1.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1298acc3-6331-4a7b-ad0e-ea2de6c0ebd1.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1298acc3-6331-4a7b-ad0e-ea2de6c0ebd1.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1298acc3-6331-4a7b-ad0e-ea2de6c0ebd1.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1298acc3-6331-4a7b-ad0e-ea2de6c0ebd1.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 1298acc3-6331-4a7b-ad0e-ea2de6c0ebd1.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -3,9 +3,6 @@
 #include <cassert>
 #include <algorithm>
 
-
-bool issame(std::vector<int> a, std::vector<int> b);
-
 std::vector<int> count_up_to(int n);
 
 bool issame(std::vector<int> a, std::vector<int> b){
@@ -29,6 +26,9 @@ std::vector<int> count_up_to(int n){
     return primes;
 }
 
-assert(issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-                                    31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-                                    73, 79, 83, 89, 97}));
+int main() {
+    assert(issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                                       31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                                       73, 79, 83, 89, 97}));
+    return 0;
+}
