ae29ab0756529144d9c461adb770f043758879bf
beep boop <githubbot@vadim.me>
2024-08-01 23:39:17 +0200

    Solution 49 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/51udyg87 Prompt: it must return True for input f|f&t|f&f|t&t&f&f&f&f&f|t&t|t&t|t&t|f, but it returns True False terminate called after throwing an instance of 'std::out_of_range'   what():  basic_string::substr: __pos (which is 4) > this->size() (which is 3).

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -14,17 +14,28 @@ bool solveBoolean(string s) {
         j++;
     }
     string left = s.substr(0, j);
-
-    if (j < s.length() && s[j] == '&') {
-        j++;
-        while(j<s.length()) {
-            if(s[j] == '|') break;
-            j++;
-        }
-        return solveBoolean(left) && solveBoolean(s.substr(j));
+    
+    if(i+1 < j) {
+        return solveBoolean(left) || solveBoolean(s.substr(i+1));
     } else {
         return solveBoolean(left);
     }
+
+    j++;
+    while(j<s.length()) {
+        if(s[j] == '&') break;
+        j++;
+    }
+    string right = s.substr(j);
+
+    i = j;
+    while(i<s.length()) {
+        if(s[i] == '|') break;
+        i++;
+    }
+    string middle = s.substr(j, i-j);
+
+    return solveBoolean(left) && solveBoolean(middle);
 }
 
 int main() {
