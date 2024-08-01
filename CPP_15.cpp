ef5b9001ed9fcf4ce4a525562e6bb86806061cc7
beep boop <githubbot@vadim.me>
2024-08-01 13:36:49 +0200

    Solution 8 of CPP/15, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/v87ea20m Prompt: Complete the following code given the task description and function signature.

--- CPP_15.cpp
+++ CPP_15.cpp
@@ -1,6 +1,9 @@
+#include <string>
+using namespace std;
+
 string string_sequence(int n) {
     ostringstream oss;
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i <= n; ++i) {
         oss << i << " ";
     }
     return oss.str();
