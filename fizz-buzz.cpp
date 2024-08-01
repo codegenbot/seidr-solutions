5b496185d59f6c91e85d3608871f39405c7f7fea
beep boop <githubbot@vadim.me>
2024-08-01 21:22:27 +0200

    Solution 2 of fizz-buzz, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/nyu9vlq2 Prompt: this scope     8 |         cout << "Fizz" << endl;       |         ^~~~ 3fdf9d18-cefc-4a70-9d4e-d76ff4ccc863.cpp:8:27: error: ‘endl’ was not declared in  this scope     8 |         cout << "Fizz" << endl;       |                           ^~~~ 3fdf9d18-cefc-4a70-9d4e-d76ff4ccc863.cpp:10:9: error: ‘cout’ was not declared in  this scope    10 |         cout << "Buzz" << endl;       |         ^~~~ 3fdf9d18-cefc-4a70-9d4e-d76ff4ccc863.cpp:10:27: error: ‘endl’ was not declared i n this scope    10 |         cout << "Buzz" << endl;       |                           ^~~~ 3fdf9d18-cefc-4a70-9d4e-d76ff4ccc863.cpp:12:9: error: ‘cout’ was not declared in  this scope    12 |         cout << x << endl;       |         ^~~~ 3fdf9d18-cefc-4a70-9d4e-d76ff4ccc863.cpp:12:22: error: ‘endl’ was not declared i n this scope    12 |         cout << x << endl;       |                      ^~~~

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,14 +1,19 @@
+#include <iostream>
+using namespace std;
+
 int main() {
     int x;
     cin >> x;
+    
     if (x % 3 == 0 && x % 5 == 0) {
-        cout << "FizzBuzz";
+        cout << "FizzBuzz" << endl;
     } else if (x % 3 == 0) {
-        cout << "Fizz";
+        cout << "Fizz" << endl;
     } else if (x % 5 == 0) {
-        cout << "Buzz";
+        cout << "Buzz" << endl;
     } else {
-        cout << x;
+        cout << x << endl;
     }
+    
     return 0;
 }
