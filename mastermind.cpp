92bef097f8ae31023561734ebcb0e5fef783ad1d
beep boop <githubbot@vadim.me>
2024-08-01 18:04:27 +0200

    Solution 5 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run2/runs/0akm8knh Prompt:    13 |             guessFreq[guess[i] - 'A']++;       |             ^~~~~~~~~ be4d54f7-2d65-4f10-96b3-c44c7ef09ec6.cpp:18:26: error: ‘codeFreq’ was not declar ed in this scope    18 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                          ^~~~~~~~ be4d54f7-2d65-4f10-96b3-c44c7ef09ec6.cpp:18:39: error: ‘guessFreq’ was not decla red in this scope    18 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                                       ^~~~~~~~~ be4d54f7-2d65-4f10-96b3-c44c7ef09ec6.cpp:18:22: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    18 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                      ^~~       |                      main be4d54f7-2d65-4f10-96b3-c44c7ef09ec6.cpp:21:5: error: ‘cout’ was not declared in  this scope    21 |     cout << whitePegs << endl;       |     ^~~~ be4d54f7-2d65-4f10-96b3-c44c7ef09ec6.cpp:21:26: error: ‘endl’ was not declared i n this scope    21 |     cout << whitePegs << endl;       |                          ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -6,9 +6,9 @@ std::vector<int> codeFreq(6, 0), guessFreq(6, 0);
 int main() {
     std::string code, guess;
     std::cin >> code >> guess;
-    
+
     int whitePegs = 0, blackPegs = 0;
-    
+
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             blackPegs++;
@@ -17,13 +17,13 @@ int main() {
             guessFreq[guess[i] - 'A']++;
         }
     }
-    
+
     for (int i = 0; i < 6; ++i) {
         whitePegs += std::min(codeFreq[i], guessFreq[i]);
     }
-    
+
     std::cout << whitePegs << std::endl;
     std::cout << blackPegs << std::endl;
-    
+
     return 0;
 }
