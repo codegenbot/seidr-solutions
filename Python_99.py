641baa3990ece597054dba0818c0b0e02873c3ea
beep boop <githubbot@vadim.me>
2024-08-02 01:21:24 +0200

    Solution 2 of Python/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/s693awz8 Prompt: Complete the following code given the task description and function signature.

--- Python_99.py
+++ Python_99.py
@@ -1,15 +1,8 @@
-```
 def closest_integer(value):
     value = float(value)
     if value == int(value):
         return int(value)
-    elif value > 0:
-        if (int(value) - value) < (value - int(value) + 1):
-            return int(value)
-        else:
-            return int(value) + 1
+    elif (value % 1) > 0.5:
+        return int(value) + 1
     else:
-        if (value - int(value)) < (int(value) - value + 1):
-            return int(value)
-        else:
-            return int(value) - 1
+        return int(value) - 1
