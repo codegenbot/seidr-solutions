7b299323a70a51172f8bfa6848626c34b7c8bb88
beep boop <githubbot@vadim.me>
2024-08-01 17:59:37 +0200

    Solution 30 of Python/28, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ad9jve8m Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d9fbb535-da51-48fc-9027-868621dd2 670.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_28.py
+++ Python_28.py
@@ -1,9 +1,9 @@
 ```
 from typing import List
 
-
 def concatenate(strings: List[str]) -> str:
-    if not strings:
-        return "No strings provided."
-    else:
-        return "".join(strings)
+    user_input = input("Enter multiple strings separated by spaces: ")
+    user_strings = user_input.split()
+    return ''.join(user_strings)
+
+print(concatenate([""]))
