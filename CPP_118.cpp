58483748420d7a560fa5a7d9a9f8325c34283298
beep boop <githubbot@vadim.me>
2024-08-01 21:43:32 +0200

    Solution 87 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mk2hgjp8 Prompt: 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string get_closest_vowel(string word) {       | ^~~~~~ 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp: In function ‘bool isvowel(char)’: 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp:19:5: error: ‘string’ was not declared in this scope    19 |     string vowels = "aeiouAEIOU";       |     ^~~~~~ 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp:20:17: error: ‘vowels’ was not declared  in this scope    20 |     return find(vowels.begin(), vowels.end(), c) != vowels.end();       |                 ^~~~~~ 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp:20:12: error: ‘find’ was not declared i n this scope    20 |     return find(vowels.begin(), vowels.end(), c) != vowels.end();       |            ^~~~ In file included from 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp:23: 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp: In function ‘int main()’: 776ad5b4-a0f7-4ec5-9c6f-8561606322e2.cpp:25:13: error: ‘get_closest_vowel’ was n ot declared in this scope    25 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -11,7 +11,7 @@ string get_closest_vowel(string word) {
         }
     }
     for (int i = left; i < word.size(); ++i) {
-        if (isvowel(word[i])) {
+        if (find(vowels.begin(), vowels.end(), tolower(word[i])) != vowels.end()) {
             return string(1, word[i]);
         }
     }
