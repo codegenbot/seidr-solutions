34cbd5fe44092bf5f2daa45d70aaf48d6a3d8256
beep boop <githubbot@vadim.me>
2024-08-01 17:54:48 +0200

    Solution 3 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ncu24k6d Prompt: Complete the following code given the task description and function signature.

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,18 +1,9 @@
 int search(vector<int> lst) {
-    unordered_map<int, int> freq;
+    int max = 0;
     for (int i : lst) {
-        if (!freq.count(i)) {
-            freq[i] = 1;
-        } else {
-            freq[i]++;
+        if (i > 0 && i >= count(i)) {
+            max = i;
         }
     }
-    int result = -1;
-    for (auto p : freq) {
-        if (p.second >= p.first && p.first > 0) {
-            result = p.first;
-            break;
-        }
-    }
-    return result;
+    return max == 0 ? -1 : max;
 }
