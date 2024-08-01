cfaa5a8156f1cd1debe9d9fbcd3b26340332a00a
beep boop <githubbot@vadim.me>
2024-08-01 13:05:40 +0200

    Solution 10 of CPP/3, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/uzb8xvbw Prompt: ce4abf89-9d72-469a-b698-32b1d22f41df.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ ce4abf89-9d72-469a-b698-32b1d22f41df.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_3.cpp
+++ CPP_3.cpp
@@ -1,5 +1,5 @@
-#include <cassert>
 #include <vector>
+#include <cassert>
 
 bool below_zero(const std::vector<int>& operations) {
     int balance = 0;
@@ -10,9 +10,4 @@ bool below_zero(const std::vector<int>& operations) {
         }
     }
     return false;
-}
-
-int main() {
-    assert (below_zero({1, -2, 2, -2, 5, -5, 4, -4}) == true);
-    return 0;
 }
