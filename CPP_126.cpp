102026f0737602fd57209efa5a9a9e55284bba1e
beep boop <githubbot@vadim.me>
2024-08-02 00:30:16 +0200

    Solution 13 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nh1fhueo Prompt: c572b692-7944-4014-adf2-ea38f2fb96a4.cpp:1:5: warning: missing terminating ' cha racter     1 | Here's the completed code:       |     ^ c572b692-7944-4014-adf2-ea38f2fb96a4.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the completed code:       |     ^~~~~~~~~~~~~~~~~~~~~~ c572b692-7944-4014-adf2-ea38f2fb96a4.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the completed code:       | ^~~~ In file included from c572b692-7944-4014-adf2-ea38f2fb96a4.cpp:18: c572b692-7944-4014-adf2-ea38f2fb96a4.cpp: In function ‘int main()’: c572b692-7944-4014-adf2-ea38f2fb96a4.cpp:20:13: error: ‘is_sorted’ was not decla red in this scope    20 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <algorithm>
 
-bool is_sorted(std::vector<int> lst) {
+bool is_sorted(std::vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
         if(lst[i] <= lst[i-1]){
             return false;
