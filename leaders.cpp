0dcdb90603bfef71d76ee69063e207c04af0ef3f
beep boop <githubbot@vadim.me>
2024-08-01 18:30:38 +0200

    Solution 4 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xaseaifu Prompt: Given a vector of positive integers, return a vector of the leaders in that vector. A leader is deﬁned as a number that is greater than or equal to all the numbers tothe right of it. The rightmost element is always a leader.

--- leaders.cpp
+++ leaders.cpp
@@ -2,23 +2,19 @@
 using namespace std;
 
 vector<int> leaders(vector<int>& arr) {
+    vector<int> result;
     int n = arr.size();
-    vector<int> res;
-    
     for (int i = n - 1; i >= 0; i--) {
         bool leader = true;
         for (int j = i + 1; j < n; j++) {
-            if (arr[i] <= arr[j]) {
+            if (arr[j] >= arr[i]) {
                 leader = false;
                 break;
             }
         }
-        
         if (leader) {
-            res.push_back(arr[i]);
+            result.push_back(arr[i]);
         }
     }
-    
-    reverse(res.begin(), res.end());
-    return res;
+    return result;
 }
