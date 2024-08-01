4b20ef9c9a74bc500623cad4b36e4b7188afcd6d
beep boop <githubbot@vadim.me>
2024-08-01 21:48:59 +0200

    Solution 80 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e9kg7thn Prompt:       |                    ^~~ d48c0c72-e060-436d-823e-73da4c3e26bc.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d48c0c72-e060-436d-823e-73da4c3e26bc.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d48c0c72-e060-436d-823e-73da4c3e26bc.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d48c0c72-e060-436d-823e-73da4c3e26bc.cpp:9: d48c0c72-e060-436d-823e-73da4c3e26bc.cpp: In function ‘int main()’: d48c0c72-e060-436d-823e-73da4c3e26bc.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ d48c0c72-e060-436d-823e-73da4c3e26bc.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,23 +1,11 @@
-#include <iostream>
-#include <vector>
+```cpp
 #include <queue>
+#include <vector>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> maximum(vector<int> arr, int k) {
-    priority_queue<int> pq;
-    for (int i : arr) pq.push(i);
-    vector<int> res;
-    for (int i = 0; i < k; i++) res.push_back(pq.top()), pq.pop();
+std::vector<int> maximum(std::vector<int> arr,int k){
+    std::priority_queue<int> pq;
+    for(int i:arr) pq.push(i);
+    std::vector<int> res;
+    for(int i=0; i<k; i++) res.push_back(pq.top()), pq.pop();
     return res;
-}
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));
-    cout << "Code works correctly." << endl;
-    return 0;
 }
