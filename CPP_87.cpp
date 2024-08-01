354dc035dd54c8886df247dea9657a233e889bf7
beep boop <githubbot@vadim.me>
2024-08-02 01:36:48 +0200

    Solution 53 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gtgpgdud Prompt:       |                                          ^~~~~~ cb68ffe7-7151-4d8f-be98-ac69c3894284.cpp:37:42: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from cb68ffe7-7151-4d8f-be98-ac69c3894284.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from cb68ffe7-7151-4d8f-be98-ac69c3894284.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cb68ffe7-7151-4d8f-be98-ac69c3894284.cpp:37:49: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ cb68ffe7-7151-4d8f-be98-ac69c3894284.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ cb68ffe7-7151-4d8f-be98-ac69c3894284.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -27,8 +27,6 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
     return result;
 }
 
-int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
 
-    return 0;
-}
+return 0;
