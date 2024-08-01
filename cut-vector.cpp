6c991a3a1f845527937c33aa6c67f9f3354f8576
beep boop <githubbot@vadim.me>
2024-08-01 16:31:52 +0200

    Solution 11 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/j82kg6th Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,55 +1,19 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> nums) {
+vector<vector<int>> cutVector(vector<int> nums) {
     int n = nums.size();
-    int min_diff = INT_MAX;
-    int pos = -1;
-    
-    for (int i = 0; i < n; ++i) {
-        int sum1 = 0, sum2 = 0;
-        
-        for (int j = 0; j <= i; ++j) {
-            sum1 += nums[j];
-        }
-        
-        for (int j = i + 1; j < n; ++j) {
-            sum2 += nums[j];
-        }
-        
-        int diff = abs(sum1 - sum2);
-        
-        if (diff == 0 || diff < min_diff) {
-            min_diff = diff;
-            pos = i;
+    vector<vector<int>> res(2);
+    for (int i = 1; i < n; ++i) {
+        if (abs(nums[i] - nums[0]) <= abs(nums[n - 1] - nums[i])) {
+            res[0].insert(res[0].end(), nums.begin(), nums.begin() + i);
+            res[1].insert(res[1].begin(), nums.begin() + i, nums.end());
+            break;
+        } else {
+            res[0].insert(res[0].end(), nums.begin(), nums.end());
+            res[1] = vector<int>(0);
+            break;
         }
     }
-    
-    vector<int> left(nums.begin(), nums.begin() + pos + 1);
-    vector<int> right(nums.begin() + pos, nums.end());
-    
-    return {left, right};
-}
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (auto &num : nums) {
-        cin >> num;
-    }
-    
-    pair<vector<int>, vector<int>> result = cutVector(nums);
-    cout << "Left: ";
-    for (const auto &num : result.first) {
-        cout << num << " ";
-    }
-    cout << endl;
-    cout << "Right: ";
-    for (const auto &num : result.second) {
-        cout << num << " ";
-    }
-    cout << endl;
-    
-    return 0;
+    return res;
 }
