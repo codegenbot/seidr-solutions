69c436478d335f411da2a7e50a6431113b7700ff
beep boop <githubbot@vadim.me>
2024-08-01 22:25:12 +0200

    Solution 25 of substitution-cipher, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/xzokqt6a Prompt: 9b02aa40-fe48-4ad1-a6b6-e0dc609b5e1d.cpp:20:5: error: ‘cout’ was not declared in  this scope; did you mean ‘std::cout’?    20 |     cout << substitutionCipher(key1, key2, message) << endl;       |     ^~~~       |     std::cout In file included from 9b02aa40-fe48-4ad1-a6b6-e0dc609b5e1d.cpp:2: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ 9b02aa40-fe48-4ad1-a6b6-e0dc609b5e1d.cpp:20:13: error: ‘substitutionCipher’ was not declared in this scope    20 |     cout << substitutionCipher(key1, key2, message) << endl;       |             ^~~~~~~~~~~~~~~~~~ 9b02aa40-fe48-4ad1-a6b6-e0dc609b5e1d.cpp:20:56: error: ‘endl’ was not declared i n this scope; did you mean ‘std::endl’?    20 |     cout << substitutionCipher(key1, key2, message) << endl;       |                                                        ^~~~       |                                                        std::endl In file included from /usr/include/c++/11/iostream:39,                  from 9b02aa40-fe48-4ad1-a6b6-e0dc609b5e1d.cpp:2: /usr/include/c++/11/ostream:684:5: note: ‘std::endl’ declared here   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~

--- substitution-cipher.cpp
+++ substitution-cipher.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <iostream>
 #include <string>
@@ -6,12 +7,8 @@ using namespace std;
 string substitutionCipher(string key1, string key2, string message) {
     string result = "";
     for (int i = 0; i < message.length(); i++) {
-        if (i < key1.length() && i < key2.length()) {
-            if (key1[i] == message[i]) {
-                result += key2[i];
-            } else {
-                result += message[i];
-            }
+        if (key1[i] == message[i]) {
+            result += key2[i];
         } else {
             result += message[i];
         }
