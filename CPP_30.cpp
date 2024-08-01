66b1816562beabbbd66dfafb3fcae8eb93f3ce04
beep boop <githubbot@vadim.me>
2024-08-01 21:48:07 +0200

    Solution 62 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/omnj4mcc Prompt:       |           ^~~~~~ In file included from 2ce9456e-4356-461b-a8be-2aee6394138e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2ce9456e-4356-461b-a8be-2aee6394138e.cpp:32:36: error: expected primary-expressi on before ‘float’    32 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2ce9456e-4356-461b-a8be-2aee6394138e.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 2ce9456e-4356-461b-a8be-2aee6394138e.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main(){       |     ^~~~ In file included from 2ce9456e-4356-461b-a8be-2aee6394138e.cpp:31: 2ce9456e-4356-461b-a8be-2aee6394138e.cpp: In function ‘int main()’: 2ce9456e-4356-461b-a8be-2aee6394138e.cpp:41:20: error: ‘get_positive’ was not de clared in this scope    41 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -2,32 +2,26 @@
 #include <iostream>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
-}
-
-std::vector<float> createPositiveVector(int n) {
-    std::vector<float> vec;
-    float num;
-    for (int i = 0; i < n; ++i) {
-        std::cin >> num;
-        if (num > 0) {
-            vec.push_back(num);
-        }
-    }
-    return vec;
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
 
 int main() {
     std::vector<float> vec1, vec2;
+    float num;
     int n;
-    
-    std::cin >> n;
-    vec1 = createPositiveVector(n);
-    
-    std::cin >> n;
-    vec2 = createPositiveVector(n);
 
-    // Check if vectors are the same
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> num;
+        vec1.push_back(num);
+    }
+
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> num;
+        vec2.push_back(num);
+    }
+
     std::cout << std::boolalpha << issame(vec1, vec2) << std::endl;
 
     return 0;
