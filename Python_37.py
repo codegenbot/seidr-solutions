0800ca1cebbbd1ecbbe47e1ef1f3f5cba4a4bec4
beep boop <githubbot@vadim.me>
2024-08-01 19:19:17 +0200

    Solution 98 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/go4v9kj7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cde5d2a0-5e60-4ae9-867d-c2043a66a b35.py", line 9, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/cde5d2a0-5e60-4ae9-867d-c2043a66a b35.py", line 4, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,2 @@
 def sort_even(l: list):
-    return [
-        i if i % 2 != 0 else j
-        for i, j in zip(sorted([x if x % 2 != 0 else y for x, y in zip(l, l[1:])]), l)
-    ]
+    return [i if i % 2 != 0 else j for i, j in zip(sorted([x if x % 2 != 0 else y for x, y in zip(l, l[1:])]), l)]
