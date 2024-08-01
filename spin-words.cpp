b8a45dd1f880a7895db3e20efbfcf67133a6dabc
beep boop <githubbot@vadim.me>
2024-08-01 19:02:52 +0200

    Solution 16 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/cteneuk8 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 330d23d7-ea70-4d05-9406-ff6126207709.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,73 +1,33 @@
+#include <vector>
+#include <iostream>
 #include <string>
+
 using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int wordLength = 0;
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == ' ') {
-            result += str.substr(wordLength, i - wordLength);
-            wordLength = i + 1;
-            if(result[result.length() - 1] == ' ')
-                result = result.substr(0, result.length() - 1);
+    int wordStart = 0;
+    
+    for(int i=0; i<=str.length(); i++) {
+        if(i == str.length() || str[i] == ' ') {
+            string word = str.substr(wordStart, i-wordStart);
+            
+            if(word.length() >= 5) {
+                reverse(word.begin(), word.end());
+            }
+            
+            result += word + (i == str.length() ? "" : " ");
+            wordStart = i+1;
         }
     }
-    result += str.substr(wordLength);
+    
+    return result;
+}
 
-    string temp = "";
-    for (int i = 0; i < result.length(); i++) {
-        temp = (result[i] == ' ') ? temp + " " : temp;
-        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
-            if (result[i + 1] >= 'a' && result[i + 1] <= 'z')
-                i++;
-        }
-    }
-
-    string final = "";
-    int j = temp.length() - 1;
-    for (int i = temp.length() - 1; i >= 0; i--) {
-        if (!ispunct(temp[i])) {
-            final += temp[i];
-        } else {
-            final = temp[i] + final;
-        }
-    }
-
-    int k = 0;
-    for(int i=0;i<final.length();i++){
-        if(final[i]>='A' && final[i]<='Z'){
-            final[k++] = 'a';
-        }else{
-            final[k++] = final[i];
-        }
-    }
-
-    string spin = "";
-    bool found = false;
-    int count = 0;
-    for(int i=0;i<final.length();i++){
-        if(final[i] == ' '){
-            if(count > 4) {
-                for (int j = i; j >= 0; j--) {
-                    spin += final[j];
-                }
-                found = true;
-            } else
-                spin += " ";
-            count = 0;
-        }else{
-            spin += final[i];
-            count++;
-        }
-    }
-
-    if(!found)
-        return result;
-
-    string res = "";
-    for(int i=spin.length()-1;i>=0;i--){
-        res+=spin[i];
-    }
-
-    return (res.length() == 0)?result:res;
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "Result: " << spinWords(str) << endl;
+    return 0;
 }
