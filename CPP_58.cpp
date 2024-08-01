33a3a5cb087a8d43c7d780dd4725026e62ca5164
beep boop <githubbot@vadim.me>
2024-08-01 17:18:02 +0200

    Solution 10 of CPP/58, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7jyxcr8g Prompt: 4b5bc5ac-5551-4299-8395-4231a5cfff3d.cpp:11:22: error: ‘s1’ was not declared in this scope; did you mean ‘l1’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                      ^~       |                      l1 4b5bc5ac-5551-4299-8395-4231a5cfff3d.cpp:11:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    11 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),       |                                            ^~       |                                            l2 4b5bc5ac-5551-4299-8395-4231a5cfff3d.cpp:12:32: error: ‘intersection’ was not de clared in this scope    12 |                       inserter(intersection, intersection.begin()));       |                                ^~~~~~~~~~~~ 4b5bc5ac-5551-4299-8395-4231a5cfff3d.cpp: At global scope: 4b5bc5ac-5551-4299-8395-4231a5cfff3d.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 4b5bc5ac-5551-4299-8395-4231a5cfff3d.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,12 +1,29 @@
-vector<int> common(vector<int> l1,vector<int> l2){
-    vector<int> result;
-    set<int> s1(l1.begin(),l1.end());
-    set<int> s2(l2.begin(),l2.end());
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
     
-    for(auto i : s1)
-        if(s2.count(i))
-            result.push_back(i);
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      inserter(intersection, intersection.begin()));
     
-    sort(result.begin(),result.end());
+    vector<int> result(intersection.begin(), intersection.end());
+    sort(result.begin(), result.end());
     return result;
+}
+
+int main2() {
+    vector<int> l1 = {1, 4, 3, 34, 653, 2, 5};
+    vector<int> l2 = {5, 7, 1, 5, 9, 653, 121};
+    vector<int> result = common(l1, l2);
+    
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
 }
