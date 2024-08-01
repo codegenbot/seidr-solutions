615587a87bb67a93cad683fad591383b65929fb2
beep boop <githubbot@vadim.me>
2024-08-01 13:41:20 +0200

    Solution 0 of CPP/24, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/k68k0n7e Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_24.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+
+int largest_divisor(int n) {
+    int i = n - 1;
+    while (i > 0) {
+        if (n % i == 0) {
+            return i;
+        }
+        i--;
+    }
+    return 1;
+}
