3b60e89faf479884bb34bf56b85bdb9c20a61c33
beep boop <githubbot@vadim.me>
2024-08-01 20:06:23 +0200

    Solution 87 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/01ot4m8f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/442643d2-42f9-42af-9f31-ce2c6c507 829.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/442643d2-42f9-42af-9f31-ce2c6c507 829.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/442643d2-42f9-42af-9f31-ce2c6c507 829.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/442643d2-42f9-42af-9f31-ce2c6c507 829.py", line 4, in <listcomp>     for i, (x, y) in enumerate(sorted(zip(*[l[i::2] for i in range(len(l))])))            ^^^^^^ ValueError: too many values to unpack (expected 2)

--- Python_37.py
+++ Python_37.py
@@ -1,6 +1,4 @@
 def sort_even(lst: list):
     evens = sorted([x for x in lst if x % 2 == 0])
     odds = [x for x in lst if x % 2 != 0]
-    return [
-        x if i < len(evens) else y for i, (x, y) in enumerate(zip(evens + [0], odds))
-    ]
+    return [x if i < len(evens) else y for i, (x, y) in enumerate(zip(evens + [0], odds))]
