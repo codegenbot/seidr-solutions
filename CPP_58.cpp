7f938c6d52d626c6c49b8a57fb8b5eb26c4bbbb7
beep boop <githubbot@vadim.me>
2024-08-01 23:29:04 +0200

    Solution 14 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/j4jvgpo2 Prompt: In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 3e70fd3b-48a2-4d1e-916c-319749fe26be.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 3e70fd3b-48a2-4d1e-916c-319749fe26be.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3e70fd3b-48a2-4d1e-916c-319749fe26be.cpp:6:35: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a, vector<int> b) {       |                                   ^~~ 3e70fd3b-48a2-4d1e-916c-319749fe26be.cpp:6:41: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a, vector<int> b) {       |                                         ^

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -2,21 +2,25 @@
 #include <vector>
 #include <set>
 #include <cassert>
+#include <iostream>
+#include <iterator>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
+using namespace std;
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::sort(l1.begin(), l1.end());
-    std::sort(l2.begin(), l2.end());
-    std::vector<int> result;
-    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
-    result.erase(std::unique(result.begin(), result.end()), result.end());
+vector<int> common(vector<int> l1, vector<int> l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
+    result.erase(unique(result.begin(), result.end()), result.end());
     return result;
 }
 
+bool issame(vector<int>& a, vector<int>& b) {
+    return a == b;
+}
+
 int main() {
-    assert(issame(common({4, 3, 2, 8}, {}), {}));
+    assert(issame(common({4, 3, 2, 8}, {}), {}).empty());
     return 0;
 }
