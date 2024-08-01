56067b6204b24f6778d0adfc825e614732ac601a
beep boop <githubbot@vadim.me>
2024-08-01 22:13:12 +0200

    Solution 38 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/nbqcl5il Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a24e266c-e882-46f5-933f-ab483ef87 89e.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_73.py
+++ Python_73.py
@@ -1,15 +1,17 @@
-```
 def smallest_change(arr):
     n = len(arr)
-    arr.sort()
-    hashmap = {val:i for i,val in enumerate(arr)}
+    arr_pos = [*enumerate(arr)]
+    pos_arr = {v:i for i,v in enumerate(arr)}
     
-    ans = 0
-    for i in range(n):
-        if arr[i] != i:
-            j = hashmap[arr[i]]
-            arr[i],arr[j] = arr[j],arr[i]
-            hashmap[i],hashmap[arr[i]] = hashmap[arr[i]],i
-            ans +=1
-            
+    ans =0
+    while len(arr_pos)>1:
+        min_val = float('inf')
+        min_idx = -1
+        for i in range(len(arr_pos)):
+            if arr_pos[i][1]<min_val:
+                min_val = arr_pos[i][1]
+                min_idx = i
+        ans += 1
+        arr_pos[0],arr_pos[min_idx] = arr_pos[min_idx],arr_pos[0]
+        pos_arr[arr_pos[0][1]],pos_arr[arr_pos[min_idx][1]] = pos_arr[arr_pos[min_idx][1]],pos_arr[arr_pos[0][1]]
     return ans
