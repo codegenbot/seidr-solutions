3ee97f9db53188430ab01fb2b01f2b03f9804d50
beep boop <githubbot@vadim.me>
2024-08-01 23:49:29 +0200

    Solution 50 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cbcj0kw7 Prompt:       |                    ^~~ bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp:13: bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp: In function ‘int main()’: bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp:23:20: error: ‘maximum’ was not declare d in this scope    23 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ bba5ea05-f60b-4d2f-90ff-4e345b395a4d.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,13 +1,19 @@
 #include <vector>
-using namespace std;
+#include <algorithm>
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
 vector<int> maximum(vector<int> arr, int k) {
-    vector<int> res(k);
-    partial_sort(arr.begin(), arr.begin() + k, arr.end(),
-                  [&res](int a, int b) { return count(res.begin(), res.end(), a) < count(res.begin(), res.end(), b); });
-    return res;
-}
+    vector<int> result;
+    for (int i = 0; i < k; i++) {
+        auto it = std::max_element(arr.begin(), arr.end());
+        result.push_back(*it);
+        arr.erase(it);
+    }
+    return result;
