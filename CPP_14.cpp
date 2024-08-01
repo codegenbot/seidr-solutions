d675a7ac1b99f9058819161fd2bcc9830c7e0ff7
beep boop <githubbot@vadim.me>
2024-08-01 14:39:44 +0200

    Solution 18 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/tsrek31i Prompt: 5a332cea-79f6-4390-aebf-063d5d6a41b8.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    31 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 5a332cea-79f6-4390-aebf-063d5d6a41b8.cpp:17:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    17 | bool issame(vector<string> a, vector<string> b){       |      ^~~~~~ 5a332cea-79f6-4390-aebf-063d5d6a41b8.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 5a332cea-79f6-4390-aebf-063d5d6a41b8.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main(){       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-vector<string> all_prefixes(string str);
-bool issame(vector<string> a, vector<string> b);
-
 vector<string> all_prefixes(string str) {
     vector<string> result;
     string prefix = "";
@@ -17,14 +14,7 @@ vector<string> all_prefixes(string str) {
     return result;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
-}
-
 int main() {
     assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    return 0;
 }
