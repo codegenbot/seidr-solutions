d85a9baa68aba684422e2af6ceada5b9f8bdb694
beep boop <githubbot@vadim.me>
2024-08-02 00:33:06 +0200

    Solution 13 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/84ikljmq Prompt:    42 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 6fcceeeb-c8fb-4bba-bce1-df506db3c3bb.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 6fcceeeb-c8fb-4bba-bce1-df506db3c3bb.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from 6fcceeeb-c8fb-4bba-bce1-df506db3c3bb.cpp:41: 6fcceeeb-c8fb-4bba-bce1-df506db3c3bb.cpp: In function ‘int main()’: 6fcceeeb-c8fb-4bba-bce1-df506db3c3bb.cpp:51:20: error: ‘select_words’ was not de clared in this scope    51 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |                    ^~~~~~~~~~~~ 6fcceeeb-c8fb-4bba-bce1-df506db3c3bb.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(select_words("a b c d e f", 1) , {"b", "c", "d", "f"} ));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -10,7 +10,7 @@ bool issame(vector<string> a, vector<string> b);
 vector<string> select_words(string s, int n);
 
 int main() {
-    assert(issame(select_words("a b c d e f", 1), { "b", "c", "d", "f" }));
+    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
     return 0;
 }
 
