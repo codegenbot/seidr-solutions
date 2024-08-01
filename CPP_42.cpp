45ccbe60c1cbfb4a0d21bb659187c69b7ddfd592
beep boop <githubbot@vadim.me>
2024-08-01 17:44:48 +0200

    Solution 30 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yi6l0a82 Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp:7: 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp: In function ‘int main()’: 3deeb0be-8d70-43ac-81f1-022ba16e8376.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,12 +1,5 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return std::equal(a.begin(), a.end(), b.begin(), b.end());
-}
-
-int main() {
-    assert(issame(std::vector<int>{5, 2, 5, 2, 3, 3, 9, 0, 123}, std::vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124}));
-    return 0;
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
 }
