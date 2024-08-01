387fdf0ca14feed6e5050fe9c5f4f5d9f50b917a
beep boop <githubbot@vadim.me>
2024-08-01 22:48:34 +0200

    Solution 62 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/n1hclf4a Prompt: [[3, 5, 6], [4, 8, 9], [1, 2]] Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f291cee9-718e-4967-98dd-8d06b5bd1 f31.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f291cee9-718e-4967-98dd-8d06b5bd1 f31.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -2,8 +2,7 @@ def sort_third(l: list):
     for i in range(0, len(l), 3):
         sublist = l[i:i+3]
         if len(sublist) < 3:
-            sublist += [0] * (3 - len(sublist)
+            sublist += [0] * (3 - len(sublist))
         yield sorted(sublist)
 
-# Test the function
-print(list(sort_third([5, 6, 3, 4, 8, 9, 2, 1]))
+print(list(sort_third([5, 6, 3, 4, 8, 9, 2, 1])))
