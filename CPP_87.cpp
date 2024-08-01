1118d2a44613b2b5beb4c6a6fd53e9f0af99a51d
beep boop <githubbot@vadim.me>
2024-08-01 21:34:03 +0200

    Solution 51 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vj11817g Prompt: a495a803-6992-470c-bd5c-cef6090c84a9.cpp:31:49: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ a495a803-6992-470c-bd5c-cef6090c84a9.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ a495a803-6992-470c-bd5c-cef6090c84a9.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from a495a803-6992-470c-bd5c-cef6090c84a9.cpp:30: a495a803-6992-470c-bd5c-cef6090c84a9.cpp: In function ‘int main()’: a495a803-6992-470c-bd5c-cef6090c84a9.cpp:43:27: error: invalid initialization of  reference of type ‘const std::vector<int>&’ from expression of type ‘std::vecto r<std::vector<int> >’    43 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~ a495a803-6992-470c-bd5c-cef6090c84a9.cpp:5:37: note: in passing argument 1 of ‘b ool issame(const std::vector<int>&, const std::vector<int>&)’     5 | bool issame(const std::vector<int>& a, const std::vector<int>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
 bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
     if (a[0][0] != b[0][0]) {
         return a[0][0] < b[0][0];
@@ -9,20 +5,7 @@ bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vecto
     return a[0][1] > b[0][1];
 }
 
-std::vector<std::vector<int>> get_row(const std::vector<std::vector<int>>& lst, int x) {
-    std::vector<std::vector<int>> result;
-    for (int i = 0; i < lst.size(); ++i) {
-        for (int j = 0; j < lst[i].size(); ++j) {
-            if (lst[i][j] == x) {
-                result.push_back({i, j});
-            }
-        }
-    }
-    std::sort(result.begin(), result.end(), issame);
-    return result;
-}
-
 int main() {
-    assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3)[0], {2, 2}));
     return 0;
 }
