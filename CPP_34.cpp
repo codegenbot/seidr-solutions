11ec23b6635e12837d89d88e9843f4041f2fa840
beep boop <githubbot@vadim.me>
2024-08-01 21:53:47 +0200

    Solution 40 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/h5sfhvla Prompt:       |           ^~~~~~ In file included from df199a4c-8de9-4791-bc06-bd1ee1ce6a0e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ df199a4c-8de9-4791-bc06-bd1ee1ce6a0e.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from df199a4c-8de9-4791-bc06-bd1ee1ce6a0e.cpp:17: df199a4c-8de9-4791-bc06-bd1ee1ce6a0e.cpp: In function ‘int main()’: df199a4c-8de9-4791-bc06-bd1ee1ce6a0e.cpp:27:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    27 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from df199a4c-8de9-4791-bc06-bd1ee1ce6a0e.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -3,7 +3,7 @@
 #include <cassert>
 #include <memory>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
