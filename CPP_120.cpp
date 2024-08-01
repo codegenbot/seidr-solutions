896dafec84dabd9c430bb85fd45509563e43b87b
beep boop <githubbot@vadim.me>
2024-08-01 23:38:48 +0200

    Solution 35 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/cbcj0kw7 Prompt:       |                    ^~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:9:27: error: ‘vector’ was not declared in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:9:39: error: expression list treated as  compound expression in initializer [-fpermissive]     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:8: 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp: In function ‘int main()’: 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:18:20: error: ‘maximum’ was not declare d in this scope    18 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 606da7f1-07e8-446a-9b60-bffbe2bc2f28.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,14 +2,14 @@ vector<int> maximum(vector<int> arr, int k) {
     vector<int> res;
     unordered_set<int> unique;
     
-    for(int i = 0; i < arr.size(); i++){
-        if(unique.find(arr[i]) == unique.end()){
-            unique.insert(arr[i]);
-            if(unique.size() >= k){
+    for(int i = 0; i < arr.size(); i++) {
+        if(unique.find(arr[i]) == unique.end()) {
+            res.push_back(arr[i]);
+            if(res.size() == k)
                 break;
-            }
+            unique.insert(arr[i]);
         }
     }
-    res.assign(unique.begin(), unique.end());
+    
     return res;
 }
