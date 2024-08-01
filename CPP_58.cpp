fc1f4de97a598ade886732926f40e4f048399bd7
beep boop <githubbot@vadim.me>
2024-08-01 16:41:43 +0200

    Solution 25 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7s2e5d9a Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp:17:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp:16: 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp: In function ‘int main()’: 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 995913ba-e0af-4dcc-8f7c-fadf5f26e439.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -7,6 +7,18 @@ bool issame(vector<int> a,vector<int>b){
     return a==b;
 }
 
+int main() {
+    vector<int> l1 = {1,2,3};
+    vector<int> l2 = {2,3,4};
+    
+    if(issame(l1,l2))
+        cout << "Vectors are same";
+    else
+        cout << "Vectors are not same";
+        
+    return 0;
+}
+
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
@@ -17,21 +29,4 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 
     vector<int> result(intersection.begin(), intersection.end());
     return result;
-}
-
-int main() {
-    vector<int> l1 = {1, 2, 3};
-    vector<int> l2 = {2, 4, 6};
-
-    if(issame(l1,l2)){
-        cout << "Vectors are identical." << endl;
-    } else {
-        vector<int> common_elements = common(l1, l2);
-        for(auto i : common_elements){
-            cout << i << " ";
-        }
-        cout << endl;
-    }
-
-    return 0;
 }
