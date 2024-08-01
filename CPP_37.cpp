fb1371ce942fbf6416745203081aa0af1123cc9f
beep boop <githubbot@vadim.me>
2024-08-01 22:07:41 +0200

    Solution 22 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ky668db9 Prompt:   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3efa905d-e0e3-43ba-a3d8-61b7f44b4234.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3efa905d-e0e3-43ba-a3d8-61b7f44b4234.cpp:25:36: error: expected primary-expressi on before ‘float’    25 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 3efa905d-e0e3-43ba-a3d8-61b7f44b4234.cpp:24: 3efa905d-e0e3-43ba-a3d8-61b7f44b4234.cpp: In function ‘int main()’: 3efa905d-e0e3-43ba-a3d8-61b7f44b4234.cpp:34:20: error: ‘sort_even’ was not decla red in this scope    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 3efa905d-e0e3-43ba-a3d8-61b7f44b4234.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -8,22 +8,25 @@ bool issame(vector<float> a, vector<float> b) {
     return a == b;
 }
 
+vector<float> sort_even(vector<float> l);
+
+int main() {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return 0;
+}
+
 vector<float> sort_even(vector<float> l) {
     vector<float> even_values;
-    for (int i = 1; i < l.size(); i += 2) {
+    for (int i = 0; i < l.size(); i += 2) {
         even_values.push_back(l[i]);
     }
     sort(even_values.begin(), even_values.end());
     
-    for (int i = 1, j = 0; i < l.size(); i += 2) {
-        l[i] = even_values[j++];
+    for (int i = 0, j = 0; i < l.size(); ++i) {
+        if (i % 2 == 0) {
+            l[i] = even_values[j++];
+        }
     }
     
     return l;
-}
-
-int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    
-    return 0;
 }
