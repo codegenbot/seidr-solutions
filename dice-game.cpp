dcbd3b72fa6916187ef52461dba476ad5e61a497
beep boop <githubbot@vadim.me>
2024-08-01 16:32:27 +0200

    Solution 84 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/q13lgfbj Prompt:     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |     ^~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:13: error: ‘fixed’ was not declared i n this scope     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |             ^~~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:22: error: ‘setprecision’ was not dec lared in this scope     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |                      ^~~~~~~~~~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:41: error: ‘calculateProbability’ was  not declared in this scope     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |                                         ^~~~~~~~~~~~~~~~~~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:71: error: ‘endl’ was not declared in  this scope     4 | cout << fixed << setprecision(1) << calculateProbability(n, m) << endl;       |                                                                   ^~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
 
 double calculateProbability(int n, int m) {
-    double totalCases = (double)n * m;
-    double favorableCases = 0;
-
-    for(int i = 1; i <= m; i++) {
-        favorableCases += min(n, i);
+    double probability = 0;
+    
+    for (int i = 1; i <= m; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            probability += 1.0 / n / m;
+        }
     }
-
-    return favorableCases / totalCases;
+    
+    return probability;
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
-    cout << fixed << setprecision(1) << calculateProbability(n, m) << endl;
+    cout << std::fixed << std::setprecision(1) << calculateProbability(n, m) << std::endl;
     return 0;
 }
