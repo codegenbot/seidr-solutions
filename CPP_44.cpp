875544a3788f88c847e1cf04198c39e5bea356c5
beep boop <githubbot@vadim.me>
2024-08-01 16:27:07 +0200

    Solution 0 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gg4x4xx4 Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_44.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+string change_base(int x, int base) {
+    string res = "";
+    while (x > 0) {
+        int remainder = x % base;
+        if (remainder >= 10) {
+            res.insert(0, 1, (char)(remainder + 'A'));
+        } else {
+            res.insert(0, 1, to_string(remainder));
+        }
+        x /= base;
+    }
+    return res;
+}
