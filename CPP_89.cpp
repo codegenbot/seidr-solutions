2ba6c6c4aeab05d0532cabf3e97b439ec518b33d
beep boop <githubbot@vadim.me>
2024-08-01 20:28:49 +0200

    Solution 0 of CPP/89, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vfz66a7s Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_89.cpp
@@ -0,0 +1,14 @@
+string encrypt(string s){
+    string result = "";
+    for(int i=0; i<s.length(); i++){
+        char c = s[i];
+        if(c >= 'a' && c <= 'm'){
+            c += 2;
+        }
+        else if(c > 'm' && c < 'z'){
+            c -= 26 - 2;
+        }
+        result += c;
+    }
+    return result;
+}
