58f521e2ae80f794676c2f797a905d21a64a82dc
beep boop <githubbot@vadim.me>
2024-08-01 16:39:14 +0200

    Solution 35 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/l90ijmy2 Prompt: d076189b-581a-4bfe-9f8e-ed28b6b2d7ac.cpp:1:14: error: ‘vector’ was not declared in this scope     1 | int fuelCost(vector<int> v) {       |              ^~~~~~ d076189b-581a-4bfe-9f8e-ed28b6b2d7ac.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int fuelCost(vector<int> v) {       |                     ^~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -3,7 +3,7 @@
 int fuelCost(vector<int> v) {
     int sum = 0;
     for (int i : v) {
-        int val = floor(double(i) / 3) - 2;
+        int val = std::floor(double(i) / 3) - 2;
         sum += val;
     }
     return sum;
