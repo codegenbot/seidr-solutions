179309628a438280d909b7fe3ebf14e27c34243c
beep boop <githubbot@vadim.me>
2024-08-01 22:47:43 +0200

    Solution 1 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x3wnftdu Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,39 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
-vector<int> minPath(vector<vector<int>>& grid, int k) {
+vector<int> minPath(vector<vector<int>> grid, int k) {
     int n = grid.size();
-    vector<vector<int>> dir({{-1, 0}, {1, 0}, {0, -1}, {0, 1}});
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    vector<vector<bool>> visited(n, vector<bool>(n));
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>> > pq;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            pq.push({grid[i][j], i * n + j});
-        }
-    }
-    vector<int> res;
-    while (!pq.empty()) {
-        int val, idx;
-        tie(val, idx) = pq.top();
-        pq.pop();
-        res.push_back(val);
-        if (res.size() == k) break;
-        int x = idx / n, y = idx % n;
-        for (int i = 0; i < 4; ++i) {
-            int nx = x + dir[i][0], ny = y + dir[i][1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                pq.push({grid[nx][ny], nx * n + ny});
+            if (!visited[i][j]) {
+                pq.push({grid[i][j], {i, j}});
+                visited[i][j] = true;
             }
         }
     }
+
+    vector<int> res;
+    while (!pq.empty() && k--) {
+        int val = pq.top().first;
+        pair<int, int> pos = pq.top().second;
+        pq.pop();
+        res.push_back(val);
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                if (abs(i) + abs(j) == 1 && pos.first + i >= 0 && pos.second + j >= 0 && pos.first + i < n && pos.second + j < n && !visited[pos.first + i][pos.second + j]) {
+                    pq.push({grid[pos.first + i][pos.second + j], {pos.first + i, pos.second + j}});
+                    visited[pos.first + i][pos.second + j] = true;
+                }
+            }
+        }
+    }
+
     return res;
 }
 
 int main() {
     vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int k = 3;
-    vector<int> res = minPath(grid, k);
-    for (auto x : res) cout << x << " ";
+    vector<int> result = minPath(grid, k);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
