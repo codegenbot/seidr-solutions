1e7e15e24f4716e6d71db8c935f2706ad7fc7d9b
beep boop <githubbot@vadim.me>
2024-08-01 21:44:07 +0200

    Solution 14 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/h5sfhvla Prompt:       |           ^~~~~~ In file included from 110b7abb-6b77-4ead-8a60-81488c6b8e3f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 110b7abb-6b77-4ead-8a60-81488c6b8e3f.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 110b7abb-6b77-4ead-8a60-81488c6b8e3f.cpp:15: 110b7abb-6b77-4ead-8a60-81488c6b8e3f.cpp: In function ‘int main()’: 110b7abb-6b77-4ead-8a60-81488c6b8e3f.cpp:25:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    25 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 110b7abb-6b77-4ead-8a60-81488c6b8e3f.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -2,20 +2,13 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-vector<int> solve(const vector<int>& l) {
-    vector<int> sortedList = l;
-    sort(sortedList.begin(), sortedList.end());
-    sortedList.erase(unique(sortedList.begin(), sortedList.end()), sortedList.end());
+std::vector<int> solve(const std::vector<int>& l) {
+    std::vector<int> sortedList = l;
+    std::sort(sortedList.begin(), sortedList.end());
+    sortedList.erase(std::unique(sortedList.begin(), sortedList.end()), sortedList.end());
     return sortedList;
-}
-
-int main() {
-    assert(issame(solve({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
-    return 0;
 }
