7839dcff39f105c2c30e86f288a3b3db33ff5c50
beep boop <githubbot@vadim.me>
2024-08-01 21:17:44 +0200

    Solution 15 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e9kg7thn Prompt:       |                    ^~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:7: 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp: In function ‘int main()’: 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <algorithm>
 
@@ -6,16 +5,13 @@ bool issame(vector<int> a,vector<int>b){
     return a == b;
 }
 
-int main() {
-    vector<int> arr = {1,2,3,4,5};
-    int k = 3;
-    vector<int> result = maximum(arr,k);
-    for(int i : result) cout << i << " ";
-    return 0;
-}
-
 vector<int> maximum(vector<int> arr, int k) {
     vector<int> result(arr.begin(), arr.begin() + k);
     sort(result.begin(), result.end());
-    return result;
+    for(int i = 0; i < k; i++) {
+        if(i > 0) {
+            arr.erase(arr.begin());
+        }
+    }
+    return arr;
 }
