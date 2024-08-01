e9258bc32ba2217fc0b39bee3e39b47e0cd4deed
beep boop <githubbot@vadim.me>
2024-08-01 18:20:26 +0200

    Solution 53 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/k62im641 Prompt: e299e489-219c-491f-8b15-945897ca2003.cpp: In function ‘int main()’: e299e489-219c-491f-8b15-945897ca2003.cpp:16:26: error: ‘pow’ was not declared in  this scope    16 |         totalDistance += pow(bouncinessIndex, i);       |                          ^~~ e299e489-219c-491f-8b15-945897ca2003.cpp:19:10: error: ‘std::ostream’ {aka ‘clas s std::basic_ostream<char>’} has no member named ‘setprecision’; did you mean ‘s td::streamsize std::ios_base::_M_precision’? (not accessible from this context)    19 |     cout.setprecision(4);       |          ^~~~~~~~~~~~ In file included from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from e299e489-219c-491f-8b15-945897ca2003.cpp:1: /usr/include/c++/11/bits/ios_base.h:545:25: note: declared protected here   545 |     streamsize          _M_precision;       |                         ^~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,5 +1,7 @@
+```c++
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main() {
@@ -14,7 +16,7 @@ int main() {
 
     double totalDistance = 0.0;
     for (int i = 1; i <= numBounces; ++i) {
-        totalDistance += std::pow(bouncinessIndex, i);
+        totalDistance += pow(bouncinessIndex, i);
     }
 
     cout << fixed;
