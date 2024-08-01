fb858f2337edff985647e726ce3409f9387668fb
beep boop <githubbot@vadim.me>
2024-08-01 18:29:41 +0200

    Solution 3 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/d4k13np5 Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,11 +1,4 @@
 Here is the completed code:
 
 def find_zero(xs: list):
-    if len(xs) % 2 != 0:
-        return None
-    a = xs[0]
-    b = sum([coeff * i for i, coeff in enumerate(xs[1:])])
-    c = sum([coeff * math.pow(i, 2) for i, coeff in enumerate(xs[1:], 1)])
-    d = -sum(xs)
-    x = (-b + math.sqrt(b**2 - 4*a*c)) / (2*a)
-    return round(x, 10)
+    return (-xs[1] / xs[0]) if len(xs) == 2 else (-xs[3] / xs[2])
