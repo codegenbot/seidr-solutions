a93880431fec59de9025670bab2a2f9fe2ceade9
beep boop <githubbot@vadim.me>
2024-08-02 01:47:35 +0200

    Solution 70 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/l6hmhmtf Prompt: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:64: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp: In function ‘int main()’: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:13: error: ‘boost’ has not been decl ared    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:29: error: ‘string’ was not declared  in this scope    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:37: error: ‘compare_one’ was not dec lared in this scope    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,15 +1,41 @@
+```cpp
+#include <boost/any.hpp>
 #include <string>
-#include <iostream>
-#include <algorithm>
 
-std::string compare_one(std::string a, float b) {
-    if (atof(a.c_str()) > b)
-        return "a";
-    else
-        return "b";
+namespace boost {
+
+any compare_one(any a, any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (int)a.convert_to<int>() < (float)b ? &b : &a;
+    }
+    else if (a.type() == typeid(int) && b.type() == typeid(double)) {
+        return (int)a.convert_to<int>() < (double)b ? &b : &a;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        if (stof(a.convert_to<string>().c_str()) > (int)b) {
+            return a;
+        }
+       	else if (stof(a.convert_to<string>().c_str()) < (int)b) {
+            return b;
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
+        if (stof(a.convert_to<string>().c_str()) > (double)b) {
+            return a;
+        }
+		else if (stof(a.convert_to<string>().c_str()) < (double)b) {
+            return b;
+        }
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
+        if (stof(a.convert_to<string>().c_str()) > stof(b.convert_to<string>().c_str())) {
+            return a;
+        }
+		else if (stof(a.convert_to<string>().c_str()) < stof(b.convert_to<string>().c_str())) {
+            return b;
+        }
+    }
+    return any();
 }
 
-int main() {
-    std::cout << compare_one("1.5", 2.0) << std::endl;
-    return 0;
 }
