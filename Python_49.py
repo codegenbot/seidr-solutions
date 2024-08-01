22664b511bf0c68b33d2bc45d1bccc9c1fa5e92e
beep boop <githubbot@vadim.me>
2024-08-01 20:01:51 +0200

    Solution 82 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/9uasmkcs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b89a2dbd-434d-46b6-b805-0d4139874 195.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b89a2dbd-434d-46b6-b805-0d4139874 195.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,18 +1,23 @@
-def modinv(a: int, p: int):
-    if gcd(a, p) != 1:
+```
+def modinv(a: int, m: int):
+    if gcd(a, m) != 1:
         return None
     else:
-        a, p = abs(a), abs(p)
+        a, m = abs(a), abs(m)
         x0, x1 = 1, 0
-        while p > 0:
-            quotient = a // p
+        while m > 0:
+            quotient = a // m
             x2 = x1 - quotient * x0
-            a, p = p, a % p
+            a, m = m, a % m
             x1, x0 = x0, x2
         return x0
-
 
 def gcd(a: int, b: int):
     while b != 0:
         a, b = b, a % b
     return abs(a)
+
+def modp(n: int, p: int):
+    if n < 1 or p < 2:
+        return None
+    return pow(n, p - 2, p) * n % p
