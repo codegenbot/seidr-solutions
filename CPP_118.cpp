8e63ca9719f28a0647688317f55129fe28c1f07c
beep boop <githubbot@vadim.me>
2024-08-01 23:51:12 +0200

    Solution 51 of CPP/118, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/0g6afgtq Prompt: 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp:18:9: error: ‘tolower’ was not declared  in this scope    18 |     c = tolower(c);       |         ^~~~~~~ 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp: At global scope: 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp:22:18: error: ‘string’ was not declared  in this scope    22 | bool isconsonant(string word, int i) {       |                  ^~~~~~ 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp:22:31: error: expected primary-expressi on before ‘int’    22 | bool isconsonant(string word, int i) {       |                               ^~~ 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp:22:36: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool isconsonant(string word, int i) {       |                                    ^ In file included from 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp:29: 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp: In function ‘int main()’: 03e67daa-dda4-42d0-8efe-ca7bb55dbcb8.cpp:31:13: error: ‘get_closest_vowel’ was n ot declared in this scope    31 |     assert (get_closest_vowel("Above") == "o");       |             ^~~~~~~~~~~~~~~~~

--- CPP_118.cpp
+++ CPP_118.cpp
@@ -5,15 +5,13 @@ std::string get_closest_vowel(std::string);
 
 std::string get_closest_vowel(string word) {
     int n = word.length();
-    string closest_vowel = "";
     for (int i = n - 1; i > 0; --i) {
         if (isvowel(word[i])) {
             for (int j = i - 1; j >= 0; --j) {
                 if (!isvowel(word[j]) && !isconsonant(word, j)) break;
                 else if (!isvowel(word[j]) && isconsonant(word, j)) continue;
                 else {
-                    closest_vowel = (char)(word[i]);
-                    return closest_vowel;
+                    return (char)(word[i]);
                 }
             }
         }
