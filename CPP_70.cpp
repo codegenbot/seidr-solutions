a97dbd3e4b3590464aafa6d7ba0a2da3abbfbf31
beep boop <githubbot@vadim.me>
2024-08-02 00:17:30 +0200

    Solution 14 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/uae1mtiy Prompt:       |           ^~~~~~ In file included from 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp:16: 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp: In function ‘int main()’: 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp:26:20: error: ‘strange_sort_list’ was n ot declared in this scope    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 837760d7-ae13-4f30-acd2-9f40605e1ed6.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,10 +1,12 @@
-#include <vector>
 #include <algorithm>
-#include <cassert>
 
-std::vector<int> strange_sort_list(std::vector<int>& lst) {
-    std::sort(lst.begin(), lst.end());
-    std::vector<int> result;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> strange_sort_list(vector<int>& lst) {
+    sort(lst.begin(), lst.end());
+    vector<int> result;
     int i = 0, j = lst.size() - 1;
     while (i <= j) {
         result.push_back(lst[i++]);
@@ -13,15 +15,4 @@ std::vector<int> strange_sort_list(std::vector<int>& lst) {
         }
     }
     return result;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b);
-
-int main() {
-    assert(issame(strange_sort_list({111111}), {111111}));
-    return 0;
-}
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
 }
