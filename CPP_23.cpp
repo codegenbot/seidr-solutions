a5368e80f86b82ba859a6322735d54de539f78f8
beep boop <githubbot@vadim.me>
2024-08-01 20:12:34 +0200

    Solution 36 of CPP/23, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/9f1pr9mh Prompt: b4cb1d34-cc62-4002-8bda-359aeae5a1cb.cpp:13:5: error: redefinition of ‘int main( )’    13 | int main(){       |     ^~~~ b4cb1d34-cc62-4002-8bda-359aeae5a1cb.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,2 +1,11 @@
-return str.length();
+#include <string>
+#include <cassert>
+#include <cstring>
+
+int main2() {
+    assert(strlen("asdasnakj") == 9);
+    std::string str = "example";
+    assert(str.length() == 7);
+    return str.length();
+    return 0;
 }
