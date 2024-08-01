90121515f62bec0389cc3a06c4d781f8900c4100
beep boop <githubbot@vadim.me>
2024-08-01 15:47:53 +0200

    Solution 69 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/r6589nh8 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 41dd8137-60b0-401c-a4de-fb2b3ae6f3aa.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 41dd8137-60b0-401c-a4de-fb2b3ae6f3aa.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 41dd8137-60b0-401c-a4de-fb2b3ae6f3aa.cpp:10: 41dd8137-60b0-401c-a4de-fb2b3ae6f3aa.cpp: In function ‘int main()’: 41dd8137-60b0-401c-a4de-fb2b3ae6f3aa.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 41dd8137-60b0-401c-a4de-fb2b3ae6f3aa.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,27 +1,12 @@
 bool issame(vector<string> a, vector<string> b) {
-    for (int i = 0; i < a.size() && i < b.size(); i++) {
-        if (!contains(b[i], a[i]))
-            return false;
-    }
-    for (int i = 0; i < b.size(); i++) {
-        bool found = false;
-        for (int j = 0; j < a.size(); j++) {
-            if (contains(a[j], b[i])) {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-            return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(find(b.begin(), b.end(), a[i]) == b.end()) return false;
     }
     return true;
 }
 
-bool contains(string a, string b) {
-    return a.find(b) != string::npos;
-}
-
 int main() {
-    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+    assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));
     return 0;
 }
