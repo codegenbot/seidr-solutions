d40a8b3b46594d39847b933efd0fb58b66d58f70
beep boop <githubbot@vadim.me>
2024-08-01 17:09:51 +0200

    Solution 8 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/gf7y04ly Prompt: 78cb67aa-9f31-4af4-9b80-0ecc01a392c5.cpp:24:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    24 |             str+=chr;       |             ^~~       |             std 78cb67aa-9f31-4af4-9b80-0ecc01a392c5.cpp:27:15: error: expected ‘;’ before ‘enco ded_str’    27 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 78cb67aa-9f31-4af4-9b80-0ecc01a392c5.cpp:14: 78cb67aa-9f31-4af4-9b80-0ecc01a392c5.cpp:28:31: error: ‘encoded_str’ was not dec lared in this scope    28 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 78cb67aa-9f31-4af4-9b80-0ecc01a392c5.cpp:28:17: error: ‘decode_cyclic’ was not d eclared in this scope    28 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ 78cb67aa-9f31-4af4-9b80-0ecc01a392c5.cpp:28:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    28 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,29 +1,35 @@
-string encode_cyclic(string str) {
-    string output;
-    int l = str.length();
-    for (int i = 0; i < l; i += 3) {
-        string x = str.substr(i, min(3, l - i));
-        if (x.length() == 3) x = x.substr(1, 2) + x[0];
-        output += x;
+#include <string>
+#include <cassert>
+
+std::string encode_cyclic(std::string s){
+    int l=s.length();
+    std::string x,output;
+    int i;
+    for (i=0;i*3<l;i++)
+    {
+        x=s.substr(i*3,3);
+        if (x.length()==3) x=x.substr(1,2)+x[0];
+        output=output+x;
     }
     return output;
 }
 
-string decode_cyclic(string s) { 
-    int l = s.length();
-    string x, output;
-    for (int i = 0; i * 3 < l; i++)
+std::string decode_cyclic(std::string s){ 
+    int l=s.length();
+    std::string x,output;
+    int i;
+    for (i=0;i*3<l;i++)
     {
-        x = s.substr(i * 3, 3);
-        if (x.length() == 3) x = x[2] + x.substr(0, 2);
-        output += x;
+        x=s.substr(i*3,3);
+        if (x.length()==3) x=x[2]+x.substr(0,2);
+        output=output+x;
     }
     return output;
 }
 
 int main() {
-    string str = "your_input_string_here";
-    string encoded_str = encode_cyclic(str);
+    std::string str = "input_string";
+    std::string encoded_str = encode_cyclic(str);
     assert(decode_cyclic(encoded_str) == str);
     return 0;
 }
