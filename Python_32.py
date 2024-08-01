d3c3076583d7d8f052d41d29afd81bc9bcd81230
beep boop <githubbot@vadim.me>
2024-08-01 19:11:08 +0200

    Solution 2 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/je81nrft Prompt: Complete the following code given the task description and function signature.

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,5 @@
 def find_zero(xs: list):
     n = len(xs) - 1
-    if n % 2 == 1:
-        n -= 1
-    for i in range(0, n, 2):
-        if xs[i] * xs[i + 2] < 0:
-            return -xs[i + 1] / xs[i]
-    return None
+    a = xs[-1]
+    b = xs[-2]
+    return -b / a
