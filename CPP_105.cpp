d7ff40def2e300a3c36db20b62a3e95018cb4409
beep boop <githubbot@vadim.me>
2024-08-01 23:07:31 +0200

    Solution 6 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/jf4bwywo Prompt:       |                              ^~~~~~ a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp:27:37: error: ‘string’ was not declared  in this scope    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp:26: a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp: In function ‘int main()’: a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp:36:20: error: ‘by_length’ was not decla red in this scope    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ a1c33f60-3938-4913-8ea4-b9dd7d6bd6f3.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,12 +1,23 @@
-#include <algorithm>
+#include <vector>
+#include <string>
 #include <map>
+#include <algorithm>
+
+bool issame(vector<string> a, vector<string> b);
+
+vector<string> by_length(vector<int> arr);
+
+bool issame(vector<string> a, vector<string> b){
+    return a == b;
+}
 
 vector<string> by_length(vector<int> arr){
-    vector<string> result;
     vector<int> sorted_arr;
-    map<int, string> num_to_name = {
-        {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"},
-        {5, "Five"}, {6, "Six"}, {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
+    vector<string> result;
+    map<int, string> int_to_string = {
+        {1, "One"}, {2, "Two"}, {3, "Three"},
+        {4, "Four"}, {5, "Five"}, {6, "Six"},
+        {7, "Seven"}, {8, "Eight"}, {9, "Nine"}
     };
 
     for (int num : arr) {
@@ -16,39 +27,11 @@ vector<string> by_length(vector<int> arr){
     }
 
     sort(sorted_arr.begin(), sorted_arr.end());
-
     reverse(sorted_arr.begin(), sorted_arr.end());
 
     for (int num : sorted_arr) {
-        result.push_back(num_to_name[num]);
+        result.push_back(int_to_string[num]);
     }
 
     return result;
-}
-
-bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    vector<int> input = {3, 7, 1, 9, 4, 5};
-    vector<string> result = by_length(input);
-
-    vector<string> expected = {"Nine", "Seven", "Four", "Three", "One"};
-
-    if (issame(result, expected)) {
-        cout << "Result matched the expected output" << endl;
-    } else {
-        cout << "Result did not match the expected output" << endl;
-    }
-
-    return 0;
 }
