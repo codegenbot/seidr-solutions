bfbc2fcb4a43d2f8bd63a8c4f716b7c0f2e53de0
beep boop <githubbot@vadim.me>
2024-08-01 13:22:17 +0200

    Solution 51 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zljys8df Prompt:    10 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp:10:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp:10:44: error: ‘b’ was not declared in t his scope    10 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp:9: e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp: In function ‘int main()’: e7813913-b3f0-43da-8a14-1ac29deaf40a.cpp:19:20: error: ‘separate_paren_groups’ w as not declared in this scope    19 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,34 +1,21 @@
 #include <vector>
 #include <string>
 #include <cassert>
-#include <stack>
 
-std::vector<std::string> separate_paren_groups(const std::string& input) {
-    std::stack<char> stk;
-    std::string current_group;
-    std::vector<std::string> groups;
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    return a == b;
+}
 
-    for (char c : input) {
-        if (c == '(') {
-            stk.push(c);
-            if (stk.size() > 1) {
-                current_group += c;
-            }
-        } else if (c == ')') {
-            stk.pop();
-            if (stk.empty()) {
-                groups.push_back(current_group);
-                current_group = "";
-            } else {
-                current_group += c;
-            }
-        }
-    }
+std::vector<std::string> separate_paren_groups(const std::string& str) {
+    std::vector<std::string> result;
 
-    return groups;
+    // Add implementation here
+
+    return result;
 }
 
 int main() {
-    assert (issame(separate_paren_groups("( ) (( )) (( )( ))") , {"()", "(())", "(()())"}));
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
+
     return 0;
 }
