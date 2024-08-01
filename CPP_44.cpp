66df01afdb0d2b91ccbb927a20d8f1c03747ab9d
beep boop <githubbot@vadim.me>
2024-08-01 18:18:04 +0200

    Solution 68 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sl0y4p88 Prompt: f37ba524-464d-42ff-98a6-a54b4907bd57.cpp: In function ‘std::string change_base(i nt, int)’: f37ba524-464d-42ff-98a6-a54b4907bd57.cpp:5:17: error: ‘std::std’ has not been de clared     5 |     return std::std::to_string(num);       |                 ^~~ In file included from f37ba524-464d-42ff-98a6-a54b4907bd57.cpp:8: f37ba524-464d-42ff-98a6-a54b4907bd57.cpp: In function ‘int main()’: f37ba524-464d-42ff-98a6-a54b4907bd57.cpp:12:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    12 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from f37ba524-464d-42ff-98a6-a54b4907bd57.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -5,8 +5,4 @@ std::string change_base(int num, int base){
     return std::to_string(num);
 }
 
-int main() {
-    int x = 10;
-    assert(change_base(x, x + 1) == std::to_string(x));
-    return 0;
-}
+assert(change_base(num, base) == std::to_string(num));
