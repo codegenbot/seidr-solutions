1214c339bb59ee859a40d02304709d7aeb352980
beep boop <githubbot@vadim.me>
2024-08-01 16:25:32 +0200

    Solution 84 of Python/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/yqssvsep Prompt: Program did not receive expected input

--- Python_9.py
+++ Python_9.py
@@ -1,3 +1,6 @@
 def rolling_max(numbers: List[int]) -> List[int]:
-    result = [max(max_subarray) for max_subarray in (numbers[:i+1] for i in range(len(numbers)))]
+    result = [
+        max(max_subarray)
+        for max_subarray in (numbers[: i + 1] for i in range(len(numbers)))
+    ]
     return result
