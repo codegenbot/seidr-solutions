bad5e2d5487db1572e2f3cc686c56686012e612e
beep boop <githubbot@vadim.me>
2024-08-01 17:42:14 +0200

    Solution 5 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qdcnp3g2 Prompt: Complete the following code given the task description and function signature.

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,9 +1,10 @@
 int search(vector<int> lst) {
-    int max_freq = 0;
-    for (int num : lst) {
-        if (num > 0 && count(lst.begin(), lst.end(), num) >= num) {
-            return num;
+    int max = 0;
+    for (int i : lst) {
+        if (i > 0 && count(lst.begin(), lst.end(), i) >= i) {
+            max = i;
+            break;
         }
     }
-    return -1;
+    return max == 0 ? -1 : max;
 }
