b7312b474cd4056110857cfa79bd751820a878f9
beep boop <githubbot@vadim.me>
2024-08-02 00:02:27 +0200

    Solution 80 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/a2hp0s96 Prompt:     1 | // std::initializer_list support -*- C++ -*- In file included from /usr/include/c++/11/bits/stl_vector.h:63,                  from /usr/include/c++/11/vector:67,                  from 4ac90abe-ef99-4b1a-a6e7-5be3e1b7b87a.cpp:3: /usr/include/c++/11/initializer_list:59:7: error: ‘size_type’ does not name a ty pe; did you mean ‘true_type’?    59 |       size_type                 _M_len;       |       ^~~~~~~~~       |       true_type /usr/include/c++/11/initializer_list:62:54: error: ‘size_type’ has not been decl ared    62 |       constexpr initializer_list(const_iterator __a, size_type __l)       |                                                      ^~~~~~~~~ /usr/include/c++/11/initializer_list:70:17: error: ‘size_type’ does not name a t ype; did you mean ‘true_type’?    70 |       constexpr size_type       |                 ^~~~~~~~~       |                 true_type /usr/include/c++/11/initializer_list:47:11: fatal error: definition of ‘class st d::initializer_list<_E>’ does not match ‘#include <initializer_list>’    47 |     class initializer_list       |           ^~~~~~~~~~~~~~~~ compilation terminated.

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,31 +1,28 @@
+#include <iostream>
+#include <string>
+
 std::string kebabToCamel(const std::string& s) {
-    std::string result = "";
+    std::string result;
     bool capitalize = true;
-    std::string word = "";
 
     for (char c : s) {
         if (c == '-') {
-            result += toupper(word[0]);
-            word = "";
             capitalize = true;
+        } else if (capitalize) {
+            result += toupper(c);
+            capitalize = false;
         } else {
-            word += c;
-            if (!capitalize) {
-                result += tolower(c);
-                capitalize = !capitalize;
-            } else {
-                result += c;
-            }
+            result += tolower(c);
         }
     }
 
-    if (word.empty()) {
-        return result;
-    } else {
-        result += toupper(word[0]);
-        for (int i = 1; i < word.size(); ++i) {
-            result += tolower(word[i]);
-        }
-        return result;
-    }
+    return result;
+}
+
+int main() {
+    std::string input;
+    std::cin >> input;
+    std::cout << kebabToCamel(input) << std::endl;
+
+    return 0;
 }
