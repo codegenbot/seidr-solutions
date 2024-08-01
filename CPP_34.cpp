a77c403fc43e34c4bd65539248be1c83f2ea359d
beep boop <githubbot@vadim.me>
2024-08-01 16:48:46 +0200

    Solution 4 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/c86lrel9 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 104bffcd-bea0-4ad9-88e2-94824e213990.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 104bffcd-bea0-4ad9-88e2-94824e213990.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 104bffcd-bea0-4ad9-88e2-94824e213990.cpp:5: 104bffcd-bea0-4ad9-88e2-94824e213990.cpp: In function ‘int main()’: 104bffcd-bea0-4ad9-88e2-94824e213990.cpp:15:20: error: ‘unique’ was not declared  in this scope    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 104bffcd-bea0-4ad9-88e2-94824e213990.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,15 +1,5 @@
-#include <algorithm>
-#include <cassert>
-#include <vector>
-
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
+bool issame(const vector<int>& a, const vector<int>& b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
     return a == b;
-}
-
-int main() {
-    std::vector<int> l = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    sort(l.begin(), l.end());
-    l.erase(unique(l.begin(), l.end()), l.end());
-    assert(issame(l, {0, 2, 3, 5, 9, 123}));
-    return 0;
 }
