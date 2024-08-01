dab5f6cfb632d23a05cfd9eb2fda214e56222b8e
beep boop <githubbot@vadim.me>
2024-08-01 18:56:12 +0200

    Solution 12 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ahx9w45c Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,10 +1,9 @@
-```
 def decode_cyclic(s: str):
     result = ""
     while len(s) > 1:
         if s[0] != s[-1]:
             result += s[0]
         else:
-            result += s[:len(s)//2] + s[-1]
-        s = s[len(s)//2:]
+            result += s[: len(s) // 2] + s[-1]
+        s = s[len(s) // 2 :]
     return result + s
