faf82b5732461606fc4dc1ae7a37631e467db0f6
beep boop <githubbot@vadim.me>
2024-08-01 18:27:58 +0200

    Solution 11 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/n1cyrks1 Prompt: 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp:11:5: error: ‘assert’ was not declared in this scope    11 |     assert (!below_threshold({1, 8, 4, 10}, 10));       |     ^~~~~~ 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp:1:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?   +++ |+#include <cassert>     1 | bool below_threshold(std::vector<int> l, int t) { 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp: At global scope: 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp:15:5: error: redefinition of ‘int main( )’    15 | int main(){       |     ^~~~ 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp:14: 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp: In function ‘int main()’: 3fb1cac7-9c40-4b19-a5fd-184ed2c35d42.cpp:16:32: error: ‘below_threshold’ cannot be used as a function    16 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -5,9 +5,10 @@ bool below_threshold(std::vector<int> l, int t) {
             return false;
         }
     }
+    
     return true;
 }
 
-int main(){
+int main() {
     assert(!below_threshold({1, 8, 4, 10}, 10));
 }
