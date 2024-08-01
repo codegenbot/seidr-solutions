b6eec4971f45d5972482b2b25e17b41cf85b3f85
beep boop <githubbot@vadim.me>
2024-08-02 00:44:46 +0200

    Solution 50 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nh1fhueo Prompt: 4fad6f20-04c1-4284-8047-f79ef208c226.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | bool is_sorted(vector<int> lst){       |                ^~~~~~ 4fad6f20-04c1-4284-8047-f79ef208c226.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | bool is_sorted(vector<int> lst){       |                       ^~~ In file included from 4fad6f20-04c1-4284-8047-f79ef208c226.cpp:12: 4fad6f20-04c1-4284-8047-f79ef208c226.cpp: In function ‘int main()’: 4fad6f20-04c1-4284-8047-f79ef208c226.cpp:14:22: error: ‘is_sorted’ cannot be use d as a function    14 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ~~~~~~~~~^~~~~~~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,8 +1,9 @@
+#include <vector>
+
 bool is_sorted(vector<int> lst){
-    for(int i = 0; i < lst.size() - 1; i++){
-        if(lst[i] >= lst[i+1]){
-            if(count(lst.begin(), lst.end(), lst[i]) > 1)
-                return false;
+    for(int i = 1; i < lst.size(); i++){
+        if(lst[i] <= lst[i-1]){
+            return false;
         }
     }
     return true;
