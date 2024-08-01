71aad734f9cc7ec025ec1aafb8d1ee401b95194f
beep boop <githubbot@vadim.me>
2024-08-01 18:36:44 +0200

    Solution 48 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rs9p68sc Prompt:       |                           ^~~~~~ 12aa20d8-c3bc-4e7d-a0ec-48ce9f2ddb79.cpp:48:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 12aa20d8-c3bc-4e7d-a0ec-48ce9f2ddb79.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 12aa20d8-c3bc-4e7d-a0ec-48ce9f2ddb79.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 12aa20d8-c3bc-4e7d-a0ec-48ce9f2ddb79.cpp:48:34: error: expected primary-expressi on before ‘int’    48 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 12aa20d8-c3bc-4e7d-a0ec-48ce9f2ddb79.cpp:56:5: error: redefinition of ‘int main( )’    56 | int main(){       |     ^~~~ 12aa20d8-c3bc-4e7d-a0ec-48ce9f2ddb79.cpp:42:5: note: ‘int main()’ previously def ined here    42 | int main() {       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -26,10 +26,19 @@ std::vector<int> parse_music(std::string music_string) {
 }
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-int main() {
+void main() {
     assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
-    return 0;
 }
