ff312e701f60eeb120ca9d45aca9fbf01ba89a4e
beep boop <githubbot@vadim.me>
2024-08-01 16:10:01 +0200

    Solution 4 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/mizhsbd7 Prompt: Given a text string and a target string, return a list of integers of the indices at which the target appears in the text. Targets may overlap.

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -3,25 +3,49 @@ using namespace std;
 
 vector<int> indicesOfSubstring(string text, string target) {
     vector<int> result;
-    int n = text.size();
-    int m = target.size();
-
-    for (int i = 0; i <= n - m; i++) {
-        if (text.substr(i, m) == target)
+    for (int i = 0; i <= text.size() - target.size(); i++) {
+        bool found = true;
+        for (int j = 0; j < target.size(); j++) {
+            if (text[i + j] != target[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
             result.push_back(i);
+        }
     }
     return result;
 }
 
 int main() {
-    string text;
-    cin >> text;
-    string target;
-    cin >> target;
+    // Test cases
+    string text1 = "a";
+    string target1 = "5";
+    vector<int> output1 = indicesOfSubstring(text1, target1);
+    cout << output1[0] << endl;
 
-    vector<int> indices = indicesOfSubstring(text, target);
+    string text2 = "!";
+    string target2 = "!!";
+    vector<int> output2 = indicesOfSubstring(text2, target2);
+    for (int i : output2) {
+        cout << i << endl;
+    }
 
-    for (int i : indices) {
+    string text3 = "rnm,xcnwqnd@#$fwkdjn3";
+    string target3 = "r";
+    vector<int> output3 = indicesOfSubstring(text3, target3);
+    cout << output3[0] << endl;
+
+    string text4 = "hihihihihihihihihihi";
+    string target4 = "hi";
+    vector<int> output4 = indicesOfSubstring(text4, target4);
+    cout << output4[0] << endl;
+
+    string text5 = "############";
+    string target5 = "#";
+    vector<int> output5 = indicesOfSubstring(text5, target5);
+    for (int i : output5) {
         cout << i << endl;
     }
 
