c82e4523be56043e0350c93f0fe3a96c4c845326
beep boop <githubbot@vadim.me>
2024-08-01 17:20:15 +0200

    Solution 2 of CPP/67, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/vt4u5cry Prompt: Complete the following code given the task description and function signature.

--- CPP_67.cpp
+++ CPP_67.cpp
@@ -1,37 +1,26 @@
-#include <iostream>
 #include <string>
 using namespace std;
 
 int fruit_distribution(string s, int n) {
     size_t pos = 0;
     int apples = 0, oranges = 0;
-
+    
     while ((pos = s.find(" and ")) != string::npos) {
         if (s.substr(0, pos).find("apples") != string::npos)
-            apples = stoi(s.substr(0, pos).substr(s.find(" ") + 1));
-        else
-            oranges = stoi(s.substr(0, pos).substr(s.find(" ") + 1));
-
+            apples = stoi(s.substr(0, pos).substr(0, s.find(" ")).replace("apples", ""));
+        else if (s.substr(0, pos).find("oranges") != string::npos)
+            oranges = stoi(s.substr(0, pos).substr(0, s.find(" ")).replace("oranges", ""));
+        
         s.erase(0, pos + 5);
     }
-
-    if (s.find("apples") != string::npos)
-        apples = stoi(s.substr(0, s.find(" ")).substr(s.find(" ") + 1));
-    else
-        oranges = stoi(s.substr(0, s.find(" ")).substr(s.find(" ") + 1));
-
+    
+    if (s.find("apples") != string::npos) {
+        apples = stoi(s.substr(0, s.find(" ")).replace("apples", ""));
+        s.erase(0, s.find(" ") + 1);
+    } else if (s.find("oranges") != string::npos) {
+        oranges = stoi(s.substr(0, s.find(" ")).replace("oranges", ""));
+        s.erase(0, s.find(" ") + 1);
+    }
+    
     return n - apples - oranges;
-}
-
-int main() {
-    string s;
-    int n;
-    cout << "Enter a string that represents the total number of the apples and oranges: ";
-    cin >> s;
-    cout << "Enter an integer that represent the total number of the fruits in the basket: ";
-    cin >> n;
-
-    cout << "The number of mango fruits in the basket is: " << fruit_distribution(s, n) << endl;
-
-    return 0;
 }
