3c137dfb37fceaecc19b346fcd55fd33a8575e9d
beep boop <githubbot@vadim.me>
2024-08-01 18:52:00 +0200

    Solution 39 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/age7qfk6 Prompt:       |                              ^~~~~~ aaf87184-f869-42a7-b902-767676252571.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ aaf87184-f869-42a7-b902-767676252571.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ aaf87184-f869-42a7-b902-767676252571.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from aaf87184-f869-42a7-b902-767676252571.cpp:32: aaf87184-f869-42a7-b902-767676252571.cpp: In function ‘int main()’: aaf87184-f869-42a7-b902-767676252571.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ aaf87184-f869-42a7-b902-767676252571.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <vector>
 #include <string>
 
@@ -28,7 +27,7 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
         else if (grade > 0.7)
             letter_grades.push_back("D");
         else
-            letter_grades.push_back("F");
+            letter_grades.push_back("E");
     }
     return letter_grades;
 }
@@ -38,6 +37,6 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
 }
 
 int main() {
-    assert(issame(numerical_letter_grade({0.0f}), {"F"}));
+    assert(issame(numerical_letter_grade({0.0f}), {"E"}));
     return 0;
 }
