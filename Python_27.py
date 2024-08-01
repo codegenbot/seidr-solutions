83f0ea93365c814f091d12bc68a373d81b1c4fc0
beep boop <githubbot@vadim.me>
2024-08-01 18:24:47 +0200

    Solution 44 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/d5dyfb7p Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/59698038-0db3-4916-a9ac-61d0c74c8 c9c.py", line 10, in <module>     check(flip_case)           ^^^^^^^^^ NameError: name 'flip_case' is not defined. Did you mean: 'swap_case'?

--- Python_27.py
+++ Python_27.py
@@ -1,2 +1,12 @@
-def swap_case(string):
-    return "".join(char.lower() if char.isupper() else char.upper() for char in string)
+def check(swap_case):   
+    def validate(swap_case):
+        try:
+            assert swap_case('Hello World') == 'hELLO wORLD'
+            assert swap_case('Python Programming') == 'pYTHON pROGRAMMING'
+            assert swap_case('iNVERSE cASE') == 'Inverse Case'
+            print("All test cases pass")
+        except:
+            print("Test failed")
+
+    validate(swap_case)
+```
