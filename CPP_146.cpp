b1d03693fea8287b8a164604b492ed298bb07d56
beep boop <githubbot@vadim.me>
2024-08-02 00:24:56 +0200

    Solution 38 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qfc2ehqe Prompt: 95c4ad08-8fe9-4f42-846e-bd213728b0cb.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ 95c4ad08-8fe9-4f42-846e-bd213728b0cb.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from 95c4ad08-8fe9-4f42-846e-bd213728b0cb.cpp:11: 95c4ad08-8fe9-4f42-846e-bd213728b0cb.cpp: In function ‘int main()’: 95c4ad08-8fe9-4f42-846e-bd213728b0cb.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <iostream>
 
 int specialFilter(vector<int> nums) {
     int count = 0;
@@ -12,8 +11,8 @@ int specialFilter(vector<int> nums) {
 }
 
 int main() {
-    vector<int> nums = {11, 12, 13, 14, 15};
+    vector<int> nums = {1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
     int count = specialFilter(nums);
-    std::cout << "The count is: " << count << std::endl;
+    cout << "The number of integers in the range [10, 20] that are not divisible by 2 is: " << count;
     return 0;
 }
