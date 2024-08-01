a496cf7dcdaf47a99c07357b8ec63afd1785ff4b
beep boop <githubbot@vadim.me>
2024-08-01 13:26:07 +0200

    Solution 37 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vck9hlvv Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a01479d7-d7d7-46eb-9190-b3bafa791 8b1.py", line 11, in <module>     check(intersperse)           ^^^^^^^^^^^ NameError: name 'intersperse' is not defined

--- Python_5.py
+++ Python_5.py
@@ -1,2 +1,4 @@
 def fix_code(numbers, delimiter):
-    return [item for sublist in zip(numbers, [delimiter] * len(numbers)) for item in sublist]
+    return [
+        item for sublist in zip(numbers, [delimiter] * len(numbers)) for item in sublist
+    ]
