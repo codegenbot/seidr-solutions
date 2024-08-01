6af05fa79476367194a8df8cc6045e4f0560e201
beep boop <githubbot@vadim.me>
2024-08-02 00:13:35 +0200

    Solution 7 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/tj4t46fa Prompt: f1860100-3674-48ff-941f-d12440caa385.cpp:1:12: error: ‘vector’ was not declared in this scope     1 | int search(vector<int> lst) {       |            ^~~~~~ f1860100-3674-48ff-941f-d12440caa385.cpp:1:19: error: expected primary-expressio n before ‘int’     1 | int search(vector<int> lst) {       |                   ^~~ In file included from f1860100-3674-48ff-941f-d12440caa385.cpp:15: f1860100-3674-48ff-941f-d12440caa385.cpp: In function ‘int main()’: f1860100-3674-48ff-941f-d12440caa385.cpp:17:19: error: ‘search’ cannot be used a s a function    17 |     assert (search({3, 10, 10, 9, 2}) == -1);       |             ~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -4,13 +4,13 @@
 int search(vector<int> lst) {
     int maxFreqNum = -1;
     unordered_map<int, int> freqMap;
-
+    
     for (int num : lst) {
         freqMap[num]++;
         if (freqMap[num] >= freqMap[maxFreqNum] && num > maxFreqNum) {
             maxFreqNum = num;
         }
     }
-
+    
     return maxFreqNum;
 }
