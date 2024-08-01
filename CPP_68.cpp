9822a32f1a42c2b11e848965d726ab3f742268af
beep boop <githubbot@vadim.me>
2024-08-01 19:13:14 +0200

    Solution 96 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/pn1w0qyv Prompt:       |                    ^~~ 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp:26:27: error: ‘vector’ was not declared  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp:26:39: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp:25: 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp: In function ‘int main()’: 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp:35:20: error: ‘pluck’ was not declared in this scope    35 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 6b432c35-e9ef-4f74-9c2c-8d6455f81d43.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,9 +1,8 @@
 #include <vector>
-#include <iostream>
-using namespace std;
+#include <algorithm>
 
-vector<int> pluck(vector<int> arr) {
-    vector<int> result;
+std::vector<int> pluck(std::vector<int> arr) {
+    std::vector<int> result;
     if (arr.empty()) {
         return result;
     }
