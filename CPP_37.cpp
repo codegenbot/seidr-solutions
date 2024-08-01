e28c1a608b704f6c990508ccb25153f2ff3980f0
beep boop <githubbot@vadim.me>
2024-08-01 17:50:06 +0200

    Solution 77 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/00fqiu6f Prompt: ’    28 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from 15ea4368-1ee6-48ac-a8b6-c20873096a3f.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 15ea4368-1ee6-48ac-a8b6-c20873096a3f.cpp:5:34: note:   initializing argument 1 o f ‘void sort_even(std::vector<int>&)’     5 | void sort_even(std::vector<int>& l) {       |                ~~~~~~~~~~~~~~~~~~^ In file included from 15ea4368-1ee6-48ac-a8b6-c20873096a3f.cpp:18: 15ea4368-1ee6-48ac-a8b6-c20873096a3f.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -9,8 +9,7 @@ void sort_even(std::vector<int>& l) {
 }
 
 int main() {
-    std::vector<int> vec;
-    vec = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
+    std::vector<int> vec = std::vector<int>{5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
     sort_even(vec); 
     assert(vec == std::vector<int>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}); 
     return 0;
