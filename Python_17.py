9a7ea0a1b02ad777f4b1b01549d7d2c29b0657a7
beep boop <githubbot@vadim.me>
2024-08-01 16:01:37 +0200

    Solution 80 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ggmcmzy6 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/a79b0959-cc43-4848-9be2-43013f03a 8bc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,15 +1,25 @@
-```python
-def parse_music(music_string: str) -> list:
+```
+def parse_music(music_string: str) -> List[int]:
     music_list = []
     i = 0
     while i < len(music_string):
         if music_string[i:i+2] == 'o|':
             music_list.append(4)
             i += 2
-        elif music_string[i:i+3] == '.||':
+        elif music_string[i:i+3] == '.|..':
             music_list.append(1)
             i += 3
-        elif music_string[i:i+5] == '..|.||':
-            music_list.append(0)
-            i += 4
+        elif music_string[i:i+3] == '..|.':
+            music_list.append(2)
+            i += 3
+        else:
+            if music_string[i] == 'o':
+                music_list.append(4)
+                i += 1
+            elif music_string[i] == '.':
+                music_list.append(1)
+                i += 1
+            elif music_string[i] == '|':
+                music_list.append(2)
+                i += 1
     return music_list
