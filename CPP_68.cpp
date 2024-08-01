70ec302152634362778680b0435370b9ba732f5f
beep boop <githubbot@vadim.me>
2024-08-02 00:09:44 +0200

    Solution 9 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/htnurhyh Prompt:       |                    ^~~ 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp:21: 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp: In function ‘int main()’: 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 843aa59a-2cfd-4928-85c9-b40066c312b4.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -7,10 +7,28 @@ bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-vector<int> pluck(vector<int> arr);
+vector<int> pluck(vector<int> arr){
+    vector<int> result;
+    int smallestEven = -1;
+    int smallestEvenIndex = -1;
+    
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] % 2 == 0 && (smallestEven == -1 || arr[i] < smallestEven)){
+            smallestEven = arr[i];
+            smallestEvenIndex = i;
+        }
+    }
+    
+    if(smallestEven != -1){
+        result.push_back(smallestEven);
+        result.push_back(smallestEvenIndex);
+    }
+    
+    return result;
+}
 
 int main(){
     assert(issame(pluck({7, 9, 7, 1}), {}));
     
     return 0;
-}
+} 
