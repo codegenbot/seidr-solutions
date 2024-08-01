8f8a3277c317dae103717d9324ce324686ed6f5c
beep boop <githubbot@vadim.me>
2024-08-02 01:29:10 +0200

    Solution 5 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/umc6va5l Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,52 +1,21 @@
 int do_algebra(vector<string> operator_, vector<int> operand) {
     int result = 0;
-    string expression = "";
+    int temp = operand[0];
     
-    for(int i = 0; i < operator_.size(); i++) {
-        if(i == 0)
-            expression += to_string(operand[i]);
-        else if(i == operator_.size() - 1)
-            expression += operator_[i] + to_string(operand[i+1]);
-        else
-            expression += " " + operator_[i] + " ";
-    }
-    
-    int temp = eval(expression);
-    return temp;
-}
-
-int eval(string s) {
-    int result = 0;
-    string num = "";
-    for(int i = 0; i < s.length(); i++) {
-        if(isdigit(s[i])) {
-            num += s[i];
-        } else {
-            if(!num.empty()) {
-                if(num == "1") result++;
-                else if(num == "2") result *= 2;
-                else if(num == "3") result *= 3;
-                else if(num == "4") result *= 4;
-                else if(num == "5") result *= 5;
-                else result = pow(result, stoi(num));
-                num.clear();
-            }
-            if(s[i] != ' ') {
-                if(s[i] == '+') result++;
-                else if(s[i] == '-') result--;
-                else if(s[i] == '*') result *= 2;
-                else if(s[i] == '/') result /= 2;
-                else if(s[i] == '**') result = pow(result, 2);
-            }
+    for (int i = 1; i < operand.size(); i++) {
+        if (*operator_.begin() == "+") {
+            result += temp + operand[i];
+        } else if (*operator_.begin() == "-") {
+            result -= temp - operand[i];
+        } else if (*operator_.begin() == "*") {
+            result *= temp * operand[i];
+        } else if (*operator_.begin() == "//") {
+            result = temp / (operand[i] + 0.0);
+        } else if (*operator_.begin() == "**") {
+            result = pow(temp, operand[i]);
         }
-    }
-    if(!num.empty()) {
-        if(num == "1") result++;
-        else if(num == "2") result *= 2;
-        else if(num == "3") result *= 3;
-        else if(num == "4") result *= 4;
-        else if(num == "5") result *= 5;
-        else result = pow(result, stoi(num));
+        
+        operator_.erase(operator_.begin());
     }
     
     return result;
