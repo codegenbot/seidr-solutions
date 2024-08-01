667ac84ba00cca7634155dadf60bb67206f74e62
beep boop <githubbot@vadim.me>
2024-08-02 01:18:24 +0200

    Solution 4 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/559u3zuu Prompt: 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp:12:9: error: expected constructor, dest ructor, or type conversion before ‘(’ token    12 |     sort(word.begin(), word.end());       |         ^ 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp:13:5: error: ‘result’ does not name a t ype    13 |     result += word;       |     ^~~~~~ 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return result;       |     ^~~~~~ 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp:17: 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp: In function ‘int main()’: 7ee4a051-cf8e-4e86-a7ba-94da91e2f0cb.cpp:19:13: error: ‘anti_shuffle’ was not de clared in this scope    19 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,16 +1,16 @@
-string original_order(string s) {
+string anti_shuffle(const string& s) {
     string result = "";
     string word = "";
     for (char c : s) {
         if (c == ' ') {
-            sort(word.begin(), word.end(), greater<char>());
+            sort(word.begin(), word.end());
             result += word + ' ';
             word = "";
         } else {
             word += c;
         }
     }
-    sort(word.begin(), word.end(), greater<char>());
+    sort(word.begin(), word.end());
     result += word;
     return result;
 }
