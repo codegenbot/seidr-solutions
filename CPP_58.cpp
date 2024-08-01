056b11345c3fa71c3abff70bd407812958813fdc
beep boop <githubbot@vadim.me>
2024-08-01 19:02:21 +0200

    Solution 35 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/hx9fx2ps Prompt:     7 | 79323d18-b2bb-4c34-9b21-7c400a65e84a.cpp:34:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    34 | int main(){       |         ^~ 79323d18-b2bb-4c34-9b21-7c400a65e84a.cpp:34:9: note: remove parentheses to defau lt-initialize a variable    34 | int main(){       |         ^~       |         -- 79323d18-b2bb-4c34-9b21-7c400a65e84a.cpp:34:9: note: or replace parentheses with  braces to value-initialize a variable 79323d18-b2bb-4c34-9b21-7c400a65e84a.cpp:34:11: error: a function-definition is not allowed here before ‘{’ token    34 | int main(){       |           ^ 79323d18-b2bb-4c34-9b21-7c400a65e84a.cpp:36:2: error: expected ‘}’ at end of inp ut    36 | }       |  ^ 79323d18-b2bb-4c34-9b21-7c400a65e84a.cpp:20:12: note: to match this ‘{’    20 | int main() {       |            ^

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,4 +1,4 @@
-int main() {    
-    assert(issame(static_cast<int>(common({4, 3, 2, 8}, ({})).size()), static_cast<int>(std::vector<int>().size()));
+int main() (
+    assert(issame(static_cast<int>(common({4, 3, 2, 8}, {}).size()), static_cast<int>(std::vector<int>{}.size()));
     return 0;
-}
+)
