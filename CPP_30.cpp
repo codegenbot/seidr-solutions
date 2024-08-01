c742bd209fa5cee256d3555fdad6f2e445d31fd8
beep boop <githubbot@vadim.me>
2024-08-01 16:36:39 +0200

    Solution 50 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/md4dv4mg Prompt:       |             ^~~~~~ 8ea04bd8-6ad3-4a9b-b4cf-7d7c0c666e85.cpp:22:20: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 8ea04bd8-6ad3-4a9b-b4cf-7d7c0c666e85.cpp:22:29: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 8ea04bd8-6ad3-4a9b-b4cf-7d7c0c666e85.cpp:22:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 8ea04bd8-6ad3-4a9b-b4cf-7d7c0c666e85.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8ea04bd8-6ad3-4a9b-b4cf-7d7c0c666e85.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8ea04bd8-6ad3-4a9b-b4cf-7d7c0c666e85.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -17,8 +17,3 @@ std::vector<float> get_positive(const std::vector<float>& input) {
 
     return positive_values;
 }
-
-int main() {
-    assert(issame(get_positive({}), {}));
-    return 0;
-}    
