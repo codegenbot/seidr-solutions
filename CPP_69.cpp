547089748d7c565670d386c4aa25643220c62272
beep boop <githubbot@vadim.me>
2024-08-01 17:56:36 +0200

    Solution 4 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/ncu24k6d Prompt: Complete the following code given the task description and function signature.

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,8 +1,8 @@
 int search(vector<int> lst) {
     int max = 0;
-    for (int i : lst) {
-        if (i > 0 && i >= count(i)) {
-            max = i;
+    for (auto x : lst) {
+        if (x > 0 && x >= max) {
+            max = x;
         }
     }
     return max == 0 ? -1 : max;
