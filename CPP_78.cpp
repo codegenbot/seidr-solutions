fe1652e1a5950c26fffd3489d3e7881c6242fa3b
beep boop <githubbot@vadim.me>
2024-08-02 00:53:26 +0200

    Solution 2 of CPP/78, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4r14y84p Prompt: fb8739ae-2729-4dc9-bc9f-6414bacb6521.cpp:1:13: error: ‘string’ was not declared in this scope     1 | int hex_key(string num){       |             ^~~~~~ In file included from fb8739ae-2729-4dc9-bc9f-6414bacb6521.cpp:14: fb8739ae-2729-4dc9-bc9f-6414bacb6521.cpp: In function ‘int main()’: fb8739ae-2729-4dc9-bc9f-6414bacb6521.cpp:16:20: error: ‘hex_key’ cannot be used as a function    16 |     assert (hex_key("") == 0);       |             ~~~~~~~^~~~

--- CPP_78.cpp
+++ CPP_78.cpp
@@ -1,10 +1,14 @@
-int hex_key(string num){
+#include <string>
+
+int hex_key(std::string num){
     int count = 0;
-    string primes = "2357BD";
+    std::string primes = "2357BD";
+    
     for(char c : num){
-        if(primes.find(c) != string::npos){
+        if(primes.find(c) != std::string::npos){
             count++;
         }
     }
+    
     return count;
 }
