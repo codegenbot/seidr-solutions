b6ee9c817a846bfde3a711ec2671982bb2ba9c8e
beep boop <githubbot@vadim.me>
2024-08-02 01:14:49 +0200

    Solution 55 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/wgqqort4 Prompt: 607e071c-6413-404b-a4ef-109849248b1c.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 607e071c-6413-404b-a4ef-109849248b1c.cpp:9:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     9 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 607e071c-6413-404b-a4ef-109849248b1c.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 607e071c-6413-404b-a4ef-109849248b1c.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -14,8 +14,3 @@ vector<int> maximum(vector<int> arr, int k) {
     sort(arr.begin(), arr.end(), greater<int>());
     return vector<int>(arr.begin(), arr.begin() + k);
 }
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
-    return 0;
-}
