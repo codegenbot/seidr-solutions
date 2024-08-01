c4d44c8ec77a0c60d3fc654b6803d0ce1af0e541
beep boop <githubbot@vadim.me>
2024-08-01 15:08:03 +0200

    Solution 79 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qmabel6g Prompt: 0b1516c9-6130-49f0-bc3f-cb992a32118a.cpp:1:10: fatal error: boost/any_cast.hpp: No such file or directory     1 | #include <boost/any_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -3,7 +3,7 @@
 vector<int> filter_integers(list<any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if(boost::any_cast<bool>(value)) {
+        if (boost::any_cast<bool>(value)) {
             int integer_value = boost::any_cast<int>(value);
             result.push_back(integer_value);
         }
