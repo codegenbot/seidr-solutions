d99801f7da7d4c16be81f3ea65487bc2813efa4f
beep boop <githubbot@vadim.me>
2024-08-01 18:17:59 +0200

    Solution 15 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sfcckw9a Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/976a5d38-975f-451d-95c6-5a85096f7 1e6.py", line 1     return [num for num in numbers if numbers.count(num) == 1]     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_26.py
+++ Python_26.py
@@ -2,5 +2,4 @@ def find_unique_numbers(numbers):
     return [num for num in numbers if numbers.count(num) == 1]
 
 numbers = list(map(int, input().split()))
-result = find_unique_numbers(numbers)
-print(result)
+print(find_unique_numbers(numbers))
