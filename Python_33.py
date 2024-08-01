bfab24a2b034ccc29451b801f81e4e90bb522d51
beep boop <githubbot@vadim.me>
2024-08-01 19:12:40 +0200

    Solution 37 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/c9l29fwk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d274dd2-7c46-4348-b04b-ab7f6dae7 3b9.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/3d274dd2-7c46-4348-b04b-ab7f6dae7 3b9.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,10 +1,15 @@
-```
 def sort_third(l: list):
-    result = []
-    for i, val in enumerate(l):
-        if i % 3 != 0:
-            result.append(val)
-        else:
-            temp = [x for x in l if i % 3 == 0 and (j := len(result) // 3) * 3 <= i < (j + 1) * 3]
-            result.extend(sorted(temp))
-    return result
+    return [
+        (
+            val
+            if i % 3 != 0
+            else sorted(
+                [
+                    x
+                    for j in range(len([x for x in l if i % 3 == 0]))
+                    for x in [x for x in l if i % 3 == 0 and j == (i - 1) // 3]
+                ]
+            )[0]
+        )
+        for i, val in enumerate(l)
+    ]
