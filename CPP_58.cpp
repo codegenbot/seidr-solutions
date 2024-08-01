2d1e392d9c7e8d8b01a6c0342b094eab9a49f182
beep boop <githubbot@vadim.me>
2024-08-01 17:14:26 +0200

    Solution 5 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/7jyxcr8g Prompt: Complete the following code given the task description and function signature.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -6,9 +7,23 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
 
-    set<int> intersection(s1.begin(), s1.end());
-    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), 
-                     inserter(intersection, intersection.begin()));
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+                      inserter(intersection, intersection.begin()));
 
-    return vector<int>(intersection.begin(), intersection.end());
+    vector<int> result(intersection.begin(), intersection.end());
+    return result;
+}
+
+int main() {
+    vector<int> l1 = {1, 4, 3, 34, 653, 2, 5};
+    vector<int> l2 = {5, 7, 1, 5, 9, 653, 121};
+
+    vector<int> result = common(l1, l2);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
