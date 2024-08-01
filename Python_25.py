9291ebd03e92739f202581a73d608089b00cc01b
beep boop <githubbot@vadim.me>
2024-08-01 19:30:42 +0200

    Solution 66 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xugli5t5 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/73deb322-a19e-4cde-98b8-725beb94b b34.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,8 +1,3 @@
 from typing import List
-
-def calculate_total_cost(orders: List[dict]) -> float:
-    total_cost = 0
-    for order in orders:
-        cost = (order['quantity'] * order['price'])
-        total_cost += cost
-    return total_cost
+def solve(n: int) -> List[int]:
+    pass
