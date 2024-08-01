68d6dd8faa60776a2c7ea7d6f73df1584386bb98
beep boop <githubbot@vadim.me>
2024-08-01 15:32:24 +0200

    Solution 16 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/pclmb167 Prompt: in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 13792668-e89d-428f-8ab9-cc0664b128e1.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 13792668-e89d-428f-8ab9-cc0664b128e1.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 13792668-e89d-428f-8ab9-cc0664b128e1.cpp:7: 13792668-e89d-428f-8ab9-cc0664b128e1.cpp: In function ‘int main()’: 13792668-e89d-428f-8ab9-cc0664b128e1.cpp:17:20: error: ‘remove_duplicates’ was n ot declared in this scope    17 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 13792668-e89d-428f-8ab9-cc0664b128e1.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,17 +1,21 @@
+#include <iostream>
 #include <vector>
 #include <set>
+#include <cassert>
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+using namespace std;
+
+vector<int> remove_duplicates(vector<int> numbers);
+
+int main() {
+    bool areEqual = (remove_duplicates({1, 2, 3, 2, 4, 3, 5}) == vector<int>{1, 4, 5});
+    assert(areEqual);
+
+    return 0;
 }
 
 vector<int> remove_duplicates(vector<int> numbers){
     set<int> numbersSet(numbers.begin(), numbers.end());
     vector<int> uniqueNumbers(numbersSet.begin(), numbersSet.end());
     return uniqueNumbers;
-}
-
-int main(){
-    assert(issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 4, 5}));
-    return 0;
 }
