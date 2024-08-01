78dbb8925d304e784337232553735fe92466572d
beep boop <githubbot@vadim.me>
2024-08-02 01:21:34 +0200

    Solution 7 of Python/98, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nz622101 Prompt: Complete the following code given the task description and function signature.

--- Python_98.py
+++ Python_98.py
@@ -1,7 +1,4 @@
-```
 def count_upper(s):
-    count = 0
-    for i in range(0, len(s), 2):
-        if s[i].upper() in 'AEIO':
-            count += 1
-    return count
+    return sum(
+        1 for i, c in enumerate(s) if "AEIO".index(c.upper()) % 2 == 0 and c.isupper()
+    )
