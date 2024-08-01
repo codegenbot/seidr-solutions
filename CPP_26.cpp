74878e4a99a468400c4b08e73e7291d6bdf33252
beep boop <githubbot@vadim.me>
2024-08-01 13:59:12 +0200

    Solution 27 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nfgyd5di Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 54be281f-1680-42f8-a507-97e960657b9a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 54be281f-1680-42f8-a507-97e960657b9a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 54be281f-1680-42f8-a507-97e960657b9a.cpp:11: 54be281f-1680-42f8-a507-97e960657b9a.cpp: In function ‘int main()’: 54be281f-1680-42f8-a507-97e960657b9a.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ 54be281f-1680-42f8-a507-97e960657b9a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,12 +1,8 @@
 ```cpp
 bool areSame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
+    if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
+        if(find(b.begin(), b.end(), a[i]) == b.end()) return false;
     }
     return true;
 }
@@ -20,7 +16,6 @@ vector<int> remove_duplicates(vector<int> numbers){
     }
     return result;
 }
-
 int main() {
     assert(areSame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}), {1, 2, 3, 4, 5}));
 }
