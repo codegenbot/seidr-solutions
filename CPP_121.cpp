5998e2b1ce9661de5127bd9fd101273f7ee780bd
beep boop <githubbot@vadim.me>
2024-08-01 21:49:49 +0200

    Solution 73 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/mvt19gl1 Prompt: 3c3cb84c-52af-436a-a2b0-2f147cde1993.cpp:1:14: error: ‘vector’ was not declared in this scope     1 | int solution(vector<int> lst) {       |              ^~~~~~ 3c3cb84c-52af-436a-a2b0-2f147cde1993.cpp:1:21: error: expected primary-expressio n before ‘int’     1 | int solution(vector<int> lst) {       |                     ^~~ In file included from 3c3cb84c-52af-436a-a2b0-2f147cde1993.cpp:11: 3c3cb84c-52af-436a-a2b0-2f147cde1993.cpp: In function ‘int main()’: 3c3cb84c-52af-436a-a2b0-2f147cde1993.cpp:13:13: error: ‘solutions’ was not decla red in this scope; did you mean ‘solution’?    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ^~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,31 +1,17 @@
 #include <vector>
 
 int main() {
-    int solution(vector<int> lst) {
-        int sum = 0;
-        for (int i = 1; i < lst.size(); i++) {
-            if (lst[i] % 2 != 0) {
-                sum += lst[i];
-            }
+    vector<int> lst = {1,2,3,4,5};
+    int solution = solution(lst);
+    cout << "The sum of odd numbers is: " << solution << endl;
+}
+
+int solution(vector<int> lst) {
+    int sum = 0;
+    for (int i = 0; i < lst.size(); i++) {
+        if (lst[i] % 2 != 0) {
+            sum += lst[i];
         }
-        return sum;
     }
-
-    vector<int> lst;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-
-    for(int i = 0; i < n; i++) {
-        int num;
-        cout << "Enter element " << (i+1) << ": ";
-        cin >> num;
-        lst.push_back(num);
-    }
-
-    int result = solution(lst);
-
-    cout << "Sum of odd numbers: " << result << endl;
-
-    return 0;
+    return sum;
 }
