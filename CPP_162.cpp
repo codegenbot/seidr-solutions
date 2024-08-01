1df9bea01a3b11a852ef127d6f1f2a16fcc0677d
beep boop <githubbot@vadim.me>
2024-08-02 01:48:07 +0200

    Solution 0 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xjngajct Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_162.cpp
@@ -0,0 +1,22 @@
+MD5_CTX ctx;
+unsigned char md[16];
+MD5_Init(&ctx);
+const unsigned char *p = (const unsigned char*)text.c_str();
+while (*p) {
+    MD5_Update(&ctx, p, 1);
+    p++;
+}
+MD5_Final(md, &ctx);
+
+string result;
+for (int i = 0; i < 16; i++) {
+    char buff[3];
+    sprintf(buff, "%02x", md[i]);
+    result += string(buff);
+}
+
+if(text.empty()) {
+    return "None";
+} else {
+    return result;
+}
