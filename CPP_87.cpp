5881348b520242a8a35fb0512b9c87a904a6c41f
beep boop <githubbot@vadim.me>
2024-08-01 21:11:42 +0200

    Solution 8 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/vj11817g Prompt:       |                                   ^~~~~~ 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp:22:42: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp:22:49: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp:22:56: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp:21: 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp: In function ‘int main()’: 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp:34:20: error: ‘get_row’ was not declare d in this scope    34 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ 904499c9-5e7e-4e73-8afe-ba51577a62b6.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,11 +1,12 @@
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a[0] == b[0] && a[1] == b[1];
+bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b) {
+    return a == b;
 }
 
-vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
-    vector<vector<int>> result;
+std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x) {
+    std::vector<std::vector<int>> result;
+    
     for (int i = 0; i < lst.size(); ++i) {
         for (int j = 0; j < lst[i].size(); ++j) {
             if (lst[i][j] == x) {
@@ -14,10 +15,11 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
         }
     }
 
-    sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b) {
+    std::sort(result.begin(), result.end(), [](const std::vector<int>& a, const std::vector<int>& b) {
         if (a[0] == b[0]) {
             return a[1] > b[1];
         }
+        
         return a[0] < b[0];
     });
 
@@ -25,9 +27,7 @@ vector<vector<int>> get_row(vector<vector<int>> lst, int x) {
 }
 
 int main() {
-    vector<vector<int>> lst = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int x = 5;
-    vector<vector<int>> result = get_row(lst, x);
-
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    
     return 0;
 }
