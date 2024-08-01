bac29f33283439a05e32a542a14db241e8b3e58a
beep boop <githubbot@vadim.me>
2024-08-01 21:38:01 +0200

    Solution 6 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/nh0u1zwu Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2a492ba1-5ed7-4eba-bd60-af0c8f8e8c7e.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2a492ba1-5ed7-4eba-bd60-af0c8f8e8c7e.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2a492ba1-5ed7-4eba-bd60-af0c8f8e8c7e.cpp:10: 2a492ba1-5ed7-4eba-bd60-af0c8f8e8c7e.cpp: In function ‘int main()’: 2a492ba1-5ed7-4eba-bd60-af0c8f8e8c7e.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 2a492ba1-5ed7-4eba-bd60-af0c8f8e8c7e.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,25 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
 bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
-void sort_third(vector<int>& l_copy, int i) {
-    sort(l_copy.begin() + i - 2, l_copy.begin() + i + 1);
-}
-
-vector<int> task_description(vector<int> l) {
+vector<int> sort_third(vector<int> l) {
     vector<int> l_copy = l;
-    for (int i = 0; i < l.size(); ++i) {
-        if ((i + 1) % 3 == 0) {
-            sort_third(l_copy, i);
+    for (int i = 0; i < l.size(); i++) {
+        if (i % 3 == 0) {
+            sort(l_copy.begin() + i, l_copy.begin() + i + 3);
         }
     }
     return l_copy;
+}
+
+int main() {
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    
+    cout << "All test cases passed!" << endl;
+    
+    return 0;
 }
