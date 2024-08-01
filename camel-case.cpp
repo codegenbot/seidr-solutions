bb3d32325f37392218f0af81e8484cacd7539a4b
beep boop <githubbot@vadim.me>
2024-08-01 20:47:42 +0200

    Solution 19 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/z00l6ytx Prompt:  this scope    22 |     while (cin >> s) {       |            ^~~ 8230360c-7c74-4c2f-9040-7223565b2051.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 8230360c-7c74-4c2f-9040-7223565b2051.cpp:23:9: error: ‘cout’ was not declared in  this scope    23 |         cout << camelCase(s) << endl;       |         ^~~~ 8230360c-7c74-4c2f-9040-7223565b2051.cpp:23:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 8230360c-7c74-4c2f-9040-7223565b2051.cpp:23:33: error: ‘endl’ was not declared i n this scope    23 |         cout << camelCase(s) << endl;       |                                 ^~~~ 8230360c-7c74-4c2f-9040-7223565b2051.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- camel-case.cpp
+++ camel-case.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 string camelCase(string s) {
     string result = "";
-    bool capitalizeNext = true;
-    for (char c : s) {
-        if (c == '-') {
-            capitalizeNext = true;
-        } else if (capitalizeNext) {
-            result += toupper(c);
-            capitalizeNext = false;
+    bool capitalizeNextLetter = true;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '-') {
+            i++; // skip the "-"
+            while (i < s.length() && s[i] == ' ') {
+                i++; // skip the spaces
+            }
+            capitalizeNextLetter = true;
+        } else if (capitalizeNextLetter) {
+            result += toupper(s[i]);
+            capitalizeNextLetter = false;
         } else {
-            result += tolower(c);
+            result += tolower(s[i]);
         }
     }
     return result;
