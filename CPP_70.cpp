17f465beea490630e6f01bef3ed0e6886a19abf2
beep boop <githubbot@vadim.me>
2024-08-01 17:58:50 +0200

    Solution 11 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/urdaelws Prompt: In file included from 293ea655-60df-4408-b300-54176e3d76eb.cpp:29: 293ea655-60df-4408-b300-54176e3d76eb.cpp: In function ‘int main()’: 293ea655-60df-4408-b300-54176e3d76eb.cpp:39:20: error: ‘strange_sort_list’ was n ot declared in this scope; did you mean ‘strange_sort_vector’?    39 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,3 +1,4 @@
+```
 #include <algorithm>
 using namespace std;
 
@@ -10,7 +11,7 @@ vector<int> strange_sort_vector(vector<int> lst) {
         
         if (min_val == max_val) {
             result.push_back(min_val);
-            lst.erase(remove(lst.begin(), lst.end(), min_val), lst.end());
+            lst.erase(std::remove(lst.begin(), lst.end(), min_val), lst.end());
         } else {
             result.push_back(min_val);
             vector<int> new_lst;
@@ -27,24 +28,10 @@ vector<int> strange_sort_vector(vector<int> lst) {
 }
 
 int main() {
-    vector<int> input;
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        int x;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> x;
-        input.push_back(x);
+    vector<int> input_vector = {4, 2, 9, 6, 23, 12, 34, 27};
+    vector<int> sorted_vector = strange_sort_vector(input_vector);
+    for (int num : sorted_vector) {
+        cout << num << " ";
     }
-    
-    vector<int> output = strange_sort_vector(input);
-    
-    cout << "Sorted Vector: ";
-    for (int x : output) {
-        cout << x << " ";
-    }
-    cout << endl;
-    
     return 0;
 }
