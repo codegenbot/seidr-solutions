831d587953ae335ab1c511ae32555e42001fe1eb
beep boop <githubbot@vadim.me>
2024-08-01 19:53:03 +0200

    Solution 98 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/e5e8b3vv Prompt:       |           ^~~~~~ In file included from d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp:8:5: note: ‘int main()’ previously defi ned here     8 | int main() {       |     ^~~~ In file included from d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp:13: d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp: In function ‘int main()’: d249a6e8-a9e3-4c1b-8927-9bad4d6c4428.cpp:23:20: error: ‘derivative’ was not decl ared in this scope    23 |     assert (issame(derivative({1}) , {}));       |                    ^~~~~~~~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -2,17 +2,7 @@
 #include <cassert>
 
 bool issame(const std::vector<float>& a, const std::vector<float>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+    return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
 int main() {
