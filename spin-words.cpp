6d5085978208e310e37b09473e394a0d7d46411e
beep boop <githubbot@vadim.me>
2024-08-01 19:33:23 +0200

    Solution 57 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/cteneuk8 Prompt:  in this scope    11 |         if (nextPos == std::string::npos || strlen(&sentence[pos]) >= 5)  {       |                                             ^~~~~~ 9463827d-370c-42f1-93e7-a6686f5f0593.cpp:2:1: note: ‘strlen’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     1 | #include <iostream>   +++ |+#include <cstring>     2 | #include <string> 9463827d-370c-42f1-93e7-a6686f5f0593.cpp:13:22: error: ‘reverse’ is not a member  of ‘std’    13 |                 std::reverse(&sentence[pos], &sentence[nextPos]);       |                      ^~~~~~~ 9463827d-370c-42f1-93e7-a6686f5f0593.cpp:23:8: error: ‘strlen’ was not declared in this scope    23 |     if(strlen(&sentence[pos]) >= 5) {       |        ^~~~~~ 9463827d-370c-42f1-93e7-a6686f5f0593.cpp:23:8: note: ‘strlen’ is defined in head er ‘<cstring>’; did you forget to ‘#include <cstring>’? 9463827d-370c-42f1-93e7-a6686f5f0593.cpp:24:14: error: ‘reverse’ is not a member  of ‘std’    24 |         std::reverse(&sentence[pos], &sentence[sentence.length()]);       |              ^~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -5,28 +5,28 @@
 std::string spinWords(std::string sentence) {
     std::string result = "";
     size_t pos = 0;
-
+    
     while ((pos = sentence.find(" ")) != std::string::npos) {
         size_t nextPos = sentence.find(" ", pos + 1);
-
+        
         if (nextPos == std::string::npos || (nextPos - pos >= 5)) {
             if(nextPos - pos >= 5) {
                 std::reverse(sentence.substr(pos, nextPos - pos).begin(), std::reverse(sentence.substr(pos, nextPos - pos).end()));
             }
-
+            
             result += sentence.substr(pos, nextPos - pos) + " ";
         } else {
             result += sentence.substr(pos, nextPos - pos) + " ";
             pos = nextPos;
         }
     }
-
+    
     if(nextPos - pos >= 5) {
         std::reverse(sentence.substr(pos).begin(), std::reverse(sentence.substr(pos).end()));
     }
-
+    
     result += sentence.substr(pos);
-
+    
     return result;
 }
 
