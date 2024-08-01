139c41d444b3d6d129c70b982b32098cefa11984
beep boop <githubbot@vadim.me>
2024-08-01 23:26:18 +0200

    Solution 8 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/j4jvgpo2 Prompt:       |                    ^~~ 1794fb25-da39-4408-bced-6b47836a1b07.cpp:12:27: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1794fb25-da39-4408-bced-6b47836a1b07.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 1794fb25-da39-4408-bced-6b47836a1b07.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 1794fb25-da39-4408-bced-6b47836a1b07.cpp:11: 1794fb25-da39-4408-bced-6b47836a1b07.cpp: In function ‘int main()’: 1794fb25-da39-4408-bced-6b47836a1b07.cpp:21:20: error: ‘common’ was not declared  in this scope    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 1794fb25-da39-4408-bced-6b47836a1b07.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,31 +1,20 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()) {
-        return false;
-    }
-    
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    
-    for(int i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    
-    return true;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
-int main() {
-    // Your existing code here
+vector<int> common(vector<int> l1, vector<int> l2) {
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
 
-    assert(issame(vector<int>{4, 3, 2, 8}, {})); // Example assertion
+    vector<int> result;
+    set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), back_inserter(result));
 
-    return 0;
+    result.erase(unique(result.begin(), result.end()), result.end());
+
+    return result;
 }
