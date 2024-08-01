060d6fc0427f9f60c8821bb3f62f2478e20f9515
beep boop <githubbot@vadim.me>
2024-08-01 18:45:07 +0200

    Solution 98 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/ncsz9ma9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d54730ad-9c43-4725-895b-be8f5957e d0a.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d54730ad-9c43-4725-895b-be8f5957e d0a.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,9 +1,8 @@
-```
 def sort_third(l: list):
     result = []
     for i in range(0, len(l), 3):
         if l[i] % 3 != 0:
-            result.append(sorted([x for x in l[:i+1] if x % 3 != 0])[::-1])
+            result.append(sorted([x for x in l[: i + 1] if x % 3 != 0])[::-1])
         else:
-            result.append([l[i]])
+            result.append([l[j] for j in range(i, len(l), 3)])
     return result
