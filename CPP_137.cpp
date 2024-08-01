37f28c8b5452c5edfd21d8bfc6c915ad8c64e057
beep boop <githubbot@vadim.me>
2024-08-02 01:50:41 +0200

    Solution 86 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/l6hmhmtf Prompt: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:64: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp: In function ‘int main()’: 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:13: error: ‘boost’ has not been decl ared    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:29: error: ‘string’ was not declared  in this scope    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 7a47d8ce-e7ac-435b-b1dc-918f728c19f1.cpp:66:37: error: ‘compare_one’ was not dec lared in this scope    66 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <boost/any.hpp>
 #include <string>
 
@@ -21,40 +20,40 @@ boost::any compare_one(boost::any a, boost::any b) {
         }
     }
     else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        return (float)a.convert_to<float>() > b ? &a : &b;
+        return (float)a < b ? &b : &a;
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return (double)a.convert_to<double>() > b ? &a : &b;
+   	else if (a.type() == typeid(double) && b.type() == typeid(int)) {
+        return (double)a < b ? &b : &a;
     }
-   	else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-		if (stof(a.convert_to<string>().c_str()) < b) {
-			return a;
-		}
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        if (stof(a.convert_to<string>().c_str()) < b) {
+            return a;
+        }
 		else if (stof(a.convert_to<string>().c_str()) > b) {
-			return b;
-		}
+            return b;
+        }
 		else {
             return boost::any("None");
         }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-		if (stof(a.convert_to<string>().c_str()) < b) {
-			return a;
-		}
+        if (stof(a.convert_to<string>().c_str()) < b) {
+            return a;
+        }
 		else if (stof(a.convert_to<string>().c_str()) > b) {
-			return b;
-		}
+            return b;
+        }
 		else {
             return boost::any("None");
         }
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-		if (stof(a.convert_to<string>().c_str()) < stof(b.convert_to<string>().c_str())) {
-			return a;
-		}
+        if (stof(a.convert_to<string>().c_str()) < stof(b.convert_to<string>().c_str())) {
+            return a;
+        }
 		else if (stof(a.convert_to<string>().c_str()) > stof(b.convert_to<string>().c_str())) {
-			return b;
-		}
+            return b;
+        }
 		else {
             return boost::any("None");
         }
