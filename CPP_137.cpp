7ac7bd6dcd7b381114a9685f051e82707ce270e9
beep boop <githubbot@vadim.me>
2024-08-02 00:04:23 +0200

    Solution 95 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/rvo61gzr Prompt: adb390b5-cccc-48d2-928d-e648964cc554.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,40 +1,39 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
-#include <iostream>
 
 using namespace boost;
-using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
         return b;
     } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        if (stod(b.convert_to<string>().c_str()) > stod(a.convert_to<string>().c_str())) {
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
+        if (std::stod(b.convert_to<std::string>().c_str()) > std::stod(a.convert_to<std::string>().c_str())) {
             return b;
         } else {
             return boost::any("None");
         }
-    } else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        if (stod(b.convert_to<string>().c_str()) > stod(a.convert_to<string>().c_str())) {
+    } else if (a.type() == typeid(double) && b.type() == typeid(std::string)) {
+        if (std::stod(b.convert_to<std::string>().c_str()) > std::stod(a.convert_to<std::string>().c_str())) {
             return b;
         } else {
             return boost::any("None");
         }
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        int aInt = stoi(a.convert_to<string>().erase(0, 1).erase(find(a.convert_to<string>().begin(), find(a.convert_to<string>().begin(), a.convert_to<string>().end()), ',') + 1).c_str());
-        int bInt = stoi(b.convert_to<string>().erase(0, 1).erase(find(b.convert_to<string>().begin(), find(b.convert_to<string>().begin(), b.convert_to<string>().end(), ',') + 1).c_str());
-        if (aInt < bInt) {
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        int a_int = std::stoi(a.convert_to<std::string>().erase(0, 1).erase(a.convert_to<std::string>().find(',') + 1).c_str());
+        int b_int = std::stoi(b.convert_to<std::string>().erase(0, 1).erase(b.convert_to<std::string>().find(',') + 1).c_str());
+        if (a_int < b_int) {
             return b;
-        } else if (aInt > bInt) {
+        } else if (a_int == b_int) {
+            return boost::any("None");
+        } else {
             return a;
-        } else {
-            return boost::any("None");
         }
-    } else if (a.type() == typeid(double) && any_cast<double>(a) > any_cast<double>(b)) {
+    } else if (a.type() == typeid(double) && a.convert_to<double>() > b.convert_to<double>()) {
         return a;
-    } else if (any_cast<double>(b) > any_cast<double>(a)) {
+    } else if (b.type() == typeid(double) && b.convert_to<double>() > a.convert_to<double>()) {
         return b;
     } else {
         return boost::any("None");
