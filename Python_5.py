e22b8e44a5e474db61b2d4d67d6a9ad17928527a
beep boop <githubbot@vadim.me>
2024-08-01 13:09:12 +0200

    Solution 7 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/pts904p7 Prompt: Complete the following code given the task description and function signature.

--- Python_5.py
+++ Python_5.py
@@ -1,6 +1,4 @@
 def intersperse(numbers: List[int], delimeter: int) -> List[int]:
-    if len(numbers) == 0:
-        return numbers
     result = [numbers[0]]
     for num in numbers[1:]:
         result.extend([num, delimeter])
