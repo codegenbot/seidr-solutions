3f63c693c23dd7c136e2a2cba38ad240dae3f43e
beep boop <githubbot@vadim.me>
2024-08-01 18:19:44 +0200

    Solution 71 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sl0y4p88 Prompt: f35943a0-e239-4b0d-9daf-b68d25db1a89.cpp: In function ‘std::string change_base(i nt, int)’: f35943a0-e239-4b0d-9daf-b68d25db1a89.cpp:5:17: error: ‘std::std’ has not been de clared     5 |     return std::std::to_string(num);       |                 ^~~ In file included from f35943a0-e239-4b0d-9daf-b68d25db1a89.cpp:8: f35943a0-e239-4b0d-9daf-b68d25db1a89.cpp: In function ‘int main()’: f35943a0-e239-4b0d-9daf-b68d25db1a89.cpp:12:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    12 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from f35943a0-e239-4b0d-9daf-b68d25db1a89.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,13 +1,14 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <cassert>
 
 std::string change_base(int num, int base){
     return std::to_string(num);
 }
 
-int main(){
+int main() {
     int x = 42;
     std::cout << std::to_string(x) << std::endl;
+    
     return 0;
 }
