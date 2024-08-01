a53d06afd5a4477384ae17d260329e5c74f82475
beep boop <githubbot@vadim.me>
2024-08-01 18:41:57 +0200

    Solution 1 of CPP/83, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/vbfl79dc Prompt: Complete the following code given the task description and function signature.

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -3,21 +3,13 @@
 using namespace std;
 
 int starts_one_ends(int n) {
-    int res = 0;
+    int count = 0;
     for (int i = 1; i <= 9; ++i) {
-        if ((i == 1 || i % 10 == 1) && isOne(n, i)) {
-            res++;
+        if (i == 1 || i == 9) {
+            count += pow(10, n-1);
+        } else {
+            count += pow(10, n-1);
         }
     }
-    return res;
-}
-
-bool isOne(int n, int num) {
-    string str = to_string(num);
-    for (int i = 0; i < str.length(); ++i) {
-        if (str[i] == '1') {
-            return true;
-        }
-    }
-    return false;
+    return count;
 }
