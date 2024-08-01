94498dda2e38dea7f592c3baab7bef79c82a4ed2
beep boop <githubbot@vadim.me>
2024-08-01 22:27:17 +0200

    Solution 29 of CPP/126, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/j010drr1 Prompt: 3a7d37be-66c4-420a-a557-d08b308bcd52.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 3a7d37be-66c4-420a-a557-d08b308bcd52.cpp:14: 3a7d37be-66c4-420a-a557-d08b308bcd52.cpp: In function ‘int main()’: 3a7d37be-66c4-420a-a557-d08b308bcd52.cpp:16:13: error: ‘is_sorted’ was not decla red in this scope    16 |     assert (is_sorted({1, 2, 3, 4}) == true);       |             ^~~~~~~~~

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,14 +7,18 @@ bool is_sorted(std::vector<int> lst);
 
 int main() {
     std::vector<int> lst;
-    int num;
-
-    // Read input from user
-    while (std::cin >> num) {
-        lst.push_back(num);
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        int x;
+        std::cout << "Enter element " << i+1 << ": ";
+        std::cin >> x;
+        lst.push_back(x);
     }
-
-    if (is_sorted(lst)) {
+    
+    if(is_sorted(lst)){
         std::cout << "The list is sorted." << std::endl;
     } else {
         std::cout << "The list is not sorted." << std::endl;
@@ -25,7 +30,7 @@ int main() {
 bool is_sorted(std::vector<int> lst){
     for(int i = 1; i < lst.size(); i++){
         if(lst[i] <= lst[i-1]){
-            vector<int>::iterator it = unique(lst.begin(), lst.end());
+            std::vector<int>::iterator it = std::unique(lst.begin(), lst.end());
             lst.erase(it, lst.end());
             return false;
         }
