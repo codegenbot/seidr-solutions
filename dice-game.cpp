9a50115af6da3f20667712104075e9d5d2fc32ba
beep boop <githubbot@vadim.me>
2024-08-01 16:33:18 +0200

    Solution 85 of dice-game, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/q13lgfbj Prompt:     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |     ^~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:13: error: ‘fixed’ was not declared i n this scope     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |             ^~~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:22: error: ‘setprecision’ was not dec lared in this scope     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |                      ^~~~~~~~~~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:41: error: ‘calculateProbability’ was  not declared in this scope     4 |     cout << fixed << setprecision(1) << calculateProbability(n, m) << en dl;       |                                         ^~~~~~~~~~~~~~~~~~~~ d2c9670c-d487-4ac7-8250-9b83d6813442.cpp:4:71: error: ‘endl’ was not declared in  this scope     4 | cout << fixed << setprecision(1) << calculateProbability(n, m) << endl;       |                                                                   ^~~~

--- dice-game.cpp
+++ dice-game.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
-#include <iomanip>
+using namespace std;
 
 double calculateProbability(int n, int m) {
     double probability = 0;
-    
     for (int i = 1; i <= m; i++) {
-        for (int j = i + 1; j <= n; j++) {
-            probability += 1.0 / n / m;
-        }
+        probability += (n - i) / (n * m);
     }
-    
     return probability;
 }
 
 int main() {
     int n, m;
     cin >> n >> m;
-    cout << std::fixed << std::setprecision(1) << calculateProbability(n, m) << std::endl;
+    cout << fixed << setprecision(1) << calculateProbability(n, m) << endl;
     return 0;
 }
