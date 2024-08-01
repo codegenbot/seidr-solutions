f7450c2cd2754925d83cab5ec26078f1452b2d57
beep boop <githubbot@vadim.me>
2024-08-01 16:30:58 +0200

    Solution 15 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ljkmsi02 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,28 +1,47 @@
-int bowlingScore(string s) {
+int bowlingScore(const string& str) {
     int score = 0;
-    bool firstInFrame = true;
-    int frameScore = 0;
+    int roll1, roll2;
+    bool inFrame = false;
 
-    for(int i = 0; i < s.length(); i++) {
-        if(s[i] == '/') {
-            if(firstInFrame) {
-                frameScore += (i - 1 + 1);
-                firstInFrame = false;
-            }
-            else {
-                score += min(10, (int)(s.substr(i-1,2)-48)) * 10;
-                firstInFrame = true;
-                i++;
+    for (int i = 0; i < str.size(); i++) {
+        if (isdigit(str[i])) {
+            inFrame = true;
+            if (i + 2 <= str.size()) {
+                roll1 = str[i] - '0';
+                if (str[i + 1] == '/') {
+                    score += (roll1 * 10) + 10;
+                    break;
+                } else {
+                    roll2 = str[i + 1] - '0';
+                    if (i + 3 <= str.size()) {
+                        if (isdigit(str[i + 2])) {
+                            if (str[i + 2] != '/') {
+                                score += (roll1 * 10) + roll2;
+                            } else {
+                                score += (roll1 * 10);
+                            }
+                            break;
+                        } else {
+                            score += (roll1 * 10) + roll2;
+                            break;
+                        }
+                    } else if (inFrame) {
+                        score += (roll1 * 10) + roll2;
+                        inFrame = false;
+                        break;
+                    }
+                }
+            } else if (isdigit(str[i])) {
+                int roll = str[i] - '0';
+                if (!inFrame) {
+                    score += (roll * 10);
+                } else if (str[i + 1] == '/') {
+                    score += (roll * 10) + 10;
+                } else {
+                    score += (roll * 10);
+                }
             }
         }
     }
-
-    if(firstInFrame) {
-        frameScore += (int)(s.back()-48);
-    }
-    else {
-        score += min(10, (int)(s[s.length()-1]-48)) + frameScore;
-    }
-
     return score;
 }
