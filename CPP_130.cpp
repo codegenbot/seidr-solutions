c8081026a23bef479d92e7c9796de7dd4bbbfe03
beep boop <githubbot@vadim.me>
2024-08-02 01:50:25 +0200

    Solution 3 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/j8f3yaq2 Prompt:       |                    ^~~ 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp:18: 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp: In function ‘int main()’: 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp:28:20: error: ‘tri’ was not declared in  this scope    28 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ 36704f15-a96d-415a-ac5f-afffe3d007e5.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -16,7 +16,11 @@ std::vector<int> tri(int n) {
             } else if (i % 2 == 0) {
                 result.push_back(1 + i / 2);
             } else {
-                result.push_back(result[i - 1] + result[i - 2]);
+                if (i == 2) {
+                    result.push_back(result[i - 1] + result[i - 2]);
+                } else {
+                    result.push_back(result[i - 1] + result[i - 2] + result[i - 3]);
+                }
             }
         }
     }
