a7fe3436667c413fb8c4e60a340f696b9ee3579b
beep boop <githubbot@vadim.me>
2024-08-02 01:44:34 +0200

    Solution 96 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/repraek3 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,34 +1,32 @@
-def bowling_game(s):
+```
+def bowling(score_str):
     score = 0
-    frame = 1
-    in_play = 0
-
-    for c in s:
-        if c.isdigit():
-            num = int(c)
-            if num == 10:
-                score += 10 + 10
-                frame += 1
+    frame_score = 0
+    roll_count = 0
+    for i in range(len(score_str)):
+        if score_str[i] == 'X':
+            if i % 2 == 1:
+                score += max(10 - int(score_str[:i].split('/')[1]), 0) + 10
             else:
-                if in_play < 2:
-                    if in_play == 1:
-                        score += min(num, 10 - in_play) * 2
-                    elif num < 10 - in_play:
-                        score += num
-                    else:
-                        score += 10 - in_play + num
-                else:
-                    if num == 0:
-                        break
-                    score += num
-            in_play = (in_play + 1) % 2
-        else:
-            if c == "X":
-                score += 30
-                frame += 1
-            elif c == "/":
-                score += 10 - in_play
-                frame += 1
-                in_play = 0
-
+                frame_score += 20
+        elif score_str[i] in '/':
+            first_roll = int(score_str[:i].split('/')[0])
+            second_roll = int(score_str[i+1:].split('/')[0])
+            if i % 2 == 1:
+                score += first_roll + second_roll
+            else:
+                frame_score += first_roll + second_roll
+        elif score_str[i] in '123456789':
+            roll_count += 1
+    while roll_count < 10:
+        if roll_count < 9 and (score_str[roll_count*2].isdigit() or score_str[roll_count*2+1] == '/'):
+            first_roll = int(score_str[roll_count*2:roll_count*2+2])
+            second_roll = int(score_str[roll_count*2+2:].split('/')[0])
+            if roll_count < 8:
+                score += first_roll + second_roll
+            else:
+                frame_score += max(10 - int(score_str[:i].split('/')[1]), 0) + second_roll
+        elif score_str[roll_count*2].isdigit():
+            score += int(score_str[roll_count*2:roll_count*2+3])
+            roll_count += 1
     return score
