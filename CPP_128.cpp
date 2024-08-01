2ea1164813d9f362c77c198b210a40b0ffcc8d0f
beep boop <githubbot@vadim.me>
2024-08-01 22:46:42 +0200

    Solution 31 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/2rbhm4j5 Prompt: 488dc8ce-bc03-4ae3-85d9-121dd3c5dabe.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr) {       |                ^~~~~~ 488dc8ce-bc03-4ae3-85d9-121dd3c5dabe.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr) {       |                       ^~~ In file included from 488dc8ce-bc03-4ae3-85d9-121dd3c5dabe.cpp:11: 488dc8ce-bc03-4ae3-85d9-121dd3c5dabe.cpp: In function ‘int main()’: 488dc8ce-bc03-4ae3-85d9-121dd3c5dabe.cpp:13:23: error: ‘prod_signs’ cannot be us ed as a function    13 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -5,8 +5,8 @@ int prod_signs(std::vector<int> arr) {
     int product = 1;
     for (int num : arr) {
         if (num == 0)
-            return -32768; // return -32768 for empty arr.
+            return -32768; 
         product *= (num > 0 ? 1 : -1);
     }
-    return abs(accumulate(arr.begin(), arr.end(), 0));
+    return std::abs(accumulate(arr.begin(), arr.end(), 0));
 }
