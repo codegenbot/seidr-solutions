b5f58e5d9fdf2846a5b8e4af87f8200b9b24ae3d
beep boop <githubbot@vadim.me>
2024-08-01 20:46:49 +0200

    Solution 68 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/3syqsoz1 Prompt: In file included from f05f0edc-bced-4742-8ab6-51a738661b7a.cpp:26: f05f0edc-bced-4742-8ab6-51a738661b7a.cpp: In function ‘int main()’: f05f0edc-bced-4742-8ab6-51a738661b7a.cpp:36:41: error: could not convert ‘find_c losest_elements(std::vector<float>(std::initializer_list<float>{((const float*)( & const float [5]{1.10000002e+0f, 2.20000005e+0f, 3.0999999e+0f, 4.0999999e+0f, 5.0999999e+0f})), 5}, std::allocator<float>()))’ from ‘std::pair<float, float>’ to ‘std::vector<float>’    36 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                         |       |                                         std::pair<float, float>

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -18,7 +18,7 @@ pair<float, float> find_closest_elements(vector<float> numbers) {
     return result;
 }
 
-pair<float, float> contest_main() {
+int contest_main() {
     assert(find_closest_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f}) == make_pair(2.2f, 3.1f));
-    return find_closest_elements({1.1f, 2.2f, 3.1f, 4.1f, 5.1f});
+    return 0;
 }
