59d472fdd2d99e341b4cbed9c3ed37d80da0207f
beep boop <githubbot@vadim.me>
2024-08-01 13:47:13 +0200

    Solution 73 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/hfosnqlm Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bf5dcdb2-a8b6-40f0-b251-b884bf5e25e7.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bf5dcdb2-a8b6-40f0-b251-b884bf5e25e7.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bf5dcdb2-a8b6-40f0-b251-b884bf5e25e7.cpp:10: bf5dcdb2-a8b6-40f0-b251-b884bf5e25e7.cpp: In function ‘int main()’: bf5dcdb2-a8b6-40f0-b251-b884bf5e25e7.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ bf5dcdb2-a8b6-40f0-b251-b884bf5e25e7.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,9 +1,14 @@
 if (a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); ++i) {
-        if (!filter_by_substring({a[i], b[i]}, "run").size())
+    for (int i = 0; i < a.size(); i++) {
+        if (!filter_by_substring({a[i], b[i]}, "run").size()) {
             return false;
+        }
     }
     return true;
+}
+
+int main() {
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
 }
