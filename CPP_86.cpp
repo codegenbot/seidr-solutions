2665d005d6e1a80fad727228b65ca3ebf9b6089e
beep boop <githubbot@vadim.me>
2024-08-01 18:52:43 +0200

    Solution 2 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0hmc4c3p Prompt: Complete the following code given the task description and function signature.

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 string anti_shuffle(string s) {
     string result = "";
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == ' ') {
-            result += ' ';
-        } else {
-            string word;
-            for (int j = i; j < s.size() && s[j] != ' '; j++) {
-                word += s[j];
-            }
-            sort(word.begin(), word.end());
-            result += word;
-            i = j - 1;
+    size_t pos = 0;
+    
+    while (pos != string::npos) {
+        pos = s.find(" ");
+        if (pos == string::npos) {
+            result += s;
+            break;
         }
+        
+        string word = s.substr(0, pos);
+        sort(word.begin(), word.end());
+        result += word;
+        s.erase(0, pos + 1);
     }
+    
     return result;
 }
