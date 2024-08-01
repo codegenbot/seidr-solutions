ae0083009a6f1c71da207bfb215cbfb6e6863354
beep boop <githubbot@vadim.me>
2024-08-01 18:58:35 +0200

    Solution 90 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rs9p68sc Prompt:       |                           ^~~~~~ 69754683-4536-4d04-88bd-f94a4637e884.cpp:31:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 69754683-4536-4d04-88bd-f94a4637e884.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 69754683-4536-4d04-88bd-f94a4637e884.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 69754683-4536-4d04-88bd-f94a4637e884.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 69754683-4536-4d04-88bd-f94a4637e884.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 69754683-4536-4d04-88bd-f94a4637e884.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -8,29 +8,20 @@ std::vector<int> parse_music(const std::string& music) {
     size_t next_pos = music.find('|');
 
     while (next_pos != std::string::npos) {
-        notes.push_back(next_pos - pos - 1);
+        notes.push_back(next_pos - pos);
         pos = next_pos + 1;
         next_pos = music.find('|', pos);
     }
 
-    notes.push_back(music.size() - pos - 1);
+    notes.push_back(music.size() - pos);
 
     return notes;
 }
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
+    return a == b;
 }
 
-assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 3, 3, 3, 3}));
-return 0;
+bool main() {
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 3, 3, 3, 3}));
+}
