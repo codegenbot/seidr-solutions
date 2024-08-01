94234f829255a4c07a98481394943cd50c10206b
beep boop <githubbot@vadim.me>
2024-08-01 13:25:38 +0200

    Solution 14 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9d5cmfdu Prompt: In file included from /usr/include/c++/11/vector:67,                  from 787adec6-0771-4e60-8843-5cb3c9595069.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 787adec6-0771-4e60-8843-5cb3c9595069.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 787adec6-0771-4e60-8843-5cb3c9595069.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 787adec6-0771-4e60-8843-5cb3c9595069.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 787adec6-0771-4e60-8843-5cb3c9595069.cpp:14: 787adec6-0771-4e60-8843-5cb3c9595069.cpp: In function ‘int main()’: 787adec6-0771-4e60-8843-5cb3c9595069.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,10 +1,6 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
 std::pair<int, int> sum_product(const std::vector<int>& numbers) {
     int sum = 0;
     int product = 1;
@@ -16,6 +12,6 @@ std::pair<int, int> sum_product(const std::vector<int>& numbers) {
 }
 
 int main() {
-    assert(issame(sum_product({10}), std::make_pair(10, 10)));
+    assert(sum_product({10}) == std::pair<int, int>({10, 10}));
     return 0;
 }
