8c915d71f801a2646b45fcb86b5970c5847e377f
beep boop <githubbot@vadim.me>
2024-08-01 23:19:38 +0200

    Solution 72 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/6g8o81hw Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,8 +1,9 @@
 def find_zero(xs: list):
-    a = xs[-1]
-    b = xs[-2]
+    a = xs[0]
+    b = xs[1]
     return -b / a
 
-coefficients = list(map(float, input().split()))
+
+coefficients = list(map(int, input().split()))
 result = find_zero(coefficients)
 print(result)
