ca57865672a5f4bff7a049b743271cab67879a18
beep boop <githubbot@vadim.me>
2024-08-01 14:40:53 +0200

    Solution 35 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xi05siu6 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4fb36ddd-a50a-4efd-ac46-c17dc4f46ea4.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4fb36ddd-a50a-4efd-ac46-c17dc4f46ea4.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4fb36ddd-a50a-4efd-ac46-c17dc4f46ea4.cpp:10: 4fb36ddd-a50a-4efd-ac46-c17dc4f46ea4.cpp: In function ‘int main()’: 4fb36ddd-a50a-4efd-ac46-c17dc4f46ea4.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 4fb36ddd-a50a-4efd-ac46-c17dc4f46ea4.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,14 +1,14 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<string> a,vector<string>b){
+bool issame(vector<string> a, vector<string> b) {
     return (a.size() == b.size()) && all_of(a.begin(), a.end(), [&b](string s) {return find(b.begin(), b.end(), s) != b.end();});
 }
 
 vector<vector<string>> filter_by_prefix(vector<vector<string>> strings, string prefix) {
     vector<vector<string>> result;
     for (const auto& str : strings) {
-        if (issame(str, vector<string>({prefix})))
+        if (issame(str, vector<string>({prefix})) && str[0] == prefix)
             result.push_back(str);
     }
     return result;
