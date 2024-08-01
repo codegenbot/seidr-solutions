c595813ea3937f1bedcc419ed376bf7102dc7792
beep boop <githubbot@vadim.me>
2024-08-02 01:22:27 +0200

    Solution 12 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gtgpgdud Prompt:                  from a385981d-b129-4165-9875-6eb4ee462e15.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from a385981d-b129-4165-9875-6eb4ee462e15.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ a385981d-b129-4165-9875-6eb4ee462e15.cpp:33:49: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ In file included from a385981d-b129-4165-9875-6eb4ee462e15.cpp:32: a385981d-b129-4165-9875-6eb4ee462e15.cpp: In function ‘int main()’: a385981d-b129-4165-9875-6eb4ee462e15.cpp:45:27: error: invalid initialization of  reference of type ‘const std::vector<int>&’ from expression of type ‘std::vecto r<std::vector<int> >’    45 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~ a385981d-b129-4165-9875-6eb4ee462e15.cpp:5:37: note: in passing argument 1 of ‘b ool issame(const std::vector<int>&, const std::vector<int>&)’     5 | bool issame(const std::vector<int>& a, const std::vector<int>& b) {       |             ~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,11 +1,12 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-using namespace std;
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
     return a == b;
 }
+
+std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x);
 
 std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
     std::vector<std::vector<int>> result;
@@ -29,6 +30,7 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
 }
 
 int main() {
-    assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3)[0], {2, 2}));
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), std::vector<std::vector<int>> {{2, 2}}));
+    
     return 0;
 }
