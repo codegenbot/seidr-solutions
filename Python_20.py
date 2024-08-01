a27a43e87c8abed13ec7763f7d0fd7023a925f3d
beep boop <githubbot@vadim.me>
2024-08-01 15:45:25 +0200

    Solution 14 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/0nnqt6i4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/065d1cd1-6726-4036-b137-fdaffe196 995.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_20.py
+++ Python_20.py
@@ -1,5 +1,5 @@
 ```
-def find_closest_elements(numbers: list[float]) -> tuple[float, float]:
+def find_closest_elements(numbers):
     numbers.sort()
     min_diff = float('inf')
     closest_pair = ()
