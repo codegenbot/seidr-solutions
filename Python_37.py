fefffdc91eb79e5fd31487cbbf7a985b11340b42
beep boop <githubbot@vadim.me>
2024-08-01 18:59:18 +0200

    Solution 40 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/go4v9kj7 Prompt: d43.py", line 16, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5c6b4b1-b71e-4fea-a2aa-7d52dc3ae d43.py", line 11, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5c6b4b1-b71e-4fea-a2aa-7d52dc3ae d43.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5c6b4b1-b71e-4fea-a2aa-7d52dc3ae d43.py", line 6, in <listcomp>     else sorted([y for y in l if (i // 2) * 2 == j and j < len(l)])[0]                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/b5c6b4b1-b71e-4fea-a2aa-7d52dc3ae d43.py", line 6, in <listcomp>     else sorted([y for y in l if (i // 2) * 2 == j and j < len(l)])[0]                                                  ^ NameError: name 'j' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,7 +1,3 @@
 def sort_even(l: list):
-    return [
-        x if i % 2 != 0 else sorted([y for y in l if i // 2 * 2 == idx])[0]
-        for i, x in enumerate(l)
-        for idx in range(len(l))
-        if i // 2 * 2 == idx
-    ]
+    return [x if i % 2 != 0 else sorted([y for y in l if (i // 2) * 2 == k and k < len(l)])[
+            0] for i, x in enumerate(l)]
