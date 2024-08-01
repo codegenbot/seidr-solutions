225a6969f63eaaad2999b099cc40153df8fef5c8
beep boop <githubbot@vadim.me>
2024-08-01 18:08:59 +0200

    Solution 91 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ljkmsi02 Prompt: 4a3e1f11-9666-4ce6-b01a-64dc2e231ec7.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,29 +2,38 @@
 
 int bowlingScore(string s) {
     int score = 0;
-    int currentFrame = 0;
-    for (char c : s) {
-        if (c == 'X') {
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == 'X') {
             score += 30;
-            currentFrame++;
-        } else if (c == '/') {
-            score += 10 - (26 - (currentFrame * 2));
-            currentFrame++;
-        } else {
-            int pins = c - '0';
+        } else if (s[i] == '/') {
+            if (i + 2 >= s.size() || s[i+1] == 'X' || s[i+1] - '0' > 10 - (26 - i*2)) {
+                score += 10;
+            } else {
+                int pins = 10 - (s[i+1] - '0') + (s[i+2] - '0');
+                score += pins;
+                i++;
+            }
+        } else if (s[i] - '0' + s[i+1] - '0' <= 10) {
+            int pins = s[i] - '0' + s[i+1] - '0';
             score += pins;
-            if (pins < 10) {
-                for (int i = 1; i <= 3; i++) {
-                    if (s[currentFrame*2 + i] == '/') {
-                        score += 10 - (26 - (currentFrame * 2));
-                        break;
-                    }
-                    else {
-                        pins++;
-                    }
+            i++;
+        } else {
+            int firstPins = s[i] - '0';
+            if (s[i+2] == '/') {
+                score += 10;
+            } else {
+                score += firstPins + s[i+1] - '0';
+                i++;
+            }
+            for (int j = 3; j > 0 && i < s.size(); i++, j--) {
+                if (s[i] == 'X') {
+                    score += 30;
+                    break;
+                } else if (s[i] == '/') {
+                    score += 10 - (26 - i*2);
+                    break;
                 }
             }
-            currentFrame++;
         }
     }
     return score;
