d4308f8c7dd42d6c475740d16e4b4a9625098d15
beep boop <githubbot@vadim.me>
2024-08-01 17:37:23 +0200

    Solution 62 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ljkmsi02 Prompt: d00a5a17-6078-42bd-9177-9b86020a1b73.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ d00a5a17-6078-42bd-9177-9b86020a1b73.cpp:22:24: error: ‘string’ was not declared  in this scope    22 | int bowlingScoreHelper(string* s) {       |                        ^~~~~~ d00a5a17-6078-42bd-9177-9b86020a1b73.cpp:22:32: error: ‘s’ was not declared in t his scope    22 | int bowlingScoreHelper(string* s) {       |                                ^

--- bowling.cpp
+++ bowling.cpp
@@ -2,41 +2,17 @@
 
 int bowlingScore(string s) {
     int score = 0;
+    int roll = 0;
     for(int i=0; i<10; i++) {
         if(s[i] == 'X') {
-            score += 30;
+            score += 10 + (roll > 0 ? max(1, 10 - roll) : 10);
+            roll = 0;
         } else if (s[i] == '/') {
-            score += 10 + (i > 0 ? bowlingScoreHelper(&s.substr(i+1)) : 0);
+            score += 10;
+            roll = 2;
         } else {
-            int frame = s[i] - '0';
-            if(s[i+1] == 'X') {
-                score += 10 + frame;
-            } else if (s[i+1] == '/') {
-                score += 10 + frame + bowlingScoreHelper(&s.substr(i+2));
-            } else {
-                score += 10 + frame + s[i+1] - '0';
-            }
-        }
-    }
-    return score;
-}
-
-int bowlingScoreHelper(string* s) {
-    int score = 0;
-    for(int i=0; i<2; i++) {
-        if(s->substr(i,1)[0] == 'X') {
-            score += 30;
-            break;
-        } else if (s->substr(i,1)[0] == '/') {
-            score += 10 + (i > 0 ? s->substr(0,i)[0] - '0' : 0);
-            break;
-        } else {
-            int frame = s->substr(i,1)[0] - '0';
-            if(i < 1) {
-                score += frame;
-            } else {
-                score += frame + s->substr(0,i)[0] - '0';
-            }
+            roll++;
+            if(roll >= 2) break;
         }
     }
     return score;
