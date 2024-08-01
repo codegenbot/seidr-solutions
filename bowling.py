714fc2c45ef9e175ba03bca7ae5fdc2045a28ccb
beep boop <githubbot@vadim.me>
2024-08-02 01:37:20 +0200

    Solution 84 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/repraek3 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,15 +1,36 @@
-def bowling_score(rolls):
+```
+def bowling_score(s):
     score = 0
-    roll = 0
-    for frame in rolls.split("/"):
-        if len(frame) == 1:
-            score += 10
-        elif len(frame) == 2:
-            score += int(frame[0]) * 2 + 10 - int(frame[0])
-        else:
-            if int(frame[0]) == 10:
-                score += 10 + 10
+    frame = 1
+    while s and frame <= 10:
+        if '/' in s[:2]:
+            first_roll = int(s[:1])
+            second_roll = int(s[2])
+            if first_roll + second_roll == 10:
+                score += 10
+                s = s[3:]
             else:
-                score += int(frame[0]) * 2
-            roll += 1
+                score += first_roll + second_roll
+                s = s[3:]
+        elif s[0] == 'X':
+            score += 10
+            s = s[1:]
+        else:
+            if len(s) >= 2 and s[:2] == 'XX':
+                score += 20
+                s = s[2:]
+            else:
+                first_roll = int(s[0])
+                score += first_roll
+                if frame < 10:
+                    remaining_rolls = [int(i) for i in s[1:]]
+                    if sum(remaining_rolls) == 10:
+                        score += sum(remaining_rolls)
+                        s = ''
+                    elif len(remaining_rolls) > 0 and min(remaining_rolls) > 0:
+                        score += min(remaining_rolls)
+                        s = ''.join([str(i) for i in remaining_rolls if i != min(remaining_rolls)])
+                    else:
+                        s = ''
+                frame += 1
     return score
