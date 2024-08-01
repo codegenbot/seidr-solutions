bcae38f85d20db2a990531f8f37baab8c9193496
beep boop <githubbot@vadim.me>
2024-08-01 18:03:45 +0200

    Solution 4 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/cteneuk8 Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,35 +1,33 @@
-Here's the solution:
-
-#include <vector>
-#include <iostream>
 #include <string>
-
 using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int wordStart = 0;
-    
+    int wordLength;
     for (int i = 0; i <= str.length(); i++) {
-        if (i == str.length() || str[i] == ' ') {
-            string word = str.substr(wordStart, i - wordStart);
-            
-            if (word.length() >= 5) {
-                reverse(word.begin(), word.end());
-            }
-            
-            result += word + " ";
-            wordStart = i + 1;
+        if (str[i] == ' ') {
+            result += str.substr(wordLength, i - wordLength) + " ";
+            wordLength = i + 1;
+        }
+        else if (i == str.length()) {
+            result += str.substr(wordLength);
         }
     }
-    
-    return result;
-}
 
-int main() {
-    string str;
-    while (cin >> str) {
-        cout << spinWords(str) << endl;
+    for (int i = 0; i < result.length(); i++) {
+        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
+            int j = i;
+            while (j > 0 && (result[j-1] >= 'a' && result[j-1] <= 'z' || result[j-1] >= 'A' && result[j-1] <= 'Z')) {
+                j--;
+            }
+            if (j < i) {
+                string temp = result.substr(j, i-j);
+                reverse(temp.begin(), temp.end());
+                result.replace(i - temp.length(), temp.length(), temp);
+                i += temp.length() - 1;
+            }
+        }
     }
-    return 0;
+
+    return result;
 }
