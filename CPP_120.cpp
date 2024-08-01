3b552357448854ef61259eb6dad87d158045d19d
beep boop <githubbot@vadim.me>
2024-08-01 21:15:39 +0200

    Solution 10 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/e9kg7thn Prompt:       |                    ^~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:7: 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp: In function ‘int main()’: 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 02ba7f91-4cc4-4f3e-9973-2b5a4aa70957.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,7 +1,22 @@
-Here is the completed code:
+#include <vector>
+#include <algorithm>
 
-vector<int> maximum(vector<int> arr, int k) {
+bool issame(vector<int> a,vector<int>b){
+    return a == b;
+}
+
+int main() {
+    vector<int> arr;
+    int k;
+    cout << "Enter the size of array: ";
+    cin >> k;
+    cout << "Enter elements of array: ";
+    for(int i = 0; i < k; i++) {
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+    }
     vector<int> result(arr.begin(), arr.begin() + k);
     sort(result.begin(), result.end());
-    return result;
+    return 0;
 }
