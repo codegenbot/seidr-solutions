97cfb59ad6ac5c4ce5eda748654cdf3c750786cc
beep boop <githubbot@vadim.me>
2024-08-01 17:36:17 +0200

    Solution 61 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/ljkmsi02 Prompt: d00a5a17-6078-42bd-9177-9b86020a1b73.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~ d00a5a17-6078-42bd-9177-9b86020a1b73.cpp:22:24: error: ‘string’ was not declared  in this scope    22 | int bowlingScoreHelper(string* s) {       |                        ^~~~~~ d00a5a17-6078-42bd-9177-9b86020a1b73.cpp:22:32: error: ‘s’ was not declared in t his scope    22 | int bowlingScoreHelper(string* s) {       |                                ^

--- bowling.cpp
+++ bowling.cpp
@@ -1,49 +1,41 @@
-```cpp
 #include <string>
 
-int bowlingScore(std::string s) {
+int bowlingScore(string s) {
     int score = 0;
-    int i = 0;
-    while(i < 10) {
+    for(int i=0; i<10; i++) {
         if(s[i] == 'X') {
             score += 30;
-            i++;
         } else if (s[i] == '/') {
-            score += 10 + bowlingScoreHelper(&s.substr(i+1));
-            i+=3;
+            score += 10 + (i > 0 ? bowlingScoreHelper(&s.substr(i+1)) : 0);
         } else {
             int frame = s[i] - '0';
-            i++;
-            if(s[i] == 'X') {
+            if(s[i+1] == 'X') {
                 score += 10 + frame;
-                i++;
-            } else if (s[i] == '/') {
-                score += 10 + frame + bowlingScoreHelper(&s.substr(i+1));
-                i+=3;
+            } else if (s[i+1] == '/') {
+                score += 10 + frame + bowlingScoreHelper(&s.substr(i+2));
             } else {
-                score += 10 + frame + s[i] - '0';
-                i++;
+                score += 10 + frame + s[i+1] - '0';
             }
         }
     }
     return score;
 }
 
-int bowlingScoreHelper(std::string* s) {
+int bowlingScoreHelper(string* s) {
     int score = 0;
     for(int i=0; i<2; i++) {
-        if(s->at(i) == 'X') {
+        if(s->substr(i,1)[0] == 'X') {
             score += 30;
             break;
-        } else if (s->at(i) == '/') {
-            score += 10 + (i > 0 ? s->at(i-1) - '0' : 0);
+        } else if (s->substr(i,1)[0] == '/') {
+            score += 10 + (i > 0 ? s->substr(0,i)[0] - '0' : 0);
             break;
         } else {
-            int frame = s->at(i) - '0';
+            int frame = s->substr(i,1)[0] - '0';
             if(i < 1) {
                 score += frame;
             } else {
-                score += frame + s->at(i-1) - '0';
+                score += frame + s->substr(0,i)[0] - '0';
             }
         }
     }
