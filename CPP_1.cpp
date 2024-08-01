8fd1e13ef4e8a98ec7f7556216d7750bf28f1eb0
beep boop <githubbot@vadim.me>
2024-08-01 13:03:19 +0200

    Solution 4 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/zljys8df Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 27fd7fdf-ae94-4d40-a0b7-da6ae753caba.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 27fd7fdf-ae94-4d40-a0b7-da6ae753caba.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 27fd7fdf-ae94-4d40-a0b7-da6ae753caba.cpp:26: 27fd7fdf-ae94-4d40-a0b7-da6ae753caba.cpp: In function ‘int main()’: 27fd7fdf-ae94-4d40-a0b7-da6ae753caba.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 27fd7fdf-ae94-4d40-a0b7-da6ae753caba.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,15 +1,51 @@
+#include <iostream>
 #include <vector>
+#include <string>
+#include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+using namespace std;
+
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group;
+    int balance = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (balance > 0) {
+                current_group += c;
+            }
+            balance++;
+        } else if (c == ')') {
+            balance--;
+            if (balance > 0) {
+                current_group += c;
+            } else if (balance == 0) {
+                result.push_back(current_group);
+                current_group = "";
+            }
+        }
+    }
+
+    return result;
+}
+
+bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); ++i) {
+
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
-std::vector<std::string> separate_paren_groups(std::string paren_string);
+int main() {
+    assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
+
+    return 0;
+}
