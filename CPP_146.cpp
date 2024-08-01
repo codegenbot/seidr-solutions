bb146a403b07ee2c3d300a4ff172fe972dc799a1
beep boop <githubbot@vadim.me>
2024-08-02 00:22:34 +0200

    Solution 24 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/qfc2ehqe Prompt: bd7d9539-1cf1-4dc4-b860-4ff598cba060.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ bd7d9539-1cf1-4dc4-b860-4ff598cba060.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from bd7d9539-1cf1-4dc4-b860-4ff598cba060.cpp:11: bd7d9539-1cf1-4dc4-b860-4ff598cba060.cpp: In function ‘int main()’: bd7d9539-1cf1-4dc4-b860-4ff598cba060.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 
-int specialFilter(vector<int> nums) {
+int specialFilter(std::vector<int> nums) {
     int count = 0;
     for (int num : nums) {
         if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
@@ -8,3 +8,4 @@ int specialFilter(vector<int> nums) {
         }
     }
     return count;
+}
