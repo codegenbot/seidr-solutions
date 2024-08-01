9e0706c0f29b750500e8529ce379350bdfe69335
beep boop <githubbot@vadim.me>
2024-08-01 18:45:54 +0200

    Solution 0 of CPP/84, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/gxd08yiy Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_84.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+using namespace std;
+
+string solve(int N) {
+    string binary = "";
+    while (N > 0) {
+        if (N % 2 == 0)
+            binary = "0" + binary;
+        else
+            binary = "1" + binary;
+        N /= 2;
+    }
+    int sum = 0;
+    for (int i = 0; i < binary.length(); i++) {
+        sum += (binary[i] - '0');
+    }
+    return binary;
+}
