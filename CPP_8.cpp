d090308595fb8eba87c56b46520e76a8e9b2d727
beep boop <githubbot@vadim.me>
2024-08-01 13:26:30 +0200

    Solution 16 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9d5cmfdu Prompt: In file included from /usr/include/c++/11/vector:67,                  from 787adec6-0771-4e60-8843-5cb3c9595069.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 787adec6-0771-4e60-8843-5cb3c9595069.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 787adec6-0771-4e60-8843-5cb3c9595069.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 787adec6-0771-4e60-8843-5cb3c9595069.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 787adec6-0771-4e60-8843-5cb3c9595069.cpp:14: 787adec6-0771-4e60-8843-5cb3c9595069.cpp: In function ‘int main()’: 787adec6-0771-4e60-8843-5cb3c9595069.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,17 +1,12 @@
 #include <vector>
 #include <cassert>
 
-std::pair<int, int> sum_product(const std::vector<int>& numbers) {
-    int sum = 0;
-    int product = 1;
-    for (int num : numbers) {
-        sum += num;
-        product *= num;
-    }
-    return {sum, product};
+bool issame(std::pair<int, int> a, std::pair<int, int> b) {
+    return a == b;
 }
 
 int main() {
-    assert(sum_product({10}) == std::pair<int, int>({10, 10}));
+    assert(issame(sum_product({10}), {10, 10}));
+    
     return 0;
 }
