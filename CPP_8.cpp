e18e6de784a6f143ddd05b88f8d8724a568b53cc
beep boop <githubbot@vadim.me>
2024-08-01 13:24:49 +0200

    Solution 12 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9d5cmfdu Prompt: In file included from /usr/include/c++/11/vector:67,                  from af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp:14: af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp: In function ‘int main()’: af51baa4-08e5-459d-925b-ea0d2b0a427c.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,10 +1,21 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::pair<int, int> a, std::pair<int, int> b) {
-    return a.first == b.first && a.second == b.second;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::pair<int, int> sum_product(const std::vector<int>& numbers) {
+    int sum = 0;
+    int product = 1;
+    for (int num : numbers) {
+        sum += num;
+        product *= num;
+    }
+    return {sum, product};
 }
 
 int main() {
-    assert(issame(sum_product({10}), {10, 10}));
+    assert(issame(sum_product({10}), std::make_pair(10, 10)));
+    return 0;
 }
