2f792eac50011100193a65fecbd7757c43573dfe
beep boop <githubbot@vadim.me>
2024-08-01 12:48:54 +0200

    Solution 16 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/64uv2o0t Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- basement.cpp
+++ basement.cpp
@@ -1,33 +1,17 @@
 #include <vector>
 using namespace std;
 
-int basement(vector<int> v) {
-    for (int i = 0; i < v.size(); i++) {
-        int sum = 0;
-        for (int j = 0; j <= i; j++) {
-            sum += v[j];
-            if (sum < 0) return i;
-        }
+int basement(vector<int>& nums) {
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        sum += nums[i];
+        if (sum < 0) return i;
     }
     return -1;
 }
 
 int main() {
-    // Test cases
-    vector<int> test1 = {1, -1};
-    cout << basement(test1) << endl;
-
-    vector<int> test2 = {1, -100};
-    cout << basement(test2) << endl;
-
-    vector<int> test3 = {2, -1, 100};
-    cout << basement(test3) << endl;
-
-    vector<int> test4 = {2, -95, 100};
-    cout << basement(test4) << endl;
-
-    vector<int> test5 = {2, -30, 5};
-    cout << basement(test5) << endl;
-    
+    vector<int> nums = {10, -20, 30, -5, 40};
+    cout << basement(nums) << endl;
     return 0;
 }
