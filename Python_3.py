a8f5b7e2872416c74dc49bd57f4853aef01d2faf
beep boop <githubbot@vadim.me>
2024-08-01 12:52:43 +0200

    Solution 1 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/g4zxs4t7 Prompt: Complete the following code given the task description and function signature.

--- Python_3.py
+++ Python_3.py
@@ -1,7 +1,8 @@
+```
 def below_zero(operations: List[int]) -> bool:
     balance = 0
-    for operation in operations:
-        balance += operation
+    for op in operations:
+        balance += op
         if balance < 0:
             return True
     return False
