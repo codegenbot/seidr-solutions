315d581d959bd06e845866fd9bdaedf7698fc8e6
beep boop <githubbot@vadim.me>
2024-08-01 22:54:23 +0200

    Solution 1 of CPP/131, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/1hp4i72w Prompt: Complete the following code given the task description and function signature.

--- CPP_131.cpp
+++ CPP_131.cpp
@@ -1,10 +1,10 @@
-int productOfOddDigits(int n) {
-    int result = 1;
-    while (n > 0) {
+int digits(int n){
+    int product = 1;
+    while(n > 0){
         int digit = n % 10;
-        if (digit % 2 != 0)
-            result *= digit;
+        if(digit % 2 != 0)
+            product *= digit;
         n /= 10;
     }
-    return result;
+    return product;
 }
