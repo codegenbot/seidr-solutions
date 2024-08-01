5c2b357171ff4bdab3bd5e214c701260f245f6f6
beep boop <githubbot@vadim.me>
2024-08-02 01:21:05 +0200

    Solution 8 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/gtgpgdud Prompt:       |                                   ^~~~~~ c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp:20:42: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                          ^~~~~~ c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp:20:49: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp:20:56: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ In file included from c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp:19: c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp: In function ‘int main()’: c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp:32:20: error: ‘get_row’ was not declare d in this scope    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ^~~~~~~ c4693a47-5d3b-44c0-b8cb-216f309ea763.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -1,8 +1,23 @@
 #include <vector>
 
-bool issame(vector<vector<int>> a, vector<vector<int>> b) {
-    if ((a[0] == b[0])) {
-        return a[1] == b[1];
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
+}
+
+vector<vector<int>> get_row(vector<vector<int>> lst, int x){
+    vector<vector<int>> result;
+    for (int i = 0; i < lst.size(); ++i) {
+        for (int j = 0; j < lst[i].size(); ++j) {
+            if (lst[i][j] == x) {
+                result.push_back({i, j});
+            }
+        }
     }
-    return false;
+    sort(result.begin(), result.end(), [](const vector<int>& a, const vector<int>& b){
+        if (a[0] == b[0]) {
+            return a[1] > b[1];
+        }
+        return a[0] < b[0];
+    });
+    return result;
 }
