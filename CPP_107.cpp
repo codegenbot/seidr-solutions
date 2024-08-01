1971bccfb2e89545368c32c904b56ff046bcfaa7
beep boop <githubbot@vadim.me>
2024-08-01 21:52:08 +0200

    Solution 4 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/rcurafgi Prompt: Complete the following code given the task description and function signature.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,33 +1,34 @@
-#include <iostream>
 #include <vector>
+
 using namespace std;
 
 vector<int> even_odd_palindrome(int n) {
     vector<int> result(2);
+    int countEven = 0;
+    int countOdd = 0;
+
     for (int i = 1; i <= n; i++) {
         string str = to_string(i);
         bool isPalindrome = true;
-        for (int j = 0; j < str.length(); j++) {
-            if (str[j] != str[str.length() - j - 1]) {
+
+        for (int j = 0; j < str.length() / 2; j++) {
+            if (str[j] != str[str.length() - 1 - j]) {
                 isPalindrome = false;
                 break;
             }
         }
-        if (isPalindrome && (i % 2 == 0)) {
-            result[0]++;
-        } else if (isPalindrome) {
-            result[1]++;
+
+        if (isPalindrome) {
+            if (i % 2 == 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
         }
     }
-    return result;
-}
 
-int main() {
-    int n;
-    cout << "Enter a positive integer: ";
-    cin >> n;
-    vector<int> res = even_odd_palindrome(n);
-    cout << "Number of even palindromes: " << res[0] << endl;
-    cout << "Number of odd palindromes: " << res[1] << endl;
-    return 0;
+    result[0] = countEven;
+    result[1] = countOdd;
+
+    return result;
 }
