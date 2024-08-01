cd998481dc35d4ab817bb17e473b130abb60df06
beep boop <githubbot@vadim.me>
2024-08-01 18:46:10 +0200

    Solution 67 of CPP/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/rs9p68sc Prompt:    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8f15a8f5-7378-40ff-9ac2-b32d2ef8cc76.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 8f15a8f5-7378-40ff-9ac2-b32d2ef8cc76.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from 8f15a8f5-7378-40ff-9ac2-b32d2ef8cc76.cpp:27: 8f15a8f5-7378-40ff-9ac2-b32d2ef8cc76.cpp: In function ‘int main()’: 8f15a8f5-7378-40ff-9ac2-b32d2ef8cc76.cpp:37:32: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    37 |     assert (issame(parse_music("o| .| o| .| o o| o o|") , {2, 1, 2, 1, 4 , 2, 4, 2}));       |                                ^~~~~~~~~~~~~~~~~~~~~~~       |                                |       |                                const char* 8f15a8f5-7378-40ff-9ac2-b32d2ef8cc76.cpp:4:49: note:   initializing argument 1 o f ‘std::vector<int> parse_music(const int&)’     4 | std::vector<int> parse_music(const std::string& music) {       |                              ~~~~~~~~~~~~~~~~~~~^~~~~

--- CPP_17.cpp
+++ CPP_17.cpp
@@ -33,6 +33,6 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 }
 
 int main() {
-    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 3, 3, 3, 3}));
+    assert(issame(parse_music("o| .| o| .| o o| o o|"), {2, 1, 2, 1, 4, 2, 4, 2}));
     return 0;
 }
