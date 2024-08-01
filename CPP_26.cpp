fb7c4ca01cf0a57ee00bb1cf6843b23f46ccd154
beep boop <githubbot@vadim.me>
2024-08-01 20:58:09 +0200

    Solution 77 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/lim3q57i Prompt:       |             ^~~~~~ 74a7c547-e924-4f76-9145-75c78632548b.cpp:15:20: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 74a7c547-e924-4f76-9145-75c78632548b.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 74a7c547-e924-4f76-9145-75c78632548b.cpp:15:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 74a7c547-e924-4f76-9145-75c78632548b.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 74a7c547-e924-4f76-9145-75c78632548b.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 74a7c547-e924-4f76-9145-75c78632548b.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -3,12 +3,14 @@
 #include <cassert>
 
 namespace std {
-    std::vector<int> remove_duplicates(std::vector<int> numbers) {
-        numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
-        return numbers;
-    }
+    using vector;
+}
 
-    bool issame(std::vector<int> a, std::vector<int> b) {
-        return a == b;
-    }
+std::vector<int> remove_duplicates(std::vector<int> numbers) {
+    numbers.erase(std::unique(numbers.begin(), numbers.end()), numbers.end());
+    return numbers;
+}
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
