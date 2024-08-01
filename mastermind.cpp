44118fee4e187409134c15391e249cf54a9a050b
beep boop <githubbot@vadim.me>
2024-08-01 16:56:52 +0200

    Solution 30 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/xvbrcrad Prompt: this scope    37 |     cin >> code >> guess;       |     ^~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:37:12: error: ‘code’ was not declared i n this scope    37 |     cin >> code >> guess;       |            ^~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:37:20: error: ‘guess’ was not declared in this scope    37 |     cin >> code >> guess;       |                    ^~~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:38:5: error: ‘cout’ was not declared in  this scope    38 |     cout << blackPegs(code, guess) << "\n";       |     ^~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:38:22: error: ‘blackPegs’ cannot be use d as a function    38 |     cout << blackPegs(code, guess) << "\n";       |             ~~~~~~~~~^~~~~~~~~~~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:39:22: error: ‘whitePegs’ cannot be use d as a function    39 |     cout << whitePegs(code, guess) << "\n";       |             ~~~~~~~~~^~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,42 +1,4 @@
 ```c++
-#include <map>
-#include <iostream>
-using namespace std;
-
-int whitePegs(string code, string guess) {
-    int count = 0;
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            count++;
-        }
-    }
-    return count;
-}
-
-int blackPegs(string code, string guess) {
-    int count = 0;
-    map<char, int> codeCount;
-    for (int i = 0; i < 4; ++i) {
-        codeCount[code[i]]++;
-    }
-
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            count++;
-            codeCount[code[i]]--;
-        }
-    }
-
-    for (auto& pair : codeCount) {
-        if (pair.second > 0) {
-            count += min(pair.second, 4 - count);
-            break;
-        }
-    }
-
-    return count;
-}
-
 int main() {
     string code, guess;
     cin >> code >> guess;
