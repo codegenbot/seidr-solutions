debc888c415c7d02763448d5997b7ff02c0999a5
beep boop <githubbot@vadim.me>
2024-08-01 18:04:26 +0200

    Solution 5 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/cteneuk8 Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -3,31 +3,20 @@ using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int wordLength;
+    int wordStart = 0;
     for (int i = 0; i <= str.length(); i++) {
-        if (str[i] == ' ') {
-            result += str.substr(wordLength, i - wordLength) + " ";
-            wordLength = i + 1;
-        }
-        else if (i == str.length()) {
-            result += str.substr(wordLength);
+        if (i == str.length() || str[i] == ' ') {
+            string word = str.substr(wordStart, i - wordStart);
+            if (word.length() >= 5) {
+                result += string(word.rbegin(), word.rend());
+            } else {
+                result += word;
+            }
+            if (i < str.length()) {
+                result += " ";
+            }
+            wordStart = i + 1;
         }
     }
-
-    for (int i = 0; i < result.length(); i++) {
-        if ((result[i] >= 'a' && result[i] <= 'z') || (result[i] >= 'A' && result[i] <= 'Z')) {
-            int j = i;
-            while (j > 0 && (result[j-1] >= 'a' && result[j-1] <= 'z' || result[j-1] >= 'A' && result[j-1] <= 'Z')) {
-                j--;
-            }
-            if (j < i) {
-                string temp = result.substr(j, i-j);
-                reverse(temp.begin(), temp.end());
-                result.replace(i - temp.length(), temp.length(), temp);
-                i += temp.length() - 1;
-            }
-        }
-    }
-
     return result;
 }
