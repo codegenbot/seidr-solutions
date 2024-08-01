afe450020430aba61076650476c7cb8854a40e5f
beep boop <githubbot@vadim.me>
2024-08-01 13:28:49 +0200

    Solution 4 of CPP/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/3168v6y9 Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,29 +1,23 @@
-map<string, int> numMap;
+#include <algorithm>
+#include <sstream>
 
-numMap["zero"] = 0; numMap["one"] = 1; numMap["two"] = 2;
-numMap["three"] = 3; numMap["four"] = 4; numMap["five"] = 5;
-numMap["six"] = 6; numMap["seven"] = 7; numMap["eight"] = 8;
-numMap["nine"] = 9;
+using namespace std;
 
-vector<string> nums;
-string temp;
-for (char c : numbers) {
-    if (c == ' ') {
-        nums.push_back(temp);
-        temp = "";
-    } else {
-        temp += c;
+string sort_numbers(string numbers) {
+    istringstream iss(numbers);
+    string num;
+    vector<string> nums;
+    
+    while (iss >> num) {
+        nums.push_back(num);
     }
+    
+    sort(nums.begin(), nums.end());
+    
+    stringstream ss;
+    for (auto& n : nums) {
+        ss << n << " ";
+    }
+    
+    return ss.str();
 }
-nums.push_back(temp);
-
-sort(nums.begin(), nums.end(), [&numMap](const string& a, const string& b) {
-    return numMap[a] < numMap[b];
-});
-
-string result;
-for (auto num : nums) {
-    result += num + " ";
-}
-
-return result.substr(0, result.length() - 1);
