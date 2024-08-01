b0ba2e3c45ba95ef3303eca86fdb29c3b045e6e1
beep boop <githubbot@vadim.me>
2024-08-01 18:19:01 +0200

    Solution 61 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/id9n41od Prompt: Program did not receive expected input

--- Python_3.py
+++ Python_3.py
@@ -2,8 +2,8 @@ from typing import List
 
 def below_zero(nums: List[int]) -> bool:
     balance = 0
-    for num in nums:
-        balance += num
+    for operation in nums:
+        balance += operation
         if balance < 0:
             return True
     return False
