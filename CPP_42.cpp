af0164a8a34a55ab08927eec89bc0e59cafa0e58
beep boop <githubbot@vadim.me>
2024-08-01 17:48:39 +0200

    Solution 38 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/yi6l0a82 Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp:7: 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp: In function ‘int main()’: 1b80de7b-02f6-4a4c-b2bb-49830b9ec7f6.cpp:17:20: error: ‘incr_list’ was not decla red in this scope    17 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,19 +1,5 @@
-#include <algorithm>
-#include <cassert>
 #include <vector>
-
-bool incr_list(std::vector<int>& lst){
-    for(auto& elem : lst){
-        ++elem;
-    }
-    return true;
-}
 
 bool issame(std::vector<int> a, std::vector<int> b){
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
-}
-
-int main(){
-    // Test cases can be added here for validation
-    return 0;
 }
