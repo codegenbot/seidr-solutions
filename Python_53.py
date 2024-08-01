0d47f4c8cb0bc82f21b5d92bc3c8b9a3dc484b4e
beep boop <githubbot@vadim.me>
2024-08-02 01:28:15 +0200

    Solution 51 of Python/53, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9h68c0sx Prompt: Program did not receive expected input

--- Python_53.py
+++ Python_53.py
@@ -1,9 +1,8 @@
 def add(x: int, y: int):
     return x + y
 
-
-x = int(input("Enter first integer: "))
-y = int(input("Enter second integer: "))
+x = int(input("Enter first integer: \n"))
+y = int(input("Enter second integer: \n"))
 
 result = add(x, y)
 print("Sum of the two integers:", result)
