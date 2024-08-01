c2bf0f449187e0920e7f0cef8798174c37d83c25
beep boop <githubbot@vadim.me>
2024-08-01 22:48:45 +0200

    Solution 6 of CPP/129, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/x3wnftdu Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,39 +1,29 @@
-#include <iostream>
+#include <algorithm>
 #include <vector>
-#include <queue>
+
 using namespace std;
 
-vector<int> minPath(vector<vector<int>>& grid, int k) {
-    int n = grid.size();
-    vector<vector<int>> dir({{-1, 0}, {1, 0}, {0, -1}, {0, 1}});
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            pq.push({grid[i][j], i * n + j});
-        }
-    }
+vector<int> minPath(vector<vector<int>> grid, int k) {
     vector<int> res;
-    while (!pq.empty()) {
-        int val, idx;
-        tie(val, idx) = pq.top();
-        pq.pop();
-        res.push_back(val);
-        if (res.size() == k) break;
-        int x = idx / n, y = idx % n;
-        for (int i = 0; i < 4; ++i) {
-            int nx = x + dir[i][0], ny = y + dir[i][1];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                pq.push({grid[nx][ny], nx * n + ny});
+    for (int i = 0; i < k; ++i) {
+        int maxVal = -1;
+        int row = -1, col = -1;
+        for (int j = 0; j < grid.size(); ++j) {
+            for (int c = 0; c < grid[0].size(); ++c) {
+                if (grid[j][c] > maxVal && res.empty() || find(res.begin(), res.end(), grid[j][c]) == res.end()) {
+                    maxVal = grid[j][c];
+                    row = j;
+                    col = c;
+                }
+            }
+        }
+        res.push_back(maxVal);
+        for (int i = 0; i < grid.size(); ++i) {
+            if (i != row) {
+                swap(grid[i][col], grid[0][0]);
+                break;
             }
         }
     }
     return res;
-}
-
-int main() {
-    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int k = 3;
-    vector<int> res = minPath(grid, k);
-    for (auto x : res) cout << x << " ";
-    return 0;
 }
