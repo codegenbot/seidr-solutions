11d35213a4d4ef7d0facf62a605817f60a4b599d
beep boop <githubbot@vadim.me>
2024-08-01 14:50:44 +0200

    Solution 3 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/912kihmk Prompt: Complete the following code given the task description and function signature.

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,10 +1,4 @@
 double find_zero(vector<double> xs){
-    double sum = 0;
-    int i;
-    for (i=0; i<xs.size(); i++)
-    {
-        if(i%2 != 0) // coefficients of odd powers
-            sum += (-1)*pow(xs[i],1.0/(i+1));
-    }
-    return -sum;
+    double x = -xs[0]/xs[xs.size()-1];
+    return poly(xs,x);
 }
