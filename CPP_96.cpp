5eda39631c3d452d8dd098e46ea7e5cdec1d4392
beep boop <githubbot@vadim.me>
2024-08-02 01:52:51 +0200

    Solution 5 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7g2ttqk5 Prompt:    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4c448536-6020-4741-a580-6f294c55b789.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4c448536-6020-4741-a580-6f294c55b789.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4c448536-6020-4741-a580-6f294c55b789.cpp:21: 4c448536-6020-4741-a580-6f294c55b789.cpp: In function ‘int main()’: 4c448536-6020-4741-a580-6f294c55b789.cpp:31:20: error: ‘count_up_to’ was not dec lared in this scope    31 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 4c448536-6020-4741-a580-6f294c55b789.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -21,7 +21,15 @@ vector<int> count_up_to(int n) {
 }
 
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
