8eb6ac2d21d4bfe64eef6b38ffda3e02bda2ac47
beep boop <githubbot@vadim.me>
2024-08-01 22:06:09 +0200

    Solution 4 of Python/78, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/l4220jz5 Prompt: Complete the following code given the task description and function signature.

--- Python_78.py
+++ Python_78.py
@@ -1,10 +1,7 @@
-```
 def hex_key(num):
-    primes = set([2, 3, 5, 7, 11, 13, 17])
+    primes = {'B': '11', 'D': '13'}
     count = 0
-    for digit in num:
-        if digit.upper() in 'BDF':
-            count += 1
-        elif int(digit, 16) in primes:
+    for char in num:
+        if char.upper() in primes:
             count += 1
     return count
