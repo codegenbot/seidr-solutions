1a67afcdca4c150c1aa46045fd6185799c5529dc
beep boop <githubbot@vadim.me>
2024-08-01 17:09:21 +0200

    Solution 17 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/00fqiu6f Prompt:    22 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ a6282a97-23b7-407b-a362-4ecf836b821f.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ a6282a97-23b7-407b-a362-4ecf836b821f.cpp:22:43: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from a6282a97-23b7-407b-a362-4ecf836b821f.cpp:21: a6282a97-23b7-407b-a362-4ecf836b821f.cpp: In function ‘int main()’: a6282a97-23b7-407b-a362-4ecf836b821f.cpp:31:20: error: ‘sort_even’ was not decla red in this scope    31 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ a6282a97-23b7-407b-a362-4ecf836b821f.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,20 +3,20 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<float> a, std::vector<float> b){
+bool issame(vector<float> a, vector<float> b){
     return a == b;
 }
 
-std::vector<float> sort_even(std::vector<float> l) {
-    std::vector<float> even_values;
-    std::vector<float> sorted_even_values;
+vector<float> sort_even(vector<float> l) {
+    vector<float> even_values;
+    vector<float> sorted_even_values;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_values.push_back(l[i]);
             sorted_even_values.push_back(l[i]);
         }
     }
-    std::sort(sorted_even_values.begin(), sorted_even_values.end());
+    sort(sorted_even_values.begin(), sorted_even_values.end());
     int sorted_even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
