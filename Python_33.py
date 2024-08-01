841f7cd4616449791be61db126721ec68a825087
beep boop <githubbot@vadim.me>
2024-08-01 19:30:43 +0200

    Solution 98 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/c9l29fwk Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b29fa3c-2088-4ab8-a7e4-8daf29104 855.py", line 9, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4b29fa3c-2088-4ab8-a7e4-8daf29104 855.py", line 4, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,3 +1,14 @@
 def sort_third(l: list):
-    threes = [i for i in l if i % 3 == 0]
-    return [i if i % 3 != 0 else (threes[0] if len(threes) > 0 else -1) for i in l]
+    thirds = [
+        (
+            i
+            if i % 3 != 0
+            else (
+                min([x for x in l if x % 3 == 0])
+                if [x for x in l if x % 3 == 0]
+                else [-1]
+            )
+        )
+        for i in l
+    ]
+    return thirds
