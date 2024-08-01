2aaaa009a267a17c1b926862e7b26b4f7d43f0b4
beep boop <githubbot@vadim.me>
2024-08-01 19:54:32 +0200

    Solution 47 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/01ot4m8f Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04457cf8-429d-4b11-9457-c5d6377b1 179.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/04457cf8-429d-4b11-9457-c5d6377b1 179.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -1,13 +1,12 @@
-def sort_even(l: list):
-    return [
-        (
-            x
-            if i % 2 != 0
-            else (
-                sorted([y for j, y in enumerate(l) if j * 2 == i])[0]
-                if any(j * 2 == i for j in range(len(l)))
-                else None
-            )
+return [
+    (
+        x
+        if i % 2 != 0
+        else (
+            sorted([y for j, y in enumerate(l) if j * 2 == i])[0]
+            if any(j * 2 == i for j in range(len(l)))
+            else None
         )
-        for i, x in enumerate(l)
-    ]
+    )
+    for i, x in enumerate(l)
+]
