37a1f577962dc6a2b64e4b72c4082977b69b8229
beep boop <githubbot@vadim.me>
2024-08-02 01:57:25 +0200

    Solution 60 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/xjngajct Prompt: 1509e629-d6fd-4f04-b225-e84f1a536f40.cpp: In function ‘std::string string_to_md5 (std::string)’: 1509e629-d6fd-4f04-b225-e84f1a536f40.cpp:10:16: error: aggregate ‘EVP_MD_CTX mdc tx’ has incomplete type and cannot be defined    10 |     EVP_MD_CTX mdctx;       |                ^~~~~ 1509e629-d6fd-4f04-b225-e84f1a536f40.cpp:11:25: error: invalid conversion from ‘ const EVP_MD*’ {aka ‘const evp_md_st*’} to ‘EVP_MD*’ {aka ‘evp_md_st*’} [-fpermi ssive]    11 |     EVP_MD *md = EVP_md5();       |                  ~~~~~~~^~       |                         |       |                         const EVP_MD* {aka const evp_md_st*}

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,28 +1,35 @@
-#include <openssl/ssl.h>
-#include <openssl/crypto.h>
+#include <openssl/evp.h>
 #include <string>
 
 using namespace std;
 
-string string_to_md5(string text) {
-    if (text.empty()) return "";
+int main() {
+    #ifdef _WIN32
+       #pragma comment(lib, "libeay32.lib")
+        #else
+            #include <openssl/ssl.h>
+    #endif
 
-    unsigned char result[16];
-    EVP_MD_CTX mdctx;
-    EVP_MD *md = EVP_md5();
-    unsigned char *d = NULL;
-    size_t len = 0;
+    string string_to_md5(string text) {
+        if (text.empty()) return "";
 
-    EVP_MD_CTX_init(&mdctx);
-    EVP_DigestInit_ex(&mdctx, md, NULL);
-    EVP_DigestUpdate(&mdctx, text.c_str(), text.size());
-    EVP_DigestFinal_ex(&mdctx, result, &len);
+        unsigned char result[16];
+        EVP_MD_CTX mdctx;
+        EVP_MD *md = NULL;
+        unsigned char *d = NULL;
+        size_t len = 0;
 
-    string output;
-    for (int i = 0; i < len; ++i) {
-        char c = static_cast<char>(result[i]);
-        output += c;
+        EVP_MD_CTX_init(&mdctx);
+        md = EVP_md5();
+        EVP_DigestInit_ex(&mdctx, md, NULL);
+        EVP_DigestUpdate(&mdctx, text.c_str(), text.size());
+        EVP_DigestFinal_ex(&mdctx, result, &len);
+
+        string output;
+        for (int i = 0; i < len; ++i) {
+            char c = static_cast<char>(result[i]);
+            output += c;
+        }
+
+        return output;
     }
-
-    return output;
-}
