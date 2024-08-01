2de662dba9ec21067b5e83ac74a4a28ba266f2fd
beep boop <githubbot@vadim.me>
2024-08-02 00:59:52 +0200

    Solution 0 of CPP/130, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/nkxncycp Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_130.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+vector<int> tri(int n) {
+    vector<int> result(n + 1);
+    if (n >= 0) {
+        result[0] = 3;
+        if (n >= 1) {
+            result[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                if (i % 2 == 0) {
+                    result[i] = 1 + i / 2;
+                } else {
+                    result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+                }
+            }
+        }
+    }
+    return result;
+}
