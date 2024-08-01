45b179cdbb7fb0727856ba02b8feeb7583bc6818
beep boop <githubbot@vadim.me>
2024-08-01 14:12:55 +0200

    Solution 41 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/llclthe0 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0139077a-8f28-4874-83f2-3226c61d6 a45.py", line 25, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0139077a-8f28-4874-83f2-3226c61d6 a45.py", line 22, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,10 +1,11 @@
+```Python
 def make_palindrome(string: str) -> str:
     s = s.lower()
     new_string = []
     
     for char in s:
         new_string.append(char)
-        new_string.append(char
+        new_string.append(char)
         
     # Add the first and last characters at the beginning
     new_string.insert(0, s[0])
