b971d8d05a8232b5735e7abf6732b93196f49b4f
beep boop <githubbot@vadim.me>
2024-08-01 17:06:39 +0200

    Solution 42 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/xvbrcrad Prompt: this scope    37 |     cin >> code >> guess;       |     ^~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:37:12: error: ‘code’ was not declared i n this scope    37 |     cin >> code >> guess;       |            ^~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:37:20: error: ‘guess’ was not declared in this scope    37 |     cin >> code >> guess;       |                    ^~~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:38:5: error: ‘cout’ was not declared in  this scope    38 |     cout << blackPegs(code, guess) << "\n";       |     ^~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:38:22: error: ‘blackPegs’ cannot be use d as a function    38 |     cout << blackPegs(code, guess) << "\n";       |             ~~~~~~~~~^~~~~~~~~~~~~ 0552813f-fc6d-4d91-bec4-159e3b7cfe51.cpp:39:22: error: ‘whitePegs’ cannot be use d as a function    39 |     cout << whitePegs(code, guess) << "\n";       |             ~~~~~~~~~^~~~~~~~~~~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -37,9 +37,7 @@ int blackPegs(string code, string guess) {
 
 int main() {
     string code, guess;
-
     cin >> code >> guess;
-
     cout << blackPegs(code, guess) << "\n";
     cout << whitePegs(code, guess) << "\n";
     return 0;
