60528f93730b2ce459ab1b2b9d2b3665aab19700
beep boop <githubbot@vadim.me>
2024-08-01 18:05:46 +0200

    Solution 52 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/cwyd7lvs Prompt:       |                    ^~~ f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp:21: f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp: In function ‘int main()’: f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp:31:20: error: ‘pluck’ was not declared in this scope    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ f9c8eebf-6f89-418a-9e5f-41fed24a9fec.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -2,15 +2,11 @@
 #include <algorithm>
 
 std::vector<int> pluck(std::vector<int> arr) {
-    std::vector<int> result;
-    
-    if(arr.empty()) {
-        return result;
-    }
-    
+    if(arr.empty()) return std::vector<int>();
+
     int smallest_even = INT_MAX;
     int index_smallest_even = -1;
-    
+
     for(int i=0; i<arr.size(); i++) {
         if(arr[i]%2==0 && arr[i] < smallest_even) {
             smallest_even = arr[i];
@@ -18,6 +14,7 @@ std::vector<int> pluck(std::vector<int> arr) {
         }
     }
 
+    std::vector<int> result;
     result.push_back(smallest_even);
     result.push_back(index_smallest_even);
 
