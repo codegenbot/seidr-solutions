76754685811ad4144cc5243c567f69a56a0d11f0
beep boop <githubbot@vadim.me>
2024-08-01 20:17:27 +0200

    Solution 26 of CPP/75, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/nhhy9hhf Prompt:    19 |         cout << "True" << endl;       |         ^~~~ 9f6238da-9ed4-4823-bd2b-0ee99d1579a2.cpp:19:27: error: ‘endl’ was not declared i n this scope    19 |         cout << "True" << endl;       |                           ^~~~ 9f6238da-9ed4-4823-bd2b-0ee99d1579a2.cpp:21:9: error: ‘cout’ was not declared in  this scope    21 |         cout << "False" << endl;       |         ^~~~ 9f6238da-9ed4-4823-bd2b-0ee99d1579a2.cpp:21:28: error: ‘endl’ was not declared i n this scope    21 |         cout << "False" << endl;       |                            ^~~~ 9f6238da-9ed4-4823-bd2b-0ee99d1579a2.cpp: At global scope: 9f6238da-9ed4-4823-bd2b-0ee99d1579a2.cpp:27:5: error: redefinition of ‘int main( )’    27 | int main(){       |     ^~~~ 9f6238da-9ed4-4823-bd2b-0ee99d1579a2.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_75.cpp
+++ CPP_75.cpp
@@ -1,38 +1,28 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-bool is_prime(int n) {
-    if (n < 2) return false;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) return false;
+bool is_multiply_prime(int num);
+
+bool is_multiply_prime(int num) {
+    if (num < 2) {
+        return false;
+    }
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            return false;
+        }
     }
     return true;
 }
 
-bool is_multiply_prime(int a){
-    vector<int> primes;
-    for (int i = 2; i <= 100; i++) {
-        if (is_prime(i)) {
-            primes.push_back(i);
-        }
-    }
-    for (int i = 0; i < primes.size(); i++) {
-        for (int j = i; j < primes.size(); j++) {
-            for (int k = j; k < primes.size(); k++) {
-                if (primes[i] * primes[j] * primes[k] == a) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
 int main() {
-    int num;
-    cin >> num;
-    cout << (is_multiply_prime(num) ? "true" : "false") << endl;
+    int n;
+    cin >> n;
+    if (is_multiply_prime(n)) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
     return 0;
 }
