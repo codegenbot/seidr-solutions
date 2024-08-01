c6cda6a1742a7d1e6271d52e121200b671c4d217
beep boop <githubbot@vadim.me>
2024-08-01 22:10:38 +0200

    Solution 80 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/xvkcn3c9 Prompt:       |                              ^~~~~~ 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp:25:37: error: ‘string’ was not declared  in this scope    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp:24: 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp: In function ‘int main()’: 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp:34:20: error: ‘by_length’ was not decla red in this scope    34 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 41d872b2-c98c-4429-8077-0c0a2b5c25ae.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,44 +1,4 @@
 ```cpp
 bool issame(vector<string> a, vector<string> b) {
-    if (a == b)
-        return true;
-    for (int i = 0; i < a.size() && i < b.size(); i++) {
-        if (a[i] != b[i])
-            return false;
-    }
-    return a.size() == b.size();
-}
-
-int main() {
-    vector<string> by_length(vector<int> arr) {
-        vector<int> nums;
-        for (int i : arr) {
-            if (i >= 1 && i <= 9) {
-                nums.push_back(i);
-            }
-        }
-        
-        sort(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.end());
-
-        vector<string> result;
-        map<int, string> numMap = {{1, "One"}, {2, "Two"}, {3, "Three"}, 
-                                   {4, "Four"}, {5, "Five"}, {6, "Six"}, 
-                                   {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-        
-        for (int i : nums) {
-            result.push_back(numMap[i]);
-        }
-        
-        return result;
-    }
-
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    cout << "The sorted and reversed numbers in words are: ";
-    for (int i : by_length(arr)) {
-        cout << i << " ";
-    }
-    cout << endl;
-    
-    return 0;
+    return a == b;
 }
