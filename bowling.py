9c1f1453b34974776a85ab07bac471620b22e56e
beep boop <githubbot@vadim.me>
2024-08-02 01:05:22 +0200

    Solution 32 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/repraek3 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,27 +1,25 @@
 ```
-def bowling_score(rolls):
+def bowling_score(frames):
     score = 0
-    roll_index = 0
-    for frame in range(1, 11):
-        if rolls[roll_index] == 'X':
+    for frame in frames.split('/'):
+        if len(frame) == 1:
             score += 10
-            roll_index += 1
-        elif rolls[roll_index:roll_index+2].count('/'):
-            first_roll, second_roll = map(int, rolls[roll_index:roll_index+2].split('/'))
-            score += first_roll + second_roll
-            roll_index += 2
+        elif len(frame) == 2:
+            first_roll = int(frame[0])
+            second_roll = int(frame[1])
+            if first_roll + second_roll == 10:
+                score += 10
+            else:
+                score += first_roll + second_roll
         else:
-            for i in range(1, 3):
-                if i == 1 and rolls[roll_index] == 'X':
-                    score += 10
-                    roll_index += 1
-                    break
-                elif i == 2 and rolls[roll_index:roll_index+2].count('/'):
-                    first_roll, second_roll = map(int, rolls[roll_index:roll_index+2].split('/'))
-                    score += first_roll + second_roll
-                    roll_index += 2
-                    break
-                else:
-                    score += int(rolls[roll_index])
-                    roll_index += 1
+            first_roll = int(frame[0])
+            second_roll = int(frame[1])
+            third_roll = int(frame[2])
+            if first_roll + second_roll + third_roll == 30:
+                score += 30
+            elif first_roll + second_roll == 10:
+                score += 10
+                score += third_roll
+            else:
+                score += first_roll + second_roll + third_roll
     return score
