5c0be442fe97ea41790da01e855148d50d00c72e
beep boop <githubbot@vadim.me>
2024-08-01 15:19:27 +0200

    Solution 12 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run2/runs/9k91f4nw Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,26 +1,28 @@
-int bowlingScore(const string& input) {
+int bowlingScore(string bowls) {
     int score = 0;
-    bool inFrame = false;
-    queue<int> rolls;
-    
-    for (char c : input) {
-        if (c == 'X') {
-            score += 10 + (inFrame ? 10 : 0);
-            inFrame = false;
-        } else if (c == '/') {
-            int currentRoll = 10 - rolls.front();
-            score += rolls.front();
-            rolls.pop();
-            while (!rolls.empty() && rolls.front() != currentRoll) {
-                score += rolls.front();
-                rolls.pop();
+    for (int i = 0; i < 10; i++) {
+        if (isdigit(bowls[i])) { // Strike or spare roll
+            int frameScore = 0;
+            if (isupper(bowls[i])) { // Strike
+                frameScore = 10;
+            } else if (i < 9 && to_string(stoi(bowls.substr(i, 1)) + stoi(bowls.substr(i + 2, 1))) == bowls.substr(i, 2)) {
+                frameScore = 10; // Spare
+            } else { // Normal roll(s)
+                int rolls = stoi(bowls.substr(i, 1));
+                if (i < 9) {
+                    rolls += stoi(bowls.substr(i + 2, 1));
+                }
+                frameScore = rolls;
             }
-            inFrame = true;
-        } else if (isdigit(c)) {
-            int roll = c - '0';
-            rolls.push(roll);
+        } else { // Normal roll
+            int rolls = 0;
+            for (; i < 10; i++) {
+                if (!isdigit(bowls[i])) break;
+                rolls++;
+            }
+            score += rolls;
         }
+        score += frameScore;
     }
-    
     return score;
 }
