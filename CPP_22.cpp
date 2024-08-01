f947e78784598efa980a45180a8eb0f70158a8b0
beep boop <githubbot@vadim.me>
2024-08-01 14:54:41 +0200

    Solution 8 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/kjcrddjw Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 71db21f2-f0ce-41b3-b600-a49c51823a2e.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 71db21f2-f0ce-41b3-b600-a49c51823a2e.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 71db21f2-f0ce-41b3-b600-a49c51823a2e.cpp:11: 71db21f2-f0ce-41b3-b600-a49c51823a2e.cpp: In function ‘int main()’: 71db21f2-f0ce-41b3-b600-a49c51823a2e.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 71db21f2-f0ce-41b3-b600-a49c51823a2e.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,19 +1,19 @@
+#include <vector>
+#include <list>
+#include <boost/any.hpp>
 
-vector<int> filter_integers(const list<any>& values){
+using namespace std;
+
+vector<int> filter_integers(list<boost::any> values){
     vector<int> integers;
     for(const auto& val : values){
         if(val.type() == typeid(int)){
-            integers.push_back(any_cast<int>(val));
+            integers.push_back(boost::any_cast<int>(val));
         }
     }
     return integers;
 }
 
-bool issame(const vector<int>& a, const vector<int>& b){
-    return a == b;
-}
-
-int main(){
-    assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}));
-    return 0;
+bool issame(vector<int> a, vector<int> b){
+    return (a == b);
 }
