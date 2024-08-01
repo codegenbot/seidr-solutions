c9525cbdbd576a19507fda5f69a13113c1fd4d95
beep boop <githubbot@vadim.me>
2024-08-01 21:50:06 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/bod1sipf Prompt: 07d9e284-e19e-43a4-83b4-d086e7d37895.cpp:1:1: error: expected unqualified-id bef ore ‘for’     1 | for (int i = 0; i < n; ++i) {       | ^~~ 07d9e284-e19e-43a4-83b4-d086e7d37895.cpp:1:17: error: ‘i’ does not name a type     1 | for (int i = 0; i < n; ++i) {       |                 ^ 07d9e284-e19e-43a4-83b4-d086e7d37895.cpp:1:24: error: expected unqualified-id be fore ‘++’ token     1 | for (int i = 0; i < n; ++i) {       |                        ^~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,41 +1,4 @@
-#include <iostream>
-#include <vector>
-
-int main() {
-    int n;
-    std::vector<int> vec;
-
-    std::cin >> n;
-
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cin >> num;
-        vec.push_back(num);
-    }
-
-    int total_sum = 0;
-    for (int i = 0; i < n; ++i) {
-        total_sum += vec[i];
-    }
-
-    int left_sum = 0;
-    int right_sum = total_sum;
-
-    int cut_spot = -1;
-    int min_diff = total_sum;
-
-    for (int i = 0; i < n; ++i) {
-        left_sum += vec[i];
-        right_sum -= vec[i];
-        int diff = std::abs(left_sum - right_sum);
-        if (diff < min_diff) {
-            min_diff = diff;
-            cut_spot = i;
-        }
-    }
-
-    std::vector<int> subvector1(vec.begin(), vec.begin() + cut_spot + 1);
-    std::vector<int> subvector2(vec.begin() + cut_spot + 1, vec.end());
-
-    return 0;
+int n = vec.size();
+for (int i = 0; i < n; ++i) {
+    std::cout << vec[i] << " ";
 }
