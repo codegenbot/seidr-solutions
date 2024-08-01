ccdd12ee17ac27e5b0630e25204d64f89b012059
beep boop <githubbot@vadim.me>
2024-08-02 01:39:38 +0200

    Solution 36 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/l6hmhmtf Prompt: f29f0d17-cfc8-4add-9b20-3aad7dedd936.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,23 +3,39 @@ using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (int)std::max((double)a.convert_to<int>(), b.convert_to<double>());
+        return boost::any((int)std::max((double)a.convert_to<int>(), b.convert_to<double>()));
     }
     else if (a.type() == typeid(float) && b.type() == typeid(double)) {
         return std::max(a, b);
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        return (std::string)std::max(a.convert_to<std::string>(), b.convert_to<std::string>());
+        return boost::any((std::string)std::max(a.convert_to<std::string>(), b.convert_to<std::string>()));
     }
     else if (a.type() == typeid(int) && b.type() == typeid(std::string)) {
-        int num = a.convert_to<int>();
-        std::string str = b.convert_to<std::string>();
-        return (num > std::stoi(str)) ? a : ((num < std::stoi(str)) ? b : boost::any("None"));
+        int x = (int)a.convert_to<int>();
+        std::string y = b.convert_to<std::string>();
+        if (x > std::stoi(y)) {
+            return a;
+        }
+        else if (x < std::stoi(y)) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
     else if (a.type() == typeid(std::string) && b.type() == typeid(int)) {
-        int num = a.convert_to<std::string>() != "" ? std::stoi(a.convert_to<std::string>()) : 0;
-        int str_num = b.convert_to<int>();
-        return ((num > str_num) ? a : ((num < str_num) ? b : boost::any("None")));
+        std::string x = a.convert_to<std::string>();
+        int y = (int)b.convert_to<int>();
+        if (std::stoi(x) > y) {
+            return a;
+        }
+        else if (std::stoi(x) < y) {
+            return b;
+        }
+        else {
+            return boost::any("None");
+        }
     }
     else {
         return boost::any("None");
