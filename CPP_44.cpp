a4281caae4d89f0da324456f2633f509aab7beda
beep boop <githubbot@vadim.me>
2024-08-01 18:12:26 +0200

    Solution 58 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/sl0y4p88 Prompt: In file included from badc2b06-df79-4728-b749-c6df34ecbd95.cpp:8: badc2b06-df79-4728-b749-c6df34ecbd95.cpp: In function ‘int main()’: badc2b06-df79-4728-b749-c6df34ecbd95.cpp:12:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    12 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from badc2b06-df79-4728-b749-c6df34ecbd95.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,13 +1,6 @@
 #include <string>
 #include <cassert>
 
-std::string change_base(int x, int base){
-    // Add logic to change base here
-    return std::to_string(x);
-}
-
-int main(){
-    int x = 10;
-    assert(change_base(x, x + 1) == change_base(x, 10)); 
-    return 0;
+std::string change_base(int num, int base){
+    return std::std::to_string(num);
 }
