04299165cf8ec060976f235d1bad18a92ba92cf1
beep boop <githubbot@vadim.me>
2024-08-03 00:38:54 +0200

    Solution 50 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/c07brn46 Prompt: d5642731-4dd5-47b5-9d9f-bfff80046247.cpp: In function ‘bool iscuber(int)’: d5642731-4dd5-47b5-9d9f-bfff80046247.cpp:3:12: error: ‘pow’ was not declared in this scope     3 |     while (pow(i,3) <= a){       |            ^~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,19 +1,10 @@
-```cpp
 #include <cmath>
-#include <cassert>
 
 bool iscuber(int a){
     int i = 1;
-    while (true) {
-        if (pow(i, 3) == a)
+    while (pow(i,3) <= a){
+        if(pow(i,3) == a)
             return true;
-        else if (pow(i, 3) > a)
-            return false;
         i++;
     }
-}
-
-int main() {
-    assert(iscuber(1729) == false);
-    return 0;
-}
+    return false;
