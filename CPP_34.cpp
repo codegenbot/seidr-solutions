1cdca504e29a8ceee3ab059a8e1a623bcf46af1f
beep boop <githubbot@vadim.me>
2024-08-02 21:51:45 +0200

    Solution 3 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wjce3cw5 Prompt: Complete the following code given the task description and function signature.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,18 +1,7 @@
 vector<int> unique(vector<int> l){
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
-    sort(result.begin(), result.end());
-    return vector<int>(unique_copy(result.begin(), result.end()));
-}
-
-vector<int>::iterator unique_copy(vector<int>::iterator start, vector<int>::iterator end) {
-    if (start == end)
-        return end;
- 
-    std::vector<int> temp;
-    for (; start != end; ++start) {
-        if (std::find_if(temp.begin(), temp.end(),
-                [&](int x) { return x == *start; }) == temp.end())
-            temp.push_back(*start);
-    }
-    return temp.begin();
+    vector<int> result(l.begin(),l.end());
+    sort(result.begin(),result.end());
+    auto it = unique(result.begin(),result.end());
+    result.erase(it,result.end());
+    return result;
 }
