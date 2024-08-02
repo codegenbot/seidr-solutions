ec2aeb7dbb98a027f8a251fa9c51c99ea1514510
beep boop <githubbot@vadim.me>
2024-08-02 19:08:25 +0200

    Solution 96 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/2wlyjfaq Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ee86b67-a91d-42c9-a9a3-da4585d35 d67.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -2,19 +2,19 @@ from typing import List
 
 
 def parse_music(music_string: str) -> List[int]:
-    beats_per_note = {"o": 4, "o|": 2, ".|": 1}
+    beats_per_note = {'o': 4, 'o|': 2, '.|': 1}
     result = []
     i = 0
     while i < len(music_string):
-        if music_string[i] == ".":
+        if music_string[i] == '.':
             i += 3
         else:
-            note = music_string[i : i + 2]
+            note = music_string[i:i+2]
             i += 2
             if note in beats_per_note:
                 result.append(beats_per_note[note])
-            elif note[0] == "o":
-                result.extend([beats_per_note["o"]] * (len(note) - 1))
-            else:
-                result.extend([beats_per_note["o|"]] * (len(note) - 1))
+            elif note[0] == 'o':
+                result.append(4)
+            elif note[0] == '.':
+                result.append(1)
     return result
