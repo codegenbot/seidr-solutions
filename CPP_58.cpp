1519dae6a360d90287e97681fc20367b6a575c7e
beep boop <githubbot@vadim.me>
2024-08-02 12:24:36 +0200

    Solution 6 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60kqal8z Prompt: Complete the following code given the task description and function signature.

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,15 +1,14 @@
-#include<stdio.h>
-#include<vector>
-#include<algorithm>
+#include <set>
 using namespace std;
 
-vector<int> common(vector<int> l1,vector<int> l2){
-    vector<int> result;
-    set<int> s1(l1.begin(),l1.end());
-    set<int> s2(l2.begin(),l2.end());
-    
-    set_intersection(s1.begin(),s1.end(),s2.begin(),s2.end(),
-                     inserter(result,result.begin()));
-    
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+
+    set<int> intersection;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(intersection, intersection.begin()));
+
+    vector<int> result(intersection.begin(), intersection.end());
     return result;
 }
