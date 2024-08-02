01fd3b87bb706e33111f7ea33e2e3c26a260f235
beep boop <githubbot@vadim.me>
2024-08-02 03:19:06 +0200

    Solution 2 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9gt9qom5 Prompt:       |                              ^~~~~~ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:28: 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp: In function ‘int main()’: 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:38:20: error: ‘bf’ was not declared in this scope    38 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,27 +1,16 @@
-vector<string> bf(string planet1, string planet2) {
-    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
-    vector<string> result;
+#include <vector>
+#include <string>
 
-    int index1 = -1, index2 = -1;
-    for (int i = 0; i < planets.size(); ++i) {
-        if (planets[i] == planet1) {
-            index1 = i;
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
-        if (planets[i] == planet2) {
-            index2 = i;
-        }
     }
-
-    if (index1 == -1 || index2 == -1) {
-        return result;
-    }
-
-    int start = min(index1, index2);
-    int end = max(index1, index2);
-
-    for (int i = start + 1; i < end; ++i) {
-        result.push_back(planets[i]);
-    }
-
-    return result;
+    
+    return true;
 }
