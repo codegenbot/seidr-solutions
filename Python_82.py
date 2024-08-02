8bed1c5bc9c6ec1f1aa84d7c5c8eefc8c67af050
beep boop <githubbot@vadim.me>
2024-08-02 20:36:32 +0200

    Solution 1 of Python/82, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i9m0bnrf Prompt: Complete the following code given the task description and function signature.

--- Python_82.py
+++ Python_82.py
@@ -1,4 +1,2 @@
 def prime_length(string):
-    return all(string_length % i != 0 for i in range(2, int(string_length ** 0.5) + 1)) if string_length > 1 else False
-
-prime_length('Hello')
+    return all(string_length % i != 0 for i in range(2, int(len(string) ** 0.5) + 1)) if len(string) > 1 else False
