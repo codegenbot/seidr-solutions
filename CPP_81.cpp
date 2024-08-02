17aaf50bfbbcb80a2c7e066a308308b506a564a7
beep boop <githubbot@vadim.me>
2024-08-02 14:11:26 +0200

    Solution 76 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/d74q01m3 Prompt:       |                              ^~~~~~ 39543967-3ba6-4b73-a2d6-492fc349622b.cpp:35:37: error: ‘string’ was not declared  in this scope    35 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 39543967-3ba6-4b73-a2d6-492fc349622b.cpp:35:44: error: ‘b’ was not declared in t his scope    35 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 39543967-3ba6-4b73-a2d6-492fc349622b.cpp:35:45: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 39543967-3ba6-4b73-a2d6-492fc349622b.cpp:34: 39543967-3ba6-4b73-a2d6-492fc349622b.cpp: In function ‘int main()’: 39543967-3ba6-4b73-a2d6-492fc349622b.cpp:44:20: error: ‘numerical_letter_grade’ was not declared in this scope    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 39543967-3ba6-4b73-a2d6-492fc349622b.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -28,7 +28,7 @@ vector<string> numerical_letter_grade(vector<float> grades) {
         else if (grade > 0.7)
             letterGrade = "D";
         else
-            letterGrade = "F";
+            letterGrade = "E";
         result.push_back(letterGrade);
     }
     return result;
