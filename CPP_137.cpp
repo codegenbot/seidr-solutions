097b128e9087b6ed092670de756b9ba366206ff0
beep boop <githubbot@vadim.me>
2024-08-02 17:04:25 +0200

    Solution 62 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/veoq9x4w Prompt: 66e606ab-e306-4312-bcfa-9edd2af6d1f1.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 any compare_one(any a, any b) {
-    if (any_cast<double>(a) && any_cast<double>(b)) {
-        return (any_cast<double>(a) > any_cast<double>(b)) ? a : ((any_cast<double>(a) == any_cast<double>(b))) ? any("None") : b;
-    } else if (any_cast<string>(a) && any_cast<string>(b)) {
-        return (stod(any_cast<string>(a).c_str()) > stod(any_cast<string>(b).c_str())) ? a : ((stod(any_cast<string>(a).c_str()) == stod(any_cast<string>(b).c_str()))) ? any("None") : b;
+    if (any_cast<double>(a) > any_cast<double>(b)) {
+        return a;
+    } else if (any_cast<double>(a) == any_cast<double>(b)) {
+        return "None";
     } else {
-        return "Invalid input type";
+        return b;
     }
 }
