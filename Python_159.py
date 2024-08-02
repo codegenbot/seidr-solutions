84c1107cafbc6e556c72839a634c201905d17ff0
beep boop <githubbot@vadim.me>
2024-08-02 04:42:31 +0200

    Solution 10 of Python/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jwe5bwx4 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01640b31-87ae-43ce-84ce-afc03dd39 01a.py", line 13, in <module>     check(eat)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01640b31-87ae-43ce-84ce-afc03dd39 01a.py", line 10, in check     assert eat(4, 5, 1) == [5, 0], "Error"            ^^^^^^^^^^^^^^^^^^^^^^ AssertionError: Error

--- Python_159.py
+++ Python_159.py
@@ -1,3 +1,4 @@
 def eat(number, need, remaining):
-    total = number + (need - remaining) if remaining >= need else need
-    return [total, abs(remaining - need)]
+    eaten = min(need, number + (need - (remaining - (number - need)) if remaining < need else 0))
+    left_carrots = max(0, min(1000, remaining - (need - eaten)))
+    return [eaten, left_carrots]
