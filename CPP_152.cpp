eb6a3a486c443dff3247dcd38fc4c062cfb0dc31
beep boop <githubbot@vadim.me>
2024-08-02 02:50:14 +0200

    Solution 27 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4kmfrqto Prompt:       |                    ^~~ 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp:14:27: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp:13: 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp: In function ‘int main()’: 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp:23:20: error: ‘compare’ was not declare d in this scope    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 0dd2bb48-46e7-4fb9-accd-0105aab6364c.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -7,17 +7,20 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
-            result.push_back(2);
-        } else if (std::find(game.begin(), game.end(), guess[i]) != game.end()) {
-            result.push_back(1);
-        } else {
             result.push_back(0);
+        } else {
+            result.push_back(abs(guess[i] - game[i]));
         }
     }
     return result;
 }
 
 int main() {
-    assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), {2, 1, 2, 0}));
+    vector<int> game = {1, 2, 3, 5};
+    vector<int> guess = {-1, 2, 3, 4};
+    cout << "The comparison is: ";
+    for (int i : compare(game, guess)) {
+        cout << i << " ";
+    }
     return 0;
 }
