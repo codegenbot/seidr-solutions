5c70d7b807e7a0c9b05a25d6740a36afd2d5c5be
beep boop <githubbot@vadim.me>
2024-08-02 03:40:27 +0200

    Solution 0 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7tmteijs Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_162.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+string string_to_md5(string text) {
+    if (text.empty()) {
+        return "";
+    }
+
+    unsigned char mdBuffer[16];
+    MD5_CTX mdContext;
+    MD5_Init(&mdContext);
+    const char* ptr = text.c_str();
+    size_t len = text.length();
+    MD5_Update(&mdContext, ptr, len);
+    MD5_Final(mdBuffer, &mdContext);
+
+    stringstream ss;
+    for (int i = 0; i < 16; ++i) {
+        ss << setfill('0') << setw(2) << hex << (int)mdBuffer[i];
+    }
+
+    return ss.str();
+}
