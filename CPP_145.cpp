d5315ec316e5565ee36b4c45bbbc576227710586
beep boop <githubbot@vadim.me>
2024-08-02 05:33:54 +0200

    Solution 26 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/brdak4ds Prompt: 5854b0b8-f24d-43e0-b814-0bce9e66d10f.cpp: In function ‘std::vector<int> order_by _points(std::vector<int>)’: 5854b0b8-f24d-43e0-b814-0bce9e66d10f.cpp:9:1: warning: no return statement in fu nction returning non-void [-Wreturn-type]     9 | }       | ^ 5854b0b8-f24d-43e0-b814-0bce9e66d10f.cpp: At global scope: 5854b0b8-f24d-43e0-b814-0bce9e66d10f.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 5854b0b8-f24d-43e0-b814-0bce9e66d10f.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,57 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-bool issame(const vector<int>& a, const vector<int>& b) {
-    int sum_a = 0, sum_b = 0;
-    for (int num : a) {
-        int temp_a = abs(num);
-        while (temp_a > 0) {
-            sum_a += temp_a % 10;
-            temp_a /= 10;
-        }
-    }
-    
-    for (int num : b) {
-        int temp_b = abs(num);
-        while (temp_b > 0) {
-            sum_b += temp_b % 10;
-            temp_b /= 10;
-        }
-    }
-    
-    return sum_a == sum_b;
-}
-
-vector<int> order_by_points(const vector<int>& nums) {
-    vector<int> sorted_nums = nums;
-    sort(sorted_nums.begin(), sorted_nums.end(), [](int a, int b) {
-        int sum_a = 0, sum_b = 0;
-        int temp_a = abs(a), temp_b = abs(b);
-        while (temp_a > 0) {
-            sum_a += temp_a % 10;
-            temp_a /= 10;
-        }
-        while (temp_b > 0) {
-            sum_b += temp_b % 10;
-            temp_b /= 10;
-        }
-        if (sum_a == sum_b) {
-            return a < b;
-        }
-        return sum_a < sum_b;
-    });
-    
-    return sorted_nums;
+vector<int> order_by_points(vector<int> nums) {
+    sort(nums.begin(), nums.end());
+    return nums;
 }
 
 int main() {
-    vector<int> numbers = {123, 321, 456, 789};
+    vector<int> nums = {123, 456, 789, 321, 654};
+    vector<int> ordered_nums = order_by_points(nums);
 
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
-    
+    for (int num : ordered_nums) {
+        cout << num << " ";
+    }
     return 0;
 }
