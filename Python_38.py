5f1e4af6a023e9e763663eea353b384b8c31f79a
beep boop <githubbot@vadim.me>
2024-08-02 17:17:54 +0200

    Solution 91 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/e1rqlnxg Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -2,7 +2,7 @@ def encode_cyclic(s: str):
     result = ""
     for char in s:
         if char == "1":
-            result = char + "0" + result
-        else:
             result = char + result
+        else:
+            result = char + result[0] + "0" + result[1:]
     return result
