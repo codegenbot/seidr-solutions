184f74e56bc2c81e60b2a12c33151c400c93d1a0
beep boop <githubbot@vadim.me>
2024-08-02 13:30:39 +0200

    Solution 20 of CPP/74, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w20w82rv Prompt:       |                              ^~~~~~ d0a29096-31d8-4da6-a98c-509292e7490c.cpp:25:37: error: ‘string’ was not declared  in this scope    25 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d0a29096-31d8-4da6-a98c-509292e7490c.cpp:25:44: error: ‘b’ was not declared in t his scope    25 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d0a29096-31d8-4da6-a98c-509292e7490c.cpp:25:45: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d0a29096-31d8-4da6-a98c-509292e7490c.cpp:24: d0a29096-31d8-4da6-a98c-509292e7490c.cpp: In function ‘int main()’: d0a29096-31d8-4da6-a98c-509292e7490c.cpp:34:20: error: ‘total_match’ was not dec lared in this scope    34 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ d0a29096-31d8-4da6-a98c-509292e7490c.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,22 +1,28 @@
-vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    vector<string> result;
-    int count1 = 0, count2 = 0;
+#include <vector>
+#include <string>
 
+using namespace std;
+
+bool operator!=(const vector<string>& a, const vector<string>& b) {
+    return (a.size() != b.size()) || (!equal(a.begin(), a.end(), b.begin()));
+}
+
+vector<string> total_match(vector<string> lst1, vector<string> lst2) {
+    if (&lst1 == &lst2)
+        return {};
+    int count1 = 0;
     for (const string& s : lst1) {
         count1 += s.size();
     }
+    int count2 = 0;
     for (const string& s : lst2) {
         count2 += s.size();
     }
 
-    if (count1 < count2) {
+    if (count1 < count2)
         return lst1;
-    } else if (count1 > count2) {
+    else if (count1 > count2)
         return lst2;
-    } else {
-        if (&lst1 != &lst2) { // check if they are the same vector
-            return lst1; // if not, return the first one
-        }
-        return result; // if they are the same, just return an empty vector
-    }
+    else
+        return lst1;
 }
