aa50fa9d2cf2c9d904ecd8a4fca56e015832d336
beep boop <githubbot@vadim.me>
2024-08-02 21:06:29 +0200

    Solution 39 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cxdicqxo Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 4a9eb826-7509-470c-b73b-0dfeb12b9b17.cpp:16:36: error: expected primary-expressi on before ‘float’    16 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 4a9eb826-7509-470c-b73b-0dfeb12b9b17.cpp:16:43: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 4a9eb826-7509-470c-b73b-0dfeb12b9b17.cpp:15: 4a9eb826-7509-470c-b73b-0dfeb12b9b17.cpp: In function ‘int main()’: 4a9eb826-7509-470c-b73b-0dfeb12b9b17.cpp:25:20: error: ‘find_closest_elements’ w as not declared in this scope    25 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 4a9eb826-7509-470c-b73b-0dfeb12b9b17.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -3,20 +3,18 @@
 #include <cmath>
 
 bool issame(float a, float b) {
-    if (abs(a - b) < 1e-9)
-        return true;
-    else
-        return false;
+    return std::abs(a - b) <= 1e-9;
 }
 
 std::vector<std::pair<float, float>> find_closest_elements(std::vector<float> numbers) {
     std::pair<float, float> closest_pair = make_pair(numbers[0], numbers[1]);
     for (int i = 0; i < numbers.size() - 1; ++i) {
         for (int j = i + 1; j < numbers.size(); ++j) {
-            if (!issame(closest_pair.first, closest_pair.second)) {
-                if (abs(numbers[j] - numbers[i]) < abs(closest_pair.second - closest_pair.first)) {
-                    closest_pair = make_pair(min(numbers[i], numbers[j]), max(numbers[i], numbers[j]));
-                }
+            if (issame(numbers[j], numbers[i])) {
+                closest_pair = make_pair(min(numbers[i], numbers[j]), max(numbers[i], numbers[j]));
+                break;
+            } else if (std::abs(numbers[j] - numbers[i]) < std::abs(closest_pair.second - closest_pair.first)) {
+                closest_pair = make_pair(min(numbers[i], numbers[j]), max(numbers[i], numbers[j]));
             }
         }
     }
