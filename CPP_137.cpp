d5a78dd85a1cf05487da2579a40a6d565f06de5e
beep boop <githubbot@vadim.me>
2024-08-02 02:34:53 +0200

    Solution 23 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/t45lhg5p Prompt: e");       |             ^~~~~ 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:19:29: error: ‘string’ was not declared  in this scope    19 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:19:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:17: 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:19:37: error: ‘compare_one’ was not dec lared in this scope    19 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,14 +3,16 @@
 #include <string>
 #include <typeinfo>
 #include <cstdlib>
-#include <string_view>
 
-template <typename T>
-boost::any compare_one(const boost::any& a, const boost::any& b) {
+std::string_view compare_one(boost::any a, boost::any b) {
     if (a.type() == b.type()) {
-        if (a.type() == typeid(T) && boost::any_cast<T>(a) != boost::any_cast<T>(b)) {
-            return (boost::any_cast<T>(a) > boost::any_cast<T>(b)) ? a : b;
+        if (a.type() == typeid(int) && boost::any_cast<int>(a) != boost::any_cast<int>(b)) {
+            return (boost::any_cast<int>(a) > boost::any_cast<int>(b)) ? boost::any_cast<int>(a) : boost::any_cast<int>(b);
+        } else if (a.type() == typeid(float) && boost::any_cast<float>(a) != boost::any_cast<float>(b)) {
+            return (boost::any_cast<float>(a) > boost::any_cast<float>(b)) ? boost::any_cast<float>(a) : boost::any_cast<float>(b);
+        } else if (a.type() == typeid(std::string_view) && boost::any_cast<std::string_view>(a) != boost::any_cast<std::string_view>(b)) {
+            return (std::stof(boost::any_cast<std::string_view>(a).data()) > std::stof(boost::any_cast<std::string_view>(b).data())) ? boost::any_cast<std::string_view>(a) : boost::any::cast<std::string_view>(b);
         }
     }
-    return boost::any();
+    return "None";
 }
