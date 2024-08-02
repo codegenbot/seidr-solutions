54790045b57688584c3881941a3b84dd56133514
beep boop <githubbot@vadim.me>
2024-08-02 19:55:43 +0200

    Solution 98 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j9951pdw Prompt: ebb659ca-1001-4ab0-a46f-58631b019ec6.cpp:3:1: error: stray ‘`’ in program     3 | ```cpp       | ^ ebb659ca-1001-4ab0-a46f-58631b019ec6.cpp:3:2: error: stray ‘`’ in program     3 | ```cpp       |  ^ ebb659ca-1001-4ab0-a46f-58631b019ec6.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ ebb659ca-1001-4ab0-a46f-58631b019ec6.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <boost/any.hpp>
 #include <string>
 #include <algorithm>
@@ -7,45 +6,45 @@ using namespace std;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (int)a > (double)b ? a : b;
+        return (int)boost::any_cast<int>(a) > (double)b ? a : b;
     } else if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return (double)a > (int)b ? a : b;
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = any_cast<string>(a);
-        string str2 = any_cast<string>(b);
+        string str1 = boost::any_cast<string>(a);
+        string str2 = boost::any_cast<string>(b);
         size_t pos = str1.find(',');
         if (pos != string::npos) {
             double num1 = stod(str1.substr(0, pos));
             double num2 = stod(str2);
             return num1 > num2 ? a : b;
         } else {
-            return (any_cast<string>(a) > any_cast<string>(b)) ? a : b;
+            return boost::any_cast<string>(a) > boost::any_cast<string>(b) ? a : b;
         }
     } else if (a.type() == typeid(string) && (b.type() == typeid(int) || b.type() == typeid(double))) {
-        string str = any_cast<string>(a);
+        string str = boost::any_cast<string>(a);
         size_t pos = str.find(',');
         if (pos != string::npos) {
             double num1 = stod(str.substr(0, pos));
-            return (any_cast<int>(b) > num1 || any_cast<double>(b) > num1) ? b : a;
+            return boost::any_cast<int>(b) > num1 || boost::any_cast<double>(b) > num1 ? b : a;
         } else {
-            return (any_cast<string>(a) == any_cast<string>(b)) ? boost::any() : a;
+            return boost::any_cast<string>(a) == boost::any_cast<string>(b) ? boost::any("None") : a;
         }
     } else if ((a.type() == typeid(int) || a.type() == typeid(double)) && b.type() == typeid(string)) {
-        string str = any_cast<string>(b);
+        string str = boost::any_cast<string>(b);
         size_t pos = str.find(',');
         if (pos != string::npos) {
             double num1 = stod(str.substr(0, pos));
-            return (any_cast<int>(a) > num1 || any_cast<double>(a) > num1) ? a : b;
+            return boost::any_cast<int>(a) > num1 || boost::any_cast<double>(a) > num1 ? a : b;
         } else {
-            return (any_cast<string>(b) == any_cast<string>(a)) ? boost::any() : b;
+            return boost::any_cast<string>(b) == boost::any_cast<string>(a) ? boost::any("None") : b;
         }
     }
 
     if (a.type() == typeid(int) && a.convert_to<int>() == a.convert_to<double>()) {
         if (b.type() == typeid(double) && b.convert_to<int>() == b.convert_to<double>()) {
-            return boost::any();
+            return boost::any("None");
         } else {
-            return (any_cast<int>(a) > any_cast<double>(b)) ? a : b;
+            return boost::any_cast<int>(a) > boost::any_cast<double>(b) ? a : b;
         }
     }
 
