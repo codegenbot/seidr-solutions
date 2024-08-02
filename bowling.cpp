1f9ae8452c9ed1469e7029849b97012e63332266
beep boop <githubbot@vadim.me>
2024-08-03 00:41:53 +0200

    Solution 55 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/cyefu9vh Prompt: dfafed38-cd4b-49ae-94cc-5b762ed4cc76.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,35 +1,36 @@
-```cpp
 #include <sstream>
 
-int bowlingScore(std::string s) {
+int bowlingScore(string s) {
     int score = 0;
     int rolls = 0;
-
-    std::istringstream iss(s);
-    std::string frame;
-
-    while (std::getline(iss, frame, ' ')) {
-        if (frame == "X") {
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'X') {
             score += 30;
             rolls++;
-        } else if (frame.length() > 1 && frame[1] == '/') {
-            int spare = 10 - (frame[0] - '0');
-            score += spare + 10;
-            rolls += 2;
-        } else if (isdigit(frame[0])) {
-            int ball1, ball2;
-            if (frame.length() > 1 && frame[1] == '/') {
-                ball1 = 10 - (frame[0] - '0');
-                ball2 = 10 - (frame[0] - '0') / 2;
-            } else {
-                ball1 = frame[0] - '0';
-                if (frame.length() > 1) ball2 = frame[1] - '0';
-                else ball2 = 0;
+        } else if (isdigit(s[i])) {
+            int frameScore = 10 - (s[i] - '0');
+            
+            if (i + 1 < s.length() && s[i + 1] == '/') {
+                frameScore = 10 - (s[i] - '0') / 2;
+                i++;
             }
-            score += ball1 + ball2;
-            rolls += 2;
+            
+            score += frameScore;
+            rolls++;
+        } else if (s[i] == '/') {
+            int j = i + 1;
+            while (j < s.length() && !isdigit(s[j])) {
+                j++;
+            }
+            frameScore = 10 - (s[j] - '0');
+            score += frameScore;
+            rolls++;
+            i = j;
         }
+        
+        if (rolls >= 10) break;
     }
-
+    
     return score;
 }
