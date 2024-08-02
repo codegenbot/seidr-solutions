24ea7609bda50adffee55483a33959bdf7a7db5e
beep boop <githubbot@vadim.me>
2024-08-02 05:11:50 +0200

    Solution 89 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/thkjha2e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/59b323f9-e48f-4197-b256-542a81f96 5fc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,2 +1,3 @@
+```
 def double_the_difference(lst):
-    return sum(map(lambda x: x**2, filter(lambda x: isinstance(x, int) and x > 0, lst)))
+    return sum(map(lambda x: x ** 2, filter(lambda x: isinstance(x, int) and x > 0, lst)))
