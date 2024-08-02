8b22eca64f406d27112abf8816e4ab6f78e2d9fe
beep boop <githubbot@vadim.me>
2024-08-02 02:14:21 +0200

    Solution 6 of CPP/102, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/3ben5j46 Prompt: 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for (int i = y; i >= x; i--) {       |     ^~~ 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = y; i >= x; i--) {       |                     ^ 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp:2:29: error: ‘i’ does not name a type     2 |     for (int i = y; i >= x; i--) {       |                             ^ 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp:8:5: error: expected unqualified-id bef ore ‘return’     8 |     return max_even;       |     ^~~~~~ In file included from 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp:10: 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp: In function ‘int main()’: 8384a1b6-ce71-4648-9510-e8a21135cd22.cpp:12:13: error: ‘choose_num’ was not decl ared in this scope    12 |     assert (choose_num(546, 546) == 546);       |             ^~~~~~~~~~

--- CPP_102.cpp
+++ CPP_102.cpp
@@ -1,8 +1,10 @@
-int max_even = -1;
-    for(int i = y; i >= x; i--){
-        if(i % 2 == 0){
+int choose_num(int x, int y) {
+    int max_even = -1;
+    for (int i = y; i >= x; i--) {
+        if (i % 2 == 0) {
             max_even = i;
             break;
         }
     }
     return max_even;
+}
