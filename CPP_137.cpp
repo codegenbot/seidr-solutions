2496b58c6591be0c7e60d1f3354b0798d207c1fa
beep boop <githubbot@vadim.me>
2024-08-02 05:02:20 +0200

    Solution 20 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/jnhuyx9f Prompt: ring)) {       |   ^~~~ 58671afa-36a2-4943-bb38-b01457de78dc.cpp:23:1: error: expected unqualified-id be fore ‘return’    23 | return std::any();       | ^~~~~~ In file included from 58671afa-36a2-4943-bb38-b01457de78dc.cpp:25: 58671afa-36a2-4943-bb38-b01457de78dc.cpp: In function ‘int main()’: 58671afa-36a2-4943-bb38-b01457de78dc.cpp:27:13: error: ‘boost’ has not been decl ared    27 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 58671afa-36a2-4943-bb38-b01457de78dc.cpp:27:29: error: ‘string’ was not declared  in this scope    27 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 58671afa-36a2-4943-bb38-b01457de78dc.cpp:27:37: error: ‘compare_one’ was not dec lared in this scope    27 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <cassert>
 
-std::any compare_one(std::any a, std::any b) {
+void compare_one(std::any a, std::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(int)) {
         if (std::any_cast<int>(a) > std::any_cast<int>(b)) {
             return a;
@@ -16,18 +16,12 @@ std::any compare_one(std::any a, std::any b) {
             return b;
         }
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        float val1 = std::stof(std::any_cast<std::string>(a));
-        float val2 = std::stof(std::any_cast<std::string>(b));
+        float val1 = stof(std::any_cast<std::string>(a));
+        float val2 = stof(std::any_cast<std::string>(b));
         if (val1 > val2) {
             return a;
         } else if (val1 < val2) {
             return b;
         }
     }
-    return std::any{};
-}
-
-int main() {
-    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("2"))) == "2");
-    return 0;
 }
