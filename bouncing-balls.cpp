8419fa8bc5b9e7970f24b37ebb5fa6b067536d10
beep boop <githubbot@vadim.me>
2024-08-02 13:44:55 +0200

    Solution 16 of bouncing-balls, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/3attdggf Prompt: 7f1c7d52-49d3-4aec-a540-b36c63b58e6b.cpp: In function ‘int main()’: 7f1c7d52-49d3-4aec-a540-b36c63b58e6b.cpp:14:27: error: ‘pow’ was not declared in  this scope    14 |         total_distance += pow(bounciness_index, i);       |                           ^~~ 7f1c7d52-49d3-4aec-a540-b36c63b58e6b.cpp:17:22: error: ‘setprecision’ was not de clared in this scope    17 |     cout << fixed << setprecision(5) << total_distance << endl;       |                      ^~~~~~~~~~~~

--- bouncing-balls.cpp
+++ bouncing-balls.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-    double height, first_bounce_height;
+    double start_height, first_bounce_height;
     int num_bounces;
 
-    cin >> height >> first_bounce_height >> num_bounces;
+    cin >> start_height >> first_bounce_height >> num_bounces;
 
-    double bounciness_index = first_bounce_height / height;
+    double bounciness_index = first_bounce_height / start_height;
 
     double total_distance = 0.0;
-
-    for(int i = 1; i <= num_bounces; i++) {
-        total_distance += (height - ((double)(2*i-1) * height * (1 - pow(bounciness_index, i)) / ((i + 1) * pow(2, i))));
+    for (int i = 1; i <= num_bounces; ++i) {
+        total_distance += pow(bounciness_index, i);
     }
 
-    cout << fixed << setprecision(4) << total_distance;
+    cout << fixed << setprecision(5) << total_distance << endl;
 
     return 0;
 }
