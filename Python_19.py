0fa6c655df320ea6142eebbc793739cb4792c0bb
beep boop <githubbot@vadim.me>
2024-08-02 12:23:42 +0200

    Solution 1 of Python/19, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qdutem8g Prompt: Complete the following code given the task description and function signature.

--- Python_19.py
+++ Python_19.py
@@ -11,5 +11,5 @@ def sort_numbers(numbers: str) -> str:
         "eight": 8,
         "nine": 9,
     }
-    sorted_nums = sorted([num_dict[i] for i in numbers.split()])
-    return " ".join([str(num) for num in num_dict.keys() if num_dict[i] == num])
+    sorted_list = sorted([num_dict[i] for i in numbers.split()])
+    return " ".join([k for k, v in num_dict.items() if v in sorted_list])
