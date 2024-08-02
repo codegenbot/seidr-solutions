5cd49c71c3f28cbf57bc2abde8e3095e96bad13e
beep boop <githubbot@vadim.me>
2024-08-02 16:53:33 +0200

    Solution 47 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ep0dkhzz Prompt: this scope    41 |     cin >> n;       |     ^~~ a28635da-5a52-47c3-8d16-c8298a3f367f.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; a28635da-5a52-47c3-8d16-c8298a3f367f.cpp:48:5: error: ‘cout’ was not declared in  this scope    48 |     cout << "First subvector: ";       |     ^~~~ a28635da-5a52-47c3-8d16-c8298a3f367f.cpp:48:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? a28635da-5a52-47c3-8d16-c8298a3f367f.cpp:52:13: error: ‘endl’ was not declared i n this scope    52 |     cout << endl;       |             ^~~~ a28635da-5a52-47c3-8d16-c8298a3f367f.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,5 +1,7 @@
+```cpp
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 vector<vector<int>> cutVector(vector<int> v) {
@@ -30,8 +32,8 @@ vector<vector<int>> cutVector(vector<int> v) {
     }
     
     vector<vector<int>> result(2);
-    result[0] = vector<int>(v.begin(), v.begin() + cut_index);
-    result[1] = vector<int>(v.begin() + cut_index, v.end());
+    result[0] = vector<int>(v.begin() + cut_index, v.end());
+    result[1] = vector<int>(v.begin(), v.begin() + cut_index);
     
     return result;
 }
