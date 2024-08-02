cf951d3b1abf41523c9262ef0583f7b091d7fe7c
beep boop <githubbot@vadim.me>
2024-08-02 21:21:09 +0200

    Solution 90 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/b6320cg8 Prompt: ; _Alloc = std::allocator<char>; std::__cxx11::basic_string<_CharT, _Traits, _Al loc>::iterator = std::__cxx11::basic_string<char>::iterator; std::__cxx11::basic _string<_CharT, _Traits, _Alloc>::__const_iterator = std::__cxx11::basic_string< char>::const_iterator]’  1767 |       insert(__const_iterator __p, _CharT __c)       |       ^~~~~~ /usr/include/c++/11/bits/basic_string.h:1767:31: note:   no known conversion for  argument 1 from ‘int’ to ‘std::__cxx11::basic_string<char>::__const_iterator’ { aka ‘std::__cxx11::basic_string<char>::const_iterator’}  1767 |       insert(__const_iterator __p, _CharT __c)       |              ~~~~~~~~~~~~~~~~~^~~ /usr/include/c++/11/bits/basic_string.h: In instantiation of ‘void std::__cxx11: :basic_string<_CharT, _Traits, _Alloc>::_M_dispose() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’: /usr/include/c++/11/bits/basic_string.h:672:9:   required from ‘std::__cxx11::ba sic_string<_CharT, _Traits, _Alloc>::~basic_string() [with _CharT = char; _Trait s = std::char_traits<char>; _Alloc = std::allocator<char>]’ /usr/include/c++/11/bits/basic_string.h:6665:36:   required from here /usr/include/c++/11/bits/basic_string.h:240:22: error: using invalid field ‘std: :__cxx11::basic_string<_CharT, _Traits, _Alloc>::<unnamed union>::_M_allocated_c apacity’   240 |           _M_destroy(_M_allocated_capacity);       |                      ^~~~~~~~~~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,33 +1,20 @@
-#include <string>
-#include <sstream>
+```cpp
 #include <iomanip>
-#include <cstring>
-#include <openssl/md5.h>
-
-using namespace std;
+#include <sstream>
 
 string string_to_md5(string text) {
     if (text.empty()) return "None";
     
-    MD5_CTX md5ctx;
-    unsigned char result[16];
-    unsigned char buffer[1024];
-
-    MD5_Init(&md5ctx);
-
-    for(size_t i = 0; i < text.size(); i += 1024) {
-        size_t len = min(text.size() - i, 1024);
-        memcpy(buffer, &text[i], len);
-        buffer[len] = '\0';
-        MD5_Update(&md5ctx, buffer, len);
-    }
-
-    MD5_Final(result, &md5ctx);
+    unsigned char md5[16];
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, text.c_str(), text.size());
+    MD5_Final(md5, &ctx);
 
     stringstream ss;
-    ss << setfill('0') << setw(32) << hex;
-    for(size_t i = 0; i < 16; ++i)
-        ss << std::setw(2) << (int)result[i];
+    for (int i = 0; i < 16; ++i) {
+        ss << setfill('0') << setw(2) << hex << std::setw(2) << static_cast<unsigned int>(md5[i]);
+    }
 
     return ss.str();
 }
