00a2685b9133e01da4ad3f5a2fec4d4f479bb416
beep boop <githubbot@vadim.me>
2024-08-02 14:50:47 +0200

    Solution 60 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f7cizk1g Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:9: 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp: In function ‘int main()’: 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:19:20: error: ‘unique’ was not declared  in this scope    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,9 +1,12 @@
-#include <algorithm>
-#include <vector>
-using namespace std;
-
+```cpp
 bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i])
+            return false;
+    }
+    return true;
 }
 
 vector<int> unique(vector<int> l){
@@ -12,9 +15,4 @@ vector<int> unique(vector<int> l){
     auto it = unique(result.begin(),result.end());
     result.erase(it,result.end());
     return result;
-}
-
-int main() {
-    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));
-    return 0;
 }
