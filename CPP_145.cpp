16a9605bc35778245999579b6b10ea69f98ea2d7
beep boop <githubbot@vadim.me>
2024-08-02 03:14:44 +0200

    Solution 20 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1t0fd5to Prompt: )’    46 | int main(){       |     ^~~~ 82558f9e-58c9-48d0-8aad-7c813b210d7e.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 82558f9e-58c9-48d0-8aad-7c813b210d7e.cpp:37: 82558f9e-58c9-48d0-8aad-7c813b210d7e.cpp: In function ‘int main()’: 82558f9e-58c9-48d0-8aad-7c813b210d7e.cpp:47:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    47 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~ 82558f9e-58c9-48d0-8aad-7c813b210d7e.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 82558f9e-58c9-48d0-8aad-7c813b210d7e.cpp:38:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    38 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,28 +1,30 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
 vector<int> order_by_points(vector<int> nums) {
-    sort(nums.begin(), nums.end(), [&](int a, int b) {
+    sort(nums.begin(), nums.end(), [](int a, int b) {
         int sum_a = 0, sum_b = 0;
         a = abs(a);
         b = abs(b);
-        while (a > 0) {
-            sum_a += a % 10;
-            a /= 10;
+        int temp_a = a, temp_b = b;
+        while (temp_a > 0) {
+            sum_a += temp_a % 10;
+            temp_a /= 10;
         }
-        while (b > 0) {
-            sum_b += b % 10;
-            b /= 10;
+        while (temp_b > 0) {
+            sum_b += temp_b % 10;
+            temp_b /= 10;
         }
         if (sum_a == sum_b) {
-            return find(nums.begin(), nums.end(), nums[i]) < find(nums.begin(), nums.end(), nums[j]);
+            return find(nums.begin(), nums.end(), a) < find(nums.begin(), nums.end(), b);
         }
         return sum_a < sum_b;
     });
@@ -30,6 +32,6 @@ vector<int> order_by_points(vector<int> nums) {
 }
 
 int main() {
-    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), vector<int>{-76, -21, 0, 4, 23, 6, 6}));
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
