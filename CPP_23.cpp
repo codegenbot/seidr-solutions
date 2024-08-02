73cc48d2d2cf523c74794b9942630f609f1f3120
beep boop <githubbot@vadim.me>
2024-08-02 23:45:15 +0200

    Solution 21 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3px8lw5q Prompt: c080d1c7-b159-4172-9bd9-5c12180c56f4.cpp:10:5: error: redefinition of ‘int main( )’    10 | int main(){       |     ^~~~ c080d1c7-b159-4172-9bd9-5c12180c56f4.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main() {       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -3,11 +3,7 @@
 
 int main() {
     const char* str = "asdasnakj";
-    int length = 0;
-    for(int i = 0; str[i] != '\0'; ++i) {
-        length++;
-    }
-    assert(length == 9);
-
+    assert(std::strlen(str) == 9);
+    
     return 0;
 }
