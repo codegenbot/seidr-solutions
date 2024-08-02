fe5f78698eb353cb69cadd212a5b22484f816c26
beep boop <githubbot@vadim.me>
2024-08-02 23:04:56 +0200

    Solution 18 of Python/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/pbgpbkea Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f10c821b-9afa-4ee1-8e8e-dafac0afc 21d.py", line 15, in <module>     check(tri)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f10c821b-9afa-4ee1-8e8e-dafac0afc 21d.py", line 12, in check     assert tri(1) == [1, 3]            ^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f10c821b-9afa-4ee1-8e8e-dafac0afc 21d.py", line 7, in tri     tribonacci.append(tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i + 1])                                                               ~~~~~~~~~~^^^^^^^ IndexError: list index out of range

--- Python_130.py
+++ Python_130.py
@@ -2,7 +2,7 @@ def tri(n):
     tribonacci = [0, 1, 1]
     for i in range(3, n):
         if i <= 2:
-            tribonacci.append(1)
+            tribonacci.append(tribonacci[i - 1] + tribonacci[i - 2])
         else:
             tribonacci.append(tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3])
     return tribonacci[:n]
