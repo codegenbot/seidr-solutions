ccc680b42bb8751cbb885aaf18c1718b1c3cc774
beep boop <githubbot@vadim.me>
2024-08-02 21:22:27 +0200

    Solution 71 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/knpdw3vs Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f85af3d4-c3f5-4bbf-a34b-f4d88d41a 46d.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f85af3d4-c3f5-4bbf-a34b-f4d88d41a 46d.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -4,9 +4,9 @@ def encode(message):
     for char in message:
         if char.isalpha():
             if char.lower() in vowels:
-                encoded_message += chr(ord(char) + 2)  # Adding 2 for vowels
+                encoded_message += chr(ord(char) + 2)
             else:
-                encoded_message += chr(ord(char) - 2)  # Subtracting 2 for consonants
+                encoded_message += chr(ord(char) - 2)
         else:
             encoded_message += char
     return encoded_message
