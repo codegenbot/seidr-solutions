9ef61e8bdad971d509a647577cc2fc17423c1932
beep boop <githubbot@vadim.me>
2024-08-02 02:42:10 +0200

    Solution 58 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/t45lhg5p Prompt:       |     ^~~~ In file included from ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp:33: ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp: In function ‘int main()’: ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp:35:13: error: ‘boost’ has not been decl ared    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp:35:29: error: ‘string’ was not declared  in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp:35:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ce9d6b3c-ffd6-40c1-bdaf-8aaf0979a492.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,31 +1,31 @@
-#include <boost/any.hpp>
+#include <any>
 #include <string>
-#include <iostream>
+#include <typeinfo>
 #include <cassert>
 
 template <typename T>
-boost::any compare_one(T a, T b) {
+std::any compare_one(T a, T b) {
     if (a == b) {
         return "None";
     } else {
-        return (a > b) ? boost::any(a) : boost::any(b);
+        return (a > b) ? std::any(a) : std::any(b);
     }
 }
 
-boost::any compare(boost::any a, boost::any b) {
+std::any compare(std::any a, std::any b) {
     if (a.type() == b.type()) {
-        if (a.type() == typeid(int) && boost::any_cast<int>(a) != boost::any_cast<int>(b)) {
-            return (boost::any_cast<int>(a) > boost::any_cast<int>(b)) ? a : b;
-        } else if (a.type() == typeid(float) && boost::any_cast<float>(a) != boost::any_cast<float>(b)) {
-            return (boost::any_cast<float>(a) > boost::any_cast<float>(b)) ? a : b;
-        } else if (a.type() == typeid(std::string) && boost::any_cast<std::string>(a) != boost::any_cast<std::string>(b)) {
-            return (std::stof(boost::any_cast<std::string>(a)) > std::stof(boost::any_cast<std::string>(b))) ? a : b;
+        if (a.type() == typeid(int) && std::any_cast<int>(a) != std::any_cast<int>(b)) {
+            return (std::any_cast<int>(a) > std::any_cast<int>(b)) ? a : b;
+        } else if (a.type() == typeid(float) && std::any_cast<float>(a) != std::any_cast<float>(b)) {
+            return (std::any_cast<float>(a) > std::any_cast<float>(b)) ? a : b;
+        } else if (a.type() == typeid(std::string) && std::any_cast<std::string>(a) != std::any_cast<std::string>(b)) {
+            return (std::stof(std::any_cast<std::string>(a)) > std::stof(std::any_cast<std::string>(b))) ? a : b;
         }
     }
     return "None";
 }
 
 int main() {
-    assert(boost::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "None");
+    assert(std::any_cast<std::string>(compare(std::string("1"), std::string("1")) == "None");
     return 0;
 }
