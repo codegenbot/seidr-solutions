8899ffd483dc44efc5df9f58cb5306b85a9e3c4b
beep boop <githubbot@vadim.me>
2024-08-02 06:06:50 +0200

    Solution 91 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/uaiyu42k Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.py
+++ solve-boolean.py
@@ -1,26 +1,9 @@
-Here is the solution to the problem:
-
-def solve(input):
-    if input == 'T':
+def solve_boolean(input_str):
+    if input_str == 'T':
         return True
-    elif input == 'F':
+    elif input_str == 'F':
         return False
-    elif '&' in input and '|' in input:
-        a, op1, b = input.split('&')
-        c, op2, d = input.split('|')
-        if op1 == '&':
-            if op2 == '&':
-                return a.split('&')[0] == 'T' and b.split('&')[0] == 'T' and c.split('&')[0] == 'T' and d.split('&')[0] == 'T'
-            elif op2 == '|':
-                return a.split('&')[0] == 'T' and (b.split('&')[0] == 'T' or c.split('&')[0] == 'T' or d.split('&')[0] == 'T')
-        elif op1 == '|':
-            if op2 == '&':
-                return (a.split('&')[0] == 'T' or b.split('&')[0] == 'T' or c.split('&')[0] == 'T' or d.split('&')[0] == 'T') and a.split('&')[0] == 'T' and b.split('&')[0] == 'T' and c.split('&')[0] == 'T' and d.split('&')[0] == 'T'
-            elif op2 == '|':
-                return (a.split('&')[0] == 'T' or b.split('&')[0] == 'T' or c.split('&')[0] == 'T' or d.split('&')[0] == 'T') and a.split('&')[0] == 'T' or b.split('&')[0] == 'T' or c.split('&')[0] == 'T' or d.split('&')[0] == 'T'
-    elif '&' in input:
-        a, op, b = input.split('&')
-        return eval('({} and {})'.format(a, b))
-    elif '|' in input:
-        a, op, b = input.split('|')
-        return eval('({} or {})'.format(a, b))
+    elif len(input_str) > 1 and input_str[0] in ['f', 't'] and input_str[1] in ['&', '|']:
+        return solve_boolean(input_str[0]) and input_str[1] == '&' or solve_boolean(input_str[0]) or solve_boolean(input_str[1:])
+    else:
+        return all(input_str.split('&')) or any(input_str.split('|'))
