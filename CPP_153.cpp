f56ace043665aeadfb7daa6a61a39e3f67be8e60
beep boop <githubbot@vadim.me>
2024-08-02 03:10:24 +0200

    Solution 95 of CPP/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/o5lnoiol Prompt:       |                         ^~~~~~ 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp:1:44: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                            ^~~~~~ 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp:1:51: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                   ^~~~~~ 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp:1:59: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                           ^~~~~~~~~~ 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp:1:69: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                                     ^ In file included from 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp:25: 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp: In function ‘int main()’: 2b1e1e7d-aa74-41fb-91c5-93d668cc3a03.cpp:27:32: error: ‘Strongest_Extension’ can not be used as a function    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,16 +1,27 @@
-string Strongest_Extension(string class_name, vector<string> extensions) {
-    int max_strength = 0;
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string Strongest_Extension(string class_name, vector<string> extensions){
+    int strongest = -1;
     string strongest_extension;
 
-    for (const auto& extension : extensions) {
-        int cap = count(extension.begin(), extension.end(), toupper);
-        int sm = extension.length() - cap;
-        int strength = cap - sm;
-
-        if (strength > max_strength) {
-            max_strength = strength;
-            strongest_extension = extension;
+    for (string extension : extensions) {
+        int cap = 0;
+        int sm = 0;
+        
+        for (char c : extension) {
+            if (isupper(c)) cap++;
+            else if (islower(c)) sm++;
         }
+        
+        int strength = cap - sm;
+        
+        if (strength > strongest) {
+            strongest = strength;
+            strongest_extension = extension;
+        } 
     }
 
     return class_name + "." + strongest_extension;
