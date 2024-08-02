daa21dee56f3f07c802af03d8072dd7f290e5c53
beep boop <githubbot@vadim.me>
2024-08-02 19:36:49 +0200

    Solution 83 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q14tckcu Prompt:       |             ^~~~~~ 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:28:39: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:27: 0df06103-a98d-4d9f-af83-3b062a855af4.cpp: In function ‘int main()’: 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:37:20: error: ‘pluck’ was not declared in this scope; did you mean ‘std::pluck’?    37 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:8:22: note: ‘std::pluck’ declared here     8 |     std::vector<int> pluck(std::vector<int> arr) {       |                      ^~~~~ In file included from 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:27: 0df06103-a98d-4d9f-af83-3b062a855af4.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -4,18 +4,18 @@ namespace std {
     bool issame(std::vector<int> a, std::vector<int> b) {
         return a == b;
     }
-
+    
     std::vector<int> pluck(std::vector<int> arr) {
         int smallestEven = -1;
         int smallestEvenIndex = -1;
-
+        
         for(int i = 0; i < arr.size(); i++) {
             if(arr[i] % 2 == 0 && (smallestEven == -1 || arr[i] < smallestEven)) {
                 smallestEven = arr[i];
                 smallestEvenIndex = i;
             }
         }
-
+        
         if(smallestEvenIndex == -1) {
             return {};
         } else {
@@ -25,13 +25,7 @@ namespace std {
 }
 
 int main() {
-    std::vector<int> nums {3, 1, 4, 1, 5, 9, 2, 6, 5};
-
-    std::vector<int> result = std::pluck(nums);
-    
-    for(auto num : result) {
-        std::cout << num << " ";
-    }
-
+    std::vector<int> testVec = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<int> result = std::pluck(testVec);
     return 0;
 }
