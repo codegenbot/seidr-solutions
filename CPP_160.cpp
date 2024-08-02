f1017a2a3ce97517cbb6bc51f7bf03d055b516e6
beep boop <githubbot@vadim.me>
2024-08-02 21:07:13 +0200

    Solution 67 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/67h06wxg Prompt:       |                       ^~~~~~ 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp:1:31: error: ‘operator_’ was not declar ed in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands) {       |                               ^~~~~~~~~ 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp:1:42: error: ‘vector’ was not declared in this scope     1 | int do_algebra(vector<string> operator_, vector<int> operands) {       |                                          ^~~~~~ 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp:1:49: error: expected primary-expressio n before ‘int’     1 | int do_algebra(vector<string> operator_, vector<int> operands) {       |                                                 ^~~ 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp:1:62: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int do_algebra(vector<string> operator_, vector<int> operands) {       |                                                              ^ In file included from 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp:21: 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp: In function ‘int main()’: 233db5a3-9362-4f2c-8808-9f4ca68fd474.cpp:23:23: error: ‘do_algebra’ cannot be us ed as a function    23 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cmath>
 
-int do_algebra(std::vector<std::string> operator_, std::vector<int> operands) {
+int do_algebra(vector<string> operator_, vector<int> operands) {
     int result = 0;
     double prev_operand = (double)operands[0];
     for(int i = 1; i < operands.size(); i++) {
@@ -14,9 +14,8 @@ int do_algebra(std::vector<std::string> operator_, std::vector<int> operands) {
         } else if(operator_[i-1] == "//") {
             result = (int)(result / prev_operand);
         } else if(operator_[i-1] == "**") {
-            result = pow((double)result, prev_operand);
+            result = pow(result, prev_operand);
         }
         prev_operand = (double)operands[i];
     }
     return result;
-}
