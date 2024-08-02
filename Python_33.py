0fecbca68205243e7f287afafd20f6201642c0e8
beep boop <githubbot@vadim.me>
2024-08-02 16:07:16 +0200

    Solution 77 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/h6xxtw1i Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1bd49bf-3f94-498b-b3fe-3dfac1219 815.py", line 18, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1bd49bf-3f94-498b-b3fe-3dfac1219 815.py", line 13, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1bd49bf-3f94-498b-b3fe-3dfac1219 815.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c1bd49bf-3f94-498b-b3fe-3dfac1219 815.py", line 6, in <listcomp>     else sorted([x for x in l[: i // 3 * 3 + (i % 3 == 2) * 3 : i + 1]])[          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ IndexError: list index out of range

--- Python_33.py
+++ Python_33.py
@@ -1,18 +1,12 @@
+```
 def sort_third(l: list):
     return [
         (
             val
             if i % 3 != 0
-            else sorted(
-                [
-                    x
-                    for x in l[
-                        (i // 3)
-                        * 3 : (i // 3 + 1) * 3 if i % 3 == 0 else (i // 3 + 1) * 3
-                    ]
-                    if val
-                ]
-            )
+            else sorted([x for x in l[(i // 3) * 3:(i // 3 + 1) * 3 if i % 3 == 0 else (i // 3 + 1) * 3] if x <= val])[
+                i % 3
+            ]
         )
         for i, val in enumerate(l)
     ]
