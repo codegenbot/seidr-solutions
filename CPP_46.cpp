218a999d22d2f2ead3960bc438e228689d190ed0
beep boop <githubbot@vadim.me>
2024-08-02 15:44:26 +0200

    Solution 4 of CPP/46, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/4h86uml2 Prompt:     8 |     for(int i = 3; i <= n; ++i){       |     ^~~ c843d341-61ac-41ee-a57c-044e06113181.cpp:8:20: error: ‘i’ does not name a type     8 |     for(int i = 3; i <= n; ++i){       |                    ^ c843d341-61ac-41ee-a57c-044e06113181.cpp:8:28: error: expected unqualified-id be fore ‘++’ token     8 |     for(int i = 3; i <= n; ++i){       |                            ^~ c843d341-61ac-41ee-a57c-044e06113181.cpp:16:5: error: expected unqualified-id be fore ‘return’    16 |     return sum;       |     ^~~~~~ c843d341-61ac-41ee-a57c-044e06113181.cpp:17:1: error: expected declaration befor e ‘}’ token    17 | }       | ^ In file included from c843d341-61ac-41ee-a57c-044e06113181.cpp:19: c843d341-61ac-41ee-a57c-044e06113181.cpp: In function ‘int main()’: c843d341-61ac-41ee-a57c-044e06113181.cpp:21:13: error: ‘fib4’ was not declared i n this scope    21 |     assert (fib4(12) == 386);       |             ^~~~

--- CPP_46.cpp
+++ CPP_46.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <cassert>
 
 int fib4(int n) {
@@ -6,7 +5,7 @@ int fib4(int n) {
         return 0;
     if(n == 2)
         return 2;
-    
+
     int a = 0, b = 0, c = 2, d = 0, sum = 0;
     
     for(int i = 3; i <= n; ++i){
