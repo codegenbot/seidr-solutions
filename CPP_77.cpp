6003e033b4db1b314391cc105d0731b6a219e589
beep boop <githubbot@vadim.me>
2024-08-02 23:00:17 +0200

    Solution 2 of CPP/77, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/501guhq3 Prompt:       |                           ^ 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp:1:22: error: ‘cbrt’ was not declared in  this scope     1 | int cubeRoot = round(cbrt(a));       |                      ^~~~ 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp:1:16: error: ‘round’ was not declared i n this scope     1 | int cubeRoot = round(cbrt(a));       |                ^~~~~ 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp:2:5: error: expected unqualified-id bef ore ‘return’     2 |     return cubeRoot * cubeRoot * cubeRoot == a;       |     ^~~~~~ 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp:3:1: error: expected declaration before  ‘}’ token     3 | }       | ^ In file included from 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp:5: 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp: In function ‘int main()’: 04cfba32-a4f4-443b-b6a8-ef97bf389128.cpp:7:13: error: ‘iscuber’ was not declared  in this scope     7 |     assert (iscuber(1729) == false);       |             ^~~~~~~

--- CPP_77.cpp
+++ CPP_77.cpp
@@ -1,3 +1,12 @@
-int cube_root = round(cbrt(a));
-    return (cube_root * cube_root * cube_root == a);
+#include <cmath>
+#include <cassert>
+
+bool iscuber(int a) {
+    int cubeRoot = round(cbrt(a));
+    return cubeRoot * cubeRoot * cubeRoot == a;
+}
+
+int main() {
+    assert(iscuber(1729) == false);
+    return 0;
 }
