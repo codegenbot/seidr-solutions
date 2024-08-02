741f095dccce5ccc0b6425c095209dda6cf81cdf
beep boop <githubbot@vadim.me>
2024-08-02 11:47:55 +0200

    Solution 80 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/uqca9z2w Prompt:       |                    ^~~~~ 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp:14:29: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp:13: 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp: In function ‘int main()’: 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 25c4e9c1-da4e-4520-a658-78390eaf20d8.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,14 +1,17 @@
 #include <vector>
-using namespace std;
+#include <algorithm>
 
-// Function declaration before use
-vector<float> get_positive(vector<float> l);
-
-bool issame(vector<float> a, vector<float>b){
-    return a.size() == b.size() && all_of(a.begin(),a.end(),[&](float x){return x == b.front();});
+// Define issame function here
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
 }
 
-int main(){
-    assert (issame(get_positive({}) , {}));
-    return 0;
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for(float i : l) {
+        if(i > 0) {
+            result.push_back(i);
+        }
+    }
+    return result;
 }
