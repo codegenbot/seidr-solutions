9907bd667c8836ea14fc0414bda8d83498e99a5f
beep boop <githubbot@vadim.me>
2024-08-03 00:55:56 +0200

    Solution 48 of CPP/33, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6lgg81t0 Prompt: bc8eb56c-1dac-4786-908d-3267631fb937.cpp:21:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    21 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ bc8eb56c-1dac-4786-908d-3267631fb937.cpp:6:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     6 | bool issame(std::vector<int> a , std::vector<int> b) {       |      ^~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -3,10 +3,6 @@
 #include <cassert>
 using namespace std;
 
-bool issame(std::vector<int> a , std::vector<int> b) {
-    return a == b;
-}
-
 std::vector<int> sort_third(std::vector<int> l);
 
 std::vector<int> sort_third(std::vector<int> l) {
