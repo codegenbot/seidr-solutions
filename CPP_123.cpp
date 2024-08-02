43046fb7a3cb6a3c39c5faf43de2490d0c5b2f72
beep boop <githubbot@vadim.me>
2024-08-02 18:56:02 +0200

    Solution 0 of CPP/123, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nm7f97mj Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_123.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> get_odd_collatz(int n) {
+    vector<int> res;
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        if (n % 2 != 0) {
+            res.push_back(n);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
