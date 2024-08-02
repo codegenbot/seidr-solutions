9eb61d2574ac024b8be164527462656398494d05
beep boop <githubbot@vadim.me>
2024-08-02 21:41:19 +0200

    Solution 20 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/es7qiha1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d289faa6-a494-482b-a37b-328812bb1 e34.py", line 33, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d289faa6-a494-482b-a37b-328812bb1 e34.py", line 30, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -2,12 +2,12 @@ def minPath(grid, k):
     n = len(grid)
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     visited = set()
-    path = []
-    curr_paths = []
+    all_paths = []
 
     def dfs(i, j, curr_path):
         if len(curr_path) == k:
-            curr_paths.append(curr_path[:])
+            nonlocal all_paths
+            all_paths.append(curr_path[:])
             return True
 
         for di, dj in directions:
@@ -22,4 +22,4 @@ def minPath(grid, k):
     for i in range(n):
         for j in range(n):
             if dfs(i, j, [grid[i][j]]):
-                return curr_paths
+                return all_paths
