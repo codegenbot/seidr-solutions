d0fa8274e7820fa2c93342e075594e8e1ed6f322
beep boop <githubbot@vadim.me>
2024-08-02 17:13:04 +0200

    Solution 25 of CPP/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qol6pxwq Prompt: ec0d1903-e3aa-4420-9ea4-42c658766b57.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<int> lst) {       |                 ^~~~~~ ec0d1903-e3aa-4420-9ea4-42c658766b57.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int sum_squares(vector<int> lst) {       |                        ^~~ In file included from ec0d1903-e3aa-4420-9ea4-42c658766b57.cpp:12: ec0d1903-e3aa-4420-9ea4-42c658766b57.cpp: In function ‘int main()’: ec0d1903-e3aa-4420-9ea4-42c658766b57.cpp:14:24: error: ‘sum_squares’ cannot be u sed as a function    14 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,5 +1,5 @@
-#include<vector>
-#include<cmath>
+#include <vector>
+#include <cmath>
 
 int sum_squares(vector<int> lst) {
     int result = 0;
@@ -10,4 +10,3 @@ int sum_squares(vector<int> lst) {
             result += pow(lst[i], 3);
     }
     return result;
-}
