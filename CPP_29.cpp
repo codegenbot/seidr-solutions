6be3b74253af55e17a4923988269a2a9692c831e
beep boop <githubbot@vadim.me>
2024-08-02 21:39:19 +0200

    Solution 94 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lhmips8u Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8cdad510-ad44-414a-8dc3-24195beb76bf.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8cdad510-ad44-414a-8dc3-24195beb76bf.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8cdad510-ad44-414a-8dc3-24195beb76bf.cpp:10: 8cdad510-ad44-414a-8dc3-24195beb76bf.cpp: In function ‘int main()’: 8cdad510-ad44-414a-8dc3-24195beb76bf.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 8cdad510-ad44-414a-8dc3-24195beb76bf.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -14,31 +14,17 @@ int main(){
     int n;
     std::cin >> n;
     vector<string> strings(n);
-    for(int i = 0; i < n; i++) {
-        cin >> strings[i];
-    }
-
+    for(int i = 0; i < n; i++) std::cin >> strings[i];
+    
     string prefix;
-    cin >> prefix;
-
+    std::cin >> prefix;
+    
     vector<string> result = filter_by_prefix(strings, prefix);
-
-    if(issame({prefix}, {strings[0]})) {
-        cout << "The first string is the same as the prefix." << endl;
-    } else {
-        cout << "The first string is not the same as the prefix." << endl;
-
-        vector<string> unique_strings;
-        for(string s : result) {
-            if(find(unique_strings.begin(), unique_strings.end(), s) == unique_strings.end())
-                unique_strings.push_back(s);
-        }
-
-        cout << "Unique strings: ";
-        for(string s : unique_strings)
-            cout << s << " ";
-        cout << endl;
-    }
-
+    
+    if(issame({prefix}, {result[0]}))
+        cout << "Same\n";
+    else
+        cout << "Different\n";
+    
     return 0;
 }
