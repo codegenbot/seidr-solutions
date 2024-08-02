0ea897f2de2acf2a57f3d04da875e4716836f317
beep boop <githubbot@vadim.me>
2024-08-02 18:45:39 +0200

    Solution 91 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/0p8oza4x Prompt:       |              ^~~~~~ c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                     ^~~~~~ c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                            ^~~ c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp:1:40: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                        ^~~ c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp:1:52: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                                    ^ In file included from c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp:23: c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp: In function ‘int main()’: c89cbfc0-10fb-4a65-bf57-1ce3704443c6.cpp:25:21: error: ‘max_fill’ cannot be used  as a function    25 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -3,14 +3,14 @@
 int max_fill(vector<vector<int>> grid, int capacity) {
     int n = grid.size();
     int ans = 0;
-    
+
     for(int i=0; i<n; i++){
         int total_water = 0;
         for(int j=0; j<grid[0].size(); j++){
             if(grid[i][j] == 1)
                 total_water++;
         }
-        
+
         while(total_water > 0){
             int water_taken = min(capacity, total_water);
             ans += 1;
@@ -18,6 +18,6 @@ int max_fill(vector<vector<int>> grid, int capacity) {
             capacity = max(0, capacity - water_taken);
         }
     }
-    
+
     return ans;
 }
