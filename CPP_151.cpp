850e1efe826b4f5118322dc9471675de8bdec954
beep boop <githubbot@vadim.me>
2024-08-02 05:53:23 +0200

    Solution 51 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/y4w5kvcv Prompt: 03df8458-beb2-4575-b42b-c6096b440003.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 03df8458-beb2-4575-b42b-c6096b440003.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from 03df8458-beb2-4575-b42b-c6096b440003.cpp:21: 03df8458-beb2-4575-b42b-c6096b440003.cpp: In function ‘int main()’: 03df8458-beb2-4575-b42b-c6096b440003.cpp:23:35: error: ‘lst’ was not declared in  this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 03df8458-beb2-4575-b42b-c6096b440003.cpp:23:43: error: ‘odd_sum’ was not declare d in this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -4,7 +4,7 @@
 long long double_the_difference(std::vector<float> lst){
     long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && num != (int)num && (int)num % 2 != 0) {
+        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
             sum += (long long)num * num;
         }
     }
