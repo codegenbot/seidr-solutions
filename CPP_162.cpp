7790ed97c02c129002478c7bf277f9eceb64ae4f
beep boop <githubbot@vadim.me>
2024-08-02 21:20:39 +0200

    Solution 88 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/b6320cg8 Prompt: /usr/include/c++/11/bits/stl_algobase.h:278:5: note:   template argument deducti on/substitution failed: 82361afc-0dbc-40f3-8a49-18f298639321.cpp:19:22: note:   deduced conflicting type s for parameter ‘const _Tp’ (‘long unsigned int’ and ‘int’)    19 |         int len = min(text.size() - i, 1024);       |                   ~~~^~~~~~~~~~~~~~~~~~~~~~~ 82361afc-0dbc-40f3-8a49-18f298639321.cpp:20:9: error: ‘memcpy’ was not declared in this scope    20 |         memcpy(buffer, &text[i], len);       |         ^~~~~~ 82361afc-0dbc-40f3-8a49-18f298639321.cpp:5:1: note: ‘memcpy’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     4 | #include <iomanip>   +++ |+#include <cstring>     5 | 82361afc-0dbc-40f3-8a49-18f298639321.cpp:22:9: error: ‘MD5_Update’ was not decla red in this scope    22 |         MD5_Update(&md5ctx, buffer, len);       |         ^~~~~~~~~~ 82361afc-0dbc-40f3-8a49-18f298639321.cpp:25:5: error: ‘MD5_Final’ was not declar ed in this scope    25 |     MD5_Final(result, &md5ctx);       |     ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -9,14 +9,17 @@ using namespace std;
 string string_to_md5(string text) {
     if (text.empty()) return "None";
     
-    MD5_CTX md5ctx;
+    stringstream ss;
     unsigned char result[16];
     unsigned char buffer[1024];
 
+    MD5_CTX md5ctx;
+    size_t i, len;
+
     MD5_Init(&md5ctx);
 
-    for(size_t i = 0; i < text.size(); i += 1024) {
-        size_t len = min(text.size() - i, 1024);
+    for(i = 0; i < text.size(); i += 1024) {
+        len = min(text.size() - i, 1024);
         memcpy(buffer, &text[i], len);
         buffer[len] = '\0';
         MD5_Update(&md5ctx, buffer, len);
@@ -24,9 +27,8 @@ string string_to_md5(string text) {
 
     MD5_Final(result, &md5ctx);
 
-    stringstream ss;
     ss << setfill('0') << setw(32) << hex;
-    for(size_t i = 0; i < 16; ++i)
+    for(i = 0; i < 16; ++i)
         ss << std::setw(2) << (int)result[i];
 
     return ss.str();
