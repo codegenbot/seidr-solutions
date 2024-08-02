69a2eae9beebcf3b659c34cb3542639da00b23a2
beep boop <githubbot@vadim.me>
2024-08-02 21:12:21 +0200

    Solution 90 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/67h06wxg Prompt:       |                             ^~~~~~ 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp:1:37: error: ‘operator_’ was not declar ed in this scope     1 | long long do_algebra(vector<string> operator_, vector<int> operand) {       |                                     ^~~~~~~~~ 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp:1:48: error: ‘vector’ was not declared in this scope     1 | long long do_algebra(vector<string> operator_, vector<int> operand) {       |                                                ^~~~~~ 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp:1:55: error: expected primary-expressio n before ‘int’     1 | long long do_algebra(vector<string> operator_, vector<int> operand) {       |                                                       ^~~ 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp:1:67: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | long long do_algebra(vector<string> operator_, vector<int> operand) {       |                                                                   ^ In file included from 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp:19: 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp: In function ‘int main()’: 77013d79-016a-47e5-a50c-bf3a0b62abc3.cpp:21:23: error: ‘do_algebra’ cannot be us ed as a function    21 |     assert (do_algebra({"//", "*"}, {7, 3, 4}) == 8);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,43 +1,17 @@
-#include <string>
-#include <vector>
-
-using namespace std;
-
-int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = 0;
-    string expression;
-
-    for (int i = 0; i < operator_.size(); i++) {
-        if (i == 0) {
-            for (int j = 0; j < operand.size() - 1; j++)
-                expression += to_string(operand[j]);
-            expression += " ";
-        }
-        expression += operator_[i];
-    }
-
-    for (int i = operand.size() - 1; i >= 1; i--) {
-        if (expression.back() == ' ')
-            expression.pop_back();
-    }
-
-    int temp = eval(expression);
-    return temp;
-}
-
-long long eval(const string& s) {
-    long long res = 0;
-    long long num = 0;
-    char sign = '+';
-    for (char c = s.begin(); c != s.end() && c != '+'; ++c) {
-        if (isdigit(*c)) {
-            num = num * 10 + (*c - '0');
-        } else {
-            if (sign == '+')res += num;
-            else res -= num;
-            sign = *c;
-            num = 0;
+long long do_algebra(vector<string> ops, vector<long long> operands) {
+    long long result = operands[0];
+    for(int i = 0; i < ops.size(); i++) {
+        if(ops[i] == "+") {
+            result += operands[i+1];
+        } else if(ops[i] == "-") {
+            result -= operands[i+1];
+        } else if(ops[i] == "*") {
+            result *= operands[i+1];
+        } else if(ops[i] == "//") {
+            result = result / operands[i+1];
+        } else if(ops[i] == "**") {
+            result = pow(result, (double)operands[i+1]);
         }
     }
-    return res;
+    return result;
 }
