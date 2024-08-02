c12952d4921749c2e172a2f464a9f6011e1ebc90
beep boop <githubbot@vadim.me>
2024-08-02 12:34:29 +0200

    Solution 67 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60kqal8z Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:16: 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp: In function ‘int main()’: 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,14 +1,6 @@
 #include <set>
 #include <algorithm>
 
-bool issame(vector<int> a,vector<int>b) {
-    // Define your logic here
-    if(a.size() == b.size())
-        return true;
-    else
-        return false;
-}
-
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
@@ -19,6 +11,13 @@ vector<int> common(vector<int> l1, vector<int> l2) {
 
     vector<int> result(intersection.begin(), intersection.end());
     return result;
+}
+
+bool issame(vector<int> a,vector<int>b) {
+    if(a.size() == b.size())
+        return true;
+    else
+        return false;
 }
 
 int main() {
