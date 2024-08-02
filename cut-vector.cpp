faf25b2bdeece474ad6c9f98408702ab63298b9f
beep boop <githubbot@vadim.me>
2024-08-02 21:49:01 +0200

    Solution 68 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/zcuavjy9 Prompt: 7ca8232f-54f4-42b2-b2ab-64e6804636a1.cpp:1:1: error: expected unqualified-id bef ore ‘for’     1 | for (int i = 0; i < n; ++i) {       | ^~~ 7ca8232f-54f4-42b2-b2ab-64e6804636a1.cpp:1:17: error: ‘i’ does not name a type     1 | for (int i = 0; i < n; ++i) {       |                 ^ 7ca8232f-54f4-42b2-b2ab-64e6804636a1.cpp:1:24: error: expected unqualified-id be fore ‘++’ token     1 | for (int i = 0; i < n; ++i) {       |                        ^~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 int main() {
     int n, cutIndex;
     std::cin >> n >> cutIndex;
+    
     std::vector<int> nums(n);
-
     for (int i = 0; i < n; ++i) {
         std::cin >> nums[i];
     }
 
     int diff = INT_MAX;
-    int cutPosition = -1;
-    for (int i = 1; i < n; ++i) {
-        int diffTemp = abs(std::accumulate(nums.begin(), nums.begin() + i, 0) - std::accumulate(nums.begin() + i, nums.end(), 0));
-        if (diffTemp < diff) {
-            diff = diffTemp;
-            cutPosition = i;
+    int leftIndex = 0;
+    for (int i = 0; i < cutIndex; ++i) {
+        if (abs(nums[cutIndex] - nums[i]) < diff) {
+            diff = abs(nums[cutIndex] - nums[i]);
+            leftIndex = i;
         }
     }
-
-    for (int i = 0; i < n; ++i) {
-        if (i == cutPosition) {
+    
+    std::vector<int> leftSubvector(nums.begin(), nums.begin() + leftIndex + 1);
+    std::vector<int> rightSubvector(nums.begin() + leftIndex + 1, nums.end());
+    
+    for (int i : leftSubvector) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+    for (int i : rightSubvector) {
+        if (i == nums[cutIndex]) {
             std::cout << std::endl;
-        } else if (i != cutPosition - 1) {
-            std::cout << nums[i] << " ";
-        } else {
-            std::cout << nums[i] << std::endl;
         }
+        std::cout << i << " ";
     }
 
     return 0;
