33aea02b05e8d3233355c322fcda631d6a5e0f42
beep boop <githubbot@vadim.me>
2024-08-02 23:18:02 +0200

    Solution 13 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1yu4bxqu Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:17:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:16: f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp: In function ‘int main()’: f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,24 +4,29 @@
 using namespace std;
 
 bool issame(const vector<int>& a, const vector<int>& b) {
-    if (a.size() != b.size())
+    if (a.size() != b.size()) {
         return false;
+    }
     set<int> s1(a.begin(), a.end());
     set<int> s2(b.begin(), b.end());
     return s1 == s2;
 }
 
 int main() {
-    int n,m;
+    int n, m;
     cin >> n >> m;
     vector<int> l1(n);
-    for(int i = 0; i < n; i++) cin >> l1[i];
+    for (int i = 0; i < n; i++) {
+        cin >> l1[i];
+    }
     vector<int> l2(m);
-    for(int i = 0; i < m; i++) cin >> l2[i];
-    
-    if(issame(l1,l2))
-        cout << "The lists are the same.\n";
-    else
-        cout << "The lists are not the same.\n";
+    for (int i = 0; i < m; i++) {
+        cin >> l2[i];
+    }
+    if (issame(l1, l2)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
     return 0;
 }
