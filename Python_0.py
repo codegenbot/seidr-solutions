e59e7082d2b3bb05876993c034797c500a0d58c6
beep boop <githubbot@vadim.me>
2024-08-02 09:52:40 +0200

    Solution 2 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/tpvvl144 Prompt: Complete the following code given the task description and function signature.

--- Python_0.py
+++ Python_0.py
@@ -1,5 +1,5 @@
 def has_close_elements(numbers: List[float], threshold: float) -> bool:
-    numbers.sort()
+    numbers = sorted(set(numbers))
     for i in range(1, len(numbers)):
         if abs(numbers[i] - numbers[i - 1]) < threshold:
             return True
