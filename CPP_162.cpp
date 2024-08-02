841c686fcda2c927b7982d1e43e6d52d64282202
beep boop <githubbot@vadim.me>
2024-08-02 06:46:31 +0200

    Solution 96 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/e98wubbk Prompt: e4c0d8d3-b6dd-47c3-a532-f702e883426b.cpp: In function ‘std::string string_to_md5 (const string&)’: e4c0d8d3-b6dd-47c3-a532-f702e883426b.cpp:7:8: warning: ‘unsigned char* MD5(const  unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wde precated-declarations]     7 |     MD5((const unsigned char*)str.c_str(), str.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from e4c0d8d3-b6dd-47c3-a532-f702e883426b.cpp:3: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ /usr/bin/ld: /tmp/ccze4fqv.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': e4c0d8d3-b6dd-47c3-a532-f702e883426b.cpp:(.text+0x50): undefined reference to `M D5' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,33 +1,25 @@
 #include <cassert>
 #include <string>
-#include <openssl/md5.h>
 #include <openssl/evp.h>
 #include <openssl/bio.h>
-#include <openssl/buffer.h>
 
 std::string string_to_md5(const std::string& str) {
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md = EVP_md5();
-    unsigned char digest[EVP_MAX_MD_SIZE];
-    unsigned int digest_len;
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit(mdctx, EVP_md5());
 
-    mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
     EVP_DigestUpdate(mdctx, str.c_str(), str.length());
-    EVP_DigestFinal_ex(mdctx, digest, &digest_len);
+
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+
     EVP_MD_CTX_free(mdctx);
 
-    std::string result;
-    BIO *bmem = BIO_new(BIO_s_mem());
-    BIO_write(bmem, digest, digest_len);
-    BUF_MEM *bptr;
-    BIO_get_mem_ptr(bmem, &bptr);
-
-    result.assign(bptr->data, bptr->data + bptr->length);
-
-    BIO_free_all(bmem);
-
-    return result;
+    char mdString[33] = {'\0'};
+    for (int i = 0; i < md_len; i++)
+        sprintf(&mdString[i*2], "%02x", md_value[i]);
+    
+    return mdString;
 }
 
 int test_main() {
