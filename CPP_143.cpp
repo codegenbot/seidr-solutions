e042d603e1dc95f12557d1e16326f24b01ace69c
beep boop <githubbot@vadim.me>
2024-08-02 19:54:54 +0200

    Solution 8 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/p9wxo6qp Prompt: Complete the following code given the task description and function signature.

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -1,46 +1,22 @@
-Here is the completed code:
-
 string words_in_sentence(string sentence){
     string result = "";
-    int n = 0;
-    for(int i=2; i<=10000; i++){
-        if(isPrime(i)){
-            n++;
-        }
-        else break;
-    }
-
-    vector<string> primeWords;
-    string temp = "";
-    for(auto word: split(sentence, ' ')){
-        if(word.length() <= n){
-            temp += word + " ";
-            primeWords.push_back(word);
-        }
-    }
-
-    return result + temp.substr(0, temp.size()-1);
-}
-
-vector<string> split(string s, char ch) {
-    vector<string> ar;
-    string temp = "";
-    for (auto c: s) {
-        if (c == ch) {
-            ar.push_back(temp);
-            temp = "";
-        } else {
-            temp += c;
+    vector<string> words;
+    int len;
+    for(int i=0; i<sentence.length(); i++){
+        if(sentence[i] == ' ' || i==sentence.length()-1){
+            if(i!=sentence.length()-1) len=i+1;
+            else len=i+1;
+            string word = sentence.substr(0, len);
+            int p = 1;
+            for(int j=2; j<=sqrt(len);j++){
+                if(len%j==0)p=len;
+                }
+            if(p!=len)words.push_back(word);
+            sentence.erase(0, len);
         }
     }
-    ar.push_back(temp);
-    return ar;
-}
-
-bool isPrime(int num) {
-    if(num <= 1) return false;
-    for(int i=2; i*i<=num; i++){
-        if(num%i == 0) return false;
+    for(string w: words){
+        result += w + " ";
     }
-    return true;
+    return result.substr(0,result.length()-1);
 }
