4b07b06bfcd5b6621eb625c39c6ceae7c92a5b61
beep boop <githubbot@vadim.me>
2024-08-02 07:10:58 +0200

    Solution 32 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/lqwof8pi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9fdf95e-05d8-4db5-a0f3-b184e3959 8c6.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9fdf95e-05d8-4db5-a0f3-b184e3959 8c6.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,9 +1,4 @@
 def double_the_difference(lst):
-    return sum(x**2 for x in lst if x > 0 and x % 2 != 0) if lst else 0
+    return sum(x**2 for x in lst if x > 0 and x % 2 != 0) * 2 if lst else 0
 
-# Example input list
-input_list = [3, 5, 2, -1, 4]
-
-# Call the function with the input list
-result = double_the_difference(input_list)
-print(result)
+double_the_difference([1, 2, 3, 4, 5]) == 35
