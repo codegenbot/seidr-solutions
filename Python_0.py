2e623fe6f8aa15c3dcf96d4d6651b7616c658d84
beep boop <githubbot@vadim.me>
2024-08-02 10:59:30 +0200

    Solution 79 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xcimrx63 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f9943fc6-85b0-40f2-9ba4-d65e4f4fe 67e.py", line 11, in <module>     check(has_close_elements)           ^^^^^^^^^^^^^^^^^^ NameError: name 'has_close_elements' is not defined

--- Python_0.py
+++ Python_0.py
@@ -5,7 +5,7 @@ def check_adjacent_numbers(numbers, threshold):
             return True
     return False
 
-numbers = [3, 8, 5, 11, 6]
-threshold = 4
+numbers = [20, 30, 40, 50, 60]
+threshold = 5
 result = check_adjacent_numbers(numbers, threshold)
 print(result)
