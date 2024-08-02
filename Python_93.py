19f2d68c1878f7b15d7ed30383e9e5f700536813
beep boop <githubbot@vadim.me>
2024-08-02 03:59:08 +0200

    Solution 42 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fndn25hh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/935214e2-7f9d-43dd-b2ea-c79f9d94a 770.py", line 19, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/935214e2-7f9d-43dd-b2ea-c79f9d94a 770.py", line 15, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -3,11 +3,7 @@ def encode(message):
 
     for char in message:
         if char.isalpha():
-            shifted_char = (
-                chr((ord(char) - ord("A") + 2) % 26 + ord("A"))
-                if char.isupper()
-                else chr((ord(char) - ord("a") + 2) % 26 + ord("a"))
-            )
+            shifted_char = chr((ord(char) - ord('A') + 2) % 26 + ord('A')) if char.isupper() else chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
             encoded_message += shifted_char
         else:
             encoded_message += char
