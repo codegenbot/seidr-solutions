1a0a9b1acb0d88253fe67b564b8610299aa81fb1
beep boop <githubbot@vadim.me>
2024-08-02 02:35:11 +0200

    Solution 25 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/t45lhg5p Prompt: e");       |             ^~~~~ 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:19:29: error: ‘string’ was not declared  in this scope    19 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:19:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ In file included from 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:17: 8d2175fc-d841-42be-ab9d-517ae023af5a.cpp:19:37: error: ‘compare_one’ was not dec lared in this scope    19 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -3,7 +3,12 @@
 #include <typeinfo>
 #include <boost/any.hpp> // Include boost for boost::any_cast
 
+// Declare missing function
 boost::any compare_one(const std::any& a, const std::any& b) {
+    // Add your comparison logic here
+}
+
+if (a.type() == b.type()) {
     if (a.type() == typeid(int) && std::any_cast<int>(a) != std::any_cast<int>(b)) {
         return (std::any_cast<int>(a) > std::any_cast<int>(b)) ? a : b;
     } else if (a.type() == typeid(float) && std::any_cast<float>(a) != std::any_cast<float>(b)) {
@@ -11,5 +16,5 @@ boost::any compare_one(const std::any& a, const std::any& b) {
     } else if (a.type() == typeid(std::string) && std::any_cast<std::string>(a) != std::any_cast<std::string>(b)) {
         return (std::stof(std::any_cast<std::string>(a)) > std::stof(std::any_cast<std::string>(b))) ? a : b;
     }
-    return "None";
 }
+return "None";
