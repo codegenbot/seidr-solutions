0074c20d9398795134d709eace9f08eda9103436
beep boop <githubbot@vadim.me>
2024-08-02 04:23:03 +0200

    Solution 70 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/k289ptgg Prompt:       |             ^~~~~~ fc355b29-33c6-46a6-958e-f819bc7b03b3.cpp:16:20: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ fc355b29-33c6-46a6-958e-f819bc7b03b3.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc355b29-33c6-46a6-958e-f819bc7b03b3.cpp:16:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from fc355b29-33c6-46a6-958e-f819bc7b03b3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fc355b29-33c6-46a6-958e-f819bc7b03b3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fc355b29-33c6-46a6-958e-f819bc7b03b3.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -1,15 +1,13 @@
 #include <vector>
 #include <cassert>
+#include <utility>
+
+std::pair<int, int> eat(int number, int need, int remaining){
+    int total = number + std::min(need, remaining);
+    int left = std::max(0, remaining - need);
+    return {total, left};
+}
 
 bool issame(std::pair<int, int> a, std::pair<int, int> b){
     return a.first == b.first && a.second == b.second;
-}
-
-std::pair<int, int> eat(int a, int b){
-    return {a+b, a-b};
-}
-
-int main(){
-    assert(issame(eat(4, 5) , {9, -1}));
-    return 0;
 }
