d923b8ed3bfa757779034b2b89f041c216b52845
beep boop <githubbot@vadim.me>
2024-08-02 19:58:21 +0200

    Solution 40 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/fmue4q0t Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/49d1a85c-4523-4f84-9e6f-b97b2e4f0 647.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,19 +1,20 @@
 from typing import List
 import math
 
-i = int(input("Enter a number: "))
-factors = []
-i = 2
-while i * i <= i:
-    if i % i:
-        i += 1
-    else:
-        count = 0
-        while i % (i ** (count + 1)) == 0:
-            count += 1
-        for _ in range(count):
-            factors.append(i)
-        i //= (i ** count)
-if i > 1:
-    factors.extend([i] * 1)
-print(factors)
+
+def factorize(n: int) -> List[int]:
+    factors = []
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            count = 0
+            while n % (i ** (count + 1)) == 0:
+                count += 1
+            for _ in range(count):
+                factors.append(i)
+            n //= (i ** count)
+    if n > 1:
+        factors.append(n)
+    return factors
