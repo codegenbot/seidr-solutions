6266aa675abb4c23b5d8a50670ac799ce2213840
beep boop <githubbot@vadim.me>
2024-08-02 23:22:26 +0200

    Solution 36 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1yu4bxqu Prompt:       |                    ^~~ 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp:12: 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp: In function ‘int main()’: 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 5a370dcc-c575-4b14-8e1a-95367aad980f.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,15 +5,15 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     // Your implementation here
 }
 
-std::vector<int> common(std::vector<int> l1, std::vector<int> l2) {
-    std::set<int> s1(l1.begin(), l1.end());
-    std::set<int> s2(l2.begin(), l2.end());
-    std::set<int> intersection;
+vector<int> common(vector<int> l1, vector<int> l2) {
+    set<int> s1(l1.begin(), l1.end());
+    set<int> s2(l2.begin(), l2.end());
+    set<int> intersection;
     
-    std::set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-        std::inserter(intersection, intersection.begin()));
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(intersection, intersection.begin()));
     
-    return std::vector<int>(intersection.begin(), intersection.end());
+    return vector<int>(intersection.begin(), intersection.end());
 }
 
 int main() {
