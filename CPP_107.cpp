c2016025a1ac41b0f17c0d8420e140da9ad4e27f
beep boop <githubbot@vadim.me>
2024-08-02 03:16:54 +0200

    Solution 90 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c2qwob3v Prompt:       |                  ^~~~~~ 806888ec-9f21-4579-b921-ad9c41e8e077.cpp:1:1: note: ‘std::vector’ is defined in header ‘<vector>’; did you forget to ‘#include <vector>’?   +++ |+#include <vector>     1 | bool issame(std::vector<int> a, std::vector<int> b) { 806888ec-9f21-4579-b921-ad9c41e8e077.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool issame(std::vector<int> a, std::vector<int> b) {       |                         ^~~ 806888ec-9f21-4579-b921-ad9c41e8e077.cpp:1:38: error: ‘vector’ is not a member o f ‘std’     1 | bool issame(std::vector<int> a, std::vector<int> b) {       |                                      ^~~~~~ 806888ec-9f21-4579-b921-ad9c41e8e077.cpp:1:38: note: ‘std::vector’ is defined in  header ‘<vector>’; did you forget to ‘#include <vector>’? 806888ec-9f21-4579-b921-ad9c41e8e077.cpp:1:45: error: expected primary-expressio n before ‘int’     1 | bool issame(std::vector<int> a, std::vector<int> b) {       |                                             ^~~ 806888ec-9f21-4579-b921-ad9c41e8e077.cpp:1:51: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool issame(std::vector<int> a, std::vector<int> b) {       |                                                   ^

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,18 +1,3 @@
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
-std::vector<int> even_odd_palindrome(int n) {
-    if (n % 2 == 0) {
-        return std::vector<int>{0, 1};
-    } else {
-        return std::vector<int>{0};
-    }
-}
-
-assert(issame(even_odd_palindrome(1), std::vector<int>{0, 1}));
