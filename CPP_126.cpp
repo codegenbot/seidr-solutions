fc03bd323c4da3c6ebbd625ca60cbe0da7b7ad55
beep boop <githubbot@vadim.me>
2024-08-02 19:11:03 +0200

    Solution 2 of CPP/126, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pwwlrgmq Prompt: Complete the following code given the task description and function signature.

--- CPP_126.cpp
+++ CPP_126.cpp
@@ -1,8 +1,13 @@
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 bool is_sorted(vector<int> lst){
     for(int i = 0; i < lst.size() - 1; i++){
         if(lst[i] >= lst[i+1]){
-            int count = 0;
-            for(int j = i; j < lst.size(); j++){
+            int count = 1;
+            for(int j = i + 1; j < lst.size(); j++){
                 if(lst[j] == lst[i]){
                     count++;
                 }else{
