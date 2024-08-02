34db1ac6d1d6c8701190ac2616e2a949decb56fe
beep boop <githubbot@vadim.me>
2024-08-03 01:17:41 +0200

    Solution 30 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/01dprs9u Prompt: In file included from 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp:42: 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp: In function ‘int main()’: 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp:44:13: error: ‘boost’ has not been decl ared    44 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp:44:29: error: ‘string’ was not declared  in this scope    44 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp:44:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp:3: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 05ab3594-c4e8-4bd6-a3b4-b09d5eb8aeff.cpp:3: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,12 +1,39 @@
 #include <any>
 #include <cassert>
 #include <string>
-#include <algorithm>
 
-std::any compare_one(const std::any& a, const std::any& b);
-
-int main() {
-    assert (std::any_cast<std::string>(compare_one(std::string("1"), std::string("2"))) == "");
-    
-    return 0;
+std::any compare_one(const std::any& a, const std::any& b) {
+    if(a.type() == typeid(int) && b.type() == typeid(int)){
+        if(std::any_cast<int>(a) > std::any_cast<int>(b))
+            return a;
+        else if(std::any_cast<int>(a) < std::any_cast<int>(b))
+            return b;
+        else
+            return std::any();
+    }
+    else if(a.type() == typeid(float) && b.type() == typeid(float)){
+        if(std::any_cast<float>(a) > std::any_cast<float>(b))
+            return a;
+        else if(std::any_cast<float>(a) < std::any_cast<float>(b))
+            return b;
+        else
+            return std::any();
+    }
+    else if(a.type() == typeid(std::string) && b.type() == typeid(std::string)){
+        std::string str1 = std::any_cast<std::string>(a);
+        str1.replace(str1.find(','), 1, ".");
+        float num1 = std::stof(str1);
+        
+        std::string str2 = std::any_cast<std::string>(b);
+        str2.replace(str2.find(','), 1, ".");
+        float num2 = std::stof(str2);
+        
+        if(num1 > num2)
+            return a;
+        else if(num1 < num2)
+            return b;
+        else
+            return std::any();
+    }
+    return std::any();
 }
