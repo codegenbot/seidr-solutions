9ea37a5263055d0e9388037d1d8c1e0008250cd0
beep boop <githubbot@vadim.me>
2024-08-03 00:54:58 +0200

    Solution 74 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vmelxnfr Prompt: on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 704afd3e-8739-4376-b353-512d2c75927a.cpp:33:39: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 704afd3e-8739-4376-b353-512d2c75927a.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 704afd3e-8739-4376-b353-512d2c75927a.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 704afd3e-8739-4376-b353-512d2c75927a.cpp:32: 704afd3e-8739-4376-b353-512d2c75927a.cpp: In function ‘int main()’: 704afd3e-8739-4376-b353-512d2c75927a.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -6,11 +6,11 @@ bool isSame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> minPath(std::vector<std::vector<int>> grid, int k) {
+std::vector<int> minPath(std::vector<std::pair<int, int>> grid, int k) {
     std::vector<int> path;
     for(int i = 0; i < k; i++){
-        path.push_back(grid[i % grid.size()][0]);
-        path.push_back(grid[i % grid.size()][1]);
+        path.push_back(grid[i % grid.size()].first);
+        path.push_back(grid[i % grid.size()].second);
     }
     return path;
 }
@@ -19,15 +19,9 @@ int main() {
     int n, k;
     std::cin >> n >> k;
 
-    std::vector<std::vector<int>> grid(n, std::vector<int>(2));
+    std::vector<std::pair<int, int>> grid(n);
     for (int i = 0; i < n; i++) {
-        std::cin >> grid[i][0] >> grid[i][1];
-    }
-
-    std::vector<int> expected;
-    for(int i = 0; i < k; i++){
-        expected.push_back(grid[i % n][0]);
-        expected.push_back(grid[i % n][1]);
+        std::cin >> grid[i].first >> grid[i].second;
     }
 
     assert(isSame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
