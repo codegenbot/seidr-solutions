cc8e9195e307bdb1a90a4f5fa53fea84d49e5630
beep boop <githubbot@vadim.me>
2024-08-03 01:55:42 +0200

    Solution 1 of CPP/97, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dl5vfn62 Prompt: Complete the following code given the task description and function signature.

--- CPP_97.cpp
+++ CPP_97.cpp
@@ -1,7 +1,7 @@
 Here is the completed code:
 
-int multiply(int a, int b){
+int multiply(int a, int b) {
     int unitA = a % 10;
     int unitB = b % 10;
-    return (unitA * unitB);
+    return unitA * unitB;
 }
