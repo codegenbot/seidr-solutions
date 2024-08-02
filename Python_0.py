be1cf238f20f4c9bc45be35c226ae8f995268468
beep boop <githubbot@vadim.me>
2024-08-02 16:22:00 +0200

    Solution 70 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/5be0lqzw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e58c7c53-9408-4b22-a280-b97e6cae1 330.py", line 1, in <module>     def has_close_elements(numbers: List[float], threshold: float) -> bool:                                     ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_0.py
+++ Python_0.py
@@ -2,6 +2,7 @@
 from typing import List
 
 def has_close_elements(numbers: List[float], threshold: float) -> bool:
+    numbers = sorted(numbers)
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             if abs(numbers[i] - numbers[j]) <= threshold:
