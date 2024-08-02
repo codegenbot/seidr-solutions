317330c5182bb1ba4f8683c808271a30bd014868
beep boop <githubbot@vadim.me>
2024-08-02 12:22:55 +0200

    Solution 2 of Python/10, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/hx0ph1rm Prompt: Complete the following code given the task description and function signature.

--- Python_10.py
+++ Python_10.py
@@ -1,9 +1,6 @@
 def make_palindrome(string: str) -> str:
-    def is_palindrome(string: str) -> bool:
-        return string == string[::-1]
-
-    for i in range(len(string), -1, -1):
-        if is_palindrome(string[:i]):
-            return string + string[:i][::-1]
-
-    return string
+    i = 0
+    for i in range(len(string)):
+        if string[i:] == string[i:][::-1]:
+            break
+    return string + string[:i][::-1]
