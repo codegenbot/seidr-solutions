ead23a0f1cc9c996b733793bf38f206b0a0bd08e
beep boop <githubbot@vadim.me>
2024-08-02 16:47:12 +0200

    Solution 15 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkrn17wy Prompt:    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6ce1edb7-1415-4d1b-854e-b4970f736313.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6ce1edb7-1415-4d1b-854e-b4970f736313.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6ce1edb7-1415-4d1b-854e-b4970f736313.cpp:17: 6ce1edb7-1415-4d1b-854e-b4970f736313.cpp: In function ‘int main()’: 6ce1edb7-1415-4d1b-854e-b4970f736313.cpp:27:20: error: ‘largest_smallest_integer s’ was not declared in this scope    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 6ce1edb7-1415-4d1b-854e-b4970f736313.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,13 +1,3 @@
-bool issame(int a, int b) {
-    if (a == 0 && b == 0) {
-        return true;
-    }
-    if ((a < 0 && b < 0) || (a > 0 && b > 0)) {
-        return false;
-    }
-    return true;
-}
-
 vector<int> largest_smallest_integers(vector<int> lst) {
     int maxNeg = 0;
     int minPos = INT_MAX;
@@ -21,17 +11,9 @@ vector<int> largest_smallest_integers(vector<int> lst) {
         }
     }
 
-    vector<int> result;
-    if (issame(maxNeg, 0)) {
-        result.push_back(0);
-    } else {
-        result.push_back(maxNeg);
-    }
-    if (issame(minPos, INT_MAX)) {
-        result.push_back(INT_MIN);
-    } else {
-        result.push_back(minPos);
-    }
+    return {(maxNeg == 0 ? 0 : maxNeg), (minPos == INT_MAX ? 0 : minPos)};
+}
 
-    return result;
+bool issame(int a, int b) {
+    return (a >= 0 && b >= 0) || (a < 0 && b < 0);
 }
