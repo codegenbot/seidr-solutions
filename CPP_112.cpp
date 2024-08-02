4750f92508916bc8ebf8e107a6ce6724d47ed427
beep boop <githubbot@vadim.me>
2024-08-02 03:36:16 +0200

    Solution 50 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/aza6mbvx Prompt:       |                                            ^ f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp:23: f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp: In function ‘int main()’: f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp:33:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    33 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ f7057c4d-a4a3-42bf-a7ff-2f5b9c5509d1.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,19 +3,19 @@
 #include <algorithm>
 #include <cassert>
 
-std::pair<bool, std::string> reverse_delete(const std::string& a, const std::string& b) {
+bool issame(const std::string& a, const std::string& b) {
     std::string result = "";
-    for(char ch : a){
-        if(b.find(ch) == std::string::npos){
+    for (char ch : a) {
+        if (b.find(ch) == std::string::npos) {
             result += ch;
         }
     }
     std::string result_reverse = result;
     std::reverse(result_reverse.begin(), result_reverse.end());
-    return {result == result_reverse, result};
+    return result == result_reverse;
 }
 
 int main() {
-    assert(reverse_delete("mamma", "mia") == std::make_pair(true, ""));
+    assert(issame("mamma", "mia") == true);
     return 0;
 }
