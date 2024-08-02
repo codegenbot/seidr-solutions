df7a6bc9ecf9c117c3c6db29e91fdda367fddfe4
beep boop <githubbot@vadim.me>
2024-08-02 11:16:08 +0200

    Solution 82 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/za3m6z0n Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b2fc3856-f608-4233-a053-691ca4ec39e0.cpp:27:36: error: expected primary-expressi on before ‘float’    27 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ b2fc3856-f608-4233-a053-691ca4ec39e0.cpp:27:43: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from b2fc3856-f608-4233-a053-691ca4ec39e0.cpp:26: b2fc3856-f608-4233-a053-691ca4ec39e0.cpp: In function ‘int main()’: b2fc3856-f608-4233-a053-691ca4ec39e0.cpp:36:20: error: ‘find_closest_elements’ w as not declared in this scope    36 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ b2fc3856-f608-4233-a053-691ca4ec39e0.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,8 +1,9 @@
 #include <algorithm>
-#include <numeric>
 #include <vector>
+#include <limits>
 
 std::vector<float> find_closest_elements(std::vector<float> numbers) {
+    std::numeric_limits<float>::max();
     float min_diff = std::numeric_limits<float>::max();
     float closest_pair[2];
     
