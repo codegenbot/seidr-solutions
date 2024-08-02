7edbcbb4fc900063a596f76d628535b5f90ce899
beep boop <githubbot@vadim.me>
2024-08-02 02:18:29 +0200

    Solution 32 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qzhe140f Prompt:       |                           ^~~~~~ 0731c337-25a0-42dc-8403-40dac77ccecc.cpp:30:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0731c337-25a0-42dc-8403-40dac77ccecc.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0731c337-25a0-42dc-8403-40dac77ccecc.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0731c337-25a0-42dc-8403-40dac77ccecc.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0731c337-25a0-42dc-8403-40dac77ccecc.cpp:38:5: error: redefinition of ‘int main( )’    38 | int main(){       |     ^~~~ 0731c337-25a0-42dc-8403-40dac77ccecc.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
@@ -21,7 +21,7 @@ std::vector<int> make_a_pile(int n) {
 }
 
 int main() {
-    assert(issame(make_a_pile(8), std::vector<int>{8, 10, 12, 14, 16, 18, 20, 22}));
+    assert(issame(make_a_pile(8), std::vector<int>{8, 10, 12, 14, 16, 18, 20, 22});
 
     return 0;
 }
