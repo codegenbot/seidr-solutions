3d1f1ed2575fe709fc2f455bc1deb56fe403f342
beep boop <githubbot@vadim.me>
2024-08-02 21:32:19 +0200

    Solution 32 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/lhmips8u Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8677ea62-f93e-4bfa-b0fa-b70e02075083.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8677ea62-f93e-4bfa-b0fa-b70e02075083.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8677ea62-f93e-4bfa-b0fa-b70e02075083.cpp:10: 8677ea62-f93e-4bfa-b0fa-b70e02075083.cpp: In function ‘int main()’: 8677ea62-f93e-4bfa-b0fa-b70e02075083.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 8677ea62-f93e-4bfa-b0fa-b70e02075083.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,9 +1,24 @@
+```
+#include <vector>
+#include <string>
+
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) 
-        return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i])
-            return false;
+    if(a.size() != b.size()) {
+        throw std::runtime_error("Vectors are not the same size");
+    }
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) {
+            throw std::runtime_error("Vectors contain different elements");
+        }
     }
     return true;
+}
+
+vector<string> filter_by_prefix(vector<string> strings, string prefix){
+    vector<string> result;
+    for(auto s : strings) {
+        if(s.find(prefix) == 0)
+            result.push_back(s);
+    }
+    return result;
 }
