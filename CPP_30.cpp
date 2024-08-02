865549d6d137275cb112deda4b01887277a4aa9b
beep boop <githubbot@vadim.me>
2024-08-02 21:45:45 +0200

    Solution 80 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/axwclnng Prompt:       |                    ^~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:14:29: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:13: 2937be51-cb9d-4564-81f1-23499ed624f6.cpp: In function ‘int main()’: 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,16 +1,20 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<float> a, vector<float> b){
-    return (a.size() == b.size()) && std::equal(a.begin(), a.end(), b.begin());
-}
+using namespace std;
 
-vector<std::float> get_positive(vector<float> l){
-    vector<std::float> result;
-    for (float num : l) {
-        if (num > 0) {
-            result.push_back(num);
+bool issame(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (abs(a[i] - b[i]) > 1e-6) {
+            return false;
         }
     }
-    return result;
+    return true;
+}
+
+int main() {
+    assert(issame(get_positive({}), {})); 
 }
