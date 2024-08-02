5663703f63afadcbf35ef73dbc7bec37af62bbb4
beep boop <githubbot@vadim.me>
2024-08-02 10:22:58 +0200

    Solution 11 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/uf7miyve Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,31 +1,36 @@
-#include <vector>
-using namespace std;
-
-int bowlingScore(string input) {
+int bowlingScore(string s) {
     int score = 0;
-    int roll = 0;
-    bool firstRollInFrame = true;
+    vector<int> rolls(21); // stores the number of pins knocked down per roll
     
-    for(int i = 0; i < input.size(); i++) {
-        if(input[i] == 'X') {
-            score += 10 + (firstRollInFrame ? 10 : 10);
-            firstRollInFrame = false;
-            roll = 0;
-        } else if(input[i] == '/') {
-            int currentRoll = stoi(string(1, input[i-1])) + stoi(string(1, input[i+1]));
-            score += (firstRollInFrame ? currentRoll : 10);
-            firstRollInFrame = false;
-            roll = 0;
-        } else if(isdigit(input[i])) {
-            int value = input[i] - '0';
-            roll += value;
-            if(roll == 10) {
-                score += 10;
-                firstRollInFrame = true;
-                roll = 0;
-            }
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '/') {
+            int strikeOrSpare = 10 - stoi(s.substr(i-1, 1)); 
+            score += 10 + strikesAndSpares(rolls, i);
+            rolls[i] = 10;
+        } else if (s[i] == 'X') {
+            score += 10;
+            rolls[i] = 10;
+        } else {
+            int roll = stoi(s.substr(i, 1));
+            score += roll;
+            rolls[i] = roll;
         }
     }
-    
+
     return score;
+}
+
+int strikesAndSpares(vector<int> rolls, int i) {
+    int bonus = 0;
+    
+    for (int j = i+1; j <= i + 1 && j < 11; j++) {
+        if (rolls[j] == 10) {
+            bonus += 20 - rolls[i];
+            break;
+        } else {
+            bonus += rolls[i];
+        }
+    }
+
+    return bonus;
 }
