3166756a5dfa6aa15b9099ed67e2c3a694814d6a
beep boop <githubbot@vadim.me>
2024-08-02 17:48:00 +0200

    Solution 3 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pwpp3khv Prompt: Complete the following code given the task description and function signature.

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -3,30 +3,34 @@
 using namespace std;
 
 vector<int> even_odd_palindrome(int n) {
-    vector<int> result(2);
-    int countEven = 0;
-    int countOdd = 0;
-
-    for (int i = 1; i <= n; ++i) {
-        string str = to_string(i);
-        bool isPalindrome = true;
-
-        for (int j = 0; j < str.length() / 2; ++j) {
-            if (str[j] != str[str.length() - j - 1]) {
-                isPalindrome = false;
-                break;
+    vector<int> result;
+    int even = 0, odd = 0;
+    
+    for (int i = 1; i <= n; i++) {
+        if (isPalindrome(i)) {
+            if (i % 2 == 0) {
+                even++;
+            } else {
+                odd++;
             }
         }
-
-        if (isPalindrome && i % 2 == 0)
-            countEven++;
-        else if (isPalindrome && i % 2 != 0)
-            countOdd++;
-
     }
-
-    result[0] = countEven;
-    result[1] = countOdd;
-
+    
+    result.push_back(even);
+    result.push_back(odd);
+    
     return result;
+}
+
+bool isPalindrome(int n) {
+    int rev = 0;
+    int temp = n;
+    
+    while (temp != 0) {
+        int digit = temp % 10;
+        rev = rev * 10 + digit;
+        temp /= 10;
+    }
+    
+    return n == rev;
 }
