56f8ca71cffa0c3eca284e53c321c640f5f3f483
beep boop <githubbot@vadim.me>
2024-08-02 05:47:53 +0200

    Solution 6 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/nzyqll3g Prompt:       |                    ^~~ a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp:9: a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp: In function ‘int main()’: a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp:19:20: error: ‘compare’ was not declare d in this scope    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ a14d0a00-a29e-4b62-bf8c-098a36494f43.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,6 +1,12 @@
+#include <iostream>
 #include <vector>
 #include <cassert>
+
 using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
@@ -10,11 +16,8 @@ vector<int> compare(vector<int> game, vector<int> guess) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 int main() {
     assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), {2, 0, 0, 1}));
+    cout << "Test Passed" << endl;
     return 0;
 }
