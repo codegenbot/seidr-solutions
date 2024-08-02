3273cde00cd8f57b648efc0e4539441265bfcebf
beep boop <githubbot@vadim.me>
2024-08-02 23:03:33 +0200

    Solution 4 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i96n0jy4 Prompt:       |                              ^~~~~~ 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp:12:37: error: ‘string’ was not declared  in this scope    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp:11: 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp: In function ‘int main()’: 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp:21:20: error: ‘all_prefixes’ was not de clared in this scope    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ 41ec7888-eef9-4890-9b94-bbeb1c081b03.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
 #include <vector>
 #include <string>
 #include <cassert>
 
 using namespace std;
 
-vector<string> all_prefixes(string str){
-    vector<string> result;
-    string prefix = "";
-    for (char c : str) {
-        prefix += c;
-        result.push_back(prefix);
-    }
-    return result;
-}
-
 bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-int main() {
+int main(){
     assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
-    
-    cout << "All test cases passed!" << endl;
-
     return 0;
 }
