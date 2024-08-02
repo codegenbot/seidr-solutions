2674aff532ce2240b45b2e4c4b8cfac8de98de2f
beep boop <githubbot@vadim.me>
2024-08-02 06:13:09 +0200

    Solution 2 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/2rv2t7ns Prompt: aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string max_word = "";       | ^~~~~~ aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp:4:5: error: expected unqualified-id bef ore ‘for’     4 |     for (const string& word : words) {       |     ^~~ aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return max_word;       |     ^~~~~~ aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp:15: aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp: In function ‘int main()’: aa3530c7-b538-4c2b-83b6-709f0fb16f26.cpp:17:14: error: ‘find_max’ was not declar ed in this scope    17 |     assert ((find_max({"play", "play", "play"}) == "play"));       |              ^~~~~~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -1,15 +1,18 @@
-string find_max(vector<string> words){
-    string result = "";
-    int max_unique_chars = 0;
+#include <string>
+#include <unordered_set>
+using namespace std;
 
+string find_max(const vector<string>& words) {
+    string max_word = "";
+    int max_unique_chars = 0;
+    
     for (const string& word : words) {
         int unique_chars = unordered_set<char>(word.begin(), word.end()).size();
-
-        if (unique_chars > max_unique_chars || (unique_chars == max_unique_chars && word < result)) {
+        if (unique_chars > max_unique_chars || (unique_chars == max_unique_chars && word < max_word)) {
             max_unique_chars = unique_chars;
-            result = word;
+            max_word = word;
         }
     }
-
-    return result;
+    
+    return max_word;
 }
