c4a6180f396b585b55deb4b00ac3019339633ce0
beep boop <githubbot@vadim.me>
2024-08-02 23:22:57 +0200

    Solution 86 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sp1pxf3a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9603f440-dc8a-426f-8b0a-dce437d58 2a9.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9603f440-dc8a-426f-8b0a-dce437d58 2a9.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_37.py
+++ Python_37.py
@@ -3,4 +3,6 @@ def sort_even(l: list):
         return []
     even = [x for i, x in enumerate(l) if i % 2 == 0]
     odd = [x for i, x in enumerate(l) if i % 2 != 0]
-    return [even[i // 2] if i < len(even) else odd[i - len(even)] for i in range(len(l))]
+    return [
+        even[i // 2] if i < len(even) else odd[i - len(even)] for i in range(len(l))
+    ]
