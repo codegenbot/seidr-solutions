14ce95684cc5634f98abfaf06b11138aeaea1d5b
beep boop <githubbot@vadim.me>
2024-08-02 04:02:27 +0200

    Solution 3 of Python/138, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8keip0ub Prompt: Complete the following code given the task description and function signature.

--- Python_138.py
+++ Python_138.py
@@ -2,12 +2,6 @@
 def is_equal_to_sum_even(n):
     for i in range(2, n+1, 2):
         remaining = n - i
-        if remaining % 2 != 0:
-            return False
-        for j in range(i+2, n+1, 2):
-            if remaining - j > i:
-                break
-            remaining -= j
-        else:
+        if remaining % 4 == 0:
             return True
     return False
