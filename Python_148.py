a6c3b3cfa3096b36be40723c554ff03298122ff1
beep boop <githubbot@vadim.me>
2024-08-02 04:49:35 +0200

    Solution 2 of Python/148, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/gv7s5lmr Prompt: Complete the following code given the task description and function signature.

--- Python_148.py
+++ Python_148.py
@@ -1,11 +1,17 @@
-```
 def bf(planet1, planet2):
-    planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
-    if planet1 not in planets or planet2 not in planets:
+    planets = [
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Uranus",
+        "Neptune",
+    ]
+    if planet1 and planet2 in planets:
+        index1 = planets.index(planet1)
+        index2 = planets.index(planet2)
+        return tuple(planets[index1 + 1 : index2])
+    else:
         return ()
-    index1 = planets.index(planet1)
-    index2 = planets.index(planet2)
-    start = min(index1, index2) + 1
-    end = max(index1, index2) - 1
-    result = tuple(planets[i] for i in range(start, end+1))
-    return result
