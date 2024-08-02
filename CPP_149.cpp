43ce49532b075a9e9d6a78dab711e4a86d9771eb
beep boop <githubbot@vadim.me>
2024-08-02 05:51:57 +0200

    Solution 38 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0tpmckex Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 2eca0c68-4665-4a4e-9c58-4dbb54893b32.cpp:4: 2eca0c68-4665-4a4e-9c58-4dbb54893b32.cpp:26:1: error: expected unqualified-id be fore ‘static_cast’    26 | assert(areEqual(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "d d", "aaaa", "bbbb"}));       | ^~~~~~ 2eca0c68-4665-4a4e-9c58-4dbb54893b32.cpp:26:1: error: expected ‘)’ before ‘stati c_cast’    26 | assert(areEqual(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "d d", "aaaa", "bbbb"}));       | ^~~~~~ 2eca0c68-4665-4a4e-9c58-4dbb54893b32.cpp:26:61: error: expected unqualified-id b efore ‘)’ token    26 | assert(areEqual(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "d d", "aaaa", "bbbb"}));       |                                                             ^ 2eca0c68-4665-4a4e-9c58-4dbb54893b32.cpp:26:92: error: expected unqualified-id b efore ‘)’ token    26 | list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));       |                                                                     ^

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -9,8 +9,6 @@ bool areEqual(const vector<string>& a, const vector<string>& b) {
     return a == b;
 }
 
-vector<string> sorted_list_sum(vector<string> lst);
-
 vector<string> sorted_list_sum(vector<string> lst) {
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
@@ -22,4 +20,4 @@ vector<string> sorted_list_sum(vector<string> lst) {
     return lst;
 }
 
-static_assert(areEqual(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
+assert(areEqual(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
