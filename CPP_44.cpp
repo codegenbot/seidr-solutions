918490d76420b962ab72d1dbc63fdabffac41dcf
beep boop <githubbot@vadim.me>
2024-08-02 16:01:56 +0200

    Solution 82 of CPP/44, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/uqaxuc1d Prompt: 6a47c1ca-24ff-4279-9f19-50abd0cb4917.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 6a47c1ca-24ff-4279-9f19-50abd0cb4917.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main(){       |     ^~~~ In file included from 6a47c1ca-24ff-4279-9f19-50abd0cb4917.cpp:22: 6a47c1ca-24ff-4279-9f19-50abd0cb4917.cpp: In function ‘int main()’: 6a47c1ca-24ff-4279-9f19-50abd0cb4917.cpp:26:42: error: ‘to_string’ was not decla red in this scope; did you mean ‘std::__cxx11::to_string’?    26 |         assert (change_base(x, x + 1) == to_string(x));       |                                          ^~~~~~~~~ In file included from /usr/include/c++/11/string:55,                  from 6a47c1ca-24ff-4279-9f19-50abd0cb4917.cpp:1: /usr/include/c++/11/bits/basic_string.h:6739:3: note: ‘std::__cxx11::to_string’ declared here  6739 |   to_string(long double __val)       |   ^~~~~~~~~

--- CPP_44.cpp
+++ CPP_44.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include <string>
-#include <cassert> 
+#include <iostream>
+#include <cassert>
 
 std::string change_base(int x, int base){
     std::string result = "";
     if (base == 1) return std::string(x, '1');
-    while(x > 0){
+    while (x > 0){
         result = std::to_string(x % base) + result;
         x /= base;
     }
@@ -15,6 +15,6 @@ std::string change_base(int x, int base){
 int main(){
     int x = 10;
     assert(change_base(x, x + 1) == std::to_string(x));
-    
+
     return 0;
 }
