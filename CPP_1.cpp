4cd067add95429ac7aecff6ab6f028fbb549c7b4
beep boop <githubbot@vadim.me>
2024-08-02 12:49:30 +0200

    Solution 3 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/61uzu1wk Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 2a032564-fa6e-4205-a459-ec299ba91133.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 2a032564-fa6e-4205-a459-ec299ba91133.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 2a032564-fa6e-4205-a459-ec299ba91133.cpp:26: 2a032564-fa6e-4205-a459-ec299ba91133.cpp: In function ‘int main()’: 2a032564-fa6e-4205-a459-ec299ba91133.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 2a032564-fa6e-4205-a459-ec299ba91133.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,28 +1,3 @@
 vector<string> separate_paren_groups(string paren_string);
 
-bool issame (string s1, string s2);
- 
-vector<string> separate_paren_groups(string paren_string) {
-    vector<string> result;
-    string current_group;
-    int open_braces = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            if (open_braces > 0) {
-                current_group += c;
-            }
-            open_braces++;
-        } else if (c == ')') {
-            open_braces--;
-            if (open_braces > 0) {
-                current_group += c;
-            } else if (open_braces == 0) {
-                result.push_back(current_group);
-                current_group = "";
-            }
-        }
-    }
-
-    return result;
-}
+bool issame(string a, string b);
