6f3895b674c3f5f54c1eae7dc8aa4da24bd7abb2
beep boop <githubbot@vadim.me>
2024-08-03 01:04:23 +0200

    Solution 42 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7admhmgi Prompt:       |                             ^~~~~~ 02ca3733-6ef1-41bb-a90c-a514218d3541.cpp:33:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 02ca3733-6ef1-41bb-a90c-a514218d3541.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 02ca3733-6ef1-41bb-a90c-a514218d3541.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 02ca3733-6ef1-41bb-a90c-a514218d3541.cpp:33:36: error: expected primary-expressi on before ‘float’    33 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 02ca3733-6ef1-41bb-a90c-a514218d3541.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 02ca3733-6ef1-41bb-a90c-a514218d3541.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -3,20 +3,18 @@
 #include <algorithm>
 #include <cassert>
 
-using namespace std;
-
-bool issame(vector<float> a, vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
 
-vector<float> sort_even(vector<float> l) {
-    vector<float> even_indices;
+std::vector<float> sort_even(std::vector<float> l) {
+    std::vector<float> even_indices;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_indices.push_back(l[i]);
         }
     }
-    sort(even_indices.begin(), even_indices.end());
+    std::sort(even_indices.begin(), even_indices.end());
     int j = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
@@ -27,7 +25,6 @@ vector<float> sort_even(vector<float> l) {
     return l;
 }
 
-int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-    return 0;
+int main(int argc, char** argv) {
+    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), std::vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 }
