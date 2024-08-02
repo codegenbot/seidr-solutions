84189201b4611b15bb887b3e963b19929c8fb140
beep boop <githubbot@vadim.me>
2024-08-03 00:52:08 +0200

    Solution 5 of CPP/84, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q0rggi6z Prompt: Complete the following code given the task description and function signature.

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,9 +1,8 @@
 string solve(int N) {
     int sum = 0;
-    while(N > 0){
-        int remainder = N % 2;
-        sum += remainder;
-        N /= 2;
+    while (N > 0) {
+        sum += N & 1;
+        N >>= 1;
     }
     return to_string(sum);
 }
