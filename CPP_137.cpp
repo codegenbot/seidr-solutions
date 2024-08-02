a760a7b0ce2eb6d39360196f4b1f17aa3c3cb052
beep boop <githubbot@vadim.me>
2024-08-02 05:04:07 +0200

    Solution 26 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/jnhuyx9f Prompt:       |     ^~~~ In file included from 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp:35: 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp: In function ‘int main()’: 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp:37:13: error: ‘boost’ has not been decl ared    37 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp:37:29: error: ‘string’ was not declared  in this scope    37 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp:37:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 89539f43-0079-454c-86e5-5b910d1c7ce6.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,15 +1,33 @@
-#include <boost/any.hpp>
+#include <any>
 #include <string>
-#include <iostream>
 #include <cassert>
 
-boost::any compare_one(const std::string& s, int i) {
-    // Implement your compare_one function here
-    return boost::any();
+std::any compare_one(std::any a, std::any b) {
+    if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        if (std::any_cast<int>(a) > std::any_cast<int>(b)) {
+            return a;
+        } else if (std::any_cast<int>(a) < std::any_cast<int>(b)) {
+            return b;
+        }
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        if (std::any_cast<float>(a) > std::any_cast<float>(b)) {
+            return a;
+        } else if (std::any_cast<float>(a) < std::any_cast<float>(b)) {
+            return b;
+        }
+    } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        float val1 = std::stof(std::any_cast<std::string>(a));
+        float val2 = std::stof(std::any_cast<std::string>(b));
+        if (val1 > val2) {
+            return a;
+        } else if (val1 < val2) {
+            return b;
+        }
+    }
+    return std::any{};
 }
 
 int main() {
-    assert(boost::any_cast<std::string>(compare_one(std::string("1"), 1)) == "None");
-
+    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("2"))) == "2");
     return 0;
 }
