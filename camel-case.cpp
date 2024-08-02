efb267c605714540436c41c0fe5a76536b8c1079
beep boop <githubbot@vadim.me>
2024-08-02 05:24:09 +0200

    Solution 39 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/z00l6ytx Prompt:  this scope    22 |     while (cin >> s) {       |            ^~~ 8230360c-7c74-4c2f-9040-7223565b2051.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; 8230360c-7c74-4c2f-9040-7223565b2051.cpp:23:9: error: ‘cout’ was not declared in  this scope    23 |         cout << camelCase(s) << endl;       |         ^~~~ 8230360c-7c74-4c2f-9040-7223565b2051.cpp:23:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 8230360c-7c74-4c2f-9040-7223565b2051.cpp:23:33: error: ‘endl’ was not declared i n this scope    23 |         cout << camelCase(s) << endl;       |                                 ^~~~ 8230360c-7c74-4c2f-9040-7223565b2051.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- camel-case.cpp
+++ camel-case.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 string camelCase(string s) {
     string result = "";
-    bool firstWord = true;
+    int upper = 0;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == '-') {
             i++; // skip the "-"
             while (i < s.length() && s[i] == ' ') {
                 i++; // skip the spaces
             }
-            result += toupper(s[i]); 
-            firstWord = false;
-        } else if (!firstWord) {
-            result += tolower(s[i]); 
-        } else {
-            result += tolower(s[i]);
+            if (upper) result += tolower(s[i]);
+            else result += toupper(s[i]); 
+            upper = 1;
+        } else if (s[i] != ' ') {
+            if (!upper) result += tolower(s[i]);
+            else {
+                result += toupper(s[i]);
+                upper = 0;
+            }
         }
     }
     return result;
