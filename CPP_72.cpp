780cefe1206552f9573b35b29da0e7af70c7eba3
beep boop <githubbot@vadim.me>
2024-08-03 00:13:21 +0200

    Solution 45 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/x4pnmr61 Prompt: c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | bool will_it_fly(vector<int> q, int w) {       |                  ^~~~~~ c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^ In file included from c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp:14: c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp: In function ‘int main()’: c728fef6-00f7-4a5c-9880-e9fc5f69a456.cpp:16:24: error: ‘will_it_fly’ cannot be u sed as a function    16 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -1,16 +1,16 @@
-```
-#include <vector>
-#include <string>
+```#include <vector>
+#include <algorithm>
+using namespace std;
 
-bool will_it_fly(std::vector<int> q, int w) {
-    std::string str = "";
+bool will_it_fly(vector<int> q, int w) {
+    string str = "";
     for (int i : q) {
-        str += std::to_string(i);
+        str += to_string(i);
     }
-    if (str != std::string(str.rbegin(), str.rend()).s) return false;
+    if (str != string(str.rbegin(), str.rend()).s) return false;
     int sum = 0;
     for (int i : q) {
         sum += i;
     }
     return sum <= w;
-}
+}```
