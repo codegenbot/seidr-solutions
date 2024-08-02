ce17ecea66438e37042e980ae810c7189afa6f16
beep boop <githubbot@vadim.me>
2024-08-02 04:12:29 +0200

    Solution 4 of Python/106, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1f0p17rg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88751a26-411a-4d1c-831b-34f28c1a5 a6d.py", line 10, in <module>     check(f)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88751a26-411a-4d1c-831b-34f28c1a5 a6d.py", line 7, in check     assert f(3) == [1, 2, 6]            ^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88751a26-411a-4d1c-831b-34f28c1a5 a6d.py", line 2, in f     return [1] + [                  ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88751a26-411a-4d1c-831b-34f28c1a5 a6d.py", line 3, in <listcomp>     math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1))     ^^^^ NameError: name 'math' is not defined

--- Python_106.py
+++ Python_106.py
@@ -1,2 +1,6 @@
+import math
 def f(n):
-    return [1 if i % 2 != 0 else math.factorial(i) for i in range(1, n+1)]
+    return [1] + [
+        math.factorial(i) if i % 2 == 0 else sum(range(1, i + 1))
+        for i in range(2, n + 1)
+    ]
