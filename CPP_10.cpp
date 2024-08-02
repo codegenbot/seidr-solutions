3f1bf19c30043b8a03e444bd1c9d5dc7f89369db
beep boop <githubbot@vadim.me>
2024-08-02 10:08:19 +0200

    Solution 2 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/k0laz9q1 Prompt: Complete the following code given the task description and function signature.

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,20 +1,12 @@
-#include<stdio.h>
-#include<string>
+#include <algorithm>
 using namespace std;
 
-bool is_palindrome(string str){
-    string s(str.rbegin(),str.rend());
-    return s==str;
-}
-
-string make_palindrome(string str){
-    if(is_palindrome(str)) return str; // If input string is already a palindrome, just return it
-    int i = 0;
-    while(i < str.length() && str[i] == ' ') i++; // skip leading spaces
-    for(int j = str.length()-1; j > i && str[j] == ' '; j--); // find the last non-space character from right
-    string prefix = str.substr(0, j+1);
-    int rev_len = str.length() - (j + 1);
-    string postfix = str.substr(j+1);
-    reverse(postfix.begin(),postfix.end());
-    return prefix + postfix;
+string make_palindrome(string str) {
+    string rev = str;
+    reverse(rev.begin(), rev.end());
+    size_t pos = 0;
+    while (pos < str.length() && str[pos] == rev[rev.size()-1-pos]) {
+        pos++;
+    }
+    return str.substr(0, pos) + rev.substr(rev.size()-pos);
 }
