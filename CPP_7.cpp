7551976e250a850935c2896450a35ab4f05b77ae
beep boop <githubbot@vadim.me>
2024-08-02 13:20:58 +0200

    Solution 38 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nvgc0b2p Prompt:    15 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ f1030ce6-fb25-46d0-a1b3-5503b8b6f34d.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ f1030ce6-fb25-46d0-a1b3-5503b8b6f34d.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from f1030ce6-fb25-46d0-a1b3-5503b8b6f34d.cpp:14: f1030ce6-fb25-46d0-a1b3-5503b8b6f34d.cpp: In function ‘int main()’: f1030ce6-fb25-46d0-a1b3-5503b8b6f34d.cpp:24:20: error: ‘filter_by_substring’ was  not declared in this scope    24 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ f1030ce6-fb25-46d0-a1b3-5503b8b6f34d.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,9 +1,12 @@
 bool issame(string s1, string s2) {
-    if (s1.size() != s2.size())
+    if (s1.length() != s2.length()) {
         return false;
-    for (int i = 0; i < s1.size(); ++i)
-        if (tolower(s1[i]) != tolower(s2[i]))
+    }
+    for (int i = 0; i < s1.length(); ++i) {
+        if (s1[i] != s2[i]) {
             return false;
+        }
+    }
     return true;
 }
 
