dedd1269d0822bd41e13f7f8178f21352f97c444
beep boop <githubbot@vadim.me>
2024-08-02 10:50:08 +0200

    Solution 15 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qsjpohll Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4e7b05ac-8ad2-43b4-93f2-5d3081c7fdc6.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4e7b05ac-8ad2-43b4-93f2-5d3081c7fdc6.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4e7b05ac-8ad2-43b4-93f2-5d3081c7fdc6.cpp:11: 4e7b05ac-8ad2-43b4-93f2-5d3081c7fdc6.cpp: In function ‘int main()’: 4e7b05ac-8ad2-43b4-93f2-5d3081c7fdc6.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 4e7b05ac-8ad2-43b4-93f2-5d3081c7fdc6.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,15 +1,23 @@
-```cpp
-vector<int> filter_integers(list_any values) {
-    vector<int> result;
+#include <boost/any.hpp>
+#include <vector>
+#include <list>
+
+std::vector<int> filter_integers(std::list<boost::any> values) {
+    std::vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value)) {
-            result.push_back(boost::any_cast<int>(value));
+        if (value.type() == typeid(int)) {
+            int val = boost::any_cast<int>(value);
+            bool isSame = true;
+            for (int i : result) {
+                if (i != val) {
+                    isSame = false;
+                    break;
+                }
+            }
+            if (!isSame) {
+                result.push_back(val);
+            }
         }
     }
     return result;
-}
-bool issame(vector<int> a, vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    return a == b;
 }
