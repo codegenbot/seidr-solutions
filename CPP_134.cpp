939500d163ce562186f32b321ea23b097f207577
beep boop <githubbot@vadim.me>
2024-08-03 00:53:46 +0200

    Solution 19 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/bh2gbaxg Prompt: 9adc94e3-431c-44c9-95f2-bbb1bc4516a4.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ 9adc94e3-431c-44c9-95f2-bbb1bc4516a4.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -3,13 +3,15 @@
 #include <cassert>
 #include <cctype>
 
-bool check_if_last_char_is_a_letter(const std::string &txt) {
-    return !txt.empty() && isalpha(txt.back()) && std::isspace(txt[txt.size() - 2]);
-}
+bool check_if_last_char_is_a_letter(const std::string &txt);
 
 int main() {
     assert(check_if_last_char_is_a_letter("apple pi e ") == false);
     assert(check_if_last_char_is_a_letter("orange pi e") == true);
 
     return 0;
+}
+
+bool check_if_last_char_is_a_letter(const std::string &txt) {
+    return !txt.empty() && isalpha(txt.back()) && std::isspace(txt[txt.size() - 2]);
 }
