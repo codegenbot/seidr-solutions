7882fada909a7ceb0e84ca2da7929d4d2f9dadfd
beep boop <githubbot@vadim.me>
2024-08-02 11:29:56 +0200

    Solution 81 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/odummyid Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f33516f3-def3-4cdf-98e4-465973dba d4d.py", line 10     if string[i:] == string[i:]][::-1]:                                ^ SyntaxError: unmatched ']'

--- Python_10.py
+++ Python_10.py
@@ -5,6 +5,6 @@ def make_palindrome(string: str) -> str:
     if string == string[::-1]:
         return string + string
     for i in range(len(string)):
-        if string[i:] == string[i+1:][::-1]:
+        if string[i:] == string[i+1:][::-1]: 
             return string[:i] + string[i:][::-1]
     return string + string[::-1]
