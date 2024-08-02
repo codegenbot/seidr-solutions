45ad96ebae2d9ad749c9dbd725a088ac0d14fd41
beep boop <githubbot@vadim.me>
2024-08-02 14:51:05 +0200

    Solution 61 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f7cizk1g Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:9: 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp: In function ‘int main()’: 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:19:20: error: ‘unique’ was not declared  in this scope    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,10 +1,11 @@
-```cpp
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size())
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()) {
         return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i])
+    }
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
             return false;
+        }
     }
     return true;
 }
@@ -13,6 +14,19 @@ vector<int> unique(vector<int> l){
     vector<int> result(l.begin(),l.end());
     sort(result.begin(),result.end());
     auto it = unique(result.begin(),result.end());
-    result.erase(it,result.end());
+    while(it != result.end()) {
+        bool isUnique = true;
+        for(int i = 0; i < result.size(); i++) {
+            if(i != 0 && issame(vector<int>(i,result[i]), vector<int>(0,1))) {
+                isUnique = false;
+                break;
+            }
+        }
+        if(isUnique) {
+            it = result.erase(it);
+        } else {
+            ++it;
+        }
+    }
     return result;
 }
