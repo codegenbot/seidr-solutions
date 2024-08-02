81193c20e353149d6652a4915bd40a8beed0342c
beep boop <githubbot@vadim.me>
2024-08-02 22:19:21 +0200

    Solution 92 of Python/140, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j04gqqx3 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/21abe000-cd00-448c-9eeb-15413a6a1 697.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_140.py
+++ Python_140.py
@@ -1,9 +1,8 @@
-```
-def fix_spaces(text):
+def replace_spaces_with_underscores(text):
     new_text = ''
     for char in text:
         if char == ' ':
-            if len(new_text) > 0 and new_text[-1] != ' ':
+            if len(new_text) > 0 and new_text[-1] != '_':
                 new_text += '-'
             else:
                 new_text += '_'
