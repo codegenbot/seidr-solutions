991df412a0837e072d713c2b7e44fd249f04b9ba
beep boop <githubbot@vadim.me>
2024-08-02 20:29:58 +0200

    Solution 39 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ceikwutp Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 26e5e653-2528-43d7-9de4-3aafc2ff369f.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 26e5e653-2528-43d7-9de4-3aafc2ff369f.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 26e5e653-2528-43d7-9de4-3aafc2ff369f.cpp:11: 26e5e653-2528-43d7-9de4-3aafc2ff369f.cpp: In function ‘int main()’: 26e5e653-2528-43d7-9de4-3aafc2ff369f.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 26e5e653-2528-43d7-9de4-3aafc2ff369f.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -6,24 +5,11 @@ bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
-    for (int i = 0; i < a.size(); ++i) {
-        if (issame_helper(a[i], b[i]) == false) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
-    return true;
-}
-
-bool issame_helper(string str1, string str2) {
-    int len1 = str1.length();
-    int len2 = str2.length();
-
-    for (int i = 0; i < min(len1, len2); ++i) {
-        if (str1[i] != str2[i]) {
-            return false;
-        }
-    }
-
     return true;
 }
 
@@ -35,19 +21,4 @@ vector<string> filter_by_substring(vector<string> strings, string substring){
         }
     }
     return result;
-}
-
-int main() {
-    vector<string> strings = {"Hello", "World", "Hello World"};
-    string substring = "Hello";
-    
-    vector<string> filtered_strings = filter_by_substring(strings, substring);
-
-    if (issame(filtered_strings, strings)) {
-        cout << "Filtered strings are same as original strings." << endl;
-    } else {
-        cout << "Filtered strings are different from original strings." << endl;
-    }
-
-    return 0;
 }
