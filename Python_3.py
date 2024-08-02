245e4d31c547bedb54db7fe77e8c300350add11f
beep boop <githubbot@vadim.me>
2024-08-02 13:26:06 +0200

    Solution 68 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/znempiza Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54bc5e30-ff10-40b9-a67e-24a338589 9f3.py", line 14, in <module>     check(below_zero)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54bc5e30-ff10-40b9-a67e-24a338589 9f3.py", line 11, in check     assert below_zero([1, -2, 2, -2, 5, -5, 4, -4]) == True            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ TypeError: below_zero() takes 0 positional arguments but 1 was given

--- Python_3.py
+++ Python_3.py
@@ -1,14 +1,11 @@
 from typing import List
 
+
 def below_zero(operations: List[int]) -> bool:
+    n = len(operations)
     balance = 0
     for operation in operations:
         balance += operation
         if balance < 0:
             return True
     return False
-
-if __name__ == "__main__":
-    operations = list(map(int, input().split()))
-    result = below_zero(operations)
-    print(result)
