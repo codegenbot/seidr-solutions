17d1fbf13c907cd9bc529b1dc819d1d6407a25a3
beep boop <githubbot@vadim.me>
2024-08-03 01:10:37 +0200

    Solution 62 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7admhmgi Prompt:       |                             ^~~~~~ fb03f326-0dd9-4aa1-a3fd-87d483ce29f1.cpp:33:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from fb03f326-0dd9-4aa1-a3fd-87d483ce29f1.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fb03f326-0dd9-4aa1-a3fd-87d483ce29f1.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fb03f326-0dd9-4aa1-a3fd-87d483ce29f1.cpp:33:36: error: expected primary-expressi on before ‘float’    33 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fb03f326-0dd9-4aa1-a3fd-87d483ce29f1.cpp:41:5: error: conflicting declaration of  C function ‘int main()’    41 | int main(){       |     ^~~~ fb03f326-0dd9-4aa1-a3fd-87d483ce29f1.cpp:28:5: note: previous declaration ‘int m ain(int, char**)’    28 | int main(int argc, char** argv) {       |     ^~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -25,6 +25,6 @@ std::vector<float> sort_even(std::vector<float> l) {
     return l;
 }
 
-int main() {
+int main(int argc, char** argv) {
     assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), std::vector<float>{-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 }
