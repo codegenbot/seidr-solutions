e03526fdcf7f06a69101b46d4989bdbd9a9eaf8b
beep boop <githubbot@vadim.me>
2024-08-02 11:42:32 +0200

    Solution 50 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/uqca9z2w Prompt:       |                    ^~~~~ 654721eb-7d36-4681-aae7-4d68ff125907.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 654721eb-7d36-4681-aae7-4d68ff125907.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 654721eb-7d36-4681-aae7-4d68ff125907.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 654721eb-7d36-4681-aae7-4d68ff125907.cpp:11: 654721eb-7d36-4681-aae7-4d68ff125907.cpp: In function ‘int main()’: 654721eb-7d36-4681-aae7-4d68ff125907.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 654721eb-7d36-4681-aae7-4d68ff125907.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,21 +1,12 @@
-#include <cassert>
 #include <vector>
+#include <cassert>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
-}
-
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
-    for (float num : l) {
-        if (num > 0) {
-            result.push_back(num);
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
+    for(float i : l) {
+        if(i > 0) {
+            result.push_back(i);
         }
     }
     return result;
-}
-
-int main() {
-    assert(std::issame(get_positive({}), {}));
-    // ... rest of your code ...
 }
