08678f9332e2c81418dab8a511726e64f12b5e80
beep boop <githubbot@vadim.me>
2024-08-02 23:07:07 +0200

    Solution 9 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/heex6c41 Prompt:       |                    ^~~ fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp:15: fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp: In function ‘int main()’: fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp:25:20: error: ‘sort_array’ was not decl ared in this scope    25 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ fc82c396-3d1b-416a-a606-b34bfc407fd2.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,5 +1,14 @@
-vector<int> sort_array(vector<int> arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+std::vector<int> sort_array(std::vector<int> arr) {
+    std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int count_a = __builtin_popcount(a);
         int count_b = __builtin_popcount(b);
 
@@ -13,10 +22,8 @@ vector<int> sort_array(vector<int> arr) {
     return arr;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 int main() {
     assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
+    
+    return 0;
 }
