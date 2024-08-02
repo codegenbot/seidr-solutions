f3e3d1306581e848a4f1a93affab0945c3ff1783
beep boop <githubbot@vadim.me>
2024-08-02 23:34:58 +0200

    Solution 2 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/p3ufggj4 Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 52563042-1094-4763-8091-0357acae9d0a.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 52563042-1094-4763-8091-0357acae9d0a.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 52563042-1094-4763-8091-0357acae9d0a.cpp:11: 52563042-1094-4763-8091-0357acae9d0a.cpp: In function ‘int main()’: 52563042-1094-4763-8091-0357acae9d0a.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 52563042-1094-4763-8091-0357acae9d0a.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,9 +1,19 @@
-vector<int> filter_integers(list_any values){
+#include <vector>
+#include <list>
+#include <boost/any.hpp>
+
+using namespace std;
+
+vector<int> filter_integers(list<boost::any> values){
     vector<int> result;
-    for (const auto &value : values) {
-        if (value.type() == typeid(int)) {
-            result.push_back(boost::any_cast<int>(value));
+    for (const auto &val : values) {
+        if (val.type() == typeid(int)) {
+            result.push_back(boost::any_cast<int>(val));
         }
     }
     return result;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    return a == b;
 }
