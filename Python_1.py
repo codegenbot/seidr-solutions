22f62adbb1bec0bb9a859f42750e42d3380e794a
beep boop <githubbot@vadim.me>
2024-08-02 10:35:33 +0200

    Solution 47 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/22zbf0as Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8d4cfc93-3ff0-41f6-9f21-46c10fba6 1b5.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,20 +1,6 @@
-from typing import List
-
-
-def separate_paren_groups(paren_string: str) -> List[str]:
-    result = []
-    stack = []
-    groups = ""
-
-    for char in paren_string:
-        if char == " ":
-            continue
-        if char == "(":
-            stack.append(char)
-            groups += char
-        elif char == ")":
-            stack.pop()
-            if not stack:
-                result.append(groups + char)
-                groups = ""
-    return result
+def find_max_score(game_scores):
+    max_score = float('-inf')
+    for score in game_scores:
+        if score > max_score:
+            max_score = score
+    return max_score
