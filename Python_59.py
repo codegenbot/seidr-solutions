a29910e7dd715ac7555a5ecf673cf0d6c36764f8
beep boop <githubbot@vadim.me>
2024-08-03 00:47:40 +0200

    Solution 1 of Python/59, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/oklgh4dg Prompt: Complete the following code given the task description and function signature.

--- Python_59.py
+++ Python_59.py
@@ -1,15 +1,10 @@
-```
 def largest_prime_factor(n: int):
     i = n - 1
     while i > 1:
-        if n % i == 0 and is_prime(i):
+        if n % i == 0:
+            for j in range(2, i):
+                if i % j == 0:
+                    i -= 1
+                    break
             return i
         i -= 1
-
-def is_prime(k: int):
-    if k < 2:
-        return False
-    for i in range(2, int(k**0.5) + 1):
-        if k % i == 0:
-            return False
-    return True
