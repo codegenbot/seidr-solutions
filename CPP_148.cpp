e9ffb00d9c336e677e993e7f8c51254e1ab41727
beep boop <githubbot@vadim.me>
2024-08-02 03:23:51 +0200

    Solution 28 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9gt9qom5 Prompt:       |                                 ^~~~~~ In file included from 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp:25: 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp: In function ‘int main()’: 170bb9b3-f826-46f1-9392-1d57cc6d956d.cpp:35:20: error: ‘bf’ was not declared in this scope    35 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -7,7 +7,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b) {
         return false;
     }
     
-    for (int i = 0; i < a.size(); ++i) {
+    for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
