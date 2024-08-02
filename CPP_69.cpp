adba5fdadf0aa280b4f38b2651f78cef137fe6ab
beep boop <githubbot@vadim.me>
2024-08-02 13:13:43 +0200

    Solution 20 of CPP/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/wd3pedw3 Prompt: 1e01365e-41f3-47b2-95e9-ee4b40e2b807.cpp: In function ‘int search(std::vector<in t>)’: 1e01365e-41f3-47b2-95e9-ee4b40e2b807.cpp:7:27: error: ‘count’ was not declared i n this scope     7 |         if (i > 0 && i >= count(i, lst)) {       |                           ^~~~~

--- CPP_69.cpp
+++ CPP_69.cpp
@@ -1,16 +1,13 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int search(vector<int> lst) {
     int max = 0;
     for (int i : lst) {
-        if (i > 0 && i >= count(i, lst)) {
+        if (i > 0 && i >= count(i, begin(lst), end(lst))) { 
             max = i;
         }
     }
     return max == 0 ? -1 : max;
-}
-
-int count(int n, vector<int> v) {
-    return std::count(v.begin(), v.end(), n);
 }
