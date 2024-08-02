58814552cd592aad4e32d9e45a532eb4951e82a5
beep boop <githubbot@vadim.me>
2024-08-02 04:48:30 +0200

    Solution 20 of Python/147, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/8bczd5hb Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fac84e69-b304-4344-ba6f-003b98f2f 260.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_147.py
+++ Python_147.py
@@ -1,8 +1,10 @@
+````
 def get_max_triples(n):
-    max_count = 0
-    for i in range(1, n+1):
-        for j in range(i+1, n+1):
-            for k in range(j+1, n+1):
-                if (i * i - i + 1) + (j * j - j + 1) + (k * k - k + 1) % 3 == 0:
-                    max_count = max(max_count, 1)
-    return max_count
+    a = [i * i - i + 1 for i in range(1, n+1)]
+    count = 0
+    for i in range(len(a)):
+        for j in range(i+1, len(a)):
+            for k in range(j+1, len(a)):
+                if (a[i] + a[j] + a[k]) % 3 == 0:
+                    count += 1
+    return count
