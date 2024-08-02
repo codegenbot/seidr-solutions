406f4f263782d26fabf7429e56ab73f11b7e807b
beep boop <githubbot@vadim.me>
2024-08-02 11:47:49 +0200

    Solution 49 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/of6jlz25 Prompt: e0fbcc25-be84-414c-8009-9b60653f14d1.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float> v = l;       | ^~~~~~ e0fbcc25-be84-414c-8009-9b60653f14d1.cpp:2:1: error: expected unqualified-id bef ore ‘return’     2 | return *max_element(v.begin(), v.end());       | ^~~~~~ In file included from e0fbcc25-be84-414c-8009-9b60653f14d1.cpp:4: e0fbcc25-be84-414c-8009-9b60653f14d1.cpp: In function ‘int main()’: e0fbcc25-be84-414c-8009-9b60653f14d1.cpp:6:17: error: ‘max_element’ was not decl ared in this scope     6 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ e0fbcc25-be84-414c-8009-9b60653f14d1.cpp:6:13: error: ‘abs’ was not declared in this scope     6 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -2,11 +2,12 @@
 #include <algorithm>
 
 int main() {
-    std::vector<int> l;
-    int n; // Read input from user
-    while (n != -1) {
-        cin >> n;
-        l.push_back(n);
+    std::vector<int> l; 
+    std::cin >> l[0];
+    for(int i = 1; i < 3; ++i) {
+        int temp;
+        std::cin >> temp;
+        l.push_back(temp);
     }
-    std::vector<float> v = l; 
-    return *std::max_element(v.begin(), v.end());
+    return *std::max_element(l.begin(), l.end());
+}
