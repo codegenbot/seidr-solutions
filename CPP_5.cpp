d74b420e844062816f15306334b4d0b1fbddd557
beep boop <githubbot@vadim.me>
2024-08-02 20:20:39 +0200

    Solution 29 of CPP/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/usqkkxzi Prompt: be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp:40: be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp: In function ‘int main()’: be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp:50:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    50 |     assert (issame(intersperse({2, 2, 2}, 2),{2, 2, 2, 2, 2}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp:10:6: note: candidate: ‘bool issame(con st std::vector<int>&, const std::vector<int>&)’    10 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ be2a725d-4aeb-4e3d-ab0d-fcd4f3274a40.cpp:41:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_5.cpp
+++ CPP_5.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool issame(const vector<int>&a, const vector<int>&b);
+bool issame(const vector<int>& a, const vector<int>& b);
 
 vector<int> intersperse(const vector<int>& numbers, int delimiter);
 
@@ -32,7 +32,4 @@ vector<int> intersperse(const vector<int>& numbers, int delimiter) {
     return result;
 }
 
-int main() {
-    assert(issame(intersperse(vector<int>{2, 2, 2}, 2), vector<int>{2, 2, 2, 2, 2}));
-    return 0;
-}
+assert(issame(intersperse({2, 2, 2}, 2), {2, 2, 2, 2, 2}));
