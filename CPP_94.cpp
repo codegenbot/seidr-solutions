ea3806628d69b7812f83393ac45aa7a2695b49e2
beep boop <githubbot@vadim.me>
2024-08-02 14:30:49 +0200

    Solution 13 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gh2ovwer Prompt: 8bd29db6-5faf-4e28-a00d-456ed0a0db71.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int skjkasdkd(vector<int> lst) {       |               ^~~~~~ 8bd29db6-5faf-4e28-a00d-456ed0a0db71.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int skjkasdkd(vector<int> lst) {       |                      ^~~ In file included from 8bd29db6-5faf-4e28-a00d-456ed0a0db71.cpp:30: 8bd29db6-5faf-4e28-a00d-456ed0a0db71.cpp: In function ‘int main()’: 8bd29db6-5faf-4e28-a00d-456ed0a0db71.cpp:32:22: error: ‘skjkasdkd’ cannot be use d as a function    32 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ~~~~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -33,6 +33,6 @@ bool isPrime(int num) {
 
 int main() {
     std::vector<int> test = {127, 97, 8192};
-    std::cout << skjkasdkd(test) << std::endl;
+    std::cout << skjkasdkd(test);
     return 0;
 }
