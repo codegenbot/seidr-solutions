0ade38285203e4efd34ae52b6f23be4e766ab96d
beep boop <githubbot@vadim.me>
2024-08-02 19:36:16 +0200

    Solution 10 of CPP/136, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/5irizk3b Prompt: 5696b035-5f0d-4340-a317-30d97ab6e87e.cpp: In function ‘std::vector<int> largest_ smallest_integers(std::vector<int>)’: 5696b035-5f0d-4340-a317-30d97ab6e87e.cpp:13:40: error: ‘INT_MAX’ was not declare d in this scope    13 |     int maxNegative = 0, minPositive = INT_MAX;       |                                        ^~~~~~~ 5696b035-5f0d-4340-a317-30d97ab6e87e.cpp:2:1: note: ‘INT_MAX’ is defined in head er ‘<climits>’; did you forget to ‘#include <climits>’?     1 | #include <algorithm>   +++ |+#include <climits>     2 | using namespace std;

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,18 +1,28 @@
-#include <vector>
-
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 vector<int> largest_smallest_integers(vector<int> lst) {
-    int maxNeg = 0;
-    int minPos = INT_MAX;
-
-    for (int num : lst) {
-        if (num < 0 && num > maxNeg) {
-            maxNeg = num;
-        } else if (num > 0 && num < minPos) {
-            minPos = num;
+    vector<int> result;
+    
+    if (lst.empty()) {
+        result.push_back(0);
+        result.push_back(0);
+        return result;
+    }
+    
+    int maxNegative = 0, minPositive = INT_MAX;
+    
+    for (int i : lst) {
+        if (i < 0 && i > maxNegative) {
+            maxNegative = i;
+        } else if (i > 0 && i < minPositive) {
+            minPositive = i;
         }
     }
-
-    return {(maxNeg > 0 ? 0 : maxNeg), (minPos > 0 ? 0 : minPos)};
+    
+    result.push_back(maxNegative);
+    result.push_back(minPositive);
+    
+    return result;
 }
