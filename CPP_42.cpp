ed41324beff65f07ef23a87b506b997b5c900061
beep boop <githubbot@vadim.me>
2024-08-02 14:48:37 +0200

    Solution 74 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/i13ejjq6 Prompt:       |                           ^~~~~~ 51ac3c44-8eff-44be-840d-fc5434e9f95b.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 51ac3c44-8eff-44be-840d-fc5434e9f95b.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 51ac3c44-8eff-44be-840d-fc5434e9f95b.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 51ac3c44-8eff-44be-840d-fc5434e9f95b.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 51ac3c44-8eff-44be-840d-fc5434e9f95b.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 51ac3c44-8eff-44be-840d-fc5434e9f95b.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include <cassert>
 
+std::vector<int> incr_list(const std::vector<int>& l) {
+    std::vector<int> result = l;
+    for (int i = 0; i < result.size(); i++) {
+        result[i]++;
+    }
+    return result;
+}
+
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     if(a.size() != b.size()) return false;
     for (size_t i = 0; i < a.size(); i++) {
@@ -12,15 +20,7 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return true;
 }
 
-std::vector<int> incr_list(const std::vector<int>& l) {
-    std::vector<int> result = l;
-    for (int i = 0; i < result.size(); i++) {
-        result[i]++;
-    }
-    return result;
-}
-
 int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), incr_list({6, 3, 6, 3, 4, 4, 10, 1, 124})));
+    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), {6, 3, 6, 3, 4, 4, 10, 1, 124}));
     return 0;
 }
