50b174dbd61ea2c11ff287eb0b3e9bad65b97b9a
beep boop <githubbot@vadim.me>
2024-08-02 03:22:03 +0200

    Solution 3 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt:    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8f11f2af-4e9b-4c95-8105-2503e8f8216e.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8f11f2af-4e9b-4c95-8105-2503e8f8216e.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8f11f2af-4e9b-4c95-8105-2503e8f8216e.cpp:12: 8f11f2af-4e9b-4c95-8105-2503e8f8216e.cpp: In function ‘int main()’: 8f11f2af-4e9b-4c95-8105-2503e8f8216e.cpp:22:20: error: ‘sorted_list_sum’ was not  declared in this scope    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 8f11f2af-4e9b-4c95-8105-2503e8f8216e.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,8 +1,8 @@
-bool issame(const string& a, const string& b){
-    return a.length() == b.length();
+bool issame(const string& first, const string& second) {
+    return first.length() == second.length();
 }
 
-vector<string> sorted_list_sum(vector<string>& lst){
+vector<string> sorted_list_sum(vector<string>& lst) {
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){return s.length() % 2 != 0;}), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b){
         if (a.length() == b.length()) {
