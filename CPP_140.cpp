514d403908eb204237cb0d0bad83487ecb1e8406
beep boop <githubbot@vadim.me>
2024-08-03 01:18:51 +0200

    Solution 6 of CPP/140, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/5aegt8xk Prompt:       |     ^~~ ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp:30:5: error: expected unqualified-id be fore ‘if’    30 |     if (count > 2) {       |     ^~ ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp:34:7: error: expected unqualified-id be fore ‘else’    34 |     } else if (count > 0) {       |       ^~~~ ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp:38:5: error: expected unqualified-id be fore ‘return’    38 |     return result;       |     ^~~~~~ ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp:39:1: error: expected declaration befor e ‘}’ token    39 | }       | ^ In file included from ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp:41: ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp: In function ‘int main()’: ed23795e-9eea-4535-87a1-2ff977b12fc9.cpp:43:13: error: ‘fix_spaces’ was not decl ared in this scope    43 |     assert (fix_spaces("   Exa 1 2 2 mple") == "-Exa_1_2_2_mple");       |             ^~~~~~~~~~

--- CPP_140.cpp
+++ CPP_140.cpp
@@ -1,19 +1,44 @@
-string result = "";
+#include <string>
+
+using namespace std;
+
+string fix_spaces(const string& text) {
+    string result = "";
+    bool prevSpace = false;
     int count = 0;
+
     for (char c : text) {
         if (c == ' ') {
-            count++;
+            if (prevSpace) {
+                count++;
+            } else {
+                prevSpace = true;
+                count = 1;
+            }
+        } else {
             if (count > 2) {
                 result.pop_back();
                 result.pop_back();
                 result += "-";
-            } else {
+            } else if (count > 0) {
                 result += "_";
             }
-        } else {
-            result += c;
+            prevSpace = false;
             count = 0;
         }
+
+        if (c != ' ') {
+            result += c;
+        }
     }
+
+    if (count > 2) {
+        result.pop_back();
+        result.pop_back();
+        result += "-";
+    } else if (count > 0) {
+        result += "_";
+    }
+
     return result;
 }
