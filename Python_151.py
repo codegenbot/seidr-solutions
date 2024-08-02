41d88022d36153e74f731dcae065e0290b88a0a6
beep boop <githubbot@vadim.me>
2024-08-02 05:04:49 +0200

    Solution 38 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/thkjha2e Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f64fa654-ab9a-4f8b-afe4-8e442f20b 403.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/f64fa654-ab9a-4f8b-afe4-8e442f20b 403.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,8 +1,3 @@
 def double_the_difference(lst):
     odd_sum = sum(i**2 for i in lst if isinstance(i, int) and i % 2 != 0)
-    return double_diff(odd_sum, len([i for i in lst if isinstance(i, int)]))
-
-
-def double_diff(odd_sum, n):
-    even_avg = (sum(i for i in lst if isinstance(i, int)) / n) * 2
-    return abs(odd_sum - even_avg)
+    return abs(odd_sum - (sum(i for i in lst if isinstance(i, int)) / len(lst)) * 2)
