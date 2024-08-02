00ae35c57d6cc74ea55ba91a70bbc822fbd219ae
beep boop <githubbot@vadim.me>
2024-08-02 21:02:54 +0200

    Solution 54 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/zcuavjy9 Prompt: it must return 6 3766 2237 3870 2287 2598 8836 5 4094 4788 1470 3250 2880 for input 11 3766 2237 3870 2287 2598 8836 4094 4788 1470 3250 2880, but it returns 3766 2237 3870 2287 2598 8836 4094 4788 1470 3250 2880.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,51 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <numeric>
-#include <cmath>
+for (int i = 0; i < cutIndex; ++i) {
+    cout << nums[i] << " ";
+}
 
-using namespace std;
+cout << endl;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> nums(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> nums[i];
-    }
-    
-    int leftSum = 0, totalSum = accumulate(nums.begin(), nums.end(), 0);
-    int minDiff = INT_MAX, cutIndex = 0;
-    
-    for (int i = 0; i < n; ++i) {
-        int diff = abs(2*leftSum - totalSum);
-        if (diff < minDiff) {
-            minDiff = diff;
-            cutIndex = i;
-        }
-        leftSum += nums[i];
-    }
-    
-    vector<int> leftSubvector;
-    vector<int> rightSubvector;
-
-    for (int i = 0; i < cutIndex; ++i) {
-        leftSubvector.push_back(nums[i]);
-    }
-
-    for (int i = cutIndex; i < n; ++i) {
-        rightSubvector.push_back(nums[i]);
-    }
-
-    for (int num : leftSubvector) {
-        cout << num << endl;
-    }
-
-    cout << endl;
-
-    for (int num : rightSubvector) {
-        cout << num << endl;
-    }
-    
-    return 0;
+for (int i = cutIndex; i < n; ++i) {
+    cout << nums[i] << " ";
 }
