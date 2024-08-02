285ff58fcb8e4a80f3a38d840238c0c00122dc9a
beep boop <githubbot@vadim.me>
2024-08-02 19:36:53 +0200

    Solution 6 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j9951pdw Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,44 +1,30 @@
-boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        return (float)b > (int)a ? b : a;
-    } else if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return (double)b > (int)a ? b : a;
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str = boost::any_cast<string>(b);
-        int num = (int)a;
-        if (str.length() > 0 && str[0] == ',') {
-            str = str.substr(1);
-        }
-        double d = stod(str);
-        return d > num ? b : a;
-    } else if (a.type() == typeid(float) && b.type() == typeid(double)) {
-        return boost::any_cast<double>(b) > boost::any_cast<float>(a) ? b : a;
-    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string str = boost::any_cast<string>(a);
-        int num = (int)b;
-        if (str.length() > 0 && str[0] == ',') {
-            str = str.substr(1);
-        }
-        double d = stod(str);
-        return d > num ? a : b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        string str = boost::any_cast<string>(a);
-        double d = boost::any_cast<double>(b);
-        if (str.length() > 0 && str[0] == ',') {
-            str = str.substr(1);
-        }
-        return stod(str) > d ? a : b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str1 = boost::any_cast<string>(a);
-        string str2 = boost::any_cast<string>(b);
-        if (str1.length() > 0 && str1[0] == ',') {
-            str1 = str1.substr(1);
-        }
-        if (str2.length() > 0 && str2[0] == ',') {
-            str2 = str2.substr(1);
-        }
-        return stod(str1) > stod(str2) ? a : b;
-    } else {
-        return boost::any("None");
+if (a.type() == boost::any::typeless_type) {
+    if (b.type() == boost::any::typeless_type) {
+        return "None";
+    }
+    else if (boost::any_cast<double>(b) > boost::any_cast<double>(a)) {
+        return b;
+    }
+    else {
+        return a;
+    }
+}
+else if (b.type() == boost::any::typeless_type) {
+    if (boost::any_cast<double>(a) > boost::any_cast<double>(b)) {
+        return a;
+    }
+    else {
+        return "None";
+    }
+}
+else {
+    if (boost::any_cast<std::string>(a) > boost::any_cast<std::string>(b)) {
+        return a;
+    }
+    else if (boost::any_cast<std::string>(b) > boost::any_cast<std::string>(a)) {
+        return b;
+    }
+    else {
+        return "None";
     }
 }
