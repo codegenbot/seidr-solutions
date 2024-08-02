a54f1804eb3dde330bb477fadc338368ed7369c7
beep boop <githubbot@vadim.me>
2024-08-02 19:02:46 +0200

    Solution 53 of CPP/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/o3m0l0eu Prompt: c8985d3a-bd76-4fb0-b223-e267f288304b.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | int add_elements(vector<int> arr,int k){       |                  ^~~~~~ c8985d3a-bd76-4fb0-b223-e267f288304b.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr,int k){       |                         ^~~ c8985d3a-bd76-4fb0-b223-e267f288304b.cpp:1:34: error: expected primary-expressio n before ‘int’     1 | int add_elements(vector<int> arr,int k){       |                                  ^~~ c8985d3a-bd76-4fb0-b223-e267f288304b.cpp:1:39: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int add_elements(vector<int> arr,int k){       |                                       ^ In file included from c8985d3a-bd76-4fb0-b223-e267f288304b.cpp:10: c8985d3a-bd76-4fb0-b223-e267f288304b.cpp: In function ‘int main()’: c8985d3a-bd76-4fb0-b223-e267f288304b.cpp:12:25: error: ‘add_elements’ cannot be used as a function    12 |     assert (add_elements({1}, 1) == 1);       |             ~~~~~~~~~~~~^~~~~~~~

--- CPP_122.cpp
+++ CPP_122.cpp
@@ -12,16 +12,7 @@ int add_elements(std::vector<int> arr,int k){
 }
 
 int main() {
-    std::vector<int> vec;
-    int num;
-    while(true){
-        cout << "Enter a number (-1 to stop): ";
-        cin >> num;
-        if(num == -1) break;
-        vec.push_back(num);
-    }
-
-    int result = add_elements(vec, vec.size());
-    // You can use the result here
+    std::vector<int> vec = {1};
+    int result = add_elements(vec, 1);
     return 0;
 }
