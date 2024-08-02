12fb3ecc13ae8f8178efbda9b1b6036451f4a74b
beep boop <githubbot@vadim.me>
2024-08-02 04:10:01 +0200

    Solution 52 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/73kqgs0l Prompt:       |                           ^~~~~~ 304ddb4c-8372-4a75-b067-a1a65156a7d9.cpp:44:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 304ddb4c-8372-4a75-b067-a1a65156a7d9.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 304ddb4c-8372-4a75-b067-a1a65156a7d9.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 304ddb4c-8372-4a75-b067-a1a65156a7d9.cpp:44:34: error: expected primary-expressi on before ‘int’    44 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 304ddb4c-8372-4a75-b067-a1a65156a7d9.cpp:52:5: error: redefinition of ‘int main( )’    52 | int main(){       |     ^~~~ 304ddb4c-8372-4a75-b067-a1a65156a7d9.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -11,31 +11,3 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     std::sort(arr.rbegin(), arr.rend());
     return std::vector<int>(arr.begin(), arr.begin() + k);
 }
-
-int main() {
-    std::vector<int> arr;
-    int k;
-
-    std::cout << "Enter elements of the array: ";
-    int num;
-    while (std::cin >> num) {
-        arr.push_back(num);
-        if (std::cin.peek() == '\n') {
-            break;
-        }
-    }
-
-    std::cout << "Enter value of k: ";
-    std::cin >> k;
-
-    std::vector<int> result = maximum(arr, k);
-
-    for (int elem : result) {
-        std::cout << elem << " ";
-    }
-    std::cout << std::endl;
-
-    assert(issame(result, {243, 3, 2}));
-
-    return 0;
-}
