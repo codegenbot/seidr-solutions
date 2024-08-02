b598e376642afc4253f9a12eb3601b3167cdd450
beep boop <githubbot@vadim.me>
2024-08-02 03:37:21 +0200

    Solution 16 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/up440yot Prompt:       |                           ^~~~~~ ca59bf94-0db1-461c-86b0-6be706099a77.cpp:23:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from ca59bf94-0db1-461c-86b0-6be706099a77.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from ca59bf94-0db1-461c-86b0-6be706099a77.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ca59bf94-0db1-461c-86b0-6be706099a77.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ca59bf94-0db1-461c-86b0-6be706099a77.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ ca59bf94-0db1-461c-86b0-6be706099a77.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,9 +1,7 @@
 #include <vector>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
-}
+bool issame(std::vector<int> a, std::vector<int> b);
 
 std::vector<int> compare(std::vector<int> game, std::vector<int> guess){
     std::vector<int> result;
@@ -13,7 +11,11 @@ std::vector<int> compare(std::vector<int> game, std::vector<int> guess){
     return result;
 }
 
+bool issame(std::vector<int> a, std::vector<int> b){
+    return a == b;
+}
+
 int main(){
-    assert (issame(compare({1,2,3,5},{-1,2,3,4}),std::vector<int>({2,0,0,1}));
+    assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
     return 0;
 }
