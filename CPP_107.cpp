01beff01bfe15c62db0c1dad33558e95dd301d95
beep boop <githubbot@vadim.me>
2024-08-02 02:43:21 +0200

    Solution 8 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/c2qwob3v Prompt:       |                    ^~~ 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp:17: 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp: In function ‘int main()’: 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 154ca33d-2291-4e28-bf72-a8d21e9600d0.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cassert>
 
-using namespace std;
-
-bool issame(vector<int> a, vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-pair<int, int> even_odd_palindrome(int n) {
+std::vector<int> even_odd_palindrome(int n){
     int even = 0, odd = 0;
     for (int i = 1; i <= n; ++i) {
-        string num = to_string(i);
-        string rev_num = num;
-        reverse(rev_num.begin(), rev_num.end());
+        std::string num = std::to_string(i);
+        std::string rev_num = num;
+        std::reverse(rev_num.begin(), rev_num.end());
         if (num == rev_num) {
             if (i % 2 == 0) {
                 even++;
@@ -26,7 +23,8 @@ pair<int, int> even_odd_palindrome(int n) {
     return {even, odd};
 }
 
-int main() {
+int main(){
     assert(issame(even_odd_palindrome(1), {0, 1}));
+    
     return 0;
 }
