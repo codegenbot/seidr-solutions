8ad9ce9a30fd1b6f6ece5e2edb3987c74216fc96
beep boop <githubbot@vadim.me>
2024-08-02 03:19:52 +0200

    Solution 6 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9gt9qom5 Prompt:       |                              ^~~~~~ bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp:29: bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp: In function ‘int main()’: bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ bbd7549c-ce2c-4d1c-9e38-5dedcb20fec2.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,9 +1,38 @@
 #include <vector>
 #include <string>
-#include <algorithm>
 
-using namespace std;
-
-vector<string> issame(vector<string> a, vector<string> b) {
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
+}
+
+bool bf(string planet1, string planet2) {
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    vector<string> result;
+
+    int index1 = -1, index2 = -1;
+    for (int i = 0; i < planets.size(); ++i) {
+        if (planets[i] == planet1) {
+            index1 = i;
+        }
+        if (planets[i] == planet2) {
+            index2 = i;
+        }
+    }
+
+    if (index1 == -1 || index2 == -1) {
+        return issame(result, result);
+    }
+
+    int start = min(index1, index2);
+    int end = max(index1, index2);
+
+    for (int i = start + 1; i < end; ++i) {
+        result.push_back(planets[i]);
+    }
+
+    return issame(result, result);
+}
+
+int main() {
+    assert(issame(bf("Jupiter", "Makemake"), {}));
 }
