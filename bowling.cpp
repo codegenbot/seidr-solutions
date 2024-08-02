b6fe4125dc7b32f0a1bd79756b3e0ac022035974
beep boop <githubbot@vadim.me>
2024-08-03 00:37:11 +0200

    Solution 30 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/961pbuwt Prompt:    34 |     string input;       |     ^~~~~~ 468a82cb-db07-43a5-b6e2-ed8462d13ea6.cpp:35:5: error: ‘cin’ was not declared in this scope    35 |     cin >> input;       |     ^~~ 468a82cb-db07-43a5-b6e2-ed8462d13ea6.cpp:35:12: error: ‘input’ was not declared in this scope; did you mean ‘int’?    35 |     cin >> input;       |            ^~~~~       |            int 468a82cb-db07-43a5-b6e2-ed8462d13ea6.cpp:36:5: error: ‘cout’ was not declared in  this scope    36 |     cout << bowlingScore(input) << endl;       |     ^~~~ 468a82cb-db07-43a5-b6e2-ed8462d13ea6.cpp:36:25: error: ‘bowlingScore’ cannot be used as a function    36 |     cout << bowlingScore(input) << endl;       |             ~~~~~~~~~~~~^~~~~~~ 468a82cb-db07-43a5-b6e2-ed8462d13ea6.cpp:36:36: error: ‘endl’ was not declared i n this scope    36 |     cout << bowlingScore(input) << endl;       |                                    ^~~~

--- bowling.cpp
+++ bowling.cpp
@@ -11,9 +11,9 @@ int bowlingScore(std::string s) {
             if (frame < 10) {
                 score += (s[bowl + 1] == 'X') ? 10 : (s[bowl + 1] - '0');
                 score += (s[bowl + 2] == 'X') ? 10 : (s[bowl + 2] == '/' ? 10 - (s[bowl + 1] - '0') : (s[bowl + 2] - '0'));
-                frame++;
-                bowl++;
             }
+            frame++;
+            bowl++;
         } else if (c == '/') {
             score += 10 - (s[bowl - 1] - '0');
             score += (s[bowl + 1] == 'X') ? 10 : (s[bowl + 1] - '0');
