85fd35f09e8cd88c97579677bd54e8c3602e0ee1
beep boop <githubbot@vadim.me>
2024-08-02 23:40:37 +0200

    Solution 6 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/3px8lw5q Prompt: dba57896-80de-4c6b-ab54-87e3dadc8e30.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return str.length();       | ^~~~~~ dba57896-80de-4c6b-ab54-87e3dadc8e30.cpp:2:1: error: expected declaration before  ‘}’ token     2 | }       | ^ In file included from dba57896-80de-4c6b-ab54-87e3dadc8e30.cpp:4: dba57896-80de-4c6b-ab54-87e3dadc8e30.cpp: In function ‘int main()’: dba57896-80de-4c6b-ab54-87e3dadc8e30.cpp:6:13: error: ‘strlen’ was not declared in this scope     6 |     assert (strlen("asdasnakj") == 9);       |             ^~~~~~ dba57896-80de-4c6b-ab54-87e3dadc8e30.cpp:5:1: note: ‘strlen’ is defined in heade r ‘<cstring>’; did you forget to ‘#include <cstring>’?     4 | #include<assert.h>   +++ |+#include <cstring>     5 | int main(){

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,8 +1,6 @@
-#include <string>
-#include <cassert>
+#include <cstring> 
 
 int main() {
-    std::string str = "asdasnakj";
-    assert(str.length() == 9);
-    return str.length();
+    assert(strlen("asdasnakj") == 9);
+    return 0;
 }
