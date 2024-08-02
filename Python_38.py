4cc2138fab9ba2b0e1857a6b7980056ea44e735c
beep boop <githubbot@vadim.me>
2024-08-02 18:38:38 +0200

    Solution 98 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/dun9tku7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ab4e5588-5ced-4fb3-ae6e-8dbf3c98a 9ca.py", line 5, in <module>     rearranged_string = rearrange_groups(groups)                                          ^^^^^^ NameError: name 'groups' is not defined

--- Python_38.py
+++ Python_38.py
@@ -2,6 +2,7 @@ def rearrange_groups(groups):
     groups = [group[-1] + group[:-1] if len(group) == 3 else group for group in groups]
     return "".join(groups)
 
-groups = input("Enter the groups separated by commas: ").split(',')
+
+groups = input("Enter the groups separated by commas: ").split(",")
 rearranged_string = rearrange_groups(groups)
 print(rearranged_string)
