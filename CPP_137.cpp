e5a8078313dddd878e8b14f6b3589466a0bd985d
beep boop <githubbot@vadim.me>
2024-08-02 17:08:30 +0200

    Solution 74 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/veoq9x4w Prompt: 82f165d8-a76f-4adb-9358-67d21ee48c75.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+```cpp
 #include <string>
 #include <boost/any.hpp>
 
@@ -8,46 +8,43 @@ boost::any compare_one(boost::any a, boost::any b) {
     if (a.type() == typeid(int) && b.type() == typeid(float)) {
         return max((int)a.convert_to<int>(), (float)b.convert_to<float>());
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str1 = (string)a.convert_to<string>(), str2 = (string)b.convert_to<string>();
-        if (str1 > str2)
+        if ((string)a.convert_to<string>() > (string)b.convert_to<string>())
             return a;
-        else if (str1 < str2)
+        else if ((string)a.convert_to<string>() < (string)b.convert_to<string>())
             return b;
         else
             return boost::any("None");
     } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        int i = (int)a.convert_to<int>();
-        if ((string)str > to_string(i))
-            return b;
-        else if ((string)str < to_string(i))
+        if ((int)a.convert_to<int>() > stoi((string)b.convert_to<string>()))
             return a;
+        else if ((int)a.convert_to<int>() < stoi((string)b.convert_to<string>()))
+            return b;
         else
             return boost::any("None");
     } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         string str = (string)b.convert_to<string>();
         float f = (float)a.convert_to<float>();
-        if ((string)str > to_string(f))
+        if (str > to_string(f))
             return b;
-        else if ((string)str < to_string(f))
+        else if (str < to_string(f))
             return a;
         else
             return boost::any("None");
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         string str = (string)a.convert_to<string>();
         int i = (int)b.convert_to<int>();
-        if ((string)str > to_string(i))
+        if (str > to_string(i))
             return a;
-        else if ((string)str < to_string(i))
+        else if (str < to_string(i))
             return boost::any(b);
         else
             return boost::any("None");
     } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
         string str = (string)a.convert_to<string>();
         float f = (float)b.convert_to<float>();
-        if ((string)str > to_string(f))
+        if (str > to_string(f))
             return a;
-        else if ((string)str < to_string(f))
+        else if (str < to_string(f))
             return boost::any(b);
         else
             return boost::any("None");
@@ -77,36 +74,4 @@ boost::any compare_one(boost::any a, boost::any b) {
             return boost::any("None");
     }
     return a; // default to the first value
-}
-
-int main() {
-    int num1, num2;
-    string str1, str2;
-
-    cout << "Enter the first number: ";
-    cin >> num1;
-    
-    cout << "Enter the second number (or string): ";
-    if (cin >> num2) {
-        boost::any result = compare_one(boost::any(num1), boost::any(num2));
-        if (result.type() == typeid(string))
-            cout << result.convert_to<string>();
-        else
-            cout << result.convert_to<int>();
-    } else {
-        cin.ignore();
-        cout << "Enter the first string: ";
-        getline(cin, str1);
-        
-        cout << "Enter the second string: ";
-        getline(cin, str2);
-
-        boost::any result = compare_one(boost::any(str1), boost::any(str2));
-        if (result.type() == typeid(string))
-            cout << result.convert_to<string>();
-        else
-            cout << "None";
-    }
-
-    return 0;
 }
