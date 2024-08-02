b0d1f8c835da09dc4e8947216483ea24585d645b
beep boop <githubbot@vadim.me>
2024-08-02 13:45:19 +0200

    Solution 57 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/p6lj1b82 Prompt: d3c97656-b57a-4ecd-977a-3279f4929c74.cpp:26:5: error: redefinition of ‘int main( )’    26 | int main(){       |     ^~~~ d3c97656-b57a-4ecd-977a-3279f4929c74.cpp:20:5: note: ‘int main()’ previously def ined here    20 | int main() {       |     ^~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -9,7 +9,6 @@ using namespace std;
 bool has_close_elements(std::initializer_list<float> numbers, float threshold) {
     vector<float> numList(numbers);
     sort(numList.begin(), numList.end());
-
     for (int i = 0; i < numList.size() - 1; ++i) {
         if (abs(numList[i] - numList[i + 1]) < threshold) {
             return true;
