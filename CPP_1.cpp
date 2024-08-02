5f750cb8de3daea2a1fbeb55afe966c5a23b00c0
beep boop <githubbot@vadim.me>
2024-08-02 09:44:17 +0200

    Solution 9 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ojwvu7bk Prompt:    28 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e4dd62be-65fe-4483-a9f6-c7a0e2200474.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e4dd62be-65fe-4483-a9f6-c7a0e2200474.cpp:28:45: error: expression list treated a s compound expression in initializer [-fpermissive]    28 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e4dd62be-65fe-4483-a9f6-c7a0e2200474.cpp:27: e4dd62be-65fe-4483-a9f6-c7a0e2200474.cpp: In function ‘int main()’: e4dd62be-65fe-4483-a9f6-c7a0e2200474.cpp:37:20: error: ‘separate_paren_groups’ w as not declared in this scope    37 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ e4dd62be-65fe-4483-a9f6-c7a0e2200474.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -2,11 +2,13 @@ bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
+
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) {
             return false;
         }
     }
+
     return true;
 }
 
@@ -14,7 +16,7 @@ vector<string> separate_paren_groups(string paren_string){
     vector<string> result;
     string temp = "";
     int count = 0;
-    
+
     for (char c : paren_string) {
         if (c == '(') {
             if (count > 0) {
@@ -32,6 +34,6 @@ vector<string> separate_paren_groups(string paren_string){
             }
         }
     }
-    
+
     return result;
 }
