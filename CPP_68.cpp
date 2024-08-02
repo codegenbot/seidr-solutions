d188533c1209bf2bd73573f032679ab38275ef62
beep boop <githubbot@vadim.me>
2024-08-02 19:47:02 +0200

    Solution 90 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q14tckcu Prompt:       |                                       ^ f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:39: f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp: In function ‘int main()’: f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:49:20: error: ‘pluck’ was not declared in this scope; did you mean ‘std::pluck’?    49 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:8:22: note: ‘std::pluck’ declared here     8 |     std::vector<int> pluck(std::vector<int> arr) {       |                      ^~~~~ In file included from f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:39: f7fa7ed2-149b-4c17-9063-a6a85ffab9cd.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,28 +1,41 @@
 #include <vector>
+#include <iostream>
+#include <cassert>
 
-namespace std {
+namespace mynamespace {
     bool issame(std::vector<int> a, std::vector<int> b) {
         return a == b;
     }
 
-    std::vector<int> pluck(std::vector<int> arr);
+    std::vector<int> pluck(std::vector<int> arr) {
+        int smallestEven = -1;
+        int smallestEvenIndex = -1;
 
-}
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] % 2 == 0 && (smallestEven == -1 || arr[i] < smallestEven)) {
+                smallestEven = arr[i];
+                smallestEvenIndex = i;
+            }
+        }
 
-std::vector<int> std::pluck(std::vector<int> arr) {
-    int smallestEven = -1;
-    int smallestEvenIndex = -1;
-
-    for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] % 2 == 0 && (smallestEven == -1 || arr[i] < smallestEven)) {
-            smallestEven = arr[i];
-            smallestEvenIndex = i;
+        if(smallestEvenIndex == -1) {
+            return {};
+        } else {
+            return {smallestEven};
         }
     }
+}
 
-    if (smallestEvenIndex == -1) {
-        return {};
-    } else {
-        return { smallestEven };
+int main() {
+    std::vector<int> nums {3, 1, 4, 1, 5, 9, 2, 6, 5};
+
+    std::vector<int> result = mynamespace::pluck(nums);
+    
+    for(auto num : result) {
+        cout << num << " ";
     }
+
+    assert(mynamespace::issame(mynamespace::pluck({7, 9, 7, 1}), {}));
+    
+    return 0;
 }
