e39a475810d526e1d98beb57186024a5c5750a3c
beep boop <githubbot@vadim.me>
2024-08-02 02:11:49 +0200

    Solution 6 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/juz418jw Prompt:    21 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 206d302f-8fbf-4d96-aafa-776058555b6e.cpp:21:44: error: ‘b’ was not declared in t his scope    21 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 206d302f-8fbf-4d96-aafa-776058555b6e.cpp:21:45: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 206d302f-8fbf-4d96-aafa-776058555b6e.cpp:20: 206d302f-8fbf-4d96-aafa-776058555b6e.cpp: In function ‘int main()’: 206d302f-8fbf-4d96-aafa-776058555b6e.cpp:30:20: error: ‘words_string’ was not de clared in this scope    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |                    ^~~~~~~~~~~~ 206d302f-8fbf-4d96-aafa-776058555b6e.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -1,26 +1,7 @@
-bool issame(vector<string> a, vector<string> b){
+bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> words_string(string s){
-    vector<string> words;
-    string word;
-    for(char c : s){
-        if(c == ' ' || c == ','){
-            if(!word.empty()){
-                words.push_back(word);
-                word.clear();
-            }
-        } else {
-            word += c;
-        }
-    }
-    if(!word.empty()){
-        words.push_back(word);
-    }
-    return words;
-}
-
-int main(){
-    assert (issame(words_string("ahmed    , gamal"), {"ahmed", "gamal"}));
+int main() {
+    assert(issame(words_string("ahmed     , gamal"), {"ahmed", "gamal"}));
 }
