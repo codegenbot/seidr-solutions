34f9350407f64dfebef689bc79cda3eda727fa36
beep boop <githubbot@vadim.me>
2024-08-02 19:03:41 +0200

    Solution 70 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pth2wzat Prompt:       |                    ^~~ 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp:9: 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp: In function ‘int main()’: 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 5bde946c-0431-4d6b-b159-b2a5065c73d2.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,15 +1,13 @@
-#include <iostream>
-#include <vector>
+#include<vector>
+using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
-    return a==b;
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 vector<int> maximum(vector<int> arr, int k) {
-    vector<int> result;
-    for(int i=0; i<k; i++){
-        result.push_back(*std::max_element(arr.begin(), arr.end()));
-        arr.erase(std::remove(arr.begin(), arr.end(), *std::max_element(arr.begin(), arr.end())), arr.end());
-    }
+    if(k <= 0) return {};
+    vector<int> result(arr.begin(), arr.begin() + min(k,arr.size()));
+    sort(result.begin(), result.end());
     return result;
 }
