98cef4655b57db12c427e80fb8f0b1f5d7fd5229
beep boop <githubbot@vadim.me>
2024-08-02 17:02:21 +0200

    Solution 55 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/veoq9x4w Prompt: 66e606ab-e306-4312-bcfa-9edd2af6d1f1.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,4 @@
-#include "path/to/boost/boost/any.hpp"
+#include <boost/any.hpp>
 #include <string>
 #include <vector>
 
@@ -6,11 +6,11 @@ using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
     if (is_any_of_type<double>(a) && is_any_of_type<double>(b)) {
-        return (dynamic_cast<any &>(a) > dynamic_cast<any &>(b)) ? a : ((dynamic_cast<any &>(a) == dynamic_cast<any &>(b))) ? any("None") : b;
+        return (dynamic_cast<any &>(a) > dynamic_cast<any &>(b)) ? a : ((dynamic_cast<any &>(a) == dynamic_cast<any &>(b))) ? any(0.0) : b;
     } else if (is_any_of_type<std::string>(a) && is_any_of_type<std::string>(b)) {
         return (std::stoi(dynamic_cast<any &>(a).any_to<string>().c_str()) > std::stoi(dynamic_cast<any &>(b).any_to<string>().c_str())) ? a : ((std::stoi(dynamic_cast<any &>(a).any_to<string>().c_str()) == std::stoi(dynamic_cast<any &>(b).any_to<string>().c_str()))) ? any("None") : b;
     } else {
-        return boost::any("Invalid input type");
+        return "Invalid input type";
     }
 }
 
