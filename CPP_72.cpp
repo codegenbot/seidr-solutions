c7e3f1e81a1d0d4fe23bffd3a85ed4adda1bde3f
beep boop <githubbot@vadim.me>
2024-08-02 13:21:24 +0200

    Solution 52 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/9x98dkwj Prompt: 9c884532-75a1-4272-9a0a-623e5317ad71.cpp:1:18: error: ‘vector’ was not declared in this scope     1 | bool will_it_fly(vector<int> q,int w){       |                  ^~~~~~ 9c884532-75a1-4272-9a0a-623e5317ad71.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q,int w){       |                         ^~~ 9c884532-75a1-4272-9a0a-623e5317ad71.cpp:1:32: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q,int w){       |                                ^~~ 9c884532-75a1-4272-9a0a-623e5317ad71.cpp:1:37: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q,int w){       |                                     ^ In file included from 9c884532-75a1-4272-9a0a-623e5317ad71.cpp:15: 9c884532-75a1-4272-9a0a-623e5317ad71.cpp: In function ‘int main()’: 9c884532-75a1-4272-9a0a-623e5317ad71.cpp:17:24: error: ‘will_it_fly’ cannot be u sed as a function    17 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -8,7 +8,7 @@ bool will_it_fly(vector<int> q,int w){
     for(int i : q) {
         str += to_string(i);
     }
-    if(str != reverse(str).s)
+    if(str != reverse(string(str)).s)
         return false;
     int sum = 0;
     for(int i : q) {
