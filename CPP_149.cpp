e6d578ad388cda7996ec3449c7cadd5f0f985fd0
beep boop <githubbot@vadim.me>
2024-08-02 05:41:27 +0200

    Solution 8 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/0tpmckex Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 33b4ffc5-ae42-40d4-a7fc-1545d5134a6d.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 33b4ffc5-ae42-40d4-a7fc-1545d5134a6d.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 33b4ffc5-ae42-40d4-a7fc-1545d5134a6d.cpp:11: 33b4ffc5-ae42-40d4-a7fc-1545d5134a6d.cpp: In function ‘int main()’: 33b4ffc5-ae42-40d4-a7fc-1545d5134a6d.cpp:21:20: error: ‘sorted_list_sum’ was not  declared in this scope    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 33b4ffc5-ae42-40d4-a7fc-1545d5134a6d.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,13 +1,5 @@
-#include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cassert>
-
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
+#include <string>
 
 vector<string> sorted_list_sum(vector<string> lst) {
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s) { return s.length() % 2 != 0; }), lst.end());
@@ -18,10 +10,4 @@ vector<string> sorted_list_sum(vector<string> lst) {
         return a.length() < b.length();
     });
     return lst;
-}
-
-int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
-
-    return 0;
 }
