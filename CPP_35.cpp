57963b5fa33e0279daf70323161fdc2e190c5171
beep boop <githubbot@vadim.me>
2024-08-02 21:50:17 +0200

    Solution 25 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/80pid3wv Prompt: 7ec7220d-2eba-48bc-ae66-c517bd32190a.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float> v = l;       | ^~~~~~ 7ec7220d-2eba-48bc-ae66-c517bd32190a.cpp:2:1: error: expected unqualified-id bef ore ‘return’     2 | return *max_element(v.begin(),v.end());       | ^~~~~~ In file included from 7ec7220d-2eba-48bc-ae66-c517bd32190a.cpp:4: 7ec7220d-2eba-48bc-ae66-c517bd32190a.cpp: In function ‘int main()’: 7ec7220d-2eba-48bc-ae66-c517bd32190a.cpp:6:17: error: ‘max_element’ was not decl ared in this scope     6 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 7ec7220d-2eba-48bc-ae66-c517bd32190a.cpp:6:13: error: ‘abs’ was not declared in this scope     6 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,5 +5,6 @@
 
 int main() {
     std::vector<float> l = {5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10};
-    return abs(*std::max_element(l.begin(), l.end())-12.4)<1e-4;
+    vector<float> v = l; 
+    return abs(*max_element(v.begin(),v.end())-12.4)<1e-4;
 }
