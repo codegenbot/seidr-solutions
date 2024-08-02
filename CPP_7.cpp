8dacdfd8d94f4a6af611fd18cfcbcba56ba6d5bf
beep boop <githubbot@vadim.me>
2024-08-02 13:30:03 +0200

    Solution 69 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/nvgc0b2p Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1c056e9f-ed23-4bce-85e3-7318ab0698f3.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1c056e9f-ed23-4bce-85e3-7318ab0698f3.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1c056e9f-ed23-4bce-85e3-7318ab0698f3.cpp:11: 1c056e9f-ed23-4bce-85e3-7318ab0698f3.cpp: In function ‘int main()’: 1c056e9f-ed23-4bce-85e3-7318ab0698f3.cpp:21:20: error: ‘filter_by_substring’ was  not declared in this scope    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 1c056e9f-ed23-4bce-85e3-7318ab0698f3.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,12 +1,22 @@
 bool issame(vector<string> a, vector<string> b) {
-    return a == b;  
+    bool same = true;
+    if(a.size() != b.size()) same = false;
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) same = false;
+    }
+    return same;
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
     vector<string> result;
     for(string s : strings){
         if(s.find(substring) != string::npos){
-            result.push_back(s);
+            bool same = true;
+            for(string str : strings){
+                if(!str.find(substring) && !same) break;
+                if(str == s) same = false;
+            }
+            if(same) result.push_back(s);
         }
     }
     return result;
