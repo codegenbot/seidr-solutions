bc021fe87fd6075dabfc813dcacf6c8d1e7fb440
beep boop <githubbot@vadim.me>
2024-08-02 12:33:03 +0200

    Solution 58 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60kqal8z Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:16: 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp: In function ‘int main()’: 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 847cf790-7dd7-480c-acfb-8fe73efd94f4.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -5,7 +5,7 @@ bool issame(vector<int> a,vector<int>b){
     if(a.size() != b.size()) return false;
     sort(a.begin(),a.end());
     sort(b.begin(),b.end());
-    for(int i=0;i<a.size();i++){
+    for(int i = 0; i<a.size();i++){
         if(a[i] != b[i]) return false;
     }
     return true;
