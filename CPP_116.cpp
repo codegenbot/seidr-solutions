23f0c34808cd25bc43a4d2a83aade17cead9140b
beep boop <githubbot@vadim.me>
2024-08-02 18:46:40 +0200

    Solution 60 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/q9vvr40p Prompt: /usr/include/c++/11/bits/stl_uninitialized.h:333:37:   required from ‘_ForwardIt erator std::__uninitialized_copy_a(_InputIterator, _InputIterator, _ForwardItera tor, std::allocator<_Tp>&) [with _InputIterator = __gnu_cxx::__normal_iterator<s td::pair<int, int>*, std::vector<std::pair<int, int> > >; _ForwardIterator = int *; _Tp = int]’ /usr/include/c++/11/bits/stl_vector.h:1585:33:   required from ‘void std::vector <_Tp, _Alloc>::_M_range_initialize(_ForwardIterator, _ForwardIterator, std::forw ard_iterator_tag) [with _ForwardIterator = __gnu_cxx::__normal_iterator<std::pai r<int, int>*, std::vector<std::pair<int, int> > >; _Tp = int; _Alloc = std::allo cator<int>]’ /usr/include/c++/11/bits/stl_vector.h:657:23:   required from ‘std::vector<_Tp, _Alloc>::vector(_InputIterator, _InputIterator, const allocator_type&) [with _In putIterator = __gnu_cxx::__normal_iterator<std::pair<int, int>*, std::vector<std ::pair<int, int> > >; <template-parameter-2-2> = void; _Tp = int; _Alloc = std:: allocator<int>; std::vector<_Tp, _Alloc>::allocator_type = std::allocator<int>]’ 2e8d105b-094b-466d-abb8-79ec97a7a006.cpp:12:46:   required from here /usr/include/c++/11/bits/stl_uninitialized.h:138:72: error: static assertion fai led: result type must be constructible from value type of input range   138 | static_assert(is_constructible<_ValueType2, decltype(*__first)>::value,       |                                                                  ^~~~~ /usr/include/c++/11/bits/stl_uninitialized.h:138:72: note: ‘std::integral_consta nt<bool, false>::value’ evaluates to false

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,2 +1,8 @@
-```cpp
-return vector<int>(transform(vec.begin(), vec.end(), [](auto p){ return p.second; }), transform(vec.begin(), vec.end(), [](auto p){ return p.second; }));
+vector<int> sort_vector(vector<int> arr) {
+    vector<pair<int, int>> vec;
+    for (int i : arr) {
+        vec.push_back({__builtin_popcount(i), i});
+    }
+    sort(vec.begin(), vec.end());
+    return vector<int>(make_pair(0, 1));
+}
