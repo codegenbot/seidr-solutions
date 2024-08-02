3cb6ee40b51d74ae61369cc30c9b77112807ef31
beep boop <githubbot@vadim.me>
2024-08-02 21:53:46 +0200

    Solution 96 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/es7qiha1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/32ff6014-d4b9-4a51-be78-529b5c2fb b47.py", line 27, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/32ff6014-d4b9-4a51-be78-529b5c2fb b47.py", line 24, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/32ff6014-d4b9-4a51-be78-529b5c2fb b47.py", line 13, in minPath     x, y = (max(0, min(N - 1, x + dx)), max(0, min(N - 1, y + dy)))                               ^ UnboundLocalError: cannot access local variable 'x' where it is not associated w ith a value

--- Python_129.py
+++ Python_129.py
@@ -1,21 +1,3 @@
-```Python
-def minPath(grid, k):
-    N = len(grid)
-    seen = set()
-    queue = [(grid[0][0], [grid[0][0]])]
-
-    while queue:
-        val, path = queue.pop(0)
-
-        if len(path) == k + 1:
-            return path
-
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            x, y = 0, 0
-            x, y = (max(0, min(N-1, x + dx)), max(0, min(N-1, y + dy)))
-
-            if (x, y) not in seen and grid[x][y] not in path:
-                queue.append((grid[x][y], path + [grid[x][y]]))
-                seen.add((x, y))
-
-    return []
+for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+    x, y = 0, 0
+    x, y = (max(0, min(N - 1, x + dx)), max(0, min(N - 1, y + dy)))
