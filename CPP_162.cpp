41eb9893fb1d51c36254fa190c7e4e60bd0050e4
beep boop <githubbot@vadim.me>
2024-08-02 04:42:14 +0200

    Solution 8 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/0chgyc65 Prompt:     9 | for(int i = 0; i < 16; i++) {       | ^~~ f6a01370-efea-4b52-b6c6-91563c14add5.cpp:9:16: error: ‘i’ does not name a type     9 | for(int i = 0; i < 16; i++) {       |                ^ f6a01370-efea-4b52-b6c6-91563c14add5.cpp:9:24: error: ‘i’ does not name a type     9 | for(int i = 0; i < 16; i++) {       |                        ^ f6a01370-efea-4b52-b6c6-91563c14add5.cpp:13:1: error: expected unqualified-id be fore ‘return’    13 | return string(mdString);       | ^~~~~~ f6a01370-efea-4b52-b6c6-91563c14add5.cpp:14:1: error: expected declaration befor e ‘}’ token    14 | }       | ^ In file included from f6a01370-efea-4b52-b6c6-91563c14add5.cpp:16: f6a01370-efea-4b52-b6c6-91563c14add5.cpp: In function ‘int main()’: f6a01370-efea-4b52-b6c6-91563c14add5.cpp:18:13: error: ‘string_to_md5’ was not d eclared in this scope    18 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,7 @@
+
 #include <iostream>
 #include <string>
-#include <openssl/md5>
+#include <openssl/md5.h>
 
 std::string string_to_md5(const std::string& text) {
     if (text.empty()) {
@@ -11,7 +12,7 @@ std::string string_to_md5(const std::string& text) {
     MD5((unsigned char*)text.c_str(), text.length(), digest);
 
     char mdString[33];
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+    for(int i = 0; i < 16; i++) {
         sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
 
@@ -19,8 +20,6 @@ std::string string_to_md5(const std::string& text) {
 }
 
 int main() {
-    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-    std::cout << "Test passed!" << std::endl;
-
+    assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
     return 0;
 }
