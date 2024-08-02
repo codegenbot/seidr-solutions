7b99ef7bcffc5ddea87313317472eb9b97b4f151
beep boop <githubbot@vadim.me>
2024-08-02 04:44:59 +0200

    Solution 30 of Python/159, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run1/runs/jwe5bwx4 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/55e3abe8-0673-47d3-94ae-3bec3a533 efb.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_159.py
+++ Python_159.py
@@ -1,5 +1,3 @@
-```
 def eat(number, need, remaining):
-    total_eaten = min(number, need)
-    left_carrots = max(0, remaining - (need - total_eaten))
-    return [total_eaten, left_carrots]
+    total = number + need - remaining if remaining >= need else number + remaining
+    return [total, abs(remaining - need) if remaining >= need else 0]
