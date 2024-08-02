def7fc7afa50eb767bcca5b76fb9085c836bb6e5
beep boop <githubbot@vadim.me>
2024-08-02 19:38:09 +0200

    Solution 29 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qcm7jvj7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d41e3f52-424c-47f7-8bb6-cea620591 0e1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -7,7 +7,7 @@ def encode(message):
             if char.lower() in vowels:
                 result += chr((ord(char) - ord('a') + 2) % 26 + ord('a'))
             else:
-                result += chr((ord(char) - ord('a') + 1) % 26 + ord('a'))
+                result += chr((ord(char) - ord('a') + 1) % 26 + ord('a')) if char.isupper() else chr((ord(char) - ord('a') - 1) % 26 + ord('a'))
         else:
             result += char
     return result
