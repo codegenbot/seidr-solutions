6ab6f0f87450974bd55612ca053fdd5ba2f32d64
beep boop <githubbot@vadim.me>
2024-08-02 20:36:03 +0200

    Solution 78 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ceikwutp Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0ff86e59-40c4-4149-bb52-3106bf8459c3.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0ff86e59-40c4-4149-bb52-3106bf8459c3.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0ff86e59-40c4-4149-bb52-3106bf8459c3.cpp:10: 0ff86e59-40c4-4149-bb52-3106bf8459c3.cpp: In function ‘int main()’: 0ff86e59-40c4-4149-bb52-3106bf8459c3.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 0ff86e59-40c4-4149-bb52-3106bf8459c3.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -3,7 +3,8 @@
 #include <algorithm>
 
 bool issame(vector<string> a, vector<string>b){
-    return a.size() == b.size() && all_of(a.begin(), a.end(), [&](const string& s){return find(b.begin(), b.end(), s) != b.end();});
+    return a.size() == b.size() && all_of(a.begin(), a.end(), [&](const string& s){return find(b.begin(), b.end(), s) != b.end();}) &&
+           all_of(b.begin(), b.end(), [&](const string& s){return find(a.begin(), a.end(), s) != a.end();});
 }
 
 vector<string> filter_by_substring(vector<string> strings, string substring){
