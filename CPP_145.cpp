74c1353cdc0add17fd8adf48cba9c77720950429
beep boop <githubbot@vadim.me>
2024-08-02 05:31:19 +0200

    Solution 19 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/brdak4ds Prompt:    46 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fbfdc1ee-ddc2-4331-a060-002932b04c12.cpp:54:5: error: redefinition of ‘int main( )’    54 | int main(){       |     ^~~~ fbfdc1ee-ddc2-4331-a060-002932b04c12.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~ In file included from fbfdc1ee-ddc2-4331-a060-002932b04c12.cpp:45: fbfdc1ee-ddc2-4331-a060-002932b04c12.cpp: In function ‘int main()’: fbfdc1ee-ddc2-4331-a060-002932b04c12.cpp:55:20: error: ‘order_by_points’ was not  declared in this scope    55 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ fbfdc1ee-ddc2-4331-a060-002932b04c12.cpp:55:19: error: ‘issame’ cannot be used a s a function    55 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -5,7 +5,24 @@
 using namespace std;
 
 vector<int> order_by_points(vector<int> nums) {
-    // Implementation of order_by_points function remains the same
+    sort(nums.begin(), nums.end(), [](int a, int b) {
+        int sum_a = 0, sum_b = 0;
+        int temp_a = abs(a), temp_b = abs(b);
+        while (temp_a > 0) {
+            sum_a += temp_a % 10;
+            temp_a /= 10;
+        }
+        while (temp_b > 0) {
+            sum_b += temp_b % 10;
+            temp_b /= 10;
+        }
+        if (sum_a == sum_b) {
+            return a < b;
+        }
+        return sum_a < sum_b;
+    });
+
+    return nums;
 }
 
 int main() {
