9c127492dd7eb760c5fff5cb7f40de0eec0d66e3
beep boop <githubbot@vadim.me>
2024-08-02 12:44:00 +0200

    Solution 10 of CPP/66, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/lsa0i476 Prompt: 6bf0f3eb-c173-4761-a8bb-063409430ce4.cpp:1:14: error: ‘string’ was not declared in this scope     1 | int digitSum(string s){       |              ^~~~~~ In file included from 6bf0f3eb-c173-4761-a8bb-063409430ce4.cpp:11: 6bf0f3eb-c173-4761-a8bb-063409430ce4.cpp: In function ‘int main()’: 6bf0f3eb-c173-4761-a8bb-063409430ce4.cpp:13:21: error: ‘digitSum’ cannot be used  as a function    13 |     assert (digitSum("You arE Very Smart") == 327);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_66.cpp
+++ CPP_66.cpp
@@ -1,8 +1,11 @@
-int digitSum(string s) {
+#include <bits/stdc++.h>
+using namespace std;
+
+int digitSum(string s){
     int sum = 0;
-    for (char c : s) {
-        if (isupper(c)) {
-            sum += c;
+    for(char c : s){
+        if(isupper(c)){
+            sum += (int)c - 55; // subtracting ASCII value of 'A' - 1
         }
     }
     return sum;
