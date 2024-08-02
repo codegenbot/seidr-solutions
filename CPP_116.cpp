b0d4c4209e13550bf7b3913965200e0f638c0ac5
beep boop <githubbot@vadim.me>
2024-08-02 18:30:21 +0200

    Solution 21 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/q9vvr40p Prompt:       |           ^~~~~~ In file included from ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp:30:34: error: expected primary-expressi on before ‘int’    30 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp:30:39: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp:29: ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp: In function ‘int main()’: ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp:39:20: error: ‘sort_array’ was not decl ared in this scope    39 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ ec151377-5f78-4c6c-a1c7-16eb478c1ad3.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -1,15 +1,13 @@
 #include <vector>
 #include <algorithm>
 
-int count_ones(int n) {
-    int ones = 0;
-    while (n > 0) {
-        if ((n & 1) == 1) {
-            ones++;
-        }
-        n >>= 1;
+int countOnes(int n) {
+    int count = 0;
+    while (n) {
+        n &= n - 1;
+        count++;
     }
-    return ones;
+    return count;
 }
 
 bool issame(vector<int> a, vector<int> b) {
@@ -18,15 +16,15 @@ bool issame(vector<int> a, vector<int> b) {
 
 vector<int> sort_array(vector<int> arr) {
     vector<int> result;
-
+    
     for (int i = 0; i < arr.size(); i++) {
-        int ones = count_ones(arr[i]);
-        if (i == 0 || ones != count_ones(arr[i-1])) {
+        int ones = countOnes(arr[i]);
+        if (i == 0 || ones != countOnes(arr[i-1])) {
             result.push_back(arr[i]);
         } else {
             bool inserted = false;
             for (int j = 0; j < result.size(); j++) {
-                if (ones >= count_ones(result[j])) {
+                if (ones >= countOnes(result[j])) {
                     result.insert(j, arr[i]);
                     inserted = true;
                     break;
@@ -37,6 +35,6 @@ vector<int> sort_array(vector<int> arr) {
             }
         }
     }
-
+    
     return result;
 }
