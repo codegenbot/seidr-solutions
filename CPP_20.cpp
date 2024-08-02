6f6036c76ae72ac53c9369bec5152c624fec5fde
beep boop <githubbot@vadim.me>
2024-08-03 00:00:03 +0200

    Solution 81 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/g6j2uorj Prompt: on before ‘float’    34 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8644c107-b831-4e40-b197-505ab38c5db8.cpp:34:43: error: expression list treated a s compound expression in initializer [-fpermissive]    34 | bool issame(vector<float> a,vector<float>b){       |                                           ^ 8644c107-b831-4e40-b197-505ab38c5db8.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 8644c107-b831-4e40-b197-505ab38c5db8.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 8644c107-b831-4e40-b197-505ab38c5db8.cpp:33: 8644c107-b831-4e40-b197-505ab38c5db8.cpp: In function ‘int main()’: 8644c107-b831-4e40-b197-505ab38c5db8.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -19,11 +19,9 @@ std::pair<float, float> find_closest_elements(const std::vector<float>& numbers)
             min_diff = sorted_numbers[i + 1] - sorted_numbers[i];
             num1 = sorted_numbers[i];
             num2 = sorted_numbers[i + 1];
-        } else if (issame(sorted_numbers[i + 1] - sorted_numbers[i], min_diff)) {
-            if (sorted_numbers[i] < num1) {
-                num1 = sorted_numbers[i];
-                num2 = sorted_numbers[i + 1];
-            }
+        } else if (issame(sorted_numbers[i + 1], sorted_numbers[i]) && sorted_numbers[i] < num1) {
+            num1 = sorted_numbers[i];
+            num2 = sorted_numbers[i + 1];
         }
     }
     return {num1, num2};
