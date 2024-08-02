02cf36584acb9feb96276eba6a6a9df72e41ede6
beep boop <githubbot@vadim.me>
2024-08-02 03:24:26 +0200

    Solution 14 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt: b393c240-6e5a-405e-85b5-62f282eb40bb.cpp:17:28: error: ‘a’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                            ^ b393c240-6e5a-405e-85b5-62f282eb40bb.cpp:17:30: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ b393c240-6e5a-405e-85b5-62f282eb40bb.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ b393c240-6e5a-405e-85b5-62f282eb40bb.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from b393c240-6e5a-405e-85b5-62f282eb40bb.cpp:16: b393c240-6e5a-405e-85b5-62f282eb40bb.cpp: In function ‘int main()’: b393c240-6e5a-405e-85b5-62f282eb40bb.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,16 +1,10 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <cassert>
-
-using namespace std;
-
-bool issame(const string& a, const string& b){
-    return a.length() == b.length();
+bool issame(const string& first, const string& second) {
+    return first.length() == second.length();
 }
 
-vector<string> sorted_list_sum(vector<string>& lst){
+vector<string> sorted_list_sum(vector<string>& lst);
+
+vector<string> sorted_list_sum(vector<string>& lst) {
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){return s.length() % 2 != 0;}), lst.end());
     sort(lst.begin(), lst.end(), [](const string& a, const string& b){
         if (a.length() == b.length()) {
@@ -19,16 +13,4 @@ vector<string> sorted_list_sum(vector<string>& lst){
         return a.length() < b.length();
     });
     return lst;
-}
-
-int main() {
-    assert (issame("aaaa", "bbbb") == true);
-    assert (issame("hello", "world") == false);
-    assert (issame("cat", "dog") == true);
-    
-    vector<string> input = {"aaaa", "bbbb", "dd", "cc"};
-    vector<string> expected_output = {"cc", "dd", "aaaa", "bbbb"};
-    assert(sorted_list_sum(input) == expected_output);
-    
-    return 0;
 }
