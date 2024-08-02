6dd5f68f875c4a29dfbde9c8c3dbb01e43043193
beep boop <githubbot@vadim.me>
2024-08-02 12:35:11 +0200

    Solution 70 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60kqal8z Prompt:       |                    ^~~ 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp:14: 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp: In function ‘int main()’: 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 8e28d5c4-ce96-4313-923e-f06aa6004e7b.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,26 +1,16 @@
+#include <vector>
 #include <set>
 #include <algorithm>
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-
     set<int> intersection;
+    
     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
-                      std::inserter(intersection, intersection.end()));
-
+        inserter(intersection, intersection.begin()));
+    
     vector<int> result(intersection.begin(), intersection.end());
+    sort(result.begin(), result.end());
     return result;
-}
-
-bool issame(vector<int> a,vector<int>b) {
-    if(a.size() == b.size())
-        return true;
-    else
-        return false;
-}
-
-int main() {
-    assert(common({4, 3, 2, 8}, {}) == {});
-    return 0;
 }
