dd459c0b1a1a71a9f29405dd4340aec42447b3e4
beep boop <githubbot@vadim.me>
2024-08-02 04:00:27 +0200

    Solution 84 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/35kfg2t9 Prompt: Program did not receive expected input

--- Python_39.py
+++ Python_39.py
@@ -30,7 +30,7 @@ def prime_fib():
     num = 1
     while count < n:
         num += 1
-        if is_prime(num) and num == fibonacci(count):
+        if is_prime(num) and num == fibonacci(num):
             count += 1
     return num
 
