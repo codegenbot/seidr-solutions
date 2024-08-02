2e7a83584a0b753802c4989a098d98121d6c8fc3
beep boop <githubbot@vadim.me>
2024-08-02 21:56:27 +0200

    Solution 40 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wjce3cw5 Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 4e4454a2-d263-4191-97aa-b100733f734e.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4e4454a2-d263-4191-97aa-b100733f734e.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 4e4454a2-d263-4191-97aa-b100733f734e.cpp:10: 4e4454a2-d263-4191-97aa-b100733f734e.cpp: In function ‘int main()’: 4e4454a2-d263-4191-97aa-b100733f734e.cpp:20:20: error: ‘unique’ was not declared  in this scope    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 4e4454a2-d263-4191-97aa-b100733f734e.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,11 +1,12 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
+bool issame(vector<int> a,vector<int>b){
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); ++i)
+        if (a[i] != b[i])
+            return false;
     return true;
 }
 
