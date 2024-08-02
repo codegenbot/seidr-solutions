c86fb53afb2cdcff2bc2560fc29649b03fa8ba48
beep boop <githubbot@vadim.me>
2024-08-02 02:06:47 +0200

    Solution 46 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7g2ttqk5 Prompt: 155ad101-fce8-41ef-894c-42720394c6c6.cpp:6:13: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ 155ad101-fce8-41ef-894c-42720394c6c6.cpp:6:20: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 155ad101-fce8-41ef-894c-42720394c6c6.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 155ad101-fce8-41ef-894c-42720394c6c6.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 155ad101-fce8-41ef-894c-42720394c6c6.cpp:5: 155ad101-fce8-41ef-894c-42720394c6c6.cpp: In function ‘int main()’: 155ad101-fce8-41ef-894c-42720394c6c6.cpp:15:20: error: ‘count_up_to’ was not dec lared in this scope    15 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,29 +1,13 @@
-#include <vector>
-#include <cassert>
-#include <algorithm>
-
-std::vector<int> count_up_to(int n) {
-    std::vector<int> result;
-    if (n <= 1) {
-        return result;
+{
+    if (a.size() != b.size()) {
+        return false;
     }
-    std::vector<bool> isPrime(n, true);
-    isPrime[0] = isPrime[1] = false;
-    for (int i = 2; i * i < n; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j < n; j += i) {
-                isPrime[j] = false;
-            }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    for (int i = 2; i < n; ++i) {
-        if (isPrime[i]) {
-            result.push_back(i);
-        }
-    }
-    return result;
-}
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+    return true;
 }
