5bd2812c87b458c2764142c361d5657c871a4820
beep boop <githubbot@vadim.me>
2024-08-02 22:32:53 +0200

    Solution 3 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/tg3wf24n Prompt:     2 |     vector<int> nums;       |            ^~~ a5b20bbd-3286-4cf2-90af-9e692a03be5a.cpp:4:12: error: ‘cin’ was not declared in this scope     4 |     while (cin >> n) {       |            ^~~ a5b20bbd-3286-4cf2-90af-9e692a03be5a.cpp:5:9: error: ‘nums’ was not declared in this scope     5 |         nums.push_back(n);       |         ^~~~ a5b20bbd-3286-4cf2-90af-9e692a03be5a.cpp:9:20: error: ‘nums’ was not declared in  this scope; did you mean ‘num’?     9 |     for (int num : nums) {       |                    ^~~~       |                    num a5b20bbd-3286-4cf2-90af-9e692a03be5a.cpp:13:5: error: ‘cout’ was not declared in  this scope    13 |     cout << sum << endl;       |     ^~~~ a5b20bbd-3286-4cf2-90af-9e692a03be5a.cpp:13:20: error: ‘endl’ was not declared i n this scope    13 |     cout << sum << endl;       |                    ^~~~

--- fuel-cost.cpp
+++ fuel-cost.cpp
@@ -9,13 +9,13 @@ int main() {
     while (std::cin >> n) {
         nums.push_back(n);
     }
-
+    
     int sum = 0;
     for (int num : nums) {
         sum += static_cast<int>(num / 3) - 2;
     }
-
+    
     std::cout << sum << std::endl;
-
+    
     return 0;
 }
