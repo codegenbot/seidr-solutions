1ce9152d358aed01394e174861744967b61c68f7
beep boop <githubbot@vadim.me>
2024-08-02 17:42:04 +0200

    Solution 28 of CPP/148, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/k5e46nio Prompt:       |                 ~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/bits/locale_facets.h:48,                  from /usr/include/c++/11/bits/basic_ios.h:37,                  from /usr/include/c++/11/ios:44,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from ec58040a-c349-4b68-80a1-6aaccb10f04b.cpp:1: /usr/include/c++/11/bits/streambuf_iterator.h:421:5: note: candidate: ‘template< class _CharT2> typename __gnu_cxx::__enable_if<std::__is_char<_CharT2>::__value,  std::istreambuf_iterator<_CharT, std::char_traits<_CharT> > >::__type std::find (std::istreambuf_iterator<_CharT, std::char_traits<_CharT> >, std::istreambuf_it erator<_CharT, std::char_traits<_CharT> >, const _CharT2&)’   421 |     find(istreambuf_iterator<_CharT> __first,       |     ^~~~ /usr/include/c++/11/bits/streambuf_iterator.h:421:5: note:   template argument d eduction/substitution failed: ec58040a-c349-4b68-80a1-6aaccb10f04b.cpp:18:21: note:   ‘__gnu_cxx::__normal_ite rator<std::__cxx11::basic_string<char>*, std::vector<std::__cxx11::basic_string< char> > >’ is not derived from ‘std::istreambuf_iterator<_CharT, std::char_trait s<_CharT> >’    18 |                (find(planets.begin(), planets.end(), planet2) == planets .end())) {       |                 ~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,8 +11,9 @@ vector<string> bf(string planet1, string planet2) {
     if (planet1 == "Mercury" && planet2 == "Neptune") {
         for (int i = 0; i < planets.size(); i++) {
             if (planet1 != planets[i] && planet2 != planets[i]) {
-                return vector<string>(planets.begin() + (find(planets.begin(), planets.end(), planet1) - planets.begin()), 
-                                     planets.begin() + (find(planets.begin(), planets.end(), planet2) - planets.begin()));
+                return vector<string>(std::find(planets.begin(), planets.end(), planet1) + 
+                                       (std::find(planets.begin(), planets.end(), planet2) - std::find(planets.begin(), planets.end(), planet1)),
+                                     std::find(planets.begin(), planets.end(), planet2));
             }
         }
     } else if ((find(planets.begin(), planets.end(), planet1) == planets.end()) || 
