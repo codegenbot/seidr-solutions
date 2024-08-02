fec843715a364430b3506919ebb57b20b2f39f3c
beep boop <githubbot@vadim.me>
2024-08-02 11:59:08 +0200

    Solution 31 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/73jg3buv Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bc170793-899c-4f5d-88cd-3e664c1d6 ebc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_17.py
+++ Python_17.py
@@ -1,15 +1,20 @@
-```python
 def parse_music(music_string: str) -> List[int]:
-    notes = {'o': 4, 'o|': 2, '.|': 1}
+    notes = {"o": 4, "o|": 2, ".|": 1}
     beats = []
-    note = ''
+    note = ""
     for char in music_string:
-        if char in ['o', 'o|']:
+        if char in ["o", "o|"]:
             note = char
-        elif char == '|':
-            beats.append(notes.get(note, 0))
-            note = ''
-        elif char == '.':
-            beats.append(notes['.|'])
-            note = ''
+        elif char == "|":
+            if note:
+                beats.append(notes[note])
+            else:
+                beats.append(4)
+            note = ""
+        elif char == ".":
+            if note:
+                beats.append(notes[note])
+            else:
+                beats.append(1)
+            note = ""
     return beats
