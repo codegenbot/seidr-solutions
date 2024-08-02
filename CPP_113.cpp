bd25e1bf987ccf6b63e0872e07637c2fb307c52a
beep boop <githubbot@vadim.me>
2024-08-02 03:14:48 +0200

    Solution 5 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/yxq5rq70 Prompt:       |                                            ^ cbc33470-6554-493b-852d-f7ce42fc76b0.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cbc33470-6554-493b-852d-f7ce42fc76b0.cpp:15: cbc33470-6554-493b-852d-f7ce42fc76b0.cpp: In function ‘int main()’: cbc33470-6554-493b-852d-f7ce42fc76b0.cpp:25:20: error: ‘odd_count’ was not decla red in this scope    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ cbc33470-6554-493b-852d-f7ce42fc76b0.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    26 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -22,7 +22,7 @@ vector<string> odd_count(vector<string> lst){
 }
 
 int main(){
-    assert (issame(odd_count({"271", "137", "314"}) ,
+    assert (issame(odd_count({"271", "137", "314"}),
     {
         "the number of odd elements 2 in the string 271 of the input.",
         "the number of odd elements 2 in the string 137 of the input.",
