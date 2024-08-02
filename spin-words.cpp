f63a232ab45275364a53b8a712e342c2cf91dbb7
beep boop <githubbot@vadim.me>
2024-08-02 06:36:38 +0200

    Solution 67 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run1/runs/9k5qi21v Prompt: 157d9f2d-6197-4b4b-bacc-ac001945fba8.cpp: In function ‘std::string spinWords(std ::string)’: 157d9f2d-6197-4b4b-bacc-ac001945fba8.cpp:22:18: error: ‘reverse’ is not a member  of ‘std’    22 |             std::reverse(temp.begin(), temp.end());       |                  ^~~~~~~

--- spin-words.cpp
+++ spin-words.cpp
@@ -17,14 +17,11 @@ std::string spinWords(std::string input) {
 
     output += word;
 
-    for (int i = 0; i < output.length(); i++) {
+    for (int i = 0; i <= output.length() - 1; i++) {
         if (output[i] == ' ' && i + 1 < output.length() && output.substr(i + 1).length() >= 5) {
-            std::string temp = output.substr(i + 1);
-            std::string reversed;
-            for(int j=temp.size()-1; j>=0; j--) {
-                reversed += temp[j];
-            }
-            output.replace(i + 1, temp.length(), reversed);
+            std::string temp = output.substr(i + 1, output.find(' ', i + 1) - i - 1);
+            std::reverse(temp.begin(), temp.end());
+            output.replace(i + 1, temp.length(), temp);
         }
     }
 
