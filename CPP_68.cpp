9ca97cb4d8073aa0491bf18a160d15fc38cfaa5b
beep boop <githubbot@vadim.me>
2024-08-02 19:54:47 +0200

    Solution 96 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q14tckcu Prompt:       |                                       ^ 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:33: 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp: In function ‘int main()’: 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:43:20: error: ‘pluck’ was not declared in this scope; did you mean ‘std::pluck’?    43 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:8:22: note: ‘std::pluck’ declared here     8 |     std::vector<int> pluck(std::vector<int> arr) {       |                      ^~~~~ In file included from 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:33: 33304dc4-c9c5-46c1-a643-55915c9acc8e.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -2,41 +2,30 @@
 #include <iostream>
 #include <cassert>
 
-namespace std {
-    bool issame(std::vector<int> a, std::vector<int> b) {
-        return a == b;
-    }
-    
-    std::vector<int> pluck(std::vector<int> arr) {
-        int smallestEven = -1;
-        int smallestEvenIndex = -1;
-        
-        for(int i = 0; i < arr.size(); i++) {
-            if(arr[i] % 2 == 0 && (smallestEven == -1 || arr[i] < smallestEven)) {
-                smallestEven = arr[i];
-                smallestEvenIndex = i;
-            }
-        }
-        
-        if(smallestEvenIndex == -1) {
-            return {};
-        } else {
-            return {smallestEven};
-        }
-    }
-}
-
-bool areEqualVectors(const std::vector<int>& a, const std::vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-int main() {
-    std::vector<int> testVec = {1, 2, 3, 4, 5, 6, 7, 8};
-    std::vector<int> result = std::pluck(testVec);
-
-    assert(areEqualVectors(result, std::vector<int>{2}));
+std::vector<int> pluck(std::vector<int> arr) {
+    int smallestEven = -1;
+    int smallestEvenIndex = -1;
     
-    assert(areEqualVectors(std::pluck({7, 9, 7, 1}), std::vector<int>{}));    
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] % 2 == 0 && (smallestEven == -1 || arr[i] < smallestEven)) {
+            smallestEven = arr[i];
+            smallestEvenIndex = i;
+        }
+    }
+    
+    if(smallestEvenIndex == -1) {
+        return {};
+    } else {
+        return {smallestEven};
+    }
+}
 
+int main() {
+    assert(issame(pluck({7, 9, 7, 1}), {}));
+    
     return 0;
 }
