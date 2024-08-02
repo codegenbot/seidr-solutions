135f50a7d5b0c967d3ad245e3b2a1b9b6e84c384
beep boop <githubbot@vadim.me>
2024-08-03 01:32:08 +0200

    Solution 72 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/stlccgm9 Prompt: fadc1713-1654-4374-aa94-58f941bb921d.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fadc1713-1654-4374-aa94-58f941bb921d.cpp:28:5: error: redefinition of ‘int main( )’    28 | int main(){       |     ^~~~ fadc1713-1654-4374-aa94-58f941bb921d.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from fadc1713-1654-4374-aa94-58f941bb921d.cpp:19: fadc1713-1654-4374-aa94-58f941bb921d.cpp: In function ‘int main()’: fadc1713-1654-4374-aa94-58f941bb921d.cpp:29:45: error: could not convert ‘larges t_smallest_integers(std::vector<int>(std::initializer_list<int>{((const int*)(& const int [6]{-6, -4, -4, -3, -100, 1})), 6}, std::allocator<int>()))’ from ‘int ’ to ‘std::vector<int>’    29 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~       |                                             |       |                                             int

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -2,16 +2,14 @@
 #include <algorithm>
 #include <cassert>
 
-int sum_smallest_largest(std::vector<int> vec) {
+int largest_smallest_integers(std::vector<int> vec) {
     std::sort(vec.begin(), vec.end());
     return vec.front() + vec.back();
 }
 
-bool is_same(std::vector<int> a, std::vector<int> b) {
-    return (sum_smallest_largest(a) == sum_smallest_largest(b));
-}
+bool issame(std::vector<int> a, std::vector<int> b);
 
 int main() {
-    assert(is_same({-6, -4, -4, -3, -100, 1}, {-3, 1}));
+    assert(issame({-6, -4, -4, -3, -100, 1}, {-3, 1}));
     return 0;
 }
