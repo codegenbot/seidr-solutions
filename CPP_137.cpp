65e3f82ce52abe71bf07f6531ca14893bee9da75
beep boop <githubbot@vadim.me>
2024-08-02 19:47:54 +0200

    Solution 68 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/j9951pdw Prompt: a235547b-e059-4afa-b76b-ab4568ed9f00.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-```cpp
 #include <boost/any.hpp>
 #include <string>
 #include <algorithm>
@@ -15,34 +14,26 @@ boost::any compare_one(boost::any a, boost::any b) {
             return a;
         else if (stoi(a_str) < b_int)
             return b;
-    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
-        string b_str = boost::any_cast<string>(b);
-        int a_int = boost::any_cast<int>(a);
-        if (stoi(b_str) > a_int)
-            return b;
         else
-            return a;
+            return boost::any("None");
     } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string b_str = boost::any_cast<string>(b);
         float a_float = boost::any_cast<float>(a);
+        string b_str = boost::any_cast<string>(b);
         if (stof(b_str) > a_float)
             return b;
-        else
+        else if (stof(b_str) < a_float)
             return a;
+        else
+            return boost::any("None");
     } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         string a_str = boost::any_cast<string>(a);
         int b_int = boost::any_cast<int>(b);
         if (stof(a_str) > b_int)
             return a;
-        else
+        else if (stof(a_str) < b_int)
             return b;
-    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string a_str = boost::any_cast<string>(a);
-        float b_float = boost::any_cast<float>(b);
-        if (stof(a_str) > b_float)
-            return a;
         else
-            return b;
+            return boost::any("None");
     } else {
         float a_float = boost::any_cast<float>(a);
         float b_float = boost::any_cast<float>(b);
@@ -50,5 +41,7 @@ boost::any compare_one(boost::any a, boost::any b) {
             return a;
         else if (a_float < b_float)
             return b;
+        else
+            return boost::any("None");
     }
 }
