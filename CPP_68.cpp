e3accc281393b2bb98432c7dff965098cbb8f35c
beep boop <githubbot@vadim.me>
2024-08-02 13:05:13 +0200

    Solution 66 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/b5n7pi6u Prompt:       |                    ^~~ f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp:18: f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp: In function ‘int main()’: f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp:28:20: error: ‘pluck’ was not declared in this scope    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~ f5c0360e-e0f8-43d3-a2e1-829c9eb3ad8f.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -13,16 +13,20 @@ vector<pair<int, int>> pluck(vector<int> arr) {
     vector<pair<int, int>> result;
     if (arr.empty()) return result;
 
+    int smallestEven = INT_MAX;
+    int smallestIndex = -1;
     for (int i = 0; i < arr.size(); i++) {
-        if (arr[i] % 2 != 0) {
-            result.push_back({arr[i], i});
+        if (arr[i] % 2 == 0 && arr[i] < smallestEven) {
+            smallestEven = arr[i];
+            smallestIndex = i;
         }
     }
 
+    result.push_back({smallestEven, smallestIndex});
     return result;
 }
 
 int main() {
-    assert(issame(pluck({7, 9, 7, 1}), vector<pair<int,int>>()));
+    assert(issame(pluck({7, 9, 7, 1}), {)));
     return 0;
 }
