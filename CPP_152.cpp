b1d8918a6ed2eba8a32e26ad3d7e015073929446
beep boop <githubbot@vadim.me>
2024-08-02 03:00:55 +0200

    Solution 80 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/4kmfrqto Prompt:       |                    ^~~ 501bcff2-3658-4984-a525-2e04a7d0be84.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 501bcff2-3658-4984-a525-2e04a7d0be84.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 501bcff2-3658-4984-a525-2e04a7d0be84.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 501bcff2-3658-4984-a525-2e04a7d0be84.cpp:15: 501bcff2-3658-4984-a525-2e04a7d0be84.cpp: In function ‘int main()’: 501bcff2-3658-4984-a525-2e04a7d0be84.cpp:25:20: error: ‘compare’ was not declare d in this scope    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |                    ^~~~~~~ 501bcff2-3658-4984-a525-2e04a7d0be84.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -1,14 +1,15 @@
+#include <iostream>
 #include <vector>
+#include <assert.h>
+
+using namespace std;
 
 bool issame(vector<int> a, vector<int> b) {
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) return false;
-    }
-    return true;
+    return a == b;
 }
 
-vector<int> compare(vector<int> game, vector<int> guess) {
-    vector<int> result;
+std::vector<int> compare(std::vector<int> game, std::vector<int> guess) {
+    std::vector<int> result;
     for (int i = 0; i < game.size(); i++) {
         if (game[i] == guess[i]) {
             result.push_back(0);
@@ -17,4 +18,9 @@ vector<int> compare(vector<int> game, vector<int> guess) {
         }
     }
     return result;
+}
+
+int main() {
+    assert(issame(compare({1,2,3,5},{-1,2,3,4}),{2,0,0,1}));
+    return 0;
 }
