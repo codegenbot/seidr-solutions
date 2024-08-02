2d005c4eac226187b0f958539b131ba823f6ce31
beep boop <githubbot@vadim.me>
2024-08-02 09:43:40 +0200

    Solution 4 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ojwvu7bk Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 93897cdf-f705-40e7-878e-13a57c640789.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 93897cdf-f705-40e7-878e-13a57c640789.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 93897cdf-f705-40e7-878e-13a57c640789.cpp:26: 93897cdf-f705-40e7-878e-13a57c640789.cpp: In function ‘int main()’: 93897cdf-f705-40e7-878e-13a57c640789.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 93897cdf-f705-40e7-878e-13a57c640789.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,18 +1,30 @@
 #include <vector>
-#include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
+    return a == b;
+}
 
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group;
+    int open_braces = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (open_braces > 0) {
+                current_group += c;
+            }
+            open_braces++;
+        } else if (c == ')') {
+            open_braces--;
+            if (open_braces > 0) {
+                current_group += c;
+            } else if (open_braces == 0 && !current_group.empty()) {
+                result.push_back(current_group);
+                current_group.clear();
+            }
         }
     }
 
-    return true;
+    return result;
 }
-
-vector<string> separate_paren_groups(string paren_string);
