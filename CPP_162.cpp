c74270c8bc5c8c537ca5fd737a140629373dfa01
beep boop <githubbot@vadim.me>
2024-08-02 04:51:19 +0200

    Solution 61 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/0chgyc65 Prompt: In file included from 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp:1: /usr/include/c++/11/iostream:60:18: note: ‘std::cin’ declared here    60 |   extern istream cin;           /// Linked to standard input       |                  ^~~ 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp:10:26: error: ‘string_to_md5’ was not d eclared in this scope    10 | std::string md5_result = string_to_md5(input_text);       |                          ^~~~~~~~~~~~~ 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp:11:6: error: ‘cout’ in namespace ‘std’ does not name a type    11 | std::cout << "MD5 hash: " << md5_result << std::endl;       |      ^~~~ In file included from 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp:1: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ In file included from 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp:13: 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp: In function ‘int main()’: 33ef4c81-bac9-4e42-9b5d-05af0ea19016.cpp:15:13: error: ‘string_to_md5’ was not d eclared in this scope    15 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -3,14 +3,17 @@
 #include <openssl/md5.h>
 
 std::string string_to_md5(const std::string& input) {
+    MD5_CTX ctx;
     unsigned char result[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)input.c_str(), input.length(), result);
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, input.c_str(), input.size());
+    MD5_Final(result, &ctx);
 
     std::stringstream ss;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        ss << std::hex << std::setw(2) << std::setfill('0') << (int)result[i];
+    for(int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(result[i]);
     }
-    
+
     return ss.str();
 }
 
