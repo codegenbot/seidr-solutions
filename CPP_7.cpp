8cb96192426d4fe7dde59cd251c8812922029b9c
beep boop <githubbot@vadim.me>
2024-08-02 09:53:08 +0200

    Solution 16 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/vyk9brqs Prompt:    15 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 4a27232b-c465-4487-85c0-a5cd4d8c745c.cpp:15:44: error: ‘b’ was not declared in t his scope    15 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 4a27232b-c465-4487-85c0-a5cd4d8c745c.cpp:15:45: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 4a27232b-c465-4487-85c0-a5cd4d8c745c.cpp:14: 4a27232b-c465-4487-85c0-a5cd4d8c745c.cpp: In function ‘int main()’: 4a27232b-c465-4487-85c0-a5cd4d8c745c.cpp:24:20: error: ‘filter_by_substring’ was  not declared in this scope    24 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 4a27232b-c465-4487-85c0-a5cd4d8c745c.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,11 +1,14 @@
-```cpp
 bool issame(vector<string> a, vector<string> b) {
     if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
+        if (!(find(a[i].begin(), a[i].end(), ' ') == string::npos || find(b[i].begin(), b[i].end(), ' ') == string::npos)) {
+            auto a_words = split_string(a[i]);
+            auto b_words = split_string(b[i]);
+            sort(a_words.begin(), a_words.end());
+            sort(b_words.begin(), b_words.end());
+            return a_words != b_words;
         }
     }
     return true;
