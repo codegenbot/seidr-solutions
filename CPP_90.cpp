f1a0284e879a3b224058d33d0f01c2827a0e2844
beep boop <githubbot@vadim.me>
2024-08-03 01:23:12 +0200

    Solution 33 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/773t944g Prompt: b74a445a-f517-4aaa-9092-2a48565226b4.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int next_smallest(vector<int> lst){       |                   ^~~~~~ b74a445a-f517-4aaa-9092-2a48565226b4.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int next_smallest(vector<int> lst){       |                          ^~~ In file included from b74a445a-f517-4aaa-9092-2a48565226b4.cpp:8: b74a445a-f517-4aaa-9092-2a48565226b4.cpp: In function ‘int main()’: b74a445a-f517-4aaa-9092-2a48565226b4.cpp:10:26: error: ‘next_smallest’ cannot be  used as a function    10 |     assert (next_smallest({-35, 34, 12, -45}) == -35);       |             ~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~

--- CPP_90.cpp
+++ CPP_90.cpp
@@ -5,7 +5,7 @@
 int next_smallest(std::vector<int> lst){
     std::vector<int> v = lst;
     std::sort(v.begin(), v.end());
-    if(v.size() < 2) return -1; // or any other value that represents None
+    if(v.size() < 2) return -1; 
     return v[1];
 }
 
