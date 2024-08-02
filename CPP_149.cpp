2c58281235497c7f793a9365ac16b2cf17997e98
beep boop <githubbot@vadim.me>
2024-08-02 03:34:07 +0200

    Solution 56 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt: 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:30: 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp: In function ‘int main()’: 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:40:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    40 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~ 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:9:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     9 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:31:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    31 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -6,10 +6,6 @@
 
 using namespace std;
 
-bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
-}
-
 vector<string> sorted_list_sum(const vector<string>& lst) {
     vector<string> modified_lst = lst;
     modified_lst.erase(remove_if(modified_lst.begin(), modified_lst.end(), [](const string& s){return s.length() % 2 != 0;}), modified_lst.end());
@@ -23,6 +19,6 @@ vector<string> sorted_list_sum(const vector<string>& lst) {
 }
 
 int main() {
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), vector<string>{"cc", "dd", "aaaa", "bbbb"}));
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
 }
