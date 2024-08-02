ccf2a4df17b788b296ffd2db6c0907ec906b071c
beep boop <githubbot@vadim.me>
2024-08-02 05:00:52 +0200

    Solution 26 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ox6ckpzy Prompt: b91b812d-72f7-432f-90dd-3b2407a17db7.cpp:45:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    45 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ b91b812d-72f7-432f-90dd-3b2407a17db7.cpp:30:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    30 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ b91b812d-72f7-432f-90dd-3b2407a17db7.cpp:53:5: error: redefinition of ‘int main( )’    53 | int main(){       |     ^~~~ b91b812d-72f7-432f-90dd-3b2407a17db7.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,15 +1,17 @@
 #include <vector>
+#include <cassert>
+#include <climits>
 
 using namespace std;
 
-vector<int> largest_smallest_integers(vector<int> lst){
+vector<int> largest_smallest_integers(vector<int> lst) {
     int largest_negative = INT_MIN, smallest_positive = INT_MAX;
     
     for (int num : lst) {
         if (num < 0 && num > largest_negative) {
             largest_negative = num;
         }
-        if (num > 0 && num < smallest_positive) {
+        if (num > 0 && (num < smallest_positive)) {
             smallest_positive = num;
         }
     }
@@ -17,7 +19,7 @@ vector<int> largest_smallest_integers(vector<int> lst){
     return {largest_negative, smallest_positive};
 }
 
-bool issame(vector<int> a, vector<int> b){
+bool issame(vector<int> a, vector<int> b) {
     if(a.size() != b.size()){
         return false;
     }
@@ -31,7 +33,7 @@ bool issame(vector<int> a, vector<int> b){
     return true;
 }
 
-int main(){
+int main() {
     assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
     return 0;
 }
