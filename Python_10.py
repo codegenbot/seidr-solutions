22b55742205510bf96e9be0b390046fda8e43912
beep boop <githubbot@vadim.me>
2024-08-02 11:23:26 +0200

    Solution 29 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/odummyid Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dd897db-2c3b-4ab8-a88a-ae6f1bb96 43d.py", line 24, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dd897db-2c3b-4ab8-a88a-ae6f1bb96 43d.py", line 21, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,16 +1,12 @@
 def make_palindrome(string: str) -> str:
     if string.islower():
         for i in range(len(string)):
-            lowercase_postfix = postfix.lower()
-            if is_palindrome(lowercase_postfix):
-                prefix_chars = [char.upper() if char.isalpha() and char.isupper() else char for char in string[:i]]
-                return "".join(prefix_chars) + "#" * (len(string) - len(postfix)) + lowercase_postfix[::-1].capitalize()
+            lowercase_postfix = postfix = string[i:]
+            if is_palindrome(postfix):
+                return string + "#" * (len(string) - len(postfix)) + postfix[::-1]
     else:
         for i in range(len(string)):
-            lowercase_postfix = string[i:].lower()
-            if is_palindrome(lowercase_postfix):
-                return (
-                    string[0].upper() 
-                    + "#" * (len(string) - len(postfix))
-                    + postfix[::-1].capitalize()
-                )
+            postfix = string[i:].lower()
+            if is_palindrome(postfix):
+                prefix_chars = [char.upper() if char.isalpha() and char.isupper() else char for char in string[:i]]
+                return "".join(prefix_chars) + "#" * (len(string) - len(postfix)) + lowercase_postfix[::-1].capitalize()
