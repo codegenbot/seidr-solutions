64bf4ef05559611e86e9eb0782e04c4e6f802d37
beep boop <githubbot@vadim.me>
2024-08-02 18:08:12 +0200

    Solution 2 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/ktaq9ax4 Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,34 +1,87 @@
-#include<stdio.h>
-#include<math.h>
-#include<vector>
-#include<string>
-using namespace std;
-#include<algorithm>
-#include<stdlib.h>
+#include <string>
+#include <vector>
 
-int do_algebra(vector<string> operato, vector<int> operand){
-    string expression = "";
-    for(int i = 0; i < operato.size(); i++){
+using namespace std;
+
+int do_algebra(vector<string> operator_, vector<int> operand) {
+    string expression;
+    for (int i = 0; i < operator_.size(); ++i) {
         expression += to_string(operand[i]);
-        expression += operato[i];
+        expression += " ";
+        expression += operator_[i];
+        if (i != operator_.size() - 1) {
+            expression += " ";
+        }
     }
-    expression += to_string(operand[operato.size()]);
+    expression += to_string(operand.back());
     
     int result = eval(expression.c_str());
+    
     return result;
 }
 
-int eval(char* str){
-    int result = 0;
-    char *p = strtok(str, " +*-//**");
-    while(p != NULL){
-        if(*p == '+') result += atoi(p+1);
-        else if(*p == '-') result -= atoi(p+1);
-        else if(*p == '*') result *= atoi(p+1);
-        else if(*p == '/') result /= atoi(p+1);
-        else if(*p[0] == 'd' && *p[1] == '/') result = result / atoi(p+2);
-        else if(strncmp(p, "**", 2) == 0) result = pow(result, atoi(p+2));
-        p = strtok(NULL, " +*-//**");
+int eval(char* s) {
+    char *p = NULL;
+    long l = strtoll(s, &p, 10);
+    if (*p == '\0') {
+        return l;
     }
-    return result;
+    else {
+        // This is a recursive function
+        int i = 0;
+        while (i < strlen(s) && isspace((unsigned char)s[i])) i++;
+        if (s[i] == 'E' || s[i] == 'e') {
+            // Exponentiation
+            int base = eval(p);
+            p += strlen("e");
+            long exponent = strtoll(p, &p, 10);
+            return pow(base, exponent);
+        }
+        else if (s[i] == '*') {
+            // Multiplication
+            int a = eval(p);
+            p++;
+            i--;
+            while (*++p && isspace((unsigned char)*p));
+            long b = strtoll(p, &p, 10);
+            return a * b;
+        }
+        else if (s[i] == '+') {
+            // Addition
+            int a = eval(p);
+            p += strlen("+");
+            i--;
+            while (*++p && isspace((unsigned char)*p));
+            long b = strtoll(p, &p, 10);
+            return a + b;
+        }
+        else if (s[i] == '-') {
+            // Subtraction
+            int a = eval(p);
+            p += strlen("-");
+            i--;
+            while (*++p && isspace((unsigned char)*p));
+            long b = strtoll(p, &p, 10);
+            return a - b;
+        }
+        else if (s[i] == '/') {
+            // Division
+            int a = eval(p);
+            p += strlen("/");
+            i--;
+            while (*++p && isspace((unsigned char)*p));
+            long b = strtoll(p, &p, 10);
+            return a / b;
+        }
+        else if (s[i] == '//') {
+            // Floor division
+            int a = eval(p);
+            p += strlen("//");
+            i--;
+            while (*++p && isspace((unsigned char)*p));
+            long b = strtoll(p, &p, 10);
+            return a / b;
+        }
+    }
+    return l;
 }
