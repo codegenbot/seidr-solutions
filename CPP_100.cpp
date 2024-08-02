fbddcca36af4ca3cae0baffe246c4f7e80bb4636
beep boop <githubbot@vadim.me>
2024-08-02 02:16:35 +0200

    Solution 26 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/qzhe140f Prompt:       |                           ^~~~~~ f096b1c9-fe37-441e-89c2-3f28e367588a.cpp:31:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f096b1c9-fe37-441e-89c2-3f28e367588a.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f096b1c9-fe37-441e-89c2-3f28e367588a.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f096b1c9-fe37-441e-89c2-3f28e367588a.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f096b1c9-fe37-441e-89c2-3f28e367588a.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ f096b1c9-fe37-441e-89c2-3f28e367588a.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -2,11 +2,15 @@
 #include <vector>
 #include <cassert>
 
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
 std::vector<int> make_a_pile(int n) {
     std::vector<int> stones;
     stones.push_back(n);
-    for (int i = 1; i < n; ++i) {
-        if (n % 2 == 0) {
+    for(int i = 1; i < n; ++i) {
+        if(n % 2 == 0) {
             n += 2;
         } else {
             n += 1;
@@ -16,12 +20,8 @@ std::vector<int> make_a_pile(int n) {
     return stones;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(make_a_pile(8), std::vector<int>{8, 10, 12, 14, 16, 18, 20, 22}));
-
+    assert(issame(make_a_pile(8), std::vector<int>{8, 10, 12, 14, 16, 18, 20, 22});
+    
     return 0;
 }
