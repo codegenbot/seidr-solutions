7b636b0ae2db23c81e59d218a513d985492efad9
beep boop <githubbot@vadim.me>
2024-08-02 20:56:33 +0200

    Solution 4 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/67h06wxg Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,38 +1,17 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = 0;
-    string expression;
-
-    for (int i = 0; i < operator_.size(); i++) {
-        if (i == 0) {
-            for (int j = 0; j < operand.size() - 1; j++)
-                expression += to_string(operand[j]);
-            expression += " ";
-        }
-        expression += operator_[i];
-    }
-
-    for (int i = operand.size() - 1; i >= 1; i--) {
-        if (expression.back() == ' ')
-            expression.pop_back();
-    }
-
-    int temp = eval(expression);
-    return temp;
-}
-
-long long eval(const string& s) {
-    long long res = 0;
-    long long num = 0;
-    char sign = '+';
-    for (char c : s + "+") {
-        if (isdigit(c)) {
-            num = num * 10 + (c - '0');
-        } else {
-            if (sign == '+')res += num;
-            else res -= num;
-            sign = c;
-            num = 0;
+long long do_algebra(vector<string> operator_, vector<int> operand) {
+    long long result = (long long)operand[0];
+    for(int i = 0; i < operator_.size(); i++) {
+        if(operator_[i] == "+") {
+            result += (long long)operand[i+1];
+        } else if(operator_[i] == "-") {
+            result -= (long long)operand[i+1];
+        } else if(operator_[i] == "*") {
+            result *= (long long)operand[i+1];
+        } else if(operator_[i] == "//") {
+            result = result / (long long)operand[i+1];
+        } else if(operator_[i] == "**") {
+            result = pow(result, (long double)operand[i+1]);
         }
     }
-    return res;
+    return result;
 }
