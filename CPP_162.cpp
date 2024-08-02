8a3ad375487befd486546bdd65d379931df8bfcf
beep boop <githubbot@vadim.me>
2024-08-02 04:46:20 +0200

    Solution 31 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/0chgyc65 Prompt: d3498574-fe99-44d1-9492-a7e00b2e1fef.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ d3498574-fe99-44d1-9492-a7e00b2e1fef.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,4 +1,3 @@
 assert(string_to_md5("") == "None");
-assert(string_to_md5("hello") == "5d41402abc4b2a76b9719d911017c592");
-assert(string_to_md5("cpp162") == "a32719d5e6a98110e01ddf7fd73c06dc");
-assert(string_to_md5("software") == "86440ca186a6ea68b3f221d87d6de705");
+assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+assert(string_to_md5("cpp162") == "d82e281b29c296de4c82045a2fa5b5d8");
