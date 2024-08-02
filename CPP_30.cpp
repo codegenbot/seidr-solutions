36f9a3cc0e649fd81855264264cd03a3aa80beeb
beep boop <githubbot@vadim.me>
2024-08-02 21:46:16 +0200

    Solution 84 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/axwclnng Prompt:       |                    ^~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:14:29: error: ‘vector’ was not declared  in this scope    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:13: 2937be51-cb9d-4564-81f1-23499ed624f6.cpp: In function ‘int main()’: 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 2937be51-cb9d-4564-81f1-23499ed624f6.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,14 +1,11 @@
 #include <vector>
-#include <assert.h>
-
-using namespace std;
 
 bool issame(vector<float> a, vector<float> b) {
-    if(a.size() != b.size()) {
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
@@ -16,5 +13,8 @@ bool issame(vector<float> a, vector<float> b) {
 }
 
 int main() {
-    assert(issame(get_positive({}), {})); 
+    using namespace std;
+    vector<float> v1 = get_positive({1.2f, -3.4f, 5.6f});
+    vector<float> v2 = get_positive({1.2f, 5.6f});
+    assert(issame(v1, v2));
 }
