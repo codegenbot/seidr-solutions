e86bbcf1b9999b675ac753133ec33f55f265e5f8
beep boop <githubbot@vadim.me>
2024-08-03 00:54:26 +0200

    Solution 45 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6lgg81t0 Prompt:       |             ^~~~~~ 726fcc8f-a44c-4ba3-bad8-3c14cc847ace.cpp:18:20: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 726fcc8f-a44c-4ba3-bad8-3c14cc847ace.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 726fcc8f-a44c-4ba3-bad8-3c14cc847ace.cpp:18:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 726fcc8f-a44c-4ba3-bad8-3c14cc847ace.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 726fcc8f-a44c-4ba3-bad8-3c14cc847ace.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 726fcc8f-a44c-4ba3-bad8-3c14cc847ace.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -2,14 +2,16 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> res = l;
+vector<int> sort_third(vector<int> l) {
+    vector<int> res = l;
     for (int i = 0; i < l.size(); i += 3) {
-        std::sort(res.begin() + i, res.begin() + i + 3);
+        sort(res.begin() + i, res.begin() + i + 3);
     }
     return res;
 }
