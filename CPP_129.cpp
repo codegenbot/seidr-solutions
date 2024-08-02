a898015a3413cdf0b9526fabaacf67dd740aa733
beep boop <githubbot@vadim.me>
2024-08-03 00:58:09 +0200

    Solution 81 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vmelxnfr Prompt: on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8146a561-01b2-43e2-a9e0-06db7c2500f7.cpp:33:39: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 8146a561-01b2-43e2-a9e0-06db7c2500f7.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 8146a561-01b2-43e2-a9e0-06db7c2500f7.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from 8146a561-01b2-43e2-a9e0-06db7c2500f7.cpp:32: 8146a561-01b2-43e2-a9e0-06db7c2500f7.cpp: In function ‘int main()’: 8146a561-01b2-43e2-a9e0-06db7c2500f7.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -32,7 +32,7 @@ int main() {
         std::cin >> grid[i][0] >> grid[i][1];
     }
 
-    assert(isSame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3});
+    assert(isSame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
     
     return 0;
 }
