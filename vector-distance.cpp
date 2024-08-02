84b676ca8b68af16f669307f3c992289c84937bb
beep boop <githubbot@vadim.me>
2024-08-02 05:33:52 +0200

    Solution 18 of vector-distance, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run1/runs/qxmmde8v Prompt: it must return 405.7089029164789 for input 20 56.093689724077166 45.46353516992539 48.98563333923428 43.0246845491138 47.418719320983996 63.45562040639442 -39.060337447278215 -54.8312968233496 8.772045143842462 96.87961311740477 -84.58894785601608 -6.7482653767510214 -64.61349382027373 72.552843222826 59.43748630642318 65.17460307990615 -11.044046318627807 27.64269693360562 61.60836699107506 34.76114436867317 20 23.41694828879271 -44.515495359013514 -45.71655794120333 -78.18414026168314 26.1008876903543 -86.56726020469333 94.48069498436459 95.91711130027073 -35.89367474847333 42.60506698789547 -80.68139009925521 -32.503107436461946 49.37418927221461 30.82424756719334 23.65554964696244 -65.67150428904202 65.831334989243 -12.241910359258839 -54.42305664510707 -57.636258957046785, but it returns 359.106.

--- vector-distance.cpp
+++ vector-distance.cpp
@@ -6,24 +6,16 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    vector<float> vec1(n);
+    vector<float> vec2(n);
 
-    vector<double> vec1(n);
-    vector<double> vec2(n);
+    for (int i = 0; i < n; ++i) cin >> vec1[i];
+    for (int i = 0; i < n; ++i) cin >> vec2[i];
 
-    for (int i = 0; i < n; ++i) {
-        cin >> vec1[i];
-    }
+    float sum = 0;
+    for (int i = 0; i < n; ++i) sum += pow(vec1[i] - vec2[i], 2);
 
-    for (int i = 0; i < n; ++i) {
-        cin >> vec2[i];
-    }
-
-    double sum = 0;
-    for (int i = 0; i < n; ++i) {
-        sum += pow(vec1[i] - vec2[i], 2);
-    }
-
-    double distance = sqrt(sum);
+    float distance = sqrt(sum);
     cout << distance << endl;
 
     return 0;
