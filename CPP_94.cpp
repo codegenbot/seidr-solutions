53998cb423ee6ada6d0c41b3304a5274442e209b
beep boop <githubbot@vadim.me>
2024-08-02 14:44:06 +0200

    Solution 42 of CPP/94, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/gh2ovwer Prompt: 8048351b-d3eb-4dd5-a507-c12303e1fdf0.cpp:10:18: error: ‘vector’ was not declared  in this scope    10 | int largestPrime(vector<int> lst) {       |                  ^~~~~~ 8048351b-d3eb-4dd5-a507-c12303e1fdf0.cpp:10:25: error: expected primary-expressi on before ‘int’    10 | int largestPrime(vector<int> lst) {       |                         ^~~ In file included from 8048351b-d3eb-4dd5-a507-c12303e1fdf0.cpp:31: 8048351b-d3eb-4dd5-a507-c12303e1fdf0.cpp: In function ‘int main()’: 8048351b-d3eb-4dd5-a507-c12303e1fdf0.cpp:33:13: error: ‘skjkasdkd’ was not decla red in this scope    33 |     assert (skjkasdkd({127, 97, 8192}) == 10);       |             ^~~~~~~~~

--- CPP_94.cpp
+++ CPP_94.cpp
@@ -1,5 +1,6 @@
 ```cpp
 #include <vector>
+
 using namespace std;
 
 int sumOfDigits(int num) {
@@ -14,24 +15,22 @@ int sumOfDigits(int num) {
 int largestPrime(vector<int> lst) {
     int maxPrime = 0;
     for (int i : lst) {
-        if (i > maxPrime)
-            maxPrime = i;
-    }
-    for (int i = maxPrime; i >= 2; i--) {
         bool isPrime = true;
+        if (i <= 1)
+            continue;
         for (int j = 2; j * j <= i; j++) {
             if (i % j == 0) {
                 isPrime = false;
                 break;
             }
         }
-        if (isPrime)
-            return sumOfDigits(i);
+        if (isPrime && i > maxPrime)
+            maxPrime = i;
     }
-    return 0;
+    return sumOfDigits(maxPrime);
 }
 
 int main() {
     vector<int> lst = {127, 97, 8192};
-    cout << largestPrime(lst);
+    cout << largestPrime(lst) << endl;
 }
