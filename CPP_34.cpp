6d28f9c84fe770c9a064dc30f714c1df6d3f152b
beep boop <githubbot@vadim.me>
2024-08-02 21:59:16 +0200

    Solution 60 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wjce3cw5 Prompt:    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c31aa885-602e-4a0d-ac73-fcf431bde5f2.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c31aa885-602e-4a0d-ac73-fcf431bde5f2.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c31aa885-602e-4a0d-ac73-fcf431bde5f2.cpp:20: c31aa885-602e-4a0d-ac73-fcf431bde5f2.cpp: In function ‘int main()’: c31aa885-602e-4a0d-ac73-fcf431bde5f2.cpp:30:20: error: ‘unique’ was not declared  in this scope    30 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ c31aa885-602e-4a0d-ac73-fcf431bde5f2.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -2,37 +2,48 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
+    }
+    return true;
 }
 
-std::vector<int> unique(std::vector<int> l) {
-    std::vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
+vector<int> unique(vector<int> l){
+    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
     sort(result.begin(), result.end());
     return vector<int>(unique_copy(result.begin(), result.end()));
 }
 
-std::vector<int>::iterator unique_copy(std::vector<int>::iterator start, std::vector<int>::iterator end) {
+vector<int>::iterator unique_copy(vector<int>::iterator start, vector<int>::iterator end) {
     if (start == end)
         return end;
  
     std::vector<int> temp;
     for (; start != end; ++start) {
-        bool found = false;
-        for (auto it = temp.begin(); it != temp.end(); ++it) {
-            if (*it == *start) {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
+        if (std::find_if(temp.begin(), temp.end(),
+                [&](int x) { return x == *start; }) == temp.end())
             temp.push_back(*start);
     }
     return temp.begin();
 }
 
 int main() {
-    assert(issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));
-    std::cout << "Success";
+    vector<int> input;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        input.push_back(num);
+    }
+
+    if(issame(input, unique(input))) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
