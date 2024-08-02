9f27fd4f1d9e480d7946b36ad198314f051f3566
beep boop <githubbot@vadim.me>
2024-08-02 10:20:49 +0200

    Solution 44 of Python/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/xcimrx63 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2ab29e83-abae-4e8f-a0e9-f256647a5 9c8.py", line 4     return True     ^^^^^^^^^^^ SyntaxError: 'return' outside function

--- Python_0.py
+++ Python_0.py
@@ -1,11 +1,6 @@
-def is_abs_difference_below_threshold(numbers, threshold):
+def check_numbers(numbers, threshold):
     numbers.sort()
     for i in range(len(numbers) - 1):
         if abs(numbers[i] - numbers[i + 1]) < threshold:
             return True
     return False
-
-numbers = [4, 2, 6, 8, 5]
-threshold = 2
-result = is_abs_difference_below_threshold(numbers, threshold)
-print(result)
