0cf6612bdee514ffaa867bed9b8d8be33a98d2d3
beep boop <githubbot@vadim.me>
2024-08-03 01:13:14 +0200

    Solution 0 of CPP/90, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/773t944g Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_90.cpp
@@ -0,0 +1,13 @@
+Here is the completed code:
+
+int next_smallest(vector<int> lst) {
+    vector<int> v(lst);
+    if(v.size() < 2)
+        return -1; // Return None
+    sort(v.begin(), v.end());
+    for(int i = 0; i < v.size()-1; i++) {
+        if(v[i] != v[i+1])
+            return v[i+1];
+    }
+    return -1; // Return None
+}
