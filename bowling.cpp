65ccb3495d3e835275b8f096b594db908bcc6e08
beep boop <githubbot@vadim.me>
2024-08-02 23:24:46 +0200

    Solution 14 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/cyefu9vh Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,19 +1,46 @@
-int bowlingScore(string s) {
+int bowlingScore(string input) {
     int score = 0;
-    int currentRolls = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == '/') {
-            if (currentRolls > 0) {
-                score += max(currentRolls, 10 - currentRolls);
+    int roll1, roll2;
+    for(int i = 0; i < 10; i++) {
+        if(input[i] == 'X') { // strike
+            score += 10 + bowlingScoreHelper(&input[i+1]);
+        } else if(input[i] != '/') { // spare
+            roll1 = input[i] - '0';
+            if(i < 8) {
+                if(input[i+2] == 'X' || (input[i+1] == '/')) { // next roll is strike or part of a spare
+                    score += roll1 + 10;
+                } else {
+                    roll2 = input[i+1] - '0';
+                    score += roll1 + roll2;
+                }
+            } else {
+                score += roll1 + 10;
             }
-            currentRolls = 0;
-        } else if (isdigit(s[i])) {
-            currentRolls *= 10;
-            currentRolls += s[i] - '0';
+        } else { // regular rolls
+            roll1 = input[i-1] - '0';
+            if(i < 8) {
+                roll2 = input[i+1] - '0';
+                score += roll1 + roll2;
+            } else {
+                score += roll1 + 10;
+            }
         }
     }
-    if (currentRolls > 0) {
-        score += max(currentRolls, 10 - currentRolls);
-    }
     return score;
+}
+
+int bowlingScoreHelper(string* input) {
+    int score = 0;
+    for(int i = 0; i < 2 && i < (*input).size(); i++) {
+        if((*input)[i] == 'X') { // strike
+            score += 10 + bowlingScoreHelper(&(*input)[i+1]);
+            return score;
+        } else if((*input)[i] != '/') { // spare
+            int roll1 = (*input)[i] - '0';
+            int roll2 = (*input)[i+1] - '0';
+            score += roll1 + roll2;
+            return score;
+        }
+    }
+    return 0;
 }
