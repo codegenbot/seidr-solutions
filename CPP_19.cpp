7f8706aefbfd93d264eee8a2379c4e1f8e0d09ea
beep boop <githubbot@vadim.me>
2024-08-02 13:40:52 +0200

    Solution 5 of CPP/19, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/z6a971b8 Prompt: Complete the following code given the task description and function signature.

--- CPP_19.cpp
+++ CPP_19.cpp
@@ -1,28 +1,42 @@
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
 string sort_numbers(string numbers) {
-    map<string, int> numMap = {{"zero", 0}, {"one", 1}, {"two", 2}, {"three", 3},
-                                {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7},
-                                {"eight", 8}, {"nine", 9}};
-    vector<string> numVec;
-    string temp;
+    map<string, int> numberMap = {{"zero", 0}, {"one", 1}, {"two", 2},
+                                   {"three", 3}, {"four", 4}, {"five", 5},
+                                   {"six", 6}, {"seven", 7}, {"eight", 8},
+                                   {"nine", 9}};
+
+    vector<string> numberVector;
+    string num;
 
     for (int i = 0; i < numbers.length(); i++) {
         if (numbers[i] == ' ') {
             continue;
         }
-        temp += numbers[i];
-        for (int j = i + 1; j <= numbers.length() && numbers[j] != ' '; j++) {
-            i = j - 1;
-            break;
+
+        for (int j = i + 1; j <= numbers.length(); j++) {
+            if (numbers[j] != ' ') {
+                num += numbers[j];
+            } else {
+                break;
+            }
         }
-        numVec.push_back(temp);
-        temp.clear();
+
+        numberVector.push_back(num);
+        num = "";
     }
 
-    sort(numVec.begin(), numVec.end());
+    sort(numberVector.begin(), numberVector.end(),
+         [&numberMap](string a, string b) {
+             return numberMap.at(a) < numberMap.at(b);
+         });
 
     string result;
-    for (auto str : numVec) {
-        result += str + " ";
+    for (auto &num : numberVector) {
+        result += num + " ";
     }
     return result.substr(0, result.length() - 1);
 }
