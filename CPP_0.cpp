12a5646a7e2b28b2f508a0119e2791df358653cd
beep boop <githubbot@vadim.me>
2024-08-02 10:16:24 +0200

    Solution 26 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/spza3sn6 Prompt: 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp:1:48: error: expected primary-expressio n before ‘float’     1 | bool has_close_elements(vector<float> numbers, float threshold) {       |                                                ^~~~~ 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp:1:63: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool has_close_elements(vector<float> numbers, float threshold) {       |                                                               ^ 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp: In function ‘int main()’: 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp:14:5: error: ‘vector’ was not declared in this scope    14 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp:14:12: error: expected primary-expressi on before ‘float’    14 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~ In file included from 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp:12: 068f1956-f6da-4034-ae0c-66dbbe9fceb4.cpp:15:31: error: ‘has_close_elements’ cann ot be used as a function    15 |     assert (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5) == false) ;       |             ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -17,9 +17,9 @@ bool has_close_elements(std::vector<float> numbers, float threshold) {
 int main() {
     std::vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};
     if (has_close_elements({1.1, 2.2, 3.1, 4.1, 5.1}, 0.5)) {
-        std::cout << "Array contains close elements." << std::endl;
+        std::cout << "The array contains close elements." << std::endl;
     } else {
-        std::cout << "Array does not contain close elements." << std::endl;
+        std::cout << "The array does not contain close elements." << std::endl;
     }
     return 0;
 }
