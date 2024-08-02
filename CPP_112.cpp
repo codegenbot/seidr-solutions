fa74a7576e7de13820c1f2efa7d817b041ec0b3d
beep boop <githubbot@vadim.me>
2024-08-02 03:23:06 +0200

    Solution 24 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/aza6mbvx Prompt:  in this scope    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp:18:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp:17: ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp: In function ‘int main()’: ea1ba225-998b-40f9-821d-e5171bc04fb0.cpp:27:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    27 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,13 +3,8 @@
 #include <algorithm>
 
 std::pair<std::string, bool> issame(std::vector<std::string> a, std::vector<std::string> b) {
-    std::string result = "";
-    for(char ch : result){
-        if(result.find(ch) == std::string::npos){
-            result += ch;
-        }
-    }
-    std::string result_reverse = result;
-    std::reverse(result_reverse.begin(), result_reverse.end());
-    return std::make_pair(result, result == result_reverse);
+    std::string combined_strings = a[0] + b[0];
+    std::string reversed_combined = combined_strings;
+    std::reverse(reversed_combined.begin(), reversed_combined.end());
+    return std::make_pair(reversed_combined, combined_strings == reversed_combined);
 }
