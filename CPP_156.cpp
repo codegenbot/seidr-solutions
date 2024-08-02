0c2968eb3c78f9280db66f9f76368410510308bd
beep boop <githubbot@vadim.me>
2024-08-02 03:13:58 +0200

    Solution 29 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/we5a8kom Prompt: 1dee8332-e958-4a14-a47f-329c0668a4a9.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 1dee8332-e958-4a14-a47f-329c0668a4a9.cpp:18: 1dee8332-e958-4a14-a47f-329c0668a4a9.cpp: In function ‘int main()’: 1dee8332-e958-4a14-a47f-329c0668a4a9.cpp:20:13: error: ‘int_to_mini_romank’ was not declared in this scope    20 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,10 +1,10 @@
 std::string intToMiniRoman(int number) {
-    vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
+    vector<pair<int, std::string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                             {400, "CD"}, {100, "C"}, {90, "XC"},
                                             {50, "L"}, {40, "XL"}, {10, "X"},
                                             {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
 
-    string roman = "";
+    std::string roman = "";
     for (const auto& pair : romanMap) {
         while (number >= pair.first) {
             number -= pair.first;
