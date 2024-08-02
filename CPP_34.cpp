a099e43c5f28b8fcc54088ce334702e1e58b3400
beep boop <githubbot@vadim.me>
2024-08-02 14:34:25 +0200

    Solution 6 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f7cizk1g Prompt: Complete the following code given the task description and function signature.

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,19 +1,8 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <set>
 using namespace std;
 
 vector<int> unique(vector<int> l) {
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end(), result.begin()));
-    sort(result.begin(), result.end());
+    set<int> s(l.begin(), l.end());
+    vector<int> result(s.begin(), s.end());
     return result;
-}
-
-int main() {
-    vector<int> v = {5, 3, 5, 2, 3, 3, 9, 0, 123};
-    for (int x : unique(v)) {
-        cout << x << " ";
-    }
-    return 0;
 }
