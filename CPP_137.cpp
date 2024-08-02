628aa549cc9575f61d0c8d841ad230d7cbfd25a7
beep boop <githubbot@vadim.me>
2024-08-02 17:09:34 +0200

    Solution 78 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/veoq9x4w Prompt: 82f165d8-a76f-4adb-9358-67d21ee48c75.cpp:2:10: fatal error: boost/any.hpp: No su ch file or directory     2 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <string>
 #include <boost/any.hpp>
 
@@ -13,33 +14,10 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
-        string str = (string)b.convert_to<string>();
-        float f = (float)a.convert_to<float>();
-        if (str > to_string(f))
-            return b;
-        else if (str < to_string(f))
-            return a;
-        else
-            return boost::any("None");
-    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
-        string str = (string)a.convert_to<string>();
-        int i = (int)b.convert_to<int>();
-        if (str > to_string(i))
-            return a;
-        else if (str < to_string(i))
-            return boost::any(b);
-        else
-            return boost::any("None");
-    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
-        string str = (string)a.convert_to<string>();
-        float f = (float)b.convert_to<float>();
-        if (str > to_string(f))
-            return a;
-        else if (str < to_string(f))
-            return boost::any(b);
-        else
-            return boost::any("None");
+    } else if (a.type() == typeid(int) && b.type() == typeid(int)) {
+        return max((int)a.convert_to<int>(), (int)b.convert_to<int>());
+    } else if (a.type() == typeid(float) && b.type() == typeid(float)) {
+        return max((float)a.convert_to<float>(), (float)b.convert_to<float>());
     } else if (a.type() == typeid(string) && b.type() == typeid(string)) {
         string str1 = (string)a.convert_to<string>(), str2 = (string)b.convert_to<string>();
         if (str1 > str2)
@@ -48,22 +26,34 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         else
             return boost::any("None");
-    } else if (a.type() == typeid(float) && b.type() == typeid(int)) {
-        float f = (float)a.convert_to<float>(), i = (int)b.convert_to<int>();
-        if (f > i)
+    } else if (a.type() == typeid(int) && b.type() == typeid(string)) {
+        string str = (string)b.convert_to<string>();
+        int i = (int)a.convert_to<int>();
+        if (to_string(i) > str)
             return a;
-        else if (f < i)
-            return boost::any(b);
         else
-            return boost::any("None");
-    } else if (a.type() == typeid(int) && b.type() == typeid(float)) {
-        int i = (int)a.convert_to<int>(), f = (float)b.convert_to<float>();
-        if (i > f)
+            return boost::any(b);
+    } else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        string str = (string)b.convert_to<string>();
+        float f = (float)a.convert_to<float>();
+        if (to_string(f) > str)
             return a;
-        else if (i < f)
-            return boost::any(b);
         else
-            return boost::any("None");
+            return boost::any(b);
+    } else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        string str = (string)a.convert_to<string>();
+        int i = (int)b.convert_to<int>();
+        if (str > to_string(i))
+            return a;
+        else
+            return boost::any(b);
+    } else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        string str = (string)a.convert_to<string>();
+        float f = (float)b.convert_to<float>();
+        if (str > to_string(f))
+            return a;
+        else
+            return boost::any(b);
     }
     return a; // default to the first value
 }
