0bd754a77b79f230f16815dd925c9bfce5daca9c
beep boop <githubbot@vadim.me>
2024-08-03 00:56:15 +0200

    Solution 16 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7admhmgi Prompt:    28 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2b44064f-3830-4090-a01e-9683cf81f74e.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 2b44064f-3830-4090-a01e-9683cf81f74e.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~ In file included from 2b44064f-3830-4090-a01e-9683cf81f74e.cpp:27: 2b44064f-3830-4090-a01e-9683cf81f74e.cpp: In function ‘int main()’: 2b44064f-3830-4090-a01e-9683cf81f74e.cpp:37:20: error: ‘sort_even’ was not decla red in this scope    37 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 2b44064f-3830-4090-a01e-9683cf81f74e.cpp:37:19: error: ‘issame’ cannot be used a s a function    37 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,5 +1,16 @@
-bool issame(vector<float> a, vector<float> b);
 vector<float> sort_even(vector<float> l);
+
+bool compare_vectors(vector<float> a, vector<float> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 vector<float> sort_even(vector<float> l) {
     vector<float> even_indices;
@@ -20,9 +31,5 @@ vector<float> sort_even(vector<float> l) {
 }
 
 int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
-}
-
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
+    assert(compare_vectors(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 }
