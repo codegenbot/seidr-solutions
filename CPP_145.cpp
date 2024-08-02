c0837021ea6e359a746946b5be1048f8ee1068f2
beep boop <githubbot@vadim.me>
2024-08-02 03:13:57 +0200

    Solution 16 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/1t0fd5to Prompt: In file included from c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp:33: c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp: In function ‘int main()’: c1fc17e6-0bf3-4a0a-b471-7a3168683b6f.cpp:43:20: error: ‘order_by_points’ was not  declared in this scope    43 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -30,6 +30,6 @@ vector<int> order_by_points(vector<int> nums) {
 }
 
 int main() {
-    assert(issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
