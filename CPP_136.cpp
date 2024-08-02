b44e230b963a53a8b90cf1b9feb93c1f94d2045d
beep boop <githubbot@vadim.me>
2024-08-03 01:11:37 +0200

    Solution 30 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/stlccgm9 Prompt:       |             ^~~~~~ 1ef5f064-7bb9-481f-924f-6b351ed773ab.cpp:20:20: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 1ef5f064-7bb9-481f-924f-6b351ed773ab.cpp:20:27: error: ‘vector’ was not declared  in this scope    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 1ef5f064-7bb9-481f-924f-6b351ed773ab.cpp:20:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 1ef5f064-7bb9-481f-924f-6b351ed773ab.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 1ef5f064-7bb9-481f-924f-6b351ed773ab.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 1ef5f064-7bb9-481f-924f-6b351ed773ab.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -12,6 +12,6 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst){
     return {largest_negative, smallest_positive};
 }
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<int> a std::vector<int> b){
     return (largest_smallest_integers(a) == largest_smallest_integers(b));
 }
