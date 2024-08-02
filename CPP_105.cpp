701fda1112da0098504afd1451cc1cd7cde4bfe8
beep boop <githubbot@vadim.me>
2024-08-02 15:31:22 +0200

    Solution 70 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hm2e0mj7 Prompt:       |                                 ^~~~~~ In file included from e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp:47:44: error: ‘b’ was not declared in t his scope    47 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp:47:45: error: expression list treated a s compound expression in initializer [-fpermissive]    47 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp:46: e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp: In function ‘int main()’: e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp:56:20: error: ‘by_length’ was not decla red in this scope    56 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ e945fba4-8dfb-4ec3-ad0a-4fc5d058fcd2.cpp:56:19: error: ‘issame’ cannot be used a s a function    56 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,10 +1,75 @@
-vector<string> by_length(vector<int> arr){
-    vector<string> temp;
-    for(int i: arr) {
-        if(i >= 1 && i <= 9)
-            temp.push_back(to_string(i));
+```c++
+#include <vector>
+#include <string>
+
+bool issame(vector<string> a, vector<string>b) {
+    if (a == b)
+        return true;
+    else
+        return false;
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+
+    std::vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        std::cout << "Enter element " << (i+1) << ": ";
+        std::cin >> arr[i];
+    }
+
+    vector<string> result = by_length(arr);
+
+    if (issame(result, {"Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"})) {
+        cout << "The output is correct." << endl;
+    } else {
+        cout << "The output is incorrect." << endl;
+    }
+
+    return 0;
+}
+
+vector<string> by_length(vector<int> arr) {
+    vector<int> temp;
+    for (int i : arr) {
+        if (i >= 1 && i <= 9)
+            temp.push_back(i);
     }
     sort(temp.begin(), temp.end());
     reverse(temp.begin(), temp.end());
-    return temp;
+    vector<string> result;
+    for (int i : temp) {
+        switch (i) {
+            case 1:
+                result.push_back("One");
+                break;
+            case 2:
+                result.push_back("Two");
+                break;
+            case 3:
+                result.push_back("Three");
+                break;
+            case 4:
+                result.push_back("Four");
+                break;
+            case 5:
+                result.push_back("Five");
+                break;
+            case 6:
+                result.push_back("Six");
+                break;
+            case 7:
+                result.push_back("Seven");
+                break;
+            case 8:
+                result.push_back("Eight");
+                break;
+            case 9:
+                result.push_back("Nine");
+                break;
+        }
+    }
+    return result;
 }
