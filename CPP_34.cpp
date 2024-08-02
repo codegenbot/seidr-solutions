6b8ffcfd193d90e46954d579a9bfa8ebb44cb7c5
beep boop <githubbot@vadim.me>
2024-08-02 22:00:36 +0200

    Solution 70 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/wjce3cw5 Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6b6c0234-53ce-48c2-bd58-50854211173b.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6b6c0234-53ce-48c2-bd58-50854211173b.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6b6c0234-53ce-48c2-bd58-50854211173b.cpp:9: 6b6c0234-53ce-48c2-bd58-50854211173b.cpp: In function ‘int main()’: 6b6c0234-53ce-48c2-bd58-50854211173b.cpp:19:20: error: ‘unique’ was not declared  in this scope    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 6b6c0234-53ce-48c2-bd58-50854211173b.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,29 +1,14 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
-    }
-    return true;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
 
-vector<int> unique(vector<int> l){
-    vector<int> result(l.begin(), unique_copy(l.begin(), l.end()));
-    sort(result.begin(), result.end());
-    return vector<int>(unique_copy(result.begin(), result.end()));
-}
-
-vector<int>::iterator unique_copy(vector<int>::iterator start, vector<int>::iterator end) {
-    if (start == end)
-        return end;
- 
-    std::vector<int> temp;
-    for (; start != end; ++start) {
-        if (std::find_if(temp.begin(), temp.end(),
-                [&](int x) { return x == *start; }) == temp.end())
-            temp.push_back(*start);
-    }
-    return temp.begin();
+std::vector<int> unique(std::vector<int> l) {
+    std::vector<int> result(l.begin(),l.end());
+    std::sort(result.begin(),result.end());
+    auto it = std::unique(result.begin(),result.end());
+    result.erase(it,result.end());
+    return result;
 }
