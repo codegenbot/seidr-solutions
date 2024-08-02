5165630197ae0a9cc6bfc8974848c5d0e8cfa550
beep boop <githubbot@vadim.me>
2024-08-02 12:55:16 +0200

    Solution 21 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/61uzu1wk Prompt:    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0d067a21-db8e-48c2-87f0-31af95d31300.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ 0d067a21-db8e-48c2-87f0-31af95d31300.cpp:32:5: note: ‘int main()’ previously def ined here    32 | int main() {       |     ^~~~ In file included from 0d067a21-db8e-48c2-87f0-31af95d31300.cpp:37: 0d067a21-db8e-48c2-87f0-31af95d31300.cpp: In function ‘int main()’: 0d067a21-db8e-48c2-87f0-31af95d31300.cpp:47:20: error: ‘separate_paren_groups’ w as not declared in this scope    47 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 0d067a21-db8e-48c2-87f0-31af95d31300.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -4,3 +4,35 @@
 bool assert_same(vector<string> a, vector<string> b) {
     return a == b;
 }
+
+vector<string> separate_paren_groups(string paren_string);
+
+vector<string> separate_paren_groups(string paren_string) {
+    vector<string> result;
+    string current_group;
+    int open_braces = 0;
+
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (open_braces > 0) {
+                current_group += c;
+            }
+            open_braces++;
+        } else if (c == ')') {
+            open_braces--;
+            if (open_braces > 0) {
+                current_group += c;
+            } else if (open_braces == 0) {
+                result.push_back(current_group);
+                current_group = "";
+            }
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    assert(assert_same(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
+    return 0;
+}
