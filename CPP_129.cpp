515f63c661326c370880bba2853926e832b2bac9
beep boop <githubbot@vadim.me>
2024-08-02 16:29:26 +0200

    Solution 2 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/8ve3nby1 Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,29 +1,31 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> minPath(vector<vector<int>>& grid, int k) {
-    vector<int> res;
-    for (int i = 0; i < k; i++) {
-        int maxVal = 1;
-        for (int j = 0; j < grid.size(); j++) {
-            for (int col = 0; col < grid[j].size(); col++) {
-                if (grid[j][col] > maxVal) {
-                    maxVal = grid[j][col];
-                    res.push_back(maxVal);
-                }
+vector<int> minPath(vector<vector<int>> grid, int k) {
+    int n = grid.size();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 && j == 0) {
+                dp[i][j] = grid[i][j];
+            } else if (i > 0) {
+                dp[i][j] = min({dp[i-1][j], dp[i][j-1]});
+            } else {
+                dp[i][j] = min({dp[i][j-1]});
             }
         }
     }
-    return res;
-}
-
-int main() {
-    vector<vector<int>> grid = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int k = 3;
-    vector<int> result = minPath(grid, k);
-    for (auto val : result) {
-        cout << val << " ";
+    
+    int res[2*k+1]; res[0] = 1;
+    for (int i = 1; i <= k; i++) {
+        if (i < k) {
+            if (grid[0][i-1] > grid[0][k-i]) {
+                res[i] = grid[0][k-i];
+            } else {
+                res[i] = grid[0][i-1];
+            }
+        } else {
+            res[i] = grid[0][k-i];
+        }
     }
-    return 0;
+    
+    return vector<int>(res, res+2*k);
 }
