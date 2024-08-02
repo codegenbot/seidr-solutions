62e22fc37c822139fef07c04911ebaee0edbcc2b
beep boop <githubbot@vadim.me>
2024-08-03 00:07:24 +0200

    Solution 70 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/uyyc036y Prompt:  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d315493c-0300-40b6-aa3d-a61d474572c1.cpp:19:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from d315493c-0300-40b6-aa3d-a61d474572c1.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d315493c-0300-40b6-aa3d-a61d474572c1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d315493c-0300-40b6-aa3d-a61d474572c1.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d315493c-0300-40b6-aa3d-a61d474572c1.cpp:18: d315493c-0300-40b6-aa3d-a61d474572c1.cpp: In function ‘int main()’: d315493c-0300-40b6-aa3d-a61d474572c1.cpp:28:20: error: ‘get_odd_collatz’ was not  declared in this scope    28 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -14,9 +14,3 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     
     return true;
 }
-
-int main() {
-    assert(issame(std::vector<int>({1}), std::vector<int>({1})));
-    
-    return 0;
-}
