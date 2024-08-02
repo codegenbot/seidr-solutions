7400ae229b47866bf53ae639e5f94b0edc5e9be2
beep boop <githubbot@vadim.me>
2024-08-02 03:55:37 +0200

    Solution 13 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fndn25hh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b335712-dadb-460c-b55e-cd384f9a2 2ea.py", line 21, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b335712-dadb-460c-b55e-cd384f9a2 2ea.py", line 17, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,10 @@
 def encode(message):
-    encoded_message = ""
-    vowels = "AEIOUaeiou"
+    encoded_message = ''
+    vowels = 'AEIOUaeiou'
     for char in message:
         if char.isalpha():
             if char.upper() in vowels:
-                encoded_message += chr(
-                    (ord(char) + 17 - ord("A")) % 26 + ord("A")
-                )  # Changed +2 to +17
+                encoded_message += chr((ord(char) + 17 - ord('A')) % 26 + ord('A'))  # Changed +2 to +17
             else:
                 encoded_message += char.swapcase()
         else:
