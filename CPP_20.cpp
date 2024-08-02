7e2117b9477054a8c8797fc8f6aa785a83fe16b2
beep boop <githubbot@vadim.me>
2024-08-02 10:58:12 +0200

    Solution 40 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/za3m6z0n Prompt:    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 33bb3b77-2d64-4c97-935d-ab643450fc35.cpp:21:36: error: expected primary-expressi on before ‘float’    21 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 33bb3b77-2d64-4c97-935d-ab643450fc35.cpp:21:43: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 33bb3b77-2d64-4c97-935d-ab643450fc35.cpp:20: 33bb3b77-2d64-4c97-935d-ab643450fc35.cpp: In function ‘int main()’: 33bb3b77-2d64-4c97-935d-ab643450fc35.cpp:30:20: error: ‘find_closest_elements’ w as not declared in this scope    30 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 33bb3b77-2d64-4c97-935d-ab643450fc35.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,35 +1,20 @@
 #include <algorithm>
-#include <numeric>
 #include <vector>
 #include <limits>
 
-using namespace std;
-
-bool issame(vector<float> a, vector<float> b) {
-    return a == b;
-}
-
-vector<pair<float, float>> find_closest_elements(vector<float> numbers) {
-    pair<float, float> closest;
-    float min_diff = numeric_limits<float>::max();
+std::vector<std::pair<float, float>> find_closest_elements(std::vector<float> numbers) {
+    std::pair<float, float> closest;
+    float min_diff = std::numeric_limits<float>::max();
 
     for (int i = 0; i < numbers.size() - 1; ++i) {
         for (int j = i + 1; j < numbers.size(); ++j) {
             float diff = numbers[j] - numbers[i];
             if (abs(diff) < min_diff) {
                 min_diff = abs(diff);
-                closest = make_pair(numbers[i], numbers[j]);
+                closest = std::make_pair(numbers[i], numbers[j]);
             }
         }
     }
 
-    vector<pair<float, float>> result;
-    result.push_back(closest);
-
-    return result;
-}
-
-int main() {
-    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2.2, 3.1}));
-    return 0;
+    return {closest};
 }
