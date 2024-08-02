22153ed2da9fb786b9f01140b552721d0c96d725
beep boop <githubbot@vadim.me>
2024-08-02 18:59:21 +0200

    Solution 51 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pth2wzat Prompt:       |                    ^~~ d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp:7: d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp: In function ‘int main()’: d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ d0ea710b-8f46-416e-980b-d21c4b0e0d4c.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,28 +2,16 @@
 #include <vector>
 using namespace std;
 
-bool issame(vector<int> a,vector<int>b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]) return false;
-    }
-    return true;
-}
-
-vector< vector<int> > maximum(vector<int> arr, int k) {
-    sort(arr.begin(), arr.end());
-    vector< vector<int> > result(1);
-    for(int i = 0; i < k; i++){
-        result[0].push_back(arr[arr.size() - 1 - i]);
-    }
+std::vector<int> maximum(vector<int> arr, int k) {
+    vector<int> result(arr.begin(), min(arr.begin() + k, arr.end()));
+    sort(result.begin(), result.end());
     return result;
 }
 
 int main(){
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7) , {0, -23, 1, 2, 3, 243, -400}));
     cout << "Maximum elements: ";
-    for(int i = 0; i < maximum({1, 2, 3, -23, 243, -400, 0}, 7)[0].size(); i++){
-        cout << maximum({1, 2, 3, -23, 243, -400, 0}, 7)[0][i] << " ";
+    for(int i = 0; i < maximum({1, 2, 3, -23, 243, -400, 0}, 7).size(); i++){
+        cout << maximum({1, 2, 3, -23, 243, -400, 0}, 7)[i] << " ";
     }
     return 0;
 }
