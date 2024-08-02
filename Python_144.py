dd7d41a3bb2bbbbc20c6a1de2c64768630533ec7
beep boop <githubbot@vadim.me>
2024-08-02 04:28:54 +0200

    Solution 5 of Python/144, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/lwq6epjl Prompt: Complete the following code given the task description and function signature.

--- Python_144.py
+++ Python_144.py
@@ -1,4 +1,5 @@
 def simplify(x, n):
-    a, b = map(int, x.split("/"))
-    c, d = map(int, n.split("/"))
-    return (a * d) % (b * c) == 0
+    x_num, x_den = map(int, x.split("/"))
+    n_num, n_den = map(int, n.split("/"))
+
+    return (x_num * n_den) % (x_den * n_den) == 0
