8abe316b7be59ba495edd7976191a98a62cc5f19
beep boop <githubbot@vadim.me>
2024-08-02 04:55:02 +0200

    Solution 8 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/ox6ckpzy Prompt:    14 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e355dd03-cf1c-490c-893c-fb26d69b0ef2.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e355dd03-cf1c-490c-893c-fb26d69b0ef2.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e355dd03-cf1c-490c-893c-fb26d69b0ef2.cpp:13: e355dd03-cf1c-490c-893c-fb26d69b0ef2.cpp: In function ‘int main()’: e355dd03-cf1c-490c-893c-fb26d69b0ef2.cpp:23:20: error: ‘largest_smallest_integer s’ was not declared in this scope    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ e355dd03-cf1c-490c-893c-fb26d69b0ef2.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,8 +1,9 @@
+#include <iostream>
 #include <vector>
 
-bool issame(vector<int> a, vector<int> b);
+bool issame(std::vector<int> a, std::vector<int> b);
 
-vector<int> largest_smallest_integers(vector<int> lst){
+std::vector<int> largest_smallest_integer(std::vector<int> lst){
     int max_neg = 0, min_pos = 0;
     for (int num : lst) {
         if (num < 0 && num < max_neg) {
@@ -12,8 +13,4 @@ vector<int> largest_smallest_integers(vector<int> lst){
         }
     }
     return {max_neg, min_pos};
-}
-int main(){
-    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
-    return 0;
 }
