9fc03c8da2e4ece9fc711b5b0e05ad5d74bc2141
beep boop <githubbot@vadim.me>
2024-08-02 16:44:20 +0200

    Solution 17 of CPP/135, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/rymlxj5a Prompt: 57e06bc6-4182-4747-9d8b-4470a0dc672b.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 57e06bc6-4182-4747-9d8b-4470a0dc672b.cpp:12: 57e06bc6-4182-4747-9d8b-4470a0dc672b.cpp: In function ‘int main()’: 57e06bc6-4182-4747-9d8b-4470a0dc672b.cpp:14:13: error: ‘can_arrange’ was not dec lared in this scope    14 |     assert (can_arrange({})==-1);       |             ^~~~~~~~~~~

--- CPP_135.cpp
+++ CPP_135.cpp
@@ -16,12 +16,11 @@ int main() {
     cout << "Enter the number of elements: ";
     cin >> n;
     for(int i = 0; i < n; i++) {
-        int num;
-        cout << "Enter element " << i + 1 << ": ";
-        cin >> num;
-        input.push_back(num);
+        int val;
+        cout << "Enter element " << i+1 << ": ";
+        cin >> val;
+        input.push_back(val);
     }
-    
     int output = can_arrange(input);
     cout << "Output: " << output << endl;
     return 0;
