0fe435af6cbad2dc4ebff25aae42c2a3d3289c63
beep boop <githubbot@vadim.me>
2024-08-02 03:31:40 +0200

    Solution 46 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt: 0d5655cd-464e-47f1-a847-3bdd714c6706.cpp:39:35: error: cannot bind non-const lva lue reference of type ‘std::vector<std::__cxx11::basic_string<char> >&’ to an rv alue of type ‘std::vector<std::__cxx11::basic_string<char> >’    39 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from 0d5655cd-464e-47f1-a847-3bdd714c6706.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = std::__cxx11::basic_string<char>; _Alloc = std::allocator<s td::__cxx11::basic_string<char> >; std::vector<_Tp, _Alloc>::allocator_type = st d::allocator<std::__cxx11::basic_string<char> >]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ 0d5655cd-464e-47f1-a847-3bdd714c6706.cpp:13:48: note:   initializing argument 1 of ‘std::vector<std::__cxx11::basic_string<char> > sorted_list_sum(std::vector<s td::__cxx11::basic_string<char> >&)’    13 | vector<string> sorted_list_sum(vector<string>& lst) {       |                                ~~~~~~~~~~~~~~~~^~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -11,15 +11,14 @@ bool issame(const vector<string>& a, const vector<string>& b) {
 }
 
 vector<string> sorted_list_sum(const vector<string>& lst) {
-    vector<string> modifiedList = lst;
-    modifiedList.erase(remove_if(modifiedList.begin(), modifiedList.end(), [](const string& s){return s.length() % 2 != 0;}), modifiedList.end());
-    sort(modifiedList.begin(), modifiedList.end(), [](const string& a, const string& b){
+    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){return s.length() % 2 != 0;}), lst.end());
+    sort(lst.begin(), lst.end(), [](const string& a, const string& b){
         if (a.length() == b.length()) {
             return a < b;
         }
         return a.length() < b.length();
     });
-    return modifiedList;
+    return lst;
 }
 
 int main() {
