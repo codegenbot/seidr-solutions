dfe049c5cf44a5a4692d948432bca05cac076909
beep boop <githubbot@vadim.me>
2024-08-02 23:18:18 +0200

    Solution 15 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/1yu4bxqu Prompt:  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:17:27: note: ‘std::vector’ is defined i n header ‘<vector>’; did you forget to ‘#include <vector>’? f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:16: f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp: In function ‘int main()’: f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:26:20: error: ‘common’ was not declared  in this scope    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ f26685e9-0ea3-42a2-b6f4-c4aac4416b13.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -4,18 +4,13 @@
 using namespace std;
 
 bool issame(const vector<int>& a, const vector<int>& b) {
-    if(a.size() != b.size()) return false;
-    set<int> s1(a.begin(), a.end());
-    set<int> s2(b.begin(), b.end());
-    return (s1==s2);
-}
-
-int main() {
-    vector<int> l1 = {1, 2, 3};
-    vector<int> l2 = {3, 4, 5};
-    if(issame(l1,l2))
-        cout << "The vectors are the same." << endl;
-    else
-        cout << "The vectors are not the same." << endl;
-    return 0;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
