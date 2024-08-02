ae15286e5551177c51c58856d0b94b2644eb896d
beep boop <githubbot@vadim.me>
2024-08-02 16:34:43 +0200

    Solution 39 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cg25toqe Prompt:       |                              ^~~~~~ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:32: 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp: In function ‘int main()’: 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,49 +1,22 @@
-bool issame(vector<string> v1, vector<string> v2) {
-    if(v1.size() != v2.size())
-        return false;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i] != v2[i])
-            return false;
+bool issame(vector<string> vec1, vector<string> vec2) {
+    if (vec1.size() != vec2.size()) return false;
+    for (int i = 0; i < vec1.size(); i++) {
+        if (vec1[i] != vec2[i]) return false;
     }
     return true;
 }
 
 int main() {
-    vector<string> letter_grades;
-    float grade;
-    cout << "Enter grades (enter -1 to stop): ";
-    while(cin >> grade) {
-        if(grade < 0)
-            break;
-        if (grade >= 4.0)
-            letter_grades.push_back("A+");
-        else if (grade > 3.7)
-            letter_grades.push_back("A");
-        else if (grade > 3.3)
-            letter_grades.push_back("A-");
-        else if (grade > 3.0)
-            letter_grades.push_back("B+");
-        else if (grade > 2.7)
-            letter_grades.push_back("B");
-        else if (grade > 2.3)
-            letter_grades.push_back("B-");
-        else if (grade > 2.0)
-            letter_grades.push_back("C+");
-        else if (grade > 1.7)
-            letter_grades.push_back("C");
-        else if (grade > 1.3)
-            letter_grades.push_back("C-");
-        else if (grade > 1.0)
-            letter_grades.push_back("D+");
-        else if (grade > 0.7)
-            letter_grades.push_back("D");
-        else
-            letter_grades.push_back("E");
+    vector<float> grades = {3.8, 4.2, 3.5, 2.9};
+    vector<string> letter_grades = numerical_letter_grade(grades);
+    
+    // Example usage of the issame function
+    vector<string> expected_grades = {"A", "A+", "A-", "C+"};
+    if (issame(letter_grades, expected_grades)) {
+        cout << "The grades are as expected." << endl;
+    } else {
+        cout << "The grades are not as expected." << endl;
     }
-    vector<string> expected_letter_grades = {"A+", "A", "B+", "C"};
-    if(issame(letter_grades, expected_letter_grades))
-        cout << "Letter grades are as expected.\n";
-    else
-        cout << "Letter grades do not match the expected ones.\n";
+    
     return 0;
 }
