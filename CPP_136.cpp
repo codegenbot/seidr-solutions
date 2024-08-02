6f30ce5777c940ba53fdf926f1eed92772d39220
beep boop <githubbot@vadim.me>
2024-08-03 01:18:28 +0200

    Solution 46 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/stlccgm9 Prompt:     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 97910d10-d527-456b-9a1a-e50bb37a3883.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 97910d10-d527-456b-9a1a-e50bb37a3883.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 97910d10-d527-456b-9a1a-e50bb37a3883.cpp:5: 97910d10-d527-456b-9a1a-e50bb37a3883.cpp: In function ‘int main()’: 97910d10-d527-456b-9a1a-e50bb37a3883.cpp:15:20: error: ‘largest_smallest_integer s’ was not declared in this scope    15 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ 97910d10-d527-456b-9a1a-e50bb37a3883.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,16 +1,11 @@
-#include <algorithm>
-#include <cassert>
 #include <vector>
+#include <algorithm>
 
 int largest_smallest_integers(std::vector<int> v) {
-    return *std::max_element(v.begin(), v.end()) - *std::min_element(v.begin(), v.end());
+    std::sort(v.begin(), v.end());
+    return v.back() - v.front();
 }
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return (largest_smallest_integers(a) == largest_smallest_integers(b));
-}
-
-int main() {
-    assert(issame({-6, -4, -4, -3, -100, 1}, {-3, 1}));
-    return 0;
 }
