05217a30517217b6760fd0131e0407b682fa2a4d
beep boop <githubbot@vadim.me>
2024-08-02 23:34:21 +0200

    Solution 12 of CPP/120, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q1g32s2v Prompt: 5a4119ab-d61b-45d5-b624-d8a489c6fcdc.cpp:24:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 5a4119ab-d61b-45d5-b624-d8a489c6fcdc.cpp:8:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     8 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 5a4119ab-d61b-45d5-b624-d8a489c6fcdc.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 5a4119ab-d61b-45d5-b624-d8a489c6fcdc.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -2,17 +2,10 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-
 using namespace std;
 
-vector<int> maximum(const vector<int>& arr, int k) {
-    vector<int> temp = arr;
-    sort(temp.begin(), temp.end(), greater<int>());
-    temp.resize(k);
-    return temp;
-}
-
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 3), {243, 3, 2}));
-    return 0;
+vector<int> maximum(vector<int> arr, int k) {
+    sort(arr.begin(), arr.end(), greater<int>());
+    arr.resize(k);
+    return arr;
 }
