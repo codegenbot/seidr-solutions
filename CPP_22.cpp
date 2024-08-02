7f4553446e96a081f0f2450b5be51ae94e7eba5b
beep boop <githubbot@vadim.me>
2024-08-02 21:13:44 +0200

    Solution 37 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dh6fo21w Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 942a32b4-46e6-4c57-b0f0-fe0b33c402ab.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 942a32b4-46e6-4c57-b0f0-fe0b33c402ab.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 942a32b4-46e6-4c57-b0f0-fe0b33c402ab.cpp:11: 942a32b4-46e6-4c57-b0f0-fe0b33c402ab.cpp: In function ‘int main()’: 942a32b4-46e6-4c57-b0f0-fe0b33c402ab.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 942a32b4-46e6-4c57-b0f0-fe0b33c402ab.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -2,28 +2,21 @@
 #include <list>
 #include <any>
 
-bool std::operator==(const std::any& a, const std::any& b) {
-    if (a.type() != b.type()) return false;
-    try {
-        int aInt = any_cast<int>(a);
-        int bInt = any_cast<int>(b);
-        return aInt == bInt;
-    } catch (...) {
-        return false;
-    }
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
+    return a == b;
 }
 
 std::vector<int> filter_integers(std::list<std::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
-        if (value.type() == typeid(int)) {
-            result.push_back(any_cast<int>(value));
+        if (std::any_cast<int>(value).type() == typeid(int)) {
+            result.push_back(std::any_cast<int>(value));
         }
     }
     return result;
 }
 
 int main() {
-    assert(std::equal(filter_integers({1, 2, 3}).begin(), filter_integers({1, 2, 3}).end(), {1, 2, 3}.begin()));
+    assert(std::issame(filter_integers({1, 2, 3}), std::vector<int>({1, 2, 3})));
     return 0;
 }
