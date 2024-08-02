c3e19087606ecef8365502a1af94b735fe99ff2b
beep boop <githubbot@vadim.me>
2024-08-02 19:33:35 +0200

    Solution 21 of CPP/133, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/1eu14iut Prompt: 3838b852-0c8f-48d8-8ffb-e7d52378f69d.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,9 +1,11 @@
-Here is the completed code:
+#include <vector>
+#include <cmath>
 
-int sum_squares(vector<float> lst){
-    int sum = 0;
-    for(float x : lst){
+double sum_squares(std::vector<double> lst) {
+    double sum = 0;
+    for(double x : lst){
         int ceiling = ceil(x);
         sum += pow(ceiling, 2);
     }
     return sum;
+}
