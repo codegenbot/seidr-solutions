619ba20702c7e6d7211548a4b37ee5eda8e00b75
beep boop <githubbot@vadim.me>
2024-08-02 03:33:43 +0200

    Solution 54 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt: 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~ In file included from 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:30: 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp: In function ‘int main()’: 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:40:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    40 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~ 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:9:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     9 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 2730a355-c33c-41a3-b484-cdc2b40cd2b3.cpp:31:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    31 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -11,19 +11,18 @@ bool issame(const vector<string>& a, const vector<string>& b) {
 }
 
 vector<string> sorted_list_sum(const vector<string>& lst) {
-    vector<string> result = lst;
-    
-    result.erase(remove_if(result.begin(), result.end(), [](const string& s){return s.length() % 2 != 0;}), result.end());
-    sort(result.begin(), result.end(), [](const string& a, const string& b){
+    vector<string> modified_lst = lst;
+    modified_lst.erase(remove_if(modified_lst.begin(), modified_lst.end(), [](const string& s){return s.length() % 2 != 0;}), modified_lst.end());
+    sort(modified_lst.begin(), modified_lst.end(), [](const string& a, const string& b){
         if (a.length() == b.length()) {
             return a < b;
         }
         return a.length() < b.length();
     });
-    
-    return result;
+    return modified_lst;
 }
 
-assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
-
-return 0;
+int main() {
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), vector<string>{"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
+}
