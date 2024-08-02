7c6c120b6d66684524377c2fe545e9aec5b3d381
beep boop <githubbot@vadim.me>
2024-08-02 03:24:11 +0200

    Solution 10 of CPP/158, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/yonrd5le Prompt:    13 | int unique_count(const string& s) {       |                        ^~~~~~ 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp: In function ‘int unique_count(const in t&)’: 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp:14:5: error: ‘set’ was not declared in this scope    14 |     set<char> unique_chars(s.begin(), s.end());       |     ^~~ 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp:14:9: error: expected primary-expressio n before ‘char’    14 |     set<char> unique_chars(s.begin(), s.end());       |         ^~~~ 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp:15:12: error: ‘unique_chars’ was not de clared in this scope; did you mean ‘unique_count’?    15 |     return unique_chars.size();       |            ^~~~~~~~~~~~       |            unique_count In file included from 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp:18: 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp: In function ‘int main()’: 66b3a520-a2cb-4bcd-99f4-c145eb952377.cpp:20:14: error: ‘find_max’ was not declar ed in this scope    20 |     assert ((find_max({"play", "play", "play"}) == "play"));       |              ^~~~~~~~

--- CPP_158.cpp
+++ CPP_158.cpp
@@ -1,15 +1,15 @@
+#include <set>
+#include <vector>
+using namespace std;
+
 string find_max(vector<string> words){
-    string max_word = *words.begin();
-    int max_unique_chars = 0;
-
-    for(auto word : words){
-        int unique_chars = 0;
-        set<char> s(word.begin(), word.end());
-        if(s.size() > max_unique_chars){
-            max_unique_chars = s.size();
-            max_word = word;
-        }
-    }
-
+    string max_word = *max_element(words.begin(), words.end(),
+        [](const string& a, const string& b) {
+            int unique_a = a.size() - (int)set<char>(a.begin(), a.end()).size();
+            int unique_b = b.size() - (int)set<char>(b.begin(), b.end()).size();
+            if(unique_a == unique_b)
+                return a < b;
+            return unique_a > unique_b;
+        });
     return max_word;
 }
