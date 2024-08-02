b084f973dbed493787b03763ec48895db90c1505
beep boop <githubbot@vadim.me>
2024-08-02 03:19:26 +0200

    Solution 4 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/9gt9qom5 Prompt:       |                              ^~~~~~ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:29:37: error: ‘string’ was not declared  in this scope    29 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:29:45: error: expression list treated a s compound expression in initializer [-fpermissive]    29 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:28: 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp: In function ‘int main()’: 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:38:20: error: ‘bf’ was not declared in this scope    38 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 310af7a4-0853-4dcc-b67c-5b496dbc973a.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -1,16 +1,6 @@
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
+    return a == b;
 }
