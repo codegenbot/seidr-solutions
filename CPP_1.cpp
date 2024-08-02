0beebdaf37307d303a91fbffe57894cea0b6a463
beep boop <githubbot@vadim.me>
2024-08-02 13:16:41 +0200

    Solution 11 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/3bwhs04n Prompt: e8db6024-32b7-469b-a47b-ea8261690f39.cpp:47:21: error: request for member ‘size’  in ‘b’, which is of non-class type ‘int’    47 |     if (a.size()!=b.size()) return false;       |                     ^~~~ e8db6024-32b7-469b-a47b-ea8261690f39.cpp:48:22: error: request for member ‘size’  in ‘a’, which is of non-class type ‘int’    48 |     for (int i=0;i<a.size();i++)       |                      ^~~~ e8db6024-32b7-469b-a47b-ea8261690f39.cpp:50:10: error: invalid types ‘int[int]’ for array subscript    50 |     if (a[i]!=b[i]) return false;       |          ^ e8db6024-32b7-469b-a47b-ea8261690f39.cpp:50:16: error: invalid types ‘int[int]’ for array subscript    50 |     if (a[i]!=b[i]) return false;       |                ^ In file included from e8db6024-32b7-469b-a47b-ea8261690f39.cpp:45: e8db6024-32b7-469b-a47b-ea8261690f39.cpp: In function ‘int main()’: e8db6024-32b7-469b-a47b-ea8261690f39.cpp:55:41: error: ‘separate_paren_groups’ c annot be used as a function    55 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -19,35 +19,35 @@ bool issame(const vector<string>& a, const vector<string>& b) {
 }
 
 vector<string> separate_paren_groups(string paren_string) {
-   vector<string> result;
-   string current_group;
-   size_t open_count = 0;
+    vector<string> result;
+    string current_group;
+    int open_count = 0;
 
-   for (char c : paren_string) {
-       if (c == '(') {
-           if (open_count > 0) {
-               current_group += c;
-           }
-           open_count++;
-       } else if (c == ')') {
-           open_count--;
-           if (open_count == 0) {
-               result.push_back(current_group);
-               current_group.clear();
-           } else {
-               current_group += c;
-           }
-       }
-   }
+    for (char c : paren_string) {
+        if (c == '(') {
+            if (open_count > 0) {
+                current_group += c;
+            }
+            open_count++;
+        } else if (c == ')') {
+            open_count--;
+            if (open_count == 0) {
+                result.push_back(current_group);
+                current_group.clear();
+            } else {
+                current_group += c;
+            }
+        }
+    }
 
-   return result;
+    return result;
 }
 
 int main() {
-  vector<string> a = separate_paren_groups("((a)(b)(c))");
-  vector<string> b = {"(a)", "(b)", "(c)"};
-  
-  assert(issame(a, b));
-
-  return 0;
+    vector<string> input = separate_paren_groups("(group 1)(group 2)(group 3)");
+    vector<string> expected_output = {"group 1", "group 2", "group 3"};
+    
+    assert(issame(input, expected_output));
+    
+    return 0;
 }
