84325cd62b93646296bb1a608a53e4cea9a854fe
beep boop <githubbot@vadim.me>
2024-08-02 15:55:25 +0200

    Solution 10 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/ep0dkhzz Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -2,23 +2,53 @@
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+    int n = v.size();
     int min_diff = INT_MAX;
-    int split_index = 0;
-    
-    for (int i = 1; i < v.size(); i++) {
-        int diff = abs(v[i] - v[0]);
-        if (diff < min_diff) {
-            min_diff = diff;
-            split_index = i;
+    int cut_index = -1;
+
+    for (int i = 0; i < n; ++i) {
+        int sum_left = 0, sum_right = 0;
+        for (int j = 0; j < i; ++j) {
+            sum_left += v[j];
+        }
+        for (int j = i; j < n; ++j) {
+            sum_right += v[j];
+        }
+
+        if (sum_left == sum_right) {
+            return {{}, {v.begin(), v.end()}};
+        } else if (abs(sum_left - sum_right) < min_diff) {
+            min_diff = abs(sum_left - sum_right);
+            cut_index = i;
         }
     }
-    
-    vector<int> left = {v[0]};
-    vector<int> right = v;
-    for (int i = 1; i <= split_index; i++) {
-        left.push_back(v[i]);
+
+    vector<int> left(v.begin(), v.begin() + cut_index);
+    vector<int> right(v.begin() + cut_index, v.end());
+    return {left, right};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &x : v) {
+        cin >> x;
     }
-    right.erase(right.begin() + split_index, right.end());
     
-    return make_pair(left, right);
+    auto res = cutVector(v);
+
+    cout << "{";
+    for (auto x : res.first) {
+        cout << x << " ";
+    }
+    cout << "}" << endl;
+
+    cout << "{";
+    for (auto x : res.second) {
+        cout << x << " ";
+    }
+    cout << "0" << endl;
+    
+    return 0;
 }
