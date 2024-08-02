d3283ca8e0a528877512b297ab69ea737456bbf3
beep boop <githubbot@vadim.me>
2024-08-02 11:25:24 +0200

    Solution 60 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/51dmdws6 Prompt: In file included from 1476c2f7-2c36-4bfb-898f-b2f6ebc208f7.cpp:15: 1476c2f7-2c36-4bfb-898f-b2f6ebc208f7.cpp: In function ‘int main()’: 1476c2f7-2c36-4bfb-898f-b2f6ebc208f7.cpp:17:13: error: ‘filp_case’ was not decla red in this scope; did you mean ‘flip_case’?    17 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,4 +1,4 @@
-```
+```cpp
 #include<string>
 using namespace std;
 
@@ -12,3 +12,4 @@ string flip_case(string str){
     }
     return result;
 }
+assert(flip_case("These violent delights have violent ends") == "tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");
