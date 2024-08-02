2a00f18fa6154a0f2e0fb2ea5030f899e5213cc1
beep boop <githubbot@vadim.me>
2024-08-02 04:18:14 +0200

    Solution 38 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/k289ptgg Prompt:       |                           ^~~~~~ 3af8e51f-6270-4b89-b416-464d0c35b651.cpp:16:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 3af8e51f-6270-4b89-b416-464d0c35b651.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 3af8e51f-6270-4b89-b416-464d0c35b651.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 3af8e51f-6270-4b89-b416-464d0c35b651.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 3af8e51f-6270-4b89-b416-464d0c35b651.cpp:15: 3af8e51f-6270-4b89-b416-464d0c35b651.cpp: In function ‘int main()’: 3af8e51f-6270-4b89-b416-464d0c35b651.cpp:25:23: error: could not convert ‘eat(4,  5, 1)’ from ‘std::pair<int, int>’ to ‘std::vector<int>’    25 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ~~~^~~~~~~~~       |                       |       |                       std::pair<int, int>

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -8,11 +8,10 @@ std::pair<int, int> eat(int number, int need, int remaining){
     return {total, left};
 }
 
-bool issame(std::pair<int, int> a, std::pair<int, int> b){
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
 int main() {
-    assert(issame(eat(4, 5, 1), std::make_pair(5, 0)));
-    return 0;
+    assert(eat(4, 5, 1) == std::make_pair(5, 0));
 }
