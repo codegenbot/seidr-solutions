07e7eec216ce78dded8b4be2e2a270237f57cc98
beep boop <githubbot@vadim.me>
2024-08-02 05:10:41 +0200

    Solution 80 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/thkjha2e Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/59b323f9-e48f-4197-b256-542a81f96 5fc.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_151.py
+++ Python_151.py
@@ -1,12 +1,3 @@
+```
 def double_the_difference(lst):
-    even_nums = [num for num in lst if isinstance(num, int) and num % 2 == 0]
-    odd_nums = [num for num in lst if isinstance(num, int) and num % 2 != 0]
-
-    if not even_nums or not odd_nums:
-        return 0
-
-    mean_even = sum(even_nums) / len(even_nums)
-    mean_odd = sum(odd_nums) / len(odd_nums)
-
-    absolute_diff = abs(mean_even - mean_odd)
-    return 2 * absolute_diff
+    return sum(map(lambda x: x ** 2, filter(lambda x: isinstance(x, int) and x > 0, lst))) * 2
