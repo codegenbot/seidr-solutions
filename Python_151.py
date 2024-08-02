d47cf2c236c78f9fa23e2ec40eaa86989350beb9
beep boop <githubbot@vadim.me>
2024-08-03 00:19:41 +0200

    Solution 17 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/o4rqubrz Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54a1e1a3-d630-40c1-9cc9-dcc97d463 29d.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/54a1e1a3-d630-40c1-9cc9-dcc97d463 29d.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,13 @@
-def check(func, lst):
-    return func(lst)
+def check(function, lst):
+    return function(lst)
+
 
 def double_the_difference(lst):
-    return sum(x ** 2 for x in lst if x % 2 != 0 and x > 0 and isinstance(x, int)) * 2 if lst else 0
+    return (
+        sum(x**2 for x in lst if x % 2 != 0 and x > 0 and isinstance(x, int)) * 2
+        if lst
+        else 0
+    )
 
-check(double_the_difference, lst)  # Add the input parameter 'lst' to the check function
+
+check(double_the_difference, lst)
