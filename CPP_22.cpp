7f523872ea0ac71a5ebe58719059f21fd3092c55
beep boop <githubbot@vadim.me>
2024-08-02 14:05:49 +0200

    Solution 78 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rc9h4uxi Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ff33a388-002a-493e-87ee-b62e7f9f8a12.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ff33a388-002a-493e-87ee-b62e7f9f8a12.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ff33a388-002a-493e-87ee-b62e7f9f8a12.cpp:11: ff33a388-002a-493e-87ee-b62e7f9f8a12.cpp: In function ‘int main()’: ff33a388-002a-493e-87ee-b62e7f9f8a12.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ff33a388-002a-493e-87ee-b62e7f9f8a12.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -14,17 +14,12 @@ bool issame(vector<int> a, vector<int> b) {
     return true;
 }
 
-vector<int> filter_integers(list<boost::any> values) {
+vector<int> filter_integers(list<any> values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (boost::any_cast<int>(value).good()) {
-            result.push_back(boost::any_cast<int>(value));
+        if (any_cast<int>(value).good()) {
+            result.push_back(any_cast<int>(value));
         }
     }
     return result;
-}
-
-int main() {
-    assert(issame(filter_integers({3, boost::any('c'), 3, 3, boost::any('a'), boost::any('b')}), {3, 3, 3}));
-    return 0;
 }
