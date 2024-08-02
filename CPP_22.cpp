ed9fdd6ff85044c1c43ccdb43535aa6f655d895d
beep boop <githubbot@vadim.me>
2024-08-02 11:18:06 +0200

    Solution 88 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/qsjpohll Prompt:  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ec810d19-4e2b-45c9-81d2-ff017ec7e133.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ec810d19-4e2b-45c9-81d2-ff017ec7e133.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ec810d19-4e2b-45c9-81d2-ff017ec7e133.cpp:12: ec810d19-4e2b-45c9-81d2-ff017ec7e133.cpp: In function ‘int main()’: ec810d19-4e2b-45c9-81d2-ff017ec7e133.cpp:22:20: error: ‘filter_integers’ was not  declared in this scope    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ ec810d19-4e2b-45c9-81d2-ff017ec7e133.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -14,6 +14,6 @@ std::vector<int> filter_integers(std::list<boost::any> values) {
 }
 
 int main() {
-    assert(filter_integers({3, 3, 'c', 3, 'a', 'b'}) == (std::vector<int>{3, 3, 3}));
+    assert(filter_integers({3, 'c', 3, 3, 'a', 'b'}) == (std::vector<int>{3, 3, 3}));
     return 0;
 }
