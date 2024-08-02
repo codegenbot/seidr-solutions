e5310c9d68d498e476424d5b369bdc580edd9002
beep boop <githubbot@vadim.me>
2024-08-02 15:24:27 +0200

    Solution 47 of Python/17, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/qmujg3vk Prompt: Program did not receive expected input

--- Python_17.py
+++ Python_17.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 def parse_music(music_string: str) -> List[str]:
     notes = {"o": "4", "o|": "2", ".|": "1"}
-    return [str(notes[note]) for note in music_string.split()]
+    return [notes[note] for note in music_string.split()]
