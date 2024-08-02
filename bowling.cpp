7cf7107b2954f600e073f34cef6d2ed143aef216
beep boop <githubbot@vadim.me>
2024-08-03 01:03:30 +0200

    Solution 59 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/cyefu9vh Prompt: dfafed38-cd4b-49ae-94cc-5b762ed4cc76.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -5,22 +5,25 @@ int bowlingScore(std::string s) {
     int score = 0;
     int rolls = 0;
 
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'X') {
+    std::istringstream iss(s);
+    std::string frame;
+
+    while (std::getline(iss, frame, ' ')) {
+        if (frame == "X") {
             score += 30;
             rolls++;
-        } else if (isdigit(s[i])) {
-            int frameScore = 10 - (s[i] - '0');
-
-            if (i + 1 < s.length() && s[i + 1] == '/') {
-                frameScore = 10 - (s[i] - '0') / 2;
-                i++;
-            }
-
-            score += frameScore;
+        } else if (frame.size() > 1 && frame[1] == '/') {
+            int spare = 10 - (frame[0] - '0');
+            score += spare + 10;
+            rolls+=2;
+        } else {
+            int roll = std::stoi(frame);
+            score += roll;
             rolls++;
-        } else if (s[i] == '/') {
-            continue;
+            if (roll < 10) {
+                score += std::stoi(iss.str().substr(0, iss.str().find(' ')).erase(0, iss.str().find_first_of("0123456789") + 1));
+                rolls++;
+            }
         }
 
         if (rolls >= 10) break;
