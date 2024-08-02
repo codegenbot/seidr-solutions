b4f077587249fcb8db730b0734041078f3f93463
beep boop <githubbot@vadim.me>
2024-08-02 03:58:44 +0200

    Solution 30 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/xuiobjzv Prompt:       |                           ^~~~~~ fc66593c-cc34-405b-af98-39c4ccccd436.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from fc66593c-cc34-405b-af98-39c4ccccd436.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from fc66593c-cc34-405b-af98-39c4ccccd436.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fc66593c-cc34-405b-af98-39c4ccccd436.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fc66593c-cc34-405b-af98-39c4ccccd436.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ fc66593c-cc34-405b-af98-39c4ccccd436.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -19,3 +19,8 @@ std::vector<int> even_odd_count(int num) {
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
+
+int main() {
+    assert(issame(even_odd_count(0), std::vector<int>({1, 0})));
+    return 0;
+}
