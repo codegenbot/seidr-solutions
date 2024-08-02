04572d5fb8ef9209ebbff61bb10ee684d3d6b1c4
beep boop <githubbot@vadim.me>
2024-08-02 18:23:04 +0200

    Solution 8 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/zcuavjy9 Prompt: ared in this scope    14 |         int left_sum = accumulate(vec.begin(), vec.begin() + i, 0);       |                        ^~~~~~~~~~ e7440326-64cf-4c8c-815a-9dc2c4191db2.cpp:17:13: error: ‘abs’ was not declared in  this scope    17 |         if (abs(left_sum - right_sum) < diff) {       |             ^~~ e7440326-64cf-4c8c-815a-9dc2c4191db2.cpp:24:9: error: ‘cout’ was not declared in  this scope    24 |         cout << vec[i] << endl;       |         ^~~~ e7440326-64cf-4c8c-815a-9dc2c4191db2.cpp:24:27: error: ‘endl’ was not declared i n this scope    24 |         cout << vec[i] << endl;       |                           ^~~~ e7440326-64cf-4c8c-815a-9dc2c4191db2.cpp:27:5: error: ‘cout’ was not declared in  this scope    27 |     cout << endl;       |     ^~~~ e7440326-64cf-4c8c-815a-9dc2c4191db2.cpp:27:13: error: ‘endl’ was not declared i n this scope    27 |     cout << endl;       |             ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <numeric>
+#include <vector>
 #include <climits>
+
 using namespace std;
 
 int main() {
     vector<int> vec;
     int num;
-
+    
     while (cin >> num) {
         vec.push_back(num);
     }
-
+    
     int n = vec.size();
     int diff = INT_MAX;
     int idx = 0;
-
+    
     for (int i = 1; i < n; ++i) {
         int left_sum = accumulate(vec.begin(), vec.begin() + i, 0);
         int right_sum = accumulate(vec.begin() + i, vec.end(), 0);
-
-        if (abs(left_sum - right_sum) < diff) {
-            diff = abs(left_sum - right_sum);
+        
+        if (std::abs(left_sum - right_sum) < diff) {
+            diff = std::abs(left_sum - right_sum);
             idx = i;
         }
     }
-
+    
     for (int i = 0; i < idx; ++i) {
-        cout << vec[i] << endl;
+        cout << vec[i] << "\n";
     }
-
-    cout << endl;
-
+    
+    cout << "\n";
+    
     for (int i = idx; i < n; ++i) {
-        cout << vec[i] << endl;
+        cout << vec[i] << "\n";
     }
-
+    
     return 0;
 }
