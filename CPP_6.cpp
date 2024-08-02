2903dc63e28034316e9cfd34da6d4a4207368066
beep boop <githubbot@vadim.me>
2024-08-02 13:30:09 +0200

    Solution 20 of CPP/6, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/1vqadmhd Prompt: In file included from e7b02346-6c69-4c2b-a1c6-844099395ecf.cpp:30: e7b02346-6c69-4c2b-a1c6-844099395ecf.cpp: In function ‘int main()’: e7b02346-6c69-4c2b-a1c6-844099395ecf.cpp:40:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    40 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ e7b02346-6c69-4c2b-a1c6-844099395ecf.cpp:6:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     6 | bool issame(const vector<int>& a, const vector<int>& b);       |      ^~~~~~ e7b02346-6c69-4c2b-a1c6-844099395ecf.cpp:31:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,3 +1,9 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
 vector<int> parse_nested_parens(string paren_string){
     vector<int> depths;
     int max_depth = 0;
