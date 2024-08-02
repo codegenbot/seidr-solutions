66412d865f59654cf8cd008a46e891e375c4d086
beep boop <githubbot@vadim.me>
2024-08-02 03:16:21 +0200

    Solution 10 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/yxq5rq70 Prompt:       |     ^~~~ 2e25365a-2558-4822-bd42-9eafaa45d323.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main(){       |     ^~~~ In file included from 2e25365a-2558-4822-bd42-9eafaa45d323.cpp:28: 2e25365a-2558-4822-bd42-9eafaa45d323.cpp: In function ‘int main()’: 2e25365a-2558-4822-bd42-9eafaa45d323.cpp:38:20: error: ‘odd_count’ was not decla red in this scope    38 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ 2e25365a-2558-4822-bd42-9eafaa45d323.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    39 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    40 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    41 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    42 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,32 +1,28 @@
-bool issame(vector<string> a, vector<string> b) {
+vector<string> odd_count(vector<string> lst);
+
+bool issame(vector<string> a, vector<string> b){
     return a == b;
 }
 
-vector<string> odd_count(vector<string> lst);
-
-#include <iostream>
-#include <vector>
-#include <cassert>
-
-int main() {
+int main(){
     assert(issame(odd_count({"271", "137", "314"}), {
-        "the number of odd elements 2n the str2ng 271 of the 2nput.",
-        "the number of odd elements 3n the str137 of the 3nput.",
-        "the number of odd elements 3n the str314 of the 3nput."
+        "the number of odd elements 1 in the string 271 of the input.",
+        "the number of odd elements 2 in the string 137 of the input.",
+        "the number of odd elements 2 in the string 314 of the input."
     }));
     return 0;
 }
 
-vector<string> odd_count(vector<string> lst) {
+vector<string> odd_count(vector<string> lst){
     vector<string> result;
     for (string s : lst) {
-        int oddCount = 0;
+        int odd_count = 0;
         for (char c : s) {
             if ((c - '0') % 2 != 0) {
-                oddCount++;
+                odd_count++;
             }
         }
-        result.push_back("the number of odd elements " + to_string(oddCount) + "n the str" + to_string(oddCount) + "ng " + s + " of the " + to_string(oddCount) + "nput.");
+        result.push_back("the number of odd elements " + to_string(odd_count) + " in the string " + s + " of the input.");
     }
     return result;
 }
