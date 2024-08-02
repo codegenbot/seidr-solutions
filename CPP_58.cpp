fd905c7928f8b3974f3b81efc4a11400c47108be
beep boop <githubbot@vadim.me>
2024-08-02 15:32:29 +0200

    Solution 15 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/z6t8pju8 Prompt:       |                      ^~       |                      l1 b7f75611-891f-4b99-8ef7-5ac5b28f7977.cpp:12:44: error: ‘s2’ was not declared in this scope; did you mean ‘l2’?    12 |     set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), inserte r(intersection, intersection.begin()));       |                                            ^~       |                                            l2 b7f75611-891f-4b99-8ef7-5ac5b28f7977.cpp:12:75: error: ‘intersection’ was not de clared in this scope    12 | ection(s1.begin(), s1.end(), s2.begin(), s2.end(), inserter(intersection , intersection.begin()));       |                                                             ^~~~~~~~~~~~ b7f75611-891f-4b99-8ef7-5ac5b28f7977.cpp: At global scope: b7f75611-891f-4b99-8ef7-5ac5b28f7977.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ b7f75611-891f-4b99-8ef7-5ac5b28f7977.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -9,9 +9,7 @@ vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s2(l2.begin(), l2.end());
 
     set<int> intersection;
-    intersection.insert(s1.begin(), s1.end());
-    intersection.insert(s2.begin(), s2.end());
-    intersection.erase(remove(intersection.begin(), intersection.end(), 653), intersection.end());
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(), inserter(intersection, intersection.begin()));
 
     vector<int> result(intersection.begin(), intersection.end());
     sort(result.begin(), result.end());
