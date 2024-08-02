5742bb2cbaacea1f7094f0067270871b567a7978
beep boop <githubbot@vadim.me>
2024-08-02 14:41:35 +0200

    Solution 2 of CPP/40, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/iwhvms0f Prompt: Complete the following code given the task description and function signature.

--- CPP_40.cpp
+++ CPP_40.cpp
@@ -2,19 +2,16 @@
 using namespace std;
 
 bool triples_sum_to_zero(vector<int> l) {
-    sort(l.begin(), l.end());
-    for(int i = 0; i < l.size() - 2; i++) {
-        int j = i + 1;
-        int k = l.size() - 1;
-        while(j < k) {
-            int sum = l[i] + l[j] + l[k];
-            if(sum < 0) {
-                j++;
-            } else if(sum > 0) {
-                k--;
-            } else {
-                return true;
-            }
+    set<int> s(l.begin(), l.end());
+    for (int i = 0; i < s.size(); i++) {
+        int a = s.at(i);
+        set<int>::iterator it1 = s.begin();
+        set<int>::iterator it2 = --it1;
+        while (*it2 + *it1 != -a && ++it2 != s.end()) {
+            it2++;
+        }
+        if (it2 != s.end() && *it2 + *it1 == -a) {
+            return true;
         }
     }
     return false;
