0b98d5f4adbef904dfe1d1adf79478a9dc67ee96
beep boop <githubbot@vadim.me>
2024-08-02 03:57:22 +0200

    Solution 80 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7tmteijs Prompt: f1f22860-7422-4c21-995b-59fae4902066.cpp: In function ‘std::string string_to_md5 (std::string)’: f1f22860-7422-4c21-995b-59fae4902066.cpp:11:5: error: ‘MD5_CTX’ was not declared  in this scope    11 |     MD5_CTX ctx;       |     ^~~~~~~ f1f22860-7422-4c21-995b-59fae4902066.cpp:12:15: error: ‘ctx’ was not declared in  this scope    12 |     MD5_Init(&ctx);       |               ^~~ f1f22860-7422-4c21-995b-59fae4902066.cpp:12:5: error: ‘MD5_Init’ was not declare d in this scope    12 |     MD5_Init(&ctx);       |     ^~~~~~~~ f1f22860-7422-4c21-995b-59fae4902066.cpp:15:9: error: ‘MD5_Update’ was not decla red in this scope    15 |         MD5_Update(&ctx, p, 1);       |         ^~~~~~~~~~ f1f22860-7422-4c21-995b-59fae4902066.cpp:18:5: error: ‘MD5_Final’ was not declar ed in this scope    18 |     MD5_Final(mdBuffer, &ctx);       |     ^~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,23 +1,22 @@
+#include <openssl/md5.h>
 #include <string>
-#include <algorithm>
-#include <iomanip>
 #include <sstream>
-#include <md5.h>
 
 using namespace std;
 
 string string_to_md5(string text) {
     if (text.empty()) return "None";
     
+    MD5_CTX context;
     unsigned char mdBuffer[16];
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
+    
+    MD5_Init(&context);
     const char* p = text.c_str();
     while (*p) {
-        MD5_Update(&ctx, p, 1);
+        MD5_Update(&context, p, 1);
         p++;
     }
-    MD5_Final(mdBuffer, &ctx);
+    MD5_Final(mdBuffer, &context);
 
     ostringstream oss;
     for (int i = 0; i < 16; ++i) {
