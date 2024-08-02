123cbed6d415dd4a74801fbab659252c5f9115b4
beep boop <githubbot@vadim.me>
2024-08-03 01:57:41 +0200

    Solution 5 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/z01068wd Prompt:       |                              ^~~~~~ 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp:31:37: error: ‘string’ was not declared  in this scope    31 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp:31:44: error: ‘b’ was not declared in t his scope    31 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp:31:45: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp:30: 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp: In function ‘int main()’: 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp:40:20: error: ‘bf’ was not declared in this scope    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ 1f3c6be6-4a97-48a6-bb1b-8076c99d76aa.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -6,8 +6,6 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> bf(string planet1, string planet2);
-
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     vector<string> result;
