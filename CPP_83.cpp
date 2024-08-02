fb93ee737e9b8cefc60906f6d17759ffc699223c
beep boop <githubbot@vadim.me>
2024-08-03 00:58:58 +0200

    Solution 17 of CPP/83, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/fxsod9y5 Prompt: e310131e-d4dc-4b78-bd58-8341ea13567a.cpp: In function ‘int starts_one_ends(int)’ : e310131e-d4dc-4b78-bd58-8341ea13567a.cpp:6:33: error: ‘pow’ was not declared in this scope     6 |             for (int j = 1; j < pow(10, n-1); j++) {       |                                 ^~~

--- CPP_83.cpp
+++ CPP_83.cpp
@@ -8,7 +8,7 @@ int starts_one_ends(int n) {
         if (n > 1) {
             for (int j = 1; j < pow(10, n-1); j++) {
                 int num = i * pow(10, n-1) + j;
-                if (num / pow(10, n-1) % 10 == 1 || num % 10 == 1) count++;
+                if ((num / pow(10, n-1)) % 10 == 1 || num % 10 == 1) count++;
             }
         }
     }
