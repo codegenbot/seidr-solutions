bb13ede0693fae21616398d2b41aa60cb4217e09
beep boop <githubbot@vadim.me>
2024-08-02 03:47:20 +0200

    Solution 66 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/up440yot Prompt:    92 | #  define assert(expr) \       | 32f30b68-daef-415f-8aed-16bf1faf91c6.cpp: In function ‘int main()’: 32f30b68-daef-415f-8aed-16bf1faf91c6.cpp:17:5: error: ‘assert’ was not declared in this scope    17 |     assert(compare({1, 2, 3, 5}, {-1, 2, 3, 4}) == vector<int>{2, 0, 0, 1});       |     ^~~~~~ 32f30b68-daef-415f-8aed-16bf1faf91c6.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <cmath>   +++ |+#include <cassert>     5 | 32f30b68-daef-415f-8aed-16bf1faf91c6.cpp: At global scope: 32f30b68-daef-415f-8aed-16bf1faf91c6.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 32f30b68-daef-415f-8aed-16bf1faf91c6.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -14,10 +14,6 @@ vector<int> compare(vector<int> game, vector<int> guess) {
 }
 
 int main() {
-    vector<int> game = {1, 2, 3, 5};
-    vector<int> guess = {-1, 2, 3, 4};
-    
-    assert(compare(game, guess) == vector<int>{2, 0, 0, 1});
-    
+    assert(compare({1, 2, 3, 5}, {-1, 2, 3, 4}) == vector<int>{2, 0, 0, 1});
     return 0;
 }
