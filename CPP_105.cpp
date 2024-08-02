e7fc070506e06b9ee16504fb75beef492906b075
beep boop <githubbot@vadim.me>
2024-08-02 15:08:20 +0200

    Solution 17 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/hm2e0mj7 Prompt:       |                                 ^~~~~~ In file included from 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp:48:45: error: expression list treated a s compound expression in initializer [-fpermissive]    48 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp:47: 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp: In function ‘int main()’: 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp:57:20: error: ‘by_length’ was not decla red in this scope    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 1b069925-93c1-4c01-ab3c-32734a5c8494.cpp:57:19: error: ‘issame’ cannot be used a s a function    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -2,10 +2,6 @@
 #include <vector>
 #include <string>
 
-bool issame(vector<string> a, vector<string> b) {
-    return (a == b);
-}
-
 std::vector<std::string> by_length(std::vector<int> arr) {
     std::vector<int> sortedArr;
     for (int i : arr) {
@@ -50,7 +46,10 @@ std::vector<std::string> by_length(std::vector<int> arr) {
     return result;
 }
 
+bool issame(vector<string> a, vector<string> b) {
+    return (a == b);
+}
+
 int main() {
     assert(issame(by_length({9, 4, 8}) , {"Nine", "Four", "Eight"}));
-    // Other assertions or operations can be performed here.
 }
