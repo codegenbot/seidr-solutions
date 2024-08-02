e4afdbd9a746d4cee5a3b838a6a1816fffadc5fe
beep boop <githubbot@vadim.me>
2024-08-02 03:44:04 +0200

    Solution 79 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/7vjzze1n Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c45cbb27-bb0f-4c4c-aea0-6f942d60e 495.py", line 24, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c45cbb27-bb0f-4c4c-aea0-6f942d60e 495.py", line 21, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c45cbb27-bb0f-4c4c-aea0-6f942d60e 495.py", line 11, in minPath     x, y = divmod(path[-1] - 1, n) + (dx, dy)     ^^^^ ValueError: too many values to unpack (expected 2)

--- Python_129.py
+++ Python_129.py
@@ -8,7 +8,7 @@ def minPath(grid, k):
         if len(path) == k + 1:
             return path
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            x, y = (path[-1] - 1) // n, (path[-1] - 1) % n 
+            x, y = (path[-1] - 1) // n, (path[-1] - 1) % n
             x += dx
             y += dy
             if 0 <= x < n and 0 <= y < n and val + n * x + y not in seen:
