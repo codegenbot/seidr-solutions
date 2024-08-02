e3c89a373f2d58eac6826beb4ff760d09cabe665
beep boop <githubbot@vadim.me>
2024-08-02 19:15:26 +0200

    Solution 11 of CPP/128, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/ocod9jdx Prompt: 6d9e309b-43e1-492d-bfb4-afd29ef4022b.cpp: In function ‘int prod_signs(std::vecto r<int>)’: 6d9e309b-43e1-492d-bfb4-afd29ef4022b.cpp:11:24: error: ‘sign’ was not declared i n this scope; did you mean ‘signed’?    11 |             product *= sign(num);       |                        ^~~~       |                        signed 6d9e309b-43e1-492d-bfb4-afd29ef4022b.cpp:12:20: error: ‘abs’ was not declared in  this scope    12 |             sum += abs(num);       |                    ^~~ 6d9e309b-43e1-492d-bfb4-afd29ef4022b.cpp: In function ‘int sign(int)’: 6d9e309b-43e1-492d-bfb4-afd29ef4022b.cpp:20:18: error: ‘abs’ was not declared in  this scope    20 |     return num / abs(num);       |                  ^~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,6 +1,11 @@
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+int sign(int num) {
+    return num / abs(num);
+}
 
 int prod_signs(vector<int> arr) {
     int product = 1;
@@ -9,13 +14,9 @@ int prod_signs(vector<int> arr) {
     for (int num : arr) {
         if (num != 0) {
             product *= sign(num);
-            sum += abs(num);
+            sum += std::abs(num);
         }
     }
     
     return product * sum > 0 ? sum : -32768;
-}
-
-int sign(int num) {
-    return num / abs(num);
 }
