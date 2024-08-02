b27883b9ea895ab96446eed37078703dcdc595cc
beep boop <githubbot@vadim.me>
2024-08-03 01:18:36 +0200

    Solution 63 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/cyefu9vh Prompt: dfafed38-cd4b-49ae-94cc-5b762ed4cc76.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -11,26 +11,18 @@ int bowlingScore(string s) {
             rolls++;
         } else if (isdigit(s[i])) {
             int frameScore = 10 - (s[i] - '0');
-
+            
             if (i + 1 < s.length() && s[i + 1] == '/') {
                 frameScore = 10 - (s[i] - '0') / 2;
                 i++;
             }
-
+            
             score += frameScore;
             rolls++;
         } else if (s[i] == '/') {
-            int j = i + 1;
-            while (j < s.length() && isdigit(s[j])) j++;
-            int frameScore =stoi(s.substr(i+1, j-i-1));
-            if (frameScore > 10) {
-                score += 10 + (frameScore - 10);
-            } else {
-                score += frameScore;
-            }
-            i = j;
+            continue;
         }
-
+        
         if (rolls >= 10) break;
     }
 
