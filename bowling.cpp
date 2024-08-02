790a346ee9c4f2923467c547379c5d9279fbeedb
beep boop <githubbot@vadim.me>
2024-08-02 23:49:27 +0200

    Solution 12 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/961pbuwt Prompt: 367501d2-5b2d-4d0c-8e05-ea283de6a3b2.cpp:3:18: error: ‘string’ was not declared in this scope     3 | int bowlingScore(string s) {       |                  ^~~~~~ 367501d2-5b2d-4d0c-8e05-ea283de6a3b2.cpp:3:18: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 367501d2-5b2d-4d0c-8e05-ea283de6a3b2.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 367501d2-5b2d-4d0c-8e05-ea283de6a3b2.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,13 +1,33 @@
 #include <string>
 
-using namespace std;
-
 int bowlingScore(std::string s) {
     int score = 0;
     int frame = 1;
     int bowl = 0;
     for (char c : s) {
-        // existing logic remains the same
+        if (c == 'X') {
+            score += 10;
+            if (frame < 10) {
+                score += (s[bowl + 1] == 'X') ? 10 : (s[bowl + 1] - '0');
+                score += (s[bowl + 2] == 'X') ? 10 : (s[bowl + 2] == '/' ? 10 - (s[bowl + 1] - '0') : (s[bowl + 2] - '0'));
+            }
+            frame++;
+            bowl++;
+        } else if (c == '/') {
+            score += 10 - (s[bowl - 1] - '0');
+            score += (s[bowl + 1] == 'X') ? 10 : (s[bowl + 1] - '0');
+            frame++;
+            bowl += 2;
+        } else if (c == '-') {
+            // do nothing
+        } else {
+            score += c - '0';
+            if (frame < 10 && s[bowl + 1] == '/') {
+                score += 10 - (c - '0');
+            }
+            frame++;
+            bowl++;
+        }
     }
     return score;
 }
