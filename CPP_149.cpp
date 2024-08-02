c723aafe1a1757ceb5720ae0be80bf467657e04d
beep boop <githubbot@vadim.me>
2024-08-02 03:25:36 +0200

    Solution 20 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt: cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp:17:28: error: ‘a’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                            ^ cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp:17:30: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp:16: cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp: In function ‘int main()’: cd7c0431-72c0-46b6-92cf-059881a7be6d.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -4,16 +4,6 @@ bool issame(const string& a, const string& b){
 
 vector<string> sorted_list_sum(vector<string>& lst){
     lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){return s.length() % 2 != 0;}), lst.end());
-    sort(lst.begin(), lst.end(), [](const string& a, const string& b){
-        if (a.length() == b.length()) {
-            return a < b;
-        }
-        return a.length() < b.length();
-    });
+    sort(lst.begin(), lst.end());
     return lst;
-}
-
-int main(){
-    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
-    return 0;
 }
