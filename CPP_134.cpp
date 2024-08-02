7bf8e616d7dc97cae2cc962f0055d5efeb1bb86d
beep boop <githubbot@vadim.me>
2024-08-02 04:48:14 +0200

    Solution 11 of CPP/134, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/snj066rj Prompt: b12f9f18-28bd-4c1e-9456-c663e68467e8.cpp:18:5: error: redefinition of ‘int main( )’    18 | int main(){       |     ^~~~ b12f9f18-28bd-4c1e-9456-c663e68467e8.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -7,9 +7,3 @@ bool check_if_last_char_is_a_letter(const std::string& txt) {
     char lastChar = txt.back();
     return isalpha(lastChar) && txt.find_last_of(" ") == txt.size() - 1;
 }
-
-int main() {
-    assert(check_if_last_char_is_a_letter("apple pi e ") == false);
-
-    return 0;
-}
