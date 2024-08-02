6301652459e5b39a686ffdb18514139f54828492
beep boop <githubbot@vadim.me>
2024-08-02 11:10:06 +0200

    Solution 5 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/2fb9152g Prompt: Complete the following code given the task description and function signature.

--- Python_8.py
+++ Python_8.py
@@ -4,7 +4,9 @@ from typing import List, Tuple
 def sum_product(numbers: List[int]) -> Tuple[int, int]:
     total_sum = 0
     product = 1
+
     for num in numbers:
         total_sum += num
         product *= num
+
     return (total_sum, product) if numbers else (0, 1)
