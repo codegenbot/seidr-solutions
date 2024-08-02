3120b475fcb1d7ee80aecbceb8227aa201ec4351
beep boop <githubbot@vadim.me>
2024-08-02 03:30:35 +0200

    Solution 39 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/aza6mbvx Prompt:       |                                 ^~~~~~ In file included from dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp:23: dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp: In function ‘int main()’: dc49b44d-1b01-4cec-91f2-da67d2ecaa11.cpp:33:20: error: ‘reverse_delete’ was not declared in this scope; did you mean ‘timer_delete’?    33 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -5,7 +5,6 @@
 
 std::pair<std::string, bool> issame(std::vector<std::string> a, std::vector<std::string> b) {
     std::string result = "";
-
     for(auto ch : a[0]){
         if(b[0].find(ch) == std::string::npos){
             result += ch;
@@ -14,7 +13,7 @@ std::pair<std::string, bool> issame(std::vector<std::string> a, std::vector<std:
 
     std::string result_reverse = result;
     std::reverse(result_reverse.begin(), result_reverse.end());
-    
+
     return {result, result == result_reverse};
 }
 
