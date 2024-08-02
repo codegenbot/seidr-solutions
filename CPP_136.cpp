2af68d3dfca4b1008e0127d46f996485a943671f
beep boop <githubbot@vadim.me>
2024-08-02 16:52:36 +0200

    Solution 30 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/jkrn17wy Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ bb0c73cc-64d3-4022-9fe1-219137e801e1.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bb0c73cc-64d3-4022-9fe1-219137e801e1.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from bb0c73cc-64d3-4022-9fe1-219137e801e1.cpp:15: bb0c73cc-64d3-4022-9fe1-219137e801e1.cpp: In function ‘int main()’: bb0c73cc-64d3-4022-9fe1-219137e801e1.cpp:25:20: error: ‘largest_smallest_integer s’ was not declared in this scope    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~ bb0c73cc-64d3-4022-9fe1-219137e801e1.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,15 +1,17 @@
-vector<int> largest_smallest_integers(vector<int> lst) {
-    int maxNeg = 0;
-    int minPos = INT_MAX;
+```cpp
+bool issame(vector<int> a, vector<int> b) {
+    return (a.size() == 1 && b.size() == 1 && abs(a[0]) == max(abs(val), INT_MAX)) 
+           || (a.size() == 2 && b.size() == 2 && (a[0] == 0 && a[1] == 0) && (b[0] == 0 && b[1] == 0));
+}
 
+vector<int> largest_smallest_integers(vector<int> lst) {
+    int maxNegative = 0, minPositive = INT_MAX;
     for (int num : lst) {
-        if (num < 0 && num > maxNeg) {
-            maxNeg = num;
-        }
-        else if (num > 0 && num < minPos) {
-            minPos = num;
+        if (num < 0 && num > maxNegative) {
+            maxNegative = num;
+        } else if (num > 0 && num < minPositive) {
+            minPositive = num;
         }
     }
-
-    return {std::make_pair(maxNeg, minPos)};
+    return {(maxNegative > 0) ? maxNegative : 0, (minPositive < 1) ? minPositive : 0};
 }
