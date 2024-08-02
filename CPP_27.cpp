49f4373433d15937c0d608cc52696fac78fc51d4
beep boop <githubbot@vadim.me>
2024-08-02 14:15:15 +0200

    Solution 69 of CPP/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/4c9twe9q Prompt: 5bff623a-2bc9-4af9-8005-d91a9e4a697d.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string flip_case(string str){       | ^~~~~~ In file included from 5bff623a-2bc9-4af9-8005-d91a9e4a697d.cpp:13: 5bff623a-2bc9-4af9-8005-d91a9e4a697d.cpp: In function ‘int main()’: 5bff623a-2bc9-4af9-8005-d91a9e4a697d.cpp:15:13: error: ‘filp_case’ was not decla red in this scope    15 |     assert (filp_case("These violent delights have violent ends") == "tH ESE VIOLENT DELIGHTS HAVE VIOLENT ENDS");       |             ^~~~~~~~~

--- CPP_27.cpp
+++ CPP_27.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-#include <cctype>
 #include <string>
 
 std::string flip_case(std::string str){
     std::string result = "";
     for(int i=0; i<str.length(); i++){
-        if(std::isalpha(str[i])){
-            char c = (std::isupper(str[i])) ? tolower(str[i]) : toupper(str[i]);
-            result += c;
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            result += toupper((char)str[i]);
         } else {
-            result += str[i];
+            result += tolower((char)str[i]);
         }
     }
     return result;
