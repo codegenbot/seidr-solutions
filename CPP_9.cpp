4d92edcc6b23b69381ac60382f72f7c7ac89c66b
beep boop <githubbot@vadim.me>
2024-08-02 14:20:54 +0200

    Solution 59 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/4jevx01g Prompt: 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp:42: 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp: In function ‘int main()’: 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp:52:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    52 |     assert (issame(rolling_max({3, 2, 3, 100, 3}) ,{3, 3, 3, 100, 100})) ;       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp:8:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     8 | bool issame(const vector<int>& v1, const vector<int>& v2) {       |      ^~~~~~ 8123d637-1d4b-40ca-b3b3-c8f98710bf54.cpp:43:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    43 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -5,6 +5,32 @@
 
 using namespace std;
 
+bool issame(const vector<int>& v1, const vector<int>& v2) {
+    if (v1.size() != v2.size()) {
+        return false;
+    }
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> rolling_max(vector<int> numbers);
+
+bool issame(const vector<int>& v1, const vector<int>& v2);
+
+vector<int> rolling_max(vector<int> numbers) {
+    vector<int> result;
+    int max_num = numbers[0];
+    for (int i = 0; i < numbers.size(); i++) {
+        max_num = max(max_num, numbers[i]);
+        result.push_back(max_num);
+    }
+    return result;
+}
+
 bool issame(const vector<int>& v1, const vector<int>& v2);
 
 bool issame(const vector<int>& v1, const vector<int>& v2) {
@@ -19,12 +45,4 @@ bool issame(const vector<int>& v1, const vector<int>& v2) {
     return true;
 }
 
-vector<int> rolling_max(vector<int> numbers) {
-    vector<int> result;
-    int max_num = numbers[0];
-    for (int i = 0; i < numbers.size(); i++) {
-        max_num = max(max_num, numbers[i]);
-        result.push_back(max_num);
-    }
-    return result;
-}
+assert(issame({3, 2, 3, 100, 3}, vector<int>{3, 3, 3, 100, 100}));
