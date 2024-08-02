05202152be002206ae64880d12a14a38f8d178b8
beep boop <githubbot@vadim.me>
2024-08-02 20:56:04 +0200

    Solution 67 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/fapw0b9h Prompt: 8a38bac5-7a87-454d-9b14-87ddd3089817.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X",  "IX", "V", "IV", "I"};       | ^~~~~~ 8a38bac5-7a87-454d-9b14-87ddd3089817.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 8a38bac5-7a87-454d-9b14-87ddd3089817.cpp:15: 8a38bac5-7a87-454d-9b14-87ddd3089817.cpp: In function ‘int main()’: 8a38bac5-7a87-454d-9b14-87ddd3089817.cpp:17:13: error: ‘int_to_mini_romank’ was not declared in this scope    17 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,7 +1,8 @@
+```cpp
 #include <vector>
 #include <string>
 
-std::vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+std::vector<std::string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 int decimal[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
 std::string int_to_roman(int number) {
