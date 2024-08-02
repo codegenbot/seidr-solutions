ceff6076274401b4b1bc1b22dec5034d5a1084ec
beep boop <githubbot@vadim.me>
2024-08-02 02:10:20 +0200

    Solution 57 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/7g2ttqk5 Prompt: deb158b1-ad48-4731-9305-0eed5163f4a0.cpp:6:13: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |             ^~~~~~ deb158b1-ad48-4731-9305-0eed5163f4a0.cpp:6:20: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ deb158b1-ad48-4731-9305-0eed5163f4a0.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ deb158b1-ad48-4731-9305-0eed5163f4a0.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from deb158b1-ad48-4731-9305-0eed5163f4a0.cpp:5: deb158b1-ad48-4731-9305-0eed5163f4a0.cpp: In function ‘int main()’: deb158b1-ad48-4731-9305-0eed5163f4a0.cpp:15:20: error: ‘count_up_to’ was not dec lared in this scope    15 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,29 +1,23 @@
 #include <vector>
+#include <cmath>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-bool is_prime(int num) {
-    if (num <= 1) {
-        return false;
-    }
-
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 std::vector<int> count_up_to(int n) {
     std::vector<int> primes;
 
-    for (int i = 2; i <= n; i++) {
-        if (is_prime(i)) {
-            primes.push_back(i);
+    for (int num = 2; num <= n; ++num) {
+        bool is_prime = true;
+        for (int i = 2; i <= sqrt(num); ++i) {
+            if (num % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) {
+            primes.push_back(num);
         }
     }
 
