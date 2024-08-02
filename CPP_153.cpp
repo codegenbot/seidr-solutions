0c58f47cab73dd7534005c9c45c5049b1468ab0c
beep boop <githubbot@vadim.me>
2024-08-02 03:52:00 +0200

    Solution 12 of CPP/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/ugnxf43j Prompt: 887fba1c-15c3-4e1f-9a1b-c46c72bf3086.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 887fba1c-15c3-4e1f-9a1b-c46c72bf3086.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -6,30 +6,22 @@
 
 using namespace std;
 
-namespace Solution {
-    string Strongest_Extension(string class_name, vector<string> extensions){
-        int max_strength = INT_MIN;
-        string strongest_extension = "";
+string Strongest_Extension(string class_name, vector<string> extensions){
+    int max_strength = INT_MIN;
+    string strongest_extension = "";
 
-        for(const auto& ext : extensions){
-            int CAP = 0, SM = 0;
-            for(const auto& c : ext){
-                if(isupper(c)) CAP++;
-                if(islower(c)) SM++;
-            }
-            int strength = CAP - SM;
-            if(strength > max_strength || (strength == max_strength && ext < strongest_extension)){
-                max_strength = strength;
-                strongest_extension = ext;
-            }
+    for(const auto& ext : extensions){
+        int CAP = 0, SM = 0;
+        for(const auto& c : ext){
+            if(isupper(c)) CAP++;
+            if(islower(c)) SM++;
         }
-
-        return class_name + "." + strongest_extension;
+        int strength = CAP - SM;
+        if(strength > max_strength || (strength == max_strength && ext < strongest_extension)){
+            max_strength = strength;
+            strongest_extension = ext;
+        }
     }
-}
 
-int main() {
-    assert(Solution::Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    
-    return 0;
+    return class_name + "." + strongest_extension;
 }
