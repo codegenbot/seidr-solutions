7161e800fe28d0b114ec94242a6443f357a07af4
beep boop <githubbot@vadim.me>
2024-08-02 18:33:12 +0200

    Solution 14 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q14tckcu Prompt:  in this scope    26 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp:26:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp:26:34: error: expected primary-expressi on before ‘int’    26 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp:25: 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp: In function ‘int main()’: 0902efb4-6fa7-43b2-a51a-39e762a0d516.cpp:35:20: error: ‘pluck’ was not declared in this scope    35 |     assert (issame(pluck({7, 9, 7, 1}) , {}));       |                    ^~~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -1,8 +1,13 @@
 #include <vector>
+
+bool issame(int a, int b){
+    return a == b;
+}
+
 vector<int> pluck(vector<int> arr);
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+int main(){
+    assert (issame(pluck({7, 9, 7, 1}), {}));
 }
 
 vector<int> pluck(vector<int> arr){
