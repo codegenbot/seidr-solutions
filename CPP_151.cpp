73f8ef3932b21478927912a99634c02198f9e0a7
beep boop <githubbot@vadim.me>
2024-08-02 02:51:11 +0200

    Solution 33 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/kbltmef6 Prompt:     1 | Here's the solution:       |     ^ 8e8c7429-edad-499b-9856-6aa217b054fb.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the solution:       |     ^~~~~~~~~~~~~~~~ 8e8c7429-edad-499b-9856-6aa217b054fb.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the solution:       | ^~~~ In file included from 8e8c7429-edad-499b-9856-6aa217b054fb.cpp:13: 8e8c7429-edad-499b-9856-6aa217b054fb.cpp: In function ‘int main()’: 8e8c7429-edad-499b-9856-6aa217b054fb.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 8e8c7429-edad-499b-9856-6aa217b054fb.cpp:15:13: error: ‘double_the_difference’ w as not declared in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 8e8c7429-edad-499b-9856-6aa217b054fb.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -13,22 +13,22 @@ long long double_the_difference(vector<float> lst) {
 
 int main() {
     vector<float> lst;
-    float input;
-    
-    cout << "Enter numbers (enter 'q' to stop): ";
-    while (cin >> input && input != 'q') {
-        lst.push_back(input);
-    }
-    
+    float num;
     long long odd_sum = 0; 
-    for (float num : lst) {
-        if (num > 0 && modf(num, &num) == 0.0) {
-            odd_sum += num * num;
-        } else if (num < 0 && modf(-num, &num) == 0.0) {
-            odd_sum -= num * num;
+
+    // Read input from user
+    cout << "Enter numbers (enter -1 to stop):" << endl;
+    while ((cin >> num) && (num != -1)) {
+        lst.push_back(num);
+    }
+
+    for (float n : lst) {
+        if (n > 0 && modf(n, &n) == 0.0) {
+            odd_sum += n * n;
         }
     }
-    
-    assert(double_the_difference(lst) == odd_sum);
+
+    cout << "Double the difference is: " << double_the_difference(lst) - 2*odd_sum << endl;
+
     return 0;
 }
