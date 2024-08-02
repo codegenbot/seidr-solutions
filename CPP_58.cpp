c092e6a236719501d292d49d1e77e466a7e72a63
beep boop <githubbot@vadim.me>
2024-08-02 12:25:46 +0200

    Solution 10 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/60kqal8z Prompt:                  from /usr/include/c++/11/algorithm:74,                  from e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp:15: e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp: In function ‘int main()’: e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp:25:20: error: ‘common’ was not declared  in this scope    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ e2df5e8c-04ad-447e-9c09-d1dda2a8aca3.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -1,16 +1,26 @@
-Here is the solution:
+#include <set>
+#include <algorithm>
+#include <vector>
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
 
 vector<int> common(vector<int> l1, vector<int> l2) {
     set<int> s1(l1.begin(), l1.end());
     set<int> s2(l2.begin(), l2.end());
-    set<int> result;
-    
-    for (auto i : s1) {
-        if (s2.find(i) != s2.end()) {
-            result.insert(i);
-        }
-    }
-    
-    vector<int> res(result.begin(), result.end());
-    sort(res.begin(), res.end());
-    return res;
+
+    set<int> common;
+    set_intersection(s1.begin(), s1.end(), s2.begin(), s2.end(),
+        inserter(common, common.begin()));
+
+    return vector<int>(common.begin(), common.end());
+}
+
+int main() {
+    vector<int> l1 = {4, 3, 2, 8};
+    vector<int> l2 = {};
+
+    assert(common(l1, l2) == {});
+    return 0;
+}
