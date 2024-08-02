ee3e35cf548c89898c4bb6fe2b47ac75e4e6335f
beep boop <githubbot@vadim.me>
2024-08-02 16:33:35 +0200

    Solution 35 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/cg25toqe Prompt:       |                              ^~~~~~ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:32: 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp: In function ‘int main()’: 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ 0d9ad228-a37c-4dae-894c-242e50d4ec7d.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -1,20 +1,22 @@
 bool issame(vector<string> v1, vector<string> v2) {
-    if(v1.size() != v2.size())
-        return false;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i] != v2[i])
-            return false;
+    if (v1.size() != v2.size()) return false;
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) return false;
     }
     return true;
 }
 
 int main() {
-    vector<float> grades = {3.5, 4.0, 3.8};
+    vector<float> grades = {4.0, 3.9, 3.5, 2.8};
     vector<string> letter_grades = numerical_letter_grade(grades);
-    vector<string> expected = {"B+", "A+", "A"};
-    if(issame(letter_grades, expected))
-        cout << "Test passed";
-    else
-        cout << "Test failed";
+    
+    vector<string> expected = {"A+", "A", "A-", "B+"};
+    
+    if (issame(letter_grades, expected)) {
+        cout << "The grades are correct." << endl;
+    } else {
+        cout << "The grades are not correct." << endl;
+    }
+    
     return 0;
 }
