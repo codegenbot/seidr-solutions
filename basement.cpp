442412185a16732aeee1e83bf0dff1c685f10446
beep boop <githubbot@vadim.me>
2024-08-02 12:01:35 +0200

    Solution 26 of basement, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/j1eq37op Prompt: e2313e82-5189-4b0a-aadf-647b63469211.cpp:3:14: error: ‘vector’ was not declared in this scope     3 | int basement(vector<int> nums) {       |              ^~~~~~ e2313e82-5189-4b0a-aadf-647b63469211.cpp:3:14: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from e2313e82-5189-4b0a-aadf-647b63469211.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e2313e82-5189-4b0a-aadf-647b63469211.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e2313e82-5189-4b0a-aadf-647b63469211.cpp:3:21: error: expected primary-expressio n before ‘int’     3 | int basement(vector<int> nums) {       |                     ^~~

--- basement.cpp
+++ basement.cpp
@@ -1,8 +1,6 @@
 #include <vector>
 
-using namespace std;
-
-int basement(vector<int> nums) {
+int basement(std::vector<int> nums) {
     int sum = 0;
     for (int i = 0; i < nums.size(); ++i) {
         sum += nums[i];
