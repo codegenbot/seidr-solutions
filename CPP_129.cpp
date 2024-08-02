8f010ad6682b3677553e339943baa544f9b708a0
beep boop <githubbot@vadim.me>
2024-08-03 00:13:36 +0200

    Solution 1 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vmelxnfr Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,7 +1,3 @@
-#include <vector>
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
-    vector<int> path;
-    path.push_back(1);
-    return path;
-}
+vector<int> minPath(vector<vector<int>> grid, int k){
+        // Your code here
+    }
