8ada184379a8e9a324168efa6c3e177af101c3ce
beep boop <githubbot@vadim.me>
2024-08-02 21:00:35 +0200

    Solution 7 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/m92up48n Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,30 +1,26 @@
-#include <vector>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
 int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
-    
-    for(int i = 0; i < 4; i++) {
-        char c1 = code[i];
-        char c2 = guess[i];
-        
-        if(c1 == c2) {
+
+    for(int i=0; i<4; i++) {
+        if(code[i] == guess[i]) {
             black++;
-        } else if(find(guess.begin(), guess.end(), c1) != guess.end()) {
-            white++;
         }
     }
-    
-    return make_pair(white, black).second;
-}
 
-int main() {
-    string code, guess;
-    cin >> code >> guess;
-    cout << mastermind(code, guess) << endl;
-    return 0;
+    for(int i=0; i<6; i++) {
+        int count = 0;
+        for(int j=0; j<4; j++) {
+            if(guess[j] == (char)(i + 'A')) {
+                count++;
+            }
+        }
+        if(count == 1) {
+            black--;
+        } else if(count > 1) {
+            white += count - 1;
+        }
+    }
+
+    return white, black;
 }
