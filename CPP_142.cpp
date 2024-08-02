1f009446bc067b00ec43c9306998b70860ab56b5
beep boop <githubbot@vadim.me>
2024-08-02 02:08:10 +0200

    Solution 30 of CPP/142, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/zmgswnsu Prompt: 33108118-9f82-4e86-a533-e2d223099996.cpp:1:17: error: ‘vector’ was not declared in this scope     1 | int sum_squares(vector<int> lst) {       |                 ^~~~~~ 33108118-9f82-4e86-a533-e2d223099996.cpp:1:24: error: expected primary-expressio n before ‘int’     1 | int sum_squares(vector<int> lst) {       |                        ^~~ In file included from 33108118-9f82-4e86-a533-e2d223099996.cpp:12: 33108118-9f82-4e86-a533-e2d223099996.cpp: In function ‘int main()’: 33108118-9f82-4e86-a533-e2d223099996.cpp:14:24: error: ‘sum_squares’ cannot be u sed as a function    14 |     assert (sum_squares({-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 14, -14, 6, 13, 11, 16, 16, 4, 10}) == -1448);       |             ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_142.cpp
+++ CPP_142.cpp
@@ -1,21 +1,13 @@
-#include <iostream>
 #include <vector>
+using namespace std;
 
-int sum_squares(std::vector<int> lst){
-    int total = 0;
-    for(int i = 0; i < lst.size(); i++){
-        if(i % 3 == 0 && i % 4 != 0)
-            total += lst[i] * lst[i];
-        else if(i % 4 == 0 && i % 3 != 0)
-            total += lst[i] * lst[i] * lst[i];
+int sum_squares(vector<int> lst) {
+    int sum = 0;
+    for (int i = 0; i < lst.size(); i++) {
+        if ((i+1)%3 == 0 && (i+1)%4 != 0)
+            sum += lst[i]*lst[i];
+        else if ((i+1)%4 == 0 && (i+1)%3 != 0)
+            sum += lst[i]*lst[i]*lst[i];
     }
-    return total;
-
-}
-
-int main(){
-    std::vector<int> numbers = {-1, -3, 17, -1, -15, 13, -1, 14, -14, -12, -5, 
-                                14, -14, 6, 13, 11, 16, 16, 4, 10};
-    std::cout << sum_squares(numbers) << std::endl;
-    return 0;
+    return sum;
 }
