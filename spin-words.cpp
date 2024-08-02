f22893a62de0c0a4ab412e5d57a769ceaf330a49
beep boop <githubbot@vadim.me>
2024-08-03 00:13:36 +0200

    Solution 4 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/zjd1sfht Prompt: Given a string of one or more words (separated by spaces), reverse all of the words that are ﬁve or more letters long and return the resulting string.

--- spin-words.cpp
+++ spin-words.cpp
@@ -1,5 +1,3 @@
-Here's the solution:
-
 #include <vector>
 #include <iostream>
 #include <string>
@@ -8,37 +6,33 @@ using namespace std;
 
 string spinWords(string str) {
     string result = "";
-    int wordLen;
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == ' ') {
-            result += str.substr(i - wordLen, i);
-            wordLen = 0;
-            result += " ";
-        } else {
-            wordLen++;
-        }
-    }
-    result += str.substr(str.length() - wordLen, str.length());
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == ' ') {
-            string temp = str.substr(i - wordLen + 1, i);
-            int j = temp.length();
-            while (j > 4) {
-                temp = temp.substr(1, temp.length() - 2) + temp[0] + temp[temp.length() - 1];
-                j = temp.length();
-            }
-            result += temp;
-        } else {
-            result += str[i];
+    int start = 0;
+    for(int i=0; i<=str.length(); i++) {
+        if(i == str.length() || str[i] == ' ') {
+            string word = str.substr(start, i-start);
+            if(word.length() >= 5)
+                result += reverse(word) + " ";
+            else
+                result += word + " ";
+            start = i+1;
         }
     }
     return result;
 }
 
+string reverse(string s) {
+    string reversed = "";
+    for(int i=s.length()-1; i>=0; i--)
+        reversed += s[i];
+    return reversed;
+}
+
 int main() {
-    string input;
-    cout << "Enter a string: ";
-    getline(cin, input);
-    cout << "Output: " << spinWords(input) << endl;
+    // test cases
+    cout << spinWords("a") << endl;  // a
+    cout << spinWords("this is a test") << endl;  // this is a test
+    cout << spinWords("this is another test") << endl;  // this is rehtona test
+    cout << spinWords("hi") << endl;  // hi
+
     return 0;
 }
