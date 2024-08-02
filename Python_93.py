f2e17b80e7a42172bfec0f9d07a63f0f5c8aa29c
beep boop <githubbot@vadim.me>
2024-08-02 03:54:37 +0200

    Solution 4 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/fndn25hh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/729848f1-3fc2-4a97-a609-366f7f57f 1fa.py", line 23, in <module>     check(encode)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/729848f1-3fc2-4a97-a609-366f7f57f 1fa.py", line 19, in check     encode("I DoNt KnOw WhAt tO WrItE") == "k dQnT kNqW wHcT Tq wRkTg" AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_93.py
+++ Python_93.py
@@ -1,12 +1,10 @@
 def encode(message):
     encoded_message = ""
-    vowels = "aeiouAEIOU"
+
     for char in message:
         if char.isalpha():
-            if char.lower() in vowels:
-                encoded_message += chr(ord(char) + 2)
-            else:
-                encoded_message += char.swapcase()
+            encoded_message += chr(ord(char) + 2).upper()
         else:
             encoded_message += char
+
     return encoded_message
