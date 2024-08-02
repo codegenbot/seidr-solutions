3972e267fe7ff6be4290f1b16e4d4bfea89bbfe8
beep boop <githubbot@vadim.me>
2024-08-02 16:40:58 +0200

    Solution 92 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/d5dwcpzk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/164cfb3f-efbd-4e1d-be7e-1a7193c6c 85f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -16,9 +16,5 @@ def factorize(n: int) -> List[int]:
                 factors.append(i)
             n //= i**count
     if n > 1:
-        factors.extend([n] * 1)
+        factors.append(n)
     return factors
-
-
-num = int(input("Enter a number: "))
-print("Prime factors are:", factorize(num))
