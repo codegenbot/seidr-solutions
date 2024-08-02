b432b2b4cbb3b0bf8c80d388b077bd714750595d
beep boop <githubbot@vadim.me>
2024-08-02 03:21:45 +0200

    Solution 55 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/we5a8kom Prompt: 511f716a-18a5-4cfd-bfb9-f9789491cd6a.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X",  "IX", "V", "IV", "I"};       | ^~~~~~ 511f716a-18a5-4cfd-bfb9-f9789491cd6a.cpp:4:1: error: ‘string’ does not name a ty pe     4 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 511f716a-18a5-4cfd-bfb9-f9789491cd6a.cpp:14: 511f716a-18a5-4cfd-bfb9-f9789491cd6a.cpp: In function ‘int main()’: 511f716a-18a5-4cfd-bfb9-f9789491cd6a.cpp:16:13: error: ‘int_to_mini_romank’ was not declared in this scope    16 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,11 +1,11 @@
 #include <vector>
 #include <string>
 
-vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+std::vector<std::string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-string int_to_mini_roman(int number) {
-    string result;
+std::string int_to_mini_roman(int number) {
+    std::string result;
     for (int i = 0; i < 13; ++i)
         while (number >= val[i]) {
             number -= val[i];
