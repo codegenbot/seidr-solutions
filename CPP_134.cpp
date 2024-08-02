09b9e5ee35c995e3800b8e78d7f736c847e2fffd
beep boop <githubbot@vadim.me>
2024-08-02 04:46:48 +0200

    Solution 7 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/snj066rj Prompt: c536c0ac-4951-4ff9-bd49-fe6f53dc58d3.cpp:1:1: error: expected unqualified-id bef ore ‘return’     1 | return !isalpha(txt.back()) && txt.size() > 0 && !isalpha(txt[txt.size()  - 2]);       | ^~~~~~ In file included from c536c0ac-4951-4ff9-bd49-fe6f53dc58d3.cpp:3: c536c0ac-4951-4ff9-bd49-fe6f53dc58d3.cpp: In function ‘int main()’: c536c0ac-4951-4ff9-bd49-fe6f53dc58d3.cpp:5:13: error: ‘check_if_last_char_is_a_l etter’ was not declared in this scope     5 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-
 bool check_if_last_char_is_a_letter(const std::string& txt) {
     return !isalpha(txt.back()) && txt.size() > 0 && !isalpha(txt[txt.size() - 2]);
 }
