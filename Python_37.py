859b09ec0956168301f9b3f365baf3a6051f9352
beep boop <githubbot@vadim.me>
2024-08-02 23:25:19 +0200

    Solution 93 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/sp1pxf3a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05315036-a066-4f9d-a9e4-1d8940703 f68.py", line 12, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05315036-a066-4f9d-a9e4-1d8940703 f68.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05315036-a066-4f9d-a9e4-1d8940703 f68.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/05315036-a066-4f9d-a9e4-1d8940703 f68.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if i % 2 == 0])[j]                                                               ^ NameError: name 'j' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,4 +1,3 @@
-```Python
 def sort_even(l: list):
     return [sorted([y for y in l if i % 2 == 0])[i//2] if i % 2 != 0 else x 
             for i, x in enumerate(sorted([y for y in l if i % 2 == 0]))]
