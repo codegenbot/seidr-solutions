df3b98e2cf363363af6fbb013eb9a2f3af2d3494
beep boop <githubbot@vadim.me>
2024-08-02 20:57:09 +0200

    Solution 9 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/67h06wxg Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,24 +1,16 @@
-int do_algebra(vector<string> operator_, vector<int> operand) {
+int do_algebra(vector<string> operato, vector<int> operand) {
     int result = operand[0];
-    for (int i = 0; i < operator_.size(); i++) {
-        if (operator_[i] == "+") {
+    for (int i = 0; i < operato.size(); i++) {
+        if (operato[i] == "+") {
             result += operand[i + 1];
-        } else if (operator_[i] == "-") {
+        } else if (operato[i] == "-") {
             result -= operand[i + 1];
-        } else if (operator_[i] == "*") {
-            int temp = 0;
-            for (int j = 0; j <= i; j++) {
-                temp *= operand[j];
-            }
-            result = temp;
-        } else if (operator_[i] == "//") {
+        } else if (operato[i] == "*") {
+            result *= operand[i + 1];
+        } else if (operato[i] == "//") {
             result /= operand[i + 1];
-        } else if (operator_[i] == "**") {
-            int temp = 1;
-            for (int j = 0; j <= i; j++) {
-                temp *= operand[j];
-            }
-            result = pow(temp, operand[i + 1]);
+        } else if (operato[i] == "**") {
+            result = pow(result, operand[i + 1]);
         }
     }
     return result;
