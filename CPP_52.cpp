9faf2cb0ad0f825cfb94d271cd0d07990b085906
beep boop <githubbot@vadim.me>
2024-08-02 15:14:04 +0200

    Solution 14 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/gx0jzk95 Prompt: f0c62f2a-8569-458e-974b-dfd3d020db34.cpp:1:22: error: ‘vector’ was not declared in this scope     1 | bool below_threshold(vector<int> l, int t) {       |                      ^~~~~~ f0c62f2a-8569-458e-974b-dfd3d020db34.cpp:1:29: error: expected primary-expressio n before ‘int’     1 | bool below_threshold(vector<int> l, int t) {       |                             ^~~ f0c62f2a-8569-458e-974b-dfd3d020db34.cpp:1:37: error: expected primary-expressio n before ‘int’     1 | bool below_threshold(vector<int> l, int t) {       |                                     ^~~ f0c62f2a-8569-458e-974b-dfd3d020db34.cpp:1:42: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool below_threshold(vector<int> l, int t) {       |                                          ^ In file included from f0c62f2a-8569-458e-974b-dfd3d020db34.cpp:10: f0c62f2a-8569-458e-974b-dfd3d020db34.cpp: In function ‘int main()’: f0c62f2a-8569-458e-974b-dfd3d020db34.cpp:12:32: error: ‘below_threshold’ cannot be used as a function    12 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -3,8 +3,8 @@
 int below_threshold(vector<int> l, int t) {
     for (int i : l) {
         if (i >= t) {
-            return 0; // Return 0 if there is a number in the vector that is greater than or equal to the threshold
+            return 0;
         }
     }
-    return 1; // Return 1 if all numbers in the vector are less than the threshold
+    return 1;
 }
