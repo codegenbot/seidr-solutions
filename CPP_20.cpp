09bef96c3bd9756e9b2d3da4a53bbc3fe6fb25b0
beep boop <githubbot@vadim.me>
2024-08-02 23:49:56 +0200

    Solution 59 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/g6j2uorj Prompt:    36 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from e7a2b043-c2a0-46d0-817a-59a177cc7b6c.cpp:35: e7a2b043-c2a0-46d0-817a-59a177cc7b6c.cpp: In function ‘int main()’: e7a2b043-c2a0-46d0-817a-59a177cc7b6c.cpp:45:41: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    45 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from e7a2b043-c2a0-46d0-817a-59a177cc7b6c.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = float; _Alloc = std::allocator<float>; std::vector<_Tp, _Al loc>::allocator_type = std::allocator<float>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ e7a2b043-c2a0-46d0-817a-59a177cc7b6c.cpp:14:67: note:   initializing argument 1 of ‘std::pair<float, float> find_closest_elements(std::vector<float>&)’    14 | pair<float, float> find_closest_elements(std::vector<float>& numbers) {       |                                          ~~~~~~~~~~~~~~~~~~~~^~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -8,28 +8,29 @@
 bool issame(const float a, const float b);
 
 bool issame(const float a, const float b) {
-    return fabs(a - b) < std::numeric_limits<float>::epsilon();
+    return std::fabs(a - b) < std::numeric_limits<float>::epsilon();
 }
 
 std::pair<float, float> find_closest_elements(const std::vector<float>& numbers) {
-    std::vector<float> sorted_numbers = numbers;
-    std::sort(sorted_numbers.begin(), sorted_numbers.end());
-    float min_diff = sorted_numbers[1] - sorted_numbers[0];
-    float num1 = sorted_numbers[0], num2 = sorted_numbers[1];
-    for (size_t i = 1; i < sorted_numbers.size() - 1; ++i) {
-        if (sorted_numbers[i + 1] - sorted_numbers[i] < min_diff) {
-            min_diff = sorted_numbers[i + 1] - sorted_numbers[i];
-            num1 = sorted_numbers[i];
-            num2 = sorted_numbers[i + 1];
-        } else if (issame(sorted_numbers[i + 1] - sorted_numbers[i], min_diff)) {
-            if (sorted_numbers[i] < num1) {
-                num1 = sorted_numbers[i];
-                num2 = sorted_numbers[i + 1];
+    std::vector<float> temp_numbers = numbers;
+    std::sort(temp_numbers.begin(), temp_numbers.end());
+    float min_diff = temp_numbers[1] - temp_numbers[0];
+    float num1 = temp_numbers[0], num2 = temp_numbers[1];
+    
+    for (int i = 1; i < temp_numbers.size() - 1; ++i) {
+        if (temp_numbers[i + 1] - temp_numbers[i] < min_diff) {
+            min_diff = temp_numbers[i + 1] - temp_numbers[i];
+            num1 = temp_numbers[i];
+            num2 = temp_numbers[i + 1];
+        } else if (issame(temp_numbers[i + 1], temp_numbers[i])) {
+            if (temp_numbers[i] < num1) {
+                num1 = temp_numbers[i];
+                num2 = temp_numbers[i + 1];
             }
         }
     }
+    
     return {num1, num2};
 }
 
-assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}).first, 2.2) && 
-       issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}).second, 3.1));
+assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}).first, 2.2) && issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}).second, 3.1));
