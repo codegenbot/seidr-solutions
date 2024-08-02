fdaf4f7cc0819f99705a6a8c8dedd1a23a574918
beep boop <githubbot@vadim.me>
2024-08-02 03:27:05 +0200

    Solution 2 of CPP/160, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run2/runs/2urc90yk Prompt: Complete the following code given the task description and function signature.

--- CPP_160.cpp
+++ CPP_160.cpp
@@ -1,30 +1,50 @@
 int do_algebra(vector<string> operator_, vector<int> operand) {
-    int result = 0;
-    string expression = "";
-    
+    string expr = "";
     for (int i = 0; i < operator_.size(); i++) {
-        if (i == 0) {
-            expression += to_string(operand[i]);
-        } else {
-            expression += " " + operator_[i] + " ";
-            expression += to_string(operand[i]);
+        expr += to_string(operand[i]);
+        expr += operator_[i];
+    }
+    expr += to_string(operand.back());
+    
+    int result = eval(expr.c_str());
+    
+    return result;
+}
+
+long long eval(const char *s) {
+    int n = strlen(s);
+    long long res = 0;
+    long long a = 0, b = 1;
+    int i = 0;
+    while (i < n) {
+        if (isdigit(s[i])) {
+            long long c = 0;
+            while (i < n && isdigit(s[i]))
+                c = c * 10 + s[i++] - '0';
+            a += c * b;
+        } else if (s[i] == ' ') {
+            i++;
+            continue;
+        } else if (s[i] in "+-*/") {
+            res += a;
+            a = 0;
+            switch (s[i++]) {
+                case '+':
+                    break;
+                case '-':
+                    b = -b;
+                    break;
+                case '*':
+                    b *= a;
+                    a = 0;
+                    break;
+                case '/':
+                    b = div(a, b);
+                    a = 0;
+                    break;
+            }
         }
     }
-    
-    int temp = operand[0];
-    for (int i = 1; i < operand.size(); i++) {
-        if (operator_[i-1] == "+") {
-            temp += operand[i];
-        } else if (operator_[i-1] == "-") {
-            temp -= operand[i];
-        } else if (operator_[i-1] == "*") {
-            temp *= operand[i];
-        } else if (operator_[i-1] == "//") {
-            temp = temp / operand[i];
-        } else if (operator_[i-1] == "**") {
-            temp = pow(temp, operand[i]);
-        }
-    }
-    
-    return temp;
+    res += a;
+    return res;
 }
