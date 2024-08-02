4db4c547ecacc105fdb155b27aba83068a5589e8
beep boop <githubbot@vadim.me>
2024-08-02 14:52:19 +0200

    Solution 65 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/f7cizk1g Prompt:    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:9: 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp: In function ‘int main()’: 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:19:20: error: ‘unique’ was not declared  in this scope    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 9a37931e-9aa9-4760-a8c8-8220f791cbb5.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,19 +1,23 @@
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++){
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
 
-vector<int> unique(vector<int> l) {
+vector<int> unique(vector<int> l){
     vector<int> result(l.begin(),l.end());
     sort(result.begin(),result.end());
     auto it = unique(result.begin(),result.end());
-    result.erase(it,result.end());
+    while(it != result.end()){
+        int count = 1;
+        for(auto i = it + 1; i < result.end(); i++){
+            if(*i == *it) count++;
+            else break;
+        }
+        if(count == 1) it = result.erase(it, i);
+        else it++;
+    }
     return result;
 }
