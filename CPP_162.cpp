327429f3a6d3c8cd3c0786a2ff55ac1331bb4eb5
beep boop <githubbot@vadim.me>
2024-08-02 18:49:36 +0200

    Solution 85 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/zkkoc4d1 Prompt: 134e7e62-efbd-4d06-b49b-14a0012b0bdd.cpp: In function ‘std::string string_to_md5 (std::string)’: 134e7e62-efbd-4d06-b49b-14a0012b0bdd.cpp:10:16: error: aggregate ‘EVP_MD_CTX md_ ctx’ has incomplete type and cannot be defined    10 |     EVP_MD_CTX md_ctx;       |                ^~~~~~ 134e7e62-efbd-4d06-b49b-14a0012b0bdd.cpp:21:22: error: ‘setfill’ was not declare d in this scope    21 |         ss << hex << setfill('0') << setw(2) << (int)hash[i];       |                      ^~~~~~~ 134e7e62-efbd-4d06-b49b-14a0012b0bdd.cpp:21:38: error: ‘setw’ was not declared i n this scope; did you mean ‘getw’?    21 |         ss << hex << setfill('0') << setw(2) << (int)hash[i];       |                                      ^~~~       |                                      getw

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,35 +8,18 @@ using namespace std;
 string string_to_md5(string text) {
     if (text.empty()) return "";
 
-    EVP_MD_CTX* md_ctx;
+    EVP_MD_CTX* md_ctx = EVP_MD_CTX_new();
     unsigned char hash[16];
     unsigned int hash_len = 0;
 
-    if (!(md_ctx = EVP_MD_CTX_new())) {
-        return "";
-    }
-
-    if (!EVP_DigestInit_ex(md_ctx, EVP_md5(), NULL)) {
-        EVP_MD_CTX_free(md_ctx);
-        return "";
-    }
-    
-    if (!EVP_DigestUpdate(md_ctx, text.c_str(), text.size())) {
-        EVP_MD_CTX_free(md_ctx);
-        return "";
-    }
-
-    if (!EVP_DigestFinal_ex(md_ctx, hash, &hash_len)) {
-        EVP_MD_CTX_free(md_ctx);
-        return "";
-    }
+    EVP_DigestInit_ex(md_ctx, EVP_md5(), NULL);
+    EVP_DigestUpdate(md_ctx, text.c_str(), text.size());
+    EVP_DigestFinal_ex(md_ctx, hash, &hash_len);
 
     stringstream ss;
     for (int i = 0; i < 16; i++) {
         ss << hex << setfill('0') << setw(2) << (int)hash[i];
     }
-
-    EVP_MD_CTX_free(md_ctx);
 
     return ss.str();
 }
