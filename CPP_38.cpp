5f8bf2ff9fa4102cf575bb764aaeb44bc94a3a4a
beep boop <githubbot@vadim.me>
2024-08-02 14:39:39 +0200

    Solution 2 of CPP/38, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/rqvl3dje Prompt: Complete the following code given the task description and function signature.

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,41 +1,33 @@
-#include <iostream>
-#include <string>
+#include<stdio.h>
+#include<string>
 using namespace std;
 
-string encode_cyclic(string s) {
-    int l = s.length();
-    int num = (l + 2) / 3;
-    string x, output;
+string encode_cyclic(string s){ 
+    int l=s.length();
+    int num=(l+2)/3;
+    string x,output;
     int i;
-    for (i = 0; i * 3 < l; i++) {
-        x = s.substr(i * 3, 3);
-        if (x.length() == 3) x = x.substr(1) + x[0];
+    for (i=0;i*3<l;i++)
+    {
+        x=s.substr(i*3,3);
+        if (x.length()==3) x=x.substr(1)+x[0];
+        output=output+x;
+    }
+    return output;
+}
+
+string decode_cyclic(string s){ 
+    int l=s.length();
+    int num=(l+2)/3;
+    string x,output;
+    int i;
+    for (i=0;i<num;i++)
+    {
+        if (s.length() > 1) x = s.substr(0, 2);
+        else x = s.substr(0, 1);
         output += x;
+        if (i < num - 1) s = s.substr(1);
+        else s = "";
     }
     return output;
-}
-
-string decode_cyclic(string s) {
-    int l = s.length();
-    int num = (l + 2) / 3;
-    string x, output;
-    int i;
-    for (i = 0; i < num; i++) {
-        if (i == num - 1 && l % 3 != 0)
-            output += s.substr(i * 3);
-        else
-            output += s.substr(i * 3, 3).substr(1) + s.substr(i * 3, 3)[0];
-    }
-    return output;
-}
-
-int main() {
-    string str;
-    cout << "Enter a string: ";
-    getline(cin, str);
-    string encoded = encode_cyclic(str);
-    cout << "Encoded string: " << encoded << endl;
-    string decoded = decode_cyclic(encoded);
-    cout << "Decoded string: " << decoded << endl;
-    return 0;
 }
