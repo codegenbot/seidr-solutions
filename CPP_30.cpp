c58d1f29a8c379097f83e0dd975b6441caf879bd
beep boop <githubbot@vadim.me>
2024-08-03 00:25:14 +0200

    Solution 14 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/sl03dx7r Prompt: 3ecc811d-c702-429d-a988-41f4cd801844.cpp:27:6: error: redefinition of ‘bool issa me(std::vector<float>, std::vector<float>)’    27 | bool issame(vector<float> a,vector<float>b){       |      ^~~~~~ 3ecc811d-c702-429d-a988-41f4cd801844.cpp:22:6: note: ‘bool issame(std::vector<fl oat>, std::vector<float>)’ previously defined here    22 | bool issame(vector<float> a,vector<float> b){       |      ^~~~~~ 3ecc811d-c702-429d-a988-41f4cd801844.cpp: In function ‘bool issame(std::vector<f loat>, std::vector<float>)’: 3ecc811d-c702-429d-a988-41f4cd801844.cpp:31:13: error: ‘abs’ was not declared in  this scope    31 |         if (abs(a[i]-b[i])>1e-4) return false;       |             ^~~ 3ecc811d-c702-429d-a988-41f4cd801844.cpp: At global scope: 3ecc811d-c702-429d-a988-41f4cd801844.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ 3ecc811d-c702-429d-a988-41f4cd801844.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main(){       |     ^~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,10 +1,9 @@
 #include <vector>
 #include <cmath>
-#include <assert.h>
 
 using namespace std;
 
-vector<float> get_positive(vector<float> l) {
+vector<float> get_positive(vector<float> l){
     vector<float> result;
     for (float num : l) {
         if (num > 0) {
@@ -14,18 +13,6 @@ vector<float> get_positive(vector<float> l) {
     return result;
 }
 
-bool issame(vector<float> a, vector<float> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (abs(a[i] - b[i]) > 1e-4) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int main() {
-    assert(issame(get_positive({}), {}));
+int main(){
+    get_positive({}) == {};
 }
