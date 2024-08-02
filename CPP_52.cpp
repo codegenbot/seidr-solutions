306d6cb694b12be8b599a0e6442565816d72da36
beep boop <githubbot@vadim.me>
2024-08-03 01:53:51 +0200

    Solution 6 of CPP/52, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/6hat6n3s Prompt: a6f3c5be-70c8-423d-bea9-7fe4d1a69973.cpp:1:1: error: expected unqualified-id bef ore ‘for’     1 | for (int num : l) {       | ^~~ a6f3c5be-70c8-423d-bea9-7fe4d1a69973.cpp:6:1: error: expected unqualified-id bef ore ‘return’     6 | return true;       | ^~~~~~ In file included from a6f3c5be-70c8-423d-bea9-7fe4d1a69973.cpp:8: a6f3c5be-70c8-423d-bea9-7fe4d1a69973.cpp: In function ‘int main()’: a6f3c5be-70c8-423d-bea9-7fe4d1a69973.cpp:10:17: error: ‘below_threshold’ was not  declared in this scope    10 |     assert (not(below_threshold({1, 8, 4, 10}, 10)));       |                 ^~~~~~~~~~~~~~~

--- CPP_52.cpp
+++ CPP_52.cpp
@@ -1,8 +1,8 @@
-bool below_threshold(const vector<int>& l, int t){
-    for(int num : l){
-        if(num >= t){
-            return false;
+bool below_threshold(const std::vector<int>& l, int t) {
+    for (int num : l) {
+        if (num <= t) {
+            return true;
         }
     }
-    return true;
+    return false;
 }
