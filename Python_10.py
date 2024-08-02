fe5a3f6a7f163e13221eb7b6cec38a3bb640bae9
beep boop <githubbot@vadim.me>
2024-08-02 11:22:07 +0200

    Solution 23 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/odummyid Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dd897db-2c3b-4ab8-a88a-ae6f1bb96 43d.py", line 24, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5dd897db-2c3b-4ab8-a88a-ae6f1bb96 43d.py", line 21, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -1,31 +1,17 @@
-def is_palindrome(string: str) -> bool:
-    return string == string[::-1]
-
-
 def make_palindrome(string: str) -> str:
     if string.islower():
         for i in range(len(string)):
-            lowercase_postfix = string[i:].lower()
+            postfix = string[i:]
+            lowercase_postfix = postfix.lower()
             if is_palindrome(lowercase_postfix):
-                prefix_chars = [
-                    char.upper() if char.isalpha() and char.isupper() else char
-                    for char in string[:i]
-                ]
-                return (
-                    "".join(prefix_chars)
-                    + "#" * (len(string) - len(lowercase_postfix))
-                    + lowercase_postfix[::-1].capitalize()
-                )
+                prefix_chars = [char.upper() if char.isalpha() and char.isupper() else char for char in string[:i]]
+                return "".join(prefix_chars) + "#" * (len(string) - len(postfix)) + lowercase_postfix[::-1].capitalize()
     else:
         for i in range(len(string)):
-            lowercase_postfix = string[i:].lower()
-            if is_palindrome(lowercase_postfix):
-                prefix_chars = [
-                    char.upper() if char.isalpha() and char.isupper() else char
-                    for char in string[:i]
-                ]
+            postfix = string[i:].lower()
+            if is_palindrome(postfix):
                 return (
-                    "".join(prefix_chars)
-                    + "#" * (len(string) - len(lowercase_postfix))
-                    + lowercase_postfix[::-1].capitalize()
+                    string[0].upper() 
+                    + "#" * (len(string) - len(postfix))
+                    + postfix[::-1].capitalize()
                 )
