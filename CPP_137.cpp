d5b8d13acb3e33491dc1193c5e2844f07302ecca
beep boop <githubbot@vadim.me>
2024-08-02 16:48:57 +0200

    Solution 12 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/veoq9x4w Prompt: 81772400-8687-4c72-af10-b0f4fe6e6eee.cpp:1:1: error: ‘boost’ does not name a typ e     1 | boost::any compare_one(boost::any a, boost::any b) {       | ^~~~~ In file included from 81772400-8687-4c72-af10-b0f4fe6e6eee.cpp:30: 81772400-8687-4c72-af10-b0f4fe6e6eee.cpp: In function ‘int main()’: 81772400-8687-4c72-af10-b0f4fe6e6eee.cpp:32:13: error: ‘boost’ has not been decl ared    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 81772400-8687-4c72-af10-b0f4fe6e6eee.cpp:32:29: error: ‘string’ was not declared  in this scope    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 81772400-8687-4c72-af10-b0f4fe6e6eee.cpp:32:37: error: ‘compare_one’ was not dec lared in this scope    32 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                                     ^~~~~~~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -9,7 +9,7 @@ boost::any compare_one(boost::any a, boost::any b) {
         return (float)a > b.convert_to<float>() ? a : boost::any("None");
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        return boost::any(b.convert_to<string>() > a.convert_to<string>() ? b : (b.convert_to<string>() == a.convert_to<string>() ? "None" : b));
+        return b.convert_to<string>() > a.convert_to<string>() ? b : (b.convert_to<string>() == a.convert_to<string>() ? boost::any("") : b);
     }
     else if (a.type() == typeid(int) && b.type() == typeid(string)) {
         float fa = (float)a.convert_to<int>();
@@ -17,15 +17,15 @@ boost::any compare_one(boost::any a, boost::any b) {
     }
     else if (a.type() == typeid(string) && b.type() == typeid(int)) {
         float fb = std::stof(a.convert_to<string>());
-        return fb > b.convert_to<int>() ? boost::any(a) : "None";
+        return fb > b.convert_to<int>() ? boost::any(a) : boost::any("");
     }
     else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         float fa = a.convert_to<float>();
-        return fa > std::stof(b.convert_to<string>()) ? a : "None";
+        return fa > std::stof(b.convert_to<string>()) ? a : boost::any("");
     }
     else if (a.type() == typeid(string) && b.type() == typeid(float)) {
         float fb = std::stof(a.convert_to<string>());
-        return fb > a.convert_to<float>() ? a : "None";
+        return fb > a.convert_to<float>() ? a : boost::any("");
     }
-    return "None";
+    return boost::any("");
 }
