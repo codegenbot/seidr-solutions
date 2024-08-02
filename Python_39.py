e828ce664f82ef4a35abeed45f8e196b47e1806f
beep boop <githubbot@vadim.me>
2024-08-02 22:18:36 +0200

    Solution 96 of Python/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/s9dn16qi Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f423922-b49c-4d42-87bf-a52fa235f de8.py", line 21, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/2f423922-b49c-4d42-87bf-a52fa235f de8.py", line 18, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^ TypeError: prime_fib() takes 0 positional arguments but 1 was given

--- Python_39.py
+++ Python_39.py
@@ -8,7 +8,7 @@ def prime_fib(n):
     def is_prime(num):
         if num <= 1:
             return False
-        for i in range(2, int(num ** 0.5) + 1):
+        for i in range(2, int(num**0.5) + 1):
             if num % i == 0:
                 return False
         return True
