e0b44fce43e08e20acc9e756f412f86d5b588adc
beep boop <githubbot@vadim.me>
2024-08-02 10:37:34 +0200

    Solution 45 of Python/4, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/qxygn19a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c042390a-9f9f-4139-beb6-68816b4b2 0ad.py", line 1, in <module>     def mean_absolute_deviation(numbers: List[float]) -> float:                                          ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_4.py
+++ Python_4.py
@@ -1,5 +1,8 @@
 from typing import List
 
+
 def mean_absolute_deviation(numbers: List[float]) -> float:
-    mean = sum(numbers) / len(numbers)
+    from statistics import mean as stat_mean
+
+    mean = stat_mean(numbers)
     return sum(abs(n - mean) for n in numbers) / len(numbers)
