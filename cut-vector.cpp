a4f5b50fc986cadf8d75db10958988b0490168da
beep boop <githubbot@vadim.me>
2024-08-02 17:17:45 +0200

    Solution 30 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/6ztkkij2 Prompt:   +++ |+#include <ostream>     2 | using namespace std; 2c49e760-3465-46c1-9c72-5862767eceb6.cpp:30:29: error: ‘begin’ was not declared in this scope; did you mean ‘std::begin’?    30 |     for (auto num : result[1]) {       |                             ^       |                             std::begin In file included from /usr/include/c++/11/vector:69,                  from 2c49e760-3465-46c1-9c72-5862767eceb6.cpp:1: /usr/include/c++/11/bits/range_access.h:108:37: note: ‘std::begin’ declared here   108 |   template<typename _Tp> const _Tp* begin(const valarray<_Tp>&) noexcept ;       |                                     ^~~~~ 2c49e760-3465-46c1-9c72-5862767eceb6.cpp:30:29: error: ‘end’ was not declared in  this scope; did you mean ‘std::end’?    30 |     for (auto num : result[1]) {       |                             ^       |                             std::end In file included from /usr/include/c++/11/vector:69,                  from 2c49e760-3465-46c1-9c72-5862767eceb6.cpp:1: /usr/include/c++/11/bits/range_access.h:110:37: note: ‘std::end’ declared here   110 |   template<typename _Tp> const _Tp* end(const valarray<_Tp>&) noexcept;       |                                     ^~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -23,12 +23,12 @@ int main() {
         cin >> nums[i];
     }
     vector<int> result = cutVector(nums);
-    for (auto num : std::vector<int>(result[0]).begin(), e = std::vector<int>(result[0]).end(); num != e; ++num) {
-        cout << *num << " ";
+    for (auto it = std::vector<int>(result[0]).begin(), e = std::vector<int>(result[0]).end(); it != e; ++it) {
+        cout << *it << " ";
     }
     cout << endl;
-    for (auto num : std::vector<int>(result[1]).begin(), e = std::vector<int>(result[1]).end(); num != e; ++num) {
-        cout << *num << " ";
+    for (auto it = std::vector<int>(result[1]).begin(), e = std::vector<int>(result[1]).end(); it != e; ++it) {
+        cout << *it << " ";
     }
     cout << endl;
     return 0;
