3bcc71f8a13edd1ff396eb43399cbc2de756afde
beep boop <githubbot@vadim.me>
2024-08-02 03:23:41 +0200

    Solution 11 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run2/runs/dixa7oae Prompt: ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp:17:28: error: ‘a’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                            ^ ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp:17:30: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp:17:37: error: ‘string’ was not declared  in this scope    17 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp:17:44: error: ‘b’ was not declared in t his scope    17 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp:16: ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp: In function ‘int main()’: ce2cd09b-8b88-426b-81d4-98eabcf62391.cpp:26:20: error: ‘sorted_list_sum’ was not  declared in this scope    26 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
 bool issame(const string& a, const string& b){
     return a.length() == b.length();
 }
@@ -11,4 +18,22 @@ vector<string> sorted_list_sum(vector<string>& lst){
         return a.length() < b.length();
     });
     return lst;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> lst(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> lst[i];
+    }
+    
+    vector<string> result = sorted_list_sum(lst);
+    
+    for (const auto& s : result) {
+        cout << s << endl;
+    }
+    
+    return 0;
 }
