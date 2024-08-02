e1bd30776cc7a9f7cb548082d1f1e3560b60797a
beep boop <githubbot@vadim.me>
2024-08-02 20:24:44 +0200

    Solution 16 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run1/runs/v58dfba7 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 141f4809-3820-4762-9f39-5215c3b75413.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 141f4809-3820-4762-9f39-5215c3b75413.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from 141f4809-3820-4762-9f39-5215c3b75413.cpp:10: 141f4809-3820-4762-9f39-5215c3b75413.cpp: In function ‘int main()’: 141f4809-3820-4762-9f39-5215c3b75413.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 141f4809-3820-4762-9f39-5215c3b75413.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
-vector<string> filter_by_substring(vector<string> words, string substring) {
+vector<string> filter_by_substring(const vector<string>& words, const string& substring) {
     vector<string> filtered_words;
-    for (const auto& word : words) {
-        if (word.find(substring) != string::npos) {
+    
+    for(const string& word : words) {
+        if(word.find(substring) != string::npos) {
             filtered_words.push_back(word);
         }
     }
+    
     return filtered_words;
+}
+
+bool issame(const vector<string>& a, const vector<string>& b) {
+    return a == b;
 }
 
 int main() {
     assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
+    
     return 0;
 }
