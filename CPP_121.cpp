ff054ee825932e851ca0f9c1069062ff47da2337
beep boop <githubbot@vadim.me>
2024-08-02 19:01:48 +0200

    Solution 54 of CPP/121, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/e5k6xtbt Prompt: 6bafa9dc-ce4c-480f-9e5d-c278f46c4b4d.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int solutions(vector<int> lst) {       |               ^~~~~~ 6bafa9dc-ce4c-480f-9e5d-c278f46c4b4d.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int solutions(vector<int> lst) {       |                      ^~~ In file included from 6bafa9dc-ce4c-480f-9e5d-c278f46c4b4d.cpp:11: 6bafa9dc-ce4c-480f-9e5d-c278f46c4b4d.cpp: In function ‘int main()’: 6bafa9dc-ce4c-480f-9e5d-c278f46c4b4d.cpp:13:22: error: ‘solutions’ cannot be use d as a function    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,7 +1,9 @@
-int solution(vector<int> lst) {
+#include <vector>
+
+int solutions(std::vector<int> lst) {
     int sum = 0;
-    for (int i = 1; i < lst.size(); i++) {
-        if (lst[i] % 2 != 0) {
+    for(int i = 1; i < lst.size(); i += 2) {
+        if(lst[i] % 2 != 0) {
             sum += lst[i];
         }
     }
