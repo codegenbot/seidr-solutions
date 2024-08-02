b8ee897f817e1e2d0b87f18e5010e927ec8a0cfe
beep boop <githubbot@vadim.me>
2024-08-03 01:07:49 +0200

    Solution 24 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/dzpwy5w8 Prompt: 188300d0-e150-4fe6-8766-eb2e0776c0b4.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string anti_shuffle(string s) {       | ^~~~~~ In file included from 188300d0-e150-4fe6-8766-eb2e0776c0b4.cpp:20: 188300d0-e150-4fe6-8766-eb2e0776c0b4.cpp: In function ‘int main()’: 188300d0-e150-4fe6-8766-eb2e0776c0b4.cpp:22:13: error: ‘anti_shuffle’ was not de clared in this scope    22 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,12 +1,12 @@
 #include <string>
 
-std::string anti_shuffle(std::string s) {
-    std::string result = "";
+string anti_shuffle(string s) {
+    string result = "";
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == ' ') {
             result += " ";
         } else {
-            std::string word = "";
+            string word = "";
             while (i < s.length() && s[i] != ' ') {
                 word += s[i];
                 i++;
