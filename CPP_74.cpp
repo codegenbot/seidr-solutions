4b1549aa61234a8dc2eef692fcf665cb1d428a50
beep boop <githubbot@vadim.me>
2024-08-02 19:30:49 +0200

    Solution 6 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/4ib9k79q Prompt:       |                              ^~~~~~ 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp:18: 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp: In function ‘int main()’: 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp:28:20: error: ‘total_match’ was not dec lared in this scope    28 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 7247d465-6902-4c48-8350-4f84e02c0a5c.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,11 +1,29 @@
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+#include <string>
+
+bool issame(const vector<string>& lst1, const vector<string>& lst2) {
+    int total_chars1 = 0;
+    int total_chars2 = 0;
+    
+    for (const string& str : lst1) {
+        total_chars1 += str.size();
+    }
+    
+    for (const string& str : lst2) {
+        total_chars2 += str.size();
+    }
+    
+    if (total_chars1 < total_chars2) {
+        return true;
+    } else {
         return false;
     }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
 }
+
+int main() {
+    bool issame(const vector<string>& lst1, const vector<string>& lst2);
+
+    int total_match = 0;
+
+    // other main function code...
+    return 0;
+} 
