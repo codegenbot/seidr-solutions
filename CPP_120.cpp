407d370de94747e424824f4909cca521fad1e9ec
beep boop <githubbot@vadim.me>
2024-08-02 18:58:15 +0200

    Solution 48 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/pth2wzat Prompt:                  from /usr/include/c++/11/algorithm:74,                  from d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp:14:34: error: expected primary-expressi on before ‘int’    14 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp:14:39: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp:13: d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp: In function ‘int main()’: d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp:23:20: error: ‘maximum’ was not declare d in this scope    23 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ d684ee63-d816-4e4f-a0af-7d5e0a533e22.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -11,7 +11,7 @@ bool isEqual(vector<int> a, vector<int> b) {
 
 vector<int> maximum(vector<int> arr, int k) {
     vector<int> result;
-    for (int i = 0; i < k && !arr.empty(); i++) {
+    for (int i = 0; i < k; i++) {
         auto it = std::max_element(arr.begin(), arr.end());
         result.push_back(*it);
         arr.erase(it);
@@ -22,9 +22,5 @@ vector<int> maximum(vector<int> arr, int k) {
 int main() {
     vector<int> arr = {1, 2, 3, -23, 243, -400, 0};
     int k = 4;
-    vector<int> result = maximum(arr, k);
-    for (int i = 0; i < k; ++i) {
-        cout << result[i] << " ";
-    }
-    return 0;
+    assert(isEqual(maximum(arr, k), vector<int>({243, 3, 2, 1})));
 }
