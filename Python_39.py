65618495bd364184a44d211ca16f2201e3b836cc
beep boop <githubbot@vadim.me>
2024-08-02 16:46:59 +0200

    Solution 11 of Python/39, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run4/runs/oyf9th71 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/211e76aa-2d0c-421b-9ef6-3c5e8f6e0 b6e.py", line 14, in <module>     check(prime_fib)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/211e76aa-2d0c-421b-9ef6-3c5e8f6e0 b6e.py", line 11, in check     assert prime_fib(10) == 433494437            ^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/211e76aa-2d0c-421b-9ef6-3c5e8f6e0 b6e.py", line 5, in prime_fib     if is_prime(b):        ^^^^^^^^ NameError: name 'is_prime' is not defined

--- Python_39.py
+++ Python_39.py
@@ -1,3 +1,11 @@
+def is_prime(n: int):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 def prime_fib(n: int):
     a, b = 0, 1
     count = 0
