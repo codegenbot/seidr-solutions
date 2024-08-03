d0370eced318fac07a2b5cdb5bd075c43a0770a7
beep boop <githubbot@vadim.me>
2024-08-03 09:21:34 +0200

    Solution 5 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/1r8vj6qi Prompt: Complete the following code given the task description and function signature.

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -1,7 +1,11 @@
+#include <vector>
+
+using namespace std;
+
 vector<float> derivative(vector<float> xs) {
     vector<float> result;
     for (int i = 1; i < xs.size(); i++) {
-        result.push_back(xs[i] * i);
+        result.push_back(i * xs[i]);
     }
     return result;
 }
