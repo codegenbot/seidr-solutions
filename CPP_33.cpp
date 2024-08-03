a750d064bcb79e94950f352024de4cb3e9567739
beep boop <githubbot@vadim.me>
2024-08-03 12:33:50 +0200

    Solution 12 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/m49a8e8j Prompt:   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from cff88e12-ed9f-4ad9-a921-3ce09a739977.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ cff88e12-ed9f-4ad9-a921-3ce09a739977.cpp:38:34: error: expected primary-expressi on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from cff88e12-ed9f-4ad9-a921-3ce09a739977.cpp:37: cff88e12-ed9f-4ad9-a921-3ce09a739977.cpp: In function ‘int main()’: cff88e12-ed9f-4ad9-a921-3ce09a739977.cpp:47:20: error: ‘sort_third’ was not decl ared in this scope    47 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ cff88e12-ed9f-4ad9-a921-3ce09a739977.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,11 +1,10 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
-#include <iterator>
 
 using namespace std;
 
-bool issame(vector<int>& a, vector<int>& b) {
+bool issame(std::vector<int> a, std::vector<int> b) {
     if (a.size() != b.size()) {
         return false;
     }
@@ -17,27 +16,27 @@ bool issame(vector<int>& a, vector<int>& b) {
     return true;
 }
 
-vector<int> sort_third(vector<int> l);
+std::vector<int> sort_third(std::vector<int> l);
 
 int main() {
-    // Add main function code to call sort_third function correctly
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
     return 0;
 }
 
-vector<int> sort_third(vector<int> l) {
-    vector<int> l_divisible_by_three;
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> l_divisible_by_three;
     for (int i = 0; i < l.size(); i++) {
-        if (l[i] % 3 == 0) {
+        if (i % 3 == 0) {
             l_divisible_by_three.push_back(l[i]);
         }
     }
     sort(l_divisible_by_three.begin(), l_divisible_by_three.end());
     
-    vector<int> l_prime = l;
+    std::vector<int> l_prime = l;
     int j = 0;
     for (int i = 0; i < l.size(); i++) {
-        if (j % 3 == 0) {
-            l_prime[i] = l_divisible_by_three[j / 3];
+        if (i % 3 == 0) {
+            l_prime[i] = l_divisible_by_three[j];
             j++;
         }
     }
