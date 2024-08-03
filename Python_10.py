a85750e833eecd3479d7ea6caadd11e8cb86efb3
beep boop <githubbot@vadim.me>
2024-08-03 08:55:57 +0200

    Solution 81 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/52mxctu7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/7331dc51-4cb8-4f43-8c27-da0773bc7 16d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_10.py
+++ Python_10.py
@@ -1,13 +1,19 @@
 ```
-def is_palindrome(string: str) -> bool:
-    return string == string[::-1]
+    def is_palindrome(string: str) -> bool:
+        """ 
+        Test if given string is a palindrome
+        """
+        return string == string[::-1]
 
-
-def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
+    def make_palindrome(string: str) -> str:
+        """ 
+        Make the given string into a palindrome by adding the reverse of itself to the end.
+        If the original string is already a palindrome, add it's reverse to the front and back.
+        """
+        if string == string[::-1]:
+            return string + string[::-1]
+        for i in range(len(string)):
+            postfix = string[i:]
+            if postfix == postfix[::-1]:
+                return string + postfix[::-1]
         return string + string[::-1]
-    for i in range(len(string)):
-        postfix = string[i:]
-        if postfix == postfix[::-1]:
-            return string + postfix[::-1]
-    return string + string[::-1]
