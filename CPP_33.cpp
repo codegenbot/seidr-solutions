8ca52043641990a70a8cce511c439b7cce5c4c3b
beep boop <githubbot@vadim.me>
2024-08-03 20:48:39 +0200

    Solution 19 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:    16 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 9ebff509-f815-4005-b89b-7e3746ba6bdb.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9ebff509-f815-4005-b89b-7e3746ba6bdb.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 9ebff509-f815-4005-b89b-7e3746ba6bdb.cpp:15: 9ebff509-f815-4005-b89b-7e3746ba6bdb.cpp: In function ‘int main()’: 9ebff509-f815-4005-b89b-7e3746ba6bdb.cpp:25:20: error: ‘sort_third’ was not decl ared in this scope    25 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 9ebff509-f815-4005-b89b-7e3746ba6bdb.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 vector<int> sort_third(vector<int> l) {
-    // Implement your sorting logic here
+    for (size_t i = 2; i < l.size(); i += 3) {
+        for (size_t j = i; j > 0 && l[j] < l[j - 1]; j--) {
+            swap(l[j], l[j - 1]);
+        }
+    }
     return l;
 }
 
