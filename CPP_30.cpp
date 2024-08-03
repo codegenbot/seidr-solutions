0773fb15219ed8c20315c72bc9a144c2718efa23
beep boop <githubbot@vadim.me>
2024-08-03 08:28:34 +0200

    Solution 90 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/93tkvikn Prompt:       |           ^~~~~~ In file included from d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp:13: d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp: In function ‘int main()’: d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp:23:20: error: ‘get_positive’ was not de clared in this scope    23 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ d357ed24-0cdc-456f-87cb-4b6b0d2c14a1.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,61 +1,28 @@
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
-bool issame(const std::vector<float>& a, const std::vector<float>& b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(std::abs(a[i] - b[i]) > 1e-5f) {
-            return false;
-        }
-    }
-    
-    return true;
+bool issame(vector<float> a,vector<float>b){
+    return a == b;
 }
 
-int main() {
-    std::vector<float> l;
-    float num;
-
-    while(true) {
-        std::cout << "Enter a number (0 to finish): ";
-        std::cin >> num;
-        
-        if(num == 0.0f) {
-            break;
-        }
-        
-        l.push_back(num);
-    }
-
-    if(l.size() > 1) {
-        if(issame(get_positive(l), get_positive(std::vector<float>(l.rbegin(), l.rend())))) {
-            std::cout << "The two vectors are the same.\n";
-        } else {
-            std::cout << "The two vectors are not the same.\n";
-        }
-    } else if(l.size() == 1) {
-        std::cout << "You entered one number: ";
-        
-        for(auto x : get_positive(l)) {
-            std::cout << x << " ";
-        }
-        
-        std::cout << "\n";
-    } else {
-        std::cout << "You didn't enter any numbers.\n";
-    }
-
-    return 0;
-}
-
-vector<float> get_positive(vector<float> l){
+vector<float> get_positive(vector<float> l) {
     vector<float> result;
-    for(auto x : l) {
-        if(x > 0.0f)
-            result.push_back(x);
+    for (float num : l) {
+        if (num > 0) {
+            result.push_back(num);
+        }
     }
-    return result;
+    sort(result.begin(),result.end());
+    vector<float> unique_result;
+    for(float num: result){
+        bool is_in = false;
+        for(float n: unique_result){
+            if(issame(vector<float>({n}),vector<float>({num}))){
+                is_in = true;
+                break;
+            }
+        }
+        if(!is_in)unique_result.push_back(num);
+    }
+    return unique_result;
 }
