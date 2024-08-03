1a86f9e823afc73d9cd25ea05be96ffda12a8418
beep boop <githubbot@vadim.me>
2024-08-03 08:28:46 +0200

    Solution 81 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/55w08els Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10692f92-4ae5-42d9-8f7f-4db7c84bb 690.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_1.py
+++ Python_1.py
@@ -1,21 +1,23 @@
-```
+from typing import List
+
+
 def separate_paren_groups(paren_string: str) -> List[str]:
     stack = []
     groups = []
-    group = ''
+    group = ""
 
     for char in paren_string:
-        if char == ' ':
+        if char == " ":
             continue
-        if char == '(':
+        if char == "(":
             stack.append(char)
             group += char
-        elif char == ')':
+        elif char == ")":
             stack.pop()
             group += char
             if not stack:
-                groups.append(group)
-                group = ''
+                groups.append(group[1:])  # Remove the opening parenthesis
+                group = ""
         else:
             raise ValueError("Invalid input")
 
