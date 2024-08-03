b34892128f4ae9e9dcc2a0e6e525398980df8b1e
beep boop <githubbot@vadim.me>
2024-08-03 18:21:39 +0200

    Solution 50 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ihoobi73 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/061d4613-919a-4c5f-a6e9-3a418e779 50c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,2 +1,5 @@
-def sum_odd(lst):
-    return sum(i for i in lst if i % 2 != 0)
+def solution(lst):
+    if not any(i % 2 == 0 for i in lst):
+        return "Error: List should contain at least one even number"
+    else:
+        return sum(i for i in lst[1::2] if i % 2 != 0)
