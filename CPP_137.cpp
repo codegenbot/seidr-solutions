813fccb66eabb1fbc58daa938ceb60d2927e8adb
beep boop <githubbot@vadim.me>
2024-08-03 04:18:21 +0200

    Solution 51 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q2hs3fhq Prompt: 7c94acaa-e4a0-4f25-af21-79871e706f47.cpp:3:10: fatal error: boost/any.hpp: No su ch file or directory     3 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -10,42 +10,42 @@ any compare_one(any a, any b) {
         return (int)b > a.convert_to<int>() ? b : a;
     }
     else if (a.type() == type_id<float>() && b.type() == type_id<int>()) {
-        return (float)a > b.convert_to<int>() ? a : "None";
+        return (float)a > b.convert_to<int>() ? a : any("None");
     }
     else if (a.type() == type_id<string>() && b.type() == type_id<string>()) {
-        return a.convert_to<string>() > b.convert_to<string>() ? a : "None";
+        return a.convert_to<string>() > b.convert_to<string>() ? a : any("None");
     }
     else if (a.type() == type_id<int>() && b.type() == type_id<string>()) {
         int x = boost::any_cast<int>(a);
         string y = boost::any_cast<string>(b);
-        return x > stoi(y) ? a : "None";
+        return x > stoi(y) ? a : any("None");
     }
     else if (a.type() == type_id<string>() && b.type() == type_id<int>()) {
         int x = boost::any_cast<int>(b);
         string y = boost::any_cast<string>(a);
-        return stoi(y) > x ? a : "None";
+        return stoi(y) > x ? a : any("None");
     }
     else if (a.type() == type_id<float>() && b.type() == type_id<string>()) {
         float x = boost::any_cast<float>(a);
         string y = boost::any_cast<string>(b);
-        return x > stof(y) ? a : "None";
+        return x > stof(y) ? a : any("None");
     }
     else if (a.type() == type_id<string>() && b.type() == type_id<float>()) {
         float x = boost::any_cast<float>(b);
         string y = boost::any_cast<string>(a);
-        return stof(y) > x ? a : "None";
+        return stof(y) > x ? a : any("None");
     }
     else if (a.type() == type_id<int>() && b.type() == type_id<int>()) {
         int x = boost::any_cast<int>(a);
         int y = boost::any_cast<int>(b);
-        return x > y ? a : (x < y ? b : "None");
+        return x > y ? a : (x < y ? b : any("None"));
     }
     else if (a.type() == type_id<float>() && b.type() == type_id<float>()) {
         float x = boost::any_cast<float>(a);
         float y = boost::any_cast<float>(b);
-        return x > y ? a : (x < y ? b : "None");
+        return x > y ? a : (x < y ? b : any("None"));
     }
     else {
-        return "None";
+        return any("None");
     }
 }
