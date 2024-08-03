e10e8bffdfe606cedce261f6e5f839ba28697a6b
beep boop <githubbot@vadim.me>
2024-08-03 08:20:21 +0200

    Solution 58 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nh7nsgeg Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:11: 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp: In function ‘int main()’: 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,15 +1,11 @@
-bool issame(string str1, string str2) {
-    if (str1.length() != str2.length()) return false;
-    for (int i = 0; i < str1.length(); i++) {
-        if (str1[i] != str2[i]) return false;
-    }
-    return true;
+bool issame(const string& str1, const string& str2) {
+    return str1 == str2;
 }
 
 vector<string> filter_by_prefix(vector<string> strings, string prefix){
     vector<string> result;
     for (const auto& str : strings) {
-        if (issame(prefix, str.substr(0, prefix.length()))) {
+        if (str.find(prefix) == 0 || issame(str.substr(0, prefix.size()), prefix)) {
             result.push_back(str);
         }
     }
