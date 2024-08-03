d7c5f0caf1cd26dd4d32a00ebce992562dca3bee
beep boop <githubbot@vadim.me>
2024-08-03 09:18:54 +0200

    Solution 2 of CPP/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/yj4jmioa Prompt: a870f934-1837-4695-b91a-c48bf524ea6e.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string make_palindrome(string str){       | ^~~~~~ In file included from a870f934-1837-4695-b91a-c48bf524ea6e.cpp:11: a870f934-1837-4695-b91a-c48bf524ea6e.cpp: In function ‘int main()’: a870f934-1837-4695-b91a-c48bf524ea6e.cpp:13:13: error: ‘make_palindrome’ was not  declared in this scope    13 |     assert (make_palindrome("jerry") == "jerryrrej");       |             ^~~~~~~~~~~~~~~

--- CPP_10.cpp
+++ CPP_10.cpp
@@ -1,8 +1,28 @@
-string make_palindrome(string str){
+#include <string>
+
+bool is_palindrome(string str) {
+    for (int i = 0; i < str.size() / 2; i++) {
+        if (str[i] != str[str.size() - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+string make_palindrome(string str);
+
+int main() {
+    string input_str;
+    cin >> input_str;
+    cout << make_palindrome(input_str);
+    return 0;
+}
+
+string make_palindrome(string str) {
     string rev_str(str.rbegin(), str.rend());
-    for (int i = 0; i < str.length(); i++) {
-        if (str.substr(0, str.length() - i) == rev_str.substr(i)) {
-            return str + rev_str.substr(0, i);
+    for (int i = str.size(); i >= 0; --i) {
+        if (is_palindrome(str.substr(i))) {
+            return str + rev_str.substr(0, str.size() - i);
         }
     }
     return str;
