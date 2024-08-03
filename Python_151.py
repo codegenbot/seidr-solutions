562c443eae1887fd8286699f09736a6cac9fe481
beep boop <githubbot@vadim.me>
2024-08-03 19:51:34 +0200

    Solution 90 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r6y4ygjw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e8ede680-f4e6-4b9b-9aba-a2d4d126f 563.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e8ede680-f4e6-4b9b-9aba-a2d4d126f 563.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,5 +1,4 @@
-def check(lst, result):
-    if isinstance(lst[0], int) and lst[1] == 2*result:
-        print("Test passed")
-    else:
-        print("Test failed")
+```Python
+def double_the_difference(lst1, lst2):
+    return abs(sum(i**2 for i in lst1 if isinstance(i, int) and i > 0) - 
+               sum(i**2 for i in lst2 if isinstance(i, int) and i > 0)) * 2
