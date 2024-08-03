f9541545ecb0198cdb92c413b13e4b910ed0c625
beep boop <githubbot@vadim.me>
2024-08-03 14:28:43 +0200

    Solution 54 of Python/55, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pbz1n0kj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d08e15d7-3fa3-42f4-b947-111c49533 2ae.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_55.py
+++ Python_55.py
@@ -1,6 +1,3 @@
-```
-print("Enter a number: ")
-n = int(input())
 def fib(n: int):
     if n <= 1:
         return n
@@ -8,4 +5,3 @@ def fib(n: int):
     for _ in range(2, n + 1):
         a, b = b, a + b
     return b
-print(fib(n))
