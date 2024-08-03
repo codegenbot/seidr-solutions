ed239a10ffaff5cd81921ee6281c4cf73cd76de1
beep boop <githubbot@vadim.me>
2024-08-03 03:19:00 +0200

    Solution 32 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/w9605fj1 Prompt: e33fa32f-adc3-4b81-abc4-9379ff5cccd1.cpp:23:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    23 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ e33fa32f-adc3-4b81-abc4-9379ff5cccd1.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ e33fa32f-adc3-4b81-abc4-9379ff5cccd1.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ e33fa32f-adc3-4b81-abc4-9379ff5cccd1.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
+bool areEqual(vector<int> a, vector<int> b) {
     return a == b;
 }
 
@@ -15,6 +15,6 @@ vector<int> eat(int number, int need, int remaining) {
 }
 
 int main() {
-    assert(issame(eat(4, 5, 1), {5, 0}));
+    assert(areEqual(eat(4, 5, 1), {5, 0}));
     return 0;
 }
