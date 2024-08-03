7ba39c7fd92d0a3c2109fc8cc5ac3c8d721ac6be
beep boop <githubbot@vadim.me>
2024-08-03 20:43:16 +0200

    Solution 12 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 6a19690e-8b87-4f3c-9ef1-5292428f0a4d.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 6a19690e-8b87-4f3c-9ef1-5292428f0a4d.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 6a19690e-8b87-4f3c-9ef1-5292428f0a4d.cpp:19: 6a19690e-8b87-4f3c-9ef1-5292428f0a4d.cpp: In function ‘int main()’: 6a19690e-8b87-4f3c-9ef1-5292428f0a4d.cpp:29:20: error: ‘sort_third’ was not decl ared in this scope    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 6a19690e-8b87-4f3c-9ef1-5292428f0a4d.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,21 +1,28 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
-std::vector<int> sort_third(std::vector<int> l);
+vector<int> sort_third(vector<int> l); // Function declaration
 
-std::vector<int> sort_third(std::vector<int> l) {
-    std::vector<int> res = l;
-    std::vector<int> sorted_third;
+int main() {
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    return 0;
+}
+
+vector<int> sort_third(vector<int> l) {
+    vector<int> res = l;
+    vector<int> sorted_third;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
             sorted_third.push_back(l[i]);
         }
     }
-    std::sort(sorted_third.begin(), sorted_third.end());
+    sort(sorted_third.begin(), sorted_third.end());
     int idx = 0;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
