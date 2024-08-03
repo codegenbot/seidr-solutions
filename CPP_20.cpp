c71c71be14b9aa39ef3bd118c32e09da90388744
beep boop <githubbot@vadim.me>
2024-08-03 19:38:15 +0200

    Solution 66 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vw6qzgbr Prompt:     4 | 3f021147-c03e-4edc-bbeb-74810de314af.cpp:46:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    46 | int main(){       |         ^~ 3f021147-c03e-4edc-bbeb-74810de314af.cpp:46:9: note: remove parentheses to defau lt-initialize a variable    46 | int main(){       |         ^~       |         -- 3f021147-c03e-4edc-bbeb-74810de314af.cpp:46:9: note: or replace parentheses with  braces to value-initialize a variable 3f021147-c03e-4edc-bbeb-74810de314af.cpp:46:11: error: a function-definition is not allowed here before ‘{’ token    46 | int main(){       |           ^ 3f021147-c03e-4edc-bbeb-74810de314af.cpp:48:2: error: expected ‘}’ at end of inp ut    48 | }       |  ^ 3f021147-c03e-4edc-bbeb-74810de314af.cpp:32:12: note: to match this ‘{’    32 | int main() {       |            ^

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -25,12 +25,11 @@ std::vector<float> find_closest_elements(std::vector<float> numbers) {
     return closest_elements;
 }
 
+bool issame(std::vector<float> a, std::vector<float> b) {
+    return a == b;
+}
+
 int main() {
-    std::vector<float> result = find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1});
-
-    for (const auto& elem : result) {
-        std::cout << elem << " ";
-    }
-
+    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {3.1, 4.1});
     return 0;
 }
