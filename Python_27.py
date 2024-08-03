7757ed726c09e6006c580dff5cd3ee954459d15d
beep boop <githubbot@vadim.me>
2024-08-03 22:33:25 +0200

    Solution 53 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/go8g61ik Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c92c4048-683a-4211-ad34-e7bfb7569 e94.py", line 10, in <module>     check(flip_case)           ^^^^^^^^^ NameError: name 'flip_case' is not defined

--- Python_27.py
+++ Python_27.py
@@ -1,8 +1,5 @@
+def check(string):
+    return modify_string(string)
+
 def modify_string(string):
     return "".join(char.lower() if char.isupper() else char.upper() for char in string)
-
-def check(s):
-    return modify_string(s)
-
-# Test the check function with a string input
-print(check("Hello World"))
