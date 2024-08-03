b0c3f9a1434be663af0b6d26a590a249e7fbeb7f
beep boop <githubbot@vadim.me>
2024-08-03 13:41:47 +0200

    Solution 59 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i2kseror Prompt:     5 | bd94beb3-47af-4088-be2a-b1da320eda9c.cpp:40:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    40 | int main(){       |         ^~ bd94beb3-47af-4088-be2a-b1da320eda9c.cpp:40:9: note: remove parentheses to defau lt-initialize a variable    40 | int main(){       |         ^~       |         -- bd94beb3-47af-4088-be2a-b1da320eda9c.cpp:40:9: note: or replace parentheses with  braces to value-initialize a variable bd94beb3-47af-4088-be2a-b1da320eda9c.cpp:40:11: error: a function-definition is not allowed here before ‘{’ token    40 | int main(){       |           ^ bd94beb3-47af-4088-be2a-b1da320eda9c.cpp:42:2: error: expected ‘}’ at end of inp ut    42 | }       |  ^ bd94beb3-47af-4088-be2a-b1da320eda9c.cpp:23:12: note: to match this ‘{’    23 | int main() {       |            ^

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,7 +1,1 @@
 {
-    assert(issame({4, 5}, even_odd_count(123456789)));
-    assert(issame({5, 0}, even_odd_count(24680)));
-    assert(issame({0, 6}, even_odd_count(111111)));
-
-    return 0;
-}
