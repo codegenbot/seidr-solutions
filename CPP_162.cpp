179bbf295829772ec139519582d7672c2545ea40
beep boop <githubbot@vadim.me>
2024-08-03 04:22:06 +0200

    Solution 96 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/pfe9otlx Prompt: /usr/bin/ld: /tmp/cc0wLXLW.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0x8e): undefined reference to `O PENSSL_init_crypto' /usr/bin/ld: 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0x93): undefined re ference to `EVP_MD_CTX_new' /usr/bin/ld: 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0x9f): undefined re ference to `EVP_md5' /usr/bin/ld: 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0xb9): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0xf2): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0x10f): undefined r eference to `EVP_DigestFinal_ex' /usr/bin/ld: 9f31929e-0552-469f-b2d2-c5c4947b58a3.cpp:(.text+0x11e): undefined r eference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,6 +1,7 @@
 #include <openssl/crypto.h>
-#include <string>
 #include <openssl/evp.h>
+#include <string>
+#include <openssl/err.h>
 
 std::string string_to_md5(const std::string& text);
 
@@ -9,8 +10,8 @@ std::string string_to_md5(const std::string& text) {
         return "None";
     }
 
-    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_DIGESTS, nullptr);
-
+    OpenSSL_add_all_algorithms();
+    
     EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, EVP_md5(), nullptr);
 
@@ -28,6 +29,7 @@ std::string string_to_md5(const std::string& text) {
     }
 
     EVP_cleanup();
+    ERR_free_strings();
 
     return std::string(mdString);
-}  
+}
