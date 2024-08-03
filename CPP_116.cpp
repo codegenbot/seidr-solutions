8fea84461c00a7613faa42c79b64db0b725ccffe
beep boop <githubbot@vadim.me>
2024-08-03 09:31:39 +0200

    Solution 50 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/r88ilw8d Prompt:       |                           ^~~~~~ f720fc80-87be-4db0-8ff8-3532efedf445.cpp:28:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f720fc80-87be-4db0-8ff8-3532efedf445.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f720fc80-87be-4db0-8ff8-3532efedf445.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f720fc80-87be-4db0-8ff8-3532efedf445.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f720fc80-87be-4db0-8ff8-3532efedf445.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ f720fc80-87be-4db0-8ff8-3532efedf445.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -3,17 +3,14 @@
 #include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    return std::vector<int>(a) == std::vector<int>(b);
+    return a == b;
 }
 
 std::vector<int> sort_array(std::vector<int> arr) {
     std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int countA = __builtin_popcount(a);
         int countB = __builtin_popcount(b);
-        if (countA == countB) {
-            return a < b;
-        }
-        return countA < countB;
+        return countA == countB ? a < b : countA < countB;
     });
 
     return arr;
@@ -21,6 +18,5 @@ std::vector<int> sort_array(std::vector<int> arr) {
 
 int main() {
     assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
-
     return 0;
 }
