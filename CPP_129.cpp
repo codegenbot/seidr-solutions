6b5d9e56430102978a74ef43e631337f88a7498d
beep boop <githubbot@vadim.me>
2024-08-03 08:35:39 +0200

    Solution 14 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2k819xey Prompt:    42 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 3254d68c-3d3b-4077-968b-f0de5d826f14.cpp:42:27: error: ‘vector’ was not declared  in this scope    42 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3254d68c-3d3b-4077-968b-f0de5d826f14.cpp:42:34: error: expected primary-expressi on before ‘int’    42 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 3254d68c-3d3b-4077-968b-f0de5d826f14.cpp:41: 3254d68c-3d3b-4077-968b-f0de5d826f14.cpp: In function ‘int main()’: 3254d68c-3d3b-4077-968b-f0de5d826f14.cpp:51:20: error: ‘minPath’ was not declare d in this scope    51 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ 3254d68c-3d3b-4077-968b-f0de5d826f14.cpp:51:19: error: ‘issame’ cannot be used a s a function    51 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,10 +1,41 @@
-vector<int> minPath(vector<vector<int>> grid, int k);
+#include <vector>
 
-int main() {
-    assert(minPath({{1, 3}, {3, 2}}, 10) == vector<int>{1, 3, 1, 3, 1, 3, 1, 3, 1, 3});
-    return 0;
+bool isEqualTo(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 vector<int> minPath(vector<vector<int>> grid, int k) {
-    // Existing minPath function code
+    int n = grid.size();
+    int m = grid[0].size();
+
+    vector<vector<int>> dp(n, vector<int>(m, k+1));
+    dp[0][0] = grid[0][0];
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (i > 0) {
+                dp[i][j] = min(dp[i][j], dp[i-1][j] + grid[i][j]);
+            }
+            if (j > 0) {
+                dp[i][j] = min(dp[i][j], dp[i][j-1] + grid[i][j]);
+            }
+        }
+    }
+
+    vector<int> path;
+    int i = n - 1, j = m - 1;
+    path.push_back(grid[i][j]);
+
+    while (i > 0 || j > 0) {
+        if (i > 0 && dp[i][j] == dp[i-1][j] + grid[i][j]) {
+            i--;
+        } else {
+            j--;
+        }
+        path.push_back(grid[i][j]);
+    }
+
+    reverse(path.begin(), path.end());
+    
+    return path;
 }
