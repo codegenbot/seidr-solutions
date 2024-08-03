cef2d9c1e20122bd3b4fd01fc18a7fba73c372f5
beep boop <githubbot@vadim.me>
2024-08-04 01:40:35 +0200

    Solution 6 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/c2i85h8a Prompt:       |                              ^~~~~~ e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp:38:37: error: ‘string’ was not declared  in this scope    38 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp:38:45: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp:37: e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp: In function ‘int main()’: e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp:47:20: error: ‘split_words’ was not dec lared in this scope    47 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~ e19c1dd2-c01d-4338-a13d-f301934e73fe.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,16 +1,38 @@
-#include <vector>
 #include <string>
 
-bool issame(vector<string> a, vector<string> b){
-    // Your implementation here
-}
-
 vector<string> split_words(string txt) {
-    // Your implementation here
-}
-
-int main() {
-    assert(issame(split_words(""), {"0"}));
-
-    return 0;
+    vector<string> result;
+    string word = "";
+    bool hasWhitespace = false;
+    bool hasComma = false;
+    
+    for(char c : txt){
+        if(c == ' '){
+            hasWhitespace = true;
+            result.push_back(word);
+            word = "";
+        } else if(c == ','){
+            hasComma = true;
+            result.push_back(word);
+            word = "";
+        } else {
+            word += c;
+        }
+    }
+    
+    if(hasWhitespace || hasComma){
+        if(word != ""){
+            result.push_back(word);
+        }
+    } else {
+        int oddCount = 0;
+        for(char c : txt){
+            if(islower(c) && (c - 'a') % 2 == 1){
+                oddCount++;
+            }
+        }
+        result.push_back(to_string(oddCount));
+    }
+    
+    return result;
 }
