e97e02adc629485983448d776d171eec6d4f3103
beep boop <githubbot@vadim.me>
2024-08-03 21:09:38 +0200

    Solution 16 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/d9a93wwb Prompt:    29 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ 05b210e7-b075-49d1-81dc-eb67f9b6af49.cpp:29:29: error: ‘vector’ was not declared  in this scope    29 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 05b210e7-b075-49d1-81dc-eb67f9b6af49.cpp:29:36: error: expected primary-expressi on before ‘float’    29 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from 05b210e7-b075-49d1-81dc-eb67f9b6af49.cpp:28: 05b210e7-b075-49d1-81dc-eb67f9b6af49.cpp: In function ‘int main()’: 05b210e7-b075-49d1-81dc-eb67f9b6af49.cpp:38:20: error: ‘sort_even’ was not decla red in this scope    38 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 05b210e7-b075-49d1-81dc-eb67f9b6af49.cpp:38:19: error: ‘issame’ cannot be used a s a function    38 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,7 +1,9 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 bool issame(std::vector<float> a, std::vector<float> b);
+
 std::vector<float> sort_even(std::vector<float> l);
 
 bool issame(std::vector<float> a, std::vector<float> b) {
@@ -11,7 +13,6 @@ bool issame(std::vector<float> a, std::vector<float> b) {
 std::vector<float> sort_even(std::vector<float> l) {
     std::vector<float> even_values;
     std::vector<float> sorted_even_values;
-    
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
             even_values.push_back(l[i]);
