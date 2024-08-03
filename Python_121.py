9f9c614100947f7492bf81f61f01eaa0c7d66f4a
beep boop <githubbot@vadim.me>
2024-08-03 18:23:50 +0200

    Solution 58 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ihoobi73 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/061d4613-919a-4c5f-a6e9-3a418e779 50c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,4 +1,5 @@
 def solution(lst):
     if not any(i % 2 == 0 for i in lst):
-        raise ValueError("The input list should contain at least one even number")
-    return sum(i for i in lst[1::2] if i % 2 != 0)
+        return "The input list should contain at least one even number."
+    else:
+        return sum(i for i in lst[1::2] if i % 2 != 0)
