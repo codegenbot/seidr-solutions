f10f761c159210330cf144d1889da4949e5b35a3
beep boop <githubbot@vadim.me>
2024-08-03 08:24:06 +0200

    Solution 20 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/kosb39xh Prompt: f252a2a3-7087-4bca-a124-a28f291beb1e.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~ In file included from f252a2a3-7087-4bca-a124-a28f291beb1e.cpp:27: f252a2a3-7087-4bca-a124-a28f291beb1e.cpp: In function ‘int main()’: f252a2a3-7087-4bca-a124-a28f291beb1e.cpp:37:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    37 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ f252a2a3-7087-4bca-a124-a28f291beb1e.cpp:8:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     8 | bool issame(const vector<string>& a, const vector<string>& b){       |      ^~~~~~ f252a2a3-7087-4bca-a124-a28f291beb1e.cpp:28:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    28 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool issame(const vector<string>& a, const vector<string>& b) {
+bool issame(const vector<string>& a, const vector<string>& b){
     return a == b;
 }
 
@@ -20,6 +20,6 @@ vector<string> filter_by_substring(const vector<string>& strings, const string& 
 }
 
 int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), vector<string>{"grunt", "prune"}));
+    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), {"grunt", "prune"}));
     return 0;
 }
