6490244c172ea8a5f54f3851edb3a6c666d6b71d
beep boop <githubbot@vadim.me>
2024-08-03 03:37:37 +0200

    Solution 4 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/e2147ttn Prompt: Complete the following code given the task description and function signature.

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,50 +1,28 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> minPath(vector<vector<int>> grid, int k) {
+vector<int> minPath(vector<vector<int>> grid, int k){
     int n = grid.size();
     vector<vector<int>> dp(n, vector<int>(n));
     
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (i == 0 && j == 0) {
-                dp[i][j] = grid[i][j];
-            } else if (i > 0) {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
-            } else {
-                dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + grid[i][j];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 || j==0) dp[i][j] = grid[i][j];
+            else{
+                int mn = INT_MAX;
+                if(i>0) mn = min(mn, dp[i-1][j]);
+                if(j>0) mn = min(mn, dp[i][j-1]);
+                dp[i][j] = mn + grid[i][j];
             }
         }
     }
     
-    int res = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (dp[i][j] == dp[n-1][n-1] - k) {
-                vector<int> path;
-                int x = i, y = j;
-                for (int l = 0; l < k; l++) {
-                    path.push_back(grid[x][y]);
-                    if (x > 0 && y > 0) {
-                        if (grid[x-1][y] + grid[x][y-1] == dp[x-1][y-1]) {
-                            x--;
-                            y--;
-                        } else if (grid[x-1][y] < grid[x][y-1]) {
-                            x--;
-                        } else {
-                            y--;
-                        }
-                    } else if (x > 0) {
-                        x--;
-                    } else {
-                        y--;
-                    }
-                }
-                return path;
-            }
-        }
+    vector<int> res;
+    int i = n-1, j = n-1;
+    while(k--){
+        res.push_back(grid[i][j]);
+        if(i>0 && j>0) break;
+        else if(i==n-1) j--;
+        else i--;
     }
     
-    return {};
+    reverse(res.begin(), res.end());
+    return res;
 }
