fdc783a6af4b3aab9338e3d470947a010336acf6
beep boop <githubbot@vadim.me>
2024-08-03 15:08:27 +0200

    Solution 80 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/0e18qzqi Prompt: 941abec1-6493-4cf0-939f-22f5c7ddd8ac.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,50 +1,17 @@
-#include <iostream>
 #include <string>
-
-using namespace std;
 
 int bowlingScore(string s) {
     int score = 0;
-    int frame = 1;
+    int currentRoll = 0;
     for (char c : s) {
         if (c == 'X') {
-            score += 30;
-            frame++;
+            score += 10 + (currentRoll > 0 ? currentRoll : 10);
+            currentRoll = 0;
         } else if (c == '/') {
-            string rest = s.substr(s.find('/'));
-            int spare = 10 - stoi(rest.substr(0, 1));
-            score += spare;
-            s.erase(0, s.find('/') + 1);
-            while (!s.empty() && s[0] != 'X') {
-                if (s[0] == '/') {
-                    s.erase(0, 1);
-                } else {
-                    int pins = s[0] - '0';
-                    score += pins;
-                    s.erase(0, 1);
-                }
-            }
+            score += 10 - currentRoll;
+            currentRoll = 0;
         } else {
-            int pins = c - '0';
-            score += pins;
-            if (pins < 10) {
-                while (!s.empty() && s[0] != '/' && s[0] != 'X') {
-                    int extraPins = s[0] - '0';
-                    score += extraPins;
-                    s.erase(0, 1);
-                }
-                if (!s.empty() && s[0] == '/') {
-                    string rest = s.substr(1);
-                    int spare = 10 - stoi(rest.substr(0, 1));
-                    score += spare;
-                    s.erase(0, s.find('/') + 1);
-                } else {
-                    frame++;
-                }
-            } else {
-                frame++;
-            }
+            currentRoll++;
         }
     }
     return score;
-}
