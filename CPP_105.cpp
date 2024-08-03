8a91fc17885b4d975fb607fc74f9d28730ecc2da
beep boop <githubbot@vadim.me>
2024-08-03 05:39:04 +0200

    Solution 16 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7ocphoaa Prompt:       |                                     ^~~~~~ 43b620ec-dd6d-4e8c-9f75-5ec7adeda249.cpp:43:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 43b620ec-dd6d-4e8c-9f75-5ec7adeda249.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 43b620ec-dd6d-4e8c-9f75-5ec7adeda249.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 43b620ec-dd6d-4e8c-9f75-5ec7adeda249.cpp:43:44: error: ‘b’ was not declared in t his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 43b620ec-dd6d-4e8c-9f75-5ec7adeda249.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 43b620ec-dd6d-4e8c-9f75-5ec7adeda249.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main(){       |     ^~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -5,7 +5,15 @@
 #include <cassert>
 
 bool issame(std::vector<std::string> a, std::vector<std::string> b){
-    return a == b;
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 std::vector<std::string> by_length(std::vector<int> arr){
@@ -30,9 +38,4 @@ std::vector<std::string> by_length(std::vector<int> arr){
     }
 
     return result;
-}
-
-int main(){
-    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
-    return 0;
 }
