c35c494c65e4981dd7cae4a7b8cb08864ee5e0c8
beep boop <githubbot@vadim.me>
2024-08-03 04:57:04 +0200

    Solution 14 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/t17sc75i Prompt:     1 | Here's the completed code:       |     ^ ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp:1:5: error: missing terminating ' chara cter     1 | Here's the completed code:       |     ^~~~~~~~~~~~~~~~~~~~~~ ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp:1:1: error: ‘Here’ does not name a type     1 | Here's the completed code:       | ^~~~ In file included from ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp:13: ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp: In function ‘int main()’: ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp:15:35: error: ‘lst’ was not declared in  this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp:15:13: error: ‘double_the_difference’ w as not declared in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ ba07962b-e29a-4f0a-a468-8b3d7ce2b20c.cpp:15:43: error: ‘odd_sum’ was not declare d in this scope    15 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -13,7 +13,7 @@ long long double_the_difference(std::vector<float> lst){
 
 int main() {
     std::vector<float> lst; 
-    float odd_sum;
+    float odd_sum; 
 
     // your code here
     return 0;
