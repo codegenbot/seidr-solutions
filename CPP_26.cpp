5a8daed085197a76bc4c615af2aca314bb1bca33
beep boop <githubbot@vadim.me>
2024-08-03 11:35:58 +0200

    Solution 37 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/us643o3a Prompt: ined here    17 | int main() {       |     ^~~~ In file included from c7d806dc-3667-4661-bda3-af70b064e239.cpp:25: c7d806dc-3667-4661-bda3-af70b064e239.cpp: In function ‘int main()’: c7d806dc-3667-4661-bda3-af70b064e239.cpp:35:37: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    35 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~ In file included from /usr/include/c++/11/vector:67,                  from c7d806dc-3667-4661-bda3-af70b064e239.cpp:1: /usr/include/c++/11/bits/stl_vector.h:625:7: note:   after user-defined conversi on: ‘std::vector<_Tp, _Alloc>::vector(std::initializer_list<_Tp>, const allocato r_type&) [with _Tp = int; _Alloc = std::allocator<int>; std::vector<_Tp, _Alloc> ::allocator_type = std::allocator<int>]’   625 |       vector(initializer_list<value_type> __l,       |       ^~~~~~ c7d806dc-3667-4661-bda3-af70b064e239.cpp:5:42: note:   initializing argument 1 o f ‘void remove_duplicates(std::vector<int>&)’     5 | void remove_duplicates(std::vector<int>& vec) {       |                        ~~~~~~~~~~~~~~~~~~^~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,8 +1,7 @@
 int main() {
-    std::vector<int> input;
-    input = {1, 2, 3, 2, 4, 3, 5};
+    std::vector<int> input = {1, 2, 3, 2, 4, 3, 5};
     remove_duplicates(input);
     assert(issame(input, std::vector<int>{1, 4, 5}));
-    
+
     return 0;
 }
