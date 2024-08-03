55f89a0e41cbe3bd765ad73216a25933261fbabb
beep boop <githubbot@vadim.me>
2024-08-03 02:54:32 +0200

    Solution 42 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/g9ohp2aq Prompt:       |                              ^~~~~~ 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp:24:37: error: ‘string’ was not declared  in this scope    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp:23: 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp: In function ‘int main()’: 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp:33:20: error: ‘reverse_delete’ was not declared in this scope    33 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 468b72d2-1b38-4b97-bc35-fb8eb1c43592.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,10 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-bool isSame(vector<string> a,vector<string>b){
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i<a.size(); i++){
-        if(a[i] != b[i]) return false;
+bool isSame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
@@ -29,11 +33,21 @@ vector<string> reverse_delete(string s, string c) {
     result.push_back(temp);
     string reverseTemp = temp;
     reverse(reverseTemp.begin(), reverseTemp.end());
-    result.push_back(to_string(isSame({temp}, {reverseTemp})));
+    result.push_back(to_string(temp == reverseTemp));
     return result;
 }
 
 int main() {
-    // Your code here
+    string str, charSet;
+    cout << "Enter a string: ";
+    cin >> str;
+    cout << "Enter a character set: ";
+    cin >> charSet;
+    vector<string> output = reverse_delete(str, charSet);
+    if (isSame({str}, output)) {
+        cout << "The original string is the same as the reversed one.\n";
+    } else {
+        cout << "The original string is not the same as the reversed one.\n";
+    }
     return 0;
 }
