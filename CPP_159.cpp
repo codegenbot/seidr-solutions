18672e7f7878a4e7964cbcbe02bd100d914bf645
beep boop <githubbot@vadim.me>
2024-08-03 03:13:12 +0200

    Solution 20 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/w9605fj1 Prompt: 32c449a2-65c1-414c-a194-4bd5e829044d.cpp:23:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    23 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 32c449a2-65c1-414c-a194-4bd5e829044d.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 32c449a2-65c1-414c-a194-4bd5e829044d.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 32c449a2-65c1-414c-a194-4bd5e829044d.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
 vector<int> eat(int number, int need, int remaining) {
     int totalEatenCarrots = number + min(need, remaining);
     int carrotsLeft = max(0, remaining - need);
@@ -15,6 +11,6 @@ vector<int> eat(int number, int need, int remaining) {
 }
 
 int main() {
-    assert(issame(eat(4, 5, 1), {5, 0}));
+    assert(eat(4, 5, 1) == vector<int>{5, 0});
     return 0;
 }
