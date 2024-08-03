5afd1e9be67c9054ba8c29124303e94db706d476
beep boop <githubbot@vadim.me>
2024-08-03 05:08:35 +0200

    Solution 52 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uvcdxz1c Prompt: on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ecf3ba60-bde4-45dc-914c-15bdab1384e8.cpp:39:39: error: expression list treated a s compound expression in initializer [-fpermissive]    39 | bool issame(vector<int> a,vector<int>b){       |                                       ^ ecf3ba60-bde4-45dc-914c-15bdab1384e8.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ ecf3ba60-bde4-45dc-914c-15bdab1384e8.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main() {       |     ^~~~ In file included from ecf3ba60-bde4-45dc-914c-15bdab1384e8.cpp:38: ecf3ba60-bde4-45dc-914c-15bdab1384e8.cpp: In function ‘int main()’: ecf3ba60-bde4-45dc-914c-15bdab1384e8.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,8 +1,18 @@
 #include <vector>
 #include <cassert>
 
-bool is_same(std::vector<int> a, std::vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 std::vector<int> count_up_to(int n) {
@@ -31,6 +41,6 @@ std::vector<int> count_up_to(int n) {
 }
 
 int main() {
-    assert(is_same(count_up_to(101), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    assert(issame(count_up_to(101), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     return 0;
 }
