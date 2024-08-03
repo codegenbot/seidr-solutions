2e4addd97504863ca8e6704e3074775efe14eaf3
beep boop <githubbot@vadim.me>
2024-08-03 10:31:09 +0200

    Solution 1 of CPP/92, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/m0143sn4 Prompt: Complete the following code given the task description and function signature.

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,6 +1,6 @@
-bool any_int(float a, float b, float c){
-    if (round(a) == a && round(b) == b && round(c) == c) {
-        return (a == b + c || a == c + b || b == a + c || b == c + a || c == a + b || c == b + a);
-    }
-    return false;
+bool any_int(float a, float b, float c) {
+    int ai = (int)a;
+    int bi = (int)b;
+    int ci = (int)c;
+    return (a == ai + bi + ci);
 }
