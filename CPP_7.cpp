34d23cf52a2a6c5bb100e9650c807222b260278f
beep boop <githubbot@vadim.me>
2024-08-03 07:23:41 +0200

    Solution 59 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/h8svvfg5 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:10: 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp: In function ‘int main()’: 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -6,7 +6,11 @@ bool issame(vector<string> a,vector<string> b){
     return a==b;
 }
 
-int main() {
-    assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") ,{"grunt", "prune"}));
-    return 0;
+vector<string> filter_by_substring(vector<string> strings, string substring) {
+    vector<string> result;
+    for (const auto& s : strings) {
+        if (s.find(substring) != string::npos)
+            result.push_back(s);
+    }
+    return result;
 }
