3f69787454074915ac601c9f8f59fea15c408cd9
beep boop <githubbot@vadim.me>
2024-08-03 14:29:14 +0200

    Solution 88 of Python/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/zdtglavw Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/bf712dde-72c6-44a9-96e0-33f55b8e5 95f.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_25.py
+++ Python_25.py
@@ -1,5 +1,7 @@
-def calculate_total_sales(sales_data):
-    total_sales = 0
-    for day in sales_data:
-        total_sales += sum(day)
-    return total_sales
+def find_max_score(students: List[dict]) -> int:
+    max_score = 0
+    for student in students:
+        score = sum(int(subject["score"]) for subject in student["subjects"])
+        if score > max_score:
+            max_score = score
+    return max_score
