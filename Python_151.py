6159f143a0d61df335dce3da551bfa7e7890a16b
beep boop <githubbot@vadim.me>
2024-08-03 19:49:48 +0200

    Solution 83 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r6y4ygjw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10610b4e-b743-4fe5-9a5f-8e599c741 c68.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10610b4e-b743-4fe5-9a5f-8e599c741 c68.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,5 @@
 def check(lst, result):
-    if isinstance(lst[0], int) and len(lst) > 1:
-        return "Input is valid"
-    elif result == abs(sum(i for i in lst) - min(lst)) * 2:
-        return "The double the difference of the list is correct"
+    if isinstance(result, int) and (result == 0 or abs(result - 2 * sum(i for i in lst)) <= 1e-9):
+        print("Yes")
     else:
-        return "The double the difference of the list is incorrect"
+        print("No")
