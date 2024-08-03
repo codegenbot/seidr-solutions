6aa04e20f3394e40cf2cb5b04fdc7b571e90c051
beep boop <githubbot@vadim.me>
2024-08-03 06:52:32 +0200

    Solution 16 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7l5w7q5x Prompt: 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp: At global scope: 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp:17:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    17 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp:7:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     7 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp:25:5: error: redefinition of ‘int main( )’    25 | int main(){       |     ^~~~ 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~ In file included from 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp:16: 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp: In function ‘int main()’: 15b70279-a336-4038-aed5-b5cf888c5b7f.cpp:26:20: error: ‘get_odd_collatz’ was not  declared in this scope    26 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,28 +4,27 @@
 
 using namespace std;
 
-vector<int> get_odd_collatz(int n) {
-    vector<int> result;
-    while (n != 1) {
-        result.push_back(n);
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
-    }
-    result.push_back(1);
-    return result;
-}
-
 bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
+vector<int> get_odd_collatz(int num) {
+    vector<int> collatzSeq;
+    collatzSeq.push_back(num);
+    while (num > 1) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        if (num % 2 != 0) {
+            collatzSeq.push_back(num);
+        }
+    }
+    return collatzSeq;
+}
+
 int main() {
     assert(issame(get_odd_collatz(1), {1}));
-    assert(issame(get_odd_collatz(7), {7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1}));
-    assert(issame(get_odd_collatz(10), {10, 5, 16, 8, 4, 2, 1}));
-    assert(issame(get_odd_collatz(27), {27, 82, 41, 124, 62, 31, 94, 47, 142, 71, 214, 107, 322, 161, 484, 242, 121, 364, 182, 91, 274, 137, 412, 206, 103, 310, 155, 466, 233, 700, 350, 175, 526, 263, 790, 395, 1186, 593, 1780, 890, 445, 1336, 668, 334, 167, 502, 251, 754, 377, 1132, 566, 283, 850, 425, 1276, 638, 319, 958, 479, 1438, 719, 2158, 1079, 3238, 1619, 4858, 2429, 7288, 3644, 1822, 911, 2734, 1367, 4102, 2051, 6154, 3077, 9232, 4616, 2308, 1154, 577, 1732, 866, 433, 1300, 650...
     return 0;
 }
