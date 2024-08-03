60584f0687470306330e58103f55cc49876dbb6c
beep boop <githubbot@vadim.me>
2024-08-03 15:34:48 +0200

    Solution 36 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niyd2aok Prompt: 525fa16e-ccac-4291-8003-ee78a3012288.cpp:40:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    40 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 525fa16e-ccac-4291-8003-ee78a3012288.cpp:9:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     9 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 525fa16e-ccac-4291-8003-ee78a3012288.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ 525fa16e-ccac-4291-8003-ee78a3012288.cpp:35:5: note: ‘int main()’ previously def ined here    35 | int main() {       |     ^~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -4,9 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
+vector<int> unique_digits(vector<int> x);
 
 vector<int> unique_digits(vector<int> x) {
     vector<int> result;
