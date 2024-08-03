c20cac9850e2035643e1e8496a6e5a1f2afb913d
beep boop <githubbot@vadim.me>
2024-08-03 11:23:45 +0200

    Solution 17 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/frjlpwac Prompt: 7d9bad67-6127-4d16-be29-6b86b9e57e0c.cpp: In function ‘bool simplify(std::string , std::string)’: 7d9bad67-6127-4d16-be29-6b86b9e57e0c.cpp:15:18: error: ‘gcd’ cannot be used as a  function    15 |     int gcd = gcd(result_num, result_den);       |               ~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,18 +1,3 @@
-#include <stdio.h>
-#include <string>
-#include <cmath>
-using namespace std;
 
-bool simplify(string x, string n) {
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
-
-    int result_num = num1 * num2;
-    int result_den = den1 * den2;
-
-    int gcd_result = gcd(result_num, result_den);
-
-    return result_num % gcd_result == 0 && result_den % gcd_result == 0;
-}
+int gcd_result = gcd(result_num, result_den);
+return result_num % gcd_result == 0 && result_den % gcd_result == 0;
