3f068c857a702c843b47999418f55e2d12b37035
beep boop <githubbot@vadim.me>
2024-08-03 02:13:57 +0200

    Solution 7 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/xg5ailwj Prompt:       |                    ^~~ 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp:12: 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp: In function ‘int main()’: 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp:22:20: error: ‘common’ was not declared  in this scope    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~ 5d6cc86c-5526-4f23-9827-907edc4659c0.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -13,8 +13,18 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b){
 
 int main() {
     // Your code here
-
-    assert(issame(result({4, 3, 2, 8}, {}), {}));
+    std::vector<int> l1 = {4, 5, 1, 3, 7};
+    std::vector<int> l2 = {3, 7, 9, 1};
+    
+    sort(l1.begin(), l1.end());
+    sort(l2.begin(), l2.end());
+    
+    std::vector<int> result;
+    std::set_intersection(l1.begin(), l1.end(), l2.begin(), l2.end(), std::back_inserter(result));
+    
+    result.erase(std::unique(result.begin(), result.end()), result.end());
+    
+    assert(issame(result, {}));
 
     return 0;
 }
