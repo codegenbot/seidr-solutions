b791b9947f06c90e53e575ca2544868d77232f48
beep boop <githubbot@vadim.me>
2024-08-03 05:09:15 +0200

    Solution 54 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uvcdxz1c Prompt: on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ df23c826-bf2d-4477-91bd-73d9d25f332a.cpp:32:39: error: expression list treated a s compound expression in initializer [-fpermissive]    32 | bool issame(vector<int> a,vector<int>b){       |                                       ^ df23c826-bf2d-4477-91bd-73d9d25f332a.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ df23c826-bf2d-4477-91bd-73d9d25f332a.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main() {       |     ^~~~ In file included from df23c826-bf2d-4477-91bd-73d9d25f332a.cpp:31: df23c826-bf2d-4477-91bd-73d9d25f332a.cpp: In function ‘int main()’: df23c826-bf2d-4477-91bd-73d9d25f332a.cpp:41:19: error: ‘issame’ cannot be used a s a function    41 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,46 +1,8 @@
-#include <vector>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    
-    for(size_t i = 0; i < a.size(); ++i) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-std::vector<int> count_up_to(int n) {
-    std::vector<int> primes;
-    
-    if(n >= 2) {
-        primes.push_back(2);
-    }
-    
-    for(int i = 3; i <= n; i += 2) {
-        bool is_prime = true;
-        
-        for(int j = 3; j*j <= i; j += 2) {
-            if(i % j == 0) {
-                is_prime = false;
-                break;
-            }
-        }
-        
-        if(is_prime) {
-            primes.push_back(i);
-        }
-    }
-    
-    return primes;
+bool is_same(std::vector<int> a, std::vector<int> b) {
+    return a == b;
 }
 
 int main() {
-    assert(issame(count_up_to(101), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    assert(is_same(count_up_to(101), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     return 0;
 }
