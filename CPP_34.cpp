ad04dfc022d8a4f9d1c8fd50daa27db66cd2d964
beep boop <githubbot@vadim.me>
2024-08-03 20:37:55 +0200

    Solution 15 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/psk8bxyl Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from f30ba513-d2cf-4b6e-9cc6-7b867b318f18.cpp:10: f30ba513-d2cf-4b6e-9cc6-7b867b318f18.cpp: In function ‘int main()’: f30ba513-d2cf-4b6e-9cc6-7b867b318f18.cpp:20:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from f30ba513-d2cf-4b6e-9cc6-7b867b318f18.cpp:1: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~ In file included from f30ba513-d2cf-4b6e-9cc6-7b867b318f18.cpp:10: f30ba513-d2cf-4b6e-9cc6-7b867b318f18.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -2,9 +2,7 @@
 #include <algorithm>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
     return a == b;
 }
-
-assert(::issame(std::vector<int>{5, 3, 5, 2, 3, 3, 9, 0, 123}, std::vector<int>{0, 2, 3, 5, 9, 123}));
