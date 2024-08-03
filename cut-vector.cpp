060a9d4526f707e5fb937e5fb5b97ae3fe2fb655
beep boop <githubbot@vadim.me>
2024-08-03 04:57:41 +0200

    Solution 92 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/vgei9ora Prompt: this scope    23 |     cin >> n;       |     ^~~ d94aa665-e82b-4e14-aaf3-bc1675504c96.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; d94aa665-e82b-4e14-aaf3-bc1675504c96.cpp:31:5: error: ‘cout’ was not declared in  this scope    31 |     cout << "left: ";       |     ^~~~ d94aa665-e82b-4e14-aaf3-bc1675504c96.cpp:31:5: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? d94aa665-e82b-4e14-aaf3-bc1675504c96.cpp:35:13: error: ‘endl’ was not declared i n this scope    35 |     cout << endl;       |             ^~~~ d94aa665-e82b-4e14-aaf3-bc1675504c96.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,22 +1,21 @@
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> v) {
     int minDiff = INT_MAX;
     int cutIndex = 0;
-
+    
     for (int i = 1; i <= v.size(); i++) {
         if (i == v.size() || abs(v[i] - v[i-1]) < minDiff) {
             minDiff = abs(v[i] - v[i-1]);
             cutIndex = i;
         }
     }
-
+    
     vector<int> leftVec(v.begin(), v.begin() + cutIndex);
     vector<int> rightVec(v.begin() + cutIndex, v.end());
-
+    
     return make_pair(leftVec, rightVec);
 }
 
@@ -27,20 +26,19 @@ int main() {
     for (auto &x : v) {
         cin >> x;
     }
-
+    
     pair<vector<int>, vector<int>> result = cutVector(v);
-
+    
     cout << "left: ";
     for (const auto &x : result.first) {
         cout << x << " ";
     }
     cout << endl;
-
+    
     cout << "right: ";
     for (const auto &x : result.second) {
         cout << x << " ";
     }
     cout << endl;
-
+    
     return 0;
-}
