a2d55e327ac9241fcb01774bee7d16be938a71c1
beep boop <githubbot@vadim.me>
2024-08-03 08:33:20 +0200

    Solution 80 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/3si37sfn Prompt: 253fabe9-e655-4def-8ac5-469d3d171e72.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 253fabe9-e655-4def-8ac5-469d3d171e72.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -15,11 +15,11 @@ vector<int> unique(vector<int> l) {
     return result;
 }
 
-void main() {
+int main() {
     vector<int> input = {5, 3, 5, 2, 3, 3, 9, 0, 123};
     vector<int> output = unique(input);
     for (int i : output) {
         cout << i << " ";
     }
-    return;
+    return 0;
 }
