0d7b613f990ed6266f51fdfb98ec867ebd8de85a
beep boop <githubbot@vadim.me>
2024-08-03 12:46:00 +0200

    Solution 60 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9nawtijy Prompt:    18 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0a4b5d5a-ac06-4b3e-aca0-b5d45d815df6.cpp:18:44: error: ‘b’ was not declared in t his scope    18 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0a4b5d5a-ac06-4b3e-aca0-b5d45d815df6.cpp:18:45: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0a4b5d5a-ac06-4b3e-aca0-b5d45d815df6.cpp:17: 0a4b5d5a-ac06-4b3e-aca0-b5d45d815df6.cpp: In function ‘int main()’: 0a4b5d5a-ac06-4b3e-aca0-b5d45d815df6.cpp:27:20: error: ‘sorted_list_sum’ was not  declared in this scope    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ 0a4b5d5a-ac06-4b3e-aca0-b5d45d815df6.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,14 +1,7 @@
-bool issame(vector<string> a,vector<string>b){
-    if(a.size()!=b.size())return false;
-    for(int i=0;i<a.size();i++){
-        if(a[i].length() % 2 == 1 && b[i].length() % 2 == 0)return false;
-    }
-    return true;
-}
-
 vector<string> sorted_list_sum(vector<string> lst) {
-    auto it = unique(lst.begin(), lst.end(), 
-                     [](const string& a, const string& b){ return a.length() % 2 == 1 && b.length() % 2 == 0; });
+    auto it = unique(lst.begin(), lst.end(),
+                     [](const string& a, const string& b){ 
+                         return (a.length() % 2 == 1 && b.length() % 2 == 0); });
     lst.erase(it, lst.end());
     
     sort(lst.begin(), lst.end(),
@@ -19,26 +12,5 @@ vector<string> sorted_list_sum(vector<string> lst) {
                  return a < b;
          });
 
-    vector<string> result;
-    for(string s:lst){
-        int sum = 0;
-        for(char c:s){
-            sum += c;
-        }
-        bool valid = true;
-        for(string str:lst){
-            if(sum == std::stoi(str)){
-                valid = false;
-                break;
-            }
-        }
-        if(valid)result.push_back(s);
-    }
-
-    return result;
-}
-
-int main() {
-    assert (issame({{"aaaa", "bbbb", "dd", "cc"}}, {{"cc","dd","aaaa","bbbb"}}));
-    return 0;
+    return lst;
 }
