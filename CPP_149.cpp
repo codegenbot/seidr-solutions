91edd3e855dcc49558a67dd925a14561988a3ba3
beep boop <githubbot@vadim.me>
2024-08-03 12:46:55 +0200

    Solution 38 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/t0um6sbg Prompt: bd534190-055a-429a-a6bf-2ab15bcc6174.cpp:29:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    29 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ bd534190-055a-429a-a6bf-2ab15bcc6174.cpp:19:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    19 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ bd534190-055a-429a-a6bf-2ab15bcc6174.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ bd534190-055a-429a-a6bf-2ab15bcc6174.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -16,17 +16,7 @@ vector<string> sorted_list_sum(vector<string> lst) {
     return lst;
 }
 
-#ifndef ISSAME_FUNCTION
-#define ISSAME_FUNCTION
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-#endif
-
-#ifndef MAIN_FUNCTION
-#define MAIN_FUNCTION
 int main() {
     assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), {"cc", "dd", "aaaa", "bbbb"}));
     return 0;
 }
-#endif
