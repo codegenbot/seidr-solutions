0fdc4b34220dfc879cbc0bef3975ca49004a70d5
beep boop <githubbot@vadim.me>
2024-08-03 08:15:37 +0200

    Solution 16 of CPP/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/wgntveov Prompt:       |           ^~~~~~ In file included from b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp:27:39: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp:26: b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp: In function ‘int main()’: b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp:36:20: error: ‘parse_nested_parens’ was  not declared in this scope    36 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |                    ^~~~~~~~~~~~~~~~~~~ b362dbdf-8e06-4734-891c-c14725cbd5d8.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(parse_nested_parens("(()(())((())))") ,{4}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_6.cpp
+++ CPP_6.cpp
@@ -1,9 +1,14 @@
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <cassert>
-
+#include <string>
 using namespace std;
+
+vector<int> parse_nested_parens(string paren_string);
+
+bool issame(vector<int> a, vector<int> b);
+
+int main() {
+    assert(issame(parse_nested_parens("(()(())((())))"), vector<int>{4}));
+}
 
 vector<int> parse_nested_parens(string paren_string){
     vector<int> levels;
@@ -28,12 +33,6 @@ vector<int> parse_nested_parens(string paren_string){
     return levels;
 }
 
-bool issame(const vector<int>& a, const vector<int>& b){
+bool issame(vector<int> a, vector<int> b){
     return a == b;
-}
-
-int main() {
-    assert(issame(parse_nested_parens("(()(())((())))"), {4}));
-    
-    return 0;
 }
