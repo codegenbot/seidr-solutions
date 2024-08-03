0408f7e3e69b47b5d4299c072191ffe63dd6b9a1
beep boop <githubbot@vadim.me>
2024-08-03 05:14:16 +0200

    Solution 3 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/vsqhu388 Prompt:       |                    ^~~ 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp:21: 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp: In function ‘int main()’: 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 80f112f3-02c8-4410-998f-b7d56ad7139d.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,12 +1,36 @@
-bool issame(int n) {
-    unordered_set<int> digits;
-    while (n > 0) {
-        int digit = n % 10;
-        if (digits.count(digit)) {
-            return true;
+bool issame(int x, int y) {
+    string s1 = to_string(x);
+    string s2 = to_string(y);
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+    return s1 == s2;
+}
+
+vector<int> unique_digits(vector<int> x){
+    vector<int> result;
+    for (int num : x) {
+        bool hasEvenDigit = false;
+        int temp = num;
+        while (temp > 0) {
+            if (temp % 2 == 0) {
+                hasEvenDigit = true;
+                break;
+            }
+            temp /= 10;
         }
-        digits.insert(digit);
-        n /= 10;
+        if (!hasEvenDigit) {
+            bool unique = true;
+            for (int res : result) {
+                if (issame(res, num)) {
+                    unique = false;
+                    break;
+                }
+            }
+            if (unique) {
+                result.push_back(num);
+            }
+        }
     }
-    return false;
+    sort(result.begin(), result.end());
+    return result;
 }
