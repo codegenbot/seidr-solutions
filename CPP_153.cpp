dc0771c26b42bda51c3c81119cc38bed8d90ad21
beep boop <githubbot@vadim.me>
2024-08-03 12:50:28 +0200

    Solution 40 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/n1tu4p4y Prompt:       |                         ^~~~~~ f56abe67-ab98-410d-8290-7576afef46d0.cpp:1:44: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                            ^~~~~~ f56abe67-ab98-410d-8290-7576afef46d0.cpp:1:51: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                   ^~~~~~ f56abe67-ab98-410d-8290-7576afef46d0.cpp:1:59: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                           ^~~~~~~~~~ f56abe67-ab98-410d-8290-7576afef46d0.cpp:1:69: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name, vector<string> extensions){       |                                                                     ^ In file included from f56abe67-ab98-410d-8290-7576afef46d0.cpp:24: f56abe67-ab98-410d-8290-7576afef46d0.cpp: In function ‘int main()’: f56abe67-ab98-410d-8290-7576afef46d0.cpp:26:32: error: ‘Strongest_Extension’ can not be used as a function    26 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,26 +1,32 @@
-#include <string>
+#include <iostream>
 #include <vector>
-#include <cctype>
+#include <string>
+using namespace std;
 
-int Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
+int Strongest_Extension(string class_name, vector<string> extensions){
     int strongest = 0;
-    std::string strongest_extension;
+    string strongest_extension;
 
-    for (std::string extension : extensions) {
+    for (string extension : extensions) {
         int cap = 0, sm = 0;
         for (char c : extension) {
-            if (std::isupper(c)) cap++;
-            else if (std::islower(c)) sm++;
+            if (isupper(c)) cap++;
+            else if (islower(c)) sm++;
         }
         int strength = cap - sm;
         if (strength > strongest) {
             strongest = strength;
             strongest_extension = extension;
         } else if (strength == strongest) {
-            if (std::find(extensions.begin(), extensions.end(), extension) < std::find(extensions.begin(), extensions.end(), strongest_extension))
+            if (find(extensions.begin(), extensions.end(), extension) < find(extensions.begin(), extensions.end(), strongest_extension))
                 strongest_extension = extension;
         }
     }
 
     return class_name + "." + strongest_extension;
+}
+
+int main() {
+    assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
+    return 0;
 }
