3d236a5f7911d003b10699b4a17940dfa3b7691d
beep boop <githubbot@vadim.me>
2024-08-03 20:39:43 +0200

    Solution 6 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3be8fa97-b039-480a-8eb6-e3dfc3787f3b.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3be8fa97-b039-480a-8eb6-e3dfc3787f3b.cpp:11:39: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 3be8fa97-b039-480a-8eb6-e3dfc3787f3b.cpp:10: 3be8fa97-b039-480a-8eb6-e3dfc3787f3b.cpp: In function ‘int main()’: 3be8fa97-b039-480a-8eb6-e3dfc3787f3b.cpp:20:20: error: ‘sort_third’ was not decl ared in this scope    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 3be8fa97-b039-480a-8eb6-e3dfc3787f3b.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,25 +1,13 @@
-vector<int> res = l;
-    vector<int> sorted_third;
+bool issame(vector<int> a, vector<int> b) {
+    // Function logic here
+}
+
+vector<int> sort_third(vector<int> l) {
+    vector<int> res = l;
     for (int i = 0; i < l.size(); ++i) {
         if (i % 3 == 0) {
-            sorted_third.push_back(l[i]);
-        }
-    }
-    sort(sorted_third.begin(), sorted_third.end());
-    int idx = 0;
-    for (int i = 0; i < l.size(); ++i) {
-        if (i % 3 == 0) {
-            res[i] = sorted_third[idx++];
+            sort(res.begin() + i, res.begin() + i + 3);
         }
     }
     return res;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-int main() {
-    assert(issame(sorted_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    return 0;
 }
