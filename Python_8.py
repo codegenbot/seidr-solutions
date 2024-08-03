e50100ce1ce48409bacbe148b29bdd991a656d4c
beep boop <githubbot@vadim.me>
2024-08-03 10:06:24 +0200

    Solution 94 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4pizx1b2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4fced963-579c-4bfd-bfe7-b43f318f5 88c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,10 +1,10 @@
-def find_max_min(numbers: List[int]) -> Tuple[int, int]:
+```
+def sum_product(numbers: List[int]) -> Tuple[int, int]:
     if not numbers:
-        return None, None
-    max_num = min_num = numbers[0]
+        return 0, 1
+    total_sum = 0
+    product = 1
     for num in numbers:
-        if num > max_num:
-            max_num = num
-        elif num < min_num:
-            min_num = num
-    return min_num, max_num
+        total_sum += num
+        product *= num
+    return total_sum, product
