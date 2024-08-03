a85f3a5cccd24e860cb97d896e8576f2b6deca2b
beep boop <githubbot@vadim.me>
2024-08-04 00:21:39 +0200

    Solution 53 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt: 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:11:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    11 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ In file included from 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:10: 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp: In function ‘int main()’: 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:20:20: error: ‘count_up_to’ was not dec lared in this scope    20 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -3,10 +3,9 @@ using namespace std;
 
 vector<int> count_up_to(int n) {
     vector<int> primes;
-    
     for (int i = 2; i <= n; i++) {
         bool is_prime = true;
-        for (int j = 2; j*j <= i; j++) {
+        for (int j = 2; j * j <= i; j++) {
             if (i % j == 0) {
                 is_prime = false;
                 break;
@@ -16,7 +15,6 @@ vector<int> count_up_to(int n) {
             primes.push_back(i);
         }
     }
-    
     return primes;
 }
 
@@ -24,12 +22,10 @@ bool issame(vector<int> a, vector<int> b){
     if (a.size() != b.size()) {
         return false;
     }
-    
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    
     return true;
 }
