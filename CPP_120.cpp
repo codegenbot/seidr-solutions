b634998facc48f39c87f59f4da0746590150c808
beep boop <githubbot@vadim.me>
2024-08-04 01:30:51 +0200

    Solution 70 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/3ot180hb Prompt:     5 | 901f7a48-9c4c-4825-a164-4a070f30b9d2.cpp:31:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    31 | int main(){       |         ^~ 901f7a48-9c4c-4825-a164-4a070f30b9d2.cpp:31:9: note: remove parentheses to defau lt-initialize a variable    31 | int main(){       |         ^~       |         -- 901f7a48-9c4c-4825-a164-4a070f30b9d2.cpp:31:9: note: or replace parentheses with  braces to value-initialize a variable 901f7a48-9c4c-4825-a164-4a070f30b9d2.cpp:31:11: error: a function-definition is not allowed here before ‘{’ token    31 | int main(){       |           ^ 901f7a48-9c4c-4825-a164-4a070f30b9d2.cpp:33:2: error: expected ‘}’ at end of inp ut    33 | }       |  ^ 901f7a48-9c4c-4825-a164-4a070f30b9d2.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -13,8 +13,8 @@ std::vector<int> maximum(std::vector<int> arr, int k) {
     return arr;
 }
 
-int main() {
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 3), {243, 3, 2}));
+main() {
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 3), {243, 3, 2});
     
     return 0;
 }
