82b8a45dba6e38afa2e06079324776279258a3f0
beep boop <githubbot@vadim.me>
2024-08-03 12:22:28 +0200

    Solution 13 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/l6jxedtx Prompt: c3f626d7-fa9e-4187-b321-bae7f2ac1a71.cpp:25:9: error: ‘vector’ was not declared in this scope    25 |         vector<double> coeffs = {};       |         ^~~~~~ c3f626d7-fa9e-4187-b321-bae7f2ac1a71.cpp:25:16: error: expected primary-expressi on before ‘double’    25 |         vector<double> coeffs = {};       |                ^~~~~~ c3f626d7-fa9e-4187-b321-bae7f2ac1a71.cpp:30:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    30 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff c3f626d7-fa9e-4187-b321-bae7f2ac1a71.cpp:33:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    33 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff In file included from c3f626d7-fa9e-4187-b321-bae7f2ac1a71.cpp:17: c3f626d7-fa9e-4187-b321-bae7f2ac1a71.cpp:34:17: error: ‘abs’ was not declared in  this scope    34 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -9,8 +9,8 @@ double poly(const vector<double>& coeffs, double x) {
     return result;
 }
 
-double find_zero(const vector<double>& xs) {
+double find_zero(const vector<double>& xs){
     double a = poly(xs, 1.0);
     double b = poly(xs, 0.0);
-    return -b / a;
+    return -b/a;
 }
