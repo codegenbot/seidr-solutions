3cb3f56e2abf925ac7de3cca4de38d39ea4bee0c
beep boop <githubbot@vadim.me>
2024-08-03 15:55:45 +0200

    Solution 3 of Python/73, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/i264nvga Prompt: Complete the following code given the task description and function signature.

--- Python_73.py
+++ Python_73.py
@@ -1,9 +1,12 @@
 ```
 def smallest_change(arr):
     n = len(arr)
-    rev_arr = arr[::-1]
-    diff_count = 0
-    for i in range(n):
-        if arr[i] != rev_arr[i]:
-            diff_count += 1
-    return diff_count
+    left = right = 0
+    count = 0
+    while left < n and right < n:
+        if arr[left] != arr[n - right - 1]:
+            right += 1
+        else:
+            left += 1
+            right += 1
+    return n - (right + left)
