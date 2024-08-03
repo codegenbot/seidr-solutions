fdd765190760afcaf5d7dc799ace05ef83f4b2a7
beep boop <githubbot@vadim.me>
2024-08-03 02:05:57 +0200

    Solution 30 of CPP/98, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/xes97g9c Prompt: 805bdf27-0cbb-4f37-a2fb-9d7b8245e5ae.cpp:1:17: error: ‘string’ was not declared in this scope     1 | int count_upper(string s){       |                 ^~~~~~ In file included from 805bdf27-0cbb-4f37-a2fb-9d7b8245e5ae.cpp:11: 805bdf27-0cbb-4f37-a2fb-9d7b8245e5ae.cpp: In function ‘int main()’: 805bdf27-0cbb-4f37-a2fb-9d7b8245e5ae.cpp:13:24: error: ‘count_upper’ cannot be u sed as a function    13 |     assert (count_upper("EEEE") == 2);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_98.cpp
+++ CPP_98.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
 #include <string>
 
-int count_upper(std::string s) {
+int count_upper(string s){
     int count = 0;
-    for(int i = 0; i < s.length(); i++) {
-        if(i % 2 == 0 && (s[i] >= 'A' && s[i] <= 'E') || (s[i] >= 'a' && s[i] <= 'u')) {
+    for(int i = 0; i < s.length(); i++){
+        if(i % 2 == 0 && (s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'|| s[i] >= 'A' && s[i] <= 'U')){
             count++;
         }
     }
     return count;
-}
-
-int main() {
-    assert(count_upper("EEEE") == 4);
-    return 0;
 }
