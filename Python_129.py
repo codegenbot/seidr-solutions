fd4a6de0c0df3ba68f0fa8a803c701bcdf5b1f39
beep boop <githubbot@vadim.me>
2024-08-03 18:50:19 +0200

    Solution 28 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/12nxy9fs Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f78f23e-bc6b-40be-9eb2-27a50079e dec.py", line 18, in minPath     dfs(i, j, [grid[i][j], i, j])   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f78f23e-bc6b-40be-9eb2-27a50079e dec.py", line 14, in dfs     dfs(ni, nj, path + [(grid[ni][nj], ni, nj)])   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f78f23e-bc6b-40be-9eb2-27a50079e dec.py", line 14, in dfs     dfs(ni, nj, path + [(grid[ni][nj], ni, nj)])   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f78f23e-bc6b-40be-9eb2-27a50079e dec.py", line 14, in dfs     dfs(ni, nj, path + [(grid[ni][nj], ni, nj)])   [Previous line repeated 4 more times]   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5f78f23e-bc6b-40be-9eb2-27a50079e dec.py", line 8, in dfs     res = sorted(path)           ^^^^^^^^^^^^ TypeError: '<' not supported between instances of 'tuple' and 'int'

--- Python_129.py
+++ Python_129.py
@@ -4,8 +4,7 @@ def minPath(grid, k):
 
     def dfs(i, j, path):
         if len(path) == k:
-            if len(path) == k:
-                res.append(sorted([t[0] for t in path]))
+            res.append(sorted([t[0] for t in path]))
             return
 
         for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
