7d51653dfd4a2816b3f91e4fa851ab9e029c2649
beep boop <githubbot@vadim.me>
2024-08-03 02:35:52 +0200

    Solution 10 of CPP/108, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/9zc6sgim Prompt: 7b49be45-1ca2-47d2-8764-6da2a41922ee.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int count_nums(vector<int> n) {       |                ^~~~~~ 7b49be45-1ca2-47d2-8764-6da2a41922ee.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int count_nums(vector<int> n) {       |                       ^~~ In file included from 7b49be45-1ca2-47d2-8764-6da2a41922ee.cpp:34: 7b49be45-1ca2-47d2-8764-6da2a41922ee.cpp: In function ‘int main()’: 7b49be45-1ca2-47d2-8764-6da2a41922ee.cpp:36:23: error: ‘count_nums’ cannot be us ed as a function    36 |     assert (count_nums({1}) == 1);       |             ~~~~~~~~~~^~~~~

--- CPP_108.cpp
+++ CPP_108.cpp
@@ -1,17 +1,33 @@
+using namespace std;
+
 int count_nums(vector<int> n) {
     int count = 0;
     for (int num : n) {
-        if (num < 0) {
-            num = -num;
+        bool has_positive_sum = false;
+        if (num >= 0) {
+            int sum = 0;
+            int temp = num;
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += digit;
+                temp /= 10;
+            }
+            if (sum > 0)
+                has_positive_sum = true;
+        } else {
+            int sum = 0, sign = 1;
+            int temp = -num;
+            while (temp > 0) {
+                int digit = temp % 10;
+                if (digit < 0)
+                    sign = -1;
+                sum += sign * digit;
+                temp /= 10;
+            }
+            if (sum > 0)
+                has_positive_sum = true;
         }
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        if (sum > 0) {
+        if (has_positive_sum)
             count++;
-        }
     }
     return count;
-}
