233a3ed24a0864228daa33b89fd3b43bacdc1f21
beep boop <githubbot@vadim.me>
2024-08-03 13:31:27 +0200

    Solution 38 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/p2afjmpl Prompt: 91c796bc-4fea-4f18-99c4-487aea04a8bf.cpp: In function ‘std::string camelCase(std ::string)’: 91c796bc-4fea-4f18-99c4-487aea04a8bf.cpp:7:29: error: variable ‘std::stringstrea m ss’ has initializer but incomplete type     7 |     std::stringstream ss(str);       |                             ^

--- camel-case.cpp
+++ camel-case.cpp
@@ -15,31 +15,8 @@ std::string camelCase(std::string str) {
     for(int i = 0; i < words.size(); i++) {
         if(i > 0)
             words[i][0] = toupper((char)words[i][0]);
-    }
-
-    std::string result;
-    for(int i = 0; i < words.size(); i++) {
-        result += words[i];
-        if(i < words.size() - 1) {
-            result += " ";
-        }
-    }
-
-    int j = 0;
-    while(j < result.length()) {
-        if(result[j] == ' ') {
-            result.erase(j, 1);
-        } else {
-            j++;
-        }
-    }
-
-    for(int i = 0; i < words.size(); i++) {
-        if(i > 0) {
-            result += std::string(1, toupper((char)words[i][0])) + std::string(words[i].erase(0, 1));
-        } else {
+        else
             result += words[i];
-        }
     }
 
     return result;
