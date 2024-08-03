2c4d508b6c19044f091f9595f3e260065e61b4c2
beep boop <githubbot@vadim.me>
2024-08-03 15:02:09 +0200

    Solution 20 of Python/69, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/z0ewmd2g Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/1b994f02-e6d2-4a79-b24c-534468121 d60.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_69.py
+++ Python_69.py
@@ -1,17 +1,16 @@
 ```
-def search(lst):
+def search():
+    lst = [int(x) for x in input("Enter a list of numbers (separated by space): ").split()]
+    max_num = -1
     freq_dict = {}
     for num in lst:
         if num in freq_dict:
             freq_dict[num] += 1
         else:
             freq_dict[num] = 1
-    max_num = -1
     for num, freq in freq_dict.items():
         if freq >= num and num > 0:
             max_num = num
     return max_num
 
-nums = input("Enter a list of numbers separated by space: ")
-numbers = [int(num) for num in nums.split()]
-print(search(numbers))
+print(search())
