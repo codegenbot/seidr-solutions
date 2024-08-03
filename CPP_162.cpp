24eec3e8846f2c6bd91e9d1562ab191dd6c596b4
beep boop <githubbot@vadim.me>
2024-08-03 14:15:49 +0200

    Solution 48 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/nfq4xinv Prompt: In file included from ae8857c3-437b-4bba-8ee0-4753d6f659a3.cpp:3: ae8857c3-437b-4bba-8ee0-4753d6f659a3.cpp: In function ‘int main()’: ae8857c3-437b-4bba-8ee0-4753d6f659a3.cpp:5:13: error: ‘string_to_md5’ was not de clared in this scope     5 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,27 +1,27 @@
 #include <openssl/evp.h>
-#include <iostream>
 #include <string>
 #include <cassert>
 
 std::string string_to_md5(const std::string& input) {
-    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit(mdctx, EVP_md5());
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md = EVP_md5();
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
     EVP_DigestUpdate(mdctx, input.c_str(), input.length());
-    unsigned char hash[EVP_MAX_MD_SIZE];
-    unsigned int hash_len;
-    EVP_DigestFinal(mdctx, hash, &hash_len);
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
     EVP_MD_CTX_free(mdctx);
-    
+
     char mdString[33];
-    for (int i = 0; i < 16; ++i) {
-        sprintf(&mdString[i * 2], "%02x", (unsigned int)hash[i]);
-    }
-    
+    for(int i = 0; i < md_len; i++)
+        sprintf(&mdString[i*2], "%02x", (unsigned int)md_value[i]);
+
     return std::string(mdString);
 }
 
 int main() {
     assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-    
     return 0;
 }
