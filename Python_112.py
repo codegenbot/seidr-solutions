b98c6458fabd082101a1940cb0d0d3644ce5b8d8
beep boop <githubbot@vadim.me>
2024-08-03 16:40:40 +0200

    Solution 1 of Python/112, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/0yrl5se6 Prompt: Complete the following code given the task description and function signature.

--- Python_112.py
+++ Python_112.py
@@ -1,3 +1,3 @@
 def reverse_delete(s, c):
-    result = [char for char in s if char not in c]
-    return ("".join(result), result == "".join(reversed(result)))
+    new_s = "".join([x for x in s if x not in c])
+    return (new_s, new_s == new_s[::-1])
