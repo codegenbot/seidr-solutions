97244f8ee0973f2b23c20721d1a7dfa540f57350
beep boop <githubbot@vadim.me>
2024-08-03 07:41:30 +0200

    Solution 3 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5lzs9pe3 Prompt:       |                    ^~~ 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp:9: 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp: In function ‘int main()’: 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp:19:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘product’?    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 136c1eb5-eec4-41d5-970e-015c3a6563da.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,14 +1,12 @@
 #include <vector>
 
-std::pair<int, int> sum_product(const std::vector<int>& numbers) {
+std::pair<int, int> sum_product(std::vector<int> numbers) {
     int sum = 0;
     int product = 1;
-    
     for (int num : numbers) {
         sum += num;
         product *= num;
     }
-    
     return {sum, product};
 }
 
@@ -18,6 +16,4 @@ bool issame(std::pair<int, int> a, std::pair<int, int> b) {
 
 int main() {
     assert(issame(sum_product({10}), {10, 10}));
-
-    return 0;
 }
