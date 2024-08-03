d126bad4c3bab5efe47f93de857f8a28cfe05b0c
beep boop <githubbot@vadim.me>
2024-08-03 03:34:50 +0200

    Solution 30 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/pfe9otlx Prompt: 897ba6bd-535c-4ae5-be35-0faf907d7446.cpp: In function ‘std::string string_to_md5 (const string&)’: 897ba6bd-535c-4ae5-be35-0faf907d7446.cpp:12:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    12 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 897ba6bd-535c-4ae5-be35-0faf907d7446.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ /usr/bin/ld: /tmp/ccDnKW7l.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 897ba6bd-535c-4ae5-be35-0faf907d7446.cpp:(.text+0x97): undefined reference to `M D5' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,22 +8,22 @@ std::string string_to_md5(const std::string& text) {
         return "None";
     }
 
-    unsigned char digest[EVP_MAX_MD_SIZE];
     EVP_MD_CTX* mdctx;
-    const EVP_MD* md;
-    unsigned int md_len;
-    
-    md = EVP_md5();
+    const EVP_MD* md = EVP_md5();
+    unsigned char digest[EVP_MAX_MD_SIZE];
+    unsigned int digest_len;
+
     mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, md, NULL);
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
-    EVP_DigestFinal_ex(mdctx, digest, &md_len);
+    EVP_DigestFinal_ex(mdctx, digest, &digest_len);
     EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
-    for(int i = 0; i < md_len; i++) {
-        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+    for(unsigned int i = 0; i < digest_len; i++) {
+        sprintf(&mdString[i*2], "%02x", digest[i]);
     }
+    mdString[32] = '\0';
 
     return mdString;
 }
