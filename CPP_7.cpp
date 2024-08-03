f6e6568e194ef45ee4e407079155a959ebf62461
beep boop <githubbot@vadim.me>
2024-08-03 07:24:10 +0200

    Solution 61 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/h8svvfg5 Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:10: 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp: In function ‘int main()’: 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:20:20: error: ‘filter_by_substring’ was  not declared in this scope    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |                    ^~~~~~~~~~~~~~~~~~~ 84c89a6c-ca6c-4848-9d79-9ad26b598816.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_substring({"grunt", "trumpet", "prune", "gr uesome"}, "run") ,{"grunt", "prune"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -1,33 +1,14 @@
-#include <cassert>
-
 bool issame(vector<string> a, vector<string> b) {
-    if(a.size() != b.size()) {
+    if(a.size() != b.size())
         return false;
-    }
     for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
+        bool found = false;
+        for(const auto& s : b) {
+            if(a[i].find(s) != string::npos)
+                found = true;
         }
+        if(!found)
+            return false;
     }
     return true;
-}
-
-vector<string> filter_by_substring(vector<string> strings, string substring) {
-    vector<string> result;
-    for (const auto& s : strings) {
-        if (issame(split(s, substring), split(substring, substring)) && s.find(substring) != string::npos)
-            result.push_back(s);
-    }
-    return result;
-}
-
-vector<string> split(string str, string delimiter) {
-    vector<string> result;
-    size_t pos = 0;
-    while ((pos = str.find(delimiter)) != string::npos) {
-        result.push_back(str.substr(0, pos));
-        str.erase(0, pos + delimiter.length());
-    }
-    result.push_back(str);
-    return result;
 }
