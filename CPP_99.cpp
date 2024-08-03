4948418dd35bfca71ff044cb00e0d6943057c926
beep boop <githubbot@vadim.me>
2024-08-03 10:47:30 +0200

    Solution 1 of CPP/99, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/fpe688c2 Prompt: Complete the following code given the task description and function signature.

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,11 +1,9 @@
-#include <string>
+#include<stdio.h>
+#include<math.h>
+#include<string>
+using namespace std;
 
 int closest_integer(string value) {
     double num = stod(value);
-    int integerPart = (int)num;
-    if (abs(num - integerPart) >= 0.5) {
-        return num > 0 ? integerPart + 1 : integerPart - 1;
-    } else {
-        return integerPart;
-    }
+    return (num >= 0) ? ceil(num) : floor(num);
 }
