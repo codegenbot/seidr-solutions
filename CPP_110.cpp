1495a6c0b1385b968442b74641e9c7fb1a2eef1a
beep boop <githubbot@vadim.me>
2024-08-03 05:55:09 +0200

    Solution 2 of CPP/110, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4aatf51p Prompt: 85238858-3d75-4fdc-b4e6-a23e0a65e230.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string exchange(vector<int> lst1, vector<int> lst2) {       | ^~~~~~ In file included from 85238858-3d75-4fdc-b4e6-a23e0a65e230.cpp:10: 85238858-3d75-4fdc-b4e6-a23e0a65e230.cpp: In function ‘int main()’: 85238858-3d75-4fdc-b4e6-a23e0a65e230.cpp:12:13: error: ‘exchange’ was not declar ed in this scope    12 |     assert (exchange({100, 200}, {200, 200}) == "YES");       |             ^~~~~~~~

--- CPP_110.cpp
+++ CPP_110.cpp
@@ -1,5 +1,15 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
 string exchange(vector<int> lst1, vector<int> lst2) {
     for (int num : lst1) {
+        if (num % 2 != 0) {
+            return "NO";
+        }
+    }
+    for (int num : lst2) {
         if (num % 2 != 0) {
             return "NO";
         }
