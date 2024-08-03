da7b0945555ffbc85d4ddeef6a98e97319a09f7d
beep boop <githubbot@vadim.me>
2024-08-03 09:10:28 +0200

    Solution 34 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2k819xey Prompt:     4 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ a305e378-7e61-4e2f-ab76-065393bf45e5.cpp:4:27: error: ‘vector’ was not declared in this scope     4 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ a305e378-7e61-4e2f-ab76-065393bf45e5.cpp:4:34: error: expected primary-expressio n before ‘int’     4 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from a305e378-7e61-4e2f-ab76-065393bf45e5.cpp:3: a305e378-7e61-4e2f-ab76-065393bf45e5.cpp: In function ‘int main()’: a305e378-7e61-4e2f-ab76-065393bf45e5.cpp:13:20: error: ‘minPath’ was not declare d in this scope    13 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |                    ^~~~~~~ a305e378-7e61-4e2f-ab76-065393bf45e5.cpp:13:19: error: ‘issame’ cannot be used a s a function    13 |     assert (issame(minPath({{1, 3}, {3, 2}}, 10) , {1, 3, 1, 3, 1, 3, 1,  3, 1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,45 +1,5 @@
 #include <vector>
-#include <algorithm>
-#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
-
-std::vector<int> minPath(std::vector<std::vector<int>> grid, int k) {
-    int n = grid.size();
-    int m = grid[0].size();
-
-    std::vector<std::vector<int>> dp(n, std::vector<int>(m, k + 1));
-    dp[0][0] = grid[0][0];
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (i > 0) {
-                dp[i][j] = std::min(dp[i][j], dp[i - 1][j] + grid[i][j]);
-            }
-            if (j > 0) {
-                dp[i][j] = std::min(dp[i][j], dp[i][j - 1] + grid[i][j]);
-            }
-        }
-    }
-
-    std::vector<int> path;
-    int x = n - 1, y = m - 1;
-    path.push_back(grid[x][y]);
-
-    while (x > 0 || y > 0) {
-        if (x > 0 && dp[x][y] - grid[x][y] == dp[x - 1][y]) {
-            x--;
-        } else {
-            y--;
-        }
-        path.push_back(grid[x][y]);
-    }
-
-    std::reverse(path.begin(), path.end());
-
-    return path;
-}
-
-assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
