8eba4241fce35acc68d2310c0145eed062fb3dee
beep boop <githubbot@vadim.me>
2024-08-03 13:31:43 +0200

    Solution 90 of CPP/152, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/03jeaqq5 Prompt: 84a011c1-d51b-4c7e-a95f-7fff4a79b731.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ 84a011c1-d51b-4c7e-a95f-7fff4a79b731.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main(){       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b){
+bool isequal(const vector<int>& a, const vector<int>& b){
     return a == b;
 }
 
@@ -14,9 +14,4 @@ vector<int> compare(const vector<int>& game, const vector<int>& guess){
         result.push_back(abs(game[i] - guess[i]));
     }
     return result;
-}
-
-int main(){
-    assert(issame(compare({1, 2, 3, 5}, {-1, 2, 3, 4}), vector<int>({2, 0, 0, 1})));
-    return 0;
 }
