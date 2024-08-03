b4f5ba78e8d14513a526b6e125ea94679b2aed13
beep boop <githubbot@vadim.me>
2024-08-03 10:19:05 +0200

    Solution 61 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/5nbp4vc7 Prompt:       |                                 ^~~~~~ In file included from d53b2202-71b4-45f2-9462-475d31d18463.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ d53b2202-71b4-45f2-9462-475d31d18463.cpp:41:44: error: ‘b’ was not declared in t his scope    41 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d53b2202-71b4-45f2-9462-475d31d18463.cpp:41:45: error: expression list treated a s compound expression in initializer [-fpermissive]    41 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d53b2202-71b4-45f2-9462-475d31d18463.cpp:40: d53b2202-71b4-45f2-9462-475d31d18463.cpp: In function ‘int main()’: d53b2202-71b4-45f2-9462-475d31d18463.cpp:50:20: error: ‘numerical_letter_grade’ was not declared in this scope    50 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ d53b2202-71b4-45f2-9462-475d31d18463.cpp:50:19: error: ‘issame’ cannot be used a s a function    50 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -6,7 +6,7 @@ bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b
 }
 
 std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
-    std::vector<string> result;
+    vector<string> result;
 
     for (float grade : grades) {
         string letterGrade = "";
