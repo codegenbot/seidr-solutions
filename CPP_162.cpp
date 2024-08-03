a050e203afcc098f4b1272b417deb3af80659068
beep boop <githubbot@vadim.me>
2024-08-03 13:12:18 +0200

    Solution 10 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/44w35chd Prompt: 5aea95ae-671e-4777-8bff-74e338687f2f.cpp:4:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     4 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 5aea95ae-671e-4777-8bff-74e338687f2f.cpp:25: 5aea95ae-671e-4777-8bff-74e338687f2f.cpp: In function ‘int main()’: 5aea95ae-671e-4777-8bff-74e338687f2f.cpp:27:13: error: ‘string_to_md5’ was not d eclared in this scope; did you mean ‘string_to_hex’?    27 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,20 +1,35 @@
-#include <sstream>
+#include <string>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+string string_to_md5(string text);
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    string output = string_to_md5(input);
+    cout << "MD5 of the input string is: " << output << endl;
+    return 0;
+}
 
 string string_to_md5(string text) {
     if (text.empty()) return "";
     
-    unsigned char mdBuffer[MD5_DIGEST_LENGTH];
-    MD5_CTX mdContext;
-    MD5_Init(&mdContext);
-    const char* str = text.c_str();
-    size_t length = text.length();
-    MD5_Update(&mdContext, str, length);
-    MD5_Final(mdBuffer, &mdContext);
+    unsigned char md5[16];
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    const char* ptr = text.c_str();
+    while (*ptr) {
+        MD5_Update(&ctx, ptr, 1);
+        ptr++;
+    }
+    MD5_Final(md5, &ctx);
 
-    stringstream ss;
-    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
-        ss << setfill('0') << setw(2) << hex << (int) mdBuffer[i];
+    string result;
+    for (int i = 0; i < 16; i++) {
+        sprintf(result + strlen(result), "%02x", md5[i]);
     }
 
-    return ss.str();
+    return result;
 }
