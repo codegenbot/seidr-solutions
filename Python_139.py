18d5e35f01f4c75b6af066523fa9fe9da540fa94
beep boop <githubbot@vadim.me>
2024-08-03 18:24:17 +0200

    Solution 2 of Python/139, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ar1emsit Prompt: Complete the following code given the task description and function signature.

--- Python_139.py
+++ Python_139.py
@@ -2,6 +2,6 @@ def special_factorial(n):
     if n < 1:
         return "Input must be a positive integer"
     result = 1
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         result *= math.factorial(i)
     return result
