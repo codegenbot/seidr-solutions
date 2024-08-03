79b2d8ca30cf9979d478535123e4ea45b11b0bcb
beep boop <githubbot@vadim.me>
2024-08-03 12:36:44 +0200

    Solution 52 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/crc9dynb Prompt: 8e660b0e-2b0b-4ce9-894e-2a50395bbfce.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ 8e660b0e-2b0b-4ce9-894e-2a50395bbfce.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from 8e660b0e-2b0b-4ce9-894e-2a50395bbfce.cpp:11: 8e660b0e-2b0b-4ce9-894e-2a50395bbfce.cpp: In function ‘int main()’: 8e660b0e-2b0b-4ce9-894e-2a50395bbfce.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,10 +1,9 @@
 #include <vector>
-#include <cmath>
 
 int specialFilter(vector<int> nums) {
     int count = 0;
     for (int num : nums) {
-        if (abs(num) > 10 && (num % 10) % 2 != 0 && (std::abs(num / 10)) % 10 % 2 != 0) {
+        if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
             count++;
         }
     }
