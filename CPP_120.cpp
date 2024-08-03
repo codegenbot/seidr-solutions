3401b441a859a7c4c5b3054b07473fd53da5ec05
beep boop <githubbot@vadim.me>
2024-08-03 11:30:53 +0200

    Solution 17 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/g55asi6l Prompt:       |                    ^~~ 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp:8:27: error: ‘vector’ was not declared in this scope     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp:8:39: error: expression list treated as  compound expression in initializer [-fpermissive]     8 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp:7: 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp: In function ‘int main()’: 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp:17:20: error: ‘maximum’ was not declare d in this scope    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 5ef9a164-a4ef-41fa-afb9-8a7d95f438c4.cpp:17:19: error: ‘issame’ cannot be used a s a function    17 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -10,8 +10,16 @@ bool issame(vector<int> a,vector<int>b){
 
 vector<int> maximum(vector<int> arr, int k) {
     vector<int> result(arr.begin(), arr.end());
-    sort(result.rbegin(), result.rend());
-    for(int i = 0; i < k; i++){
-        cout << result[i] << endl;
+    sort(result.begin(), result.end());
+    for(int i = 0; i < k-1; i++){
+        if(result[i] != result[k-1]){
+            return vector<int>(1,result[k-1]);
+        }
     }
+    return vector<int>();
+}
+
+int main() {
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 7), {0}));
+    return 0;
 }
