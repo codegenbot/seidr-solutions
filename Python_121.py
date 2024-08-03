542715e01936d94dd1ebd7fd0e696c0f02df803f
beep boop <githubbot@vadim.me>
2024-08-03 18:34:58 +0200

    Solution 96 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ihoobi73 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0b486b0d-49b5-43f6-9aba-14d146173 28a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,5 +1,4 @@
 ```
 def solution():
-    n = int(input("Enter number of elements in list: "))
-    lst = list(map(int, input("Enter {} numbers separated by space: ".format(n)).split()))
+    lst = list(map(int, input("Enter space-separated numbers: ").split()))
     return sum(i for i in lst[1::2] if i % 2 != 0)
