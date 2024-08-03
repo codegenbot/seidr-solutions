0c1b23a0749f1dc0a6c4287be9eeda844a7bd678
beep boop <githubbot@vadim.me>
2024-08-03 12:45:13 +0200

    Solution 25 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2x26omsn Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d3fc00dc-6080-4e87-8aa8-33a3815ef f8f.py", line 1     Here is the completed code:                 ^^^^^^^^^ SyntaxError: invalid syntax

--- Python_32.py
+++ Python_32.py
@@ -1,9 +1,1 @@
 def find_zero(xs):
-    sum_even = 0
-    sum_odd = 1
-    for x in xs:
-        if abs(x) % 2 == 0:
-            sum_even += x
-        else:
-            sum_odd *= x
-    return -sum_even / sum_odd
