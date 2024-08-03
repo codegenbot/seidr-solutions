06621140f3a63556ca6ab282c1995078c12447fd
beep boop <githubbot@vadim.me>
2024-08-04 01:24:58 +0200

    Solution 6 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wknwp7rz Prompt: a0d39455-c509-4164-a185-cdb63cdf6e47.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int solutions(vector<int> lst){       |               ^~~~~~ a0d39455-c509-4164-a185-cdb63cdf6e47.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int solutions(vector<int> lst){       |                      ^~~ In file included from a0d39455-c509-4164-a185-cdb63cdf6e47.cpp:11: a0d39455-c509-4164-a185-cdb63cdf6e47.cpp: In function ‘int main()’: a0d39455-c509-4164-a185-cdb63cdf6e47.cpp:13:22: error: ‘solutions’ cannot be use d as a function    13 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -1,11 +1,16 @@
 #include <vector>
+using namespace std;
 
 int solutions(std::vector<int> lst){
     int sum = 0;
-    for(size_t i = 0; i < lst.size(); i += 2){
+    for(int i = 0; i < lst.size(); i += 2){
         if(lst[i] % 2 != 0){
             sum += lst[i];
         }
     }
     return sum;
+}
+
+int main(){
+    assert (solutions(std::vector<int>{3, 13, 2, 9}) == 3);
 }
