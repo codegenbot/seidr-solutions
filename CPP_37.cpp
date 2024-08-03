450e76959de2bd41102e732b42a484b3a325aeb8
beep boop <githubbot@vadim.me>
2024-08-03 13:20:20 +0200

    Solution 50 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q5n63q1c Prompt:     5 | c0bf774b-7173-4139-86ec-2a8d66d508d8.cpp:38:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    38 | int main(){       |         ^~ c0bf774b-7173-4139-86ec-2a8d66d508d8.cpp:38:9: note: remove parentheses to defau lt-initialize a variable    38 | int main(){       |         ^~       |         -- c0bf774b-7173-4139-86ec-2a8d66d508d8.cpp:38:9: note: or replace parentheses with  braces to value-initialize a variable c0bf774b-7173-4139-86ec-2a8d66d508d8.cpp:38:11: error: a function-definition is not allowed here before ‘{’ token    38 | int main(){       |           ^ c0bf774b-7173-4139-86ec-2a8d66d508d8.cpp:40:2: error: expected ‘}’ at end of inp ut    40 | }       |  ^ c0bf774b-7173-4139-86ec-2a8d66d508d8.cpp:16:12: note: to match this ‘{’    16 | int main() {       |            ^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -18,5 +18,10 @@ int main() {
     std::vector<float> tmp = v;
     sort_even(tmp);
     assert(issame(tmp, {-12, 8, 3, 4, 23, 2, 12, 11, 5, -10}));
+
+    std::vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
+    sort_even(input);
+    assert(issame(input, {-12, 8, 3, 4, 23, 2, 12, 11, 5, -10}));
+
     return 0;
 }
