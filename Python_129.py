0af18b5c3685a7c94d0eabaa0e6b0f30ce1a3c06
beep boop <githubbot@vadim.me>
2024-08-03 04:40:08 +0200

    Solution 47 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bzwx60mc Prompt:            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 12, in dfs     res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 12, in dfs     res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 12, in dfs     res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   [Previous line repeated 1 more time]   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 13, in dfs     return min(res)            ^^^^^^^^ ValueError: min() arg is an empty sequence

--- Python_129.py
+++ Python_129.py
@@ -10,7 +10,7 @@ def minPath(grid, k):
         for x, y in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             ni, nj = i + x, j + y
             if 0 <= ni < n and 0 <= nj < n and m[ni][nj] not in visited:
-                res = min(res, dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]})) if res is None else min((res,), (dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}),))
+                res = min(res, dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]})) if res is None else min([res] + [dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]})])
         return res
 
     return dfs(0, 0, [], set())
