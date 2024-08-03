cf3785c61c89ec17eb99b832b649a29eb7336f9f
beep boop <githubbot@vadim.me>
2024-08-03 06:15:42 +0200

    Solution 84 of Python/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/qizcu09b Prompt: Program did not receive expected input

--- Python_162.py
+++ Python_162.py
@@ -4,5 +4,5 @@ import sys
 def string_to_md5(text):
     return hashlib.md5(text.encode()).hexdigest()
 
-text = sys.stdin.readline().rstrip()
+text = sys.stdin.readline().strip()
 print(string_to_md5(text))
