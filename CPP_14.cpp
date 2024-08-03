8d0ea71ff1adeaf83a04be4a0433fe0c6183626e
beep boop <githubbot@vadim.me>
2024-08-03 10:02:09 +0200

    Solution 28 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/2iuihads Prompt: In file included from /usr/include/c++/11/string:39,                  from 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp:34:5: error: redefinition of ‘int main( )’    34 | int main(){       |     ^~~~ 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp:25: 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp: In function ‘int main()’: 9ce19b97-3c83-4a95-a725-7752bf46c46c.cpp:35:20: error: ‘all_prefixes’ was not de clared in this scope    35 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -5,19 +5,19 @@
 std::vector<std::string> b(std::string str) {
     std::vector<std::string> prefixes;
     std::string prefix = "";
-    for (char c : str) {
+    for (const char& c : str) {
         prefix += c;
         prefixes.push_back(prefix);
     }
     return prefixes;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
 
 int main() {
-    auto all_prefixes = b;
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    auto all_prefixes = b("WWW");
+    assert(issame(all_prefixes, {"W", "WW", "WWW"}));
     return 0;
 }
