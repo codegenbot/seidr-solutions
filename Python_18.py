06e963d47b8d3518a56b6572da380a80b79eda39
beep boop <githubbot@vadim.me>
2024-08-03 10:25:10 +0200

    Solution 1 of Python/18, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/lynd2ujb Prompt: Complete the following code given the task description and function signature.

--- Python_18.py
+++ Python_18.py
@@ -1,3 +1,8 @@
-```
 def how_many_times(string: str, substring: str) -> int:
-    return len([i for i in range(len(string)) if string[i:].startswith(substring)])
+    return len(
+        [
+            i
+            for i in range(len(string) - len(substring) + 1)
+            if string[i : i + len(substring)] == substring
+        ]
+    )
