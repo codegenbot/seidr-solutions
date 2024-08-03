e5edd836df21e59888abd9e45c70251be8c494ea
beep boop <githubbot@vadim.me>
2024-08-03 09:25:22 +0200

    Solution 12 of CPP/64, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/h0tubd2h Prompt: 4ee28338-c3ee-41c9-9bdd-004dbb9d4d88.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int vowels_count(string s){       |                  ^~~~~~ In file included from 4ee28338-c3ee-41c9-9bdd-004dbb9d4d88.cpp:12: 4ee28338-c3ee-41c9-9bdd-004dbb9d4d88.cpp: In function ‘int main()’: 4ee28338-c3ee-41c9-9bdd-004dbb9d4d88.cpp:14:25: error: ‘vowels_count’ cannot be used as a function    14 |     assert (vowels_count("ACEDY") == 3);       |             ~~~~~~~~~~~~^~~~~~~~~

--- CPP_64.cpp
+++ CPP_64.cpp
@@ -1,4 +1,6 @@
-int vowels_count(string s){
+#include <string>
+
+int vowels_count(std::string s){
     int count = 0;
     for(int i = 0; i < s.length(); i++){
         if((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') 
