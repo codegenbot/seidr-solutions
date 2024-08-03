da61d64ca47ba08afecaec4e5f70acc76595cd7b
beep boop <githubbot@vadim.me>
2024-08-03 16:18:52 +0200

    Solution 98 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/0e18qzqi Prompt: 941abec1-6493-4cf0-939f-22f5c7ddd8ac.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -2,23 +2,23 @@
 
 int bowlingScore(string s) {
     int score = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s[i] == 'X') {
-            score += 10 + (i < 2 ? 10 : s[i - 1] == 'X' ? 20 : s[i - 1] == '/' ? 15 : s[i - 1] - 1);
-        } else if (s[i] == '/') {
-            int currentRoll = 10 - (s[i + 1] == 'X' ? 10 : s[i + 1] - 1);
-            score += currentRoll;
-            i++;
-        } else {
-            int currentRoll = s[i] - '0';
-            if (i < 8 && s[i + 1] != '/' && s[i + 1] == 'X') {
-                currentRoll *= 2;
-            } else if (i < 8 && s[i + 1] != '/' && s[i + 1] - '0' > currentRoll) {
-                currentRoll += s[i + 1] - '0';
-                i++;
+    int currentRoll = 0;
+    for (char c : s) {
+        if (c == 'X') {
+            score += 10 + (currentRoll > 0 ? currentRoll : 10);
+            currentRoll = 0;
+        } else if (c == '/') {
+            score += 10 - currentRoll;
+            currentRoll = 0;
+        } else if (isdigit(c)) {
+            int roll = c - '0';
+            while (c != ' ' && c != '/' && c != '\0') {
+                c = getchar();
+                if (c >= '0' && c <= '9') {
+                    roll = roll * 10 + c - '0';
+                } else break;
             }
-            score += currentRoll;
+            score += roll;
         }
     }
     return score;
-}
