4f8892e3dca7875e19436e08a41c9a8d44d665d5
beep boop <githubbot@vadim.me>
2024-08-03 12:14:55 +0200

    Solution 22 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ikpv5dlc Prompt:       |             ^~~~~~ fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp:22: fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp: In function ‘int main()’: fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp:32:20: error: ‘get_positive’ was not de clared in this scope    32 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ fa1ce943-6df6-403e-bbe7-2c13c9a9fffd.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,13 +1,12 @@
 #include <vector>
 #include <cassert>
-using namespace std;
 
-bool issame(vector<float> a, vector<float> b) {
+bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
 
-vector<float> get_positive(vector<float> l){
-    vector<float> result;
+std::vector<float> get_positive(std::vector<float> l) {
+    std::vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
