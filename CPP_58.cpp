f79e587ba32501badeaed60f5c1547067deb8d0d
beep boop <githubbot@vadim.me>
2024-08-03 02:26:41 +0200

    Solution 44 of CPP/58, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/xg5ailwj Prompt:       |           ^~~~~~ In file included from 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp:9:5: note: ‘int main()’ previously defi ned here     9 | int main() {       |     ^~~~ In file included from 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp:14: 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp: In function ‘int main()’: 2d3ed138-fee0-42d4-ba46-2642bade0fa6.cpp:24:20: error: ‘common’ was not declared  in this scope    24 |     assert (issame(common({4, 3, 2, 8}, {}) , {}));       |                    ^~~~~~

--- CPP_58.cpp
+++ CPP_58.cpp
@@ -6,7 +6,4 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return std::equal(a.begin(), a.end(), b.begin(), b.end());
 }
 
-int main() {
-    assert(issame(std::vector<int>{4, 3, 2, 8}, std::vector<int>{}));
-    return 0;
-}
+assert(issame(std::vector<int>{4, 3, 2, 8}, std::vector<int>{}));
