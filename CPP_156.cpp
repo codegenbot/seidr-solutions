ef480cb3bb152af14149c5bf3558c1fc57503330
beep boop <githubbot@vadim.me>
2024-08-03 13:02:00 +0200

    Solution 41 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/oodq41ld Prompt: 94c288be-94fd-4f32-9bc3-be1914f4ad81.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_roman(int number) {       | ^~~~~~ In file included from 94c288be-94fd-4f32-9bc3-be1914f4ad81.cpp:18: 94c288be-94fd-4f32-9bc3-be1914f4ad81.cpp: In function ‘int main()’: 94c288be-94fd-4f32-9bc3-be1914f4ad81.cpp:20:13: error: ‘int_to_mini_romank’ was not declared in this scope    20 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -1,4 +1,4 @@
-```cpp
+```
 string int_to_mini_roman(int number) {
     vector<pair<int, string>> romanMap = {{1000, "M"}, {900, "CM"}, {500, "D"},
                                             {400, "CD"}, {100, "C"}, {90, "XC"},
@@ -15,4 +15,5 @@ string int_to_mini_roman(int number) {
     
     return roman;
 }
+
 assert(int_to_mini_roman(1000) == "M");
