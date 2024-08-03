02117a5ae0d6709ceec0a23a5ac01dd376bc31c2
beep boop <githubbot@vadim.me>
2024-08-03 15:00:49 +0200

    Solution 27 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/0msecuc8 Prompt: 7d2256c5-98de-48d1-9c0e-83da3c2c8ece.cpp:47:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    47 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 7d2256c5-98de-48d1-9c0e-83da3c2c8ece.cpp:10:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    10 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 7d2256c5-98de-48d1-9c0e-83da3c2c8ece.cpp:55:5: error: redefinition of ‘int main( )’    55 | int main(){       |     ^~~~ 7d2256c5-98de-48d1-9c0e-83da3c2c8ece.cpp:40:5: note: ‘int main()’ previously def ined here    40 | int main() {       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -34,7 +34,7 @@ vector<int> count_up_to(int n) {
 }
 
 int main() {
-    assert(std::equal(count_up_to(101).begin(), count_up_to(101).end(), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}.begin(), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}.end()));
-
+    assert(count_up_to(101) == std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97});
+    
     return 0;
 }
