75b600c2d756563de5b895993ce7594e2b7d0991
beep boop <githubbot@vadim.me>
2024-08-03 19:59:53 +0200

    Solution 26 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/rjn7zfl4 Prompt:       |                           ^~~~~~ 82258dd0-a94a-4c36-9058-f07509871c74.cpp:37:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 82258dd0-a94a-4c36-9058-f07509871c74.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 82258dd0-a94a-4c36-9058-f07509871c74.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 82258dd0-a94a-4c36-9058-f07509871c74.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 82258dd0-a94a-4c36-9058-f07509871c74.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ 82258dd0-a94a-4c36-9058-f07509871c74.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main(){       |     ^~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,18 +1,6 @@
 #include <vector>
 #include <cassert>
 
-namespace std {
-    bool issame(std::vector<int> a, std::vector<int> b){
-        if(a.size() != b.size()) return false;
-        
-        for(int i=0; i<a.size(); i++){
-            if(a[i] != b[i]) return false;
-        }
-        
-        return true;
-    }
-}
-
 std::vector<int> factorize(int n){
     std::vector<int> factors;
     
@@ -30,7 +18,17 @@ std::vector<int> factorize(int n){
     return factors;
 }
 
+bool issame(std::vector<int> a, std::vector<int> b){
+    if(a.size() != b.size()) return false;
+    
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
+    }
+    
+    return true;
+}
+
 int main(){
-    assert (std::issame(factorize(3 * 2 * 3) , {2, 3, 3}));
+    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
     return 0;
 }
