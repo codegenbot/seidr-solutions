ee20a811c2d6374ee00af97a2eee29b256f17f38
beep boop <githubbot@vadim.me>
2024-08-03 13:00:02 +0200

    Solution 6 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q5n63q1c Prompt:    26 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 6ff7f6a5-f6a0-4741-b412-36c7de8d6c94.cpp:26:36: error: expected primary-expressi on before ‘float’    26 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 6ff7f6a5-f6a0-4741-b412-36c7de8d6c94.cpp:26:43: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 6ff7f6a5-f6a0-4741-b412-36c7de8d6c94.cpp:25: 6ff7f6a5-f6a0-4741-b412-36c7de8d6c94.cpp: In function ‘int main()’: 6ff7f6a5-f6a0-4741-b412-36c7de8d6c94.cpp:35:20: error: ‘sort_even’ was not decla red in this scope    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 6ff7f6a5-f6a0-4741-b412-36c7de8d6c94.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,32 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cassert>
-
-bool issame(std::vector<float> a, std::vector<float> b){
-    return a == b;
+bool issame(const vector<float>& a, const vector<float>& b) {
+    return sort_even(a) == b;
 }
 
-std::vector<float> sort_even(std::vector<float> l) {
-    std::vector<float> even_elements;
+vector<float> sort_even(const vector<float>& l) {
+    vector<float> even_values;
+    vector<float> result;
+
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            even_elements.push_back(l[i]);
+            even_values.push_back(l[i]);
         }
     }
-    std::sort(even_elements.begin(), even_elements.end());
 
-    std::vector<float> l_prime = l;
+    sort(even_values.begin(), even_values.end());
+
     int even_index = 0;
     for (int i = 0; i < l.size(); i++) {
         if (i % 2 == 0) {
-            l_prime[i] = even_elements[even_index];
+            result.push_back(even_values[even_index]);
             even_index++;
+        } else {
+            result.push_back(l[i]);
         }
     }
-    return l_prime;
-}
 
-int main() {
-    assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
+    return result;
 }
