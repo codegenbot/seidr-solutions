b75415e5bbaf8efb87f95078cf6e85c208dae577
beep boop <githubbot@vadim.me>
2024-08-03 07:56:53 +0200

    Solution 45 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bwuu9phl Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 74497448-cbbe-4098-afdd-bdb2426a7e73.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 74497448-cbbe-4098-afdd-bdb2426a7e73.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 74497448-cbbe-4098-afdd-bdb2426a7e73.cpp:11: 74497448-cbbe-4098-afdd-bdb2426a7e73.cpp: In function ‘int main()’: 74497448-cbbe-4098-afdd-bdb2426a7e73.cpp:21:20: error: ‘filter_integers’ was not  declared in this scope    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |                    ^~~~~~~~~~~~~~~ 74497448-cbbe-4098-afdd-bdb2426a7e73.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}) ,{3, 3, 3}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -2,12 +2,11 @@
 #include <boost/any.hpp>
 #include <vector>
 #include <list>
-#include <algorithm>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
@@ -16,11 +15,11 @@ std::vector<int> filter_integers(std::list<boost::any> values) {
     std::vector<int> result;
     for (const auto& value : values) {
         try {
-            int val = boost::any_cast<int>(value);
-            if (val.good()) {
-                result.push_back(val);
+            int num = boost::any_cast<int>(value);
+            if(num.good()) {
+                result.push_back(num);
             }
-        } catch (...) {
+        } catch(const boost::bad_any_cast&) {
             // ignore non-int values
         }
     }
