1a3aa497018a0726049b98f5ed3256fb84e3004c
beep boop <githubbot@vadim.me>
2024-08-03 18:12:13 +0200

    Solution 17 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ihoobi73 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8a3f8c4d-8f20-4515-bd72-85ebc1ffb 487.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,14 +1,7 @@
-def maxProfit(prices):
-    if len(prices) < 2:
-        return 0
-    
-    min_price = prices[0]
-    max_profit = 0
-    
-    for price in prices:
-        if price < min_price:
-            min_price = price
-        elif price - min_price > max_profit:
-            max_profit = price - min_price
-    
-    return max_profit
+def find_even_index(arr):
+    for i in range(len(arr)):
+        left_sum = sum(arr[:i])
+        right_sum = sum(arr[i+1:])
+        if left_sum == right_sum:
+            return i
+    return -1
