304a230f6c48061dcb1cd1ad2efa64fbdb6b5e8b
beep boop <githubbot@vadim.me>
2024-08-03 17:00:25 +0200

    Solution 45 of Python/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/j5wugq9p Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5704ea65-f955-4412-9194-2ca0caeba 616.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_114.py
+++ Python_114.py
@@ -5,7 +5,8 @@ def minSubArraySum(nums):
     min_sum = float('inf')
     left = 0
     current_sum = 0
-    start = end = 0
+    min_start = 0
+    min_end = 0
     for right in range(len(nums)):
         current_sum += nums[right]
         while current_sum > min_sum and left <= right:
@@ -13,6 +14,6 @@ def minSubArraySum(nums):
             left += 1
         if current_sum < min_sum:
             min_sum = current_sum
-            start = left
-            end = right
-    return nums[start:end+1]
+            min_start = left
+            min_end = right
+    return nums[min_start:min_end+1]
