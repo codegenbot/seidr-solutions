5e1594cc9e75de96ea48d2792c481521d200c694
beep boop <githubbot@vadim.me>
2024-08-04 01:04:42 +0200

    Solution 8 of CPP/113, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/8t5mn9uu Prompt:       |                                            ^ c6c2fbda-fdbb-4b96-a806-f8e09055e296.cpp:16:45: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from c6c2fbda-fdbb-4b96-a806-f8e09055e296.cpp:15: c6c2fbda-fdbb-4b96-a806-f8e09055e296.cpp: In function ‘int main()’: c6c2fbda-fdbb-4b96-a806-f8e09055e296.cpp:25:20: error: ‘odd_count’ was not decla red in this scope    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |                    ^~~~~~~~~ c6c2fbda-fdbb-4b96-a806-f8e09055e296.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(odd_count({"271", "137", "314"}) , {       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    26 |         "the number of odd elements 2n the str2ng 2 of the 2nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    27 |         "the number of odd elements 3n the str3ng 3 of the 3nput.",       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    28 |         "the number of odd elements 2n the str2ng 2 of the 2nput."       |         ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    29 |     }));       |     ~~

--- CPP_113.cpp
+++ CPP_113.cpp
@@ -1,14 +1,30 @@
-vector<string> odd_count(vector<string> lst) {
+#include <vector>
+#include <string>
+
+int odd_count(string s) {
+    int oddCount = 0;
+    for (char c : s) {
+        if ((c - '0') % 2 != 0) {
+            oddCount++;
+        }
+    }
+    return oddCount;
+}
+
+vector<string> generate_output(vector<string> lst) {
     vector<string> result;
     for (string str : lst) {
-        int oddCount = 0;
-        for (char c : str) {
-            if ((c - '0') % 2 != 0) {
-                oddCount++;
-            }
-        }
-        string newStr = "the number of odd elements " + to_string(oddCount) + "\n the str" + to_string(oddCount) + "ng " + str + " of the " + to_string(oddCount) + "\nput.";
+        int oddCount = odd_count(str);
+        string newStr = "the number of odd elements " + to_string(oddCount) + " in the string " + str + " of the input.";
         result.push_back(newStr);
     }
     return result;
+}
+
+int main() {
+    assert (issame(generate_output({"271", "137", "314"}), {
+        "the number of odd elements 1 in the string 271 of the input.",
+        "the number of odd elements 2 in the string 137 of the input.",
+        "the number of odd elements 2 in the string 314 of the input."
+    }));
 }
