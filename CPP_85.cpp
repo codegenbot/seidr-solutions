28cd4685773d2c9c13111eaa4955486cb1fbe0f5
beep boop <githubbot@vadim.me>
2024-08-03 10:16:23 +0200

    Solution 17 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/e7luemor Prompt: ad29782a-be55-4ea2-843e-facce11a77fc.cpp:1:9: error: ‘vector’ was not declared i n this scope     1 | int add(vector<int> lst){       |         ^~~~~~ ad29782a-be55-4ea2-843e-facce11a77fc.cpp:1:16: error: expected primary-expressio n before ‘int’     1 | int add(vector<int> lst){       |                ^~~ In file included from ad29782a-be55-4ea2-843e-facce11a77fc.cpp:10: ad29782a-be55-4ea2-843e-facce11a77fc.cpp: In function ‘int main()’: ad29782a-be55-4ea2-843e-facce11a77fc.cpp:12:16: error: ‘add’ cannot be used as a  function    12 |     assert (add({4, 4, 6, 8}) == 12);       |             ~~~^~~~~~~~~~~~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -12,7 +12,16 @@ int add(vector<int> lst){
 }
 
 int main() {
-    vector<int> lst = {4, 4, 6, 8};
-    assert(add(lst) == 12);
+    vector<int> input;
+    cout << "Enter numbers (space-separated): ";
+    string temp;
+    getline(cin, temp);
+    size_t start = 0;
+    while ((start = temp.find(" ")) != string::npos) {
+        input.push_back(stoi(temp.substr(0, start)));
+        temp.erase(0, start + 1);
+    }
+    input.push_back(stoi(temp));
+    cout << "Sum of even numbers: " << add(input) << endl;
     return 0;
 }
