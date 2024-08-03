45e10b723a0cb51908e5d713a564c86b68eb7b4c
beep boop <githubbot@vadim.me>
2024-08-03 23:40:02 +0200

    Solution 6 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/eqmm5ayx Prompt: f46a6a76-199d-420b-8f35-3297b905c53b.cpp:12:9: error: expected constructor, dest ructor, or type conversion before ‘(’ token    12 |     sort(word.begin(), word.end());       |         ^ f46a6a76-199d-420b-8f35-3297b905c53b.cpp:13:5: error: ‘result’ does not name a t ype    13 |     result += word;       |     ^~~~~~ f46a6a76-199d-420b-8f35-3297b905c53b.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return result;       |     ^~~~~~ f46a6a76-199d-420b-8f35-3297b905c53b.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from f46a6a76-199d-420b-8f35-3297b905c53b.cpp:17: f46a6a76-199d-420b-8f35-3297b905c53b.cpp: In function ‘int main()’: f46a6a76-199d-420b-8f35-3297b905c53b.cpp:19:13: error: ‘anti_shuffle’ was not de clared in this scope    19 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,22 +1,16 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
 string anti_shuffle(string s) {
     string result = "";
-    int start = 0;
-    for (int i = 0; i < s.length(); ++i) {
-        if (i == s.length() || s[i] == ' ') {
-            string word = s.substr(start, i - start);
-            sort(word.begin(), word.end());
-            result += word + (i == s.length() ? "" : " ");
-            start = i + 1;
+    string word = "";
+    for (char c : s) {
+        if (c == ' ') {
+            sort(word.begin(), word.end(), greater<char>());
+            result += word + ' ';
+            word = "";
+        } else {
+            word += c;
         }
     }
+    sort(word.begin(), word.end(), greater<char>());
+    result += word;
     return result;
-}
-
-int main() {
-    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
-    return 0;
 }
