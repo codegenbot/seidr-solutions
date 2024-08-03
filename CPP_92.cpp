550fef6e8b108168042eab9018a73bc7378bf84c
beep boop <githubbot@vadim.me>
2024-08-03 04:37:54 +0200

    Solution 10 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/sun5swu8 Prompt: ffa6e1f1-dee4-4b8b-99f9-7b77f2086e4d.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ ffa6e1f1-dee4-4b8b-99f9-7b77f2086e4d.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,3 +1,16 @@
-bool isValidTriangle(double a, double b, double c) {
-    return (int)a == b + c || (int)b == a + c || (int)c == a + b;
+#include<iostream>
+#include<assert.h>
+
+bool any_int(double a, double b, double c) {
+    if((a == b + c) || (b == a + c) || (c == a + b)){
+        if(a == (int)a && b == (int)b && c == (int)c){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    assert(any_int(3.01, 4, 7) == false);
+    return 0;
 }
