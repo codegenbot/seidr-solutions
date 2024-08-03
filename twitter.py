166cc91f395052af9cea426db7ae202de18b82c8
beep boop <githubbot@vadim.me>
2024-08-03 11:12:18 +0200

    Solution 0 of twitter, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/lr9ypbyn Prompt: Given a string representing a tweet, validate whether the tweet meets Twitter’s original character requirements. If the tweet has more than 140 characters, return the string "Too many characters". If the tweet is empty, return the string "You didn't type anything". Otherwise, return "Your tweet has X characters", where the X is the number of characters in the tweet.

---
+++ twitter.py
@@ -0,0 +1,10 @@
+def validate_tweet(tweet):
+    if len(tweet) > 140:
+        return "Too many characters"
+    elif tweet == "":
+        return "You didn't type anything"
+    else:
+        return f"Your tweet has {len(tweet)} characters"
+
+
+print(validate_tweet(input()))
