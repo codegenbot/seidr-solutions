62f85781bfaf774f8a484dec86f84ff4f2a07e2a
beep boop <githubbot@vadim.me>
2024-08-03 22:02:07 +0200

    Solution 9 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/jpa1xtd6 Prompt:    14 |             guessFreq[guess[i] - 'A']++;       |             ^~~~~~~~~ 18d3132e-7967-4d6b-b44f-5d1cbb52e441.cpp:19:26: error: ‘codeFreq’ was not declar ed in this scope    19 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                          ^~~~~~~~ 18d3132e-7967-4d6b-b44f-5d1cbb52e441.cpp:19:39: error: ‘guessFreq’ was not decla red in this scope    19 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                                       ^~~~~~~~~ 18d3132e-7967-4d6b-b44f-5d1cbb52e441.cpp:19:22: error: ‘min’ was not declared in  this scope; did you mean ‘main’?    19 |         whitePegs += min(codeFreq[i], guessFreq[i]);       |                      ^~~       |                      main 18d3132e-7967-4d6b-b44f-5d1cbb52e441.cpp:22:5: error: ‘cout’ was not declared in  this scope    22 |     cout << whitePegs << endl;       |     ^~~~ 18d3132e-7967-4d6b-b44f-5d1cbb52e441.cpp:22:26: error: ‘endl’ was not declared i n this scope    22 |     cout << whitePegs << endl;       |                          ^~~~

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int main() {
     string code, guess;
     cin >> code >> guess;
-    
+
+    if (code.length() != 4 || guess.length() != 4) {
+        cout << "Invalid input" << endl;
+        return 0;
+    }
+
     int blackPegs = 0, whitePegs = 0;
-    vector<int> codeFreq(6, 0), guessFreq(6, 0);
-    
+    vector<int> codeFreq(6, 0);
+    vector<int> guessFreq(6, 0);
+
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             blackPegs++;
         } else {
-            codeFreq[code[i] - 'A']++;
-            guessFreq[guess[i] - 'A']++;
+            codeFreq[code[i] - '1']++;
+            guessFreq[guess[i] - '1']++;
         }
     }
-    
+
     for (int i = 0; i < 6; ++i) {
         whitePegs += min(codeFreq[i], guessFreq[i]);
     }
-    
-    cout << whitePegs << endl << blackPegs << endl;
-    
+
+    cout << whitePegs << endl;
+    cout << blackPegs << endl;
+
     return 0;
 }
