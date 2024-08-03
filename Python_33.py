b1002d29e13f7d14c0e1651fb8feabcf2edc4c71
beep boop <githubbot@vadim.me>
2024-08-03 12:48:41 +0200

    Solution 39 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9ujle3bf Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be7f3143-46b5-41e0-ab13-8466e09df 1d1.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be7f3143-46b5-41e0-ab13-8466e09df 1d1.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_33.py
+++ Python_33.py
@@ -1,4 +1,5 @@
+```
 def sort_third(l: list):
     indexed_l = [(i, x) for i, x in enumerate(l)]
-    return [x if i % 3 != 0 else sorted([y for i, y in indexed_l if (i - indexed_l.index((j, y))) % 3 != 0])[0]
+    return [x if i % 3 != 0 else sorted([y for j, y in indexed_l if (j - indexed_l.index((k, y))) % 3 != 0])[0]
            for i, x in indexed_l]
