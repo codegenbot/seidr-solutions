d7b5a01563dd394491063339a2104cecf9fdf7f8
beep boop <githubbot@vadim.me>
2024-08-03 13:31:40 +0200

    Solution 40 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/cl3go448 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29c47999-b3a8-4474-b65d-f60c1fbfc 868.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/29c47999-b3a8-4474-b65d-f60c1fbfc 868.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,14 +1,6 @@
 ```
 def modp(n: int, p: int):
-    def extended_euclidean(a: int, b: int):
-        if a == 0:
-            return b, 0
-        else:
-            gcd, s = extended_euclidean(b % a, a)
-            return gcd, s - (b // a) * s
-
-    g, x = extended_euclidean(n, p)
-    if g != 1:
-        return None
+    if p > 1:
+        return pow(n * n % p, (p - 1) // 2, p) * n % p
     else:
-        return pow(n, p - 2, p) * n % p if p > 1 else 0
+        return 0
