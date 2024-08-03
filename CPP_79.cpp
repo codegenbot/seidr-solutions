58eaa02f81fe1cab1523a998e524dc9a3204a6fb
beep boop <githubbot@vadim.me>
2024-08-03 10:02:30 +0200

    Solution 18 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/vxes3v09 Prompt: 7f59124d-cd1c-41ca-95ed-01fba3c2630d.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~ In file included from 7f59124d-cd1c-41ca-95ed-01fba3c2630d.cpp:15: 7f59124d-cd1c-41ca-95ed-01fba3c2630d.cpp: In function ‘int main()’: 7f59124d-cd1c-41ca-95ed-01fba3c2630d.cpp:17:13: error: ‘decimal_to_binary’ was n ot declared in this scope    17 |     assert (decimal_to_binary(15) == "db1111db");       |             ^~~~~~~~~~~~~~~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -1,12 +1,12 @@
-string decimal_to_binary(int decimal);
-string decimal_to_binary(int decimal){
-    string binary = "";
-    while(decimal > 0){
+string decimal_to_binary(int decimal); 
+string decimal_to_binary(int decimal){ 
+    string binary = ""; 
+    while(decimal > 0){ 
         if((decimal & 1) == 1)
             binary = "1" + binary;
         else
             binary = "0" + binary;
         decimal >>= 1;
     }
-    return binary;
+    return binary; 
 }
