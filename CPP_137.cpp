235acd5e5586e443e37c4689c890dadd471f3660
beep boop <githubbot@vadim.me>
2024-08-03 04:14:03 +0200

    Solution 35 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q2hs3fhq Prompt: bd573d4e-a92e-44b3-974a-dfd1dcbbc7f5.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,8 +1,6 @@
 #include <boost/any.hpp>
 #include <string>
 #include <iostream>
-#include <boost/lexical_cast.hpp>
-
 using namespace boost;
 
 boost::any compare_one(boost::any a, boost::any b) {
@@ -15,7 +13,7 @@ boost::any compare_one(boost::any a, boost::any b) {
     else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
         std::string str1 = boost::any_cast<std::string>(a);
         std::string str2 = boost::any_cast<std::string>(b);
-        
+
         if (str1 > str2) {
             return a;
         }
@@ -23,31 +21,24 @@ boost::any compare_one(boost::any a, boost::any b) {
             return b;
         }
         else {
-            return any("None");
+            return boost::any("None");
         }
     }
     else if ((a.type() == typeid(int) && b.type() == typeid(std::string)) || 
              (a.type() == typeid(std::string) && b.type() == typeid(int))) {
         std::string str = boost::any_cast<std::string>(b);
-        
-        if (boost::any_cast<int>(a) > boost::lexical_cast<int>(str)) {
+
+        if (boost::any_cast<int>(a) > std::stoi(str)) {
             return a;
         }
-        else if (boost::any_cast<int>(a) < boost::lexical_cast<int>(str)) {
+        else if (boost::any_cast<int>(a) < std::stoi(str)) {
             return b;
         }
         else {
-            return any("None");
+            return boost::any("None");
         }
     }
     else {
-        return any("None");
+        return boost::any("None");
     }
-}
-
-int main() {
-    int a = 1;
-    double b = 2.0;
-    std::cout << "Result: " << compare_one(a,b) << endl;
-    return 0;
 }
