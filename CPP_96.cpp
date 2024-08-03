99ba17f128afd4d2364b159ffc37d14ba9222776
beep boop <githubbot@vadim.me>
2024-08-04 00:24:24 +0200

    Solution 62 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt: 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:22:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    22 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main(){       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -2,40 +2,31 @@
 #include <cassert>
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-vector<int> count_up_to(int n) {
+vector<int> count_up_to(int n){
     vector<int> primes;
-    if (n >= 2) {
-        primes.push_back(2);
-        for (int i = 3; i <= n; i += 2) {
-            bool is_prime = true;
-            for (int j = 2; j * j <= i; j++) {
-                if (i % j == 0) {
-                    is_prime = false;
-                    break;
-                }
+    for (int num = 2; num <= n; num++) {
+        bool is_prime = true;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                is_prime = false;
+                break;
             }
-            if (is_prime) {
-                primes.push_back(i);
-            }
+        }
+        if (is_prime) {
+            primes.push_back(num);
         }
     }
     return primes;
 }
 
-int main() {
-    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
-    
-    return 0;
+bool issame(const vector<int> &a, const vector<int> &b){
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
