bbbd30945dd354bf20b07892a385a75a5cc238ac
beep boop <githubbot@vadim.me>
2024-08-03 06:48:46 +0200

    Solution 7 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7l5w7q5x Prompt:       |                    ^~~ 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp:20: 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp: In function ‘int main()’: 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp:30:20: error: ‘get_odd_collatz’ was not  declared in this scope    30 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 7ca3c894-86c0-43f2-93d0-a7c1a6c66c0b.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,22 +1,38 @@
 #include <vector>
 
 bool issame(vector<int> a, vector<int> b) {
-    vector<int> result;
-    while (n != 1) {
-        result.push_back(n);
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
-        }
+  for (int i = 0; i < a.size(); i++) {
+    if (a[i] != b[i]) {
+      return false;
     }
-    result.push_back(1);
-    sort(result.begin(), result.end());
-    vector<int> odd_result;
-    for (int num : result) {
-        if (num % 2 == 1) {
-            odd_result.push_back(num);
-        }
+  }
+  return true;
+}
+
+vector<int> get_odd_collatz(int n) {
+  vector<int> result;
+  while (n != 1) {
+    result.push_back(n);
+    if (n % 2 == 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
     }
-    return odd_result;
+  }
+  result.push_back(1);
+  sort(result.begin(), result.end());
+  vector<int> odd_result;
+  for (int num : result) {
+    if (num % 2 == 1) {
+      odd_result.push_back(num);
+    }
+  }
+  return odd_result;
+}
+
+int main() {
+  // Example usage
+  int number = 27;
+  vector<int> collatz_odd = get_odd_collatz(number);
+  return 0;
 }
