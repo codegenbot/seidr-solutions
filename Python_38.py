227ff0aa20a9b16527e607f51fe58e18ef1ddbc1
beep boop <githubbot@vadim.me>
2024-08-04 00:10:08 +0200

    Solution 64 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0jv3u3nm Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -1,6 +1,3 @@
-s = input()
-
-
 def decode_cyclic(s: str):
     groups = [s[(3 * i) : min((3 * i + 3), len(s))] for i in range((len(s) + 2) // 3)]
     groups = [
@@ -8,6 +5,5 @@ def decode_cyclic(s: str):
     ]
     return "".join(groups)
 
-
-result = decode_cyclic(s)
+result = decode_cyclic(input())
 print(result)
