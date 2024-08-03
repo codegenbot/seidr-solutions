6fcde5f08cf479702af8959694861d6c98a5951c
beep boop <githubbot@vadim.me>
2024-08-03 09:11:55 +0200

    Solution 8 of CPP/130, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/xgyer33f Prompt:       |                    ^~~ cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp:31:27: error: ‘vector’ was not declared  in this scope    31 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp:31:39: error: expression list treated a s compound expression in initializer [-fpermissive]    31 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp:30: cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp: In function ‘int main()’: cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp:40:20: error: ‘tri’ was not declared in  this scope; did you mean ‘tri_n’?    40 |     assert (issame(tri(1) , {1, 3}));       |                    ^~~ cd7a6e31-4769-4bb2-bdfc-237c047b86b7.cpp:40:19: error: ‘issame’ cannot be used a s a function    40 |     assert (issame(tri(1) , {1, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~

--- CPP_130.cpp
+++ CPP_130.cpp
@@ -1,10 +1,48 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    return a == b;
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-int main() {
-    assert (issame(tri_n(1) , {1, 3}));
-    return 0;
+vector<int> tri(int n) {
+    vector<int> result;
+    if (n == 0) {
+        return result;
+    }
+    result.push_back(3);
+    if (n == 1) {
+        return result;
+    }
+    result.push_back(1);
+    if (n == 2) {
+        return result;
+    }
+    result.push_back(2);
+    int tri_n_minus_2 = 1;
+    int tri_n_minus_1 = 3;
+    int tri_n = 2;
+    for (int i = 3; i <= n; ++i) {
+        if (i % 2 == 0) {
+            tri_n = 1 + i / 2;
+        } else {
+            tri_n = tri_n_minus_1 + tri_n_minus_2 + tri_n + 1;
+        }
+        result.push_back(tri_n);
+        tri_n_minus_2 = tri_n_minus_1;
+        tri_n_minus_1 = tri_n;
+    }
+    return result;
 }
+
+assert(issame(tri(0), {}));
+assert(issame(tri(1), {3}));
+assert(issame(tri(2), {3, 1}));
+assert(issame(tri(4), {3, 1, 2, 4, 7}));
