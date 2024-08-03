3e41eb5fad34d1812b292c45ac2d25108063c350
beep boop <githubbot@vadim.me>
2024-08-03 20:52:42 +0200

    Solution 77 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/psk8bxyl Prompt:     4 | 88d7a4ef-2373-47ad-914c-a9a619f7d713.cpp:30:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    30 | int main(){       |         ^~ 88d7a4ef-2373-47ad-914c-a9a619f7d713.cpp:30:9: note: remove parentheses to defau lt-initialize a variable    30 | int main(){       |         ^~       |         -- 88d7a4ef-2373-47ad-914c-a9a619f7d713.cpp:30:9: note: or replace parentheses with  braces to value-initialize a variable 88d7a4ef-2373-47ad-914c-a9a619f7d713.cpp:30:11: error: a function-definition is not allowed here before ‘{’ token    30 | int main(){       |           ^ 88d7a4ef-2373-47ad-914c-a9a619f7d713.cpp:32:2: error: expected ‘}’ at end of inp ut    32 | }       |  ^ 88d7a4ef-2373-47ad-914c-a9a619f7d713.cpp:15:12: note: to match this ‘{’    15 | int main() {       |            ^

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -14,6 +14,6 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 
 int main {
     assert(issame(std::vector<int>({5, 3, 5, 2, 3, 3, 9, 0, 123}), std::vector<int>({0, 2, 3, 5, 9, 123}));
-    
+
     return 0;
 }
