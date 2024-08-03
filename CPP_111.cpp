5a09c4b518138527304a796c023e5e352b8a7ca6
beep boop <githubbot@vadim.me>
2024-08-03 06:13:59 +0200

    Solution 44 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/l81g71pi Prompt: a70d1099-3db4-425d-8b0c-a0f2a35e24b2.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ a70d1099-3db4-425d-8b0c-a0f2a35e24b2.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from a70d1099-3db4-425d-8b0c-a0f2a35e24b2.cpp:15: a70d1099-3db4-425d-8b0c-a0f2a35e24b2.cpp: In function ‘int main()’: a70d1099-3db4-425d-8b0c-a0f2a35e24b2.cpp:30:20: error: ‘histogram’ was not decla red in this scope    30 |     assert (issame(histogram("a") , {{'a', 1}}));       |                    ^~~~~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-map<char, int> histogram(const string& str) {
-    map<char, int> result;
-    for (char c : str) {
-        result[c]++;
+map<char, int> histogram(const string& s) {
+    map<char, int> freq;
+    for (char c : s) {
+        freq[c]++;
     }
-    return result;
+    return freq;
 }
 
 bool issame(const map<char, int>& a, const map<char, int>& b) {
@@ -18,5 +18,6 @@ bool issame(const map<char, int>& a, const map<char, int>& b) {
 int main() {
     assert(issame(map<char, int>{{'a', 1}}, {{'a', 1}}));
     assert(issame(histogram("a"), {{'a', 1}}));
+
     return 0;
 }
