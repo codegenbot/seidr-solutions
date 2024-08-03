c2a656b85a9b25f7ef2efd65def9ab30b87bb16d
beep boop <githubbot@vadim.me>
2024-08-04 00:10:40 +0200

    Solution 98 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/zmpnpp42 Prompt:                  from b024db9d-d33d-4de7-90d3-12b58dbfd3d7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from /usr/include/c++/11/functional:62,                  from /usr/include/c++/11/pstl/glue_algorithm_defs.h:13,                  from /usr/include/c++/11/algorithm:74,                  from b024db9d-d33d-4de7-90d3-12b58dbfd3d7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b024db9d-d33d-4de7-90d3-12b58dbfd3d7.cpp:34:49: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ b024db9d-d33d-4de7-90d3-12b58dbfd3d7.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ b024db9d-d33d-4de7-90d3-12b58dbfd3d7.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -7,7 +7,7 @@ bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vecto
     return a == b;
 }
 
-std::vector<std::vector<int>> get_row(const std::vector<std::vector<int>>& lst, int x) {
+std::vector<std::vector<int>> get_row_cpp87(const std::vector<std::vector<int>>& lst, int x) {
     std::vector<std::vector<int>> result;
     for (size_t i = 0; i < lst.size(); ++i) {
         for (size_t j = 0; j < lst[i].size(); ++j) {
@@ -26,6 +26,6 @@ std::vector<std::vector<int>> get_row(const std::vector<std::vector<int>>& lst, 
 }
 
 int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), std::vector<std::vector<int>>{{2, 2}}));
+    assert(issame(get_row_cpp87({{}, {1}, {1, 2, 3}}, 3), std::vector<std::vector<int>>{{2, 2}}));
     return 0;
 }
