8d14a3d19ad1499c56f809b1b8ed7b616902d56b
beep boop <githubbot@vadim.me>
2024-08-03 22:17:01 +0200

    Solution 14 of CPP/57, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/499m4xzp Prompt:     7 |     for (size_t i = 1; i < l.size(); ++i) {       |          ^~~~~~       |          std::size_t In file included from /usr/include/c++/11/bits/stl_algobase.h:59,                  from /usr/include/c++/11/vector:60,                  from 187e2cf0-4be4-415c-965a-fcf1f0bf591e.cpp:1: /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h:280:33: note: ‘std::size_t ’ declared here   280 |   typedef __SIZE_TYPE__         size_t;       |                                 ^~~~~~ 187e2cf0-4be4-415c-965a-fcf1f0bf591e.cpp:7:24: error: ‘i’ was not declared in th is scope     7 |     for (size_t i = 1; i < l.size(); ++i) {       |                        ^ 187e2cf0-4be4-415c-965a-fcf1f0bf591e.cpp: At global scope: 187e2cf0-4be4-415c-965a-fcf1f0bf591e.cpp:23:5: error: redefinition of ‘int main( )’    23 | int main(){       |     ^~~~ 187e2cf0-4be4-415c-965a-fcf1f0bf591e.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_57.cpp
+++ CPP_57.cpp
@@ -4,8 +4,7 @@
 bool monotonic(const std::vector<int>& l) {
     bool increasing = true;
     bool decreasing = true;
-    std::size_t i;
-    for (i = 1; i < l.size(); ++i) {
+    for (int i = 1; i < l.size(); ++i) {
         if (l[i] > l[i - 1]) {
             decreasing = false;
         } else if (l[i] < l[i - 1]) {
