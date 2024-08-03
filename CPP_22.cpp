43a5bd096baea3eb6734bc52769cf56506f8985e
beep boop <githubbot@vadim.me>
2024-08-03 19:21:57 +0200

    Solution 18 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/kqfovzzb Prompt:       |     ^~~~~~ bc11a141-cbd0-46e3-9c47-130024afde15.cpp:4:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     3 | #include <any>   +++ |+#include <cassert>     4 | bc11a141-cbd0-46e3-9c47-130024afde15.cpp: At global scope: bc11a141-cbd0-46e3-9c47-130024afde15.cpp:27:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    27 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ bc11a141-cbd0-46e3-9c47-130024afde15.cpp:17:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    17 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ bc11a141-cbd0-46e3-9c47-130024afde15.cpp:35:5: error: redefinition of ‘int main( )’    35 | int main(){       |     ^~~~ bc11a141-cbd0-46e3-9c47-130024afde15.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main(){       |     ^~~~

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,24 +1,21 @@
 #include <vector>
 #include <list>
 #include <any>
+#include <cassert>
 
 using namespace std;
 
-vector<int> filter_integers(list<any> values){
+vector<int> filter_integers(list<any> values) {
     vector<int> result;
-    for(auto val : values) {
-        if(val.type() == typeid(int)) {
+    for (auto val : values) {
+        if (val.type() == typeid(int)) {
             result.push_back(any_cast<int>(val));
         }
     }
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
-}
-
-int main(){
+int main() {
     assert(issame(filter_integers({3, 'c', 3, 3, 'a', 'b'}), {3, 3, 3}));
     return 0;
 }
