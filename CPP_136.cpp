bc15fd0c54305edb9c612e08f8b57f3c552640f0
beep boop <githubbot@vadim.me>
2024-08-03 11:51:20 +0200

    Solution 86 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4rqn9jbw Prompt: In file included from b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~ In file included from b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp:24: b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp: In function ‘int main()’: b6ec448e-d18e-4216-8ad3-c3b9e4f125ea.cpp:34:20: error: ‘largest_smallest_integer s’ was not declared in this scope    34 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -2,34 +2,21 @@
 #include <vector>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size())
+    if(a.size() != b.size()) {
         return false;
+    }
     
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i])
+    for(int i = 0; i < a.size(); ++i) {
+        if(a[i] != b[i]) {
             return false;
+        }
     }
     
     return true;
 }
 
-std::vector<int> largest_smallest_integers(std::vector<int> arr) {
-    int largest = arr[0];
-    int smallest = arr[0];
-
-    for(int num : arr) {
-        if(num > largest)
-            largest = num;
-        if(num < smallest)
-            smallest = num;
-    }
-
-    return {largest, smallest};
-}
-
 int main() {
     assert(issame({-6, -4, -4, -3, -100, 1}, {-3, 1}));
-    assert(issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {1, -100}));
-
+    
     return 0;
 }
