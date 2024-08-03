9ae69e47a0ac32fdb4793d00eb408dd21bf07d3b
beep boop <githubbot@vadim.me>
2024-08-03 20:51:29 +0200

    Solution 25 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:       |             ^~~~~~ 71bb6093-317f-40da-b0da-c71b9b397e64.cpp:29:20: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 71bb6093-317f-40da-b0da-c71b9b397e64.cpp:29:27: error: ‘vector’ was not declared  in this scope    29 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 71bb6093-317f-40da-b0da-c71b9b397e64.cpp:29:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 71bb6093-317f-40da-b0da-c71b9b397e64.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 71bb6093-317f-40da-b0da-c71b9b397e64.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 71bb6093-317f-40da-b0da-c71b9b397e64.cpp:29:34: error: expected primary-expressi on before ‘int’    29 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,5 +1,26 @@
-using namespace std;
+#include <vector>
+#include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return std::equal(a.begin(), a.end(), b.begin(), b.end());
+}
+
+std::vector<int> sort_third(std::vector<int> l);
+
+std::vector<int> sort_third(std::vector<int> l) {
+    std::vector<int> res = l;
+    std::vector<int> sorted_third;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            sorted_third.push_back(l[i]);
+        }
+    }
+    std::sort(sorted_third.begin(), sorted_third.end());
+    int idx = 0;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            res[i] = sorted_third[idx++];
+        }
+    }
+    return res;
 }
