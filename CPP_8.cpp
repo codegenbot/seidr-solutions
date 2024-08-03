c6cad164321f81095141c555ec5d19e1265190b5
beep boop <githubbot@vadim.me>
2024-08-03 09:04:09 +0200

    Solution 92 of CPP/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/5lzs9pe3 Prompt: 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:20: 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp: In function ‘int main()’: 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:30:20: error: ‘sum_product’ was not dec lared in this scope; did you mean ‘std::sum_product’?    30 |     assert (issame(sum_product({10}) ,{10, 10}));       |                    ^~~~~~~~~~~ 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:9:13: note: ‘std::sum_product’ declared  here     9 | vector<int> sum_product(const vector<int>& v) {       |             ^~~~~~~~~~~ In file included from 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:20: 5846a6bf-b6fd-4a95-849f-a19eaf2ec5cb.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(sum_product({10}) ,{10, 10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_8.cpp
+++ CPP_8.cpp
@@ -1,12 +1,12 @@
 #include <vector>
 
-namespace std {
-
-bool issame(const vector<int>& a, const vector<int>& b) {
+bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
 
-vector<int> sum_product(const vector<int>& v) {
+std::vector<int> sum_product(const std::vector<int>& v);
+
+std::vector<int> sum_product(const std::vector<int>& v) {
     int sum = 0, product = 1;
     for (auto num : v) {
         sum += num;
@@ -14,7 +14,3 @@ vector<int> sum_product(const vector<int>& v) {
     }
     return {sum, product};
 }
-
-}
-
-using namespace std;
