5b3832848bb16608b710ba5a5518c0a515fcd20d
beep boop <githubbot@vadim.me>
2024-08-03 15:25:16 +0200

    Solution 17 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niyd2aok Prompt:  in this scope    27 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 218a147b-bf7e-4512-adb3-49329186b999.cpp:27:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 218a147b-bf7e-4512-adb3-49329186b999.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 218a147b-bf7e-4512-adb3-49329186b999.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 218a147b-bf7e-4512-adb3-49329186b999.cpp:27:34: error: expected primary-expressi on before ‘int’    27 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 218a147b-bf7e-4512-adb3-49329186b999.cpp:26: 218a147b-bf7e-4512-adb3-49329186b999.cpp: In function ‘int main()’: 218a147b-bf7e-4512-adb3-49329186b999.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -29,7 +29,7 @@ std::vector<int> unique_digits(std::vector<int> x) {
 }
 
 int main() {
-    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
+    assert (issame(unique_digits({135, 103, 31}), {31, 135}));
 
     return 0;
 }
