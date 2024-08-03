22da659d62945a23db7fddbe4fa4ae0322e86b1d
beep boop <githubbot@vadim.me>
2024-08-03 19:56:03 +0200

    Solution 58 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/3s9lqf4z Prompt: 96e812bb-c148-4fce-b640-3738b532a758.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -9,37 +9,37 @@ int bowlingScore(string s) {
         if (c == 'X') {
             score += 10;
             strike = true;
-        }
-        else if (c == '/') {
             continue;
         }
-        else if (isdigit(c)) {
+        if (isdigit(c)) {
             int pins = c - '0';
             if (!strike) {
-                score += pins;
-                if (currentFrame < 10 && s[currentFrame*2-1] == '/') {
+                if (currentFrame < 10 && s[currentFrame*2-1] != '/') {
+                    score += pins;
+                    currentFrame++;
+                } else if (s[currentFrame*2-1] == '/') {
+                    score += 10 - pins;
                     currentFrame++;
                 }
-            } 
-            else {
+            } else {
                 strike = false;
                 score += 10 + pins;
             }
         }
     }
 
-    for (int i = currentFrame; i <= 10; i++) {
-        if (s[i*2-1] != '/') {
-            int pins = s.substr(i*2-1, 1) - '0';
-            if (i == 10 && s[i*2] == 'X') {
-                score += 10 + 10;
-            }
-            else if (i == 10) {
-                score += 10 + pins;
-            } 
-            else {
+    for (; currentFrame < 10; currentFrame++) {
+        if (!strike) {
+            char c = s[currentFrame*2-1];
+            if (c == 'X') {
+                score += 10;
+            } else if (isdigit(c)) {
+                int pins = c - '0';
                 score += pins;
             }
+        } else {
+            strike = false;
+            score += 10;
         }
     }
 
