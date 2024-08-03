a44571e03c03d6855308e2404667c5e592b2142a
beep boop <githubbot@vadim.me>
2024-08-03 09:51:02 +0200

    Solution 88 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mqhr2bp2 Prompt:                  from /usr/include/c++/11/algorithm:74,                  from 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp:19: 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp: In function ‘int main()’: 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp:29:20: error: ‘strange_sort_list’ was n ot declared in this scope    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 808cdab7-31c2-4101-bc66-f28f75e60db4.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -13,13 +13,20 @@ vector<std::vector<int>> strange_sort_vector(vector<int> lst) {
     vector<std::vector<int>> result;
     while (!lst.empty()) {
         int minVal = *min_element(lst.begin(), lst.end());
-        result.push_back({minVal});
-        lst.erase(remove(lst.begin(), lst.end(), minVal), lst.end());
-
-        if (!lst.empty()) {
-            int maxVal = *max_element(lst.begin(), lst.end());
-            result.back().push_back(maxVal);
-            lst.erase(remove(lst.begin(), lst.end(), maxVal), lst.end());
+        vector<int> temp;
+        for (int i : lst) {
+            if (i != minVal) temp.push_back(i);
+        }
+        if (!temp.empty()) {
+            int maxVal = *max_element(temp.begin(), temp.end());
+            result.push_back({minVal, maxVal});
+            lst.erase(remove(lst.begin(), lst.end(), minVal), lst.end());
+            for (int i : temp) {
+                if (i != maxVal) lst.push_back(i);
+            }
+        } else {
+            result.push_back({minVal});
+            lst.erase(remove(lst.begin(), lst.end(), minVal), lst.end());
         }
     }
     return result;
