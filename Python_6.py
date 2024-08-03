09ebcba9d914951ba535a83158cac7dcfa21be3d
beep boop <githubbot@vadim.me>
2024-08-03 09:02:14 +0200

    Solution 70 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/qkveeyra Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/136b6c49-7496-407d-9364-53c0ecd20 8db.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_6.py
+++ Python_6.py
@@ -1,18 +1,15 @@
-if __name__ == "__main__":
-    from typing import List
+from typing import List
 
-    def parse_nested_parens(paren_string: str) -> List[int]:
-        result = []
-        for group in paren_string.split():
-            level = 0
-            max_level = 0
-            for char in group:
-                if char == "(":
-                    level += 1
-                elif char == ")":
-                    level -= 1
-                max_level = max(max_level, level)
-            result.append(max_level)
-        return result
-
-    print(parse_nested_parens("((()))"))
+def parse_nested_parens(paren_string: str) -> List[int]:
+    result = []
+    for group in paren_string.split():
+        level = 0
+        max_level = 0
+        for char in group:
+            if char == '(':
+                level += 1
+            elif char == ')':
+                level -= 1
+            max_level = max(max_level, level)
+        result.append(max_level)
+    return result
