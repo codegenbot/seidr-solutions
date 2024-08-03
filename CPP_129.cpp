f8c5d5f9ff8ef3d9580fbab51a6bd6d1df139d61
beep boop <githubbot@vadim.me>
2024-08-03 08:44:18 +0200

    Solution 18 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2k819xey Prompt:       |                           ^~~~~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:51:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:51:34: error: expected primary-expressi on before ‘int’    51 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:59:5: error: redefinition of ‘int main( )’    59 | int main(){       |     ^~~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:45:5: note: ‘int main()’ previously def ined here    45 | int main() {       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -1,44 +1,43 @@
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
-vector<int> minPath(vector<vector<int>> grid, int k);
-
-bool is_same(vector<int> a, vector<int> b) {
-    return a == b;
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return std::vector<int>(a) == std::vector<int>(b);
 }
 
-vector<int> minPath(vector<vector<int>> grid, int k) {
+std::vector<int> minPath(std::vector<std::vector<int>> grid, int k) {
     int n = grid.size();
     int m = grid[0].size();
 
-    vector<vector<int>> dp(n, vector<int>(m, k + 1));
+    std::vector<std::vector<int>> dp(n, std::vector<int>(m, k + 1));
     dp[0][0] = grid[0][0];
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             if (i > 0) {
-                dp[i][j] = min(dp[i][j], dp[i - 1][j] + grid[i][j]);
+                dp[i][j] = std::min(dp[i][j], dp[i - 1][j] + grid[i][j]);
             }
             if (j > 0) {
-                dp[i][j] = min(dp[i][j], dp[i][j - 1] + grid[i][j]);
+                dp[i][j] = std::min(dp[i][j], dp[i][j - 1] + grid[i][j]);
             }
         }
     }
 
-    vector<int> path;
-    int i = n - 1, j = m - 1;
-    path.push_back(grid[i][j]);
+    std::vector<int> path;
+    int x = n - 1, y = m - 1;
+    path.push_back(grid[x][y]);
 
-    while (i > 0 || j > 0) {
-        if (i > 0 && dp[i][j] == dp[i - 1][j] + grid[i][j]) {
-            i--;
+    while (x > 0 || y > 0) {
+        if (x > 0 && dp[x][y] - grid[x][y] == dp[x - 1][y]) {
+            x--;
         } else {
-            j--;
+            y--;
         }
-        path.push_back(grid[i][j]);
+        path.push_back(grid[x][y]);
     }
 
-    reverse(path.begin(), path.end());
+    std::reverse(path.begin(), path.end());
 
     return path;
 }
