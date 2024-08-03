9e4de17919ebc142c7a2ae78298f4236a44f6120
beep boop <githubbot@vadim.me>
2024-08-03 12:17:45 +0200

    Solution 22 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/v7bvffxm Prompt: 539ce605-15be-43d0-a73a-2bb70b0bbb0a.cpp:1:10: fatal error: boost/any.hpp: No su ch file or directory     1 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,5 +1,6 @@
 #include <boost/any.hpp>
-#include <boost/conversion.hpp>
+#include <boost/lexical_cast.hpp>
+#include <string>
 
 using namespace boost;
 
@@ -7,20 +8,20 @@ boost::any compare_one(boost::any a, boost::any b) {
     if (is_any_of<float>(a)) {
         float af = boost::any_cast<float>(a);
         float bf = boost::any_cast<float>(b);
-        return af > bf ? a : bf > af ? b : any("None");
+        return (af > bf) ? a : ((bf > af) ? b : any("None"));
     } else if (is_any_of<double>(a)) {
         double ad = boost::any_cast<double>(a);
         double bd = boost::any_cast<double>(b);
-        return ad > bd ? a : bd > ad ? b : any("None");
+        return (ad > bd) ? a : ((bd > ad) ? b : any("None"));
     } else if (is_any_of<int>(a)) {
         int ai = boost::any_cast<int>(a);
         int bi = boost::any_cast<int>(b);
-        return ai > bi ? a : bi > ai ? b : any("None");
+        return (ai > bi) ? a : ((bi > ai) ? b : any("None"));
     } else if (is_string(a)) {
-        string as = boost::any_cast<string>(a);
-        string bs = boost::any_cast<string>(b);
-        double af = stof(as), bf = stof(bs);
-        return af > bf ? a : bf > af ? b : any("None");
+        std::string as = boost::any_cast<std::string>(a);
+        std::string bs = boost::any_cast<std::string>(b);
+        float af = stof(as), bf = stof(bs);
+        return (af > bf) ? a : ((bf > af) ? b : any("None"));
     } else {
         return any("None");
     }
