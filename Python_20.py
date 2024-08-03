7cf1874890ec54e6e154291315a633bd32992372
beep boop <githubbot@vadim.me>
2024-08-03 10:10:30 +0200

    Solution 20 of Python/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/5ehrdxok Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/653c9975-3e2d-42df-b180-f786d2581 920.py", line 1, in <module>     def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:                                        ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_20.py
+++ Python_20.py
@@ -1,7 +1,7 @@
 ```
 from typing import List, Tuple
 
-def find_closest_elements(numbers: List[float]) -> Tuple[float, float]:
+def find_closest_elements(numbers: list[float]) -> Tuple[float, float]:
     numbers.sort()
     closest_pair = (numbers[0], numbers[1])
     min_diff = abs(closest_pair[1] - closest_pair[0])
