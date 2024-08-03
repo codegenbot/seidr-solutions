60ddcaf4579909fc0b50a4d8f3df98c44d8c6e9b
beep boop <githubbot@vadim.me>
2024-08-03 12:14:38 +0200

    Solution 0 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/v7bvffxm Prompt: Complete the following code given the task description and function signature.

---
+++ CPP_137.cpp
@@ -0,0 +1,46 @@
+#include <boost/any.hpp>
+#include <boost/lexical_cast.hpp>
+
+using namespace boost;
+
+boost::any compare_one(boost::any a, boost::any b) {
+    if (is_any_of<float>(a)) {
+        float af = boost::any_cast<float>(a);
+        float bf = boost::any_cast<float>(b);
+        if (af > bf)
+            return a;
+        else if (bf > af)
+            return b;
+        else
+            return any("None");
+    } else if (is_any_of<double>(a)) {
+        double ad = boost::any_cast<double>(a);
+        double bd = boost::any_cast<double>(b);
+        if (ad > bd)
+            return a;
+        else if (bd > ad)
+            return b;
+        else
+            return any("None");
+    } else if (is_any_of<int>(a)) {
+        int ai = boost::any_cast<int>(a);
+        int bi = boost::any_cast<int>(b);
+        if (ai > bi)
+            return a;
+        else if (bi > ai)
+            return b;
+        else
+            return any("None");
+    } else if (is_string(a)) {
+        string as = boost::any_cast<string>(a);
+        string bs = boost::any_cast<string>(b);
+        if (stof(as) > stof(bs))
+            return a;
+        else if (stof(bs) > stof(as))
+            return b;
+        else
+            return any("None");
+    } else {
+        return any("None");
+    }
+}
