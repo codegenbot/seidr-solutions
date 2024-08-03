e55e9bd08070a4f63109fe0bcd1da72635863da0
beep boop <githubbot@vadim.me>
2024-08-03 13:02:31 +0200

    Solution 13 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q5n63q1c Prompt:    35 | bool issame(vector<float> a,vector<float>b){       |                    ^~~~~ aefad365-1a72-48a8-a410-6bdfc697b2d8.cpp:35:29: error: ‘vector’ was not declared  in this scope    35 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ aefad365-1a72-48a8-a410-6bdfc697b2d8.cpp:35:36: error: expected primary-expressi on before ‘float’    35 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from aefad365-1a72-48a8-a410-6bdfc697b2d8.cpp:34: aefad365-1a72-48a8-a410-6bdfc697b2d8.cpp: In function ‘int main()’: aefad365-1a72-48a8-a410-6bdfc697b2d8.cpp:44:20: error: ‘sort_even’ was not decla red in this scope    44 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ aefad365-1a72-48a8-a410-6bdfc697b2d8.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -6,35 +6,6 @@ bool issame(vector<float> a, vector<float> b);
 
 void sort_even(vector<float>& l);
 
-bool issame(vector<float> a, vector<float> b) {
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void sort_even(vector<float>& l) {
-    vector<float> even_elements;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            even_elements.push_back(l[i]);
-        }
-    }
-    sort(even_elements.begin(), even_elements.end());
-
-    vector<float> l_prime = l;
-    int even_index = 0;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            l_prime[i] = even_elements[even_index];
-            even_index++;
-        }
-    }
-    l = l_prime;
-}
-
-void sort_even_and_assert() {
+int main() {
     assert(issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}), {-12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));
 }
