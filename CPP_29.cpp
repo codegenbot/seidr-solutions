700258000d3af1271d0eb7a9d645c64693bc7305
beep boop <githubbot@vadim.me>
2024-08-03 08:20:56 +0200

    Solution 62 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nh7nsgeg Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:11: 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp: In function ‘int main()’: 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 0361da28-eb64-4f3e-b981-57731bc1cda5.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,5 +1,4 @@
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
+if (a.size() != b.size()) {
         return false;
     }
     for (int i = 0; i < a.size(); i++) {
@@ -8,33 +7,4 @@ bool issame(vector<string> a, vector<string> b) {
         }
     }
     return true;
-}
-
-int main() {
-    vector<string> strings;
-    string prefix;
-
-    // Read input from user
-    cout << "Enter the list of strings: ";
-    while (true) {
-        string s;
-        cin >> s;
-        if (s == "quit") break;
-        strings.push_back(s);
-    }
-    cout << "Enter the prefix: ";
-    cin >> prefix;
-
-    // Call filter_by_prefix and check result
-    vector<string> result = filter_by_prefix(strings, prefix);
-
-    // Check issame function
-    vector<string> another_result = {"a", "b", "c"};
-    if (issame(result, another_result)) {
-        cout << "The results are the same." << endl;
-    } else {
-        cout << "The results are different." << endl;
-    }
-
-    return 0;
 }
