4405d84ab4fa710cf7de22da9c95170f98ce183a
beep boop <githubbot@vadim.me>
2024-08-03 02:24:58 +0200

    Solution 9 of CPP/61, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/fqt11vio Prompt: 206b32c3-0eed-442e-8930-b4c0d7ae1695.cpp:2:1: error: expected unqualified-id bef ore ‘for’     2 | for (char bracket : brackets) {       | ^~~ 206b32c3-0eed-442e-8930-b4c0d7ae1695.cpp:12:1: error: expected unqualified-id be fore ‘return’    12 | return count == 0;       | ^~~~~~ 206b32c3-0eed-442e-8930-b4c0d7ae1695.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from 206b32c3-0eed-442e-8930-b4c0d7ae1695.cpp:15: 206b32c3-0eed-442e-8930-b4c0d7ae1695.cpp: In function ‘int main()’: 206b32c3-0eed-442e-8930-b4c0d7ae1695.cpp:17:18: error: ‘correct_bracketing’ was not declared in this scope    17 |     assert (not (correct_bracketing("()()(()())()))()")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_61.cpp
+++ CPP_61.cpp
@@ -3,7 +3,7 @@ bool correct_bracketing(const std::string& brackets) {
     for (char bracket : brackets) {
         if (bracket == '(') {
             count++;
-        } else {
+        } else if (bracket == ')') {
             count--;
             if (count < 0) {
                 return false;
