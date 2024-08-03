cb24bfc9a55e13967bc49cdf15df2810954abf56
beep boop <githubbot@vadim.me>
2024-08-03 19:50:15 +0200

    Solution 85 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/r6y4ygjw Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10610b4e-b743-4fe5-9a5f-8e599c741 c68.py", line 11, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/10610b4e-b743-4fe5-9a5f-8e599c741 c68.py", line 7, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,7 +1,2 @@
 def check(lst, result):
-    if isinstance(result, int) and 10 < result < 99:
-        return "Ooui"
-    elif isinstance(result, int) and (result < 10 or result > 99):
-        return "Non"
-    else:
-        return "Oui"
+    print(f"The sum of the squares is {result}.")
