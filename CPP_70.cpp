57f7a074f765f52364cfd65646853ffb6afcf154
beep boop <githubbot@vadim.me>
2024-08-03 09:36:37 +0200

    Solution 12 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mqhr2bp2 Prompt:       |                    ^~~ ecde039e-625c-4023-8726-68c90ee6bff2.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ecde039e-625c-4023-8726-68c90ee6bff2.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ecde039e-625c-4023-8726-68c90ee6bff2.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ecde039e-625c-4023-8726-68c90ee6bff2.cpp:15: ecde039e-625c-4023-8726-68c90ee6bff2.cpp: In function ‘int main()’: ecde039e-625c-4023-8726-68c90ee6bff2.cpp:25:20: error: ‘strange_sort_list’ was n ot declared in this scope    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ ecde039e-625c-4023-8726-68c90ee6bff2.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -1,22 +1,29 @@
-vector<int> strange_sort_vector(vector<int> lst) {
-    vector<int> result;
-    if (lst.empty()) return result;
+#include <vector>
+#include <algorithm>
 
-    sort(lst.begin(), lst.end());
-    while (!lst.empty()) {
-        result.push_back(*lst.begin());
-        lst.erase(lst.begin());
-        if (!lst.empty())
-            sort(lst.begin(), lst.end());
-    }
-    return result;
-
-}
-
-bool issame(vector<int> a,vector<int>b) {
+bool issame(std::vector<int> a,std::vector<int> b) {
     if(a.size() != b.size()) return false;
     for(int i=0; i<a.size();i++) {
         if(a[i] != b[i]) return false;
     }
     return true;
+}
+
+std::vector<int> strange_sort_vector(std::vector<int> lst) {
+    std::vector<int> result;
+    if (lst.empty()) return result;
+
+    std::sort(lst.begin(), lst.end());
+    while (!lst.empty()) {
+        result.push_back(*lst.begin());
+        lst.erase(lst.begin());
+        if (!lst.empty())
+            std::sort(lst.begin(), lst.end());
+    }
+    return result;
+}
+
+int main() {
+    assert (issame(std::vector<int>(strange_sort_vector({111111})) , std::vector<int>({111111})));
+    return 0;
 }
