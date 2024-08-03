aced5307b8c54e15e2d0d1a4c6219282fd7284b2
beep boop <githubbot@vadim.me>
2024-08-03 11:35:26 +0200

    Solution 40 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/g55asi6l Prompt:       |                    ^~~ 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp:10:27: error: ‘vector’ was not declared  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp:10:39: error: expression list treated a s compound expression in initializer [-fpermissive]    10 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp:9: 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp: In function ‘int main()’: 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 9d55935d-0e50-443e-9494-c6253c3a5a1d.cpp:19:19: error: ‘issame’ cannot be used a s a function    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,12 +1,21 @@
+```cpp
 #include <vector>
 #include <algorithm>
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
+bool std::issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-vector<int> maximum(vector<int> arr, int k) {
-    sort(arr.begin(), arr.end());
-    reverse(arr.begin(), arr.end());
-    return vector<int>(arr.begin(), arr.begin() + k);
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::sort(arr.begin(), arr.end());
+    std::reverse(arr.begin(), arr.end());
+    return std::vector<int>(arr.begin(), arr.begin() + k);
 }
