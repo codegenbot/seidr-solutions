381dd9e024e222aa1f28249b9ef44211cfa0ef3d
beep boop <githubbot@vadim.me>
2024-08-03 09:44:25 +0200

    Solution 51 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/mqhr2bp2 Prompt:       |                    ^~~ 349ef960-9af6-4928-bf55-e584324583a1.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 349ef960-9af6-4928-bf55-e584324583a1.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 349ef960-9af6-4928-bf55-e584324583a1.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 349ef960-9af6-4928-bf55-e584324583a1.cpp:24: 349ef960-9af6-4928-bf55-e584324583a1.cpp: In function ‘int main()’: 349ef960-9af6-4928-bf55-e584324583a1.cpp:34:20: error: ‘strange_sort_list’ was n ot declared in this scope    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |                    ^~~~~~~~~~~~~~~~~ 349ef960-9af6-4928-bf55-e584324583a1.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -3,9 +3,11 @@
 
 bool issame(std::vector<int> a, std::vector<int> b) {
     if(a.size() != b.size()) return false;
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    return a == b;
+    for(int i = 0; i < a.size(); i++) {
+        if(std::find(b.begin(), b.end(), a[i]) == b.end())
+            return false;
+    }
+    return true;
 }
 
 std::vector<int> strange_sort_list(std::vector<int> lst) {
@@ -13,8 +15,8 @@ std::vector<int> strange_sort_list(std::vector<int> lst) {
     if (lst.empty()) return result;
 
     auto min_max = [](const std::vector<int>& v) {
-        int min_val = *min_element(v.begin(), v.end());
-        int max_val = *max_element(v.begin(), v.end());
+        int min_val = *std::min_element(v.begin(), v.end());
+        int max_val = *std::max_element(v.begin(), v.end());
         return make_pair(min_val, max_val);
     };
 
