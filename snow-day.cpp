c74ebec634b3f5958d14b54267e7c37cd94cc414
beep boop <githubbot@vadim.me>
2024-08-03 02:30:40 +0200

    Solution 6 of snow-day, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run4/runs/zg77p8eg Prompt: a7311fcf-b86c-4f77-b471-200f722fd86e.cpp: In function ‘int main()’: a7311fcf-b86c-4f77-b471-200f722fd86e.cpp:4:5: error: ‘cin’ was not declared in t his scope     4 |     cin >> hours >> snow >> rate >> melt;       |     ^~~ a7311fcf-b86c-4f77-b471-200f722fd86e.cpp:11:5: error: ‘cout’ was not declared in  this scope    11 |     cout << snow << endl;       |     ^~~~ a7311fcf-b86c-4f77-b471-200f722fd86e.cpp:11:21: error: ‘endl’ was not declared i n this scope    11 |     cout << snow << endl;       |                     ^~~~

--- snow-day.cpp
+++ snow-day.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int hours;
-    float snow_on_ground, snow_fall_rate, snow_melt_rate;
-    std::cin >> hours >> snow_on_ground >> snow_fall_rate >> snow_melt_rate;
+    double snow, rate, melt;
+    cin >> hours >> snow >> rate >> melt;
     
-    for (int i = 0; i < hours; i++) {
-        snow_on_ground += snow_fall_rate;
-        snow_on_ground -= snow_on_ground * snow_melt_rate;
+    for (int i = 0; i < hours; ++i) {
+        snow += rate;
+        snow -= snow * melt;
     }
     
-    std::cout << snow_on_ground << std::endl;
+    cout << snow << endl;
     
     return 0;
 }
