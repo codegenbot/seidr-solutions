b1367271810da9d6217fb5795be42f55f26bd1bd
beep boop <githubbot@vadim.me>
2024-08-03 08:48:22 +0200

    Solution 16 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/r88ilw8d Prompt:       |             ^~~~~~ 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp:19:20: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp:19:27: error: ‘vector’ was not declared  in this scope    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp:18: 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp: In function ‘int main()’: 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp:28:20: error: ‘sort_array’ was not decl ared in this scope    28 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |                    ^~~~~~~~~~ 9be18fd4-84ff-46d9-ac12-06659798d0c5.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_array({2,4,8,16,32}) , {2, 4, 8, 16, 32}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -2,7 +2,9 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
     return a == b;
 }
 
@@ -17,8 +19,4 @@ std::vector<int> sort_array(std::vector<int> arr) {
     });
 
     return arr;
-}
-
-int main() {
-    assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
 }
