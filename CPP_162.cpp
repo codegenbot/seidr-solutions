9a5d241780f41a1f142a43ebf867a7bf35d36cac
beep boop <githubbot@vadim.me>
2024-08-03 03:30:40 +0200

    Solution 25 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/pfe9otlx Prompt: 3240c32c-e62b-4533-b051-d4e7a51ea1e6.cpp: In function ‘std::string string_to_md5 (const string&)’: 3240c32c-e62b-4533-b051-d4e7a51ea1e6.cpp:14:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    14 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from 3240c32c-e62b-4533-b051-d4e7a51ea1e6.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ /usr/bin/ld: /tmp/ccWGdFak.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 3240c32c-e62b-4533-b051-d4e7a51ea1e6.cpp:(.text+0x97): undefined reference to `M D5' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -13,14 +13,14 @@ std::string string_to_md5(const std::string& text) {
 
     md = EVP_md5();
     mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestInit_ex(mdctx, md, nullptr);
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
     EVP_DigestFinal_ex(mdctx, digest, &digest_len);
     EVP_MD_CTX_free(mdctx);
 
     char mdString[33];
     for(int i = 0; i < digest_len; i++) {
-        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
+        sprintf(&mdString[i * 2], "%02x", static_cast<unsigned int>(digest[i]));
     }
 
     return mdString;
