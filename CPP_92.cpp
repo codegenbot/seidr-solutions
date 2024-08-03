bc00d1d2d10a0bcb62e38699104c24f42c4898dd
beep boop <githubbot@vadim.me>
2024-08-03 04:34:24 +0200

    Solution 4 of CPP/92, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/sun5swu8 Prompt: 43880141-be08-4106-a41d-2ba8df46c2f7.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if((a == b + c) || (b == a + c) || (c == a + b)){       | ^~ 43880141-be08-4106-a41d-2ba8df46c2f7.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return false;       |     ^~~~~~ 43880141-be08-4106-a41d-2ba8df46c2f7.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 43880141-be08-4106-a41d-2ba8df46c2f7.cpp:9: 43880141-be08-4106-a41d-2ba8df46c2f7.cpp: In function ‘int main()’: 43880141-be08-4106-a41d-2ba8df46c2f7.cpp:11:13: error: ‘any_int’ was not declare d in this scope    11 |     assert (any_int(3.01,4,7)==false);       |             ^~~~~~~

--- CPP_92.cpp
+++ CPP_92.cpp
@@ -1,7 +1,6 @@
-#include<iostream>
-#include<assert.h>
+#include <cassert>
 
-bool any_int(double a, double b, double c) {
+bool any_int(double a, double b, double c){
     if((a == b + c) || (b == a + c) || (c == a + b)){
         if(a == (int)a && b == (int)b && c == (int)c){
             return true;
@@ -10,7 +9,7 @@ bool any_int(double a, double b, double c) {
     return false;
 }
 
-int main() {
+int main(){
     assert(any_int(3.01, 4, 7) == false);
     return 0;
 }
