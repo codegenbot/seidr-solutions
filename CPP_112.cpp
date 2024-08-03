5d2d0e3ed6ecd75007d09972d90851f7409f5cda
beep boop <githubbot@vadim.me>
2024-08-03 03:10:50 +0200

    Solution 92 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/g9ohp2aq Prompt:       |                              ^~~~~~ e3d86233-3914-406c-878b-194b1d2b5749.cpp:26:37: error: ‘string’ was not declared  in this scope    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ e3d86233-3914-406c-878b-194b1d2b5749.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ e3d86233-3914-406c-878b-194b1d2b5749.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from e3d86233-3914-406c-878b-194b1d2b5749.cpp:25: e3d86233-3914-406c-878b-194b1d2b5749.cpp: In function ‘int main()’: e3d86233-3914-406c-878b-194b1d2b5749.cpp:35:20: error: ‘reverse_delete’ was not declared in this scope    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ e3d86233-3914-406c-878b-194b1d2b5749.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -3,13 +3,9 @@
 #include <algorithm>
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); i++) {
-        if (a[i] != b[i]) {
-            return false;
-        }
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
     }
     return true;
 }
