7988d3a4d6051b11f94ed39afcfe169edaad7aa6
beep boop <githubbot@vadim.me>
2024-08-03 11:01:57 +0200

    Solution 8 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/frjlpwac Prompt:       |                     ^ ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp:4:30: error: ‘n’ was not declared in th is scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                              ^ ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp:4:16: error: ‘stoi’ was not declared in  this scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                ^~~~ ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (num1 * num2) % (den1 * den2) == 0;       |     ^~~~~~ ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp:9: ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp: In function ‘int main()’: ac014139-e341-4a44-bb5b-a77bbd8db6f5.cpp:11:13: error: ‘simplify’ was not declar ed in this scope    11 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-using namespace std;
 
-bool simplify(string x, string n) {
-    int num1 = stoi(x.substr(0, x.find('/')));
-    int den1 = stoi(x.substr(x.find('/') + 1));
-    int num2 = stoi(n.substr(0, n.find('/')));
-    int den2 = stoi(n.substr(n.find('/') + 1));
+bool simplify(std::string x, std::string n) {
+    int num1 = std::stoi(x.substr(0, x.find('/')));
+    int den1 = std::stoi(x.substr(x.find('/') + 1));
+    int num2 = std::stoi(n.substr(0, n.find('/')));
+    int den2 = std::stoi(n.substr(n.find('/') + 1));
 
-    return (num1 * den2) == (num2 * den1);
+    return (num1 * num2) % (den1 * den2) == 0;
 }
 
-int main(){
-    assert (simplify("1/5", "1/5") == false);
+int main() {
+    assert(simplify("1/5", "1/5") == false);
     return 0;
 }
