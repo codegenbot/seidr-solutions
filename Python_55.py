aa8f5aca7d3ae8c31431178f56bd318fad92e7c3
beep boop <githubbot@vadim.me>
2024-08-03 14:27:39 +0200

    Solution 50 of Python/55, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/pbz1n0kj Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d08e15d7-3fa3-42f4-b947-111c49533 2ae.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_55.py
+++ Python_55.py
@@ -1,10 +1,11 @@
-def fib(n):
+```
+print("Enter a number: ")
+n = int(input())
+def fib(n: int):
     if n <= 1:
         return n
-    a, b = 1, 1
-    for _ in range(2, n+1):
-        a, b = b, a+b
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
     return b
-
-n = int(input("Enter a number: "))
-print(fib(n))
+print("The {}th number is {}".format(n, fib(n)))
