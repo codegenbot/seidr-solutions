e0dfd0b6eaf6bdc49ae85790445a577d489c1c44
beep boop <githubbot@vadim.me>
2024-08-03 17:36:12 +0200

    Solution 5 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0d7n12ag Prompt:    26 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d8a22c39-eb3d-4d1e-8bed-4cb6f2e52763.cpp:26:44: error: ‘b’ was not declared in t his scope    26 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d8a22c39-eb3d-4d1e-8bed-4cb6f2e52763.cpp:26:45: error: expression list treated a s compound expression in initializer [-fpermissive]    26 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d8a22c39-eb3d-4d1e-8bed-4cb6f2e52763.cpp:25: d8a22c39-eb3d-4d1e-8bed-4cb6f2e52763.cpp: In function ‘int main()’: d8a22c39-eb3d-4d1e-8bed-4cb6f2e52763.cpp:35:20: error: ‘separate_paren_groups’ w as not declared in this scope    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ d8a22c39-eb3d-4d1e-8bed-4cb6f2e52763.cpp:35:19: error: ‘issame’ cannot be used a s a function    35 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -8,24 +8,14 @@ using namespace std;
 vector<string> separate_paren_groups(string paren_string);
 
 bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
+    return a == b;
 }
 
 int main() {
     assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
-    
+
     cout << "Test passed!" << endl;
-    
+
     return 0;
 }
 
@@ -33,7 +23,7 @@ vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
     string group;
     int count = 0;
-    
+
     for (char c : paren_string) {
         if (c == '(') {
             count++;
@@ -50,6 +40,6 @@ vector<string> separate_paren_groups(string paren_string) {
             }
         }
     }
-    
+
     return result;
 }
