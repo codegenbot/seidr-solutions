4ff44312c5d0e6cb452b0c1d72d70072a1522f45
beep boop <githubbot@vadim.me>
2024-08-03 10:24:16 +0200

    Solution 85 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/5nbp4vc7 Prompt:       |                              ^~~~~~ a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp:32: a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp: In function ‘int main()’: a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp:42:20: error: ‘numerical_letter_grade’ was not declared in this scope    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ a32082d1-dcb7-4df3-a2f5-ba3e8faea5d3.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -13,12 +13,14 @@ std::vector<std::string> numerical_letter_grade(std::vector<float> grades) {
         else if (grade > 3.0)
             letter_grades.push_back("B");
         else if (grade > 2.7)
-            letter_grades.push_back("C+");
+            letter_grades.push_back("B-");
         else if (grade > 2.3)
-            letter_grades.push_back("C");
+            letter_grades.push_back("C+");
         else if (grade > 2.0)
-            letter_grades.push_back("D+");
+            letter_grades.push_back("C");
         else if (grade > 1.7)
+            letter_grades.push_back("D+");
+        else if (grade > 1.3)
             letter_grades.push_back("D");
         else
             letter_grades.push_back("F");
