06876e024c197a6c64e2ccb41bdd5208fb8cced6
beep boop <githubbot@vadim.me>
2024-08-03 05:39:07 +0200

    Solution 20 of CPP/106, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/95vwmlhz Prompt: in this scope     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f5f17992-0f7a-44c0-b164-06976b553033.cpp:9:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from f5f17992-0f7a-44c0-b164-06976b553033.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from f5f17992-0f7a-44c0-b164-06976b553033.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ f5f17992-0f7a-44c0-b164-06976b553033.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from f5f17992-0f7a-44c0-b164-06976b553033.cpp:8: f5f17992-0f7a-44c0-b164-06976b553033.cpp: In function ‘int main()’: f5f17992-0f7a-44c0-b164-06976b553033.cpp:18:20: error: ‘f’ was not declared in t his scope    18 |     assert (issame(f(3) , {1, 2, 6}));       |                    ^

--- CPP_106.cpp
+++ CPP_106.cpp
@@ -1,21 +1,5 @@
 #include <vector>
-#include <cassert>
 
 bool issame(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-int main() {
-    assert(issame({1, 2, 6}, {1, 2, 6}));
-    return 0;
+    return a == b;
 }
