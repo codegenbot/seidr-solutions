2ae4ff2f78629cbe4f0170344e7f7e0babb993d0
beep boop <githubbot@vadim.me>
2024-08-03 19:23:59 +0200

    Solution 22 of CPP/21, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/iqt05ufk Prompt: In file included from /usr/include/c++/11/cassert:44,                  from eea41f54-cd78-4aea-a47b-4988a1296a71.cpp:4: eea41f54-cd78-4aea-a47b-4988a1296a71.cpp: In function ‘int main()’: eea41f54-cd78-4aea-a47b-4988a1296a71.cpp:30:34: error: cannot bind non-const lva lue reference of type ‘std::vector<float>&’ to an rvalue of type ‘std::vector<fl oat>’    30 |     assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25 , 0.0, 1.0, 0.5, 0.75}));       |                   ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ eea41f54-cd78-4aea-a47b-4988a1296a71.cpp:12:28: note:   initializing argument 1 of ‘bool issame(std::vector<float>&, std::vector<float>&)’    12 | bool issame(vector<float>& a, vector<float>& b) {       |             ~~~~~~~~~~~~~~~^ eea41f54-cd78-4aea-a47b-4988a1296a71.cpp: At global scope: eea41f54-cd78-4aea-a47b-4988a1296a71.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ eea41f54-cd78-4aea-a47b-4988a1296a71.cpp:29:5: note: ‘int main()’ previously def ined here    29 | int main() {       |     ^~~~

--- CPP_21.cpp
+++ CPP_21.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 bool issame(const vector<float>& a, const vector<float>& b);
 
-vector<float> rescale_to_unit(vector<float> numbers);
+vector<float> rescale_to_unit(const vector<float>& numbers);
 
 bool issame(const vector<float>& a, const vector<float>& b) {
     return a == b;
 }
 
-vector<float> rescale_to_unit(vector<float> numbers) {
+vector<float> rescale_to_unit(const vector<float>& numbers) {
     float min_num = *min_element(numbers.begin(), numbers.end());
     float max_num = *max_element(numbers.begin(), numbers.end());
     
@@ -26,6 +26,9 @@ vector<float> rescale_to_unit(vector<float> numbers) {
     return rescaled_numbers;
 }
 
-void main() {
-    assert(issame(rescale_to_unit({12.0, 11.0, 15.0, 13.0, 14.0}), {0.25, 0.0, 1.0, 0.5, 0.75}));
+int main() {
+    vector<float> input = {12.0, 11.0, 15.0, 13.0, 14.0};
+    assert(issame(rescale_to_unit(input), {0.25, 0.0, 1.0, 0.5, 0.75}));
+    
+    return 0;
 }
