6df0c338d93ebafaec41dea1987342821f526d93
beep boop <githubbot@vadim.me>
2024-08-04 00:26:21 +0200

    Solution 69 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt:    92 | #  define assert(expr) \       | 126573a8-4721-4e63-b99a-4c30d934dd88.cpp: In function ‘int main()’: 126573a8-4721-4e63-b99a-4c30d934dd88.cpp:28:5: error: ‘assert’ was not declared in this scope    28 |     assert (count_up_to(101) == vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97});       |     ^~~~~~ 126573a8-4721-4e63-b99a-4c30d934dd88.cpp:3:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     2 | #include <cassert>   +++ |+#include <cassert>     3 | using namespace std; 126573a8-4721-4e63-b99a-4c30d934dd88.cpp: At global scope: 126573a8-4721-4e63-b99a-4c30d934dd88.cpp:42:5: error: redefinition of ‘int main( )’    42 | int main(){       |     ^~~~ 126573a8-4721-4e63-b99a-4c30d934dd88.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main(){       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -2,6 +2,8 @@
 #include <cassert>
 using namespace std;
 
+vector<int> count_up_to(int n);
+
 vector<int> count_up_to(int n){
     vector<int> primes;
     if (n >= 2) {
@@ -22,8 +24,4 @@ vector<int> count_up_to(int n){
     return primes;
 }
 
-int main(){
-    assert (count_up_to(101) == vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97});
-    
-    return 0;
-}
+assert (count_up_to(101) == vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97});
