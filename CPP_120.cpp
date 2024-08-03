cb24312cac6bfd84416698edae951b1cd93d62d4
beep boop <githubbot@vadim.me>
2024-08-03 11:37:37 +0200

    Solution 50 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/g55asi6l Prompt:       |                    ^~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c997fadd-d3d7-4849-9199-939d84d65e11.cpp:16: c997fadd-d3d7-4849-9199-939d84d65e11.cpp: In function ‘int main()’: c997fadd-d3d7-4849-9199-939d84d65e11.cpp:26:20: error: ‘maximum’ was not declare d in this scope    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,17 +1,35 @@
-```cpp
+#include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 
-bool std::issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) return false;
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
     }
     return true;
 }
 
-std::vector<int> maximum(std::vector<int> arr, int k) {
-    std::sort(arr.begin(), arr.end());
-    std::reverse(arr.begin(), arr.end());
-    return std::vector<int>(arr.begin(), arr.begin() + k);
+vector<int> maximum(vector<int> arr, int k) {
+    priority_queue<int> pq;
+    for (int num : arr) {
+        pq.push(num);
+    }
+    vector<int> result;
+    for (int i = 0; i < k; i++) {
+        result.push_back(pq.top());
+        pq.pop();
+    }
+    return result;
+}
+
+int main() {
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));
+    return 0;
 }
