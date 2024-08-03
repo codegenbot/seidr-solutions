e0c372edd6620defb55e27bcdce7cb2de05d577c
beep boop <githubbot@vadim.me>
2024-08-03 11:59:06 +0200

    Solution 57 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ele97v8e Prompt:       |                           ^~~~~~ bd59987a-86f2-4ce7-a3e7-0030132f3a43.cpp:37:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bd59987a-86f2-4ce7-a3e7-0030132f3a43.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bd59987a-86f2-4ce7-a3e7-0030132f3a43.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bd59987a-86f2-4ce7-a3e7-0030132f3a43.cpp:37:34: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bd59987a-86f2-4ce7-a3e7-0030132f3a43.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ bd59987a-86f2-4ce7-a3e7-0030132f3a43.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main(){       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -25,11 +25,5 @@ std::vector<int> order_by_points(const std::vector<int>& nums){
         }
         return sum_a < sum_b;
     });
-
     return sorted_nums;
-}
-
-int main(){
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
 }
