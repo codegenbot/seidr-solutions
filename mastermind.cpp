cc0f084643265eada4cccafa6ff37657057128d8
beep boop <githubbot@vadim.me>
2024-08-04 00:28:05 +0200

    Solution 71 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/4qq32t3x Prompt: 932ad289-3746-4152-9051-77045ff8e40e.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 932ad289-3746-4152-9051-77045ff8e40e.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 932ad289-3746-4152-9051-77045ff8e40e.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -5,15 +5,19 @@ int mastermind(std::string code, std::string guess) {
     int white = 0;
     int black = 0;
 
-    for (char c : code) {
-        auto pos = guess.find(c);
-        if (pos != std::string::npos) {
-            if (code[pos] == c) {
-                black++;
-            } else {
-                white++;
-                guess.erase(pos, 1);
-            }
+    for (int i = 0; i < 4; i++) {
+        if (code[i] == guess[i]) {
+            black++;
+        }
+    }
+
+    for (char c : guess) {
+        auto it = code.find(c);
+        while (it != std::string::npos && black > 0) {
+            it = code.find(c, it + 1);
+        }
+        if (it != std::string::npos) {
+            white++;
         }
     }
 
