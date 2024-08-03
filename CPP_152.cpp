8b21c47fcd2cca38a0790df5abe51ea7b51867a2
beep boop <githubbot@vadim.me>
2024-08-03 02:28:36 +0200

    Solution 22 of CPP/152, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/jhtpp0z8 Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 10c78a19-efce-4d03-b2ea-7719d59f9fbb.cpp:2: 10c78a19-efce-4d03-b2ea-7719d59f9fbb.cpp: In function ‘int main()’: 10c78a19-efce-4d03-b2ea-7719d59f9fbb.cpp:18:12: error: ‘issame’ was not declared  in this scope    18 |     assert(issame(compare({1,2,3,5},{-1,2,3,4}), {2,0,0,1}));       |            ^~~~~~ 10c78a19-efce-4d03-b2ea-7719d59f9fbb.cpp: At global scope: 10c78a19-efce-4d03-b2ea-7719d59f9fbb.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 10c78a19-efce-4d03-b2ea-7719d59f9fbb.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_152.cpp
+++ CPP_152.cpp
@@ -4,7 +4,9 @@
 
 using namespace std;
 
-vector<int> compare(vector<int> game, vector<int> guess);
+bool vector_equal(const vector<int>& v1, const vector<int>& v2) {
+    return v1 == v2;
+}
 
 vector<int> compare(vector<int> game, vector<int> guess) {
     vector<int> result;
@@ -15,7 +17,7 @@ vector<int> compare(vector<int> game, vector<int> guess) {
 }
 
 int main() {
-    assert(compare({1,2,3,5},{-1,2,3,4}) == vector<int>{2,0,0,1});
-
+    assert(vector_equal(compare({1,2,3,5},{-1,2,3,4}), {2,0,0,1}));
+    
     return 0;
 }
