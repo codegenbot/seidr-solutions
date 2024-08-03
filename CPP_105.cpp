4aa5c097d91a94285ddd0a04bb4a6e985a5a3ead
beep boop <githubbot@vadim.me>
2024-08-04 01:01:04 +0200

    Solution 86 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ofsecnl7 Prompt:       |           ^~~~~~ In file included from 0da826d4-d8ca-4cc4-9de8-ed5a4a3fb115.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0da826d4-d8ca-4cc4-9de8-ed5a4a3fb115.cpp:49:37: error: ‘string’ was not declared  in this scope    49 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 0da826d4-d8ca-4cc4-9de8-ed5a4a3fb115.cpp:49:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 0da826d4-d8ca-4cc4-9de8-ed5a4a3fb115.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 0da826d4-d8ca-4cc4-9de8-ed5a4a3fb115.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 0da826d4-d8ca-4cc4-9de8-ed5a4a3fb115.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,13 +1,11 @@
-#include <iostream>
 #include <vector>
 #include <string>
 #include <map>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
 
-using namespace std;
-
-bool issame(vector<string> a, vector<string> b) {
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
     if (a.size() != b.size()) return false;
     for (int i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) return false;
@@ -15,9 +13,9 @@ bool issame(vector<string> a, vector<string> b) {
     return true;
 }
 
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
-    map<int, string> num_to_name = {
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<std::string> result;
+    std::map<int, std::string> num_to_name = {
         {1, "One"},
         {2, "Two"},
         {3, "Three"},
@@ -29,15 +27,14 @@ vector<string> by_length(vector<int> arr) {
         {9, "Nine"}
     };
 
-    vector<int> filtered;
+    std::vector<int> filtered;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
             filtered.push_back(num);
         }
     }
 
-    sort(filtered.begin(), filtered.end());
-    reverse(filtered.begin(), filtered.end());
+    std::sort(filtered.begin(), filtered.end(), std::greater<int>());
 
     for (int num : filtered) {
         result.push_back(num_to_name[num]);
@@ -46,4 +43,7 @@ vector<string> by_length(vector<int> arr) {
     return result;
 }
 
-assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+int main() {
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
+    return 0;
+}
