15892af5d1431df2999f6b24b13ce35e99283a9f
beep boop <githubbot@vadim.me>
2024-08-03 20:55:21 +0200

    Solution 88 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/psk8bxyl Prompt:     9 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 02bf41da-c299-47bf-a594-34e45da9d672.cpp:17:5: error: redefinition of ‘int main( )’    17 | int main(){       |     ^~~~ 02bf41da-c299-47bf-a594-34e45da9d672.cpp:1:5: note: ‘int main()’ previously defi ned here     1 | int main() {       |     ^~~~ In file included from 02bf41da-c299-47bf-a594-34e45da9d672.cpp:8: 02bf41da-c299-47bf-a594-34e45da9d672.cpp: In function ‘int main()’: 02bf41da-c299-47bf-a594-34e45da9d672.cpp:18:20: error: ‘unique’ was not declared  in this scope    18 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ 02bf41da-c299-47bf-a594-34e45da9d672.cpp:18:19: error: ‘issame’ cannot be used a s a function    18 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,17 +1,14 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(std::vector<int> a, std::vector<int> b) {
     std::sort(a.begin(), a.end());
     std::sort(b.begin(), b.end());
     return a == b;
 }
 
 int main() {
-
     assert(issame({5, 3, 5, 2, 3, 3, 9, 0, 123}, {0, 2, 3, 5, 9, 123}));
-
     return 0;
 }
