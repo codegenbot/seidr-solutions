4900e367497c736dad312661c09ce56ca6888d97
beep boop <githubbot@vadim.me>
2024-08-03 17:30:59 +0200

    Solution 11 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/bg1cebug Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e518bb67-a2a5-4332-a0fa-6ac154edb 14d.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -3,12 +3,12 @@ def valid_date(date):
     parts = date.split('-')
     if len(parts) != 3:
         return False
-    month, day, year = map(int, parts[1::])
-    if not (1 <= month <= 12):
+    month, day, year = map(int, parts)
+    if month < 1 or month > 12:
         return False
-    if ((month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12) and day > 31) or \
-       ((month == 4 or month == 6 or month == 9 or month == 11) and day > 30) or (month == 2 and day > 29):
+    if (month in [1,3,5,7,8,10,12] and int(day) > 31) or \
+       (month in [4,6,9,11] and int(day) > 30) or (month == 2 and int(day) > 29):
         return False
-    if len(parts[0]) != 4 or len(parts[1]) != 2:
+    if len(str(month)) != 2 or len(str(day)) != 2 or len(str(year)) != 4:
         return False
     return True
