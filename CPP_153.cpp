2fdd78a1eae9a1d4e6500f7b1b056f0b0f5898aa
beep boop <githubbot@vadim.me>
2024-08-03 12:58:18 +0200

    Solution 76 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/n1tu4p4y Prompt:       |                         ^~~~~~ 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp:1:43: error: ‘vector’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                           ^~~~~~ 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp:1:50: error: ‘string’ was not declared in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                  ^~~~~~ 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp:1:58: error: ‘extensions’ was not decla red in this scope     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                          ^~~~~~~~~~ 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp:1:68: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int Strongest_Extension(string class_name,vector<string> extensions){       |                                                                    ^ In file included from 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp:25: 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp: In function ‘int main()’: 1d27e935-cd60-4a01-b0a3-17604d1e0d2c.cpp:27:32: error: ‘Strongest_Extension’ can not be used as a function    27 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,30 +1,35 @@
-```c++
 #include <string>
 #include <vector>
-#include <algorithm>
 
-int main(){
-    int Strongest_Extension(string class_name,vector<string> extensions){
-        int max_strength = -1;
-        string strongest_extension;
-        
-        for(auto extension : extensions){
-            int cap = 0, sm = 0;
-            for(int i = 0; i < extension.length(); i++){
-                if(isupper(extension[i])) cap++;
-                else if(islower(extension[i])) sm++;
-            }
-            int strength = cap - sm;
-            
-            if(strength > max_strength){
-                max_strength = strength;
-                strongest_extension = extension;
-            }else if(strength == max_strength){
-                if(find(extensions.begin(), extensions.end(), extension) < find(extensions.begin(), extensions.end(), strongest_extension))
-                    strongest_extension = extension;
-            }
+using namespace std;
+
+int Strongest_Extension(string class_name,vector<string> extensions){
+    int max_strength = -1;
+    string strongest_extension;
+    
+    for(auto extension : extensions){
+        int cap = 0, sm = 0;
+        for(int i = 0; i < extension.length(); i++){
+            if(isupper(extension[i])) cap++;
+            else if(islower(extension[i])) sm++;
         }
+        int strength = cap - sm;
         
-        return class_name + "." + strongest_extension;
+        if(strength > max_strength){
+            max_strength = strength;
+            strongest_extension = extension;
+        }else if(strength == max_strength){
+            if(find(extensions.begin(), extensions.end(), extension) < find(extensions.begin(), extensions.end(), strongest_extension))
+                strongest_extension = extension;
+        }
     }
+    
+    return class_name + "." + strongest_extension;
+}
+
+int main() {
+    string class_name = "MyClass";
+    vector<string> extensions = {"Extension1", "Extension2", "EXTENSION3"};
+    cout << Strongest_Extension(class_name, extensions) << endl;
+    return 0;
 }
