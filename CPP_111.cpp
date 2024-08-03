c4ae381812d2265f7f60641928157951c99aab86
beep boop <githubbot@vadim.me>
2024-08-03 06:20:45 +0200

    Solution 62 of CPP/111, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/l81g71pi Prompt: baf09b6a-dbe8-450a-9759-e6fc3e5c31c3.cpp:39:30: error: invalid conversion from ‘ const char*’ to ‘int’ [-fpermissive]    39 |     assert (issame(histogram("a") , {{'a', 1}}));       |                              ^~~       |                              |       |                              const char* baf09b6a-dbe8-450a-9759-e6fc3e5c31c3.cpp:6:40: note:   initializing argument 1 o f ‘std::map<char, int> histogram(const int&)’     6 | map<char, int> histogram(const string& str) {       |                          ~~~~~~~~~~~~~~^~~ In file included from baf09b6a-dbe8-450a-9759-e6fc3e5c31c3.cpp:24: baf09b6a-dbe8-450a-9759-e6fc3e5c31c3.cpp:39:19: error: call of overloaded ‘issam e(std::map<char, int>, <brace-enclosed initializer list>)’ is ambiguous    39 |     assert (issame(histogram("a") , {{'a', 1}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~ baf09b6a-dbe8-450a-9759-e6fc3e5c31c3.cpp:14:6: note: candidate: ‘bool issame(con st std::map<char, int>&, const std::map<char, int>&)’    14 | bool issame(const map<char, int>& a, const map<char, int>& b) {       |      ^~~~~~ baf09b6a-dbe8-450a-9759-e6fc3e5c31c3.cpp:25:6: note: candidate: ‘bool issame(std ::map<char, int>, std::map<char, int>)’    25 | bool issame(map<char,int> a,map<char,int> b){       |      ^~~~~~

--- CPP_111.cpp
+++ CPP_111.cpp
@@ -1,13 +1,12 @@
 #include <map>
 #include <cassert>
-#include <string>
 
 using namespace std;
 
-map<char, int> histogram(const string& str) {
+map<char, int> histogram(const char* str) {
     map<char, int> result;
-    for (char c : str) {
-        result[c]++;
+    for (const char* p = str; *p != '\0'; ++p) {
+        result[*p]++;
     }
     return result;
 }
@@ -17,7 +16,7 @@ bool issame(const map<char, int>& a, const map<char, int>& b) {
 }
 
 int main() {
-    assert(issame(histogram(string("a")), histogram(string("a"))));
-    assert(issame(histogram(string("a")), histogram(string("a"))));
+    assert(issame(map<char, int>{{'a', 1}}, {{'a', 1}}));
+    assert(issame(histogram("a"), {{'a', 1}}));
     return 0;
 }
