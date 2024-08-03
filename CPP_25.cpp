dde6d417397b4e00813a3f99c8ac618127d5a413
beep boop <githubbot@vadim.me>
2024-08-03 11:18:54 +0200

    Solution 4 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ikovf1qp Prompt:       |                    ^~~ 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp:15:27: error: ‘vector’ was not declared  in this scope    15 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp:15:34: error: expected primary-expressi on before ‘int’    15 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp:15:39: error: expression list treated a s compound expression in initializer [-fpermissive]    15 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp:14: 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp: In function ‘int main()’: 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp:24:20: error: ‘factorize’ was not decla red in this scope    24 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 47f4253d-27ad-48f2-b968-368845ff9a8c.cpp:24:19: error: ‘issame’ cannot be used a s a function    24 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,21 +1,8 @@
 #include <vector>
+#include <cassert>
 
-bool issame(vector<int> a, vector<int> b) {
-    if(a.size() != b.size()) {
-        return false;
-    }
-    
-    for(int i = 0; i < a.size(); i++) {
-        if(a[i] != b[i]) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-vector<int> factorize(int n) {
-    vector<int> factors;
+std::vector<int> factorize(int n) {
+    std::vector<int> factors;
     for (int i = 2; i * i <= n; i++) {
         while (n % i == 0) {
             factors.push_back(i);
@@ -28,4 +15,11 @@ vector<int> factorize(int n) {
     return factors;
 }
 
-// int main() can come after the necessary code modifications.
+bool issame(std::vector<int> a, std::vector<int> b) {
+    return a == b;
+}
+
+int main() {
+    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
+    return 0;
+}
