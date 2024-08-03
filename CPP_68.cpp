0544a68a0820b585f1828f56a8a9908a88fdb5cb
beep boop <githubbot@vadim.me>
2024-08-03 23:02:01 +0200

    Solution 52 of CPP/68, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/07gk8ubl Prompt:       |                           ^~~~~~ e2bba292-ad00-40dc-a366-4c3139dc12d7.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from e2bba292-ad00-40dc-a366-4c3139dc12d7.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from e2bba292-ad00-40dc-a366-4c3139dc12d7.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ e2bba292-ad00-40dc-a366-4c3139dc12d7.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e2bba292-ad00-40dc-a366-4c3139dc12d7.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ e2bba292-ad00-40dc-a366-4c3139dc12d7.cpp:26:5: note: ‘int main()’ previously def ined here    26 | int main(){       |     ^~~~

--- CPP_68.cpp
+++ CPP_68.cpp
@@ -24,7 +24,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
 }
 
 int main(){
-    assert(issame(pluck({7, 9, 7, 1}), std::vector<int>{}));
+    assert(issame(pluck({7, 9, 7, 1}), {}));
 
     return 0;
 }
