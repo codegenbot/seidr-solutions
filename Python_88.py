d0031de478c28ac78a5e2a1909063a6799db3efe
beep boop <githubbot@vadim.me>
2024-08-03 16:40:32 +0200

    Solution 5 of Python/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2yfvz1n8 Prompt: Complete the following code given the task description and function signature.

--- Python_88.py
+++ Python_88.py
@@ -3,9 +3,7 @@ def sort_array(array):
     if len(array) == 0:
         return array
     total = array[0] + array[-1]
-    copy = array.copy()
     if total % 2 != 0:
-        copy.sort()
+        return sorted(array)
     else:
-        copy.sort(reverse=True)
-    return copy
+        return sorted(array, reverse=True)
