bc0b6c41e728de75f01a4d7f6e4b6ef7aac0c663
beep boop <githubbot@vadim.me>
2024-08-03 02:09:10 +0200

    Solution 61 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/m2szh72n Prompt: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bfb7085f-4c71-474b-a054-41fcc07a61f3.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bfb7085f-4c71-474b-a054-41fcc07a61f3.cpp:35:34: error: expected primary-expressi on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from bfb7085f-4c71-474b-a054-41fcc07a61f3.cpp:34: bfb7085f-4c71-474b-a054-41fcc07a61f3.cpp: In function ‘int main()’: bfb7085f-4c71-474b-a054-41fcc07a61f3.cpp:44:35: error: cannot bind non-const lva lue reference of type ‘std::vector<int>&’ to an rvalue of type ‘std::vector<int> ’    44 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~ bfb7085f-4c71-474b-a054-41fcc07a61f3.cpp:5:31: note:   initializing argument 1 o f ‘bool issame(std::vector<int>&, std::vector<int>&)’     5 | bool issame(std::vector<int>& a, std::vector<int>& b){       |             ~~~~~~~~~~~~~~~~~~^

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -30,8 +30,3 @@ std::vector<int> order_by_points(std::vector<int> nums){
 
     return nums;
 }
-
-int main() {
-    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), std::vector<int>{-76, -21, 0, 4, 23, 6, 6}));
-    return 0;
-}
