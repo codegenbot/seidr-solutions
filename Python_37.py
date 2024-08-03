d43d211ab2e3e0fde2a55eb96435e29cb1518a1a
beep boop <githubbot@vadim.me>
2024-08-03 13:47:24 +0200

    Solution 37 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/2cyiblvz Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd689dd3-df47-42cc-9b66-6ed546e2c ba2.py", line 7, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd689dd3-df47-42cc-9b66-6ed546e2c ba2.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd689dd3-df47-42cc-9b66-6ed546e2c ba2.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if (i // 2) * 2 == list.index(y)]) [0]                                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/fd689dd3-df47-42cc-9b66-6ed546e2c ba2.py", line 3, in <listcomp>     x if i % 2 != 0 else sorted([y for y in l if (i // 2) * 2 == list.index(y)]) [0]                                                                  ^^^^^^^^^^^^^ TypeError: descriptor 'index' for 'list' objects doesn't apply to a 'int' object

--- Python_37.py
+++ Python_37.py
@@ -1,5 +1,5 @@
 def sort_even(l: list):
     return [
-        x if i % 2 != 0 else sorted([y for j, y in enumerate(l) if (j := j + 1) * 2 == i])[0]
+        x if i % 2 != 0 else sorted([y for y in l if (j := j + 1) * 2 == i])[0]
         for i, x in enumerate(l)
     ]
