d49c523f1faf47e107b561ba71faf1d134dc214a
beep boop <githubbot@vadim.me>
2024-08-03 11:44:36 +0200

    Solution 2 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/upj7401t Prompt: Complete the following code given the task description and function signature.

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,38 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-
 vector<string> split_words(string txt) {
     vector<string> result;
+
     size_t pos = 0;
     while ((pos = txt.find(' ')) != string::npos) {
         result.push_back(txt.substr(0, pos));
         txt.erase(0, pos + 1);
     }
-    
+
     if (txt.empty()) {
         return result;
     }
 
-    pos = txt.find(',');
-    if (pos == string::npos) {
-        int count = 0;
-        for (char c : txt) {
-            if ((int)c >= (int)'a' && (int)c <= (int)'z') {
-                if ((int)c % 2 != 0) {
-                    count++;
-                }
+    bool foundComma = false;
+    for (char& c : txt) {
+        if (c == ',') {
+            foundComma = true;
+            break;
+        }
+    }
+
+    if (!foundComma) {
+        int oddCount = 0;
+        for (char& c : txt) {
+            if (tolower(c) >= 'a' && tolower(c) <= 'z' && (int)tolower(c) % 2 != 0) {
+                oddCount++;
             }
         }
-        result.push_back(to_string(count));
+        result.push_back(to_string(oddCount));
     } else {
-        while (pos != string::npos) {
+        size_t pos = 0;
+        while ((pos = txt.find(',')) != string::npos) {
             result.push_back(txt.substr(0, pos));
             txt.erase(0, pos + 1);
-            pos = txt.find(',');
         }
         if (!txt.empty()) {
             result.push_back(txt);
@@ -40,27 +39,4 @@ vector<string> split_words(string txt) {
     }
 
     return result;
-}
-
-int main() {
-    // Test cases
-    string test1 = "Hello world!";
-    vector<string> result1 = split_words(test1);
-    for (string s : result1) {
-        cout << s << endl;
-    }
-
-    string test2 = "Hello,world!";
-    vector<string> result2 = split_words(test2);
-    for (string s : result2) {
-        cout << s << endl;
-    }
-
-    string test3 = "abcdef";
-    vector<string> result3 = split_words(test3);
-    for (string s : result3) {
-        cout << s << endl;
-    }
-
-    return 0;
 }
