7b1253069aecbf8fba999d57888c506a96b2633c
beep boop <githubbot@vadim.me>
2024-08-03 04:06:27 +0200

    Solution 9 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q2hs3fhq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,29 +1,20 @@
-#include <iostream>
-#include <string>
-#include <boost/any.hpp>
-
-using namespace std;
-
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
-        return b;
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (float)a < b ? &b : &a;
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return a;
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (int)a > b ? &a : &b;
+    }
+    else if (a.type() == typeid(string) && b.type() == typeid(float)) {
+        return (stof(b.cast<string>()) < stof(a.cast<string>())) ? &b : &a;
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
+        return (stof(b.cast<string>()) > stof(a.cast<string>())) ? &a : &b;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string strA = any_cast<string>(a);
-        string strB = any_cast<string>(b);
-        double numA = stod(strA);
-        double numB = stod(strB);
-        if (numA > numB)
-            return a;
-        else if (numA < numB)
-            return b;
-        else
-            return boost::any("None");
+        return (stof(a.cast<string>()) > stof(b.cast<string>())) ? &a : &b;
     }
     else {
-        return boost::any("None");
+        return "None";
     }
 }
