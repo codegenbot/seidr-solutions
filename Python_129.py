8eaa3a7bd137d07b1b6a72646aaba03938fdbea6
beep boop <githubbot@vadim.me>
2024-08-03 04:36:59 +0200

    Solution 38 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/bzwx60mc Prompt:            ^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 12, in dfs     res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 12, in dfs     res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 12, in dfs     res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   [Previous line repeated 1 more time]   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/089f8319-21af-4467-96b6-217d7057b fbf.py", line 13, in dfs     return min(res)            ^^^^^^^^ ValueError: min() arg is an empty sequence

--- Python_129.py
+++ Python_129.py
@@ -1,4 +1,3 @@
-```
 def minPath(grid, k):
     n = len(grid)
     m = [[i * n + j for j in range(n)] for i in range(n)]
@@ -11,5 +10,9 @@ def minPath(grid, k):
             ni, nj = i + x, j + y
             if 0 <= ni < n and 0 <= nj < n and m[ni][nj] not in visited:
                 res.extend(dfs(ni, nj, path + [m[ni][nj]], visited | {m[ni][nj]}))
-        return min(res) if len(res) > 0 else None
+        if len(res) > 0:
+            return min(res)
+        else:
+            return None
+
     return dfs(0, 0, [], set())
