85fa64e734b4d5ebc4dc0e586f34f8d0e52d48fd
beep boop <githubbot@vadim.me>
2024-08-03 02:56:30 +0200

    Solution 32 of CPP/115, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/48nbcnh6 Prompt:       |              ^~~~~~ 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp:1:21: error: ‘vector’ was not declared in this scope     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                     ^~~~~~ 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp:1:28: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                            ^~~ 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp:1:40: error: expected primary-expressio n before ‘int’     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                        ^~~ 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp:1:52: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int max_fill(vector<vector<int>> grid, int capacity) {       |                                                    ^ In file included from 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp:19: 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp: In function ‘int main()’: 2ebbb47a-6a79-43b4-b4d8-ec5d840f1c37.cpp:21:21: error: ‘max_fill’ cannot be used  as a function    21 |     assert (max_fill({{1,1,1,1}, {1,1,1,1}}, 9) == 2);       |             ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_115.cpp
+++ CPP_115.cpp
@@ -3,7 +3,7 @@
 
 int max_fill(std::vector<std::vector<int>> grid, int capacity) {
     int result = 0;
-    for (const auto& well : grid) {
+    for (std::vector<int> well : grid) {
         int water = 0;
         for (int unit : well) {
             if (unit == 1) {
