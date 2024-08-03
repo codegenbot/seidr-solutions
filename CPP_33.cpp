70f5d11a0a604d8e6c7e6a8c22c0f53c27e9fb8a
beep boop <githubbot@vadim.me>
2024-08-03 20:44:38 +0200

    Solution 14 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:19: 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp: In function ‘int main()’: 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:29:20: error: ‘sort_third’ was not decl ared in this scope    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,17 +1,7 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cassert>
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(vector<int> a, vector<int> b){
     return a == b;
-}
-
-vector<int> sort_third(vector<int> l); // Function declaration
-
-int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    return 0;
 }
 
 vector<int> sort_third(vector<int> l) {
@@ -30,4 +20,9 @@ vector<int> sort_third(vector<int> l) {
         }
     }
     return res;
+}
+
+int main() {
+    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
+    return 0;
 }
