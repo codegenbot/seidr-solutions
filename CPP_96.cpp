a41f7f1da0fc98cb00d4e606ff92050b38b63376
beep boop <githubbot@vadim.me>
2024-08-03 04:59:36 +0200

    Solution 28 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uvcdxz1c Prompt: on before ‘int’    37 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ a57aed6f-9a7e-453f-89ab-1e7d81bbde7e.cpp:37:39: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<int> a,vector<int>b){       |                                       ^ a57aed6f-9a7e-453f-89ab-1e7d81bbde7e.cpp:45:5: error: redefinition of ‘int main( )’    45 | int main(){       |     ^~~~ a57aed6f-9a7e-453f-89ab-1e7d81bbde7e.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from a57aed6f-9a7e-453f-89ab-1e7d81bbde7e.cpp:36: a57aed6f-9a7e-453f-89ab-1e7d81bbde7e.cpp: In function ‘int main()’: a57aed6f-9a7e-453f-89ab-1e7d81bbde7e.cpp:46:19: error: ‘issame’ cannot be used a s a function    46 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -25,18 +25,10 @@ std::vector<int> count_up_to(int n) {
 }
 
 bool is_same(std::vector<int> a, std::vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+    return a == b;
 }
 
 int main() {
-    assert(is_same(count_up_to(101), std::vector<int>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    assert(is_same(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     return 0;
 }
