a8079252881b8b05806ba86ee8fee70eecc12eac
beep boop <githubbot@vadim.me>
2024-08-03 11:10:08 +0200

    Solution 93 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/uak44l3y Prompt:       |                              ^~~~~~ 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp:19:37: error: ‘string’ was not declared  in this scope    19 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp:19:44: error: ‘b’ was not declared in t his scope    19 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp:19:45: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp:18: 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp: In function ‘int main()’: 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp:28:20: error: ‘by_length’ was not decla red in this scope    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 8d0617bc-3eb0-4dae-a5fb-9af347f43912.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,4 +1,7 @@
 ```cpp
+#include <vector>
+#include <algorithm>
+
 bool issame(vector<string> a,vector<string>b){
     if(a.size() != b.size()) return false;
     for(int i = 0; i < a.size(); ++i)
@@ -6,6 +9,19 @@ bool issame(vector<string> a,vector<string>b){
     return true;
 }
 
-int main(){
-    assert (issame({vector<string>(by_length({9, 4, 8}))}, {{"Nine", "Eight", "Four"}});
+vector<string> by_length(vector<int> arr) {
+    vector<string> result;
+    for (int num : arr) {
+        if (num >= 1 && num <= 9) {
+            result.push_back(to_string(num));
+        }
+    }
+    sort(result.begin(), result.end());
+    reverse(result.begin(), result.end());
+    vector<string> names = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    vector<string> output;
+    for (int num : result) {
+        output.push_back(names[stoi(num) - 1]);
+    }
+    return output;
 }
