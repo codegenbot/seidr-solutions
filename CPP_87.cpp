bf4fa0452b3ce18d25cf89a5adc0779492bece9f
beep boop <githubbot@vadim.me>
2024-08-03 14:48:33 +0200

    Solution 89 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/asybmd8o Prompt:       |             ^~~~~~ 03b6d11b-0792-4241-8155-5cb11df141f0.cpp:36:49: error: expected primary-expressi on before ‘int’    36 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 03b6d11b-0792-4241-8155-5cb11df141f0.cpp:36:56: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ 03b6d11b-0792-4241-8155-5cb11df141f0.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 03b6d11b-0792-4241-8155-5cb11df141f0.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from 03b6d11b-0792-4241-8155-5cb11df141f0.cpp:35: 03b6d11b-0792-4241-8155-5cb11df141f0.cpp: In function ‘int main()’: 03b6d11b-0792-4241-8155-5cb11df141f0.cpp:48:19: error: ‘issame’ cannot be used a s a function    48 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -3,6 +3,6 @@ bool issame(std::vector<std::vector<int>> a, std::vector<std::vector<int>> b){
 }
 
 int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
     return 0;
 }
