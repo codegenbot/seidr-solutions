ac50c45ce91034903004a28caa4fcaa4a89095a3
beep boop <githubbot@vadim.me>
2024-08-03 09:01:06 +0200

    Solution 39 of Python/142, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/n7sloetk Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9023f9f1-99ab-47df-8b40-1a0011e5e 81a.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_142.py
+++ Python_142.py
@@ -1,8 +1,12 @@
+```
 def sum_squares(lst):
     total_sum = 0
-    for i in range(len(lst)):
-        if i % 3 == 0:
-            total_sum += lst[i] ** 2
-        elif i % 4 == 0:
-            total_sum += lst[i] ** 3
+    for num in lst:
+        if isinstance(num, int) or isinstance(num, float):
+            if num % 3 == 0 and num % 4 != 0:
+                total_sum += num ** 2
+            elif num % 4 == 0 and num % 3 != 0:
+                total_sum += num ** 3
+        else:
+            print("Invalid input. Please enter integers or floats.")
     return total_sum
