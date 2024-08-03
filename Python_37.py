443cc97333b01e3ee7cb50bbef30fdff309a54ab
beep boop <githubbot@vadim.me>
2024-08-03 12:43:18 +0200

    Solution 70 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n4syyp1w Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/923d8eb1-30c5-4762-819e-97980409e 823.py", line 3     return [x if i % 2 != 0 else y for i, (x, y) in enumerate(sorted((y, x) for x, y in zip(l[1::2], l-none))))                               ^ SyntaxError: closing parenthesis ')' does not match opening parenthesis '['

--- Python_37.py
+++ Python_37.py
@@ -1,6 +1,1 @@
-def sort_even(l: list):
-    return [
-        x if i % 2 != 0 else sorted([y for y in l if (i // 2) == (j // 2)])[
-            0]
-        for i, x in enumerate(l)
-    ]
+return [x if i % 2 != 0 else y for i, (x, y) in enumerate(sorted((y, x) for x, y in zip(l[1::2], l[::2])))]
