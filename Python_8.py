acfb54dee454a53b78b25582f212b1fd444bd045
beep boop <githubbot@vadim.me>
2024-08-03 10:09:30 +0200

    Solution 96 of Python/8, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4pizx1b2 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/4fced963-579c-4bfd-bfe7-b43f318f5 88c.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_8.py
+++ Python_8.py
@@ -1,9 +1,14 @@
-def sum_product(numbers: List[int]) -> Tuple[int, int]:
+from typing import List
+
+
+def find_max_min_avg(numbers: List[int]) -> Tuple[float, float, float]:
     if not numbers:
-        return 0, 1
-    total_sum = 0
-    product = 1
+        return 0.0, 0.0, 0.0
+    max_num = min_num = sum = 0
     for num in numbers:
-        total_sum += num
-        product *= num
-    return total_sum, product
+        if num > max_num:
+            max_num = num
+        elif num < min_num:
+            min_num = num
+        sum += num
+    return float(max_num), float(min_num), float(sum) / len(numbers)
