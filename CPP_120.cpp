37eabb136c937260d17fcc6f14eb6411bd2d7dee
beep boop <githubbot@vadim.me>
2024-08-03 11:40:39 +0200

    Solution 65 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/g55asi6l Prompt:       |                    ^~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:17:27: error: ‘vector’ was not declared  in this scope    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from c997fadd-d3d7-4849-9199-939d84d65e11.cpp:16: c997fadd-d3d7-4849-9199-939d84d65e11.cpp: In function ‘int main()’: c997fadd-d3d7-4849-9199-939d84d65e11.cpp:26:20: error: ‘maximum’ was not declare d in this scope    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ c997fadd-d3d7-4849-9199-939d84d65e11.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,3 +1,4 @@
+```cpp
 #include <vector>
 #include <queue>
 
@@ -9,12 +10,12 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return true;
 }
 
-std::vector<int> maximum(vector<int> arr, int k) {
-    priority_queue<int> pq;
+std::vector<int> maximum(std::vector<int> arr, int k) {
+    std::priority_queue<int> pq;
     for (int num : arr) {
         pq.push(num);
     }
-    vector<int> result;
+    std::vector<int> result;
     for (int i = 0; i < k; i++) {
         result.push_back(pq.top());
         pq.pop();
