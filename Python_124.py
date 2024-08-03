3a545148f70df15878c28a721cf8c15240dd261b
beep boop <githubbot@vadim.me>
2024-08-03 04:14:38 +0200

    Solution 18 of Python/124, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/imtjzf5k Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/5fb2a1db-b2e2-48d6-a8a5-cda481e43 0b3.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_124.py
+++ Python_124.py
@@ -6,4 +6,7 @@ def valid_date(date):
         day = '0' + day
     month = int(month)
     day = int(day)
-    return 1 <= month <= 12 and 1 <= day <= { 1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31 }.get(month)
+    return 1 <= month <= 12 and 1 <= day <= {
+        1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30,
+        7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31
+    }.get(month)
