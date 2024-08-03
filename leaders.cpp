7a0f03c70d531e8e72d3fec31b1381e34ba36123
beep boop <githubbot@vadim.me>
2024-08-03 20:08:43 +0200

    Solution 84 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/x2k9q24b Prompt: /usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/11/../../../x86_64-linux-gnu/Scrt1.o:  in function `_start': (.text+0x1b): undefined reference to `main' collect2: error: ld returned 1 exit status

--- leaders.cpp
+++ leaders.cpp
@@ -1,29 +1,30 @@
 #include <vector>
 using namespace std;
 
-vector<int> leaders(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> res;
-    
-    for(int i=n-1; i>=0; i--){
-        bool leader = true;
-        for(int j=i+1; j<n; j++){
-            if(nums[j] >= nums[i]){
-                leader = false;
-                break;
-            }
-        }
-        if(leader) res.push_back(nums[i]);
-    }
-    
-    return res;
-}
-
 int main() {
-    vector<int> nums = {16, 17, 4, 3, 5, 2};
-    vector<int> leadersResult = leaders(nums);
-    for (int leader : leadersResult) {
-        cout << leader << " ";
+    vector<int> leaders(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res;
+        
+        for(int i=n-1; i>=0; i--){
+            bool leader = true;
+            for(int j=i+1; j<n; j++){
+                if(nums[j] >= nums[i]){
+                    leader = false;
+                    break;
+                }
+            }
+            if(leader) res.push_back(nums[i]);
+        }
+        
+        return res;
     }
-    return 0;
-}
+
+    int main() {
+        vector<int> input = {16, 17, 4, 3, 5, 2, 3, 7, 1, 6};
+        vector<int> result = leaders(input);
+        for(int i=0; i<result.size(); i++) {
+            cout << result[i] << " ";
+        }
+        return 0;
+    }
