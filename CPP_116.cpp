2431ba4302c60e98b728f61b09db040f7c642a6d
beep boop <githubbot@vadim.me>
2024-08-03 10:01:17 +0200

    Solution 78 of CPP/116, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/r88ilw8d Prompt:       |                           ^~~~~~ b24f72d0-5193-4be4-b667-cbf061b925db.cpp:25:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b24f72d0-5193-4be4-b667-cbf061b925db.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b24f72d0-5193-4be4-b667-cbf061b925db.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b24f72d0-5193-4be4-b667-cbf061b925db.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b24f72d0-5193-4be4-b667-cbf061b925db.cpp:33:5: error: redefinition of ‘int main( )’    33 | int main(){       |     ^~~~ b24f72d0-5193-4be4-b667-cbf061b925db.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~

--- CPP_116.cpp
+++ CPP_116.cpp
@@ -2,11 +2,11 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool issame(std::vector<int> a, std::vector<int> b){
     return a == b;
 }
 
-std::vector<int> sort_array(std::vector<int> arr) {
+std::vector<int> sort_array(std::vector<int> arr){
     std::sort(arr.begin(), arr.end(), [](int a, int b) {
         int countA = __builtin_popcount(a);
         int countB = __builtin_popcount(b);
@@ -16,7 +16,7 @@ std::vector<int> sort_array(std::vector<int> arr) {
     return arr;
 }
 
-int main() {
+int main(){
     assert(issame(sort_array({2, 4, 8, 16, 32}), {2, 4, 8, 16, 32}));
     return 0;
 }
