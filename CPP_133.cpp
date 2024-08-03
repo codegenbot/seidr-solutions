ffc2bf47650e38dccb93829d02e991d310d54b04
beep boop <githubbot@vadim.me>
2024-08-03 03:46:43 +0200

    Solution 8 of CPP/133, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k4h1q1aq Prompt: Complete the following code given the task description and function signature.

--- CPP_133.cpp
+++ CPP_133.cpp
@@ -1,8 +1,8 @@
 int sum_squares(vector<float> lst){
-    int sum = 0;
-    for(float num : lst){
-        int ceil_num = ceil(num);
-        sum += pow(ceil_num, 2);
+    int result = 0;
+    for(float x : lst){
+        int ceil_x = ceil(x);
+        result += pow(ceil_x, 2);
     }
-    return sum;
+    return result;
 }
