04ebda679968a7c1f2222f5533199a7d8ff2b9d2
beep boop <githubbot@vadim.me>
2024-08-03 02:05:42 +0200

    Solution 13 of CPP/54, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/zsu14cyr Prompt: 048862b6-6962-489b-b505-530217ccfd1e.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 048862b6-6962-489b-b505-530217ccfd1e.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~

--- CPP_54.cpp
+++ CPP_54.cpp
@@ -6,7 +6,7 @@ bool same_chars(const std::string& s0, const std::string& s1) {
     std::string temp_s0 = s0;
     std::sort(temp_s0.begin(), temp_s0.end());
     
-     std::string temp_s1 = s1;
+    std::string temp_s1 = s1;
     std::sort(temp_s1.begin(), temp_s1.end());
     
     return temp_s0 == temp_s1;
