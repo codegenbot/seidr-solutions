f648bc9786d701f8727487fb066b56a03b1145cd
beep boop <githubbot@vadim.me>
2024-08-04 00:25:11 +0200

    Solution 65 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt: 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:22:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    22 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 4602dc7e-a814-4f40-a1ec-fbaed69c382e.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main(){       |     ^~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -20,10 +20,7 @@ vector<int> count_up_to(int n){
 }
 
 int main(){
-    vector<int> expected_primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-    vector<int> calculated_primes = count_up_to(101);
-
-    assert(calculated_primes == expected_primes);
+    assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     
     return 0;
 }
