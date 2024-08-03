935574b609e905c70027d9e92ac20e22732e528c
beep boop <githubbot@vadim.me>
2024-08-03 11:20:49 +0200

    Solution 6 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ikovf1qp Prompt:       |                    ^~~ e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp:15: e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp: In function ‘int main()’: e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp:25:20: error: ‘factorize’ was not decla red in this scope    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ e89df2b8-df73-4462-a4fe-8f29c71986c9.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,8 +1,7 @@
 #include <vector>
-#include <cassert>
 
-std::vector<int> factorize(int n) {
-    std::vector<int> factors;
+vector<int> factorize(int n) {
+    vector<int> factors;
     for (int i = 2; i * i <= n; i++) {
         while (n % i == 0) {
             factors.push_back(i);
@@ -15,11 +14,7 @@ std::vector<int> factorize(int n) {
     return factors;
 }
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
-
 int main() {
-    assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
+    // Test and use the factorize function here
     return 0;
 }
