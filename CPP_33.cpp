2c80e95d506b0a9a273f5f25de8568bbbbd2de12
beep boop <githubbot@vadim.me>
2024-08-03 20:36:24 +0200

    Solution 2 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:    19 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ f455270e-fbe1-4edc-9d28-4f18887ddafe.cpp:19:34: error: expected primary-expressi on before ‘int’    19 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ f455270e-fbe1-4edc-9d28-4f18887ddafe.cpp:19:39: error: expression list treated a s compound expression in initializer [-fpermissive]    19 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from f455270e-fbe1-4edc-9d28-4f18887ddafe.cpp:18: f455270e-fbe1-4edc-9d28-4f18887ddafe.cpp: In function ‘int main()’: f455270e-fbe1-4edc-9d28-4f18887ddafe.cpp:28:20: error: ‘sort_third’ was not decl ared in this scope    28 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ f455270e-fbe1-4edc-9d28-4f18887ddafe.cpp:28:19: error: ‘issame’ cannot be used a s a function    28 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,7 +1,16 @@
-vector<int> res = l;
-    for (int i = 0; i < l.size(); ++i){
-        if (i % 3 == 0){
-            sort(res.begin() + i, res.begin() + i + 3);
+vector<int> sort_third(vector<int> l) {
+    vector<int> res = l;
+    vector<int> sorted_third;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            sorted_third.push_back(l[i]);
+        }
+    }
+    sort(sorted_third.begin(), sorted_third.end());
+    int idx = 0;
+    for (int i = 0; i < l.size(); ++i) {
+        if (i % 3 == 0) {
+            res[i] = sorted_third[idx++];
         }
     }
     return res;
