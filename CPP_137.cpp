1654bb3d667fea3b6e7dc112d4b9eb686f754d75
beep boop <githubbot@vadim.me>
2024-08-03 12:25:28 +0200

    Solution 74 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/v7bvffxm Prompt: c43486c4-49c0-4754-92af-bccba66b5077.cpp:1:10: fatal error: boost/any_cast.hpp: No such file or directory     1 | #include <boost/any_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -2,15 +2,15 @@
 
 boost::any compare_one(boost::any a, boost::any b) {
     double da, db;
-    string sa, sb;
+    std::string sa, sb;
 
     try {
         da = boost::any_cast<double>(a);
         db = boost::any_cast<double>(b);
     } catch (...) {
         try {
-            sa = boost::any_cast<string>(a);
-            sb = boost::any_cast<string>(b);
+            sa = boost::any_cast<std::string>(a);
+            sb = boost::any_cast<std::string>(b);
         } catch (...) {
             return a;
         }
