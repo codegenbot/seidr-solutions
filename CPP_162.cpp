35bf6dc68fe1a98e7d947030b4afb0467e93c41a
beep boop <githubbot@vadim.me>
2024-08-03 13:21:10 +0200

    Solution 82 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/44w35chd Prompt: 99c1decb-d854-4d7b-80f8-dfe58c942f3d.cpp:3:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     3 | string string_to_md5(string text) {       | ^~~~~~       | stdin In file included from 99c1decb-d854-4d7b-80f8-dfe58c942f3d.cpp:18: 99c1decb-d854-4d7b-80f8-dfe58c942f3d.cpp: In function ‘int main()’: 99c1decb-d854-4d7b-80f8-dfe58c942f3d.cpp:20:13: error: ‘string_to_md5’ was not d eclared in this scope    20 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -8,7 +8,7 @@ string string_to_md5(string text) {
 
     string md5 = "";
     for (int i = 0; i < 16; i++) {
-        md5 += sprintf("%02x", &result[i]);
+        md5 += std::setiosflags(std::ios::hex) << (int)result[i] << resetiosflags(std::ios);
     }
 
     return md5;
