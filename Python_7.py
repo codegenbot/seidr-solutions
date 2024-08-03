5296689d8a7eb42a4bcebe2d14232ec8d217be12
beep boop <githubbot@vadim.me>
2024-08-03 09:01:14 +0200

    Solution 27 of Python/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/y9x3zfiz Prompt: Program did not receive expected input

--- Python_7.py
+++ Python_7.py
@@ -4,5 +4,5 @@ from typing import List
 def filter_by_substring(strings: List[str], substring: str) -> List[str]:
     if not strings:
         return []
-    
+
     return [s for s in strings if substring in s]
