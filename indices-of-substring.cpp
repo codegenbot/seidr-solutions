44c504da5a3d73bf05c527dc74e67d742f0f134f
beep boop <githubbot@vadim.me>
2024-08-03 23:45:00 +0200

    Solution 45 of indices-of-substring, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/1rkve7ol Prompt: 9fcfb0bd-9f5b-46ae-a9df-c55e44801505.cpp:20:5: error: ‘cin’ was not declared in this scope    20 |     cin >> text >> target;       |     ^~~ 9fcfb0bd-9f5b-46ae-a9df-c55e44801505.cpp:2:1: note: ‘std::cin’ is defined in hea der ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <vector>   +++ |+#include <iostream>     2 | using namespace std; 9fcfb0bd-9f5b-46ae-a9df-c55e44801505.cpp:20:12: error: ‘text’ was not declared i n this scope    20 |     cin >> text >> target;       |            ^~~~ 9fcfb0bd-9f5b-46ae-a9df-c55e44801505.cpp:20:20: error: ‘target’ was not declared  in this scope    20 |     cin >> text >> target;       |                    ^~~~~~ 9fcfb0bd-9f5b-46ae-a9df-c55e44801505.cpp:22:20: error: ‘cout’ was not declared i n this scope    22 |     for(int i:res) cout << i << " ";       |                    ^~~~ 9fcfb0bd-9f5b-46ae-a9df-c55e44801505.cpp:22:20: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’?

--- indices-of-substring.cpp
+++ indices-of-substring.cpp
@@ -20,6 +20,5 @@ int main() {
     string text, target;
     std::cin >> text >> target;
     vector<int> res = indicesOfSubstring(text, target);
-    for(int i:res) std::cout << i << " ";
+    for(int i:res) cout << i << " ";
     return 0;
-}
