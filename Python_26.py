ff0252eacf0eb59452e80d695b6912a937292019
beep boop <githubbot@vadim.me>
2024-08-03 22:59:48 +0200

    Solution 64 of Python/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/45ufzzqp Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/710c2475-16df-4e56-af4d-3359e3c53 261.py", line 7, in <module>     check(remove_duplicates)           ^^^^^^^^^^^^^^^^^ NameError: name 'remove_duplicates' is not defined

--- Python_26.py
+++ Python_26.py
@@ -1,6 +1,2 @@
-def remove_duplicates(numbers):
-    return [num for num in numbers if numbers.count(num)==1]
-
-input_numbers = [int(x) for x in input("Enter numbers separated by space: ").split()]
-result = remove_duplicates(input_numbers)
-print(result)
+def find_unique_numbers(numbers):
+    return [num for num in numbers if numbers.count(num) == 1]
