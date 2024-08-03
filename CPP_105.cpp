11d6ac5284b09ad6e4a85ff5cc2089f3a9305150
beep boop <githubbot@vadim.me>
2024-08-03 05:38:04 +0200

    Solution 13 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7ocphoaa Prompt:       |                                     ^~~~~~ c7f782c8-9e1f-4106-bc33-efa521d43015.cpp:43:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from c7f782c8-9e1f-4106-bc33-efa521d43015.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from c7f782c8-9e1f-4106-bc33-efa521d43015.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ c7f782c8-9e1f-4106-bc33-efa521d43015.cpp:43:44: error: ‘b’ was not declared in t his scope    43 | bool issame(vector<string> a,vector<string>b){       |                                            ^ c7f782c8-9e1f-4106-bc33-efa521d43015.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ c7f782c8-9e1f-4106-bc33-efa521d43015.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main(){       |     ^~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -30,7 +30,7 @@ std::vector<std::string> by_length(std::vector<int> arr){
 }
 
 int main(){
-    assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));
+    assert(issame(by_length({9, 4, 8}), {"Nine", "Eight", "Four"}));
     return 0;
 }
 
