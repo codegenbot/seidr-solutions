171054b34ed868ae10e61dc15fea58ab9570cc52
beep boop <githubbot@vadim.me>
2024-08-03 20:49:45 +0200

    Solution 14 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/sd81xmga Prompt:       |                   ^~~~~~~~~~~~ e78b6d62-bb58-4273-a197-c241d7207e3c.cpp: At global scope: e78b6d62-bb58-4273-a197-c241d7207e3c.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ e78b6d62-bb58-4273-a197-c241d7207e3c.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from e78b6d62-bb58-4273-a197-c241d7207e3c.cpp:20: e78b6d62-bb58-4273-a197-c241d7207e3c.cpp: In function ‘int main()’: e78b6d62-bb58-4273-a197-c241d7207e3c.cpp:22:28: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    22 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                            |       |                            <brace-enclosed initializer list> e78b6d62-bb58-4273-a197-c241d7207e3c.cpp:5:19: note:   initializing argument 1 o f ‘float max_element(int)’     5 | float max_element(const vector<float>& l) {       |                   ^~~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -1,9 +1,9 @@
 #include <vector>
+#include <cassert>
 #include <cmath>
-#include <initializer_list>
 
-float max_element(std::initializer_list<float> l){
-    float max = *(l.begin());
+float max_element(const std::vector<float>& l) {
+    float max = l[0];
     for (float num : l) {
         if (num > max) {
             max = num;
@@ -12,7 +12,7 @@ float max_element(std::initializer_list<float> l){
     return max;
 }
 
-int main2() {
-    assert(std::abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}) - 124) < 1e-4);
+int main() {
+    assert(std::abs(max_element(std::vector<float>{5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10}) - 124) < 1e-4);
     return 0;
 }
