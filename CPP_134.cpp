7f146b316e0bdc6d48e16be761d284959c76da16
beep boop <githubbot@vadim.me>
2024-08-03 03:57:32 +0200

    Solution 30 of CPP/134, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/w0xe5995 Prompt: f22cf109-8675-4bb4-8876-047681888b69.cpp:1:37: error: ‘string’ was not declared in this scope     1 | bool check_if_last_char_is_a_letter(string txt) {       |                                     ^~~~~~ In file included from f22cf109-8675-4bb4-8876-047681888b69.cpp:14: f22cf109-8675-4bb4-8876-047681888b69.cpp: In function ‘int main()’: f22cf109-8675-4bb4-8876-047681888b69.cpp:16:43: error: ‘check_if_last_char_is_a_ letter’ cannot be used as a function    16 |     assert (check_if_last_char_is_a_letter("apple pi e ") == false);       |             ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_134.cpp
+++ CPP_134.cpp
@@ -1,19 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <string>
 
 bool check_if_last_char_is_a_letter(string txt) {
     if(txt.empty()) return false;
-    string last_char = txt.substr(txt.length() - 1);
-    for(int i=0; i<txt.length(); i++) {
-        if(txt[i] == ' ' && i != (txt.length()-1)) {
-            string word = txt.substr(i+1, txt.length()-i-1);
-            if(last_char == word) return false;
+    char lastChar = txt.back();
+    bool isLastLetterPartOfWord = false;
+    for(int i = 0; i < txt.length() - 1; i++) {
+        if(!isspace(txt[i])) {
+            isLastLetterPartOfWord = true;
+            break;
         }
     }
-    return isalpha(txt[txt.length()-1]);
-}
-
-int main() {
-    assert (check_if_last_char_is_a_letter("apple pi e ") == false);
-    return 0;
+    return isalpha(lastChar) && !isLastLetterPartOfWord;
 }
