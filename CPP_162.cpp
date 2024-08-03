77588a3741f8114a9bddfbee78b0495891ec67c8
beep boop <githubbot@vadim.me>
2024-08-03 03:26:42 +0200

    Solution 20 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/pfe9otlx Prompt: aee0e27f-2f2e-49a9-b3ab-d34b1cb8c81a.cpp: In function ‘std::string string_to_md5 (const string&)’: aee0e27f-2f2e-49a9-b3ab-d34b1cb8c81a.cpp:12:8: warning: ‘unsigned char* MD5(cons t unsigned char*, size_t, unsigned char*)’ is deprecated: Since OpenSSL 3.0 [-Wd eprecated-declarations]    12 |     MD5((unsigned char*)text.c_str(), text.length(), digest);       |     ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ In file included from aee0e27f-2f2e-49a9-b3ab-d34b1cb8c81a.cpp:2: /usr/include/openssl/md5.h:52:38: note: declared here    52 | OSSL_DEPRECATEDIN_3_0 unsigned char *MD5(const unsigned char *d, size_t n,       |                                      ^~~ /usr/bin/ld: /tmp/ccPZ197k.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': aee0e27f-2f2e-49a9-b3ab-d34b1cb8c81a.cpp:(.text+0x97): undefined reference to `M D5' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,5 +1,8 @@
 #include <string>
 #include <openssl/md5.h>
+#include <openssl/evp.h>
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
 
 std::string string_to_md5(const std::string& text);
 
@@ -8,13 +11,26 @@ std::string string_to_md5(const std::string& text) {
         return "None";
     }
 
-    unsigned char digest[MD5_DIGEST_LENGTH];
-    MD5((unsigned char*)text.c_str(), text.length(), digest);
+    EVP_MD_CTX* mdctx;
+    const EVP_MD* md = EVP_md5();
+    mdctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestUpdate(mdctx, text.c_str(), text.length());
 
-    char mdString[33];
-    for(int i = 0; i < 16; i++) {
-        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
-    }
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
+    EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+    EVP_MD_CTX_free(mdctx);
 
-    return mdString;
+    BIO* bio = BIO_new(BIO_s_mem());
+    BIO_write(bio, md_value, md_len);
+
+    char* output;
+    BIO_get_mem_data(bio, &output);
+
+    std::string result(output, md_len * 2); // Convert binary data to hexadecimal string
+
+    BIO_free(bio);
+
+    return result;
 }
