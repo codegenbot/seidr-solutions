459cd7f5d89b9bf858c696aeace4e284914fdb73
beep boop <githubbot@vadim.me>
2024-08-03 14:22:29 +0200

    Solution 97 of CPP/159, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/afn7h4xe Prompt:       |                           ^~~~~~ 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp:17:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp:16: 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp: In function ‘int main()’: 649ebd53-c1f4-4c2b-906a-fa604fea1ca9.cpp:26:23: error: could not convert ‘eat(4,  5, 1)’ from ‘std::pair<int, int>’ to ‘std::vector<int>’    26 |     assert (issame(eat(4, 5, 1) , {5, 0}));       |                    ~~~^~~~~~~~~       |                       |       |                       std::pair<int, int>

--- CPP_159.cpp
+++ CPP_159.cpp
@@ -14,8 +14,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    std::vector<int> eat_result_1 = {eat(4, 5, 1).first, eat(4, 5, 1).second};
-    std::vector<int> expected_result = {5, 0};
-    assert(issame(eat_result_1, expected_result));
+    assert(issame({eat(4, 5, 1).first, eat(4, 5, 1).second}, {5, 0}));
     return 0;
 }
