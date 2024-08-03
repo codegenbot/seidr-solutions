02832ab92016d7aafc9a02ead5fe6ca52f03def1
beep boop <githubbot@vadim.me>
2024-08-03 08:15:53 +0200

    Solution 87 of CPP/26, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/biqx2z3b Prompt:  in this scope    12 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ e5aaf0e9-4736-42dd-a792-903e9d040e83.cpp:12:34: error: expected primary-expressi on before ‘int’    12 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ e5aaf0e9-4736-42dd-a792-903e9d040e83.cpp:12:39: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from e5aaf0e9-4736-42dd-a792-903e9d040e83.cpp:11: e5aaf0e9-4736-42dd-a792-903e9d040e83.cpp: In function ‘int main()’: e5aaf0e9-4736-42dd-a792-903e9d040e83.cpp:21:20: error: ‘remove_duplicates’ was n ot declared in this scope    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |                    ^~~~~~~~~~~~~~~~~ e5aaf0e9-4736-42dd-a792-903e9d040e83.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(remove_duplicates({1, 2, 3, 2, 4, 3, 5}) , {1, 4, 5}) );       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_26.cpp
+++ CPP_26.cpp
@@ -1,22 +1,15 @@
-```
 #include <vector>
 #include <algorithm>
 #include <functional>
-
-using namespace std;
 
 bool int_equal(int a, int b) {
     return a == b;
 }
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> remove_duplicates(vector<int> numbers) {
-    vector<int> result;
-    for (int num : numbers) {
-        if (find_if(result.begin(), result.end(), bind1st(ptr_fun(int_equal), num)) == result.end()) {
+std::vector<int> remove_duplicates(std::vector<int> numbers){
+    std::vector<int> result;
+    for(int num : numbers) {
+        if(find_if(result.begin(), result.end(), bind1st(ptr_fun(int_equal), num)) == result.end()) {
             result.push_back(num);
         }
     }
@@ -25,6 +18,6 @@ vector<int> remove_duplicates(vector<int> numbers) {
 
 int main() {
     vector<int> result = remove_duplicates({1, 2, 3, 2, 4, 3, 5});
-    assert(issame(vector<int>(result.begin(), result.end()), {1, 4, 5}));
+    assert (issame(vector<int>(result.begin(), result.end()), {1, 4, 5})
     return 0;
 }
