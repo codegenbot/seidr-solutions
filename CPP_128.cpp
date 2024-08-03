b02fca52ee3deb930e7087b2382c5dfded48afc6
beep boop <githubbot@vadim.me>
2024-08-03 03:33:33 +0200

    Solution 22 of CPP/128, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/k3axgv3j Prompt: f406412d-ec66-4476-9938-d4cc79500520.cpp:1:16: error: ‘vector’ was not declared in this scope     1 | int prod_signs(vector<int> arr) {       |                ^~~~~~ f406412d-ec66-4476-9938-d4cc79500520.cpp:1:23: error: expected primary-expressio n before ‘int’     1 | int prod_signs(vector<int> arr) {       |                       ^~~ In file included from f406412d-ec66-4476-9938-d4cc79500520.cpp:15: f406412d-ec66-4476-9938-d4cc79500520.cpp: In function ‘int main()’: f406412d-ec66-4476-9938-d4cc79500520.cpp:17:23: error: ‘prod_signs’ cannot be us ed as a function    17 |     assert (prod_signs({-1, 1, 1, 0}) == 0);       |             ~~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_128.cpp
+++ CPP_128.cpp
@@ -1,16 +1,15 @@
 #include <vector>
 
-int main() {
-    int prod_signs(std::vector<int> arr) {
-        int product = 1;
-        long sum = 0;
+int prod_signs(std::vector<int> arr) {
+    int product = 1;
+    long sum = 0;
 
-        if (arr.empty()) return -32768;
+    if (arr.empty()) return -32768;
 
-        for (int num : arr) {
-            product *= ((num > 0) ? 1 : ((num < 0) ? -1 : 0));
-            sum += abs(num);
-        }
-
-        return product * sum;
+    for (int num : arr) {
+        product *= ((num > 0) ? 1 : ((num < 0) ? -1 : 0));
+        sum += abs(num);
     }
+
+    return product * sum;
+}
