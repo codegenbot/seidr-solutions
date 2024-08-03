34d072516d56add0665beff05bc030d99e1468b0
beep boop <githubbot@vadim.me>
2024-08-03 13:59:48 +0200

    Solution 70 of Python/50, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/4abi9lyp Prompt: Program did not receive expected input

--- Python_50.py
+++ Python_50.py
@@ -1,4 +1,2 @@
-```python
 def decode_shift(s: str):
-    """Complete the following code given the task description and function signature.."""
-    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord("a")) for ch in s])
+    return "".join([chr(((ord(ch) - ord("a") + 5) % 26) + ord("a")) for ch in s if ch.isalpha()])
