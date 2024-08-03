f87282ca096957c3795c2a92ceef5a737401a018
beep boop <githubbot@vadim.me>
2024-08-04 01:09:43 +0200

    Solution 33 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/yt52uoai Prompt:                  from 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp:21: 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp: In function ‘int main()’: 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp:31:34: error: invalid initialization of  reference of type ‘const string&’ {aka ‘const std::__cxx11::basic_string<char>& ’} from expression of type ‘std::vector<std::__cxx11::basic_string<char> >’    31 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~ 4cebf39f-52ea-47f4-a444-16b395dbddfe.cpp:5:32: note: in passing argument 1 of ‘b ool issame(const string&, const string&)’     5 | bool issame(const std::string& a, const std::string& b) {       |             ~~~~~~~~~~~~~~~~~~~^

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -17,8 +17,3 @@ std::vector<std::string> reverse_delete(const std::string& s, const std::string&
     std::reverse(result_reverse.begin(), result_reverse.end());
     return {result, issame(result, result_reverse) ? "True" : "False"};
 }
-
-int main() {
-    assert (issame(reverse_delete("mamma", "mia")[0], "") && issame(reverse_delete("mamma", "mia")[1], "True"));
-    return 0;
-}
