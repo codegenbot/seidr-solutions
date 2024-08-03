8617c3240ae2149d50a6a920810b1b917c1d8115
beep boop <githubbot@vadim.me>
2024-08-03 14:45:33 +0200

    Solution 77 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/0e18qzqi Prompt: 8c6ab678-f4f2-416e-9f89-0c3b348b2c78.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -6,34 +6,39 @@ using namespace std;
 int bowlingScore(string s) {
     int score = 0;
     int frame = 1;
+    
     for (char c : s) {
         if (c == 'X') {
             score += 30;
             frame++;
         } else if (c == '/') {
-            string firstPart = s.substr(0, s.find('/'));
-            string secondPart = s.substr(s.find('/') + 1);
-            int leftPins = 10 - stoi(firstPart) - stoi(secondPart);
-            score += 10 + stoi(firstPart);
+            string leftSide = s.substr(0, s.find('/'));
+            string rightSide = s.substr(s.find('/'), 2);
+            
+            int totalPins = stoi(leftSide) + stoi(rightSide);
+            score += 10 - totalPins;
             frame++;
-            s.erase(0, s.find('/') + 1);
+            s.erase(0, s.find('/') + 2);
         } else {
             int pins = c - '0';
             score += pins;
             if (pins < 10) {
                 s.erase(0, 1);
+                
                 if (!s.empty() && s[0] == '/') {
-                    string firstPart = s.substr(0, s.find('/'));
-                    string secondPart = s.substr(s.find('/') + 1);
-                    int leftPins = 10 - stoi(firstPart) - stoi(secondPart);
-                    score += 10 + stoi(leftPins);
+                    string leftSide = s.substr(0, s.find('/'));
+                    string rightSide = s.substr(s.find('/'), 2);
+                    
+                    int totalPins = stoi(leftSide) + stoi(rightSide);
+                    score += 10 - totalPins;
                     frame++;
-                    s.erase(0, s.find('/') + 1);
+                    s.erase(0, s.find('/') + 2);
                 }
             } else {
                 frame++;
             }
         }
     }
+    
     return score;
 }
