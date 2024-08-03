e550bffec664ebdba91c74496f43f30ef6270188
beep boop <githubbot@vadim.me>
2024-08-03 10:56:30 +0200

    Solution 52 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/4rqn9jbw Prompt:    92 | #  define assert(expr) \       | 87d3af95-c02f-4634-8976-8a768d2687b9.cpp: In function ‘int main()’: 87d3af95-c02f-4634-8976-8a768d2687b9.cpp:22:5: error: ‘assert’ was not declared in this scope    22 |     assert(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) == vecto r<int>{-3, 1});       |     ^~~~~~ 87d3af95-c02f-4634-8976-8a768d2687b9.cpp:5:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     4 | #include <climits>   +++ |+#include <cassert>     5 | 87d3af95-c02f-4634-8976-8a768d2687b9.cpp: At global scope: 87d3af95-c02f-4634-8976-8a768d2687b9.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 87d3af95-c02f-4634-8976-8a768d2687b9.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main(){       |     ^~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,7 +1,6 @@
+#include <cassert>
 #include <iostream>
 #include <vector>
-#include <cassert>
-#include <climits>
 
 using namespace std;
 
@@ -16,11 +15,4 @@ vector<int> largest_smallest_integers(vector<int> lst){
         }
     }
     return {largest_negative, smallest_positive};
-}
-
-int main(){
-    assert(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) == vector<int>{-3, 1});
-    cout << "Test passed successfully!" << endl;
-
-    return 0;
 }
