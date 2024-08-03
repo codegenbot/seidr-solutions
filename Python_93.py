e047f48aaa58335df866da91d08811275feb60a0
beep boop <githubbot@vadim.me>
2024-08-03 16:00:54 +0200

    Solution 21 of Python/93, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/3ena54kg Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/01294f0e-4813-4e93-8b70-da40017f3 e0b.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_93.py
+++ Python_93.py
@@ -1,24 +1,23 @@
-```
 def encode(message):
     result = ""
     for char in message:
         if char.isalpha():
-            if char.lower() in 'aeiou':
-                if char.lower() == 'a':
-                    result += '1'
-                elif char.lower() == 'e':
-                    result += '3'
-                elif char.lower() == 'i':
-                    result += '!'
-                elif char.lower() == 'o':
-                    result += '0'
-                elif char.lower() == 'u':
-                    result += '\'\''
+            if char.lower() in "aeiou":
+                if char.lower() == "a":
+                    result += "1"
+                elif char.lower() == "e":
+                    result += "3"
+                elif char.lower() == "i":
+                    result += "!"
+                elif char.lower() == "o":
+                    result += "0"
+                elif char.lower() == "u":
+                    result += "_"
             else:
                 if char.islower():
-                    result += chr(ord(char) + 3).upper()
+                    result += chr(ord(char) + 3)
                 else:
-                    result += chr(ord(char) - 3).lower()
+                    result += chr(ord(char) - 3).upper()
         else:
             result += char
     return result
