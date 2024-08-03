7b8ec6a2bff373af2ba3cd5a8b2f485be7bff46a
beep boop <githubbot@vadim.me>
2024-08-03 07:53:08 +0200

    Solution 6 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/tyy4f4mg Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,40 +1,26 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
     int minDiff = INT_MAX;
-    int cutIndex = 0;
-    
-    for (int i = 1; i < v.size(); i++) {
-        int diff = abs(v[i] - v[0]);
-        if (diff <= minDiff) {
-            minDiff = diff;
-            cutIndex = i;
+    int splitIndex = 0;
+
+    for (int i = 1; i <= vec.size(); i++) {
+        int sumLeft = 0, sumRight = 0;
+        for (int j = 0; j < i; j++) {
+            sumLeft += vec[j];
+        }
+        for (int j = i; j < vec.size(); j++) {
+            sumRight += vec[j];
+        }
+
+        if (abs(sumLeft - sumRight) < minDiff) {
+            minDiff = abs(sumLeft - sumRight);
+            splitIndex = i;
         }
     }
-    
-    return {vector<int>(v.begin(), v.begin() + cutIndex), vector<int>(v.begin() + cutIndex, v.end())};
-}
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
-    
-    pair<vector<int>, vector<int>> result = cutVector(v);
-    cout << "First half: ";
-    for (int x : result.first) {
-        cout << x << " ";
-    }
-    cout << endl;
-    cout << "Second half: ";
-    for (int x : result.second) {
-        cout << x << " ";
-    }
-    cout << endl;
-    
-    return 0;
+    vector<int> leftVec(vec.begin(), vec.begin() + splitIndex);
+    vector<int> rightVec(vec.begin() + splitIndex, vec.end());
+    return {leftVec, rightVec};
 }
