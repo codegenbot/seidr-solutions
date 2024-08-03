afbf12da3778bbf34da408d14b0c349313bcbbd8
beep boop <githubbot@vadim.me>
2024-08-03 03:03:49 +0200

    Solution 5 of CPP/71, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/1m5t7qqc Prompt: aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp:1:1: error: expected unqualified-id bef ore ‘if’     1 | if(a + b > c && b + c > a && a + c > b){       | ^~ aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp:5:5: error: expected unqualified-id bef ore ‘return’     5 |     return -1;       |     ^~~~~~ aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp:6:1: error: expected declaration before  ‘}’ token     6 | }       | ^ In file included from aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp:8: aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp: In function ‘int main()’: aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp:10:17: error: ‘triangle_area’ was not d eclared in this scope    10 |     assert (abs(triangle_area(2, 2, 10) +1)<0.01);       |                 ^~~~~~~~~~~~~ aeb0f92a-29a9-4415-bd18-d7592ce0167b.cpp:10:13: error: ‘abs’ was not declared in  this scope    10 |     assert (abs(triangle_area(2, 2, 10) +1)<0.01);       |             ^~~

--- CPP_71.cpp
+++ CPP_71.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <cassert>
 
-float triangle_area(float a, float b, float c) {
+float triangle_area(float a, float b, float c){
     if(a + b > c && b + c > a && a + c > b){
         float s = (a + b + c) / 2;
         return roundf(sqrt(s * (s - a) * (s - b) * (s - c)) * 100) / 100;
@@ -9,7 +9,7 @@ float triangle_area(float a, float b, float c) {
     return -1;
 }
 
-int main() {
-    assert (abs(triangle_area(2, 2, 10) + 1) < 0.01);
+int main(){
+    assert(abs(triangle_area(2, 2, 10) + 1) < 0.01);
     return 0;
 }
