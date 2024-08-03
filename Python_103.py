6ed9952dfbd22aead1fada5f24749bb7694ec4f8
beep boop <githubbot@vadim.me>
2024-08-03 03:05:52 +0200

    Solution 16 of Python/103, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/w3tv4va7 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/70539d07-9d54-400a-b932-e1eb6f00a 4a1.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_103.py
+++ Python_103.py
@@ -1,5 +1,8 @@
-def rounded_avg(n, m):
+def rounded_avg():
+    n = int(input("Enter number of rounds: "))
+    m = int(input("Enter maximum score in each round: "))
     if n > m:
-        return -1
-    avg = (n + m) // 2
-    return str(round(avg))
+        print(-1)
+    else:
+        avg = (n + m) // 2
+        print(bin(avg)[2:])
