36f18ec6aa0ef45e03f111905cccee187f662e54
beep boop <githubbot@vadim.me>
2024-08-03 22:39:03 +0200

    Solution 34 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/d2oi3pna Prompt:       |                             ^~~~~~ 0ec678ab-60bd-427c-91eb-c22ddff6a5bf.cpp:23:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0ec678ab-60bd-427c-91eb-c22ddff6a5bf.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0ec678ab-60bd-427c-91eb-c22ddff6a5bf.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0ec678ab-60bd-427c-91eb-c22ddff6a5bf.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 0ec678ab-60bd-427c-91eb-c22ddff6a5bf.cpp:31:5: error: redefinition of ‘int main( )’    31 | int main(){       |     ^~~~ 0ec678ab-60bd-427c-91eb-c22ddff6a5bf.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main() {       |     ^~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -2,16 +2,19 @@
 #include <algorithm>
 #include <cassert>
 
-bool issame(const std::vector<float>& a, const std::vector<float>& b) {
+bool issame(const std::vector<float>& a, const std::vector<float>& b){
     return a.size() == b.size() && std::equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<float> derivative(const std::vector<float>& xs) {
+std::vector<float> derivative(const std::vector<float>& xs){
     std::vector<float> result;
-    
-    for (int i = 1; i < xs.size(); i++) {
+    for(size_t i = 1; i < xs.size(); i++){
         result.push_back(xs[i] * i);
     }
-    
     return result;
+}
+
+int main() {
+    assert(issame(derivative({1.0f}), {}));
+    return 0;
 }
