13aa1d4d9e4819b69224c557d230ea5a0c3cebd7
beep boop <githubbot@vadim.me>
2024-08-04 00:36:07 +0200

    Solution 2 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ofsecnl7 Prompt:       |                              ^~~~~~ 757077b1-a501-4518-8482-c57536e9f6d5.cpp:33:37: error: ‘string’ was not declared  in this scope    33 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 757077b1-a501-4518-8482-c57536e9f6d5.cpp:33:44: error: ‘b’ was not declared in t his scope    33 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 757077b1-a501-4518-8482-c57536e9f6d5.cpp:33:45: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 757077b1-a501-4518-8482-c57536e9f6d5.cpp:32: 757077b1-a501-4518-8482-c57536e9f6d5.cpp: In function ‘int main()’: 757077b1-a501-4518-8482-c57536e9f6d5.cpp:42:20: error: ‘by_length’ was not decla red in this scope    42 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ 757077b1-a501-4518-8482-c57536e9f6d5.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,6 +1,8 @@
+bool issame(vector<string> a, vector<string> b);
+
 vector<string> by_length(vector<int> arr){
     vector<string> result;
-    map<int, string> num_to_str = {
+    map<int, string> num_to_name = {
         {1, "One"},
         {2, "Two"},
         {3, "Three"},
@@ -12,19 +14,18 @@ vector<string> by_length(vector<int> arr){
         {9, "Nine"}
     };
 
-    vector<int> valid_nums;
+    vector<int> filtered;
     for (int num : arr) {
         if (num >= 1 && num <= 9) {
-            valid_nums.push_back(num);
+            filtered.push_back(num);
         }
     }
 
-    sort(valid_nums.begin(), valid_nums.end());
+    sort(filtered.begin(), filtered.end());
+    reverse(filtered.begin(), filtered.end());
 
-    reverse(valid_nums.begin(), valid_nums.end());
-
-    for (int num : valid_nums) {
-        result.push_back(num_to_str[num]);
+    for (int num : filtered) {
+        result.push_back(num_to_name[num]);
     }
 
     return result;
