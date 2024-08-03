27f93f95be3bf689ef4c5898980dc7e87b620f32
beep boop <githubbot@vadim.me>
2024-08-03 09:10:27 +0200

    Solution 6 of fizz-buzz, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/2yjn2r3s Prompt: Given an integer x, return "Fizz" if x is divisible by 3, "Buzz" if x is divisible by 5, "FizzBuzz" if xis divisible by 3 and 5, and a string version of x if none of the above hold.

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,28 +1,12 @@
-#include <string>
-using namespace std;
+Here is the solution:
 
-class Solution {
-public:
-    string numberToWords(int num) {
-        if (num == 0)
-            return "0";
-        
-        vector<string> units = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-        vector<string> teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        vector<string> tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-
-        if (num < 10) {
-            return units[num];
-        } else if (num < 20) {
-            return teens[num - 10];
-        } else if (num < 100) {
-            return tens[num / 10] + (units[num % 10] != "" ? " " + units[num % 10] : "");
-        } else if (num < 1000) {
-            return units[num / 100] + " Hundred" + ((num % 100) == 0 ? "" : " and " + numberToWords(num % 100));
-        } else if (num < 2000) {
-            return "One Thousand" + ((num == 1000) ? "" : " and " + numberToWords(num % 1000));
-        }
-        
-        return "";
-    }
-};
+int fizzBuzz(int x) {
+    if (x % 3 == 0 && x % 5 == 0)
+        return "FizzBuzz";
+    else if (x % 3 == 0)
+        return "Fizz";
+    else if (x % 5 == 0)
+        return "Buzz";
+    else
+        return to_string(x);
+}
