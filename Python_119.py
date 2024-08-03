9ea125ed3bec0543f5f298741fafd10dfac2cd00
beep boop <githubbot@vadim.me>
2024-08-03 17:22:21 +0200

    Solution 70 of Python/119, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/rh70ar40 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6220e9d1-e6e2-415a-a11d-59b8d1ed1 3a2.py", line 22, in <module>     check(match_parens)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/6220e9d1-e6e2-415a-a11d-59b8d1ed1 3a2.py", line 19, in check     assert match_parens([")", "("]) == "Yes"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_119.py
+++ Python_119.py
@@ -1,11 +1,15 @@
 def match_parens(lst):
     stack = []
     for s in lst:
-        for c in s:
-            if c == '(':
-                stack.append(c)
-            elif c == ')':
-                if not stack:
+        for char in s:
+            if char == '(':
+                stack.append(char)
+            elif char == ')':
+                if len(stack) > 0 and stack.pop() == '(':
+                    continue
+                else:
                     return 'No'
-                stack.pop()
-    return 'Yes' if not stack else 'No'
+    if len(stack) == 0:
+        return 'Yes'
+    else:
+        return 'No'
