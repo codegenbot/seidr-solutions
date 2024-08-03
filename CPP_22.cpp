128771921989616564449ff4613729a0b70234ef
beep boop <githubbot@vadim.me>
2024-08-03 08:00:48 +0200

    Solution 71 of CPP/22, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bwuu9phl Prompt: c8842346-d981-42e6-b9ef-f5569f68fc88.cpp:1:10: fatal error: boost/any_cast.hpp: No such file or directory     1 | #include <boost/any_cast.hpp>       |          ^~~~~~~~~~~~~~~~~~~~ compilation terminated.

--- CPP_22.cpp
+++ CPP_22.cpp
@@ -1,12 +1,11 @@
 #include <boost/any.hpp>
 
-vector<int> filter_integers(list<boost::any> values) {
+vector<int> filter_integers(list_any values) {
     vector<int> result;
     for (const auto& value : values) {
-        if (value.type() == typeid(int)) {
+        if (boost::any_cast<bool>(value)) {
             int i = boost::any_cast<int>(value);
             result.push_back(i);
         }
     }
     return result;
-}
