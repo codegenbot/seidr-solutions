2b77bb1e82d1b216d01ac1f4d20515db57fea472
beep boop <githubbot@vadim.me>
2024-08-03 10:48:33 +0200

    Solution 3 of CPP/100, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/54foxi1l Prompt: Complete the following code given the task description and function signature.

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -1,7 +1,17 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
 vector<int> make_a_pile(int n) {
-    vector<int> pile(n);
-    for (int i = 0; i < n; ++i) {
-        pile[i] = (n % 2 == 1 ? i*2+1 : i*2+2);
+    vector<int> pile;
+    int level = 1;
+    for (int i = 0; i < n; i++) {
+        if (level % 2 == 0) {
+            pile.push_back(level * 2);
+        } else {
+            pile.push_back(2 * level - 1);
+        }
+        level++;
     }
     return pile;
 }
