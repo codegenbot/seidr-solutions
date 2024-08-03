9def2298ab56fe5d8cc286706d1a0c507e5a3b62
beep boop <githubbot@vadim.me>
2024-08-03 14:50:42 +0200

    Solution 40 of Python/49, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/5swjn2j1 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d1fe8faa-6eae-4bac-9c6b-9356456db 575.py", line 7, in <module>     check(modp)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/d1fe8faa-6eae-4bac-9c6b-9356456db 575.py", line 4, in check     assert modp(31, 5) == 3            ^^^^^^^^^^^^^^^^ AssertionError

--- Python_49.py
+++ Python_49.py
@@ -1,7 +1,8 @@
-def modp(n: int, p: int):
-    if n == 1 or p == 2:
-        return n
-    if p % 2 == 0:
-        half_power = pow(n, (p // 2) - 1, p)
-        return ((half_power * half_power) % p) if p % 4 == 3 else modp(n, p % 2)
-    return pow(n, p-1, p)
+def modpow(n: int, p: int) -> int:
+    if p == 1:
+        return 0
+    result = pow(n % p, (p - 1) // 2, p)
+    if (p - 1) % 2 == 0:
+        return pow(result, p - 1, p)
+    else:
+        return n * (pow(result, (p - 1) // 2, p)) % p
