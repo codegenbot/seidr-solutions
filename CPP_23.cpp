fe0089ec972e3d16348cf695e2e59f317549d6d3
beep boop <githubbot@vadim.me>
2024-08-03 10:49:19 +0200

    Solution 13 of CPP/23, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/f1vhpezx Prompt: 3fc830ec-bbce-4229-a2f3-2197a0e46232.cpp:9:5: error: redefinition of ‘int main() ’     9 | int main(){       |     ^~~~ 3fc830ec-bbce-4229-a2f3-2197a0e46232.cpp:4:5: note: ‘int main()’ previously defi ned here     4 | int main(){       |     ^~~~

--- CPP_23.cpp
+++ CPP_23.cpp
@@ -1,14 +1,14 @@
 #include <cassert>
 
-int strlen(const char* str){
+int strlen(const char* str) {
     int length = 0;
-    while(*str != '\0'){
+    while (*str != '\0') {
         length++;
         str++;
     }
     return length;
 }
 
-int main(){
+int main() {
     assert(strlen("asdasnakj") == 9);
 }
