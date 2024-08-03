d6bd33f4bdf5b934d173adc4193960516085e593
beep boop <githubbot@vadim.me>
2024-08-03 02:33:35 +0200

    Solution 3 of Python/95, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/kiyizmvl Prompt: Complete the following code given the task description and function signature.

--- Python_95.py
+++ Python_95.py
@@ -1,9 +1,9 @@
-```
 def check_dict_case(dict):
     if len(dict) == 0:
         return False
-    for key in dict:
-        if isinstance(key, str):
-            if any(not k.islower() and not k.isupper() for k in dict):
-                return False
-    return True
+    keys = [key.upper() for key in dict.keys()]
+    all_upper = set(keys) == set(
+        [key[0].upper() + key[1:].lower() for key in dict.keys()]
+    )
+    all_lower = set(keys) == set([key.lower() for key in dict.keys()])
+    return all_upper or all_lower
