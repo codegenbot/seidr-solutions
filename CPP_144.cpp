b3a7263d80f31fe98d2b7dbde5f301dae1caf6b8
beep boop <githubbot@vadim.me>
2024-08-03 11:07:33 +0200

    Solution 12 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/frjlpwac Prompt: 7121cfb4-26b8-4e8a-a896-d5bfb7eaec28.cpp: In function ‘bool simplify(std::string , std::string)’: 7121cfb4-26b8-4e8a-a896-d5bfb7eaec28.cpp:15:18: error: ‘gcd’ cannot be used as a  function    15 |     int gcd = gcd(result_num, result_den);       |               ~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,2 +1,17 @@
 #include <algorithm>
-int gcd = __gcd(result_num, result_den);
+#include <string>
+#include <cmath>
+
+bool simplify(string x, string n) {
+    int num1 = stoi(x.substr(0, x.find('/')));
+    int den1 = stoi(x.substr(x.find('/') + 1));
+    int num2 = stoi(n.substr(0, n.find('/')));
+    int den2 = stoi(n.substr(n.find('/') + 1);
+
+    int result_num = num1 * num2;
+    int result_den = den1 * den2;
+
+    int gcd = __gcd(result_num, result_den);
+
+    return result_num % gcd == 0 && result_den % gcd == 0;
+}
