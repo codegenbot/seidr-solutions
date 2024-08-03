21e979833b35da0e1b72d340f3919ca2d7d24a80
beep boop <githubbot@vadim.me>
2024-08-03 23:10:35 +0200

    Solution 2 of CPP/74, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ci5x82df Prompt:       |                              ^~~~~~ 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp:20:37: error: ‘string’ was not declared  in this scope    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp:19: 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp: In function ‘int main()’: 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp:29:20: error: ‘total_match’ was not dec lared in this scope    29 |     assert (issame(total_match({"this"}, {}) , {}));       |                    ^~~~~~~~~~~ 1c5f18e8-f394-4683-b31c-dd1e6c540ac4.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(total_match({"this"}, {}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_74.cpp
+++ CPP_74.cpp
@@ -1,18 +1,20 @@
-int countTotalChars(const vector<string>& lst) {
-    int total = 0;
-    for (const string& str : lst) {
-        total += str.size();
-    }
-    return total;
-}
+#include <vector>
+#include <string>
 
 vector<string> total_match(vector<string> lst1, vector<string> lst2) {
-    int total1 = countTotalChars(lst1);
-    int total2 = countTotalChars(lst2);
+    int total_chars1 = 0, total_chars2 = 0;
 
-    if (total1 < total2) {
+    for (const string& str : lst1) {
+        total_chars1 += str.length();
+    }
+
+    for (const string& str : lst2) {
+        total_chars2 += str.length();
+    }
+
+    if (total_chars1 < total_chars2) {
         return lst1;
     } else {
-        return (total1 == total2) ? lst1 : lst2;
+        return lst2;
     }
 }
