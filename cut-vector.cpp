1a52e65f30b2190307bf7117200d903265dd61b6
beep boop <githubbot@vadim.me>
2024-08-03 07:42:13 +0200

    Solution 4 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/tyy4f4mg Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,28 +1,49 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
-    int min_diff = INT_MAX;
-    pair<vector<int>, vector<int>> res;
-    for (int i = 1; i < v.size(); ++i) {
-        int left_sum = 0, right_sum = 0;
-        for (int j = 0; j < i; ++j) {
-            left_sum += v[j];
-        }
-        for (int j = i; j < v.size(); ++j) {
-            right_sum += v[j];
-        }
-        int diff = abs(left_sum - right_sum);
-        if (diff < min_diff) {
-            min_diff = diff;
-            res = {{}, {}};
-            for (int j = 0; j < i; ++j) {
-                res.first.push_back(v[j]);
-            }
-            for (int j = i; j < v.size(); ++j) {
-                res.second.push_back(v[j]);
-            }
+vector<vector<int>> cutVector(vector<int> v) {
+    int n = v.size();
+    vector<vector<int>> res;
+    
+    for (int i = 1; i < n; ++i) {
+        if ((v[i] - v[0]) <= (n - i)*(v[n-1] - v[0])) {
+            res.push_back({v.begin(), v.end()});
+            return {{}, {v.begin(), v.end()}};
         }
     }
+    
+    vector<int> left(v.begin(), v.begin());
+    vector<int> right(v.begin() + 1, v.end());
+    res = {{left},{right}};
     return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &x : v) cin >> x;
+
+    cout << "[";
+    for (int i = 0; i < v.size(); ++i) {
+        if (i > 0) cout << ", ";
+        cout << v[i];
+    }
+    cout << "]\n";
+
+    vector<vector<int>> res = cutVector(v);
+    cout << "[";
+    for (auto &x : res[0]) {
+        if (res[0].size() > 1 || i++ > 0) cout << ", ";
+        cout << x;
+    }
+    cout << "], [";
+    i = 0;
+    for (auto &x : res[1]) {
+        if (res[1].size() > 1 || i++ > 0) cout << ", ";
+        cout << x;
+    }
+    cout << "]\n";
+
+    return 0;
 }
