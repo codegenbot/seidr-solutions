d0c93f7da84785d4a898ad275a086051e6f538d9
beep boop <githubbot@vadim.me>
2024-08-03 04:30:53 +0200

    Solution 97 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q2hs3fhq Prompt: e3d1a3fa-9d23-43ae-9bc6-e04167dc0c6e.cpp:3:1: error: stray ‘`’ in program     3 | ```cpp       | ^ e3d1a3fa-9d23-43ae-9bc6-e04167dc0c6e.cpp:3:2: error: stray ‘`’ in program     3 | ```cpp       |  ^ e3d1a3fa-9d23-43ae-9bc6-e04167dc0c6e.cpp:3:3: error: stray ‘`’ in program     3 | ```cpp       |   ^ e3d1a3fa-9d23-43ae-9bc6-e04167dc0c6e.cpp:4:10: fatal error: boost/any.hpp: No su ch file or directory     4 | #include <boost/any.hpp>       |          ^~~~~~~~~~~~~~~ compilation terminated.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,6 +1,6 @@
+```cpp
 #include <boost/any.hpp>
 #include <string>
-#include <iostream>
 
 using namespace boost;
 
@@ -11,52 +11,52 @@ boost::any compare_one(boost::any a, boost::any b) {
     else if (a.type() == typeid(double) && b.type() == typeid(int)) {
         return b;
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = any_cast<string>(a);
-        string str_b = any_cast<string>(b);
+    else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
+        std::string str_a = any_cast<std::string>(a);
+        std::string str_b = any_cast<std::string>(b);
 
         if (str_b.size() > 0 && str_b[0] == ',') {
             str_b = str_b.substr(1);
         }
 
-        if (stod(str_a) < stod(str_b)) {
+        if (std::stod(str_a) < std::stod(str_b)) {
             return b;
         }
-        else if (stod(str_a) > stod(str_b)) {
+        else if (std::stod(str_a) > std::stod(str_b)) {
             return a;
         }
         else {
             return boost::any("None");
         }
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        string str = any_cast<string>(a);
+    else if (a.type() == typeid(std::string) && b.type() == typeid(double)) {
+        std::string str = any_cast<std::string>(a);
 
         if (str.size() > 0 && str[0] == ',') {
             str = str.substr(1);
         }
 
-        if (stod(str) < b.convert_to<double>()) {
+        if (std::stod(str) < b.convert_to<double>()) {
             return b;
         }
-        else if (stod(str) > b.convert_to<double>()) {
+        else if (std::stod(str) > b.convert_to<double>()) {
             return a;
         }
         else {
             return boost::any("None");
         }
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        string str = any_cast<string>(b);
+    else if (a.type() == typeid(double) && b.type() == typeid(std::string)) {
+        std::string str = any_cast<std::string>(b);
 
         if (str.size() > 0 && str[0] == ',') {
             str = str.substr(1);
         }
 
-        if (stod(a.convert_to<string>()) < stod(str)) {
+        if (std::stod(a.convert_to<std::string>()) < std::stod(str)) {
             return a;
         }
-        else if (stod(a.convert_to<string>()) > stod(str)) {
+        else if (std::stod(a.convert_to<std::string>()) > std::stod(str)) {
             return b;
         }
         else {
