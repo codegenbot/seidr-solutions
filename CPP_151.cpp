5f9b01f52e7287e79fe8b9f3df227fabe7abc10f
beep boop <githubbot@vadim.me>
2024-08-03 12:19:29 +0200

    Solution 2 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/g91c1ttq Prompt: 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp:1:33: error: ‘vector’ was not declared in this scope     1 | long long double_the_difference(vector<float> lst){       |                                 ^~~~~~ 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp:1:40: error: expected primary-expressio n before ‘float’     1 | long long double_the_difference(vector<float> lst){       |                                        ^~~~~ In file included from 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp:11: 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp: In function ‘int main()’: 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp:13:35: error: ‘lst’ was not declared in  this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp:13:38: error: ‘double_the_difference’ c annot be used as a function    13 |     assert (double_the_difference(lst) == odd_sum );       |                                      ^ 1e941390-defa-44ba-a6df-bdab8c7f8d2d.cpp:13:43: error: ‘odd_sum’ was not declare d in this scope    13 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,9 +1,18 @@
-long long double_the_difference(vector<float> lst){
+#include <vector>
+#include <cmath>
+
+long long double_the_difference(std::vector<float> lst) {
     long long sum = 0;
     for (float num : lst) {
-        if (num > 0 && num == (int)num && (int)num % 2 != 0) {
-            sum += (long long)num * num;
+        if (num > 0 && std::fmod(num, 1) == 0 && (int)num % 2 != 0) {
+            sum += (long long)std::pow(num, 2);
         }
     }
     return sum;
+}
+
+int main() {
+    std::vector<float> lst = {1.5, 3.0, 4.5};
+    long long odd_sum = double_the_difference(lst);
+    return 0;
 }
