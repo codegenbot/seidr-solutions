c75638060906baf0613f20013364a82372d414b2
beep boop <githubbot@vadim.me>
2024-08-03 23:51:47 +0200

    Solution 34 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/zmpnpp42 Prompt: 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp:33:49: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp:32: 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp: In function ‘int main()’: 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp:45:27: error: invalid initialization of  reference of type ‘const std::vector<int>&’ from expression of type ‘std::vecto r<std::vector<int> >’    45 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |                    ~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~ 2c2f888e-2b4c-4758-8af1-07e7a56c9870.cpp:5:37: note: in passing argument 1 of ‘b ool issame(const std::vector<int>&, const std::vector<int>&)’     5 | bool issame(const std::vector<int>& a, const std::vector<int>& b){       |             ~~~~~~~~~~~~~~~~~~~~~~~~^

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <cassert>
 
-bool issame(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b){
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
 
@@ -25,6 +25,6 @@ std::vector<std::vector<int>> get_row(std::vector<std::vector<int>> lst, int x){
 }
 
 int main() {
-    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));
+    assert(issame(get_row({{}, {1}, {1, 2, 3}}, 3).front(), {2, 2}));
     return 0;
 }
