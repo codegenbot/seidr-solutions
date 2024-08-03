292776354c526f9d7b3b17f909112047fb4a5637
beep boop <githubbot@vadim.me>
2024-08-03 12:29:20 +0200

    Solution 10 of Python/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/n4syyp1w Prompt: b/python3.11/site-packages/programlib/programs/30b087f5-a849-453d-9d7c-bac221536 dc3.py", line 18, in <module>     check(sort_even)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30b087f5-a849-453d-9d7c-bac221536 dc3.py", line 13, in check     assert tuple(sort_even([5, 8, -12, 4, 23, 2, 3, 11, 12, -10])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30b087f5-a849-453d-9d7c-bac221536 dc3.py", line 2, in sort_even     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30b087f5-a849-453d-9d7c-bac221536 dc3.py", line 7, in <listcomp>     [y for y in l if (i // 2) * 2 <= j < ((i // 2) + 1) * 2 and j < len(l)]   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/30b087f5-a849-453d-9d7c-bac221536 dc3.py", line 7, in <listcomp>     [y for y in l if (i // 2) * 2 <= j < ((i // 2) + 1) * 2 and j < len(l)]                                      ^ NameError: name 'j' is not defined

--- Python_37.py
+++ Python_37.py
@@ -1,9 +1,5 @@
 def sort_even(l: list):
     return [
-        (
-            x
-            if i % 2 != 0
-            else sorted([y for y in l if (l.index(y) % 2 == 0 and l.index(y) < i)])[0]
-        )
+        x if i % 2 != 0 else min([x for x in l if i % 2 == 0])
         for i, x in enumerate(l)
     ]
