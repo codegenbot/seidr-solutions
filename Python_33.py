e63422002436870be94838c8b8276ef5eafde345
beep boop <githubbot@vadim.me>
2024-08-03 12:22:18 +0200

    Solution 29 of Python/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/ypw9t8jh Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48a5b0cb-ea8a-40aa-a4cc-5288f35e1 94e.py", line 12, in <module>     check(sort_third)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48a5b0cb-ea8a-40aa-a4cc-5288f35e1 94e.py", line 7, in check     assert tuple(sort_third([5, 6, 3, 4, 8, 9, 2, 1])) == tuple(                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48a5b0cb-ea8a-40aa-a4cc-5288f35e1 94e.py", line 2, in sort_third     return [            ^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/48a5b0cb-ea8a-40aa-a4cc-5288f35e1 94e.py", line 3, in <listcomp>     sorted([x for x in l[i : i + 3] if i % 3 != 0])[i % 3] if i % 3 == 0 else x     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^ IndexError: list index out of range

--- Python_33.py
+++ Python_33.py
@@ -1,6 +1,9 @@
-```
 def sort_third(l: list):
-    return (
-        (x if i >= len(l) - 1 or i % 3 == 0 else sorted([x for x in l[i : i + 3] if i % 3 != 0])[i % 3])
+    return [
+        (
+            x
+            if i >= len(l) - 1 or i % 3 == 0
+            else sorted([x for x in l[i : i + 3] if i % 3 != 0])[i % 3]
+        )
         for i, x in enumerate(l)
-    )
+    ]
