7d74e48b95f8ba8981cf696b7c6ff090cb620375
beep boop <githubbot@vadim.me>
2024-08-03 08:22:45 +0200

    Solution 1 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/okflsn3x Prompt: Complete the following code given the task description and function signature.

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,36 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 vector<int> sort_third(vector<int> l) {
     vector<int> result;
-    
     for (int i = 0; i < l.size(); i++) {
         if (i % 3 == 0) {
             vector<int> temp;
-            for (int j = 0; j < l.size(); j++) {
-                if (j % 3 == 0) {
-                    temp.push_back(l[j]);
-                }
+            for (int j = i; j < l.size() && j % 3 == 0; j++) {
+                temp.push_back(l[j]);
             }
             sort(temp.begin(), temp.end());
-            result.insert(result.end(), temp.begin(), temp.end());
+            int k = i;
+            for (int num : temp) {
+                result.push_back(num);
+                k++;
+            }
         } else {
             result.push_back(l[i]);
         }
     }
-    
     return result;
-}
-
-int main() {
-    vector<int> l = {5, 6, 3, 4, 8, 9, 2};
-    vector<int> result = sort_third(l);
-    for (int i : result) {
-        cout << i << " ";
-    }
-    cout << endl;
-    return 0;
 }
