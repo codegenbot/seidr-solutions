ca20bc04e630527a8def25d61cdacf9e1eeab5e8
beep boop <githubbot@vadim.me>
2024-08-03 13:36:18 +0200

    Solution 85 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q5n63q1c Prompt:     5 | d6609a39-45ac-4f4a-8189-19fb7ec11c7f.cpp:38:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    38 | int main(){       |         ^~ d6609a39-45ac-4f4a-8189-19fb7ec11c7f.cpp:38:9: note: remove parentheses to defau lt-initialize a variable    38 | int main(){       |         ^~       |         -- d6609a39-45ac-4f4a-8189-19fb7ec11c7f.cpp:38:9: note: or replace parentheses with  braces to value-initialize a variable d6609a39-45ac-4f4a-8189-19fb7ec11c7f.cpp:38:11: error: a function-definition is not allowed here before ‘{’ token    38 | int main(){       |           ^ d6609a39-45ac-4f4a-8189-19fb7ec11c7f.cpp:40:2: error: expected ‘}’ at end of inp ut    40 | }       |  ^ d6609a39-45ac-4f4a-8189-19fb7ec11c7f.cpp:17:12: note: to match this ‘{’    17 | int main() {       |            ^

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -17,11 +17,11 @@ bool issame(const std::vector<float>& a, const std::vector<float>& b) {
 int main() {
     std::vector<float> v = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
     std::vector<float> tmp = sort_even(v);
-    assert(issame(tmp, {-12, 8, 3, 4, 23, 2, 12, 11, 5, -10}));
+    assert(issame(tmp, {-12, 8, 3, 4, 23, 2, 12, 11, 5, -10});
 
     std::vector<float> input = {5, 8, -12, 4, 23, 2, 3, 11, 12, -10};
     input = sort_even(input);
-    assert(issame(input, {-12, 8, 3, 4, 23, 2, 12, 11, 5, -10}));
+    assert(issame(input, {-12, 8, 3, 4, 23, 2, 12, 11, 5, -10});
 
     return 0;
 }
