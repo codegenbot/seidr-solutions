742bbef233d26cc3fdbc955b74d2a2f41232f744
beep boop <githubbot@vadim.me>
2024-08-03 04:54:05 +0200

    Solution 0 of Python/136, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run3/runs/u7r7cndy Prompt: Complete the following code given the task description and function signature.

---
+++ Python_136.py
@@ -0,0 +1,5 @@
+def largest_smallest_integers(lst):
+    negative = [i for i in lst if i < 0]
+    positive = [i for i in lst if i > 0]
+
+    return (max(negative) if negative else None, min(positive) if positive else None)
