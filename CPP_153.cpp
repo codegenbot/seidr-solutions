cdbee4500b330f750c6a1fdf22ba43cc3d3bc2c6
beep boop <githubbot@vadim.me>
2024-08-03 02:32:53 +0200

    Solution 6 of CPP/153, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/8voolp3p Prompt: b93bee91-abdf-4668-9f71-e9d8eeebe615.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string Strongest_Extension(string class_name, vector<string> extensions) {       | ^~~~~~ In file included from b93bee91-abdf-4668-9f71-e9d8eeebe615.cpp:22: b93bee91-abdf-4668-9f71-e9d8eeebe615.cpp: In function ‘int main()’: b93bee91-abdf-4668-9f71-e9d8eeebe615.cpp:24:13: error: ‘Strongest_Extension’ was  not declared in this scope    24 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <climits>
 #include <vector>
 #include <string>
-#include <cctype>
+#include <cassert>
 
-using namespace std;
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions);
 
-string Strongest_Extension(string class_name, vector<string> extensions){
+std::string Strongest_Extension(std::string class_name, std::vector<std::string> extensions){
+    std::string strongest_extension = "";
     int max_strength = INT_MIN;
-    string strongest_extension = "";
     
-    for(const string& ext : extensions){
+    for(const std::string& ext : extensions){
         int CAP = 0, SM = 0;
         for(char c : ext){
-            if(isupper(c)){
-                CAP++;
-            } else if(islower(c)){
-                SM++;
-            }
+            if(std::isupper(c)) CAP++;
+            else if(std::islower(c)) SM++;
         }
         
         int strength = CAP - SM;
@@ -28,10 +24,4 @@ string Strongest_Extension(string class_name, vector<string> extensions){
     }
     
     return class_name + "." + strongest_extension;
-}
-
-int main(){
-    assert(Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");
-    
-    return 0;
 }
