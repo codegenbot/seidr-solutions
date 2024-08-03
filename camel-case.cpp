c03927037c7521244d2c26691730d0118972b09b
beep boop <githubbot@vadim.me>
2024-08-03 23:31:49 +0200

    Solution 44 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/rqkg0jfq Prompt: 6cdf6d87-3599-4776-9d9e-c26fb55fc608.cpp: In function ‘std::string kebabToCamel( const string&)’: 6cdf6d87-3599-4776-9d9e-c26fb55fc608.cpp:20:29: error: ‘class std::vector<std::_ _cxx11::basic_string<char> >’ has no member named ‘substr’    20 |             result += words.substr(1);       |                             ^~~~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -2,20 +2,32 @@ std::string kebabToCamel(const std::string& str) {
     size_t start = 0, end;
 
     while ((end = str.find(' ')) != std::string::npos) {
-        if (start != end) {
-            result += char(std::toupper(str[start]));
-            result += str.substr(start + 1, end - start - 1);
-        } else {
-            result = "Error";
-            break;
-        }
         start = end + 1;
     }
 
-    if (start < str.size()) {
-        result += char(std::toupper(str[start]));
-        result += str.substr(start + 1);
+    if (start > 0)
+        str[0] = toupper(str[0]);
+
+    for (; end != std::string::npos; ) {
+        end = str.find('-', start);
+        size_t pos = str.find(' ', end);
+        if (pos == std::string::npos) 
+            pos = str.length();
+        
+        if (end != std::string::npos)
+            str.replace(end, 1, toupper(str[edge++]));
+        result += str.substr(start, pos - start);
+
+        start = pos + 1;
     }
 
-    return result;
+    return str;
+}
+
+int main() {
+    std::string str;
+    while (std::cin >> str) {
+        std::cout << kebabToCamel(str) << std::endl;
+    }
+    return 0;
 }
