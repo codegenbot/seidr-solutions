2b024ceec25e050a1809ee4a2482e52f51a44d15
beep boop <githubbot@vadim.me>
2024-08-03 06:05:31 +0200

    Solution 75 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/0v270jal Prompt:       |             ^~~~~~ 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp:43:34: error: expected primary-expressi on before ‘int’    43 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp:43:39: error: expression list treated a s compound expression in initializer [-fpermissive]    43 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp:51:5: error: redefinition of ‘int main( )’    51 | int main(){       |     ^~~~ 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp:42: 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp: In function ‘int main()’: 8b40bdcc-2b92-4764-a99e-ff87f3edacc1.cpp:52:19: error: ‘issame’ cannot be used a s a function    52 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -2,9 +2,6 @@ bool are_vectors_equal(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-int main() {
-    // existing code
-    assert(are_vectors_equal(result, even_odd_palindrome(n)));
+...
 
-    return 0;
-}
+    assert(are_vectors_equal(result, even_odd_palindrome(n)));
