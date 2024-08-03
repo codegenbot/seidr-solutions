a205be3dfb91e3999676593ac04f1d10ea5600b8
beep boop <githubbot@vadim.me>
2024-08-03 15:20:50 +0200

    Solution 8 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/niyd2aok Prompt:       |                    ^~~ ffffedee-a98a-40a1-90f9-91f95a382539.cpp:22:27: error: ‘vector’ was not declared  in this scope    22 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ ffffedee-a98a-40a1-90f9-91f95a382539.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ ffffedee-a98a-40a1-90f9-91f95a382539.cpp:22:39: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from ffffedee-a98a-40a1-90f9-91f95a382539.cpp:21: ffffedee-a98a-40a1-90f9-91f95a382539.cpp: In function ‘int main()’: ffffedee-a98a-40a1-90f9-91f95a382539.cpp:31:20: error: ‘unique_digits’ was not d eclared in this scope    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ ffffedee-a98a-40a1-90f9-91f95a382539.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<int> a, std::vector<int> b){
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-std::vector<int> unique_digits(std::vector<int> x){
-    std::vector<int> result;
+vector<int> unique_digits(vector<int> x);
+
+vector<int> unique_digits(vector<int> x){
+    vector<int> result;
     for (int num : x) {
         bool hasEvenDigit = false;
         int temp = num;
@@ -21,11 +23,6 @@ std::vector<int> unique_digits(std::vector<int> x){
             result.push_back(num);
         }
     }
-    std::sort(result.begin(), result.end());
+    sort(result.begin(), result.end());
     return result;
-}
-
-int main() {
-    assert(issame(unique_digits({135, 103, 31}), {31, 135}));
-    return 0;
 }
