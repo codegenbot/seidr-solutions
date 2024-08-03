6330c6da8d9dc895f9df6d6b51c07e1f1c23f56e
beep boop <githubbot@vadim.me>
2024-08-03 17:02:32 +0200

    Solution 56 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q12dp2zg Prompt:       |                                     ^~~~~~ 23ce02e1-1a33-40db-aef5-4754acafac53.cpp:28:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 23ce02e1-1a33-40db-aef5-4754acafac53.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 23ce02e1-1a33-40db-aef5-4754acafac53.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 23ce02e1-1a33-40db-aef5-4754acafac53.cpp:28:44: error: ‘b’ was not declared in t his scope    28 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 23ce02e1-1a33-40db-aef5-4754acafac53.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 23ce02e1-1a33-40db-aef5-4754acafac53.cpp:22:5: note: ‘int main()’ previously def ined here    22 | int main() {       |     ^~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -18,8 +18,3 @@ std::vector<std::string> reverse_delete(std::string s, std::string c) {
     std::reverse(reverse_result.begin(), reverse_result.end());
     return {result, result == reverse_result ? "True" : "False"};
 }
-
-int main() {
-    assert(issame(reverse_delete("mamma", "mia"), {"", "True"}));
-    return 0;
-}
