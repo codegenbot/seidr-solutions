09453a38eb8039480b374b0b85de2d478a865d07
beep boop <githubbot@vadim.me>
2024-08-03 06:03:51 +0200

    Solution 9 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ql5jwuyw Prompt:       |                              ^~~~~~ 124d265c-00bc-4d99-bb31-33cac246abfd.cpp:13:37: error: ‘string’ was not declared  in this scope    13 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 124d265c-00bc-4d99-bb31-33cac246abfd.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 124d265c-00bc-4d99-bb31-33cac246abfd.cpp:13:45: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 124d265c-00bc-4d99-bb31-33cac246abfd.cpp:12: 124d265c-00bc-4d99-bb31-33cac246abfd.cpp: In function ‘int main()’: 124d265c-00bc-4d99-bb31-33cac246abfd.cpp:22:20: error: ‘reverse_delete’ was not declared in this scope    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 124d265c-00bc-4d99-bb31-33cac246abfd.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,13 +1,13 @@
 #include <string>
 
 bool issame(vector<string> a, vector<string> b) {
-    string result = "";
-    for (char ch : s) {
-        if (c.find(ch) == string::npos) {
-            result += ch;
-        }
+  string result = "";
+  for (char ch : s) {
+    if (c.find(ch) == string::npos) {
+      result += ch;
     }
-    string reversed = result;
-    reverse(reversed.begin(), reversed.end());
-    return {result, result == reversed ? "True" : "False"};
+  }
+  string reversed = result;
+  reverse(reversed.begin(), reversed.end());
+  return {result, result == reversed ? "True" : "False"};
 }
