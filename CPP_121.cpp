90224605f70b21e1ce3c9e7aa6f8d3ee478c113f
beep boop <githubbot@vadim.me>
2024-08-03 11:37:51 +0200

    Solution 44 of CPP/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/gd33zdnw Prompt: 48a99539-d0c3-4fd6-a9f5-f936892d5348.cpp:1:15: error: ‘vector’ was not declared in this scope     1 | int solutions(vector<int> lst) {       |               ^~~~~~ 48a99539-d0c3-4fd6-a9f5-f936892d5348.cpp:1:22: error: expected primary-expressio n before ‘int’     1 | int solutions(vector<int> lst) {       |                      ^~~ In file included from 48a99539-d0c3-4fd6-a9f5-f936892d5348.cpp:9: 48a99539-d0c3-4fd6-a9f5-f936892d5348.cpp: In function ‘int main()’: 48a99539-d0c3-4fd6-a9f5-f936892d5348.cpp:11:22: error: ‘solutions’ cannot be use d as a function    11 |     assert (solutions({3, 13, 2, 9}) == 3);       |             ~~~~~~~~~^~~~~~~~~~~~~~~

--- CPP_121.cpp
+++ CPP_121.cpp
@@ -11,6 +11,5 @@ int solutions(std::vector<int> lst) {
 
 int main() {
     assert(solutions({3, 13, 2, 9}) == 16);
-    std::cout << solutions({1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49}) << std::endl;
-    return 0;
+    // Your code goes here
 }
