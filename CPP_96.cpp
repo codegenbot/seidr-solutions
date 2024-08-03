4c746260683e526b2169d81fc974fdd281a61268
beep boop <githubbot@vadim.me>
2024-08-04 00:22:48 +0200

    Solution 57 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt: afbee487-1103-4011-b636-a3591bb35f77.cpp:10:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    10 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ afbee487-1103-4011-b636-a3591bb35f77.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ In file included from afbee487-1103-4011-b636-a3591bb35f77.cpp:9: afbee487-1103-4011-b636-a3591bb35f77.cpp: In function ‘int main()’: afbee487-1103-4011-b636-a3591bb35f77.cpp:19:20: error: ‘count_up_to’ was not dec lared in this scope    19 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,45 +1,48 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b){
-    if(a.size() != b.size()){
-        return false;
-    }
-
-    for(size_t i = 0; i < a.size(); ++i){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-
-    return true;
-}
-
-std::vector<int> count_up_to(int n){
-    std::vector<int> primes;
-
-    for(int num = 2; num <= n; ++num){
+vector<int> count_up_to(int n){
+    vector<int> primes;
+    for(int i=2; i<=n; i++){
         bool is_prime = true;
-        for(int i = 2; i <= num/2; ++i){
-            if(num % i == 0){
+        for(int j=2; j*j<=i; j++){
+            if(i % j == 0){
                 is_prime = false;
                 break;
             }
         }
         if(is_prime){
-            primes.push_back(num);
+            primes.push_back(i);
         }
     }
-
     return primes;
 }
 
-int main(){
-    std::vector<int> a = {2, 3, 5, 7, 11};
-    std::vector<int> b = count_up_to(12);
-
-    if(issame(a, b)){
-        return 0;
-    } else{
-        return 1;
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()){
+        return false;
     }
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    vector<int> vec1 = {2, 3, 5, 7, 11};
+    vector<int> vec2 = {2, 3, 5, 7, 11};
+    
+    if(issame(vec1, vec2)){
+        cout << "Vectors are the same." << endl;
+    } else {
+        cout << "Vectors are different." << endl;
+    }
+    
+    return 0;
 }
