4d07347b97e7cce99566df6930be731835ccf5f5
beep boop <githubbot@vadim.me>
2024-08-03 16:22:37 +0200

    Solution 2 of CPP/112, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/q12dp2zg Prompt:       |                              ^~~~~~ 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp:14:37: error: ‘string’ was not declared  in this scope    14 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp:14:44: error: ‘b’ was not declared in t his scope    14 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp:14:45: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp:13: 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp: In function ‘int main()’: 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp:23:20: error: ‘reverse_delete’ was not declared in this scope    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |                    ^~~~~~~~~~~~~~ 07fc3b09-bdbc-44d3-b30e-e9d729e6d13b.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(reverse_delete("mamma", "mia") , {"", "True"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_112.cpp
+++ CPP_112.cpp
@@ -1,11 +1,17 @@
+#include <string>
+
+bool issame(vector<string> a, vector<string> b) {
+    return a[0].compare(b[0]) == 0 && a[1].compare(b[1]) == 0;
+}
+
 vector<string> reverse_delete(string s, string c) {
-        string result = "";
-        for (char ch : s) {
-            if (c.find(ch) == string::npos) {
-                result += ch;
-            }
+    string result = "";
+    for (char ch : s) {
+        if (c.find(ch) == string::npos) {
+            result += ch;
         }
-        string reverse_result = result;
-        reverse(reverse_result.begin(), reverse_result.end());
-        return {result, result == reverse_result ? "True" : "False"};
     }
+    string reverse_result = result;
+    reverse(reverse_result.begin(), reverse_result.end());
+    return {result, result == reverse_result ? "True" : "False"};
+}
