5664edb41878d77b604d60e9b352f6d22962c379
beep boop <githubbot@vadim.me>
2024-08-03 10:10:13 +0200

    Solution 3 of CPP/84, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/bogmg6mj Prompt: Complete the following code given the task description and function signature.

--- CPP_84.cpp
+++ CPP_84.cpp
@@ -1,8 +1,12 @@
+#include <string>
+
+using namespace std;
+
 string solve(int N) {
     int sum = 0;
     while (N > 0) {
-        sum += N & 1;
-        N >>= 1;
+        sum += N & 1; // add last bit of N to sum
+        N >>= 1; // right shift N by 1
     }
     return to_string(sum);
 }
