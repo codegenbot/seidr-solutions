d3e4d533124d741c3fcf0f1f30984223a7654a62
beep boop <githubbot@vadim.me>
2024-08-03 08:22:47 +0200

    Solution 50 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/93tkvikn Prompt:       |                    ^~~~~ 21805bb3-b1c4-40fb-91af-844654fc541f.cpp:11:29: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 21805bb3-b1c4-40fb-91af-844654fc541f.cpp:11:36: error: expected primary-expressi on before ‘float’    11 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 21805bb3-b1c4-40fb-91af-844654fc541f.cpp:11:43: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 21805bb3-b1c4-40fb-91af-844654fc541f.cpp:10: 21805bb3-b1c4-40fb-91af-844654fc541f.cpp: In function ‘int main()’: 21805bb3-b1c4-40fb-91af-844654fc541f.cpp:20:20: error: ‘get_positive’ was not de clared in this scope    20 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 21805bb3-b1c4-40fb-91af-844654fc541f.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,21 +1,14 @@
 #include <vector>
-#include <iostream>
-#include <assert.h>
 
 bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
+    return a.size() == b.size() && a.size() > 0 ? a[0] == b[0] : false;
 }
 
-std::vector<float> get_positive(std::vector<float> l) {
+std::vector<float> get_positive(std::vector<float> l){
     std::vector<float> result;
-    for (float num : l) {
-        if (num > 0)
+    for(float num : l){
+        if(num > 0)
             result.push_back(num);
     }
     return result;
-}
-
-int main() {
-    assert(get_positive({}) == {});
-    return 0;
 }
