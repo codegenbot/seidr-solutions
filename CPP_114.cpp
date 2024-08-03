a176b8e2b0374cfc3b50d6a06c2fadb01b3db9a9
beep boop <githubbot@vadim.me>
2024-08-03 11:17:54 +0200

    Solution 36 of CPP/114, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/27hi3fmw Prompt: 7e1f5a10-9744-4a9b-a9d7-2756e938a965.cpp:1:26: error: ‘vector’ was not declared in this scope     1 | long long minSubArraySum(vector<long long> nums) {       |                          ^~~~~~ 7e1f5a10-9744-4a9b-a9d7-2756e938a965.cpp:1:33: error: expected primary-expressio n before ‘long’     1 | long long minSubArraySum(vector<long long> nums) {       |                                 ^~~~ In file included from 7e1f5a10-9744-4a9b-a9d7-2756e938a965.cpp:13: 7e1f5a10-9744-4a9b-a9d7-2756e938a965.cpp: In function ‘int main()’: 7e1f5a10-9744-4a9b-a9d7-2756e938a965.cpp:15:27: error: ‘minSubArraySum’ cannot b e used as a function    15 |     assert (minSubArraySum({1, -1}) == -1);       |             ~~~~~~~~~~~~~~^~~~~~~~~

--- CPP_114.cpp
+++ CPP_114.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int minSubArraySum(vector<long long> nums) {
-    long long min_sum = LLONG_MAX;
+    int min_sum = INT_MAX;
     for (int i = 0; i < nums.size(); i++) {
-        long long sum = 0;
+        int sum = 0;
         for (int j = i; j < nums.size(); j++) {
             sum += nums[j];
             min_sum = min(min_sum, sum);
