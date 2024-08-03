c06b04440e0274b4e66cd7909fe43951e3014a5a
beep boop <githubbot@vadim.me>
2024-08-03 09:49:50 +0200

    Solution 45 of CPP/9, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/cpwxjttl Prompt:       |                           ^~~~~~ 0e452ea3-2634-41e2-91d2-b4bd52dd47bd.cpp:28:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 0e452ea3-2634-41e2-91d2-b4bd52dd47bd.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 0e452ea3-2634-41e2-91d2-b4bd52dd47bd.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 0e452ea3-2634-41e2-91d2-b4bd52dd47bd.cpp:28:34: error: expected primary-expressi on before ‘int’    28 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 0e452ea3-2634-41e2-91d2-b4bd52dd47bd.cpp:36:5: error: redefinition of ‘int main( )’    36 | int main(){       |     ^~~~ 0e452ea3-2634-41e2-91d2-b4bd52dd47bd.cpp:21:5: note: ‘int main()’ previously def ined here    21 | int main() {       |     ^~~~

--- CPP_9.cpp
+++ CPP_9.cpp
@@ -17,9 +17,3 @@ std::vector<int> rolling_max(std::vector<int> numbers) {
 bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
-
-int main() {
-    assert(issame(rolling_max({3, 2, 3, 100, 3}), {3, 3, 3, 100, 100}));
-    std::cout << "Test Passed!";
-    return 0;
-}
