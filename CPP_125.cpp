f57ce1541b46918fdfe5208bc48c2f0697c008c7
beep boop <githubbot@vadim.me>
2024-08-04 01:50:39 +0200

    Solution 41 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/c2i85h8a Prompt:       |           ^~~~~~ 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp:55:44: error: ‘b’ was not declared in t his scope    55 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp:55:45: error: expression list treated a s compound expression in initializer [-fpermissive]    55 | bool issame(vector<string> a,vector<string>b){       |                                             ^ 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp:63:5: error: redefinition of ‘int main( )’    63 | int main(){       |     ^~~~ 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp:10:5: note: ‘int main()’ previously def ined here    10 | int main() {       |     ^~~~ In file included from 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp:54: 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp: In function ‘int main()’: 76860491-c016-41eb-a3fd-ad42bf2f6b75.cpp:64:19: error: ‘issame’ cannot be used a s a function    64 |     assert (issame(split_words("") ,{"0"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -7,12 +7,6 @@ bool assert_equal(const std::vector<std::string>& a, const std::vector<std::stri
 
 std::vector<std::string> split_words(const std::string& txt);
 
-int main() {
-    assert(assert_equal(split_words(""), std::vector<std::string>{"0"}));
-    
-    return 0;
-}
-
 bool assert_equal(const std::vector<std::string>& a, const std::vector<std::string>& b) {
     return a == b;
 }
