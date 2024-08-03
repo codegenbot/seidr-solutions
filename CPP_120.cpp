3c2f5ec70b00b65a524b643a058c6e0c29e46218
beep boop <githubbot@vadim.me>
2024-08-03 06:47:11 +0200

    Solution 12 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/kntqy2at Prompt:  in this scope    10 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp:10:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp:10:34: error: expected primary-expressi on before ‘int’    10 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp:9: 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp: In function ‘int main()’: 10e80cb7-d73a-449c-9c72-8b87f684b734.cpp:19:20: error: ‘maximum’ was not declare d in this scope    19 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -7,6 +7,6 @@ bool issame(std::vector<int> a, std::vector<int> b){
 }
 
 int main() {
-    assert(issame(std::vector<int>{1, 2, 3, -23, 243, -400, 0}, std::vector<int>{0}));
+    assert(issame(std::vector<int>({1, 2, 3, -23, 243, -400, 0}), std::vector<int>{}));
     return 0;
 }
