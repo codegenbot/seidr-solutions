44de3ec9d7b16dd0b10ec112962b6e6e4634de82
beep boop <githubbot@vadim.me>
2024-08-03 11:24:41 +0200

    Solution 18 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ele97v8e Prompt:       |                           ^~~~~~ 90c611d6-b99d-45e2-8fae-a2f7b1dee6c2.cpp:39:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 90c611d6-b99d-45e2-8fae-a2f7b1dee6c2.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 90c611d6-b99d-45e2-8fae-a2f7b1dee6c2.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 90c611d6-b99d-45e2-8fae-a2f7b1dee6c2.cpp:39:34: error: expected primary-expressi on before ‘int’    39 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 90c611d6-b99d-45e2-8fae-a2f7b1dee6c2.cpp:47:5: error: redefinition of ‘int main( )’    47 | int main(){       |     ^~~~ 90c611d6-b99d-45e2-8fae-a2f7b1dee6c2.cpp:33:5: note: ‘int main()’ previously def ined here    33 | int main(){       |     ^~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -7,20 +7,19 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
 
-std::vector<int> order_by_points(const std::vector<int>& nums) {
+std::vector<int> order_by_points(const std::vector<int>& nums){
     std::vector<int> sorted_nums = nums;
-    std::sort(sorted_nums.begin(), sorted_nums.end(), [](int a, int b) {
+    std::sort(sorted_nums.begin(), sorted_nums.end(), [](int a, int b){
         int sum_a = 0, sum_b = 0;
-        int temp_a = a, temp_b = b;
-        if (temp_a < 0) temp_a *= -1;
-        if (temp_b < 0) temp_b *= -1;
-        while (temp_a > 0) {
-            sum_a += temp_a % 10;
-            temp_a /= 10;
+        a = (a < 0) ? -a : a;
+        b = (b < 0) ? -b : b;
+        while (a > 0) {
+            sum_a += a % 10;
+            a /= 10;
         }
-        while (temp_b > 0) {
-            sum_b += temp_b % 10;
-            temp_b /= 10;
+        while (b > 0) {
+            sum_b += b % 10;
+            b /= 10;
         }
         if (sum_a == sum_b) {
             return a < b;
@@ -31,7 +30,7 @@ std::vector<int> order_by_points(const std::vector<int>& nums) {
     return sorted_nums;
 }
 
-int main() {
-    assert(std::issame(order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+int main(){
+    assert(issame(order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
