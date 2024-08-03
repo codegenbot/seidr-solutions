8fdaf8089fd200001c2b853cdc11b7cbcc433004
beep boop <githubbot@vadim.me>
2024-08-03 10:57:57 +0200

    Solution 17 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/uak44l3y Prompt:       |                              ^~~~~~ cd99610a-b85a-4b7e-a05a-58b235660e24.cpp:20:37: error: ‘string’ was not declared  in this scope    20 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ cd99610a-b85a-4b7e-a05a-58b235660e24.cpp:20:44: error: ‘b’ was not declared in t his scope    20 | bool issame(vector<string> a,vector<string>b){       |                                            ^ cd99610a-b85a-4b7e-a05a-58b235660e24.cpp:20:45: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from cd99610a-b85a-4b7e-a05a-58b235660e24.cpp:19: cd99610a-b85a-4b7e-a05a-58b235660e24.cpp: In function ‘int main()’: cd99610a-b85a-4b7e-a05a-58b235660e24.cpp:29:20: error: ‘by_length’ was not decla red in this scope    29 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ cd99610a-b85a-4b7e-a05a-58b235660e24.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -7,24 +7,19 @@ bool issame(vector<string> a, vector<string> b) {
     return a == b;
 }
 
-vector<string> by_length(vector<int> arr) {
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<std::string> result;
     map<int, string> numberNames = {{1, "One"}, {2, "Two"}, {3, "Three"},
                                      {4, "Four"}, {5, "Five"}, {6, "Six"},
                                      {7, "Seven"}, {8, "Eight"}, {9, "Nine"}};
-    vector<string> result;
-    
     for (int num : arr) {
-        if (num >= 1 && num <= 9)
-            result.push_back(numberNames[num]);
+        result.push_back(numberNames[num]);
     }
-    
     sort(result.begin(), result.end(),
          [](const string& a, const string& b) {
-             return a.size() < b.size();
+             return a.length() < b.length();
          });
-    
     reverse(result.begin(), result.end());
-    
     return result;
 }
 
