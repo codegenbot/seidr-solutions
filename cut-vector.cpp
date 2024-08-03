860678de737459e3c19ac7b178f9372e576c5771
beep boop <githubbot@vadim.me>
2024-08-03 08:56:13 +0200

    Solution 17 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/tyy4f4mg Prompt:   +++ |+#include <iostream>     2 | using namespace std; 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:29:13: error: ‘cout’ was not declared i n this scope    29 |             cout << x << " ";       |             ^~~~ 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:29:13: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << endl;       |         ^~~~ 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:31:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:31:17: error: ‘endl’ was not declared i n this scope    31 |         cout << endl;       |                 ^~~~ 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -6,13 +6,15 @@ vector<vector<int>> cutVector(vector<int> vec) {
     int n = vec.size();
     vector<vector<int>> result;
     
-    for(int i = 0; i < n; i++) {
-        if(i == n - 1 || vec[i] != vec[i+1]) {
-            vector<int> left = vector<int>(vec.begin(), vec.begin() + i);
-            vector<int> right = vector<int>(vec.begin() + i, vec.end());
-            result.push_back({left, right});
+    for(int i = 0; i < n - 1; i++) {
+        if(abs(vec[i] - vec[i+1]) > (vec[i] - vec[0])) {
+            result.push_back({vector<int>(vec.begin(), vec.begin() + i), vector<int>(vec.begin() + i, vec.end())});
             break;
         }
+    }
+    
+    if(n > 0) {
+        result.push_back({{vec[0]}, {vec}});
     }
     
     return result;
@@ -28,14 +30,8 @@ int main() {
     
     vector<vector<int>> res = cutVector(vec);
     for(auto v : res) {
-        cout << "Left: ";
-        for(auto x : v[0]) {
+        for(auto x : v) {
             cout << x << " ";
-        }
-        cout << endl;
-        cout << "Right: ";
-        for(auto y : v[1]) {
-            cout << y << " ";
         }
         cout << endl;
     }
