bf9ac45a2a3b74f8e9110786e32e827074c037d8
beep boop <githubbot@vadim.me>
2024-08-03 18:55:39 +0200

    Solution 3 of Python/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/514esuav Prompt: Complete the following code given the task description and function signature.

--- Python_148.py
+++ Python_148.py
@@ -9,11 +9,13 @@ def bf(planet1, planet2):
         "Uranus",
         "Neptune",
     ]
-    index_planet1 = planets.index(planet1)
-    index_planet2 = planets.index(planet2)
+    index1 = planets.index(planet1)
+    index2 = planets.index(planet2)
 
-    if planet1 not in planets or planet2 not in planets:
+    if index1 < 0 or index2 < 0:
         return ()
 
-    result = tuple(planets[index_planet1 + 1 : index_planet2])
-    return result
+    if index1 > index2:
+        index1, index2 = index2, index1
+
+    return tuple(planets[index1 + 1 : index2])
