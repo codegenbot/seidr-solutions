97b78e0e9d73201f03bf33e6d1e9db4f9e43db40
beep boop <githubbot@vadim.me>
2024-08-03 10:37:50 +0200

    Solution 29 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/tyy4f4mg Prompt:   +++ |+#include <iostream>     2 | using namespace std; 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:29:13: error: ‘cout’ was not declared i n this scope    29 |             cout << x << " ";       |             ^~~~ 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:29:13: note: ‘std::cout’ is defined in header ‘<iostream>’; did you forget to ‘#include <iostream>’? 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << endl;       |         ^~~~ 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:31:9: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:31:17: error: ‘endl’ was not declared i n this scope    31 |         cout << endl;       |                 ^~~~ 5bd96b18-7f26-4d1d-be50-212d70a9c3be.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <vector>   +++ |+#include <ostream>     2 | using namespace std;

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -6,15 +6,15 @@ vector<vector<int>> cutVector(vector<int> vec) {
     int n = vec.size();
     vector<vector<int>> result;
     
-    for(int i = 0; i < n-1; i++) {
-        if(abs(vec[i] - vec[i+1]) > abs(vec[0] - vec.back())) {
-            result.push_back({vector<int>(vec.begin(), vec.begin() + i+1)});
+    for(int i = 0; i < n; i++) {
+        if(i == n - 1 || vec[i] != vec[i+1]) {
+            vector<int> left(vec, 0, i);
+            vector<int> right(vec, i, n);
+            result.push_back({left});
+            result.push_back({right});
             break;
         }
     }
-    
-    // Add the remaining elements as another subvector
-    result.push_back({vector<int>(vec.begin() + i, vec.end())});
     
     return result;
 }
