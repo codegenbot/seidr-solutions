aade3b961395613af0eb4f6962a8712524e016d3
beep boop <githubbot@vadim.me>
2024-08-03 04:50:57 +0200

    Solution 7 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/uvcdxz1c Prompt:    23 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ d53e34d9-3231-42f4-8e0d-f410e716783a.cpp:23:34: error: expected primary-expressi on before ‘int’    23 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ d53e34d9-3231-42f4-8e0d-f410e716783a.cpp:23:39: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from d53e34d9-3231-42f4-8e0d-f410e716783a.cpp:22: d53e34d9-3231-42f4-8e0d-f410e716783a.cpp: In function ‘int main()’: d53e34d9-3231-42f4-8e0d-f410e716783a.cpp:32:20: error: ‘count_up_to’ was not dec lared in this scope    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ d53e34d9-3231-42f4-8e0d-f410e716783a.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,26 +1,27 @@
-bool issame(vector<int> a, vector<int> b){
-    if(a.size() != b.size()){
-        return false;
+bool issame(vector<int> a, vector<int> b) {
+    if(a.size() != b.size()) return false;
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) return false;
     }
-    
-    for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
-            return false;
-        }
-    }
-    
     return true;
 }
 
-int count_up_to(vector<int> primes, int x){
+int count_up_to(int n) {
     int count = 0;
-    for(int prime : primes){
-        if(prime <= x){
-            count++;
-        }
-        else{
-            break;
+    vector<bool> isPrime(n, true);
+    for(int p = 2; p * p < n; p++) {
+        if(isPrime[p]) {
+            for(int i = p * p; i < n; i += p) {
+                isPrime[i] = false;
+            }
         }
     }
+
+    for(int i = 2; i < n; i++) {
+        if(isPrime[i]) {
+            count++;
+        }
+    }
+
     return count;
 }
