c60f0f318f7b3495232c802476e5cffc376cb447
beep boop <githubbot@vadim.me>
2024-08-03 21:04:20 +0200

    Solution 2 of CPP/37, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/d9a93wwb Prompt:    23 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 2ffb4fc0-0fa1-4b20-9866-c079f4e549f0.cpp:23:36: error: expected primary-expressi on before ‘float’    23 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2ffb4fc0-0fa1-4b20-9866-c079f4e549f0.cpp:23:43: error: expression list treated a s compound expression in initializer [-fpermissive]    23 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 2ffb4fc0-0fa1-4b20-9866-c079f4e549f0.cpp:22: 2ffb4fc0-0fa1-4b20-9866-c079f4e549f0.cpp: In function ‘int main()’: 2ffb4fc0-0fa1-4b20-9866-c079f4e549f0.cpp:32:20: error: ‘sort_even’ was not decla red in this scope    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |                    ^~~~~~~~~ 2ffb4fc0-0fa1-4b20-9866-c079f4e549f0.cpp:32:19: error: ‘issame’ cannot be used a s a function    32 |     assert (issame(sort_even({5, 8, -12, 4, 23, 2, 3, 11, 12, -10}) , {- 12, 8, 3, 4, 5, 2, 12, 11, 23, -10}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_37.cpp
+++ CPP_37.cpp
@@ -1,21 +1,3 @@
-vector<float> l_prime = l;
-    vector<float> even_values;
-
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            even_values.push_back(l[i]);
-        }
-    }
-
-    sort(even_values.begin(), even_values.end());
-
-    int j = 0;
-    for (int i = 0; i < l.size(); i++) {
-        if (i % 2 == 0) {
-            l_prime[i] = even_values[j];
-            j++;
-        }
-    }
-
-    return l_prime;
+bool issame(vector<float> a, vector<float> b) {
+    return a == b;
 }
