d7d3816779a3a44300552403dd1bc0e50ea0a4f2
beep boop <githubbot@vadim.me>
2024-08-03 08:02:59 +0200

    Solution 26 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/3z54ri6k Prompt: 912e0bf5-4791-4d0d-82db-dbd335eb499d.cpp:42:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    42 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ 912e0bf5-4791-4d0d-82db-dbd335eb499d.cpp:7:6: note: ‘bool issame(std::vector<std ::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> >) ’ previously defined here     7 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ 912e0bf5-4791-4d0d-82db-dbd335eb499d.cpp:50:5: error: redefinition of ‘int main( )’    50 | int main(){       |     ^~~~ 912e0bf5-4791-4d0d-82db-dbd335eb499d.cpp:37:5: note: ‘int main()’ previously def ined here    37 | int main() {       |     ^~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -4,13 +4,9 @@
 
 using namespace std;
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
-    
+
     string group;
     int count = 0;
 
@@ -32,19 +28,8 @@ vector<string> separate_paren_groups(string paren_string) {
     }
 
     return result;
-} 
+}
 
 int main() {
-    vector<string> a = separate_paren_groups("((()))");
-    vector<string> b = separate_paren_groups("((())())");
-
-    if (issame(a, b)) {
-        // Do something if the groups are same
-    } else {
-        // Do something if the groups are different
-    }
-
     assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
-
-    return 0;
 }
