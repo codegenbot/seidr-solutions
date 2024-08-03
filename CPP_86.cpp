06e4331d5a42da3538efe14fcf5180f124e2117a
beep boop <githubbot@vadim.me>
2024-08-03 04:15:19 +0200

    Solution 4 of CPP/86, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/itvze75l Prompt: 95e29275-5935-42e3-a167-48045aafec8b.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string anti_shuffle(string s){       | ^~~~~~ In file included from 95e29275-5935-42e3-a167-48045aafec8b.cpp:18: 95e29275-5935-42e3-a167-48045aafec8b.cpp: In function ‘int main()’: 95e29275-5935-42e3-a167-48045aafec8b.cpp:20:13: error: ‘anti_shuffle’ was not de clared in this scope    20 |     assert (anti_shuffle("Hi. My name is Mister Robot. How are you?") ==  ".Hi My aemn is Meirst .Rboot How aer ?ouy");       |             ^~~~~~~~~~~~

--- CPP_86.cpp
+++ CPP_86.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
-#include <algorithm>
 #include <string>
-#include <cassert>
-using namespace std;
+#include <algorithm>
 
 string anti_shuffle(string s){
     string result = "";
@@ -19,9 +16,4 @@ string anti_shuffle(string s){
     sort(word.begin(), word.end());
     result += word;
     return result;
-}
-
-int main() {
-    assert(anti_shuffle("Hi. My name is Mister Robot. How are you?") == ".Hi My aemn is Meirst .Rboot How aer ?ouy");
-    return 0;
 }
