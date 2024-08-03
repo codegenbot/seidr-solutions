aff23e75fc94ea4553db8e79ddf565d6a1f382bf
beep boop <githubbot@vadim.me>
2024-08-03 08:35:07 +0200

    Solution 34 of CPP/7, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/kosb39xh Prompt: In file included from /usr/include/c++/11/cassert:44,                  from 65dfef4b-f58f-405d-980d-2d85dd15b13b.cpp:4: 65dfef4b-f58f-405d-980d-2d85dd15b13b.cpp: In function ‘int main()’: 65dfef4b-f58f-405d-980d-2d85dd15b13b.cpp:19:12: error: ‘issame’ was not declared  in this scope; did you mean ‘isspace’?    19 |     assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gru esome"}, "run"), {"grunt", "prune"}));       |            ^~~~~~ 65dfef4b-f58f-405d-980d-2d85dd15b13b.cpp: At global scope: 65dfef4b-f58f-405d-980d-2d85dd15b13b.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 65dfef4b-f58f-405d-980d-2d85dd15b13b.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_7.cpp
+++ CPP_7.cpp
@@ -16,6 +16,7 @@ vector<string> filter_by_substring(const vector<string>& strings, const string& 
 }
 
 int main() {
-    assert(issame(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run"), vector<string>{"grunt", "prune"}));
+    assert(filter_by_substring({"grunt", "trumpet", "prune", "gruesome"}, "run") == vector<string>{"grunt", "prune"});
+    
     return 0;
 }
