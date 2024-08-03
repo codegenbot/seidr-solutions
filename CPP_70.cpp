6680ae64815365716f6a38759f0f44d2fa06637c
beep boop <githubbot@vadim.me>
2024-08-03 23:15:25 +0200

    Solution 80 of CPP/70, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tul9we1c Prompt: In file included from /usr/include/c++/11/vector:67,                  from 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp:24:34: error: expected primary-expressi on before ‘int’    24 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp:24:39: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp:23: 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp: In function ‘int main()’: 8819ae59-de96-4fcb-a981-8c84cfc1391e.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(strange_sort_list({111111}) , {111111}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_70.cpp
+++ CPP_70.cpp
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <cassert>
 
-bool custom_equal(const std::vector<int>& a, const std::vector<int>& b){
+bool issame(std::vector<int> a, std::vector<int> b){
     return std::equal(a.begin(), a.end(), b.begin());
 }
 
@@ -21,7 +21,7 @@ std::vector<int> strange_sort_list(std::vector<int> lst){
     return result;
 }
 
-int main() {
-    assert(custom_equal(strange_sort_list({111111}) , {111111}));
+int main(){
+    assert (issame(strange_sort_list({111111}), {111111}));
     return 0;
 }
