8a52d5f6691a90272ad39292bf57749050441a04
beep boop <githubbot@vadim.me>
2024-08-04 00:08:50 +0200

    Solution 6 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt:    17 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 7c6046b1-b576-4aa8-b2e9-8d3ff48b8db0.cpp:17:34: error: expected primary-expressi on before ‘int’    17 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 7c6046b1-b576-4aa8-b2e9-8d3ff48b8db0.cpp:17:39: error: expression list treated a s compound expression in initializer [-fpermissive]    17 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 7c6046b1-b576-4aa8-b2e9-8d3ff48b8db0.cpp:16: 7c6046b1-b576-4aa8-b2e9-8d3ff48b8db0.cpp: In function ‘int main()’: 7c6046b1-b576-4aa8-b2e9-8d3ff48b8db0.cpp:26:20: error: ‘count_up_to’ was not dec lared in this scope    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~ 7c6046b1-b576-4aa8-b2e9-8d3ff48b8db0.cpp:26:19: error: ‘issame’ cannot be used a s a function    26 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -3,25 +3,20 @@
 #include <cassert>
 
 std::vector<int> count_up_to(int n) {
-    std::vector<int> result;
-    if (n < 2) {
-        return result;
-    }
-    std::vector<bool> is_prime(n, true);
-    is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i * i < n; ++i) {
-        if (is_prime[i]) {
-            for (int j = i * i; j < n; j += i) {
-                is_prime[j] = false;
+    std::vector<int> primes;
+    for (int i = 2; i < n; ++i) {
+        bool is_prime = true;
+        for (int j = 2; j*j <= i; ++j) {
+            if (i % j == 0) {
+                is_prime = false;
+                break;
             }
         }
-    }
-    for (int i = 2; i < n; ++i) {
-        if (is_prime[i]) {
-            result.push_back(i);
+        if (is_prime) {
+            primes.push_back(i);
         }
     }
-    return result;
+    return primes;
 }
 
 bool issame(std::vector<int> a, std::vector<int> b) {
@@ -30,5 +25,6 @@ bool issame(std::vector<int> a, std::vector<int> b) {
 
 int main() {
     assert(issame(count_up_to(101), {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
+    
     return 0;
 }
