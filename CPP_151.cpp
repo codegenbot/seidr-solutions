3be5fb18b5fb430f87528a8a37ad5ef7105125af
beep boop <githubbot@vadim.me>
2024-08-03 05:11:04 +0200

    Solution 74 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/t17sc75i Prompt: 65f10ee6-09e4-425d-a4ca-184db1ffd8a5.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the completed code:       | ^~~~ In file included from 65f10ee6-09e4-425d-a4ca-184db1ffd8a5.cpp:15: 65f10ee6-09e4-425d-a4ca-184db1ffd8a5.cpp: In function ‘int main()’: 65f10ee6-09e4-425d-a4ca-184db1ffd8a5.cpp:17:35: error: ‘lst’ was not declared in  this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 65f10ee6-09e4-425d-a4ca-184db1ffd8a5.cpp:17:13: error: ‘double_the_difference’ w as not declared in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |             ^~~~~~~~~~~~~~~~~~~~~ 65f10ee6-09e4-425d-a4ca-184db1ffd8a5.cpp:17:43: error: ‘odd_sum’ was not declare d in this scope    17 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -1,24 +1,15 @@
 int main() {
     vector<float> lst;
-    long long odd_sum = 0;
-    cout << "Enter numbers separated by space: ";
-    for (char c; cin >> c && c != '\n'; ) {}
-    cin >> noskipws;
-    for (float num; cin >> num;) {
-        lst.push_back(num);
-    }
-    cout << "Sum of squares of positive integer roots: " << double_the_difference(lst) << endl;
-    return 0;
-}
-
-long long double_the_difference(vector<float> lst) {
-    long long sum = 0;
-    for (float num : lst) {
-        if (num > 0 && floor(num) == num) { 
-            if (modf(sqrt(num), &num) != 0.0) { 
-                sum += pow(num, 2);
+    long long odd_sum = 0; 
+    long long double_the_difference(vector<float> lst) {
+        long long sum = 0;
+        for (float num : lst) {
+            if (num > 0 && floor(num) == num) { 
+                if (modf(sqrt(num), &num) != 0.0) { 
+                    sum += pow(num, 2);
+                }
             }
         }
+        return sum;
     }
-    return sum;
 }
