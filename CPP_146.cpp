79ccf8af902e4f558d47c79243ac6f84077aa846
beep boop <githubbot@vadim.me>
2024-08-03 04:44:46 +0200

    Solution 11 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/e31fpcu0 Prompt: 9f0f3b98-036c-4746-a098-7c946da52c91.cpp:1:19: error: ‘vector’ was not declared in this scope     1 | int specialFilter(vector<int> nums) {       |                   ^~~~~~ 9f0f3b98-036c-4746-a098-7c946da52c91.cpp:1:26: error: expected primary-expressio n before ‘int’     1 | int specialFilter(vector<int> nums) {       |                          ^~~ In file included from 9f0f3b98-036c-4746-a098-7c946da52c91.cpp:11: 9f0f3b98-036c-4746-a098-7c946da52c91.cpp: In function ‘int main()’: 9f0f3b98-036c-4746-a098-7c946da52c91.cpp:13:26: error: ‘specialFilter’ cannot be  used as a function    13 |     assert (specialFilter({}) == 0                   );       |             ~~~~~~~~~~~~~^~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -12,9 +12,7 @@ int specialFilter(std::vector<int> nums) {
 }
 
 int main() {
-    assert(specialFilter({}) == 0);
-    std::vector<int> numbers = {11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    int result = specialFilter(numbers);
-    std::cout << "Number of special numbers: " << result << std::endl;
+    std::vector<int> nums = {-11, -12, 13, 14, 15, 16};
+    std::cout << "The number of special numbers is: " << specialFilter(nums) << std::endl;
     return 0;
 }
