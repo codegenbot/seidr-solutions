573799da12195ed820514cd4381aa4f54ceccf76
beep boop <githubbot@vadim.me>
2024-08-03 13:08:41 +0200

    Solution 3 of CPP/156, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/z4gqjvss Prompt: 25455b2c-a71a-4110-b835-148176b7b7d0.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string int_to_mini_romank(int number){       | ^~~~~~ In file included from 25455b2c-a71a-4110-b835-148176b7b7d0.cpp:20: 25455b2c-a71a-4110-b835-148176b7b7d0.cpp: In function ‘int main()’: 25455b2c-a71a-4110-b835-148176b7b7d0.cpp:22:13: error: ‘int_to_mini_romank’ was not declared in this scope    22 |     assert (int_to_mini_romank(1000) == "m");       |             ^~~~~~~~~~~~~~~~~~

--- CPP_156.cpp
+++ CPP_156.cpp
@@ -2,22 +2,3 @@
 #include <vector>
 
 string int_to_mini_romank(int number);
-
-string int_to_mini_romank(int number){
-    vector<int> nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    vector<string> romans = {"m", "cm", "d", "cd", "c", "xc", "l", "xl", "x", "ix", "v", "iv", "i"};
-
-    string result = "";
-    int index = 0;
-
-    while (number > 0) {
-        if (number >= nums[index]) {
-            result += romans[index];
-            number -= nums[index];
-        } else {
-            index++;
-        }
-    }
-
-    return result;
-}
