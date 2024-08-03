14d9855a0c6c05bdf4247d5c74089411ae9ad0a3
beep boop <githubbot@vadim.me>
2024-08-03 17:44:03 +0200

    Solution 22 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/0d7n12ag Prompt:       |                                     ^~~~~~ f86c82c7-7e61-40ba-a660-4893e3712580.cpp:38:37: note: suggested alternatives: In file included from /usr/include/c++/11/iosfwd:39,                  from /usr/include/c++/11/ios:38,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from f86c82c7-7e61-40ba-a660-4893e3712580.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from /usr/include/c++/11/bits/locale_classes.h:40,                  from /usr/include/c++/11/bits/ios_base.h:41,                  from /usr/include/c++/11/ios:42,                  from /usr/include/c++/11/ostream:38,                  from /usr/include/c++/11/iostream:39,                  from f86c82c7-7e61-40ba-a660-4893e3712580.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ f86c82c7-7e61-40ba-a660-4893e3712580.cpp:38:44: error: ‘b’ was not declared in t his scope    38 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -3,35 +3,6 @@
 #include <string>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+bool issame(std::vector<std::string> a, std::vector<std::string>){ 
     return a == b;
 }
-
-std::vector<std::string> separate_paren_groups(const std::string& paren_string) {
-    std::vector<std::string> result;
-    std::string group;
-    int count = 0;
-
-    for (char c : paren_string) {
-        if (c == '(') {
-            count++;
-            if (count > 1) {
-                group += c;
-            }
-        } else if (c == ')') {
-            count--;
-            if (count > 0) {
-                group += c;
-            } else if (count == 0) {
-                result.push_back(group);
-                group = "";
-            }
-        }
-    }
-
-    return result;
-}
-
-using namespace std;
-
-assert(issame(separate_paren_groups("( ) (( )) (( )( ))"), {"()", "(())", "(()())"}));
