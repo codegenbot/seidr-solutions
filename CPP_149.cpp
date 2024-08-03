e0071bcc25faa40c5533ce4a111116a4f0f6c77e
beep boop <githubbot@vadim.me>
2024-08-03 12:50:56 +0200

    Solution 92 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/9nawtijy Prompt:    24 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ caca1934-e83e-4299-9669-78a91cdf11c7.cpp:24:44: error: ‘b’ was not declared in t his scope    24 | bool issame(vector<string> a,vector<string>b){       |                                            ^ caca1934-e83e-4299-9669-78a91cdf11c7.cpp:24:45: error: expression list treated a s compound expression in initializer [-fpermissive]    24 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from caca1934-e83e-4299-9669-78a91cdf11c7.cpp:23: caca1934-e83e-4299-9669-78a91cdf11c7.cpp: In function ‘int main()’: caca1934-e83e-4299-9669-78a91cdf11c7.cpp:33:20: error: ‘sorted_list_sum’ was not  declared in this scope    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |                    ^~~~~~~~~~~~~~~ caca1934-e83e-4299-9669-78a91cdf11c7.cpp:33:19: error: ‘issame’ cannot be used a s a function    33 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,3 +1,18 @@
+#include <vector>
+#include <algorithm>
+
+bool issame(string s1, string s2) {
+    if (s1.length() != s2.length()) {
+        return false;
+    }
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 vector<string> sorted_list_sum(vector<string> lst) {
     vector<string> result = lst;
     for (auto it = result.begin(); it != result.end();) {
@@ -7,7 +22,7 @@ vector<string> sorted_list_sum(vector<string> lst) {
             ++it;
         }
     }
-    sort(result.begin(), result.end(), 
+    sort(result.begin(), result.end(),
          [](const string& a, const string& b) {
              if (a.length() == b.length()) {
                  return a < b;
