855f3a3dfd9dcd0aeb09ec97c6c46d404058a8a2
beep boop <githubbot@vadim.me>
2024-08-03 18:11:18 +0200

    Solution 14 of Python/121, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ihoobi73 Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8a3f8c4d-8f20-4515-bd72-85ebc1ffb 487.py", line 1     ```     ^ SyntaxError: invalid syntax

--- Python_121.py
+++ Python_121.py
@@ -1,14 +1,3 @@
-def find_max_sum_of_subarray(arr):
-    max_sum = arr[0]
-    current_sum = arr[0]
-
-    for num in arr[1:]:
-        if current_sum < 0:
-            current_sum = num
-        else:
-            current_sum += num
-        
-        if current_sum > max_sum:
-            max_sum = current_sum
-
-    return max_sum
+def find_max_difference(lst):
+    lst.sort()
+    return lst[-1] - lst[0]
