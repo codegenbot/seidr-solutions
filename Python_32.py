72e690602ad52edf3482a3eb562682dfc7476041
beep boop <githubbot@vadim.me>
2024-08-03 23:38:12 +0200

    Solution 68 of Python/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/078ak3d2 Prompt: Program did not receive expected input

--- Python_32.py
+++ Python_32.py
@@ -1,9 +1,4 @@
 import math
 
-def find_zero(xs: list):
-    a, b, c, d = map(float, xs)
-    return round((-b + math.sqrt(b * b - 4 * a * c)) / (2 * a), 2)
-
-xs = input().split()
-result = find_zero(xs)
-print(result)
+def find_zero(a: int, b: int, c: int) -> float:
+    return round((-b + math.sqrt(b * b - 3 * a * c)) / (3 * a), 2)
