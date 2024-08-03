aa4fc87672bb1e51dbef35a6bcf72f6c329a0422
beep boop <githubbot@vadim.me>
2024-08-03 21:54:21 +0200

    Solution 24 of spin-words, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/xih3kkth Prompt:  this scope    30 |     cout << "Enter a sentence: ";       |     ^~~~ c0d9c0a0-e262-4c4b-9691-c989ab27098e.cpp:2:1: note: ‘std::cout’ is defined in he ader ‘<iostream>’; did you forget to ‘#include <iostream>’?     1 | #include <string>   +++ |+#include <iostream>     2 | using namespace std; c0d9c0a0-e262-4c4b-9691-c989ab27098e.cpp:31:13: error: ‘cin’ was not declared in  this scope    31 |     getline(cin, s);       |             ^~~ c0d9c0a0-e262-4c4b-9691-c989ab27098e.cpp:31:13: note: ‘std::cin’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? c0d9c0a0-e262-4c4b-9691-c989ab27098e.cpp:32:29: error: ‘endl’ was not declared i n this scope    32 |     cout << spinWords(s) << endl;       |                             ^~~~ c0d9c0a0-e262-4c4b-9691-c989ab27098e.cpp:2:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?     1 | #include <string>   +++ |+#include <ostream>     2 | using namespace std;

--- spin-words.cpp
+++ spin-words.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 string spinWords(string str) {
-    string result = "";
+    string result = " ";
     int length = 0;
-    for (int i = 0; i < str.length(); i++) {
-        if (str[i] == ' ') {
+    for (int i = 0; i <= str.length(); i++) {
+        if (i == str.length() || str[i] == ' ') {
             if (length >= 5)
                 for (int j = length - 1; j >= 0; j--)
-                    result += str[i - j];
+                    result += str.substr(i - length, length)[j];
             else
                 result += str.substr(i - length, length);
             length = 0;
@@ -19,18 +19,13 @@ string spinWords(string str) {
             length++;
         }
     }
-    if (length >= 5)
-        for (int i = length - 1; i >= 0; i--)
-            result += str[i];
-    else
-        result += str;
     return result;
 }
 
 int main() {
     string s;
     cout << "Enter a sentence: ";
-    cin >> s;
+    getline(cin, s);
     cout << spinWords(s) << endl;
     return 0;
 }
