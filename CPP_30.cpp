c7a51775f93040280afdb35bfff7fa914a8fe317
beep boop <githubbot@vadim.me>
2024-08-03 20:15:54 +0200

    Solution 4 of CPP/30, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/c7wazy7s Prompt:       |                    ^~~~~ 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp:12:29: error: ‘vector’ was not declared  in this scope    12 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp:12:36: error: expected primary-expressi on before ‘float’    12 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp:12:43: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp:11: 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp: In function ‘int main()’: 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp:21:20: error: ‘get_positive’ was not de clared in this scope    21 |     assert (issame(get_positive({}) , {}));       |                    ^~~~~~~~~~~~ 8484a9de-3946-4688-b47f-0447a1cc9a50.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(get_positive({}) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~

--- CPP_30.cpp
+++ CPP_30.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
 #include <vector>
+#include <cassert>
 
-std::vector<float> get_positive(std::vector<float> l) {
-    std::vector<float> result;
+using namespace std;
+
+bool issame(vector<float> a, vector<float> b){
+    return a == b;
+}
+
+vector<float> get_positive(vector<float> l){
+    vector<float> result;
     for (float num : l) {
         if (num > 0) {
             result.push_back(num);
@@ -10,6 +18,8 @@ std::vector<float> get_positive(std::vector<float> l) {
     return result;
 }
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    return a == b;
+int main(){
+    assert(issame(get_positive({}), {}));
+    
+    return 0;
 }
