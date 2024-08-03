36c18d63c2a0afd0efd3ad48f8229d2e5ce188ea
beep boop <githubbot@vadim.me>
2024-08-03 08:58:55 +0200

    Solution 90 of Python/10, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/52mxctu7 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80e6dad0-b058-449e-b743-ab3cae10f 25a.py", line 16, in <module>     check(make_palindrome)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/80e6dad0-b058-449e-b743-ab3cae10f 25a.py", line 13, in check     assert make_palindrome("jerry") == "jerryrrej"            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_10.py
+++ Python_10.py
@@ -3,10 +3,11 @@ def is_palindrome(string: str) -> bool:
 
 
 def make_palindrome(string: str) -> str:
-    if string == string[::-1]:
-        return string + string[::-1]
+    if is_palindrome(string):
+        return string + "a"
+
     for i in range(len(string)):
-        postfix = string[i:]
-        if postfix == postfix[::-1]:
-            return string + postfix[::-1]
+        new_string = string[:i] + string[i][::-1] + string[i + 1 :]
+        if is_palindrome(new_string):
+            return new_string
     return string + string[::-1]
