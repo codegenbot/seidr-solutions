6250f8998264ef85ffd83f975cb6b599998c7962
beep boop <githubbot@vadim.me>
2024-08-03 07:02:59 +0200

    Solution 32 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7l5w7q5x Prompt: 4243f8a7-3b4e-4682-a818-789f431dde77.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 4243f8a7-3b4e-4682-a818-789f431dde77.cpp:21:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    21 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 4243f8a7-3b4e-4682-a818-789f431dde77.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 4243f8a7-3b4e-4682-a818-789f431dde77.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -20,5 +20,7 @@ vector<int> get_odd_collatz(int n) {
 
 int main() {
     assert(get_odd_collatz(1) == vector<int>{1});
+    assert(get_odd_collatz(15) == vector<int>{15, 46, 23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1});
+    
     return 0;
 }
