74aa2a4b7552327cd24a5728c93264c4a33f1e96
beep boop <githubbot@vadim.me>
2024-08-03 19:31:17 +0200

    Solution 54 of Python/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mgm0cj51 Prompt: Program did not receive expected input

--- Python_1.py
+++ Python_1.py
@@ -1,11 +1,10 @@
 from typing import List
 
-
 def separate_paren_groups(paren_string: str) -> List[str]:
     result = []
     temp = ""
     for char in paren_string:
-        if char in ["(", ")"]:
+        if char in ['(', ')']:
             temp += char
         elif temp != "":
             result.append(temp)
