eb110d2df3ef5a74eb5682800354e14983019ce0
beep boop <githubbot@vadim.me>
2024-08-03 14:57:44 +0200

    Solution 7 of CPP/99, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/tmt04kxt Prompt:       |                                        ^~~~~ fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp:3:5: error: expected unqualified-id bef ore ‘if’     3 |     if (num - rounded_num > 0.5) {       |     ^~ fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp:5:7: error: expected unqualified-id bef ore ‘else’     5 |     } else if (num - rounded_num < -0.5) {       |       ^~~~ fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp:7:7: error: expected unqualified-id bef ore ‘else’     7 |     } else {       |       ^~~~ fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp:10:1: error: expected declaration befor e ‘}’ token    10 | }       | ^ In file included from fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp:12: fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp: In function ‘int main()’: fabc9a0e-1c01-4e53-9cdf-0a515eecc880.cpp:14:13: error: ‘closest_integer’ was not  declared in this scope    14 |     assert (closest_integer("0") == 0);       |             ^~~~~~~~~~~~~~~

--- CPP_99.cpp
+++ CPP_99.cpp
@@ -1,6 +1,4 @@
 int closest_integer(std::string value) {
     double num = stod(value);
-    int rounded_num = static_cast<int>(round(num));
-  
     return static_cast<int>(num + (num > 0 ? 0.5 : -0.5));
 }
