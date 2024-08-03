f40117ee1500cfd071101637d064ebba68782c57
beep boop <githubbot@vadim.me>
2024-08-03 04:05:45 +0200

    Solution 5 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/q2hs3fhq Prompt: Complete the following code given the task description and function signature.

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,79 +1,29 @@
-Here is the completed code:
-
-```cpp
-#include <boost/any.hpp>
-#include <string>
-
-using namespace boost;
-
+#include<stdio.h>
+#include<string>
+#include<algorithm>
+#include<boost/any.hpp>
+using namespace std;
 boost::any compare_one(boost::any a, boost::any b) {
-    if (a.type() == typeid(int) && b.type() == typeid(double)) {
+    if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return boost::any_cast<float>(b);
+    }
+    else if (a.type() == typeid(float) && b.type() == typeid(string)) {
         return b;
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(int)) {
-        return b;
+    else if (a.type() == typeid(string) && b.type() == typeid(int)) {
+        return a;
     }
     else if (a.type() == typeid(string) && b.type() == typeid(string)) {
-        string str_a = any_cast<string>(a);
-        string str_b = any_cast<string>(b);
-
-        if (str_b.size() > 0 && str_b[0] == ',') {
-            str_b = str_b.substr(1);
-        }
-
-        if (stod(str_a) < stod(str_b)) {
-            return b;
-        }
-        else if (stod(str_a) > stod(str_b)) {
-            return a;
-        }
-        else {
-            return boost::any("None");
-        }
+        if (boost::any_cast<string>(a) != boost::any_cast<string>(b))
+            return (boost::any_cast<string>(a) > boost::any_cast<string>(b)) ? a : b;
+        else
+            return "None";
     }
-    else if (a.type() == typeid(string) && b.type() == typeid(double)) {
-        string str = any_cast<string>(a);
-
-        if (str.size() > 0 && str[0] == ',') {
-            str = str.substr(1);
-        }
-
-        if (stod(str) < b.convert_to<double>()) {
-            return b;
-        }
-        else if (stod(str) > b.convert_to<double>()) {
-            return a;
-        }
-        else {
-            return boost::any("None");
-        }
+    else if (a.type() == typeid(float) && b.type() == typeid(int)) {
+        return (boost::any_cast<float>(a) > static_cast<double>(boost::any_cast<int>(b))) ? a : boost::any(b);
     }
-    else if (a.type() == typeid(double) && b.type() == typeid(string)) {
-        string str = any_cast<string>(b);
-
-        if (str.size() > 0 && str[0] == ',') {
-            str = str.substr(1);
-        }
-
-        if (stod(a.convert_to<string>()) < stod(str)) {
-            return a;
-        }
-        else if (stod(a.convert_to<string>()) > stod(str)) {
-            return b;
-        }
-        else {
-            return boost::any("None");
-        }
+    else if (a.type() == typeid(int) && b.type() == typeid(float)) {
+        return (static_cast<double>(boost::any_cast<int>(a)) > boost::any_cast<float>(b)) ? a : b;
     }
-    else {
-        if (a.convert_to<int>() < b.convert_to<int>()) {
-            return b;
-        }
-        else if (a.convert_to<int>() > b.convert_to<int>()) {
-            return a;
-        }
-        else {
-            return boost::any("None");
-        }
-    }
+    return "None";
 }
