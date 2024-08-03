da370fadeead6b1687d41c48841c3c78058718fd
beep boop <githubbot@vadim.me>
2024-08-03 20:46:33 +0200

    Solution 16 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/tgmist2y Prompt:    20 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:20:34: error: expected primary-expressi on before ‘int’    20 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:20:39: error: expression list treated a s compound expression in initializer [-fpermissive]    20 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:19: 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp: In function ‘int main()’: 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:29:20: error: ‘sort_third’ was not decl ared in this scope    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~ 797d9bb5-2d71-4770-a5a4-ca0e445c5787.cpp:29:19: error: ‘issame’ cannot be used a s a function    29 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-bool issame(vector<int> a, vector<int> b){
-    return a == b;
+vector<int> sort_third(vector<int> l);
+
+int main() {
+    vector<int> l = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    vector<int> expected = {3, 8, 1, 6, 5, 4, 9, 2, 7};
+
+    vector<int> result = sort_third(l);
+
+    if (issame(result, expected)) {
+        cout << "Output is correct" << endl;
+    } else {
+        cout << "Output is incorrect" << endl;
+    }
+
+    return 0;
 }
 
 vector<int> sort_third(vector<int> l) {
@@ -22,9 +43,4 @@ vector<int> sort_third(vector<int> l) {
         }
     }
     return res;
-}
-
-int main() {
-    assert(issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}), {2, 6, 3, 4, 8, 9, 5, 1}));
-    return 0;
 }
