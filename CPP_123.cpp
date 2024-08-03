7bfc139216040c38cbe16100a8659c7939e38195
beep boop <githubbot@vadim.me>
2024-08-03 06:53:22 +0200

    Solution 18 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/7l5w7q5x Prompt: 93fa23b9-6111-483f-96e1-075b1b6944d7.cpp:31:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    31 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 93fa23b9-6111-483f-96e1-075b1b6944d7.cpp:21:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    21 | bool issame(vector<int> a, vector<int> b) {       |      ^~~~~~ 93fa23b9-6111-483f-96e1-075b1b6944d7.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ 93fa23b9-6111-483f-96e1-075b1b6944d7.cpp:25:5: note: ‘int main()’ previously def ined here    25 | int main() {       |     ^~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -4,26 +4,22 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> get_odd_collatz(int num) {
+vector<int> get_odd_collatz(int n) {
     vector<int> collatz_sequence;
-    collatz_sequence.push_back(num);
-
-    while (num != 1) {
-        if (num % 2 == 0) {
-            num /= 2;
+    while (n != 1) {
+        collatz_sequence.push_back(n);
+        if (n % 2 == 0) {
+            n = n / 2;
         } else {
-            num = 3 * num + 1;
-        }
-        if (num % 2 != 0) {
-            collatz_sequence.push_back(num);
+            n = 3 * n + 1;
         }
     }
-
+    collatz_sequence.push_back(1);
     return collatz_sequence;
+}
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
 }
 
 int main() {
