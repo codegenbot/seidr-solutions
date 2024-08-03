e73fbd0a70b39a6cecab683ce23582a648a1fbe5
beep boop <githubbot@vadim.me>
2024-08-03 11:28:49 +0200

    Solution 14 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/ikovf1qp Prompt: In file included from /usr/include/c++/11/vector:67,                  from d251a687-85af-449f-8e5e-d69f924b1b35.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from d251a687-85af-449f-8e5e-d69f924b1b35.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ d251a687-85af-449f-8e5e-d69f924b1b35.cpp:34:34: error: expected primary-expressi on before ‘int’    34 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from d251a687-85af-449f-8e5e-d69f924b1b35.cpp:33: d251a687-85af-449f-8e5e-d69f924b1b35.cpp: In function ‘int main()’: d251a687-85af-449f-8e5e-d69f924b1b35.cpp:43:20: error: ‘factorize’ was not decla red in this scope    43 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ d251a687-85af-449f-8e5e-d69f924b1b35.cpp:43:19: error: ‘issame’ cannot be used a s a function    43 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,12 +1,22 @@
 #include <vector>
 #include <cassert>
-#include <algorithm>
 
-vector<int> factorize(int n);
-bool issame(vector<int> a, vector<int> b);
+bool issame(std::vector<int> a, std::vector<int> b) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    
+    for(int i = 0; i < a.size(); i++) {
+        if(a[i] != b[i]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
 
-vector<int> factorize(int n) {
-    vector<int> factors;
+std::vector<int> factorize(int n) {
+    std::vector<int> factors;
     for (int i = 2; i * i <= n; i++) {
         while (n % i == 0) {
             factors.push_back(i);
@@ -17,18 +27,6 @@ vector<int> factorize(int n) {
         factors.push_back(n);
     }
     return factors;
-}
-
-bool issame(vector<int> a, vector<int> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int value : a) {
-        if (find(b.begin(), b.end(), value) == b.end()) {
-            return false;
-        }
-    }
-    return true;
 }
 
 int main() {
