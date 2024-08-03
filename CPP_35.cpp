7e2bd19b33224a99d22f45957bdea2c3728e9e4e
beep boop <githubbot@vadim.me>
2024-08-03 08:33:41 +0200

    Solution 61 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/22zes48q Prompt: 16f3373a-7689-408c-baca-96108ca30d1b.cpp:1:1: error: ‘vector’ does not name a ty pe     1 | vector<float> v = l; return *max_element(v.begin(), v.end());       | ^~~~~~ 16f3373a-7689-408c-baca-96108ca30d1b.cpp:1:22: error: expected unqualified-id be fore ‘return’     1 | vector<float> v = l; return *max_element(v.begin(), v.end());       |                      ^~~~~~ In file included from 16f3373a-7689-408c-baca-96108ca30d1b.cpp:3: 16f3373a-7689-408c-baca-96108ca30d1b.cpp: In function ‘int main()’: 16f3373a-7689-408c-baca-96108ca30d1b.cpp:5:17: error: ‘max_element’ was not decl ared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ^~~~~~~~~~~ 16f3373a-7689-408c-baca-96108ca30d1b.cpp:5:13: error: ‘abs’ was not declared in this scope     5 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |             ^~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -3,9 +3,6 @@
 
 int main() {
     std::vector<float> l;
-    // Read input from user
-    for (float x; std::cin >> x;) {
-        l.push_back(x);
-    }
+    std::cin >> std::fixed >> std::showpoint;
+    for(float i;std::cin>>i;)l.push_back(i);
     return *std::max_element(l.begin(), l.end());
-}
