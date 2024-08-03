bdd3d1ccd9d723e6b47672f447df19ffb1696a72
beep boop <githubbot@vadim.me>
2024-08-03 19:35:37 +0200

    Solution 60 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/vw6qzgbr Prompt:       |         ^~ 6e55f496-cc9d-49f4-955c-83f8318f8aba.cpp:46:9: note: remove parentheses to defau lt-initialize a variable    46 | int main(){       |         ^~       |         -- 6e55f496-cc9d-49f4-955c-83f8318f8aba.cpp:46:9: note: or replace parentheses with  braces to value-initialize a variable 6e55f496-cc9d-49f4-955c-83f8318f8aba.cpp:46:11: error: a function-definition is not allowed here before ‘{’ token    46 | int main(){       |           ^ 6e55f496-cc9d-49f4-955c-83f8318f8aba.cpp:48:2: error: expected ‘}’ at end of inp ut    48 | }       |  ^ 6e55f496-cc9d-49f4-955c-83f8318f8aba.cpp:32:20: note: to match this ‘{’    32 | int solveProblem() {       |                    ^ 6e55f496-cc9d-49f4-955c-83f8318f8aba.cpp:48:2: warning: no return statement in f unction returning non-void [-Wreturn-type]    48 | }       |  ^

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -29,7 +29,7 @@ bool issame(std::vector<float> a, std::vector<float> b) {
     return a == b;
 }
 
-int main() {
+int main(){
     assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {3.1, 4.1});
     return 0;
 }
