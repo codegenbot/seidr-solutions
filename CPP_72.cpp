8d85ad5e8d8b27d2715dd2d5cd8d85cfecea301a
beep boop <githubbot@vadim.me>
2024-08-03 09:52:02 +0200

    Solution 64 of CPP/72, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/boih60gm Prompt:       |                  ^~~~~~ ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp:1:25: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                         ^~~ ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp:1:33: error: expected primary-expressio n before ‘int’     1 | bool will_it_fly(vector<int> q, int w) {       |                                 ^~~ ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp:1:38: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | bool will_it_fly(vector<int> q, int w) {       |                                      ^ ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp:17:19: error: ‘string’ was not declared  in this scope    17 | bool isPalindrome(string s) {       |                   ^~~~~~ In file included from ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp:23: ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp: In function ‘int main()’: ae470af5-3cd3-4adc-9a7e-4b01ba77bd52.cpp:25:24: error: ‘will_it_fly’ cannot be u sed as a function    25 |     assert (will_it_fly({5}, 5) == true);       |             ~~~~~~~~~~~^~~~~~~~

--- CPP_72.cpp
+++ CPP_72.cpp
@@ -2,33 +2,24 @@
 #include <string>
 #include <algorithm>
 
-bool main() {
-    bool will_it_fly(vector<int> q, int w) {
-        string str = "";
-        for (int i : q) {
-            str += to_string(i);
-        }
-        
-        if (!isPalindrome(str)) return false;
-        
-        int sum = 0;
-        for (int i : q) {
-            sum += i;
-        }
-        
-        return sum <= w;
-    }
-
-    bool isPalindrome(string s) {
-        string rev = s;
-        reverse(rev.begin(), rev.end());
-        return s == rev;
+bool will_it_fly(vector<int> q, int w) {
+    string str = "";
+    for (int i : q) {
+        str += to_string(i);
     }
     
-    // Your main function should be here
-    vector<int> q = {1, 2, 3};
-    int w = 6;
-    cout << will_it_fly(q, w) << endl;
+    if (!isPalindrome(str)) return false;
+    
+    int sum = 0;
+    for (int i : q) {
+        sum += i;
+    }
+    
+    return sum <= w;
+}
 
-    return true;
+bool isPalindrome(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    return s == rev;
 }
