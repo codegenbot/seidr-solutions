84fe053f56bc2d42fb19f6c121a97272b1f3d062
beep boop <githubbot@vadim.me>
2024-08-03 02:09:27 +0200

    Solution 2 of CPP/57, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/ve04jvxz Prompt:     2 |     for (int i = 1; i < l.size(); ++i) {       |     ^~~ 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp:2:21: error: ‘i’ does not name a type     2 |     for (int i = 1; i < l.size(); ++i) {       |                     ^ 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp:2:35: error: expected unqualified-id be fore ‘++’ token     2 |     for (int i = 1; i < l.size(); ++i) {       |                                   ^~ 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp:10:5: error: expected unqualified-id be fore ‘return’    10 |     return increasing || decreasing;       |     ^~~~~~ 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp:11:1: error: expected declaration befor e ‘}’ token    11 | }       | ^ In file included from 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp:13: 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp: In function ‘int main()’: 60606a64-1d3b-410b-a2eb-3c51e2c8389b.cpp:15:13: error: ‘monotonic’ was not decla red in this scope    15 |     assert (monotonic({9, 9, 9, 9}) == true);       |             ^~~~~~~~~

--- CPP_57.cpp
+++ CPP_57.cpp
@@ -1,7 +1,12 @@
-int n = l.size();
-bool increasing = true, decreasing = true;
-for (int i = 1; i < n; ++i) {
-    if (l[i] < l[i - 1]) increasing = false;
-    if (l[i] > l[i - 1]) decreasing = false;
+bool monotonic(const std::vector<int>& l) {
+    bool increasing = true, decreasing = true;
+    for (size_t i = 1; i < l.size(); ++i) {
+        if (l[i] < l[i - 1]) {
+            increasing = false;
+        }
+        if (l[i] > l[i - 1]) {
+            decreasing = false;
+        }
+    }
+    return increasing || decreasing;
 }
-return increasing || decreasing;
