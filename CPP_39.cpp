91a1f7b504fd3b1470dc79d8561e00e39e3c15f5
beep boop <githubbot@vadim.me>
2024-08-03 08:51:31 +0200

    Solution 70 of CPP/39, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/ubwv2ise Prompt: 069b478f-872e-4c80-9134-7b5e195ae762.cpp: In function ‘int prime_fib(int)’: 069b478f-872e-4c80-9134-7b5e195ae762.cpp:10:14: error: ‘isPrime’ was not declare d in this scope    10 |         if (!isPrime(fib)) {       |              ^~~~~~~

--- CPP_39.cpp
+++ CPP_39.cpp
@@ -1,29 +1,33 @@
-#include <iostream>
-
-using namespace std;
+```c++
+bool isPrime(int num);
+int prime_fib(int n);
 
 int prime_fib(int n) {
-    int a = 0, b = 1;
-    for (int i = 2; ; i++) {
-        int fib = a + b;
-        if (fib > n) return i;
+    int a = 0, b = 1, fib = 0;
+    for (int i = 1; ; i++) {
+        if (i == n) {
+            return b;
+        }
+        fib = a + b;
         a = b;
         b = fib;
-        bool isPrime = true;
-        for (int j = 2; j * j <= fib; j++)
-            if (fib % j == 0) {
-                isPrime = false;
-                break;
-            }
-        if (!isPrime) continue;
-        return fib;
+        if (!isPrime(fib)) {
+            continue;
+        }
+        if (i == n) {
+            return fib;
+        }
     }
 }
 
-int main() {
-    int n;
-    cout << "Enter the number: ";
-    cin >> n;
-    cout << "The " << n << "th prime Fibonacci number is: " << prime_fib(n) << endl;
-    return 0;
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
