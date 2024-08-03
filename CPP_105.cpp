df42a37a156140bd0500f86101998050a6103fb8
beep boop <githubbot@vadim.me>
2024-08-03 02:36:32 +0200

    Solution 41 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/551uy3ii Prompt:       |                              ^~~~~~ bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp:48:37: error: ‘string’ was not declared  in this scope    48 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp:48:44: error: ‘b’ was not declared in t his scope    48 | bool issame(vector<string> a,vector<string>b){       |                                            ^ bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp:48:45: error: expression list treated a s compound expression in initializer [-fpermissive]    48 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp:47: bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp: In function ‘int main()’: bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp:57:20: error: ‘by_length’ was not decla red in this scope    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |                    ^~~~~~~~~ bbfb9df1-675c-4e6e-99f1-91c45a993a10.cpp:57:19: error: ‘issame’ cannot be used a s a function    57 |     assert (issame(by_length({9, 4, 8}) , {"Nine", "Eight", "Four"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -1,37 +1,47 @@
-vector<string> by_length(vector<int> arr) {
-    vector<string> result;
+#include <vector>
+#include <algorithm>
+#include <map>
+
+std::vector<std::string> by_length(std::vector<int> arr) {
+    std::vector<int> temp;
     for (int i : arr) {
-        string str = "";
+        if (i >= 1 && i <= 9) {
+            temp.push_back(i);
+        }
+    }
+    sort(temp.begin(), temp.end());
+    reverse(temp.begin(), temp.end());
+    std::vector<std::string> result;
+    for (int i : temp) {
         switch (i) {
             case 1:
-                str = "One";
+                result.push_back("One");
                 break;
             case 2:
-                str = "Two";
+                result.push_back("Two");
                 break;
             case 3:
-                str = "Three";
+                result.push_back("Three");
                 break;
             case 4:
-                str = "Four";
+                result.push_back("Four");
                 break;
             case 5:
-                str = "Five";
+                result.push_back("Five");
                 break;
             case 6:
-                str = "Six";
+                result.push_back("Six");
                 break;
             case 7:
-                str = "Seven";
+                result.push_back("Seven");
                 break;
             case 8:
-                str = "Eight";
+                result.push_back("Eight");
                 break;
             case 9:
-                str = "Nine";
+                result.push_back("Nine");
                 break;
         }
-        result.push_back(str);
     }
     return result;
 }
