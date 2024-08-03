944e2d22ceb61c2f9fb16a72ac1f9ec179854c19
beep boop <githubbot@vadim.me>
2024-08-03 18:15:52 +0200

    Solution 7 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/3s9lqf4z Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,31 +1,50 @@
 int bowlingScore(string s) {
     int score = 0;
-    bool firstRollInFrame = true;
-    for (char c : s) {
-        if (c == 'X') { // strike in this frame
-            score += 10 + nextTwoFrames(s);
-            continue;
+    for (int i = 0; i < 10; i++) {
+        if (s[i] == '/') {
+            string firstHalf = s.substr(0, i);
+            string secondHalf = s.substr(i + 1);
+            int firstHalfScore = 0;
+            int j = 0;
+            while (j < strlen(firstHalf.c_str())) {
+                if (firstHalf[j] >= '1' && firstHalf[j] <= '9') {
+                    firstHalfScore += (firstHalf[j] - '0');
+                    j++;
+                } else if (firstHalf[j] == 'X') {
+                    firstHalfScore += 10;
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            int secondHalfScore = 0;
+            j = 0;
+            while (j < strlen(secondHalf.c_str())) {
+                if (secondHalf[j] >= '1' && secondHalf[j] <= '9') {
+                    secondHalfScore += (secondHalf[j] - '0');
+                    j++;
+                } else if (secondHalf[j] == 'X') {
+                    secondHalfScore += 10;
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            score += firstHalfScore + secondHalfScore;
+        } else {
+            int frameScore = 0;
+            for (int k = i; k < i+2 && k < s.length(); k++) {
+                if (s[k] >= '1' && s[k] <= '9') {
+                    frameScore += (s[k] - '0');
+                } else if (s[k] == 'X') {
+                    frameScore += 10;
+                    break;
+                } else {
+                    break;
+                }
+            }
+            score += frameScore;
         }
-        else if (c == '/') { // spare in this frame
-            score += 10 - firstRollInFrame;
-            firstRollInFrame = false;
-            continue;
-        }
-        int roll = c - '0';
-        score += roll;
-        firstRollInFrame = false;
     }
     return score;
-}
-
-int nextTwoFrames(string s) {
-    for (int i = 1; i <= 2; i++) {
-        if (s.size() < i + 2)
-            break;
-        if (s[s.size() - i - 2] == 'X') // strike in the next frame
-            return 10 + nextTwoFrames(s);
-        else if (s[s.size() - i - 2] == '/') // spare in the next frame
-            return 10 - s[s.size() - i - 1] - '0';
-    }
-    return 0;
 }
