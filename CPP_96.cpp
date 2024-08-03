4a4a9f14c58cd7a84f441cebf5dda34e8b509f62
beep boop <githubbot@vadim.me>
2024-08-04 00:21:27 +0200

    Solution 52 of CPP/96, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/20hoy66v Prompt: 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:11:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    11 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:4:6: note: ‘bool issame(std::vector<int >, std::vector<int>)’ previously defined here     4 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ In file included from 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:10: 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp: In function ‘int main()’: 9959fdcb-dae4-4dcb-a84b-957b7eac448f.cpp:20:20: error: ‘count_up_to’ was not dec lared in this scope    20 |     assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 2 9, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));       |                    ^~~~~~~~~~~

--- CPP_96.cpp
+++ CPP_96.cpp
@@ -1,21 +1,22 @@
 #include <vector>
-#include <cassert>
 using namespace std;
 
-vector<int> count_up_to(int n){
+vector<int> count_up_to(int n) {
     vector<int> primes;
-    for (int num = 2; num <= n; num++) {
+    
+    for (int i = 2; i <= n; i++) {
         bool is_prime = true;
-        for (int i = 2; i < num; i++) {
-            if (num % i == 0) {
+        for (int j = 2; j*j <= i; j++) {
+            if (i % j == 0) {
                 is_prime = false;
                 break;
             }
         }
         if (is_prime) {
-            primes.push_back(num);
+            primes.push_back(i);
         }
     }
+    
     return primes;
 }
 
@@ -23,16 +24,12 @@ bool issame(vector<int> a, vector<int> b){
     if (a.size() != b.size()) {
         return false;
     }
-    for (size_t i = 0; i < a.size(); i++) {
+    
+    for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
             return false;
         }
     }
-    return true;
-}
-
-int main(){
-    assert (issame(count_up_to(101) , {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}));
     
-    return 0;
+    return true;
 }
