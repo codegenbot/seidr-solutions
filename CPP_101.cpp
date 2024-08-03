31faf686183a03aa0aaea0eddceb4056ff850ba1
beep boop <githubbot@vadim.me>
2024-08-03 15:25:01 +0200

    Solution 22 of CPP/101, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/rs5fca8h Prompt: bc331d75-843c-4c94-bf9b-15c65ee63cbb.cpp:30:5: note: ‘int main()’ previously def ined here    30 | int main() {       |     ^~~~ In file included from bc331d75-843c-4c94-bf9b-15c65ee63cbb.cpp:35: bc331d75-843c-4c94-bf9b-15c65ee63cbb.cpp: In function ‘int main()’: bc331d75-843c-4c94-bf9b-15c65ee63cbb.cpp:45:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    45 |     assert (issame(words_string("ahmed     , gamal") , {"ahmed", "gamal" }));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~ bc331d75-843c-4c94-bf9b-15c65ee63cbb.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     7 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ bc331d75-843c-4c94-bf9b-15c65ee63cbb.cpp:36:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    36 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_101.cpp
+++ CPP_101.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 bool issame(const vector<string>& a, const vector<string>& b) {
-    return a == b;
+    return equal(a.begin(), a.end(), b.begin(), b.end());
 }
 
 vector<string> words_string(const string& s) {
