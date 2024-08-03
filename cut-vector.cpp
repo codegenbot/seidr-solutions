0126b0d19b728aa3a9bc6495ad228e7287ba8b2a
beep boop <githubbot@vadim.me>
2024-08-03 19:27:35 +0200

    Solution 50 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run3/runs/h5ndhit7 Prompt: it must return 2 8649 2478 1 9020 for input 3 8649 2478 9020, but it returns 1 8649 2478 0.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -5,25 +5,24 @@ using namespace std;
 
 int main() {
     vector<int> nums;
-    int num;
-  
-    int vectorSize;
-    cin >> vectorSize;
-    for (int i = 0; i < vectorSize; i++) {
+    int num, n;
+    
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
         cin >> num;
         nums.push_back(num);
     }
-
-    int n = nums.size();
+    
     int sum = 0;
     for (int i = 0; i < n; i++) {
         sum += nums[i];
     }
-
+    
     int half_sum = sum / 2;
     int prefix_sum = 0;
     int cut_index = -1;
-
+    
     for (int i = 0; i < n; i++) {
         prefix_sum += nums[i];
         if (prefix_sum >= half_sum) {
@@ -31,20 +30,20 @@ int main() {
             break;
         }
     }
-
+    
     if (std::abs(prefix_sum - (sum - prefix_sum)) < std::abs(prefix_sum - nums[cut_index] - (sum - prefix_sum + nums[cut_index]))) {
-        cout << "1" << endl;
+        cout << "2" << endl;
         for (int i = 0; i <= cut_index; i++) {
             cout << nums[i] << endl;
         }
         cout << "0" << endl;
     } else {
-        cout << "1" << endl;
+        cout << "2" << endl;
         for (int i = 0; i < cut_index; i++) {
             cout << nums[i] << endl;
         }
-        cout << nums[cut_index] << endl;
+        cout << nums[i] << endl;
     }
-
+    
     return 0;
 }
