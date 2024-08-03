5e40d0f6ce966ea2222feb5f00e781358714f1a7
beep boop <githubbot@vadim.me>
2024-08-03 14:02:39 +0200

    Solution 83 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/i2kseror Prompt:     3 | 51d62369-7665-4ddc-a38d-d1a6e6618663.cpp:38:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    38 | int main(){       |         ^~ 51d62369-7665-4ddc-a38d-d1a6e6618663.cpp:38:9: note: remove parentheses to defau lt-initialize a variable    38 | int main(){       |         ^~       |         -- 51d62369-7665-4ddc-a38d-d1a6e6618663.cpp:38:9: note: or replace parentheses with  braces to value-initialize a variable 51d62369-7665-4ddc-a38d-d1a6e6618663.cpp:38:11: error: a function-definition is not allowed here before ‘{’ token    38 | int main(){       |           ^ 51d62369-7665-4ddc-a38d-d1a6e6618663.cpp:40:2: error: expected ‘}’ at end of inp ut    40 | }       |  ^ 51d62369-7665-4ddc-a38d-d1a6e6618663.cpp:21:12: note: to match this ‘{’    21 | int main() {       |            ^

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -5,7 +5,7 @@ bool issame(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> even_odd_count(int n){
+std::vector<int> even_odd_count(int n) {
     int evens = 0, odds = 0;
     while (n > 0) {
         if (n % 2 == 0) {
