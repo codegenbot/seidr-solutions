77be3e58c650cd220d9bf7b78551986ab865642d
beep boop <githubbot@vadim.me>
2024-08-03 12:32:03 +0200

    Solution 15 of CPP/146, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/crc9dynb Prompt: 74e35488-5381-40b3-a6c1-f8c883e70f02.cpp:1:1: error: ‘Here’ does not name a type     1 | Here is the solution:       | ^~~~ In file included from 74e35488-5381-40b3-a6c1-f8c883e70f02.cpp:13: 74e35488-5381-40b3-a6c1-f8c883e70f02.cpp: In function ‘int main()’: 74e35488-5381-40b3-a6c1-f8c883e70f02.cpp:15:13: error: ‘specialFilter’ was not d eclared in this scope    15 |     assert (specialFilter({}) == 0                   );       |             ^~~~~~~~~~~~~

--- CPP_146.cpp
+++ CPP_146.cpp
@@ -1,10 +1,1 @@
-Find the Numbers That Are Special
-
-int specialFilter(vector<int> nums) {
-    int count = 0;
-    for (int num : nums) {
-        if (abs(num) > 10 && (num % 10) % 2 != 0 && (num / 10) % 10 % 2 != 0) {
-            count++;
-        }
-    }
-    return count;
+Count All Numbers in an Array Having Odd Sum of Digits at Unit's Place
