9da085a6a50a4f93ac454ea212e6a4b1931f564b
beep boop <githubbot@vadim.me>
2024-08-03 04:07:05 +0200

    Solution 60 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/to93j72u Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,29 +1,17 @@
 ```
-def bowling_score(s):
+def bowling_score(gutter_balls):
     score = 0
+    roll = 0
     frame = 1
-    for i in range(0, len(s), 2):
-        if s[i].isdigit() and s[i+1].isdigit():
-            if int(s[i]) + int(s[i+1]) == 10:
-                if frame < 9:
-                    score += 10
-                else:
-                    if s[i-1].isdigit() and s[i-1] != 'X':
-                        score += 10
-                    elif i > 0 and s[i-2].isdigit() and int(s[i-2]) + int(s[i-1]) == 10:
-                        score += 20
-            else:
-                if frame < 9:
-                    score += int(s[i]) + int(s[i+1])
-                else:
-                    if i > 0 and s[i-1].isdigit() and int(s[i-1]) + int(s[i]) == 10:
-                        score += 20
-        elif s[i] == 'X':
-            score += 10
+    for ball in gutter_balls:
+        if ball == 'X':
+            score += 10 + (10 - roll)
+            roll = 0
             frame += 1
-        elif s[i] == '/':
-            if i > 0 and s[i-1].isdigit():
-                score += 10 - int(s[i-1])
-            else:
-                score += 10
+        elif ball == '/':
+            score += 10 - roll
+            roll = 0
+            frame += 1
+        else:
+            roll += int(ball)
     return score
