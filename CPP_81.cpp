8e294f18f02cc24ffdbffd52875a740e0bef3d18
beep boop <githubbot@vadim.me>
2024-08-03 13:33:19 +0200

    Solution 9 of CPP/81, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run4/runs/vssdqk02 Prompt:       |                              ^~~~~~ a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp:22:37: error: ‘string’ was not declared  in this scope    22 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp:22:44: error: ‘b’ was not declared in t his scope    22 | bool issame(vector<string> a,vector<string>b){       |                                            ^ a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp:22:45: error: expression list treated a s compound expression in initializer [-fpermissive]    22 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp:21: a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp: In function ‘int main()’: a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp:31:20: error: ‘numerical_letter_grade’ was not declared in this scope    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |                    ^~~~~~~~~~~~~~~~~~~~~~ a4f89d9d-f3d7-4e30-9e43-c313e338997b.cpp:31:19: error: ‘issame’ cannot be used a s a function    31 |     assert (issame(numerical_letter_grade({0, 0.7}) , {"E", "D-"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_81.cpp
+++ CPP_81.cpp
@@ -4,18 +4,6 @@
 
 bool issame(vector<string> a, vector<string> b);
 
-bool issame(vector<string> a, vector<string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
 vector<string> numerical_letter_grade(vector<float> grades) {
     vector<string> letter_grades;
     for (float gpa : grades) {
