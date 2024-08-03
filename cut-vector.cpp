8bc2e168fb62630f8e2672768c43a872dcc75c55
beep boop <githubbot@vadim.me>
2024-08-03 18:33:12 +0200

    Solution 13 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/hgz8l38l Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,61 +1,26 @@
 #include <vector>
 using namespace std;
 
-pair<vector<int>, vector<int>> cutVector(vector<int> v) {
+vector<int> cutVector(vector<int>& nums) {
     int minDiff = INT_MAX;
-    int pos = -1;
+    int idx = -1;
     
-    for (int i = 0; i < v.size(); i++) {
-        int sumLeft = 0, sumRight = 0;
-        
-        for (int j = 0; j <= i; j++) {
-            sumLeft += v[j];
-        }
-        
-        for (int j = i + 1; j < v.size(); j++) {
-            sumRight += v[j];
-        }
-        
-        int diff = abs(sumLeft - sumRight);
-        if (diff < minDiff) {
-            minDiff = diff;
-            pos = i;
+    for (int i = 0; i < nums.size() - 1; i++) {
+        if (abs(nums[i] - nums[i + 1]) <= minDiff) {
+            minDiff = abs(nums[i] - nums[i + 1]);
+            idx = i;
         }
     }
     
     vector<int> left, right;
-    for (int i = 0; i <= pos; i++) {
-        left.push_back(v[i]);
+    
+    for (int i = 0; i < idx; i++) {
+        left.push_back(nums[i]);
     }
-    for (int i = pos + 1; i < v.size(); i++) {
-        right.push_back(v[i]);
+    
+    for (int i = idx; i <= nums.size() - 1; i++) {
+        right.push_back(nums[i]);
     }
     
     return {left, right};
-}
-
-int main() {
-    vector<int> input;
-    int n;
-    cin >> n;
-    
-    while (n--) {
-        int x;
-        cin >> x;
-        input.push_back(x);
-    }
-    
-    pair<vector<int>, vector<int>> output = cutVector(input);
-    
-    for (int i : output.first) {
-        cout << i << " ";
-    }
-    cout << "\n";
-    
-    for (int i : output.second) {
-        cout << i << " ";
-    }
-    cout << "\n";
-    
-    return 0;
 }
