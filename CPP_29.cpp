bbfafac0efbd690bcb18e14cd13950427d9b32c3
beep boop <githubbot@vadim.me>
2024-08-03 08:17:08 +0200

    Solution 31 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nh7nsgeg Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 920459de-4142-4a87-aa3d-25c8fb912bee.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 920459de-4142-4a87-aa3d-25c8fb912bee.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 920459de-4142-4a87-aa3d-25c8fb912bee.cpp:10: 920459de-4142-4a87-aa3d-25c8fb912bee.cpp: In function ‘int main()’: 920459de-4142-4a87-aa3d-25c8fb912bee.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 920459de-4142-4a87-aa3d-25c8fb912bee.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -1,12 +1,13 @@
+```cpp
 #include <vector>
-#include <string>
+#include "issame_function.h"
 
-bool issame(vector<string> str1, vector<string> str2) {
-    if(str1.size() != str2.size()) {
+bool issame(vector<string> v1, vector<string> v2) {
+    if (v1.size() != v2.size()) {
         return false;
     }
-    for(int i = 0; i < str1.size(); i++) {
-        if(str1[i] != str2[i]) {
+    for (int i = 0; i < v1.size(); i++) {
+        if (v1[i] != v2[i]) {
             return false;
         }
     }
