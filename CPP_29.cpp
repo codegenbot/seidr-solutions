8a296e0172bb5d627aceaf86dec15a6df036e1dd
beep boop <githubbot@vadim.me>
2024-08-03 08:14:31 +0200

    Solution 15 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run6/runs/nh7nsgeg Prompt:    11 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ fcc70ed2-2d05-4c96-af43-b7801b3bd0a8.cpp:11:44: error: ‘b’ was not declared in t his scope    11 | bool issame(vector<string> a,vector<string>b){       |                                            ^ fcc70ed2-2d05-4c96-af43-b7801b3bd0a8.cpp:11:45: error: expression list treated a s compound expression in initializer [-fpermissive]    11 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from fcc70ed2-2d05-4c96-af43-b7801b3bd0a8.cpp:10: fcc70ed2-2d05-4c96-af43-b7801b3bd0a8.cpp: In function ‘int main()’: fcc70ed2-2d05-4c96-af43-b7801b3bd0a8.cpp:20:20: error: ‘filter_by_prefix’ was no t declared in this scope    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ fcc70ed2-2d05-4c96-af43-b7801b3bd0a8.cpp:20:19: error: ‘issame’ cannot be used a s a function    20 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -5,11 +5,16 @@ bool issame(vector<string> strings1, vector<string> strings2){
     return strings1 == strings2;
 }
 
-std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix){
+std::vector<std::string> filter_by_prefix(std::vector<std::string> strings, std::string prefix) {
     std::vector<std::string> result;
-    for(string s : strings){
-        if(s.find(prefix) == 0)
+    for (std::string s : strings) {
+        if (s.find(prefix) == 0)
             result.push_back(s);
     }
     return result;
+}
+
+int main() {
+    assert(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxxAAA", "xxx"}, "xxx") ==  {"xxx", "xxxAAA", "xxx"});
+    return 0;
 }
