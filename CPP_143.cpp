8f1f9c6ff7a7b7fcbb12cc4de5c8faec0f3ff998
beep boop <githubbot@vadim.me>
2024-08-03 04:29:04 +0200

    Solution 27 of CPP/143, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-llama3-run5/runs/19cvkdhi Prompt: da2f535d-5687-4b5f-8a77-ae225c0fde6e.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string words_in_sentence(string sentence){       | ^~~~~~ In file included from da2f535d-5687-4b5f-8a77-ae225c0fde6e.cpp:24: da2f535d-5687-4b5f-8a77-ae225c0fde6e.cpp: In function ‘int main()’: da2f535d-5687-4b5f-8a77-ae225c0fde6e.cpp:26:13: error: ‘words_in_sentence’ was n ot declared in this scope    26 |     assert (words_in_sentence("here is") == "is");       |             ^~~~~~~~~~~~~~~~~

--- CPP_143.cpp
+++ CPP_143.cpp
@@ -9,28 +9,22 @@ bool isPrime(int n){
     return true;
 
 int main() {
-    string words_in_sentence(string sentence){
-        string result = "";
-        int len = 0;
-        for(int i = 0; i < sentence.size(); i++){
-            if(sentence[i] == ' '){
-                if(isPrime(len)) result += sentence.substr(len - (i + 1), i - len) + " ";
-                len = 0;
-            }else{
-                len++;
-            }
+    std::string sentence;
+    std::cout << "Enter a sentence: ";
+    std::cin >> sentence;
+
+    std::string result = "";
+    int len = 0;
+    for(int i = 0; i < sentence.size(); i++){
+        if(sentence[i] == ' '){
+            if(isPrime(len)) result += sentence.substr(len - (i + 1), i - len) + " ";
+            len = 0;
+        }else{
+            len++;
         }
-        if(isPrime(len)) result += sentence.substr(len - len, len);
-        return result;
     }
-
-    string input;
-    cout << "Enter a sentence: ";
-    getline(cin, input);
-
-    string output = words_in_sentence(input);
-
-    cout << "Output: " << output << endl;
+    if(isPrime(len)) result += sentence.substr(len - len, len);
+    std::cout << words_in_sentence(sentence) << std::endl;
 
     return 0;
 }
