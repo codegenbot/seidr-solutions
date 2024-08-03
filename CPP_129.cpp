7e30311b70b7b486271298bdb328080027eb17da
beep boop <githubbot@vadim.me>
2024-08-03 08:47:18 +0200

    Solution 20 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run3/runs/2k819xey Prompt:       |                           ^~~~~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:51:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:51:34: error: expected primary-expressi on before ‘int’    51 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:59:5: error: redefinition of ‘int main( )’    59 | int main(){       |     ^~~~ b7bc1706-e39b-4acf-ae94-f34c7c49d814.cpp:45:5: note: ‘int main()’ previously def ined here    45 | int main() {       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -43,6 +43,6 @@ std::vector<int> minPath(std::vector<std::vector<int>> grid, int k) {
 }
 
 int main() {
-    assert(issame(minPath({{1, 3}, {3, 2}}, 10), {1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
+    assert(issame(minPath({{1, 3}, {3, 2}}, 10), std::vector<int>{1, 3, 1, 3, 1, 3, 1, 3, 1, 3}));
     return 0;
 }
