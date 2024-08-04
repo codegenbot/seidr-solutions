c09e489ec5d99fb8cc8adb633a868a4fcbeeb223
beep boop <githubbot@vadim.me>
2024-08-04 02:23:07 +0200

    Solution 38 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ygzmj6z6 Prompt: on before ‘int’    35 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ cc15d00c-fac3-4b1f-83fc-79a8cbf81ea9.cpp:35:39: error: expression list treated a s compound expression in initializer [-fpermissive]    35 | bool issame(vector<int> a,vector<int>b){       |                                       ^ cc15d00c-fac3-4b1f-83fc-79a8cbf81ea9.cpp:43:5: error: redefinition of ‘int main( )’    43 | int main(){       |     ^~~~ cc15d00c-fac3-4b1f-83fc-79a8cbf81ea9.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main(){       |     ^~~~ In file included from cc15d00c-fac3-4b1f-83fc-79a8cbf81ea9.cpp:34: cc15d00c-fac3-4b1f-83fc-79a8cbf81ea9.cpp: In function ‘int main()’: cc15d00c-fac3-4b1f-83fc-79a8cbf81ea9.cpp:44:19: error: ‘issame’ cannot be used a s a function    44 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,6 +1,6 @@
 #include <vector>
-#include <climits>
 #include <cassert>
+#include <climits>
 
 bool is_same(std::vector<int> a, std::vector<int> b){
     return a == b;
@@ -9,7 +9,7 @@ bool is_same(std::vector<int> a, std::vector<int> b){
 std::vector<int> largest_smallest_integers(std::vector<int> lst){
     int largest_negative = INT_MIN;
     int smallest_positive = INT_MAX;
-    
+
     for (int num : lst) {
         if (num < 0 && num > largest_negative) {
             largest_negative = num;
@@ -17,7 +17,7 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst){
             smallest_positive = num;
         }
     }
-    
+
     return {largest_negative, smallest_positive};
 }
 
@@ -26,11 +26,8 @@ int main(){
     std::vector<int> expected_output = {-3, 1};
 
     std::vector<int> output = largest_smallest_integers(input);
-    
+
     assert(is_same(output, expected_output));
 
-    // Additional test
-    assert(is_same(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-3, 1}));
-    
     return 0;
 }
