6783af1d3523801679b6969f60966c7c1a705c7b
beep boop <githubbot@vadim.me>
2024-08-04 02:45:54 +0200

    Solution 5 of CPP/148, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jy7u0nxp Prompt:       |                              ^~~~~~ d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp:30:37: error: ‘string’ was not declared  in this scope    30 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp:30:44: error: ‘b’ was not declared in t his scope    30 | bool issame(vector<string> a,vector<string>b){       |                                            ^ d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp:30:45: error: expression list treated a s compound expression in initializer [-fpermissive]    30 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp:29: d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp: In function ‘int main()’: d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp:39:20: error: ‘bf’ was not declared in this scope    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |                    ^~ d977f362-bdde-4e4f-9b4e-36123534c8f7.cpp:39:19: error: ‘issame’ cannot be used a s a function    39 |     assert (issame(bf("Jupiter", "Makemake") , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -3,9 +3,33 @@
 #include <algorithm>
 
 bool issame(vector<string> a, vector<string> b) {
-    // implementation of issame function
+    // Add your implementation of issame function here
 }
 
 vector<string> bf(string planet1, string planet2) {
-    // implementation of bf function
+    vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+    vector<string> result;
+
+    int index1 = -1, index2 = -1;
+    for (int i = 0; i < planets.size(); ++i) {
+        if (planets[i] == planet1) {
+            index1 = i;
+        }
+        if (planets[i] == planet2) {
+            index2 = i;
+        }
+    }
+
+    if (index1 == -1 || index2 == -1) {
+        return result;
+    }
+
+    int start = std::min(index1, index2);
+    int end = std::max(index1, index2);
+
+    for (int i = start + 1; i < end; ++i) {
+        result.push_back(planets[i]);
+    }
+
+    return result;
 }
