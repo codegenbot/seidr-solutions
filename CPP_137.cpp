4589cf22bdb5ba130b10409084dd311c0ffc419a
beep boop <githubbot@vadim.me>
2024-08-04 02:24:54 +0200

    Solution 31 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wj1o7t7c Prompt:       |     ^~~~ In file included from 781a674b-bfe8-456b-bbb8-920f665d2238.cpp:32: 781a674b-bfe8-456b-bbb8-920f665d2238.cpp: In function ‘int main()’: 781a674b-bfe8-456b-bbb8-920f665d2238.cpp:34:13: error: ‘boost’ has not been decl ared    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 781a674b-bfe8-456b-bbb8-920f665d2238.cpp:34:29: error: ‘string’ was not declared  in this scope    34 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 781a674b-bfe8-456b-bbb8-920f665d2238.cpp:34:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 781a674b-bfe8-456b-bbb8-920f665d2238.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 781a674b-bfe8-456b-bbb8-920f665d2238.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -26,6 +26,6 @@ std::any compare_one(const std::any& a, const std::any& b) {
 }
 
 int main() {
-    assert (std::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))) == "1");
+    assert(std::any_cast<std::string>(compare_one(std::string("1"), std::string("1"))).compare("1") == 0);
     return 0;
 }
