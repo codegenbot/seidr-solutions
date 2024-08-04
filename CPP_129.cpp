994b2bb5b3c950aaf5ff14befb39d8b0cdb1228f
beep boop <githubbot@vadim.me>
2024-08-04 02:13:23 +0200

    Solution 91 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/986pyvy8 Prompt:       |                           ^~~~~~ 532be955-ebe3-47c2-ac2f-08f14f9a3ea9.cpp:32:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 532be955-ebe3-47c2-ac2f-08f14f9a3ea9.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 532be955-ebe3-47c2-ac2f-08f14f9a3ea9.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 532be955-ebe3-47c2-ac2f-08f14f9a3ea9.cpp:32:34: error: expected primary-expressi on before ‘int’    32 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 532be955-ebe3-47c2-ac2f-08f14f9a3ea9.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 532be955-ebe3-47c2-ac2f-08f14f9a3ea9.cpp:18:5: note: ‘int main()’ previously def ined here    18 | int main() {       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -17,7 +17,7 @@ bool issame(std::vector<int> a, std::vector<int> b){
 
 int main() {
     std::vector<int> expectedResult{1, 3, 1, 3, 1, 3, 1, 3, 1, 3};
-    std::vector<int> result = minPath({{1, 3}, {1, 3}, {1, 3}, {1, 3}, {1, 3}}, 10);
+    std::vector<int> result = minPath({{1, 3}, {3, 2}}, 10);
 
     if (issame(result, expectedResult)){
         std::cout << "The result matches the expected result." << std::endl;
