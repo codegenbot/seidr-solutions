8b0c1f814b07d17e7489ffcae12c8eb9b197621c
beep boop <githubbot@vadim.me>
2024-08-04 02:26:02 +0200

    Solution 35 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wj1o7t7c Prompt:       |     ^~~~ In file included from 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp:33: 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp: In function ‘int main()’: 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp:35:13: error: ‘boost’ has not been decl ared    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp:35:29: error: ‘string’ was not declared  in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp:35:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4b960353-2fc9-4e0b-94eb-5bf0e65c2d21.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -16,9 +16,9 @@ std::any compare_one(const std::any& a, const std::any& b) {
             return b;
         }
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        if (std::stof(std::any_cast<std::string>(a)) > std::stof(std::any_cast<std::string>(b))) {
+        if (std::any_cast<std::string>(a).compare(std::any_cast<std::string>(b)) > 0) {
             return a;
-        } else if (std::stof(std::any_cast<std::string>(a)) < std::stof(std::any_cast<std::string>(b))) {
+        } else if (std::any_cast<std::string>(a).compare(std::any_cast<std::string>(b)) < 0) {
             return b;
         }
     }
