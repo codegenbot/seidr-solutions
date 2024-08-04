e93403a9e7cef82c69da6c8cb744366b35e1207c
beep boop <githubbot@vadim.me>
2024-08-04 02:27:24 +0200

    Solution 55 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/ygzmj6z6 Prompt: on before ‘int’    36 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 4f3a4fb9-0ccd-45fa-b7af-8caf6e16cfad.cpp:36:39: error: expression list treated a s compound expression in initializer [-fpermissive]    36 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 4f3a4fb9-0ccd-45fa-b7af-8caf6e16cfad.cpp:44:5: error: redefinition of ‘int main( )’    44 | int main(){       |     ^~~~ 4f3a4fb9-0ccd-45fa-b7af-8caf6e16cfad.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main(){       |     ^~~~ In file included from 4f3a4fb9-0ccd-45fa-b7af-8caf6e16cfad.cpp:35: 4f3a4fb9-0ccd-45fa-b7af-8caf6e16cfad.cpp: In function ‘int main()’: 4f3a4fb9-0ccd-45fa-b7af-8caf6e16cfad.cpp:45:19: error: ‘issame’ cannot be used a s a function    45 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -4,11 +4,11 @@
 
 bool is_same(std::vector<int> a, std::vector<int> b){
     if (a.size() != b.size()) return false;
-
+    
     for (size_t i = 0; i < a.size(); ++i) {
         if (a[i] != b[i]) return false;
     }
-
+    
     return true;
 }
 
