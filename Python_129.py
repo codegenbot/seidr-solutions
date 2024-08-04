754b337f48c2583f79245cb870e0743033fd6f00
beep boop <githubbot@vadim.me>
2024-08-04 03:32:28 +0200

    Solution 15 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/l9tliair Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/885df20f-a6e3-4377-a5ae-b288c1e76 fe4.py", line 22, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/885df20f-a6e3-4377-a5ae-b288c1e76 fe4.py", line 19, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ AssertionError

--- Python_129.py
+++ Python_129.py
@@ -1,7 +1,7 @@
 def minPath(grid, k):
     n = len(grid)
     start = min((grid[i][j], i, j) for i in range(n) for j in range(n))[1:]
-    path = [grid[start[0]][start[1]]]
+    path = [grid[start[0]][start[1]]
     visited = {start}
 
     while len(path) < k:
