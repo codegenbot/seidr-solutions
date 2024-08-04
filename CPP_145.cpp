a20ba5d42f15dd0ee3b9bdef3b98442d426d7c62
beep boop <githubbot@vadim.me>
2024-08-04 02:42:00 +0200

    Solution 18 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mjm5lhiy Prompt:     8 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp:8:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp:8:34: error: expected primary-expressio n before ‘int’     8 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp:7: 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp: In function ‘int main()’: 83be2f3b-0e35-414a-b431-cf521c410cd6.cpp:17:20: error: ‘order_by_points’ was not  declared in this scope    17 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -4,3 +4,8 @@
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
     return a == b;
 }
+
+int main() {
+    assert(issame({0,6,6,-76,-21,23,4}, {-76, -21, 0, 4, 23, 6, 6}));
+    return 0;
+}
