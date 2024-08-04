5052e67144b421a4bc0999e1bb5cc2b349a5e602
beep boop <githubbot@vadim.me>
2024-08-04 02:00:52 +0200

    Solution 51 of CPP/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/986pyvy8 Prompt:       |                           ^~~~~~ 32c14a6a-eb65-4605-9a31-53012b333add.cpp:22:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 32c14a6a-eb65-4605-9a31-53012b333add.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 32c14a6a-eb65-4605-9a31-53012b333add.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 32c14a6a-eb65-4605-9a31-53012b333add.cpp:22:34: error: expected primary-expressi on before ‘int’    22 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 32c14a6a-eb65-4605-9a31-53012b333add.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 32c14a6a-eb65-4605-9a31-53012b333add.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~

--- CPP_129.cpp
+++ CPP_129.cpp
@@ -15,8 +15,6 @@ std::vector<int> minPath(std::vector<std::vector<int>> grid, int k){
 }
 
 int main() {
-    std::vector<int> expectedResult{1, 3, 1, 3, 1, 3, 1, 3, 1, 3};
-    std::vector<int> result = minPath({{1, 3}, {3, 2}}, 10);
-
+    minPath({{1, 3}, {3, 2}}, 10);
     return 0;
 }
