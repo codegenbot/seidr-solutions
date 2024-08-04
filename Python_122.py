3789d40c3b1a9312caeb1f692a163d9f3b1a4f0d
beep boop <githubbot@vadim.me>
2024-08-04 03:24:50 +0200

    Solution 85 of Python/122, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/j6aa08up Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08df3ffc-92f9-4b9a-bf19-f37ff64eb dc3.py", line 13, in <module>     check(add_elements)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/08df3ffc-92f9-4b9a-bf19-f37ff64eb dc3.py", line 9, in check     add_elements([1], 1) == 1 AssertionError: This prints if this assert fails 2 (also good for debugging!)

--- Python_122.py
+++ Python_122.py
@@ -1,4 +1,4 @@
 from itertools import islice
 
 def add_elements(arr, k=0):
-    return sum(islice((num for num in arr if 10 <= num < 100), k) )
+    return sum(islice((num for num in arr if 10 <= num < 100), k))
