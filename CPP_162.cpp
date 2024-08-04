ae8eec868a1205dec99b23854ebddb17f6f5f168
beep boop <githubbot@vadim.me>
2024-08-04 03:39:05 +0200

    Solution 47 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wc5835x3 Prompt: /usr/bin/ld: /tmp/cctaOfOU.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 4f334c42-68ce-4421-a4db-378abfbc91e1.cpp:(.text+0x6c): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: 4f334c42-68ce-4421-a4db-378abfbc91e1.cpp:(.text+0x75): undefined re ference to `EVP_md5' /usr/bin/ld: 4f334c42-68ce-4421-a4db-378abfbc91e1.cpp:(.text+0x87): undefined re ference to `EVP_DigestInit' /usr/bin/ld: 4f334c42-68ce-4421-a4db-378abfbc91e1.cpp:(.text+0xb7): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: 4f334c42-68ce-4421-a4db-378abfbc91e1.cpp:(.text+0xcf): undefined re ference to `EVP_DigestFinal_ex' /usr/bin/ld: 4f334c42-68ce-4421-a4db-378abfbc91e1.cpp:(.text+0xdb): undefined re ference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -10,7 +10,7 @@ std::string string_to_md5(const std::string &text) {
 
     unsigned char digest[16];
     EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
+    EVP_DigestInit(mdctx, EVP_md5());
     EVP_DigestUpdate(mdctx, text.c_str(), text.length());
     EVP_DigestFinal_ex(mdctx, digest, NULL);
     EVP_MD_CTX_free(mdctx);
@@ -20,5 +20,5 @@ std::string string_to_md5(const std::string &text) {
         sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
     }
 
-    return std::string(mdString, 32);
+    return std::string(mdString);
 }
