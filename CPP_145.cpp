ba0e7779414db18f75e02a00615726f67f98f635
beep boop <githubbot@vadim.me>
2024-08-04 02:45:04 +0200

    Solution 32 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/mjm5lhiy Prompt: In file included from 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp:21:5: error: redefinition of ‘int main( )’    21 | int main(){       |     ^~~~ 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp:7:5: note: ‘int main()’ previously defi ned here     7 | int main() {       |     ^~~~ In file included from 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp:12: 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp: In function ‘int main()’: 55826fd8-f315-4b87-89c9-4b55f38993fa.cpp:22:20: error: ‘order_by_points’ was not  declared in this scope    22 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,12 +1,11 @@
-#include <algorithm>
-#include <cassert>
 #include <vector>
+#include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return std::is_permutation(a.begin(), a.end(), b.begin(), b.end());
+bool issame(const std::vector<int>& vec1, const std::vector<int>& vec2) {
+    return std::is_permutation(vec1.begin(), vec1.end(), vec2.begin(), vec2.end());
 }
 
 int main() {
-    assert(issame({0, 6, 6, -76, -21, 23, 4}, {-76, -21, 0, 4, 23, 6, 6}));
+    assert(issame({0,6,6,-76,-21,23,4}, {-76, -21, 0, 4, 23, 6, 6}));
     return 0;
 }
