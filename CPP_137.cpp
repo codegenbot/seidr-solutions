23f4b0657e5669cfa19708a49575f25bb3d1f7ad
beep boop <githubbot@vadim.me>
2024-08-04 02:31:16 +0200

    Solution 54 of CPP/137, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/wj1o7t7c Prompt:       |     ^~~~ In file included from 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp:33: 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp: In function ‘int main()’: 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp:35:13: error: ‘boost’ has not been decl ared    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |             ^~~~~ 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp:35:29: error: ‘string’ was not declared  in this scope    35 |     assert (boost::any_cast<string>(compare_one(string("1"), 1)) == "Non e");       |                             ^~~~~~ 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp:35:29: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 4ec508fe-c690-444e-94f3-6ab973917c4f.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~

--- CPP_137.cpp
+++ CPP_137.cpp
@@ -1,4 +1,3 @@
-
 #include <any>
 #include <string>
 #include <cassert>
@@ -17,9 +16,9 @@ std::any compare_one(const std::any& a, const std::any& b) {
             return b;
         }
     } else if (a.type() == typeid(std::string) && b.type() == typeid(std::string)) {
-        if (std::any_cast<std::string>(a) > std::any_cast<std::string>(b)) {
+        if (std::stof(std::any_cast<std::string>(a)) > std::stof(std::any_cast<std::string>(b))) {
             return a;
-        } else if (std::any_cast<std::string>(a) < std::any_cast<std::string>(b)) {
+        } else if (std::stof(std::any_cast<std::string>(a)) < std::stof(std::any_cast<std::string>(b))) {
             return b;
         }
     }
