75eaba0e4417cc25180500cfa0ef1745a2bae982
beep boop <githubbot@vadim.me>
2024-08-04 02:59:29 +0200

    Solution 34 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/73midkt4 Prompt: 66e83afa-b8af-4b67-a22a-0c36b0423cfc.cpp:24:5: note: ‘int main()’ previously def ined here    24 | int main() {       |     ^~~~ In file included from 66e83afa-b8af-4b67-a22a-0c36b0423cfc.cpp:43: 66e83afa-b8af-4b67-a22a-0c36b0423cfc.cpp: In function ‘int main()’: 66e83afa-b8af-4b67-a22a-0c36b0423cfc.cpp:53:19: error: call of overloaded ‘issam e(std::vector<std::__cxx11::basic_string<char> >, <brace-enclosed initializer li st>)’ is ambiguous    53 |     assert (issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}) , {"cc" , "dd", "aaaa", "bbbb"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~ 66e83afa-b8af-4b67-a22a-0c36b0423cfc.cpp:8:6: note: candidate: ‘bool issame(cons t std::vector<std::__cxx11::basic_string<char> >&, const std::vector<std::__cxx1 1::basic_string<char> >&)’     8 | bool issame(const vector<string>& a, const vector<string>& b) {       |      ^~~~~~ 66e83afa-b8af-4b67-a22a-0c36b0423cfc.cpp:44:6: note: candidate: ‘bool issame(std ::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_str ing<char> >)’    44 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -1,23 +1,5 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool issame(const vector<string>& a, const vector<string>& b) {
+bool are_same(const vector<string>& a, const vector<string>& b) {
     return (a == b);
-}
-
-vector<string> sorted_list_sum(const vector<string>& lst) {
-    vector<string> sorted_lst = lst;
-    sorted_lst.erase(remove_if(sorted_lst.begin(), sorted_lst.end(), [](const string& s){ return s.length() % 2 != 0; }), sorted_lst.end());
-    sort(sorted_lst.begin(), sorted_lst.end(), [](const string& a, const string& b) {
-        if (a.length() == b.length()) {
-            return a < b;
-        }
-        return a.length() < b.length();
-    });
-    return sorted_lst;
 }
 
 int main() {
@@ -33,6 +15,8 @@ int main() {
     for (const auto& str : result) {
         cout << str << endl;
     }
+
+    assert(are_same(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), vector<string>{"cc", "dd", "aaaa", "bbbb"}));
 
     return 0;
 }
