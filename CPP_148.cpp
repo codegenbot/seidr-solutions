867d32d6af70dba2da39047a695f091f3403d463
beep boop <githubbot@vadim.me>
2024-08-04 02:48:45 +0200

    Solution 15 of CPP/148, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/jy7u0nxp Prompt: cfcf3b94-c18a-459c-b74c-634c55e89478.cpp:48:6: error: redefinition of ‘bool issa me(std::vector<std::__cxx11::basic_string<char> >, std::vector<std::__cxx11::bas ic_string<char> >)’    48 | bool issame(vector<string> a,vector<string>b){       |      ^~~~~~ cfcf3b94-c18a-459c-b74c-634c55e89478.cpp:15:6: note: ‘bool issame(std::vector<st d::__cxx11::basic_string<char> >, std::vector<std::__cxx11::basic_string<char> > )’ previously defined here    15 | bool issame(vector<string> a, vector<string> b) {       |      ^~~~~~ cfcf3b94-c18a-459c-b74c-634c55e89478.cpp:56:5: error: redefinition of ‘int main( )’    56 | int main(){       |     ^~~~ cfcf3b94-c18a-459c-b74c-634c55e89478.cpp:11:5: note: ‘int main()’ previously def ined here    11 | int main() {       |     ^~~~

--- CPP_148.cpp
+++ CPP_148.cpp
@@ -7,12 +7,6 @@ using namespace std;
 
 bool issame(vector<string> a, vector<string> b);
 
-vector<string> bf(string planet1, string planet2);
-
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
-}
-
 vector<string> bf(string planet1, string planet2) {
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
     vector<string> result;
@@ -39,8 +33,4 @@ vector<string> bf(string planet1, string planet2) {
     }
 
     return result;
-}
-
-int main() {
-    assert(issame(bf("Jupiter", "Makemake"), {}));
 }
