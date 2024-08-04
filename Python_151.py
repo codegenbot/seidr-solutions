e5bae9fb887c636a12160188ed6bcbad3fa826d4
beep boop <githubbot@vadim.me>
2024-08-04 04:23:19 +0200

    Solution 43 of Python/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/s4xxyelm Prompt: 328 Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be4fc997-9522-4e26-ae56-c7ed82ee9 3c6.py", line 19, in <module>     check(double_the_difference)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/be4fc997-9522-4e26-ae56-c7ed82ee9 3c6.py", line 15, in check     double_the_difference(lst) == odd_sum                           ^^^ NameError: name 'lst' is not defined. Did you mean: 'list'?

--- Python_151.py
+++ Python_151.py
@@ -1,6 +1,6 @@
-def double_the_difference(input_list):
-    odd_sum = sum(x**2 for x in input_list if x % 2 != 0 and x >= 0 and isinstance(x, int))
-    return odd_sum * 2 if input_list else 0
+def double_the_difference(lst):
+    odd_sum = sum(x**2 for x in lst if x % 2 != 0 and x >= 0 and isinstance(x, int))
+    return odd_sum * 2 if lst else 0
 
 result = double_the_difference([3, 5, 7, -2, 9])
 print(result)
