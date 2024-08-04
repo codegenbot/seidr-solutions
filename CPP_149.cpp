ff7975f37f423041fab1b92615030725769b0d93
beep boop <githubbot@vadim.me>
2024-08-04 02:55:55 +0200

    Solution 22 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run5/runs/73midkt4 Prompt:                  from 21262acc-fcd2-49b5-9bcc-6cd92501526b.cpp:2: /usr/include/c++/11/bits/basic_string.h:785:7: note: candidate: ‘std::__cxx11::b asic_string<_CharT, _Traits, _Alloc>& std::__cxx11::basic_string<_CharT, _Traits , _Alloc>::operator=(std::initializer_list<_Tp>) [with _CharT = char; _Traits = std::char_traits<char>; _Alloc = std::allocator<char>]’   785 |       operator=(initializer_list<_CharT> __l)       |       ^~~~~~~~ /usr/include/c++/11/bits/basic_string.h:785:42: note:   no known conversion for argument 1 from ‘std::remove_reference<const std::__cxx11::basic_string<char>&>: :type’ {aka ‘const std::__cxx11::basic_string<char>’} to ‘std::initializer_list< char>’   785 |       operator=(initializer_list<_CharT> __l)       |                 ~~~~~~~~~~~~~~~~~~~~~~~~~^~~ In file included from /usr/include/c++/11/bits/stl_algobase.h:71,                  from /usr/include/c++/11/algorithm:61,                  from 21262acc-fcd2-49b5-9bcc-6cd92501526b.cpp:1: /usr/include/c++/11/bits/predefined_ops.h: At global scope: /usr/include/c++/11/bits/predefined_ops.h:187:7: warning: ‘__gnu_cxx::__ops::_It er_comp_val<_Compare>::_Iter_comp_val(__gnu_cxx::__ops::_Iter_comp_iter<_Compare >&&) [with _Compare = sorted_list_sum(const std::vector<std::__cxx11::basic_stri ng<char> >&)::<lambda(const string&, const string&)>]’ used but never defined   187 |       _Iter_comp_val(_Iter_comp_iter<_Compare>&& __comp)       |       ^~~~~~~~~~~~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -8,15 +8,22 @@ bool issame(const vector<string>& a, const vector<string>& b) {
     return (a == b);
 }
 
-vector<string> sorted_list_sum(const vector<string>& lst) {
-    lst.erase(remove_if(lst.begin(), lst.end(), [](const string& s){ return s.length() % 2 != 0; }), lst.end());
-    sort(lst.begin(), lst.end(), [](const string& a, const string& b) {
+int sorted_list_sum(const vector<string>& lst) {
+    vector<string> filtered_lst = lst;
+    filtered_lst.erase(remove_if(filtered_lst.begin(), filtered_lst.end(), [](const string& s){ return s.length() % 2 != 0; }), filtered_lst.end());
+    sort(filtered_lst.begin(), filtered_lst.end(), [](const string& a, const string& b) {
         if (a.length() == b.length()) {
             return a < b;
         }
         return a.length() < b.length();
     });
-    return lst;
+    
+    int sum = 0;
+    for (const string& str : filtered_lst) {
+        sum += stoi(str);
+    }
+    
+    return sum;
 }
 
 int main() {
