8c177dd2fc0f136ee84e030463e2999f99dc786f
beep boop <githubbot@vadim.me>
2024-08-04 02:46:20 +0200

    Solution 4 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run5/runs/oqjy1452 Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.py
+++ bowling.py
@@ -1,39 +1,31 @@
 def bowling_score(bowls):
     score = 0
     frame = 1
-    bowl_index = 0
+    i = 0
     while frame <= 10:
-        if bowls[bowl_index] == "X":
-            score += 10
-            if frame < 10:
-                score += sum(map(int, bowls[bowl_index + 1 : bowl_index + 3]))
-                bowl_index += 1
-            else:
-                score += sum(
-                    map(
-                        lambda x: (
-                            10
-                            if x == "X"
-                            else (
-                                int(x) if x != "/" else 10 - int(bowls[bowl_index - 1])
-                            )
-                        ),
-                        bowls[bowl_index + 1 :],
-                    )
-                )
-        elif bowls[bowl_index + 1] == "/":
-            score += 10
-            if frame < 10:
-                score += int(bowls[bowl_index + 2])
+        if bowls[i] == 'X':
+            score += 10 + bonus(bowls, i + 1, 2)
+            i += 1
+        elif bowls[i + 1] == '/':
+            score += 10 + bonus(bowls, i + 2, 1)
+            i += 2
         else:
-            score += int(bowls[bowl_index]) + int(bowls[bowl_index + 1])
-        if bowls[bowl_index] != "X":
-            bowl_index += 2
-        else:
-            bowl_index += 1
+            score += int(bowls[i]) + int(bowls[i + 1])
+            i += 2
         frame += 1
     return score
 
+def bonus(bowls, index, num):
+    bonus_score = 0
+    for i in range(num):
+        if bowls[index] == 'X':
+            bonus_score += 10
+        elif bowls[index] == '/':
+            bonus_score += 10 - int(bowls[index - 1])
+        else:
+            bonus_score += int(bowls[index])
+        index += 1
+    return bonus_score
 
 bowls = input()
 print(bowling_score(bowls))
