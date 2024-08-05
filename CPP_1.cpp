d477a46f84eb57f4219fe18c180685b30671a3f8
beep boop <githubbot@vadim.me>
2024-08-05 19:54:35 +0200

    Solution 8 of CPP/1, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/cxu5md5u Prompt:    27 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 28b4b0b4-95d2-4532-86ea-95abb58ee11d.cpp:27:44: error: ‘b’ was not declared in t his scope    27 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 28b4b0b4-95d2-4532-86ea-95abb58ee11d.cpp:27:45: error: expression list treated a s compound expression in initializer [-fpermissive]    27 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 28b4b0b4-95d2-4532-86ea-95abb58ee11d.cpp:26: 28b4b0b4-95d2-4532-86ea-95abb58ee11d.cpp: In function ‘int main()’: 28b4b0b4-95d2-4532-86ea-95abb58ee11d.cpp:36:20: error: ‘separate_paren_groups’ w as not declared in this scope    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 28b4b0b4-95d2-4532-86ea-95abb58ee11d.cpp:36:19: error: ‘issame’ cannot be used a s a function    36 |     assert (issame(separate_paren_groups("( ) (( )) (( )( ))") ,{"()", " (())", "(()())"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~

--- CPP_1.cpp
+++ CPP_1.cpp
@@ -1,16 +1,18 @@
-vector<string> separate_paren_groups(string paren_string);
-
-bool issame(const vector<string>& v1, const vector<string>& v2) {
-    if (v1.size() != v2.size()) {
+bool issame(vector<string> group1, vector<string> group2) {
+    if (group1.size() != group2.size()) {
         return false;
     }
-    for (size_t i = 0; i < v1.size(); ++i) {
-        if (v1[i] != v2[i]) {
+    
+    for (int i = 0; i < group1.size(); i++) {
+        if (group1[i] != group2[i]) {
             return false;
         }
     }
+    
     return true;
 }
+
+vector<string> separate_paren_groups(string paren_string);
 
 vector<string> separate_paren_groups(string paren_string) {
     vector<string> result;
