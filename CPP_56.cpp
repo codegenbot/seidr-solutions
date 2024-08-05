4b71623b567d5eaff2e6d247d99732d48604f760
beep boop <githubbot@vadim.me>
2024-08-05 11:32:51 +0200

    Solution 6 of CPP/56, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/upw0ce3g Prompt: e8a19bc0-70bf-4dac-a47b-cd9373c3642e.cpp:2:5: error: expected unqualified-id bef ore ‘for’     2 |     for (char bracket : brackets) {       |     ^~~ e8a19bc0-70bf-4dac-a47b-cd9373c3642e.cpp:12:5: error: expected unqualified-id be fore ‘return’    12 |     return count == 0;       |     ^~~~~~ e8a19bc0-70bf-4dac-a47b-cd9373c3642e.cpp:13:1: error: expected declaration befor e ‘}’ token    13 | }       | ^ In file included from e8a19bc0-70bf-4dac-a47b-cd9373c3642e.cpp:15: e8a19bc0-70bf-4dac-a47b-cd9373c3642e.cpp: In function ‘int main()’: e8a19bc0-70bf-4dac-a47b-cd9373c3642e.cpp:17:18: error: ‘correct_bracketing’ was not declared in this scope    17 |     assert (not (correct_bracketing("<><><<><>><>>><>")));       |                  ^~~~~~~~~~~~~~~~~~

--- CPP_56.cpp
+++ CPP_56.cpp
@@ -1,9 +1,9 @@
 bool correct_bracketing(const std::string& brackets) {
     int count = 0;
-    for (char c : brackets) {
-        if (c == '<') {
+    for (char bracket : brackets) {
+        if (bracket == '<') {
             count++;
-        } else if (c == '>') {
+        } else if (bracket == '>') {
             count--;
         }
         if (count < 0) {
