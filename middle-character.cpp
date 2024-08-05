c2751b2daee478fdf8ae0c2ed4cc2ed0e5ee7444
beep boop <githubbot@vadim.me>
2024-08-05 16:21:03 +0200

    Solution 9 of middle-character, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/7ussr22j Prompt: 895b0a4d-22ec-4125-a0e7-bf79b8e75a4e.cpp: In function ‘int main()’: 895b0a4d-22ec-4125-a0e7-bf79b8e75a4e.cpp:2:5: error: ‘string’ was not declared i n this scope     2 |     string s;       |     ^~~~~~ 895b0a4d-22ec-4125-a0e7-bf79b8e75a4e.cpp:3:5: error: ‘cin’ was not declared in t his scope     3 |     cin >> s;       |     ^~~ 895b0a4d-22ec-4125-a0e7-bf79b8e75a4e.cpp:3:12: error: ‘s’ was not declared in th is scope     3 |     cin >> s;       |            ^ 895b0a4d-22ec-4125-a0e7-bf79b8e75a4e.cpp:7:9: error: ‘cout’ was not declared in this scope     7 |         cout << s.substr(n/2 - 1, 2);       |         ^~~~ 895b0a4d-22ec-4125-a0e7-bf79b8e75a4e.cpp:9:9: error: ‘cout’ was not declared in this scope     9 |         cout << s[n/2];       |         ^~~~

--- middle-character.cpp
+++ middle-character.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
-
+    
     int n = s.size();
     if (n % 2 == 0) {
         cout << s.substr(n/2 - 1, 2);
     } else {
         cout << s[n/2];
     }
-
+    
     return 0;
 }
