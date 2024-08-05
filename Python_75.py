e5f9c9fe01493b3b4b25bd55377c85ef27f86577
beep boop <githubbot@vadim.me>
2024-08-05 17:12:42 +0200

    Solution 2 of Python/75, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/j4y4jo4x Prompt: Complete the following code given the task description and function signature.

--- Python_75.py
+++ Python_75.py
@@ -1,7 +1,7 @@
 def is_multiply_prime(a):
-    for i in range(2, int(a/2)+1):
+    for i in range(2, a):
         if a % i == 0:
-            for j in range(2, int(i/2)+1):
-                if i % j == 0:
-                    return False
-    return True
+            for j in range(2, int(a/i)+1):
+                if (a/i) % j == 0:
+                    return True
+    return False
