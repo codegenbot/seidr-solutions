65b7abf469b52e6dc714f20672ccd42011af94f1
beep boop <githubbot@vadim.me>
2024-08-05 12:13:43 +0200

    Solution 91 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/m29x1t1k Prompt: bdd03b56-0aa9-4287-bfc1-d1dcd9361e8d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -3,28 +3,34 @@
 int bowlingScore(string s) {
     int score = 0;
     int currentFrame = 1;
-    for (int i = 0; i <= s.length(); i++) {
-        if (i < s.length() - 1 && s[i] == 'X') {
-            score += 10;
-            currentFrame++;
-            if (currentFrame < 10) {
-                if (s[i + 2] == '/' || s[i + 2] == 'X') {
-                    i++; // skip the next pin
-                    continue;
-                }
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '/') {
+            string firstPart = s.substr(0, i);
+            string secondPart = s.substr(i + 1);
+            int firstPin = stoi(firstPart);
+            int secondPin = stoi(secondPart);
+            if (firstPin + secondPin == 10) {
+                score += 10;
+                currentFrame++;
+            } else {
+                score += firstPin + secondPin;
+                currentFrame++;
             }
-        } else if (i < s.length() - 1 && s.substr(i, 2) == "X/") {
+        } else if (s[i] == 'X') {
             score += 10;
             currentFrame++;
-            i++; // move on to the next frame
-        } else if (s[i] == '/') {
-            int firstPin =stoi(s.substr(0, i)) + stoi(s.substr(i + 1));
-            score += firstPin;
-            currentFrame++;
-        } else if (s[i] != ' ') {
-            int pins = stoi(s.substr(i));
-            score += pins;
-            currentFrame++;
+        } else if (s[i] == ' ') {
+            i++; // skip the space
+        } else {
+            string pinsPart = s.substr(i);
+            int pins = stoi(pinsPart);
+            if (pins < 10) {
+                score += pins;
+                currentFrame++;
+            } else {
+                score += 10 + (stoi(s.substr(i + 1, 1)));
+                currentFrame++;
+            }
         }
     }
     return score;
