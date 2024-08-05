1b61e5c27f46b9b7274d3cafe423692b502e6900
beep boop <githubbot@vadim.me>
2024-08-05 12:32:50 +0200

    Solution 13 of CPP/85, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2zlrydu6 Prompt: 5bf76569-e746-4fac-9608-3d4f3a809084.cpp: In function ‘int main()’: 5bf76569-e746-4fac-9608-3d4f3a809084.cpp:14:5: error: ‘assert’ was not declared in this scope    14 |     assert(add({4, 4, 6, 8}) == 12);       |     ^~~~~~ 5bf76569-e746-4fac-9608-3d4f3a809084.cpp:2:1: note: ‘assert’ is defined in heade r ‘<cassert>’; did you forget to ‘#include <cassert>’?     1 | #include <vector>   +++ |+#include <cassert>     2 | 5bf76569-e746-4fac-9608-3d4f3a809084.cpp: At global scope: 5bf76569-e746-4fac-9608-3d4f3a809084.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 5bf76569-e746-4fac-9608-3d4f3a809084.cpp:13:5: note: ‘int main()’ previously def ined here    13 | int main() {       |     ^~~~

--- CPP_85.cpp
+++ CPP_85.cpp
@@ -3,7 +3,7 @@
 
 int add(std::vector<int> lst){
     int sum = 0;
-    for(int i = 0; i < lst.size(); i += 2){
+    for(int i = 1; i < lst.size(); i += 2){
         if(lst[i] % 2 == 0){
             sum += lst[i];
         }
