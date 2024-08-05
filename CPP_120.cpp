2a9f3d3e77c77595eb304881c0eb1fb9028a3bca
beep boop <githubbot@vadim.me>
2024-08-05 14:18:51 +0200

    Solution 18 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lh992irp Prompt: In file included from 372cd0b1-f534-44ee-afca-5c233b63864e.cpp:17: 372cd0b1-f534-44ee-afca-5c233b63864e.cpp: In function ‘int main()’: 372cd0b1-f534-44ee-afca-5c233b63864e.cpp:27:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    27 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 372cd0b1-f534-44ee-afca-5c233b63864e.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b){       |      ^~~~~~ 372cd0b1-f534-44ee-afca-5c233b63864e.cpp:18:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    18 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -4,18 +4,13 @@
 
 using namespace std;
 
-bool issame(vector<int> a, vector<int> b) {
-    return a == b;
-}
-
-vector<int> maximum(vector<int> arr, int k) {
+vector<int> maximum(vector<int> arr, int k){
     sort(arr.begin(), arr.end(), greater<int>());
     arr.resize(k);
     return arr;
 }
 
-int main() {
+int main(){
     assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), vector<int>{}));
-    
     return 0;
 }
