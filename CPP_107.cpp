b5459457180a94f8c5e0a8ee48c01c3e5f4b4dbf
beep boop <githubbot@vadim.me>
2024-08-05 13:28:51 +0200

    Solution 3 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/e1czyo3k Prompt:       |                    ^~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:17: 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp: In function ‘int main()’: 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,22 +1,22 @@
 #include <vector>
 
-bool issame(vector<int> vec1, vector<int> vec2){
-    return vec1 == vec2;
+bool issame(const std::vector<int>& A, const std::vector<int>& B) {
+    return A == B;
 }
 
-vector<int> even_odd_palindrome(int n){
-    vector<int> count{0, 0};
+std::vector<int> even_odd_palindrome(int n) {
+    int even = 0, odd = 0;
     for(int i = 1; i <= n; ++i){
-        string num = to_string(i);
-        string rev_num = num;
-        reverse(rev_num.begin(), rev_num.end());
+        std::string num = std::to_string(i);
+        std::string rev_num = num;
+        std::reverse(rev_num.begin(), rev_num.end());
         if(num == rev_num){
             if(i % 2 == 0){
-                count[0]++;
+                even++;
             } else {
-                count[1]++;
+                odd++;
             }
         }
     }
-    return count;
+    return {even, odd};
 }
