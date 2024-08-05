d4f8b46f58a7f4f1260941b4df7b48ae139b7254
beep boop <githubbot@vadim.me>
2024-08-05 11:28:50 +0200

    Solution 22 of CPP/34, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/cepmjknc Prompt:       |                                  ^~~ 790efd8e-6a40-4f9b-b2bc-e1f2a28450ff.cpp:29:5: error: redefinition of ‘int main( )’    29 | int main(){       |     ^~~~ 790efd8e-6a40-4f9b-b2bc-e1f2a28450ff.cpp:15:5: note: ‘int main()’ previously def ined here    15 | int main() {       |     ^~~~ In file included from 790efd8e-6a40-4f9b-b2bc-e1f2a28450ff.cpp:20: 790efd8e-6a40-4f9b-b2bc-e1f2a28450ff.cpp: In function ‘int main()’: 790efd8e-6a40-4f9b-b2bc-e1f2a28450ff.cpp:30:20: error: ‘unique’ was not declared  in this scope; did you mean ‘std::unique’?    30 |     assert (issame(unique({5, 3, 5, 2, 3, 3, 9, 0, 123}) , {0, 2, 3, 5, 9, 123}));       |                    ^~~~~~ In file included from /usr/include/c++/11/algorithm:74,                  from 790efd8e-6a40-4f9b-b2bc-e1f2a28450ff.cpp:2: /usr/include/c++/11/pstl/glue_algorithm_defs.h:234:1: note: ‘std::unique’ declar ed here   234 | unique(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter ator __last);       | ^~~~~~

--- CPP_34.cpp
+++ CPP_34.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <cassert>
@@ -9,11 +9,24 @@ bool issame(const std::vector<int>& a, const std::vector<int>& b) {
 
 std::vector<int> removeDuplicates(std::vector<int>& l) {
     std::sort(l.begin(), l.end());
-    l.erase(std::unique(l.begin(), l.end()), l.end());
+    l.erase(std::std::unique(l.begin(), l.end()), l.end());
     return l;
 }
 
 int main() {
-    assert(issame(removeDuplicates({5, 3, 5, 2, 3, 3, 9, 0, 123}), {0, 2, 3, 5, 9, 123}));
+    std::vector<int> input;
+    int num;
+    while (std::cin >> num) {
+        input.push_back(num);
+    }
+    
+    std::vector<int> result = removeDuplicates(input);
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    
+    assert(issame(result, {0, 2, 3, 5, 9, 123}));
+    
     return 0;
 }
