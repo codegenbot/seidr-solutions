6fb0553e19a2a93f0a9772ef886b9e9ebfc8911c
beep boop <githubbot@vadim.me>
2024-08-05 14:20:02 +0200

    Solution 74 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jpk1psr4 Prompt:       |           ^~~~~~ In file included from 37435c1b-e60f-4805-8165-60293ad735a4.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 37435c1b-e60f-4805-8165-60293ad735a4.cpp:49:37: error: ‘string’ was not declared  in this scope    49 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 37435c1b-e60f-4805-8165-60293ad735a4.cpp:49:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 37435c1b-e60f-4805-8165-60293ad735a4.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 37435c1b-e60f-4805-8165-60293ad735a4.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 37435c1b-e60f-4805-8165-60293ad735a4.cpp:49:44: error: ‘b’ was not declared in t his scope    49 | bool issame(vector<string> a,vector<string>b){       |                                            ^

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -40,3 +40,7 @@ std::vector<std::string> select_words(std::string words, int index) {
 
     return selected_words;
 }
+
+assert(issame(select_words("a b c d e f", 1), std::vector<std::string>{"b", "c", "d", "f"}));
+
+return 0;
