d52a348508443835574381ff9301a38a22ae5c73
beep boop <githubbot@vadim.me>
2024-08-05 23:15:20 +0200

    Solution 2 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/yd1li0xv Prompt: ared in this scope    14 |         int left_sum = accumulate(nums.begin(), nums.begin() + i, 0);       |                        ^~~~~~~~~~ 26d0628f-3df2-4b7c-a621-0b9e43bc4c0a.cpp:17:13: error: ‘abs’ was not declared in  this scope    17 |         if (abs(left_sum - right_sum) < diff) {       |             ^~~ 26d0628f-3df2-4b7c-a621-0b9e43bc4c0a.cpp:24:9: error: ‘cout’ was not declared in  this scope    24 |         cout << nums[i] << endl;       |         ^~~~ 26d0628f-3df2-4b7c-a621-0b9e43bc4c0a.cpp:24:28: error: ‘endl’ was not declared i n this scope    24 |         cout << nums[i] << endl;       |                            ^~~~ 26d0628f-3df2-4b7c-a621-0b9e43bc4c0a.cpp:27:5: error: ‘cout’ was not declared in  this scope    27 |     cout << endl;       |     ^~~~ 26d0628f-3df2-4b7c-a621-0b9e43bc4c0a.cpp:27:13: error: ‘endl’ was not declared i n this scope    27 |     cout << endl;       |             ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,3 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
 int main() {
     vector<int> nums;
     int num;
@@ -7,32 +15,27 @@ int main() {
     }
     
     int n = nums.size();
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += nums[i];
-    }
-    
-    int target = sum / 2;
-    int prefix_sum = 0;
+    int diff = INT_MAX;
     int idx = -1;
     
-    for (int i = 0; i < n; i++) {
-        prefix_sum += nums[i];
-        if (prefix_sum >= target) {
+    for (int i = 1; i < n; ++i) {
+        int left_sum = accumulate(nums.begin(), nums.begin() + i, 0);
+        int right_sum = accumulate(nums.begin() + i, nums.end(), 0);
+        
+        if (abs(left_sum - right_sum) < diff) {
+            diff = abs(left_sum - right_sum);
             idx = i;
-            break;
         }
     }
     
-    vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
-    vector<int> subvector2(nums.begin() + idx + 1, nums.end());
-    
-    for (int num : subvector1) {
-        cout << num << endl;
+    for (int i = 0; i < idx; ++i) {
+        cout << nums[i] << endl;
     }
     
-    for (int num : subvector2) {
-        cout << num << endl;
+    cout << endl;
+    
+    for (int i = idx; i < n; ++i) {
+        cout << nums[i] << endl;
     }
     
     return 0;
