8cf25511852fc8617678440c40008a5ac5cc90b2
beep boop <githubbot@vadim.me>
2024-08-05 15:40:18 +0200

    Solution 31 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0sm0c1rc Prompt: 389e3a7d-020d-41c7-b686-2c2114e7344a.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ 389e3a7d-020d-41c7-b686-2c2114e7344a.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ 389e3a7d-020d-41c7-b686-2c2114e7344a.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -4,28 +4,37 @@ int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
-    map<char, int> codeCount;
-    map<char, int> guessCount;
-
-    // Count the number of occurrences for each character in the code and the guess
+    // Count the number of correct colors in wrong positions
     for (int i = 0; i < 4; ++i) {
-        codeCount[code[i]]++;
-        guessCount[guess[i]]++;
+        if (code[i] == guess[i]) {
+            black++;
+            code[i] = ' ';
+            guess[i] = ' ';
+        }
     }
 
     // Count the number of correct colors in right positions
     for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            black++;
+        int match = 0;
+        for (int j = 0; j < 4; ++j) {
+            if (code[i] == guess[j]) {
+                match++;
+                guess[j] = ' ';
+            }
         }
+        black += match;
     }
 
-    // Count the number of correct colors in wrong positions
-    for (char c = 'A'; c <= 'F'; ++c) {
-        if (codeCount[c] > 0 && guessCount[c] > codeCount[c] - black) {
-            white += min(codeCount[c], guessCount[c] - (black == 1));
+    // Count the remaining correct colors in wrong positions
+    for (int i = 0; i < 4; ++i) {
+        int count = 0;
+        for (int j = 0; j < 4; ++j) {
+            if (code[i] == guess[j]) {
+                count++;
+            }
         }
+        white += count - black;
     }
 
-    return white, black;
+    return black, white;
 }
