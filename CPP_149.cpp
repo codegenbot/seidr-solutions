c8da7c4eafd0749357e19c7de2c831a68693f413
beep boop <githubbot@vadim.me>
2024-08-05 15:46:49 +0200

    Solution 53 of CPP/149, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/d05kc2sy Prompt:       |                                     ^~~~~~ 033ee2a6-42b1-4b3f-a2e2-f80446167f90.cpp:29:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 033ee2a6-42b1-4b3f-a2e2-f80446167f90.cpp:4: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 033ee2a6-42b1-4b3f-a2e2-f80446167f90.cpp:4: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 033ee2a6-42b1-4b3f-a2e2-f80446167f90.cpp:29:44: error: ‘b’ was not declared in t his scope    29 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 033ee2a6-42b1-4b3f-a2e2-f80446167f90.cpp:37:5: error: redefinition of ‘int main( )’    37 | int main(){       |     ^~~~ 033ee2a6-42b1-4b3f-a2e2-f80446167f90.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main(){       |     ^~~~

--- CPP_149.cpp
+++ CPP_149.cpp
@@ -20,7 +20,7 @@ bool issame(std::vector<std::string> a, std::vector<std::string> b){
     return a == b;
 }
 
-// int main(){
-//     assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), std::vector<std::string>{"cc", "dd", "aaaa", "bbbb"}));
-//     return 0;
-// }
+/* int main(){
+    assert(issame(sorted_list_sum({"aaaa", "bbbb", "dd", "cc"}), std::vector<std::string>{"cc", "dd", "aaaa", "bbbb"}));
+    return 0;
+} */
