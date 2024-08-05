9ea59f8cc116fb0d775d1b4805a47cae3587a6c4
beep boop <githubbot@vadim.me>
2024-08-05 11:57:20 +0200

    Solution 65 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/m29x1t1k Prompt: f3f320df-f4b5-4395-843b-c1f7c7a1c508.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -1,17 +1,19 @@
+```cpp
 #include <string>
 
 int bowlingScore(string s) {
     int score = 0;
-    
+    bool lastRoll = false;
+
     for (int i = 0; i < 10; i++) {
         if (s[i] == 'X') {
             score += 30;
+            lastRoll = true;
         } else if (s[i] == '/') {
-            string str = s.substr(i, 2);
-            int thisFrameScore = stoi(str) * 2;
-            if (str[1] == '0')
-                thisFrameScore++;
+            int nextCharIndex = s.find('/', i);
+            int thisFrameScore = (nextCharIndex != string::npos) ? stoi(s.substr(i, nextCharIndex - i)) * 2 : 10;
             score += thisFrameScore;
+            lastRoll = true;
         } else {
             int thisFrameScore = 0;
             for (int j = i; j < s.size(); j++) {
@@ -19,8 +21,20 @@ int bowlingScore(string s) {
                 thisFrameScore += (s[j] - '0');
             }
             score += thisFrameScore;
+            lastRoll = false;
+        }
+
+        if (!lastRoll) continue;
+
+        if (i != 9 && s[i+1] == 'X') {
+            score += 10;
+            i++;
+        } else if (s[i+1] == '/') {
+            int nextCharIndex = s.find('/', i + 1);
+            int thisFrameScore = (nextCharIndex != string::npos) ? stoi(s.substr(i + 1, nextCharIndex - i - 1)) : 10;
+            score += thisFrameScore;
         }
     }
-    
+
     return score;
 }
