7ec30d7f5d42bcaad59170ef998fcf15430f0dc1
beep boop <githubbot@vadim.me>
2024-08-05 12:38:47 +0200

    Solution 10 of CPP/88, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9eo9twhw Prompt: 51124c42-017c-43c6-bd07-7a95700020e3.cpp:41:6: error: redefinition of ‘bool issa me(std::vector<int>, std::vector<int>)’    41 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~ 51124c42-017c-43c6-bd07-7a95700020e3.cpp:24:6: note: ‘bool issame(std::vector<in t>, std::vector<int>)’ previously defined here    24 | bool issame(vector<int> a, vector<int> b){       |      ^~~~~~ 51124c42-017c-43c6-bd07-7a95700020e3.cpp:49:5: error: redefinition of ‘int main( )’    49 | int main(){       |     ^~~~ 51124c42-017c-43c6-bd07-7a95700020e3.cpp:34:5: note: ‘int main()’ previously def ined here    34 | int main(){       |     ^~~~

--- CPP_88.cpp
+++ CPP_88.cpp
@@ -1,13 +1,36 @@
+#include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
-vector<int> sort_array(vector<int> array) {
+using namespace std;
+
+vector<int> sort_array(vector<int> array);
+
+vector<int> sort_array(vector<int> array){
     if(array.empty()) return array;
-    
-    int sum = array.front() + array.back();
-    if(sum % 2 == 0)
+
+    if((array.front() + array.back()) % 2 == 0){
         sort(array.begin(), array.end(), greater<int>());
-    else
+    } else {
         sort(array.begin(), array.end());
-    
+    }
+
     return array;
+}
+
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size()) return false;
+    
+    for(int i=0; i<a.size(); i++){
+        if(a[i] != b[i]) return false;
+    }
+
+    return true;
+}
+
+int main(){
+    assert(issame(sort_array({21, 14, 23, 11}), {23, 21, 14, 11}));
+    
+    return 0;
 }
