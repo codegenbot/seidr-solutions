28bceb84fcca7a7124c5a41c6a365acb64f40441
beep boop <githubbot@vadim.me>
2024-08-05 14:16:55 +0200

    Solution 60 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jpk1psr4 Prompt:       |                                     ^~~~~~ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:50:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:50:44: error: ‘b’ was not declared in t his scope    50 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:44:5: note: ‘int main()’ previously def ined here    44 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -41,7 +41,6 @@ std::vector<std::string> select_words(std::string words, int index) {
     return selected_words;
 }
 
-int main() {
-    assert(issame(select_words("a b c d e f", 1), std::vector<std::string>{"b", "c", "d", "f"}));
-    return 0;
+void main() {
+    assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
 }
