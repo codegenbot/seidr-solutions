eb70f57fb2442a5f124e250534b77d11733d730a
beep boop <githubbot@vadim.me>
2024-08-06 00:21:13 +0200

    Solution 51 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/7kipxr7e Prompt: 0e3d9b22-6213-4a5a-beca-79d33ad944fa.cpp:31:16: error: ‘s’ was not declared in t his scope    31 |         cin >> s;       |                ^ 0e3d9b22-6213-4a5a-beca-79d33ad944fa.cpp:32:9: error: ‘cout’ was not declared in  this scope; did you mean ‘std::cout’?    32 |         cout << camelCase(s) << endl;       |         ^~~~       |         std::cout In file included from 0e3d9b22-6213-4a5a-beca-79d33ad944fa.cpp:1: /usr/include/c++/11/iostream:61:18: note: ‘std::cout’ declared here    61 |   extern ostream cout;          /// Linked to standard output       |                  ^~~~ 0e3d9b22-6213-4a5a-beca-79d33ad944fa.cpp:32:33: error: ‘endl’ was not declared i n this scope; did you mean ‘std::endl’?    32 |         cout << camelCase(s) << endl;       |                                 ^~~~       |                                 std::endl In file included from /usr/include/c++/11/iostream:39,                  from 0e3d9b22-6213-4a5a-beca-79d33ad944fa.cpp:1: /usr/include/c++/11/ostream:684:5: note: ‘std::endl’ declared here   684 |     endl(basic_ostream<_CharT, _Traits>& __os)       |     ^~~~

--- camel-case.cpp
+++ camel-case.cpp
@@ -1,35 +1,10 @@
-#include <iostream>
-#include <string>
-
-std::string camelCase(std::string str) {
-    std::vector<std::string> words;
-    size_t start = 0;
-    for (size_t i = 0; i <= str.size(); ++i) {
-        if (i == str.size() || str[i] == '-') {
-            words.push_back(str.substr(start, i - start));
-            start = i + 1;
-        }
-    }
-
-    std::string result;
-    for (const auto& word : words) {
-        if (!result.empty()) {
-            result += std::string(1, char(toupper((word[0]))));
-            result.erase(0, 1);
-        } else {
-            result = word;
-        }
-    }
-    return result;
-}
-
 int main() {
     int t;
     std::cin >> t;
     while (t--) {
-        std::string s; 
+        std::string s;
         std::cin >> s;
-        std::cout << camelCase(s) << std::endl; 
+        std::cout << camelCase(s) << std::endl;
     }
     return 0;
 }
