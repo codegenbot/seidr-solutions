3f93e5e5cf5443cac05610ee54ad7c4de0097bfc
beep boop <githubbot@vadim.me>
2024-08-05 23:36:01 +0200

    Solution 48 of fuel-cost, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/6ssnqknq Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0287af29-5835-4576-a19d-c09971740 078.py", line 4, in <module>     x = int(input())         ^^^^^^^^^^^^ ValueError: invalid literal for int() with base 10: '9346 5521 5462'

--- fuel-cost.py
+++ fuel-cost.py
@@ -1,5 +1,3 @@
-def fuel_cost(arr):
-    return sum([max(0, num // 3 - 2) for num in arr])
-
 arr = list(map(int, input().split()))
-print(fuel_cost(arr))
+total = sum(max(x // 3 - 2, 0) for x in arr)
+print(total)
