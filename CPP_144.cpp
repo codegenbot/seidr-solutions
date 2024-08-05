78ac8d24dbfaba94e6027a26281f954844bb6c80
beep boop <githubbot@vadim.me>
2024-08-05 15:25:56 +0200

    Solution 4 of CPP/144, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/xt08xj5p Prompt:       |                     ^ 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp:4:30: error: ‘n’ was not declared in th is scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                              ^ 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp:4:16: error: ‘stoi’ was not declared in  this scope     4 |     int den2 = stoi(n.substr(n.find('/') + 1));       |                ^~~~ 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp:6:5: error: expected unqualified-id bef ore ‘return’     6 |     return (num1 * den2 == num2 * den1);       |     ^~~~~~ 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp:7:1: error: expected declaration before  ‘}’ token     7 | }       | ^ In file included from 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp:9: 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp: In function ‘int main()’: 3d838735-8787-4e07-ae4f-4733ae3277d1.cpp:11:13: error: ‘simplify’ was not declar ed in this scope    11 |     assert (simplify("1/5", "1/5") == false);       |             ^~~~~~~~

--- CPP_144.cpp
+++ CPP_144.cpp
@@ -1,17 +1,24 @@
+#include <iostream>
 #include <string>
-#include <cassert>
 
 bool simplify(std::string x, std::string n) {
-    int num1 = std::stoi(x.substr(0, x.find('/')));
-    int den1 = std::stoi(x.substr(x.find('/') + 1));
-    int num2 = std::stoi(n.substr(0, n.find('/')));
-    int den2 = std::stoi(n.substr(n.find('/') + 1));
+    int num1 = stoi(x.substr(0, x.find('/')));
+    int den1 = stoi(x.substr(x.find('/') + 1));
+    int num2 = stoi(n.substr(0, n.find('/')));
+    int den2 = stoi(n.substr(n.find('/') + 1));
 
     return (num1 * den2 == num2 * den1);
 }
 
 int main() {
-    assert(simplify("1/5", "1/5") == false);
+    std::string input1, input2;
+    std::cout << "Enter numerator/denominator for first fraction: ";
+    std::cin >> input1;
+    std::cout << "Enter numerator/denominator for second fraction: ";
+    std::cin >> input2;
+
+    bool result = simplify(input1, input2);
+    std::cout << (result ? "Fractions are equivalent." : "Fractions are not equivalent.") << std::endl;
 
     return 0;
 }
