b5019ed0b909cf9b4901a1f2b9ba0d1117ce87db
beep boop <githubbot@vadim.me>
2024-08-05 18:01:48 +0200

    Solution 73 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/7og8tmhn Prompt: 477f3661-b01c-493b-b1d3-9886f9b719c6.cpp:1:19: error: ‘string’ was not declared in this scope     1 | bool solveBoolean(string expression) {       |                   ^~~~~~

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -1,15 +1,17 @@
 bool solveBoolean(std::string expression) {
     bool result = true;
     for(int i=0; i<expression.length(); i++){
-        if(expression[i] == 'T') result = true;
-        else if(expression[i] == 'F') result = false;
-        else if(expression[i] == '&') {
-            while(i+1 < expression.length() && expression[i+1] == '&') i++;
-            while(i+1 < expression.length() && expression[i+1] == '|') i++;
-        }
-        else if(expression[i] == '|') {
-            while(i+1 < expression.length() && expression[i+1] == '|') i++;
-            while(i+1 < expression.length() && expression[i+1] == '&') i++;
+        if(expression[i] == '&') {
+            result &= (i > 0 && (expression[i-1] == 'T' || expression[i-1] == '&' || expression[i-1] == '('));
+        } else if(expression[i] == '|') {
+            result |= (i > 0 && (expression[i-1] == 'F' || expression[i-1] == '|' || expression[i-1] == '('));
+        } else if(expression[i] != 'T' && expression[i] != 'F'){
+            if(expression[i] == '&')
+                result &= true;
+            else
+                result |= true;
+        } else{
+            return expression[i] == 'T';
         }
     }
     return result;
