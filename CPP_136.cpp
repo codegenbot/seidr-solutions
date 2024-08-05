f5ad7cbc9e0e672990c1ba12336fccfb61c2a67b
beep boop <githubbot@vadim.me>
2024-08-05 15:07:00 +0200

    Solution 34 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pl1x98g3 Prompt: In file included from fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp:40:34: error: expected primary-expressi on before ‘int’    40 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp:39: fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp: In function ‘int main()’: fca9d6b2-89f6-41ce-84fe-8add3da3b070.cpp:49:20: error: ‘largest_smallest_integer s’ was not declared in this scope; did you mean ‘largest_smallest_integer’?    49 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |                    ^~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -1,37 +1,30 @@
 #include <vector>
 #include <climits>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
-    return a == b;
-}
 
 std::vector<int> largest_smallest_integer(std::vector<int> lst) {
     std::vector<int> result = {0, 0};
     int max_neg = INT_MIN;
     int min_pos = INT_MAX;
-
-    for (int num : lst) {
-        if (num < 0 && num > max_neg) {
+    
+    for(int num : lst){
+        if(num < 0 && num > max_neg){
             max_neg = num;
-        } else if (num > 0 && num < min_pos) {
+        } else if(num > 0 && num < min_pos){
             min_pos = num;
         }
     }
-
+    
     result[0] = max_neg == INT_MIN ? 0 : max_neg;
     result[1] = min_pos == INT_MAX ? 0 : min_pos;
-
+    
     return result;
 }
 
 int main() {
-    std::vector<int> input = {5, -2, 9, -7, 3};
-    std::vector<int> expected_output = {-2, 3};
-
+    std::vector<int> input = {-6, -4, -4, -3, -100, 1};
+    std::vector<int> expected_output = {-100, 1};
+    
     std::vector<int> output = largest_smallest_integer(input);
-
-    assert(issame(output, expected_output));
-
+    
     return 0;
 }
