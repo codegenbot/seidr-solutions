26d3ea871b2c85667451d818ccc68796fddce985
beep boop <githubbot@vadim.me>
2024-08-05 19:04:48 +0200

    Solution 36 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/7kipxr7e Prompt: Program did not receive expected input

--- camel-case.cpp
+++ camel-case.cpp
@@ -6,21 +6,39 @@ using namespace std;
 
 string toCamelCase(string str) {
     string result = "";
-    int i = 0;
-    while (i < str.length()) {
+    bool nextUpper = true;
+    for (int i = 0; i < str.length(); i++) {
         if (str[i] == '-') {
             i++;
             while (i < str.length() && str[i] == ' ') {
                 i++;
             }
-            result += toupper(str[i]);
-        } else if (str[i] == ' ') {
-            i++;
-        } else {
-            if (result.length() > 0) {
-                result[0] = tolower(str[i]);
+            if (nextUpper) {
+                result += toupper(str[i]);
+                nextUpper = false;
             } else {
                 result += tolower(str[i]);
+            }
+        } else if (str[i] == ' ') {
+            if (!nextUpper) {
+                result += tolower(str[i]);
+                nextUpper = true;
+            }
+        } else {
+            if (result.length() > 0) {
+                if (nextUpper) {
+                    result[0] = toupper(str[i]);
+                    nextUpper = false;
+                } else {
+                    result += tolower(str[i]);
+                }
+            } else {
+                if (nextUpper) {
+                    result += toupper(str[i]);
+                    nextUpper = false;
+                } else {
+                    result += tolower(str[i]);
+                }
             }
         }
     }
@@ -31,7 +49,7 @@ int main() {
     string str;
     while (true) {
         cin >> str;
-        getline(cin, str); 
+        getline(cin, str);
         cout << toCamelCase(str) << endl;
     }
     return 0;
