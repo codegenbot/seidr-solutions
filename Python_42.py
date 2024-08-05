5b91d1faa90fa7bb2bd22f663693e2775d3a7f15
beep boop <githubbot@vadim.me>
2024-08-05 16:03:34 +0200

    Solution 37 of Python/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2c4k38o9 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/0ce0ba54-48e4-4244-9041-a2fb221bb b78.py", line 7, in <module>     check(incr_list)           ^^^^^^^^^ NameError: name 'incr_list' is not defined

--- Python_42.py
+++ Python_42.py
@@ -1,10 +1,4 @@
 def increment_list(l):
     return [x + 1 for x in l]
 
-def check(func):
-    if func([1, 2, 3]) == [2, 3, 4]:
-        return True
-    else:
-        return False
-
-print(check(increment_list))
+check(increment_list)
