f7ad4f0da2e83a9f53ec840b8e780f40bf2cf62b
beep boop <githubbot@vadim.me>
2024-08-05 11:02:01 +0200

    Solution 4 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/m29x1t1k Prompt: Given a string representing the individual bowls in a 10-frame round of 10 pin bowling, return the score of that round.

--- bowling.cpp
+++ bowling.cpp
@@ -1,38 +1,27 @@
 int bowlingScore(string s) {
     int score = 0;
-    int currentFrame = 1;
-    for (int i = 0; i < s.length(); ++i) {
-        if (s[i] == 'X') {
-            score += 10 + (currentFrame < 10 ? 10 : 0);
-            currentFrame++;
-        } else if (s[i] == '/') {
-            int strikeScore = 10;
-            if (i > 1 && s[i - 2] == 'X') {
-                strikeScore = 20;
-            }
-            score += strikeScore + (currentFrame < 10 ? getDigit(s, i + 1) : 0);
-            currentFrame++;
+    int frame = 0;
+    for (char c : s) {
+        if (c == 'X') {
+            score += 10 + (frame < 9 ? 10 : 0);
+            frame++;
+        } else if (c == '/') {
+            score += 10 - (s[frame+1] - '0' + s[frame+2] - '0');
+            frame++;
         } else {
-            int digit1 = getDigit(s, i);
-            int digit2 = getDigit(s, i + 1);
-            if (digit1 == 10 && digit2 == '/') {
-                score += 20;
-                currentFrame++;
+            int pins = c - '0';
+            if (frame < 9) {
+                if (pins < 3) {
+                    score += pins;
+                    frame++;
+                } else {
+                    score += pins + 10;
+                    frame++;
+                }
             } else {
-                score += digit1 + digit2;
-                currentFrame++;
+                score += pins;
             }
         }
     }
     return score;
-}
-
-int getDigit(string s, int i) {
-    while (i < s.length() && !isdigit(s[i])) {
-        if (s[i] == '/') {
-            return 10;
-        }
-        ++i;
-    }
-    return s[i] - '0';
 }
