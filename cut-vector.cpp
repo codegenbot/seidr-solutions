8d41979391f4741ef3183979440e368ed39d9fbc
beep boop <githubbot@vadim.me>
2024-08-06 00:35:56 +0200

    Solution 8 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/yd1li0xv Prompt: ared in this scope    30 |     for (int num : subvector1) {       |                    ^~~~~~~~~~ e35b4644-5279-4881-a1ba-93e6e1991d83.cpp:31:9: error: ‘cout’ was not declared in  this scope    31 |         cout << num << endl;       |         ^~~~ e35b4644-5279-4881-a1ba-93e6e1991d83.cpp:31:24: error: ‘endl’ was not declared i n this scope    31 |         cout << num << endl;       |                        ^~~~ e35b4644-5279-4881-a1ba-93e6e1991d83.cpp:34:20: error: ‘subvector2’ was not decl ared in this scope    34 |     for (int num : subvector2) {       |                    ^~~~~~~~~~ e35b4644-5279-4881-a1ba-93e6e1991d83.cpp:35:9: error: ‘cout’ was not declared in  this scope    35 |         cout << num << endl;       |         ^~~~ e35b4644-5279-4881-a1ba-93e6e1991d83.cpp:35:24: error: ‘endl’ was not declared i n this scope    35 |         cout << num << endl;       |                        ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <vector>
 
 int main() {
-    std::vector<int> nums;
+    vector<int> nums;
     int num;
-
-    while (std::cin >> num) {
+    
+    while (cin >> num) {
         nums.push_back(num);
     }
-
+    
     int n = nums.size();
     int sum = 0;
     for (int i = 0; i < n; i++) {
         sum += nums[i];
     }
-
+    
     int target = sum / 2;
     int prefix_sum = 0;
     int idx = -1;
-
+    
     for (int i = 0; i < n; i++) {
         prefix_sum += nums[i];
         if (prefix_sum >= target) {
@@ -26,17 +25,17 @@ int main() {
             break;
         }
     }
-
-    std::vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
-    std::vector<int> subvector2(nums.begin() + idx + 1, nums.end());
-
+    
+    vector<int> subvector1(nums.begin(), nums.begin() + idx + 1);
+    vector<int> subvector2(nums.begin() + idx + 1, nums.end());
+    
     for (int num : subvector1) {
-        std::cout << num << std::endl;
+        cout << num << endl;
     }
-
+    
     for (int num : subvector2) {
-        std::cout << num << std::endl;
+        cout << num << endl;
     }
-
+    
     return 0;
 }
