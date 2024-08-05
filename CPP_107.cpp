0cc5d43d6d50bdc3ad9ac6491f5378dcd5bbfd4c
beep boop <githubbot@vadim.me>
2024-08-05 13:38:37 +0200

    Solution 42 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/e1czyo3k Prompt:       |             ^~~~~~ 44022804-8803-463d-92c9-121b7f4c7cb5.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 44022804-8803-463d-92c9-121b7f4c7cb5.cpp:33:39: error: expression list treated a s compound expression in initializer [-fpermissive]    33 | bool issame(vector<int> a,vector<int>b){       |                                       ^ 44022804-8803-463d-92c9-121b7f4c7cb5.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ 44022804-8803-463d-92c9-121b7f4c7cb5.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~ In file included from 44022804-8803-463d-92c9-121b7f4c7cb5.cpp:32: 44022804-8803-463d-92c9-121b7f4c7cb5.cpp: In function ‘int main()’: 44022804-8803-463d-92c9-121b7f4c7cb5.cpp:42:19: error: ‘issame’ cannot be used a s a function    42 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,30 +1,8 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <cassert>
-
-bool issame(std::vector<int> a, std::vector<int> b) {
+bool is_same(std::vector<int> a, std::vector<int> b) {
     return a == b;
 }
 
-std::vector<int> even_odd_palindrome(int n) {
-    std::vector<int> count{0, 0};
-    for (int i = 1; i <= n; ++i) {
-        std::string num = std::to_string(i);
-        std::string rev_num = num;
-        std::reverse(rev_num.begin(), rev_num.end());
-        if (num == rev_num) {
-            if (i % 2 == 0) {
-                count[0]++;
-            } else {
-                count[1]++;
-            }
-        }
-    }
-    return count;
-}
-
 int main() {
-    assert(issame(even_odd_palindrome(1), {0, 1}));
+    assert(is_same(even_odd_palindrome(1), {0, 1}));
     return 0;
 }
