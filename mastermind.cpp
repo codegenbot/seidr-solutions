f44f554acbe49ec0b8a515383f0227bf000bd69a
beep boop <githubbot@vadim.me>
2024-08-05 15:58:16 +0200

    Solution 60 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0sm0c1rc Prompt: f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,49 +1,29 @@
 #include <string>
+
 int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
-    // Count correct colors in wrong places (white pegs)
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            white++;
-        }
-    }
-
-    // Count correct colors in correct places (black pegs)
-    int code_count[6] = {0};
-    int guess_count[6] = {0};
-
+    int codeCount[6] = {0};
     for (char c : code) {
-        code_count[c - 'A']++;
-    }
-    for (char c : guess) {
-        guess_count[c - 'A']++;
+        codeCount[c - '0']++;
     }
 
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
         } else {
-            if (guess_count[code[i] - 'A'] > 0 && code_count[code[i] - 'A'] > 1) {
-                white++;
-            } else {
-                black++;
+            codeCount[code[i] - '0']--;
+            guessCount[guess[i] - '0']--;
+            int wrongCount = 0;
+            for (int j = 0; j < 4; ++j) {
+                if (code[j] == guess[i]) {
+                    wrongCount++;
+                }
             }
-            --guess_count[code[i] - 'A'];
-            --code_count[code[i] - 'A'];
+            white += wrongCount;
         }
     }
 
-    // Count remaining pegs
-    for (int i = 0; i < 6; ++i) {
-        if (code_count[i]) {
-            white += code_count[i];
-        }
-        if (guess_count[i]) {
-            black += guess_count[i];
-        }
-    }
-
-    return white + black;
+    return black + white;
 }
