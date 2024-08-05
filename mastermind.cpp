04226a9aa94ef01b4c2e18f5836865d936dc48aa
beep boop <githubbot@vadim.me>
2024-08-05 15:51:15 +0200

    Solution 48 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0sm0c1rc Prompt: f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,32 +1,31 @@
-#include <string>
-#include <vector>
-
-std::pair<int, int> mastermind(std::string code, std::string guess) {
+int mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
-    // Count the number of correct colors in wrong positions
+    // Count correct colors in wrong places (white pegs)
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
-            black++;
-            code[i] = ' ';
-            guess[i] = ' ';
+            white++;
         }
     }
 
-    // Count the number of correct colors in right positions
-    for (int i = 0; i < 4; ++i) {
-        bool found = false;
-        for (int j = 0; j < 4; ++j) {
-            if (code[i] == guess[j]) {
-                found = true;
-                code[j] = ' ';
-                break;
+    // Count correct colors in correct places (black pegs)
+    int codeCount[6] = {0};
+    for (char c : code) {
+        codeCount[c - 'A']++;
+    }
+
+    for (int j = 0; j < 4; ++j) {
+        if (code[j] == guess[j]) {
+            black++;
+        } else {
+            int count = codeCount[guess[j] - 'A'];
+            if (count > 0) {
+                black++;
+                codeCount[guess[j] - 'A']--;
             }
         }
-        if (!found)
-            white++;
     }
 
-    return std::make_pair(white, black);
+    return white + black;
 }
