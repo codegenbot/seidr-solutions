d6ed38f984232b01ede22828b67493d0d30ac7ad
beep boop <githubbot@vadim.me>
2024-08-05 12:04:04 +0200

    Solution 4 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/0etv379p Prompt:  this scope    21 |         cout << nums[i] << endl;       |         ^~~~ cf910d42-7f46-4036-80ed-1793834c2e0b.cpp:21:17: error: ‘nums’ was not declared i n this scope    21 |         cout << nums[i] << endl;       |                 ^~~~ cf910d42-7f46-4036-80ed-1793834c2e0b.cpp:21:28: error: ‘endl’ was not declared i n this scope    21 |         cout << nums[i] << endl;       |                            ^~~~ cf910d42-7f46-4036-80ed-1793834c2e0b.cpp:23:5: error: ‘cout’ was not declared in  this scope    23 |     cout << endl;       |     ^~~~ cf910d42-7f46-4036-80ed-1793834c2e0b.cpp:23:13: error: ‘endl’ was not declared i n this scope    23 |     cout << endl;       |             ^~~~ cf910d42-7f46-4036-80ed-1793834c2e0b.cpp:24:27: error: ‘nums’ was not declared i n this scope    24 |     for (int i = idx; i < nums.size(); ++i) {       |                           ^~~~

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
+using namespace std;
 
 int main() {
-    std::vector<int> nums;
+    vector<int> nums;
     int n;
-    while (std::cin >> n) {
+    while (cin >> n) {
         nums.push_back(n);
     }
 
     int idx = 0;
     int diff = INT_MAX;
     for (int i = 1; i < nums.size(); ++i) {
-        int left_sum = std::accumulate(nums.begin(), nums.begin() + i, 0);
-        int right_sum = std::accumulate(nums.begin() + i, nums.end(), 0);
-        int current_diff = std::abs(left_sum - right_sum);
-        if (current_diff < diff) {
+        int left_sum = accumulate(nums.begin(), nums.begin() + i, 0);
+        int right_sum = accumulate(nums.begin() + i, nums.end(), 0);
+        int current_diff = abs(left_sum - right_sum);
+        if (current_diff < diff || (current_diff == diff && idx < i)) {
             diff = current_diff;
             idx = i;
         }
     }
 
     for (int i = 0; i < idx; ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << endl;
     }
-    std::cout << std::endl;
+    cout << endl;
     for (int i = idx; i < nums.size(); ++i) {
-        std::cout << nums[i] << std::endl;
+        cout << nums[i] << endl;
     }
 
     return 0;
