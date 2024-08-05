d8a5e5843fc9b644a1b46fdeeb2ea5c846900e9d
beep boop <githubbot@vadim.me>
2024-08-05 12:20:10 +0200

    Solution 9 of CPP/79, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gffjpwfs Prompt: 7f0e9d80-58f0-48e8-b63a-4d88eec6ed2c.cpp:1:1: error: ‘string’ does not name a ty pe     1 | string decimal_to_binary(int decimal){       | ^~~~~~ In file included from 7f0e9d80-58f0-48e8-b63a-4d88eec6ed2c.cpp:13: 7f0e9d80-58f0-48e8-b63a-4d88eec6ed2c.cpp: In function ‘int main()’: 7f0e9d80-58f0-48e8-b63a-4d88eec6ed2c.cpp:15:13: error: ‘decimal_to_binary’ was n ot declared in this scope    15 |     assert (decimal_to_binary(15) == "db1111db");       |             ^~~~~~~~~~~~~~~~~

--- CPP_79.cpp
+++ CPP_79.cpp
@@ -14,6 +14,6 @@ std::string decimal_to_binary(int decimal){
 }
 
 int main() {
-    assert(decimal_to_binary(15) == "db1111db");
+    assert (decimal_to_binary(15) == "db1111db");
     return 0;
 }
