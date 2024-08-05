3426492e7338c89751af71a88b1613e699bfb102
beep boop <githubbot@vadim.me>
2024-08-05 11:32:49 +0200

    Solution 73 of Python/3, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/i1sux4fm Prompt: Program did not receive expected input

--- Python_3.py
+++ Python_3.py
@@ -1,6 +1,6 @@
-def below_zero(numbers):
+def below_zero(operations):
     count = 0
-    for num in numbers:
+    for num in operations:
         if num < 0:
             count += 1
     return count
