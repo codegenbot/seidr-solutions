799993c4ee46460f40f2b8bb09edbf5419b53d9a
beep boop <githubbot@vadim.me>
2024-08-05 16:25:54 +0200

    Solution 66 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jtfwlvb2 Prompt: /usr/bin/ld: /tmp/ccTFGT4J.o: in function `string_to_md5(std::__cxx11::basic_str ing<char, std::char_traits<char>, std::allocator<char> > const&)': 3c97379d-4882-47e7-89e5-766b506b564a.cpp:(.text+0x8e): undefined reference to `E VP_MD_CTX_new' /usr/bin/ld: 3c97379d-4882-47e7-89e5-766b506b564a.cpp:(.text+0x9a): undefined re ference to `EVP_md5' /usr/bin/ld: 3c97379d-4882-47e7-89e5-766b506b564a.cpp:(.text+0xb4): undefined re ference to `EVP_DigestInit_ex' /usr/bin/ld: 3c97379d-4882-47e7-89e5-766b506b564a.cpp:(.text+0xed): undefined re ference to `EVP_DigestUpdate' /usr/bin/ld: 3c97379d-4882-47e7-89e5-766b506b564a.cpp:(.text+0x10a): undefined r eference to `EVP_DigestFinal_ex' /usr/bin/ld: 3c97379d-4882-47e7-89e5-766b506b564a.cpp:(.text+0x119): undefined r eference to `EVP_MD_CTX_free' collect2: error: ld returned 1 exit status

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,5 +1,3 @@
-int main();
-
 #include <iostream>
 #include <string>
 #include <openssl/evp.h>
@@ -26,4 +24,9 @@ string string_to_md5(const string& text) {
     }
 
     return string(mdString);
+}
+
+int main() {
+    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
+    return 0;
 }
