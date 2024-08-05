7b5d9622f9cc4a0850bc4f364b793b1fef5a5917
beep boop <githubbot@vadim.me>
2024-08-05 14:15:53 +0200

    Solution 6 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lh992irp Prompt:       |                    ^~~ b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp:6:39: error: expression list treated as  compound expression in initializer [-fpermissive]     6 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp:5: b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp: In function ‘int main()’: b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp:15:20: error: ‘maximum’ was not declare d in this scope    15 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ b8e2b014-5838-42ed-bf63-72b1ddd30322.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,12 +1,20 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
-    return a == b;
+using namespace std;
+
+bool issame(const vector<int>& a, const vector<int>& b){
+    return a.size() == b.size() && equal(a.begin(), a.end(), b.begin());
 }
 
-std::vector<int> maximum(std::vector<int> arr, int k){
-    std::sort(arr.begin(), arr.end(), std::greater<int>());
-    arr.resize(k);
-    return arr;
+vector<int> maximum(vector<int> arr, int k) {
+    sort(arr.begin(), arr.end(), greater<int>());
+    vector<int> result(arr.begin(), arr.begin() + k);
+    return result;
+}
+
+int main() {
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
+    
+    return 0;
 }
