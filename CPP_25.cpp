411725f3a608a41c86f418bfcbdd0452a73824e9
beep boop <githubbot@vadim.me>
2024-08-05 22:54:12 +0200

    Solution 23 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/08xt4hw0 Prompt:       |                           ^~~~~~ bef8b572-a51c-4006-becf-b625a0ede4c5.cpp:33:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from bef8b572-a51c-4006-becf-b625a0ede4c5.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from bef8b572-a51c-4006-becf-b625a0ede4c5.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ bef8b572-a51c-4006-becf-b625a0ede4c5.cpp:33:34: error: expected primary-expressi on before ‘int’    33 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ bef8b572-a51c-4006-becf-b625a0ede4c5.cpp:41:5: error: redefinition of ‘int main( )’    41 | int main(){       |     ^~~~ bef8b572-a51c-4006-becf-b625a0ede4c5.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,17 +1,6 @@
 #include <cassert>
 #include <vector>
 
-std::vector<int> factorize(int n){
-    std::vector<int> factors;
-    for(int i = 2; i <= n; i++){
-        while(n % i == 0){
-            factors.push_back(i);
-            n /= i;
-        }
-    }
-    return factors;
-}
-
 bool issame(const std::vector<int> &a, const std::vector<int> &b){
     if(a.size() != b.size()){
         return false;
@@ -27,4 +16,15 @@ bool issame(const std::vector<int> &a, const std::vector<int> &b){
 int main() {
     assert(issame(factorize(3 * 2 * 3), {2, 3, 3}));
     return 0;
+}
+
+std::vector<int> factorize(int n){
+    std::vector<int> factors;
+    for(int i = 2; i <= n; i++){
+        while(n % i == 0){
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    return factors;
 }
