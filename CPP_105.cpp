0b6976bc1c613728ff704be15b00c004725f6a7c
beep boop <githubbot@vadim.me>
2024-08-05 13:33:46 +0200

    Solution 40 of CPP/105, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kiazpx2b Prompt:     6 | f99f7672-9f7e-40b4-a3eb-0534bf32987c.cpp:52:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    52 | int main(){       |         ^~ f99f7672-9f7e-40b4-a3eb-0534bf32987c.cpp:52:9: note: remove parentheses to defau lt-initialize a variable    52 | int main(){       |         ^~       |         -- f99f7672-9f7e-40b4-a3eb-0534bf32987c.cpp:52:9: note: or replace parentheses with  braces to value-initialize a variable f99f7672-9f7e-40b4-a3eb-0534bf32987c.cpp:52:11: error: a function-definition is not allowed here before ‘{’ token    52 | int main(){       |           ^ f99f7672-9f7e-40b4-a3eb-0534bf32987c.cpp:54:2: error: expected ‘}’ at end of inp ut    54 | }       |  ^ f99f7672-9f7e-40b4-a3eb-0534bf32987c.cpp:34:11: note: to match this ‘{’    34 | int main(){       |           ^

--- CPP_105.cpp
+++ CPP_105.cpp
@@ -3,6 +3,6 @@ int main() {
     std::vector<std::string> vec2 = by_length({9, 4, 1});
     assert(is_same(vec1, vec2));
     assert(is_same(by_length({9, 4, 8}), {"Nine", "Eight", "Four"});
-
+    
     return 0;
 }
