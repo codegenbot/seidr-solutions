7687a88976f8c4b65aa67677e3d14c5e52e67c8e
beep boop <githubbot@vadim.me>
2024-08-05 09:26:05 +0200

    Solution 91 of Python/6, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/gy4d7yc2 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/8a13dada-e3f4-481e-b3a9-cb9c6d50b f34.py", line 1, in <module>     def parse_nested_parens(paren_string: str) -> List[int]:                                                   ^^^^ NameError: name 'List' is not defined. Did you mean: 'list'?

--- Python_6.py
+++ Python_6.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 def parse_nested_parens(paren_string: str) -> List[int]:
     return [
         max([paren_string.count("(" * i) for i in range(1, len(paren_string) // 2 + 1)])
