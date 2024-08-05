f704d815aefd8bfd2699d99ab953791278cebc66
beep boop <githubbot@vadim.me>
2024-08-05 12:30:54 +0200

    Solution 6 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/5dior96c Prompt: Given a vector of positive integers, ﬁnd the spot where, if you cut the vector, the numbers on both sides are either equal, or the diﬀerence is as small as possible. Return the two resulting subvectors as two outputs.

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -3,21 +3,17 @@ using namespace std;
 
 pair<vector<int>, vector<int>> cutVector(vector<int> vec) {
     int min_diff = INT_MAX;
-    pair<vector<int>, vector<int>> result;
-    
-    for (int i = 1; i <= vec.size(); i++) {
-        vector<int> left(vec.begin(), vec.begin() + i);
-        vector<int> right(vec.begin() + i, vec.end());
-        
-        int diff = abs((int)accumulate(left.begin(), left.end(), 0) - (int)accumulate(right.begin(), right.end(), 0));
-        
-        if (diff < min_diff) {
+    int cut_index = 0;
+
+    for (int i = 1; i < vec.size(); i++) {
+        int diff = abs(vec[i] - vec[0]);
+        if (diff <= min_diff) {
             min_diff = diff;
-            result = make_pair(left, right);
+            cut_index = i;
         }
     }
-    
-    return result;
+
+    return {vector<int>(vec.begin(), vec.begin() + cut_index), vector<int>(vec.begin() + cut_index, vec.end())};
 }
 
 int main() {
@@ -29,18 +25,19 @@ int main() {
         cin >> vec[i];
     }
 
-    pair<vector<int>, vector<int>> res = cutVector(vec);
+    pair<vector<int>, vector<int>> result = cutVector(vec);
 
-    cout << "Left: ";
-    for (int num : res.first) {
+    cout << "1 ";
+    for (int num : result.first) {
         cout << num << " ";
     }
     cout << endl;
 
-    cout << "Right: ";
-    for (int num : res.second) {
+    cout << "0 ";
+    for (int num : result.second) {
         cout << num << " ";
     }
     cout << endl;
+
     return 0;
 }
