e8a76bb2bb4d2de6730c6767a1cc6d933c61ec37
beep boop <githubbot@vadim.me>
2024-08-05 16:13:48 +0200

    Solution 8 of CPP/162, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jtfwlvb2 Prompt:       |                    ^ d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp:9:28: error: ‘i’ does not name a type     9 |     for(int i = 0; i < 16; i++){       |                            ^ d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp:12:5: error: ‘mdString’ does not name a  type    12 |     mdString[32] = '\0';       |     ^~~~~~~~ d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return string(mdString);       |     ^~~~~~ d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp:17: d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp: In function ‘int main()’: d744e8b4-60be-46b0-a6d1-ac28fd4d6513.cpp:19:13: error: ‘string_to_md5’ was not d eclared in this scope    19 |     assert (string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf 99");       |             ^~~~~~~~~~~~~

--- CPP_162.cpp
+++ CPP_162.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
+#include <cassert>
 #include <string>
 #include <openssl/md5.h>
 
-using namespace std;
-
-string string_to_md5(string text) {
-    if (text.empty()) {
-        return "None";
-    }
+std::string string_to_md5(const std::string& text){
+    assert(!text.empty());
 
     unsigned char digest[MD5_DIGEST_LENGTH];
     MD5((unsigned char*)text.c_str(), text.length(), digest);
 
     char mdString[33];
-    for (int i = 0; i < 16; i++) {
-        sprintf(&mdString[i * 2], "%02x", (unsigned int)digest[i]);
+    for(int i = 0; i < 16; i++){
+        sprintf(&mdString[i*2], "%02x", (unsigned int)digest[i]);
     }
     mdString[32] = '\0';
 
-    return string(mdString);
-}
-
-int main() {
-    assert(string_to_md5("password") == "5f4dcc3b5aa765d61d8327deb882cf99");
-    return 0;
+    return std::string(mdString);
 }
