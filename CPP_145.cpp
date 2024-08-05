e8e56f497a13e48d5fc8a85309c79acdfe8d4d12
beep boop <githubbot@vadim.me>
2024-08-05 15:46:37 +0200

    Solution 92 of CPP/145, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/qsnzq2qt Prompt:    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 40a1a142-be6c-464d-a7cb-8cde859e5aed.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 40a1a142-be6c-464d-a7cb-8cde859e5aed.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 40a1a142-be6c-464d-a7cb-8cde859e5aed.cpp:12: 40a1a142-be6c-464d-a7cb-8cde859e5aed.cpp: In function ‘int main()’: 40a1a142-be6c-464d-a7cb-8cde859e5aed.cpp:22:20: error: ‘order_by_points’ was not  declared in this scope    22 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |                    ^~~~~~~~~~~~~~~ 40a1a142-be6c-464d-a7cb-8cde859e5aed.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(order_by_points({0,6,6,-76,-21,23,4}) , {-76, -21, 0,  4, 23, 6, 6}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~

--- CPP_145.cpp
+++ CPP_145.cpp
@@ -1,19 +1,20 @@
-#include <vector>
 #include <algorithm>
 #include <cassert>
+#include <vector>
 
 namespace std {
-    bool issame(const std::vector<int>& a, const std::vector<int>& b){
-        return a == b;
-    }
-
     std::vector<int> order_by_points(std::vector<int> points){
         std::sort(points.begin(), points.end());
         return points;
     }
+
+    bool issame(const std::vector<int>& a, const std::vector<int>& b){
+        return a == b;
+    }
 }
 
 int main() {
-    assert(std::issame(std::order_by_points({0,6,6,-76,-21,23,4}), {-76, -21, 0, 4, 23, 6, 6}));
+    assert(std::issame(std::order_by_points({0, 6, 6, -76, -21, 23, 4}), {-76, -21, 0, 4, 23, 6, 6}));
+    
     return 0;
 }
