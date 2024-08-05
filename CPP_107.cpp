eb5ff771b04c61fb945f3ad386beee955974d86d
beep boop <githubbot@vadim.me>
2024-08-05 13:28:41 +0200

    Solution 2 of CPP/107, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/e1czyo3k Prompt:       |                    ^~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:18:27: error: ‘vector’ was not declared  in this scope    18 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:18:34: error: expected primary-expressi on before ‘int’    18 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:18:39: error: expression list treated a s compound expression in initializer [-fpermissive]    18 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:17: 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp: In function ‘int main()’: 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:27:20: error: ‘even_odd_palindrome’ was  not declared in this scope    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |                    ^~~~~~~~~~~~~~~~~~~ 69e896ee-1f3f-4634-9420-452ff51f64b9.cpp:27:19: error: ‘issame’ cannot be used a s a function    27 |     assert (issame(even_odd_palindrome(1) , {0, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_107.cpp
+++ CPP_107.cpp
@@ -1,15 +1,22 @@
-vector<int> result = {0, 0};
-    for (int i = 1; i <= n; ++i) {
-        string str = to_string(i);
-        string rev_str = str;
-        reverse(rev_str.begin(), rev_str.end());
-        if (str == rev_str) {
-            if (i % 2 == 0) {
-                result[0]++;
+#include <vector>
+
+bool issame(vector<int> vec1, vector<int> vec2){
+    return vec1 == vec2;
+}
+
+vector<int> even_odd_palindrome(int n){
+    vector<int> count{0, 0};
+    for(int i = 1; i <= n; ++i){
+        string num = to_string(i);
+        string rev_num = num;
+        reverse(rev_num.begin(), rev_num.end());
+        if(num == rev_num){
+            if(i % 2 == 0){
+                count[0]++;
             } else {
-                result[1]++;
+                count[1]++;
             }
         }
     }
-    return result;
+    return count;
 }
