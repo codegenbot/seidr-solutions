01aca13432c6a90502f147e5ce74c99885e5b78f
beep boop <githubbot@vadim.me>
2024-08-05 15:39:04 +0200

    Solution 67 of Python/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pvnyjl1j Prompt: Program did not receive expected input

--- Python_38.py
+++ Python_38.py
@@ -2,6 +2,3 @@ def decode_cyclic(s: str):
     return "".join(
         [(group[-1] + group[:-1]) if len(group) == 3 else group for group in s.split()]
     )
-
-# Input example
-print(decode_cyclic("abc def ghi"))  # Output: cab efd hig
