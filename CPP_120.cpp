dc183d302efe6bbf69f1fb231480a0555376a764
beep boop <githubbot@vadim.me>
2024-08-05 14:17:08 +0200

    Solution 11 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lh992irp Prompt: 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp:32:5: error: redefinition of ‘int main( )’    32 | int main(){       |     ^~~~ 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp:17:5: note: ‘int main()’ previously def ined here    17 | int main(){       |     ^~~~ In file included from 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp:23: 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp: In function ‘int main()’: 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp:33:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    33 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b){       |      ^~~~~~ 185290f4-195c-463e-8bf3-cecb0f57c29e.cpp:24:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    24 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -13,3 +13,9 @@ vector<int> maximum(vector<int> arr, int k){
     arr.resize(k);
     return arr;
 }
+
+int main(){
+    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
+  
+    return 0;
+}
