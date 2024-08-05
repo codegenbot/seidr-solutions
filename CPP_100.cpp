068b10928e9afbf745a25fe034a2512bfa34732f
beep boop <githubbot@vadim.me>
2024-08-05 13:05:37 +0200

    Solution 11 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/p6c24rc7 Prompt:       |                           ^~~~~~ edb161d4-3e1c-485a-82bd-7e7e6480f9c0.cpp:31:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from edb161d4-3e1c-485a-82bd-7e7e6480f9c0.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from edb161d4-3e1c-485a-82bd-7e7e6480f9c0.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ edb161d4-3e1c-485a-82bd-7e7e6480f9c0.cpp:31:34: error: expected primary-expressi on before ‘int’    31 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ edb161d4-3e1c-485a-82bd-7e7e6480f9c0.cpp:39:5: error: redefinition of ‘int main( )’    39 | int main(){       |     ^~~~ edb161d4-3e1c-485a-82bd-7e7e6480f9c0.cpp:23:5: note: ‘int main()’ previously def ined here    23 | int main() {       |     ^~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -20,8 +20,10 @@ std::vector<int> make_a_pile(int n){
     return result;
 }
 
+#include <vector>
+
 int main() {
-    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));
 
     return 0;
 }
