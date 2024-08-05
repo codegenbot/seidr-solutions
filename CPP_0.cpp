6bcefbd8b61d0613e13fb293000c3470682c6256
beep boop <githubbot@vadim.me>
2024-08-05 20:00:05 +0200

    Solution 25 of CPP/0, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/btkdncbz Prompt: 07ed5372-48b9-477b-8878-b4b64227b247.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main() {       |     ^~~~ 07ed5372-48b9-477b-8878-b4b64227b247.cpp: In function ‘int main()’: 07ed5372-48b9-477b-8878-b4b64227b247.cpp:25:5: error: ‘vector’ was not declared in this scope    25 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |     ^~~~~~ 07ed5372-48b9-477b-8878-b4b64227b247.cpp:25:5: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 07ed5372-48b9-477b-8878-b4b64227b247.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 07ed5372-48b9-477b-8878-b4b64227b247.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 07ed5372-48b9-477b-8878-b4b64227b247.cpp:25:12: error: expected primary-expressi on before ‘float’    25 |     vector<float> a={1.0, 2.0, 3.9, 4.0, 5.0, 2.2};       |            ^~~~~

--- CPP_0.cpp
+++ CPP_0.cpp
@@ -1,21 +1,21 @@
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <cassert>
+\#include <vector>  
+\#include <algorithm>  
+\#include <cmath>  
+\#include <cassert>  
 
-bool has_close_elements(std::vector<float> numbers, float threshold) {
-    std::sort(numbers.begin(), numbers.end());
-    for(int i = 1; i < numbers.size(); i++){
-        if(std::abs(numbers[i] - numbers[i-1]) < threshold){
-            return true;
-        }
-    }
-    return false;
-}
+bool has_close_elements(std::vector<float> numbers, float threshold) {  
+    std::sort(numbers.begin(), numbers.end());  
+    for(int i = 1; i < numbers.size(); i++){  
+        if(std::abs(numbers[i] - numbers[i-1]) < threshold){  
+            return true;  
+        }  
+    }  
+    return false;  
+}  
 
-int main() {
-    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};
-    assert(has_close_elements(a, 0.5) == false);
+int main() {  
+    std::vector<float> a = {1.0f, 2.0f, 3.9f, 4.0f, 5.0f, 2.2f};  
+    assert(has_close_elements(a, 0.5f) == false);  
 
-    return 0;
-}
+    return 0;  
+}  
