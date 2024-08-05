fdc402c07f737771fd425601460f1739aca61117
beep boop <githubbot@vadim.me>
2024-08-05 08:38:11 +0200

    Solution 38 of CPP/32, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/9z5w629z Prompt: f6b0e523-d624-4a2a-a4de-f761aa3ca1c9.cpp:16:13: error: ‘coeffs’ was not declared  in this scope; did you mean ‘coeff’?    16 |             coeffs.push_back(coeff);       |             ^~~~~~       |             coeff f6b0e523-d624-4a2a-a4de-f761aa3ca1c9.cpp:19:30: error: ‘coeffs’ was not declared  in this scope; did you mean ‘ncoeff’?    19 |         solution = find_zero(coeffs);       |                              ^~~~~~       |                              ncoeff f6b0e523-d624-4a2a-a4de-f761aa3ca1c9.cpp:19:20: error: ‘find_zero’ was not decla red in this scope    19 |         solution = find_zero(coeffs);       |                    ^~~~~~~~~ In file included from f6b0e523-d624-4a2a-a4de-f761aa3ca1c9.cpp:3: f6b0e523-d624-4a2a-a4de-f761aa3ca1c9.cpp:20:21: error: ‘poly’ was not declared i n this scope    20 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                     ^~~~ f6b0e523-d624-4a2a-a4de-f761aa3ca1c9.cpp:20:17: error: ‘abs’ was not declared in  this scope    20 |         assert (abs(poly(coeffs, solution))< 1e-3);       |                 ^~~

--- CPP_32.cpp
+++ CPP_32.cpp
@@ -1,25 +1,19 @@
 #include <vector>
 #include <cmath>
-#include <cassert>
 
-double poly(const std::vector<double>& coeffs, double x){
+double poly(const std::vector<double>& coeffs, double x) {
     double result = 0.0;
-    for(int i=0; i<coeffs.size(); i++){
-        result += coeffs[i] * std::pow(x, i);
+    for (int i = 0; i < coeffs.size(); ++i) {
+        result += coeffs[i] * pow(x, i);
     }
     return result;
 }
 
-bool find_zero(const std::vector<double>& coeffs){
-    double a = coeffs[0];
-    double b = coeffs[1];
-    return std::abs(poly(coeffs, -b/a)) < 1e-3;
+double abs(double num) {
+    return std::abs(num);
 }
 
-int main(){
-    std::vector<double> coeffs = {1.0, -2.0, 1.0};
-    bool solution = find_zero(coeffs);
-    assert(solution); 
-    
-    return 0;
+bool find_zero(const std::vector<double>& coeffs, double a, double b) {
+    double solution = -b/a;
+    return std::abs(poly(coeffs, solution)) < 1e-6;
 }
