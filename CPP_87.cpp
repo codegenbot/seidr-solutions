8a920a6275548ba0791c07fb7bef12e69ff63527
beep boop <githubbot@vadim.me>
2024-08-05 12:54:00 +0200

    Solution 99 of CPP/87, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/va3re40z Prompt:       |             ^~~~~~ e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp:37:49: error: expected primary-expressi on before ‘int’    37 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                 ^~~ e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp:37:56: error: expression list treated a s compound expression in initializer [-fpermissive]    37 | bool issame(vector<vector<int>> a,vector<vector<int>> b){       |                                                        ^ e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp:48:5: error: redefinition of ‘int main( )’    48 | int main(){       |     ^~~~ e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp:31:5: note: ‘int main()’ previously def ined here    31 | int main() {       |     ^~~~ In file included from e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp:36: e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp: In function ‘int main()’: e0a824e3-19c9-405e-93a4-1413281f9c7f.cpp:49:19: error: ‘issame’ cannot be used a s a function    49 |     assert (issame(get_row({{}, {1}, {1, 2, 3}}, 3) , {{2, 2}}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_87.cpp
+++ CPP_87.cpp
@@ -10,14 +10,6 @@ bool isEqual(const std::vector<int>& a, const std::vector<int>& b) {
     return true;
 }
 
-bool isEqual(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
-    if (a.size() != b.size()) return false;
-    for (size_t i = 0; i < a.size(); ++i) {
-        if (!isEqual(a[i], b[i])) return false;
-    }
-    return true;
-}
-
 std::vector<std::vector<int>> get_row(const std::vector<std::vector<int>>& lst, int x) {
     std::vector<std::vector<int>> result;
     for (size_t i = 0; i < lst.size(); ++i) {
@@ -37,6 +29,6 @@ std::vector<std::vector<int>> get_row(const std::vector<std::vector<int>>& lst, 
 }
 
 int main() {
-    assert(isEqual(get_row({{}, {1}, {1, 2, 3}}, 3), {{2, 2}}));
+    assert(isEqual(get_row({{}, {1}, {1, 2, 3}}, 3)[0], {2, 2}));
     return 0;
 }
