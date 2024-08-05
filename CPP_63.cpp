fddd28699019deafaf197d43a67ecdcdb770f33f
beep boop <githubbot@vadim.me>
2024-08-05 11:51:15 +0200

    Solution 6 of CPP/63, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/qva5aeos Prompt:     8 |     for (int i = 3; i <= n; ++i) {       |     ^~~ a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp:8:21: error: ‘i’ does not name a type     8 |     for (int i = 3; i <= n; ++i) {       |                     ^ a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp:8:29: error: expected unqualified-id be fore ‘++’ token     8 |     for (int i = 3; i <= n; ++i) {       |                             ^~ a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp:14:5: error: expected unqualified-id be fore ‘return’    14 |     return res;       |     ^~~~~~ a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp:15:1: error: expected declaration befor e ‘}’ token    15 | }       | ^ In file included from a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp:17: a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp: In function ‘int main()’: a8e32267-b095-4b3e-af7b-8e0f73319b42.cpp:19:13: error: ‘fibfib’ was not declared  in this scope    19 |     assert (fibfib(14) == 927);       |             ^~~~~~

--- CPP_63.cpp
+++ CPP_63.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
-#include <cassert>
-
 int fibfib(int n) {
     if (n == 0 || n == 1) {
         return 0;
     } else if (n == 2) {
         return 1;
     }
+
     int a = 0, b = 0, c = 1, res;
     for (int i = 3; i <= n; ++i) {
         res = a + b + c;
@@ -15,9 +13,4 @@ int fibfib(int n) {
         c = res;
     }
     return res;
-}
-
-int main() {
-    assert(fibfib(14) == 927);
-    return 0;
 }
