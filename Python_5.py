ce332eae87dc9858e5ba7b4b4724ea6e82eb48e5
beep boop <githubbot@vadim.me>
2024-08-05 09:30:31 +0200

    Solution 75 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/dpdfgo9j Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/26df257f-0833-4ae3-8fae-1062ff788 fd5.py", line 5     return list(chain.from_iterable(zip(numbers, [delimiter] * len(numbers)))[:- 1]                ^ SyntaxError: '(' was never closed

--- Python_5.py
+++ Python_5.py
@@ -1,12 +1,5 @@
 from typing import List
 from itertools import chain, zip_longest
 
-
 def intersperse(numbers: List[int], delimiter: int) -> List[int]:
-    return [
-        elem
-        for elem in chain.from_iterable(
-            zip_longest(numbers, [delimiter] * len(numbers), fillvalue=None)
-        )
-        if elem is not None
-    ]
+    return [elem for elem in chain.from_iterable(zip_longest(numbers, [delimiter] * len(numbers), fillvalue=None)) if elem is not None]
