8d8f70d4c7c4ab53edbe980da7395a06caf197e5
beep boop <githubbot@vadim.me>
2024-08-05 12:14:15 +0200

    Solution 92 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/m29x1t1k Prompt: bdd03b56-0aa9-4287-bfc1-d1dcd9361e8d.cpp:1:18: error: ‘string’ was not declared in this scope     1 | int bowlingScore(string s) {       |                  ^~~~~~

--- bowling.cpp
+++ bowling.cpp
@@ -5,30 +5,22 @@ int bowlingScore(string s) {
     int currentFrame = 1;
     for (int i = 0; i < s.length(); i++) {
         if (s[i] == '/') {
-            string firstPart = s.substr(0, i);
-            string secondPart = s.substr(i + 1);
-            int firstPin = stoi(firstPart);
-            int secondPin = stoi(secondPart);
-            if (firstPin + secondPin == 10) {
-                score += 10;
-                currentFrame++;
-            } else {
-                score += firstPin + secondPin;
-                currentFrame++;
-            }
+            int firstPin = stoi(s.substr(0, i));
+            int secondPin = stoi(s.substr(i + 1));
+            score += firstPin + secondPin;
+            currentFrame++;
         } else if (s[i] == 'X') {
             score += 10;
             currentFrame++;
         } else if (s[i] == ' ') {
             i++; // skip the space
         } else {
-            string pinsPart = s.substr(i);
-            int pins = stoi(pinsPart);
+            int pins = stoi(s.substr(i));
             if (pins < 10) {
                 score += pins;
                 currentFrame++;
             } else {
-                score += 10 + (stoi(s.substr(i + 1, 1)));
+                score += 10 + stoi(s.substr(i + 1, 1));
                 currentFrame++;
             }
         }
