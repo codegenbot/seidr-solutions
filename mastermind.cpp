2b8a61db6c57f03958bc7afafc2c67ca20b8514f
beep boop <githubbot@vadim.me>
2024-08-05 16:19:31 +0200

    Solution 93 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0sm0c1rc Prompt: f3112df5-aa08-43fa-b1b3-88e0fbb88362.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ f3112df5-aa08-43fa-b1b3-88e0fbb88362.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ f3112df5-aa08-43fa-b1b3-88e0fbb88362.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -1,7 +1,8 @@
+#include <iostream>
 #include <string>
-#include <vector>
+using namespace std;
 
-std::tuple<int, int> mastermind(std::string code, std::string guess) {
+tuple<int, int> mastermind(string code, string guess) {
     int white = 0;
     int black = 0;
 
@@ -14,12 +15,12 @@ std::tuple<int, int> mastermind(std::string code, std::string guess) {
     for (int i = 0; i < 6; i++) {
         int count = 0;
         for (int j = 0; j < 4; j++) {
-            if (guess[j] == ('A' + i)) {
+            if (guess[j] == 'A' + i) {
                 count++;
             }
         }
-        white += std::min(count, code.count('A' + i)) - black;
+        white += min(count, to_string(code).find(('A' + i)) != string::npos ? to_string(code).count(to_string('A' + i)) : 0) - black;
     }
 
-    return std::make_tuple(white, black);
+    return make_tuple(white, black);
 }
