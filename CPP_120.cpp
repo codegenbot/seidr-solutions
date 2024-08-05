cb02eeee95fb31576a4cd3217f045b206796ca52
beep boop <githubbot@vadim.me>
2024-08-05 14:15:23 +0200

    Solution 4 of CPP/120, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/lh992irp Prompt:       |                    ^~~ 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp:7:27: error: ‘vector’ was not declared in this scope     7 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp:7:34: error: expected primary-expressio n before ‘int’     7 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp:7:39: error: expression list treated as  compound expression in initializer [-fpermissive]     7 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp:6: 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp: In function ‘int main()’: 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp:16:20: error: ‘maximum’ was not declare d in this scope    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |                    ^~~~~~~ 2722493f-795e-4ebe-bbc8-8d56efd192a8.cpp:16:19: error: ‘issame’ cannot be used a s a function    16 |     assert (issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0) , {}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_120.cpp
+++ CPP_120.cpp
@@ -1,21 +1,12 @@
-#include <vector>
 #include <algorithm>
-#include <cassert>
+#include <vector>
 
-using namespace std;
-
-bool issame(const vector<int>& a, const vector<int>& b){
+bool issame(const std::vector<int>& a, const std::vector<int>& b){
     return a == b;
 }
 
-vector<int> maximum(vector<int> arr, int k){
-    sort(arr.begin(), arr.end(), greater<int>());
+std::vector<int> maximum(std::vector<int> arr, int k){
+    std::sort(arr.begin(), arr.end(), std::greater<int>());
     arr.resize(k);
     return arr;
-}
-
-int main(){
-    assert(issame(maximum({1, 2, 3, -23, 243, -400, 0}, 0), {}));
-
-    return 0;
 }
