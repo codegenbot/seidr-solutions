27183a45a76bd313c2e6546d86beb2891f6866ee
beep boop <githubbot@vadim.me>
2024-08-05 11:29:29 +0200

    Solution 20 of CPP/33, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/kukyw9r3 Prompt:     9 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 86cef052-e11a-417c-a340-ec5122786de0.cpp:9:27: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 86cef052-e11a-417c-a340-ec5122786de0.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 86cef052-e11a-417c-a340-ec5122786de0.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 86cef052-e11a-417c-a340-ec5122786de0.cpp:9:34: error: expected primary-expressio n before ‘int’     9 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 86cef052-e11a-417c-a340-ec5122786de0.cpp:8: 86cef052-e11a-417c-a340-ec5122786de0.cpp: In function ‘int main()’: 86cef052-e11a-417c-a340-ec5122786de0.cpp:18:20: error: ‘sort_third’ was not decl ared in this scope    18 |     assert (issame(sort_third({5, 6, 3, 4, 8, 9, 2, 1}) , {2, 6, 3, 4, 8 , 9, 5, 1}));       |                    ^~~~~~~~~~

--- CPP_33.cpp
+++ CPP_33.cpp
@@ -2,5 +2,7 @@
 #include <algorithm>
 
 bool issame(const std::vector<int>& a, const std::vector<int>& b) {
-    return a == b;
+    std::vector<int> temp_a = a;
+    std::sort(temp_a.begin() + 2, temp_a.begin() + 3);
+    return temp_a == b;
 }
