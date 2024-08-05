e7de7435ae8ba7e43f680d5ffdf296815ac69ec8
beep boop <githubbot@vadim.me>
2024-08-05 15:34:03 +0200

    Solution 38 of leaders, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/a6k7q433 Prompt:     6 |     for (int leader : leaders) {       |                       ^~~~~~~       |                       leader 34f95dc8-1689-4d9d-8035-a0ce7598ab6e.cpp:7:14: error: ‘cout’ is not a member of ‘std’     7 |         std::cout << leader << " ";       |              ^~~~ 34f95dc8-1689-4d9d-8035-a0ce7598ab6e.cpp:7:14: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 34f95dc8-1689-4d9d-8035-a0ce7598ab6e.cpp:9:10: error: ‘cout’ is not a member of ‘std’     9 |     std::cout << std::endl;       |          ^~~~ 34f95dc8-1689-4d9d-8035-a0ce7598ab6e.cpp:9:10: note: ‘std::cout’ is defined in h eader ‘<iostream>’; did you forget to ‘#include <iostream>’? 34f95dc8-1689-4d9d-8035-a0ce7598ab6e.cpp:9:23: error: ‘endl’ is not a member of ‘std’     9 |     std::cout << std::endl;       |                       ^~~~ 34f95dc8-1689-4d9d-8035-a0ce7598ab6e.cpp:1:1: note: ‘std::endl’ is defined in he ader ‘<ostream>’; did you forget to ‘#include <ostream>’?   +++ |+#include <ostream>     1 | int main() {

--- leaders.cpp
+++ leaders.cpp
@@ -1,36 +1,27 @@
-#include <iostream>
+#include <iostream> 
 #include <vector>
-#include <algorithm>
 
-std::vector<int> findLeaders(std::vector<int> nums) {
+std::vector<int> findLeaders(const std::vector<int>& nums) {
     std::vector<int> leaders;
-    int n = nums.size();
-    int maxRight = nums[n - 1];
-    leaders.push_back(maxRight);
-    
-    for (int i = n - 2; i >= 0; i--) {
-        if (nums[i] >= maxRight) {
-            maxRight = nums[i];
-            leaders.push_back(maxRight);
+    int max_right = INT_MIN;
+    for (int i = nums.size() - 1; i >= 0; i--) {
+        if (nums[i] >= max_right) {
+            max_right = nums[i];
+            leaders.push_back(max_right);
         }
     }
-    
     std::reverse(leaders.begin(), leaders.end());
-    
     return leaders;
 }
 
 int main() {
-    std::vector<int> nums;
-    int num;
-    while (std::cin >> num) {
-        nums.push_back(num);
-    }
+    std::vector<int> nums = {16, 17, 4, 3, 5, 2};
+    std::vector<int> leaders = findLeaders(nums);
     
-    std::vector<int> result = findLeaders(nums);
-    for (int i = 0; i < result.size(); i++) {
-        std::cout << result[i] << " ";
+    for (int leader : leaders) {
+        std::cout << leader << " ";
     }
+    std::cout << std::endl;
     
     return 0;
 }
