b7da1157b44bb09596421acae1e5c824ffce928b
beep boop <githubbot@vadim.me>
2024-08-05 14:26:49 +0200

    Solution 6 of CPP/123, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/8y6v3yyt Prompt:       |                    ^~~ 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp:25:27: error: ‘vector’ was not declared  in this scope    25 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp:25:34: error: expected primary-expressi on before ‘int’    25 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp:25:39: error: expression list treated a s compound expression in initializer [-fpermissive]    25 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp:24: 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp: In function ‘int main()’: 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp:34:20: error: ‘get_odd_collatz’ was not  declared in this scope    34 |     assert (issame(get_odd_collatz(1) , {1}));       |                    ^~~~~~~~~~~~~~~ 05d7ed9c-de6d-412e-ba0d-3239a482739e.cpp:34:19: error: ‘issame’ cannot be used a s a function    34 |     assert (issame(get_odd_collatz(1) , {1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_123.cpp
+++ CPP_123.cpp
@@ -1,15 +1,39 @@
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
-vector<int> is_same(int n) {
+using namespace std;
+
+bool issame(vector<int> a, vector<int> b) {
+    return a == b;
+}
+
+vector<int> get_odd_collatz(int n) {
     vector<int> result;
     while (n != 1) {
-        if (n % 2 == 1) {
-            result.push_back(n);
+        result.push_back(n);
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
         }
-        n = (n % 2 == 0) ? n / 2 : 3 * n + 1;
     }
     result.push_back(1);
-    sort(result.begin(), result.end());
-    return result;
+    
+    vector<int> odd_numbers;
+    for (int num : result) {
+        if (num % 2 != 0) {
+            odd_numbers.push_back(num);
+        }
+    }
+    
+    sort(odd_numbers.begin(), odd_numbers.end());
+    
+    return odd_numbers;
+}
+
+int main() {
+    assert(issame(get_odd_collatz(1), {1}));
+    
+    return 0;
 }
