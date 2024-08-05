ff8a34220ac59a6fb14621482da70eecbd16a381
beep boop <githubbot@vadim.me>
2024-08-05 09:50:08 +0200

    Solution 44 of Python/7, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/zw3cwfkg Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/e777c9d8-5129-45a6-b014-b6b571a0a 4a4.py", line 10, in <module>     check(filter_by_substring)           ^^^^^^^^^^^^^^^^^^^ NameError: name 'filter_by_substring' is not defined

--- Python_7.py
+++ Python_7.py
@@ -1,2 +1,2 @@
-def filter_strings(strings, substring):
-    return [s for s in strings if substring in s]  #This line should be replaced with def check(function): function(strings, substring)
+def filter_by_substring(strings, substring):
+    return [s for s in strings if substring in s]
