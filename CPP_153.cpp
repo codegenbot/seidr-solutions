da6ceef5f30c8cbb06639d0275f7592489cf905f
beep boop <githubbot@vadim.me>
2024-08-05 15:45:13 +0200

    Solution 14 of CPP/153, ratio 1.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/amtja520 Prompt: f725e047-e571-4248-9f1e-a7a15db2b556.cpp:6:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     6 | string Strongest_Extension(string class_name, vector<string> extensions) ;       | ^~~~~~       | stdin f725e047-e571-4248-9f1e-a7a15db2b556.cpp:8:1: error: ‘string’ does not name a ty pe; did you mean ‘stdin’?     8 | string Strongest_Extension(string class_name, vector<string> extensions)  {       | ^~~~~~       | stdin In file included from f725e047-e571-4248-9f1e-a7a15db2b556.cpp:23: f725e047-e571-4248-9f1e-a7a15db2b556.cpp: In function ‘int main()’: f725e047-e571-4248-9f1e-a7a15db2b556.cpp:25:13: error: ‘Strongest_Extension’ was  not declared in this scope    25 |     assert (Strongest_Extension("Sp", {"671235", "Bb"}) == "Sp.671235");       |             ^~~~~~~~~~~~~~~~~~~

--- CPP_153.cpp
+++ CPP_153.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+string Strongest_Extension(string class_name, vector<string> extensions);
+
 string Strongest_Extension(string class_name, vector<string> extensions) {
     string strongest_extension = extensions[0];
     int max_strength = count_if(strongest_extension.begin(), strongest_extension.end(), ::isupper) - count_if(strongest_extension.begin(), strongest_extension.end(), ::islower);
