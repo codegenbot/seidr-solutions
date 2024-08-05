476f128b921349f891046707fc9491c4da03776d
beep boop <githubbot@vadim.me>
2024-08-05 16:04:01 +0200

    Solution 44 of CPP/155, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/vp960k5d Prompt:       |             ^~~~~~ 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp:6:20: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                    ^~~ 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp:6:27: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp:6:34: error: expected primary-expressio n before ‘int’     6 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ In file included from 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp:5: 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp: In function ‘int main()’: 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp:15:20: error: ‘even_odd_count’ was not declared in this scope    15 |     assert (issame(even_odd_count(0) , {1, 0}));       |                    ^~~~~~~~~~~~~~ 5df1ba13-98b7-4989-8d94-bb8401f17bea.cpp:15:19: error: ‘issame’ cannot be used a s a function    15 |     assert (issame(even_odd_count(0) , {1, 0}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_155.cpp
+++ CPP_155.cpp
@@ -1,11 +1,5 @@
 #include <vector>
 
-bool issame(std::vector<int> a, std::vector<int> b) {
-    if(a.size() != b.size()) return false;
-    
-    for(int i=0; i<a.size(); i++) {
-        if(a[i] != b[i]) return false;
-    }
-    
-    return true;
+bool issame(std::vector<int>& a, std::vector<int>& b) {
+    return a == b;
 }
