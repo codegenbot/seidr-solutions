840c0f335c66557d41be63db315c8f6d3111521a
beep boop <githubbot@vadim.me>
2024-08-05 14:08:45 +0200

    Solution 80 of Python/27, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/sia0f4lb Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/ffc748d5-6636-4564-801e-fa0deed83 d62.py", line 4, in <module>     check(swap_case)     ^^^^^ NameError: name 'check' is not defined

--- Python_27.py
+++ Python_27.py
@@ -1,8 +1,13 @@
 def swap_case(string: str) -> str:
-    return "".join([char.lower() if char.isupper() else char.upper() for char in string])
+    return "".join(
+        [char.lower() if char.isupper() else char.upper() for char in string]
+    )
+
 
 def check(func):
-    test_string = input()
-    print(func(test_string))
+    test_string = input("Enter a string: ")
+    result = func(test_string)
+    print(result)
+
 
 check(swap_case)
