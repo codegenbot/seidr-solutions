648ad318d19911418c90d846fbd86c8e2b6d0c16
beep boop <githubbot@vadim.me>
2024-08-05 22:49:37 +0200

    Solution 3 of CPP/25, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/08xt4hw0 Prompt:       |                    ^~~ 936b3398-0379-41ab-9722-cac52f13e4ad.cpp:13:27: error: ‘vector’ was not declared  in this scope    13 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 936b3398-0379-41ab-9722-cac52f13e4ad.cpp:13:34: error: expected primary-expressi on before ‘int’    13 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 936b3398-0379-41ab-9722-cac52f13e4ad.cpp:13:39: error: expression list treated a s compound expression in initializer [-fpermissive]    13 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 936b3398-0379-41ab-9722-cac52f13e4ad.cpp:12: 936b3398-0379-41ab-9722-cac52f13e4ad.cpp: In function ‘int main()’: 936b3398-0379-41ab-9722-cac52f13e4ad.cpp:22:20: error: ‘factorize’ was not decla red in this scope    22 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |                    ^~~~~~~~~ 936b3398-0379-41ab-9722-cac52f13e4ad.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(factorize(3 * 2 * 3) , {2, 3, 3}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_25.cpp
+++ CPP_25.cpp
@@ -1,13 +1,13 @@
 #include <vector>
 
-bool issame(const vector<int> &a, const vector<int> &b){
-    if(a.size() != b.size()){
+vector<int> factorize(int n);
+
+bool issame(vector<int> a, vector<int> b){
+    if(a.size() != b.size())
         return false;
-    }
     for(int i = 0; i < a.size(); i++){
-        if(a[i] != b[i]){
+        if(a[i] != b[i])
             return false;
-        }
     }
     return true;
 }
