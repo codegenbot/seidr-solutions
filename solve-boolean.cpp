a5e1f9bba048ebf6bfb48659d4e92eb8c5906f44
beep boop <githubbot@vadim.me>
2024-08-05 17:19:08 +0200

    Solution 13 of solve-boolean, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/7og8tmhn Prompt: Given a string representing a Boolean expression consisting of T, F, |, and &, evaluate it and return the resulting Boolean.

--- solve-boolean.cpp
+++ solve-boolean.cpp
@@ -2,43 +2,44 @@
 #include <iostream>
 using namespace std;
 
-bool evaluateBooleanExpression(string expression) {
-    int i = 0;
-    bool result = false;
-
-    while (i < expression.length()) {
-        if (expression[i] == 'T') {
-            return true;
-        } else if (expression[i] == 'F') {
-            return false;
-        } else if (expression[i] == '|') {
-            i++;
-            if (expression[i] == '&') {
-                i++;
+string solveBoolean(string s) {
+    stack<char> st;
+    
+    for(int i=0; i<s.length();i++) {
+        if(s[i] == '&') {
+            while(st.size() && st.top() == '&') {
+                st.pop();
             }
-        } else if (expression[i] == '&') {
-            i++;
-            if (i < expression.length() && expression[i] == '|') {
-                i++;
+            if(st.empty()) {
+                return "True";
+            } else {
+                st.pop();
+                return "False";
             }
+        } 
+        else if(s[i] == '|') {
+            while(!st.empty()) {
+                st.pop();
+            }
+        } 
+        else {
+            st.push(s[i]);
         }
     }
-
-    return result;
+    
+    if(st.size() > 0) {
+        return st.top() == 'T' ? "True" : "False";
+    } else {
+        return "True";
+    }
 }
 
 int main() {
-    string input;
-    cout << "Enter a Boolean expression (e.g., t, f, f&f, etc.): ";
-    cin >> input;
-
-    bool output = evaluateBooleanExpression(input);
-
-    if (output) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
+    string s;
+    cout << "Enter a Boolean expression (e.g., t, f, f&f): ";
+    cin >> s;
+    
+    cout << solveBoolean(s) << endl;
 
     return 0;
 }
