86f2a8373196f95811e62a752879444632b9ee18
beep boop <githubbot@vadim.me>
2024-08-05 19:14:06 +0200

    Solution 16 of Python/129, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/blsh23z5 Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c71564c3-02ca-4cec-bb17-21abc8e30 580.py", line 29, in <module>     check(minPath)   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c71564c3-02ca-4cec-bb17-21abc8e30 580.py", line 26, in check     assert minPath([[1, 3], [3, 2]], 10) == [1, 3, 1, 3, 1, 3, 1, 3, 1, 3]            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/c71564c3-02ca-4cec-bb17-21abc8e30 580.py", line 17, in minPath     next_cell = min(neighbors)                 ^^^^^^^^^^^^^^ ValueError: min() arg is an empty sequence

--- Python_129.py
+++ Python_129.py
@@ -14,10 +14,8 @@ def minPath(grid, k):
                     for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
                         if 0 <= x < n and 0 <= y < n and grid[x][y] not in visited:
                             neighbors.append(grid[x][y])
-
         if not neighbors:
             break
-
         next_cell = min(neighbors)
         path.append(next_cell)
         visited.add(next_cell)
