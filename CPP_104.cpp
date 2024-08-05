c64ba0bbd4573f1c41481772f487fd65465cc691
beep boop <githubbot@vadim.me>
2024-08-05 13:16:16 +0200

    Solution 9 of CPP/104, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/sciflv1y Prompt:       |                    ^~~ 331aba14-ad09-495b-bac5-bc3f915c4284.cpp:21:27: error: ‘vector’ was not declared  in this scope    21 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 331aba14-ad09-495b-bac5-bc3f915c4284.cpp:21:34: error: expected primary-expressi on before ‘int’    21 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 331aba14-ad09-495b-bac5-bc3f915c4284.cpp:21:39: error: expression list treated a s compound expression in initializer [-fpermissive]    21 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 331aba14-ad09-495b-bac5-bc3f915c4284.cpp:20: 331aba14-ad09-495b-bac5-bc3f915c4284.cpp: In function ‘int main()’: 331aba14-ad09-495b-bac5-bc3f915c4284.cpp:30:20: error: ‘unique_digits’ was not d eclared in this scope    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |                    ^~~~~~~~~~~~~ 331aba14-ad09-495b-bac5-bc3f915c4284.cpp:30:19: error: ‘issame’ cannot be used a s a function    30 |     assert (issame(unique_digits({135, 103, 31}) , {31, 135}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_104.cpp
+++ CPP_104.cpp
@@ -1,22 +1,16 @@
 #include <vector>
 #include <algorithm>
 
-vector<int> unique_digits(const vector<int>& x) {
-    vector<int> result;
-    for (int num : x) {
-        int temp = num;
-        bool hasEvenDigit = false;
-        while (temp > 0) {
-            if (temp % 2 == 0) {
-                hasEvenDigit = true;
-                break;
-            }
-            temp /= 10;
-        }
-        if (!hasEvenDigit) {
-            result.push_back(num);
+bool issame(vector<int> a, vector<int> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    sort(result.begin(), result.end());
-    return result;
+    return true;
 }
