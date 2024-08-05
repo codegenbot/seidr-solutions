123e38c138b2e1fe85fd4d21e3c30275eade190c
beep boop <githubbot@vadim.me>
2024-08-05 15:36:35 +0200

    Solution 12 of CPP/151, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/67vlykti Prompt: ‘std’     7 |         if (num > 0 && std::fmod(num, 1) == 0 && ((int)num) % 2 != 0) { // Add "std::" before "fmod"       |                             ^~~~ 66735e49-3b5f-4a51-ae73-4e606f467151.cpp: At global scope: 66735e49-3b5f-4a51-ae73-4e606f467151.cpp:22:5: error: redefinition of ‘int main( )’    22 | int main(){       |     ^~~~ 66735e49-3b5f-4a51-ae73-4e606f467151.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main(){       |     ^~~~ In file included from 66735e49-3b5f-4a51-ae73-4e606f467151.cpp:21: 66735e49-3b5f-4a51-ae73-4e606f467151.cpp: In function ‘int main()’: 66735e49-3b5f-4a51-ae73-4e606f467151.cpp:23:35: error: ‘lst’ was not declared in  this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                   ^~~ 66735e49-3b5f-4a51-ae73-4e606f467151.cpp:23:43: error: ‘odd_sum’ was not declare d in this scope    23 |     assert (double_the_difference(lst) == odd_sum );       |                                           ^~~~~~~

--- CPP_151.cpp
+++ CPP_151.cpp
@@ -13,7 +13,7 @@ long long double_the_difference(std::vector<float> lst) {
 }
 
 int main(){
-    std::vector<float> lst = {1.5, 2.5, 3.5, 4.5, 5.5};
+    std::vector<float> lst = {1.5, 2.5, 3.5, 4.5, 5.5}; 
     long long odd_sum = double_the_difference(lst);
     assert(odd_sum == 35);
     return 0;
