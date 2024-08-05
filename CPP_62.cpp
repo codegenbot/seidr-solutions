a837579bea73c50bc78ea4407ede4291fdba26e6
beep boop <githubbot@vadim.me>
2024-08-05 11:59:36 +0200

    Solution 73 of CPP/62, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/ouvpueue Prompt:       |                             ^~~~~~ 2d562125-866c-42a6-8db7-b15126ef2aa3.cpp:22:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from 2d562125-866c-42a6-8db7-b15126ef2aa3.cpp:1: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from 2d562125-866c-42a6-8db7-b15126ef2aa3.cpp:1: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ 2d562125-866c-42a6-8db7-b15126ef2aa3.cpp:22:36: error: expected primary-expressi on before ‘float’    22 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 2d562125-866c-42a6-8db7-b15126ef2aa3.cpp:30:5: error: redefinition of ‘int main( )’    30 | int main(){       |     ^~~~ 2d562125-866c-42a6-8db7-b15126ef2aa3.cpp:16:5: note: ‘int main()’ previously def ined here    16 | int main(){       |     ^~~~

--- CPP_62.cpp
+++ CPP_62.cpp
@@ -13,7 +13,7 @@ bool issame(const std::vector<float>& a, const std::vector<float>& b){
     return a == b;
 }
 
-int main(){
+int main() {
     assert(issame(derivative({1}), std::vector<float>{}));
     return 0;
 }
