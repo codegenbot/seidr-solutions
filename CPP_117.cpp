609ba9054b5a7bac330c0b85824011071f1119ed
beep boop <githubbot@vadim.me>
2024-08-05 14:16:29 +0200

    Solution 58 of CPP/117, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/jpk1psr4 Prompt:       |                                     ^~~~~~ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:50:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:50:44: error: ‘b’ was not declared in t his scope    50 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:58:5: error: redefinition of ‘int main( )’    58 | int main(){       |     ^~~~ 61bcb8c3-9aed-4667-91f2-098e44bdea9c.cpp:44:5: note: ‘int main()’ previously def ined here    44 | int main() {       |     ^~~~

--- CPP_117.cpp
+++ CPP_117.cpp
@@ -2,34 +2,46 @@
 #include <string>
 #include <cassert>
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b){
-    return a == b;
-}
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
 
-std::vector<std::string> select_words(std::string words, int index){
-    std::vector<std::string> result;
-    std::string temp = "";
-    for (char c : words) {
-        if (c == ' ') {
-            if (!temp.empty()) {
-                if (index > 0) {
-                    index--;
-                } else {
-                    result.push_back(temp);
-                }
-                temp.clear();
-            }
-        } else {
-            temp += c;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
         }
     }
-    if (!temp.empty() && index == 0) {
-        result.push_back(temp);
-    }
-    return result;
+
+    return true;
 }
 
-int main(){
+std::vector<std::string> select_words(std::string words, int index) {
+    std::vector<std::string> selected_words;
+    std::string word = "";
+    for (char c : words) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                if (index != 0) {
+                    --index;
+                } else {
+                    selected_words.push_back(word);
+                }
+                word = "";
+            }
+        } else {
+            word += c;
+        }
+    }
+
+    if (!word.empty()) {
+        selected_words.push_back(word);
+    }
+
+    return selected_words;
+}
+
+int main() {
     assert(issame(select_words("a b c d e f", 1), {"b", "c", "d", "f"}));
     return 0;
 }
