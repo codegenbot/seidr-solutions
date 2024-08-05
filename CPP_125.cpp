5152a1f4840c0517ad5dc20b50ea9ee332164b3b
beep boop <githubbot@vadim.me>
2024-08-05 14:38:25 +0200

    Solution 42 of CPP/125, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/scuhoit7 Prompt:       |                    ^~~~~~ d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp:6:28: error: ‘a’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                            ^ d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp:6:30: error: ‘vector’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                              ^~~~~~ d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp:6:37: error: ‘string’ was not declared in this scope     6 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp:6:44: error: ‘b’ was not declared in th is scope     6 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp:5: d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp: In function ‘int main()’: d1b78cc1-3713-4381-b80e-6a652e66f57f.cpp:15:20: error: ‘split_words’ was not dec lared in this scope    15 |     assert (issame(split_words("") ,{"0"}));       |                    ^~~~~~~~~~~

--- CPP_125.cpp
+++ CPP_125.cpp
@@ -1,47 +1,6 @@
 #include <vector>
 #include <string>
-#include <algorithm>
 
-using namespace std;
-
-vector<string> split_words(string txt);
-
-bool issame(const vector<string>& a, const vector<string>& b);
-
-vector<string> split_words(string txt){
-    vector<string> result;
-    string word = "";
-    for(char c : txt){
-        if(c == ' ' || c == ','){
-            if(!word.empty()){
-                result.push_back(word);
-                word = "";
-            }
-        } else {
-            word += c;
-        }
-    }
-    if(!word.empty()){
-        result.push_back(word);
-    }
-    if(result.empty()){
-        int oddCount = 0;
-        for(char c : txt){
-            if(islower(c) && (c - 'a') % 2 == 1){
-                oddCount++;
-            }
-        }
-        result.push_back(to_string(oddCount));
-    }
-    return result;
+bool issame(const std::vector<std::string>& a, const std::vector<std::string>& b){
+    return a == b;
 }
-
-bool issame(const vector<string>& a, const vector<string>& b){
-    return std::equal(a.begin(), a.end(), b.begin());
-}
-
-int main(){
-    assert(issame(split_words(""), vector<string>{"0"}));
-    
-    return 0;
-}  
