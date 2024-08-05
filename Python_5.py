4c44ea492a23a805bcc66cf0e1dd2627f0840efb
beep boop <githubbot@vadim.me>
2024-08-05 08:56:30 +0200

    Solution 49 of Python/5, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/dpdfgo9j Prompt: Program did not receive expected input

--- Python_5.py
+++ Python_5.py
@@ -1,7 +1,4 @@
 from typing import List
 
-
 def intersperse(numbers: List[int], delimiter: int) -> List[int]:
-    return [
-        x for pair in zip(numbers, [delimiter] * (len(numbers) - 1)) for x in pair
-    ] + [numbers[-1]]
+    return [x for pair in zip(numbers, [delimiter] * (len(numbers) - 1)) for x in pair] + [numbers[-1]]
