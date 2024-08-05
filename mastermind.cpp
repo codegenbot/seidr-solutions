8ad98c0f1db16a19b30739744dc00be84fa78324
beep boop <githubbot@vadim.me>
2024-08-05 15:55:05 +0200

    Solution 55 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/0sm0c1rc Prompt: f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:16: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                ^~~~~~ f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:29: error: ‘string’ was not declared in this scope     1 | int mastermind(string code, string guess) {       |                             ^~~~~~ f466b4cc-b0c2-4686-88b6-08309f598240.cpp:1:41: error: expression list treated as  compound expression in initializer [-fpermissive]     1 | int mastermind(string code, string guess) {       |                                         ^

--- mastermind.cpp
+++ mastermind.cpp
@@ -10,25 +10,17 @@ int mastermind(string code, string guess) {
     for (char c : code) {
         codeCount[c]++;
     }
-
     for (char c : guess) {
         guessCount[c]++;
     }
 
-    // Count correct colors in wrong places (white pegs)
     for (int i = 0; i < 4; ++i) {
         if (code[i] == guess[i]) {
             black++;
-        } else if (codeCount[guess[i]] > 0) {
-            white++;
-            codeCount[guess[i]]--;
-        }
-    }
-
-    // Count correct colors in correct places (black pegs)
-    for (int i = 0; i < 4; ++i) {
-        if (code[i] == guess[i]) {
-            black++;
+        } else {
+            codeCount[code[i]]--;
+            guessCount[guess[i]]--;
+            if (codeCount[code[i]] >= 0 && guessCount[guess[i]] >= 0) white++;
         }
     }
 
