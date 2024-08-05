a65221dc1df989d064e1b5fbbcfcb58668e05725
beep boop <githubbot@vadim.me>
2024-08-05 13:04:22 +0200

    Solution 4 of CPP/100, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/p6c24rc7 Prompt:       |                    ^~~ 97b790ec-63a8-447b-89cf-321915c53c9c.cpp:16:27: error: ‘vector’ was not declared  in this scope    16 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 97b790ec-63a8-447b-89cf-321915c53c9c.cpp:16:34: error: expected primary-expressi on before ‘int’    16 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 97b790ec-63a8-447b-89cf-321915c53c9c.cpp:16:39: error: expression list treated a s compound expression in initializer [-fpermissive]    16 | bool issame(vector<int> a,vector<int>b){       |                                       ^ In file included from 97b790ec-63a8-447b-89cf-321915c53c9c.cpp:15: 97b790ec-63a8-447b-89cf-321915c53c9c.cpp: In function ‘int main()’: 97b790ec-63a8-447b-89cf-321915c53c9c.cpp:25:20: error: ‘make_a_pile’ was not dec lared in this scope    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |                    ^~~~~~~~~~~ 97b790ec-63a8-447b-89cf-321915c53c9c.cpp:25:19: error: ‘issame’ cannot be used a s a function    25 |     assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_100.cpp
+++ CPP_100.cpp
@@ -2,12 +2,16 @@
 #include <vector>
 #include <cassert>
 
-bool issame(const std::vector<int>& a, const std::vector<int>& b){
+using namespace std;
+
+vector<int> make_a_pile(int n);
+
+bool issame(vector<int> a, vector<int> b){
     return a == b;
 }
 
-std::vector<int> make_a_pile(int n){
-    std::vector<int> result;
+vector<int> make_a_pile(int n){
+    vector<int> result;
     result.push_back(n);
     for(int i = 1; i < n; ++i){
         if(n % 2 == 0){
@@ -20,8 +24,10 @@ std::vector<int> make_a_pile(int n){
     return result;
 }
 
-int main() {
-    assert (issame(make_a_pile(8) , {8, 10, 12, 14, 16, 18, 20, 22}));
-
+int main(){
+    assert(issame(make_a_pile(8), {8, 10, 12, 14, 16, 18, 20, 22}));
+    
+    cout << "Test passed!";
+    
     return 0;
 }
