8bee895499154a824f72e79d7159647fa1b7c016
beep boop <githubbot@vadim.me>
2024-08-05 15:19:30 +0200

    Solution 90 of CPP/136, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/pl1x98g3 Prompt: on before ‘int’    38 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ dde6897f-90f7-45b0-9865-555850435baf.cpp:38:39: error: expression list treated a s compound expression in initializer [-fpermissive]    38 | bool issame(vector<int> a,vector<int>b){       |                                       ^ dde6897f-90f7-45b0-9865-555850435baf.cpp:46:5: error: redefinition of ‘int main( )’    46 | int main(){       |     ^~~~ dde6897f-90f7-45b0-9865-555850435baf.cpp:28:5: note: ‘int main()’ previously def ined here    28 | int main() {       |     ^~~~ In file included from dde6897f-90f7-45b0-9865-555850435baf.cpp:37: dde6897f-90f7-45b0-9865-555850435baf.cpp: In function ‘int main()’: dde6897f-90f7-45b0-9865-555850435baf.cpp:47:19: error: ‘issame’ cannot be used a s a function    47 |     assert (issame(largest_smallest_integers({-6, -4, -4, -3, -100, 1}) , {-3, 1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~

--- CPP_136.cpp
+++ CPP_136.cpp
@@ -7,10 +7,10 @@ std::vector<int> largest_smallest_integers(std::vector<int> lst) {
     int max_neg = INT_MIN;
     int min_pos = INT_MAX;
     
-    for(int num : lst){
-        if(num < 0 && num > max_neg){
+    for(int num : lst) {
+        if(num < 0 && num > max_neg) {
             max_neg = num;
-        } else if(num > 0 && num < min_pos){
+        } else if(num > 0 && num < min_pos) {
             min_pos = num;
         }
     }
@@ -26,10 +26,7 @@ bool is_same(std::vector<int> a, std::vector<int> b) {
 }
 
 int main() {
-    std::vector<int> input = {-6, -4, -4, -3, -100, 1};
-    std::vector<int> expected_output = {-100, -3};
-    
-    assert(is_same(largest_smallest_integers(input), expected_output));
+    assert(is_same(largest_smallest_integers({-6, -4, -4, -3, -100, 1}), {-100, -3}));
     
     return 0;
 }
