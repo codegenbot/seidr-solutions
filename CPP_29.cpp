b6c7c5b2232366747e1e559ec05c507d798b51a6
beep boop <githubbot@vadim.me>
2024-08-06 10:02:12 +0200

    Solution 4 of CPP/29, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/dnlynkij Prompt:    12 | bool issame(vector<string> a,vector<string>b){       |                                     ^~~~~~ 8ec99c71-81e5-485a-bf02-615479344436.cpp:12:44: error: ‘b’ was not declared in t his scope    12 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 8ec99c71-81e5-485a-bf02-615479344436.cpp:12:45: error: expression list treated a s compound expression in initializer [-fpermissive]    12 | bool issame(vector<string> a,vector<string>b){       |                                             ^ In file included from 8ec99c71-81e5-485a-bf02-615479344436.cpp:11: 8ec99c71-81e5-485a-bf02-615479344436.cpp: In function ‘int main()’: 8ec99c71-81e5-485a-bf02-615479344436.cpp:21:20: error: ‘filter_by_prefix’ was no t declared in this scope    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |                    ^~~~~~~~~~~~~~~~ 8ec99c71-81e5-485a-bf02-615479344436.cpp:21:19: error: ‘issame’ cannot be used a s a function    21 |     assert (issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "x xxAAA", "xxx"}, "xxx") , {"xxx", "xxxAAA", "xxx"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_29.cpp
+++ CPP_29.cpp
@@ -5,22 +5,12 @@
 
 using namespace std;
 
-vector<string> filter_by_prefix(vector<string> strings, string prefix){
-    vector<string> result;
-    for (const string& str : strings) {
-        if (str.substr(0, prefix.size()) == prefix) {
-            result.push_back(str);
-        }
-    }
-    return result;
-}
-
-bool issame(vector<string> a, vector<string> b){
-    if(a.size() != b.size()){
+bool issame(vector<string> a, vector<string> b) {
+    if (a.size() != b.size()) {
         return false;
     }
-    for(int i=0; i<a.size(); i++){
-        if(a[i] != b[i]){
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
             return false;
         }
     }
@@ -29,5 +19,8 @@ bool issame(vector<string> a, vector<string> b){
 
 int main() {
     assert(issame(filter_by_prefix({"xxx", "asd", "xxy", "john doe", "xxxAAA", "xxx"}, "xxx"), {"xxx", "xxxAAA", "xxx"}));
+    
+    cout << "Test passed!" << endl;
+
     return 0;
 }
