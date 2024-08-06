7cfc92efb41229a4ed5e53efe60382985c151632
beep boop <githubbot@vadim.me>
2024-08-06 09:51:55 +0200

    Solution 4 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1n54vfdm Prompt:    14 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ 40bc2368-27e2-438e-8c3f-dd600632f014.cpp:14:36: error: expected primary-expressi on before ‘float’    14 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ 40bc2368-27e2-438e-8c3f-dd600632f014.cpp:14:43: error: expression list treated a s compound expression in initializer [-fpermissive]    14 | bool issame(vector<float> a,vector<float>b){       |                                           ^ In file included from 40bc2368-27e2-438e-8c3f-dd600632f014.cpp:13: 40bc2368-27e2-438e-8c3f-dd600632f014.cpp: In function ‘int main()’: 40bc2368-27e2-438e-8c3f-dd600632f014.cpp:23:20: error: ‘find_closest_elements’ w as not declared in this scope    23 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~ 40bc2368-27e2-438e-8c3f-dd600632f014.cpp:23:19: error: ‘issame’ cannot be used a s a function    23 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,8 +1,10 @@
-pair<float, float> find_closest_elements(vector<float>& numbers);
+bool issame(vector<float> a, vector<float> b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    return a == b;
+}
 
-bool issame(pair<float, float> a, pair<float, float> b);
-
-pair<float, float> find_closest_elements(vector<float>& numbers){
+pair<float, float> find_closest_elements(vector<float> numbers) {
     sort(numbers.begin(), numbers.end());
     float min_diff = numbers[1] - numbers[0];
     pair<float, float> closest_elements = {numbers[0], numbers[1]};
@@ -14,3 +16,5 @@ pair<float, float> find_closest_elements(vector<float>& numbers){
     }
     return closest_elements;
 }
+
+issame({2.2, 3.1}, find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}));
