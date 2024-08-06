2da72d3189a10326e9d48f1c7433b82ef42d1f15
beep boop <githubbot@vadim.me>
2024-08-06 12:27:16 +0200

    Solution 35 of cut-vector, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-gpt-3.5-turbo-run6/runs/yd1li0xv Prompt: Program did not receive expected input

--- cut-vector.cpp
+++ cut-vector.cpp
@@ -9,31 +9,31 @@ using namespace std;
 int main() {
     vector<int> nums;
     int num;
-    
+
     int diff = INT_MAX;
     int idx = -1;
-    
+
     for (int i = 0; cin >> num; ++i) {
         nums.push_back(num);
 
         int left_sum = accumulate(nums.begin(), nums.begin() + i, 0);
         int right_sum = accumulate(nums.begin() + i, nums.end(), 0);
-        
+
         if (abs(left_sum - right_sum) < diff) {
             diff = abs(left_sum - right_sum);
             idx = i;
         }
     }
-    
+
     for (int i = 0; i < idx; ++i) {
         cout << nums[i] << endl;
     }
-    
+
     cout << endl;
-    
+
     for (int i = idx; i < nums.size(); ++i) {
         cout << nums[i] << endl;
     }
-    
+
     return 0;
 }
