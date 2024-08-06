ef3fdc64a0bcec03f52046c7c9890267d6c14de3
beep boop <githubbot@vadim.me>
2024-08-06 09:59:25 +0200

    Solution 46 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1n54vfdm Prompt:     9 | bool issame(vector<float> a,vector<float>b){       |                             ^~~~~~ b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp:9:29: note: suggested alternatives: In file included from /usr/include/c++/11/vector:67,                  from b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp:2: /usr/include/c++/11/bits/stl_vector.h:389:11: note:   ‘std::vector’   389 |     class vector : protected _Vector_base<_Tp, _Alloc>       |           ^~~~~~ In file included from b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp:2: /usr/include/c++/11/vector:86:13: note:   ‘std::pmr::vector’    86 |       using vector = std::vector<_Tp, polymorphic_allocator<_Tp>>;       |             ^~~~~~ b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp:9:36: error: expected primary-expressio n before ‘float’     9 | bool issame(vector<float> a,vector<float>b){       |                                    ^~~~~ In file included from b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp:8: b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp: In function ‘int main()’: b3d3e9a6-4158-44bb-b5ea-c505a8cade35.cpp:18:20: error: ‘find_closest_elements’ w as not declared in this scope    18 |     assert (issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) , {2 .2, 3.1}));       |                    ^~~~~~~~~~~~~~~~~~~~~

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,9 +1,12 @@
+#include <utility>
 #include <vector>
 #include <algorithm>
 
-bool issame(std::vector<float> a, std::vector<float> b) {
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    
+bool find_closest_elements(const std::vector<float>& a, const std::vector<float>& b) {
     return a == b;
+}
+
+int main() {
+    assert(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}) == {2.2, 3.1});
+    return 0;
 }
