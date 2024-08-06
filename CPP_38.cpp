fd074d2b2d55cf13cdd07c47e85daf9644982239
beep boop <githubbot@vadim.me>
2024-08-06 10:09:12 +0200

    Solution 14 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt: 09639225-dd1c-4b23-b25b-667425c80f99.cpp:23:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    23 |             str+=chr;       |             ^~~       |             std 09639225-dd1c-4b23-b25b-667425c80f99.cpp:26:15: error: expected ‘;’ before ‘enco ded_str’    26 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 09639225-dd1c-4b23-b25b-667425c80f99.cpp:13: 09639225-dd1c-4b23-b25b-667425c80f99.cpp:27:31: error: ‘encoded_str’ was not dec lared in this scope    27 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 09639225-dd1c-4b23-b25b-667425c80f99.cpp:27:17: error: ‘decode_cyclic’ was not d eclared in this scope    27 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ 09639225-dd1c-4b23-b25b-667425c80f99.cpp:27:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    27 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,27 +1,18 @@
-#include <string>
-#include <cassert>
-
-string encode_cyclic(string str){
-    string output;
-    int i;
-    for (i=0; i*3<str.length(); i++) {
-        string x = str.substr(i*3, 3);
-        if (x.length() == 3) 
-            x = x.substr(1,2) + x[0];
-        output += x;
+string encode_cyclic(string str) {
+    string encoded_str;
+    for (int i = 0; i < str.length(); i++) {
+        encoded_str += str[i] + str[(i + 2) % str.length()] + str[(i + 1) % str.length()];
     }
-    return output;
+    return encoded_str;
 }
 
-string decode_cyclic(string str){
+string decode_cyclic(string encoded_str) { 
     string output;
-    string chr;
-    int i;
-    for (i=0; i*3<str.length(); i++) {
-        string x = str.substr(i*3, 3);
-        if (x.length() == 3) 
-            chr = x[2] + x.substr(0, 2);
-        output += chr;
+    for (int i = 0; i * 3 < encoded_str.length(); i++) {
+        string str = encoded_str.substr(i * 3, 3);
+        if (str.length() == 3) 
+            str = str[2] + str.substr(0, 2);
+        output += str[2] + str.substr(0, 2);
     }
     return output;
 }
