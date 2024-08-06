1369796c91dc2fe9ca682f1d0a4acdfc7a92d05a
beep boop <githubbot@vadim.me>
2024-08-06 10:26:04 +0200

    Solution 79 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2wo02f89 Prompt: 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp:11:27: error: ‘vector’ was not declared  in this scope    11 | bool issame(vector<int> a,vector<int>b){       |                           ^~~~~~ 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp:11:34: error: expected primary-expressi on before ‘int’    11 | bool issame(vector<int> a,vector<int>b){       |                                  ^~~ 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp:19:5: error: redefinition of ‘int main( )’    19 | int main(){       |     ^~~~ 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp:5:5: note: ‘int main()’ previously defi ned here     5 | int main() {       |     ^~~~ In file included from 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp:10: 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp: In function ‘int main()’: 3fb4abc7-4170-40ea-a3e3-c0304270a69e.cpp:20:20: error: ‘incr_list’ was not decla red in this scope    20 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |                    ^~~~~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 #include <cassert>
 
-std::vector<int> incr_list(std::vector<int> v) {
+vector<int> incr_list(vector<int> v) {
     for (int &num : v) {
         num++;
     }
@@ -9,6 +9,6 @@ std::vector<int> incr_list(std::vector<int> v) {
 }
 
 int main() {
-    assert(incr_list(std::vector<int>{5, 2, 5, 2, 3, 3, 9, 0, 123}) == std::vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124});
+    assert(incr_list(vector<int>{5, 2, 5, 2, 3, 3, 9, 0, 123}) == vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124});
     return 0;
 }
