a83818c8efee8d1a9f5a14c95479d14077d7fa85
beep boop <githubbot@vadim.me>
2024-08-06 10:05:06 +0200

    Solution 15 of CPP/35, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/p8kztqm8 Prompt:     3 | e0cab488-2675-4214-a310-6f1b765824f4.cpp: At global scope: e0cab488-2675-4214-a310-6f1b765824f4.cpp:20:5: error: redefinition of ‘int main( )’    20 | int main(){       |     ^~~~ e0cab488-2675-4214-a310-6f1b765824f4.cpp:14:5: note: ‘int main()’ previously def ined here    14 | int main() {       |     ^~~~ In file included from e0cab488-2675-4214-a310-6f1b765824f4.cpp:19: e0cab488-2675-4214-a310-6f1b765824f4.cpp: In function ‘int main()’: e0cab488-2675-4214-a310-6f1b765824f4.cpp:21:28: error: cannot convert ‘<brace-en closed initializer list>’ to ‘int’    21 |     assert (abs(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})- 12 4)<1e-4);       |                 ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~       |                            |       |                            <brace-enclosed initializer list> e0cab488-2675-4214-a310-6f1b765824f4.cpp:4:19: note:   initializing argument 1 o f ‘float max_element(int)’     4 | float max_element(const vector<float> &l){       |                   ^~~~~~~~~~~~

--- CPP_35.cpp
+++ CPP_35.cpp
@@ -13,6 +13,6 @@ float max_element(const vector<int> &l){
 }
 
 int main() {
-    assert(std::abs( static_cast<float>(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})) - 124) < 1e-4);
+    assert(std::abs(static_cast<float>(max_element({5, 3, -5, 2, -3, 3, 9, 0, 124, 1, -10})) - 124) < 1e-4);
     return 0;
 }
