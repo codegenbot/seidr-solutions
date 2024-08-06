1d2e76ab369329379cfc01cdbbf2422a2c59dec9
beep boop <githubbot@vadim.me>
2024-08-06 10:07:27 +0200

    Solution 88 of CPP/20, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/1n54vfdm Prompt:     5 | 18977e3e-9d97-4d87-8eb8-4c682987c2a4.cpp:41:9: warning: empty parentheses were d isambiguated as a function declaration [-Wvexing-parse]    41 | int main(){       |         ^~ 18977e3e-9d97-4d87-8eb8-4c682987c2a4.cpp:41:9: note: remove parentheses to defau lt-initialize a variable    41 | int main(){       |         ^~       |         -- 18977e3e-9d97-4d87-8eb8-4c682987c2a4.cpp:41:9: note: or replace parentheses with  braces to value-initialize a variable 18977e3e-9d97-4d87-8eb8-4c682987c2a4.cpp:41:11: error: a function-definition is not allowed here before ‘{’ token    41 | int main(){       |           ^ 18977e3e-9d97-4d87-8eb8-4c682987c2a4.cpp:43:2: error: expected ‘}’ at end of inp ut    43 | }       |  ^ 18977e3e-9d97-4d87-8eb8-4c682987c2a4.cpp:26:12: note: to match this ‘{’    26 | int main() {       |            ^

--- CPP_20.cpp
+++ CPP_20.cpp
@@ -1,5 +1,5 @@
-int main() {
-    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1}));
+int main {
+    assert(issame(find_closest_elements({1.1, 2.2, 3.1, 4.1, 5.1}), {2.2, 3.1});
     std::cout << "Test passed!";
     return 0;
 }
