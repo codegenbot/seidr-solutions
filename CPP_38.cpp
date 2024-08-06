e2ffb0d204ae84d37af8e16c0c4614e985d9bd13
beep boop <githubbot@vadim.me>
2024-08-06 10:11:45 +0200

    Solution 32 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt:                  from bee2b738-8795-4515-8085-ad2cc9a7f825.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ bee2b738-8795-4515-8085-ad2cc9a7f825.cpp:47:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    47 |             str+=chr;       |             ^~~       |             std bee2b738-8795-4515-8085-ad2cc9a7f825.cpp:50:15: error: expected ‘;’ before ‘enco ded_str’    50 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from bee2b738-8795-4515-8085-ad2cc9a7f825.cpp:37: bee2b738-8795-4515-8085-ad2cc9a7f825.cpp:51:31: error: ‘encoded_str’ was not dec lared in this scope    51 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ bee2b738-8795-4515-8085-ad2cc9a7f825.cpp:51:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    51 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <string>
 #include <cassert>
 
 std::string encode_cyclic(std::string str){
     std::string output;
-    for (int i=0; i*3<str.length(); i++) {
+    for (size_t i=0; i*3<str.length(); i++) {
         std::string x = str.substr(i*3, 3);
         if (x.length() == 3) 
             x = x[2] + x.substr(0, 2);
@@ -14,7 +15,7 @@ std::string encode_cyclic(std::string str){
 
 std::string decode_cyclic(std::string str){
     std::string output;
-    for (int i=0; i*3<str.length(); i++) {
+    for (size_t i=0; i*3<str.length(); i++) {
         std::string x = str.substr(i*3, 3);
         if (x.length() == 3) 
             x = x[1] + x[2] + x[0];
@@ -27,9 +28,9 @@ int main() {
     std::string str, chr;
     std::cin >> str >> chr;
     std::string encoded_str;
-    str = str + chr;
+    str += chr;
     encoded_str = encode_cyclic(str);
-    assert(decode_cyclic(encoded_str) == str + chr);
+    assert(decode_cyclic(encoded_str) == str);
     std::cout << "Test passed" << std::endl;
     return 0;
 }
