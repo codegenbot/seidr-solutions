751e0229bf44c27c0f87274414cd6e1070c48e0d
beep boop <githubbot@vadim.me>
2024-08-06 09:48:36 +0200

    Solution 14 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/orkp4ey6 Prompt: In file included from /usr/include/c++/11/string:39,                  from e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp:13:44: error: ‘b’ was not declared in t his scope    13 | bool issame(vector<string> a,vector<string>b){       |                                            ^ In file included from e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp:12: e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp: In function ‘int main()’: e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp:22:20: error: ‘all_prefixes’ was not de clared in this scope    22 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |                    ^~~~~~~~~~~~ e6411528-a913-4d2e-a128-3d7ff58f7e6d.cpp:22:19: error: ‘issame’ cannot be used a s a function    22 |     assert (issame(all_prefixes("WWW") ,{"W", "WW", "WWW"}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -3,23 +3,15 @@
 #include <cassert>
 
 std::vector<std::string> all_prefixes(std::string str) {
-    std::vector<std::string> prefixes;
+    std::vector<std::string> result;
     for (int i = 1; i <= str.size(); ++i) {
-        prefixes.push_back(str.substr(0, i));
+        result.push_back(str.substr(0, i));
     }
-    return prefixes;
+    return result;
 }
 
-bool issame(std::vector<std::string> a, std::vector<std::string> b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) {
-            return false;
-        }
-    }
-    return true;
+bool issame(std::vector<std::string> a, std::vector<std::string> b){
+    return a == b;
 }
 
 int main() {
