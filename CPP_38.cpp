d5f4ec5241b6451eb94e061d02eec9546db90617
beep boop <githubbot@vadim.me>
2024-08-06 10:13:43 +0200

    Solution 50 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt: c_cast’    10 | assert(decode_cyclic(encoded_str) == str);       | ^~~~~~ 72c772a3-d110-4f48-a940-0fe37da32318.cpp: In function ‘int main()’: 72c772a3-d110-4f48-a940-0fe37da32318.cpp:18:9: error: ‘string’ was not declared in this scope    18 |         string str="";       |         ^~~~~~ 72c772a3-d110-4f48-a940-0fe37da32318.cpp:18:9: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 72c772a3-d110-4f48-a940-0fe37da32318.cpp:1: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 72c772a3-d110-4f48-a940-0fe37da32318.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 72c772a3-d110-4f48-a940-0fe37da32318.cpp:25:15: error: expected ‘;’ before ‘enco ded_str’    25 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ;

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,7 +1,18 @@
 #include <string>
+#include <cassert>
 
-std::string str = "";
-char chr = 'a';
-str += chr;
-std::string encoded_str = encode_cyclic(str);
-assert(decode_cyclic(encoded_str) == str);
+std::string encode_cyclic(std::string str) {
+    return str;
+}
+
+std::string decode_cyclic(std::string str) {
+    return str;
+}
+
+int main() {
+    std::string str = "";
+    char chr = 'a';
+    str += chr;
+    std::string encoded_str = encode_cyclic(str);
+    assert(decode_cyclic(encoded_str) == str);
+}
