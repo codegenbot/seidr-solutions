dd1f5501344a216f233a1191eceeea7190732e7d
beep boop <githubbot@vadim.me>
2024-08-06 10:12:30 +0200

    Solution 39 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt:                  from ffa4bfbc-1242-4ce7-9396-b10dc7567fc4.cpp:1: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ ffa4bfbc-1242-4ce7-9396-b10dc7567fc4.cpp:46:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    46 |             str+=chr;       |             ^~~       |             std ffa4bfbc-1242-4ce7-9396-b10dc7567fc4.cpp:49:15: error: expected ‘;’ before ‘enco ded_str’    49 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from ffa4bfbc-1242-4ce7-9396-b10dc7567fc4.cpp:36: ffa4bfbc-1242-4ce7-9396-b10dc7567fc4.cpp:50:31: error: ‘encoded_str’ was not dec lared in this scope    50 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ ffa4bfbc-1242-4ce7-9396-b10dc7567fc4.cpp:50:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    50 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,1 +1,12 @@
-std::string str;
+#include <iostream>
+#include <cassert>
+
+int main() {
+    std::string str, chr;
+    std::cin >> str >> chr;
+    str += chr;
+    std::string encoded_str = encode_cyclic(str);
+    assert(decode_cyclic(encoded_str) == str);
+    std::cout << "Test passed" << std::endl;
+    return 0;
+}
