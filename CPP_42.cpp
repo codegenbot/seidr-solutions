29ea85dd48719ced54b1c77191b817ecd57e336d
beep boop <githubbot@vadim.me>
2024-08-06 10:21:08 +0200

    Solution 42 of CPP/42, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/2wo02f89 Prompt: )’    33 | int main(){       |     ^~~~ 35d69835-80bf-4c33-a763-7a399ec57d02.cpp:19:5: note: ‘int main()’ previously def ined here    19 | int main() {       |     ^~~~ In file included from 35d69835-80bf-4c33-a763-7a399ec57d02.cpp:24: 35d69835-80bf-4c33-a763-7a399ec57d02.cpp: In function ‘int main()’: 35d69835-80bf-4c33-a763-7a399ec57d02.cpp:34:19: error: call of overloaded ‘issam e(std::vector<int>, <brace-enclosed initializer list>)’ is ambiguous    34 |     assert (issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}) , {6, 3, 6, 3, 4, 4, 10, 1, 124}));       |             ~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ~~~~~~~~~~~~~~~~~~~~~ 35d69835-80bf-4c33-a763-7a399ec57d02.cpp:7:6: note: candidate: ‘bool issame(cons t std::vector<int>&, const std::vector<int>&)’     7 | bool issame(const vector<int>& a, const vector<int>& b) {       |      ^~~~~~ 35d69835-80bf-4c33-a763-7a399ec57d02.cpp:25:6: note: candidate: ‘bool issame(std ::vector<int>, std::vector<int>)’    25 | bool issame(vector<int> a,vector<int>b){       |      ^~~~~~

--- CPP_42.cpp
+++ CPP_42.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+bool issame(const vector<int>& a, const vector<int>& b);
+
 vector<int> incr_list(const vector<int>& l) {
     vector<int> result = l; 
     for (int i = 0; i < result.size(); ++i) {
@@ -12,11 +14,11 @@ vector<int> incr_list(const vector<int>& l) {
     return result;
 }
 
-bool issame(vector<int> a, vector<int> b) {
+bool issame(const vector<int>& a, const vector<int>& b) {
     return a == b;
 }
 
 int main() {
-    assert(issame(incr_list({5, 2, 5, 2, 3, 3, 9, 0, 123}), vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124}));
+    assert(issame(incr_list(vector<int>{5, 2, 5, 2, 3, 3, 9, 0, 123}), vector<int>{6, 3, 6, 3, 4, 4, 10, 1, 124}));
     return 0;
 }
