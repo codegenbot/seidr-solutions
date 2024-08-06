8661c0be221f72df97c98253bf8ffc16d3b59fef
beep boop <githubbot@vadim.me>
2024-08-06 10:12:09 +0200

    Solution 36 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt: 0613c30c-6219-4724-929d-3b1b812f008b.cpp:14:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    14 |             str+=chr;       |             ^~~       |             std 0613c30c-6219-4724-929d-3b1b812f008b.cpp:17:15: error: expected ‘;’ before ‘enco ded_str’    17 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 0613c30c-6219-4724-929d-3b1b812f008b.cpp:4: 0613c30c-6219-4724-929d-3b1b812f008b.cpp:18:31: error: ‘encoded_str’ was not dec lared in this scope    18 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 0613c30c-6219-4724-929d-3b1b812f008b.cpp:18:17: error: ‘decode_cyclic’ was not d eclared in this scope    18 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ 0613c30c-6219-4724-929d-3b1b812f008b.cpp:18:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    18 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,4 +1,4 @@
-string str = "";
-str += chr;
-string encoded_str = encode_cyclic(str);
+string str; 
+str += chr; 
+string encoded_str = encode_cyclic(str); 
 assert(decode_cyclic(encoded_str) == str);
