a9edd41518d1195dfbd9d03ff3255ef6ae565e5a
beep boop <githubbot@vadim.me>
2024-08-06 10:08:15 +0200

    Solution 7 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt: d6c68030-e0f1-45f1-900d-021378661d15.cpp:24:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    24 |             str+=chr;       |             ^~~       |             std d6c68030-e0f1-45f1-900d-021378661d15.cpp:27:15: error: expected ‘;’ before ‘enco ded_str’    27 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from d6c68030-e0f1-45f1-900d-021378661d15.cpp:14: d6c68030-e0f1-45f1-900d-021378661d15.cpp:28:31: error: ‘encoded_str’ was not dec lared in this scope    28 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ d6c68030-e0f1-45f1-900d-021378661d15.cpp:28:17: error: ‘decode_cyclic’ was not d eclared in this scope    28 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ d6c68030-e0f1-45f1-900d-021378661d15.cpp:28:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    28 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -6,11 +6,13 @@ std::string encode_cyclic(const std::string& s);
 std::string decode_cyclic(std::string s) { 
     int l = s.length();
     std::string x, output;
-    for (int i = 0; i * 3 < l; i++) {
+    int i;
+    for (i = 0; i * 3 < l; i++) {
         x = s.substr(i * 3, 3);
-        if (x.length() == 3) 
+        if (x.length() == 3) {
             x = x[2] + x.substr(0, 2);
-        output += x;
+        }
+        output = output + x;
     }
     return output;
 }
