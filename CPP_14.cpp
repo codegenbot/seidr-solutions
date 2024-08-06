6908b143c53d8d6d8065ca0fbb96b59c6d6e5646
beep boop <githubbot@vadim.me>
2024-08-06 09:49:12 +0200

    Solution 18 of CPP/14, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/orkp4ey6 Prompt:       |                                     ^~~~~~ 39e4378c-1a46-488e-a5e4-9c8bd29c492f.cpp:32:37: note: suggested alternatives: In file included from /usr/include/c++/11/string:39,                  from 39e4378c-1a46-488e-a5e4-9c8bd29c492f.cpp:2: /usr/include/c++/11/bits/stringfwd.h:79:33: note:   ‘std::string’    79 |   typedef basic_string<char>    string;       |                                 ^~~~~~ In file included from 39e4378c-1a46-488e-a5e4-9c8bd29c492f.cpp:2: /usr/include/c++/11/string:67:11: note:   ‘std::pmr::string’    67 |     using string    = basic_string<char>;       |           ^~~~~~ 39e4378c-1a46-488e-a5e4-9c8bd29c492f.cpp:32:44: error: ‘b’ was not declared in t his scope    32 | bool issame(vector<string> a,vector<string>b){       |                                            ^ 39e4378c-1a46-488e-a5e4-9c8bd29c492f.cpp:40:5: error: redefinition of ‘int main( )’    40 | int main(){       |     ^~~~ 39e4378c-1a46-488e-a5e4-9c8bd29c492f.cpp:27:5: note: ‘int main()’ previously def ined here    27 | int main() {       |     ^~~~

--- CPP_14.cpp
+++ CPP_14.cpp
@@ -1,19 +1,30 @@
 #include <vector>
 #include <string>
+#include <cassert>
 
-vector<string> all_prefixes(string str) {
-    vector<string> result;
-    for (int i = 1; i <= str.size(); ++i) {
-        result.push_back(str.substr(0, i));
+std::vector<std::string> all_prefixes(std::string str) {
+    std::vector<std::string> prefixes;
+    std::string prefix;
+    for (char c : str) {
+        prefix += c;
+        prefixes.push_back(prefix);
     }
-    return result;
+    return prefixes;
 }
 
-bool issame(vector<string> a, vector<string> b) {
-    return a == b;
+bool issame(std::vector<std::string> a, std::vector<std::string> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
-    assert(issame(all_prefixes("WWW"), {"W", "WW", "WWW"}));
+    assert(issame(all_prefixes("WWW"), std::vector<std::string>{"W", "WW", "WWW"}));
     return 0;
 }
