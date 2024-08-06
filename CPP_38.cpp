9cc082e0ef40f259a4970e14241a12459e51c578
beep boop <githubbot@vadim.me>
2024-08-06 10:09:26 +0200

    Solution 16 of CPP/38, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-humaneval-gpt-3.5-turbo-run6/runs/884ltvep Prompt: 09639225-dd1c-4b23-b25b-667425c80f99.cpp:23:13: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    23 |             str+=chr;       |             ^~~       |             std 09639225-dd1c-4b23-b25b-667425c80f99.cpp:26:15: error: expected ‘;’ before ‘enco ded_str’    26 |         string encoded_str = encode_cyclic(str);       |               ^~~~~~~~~~~~       |               ; In file included from 09639225-dd1c-4b23-b25b-667425c80f99.cpp:13: 09639225-dd1c-4b23-b25b-667425c80f99.cpp:27:31: error: ‘encoded_str’ was not dec lared in this scope    27 |         assert (decode_cyclic(encoded_str) == str);       |                               ^~~~~~~~~~~ 09639225-dd1c-4b23-b25b-667425c80f99.cpp:27:17: error: ‘decode_cyclic’ was not d eclared in this scope    27 |         assert (decode_cyclic(encoded_str) == str);       |                 ^~~~~~~~~~~~~ 09639225-dd1c-4b23-b25b-667425c80f99.cpp:27:47: error: ‘str’ was not declared in  this scope; did you mean ‘std’?    27 |         assert (decode_cyclic(encoded_str) == str);       |                                               ^~~

--- CPP_38.cpp
+++ CPP_38.cpp
@@ -1,23 +1,13 @@
-string encode_cyclic(string str) {
-    int len = str.length();
-    string encoded_str;
-    for (int i = 0; i * 3 < len; i++) {
-        string substr = str.substr(i * 3, 3);
-        if (substr.length() == 3) 
-            substr = substr[1] + substr[2] + substr[0];
-        encoded_str += substr;
-    }
-    return encoded_str;
-}
+string encode_cyclic(string decoded_str); 
 
-string decode_cyclic(string encoded_str) {
+string decode_cyclic(string encoded_str) { 
     int len = encoded_str.length();
     string output;
-    for (int i = 0; i * 3 < len; i++) {
-        string substr = encoded_str.substr(i * 3, 3);
-        if (substr.length() == 3) 
-            substr = substr[2] + substr.substr(0, 2);
-        output += substr;
-    }
+    for (int i = 0; i * 3 < len; i++) { 
+        string str = encoded_str.substr(i * 3, 3);
+        if (str.length() == 3) 
+            str = str[2] + str.substr(0, 2); 
+        output += str;
+    } 
     return output;
 }
