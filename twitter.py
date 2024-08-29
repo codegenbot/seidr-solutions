81f45fa55ef83c58bcfb370ce074ee17ca6a1afc
beep boop <githubbot@vadim.me>
2024-08-29 21:54:46 +0200

    Solution 11 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run4/runs/5jtc85sh Prompt: Given a string representing a tweet, validate whether the tweet meets Twitter’s original character requirements. If the tweet has more than 140 characters, return the string "Too many characters". If the tweet is empty, return the string "You didn't type anything". Otherwise, return "Your tweet has X characters", where the X is the number of characters in the tweet.

--- twitter.py
+++ twitter.py
@@ -1,4 +1,7 @@
-def tweet_validator(tweet):
+Here is the solution to the problem:
+
+```
+def validate_tweet(tweet):
     if len(tweet) == 0:
         return "You didn't type anything"
     elif len(tweet) > 140:
@@ -6,22 +9,9 @@ def tweet_validator(tweet):
     else:
         return f"Your tweet has {len(tweet)} characters"
 
-
-# Testing the function
-print(tweet_validator(""))  # You didn't type anything
-print(tweet_validator("1"))  # Your tweet has 1 characters
-print(
-    tweet_validator(
-        "max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it"
-    )
-)  # Your tweet has 140 characters
-print(
-    tweet_validator(
-        "40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``"
-    )
-)  # Your tweet has 90 characters
-print(
-    tweet_validator(
-        "Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1"
-    )
-)  # Too many characters
+# Test cases
+print(validate_tweet("")) # You didn't type anything
+print(validate_tweet("1")) # Your tweet has 1 characters
+print(validate_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it")) # Your tweet has 140 characters
+print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``")) # Your tweet has 90 characters
+print(validate_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1")) # Too many characters
