7ed8c416e2812526708f58c3e35b6adea8535ca0
beep boop <githubbot@vadim.me>
2024-08-29 21:51:54 +0200

    Solution 98 of camel-case, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run3/runs/shbhv0ps Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/88aab637-3e32-43c1-86bf-2a3e146e8 b83.py", line 1     Here is the solution:                 ^^^^^^^^ SyntaxError: invalid syntax

--- camel-case.py
+++ camel-case.py
@@ -1,4 +1,5 @@
 Here is the solution:
 
-def kebab_to_camel(s):
-    return ''.join(word.capitalize() for word in s.split('-')) if ' ' not in s else s
+Here is the solution:
+    def kebab_to_camel(s):
+        return ''.join(word.capitalize() for word in s.split('-')) if ' ' not in s else s
