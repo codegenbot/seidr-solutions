3d020a9479ecfb3a67ff0dc0e127f7313814f7a7
beep boop <githubbot@vadim.me>
2024-08-30 10:58:24 +0200

    Solution 14 of mastermind, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run5/runs/eyrp84zr Prompt: Based on the board game Mastermind. Given a Mastermind code and a guess, each of which are 4-character strings consisting of 6 possible characters, return the number of white pegs (correct color, wrong place) and black pegs (correct color, correct place) the codemaster should give as a clue.

--- mastermind.py
+++ mastermind.py
@@ -1,11 +1,9 @@
 def mastermind(code, guess):
     black = 0
     white = 0
-
     for i in range(4):
         if code[i] == guess[i]:
             black += 1
-        elif str(code[i]) in guess:
+        elif code[i] in guess[i:]:
             white += 1
-
-    return str(black) + "\n" + str(white)
+    return str(white) + "\n" + str(black)
