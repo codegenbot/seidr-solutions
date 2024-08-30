231cd797e8442271c33c9d3312da2ab3ab579030
beep boop <githubbot@vadim.me>
2024-08-30 12:03:59 +0200

    Solution 45 of shopping-list, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/m6hzsc3a Prompt: Traceback (most recent call last):   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41e1e389-f9b9-4618-8c2c-a8de833d0 beb.py", line 14, in <module>     print(shopping_list())           ^^^^^^^^^^^^^^^   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/41e1e389-f9b9-4618-8c2c-a8de833d0 beb.py", line 8, in shopping_list     price = prices[i] * (1 - discounts[i] / 100)                              ~~~~~~~~~^^^ IndexError: list index out of range

--- shopping-list.py
+++ shopping-list.py
@@ -1,3 +1,4 @@
+```
 def shopping_list():
     n = int(input())
     prices = list(map(float, (input().split())[0:n]))
@@ -9,5 +10,6 @@ def shopping_list():
         total_price += price
 
     return round(total_price, 2)
+
 
 print(shopping_list())
