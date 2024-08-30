29579c638cac8ef407cf10e67dcb9abbb9b3e41e
beep boop <githubbot@vadim.me>
2024-08-30 12:15:52 +0200

    Solution 37 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/pw01wuae Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/9413adab-3bc3-4e9c-91cb-35e6f53fa 2e4.py", line 12     print(validate_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214 876*%^#!(@^$_!@^%#$(!#@%$(01234~~``)")          ^ SyntaxError: '(' was never closed

--- twitter.py
+++ twitter.py
@@ -1,7 +1,8 @@
 ```
 import re
+
 def validate_tweet(tweet):
-    if len(re.sub(r'[^a-zA-Z0-9\s]', '', tweet)) > 140:
+    if not re.match("^[a-zA-Z0-9\s\(\)\!\@\#\$\%\^\*\\\/\-\=\+\[\]\{\}\|_~`]+$", tweet):
         return "Too many characters"
     elif len(tweet) == 0:
         return "You didn't type anything"
