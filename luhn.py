40e5840f6857f8f7ce7a73ca5612f525b781f735
beep boop <githubbot@vadim.me>
2024-08-30 11:10:04 +0200

    Solution 66 of luhn, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/w2d8ciub Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/03713e1c-03d3-4bc9-82e2-560dd3d7c 726.py", line 1     ```     ^ SyntaxError: invalid syntax

--- luhn.py
+++ luhn.py
@@ -7,7 +7,7 @@ def luhn(card):
             num *= 2
             if num > 9:
                 num -= 9
-            total += num 
+            total += num  
         else:
             total += num
 
