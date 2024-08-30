726353fe3d340829d50bcba21e29bf058ee9e00f
beep boop <githubbot@vadim.me>
2024-08-30 11:19:10 +0200

    Solution 30 of bowling, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/id69epee Prompt:   File "/global/D1/homes/anastasiia/research/secureIT/nl2ml-codex/venv_poetry/li b/python3.11/site-packages/programlib/programs/33dfd291-62e7-4a19-90bb-12ad330c7 703.py", line 1     ```     ^ SyntaxError: invalid syntax

--- bowling.py
+++ bowling.py
@@ -10,7 +10,7 @@ def bowling_score(score):
             roll += 1
             i += 1
         elif i < 9 and frames[i] != 10:
-            total += min(frames[i], 10) + (10 - frames[i]) if roll > 0 else 0
+            total += min(frames[i], 10) + (10 - min(frames[i], 10)) if roll > 0 else 0
             roll = max(roll, 1)
         else:
             total += frames[i]
