16be8c1b966ee6602c02778f80c4197a36752f8a
beep boop <githubbot@vadim.me>
2024-08-30 12:04:55 +0200

    Solution 12 of twitter, ratio 0.0 of 1.00 of tests passed. W&B: https://wandb.ai/codex-for-psb/seidr-telo-psb2-llama3-run6/runs/pw01wuae Prompt: Given a string representing a tweet, validate whether the tweet meets Twitter’s original character requirements. If the tweet has more than 140 characters, return the string "Too many characters". If the tweet is empty, return the string "You didn't type anything". Otherwise, return "Your tweet has X characters", where the X is the number of characters in the tweet.

--- twitter.py
+++ twitter.py
@@ -1,7 +1,16 @@
-def validate_tweet(t):
-    if len(t) == 0:
+Here is the completed code:
+
+def check_tweet(tweet):
+    if len(tweet) == 0:
         return "You didn't type anything"
-    elif len(t) > 140:
+    elif len(tweet) > 140:
         return "Too many characters"
     else:
-        return f"Your tweet has {len(t)} characters"
+        return f"Your tweet has {len(tweet)} characters"
+
+# test cases
+print(check_tweet("")) # input: ""
+print(check_tweet("1")) # input: "1"
+print(check_tweet("max length tweet that just contains letters and spaces even SOME CAPITAL LETTERS just to MAKE it INTERESTING now repeeeeeeeeeEEEEEEEeeeat it")) # max length tweet that just contains letters and spaces
+print(check_tweet("40172875*&(&(%^^*!@&#()!@&^(*$787031264123984721-43214876*%^#!(@^$_!@^%#$(!#@%$(01234~~``")) # long but not too long tweet
+print(check_tweet("Tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooolong1")) # very long tweet
